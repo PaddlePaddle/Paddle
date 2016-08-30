291ff97cd2302661e6e007b587f49a656a257a6d
yuyang18 <yuyang18@1ad973e4-5ce8-4261-8a94-b56d1f490c56>
2016-08-30 16:15:45 +0000

    Try Fix docker hub build.
    ISSUE=4599521
    
    
    git-svn-id: https://svn.baidu.com/idl/trunk/paddle@1434 1ad973e4-5ce8-4261-8a94-b56d1f490c56

--- paddle/scripts/docker/cpu-devel/build.sh
+++ paddle/scripts/docker/cpu-devel/build.sh
@@ -13,54 +13,16 @@ apt-get install -y cmake libprotobuf-dev protobuf-compiler git \
     libgoogle-glog-dev libgflags-dev libatlas-dev libatlas3-base g++ m4 python-pip\
     python-protobuf python-numpy python-dev swig
 
-if [ ${WITH_GPU} == "ON" ]; then  # install cuda
-  cd ~
-  apt-get install -y aria2 wget
-  echo "Downloading cuda tookit"
-  set +e
-  for ((i=0; i<100; i++))
-  do
-    aria2c -x 10 -s 10 --lowest-speed-limit=${LOWEST_DL_SPEED} http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run
-    if [ $? -eq 0 ]; then
-       break
-    fi
-  done
-
-  set -e
-  wget http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run.md5
-  md5sum -c cuda_7.5.18_linux.run.md5
-  chmod +x cuda_7.5.18_linux.run
-  ./cuda_7.5.18_linux.run --extract=$PWD
-  mv cuda-linux64-rel-7.5.18-*.run tmp
-  rm *.run *.run.md5
-  mv tmp cuda-linux64.run
-  ./cuda-linux64.run -noprompt
-  rm *.run
-
-  echo "Downloading cudnn v5.1"
-  set +e
-  for ((i=0; i<100; i++))
-  do
-    aria2c -x 10 --lowest-speed-limit=${LOWEST_DL_SPEED} http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-7.5-linux-x64-v5.1.tgz
-    if [ $? -eq 0 ]; then
-        break
-    fi
-  done
-  set -e
-  echo "$CUDNN_DOWNLOAD_SUM  cudnn-7.5-linux-x64-v5.1.tgz" | sha256sum -c --strict -
-  tar -xzf cudnn-7.5-linux-x64-v5.1.tgz -C /usr/local
-  rm cudnn-7.5-linux-x64-v5.1.tgz
-  ldconfig
-  export PATH=/usr/local/cuda/bin:$PATH
-  apt-get purge -y aria2
+if [ ${WITH_GPU} == 'ON' ]; then
+  ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so /usr/lib/libcudnn.so
 fi
-set -e
+
 cd ~
 git clone https://github.com/baidu/Paddle.git paddle
 cd paddle
 mkdir build
 cd build
-cmake .. -DWITH_DOC=OFF -DWITH_GPU=${WITH_GPU} -DWITH_SWIG_PY=ON
+cmake .. -DWITH_DOC=OFF -DWITH_GPU=${WITH_GPU} -DWITH_SWIG_PY=ON -DCUDNN_ROOT=/usr/
 make -j `nproc`
 # because durning make install, there are several warning, so set +e, do not cause abort
 make install
@@ -75,7 +37,6 @@ if [ ${WITH_DEMO} == "ON" ]; then
 fi
 if [ ${IS_DEVEL} == "OFF" ]; then  # clean build packages.
   cd ~
-  # TODO(yuyang18): Do clean for devel package, and cuda devel tools
   rm -rf paddle
 fi
 apt-get clean -y
