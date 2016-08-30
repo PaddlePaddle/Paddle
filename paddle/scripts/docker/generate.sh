a46749b3b06ed6b4e3e58d134105d5fbfe9fcec4
yuyang18 <yuyang18@1ad973e4-5ce8-4261-8a94-b56d1f490c56>
2016-08-30 16:21:16 +0000

    Fix ubuntu image prefix
    ISSUE=4599563
    
    
    git-svn-id: https://svn.baidu.com/idl/trunk/paddle@1435 1ad973e4-5ce8-4261-8a94-b56d1f490c56

--- paddle/scripts/docker/generate.sh
+++ paddle/scripts/docker/generate.sh
@@ -2,17 +2,17 @@
 set -e
 cd `dirname $0`
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=OFF -DPADDLE_WITH_DEMO=OFF \
-   -DPADDLE_BASE_IMAGE=ubuntu/ubuntu:14.04\
+   -DPADDLE_BASE_IMAGE=ubuntu:14.04\
    Dockerfile.m4 > cpu/Dockerfile
 cp build.sh cpu/
 
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=OFF \
-   -DPADDLE_BASE_IMAGE=ubuntu/ubuntu:14.04\
+   -DPADDLE_BASE_IMAGE=ubuntu:14.04\
    Dockerfile.m4 > cpu-devel/Dockerfile
 cp build.sh cpu-devel/
 
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=ON \
-   -DPADDLE_BASE_IMAGE=ubuntu/ubuntu:14.04\
+   -DPADDLE_BASE_IMAGE=ubuntu:14.04\
    Dockerfile.m4 > cpu-demo/Dockerfile
 cp build.sh cpu-demo/
 
