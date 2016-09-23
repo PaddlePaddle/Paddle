327b8b9765c63b8a3d0c59a7bcba0f5aa546b2e2
Yi Wang <yiwang01@baidu.com>
2016-09-23 16:02:51 -0700

    Make sure outputs N instances

--- demo/quick_start/yi.py
+++ demo/quick_start/yi.py
@@ -18,7 +18,6 @@ def parse(inputFile, firstNRecords, outputDir, topNWords):
     """
     train = open(os.path.join(outputDir, 'train.txt'), 'w')
     test = open(os.path.join(outputDir, 'test.txt'), 'w')
-    dict = open(os.path.join(outputDir, 'dict.txt'), 'w')
 
     if inputFile.startswith("http"):
         request = urllib2.Request(inputFile)
@@ -28,12 +27,11 @@ def parse(inputFile, firstNRecords, outputDir, topNWords):
     else:
         g = gzip.open(inputFile, 'r')
 
-    lines = 0
+    written = 0
     wc = {}
 
     for l in g:
-        lines = lines + 1
-        if lines > firstNRecords:
+        if written >= firstNRecords:
             break
 
         o = train
@@ -53,35 +51,36 @@ def parse(inputFile, firstNRecords, outputDir, topNWords):
         if text:
             if rate == 5.0:
                 o.write('1\t%s\n' % text)
+                written = written + 1
             elif rate < 3.0:
                 o.write('0\t%s\n' % text)
+                written = written + 1
 
-    g.close()
     train.close()
     test.close()
+    g.close()
 
-    dict.write('%s\t%s\n' % ('unk', '-1'))
-    c = 0
-    for k, v in sorted(wc.items(), key=operator.itemgetter(1), reverse=True):
-        dict.write('%s\t%s\n' % (k, v))
-        c = c + 1
-        if c > topNWords:
-            break
-    dict.close()
-
+    with open(os.path.join(outputDir, 'dict.txt'), 'w') as dict:
+        dict.write('%s\t%s\n' % ('unk', '-1'))
+        c = 0
+        for k, v in sorted(wc.items(), key=operator.itemgetter(1), reverse=True):
+            dict.write('%s\t%s\n' % (k, v))
+            c = c + 1
+            if c > topNWords:
+                break
 
 if __name__ == '__main__':
     parser = OptionParser(usage="usage: %prog [options] filename",
                           version="%prog 1.0")
     parser.add_option("-n", "--firstn", type="int", dest="firstN", default=100,
-                      help="Use the first N number of instances")
+                      help="Generate train & test sets with N instances in total.")
     parser.add_option("-t", "--topn", type="int", dest="topN", default=30001,
                       help="Save the top N frequent words into dict.txt",)
     (options, args) = parser.parse_args()
 
     inputFile = args[0] if len(args) > 0 else defaultFile
     outputDir = os.path.dirname(sys.argv[0]) or './'
-    print "Processing the first %d records in %s. Writing to %s ..." % (options.firstN, inputFile, outputDir)
+    print "Processing the first %d records in %s. Writing to %s." % (options.firstN, inputFile, outputDir)
 
     random.seed(1)
     parse(inputFile, options.firstN, outputDir, max(1, options.topN))
