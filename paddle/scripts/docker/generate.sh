b605a255714ffb69e880a490c021ce206b91c113
Yu Yang <yuyang18@baidu.com>
2016-09-28 14:42:33 +0800

    Not to use directory to split Docker image.

--- paddle/scripts/docker/generate.sh
+++ paddle/scripts/docker/generate.sh
@@ -3,72 +3,59 @@ set -e
 cd `dirname $0`
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=OFF -DPADDLE_WITH_DEMO=OFF \
    -DPADDLE_BASE_IMAGE=ubuntu:14.04 -DPADDLE_WITH_AVX=ON\
-   Dockerfile.m4 > cpu/Dockerfile
-cp build.sh cpu/
+   Dockerfile.m4 > Dockerfile.cpu
 
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=OFF -DPADDLE_WITH_DEMO=OFF \
    -DPADDLE_BASE_IMAGE=ubuntu:14.04 -DPADDLE_WITH_AVX=OFF\
-   Dockerfile.m4 > cpu-noavx/Dockerfile
-cp build.sh cpu-noavx/
+   Dockerfile.m4 > Dockerfile.cpu-noavx
 
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=OFF \
    -DPADDLE_BASE_IMAGE=ubuntu:14.04 -DPADDLE_WITH_AVX=OFF\
-   Dockerfile.m4 > cpu-noavx-devel/Dockerfile
-cp build.sh cpu-noavx-devel/
+   Dockerfile.m4 > Dockerfile.cpu-noavx-devel
 
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=OFF \
    -DPADDLE_BASE_IMAGE=ubuntu:14.04 -DPADDLE_WITH_AVX=ON\
-   Dockerfile.m4 > cpu-devel/Dockerfile
-cp build.sh cpu-devel/
+   Dockerfile.m4 > Dockerfile.cpu-devel
 
 
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=ON \
    -DPADDLE_BASE_IMAGE=ubuntu:14.04 -DPADDLE_WITH_AVX=ON\
-   Dockerfile.m4 > cpu-demo/Dockerfile
-cp build.sh cpu-demo/
+   Dockerfile.m4 > Dockerfile.cpu-demo
 
 m4 -DPADDLE_WITH_GPU=OFF -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=ON \
    -DPADDLE_BASE_IMAGE=ubuntu:14.04 -DPADDLE_WITH_AVX=OFF\
-   Dockerfile.m4 > cpu-noavx-demo/Dockerfile
-cp build.sh cpu-noavx-demo/
+   Dockerfile.m4 > Dockerfile.cpu-noavx-demo
 
 
 m4 -DPADDLE_WITH_GPU=ON -DPADDLE_IS_DEVEL=OFF -DPADDLE_WITH_DEMO=OFF \
    -DPADDLE_BASE_IMAGE=nvidia/cuda:7.5-cudnn5-devel-ubuntu14.04 \
    -DPADDLE_WITH_AVX=ON \
-   Dockerfile.m4 > gpu/Dockerfile
-cp build.sh gpu/
+   Dockerfile.m4 > Dockerfile.gpu
 
 m4 -DPADDLE_WITH_GPU=ON -DPADDLE_IS_DEVEL=OFF -DPADDLE_WITH_DEMO=OFF \
    -DPADDLE_BASE_IMAGE=nvidia/cuda:7.5-cudnn5-devel-ubuntu14.04 \
    -DPADDLE_WITH_AVX=OFF \
-   Dockerfile.m4 > gpu-noavx/Dockerfile
-cp build.sh gpu-noavx/
+   Dockerfile.m4 > Dockerfile.gpu-noavx
 
 
 m4 -DPADDLE_WITH_GPU=ON -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=OFF \
    -DPADDLE_BASE_IMAGE=nvidia/cuda:7.5-cudnn5-devel-ubuntu14.04 \
    -DPADDLE_WITH_AVX=ON \
-   Dockerfile.m4 > gpu-devel/Dockerfile
-cp build.sh gpu-devel/
+   Dockerfile.m4 > Dockerfile.gpu-devel
 
 m4 -DPADDLE_WITH_GPU=ON -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=OFF \
    -DPADDLE_BASE_IMAGE=nvidia/cuda:7.5-cudnn5-devel-ubuntu14.04 \
    -DPADDLE_WITH_AVX=OFF \
-   Dockerfile.m4 > gpu-noavx-devel/Dockerfile
-cp build.sh gpu-noavx-devel/
+   Dockerfile.m4 > Dockerfile.gpu-noavx-devel
 
 m4 -DPADDLE_WITH_GPU=ON -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=ON \
    -DPADDLE_BASE_IMAGE=nvidia/cuda:7.5-cudnn5-devel-ubuntu14.04 \
    -DPADDLE_WITH_AVX=ON \
-   Dockerfile.m4 > gpu-demo/Dockerfile
-cp build.sh gpu-demo/
+   Dockerfile.m4 > Dockerfile.gpu-demo
 
 
 m4 -DPADDLE_WITH_GPU=ON -DPADDLE_IS_DEVEL=ON -DPADDLE_WITH_DEMO=ON \
    -DPADDLE_BASE_IMAGE=nvidia/cuda:7.5-cudnn5-devel-ubuntu14.04 \
    -DPADDLE_WITH_AVX=OFF \
-   Dockerfile.m4 > gpu-noavx-demo/Dockerfile
-cp build.sh gpu-noavx-demo/
-
+   Dockerfile.m4 > Dockerfile.gpu-noavx-demo
 
