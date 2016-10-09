2b6dd3d082c74cfb1d9bce1ceb638178d18594ac
liaogang <liaogang@baidu.com>
2016-10-09 15:52:56 +0800

    Fix bug on SpinLock unit test

--- paddle/utils/tests/test_SpinLock.cpp
+++ paddle/utils/tests/test_SpinLock.cpp
@@ -22,32 +22,30 @@ limitations under the License. */
 P_DEFINE_int32(test_thread_num, 100, "testing thread number");
 
 void testNormalImpl(size_t thread_num, const std::function
-    <void(size_t, size_t, paddle::SpinLock&)>& callback) {
- paddle::SpinLock mutex;
- std::vector<std::thread> threads;
- threads.reserve(thread_num);
+    <void(size_t, size_t&, paddle::SpinLock&)>& callback) {
+  paddle::SpinLock mutex;
+  std::vector<std::thread> threads;
+  threads.reserve(thread_num);
 
- for (size_t i = 0; i < thread_num; ++i) {
-    threads.emplace_back([&thread_num, &mutex, &callback]{
-        callback(thread_num, 0, mutex);
-    });
- }
-
- for (auto& thread : threads) {
-   thread.join();
- }
+  size_t count = 0;
+  for (size_t i = 0; i < thread_num; ++i) {
+      threads.emplace_back([&thread_num, &count, &mutex, &callback]{
+          callback(thread_num, count, mutex);
+      });
+  }
+  for (auto& thread : threads) {
+    thread.join();
+  }
+  // Check whether all threads reach this point or not
+  CHECK_EQ(count, thread_num);
 }
 
 TEST(ThreadSpinLock, normalTest) {
   for (auto &thread_num : {10, 30, 50 , 100 , 300, 1000}) {
     testNormalImpl(thread_num, [](size_t thread_num,
-        size_t count, paddle::SpinLock& mutex){
-      for (size_t i = 0; i < thread_num; ++i) {
+        size_t& count, paddle::SpinLock& mutex){
         std::lock_guard<paddle::SpinLock> lock(mutex);
         ++count;
-      }
-      // Check whether all threads reach this point or not
-      CHECK_EQ(count, thread_num);
     });
   }
 }
