e4952ca6cea9130258086b4b7f2dff7a8bf5f566
Yu Yang <yuyang18@baidu.com>
2016-10-17 03:30:10 +0000

    Add FAQ (#128)
    * Init commit for doing FAQ
    
    * Add speed up training
    
    * Add graphviz to ci
    
    * Add shared paramter
    
    * Tiny refine

--- python/paddle/trainer_config_helpers/tests/configs/generate_protostr.sh
+++ python/paddle/trainer_config_helpers/tests/configs/generate_protostr.sh
@@ -8,7 +8,7 @@ configs=(test_fc layer_activations projections test_print_layer
 test_sequence_pooling test_lstmemory_layer test_grumemory_layer
 last_first_seq test_expand_layer test_ntm_layers test_hsigmoid
 img_layers util_layers simple_rnn_layers unused_layers test_cost_layers
-test_cost_layers_with_weight test_rnn_group)
+test_rnn_group shared_fc shared_lstm test_cost_layers_with_weight)
 
 
 for conf in ${configs[*]}
