3be9054bd8ea3e9ada42ab49938d08699ebba653
alvations <alvations@gmail.com>
2016-10-20 10:30:31 +0800

    Added zoo to LSTM quick_start examples

--- demo/quick_start/trainer_config.lstm.py
+++ demo/quick_start/trainer_config.lstm.py
@@ -24,6 +24,7 @@ with open(dict_file, 'r') as f:
         word_dict[w] = i
 
 is_predict = get_config_arg('is_predict', bool, False)
+network_type = get_config_arg('network_type', str, 'lstm')
 trn = 'data/train.list' if not is_predict else None
 tst = 'data/test.list' if not is_predict else 'data/pred.list'
 process = 'process' if not is_predict else 'process_predict'
@@ -42,21 +43,56 @@ settings(
     gradient_clipping_threshold=25
 )
 
-bias_attr = ParamAttr(initial_std=0.,l2_rate=0.)
+def lstm_architecture(bias_attr, data, emb):
+    fc = fc_layer(input=emb, size=512,
+                  act=LinearActivation(),
+                  bias_attr=bias_attr,
+                  layer_attr=ExtraAttr(drop_rate=0.1))
+    lstm = lstmemory(input=fc, act=TanhActivation(),
+                     bias_attr=bias_attr,
+                     layer_attr=ExtraAttr(drop_rate=0.25))
+    lstm_last = pooling_layer(input=lstm, pooling_type=MaxPooling())
+    output = fc_layer(input=lstm_last, size=2,
+                      bias_attr=bias_attr,
+                      act=SoftmaxActivation())
+    return output
 
+def dblstm_architecture(bias_attr, data, emb):
+    hidden_0 = mixed_layer(size=128, input=[full_matrix_projection(input=emb)])
+    lstm_0 = lstmemory(input=hidden_0, layer_attr=ExtraAttr(drop_rate=0.1))
+
+    input_layers = [hidden_0, lstm_0]
+    for i in range(1,8):
+        fc = fc_layer(input=input_layers, size=128)
+        lstm = lstmemory(input=fc, layer_attr=ExtraAttr(drop_rate=0.1),
+                        reverse=(i % 2) == 1,)
+        input_layers = [fc, lstm]
+
+    lstm_last = pooling_layer(input=lstm, pooling_type=MaxPooling())
+    output = fc_layer(input=lstm_last, size=2,
+                      bias_attr=bias_attr,
+                      act=SoftmaxActivation())
+    return output
+
+def bidilstm_architecture(bias_attr, data, emb):
+    bi_lstm = bidirectional_lstm(input=emb, size=128)
+    dropout = dropout_layer(input=bi_lstm, dropout_rate=0.5)
+
+    output = fc_layer(input=dropout, size=2,
+                      bias_attr=bias_attr,
+                      act=SoftmaxActivation())
+    return output
+
+
+bias_attr = ParamAttr(initial_std=0.,l2_rate=0.)
 data = data_layer(name="word", size=len(word_dict))
 emb = embedding_layer(input=data, size=128)
-fc = fc_layer(input=emb, size=512,
-              act=LinearActivation(),
-              bias_attr=bias_attr,
-              layer_attr=ExtraAttr(drop_rate=0.1))
-lstm = lstmemory(input=fc, act=TanhActivation(),
-                 bias_attr=bias_attr,
-                 layer_attr=ExtraAttr(drop_rate=0.25))
-lstm_last = pooling_layer(input=lstm, pooling_type=MaxPooling())
-output = fc_layer(input=lstm_last, size=2,
-                  bias_attr=bias_attr,
-                  act=SoftmaxActivation())
+lstm_architectures = {'lstm': lstm_architecture,
+                      'bidi-lstm': bidilstm_architecture,
+                      'db-lstm': dblstm_architecture}
+network = lstm_architectures[network_type]
+output = network(bias_attr, data, emb)
+
 if is_predict:
     maxid = maxid_layer(output)
     outputs([maxid, output])
