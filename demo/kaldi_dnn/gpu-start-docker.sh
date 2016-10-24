8dd2d8aae3faaef3af98e2528f930005c96ee563
Li Peng <lipeng@unisound.com>
2016-10-24 23:02:13 +0800

    Obtain the demo directory automatically
    Signed-off-by: Li Peng <lipeng@unisound.com>

--- demo/kaldi_dnn/gpu-start-docker.sh
+++ demo/kaldi_dnn/gpu-start-docker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
+work_dir=$(dirname $(dirname $(realpath $0)))
 
 sudo docker run -it --rm \
   --privileged \
-  -v /home/core/lipeng/unisound-ail/paddle/demo:/paddle/demo \
+  -v $work_dir:/paddle/demo \
   -v /home/core/paddle/data:/paddle/data \
   -v /var/lib/nvidia:/usr/local/nvidia/lib64 \
   harbor.ail.unisound.com/liuqs_public/paddle:gpu-latest /bin/bash
--- demo/kaldi_dnn/start-docker.sh
+++ demo/kaldi_dnn/start-docker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
+work_dir=$(dirname $(dirname $(realpath $0)))
 
 sudo docker run -it --rm \
-  -v /work/local/lipeng/github/unisound-ail/paddle/demo:/paddle/demo \
+  -v $work_dir:/paddle/demo \
   -v /work/local/paddle/data:/paddle/data \
   paddledev/paddle:cpu-latest /bin/bash
