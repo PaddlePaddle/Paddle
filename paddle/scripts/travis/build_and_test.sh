b6cdad69b2074dde1f28fe298d2addc56c477db1
liaogang <liaogang@baidu.com>
2016-10-31 23:14:48 +0800

    Add code coverage and coveralls

--- paddle/scripts/travis/build_and_test.sh
+++ paddle/scripts/travis/build_and_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./common.sh
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_GPU=OFF -DWITH_DOC=OFF -DWITH_TESTING=ON -DON_TRAVIS=ON
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_GPU=OFF -DWITH_DOC=OFF -DWITH_TESTING=ON -DON_TRAVIS=ON -DON_COVERALLS=ON
 make -j `nproc`
-env CTEST_OUTPUT_ON_FAILURE=1 make test ARGS="-j `nproc`"
+make coveralls
 sudo make install
 sudo paddle version
