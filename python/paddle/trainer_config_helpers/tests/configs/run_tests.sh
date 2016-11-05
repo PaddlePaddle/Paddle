5afa062d5ccd523212bd8e80281a0110371f3cfa
wen-bo-yang <ywb2545251@163.com>
2016-11-05 16:29:32 +0000

    modify run_tests.sh do not use check.md5 file

--- python/paddle/trainer_config_helpers/tests/configs/run_tests.sh
+++ python/paddle/trainer_config_helpers/tests/configs/run_tests.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 cd `dirname $0`
 set -e
+
+protostr=`dirname $0`/protostr
+files=`ls $protostr | grep -v "unitest"`
+
 ./generate_protostr.sh
-md5sum -c check.md5
+
+for file in $files
+do
+    base_protostr=$protostr/$file
+    new_protostr=$protostr/$file.unitest
+    if [ -f $new_protostr ];then
+        base_md5=`md5sum $base_protostr | awk '{print $1}'`
+        new_md5=`md5sum $new_protostr | awk '{print $1}'`
+        if [ $base_md5 != $new_md5 ];then
+            echo $new_protostr: FAILED!
+        else
+            echo $new_protostr: OK!
+        fi
+    else
+        echo WARNING $new_protostr: NOT EXIST!
+    fi
+done
