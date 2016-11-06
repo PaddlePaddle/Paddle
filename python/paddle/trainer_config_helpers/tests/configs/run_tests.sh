8989322b82eb06570a3facdf7a3c3ea699be9e87
wen-bo-yang <ywb2545251@163.com>
2016-11-06 05:54:23 +0000

    modify run_test.sh use diff command, and deleted check_protostr_diff.sh

--- python/paddle/trainer_config_helpers/tests/configs/run_tests.sh
+++ python/paddle/trainer_config_helpers/tests/configs/run_tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd `dirname $0`
-set -e
 
 protostr=`dirname $0`/protostr
 files=`ls $protostr | grep -v "unitest"`
@@ -12,11 +11,8 @@ do
     base_protostr=$protostr/$file
     new_protostr=$protostr/$file.unitest
     if [ -f $new_protostr ];then
-        base_md5=`md5sum $base_protostr | awk '{print $1}'`
-        new_md5=`md5sum $new_protostr | awk '{print $1}'`
-        if [ $base_md5 != $new_md5 ];then
-            echo $new_protostr: FAILED!
-        else
+        diff $base_protostr $new_protostr -u
+        if [ $? -eq 0 ];then
             echo $new_protostr: OK!
         fi
     else
