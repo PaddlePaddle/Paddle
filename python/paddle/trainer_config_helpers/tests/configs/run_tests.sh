defdce71642b3fed9cc688d12ca20bf4b5578c72
wen-bo-yang <ywb2545251@163.com>
2016-11-07 03:10:19 +0000

    modify run_tests.sh file and delete check.md5 file

--- python/paddle/trainer_config_helpers/tests/configs/run_tests.sh
+++ python/paddle/trainer_config_helpers/tests/configs/run_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd `dirname $0`
 
+set -e
+
 protostr=`dirname $0`/protostr
 
 files=`ls $protostr | grep -v "unitest"`
@@ -11,12 +13,6 @@ for file in $files
 do
     base_protostr=$protostr/$file
     new_protostr=$protostr/$file.unitest
-    if [ -f $new_protostr ];then
-        diff $base_protostr $new_protostr -u
-        if [ $? -eq 0 ];then
-            echo $new_protostr: OK!
-        fi
-    else
-        echo WARNING $new_protostr: NOT EXIST!
-    fi
+    diff $base_protostr $new_protostr
+    mv $new_protostr $protostr/$file.unitest.bak
 done
