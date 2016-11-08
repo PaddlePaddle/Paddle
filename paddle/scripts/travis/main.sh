9407c3793d25c2c8dec0a0bcc2c962e660991412
liaogang <liaogang@baidu.com>
2016-11-08 11:24:14 +0800

    Follow comments

--- paddle/scripts/travis/main.sh
+++ paddle/scripts/travis/main.sh
@@ -2,6 +2,13 @@
 cd `dirname $0`
 
 if [ ${JOB} == "BUILD_AND_TEST" ]; then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    TRAVIS_COMMIT_RANGE="FETCH_HEAD...$TRAVIS_BRANCH"
+  fi
+  git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md$)' || {
+    echo "Only markdown docs were updated, stopping build process."
+    exit
+  }
   ./build_and_test.sh
 elif [ ${JOB} == "DOCS" ]; then
   ./docs.sh
