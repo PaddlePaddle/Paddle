4607d517bfba481cbb02b2beb1dfa3773eadfded
Yu Yang <yuyang18@baidu.com>
2016-11-11 19:31:28 +0800

    Add unittest for split datasource
    * Fix #436

--- python/paddle/trainer_config_helpers/tests/configs/generate_protostr.sh
+++ python/paddle/trainer_config_helpers/tests/configs/generate_protostr.sh
@@ -11,12 +11,18 @@ test_sequence_pooling test_lstmemory_layer test_grumemory_layer
 last_first_seq test_expand_layer test_ntm_layers test_hsigmoid
 img_layers img_trans_layers util_layers simple_rnn_layers unused_layers test_cost_layers
 test_rnn_group shared_fc shared_lstm test_cost_layers_with_weight
-test_spp_layer test_bilinear_interp test_maxout test_bi_grumemory math_ops
-test_split_datasource)
+test_spp_layer test_bilinear_interp test_maxout test_bi_grumemory math_ops)
 
+whole_configs=(test_split_datasource)
 
 for conf in ${configs[*]}
 do
     echo "Generating " $conf
     python -m paddle.utils.dump_config $conf.py > $protostr/$conf.protostr.unitest
+done
+
+for conf in ${whole_configs[*]}
+do
+    echo "Generating " $conf
+    python -m paddle.utils.dump_config $conf.py "" --whole > $protostr/$conf.protostr.unitest
 done
