b821f0820ff03794348aa82db6e61ca0f5177a8f
Shi-Liang <385936547@qq.com>
2016-12-05 11:33:40 +0800

    1. add unit test library 2. add github fork link 3. remove useless comments

--- doc_theme/static/js/paddle_doc_init.js
+++ doc_theme/static/js/paddle_doc_init.js
@@ -3,28 +3,29 @@ $(document).ready(function(){
         $('.local-toc li.active').removeClass('active');
         $(this).parent('li').addClass('active');
     });
-    
-    
+
+    if ($('.local-toc a:visible').length) {
+        $('.local-toc > ul').addClass('nav nav-stacked');
+        $('#doc-content').scrollspy({
+            target: '.local-toc'
+        });
+		$('.local-toc').perfectScrollbar();
+    } else {
+		$('.doc-content-wrap').css('margin-left', '-=50px');
+        $('.local-toc').remove();
+    }
+
     if (!$('.doc-menu-vertical > ul > li.current > ul').length) {
         $('.doc-content-wrap').css('margin-left', '-=240px');
         $('.doc-menu-vertical').remove();
         $('.local-toc').css('left', '0');
     }
-    $('.doc-menu-vertical .toctree-l2').each(function (i, e){
+
+	$('.doc-menu-vertical .toctree-l2').each(function (i, e){
         $(e).toggleClass('has-child', !!$(e).find('ul').length);
     });
-    if ($('.local-toc a:visible').length) {
-        $('.doc-content-wrap').css('margin-left', '+=50px');
-        $('.local-toc > ul').addClass('nav nav-stacked');
-        $('#doc-content').scrollspy({
-            target: '.local-toc'
-        });
-    } else {
-        $('.local-toc').remove();
-    }
 
     $('.doc-menu-vertical').find('li.current').last().addClass('active');
 
     $('.doc-menu-vertical').perfectScrollbar();
-    $('.local-toc').perfectScrollbar();
 });
