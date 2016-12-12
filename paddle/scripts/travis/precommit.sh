6ea5a9fd0361d58bcea205bc9736166cf17e1b94
Yu Yang <yuyang18@baidu.com>
2016-12-12 15:43:06 +0800

    Add tips when unittest error

--- paddle/scripts/travis/precommit.sh
+++ paddle/scripts/travis/precommit.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+function abort(){
+    echo "Your commit not fit PaddlePaddle code style" 1>&2
+    echo "Please use pre-commit scripts to auto-format your code" 1>&2
+    exit 1
+}
+
+trap 'abort' 0
 set -e
 source common.sh
 cd ..
@@ -6,3 +13,5 @@ export PATH=/usr/bin:$PATH
 pre-commit install
 clang-format --version
 pre-commit run -a
+
+trap : 0
