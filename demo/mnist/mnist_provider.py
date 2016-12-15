828303bd5f6b580b0e1adef845bb2c76feb55c4f
wangyang59 <wangyang59@baidu.com>
2016-12-14 17:59:07 -0800

    after clang-format

--- demo/mnist/mnist_provider.py
+++ demo/mnist/mnist_provider.py
@@ -1,6 +1,7 @@
 from paddle.trainer.PyDataProvider2 import *
 import numpy
 
+
 # Define a py data provider
 @provider(
     input_types={'pixel': dense_vector(28 * 28),
@@ -20,13 +21,14 @@ def process(settings, filename):  # settings is not used currently.
         n = 60000
     else:
         n = 10000
-    
-    images = numpy.fromfile(f, 'ubyte', count=n*28*28).reshape((n, 28*28)).astype('float32')
-    images = images / 255.0 * 2.0 - 1.0    
+
+    images = numpy.fromfile(
+        f, 'ubyte', count=n * 28 * 28).reshape((n, 28 * 28)).astype('float32')
+    images = images / 255.0 * 2.0 - 1.0
     labels = numpy.fromfile(l, 'ubyte', count=n).astype("int")
-    
+
     for i in xrange(n):
         yield {"pixel": images[i, :], 'label': labels[i]}
-    
+
     f.close()
     l.close()
