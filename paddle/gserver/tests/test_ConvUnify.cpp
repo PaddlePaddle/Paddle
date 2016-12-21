67fcd898c5f17bf7a61237351e9087257a8a34f2
Peng Li <lipeng17@baidu.com>
2016-12-21 10:14:10 +0800

    fix array style problem

--- paddle/gserver/tests/test_ConvUnify.cpp
+++ paddle/gserver/tests/test_ConvUnify.cpp
@@ -137,25 +137,8 @@ TEST(Layer, convParaUnified) {
   checkMatrixEqual(resultCpu, resultGpu);
 
   input = Matrix::create(1, 3 * 3 * 2, false, false);
-  real inputData2[] = {1,
-                       2,
-                       3,
-                       4,
-                       5,
-                       6,
-                       7,
-                       8,
-                       9,
-
-                       10,
-                       11,
-                       12,
-                       13,
-                       14,
-                       15,
-                       16,
-                       17,
-                       18};
+  real inputData2[] = {
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
   real param2[] = {1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 6, 5, 4, 3, 2, 1};
 
   input->setData(inputData2);
