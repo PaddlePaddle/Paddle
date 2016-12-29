7e769ac063f766c9223e1293c3b28f56b669119c
dayhaha <18800111918@163.com>
2016-12-29 09:12:31 +0000

    add python version evaluate function

---
+++ demo/recommendation/evaluate.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import re
+import math
+
+
+def get_best_pass(filename):
+    with open(filename, 'r') as f:
+        text = f.read()
+        pattern = re.compile('Test.*? cost=([0-9]+\.[0-9]+).*?pass-([0-9]+)',
+                             re.S)
+        results = re.findall(pattern, text)
+        sorted_results = sorted(results, key=lambda result: float(result[0]))
+        return sorted_results[0]
+
+
+filename = sys.argv[1]
+LOG = get_best_pass(filename)
+predict_error = math.sqrt(float(LOG[0])) / 2
+print 'Best pass is %s, error is %s, which means predict get error as %f' % (
+    LOG[1], LOG[0], predict_error)
+
+evaluate_pass = "output/pass-%s" % LOG[1]
+print "evaluating from pass %s" % evaluate_pass
