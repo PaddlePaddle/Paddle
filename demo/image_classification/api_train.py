28e2bef9a2eef68f497b5a5aef81a062269d82fc
qiaolongfei <qiaolongfei@baidu.com>
2017-01-11 19:18:02 +0800

    add some comment

--- demo/image_classification/api_train.py
+++ demo/image_classification/api_train.py
@@ -1,10 +1,23 @@
+# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
-A very basic example for how to use current Raw SWIG API to train mnist network.
+An example to show how to use current Raw SWIG API to train cifar-10 network.
 
 Current implementation uses Raw SWIG, which means the API call is directly \
 passed to C++ side of Paddle.
 
-The user api could be simpler and carefully designed.
+The user api should be simpler and carefully designed.
 """
 import numpy as np
 import py_paddle.swig_paddle as api
--- demo/image_classification/cifar_util.py
+++ demo/image_classification/cifar_util.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Some class for prepare cifar-10 image data.
+"""
 import io
 import random
 
@@ -8,7 +24,7 @@ from py_paddle import DataProviderConverter
 
 
 class BatchPool(object):
-    """Proxy of C++ ParameterUpdater class."""
+    """A class to get all data in memory and do data shuffle."""
 
     def __init__(self, generator, batch_size):
         self.data = list(generator)
