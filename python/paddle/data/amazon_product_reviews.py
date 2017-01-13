7972f74f141e7b8f37495f2c351294c8b2b32a33
Yu Yang <yuyang18@baidu.com>
2017-01-13 16:53:34 +0800

    Add md5 checks.

--- python/paddle/data/amazon_product_reviews.py
+++ python/paddle/data/amazon_product_reviews.py
@@ -62,9 +62,36 @@ class Categories(object):
     __md5__[Automotive] = '757fdb1ab2c5e2fc0934047721082011'
     __md5__[Baby] = '7698a4179a1d8385e946ed9083490d22'
     __md5__[Beauty] = '5d2ccdcd86641efcfbae344317c10829'
+    __md5__[Books] = 'bc1e2aa650fe51f978e9d3a7a4834bc6'
+    __md5__[CDsAndVinyl] = '82bffdc956e76c32fa655b98eca9576b'
+    __md5__[CellPhonesAndAccessories] = '903a19524d874970a2f0ae32a175a48f'
+    __md5__[ClothingShoesAndJewelry] = 'b333fba48651ea2309288aeb51f8c6e4'
+    __md5__[DigitalMusic] = '35e62f7a7475b53714f9b177d9dae3e7'
+    __md5__[Electronics] = 'e4524af6c644cd044b1969bac7b62b2a'
+    __md5__[GroceryAndGourmetFood] = 'd8720f98ea82c71fa5c1223f39b6e3d9'
+    __md5__[HealthAndPersonalCare] = '352ea1f780a8629783220c7c9a9f7575'
+    __md5__[HomeAndKitchen] = '90221797ccc4982f57e6a5652bea10fc'
+    __md5__[KindleStore] = 'b608740c754287090925a1a186505353'
+    __md5__[MoviesAndTV] = 'd3bb01cfcda2602c07bcdbf1c4222997'
+    __md5__[MusicalInstruments] = '8035b6e3f9194844785b3f4cee296577'
+    __md5__[OfficeProducts] = '1b7e64c707ecbdcdeca1efa09b716499'
+    __md5__[PatioLawnAndGarden] = '4d2669abc5319d0f073ec3c3a85f18af'
+    __md5__[PetSupplies] = '40568b32ca1536a4292e8410c5b9de12'
+    __md5__[SportsAndOutdoors] = '1df6269552761c82aaec9667bf9a0b1d'
+    __md5__[ToolsAndHomeImprovement] = '80bca79b84621d4848a88dcf37a1c34b'
+    __md5__[ToysAndGames] = 'dbd07c142c47473c6ee22b535caee81f'
+    __md5__[VideoGames] = '730612da2d6a93ed19f39a808b63993e'
 
 
 __all__ = ['fetch', 'Categories']
+
+
+def calculate_md5(fn):
+    h = hashlib.md5()
+    with open(fn, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            h.update(chunk)
+    return h.hexdigest()
 
 
 def fetch(category=None, directory=None):
@@ -87,33 +114,13 @@ def fetch(category=None, directory=None):
 
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+    fn = os.path.join(directory, '%s.json.gz' % category)
+
+    if os.path.exists(fn) and \
+                    calculate_md5(category) == Categories.__md5__[category]:
+        # already download.
+        return fn
+
     logger.info("Downloading amazon review dataset for %s category" % category)
-    return download(BASE_URL % category,
-                    os.path.join(directory, '%s.json.gz' % category))
-
-
-def calculate_md5(fn):
-    h = hashlib.md5()
-    with open(fn, 'rb') as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            h.update(chunk)
-    return h.hexdigest()
-
-
-def main():
-    categories = filter(
-        lambda c: getattr(Categories, c) not in Categories.__md5__.keys(),
-        filter(lambda c: c[0] != '_', dir(Categories)))
-
-    for each in categories:
-        try:
-            filename = fetch(category=getattr(Categories, each))
-        except Exception as e:
-            print type(e)
-            continue
-        print each, calculate_md5(filename)
-        os.remove(filename)
-
-
-if __name__ == '__main__':
-    main()
+    return download(BASE_URL % category, fn)
