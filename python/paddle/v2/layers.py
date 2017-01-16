26e7ca9104f26941291337b44966471bef085c12
Yu Yang <yuyang18@baidu.com>
2017-01-16 11:30:46 +0800

    Refine layers in paddle.v2

--- python/paddle/v2/layers.py
+++ python/paddle/v2/layers.py
@@ -12,149 +12,95 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import uuid
-
-import paddle.trainer_config_helpers.config_parser_utils as config_parser_utils
-import paddle.trainer_config_helpers.layers as layers
-import paddle.trainer_config_helpers.networks as networks
+import paddle.trainer_config_helpers as conf_helps
+from paddle.trainer_config_helpers.config_parser_utils import \
+    parse_network_config as __parse__
+from paddle.trainer_config_helpers.default_decorators import wrap_name_default
 
 
 class Layer(object):
-    def __init__(self, layer=None, is_output=False, **kwargs):
-        self.uuid = uuid.uuid4()
-        self.parent_layer = layer
-        self.layer_output = None
-        self.network_config = None
-        self.is_output = is_output
+    def __init__(self, name, parent_layer):
+        assert isinstance(parent_layer, dict)
+        assert isinstance(name, basestring)
+        self.name = name
+        self.__parent_layer__ = parent_layer
 
-    def _execute(self):
-        """
-        recursively set parent's into proto
-        :return:
-        """
-        if self.parent_layer is not None:
-            self.parent_layer.execute()
-
-    def _mark_output_layers(self):
-        """
-        find out all layers that is marked output and set them into proto
-        :return:
-        """
-        print self.layers()
-        output_layers = filter(lambda layer: layer.is_output, self.layers())
-        print output_layers
-        if len(output_layers) > 0:
-            networks.outputs(
-                map(lambda layer: layer.layer_output, output_layers))
-
-    def execute(self):
+    def to_proto(self, context):
         """
         function to set proto attribute
-        :return:
         """
-        pass
+        kwargs = dict()
+        for param_name in self.__parent_layer__:
+            param_value = self.__parent_layer__[param_name].to_proto(
+                context=context)
+            kwargs[param_name] = param_value
 
-    def network(self):
-        """
-        Construct the network according to this layer and all it's parent layers
-        :return: return a proto that represent this network.
-        """
+        if self.name not in context:
+            context[self.name] = self.to_proto_impl(**kwargs)
+        return context[self.name]
 
-        def construct_network():
-            self.execute()
-            self._mark_output_layers()
-
-        if self.network_config is None:
-            self.network_config = config_parser_utils.parse_network_config(
-                construct_network)
-        return self.network_config
-
-    def layers(self):
-        """
-        get all layers that have relation to this layer.
-        :return:
-        """
-        all_layers = []
-        if self.parent_layer is not None:
-            all_layers.extend(self.parent_layer.layers())
-        all_layers.append(self)
-        return all_layers
+    def to_proto_impl(self, **kwargs):
+        raise NotImplementedError()
 
 
-class DataLayer(Layer):
-    def __init__(self,
-                 name,
-                 size,
-                 height=None,
-                 width=None,
-                 layer_attr=None,
-                 **kwargs):
-        self.name = name
-        self.size = size
-        self.height = height
-        self.width = width
-        self.layer_attr = layer_attr
-        super(DataLayer, self).__init__(**kwargs)
+def parse_network(*outputs):
+    def __real_func__():
+        context = dict()
+        real_output = [each.to_proto(context=context) for each in outputs]
+        conf_helps.outputs(real_output)
 
-    def execute(self):
-        self._execute()
-        self.layer_output = \
-            layers.data_layer(self.name, self.size, self.height, self.width, self.layer_attr)
+    return __parse__(__real_func__)
 
 
-class FcLayer(Layer):
-    def __init__(self,
-                 layer,
-                 size,
-                 act=None,
-                 name=None,
-                 param_attr=None,
-                 bias_attr=None,
-                 layer_attr=None,
-                 **kwargs):
-        self.parent_layer = layer
-        self.size = size
-        self.act = act
-        self.name = name
-        self.param_attr = param_attr
-        self.bias_attr = bias_attr
-        self.layer_attr = layer_attr
-        super(FcLayer, self).__init__(layer, **kwargs)
+def __convert__(method_name, name_prefix, parent_names):
+    if name_prefix is not None:
+        wrapper = wrap_name_default(name_prefix=name_prefix)
+    else:
+        wrapper = None
 
-    def execute(self):
-        self._execute()
-        self.layer_output = \
-            layers.fc_layer(input=self.parent_layer.layer_output, size=self.size, act=self.act,
-                            name=self.name, param_attr=self.param_attr, bias_attr=self.bias_attr,
-                            layer_attr=self.layer_attr)
+    class __Impl__(Layer):
+        def __init__(self, name=None, **kwargs):
+            parent_layers = dict()
+            other_kwargs = dict()
+            for pname in parent_names:
+                parent_layers[pname] = kwargs[pname]
+
+            for key in kwargs.keys():
+                if key not in parent_names:
+                    other_kwargs[key] = kwargs[key]
+
+            super(__Impl__, self).__init__(name, parent_layers)
+            self.__other_kwargs__ = other_kwargs
+
+        if wrapper is not None:
+            __init__ = wrapper(__init__)
+
+        def to_proto_impl(self, **kwargs):
+            args = dict()
+            for each in kwargs:
+                args[each] = kwargs[each]
+            for each in self.__other_kwargs__:
+                args[each] = self.__other_kwargs__[each]
+            return getattr(conf_helps, method_name)(name=self.name, **args)
+
+    return __Impl__
 
 
-class ClassificationCost(Layer):
-    def __init__(self,
-                 layer,
-                 label,
-                 weight=None,
-                 name=None,
-                 evaluator=layers.classification_error_evaluator,
-                 layer_attr=None,
-                 is_output=False,
-                 **kwargs):
-        assert isinstance(label, Layer)
-        self.parent_layer = layer
-        self.label = label
-        self.weight = weight
-        self.name = name
-        self.evaluator = evaluator
-        self.layer_attr = layer_attr
-        super(ClassificationCost, self).__init__(layer, is_output, **kwargs)
+data_layer = __convert__('data_layer', None, [])
+fc_layer = __convert__('fc_layer', name_prefix='fc', parent_names=['input'])
+classification_cost = __convert__(
+    'classification_cost',
+    name_prefix='classification_cost',
+    parent_names=['input', 'label'])
 
-    def execute(self):
-        self._execute()
-        self.label.execute()
-        self.layer_output = \
-            layers.classification_cost(input=self.parent_layer.layer_output,
-                                       label=self.label.layer_output,
-                                       weight=self.weight,
-                                       name=self.name,
-                                       evaluator=self.evaluator,
-                                       layer_attr=self.layer_attr)
+__all__ = ['data_layer', 'fc_layer', 'classification_cost', 'parse_network']
+
+if __name__ == '__main__':
+    data = data_layer(name='pixel', size=784)
+    hidden = fc_layer(input=data, size=100, act=conf_helps.SigmoidActivation())
+    predict = fc_layer(
+        input=hidden, size=10, act=conf_helps.SoftmaxActivation())
+    cost = classification_cost(
+        input=predict, label=data_layer(
+            name='label', size=10))
+    print parse_network(cost)
