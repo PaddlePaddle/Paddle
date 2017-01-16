302bc1977ef1a4d27e3e734aa61842af5c87ff6e
liaogang <liaogang@baidu.com>
2017-01-16 17:31:13 +0800

    Fix fetch Paddle's version from git

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -3,11 +3,16 @@ set(PADDLE_VERSION $ENV{PADDLE_VERSION})
 set(tmp_version "HEAD")
 while ("${PADDLE_VERSION}" STREQUAL "")
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --tags --abbrev=0 ${tmp_version}
-    WORKING_DIRECTORY ${PROJ_ROOT}
-    OUTPUT_VARIABLE GIT_TAG_NAME
-    RESULT_VARIABLE GIT_RESULT
-    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+      COMMAND ${GIT_EXECUTABLE} rev-list --tags --max-count=1
+      OUTPUT_VARIABLE COMMIT_HASH
+      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  execute_process(
+      COMMAND ${GIT_EXECUTABLE} describe --tags ${COMMIT_HASH}
+      OUTPUT_VARIABLE GIT_TAG_NAME
+      RESULT_VARIABLE GIT_RESULT
+      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
   if (NOT ${GIT_RESULT})
     # Check the tag is a correct version
     if (${GIT_TAG_NAME} MATCHES "v[0-9]+\\.[0-9]+\\.[0-9]+(\\.(a|b|rc)\\.[0-9]+)?")
