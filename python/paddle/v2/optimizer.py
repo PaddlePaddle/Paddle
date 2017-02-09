b1270cfb1d3f4b25ae276719245dc3618d64bb53
Yu Yang <yuyang18@baidu.com>
2017-02-09 22:08:18 +0800

    Init v2 concepts.

--- python/paddle/v2/optimizer.py
+++ python/paddle/v2/optimizer.py
@@ -1,58 +1,37 @@
-import py_paddle.swig_paddle as swig_api
-import paddle.trainer_config_helpers.optimizers as v1_optimizers
-import paddle.trainer_config_helpers.config_parser_utils as config_parser_utils
-import paddle.v2
+___all__ = ['Optimizer']
 
-__all__ = ['Adam', 'Adamax']
+
+class BaseObserveEvent(object):
+    def __init__(self, pass_id, batch_id):
+        self.__pass_id__ = pass_id
+        self.__batch_id__ = batch_id
+
+
+class TrainOneBatchEvent(BaseObserveEvent):
+    def __init__(self, pass_id, batch_id, loss):
+        super(TrainOneBatchEvent, self).__init__(pass_id, batch_id)
+        self.__loss__ = loss
+
+    def __str__(self):
+        return "Pass %d, Batch %d, Loss %f" % (self.__pass_id__,
+                                               self.__batch_id__, self.__loss__)
+
+
+def default_observe_callback(gradient_machine, event):
+    if isinstance(event, TrainOneBatchEvent):
+        print event
+    else:
+        raise NotImplementedError()
 
 
 class Optimizer(object):
-    def __init__(self, **kwargs):
-        if 'batch_size' in kwargs:
-            del kwargs['batch_size']  # not important for python library.
+    def __init__(self, gradient_machine):
+        raise NotImplementedError()
 
-        def __impl__():
-            v1_optimizers.settings(batch_size=1, **kwargs)
-
-        self.__opt_conf_proto__ = config_parser_utils.parse_optimizer_config(
-            __impl__)
-        self.__opt_conf__ = swig_api.OptimizationConfig.createFromProto(
-            self.__opt_conf_proto__)
-
-    def enable_types(self):
-        """
-        get enable_types for each optimizer.
-        enable_types = [value, gradient, momentum, etc]
-        For each optimizer(SGD, Adam), GradientMachine should enable different
-        buffers.
-        """
-        tmp = swig_api.ParameterOptimizer.create(self.__opt_conf__)
-        assert isinstance(tmp, swig_api.ParameterOptimizer)
-        return tmp.getParameterTypes()
-
-    def create_local_updater(self):
-        return swig_api.ParameterUpdater.createLocalUpdater(self.__opt_conf__)
-
-    def create_remote_updater(self, pass_num):
-        return swig_api.ParameterUpdater.createRemoteUpdater(self.__opt_conf__,
-                                                             pass_num)
-
-
-class Adam(Optimizer):
-    def __init__(self, beta1=0.9, beta2=0.999, epsilon=1e-8, **kwargs):
-        learning_method = v1_optimizers.AdamOptimizer(
-            beta1=beta1, beta2=beta2, epsilon=epsilon)
-        super(Adam, self).__init__(learning_method=learning_method, **kwargs)
-
-
-class Adamax(Optimizer):
-    def __init__(self, beta1=0.9, beta2=0.999, **kwargs):
-        learning_method = v1_optimizers.AdamaxOptimizer(
-            beta1=beta1, beta2=beta2)
-        super(Adamax, self).__init__(learning_method=learning_method, **kwargs)
-
-
-if __name__ == '__main__':
-    swig_api.initPaddle('--use_gpu=false')
-    opt = paddle.v2.optimizer.Adam()
-    print opt.enable_types()
+    def train(self, train_reader=None, test_reader=None, observe_callback=None):
+        assert hasattr(train_reader, 'next')
+        assert test_reader is None or hasattr(test_reader, 'next')
+        if observe_callback is None:
+            observe_callback = default_observe_callback
+        assert callable(observe_callback)
+        raise NotImplementedError()
