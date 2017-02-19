5fd49f66657766cac11fd14a270b0b947a92c49d
Yu Yang <yuyang18@baidu.com>
2017-02-19 18:12:49 +0800

    Remove sequence information in GradientPrinter.
    * it is not necessary, because user could print sequence info
      by ValuePrinter.

--- paddle/gserver/evaluators/Evaluator.cpp
+++ paddle/gserver/evaluators/Evaluator.cpp
@@ -929,8 +929,6 @@ REGISTER_EVALUATOR(value_printer, ValuePrinter);
  */
 class GradientPrinter : public Evaluator {
 public:
-  GradientPrinter() {}
-
   virtual void eval(const NeuralNetwork& nn) {
     for (const std::string& name : config_.input_layers()) {
       const Argument& argu = nn.getLayer(name)->getOutput();
@@ -938,11 +936,6 @@ public:
         std::ostringstream os;
         argu.grad->print(os);
         LOG(INFO) << "layer=" << name << " grad matrix:\n" << os.str();
-      }
-      if (auto startPos = argu.sequenceStartPositions) {
-        std::ostringstream os;
-        startPos->getVector(false)->print(os, startPos->getSize());
-        LOG(INFO) << "layer=" << name << " sequence pos vector:\n" << os.str();
       }
     }
   }
