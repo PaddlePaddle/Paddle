b5b9a9cfb797db957d51b2dc5fc6a7d45d90e46b
Yu Yang <yuyang18@baidu.com>
2017-02-20 14:17:18 +0800

    Add comments

--- paddle/parameter/Argument.h
+++ paddle/parameter/Argument.h
@@ -306,6 +306,12 @@ struct Argument {
    */
   void getValueString(std::unordered_map<std::string, std::string>* out) const;
 
+  /**
+   * @brief printValueString will print the argument's output in order of
+   * 'value', 'id', 'sequence pos', 'sub-sequence pos'.
+   * @param stream: Output stream
+   * @param prefix: line prefix for printing.
+   */
   void printValueString(std::ostream& stream,
                         const std::string& prefix = "") const;
 };
