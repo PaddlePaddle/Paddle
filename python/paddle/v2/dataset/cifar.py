de9012a50450da19a9227bc28d5bb042ac66fcf7
Yu Yang <yuyang18@baidu.com>
2017-02-27 20:32:44 +0800

    Add MovieLens Dataset

--- python/paddle/v2/dataset/cifar.py
+++ python/paddle/v2/dataset/cifar.py
@@ -5,15 +5,13 @@ URL: https://www.cs.toronto.edu/~kriz/cifar.html
 
 the default train_creator, test_creator used for CIFAR-10 dataset.
 """
-from config import DATA_HOME
-import os
-import hashlib
-import urllib2
-import shutil
-import tarfile
 import cPickle
 import itertools
+import tarfile
+
 import numpy
+
+from config import download
 
 __all__ = [
     'cifar_100_train_creator', 'cifar_100_test_creator', 'train_creator',
@@ -45,31 +43,6 @@ def __read_batch__(filename, sub_name):
                     yield item
 
     return reader
-
-
-def download(url, md5):
-    filename = os.path.split(url)[-1]
-    assert DATA_HOME is not None
-    filepath = os.path.join(DATA_HOME, md5)
-    if not os.path.exists(filepath):
-        os.makedirs(filepath)
-    __full_file__ = os.path.join(filepath, filename)
-
-    def __file_ok__():
-        if not os.path.exists(__full_file__):
-            return False
-        md5_hash = hashlib.md5()
-        with open(__full_file__, 'rb') as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                md5_hash.update(chunk)
-
-        return md5_hash.hexdigest() == md5
-
-    while not __file_ok__():
-        response = urllib2.urlopen(url)
-        with open(__full_file__, mode='wb') as of:
-            shutil.copyfileobj(fsrc=response, fdst=of)
-    return __full_file__
 
 
 def cifar_100_train_creator():
