89d300191eed4f2ec75d673ebdce80a671e3cac1
Yi Wang <yi.wang.2005@gmail.com>
2017-02-28 19:49:46 +0000

    Add early draft of imdb.py and unit test

---
+++ python/paddle/v2/dataset/tests/imdb_test.py
@@ -0,0 +1,6 @@
+import paddle.v2.dataset.common
+
+URL = 'http://ai.stanford.edu/%7Eamaas/data/sentiment/aclImdb_v1.tar.gz'
+MD5 = '7c2ac02c03563afcf9b574c7e56c153a'
+
+paddle.v2.dataset.common.download
