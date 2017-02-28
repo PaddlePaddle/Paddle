349319e5ed9c2ddb0af5a6fb66280c6b7b3bb552
Yi Wang <yi.wang.2005@gmail.com>
2017-02-28 20:02:32 +0000

    Add imdb_test.py

--- python/paddle/v2/dataset/tests/imdb_test.py
+++ python/paddle/v2/dataset/tests/imdb_test.py
@@ -1,6 +1,12 @@
 import paddle.v2.dataset.common
+import tarfile
 
 URL = 'http://ai.stanford.edu/%7Eamaas/data/sentiment/aclImdb_v1.tar.gz'
 MD5 = '7c2ac02c03563afcf9b574c7e56c153a'
 
-paddle.v2.dataset.common.download
+tarf = tarfile.open(paddle.v2.dataset.common.download(URL, 'imdb', MD5))
+
+tf = tarf.next()
+while tf != None:
+    print tf.name
+    tf = tarf.next()
