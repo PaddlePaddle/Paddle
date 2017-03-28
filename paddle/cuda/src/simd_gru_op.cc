db600ed7d18da5f972dfe030ca90a1087686310f
liaogang <liaogang@baidu.com>
2017-03-28 19:50:07 +0800

    remove unused head files

--- paddle/cuda/src/simd_gru_op.cc
+++ paddle/cuda/src/simd_gru_op.cc
@@ -9,7 +9,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "simd_activation.cuh"
 #include "simd_recurrent.cuh"
 
 #include <immintrin.h>
