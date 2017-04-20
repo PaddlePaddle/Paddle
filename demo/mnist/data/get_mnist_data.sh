3645535797264e2abec3d990b5f4f4307322302f
Yu Yang <yuyang18@baidu.com>
2017-04-20 15:20:50 +0800

    Strange usage for decompress gzip file.
    * Change `gunzip` to `gzip -d`, because `gunzip` cannot be found in
      many linux distro.

--- demo/mnist/data/get_mnist_data.sh
+++ demo/mnist/data/get_mnist_data.sh
@@ -12,7 +12,7 @@ for fname in train-images-idx3-ubyte train-labels-idx1-ubyte t10k-images-idx3-ub
 do
     if [ ! -e $fname ]; then
         wget --no-check-certificate http://yann.lecun.com/exdb/mnist/${fname}.gz
-        gunzip ${fname}.gz
+        gzip -d ${fname}.gz
     fi
 done
 
