90a95f9363e025ef23f8a11e68bddd167aa505e5
Yu Yang <yuyang18@baidu.com>
2017-05-11 11:33:47 +0800

    Stash

--- python/paddle/v2/config.py
+++ python/paddle/v2/config.py
@@ -1,5 +1,6 @@
 import paddle.trainer_config_helpers.layers as layers
-from paddle.trainer_config_helpers.config_parser_utils import parse_network_config
+from paddle.trainer_config_helpers.config_parser_utils import \
+    parse_network_config
 
 
 class Tape(object):
@@ -39,6 +40,14 @@ gTape = Tape()
 
 
 def __convert_v2__(name, module):
+    def __convert_tape_item__(tape, a):
+        if isinstance(a, TapeItem):
+            return tape[a.idx()]
+        elif isinstance(a, list) or isinstance(a, tuple):
+            return map(lambda x: __convert_tape_item__(tape=tape, a=x), a)
+        else:
+            return a
+
     def __impl__(*args, **kwargs):
         func = getattr(module, name)
         tape = kwargs.get('tape', None)
@@ -48,14 +57,10 @@ def __convert_v2__(name, module):
             # Convert arguments
             args = list(args)
             for i in xrange(len(args)):
-                a = args[i]
-                if isinstance(a, TapeItem):
-                    args[i] = tape[a.idx()]
+                args[i] = __convert_tape_item__(tape, args[i])
 
             for key in kwargs:
-                a = kwargs[key]
-                if isinstance(a, TapeItem):
-                    kwargs[key] = tape[a.idx()]
+                kwargs[key] = __convert_tape_item__(tape, kwargs[key])
 
             del kwargs['tape']
 
@@ -74,10 +79,18 @@ def __convert_v2__(name, module):
 for nm in layers.__all__:
     if nm.endswith("_layer"):
         globals()[nm] = __convert_v2__(nm, layers)
+    if nm == "recurrent_group":
+        globals()[nm] = __convert_v2__(nm, layers)
 
 if __name__ == '__main__':
     dat = data_layer(name="a", size=10)
     hidden = fc_layer(input=dat, size=100)
+
+    def step(input):
+        return fc_layer(input=input, size=200)
+
+    hidden = recurrent_group(step=step, input=[hidden])
+
     output = fc_layer(input=hidden, size=10)
 
     print parse_network_config(output.to_proto)
