f202377d23cd070899204b147ef166d17cb381b8
qijun <qijun1994@hotmail.com>
2017-05-18 12:57:32 +0800

    change unittest cmake file

--- paddle/majel/test/allocation_test.cc
+++ paddle/majel/test/allocation_test.cc
@@ -4,7 +4,7 @@
 TEST(Allocation, malloc) {
   majel::CpuPlace cpu;
   majel::Allocation block(100, cpu);
-  // EXPECT_EQ(static_cast<size_t>(100), block.size());
-  // EXPECT_TRUE(majel::places_are_same_class(cpu, block.place()));
-  // EXPECT_EQ(100, (uint8_t*)block.end() - (uint8_t*)block.ptr());
+  EXPECT_EQ(static_cast<size_t>(100), block.size());
+  EXPECT_TRUE(majel::places_are_same_class(cpu, block.place()));
+  EXPECT_EQ(100, (uint8_t*)block.end() - (uint8_t*)block.ptr());
 }
