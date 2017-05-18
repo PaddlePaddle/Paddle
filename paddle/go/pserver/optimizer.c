bd2469f21c663c192a7ab73b2b2fdfafffdf5edb
Helin Wang <ustc.harry@gmail.com>
2017-05-17 20:01:29 -0400

    correct optimizer release, add test

--- paddle/go/pserver/optimizer.c
+++ paddle/go/pserver/optimizer.c
@@ -3,34 +3,44 @@
 #include "optimizer.h"
 
 typedef int (*update_func)(void*, void *, paddle_element_type, const void*, int);
+typedef void (*release_func)(void*);
 
 typedef struct paddle_optimizer{
-  update_func func;
+  update_func update;
+  release_func release;
   void* optimizer;
 } paddle_optimizer;
 
 void paddle_release_optimizer(paddle_optimizer* o) {
+  o->release(o->optimizer);
   free(o);
 }
 
 int paddle_update_parameter(paddle_optimizer* o, void *buffer, paddle_element_type element_type, const void* gradient, int num_bytes) {
-  return o->func(o->optimizer, buffer, element_type, gradient, num_bytes);
+  return o->update(o->optimizer, buffer, element_type, gradient, num_bytes);
 }
 
 typedef struct {
   double learning_rate;
 } SGD_optimizer;
 
-int paddle_SGD_update_parameter(void* optimizer, void *buffer, paddle_element_type element_type, const void* gradient, int num_bytes) {
+int update_SGD(void* optimizer, void *buffer, paddle_element_type element_type, const void* gradient, int num_bytes) {
+  SGD_optimizer* o = (SGD_optimizer*)optimizer;
   // TODO
   return 0;
+}
+
+void release_SGD(void *optimizer) {
+    SGD_optimizer* o = (SGD_optimizer*)optimizer;
+    // nothing allocated on heap
 }
 
 paddle_optimizer* paddle_create_SGD_optimizer(double learning_rate) {
   SGD_optimizer* o = (SGD_optimizer*)malloc(sizeof(SGD_optimizer));
   o->learning_rate = learning_rate;
   paddle_optimizer* container = (paddle_optimizer*)malloc(sizeof(paddle_optimizer));
-  container->func = paddle_SGD_update_parameter;
+  container->update = update_SGD;
+  container->release = release_SGD;
   container->optimizer = o;
   return container;
 }
