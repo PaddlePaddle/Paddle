25c3f118f00b601fa64d3984a7eb44c572bf287e
Helin Wang <ustc.harry@gmail.com>
2017-05-21 20:32:55 -0400

    apply clang-format

--- paddle/go/pserver/optimizer.h
+++ paddle/go/pserver/optimizer.h
@@ -2,10 +2,10 @@
 #define PADDLE_PSERVER_OPTIMIZER_H
 
 typedef enum {
-  PADDLE_ELEMENT_TYPE_INT32   = 0,
-  PADDLE_ELEMENT_TYPE_UINT32  = 1,
-  PADDLE_ELEMENT_TYPE_INT64   = 2,
-  PADDLE_ELEMENT_TYPE_UINT64  = 3,
+  PADDLE_ELEMENT_TYPE_INT32 = 0,
+  PADDLE_ELEMENT_TYPE_UINT32 = 1,
+  PADDLE_ELEMENT_TYPE_INT64 = 2,
+  PADDLE_ELEMENT_TYPE_UINT64 = 3,
   PADDLE_ELEMENT_TYPE_FLOAT32 = 4,
   PADDLE_ELEMENT_TYPE_FLOAT64 = 5,
 } paddle_element_type;
@@ -13,6 +13,10 @@ typedef enum {
 struct paddle_optimizer;
 struct paddle_optimizer* paddle_create_SGD_optimizer(double learning_rate);
 void paddle_release_optimizer(struct paddle_optimizer* o);
-int paddle_update_parameter(struct paddle_optimizer* o, void *buffer, paddle_element_type element_type, const void* gradient, int num_bytes);
+int paddle_update_parameter(struct paddle_optimizer* o,
+                            void* buffer,
+                            paddle_element_type element_type,
+                            const void* gradient,
+                            int num_bytes);
 
 #endif /* PADDLE_PSERVER_OPTIMIZER_H */
