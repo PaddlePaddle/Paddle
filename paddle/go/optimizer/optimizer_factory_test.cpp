bcbd26404ed14a63994c6470ec596cab70d273c1
dzhwinter <dzhwinter@gmail.com>
2017-05-31 17:15:28 +0800

    "enable testing"

--- paddle/go/optimizer/optimizer_factory_test.cpp
+++ paddle/go/optimizer/optimizer_factory_test.cpp
@@ -1,13 +1,21 @@
-// #include "Tensor.h"
-// #include "gtest/gtest.h"
+#include "Tensor.h"
+#include "gtest/gtest.h"
 
-// TEST(Tensor, testDot) {
-//   int[] a = {1, 2, 3};
-//   int[] b = {1, 2, 3};
-//   int[] c = {2, 4, 6};
-//   size_t size = 3;
-//   paddle::Tensor<int> T1(&a, size);
-//   paddle::Tensor<int> T2(&b, size);
-//   auto T1 += T2;
-//   // ASSERT_EQ(T1, T2);
-// }
+TEST(Tensor, testDot) {
+  int[] a = {1, 2, 3};
+  int[] b = {1, 2, 3};
+  int[] c = {2, 4, 6};
+  size_t size = 3;
+  paddle::Tensor<int> T1(&a, size);
+  paddle::Tensor<int> T2(&b, size);
+  auto T1 += T2;
+  // ASSERT_EQ(T1, T2);
+}
+
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+
+}
+
--- paddle/go/optimizer/optimizer_test.cpp
+++ paddle/go/optimizer/optimizer_test.cpp
@@ -1,11 +1,11 @@
-// #include "optimizer.h"
-// #include "gtest/gtest.h"
+#include "optimizer.h"
+#include "gtest/gtest.h"
 
-// template <class T>
-// class OpitmizerTests {
-// private:
-//   Tensor<T> parameter;
-//   Tensor<T> gradient;
-// };
+template <class T>
+class OpitmizerTests {
+private:
+  Tensor<T> parameter;
+  Tensor<T> gradient;
+};
 
-// void applyGradientDescent_TEST() {}
+void applyGradientDescent_TEST() {}
