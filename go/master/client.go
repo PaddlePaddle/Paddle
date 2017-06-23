3c88fd21259a1378dd679664f1905b5a9a6437ee
Yancey1989 <yancey1989@gmail.com>
2017-06-23 15:17:55 +0800

    update

--- go/master/client.go
+++ go/master/client.go
@@ -26,11 +26,48 @@ type Client struct {
 }
 
 // MasterAddresser provide master address
-type MasterAddresser string
+type masterAddresser struct {
+	client    *clientv3.Client
+	timeout   time.Duration
+	endpoints []string
+}
 
 // Address return the address
-func (m MasterAddresser) Address() string {
-	return string(m)
+func (m masterAddresser) Address() string {
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), m.timeout)
+		resp, err := m.client.Get(ctx, masterAddrPath)
+		cancel()
+		if err != nil {
+			log.Errorf("Fetch master addr failed, reconnecting to etcd, %v", err)
+			err := m.client.Close()
+			if err != nil {
+				log.Errorln(err)
+				time.Sleep(m.timeout)
+				continue
+			}
+			// reconnect to etcd server
+			m.client, err = clientv3.New(clientv3.Config{
+				Endpoints:   m.endpoints,
+				DialTimeout: m.timeout,
+			})
+			if err != nil {
+				log.Errorf("Reconnecting etcd failed, sleep for %d seconds ...\n%v", m.timeout, err)
+				time.Sleep(m.timeout)
+				continue
+			}
+			continue
+		}
+		kvs := resp.Kvs
+		if len(kvs) == 0 {
+			log.Infoln("Waiting for master be ready ...")
+			time.Sleep(m.timeout)
+			continue
+		}
+		mAddr := kvs[0].Value
+		log.Debugf("Fetched master address: %s\n", mAddr)
+		return string(mAddr)
+	}
 }
 
 // NewClient creates a new Client.
@@ -48,42 +85,28 @@ func NewClient(addr Addresser, bufSize int) *Client {
 
 // NewEtcdClient create a new master client by etcd
 //
-// etcdEndpoints is the endpoints for etcd, it's separated by "," such as
+// endpoints is the endpoints for etcd and separated by ",", such as
 // "172.0.1.0:2379,172.0.1.1:2379"
+// timeout is the timeout for etcd calls
 // bufSize is the record buffer size. NextRecord will read from this buffer.
-func NewEtcdClient(etcdEndpoints string, etcdTimeout int, bufSize int) *Client {
-	timeout := time.Second * time.Duration(etcdTimeout)
-	ep := strings.Split(etcdEndpoints, ",")
+func NewEtcdClient(endpoints string, timeout int, bufSize int) *Client {
+	t := time.Second * time.Duration(timeout)
+	ep := strings.Split(endpoints, ",")
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   ep,
-		DialTimeout: timeout,
+		DialTimeout: t,
 	})
 	if err != nil {
 		log.Errorf("Init etcd connection failed: %v", err)
 		panic(err)
 	}
-	log.Debugf("Connected to etcd: %s\n", etcdEndpoints)
-	for {
-
-		ctx, cancel := context.WithTimeout(context.Background(), timeout)
-		resp, err := cli.Get(ctx, masterAddrPath)
-		cancel()
-		if err != nil {
-			log.Errorf("Fetch master addr failed, %v\n", err)
-			time.Sleep(timeout)
-			continue
-		}
-		kvs := resp.Kvs
-		if len(kvs) == 0 {
-			log.Infoln("Waiting for master be ready ...\n")
-			time.Sleep(timeout)
-			continue
-		}
-
-		mAddr := kvs[0].Value
-		log.Debugf("Fetched master address: %s\n", mAddr)
-		return NewClient(MasterAddresser(mAddr), bufSize)
+	log.Debugf("Connected to etcd: %s\n", endpoints)
+	mAddresser := masterAddresser{
+		client:    cli,
+		timeout:   t,
+		endpoints: ep,
 	}
+	return NewClient(mAddresser, bufSize)
 }
 
 func (c *Client) getRecords() {
