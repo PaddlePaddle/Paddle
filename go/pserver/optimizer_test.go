6bb84b963d421394c04c43c202ccea874bdb53f6
dongzhihong <dzhwinter@gmail.com>
2017-06-28 17:20:55 +0800

    "remove unused debug info"

--- go/pserver/optimizer_test.go
+++ go/pserver/optimizer_test.go
@@ -2,6 +2,7 @@ package pserver
 
 import (
 	"io/ioutil"
+	"reflect"
 	"testing"
 )
 
@@ -20,5 +21,28 @@ func TestOptimizerCreateRelease(t *testing.T) {
 		Config: config,
 	}
 	o := newOptimizer(param)
+	o.Cleanup()
+}
+
+func TestOptimizerFull(t *testing.T) {
+	p := Parameter{
+		Name:        "a",
+		ElementType: Float32,
+	}
+	p.Content = []byte{1, 3}
+	config, err := ioutil.ReadFile("./cclient/test/testdata/optimizer.pb.txt")
+	if err != nil {
+		t.Fatalf("read optimizer proto failed")
+	}
+	param := ParameterWithConfig{
+		Param:  p,
+		Config: config,
+	}
+	o := newOptimizer(param)
+	g := Gradient(p)
+	if !reflect.DeepEqual(p.Content, o.GetWeights()) {
+		t.FailNow()
+	}
+	o.UpdateParameter(g)
 	o.Cleanup()
 }
