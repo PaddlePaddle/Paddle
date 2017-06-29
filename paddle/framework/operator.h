7fa796c70f8ae31edbc827500670f3575e516867
qiaolongfei <qiaolongfei@baidu.com>
2017-06-29 17:38:45 +0800

    add design doc to operator impl

--- paddle/framework/operator.h
+++ paddle/framework/operator.h
@@ -1,3 +1,17 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserve.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 #pragma once
 
 #include <vector>
@@ -12,8 +26,8 @@ namespace framework {
  * following is just for demo
  */
 class Context {};
-class CpuContext: public Context {};
-class GpuContext: public Context {};
+class CpuContext : public Context {};
+class GpuContext : public Context {};
 
 class Scope {
  public:
@@ -24,8 +38,7 @@ class OpDesc {};
 
 class AttrbuteMap {
  public:
-
-  template<typename T>
+  template <typename T>
   Error get(const std::string key, T* attr) const;
 };
 
@@ -50,11 +63,12 @@ class OperatorBase {
 template <typename DeviceContext>
 class Operator : public OperatorBase {
  public:
-  explicit Operator(const OpDesc& desc): OperatorBase(desc) {}
+  explicit Operator(const OpDesc& desc) : OperatorBase(desc) {}
 
  private:
   /// This function will get all input and output Vars from scope and ten call
-  /// Run(std::vector<Variable> inputs, std::vector<Variable> outputs, T* context)
+  /// Run(std::vector<Variable> inputs, std::vector<Variable> outputs, T*
+  /// context)
   Error Run(Scope* scope, Context* context) const final {
     DeviceContext* dev_context = dynamic_cast<DeviceContext*>(context);
     if (dev_context == nullptr) {
@@ -65,11 +79,11 @@ class Operator : public OperatorBase {
     std::vector<Variable*> output_vars;
 
     input_vars.reserve(inputs_.size());
-    for(auto& input: inputs_) {
+    for (auto& input : inputs_) {
       input_vars.push_back(scope->getOrCreateVariable(input));
     }
     output_vars.reserve(outputs_.size());
-    for(auto& input: outputs_) {
+    for (auto& input : outputs_) {
       output_vars.push_back(scope->getOrCreateVariable(input));
     }
 
--- paddle/operators/cosine_operator.cc
+++ paddle/operators/cosine_operator.cc
@@ -8,29 +8,27 @@ namespace operators {
 // Sample Operator implement. Show how to implement a Cosine Operator.
 template <typename DeviceContext>
 class CosineOp final : public Operator<DeviceContext> {
-public:
-    explicit CosineOp(const OpDesc& desc):
-            Operator<DeviceContext>(desc) {}
+ public:
+  explicit CosineOp(const OpDesc& desc) : Operator<DeviceContext>(desc) {}
 
-    /// init attrs that is needed by this Operator, check the legality here.
-    Error InitializeAttributes(const AttrbuteMap& attrs) {
-      attrs.get<float>("scale", &scale_);
-      if (scale_ <= 0.0) {
-        return Error("scale of CosineOp must be larger than 0.0, get %f", scale_);
-      }
-      return Error();
+  /// init attrs that is needed by this Operator, check the legality here.
+  Error InitializeAttributes(const AttrbuteMap& attrs) {
+    attrs.get<float>("scale", &scale_);
+    if (scale_ <= 0.0) {
+      return Error("scale of CosineOp must be larger than 0.0, get %f", scale_);
     }
+    return Error();
+  }
 
-    /// Add the actual calculate logic in this function.
-    Error Run(std::vector<Variable*>& inputs,
-              std::vector<Variable*>& outputs,
-              DeviceContext* context) const override {
-      // TODO(to be implement)
-      return Error();
-    }
+  /// Add the actual calculate logic in this function.
+  Error Run(std::vector<Variable*>& inputs, std::vector<Variable*>& outputs,
+            DeviceContext* context) const override {
+    // TODO(to be implement)
+    return Error();
+  }
 
-private:
-    float scale_;
+ private:
+  float scale_;
 };
 
 }  // namespace operators
