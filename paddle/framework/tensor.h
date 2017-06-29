67fe7093c9668a9662a4e19780d612722cfa0f1b
dangqingqing <dangqingqing@baidu.com>
2017-06-29 16:54:13 +0800

    Replace product(src.Dims()) by Numel() in tensor.

--- paddle/framework/tensor.h
+++ paddle/framework/tensor.h
@@ -25,21 +25,21 @@ class Tensor {
   const T* data() const {
     PADDLE_ENFORCE(holder_ != nullptr);
     PADDLE_ENFORCE(holder_->Place() == place_);
-    PADDLE_ENFORCE(holder_->Size() >= product(dims_) * sizeof(T));
+    PADDLE_ENFORCE(holder_->Size() >= Numel() * sizeof(T));
     return static_cast<const T*>(holder->Ptr());
   }
 
   template <typename T>
   bool NeedReset() const {
     return (holder_ == nullptr || holder_->Place() != place_ ||
-            holder_->Size() < product(dims_) * sizeof(T));
+            holder_->Size() < Numel() * sizeof(T));
   }
 
   // must be POD types
   template <typename T, typename = std::enable_if<std::is_pod<T>::value>::type>
   T* mutable_data() {
     if (NeedReset<T>()) {
-      holder_.reset(new PlaceholderImpl(place_, product(dims_) * sizeof(T)));
+      holder_.reset(new PlaceholderImpl(place_, Numel() * sizeof(T)));
     }
     return static_cast<T*>(holder_->Ptr());
   }
@@ -64,7 +64,7 @@ class Tensor {
   void Resize(const DDim& dims) { dims_ = dims; }
 
   void Reshape(const DDim& dims) {
-    PADDLE_ENFORCE(product(dims) == product(dims_),
+    PADDLE_ENFORCE(product(dims) == Numel(),
                    "Reshape() can not change tensor's numel!");
     dims_ = dims;
   }
