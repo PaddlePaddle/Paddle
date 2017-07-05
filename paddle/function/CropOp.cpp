acfd2fc6dfc1bf06bbfd6e25496ca1dfde881551
wanghaoshuang <wanghaoshuang@baidu.com>
2017-07-05 11:54:47 +0800

    fix cpp format

--- paddle/function/CropOp.cpp
+++ paddle/function/CropOp.cpp
@@ -13,8 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "CropOp.h"
-#include "paddle/math/Vector.h"
 #include "paddle/function/TensorShape.h"
+#include "paddle/math/Vector.h"
+
 namespace paddle {
 
 template <>
