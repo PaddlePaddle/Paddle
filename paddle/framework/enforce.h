5979a40e9a66d5352d8b7c3bff166ea10ce20cfc
Superjom <superjom@gmail.com>
2017-07-13 17:30:21 +0800

    fix compile error with DEBUG

--- paddle/framework/enforce.h
+++ paddle/framework/enforce.h
@@ -10,6 +10,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
+#include <glog/logging.h>
 #include <paddle/string/printf.h>
 #include <exception>
 #include <sstream>
