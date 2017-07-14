e588730147069e967e4c9108d24d663a5a5b55f6
qiaolongfei <qiaolongfei@baidu.com>
2017-07-14 09:56:27 +0800

    change op to operators

--- paddle/operators/add_op.cu
+++ paddle/operators/add_op.cu
@@ -1,5 +1,5 @@
-#include <paddle/op/add_op.h>
+#include <paddle/operators/add_op.h>
 #include <paddle/framework/op_registry.h>
 
 REGISTER_OP_GPU_KERNEL(add_two,
-                       paddle::op::AddKernel<paddle::platform::GPUPlace>);
+                       paddle::operators::AddKernel<paddle::platform::GPUPlace>);
