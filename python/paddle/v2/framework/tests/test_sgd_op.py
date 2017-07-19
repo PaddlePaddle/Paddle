4ab456049ebb6c0380dd76cd69e682c2f3ce16ce
qiaolongfei <qiaolongfei@baidu.com>
2017-07-19 15:06:52 +0800

    optimize test

--- python/paddle/v2/framework/tests/test_sgd_op.py
+++ python/paddle/v2/framework/tests/test_sgd_op.py
@@ -1,49 +1,17 @@
 import unittest
-import paddle.v2.framework.create_op_creation_methods as creation
-import paddle.v2.framework.core as core
-from op_test_util import OpTestMeta
 import numpy
+from op_test_util import OpTestMeta
 
 
-class TestSgdOp(unittest.TestCase):
-    def test_run(self):
-        scope = core.Scope(None)
-        # 1, set param
-        param = scope.create_var("param")
-        param_tensor = param.get_tensor()
+class TestSGD(unittest.TestCase):
+    __metaclass__ = OpTestMeta
 
-        param_tensor.set_dims([2, 2])
-        param_tensor.alloc_float()
-
-        param_data = numpy.random.random((2, 2)).astype("float32")
-        param_tensor.set(param_data)
-
-        # 2. set grad
-        param = scope.create_var("grad")
-        grad_tensor = param.get_tensor()
-
-        grad_tensor.set_dims([2])
-        grad_tensor.alloc_float()
-
-        grad_data = numpy.random.random((2, 2)).astype("float32")
-        grad_tensor.set(grad_data)
-
-        param_out = scope.create_var("param_out")
-
-        learning_rate = 0.1
-        sgd_op = creation.op_creations.sgd(param="param",
-                                           grad="grad",
-                                           param_out="param_out",
-                                           learning_rate=learning_rate)
-        sgd_op.infer_shape(scope)
-
-        ctx = core.DeviceContext.cpu_context()
-        sgd_op.run(scope, ctx)
-
-        param_out_tensor = numpy.array(scope.get_var("param_out").get_tensor())
-        self.assertTrue(
-            numpy.alltrue(param_out_tensor == param_tensor - learning_rate *
-                          grad_data))
+    def setUp(self):
+        self.type = "sgd"
+        self.param = numpy.random.random((342, 345)).astype("float32")
+        self.grad = numpy.random.random((342, 345)).astype("float32")
+        self.learning_rate = 0.1
+        self.param_out = self.param - self.learning_rate * self.grad
 
 
 if __name__ == "__main__":
