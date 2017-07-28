745cd53681bcf5a0fe1ab1d43a021b387b365bc6
tensor-tang <jian.j.tang@intel.com>
2017-07-28 11:19:46 +0800

    refactor MkldnnMemory

--- paddle/gserver/layers/MkldnnMemory.h
+++ paddle/gserver/layers/MkldnnMemory.h
@@ -45,7 +45,7 @@ const static std::map<dnnfmt, std::string> DNN_FORMAT_STR = {
   {dnnfmt::Ohwi16o, "Ohwi16o"}, {dnnfmt::OhIw16o4i, "OhIw16o4i"},
   {dnnfmt::goihw, "goihw"}, {dnnfmt::gOIhw8i8o, "gOIhw8i8o"},
   {dnnfmt::gOIhw16i16o, "gOIhw16i16o"}, {dnnfmt::gOIhw8i16o2i, "gOIhw8i16o2i"},
-  // {dnnfmt::gOhwi8o, "gOhwi8o"}, {dnnfmt::gOhwi16o, "gOhwi16o"},
+  // {dnnfmt::gOhwi8o, "gOhwi8o"}, {dnnfmt::gOhwi16o, "gOhwi16o"},  // later ver
   {dnnfmt::gOIhw8o8i, "gOIhw8o8i"}, {dnnfmt::gOIhw16o16i, "gOIhw16o16i"},
   {dnnfmt::gOhIw16o4i, "gOhIw16o4i"}};
 
@@ -78,117 +78,138 @@ public:
 
   ~MkldnnBuffer() {}
 
-  void checkType(mem::primitive_desc& pd) {
-    CHECK_EQ(int(pd.desc().data.data_type), int(tp_))
-        << "input data type does not match: "
-        << pd.desc().data.data_type << " vs " << tp_;
+  /**
+   * get memory desc from details info
+   */
+  static inline mem::desc getMD(const mem::dims& dm,
+    const mem::format& fmt = mem::format::any,
+    const mem::data_type &tp = mem::data_type::f32) {
+    return mem::desc({dm}, tp, fmt);
   }
 
-  void checkType(const mem::desc& md) {
-    CHECK_EQ(int(md.data.data_type), int(tp_))
-      << "input data type does not match: "
-      << md.data.data_type << " vs " << tp_;
-  }
-
-  void resetUser(void *pdata, mem::primitive_desc pd) {
-    if (pdata == NULL) {
-      pUser_.reset(new mem(pd));
-    } else {
-      checkType(pd);
-      pUser_.reset(new mem(pd, pdata));
-    }
-  }
-
-  void resetUser(
-    void *pd, const mem::desc& md, const mkldnn::engine& eg) {
-    resetUser(pd, mem::primitive_desc(md, eg));
-  }
-
+  /**
+   * reset user buffer functions
+   */
   void resetUser(void *pd, const mem::dims& dm,
     const mem::format& fmt, const mkldnn::engine& eg) {
     resetUser(pd, mem::desc({dm}, tp_, fmt), eg);
+  }
+
+  void resetUser(
+    void *pdata, const mem::desc& md, const mkldnn::engine& eg) {
+    resetUser(pdata, mem::primitive_desc(md, eg));
+  }
+
+  void resetUser(void *pdata, const mem::primitive_desc& pd) {
+    resetMem(pUser_, pd, pdata);
   }
 
   void resetUser(const std::shared_ptr<mem>& user) {
     pUser_ = user;
   }
 
-  const std::shared_ptr<mem>& getUser() {
-    return pUser_;
+  /**
+   * update user data handle
+   */
+  void updateUserData(void* data) {
+    setData(pUser_, data);
   }
 
-  // get internal data handle
-  void* getIntlData() {
-    CHECK(pIntl_) << "shoud have inited internal buffer";
-    return pIntl_->get_data_handle();
-  }
-
-  // get internal data handle
-  void* getUserData() {
-    CHECK(pUser_) << "shoud have inited user buffer";
-    return pUser_->get_data_handle();
-  }
-
-  void resetIntl(mem::primitive_desc intlPD, void *pdata = NULL) {
-    if (nullptr != pUser_
-        && pUser_->get_primitive_desc() == intlPD
-        && (pdata == NULL || pdata == getUserData())) {
+  /**
+   * reset internal buffer functions
+   * generally, it should be after resetUser
+   */
+  void resetIntl(const mem::primitive_desc& intlPD, void *pdata = NULL) {
+    if (nullptr != pUser_ && 
+      getUserPD() == intlPD && (pdata == NULL || pdata == getUserData())) {
       pIntl_ = pUser_;
-    } else {
-      checkType(intlPD);
-      pIntl_.reset(new mem(intlPD));
+      return;
     }
-  }
 
+    if (nullptr == pUser_) {
+      LOG(WARNING) << "Generally, internal buffer should be after resetUser";
+    }
+    resetMem(pIntl_, intlPD, pdata);
+  }
+  
   void resetIntl(const std::shared_ptr<mem>& intl) {
     pIntl_ = intl;
   }
 
-  /// functions for getting infos
-  size_t getSize(size_t sz) {
-    size_t unit;
-    switch (tp_) {
-      case mem::data_type::f32:
-        unit = sizeof(float);
-        break;
-      case mem::data_type::s32:
-        unit = sizeof(signed int);
-        break;
-      default:
-        LOG(ERROR) << "Error data type";
-        return 0;
-    }
-    return sz / unit;
+  /**
+   * Other get functions of user
+   */
+  const std::shared_ptr<mem>& getUser() {
+    return pUser_;
   }
 
-  /// it's the element size not memory size
-  size_t getIntlSize() {
-    CHECK(pIntl_) << "haven't init internal layout, call resetUser then initCvt";
-    return getSize(pIntl_->get_primitive_desc().get_size());
+  // get primitive desc
+  // do not use const &
+  mem::primitive_desc getUserPD() {
+    return getPD(pUser_);
+  }
+
+  // get memory desc
+  mem::desc getUserMD() {
+    return getMD(pUser_);
+  }
+
+  // get user memory format
+  std::string getUserFmt() {
+    dnnfmt fmt = dnnfmt(getFmt(pUser_));
+    std::string fmtStr;
+    CHECK(mapGet(fmt, DNN_FORMAT_STR, &fmtStr)) << "invalid format: " << fmt;
+    return fmtStr;
+  }
+
+  const void* getUserData() {
+    return getData(pUser_);
   }
 
   /// it's the element size not memory size
   size_t getUserSize() {
-    CHECK(pUser_) << "haven't init user layout";
-    return getSize(pUser_->get_primitive_desc().get_size());
+    return getElementCnt(pUser_);
   }
 
+  /**
+   * Other get functions of internal
+   */
   const std::shared_ptr<mem>& getIntl() {
      return pIntl_;
   }
 
-  // get user primitive desc
-  mem::primitive_desc getUserPD() {
-    CHECK(pUser_) << "shoud have inited user buffer";
-    return pUser_->get_primitive_desc();
-  }
-
-  // get internal primitive desc
+  // get primitive desc
   mem::primitive_desc getIntlPD() {
-    CHECK(pIntl_) << "shoud have inited internal buffer";
-    return pIntl_->get_primitive_desc();
+    return getPD(pIntl_);
   }
 
+  // get memory desc
+  mem::desc getIntlMD() {
+    return getMD(pIntl_);
+  }
+
+  // get internal memory format
+  std::string getIntlFmt() {
+    CHECK(pIntl_) << "shoud have inited internal buffer";
+    dnnfmt fmt = dnnfmt(getFmt(pIntl_));
+    std::string fmtStr;
+    CHECK(mapGet(fmt, DNN_FORMAT_STR, &fmtStr)) << "invalid format: " << fmt;
+    return fmtStr;
+  }
+
+  const void* getIntlData() {
+    return getData(pIntl_);
+  }
+  
+  /// it's the element size not memory size
+  size_t getIntlSize() {
+    return getElementCnt(pIntl_);
+  }
+
+  /****************************************************************************/
+  /**
+   * Functions for reorder
+   */
   bool needReorder() {
     if (pUser_ == pIntl_) {
       return false;
@@ -202,12 +223,12 @@ public:
   void resetReorder(dnnCvtType_t type) {
     CHECK(type == dnnUser2Intl || type == dnnIntl2User)
       << "please specify one type of reorder";
-    if (pUser_ == pIntl_) {
+
+    if (!needReorder()) {
+      pReorder = nullptr;
       return;
     }
-    if (getIntlPD() == getUserPD() && getIntlData() == getUserData()) {
-      return;
-    }
+
     if (type == dnnUser2Intl) {
       pReorder.reset(new mkldnn::reorder(*pUser_, *pIntl_));
     } else {
@@ -222,98 +243,104 @@ public:
     return pReorder;
   }
 
-  void updateUserData(void * data) {
-    CHECK(pUser_) << "shoud have inited user buffer";
-    pUser_->set_data_handle(data);
+
+  /*************** protected functions ***************************************************/
+protected:
+  /**
+   * Reset memory buffer
+   */
+  void resetMem(
+    std::shared_ptr<mem>& pMem, const mem::primitive_desc& pd, void* pdata = NULL) {
+    checkType(pd);
+    mem::primitive_desc tmp = pd;
+    if (pdata == NULL) {
+      pMem.reset(new mem(tmp));
+    } else {
+      pMem.reset(new mem(tmp, pdata));
+    }
   }
 
-  static mem::desc getMD(const mem::dims& dm,
-    const mem::format& fmt = mem::format::any,
-    const mem::data_type &tp = mem::data_type::f32) {
-    return mem::desc({dm}, tp, fmt);
+  /**
+   * Get primitive desc
+   * caution: get_primitive_desc() return a local var
+   */
+  inline mem::primitive_desc getPD(const std::shared_ptr<mem>& pMem) {
+    CHECK(pMem) << "should have mkldnn memory";
+    return pMem->get_primitive_desc();
   }
 
-  // get user memory desc
-  mem::desc getUserMD() {
-    CHECK(pUser_) << "shoud have inited user buffer";
-    return pUser_->get_primitive_desc().desc();
+  // get memory desc from mkldnn memory
+  inline mem::desc getMD(const std::shared_ptr<mem>& pMem) {
+    return getMD(getPD(pMem));
   }
 
-  // get internal memory desc
-  mem::desc getIntlMD() {
-    CHECK(pIntl_) << "shoud have inited internal buffer";
-    return pIntl_->get_primitive_desc().desc();
+  // get memory desc from mkldnn memory
+  inline mem::desc getMD(const mem::primitive_desc& pd) {
+    mem::primitive_desc tmp = pd;
+    return tmp.desc();
   }
 
-  // get format from MD
-  static int getMDFmt(const mem::desc& md) {
+  // get format from memory desc
+  inline int getFmt(const mem::desc& md) {
     return md.data.format;
   }
 
-  // get internal memory format
-  std::string getIntlFmt() {
-    CHECK(pIntl_) << "shoud have inited internal buffer";
-    dnnfmt fmt = dnnfmt(getMDFmt(getIntlMD()));
-    std::string fmtStr;
-    CHECK(mapGet(fmt, DNN_FORMAT_STR, &fmtStr)) << "invalid format: " << fmt;
-    return fmtStr;
+  // get format from primitive desc
+  inline int getFmt(const mem::primitive_desc& pd) {
+    return getFmt(getMD(pd));
   }
 
-  // get user memory format
-  std::string getUserFmt() {
-    CHECK(pUser_) << "shoud have inited user buffer";
-    dnnfmt fmt = dnnfmt(getMDFmt(getUserMD()));
-    std::string fmtStr;
-    CHECK(mapGet(fmt, DNN_FORMAT_STR, &fmtStr)) << "invalid format: " << fmt;
-    return fmtStr;
+  // get format from memory
+  inline int getFmt(const std::shared_ptr<mem>& pMem) {
+    return getFmt(getMD(pMem));
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-  const std::shared_ptr<mem::desc> getUserMD() {
-    CHECK(pUser_) << "haven't init user layout";
-    return std::shared_ptr<mem::desc> &(pUser_->get_primitive_desc().desc());
+  // update the data handle of memory
+  inline void setData(const std::shared_ptr<mem>& pMem, void* data) {
+    CHECK(pMem) << "should have mkldnn memory";
+    pMem->set_data_handle(data);
   }
 
+  // get the data handle of  memory
+  inline const void* getData(const std::shared_ptr<mem>& pMem) {
+    CHECK(pMem) << "should have mkldnn memory";
+    return pMem->get_data_handle();
+  }
 
+  size_t getElementCnt(const std::shared_ptr<mem>& pMem) {
+    size_t bufferSize = getPD(pMem).get_size();
+    size_t unit;
+    switch (tp_) {
+      case mem::data_type::f32:
+        unit = sizeof(float);
+        break;
+      case mem::data_type::s32:
+        unit = sizeof(signed int);
+        break;
+      default:
+        LOG(ERROR) << "Error data type";
+        return 0;
+    }
+    return bufferSize / unit;
+  }
 
-//  std::shared_ptr<memory> getUserMem() {
-//     return this->pUser_;
-//  }
-
-
-  static mem::dims getMDDims(const mem::desc& md) {
+  // get dims from memory desc
+  inline std::vector<int> getDims(const mem::desc& md) {
     const int* dm = md.data.dims;
     int ndims = md.data.ndims;
     std::vector<int> v(dm, dm + ndims);
     return v;
   }
 
-
-  // get format from PD
-  static int getPDFmt(mem::primitive_desc pd) {
-    return pd.desc().data.format;
+  void checkType(const mem::desc& md) {
+    CHECK_EQ(int(md.data.data_type), int(tp_))
+      << "input data type does not match: "
+      << md.data.data_type << " vs " << tp_;
   }
-
-  mem::dims getUserDims() {
-    return getMDDims(getUserMD());
+  void checkType(const mem::primitive_desc& pd) {
+     mem::primitive_desc tmp = pd;
+    checkType(tmp.desc());
   }
-
-
-
-
-  */
 
 };
 
