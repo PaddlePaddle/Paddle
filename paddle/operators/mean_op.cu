c69ff8b5ed3ecd986789ce25b52f9d5d97ab1182
liaogang <liaogang@baidu.com>
2017-07-31 23:02:33 +0800

    FIX mean op kernel

--- paddle/operators/mean_op.cu
+++ paddle/operators/mean_op.cu
@@ -1,4 +1,4 @@
 #include "paddle/framework/op_registry.h"
 #include "paddle/operators/mean_op.h"
 
-REGISTER_OP_GPU_KERNEL(mean, ops::AddKernel<ops::GPUPlace, float>);
+REGISTER_OP_GPU_KERNEL(mean, ops::MeanKernel<ops::GPUPlace, float>);
