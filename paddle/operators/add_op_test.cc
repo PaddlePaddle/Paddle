a8a947552a00fd63b925b4fa324a7dc24af8e872
qijun <qijun1994@hotmail.com>
2017-08-01 17:25:45 +0800

    add unittest

--- paddle/operators/add_op_test.cc
+++ paddle/operators/add_op_test.cc
@@ -26,7 +26,43 @@ TEST(AddOp, GetOpProto) {
   auto it1 = op_creators.find("add_two_grad");
   ASSERT_NE(it1, op_creators.end());
 }
+#ifndef PADDLE_ONLY_CPU
 TEST(AddOp, Kernel) {
-  
+  Tensor t;
+  float* p = t.mutable_data<float>(make_ddim({6}), platform::CPUPlace());
+  for (int i = 0; i < 6; i++) {
+    p[i] = static_cast<float>(i);
+  }
 
+  Tensor t1;
+  float* p1 = t1.mutable_data<float>(make_ddim({6}), platform::CPUPlace());
+  for (int i = 0; i < 6; i++) {
+    p1[i] = static_cast<float>(i);
+  }
+
+  Tensor t2;
+  float* p2 = t2.mutable_data<float>(make_ddim({6}), platform::CPUPlace());
+  for (int i = 0; i < 6; i++) {
+    p2[i] = static_cast<float>(i);
+  }
+
+  Tensor t3;
+  float* p3 = t3.mutable_data<float>(make_ddim({6}), platform::CPUPlace());
+  for (int i = 0; i < 6; i++) {
+    p3[i] = static_cast<float>(i);
+  }
+
+  t1.mutable_data<float>(platform::GPUPlace(0));
+  t2.mutable_data<float>(platform::GPUPlace(0));
+
+  t1.CopyFrom<T>(t, platform::GPUPlace(0));
+  t2.CopyFrom<T>(t, platform::GPUPlace(0));
+
+  t3.mutable_data<float>(platform::GPUPlace(0));
+
+  CUDADeviceContext* dd = new CUDADeviceContext(0);
+
+  EigenVector<T>::Flatten(t3).device(*(dd->eigen_device())) =
+      EigenVector<T>::Flatten(t1) + EigenVector<T>::Flatten(t1);
 }
+#endif
