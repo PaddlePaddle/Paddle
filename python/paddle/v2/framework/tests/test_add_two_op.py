c9c8b3b161698e1223599cd757ce3dac63422a6f
qiaolongfei <qiaolongfei@baidu.com>
2017-08-02 17:22:49 +0800

    update unit test

--- python/paddle/v2/framework/tests/test_add_two_op.py
+++ python/paddle/v2/framework/tests/test_add_two_op.py
@@ -1,6 +1,10 @@
 import unittest
-from op_test_util import OpTestMeta
+
 import numpy
+import paddle.v2.framework.core as core
+import paddle.v2.framework.create_op_creation_methods as creation
+
+from op_test_util import OpTestMeta
 
 
 class TestAddOp(unittest.TestCase):
@@ -11,6 +15,14 @@ class TestAddOp(unittest.TestCase):
         self.X = numpy.random.random((342, 345)).astype("float32")
         self.Y = numpy.random.random((342, 345)).astype("float32")
         self.Out = self.X + self.Y
+
+
+class TestAddGradOp(unittest.TestCase):
+    def test_add_grad(self):
+        op = creation.op_creations.add_two(X="X", Y="Y")
+        backward_op = core.Operator.backward(op, set())
+        expected = '''Op(add_two_grad), inputs:(X, Y, @EMPTY@, @EMPTY@@GRAD), outputs:(X@GRAD, Y@GRAD).'''
+        self.assertEqual(expected, str(backward_op))
 
 
 if __name__ == '__main__':
