7665bdba134a05a957a7ec230429fb8e60180d26
Yan Chunwei <yanchunwei@outlook.com>
2017-08-07 14:29:50 +0800

    Rnn forward logic test (#3291)
    * finish forward debug

--- paddle/framework/operator.h
+++ paddle/framework/operator.h
@@ -174,7 +174,11 @@ class OperatorContext {
   template <typename T>
   T* Output(const size_t index) const {
     auto var = OutputVar(index);
-    PADDLE_ENFORCE(var != nullptr, "Output(%d) should not be nullptr", index);
+    PADDLE_ENFORCE(
+        var != nullptr,
+        "Output(%d) not be nullptr, which means variable [%s] does not "
+        "exist in scope",
+        index, op_.outputs_[index]);
     return var->GetMutable<T>();
   }
 
