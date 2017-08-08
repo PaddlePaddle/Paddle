7945572cee3f520eb68e906ba96742451e0ff121
dzhwinter <dongzhihong01@baidu.com>
2017-08-08 16:39:34 +0800

    Update fill_zeros_like_op.cu

--- paddle/operators/fill_zeros_like_op.cu
+++ paddle/operators/fill_zeros_like_op.cu
@@ -13,6 +13,7 @@
    limitations under the License. */
 
 #define EIGEN_USE_GPU
+#include "paddle/operators/fill_zeros_like_op.h"
 #include "paddle/framework/op_registry.h"
 
 namespace ops = paddle::operators;
