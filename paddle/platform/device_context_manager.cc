8c7027f02383cacf0dda4ba38f8cffbf75b52027
qijun <qijun1994@hotmail.com>
2017-08-16 09:38:31 +0000

    fix gpu build error

--- paddle/platform/device_context_manager.cc
+++ paddle/platform/device_context_manager.cc
@@ -1,3 +1,14 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserve.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 #include "paddle/platform/device_context_manager.h"
 
 namespace paddle {
@@ -5,6 +16,9 @@ namespace platform {
 
 DeviceContextManager::DeviceContextManager() {
 #ifndef PADDLE_ONLY_CPU
+  gpu_cnt_.reserve(kNUM_GPUS);
+  cuda_contexts_.reserve(kNUM_GPUS);
+  cuda_io_contexts_.reserve(kNUM_GPUS);  
   for (size_t i = 0; i < kNUM_GPUS; i++) {
     gpu_cnt_.at(i) = -1;
   }
@@ -14,26 +28,34 @@ DeviceContextManager::DeviceContextManager() {
 
 DeviceContext& DeviceContextManager::GetDeviceContext(Place& place) {
   if (is_cpu_place(place)) {
-    return CPUDeviceContext();
-  } else
-    (is_gpu_place(place)) {
+    if (!cpu_context_) {
+      cpu_context_ = new CPUDeviceContext();
+    }    
+    return *cpu_context_;
+  } else {
 #ifndef PADDLE_ONLY_CPU
-      PADDLE_ENFORCE(place.device < kNUM_GPUS,
-                     "GPU device id must less than kNUM_GPUS");
-      PADDLE_ENFORCE(SetDeviceId(place.device));
-      for (auto&& ctx : cuda_contexts_) {
-        ctx = new CUDADeviceContext(place);
-      }
-      gpu_cnt_.at(place.device) = 0;
-      return *cuda_contexts_[place.device][0];
+    auto gpu_place = boost::get<GPUPlace>(place);
+    auto gpu_id = gpu_place.device;
+    PADDLE_ENFORCE(gpu_id < kNUM_GPUS,
+                   "GPU device id must less than kNUM_GPUS");
+    SetDeviceId(gpu_id);
+    if(gpu_cnt_[gpu_id] == -1) {
+    cuda_contexts_[gpu_id].reserve(kNUM_STREAMS);
+    for (auto&& ctx : cuda_contexts_[gpu_id]) {
+      ctx = new CUDADeviceContext(gpu_place);
+    }
+    gpu_cnt_[gpu_id] = 0;
+    }
+    return *cuda_contexts_[gpu_id][0];
 
 #else
-      PADDLE_THROW("'GPUPlace' is not supported in CPU only device.");
+    PADDLE_THROW("'GPUPlace' is not supported in CPU only device.");
 #endif
-    }
+  }
 }
 
 DeviceContextManager::~DeviceContextManager() {
+  delete cpu_context_;
 #ifndef PADDLE_ONLY_CPU
   for (size_t i = 0; i < kNUM_GPUS; i++) {
     if (gpu_cnt_.at(i) != -1) {
--- paddle/platform/device_context_manager.h
+++ paddle/platform/device_context_manager.h
@@ -1,7 +1,18 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserve.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+#pragma once
 #include "paddle/platform/device_context.h"
 
-#define kNUM_GPUS = 16;
-#define kNUM_STREAMS = 16;
+#define kNUM_GPUS 16
+#define kNUM_STREAMS 16
 
 namespace paddle {
 namespace platform {
@@ -11,15 +22,15 @@ class DeviceContextManager {
   DeviceContextManager();
   ~DeviceContextManager();
 
-  DeviceContext* GetDeviceContext(Place& place);
-  DeviceContext* GetIODeviceContext(Place& place);
+  DeviceContext& GetDeviceContext(Place& place);
+  DeviceContext& GetIODeviceContext(Place& place);
 
  private:
+  CPUDeviceContext* cpu_context_{nullptr};
 #ifndef PADDLE_ONLY_CPU
-  std::array<std::array<CUDADeviceContext*, kNUM_STREAMS>, kNUM_GPUS>
-      cuda_contexts_;
-  std::array<CDUADeviceContext*, kNUM_GPUS> cuda_io_contexts_;
-  std::array<int, kNUM_GPUS> gpu_cnt_;
+  std::vector<std::vector<CUDADeviceContext*>> cuda_contexts_;
+  std::vector<CUDADeviceContext*> cuda_io_contexts_;
+  std::vector<int> gpu_cnt_;
 #endif
 };
 }
