6ba04dcd112e0caac46a7a829182ce00f301752f
hedaoyuan <hedaoyuan@github.com>
2017-08-17 16:56:46 +0800

    Remove the header files that do not need to be included.

--- paddle/function/DepthwiseConvOpGpu.cu
+++ paddle/function/DepthwiseConvOpGpu.cu
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "DepthwiseConvOp.h"
-#include "GemmFunctor.h"
 #include "paddle/math/BaseMatrix.h"
 
 namespace paddle {
