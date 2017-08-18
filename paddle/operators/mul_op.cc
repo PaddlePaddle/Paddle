50cf127eea23e8771c17844cf09becec61004e96
dongzhihong <dzhwinter@gmail.com>
2017-08-17 17:12:23 -0700

    "change Output to Input"

--- paddle/operators/mul_op.cc
+++ paddle/operators/mul_op.cc
@@ -66,11 +66,11 @@ class MulOpGrad : public framework::OperatorWithKernel {
     PADDLE_ENFORCE_NOT_NULL(ctx.InputVar("Y"), "Input(Y) should not be null");
     PADDLE_ENFORCE_NOT_NULL(ctx.InputVar(framework::GradVarName("Out")),
                             "Input(Out@GRAD) should not be null");
-    auto x_dims = ctx.Output<Tensor>("X")->dims();
-    auto y_dims = ctx.Output<Tensor>("Y")->dims();
+    auto x_dims = ctx.Input<Tensor>("X")->dims();
+    auto y_dims = ctx.Input<Tensor>("Y")->dims();
+    auto out_dims = ctx.Input<Tensor>(framework::GradVarName("Out"))->dims();
     auto *x_grad = ctx.Output<Tensor>(framework::GradVarName("X"));
     auto *y_grad = ctx.Output<Tensor>(framework::GradVarName("Y"));
-    auto out_dims = ctx.Input<Tensor>(framework::GradVarName("Out"))->dims();
     PADDLE_ENFORCE(x_dims[0] == out_dims[0],
                    "Out@GRAD M X N must equal to X dims 0, M ");
     PADDLE_ENFORCE(y_dims[1] == out_dims[1],
