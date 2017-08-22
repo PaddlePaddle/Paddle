ca9003ec95ee41b82cf6813dddd9cd6fc1bef599
liaogang <liaogang@baidu.com>
2017-08-22 12:44:47 +0800

    Generate optimizer.go from optimizer.go.in in Travis CI

--- paddle/scripts/travis/check_style.sh
+++ paddle/scripts/travis/check_style.sh
@@ -8,6 +8,11 @@ function abort(){
 trap 'abort' 0
 set -e
 
+# Create the build directory for CMake.
+mkdir -p $TRAVIS_BUILD_DIR/build && cd $TRAVIS_BUILD_DIR/build
+# Generate optimizer.go from optimizer.go.in
+cmake .. -DWITH_COVERAGE=ON -DWITH_GPU=OFF -DWITH_GOLANG=ON
+
 cd $TRAVIS_BUILD_DIR
 export PATH=/usr/bin:$PATH
 pre-commit install
