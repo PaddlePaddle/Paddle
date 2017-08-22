5ca4118451a38a8fa1e876fd5416028010ec218b
Liu Yiqun <liuyiqun01@baidu.com>
2017-08-22 17:27:04 +0800

    Update Dockerfile of android to support building for arm64-v8a and armeabi.

--- paddle/scripts/docker/build_android.sh
+++ paddle/scripts/docker/build_android.sh
@@ -2,22 +2,55 @@
 
 set -xe
 
-mkdir -p /paddle/build_android
-cd /paddle/build_android
+mkdir -p /paddle/build_android/$ANDROID_ABI
+cd /paddle/build_android/$ANDROID_ABI
 rm -rf /paddle/install 2>/dev/null || true
-cmake -DCMAKE_SYSTEM_NAME=Android \
-      -DANDROID_STANDALONE_TOOLCHAIN=$ANDROID_STANDALONE_TOOLCHAIN \
-      -DANDROID_ABI=armeabi-v7a \
-      -DANDROID_ARM_NEON=ON \
-      -DANDROID_ARM_MODE=ON \
-      -DHOST_C_COMPILER=/usr/bin/gcc \
-      -DHOST_CXX_COMPILER=/usr/bin/g++ \
-      -DCMAKE_INSTALL_PREFIX=/paddle/install \
-      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-      -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O3" \
-      -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O3" \
-      -DWITH_C_API=ON \
-      -DWITH_SWIG_PY=OFF \
-      ..
+
+THIRD_PARTY_PATH=/paddle/third_party_android/$ANDROID_ABI
+
+if [ $ANDROID_ABI == "armeabi-v7a" ]; then
+  cmake -DCMAKE_SYSTEM_NAME=Android \
+        -DANDROID_STANDALONE_TOOLCHAIN=$ANDROID_ARM_STANDALONE_TOOLCHAIN \
+        -DANDROID_ABI=$ANDROID_ABI \
+        -DANDROID_ARM_NEON=ON \
+        -DANDROID_ARM_MODE=ON \
+        -DHOST_C_COMPILER=/usr/bin/gcc \
+        -DHOST_CXX_COMPILER=/usr/bin/g++ \
+        -DCMAKE_INSTALL_PREFIX=/paddle/install \
+        -DTHIRD_PARTY_PATH=$THIRD_PARTY_PATH \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DWITH_C_API=ON \
+        -DWITH_SWIG_PY=OFF \
+        /paddle
+elif [ $ANDROID_ABI == "arm64-v7a" ]; then
+  cmake -DCMAKE_SYSTEM_NAME=Android \
+        -DANDROID_STANDALONE_TOOLCHAIN=$ANDROID_ARM64_STANDALONE_TOOLCHAIN \
+        -DANDROID_ABI=$ANDROID_ABI \
+        -DANDROID_ARM_MODE=ON \
+        -DHOST_C_COMPILER=/usr/bin/gcc \
+        -DHOST_CXX_COMPILER=/usr/bin/g++ \
+        -DCMAKE_INSTALL_PREFIX=/paddle/install \
+        -DTHIRD_PARTY_PATH=$THIRD_PARTY_PATH \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DWITH_C_API=ON \
+        -DWITH_SWIG_PY=OFF \
+        /paddle
+elif [ $ANDROID_ABI == "armeabi" ]; then
+  cmake -DCMAKE_SYSTEM_NAME=Android \
+        -DANDROID_STANDALONE_TOOLCHAIN=$ANDROID_ARM_STANDALONE_TOOLCHAIN \
+        -DANDROID_ABI=$ANDROID_ABI \
+        -DANDROID_ARM_MODE=ON \
+        -DHOST_C_COMPILER=/usr/bin/gcc \
+        -DHOST_CXX_COMPILER=/usr/bin/g++ \
+        -DCMAKE_INSTALL_PREFIX=/paddle/install \
+        -DTHIRD_PARTY_PATH=$THIRD_PARTY_PATH \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DWITH_C_API=ON \
+        -DWITH_SWIG_PY=OFF \
+        /paddle
+else
+  echo "Invalid ANDROID_ABI: $ANDROID_ABI"
+fi
+
 make -j `nproc`
 make install -j `nproc`
