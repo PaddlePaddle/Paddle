16fddf32a54d19913c372f4ab59c98ee507fd6ff
Xinghai Sun <sunxinghai1216@gmail.com>
2017-09-03 17:51:25 +0800

    Add broadcasting support (e.g. matrix-vector) for cos sim operator.

--- python/paddle/v2/framework/tests/test_cos_sim_op.py
+++ python/paddle/v2/framework/tests/test_cos_sim_op.py
@@ -4,7 +4,7 @@ from gradient_checker import GradientChecker, create_op
 from op_test_util import OpTestMeta
 
 
-class TestCosSimOp(unittest.TestCase):
+class TestCosSimOpWithRank2(unittest.TestCase):
     __metaclass__ = OpTestMeta
 
     def setUp(self):
@@ -24,12 +24,72 @@ class TestCosSimOp(unittest.TestCase):
         }
 
 
+class TestCosSimOpWithRank2Bcast(unittest.TestCase):
+    __metaclass__ = OpTestMeta
+
+    def setUp(self):
+        self.type = "cos_sim"
+        self.inputs = {
+            'X': np.random.random((32, 64)).astype("float32"),
+            'Y': np.random.random((1, 64)).astype("float32")
+        }
+        expect_x_norm = np.linalg.norm(self.inputs['X'], axis=1)
+        expect_y_norm = np.linalg.norm(self.inputs['Y'], axis=1)
+        expect_out = (self.inputs['X'] * self.inputs['Y']).sum(axis=1) / \
+            expect_x_norm / expect_y_norm
+        self.outputs = {
+            'XNorm': np.expand_dims(expect_x_norm, 1),
+            'YNorm': np.expand_dims(expect_y_norm, 1),
+            'Out': np.expand_dims(expect_out, 1)
+        }
+
+
+class TestCosSimOpWithRank3(unittest.TestCase):
+    __metaclass__ = OpTestMeta
+
+    def setUp(self):
+        self.type = "cos_sim"
+        self.inputs = {
+            'X': np.random.random((32, 64, 10)).astype("float32"),
+            'Y': np.random.random((32, 64, 10)).astype("float32")
+        }
+        expect_x_norm = np.linalg.norm(self.inputs['X'], axis=(1, 2))
+        expect_y_norm = np.linalg.norm(self.inputs['Y'], axis=(1, 2))
+        expect_out = (self.inputs['X'] * self.inputs['Y']).sum(axis=(1, 2)) / \
+            expect_x_norm / expect_y_norm
+        self.outputs = {
+            'XNorm': np.expand_dims(expect_x_norm, 1),
+            'YNorm': np.expand_dims(expect_y_norm, 1),
+            'Out': np.expand_dims(expect_out, 1)
+        }
+
+
+class TestCosSimOpWithRank3Bcast(unittest.TestCase):
+    __metaclass__ = OpTestMeta
+
+    def setUp(self):
+        self.type = "cos_sim"
+        self.inputs = {
+            'X': np.random.random((32, 64, 10)).astype("float32"),
+            'Y': np.random.random((1, 64, 10)).astype("float32")
+        }
+        expect_x_norm = np.linalg.norm(self.inputs['X'], axis=(1, 2))
+        expect_y_norm = np.linalg.norm(self.inputs['Y'], axis=(1, 2))
+        expect_out = (self.inputs['X'] * self.inputs['Y']).sum(axis=(1, 2)) / \
+            expect_x_norm / expect_y_norm
+        self.outputs = {
+            'XNorm': np.expand_dims(expect_x_norm, 1),
+            'YNorm': np.expand_dims(expect_y_norm, 1),
+            'Out': np.expand_dims(expect_out, 1)
+        }
+
+
 class TestCosSimGradOp(GradientChecker):
     def setUp(self):
         self.op = create_op("cos_sim")
         self.inputs = {
-            'X': np.random.random((10, 5)).astype("float32"),
-            'Y': np.random.random((10, 5)).astype("float32")
+            'X': np.random.random((6, 5)).astype("float32"),
+            'Y': np.random.random((6, 5)).astype("float32")
         }
 
     def test_cpu_gpu_compare(self):
@@ -54,6 +114,33 @@ class TestCosSimGradOp(GradientChecker):
             "Out",
             max_relative_error=0.05,
             no_grad_set={"Y"})
+
+
+class TestCosSimGradOpWithRank2Bcast(TestCosSimGradOp):
+    def setUp(self):
+        self.op = create_op("cos_sim")
+        self.inputs = {
+            'X': np.random.random((6, 5)).astype("float32"),
+            'Y': np.random.random((1, 5)).astype("float32")
+        }
+
+
+class TestCosSimGradOpWithRank3(TestCosSimGradOp):
+    def setUp(self):
+        self.op = create_op("cos_sim")
+        self.inputs = {
+            'X': np.random.random((6, 5, 2)).astype("float32"),
+            'Y': np.random.random((6, 5, 2)).astype("float32")
+        }
+
+
+class TestCosSimGradOpWithRank3Bcast(TestCosSimGradOp):
+    def setUp(self):
+        self.op = create_op("cos_sim")
+        self.inputs = {
+            'X': np.random.random((6, 5, 2)).astype("float32"),
+            'Y': np.random.random((1, 5, 2)).astype("float32")
+        }
 
 
 if __name__ == '__main__':
