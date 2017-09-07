e033569dd649c08a986a4d97608692f354003c78
wanghaoshuang <wanghaoshuang@baidu.com>
2017-09-07 11:22:21 +0800

    Fix format

--- paddle/function/SwitchOpGpu.cu
+++ paddle/function/SwitchOpGpu.cu
@@ -12,14 +12,18 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "hl_base.h"
 #include "SwitchOp.h"
+#include "hl_base.h"
 
 namespace paddle {
 
-__global__ void KeNCHW2NHWC(real* outputs, const real* inputs,
-                      int inC, int inH, int inW,
-                      int nthreads, int argType) {
+__global__ void KeNCHW2NHWC(real* outputs,
+                            const real* inputs,
+                            int inC,
+                            int inH,
+                            int inW,
+                            int nthreads,
+                            int argType) {
   const int idx = threadIdx.x + blockIdx.x * blockDim.x;
   if (idx < nthreads) {
     const int w = idx % inW;
@@ -27,7 +31,7 @@ __global__ void KeNCHW2NHWC(real* outputs, const real* inputs,
     const int c = (idx / inW / inH) % inC;
     const int n = idx / inW / inH / inC;
 
-    const int off = ((n * inH + h) * inW + w) * inC +c;
+    const int off = ((n * inH + h) * inW + w) * inC + c;
     if (argType == ADD_TO) {
       outputs[off] += inputs[idx];
     } else {
@@ -38,23 +42,27 @@ __global__ void KeNCHW2NHWC(real* outputs, const real* inputs,
 
 template <>
 void NCHW2NHWC<DEVICE_TYPE_GPU>(real* outputs,
-                          const real* inputs,
-                          const int num,
-                          const int inC,
-                          const int inH,
-                          const int inW,
-                          const int argType) {
+                                const real* inputs,
+                                const int num,
+                                const int inC,
+                                const int inH,
+                                const int inW,
+                                const int argType) {
   size_t nth = num * inC * inH * inW;
   int blockSize = 1024;
   int gridSize = (nth + 1024 - 1) / 1024;
-  KeNCHW2NHWC<<<gridSize, blockSize, 0, STREAM_DEFAULT>>>
-    (outputs, inputs, inC, inH, inW, nth, argType);
+  KeNCHW2NHWC<<<gridSize, blockSize, 0, STREAM_DEFAULT>>>(
+      outputs, inputs, inC, inH, inW, nth, argType);
   CHECK_SYNC("NCHW2NHWC");
 }
 
-__global__ void KeNHWC2NCHW(real* outputs, const real* inputs,
-                          int inH, int inW, int inC,
-                          int nthreads, int argType) {
+__global__ void KeNHWC2NCHW(real* outputs,
+                            const real* inputs,
+                            int inH,
+                            int inW,
+                            int inC,
+                            int nthreads,
+                            int argType) {
   const int idx = threadIdx.x + blockIdx.x * blockDim.x;
   if (idx < nthreads) {
     const int c = idx % inC;
@@ -73,17 +81,17 @@ __global__ void KeNHWC2NCHW(real* outputs, const real* inputs,
 
 template <>
 void NHWC2NCHW<DEVICE_TYPE_GPU>(real* outputs,
-                              const real* inputs,
-                              const int num,
-                              const int inH,
-                              const int inW,
-                              const int inC,
-                              const int argType) {
+                                const real* inputs,
+                                const int num,
+                                const int inH,
+                                const int inW,
+                                const int inC,
+                                const int argType) {
   int nth = num * inC * inH * inW;
   int blockSize = 1024;
   int gridSize = (nth + 1024 - 1) / 1024;
-  KeNHWC2NCHW<<<gridSize, blockSize, 0, STREAM_DEFAULT>>>
-    (outputs, inputs, inH, inW, inC, nth, argType);
+  KeNHWC2NCHW<<<gridSize, blockSize, 0, STREAM_DEFAULT>>>(
+      outputs, inputs, inH, inW, inC, nth, argType);
   CHECK_SYNC("NHWC2NCHW");
 }
 
