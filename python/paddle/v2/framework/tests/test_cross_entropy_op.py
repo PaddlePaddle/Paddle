c0192889ccfb947c391c08bf9827317f3769497e
qijun <qijun1994@hotmail.com>
2017-09-08 15:51:13 +0800

    fix code style

--- python/paddle/v2/framework/tests/test_cross_entropy_op.py
+++ python/paddle/v2/framework/tests/test_cross_entropy_op.py
@@ -8,7 +8,8 @@ class TestCrossEntropy(OpTest):
         self.op_type = "onehot_cross_entropy"
         batch_size = 30
         class_num = 10
-        X = numpy.random.uniform(0.1, 1.0, [batch_size, class_num]).astype("float32")
+        X = numpy.random.uniform(0.1, 1.0,
+                                 [batch_size, class_num]).astype("float32")
         label = (class_num / 2) * numpy.ones(batch_size).astype("int32")
         self.inputs = {'X': X, 'label': label}
         Y = []
