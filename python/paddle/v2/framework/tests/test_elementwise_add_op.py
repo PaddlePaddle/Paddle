d39e95e5b1123c1c01358bc2b229e327b0b28eaa
gongweibao <weibao.gong@gmail.com>
2017-09-15 12:52:35 +0000

    restruct test ok

--- python/paddle/v2/framework/tests/test_elementwise_add_op.py
+++ python/paddle/v2/framework/tests/test_elementwise_add_op.py
@@ -3,14 +3,9 @@ import numpy as np
 from op_test import OpTest
 
 
-class TestElementwiseMulOp_Matrix(OpTest):
+class TestElementwiseOp(OpTest):
     def setUp(self):
         self.op_type = "elementwise_add"
-        """ Warning
-        CPU gradient check error!
-        'X': np.random.random((32,84)).astype("float32"),
-        'Y': np.random.random((32,84)).astype("float32")
-        """
         self.inputs = {
             'X': np.random.uniform(0.1, 1, [13, 17]).astype("float32"),
             'Y': np.random.uniform(0.1, 1, [13, 17]).astype("float32")
@@ -32,7 +27,7 @@ class TestElementwiseMulOp_Matrix(OpTest):
             ['X'], 'Out', max_relative_error=0.1, no_grad_set=set('Y'))
 
 
-class TestElementwiseMulOp_Vector(OpTest):
+class TestElementwiseMulOp_Vector(TestElementwiseOp):
     def setUp(self):
         self.op_type = "elementwise_add"
         self.inputs = {
@@ -41,22 +36,8 @@ class TestElementwiseMulOp_Vector(OpTest):
         }
         self.outputs = {'Out': np.add(self.inputs['X'], self.inputs['Y'])}
 
-    def test_check_output(self):
-        self.check_output()
 
-    def test_check_grad_normal(self):
-        self.check_grad(['X', 'Y'], 'Out', max_relative_error=0.1)
-
-    def test_check_grad_ingore_x(self):
-        self.check_grad(
-            ['Y'], 'Out', max_relative_error=0.1, no_grad_set=set("X"))
-
-    def test_check_grad_ingore_y(self):
-        self.check_grad(
-            ['X'], 'Out', max_relative_error=0.1, no_grad_set=set('Y'))
-
-
-class TestElementwiseMulOp_broadcast_0(OpTest):
+class TestElementwiseMulOp_broadcast_0(TestElementwiseOp):
     def setUp(self):
         self.op_type = "elementwise_add"
         self.inputs = {
@@ -69,22 +50,8 @@ class TestElementwiseMulOp_broadcast_0(OpTest):
             'Out': self.inputs['X'] + self.inputs['Y'].reshape(2, 1, 1)
         }
 
-    def test_check_output(self):
-        self.check_output()
 
-    def test_check_grad_normal(self):
-        self.check_grad(['X', 'Y'], 'Out', max_relative_error=0.1)
-
-    def test_check_grad_ingore_x(self):
-        self.check_grad(
-            ['Y'], 'Out', max_relative_error=0.1, no_grad_set=set("X"))
-
-    def test_check_grad_ingore_y(self):
-        self.check_grad(
-            ['X'], 'Out', max_relative_error=0.1, no_grad_set=set('Y'))
-
-
-class TestElementwiseMulOp_broadcast_1(OpTest):
+class TestElementwiseMulOp_broadcast_1(TestElementwiseOp):
     def setUp(self):
         self.op_type = "elementwise_add"
         self.inputs = {
@@ -97,22 +64,8 @@ class TestElementwiseMulOp_broadcast_1(OpTest):
             'Out': self.inputs['X'] + self.inputs['Y'].reshape(1, 3, 1)
         }
 
-    def test_check_output(self):
-        self.check_output()
 
-    def test_check_grad_normal(self):
-        self.check_grad(['X', 'Y'], 'Out', max_relative_error=0.1)
-
-    def test_check_grad_ingore_x(self):
-        self.check_grad(
-            ['Y'], 'Out', max_relative_error=0.1, no_grad_set=set("X"))
-
-    def test_check_grad_ingore_y(self):
-        self.check_grad(
-            ['X'], 'Out', max_relative_error=0.1, no_grad_set=set('Y'))
-
-
-class TestElementwiseMulOp_broadcast_2(OpTest):
+class TestElementwiseMulOp_broadcast_2(TestElementwiseOp):
     def setUp(self):
         self.op_type = "elementwise_add"
         self.inputs = {
@@ -124,22 +77,8 @@ class TestElementwiseMulOp_broadcast_2(OpTest):
             'Out': self.inputs['X'] + self.inputs['Y'].reshape(1, 1, 4)
         }
 
-    def test_check_output(self):
-        self.check_output()
 
-    def test_check_grad_normal(self):
-        self.check_grad(['X', 'Y'], 'Out', max_relative_error=0.1)
-
-    def test_check_grad_ingore_x(self):
-        self.check_grad(
-            ['Y'], 'Out', max_relative_error=0.1, no_grad_set=set("X"))
-
-    def test_check_grad_ingore_y(self):
-        self.check_grad(
-            ['X'], 'Out', max_relative_error=0.1, no_grad_set=set('Y'))
-
-
-class TestElementwiseMulOp_broadcast_3(OpTest):
+class TestElementwiseMulOp_broadcast_3(TestElementwiseOp):
     def setUp(self):
         self.op_type = "elementwise_add"
         self.inputs = {
--- python/paddle/v2/framework/tests/test_elementwise_op.py
+++ python/paddle/v2/framework/tests/test_elementwise_op.py
@@ -6,6 +6,7 @@ from op_test import OpTest
 def generator():
     l = []
 
+    #matrix * matrix
     x = np.random.uniform(0.1, 1, [13, 17]).astype("float32")
     y = np.random.uniform(0.1, 1, [13, 17]).astype("float32")
     d = {
@@ -18,52 +19,132 @@ def generator():
             'Out': np.multiply(x, y)
         }
     }
+    l.append(d)
 
+    #vector * vector
+    x = np.random.random((32, )).astype("float32")
+    y = np.random.random((32, )).astype("float32")
+    d = {
+        'op_type': "elementwise_mul",
+        'inputs': {
+            'X': x,
+            'Y': y
+        },
+        'outputs': {
+            'Out': np.multiply(x, y)
+        }
+    }
+    l.append(d)
+
+    #broad cast axis 0
+    x = np.random.rand(2, 3, 4).astype(np.float32)
+    y = np.random.rand(2).astype(np.float32)
+    d = {
+        'op_type': "elementwise_mul",
+        'inputs': {
+            'X': x,
+            'Y': y
+        },
+        'outputs': {
+            'Out': np.multiply(x, y.reshape(2, 1, 1))
+        },
+        'attrs': {
+            'axis': 0
+        }
+    }
+    l.append(d)
+
+    #broad cast axis 1
+    x = np.random.rand(2, 3, 4).astype(np.float32)
+    y = np.random.rand(3).astype(np.float32)
+    d = {
+        'op_type': "elementwise_mul",
+        'inputs': {
+            'X': x,
+            'Y': y
+        },
+        'outputs': {
+            'Out': np.multiply(x, y.reshape(1, 3, 1))
+        },
+        'attrs': {
+            'axis': 1
+        }
+    }
+    l.append(d)
+
+    #broad cast axis 2
+    x = np.random.rand(2, 3, 4).astype(np.float32)
+    y = np.random.rand(4).astype(np.float32)
+    d1 = {
+        'op_type': "elementwise_mu",
+        'inputs': {
+            'X': x,
+            'Y': y
+        },
+        'outputs': {
+            'Out': np.multiply(x, y.reshape(1, 1, 4))
+        },
+        'attrs': {
+            'axis': 2
+        }
+    }
+    l.append(d1)
+
+    #broad cast with axis 2
+    x = np.random.rand(2, 3, 4, 5).astype(np.float32)
+    y = np.random.rand(3, 4).astype(np.float32)
+    d = {
+        'op_type': "elementwise_mul",
+        'inputs': {
+            'X': x,
+            'Y': y
+        },
+        'outputs': {
+            'Out': np.multiply(x, y.reshape(1, 3, 4, 1))
+        },
+        'attrs': {
+            'axis': 1
+        }
+    }
     l.append(d)
 
     for t in l:
         yield t
 
 
-class DataProvider(type):
-    def __new__(cls, name, bases, metaattrs):
-        for t in generator():
-            metaattrs['op_type'] = t['op_type']
-            print metaattrs['op_type']
-            metaattrs['inputs'] = t['inputs']
-            metaattrs['outputs'] = t['outputs']
-            if 'attr' in t:
-                metaattrs['attr'] = t['attr']
-
-            return type.__new__(cls, name, bases, metaattrs)
+def test_check_output(self):
+    self.check_output()
 
 
-class ElementwiseOp(OpTest, object):
-    __metaclass__ = DataProvider
-    '''
-    def setUp(self):
-        self.op_type = "elementwise"
-        self.inputs = {
-            'X': np.random.uniform(0.1, 1, [13, 17]).astype("float32"),
-            'Y': np.random.uniform(0.1, 1, [13, 17]).astype("float32")
-        }
-        self.outputs = {'Out': np.multiply(self.inputs['X'], self.inputs['Y'])}
-    '''
+def test_check_grad_normal(self):
+    self.check_grad(['X', 'Y'], 'Out', max_relative_error=0.1)
 
-    def test_check_output(self):
-        self.check_output()
 
-    def test_check_grad_normal(self):
-        self.check_grad(['X', 'Y'], 'Out', max_relative_error=0.1)
+def test_check_grad_ingore_x(self):
+    self.check_grad(['Y'], 'Out', max_relative_error=0.1, no_grad_set=set("X"))
 
-    def test_check_grad_ingore_x(self):
-        self.check_grad(
-            ['Y'], 'Out', max_relative_error=0.1, no_grad_set=set("X"))
 
-    def test_check_grad_ingore_y(self):
-        self.check_grad(
-            ['X'], 'Out', max_relative_error=0.1, no_grad_set=set('Y'))
+def test_check_grad_ingore_y(self):
+    self.check_grad(['X'], 'Out', max_relative_error=0.1, no_grad_set=set('Y'))
 
+
+c = []
+for t in generator():
+    c.append(
+        type('Test', (OpTest, ), {
+            'test_check_output': test_check_output,
+            'test_check_grad_normal': test_check_grad_normal,
+            'test_check_grad_ingore_x': test_check_grad_ingore_x,
+            'test_check_grad_ingore_y': test_check_grad_ingore_y,
+            'op_type': t['op_type'],
+            'inputs': t['inputs'],
+            'attrs': t['attrs'] if 'attrs' in t else dict(),
+            'outputs': t['outputs']
+        }))
+"""
+for t in c:
+    print t.op_type
+"""
 
 if __name__ == '__main__':
     unittest.main()
