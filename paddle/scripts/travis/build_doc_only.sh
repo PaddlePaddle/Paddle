1489e7d51bbd739039fb17c94c7872134125300f
Varun Arora <aroravarun@baidu.com>
2017-09-20 14:56:51 -0700

    Fix src documentation dir

--- paddle/scripts/travis/build_doc_only.sh
+++ paddle/scripts/travis/build_doc_only.sh
@@ -4,6 +4,19 @@ set -e
 rm CMakeLists.txt
 mv CMakeLists.doc.txt CMakeLists.txt
 
+rm requirements.txt
+cat >> requirements.txt <<EOF
+Sphinx==1.5.6
+sphinx_rtd_theme==0.1.9
+recommonmark
+numpy>=1.12
+protobuf==3.1
+nltk>=3.2.2
+rarfile
+scipy>=0.19.0
+Pillow
+EOF
+
 # Create the build directory for CMake.
 mkdir -p $TRAVIS_BUILD_DIR/build
 cd $TRAVIS_BUILD_DIR/build
@@ -15,8 +28,8 @@ make -j `nproc` paddle_docs paddle_docs_cn
 
 mkdir -p $TRAVIS_BUILD_DIR/build_docs/en
 mkdir -p $TRAVIS_BUILD_DIR/build_docs/cn
-mv docs/en/ $TRAVIS_BUILD_DIR/build_docs/en/
-mv docs/cn/ $TRAVIS_BUILD_DIR/build_docs/en/
+mv doc/en/* $TRAVIS_BUILD_DIR/build_docs/en/
+mv doc/cn/* $TRAVIS_BUILD_DIR/build_docs/en/
 
 # deploy to remote server
 openssl aes-256-cbc -d -a -in $TRAVIS_BUILD_DIR/paddle/scripts/travis/ubuntu.pem.enc -out ubuntu.pem -k $DEC_PASSWD
