948d1d782b3e1e367bab03a57efe20204d9da176
fengjiayi <fengjiayi@baidu.com>
2017-09-22 12:58:52 -0700

    Fix progma_once error

--- paddle/operators/elementwise_div_op.h
+++ paddle/operators/elementwise_div_op.h
@@ -12,6 +12,8 @@
    See the License for the specific language governing permissions and
    limitations under the License. */
 
+#pragma once
+
 #include "paddle/operators/elementwise_op.h"
 
 namespace paddle {
