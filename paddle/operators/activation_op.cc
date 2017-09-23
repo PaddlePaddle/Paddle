9acdb74b0490f62f2721131795cdabba4453fd5f
qiaolongfei <qiaolongfei@baidu.com>
2017-09-23 11:45:28 -0700

    change add_op, clip_op, conv2d_op and activation_op

--- paddle/operators/activation_op.cc
+++ paddle/operators/activation_op.cc
@@ -22,9 +22,8 @@ class ActivationOp : public framework::OperatorWithKernel {
   using framework::OperatorWithKernel::OperatorWithKernel;
 
  protected:
-  void InferShape(const framework::InferShapeContext &ctx) const override {
-    ctx.Output<framework::LoDTensor>("Y")->Resize(
-        ctx.Input<framework::Tensor>("X")->dims());
+  void InferShape(const framework::InferShapeContextBase &ctx) const override {
+    ctx.SetOutputDim("Y", ctx.GetInputDim("X"));
   }
 };
 
@@ -33,9 +32,8 @@ class ActivationOpGrad : public framework::OperatorWithKernel {
   using framework::OperatorWithKernel::OperatorWithKernel;
 
  protected:
-  void InferShape(const framework::InferShapeContext &ctx) const override {
-    ctx.Output<framework::LoDTensor>(framework::GradVarName("X"))
-        ->Resize(ctx.Input<framework::Tensor>("Y")->dims());
+  void InferShape(const framework::InferShapeContextBase &ctx) const override {
+    ctx.SetOutputDim(framework::GradVarName("X"), ctx.GetInputDim("Y"));
   }
 };
 
