735737d28369d6040d0bacbae9973052e51cd7af
caoying03 <caoying03@baidu.com>
2017-09-29 21:33:19 +0800

    initialize crf opreator.

---
+++ python/paddle/v2/framework/tests/test_crf_op.py
@@ -0,0 +1,13 @@
+import unittest
+import numpy as np
+
+
+class TestCrfOp(OpTest):
+    def setUp(self):
+        self.op_type = "crf"
+        batch_size = 3
+        class_num = 37
+
+
+if __name__ == "__main__":
+    unittest.main()
