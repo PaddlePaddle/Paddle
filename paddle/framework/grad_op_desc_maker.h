3723304da953cce7aa88d1fdbd684bff91412dae
Yu Yang <yuyang18@baidu.com>
2017-10-02 11:56:14 -0700

    Add missing ctor

--- paddle/framework/grad_op_desc_maker.h
+++ paddle/framework/grad_op_desc_maker.h
@@ -79,6 +79,8 @@ class GradOpDescMakerBase {
 
 class SingleGradOpDescMaker : public GradOpDescMakerBase {
  public:
+  using GradOpDescMakerBase::GradOpDescMakerBase;
+
   std::vector<OpDescBind> operator()() const { return {this->Apply()}; }
 
  protected:
@@ -86,6 +88,9 @@ class SingleGradOpDescMaker : public GradOpDescMakerBase {
 };
 
 class DefaultGradOpDescMaker : public SingleGradOpDescMaker {
+ public:
+  using SingleGradOpDescMaker::SingleGradOpDescMaker;
+
  protected:
   virtual OpDescBind Apply() const {
     OpDescBind grad;
