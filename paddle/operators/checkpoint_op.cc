6d24d23bb1e8b29298ed89fca912cfe260e238c3
dongzhihong <dzhwinter@gmail.com>
2017-10-09 17:20:15 -0700

    "fix run once"

--- paddle/operators/checkpoint_op.cc
+++ paddle/operators/checkpoint_op.cc
@@ -25,7 +25,10 @@ class CheckpointOp : public framework::OperatorWithKernel {
 
  protected:
   void InferShape(framework::InferShapeContextBase* ctx) const override {
-    PADDLE_ENFORCE(ctx->HasInput("absolutePath"),
+    PADDLE_ENFORCE(ctx->HasInput("Step"),
+                   "Input(Step) of Checkpoint should not be null.");
+    std::string absolutePath = ctx->Attrs().Get<std::string>("absolutePath");
+    PADDLE_ENFORCE(absolutePath != "",
                    "Input(absolutePath) of Checkpoint should not be null.");
   }
 };
@@ -35,10 +38,13 @@ class CheckpointOpMaker : public framework::OpProtoAndCheckerMaker {
   CheckpointOpMaker(framework::OpProto* proto,
                     framework::OpAttrChecker* op_checker)
       : OpProtoAndCheckerMaker(proto, op_checker) {
+    AddInput("Step", "(Tensor), indicate the training process.");
     AddAttr<std::string>("absolutePath", "the absolutePath for save model.");
-    AddAttr<int>("interval",
-                 "(int, default 0) time seconds interval for saving "
-                 "checkpoint. 0 means only save chekcpoint once");
+    AddAttr<int>(
+        "everyNStep",
+        "(int, default 0) interval for saving checkpoint in every n step."
+        "0 means only save checkpoint once")
+        .SetDefault(0);
     AddComment(R"DOC(
 Save the workload environment to the absolute path.
 
@@ -54,12 +60,21 @@ class CheckpointKernel : public framework::OpKernel<T> {
   void Compute(const framework::ExecutionContext& ctx) const override {
     std::string absolutePath = ctx.template Attr<std::string>("absolutePath");
     // TODO(dzh) : checkpoint kernel need executor support :
-    // 1. asynchronously call of operator
+    // 1. operator should support asynchronously running
     // 2. checkpoint op need at least two thread.
-    //    Because checkpoint will happen per-interval, so need a thread wait
+    //    Because checkpoint will happen every, so need a thread wait
     //    the timer/steps to reach the condition.
-    // int interval = ctx.template Attr<int>("interval");
+    auto* Step = ctx.Input<Tensor>("Step");
+    const int* curr_step = Step->data<int>();
+    int every_n_step = ctx.template Attr<int>("everyNStep");
     auto& scope = ctx.scope();
+
+    if (every_n_step == 0 && !run_once) {
+      run_once = true;
+    }
+
+    if (run_once || *curr_step % every_n_step != 0) return;
+
     std::vector<std::string> ins = scope.GetAllNames();
     std::vector<framework::Variable*> inputs;
     for (auto& name : ins) {
@@ -74,6 +89,9 @@ class CheckpointKernel : public framework::OpKernel<T> {
     }
     fout.close();
   }
+
+  // flag indicate this op may be skipped.
+  mutable bool run_once = false;
 };
 
 }  // namespace operators
