2304935d24f4477ea5b02d671a3733cab19e1943
Kavya Srinet <kavyasrinet@baidu.com>
2017-10-09 17:39:00 -0700

    Fixing the unit test

--- python/paddle/v2/framework/tests/test_activation_op.py
+++ python/paddle/v2/framework/tests/test_activation_op.py
@@ -68,15 +68,12 @@ class TestHardShrink(OpTest):
         self.op_type = "hard_shrink"
         x = np.random.uniform(-1, 1, [4, 4]).astype("float32")
         threshold = 0.5
-        x[(-threshold + 0.005) <= x] = 0
-        x[x <= (threshold + 0.005)] = 0
 
         self.inputs = {'X': x}
         self.attrs = {'lambda': threshold}
 
         t = np.copy(x)
-        t[-threshold <= t] = 0
-        t[t <= threshold] = 0
+        t[(t >= -threshold) & (t <= threshold)] = 0
         self.outputs = {'Y': t}
 
     def test_check_output(self):
