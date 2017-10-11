e9a0c4ef87134d061ba952bb89c0dfe01eedc37e
qiaolongfei <qiaolongfei@baidu.com>
2017-10-10 19:57:30 -0700

    expose AppendBackward of ProgramDesc to python

--- python/paddle/v2/framework/tests/test_program.py
+++ python/paddle/v2/framework/tests/test_program.py
@@ -1,4 +1,6 @@
 import unittest
+
+import paddle.v2.framework.core as core
 from paddle.v2.framework.graph import g_program
 
 
@@ -30,6 +32,21 @@ class TestProgram(unittest.TestCase):
         b = g_program.current_block()
         self.assertEqual(1, b.idx)
         self.assertEqual(0, b.parent_idx)
+
+    def test_backward(self):
+        prog = core.ProgramDesc.__create_program_desc__()
+        self.assertIsNotNone(prog)
+        block = prog.block(0)
+        self.assertIsNotNone(block)
+
+        sum_op_desc = block.append_op()
+        sum_op_desc.set_type("sum")
+        sum_op_desc.set_input("X", ["x1", "x2"])
+        sum_op_desc.set_output("Out", ["out"])
+
+        self.assertEqual(len(block.all_ops()), 1)
+        prog.backward(set())
+        self.assertEqual(len(block.all_ops()), 3)
 
 
 if __name__ == '__main__':
