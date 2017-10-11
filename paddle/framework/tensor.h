935e64577ed3965704ea92cc3216510a131e5346
qijun <qijun1994@hotmail.com>
2017-10-10 19:39:13 -0700

    refine code comments and add TODO list

--- paddle/framework/tensor.h
+++ paddle/framework/tensor.h
@@ -89,10 +89,13 @@ class Tensor {
    *
    * @param[in] src        The external tensor.
    * @param[in] dst_place  The dst place.
-   * @param[in] ctx        The device context contains CUDA stream.
+   * @param[in] ctx        The device context contains device resources.
    *
    * @note    CopyFrom supports CPU <-> GPU, GPU <-> GPU.
    */
+  // TODO(qijun): https://github.com/PaddlePaddle/Paddle/issues/4647
+  // Remove `CopyFrom` and `CopyFromVector` from Tensor interface
+  // and make them global functions
   template <typename T>
   inline void CopyFrom(const Tensor& src, const platform::Place& dst_place,
                        const platform::DeviceContext& ctx);
@@ -101,7 +104,7 @@ class Tensor {
    * @brief   Copy the content of an external vector to a tensor.
    *
    * @param[in] src        The external tensor.
-   * @param[in] ctx        The device context contains CUDA stream.
+   * @param[in] ctx        The device context contains device resources.
    *
    * * @note    CopyFromVector assumes that the tensor has been resized
    *            before invoking.
