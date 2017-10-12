4aae1fff78d805ef9c2c08e6fc8702cc3e3ccc25
chengduoZH <zhaochengduo@163.com>
2017-10-12 15:13:10 +0800

    fix conv3d_gemm, unit test and follow comments

--- paddle/operators/conv3d_op.cc
+++ paddle/operators/conv3d_op.cc
@@ -1,16 +1,16 @@
 /* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserve.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. */
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
 
 #include "paddle/operators/conv3d_op.h"
 
@@ -52,7 +52,7 @@ void Conv3DOp::InferShape(framework::InferShapeContext* ctx) const {
     output_shape.push_back(OutputSizeConv3d(in_dims[i + 2], filter_dims[i],
                                             paddings[i], strides[i]));
   }
-  ctx->SetOutputDim("Out", framework::make_ddim(output_shape));
+  ctx->SetOutputDim("Output", framework::make_ddim(output_shape));
 }
 
 void Conv3DOpGrad::InferShape(framework::InferShapeContext* ctx) const {
