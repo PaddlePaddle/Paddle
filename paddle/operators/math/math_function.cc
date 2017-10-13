7b1834330537594b2dc9267d9249f9d84fef391b
qijun <qijun1994@hotmail.com>
2017-10-13 15:02:52 -0700

    remove unused header file

--- paddle/operators/math/math_function.cc
+++ paddle/operators/math/math_function.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/operators/math/math_function.h"
-#include <set>
 
 namespace paddle {
 namespace operators {
