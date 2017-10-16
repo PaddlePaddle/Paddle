6e1c25c0cee877fdd4e12be8ed9be8bf6019d35f
ludaming <ludaming@baidu.com>
2017-10-16 16:17:25 -0700

    try deploy again

--- paddle/scripts/travis/build_doc_only.sh
+++ paddle/scripts/travis/build_doc_only.sh
@@ -22,16 +22,10 @@ mkdir -p $TRAVIS_BUILD_DIR/build_docs/cn
 mv doc/en/* $TRAVIS_BUILD_DIR/build_docs/en/
 mv doc/cn/* $TRAVIS_BUILD_DIR/build_docs/cn/
 
-# deploy to remote server
-openssl aes-256-cbc -d -a -in $TRAVIS_BUILD_DIR/paddle/scripts/travis/ubuntu.pem.enc -out ubuntu.pem -k $DEC_PASSWD
-
-eval "$(ssh-agent -s)"
-chmod 400 ubuntu.pem
-
-ssh-add ubuntu.pem
 
 mkdir -p $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH
 mv $TRAVIS_BUILD_DIR/build_docs/* $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH/
+
 
 # pull PaddlePaddle.org app and strip
 # https://github.com/PaddlePaddle/PaddlePaddle.org/archive/master.zip
@@ -44,7 +38,18 @@ sudo pip install -r requirements.txt
 mkdir ./tmp
 python manage.py deploy_documentation $TRAVIS_BUILD_DIR/build_docs_versioned/$TRAVIS_BRANCH/ $TRAVIS_BRANCH ./tmp
 
-rsync -r $TRAVIS_BUILD_DIR/build_docs_versioned/ ubuntu@52.76.173.135:/var/content_staging/docs
+cd ../..
+
+# deploy to remote server
+openssl aes-256-cbc -d -a -in $TRAVIS_BUILD_DIR/paddle/scripts/travis/ubuntu.pem.enc -out ubuntu.pem -k $DEC_PASSWD
+
+eval "$(ssh-agent -s)"
+chmod 400 ubuntu.pem
+
+ssh-add ubuntu.pem
+
+
+rsync -r PaddlePaddle.org-master/portal/tmp ubuntu@52.76.173.135:/var/content_staging/docs
 
 rm -rf PaddlePaddle.org-master/
 rm -rf master.zip
