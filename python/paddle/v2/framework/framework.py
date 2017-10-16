8e52b34a0c2f14175724571b89814277cbf28d59
Yu Yang <yuyang18@baidu.com>
2017-10-16 16:08:01 -0700

    Implement FC layer with helper (#4726)
    * Implement FC layer with helper
    
    * Update LayerHelper
    
    * Add debug string for Python ProtoBuf
    
    and Rename `Sync` to `Flush`
    
    * Add check of ProtoBuf initialization
    
    * Layer wrapper for FC
    
    * Fix unittest
    
    * Fix CI
    
    * Add code generator
    
    * AttributeChecker Better error log and speicalize bool
    
    Since lots of types can be cast to bool
    
    * Complete mlp, fit_a_line

--- python/paddle/v2/framework/framework.py
+++ python/paddle/v2/framework/framework.py
@@ -153,7 +153,8 @@ class OpProtoHolder(object):
             self.op_proto_map[proto.type] = proto
 
     def get_op_proto(self, type):
-        assert type in self.op_proto_map, "Operator \"%s\" has not been registered." % type
+        if type not in self.op_proto_map:
+            raise ValueError("Operator \"%s\" has not been registered." % type)
         return self.op_proto_map[type]
 
 
@@ -374,10 +375,10 @@ class Program(object):
             cls._instance = cls()
         return cls._instance
 
-    def __init__(self):
-        assert not hasattr(self.__class__,
-                           '_instance'), 'Do not call constructor directly!'
-        self.desc = core.ProgramDesc.instance()
+    def __init__(self, desc=None):
+        if desc is None:
+            desc = core.ProgramDesc.instance()
+        self.desc = desc
         self.blocks = [Block(self, 0)]
         self.current_block_idx = 0
 
@@ -428,7 +429,6 @@ class Parameter(Variable):
             if each < 0:
                 raise ValueError("Parameter shape should not be related with "
                                  "batch-size")
-
         Variable.__init__(self, block, shape=shape, dtype=dtype, **kwargs)
         self.trainable = kwargs.get('trainable', True)
         self.init_attr = kwargs.get('initialize_attr', {
@@ -441,7 +441,7 @@ class Parameter(Variable):
         self._append_initialize_ops_()
 
     def _append_initialize_ops_(self):
-        attr = copy.deepcopy(self.init_attr)
+        attr = self.init_attr
         op_type = attr.pop('type', None)
         block = self.block
         assert isinstance(block, Block)
