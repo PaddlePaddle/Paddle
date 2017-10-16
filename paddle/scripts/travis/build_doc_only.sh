e4300a966f1f698c616f9ae7a6936c1e55c00760
ludaming <ludaming@baidu.com>
2017-10-16 16:28:03 -0700

    put content to corrent position

--- paddle/scripts/travis/build_doc_only.sh
+++ paddle/scripts/travis/build_doc_only.sh
@@ -49,7 +49,7 @@ chmod 400 ubuntu.pem
 ssh-add ubuntu.pem
 
 
-rsync -r PaddlePaddle.org-master/portal/tmp ubuntu@52.76.173.135:/var/content_staging/docs
+rsync -r PaddlePaddle.org-master/portal/tmp/$TRAVIS_BRANCH ubuntu@52.76.173.135:/var/content_staging/docs/$TRAVIS_BRANCH/documentation
 
 rm -rf PaddlePaddle.org-master/
 rm -rf master.zip
