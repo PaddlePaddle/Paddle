164898277c3f274e6d57c4baceb00fe62a8b769c
Markus Kliegl <markus@coniri.com>
2017-10-17 19:29:09 -0700

    MatMul operator (#4856)
    * initial matmul operator
    
    Similar to np.matmul, but also has transpose_X and transpose_Y flags,
    and only supports tensors from rank 1 to 3 inclusive.
    
    For GPU, uses cublas?gemmStridedBatched. For CPU, uses
    cblas_?gemm_batch if available via MKL; otherwise a simple serial
    implementation that loops over the batch dimension is employed for now.

--- paddle/operators/math/math_function.h
+++ paddle/operators/math/math_function.h
@@ -63,7 +63,7 @@ namespace math {
 
 // Support continuous memory now
 // If transA = N, and transB = N
-// Then matrixA: M * K, matrixB: K * N matrixC : M * N
+// Then matrixA: M * K, matrixB: K * N, matrixC : M * N
 // For more detailed info, please refer to
 // http://www.netlib.org/lapack/explore-html/d4/de2/sgemm_8f.html
 template <typename Place, typename T>
@@ -84,6 +84,14 @@ void matmul(const platform::DeviceContext& context,
             const framework::Tensor& matrix_a, bool trans_a,
             const framework::Tensor& matrix_b, bool trans_b, T alpha,
             framework::Tensor* matrix_out, T beta);
+
+// Batched gemm
+template <typename Place, typename T>
+void batched_gemm(const platform::DeviceContext& context,
+                  const CBLAS_TRANSPOSE transA, const CBLAS_TRANSPOSE transB,
+                  const int M, const int N, const int K, const T alpha,
+                  const T* A, const T* B, const T beta, T* C,
+                  const int batchCount, const int strideA, const int strideB);
 
 template <typename Place, typename T>
 struct SetConstant {
