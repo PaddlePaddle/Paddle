3db52783012d20d5174e39ed4ae419179614a1d0
Yu Yang <yuyang18@baidu.com>
2017-10-19 10:08:12 -0700

    Feature/py executor test (#4922)
    * Implement FC layer with helper
    
    * Update LayerHelper
    
    * Add debug string for Python ProtoBuf
    
    and Rename `Sync` to `Flush`
    
    * Add check of ProtoBuf initialization
    
    * Layer wrapper for FC
    
    * Fix unittest
    
    * Fix CI
    
    * Add code generator
    
    * AttributeChecker Better error log and speicalize bool
    
    Since lots of types can be cast to bool
    
    * Complete mlp, fit_a_line
    
    * Expose get global scope
    
    * Make global scope not thread-safe
    
    1. It is no need to make global scope thread-safe, since it will be
    invoked in Python main thread.
    2. Do not free the global scope when C++ exit. Let the OS free memories,
    otherwise, we need to handle the destroy dependencies.
    
    See
    https://google.github.io/styleguide/cppguide.html#Static_and_Global_Variables
    
    * Fix
    
    * Implementation of simple conv_2d layer
    
    * Stash
    
    * Remove private data members in OpRegister
    
    * Fix bugs
    
    * Stash
    
    * Expose FeedFetchList as VarType
    
    * Change ProgramDesc not a global variable
    
    * Polish code style
    
    * Stash
    
    * Correct implement BlockDesc destructor
    
    * Correct implement BlockDesc destructor
    
    * Unify program as parameter name
    
    * Fix bugs
    
    * Add unittest
    
    * Fix unit test error
    
    * Remove unused functions
    
    * Add clone for Python Program
    
    * Working on executor
    
    * Stash
    
    * Add glog as dependencies of ops
    
    * Use VLOG to logging some information is helpful when we debug Paddle
    
    * Expose VarDesc::persistable to Python
    
    * Test executor
    
    * Complete unittest
    
    * Polish code
    
    * Fix merge error
    
    * Follow comment
    
    * Polish Python Code

--- python/paddle/v2/framework/framework.py
+++ python/paddle/v2/framework/framework.py
@@ -256,7 +256,8 @@ class Operator(object):
                     self.desc.set_block_attr(attr_name, attrs[attr_name].desc)
 
         self.desc.check_attrs()
-        self.desc.infer_shape(self.block.desc)
+        if type not in {'feed', 'fetch'}:
+            self.desc.infer_shape(self.block.desc)
 
     def __str__(self):
         protostr = self.desc.serialize_to_string()
@@ -323,9 +324,12 @@ class Block(object):
         return self.desc.id
 
     def var(self, name):
-        if name not in self.vars:
+        if not isinstance(name, basestring):
+            raise TypeError()
+        v = self.vars.get(name, None)
+        if v is None:
             raise ValueError("var %s not in this block" % name)
-        return self.vars[name]
+        return v
 
     def all_parameters(self):
         return {v for k, v in self.vars.iteritems() if isinstance(v, Parameter)}
--- python/paddle/v2/framework/layers.py
+++ python/paddle/v2/framework/layers.py
@@ -55,9 +55,11 @@ def data(name,
          shape,
          data_type='float32',
          type=core.VarDesc.VarType.LOD_TENSOR,
+         append_batch_size=True,
          program=None):
     helper = LayerHelper('data', **locals())
-    shape = [-1] + shape  # append batch size as -1
+    if append_batch_size:
+        shape = [-1] + shape  # append batch size as -1
     return helper.create_global_variable(
         name=name, shape=shape, dtype=data_type, type=type)
 
@@ -112,6 +114,7 @@ def _create_op_func_(op_type):
 
 
 _create_op_func_('mean')
+_create_op_func_('mul')
 _create_op_func_('pool2d')
 
 
