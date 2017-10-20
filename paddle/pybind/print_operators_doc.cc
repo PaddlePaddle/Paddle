de89bf21873fd8013b8de7f17eca318c5e28481f
Yi Wang <yi.wang.2005@gmail.com>
2017-10-20 13:44:22 -0700

    Update the print_operators_doc.cc

--- paddle/pybind/print_operators_doc.cc
+++ paddle/pybind/print_operators_doc.cc
@@ -5,8 +5,75 @@
 #include "paddle/framework/op_registry.h"
 #include "paddle/pybind/pybind.h"
 
+std::string Escape(const std::string& s) {
+  std::string r;
+  for (size_t i = 0; i < s.size(); i++) {
+    switch (s[i]) {
+      case '\"':
+        r += "\\\"";
+        break;
+      case '\\':
+        r += "\\\\";
+        break;
+      case '\n':
+        r += "\\n";
+        break;
+      case '\r':
+        break;
+      default:
+        r += s[i];
+        break;
+    }
+  }
+  return r;
+}
+
+std::string AttrType(paddle::framework::AttrType at) {
+  switch (at) {
+    case paddle::framework::INT:
+      return "int";
+    case paddle::framework::FLOAT:
+      return "float";
+    case paddle::framework::STRING:
+      return "string";
+    case paddle::framework::BOOLEAN:
+      return "bool";
+    case paddle::framework::INTS:
+      return "int array";
+    case paddle::framework::FLOATS:
+      return "float array";
+    case paddle::framework::STRINGS:
+      return "string array";
+    case paddle::framework::BOOLEANS:
+      return "bool array";
+    case paddle::framework::BLOCK:
+      return "block id";
+  }
+  return "UNKNOWN";  // not possible
+}
+
+void PrintVar(const paddle::framework::OpProto::Var& v, bool comma) {
+  std::cout << " { "
+            << "\n"
+            << "   \"name\" : \"" << Escape(v.name()) << "\",\n"
+            << "   \"comment\" : \"" << Escape(v.comment()) << "\",\n"
+            << "   \"duplicable\" : " << v.duplicable() << ",\n"
+            << "   \"intermediate\" : " << v.intermediate() << "\n"
+            << " } " << (comma ? ",\n" : "\n");
+}
+
+void PrintAttr(const paddle::framework::OpProto::Attr& a, bool comma) {
+  std::cout << " { "
+            << "\n"
+            << "   \"name\" : \"" << Escape(a.name()) << "\",\n"
+            << "   \"type\" : \"" << AttrType(a.type()) << "\",\n"
+            << "   \"comment\" : \"" << Escape(a.comment()) << "\",\n"
+            << "   \"generated\" : " << a.generated() << "\n"
+            << " } " << (comma ? ",\n" : "\n");
+}
+
 void PrintOpProto(const std::string& type,
-                  const paddle::framework::OpInfo& opinfo) const {
+                  const paddle::framework::OpInfo& opinfo) {
   std::cerr << "Processing " << type << "\n";
 
   const paddle::framework::OpProto* p = opinfo.proto_;
@@ -45,77 +112,9 @@ void PrintOpProto(const std::string& type,
   std::cout << "},\n";
 }
 
-void PrintVar(const paddle::framework::OpProto::Var& v, bool comma) const {
-  std::cout << " { "
-            << "\n"
-            << "   \"name\" : \"" << Escape(v.name()) << "\",\n"
-            << "   \"comment\" : \"" << Escape(v.comment()) << "\",\n"
-            << "   \"duplicable\" : " << v.duplicable() << ",\n"
-            << "   \"intermediate\" : " << v.intermediate() << "\n"
-            << " } " << (comma ? ",\n" : "\n");
-}
-
-void PrintAttr(const paddle::framework::OpProto::Attr& a, bool comma) const {
-  std::cout << " { "
-            << "\n"
-            << "   \"name\" : \"" << Escape(a.name()) << "\",\n"
-            << "   \"type\" : \"" << AttrType(a.type()) << "\",\n"
-            << "   \"comment\" : \"" << Escape(a.comment()) << "\",\n"
-            << "   \"generated\" : " << a.generated() << "\n"
-            << " } " << (comma ? ",\n" : "\n");
-}
-
-std::string AttrType(paddle::framework::AttrType at) const {
-  switch (at) {
-    case paddle::framework::INT:
-      return "int";
-    case paddle::framework::FLOAT:
-      return "float";
-    case paddle::framework::STRING:
-      return "string";
-    case paddle::framework::BOOLEAN:
-      return "bool";
-    case paddle::framework::INTS:
-      return "int array";
-    case paddle::framework::FLOATS:
-      return "float array";
-    case paddle::framework::STRINGS:
-      return "string array";
-    case paddle::framework::BOOLEANS:
-      return "bool array";
-    case paddle::framework::BLOCK:
-      return "block id";
-  }
-  return "UNKNOWN";  // not possible
-}
-
-std::string Escape(const std::string& s) const {
-  std::string r;
-  for (size_t i = 0; i < s.size(); i++) {
-    switch (s[i]) {
-      case '\"':
-        r += "\\\"";
-        break;
-      case '\\':
-        r += "\\\\";
-        break;
-      case '\n':
-        r += "\\n";
-        break;
-      case '\r':
-        break;
-      default:
-        r += s[i];
-        break;
-    }
-  }
-  return r;
-}
-
 int main() {
-  OpInfoIterator iter;
   std::cout << "[\n";
-  for (auto iter : OpInfoMap::Instance().map()) {
+  for (auto iter : paddle::framework::OpInfoMap::Instance().map()) {
     PrintOpProto(iter.first, iter.second);
   }
   std::cout << "]\n";
