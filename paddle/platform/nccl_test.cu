7bbbd36f84ddc114a1a178178a46389d173e3a2a
Yu Yang <yuyang18@baidu.com>
2017-10-22 16:37:47 -0700

    Add nccl test

--- paddle/platform/nccl_test.cu
+++ paddle/platform/nccl_test.cu
@@ -14,9 +14,13 @@
 
 #include "glog/logging.h"
 #include "gtest/gtest.h"
+#include "paddle/platform/device_context.h"
 #include "paddle/platform/dynload/nccl.h"
+#include "paddle/platform/enforce.h"
 #include "paddle/platform/gpu_info.h"
 
+#include <thrust/device_vector.h>
+#include <memory>
 #include <vector>
 
 static int dev_count = 0;
@@ -28,10 +32,95 @@ TEST(NCCL, init) {
   std::vector<ncclComm_t> comms;
   comms.resize(dev_count);
 
-  auto status = dynload::ncclCommInitAll(comms, nGPUs);
+  auto status = dynload::ncclCommInitAll(comms.data(), dev_count, nullptr);
   PADDLE_ENFORCE(status);
   for (int i = 0; i < dev_count; ++i) {
-    PADDLE_ENFORCE(ncclCommDestroy(comms[i]));
+    dynload::ncclCommDestroy(comms[i]);
+  }
+}
+
+template <typename T>
+struct PerThreadData {
+  thrust::device_vector<T> send_buff;
+  thrust::device_vector<T> recv_buff;
+  CUDADeviceContext dev_ctx;
+
+  T* SendBuff() { return thrust::raw_pointer_cast(send_buff.data()); }
+
+  T* RecvBuff() { return thrust::raw_pointer_cast(recv_buff.data()); }
+
+  PerThreadData(int gpu_id, size_t size) : dev_ctx(GPUPlace(gpu_id)) {
+    send_buff.resize(size);
+    for (size_t i = 0; i < size; ++i) {
+      send_buff[i] = static_cast<T>(i);
+    }
+    recv_buff.resize(size);
+  }
+};
+
+static constexpr int ELEM_COUNT = 10000;
+
+TEST(NCCL, all_reduce) {
+  std::vector<ncclComm_t> comms;
+  comms.resize(dev_count);
+  VLOG(1) << "Initializing ncclComm";
+  auto status = dynload::ncclCommInitAll(comms.data(), dev_count, nullptr);
+  PADDLE_ENFORCE(status);
+  VLOG(1) << "ncclComm initialized";
+  VLOG(1) << "Creating thread data";
+  std::vector<std::unique_ptr<PerThreadData<double>>> data;
+  data.reserve(dev_count);
+  for (int i = 0; i < dev_count; ++i) {
+    VLOG(1) << "Creating thread data for device " << i;
+    SetDeviceId(i);
+    data.emplace_back(new PerThreadData<double>(i, ELEM_COUNT));
+  }
+  VLOG(1) << "Thread data created";
+
+  VLOG(1) << "Check send_buf data";
+  for (int i = 0; i < dev_count; ++i) {
+    VLOG(1) << "Check on device " << i;
+    SetDeviceId(i);
+    thrust::host_vector<double> tmp = data[i]->send_buff;
+    for (size_t j = 0; j < tmp.size(); ++j) {
+      ASSERT_NEAR(static_cast<double>(j), tmp[j], 1e-5);
+    }
+  }
+
+  VLOG(1) << "Invoking ncclAllReduce";
+
+  for (int i = 0; i < dev_count; ++i) {
+    VLOG(1) << "Invoking ncclAllReduce with device " << i;
+    SetDeviceId(i);
+    PADDLE_ENFORCE(dynload::ncclAllReduce(
+        data[i]->SendBuff(), data[i]->RecvBuff(), ELEM_COUNT, ncclDouble,
+        ncclSum, comms[i], data[i]->dev_ctx.stream()));
+    VLOG(1) << "Invoked ncclAllReduce for device " << i;
+  }
+
+  VLOG(1) << "Invoked ncclAllReduce";
+
+  VLOG(1) << "Sync devices";
+  for (int i = 0; i < dev_count; ++i) {
+    VLOG(1) << "Sync device " << i;
+    SetDeviceId(i);
+    data[i]->dev_ctx.Wait();
+  }
+  VLOG(1) << "device synced";
+
+  for (int i = 0; i < dev_count; ++i) {
+    SetDeviceId(i);
+    VLOG(1) << "Checking vector on device " << i;
+    thrust::host_vector<double> tmp = data[i]->recv_buff;
+    for (size_t j = 0; j < tmp.size(); ++j) {
+      auto elem = static_cast<double>(j);
+      elem *= dev_count;
+      ASSERT_NEAR(tmp[j], elem, 1e-4);
+    }
+  }
+
+  for (int i = 0; i < dev_count; ++i) {
+    dynload::ncclCommDestroy(comms[i]);
   }
 }
 }
@@ -40,8 +129,9 @@ TEST(NCCL, init) {
 int main(int argc, char** argv) {
   dev_count = paddle::platform::GetCUDADeviceCount();
   if (dev_count <= 1) {
-    LOG(WARN) << "Cannot test multi-gpu nccl, because the CUDA device count is "
-              << dev_count;
+    LOG(WARNING)
+        << "Cannot test multi-gpu nccl, because the CUDA device count is "
+        << dev_count;
     return 0;
   }
   testing::InitGoogleTest(&argc, argv);
