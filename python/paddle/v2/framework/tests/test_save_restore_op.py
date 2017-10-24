4bc80d94cb2d31861125a13c66dec2e684ea1232
fengjiayi <fengjiayi@baidu.com>
2017-10-23 18:31:19 -0700

    Rewrite save_restore_op_test with new Python framework

--- python/paddle/v2/framework/tests/test_save_restore_op.py
+++ python/paddle/v2/framework/tests/test_save_restore_op.py
@@ -1,87 +1,70 @@
-from paddle.v2.framework import core
-from paddle.v2.framework.op import Operator
+import paddle.v2.framework.core as core
+import paddle.v2.framework.framework as framework
+import paddle.v2.framework.executor as executor
+
 import numpy as np
-from op_test import OpTest, create_op
 import unittest
-import os, sys
+import os
+import sys
+import shutil
 
-ABSOLUTE_PATH = "/tmp/MODEL"  # directory for save parameter files.
-
-scope = core.Scope()
-place = core.CPUPlace()
-dev_ctx = core.DeviceContext.create(place)
-
-# Saver is a demo for saving arbitrary tensor.
-# TODO(dzh): Saver also need to save ProgramDesc. Should be done after
-# python API done.
+FOLDER_PATH = "./tmp_test_dir"
 
 
-def Saver(var_list=None):
-    net = core.Net.create()
-    save_tensors = []
-    save_op = Operator("save", X=save_tensors, absolutePath=ABSOLUTE_PATH)
-    net.append_op(save_op)
-    net.run(scope, dev_ctx)
+class TestSaveRestoreOp(unittest.TestCase):
+    def test_save_restore_op(self):
+        tensor_1_val = np.random.rand(3, 9).astype("float32")
+        tensor_2_val = np.random.rand(4, 2).astype("float32")
+        place = core.CPUPlace()
 
+        program = framework.Program()
+        block = program.global_block()
+        v_a = block.create_var(
+            dtype="float32", shape=[3, 9], lod_level=0, name="tensor_1")
+        v_b = block.create_var(
+            dtype="float32", shape=[4, 2], lod_level=0, name="tensor_2")
 
-class TestSaver(unittest.TestCase):
-    def test_save_tensors(self):
-        a = scope.var("a")
-        b = scope.var("b")
-        Saver(["a", "b"])
-        self.assertTrue(os.path.exists(ABSOLUTE_PATH))
+        t_1 = core.LoDTensor()
+        t_1.set(tensor_1_val, place)
+        t_2 = core.LoDTensor()
+        t_2.set(tensor_2_val, place)
+        block.append_op(
+            type="save",
+            inputs={"X": [v_a, v_b]},
+            attrs={"folderPath": FOLDER_PATH})
+        block.append_op(
+            type="fill_constant",
+            outputs={"Out": [v_a]},
+            attrs={"shape": [2, 2],
+                   "value": 0.0})
+        block.append_op(
+            type="fill_constant",
+            outputs={"Out": [v_b]},
+            attrs={"shape": [2, 2],
+                   "value": 0.0})
+        block.append_op(
+            type="restore",
+            outputs={"Out": [v_a, v_b]},
+            attrs={"folderPath": FOLDER_PATH})
 
+        if os.path.exists(FOLDER_PATH):
+            shutil.rmtree(FOLDER_PATH)
+        os.makedirs(FOLDER_PATH)
 
-class TestSaveOp(OpTest):
-    def setUp(self):
-        self.op_type = "save"
-        x0 = np.ones((1, 1)).astype("float32")
-        x1 = np.ones((1, 1)).astype("float32")
-        x2 = np.ones((2, 1)).astype("float32")
+        exe = executor.Executor(place)
+        out = exe.run(program,
+                      feed={"tensor_1": t_1,
+                            "tensor_2": t_2},
+                      fetch_list=[v_a, v_b])
 
-        self.inputs = {"X": [("x0", x0), ("x1", x1), ("x2", x2)], }
+        self.assertTrue(os.path.isdir(FOLDER_PATH))
+        self.assertTrue(os.path.isfile(FOLDER_PATH + "/__tensor_1__"))
+        self.assertTrue(os.path.isfile(FOLDER_PATH + "/__tensor_2__"))
 
-        self.attrs = {"absolutePath": ABSOLUTE_PATH}
+        self.assertTrue(np.array_equal(np.array(out[0]), tensor_1_val))
+        self.assertTrue(np.array_equal(np.array(out[1]), tensor_2_val))
 
-    def test_check_output(self):
-        if os.path.exists(ABSOLUTE_PATH):
-            try:
-                if os.path.isdir(ABSOLUTE_PATH):
-                    os.rmdir(ABSOLUTE_PATH)
-                elif os.path.isfile(ABSOLUTE_PATH):
-                    os.remove(ABSOLUTE_PATH)
-            except OSError:
-                pass
-        self.check_output()
-        self.assertTrue(os.path.exists(ABSOLUTE_PATH))
-
-
-# must run saveTest first
-class TestRestoreOp(OpTest):
-    def setUp(self):
-        self.op_type = "restore"
-
-        src_x0 = np.ones((2, 3)).astype("float32")
-        src_x1 = np.ones((1, 2)).astype("float32")
-        src_x2 = np.ones((2, 1)).astype("float32")
-
-        x0 = scope.var('x0').get_tensor()
-        x1 = scope.var('x1').get_tensor()
-        x2 = scope.var('x2').get_tensor()
-
-        self.src_tensors = [src_x0, src_x1, src_x2]
-        self.inputs = {}
-        self.outputs = {"Out": [("x0", x0), ("x1", x1), ("x2", x2)]}
-
-        self.attrs = {"absolutePath": ABSOLUTE_PATH}
-
-    def test_check_output(self):
-        self.check_output()
-        self.dst_tensors = [
-            scope.find_var("x0"), scope.find_var("x1"), scope.find_var("x2")
-        ]
-        for src, dst in zip(self.save_tensors, self.dst_tensors):
-            self.assertTrue(np.array_equal(src, dst))
+        shutil.rmtree(FOLDER_PATH)
 
 
 if __name__ == "__main__":
