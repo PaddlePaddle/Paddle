74a3d1931baba1d8caeed87a9b772ce8e7417797
Liu Yiqun <liuyiqun01@baidu.com>
2017-10-25 09:31:31 +0000

    Refine the comments of warpctc_op.

--- paddle/operators/warpctc_op.cc
+++ paddle/operators/warpctc_op.cc
@@ -50,18 +50,52 @@ class WarpCTCOpMaker : public framework::OpProtoAndCheckerMaker {
   WarpCTCOpMaker(framework::OpProto* proto,
                  framework::OpAttrChecker* op_checker)
       : OpProtoAndCheckerMaker(proto, op_checker) {
-    AddInput("Logits", "(LodTensor)");
-    AddInput("Label", "(LodTensor)");
-    AddOutput("WarpCTCGrad", "(Tensor)").AsIntermediate();
-    AddOutput("Loss", "(Tensor)");
-    AddAttr<int>("blank", "").SetDefault(0);
-    AddAttr<bool>("normByTimes", "").SetDefault(false);
+    AddInput("Logits",
+             "(LodTensor, default: LoDTensor<float>), the unscaled "
+             "probabilities of variable-length sequences, which is a 2-D "
+             "Tensor with LoD information. It is of the shape [Lp, K], where "
+             "Lp is the sum of all input sequences' length and K is the number "
+             "of classes.");
+    AddInput("Label",
+             "(LodTensor, default: LoDTensor<int>), the ground truth "
+             "of variable-length sequence, which is a 2-D Tensor with LoD "
+             "information. It is of the shape [Lg, 1], where Lg is th sum of "
+             "all labels' length.");
+    AddOutput("WarpCTCGrad",
+              "(Tensor, default: Tensor<float>), a temporary "
+              "output Tensor to store the gradients of warp-ctc, which is "
+              "computed with loss together in one call. It is a 3-D Tensor "
+              "of the shape [max_sequence_length, batch_size, num_classes].")
+        .AsIntermediate();
+    AddOutput("Loss",
+              "(Tensor, default: Tensor<float>), the Connectionist "
+              "Temporal Classification (CTC) loss, which is a 2-D Tensor of "
+              "the shape [batch_size, 1]");
+    AddAttr<int>("blank",
+                 "(int, default: 0), the blank label of Connectionist "
+                 "Temporal Classification (CTC) loss, which is in the "
+                 "half-opened interval [0, num_classes), "
+                 "where the true number of classes is num_classes - 1.")
+        .SetDefault(0);
+    AddAttr<bool>("normByTimes",
+                  "(bool, default: false), whether to "
+                  "normalize the gradients by the number of time-step, "
+                  "which is also the sequence's length.")
+        .SetDefault(false);
     AddComment(R"DOC(
-An operator integrating the open-source warp-ctc library
-<https://github.com/baidu-research/warp-ctc> to compute connectionist
-temporal classification cost.
-It can be aliased as softmax_with_ctc, since a softmax is computed to
-normlize values for each row of the input tensor, following with a ctc loss.
+An operator integrating the open-source
+[warp-ctc](https://github.com/baidu-research/warp-ctc) library, which is used in
+[Deep Speech 2: End-toEnd Speech Recognition in English and Mandarin](
+https://arxiv.org/pdf/1512.02595v1.pdf),
+to compute Connectionist Temporal Classification (CTC) loss.
+It can be aliased as softmax with ctc, since a native softmax activation is
+interated to the warp-ctc library, to to normlize values for each row of the
+input tensor.
+
+More detail of CTC loss can be found by refering to
+[Connectionist Temporal Classification: Labelling Unsegmented Sequence Data with
+Recurrent Neural Networks](
+http://machinelearning.wustl.edu/mlpapers/paper_files/icml2006_GravesFGS06.pdf).
 )DOC");
   }
 };
