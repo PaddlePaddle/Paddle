ce6b0167baeaf48bac4d8c1d8269ba3fcdfdd90d
fengjiayi <fengjiayi@baidu.com>
2017-10-25 15:55:52 -0700

    Follow comments

--- python/paddle/v2/framework/save_load.py
+++ python/paddle/v2/framework/save_load.py
@@ -10,30 +10,29 @@ import cStringIO
 import os
 
 
-def _dump_all_persistable_vars_(folder_path, program=None, exempt_list=None):
+def _save_all_persistable_vars_(folder_path, program=None):
     if program is None:
         program = framework.g_program
-    dump_program = framework.Program()
-    dump_block = dump_program.global_block()
-    if exempt_list is None:
-        exempt_list = {}
+    save_program = framework.Program()
+    save_block = save_program.global_block()
 
     save_op_inputs = []
     for var in program.global_block().vars.itervalues():
-        if var.desc.persistable() and not var.name in exempt_list:
-            v = dump_block.create_var(
+        if var.desc.persistable(
+        ) and var.type == core.VarDesc.VarType.LOD_TENSOR:
+            v = save_block.create_var(
                 name=var.name, dtype=var.data_type, persistable=True)
-            save_op_inputs.append(var)
-    dump_block.append_op(
+            save_op_inputs.append(v)
+    save_block.append_op(
         type="save",
         inputs={"X": save_op_inputs},
         attrs={"folderPath": folder_path})
 
     exe = executor.Executor(core.CPUPlace())
-    exe.run(dump_program, feed={}, fetch_list=[])
+    exe.run(save_program, feed={}, fetch_list=[])
 
 
-def dump_inference_model(folder_path,
+def save_inference_model(folder_path,
                          feeded_var_names,
                          target_vars,
                          program=None):
@@ -53,38 +52,24 @@ def dump_inference_model(folder_path,
     for var in target_vars:
         var.op.mark_as_target()
 
-    dump_list = [(program, "__program_desc__", "cpp"),
-                 (feed_order, "__feed_order__", "python"),
-                 (fetch_order, "__fetch_order__", "python")]
-    model_file_name = folder_path + "/model.tar"
+    model_file_name = folder_path + "/__model__"
     with open(model_file_name, "w") as f:
-        tar = tarfile.TarFile(fileobj=f, mode="w")
-        for to_dump in dump_list:
-            if to_dump[2] == "cpp":
-                binary_str = to_dump[0].desc.serialize_to_string()
-            elif to_dump[2] == "python":
-                binary_str = pickle.dumps(to_dump[0])
-            else:
-                raise ValueError("Unknown dump type: '%s'", to_dump[2])
-            buf = cStringIO.StringIO()
-            buf.write(binary_str)
-            tarinfo = tarfile.TarInfo(name=to_dump[1])
-            buf.seek(0)
-            tarinfo.size = len(buf.getvalue())
-            tar.addfile(tarinfo=tarinfo, fileobj=buf)
+        pickle.dump({
+            "program_desc_str": program.desc.serialize_to_string(),
+            "feed_order": feed_order,
+            "fetch_order": fetch_order
+        }, f, -1)
 
     # Build another program to hold save_ops
-    _dump_all_persistable_vars_(
-        folder_path=folder_path, program=program,
-        exempt_list={"feed", "fetch"})
+    _save_all_persistable_vars_(folder_path=folder_path, program=program)
 
 
-def dump_checkpoint(folder_path, program=None):
+def save_checkpoint(folder_path, program=None):
     if program is None:
         program = framework.g_program
     if not os.path.isdir(folder_path):
         os.makedirs(folder_path)
-    _dump_all_persistable_vars_(folder_path=folder_path, program=program)
+    _save_all_persistable_vars_(folder_path=folder_path, program=program)
 
 
 def load_inference_model(folder_path):
