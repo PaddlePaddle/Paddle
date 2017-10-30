94526a24b550d9df4c4111254b12ae68102e20dd
fengjiayi <fengjiayi@baidu.com>
2017-10-30 15:17:21 -0700

    init

---
+++ python/paddle/v2/framework/tests/test_understand_sentiment_conv.py
@@ -0,0 +1,14 @@
+import paddle.v2 as paddle
+import paddle.v2.framework.layers as layers
+import paddle.v2.framework.nets as nets
+import paddle.v2.framework.core as core
+import paddle.v2.framework.optimizer as optimizer
+
+from paddle.v2.framework.framework import Program, g_program
+from paddle.v2.framework.executor import Executor
+
+import numpy as np
+
+
+def convolution_net(input_dim, class_dim=2, emb_dim=128, hid_dim=128):
+    pass
