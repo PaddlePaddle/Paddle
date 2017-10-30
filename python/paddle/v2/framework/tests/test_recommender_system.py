394ffb31f4c02bfa9d7c4687a9dbdbed73bbad8f
Dong Zhihong <dzhwinter@gmail.com>
2017-10-30 16:30:09 -0700

    "debug embedding table"

--- python/paddle/v2/framework/tests/test_recommender_system.py
+++ python/paddle/v2/framework/tests/test_recommender_system.py
@@ -11,6 +11,8 @@ import numpy as np
 
 init_program = Program()
 program = Program()
+is_sparse = True
+BATCH_SIZE = 256
 
 
 def get_usr_combined_features():
@@ -30,83 +32,85 @@ def get_usr_combined_features():
         input=uid,
         size=[USR_DICT_SIZE, 32],
         param_attr={'name': 'user_table'},
+        is_sparse=is_sparse,
         program=program,
         init_program=init_program)
 
-    usr_fc = layers.fc(input=usr_emb,
-                       size=32,
-                       program=program,
-                       init_program=init_program)
+    # usr_fc = layers.fc(input=usr_emb,
+    #                    size=32,
+    #                    program=program,
+    #                    init_program=init_program)
 
-    USR_GENDER_DICT_SIZE = 2
+    # USR_GENDER_DICT_SIZE = 2
 
-    usr_gender_id = layers.data(
-        name='gender_id',
-        shape=[1],
-        data_type='int32',
-        program=program,
-        init_program=init_program)
+    # usr_gender_id = layers.data(
+    #     name='gender_id',
+    #     shape=[1],
+    #     data_type='int32',
+    #     program=program,
+    #     init_program=init_program)
 
-    usr_gender_emb = layers.embedding(
-        input=usr_gender_id,
-        size=[USR_GENDER_DICT_SIZE, 16],
-        param_attr={'name': 'gender_table'},
-        program=program,
-        init_program=init_program)
+    # usr_gender_emb = layers.embedding(
+    #     input=usr_gender_id,
+    #     size=[USR_GENDER_DICT_SIZE, 16],
+    #     param_attr={'name': 'gender_table'},
+    #     program=program,
+    #     init_program=init_program)
 
-    usr_gender_fc = layers.fc(input=usr_gender_emb,
-                              size=16,
-                              program=program,
-                              init_program=init_program)
+    # usr_gender_fc = layers.fc(input=usr_gender_emb,
+    #                           size=16,
+    #                           program=program,
+    #                           init_program=init_program)
 
-    USR_AGE_DICT_SIZE = len(paddle.dataset.movielens.age_table)
-    usr_age_id = layers.data(
-        name='age_id',
-        shape=[1],
-        data_type="int32",
-        program=program,
-        init_program=init_program)
+    # USR_AGE_DICT_SIZE = len(paddle.dataset.movielens.age_table)
+    # usr_age_id = layers.data(
+    #     name='age_id',
+    #     shape=[1],
+    #     data_type="int32",
+    #     program=program,
+    #     init_program=init_program)
 
-    usr_age_emb = layers.embedding(
-        input=usr_age_id,
-        size=[USR_AGE_DICT_SIZE, 16],
-        param_attr={'name': 'age_table'},
-        program=program,
-        init_program=init_program)
+    # usr_age_emb = layers.embedding(
+    #     input=usr_age_id,
+    #     size=[USR_AGE_DICT_SIZE, 16],
+    #     param_attr={'name': 'age_table'},
+    #     program=program,
+    #     init_program=init_program)
 
-    usr_age_fc = layers.fc(input=usr_age_emb,
-                           size=16,
-                           program=program,
-                           init_program=init_program)
+    # usr_age_fc = layers.fc(input=usr_age_emb,
+    #                        size=16,
+    #                        program=program,
+    #                        init_program=init_program)
 
-    USR_JOB_DICT_SIZE = paddle.dataset.movielens.max_job_id() + 1
-    usr_job_id = layers.data(
-        name='job_id',
-        shape=[1],
-        data_type="int32",
-        program=program,
-        init_program=init_program)
+    # USR_JOB_DICT_SIZE = paddle.dataset.movielens.max_job_id() + 1
+    # usr_job_id = layers.data(
+    #     name='job_id',
+    #     shape=[1],
+    #     data_type="int32",
+    #     program=program,
+    #     init_program=init_program)
 
-    usr_job_emb = layers.embedding(
-        input=usr_job_id,
-        size=[USR_JOB_DICT_SIZE, 16],
-        param_attr={'name': 'job_table'},
-        program=program,
-        init_program=init_program)
+    # usr_job_emb = layers.embedding(
+    #     input=usr_job_id,
+    #     size=[USR_JOB_DICT_SIZE, 16],
+    #     param_attr={'name': 'job_table'},
+    #     program=program,
+    #     init_program=init_program)
 
-    usr_job_fc = layers.fc(input=usr_job_emb,
-                           size=16,
-                           program=program,
-                           init_program=init_program)
+    # usr_job_fc = layers.fc(input=usr_job_emb,
+    #                        size=16,
+    #                        program=program,
+    #                        init_program=init_program)
 
-    concat_embed = layers.concat(
-        input=[usr_fc, usr_gender_fc, usr_age_fc, usr_job_fc],
-        axis=1,
-        program=program,
-        init_program=init_program)
+    # concat_embed = layers.concat(
+    #     input=[usr_fc, usr_gender_fc, usr_age_fc, usr_job_fc],
+    #     axis=1,
+    #     program=program,
+    #     init_program=init_program)
 
     # FIXME(dzh) : need tanh operator
-    usr_combined_features = layers.fc(input=concat_embed,
+    # usr_combined_features = layers.fc(input=concat_embed,
+    usr_combined_features = layers.fc(input=usr_emb,
                                       size=200,
                                       act="tanh",
                                       program=program,
@@ -133,67 +137,68 @@ def get_mov_combined_features():
         program=program,
         init_program=init_program)
 
-    mov_fc = layers.fc(input=mov_emb,
-                       size=32,
-                       program=program,
-                       init_program=init_program)
+    # mov_fc = layers.fc(input=mov_emb,
+    #                    size=32,
+    #                    program=program,
+    #                    init_program=init_program)
 
-    CATEGORY_DICT_SIZE = len(paddle.dataset.movielens.movie_categories())
+    # CATEGORY_DICT_SIZE = len(paddle.dataset.movielens.movie_categories())
 
-    category_id = layers.data(
-        name='category_id',
-        shape=[1],
-        data_type='int32',
-        program=program,
-        init_program=init_program)
+    # category_id = layers.data(
+    #     name='category_id',
+    #     shape=[1],
+    #     data_type='int32',
+    #     program=program,
+    #     init_program=init_program)
 
-    mov_categories_emb = layers.embedding(
-        input=category_id,
-        size=[CATEGORY_DICT_SIZE, 32],
-        program=program,
-        init_program=init_program)
+    # mov_categories_emb = layers.embedding(
+    #     input=category_id,
+    #     size=[CATEGORY_DICT_SIZE, 32],
+    #     program=program,
+    #     init_program=init_program)
 
-    mov_categories_hidden = layers.sequence_pool(
-        input=mov_categories_emb,
-        pool_size=None,
-        pool_type="sum",
-        program=program,
-        init_program=init_program)
+    # mov_categories_hidden = layers.sequence_pool(
+    #     input=mov_categories_emb,
+    #     pool_size=None,
+    #     pool_type="sum",
+    #     program=program,
+    #     init_program=init_program)
 
-    MOV_TITLE_DICT_SIZE = len(paddle.dataset.movielens.get_movie_title_dict())
+    # MOV_TITLE_DICT_SIZE = len(paddle.dataset.movielens.get_movie_title_dict())
 
-    mov_title_id = layers.data(
-        name='movie_title',
-        shape=[1],
-        data_type='int32',
-        program=program,
-        init_program=init_program)
+    # mov_title_id = layers.data(
+    #     name='movie_title',
+    #     shape=[1],
+    #     data_type='int32',
+    #     program=program,
+    #     init_program=init_program)
 
-    mov_title_emb = layers.embedding(
-        input=mov_title_id,
-        size=[MOV_TITLE_DICT_SIZE, 32],
-        program=program,
-        init_program=init_program)
+    # mov_title_emb = layers.embedding(
+    #     input=mov_title_id,
+    #     size=[MOV_TITLE_DICT_SIZE, 32],
+    #     program=program,
+    #     init_program=init_program)
 
-    mov_title_conv = nets.sequence_conv_pool(
-        input=mov_title_emb,
-        num_filters=32,
-        filter_size=3,
-        pool_type="sum",
-        pool_size=3,
-        pool_stride=1,
-        act="tanh",
-        program=program,
-        init_program=init_program)
+    # mov_title_conv = nets.sequence_conv_pool(
+    #     input=mov_title_emb,
+    #     num_filters=32,
+    #     filter_size=3,
+    #     pool_type="sum",
+    #     pool_size=3,
+    #     pool_stride=1,
+    #     act="tanh",
+    #     program=program,
+    #     init_program=init_program)
 
-    concat_embed = layers.concat(
-        input=[mov_fc, mov_categories_hidden, mov_title_conv],
-        axis=1,
-        program=program,
-        init_program=init_program)
+    # concat_embed = layers.concat(
+    #     input=[mov_fc, mov_categories_hidden, mov_title_conv],
+    #     axis=1,
+    #     program=program,
+    #     init_program=init_program)
 
     # FIXME(dzh) : need tanh operator
-    mov_combined_features = layers.fc(input=concat_embed,
+    # mov_combined_features = layers.fc(input=concat_embed,
+    mov_combined_features = layers.fc(input=mov_emb,
                                       size=200,
                                       act="tanh",
                                       program=program,
@@ -216,7 +221,7 @@ def model():
     label = layers.data(
         name='score',
         shape=[1],
-        data_type='int32',
+        data_type='float32',
         program=program,
         init_program=init_program)
 
@@ -241,16 +246,16 @@ def main():
     train_reader = paddle.batch(
         paddle.reader.shuffle(
             paddle.dataset.movielens.train(), buf_size=8192),
-        batch_size=256)
+        batch_size=BATCH_SIZE)
 
     feeding = {
         'user_id': 0,
-        'gender_id': 1,
-        'age_id': 2,
-        'job_id': 3,
+        # 'gender_id': 1,
+        # 'age_id': 2,
+        # 'job_id': 3,
         'movie_id': 4,
-        'category_id': 5,
-        'movie_title': 6,
+        # 'category_id': 5,
+        # 'movie_title': 6,
         'score': 7
     }
 
@@ -259,8 +264,14 @@ def main():
         for (key, idx) in feeding.iteritems():
             tensor = core.LoDTensor()
             if key != "category_id" and key != "movie_title":
-                numpy_data = np.array(map(lambda x: x[idx], data)).astype(
-                    "int32")
+                if key == "score":
+                    numpy_data = np.array(map(lambda x: x[idx], data)).astype(
+                        "float32")
+                else:
+                    numpy_data = np.array(map(lambda x: x[idx], data)).astype(
+                        "int32")
+                numpy_data = numpy_data.reshape([BATCH_SIZE, 1])
+                print key, numpy_data.shape
             else:
                 numpy_data = map(lambda x: np.array(x[idx]).astype("int32"),
                                  data)
@@ -280,11 +291,14 @@ def main():
     PASS_NUM = 100
     for pass_id in range(PASS_NUM):
         for data in train_reader():
-            print data
-            print func_feed(feeding, data)
+            # print func_feed(feeding, data)
+            # print init_program
+            # print "*" * 100
+            print program
             outs = exe.run(program,
                            feed=func_feed(feeding, data),
                            fetch_list=[cost])
+            # fetch_list=[usr])
             out = np.array(outs[0])
             if out[0] < 10.0:
                 exit(
