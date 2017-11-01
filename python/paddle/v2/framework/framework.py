ac0160d7c9588c3ff9e4fd387afee84837ceaf2b
Yang Yang(Tony) <yangyang62@baidu.com>
2017-11-01 15:09:39 -0700

    first commit (#5286)

--- python/paddle/v2/framework/framework.py
+++ python/paddle/v2/framework/framework.py
@@ -264,7 +264,9 @@ class Operator(object):
                     self.desc.set_attr(attr_name, attrs[attr_name])
 
         self.desc.check_attrs()
-        no_kernel_op_set = {'feed', 'fetch', 'save', 'load'}
+        no_kernel_op_set = {
+            'feed', 'fetch', 'save', 'load', 'rnn_memory_helper_grad'
+        }
         if type not in no_kernel_op_set:
             self.desc.infer_var_type(self.block.desc)
             self.desc.infer_shape(self.block.desc)
