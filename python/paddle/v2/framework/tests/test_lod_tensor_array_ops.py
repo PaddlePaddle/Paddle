c5ff3b593aec940f0f6c8640f8edca5217f9c52b
Yu Yang <yuyang18@baidu.com>
2017-11-03 13:26:19 -0700

    Add ut

--- python/paddle/v2/framework/tests/test_lod_tensor_array_ops.py
+++ python/paddle/v2/framework/tests/test_lod_tensor_array_ops.py
@@ -11,10 +11,16 @@ class TestCPULoDTensorArrayOps(unittest.TestCase):
         return core.CPUPlace()
 
     def test_lod_tensor_to_array_level_0(self):
-        place = self.place()
         tensor = core.LoDTensor()
-        tensor.set(numpy.arange(10).reshape(10, 1).astype('int32'), place)
+        tensor.set(
+            numpy.arange(10).reshape(10, 1).astype('int32'), self.place())
         tensor.set_lod([[0, 3, 9, 10]])
+        expect = map(lambda x: numpy.array(x).astype('int32'),
+                     [[3, 0, 9], [4, 1], [5, 2], [6], [7], [8]])
+        self.main(tensor=tensor, expect_array=expect, expect_lod=[] * 6)
+
+    def main(self, tensor, expect_array, expect_lod):
+        place = self.place()
         program = Program()
         x = layers.data(name='x', shape=[10], program=program)
         table = layers.lod_rank_table(x, level=0, program=program)
@@ -26,19 +32,17 @@ class TestCPULoDTensorArrayOps(unittest.TestCase):
         exe = Executor(place)
         scope = core.Scope()
         exe.run(program, feed={'x': tensor}, scope=scope)
-
         var = scope.find_var(array.name)
-
         array = var.get_lod_tensor_array()
-        expect = map(lambda x: numpy.array(x).astype('int32'),
-                     [[3, 0, 9], [4, 1], [5, 2], [6], [7], [8]])
-        self.check_array_same(array, expect)
+        self.check_array_same(array, expect_array, expect_lod)
         self.check_tensor_same(scope.find_var(x.name).get_tensor(), tensor)
 
-    def check_array_same(self, array, expect):
-        self.assertEqual(len(expect), len(array))
-        for i, exp in enumerate(expect):
-            self.assertEqual(exp, numpy.array(array[i]))
+    def check_array_same(self, array, expect_tensor, expect_lod):
+        self.assertEqual(len(expect_tensor), len(array))
+        for i, exp in enumerate(zip(expect_tensor, expect_lod)):
+            exp_tensor, exp_lod = exp
+            self.assertEqual(exp_tensor, numpy.array(array[i]))
+            self.assertEqual(exp_lod, array[i].get_lod())
 
     def check_tensor_same(self, actual, expect):
         self.assertEqual(numpy.array(actual), numpy.array(expect))
