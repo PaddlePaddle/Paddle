f6db830c974a86a0558b21b5e0ca2d509d360db8
fengjiayi <fengjiayi@baidu.com>
2017-11-05 14:32:44 -0800

    fix a compile bug

--- paddle/framework/lod_tensor_test.cc
+++ paddle/framework/lod_tensor_test.cc
@@ -146,9 +146,10 @@ TEST(LodExpand, test) {
 
 TEST(LoD, GetFineGrainedLoDLength) {
   LoD lod;
-  lod.push_back({0, 2, 4, 5});
-  lod.push_back({0, 1, 6, 8, 10, 11});
-  lod.push_back({0, 2, 5, 7, 10, 12, 15, 17, 20, 24, 26, 29});
+  lod.push_back(std::vector<size_t>{0, 2, 4, 5});
+  lod.push_back(std::vector<size_t>{0, 1, 6, 8, 10, 11});
+  lod.push_back(
+      std::vector<size_t>{0, 2, 5, 7, 10, 12, 15, 17, 20, 24, 26, 29});
 
   std::vector<std::vector<size_t>> lod_length;
   size_t start_offset;
@@ -156,30 +157,31 @@ TEST(LoD, GetFineGrainedLoDLength) {
                                              &start_offset);
 
   std::vector<std::vector<size_t>> expected;
-  expected.push_back({2});
-  expected.push_back({2, 2});
-  expected.push_back({2, 3, 4, 2});
+  expected.push_back(std::vector<size_t>{2});
+  expected.push_back(std::vector<size_t>{2, 2});
+  expected.push_back(std::vector<size_t>{2, 3, 4, 2});
   EXPECT_EQ(lod_length, expected);
   EXPECT_EQ(start_offset, 15UL);
 }
 
 TEST(LoD, AppendLoD) {
   std::vector<std::vector<size_t>> lod_lens;
-  lod_lens.push_back({2});
-  lod_lens.push_back({2, 2});
-  lod_lens.push_back({2, 3, 4, 2});
+  lod_lens.push_back(std::vector<size_t>{2});
+  lod_lens.push_back(std::vector<size_t>{2, 2});
+  lod_lens.push_back(std::vector<size_t>{2, 3, 4, 2});
 
   LoD origin;
-  origin.push_back({0, 2});
-  origin.push_back({0, 1, 6});
-  origin.push_back({0, 2, 5, 7, 10, 12, 15});
+  origin.push_back(std::vector<size_t>{0, 2});
+  origin.push_back(std::vector<size_t>{0, 1, 6});
+  origin.push_back(std::vector<size_t>{0, 2, 5, 7, 10, 12, 15});
 
   paddle::framework::AppendLoD(&origin, lod_lens);
 
   LoD expected;
-  expected.push_back({0, 2, 4});
-  expected.push_back({0, 1, 6, 8, 10});
-  expected.push_back({0, 2, 5, 7, 10, 12, 15, 17, 20, 24, 26});
+  expected.push_back(std::vector<size_t>{0, 2, 4});
+  expected.push_back(std::vector<size_t>{0, 1, 6, 8, 10});
+  expected.push_back(
+      std::vector<size_t>{0, 2, 5, 7, 10, 12, 15, 17, 20, 24, 26});
 
   EXPECT_EQ(origin, expected);
 }
