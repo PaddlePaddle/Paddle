f529d4654000beaf7e23ccfb8b10fa0a240f8e4a
dangqingqing <dangqingqing@baidu.com>
2017-11-06 12:05:36 +0800

    Fix Python API.

--- python/paddle/v2/framework/layers.py
+++ python/paddle/v2/framework/layers.py
@@ -372,11 +372,13 @@ def sequence_pool(input, pool_type, **kwargs):
     helper = LayerHelper('sequence_pool', input=input, **kwargs)
     dtype = helper.input_dtype()
     pool_out = helper.create_tmp_variable(dtype)
+    max_index = helper.create_tmp_variable(dtype)
 
     helper.append_op(
         type="sequence_pool",
-        inputs={"X": [input]},
-        outputs={"Out": [pool_out]},
+        inputs={"X": input},
+        outputs={"Out": pool_out,
+                 "MaxIndex": max_index},
         attrs={"pooltype": pool_type.upper()})
 
     return pool_out
