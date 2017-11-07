364b54f98bbbbec228ce0d333ab196aef71775cb
fengjiayi <fengjiayi@baidu.com>
2017-11-06 16:18:42 -0800

    Add grad for ops

--- paddle/operators/array_to_lod_tensor_op.cc
+++ paddle/operators/array_to_lod_tensor_op.cc
@@ -141,10 +141,26 @@ class ArrayToLoDTensorInferShape : public framework::InferShapeBase {
   }
 };
 
+class ArrayToLoDTensorGradMaker : public framework::SingleGradOpDescMaker {
+ public:
+  using framework::SingleGradOpDescMaker::SingleGradOpDescMaker;
+
+ protected:
+  std::unique_ptr<framework::OpDescBind> Apply() const override {
+    auto *grad_op = new framework::OpDescBind();
+    grad_op->SetType("lod_tensor_to_array");
+    grad_op->SetInput("X", OutputGrad("Out"));
+    grad_op->SetInput("RankTable", Input("RankTable"));
+    grad_op->SetOutput("Out", InputGrad("X"));
+    grad_op->SetAttrMap(Attrs());
+    return std::unique_ptr<framework::OpDescBind>(grad_op);
+  }
+};
+
 }  // namespace operators
 }  // namespace paddle
 
 namespace ops = paddle::operators;
 REGISTER_OPERATOR(array_to_lod_tensor, ops::ArrayToLoDTensorOp,
                   ops::ArrayToLoDTensorOpProtoMaker,
-                  ops::ArrayToLoDTensorInferShape);
+                  ops::ArrayToLoDTensorInferShape, ops::LoDTensorToArrayOp);
