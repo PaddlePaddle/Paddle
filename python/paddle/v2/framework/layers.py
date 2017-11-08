0a41c38d41a70a05bc6b4a495b7b219754e24b74
Yang Yu <yuyang18@baidu.com>
2017-11-07 17:33:33 -0800

    Shrink State Operator
    Used for shrink memories state in DyRNN. The height of state could
    be shrinked after running a step block.

--- python/paddle/v2/framework/layers.py
+++ python/paddle/v2/framework/layers.py
@@ -801,13 +801,12 @@ def zeros(shape, dtype, main_program=None):
 
 def increment(x, value=1.0, main_program=None):
     helper = LayerHelper("increment", **locals())
-    tmp = helper.create_tmp_variable(dtype=x.data_type)
     helper.append_op(
         type='increment',
         inputs={'X': [x]},
-        outputs={'Out': [tmp]},
+        outputs={'Out': [x]},
         attrs={'step': value})
-    return tmp
+    return x
 
 
 def array_write(x, i, array=None, main_program=None):
@@ -837,4 +836,17 @@ def array_read(array, i, main_program=None):
         inputs={'X': [array],
                 'I': [i]},
         outputs={'Out': [out]})
+    return out
+
+
+def shrink_memory(x, i, table, main_program=None):
+    helper = LayerHelper('shrink_memory', **locals())
+    out = helper.create_tmp_variable(dtype=x.data_type)
+    helper.append_op(
+        type='shrink_state',
+        inputs={'X': [x],
+                'I': [i],
+                'RankTable': [table]},
+        outputs={'Out': [out]},
+        attrs={})
     return out
