79e0a26a6472a047ff5b3ebaedc3da6c6eeb6d2a
guosheng <guosheng@baidu.com>
2017-11-10 20:03:36 +0800

    Fix test_roi_pool_layer.py

--- python/paddle/trainer_config_helpers/tests/configs/test_roi_pool_layer.py
+++ python/paddle/trainer_config_helpers/tests/configs/test_roi_pool_layer.py
@@ -4,8 +4,17 @@ data = data_layer(name='data', size=3 * 14 * 14, height=14, width=14)
 
 rois = data_layer(name='rois', size=10)
 
-roi_pool = roi_pool_layer(
+conv = img_conv_layer(
     input=data,
+    filter_size=3,
+    num_channels=3,
+    num_filters=16,
+    padding=1,
+    act=LinearActivation(),
+    bias_attr=True)
+
+roi_pool = roi_pool_layer(
+    input=conv,
     rois=rois,
     pooled_width=7,
     pooled_height=7,
