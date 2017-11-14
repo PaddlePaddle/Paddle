2800163d26a912286289d32f662391c7da0a7a51
Dong Zhihong <dzhwinter@gmail.com>
2017-11-13 20:29:21 -0800

    "add asnumpy interface"

--- python/paddle/v2/framework/executor.py
+++ python/paddle/v2/framework/executor.py
@@ -1,5 +1,6 @@
 import paddle.v2.framework.core as core
 from paddle.v2.framework.framework import Block, Program, g_main_program
+import numpy as np
 
 g_scope = core.Scope()
 
@@ -16,6 +17,74 @@ class Executor(object):
             act_places.append(p)
 
         self.executor = core.Executor(act_places)
+        self.places = act_places
+
+    def aslodtensor(self, data):
+        def accumulate(data):
+            if not isinstance(data, list):
+                return 1
+            return sum([accumulate(sub) for sub in data])
+
+        def parselod(data):
+            seq_lens = [accumulate(seq) for seq in data]
+            cur_len = 0
+            lod = [cur_len]
+            for l in seq_lens:
+                cur_len += l
+                lod.append(cur_len)
+            return lod
+
+        assert len(self.places) != 0
+        if not isinstance(data, list):
+            # pure tensor case
+            tensor = core.LoDTensor()
+            numpy_data = np.array(data).reshape([len(data), 1])
+            tensor.set(numpy_data, self.places[0])
+            return tensor
+        else:
+            # lodtensor case
+            lod = []
+            flattened_data = None
+            if not isinstance(data[0], list):
+                lod.append(parselod(data))
+                flattened_data = np.concatenate(data, axis=0).astype("int64")
+            else:
+                while isinstance(data[0], list):
+                    lod.append(parselod(seq))
+                    flattened_data = [item for seq in data for item in seq]
+                    data = flattened_data
+                flattened_data = np.concatenate(data, axis=0).astype("int64")
+            flattened_data = flattened_data.reshape([len(flattened_data), 1])
+            tensor = core.LoDTensor()
+            tensor.set(flattened_data, self.places[0])
+            tensor.set_lod(lod)
+            return tensor
+
+    def asnumpy(self, tensor):
+        if isinstance(tensor, list):
+            return [self.asnumpy(t) for t in tensor]
+        assert isinstance(tensor, core.LoDTensor)
+        lod = tensor.lod()
+        ans = None
+        tensor_data = np.array(tensor)
+        if len(lod) == 0:
+            ans = tensor_data
+        elif len(lod) == 1:
+            ans = []
+            idx = 0
+            while idx < len(lod) - 1:
+                ans.append(tensor_data[lod[idx]:lod[idx + 1]])
+                idx += 1
+        else:
+            for l in reversed(lod):
+                ans = []
+                idx = 0
+                while idx < len(l) - 1:
+                    ans.append(tensor_data[l[idx]:l[idx + 1]])
+                    idx += 1
+                tensor_data = ans
+            ans = tensor_data
+        return ans
 
     def run(self,
             program=None,
@@ -52,6 +121,7 @@ class Executor(object):
                 inputs={'X': [feed_var]},
                 outputs={'Out': [out]},
                 attrs={'col': i})
+            # core.set_feed_variable(scope, self.aslodtensor(feed[name]), feed_var.name, i)
             core.set_feed_variable(scope, feed[name], feed_var.name, i)
 
         fetch_var = global_block.create_var(
@@ -66,7 +136,8 @@ class Executor(object):
                 attrs={'col': i})
 
         self.executor.run(program.desc, scope, 0, True)
-        return [
+        outs = [
             core.get_fetch_variable(scope, fetch_var_name, i)
             for i in xrange(len(fetch_list))
         ]
+        return [self.asnumpy(out) for out in outs]
--- python/paddle/v2/framework/tests/test_executor_and_mul.py
+++ python/paddle/v2/framework/tests/test_executor_and_mul.py
@@ -1,5 +1,5 @@
 import unittest
-from paddle.v2.framework.layers import mul, data
+from paddle.v2.framework.layers import mul, data, sequence_pool
 import paddle.v2.framework.core as core
 from paddle.v2.framework.executor import Executor
 from paddle.v2.framework.framework import g_main_program
@@ -17,19 +17,39 @@ class TestExecutor(unittest.TestCase):
         out = mul(x=a, y=b)
         place = core.CPUPlace()
         a_np = numpy.random.random((100, 784)).astype('float32')
-        tensor_a = core.LoDTensor()
-        tensor_a.set(a_np, place)
         b_np = numpy.random.random((784, 100)).astype('float32')
-        tensor_b = core.LoDTensor()
-        tensor_b.set(b_np, place)
         exe = Executor(place)
         outs = exe.run(g_main_program,
-                       feed={'a': tensor_a,
-                             'b': tensor_b},
+                       feed={'a': a_np,
+                             'b': b_np},
                        fetch_list=[out])
-        out = numpy.array(outs[0])
+        out = outs[0]
         self.assertEqual((100, 100), out.shape)
         self.assertTrue(numpy.allclose(out, numpy.dot(a_np, b_np)))
+
+
+def sequence_sum(lod, x):
+    N = len(lod[0]) - 1
+    out = [None for i in range(N)]
+    for i in range(N):
+        sub_x = x[lod[0][i]:lod[0][i + 1], :]
+        out[i] = sub_x.sum(axis=0)
+    return out
+
+
+class TestExecutor2(unittest.TestCase):
+    def test_asnumpy(self):
+        seq = data(name='seq', shape=[784], data_type='float32')
+        out = sequence_pool(seq, "sum")
+        x = np.ones(shape=(3, 5)).astype('float32')
+        lod = [[0, 2, 3]]
+        tensor = core.LoDTensor()
+        place = core.CPUPlace()
+        tensor.set(x, place)
+        tensor.set_lod(lod)
+        exe = Executor(place)
+        outs = exe.run(g_main_program, feed={"seq": tensor}, fetch_list=out)
+        self.assertTrue(np.allclose(outs[0], sequence_sum(lod, x)))
 
 
 if __name__ == '__main__':
