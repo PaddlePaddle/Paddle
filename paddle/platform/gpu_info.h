fa9b71ad2600ac587ad49b55e9f93dd4c1a3d129
Dong Zhihong <dzhwinter@gmail.com>
2017-11-15 00:07:47 -0800

    "fix accuracy kernel bug"

--- paddle/platform/gpu_info.h
+++ paddle/platform/gpu_info.h
@@ -60,6 +60,9 @@ void GpuMemcpySync(void *dst, const void *src, size_t count,
 void GpuMemcpyPeer(void *dst, int dst_device, const void *src, int src_device,
                    size_t count, cudaStream_t stream);
 
+//! Set memory from address dst to count size asynchronously with value
+void GpuMemsetAsync(void *dst, int value, , size_t count, cudaStream_t stream);
+
 }  // namespace platform
 }  // namespace paddle
 
