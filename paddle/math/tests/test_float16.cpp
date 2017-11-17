7547f62824862ade59649a1e2efd65b7568bc6c2
Kexin Zhao <zhaokexin01@baidu.com>
2017-11-16 16:28:33 -0800

    two tests for cpu and gpu separately

--- paddle/math/tests/test_float16.cpp
+++ paddle/math/tests/test_float16.cpp
@@ -14,15 +14,15 @@ limitations under the License. */
 
 namespace paddle {
 
-#ifdef PADDLE_CUDA_FP16
-TEST(float16, conversion_gpu) {
-  // Conversion to and from cuda half
-  float16 v1 = half(float16(1.0f));
-  EXPECT_EQ(v1.x, 0x3c00);
-
+TEST(float16, conversion_cpu) {
   // Conversion to and from Eigen::half
-  float16 v2 = Eigen::half(float16(0.5f));
-  EXPECT_EQ(v2.x, 0x3800);
+  EXPECT_EQ(float16(Eigen::half(float16(1.0f))).x, 0x3c00);
+  EXPECT_EQ(float16(Eigen::half(float16(0.5f))).x, 0x3800);
+  EXPECT_EQ(float16(Eigen::half(float16(0.33333f))).x, 0x3555);
+  EXPECT_EQ(float16(Eigen::half(float16(0.0f))).x, 0x0000);
+  EXPECT_EQ(float16(Eigen::half(float16(-0.0f))).x, 0x8000);
+  EXPECT_EQ(float16(Eigen::half(float16(65504.0f))).x, 0x7bff);
+  EXPECT_EQ(float16(Eigen::half(float16(65536.0f))).x, 0x7c00);
 
   // Conversion from float
   EXPECT_EQ(float16(1.0f).x, 0x3c00);
@@ -39,10 +39,9 @@ TEST(float16, conversion_gpu) {
 
   // Conversion from bool
 }
-#endif
 
-TEST(float16, arithmetic_gpu) { EXPECT_EQ(float(float16(2) + float16(2)), 4); }
+TEST(float16, arithmetic_cpu) { EXPECT_EQ(float(float16(2) + float16(2)), 4); }
 
-TEST(float16, comparison_gpu) { EXPECT_TRUE(float16(1.0f) > float16(0.5f)); }
+TEST(float16, comparison_cpu) { EXPECT_TRUE(float16(1.0f) > float16(0.5f)); }
 
 }  // namespace paddle
