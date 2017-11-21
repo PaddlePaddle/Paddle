9874a18e9e771e631e9b76c121c1d69c18c89840
fengjiayi <fengjiayi@baidu.com>
2017-11-21 15:46:36 +0800

    Fix a bug

--- python/paddle/v2/fluid/tests/book/test_understand_sentiment_lstm.py
+++ python/paddle/v2/fluid/tests/book/test_understand_sentiment_lstm.py
@@ -104,9 +104,8 @@ def main():
             acc_val = np.array(outs[1])
 
             print("cost=" + str(cost_val) + " acc=" + str(acc_val))
-            if cost_val < 1.0 and acc_val > 0.7:
-                # exit(0)
-                pass
+            if acc_val > 0.7:
+                exit(0)
     exit(1)
 
 
