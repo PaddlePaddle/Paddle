0ccd123e4b08a6666f41d888a9b7822236fe1116
Yang Yu <yuyang18@baidu.com>
2017-11-29 14:42:32 +0800

    Fix CI

--- python/paddle/v2/fluid/tests/test_layers.py
+++ python/paddle/v2/fluid/tests/test_layers.py
@@ -46,10 +46,8 @@ class TestBook(unittest.TestCase):
     def test_conv2d_transpose(self):
         program = Program()
         with program_guard(program):
-            img = layers.data(
-                name='pixel', shape=[3, 2, 2], dtype='float32', **kwargs)
-            layers.conv2d_transpose(
-                input=img, num_filters=10, output_size=28, **kwargs)
+            img = layers.data(name='pixel', shape=[3, 2, 2], dtype='float32')
+            layers.conv2d_transpose(input=img, num_filters=10, output_size=28)
         print(str(program))
 
     def test_recognize_digits_conv(self):
@@ -135,7 +133,7 @@ class TestBook(unittest.TestCase):
             label = layers.data(name='label', shape=[1], dtype='int32')
             hidden = layers.fc(input=images, size=128)
             crf = layers.linear_chain_crf(input=hidden, label=label)
-            self.assertNotEqual(crf)
+            self.assertNotEqual(crf, None)
 
         print(str(program))
 
