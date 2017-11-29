b2eff2a891a38193fea7d116516b0bfc146ef871
fengjiayi <fengjiayi@baidu.com>
2017-11-29 14:30:29 +0800

    Update

--- python/paddle/v2/fluid/tests/test_dynamic_rnn.py
+++ python/paddle/v2/fluid/tests/test_dynamic_rnn.py
@@ -32,22 +32,24 @@ class TestDynamicRNN(unittest.TestCase):
         seq = [[1, 2, 3, 4], [2, 3, 4], [5]]
         input_tensor = to_lodtensor(seq, self.place)
         rnn = fluid.layers.DynamicRNN()
-        i = 1.0
+        i = 2.0
         init = fluid.layers.fill_constant(shape=[3, 1], dtype="int64", value=0)
         with rnn.step():
             xt = rnn.step_input(in_data)
             mem = rnn.memory(init=init)
             scaled_xt = fluid.layers.scale(x=xt, scale=i)
-            i = i + 1
             s = fluid.layers.elementwise_add(x=scaled_xt, y=mem)
             rnn.update_memory(s)
             rnn.output(s)
         out_data = rnn()
 
-        excepted_out = [[1, 5, 14, 30], [2, 8, 20], [5]]
+        expected_out = [[2], [6], [12], [20], [4], [10], [18], [10]]
+        expected_lod = [[0, 4, 7, 8]]
         out = self.exe.run(feed={"in_data": input_tensor},
-                           fetch_list=[out_data])
-        self.assertEqual(excepted_out, np.array(out[0]))
+                           fetch_list=[out_data],
+                           return_numpy=False)
+        np.testing.assert_array_equal(np.array(out[0]), expected_out)
+        self.assertEqual(out[0].lod(), expected_lod)
 
 
 if __name__ == '__main__':
