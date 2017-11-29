c7d1de8e24bf0b072be5a96639ba228fd0d6ec20
qijun <qijun1994@hotmail.com>
2017-11-29 20:53:42 +0800

    format code

--- python/paddle/v2/fluid/__init__.py
+++ python/paddle/v2/fluid/__init__.py
@@ -36,7 +36,8 @@ def __read_gflags_from_env__():
     read_env_flags = ['use_pinned_memory']
     if core.is_compile_gpu():
         read_env_flags.append('fraction_of_gpu_memory_to_use')
-    core.init_gflags([sys.argv[0]] + ["--tryfromenv=" + ",".join(read_env_flags)])
+    core.init_gflags([sys.argv[0]] +
+                     ["--tryfromenv=" + ",".join(read_env_flags)])
 
 
 __read_gflags_from_env__()
