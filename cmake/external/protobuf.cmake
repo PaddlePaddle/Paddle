a0648ee449335ba4989a83230874089b8685e3ad
dangqingqing <dangqingqing@baidu.com>
2017-11-30 20:37:23 +0800

    Add comments.

--- cmake/external/protobuf.cmake
+++ cmake/external/protobuf.cmake
@@ -191,6 +191,8 @@ FUNCTION(build_protobuf TARGET_NAME BUILD_FOR_HOST)
     SET(PROTOBUF_REPO "https://github.com/google/protobuf.git")
     SET(PROTOBUF_TAG "9f75c5aa851cd877fb0d93ccc31b8567a6706546")
     IF(MOBILE_INFERENCE)
+        # The reason why the official version is not used is described in
+        # https://github.com/PaddlePaddle/Paddle/issues/6114
         SET(PROTOBUF_REPO "https://github.com/qingqing01/protobuf.git")
         SET(PROTOBUF_TAG "v3.2.0")
         IF(NOT BUILD_FOR_HOST)
