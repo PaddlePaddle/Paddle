a95984b2dc90de83429799dbe182719f58f31c80
superjom <superjom@gmail.com>
2017-12-04 18:42:37 +0800

    encoder ready

--- python/paddle/v2/fluid/tests/book/test_machine_translation.py
+++ python/paddle/v2/fluid/tests/book/test_machine_translation.py
@@ -6,38 +6,41 @@ import paddle.v2.fluid.framework as framework
 import paddle.v2.fluid.layers as layers
 from paddle.v2.fluid.executor import Executor, g_scope
 from paddle.v2.fluid.optimizer import SGDOptimizer
+import paddle.v2.fluid as fluid
 
 dict_size = 30000
 source_dict_dim = target_dict_dim = dict_size
 src_dict, trg_dict = paddle.dataset.wmt14.get_dict(dict_size)
 hidden_dim = 512
 word_dim = 512
+IS_SPARSE = True
+batch_size = 50
 
-src_word_id = layers.data(name="src_word_id", shape=[1], data_type='int64')
+src_word_id = layers.data(name="src_word_id", shape=[1], dtype='int64')
 src_embedding = layers.embedding(
     input=src_word_id,
     size=[dict_size, word_dim],
-    data_type='float32',
+    dtype='float32',
     is_sparse=IS_SPARSE,
-    param_attr={'name': 'vemb'})
+    param_attr=fluid.ParamAttr(name='vemb'))
 
 
 def encoder():
 
-    lstm_0 = layers.dynamic_lstm(
+    lstm_hidden0, lstm_0 = layers.dynamic_lstm(
         input=src_embedding,
         size=hidden_dim,
         candidate_activation='sigmoid',
         cell_activation='sigmoid')
 
-    lstm_1 = layers.dynamic_lstm(
+    lstm_hidden1, lstm_1 = layers.dynamic_lstm(
         input=src_embedding,
         size=hidden_dim,
         candidate_activation='sigmoid',
         cell_activation='sigmoid',
         is_reverse=True)
 
-    bidirect_lstm_out = layers.concat(input=[lstm_0, lstm_1])
+    bidirect_lstm_out = layers.concat([lstm_hidden0, lstm_hidden1], axis=0)
 
     return bidirect_lstm_out
 
@@ -61,19 +64,26 @@ def main():
     encoder_out = encoder()
     train_data = paddle.batch(
         paddle.reader.shuffle(
-            paddle.dataset.wmt14.train(), buf_size=1000))
+            paddle.dataset.wmt14.train(8000), buf_size=1000),
+        batch_size=batch_size)
+
     place = core.CPUPlace()
     exe = Executor(place)
 
     exe.run(framework.default_startup_program())
 
-    for pass_id in xrange(10):
+    batch_id = 0
+    for pass_id in xrange(2):
+        print 'pass_id', pass_id
         for data in train_data():
-            word_data = to_lodtensor(mp(lambda x: x[0], data), place)
+            print 'batch', batch_id
+            batch_id += 1
+            if batch_id > 10: break
+            word_data = to_lodtensor(map(lambda x: x[0], data), place)
             outs = exe.run(framework.default_main_program(),
-                           feed={'word_data': word_data, },
+                           feed={'src_word_id': word_data, },
                            fetch_list=[encoder_out])
 
 
-if __main__ == '__main__':
+if __name__ == '__main__':
     main()
