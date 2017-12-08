f3bf72fc31beaddc11ccba688abd37fc95aa3cd8
qiaolongfei <qiaolongfei@baidu.com>
2017-12-08 11:09:42 +0800

    add early exit

--- python/paddle/v2/fluid/tests/book/test_machine_translation.py
+++ python/paddle/v2/fluid/tests/book/test_machine_translation.py
@@ -106,9 +106,11 @@ def main():
                            },
                            fetch_list=[avg_cost])
             avg_cost_val = np.array(outs[0])
-            batch_id += 1
             print('pass_id=' + pass_id + ' batch=' + batch_id + " avg_cost=" +
                   str(avg_cost_val))
+            if batch_id > 3:
+                exit(0)
+            batch_id += 1
 
 
 if __name__ == '__main__':
