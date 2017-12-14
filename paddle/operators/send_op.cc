1b20096a529bb6ce80d066fc0805c9dd8a8b9364
typhoonzero <typhoonzero1986@gmail.com>
2017-12-14 20:25:20 +0800

    done

--- paddle/operators/send_op.cc
+++ paddle/operators/send_op.cc
@@ -47,14 +47,12 @@ class SendOp : public framework::OperatorBase {
     // TODO(typhoonzero): currently it's non-blocking,
     // should block until server responds.
     for (auto in : ins) {
-      LOG(ERROR) << "sending grad: " << in;
       bool ret = client_->SendVariable(scope, in);
       if (!ret) {
         LOG(ERROR) << "send variable error";
       }
     }
     for (auto in : ins) {
-      LOG(ERROR) << "updating from server...";
       bool ret = client_->GetVariable(scope);
       if (!ret) {
         LOG(ERROR) << "GetVariable error";
