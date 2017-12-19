b23a6767ee2bc563d12781e9e5307d19bfbeaf02
Yang Yu <yuyang18@baidu.com>
2017-12-19 16:28:29 +0800

    Remove broadcast in cos_sim op.

--- paddle/operators/cos_sim_op.h
+++ paddle/operators/cos_sim_op.h
@@ -67,6 +67,61 @@ class CosSimKernel : public framework::OpKernel<T> {
   }
 };
 
+template <typename IT1, typename IT2, typename Callback>
+static void ForEachZip(IT1 begin1, IT1 last1, IT2 begin2, Callback callback) {
+  // This method could be implemented in CUDA
+  for (; begin1 < last1; ++begin1, ++begin2) {
+    callback(*begin1, *begin2);
+  }
+};
+
+template <typename T>
+struct CosSimGradFunctor {
+  CosSimGradFunctor(const T* x_norm, const T* y_norm, const T* x, const T* y,
+                    const T* z, const T* dz, T* dx, T* dy, int cols)
+      : x_norm_(x_norm),
+        y_norm_(y_norm),
+        x_(x),
+        y_(y),
+        z_(z),
+        dz_(dz),
+        dx_(dx),
+        dy_(dy),
+        cols_(static_cast<size_t>(cols)) {}
+
+  void operator()(const T& x_norm, const T& y_norm) const {
+    size_t x_offset = &x_norm - x_norm_;
+    size_t y_offset = &y_norm - y_norm_;
+
+    auto x_norm_square = x_norm_[x_offset] * x_norm_[x_offset];
+    auto y_norm_square = y_norm_[y_offset] * y_norm_[y_offset];
+    auto xy_norm_prod = x_norm_[x_offset] * y_norm_[y_offset];
+    auto dz = dz_[x_offset];
+
+    auto* dx = dx_ + cols_ * x_offset;
+    auto* dy = dy_ + cols_ * y_offset;
+    auto* x = x_ + cols_ * x_offset;
+    auto* y = y_ + cols_ * y_offset;
+    auto z = z_[x_offset];
+
+    for (size_t i = 0; i < cols_; ++i) {
+      dx[i] = dz * (y[i] / xy_norm_prod - z * x[i] / x_norm_square);
+      // maybe atomicAdd
+      dy[i] = dz * (x[i] / xy_norm_prod - z * y[i] / y_norm_square);
+    }
+  }
+
+  const T* x_norm_;
+  const T* y_norm_;
+  const T* x_;
+  const T* y_;
+  const T* z_;
+  const T* dz_;
+  T* dx_;
+  T* dy_;
+  size_t cols_;
+};
+
 template <typename DeviceContext, typename T>
 class CosSimGradKernel : public framework::OpKernel<T> {
  public:
@@ -100,22 +155,32 @@ class CosSimGradKernel : public framework::OpKernel<T> {
     auto& place =
         *context.template device_context<DeviceContext>().eigen_device();
     if (rows_x == rows_y) {
-      auto y_snorm_bcast = y_norm.square().eval().broadcast(bcast_cols);
-      auto norm_prod_bcast = (x_norm * y_norm).eval().broadcast(bcast_cols);
-      // compute dx
-      if (out_grad_x) {
-        out_grad_x->mutable_data<T>(context.GetPlace());
-        auto dx = EigenMatrix<T>::Reshape(*out_grad_x, 1);
-        auto grad = y / norm_prod_bcast - z_bcast * x / x_snorm_bcast;
-        dx.device(place) = dz_bcast * grad;
-      }
-      // compute dy
-      if (out_grad_y) {
-        out_grad_y->mutable_data<T>(context.GetPlace());
-        auto dy = EigenMatrix<T>::Reshape(*out_grad_y, 1);
-        auto grad = x / norm_prod_bcast - z_bcast * y / y_snorm_bcast;
-        dy.device(place) = dz_bcast * grad;
-      }
+      CosSimGradFunctor functor(
+          in_x_norm->data<T>(), in_y_norm->data<T>(), in_x->data<T>(),
+          in_y->data<T>(), in_z->data<T>(), in_grad_z->data<T>(),
+          out_grad_x->mutable_data<T>(context.GetPlace()),
+          out_grad_y->mutable_data<T>(context.GetPlace()), cols);
+
+      ForEachZip(in_x_norm->data<T>(), in_x_norm->data<T>() + rows_x,
+                 in_y_norm->data<T>(), functor);
+
+      //      auto y_snorm_bcast = y_norm.square().eval().broadcast(bcast_cols);
+      //      auto norm_prod_bcast = (x_norm *
+      //      y_norm).eval().broadcast(bcast_cols);
+      //      // compute dx
+      //      if (out_grad_x) {
+      //        out_grad_x->mutable_data<T>(context.GetPlace());
+      //        auto dx = EigenMatrix<T>::Reshape(*out_grad_x, 1);
+      //        auto grad = y / norm_prod_bcast - z_bcast * x / x_snorm_bcast;
+      //        dx.device(place) = dz_bcast * grad;
+      //      }
+      //      // compute dy
+      //      if (out_grad_y) {
+      //        out_grad_y->mutable_data<T>(context.GetPlace());
+      //        auto dy = EigenMatrix<T>::Reshape(*out_grad_y, 1);
+      //        auto grad = x / norm_prod_bcast - z_bcast * y / y_snorm_bcast;
+      //        dy.device(place) = dz_bcast * grad;
+      //      }
     } else {
       Eigen::DSizes<int, 2> bcast_rows(rows_x, 1);
       Eigen::DSizes<int, 2> bcast_rows_cols(rows_x, cols);
