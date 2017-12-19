9fbd94263e4615010b0379aca94fe1bfd07e7964
qijun <qijun1994@hotmail.com>
2017-12-19 13:20:20 +0800

    init

---
+++ python/paddle/v2/fluid/layers/utils.py
@@ -0,0 +1,22 @@
+"""
+All util layers.
+"""
+
+from ..layer_helper import LayerHelper
+from ..framework import Variable
+
+__all__ = ['get_places']
+
+
+def get_places(use_gpu, trainer_count):
+    helper = LayerHelper('get_places', **locals())
+    out_places = helper.create_tmp_variable(dtype=helper.input_dtype())
+    helper.append_op(
+        type='get_places',
+        outputs={"Out": [out_places]},
+        attrs={
+            "use_gpu": use_gpu,
+            'trainer_count': trainer_count,
+        })
+
+    return out_places
