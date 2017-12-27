4a1246647a70a63f6abf115e81d28c62a546d732
qijun <qijun1994@hotmail.com>
2017-12-27 10:32:02 +0800

    add KernelTypeToString interface

--- paddle/framework/op_kernel_type.h
+++ paddle/framework/op_kernel_type.h
@@ -68,6 +68,8 @@ struct OpKernelType {
            data_type_ == o.data_type_ && data_layout_ == o.data_layout_ &&
            library_type_ == o.library_type_;
   }
+
+  bool operator!=(const OpKernelType& o) const { return !(*this == o); }
 };
 
 inline std::ostream& operator<<(std::ostream& os,
@@ -76,6 +78,12 @@ inline std::ostream& operator<<(std::ostream& os,
      << kernel_key.data_layout_ << "]:place[" << kernel_key.place_
      << "]:library_type[" << kernel_key.library_type_ << "]";
   return os;
+}
+
+inline std::string KernelTypeToString(const OpKernelType& kernel_key) {
+  std::ostringstream stream;
+  stream << op_kernel_type;
+  return stream.str();
 }
 
 }  // namespace framework
