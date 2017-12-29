3bf38fcb14fbd39b243f4c0805927613522dd199
qiaolongfei <qiaolongfei@baidu.com>
2017-12-29 10:27:11 +0800

    use a uniform test op to do test

--- paddle/framework/device_data_transform_test.cc
+++ paddle/framework/device_data_transform_test.cc
@@ -15,26 +15,32 @@ limitations under the License. */
 #include "gtest/gtest.h"
 
 #include "paddle/framework/init.h"
+#include "paddle/framework/lod_tensor.h"
 #include "paddle/framework/op_info.h"
 #include "paddle/framework/op_registry.h"
+#include "paddle/operators/elementwise_op_function.h"
+#include "paddle/operators/math/math_function.h"
 
 namespace paddle {
 namespace framework {
+
+template <typename T>
+struct AddFunctor {
+  inline HOSTDEVICE T operator()(T a, T b) const { return a + b; }
+};
 
 class OpKernelTestProtoAndCheckerMaker : public OpProtoAndCheckerMaker {
  public:
   OpKernelTestProtoAndCheckerMaker(OpProto* proto, OpAttrChecker* op_checker)
       : OpProtoAndCheckerMaker(proto, op_checker) {
-    AddInput("input", "input of test op");
+    AddInput("input", "input1 of test op");
     AddOutput("output", "output of test op");
+    AddAttr<bool>("use_cpu", "force to use cpu kernel").SetDefault(false);
     AddComment("This is test op");
   }
 };
 
-static int cpu_kernel_run_num = 0;
-static int op_run_num = 0;
-
-class CPUOpWithKernel : public OperatorWithKernel {
+class TestOpWithKernel : public OperatorWithKernel {
  public:
   using OperatorWithKernel::OperatorWithKernel;
 
@@ -45,34 +51,22 @@ class CPUOpWithKernel : public OperatorWithKernel {
   }
 };
 
-class CPUKernel : public OpKernel<float> {
+template <typename DeviceContext, typename T>
+class TestKernel : public OpKernel<float> {
  public:
   void Compute(const ExecutionContext& ctx) const {
     std::cout << ctx.op().DebugString() << std::endl;
-    cpu_kernel_run_num++;
-    ASSERT_EQ(ctx.op().Input("input"), "IN1");
-    ASSERT_EQ(ctx.op().Output("output"), "OUT1");
-  }
-};
 
-class GeneralOpWithKernel : public OperatorWithKernel {
- public:
-  using OperatorWithKernel::OperatorWithKernel;
+    const Tensor* input = ctx.Input<Tensor>("input");
 
- protected:
-  void InferShape(framework::InferShapeContext* ctx) const override {}
-  OpKernelType GetActualKernelType(const ExecutionContext& ctx) const override {
-    return OpKernelType(proto::DataType::FP32, ctx.GetPlace());
-  }
-};
+    auto* output = ctx.Output<framework::LoDTensor>("output");
+    output->Resize(input->dims());
+    output->mutable_data<T>(ctx.GetPlace());
 
-class GeneralKernel : public OpKernel<float> {
- public:
-  void Compute(const ExecutionContext& ctx) const {
-    std::cout << ctx.op().DebugString() << std::endl;
-    op_run_num++;
-    ASSERT_EQ(ctx.op().Input("input"), "IN2");
-    ASSERT_EQ(ctx.op().Output("output"), "OUT2");
+    operators::TransformFunctor<AddFunctor<T>, T, DeviceContext> functor(
+        input, input, output, ctx.template device_context<DeviceContext>(),
+        AddFunctor<T>());
+    functor.Run();
   }
 };
 
@@ -80,14 +74,11 @@ class GeneralKernel : public OpKernel<float> {
 }  // namespace paddle
 
 REGISTER_OP_WITHOUT_GRADIENT(
-    cpu_op, paddle::framework::CPUOpWithKernel,
+    test_op, paddle::framework::TestOpWithKernel,
     paddle::framework::OpKernelTestProtoAndCheckerMaker);
-REGISTER_OP_CPU_KERNEL(cpu_op, paddle::framework::CPUKernel);
-
-REGISTER_OP_WITHOUT_GRADIENT(
-    general_op, paddle::framework::GeneralOpWithKernel,
-    paddle::framework::OpKernelTestProtoAndCheckerMaker);
-REGISTER_OP_CPU_KERNEL(general_op, paddle::framework::GeneralKernel);
+REGISTER_OP_CPU_KERNEL(
+    test_op,
+    paddle::framework::TestKernel<paddle::platform::CPUDeviceContext, float>);
 
 static void BuildVar(const std::string& param_name,
                      std::initializer_list<const char*> arguments,
@@ -98,36 +89,55 @@ static void BuildVar(const std::string& param_name,
   }
 }
 
-TEST(Operator, MulitDevice) {
-  paddle::framework::InitDevices({"CPU"});
+//#ifdef PADDLE_WITH_CUDA
+TEST(Operator, CPUtoGPU) {
+  using namespace paddle::framework;
+  using namespace paddle::platform;
+
+  //  ASSERT_EQ(InitDevices({"CPU", "GPU:0"}), true);
+  ASSERT_EQ(InitDevices({"CPU"}), true);
 
   paddle::framework::Scope scope;
+  paddle::platform::CPUPlace cpu_place;
 
   // create a op to run on CPU
   paddle::framework::proto::OpDesc cpu_op_desc;
-  cpu_op_desc.set_type("cpu_op");
+  cpu_op_desc.set_type("test_op");
   BuildVar("input", {"IN1"}, cpu_op_desc.add_inputs());
   BuildVar("output", {"OUT1"}, cpu_op_desc.add_outputs());
 
-  paddle::platform::CPUPlace cpu_place;
-
   auto cpu_op = paddle::framework::OpRegistry::CreateOp(cpu_op_desc);
-  scope.Var("OUT1");
-  ASSERT_EQ(paddle::framework::cpu_kernel_run_num, 0);
+  // prepare input
+  auto* in_t = scope.Var("IN1")->GetMutable<LoDTensor>();
+  auto* src_ptr = in_t->mutable_data<float>({2, 3}, CPUPlace());
+  for (int i = 0; i < 2 * 3; ++i) {
+    src_ptr[i] = static_cast<float>(i);
+  }
+
+  // get output
+  auto* output = scope.Var("OUT1");
   cpu_op->Run(scope, cpu_place);
-  ASSERT_EQ(paddle::framework::cpu_kernel_run_num, 1);
 
-  // create a op to run on GPU
-  paddle::framework::proto::OpDesc general_op_desc;
-  general_op_desc.set_type("general_op");
-  BuildVar("input", {"IN2"}, general_op_desc.add_inputs());
-  BuildVar("output", {"OUT2"}, general_op_desc.add_outputs());
+  auto* output_ptr = output->Get<LoDTensor>().data<float>();
+  for (int i = 0; i < 2 * 3; ++i) {
+    ASSERT_EQ(output_ptr[i], static_cast<float>(i) * 2);
+  }
 
-  paddle::platform::CUDAPlace cuda_place(0);
+  // create a op to run on CPU
+  paddle::framework::proto::OpDesc gpu_op_desc;
+  gpu_op_desc.set_type("test_op");
+  BuildVar("input", {"OUT1"}, gpu_op_desc.add_inputs());
+  BuildVar("output", {"OUT2"}, gpu_op_desc.add_outputs());
 
-  auto general_op = paddle::framework::OpRegistry::CreateOp(general_op_desc);
-  scope.Var("OUT2");
-  ASSERT_EQ(paddle::framework::op_run_num, 0);
-  general_op->Run(scope, cuda_place);
-  ASSERT_EQ(paddle::framework::op_run_num, 1);
+  auto gpu_op = paddle::framework::OpRegistry::CreateOp(gpu_op_desc);
+
+  // get output
+  auto* output2 = scope.Var("OUT2");
+  gpu_op->Run(scope, cpu_place);
+
+  auto* output2_ptr = output2->Get<LoDTensor>().data<float>();
+  for (int i = 0; i < 2 * 3; ++i) {
+    ASSERT_EQ(output2_ptr[i], static_cast<float>(i) * 4);
+  }
 }
+//#endif
