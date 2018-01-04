46f23cf66b7eb568c6b311ab386a79bb33860fba
gongweibao <weibao.gong@gmail.com>
2018-01-04 09:42:38 +0000

    refactor client

---
+++ paddle/operators/detail/grpc_client2.h
@@ -0,0 +1,269 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserve.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+
+#include <time.h>
+#include <chrono>
+#include <ctime>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "paddle/framework/data_type.h"
+#include "paddle/framework/lod_tensor.h"
+#include "paddle/framework/scope.h"
+#include "paddle/framework/selected_rows.h"
+#include "paddle/operators/detail/simple_block_queue.h"
+
+#include "paddle/operators/detail/send_recv.grpc.pb.h"
+#include "paddle/operators/detail/send_recv.pb.h"
+
+#include <grpc++/grpc++.h>
+#include <grpc/support/log.h>
+
+namespace paddle {
+namespace operators {
+namespace detail {
+
+struct VarHandle {
+  std::string ep;
+  std::string name;
+  std::string String() const {
+    std::ostringstream s;
+    s << "name:" << name << " ep:" << ep;
+    return s.str();
+  }
+};
+
+struct SendStatus {
+  std::string error;
+  std::chrono::system_clock::time_point start;
+  std::chrono::system_clock::time_point end;
+  VarHandle var;
+
+  std::string String() const {
+    std::time_t t0 = std::chrono::system_clock::to_time_t(start);
+    std::time_t t1 = std::chrono::system_clock::to_time_t(end);
+    std::ostringstream s;
+    s << var.String() << " start_time:" << std::ctime(&t0)
+      << " end_time:" << std::ctime(&t1);
+    return s.str();
+  }
+};
+
+class ClientBase {
+ public:
+  virtual bool Call(const framework::Scope* scope, const std::string& name,
+                    int64_t time_out) = 0;
+  virtual bool ProcRet() = 0;
+  virtual grpc::Status* GetStatus() = 0;
+  virtual SendStatus GetSendStatus() = 0;
+
+  virtual ~ClientBase() {}
+};
+
+class SendClient : public ClientBase {
+ public:
+  SendClient(std::shared_ptr<grpc::CompletionQueue> cq,
+             std::shared_ptr<grpc::Channel> ch, std::string ep) {
+    cq_ = cq;
+    ch_ = ch;
+    ep_ = ep;
+    stub_ = sendrecv::SendRecvService::NewStub(ch);
+    context_.reset(new grpc::ClientContext());
+    status_.reset(new grpc::Status());
+    reply_.reset(new sendrecv::VoidMessage());
+  }
+
+  virtual ~SendClient() {}
+
+  typedef std::chrono::system_clock::time_point time_point;
+
+  bool Call(const framework::Scope* scope, const std::string& name,
+            int64_t time_out) {
+    // record send status
+    send_status_.start = std::chrono::system_clock::now();
+    send_status_.end = send_status_.start;
+    send_status_.var.ep = ep_;
+    send_status_.var.name = name;
+
+    const time_point deadline =
+        send_status_.start + std::chrono::milliseconds(time_out);
+    context_->set_deadline(deadline);
+
+    sendrecv::VariableMessage req;
+    _Request(scope, name, &req);
+
+    rpc_ = stub_->AsyncSendVariable(context_.get(), req, cq_.get());
+    rpc_->Finish(reply_.get(), status_.get(), (void*)this);
+
+    return true;
+  }
+
+  bool ProcRet() { return true; }
+
+  grpc::Status* GetStatus() { return status_.get(); }
+
+  SendStatus GetSendStatus() { return send_status_; }
+
+ private:
+  void _Request(const framework::Scope* scope, const std::string name,
+                sendrecv::VariableMessage* msg) {
+    // FIXME(gongwb): pass device context to here.
+    auto ctx = platform::CPUDeviceContext();
+    auto* var = scope->FindVar(name);
+    PADDLE_ENFORCE(var);
+    // TODO(gongwb): support SelectedRows
+    PADDLE_ENFORCE(var->IsType<framework::LoDTensor>(),
+                   "Only support LoDTensor, %s has wrong type", name);
+
+    const framework::LoDTensor& tensor = var->Get<framework::LoDTensor>();
+    std::ostringstream oss;
+    framework::SerializeToStream(oss, tensor, ctx);
+    msg->set_varname(name);
+    msg->set_serialized(oss.str());
+  }
+
+ private:
+  std::shared_ptr<grpc::CompletionQueue> cq_;
+  std::shared_ptr<grpc::Channel> ch_;
+  std::string ep_;
+  std::unique_ptr<sendrecv::SendRecvService::Stub> stub_;
+  std::unique_ptr<grpc::ClientContext> context_;
+  std::unique_ptr<grpc::Status> status_;
+  std::unique_ptr<sendrecv::VoidMessage> reply_;
+  std::unique_ptr<grpc::ClientAsyncResponseReader<sendrecv::VoidMessage>> rpc_;
+  SendStatus send_status_;
+};
+
+class GetClient : public ClientBase {
+ public:
+  GetClient(std::shared_ptr<grpc::CompletionQueue> cq,
+            std::shared_ptr<grpc::Channel> ch, std::string ep) {
+    cq_ = cq;
+    ch_ = ch;
+    ep_ = ep;
+    stub_ = sendrecv::SendRecvService::NewStub(ch);
+    context_.reset(new grpc::ClientContext());
+    status_.reset(new grpc::Status());
+    reply_.reset(new sendrecv::VoidMessage());
+  }
+
+  virtual ~GetClient() {}
+
+  bool Call(const framework::Scope* scope, const std::string& name,
+            int64_t time_out) {
+    return true;
+  }
+
+  bool ProcRet() { return true; }
+
+  grpc::Status* GetStatus() { return status_.get(); }
+
+  SendStatus GetSendStatus() { return send_status_; }
+
+ private:
+  std::shared_ptr<grpc::CompletionQueue> cq_;
+  std::shared_ptr<grpc::Channel> ch_;
+  std::string ep_;
+  std::unique_ptr<sendrecv::SendRecvService::Stub> stub_;
+  std::unique_ptr<grpc::ClientContext> context_;
+  std::unique_ptr<grpc::Status> status_;
+  std::unique_ptr<sendrecv::VoidMessage> reply_;
+  std::unique_ptr<grpc::ClientAsyncResponseReader<sendrecv::VoidMessage>> rpc_;
+  SendStatus send_status_;
+};
+
+class RPCClients {
+ public:
+  bool AsyncSendVariable(const std::string& ep, const framework::Scope* scope,
+                         const std::string& var_name, int64_t time_out) {
+    auto ch = _Get(ep);
+
+    auto c = std::shared_ptr<ClientBase>(new SendClient(cq_, ch, ep));
+    clients_[c.get()] = c;
+
+    return c->Call(scope, var_name, time_out);
+  }
+  bool AsyncGetVariable(const std::string& ep, const framework::Scope* scope,
+                        const std::string& var_name, int64_t time_out) {
+    auto ch = _Get(ep);
+
+    auto c = std::shared_ptr<ClientBase>(new GetClient(cq_, ch, ep));
+    clients_[c.get()] = c;
+
+    return c->Call(scope, var_name, time_out);
+  }
+
+  bool Proceed(SendStatus& s) {
+    void* tag = NULL;
+    bool ok = false;
+
+    if (!cq_->Next(&tag, &ok)) {
+      return false;
+    }
+    GPR_ASSERT(ok);
+    PADDLE_ENFORCE(tag);
+
+    auto cls = static_cast<ClientBase*>(tag);
+    clients_.erase(tag);
+    const grpc::Status& status = *cls->GetStatus();
+    s = cls->GetSendStatus();
+
+    s.end = std::chrono::system_clock::now();
+    if (status.ok()) {
+      cls->ProcRet();
+      return true;
+    }
+
+    if (status.error_code() == grpc::StatusCode::DEADLINE_EXCEEDED) {
+      s.error = "rpc timed out";
+    } else {
+      std::ostringstream stringStream;
+      stringStream << "rpc failed because:" << status.error_code();
+      s.error = stringStream.str();
+    }
+
+    return true;
+  }
+
+ private:
+  std::shared_ptr<grpc::Channel> _Get(const std::string& ep) {
+    auto it = channels_.find(ep);
+    if (it != channels_.end()) {
+      return it->second;
+    }
+
+    auto ch = std::shared_ptr<grpc::Channel>(
+        grpc::CreateChannel(ep, grpc::InsecureChannelCredentials()));
+
+    channels_[ep] = ch;
+    return ch;
+  }
+
+ private:
+  std::shared_ptr<grpc::CompletionQueue> cq_;
+  std::map<std::string, std::shared_ptr<grpc::Channel>> channels_;
+
+  // even if user don't call proceed,
+  // the clientbase will release automaticly
+  std::map<void*, std::shared_ptr<ClientBase>> clients_;
+};
+
+}  // namespace detail
+}  // namespace operators
+}  // namespace paddle;
--- paddle/operators/send_op.cc
+++ paddle/operators/send_op.cc
@@ -19,72 +19,104 @@ limitations under the License. */
 #include "paddle/framework/lod_tensor.h"
 #include "paddle/framework/op_registry.h"
 
-#include "paddle/operators/detail/grpc_client.h"
-#include "paddle/operators/detail/send_recv_impl.h"
-#include "paddle/operators/detail/simple_block_queue.h"
+//#include "paddle/operators/detail/grpc_client.h"
+//#include "paddle/operators/detail/send_recv_impl.h"
+//#include "paddle/operators/detail/simple_block_queue.h"
+#include <future>
+#include "paddle/operators/detail/grpc_client2.h"
 
 namespace paddle {
 namespace operators {
 
+bool Send(const std::vector<std::string>& ep, const framework::Scope* scope,
+          const std::vector<std::string>& ins, int64_t time_out) {
+  detail::RPCClients c;
+  for (size_t i = 0; i < ins.size(); i++) {
+    c.AsyncSendVariable(ep[i], scope, ins[i], time_out);
+  }
+
+  bool ok = true;
+  while (true) {
+    detail::SendStatus s;
+    if (!c.Proceed(s)) {
+      LOG(ERROR) << "Send meets CompletionQueue error";
+      return false;
+    }
+
+    // TODO(gongwb): add more retry?
+    if (s.error != "") {
+      ok = false;
+      LOG(ERROR) << "sync update variable error:" << s.String();
+    } else {
+      VLOG(3) << "sync update variable ok:" << s.String();
+    }
+  }
+
+  return ok;
+}
+
+bool Get(const std::vector<std::string>& ep, const framework::Scope* scope,
+         const std::vector<std::string>& ins, int64_t time_out) {
+  detail::RPCClients c;
+  for (size_t i = 0; i < ins.size(); i++) {
+    c.AsyncGetVariable(ep[i], scope, ins[i], time_out);
+  }
+
+  bool ok = true;
+  while (true) {
+    detail::SendStatus s;
+    if (!c.Proceed(s)) {
+      LOG(ERROR) << "Get meets CompletionQueue error";
+      return false;
+    }
+
+    // TODO(gongwb): add more retry?
+    if (s.error != "") {
+      ok = false;
+      LOG(ERROR) << "sync update variable error:" << s.String();
+    } else {
+      VLOG(3) << "sync update variable ok:" << s.String();
+    }
+  }
+
+  return ok;
+}
+
 // TODO(gongwb): add more attrs to support more send pattern.
 class SendOp : public framework::OperatorBase {
  public:
-  SendOp(const std::string &type, const framework::VariableNameMap &inputs,
-         const framework::VariableNameMap &outputs,
-         const framework::AttributeMap &attrs)
-      : OperatorBase(type, inputs, outputs, attrs) {
-    // init client when the operator is created at runtime.
-    std::vector<std::string> endpoints =
-        Attr<std::vector<std::string>>("endpoints");
+  SendOp(const std::string& type, const framework::VariableNameMap& inputs,
+         const framework::VariableNameMap& outputs,
+         const framework::AttributeMap& attrs)
+      : OperatorBase(type, inputs, outputs, attrs) {}
 
-    client_.AddEndPoint(endpoints);
+  bool SyncUpdate(const std::vector<std::string>& eps,
+                  const framework::Scope* scope,
+                  const std::vector<std::string>& ins,
+                  const std::vector<std::string>& outs) const {
+    int64_t send_timeout = 5000 * 1000;
+    int64_t get_timeout = 1800 * 1000;
+    auto send_thread = std::async(Send, eps, scope, ins, send_timeout);
+    auto get_thread = std::async(Get, eps, scope, outs, get_timeout);
+
+    auto send_ok = send_thread.get();
+    auto get_ok = get_thread.get();
+
+    return (send_ok && get_ok);
   }
 
-  void LogErrors(const std::vector<detail::SendStatus> &status) const {
-    for (auto &s : status) {
-      if (s.error != "") {
-        LOG(ERROR) << "sync update variable error:" << s.String();
-      }
-    }
-  }
-
-  void Run(const framework::Scope &scope,
-           const platform::Place &dev_place) const override {
+  void Run(const framework::Scope& scope,
+           const platform::Place& dev_place) const override {
     auto ins = Inputs("X");
     auto outs = Outputs("Out");
     std::vector<std::string> epmap = Attr<std::vector<std::string>>("epmap");
-
-    std::vector<detail::VarHandle> in_vars;
-    std::vector<detail::VarHandle> out_vars;
-    for (int i = 0; i < int(ins.size()); i++) {
-      detail::VarHandle in_var;
-      in_var.name = ins[i];
-      in_var.endpoint = epmap[i];
-      in_vars.push_back(in_var);
-
-      detail::VarHandle out_var;
-      in_var.name = ins[i];
-      in_var.endpoint = epmap[i];
-      out_vars.push_back(out_var);
-    }
-
-    std::vector<detail::SendStatus> in_status;
-    std::vector<detail::SendStatus> out_status;
-    bool ok =
-        client_.SyncUpdate(&scope, in_vars, in_status, out_vars, out_status);
-    if (!ok) {
-      LogErrors(in_status);
-      LogErrors(out_status);
-    }
+    SyncUpdate(epmap, &scope, ins, outs);
   }
-
- protected:
-  mutable detail::AsyncGRPCClient client_;
 };
 
 class SendOpMaker : public framework::OpProtoAndCheckerMaker {
  public:
-  SendOpMaker(OpProto *proto, OpAttrChecker *op_checker)
+  SendOpMaker(OpProto* proto, OpAttrChecker* op_checker)
       : OpProtoAndCheckerMaker(proto, op_checker) {
     AddInput("X", "(Tensor) Input tensor to be send").AsDuplicable();
     AddOutput("Out", "(Tensor) Output tensor to get from server")
