643ff03fbc5e100176070760c9c73f11a695caf3
Yancey <yanxu05@baidu.com>
2018-01-05 18:51:49 +0800

    capi package (#7237)

--- paddle/scripts/docker/build.sh
+++ paddle/scripts/docker/build.sh
@@ -193,6 +193,25 @@ EOF
 EOF
 }
 
+function gen_capi_package() {
+  if [[ ${WITH_C_API} == "ON" ]]; then
+    install_prefix="/paddle/build/capi_output"
+    rm -rf $install_prefix
+
+    make DESTDIR="$install_prefix" install
+
+    if [[ ${WITH_MKL:-OFF} == "ON" ]]; then
+      find ./third_party/install -name 'libmklml_gnu.so' -exec cp {} $install_prefix/usr/local/lib \;
+      find ./third_party/install -name 'libmklml_intel.so' -exec cp {} $install_prefix/usr/local/lib \;
+      cp -P ./third_party/install/mkldnn/lib/* $install_prefix/usr/local/lib/
+    fi
+
+    find ./third_party/install -name 'libiomp5.so' -exec cp {} $install_prefix/usr/local/lib \;
+    cd $install_prefix/usr/local
+    ls | egrep -v "^Found.*item$" | xargs tar /paddle/build/paddle.tgz
+  fi
+}
+
 set -xe
 
 cmake_gen ${PYTHON_ABI:-""}
@@ -200,6 +219,11 @@ run_build
 run_test
 gen_docs
 gen_dockerfile
+gen_capi_package
 
-printf "If you need to install PaddlePaddle in develop docker image,"
-printf "please make install or pip install build/python/dist/*.whl.\n"
+if [[ ${WITH_C_API:-OFF} == "ON" ]]; then
+  printf "PaddlePaddle C-API libraries was generated on build/paddle.tgz\n" 
+else
+  printf "If you need to install PaddlePaddle in develop docker image,"
+  printf "please make install or pip install build/python/dist/*.whl.\n"
+fi
