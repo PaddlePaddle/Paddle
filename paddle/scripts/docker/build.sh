d59c3e12359e9f5a2f181ee920314320db29dbac
tensor-tang <jian.j.tang@intel.com>
2018-01-05 23:04:57 +0800

    package right mkldnn and mklml libs if enabled in capi

--- paddle/scripts/docker/build.sh
+++ paddle/scripts/docker/build.sh
@@ -197,16 +197,7 @@ function gen_capi_package() {
   if [[ ${WITH_C_API} == "ON" ]]; then
     install_prefix="/paddle/build/capi_output"
     rm -rf $install_prefix
-
     make DESTDIR="$install_prefix" install
-
-    if [[ ${WITH_MKL:-OFF} == "ON" ]]; then
-      find ./third_party/install -name 'libmklml_gnu.so' -exec cp {} $install_prefix/usr/local/lib \;
-      find ./third_party/install -name 'libmklml_intel.so' -exec cp {} $install_prefix/usr/local/lib \;
-      cp -P ./third_party/install/mkldnn/lib/* $install_prefix/usr/local/lib/
-    fi
-
-    find ./third_party/install -name 'libiomp5.so' -exec cp {} $install_prefix/usr/local/lib \;
     cd $install_prefix/usr/local
     ls | egrep -v "^Found.*item$" | xargs tar -cf /paddle/build/paddle.tgz
   fi
