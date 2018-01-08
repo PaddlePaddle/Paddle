7b5ec748970a6c18705cd74604327617505499be
Luo Tao <luotao02@baidu.com>
2018-01-08 12:56:02 +0800

    max_index is not used in sequence_pool op when pool_type is not max

--- python/paddle/v2/fluid/layers/nn.py
+++ python/paddle/v2/fluid/layers/nn.py
@@ -807,13 +807,18 @@ def sequence_pool(input, pool_type, **kwargs):
     helper = LayerHelper('sequence_pool', input=input, **kwargs)
     dtype = helper.input_dtype()
     pool_out = helper.create_tmp_variable(dtype)
-    max_index = helper.create_tmp_variable(dtype)
+    if (pool_type == "max"):
+        outputs = {
+            "Out": pool_out,
+            "MaxIndex": helper.create_tmp_variable(dtype)
+        }
+    else:
+        outputs = {"Out": pool_out}
 
     helper.append_op(
         type="sequence_pool",
         inputs={"X": input},
-        outputs={"Out": pool_out,
-                 "MaxIndex": max_index},
+        outputs=outputs,
         attrs={"pooltype": pool_type.upper()})
 
     return pool_out
