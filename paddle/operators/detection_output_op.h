5ec96f34ee18062a50e1c7549f8e461100bb5d34
work <work@yq01-idl-gpu-online20.yq01.baidu.com>
2018-01-10 17:18:00 +0800

    fix detection_output_op.h had a funline > 100

--- paddle/operators/detection_output_op.h
+++ paddle/operators/detection_output_op.h
@@ -19,6 +19,7 @@ limitations under the License. */
 #include "paddle/operators/math/math_function.h"
 #include "paddle/operators/math/softmax.h"
 #include "paddle/operators/strided_memcpy.h"
+
 namespace paddle {
 namespace operators {
 template <typename DeviceContext, typename T>
@@ -47,13 +48,38 @@ inline void transpose_fun(const framework::ExecutionContext& context,
     offset += in_p_tensor_transpose.dims()[4] * src_stride[4];
   }
 }
+
+template <typename T>
+inline void decode_bboxer(
+    std::vector<std::vector<operators::math::BBox<T>>>& all_de_bboxes,
+    size_t num_p, const T* p_data, T* loc_data, size_t batch_size) {
+  for (size_t n = 0; n < batch_size; ++n) {
+    std::vector<operators::math::BBox<T>> decoded_bboxes;
+    for (size_t i = 0; i < num_p; ++i) {
+      size_t p_offset = i * 8;
+      size_t loc_pred_offset = n * num_p * 4 + i * 4;
+      std::vector<math::BBox<T>> prior_bbox_vec;
+      math::GetBBoxFromPriorData<T>(p_data + p_offset, 1, prior_bbox_vec);
+      std::vector<std::vector<T>> prior_bbox_var;
+      math::GetBBoxVarFromPriorData<T>(p_data + p_offset, 1, prior_bbox_var);
+      std::vector<T> loc_pred_data;
+      for (size_t j = 0; j < 4; ++j)
+        loc_pred_data.push_back(*(loc_data + loc_pred_offset + j));
+      math::BBox<T> bbox = math::DecodeBBoxWithVar<T>(
+          prior_bbox_vec[0], prior_bbox_var[0], loc_pred_data);
+      decoded_bboxes.push_back(bbox);
+    }
+    all_de_bboxes.push_back(decoded_bboxes);
+  }
+}
+
 template <typename DeviceContext, typename T>
 class DetectionOutputKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& context) const override {
     const framework::Tensor* in_loc = context.Input<framework::Tensor>("Loc");
     const framework::Tensor* in_conf = context.Input<framework::Tensor>("Conf");
-    const framework::Tensor* in_p =
+    const framework::Tensor* in_pb =
         context.Input<framework::Tensor>("PriorBox");
     auto* out = context.Output<framework::Tensor>("Out");
     int classes = context.template Attr<int>("num_classes");
@@ -61,7 +87,7 @@ class DetectionOutputKernel : public framework::OpKernel<T> {
     int nms_top_k = context.template Attr<int>("nms_top_k");
     int label_id = context.template Attr<int>("background_label_id");
     float nms_threshold = context.template Attr<float>("nms_threshold");
-    float c_threshold = context.template Attr<float>("confidence_threshold");
+    float conf_th = context.template Attr<float>("confidence_threshold");
     size_t batch_size = in_conf->dims()[1];
     int conf_sum_size = in_conf->numel();
     std::vector<int64_t> softmax_vec({conf_sum_size / classes, classes});
@@ -74,58 +100,41 @@ class DetectionOutputKernel : public framework::OpKernel<T> {
                                 in_conf->dims()[2] * in_conf->dims()[0]});
     framework::DDim loc_shape(framework::make_ddim(l_vec));
     framework::DDim conf_shape(framework::make_ddim(c_vec));
-    framework::Tensor loc_tr;
-    framework::Tensor conf_tr;
-    loc_tr.mutable_data<T>(loc_shape, context.GetPlace());
-    conf_tr.mutable_data<T>(conf_shape, context.GetPlace());
+    framework::Tensor loc;
+    framework::Tensor conf;
+    loc.mutable_data<T>(loc_shape, context.GetPlace());
+    conf.mutable_data<T>(conf_shape, context.GetPlace());
     framework::Tensor loc_cpu;
     framework::Tensor conf_cpu;
     framework::Tensor priorbox_cpu;
-    const T* p_data = in_p->data<T>();
-    transpose_fun<DeviceContext, T>(context, *in_loc, &loc_tr);
-    transpose_fun<DeviceContext, T>(context, *in_conf, &conf_tr);
-    conf_tr.Resize(conf_shape_softmax);
+    const T* p_data = in_pb->data<T>();
+    transpose_fun<DeviceContext, T>(context, *in_loc, &loc);
+    transpose_fun<DeviceContext, T>(context, *in_conf, &conf);
+    conf.Resize(conf_shape_softmax);
     math::SoftmaxFunctor<DeviceContext, T>()(
-        context.template device_context<DeviceContext>(), &conf_tr, &conf_tr);
-    T* loc_data = loc_tr.data<T>();
-    T* conf_data = conf_tr.data<T>();
+        context.template device_context<DeviceContext>(), &conf, &conf);
+    T* loc_data = loc.data<T>();
+    T* conf_data = conf.data<T>();
     if (platform::is_gpu_place(context.GetPlace())) {
-      loc_cpu.mutable_data<T>(loc_tr.dims(), platform::CPUPlace());
-      framework::Copy(loc_tr, platform::CPUPlace(),
-                          context.device_context(), &loc_cpu);
+      loc_cpu.mutable_data<T>(loc.dims(), platform::CPUPlace());
+      framework::Copy(loc, platform::CPUPlace(), context.device_context(),
+                      &loc_cpu);
       loc_data = loc_cpu.data<T>();
-      conf_cpu.mutable_data<T>(conf_tr.dims(), platform::CPUPlace());
-      framework::Copy(conf_tr, platform::CPUPlace(),
-                          context.device_context(), &conf_cpu);
+      conf_cpu.mutable_data<T>(conf.dims(), platform::CPUPlace());
+      framework::Copy(conf, platform::CPUPlace(), context.device_context(),
+                      &conf_cpu);
       conf_data = conf_cpu.data<T>();
-      priorbox_cpu.mutable_data<T>(in_p->dims(), platform::CPUPlace());
-      framework::Copy(*in_p, platform::CPUPlace(), context.device_context(),
-                          &priorbox_cpu);
+      priorbox_cpu.mutable_data<T>(in_pb->dims(), platform::CPUPlace());
+      framework::Copy(*in_pb, platform::CPUPlace(), context.device_context(),
+                      &priorbox_cpu);
       p_data = priorbox_cpu.data<T>();
     }
-    size_t num_p = in_p->numel() / 8;
+    size_t num_p = in_pb->numel() / 8;
     std::vector<std::vector<operators::math::BBox<T>>> all_de_bboxes;
-    for (size_t n = 0; n < batch_size; ++n) {
-      std::vector<operators::math::BBox<T>> decoded_bboxes;
-      for (size_t i = 0; i < num_p; ++i) {
-        size_t p_offset = i * 8;
-        size_t loc_pred_offset = n * num_p * 4 + i * 4;
-        std::vector<math::BBox<T>> prior_bbox_vec;
-        math::GetBBoxFromPriorData<T>(p_data + p_offset, 1, prior_bbox_vec);
-        std::vector<std::vector<T>> prior_bbox_var;
-        math::GetBBoxVarFromPriorData<T>(p_data + p_offset, 1, prior_bbox_var);
-        std::vector<T> loc_pred_data;
-        for (size_t j = 0; j < 4; ++j)
-          loc_pred_data.push_back(*(loc_data + loc_pred_offset + j));
-        math::BBox<T> bbox = math::DecodeBBoxWithVar<T>(
-            prior_bbox_vec[0], prior_bbox_var[0], loc_pred_data);
-        decoded_bboxes.push_back(bbox);
-      }
-      all_de_bboxes.push_back(decoded_bboxes);
-    }
+    decode_bboxer<T>(all_de_bboxes, num_p, p_data, loc_data, batch_size);
     std::vector<std::map<size_t, std::vector<size_t>>> all_indices;
     int num_kept = math::GetDetectionIndices<T>(
-        conf_data, num_p, classes, label_id, batch_size, c_threshold, nms_top_k,
+        conf_data, num_p, classes, label_id, batch_size, conf_th, nms_top_k,
         nms_threshold, top_k, all_de_bboxes, &all_indices);
     if (num_kept <= 0) {
       std::vector<int64_t> out_shape_vec({0, 0});
