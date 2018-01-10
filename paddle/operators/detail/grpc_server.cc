09e7c828df14a9cf60baddfaf79c9dbf26f26316
gongweibao <weibao.gong@gmail.com>
2018-01-10 02:03:35 +0000

    clean code

--- paddle/operators/detail/grpc_server.cc
+++ paddle/operators/detail/grpc_server.cc
@@ -12,8 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "grpc_server.h"
-#include <future>
+#include "paddle/operators/detail/grpc_server.h"
 
 using grpc::ServerAsyncResponseWriter;
 
