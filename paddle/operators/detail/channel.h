3b5f4bbc3d346dd56da361a7010a38c3f9a8c26b
chengduoZH <zhaochengduo@163.com>
2018-01-11 14:46:46 +0800

    refine Channel

--- paddle/operators/detail/channel.h
+++ paddle/operators/detail/channel.h
@@ -11,14 +11,11 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
+
 #pragma once
 #include <condition_variable>
 #include <mutex>
 #include <queue>
-#include <vector>
-
-#include "paddle/framework/lod_tensor.h"
-#include "paddle/platform/place.h"
 
 namespace paddle {
 namespace operators {
@@ -27,47 +24,32 @@ namespace detail {
 template <typename T>
 class Channel {
  public:
-  explicit Channel(std::size_t capacity, std::size_t bytes_limit)
-      : capacity_(capacity), bytes_limit_(bytes_limit), current_bytes_(0) {}
+  explicit Channel(std::size_t capacity) : capacity_(capacity) {}
 
-  void Put(ChannelElement* buffer_element) {
+  void Send(T* channel_element) {
     std::unique_lock<std::mutex> lock(mu_);
 
-    std::size_t element_bytes = GetElementBytes(*buffer_element);
-
-    PADDLE_ENFORCE(bytes_limit_ > 0 && element_bytes > bytes_limit_,
-                   "*Attempted to insert T with combined size of %d "
-                   "bytes into Chnnel with a memory limit of %d.",
-                   element_bytes, bytes_limit_);
-
     if (IsBounded()) {
-      full_cond_var_.wait(lock, [element_bytes, this]() {
-        bool bytes_limit_valid =
-            bytes_limit_ > 0 ? !IsExceedMemoryLimit(element_bytes) : true;
+      full_cond_var_.wait(lock, [this]() {
         bool capacity_valid = capacity_ > 0 ? !IsCapacityFull() : true;
-
-        return capacity_valid && bytes_limit_valid;
+        return capacity_valid;
       });
     }
-
-    current_bytes_ += element_bytes;
-    channel_.push_back(std::move(*buffer_element));
+    channel_.push_back(std::move(*channel_element));
 
     lock.unlock();
     empty_cond_var_.notify_all();
   }
 
-  void Get(ChannelElement* buffer_element) {
+  T* Receive() {
     std::unique_lock<std::mutex> lock(mu_);
-
     empty_cond_var_.wait(lock, [this]() { return !channel_.empty(); });
 
-    *buffer_element = std::move(channel_.front());
+    T* channel_element = std::move(channel_.front());
     channel_.pop_front();
 
-    current_bytes_ -= GetElementBytes(*buffer_element);
-
-    NotifyInserters(&lock);
+    NotifyAllSenders(&lock);
+    return channel_element;
   }
 
   size_t Size() {
@@ -78,44 +60,28 @@ class Channel {
   void Clear() {
     std::unique_lock<std::mutex> lock(mu_);
     channel_.clear();
-    current_bytes_ = 0;
 
-    NotifyInserters(&lock);
+    NotifyAllSenders(&lock);
   }
 
  private:
-  using ChannelElement = std::vector<T>;
-
   std::size_t capacity_;
-  std::size_t bytes_limit_;
-  std::size_t current_bytes_;
   std::mutex mu_;
   std::condition_variable empty_cond_var_;
   std::condition_variable full_cond_var_;
-  std::deque<ChannelElement> channel_;
+  std::deque<T> channel_;
 
  private:
-  void NotifyInserters(std::unique_lock<std::mutex>* lock) {
+  void NotifyAllSenders(std::unique_lock<std::mutex>* lock) {
     if (IsBounded()) {
       lock->unlock();
       full_cond_var_.notify_all();
     }
   }
 
-  bool IsBounded() const { return capacity_ > 0 || bytes_limit_ > 0; }
+  bool IsBounded() const { return capacity_ > 0; }
 
   bool IsCapacityFull() const { return channel_.size() >= capacity_; }
-
-  bool IsExceedMemoryLimit(std::size_t bytes) const {
-    return bytes + current_bytes_ > bytes_limit_;
-  }
-
-  std::size_t GetElementBytes(const ChannelElement& tuple) {
-    return std::accumulate(tuple.begin(), tuple.end(), 0,
-                           [](const std::size_t& cal, const T& ele) {
-                             return cal + ele.memory_size();
-                           });
-  }
 };
 
 }  // namespace detail
