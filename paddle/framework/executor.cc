5c4eab944fe4734d82029e29eb6c2e836523092d
qijun <qijun1994@hotmail.com>
2018-01-12 13:13:21 +0800

    init

--- paddle/framework/executor.cc
+++ paddle/framework/executor.cc
@@ -117,6 +117,8 @@ void Executor::Run(const ProgramDesc& pdesc, Scope* scope, int block_id,
     auto op = paddle::framework::OpRegistry::CreateOp(*op_desc);
     VLOG(3) << op->DebugStringEx(local_scope);
     op->Run(*local_scope, place_);
+    LOG(INFO) << "Memory used after operator " + op->Type() + " running: "
+              << memory::memory_usage(place_);
     if (FLAGS_check_nan_inf) {
       for (auto& vname : op->OutputVars(true)) {
         auto* var = local_scope->FindVar(vname);
@@ -130,6 +132,8 @@ void Executor::Run(const ProgramDesc& pdesc, Scope* scope, int block_id,
   if (create_vars && create_local_scope) {
     scope->DeleteScope(local_scope);
   }
+  LOG(INFO) << "Memory used after deleting local scope: "
+            << memory::memory_usage(place_);
 }
 
 }  // namespace framework
