adc26dffa9dac81bd93c88d70f0ab66fcdcc81f0
fengjiayi <fengjiayi@baidu.com>
2018-01-15 10:36:09 +0800

    developing GradientClipByGlobalNorm

--- python/paddle/v2/fluid/layers/ops.py
+++ python/paddle/v2/fluid/layers/ops.py
@@ -1,23 +1,15 @@
 from ..registry import register_layer
 
 __activations__ = [
-    'abs', 'tanh', 'sigmoid', 'relu', 'sqrt', 'ceil', 'floor', 'log', 'round'
+    'abs', 'tanh', 'sigmoid', 'relu', 'sqrt', 'ceil', 'floor', 'log', 'round',
+    'pow'
 ]
 
 __all__ = [
-    'mean',
-    'mul',
-    'reshape',
-    'scale',
-    'transpose',
-    'sigmoid_cross_entropy_with_logits',
-    'elementwise_add',
-    'elementwise_div',
-    'elementwise_sub',
-    'elementwise_mul',
-    'clip',
-    'clip_by_norm',
-    'sequence_softmax',
+    'mean', 'mul', 'reshape', 'scale', 'transpose',
+    'sigmoid_cross_entropy_with_logits', 'elementwise_add', 'elementwise_div',
+    'elementwise_sub', 'elementwise_mul', 'clip', 'clip_by_norm',
+    'sequence_softmax', 'reduce_sum'
 ] + __activations__
 
 for _OP in set(__all__):
