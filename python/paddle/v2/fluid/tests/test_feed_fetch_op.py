05a3a8832700775b0cc72483018ddf92584bbdcc
dzhwinter <dzhwinter@gmail.com>
2018-01-16 18:35:52 -0800

    "add feed fetch op test"

--- python/paddle/v2/fluid/tests/test_feed_fetch_op.py
+++ python/paddle/v2/fluid/tests/test_feed_fetch_op.py
@@ -11,12 +11,14 @@
 #WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #See the License for the specific language governing permissions and
 #limitations under the License.
-import paddle.v2.fluid.core as core
 import unittest
 import numpy as np
 
+import paddle.v2.fluid as fluid
+import paddle.v2.fluid.core as core
 
-class TestFeedFetch(unittest.TestCase):
+
+class TestFeedFetchOp(unittest.TestCase):
     def test_feed_fetch_cpu(self):
         scope = core.Scope()
         place = core.CPUPlace()
@@ -38,28 +40,6 @@ class TestFeedFetch(unittest.TestCase):
         output_array = np.array(output_tensor)
         self.assertEqual(3, output_array[0, 0, 0])
         self.assertEqual(10, output_array[3, 3, 5])
-
-    # def test_feed_fetch_gpu(self):
-    #     scope = core.Scope()
-    #     place = core.CUDAPlace()
-    #     input_array = np.ones((4, 4, 6)).astype("float32")
-    #     input_array[0, 0, 0] = 3
-    #     input_array[3, 3, 5] = 10
-    #     input_tensor = core.LoDTensor([[0, 2, 4]])
-    #     input_tensor.set(input_array, place)
-
-    #     core.set_feed_variable(scope, input_tensor, "feed", 0)
-
-    #     output_tensor = core.get_fetch_variable(scope, "feed", 0)
-
-    #     output_lod = output_tensor.lod()
-    #     self.assertEqual(0, output_lod[0][0])
-    #     self.assertEqual(2, output_lod[0][1])
-    #     self.assertEqual(4, output_lod[0][2])
-
-    #     output_array = np.array(output_tensor)
-    #     self.assertEqual(3, output_array[0, 0, 0])
-    #     self.assertEqual(10, output_array[3, 3, 5])
 
 
 if __name__ == "__main__":
