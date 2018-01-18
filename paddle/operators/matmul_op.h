259858b41bce74e079503b33cf93ed4e48da9fdb
chengduoZH <zhaochengduo@163.com>
2018-01-18 20:42:07 +0800

    modify doc

--- paddle/operators/matmul_op.h
+++ paddle/operators/matmul_op.h
@@ -138,7 +138,7 @@ class MatMulGradKernel : public framework::OpKernel<T> {
     }
 
     int batch_count = 0;
-    //
+    // The previous Rank-2 dimensions are accumulated on the batch_count.
     if (x_dims.size() > 3) {
       batch_count = accumulate(x_dims.begin(), x_dims.end() - 2, 1,
                                std::multiplies<int>());
