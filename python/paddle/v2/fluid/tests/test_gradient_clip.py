d97483ca69d09b89414a2e7b1b9f84b0b81a22c6
fengjiayi <fengjiayi@baidu.com>
2018-01-22 12:50:45 +0800

    update

--- python/paddle/v2/fluid/tests/test_gradient_clip.py
+++ python/paddle/v2/fluid/tests/test_gradient_clip.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import numpy as np
 import paddle.v2 as paddle
 import paddle.v2.fluid as fluid
