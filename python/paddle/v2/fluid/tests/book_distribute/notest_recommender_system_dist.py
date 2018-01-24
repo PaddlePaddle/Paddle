c9edf959db16ee1fe60977a1b0a9ad0f2e48c5af
Abhinav Arora <aroraabhinav@baidu.com>
2018-01-24 10:26:44 -0800

    Addressing code review feedback

--- python/paddle/v2/fluid/tests/book_distribute/notest_recommender_system_dist.py
+++ python/paddle/v2/fluid/tests/book_distribute/notest_recommender_system_dist.py
@@ -27,94 +27,72 @@ PASS_NUM = 100
 
 
 def get_usr_combined_features():
-
     USR_DICT_SIZE = paddle.dataset.movielens.max_user_id() + 1
-
     uid = layers.data(name='user_id', shape=[1], dtype='int64')
-
     usr_emb = layers.embedding(
         input=uid,
         dtype='float32',
         size=[USR_DICT_SIZE, 32],
         param_attr='user_table',
         is_sparse=IS_SPARSE)
-
     usr_fc = layers.fc(input=usr_emb, size=32)
-
     USR_GENDER_DICT_SIZE = 2
 
     usr_gender_id = layers.data(name='gender_id', shape=[1], dtype='int64')
-
     usr_gender_emb = layers.embedding(
         input=usr_gender_id,
         size=[USR_GENDER_DICT_SIZE, 16],
         param_attr='gender_table',
         is_sparse=IS_SPARSE)
-
     usr_gender_fc = layers.fc(input=usr_gender_emb, size=16)
 
     USR_AGE_DICT_SIZE = len(paddle.dataset.movielens.age_table)
     usr_age_id = layers.data(name='age_id', shape=[1], dtype="int64")
-
     usr_age_emb = layers.embedding(
         input=usr_age_id,
         size=[USR_AGE_DICT_SIZE, 16],
         is_sparse=IS_SPARSE,
         param_attr='age_table')
-
     usr_age_fc = layers.fc(input=usr_age_emb, size=16)
 
     USR_JOB_DICT_SIZE = paddle.dataset.movielens.max_job_id() + 1
     usr_job_id = layers.data(name='job_id', shape=[1], dtype="int64")
-
     usr_job_emb = layers.embedding(
         input=usr_job_id,
         size=[USR_JOB_DICT_SIZE, 16],
         param_attr='job_table',
         is_sparse=IS_SPARSE)
-
     usr_job_fc = layers.fc(input=usr_job_emb, size=16)
 
     concat_embed = layers.concat(
         input=[usr_fc, usr_gender_fc, usr_age_fc, usr_job_fc], axis=1)
 
     usr_combined_features = layers.fc(input=concat_embed, size=200, act="tanh")
-
     return usr_combined_features
 
 
 def get_mov_combined_features():
-
     MOV_DICT_SIZE = paddle.dataset.movielens.max_movie_id() + 1
-
     mov_id = layers.data(name='movie_id', shape=[1], dtype='int64')
-
     mov_emb = layers.embedding(
         input=mov_id,
         dtype='float32',
         size=[MOV_DICT_SIZE, 32],
         param_attr='movie_table',
         is_sparse=IS_SPARSE)
-
     mov_fc = layers.fc(input=mov_emb, size=32)
 
     CATEGORY_DICT_SIZE = len(paddle.dataset.movielens.movie_categories())
-
     category_id = layers.data(name='category_id', shape=[1], dtype='int64')
-
     mov_categories_emb = layers.embedding(
         input=category_id, size=[CATEGORY_DICT_SIZE, 32], is_sparse=IS_SPARSE)
-
     mov_categories_hidden = layers.sequence_pool(
         input=mov_categories_emb, pool_type="sum")
 
     MOV_TITLE_DICT_SIZE = len(paddle.dataset.movielens.get_movie_title_dict())
-
     mov_title_id = layers.data(name='movie_title', shape=[1], dtype='int64')
-
     mov_title_emb = layers.embedding(
         input=mov_title_id, size=[MOV_TITLE_DICT_SIZE, 32], is_sparse=IS_SPARSE)
-
     mov_title_conv = nets.sequence_conv_pool(
         input=mov_title_emb,
         num_filters=32,
@@ -126,7 +104,6 @@ def get_mov_combined_features():
         input=[mov_fc, mov_categories_hidden, mov_title_conv], axis=1)
 
     mov_combined_features = layers.fc(input=concat_embed, size=200, act="tanh")
-
     return mov_combined_features
 
 
@@ -139,9 +116,7 @@ def model():
     scale_infer = layers.scale(x=inference, scale=5.0)
 
     label = layers.data(name='score', shape=[1], dtype='float32')
-
     square_cost = layers.square_error_cost(input=scale_infer, label=label)
-
     avg_cost = layers.mean(x=square_cost)
 
     return avg_cost
@@ -228,7 +203,9 @@ def main():
                                feed=func_feed(feeding, data, place),
                                fetch_list=[cost])
                 out = np.array(outs[0])
+                print("cost=" + str(out[0]))
                 if out[0] < 6.0:
+                    print("Training complete. Average cost is less than 6.0.")
                     # if avg cost less than 6.0, we think our code is good.
                     exit(0)
     else:
