9be862f2b6e155622ff97948f495922c6c16b68a
Yi Wang <yi.wang.2005@gmail.com>
2018-01-29 10:58:37 -0800

    Revert changes to channel_test.cc

--- paddle/framework/channel_test.cc
+++ paddle/framework/channel_test.cc
@@ -14,7 +14,6 @@ limitations under the License. */
 #include <gtest/gtest.h>
 
 #include "paddle/framework/channel.h"
-#include "threadpool.h"
 
 using paddle::framework::Channel;
 using paddle::framework::MakeChannel;
@@ -22,47 +21,5 @@ using paddle::framework::CloseChannel;
 
 TEST(Channel, MakeAndClose) {
   Channel<int>* ch = MakeChannel<int>(10);
-  CloseChannel(ch);
-}
-
-TEST(Channel, Buffered) {
-  Channel<int>* ch = MakeChannel<int>(10);
-
-  for (int i = 0; i < 10; ++i) {
-    ch->Send(&i);
-  }
-
-  int temp = -1;
-  for (int j = 0; j < 10; ++j) {
-    ch->Receive(&temp);
-    EXPECT_EQ(temp, j);
-  }
-  CloseChannel(ch);
-}
-
-TEST(Channel, MultiThread) {
-  namespace framework = paddle::framework;
-
-  const int capacity = 10;
-  Channel<int>* ch = MakeChannel<int>(capacity);
-
-  framework::ThreadPool* pool;
-  pool = framework::ThreadPool::GetInstance();
-
-  // Consumer
-  for (int i = 0; i < capacity; ++i) {
-    framework::Async([&ch]() {
-      int temp = -1;
-      ch->Receive(&temp);
-      EXPECT_GE(temp, -1);
-    });
-  }
-
-  // Producer
-  for (int i = 0; i < capacity; ++i) {
-    framework::Async([&ch, &i]() { ch->Send(&i); });
-  }
-
-  pool->Wait();
   CloseChannel(ch);
 }
