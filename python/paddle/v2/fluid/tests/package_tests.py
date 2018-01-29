9a47e931cfac7b0a11bb6f41559e80984a72247b
dzhwinter <dzhwinter@gmail.com>
2018-01-28 18:53:20 -0800

    "fix cmake"

--- python/paddle/v2/fluid/tests/package_tests.py
+++ python/paddle/v2/fluid/tests/package_tests.py
@@ -15,11 +15,15 @@
 import sys, os
 import re, io
 import inspect
-import tokenize
+import keyword
+import random
+import string
 '''
 NOTE(dzhwinter):
 inspect.getmembers(module) and inspect.isroutine, inspect.isfunction is
-useful. Consider paddle.core contains a lot symbols, it really cost a lot of time to inspect the source file. So I treat scripts as plain text, yeah, it's ugly.
+useful. Consider paddle.core contains a lot symbols,
+inspect cost a lot of time to inspect the source file.
+So I treat scripts as plain text.
 '''
 
 TEST_SINGLE_OPS = "test_\w+_op.py$"
@@ -36,7 +40,9 @@ def _pipeline(stream, functors):
 
 def _find_indent(stream):
     global INDENT, INSIDE_MAIN
+    INDENT = 2
     snippets = [x for x in stream.split("\n") if len(x.strip()) != 0]
+    snippets = iter(snippets)
     for line in snippets:
         if line.startswith("class"):
             next_line = next(snippets)
@@ -44,42 +50,62 @@ def _find_indent(stream):
     return stream
 
 
+VALID_VARIABLE = "^[a-zA-Z_][a-zA-Z0-9_]*"
+
+
 def _rename_dup(stream):
     '''
     renmae duplicated global variable, global function
     '''
-    global_names = {}
+    global_names = set()
 
     def tokenize(func):
-        assert (func.startswith("def"))
+        first_token = func.split()[0]
+        # parse global function
+        if keyword.iskeyword(first_token):
+            if first_token != "def":
+                return None
+        # parse global variable
+        if not re.match(VALID_VARIABLE, first_token):
+            return None
+
         p = 0  # current position
         left = 0
         right = len(func) - 1
         while p < len(func):
             if p + 1 < len(func):
                 next_char = func[p + 1]
-                if func[p] == ' ' and next_char.isalpha():
+                if func[p] == ' ' and next_char.isalpha() and left == 0:
                     left = p + 1
-            if func[p] == '(':
+            if func[p] == '(' or func[p] == ' ' and right == len(func) - 1:
                 right = p
-        return func[left:right - left + 1]
+            p += 1
+        return func[left:right]
 
     # python support declar function after using position,
     # so it must be two rounds
     for line in stream.split("\n"):
-        if not line.startswith(" ") and not line.startswith("class"):
+        if line.startswith("def"):
             token = tokenize(line)
-            global_names.add(token)
+            if token != None and token != '':
+                global_names.add(token)
 
     ans = ""
+    random_prefix = ''.join(
+        random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
     for line in stream.split("\n"):
-        for token in global_names:
-            ans += line.replace("__" + token) + "\n"
+        line_ans = line
+        if len(global_names) != 0:
+            for token in global_names:
+                line_ans = line_ans.replace(token,
+                                            "_" + random_prefix + "_" + token)
+        ans += line_ans + "\n"
     return ans
 
 
 def _remove_main(stream):
     global INDENT, INSIDE_MAIN
+    INSIDE_MAIN = False
     snippets = stream.split("\n")
     ans = ""
     for line in snippets:
@@ -90,23 +116,31 @@ def _remove_main(stream):
             continue
         if INSIDE_MAIN:
             ans += " " * INDENT + line + "\n"
+        else:
+            ans += line + "\n"
     return ans
+
+
+def _append_main(stream):
+    ans = '''
+if __name__ == "__main__":
+    unittest.main()
+'''
+    return stream + ans
 
 
 def main():
     files = os.listdir()
-    print(files)
     sout = ""
     trigger_files = []
     for f in files:
         if re.match(TEST_SINGLE_OPS, f):
-            print(f)
             trigger_files.append(f)
             original_contents = io.open(f, encoding="utf-8").read()
-            # sout += _pipeline(original_contents,
-            #                 [_find_indent, _rename_dup, _remove_main])
+            sout += _pipeline(original_contents,
+                              [_find_indent, _rename_dup, _remove_main])
 
-    print(" ".join(trigger_files) + " merged into single test.")
+    sout = _append_main(sout)
     sys.stdout.write(sout)
 
 
