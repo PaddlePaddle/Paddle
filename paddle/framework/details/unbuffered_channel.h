02764374ef45b0ae62e0acf2f5649253277822be
Kavya Srinet <kavyasrinet@baidu.com>
2018-01-31 15:54:52 -0800

    Adding the closed attribute

--- paddle/framework/details/unbuffered_channel.h
+++ paddle/framework/details/unbuffered_channel.h
@@ -15,7 +15,6 @@ limitations under the License. */
 #pragma once
 #include <atomic>
 #include <condition_variable>
-#include <deque>
 #include <mutex>
 
 #include "paddle/framework/channel.h"
@@ -30,21 +29,24 @@ class UnBuffered : public paddle::framework::Channel<T> {
   friend void paddle::framework::CloseChannel<T>(Channel<T>*);
 
  public:
-  std::mutex mu_ch_;
-  std::recursive_mutex mu_read_, mu_write_;
-  std::atomic<bool> reader_found_{false}, writer_found_{false};
-  std::condition_variable cv_channel_;
-  std::condition_variable_any cv_reader_, cv_writer_;
-  T* item{nullptr};
-
-  virtual void Send(T*);
-  virtual void Receive(T*);
-  virtual size_t Cap() { return 0; }
-  virtual void Close();
-  virtual ~UnBuffered();
+   virtual void Send(T*);
+   virtual void Receive(T*);
+   virtual size_t Cap() { return 0; }
+   virtual void Close();
+   virtual ~UnBuffered();
 
  private:
-  UnBuffered() {}
+   std::mutex mu_ch_;
+   std::recursive_mutex mu_read_, mu_write_;
+   std::atomic<bool> reader_found_{false}, writer_found_{false};
+   std::condition_variable cv_channel_;
+   std::condition_variable_any cv_reader_, cv_writer_;
+   T* item{nullptr};
+   std::atomic<bool> closed_{false};
+
+   UnBuffered(): closed_(false) {}
+
+   void NotifyAllSenders(std::unique_lock<std::mutex>*);
 };
 
 template <typename T>
@@ -52,12 +54,15 @@ void UnBuffered<T>::Send(T* data) {
   std::unique_lock<std::recursive_mutex> writer_lock(mu_write_);
   writer_found_ = true;
   std::unique_lock<std::recursive_mutex> cv_lock(mu_write_);
-  cv_writer_.wait(cv_lock, [this]() { return reader_found_ == true; });
-  {
-    std::unique_lock<std::mutex> channel_lock(mu_ch_);
-    item = data;
+  cv_writer_.wait(cv_lock,
+                  [this]() { return reader_found_ == true || closed_; });
+  if (!closed_) {
+    {
+      std::unique_lock<std::mutex> channel_lock(mu_ch_);
+      item = data;
+    }
+    cv_channel_.notify_one();
   }
-  cv_channel_.notify_one();
   writer_found_ = false;
 }
 
@@ -66,22 +71,44 @@ void UnBuffered<T>::Receive(T* data) {
   std::unique_lock<std::recursive_mutex> read_lock{mu_read_};
   reader_found_ = true;
   std::unique_lock<std::recursive_mutex> cv_lock{mu_read_};
-  cv_writer_.wait(cv_lock, [this]() { return writer_found_ == true; });
-  {
+  cv_reader_.wait(cv_lock,
+                  [this]() { return writer_found_ == true || closed_; });
+  if (!closed_){
     std::unique_lock<std::mutex> lock_ch{mu_ch_};
-    cv_channel_.wait(lock_ch, [this]() { return item != nullptr; });
-    *data = std::move(*item);
-    item = nullptr;
+    cv_channel_.wait(lock_ch,
+                     [this]() { return item != nullptr || closed_; });
+    if (!closed_){
+      *data = std::move(*item);
+      item = nullptr;
+      lock_ch.unlock();
+      cv_writer_.notify_one();
+    }
   }
-  cv_writer_.notify_one();
   reader_found_ = false;
 }
 
 template <typename T>
-void UnBuffered<T>::Close() {}
+void UnBuffered<T>::Close() {
+  std::unique_lock<std::mutex> lock(mu_ch_);
+  item = nullptr;
+  closed_ = true;
+  NotifyAllSenders(&lock);
+}
 
 template <typename T>
-UnBuffered<T>::~UnBuffered() {}
+UnBuffered<T>::~UnBuffered() {
+  std::unique_lock<std::mutex> lock(mu_ch_);
+  closed_ = true;
+  NotifyAllSenders(&lock);
+}
+
+template <typename T>
+void UnBuffered<T>::NotifyAllSenders(std::unique_lock<std::mutex>* lock) {
+  lock->unlock();
+  cv_writer_.notify_all();
+  cv_channel_.notify_all();
+  cv_reader_.notify_all();
+}
 
 }  // namespace details
 }  // namespace framework
