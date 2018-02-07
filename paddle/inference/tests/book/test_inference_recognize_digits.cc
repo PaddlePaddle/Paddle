83b46167007b4a2b938f39d19037802bdb566709
sidgoyal78 <vi.siddharth78@gmail.com>
2018-02-07 23:26:19 +0000

    Unify TestInference by keeping default param in template

--- paddle/inference/tests/book/test_inference_recognize_digits.cc
+++ paddle/inference/tests/book/test_inference_recognize_digits.cc
@@ -20,56 +20,6 @@ limitations under the License. */
 
 DEFINE_string(dirname, "", "Directory of the inference model.");
 
-template <typename Place, typename T, bool IsCombined>
-void TestInferenceWithCombine(
-    const std::string& dirname,
-    const std::vector<paddle::framework::LoDTensor*>& cpu_feeds,
-    std::vector<paddle::framework::LoDTensor*>& cpu_fetchs) {
-  // 1. Define place, executor and scope
-  auto place = Place();
-  auto executor = paddle::framework::Executor(place);
-  auto* scope = new paddle::framework::Scope();
-  std::unique_ptr<paddle::framework::ProgramDesc> inference_program;
-
-  LOG(INFO) << "Going to call load()" << std::endl;
-  // 2. Initialize the inference_program and load all parameters from file
-  if (IsCombined) {
-    // Hard-coding the names for combined params case
-    std::string prog_filename = "__model_combined__";
-    std::string param_filename = "__params_combined__";
-    inference_program = paddle::inference::Load(executor,
-                                                *scope,
-                                                dirname + "/" + prog_filename,
-                                                dirname + "/" + param_filename);
-  } else {
-    inference_program = paddle::inference::Load(executor, *scope, dirname);
-  }
-
-  // 3. Get the feed_target_names and fetch_target_names
-  const std::vector<std::string>& feed_target_names =
-      inference_program->GetFeedTargetNames();
-  const std::vector<std::string>& fetch_target_names =
-      inference_program->GetFetchTargetNames();
-
-  // 4. Prepare inputs: set up maps for feed targets
-  std::map<std::string, const paddle::framework::LoDTensor*> feed_targets;
-  for (size_t i = 0; i < feed_target_names.size(); ++i) {
-    // Please make sure that cpu_feeds[i] is right for feed_target_names[i]
-    feed_targets[feed_target_names[i]] = cpu_feeds[i];
-  }
-
-  // 5. Define Tensor to get the outputs: set up maps for fetch targets
-  std::map<std::string, paddle::framework::LoDTensor*> fetch_targets;
-  for (size_t i = 0; i < fetch_target_names.size(); ++i) {
-    fetch_targets[fetch_target_names[i]] = cpu_fetchs[i];
-  }
-
-  // 6. Run the inference program
-  executor.Run(*inference_program, scope, feed_targets, fetch_targets);
-
-  delete scope;
-}
-
 TEST(inference, recognize_digits) {
   if (FLAGS_dirname.empty()) {
     LOG(FATAL) << "Usage: ./example --dirname=path/to/your/model";
@@ -94,7 +44,7 @@ TEST(inference, recognize_digits) {
   cpu_fetchs1.push_back(&output1);
 
   // Run inference on CPU
-  TestInferenceWithCombine<paddle::platform::CPUPlace, float, false>(
+  TestInference<paddle::platform::CPUPlace, float>(
       dirname, cpu_feeds, cpu_fetchs1);
   LOG(INFO) << output1.dims();
 
@@ -104,7 +54,7 @@ TEST(inference, recognize_digits) {
   cpu_fetchs2.push_back(&output2);
 
   // Run inference on CUDA GPU
-  TestInferenceWithCombine<paddle::platform::CUDAPlace, float, false>(
+  TestInference<paddle::platform::CUDAPlace, float>(
       dirname, cpu_feeds, cpu_fetchs2);
   LOG(INFO) << output2.dims();
 
@@ -136,7 +86,7 @@ TEST(inference, recognize_digits_combine) {
   cpu_fetchs1.push_back(&output1);
 
   // Run inference on CPU
-  TestInferenceWithCombine<paddle::platform::CPUPlace, float, true>(
+  TestInference<paddle::platform::CPUPlace, float, true>(
       dirname, cpu_feeds, cpu_fetchs1);
   LOG(INFO) << output1.dims();
 
@@ -146,7 +96,7 @@ TEST(inference, recognize_digits_combine) {
   cpu_fetchs2.push_back(&output2);
 
   // Run inference on CUDA GPU
-  TestInferenceWithCombine<paddle::platform::CUDAPlace, float, true>(
+  TestInference<paddle::platform::CUDAPlace, float, true>(
       dirname, cpu_feeds, cpu_fetchs2);
   LOG(INFO) << output2.dims();
 
