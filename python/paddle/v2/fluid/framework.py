e9ddaaba9ccad0b2f2066ab70d5b31a7125fa7f8
Yang Yang <yangyang62@baidu.com>
2018-02-09 21:53:45 +0000

    disable ncclInit infer shape & var type

--- python/paddle/v2/fluid/framework.py
+++ python/paddle/v2/fluid/framework.py
@@ -490,7 +490,7 @@ class Operator(object):
             'feed', 'fetch', 'save', 'load', 'recurrent',
             'rnn_memory_helper_grad', 'conditional_block', 'while', 'send',
             'recv', 'listen_and_serv', 'parallel_do', 'save_combine',
-            'load_combine'
+            'load_combine', 'ncclInit'
         }
         if type not in no_kernel_op_set:
             self.desc.infer_var_type(self.block.desc)
