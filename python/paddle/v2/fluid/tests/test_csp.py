757ac4aa17ca79670c0bfc308d28d343be0840e5
Kavya Srinet <kavyasrinet@baidu.com>
2018-02-09 15:20:48 -0800

    Added an elementary test case for CSP

---
+++ python/paddle/v2/fluid/tests/test_csp.py
@@ -0,0 +1,39 @@
+#   Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import paddle.v2.fluid as fluid
+
+
+class TestCSPFramework(unittest.TestCase):
+    def helper(self, left, right):
+        fluid.send(left, 1 + fluid.recv(right))
+
+    def daisy_chain(self, n):
+        leftmost = fluid.make_channel(dtype=INT)
+        right = leftmost
+        left = leftmost
+        with fluid.while(steps=n):
+            right = fluid.make_channel(dtype=INT)
+            with fluid.go():
+                self.helper(left, right)
+            left = right
+
+        with fluid.go():
+            fluid.send(right, 1)
+        fluid.print(fluid.recv(leftmost))
+
+
+if __name__ == '__main__':
+    unittest.main()
