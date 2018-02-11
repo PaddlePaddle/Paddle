40a468ccd9032808ab3d293465a81227bee527d6
dangqingqing <dangqingqing@baidu.com>
2018-02-11 10:06:24 +0800

    Fix conflicts.

--- paddle/fluid/operators/target_assign_op.h
+++ paddle/fluid/operators/target_assign_op.h
@@ -106,7 +106,7 @@ class TargetAssignKernel : public framework::OpKernel<T> {
     int64_t k = x->dims()[2];
 
     auto x_lod = x->lod().back();
-    size_t* x_lod_data = x_lod.data(ctx.GetPlace());
+    size_t* x_lod_data = x_lod.MutableData(ctx.GetPlace());
 
     TargetAssignFunctor<T, WT> functor(x_data, match_idx_data, x_lod_data,
                                        mismatch_value, n, m, p, k, out_data,
@@ -121,7 +121,7 @@ class TargetAssignKernel : public framework::OpKernel<T> {
       PADDLE_ENFORCE_EQ(neg_indices->lod().size(), 1UL);
       const int* neg_idx_data = neg_indices->data<int>();
       auto neg_lod = neg_indices->lod().back();
-      size_t* neg_lod_data = neg_lod.data(ctx.GetPlace());
+      size_t* neg_lod_data = neg_lod.MutableData(ctx.GetPlace());
       NegTargetAssignFunctor<DeviceContext, T, WT> neg_trg_functor;
       neg_trg_functor(device_ctx, neg_idx_data, neg_lod_data, n, m, k,
                       mismatch_value, out_data, out_wt_data);
