d6f052681aed90d01690d2b84c31670913bcc366
kexinzhao <kexin.zhao.paddle@gmail.com>
2018-02-12 13:53:06 -0800

    Move float16 into fluid folder (#8394)
    * move float16 into fluid
    
    * fix include
    
    * move to platform folder

--- paddle/fluid/platform/float16_test.cu
+++ paddle/fluid/platform/float16_test.cu
@@ -9,7 +9,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/math/float16.h"
+#include "paddle/fluid/platform/float16.h"
 
 #include <gtest/gtest.h>
 
