f2a95fafacb8e9d60a908f579f4793466c83786f
Abhinav Arora <aroraabhinav@baidu.com>
2018-02-12 17:54:31 -0800

    Add very basic test case

--- python/paddle/v2/fluid/tests/test_concurrency.py
+++ python/paddle/v2/fluid/tests/test_concurrency.py
@@ -11,3 +11,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import unittest
+import numpy as np
+import paddle.v2.fluid.layers as layers
+import paddle.v2.fluid as fluid
+import paddle.v2.fluid.core as core
+from paddle.v2.fluid.executor import Executor
+
+
+class TestRoutineOp(unittest.TestCase):
+    def test_simple_routine(self):
+        counter = layers.zeros(shape=[1], dtype='int64')
+        counter = layers.increment(counter)
+
+        routine_op = fluid.Routine()
+        with routine_op.block():
+            counter = layers.increment(counter)
+
+        cpu = core.CPUPlace()
+        exe = Executor(cpu)
+
+        outs = exe.run(fetch_list=[counter])
+        self.assertEqual(2, np.sum(outs[0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
