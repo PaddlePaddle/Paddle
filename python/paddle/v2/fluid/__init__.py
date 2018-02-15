323a2e257fa9c8300a96c7e36bb2fe4f115dbeca
Abhinav Arora <aroraabhinav@baidu.com>
2018-02-15 09:30:06 -0800

    Removing import *

--- python/paddle/v2/fluid/__init__.py
+++ python/paddle/v2/fluid/__init__.py
@@ -34,7 +34,7 @@ from data_feeder import DataFeeder
 from core import LoDTensor, CPUPlace, CUDAPlace
 from distribute_transpiler import DistributeTranspiler
 from distribute_transpiler_simple import SimpleDistributeTranspiler
-from concurrency import *
+from concurrency import Go
 import clip
 from memory_optimization_transpiler import memory_optimize
 import profiler
