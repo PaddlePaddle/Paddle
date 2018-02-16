d6e61a00991169d309679e61e490ab41227f67f7
Abhinav Arora <abhinavarora28@gmail.com>
2018-02-15 17:08:57 -0800

    Python implementation for a proposed Go Op.  (#8434)
    * Adding Python boilerplate code for Go op
    
    * Add very basic test case
    
    * Adding the python logic for go routine
    
    * Fix syntax
    
    * Changing test to notest
    
    * Rename Routine to Go
    
    * Combining GoGuard and Go in one class
    
    * Modify test
    
    * Adding fluid close channel
    
    * Fixing __init__.py for calling fluid.go()
    
    * Adding stubs for channel methods and updating test case
    
    * Removing import *
    
    * Adding imports from concurrency

--- python/paddle/v2/fluid/__init__.py
+++ python/paddle/v2/fluid/__init__.py
@@ -34,13 +34,15 @@ from data_feeder import DataFeeder
 from core import LoDTensor, CPUPlace, CUDAPlace
 from distribute_transpiler import DistributeTranspiler
 from distribute_transpiler_simple import SimpleDistributeTranspiler
+from concurrency import (Go, make_channel, channel_send, channel_recv,
+                         channel_close)
 import clip
 from memory_optimization_transpiler import memory_optimize
 import profiler
 
 Tensor = LoDTensor
 
-__all__ = framework.__all__ + executor.__all__ + [
+__all__ = framework.__all__ + executor.__all__ + concurrency.__all__ + [
     'io',
     'initializer',
     'layers',
