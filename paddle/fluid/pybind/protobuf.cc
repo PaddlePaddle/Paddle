ec49c6e623bd2f83680e738307b5e594a3190f86
kavyasrinet <kavyasrinet@baidu.com>
2018-02-22 16:05:41 -0800

    Exposing Channel to be used  as a Variable and integrating with Fluid (#8486)
    * Adding set_capacity method support
    
    * Adding Python for make_channel
    
    * Updating notest_concurrency
    
    * Write python for make_channel method
    
    * Write python for make_channel method
    
    * Fix make_channel and test
    
    * Placeholder ops for channel send, recv and close
    
    * Adding ToTypeIndex method to var_type.h
    
    * Add var_type.h to channel:
    
    * Added POD_Type to the method
    
    * Add CHANNEL to executor
    
    * Updated get and set DataType to accomodate Channels
    
    * Updating get and set to incorporate channels
    
    * Adding CHANNEL as supported VarType in protobuf
    
    * Removing unecessary import
    
    * Fixing VarDesc to adapt to Channel as VarType
    
    * Add channel.h to executor
    
    * Remove innclude from channel
    
    * Updated var_type to support Channel as  var type
    
    * Adding get_channel to pybind
    
    * Added ChannelHolder
    
    * Adding make_channel as an op
    
    * Adding ChannelHolder in channel
    
    * Fixing typo
    
    * Commenting out operators in concurrency
    
    * Removing totypeid right now since we don't need it.
    
    * Reverting python changes
    
    * Fixing typo in framework.py
    
    * Modify comments for ReaderHolder

--- paddle/fluid/pybind/protobuf.cc
+++ paddle/fluid/pybind/protobuf.cc
@@ -216,6 +216,7 @@ void BindVarDsec(py::module &m) {
       .def("set_shapes", &VarDesc::SetShapes)
       .def("set_dtype", &VarDesc::SetDataType)
       .def("set_dtypes", &VarDesc::SetDataTypes)
+      .def("set_capacity", &VarDesc::SetCapacity)
       .def("shape", &VarDesc::GetShape, py::return_value_policy::reference)
       .def("shapes", &VarDesc::GetShapes, py::return_value_policy::reference)
       .def("dtype", &VarDesc::GetDataType, py::return_value_policy::reference)
@@ -246,6 +247,7 @@ void BindVarDsec(py::module &m) {
       .value("STEP_SCOPES", proto::VarType::STEP_SCOPES)
       .value("LOD_RANK_TABLE", proto::VarType::LOD_RANK_TABLE)
       .value("LOD_TENSOR_ARRAY", proto::VarType::LOD_TENSOR_ARRAY)
+      .value("CHANNEL", proto::VarType::CHANNEL)
       .value("PLACE_LIST", proto::VarType::PLACE_LIST)
       .value("READER", proto::VarType::READER)
       .value("NCCL_COM", proto::VarType::NCCL_COM);
