d5a507c09191adea6f08c97ff5c476145c90c0fd
sidgoyal78 <vi.siddharth78@gmail.com>
2018-02-24 02:34:41 +0000

    Add final code

--- paddle/fluid/inference/tests/book/test_inference_recognize_digits.cc
+++ paddle/fluid/inference/tests/book/test_inference_recognize_digits.cc
@@ -18,23 +18,34 @@ limitations under the License. */
 #include "paddle/fluid/platform/profiler.h"
 
 DEFINE_string(dirname, "", "Directory of the inference model.");
+DEFINE_bool(combine,
+            false,
+            "To run the test with load_combine and save_combine");
+DEFINE_int32(batchsize, 1, "Batch size of input");
 
-TEST(inference, recognize_digits_combine) {
-  if (FLAGS_dirname.empty()) {
-    LOG(FATAL) << "Usage: ./example --dirname=path/to/your/model";
-  }
+void with_combine() {
+  using paddle::platform::DeviceContext;
+  using paddle::platform::CUDADeviceContext;
+  using paddle::platform::CUDAPlace;
+  using paddle::platform::Event;
+  using paddle::platform::EventKind;
 
-  LOG(INFO) << "FLAGS_dirname: " << FLAGS_dirname << std::endl;
+  // LOG(INFO) << "FLAGS_dirname: " << FLAGS_dirname << std::endl;
   std::string dirname = FLAGS_dirname;
 
   // 0. Call `paddle::framework::InitDevices()` initialize all the devices
   // In unittests, this is done in paddle/testing/paddle_gtest_main.cc
 
+  // Add a start_event: setting up input
+  Event start_event1(EventKind::kPushRange, "setup_input", 0, nullptr);
+
   paddle::framework::LoDTensor input;
   // Use normilized image pixels as input data,
   // which should be in the range [-1.0, 1.0].
-  SetupTensor<float>(
-      input, {1, 1, 28, 28}, static_cast<float>(-1), static_cast<float>(1));
+  SetupTensor<float>(input,
+                     {FLAGS_batchsize, 1, 28, 28},
+                     static_cast<float>(-1),
+                     static_cast<float>(1));
   std::vector<paddle::framework::LoDTensor*> cpu_feeds;
   cpu_feeds.push_back(&input);
 
@@ -42,10 +53,14 @@ TEST(inference, recognize_digits_combine) {
   std::vector<paddle::framework::LoDTensor*> cpu_fetchs1;
   cpu_fetchs1.push_back(&output1);
 
+  Event stop_event1(EventKind::kPopRange, "setup_input", 0, nullptr);
+  LOG(INFO) << "Setting_input: " << start_event1.CpuElapsedMs(stop_event1)
+            << std::endl;
+
   // Run inference on CPU
   TestInference<paddle::platform::CPUPlace, true>(
       dirname, cpu_feeds, cpu_fetchs1);
-  LOG(INFO) << output1.dims();
+// LOG(INFO) << output1.dims();
 
 #ifdef PADDLE_WITH_CUDA
   paddle::framework::LoDTensor output2;
@@ -55,30 +70,26 @@ TEST(inference, recognize_digits_combine) {
   // Run inference on CUDA GPU
   TestInference<paddle::platform::CUDAPlace, true>(
       dirname, cpu_feeds, cpu_fetchs2);
-  LOG(INFO) << output2.dims();
+  // LOG(INFO) << output2.dims();
 
   CheckError<float>(output1, output2);
 #endif
 }
 
-TEST(inference, recognize_digits) {
-  if (FLAGS_dirname.empty()) {
-    LOG(FATAL) << "Usage: ./example --dirname=path/to/your/model";
-  }
-
+void separate() {
   using paddle::platform::DeviceContext;
   using paddle::platform::CUDADeviceContext;
   using paddle::platform::CUDAPlace;
   using paddle::platform::Event;
   using paddle::platform::EventKind;
 
-  LOG(INFO) << "FLAGS_dirname: " << FLAGS_dirname << std::endl;
+  // LOG(INFO) << "FLAGS_dirname: " << FLAGS_dirname << std::endl;
   std::string dirname = FLAGS_dirname;
 
   // 0. Call `paddle::framework::InitDevices()` initialize all the devices
   // In unittests, this is done in paddle/testing/paddle_gtest_main.cc
 
-  int64_t batch_size = 1;
+  int64_t batch_size = FLAGS_batchsize;
 
   // Add a start_event: setting up input
   Event start_event1(EventKind::kPushRange, "setup_input", 0, nullptr);
@@ -104,15 +115,15 @@ TEST(inference, recognize_digits) {
   // Run inference on CPU
   // Event start_event2(EventKind::kPushRange, "run_inference", 0, nullptr);
   TestInference<paddle::platform::CPUPlace>(dirname, cpu_feeds, cpu_fetchs1);
-  // Event stop_event2(EventKind::kPopRange, "run_inference", 0, nullptr);
-  // LOG(INFO) << "Running_inference: " <<
-  // start_event2.CpuElapsedMs(stop_event2)
-  //           << std::endl;
-  // LOG(INFO) << "Running inference: " <<
-  // start_event2.CpuElapsedMs(stop_event2)
-  //          << std::endl;
+// Event stop_event2(EventKind::kPopRange, "run_inference", 0, nullptr);
+// LOG(INFO) << "Running_inference: " <<
+// start_event2.CpuElapsedMs(stop_event2)
+//           << std::endl;
+// LOG(INFO) << "Running inference: " <<
+// start_event2.CpuElapsedMs(stop_event2)
+//          << std::endl;
 
-  LOG(INFO) << output1.dims();
+// LOG(INFO) << output1.dims();
 
 #ifdef PADDLE_WITH_CUDA
   paddle::framework::LoDTensor output2;
@@ -128,8 +139,34 @@ TEST(inference, recognize_digits) {
   //  Event stop_event_gpu1(EventKind::kPopRange, "gpu_run_inf", 0, dev_ctx);
   //  LOG(INFO) << "Running_inference_gpu: "
   //            << start_event_gpu1.CudaElapsedMs(stop_event_gpu1) << std::endl;
-  LOG(INFO) << output2.dims();
+  // LOG(INFO) << output2.dims();
 
   CheckError<float>(output1, output2);
 #endif
+}
+
+TEST(inference, recognize_digits_combine) {
+  if (FLAGS_dirname.empty()) {
+    LOG(FATAL) << "Usage: ./example --dirname=path/to/your/model";
+  }
+
+  if (FLAGS_combine) {
+    LOG(INFO) << "RUNNING WITH COMBINE" << std::endl;
+    for (int i = 0; i < 10; i++) {
+      with_combine();
+    }
+  }
+}
+
+TEST(inference, recognize_digits) {
+  if (FLAGS_dirname.empty()) {
+    LOG(FATAL) << "Usage: ./example --dirname=path/to/your/model";
+  }
+
+  if (!FLAGS_combine) {
+    LOG(INFO) << "RUNNING WITH SEPARATE" << std::endl;
+    for (int i = 0; i < 10; i++) {
+      separate();
+    }
+  }
 }
