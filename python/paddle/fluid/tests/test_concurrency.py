5744a7c6882eb6b575bc65223aa45b7004a00424
Varun Arora <aroravarun@baidu.com>
2018-02-27 13:41:38 -0800

    Daisy chain fixes

--- python/paddle/fluid/tests/test_concurrency.py
+++ python/paddle/fluid/tests/test_concurrency.py
@@ -37,22 +37,33 @@ class TestRoutineOp(unittest.TestCase):
     def test_daisy_chain(self):
         n = 10000
 
+        i = fluid.layers.zeros(shape=[1], dtype='int64')
+        array_len = fluid.layers.fill_constant(shape=[1], dtype='int64', value=n)
+        array_len.stop_gradient = True
+
+        cond = fluid.layers.less_than(x=i, y=array_len)
+
         leftmost = fluid.make_channel(dtype='int32')
         right = leftmost
         left = leftmost
 
-        with fluid.While(steps=n):
+        # with fluid.While(steps=n):
+
+        while_op = fluid.layers.While(cond=cond)
+        with while_op.block():
             right = fluid.make_channel(dtype='int32')
 
             with fluid.Go():
-                fluid.channel_send(left, 1 + fluid.channel_recv(right)[0])
+                fluid.channel_send(left, 1 + fluid.channel_recv(right, dtype='int32')[0])
 
             left = right
+
+            i = fluid.layers.increment(x=i, in_place=True)
 
         with fluid.Go():
             fluid.channel_send(right, 1)
 
-        leftmost_received = fluid.channel_recv(leftmost)
+        leftmost_received = fluid.channel_recv(leftmost, dtype='int32')
 
         cpu = core.CPUPlace()
         exe = Executor(cpu)
