dab02c3a22b3544e72d116306b4ee6eb1337047d
Liu Yiqun <liuyiqun01@baidu.com>
2018-02-28 10:39:28 +0000

    Allow the building of fluid codes for Android.

--- cmake/external/warpctc.cmake
+++ cmake/external/warpctc.cmake
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-IF(MOBILE_INFERENCE)
-    return()
-ENDIF()
+# TODO(liuyiqun): disable the dependency of warpctc for fluid mobile
+#IF(MOBILE_INFERENCE)
+#    return()
+#ENDIF()
 
 INCLUDE(ExternalProject)
 
