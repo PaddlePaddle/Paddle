b1f647fd6de059c32b60114365a8d75d8a925ae5
fengjiayi <fengjiayi@baidu.com>
2018-03-07 19:21:26 +0800

    fix errors

--- paddle/fluid/operators/reader/create_double_buffer_reader_op.cc
+++ paddle/fluid/operators/reader/create_double_buffer_reader_op.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <condition_variable>
+#include <mutex>
 #include <thread>
 #include "paddle/fluid/operators/reader/reader_op_registry.h"
 
