dade57ce98f3b737b214daece2113c8e836e79ad
Thuan Nguyen <cs2be@yahoo.com>
2018-03-06 17:39:24 -0800

    Initial checkin of select op, currently will read all the conditional_op in the cases block and also pull out all channels involved in the select.

--- paddle/fluid/framework/concurrency_test.cc
+++ paddle/fluid/framework/concurrency_test.cc
@@ -27,6 +27,7 @@ USE_NO_KERNEL_OP(channel_create);
 USE_NO_KERNEL_OP(channel_recv);
 USE_NO_KERNEL_OP(channel_send);
 USE_NO_KERNEL_OP(elementwise_add);
+USE_NO_KERNEL_OP(select);
 
 namespace f = paddle::framework;
 namespace p = paddle::platform;
@@ -45,19 +46,6 @@ void CreateIntVariable(Scope &scope, p::CPUPlace &place, std::string name,
   expect[0] = value;
 }
 
-void InitTensorsInScope(Scope &scope, p::CPUPlace &place) {
-  p::CPUDeviceContext ctx(place);
-
-  // Create channel variable
-  scope.Var("Channel");
-
-  // Create Variables, x0 will be put into channel,
-  // result will be pulled from channel
-  CreateIntVariable(scope, place, "Status", false);
-  CreateIntVariable(scope, place, "x0", 99);
-  CreateIntVariable(scope, place, "result", 0);
-}
-
 void AddOp(const std::string &type, const VariableNameMap &inputs,
            const VariableNameMap &outputs, AttributeMap attrs,
            BlockDesc *block) {
@@ -73,12 +61,59 @@ void AddOp(const std::string &type, const VariableNameMap &inputs,
   op->SetAttrMap(attrs);
 }
 
+void CreateSelect(Scope *scope, p::CPUPlace *place, ProgramDesc *program, BlockDesc *parentBlock) {
+  CreateIntVariable(*scope, *place, "case0var", 0);
+  CreateIntVariable(*scope, *place, "case1var", 0);
+  CreateIntVariable(*scope, *place, "case0cond", true);
+
+  BlockDesc *casesBlock = program->AppendBlock(*parentBlock);
+  BlockDesc *case0Block = program->AppendBlock(*casesBlock);
+  BlockDesc *case1Block = program->AppendBlock(*casesBlock);
+
+  AddOp("conditional_block",
+        {{"X", {"case0cond"}}},
+        {},
+        {{"sub_block", case0Block},
+         {"is_scalar_condition", true},
+         {"case_index", 0},
+         {"case_type", 2},
+         {"case_channel", std::string("Channel1")},
+         {"case_channel_var", std::string("case0var")}},
+        casesBlock);
+
+  AddOp("conditional_block",
+        {{"X", {"case0cond"}}},
+        {},
+        {{"sub_block", case1Block},
+         {"is_scalar_condition", true},
+         {"case_index", 1},
+         {"case_type", 1},
+         {"case_channel", std::string("Channel1")},
+         {"case_channel_var", std::string("case1var")}},
+        casesBlock);
+
+  AddOp("select",
+        {{"X", {"Channel1", "Channel2"}}},
+        {},
+        {{"sub_block", casesBlock}},
+        parentBlock);
+}
+
 TEST(Concurrency, Go_Op) {
   Scope scope;
   p::CPUPlace place;
 
   // Initialize scope variables
-  InitTensorsInScope(scope, place);
+  p::CPUDeviceContext ctx(place);
+
+  // Create channel variable
+  scope.Var("Channel");
+
+  // Create Variables, x0 will be put into channel,
+  // result will be pulled from channel
+  CreateIntVariable(scope, place, "Status", false);
+  CreateIntVariable(scope, place, "x0", 99);
+  CreateIntVariable(scope, place, "result", 0);
 
   framework::Executor executor(place);
   ProgramDesc program;
@@ -118,5 +153,65 @@ TEST(Concurrency, Go_Op) {
   auto *finalData = tensor.data<int>();
   EXPECT_EQ(finalData[0], 99);
 }
+
+TEST(Concurrency, Select) {
+  Scope scope;
+  p::CPUPlace place;
+
+  // Initialize scope variables
+  // Initialize scope variables
+  p::CPUDeviceContext ctx(place);
+
+  // Create channels variable
+  scope.Var("Channel1");
+  scope.Var("Channel2");
+
+  // Create Variables, x0 will be put into channel,
+  // result will be pulled from channel
+  CreateIntVariable(scope, place, "Status", false);
+  CreateIntVariable(scope, place, "x0", 99);
+  CreateIntVariable(scope, place, "result", 0);
+
+  framework::Executor executor(place);
+  ProgramDesc program;
+  BlockDesc *block = program.MutableBlock(0);
+
+  // Create channel OP
+  AddOp("channel_create", {}, {{"Out", {"Channel1"}}},
+        {{"capacity", 10}, {"data_type", f::proto::VarType::LOD_TENSOR}},
+        block);
+
+  CreateSelect(&scope, &place, &program, block);
+
+//  // Create Go Op routine
+//  BlockDesc *goOpBlock = program.AppendBlock(program.Block(0));
+//  AddOp("channel_send", {{"Channel", {"Channel"}}, {"X", {"x0"}}},
+//        {{"Status", {"Status"}}}, {}, goOpBlock);
+//
+//  // Create Go Op
+//  AddOp("go", {{"X", {"Channel", "x0"}}}, {}, {{"sub_block", goOpBlock}},
+//        block);
+
+  // Create Channel Receive Op
+  AddOp("channel_recv", {{"Channel", {"Channel"}}},
+        {{"Status", {"Status"}}, {"Out", {"result"}}}, {}, block);
+
+  // Create Channel Close Op
+  AddOp("channel_close", {{"Channel", {"Channel"}}}, {}, {}, block);
+
+//  // Check the result tensor to make sure it is set to 0
+//  const LoDTensor &tensor = (scope.FindVar("result"))->Get<LoDTensor>();
+//  auto *initialData = tensor.data<int>();
+//  EXPECT_EQ(initialData[0], 0);
+
+  executor.Run(program, &scope, 0, true, true);
+
+  // After we call executor.run, the Go operator should do a channel_send to set
+  // the
+  // "result" variable to 99
+//  auto *finalData = tensor.data<int>();
+//  EXPECT_EQ(finalData[0], 99);
+}
+
 }  // namespace framework
 }  // namespace paddle
