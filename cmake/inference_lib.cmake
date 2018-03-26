745b744c1b4a674b90d8259a50aa351cb029cad9
Luo Tao <luotao02@baidu.com>
2018-03-26 12:46:50 +0800

    rm libmklml_gnu.so

--- cmake/inference_lib.cmake
+++ cmake/inference_lib.cmake
@@ -69,11 +69,11 @@ if(NOT CBLAS_FOUND)
       SRCS ${CBLAS_INSTALL_DIR}/lib ${CBLAS_INSTALL_DIR}/include
       DSTS ${dst_dir} ${dst_dir}
     )
-else()
+elseif (WITH_MKLML)
     set(dst_dir "${CMAKE_INSTALL_PREFIX}/third_party/install/mklml")
     copy(mklml_lib
-      SRCS ${MKLML_LIB_DIR} ${MKLML_INC_DIR}
-      DSTS ${dst_dir} ${dst_dir}
+      SRCS ${MKLML_LIB} ${MKLML_IOMP_LIB} ${MKLML_INC_DIR}
+      DSTS ${dst_dir}/lib ${dst_dir}/lib ${dst_dir}
     )
 endif()
 
