2e577379ca8fc67dbb4fc436297cf7ae826b3fa7
chengduoZH <zhaochengduo@163.com>
2018-03-28 16:52:20 +0800

    add cos

--- python/paddle/fluid/tests/unittests/test_activation_op.py
+++ python/paddle/fluid/tests/unittests/test_activation_op.py
@@ -14,6 +14,7 @@
 
 import unittest
 import numpy as np
+import math
 import paddle.fluid.core as core
 from op_test import OpTest
 from scipy.special import expit
@@ -188,6 +189,20 @@ class TestFloor(OpTest):
         x = np.random.uniform(-1, 1, [4, 4]).astype("float32")
         self.inputs = {'X': x}
         self.outputs = {'Out': np.floor(self.inputs['X'])}
+
+    def test_check_output(self):
+        self.check_output()
+
+    def test_check_grad(self):
+        self.check_grad(['X'], 'Out', max_relative_error=0.007)
+
+
+class TestCos(OpTest):
+    def setUp(self):
+        self.op_type = "cos"
+        x = np.random.uniform(-1, 1, [4, 4]).astype("float32")
+        self.inputs = {'X': x}
+        self.outputs = {'Out': math.cos(self.inputs['X'])}
 
     def test_check_output(self):
         self.check_output()
