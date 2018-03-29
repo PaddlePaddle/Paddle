35f197d1c3318f781500b12f98392b6c97b5f083
qiaolongfei <qiaolongfei@baidu.com>
2018-03-29 16:18:03 +0800

    add prefetch_op

--- paddle/fluid/operators/prefetch_op.cc
+++ paddle/fluid/operators/prefetch_op.cc
@@ -30,9 +30,9 @@ static bool NeedSend(const framework::Scope& scope,
   PADDLE_ENFORCE_NOT_NULL(var, "Can not find variable '%s' in the send side.",
                           varname);
   if (var->IsType<framework::LoDTensor>()) {
-    return var->Get<framework::LoDTensor>().IsInitialized();
+    return var->Get<framework::LoDTensor>().numel() > 0;
   } else if (var->IsType<framework::SelectedRows>()) {
-    return var->Get<framework::SelectedRows>().rows().size() > 0UL;
+    return var->Get<framework::SelectedRows>().value().numel() > 0;
   } else {
     PADDLE_THROW(
         "Variable type in send side should be in "
@@ -41,9 +41,9 @@ static bool NeedSend(const framework::Scope& scope,
   return false;
 }
 
-class SendVarsOp : public framework::OperatorBase {
+class PrefetchOp : public framework::OperatorBase {
  public:
-  SendVarsOp(const std::string& type, const framework::VariableNameMap& inputs,
+  PrefetchOp(const std::string& type, const framework::VariableNameMap& inputs,
              const framework::VariableNameMap& outputs,
              const framework::AttributeMap& attrs)
       : OperatorBase(type, inputs, outputs, attrs) {}
@@ -51,9 +51,9 @@ class SendVarsOp : public framework::OperatorBase {
   void RunImpl(const framework::Scope& scope,
                const platform::Place& place) const override {
     auto ins = Inputs("X");
+    auto outs = Outputs("Out");
 
     std::vector<std::string> epmap = Attr<std::vector<std::string>>("epmap");
-    int sync_send = Attr<int>("sync_sent");
 
     platform::DeviceContextPool& pool = platform::DeviceContextPool::Instance();
     auto& ctx = *pool.Get(place);
@@ -67,38 +67,35 @@ class SendVarsOp : public framework::OperatorBase {
 
     for (size_t i = 0; i < ins.size(); i++) {
       if (NeedSend(scope, ins[i])) {
-        VLOG(3) << "sending " << ins[i] << " to " << epmap[i];
-        // TODO(Yancey1989): we need to use an IO threadpool which has
-        // a larger number of threads than the computing threadpool.
-        rpc_client->AsyncSendVariable(epmap[i], ctx, scope, ins[i]);
+        VLOG(3) << "sending " << ins[i] << " to " << epmap[i] << "to get "
+                << outs[i] << "back";
+        rpc_client->AsyncPrefetchVariable(epmap[i], ctx, scope, ins[i],
+                                          outs[i]);
       } else {
         VLOG(3) << "don't send no-initialied variable: " << ins[i];
       }
     }
-    if (sync_send) {
-      rpc_client->Wait();
-    }
+    rpc_client->Wait();
   }
 };
 
-class SendVarsOpMaker : public framework::OpProtoAndCheckerMaker {
+class PrefetchOpMaker : public framework::OpProtoAndCheckerMaker {
  public:
-  SendVarsOpMaker(OpProto* proto, OpAttrChecker* op_checker)
+  PrefetchOpMaker(OpProto* proto, OpAttrChecker* op_checker)
       : OpProtoAndCheckerMaker(proto, op_checker) {
-    AddInput("X", "(Tensor, SelectedRows) Input variables to be sent")
-        .AsDuplicable();
+    AddInput("X", "(LoDTensor) Input Id variables to be sent").AsDuplicable();
     AddOutput("RPCClient",
               "(RPCClient) The RPC client object which will be"
               "initialized at most once.");
+    AddOutput("Out",
+              "(SelectedRows) result "
+              "to be fetched from parameter server")
+        .AsDuplicable();
     AddComment(R"DOC(
 Send operator
 
-This operator will send variables to listen_and_serve op at the parameter server.
+This operator will send Ids variables to listen_and_serve op at the parameter server and fetch result back.
 )DOC");
-    AddAttr<int>("sync_send",
-                 "(int, default 0)"
-                 "sync send or async send.")
-        .SetDefault(0);
     AddAttr<std::vector<std::string>>("epmap",
                                       "(string vector, default 127.0.0.1:6164)"
                                       "Server endpoints in the order of input "
@@ -107,7 +104,7 @@ This operator will send variables to listen_and_serve op at the parameter server
   }
 };
 
-class SendVarsOpVarTypeInference : public framework::VarTypeInference {
+class PrefetchOpVarTypeInference : public framework::VarTypeInference {
  public:
   void operator()(const framework::OpDesc& op_desc,
                   framework::BlockDesc* block) const override {
@@ -118,7 +115,7 @@ class SendVarsOpVarTypeInference : public framework::VarTypeInference {
   }
 };
 
-class SendVarsOpShapeInference : public framework::InferShapeBase {
+class PrefetchOpShapeInference : public framework::InferShapeBase {
  public:
   void operator()(framework::InferShapeContext* ctx) const override {}
 };
@@ -128,7 +125,7 @@ class SendVarsOpShapeInference : public framework::InferShapeBase {
 
 namespace ops = paddle::operators;
 
-REGISTER_OPERATOR(send_vars, ops::SendVarsOp,
-                  paddle::framework::EmptyGradOpMaker, ops::SendVarsOpMaker,
-                  ops::SendVarsOpVarTypeInference,
-                  ops::SendVarsOpShapeInference);
+REGISTER_OPERATOR(prefetch, ops::PrefetchOp,
+                  paddle::framework::EmptyGradOpMaker, ops::PrefetchOpMaker,
+                  ops::PrefetchOpVarTypeInference,
+                  ops::PrefetchOpShapeInference);
