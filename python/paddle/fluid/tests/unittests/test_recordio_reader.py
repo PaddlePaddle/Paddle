1e91a1a9d222a6ba8ab0c2cdbecb6180c488620d
Yu Yang <yuyang18@baidu.com>
2018-03-30 15:35:34 +0800

    Add double buffer test case

--- python/paddle/fluid/tests/unittests/test_recordio_reader.py
+++ python/paddle/fluid/tests/unittests/test_recordio_reader.py
@@ -79,3 +79,90 @@ class TestRecordIO(unittest.TestCase):
     def test_double_buffer_reader(self):
         self.test_main(decorator_callback=lambda reader: fluid.layers.create_double_buffer_reader(reader,
                                                                                                   place='cuda:0' if fluid.core.is_compiled_with_cuda() else 'cpu'))
+
+
+import paddle.v2.dataset.flowers as flowers
+
+
+def vgg16_bn_drop(input):
+    def conv_block(input, num_filter, groups, dropouts):
+        return fluid.nets.img_conv_group(
+            input=input,
+            pool_size=2,
+            pool_stride=2,
+            conv_num_filter=[num_filter] * groups,
+            conv_filter_size=3,
+            conv_act='relu',
+            conv_with_batchnorm=True,
+            conv_batchnorm_drop_rate=dropouts,
+            pool_type='max')
+
+    conv1 = conv_block(input, 64, 2, [0.3, 0])
+    conv2 = conv_block(conv1, 128, 2, [0.4, 0])
+    conv3 = conv_block(conv2, 256, 3, [0.4, 0.4, 0])
+    conv4 = conv_block(conv3, 512, 3, [0.4, 0.4, 0])
+    conv5 = conv_block(conv4, 512, 3, [0.4, 0.4, 0])
+
+    drop = fluid.layers.dropout(x=conv5, dropout_prob=0.5)
+    fc1 = fluid.layers.fc(input=drop, size=512, act=None)
+    bn = fluid.layers.batch_norm(input=fc1, act='relu')
+    drop2 = fluid.layers.dropout(x=bn, dropout_prob=0.5)
+    fc2 = fluid.layers.fc(input=drop2, size=512, act=None)
+    return fc2
+
+
+class TestDoubleBuffer(unittest.TestCase):
+    BATCH_SIZE = 4
+
+    @classmethod
+    def setUpClass(cls):
+        # import os
+        # if os.path.exists('./flowers.recordio'):
+        #     return
+        with fluid.program_guard(fluid.Program(), fluid.Program()):
+            reader = paddle.batch(
+                flowers.train(), batch_size=TestDoubleBuffer.BATCH_SIZE)
+            feeder = fluid.DataFeeder(
+                feed_list=[
+                    fluid.layers.data(
+                        name='image', shape=[3, 224, 224]),
+                    fluid.layers.data(
+                        name='label', shape=[1], dtype='int64'),
+                ],
+                place=fluid.CPUPlace())
+            fluid.recordio_writer.convert_reader_to_recordio_file(
+                "./flowers_double_buffer.recordio",
+                reader,
+                feeder,
+                compressor=fluid.core.RecordIOWriter.Compressor.NoCompress)
+
+    def test_main(self):
+        with fluid.program_guard(fluid.Program(), fluid.Program()):
+            data_file = fluid.layers.open_recordio_file(
+                filename='./flowers_double_buffer.recordio',
+                shapes=[[-1, 3, 224, 224], [-1, 1]],
+                lod_levels=[0, 0],
+                dtypes=['float32', 'int64'])
+            data_file = fluid.layers.create_double_buffer_reader(
+                reader=data_file, place='CUDA:0')
+            images, label = fluid.layers.read_file(data_file)
+
+            net = vgg16_bn_drop(images)
+            predict = fluid.layers.fc(input=net, size=102, act='softmax')
+            cost = fluid.layers.cross_entropy(input=predict, label=label)
+            avg_cost = fluid.layers.mean(x=cost)
+            optimizer = fluid.optimizer.Adam(learning_rate=1e-4)
+            optimizer.minimize(avg_cost)
+
+            place = fluid.CUDAPlace(0)
+            exe = fluid.Executor(place)
+
+            # Parameter initialization
+            iters = 0
+            exe.run(fluid.default_startup_program())
+            while not data_file.eof():
+                # for batch_id, data in enumerate(train_reader()):
+                loss, = exe.run(fluid.default_main_program(),
+                                fetch_list=[avg_cost])
+                print "Iters = %d, Loss = %f" % (iters, loss)
+                iters += 1
