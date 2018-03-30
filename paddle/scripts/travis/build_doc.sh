1245cd48db208293fa9eb5f83339e9dea21d3245
weixing02 <564445201@qq.com>
2018-03-30 17:33:24 +0800

    Remove redundant commands in build.sh and build_doc.sh

--- paddle/scripts/travis/build_doc.sh
+++ paddle/scripts/travis/build_doc.sh
@@ -7,8 +7,7 @@ cd $TRAVIS_BUILD_DIR/build
 
 # Compile Documentation only.
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GPU=OFF -DWITH_MKL=OFF -DWITH_DOC=ON -DWITH_STYLE_CHECK=OFF
-make -j `nproc` gen_proto_py framework_py_proto
-make -j `nproc` copy_paddle_pybind
+
 make -j `nproc` paddle_docs paddle_apis
 
 # check websites for broken links
