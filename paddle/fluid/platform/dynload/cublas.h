b863011cf5652913bdaa3516e82ce1d64aaab030
Kexin Zhao <zhaokexin01@baidu.com>
2018-04-03 14:20:02 -0700

    make CUDA_VERSION available on cublas.h

--- paddle/fluid/platform/dynload/cublas.h
+++ paddle/fluid/platform/dynload/cublas.h
@@ -15,6 +15,7 @@ limitations under the License. */
 #pragma once
 
 #include <cublas_v2.h>
+#include <cuda.h>
 #include <dlfcn.h>
 #include <mutex>
 #include "paddle/fluid/platform/dynload/dynamic_loader.h"
