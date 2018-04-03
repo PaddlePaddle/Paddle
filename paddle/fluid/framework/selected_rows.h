2669aea67fffa9a0db1616f8f15a9c6b05be4352
qiaolongfei <qiaolongfei@baidu.com>
2018-04-03 13:07:44 +0800

    sgd_op support optimize SelectedRows

--- paddle/fluid/framework/selected_rows.h
+++ paddle/fluid/framework/selected_rows.h
@@ -1,8 +1,11 @@
-/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -10,6 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
+
+#include <vector>
+
 #include "paddle/fluid/framework/lod_tensor.h"
 #include "paddle/fluid/framework/tensor.h"
 
@@ -58,6 +64,11 @@ class SelectedRows {
   std::unique_ptr<Tensor> value_{nullptr};
   int64_t height_;
 };
+
+/**
+ * Find the index of value in rows.
+ */
+size_t GetIndex(const std::vector<int64_t>& rows, int64_t value);
 
 /*
  * Serialize/Desiralize SelectedRows to std::ostream
