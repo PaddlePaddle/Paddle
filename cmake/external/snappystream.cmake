77b544ace5f25a0128b7ef2b3243a554a96b032d
Yi Wang <yi.wang.2005@gmail.com>
2018-04-04 23:51:59 -0700

    Set include path

--- cmake/external/snappystream.cmake
+++ cmake/external/snappystream.cmake
@@ -23,7 +23,7 @@ include (ExternalProject)
 
 SET(SNAPPYSTREAM_SOURCES_DIR ${THIRD_PARTY_PATH}/snappy_stream)
 SET(SNAPPYSTREAM_INSTALL_DIR ${THIRD_PARTY_PATH}/install/snappy_stream)
-SET(SNAPPYSTREAM_INCLUDE_DIR "${SNAPPYSTREAM_INSTALL_DIR}/include/" CACHE PATH "snappy stream include directory." FORCE)
+INCLUDE_DIRECTORIES(${THIRD_PARTY_PATH}/install)
 
 ExternalProject_Add(
         extern_snappystream
@@ -53,6 +53,4 @@ ExternalProject_Add(
 add_library(snappystream STATIC IMPORTED GLOBAL)
 set_property(TARGET snappystream PROPERTY IMPORTED_LOCATION
         "${SNAPPYSTREAM_INSTALL_DIR}/lib/libsnappystream.a")
-
-include_directories(${THIRD_PARTY_PATH}/install)
 add_dependencies(snappystream extern_snappystream)
