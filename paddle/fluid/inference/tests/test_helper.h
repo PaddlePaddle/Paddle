98ffe51218808b479d9535f974ecc6bb3addbe1c
Yi Wang <yi.wang.2005@gmail.com>
2018-04-06 22:57:54 -0700

    Update

--- paddle/fluid/inference/tests/test_helper.h
+++ paddle/fluid/inference/tests/test_helper.h
@@ -30,7 +30,7 @@ void SetupTensor(paddle::framework::LoDTensor* input,
 
   T* input_ptr = input->mutable_data<T>(dims, paddle::platform::CPUPlace());
   for (int i = 0; i < input->numel(); ++i) {
-    input_ptr[i] = static_cast<T>(uniform_dist(*rng) * (upper - lower) + lower);
+    input_ptr[i] = static_cast<T>(uniform_dist(rng) * (upper - lower) + lower);
   }
 }
 
