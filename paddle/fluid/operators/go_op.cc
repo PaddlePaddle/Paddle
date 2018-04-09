be374033bb620d3c8d3ed6d625a68137df9bbaed
Abhinav Arora <aroraabhinav@baidu.com>
2018-04-08 17:08:28 -0700

    Fix CPPLInt issues in CSP operators

--- paddle/fluid/operators/go_op.cc
+++ paddle/fluid/operators/go_op.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include <thread>
+#include <thread>  // NOLINT
 #include <vector>
 #include "paddle/fluid/framework/executor.h"
 #include "paddle/fluid/framework/lod_tensor.h"
