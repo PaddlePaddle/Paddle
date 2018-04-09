cd41dca2ab9f5d98d6817147df2e71af152150f5
weixing02 <564445201@qq.com>
2018-04-09 18:22:57 +0800

    Adjust

--- python/paddle/fluid/initializer.py
+++ python/paddle/fluid/initializer.py
@@ -17,7 +17,8 @@ import numpy as np
 import contextlib
 
 __all__ = [
-    'ConstantInitializer', 'UniformInitializer', 'NormalInitializer', 'XavierInitializer', 'Constant', 'Uniform', 'Normal', 'Xavier', 'force_init_on_cpu','init_on_cpu'
+    'Constant', 'Uniform', 'Normal', 'Xavier', 'force_init_on_cpu',
+    'init_on_cpu', 'ConstantInitializer', 'UniformInitializer', 'NormalInitializer', 'XavierInitializer'
 ]
 
 _force_init_on_cpu_ = False
