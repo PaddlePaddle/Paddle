0464a980af2633ea281e42c4287374abec01dc6b
Siddharth Goyal <vi.siddharth78@gmail.com>
2018-04-09 19:26:24 +0000

    Fix cpplint errors

--- paddle/fluid/operators/crf_decoding_op.h
+++ paddle/fluid/operators/crf_decoding_op.h
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
+#include <limits>
 #include "paddle/fluid/framework/eigen.h"
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/operators/math/math_function.h"
