c022e83ca4af7f3ac9c1e579c42c9381a96e56be
gongweibao <weibao.gong@gmail.com>
2018-04-11 11:11:22 +0000

    cleanup

--- tools/codestyle/docstring_checker.py
+++ tools/codestyle/docstring_checker.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Checker for unneccessary use of str() in format() arguments."""
 
 import six
 import astroid
