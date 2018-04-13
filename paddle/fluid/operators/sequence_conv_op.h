67789dc9c3222be0d7d8463c3495e5a957f6f9d0
Abhinav Arora <abhinavarora28@gmail.com>
2018-04-13 16:24:09 -0700

    Fix CPPLint errors in operators (#9826)
    * Fix CPPLint errors in operators
    
    * Fix cast in softmax
    
    * Fix softmax_mkldnn
    
    * Fix send_recv_op_test
    
    * Send_recv
    
    * Fix softmax mkldnn

--- paddle/fluid/operators/sequence_conv_op.h
+++ paddle/fluid/operators/sequence_conv_op.h
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
+#include <algorithm>
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/operators/math/context_project.h"
 #include "paddle/fluid/operators/math/math_function.h"
