b5696b3c67219ccb3e3ed21251901a1980c0d505
Yi Wang <yi.wang.2005@gmail.com>
2018-04-13 17:02:32 -0700

    Update

--- paddle/fluid/platform/transform_test.cu
+++ paddle/fluid/platform/transform_test.cu
@@ -38,10 +38,21 @@ class Multiply {
 
 }  // namespace
 
+using paddle::memory::Alloc;
+using paddle::memory::Free;
+using paddle::memory::Copy;
+
+using paddle::platform::CPUPlace;
+using paddle::platform::CUDAPlace;
+using paddle::platform::CPUDeviceContext;
+using paddle::platform::CUDADeviceContext;
+
+using paddle::platform::Transform;
+
 TEST(Transform, CPUUnary) {
-  paddle::platform::CPUDeviceContext ctx;
+  CPUDeviceContext ctx;
   float buf[4] = {0.1, 0.2, 0.3, 0.4};
-  paddle::platform::Transform<paddle::platform::CPUDeviceContext> trans;
+  Transform<CPUDeviceContext> trans;
   trans(ctx, buf, buf + 4, buf, Scale<float>(10));
   for (int i = 0; i < 4; ++i) {
     ASSERT_NEAR(buf[i], static_cast<float>(i + 1), 1e-5);
@@ -49,18 +60,16 @@ TEST(Transform, CPUUnary) {
 }
 
 TEST(Transform, GPUUnary) {
-  paddle::platform::CUDAPlace gpu0(0);
-  paddle::platform::CUDADeviceContext ctx(gpu0);
+  CUDAPlace gpu0(0);
+  CUDADeviceContext ctx(gpu0);
   float cpu_buf[4] = {0.1, 0.2, 0.3, 0.4};
   float* gpu_buf = static_cast<float*>(Alloc(gpu0, sizeof(float) * 4));
-  paddle::memory::Copy(gpu0, gpu_buf, paddle::platform::CPUPlace(), cpu_buf,
-                       sizeof(cpu_buf), ctx.stream());
-  paddle::platform::Transform<paddle::platform::CUDADeviceContext> trans;
+  Copy(gpu0, gpu_buf, CPUPlace(), cpu_buf, sizeof(cpu_buf), ctx.stream());
+  Transform<CUDADeviceContext> trans;
   trans(ctx, gpu_buf, gpu_buf + 4, gpu_buf, Scale<float>(10));
   ctx.Wait();
-  paddle::memory::Copy(CPUPlace(), cpu_buf, gpu0, gpu_buf, sizeof(cpu_buf),
-                       ctx.stream());
-  paddle::memory::Free(gpu0, gpu_buf);
+  Copy(CPUPlace(), cpu_buf, gpu0, gpu_buf, sizeof(cpu_buf), ctx.stream());
+  Free(gpu0, gpu_buf);
   for (int i = 0; i < 4; ++i) {
     ASSERT_NEAR(cpu_buf[i], static_cast<float>(i + 1), 1e-5);
   }
@@ -68,8 +77,8 @@ TEST(Transform, GPUUnary) {
 
 TEST(Transform, CPUBinary) {
   int buf[4] = {1, 2, 3, 4};
-  paddle::platform::Transform<paddle::platform::CPUDeviceContext> trans;
-  paddle::platform::CPUDeviceContext ctx;
+  Transform<CPUDeviceContext> trans;
+  CPUDeviceContext ctx;
   trans(ctx, buf, buf + 4, buf, buf, Multiply<int>());
   for (int i = 0; i < 4; ++i) {
     ASSERT_EQ((i + 1) * (i + 1), buf[i]);
@@ -82,12 +91,11 @@ TEST(Transform, GPUBinary) {
   CUDADeviceContext ctx(gpu0);
   int* gpu_buf = static_cast<int*>(Alloc(gpu0, sizeof(buf)));
   Copy(gpu0, gpu_buf, CPUPlace(), buf, sizeof(buf), ctx.stream());
-  paddle::platform::Transform<paddle::platform::CUDADeviceContext> trans;
+  Transform<CUDADeviceContext> trans;
   trans(ctx, gpu_buf, gpu_buf + 4, gpu_buf, gpu_buf, Multiply<int>());
   ctx.Wait();
-  paddle::memory::Copy(CPUPlace(), buf, gpu0, gpu_buf, sizeof(buf),
-                       ctx.stream());
-  paddle::memory::Free(gpu0, gpu_buf);
+  Copy(CPUPlace(), buf, gpu0, gpu_buf, sizeof(buf), ctx.stream());
+  Free(gpu0, gpu_buf);
   for (int i = 0; i < 4; ++i) {
     ASSERT_EQ((i + 1) * (i + 1), buf[i]);
   }
