64c9079f84887474c345cd70841512e649c9de17
typhoonzero <typhoonzero1986@gmail.com>
2018-04-16 13:16:44 +0800

    update

--- python/paddle/fluid/metrics.py
+++ python/paddle/fluid/metrics.py
@@ -177,7 +177,7 @@ class ChunkEvaluator(MetricBase):
     """
 
     def __init__(self, name=None):
-        super(ChunkEvalutor, self).__init__(name)
+        super(ChunkEvaluator, self).__init__(name)
         self.num_infer_chunks = 0
         self.num_label_chunks = 0
         self.num_correct_chunks = 0
