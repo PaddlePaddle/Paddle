994a27bcf40fd1d8970a9fa6ed0ea00646ac2ee3
Yu Yang <yuyang18@baidu.com>
2018-04-16 16:16:27 +0800

    Revert

--- paddle/fluid/pybind/tensor_py.h
+++ paddle/fluid/pybind/tensor_py.h
@@ -190,7 +190,6 @@ void PyCUDATensorSetFromArray(
       static_cast<const platform::CUDADeviceContext *>(pool.Get(place));
   paddle::platform::GpuMemcpyAsync(dst, array.data(), sizeof(T) * array.size(),
                                    cudaMemcpyHostToDevice, dev_ctx->stream());
-  dev_ctx->Wait();
 }
 
 template <>
