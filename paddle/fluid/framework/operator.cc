2eedd187d6321576b6112bda3c9ee141595b58db
Yang Yang(Tony) <yangyang62@baidu.com>
2018-04-24 16:16:37 -0700

    Clean up unused code in operator class (#10035)
    * delete unused IsNetOp() and Rename()
    
    * rm OperatorBase::Rename implementation
    
    * delete Operator::InputVars()
    
    * remove unused OperatorBase::ShareLoD; ShareLoD has been implemented in infershape
    
    * organize operatorbase; remove unused set_type
    
    * add comments
    
    * fix comment

--- paddle/fluid/framework/operator.cc
+++ paddle/fluid/framework/operator.cc
@@ -171,17 +171,6 @@ std::string OperatorBase::DebugStringEx(const Scope* scope) const {
   return ss.str();
 }
 
-void OperatorBase::Rename(const std::string& old_name,
-                          const std::string& new_name) {
-  for (auto& input : inputs_) {
-    std::replace(input.second.begin(), input.second.end(), old_name, new_name);
-  }
-  for (auto& output : outputs_) {
-    std::replace(output.second.begin(), output.second.end(), old_name,
-                 new_name);
-  }
-}
-
 OperatorBase::OperatorBase(const std::string& type,
                            const VariableNameMap& inputs,
                            const VariableNameMap& outputs,
@@ -327,7 +316,6 @@ bool OpSupportGPU(const std::string& op_type) {
   auto it = all_kernels.find(op_type);
   if (it == all_kernels.end()) {
     // All control operator must support GPU
-
     return true;
   }
   for (auto& kern_pair : it->second) {
