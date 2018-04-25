1b92519bc64f242c3a3764b7473ba6ed4739319c
Yu Yang <yuyang18@baidu.com>
2018-04-25 13:22:20 +0800

    Rewrite matmul

--- paddle/fluid/operators/math/matmul.h
+++ paddle/fluid/operators/math/matmul.h
@@ -30,114 +30,70 @@ namespace math {
 template <typename DeviceContext, typename T>
 class MatMulFunctor {
  public:
+  struct MatDim {
+    int height_;
+    int width_;
+    int batch_size_{0};
+    int stride_{0};
+  };
+
+  static MatDim GetMatDim(bool trans, const framework::DDim& dim) {
+    MatDim res;
+    auto dim_vec = framework::vectorize(dim);
+    PADDLE_ENFORCE(!dim_vec.empty(),
+                   "Input tensor must be at least 1-dimensional");
+    if (dim_vec.size() == 1) {
+      res.height_ = trans ? dim_vec[0] : 1;
+      res.width_ = trans ? 1 : dim_vec[0];
+    } else if (dim_vec.size() == 2) {
+      res.height_ = trans ? dim_vec[1] : dim_vec[0];
+      res.width_ = trans ? dim_vec[0] : dim_vec[1];
+    } else {
+      res.height_ =
+          trans ? dim_vec[dim_vec.size() - 1] : dim_vec[dim_vec.size() - 2];
+      res.width_ =
+          trans ? dim_vec[dim_vec.size() - 2] : dim_vec[dim_vec.size() - 1];
+      res.batch_size_ =
+          std::accumulate(dim_vec.begin(), dim_vec.end() - 2, 1,
+                          [](int64_t elem, int a) -> int { return elem * a; });
+      res.stride_ = res.height_ * res.width_;
+    }
+    return res;
+  }
+
+  static CBLAS_TRANSPOSE GetTranspose(bool trans) {
+    return trans ? CblasTrans : CblasNoTrans;
+  }
+
   void operator()(const DeviceContext& context, const framework::Tensor& a,
                   bool trans_a, const framework::Tensor& b, bool trans_b,
                   T alpha, framework::Tensor* out, T beta) {
-    auto dim_a = a.dims();
-    auto dim_b = b.dims();
-
     PADDLE_ENFORCE(a.place() == b.place() && b.place() == out->place(),
                    "Tensors must all be in the same place.");
-    PADDLE_ENFORCE_GE(dim_a.size(), 1,
-                      "Input tensor a must be at least 1-dimensional.");
-    PADDLE_ENFORCE_GE(dim_b.size(), 1,
-                      "Input tensor b must be at least 1-dimensional.");
+    auto dim_a = GetMatDim(trans_a, a.dims());
+    auto dim_b = GetMatDim(trans_b, b.dims());
 
-    std::vector<int64_t> out_dim;
-    int64_t batch_count = 1;
-    if (dim_a.size() > 3) {
-      PADDLE_ENFORCE(dim_b.size() == dim_a.size(),
-                     "The dimensions of X and Y must be the same, and both of "
-                     "them should be %d-dimensional.",
-                     dim_b.size());
-      // The first rank-2 dimensions are accumulated on the batch_count, and the
-      // last two dimensions are used for matrix multiplication.
-      for (int j = 0; j < dim_a.size() - 2; ++j) {
-        PADDLE_ENFORCE_EQ(dim_b[j], dim_a[j],
-                          "The %d-th dimension of X and Y must be the same.",
-                          j);
-        out_dim.push_back(dim_a[j]);
-        batch_count *= dim_a[j];
-      }
+    PADDLE_ENFORCE_EQ(dim_a.width_, dim_b.height_);
+
+    if (dim_a.batch_size_ != 0 && dim_b.batch_size_ != 0) {
+      PADDLE_ENFORCE_EQ(dim_a.batch_size_, dim_b.batch_size_);
     }
 
-    int M = 0, N = 0, kA = 0, kB = 0, batchCountA = 0, batchCountB = 0,
-        strideA = 0, strideB = 0;
-
-    switch (dim_a.size()) {
-      case 1:
-        // similar to np.matmul:
-        // prepend dimension 1 (no transpose) or append dimension 1 (transpose)
-        M = trans_a ? dim_a[0] : 1;
-        kA = trans_a ? 1 : dim_a[0];
-        break;
-      case 2:
-        M = trans_a ? dim_a[1] : dim_a[0];
-        kA = trans_a ? dim_a[0] : dim_a[1];
-        break;
-      case 3:
-        batchCountA = dim_a[0];
-        M = trans_a ? dim_a[2] : dim_a[1];
-        kA = trans_a ? dim_a[1] : dim_a[2];
-        strideA = M * kA;
-        break;
-      default:
-        batchCountA = batch_count;
-        size_t mat_s = dim_a.size() - 2;
-        M = trans_a ? dim_a[mat_s + 1] : dim_a[mat_s];
-        kA = trans_a ? dim_a[mat_s] : dim_a[mat_s + 1];
-        strideA = M * kA;
-    }
-
-    switch (dim_b.size()) {
-      case 1:
-        // similar to np.matmul:
-        // append dimension 1 (no transpose) or prepend dimension 1 (transpose)
-        kB = trans_b ? 1 : dim_b[0];
-        N = trans_b ? dim_b[0] : 1;
-        break;
-      case 2:
-        kB = trans_b ? dim_b[1] : dim_b[0];
-        N = trans_b ? dim_b[0] : dim_b[1];
-        break;
-      case 3:
-        batchCountB = dim_b[0];
-        kB = trans_b ? dim_b[2] : dim_b[1];
-        N = trans_b ? dim_b[1] : dim_b[2];
-        strideB = kB * N;
-        break;
-      default:
-        batchCountB = batch_count;
-        size_t mat_s = dim_b.size() - 2;
-        kB = trans_b ? dim_b[mat_s + 1] : dim_b[mat_s];
-        N = trans_b ? dim_b[mat_s] : dim_b[mat_s + 1];
-        strideB = kB * N;
-    }
-
-    PADDLE_ENFORCE_EQ(
-        kA, kB,
-        "First matrix's width must be equal with second matrix's height.");
-
-    PADDLE_ENFORCE_EQ(batchCountA, batchCountB);
-
-    CBLAS_TRANSPOSE transA = (trans_a == false) ? CblasNoTrans : CblasTrans;
-    CBLAS_TRANSPOSE transB = (trans_b == false) ? CblasNoTrans : CblasTrans;
-
-    if (!batchCountA) {
-      // regular matrix multiplication
-      PADDLE_ENFORCE_EQ(a.numel(), M * kA);
-      PADDLE_ENFORCE_EQ(b.numel(), N * kA);
-      PADDLE_ENFORCE_EQ(out->numel(), M * N);
-      gemm<DeviceContext, T>(context, transA, transB, M, N, kA, alpha,
-                             a.data<T>(), b.data<T>(), beta, out->data<T>());
+    if (dim_a.batch_size_ == 0 && dim_b.batch_size_ == 0) {
+      PADDLE_ENFORCE_EQ(out->numel(), dim_a.height_ * dim_b.width_);
+      gemm<DeviceContext, T>(context, GetTranspose(trans_a),
+                             GetTranspose(trans_b), dim_a.height_, dim_b.width_,
+                             dim_a.width_, alpha, a.data<T>(), b.data<T>(),
+                             beta, out->data<T>());
     } else {
-      // batched matrix multiplication
-      PADDLE_ENFORCE_EQ(a.numel(), batchCountA * M * kA);
-      PADDLE_ENFORCE_EQ(b.numel(), batchCountA * kA * N);
-      PADDLE_ENFORCE_EQ(out->numel(), batchCountA * M * N);
+      int batch_size =
+          dim_a.batch_size_ == 0 ? dim_b.batch_size_ : dim_a.batch_size_;
+      PADDLE_ENFORCE_EQ(out->numel(),
+                        batch_size * dim_a.height_ * dim_b.width_);
       batched_gemm<DeviceContext, T>(
-          context, transA, transB, M, N, kA, alpha, a.data<T>(), b.data<T>(),
-          beta, out->data<T>(), batchCountA, strideA, strideB);
+          context, GetTranspose(trans_a), GetTranspose(trans_b), dim_a.height_,
+          dim_b.width_, dim_a.width_, alpha, a.data<T>(), b.data<T>(), beta,
+          out->data<T>(), batch_size, dim_a.stride_, dim_b.stride_);
     }
   }
 };
