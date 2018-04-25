8806801eb8064a53e6c17685051615f829a1f45b
robot <2466956298@qq.com>
2018-04-25 16:57:56 +0800

    [fix assign op] using assign op instead of scale1.0

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -193,10 +193,9 @@ def assign(input, output):
     helper = LayerHelper('assign', **locals())
     if isinstance(input, Variable):
         helper.append_op(
-            type='scale',
+            type='assign',
             inputs={'X': [input]},
             outputs={'Out': [output]},
-            attrs={'scale': 1.0})
     elif isinstance(input, numpy.ndarray):
         dtype = convert_np_dtype_to_dtype_(input.dtype)
         if dtype == VarDesc.VarType.FP32:
