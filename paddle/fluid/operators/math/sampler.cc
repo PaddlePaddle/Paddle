de272227d0881e2bfd2e2a364f554d0ea9c3f66c
Abhinav Arora <aroraabhinav@baidu.com>
2018-04-25 21:54:35 -0700

    Fix CPPLint issues in operators/math and operators/reader

--- paddle/fluid/operators/math/sampler.cc
+++ paddle/fluid/operators/math/sampler.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "sampler.h"
+#include "paddle/fluid/operators/math/sampler.h"
 
 namespace paddle {
 namespace random {
