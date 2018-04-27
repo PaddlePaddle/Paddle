6a47ae9699071c199c56ef363b0d45f1368270fb
Yu Yang <yuyang18@baidu.com>
2018-04-27 14:41:08 +0800

    Complex model

--- python/paddle/fluid/tests/unittests/test_memopt_parallel_executor.py
+++ python/paddle/fluid/tests/unittests/test_memopt_parallel_executor.py
@@ -31,35 +31,140 @@ def cosine_decay(learning_rate, step_each_epoch, epochs=120):
     return decayed_lr
 
 
-def simple_fc():
-    img = fluid.layers.data(name='img', shape=[784])
-    hidden = img
-    for i in xrange(2):
-        hidden = fluid.layers.fc(hidden, size=200, act='sigmoid')
-        hidden = fluid.layers.dropout(hidden, dropout_prob=0.1, seed=1)
-        # hidden = fluid.layers.batch_norm(
-        #     hidden,
-        #     moving_mean_name='moving_mean.{0}'.format(i),
-        #     moving_variance_name='moving_var.{0}'.format(i))
-    loss = hidden
+def squeeze_excitation(input, num_channels, reduction_ratio):
+    # pool = fluid.layers.pool2d(
+    #    input=input, pool_size=0, pool_type='avg', global_pooling=True)
+    conv = input
+    shape = conv.shape
+    reshape = fluid.layers.reshape(
+        x=conv, shape=[-1, shape[1], shape[2] * shape[3]])
+    pool = fluid.layers.reduce_mean(input=reshape, dim=2)
+
+    squeeze = fluid.layers.fc(input=pool,
+                              size=num_channels / reduction_ratio,
+                              act='relu')
+    excitation = fluid.layers.fc(input=squeeze,
+                                 size=num_channels,
+                                 act='sigmoid')
+    scale = fluid.layers.elementwise_mul(x=input, y=excitation, axis=0)
+    return scale
+
+
+def conv_bn_layer(input, num_filters, filter_size, stride=1, groups=1,
+                  act=None):
+    conv = fluid.layers.conv2d(
+        input=input,
+        num_filters=num_filters,
+        filter_size=filter_size,
+        stride=stride,
+        padding=(filter_size - 1) / 2,
+        groups=groups,
+        act=None,
+        bias_attr=False)
+    return fluid.layers.batch_norm(input=conv, act=act, momentum=0.1)
+
+
+def shortcut(input, ch_out, stride):
+    ch_in = input.shape[1]
+    if ch_in != ch_out:
+        if stride == 1:
+            filter_size = 1
+        else:
+            filter_size = 3
+        return conv_bn_layer(input, ch_out, filter_size, stride)
+    else:
+        return input
+
+
+def bottleneck_block(input, num_filters, stride, cardinality, reduction_ratio):
+    # The number of first 1x1 convolutional channels for each bottleneck build block
+    # was halved to reduce the compution cost.
+    conv0 = conv_bn_layer(
+        input=input, num_filters=num_filters, filter_size=1, act='relu')
+    conv1 = conv_bn_layer(
+        input=conv0,
+        num_filters=num_filters * 2,
+        filter_size=3,
+        stride=stride,
+        groups=cardinality,
+        act='relu')
+    conv2 = conv_bn_layer(
+        input=conv1, num_filters=num_filters * 2, filter_size=1, act=None)
+    scale = squeeze_excitation(
+        input=conv2,
+        num_channels=num_filters * 2,
+        reduction_ratio=reduction_ratio)
+
+    short = shortcut(input, num_filters * 2, stride)
+
+    return fluid.layers.elementwise_add(x=short, y=scale, act='relu')
+
+
+def SE_ResNeXt50Small():
+    img = fluid.layers.data(name='img', shape=[3, 224, 224], dtype='float32')
+    label = fluid.layers.data(shape=[1], dtype='int64', name='label')
+
+    conv = conv_bn_layer(
+        input=img, num_filters=16, filter_size=3, stride=2, act='relu')
+    conv = conv_bn_layer(
+        input=conv, num_filters=16, filter_size=3, stride=1, act='relu')
+    conv = conv_bn_layer(
+        input=conv, num_filters=16, filter_size=3, stride=1, act='relu')
+    conv = fluid.layers.pool2d(
+        input=conv, pool_size=3, pool_stride=2, pool_padding=1, pool_type='max')
+
+    cardinality = 32
+    reduction_ratio = 16
+    depth = [3, 4, 6, 3]
+    num_filters = [128, 256, 512, 1024]
+
+    for block in range(len(depth)):
+        for i in range(depth[block]):
+            conv = bottleneck_block(
+                input=conv,
+                num_filters=num_filters[block],
+                stride=2 if i == 0 and block != 0 else 1,
+                cardinality=cardinality,
+                reduction_ratio=reduction_ratio)
+
+    shape = conv.shape
+    reshape = fluid.layers.reshape(
+        x=conv, shape=[-1, shape[1], shape[2] * shape[3]])
+    pool = fluid.layers.reduce_mean(input=reshape, dim=2)
+    dropout = fluid.layers.dropout(x=pool, dropout_prob=0.2, seed=1)
+    # Classifier layer:
+    prediction = fluid.layers.fc(input=dropout, size=1000, act='softmax')
+    loss = fluid.layers.cross_entropy(input=prediction, label=label)
     loss = fluid.layers.mean(loss)
-    adam = fluid.optimizer.Adam(
-        learning_rate=cosine_decay(
-            1e-2, step_each_epoch=100),
-        regularization=fluid.regularizer.L2Decay(0.5))
-    adam.minimize(loss)
     return loss
 
 
-class FCDataRandom(object):
-    def __init__(self, batch_size=64):
+def conv_bn_layer(input, num_filters, filter_size, stride=1, groups=1,
+                  act=None):
+    conv = fluid.layers.conv2d(
+        input=input,
+        num_filters=num_filters,
+        filter_size=filter_size,
+        stride=stride,
+        padding=(filter_size - 1) / 2,
+        groups=groups,
+        act=None,
+        bias_attr=False)
+    return fluid.layers.batch_norm(input=conv, act=act)
+
+
+class ResNeXtRandomData(object):
+    def __init__(self, batch_size=10):
         self.random_state = numpy.random.RandomState(seed=1)
         self.batch_size = batch_size
 
     def next(self):
         return {
             'img': self.random_state.uniform(
-                low=-1, high=1, size=(self.batch_size, 784)).astype('float32')
+                low=-1, high=1,
+                size=(self.batch_size, 3, 224, 224)).astype('float32'),
+            'label': self.random_state.uniform(
+                low=0, high=10, size=(self.batch_size, 1)).astype('int64')
         }
 
 
@@ -106,7 +211,7 @@ def create_unittest(network_func, data_random):
     return __cls__
 
 
-TestSimpleFC = create_unittest(simple_fc, FCDataRandom)
+TestSimpleFC = create_unittest(SE_ResNeXt50Small, ResNeXtRandomData)
 
 if __name__ == '__main__':
     unittest.main()
