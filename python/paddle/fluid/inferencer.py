355c852b9936f13f9b8b83608b356121c4e2907d
Helin Wang <ustc.harry@gmail.com>
2018-05-01 15:51:20 -0700

    fix yapf stype check

--- python/paddle/fluid/inferencer.py
+++ python/paddle/fluid/inferencer.py
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-__all__ = [
-    'Inferencer',
-]
+__all__ = ['Inferencer', ]
 
 
 class Inferencer(object):
