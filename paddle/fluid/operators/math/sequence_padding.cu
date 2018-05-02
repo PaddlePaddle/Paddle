3fc30ba3b3a19b6991e3d3370aa901f8e4054618
Abhinav Arora <aroraabhinav@baidu.com>
2018-05-01 18:25:11 -0700

    Add include

--- paddle/fluid/operators/math/sequence_padding.cu
+++ paddle/fluid/operators/math/sequence_padding.cu
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#include <algorithm>
 #include "paddle/fluid/operators/math/sequence_padding.h"
 
 namespace paddle {
