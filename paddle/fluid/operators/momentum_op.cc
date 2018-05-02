a7707883665dc1a50d7c58bf3ab1e84a165e7b8f
dzhwinter <dzhwinter@gmail.com>
2018-05-02 04:56:46 -0700

    "fix ci"

--- paddle/fluid/operators/momentum_op.cc
+++ paddle/fluid/operators/momentum_op.cc
@@ -17,6 +17,8 @@ limitations under the License. */
 namespace paddle {
 namespace operators {
 
+using Tensor = framework::Tensor;
+
 class MomentumOp : public framework::OperatorWithKernel {
  public:
   using framework::OperatorWithKernel::OperatorWithKernel;
