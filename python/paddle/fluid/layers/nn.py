8ed925892ed0e40f3e3db64e3b02532d00173063
Liu Yi <liuyi05@baidu.com>
2018-05-09 18:00:30 +0800

    supports log_loss in the python front-end

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -34,6 +34,7 @@ __all__ = [
     'crf_decoding',
     'cos_sim',
     'cross_entropy',
+    'log_loss',
     'square_error_cost',
     'chunk_eval',
     'sequence_conv',
@@ -973,6 +974,44 @@ def cross_entropy(input, label, soft_label=False):
         outputs={'Y': [out]},
         attrs={"soft_label": soft_label})
     return out
+
+
+def log_loss(input, label, epsilon=1e-4):
+    """
+    **Negative Log Loss Layer**
+
+    This layer accepts input predictions and target label and returns the
+    negative log loss.
+
+    .. math::
+
+        Out = -label * log(X + epsilon) - (1 - label) * log(1 - X + epsilon)
+
+    Args:
+        input:  a 2-D tensor with shape [N x 1], where N is the batch size.
+                This input is a probability computed by the previous operator.
+        label:  the ground truth which is a 2-D tensor with shape [N x 1],
+                where N is the batch size.
+        epsilon: epsilon
+
+    Returns:
+         A 2-D tensor with shape [N x 1], the negative log loss.
+
+    Examples:
+        .. code-block:: python
+
+          prob = fluid.layers.sigmoid(net)
+          cost = fluid.layers.log_loss(input=prob, label=label)
+    """
+    helper = LayerHelper('log_loss', **locals())
+    loss = helper.create_tmp_variable(dtype=input.dtype)
+    helper.append_op(
+        type='log_loss',
+        inputs={'Predicted': [input],
+                'Labels': [label]},
+        outputs={'Loss': [loss]},
+        attrs={'epsilon': epsilon})
+    return loss
 
 
 def square_error_cost(input, label):
