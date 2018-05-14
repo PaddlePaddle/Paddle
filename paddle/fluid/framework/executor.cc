64acd42182f824f31d42a98565e2431f1988a44d
Yancey1989 <yancey1989@gmail.com>
2018-05-14 15:33:25 +0800

    fix dis train crash

--- paddle/fluid/framework/executor.cc
+++ paddle/fluid/framework/executor.cc
@@ -351,10 +351,8 @@ void Executor::RunPreparedContext(ExecutorPrepareContext* ctx, Scope* scope,
   platform::DeviceContextPool::Instance().Get(place_)->Wait();
   if (create_vars && create_local_scope) {
     scope->DeleteScope(local_scope);
-  } else {
-    // Delete the local scopes created in operators.
-    scope->DropKids();
   }
+
   if (FLAGS_benchmark) {
     VLOG(2) << "-------------------------------------------------------";
     VLOG(2) << "Memory used after deleting local scope: "
