620c0a22aa49c33f037823f5dc0545a83a590ac1
qiaolongfei <qiaolongfei@baidu.com>
2018-05-15 17:45:02 +0800

    update boost dst dir

--- cmake/inference_lib.cmake
+++ cmake/inference_lib.cmake
@@ -70,7 +70,7 @@ copy(glog_lib
   DSTS ${dst_dir} ${dst_dir}/lib
 )
 
-set(dst_dir "${CMAKE_INSTALL_PREFIX}/third_party/boost/include")
+set(dst_dir "${CMAKE_INSTALL_PREFIX}/third_party/boost/")
 copy(boost_lib
   SRCS ${BOOST_INCLUDE_DIR}/boost
   DSTS ${dst_dir}
