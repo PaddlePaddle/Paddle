dbacb0dd78d6304a62bc57e683e9816ce094e585
Liu Yi <liuyi05@baidu.com>
2018-05-16 17:19:48 +0800

    fix the problem of comments exceeding 80 columns

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -985,13 +985,15 @@ def log_loss(input, label, epsilon=1e-4):
 
     .. math::
 
-        Out = -label * log(input + epsilon) - (1 - label) * log(1 - input + epsilon)
+        Out = -label * log(input + epsilon) 
+              - (1 - label) * log(1 - input + epsilon)
 
     Args:
-        input (Variable|list):  a 2-D tensor with shape [N x 1], where N is the batch size.
-                                This input is a probability computed by the previous operator.
-        label (Variable|list):  the ground truth which is a 2-D tensor with shape [N x 1],
-                                where N is the batch size.
+        input (Variable|list):  a 2-D tensor with shape [N x 1], where N is the 
+                                batch size. This input is a probability computed
+                                by the previous operator.
+        label (Variable|list):  the ground truth which is a 2-D tensor with 
+                                shape [N x 1], where N is the batch size.
         epsilon (float): epsilon
 
     Returns:
