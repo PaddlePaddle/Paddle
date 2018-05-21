8c6f9389eeb55684a30fd2146b479c03bf2e4e32
yuyang18 <reyoung@126.com>
2018-05-21 14:30:48 +0800

    Disabled hanged unit tests

--- python/paddle/fluid/tests/unittests/test_parallel_executor_crf.py
+++ python/paddle/fluid/tests/unittests/test_parallel_executor_crf.py
@@ -168,24 +168,28 @@ class TestCRFModel(unittest.TestCase):
                           pe.run(feed=feeder.feed(cur_batch),
                                  fetch_list=[avg_cost.name]))[0]
 
+    @unittest.skip("Hang when parallel execute")
     def test_update_sparse_parameter_all_reduce(self):
         build_strategy = fluid.BuildStrategy()
         build_strategy.reduce_strategy = fluid.BuildStrategy.ReduceStrategy.AllReduce
         self.check_network_convergence(
             is_sparse=True, build_strategy=build_strategy)
 
+    @unittest.skip("Hang when parallel execute")
     def test_update_dense_parameter_all_reduce(self):
         build_strategy = fluid.BuildStrategy()
         build_strategy.reduce_strategy = fluid.BuildStrategy.ReduceStrategy.AllReduce
         self.check_network_convergence(
             is_sparse=False, build_strategy=build_strategy)
 
+    @unittest.skip("Hang when parallel execute")
     def test_update_sparse_parameter_reduce(self):
         build_strategy = fluid.BuildStrategy()
         build_strategy.reduce_strategy = fluid.BuildStrategy.ReduceStrategy.Reduce
         self.check_network_convergence(
             is_sparse=True, build_strategy=build_strategy)
 
+    @unittest.skip("Hang wen parallel execute")
     def test_update_dense_parameter_reduce(self):
         build_strategy = fluid.BuildStrategy()
         build_strategy.reduce_strategy = fluid.BuildStrategy.ReduceStrategy.Reduce
