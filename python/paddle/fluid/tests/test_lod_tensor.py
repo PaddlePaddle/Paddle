de305554a672c4efc97eac885663a304ce3b454a
Kexin Zhao <zhaokexin01@baidu.com>
2018-05-21 13:17:26 -0700

    add lod_tensor test code

--- python/paddle/fluid/tests/test_lod_tensor.py
+++ python/paddle/fluid/tests/test_lod_tensor.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import unittest
+import numpy
 import paddle.fluid as fluid
 import paddle.fluid.core as core
 from paddle.fluid.lod_tensor import create_lod_tensor, _validate_lod, _convert_lod
@@ -22,9 +23,18 @@ class TestLoDTensor(unittest.TestCase):
     def test_validate_lod_1(self):
         lod = (1, 2, 1)
         self.assertRaises(AssertionError, _validate_lod, lod, -1)
-        lod = [[1, 2], (2, 3)]
+        lod = [[1, 2], [2, 3]]
         self.assertRaises(AssertionError, _validate_lod, lod, -1)
 
+    def test_convert_lod_1(self):
+        lod = [[1, 2, 3]]
+        converted_lod = [[0, 1, 3, 6]]
+        self.assertEqual(_convert_lod(lod), converted_lod)
 
-if __name__ == ` __main__ `:
+    def test_create_lod_tensor_1(self):
+        tensor = numpy.random.random([10, 1])
+        create_lod_tensor(tensor, [[2, 2], [3, 3, 4]], fluid.CPUPlace())
+
+
+if __name__ == '__main__':
     unittest.main()
