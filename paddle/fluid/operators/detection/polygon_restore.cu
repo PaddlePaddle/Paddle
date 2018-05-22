a90bb1250eb19235711cdd9326e2c2a557fc6905
wanghaoshuang <wanghaoshuang@baidu.com>
2018-05-22 17:50:41 +0800

    Generalize geometry channels.

--- paddle/fluid/operators/detection/polygon_restore.cu
+++ paddle/fluid/operators/detection/polygon_restore.cu
@@ -12,9 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include <thrust/execution_policy.h>
-#include <thrust/reduce.h>
-#include "paddle/fluid/operators/quad_transform_op.h"
 #include "paddle/fluid/platform/cuda_primitives.h"
 #include "paddle/fluid/platform/gpu_info.h"
 
@@ -24,8 +21,8 @@ using platform::PADDLE_CUDA_NUM_THREADS;
 #define CUDA_BLOCK_SIZE 16
 
 template <typename T>
-__global__ void QuadTransformKernel(const int n, const int h, const int w,
-                                    const T* input, T* output) {
+__global__ void PolygonRestoreKernel(const int n, const int h, const int w,
+                                     const T* input, T* output) {
   int id_n = threadIdx.x + blockDim.x * blockIdx.x;
   int id_h = threadIdx.y + blockDim.y * blockIdx.y;
   int id_w = threadIdx.z + blockDim.z * blockIdx.z;
@@ -40,7 +37,7 @@ __global__ void QuadTransformKernel(const int n, const int h, const int w,
 }
 
 template <typename T>
-class QuadTransfromOpCUDAKernel : public framework::OpKernel<T> {
+class PolygonRestoreOpCUDAKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
     PADDLE_ENFORCE(platform::is_gpu_place(ctx.GetPlace()),
@@ -52,23 +49,24 @@ class QuadTransfromOpCUDAKernel : public framework::OpKernel<T> {
     T* out_data = out->mutable_data<T>(ctx.GetPlace());
 
     int batch_size = in_dims[0];
+    int geo_channels = in_dims[1];
     int height = in_dims[2];
     int width = in_dims[3];
     dim3 threadsPerBlock(
         PADDLE_CUDA_NUM_THREADS / (CUDA_BLOCK_SIZE * CUDA_BLOCK_SIZE),
         CUDA_BLOCK_SIZE, CUDA_BLOCK_SIZE);
-    dim3 numBlocks((batch_size * GEO_CHANNEL) / threadsPerBlock.x,
+    dim3 numBlocks((batch_size * geo_channels) / threadsPerBlock.x,
                    (height + threadsPerBlock.y - 1) / threadsPerBlock.y,
                    (width + threadsPerBlock.z - 1) / threadsPerBlock.z);
     auto stream = ctx.cuda_device_context().stream();
-    QuadTransformKernel<T><<<numBlocks, threadsPerBlock, 0, stream>>>(
-        batch_size * GEO_CHANNEL, height, width, in_data, out_data);
+    PolygonRestoreKernel<T><<<numBlocks, threadsPerBlock, 0, stream>>>(
+        batch_size * geo_channels, height, width, in_data, out_data);
   }
 };
 
 }  // namespace operators
 }  // namespace paddle
 
-REGISTER_OP_CUDA_KERNEL(quad_transform,
-                        paddle::operators::QuadTransfromOpCUDAKernel<float>,
-                        paddle::operators::QuadTransfromOpCUDAKernel<double>);
+REGISTER_OP_CUDA_KERNEL(polygon_restore,
+                        paddle::operators::PolygonRestoreOpCUDAKernel<float>,
+                        paddle::operators::PolygonRestoreOpCUDAKernel<double>);
