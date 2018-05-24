bf78e51f5df53c6a569057bb7bd0b2fb87c085a1
Xin Pan <panxin.grad@gmail.com>
2018-05-24 09:32:43 +0800

    debug infer threading

--- paddle/fluid/inference/tests/book/test_inference_word2vec.cc
+++ paddle/fluid/inference/tests/book/test_inference_word2vec.cc
@@ -12,11 +12,21 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#include <sys/time.h>
+#include <time.h>
+#include <thread>
+
 #include "gflags/gflags.h"
 #include "gtest/gtest.h"
 #include "paddle/fluid/inference/tests/test_helper.h"
 
 DEFINE_string(dirname, "", "Directory of the inference model.");
+
+inline uint64_t PosixInNsec() {
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  return 1000 * (static_cast<uint64_t>(tv.tv_sec) * 1000000 + tv.tv_usec);
+}
 
 TEST(inference, word2vec) {
   if (FLAGS_dirname.empty()) {
@@ -44,25 +54,40 @@ TEST(inference, word2vec) {
   cpu_feeds.push_back(&third_word);
   cpu_feeds.push_back(&fourth_word);
 
-  paddle::framework::LoDTensor output1;
-  std::vector<paddle::framework::LoDTensor*> cpu_fetchs1;
-  cpu_fetchs1.push_back(&output1);
-
+  int total_work = 1000;
+  int num_threads = 10;
+  int work_per_thread = total_work / num_threads;
   // Run inference on CPU
-  TestInference<paddle::platform::CPUPlace>(dirname, cpu_feeds, cpu_fetchs1);
-  LOG(INFO) << output1.lod();
-  LOG(INFO) << output1.dims();
+  std::vector<std::unique_ptr<std::thread>> infer_threads;
+  for (int i = 0; i < num_threads; ++i) {
+    infer_threads.emplace_back(new std::thread([&, i]() {
+      for (int j = 0; j < work_per_thread; ++j) {
+        /*
+        paddle::framework::LoDTensor output1;
+        std::vector<paddle::framework::LoDTensor*> cpu_fetchs1;
+        cpu_fetchs1.push_back(&output1);
 
+        TestInference<paddle::platform::CPUPlace>(
+            dirname, cpu_feeds, cpu_fetchs1);*/
 #ifdef PADDLE_WITH_CUDA
-  paddle::framework::LoDTensor output2;
-  std::vector<paddle::framework::LoDTensor*> cpu_fetchs2;
-  cpu_fetchs2.push_back(&output2);
+        paddle::framework::LoDTensor output2;
+        std::vector<paddle::framework::LoDTensor*> cpu_fetchs2;
+        cpu_fetchs2.push_back(&output2);
 
-  // Run inference on CUDA GPU
-  TestInference<paddle::platform::CUDAPlace>(dirname, cpu_feeds, cpu_fetchs2);
-  LOG(INFO) << output2.lod();
-  LOG(INFO) << output2.dims();
+        // Run inference on CUDA GPU
+        TestInference<paddle::platform::CUDAPlace>(
+            dirname, cpu_feeds, cpu_fetchs2);
 
-  CheckError<float>(output1, output2);
+        // CheckError<float>(output1, output2);
 #endif
+        fprintf(stderr, "working on %dth in thread %d\n", j, i);
+      }
+    }));
+  }
+  uint64_t start_ns = PosixInNsec();
+  for (int i = 0; i < num_threads; ++i) {
+    infer_threads[i]->join();
+  }
+  uint64_t stop_ns = PosixInNsec();
+  fprintf(stderr, "total time: %lu\n", stop_ns - start_ns);
 }
