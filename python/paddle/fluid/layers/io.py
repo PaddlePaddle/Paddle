30e7432e122dce94b210935ea0fdc7fe67048029
typhoonzero <typhoonzero1986@gmail.com>
2018-05-24 22:25:41 +0800

    add random reader op export

--- python/paddle/fluid/layers/io.py
+++ python/paddle/fluid/layers/io.py
@@ -21,7 +21,8 @@ from ..executor import global_scope
 
 __all__ = [
     'data', 'BlockGuardServ', 'ListenAndServ', 'Send', 'open_recordio_file',
-    'open_files', 'read_file', 'shuffle', 'batch', 'double_buffer'
+    'open_files', 'read_file', 'shuffle', 'batch', 'double_buffer',
+    'random_data_generator'
 ]
 
 
