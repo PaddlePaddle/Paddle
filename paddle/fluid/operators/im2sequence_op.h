470e11226b68bf1d7bc127920433a98ee71ebb01
guohe <guohe@baidu.com>
2018-05-28 20:53:47 +0800

    remove resize

--- paddle/fluid/operators/im2sequence_op.h
+++ paddle/fluid/operators/im2sequence_op.h
@@ -13,7 +13,8 @@
    limitations under the License. */
 
 #pragma once
-#include <vector>
+#include <string>
+#include <fstream>
 #include "paddle/fluid/framework/data_layout.h"
 #include "paddle/fluid/framework/eigen.h"
 #include "paddle/fluid/framework/op_registry.h"
@@ -38,8 +39,9 @@ class Im2SequenceKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
     const Tensor* in = ctx.Input<Tensor>("X");
+    // TODO(fuhailong): add new data layer to solve multibatch inference
+    const Tensor* imgRealSize = ctx.Input<Tensor>("Y");
     LoDTensor* out = ctx.Output<LoDTensor>("Out");
-    out->mutable_data<T>(ctx.GetPlace());
     // TODO(wanghaoshuang): Add layout checker after 'set_layout'
     // being available for python API
     // PADDLE_ENFORCE_EQ(in->layout(), framework::DataLayout::kNCHW,
@@ -49,41 +51,129 @@ class Im2SequenceKernel : public framework::OpKernel<T> {
     int img_channels = in_dim[1];
     int img_height = in_dim[2];
     int img_width = in_dim[3];
-
+    auto imgRealSize_vec = imgRealSize->data<float>();
+    auto imgRealSize_dim = imgRealSize->dims();
     auto kernels = ctx.Attr<std::vector<int>>("kernels");
     auto strides = ctx.Attr<std::vector<int>>("strides");
     auto paddings = ctx.Attr<std::vector<int>>("paddings");
-    int output_height = Im2SeqOutputSize(img_height, kernels[0], paddings[0],
-                                         paddings[2], strides[0]);
-    int output_width = Im2SeqOutputSize(img_width, kernels[1], paddings[1],
-                                        paddings[3], strides[1]);
+    auto conv_count = ctx.Attr<int>("conv_count");
+    auto conv_kernel = ctx.Attr<std::vector<int>>("conv_kernel");
+    auto is_inference = ctx.Attr<bool>("is_inference");
+    if (is_inference && batch_size > 1) {
+        std::vector<int> imgReal_H;
+        std::vector<int> imgReal_W;
+        std::vector<int> output_height;
+        std::vector<int> output_width;
+        int result = 0;
+        for (int i = 0; i < batch_size; i++) {
+            int tmp_real_H = int(imgRealSize_vec[2 * i]);
+            int tmp_real_W = int(imgRealSize_vec[2 * i + 1]);
+            for (int j = 0; j < conv_count; j++) {
+                tmp_real_H = tmp_real_H / conv_kernel[0] +
+                                       tmp_real_H % conv_kernel[0];
+                tmp_real_W = tmp_real_W / conv_kernel[1] +
+                                       tmp_real_W % conv_kernel[1];
+            }
+            imgReal_H.push_back(tmp_real_H);
+            imgReal_W.push_back(tmp_real_W);
+            output_height.push_back(Im2SeqOutputSize(imgReal_H[i],
+                                                     kernels[0],
+                                                     paddings[0],
+                                                     paddings[2],
+                                                     strides[0]));
+            output_width.push_back(Im2SeqOutputSize(imgReal_W[i],
+                                                    kernels[1],
+                                                    paddings[1],
+                                                    paddings[3],
+                                                    strides[1]));
+        // TODO(fuhailong): compute dims of output
+        // call: out->mutable_data<T>(ctx.GetPlace(), output_dims);
+            result += output_height[i] * output_width[i];
+        }
 
-    const std::vector<int> dilations({1, 1});
+        out->mutable_data<T>({result, img_channels*kernels[0]*kernels[1]},
+                              ctx.GetPlace());
 
-    auto out_dims = out->dims();
-    out->Resize({batch_size, out->numel() / batch_size});
-    for (int i = 0; i < batch_size; i++) {
-      const Tensor src =
-          in->Slice(i, i + 1).Resize({img_channels, img_height, img_width});
-      Tensor dst = out->Slice(i, i + 1).Resize(
-          {output_height, output_width, img_channels, kernels[0], kernels[1]});
+        const std::vector<int> dilations({1, 1});
+     // TODO(fuhailong): out_dims has two index,
+     // out_dims[0] and out_dims[1],
+     // {batchsize*output_height*output_width,channel*kernel[0],*kernel[1]},
+     // multi batch ,the first place is output_height[i]*output_width[i].
+        int offset_out = 0;
 
-      math::Im2ColFunctor<math::ColFormat::kOCF, DeviceContext, T> f;
-      auto& dev_ctx = ctx.template device_context<DeviceContext>();
-      f(dev_ctx, src, dilations, strides, paddings, &dst);
-    }
-    out->Resize(out_dims);
+        for (int i = 0; i < batch_size; i++) {
+            const Tensor src =
+                in->Slice(i, i + 1).Resize({img_channels,
+                                            img_height,
+                                            img_width});
+     // TODO(fuhailong): add image real size
+            Tensor dst = out->Slice(offset_out,
+                    offset_out + output_height[i]*output_width[i]).Resize(
+                    {output_height[i], output_width[i],
+                    img_channels, kernels[0], kernels[1]});
+            offset_out += output_height[i]*output_width[i];
 
-    // set lod information
-    // TODO(wanghaoshuang): Move this to InferShape
-    framework::LoD lod(1);
-    lod[0].reserve(batch_size + 1);
-    for (int i = 0, offset = 0; i < batch_size + 1; ++i) {
-      lod[0].push_back(offset);
-      offset += output_height * output_width;
-    }
-    out->set_lod(lod);
-  }
+            math::Im2ColFunctor<math::ColFormat::kOCF, DeviceContext, T> f;
+    // eq, kOCF cnn to rnn format
+            auto& dev_ctx = ctx.template device_context<DeviceContext>();
+            f(dev_ctx, src, dilations, strides, paddings, &dst);
+        }
+        // set lod information
+        // TODO(wanghaoshuang): Move this to InferShape
+        framework::LoD lod(1);
+        lod[0].reserve(batch_size + 1);
+        int offset = 0;
+        lod[0].push_back(offset);
+        for (int i = 0; i < batch_size; ++i) {
+          offset += output_height[i] * output_width[i];
+          lod[0].push_back(offset);
+        }
+        out->set_lod(lod);
+    } else {
+           out->mutable_data<T>(ctx.GetPlace());
+           int output_height = Im2SeqOutputSize(img_height,
+                                                kernels[0],
+                                                paddings[0],
+                                                paddings[2],
+                                                strides[0]);
+           int output_width = Im2SeqOutputSize(img_width,
+                                               kernels[1],
+                                               paddings[1],
+                                               paddings[3],
+                                               strides[1]);
+
+           const std::vector<int> dilations({1, 1});
+           auto out_dims = out->dims();
+           out->Resize({batch_size, out->numel() / batch_size});
+           for (int i = 0; i < batch_size; i++) {
+             const Tensor src =
+                 in->Slice(i, i + 1).Resize({img_channels,
+                                             img_height,
+                                             img_width});
+             Tensor dst = out->Slice(i, i + 1).Resize({output_height,
+                                                       output_width,
+                                                       img_channels,
+                                                       kernels[0],
+                                                       kernels[1]});
+
+             math::Im2ColFunctor<math::ColFormat::kOCF, DeviceContext, T> f;
+             auto& dev_ctx = ctx.template device_context<DeviceContext>();
+             f(dev_ctx, src, dilations, strides, paddings, &dst);
+           }
+           out->Resize(out_dims);
+           // set lod information
+           // TODO(wanghaoshuang): Move this to InferShape
+           framework::LoD lod(1);
+           lod[0].reserve(batch_size + 1);
+           int offset = 0;
+           lod[0].push_back(offset);
+           for (int i = 0; i < batch_size; ++i) {
+             offset += output_height * output_width;
+             lod[0].push_back(offset);
+           }
+           out->set_lod(lod);
+       }
+   }
 };
 
 template <typename DeviceContext, typename T>
