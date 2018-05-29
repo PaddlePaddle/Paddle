467f49a15305c01b0123c156d63265f99a14dd9b
weixing02 <wx_crome@163.com>
2018-05-29 03:30:12 +0000

    adjust

--- python/paddle/fluid/tests/unittests/test_hsigmoid_op.py
+++ python/paddle/fluid/tests/unittests/test_hsigmoid_op.py
@@ -37,15 +37,6 @@ class CodeTable(object):
 
 
 def hsigmoid(x, w, ids, bias, num_classes):
-    # code length = 
-    # initialize pre out with dims={batch_size, code_length}
-    print(
-        "============================================= start ==========================================="
-    )
-    print("x:", x)
-    print("w:", w)
-    print("bias:", bias)
-    print("ids:", ids)
     global pre_output
     batch_size = x.shape[0]
     code_length = find_latest_set(num_classes - 1)
@@ -53,19 +44,12 @@ def hsigmoid(x, w, ids, bias, num_classes):
     pre_output = np.zeros((batch_size, code_length))
     pre_sum = np.zeros((batch_size, 1))
     out = np.zeros((batch_size, 1)).astype("float32")
-    # pre_out += code(bias)
-    print("pre_output init: ", pre_output)
     for i in range(batch_size):
         code_table = CodeTable(num_classes, ids[i])
         length = code_table.get_length()
-        print("code_path: ")
         for j in range(length):
             idx = code_table.cal_index(j)
-            print("{}, \t".format(idx))
             pre_output[i][j] += bias[0][idx]
-    print("pre_output after add bias: ", pre_output)
-    # pre_out += code(w) * x
-    # for i in range(batch_size):
     for j in range(batch_size):
         code_table = CodeTable(num_classes, ids[j])
         length = code_table.get_length()
@@ -75,7 +59,6 @@ def hsigmoid(x, w, ids, bias, num_classes):
             for l in range(x.shape[1]):
                 sum += w[idx][l] * x[j][l]
             pre_output[j][k] += sum
-    print("pre_output after mul w,x: ", pre_output)
     # clip[-40.0, 40.0]
     np.clip(pre_output, -40.0, 40.0)
     # out(i, 0) = \sum_j  bit(i, j) * preout(i, j)
@@ -87,18 +70,11 @@ def hsigmoid(x, w, ids, bias, num_classes):
             if code_table.cal_bit(j):
                 sum += pre_output[i][j]
         out[i] = -1.0 * sum
-    print("out after sum: ", out)
     # soft relu
     np.clip(pre_output, -40.0, 40.0)
     pre_output = np.log(1 + np.exp(pre_output))
-    print("pre_output after softrelu: ", pre_output)
     pre_sum = pre_output.sum(1).reshape((batch_size, 1))
-    print("pre_sum: ", pre_sum)
     out += pre_sum
-    print("out finally: ", out)
-    print(
-        "=========================================== end ============================================="
-    )
     return out
 
 
@@ -114,19 +90,15 @@ class TestHSigmoidOp(OpTest):
         bias = np.random.random((1, num_classes - 1)).astype("float32")
         self.attrs = {'num_classes': num_classes}
         self.inputs = {'X': x, 'W': w, 'Ids': ids, 'Bias': bias}
-        # bias[0][0] -= 0.005
         out = hsigmoid(x, w, ids, bias, num_classes)
-        # bias[0][0] += 0.01
-        # out2 = hsigmoid(x, w, ids, bias, num_classes)
-        # delta = out.sum(0).mean() - out2.sum(0).mean()
-        # print("delta: ", delta)
         self.outputs = {'PreOut': pre_output, 'Out': out}
 
-#    def test_check_output(self):
-#        self.check_output()
+    def test_check_output(self):
+        self.check_output()
 
     def test_check_grad(self):
         self.check_grad(['Bias', 'X', 'W'], 'Out', no_grad_set=set('Ids'))
+
 
 if __name__ == '__main__':
     unittest.main()
