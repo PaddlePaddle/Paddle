e44c278e60603c37640a0a352f4bbb7f8363bebc
tangwei12 <tangwei12@baidu.com>
2018-05-30 21:55:12 +0800

    bug fix about clean

--- python/paddle/fluid/trainer.py
+++ python/paddle/fluid/trainer.py
@@ -425,7 +425,7 @@ class Trainer(object):
         return self._get_parallel_executor()
 
     def _clean_checkpoint(self):
-        if not self.checkpoint:
+        if not self.checkpoint and not self.chief:
             return
         io.clean_checkpoint(checkpoint_dir=self.checkpoint.checkpoint_dir)
 
