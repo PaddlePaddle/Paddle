0e9e15a888795c028f7f5e5bf34e97adde2726a9
dzhwinter <dzhwinter@gmail.com>
2018-06-01 06:55:40 -0700

    init memory

--- python/paddle/fluid/transpiler/memory_transpiler.py
+++ python/paddle/fluid/transpiler/memory_transpiler.py
@@ -14,20 +14,54 @@
 
 from collections import defaultdict
 
+import paddle.fluid.layers as layers
+import paddle.fluid.optimizer as optimizer
+from paddle.fluid.framework import Program, program_guard
+
 
 class SSAGraph(object):
     def __init__(self, program):
         self._program = program
         self._vars = []
-        self._domainance_vars = set()
+        self._domainant_frointers = defaultdict(list)
         self._ops = []
+        self._ssa_vars = defaultdict(list)
 
-    def _build_graph(self):
+    def build_graph(self):
         """
         build ssa graph
         """
         block = self._program.block(0)
-        for op in block.ops():
-            input_vars = op.input_names()
-            if len(inputs) == 0:  # root node
-                self._ops.append(op)
+        self._find_dominators(block)
+
+    def _insert_phi(self):
+        pass
+
+    def _compute_dom_tree(self):
+        pass
+
+    def _find_dominators(self, cfg):
+        counter = []
+        # for var in block:
+
+
+def GenTestProgram():
+    program = Program()
+    with program_guard(program, startup_program=Program()):
+        x = layers.data(name='x', shape=[13], dtype='float32')
+        y_predict = layers.fc(input=x, size=1, act=None)
+        y = layers.data(name='y', shape=[1], dtype='float32')
+        cost = layers.square_error_cost(input=y_predict, label=y)
+        avg_cost = layers.mean(cost)
+        opt = optimizer.SGD(learning_rate=0.001)
+        opt = opt.minimize(avg_cost)
+    return program
+
+
+if __name__ == "__main__":
+    program = GenTestProgram()
+    print("before optimization")
+    print(str(program))
+    # result_program = memory_optimize(program)
+    # print("after optimization")
+    # print(str(program))
