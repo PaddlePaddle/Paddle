036a90f125c3bd5f7caa15241413136f0a768f76
Wu Yi <typhoonzero1986@gmail.com>
2018-06-05 16:46:42 +0800

    Refine rpc client wait sync (#11132)

--- paddle/fluid/operators/fetch_barrier_op.cc
+++ paddle/fluid/operators/fetch_barrier_op.cc
@@ -45,13 +45,13 @@ class FetchBarrierOp : public framework::OperatorBase {
 
     auto rpc_client = detail::RPCClient::GetInstance();
 
-    PADDLE_ENFORCE(rpc_client->Wait());
+    rpc_client->Wait();
 
     for (auto& ep : eps) {
       VLOG(3) << "fetch barrier, ep: " << ep;
       rpc_client->AsyncSendFetchBarrier(ep);
     }
-    PADDLE_ENFORCE(rpc_client->Wait());
+    rpc_client->Wait();
   }
 };
 
