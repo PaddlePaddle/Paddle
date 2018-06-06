b11d15e4f448a6b23be21b8d039c64aa4d0b7c51
Xin Pan <panxin.grad@gmail.com>
2018-06-06 11:13:36 +0800

    follow comments

--- benchmark/fluid/run_fluid_benchmark.sh
+++ benchmark/fluid/run_fluid_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PADDLE_TRAINING_ROLE=PSERVER PADDLE_PSERVER_PORT=7164 PADDLE_PSERVER_IPS=127.0.0.1 PADDLE_TRAINERS=2 PADDLE_CURRENT_IP=127.0.0.1 PADDLE_TRAINER_ID=0 python fluid_benchmark.py --model resnet --device GPU --update_method pserver --iterations=10000 &
+PADDLE_TRAINING_ROLE=PSERVER PADDLE_PSERVER_PORT=7164 PADDLE_PSERVER_IPS=127.0.0.1 PADDLE_TRAINERS=2 PADDLE_CURRENT_IP=127.0.0.1 PADDLE_TRAINER_ID=0 python fluid_benchmark.py --model resnet --device CPU --update_method pserver --iterations=10000 &
 
 sleep 15
 
