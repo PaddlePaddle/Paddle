58cb0f23b05826c035e60229f63ec983d91dce4f
fengjiayi <fengjiayi@baidu.com>
2018-06-06 14:07:29 +0800

    Refine code

--- paddle/fluid/operators/reverse_op.cc
+++ paddle/fluid/operators/reverse_op.cc
@@ -105,10 +105,3 @@ REGISTER_OP_CPU_KERNEL(
     ops::ReverseKernel<paddle::platform::CPUDeviceContext, bool>,
     ops::ReverseKernel<paddle::platform::CPUDeviceContext, float>,
     ops::ReverseKernel<paddle::platform::CPUDeviceContext, double>)
-REGISTER_OP_CPU_KERNEL(
-    reverse_grad, ops::ReverseKernel<paddle::platform::CPUDeviceContext, int>,
-    ops::ReverseKernel<paddle::platform::CPUDeviceContext, uint8_t>,
-    ops::ReverseKernel<paddle::platform::CPUDeviceContext, int64_t>,
-    ops::ReverseKernel<paddle::platform::CPUDeviceContext, bool>,
-    ops::ReverseKernel<paddle::platform::CPUDeviceContext, float>,
-    ops::ReverseKernel<paddle::platform::CPUDeviceContext, double>)
