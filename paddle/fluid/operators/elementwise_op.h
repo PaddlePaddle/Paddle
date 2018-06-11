7bdb573d797e48599fe89e337ccc410e4266c406
Luo Tao <luotao02@baidu.com>
2018-06-11 20:31:41 +0800

    update with comments

--- paddle/fluid/operators/elementwise_op.h
+++ paddle/fluid/operators/elementwise_op.h
@@ -94,7 +94,7 @@ For example:
 
     shape(X) = (2, 3, 4, 5), shape(Y) = (,)
     shape(X) = (2, 3, 4, 5), shape(Y) = (5,)
-    shape(X) = (2, 3, 4, 5), shape(Y) = (4, 5)
+    shape(X) = (2, 3, 4, 5), shape(Y) = (4, 5), with axis=-1(default) or axis=2
     shape(X) = (2, 3, 4, 5), shape(Y) = (3, 4), with axis=1
     shape(X) = (2, 3, 4, 5), shape(Y) = (2), with axis=0
     shape(X) = (2, 3, 4, 5), shape(Y) = (2, 1), with axis=0
