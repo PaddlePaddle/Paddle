1e870512579b960df18fba207f298ebeae9c1def
dongzhihong <dzhwinter@gmail.com>
2018-06-15 09:46:40 +0800

    "add program reference"

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -1034,6 +1034,29 @@ class Block(object):
 
 
 class Program(object):
+    """
+    Python Program. Beneath it is a ProgramDesc, which is used for create c++ Program.
+    A program is a self-contained programing language like container. It has at least one Block, and the control flow op like conditional_block, while_op is included, then it will contains nested block.
+    Please reference the framework.proto for details.
+    Notes: we have default_startup_program and default_main_program by default, a pair of them will shared the parameters. The default_startup_program only run once to initialize parameters,  default_main_program run in every minibatch and adjust the weights.
+
+    Args:
+        None
+
+    Returns:
+        Python Program
+
+    Examples:
+       .. code-block:: python
+
+         main_program = Program()
+         startup_program = Program()
+         with fluid.program_guard(main_program=main_program, startup_program=startup_program):
+            fluid.layers.data(name="x", shape=[-1, 784], dtype='float32')
+            fluid.layers.data(name="y", shape=[-1, 1], dtype='int32')
+            fluid.layers.fc(name="fc", shape=[10], dtype='float32', act="relu")
+
+    """
     def __init__(self):
         self.desc = core.ProgramDesc()
         self.blocks = [Block(self, 0)]
