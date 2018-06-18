12a3b27fa885ca320880093a540d3e3e8be8f7f5
Superjomn <yanchunwei@outlook.com>
2018-06-18 03:52:18 +0000

    fix tests

--- paddle/fluid/inference/analysis/argument.h
+++ paddle/fluid/inference/analysis/argument.h
@@ -41,11 +41,11 @@ struct Argument {
   std::unique_ptr<framework::proto::ProgramDesc> origin_program_desc;
 };
 
-#define ANALYSIS_ARGUMENT_CHECK_FIELD(field__)               \
-  if (!(field__)) {                                          \
-    LOG(ERROR) << "field " << #field__ << " should be set."; \
-    return false;                                            \
-  }
+#define ANALYSIS_ARGUMENT_CHECK_FIELD(field__)                 \
+  if (__builtin_expect(static_cast<bool>(condition), 0))) {    \
+      LOG(ERROR) << "field " << #field__ << " should be set."; \
+      return false;                                            \
+    }
 
 }  // namespace analysis
 }  // namespace inference
