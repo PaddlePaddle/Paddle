c073bb3b2c1381d2967785b956381be231fa6583
tangwei12 <tangwei12@baidu.com>
2018-06-20 12:51:44 +0800

    code style

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -382,8 +382,7 @@ class Operator(object):
         'rnn_memory_helper_grad', 'conditional_block', 'while', 'send', 'recv',
         'listen_and_serv', 'parallel_do', 'save_combine', 'load_combine',
         'ncclInit', 'channel_create', 'channel_close', 'channel_send',
-        'channel_recv', 'select', 'checkpoint_notify'
-        , 'gen_nccl_id'
+        'channel_recv', 'select', 'checkpoint_notify', 'gen_nccl_id'
     }
 
     def __init__(self,
@@ -1022,7 +1021,7 @@ class Block(object):
                 name=var.name, persistable=var.persistable, type=var.type)
         elif var.type == core.VarDesc.VarType.RAW:
             ret_var = self.create_var(
-                name=var.name, persistable=var.persistable, type=var.type)      
+                name=var.name, persistable=var.persistable, type=var.type)
         elif var.type == core.VarDesc.VarType.SELECTED_ROWS:
             ret_var = self.create_var(
                 name=var.name,
@@ -1465,7 +1464,7 @@ def get_var(name, program=None):
     Args:
         name(str): name of the variable
         program(Program|None): program object.
-             If None, default_global_program() will be used.
+        If None, default_global_program() will be used.
 
     Returns:
         Variable
