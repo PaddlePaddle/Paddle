d7d61a2ba89e472a85141712a0b2b63ed32a0592
Superjomn <yanchunwei@outlook.com>
2018-06-20 09:09:13 +0000

    styled

--- paddle/contrib/inference/test_paddle_inference_api_tensorrt_subgraph_engine.cc
+++ paddle/contrib/inference/test_paddle_inference_api_tensorrt_subgraph_engine.cc
@@ -1,6 +1,20 @@
+// Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <gflags/gflags.h>
-#include <gtest/gtest.h>
 #include <glog/logging.h>
+#include <gtest/gtest.h>
 #include "paddle/contrib/inference/paddle_inference_api.h"
 
 namespace paddle {
@@ -24,9 +38,9 @@ void Main(bool use_gpu) {
 
     PaddleBuf buf{.data = data, .length = sizeof(data)};
     PaddleTensor tensor{.name = "",
-        .shape = std::vector<int>({4, 1}),
-        .data = buf,
-        .dtype = PaddleDType::INT64};
+                        .shape = std::vector<int>({4, 1}),
+                        .data = buf,
+                        .dtype = PaddleDType::INT64};
 
     // For simplicity, we set all the slots with the same data.
     std::vector<PaddleTensor> slots(4, tensor);
