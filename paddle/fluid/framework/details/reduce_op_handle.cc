da556ed6d441f5438cab75c8e0dd82ed62344633
chengduo <zhaochengduo@baidu.com>
2018-06-22 09:39:39 +0800

    enhance ParallelExecutor stable (#11637)

--- paddle/fluid/framework/details/reduce_op_handle.cc
+++ paddle/fluid/framework/details/reduce_op_handle.cc
@@ -80,9 +80,7 @@ void ReduceOpHandle::RunImpl() {
   }
 
   if (pre_in_var->IsType<framework::SelectedRows>()) {
-    // FIXME(zcd): A temporary fix for some language model that has sparse
-    // parameter.
-    this->RunAndRecordEventNoMutex([&] {
+    this->RunAndRecordEvent([&] {
       std::vector<const SelectedRows *> in_selected_rows =
           GetInputValues<SelectedRows>(in_var_handles, var_scopes);
       GatherSelectedRows(in_selected_rows, in_places, dev_ctxes_, t_out_p,
