bdebb9b8d5d93ea3740fece2b893f961aee10035
Superjomn <yanchunwei@outlook.com>
2018-06-27 06:40:32 +0000

    update

--- cmake/inference_lib.cmake
+++ cmake/inference_lib.cmake
@@ -149,31 +149,33 @@ copy(memory_lib
   DSTS ${dst_dir}/${module} ${dst_dir}/${module}/detail
 )
 
+set(inference_deps paddle_fluid_shared paddle_fluid)
+
+if(WITH_CONTRIB)
+    message(STATUS "installing contrib")
+    set(contrib_dst_dir "${FLUID_INSTALL_DIR}/contrib/inference")
+    if (WITH_ANAKIN)
+        copy(contrib_anakin_inference_lib DEPS paddle_inference_api inference_anakin_api
+            SRCS
+            ${PADDLE_BINARY_DIR}/paddle/contrib/inference/libinference_anakin_api* # compiled anakin api
+            ${PADDLE_BINARY_DIR}/third_party/install/anakin/*.tar.gz # anakin release
+            DSTS ${contrib_dst_dir}/anakin ${contrib_dst_dir}/anakin)
+        list(APPEND inference_deps contrib_anakin_inference_lib)
+   endif()
+
+  copy(contrib_inference_lib DEPS paddle_inference_api
+        SRCS ${PADDLE_SOURCE_DIR}/paddle/contrib/inference/paddle_inference_api.h
+        ${PADDLE_BINARY_DIR}/paddle/contrib/inference/libpaddle_inference_api.*
+        DSTS ${contrib_dst_dir} ${contrib_dst_dir})
+  list(APPEND inference_deps contrib_inference_lib)
+endif()
+
 set(module "inference")
-copy(inference_lib DEPS paddle_fluid_shared paddle_fluid
+copy(inference_lib DEPS ${inference_deps}
   SRCS ${src_dir}/${module}/*.h ${PADDLE_BINARY_DIR}/paddle/fluid/inference/libpaddle_fluid.*
   DSTS ${dst_dir}/${module} ${dst_dir}/${module}
 )
 
-if(WITH_CONTRIB)
-   set(contrib_dst_dir "${FLUID_INSTALL_DIR}/contrib/inference")
-   if (NOT WITH_ANAKIN)
-      copy(contrib_inference_lib DEPS paddle_inference_api
-            SRCS ${PADDLE_SOURCE_DIR}/paddle/contrib/inference/paddle_inference_api.h
-            ${PADDLE_BINARY_DIR}/paddle/contrib/inference/libpaddle_inference_api.*
-            DSTS ${contrib_dst_dir} ${contrib_dst_dir}
-      )
-   else()
-      copy(contrib_inference_lib DEPS paddle_inference_api
-            SRCS ${PADDLE_SOURCE_DIR}/paddle/contrib/inference/paddle_inference_api.h
-            ${PADDLE_BINARY_DIR}/paddle/contrib/inference/libpaddle_inference_api.*
-            ${PADDLE_BINARY_DIR}/paddle/contrib/inference/libinference_anakin_api* # compiled anakin api
-            ${PADDLE_BINARY_DIR}/third_party/install/anakin/*.tar.gz # anakin release
-            DSTS ${contrib_dst_dir} ${contrib_dst_dir}
-            ${contrib_dst_dir}/anakin ${contrib_dst_dir}/anakin
-      )
-   endif()
-endif()
 
 set(module "platform")
 copy(platform_lib DEPS profiler_py_proto
