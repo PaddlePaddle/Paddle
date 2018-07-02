4ccd399b571655f963fba93a54dde2acd9bada60
chenweihang <sunny_cwh@163.com>
2018-07-02 03:23:27 +0000

    complete unsqueeze op and related unittest.

--- python/paddle/fluid/tests/unittests/test_unsqueeze_op.py
+++ python/paddle/fluid/tests/unittests/test_unsqueeze_op.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ from op_test import OpTest
 
 
 # Correct: General.
-class TestSqueezeOp1(OpTest):
+class TestUnsqueezeOp(OpTest):
     def setUp(self):
         ori_shape = (3, 5)
         axes = (0, 2)
@@ -38,10 +38,86 @@ class TestSqueezeOp1(OpTest):
 
 
 # Correct: There is mins axis.
-class TestSqueezeOp2(OpTest):
+class TestUnsqueezeOp2(OpTest):
     def setUp(self):
         ori_shape = (3, 5)
         axes = (0, -2)
+        new_shape = (1, 3, 1, 5)
+
+        self.op_type = "unsqueeze"
+        self.inputs = {"X": np.random.random(ori_shape).astype("float32")}
+        self.attrs = {"axes": axes, "inpalce": False}
+        self.outputs = {"Out": self.inputs["X"].reshape(new_shape)}
+
+        def test_check_output(self):
+            self.check_output()
+
+        def test_check_grad(self):
+            self.check_grad(["X"], "Out")
+
+
+# Correct: There is duplicated axis.
+class TestUnsqueezeOp3(OpTest):
+    def setUp(self):
+        ori_shape = (3, 2, 5)
+        axes = (0, 3, 3)
+        new_shape = (1, 3, 2, 1, 1, 5)
+
+        self.op_type = "unsqueeze"
+        self.inputs = {"X": np.random.random(ori_shape).astype("float32")}
+        self.attrs = {"axes": axes, "inpalce": False}
+        self.outputs = {"Out": self.inputs["X"].reshape(new_shape)}
+
+        def test_check_output(self):
+            self.check_output()
+
+        def test_check_grad(self):
+            self.check_grad(["X"], "Out")
+
+
+# Error: Output dimension is error.
+class TestUnsqueezeOp4(OpTest):
+    def setUp(self):
+        ori_shape = (3, 2, 5)
+        axes = (0, 3)
+        new_shape = (1, 3, 2, 2, 5)
+
+        self.op_type = "unsqueeze"
+        self.inputs = {"X": np.random.random(ori_shape).astype("float32")}
+        self.attrs = {"axes": axes, "inpalce": False}
+        self.outputs = {"Out": self.inputs["X"].reshape(new_shape)}
+
+        def test_check_output(self):
+            self.check_output()
+
+        def test_check_grad(self):
+            self.check_grad(["X"], "Out")
+
+
+# Error: Input axes is invalid case 1.
+class TestUnsqueezeOp5(OpTest):
+    def setUp(self):
+        ori_shape = (3, 2, 5)
+        axes = (0, 5)
+        new_shape = (1, 3, 1, 5)
+
+        self.op_type = "unsqueeze"
+        self.inputs = {"X": np.random.random(ori_shape).astype("float32")}
+        self.attrs = {"axes": axes, "inpalce": False}
+        self.outputs = {"Out": self.inputs["X"].reshape(new_shape)}
+
+        def test_check_output(self):
+            self.check_output()
+
+        def test_check_grad(self):
+            self.check_grad(["X"], "Out")
+
+
+# Error: Input axes is invalid case 2.
+class TestUnsqueezeOp5(OpTest):
+    def setUp(self):
+        ori_shape = (3, 2, 5)
+        axes = (0, 2, 10)
         new_shape = (1, 3, 1, 5)
 
         self.op_type = "unsqueeze"
@@ -75,23 +151,23 @@ class TestUnsqueezeOpInplace1(OpTest):
         self.check_grad(["X"], "Out")
 
 
-# Correct: Inplace. There is mins axis.
+# Correct: Inplace. There is duplicated axis.
 class TestUnsqueezeOpInplace2(OpTest):
     def setUp(self):
-        ori_shape = (3, 5)
-        axes = (0, -2)
-        new_shape = (1, 3, 1, 5)
+        ori_shape = (3, 2, 5)
+        axes = (0, 3, 3)
+        new_shape = (1, 3, 2, 1, 1, 5)
 
         self.op_type = "unsqueeze"
         self.inputs = {"X": np.random.random(ori_shape).astype("float32")}
         self.attrs = {"axes": axes, "inpalce": True}
         self.outputs = {"Out": self.inputs["X"].reshape(new_shape)}
 
-    def test_check_output(self):
-        self.check_output()
+        def test_check_output(self):
+            self.check_output()
 
-    def test_check_grad(self):
-        self.check_grad(["X"], "Out")
+        def test_check_grad(self):
+            self.check_grad(["X"], "Out")
 
 
 if __name__ == "__main__":
