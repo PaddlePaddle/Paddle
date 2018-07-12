becc1c2460e971ffc67d4ab941f56a97286bab0b
minqiyang <minqiyang@baidu.com>
2018-07-12 19:05:00 +0800

    Remove unused requirements Add python3 ABI to paddle_build.sh

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -78,7 +78,7 @@ function cmake_gen() {
             PYTHON_FLAGS="-DPYTHON_EXECUTABLE:FILEPATH=/opt/python/cp27-cp27mu/bin/python
         -DPYTHON_INCLUDE_DIR:PATH=/opt/python/cp27-cp27mu/include/python2.7
         -DPYTHON_LIBRARIES:FILEPATH=/opt/_internal/cpython-2.7.11-ucs4/lib/libpython2.7.so"
-        elif [ "$1" == "3.5" ]; then
+        elif [ "$1" == "cp35-cp35m" ]; then
             export LD_LIBRARY_PATH=/opt/_internal/cpython-3.5.1/lib/
             export PATH=/opt/_internal/cpython-3.5.1/bin/:${PATH}
             export PYTHON_FLAGS="-DPYTHON_EXECUTABLE:FILEPATH=/opt/_internal/cpython-3.5.1/bin/python3
