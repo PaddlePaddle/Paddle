09a7588228dc6d64de40d82dacafeb023b452775
Luo Tao <luotao02@baidu.com>
2018-07-16 12:41:52 +0800

    refine the switch name

--- paddle/contrib/inference/demo_ci/run.sh
+++ paddle/contrib/inference/demo_ci/run.sh
@@ -1,7 +1,7 @@
 set -x
 PADDLE_ROOT=$1
-WITH_MKL=$2
-WITH_GPU=$3
+TURN_ON_MKL=$2 # use MKL or Openblas
+TEST_GPU_CPU=$3 # test both GPU/CPU mode or only CPU mode
 if [ $2 == ON ]; then
   # You can export yourself if move the install path
   MKL_LIB=${PADDLE_ROOT}/build/fluid_install_dir/third_party/install/mklml/lib
@@ -19,9 +19,9 @@ cd build
 for WITH_STATIC_LIB in ON OFF; do
   rm -rf *
   cmake .. -DPADDLE_LIB=${PADDLE_ROOT}/build/fluid_install_dir/ \
-    -DWITH_MKL=$WITH_MKL \
+    -DWITH_MKL=$TURN_ON_MKL \
     -DDEMO_NAME=simple_on_word2vec \
-    -DWITH_GPU=$WITH_GPU \
+    -DWITH_GPU=$TEST_GPU_CPU \
     -DWITH_STATIC_LIB=$WITH_STATIC_LIB
   make -j
   for use_gpu in $use_gpu_list; do
