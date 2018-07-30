3283a977275353ac5af58ec07a1bb3441be8b100
Superjomn <yanchunwei@outlook.com>
2018-07-30 11:11:35 +0000

    update

--- paddle/fluid/inference/analysis/model_store_pass.h
+++ paddle/fluid/inference/analysis/model_store_pass.h
@@ -1,3 +1,17 @@
+// Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /*
  * This file defines ModelStorePass, which store the runtime DFG to a Paddle
  * model in the disk, and that model can be reloaded for prediction.
@@ -20,8 +34,7 @@ class ModelStorePass : public DataFlowGraphPass {
     return true;
   }
 
-
-  void Run(DataFlowGraph *x) override;
+  void Run(DataFlowGraph* x) override;
 
   std::string repr() const override { return "DFG-store-pass"; }
   std::string description() const override {
