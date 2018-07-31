21cc1cd77170afa51c18da3229dfa554822edfbe
cuichaowen <939136265@qq.com>
2018-07-31 20:23:58 +0800

    Improve anakin feature (#11961)

--- paddle/fluid/inference/api/paddle_inference_api.h
+++ paddle/fluid/inference/api/paddle_inference_api.h
@@ -126,9 +126,11 @@ struct NativeConfig : public PaddlePredictor::Config {
 
 // Configurations for Anakin engine.
 struct AnakinConfig : public PaddlePredictor::Config {
+  enum TargetType { NVGPU = 0, X86 };
   int device;
   std::string model_file;
   int max_batch_size{-1};
+  TargetType target_type;
 };
 
 struct TensorRTConfig : public NativeConfig {
