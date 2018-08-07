a26f644fb25ff888cd86f091438e0e89d830de23
typhoonzero <typhoonzero1986@gmail.com>
2018-08-07 14:58:11 +0800

    update by comment

--- python/paddle/fluid/tests/unittests/dist_se_resnext.py
+++ python/paddle/fluid/tests/unittests/dist_se_resnext.py
@@ -124,7 +124,7 @@ class SE_ResNeXt():
 
         pool = fluid.layers.pool2d(
             input=conv, pool_size=7, pool_type='avg', global_pooling=True)
-        drop = fluid.layers.dropout(x=pool, dropout_prob=0.2, seed=1)
+        drop = fluid.layers.dropout(x=pool, dropout_prob=0.2)
         stdv = 1.0 / math.sqrt(drop.shape[1] * 1.0)
         out = fluid.layers.fc(input=drop, size=class_dim, act='softmax')
         return out
