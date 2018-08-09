14967f6d900109ef6f78063021c5606aafcf1f90
superjomn <yanchunwei@outlook.com>
2018-08-09 08:11:36 +0000

    update delete

--- paddle/fluid/inference/api/api.cc
+++ paddle/fluid/inference/api/api.cc
@@ -87,7 +87,7 @@ void PaddleBuf::Reset(void* data, size_t length) {
 void PaddleBuf::Free() {
   if (memory_owned_ && data_) {
     assert(length_ > 0);
-    delete static_cast<char*>(data_);
+    delete[] static_cast<char*>(data_);
     data_ = nullptr;
     length_ = 0;
   }
