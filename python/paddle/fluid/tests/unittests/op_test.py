d04b0b55a443b1f8465977e326da26c5801ce0f2
minqiyang <minqiyang@baidu.com>
2018-08-10 17:00:21 +0800

    Fix cv2 issues

--- python/paddle/fluid/tests/unittests/op_test.py
+++ python/paddle/fluid/tests/unittests/op_test.py
@@ -362,9 +362,14 @@ class OpTest(unittest.TestCase):
 
     def check_output_customized(self, checker):
         places = self._get_places()
+        import sys
+        print('places', places)
         for place in places:
             outs = self.calc_output(place)
             outs = [np.array(out) for out in outs]
+            import sys
+            print('outs', outs)
+            sys.stdout.flush()
             checker(outs)
 
     def __assert_is_close(self, numeric_grads, analytic_grads, names,
