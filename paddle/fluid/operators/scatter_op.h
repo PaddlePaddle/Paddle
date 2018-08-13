b10d24423c737a096e7ed701c93903d9eed08613
ktlichkid <liqingsheng@baidu.com>
2018-08-13 11:46:08 +0000

    Remove Data Sharing between input and output in scatter_op

--- paddle/fluid/operators/scatter_op.h
+++ paddle/fluid/operators/scatter_op.h
@@ -35,8 +35,8 @@ class ScatterOpKernel : public framework::OpKernel<T> {
     auto *Out = ctx.Output<Tensor>("Out");
 
     // In place output: Out = X, Out[Ids] += Updates
-    Out->ShareDataWith(*X);
-    // Apply ScatterUpdate: Out[index] += Updates[:]
+	framework::TensorCopy(*X, ctx.GetPlace(), Out);
+	// Apply ScatterUpdate: Out[index] += Updates[:]
     ScatterAssign<T>(ctx.device_context(), *Updates, *Ids, Out);
   }
 };
