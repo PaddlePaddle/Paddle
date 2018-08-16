1ef5f2c3e834a26137907d9150307ba257fa2568
minqiyang <minqiyang@baidu.com>
2018-08-16 18:12:28 +0800

    Make flowers reader and parallel_executor more efficient

--- python/paddle/dataset/flowers.py
+++ python/paddle/dataset/flowers.py
@@ -120,7 +120,7 @@ def reader_creator(data_file,
                 file = file.strip()
                 batch = None
                 with open(file, 'rb') as f:
-                    batch = pickle.loads(f.read())
+                    batch = pickle.load(f)
                 data = batch['data']
                 labels = batch['label']
                 for sample, label in zip(data, batch['label']):
