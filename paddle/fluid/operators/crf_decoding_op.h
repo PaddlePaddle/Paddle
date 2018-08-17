4e49d6a6d6701c614a4265f7bef1d348bac8ea89
Yihua Xu <yihuax.xu@intel.com>
2018-08-17 10:37:09 +0800

    Add the support of AVX512 instruction to optimize CRF Decoding

--- paddle/fluid/operators/crf_decoding_op.h
+++ paddle/fluid/operators/crf_decoding_op.h
@@ -87,8 +87,12 @@ class CRFDecodingOpKernel : public framework::OpKernel<T> {
 
 #ifdef __AVX__
     //Only optimize for float type.
-    if (std::is_same<T, float>::value) {
+#ifdef __AVX512F__
+      size_t step_size = 16;
+#else
       size_t step_size = 8;
+#endif
+    if (std::is_same<T, float>::value && (tag_num >= step_size)) {
       size_t steps = tag_num/step_size;
       size_t remain = tag_num%step_size;
       int last_offset = (int)remain - (int)step_size;
@@ -96,13 +100,21 @@ class CRFDecodingOpKernel : public framework::OpKernel<T> {
       //Setup the alpha initial value.
       size_t i_offset = 0;
       for (size_t i = 0; i <= steps; ++i) {
+#ifdef __AVX512F__
+        __m512 w_content, x_content, alpha_content;
+
+        w_content = _mm512_loadu_ps((const float*)(w + i_offset));
+        x_content = _mm512_loadu_ps((const float*)(x + i_offset));
+        alpha_content = _mm512_add_ps(w_content, x_content);
+        _mm512_storeu_ps((float *)(alpha_value + i_offset), alpha_content);
+#else
         __m256 w_content, x_content, alpha_content;
 
         w_content = _mm256_loadu_ps((const float*)(w + i_offset));
         x_content = _mm256_loadu_ps((const float*)(x + i_offset));
         alpha_content = _mm256_add_ps(w_content, x_content);
         _mm256_storeu_ps((float *)(alpha_value + i_offset), alpha_content);
-
+#endif
         i_offset += step_size;
         if ( i == steps - 1 ) {
           if (remain > 0) {
@@ -118,12 +130,36 @@ class CRFDecodingOpKernel : public framework::OpKernel<T> {
       for (size_t k = 1; k < seq_len; ++k) {
         size_t j_offset = 0;
         for (size_t j = 0; j <= steps; ++j) {
+#ifdef __AVX512F__
+          __m512 max_score = _mm512_set1_ps(
+                    -std::numeric_limits<T>::max());
+          __m512i max_j = _mm512_setzero_si512();
+#else
           __m256 max_score = _mm256_set1_ps(
                     -std::numeric_limits<T>::max());
           __m256i max_j = _mm256_set1_epi32(0);
+#endif
           size_t trans_offset = state_trans_base_idx * tag_num + j_offset;
           for (size_t i = 0; i < tag_num; ++i) {
 
+#ifdef __AVX512F__
+            __m512 alpha_content = _mm512_set1_ps(*(const float*)
+                                      (alpha_value + seq_offset + i));
+            __m512 w_content = _mm512_loadu_ps((const float*)(w +
+                                      trans_offset));
+            __m512 score_v = _mm512_add_ps(alpha_content, w_content);
+
+            __mmask16 mask = _mm512_cmp_ps_mask(score_v, max_score, _CMP_GT_OS);
+
+            max_j = _mm512_mask_set1_epi32(max_j, mask, i);              
+
+            max_score = _mm512_max_ps(max_score, score_v);
+            if ( i == tag_num - 1 ) {
+              __m512 x_content = _mm512_loadu_ps((const float*)(x +
+                                              seq_offset + tag_num + j_offset));
+              max_score = _mm512_add_ps(max_score, x_content);
+            }
+#else
             __m256 alpha_content = _mm256_broadcast_ss((const float*)
                                       (alpha_value + seq_offset + i));
             __m256 w_content = _mm256_loadu_ps((const float*)(w +
@@ -132,29 +168,27 @@ class CRFDecodingOpKernel : public framework::OpKernel<T> {
 
             __m256 mask = _mm256_cmp_ps(score_v, max_score, _CMP_GT_OS);
 
-            if ( i > 0) {
 #ifdef __AVX2__
-              max_j = _mm256_or_si256(
-                        _mm256_andnot_si256((__m256i)mask, max_j),
-                        _mm256_and_si256((__m256i)mask, _mm256_set1_epi32(i)));
+            max_j = _mm256_or_si256(
+                      _mm256_andnot_si256((__m256i)mask, max_j),
+                      _mm256_and_si256((__m256i)mask, _mm256_set1_epi32(i)));
 #else
-              __m128i lo_max_j = _mm256_extractf128_si256(max_j, 0);
-              __m128i hi_max_j = _mm256_extractf128_si256(max_j, 1);
-              __m128i lo_mask = _mm256_extractf128_si256((__m256i)mask, 0);
-              __m128i hi_mask = _mm256_extractf128_si256((__m256i)mask, 1);
+            __m128i lo_max_j = _mm256_extractf128_si256(max_j, 0);
+            __m128i hi_max_j = _mm256_extractf128_si256(max_j, 1);
+            __m128i lo_mask = _mm256_extractf128_si256((__m256i)mask, 0);
+            __m128i hi_mask = _mm256_extractf128_si256((__m256i)mask, 1);
 
-              lo_max_j = _mm_andnot_si128(lo_mask, lo_max_j);
-              hi_max_j = _mm_andnot_si128(hi_mask, hi_max_j);
-              lo_mask = _mm_and_si128(lo_mask, _mm_set1_epi32(i));
-              hi_mask = _mm_and_si128(hi_mask, _mm_set1_epi32(i));
+            lo_max_j = _mm_andnot_si128(lo_mask, lo_max_j);
+            hi_max_j = _mm_andnot_si128(hi_mask, hi_max_j);
+            lo_mask = _mm_and_si128(lo_mask, _mm_set1_epi32(i));
+            hi_mask = _mm_and_si128(hi_mask, _mm_set1_epi32(i));
 
-              lo_max_j = _mm_or_si128(lo_mask, lo_max_j);
-              hi_max_j = _mm_or_si128(hi_mask, hi_max_j);
+            lo_max_j = _mm_or_si128(lo_mask, lo_max_j);
+            hi_max_j = _mm_or_si128(hi_mask, hi_max_j);
 
-              max_j = _mm256_insertf128_si256(max_j, lo_max_j, 0);
-              max_j = _mm256_insertf128_si256(max_j, hi_max_j, 1);
+            max_j = _mm256_insertf128_si256(max_j, lo_max_j, 0);
+            max_j = _mm256_insertf128_si256(max_j, hi_max_j, 1);
 #endif
-            }
 
             max_score = _mm256_max_ps(max_score, score_v);
             if ( i == tag_num - 1 ) {
@@ -162,14 +196,21 @@ class CRFDecodingOpKernel : public framework::OpKernel<T> {
                                               seq_offset + tag_num + j_offset));
               max_score = _mm256_add_ps(max_score, x_content);
             }
-
+#endif
             trans_offset += tag_num;
           }
 
+#ifdef __AVX512F__
+          _mm512_storeu_ps((float *)(alpha_value +
+                            seq_offset + tag_num + j_offset), max_score);
+          _mm512_storeu_si512((__m512i *)(track_value + seq_offset + tag_num +
+                              j_offset), max_j);
+#else
           _mm256_storeu_ps((float *)(alpha_value +
                             seq_offset + tag_num + j_offset), max_score);
           _mm256_storeu_si256((__m256i *)(track_value + seq_offset + tag_num +
                               j_offset), max_j);
+#endif
 
           //Calculate the offset of next step
           j_offset += step_size;
@@ -184,7 +225,6 @@ class CRFDecodingOpKernel : public framework::OpKernel<T> {
 
         seq_offset += tag_num;
       }
-
     } else {
       for (size_t i = 0; i < tag_num; ++i) alpha_value[i] = w[i] + x[i];
 
