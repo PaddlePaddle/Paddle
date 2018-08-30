79c65d16626e690e4d1c40063bdabf8eed5ad2a7
Wang,Jeff <wangjeff@baidu.com>
2018-08-30 15:00:32 -0700

    add a smaller lib

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -488,6 +488,25 @@ EOF
     make -j `nproc` copy_paddle_pybind 
 }
 
+function gen_doc_lib_lite2() {
+    mkdir -p ${PADDLE_ROOT}/build
+    cd ${PADDLE_ROOT}/build
+    cat <<EOF
+    ========================================
+    Building documentation library ...
+    In /paddle/build
+    ========================================
+EOF
+    cmake .. \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DWITH_DOC=ON \
+        -DWITH_GPU=OFF \
+        -DWITH_MKL=OFF \
+        -DWITH_FLUID_ONLY=ON
+
+    make -j `nproc` framework_py_proto
+}
+
 function gen_html() {
     cat <<EOF
     ========================================
@@ -650,6 +669,9 @@ function main() {
         ;;
       gen_doc_lib_lite)
         gen_doc_lib_lite
+        ;;
+      gen_doc_lib_lite2)
+        gen_doc_lib_lite2
         ;;
       html)
         gen_html
