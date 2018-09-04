239b244f05e2dba91b8a89cb9ae6fba2d6178bfb
qiaolongfei <qiaolongfei@baidu.com>
2018-09-04 19:10:43 +0800

    update test_rmsprop_op.py

--- python/paddle/fluid/tests/unittests/test_rmsprop_op.py
+++ python/paddle/fluid/tests/unittests/test_rmsprop_op.py
@@ -15,15 +15,16 @@
 from __future__ import print_function
 
 import unittest
-import numpy as np
-from op_test import OpTest
-from paddle.fluid.op import Operator
 
+import numpy as np
 import paddle.fluid.core as core
+from paddle.fluid.op import Operator
 
 
 class TestBase(unittest.TestCase):
-    def setup(self):
+    def setup(self, centered, epsilon=1e-6):
+        np.random.seed(5)  # fix seed
+
         self.param_name = "param"
         self.param = np.random.random((123, 321)).astype("float32")
 
@@ -42,15 +43,22 @@ class TestBase(unittest.TestCase):
         self.moment_name = "moment"
         self.moment = np.zeros((123, 321)).astype("float32")
 
-        self.epsilon = 1e-6
+        self.epsilon = epsilon
         self.decay = 0.9
         self.momentum = 0.0
-        self.centered = False
+        self.centered = centered
 
         self.ms_out = self.decay * self.mean_square + (1 - self.decay
                                                        ) * self.grad * self.grad
-        self.moment_out = self.momentum * self.moment + \
-                          self.learning_rate * self.grad / np.sqrt(self.ms_out + self.epsilon)
+        if centered:
+            self.mg_out = self.decay * self.mean_grad + (1 - self.decay
+                                                         ) * self.grad
+            self.moment_out = self.momentum * self.moment + \
+                              self.learning_rate * self.grad / np.sqrt(self.ms_out - np.square(self.mg_out) + self.epsilon)
+        else:
+            self.moment_out = self.momentum * self.moment + \
+                              self.learning_rate * self.grad / np.sqrt(self.ms_out + self.epsilon)
+
         self.param_out = self.param - self.moment_out
 
     def check(self,
@@ -67,9 +75,9 @@ class TestBase(unittest.TestCase):
             + str(expect_t) + "\n" + "But Got" + str(actual_t))
 
 
-class TestRmspropOp3(TestBase):
-    def check_with_place(self, place):
-        self.setup()
+class TestRmspropOp(TestBase):
+    def check_with_place(self, place, centered, epsilon):
+        self.setup(centered, epsilon)
         scope = core.Scope()
 
         # create and initialize Param Variable
@@ -105,7 +113,7 @@ class TestRmspropOp3(TestBase):
                 ParamOut=self.param_name,
                 MeanSquareOut=self.mean_square_name,
                 MomentOut=self.moment_name,
-                MeanGradOut=self.mean_grad,
+                MeanGradOut=self.mean_grad_name,
                 epsilon=self.epsilon,
                 decay=self.decay,
                 momentum=self.momentum,
@@ -128,152 +136,43 @@ class TestRmspropOp3(TestBase):
 
         rmsprop_op.run(scope, place)
 
-        self.check(np.array(param), self.param_out, place, self.param_name)
+        atol = 1e-5
+        equal_nan = False
+
+        if self.centered:
+            atol = 1e-3
+            equal_nan = True
+
         self.check(
             np.array(mean_square), self.ms_out, place, self.mean_square_name)
-        self.check(np.array(moment), self.moment_out, place, self.moment_name)
+        self.check(
+            np.array(moment),
+            self.moment_out,
+            place,
+            self.moment_name,
+            atol=atol,
+            equal_nan=equal_nan)
+        self.check(
+            np.array(param),
+            self.param_out,
+            place,
+            self.param_name,
+            atol=atol,
+            equal_nan=equal_nan)
+
+        if self.centered:
+            self.check(
+                np.array(mean_grad), self.mg_out, place, self.mean_grad_name)
 
     def test_rmsprop(self):
         places = [core.CPUPlace()]
         if core.is_compiled_with_cuda():
             places.append(core.CUDAPlace(0))
         for place in places:
-            self.check_with_place(place)
-
-
-class TestRmspropOp1(OpTest):
-    ''' Test RMSProp with explicit inputs
-    '''
-
-    def setUp(self):
-        self.op_type = "rmsprop"
-
-        param = np.random.random((123, 321)).astype("float32")
-        mean_square = np.random.random((123, 321)).astype("float32")
-        learning_rate = np.array([0.01]).astype("float32")
-        grad = np.random.random((123, 321)).astype("float32")
-        moment = np.zeros((123, 321)).astype("float32")
-
-        epsilon = 1e-6
-        decay = 0.9
-        momentum = 0.0
-
-        self.inputs = {
-            'Param': param,
-            'MeanSquare': mean_square,
-            'MeanGrad': mean_square,  # just a place holder
-            'LearningRate': learning_rate,
-            'Grad': grad,
-            'Moment': moment,
-        }
-
-        self.attrs = {'epsilon': epsilon, 'decay': decay, 'momentum': momentum}
-
-        ms_out = decay * mean_square + (1 - decay) * grad * grad
-        moment_out = momentum * moment + \
-            learning_rate * grad / np.sqrt(ms_out + epsilon)
-        param_out = param - moment_out
-
-        self.outputs = {
-            'ParamOut': param_out,
-            'MomentOut': moment_out,
-            'MeanSquareOut': ms_out
-        }
-
-    def non_test_check_output(self):
-        self.check_output()
-
-
-class TestRmspropOp2(OpTest):
-    '''Test RMSProp with default values for attributes
-    '''
-
-    def setUp(self):
-        self.op_type = "rmsprop"
-
-        param = np.random.random((123, 321)).astype("float32")
-        mean_square = np.random.random((123, 321)).astype("float32")
-        learning_rate = np.array([0.01]).astype("float32")
-        grad = np.random.random((123, 321)).astype("float32")
-        moment = np.zeros((123, 321)).astype("float32")
-
-        epsilon = 1.0e-10
-        decay = 0.9
-        momentum = 0.0
-
-        self.inputs = {
-            'Param': param,
-            'MeanSquare': mean_square,
-            'MeanGrad': mean_square,  # just a place holder
-            'LearningRate': learning_rate,
-            'Grad': grad,
-            'Moment': moment,
-        }
-
-        ms_out = decay * mean_square + (1 - decay) * grad * grad
-        moment_out = momentum * moment + \
-            learning_rate * grad / np.sqrt(ms_out + epsilon)
-        param_out = param - moment_out
-
-        self.outputs = {
-            'ParamOut': param_out,
-            'MomentOut': moment_out,
-            'MeanSquareOut': ms_out
-        }
-
-    def non_test_check_output(self):
-        self.check_output()
-
-
-class TestRmspropOpV1Mode(OpTest):
-    ''' Test RMSProp with explicit inputs
-    '''
-
-    def setUp(self):
-        self.op_type = "rmsprop"
-
-        param = np.random.random((123, 321)).astype("float32")
-        mean_square = np.random.random((123, 321)).astype("float32")
-        mean_grad = np.random.random((123, 321)).astype("float32")
-        learning_rate = np.array([0.01]).astype("float32")
-        grad = np.random.random((123, 321)).astype("float32")
-        moment = np.zeros((123, 321)).astype("float32")
-
-        epsilon = 1e-6
-        decay = 0.9
-        momentum = 0.0
-
-        self.inputs = {
-            'Param': param,
-            'MeanSquare': mean_square,
-            'MeanGrad': mean_grad,
-            'LearningRate': learning_rate,
-            'Grad': grad,
-            'Moment': moment,
-        }
-
-        self.attrs = {
-            'epsilon': epsilon,
-            'decay': decay,
-            'momentum': momentum,
-            'centered': True
-        }
-
-        ms_out = decay * mean_square + (1 - decay) * grad * grad
-        mg_out = decay * mean_grad + (1 - decay) * grad
-        moment_out = momentum * moment + \
-                     learning_rate * grad / np.sqrt(ms_out - np.square(mg_out) + epsilon)
-        param_out = param - moment_out
-
-        self.outputs = {
-            'ParamOut': param_out,
-            'MomentOut': moment_out,
-            'MeanSquareOut': ms_out,
-            'MeanGradOut': mg_out
-        }
-
-    def test_check_output(self):
-        self.check_output(atol=1e-3, equal_nan=True)
+            self.check_with_place(place, False, 1e-6)
+            self.check_with_place(place, False, 1e-10)
+            self.check_with_place(place, True, 1e-6)
+            self.check_with_place(place, True, 1e-10)
 
 
 if __name__ == "__main__":
