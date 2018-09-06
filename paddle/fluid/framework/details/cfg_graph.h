8f50920a581ea637f1753e5af29ecf5940be896a
dongzhihong <dzhwinter@gmail.com>
2018-09-06 16:29:04 +0800

    "add file"

--- paddle/fluid/framework/details/cfg_graph.h
+++ paddle/fluid/framework/details/cfg_graph.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <unordered_map>
+#include <unordered_set>
 #include <string>
 
 #include "paddle/fluid/framework/"
@@ -10,8 +11,17 @@ namespace framework {
 namespace details {
 
 class ControlFlowGraph : public ir::Graph {
+public:
+  void ComputeLiveRange()
 private:
-  std::unordered_map<std::string>
+  typedef std::unordered_map<std::string, std::list<std::string>> DictType;
+  typedef std::unordered_map<std::unordered_set<std::string>> SetType;
+  DictType successors_;
+  DictType predecessors_;
+  SetType live_in_;
+  SetType live_out_;
+  SetType uses_;
+  SetType defs_;
 };
 
 }  // namespace details
