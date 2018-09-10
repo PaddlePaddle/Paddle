bfef7a25e79c65642bbd72500064e4e77f3e8290
Krzysztof Binias <krzysztof.binias@intel.com>
2018-09-10 15:29:51 +0200

    Renaming decision variable

--- paddle/fluid/platform/mkldnn_helper.h
+++ paddle/fluid/platform/mkldnn_helper.h
@@ -192,7 +192,7 @@ class MKLDNNHandler {
       mkldnn::memory::primitive_desc& user_mpd,  // NOLINT
       const std::shared_ptr<mkldnn::memory> user_memory_p,
       const std::string& suffix, const std::vector<mkldnn::primitive>& pipeline,
-      bool is_test = false) {  // NOLINT
+      bool is_persistent = false) {  // NOLINT
     // create reorder primitive if the input format is not the preferred one
     auto local_key = key_ + suffix;
     auto key_reorder_p = key_ + suffix + "reorder_p";
@@ -213,7 +213,7 @@ class MKLDNNHandler {
         pipeline.push_back(*reorder_p);
       }
       dev_ctx_.SetBlob(local_key, target_memory_p);
-    } else if (!is_test) {
+    } else if (!is_persistent) {
       // Make reorder if needed
       auto reorder_p = std::static_pointer_cast<mkldnn::reorder>(
           dev_ctx_.GetBlob(key_reorder_p));
