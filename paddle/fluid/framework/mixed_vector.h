1249486bb27d4543da51ce0e22af6c7f1a6023f0
Yu Yang <yuyang18@baidu.com>
2018-09-13 06:55:22 +0000

    Use std::vector when CPU in mixed_vector

--- paddle/fluid/framework/mixed_vector.h
+++ paddle/fluid/framework/mixed_vector.h
@@ -18,6 +18,7 @@
 #include <initializer_list>
 #include <memory>
 #include <vector>
+#include "paddle/fluid/memory/memcpy.h"
 
 #include "paddle/fluid/framework/tensor.h"
 #include "paddle/fluid/framework/tensor_util.h"
@@ -34,39 +35,24 @@ template <typename T>
 class Vector {
  public:
   using value_type = T;
+  using iterator = typename std::vector<T>::iterator;
+  using const_iterator = typename std::vector<T>::const_iterator;
 
   // Default ctor. Create empty Vector
   Vector() { InitEmpty(); }
 
   // Fill vector with value. The vector size is `count`.
-  explicit Vector(size_t count, const T &value = T()) {
+  explicit Vector(size_t count, const T &value = T()) : cpu_vec_(count, value) {
     InitEmpty();
-    if (count != 0) {
-      resize(count);
-      T *ptr = begin();
-      for (size_t i = 0; i < count; ++i) {
-        ptr[i] = value;
-      }
-    }
   }
 
   // Ctor with init_list
-  Vector(std::initializer_list<T> init) {
-    if (init.size() == 0) {
-      InitEmpty();
-    } else {
-      InitByIter(init.size(), init.begin(), init.end());
-    }
-  }
+  Vector(std::initializer_list<T> init) : cpu_vec_(init) { InitEmpty(); }
 
   // implicit cast from std::vector.
   template <typename U>
-  Vector(const std::vector<U> &dat) {  // NOLINT
-    if (dat.size() == 0) {
-      InitEmpty();
-    } else {
-      InitByIter(dat.size(), dat.begin(), dat.end());
-    }
+  Vector(const std::vector<U> &dat) : cpu_vec_(dat) {  // NOLINT
+    InitEmpty();
   }
 
   // Copy ctor
@@ -84,68 +70,83 @@ class Vector {
 
   // Move ctor
   Vector(Vector<T> &&other) {
-    this->size_ = other.size_;
     this->flag_ = other.flag_;
     if (other.cuda_vec_.memory_size()) {
       this->cuda_vec_.ShareDataWith(other.cuda_vec_);
     }
-    if (other.cpu_vec_.memory_size()) {
-      this->cpu_vec_.ShareDataWith(other.cpu_vec_);
+    if (!other.cpu_vec_.empty()) {
+      this->cpu_vec_ = std::move(other.cpu_vec_);
     }
   }
 
   // CPU data access method. Mutable.
   T &operator[](size_t i) {
     MutableCPU();
-    return const_cast<T *>(cpu_vec_.data<T>())[i];
+    return cpu_vec_[i];
   }
 
   // CPU data access method. Immutable.
   const T &operator[](size_t i) const {
     ImmutableCPU();
-    return cpu_vec_.data<T>()[i];
+    return cpu_vec_[i];
   }
 
   // std::vector iterator methods. Based on CPU data access method
-  size_t size() const { return size_; }
+  size_t size() const { return cpu_vec_.size(); }
 
-  T *begin() { return capacity() == 0 ? &EmptyDummy() : &this->operator[](0); }
-
-  T *end() {
-    return capacity() == 0 ? &EmptyDummy() : &this->operator[](size());
+  iterator begin() {
+    MutableCPU();
+    return cpu_vec_.begin();
   }
 
-  T &front() { return *begin(); }
+  iterator end() {
+    MutableCPU();
+    return cpu_vec_.end();
+  }
+
+  T &front() {
+    MutableCPU();
+    return cpu_vec_.front();
+  }
 
   T &back() {
-    auto it = end();
-    --it;
-    return *it;
+    MutableCPU();
+    return cpu_vec_.back();
   }
 
-  const T *begin() const {
-    return capacity() == 0 ? &EmptyDummy() : &this->operator[](0);
+  const_iterator begin() const {
+    ImmutableCPU();
+    return cpu_vec_.begin();
   }
 
-  const T *end() const {
-    return capacity() == 0 ? &EmptyDummy() : &this->operator[](size());
+  const_iterator end() const {
+    ImmutableCPU();
+    return cpu_vec_.end();
   }
 
-  const T *cbegin() const { return begin(); }
+  const_iterator cbegin() const { return begin(); }
 
-  const T *cend() const { return end(); }
+  const_iterator cend() const { return end(); }
 
   const T &back() const {
-    auto it = end();
-    --it;
-    return *it;
+    ImmutableCPU();
+    return cpu_vec_.back();
   }
 
-  T *data() { return begin(); }
+  T *data() {
+    MutableCPU();
+    return &(*this)[0];
+  }
 
-  const T *data() const { return begin(); }
+  const T *data() const {
+    ImmutableCPU();
+    return &(*this)[0];
+  }
 
-  const T &front() const { return *begin(); }
+  const T &front() const {
+    ImmutableCPU();
+    return cpu_vec_.front();
+  }
   // end of std::vector iterator methods
 
   // assign this from iterator.
@@ -158,43 +159,23 @@ class Vector {
   // push_back. If the previous capacity is not enough, the memory will
   // double.
   void push_back(T elem) {
-    if (size_ + 1 > capacity()) {
-      reserve((size_ + 1) << 1);
-    }
-    *end() = elem;
-    ++size_;
+    MutableCPU();
+    cpu_vec_.push_back(elem);
   }
 
   // extend a vector by iterator.
   // NOTE: the iterator must support end-begin
   template <typename It>
   void Extend(It begin, It end) {
-    size_t pre_size = size_;
-    resize(pre_size + (end - begin));
-    T *ptr = this->begin() + pre_size;
-    for (; begin < end; ++begin, ++ptr) {
-      *ptr = *begin;
-    }
+    MutableCPU();
+    cpu_vec_.reserve((end - begin) + cpu_vec_.size());
+    std::copy(begin, end, cpu_vec_.begin());
   }
 
   // resize the vector
   void resize(size_t size) {
-    if (size + 1 <= capacity()) {
-      size_ = size;
-    } else {
-      MutableCPU();
-      Tensor cpu_tensor;
-      platform::Place cpu = platform::CPUPlace();
-      T *ptr = cpu_tensor.mutable_data<T>(
-          framework::make_ddim({static_cast<int64_t>(size)}), cpu);
-      const T *old_ptr =
-          cpu_vec_.memory_size() == 0 ? nullptr : cpu_vec_.data<T>();
-      if (old_ptr != nullptr) {
-        std::copy(old_ptr, old_ptr + size_, ptr);
-      }
-      size_ = size;
-      cpu_vec_.ShareDataWith(cpu_tensor);
-    }
+    MutableCPU();
+    cpu_vec_.resize(size);
   }
 
   // get cuda ptr. immutable
@@ -214,20 +195,14 @@ class Vector {
 
   // clear
   void clear() {
-    size_ = 0;
+    cpu_vec_.clear();
     flag_ = kDirty | kDataInCPU;
   }
 
-  size_t capacity() const {
-    return cpu_vec_.memory_size() / SizeOfType(typeid(T));
-  }
+  size_t capacity() const { return cpu_vec_.capacity(); }
 
   // reserve data
-  void reserve(size_t size) {
-    size_t pre_size = size_;
-    resize(size);
-    resize(pre_size);
-  }
+  void reserve(size_t size) { cpu_vec_.reserve(size); }
 
   // the unify method to access CPU or CUDA data. immutable.
   const T *Data(platform::Place place) const {
@@ -249,10 +224,8 @@ class Vector {
 
   // implicit cast operator. Vector can be cast to std::vector implicitly.
   operator std::vector<T>() const {
-    std::vector<T> result;
-    result.resize(size());
-    std::copy(begin(), end(), result.begin());
-    return result;
+    ImmutableCPU();
+    return cpu_vec_;
   }
 
   bool operator==(const Vector<T> &other) const {
@@ -268,21 +241,13 @@ class Vector {
   }
 
  private:
-  void InitEmpty() {
-    size_ = 0;
-    flag_ = kDataInCPU;
-  }
+  void InitEmpty() { flag_ = kDataInCPU; }
 
   template <typename Iter>
   void InitByIter(size_t size, Iter begin, Iter end) {
-    platform::Place cpu = platform::CPUPlace();
-    T *ptr = this->cpu_vec_.template mutable_data<T>(
-        framework::make_ddim({static_cast<int64_t>(size)}), cpu);
-    for (size_t i = 0; i < size; ++i) {
-      *ptr++ = *begin++;
-    }
+    cpu_vec_.resize(size);
+    std::copy(begin, end, cpu_vec_.begin());
     flag_ = kDataInCPU | kDirty;
-    size_ = size;
   }
 
   enum DataFlag {
@@ -294,8 +259,11 @@ class Vector {
 
   void CopyToCPU() const {
     // COPY GPU Data To CPU
-    TensorCopy(cuda_vec_, platform::CPUPlace(), &cpu_vec_);
-    WaitPlace(cuda_vec_.place());
+    void *src = cuda_vec_.data<void>();
+    void *dst = cpu_vec_.data();
+    memory::Copy(platform::CPUPlace(), dst,
+                 boost::get<platform::CUDAPlace>(cuda_vec_.place()), src,
+                 cuda_vec_.memory_size());
   }
 
   void MutableCPU() {
@@ -308,16 +276,11 @@ class Vector {
   void ImmutableCUDA(platform::Place place) const {
     if (IsDirty()) {
       if (IsInCPU()) {
-        TensorCopy(cpu_vec_, boost::get<platform::CUDAPlace>(place),
-                   &cuda_vec_);
-        WaitPlace(place);
+        CopyCPUDataToCUDA(place);
         UnsetFlag(kDirty);
         SetFlag(kDataInCUDA);
       } else if (IsInCUDA() && !(place == cuda_vec_.place())) {
-        framework::Tensor tmp;
-        TensorCopy(cuda_vec_, boost::get<platform::CUDAPlace>(place), &tmp);
-        WaitPlace(cuda_vec_.place());
-        cuda_vec_.ShareDataWith(tmp);
+        CopyCUDADataToAnotherPlace(place);
         // Still dirty
       } else {
         // Dirty && DataInCUDA && Device is same
@@ -326,22 +289,35 @@ class Vector {
     } else {
       if (!IsInCUDA()) {
         // Even data is not dirty. However, data is not in CUDA. Copy data.
-        TensorCopy(cpu_vec_, boost::get<platform::CUDAPlace>(place),
-                   &cuda_vec_);
-        WaitPlace(place);
+        CopyCPUDataToCUDA(place);
         SetFlag(kDataInCUDA);
       } else if (!(place == cuda_vec_.place())) {
-        framework::Tensor tmp;
-        WaitPlace(cuda_vec_.place());
-        TensorCopy(cuda_vec_, boost::get<platform::CUDAPlace>(place), &tmp);
-        WaitPlace(cuda_vec_.place());
-        WaitPlace(place);
-        cuda_vec_.ShareDataWith(tmp);
+        CopyCUDADataToAnotherPlace(place);
       } else {
         // Not Dirty && DataInCUDA && Device is same
         // Do nothing.
       }
     }
+  }
+  void CopyCUDADataToAnotherPlace(const platform::Place &place) const {
+    Tensor tmp;
+    tmp.Resize(cuda_vec_.dims());
+
+    const void *src = cuda_vec_.data<T>();
+    void *dst = tmp.mutable_data<T>(place);
+
+    memory::Copy(boost::get<platform::CUDAPlace>(place), dst,
+                 boost::get<platform::CUDAPlace>(cuda_vec_.place()), src,
+                 cuda_vec_.memory_size());
+    cuda_vec_.ShareDataWith(tmp);
+  }
+  void CopyCPUDataToCUDA(const platform::Place &place) const {
+    void *src = cpu_vec_.data();
+    cuda_vec_.Resize(
+        framework::make_ddim({static_cast<int64_t>(cpu_vec_.size())}));
+    void *dst = cuda_vec_.mutable_data<T>(place);
+    memory::Copy(boost::get<platform::CUDAPlace>(place), dst,
+                 platform::CPUPlace(), src, cuda_vec_.memory_size());
   }
 
   void ImmutableCPU() const {
@@ -362,23 +338,9 @@ class Vector {
 
   bool IsInCPU() const { return flag_ & kDataInCPU; }
 
-  static void WaitPlace(const platform::Place place) {
-    if (platform::is_gpu_place(place)) {
-      platform::DeviceContextPool::Instance()
-          .Get(boost::get<platform::CUDAPlace>(place))
-          ->Wait();
-    }
-  }
-
-  static T &EmptyDummy() {
-    static T dummy = T();
-    return dummy;
-  }
-
   mutable int flag_;
-  mutable Tensor cpu_vec_;
+  mutable std::vector<T> cpu_vec_;
   mutable Tensor cuda_vec_;
-  size_t size_;
 };
 
 #else  // PADDLE_WITH_CUDA
