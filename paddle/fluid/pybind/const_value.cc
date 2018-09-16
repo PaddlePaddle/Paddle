13ae2682b3d57552f26f9e15471b9202de115825
Yibing Liu <liuyibing01@baidu.com>
2018-09-16 05:48:14 +0000

    Revert changes for debug

--- paddle/fluid/pybind/const_value.cc
+++ paddle/fluid/pybind/const_value.cc
@@ -46,9 +46,6 @@ void BindConstValue(pybind11::module* m) {
   op_proto_and_checker_maker.def(
       "kOpNameScopeAttrName",
       framework::OpProtoAndCheckerMaker::OpNamescopeAttrName);
-  op_proto_and_checker_maker.def(
-      "kOpCreationCallstackAttrName",
-      framework::OpProtoAndCheckerMaker::OpCreationCallstackAttrName);
 }
 
 }  // namespace pybind
