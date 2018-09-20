9833567d10773f938e06eb21b43937d1443f45d3
Michal Gallus <michal.gallus@intel.com>
2018-09-20 15:50:42 +0200

    Enable MKLDNN in Analysis Predictor
    Also fix MKL-DNN pooling integration for ceil mode

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -76,10 +76,10 @@ bool AnalysisPredictor::Init(
   }
 
   OptimizeInferenceProgram();
-  ctx_ = executor_->Prepare(*inference_program_, 0);
   if (config_._use_mkldnn) {
     executor_->EnableMKLDNN(*inference_program_);
   }
+  ctx_ = executor_->Prepare(*inference_program_, 0);
 
   VLOG(5) << "to create variables";
   PADDLE_ENFORCE(scope_.get());
