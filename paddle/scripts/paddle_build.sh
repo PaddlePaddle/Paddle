8a5d170df28f1dccf2470669c8e51f42de8107ee
typhoonzero <typhoonzero1986@gmail.com>
2018-09-22 18:37:17 +0800

    update

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -363,7 +363,7 @@ EOF
         ctest --output-on-failure
         # make install should also be test when unittest
         make install -j `nproc`
-        pip install $INSTALL_PREFIX/opt/paddle/share/wheels/*.whl
+        pip install ${INSTALL_PREFIX:-/paddle/build}/opt/paddle/share/wheels/*.whl
         if [[ ${WITH_FLUID_ONLY:-OFF} == "OFF" ]] ; then
             paddle version
         fi
