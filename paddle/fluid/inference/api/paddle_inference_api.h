ab9f31a7c2cf43b388805dbcb20a6ad92d6397fe
Jacek Czaja <jacek.czaja@intel.com>
2018-09-27 14:01:11 +0200

    - Disabled embedding_fc_lstm_fuse by defult and   extended test_text_classification ot use new op

--- paddle/fluid/inference/api/paddle_inference_api.h
+++ paddle/fluid/inference/api/paddle_inference_api.h
@@ -216,7 +216,7 @@ struct AnalysisConfig : public NativeConfig {
   bool enable_ir_optim = true;
   // Manually determine the IR passes to run.
   IrPassMode ir_mode{IrPassMode::kExclude};
-  std::vector<std::string> ir_passes;
+  std::vector<std::string> ir_passes{"embedding_fc_lstm_fuse_pass"};
 
   // NOTE this is just for internal development, please not use it.
   bool _use_mkldnn{false};
