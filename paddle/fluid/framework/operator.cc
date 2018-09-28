00d13327e22aa8eaa740e401562c569ab6a7914b
Superjomn <yanchunwei@outlook.com>
2018-09-28 06:01:52 +0000

    fix profile bug

--- paddle/fluid/framework/operator.cc
+++ paddle/fluid/framework/operator.cc
@@ -159,9 +159,10 @@ void OperatorBase::Run(const Scope& scope, const platform::Place& place) {
       platform::DeviceContextPool& pool =
           platform::DeviceContextPool::Instance();
       platform::RecordEvent record_event(Type(), pool.Get(place));
+      RunImpl(scope, place);
+    } else {
+      RunImpl(scope, place);
     }
-
-    RunImpl(scope, place);
 
     if (VLOG_IS_ON(3)) {
       VLOG(3) << place << " " << DebugStringEx(&scope);
