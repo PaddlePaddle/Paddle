6756e8c4661d5e285a47389981dcfa6a090e8d21
tangwei12 <tangwei12@baidu.com>
2018-09-29 16:35:25 +0800

    Update test_dist_ctr.py
    test=release/1.0.0

--- python/paddle/fluid/tests/unittests/test_dist_ctr.py
+++ python/paddle/fluid/tests/unittests/test_dist_ctr.py
@@ -25,7 +25,7 @@ class TestDistCTR2x2(TestDistBase):
 
 
 def test_dist_ctr(self):
-    self.check_with_place("dist_ctr.py", delta=1e-7, check_error_log=False)
+    self.check_with_place("dist_ctr.py", delta=1e-7)
 
 
 if __name__ == "__main__":
