29edb66e9a76d33ea756970e81b5a762c80016b3
typhoonzero <typhoonzero1986@gmail.com>
2018-10-11 20:31:56 +0800

    update

--- python/paddle/fluid/tests/unittests/dist_mnist_lars.py
+++ python/paddle/fluid/tests/unittests/dist_mnist_lars.py
@@ -57,8 +57,8 @@ class TestDistMnist2x2(TestDistRunnerBase):
 
         inference_program = fluid.default_main_program().clone()
         # Optimization
-        opt = fluid.optimizer.MomentumOptimizer(
-            learning_rate=0.001, momentum=0.9, use_lars=True)
+        opt = fluid.optimizer.LarsMomentumOptimizer(
+            learning_rate=0.001, momentum=0.9)
 
         # Reader
         train_reader = paddle.batch(
