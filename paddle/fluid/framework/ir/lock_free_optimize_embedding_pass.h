ebef08af4730ac5acfd49e41aba4176168c4af26
minqiyang <minqiyang@baidu.com>
2018-10-12 17:36:09 +0800

    Add pass for no locking optimize embedding lookup table

---
+++ paddle/fluid/framework/ir/lock_free_optimize_embedding_pass.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PADDLE_FLUID_FRAMEWORK_IR_LOCK_FREE_OPTIMIZE_EMBEDDING_PASS_H_
+#define PADDLE_FLUID_FRAMEWORK_IR_LOCK_FREE_OPTIMIZE_EMBEDDING_PASS_H_
+
+#include "paddle/fluid/framework/ir/graph.h"
+#include "paddle/fluid/framework/ir/pass.h"
+
+namespace paddle {
+namespace framework {
+namespace ir {
+
+/*
+ * Remove the sum op of all gradients of embedding lookup table.
+ * And remove the dependecies of the optimizer related to the
+ * same embedding lookup table.
+ */
+class LockFreeOptimizeEmbeddingPass : public Pass {
+ public:
+  virtual ~LockFreeOptimizeEmbeddingPass() {}
+
+ protected:
+  std::unique_ptr<ir::Graph> ApplyImpl(std::unique_ptr<ir::Graph> graph) const;
+};
+
+}  // namespace ir
+}  // namespace framework
+}  // namespace paddle
+
+#endif  // PADDLE_FLUID_FRAMEWORK_IR_LOCK_FREE_OPTIMIZE_EMBEDDING_PASS_H_
