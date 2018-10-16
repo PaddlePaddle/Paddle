a35e7f4bae3ff8970188db12fa3a8fc8e2d77959
Tao Luo <luotao02@baidu.com>
2018-10-16 20:38:41 +0800

    adjust demo_ci with fluid_inference_install_dir
    test=develop

--- paddle/fluid/inference/api/demo_ci/vis_demo.cc
+++ paddle/fluid/inference/api/demo_ci/vis_demo.cc
@@ -18,7 +18,7 @@ limitations under the License. */
 
 #include <gflags/gflags.h>
 #include <glog/logging.h>  // use glog instead of CHECK to avoid importing other paddle header files.
-#include "paddle/fluid/inference/demo_ci/utils.h"
+#include "utils.h"  // NOLINT
 
 #ifdef PADDLE_WITH_CUDA
 DECLARE_double(fraction_of_gpu_memory_to_use);
