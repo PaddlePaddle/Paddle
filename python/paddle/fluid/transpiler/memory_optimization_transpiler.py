fcc1ffab5d2bf7db8859d772b8eda7c2ea9c1d96
dongzhihong <dzhwinter@gmail.com>
2018-10-24 21:41:39 +0800

    fix mem opt

--- python/paddle/fluid/transpiler/memory_optimization_transpiler.py
+++ python/paddle/fluid/transpiler/memory_optimization_transpiler.py
@@ -171,7 +171,7 @@ class ControlFlowGraph(object):
                 self._live_out[i] |= self._live_in[s]
             self._live_in[i] = self._uses[i] | (
                 self._live_out[i] - self._defs[i])
-            if live_in[i] != self._live_in[i]:
+            if live_in[i] != set(self._live_in[i]):
                 for d in self._presuccessors[i]:
                     worklist.append(d)
 
