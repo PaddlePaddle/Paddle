0695c1fbe87514d6204797955e549bcc4f78ef21
minqiyang <minqiyang@baidu.com>
2018-10-25 10:36:21 +0800

    Add remind for code
    test=develop

--- paddle/fluid/operators/lookup_table_op.cc
+++ paddle/fluid/operators/lookup_table_op.cc
@@ -81,6 +81,8 @@ class LookupTableOpMaker : public framework::OpProtoAndCheckerMaker {
                      "Otherwise the given value indicates padding the output "
                      "with zeros whenever lookup encounters it in Ids.")
         .SetDefault(kNoPadding);
+    // NOTE(minqiyang): grad_inplace is an temporal attribute,
+    // please do NOT set this attribute in python layer.
     AddAttr<bool>("grad_inplace",
                   "(boolean, default false) "
                   "If the grad op reuse the input's variable.")
