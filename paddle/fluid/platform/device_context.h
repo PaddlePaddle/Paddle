7b08183557b3c4a5ce1ef6622f3b6f45211b7dae
Yu Yang <yuyang18@baidu.com>
2018-10-26 11:01:19 +0800

    Add future includes
    test=develop

--- paddle/fluid/platform/device_context.h
+++ paddle/fluid/platform/device_context.h
@@ -10,6 +10,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 #pragma once
 
+#include <future>  // NOLINT
 #include <memory>
 #include <mutex>  // NOLINT
 #include <string>
