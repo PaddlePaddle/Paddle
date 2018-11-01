e99da0b5836715a4368f5d273129f8ee38c150a4
dengkaipeng <dkp19930606@gmail.com>
2018-11-01 15:27:35 +0800

    api change: create_variable_for_type_inference. test=develop

--- python/paddle/fluid/tests/unittests/test_layers.py
+++ python/paddle/fluid/tests/unittests/test_layers.py
@@ -865,10 +865,10 @@ class TestBook(unittest.TestCase):
             self.assertIsNotNone(out)
         print(str(program))
 
-    def test_affine_grid_gen(self):
+    def test_grid_sampler(self):
         program = Program()
         with program_guard(program):
-            x = layers.data(name='x', shape=[2, 5, 7, 3], dtype='float32')
+            x = layers.data(name='x', shape=[2, 3, 5, 7], dtype='float32')
             grid = layers.data(name='grid', shape=[2, 5, 7, 2], dtype='float32')
             out = layers.grid_sampler(x, grid)
             self.assertIsNotNone(out)
