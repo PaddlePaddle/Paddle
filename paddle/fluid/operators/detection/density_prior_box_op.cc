b4283f454ecc80cc2902e0fbfab30edc7a0a7c3f
root <root@yq01-gpu-255-134-11-01.epc.baidu.com>
2018-11-02 08:53:57 +0000

    density

---
+++ paddle/fluid/operators/detection/density_prior_box_op.cc
@@ -0,0 +1,222 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#include "paddle/fluid/operators/detection/density_prior_box_op.h"
+
+namespace paddle {
+namespace operators {
+
+class DensityPriorBoxOp : public framework::OperatorWithKernel {
+ public:
+  using framework::OperatorWithKernel::OperatorWithKernel;
+
+  void InferShape(framework::InferShapeContext* ctx) const override {
+    LOG(ERROR) << "C++  ===================";
+    PADDLE_ENFORCE(ctx->HasInput("Input"),
+                   "Input(Input) of PriorBoxOp should not be null.");
+    PADDLE_ENFORCE(ctx->HasInput("Image"),
+                   "Input(Image) of PriorBoxOp should not be null.");
+
+    auto image_dims = ctx->GetInputDim("Image");
+    auto input_dims = ctx->GetInputDim("Input");
+    PADDLE_ENFORCE(image_dims.size() == 4, "The layout of image is NCHW.");
+    PADDLE_ENFORCE(input_dims.size() == 4, "The layout of input is NCHW.");
+
+    PADDLE_ENFORCE_LT(input_dims[2], image_dims[2],
+                      "The height of input must smaller than image.");
+
+    PADDLE_ENFORCE_LT(input_dims[3], image_dims[3],
+                      "The width of input must smaller than image.");
+
+    auto min_sizes = ctx->Attrs().Get<std::vector<float>>("min_sizes");
+    auto max_sizes = ctx->Attrs().Get<std::vector<float>>("max_sizes");
+    auto variances = ctx->Attrs().Get<std::vector<float>>("variances");
+    auto aspect_ratios = ctx->Attrs().Get<std::vector<float>>("aspect_ratios");
+    bool flip = ctx->Attrs().Get<bool>("flip");
+//  densities and fixed_size 
+//  begin wangshipeng @20181028
+    auto fixed_sizes = ctx->Attrs().Get<std::vector<float>>("fixed_sizes");
+	auto fixed_ratios = ctx->Attrs().Get<std::vector<float>>("fixed_ratios");
+    auto densities = ctx->Attrs().Get<std::vector<int>>("densities");
+//  end wangshipeng @20181028
+    std::vector<float> aspect_ratios_vec;
+    ExpandAspectRatios(aspect_ratios, flip, &aspect_ratios_vec);
+
+    size_t num_priors = aspect_ratios_vec.size() * min_sizes.size();
+    if (max_sizes.size() > 0) {
+      PADDLE_ENFORCE_EQ(max_sizes.size(), min_sizes.size(),
+                        "The number of min_size and max_size must be equal.");
+      num_priors += max_sizes.size();
+      for (size_t i = 0; i < max_sizes.size(); ++i) {
+        PADDLE_ENFORCE_GT(max_sizes[i], min_sizes[i],
+                         "max_size[%d] must be greater than min_size[%d].", i,i);
+      }
+    }
+//  begin wangshipeng @20181028
+    PADDLE_ENFORCE_EQ(fixed_sizes.size(),densities.size(),
+                        "The number of fixed_sizes and densities must be equal.");
+//  end wangshipeng @20181028
+
+    std::vector<int64_t> dim_vec(4);
+    dim_vec[0] = input_dims[2];
+    dim_vec[1] = input_dims[3];
+    dim_vec[2] = num_priors;
+    dim_vec[3] = 4;
+    ctx->SetOutputDim("Boxes", framework::make_ddim(dim_vec));
+    ctx->SetOutputDim("Variances", framework::make_ddim(dim_vec));
+  }
+//
+ protected:
+  framework::OpKernelType GetExpectedKernelType(
+      const framework::ExecutionContext& ctx) const override {
+    return framework::OpKernelType(
+        framework::ToDataType(ctx.Input<framework::Tensor>("Input")->type()),
+        platform::CPUPlace());
+  }
+};
+
+class DensityPriorBoxOpMaker : public framework::OpProtoAndCheckerMaker {
+ public:
+  void Make() override {
+    AddInput("Input",
+             "(Tensor, default Tensor<float>), "
+             "the input feature data of PriorBoxOp, The layout is NCHW.");
+    AddInput("Image",
+             "(Tensor, default Tensor<float>), "
+             "the input image data of PriorBoxOp, The layout is NCHW.");
+    AddOutput("Boxes",
+              "(Tensor, default Tensor<float>), the output prior boxes of "
+              "PriorBoxOp. The layout is [H, W, num_priors, 4]. "
+              "H is the height of input, W is the width of input, num_priors "
+              "is the box count of each position.");
+    AddOutput("Variances",
+              "(Tensor, default Tensor<float>), the expanded variances of "
+              "PriorBoxOp. The layout is [H, W, num_priors, 4]. "
+              "H is the height of input, W is the width of input, num_priors "
+              "is the box count of each position.");
+
+    AddAttr<std::vector<float>>("min_sizes",
+                                "(vector<float>) List of min sizes "
+                                "of generated prior boxes.")
+        .AddCustomChecker([](const std::vector<float>& min_sizes) {
+          PADDLE_ENFORCE_GT(min_sizes.size(), 0,
+                            "Size of min_sizes must be at least 1.");
+          for (size_t i = 0; i < min_sizes.size(); ++i) {
+            PADDLE_ENFORCE_GT(min_sizes[i], 0.0,
+                              "min_sizes[%d] must be positive.", i);
+          }
+        });
+    AddAttr<std::vector<float>>(
+        "max_sizes",
+        "(vector<float>) List of max sizes of generated prior boxes.")
+        .SetDefault(std::vector<float>{});
+    AddAttr<std::vector<float>>(
+        "aspect_ratios",
+        "(vector<float>) List of aspect ratios of generated prior boxes.");
+
+    AddAttr<std::vector<float>>(
+        "variances",
+        "(vector<float>) List of variances to be encoded in prior boxes.")
+        .AddCustomChecker([](const std::vector<float>& variances) {
+          PADDLE_ENFORCE_EQ(variances.size(), 4,
+                            "Must and only provide 4 variance.");
+          for (size_t i = 0; i < variances.size(); ++i) {
+            PADDLE_ENFORCE_GT(variances[i], 0.0,
+                              "variance[%d] must be greater than 0.", i);
+          }
+        });
+    AddAttr<bool>("flip", "(bool) Whether to flip aspect ratios.")
+        .SetDefault(true);
+    AddAttr<bool>("clip", "(bool) Whether to clip out-of-boundary boxes.")
+        .SetDefault(true);
+
+    AddAttr<float>("step_w",
+                   "Prior boxes step across width, 0.0 for auto calculation.")
+        .SetDefault(0.0)
+        .AddCustomChecker([](const float& step_w) {
+          PADDLE_ENFORCE_GE(step_w, 0.0, "step_w should be larger than 0.");
+        });
+    AddAttr<float>("step_h",
+                   "Prior boxes step across height, 0.0 for auto calculation.")
+        .SetDefault(0.0)
+        .AddCustomChecker([](const float& step_h) {
+          PADDLE_ENFORCE_GE(step_h, 0.0, "step_h should be larger than 0.");
+        });
+
+    AddAttr<float>("offset",
+                   "(float) "
+                   "Prior boxes center offset.")
+        .SetDefault(0.5);
+//  Add Arrtibutes fixed_sizes, fixed_ratios and densities
+//  begin wangshipeng @20181028
+    AddAttr<std::vector<float>>("fixed_sizes",
+                            "(vector<float>) List of fixed sizes "
+                            "of generated density prior boxes.")
+        .SetDefault(std::vector<float>{})
+        .AddCustomChecker([](const std::vector<float>& fixed_sizes) {
+      		for (size_t i = 0; i < fixed_sizes.size(); ++i) {
+        		PADDLE_ENFORCE_GT(fixed_sizes[i], 0.0,
+                          "fixed_sizes[%d] should be larger than 0.", i);
+      }
+    });
+
+    AddAttr<std::vector<float>>("fixed_ratios",
+                            "(vector<float>) List of fixed ratios "
+                            "of generated density prior boxes.")
+        .SetDefault(std::vector<float>{})
+        .AddCustomChecker([](const std::vector<float>& fixed_ratios) {
+      		for (size_t i = 0; i < fixed_ratios.size(); ++i) {
+        		PADDLE_ENFORCE_GT(fixed_ratios[i], 0.0,
+                          "fixed_ratios[%d] should be larger than 0.", i);
+      }
+    });
+
+    AddAttr<std::vector<int>>("densities",
+                            "(vector<float>) List of densities "
+                            "of generated density prior boxes.")
+        .SetDefault(std::vector<int>{})
+        .AddCustomChecker([](const std::vector<int>& densities) {
+      		for (size_t i = 0; i < densities.size(); ++i) {
+        		PADDLE_ENFORCE_GT(densities[i], 0,
+                          "densities[%d] should be larger than 0.", i);
+      }
+    });
+
+    AddAttr<bool>(
+        "min_max_aspect_ratios_order",
+        "(bool) If set True, the output prior box is in order of"
+        "[min, max, aspect_ratios], which is consistent with Caffe."
+        "Please note, this order affects the weights order of convolution layer"
+        "followed by and does not affect the final detection results.")
+        .SetDefault(false);
+    AddComment(R"DOC(
+Prior box operator
+Generate prior boxes for SSD(Single Shot MultiBox Detector) algorithm.
+Each position of the input produce N prior boxes, N is determined by
+ the count of min_sizes, max_sizes and aspect_ratios, The size of the
+ box is in range(min_size, max_size) interval, which is generated in
+ sequence according to the aspect_ratios.
+Please get more information from the following papers:
+https://arxiv.org/abs/1512.02325.
+)DOC");
+  }
+};
+
+}  // namespace operators
+}  // namespace paddle
+
+namespace ops = paddle::operators;
+REGISTER_OPERATOR(density_prior_box, ops::DensityPriorBoxOp, ops::DensityPriorBoxOpMaker,
+                  paddle::framework::EmptyGradOpMaker);
+
+REGISTER_OP_CPU_KERNEL(density_prior_box, ops::DensityPriorBoxOpKernel<float>,
+                       ops::DensityPriorBoxOpKernel<double>);
+
---
+++ paddle/fluid/operators/detection/density_prior_box_op.h
@@ -0,0 +1,324 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+#include <algorithm>
+#include <vector>
+#include "paddle/fluid/framework/op_registry.h"
+#include "paddle/fluid/operators/math/math_function.h"
+#include "paddle/fluid/platform/transform.h"
+
+namespace paddle {
+namespace operators {
+
+inline void ExpandAspectRatios(const std::vector<float>& input_aspect_ratior,
+                               bool flip,
+                               std::vector<float>* output_aspect_ratior) {
+  constexpr float epsilon = 1e-6;
+  output_aspect_ratior->clear();
+  output_aspect_ratior->push_back(1.0f);
+  for (size_t i = 0; i < input_aspect_ratior.size(); ++i) {
+    float ar = input_aspect_ratior[i];
+    bool already_exist = false;
+    for (size_t j = 0; j < output_aspect_ratior->size(); ++j) {
+      if (fabs(ar - output_aspect_ratior->at(j)) < epsilon) {
+        already_exist = true;
+        break;
+      }
+    }
+    if (!already_exist) {
+      output_aspect_ratior->push_back(ar);
+      if (flip) {
+        output_aspect_ratior->push_back(1.0f / ar);
+      }
+    }
+  }
+}
+
+template <typename T>
+struct ClipFunctor {
+  HOSTDEVICE inline T operator()(T in) const {
+    return std::min<T>(std::max<T>(in, 0.), 1.);
+  }
+};
+
+template <typename T>
+class DensityPriorBoxOpKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
+    auto* input = ctx.Input<paddle::framework::Tensor>("Input");
+    auto* image = ctx.Input<paddle::framework::Tensor>("Image");
+    auto* boxes = ctx.Output<paddle::framework::Tensor>("Boxes");
+    auto* vars = ctx.Output<paddle::framework::Tensor>("Variances");
+
+    auto min_sizes = ctx.Attr<std::vector<float>>("min_sizes");
+    auto max_sizes = ctx.Attr<std::vector<float>>("max_sizes");
+    auto input_aspect_ratio = ctx.Attr<std::vector<float>>("aspect_ratios");
+    auto variances = ctx.Attr<std::vector<float>>("variances");
+    auto flip = ctx.Attr<bool>("flip");
+    auto clip = ctx.Attr<bool>("clip");
+    auto min_max_aspect_ratios_order =
+        ctx.Attr<bool>("min_max_aspect_ratios_order");
+//  begin  wangshipeng @ 20181028
+    auto fixed_sizes = ctx.Attr<std::vector<float>>("fixed_sizes");
+    auto fixed_ratios = ctx.Attr<std::vector<float>>("fixed_ratios");
+    auto densities = ctx.Attr<std::vector<float>>("densities");
+//  end  wangshipeng @ 20181028
+    std::vector<float> aspect_ratios;
+
+    ExpandAspectRatios(input_aspect_ratio, flip, &aspect_ratios);
+
+    T step_w = static_cast<T>(ctx.Attr<float>("step_w"));
+    T step_h = static_cast<T>(ctx.Attr<float>("step_h"));
+    T offset = static_cast<T>(ctx.Attr<float>("offset"));
+
+    auto img_width = image->dims()[3];
+    auto img_height = image->dims()[2];
+
+    auto feature_width = input->dims()[3];
+    auto feature_height = input->dims()[2];
+
+    T step_width, step_height;
+    if (step_w == 0 || step_h == 0) {
+      step_width = static_cast<T>(img_width) / feature_width;
+      step_height = static_cast<T>(img_height) / feature_height;
+    } else {
+      step_width = step_w;
+      step_height = step_h;
+    }
+
+    int num_priors = aspect_ratios.size() * min_sizes.size();
+
+//  begin  wangshipeng @ 20181028
+    if (fixed_sizes.size()>0){
+      num_priors += aspect_ratios.size() * fixed_sizes.size();
+    }
+    if (densities.size() > 0) {
+        for (size_t i = 0; i < densities.size(); ++i) {
+            if (fixed_ratios.size() > 0) {
+                num_priors += (fixed_ratios.size()) * (pow(densities[i], 2) - 1);
+            } else {
+                num_priors += (aspect_ratios.size() + 1) * (pow(densities[i], 2) - 1);
+            }
+        }
+    }
+	
+//  end  wangshipeng @ 20181028
+    if (max_sizes.size() > 0) {
+      num_priors += max_sizes.size();
+    }
+	LOG(ERROR)<<"C++ ==================== num_priors"<<num_priors;
+//  apply storage
+    boxes->mutable_data<T>(ctx.GetPlace());
+    vars->mutable_data<T>(ctx.GetPlace());
+//  eigen
+    auto e_boxes = framework::EigenTensor<T, 4>::From(*boxes);
+//  step_average
+    int step_average = static_cast<int>((step_width+step_height)*0.5);
+    int h1 = 0;
+	int h2 = 0;
+	int h3 = 0;
+    for (int h = 0; h < feature_height; ++h) {
+        for (int w = 0; w < feature_width; ++w) {
+            T center_x = (w + offset) * step_width;
+            T center_y = (h + offset) * step_height;
+            T box_width, box_height;
+            int idx = 0;
+//begin wangshipeng @20181029
+            for (size_t s = 0; s < fixed_sizes.size(); ++s){
+                auto fixed_size = fixed_sizes[s];
+                box_width = box_height = fixed_size;
+//densities vector has the same size as fixed_sizes
+                int density = densities[s];
+                if (fixed_ratios.size() > 0){
+                    for(size_t r = 0; r < fixed_ratios.size(); ++r){
+                        float ar = fixed_ratios[r];
+                        int shift  = step_average / density;
+                        float box_width_ratio = fixed_size * sqrt(ar);
+                        float box_height_ratio = fixed_size / sqrt(ar);
+// if have ratios use step_average if not use fixed_size to calculate center
+                        for (int di = 0; di < density; ++di){
+                            for (int dj=0; dj < density; ++dj){
+                                float center_x_temp = center_x - step_average / 2 + shift / 2. + dj * shift;
+                                float center_y_temp = center_y - step_average / 2 + shift / 2. + di * shift;
+                                e_boxes(h, w, idx, 0) = (center_x_temp - box_width_ratio / 2.) / img_width >= 0 ?
+                                                        (center_x_temp - box_width_ratio / 2.) / img_width : 0 ;
+                                e_boxes(h, w, idx, 1) = (center_y_temp - box_height_ratio / 2.)/ img_height >= 0 ?
+                                                        (center_y_temp - box_height_ratio / 2.)/ img_height : 0;
+                                e_boxes(h, w, idx, 2) = (center_x_temp + box_width_ratio / 2.) / img_width <= 1 ?
+                                                        (center_x_temp + box_width_ratio / 2.) / img_width : 1;
+                                e_boxes(h, w, idx, 3) = (center_y_temp + box_height_ratio / 2.)/ img_height <= 1 ?
+                                                        (center_y_temp + box_height_ratio / 2.)/ img_height : 1;
+                                idx++;
+                            }
+                        }
+                    }
+		//			LOG(ERROR) << "C++ fixed_ratios idx: " << idx;
+					h1 = idx;
+                } else {
+                    // does not give fixed_ratio but give densities
+                    int shift = fixed_size/ density;
+                    for (int di = 0; di < density; ++di){
+                    	for (int dj = 0; dj < density; ++dj){
+                        	float center_x_temp = center_x - fixed_size / 2 + shift / 2. + dj * shift;
+                            float center_y_temp = center_y - fixed_size / 2 + shift / 2. + di * shift;
+                            e_boxes(h, w, idx, 0) = (center_x_temp - box_width / 2.)/ img_width >= 0 ?
+                                                        (center_x_temp - box_width / 2.)/ img_width : 0 ;
+                            e_boxes(h, w, idx, 1) = (center_y_temp - box_height / 2.)/ img_height >= 0 ?
+                                                        (center_y_temp - box_height / 2.)/ img_height : 0;
+                            e_boxes(h, w, idx, 2) = (center_x_temp + box_width / 2.)/ img_width <= 1 ?
+                                                        (center_x_temp + box_width / 2.)/ img_width : 1;
+                            e_boxes(h, w, idx, 3) = (center_y_temp + box_height / 2.)/ img_height <= 1 ?
+                                                        (center_y_temp + box_height / 2.)/ img_height : 1;
+                            idx++;
+                        }
+                    }
+            		// if does not give fixed_ratios  then use aspect ratio
+                    for (size_t r = 0; r < aspect_ratios.size(); ++r){
+                        float ar = aspect_ratios[r];
+                        if (fabs(ar - 1.) < 1e-6) {
+                        // liu@20171207 added for debugging
+                        //LOG(INFO) << "returning for aspect == 1";
+                            continue;
+                        }
+                        int shift = fixed_size / density;
+                        float box_width_ratio = fixed_size * sqrt(ar);
+                        float box_height_ratio = fixed_size / sqrt(ar);
+                        for (int di= 0; di < density; ++di){
+                            for (int dj= 0; dj < density; ++dj){
+                                float center_x_temp = center_x - fixed_size / 2 + shift / 2. + dj * shift;
+                                float center_y_temp = center_y - fixed_size / 2 + shift / 2. + di * shift;
+								// order: xmin ymin xmax ymax
+                                e_boxes(h, w, idx, 0) = (center_x_temp - box_width_ratio / 2.)
+                                                        / img_width >= 0 ?
+                                                        (center_x_temp - box_width_ratio / 2.)
+                                                        / img_width : 0 ;
+                                e_boxes(h, w, idx, 1) = (center_y_temp - box_height_ratio / 2.)
+                                                        / img_height >= 0 ?
+                                                        (center_y_temp - box_height_ratio / 2.)
+                                                        / img_height : 0;
+                                e_boxes(h, w, idx, 2) = (center_x_temp + box_width_ratio / 2.)
+                                                        / img_width <= 1 ?
+                                                        (center_x_temp + box_width_ratio / 2.)
+                                                        / img_width : 1;
+                                e_boxes(h, w, idx, 3) = (center_y_temp + box_height_ratio / 2.)
+                                                        / img_height <= 1 ?
+                                                        (center_y_temp + box_height_ratio / 2.)
+                                                        / img_height : 1;
+                                idx++;
+                            }
+                        }
+                    }
+					h2=idx;
+		//			LOG(ERROR)<<"C++ does not have fixed_ratios idx: "<<idx;
+                }
+            }
+
+			// min max aspect order
+		//	LOG(ERROR)<<"C++ total density idx --- :" << idx;
+
+            for (size_t s = 0; s < min_sizes.size(); ++s) {
+                auto min_size = min_sizes[s];
+                if (min_max_aspect_ratios_order) {
+                    box_width = box_height = min_size / 2.;
+                    e_boxes(h, w, idx, 0) = (center_x - box_width) / img_width;
+                    e_boxes(h, w, idx, 1) = (center_y - box_height) / img_height;
+                    e_boxes(h, w, idx, 2) = (center_x + box_width) / img_width;
+                    e_boxes(h, w, idx, 3) = (center_y + box_height) / img_height;
+                    idx++;
+                    if (max_sizes.size() > 0) {
+                        auto max_size = max_sizes[s];
+                       	// square prior with size sqrt(minSize * maxSize)
+                        box_width = box_height = sqrt(min_size * max_size) / 2.;
+                        e_boxes(h, w, idx, 0) = (center_x - box_width) / img_width;
+                        e_boxes(h, w, idx, 1) = (center_y - box_height) / img_height;
+                        e_boxes(h, w, idx, 2) = (center_x + box_width) / img_width;
+                        e_boxes(h, w, idx, 3) = (center_y + box_height) / img_height;
+                        idx++;
+                    }
+                    // priors with different aspect ratios
+                    for (size_t r = 0; r < aspect_ratios.size(); ++r) {
+                        float ar = aspect_ratios[r];
+                        if (fabs(ar - 1.) < 1e-6) {
+                            continue;
+                        }
+                        box_width = min_size * sqrt(ar) / 2.;
+                        box_height = min_size / sqrt(ar) / 2.;
+                        e_boxes(h, w, idx, 0) = (center_x - box_width) / img_width;
+                        e_boxes(h, w, idx, 1) = (center_y - box_height) / img_height;
+                        e_boxes(h, w, idx, 2) = (center_x + box_width) / img_width;
+                        e_boxes(h, w, idx, 3) = (center_y + box_height) / img_height;
+                        idx++;
+                    }
+                } else {
+                    for (size_t r = 0; r < aspect_ratios.size(); ++r) {
+                        float ar = aspect_ratios[r];
+                        box_width = min_size * sqrt(ar) / 2.;
+                        box_height = min_size / sqrt(ar) / 2.;
+                        e_boxes(h, w, idx, 0) = (center_x - box_width) / img_width;
+                        e_boxes(h, w, idx, 1) = (center_y - box_height) / img_height;
+                        e_boxes(h, w, idx, 2) = (center_x + box_width) / img_width;
+                        e_boxes(h, w, idx, 3) = (center_y + box_height) / img_height;
+                        idx++;
+                    }
+                    if (max_sizes.size() > 0) {
+                        auto max_size = max_sizes[s];
+                        // square prior with size sqrt(minSize * maxSize)
+                        box_width = box_height = sqrt(min_size * max_size) / 2.;
+                        e_boxes(h, w, idx, 0) = (center_x - box_width) / img_width;
+                        e_boxes(h, w, idx, 1) = (center_y - box_height) / img_height;
+                        e_boxes(h, w, idx, 2) = (center_x + box_width) / img_width;
+                        e_boxes(h, w, idx, 3) = (center_y + box_height) / img_height;
+                        idx++;
+                    }
+                }
+
+            }
+			h3 = idx;
+        }
+    }
+	LOG(ERROR)<<"C++ have fixed size w/ fixed ratio"<< h1;
+	LOG(ERROR)<<"C++ have fixed size without fixed ratio"<< h2;
+	LOG(ERROR)<<"C++ have fixed size after density"<< h3;
+ 
+    if (clip) {
+      platform::Transform<platform::CPUDeviceContext> trans;
+      ClipFunctor<T> clip_func;
+      trans(ctx.template device_context<platform::CPUDeviceContext>(),
+            boxes->data<T>(), boxes->data<T>() + boxes->numel(),
+            boxes->data<T>(), clip_func);
+    }
+    framework::Tensor var_t;
+    var_t.mutable_data<T>(
+        framework::make_ddim({1, static_cast<int>(variances.size())}),
+        ctx.GetPlace());
+
+    auto var_et = framework::EigenTensor<T, 2>::From(var_t);
+
+    for (size_t i = 0; i < variances.size(); ++i) {
+      var_et(0, i) = variances[i];
+    }
+
+    int box_num = feature_height * feature_width * num_priors;
+    auto var_dim = vars->dims();
+    vars->Resize({box_num, static_cast<int>(variances.size())});
+
+    auto e_vars = framework::EigenMatrix<T, Eigen::RowMajor>::From(*vars);
+
+    e_vars = var_et.broadcast(Eigen::DSizes<int, 2>(box_num, 1));
+
+    vars->Resize(var_dim);
+  }
+};  // namespace operators
+
+}  // namespace operators
+}  // namespace paddle
+
