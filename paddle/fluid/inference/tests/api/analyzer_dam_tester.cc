0e87dfe18eee5cafbdce7e6bba9e519c1fe57e62
Zhen Wang <wangzhen31@baidu.com>
2018-11-05 21:18:46 +0800

    Revert "use separated dam model."
    This reverts commit 13e775c86f909b164b7cc1d35a8a24b964ec622e.

--- paddle/fluid/inference/tests/api/analyzer_dam_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_dam_tester.cc
@@ -152,7 +152,8 @@ void PrepareInputs(std::vector<PaddleTensor> *input_slots, DataRecord *data,
 }
 
 void SetConfig(contrib::AnalysisConfig *cfg) {
-  cfg->model_dir = FLAGS_infer_model;
+  cfg->prog_file = FLAGS_infer_model + "/__model__";
+  cfg->param_file = FLAGS_infer_model + "/param";
   cfg->use_gpu = false;
   cfg->device = 0;
   cfg->specify_input_name = true;
