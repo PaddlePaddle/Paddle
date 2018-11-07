cd00bf9033f3084b58ab4f68fc5ce83287543930
sabreshao <sabre.shao@amd.com>
2018-11-07 12:11:06 -0500

    Add rocprim introduction, remove useless cmake code.

--- cmake/external/rocprim.cmake
+++ cmake/external/rocprim.cmake
@@ -2,6 +2,14 @@ if (NOT WITH_AMD_GPU)
     return()
 endif()
 
+# rocprim is "ROCm Parallel Primitives" for short.
+# It is a header-only library providing HIP and HC parallel primitives
+# for developing performant GPU-accelerated code on AMD ROCm platform.
+
+if("x${HCC_HOME}" STREQUAL "x")
+  set(HCC_HOME "/opt/rocm/hcc")
+endif()
+
 INCLUDE(ExternalProject)
 
 SET(ROCPRIM_SOURCE_DIR ${THIRD_PARTY_PATH}/rocprim)
@@ -14,7 +22,7 @@ ExternalProject_Add(
     GIT_TAG        5bd41b96ab8d8343330fb2c3e1b96775bde3b3fc 
     PREFIX         ${ROCPRIM_SOURCE_DIR}
     UPDATE_COMMAND  ""
-    CMAKE_ARGS     -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hcc
+    CMAKE_ARGS     -DCMAKE_CXX_COMPILER=${HCC_HOME}/bin/hcc
     CMAKE_ARGS     -DONLY_INSTALL=ON
     CMAKE_ARGS     -DBUILD_TEST=OFF
     CMAKE_ARGS     -DCMAKE_INSTALL_PREFIX=${ROCPRIM_INSTALL_DIR}
@@ -22,7 +30,6 @@ ExternalProject_Add(
     INSTALL_DIR    ${ROCPRIM_INSTALL_DIR}
     ${EXTERNAL_PROJECT_LOG_ARGS}
 )
-
 
 INCLUDE_DIRECTORIES(${ROCPRIM_INCLUDE_DIR})
 
@@ -35,5 +42,3 @@ else()
 endif()
 
 add_dependencies(rocprim extern_rocprim)
-
-LIST(APPEND external_project_dependencies rocprim)
