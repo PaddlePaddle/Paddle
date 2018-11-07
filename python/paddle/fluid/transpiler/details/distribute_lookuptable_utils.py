3d8077e9fb92a5f2a21c214162f04ba200bcc92d
Qiao Longfei <qiaolongfei@baidu.com>
2018-11-07 17:30:48 +0800

    update optimizer

--- python/paddle/fluid/transpiler/details/distribute_lookuptable_utils.py
+++ python/paddle/fluid/transpiler/details/distribute_lookuptable_utils.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import paddle.fluid.optimizer as optimizer
-import paddle.fluid.framework as framework
-
 LOOKUP_TABLE_TYPE = "lookup_table"
 
 
