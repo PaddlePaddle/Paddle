a1f6ec19d02cde7006a90349936c1bc082aeb947
Jacek Czaja <jacek.czaja@intel.com>
2018-11-09 13:09:37 +0100

    - Fix to linking for GPU builds of softmax inference
    test=develop

--- paddle/fluid/operators/math/softmax.cu
+++ paddle/fluid/operators/math/softmax.cu
@@ -100,8 +100,12 @@ template class SoftmaxGradCUDNNFunctor<platform::float16>;
 
 template class SoftmaxFunctor<platform::CUDADeviceContext, platform::float16,
                               false>;
+template class SoftmaxFunctor<platform::CUDADeviceContext, platform::float16,
+                              true>;
 template class SoftmaxFunctor<platform::CUDADeviceContext, float, false>;
 template class SoftmaxFunctor<platform::CUDADeviceContext, double, false>;
+template class SoftmaxFunctor<platform::CUDADeviceContext, float, true>;
+template class SoftmaxFunctor<platform::CUDADeviceContext, double, true>;
 template class SoftmaxGradFunctor<platform::CUDADeviceContext, float>;
 template class SoftmaxGradFunctor<platform::CUDADeviceContext, double>;
 template class SoftmaxGradFunctor<platform::CUDADeviceContext,
