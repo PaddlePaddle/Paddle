acdd84f6d634d7dcc303212d3d681e560dd12aee
typhoonzero <typhoonzero1986@gmail.com>
2018-11-12 16:49:48 +0800

    fix build test=develop

--- paddle/fluid/operators/warpctc_op.cc
+++ paddle/fluid/operators/warpctc_op.cc
@@ -14,6 +14,10 @@ limitations under the License. */
 
 #include "paddle/fluid/operators/warpctc_op.h"
 
+#ifdef PADDLE_WITH_CUDA
+#include "paddle/fluid/platform/cudnn_helper.h"
+#endif
+
 namespace paddle {
 namespace operators {
 
