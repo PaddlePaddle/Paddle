3c6f06e18974d6bee674d64ab0be67a79bde6357
typhoonzero <typhoonzero1986@gmail.com>
2018-11-12 12:12:51 +0800

    move include cudnn test=develop

--- paddle/fluid/operators/warpctc_op.cc
+++ paddle/fluid/operators/warpctc_op.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/operators/warpctc_op.h"
-#include "paddle/fluid/platform/cudnn_helper.h"
 
 namespace paddle {
 namespace operators {
