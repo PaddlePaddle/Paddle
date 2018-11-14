2223aea80a018266a63db97d583169f2206a1876
Jacek Czaja <jacek.czaja@intel.com>
2018-11-14 12:20:36 +0100

    - Softmax for Inference is enabled when ON_INFER is set
    test=develop

--- paddle/fluid/operators/math/softmax.h
+++ paddle/fluid/operators/math/softmax.h
@@ -19,7 +19,7 @@ namespace paddle {
 namespace operators {
 namespace math {
 
-template <typename DeviceContext, typename T>
+template <typename DeviceContext, typename T, bool is_test>
 class SoftmaxFunctor {
  public:
   void operator()(const DeviceContext& context, const framework::Tensor* X,
