36c23e1352ccb08005f75db483201462996afe4d
Yihua Xu <yihuax.xu@intel.com>
2018-11-15 09:12:17 +0800

    Add math headfile to fix the compile issue (test=develop)

--- paddle/fluid/operators/math/jit_kernel_layer_norm.cc
+++ paddle/fluid/operators/math/jit_kernel_layer_norm.cc
@@ -9,6 +9,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 #include "paddle/fluid/operators/math/jit_kernel.h"
+#include <math.h>
 #include <limits>
 #include <string>
 #include "paddle/fluid/operators/math/jit_kernel_macro.h"
