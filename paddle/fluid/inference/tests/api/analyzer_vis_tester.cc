9e6b1c5f974bdb42c8ec5dc323f76d405e8017d8
Yiqun Liu <liuyiqun01@baidu.com>
2018-11-15 10:28:52 +0800

    Refine tester of TensorRT engine (#14390)
    * Refine the tester for MixedRTPredictor.
    test=develop
    
    * Enable the profiler in TensorRT engine.
    
    * Support the use of combined inference model in TensorRT unittest, and print the shape of feed targets.

--- paddle/fluid/inference/tests/api/analyzer_vis_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_vis_tester.cc
@@ -94,7 +94,8 @@ void profile(bool use_mkldnn = false) {
 
   std::vector<std::vector<PaddleTensor>> input_slots_all;
   SetInput(&input_slots_all);
-  TestPrediction(cfg, input_slots_all, &outputs, FLAGS_num_threads);
+  TestPrediction(reinterpret_cast<const PaddlePredictor::Config *>(&cfg),
+                 input_slots_all, &outputs, FLAGS_num_threads);
 
   if (FLAGS_num_threads == 1 && !FLAGS_test_all_data) {
     const float ocr_result_data[] = {
@@ -136,7 +137,8 @@ void compare(bool use_mkldnn = false) {
 
   std::vector<std::vector<PaddleTensor>> input_slots_all;
   SetInput(&input_slots_all);
-  CompareNativeAndAnalysis(cfg, input_slots_all);
+  CompareNativeAndAnalysis(
+      reinterpret_cast<const PaddlePredictor::Config *>(&cfg), input_slots_all);
 }
 
 TEST(Analyzer_vis, compare) { compare(); }
