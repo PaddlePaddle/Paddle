23604231dad1e048797e309978b84a15b7b9fd2d
peizhilin <wopeizl@163.com>
2018-11-24 23:21:53 +0800

    code style fix test=develop

--- paddle/fluid/framework/operator.cc
+++ paddle/fluid/framework/operator.cc
@@ -160,7 +160,7 @@ void OperatorBase::Run(const Scope& scope, const platform::Place& place) {
     platform::DeviceContextPool& pool = platform::DeviceContextPool::Instance();
     platform::RecordEvent record_event(Type(), pool.Get(place));
     RunImpl(scope, place);
-  } else  {
+  } else {
     RunImpl(scope, place);
   }
   VLOG(30) << place << " " << DebugStringEx(&scope);
