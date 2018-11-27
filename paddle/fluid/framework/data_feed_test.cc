903570bf1e9f1732d6b05a8b7286963176c8f468
barrierye <barriery@qq.com>
2018-11-27 13:07:10 +0800

    update data_feed_test for multi-type

--- paddle/fluid/framework/data_feed_test.cc
+++ paddle/fluid/framework/data_feed_test.cc
@@ -31,7 +31,7 @@ paddle::framework::DataFeedDesc load_datafeed_param_from_file(const char* filena
   paddle::framework::DataFeedDesc data_feed_desc;
   int file_descriptor = open(filename, O_RDONLY);
   if (file_descriptor == -1){
-    LOG(ERROR) << "FATAL: cant open " << filename << std::endl;
+    LOG(ERROR) << "error: cant open " << filename << std::endl;
     exit(-1);
   }   
   google::protobuf::io::FileInputStream fileInput(file_descriptor);
@@ -44,7 +44,7 @@ const std::vector<std::string> load_filelist_from_file(const char* filename) {
   std::vector<std::string> filelist;
   std::ifstream fin(filename);
   if (!fin.good()) {
-    LOG(ERROR) << "FATAL: cant open " << filename << std::endl;
+    LOG(ERROR) << "error: cant open " << filename << std::endl;
     exit(-1);
   }
   std::string line;
@@ -55,7 +55,20 @@ const std::vector<std::string> load_filelist_from_file(const char* filename) {
   return filelist;
 }
 
-void GetElemSetFromReader(std::vector<std::set<int64_t>>& reader_elem_set,
+class MultiTypeSet {
+ public:
+  MultiTypeSet() {uint64_set_.clear(); float_set_.clear();}
+  ~MultiTypeSet() {}
+  void AddValue(uint64_t v) {uint64_set_.insert(v);}
+  void AddValue(float v) {float_set_.insert(v);}
+  const std::set<uint64_t>& GetUint64Set() const {return uint64_set_;}
+  const std::set<float>& GetFloatSet() const {return float_set_;}
+ private:
+  std::set<uint64_t> uint64_set_;
+  std::set<float> float_set_;
+};
+
+void GetElemSetFromReader(std::vector<MultiTypeSet>& reader_elem_set,
         const paddle::framework::DataFeedDesc& data_feed_desc,
         const std::vector<std::string>& filelist, const int thread_num) {
   reader_elem_set.resize(data_feed_desc.multi_slot_desc().slots_size());
@@ -82,16 +95,31 @@ void GetElemSetFromReader(std::vector<std::set<int64_t>>& reader_elem_set,
       }
       readers[idx]->Start();
       while (readers[idx]->Next()) {
-        for (size_t k = 0; k < use_slot_alias.size(); ++k){
-          auto name = use_slot_alias[k];
+        for (int k = 0; k < data_feed_desc.multi_slot_desc().slots_size(); ++k){
+          auto slot = data_feed_desc.multi_slot_desc().slots(k);
+          auto name = slot.name();
           const paddle::framework::LoDTensor *tens = feed_targets[name];
-          const int64_t* data = tens->data<int64_t>();
-          for (size_t i = 0; i < tens->NumElements(); ++i){
-            std::pair<size_t, size_t> element = tens->lod_element(0, i);
-            for (size_t j = element.first; j < element.second; ++j){
-              std::lock_guard<std::mutex> lock(mu);
-              reader_elem_set[k].insert(data[j]);
+          if (slot.type() == "uint64") {
+            const int64_t* data = tens->data<int64_t>();
+            for (size_t i = 0; i < tens->NumElements(); ++i){
+              std::pair<size_t, size_t> element = tens->lod_element(0, i);
+              for (size_t j = element.first; j < element.second; ++j){
+                std::lock_guard<std::mutex> lock(mu);
+                reader_elem_set[k].AddValue((uint64_t)data[j]);
+              }
             }
+          } else if (slot.type() == "float") {
+            const float* data = tens->data<float>();
+            for (size_t i = 0; i < tens->NumElements(); ++i){
+              std::pair<size_t, size_t> element = tens->lod_element(0, i);
+              for (size_t j = element.first; j < element.second; ++j){
+                std::lock_guard<std::mutex> lock(mu);
+                reader_elem_set[k].AddValue(data[j]);
+              }
+            }
+          } else {
+            LOG(ERROR) << "error: error type in proto file";
+            exit(-1);
           }
         } // end slots loop
       } // end while Next()
@@ -102,22 +130,36 @@ void GetElemSetFromReader(std::vector<std::set<int64_t>>& reader_elem_set,
   }
 }
 
-void CheckIsUnorderedSame(const std::vector<std::set<int64_t>>& s1,
-        const std::vector<std::set<int64_t>>& s2) {
+void CheckIsUnorderedSame(const std::vector<MultiTypeSet>& s1,
+        const std::vector<MultiTypeSet>& s2) {
   EXPECT_EQ(s1.size(), s2.size());
   for (size_t i = 0; i < s1.size(); ++i) {
-    EXPECT_EQ(s1[i].size(), s2[i].size());
-    auto it1 = s1[i].begin();
-    auto it2 = s2[i].begin();
-    while (it1 != s1[i].end()) {
-      EXPECT_EQ(*it1, *it2);
-      ++it1;
-      ++it2;
+    // check for uint64
+    const std::set<uint64_t>& uint64_s1 = s1[i].GetUint64Set();
+    const std::set<uint64_t>& uint64_s2 = s2[i].GetUint64Set();
+    EXPECT_EQ(uint64_s1.size(), uint64_s2.size());
+    auto uint64_it1 = uint64_s1.begin();
+    auto uint64_it2 = uint64_s2.begin();
+    while (uint64_it1 != uint64_s1.end()) {
+      EXPECT_EQ(*uint64_it1, *uint64_it2);
+      ++uint64_it1;
+      ++uint64_it2;
+    }
+    // check for float
+    const std::set<float>& float_s1 = s1[i].GetFloatSet();
+    const std::set<float>& float_s2 = s2[i].GetFloatSet();
+    EXPECT_EQ(float_s1.size(), float_s2.size());
+    auto float_it1 = float_s1.begin();
+    auto float_it2 = float_s2.begin();
+    while (float_it1 != float_s1.end()) {
+      EXPECT_EQ(*float_it1, *float_it2);
+      ++float_it1;
+      ++float_it2;
     }
   }
 }
 
-void GetElemSetFromFile(std::vector<std::set<int64_t>>& file_elem_set,
+void GetElemSetFromFile(std::vector<MultiTypeSet>& file_elem_set,
         const paddle::framework::DataFeedDesc& data_feed_desc,
         const std::vector<std::string>& filelist) {
   file_elem_set.resize(data_feed_desc.multi_slot_desc().slots_size());
@@ -132,10 +174,22 @@ void GetElemSetFromFile(std::vector<std::set<int64_t>>& file_elem_set,
       for (auto i = 0; i < data_feed_desc.multi_slot_desc().slots_size(); ++i) {
         int num;
         if (fin >> num) {
-          while (num--) {
-            uint64_t feasign;
-            fin >> feasign;
-            file_elem_set[i].insert(feasign);
+          auto type = data_feed_desc.multi_slot_desc().slots(i).type();
+          if (type == "uint64") {
+            while (num--) {
+              uint64_t feasign;
+              fin >> feasign;
+              file_elem_set[i].AddValue(feasign);
+            }
+          } else if (type == "float") {
+            while (num--) {
+              float feasign;
+              fin >> feasign;
+              file_elem_set[i].AddValue(feasign);
+            }
+          } else {
+            LOG(ERROR) << "error: error type in proto file";
+            exit(-1);
           }
         } else {
           end_flag = true;
@@ -150,13 +204,13 @@ void GetElemSetFromFile(std::vector<std::set<int64_t>>& file_elem_set,
   }
 }
 
-TEST(DataFeed, ReadUintTest) {
+TEST(DataFeed, MultiSlotUnitTest) {
   const char* protofile = "data_feed_desc.prototxt";
   const char* filelist_name = "filelist.txt";
   const std::vector<std::string> filelist = load_filelist_from_file(filelist_name);
   paddle::framework::DataFeedDesc data_feed_desc = load_datafeed_param_from_file(protofile);
-  std::vector<std::set<int64_t>> reader_elem_set;
-  std::vector<std::set<int64_t>> file_elem_set;
+  std::vector<MultiTypeSet> reader_elem_set;
+  std::vector<MultiTypeSet> file_elem_set;
   GetElemSetFromReader(reader_elem_set, data_feed_desc, filelist, 12);
   GetElemSetFromFile(file_elem_set, data_feed_desc, filelist);
   CheckIsUnorderedSame(reader_elem_set, file_elem_set);
