ad6ed5b745dbaa64dc6ba3c48443adbc63c50aa7
Xin Pan <panxin.grad@gmail.com>
2018-11-28 18:58:57 +0800

    fix py3
    test=develop

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -459,6 +459,7 @@ function assert_api_not_changed() {
         # Use sed to make python2 and python3 sepc keeps the same
         sed -i 's/arg0: str/arg0: unicode/g' new.spec
         sed -i "s/\(.*Transpiler.*\).__init__ ArgSpec(args=\['self'].*/\1.__init__ /g" new.spec
+        sed -i 's/\(.*ComposeNotAligned.*\).__init__ ArgSpec(args=.*/\1.__init__ /g' new.spec
     fi
     python ${PADDLE_ROOT}/tools/diff_api.py ${PADDLE_ROOT}/paddle/fluid/API.spec new.spec
     deactivate
