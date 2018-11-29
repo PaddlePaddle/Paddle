bd94ab0ef3e172fab40aa316914dbae8868c9f41
phlrain <phliuhongyu@126.com>
2018-11-29 19:21:11 +0800

    rename op; test=develop

--- paddle/fluid/operators/lstm_cudnn_op.cc
+++ paddle/fluid/operators/lstm_cudnn_op.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/operators/cudnn_lstm_op.h"
+#include "paddle/fluid/operators/lstm_cudnn_op.h"
 #include <string>
 
 #ifdef PADDLE_WITH_CUDA
