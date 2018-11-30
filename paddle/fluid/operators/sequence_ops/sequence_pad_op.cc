02f113d82d376ff3fb2f6dec422bc1accbae8d28
root <root@yq01-gpu-255-143-15-00.epc.baidu.com>
2018-11-30 09:55:22 +0000

    cherry pick e3d970f09d96123c9e47f62915df47b9523617b3

--- paddle/fluid/operators/sequence_ops/sequence_pad_op.cc
+++ paddle/fluid/operators/sequence_ops/sequence_pad_op.cc
@@ -72,9 +72,9 @@ class SequencePadOp : public framework::OperatorWithKernel {
       if (padded_length == -1) {
         padded_length = 1;
       }
-      framework::VarDesc* x_desc =
-          boost::get<framework::VarDesc*>(ctx->GetInputVarPtrs("X")[0]);
-      PADDLE_ENFORCE_GE(x_desc->GetLoDLevel(), 1);
+      //framework::VarDesc* x_desc =
+          //boost::get<framework::VarDesc*>(ctx->GetInputVarPtrs("X")[0]);
+      //PADDLE_ENFORCE_GE(x_desc->GetLoDLevel(), 1);
     }
 
     std::vector<int> out_dims_vec{out_dim_0, padded_length};
