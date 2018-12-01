3c239cd640aca1fa8da71a9cdc319b8b4e4fb36c
heqiaozhi <heqiaozhi@baidu.com>
2018-12-01 13:10:21 +0800

    pslib

--- paddle/fluid/framework/async_executor.cc
+++ paddle/fluid/framework/async_executor.cc
@@ -29,6 +29,7 @@ limitations under the License. */
 #include "paddle/fluid/inference/io.h"
 #include "paddle/fluid/platform/place.h"
 #include "paddle/fluid/pybind/pybind.h"
+#include "pslib.h"
 
 namespace paddle {
 namespace framework {
