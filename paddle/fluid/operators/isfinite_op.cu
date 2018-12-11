91c321275cb93b95926aa5b900a28c63f117c78b
Yu Yang <yuyang18@baidu.com>
2018-12-11 13:14:50 +0800

    Fix Eigen macro when using GPU
    The macro should be defined by compiler rather than by source.
    
    test=develop

--- paddle/fluid/operators/isfinite_op.cu
+++ paddle/fluid/operators/isfinite_op.cu
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#define EIGEN_USE_GPU
 #include "paddle/fluid/operators/isfinite_op.h"
 #include "paddle/fluid/platform/float16.h"
 
