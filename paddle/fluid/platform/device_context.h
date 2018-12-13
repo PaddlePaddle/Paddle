0dfee5b3f70fa75682d40ba9d1fe27120663c686
chengduozh <zhaochengduo@baidu.com>
2018-12-13 20:57:29 +0800

    remove mutex in device context

--- paddle/fluid/platform/device_context.h
+++ paddle/fluid/platform/device_context.h
@@ -216,7 +216,7 @@ class CUDADeviceContext : public DeviceContext {
 
   template <typename Callback>
   void RecordEvent(cudaEvent_t ev, Callback callback) {
-    std::lock_guard<std::mutex> guard(mtx_);
+    // std::lock_guard<std::mutex> guard(mtx_);
     callback();
     PADDLE_ENFORCE(cudaEventRecord(ev, stream_));
   }
