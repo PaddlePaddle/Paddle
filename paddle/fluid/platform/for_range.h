b98040f895c29790f646f357cb5fd06f46c1c986
Qiao Longfei <qiaolongfei@baidu.com>
2018-12-17 22:16:52 +0800

    clean code test=develop

--- paddle/fluid/platform/for_range.h
+++ paddle/fluid/platform/for_range.h
@@ -13,10 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
-
-#include <vector>
-
-#include "paddle/fluid/framework/mixed_vector.h"
 #include "paddle/fluid/platform/device_context.h"
 
 namespace paddle {
