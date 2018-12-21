0d4d8bf5853dda6154d9288fb940127558392bb8
minqiyang <minqiyang@baidu.com>
2018-12-21 16:13:16 +0800

    Polish code
    test=develop

--- paddle/fluid/platform/enforce.h
+++ paddle/fluid/platform/enforce.h
@@ -260,24 +260,24 @@ inline void throw_on_error(T e) {
 
 #define PADDLE_JUDGE
 
-#define __PADDLE_UNARY_COMPARE(COND, ...)                                     \
-  do {                                                                        \
-    auto __cond = COND;                                                       \
-    if (UNLIKELY(::paddle::platform::is_error(__cond))) {                     \
-      ::paddle::platform::throw_on_error(__cond, ##__VA_ARGS__); /* NOLINT */ \
-    }                                                                         \
-  } while (0)
+#define __PADDLE_UNARY_COMPARE(COND, ...)                        \
+  do {                                                           \
+    auto __cond = COND;                                          \
+    if (UNLIKELY(::paddle::platform::is_error(__cond))) {        \
+      ::paddle::platform::throw_on_error(__cond, ##__VA_ARGS__); \
+    }                                                            \
+  } while (0)  // NOLINT
 
 #ifndef REPLACE_ENFORCE_GLOG
 #define PADDLE_ENFORCE(COND, ...)                                       \
   do {                                                                  \
     try {                                                               \
-      __PADDLE_UNARY_COMPARE(COND, ##__VA_ARGS__); /* NOLINT */         \
+      __PADDLE_UNARY_COMPARE(COND, ##__VA_ARGS__);                      \
     } catch (...) {                                                     \
       throw ::paddle::platform::EnforceNotMet(std::current_exception(), \
                                               __FILE__, __LINE__);      \
     }                                                                   \
-  } while (false)
+  } while (0)  // NOLINT
 
 #else
 #define PADDLE_ENFORCE(COND, ...) __PADDLE_UNARY_COMPARE(COND, ##__VA_ARGS__);
