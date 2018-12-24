fbd75e08cd832edc1dac41f27c379c0a0b2e10af
minqiyang <minqiyang@baidu.com>
2018-12-24 13:02:13 +0800

    Fix syntax error in unit test
    test=develop

--- paddle/fluid/inference/analysis/analyzer_tester.cc
+++ paddle/fluid/inference/analysis/analyzer_tester.cc
@@ -69,9 +69,9 @@ void TestWord2vecPrediction(const std::string& model_path) {
   std::vector<PaddleTensor> outputs;
   CHECK(predictor->Run(slots, &outputs));
 
-  PADDLE_ENFORCE(outputs.size(), 1UL);
+  PADDLE_ENFORCE_EQ(outputs.size(), 1UL);
   // Check the output buffer size and result of each tid.
-  PADDLE_ENFORCE(outputs.front().data.length(), 33168UL);
+  PADDLE_ENFORCE_EQ(outputs.front().data.length(), 33168UL);
   float result[5] = {0.00129761, 0.00151112, 0.000423564, 0.00108815,
                      0.000932706};
   const size_t num_elements = outputs.front().data.length() / sizeof(float);
@@ -80,8 +80,8 @@ void TestWord2vecPrediction(const std::string& model_path) {
        i++) {
     LOG(INFO) << "data: "
               << static_cast<float*>(outputs.front().data.data())[i];
-    PADDLE_ENFORCE(static_cast<float*>(outputs.front().data.data())[i],
-                   result[i]);
+    PADDLE_ENFORCE_EQ(static_cast<float*>(outputs.front().data.data())[i],
+                      result[i]);
   }
 }
 
