e821b12f57487f2ecab8debb13531adc05dd9453
Brian Liu <brian.liu@intel.com>
2018-12-25 14:48:02 +0800

    Fix issue which cause abnormal CPU usage in stack op
    Stack OP has much higher CPU cost than expected in release mode.
    Caused by DebugStringEx() in base class OperatorWithKernel. Actually
    this issue occur for each OP which hasn't implement it's own
    GetExpectedKernelType().
    
    test=develop

--- paddle/fluid/framework/operator.cc
+++ paddle/fluid/framework/operator.cc
@@ -1061,8 +1061,8 @@ proto::VarType::Type OperatorWithKernel::IndicateDataType(
           t = &(var->Get<SelectedRows>().value());
         }
         if (t != nullptr) {
-          PADDLE_ENFORCE(t->IsInitialized(), "Input %s is not initialized: %s",
-                         ipt_name, DebugString());
+          PADDLE_ENFORCE(t->IsInitialized(), "Input %s is not initialized",
+                         ipt_name);
           int tmp = static_cast<int>(t->type());
           PADDLE_ENFORCE(
               tmp == data_type || data_type == -1,
