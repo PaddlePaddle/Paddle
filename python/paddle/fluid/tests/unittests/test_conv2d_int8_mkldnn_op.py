32a0311fee0b7f622ec1aba304be7ac6c63a68b4
xiaoli.liu@intel.com <xiaoli.liu@intel.com>
2018-12-29 21:10:59 +0800

    Clean unittest code test=develop

--- python/paddle/fluid/tests/unittests/test_conv2d_int8_mkldnn_op.py
+++ python/paddle/fluid/tests/unittests/test_conv2d_int8_mkldnn_op.py
@@ -19,58 +19,22 @@ import numpy as np
 
 import paddle.fluid.core as core
 from op_test import OpTest
+from test_conv2d_op import conv2d_forward_naive, TestConv2dOp
 
 
-def conv2d_forward_naive(input, filter, group, conv_param):
-    in_n, in_c, in_h, in_w = input.shape
-    out_c, f_c, f_h, f_w = filter.shape
-    assert f_c * group == in_c
-    assert np.mod(out_c, group) == 0
-    sub_out_c = out_c // group
-
-    stride, pad, dilation = conv_param['stride'], conv_param['pad'], conv_param[
-        'dilation']
-    out_h = 1 + (in_h + 2 * pad[0] - (dilation[0] * (f_h - 1) + 1)) // stride[0]
-    out_w = 1 + (in_w + 2 * pad[1] - (dilation[1] * (f_w - 1) + 1)) // stride[1]
-    out = np.zeros((in_n, out_c, out_h, out_w))
-
-    d_bolck_h = (dilation[0] * (f_h - 1) + 1)
-    d_bolck_w = (dilation[1] * (f_w - 1) + 1)
-
-    input_pad = np.pad(input, ((0, ), (0, ), (pad[0], ), (pad[1], )),
-                       mode='constant',
-                       constant_values=0)
-
-    filter_dilation = np.zeros((out_c, f_c, d_bolck_h, d_bolck_w))
-    filter_dilation[:, :, 0:d_bolck_h:dilation[0], 0:d_bolck_w:dilation[
-        1]] = filter
-
-    for i in range(out_h):
-        for j in range(out_w):
-            for g in range(group):
-                input_pad_masked = \
-                    input_pad[:, g * f_c:(g + 1) * f_c,
-                    i * stride[0]:i * stride[0] + d_bolck_h,
-                    j * stride[1]:j * stride[1] + d_bolck_w]
-
-                f_sub = filter_dilation[g * sub_out_c:(g + 1) *
-                                        sub_out_c, :, :, :]
-                for k in range(sub_out_c):
-                    out[:, g * sub_out_c + k, i, j] = \
-                        np.sum(input_pad_masked * f_sub[k, :, :, :],
-                               axis=(1, 2, 3))
+def conv2d_forward_refer(input, filter, group, conv_param):
+    out, in_n, out_h, out_w, out_c = conv2d_forward_naive(input, filter, group,
+                                                          conv_param)
     out_tmp = np.zeros((in_n, out_h, out_w, out_c))
     for n in range(in_n):
         for i in range(out_h):
             for j in range(out_w):
                 for m in range(out_c):
                     out_tmp[n, i, j, m] = out[n, m, i, j]
-
-    out = out_tmp.reshape(in_n, out_c, out_h, out_w)
-    return out
+    return out_tmp.reshape(in_n, out_c, out_h, out_w)
 
 
-class TestConv2dOp(OpTest):
+class TestConv2dInt8Op(TestConv2dOp):
     def setUp(self):
         self.op_type = "conv2d"
         self.use_cudnn = False
@@ -83,7 +47,7 @@ class TestConv2dOp(OpTest):
         self.init_group()
         self.init_dilation()
         self.init_test_case()
-        self.init_kernel_type()
+        self.init_int_out_dtype()
 
         conv2d_param = {
             'stride': self.stride,
@@ -92,7 +56,6 @@ class TestConv2dOp(OpTest):
         }
 
         filter = np.random.random(self.filter_size).astype(self.weighttype)
-
         if self.srctype == np.uint8:
             input = np.random.randint(0, 10,
                                       self.input_size).astype(self.srctype)
@@ -102,22 +65,22 @@ class TestConv2dOp(OpTest):
             input_shift = (np.ones(self.input_size) * 128).astype(np.uint8)
 
         if self.srctype == np.int8:
-            output1 = conv2d_forward_naive(
+            output1 = conv2d_forward_refer(
                 np.round((input.astype(np.int32) + input_shift) *
                          self.scale_in).astype(np.int32),
                 np.round(filter * self.scale_weights[0] * 0.5).astype(np.int32),
                 self.groups, conv2d_param).astype(np.float32) * (
                     self.scale_out / (self.scale_in * self.scale_weights[0] *
-                                      0.5))  #).astype(self.dsttype)
-            output2 = conv2d_forward_naive(
+                                      0.5))
+            output2 = conv2d_forward_refer(
                 np.round((input_shift) * self.scale_in).astype(np.int32),
                 np.round(filter * self.scale_weights[0] * 0.5).astype(np.int32),
                 self.groups, conv2d_param).astype(np.float32) * (
                     self.scale_out / (self.scale_in * self.scale_weights[0] *
-                                      0.5))  #).astype(self.dsttype)
+                                      0.5))
             output = np.round(output1 - output2).astype(self.dsttype)
         else:
-            output1 = conv2d_forward_naive(
+            output1 = conv2d_forward_refer(
                 input.astype(np.int32),
                 np.round(filter * self.scale_weights[0]).astype(np.int32),
                 self.groups, conv2d_param).astype(np.float32)
@@ -147,33 +110,33 @@ class TestConv2dOp(OpTest):
     def test_check_output(self):
         self.check_output_with_place(core.CPUPlace(), atol=0)
 
+    def test_check_grad(self):
+        pass
+
+    def test_check_grad_no_filter(self):
+        pass
+
+    def test_check_grad_no_input(self):
+        pass
+
     def init_test_case(self):
-        self.pad = [0, 0]
-        self.stride = [1, 1]
-        self.input_size = [22, 3, 5, 5]  # NCHW
-        assert np.mod(self.input_size[1], self.groups) == 0
+        TestConv2dOp.init_test_case(self)
         f_c = self.input_size[1] // self.groups
         self.filter_size = [1, f_c, 3, 3]
         self.scale_in = 1.0
         self.scale_out = 0.5
         self.scale_weights = [10.0]
 
-    def init_dilation(self):
-        self.dilations = [1, 1]
-
-    def init_group(self):
-        self.groups = 1
-
-    def init_kernel_type(self):
+    def init_int_out_dtype(self):
         self.srctype = np.uint8
         self.dsttype = np.float32
 
 
-class TestConv2d(TestConv2dOp):
+class TestConv2d(TestConv2dInt8Op):
     def init_test_case(self):
         self.pad = [0, 0]
         self.stride = [1, 1]
-        self.input_size = [2, 3, 5, 5]
+        self.input_size = [2, 3, 5, 5]  # NCHW
         assert np.mod(self.input_size[1], self.groups) == 0
         f_c = self.input_size[1] // self.groups
         self.filter_size = [6, f_c, 3, 3]
@@ -182,20 +145,18 @@ class TestConv2d(TestConv2dOp):
         self.scale_weights = [10.0]
 
 
-class TestWithPad(TestConv2dOp):
+class TestWithPad(TestConv2d):
     def init_test_case(self):
+        TestConv2d.init_test_case(self)
         self.pad = [1, 1]
-        self.stride = [1, 1]
-        self.input_size = [2, 3, 5, 5]
-        assert np.mod(self.input_size[1], self.groups) == 0
-        f_c = self.input_size[1] // self.groups
-        self.filter_size = [6, f_c, 3, 3]
-        self.scale_in = 1.0
-        self.scale_out = 1.5
-        self.scale_weights = [10.0]
 
 
-class TestWithStride(TestConv2dOp):
+class TestWithGroup(TestConv2d):
+    def init_group(self):
+        self.groups = 3
+
+
+class TestWithStride(TestConv2dInt8Op):
     def init_test_case(self):
         self.pad = [1, 1]
         self.stride = [2, 2]
@@ -208,23 +169,7 @@ class TestWithStride(TestConv2dOp):
         self.scale_weights = [10.0]
 
 
-class TestWithGroup(TestConv2dOp):
-    def init_test_case(self):
-        self.pad = [0, 0]
-        self.stride = [1, 1]
-        self.input_size = [2, 3, 5, 5]  # NCHW
-        assert np.mod(self.input_size[1], self.groups) == 0
-        f_c = self.input_size[1] // self.groups
-        self.filter_size = [6, f_c, 3, 3]
-        self.scale_in = 1.0
-        self.scale_out = 0.5
-        self.scale_weights = [10.0]
-
-    def init_group(self):
-        self.groups = 3
-
-
-class TestWith1x1(TestConv2dOp):
+class TestWith1x1(TestConv2dInt8Op):
     def init_test_case(self):
         self.pad = [0, 0]
         self.stride = [1, 1]
@@ -237,11 +182,11 @@ class TestWith1x1(TestConv2dOp):
         self.scale_weights = [12.0]
 
 
-class TestWithInput1x1Filter1x1(TestConv2dOp):
+class TestWithInput1x1Filter1x1(TestConv2dInt8Op):
     def init_test_case(self):
         self.pad = [0, 0]
         self.stride = [1, 1]
-        self.input_size = [2, 3, 1, 1]  # NCHW
+        self.input_size = [2, 3, 1, 1]
         assert np.mod(self.input_size[1], self.groups) == 0
         f_c = self.input_size[1] // self.groups
         self.filter_size = [6, f_c, 1, 1]
@@ -253,41 +198,26 @@ class TestWithInput1x1Filter1x1(TestConv2dOp):
         self.groups = 3
 
 
-class TestInt8Input(TestConv2dOp):
-    def init_kernel_type(self):
-        self.srctype = np.int8
-        self.dsttype = np.int8
+#--------------------test conv2d s8 int and s8 out--------------------
 
 
-class TestINT8WithPad(TestWithPad):
-    def init_kernel_type(self):
-        self.srctype = np.int8
-        self.dsttype = np.int8
+def create_test_int8_class(parent):
+    class TestInt8Case(parent):
+        def init_int_out_dtype(self):
+            self.srctype = np.int8
+            self.dsttype = np.int8
+
+    cls_name = "{0}_{1}".format(parent.__name__, "s8s8")
+    TestInt8Case.__name__ = cls_name
+    globals()[cls_name] = TestInt8Case
 
 
-class TestINT8WithStride(TestWithStride):
-    def init_kernel_type(self):
-        self.srctype = np.int8
-        self.dsttype = np.int8
-
-
-class TestINT8WithGroup(TestWithGroup):
-    def init_kernel_type(self):
-        self.srctype = np.int8
-        self.dsttype = np.int8
-
-
-class TestINT8With1x1(TestWith1x1):
-    def init_kernel_type(self):
-        self.srctype = np.int8
-        self.dsttype = np.int8
-
-
-class TestINT8WithInput1x1Filter1x1(TestWithInput1x1Filter1x1):
-    def init_kernel_type(self):
-        self.srctype = np.int8
-        self.dsttype = np.int8
-
+create_test_int8_class(TestConv2dInt8Op)
+create_test_int8_class(TestWithPad)
+create_test_int8_class(TestWithStride)
+create_test_int8_class(TestWithGroup)
+create_test_int8_class(TestWith1x1)
+create_test_int8_class(TestWithInput1x1Filter1x1)
 
 if __name__ == '__main__':
     unittest.main()
