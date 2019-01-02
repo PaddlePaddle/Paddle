44f5e9918a15bd3c1fac690d16f44a1d704b9702
Dun Liang <randonlang@gmail.com>
2019-01-02 15:13:12 +0800

    add topological sorting

--- paddle/fluid/framework/details/all_reduce_deps_pass.cc
+++ paddle/fluid/framework/details/all_reduce_deps_pass.cc
@@ -48,19 +48,71 @@ std::unique_ptr<ir::Graph> AllReduceDepsPass::ApplyImpl(
   auto graph_ops = ir::FilterByNodeWrapper<OpHandleBase>(*graph);
 
   // get vars order
-  int order = 0;
   std::unordered_map<std::string, int> vars;
-  // TODO(gongwb): use graph topology sort to find the order of operators.
-  //               Note that must assert topology sort is stable
   auto& ops = Get<const std::vector<OpDesc*>>(kAllOpDescs);
-  for (auto* op_desc : ops) {
-    auto outputs = op_desc->Outputs();
-    for (auto& o_it : outputs) {
-      for (auto& v : o_it.second) {  // values
-        vars[v] = order;
+
+  // TODO(liangdun): add a config in build strategy
+  bool use_topological_sort = true;
+  if (use_topological_sort) {
+    int op_size = ops.size();
+    std::vector<std::vector<int>> op_deps(op_size);
+    std::vector<int> deps(op_size, 0);
+    std::unordered_map<std::string, int> var_from_op;
+    std::vector<int> queue;
+
+    for (int i = 0; i < op_size; i++) {
+      auto op_desc = ops[i];
+      auto inputs = op_desc->Inputs();
+      auto outputs = op_desc->Outputs();
+      for (auto& it : inputs) {
+        for (auto& v : it.second) {
+          if (var_from_op.find(v) == var_from_op.end()) continue;
+          op_deps[var_from_op[v]].push_back(i);
+          deps[i]++;
+        }
       }
+
+      for (auto& it : outputs) {
+        for (auto& v : it.second) {
+          var_from_op[v] = i;
+        }
+      }
+
+      if (deps[i] == 0) queue.push_back(i);
     }
-    order++;
+
+    int start = 0;
+    while (start < static_cast<int>(queue.size())) {
+      int op_id = queue[start];
+      auto op_desc = ops[op_id];
+      auto outputs = op_desc->Outputs();
+      for (auto& o_it : outputs) {
+        for (auto& v : o_it.second) {
+          vars[v] = start;
+          VLOG(100) << "assign topological order: " << v << " = " << start;
+        }
+      }
+      for (auto next_op : op_deps[op_id]) {
+        deps[next_op]--;
+        VLOG(100) << "remove dep: " << op_desc->Type() << "(" << start
+                  << ") ->  " << ops[next_op]->Type() << "(" << next_op
+                  << ") deps:" << deps[next_op];
+        if (deps[next_op] == 0) queue.push_back(next_op);
+      }
+      start++;
+    }
+    PADDLE_ENFORCE(queue.size() == op_size);
+  } else {
+    int order = 0;
+    for (auto* op_desc : ops) {
+      auto outputs = op_desc->Outputs();
+      for (auto& o_it : outputs) {
+        for (auto& v : o_it.second) {  // values
+          vars[v] = order;
+        }
+      }
+      order++;
+    }
   }
 
   std::vector<OpHandleBase*> dist_ops;
