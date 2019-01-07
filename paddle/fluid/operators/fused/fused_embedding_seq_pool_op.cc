db8eb9b6888d7d76ec0f5e5bc07c6388dd633840
minqiyang <minqiyang@baidu.com>
2019-01-07 12:55:32 +0800

    Polish code
    test=develop

--- paddle/fluid/operators/fused/fused_embedding_seq_pool_op.cc
+++ paddle/fluid/operators/fused/fused_embedding_seq_pool_op.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/operators/fused_embedding_seq_pool_op.h"
+#include "paddle/fluid/operators/fused/fused_embedding_seq_pool_op.h"
 #include "paddle/fluid/framework/var_type_inference.h"
 
 namespace paddle {
