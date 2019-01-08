c4040847f828f140b3bb479b69c1dffb9340c6b1
minqiyang <minqiyang@baidu.com>
2019-01-08 17:49:34 +0800

    Fix Eigen macro when using GPU
    test=release/1.2

--- paddle/fluid/operators/dropout_op.cu
+++ paddle/fluid/operators/dropout_op.cu
@@ -11,8 +11,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-
-#define EIGEN_USE_GPU
 #include <thrust/device_ptr.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/random.h>
