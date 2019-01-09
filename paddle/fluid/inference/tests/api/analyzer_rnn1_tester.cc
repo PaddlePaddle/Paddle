45dbe3305c743072faf92d30fff8eb7b4d54046d
tensor-tang <tangjian03@baidu.com>
2019-01-09 13:00:49 +0000

    fix zerocopy size

--- paddle/fluid/inference/tests/api/analyzer_rnn1_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_rnn1_tester.cc
@@ -351,10 +351,10 @@ TEST(Analyzer_rnn1, ZeroCopy) {
   ASSERT_TRUE(native_predictor->Run(native_inputs.front(), &native_outputs));
   LOG(INFO) << "native output " << DescribeTensor(native_outputs.front());
 
-  int output_size{0};
+  int output_size{0};  // this is the number of elements not memory size
   auto *zero_copy_data = output_tensor->data<float>(&place, &output_size);
   auto *native_data = static_cast<float *>(native_outputs.front().data.data());
-  for (size_t i = 0; i < output_size / sizeof(float); i++) {
+  for (int i = 0; i < output_size; i++) {
     EXPECT_NEAR(zero_copy_data[i], native_data[i], 1e-3);
   }
 }
