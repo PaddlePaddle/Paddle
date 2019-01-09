3a3a28814b3c56ceb29790fa746b4798a34c347c
flame <fuchang1991@gmail.com>
2019-01-09 18:59:22 +0800

    remove unused header file

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 #include <Python.h>
 #include <algorithm>
-#include <cassert>
 #include <map>
 #include <memory>
 #include <mutex>  // NOLINT // for call_once
