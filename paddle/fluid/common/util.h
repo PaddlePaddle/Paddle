5e501fc300aeabc9b9d373800c605dfad98aa6c3
Qiao Longfei <qiaolongfei@baidu.com>
2019-01-09 10:11:03 +0800

    add common dir to paddle fluid

---
+++ paddle/fluid/common/util.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#include <functional>
+#include<sys/time.h>
+
+namespace paddle {
+namespace common {
+
+inline double GetCurrentUS() {
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  return 1e+6 * time.tv_sec + time.tv_usec;
+}
+
+inline size_t thread_id() {
+  return std::hash<std::thread::id>{}(std::this_thread::get_id());
+}
+
+}  // namespace framework
+}  // namespace paddle
