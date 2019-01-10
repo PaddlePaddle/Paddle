f968df3802309994f621dd021bb54d5345ed7b48
peizhilin <wopeizl@163.com>
2019-01-10 22:49:10 +0800

    test=develop

--- paddle/fluid/framework/operator.h
+++ paddle/fluid/framework/operator.h
@@ -160,9 +160,6 @@ class OperatorBase {
                                  const platform::Place& place,
                                  const RuntimeContext& ctx) const {}
 
-  virtual void PreRun(const Scope& scope, const platform::Place& place);
-  virtual void PostRun(const Scope& scope, const platform::Place& place);
-
  protected:
   std::string type_;
   // NOTE: in case of OpGrad, inputs_ contains:
