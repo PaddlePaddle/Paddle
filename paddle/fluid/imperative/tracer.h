60b24f5fcd1fefb0037d0248fc07448eaa7e5983
Xin Pan <panxin.grad@gmail.com>
2019-01-14 10:55:56 +0800

    code style
    test=develop

--- paddle/fluid/imperative/tracer.h
+++ paddle/fluid/imperative/tracer.h
@@ -48,7 +48,6 @@ class Tracer {
   std::vector<VarBase*> PyTrace(OpBase* op, const std::vector<VarBase*>& inputs,
                                 bool stop_gradient = false);
 
-
  private:
   framework::BlockDesc* root_block_;
 };
