958dcdf6666ab013f593211a9e4db18a6823b88a
Xin Pan <panxin.grad@gmail.com>
2019-01-16 21:11:55 +0800

    fix
    test=develop

--- python/paddle/fluid/layer_helper.py
+++ python/paddle/fluid/layer_helper.py
@@ -405,7 +405,8 @@ class LayerHelper(object):
         """
         size = list(input_var.shape[dim_start:dim_end])
         bias_attr = self.bias_attr
-        assert bias_attr is not None
+        if not bias_attr:
+            return input_var
 
         b = self.create_parameter(
             attr=bias_attr, shape=size, dtype=input_var.dtype, is_bias=True)
