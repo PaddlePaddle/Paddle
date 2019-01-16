0e8cf9f0288697e4ba046ce0e72bb5cfad939699
superjomn <yanchunwei@outlook.com>
2019-01-16 19:58:57 +0800

    update

--- paddle/fluid/inference/anakin/engine.h
+++ paddle/fluid/inference/anakin/engine.h
@@ -4,7 +4,6 @@
 #include <cstddef>
 #include <memory>
 #include <vector>
-#include "paddle/fluid/framework/ir/node.h"
 
 namespace paddle {
 namespace inference {
@@ -14,103 +13,65 @@ enum DataType { kUnk = -1, kFloat32, kFloat64, kInt32 };
 enum Place { kCpu = 0, kGpu };
 
 using shape_t = std::vector<int>;
-using attr_t = boost::variant<int, bool>;
+using attr_t = boost::variant<int, bool, float>;
 
 struct Tensor {
-  DataType dtype{DataType::kUnk};
-  void* buffer{nullptr};
-  size_t buffer_size{0};
-  shape_t shape;
-  Place place{Place::kCpu};
-  std::string name;  // for specifying inputs.
+  // Resize the shape of this tensor if needed, used by setting input or output.
+  void Resize(const shape_t& shape);
+
+  void SetName(const std::string& name);
+
+  const std::string& name() const;
+
+  // Get the tensor's data lazily, will re-malloc if the memory required (shape
+  // * dtype) is changed.
+  template <typename T>
+  T* mutable_data(Place place);
+
+  // Get the tensor's data.
+  template <typename T>
+  T* data(Place* place, size_t* size) const;
+
+  // Get the datatype of this tensor.
+  DataType dtype() const;
+
+  const shape_t& shape() const;
+
+ private:
+  std::string name_;
+  shape_t shape_;
+  void* data_;
 };
 
 class AnakinEngine {
  public:
-  // ir::Node also has OpDesc.
-  static bool IsOpSupported(framework::ir::Node* node);
+  using attrs_t = std::map<std::string, attr_t>;
+  // Is this op is supported by the engine.
+  // TODO is the inputs and outputs of this op needed?
+  static bool IsOpSupported(const std::string& op_type, const attrs_t& attrs);
 
-  // Topology releated.
-  void DeclareInput(const std::string& id, DataType dtype,
-                    const shape_t& shape);
-  void DeclareOutput(const std::string& id);
+  // @param tensor the tensor with Resize callback.
+  void DeclareInput(const std::string& id, const Tensor* tensor,
+                    const attrs_t& attrs);
+  void DeclareOutput(const std::string& id, const Tensor* tensor,
+                     const attrs_t& attrs);
 
   void AddOp(const std::string& op_type, const std::vector<std::string>& inputs,
-             const std::vector<std::string>& outputs,
-             const std::vector<attr_t>& attrs);
-  void AddVar(const std::string& id, DataType dtype, const shape_t& shape);
-  Tensor* AddWeight(const std::string& id, DataType dtype,
-                    const shape_t& shape);
+             const std::vector<std::string>& outputs, const attrs_t& attrs);
 
+  void AddVar(const std::string& id, DataType dtype, const shape_t& shape);
+
+  Tensor* AddWeight(const std::string& id, const Tensor& v);
+
+  std::unique_ptr<AnakinEngine> Clone();
+
+  // Finish creating the network.
   void FreezeNetwork();
 
-  void Execute(const std::vector<Tensor>& inputs,
-               const std::vector<Tensor>* outputs, int batch_size = 0);
-
-  // Runtime
-  bool SetInput(const std::string& id, const Tensor& data);
-  bool GetOutput(const std::string& id, Tensor* data);
-  bool GetVar(const std::string& id, Tensor* data);
+  // Execute the network, and the engine will read the declared inputs and write
+  // result to the declared outputs.
+  void Execute(int batch_size = 0);
 
  private:
   void* raw_engine_{nullptr};
 };
-
-// Pseudo codes here!
-TEST(test, main) {
-  // in anakin_subgraph_analysis_pass
-  // ir::Graph graph;
-
-  declare subgraph;
-  for (auto* node : graph) {
-    if (AnakinEngine::IsOpSupported(node)) {
-      // add to subgraph
-    }
-  }
-
-  if (IsSubgraphValid(subgraph)) {
-    // build the engine
-    AnakinEngine engine;
-    std::unordered_set<std::string> collected_vars;
-
-    auto inputs = CollectSubgraphInputs(subgraph);
-    auto outputs = CollectSubgraphOutputs(subgraph);
-
-    // Declare subgraph inputs and outputs
-    for (auto x : inputs) {
-      engine.DeclareInput(x->name, x->dtype, x->shape);
-    }
-    for (auto x : outputs) {
-      engine.DeclareOutput(x->name);
-    }
-
-    // Build the subgraph network.
-    for (auto* node : subgraph) {
-      // create vars and weights.
-      for (auto* input : node->inputs) {
-        if (collected_vars.count(input.name)) continue;
-        if (input.IsWeight()) {
-          engine.AddWeight(node.name, input.dtype, input.shape);
-        } else {
-          engine.AddVar(node.name, input.dtype, input.shape);
-        }
-      }
-
-      for (auto* output : node->output) {
-        if (collected_vars.count(input.name)) continue;
-        engine.AddVar(node.name, input.dtype, input.shape);
-      }
-
-      engine.AddOp(node->op_type, node->inputs, node->outputs, node->attrs);
-    }
-
-    engine.FreezeNetwork();
-
-    // Run inference
-    engine.Execute(...);
-  }
-}
-
-}  // namespace tensorrt
-}  // namespace inference
-}  // namespace paddle
