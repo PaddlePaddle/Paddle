4a4a21629681daaf2e7cd02a37265a8d7791879b
Superjomn <yanchunwei@outlook.com>
2019-01-16 07:25:43 +0000

    update
    test=develop

--- paddle/fluid/inference/api/api_impl.cc
+++ paddle/fluid/inference/api/api_impl.cc
@@ -161,10 +161,21 @@ bool NativePaddlePredictor::Run(const std::vector<PaddleTensor> &inputs,
 }
 
 std::unique_ptr<PaddlePredictor> NativePaddlePredictor::Clone() {
-  VLOG(3) << "Predictor::clone";
-  // Hot fix the bug that result diff in multi-thread.
+	std::unique_ptr<PaddlePredictor> cls(new NativePaddlePredictor(config_));
+	// Hot fix the bug that result diff in multi-thread.
   // TODO(Superjomn) re-implement a real clone here.
-  return CreatePaddlePredictor(config_);
+	if (!dynamic_cast<NativePaddlePredictor *>(cls.get())->Init(nullptr)) {
+		LOG(ERROR) << "fail to call Init";	
+		return nullptr;	
+  }
+
+#ifdef __clang__	
+  // fix clang compile error	
+  return cls;	
+#else	
+  // fix manylinux compile error.	
+  return std::move(cls);	
+#endif
 }
 
 bool NativePaddlePredictor::SetFeed(const std::vector<PaddleTensor> &inputs,
