f201a48ac19ba92531fc09a91b89c8ada5c9c10b
Zhang, Guoming <guoming.zhang@intel.com>
2019-01-22 13:57:23 +0800

    Fix the typo and format the License header.
    test=develop

--- python/paddle/fluid/contrib/int8_inference/utility.py
+++ python/paddle/fluid/contrib/int8_inference/utility.py
@@ -1,16 +1,16 @@
-#   copyright (c) 2018 paddlepaddle authors. all rights reserved.
+#   Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
 #
-# licensed under the apache license, version 2.0 (the "license");
-# you may not use this file except in compliance with the license.
-# you may obtain a copy of the license at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/license-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# unless required by applicable law or agreed to in writing, software
-# distributed under the license is distributed on an "as is" basis,
-# without warranties or conditions of any kind, either express or implied.
-# see the license for the specific language governing permissions and
-# limitations under the license.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import paddle.fluid.core as core
 import numpy as np
 import math
@@ -75,7 +75,7 @@ class Calibrator(object):
         """
         Check the specified variable was generated from Relu layer or not.
         If the variable was the output of one of the pool2d/reshape/concat
-        /transpose, we keep trace the ancesotr of this variable;
+        /transpose, we keep trace the ancestor of this variable;
         If the variable was the output the conv op, we check it's has_relu
         attr;
         Otherwise, we return the Calibrator.s8 as default value.
