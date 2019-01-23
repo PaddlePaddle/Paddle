fd5eff810eca34a12e1b20b249b248231cce66d5
typhoonzero <typhoonzero1986@gmail.com>
2019-01-23 18:54:16 +0800

    fix merge build test=develop

---
+++ paddle/fluid/operators/distributed/handlers/get_no_barrier_handler.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <string>
+#include "paddle/fluid/framework/scope.h"
+#include "paddle/fluid/operators/distributed/request.h"
+#include "paddle/fluid/operators/distributed/request_handler.h"
+
+namespace paddle {
+namespace operators {
+namespace distributed {
+
+using Scope = paddle::framework::Scope;
+
+class GetNoBarrierHandler final : public RequestHandler {
+ public:
+  bool Handle(RPCRequest* request) override;
+  framework::Variable* GetOrCreateRequestVar(const std::string& varname,
+                                             RPCRequest* request) override;
+};
+
+}  // namespace distributed
+}  // namespace operators
+}  // namespace paddle
