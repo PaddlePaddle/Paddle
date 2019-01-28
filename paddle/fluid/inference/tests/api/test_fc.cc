698e748fa22c6e2f7a99165fee05e7c6c91bc1ec
superjomn <yanchunwei@outlook.com>
2019-01-28 22:06:39 +0800

    update

--- paddle/fluid/inference/tests/api/test_fc.cc
+++ paddle/fluid/inference/tests/api/test_fc.cc
@@ -4,9 +4,12 @@
 
 using namespace paddle;
 
+DEFINE_string(model, "/home/chunwei/project2/models/fc/fluid_checkpoint", "");
+DEFINE_int32(batch_size, 1, "");
+
 TEST(test, main) {
   contrib::AnalysisConfig config;
-  config.SetModel("/home/chunwei/project2/models/fc/fluid_checkpoint");
+  config.SetModel(FLAGS_model);
   config.SwitchIrOptim(false);
   config.SwitchIrDebug(true);
   config.pass_builder()->TurnOnDebug();
@@ -17,9 +20,8 @@ TEST(test, main) {
   inputs.resize(1);
 
   auto& input = inputs.front();
-  int batch_size = 70;
-  input.shape.assign({batch_size, 210});
-  input.data.Resize(batch_size * 210 * sizeof(float));
+  input.shape.assign({FLAGS_batch_size, 210});
+  input.data.Resize(FLAGS_batch_size * 210 * sizeof(float));
   auto* data = static_cast<float*>(input.data.data());
   for (int i = 0; i < 210; i++) {
     data[i] = rand() / RAND_MAX;
@@ -32,5 +34,5 @@ TEST(test, main) {
   for (int i = 0; i < 1000; i++) {
     ASSERT_TRUE(predictor->Run(inputs, &outputs));
   }
-  LOG(INFO) << "latency " << timer.toc() / 1000 / batch_size;
+  LOG(INFO) << "latency " << timer.toc() / 1000 / FLAGS_batch_size;
 }
