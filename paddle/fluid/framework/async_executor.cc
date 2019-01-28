4e169c96d0c7a64767b406a4041a5a22d6b6c5ab
dongdaxiang <dongdaxiang@baidu.com>
2019-01-28 17:46:39 +0800

    make -DWITH_PSLIB=ON compilable

--- paddle/fluid/framework/async_executor.cc
+++ paddle/fluid/framework/async_executor.cc
@@ -26,6 +26,7 @@ limitations under the License. */
 #include "paddle/fluid/framework/lod_tensor_array.h"
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/framework/reader.h"
+#include "paddle/fluid/framework/trainer_desc.pb.h"
 #include "paddle/fluid/inference/io.h"
 #include "paddle/fluid/platform/place.h"
 #include "paddle/fluid/pybind/pybind.h"
@@ -56,52 +57,9 @@ void AsyncExecutor::GatherServers(const std::vector<uint64_t>& host_sign_list,
   fleet_ptr_->GatherServers(host_sign_list, node_num);
 }
 
-void AsyncExecutor::InitModel() {
-  for (auto table_id : _param_config.dense_table_id) {
-    std::vector<paddle::ps::Region> regions;
-    for (auto& t : _param_config.dense_variable_name[table_id]) {
-      Variable* var = root_scope_->FindVar(t);
-      CHECK(var != nullptr) << "var[" << t << "] not found";
-      LoDTensor* tensor = var->GetMutable<LoDTensor>();
+void AsyncExecutor::InitModel() {}
 
-      float* g = tensor->data<float>();
-      CHECK(g != nullptr) << "var[" << t << "] value not initialized";
-
-      float init_range = 0.2;
-      int rown = tensor->dims()[0];
-      init_range /= sqrt(rown);
-
-      std::normal_distribution<float> ndistr(0.0, 1.0);
-      for (auto i = 0u; i < tensor->numel(); ++i) {
-        g[i] = ndistr(local_random_engine()) * init_range;
-      }
-
-      paddle::ps::Region reg(g, tensor->numel());
-      regions.emplace_back(std::move(reg));
-    }
-
-    auto push_status = _pslib_ptr->_worker_ptr->push_dense_param(
-        regions.data(), regions.size(), table_id);
-    push_status.wait();
-    auto status = push_status.get();
-    if (status != 0) {
-      LOG(FATAL) << "push dense param failed, status[" << status << "]";
-      exit(-1);
-    }
-  }
-}
-
-void AsyncExecutor::SaveModel(const std::string& path) {
-  auto ret = _pslib_ptr->_worker_ptr->flush();
-  ret.wait();
-  ret = _pslib_ptr->_worker_ptr->save(path, 0);
-  ret.wait();
-  int32_t feasign_cnt = ret.get();
-  if (feasign_cnt == -1) {  // (colourful-tree) TODO should be feasign_cnt < 0
-    LOG(FATAL) << "save model failed";
-    exit(-1);
-  }
-}
+void AsyncExecutor::SaveModel(const std::string& path) {}
 
 void AsyncExecutor::RunFromFile(const ProgramDesc& main_program,
 <<<<<<< HEAD
