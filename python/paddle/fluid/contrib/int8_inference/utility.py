6e01a8d47ca8061880a6e96aa7984294e0352684
Haihao Shen <haihao.shen@intel.com>
2019-01-29 16:42:27 +0800

    Enhance the packing; test=develop

--- python/paddle/fluid/contrib/int8_inference/utility.py
+++ python/paddle/fluid/contrib/int8_inference/utility.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import paddle.fluid.core as core
 import numpy as np
 import math
