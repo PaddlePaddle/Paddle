12609f4ee67e9b417a9db171e75c4e5b3b651e8c
superjomn <yanchunwei@outlook.com>
2019-01-30 15:26:00 +0800

    update
    test=develop

--- paddle/fluid/inference/api/api_tester.cc
+++ paddle/fluid/inference/api/api_tester.cc
@@ -61,7 +61,10 @@ TEST(paddle_inference_api, demo) {
   predictor->Run({}, &outputs);
 }
 
-TEST(PaddleBuf, zero_length) { PaddleBuf buf(0); }
+TEST(PaddleBuf, zero_length) {
+  PaddleBuf buf(0);
+  ASSERT_EQ(buf.length(), 0)
+};
 
 TEST(PaddleBuf, Resize) {
   PaddleBuf buf;
