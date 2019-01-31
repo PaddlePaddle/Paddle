ed9102611e1b126638f1ad22c6b4460e9d764f46
baojun-nervana <baojun.liu@intel.com>
2019-01-30 21:32:48 -0800

    fixed name type test=develop

--- paddle/fluid/operators/ngraph/ngraph_ops.h
+++ paddle/fluid/operators/ngraph/ngraph_ops.h
@@ -22,7 +22,7 @@ limitations under the License. */
 #pragma once
 
 #include "ops/accuracy_op.h"
-#include "ops/binary_unnary_op.h"
+#include "ops/binary_unary_op.h"
 #include "ops/conv2d_op.h"
 #include "ops/elementwise_add_op.h"
 #include "ops/fill_constant_op.h"
