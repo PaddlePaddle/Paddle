dee6f85f35a1ed81ea2d19bad31f35b5269b1235
Yan Xu <yanxu05@baidu.com>
2019-02-12 15:48:06 +0800

    add launch mp distributed job py module test=develop (#15620)
    * add launch mp distributed mode module test=develop
    
    * delete unused file test=develop
    
    * refine usage test=develop
    
    * refine usage test=develop
    
    * move distributed package test=develop
    
    * add to whl package test=develop

--- python/paddle/distributed/__init__.py
+++ python/paddle/distributed/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,19 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-try:
-    from paddle.version import full_version as __version__
-    from paddle.version import commit as __git_commit__
-
-except ImportError:
-    import sys
-    sys.stderr.write('''Warning with import paddle: you should not
-     import paddle from the source directory; please install paddlepaddle*.whl firstly.'''
-                     )
-
-import paddle.reader
-import paddle.dataset
-import paddle.batch
-import paddle.compat
-import paddle.distributed
-batch = batch.batch
