292be63333c5490ee7ad1c06082fe2252b30a0cc
liuhongyu <phliuhongyu@126.com>
2019-02-14 16:25:29 +0800

    set lstm lstmp unsed pointer to nullptr; test=develop

--- paddle/fluid/operators/lstm_op.h
+++ paddle/fluid/operators/lstm_op.h
@@ -311,8 +311,8 @@ class LSTMGradKernel : public framework::OpKernel<T> {
         lstm_grad.prev_state_grad = c0_g ? ordered_c0_g.data<T>() : nullptr;
       }
 
-      // lstm_value.output_value not used in bp, set to null
-      // lstm_grad.state_active_grad not used in bp, set to null
+      // lstm_value.output_value not used in bp, set to nullptr
+      // lstm_grad.state_active_grad not used in bp, set to nullptr
       lstm_value.output_value = nullptr;
       lstm_grad.state_active_grad = nullptr;
       int cur_batch_size = bend - bstart;
