ed71df2b016ffe26a4e0971786457e1d26a52f3b
chengduozh <zhaochengduo@baidu.com>
2019-02-25 22:18:59 +0800

    follow comment test=develop

--- paddle/fluid/operators/alloc_continuous_space_op.cc
+++ paddle/fluid/operators/alloc_continuous_space_op.cc
@@ -22,7 +22,7 @@ namespace paddle {
 namespace operators {
 
 static framework::proto::VarType::Type kDefaultDtype =
-    static_cast<framework::proto::VarType::Type>(0);
+    framework::proto::VarType::Type::VarType_Type_BOOL;
 
 template <typename DeviceContext, typename T>
 class AllocContinuousSpaceKernel : public framework::OpKernel<T> {
@@ -62,14 +62,14 @@ class AllocContinuousSpaceKernel : public framework::OpKernel<T> {
 
     auto &dev_ctx = context.template device_context<DeviceContext>();
 
-    // Get mem_size and dtype
-    size_t mem_size = 0;
+    // Get numel and dtype
+    size_t numel = 0;
     auto dtype = kDefaultDtype;
-    GetMemSizeAndDtype(in_tensors, in_var_names, &mem_size, &dtype);
+    GetMemSizeAndDtype(in_tensors, in_var_names, &numel, &dtype);
 
     // Alloc the continuous space
     auto fused_tensor = context.Output<framework::LoDTensor>("FusedOutput");
-    fused_tensor->Resize(framework::make_ddim({static_cast<int64_t>(mem_size)}))
+    fused_tensor->Resize(framework::make_ddim({static_cast<int64_t>(numel)}))
         .mutable_data(context.GetPlace(), dtype);
 
     // Init the continuous space
@@ -106,10 +106,10 @@ class AllocContinuousSpaceKernel : public framework::OpKernel<T> {
 
   void GetMemSizeAndDtype(
       const std::vector<const framework::LoDTensor *> &lod_tensors,
-      const std::vector<std::string> var_names, size_t *mem_size,
+      const std::vector<std::string> var_names, size_t *numel,
       framework::proto::VarType::Type *dtype) const {
     PADDLE_ENFORCE_EQ(lod_tensors.size(), var_names.size());
-    *mem_size = 0;
+    *numel = 0;
     for (size_t i = 0; i < var_names.size(); ++i) {
       PADDLE_ENFORCE(lod_tensors[i]->IsInitialized(), "%s is not initialized.",
                      var_names[i]);
@@ -126,7 +126,7 @@ class AllocContinuousSpaceKernel : public framework::OpKernel<T> {
       PADDLE_ENFORCE_GT(size, 0);
       VLOG(10) << "alloc_space_for_vars: input(" << var_names[i] << ") ,dim:("
                << lod_tensors[i]->dims() << ")";
-      *mem_size += size;
+      *numel += size;
     }
   }
 };
@@ -141,14 +141,48 @@ class AllocContinuousSpaceOp : public framework::OperatorWithKernel {
 class AllocContinuousSpaceOpMaker : public framework::OpProtoAndCheckerMaker {
  public:
   void Make() override {
-    AddInput("Input", "A set of variables.").AsDuplicable();
-    AddOutput("Output", "A set of variables.").AsDuplicable();
-    AddOutput("FusedOutput", "");
-    AddAttr<bool>("copy_data", ".").SetDefault(false);
-    AddAttr<bool>("set_constant", ".").SetDefault(false);
-    AddAttr<float>("constant", ".").SetDefault(0.0);
-    AddAttr<bool>("check_name", ".").SetDefault(false);
+    AddInput("Input",
+             "(vector<LoDTensor>) The input tensors of"
+             " alloc_continuous_space operator.")
+        .AsDuplicable();
+    AddOutput("Output",
+              "(vector<LoDTensor>) The output "
+              "tensors of alloc_continuous_space operator. And the address "
+              "of output tensors are continuous, they are sliced from the "
+              "tensor of FusedOutput.")
+        .AsDuplicable();
+    AddOutput("FusedOutput",
+              "(LoDTensor) The output tensor "
+              "of alloc_continuous_space operator. And the tensors of"
+              " Output is sliced from the tensor of FusedOutput.");
+    AddAttr<bool>("copy_data", "Whether to copy the Input value to Output.")
+        .SetDefault(false);
+    AddAttr<bool>("set_constant",
+                  "Whether to set the Output with a constant value.")
+        .SetDefault(false);
+    AddAttr<float>("constant",
+                   "If set_constant is true, the constant value will be used "
+                   "to set the Output.")
+        .SetDefault(0.0);
+    AddAttr<bool>("check_name",
+                  "Whether to check the name of Input and Output to ensure "
+                  "they are the same separately.")
+        .SetDefault(false);
     AddComment(R"DOC(
+AllocContinuousSpace Operator.
+
+alloc_continuous_space is used to make the address of Output
+continuous according to the Input. This Op will alloc a big tensor
+according to the tensors of Input, the dtype is the same with those input tensors,
+the size is the sum of those input tensors' numel, and the dim of the big
+tensor is {sum(numel)}. And the big tensor is stored in FusedOutput.
+The tensors of Output are sliced from the tensor of FusedOutput.
+Note that, the dtype of Input should be the same, and the dim of Input
+and Output should equal.
+The tensors of Input and Output could be the same or different. And
+alloc_continuous_space allows copying the value of Input to Output, or
+setting the Output with a constant value.
+
 )DOC");
   }
 };
