597dc65e76669e381cf4c52dfeeb671d5eecc4e6
sneaxiy <sneaxiy@126.com>
2019-03-05 10:11:10 +0000

    enhance gc test=develop

--- paddle/fluid/framework/details/computation_op_handle.h
+++ paddle/fluid/framework/details/computation_op_handle.h
@@ -31,6 +31,8 @@ class ComputationOpHandle : public OpHandleBase {
   ComputationOpHandle(ir::Node *node, Scope *scope, platform::Place place,
                       size_t scope_idx);
 
+  OperatorBase *GetOp() { return op_.get(); }
+
   std::string Name() const override;
 
   const Scope *GetScope() const { return scope_; }
