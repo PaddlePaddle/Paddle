2e3b1a8196f3912937e00d21b6b3fd5f82400ba4
tensor-tang <tangjian03@baidu.com>
2019-03-06 09:03:49 +0000

    follow comments
    test=develop

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -142,7 +142,7 @@ def create_global_var(shape,
 def cast(x, dtype):
     """
     This layer takes in the Variable :attr:`x` with :attr:`x.dtype` and casts
-    it to the output with :attr:`dtype`. It's not meaningful if the output
+    it to the output with :attr:`dtype`. It's meaningless if the output
     dtype equals the input dtype, but it's fine if you do so.
 
     Args:
