27db1b84301c3300e9a5dee9e319f949bc1b527c
superjomn <yanchunwei@outlook.com>
2019-03-08 11:18:01 +0800

    update
    test=develop

--- paddle/fluid/framework/ir/is_test_pass.cc
+++ paddle/fluid/framework/ir/is_test_pass.cc
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/framework/ir/is_test_pass.h"
+#include <memory>
 #include <string>
 #include <utility>
 
--- paddle/fluid/inference/api/paddle_api.h
+++ paddle/fluid/inference/api/paddle_api.h
@@ -23,6 +23,7 @@
  */
 
 #include <algorithm>
+#include <numeric>
 #include <cassert>
 #include <memory>
 #include <string>
