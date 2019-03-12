d171651044aa70ab5604e318f999de0762a911c4
xujiaqi01 <xujiaqi01@baidu.com>
2019-03-12 14:26:44 +0800

    store memory data in Dataset && fix bug

--- python/paddle/fluid/__init__.py
+++ python/paddle/fluid/__init__.py
@@ -30,7 +30,7 @@ from .dataset import *
 from . import async_executor
 from .async_executor import *
 
-from . import trainer
+from . import trainer_desc
 from . import inferencer
 
 from . import io
@@ -67,7 +67,7 @@ from . import install_check
 Tensor = LoDTensor
 
 __all__ = framework.__all__ + executor.__all__ + \
-    trainer.__all__ + inferencer.__all__ + transpiler.__all__ + \
+    trainer_desc.__all__ + inferencer.__all__ + transpiler.__all__ + \
     parallel_executor.__all__ + lod_tensor.__all__ + \
     data_feed_desc.__all__ + async_executor.__all__ + compiler.__all__  + [
         'io',
