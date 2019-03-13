581347b8cac7263efd190539413c46440f126d08
dongdaxiang <dongdaxiang@baidu.com>
2019-03-13 15:40:11 +0800

    add incubate for unified API

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -1293,6 +1293,7 @@ All parameter, weight, gradient are variables in Paddle.
 
   BindRecordIOWriter(&m);
   BindAsyncExecutor(&m);
+  BindFleetWrapper(&m);
   BindGraph(&m);
   BindNode(&m);
   BindInferenceApi(&m);
