e6677bef228c8b09868b3ac5a6ba6a5189cac52e
AIFollowers <aifollowers@163.com>
2019-03-15 15:00:27 +0800

    20190315

--- python/paddle/utils/model_stat.py
+++ python/paddle/utils/model_stat.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -185,3 +185,4 @@ def _print_summary(summary_table, total):
     print(summary_table)
     print('Total PARAMs: {}({:.4f}G)'.format(sum(parmas), sum(parmas)/(10**9)))
     print('Total FLOPs: {}({:.2f}G)'.format(sum(flops), sum(flops) / 10**9))
+    
