5f77d47cb6f37b798016b465c00c4b5b0825d510
nhzlx <nhzlx.dragon@gmail.com>
2019-03-19 04:57:47 +0000

    hard code: Change cudnn workspace from 4G to 64Mb.

--- paddle/fluid/operators/conv_fusion_op.cu.cc
+++ paddle/fluid/operators/conv_fusion_op.cu.cc
@@ -100,6 +100,7 @@ class CUDNNConvFusionOpKernel : public framework::OpKernel<T> {
       int64_t max_user_size =
           std::max(static_cast<int64_t>(FLAGS_conv_workspace_size_limit),
                    user_workspace_size);
+      max_user_size = 64;
       workspace_size_limit = max_user_size * 1024 * 1024;
     }
 
