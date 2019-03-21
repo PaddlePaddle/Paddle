bd81e1fc32659a816700286e007843d38e6bdbfa
Wojciech Uss <wojciech.uss@intel.com>
2019-03-21 03:09:03 +0100

    Add enabling quantization (#16326)
    * Add enabling quantization
    
    test=develop
    
    * remove unused (here) function

--- paddle/fluid/framework/ir/mkldnn/mkldnn_placement_pass.h
+++ paddle/fluid/framework/ir/mkldnn/mkldnn_placement_pass.h
@@ -21,6 +21,9 @@ namespace paddle {
 namespace framework {
 namespace ir {
 
+/*
+ * Specifies which operators should use MKLDNN.
+ */
 class MKLDNNPlacementPass : public Pass {
  protected:
   std::unique_ptr<ir::Graph> ApplyImpl(
