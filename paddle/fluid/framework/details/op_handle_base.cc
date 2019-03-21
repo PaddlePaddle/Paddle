b29a8ef1f286b128afe0c6e9b97378c77d2457b1
chengduozh <zhaochengduo@baidu.com>
2019-03-21 15:39:40 +0800

    fix OpHandle::Run test=develop

--- paddle/fluid/framework/details/op_handle_base.cc
+++ paddle/fluid/framework/details/op_handle_base.cc
@@ -42,7 +42,7 @@ OpHandleBase::~OpHandleBase() {
 
 void OpHandleBase::Run(bool use_cuda) {
 #ifdef PADDLE_WITH_CUDA
-  if (events_.empty() && use_cuda) {
+  if (events_.empty() && use_cuda && dev_ctxes_.size() > 0) {
     for (auto &p : dev_ctxes_) {
       int dev_id = boost::get<platform::CUDAPlace>(p.first).device;
       PADDLE_ENFORCE(cudaSetDevice(dev_id));
