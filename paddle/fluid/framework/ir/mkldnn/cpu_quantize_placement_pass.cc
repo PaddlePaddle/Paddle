529e07cc12da75eb4f4d2979219862fa4ca4da7a
Wojciech Uss <wojciech.uss@intel.com>
2019-03-25 03:54:52 +0100

    Move cpu_quantize_* passes into mkldnn subfolder
    test=develop

--- paddle/fluid/framework/ir/mkldnn/cpu_quantize_placement_pass.cc
+++ paddle/fluid/framework/ir/mkldnn/cpu_quantize_placement_pass.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/framework/ir/cpu_quantize_placement_pass.h"
+#include "paddle/fluid/framework/ir/mkldnn/cpu_quantize_placement_pass.h"
 #include <string>
 #include <unordered_set>
 
