707fd71ff0511e7785608a877933c51b385fd5eb
lujun <lujun315023@126.com>
2019-03-25 18:58:13 +0800

    fix mix input type error, test=develop

--- paddle/fluid/operators/save_combine_op.cc
+++ paddle/fluid/operators/save_combine_op.cc
@@ -19,6 +19,8 @@ limitations under the License. */
 namespace paddle {
 namespace operators {
 
+using Tensor = framework::Tensor;
+
 class SaveCombineOp : public framework::OperatorWithKernel {
  public:
   using framework::OperatorWithKernel::OperatorWithKernel;
