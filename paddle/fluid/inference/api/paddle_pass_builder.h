6a56d8d5647b49f5a6b0b11fd8d9e25fffedcf26
Wojciech Uss <wojciech.uss@intel.com>
2019-03-25 17:37:12 +0100

    Fix order of mkldnn passes
    test=develop

--- paddle/fluid/inference/api/paddle_pass_builder.h
+++ paddle/fluid/inference/api/paddle_pass_builder.h
@@ -115,11 +115,10 @@ class CpuPassStrategy : public PassStrategy {
       passes_.insert(passes_.begin(), "mkldnn_placement_pass");
 
       for (auto &pass : std::vector<std::string>(
-               {"depthwise_conv_mkldnn_pass",    //
-                "conv_bias_mkldnn_fuse_pass",    //
-                "conv3d_bias_mkldnn_fuse_pass",  //
-                "conv_relu_mkldnn_fuse_pass",    //
-                "conv_elementwise_add_mkldnn_fuse_pass"})) {
+               {"depthwise_conv_mkldnn_pass", "conv_bias_mkldnn_fuse_pass",
+                "conv3d_bias_mkldnn_fuse_pass",
+                "conv_elementwise_add_mkldnn_fuse_pass",
+                "conv_relu_mkldnn_fuse_pass"})) {
         passes_.push_back(pass);
       }
     }
