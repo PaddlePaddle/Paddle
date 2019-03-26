3dd21bc6e5dc04ade339fd7818434108a6cb3588
xjqbest <173596896@qq.com>
2019-03-26 15:48:42 +0800

    fix code style of test_dataset.py test=develop

--- python/paddle/fluid/tests/unittests/test_dataset.py
+++ python/paddle/fluid/tests/unittests/test_dataset.py
@@ -1,4 +1,3 @@
-"""
 #   Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+TestCases for Dataset.
+
 """
 
 from __future__ import print_function
