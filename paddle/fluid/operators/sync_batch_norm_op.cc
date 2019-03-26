2cbcdd67ee4af4eb65b9c51942ea2eb027ff1024
zhhsplendid <zhhsplendid@gmail.com>
2019-03-26 10:47:13 +0000

    Add SyncBatchNormGradOpDescMaker
    Use SyncBatchNormGradOpDescMaker instead of DefaultGradOpDescMaker
    to avoid registering useless variables to improve GPU usage.
    
    Bias are useless variable.
    
    test=develop

--- paddle/fluid/operators/sync_batch_norm_op.cc
+++ paddle/fluid/operators/sync_batch_norm_op.cc
@@ -12,9 +12,47 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#include <memory>
+
 #include "paddle/fluid/operators/batch_norm_op.h"
+
+namespace paddle {
+namespace operators {
+
+class SyncBatchNormGradOpDescMaker : public BatchNormGradMaker {
+ public:
+  using BatchNormGradMaker::BatchNormGradMaker;
+
+ protected:
+  std::unique_ptr<framework::OpDesc> Apply() const override {
+    std::unique_ptr<framework::OpDesc> op(new framework::OpDesc());
+    op->SetType("sync_batch_norm_grad");
+
+    op->SetInput("X", Input("X"));
+    op->SetInput(framework::GradVarName("Y"), OutputGrad("Y"));
+
+    op->SetInput("Scale", Input("Scale"));
+    op->SetInput("SavedMean", Output("SavedMean"));
+    op->SetInput("SavedVariance", Output("SavedVariance"));
+
+    // sync_batch_norm doesn't support use_global_stats True, no
+    // op->SetInput("Mean", Output("MeanOut"));
+    // op->SetInput("Variance", Output("VarianceOut"));
+
+    op->SetOutput(framework::GradVarName("X"), InputGrad("X"));
+    op->SetOutput(framework::GradVarName("Scale"), InputGrad("Scale"));
+    op->SetOutput(framework::GradVarName("Bias"), InputGrad("Bias"));
+
+    op->SetAttrMap(Attrs());
+    return op;
+  }
+};
+
+}  // namespace operators
+}  // namespace paddle
 
 namespace ops = paddle::operators;
 REGISTER_OPERATOR(sync_batch_norm, ops::BatchNormOp, ops::BatchNormOpMaker,
-                  ops::BatchNormOpInferVarType, ops::BatchNormGradMaker);
+                  ops::BatchNormOpInferVarType,
+                  ops::SyncBatchNormGradOpDescMaker);
 REGISTER_OPERATOR(sync_batch_norm_grad, ops::BatchNormGradOp);
