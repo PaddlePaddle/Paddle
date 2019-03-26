06048f32e600c76d73bc2413cb8ce68c616da1b4
dongdaxiang <dongdaxiang@baidu.com>
2019-03-26 22:37:10 +0800

    disable sys/wait.h to fix windows compile problem, include scope in lodtensor_printer test=develop

--- paddle/fluid/platform/lodtensor_printer.cc
+++ paddle/fluid/platform/lodtensor_printer.cc
@@ -14,6 +14,7 @@ limitations under the License. */
 
 #include "paddle/fluid/platform/lodtensor_printer.h"
 #include "paddle/fluid/framework/lod_tensor_array.h"
+#include "paddle/fluid/framework/scope.h"
 #include "paddle/fluid/framework/variable.h"
 
 namespace paddle {
