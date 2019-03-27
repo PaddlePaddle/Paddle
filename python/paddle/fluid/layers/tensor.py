6e001f841a896e5c5ad3936b1af0884050c820cf
zhoukunsheng <zhoukunsheng@baidu.com>
2019-03-27 20:20:06 +0800

    add more comment

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -832,7 +832,8 @@ def linspace(start, stop, num, dtype):
     Examples:
         .. code-block:: python
 
-             data = fluid.layers.linspace(0, 10, 5, 'float32')
+             data = fluid.layers.linspace(0, 10, 5, 'float32') # [0.0,  2.5,  5.0,  7.5, 10.0]
+             data = fluid.layers.linspace(0, 10, 1, 'float32') # [0.0]
 
     """
     helper = LayerHelper("linspace", **locals())
