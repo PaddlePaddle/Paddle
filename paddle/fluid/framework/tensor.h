767b2a10acf21ebaf112c230e59d9b3d33b6fb98
Jacek Czaja <jacek.czaja@intel.com>
2019-03-28 14:27:17 +0100

    [MKL-DNN] Tensor modifications revert  (#16462)
    * Revert "[MKL-DNN] Fix to crash of Transformer when mkldnn is to be used (#16233)"
    
    This reverts commit 13816dd4acdabd21a715b3b1c63fb43cdbac7622.
    Apart from enabling transformer for MKL-DNN
    
    * Revert "- MKL-DNN pooling updated to set_prim_desc"
    
    This reverts commit c63f6b20393d8b21b540e2b6091419e584ea5155.
    
    Conflicts:
    	paddle/fluid/operators/mkldnn/concat_mkldnn_op.cc
    
    * Revert "[MKL-DNN] MKL-DNN specific Tensor modification (#15429)"
    
    test=develop
    
    This reverts commit dec9cf53c89e0acc605a053b436ba24be68f62c7.
    
    * - concat compilation fix
    
    - lint
    
    test=develop
    
    - Lint fixes
    
    test=develop
    
    - Lint fixes
    
    test=develop
    
    - Fix Transpose MKLDNN op
    
    test=develop

--- paddle/fluid/framework/tensor.h
+++ paddle/fluid/framework/tensor.h
@@ -18,6 +18,7 @@ limitations under the License. */
 #include <cstring>
 #include <memory>
 #include <typeindex>
+#include <utility>
 #include <vector>
 #include "paddle/fluid/framework/data_layout.h"
 #include "paddle/fluid/framework/ddim.h"
@@ -26,10 +27,6 @@ limitations under the License. */
 #include "paddle/fluid/platform/device_context.h"
 #include "paddle/fluid/platform/enforce.h"
 #include "paddle/fluid/platform/place.h"
-
-#ifdef PADDLE_WITH_MKLDNN
-#include "paddle/fluid/platform/mkldnn_utils.h"
-#endif
 
 namespace paddle {
 
@@ -41,34 +38,10 @@ class Tensor {
 #ifdef PADDLE_WITH_MKLDNN
 
  public:
-  // TODO(jczaja): This is depracted and will be removed
-  inline mkldnn::memory::format format() const {
-    if (layout_ == DataLayout::kMKLDNN) {
-      return static_cast<mkldnn::memory::format>(mem_pd_.desc().data.format);
-    } else {
-      return mkldnn::memory::format::format_undef;
-    }
-  }
+  inline mkldnn::memory::format format() const { return format_; }
 
-  // TODO(jczaja): This is depracted and will be removed
-  inline void set_format(
-      const mkldnn::memory::format fmt,
-      mkldnn::memory::data_type data_type = mkldnn::memory::f32) {
-    mem_pd_ = paddle::platform::create_prim_desc_from_format(
-        paddle::framework::vectorize2int(dims()), fmt, data_type);
-    layout_ = DataLayout::kMKLDNN;
-  }
-
-  inline mkldnn::memory::primitive_desc get_mkldnn_prim_desc() const {
-    return mem_pd_;
-  }
-
-  inline void set_mkldnn_prim_desc(
-      const mkldnn::memory::primitive_desc& mem_pd) {
-    // Internally MKL-DNN is just copying (increasing reference counter)
-    // to shared_ptr. So asignment should be quite cheap
-    mem_pd_ = mem_pd;
-    layout_ = DataLayout::kMKLDNN;
+  inline void set_format(const mkldnn::memory::format format) {
+    format_ = format;
   }
 
  protected:
@@ -76,9 +49,12 @@ class Tensor {
    * @brief the detail format of memory block which have layout as kMKLDNN
    *
    * @note MKLDNN lib support various memory format like nchw, nhwc, nChw8C,
-   *       nChw16c, etc. For a MKLDNN memory block, we store memory descriptor
+   *       nChw16c, etc. For a MKLDNN memory block, layout will be set as
+   *       DataLayout::kMKLDNN meanwhile detail memory format will be kept in
+   *       this field.
    */
-  mutable mkldnn::memory::primitive_desc mem_pd_;
+
+  mkldnn::memory::format format_ = mkldnn::memory::format::format_undef;
 #endif
 
  public:
