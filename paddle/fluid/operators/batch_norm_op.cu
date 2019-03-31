b3638c6c34305037204471705e8cdac6e87829a4
typhoonzero <typhoonzero1986@gmail.com>
2019-03-31 19:33:49 +0800

    Revert "Batch norm cudnn accurate (#16545)" test=develop
    This reverts commit 22b02bfa62bd1eca27add1ea29b7eb80a8891b8d.

--- paddle/fluid/operators/batch_norm_op.cu
+++ paddle/fluid/operators/batch_norm_op.cu
@@ -23,16 +23,6 @@ limitations under the License. */
 #include "paddle/fluid/platform/cudnn_helper.h"
 #include "paddle/fluid/platform/float16.h"
 
-// CUDNN_BATCHNORM_SPATIAL_PERSISTENT in batchnorm. This mode can be faster in
-// some tasks because an optimized path may be selected for CUDNN_DATA_FLOAT
-// and CUDNN_DATA_HALF data types, compute capability 6.0 or higher. The
-// reason we set it to false by default is that this mode may use scaled
-// atomic integer reduction that may cause a numerical overflow for certain
-// input data range.
-DEFINE_bool(cudnn_batchnorm_spatial_persistent, false,
-            "Whether enable CUDNN_BATCHNORM_SPATIAL_PERSISTENT mode for cudnn "
-            "batch_norm, defalut is False.");
-
 namespace paddle {
 namespace operators {
 
@@ -86,11 +76,7 @@ class BatchNormKernel<platform::CUDADeviceContext, T>
     }
     epsilon = std::max(epsilon, CUDNN_BN_MIN_EPSILON);
 #if CUDNN_VERSION_MIN(7, 0, 0)
-    if (FLAGS_cudnn_batchnorm_spatial_persistent) {
-      mode_ = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
-    } else {
-      mode_ = CUDNN_BATCHNORM_SPATIAL;
-    }
+    mode_ = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
 #else
     mode_ = CUDNN_BATCHNORM_SPATIAL;
 #endif
@@ -316,11 +302,7 @@ class BatchNormGradKernel<platform::CUDADeviceContext, T>
       }
       epsilon = std::max(epsilon, CUDNN_BN_MIN_EPSILON);
 #if CUDNN_VERSION_MIN(7, 0, 0)
-      if (FLAGS_cudnn_batchnorm_spatial_persistent) {
-        mode_ = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
-      } else {
-        mode_ = CUDNN_BATCHNORM_SPATIAL;
-      }
+      mode_ = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
 #else
       mode_ = CUDNN_BATCHNORM_SPATIAL;
 #endif
