9abd0040d246bfe63ab35264ce492121b0c1a859
chengduozh <zhaochengduo@baidu.com>
2019-04-01 23:23:30 +0800

    fix the bug of fast threaded executor test=develop

--- paddle/fluid/framework/parallel_executor.cc
+++ paddle/fluid/framework/parallel_executor.cc
@@ -355,17 +355,13 @@ ParallelExecutor::ParallelExecutor(const std::vector<platform::Place> &places,
         "Paddle should be compiled with CUDA for ParallelGraph Execution.");
 #endif
   } else {
-    member_->executor_.reset(new details::FastThreadedSSAGraphExecutor(
-        exec_strategy, member_->local_scopes_, member_->places_, graph));
-    //    if (exec_strategy.type_ == ExecutionStrategy::kDefault) {
-    //      member_->executor_.reset(new details::ThreadedSSAGraphExecutor(
-    //          exec_strategy, member_->local_scopes_, member_->places_,
-    //          graph));
-    //    } else {
-    //      member_->executor_.reset(new details::FastThreadedSSAGraphExecutor(
-    //          exec_strategy, member_->local_scopes_, member_->places_,
-    //          graph));
-    //    }
+    if (exec_strategy.type_ == ExecutionStrategy::kDefault) {
+      member_->executor_.reset(new details::ThreadedSSAGraphExecutor(
+          exec_strategy, member_->local_scopes_, member_->places_, graph));
+    } else {
+      member_->executor_.reset(new details::FastThreadedSSAGraphExecutor(
+          exec_strategy, member_->local_scopes_, member_->places_, graph));
+    }
   }
 
   member_->executor_.reset(new details::ScopeBufferedSSAGraphExecutor(
