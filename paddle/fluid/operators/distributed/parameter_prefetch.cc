9080d04f8272e61e8429ae61548de957396d8dde
Qiao Longfei <qiaolongfei@baidu.com>
2019-04-01 13:25:58 +0800

    fix ci build test=develop

--- paddle/fluid/operators/distributed/parameter_prefetch.cc
+++ paddle/fluid/operators/distributed/parameter_prefetch.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <memory>
 #include <set>
 #include <string>
 #include <unordered_map>
