79b1b0ef9ed738f49399416ef8e657fa3b74ba7b
gongweibao <gongweibao@baidu.com>
2019-04-01 12:32:02 +0000

    fix batch merge bug test=develop

--- python/paddle/fluid/tests/unittests/test_dist_base.py
+++ python/paddle/fluid/tests/unittests/test_dist_base.py
@@ -139,8 +139,7 @@ class TestDistRunnerBase(object):
         pass_builder = None
         if args.batch_merge_repeat > 1:
             pass_builder = build_stra._finalize_strategy_and_create_passes()
-            mypass = pass_builder.insert_pass(
-                len(pass_builder.all_passes()) - 3, "multi_batch_merge_pass")
+            mypass = pass_builder.insert_pass(0, "multi_batch_merge_pass")
             mypass.set("num_repeats", args.batch_merge_repeat)
 
         if args.update_method == "nccl2" or args.update_method == "nccl2_reduce_layer":
