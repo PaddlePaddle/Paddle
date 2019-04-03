d100c44d80c0d7be9d8e34aab7b337c4213897db
chengduozh <zhaochengduo@baidu.com>
2019-04-03 10:33:10 +0800

    replace ThreadedExecutor with FastThreadedExecutor test=develop

--- paddle/fluid/framework/details/parallel_ssa_graph_executor.h
+++ paddle/fluid/framework/details/parallel_ssa_graph_executor.h
@@ -18,8 +18,8 @@
 #include <vector>
 
 #include "ThreadPool.h"
+#include "paddle/fluid/framework/details/fast_threaded_ssa_graph_executor.h"
 #include "paddle/fluid/framework/details/multi_devices_helper.h"
-#include "paddle/fluid/framework/details/threaded_ssa_graph_executor.h"
 #include "paddle/fluid/framework/ir/graph.h"
 
 namespace paddle {
@@ -48,7 +48,8 @@ class ParallelSSAGraphExecutor : public SSAGraphExecutor {
   std::vector<platform::Place> places_;
   std::vector<std::unique_ptr<ir::Graph>> graphs_;
 
-  std::vector<std::unique_ptr<details::ThreadedSSAGraphExecutor>> executors_;
+  std::vector<std::unique_ptr<details::FastThreadedSSAGraphExecutor>>
+      executors_;
   ExceptionHolder exception_holder_;
 };
 
