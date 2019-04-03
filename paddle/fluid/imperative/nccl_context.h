369f5ade8f5ccf5b72dfd2d7159c3108b646e291
Yancey1989 <yanxu05@baidu.com>
2019-04-03 11:30:22 +0800

    fix win ci test=develop

--- paddle/fluid/imperative/nccl_context.h
+++ paddle/fluid/imperative/nccl_context.h
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#ifndef _WIN32
 #pragma once
 
 // network header files
@@ -78,4 +77,3 @@ class NCCLParallelContext : ParallelContext {
 
 }  //  namespace imperative
 }  //  namespace paddle
-#endif
