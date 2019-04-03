cddc82750851084e83905335b6f2309cf0424d28
shippingwang <shipeng1108@163.com>
2019-04-03 08:13:25 +0000

    add include memory to travis CI bug,test=develop

--- paddle/fluid/operators/pixel_shuffle_op.cc
+++ paddle/fluid/operators/pixel_shuffle_op.cc
@@ -10,6 +10,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/operators/pixel_shuffle_op.h"
+#include <memory>
 
 namespace paddle {
 namespace operators {
