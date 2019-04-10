2bc5239d439cdc1b43b04e83176741be8e88cf7a
heqiaozhi <heqiaozhi@baidu.com>
2019-04-10 14:54:56 +0800

    fix doc test=develop

--- paddle/fluid/operators/cvm_op.cc
+++ paddle/fluid/operators/cvm_op.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserve.
+/* Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
