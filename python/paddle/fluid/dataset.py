1e52c0b6d579c369bc581c641576a9b114e53c2a
guru4elephant <35550832+guru4elephant@users.noreply.github.com>
2019-04-10 17:29:13 +0800

    Update dataset.py

--- python/paddle/fluid/dataset.py
+++ python/paddle/fluid/dataset.py
@@ -136,7 +136,6 @@ class DatasetBase(object):
             slot_var.name = var.name
             if var.lod_level == 0:
                 slot_var.is_dense = True
-                print(var.shape)
                 slot_var.shape.extend(var.shape)
             if var.dtype == core.VarDesc.VarType.FP32:
                 slot_var.type = "float"
