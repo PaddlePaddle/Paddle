920e8cdc5a6b4fb454071593fb4b07cacc1de59c
xuezhong <xuezhongq@gmail.com>
2019-04-16 09:19:38 +0000

    add <memory> test=develop

--- paddle/fluid/operators/smooth_l1_loss_op.cc
+++ paddle/fluid/operators/smooth_l1_loss_op.cc
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/operators/smooth_l1_loss_op.h"
+#include <memory>
 
 namespace paddle {
 namespace operators {
