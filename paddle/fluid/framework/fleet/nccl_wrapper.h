e88979fa5873545b98997d86b9fa52a8372558a2
dongdaxiang <dongdaxiang@baidu.com>
2019-04-17 09:19:54 +0800

    fix GPU compile error problem

--- paddle/fluid/framework/fleet/nccl_wrapper.h
+++ paddle/fluid/framework/fleet/nccl_wrapper.h
@@ -24,7 +24,9 @@ limitations under the License. */
 #include "paddle/fluid/framework/program_desc.h"
 #include "paddle/fluid/framework/scope.h"
 #include "paddle/fluid/framework/variable_helper.h"
+#ifdef PADDLE_WITH_CUDA
 #include "paddle/fluid/platform/dynload/nccl.h"
+#endif
 #include "paddle/fluid/platform/macros.h"  // for DISABLE_COPY_AND_ASSIGN
 
 namespace paddle {
@@ -39,9 +41,11 @@ class NCCLInfo {
   int local_rank_;
   int global_ranks_;
   int my_global_rank_;
+#ifdef PADDLE_WITH_CUDA
   ncclUniqueId nccl_id_;
   ncclComm_t comm_;
   cudaStream_t stream_;
+#endif
 };
 
 class NCCLWrapper {
