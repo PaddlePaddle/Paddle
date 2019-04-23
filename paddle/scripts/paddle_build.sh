d0205cfd006efa7b08a9cbd66b9c206a36437b94
peizhilin <wopeizl@163.com>
2019-04-23 14:44:52 +0800

    update

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -131,9 +131,9 @@ function cmake_gen() {
         }
         if command_exists ccache ; then
           export CCACHE_MAXSIZE=10G ;
-#          export CCACHE_CPP2=true ;
-          export CCACHE_HARDLINK=true ;
-          export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches ;
+          export CCACHE_CPP2=true ;
+#          export CCACHE_HARDLINK=true ;
+#          export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches ;
           exec ccache /usr/bin/clang "\$@" ;
         else
           exec /usr/bin/clang "\$@"
@@ -150,9 +150,9 @@ EOF
         }
         if command_exists ccache ; then
           export CCACHE_MAXSIZE=10G ;
-#          export CCACHE_CPP2=true ;
-          export CCACHE_HARDLINK=true ;
-          export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches ;
+          export CCACHE_CPP2=true ;
+#          export CCACHE_HARDLINK=true ;
+#          export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches ;
           exec ccache /usr/bin/clang++ "\$@" ;
         else
           exec /usr/bin/clang "\$@"
