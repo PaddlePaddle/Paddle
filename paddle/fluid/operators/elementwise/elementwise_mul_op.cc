5a9cfbebb544ff2acd2a5438bcc4e569049d7dc6
Michal Gallus <michal.gallus@intel.com>
2019-04-25 15:21:42 +0200

    Add AVX2 support for MKL-DNN EltwiseMul, remove format attrs

--- paddle/fluid/operators/elementwise/elementwise_mul_op.cc
+++ paddle/fluid/operators/elementwise/elementwise_mul_op.cc
@@ -72,7 +72,7 @@ class ElementwiseMulDoubleGradDescMaker
 }  // namespace paddle
 
 namespace ops = paddle::operators;
-REGISTER_OPERATOR(elementwise_mul, ops::ElementwiseOp,
+REGISTER_OPERATOR(elementwise_mul, ops::ElementwiseMulOp,
                   ops::ElementwiseMulOpMaker, ops::ElementwiseOpInferVarType,
                   ops::ElementwiseMulOpGradDescMaker);
 REGISTER_OPERATOR(elementwise_mul_grad, ops::ElementwiseOpGrad,
