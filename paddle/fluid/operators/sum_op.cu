29ac6a6221c0f150d8a0bca03026fd2ca7f9d9da
zhaoyuchen <zhaoyuchen01@baidu.com>
2019-04-25 09:10:17 +0000

    Refine code according to comments.
    test=develop

--- paddle/fluid/operators/sum_op.cu
+++ paddle/fluid/operators/sum_op.cu
@@ -12,7 +12,6 @@ limitations under the License. */
 #include <paddle/fluid/platform/device_context.h>
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/operators/sum_op.h"
-#include "paddle/fluid/operators/sum_op_gpu_kernel.h"
 
 namespace plat = paddle::platform;
 
@@ -24,8 +23,206 @@ namespace operators {
 using Tensor = framework::Tensor;
 using LoDTensor = framework::LoDTensor;
 
-template <typename DeviceContext, typename T>
-class SumCUDAKernel : public framework::OpKernel<T> {
+template <class T>
+__global__ void sum_gpu(const T *in_0, const T *in_1, T *out, int64_t N) {
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+  while (id < N) {
+    out[id] = in_0[id] + in_1[id];
+    id += blockDim.x * gridDim.x;
+  }
+}
+
+template <class T>
+__global__ void sum_gpu_array(T **in, T *out, int64_t N, size_t in_size,
+                              bool read_dst) {
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+  while (id < N) {
+    T total(0);
+    for (int i = 0; i < in_size; ++i) {
+      const T *tmp = in[i];
+      if (tmp != nullptr) {
+        total += tmp[id];
+      }
+    }
+    if (read_dst) {
+      out[id] += total;
+    } else {
+      out[id] = total;
+    }
+    id += blockDim.x * gridDim.x;
+  }
+}
+
+template <class T>
+__global__ void sum_gpu_sr(T **sr_in, T **sr_out, int64_t N, size_t rows) {
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+  while (id < N) {
+    for (int i = 0; i < rows; ++i) {
+      const T *tmp = sr_in[i];
+      T *tmp_out = sr_out[i];
+      if (tmp != nullptr && tmp_out != nullptr) {
+        tmp_out[id] += tmp[id];
+      }
+    }
+    id += blockDim.x * gridDim.x;
+  }
+}
+
+template <class T>
+__global__ void sum_gpu4(const T *in_0, const T *in_1, T *out, int64_t N) {
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+  for (int i = id; i < N / 4; i += blockDim.x * gridDim.x) {
+    float4 *in0_4 = reinterpret_cast<float4 *>(const_cast<T *>(in_0));
+    float4 *in1_4 = reinterpret_cast<float4 *>(const_cast<T *>(in_1));
+    float4 tmp;
+    tmp.x = in0_4[i].x + in1_4[i].x;
+    tmp.y = in0_4[i].y + in1_4[i].y;
+    tmp.z = in0_4[i].z + in1_4[i].z;
+    tmp.w = in0_4[i].w + in1_4[i].w;
+    reinterpret_cast<float4 *>(out)[i] = tmp;
+  }
+}
+
+template <class T>
+void FuseSumCompute(const framework::ExecutionContext &context) {
+  auto in_vars = context.MultiInputVar("X");
+  const size_t in_num = in_vars.size();
+
+  constexpr size_t theory_sm_threads = 1024;
+  auto &dev_ctx =
+      context.template device_context<platform::CUDADeviceContext>();
+  auto stream = dev_ctx.stream();
+
+  auto max_threads = dev_ctx.GetMaxPhysicalThreadCount();
+  auto sm_count = max_threads / theory_sm_threads;
+  size_t tile_size = 0;
+  dim3 grids;
+  dim3 blocks;
+
+  auto KeCompute = [&](size_t length) {
+    if (length >= max_threads)
+      tile_size = 1024;
+    else if (length < max_threads && length > sm_count * 128)
+      tile_size = 512;
+    else if (length <= sm_count * 128)
+      tile_size = 256;
+    grids = dim3(CEIL_DIV(length, tile_size), 1, 1);
+    blocks = dim3(tile_size, 1, 1);
+  };
+
+  auto *out = context.Output<LoDTensor>("Out");
+
+  auto out_var = context.OutputVar("Out");
+  bool in_place = in_vars[0] == out_var;
+
+  if (!in_place) {
+    out->mutable_data<T>(context.GetPlace());
+  }
+  int start = in_place ? 1 : 0;
+  if (!in_place) {
+    if (in_num == 2 && in_vars[0]->IsType<framework::LoDTensor>() &&
+        in_vars[1]->IsType<framework::LoDTensor>()) {
+      auto &in_0 = in_vars[0]->Get<framework::LoDTensor>();
+      auto &in_1 = in_vars[1]->Get<framework::LoDTensor>();
+
+      auto length = in_0.numel();
+      KeCompute(length);
+      sum_gpu<T><<<grids, blocks, 0, stream>>>(in_0.data<T>(), in_1.data<T>(),
+                                               out->data<T>(), length);
+      return;
+    }
+  }
+
+  std::vector<const T *> in_data;
+  std::vector<int> selectrow_index;
+  int64_t lod_length = 0;
+  bool dst_write = false;
+  for (int i = start; i < in_num; ++i) {
+    if (in_vars[i]->IsType<framework::LoDTensor>()) {
+      auto &in_i = in_vars[i]->Get<framework::LoDTensor>();
+      in_data.emplace_back(in_i.data<T>());
+      lod_length = in_i.numel();
+    } else if (in_vars[i]->IsType<framework::SelectedRows>()) {
+      selectrow_index.push_back(i);
+    }
+  }
+
+  // compute select rows seperately.
+  if (!selectrow_index.empty()) {
+    std::vector<const T *> out_data;
+    std::vector<const T *> sr_in_data;
+    size_t rows = 0;
+    int64_t length = 0;
+    for (auto index : selectrow_index) {
+      auto &sr = in_vars[index]->Get<framework::SelectedRows>();
+      auto &sr_value = sr.value();
+      auto &sr_rows = sr.rows();
+
+      auto row_numel = sr_value.numel() / sr_rows.size();
+      auto out_dims = out->dims();
+
+      PADDLE_ENFORCE_EQ(sr.height(), out_dims[0]);
+      PADDLE_ENFORCE_EQ(row_numel, out->numel() / sr.height());
+
+      auto *sr_data = sr_value.data<T>();
+      auto *sr_out_data = out->data<T>();
+      rows += sr_rows.size();
+      length = row_numel;
+
+      for (size_t i = 0; i < sr_rows.size(); ++i) {
+        sr_in_data.emplace_back(&sr_data[i * row_numel]);
+        out_data.emplace_back(&sr_out_data[sr_rows[i] * row_numel]);
+      }
+    }
+    if (!sr_in_data.empty() && !out_data.empty()) {
+      auto tmp_sr_in_array =
+          platform::DeviceTemporaryAllocator::Instance().Get(dev_ctx).Allocate(
+              sr_in_data.size() * sizeof(T *));
+
+      memory::Copy(boost::get<platform::CUDAPlace>(dev_ctx.GetPlace()),
+                   tmp_sr_in_array->ptr(), platform::CPUPlace(),
+                   reinterpret_cast<void *>(sr_in_data.data()),
+                   sr_in_data.size() * sizeof(T *), dev_ctx.stream());
+
+      T **sr_in_array_data = reinterpret_cast<T **>(tmp_sr_in_array->ptr());
+
+      auto tmp_out_array =
+          platform::DeviceTemporaryAllocator::Instance().Get(dev_ctx).Allocate(
+              out_data.size() * sizeof(T *));
+
+      memory::Copy(boost::get<platform::CUDAPlace>(dev_ctx.GetPlace()),
+                   tmp_out_array->ptr(), platform::CPUPlace(),
+                   reinterpret_cast<void *>(out_data.data()),
+                   out_data.size() * sizeof(T *), dev_ctx.stream());
+
+      T **out_array_data = reinterpret_cast<T **>(tmp_out_array->ptr());
+      KeCompute(length);
+      sum_gpu_sr<T><<<grids, blocks, 0, stream>>>(sr_in_array_data,
+                                                  out_array_data, length, rows);
+      dst_write = true;
+    }
+  }
+  // if indata not null, merge into one kernel call.
+  if (!in_data.empty()) {
+    auto tmp_in_array =
+        platform::DeviceTemporaryAllocator::Instance().Get(dev_ctx).Allocate(
+            in_data.size() * sizeof(T *));
+
+    memory::Copy(boost::get<platform::CUDAPlace>(dev_ctx.GetPlace()),
+                 tmp_in_array->ptr(), platform::CPUPlace(),
+                 reinterpret_cast<void *>(in_data.data()),
+                 in_data.size() * sizeof(T *), dev_ctx.stream());
+
+    T **in_array_data = reinterpret_cast<T **>(tmp_in_array->ptr());
+    KeCompute(lod_length);
+    sum_gpu_array<T><<<grids, blocks, 0, stream>>>(
+        in_array_data, out->data<T>(), lod_length, in_data.size(), dst_write);
+  }
+}
+
+template <typename T>
+class SumKernel<platform::CUDADeviceContext, T>
+    : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext &context) const override {
     auto in_vars = context.MultiInputVar("X");
@@ -33,134 +230,8 @@ class SumCUDAKernel : public framework::OpKernel<T> {
     auto out_var = context.OutputVar("Out");
 
     bool in_place = out_var == in_vars[0];
-    constexpr size_t theory_sm_threads = 1024;
-    auto &dev_ctx = context.template device_context<DeviceContext>();
-    auto stream = dev_ctx.stream();
-
-    auto max_threads = dev_ctx.GetMaxPhysicalThreadCount();
-    auto sm_count = max_threads / theory_sm_threads;
-    size_t tile_size = 0;
-    dim3 grids;
-    dim3 blocks;
-
-    auto KeCompute = [&](size_t length) {
-      if (length >= max_threads)
-        tile_size = 1024;
-      else if (length < max_threads && length > sm_count * 128)
-        tile_size = 512;
-      else if (length <= sm_count * 128)
-        tile_size = 256;
-      grids = dim3(CEIL_DIV(length, tile_size), 1, 1);
-      blocks = dim3(tile_size, 1, 1);
-    };
-
     if (out_var->IsType<framework::LoDTensor>()) {
-      auto *out = context.Output<LoDTensor>("Out");
-
-      if (!in_place) {
-        out->mutable_data<T>(context.GetPlace());
-      }
-      int start = in_place ? 1 : 0;
-      if (!in_place) {
-        if (in_num == 2 && in_vars[0]->IsType<framework::LoDTensor>() &&
-            in_vars[1]->IsType<framework::LoDTensor>()) {
-          auto &in_0 = in_vars[0]->Get<framework::LoDTensor>();
-          auto &in_1 = in_vars[1]->Get<framework::LoDTensor>();
-
-          auto length = in_0.numel();
-          KeCompute(length);
-          sum_gpu<T><<<grids, blocks, 0, stream>>>(
-              in_0.data<T>(), in_1.data<T>(), out->data<T>(), length);
-          return;
-        }
-      }
-
-      std::vector<const T *> in_data;
-      std::vector<int> selectrow_index;
-      int64_t lod_length = 0;
-      bool dst_write = false;
-      for (int i = start; i < in_num; ++i) {
-        if (in_vars[i]->IsType<framework::LoDTensor>()) {
-          auto &in_i = in_vars[i]->Get<framework::LoDTensor>();
-          in_data.emplace_back(in_i.data<T>());
-          lod_length = in_i.numel();
-        } else if (in_vars[i]->IsType<framework::SelectedRows>()) {
-          selectrow_index.push_back(i);
-        }
-      }
-
-      // compute select rows seperately.
-      if (!selectrow_index.empty()) {
-        std::vector<const T *> out_data;
-        std::vector<const T *> sr_in_data;
-        size_t rows = 0;
-        int64_t length = 0;
-        for (auto index : selectrow_index) {
-          auto &sr = in_vars[index]->Get<framework::SelectedRows>();
-          auto &sr_value = sr.value();
-          auto &sr_rows = sr.rows();
-
-          auto row_numel = sr_value.numel() / sr_rows.size();
-          auto out_dims = out->dims();
-
-          PADDLE_ENFORCE_EQ(sr.height(), out_dims[0]);
-          PADDLE_ENFORCE_EQ(row_numel, out->numel() / sr.height());
-
-          auto *sr_data = sr_value.data<T>();
-          auto *sr_out_data = out->data<T>();
-          rows += sr_rows.size();
-          length = row_numel;
-
-          for (size_t i = 0; i < sr_rows.size(); ++i) {
-            sr_in_data.emplace_back(&sr_data[i * row_numel]);
-            out_data.emplace_back(&sr_out_data[sr_rows[i] * row_numel]);
-          }
-        }
-        if (!sr_in_data.empty() && !out_data.empty()) {
-          auto tmp_sr_in_array = platform::DeviceTemporaryAllocator::Instance()
-                                     .Get(dev_ctx)
-                                     .Allocate(sr_in_data.size() * sizeof(T *));
-
-          memory::Copy(boost::get<platform::CUDAPlace>(dev_ctx.GetPlace()),
-                       tmp_sr_in_array->ptr(), platform::CPUPlace(),
-                       reinterpret_cast<void *>(sr_in_data.data()),
-                       sr_in_data.size() * sizeof(T *), dev_ctx.stream());
-
-          T **sr_in_array_data = reinterpret_cast<T **>(tmp_sr_in_array->ptr());
-
-          auto tmp_out_array = platform::DeviceTemporaryAllocator::Instance()
-                                   .Get(dev_ctx)
-                                   .Allocate(out_data.size() * sizeof(T *));
-
-          memory::Copy(boost::get<platform::CUDAPlace>(dev_ctx.GetPlace()),
-                       tmp_out_array->ptr(), platform::CPUPlace(),
-                       reinterpret_cast<void *>(out_data.data()),
-                       out_data.size() * sizeof(T *), dev_ctx.stream());
-
-          T **out_array_data = reinterpret_cast<T **>(tmp_out_array->ptr());
-          KeCompute(length);
-          sum_gpu_sr<T><<<grids, blocks, 0, stream>>>(
-              sr_in_array_data, out_array_data, length, rows);
-          dst_write = true;
-        }
-      }
-      // if case scale too large, we choose big block.
-      if (!in_data.empty()) {
-        auto tmp_in_array = platform::DeviceTemporaryAllocator::Instance()
-                                .Get(dev_ctx)
-                                .Allocate(in_data.size() * sizeof(T *));
-
-        memory::Copy(boost::get<platform::CUDAPlace>(dev_ctx.GetPlace()),
-                     tmp_in_array->ptr(), platform::CPUPlace(),
-                     reinterpret_cast<void *>(in_data.data()),
-                     in_data.size() * sizeof(T *), dev_ctx.stream());
-
-        T **in_array_data = reinterpret_cast<T **>(tmp_in_array->ptr());
-        KeCompute(lod_length);
-        sum_gpu_array<T><<<grids, blocks, 0, stream>>>(
-            in_array_data, out->data<T>(), lod_length, in_data.size(),
-            dst_write);
-      }
+      FuseSumCompute<T>(context);
     } else if (out_var->IsType<framework::SelectedRows>()) {
       if (in_place && in_vars.size() < 2) {
         return;
@@ -203,9 +274,10 @@ class SumCUDAKernel : public framework::OpKernel<T> {
         }
       }
       if (has_data) {
-        math::scatter::MergeAdd<DeviceContext, T> merge_add;
-        merge_add(context.template device_context<DeviceContext>(), inputs,
-                  out);
+        math::scatter::MergeAdd<platform::CUDADeviceContext, T> merge_add;
+        merge_add(
+            context.template device_context<platform::CUDADeviceContext>(),
+            inputs, out);
 
         out->SyncIndex();
 
@@ -234,8 +306,10 @@ class SumCUDAKernel : public framework::OpKernel<T> {
               PADDLE_ENFORCE(out_array[i].lod() == in_array[i].lod());
               auto in = EigenVector<T>::Flatten(in_array[i]);
               auto result = EigenVector<T>::Flatten(out_array[i]);
-              result.device(*context.template device_context<DeviceContext>()
-                                 .eigen_device()) = result + in;
+              result.device(
+                  *context
+                       .template device_context<platform::CUDADeviceContext>()
+                       .eigen_device()) = result + in;
             }
           }
         }
@@ -252,8 +326,8 @@ class SumCUDAKernel : public framework::OpKernel<T> {
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
 REGISTER_OP_CUDA_KERNEL(
-    sum, ops::SumCUDAKernel<paddle::platform::CUDADeviceContext, float>,
-    ops::SumCUDAKernel<paddle::platform::CUDADeviceContext, double>,
-    ops::SumCUDAKernel<paddle::platform::CUDADeviceContext, int>,
-    ops::SumCUDAKernel<paddle::platform::CUDADeviceContext, int64_t>,
-    ops::SumCUDAKernel<paddle::platform::CUDADeviceContext, plat::float16>);
+    sum, ops::SumKernel<paddle::platform::CUDADeviceContext, float>,
+    ops::SumKernel<paddle::platform::CUDADeviceContext, double>,
+    ops::SumKernel<paddle::platform::CUDADeviceContext, int>,
+    ops::SumKernel<paddle::platform::CUDADeviceContext, int64_t>,
+    ops::SumKernel<paddle::platform::CUDADeviceContext, plat::float16>);
