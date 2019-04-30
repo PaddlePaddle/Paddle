3e939b62744c5bd764af7ff97170796d6f5b2a1d
tianshuo78520a <707759223@qq.com>
2019-04-30 10:08:01 +0800

    add document_preview.sh

---
+++ tools/document_preview.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install /paddle/build/opt/paddle/share/wheels/paddlepaddle-0.10.0-cp27-cp27mu-linux_x86_64.whl
+apt-get update && apt-get install -y python-dev build-essential
+cd /home/PaddlePaddle.org/portal
+pip install -r requirements.txt
+./runserver --paddle /home/FluidDoc
+echo $1
