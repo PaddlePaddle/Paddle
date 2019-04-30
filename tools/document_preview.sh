fd0919f27cbd1d623b050494025c7ba2ae6830ca
tianshuo78520a <707759223@qq.com>
2019-04-30 11:07:43 +0800

    nohup python

--- tools/document_preview.sh
+++ tools/document_preview.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo $1
 pip install /paddle/build/opt/paddle/share/wheels/paddlepaddle-0.10.0-cp27-cp27mu-linux_x86_64.whl
 apt-get update && apt-get install -y python-dev build-essential
 cd /home/PaddlePaddle.org/portal
 pip install -r requirements.txt
-./runserver --paddle /home/FluidDoc
-echo $1
+sed "s#8000#$1#g" runserver
+nohup ./runserver --paddle /home/FluidDoc &
+echo 1111
