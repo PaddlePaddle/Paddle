85858a45f27b00c64660519f976493accf9ab2bb
tianshuo78520a <707759223@qq.com>
2019-04-30 13:50:22 +0800

    change port runserver

--- tools/document_preview.sh
+++ tools/document_preview.sh
@@ -5,6 +5,6 @@ pip install /paddle/build/opt/paddle/share/wheels/paddlepaddle-0.10.0-cp27-cp27m
 apt-get update && apt-get install -y python-dev build-essential
 cd /home/PaddlePaddle.org/portal
 pip install -r requirements.txt
-sed "s#8000#$1#g" runserver
+sed -i "s#8000#$1#g" runserver
 nohup ./runserver --paddle /home/FluidDoc &
 echo 1111
