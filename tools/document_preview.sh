14d8be946c122725233fc0a506553474bc2a8540
tianshuo78520a <707759223@qq.com>
2019-05-05 14:39:19 +0800

    add git clone FluidDoc,PaddlePaddle.org

--- tools/document_preview.sh
+++ tools/document_preview.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd /home/FluidDoc/doc/fluid/api
-/bin/bash gen_doc.sh
+cd /home
+git clone https://github.com/PaddlePaddle/FluidDoc
+git clone https://github.com/PaddlePaddle/PaddlePaddle.org.git
+sh /home/FluidDoc/doc/fluid/api/gen_doc.sh
 pip install /paddle/build/opt/paddle/share/wheels/*.whl
 apt-get update && apt-get install -y python-dev build-essential
 cd /home/PaddlePaddle.org/portal/portal
