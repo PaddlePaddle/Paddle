cb03777ae373c0e2ad05f0495175329f3cea9092
tianshuo78520a <707759223@qq.com>
2019-05-05 16:02:48 +0800

    change PaddlePaddle.org

--- tools/document_preview.sh
+++ tools/document_preview.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 cd /home
 git clone https://github.com/PaddlePaddle/FluidDoc
-git clone https://github.com/PaddlePaddle/PaddlePaddle.org.git
+git clone https://github.com/tianshuo78520a/PaddlePaddle.org.git
 sh /home/FluidDoc/doc/fluid/api/gen_doc.sh
 pip install /paddle/build/opt/paddle/share/wheels/*.whl
 apt-get update && apt-get install -y python-dev build-essential
 cd /home/PaddlePaddle.org/portal/portal
-sed -i "210a return 'http://paddlepaddle.org'" documentation_generator.py
-sed -i "211s#^#        #g" documentation_generator.py
 cd /home/PaddlePaddle.org/portal
 pip install -r requirements.txt
 sed -i "s#8000#$1#g" runserver
