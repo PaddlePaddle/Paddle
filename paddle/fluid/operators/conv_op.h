7ef7cec6839bf166698a5149b94ad095a7323339
Dang Qingqing <dangqingqing@baidu.com>
2019-05-05 20:18:08 +0800

    Fix cpplint in travis-ci test=develop

--- paddle/fluid/operators/conv_op.h
+++ paddle/fluid/operators/conv_op.h
@@ -15,6 +15,7 @@ limitations under the License. */
 #pragma once
 
 #include <string>
+#include <unordered_map>
 #include <vector>
 #include "paddle/fluid/framework/eigen.h"
 #include "paddle/fluid/framework/op_registry.h"
