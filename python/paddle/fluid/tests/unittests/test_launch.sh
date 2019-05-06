19fe1da838f7cffa55415afdbe10cd16e9b7cff5
gongweibao <gongweibao@baidu.com>
2019-05-06 04:00:29 +0000

    fix

--- python/paddle/fluid/tests/unittests/test_launch.sh
+++ python/paddle/fluid/tests/unittests/test_launch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
+
 node_ips="127.0.0.1"
 node_id="0"
 current_node_ip="127.0.0.1"
 
 distributed_args="--node_ips ${node_ips} --node_id ${node_id} --current_node_ip ${current_node_ip}"
-python -m paddle.distributed.launch {distributed_args} multi_process.py
+python -m paddle.distributed.launch ${distributed_args} multi_process.py
