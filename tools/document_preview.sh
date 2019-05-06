f704dee0220bdb91542222abaeaf5f792522d5a2
tianshuo78520a <707759223@qq.com>
2019-05-06 11:41:33 +0800

    change directory

--- tools/document_preview.sh
+++ tools/document_preview.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd ${PADDLE_ROOT}
+cd ~ 
 git clone https://github.com/PaddlePaddle/FluidDoc
 git clone https://github.com/tianshuo78520a/PaddlePaddle.org.git
-sh ${PADDLE_ROOT}/FluidDoc/doc/fluid/api/gen_doc.sh
+sh ~/FluidDoc/doc/fluid/api/gen_doc.sh
 pip install ${PADDLE_ROOT}/build/opt/paddle/share/wheels/*.whl
 apt-get update && apt-get install -y python-dev build-essential
-cd ${PADDLE_ROOT}/PaddlePaddle.org/portal
+cd ~/PaddlePaddle.org/portal
 pip install -r requirements.txt
 #If the default port is not occupied, you can use port 8000, you need to replace it with a random port on the CI.
 sed -i "s#8000#$1#g" runserver
-nohup ./runserver --paddle /home/FluidDoc &
+nohup ./runserver --paddle ~/FluidDoc &
