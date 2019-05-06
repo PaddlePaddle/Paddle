0892c8100fdac8f3f0133efd788aecb30432e49b
tianshuo78520a <707759223@qq.com>
2019-05-06 12:20:52 +0800

    change PADDLE_ROOT

--- tools/document_preview.sh
+++ tools/document_preview.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-cd ~ 
+PADDLE_ROOT=/paddle
+cd ${PADDLE_ROOT}
 git clone https://github.com/PaddlePaddle/FluidDoc
 git clone https://github.com/tianshuo78520a/PaddlePaddle.org.git
-sh ~/FluidDoc/doc/fluid/api/gen_doc.sh
+sh ${PADDLE_ROOT}/FluidDoc/doc/fluid/api/gen_doc.sh
 pip install ${PADDLE_ROOT}/build/opt/paddle/share/wheels/*.whl
 apt-get update && apt-get install -y python-dev build-essential
-cd ~/PaddlePaddle.org/portal
+cd ${PADDLE_ROOT}/PaddlePaddle.org/portal
 pip install -r requirements.txt
 #If the default port is not occupied, you can use port 8000, you need to replace it with a random port on the CI.
 sed -i "s#8000#$1#g" runserver
-nohup ./runserver --paddle ~/FluidDoc &
+nohup ./runserver --paddle ${PADDLE_ROOT}/FluidDoc &
