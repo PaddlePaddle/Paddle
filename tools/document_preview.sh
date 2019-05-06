935891602f8d1cdbe995d3d135c5fc95c88503d2
tianshuo78520a <707759223@qq.com>
2019-05-06 10:24:59 +0800

    Add port comment

--- tools/document_preview.sh
+++ tools/document_preview.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /home
+cd ${PADDLE_ROOT}
 git clone https://github.com/PaddlePaddle/FluidDoc
 git clone https://github.com/tianshuo78520a/PaddlePaddle.org.git
-sh /home/FluidDoc/doc/fluid/api/gen_doc.sh
-pip install /paddle/build/opt/paddle/share/wheels/*.whl
+sh ${PADDLE_ROOT}/FluidDoc/doc/fluid/api/gen_doc.sh
+pip install ${PADDLE_ROOT}/build/opt/paddle/share/wheels/*.whl
 apt-get update && apt-get install -y python-dev build-essential
-cd /home/PaddlePaddle.org/portal/portal
-cd /home/PaddlePaddle.org/portal
+cd ${PADDLE_ROOT}/PaddlePaddle.org/portal
 pip install -r requirements.txt
+#If the default port is not occupied, you can use port 8000, you need to replace it with a random port on the CI.
 sed -i "s#8000#$1#g" runserver
 nohup ./runserver --paddle /home/FluidDoc &
