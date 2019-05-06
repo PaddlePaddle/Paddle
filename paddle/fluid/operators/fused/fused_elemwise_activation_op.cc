757afde5bb1e78e704b3648508dcf9def8ef34ba
Liu Yiqun <liuyiqun01@baidu.com>
2019-05-06 19:47:13 +0800

    Add including files. test=develop

--- paddle/fluid/operators/fused/fused_elemwise_activation_op.cc
+++ paddle/fluid/operators/fused/fused_elemwise_activation_op.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/operators/fused/fused_elemwise_activation_op.h"
+#include <memory>
+#include <unordered_set>
 
 namespace paddle {
 namespace operators {
