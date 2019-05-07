254ac5eb7bf799b30afbec6e2817dc4666108963
phlrain <phliuhongyu@126.com>
2019-05-07 05:52:14 +0000

    fix format; test=develop

--- paddle/fluid/framework/ddim.cc
+++ paddle/fluid/framework/ddim.cc
@@ -121,10 +121,8 @@ int64_t product(const DDim& ddim) {
   return ddim.apply_visitor(ProductVisitor());
 }
 
-bool has_mutable_dim(const DDim& ddim)
-{
-    for ( size_t i = 0; i < ddim.size(); ++i)
-    {
+bool has_mutable_dim(const DDim& ddim){
+    for ( size_t i = 0; i < ddim.size(); ++i)     {
         if ( ddim[i] < 0 )
         {
             return true;
