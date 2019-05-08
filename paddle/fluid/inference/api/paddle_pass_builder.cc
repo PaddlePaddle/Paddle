908282835fcc3a4269984da41b59dbc63887fa8c
fuchang01 <fuchang1991@gmail.com>
2019-05-08 15:35:25 +0800

    test=develop

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -123,7 +123,7 @@ CpuPassStrategy::CpuPassStrategy() : PassStrategy({}) {
                   "seqconv_eltadd_relu_fuse_pass",  //
                   // "seqpool_concat_fuse_pass",    //
                   // "embedding_fc_lstm_fuse_pass", //
-                  "fc_lstm_fuse_pass",             //
+                  "fc_lstm_fuse_pass",  //
                   // "mul_lstm_fuse_pass",            //
                   "fc_gru_fuse_pass",              //
                   "mul_gru_fuse_pass",             //
