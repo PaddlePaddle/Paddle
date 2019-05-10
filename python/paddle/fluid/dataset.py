55ecd448f87f48f0fda8402caf190ca6d465d729
xjqbest <173596896@qq.com>
2019-05-10 16:46:15 +0800

    fix code style test=develop

--- python/paddle/fluid/dataset.py
+++ python/paddle/fluid/dataset.py
@@ -302,7 +302,8 @@ class InMemoryDataset(DatasetBase):
         local_data_size = np.array([local_data_size])
         if fleet is not None:
             global_data_size = local_data_size * 0
-            fleet._role_maker._node_type_comm.Allreduce(local_data_size, global_data_size)
+            fleet._role_maker._node_type_comm.Allreduce(local_data_size,
+                                                        global_data_size)
             return global_data_size[0]
         return local_data_size[0]
 
@@ -337,7 +338,8 @@ class InMemoryDataset(DatasetBase):
         local_data_size = np.array([local_data_size])
         if fleet is not None:
             global_data_size = local_data_size * 0
-            fleet._role_maker._node_type_comm.Allreduce(local_data_size, global_data_size)
+            fleet._role_maker._node_type_comm.Allreduce(local_data_size,
+                                                        global_data_size)
             return global_data_size[0]
         return local_data_size[0]
 
