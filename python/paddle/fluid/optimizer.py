b50c1527a178cfb1a386024e9288490668cb2f6f
wopeizl <wopeizl@icloud.com>
2019-05-14 10:57:29 +0800

    add example for adamoptimizer fix API.spec test=develop

--- python/paddle/fluid/optimizer.py
+++ python/paddle/fluid/optimizer.py
@@ -1152,8 +1152,30 @@ class AdamOptimizer(Optimizer):
     Examples:
         .. code-block:: python
 
-            optimizer = fluid.optimizer.Adam(learning_rate=0.2)
-            optimizer.minimize(cost)
+            import paddle
+            import paddle.fluid as fluid
+
+            place = fluid.CPUPlace()
+            main = fluid.Program()
+            with fluid.program_guard(main):
+                x = fluid.layers.data(name='x', shape=[13], dtype='float32')
+                y = fluid.layers.data(name='y', shape=[1], dtype='float32')
+                y_predict = fluid.layers.fc(input=x, size=1, act=None)
+                cost = fluid.layers.square_error_cost(input=y_predict, label=y)
+                avg_cost = fluid.layers.mean(cost)
+
+                adam_optimizer = fluid.optimizer.AdamOptimizer(0.01)
+                adam_optimizer.minimize(avg_cost)
+
+                fetch_list = [avg_cost]
+                train_reader = paddle.batch(
+                    paddle.dataset.uci_housing.train(), batch_size=1)
+                feeder = fluid.DataFeeder(place=place, feed_list=[x, y])
+                exe = fluid.Executor(place)
+                exe.run(fluid.default_startup_program())
+                for data in train_reader():
+                    exe.run(main, feed=feeder.feed(data), fetch_list=fetch_list)
+                    break
 
     """
     _moment1_acc_str = "moment1"
