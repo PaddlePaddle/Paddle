cf62d890058896f5fdca814764d8a2c2e8c1d253
wopeizl <wopeizl@icloud.com>
2019-05-16 18:17:50 +0800

    add extra test case for pyreader test=develop

--- python/paddle/fluid/tests/unittests/test_pyreader.py
+++ python/paddle/fluid/tests/unittests/test_pyreader.py
@@ -17,6 +17,7 @@ import paddle.fluid as fluid
 import unittest
 import numpy as np
 
+
 class TestPyReader(unittest.TestCase):
     def setUp(self):
         self.batch_size = 32
@@ -27,17 +28,25 @@ class TestPyReader(unittest.TestCase):
         def reader_creator_random_image(height, width):
             def reader():
                 for i in range(self.sample_num):
-                    yield np.random.uniform(low=0, high=255, size=[height, width]),
+                    yield np.random.uniform(
+                        low=0, high=255, size=[height, width]),
+
             return reader
 
         for return_list in [True, False]:
             with fluid.program_guard(fluid.Program(), fluid.Program()):
-                image = fluid.layers.data(name='image', shape=[784, 784], dtype='float32')
-                reader = fluid.io.PyReader(feed_list=[image], capacity=4, iterable=True, return_list=return_list)
+                image = fluid.layers.data(
+                    name='image', shape=[784, 784], dtype='float32')
+                reader = fluid.io.PyReader(
+                    feed_list=[image],
+                    capacity=4,
+                    iterable=True,
+                    return_list=return_list)
 
                 user_defined_reader = reader_creator_random_image(784, 784)
                 reader.decorate_sample_list_generator(
-                    paddle.batch(user_defined_reader, batch_size=self.batch_size),
+                    paddle.batch(
+                        user_defined_reader, batch_size=self.batch_size),
                     fluid.core.CPUPlace())
                 # definition of network is omitted
                 executor = fluid.Executor(fluid.core.CPUPlace())
@@ -52,13 +61,17 @@ class TestPyReader(unittest.TestCase):
 
             with fluid.dygraph.guard():
                 batch_py_reader = fluid.io.PyReader(
-                    feed_list=[np.empty([self.batch_size, 784, 784], dtype='float32')],
+                    feed_list=[
+                        np.empty(
+                            [self.batch_size, 784, 784], dtype='float32')
+                    ],
                     capacity=2,
                     use_double_buffer=True,
                     return_list=return_list)
                 user_defined_reader = reader_creator_random_image(784, 784)
                 batch_py_reader.decorate_sample_list_generator(
-                    paddle.batch(user_defined_reader, batch_size=self.batch_size),
+                    paddle.batch(
+                        user_defined_reader, batch_size=self.batch_size),
                     fluid.core.CPUPlace())
 
                 for epoch in range(self.epoch_num):
@@ -68,6 +81,7 @@ class TestPyReader(unittest.TestCase):
 
     def test_iterable(self):
         pass
+
 
 if __name__ == "__main__":
     unittest.main()
