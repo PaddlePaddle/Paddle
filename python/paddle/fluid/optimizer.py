15453d05a8d4a4f96135545a4b4919ec0c82b4d2
Jiabin Yang <marsyang199376@gmail.com>
2019-05-17 10:44:54 +0800

    test=develop, fix AdgradOptimizer example code (#17401)

--- python/paddle/fluid/optimizer.py
+++ python/paddle/fluid/optimizer.py
@@ -1006,8 +1006,22 @@ class AdagradOptimizer(Optimizer):
     Examples:
         .. code-block:: python
 
+            import paddle.fluid as fluid
+            import numpy as np
+
+            np_inp = np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)
+            inp = fluid.layers.data(
+                name="inp", shape=[2, 2], append_batch_size=False)
+            out = fluid.layers.fc(inp, size=3)
+            out = fluid.layers.reduce_sum(out)
             optimizer = fluid.optimizer.Adagrad(learning_rate=0.2)
-            optimizer.minimize(cost)
+            optimizer.minimize(out)
+
+            exe = fluid.Executor(fluid.CPUPlace())
+            exe.run(fluid.default_startup_program())
+            exe.run(
+                feed={"inp": np_inp},
+                fetch_list=[out.name])
     """
     _moment_acc_str = "moment"
 
