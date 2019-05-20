b5daad8f17dd3f7ed63e2f31ed71bce014318816
Qiao Longfei <qiaolongfei@baidu.com>
2019-05-20 18:51:46 +0800

    fix import test=develop

--- python/paddle/fluid/communicator.py
+++ python/paddle/fluid/communicator.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from executor import global_scope
+from .executor import global_scope
 from . import core
 from .framework import Program
 
