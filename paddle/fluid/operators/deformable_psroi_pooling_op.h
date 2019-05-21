e5e6931c5110b397226c3e4a5e4102c3559e63f6
root <root@yq01-gpu-255-129-12-00.epc.baidu.com>
2019-05-21 05:22:28 +0000

    modify format

--- paddle/fluid/operators/deformable_psroi_pooling_op.h
+++ paddle/fluid/operators/deformable_psroi_pooling_op.h
@@ -1,13 +1,16 @@
-/* Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. */
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #pragma once
 #include <iostream>
@@ -175,7 +178,7 @@ class DeformablePSROIPoolCPUKernel : public framework::OpKernel<T> {
         trans_std, sample_per_part, output_dim, group_size, part_size,
         num_classes, channels_each_class, top_data, top_count_data,
         batch, roi_batch_id_data, rois);
-      }
+    }
 };
 
 template <typename T>
