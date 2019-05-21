f11950617d5e338206cf7f2b1c4c10d804e970d4
guru4elephant <35550832+guru4elephant@users.noreply.github.com>
2019-05-21 19:05:31 +0800

    Update nn.py

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -10612,6 +10612,8 @@ def get_tensor_from_selected_rows(x, name=None):
     Examples:
         .. code-block:: python
 
+	    import paddle.fluid as fluid
+	    
             b = fluid.default_main_program().global_block()
             input = b.create_var(name="X", dtype="float32", persistable=True, type=fluid.core.VarDesc.VarType.SELECTED_ROWS)
             out = fluid.layers.get_tensor_from_selected_rows(input)
