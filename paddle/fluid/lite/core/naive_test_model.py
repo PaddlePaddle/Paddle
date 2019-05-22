b5c93ec96a9f70c72e5452f9d2d1e5ef208f7940
tensor-tang <tangjian03@baidu.com>
2019-05-22 08:37:40 +0000

    update format

--- paddle/fluid/lite/core/naive_test_model.py
+++ paddle/fluid/lite/core/naive_test_model.py
@@ -1,3 +1,17 @@
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy
 import sys, os
 import numpy as np
@@ -26,8 +40,6 @@ data_1 = np.array(numpy.random.random([100, 100]), dtype='float32')
 
 #fluid.default_main_program().desc.
 
-
-
 #prog = fluid.compiler.CompiledProgram(fluid.default_main_program())
 prog = fluid.default_main_program()
 
@@ -36,11 +48,9 @@ prog = fluid.default_main_program()
 with open('main_program.pb', 'wb') as f:
     f.write(prog.desc.serialize_to_string())
 
-
 #outs = exe.run(program=prog, feed={'a':data_1, }, fetch_list=[cost])
 
 sys.exit(0)
 fluid.io.save_inference_model("./model2", [a.name], [a1], exe)
 
 print(numpy.array(outs))
-
