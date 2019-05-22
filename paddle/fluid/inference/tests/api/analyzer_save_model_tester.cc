f99aa0605f44e004db01013cbe71196344478c97
lijianshe02 <jslict09@gmail.com>
2019-05-22 06:13:37 +0000

    fix bug that saved optimal model path in test_analyzer_save_model conflict with test_analyzer_small_dam test=develop

--- paddle/fluid/inference/tests/api/analyzer_save_model_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_save_model_tester.cc
@@ -34,7 +34,7 @@ TEST(Analyzer, save_model) {
   AnalysisConfig cfg;
   SetConfig(&cfg);
   cfg.SetModel(FLAGS_infer_model + "/__model__", FLAGS_infer_model + "/param");
-  std::string optimModelPath = FLAGS_infer_model + "/saved_optim_model";
+  std::string optimModelPath = FLAGS_infer_model + "/only_for_save_test";
   mkdir(optimModelPath.c_str(), 0777);
   SaveOptimModel(&cfg, optimModelPath);
 
