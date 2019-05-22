f80a90bb5b603f547c0ecc6e6e4ef36c13ebba9d
Krzysztof Binias <krzysztof.binias@intel.com>
2019-05-22 09:14:07 +0200

    Enable square operator for the nGraph Bridge. (#17551)
    test=develop

--- python/paddle/fluid/tests/unittests/ngraph/test_activation_ngraph_op.py
+++ python/paddle/fluid/tests/unittests/ngraph/test_activation_ngraph_op.py
@@ -18,7 +18,7 @@ import unittest
 import numpy as np
 import paddle.fluid.core as core
 from paddle.fluid.tests.unittests.op_test import OpTest
-from paddle.fluid.tests.unittests.test_activation_op import TestAbs, TestSigmoid, TestRelu, TestTanh
+from paddle.fluid.tests.unittests.test_activation_op import TestAbs, TestSigmoid, TestSquare, TestRelu, TestTanh
 
 
 class TestNGRAPHReluDim4(TestRelu):
