58f7695ab2022319cc6a9e95348530496ed8d104
Qiao Longfei <qiaolongfei@baidu.com>
2019-05-23 15:35:08 +0800

    Async exe support communicator (#17386)
    Async exe support communicator

--- python/paddle/fluid/transpiler/distribute_transpiler.py
+++ python/paddle/fluid/transpiler/distribute_transpiler.py
@@ -158,7 +158,7 @@ class DistributeTranspilerConfig(object):
     wait_port = True
     # split the send recv var in runtime
     runtime_split_send_recv = False
-    sync_mode = None
+    sync_mode = True
 
 
 class DistributeTranspiler(object):
@@ -330,7 +330,7 @@ class DistributeTranspiler(object):
             return
 
         self.trainer_num = trainers
-        self.sync_mode = self.config.sync_mode if self.config.sync_mode else sync_mode
+        self.sync_mode = sync_mode
         self.trainer_id = trainer_id
         pserver_endpoints = pservers.split(",")
         self.pserver_endpoints = pserver_endpoints
