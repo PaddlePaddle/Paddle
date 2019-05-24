074a2c63b78d5e1e143858f5c3de108efd83f93e
Michał Gallus <michal.gallus@intel.com>
2019-05-24 16:51:22 +0200

    [MKL-DNN] Add Fully Connected Op for inference only(#15226)
    * fuse mul and elementwise add to fc
    
    * Reimplement the FC forward operator
    
    * Fix FC MKLDNN integration by transposing weights
    
    * Add FC MKLDNN Pass
    
    test=develop
    
    * FC MKLDNN Pass: change memcpy to std::copy
    
    * Fix MKLDNN FC handling of mismatch input and weights dims
    
    * Lower tolerance for MKL-DNN in resnet50 test
    
    test=develop
    
    * Adjust FC to support MKLDNN Op placement
    
    test=develop
    
    * Adjust Placement Op to set use_mkldnn attribute for graph
    
    test=develop
    
    * MKLDNN FC: fix weights format so that gemm version is called
    
    test=develop
    
    * FC MKLDNN: Remove tolerance decrease from tester_helper
    
    * FC MKL-DNN: Refactor the code, change input reorder to weight reorder
    
    * MKL-DNN FC: Introduce operator caching
    
    test=develop
    
    * FC MKL-DNN: Fix the tensor type in ExpectedKernelType
    
    test=develop
    
    * FC MKL-DNN: fix style changes
    
    test=develop
    
    * FC MKL-DNN: fallback to native on non-supported dim sizes
    
    test=develop
    
    * FC MKLDNN: fix CMake paths
    
    test=develop
    
    * FC MKLDNN: Refine placement pass graph mkldnn attribute
    
    test=develop
    
    * Fix Transpiler error for fuse_conv_eltwise
    
    test=develop
    
    * Fix missing STL includes in files
    
    test=develop
    
    * FC MKL-DNN: Enable new output size computation
    
    Also, refine pass to comply with newest interface.
    test=develop
    
    * FC MKL-DNN: enable only when fc_mkldnn_pass is enabled
    
    * FC MKL-DNN: Allow Weights to use oi or io format
    
    * FC MKL-DNN: Adjust UT to work with correct dims
    
    test=develop
    
    * Enable MKL DEBUG for resnet50 analyzer
    
    test=develop
    
    * FC MKL-DNN: Improve Hashing function
    
    test=develop
    
    * FC MKL-DNN: Fix shape for fc weights in transpiler
    
    * FC MKL-DNN: Update input pointer in re-used fc primitive
    
    * Add log for not handling fc fuse for unsupported dims
    
    test=develop
    
    * FC MKL-DNN: Move transpose from pass to Op Kernel
    
    test=develop
    
    * FC MKL-DNN: Disable transpose in unit test
    
    test=develop
    
    * FC MKL-DNN: Remove fc_mkldnn_pass from default list
    
    * Correct Flag for fake data analyzer tests
    
    test=develop
    
    * FC MKL-DNN: Add comment about fc mkldnn pass disablement
    
    test=develop
    
    * FC MKL-DNN: Disable fc in int8 tests
    
    test=develop

--- paddle/fluid/inference/tests/api/analyzer_resnet50_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_resnet50_tester.cc
@@ -48,6 +48,7 @@ void profile(bool use_mkldnn = false) {
 
   if (use_mkldnn) {
     cfg.EnableMKLDNN();
+    cfg.pass_builder()->AppendPass("fc_mkldnn_pass");
   }
   std::vector<std::vector<PaddleTensor>> outputs;
 
@@ -79,6 +80,7 @@ void compare(bool use_mkldnn = false) {
   SetConfig(&cfg);
   if (use_mkldnn) {
     cfg.EnableMKLDNN();
+    cfg.pass_builder()->AppendPass("fc_mkldnn_pass");
   }
 
   std::vector<std::vector<PaddleTensor>> input_slots_all;
