7ed9d903e33cb276782d08d95fa7d42ad8414c0d
tensor-tang <tangjian03@baidu.com>
2019-05-31 17:29:19 +0800

    enbale lite ci mobile (#17760)

--- paddle/fluid/lite/tools/build.sh
+++ paddle/fluid/lite/tools/build.sh
@@ -17,45 +17,18 @@ function cmake_gpu {
 }
 
 function cmake_arm {
-    # ARM_TARGET_OS="android" , "armlinux"
-    # ARM_TARGET_ARCH_ABI = "arm64-v8a", "armeabi-v7a" ,"armeabi-v7a-hf"
-    for os in "android" "armlinux" ; do
-        for abi in "arm64-v8a" "armeabi-v7a" "armeabi-v7a-hf" ; do
-            if [[ ${abi} == "armeabi-v7a-hf" ]]; then
-                echo "armeabi-v7a-hf is not supported on both android and armlinux"
-                continue
-            fi
-
-            if [[ ${os} == "armlinux" && ${abi} == "armeabi-v7a" ]]; then
-                echo "armeabi-v7a is not supported on armlinux yet"
-                continue
-            fi
-
-            echo "Build for ${os} ${abi}"
-
-            build_dir=build.lite.${os}.${abi}
-            mkdir -p $build_dir
-            cd $build_dir
-
-            cmake .. \
-                -DWITH_GPU=OFF \
-                -DWITH_LITE=ON \
-                -DLITE_WITH_CUDA=OFF \
-                -DLITE_WITH_X86=OFF \
-                -DLITE_WITH_ARM=ON \
-                -DLITE_WITH_LIGHT_WEIGHT_FRAMEWORK=ON \
-                -DWITH_TESTING=ON \
-                -DWITH_MKL=OFF \
-                -DARM_TARGET_OS=${os} -DARM_TARGET_ARCH_ABI=${abi} 
-
-            make test_fc_compute_arm -j
-            make test_softmax_compute_arm -j
-            make cxx_api_lite_bin -j
-            cd -
-
-        done
-    done
-
+    # $1: ARM_TARGET_OS in "android" , "armlinux"
+    # $2: ARM_TARGET_ARCH_ABI in "arm64-v8a", "armeabi-v7a" ,"armeabi-v7a-hf"
+    cmake .. \
+        -DWITH_GPU=OFF \
+        -DWITH_MKL=OFF \
+        -DWITH_LITE=ON \
+        -DLITE_WITH_CUDA=OFF \
+        -DLITE_WITH_X86=OFF \
+        -DLITE_WITH_ARM=ON \
+        -DLITE_WITH_LIGHT_WEIGHT_FRAMEWORK=ON \
+        -DWITH_TESTING=ON \
+        -DARM_TARGET_OS=$1 -DARM_TARGET_ARCH_ABI=$2
 }
 
 function build {
@@ -74,10 +47,37 @@ function test_lite {
     done
 }
 
-# Run test on mobile
-function test_mobile {
-    # TODO(XXX) Implement this
+port_armv8=5554
+port_armv7=5556
+
+# Run test on android
+function test_lite_android {
     local file=$1
+    local adb_abi=$2
+    local port=
+    if [[ ${adb_abi} == "armeabi-v7a" ]]; then
+        port=${port_armv7}
+    fi
+
+    if [[ ${adb_abi} == "arm64-v8a" ]]; then
+        port=${port_armv8}
+    fi
+    if [[ "${port}x" == "x" ]]; then
+        echo "Port can not be empty"
+        exit 1
+    fi
+
+    echo "file: ${file}"
+    # push all to adb and test
+    adb_work_dir="/data/local/tmp"
+    skip_list="test_model_parser_lite"
+    for _test in $(cat $file); do
+        [[ $skip_list =~ (^|[[:space:]])$_test($|[[:space:]]) ]] && continue || echo 'skip $_test'
+        testpath=$(find ./paddle/fluid -name ${_test})
+        adb -s emulator-${port} push ${testpath} ${adb_work_dir}
+        adb -s emulator-${port} shell chmod +x "${adb_work_dir}/${_test}"
+        adb -s emulator-${port} shell "./${adb_work_dir}/${_test}"
+    done
 }
 
 # Build the code and run lite server tests. This is executed in the CI system.
@@ -88,6 +88,55 @@ function build_test_server {
     cmake_x86_for_CI
     build $TESTS_FILE
     test_lite $TESTS_FILE
+}
+
+# Build the code and run lite server tests. This is executed in the CI system.
+function build_test_arm {
+    adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
+    # start android arm64-v8a armeabi-v7a emulators first
+    echo n | avdmanager create avd -f -n paddle-armv8 -k "system-images;android-24;google_apis;arm64-v8a"
+    echo -ne '\n' | ${ANDROID_HOME}/emulator/emulator -avd paddle-armv8 -noaudio -no-window -gpu off -verbose -port ${port_armv8} &
+    sleep 1m
+    echo n | avdmanager create avd -f -n paddle-armv7 -k "system-images;android-24;google_apis;armeabi-v7a"
+    echo -ne '\n' | ${ANDROID_HOME}/emulator/emulator -avd paddle-armv7 -noaudio -no-window -gpu off -verbose -port ${port_armv7} &
+    sleep 1m
+
+    for os in "android" "armlinux" ; do
+        for abi in "arm64-v8a" "armeabi-v7a" "armeabi-v7a-hf" ; do
+            if [[ ${abi} == "armeabi-v7a-hf" ]]; then
+                echo "armeabi-v7a-hf is not supported on both android and armlinux"
+                continue
+            fi
+
+            if [[ ${os} == "armlinux" && ${abi} == "armeabi-v7a" ]]; then
+                echo "armeabi-v7a is not supported on armlinux yet"
+                continue
+            fi
+
+            build_dir=build.lite.${os}.${abi}
+            mkdir -p $build_dir
+            cd $build_dir
+
+            cmake_arm ${os} ${abi}
+            build $TESTS_FILE
+
+            if [[ ${os} == "android" ]]; then
+                adb_abi=${abi}
+                if [[ ${adb_abi} == "armeabi-v7a-hf" ]]; then
+                    adb_abi="armeabi-v7a"
+                fi
+                if [[ ${adb_abi} == "armeabi-v7a" ]]; then
+                    # skip v7 tests
+                    continue
+                fi
+                test_lite_android $TESTS_FILE ${adb_abi}
+                # armlinux need in another docker
+            fi
+            cd -
+        done
+    done
+    adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
+    echo "Done"
 }
 
 ############################# MAIN #################################
@@ -141,6 +190,10 @@ function main {
                 ;;
             build_test_server)
                 build_test_server
+                shift
+                ;;
+            build_test_arm)
+                build_test_arm
                 shift
                 ;;
             *)
