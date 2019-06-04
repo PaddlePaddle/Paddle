e540ccd05e83dc8da196d702649b8477233d7a86
chengjuntao <806512756@qq.com>
2019-06-04 13:51:33 +0000

    add test_layers.py

--- python/paddle/fluid/tests/unittests/test_layers.py
+++ python/paddle/fluid/tests/unittests/test_layers.py
@@ -1991,6 +1991,36 @@ class TestBook(LayerTest):
                     padding=1)
                 return (out)
 
+    def test_deform_psroi_pooling(self):
+        with program_guard(fluid.default_main_program(),
+                           fluid.default_startup_program()):
+            input = layers.data(
+                name='input',
+                shape=[2, 3, 64, 64],
+                dtype='float32',
+                append_batch_size=False)
+            rois = layers.data(
+                name="rois", shape=[4], dtype='float32', lod_level=1)
+            trans = layers.data(
+                name="trans",
+                shape=[2, 3, 64, 64],
+                dtype='float32',
+                append_batch_size=False)
+            out = layers.deformable_psroi_pooling(
+                input=input,
+                rois=rois,
+                trans=trans,
+                no_trans=0,
+                spatial_scale=1.0,
+                output_channels=3,
+                group_size=(1, 1),
+                pooled_height=8,
+                pooled_width=8,
+                part_size=(8, 8),
+                sample_per_part=4,
+                trans_std=0.1)
+        return (out)
+
 
 if __name__ == '__main__':
     unittest.main()
