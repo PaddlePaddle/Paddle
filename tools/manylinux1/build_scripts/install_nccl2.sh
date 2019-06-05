3556e8ee55622a3faebff4d8a8ce7a453ec2ee71
Huihuang Zheng <zhhy409923957@163.com>
2019-06-05 18:03:23 +0800

    Fix 2 bugs for CI Cuda9/10 tasks (#17861)
    The detail of 2 bugs:
    
    tools/manylinux1/Dockerfile cannot build Cuda9 Cuda10 image on
    CI. Fixed it and tested manually. Will test on CI after this PR is
    merged.
    
    tools/manylinux1/build_scripts/install_nccl2.sh doesn't have
    Cuda10 version. Added it
    
    test=develop

--- tools/manylinux1/build_scripts/install_nccl2.sh
+++ tools/manylinux1/build_scripts/install_nccl2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 VERSION=$(nvcc --version | grep release | grep -oEi "release ([0-9]+)\.([0-9])"| sed "s/release //")
-if [ "$VERSION" == "9.0" ]; then
+if [ "$VERSION" == "10.0" ]; then
+  DEB="nccl-repo-ubuntu1604-2.4.7-ga-cuda10.0_1-1_amd64.deb"
+elif [ "$VERSION" == "9.0" ]; then
   DEB="nccl-repo-ubuntu1604-2.1.15-ga-cuda9.0_1-1_amd64.deb"
-  URL="http://nccl2-deb.gz.bcebos.com/nccl-repo-ubuntu1604-2.1.15-ga-cuda9.0_1-1_amd64.deb"
 else
   DEB="nccl-repo-ubuntu1604-2.1.15-ga-cuda8.0_1-1_amd64.deb"
-  URL="http://nccl2-deb.gz.bcebos.com/nccl-repo-ubuntu1604-2.1.15-ga-cuda8.0_1-1_amd64.deb"
 fi
+
+URL="http://nccl2-deb.gz.bcebos.com/$DEB"
 
 DIR="/nccl2"
 mkdir -p $DIR
