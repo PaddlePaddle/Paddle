8ac013e54cb79be1848eec8edd2aa0546a431369
gongweibao <gongweibao@baidu.com>
2019-06-08 02:51:39 +0000

    fixproxyenv test=develop

--- python/paddle/distributed/launch.py
+++ python/paddle/distributed/launch.py
@@ -173,7 +173,9 @@ def start_procs(args):
             "PADDLE_CURRENT_ENDPOINT":
             "%s:%d" % (current_node_ip, args.started_port + i),
             "PADDLE_TRAINERS_NUM": "%d" % nranks,
-            "PADDLE_TRAINER_ENDPOINTS": trainers_endpoints
+            "PADDLE_TRAINER_ENDPOINTS": trainers_endpoints,
+            "http_proxy": "",
+            "https_proxy": ""
         })
 
         cmd = [sys.executable, "-u", args.training_script
