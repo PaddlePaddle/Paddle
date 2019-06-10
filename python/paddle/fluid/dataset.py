e3afa686488a7ed17968e914d0d523a3a1494e23
xjqbest <173596896@qq.com>
2019-06-10 22:02:29 +0800

    use channel instead of vector/BlockingQueue in Dataset fix thread num bug of Dataset add Channel, Archive in paddle/fluid/framework change dataset from shared_ptr to unique_ptr in pybind test=develop

--- python/paddle/fluid/dataset.py
+++ python/paddle/fluid/dataset.py
@@ -71,6 +71,7 @@ class DatasetBase(object):
         self.proto_desc.pipe_command = "cat"
         self.dataset = core.Dataset("MultiSlotDataset")
         self.thread_num = 0
+        self.filelist = []
 
     def set_pipe_command(self, pipe_command):
         """
@@ -139,6 +140,7 @@ class DatasetBase(object):
             filelist(list): file list
         """
         self.dataset.set_filelist(filelist)
+        self.filelist = filelist
 
     def set_use_var(self, var_list):
         """
@@ -193,7 +195,15 @@ class DatasetBase(object):
         Set data_feed_desc before load or shuffle,
         user no need to call this function.
         """
+        if self.thread_num > len(self.filelist):
+            self.thread_num = len(self.filelist)
+        self.dataset.set_thread_num(self.thread_num)
         self.dataset.set_data_feed_desc(self.desc())
+        self.dataset.create_channel()
+        self.dataset.create_readers()
+
+    def _finish_to_run(self):
+        self.dataset.destroy_readers()
 
     def desc(self):
         """
@@ -242,6 +252,39 @@ class InMemoryDataset(DatasetBase):
         """
         self._prepare_to_run()
         self.dataset.load_into_memory()
+
+    def preload_into_memory(self):
+        """
+        Load data into memory in async mode
+
+        Examples:
+            .. code-block:: python
+
+              import paddle.fluid as fluid
+              dataset = fluid.DatasetFactory().create_dataset("InMemoryDataset")
+              filelist = ["a.txt", "b.txt"]
+              dataset.set_filelist(filelist)
+              dataset.preload_into_memory()
+              dataset.wait_preload_done()
+        """
+        self._prepare_to_run()
+        self.dataset.preload_into_memory()
+
+    def wait_preload_done(self):
+        """
+        Wait preload_into_memory done
+
+        Examples:
+            .. code-block:: python
+
+            import paddle.fluid as fluid
+            dataset = fluid.DatasetFactory().create_dataset("InMemoryDataset")
+            filelist = ["a.txt", "b.txt"]
+            dataset.set_filelist(filelist)
+            dataset.preload_into_memory()
+            dataset.wait_preload_done()
+        """
+        self.dataset.wait_preload_done()
 
     def local_shuffle(self):
         """
