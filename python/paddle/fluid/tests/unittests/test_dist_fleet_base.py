e98437c662546d29f253e1c8d839810459c044b9
tangwei12 <tangwei12@baidu.com>
2019-06-11 14:53:42 +0800

    remove distributed to distribute, test=develop

--- python/paddle/fluid/tests/unittests/test_dist_fleet_base.py
+++ python/paddle/fluid/tests/unittests/test_dist_fleet_base.py
@@ -32,7 +32,7 @@ import numpy as np
 
 import paddle.fluid as fluid
 import paddle.fluid.incubate.fleet.base.role_maker as role_maker
-from paddle.fluid.incubate.fleet.parameter_server.distributed_transpiler import fleet
+from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler import fleet
 from paddle.fluid.transpiler.distribute_transpiler import DistributeTranspilerConfig
 
 RUN_STEP = 5
