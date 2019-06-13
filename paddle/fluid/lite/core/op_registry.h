25eb84a33c7424a139cc4b98a4cafcd3bc857bf4
tensor-tang <tangjian03@baidu.com>
2019-06-13 14:54:58 +0800

    fix and speedup ci conv test

--- paddle/fluid/lite/core/op_registry.h
+++ paddle/fluid/lite/core/op_registry.h
@@ -153,12 +153,9 @@ class KernelRegistor : public lite::Registor<KernelType> {
  public:
   KernelRegistor(const std::string &op_type, const std::string &alias)
       : Registor<KernelType>([=] {
-<<<<<<< HEAD
-=======
           VLOG(3) << "Register kernel " << op_type << " for "
                   << TargetToStr(target) << " " << PrecisionToStr(precision)
                   << " " << DataLayoutToStr(layout) << " alias " << alias;
->>>>>>> gitlab/develop
           KernelRegistry::Global().Register<target, precision, layout>(
               op_type, [=]() -> std::unique_ptr<KernelType> {
                 std::unique_ptr<KernelType> x(new KernelType);
