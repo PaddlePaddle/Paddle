bcab7c3562df976cf5ed9b13d467cf831d73ece5
hong19860320 <9973393+hong19860320@users.noreply.github.com>
2019-06-15 11:38:30 +0000

    change pool to pool2d to fix the unit test of pooling arm kernel test=develop

--- paddle/fluid/lite/kernels/arm/pool_compute_test.cc
+++ paddle/fluid/lite/kernels/arm/pool_compute_test.cc
@@ -261,8 +261,8 @@ TEST(pool_arm, compute) {
 }
 
 TEST(pool, retrive_op) {
-  auto pool =
-      KernelRegistry::Global().Create<TARGET(kARM), PRECISION(kFloat)>("pool");
+  auto pool = KernelRegistry::Global().Create<TARGET(kARM), PRECISION(kFloat)>(
+      "pool2d");
   ASSERT_FALSE(pool.empty());
   ASSERT_TRUE(pool.front());
 }
