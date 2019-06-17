ba75031b7b64c224ee17803b5ad33c2ec7092cb0
yuanshuai <ysh329@sina.com>
2019-06-17 08:40:31 +0000

    [LITE][ARM] Add concat kernel of arm cpu. test=develop

--- paddle/fluid/lite/arm/math/funcs.h
+++ paddle/fluid/lite/arm/math/funcs.h
@@ -18,6 +18,7 @@
 #include <cmath>
 
 #include "paddle/fluid/lite/arm/math/activation.h"
+#include "paddle/fluid/lite/arm/math/concat.h"
 #include "paddle/fluid/lite/arm/math/conv_depthwise.h"
 #include "paddle/fluid/lite/arm/math/conv_direct.h"
 #include "paddle/fluid/lite/arm/math/conv_gemmlike.h"
