1a87d31538c528664c03a1df2888e83e06840582
JiabinYang <marsyang199376@gmail.com>
2019-06-19 13:30:04 +0000

    test=develop, find right num of cuda device

--- python/paddle/fluid/install_check.py
+++ python/paddle/fluid/install_check.py
@@ -11,35 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from .framework import Program, program_guard, unique_name, default_startup_program
-from .param_attr import ParamAttr
-from .initializer import Constant
-from . import layers
-from . import backward
-from .dygraph import Layer, nn
-from . import executor
-from . import optimizer
-from . import core
-from . import compiler
-import logging
-import numpy as np
 import os
-
-__all__ = ['run_check']
-
-
-class SimpleLayer(Layer):
-    def __init__(self, name_scope):
-        super(SimpleLayer, self).__init__(name_scope)
-        self._fc1 = nn.FC(self.full_name(),
-                          3,
-                          param_attr=ParamAttr(initializer=Constant(value=0.1)))
-
-    def forward(self, inputs):
-        x = self._fc1(inputs)
-        x = layers.reduce_sum(x)
-        return x
 
 
 def process_env():
@@ -65,6 +37,37 @@ def process_env():
             return [0]
 
 
+device_list = process_env()
+
+from .framework import Program, program_guard, unique_name, default_startup_program
+from .param_attr import ParamAttr
+from .initializer import Constant
+from . import layers
+from . import backward
+from .dygraph import Layer, nn
+from . import executor
+from . import optimizer
+from . import core
+from . import compiler
+import logging
+import numpy as np
+
+__all__ = ['run_check']
+
+
+class SimpleLayer(Layer):
+    def __init__(self, name_scope):
+        super(SimpleLayer, self).__init__(name_scope)
+        self._fc1 = nn.FC(self.full_name(),
+                          3,
+                          param_attr=ParamAttr(initializer=Constant(value=0.1)))
+
+    def forward(self, inputs):
+        x = self._fc1(inputs)
+        x = layers.reduce_sum(x)
+        return x
+
+
 def run_check():
     ''' intall check to verify if install is success
 
@@ -72,11 +75,12 @@ def run_check():
     '''
     print("Running Verify Fluid Program ... ")
     use_cuda = False if not core.is_compiled_with_cuda() else True
-    if use_cuda:
-        device_list = process_env()
     place = core.CPUPlace() if not core.is_compiled_with_cuda(
     ) else core.CUDAPlace(0)
     np_inp = np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)
+    if use_cuda:
+        for i in core.get_cuda_device_count():
+            np_inp = np.stack(np_inp, axis=0)
 
     def test_parallerl_exe():
         train_prog = Program()
