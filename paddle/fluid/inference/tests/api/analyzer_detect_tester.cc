6a97049076230ef136f586dfdb16811a18802183
luotao1 <luotao02@baidu.com>
2019-06-26 19:17:04 +0800

    load input data one by one

--- paddle/fluid/inference/tests/api/analyzer_detect_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_detect_tester.cc
@@ -17,6 +17,7 @@ limitations under the License. */
 #include <iostream>
 #include "paddle/fluid/inference/tests/api/tester_helper.h"
 DEFINE_string(infer_shape, "", "data shape file");
+DEFINE_int32(sample, 1, "number of sample");
 
 namespace paddle {
 namespace inference {
@@ -45,7 +46,8 @@ Record ProcessALine(const std::string &line, const std::string &shape_line) {
   }
   // VLOG(3) << "data size " << record.data.size();
   // VLOG(3) << "data shape size " << record.shape.size();
-  VLOG(2) << "data shape size " << record.shape[3];
+  // VLOG(2) << "data shape size " << record.shape[3];
+  LOG(INFO) << "data shape size " << record.shape[3];
   return record;
 }
 
@@ -57,28 +59,19 @@ void SetConfig(AnalysisConfig *cfg) {
   cfg->SetCpuMathLibraryNumThreads(FLAGS_paddle_num_threads);
 }
 
-void SetInput(std::vector<std::vector<PaddleTensor>> *inputs) {
-  std::string line;
-  std::ifstream file(FLAGS_infer_data);
-  std::string shape_line;
-  std::ifstream infer_file(FLAGS_infer_shape);
+void SetInput(std::vector<std::vector<PaddleTensor>> *inputs,
+              const std::string &line, const std::string &shape_line) {
+  auto record = ProcessALine(line, shape_line);
 
-  int iteration = FLAGS_test_all_data ? 1000 : 1;
-  for (int k = 0; k < iteration; k++) {
-    std::getline(file, line);
-    std::getline(infer_file, shape_line);
-    auto record = ProcessALine(line, shape_line);
-
-    PaddleTensor input;
-    input.shape = record.shape;
-    input.dtype = PaddleDType::FLOAT32;
-    size_t input_size = record.data.size() * sizeof(float);
-    input.data.Resize(input_size);
-    memcpy(input.data.data(), record.data.data(), input_size);
-    std::vector<PaddleTensor> input_slots;
-    input_slots.assign({input});
-    (*inputs).emplace_back(input_slots);
-  }
+  PaddleTensor input;
+  input.shape = record.shape;
+  input.dtype = PaddleDType::FLOAT32;
+  size_t input_size = record.data.size() * sizeof(float);
+  input.data.Resize(input_size);
+  memcpy(input.data.data(), record.data.data(), input_size);
+  std::vector<PaddleTensor> input_slots;
+  input_slots.assign({input});
+  (*inputs).emplace_back(input_slots);
 }
 
 // Easy for profiling independently.
@@ -92,11 +85,38 @@ void profile(bool use_mkldnn = false) {
   }
   // cfg.pass_builder()->TurnOnDebug();
   std::vector<std::vector<PaddleTensor>> outputs;
-
   std::vector<std::vector<PaddleTensor>> input_slots_all;
-  SetInput(&input_slots_all);
-  TestPrediction(reinterpret_cast<const PaddlePredictor::Config *>(&cfg),
-                 input_slots_all, &outputs, FLAGS_num_threads);
+
+  Timer run_timer;
+  double elapsed_time = 0;
+
+  int iterations = FLAGS_sample;
+  int num_times = FLAGS_repeat;
+  auto predictor = CreatePaddlePredictor<AnalysisConfig>(cfg);
+  outputs.resize(iterations);
+
+  for (int j = 0; j < num_times; j++) {
+    std::ifstream file(FLAGS_infer_data);
+    std::ifstream infer_file(FLAGS_infer_shape);
+    std::string line;
+    std::string shape_line;
+
+    for (int i = 0; i < iterations; i++) {
+      std::getline(file, line);
+      std::getline(infer_file, shape_line);
+      SetInput(&input_slots_all, line, shape_line);
+
+      run_timer.tic();
+      predictor->Run(input_slots_all[i], &outputs[i], FLAGS_batch_size);
+      elapsed_time += run_timer.toc();
+    }
+    file.close();
+    infer_file.close();
+  }
+
+  auto batch_latency = elapsed_time / (iterations * num_times);
+  PrintTime(FLAGS_batch_size, num_times, FLAGS_num_threads, 0, batch_latency,
+            iterations, VarType::FP32);
 }
 
 TEST(Analyzer_vis, profile) { profile(); }
@@ -104,46 +124,6 @@ TEST(Analyzer_vis, profile) { profile(); }
 #ifdef PADDLE_WITH_MKLDNN
 TEST(Analyzer_vis, profile_mkldnn) { profile(true /* use_mkldnn */); }
 #endif
-
-// Check the fuse status
-TEST(Analyzer_vis, fuse_statis) {
-  AnalysisConfig cfg;
-  SetConfig(&cfg);
-  int num_ops;
-  auto predictor = CreatePaddlePredictor<AnalysisConfig>(cfg);
-  GetFuseStatis(predictor.get(), &num_ops);
-}
-
-// Compare result of NativeConfig and AnalysisConfig
-void compare(bool use_mkldnn = false) {
-  AnalysisConfig cfg;
-  SetConfig(&cfg);
-  if (use_mkldnn) {
-    cfg.EnableMKLDNN();
-    cfg.pass_builder()->AppendPass("fc_mkldnn_pass");
-  }
-
-  std::vector<std::vector<PaddleTensor>> input_slots_all;
-  SetInput(&input_slots_all);
-  CompareNativeAndAnalysis(
-      reinterpret_cast<const PaddlePredictor::Config *>(&cfg), input_slots_all);
-}
-
-TEST(Analyzer_vis, compare) { compare(); }
-#ifdef PADDLE_WITH_MKLDNN
-TEST(Analyzer_vis, compare_mkldnn) { compare(true /* use_mkldnn */); }
-#endif
-
-// Compare Deterministic result
-TEST(Analyzer_vis, compare_determine) {
-  AnalysisConfig cfg;
-  SetConfig(&cfg);
-
-  std::vector<std::vector<PaddleTensor>> input_slots_all;
-  SetInput(&input_slots_all);
-  CompareDeterministic(reinterpret_cast<const PaddlePredictor::Config *>(&cfg),
-                       input_slots_all);
-}
 
 }  // namespace analysis
 }  // namespace inference
