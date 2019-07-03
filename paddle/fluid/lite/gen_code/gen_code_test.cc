e48b1c7e8bcb2e6e66fcce4df3fba1a3865bb850
sangoly <liujinquan@baidu.com>
2019-07-03 10:15:10 +0000

    add gen code arm ci test

--- paddle/fluid/lite/gen_code/gen_code_test.cc
+++ paddle/fluid/lite/gen_code/gen_code_test.cc
@@ -19,9 +19,10 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include "paddle/fluid/lite/api/paddle_use_kernels.h"
+#include "paddle/fluid/lite/api/paddle_use_ops.h"
 #include "paddle/fluid/lite/core/compatible_tensor.h"
 #include "paddle/fluid/lite/core/context.h"
-#include "paddle/fluid/lite/core/op_registry.h"
 #include "paddle/fluid/lite/core/scope.h"
 #include "paddle/fluid/lite/model_parser/cpp/op_desc.h"
 #include "paddle/fluid/lite/model_parser/model_parser.h"
@@ -48,15 +49,25 @@ TEST(gen_code, manual) {
 
   // Set weights.
   std::vector<float> w0_data({0, 1, 2, 3});
+  std::vector<float> a_data({0, 1, 2, 3});
+#ifdef LITE_WITH_ARM
+  w0->Assign<float, lite::DDim, TARGET(kARM)>(
+      w0_data.data(), lite::DDim{std::vector<int64_t>({2, 2})});
+  a->Assign<float, lite::DDim, TARGET(kARM)>(
+      a_data.data(), lite::DDim{std::vector<int64_t>({2, 2})});
+#else
   w0->Assign<float, lite::DDim, TARGET(kX86)>(
       w0_data.data(), lite::DDim{std::vector<int64_t>({2, 2})});
-
-  std::vector<float> a_data({0, 1, 2, 3});
   a->Assign<float, lite::DDim, TARGET(kX86)>(
       a_data.data(), lite::DDim{std::vector<int64_t>({2, 2})});
+#endif
 
   std::vector<Place> valid_places({
+#ifdef LITE_WITH_ARM
+      Place{TARGET(kARM), PRECISION(kFloat)},
+#else
       Place{TARGET(kX86), PRECISION(kFloat)},
+#endif
       Place{TARGET(kHost), PRECISION(kFloat)},
       Place{TARGET(kHost), PRECISION(kAny)},
   });
@@ -71,7 +82,11 @@ TEST(gen_code, manual) {
 
   mul_op->Attach(mul_op_desc, &tmp_scope);
   auto mul_kernel = std::move(mul_op->CreateKernels(valid_places).front());
+#ifdef LITE_WITH_ARM
+  auto fc_ctx = ContextScheduler::Global().NewContext(TARGET(kARM));
+#else
   auto fc_ctx = ContextScheduler::Global().NewContext(TARGET(kX86));
+#endif
   mul_op->CheckShape();
   mul_op->InferShape();
   mul_kernel->SetContext(std::move(fc_ctx));
@@ -94,7 +109,11 @@ TEST(gen_code, auto_gen) {
   op0.SetAttr<std::string>("desc", "this is a desc");
   op0.SetAttr<int>("x_col", 1);
   op0.SetAttr<int>("y_col", 2);
+#ifdef LITE_WITH_ARM
+  op0.SetAttr<std::string>(kKernelTypeAttr, "arm");
+#else
   op0.SetAttr<std::string>(kKernelTypeAttr, "x86");
+#endif
 
   gencode::Module module;
   module.AddHeaderIncludeGenCode();
@@ -134,12 +153,3 @@ TEST(gen_code, optimized_program) {
 }  // namespace gencode
 }  // namespace lite
 }  // namespace paddle
-
-USE_LITE_OP(mul);
-#ifdef LITE_WITH_X86
-USE_LITE_KERNEL(mul, kX86, kFloat, kNCHW, def);
-#endif
-
-#ifdef LITE_WITH_ARM
-USE_LITE_KERNEL(mul, kARM, kFloat, kNCHW, def);
-#endif
