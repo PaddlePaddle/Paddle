6f2a98d1eda17fcad6ea3f9e35f9c7303bb9c492
Jacek Czaja <jacek.czaja@intel.com>
2019-07-10 09:58:29 +0200

    Activations MKLDNN ops refactoring (#18191)

--- paddle/fluid/platform/mkldnn_reuse.h
+++ paddle/fluid/platform/mkldnn_reuse.h
@@ -309,6 +309,121 @@ class SumMKLDNNHandler : public MKLDNNHandler {
   std::shared_ptr<mkldnn::sum::primitive_desc> sum_pd_;
 };
 
+class ActivationMKLDNNHandler : public MKLDNNHandler {
+ public:
+  ActivationMKLDNNHandler(const platform::MKLDNNDeviceContext& dev_ctx,
+                          mkldnn::engine engine, const std::string& base_key)
+      : platform::MKLDNNHandler(dev_ctx, engine, base_key) {}
+
+  std::shared_ptr<mkldnn::eltwise_forward::primitive_desc>
+  AcquireActivationPrimitiveDescriptor(mkldnn::prop_kind prop_kind,
+                                       mkldnn::algorithm algorithm,
+                                       const mkldnn::memory::desc& md,
+                                       float alpha, float beta) {
+    // Activation PD has to be passed to Grad op that
+    // may be executed by diffrent thread, hence
+    // for that one we use key that does not contain TID
+    const std::string key_activation_pd = key_common_ + "@activation_pd";
+    activation_pd_ =
+        std::static_pointer_cast<mkldnn::eltwise_forward::primitive_desc>(
+            dev_ctx_.GetBlob(key_activation_pd));
+    if (activation_pd_ == nullptr) {
+      static std::mutex acquire_barrier;
+      std::lock_guard<std::mutex> block_threads_until_finish_this_job(
+          acquire_barrier);
+
+      activation_pd_ =
+          std::static_pointer_cast<mkldnn::eltwise_forward::primitive_desc>(
+              dev_ctx_.GetBlob(key_activation_pd));
+      if (activation_pd_ == nullptr) {
+        auto activation_desc = mkldnn::eltwise_forward::desc(
+            prop_kind, algorithm, md, alpha, beta);
+
+        activation_pd_.reset(new mkldnn::eltwise_forward::primitive_desc(
+            activation_desc, engine_));
+        dev_ctx_.SetBlob(key_activation_pd, activation_pd_);
+      }
+    }
+    return activation_pd_;
+  }
+
+  std::shared_ptr<mkldnn::eltwise_backward::primitive_desc>
+  AcquireActivationBackwardPrimitiveDescriptor(
+      mkldnn::algorithm algorithm, const mkldnn::memory::desc& diff_dst_md,
+      const mkldnn::memory::desc& src_md, float alpha, float beta) {
+    const std::string key_activation_pd = key_common_ + "@activation_pd";
+    const std::string key_activation_bwd_pd = key_ + "@activation_bwd_pd";
+    activation_bwd_pd_ =
+        std::static_pointer_cast<mkldnn::eltwise_backward::primitive_desc>(
+            dev_ctx_.GetBlob(key_activation_bwd_pd));
+    if (activation_bwd_pd_ == nullptr) {
+      activation_pd_ =
+          std::static_pointer_cast<mkldnn::eltwise_forward::primitive_desc>(
+              dev_ctx_.GetBlob(key_activation_pd));
+      // PD from FWD op has to exist.
+      PADDLE_ENFORCE(activation_pd_ != nullptr,
+                     "Eltwise MKL-DNN not found in cache!");
+      auto backward_desc = mkldnn::eltwise_backward::desc(
+          algorithm, diff_dst_md, src_md, alpha, beta);
+      activation_bwd_pd_.reset(new mkldnn::eltwise_backward::primitive_desc(
+          backward_desc, engine_, *activation_pd_));
+      dev_ctx_.SetBlob(key_activation_bwd_pd, activation_bwd_pd_);
+    }
+    return activation_bwd_pd_;
+  }
+
+  std::shared_ptr<mkldnn::eltwise_forward> AcquireActivation(
+      std::shared_ptr<mkldnn::memory> dst_memory_p,
+      std::shared_ptr<mkldnn::memory> src_memory_p) {
+    /*Generate key*/
+    auto prim_key = key_ + "@eltwise_p";
+
+    auto eltwise_p = std::static_pointer_cast<mkldnn::eltwise_forward>(
+        dev_ctx_.GetBlob(prim_key));
+    if (eltwise_p == nullptr) {
+      eltwise_p = std::make_shared<mkldnn::eltwise_forward>(
+          *activation_pd_, *(src_memory_p), *(dst_memory_p));
+      dev_ctx_.SetBlob(prim_key, eltwise_p);
+    }
+
+    return eltwise_p;
+  }
+
+  // TODO(jczaja): Merge all AcquireDstMemoryFromPrimitive into one
+  std::shared_ptr<mkldnn::memory> AcquireDstMemoryFromPrimitive(void* ptr) {
+    return this->AcquireMemoryFromPrimitive(
+        activation_pd_->dst_primitive_desc(), ptr, "@dst_mem_p");
+  }
+
+  std::shared_ptr<mkldnn::memory> AcquireDiffSrcMemoryFromPrimitive(void* ptr) {
+    return this->AcquireMemoryFromPrimitive(
+        activation_bwd_pd_->diff_src_primitive_desc(), ptr, "@diff_src_mem_p");
+  }
+
+  std::shared_ptr<mkldnn::eltwise_backward> AcquireActivationBackward(
+      std::shared_ptr<mkldnn::memory> diff_src_memory_p,
+      std::shared_ptr<mkldnn::memory> diff_dst_memory_p,
+      std::shared_ptr<mkldnn::memory> src_memory_p) {
+    /*Generate key*/
+    auto prim_key = key_ + "@eltwise_bwd_p";
+
+    auto eltwise_bwd_p = std::static_pointer_cast<mkldnn::eltwise_backward>(
+        dev_ctx_.GetBlob(prim_key));
+    if (eltwise_bwd_p == nullptr) {
+      eltwise_bwd_p = std::make_shared<mkldnn::eltwise_backward>(
+          *activation_bwd_pd_, *(src_memory_p), *(diff_dst_memory_p),
+          *(diff_src_memory_p));
+      dev_ctx_.SetBlob(prim_key, eltwise_bwd_p);
+    }
+
+    return eltwise_bwd_p;
+  }
+
+ private:
+  std::shared_ptr<mkldnn::eltwise_forward::primitive_desc> activation_pd_;
+  std::shared_ptr<mkldnn::eltwise_backward::primitive_desc> activation_bwd_pd_;
+};
+
 class TransposeMKLDNNHandler : public MKLDNNHandler {
  public:
   TransposeMKLDNNHandler(std::vector<int>& dims,  // NOLINT
