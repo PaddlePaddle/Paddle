9eaf8f3624aca6f3a64e9fbb097b39e97d2eaccd
guru4elephant <guru4elephant@gmail.com>
2019-07-17 00:51:52 +0800

    remove async executor and add data_feed.proto to the deps of train demo test=develop

--- paddle/fluid/framework/data_feed.cc
+++ paddle/fluid/framework/data_feed.cc
@@ -1,7 +1,8 @@
 /* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
+http://10.182.164.30:8900/fileview.html?path=/home/disk1/normandy/maybach/app-user-2you
+may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
