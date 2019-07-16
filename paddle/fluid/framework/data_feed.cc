087d28c9e5e6b044b12078b21822af55d7dd1bfe
guru4elephant <35550832+guru4elephant@users.noreply.github.com>
2019-07-17 00:57:31 +0800

    Update data_feed.cc
    test=develop

--- paddle/fluid/framework/data_feed.cc
+++ paddle/fluid/framework/data_feed.cc
@@ -1,8 +1,7 @@
 /* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
-http://10.182.164.30:8900/fileview.html?path=/home/disk1/normandy/maybach/app-user-2you
-may not use this file except in compliance with the License.
+you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
