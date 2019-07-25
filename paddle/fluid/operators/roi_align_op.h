557786dbda93c22ec1e7cddc4d6198baf43f1d61
FDInSky <fdinsky@qq.com>
2019-07-25 08:07:38 +0000

    test=develop fix something based on review

--- paddle/fluid/operators/roi_align_op.h
+++ paddle/fluid/operators/roi_align_op.h
@@ -284,7 +284,7 @@ class CPUROIAlignGradOpKernel : public framework::OpKernel<T> {
 
     int output_grad_size = out_grad->numel();
 
-    if ((!out_grad->IsInitialized) || (output_grad_size <= 0)) {
+    if ((!out_grad->IsInitialized()) || (output_grad_size <= 0)) {
       return;
     }
 
