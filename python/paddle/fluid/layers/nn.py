cd4b10ad27be0d91bbbf34d29a76a21437270f4f
Aurelius84 <liujiezhangbupt@gmail.com>
2019-07-31 03:52:43 +0000

    fix sample code run err test=develop

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -12622,11 +12622,11 @@ def sequence_topk_avg_pooling(input, row, col, topks, channel_num):
             x_lod_tensor = layers.data(name='x', shape=[1], lod_level=1)
             row_lod_tensor = layers.data(name='row', shape=[6], lod_level=1)
             col_lod_tensor = layers.data(name='col', shape=[6], lod_level=1)
-            out = layers.sequence_topk_pooling(input=x_lod_tensor,
-                                               row=row_lod_tensor,
-                                               col=col_lod_tensor,
-                                               topks=[1, 3, 5],
-                                               channel_num=5)
+            out = layers.sequence_topk_avg_pooling(input=x_lod_tensor,
+                                                   row=row_lod_tensor,
+                                                   col=col_lod_tensor,
+                                                   topks=[1, 3, 5],
+                                                   channel_num=5)
     """
     helper = LayerHelper('sequence_topk_avg_pooling', **locals())
     out = helper.create_variable_for_type_inference(dtype=helper.input_dtype())
