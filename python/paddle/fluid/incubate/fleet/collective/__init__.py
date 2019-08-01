c81ef7c300d14fe0ff1a4bac0e33087bbecba5db
gongweibao <weibao.gong@gmail.com>
2019-08-01 16:04:49 +0800

    add transpile function

--- python/paddle/fluid/incubate/fleet/collective/__init__.py
+++ python/paddle/fluid/incubate/fleet/collective/__init__.py
@@ -29,9 +29,6 @@ class DistFCConfig(Object):
     def __init__(self):
         pass
 
-
-
-
 class Collective(Fleet):
     def __init__(self):
         super(Collective, self).__init__(Mode.COLLECTIVE)
@@ -84,6 +81,7 @@ class DistributedStrategy(fluid.BuildStratey):
     Init function of DistributedStrategy
     """
     def __init__(self):
+        super(DistributedStrategy, self).__init__()
         self.fuse_memory_size = -1
         self.fuse_layer_size = 1
 
@@ -94,6 +92,16 @@ class DistributedStrategy(fluid.BuildStratey):
         self.dist_fc_config = None  # DistFCConfig
         self.mode = "nccl2"  # or collective
         self.collective_mode = None  # local_sgd or grad_allreduce
+
+        self._exec_strategy = fluid.ExecutionStrategy()
+        sync_allreduce = os.getenv("FLAGS_sync_nccl_allreduce")
+        if sync_allreduce == "0":
+            self._exec_strategy.num_threads = self.nccl_comm_num + 1
+            if sef.use_hierarchical_allreduce:
+                self._exec_strategy.num_threads = 2 * self.nccl_comm_num + 1
+            if self._exec_strategy.num_threads > 4:
+                print(stderr, "WARNING: if you use use_hierarchical_allreduce or "
+                              "with multi nccl comm, please set FLAGS_sync_nccl_allreduce = 0")
 
 class CollectiveOpBasedOptimizer(DistributedOptimizer):
     """
@@ -131,6 +139,10 @@ class CollectiveOptimizer(DistributedOptimizer):
     def __init__(self, optimizer, strategy=DistributedStrategy()):
         super(CollectiveOptimizer, self).__init__(optimizer, strategy)
 
+        self._origin_program = None
+        self.startup_program = None
+        self.main_program = None
+
     def backward(self,
                  loss,
                  startup_program=None,
@@ -162,9 +174,13 @@ class CollectiveOptimizer(DistributedOptimizer):
                    use_local_sgd=strategy.use_local_sgd,
                    use_lamb=main_program._use_lamb)
             assert strategy.dist_fc_config is not None, "DistributedStrategy.dist_fc_config should be set"
-        return optimizer
 
-    def _transpile(self):
+        if self._strategy.collective_mode=="local_sgd" \
+                or self._strategy.collective_mode == "grad_allreduce":
+            assert self._strategy.mode == "collective", \
+                "local_sgd and grad_allreduce can be used under collective mode"
+
+    def _transpile(self, startup_program, main_program):
         if self._strategy.fuse_all_reduce_ops:
             os.environ['FLAGS_fuse_parameter_memory_size'] = self.fuse_memory_size
             os.environ['FLAGS_fuse_parameter_groups_size'] = self.fuse_layer_size
@@ -173,15 +189,15 @@ class CollectiveOptimizer(DistributedOptimizer):
         trainer_id = fleet.worker_index()
         current_endpoint = fleet.worker_endpoints()[trainer_id]
 
-        startup_program = startup_program if startup_program else \
-            fluid.framework.default_startup_program
-
-        main_program = loss.block.program
-
         # call transpiler
         config = dist_transpiler.DistributeTranspilerConfig()
         config.mode = self._strategy.mode
         config.collective_mode = self._strategy.collective_mode
+
+        config.nccl_comm_num = self._strategy.nccl_comm_num
+        config.use_hierarchical_allreduce = self._strategy._use_hierarchical_allreduce
+        config.hierarchical_allreduce_inter_nranks = self._strategy._hierarchical_allreduce_inter_nranks
+
         t = dist_transpiler.DistributeTranspiler(config=config)
         t.transpile(
             trainer_id=trainer_id,
@@ -189,6 +205,24 @@ class CollectiveOptimizer(DistributedOptimizer):
             startup_program=startup_program,
             program=main_program,
             current_endpoint=current_endpoint)
+
+    def _try_to_compile(self,startup_program, main_program):
+        self._transpile(startup_program, main_program)
+
+        if self._strategy.mode == "collective":
+            return
+
+        self._strategy.num_trainers = fleet.worker_num()
+        self._strategy.trainer_id = fleet.worker_index()
+        self._strategy.trainers_endpoints = fleet.worker_endpoints()
+
+        self._compiled_program = compiler.CompiledProgram(main_program)
+
+        self._compiled_program.with_data_parallel(
+            loss_name=self._loss.name,
+            build_strategy=self._strategy,
+            exec_strategy=self._exec_strategy,
+            share_vars_from=None)
 
     def minimize(self,
                  loss,
@@ -211,8 +245,18 @@ class CollectiveOptimizer(DistributedOptimizer):
         process, but currently the optimization part is written into Fleet(). A user does not
         need to care about how to startup a pserver node.
         """
-        _check(loss.block.program, self._optimizer, self._strategy)
-        self._transpile()
+        main_program = loss.block.program
+        self._origin_program = main_program.clone()
+        self.main_program = main_program
+        if startup_program is None:
+            startup_program = fluid.default_startup_program
+        self.startup_program = startup_program
+
+        self._loss = loss
+
+        _check(main_program, self._optimizer, self._strategy)
+
+        self._try_to_compile(startup_program, main_program)
 
         optimize_ops, param_grads = self._optimizer.minimize(
             loss, startup_program, parameter_list, no_grad_set)
