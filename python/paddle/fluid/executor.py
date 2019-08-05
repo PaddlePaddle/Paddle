642777d546b69f8820fca9bcf4d17e69ad3c7ce2
zhiqiu <chenqiuliang@baidu.com>
2019-08-05 13:53:42 +0000

    fix memory overlapping of fetch var (return of executor.run), test=develop

--- python/paddle/fluid/executor.py
+++ python/paddle/fluid/executor.py
@@ -527,12 +527,12 @@ class Executor(object):
             exe.feed_tensors_into_local_scopes(res)
 
         fetch_var_names = list(map(_to_name_str, fetch_list))
-        exe.run(fetch_var_names, fetch_var_name)
-        arr = scope.find_var(fetch_var_name).get_lod_tensor_array()
+        arr = exe.run(fetch_var_names, fetch_var_name)
 
         if return_numpy:
             return as_numpy(arr)
-        return [arr[i] for i in range(len(arr))]
+        else:
+            return arr
 
     def run(self,
             program=None,
@@ -745,9 +745,12 @@ class Executor(object):
             exe.run(program.desc, scope, 0, True, True, fetch_var_name)
         else:
             exe.run_cached_prepared_ctx(ctx, scope, False, False, False)
-        outs = self._fetch_data(fetch_list, fetch_var_name, scope)
+        arr = scope.find_var(fetch_var_name).get_lod_tensor_array()
         if return_numpy:
-            outs = as_numpy(outs)
+            outs = as_numpy(arr)
+        else:
+            outs = core.LoDTensorArray()
+            arr.move(outs)
         return outs
 
     def _run_inference(self, exe, feed):
