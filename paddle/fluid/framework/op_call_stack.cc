452c5384a7b02c77458f8b4c76a68bb1b288a3e8
chengduozh <zhaochengduo@baidu.com>
2019-08-08 11:40:20 +0800

    Rename operator_call_stack test=develop

--- paddle/fluid/framework/op_call_stack.cc
+++ paddle/fluid/framework/op_call_stack.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/framework/operator_call_stack.h"
+#include "paddle/fluid/framework/op_call_stack.h"
 #include <string>
 #include <vector>
 #include "paddle/fluid/framework/attribute.h"
