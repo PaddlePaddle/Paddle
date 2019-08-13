115a1595b1cafe41924bcb969f41d2128d6620af
tianshuo78520a <707759223@qq.com>
2019-08-13 15:01:04 +0800

    check

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -585,7 +585,7 @@ function assert_api_spec_approvals() {
 
     s=`git diff -U0 upstream/$BRANCH`
     echo $s
-    HAS_PADDLE_ENFORCE_=`git diff -U0 upstream/$BRANCH |grep -rn "PADDLE_ENFORCE_" |grep -v '"' || true`
+    HAS_PADDLE_ENFORCE_=`git diff -U0 upstream/$BRANCH |grep -rn "PADDLE_ENFORCE_" paddle | grep -v '(\"' | grep '\"' || true`
     if [ ${HAS_PADDLE_ENFORCE_} ] && [ "${GIT_PR_ID}" != "" ]; then
         APPROVALS=`curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/Paddle/pulls/${GIT_PR_ID}/reviews?per_page=10000 | \
         python ${PADDLE_ROOT}/tools/check_pr_approval.py 1 6836917 47554610 22561442`
