c16a19c977453e2226dfe1d6e204bd0290216aff
Hao Wang <101779990@student.swin.edu.au>
2019-08-13 12:20:28 +0800

    test=develop

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -1006,7 +1006,7 @@ function example() {
     pip install /paddle/build/python/dist/*.whl
     paddle version
     cd ${PADDLE_ROOT}/python/paddle/fluid
-    python sampcd_processor.py 
+    python sampcd_processor.py cpu 
     if [ "$?" != "0" ];then
       echo "Code instance execution failed"
       exit 1
