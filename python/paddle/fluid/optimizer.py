020c126aff1b5cc6f4f449947cb1cf96d80d153c
mapingshuo <mps2012@yeah.net>
2019-08-15 16:20:37 +0800

    Add lookahead optimizer

--- python/paddle/fluid/optimizer.py
+++ python/paddle/fluid/optimizer.py
@@ -43,7 +43,7 @@ __all__ = [
     'AdamaxOptimizer', 'DecayedAdagradOptimizer', 'RMSPropOptimizer',
     'FtrlOptimizer', 'Adadelta', 'ModelAverage', 'LarsMomentum',
     'LarsMomentumOptimizer', 'DGCMomentumOptimizer', 'LambOptimizer',
-    'ExponentialMovingAverage', 'PipelineOptimizer'
+    'ExponentialMovingAverage', 'PipelineOptimizer', 'LookaheadOptimizer'
 ]
 
 
@@ -2953,3 +2953,103 @@ class PipelineOptimizer(object):
             "sync_steps": self._sync_steps,
             "param_need_sync": param_need_sync
         }
+
+
+class LookaheadOptimizer(object):
+    def __init__(self,
+                 inner_optimizer,
+                 alpha=0.5,
+                 k=5,
+                 ignore_embed=True,
+                 name=None):
+        assert inner_optimizer is not None
+        self.inner_optimizer = inner_optimizer
+        self.alpha = alpha
+        self.k = k
+        self.ignore_embed = ignore_embed
+        self.type = "lookahead"
+
+    def minimize(self, loss, startup_program=None):
+
+        # Apply SGD to the main_program
+        mini_out = self.inner_optimizer.minimize(loss)
+
+        # Get startup_program and main_program
+        if startup_program is None:
+            startup_program = default_startup_program()
+        main_block = loss.block
+        main_program = main_block.program
+
+        # add some vars to the main_program
+        params = [param.name for param in main_block.all_parameters()]
+        if self.ignore_embed:
+            params = [x for x in params if "embedding" not in x]
+        for param in params:
+            main_block.create_var(
+                name=param + "@SLOW",
+                shape=main_block.var(param).shape,
+                dtype=main_block.var(param).dtype,
+                persistable=True)
+
+        # add some vars to the startup_program
+        startup_block = startup_program.global_block()
+        for param in params:
+            assert (startup_block.var(param) is not None)
+            startup_block.create_var(
+                name=param + "@SLOW",
+                shape=startup_block.var(param).shape,
+                dtype=startup_block.var(param).dtype,
+                persistable=True)
+
+            startup_block.append_op(
+                type="assign",
+                inputs={"X": startup_block.var(param)},
+                outputs={"Out": startup_block.var(param + "@SLOW")})
+
+        # Add Var k to main prog and startup prog
+        k = layers.create_global_var(
+            name="lookahead_k",
+            shape=[1],
+            value=int(self.k),
+            dtype='int32',
+            persistable=True)
+
+        # Add Var alpha to main prog and startup prog
+        alpha = layers.create_global_var(
+            name="lookahead_alpha",
+            shape=[1],
+            value=float(self.alpha),
+            dtype='float32',
+            persistable=True)
+
+        # Add Var step
+        step = layers.create_global_var(
+            name="lookahead_step",
+            shape=[1],
+            value=int(0),
+            dtype='int32',
+            persistable=True)
+        layers.increment(x=step, value=1.0, in_place=True)
+
+        # lookahead
+        zero_var = layers.fill_constant(shape=[1], dtype='float32', value=0.0)
+
+        one_var = layers.fill_constant(shape=[1], dtype='float32', value=1.0)
+
+        block = main_block
+
+        mod = layers.elementwise_mod(step, k)
+        with layers.control_flow.Switch() as switch:
+            with switch.case(mod == zero_var):
+                for param_name in params:
+                    tmp_var = layers.elementwise_add(
+                        layers.elementwise_mul(block.var(param_name), alpha),
+                        layers.elementwise_mul(
+                            block.var(param_name + "@SLOW"),
+                            layers.elementwise_sub(one_var, alpha)))
+                    layers.assign(
+                        input=tmp_var, output=block.var(param_name + "@SLOW"))
+                    layers.assign(input=tmp_var, output=block.var(param_name))
+            with switch.default():
+                pass
+        return mini_out
