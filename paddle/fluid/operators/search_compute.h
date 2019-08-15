02a84e7883b8cf95472f3c018dfe2768861d528c
Aurelius84 <liujiezhangbupt@gmail.com>
2019-08-15 11:19:39 +0800

    clean code in search_computer.h

--- paddle/fluid/operators/search_compute.h
+++ paddle/fluid/operators/search_compute.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -118,54 +118,12 @@ static const unsigned int SSE_CUT_LEN_MASK = 3U;
 #define _mm_sqrt_px _mm_sqrt_ps
 #define _mm_div_px _mm_div_ps
 
-#elif defined(TYPE_USE_DOUBLE)
-
-#define __m256x __m256d
-#define __m128x __m128d
-
-static const unsigned int AVX_STEP_SIZE = 4;
-static const unsigned int SSE_STEP_SIZE = 2;
-static const unsigned int AVX_CUT_LEN_MASK = 3U;
-static const unsigned int SSE_CUT_LEN_MASK = 1U;
-
-#define _mm256_setzero_px _mm256_setzero_pd
-#define _mm256_mul_px _mm256_mul_pd
-#define _mm256_add_px _mm256_add_pd
-#define _mm256_load_px _mm256_loadu_pd
-#define _mm256_hadd_px _mm256_hadd_pd
-#define _mm256_permute2f128_px _mm256_permute2f128_pd
-#define _mm256_store_px _mm256_storeu_pd
-#define _mm256_broadcast_sx _mm256_broadcast_sd
-#define _mm256_castpx256_px128 _mm256_castpd256_pd128
-#define _mm256_max_px _mm256_max_pd
-#define _mm256_sub_px _mm256_sub_pd
-#define _mm256_set1_px _mm256_set1_pd
-#define _mm256_sqrt_px _mm256_sqrt_pd
-#define _mm256_div_px _mm256_div_pd
-#define _mm_setzero_px _mm_setzero_pd
-#define _mm_add_px _mm_add_pd
-#define _mm_mul_px _mm_mul_pd
-#define _mm_load_px _mm_loadu_pd
-#define _mm_hadd_px _mm_hadd_pd
-#define _mm_store_sx _mm_store_sd
-#define _mm_store_px _mm_storeu_pd
-#define _mm_load1_px _mm_load1_pd
-#define _mm_max_px _mm_max_pd
-#define _mm_sub_px _mm_sub_pd
-#define _mm_set1_px _mm_set1_pd
-#define _mm_sqrt_px _mm_sqrt_pd
-#define _mm_div_px _mm_div_pd
 #endif
 
 #if defined(TYPE_USE_FLOAT)
 
 #define X_MIN FLT_MIN
 #define X_MAX FLT_MAX
-
-#elif defined(TYPE_USE_DOUBLE)
-
-#define X_MIN DBL_MIN
-#define X_MAX DBL_MAX
 
 #endif
 
@@ -174,13 +132,7 @@ inline void sse_eltadd(const T* x, const T* y, T* z, size_t len) {
   unsigned int jjj, lll;
   jjj = lll = 0;
 
-#if defined(USE_AVX)
-  lll = len & ~AVX_CUT_LEN_MASK;
-  for (jjj = 0; jjj < lll; jjj += AVX_STEP_SIZE) {
-    _mm256_store_px(z + jjj, _mm256_add_px(_mm256_load_px(x + jjj),
-                                           _mm256_load_px(y + jjj)));
-  }
-#elif defined(USE_SSE)
+#if defined(USE_SSE)
   lll = len & ~SSE_CUT_LEN_MASK;
 
   for (jjj = 0; jjj < lll; jjj += SSE_STEP_SIZE) {
@@ -198,17 +150,7 @@ inline void sse_axpy(const T* x, T* y, size_t len, const T alpha) {
   unsigned int jjj, lll;
   jjj = lll = 0;
 
-#if defined(USE_AVX)
-  lll = len & ~AVX_CUT_LEN_MASK;
-  __m256x mm_alpha = _mm256_broadcast_sx(&alpha);
-  for (jjj = 0; jjj < lll; jjj += AVX_STEP_SIZE) {
-    _mm256_store_px(
-        y + jjj,
-        _mm256_add_px(_mm256_load_px(y + jjj),
-                      _mm256_mul_px(mm_alpha, _mm256_load_px(x + jjj))));
-  }
-
-#elif defined(USE_SSE)
+#if defined(USE_SSE)
   lll = len & ~SSE_CUT_LEN_MASK;
   __m128x mm_alpha = _mm_load1_px(&alpha);
   for (jjj = 0; jjj < lll; jjj += SSE_STEP_SIZE) {
@@ -228,14 +170,7 @@ inline void sse_axpy_noadd(const T* x, T* y, size_t len, const T alpha) {
   unsigned int jjj, lll;
   jjj = lll = 0;
 
-#if defined(USE_AVX)
-  lll = len & ~AVX_CUT_LEN_MASK;
-  __m256x mm_alpha = _mm256_broadcast_sx(&alpha);
-  for (jjj = 0; jjj < lll; jjj += AVX_STEP_SIZE) {
-    _mm256_store_px(y + jjj, _mm256_mul_px(mm_alpha, _mm256_load_px(x + jjj)));
-  }
-
-#elif defined(USE_SSE)
+#if defined(USE_SSE)
   lll = len & ~SSE_CUT_LEN_MASK;
   __m128x mm_alpha = _mm_load1_px(&alpha);
   for (jjj = 0; jjj < lll; jjj += SSE_STEP_SIZE) {
@@ -253,13 +188,7 @@ inline void sse_eltmul(const T* x, const T* y, T* z, size_t len) {
   unsigned int jjj, lll;
   jjj = lll = 0;
 
-#if defined(USE_AVX)
-  lll = len & ~AVX_CUT_LEN_MASK;
-  for (jjj = 0; jjj < lll; jjj += AVX_STEP_SIZE) {
-    _mm256_store_px(z + jjj, _mm256_mul_px(_mm256_load_px(x + jjj),
-                                           _mm256_load_px(y + jjj)));
-  }
-#elif defined(USE_SSE)
+#if defined(USE_SSE)
   lll = len & ~SSE_CUT_LEN_MASK;
 
   for (jjj = 0; jjj < lll; jjj += SSE_STEP_SIZE) {
@@ -277,14 +206,7 @@ inline void sse_add_scalar(const T* x, T* y, size_t len, const T alpha) {
   unsigned int jjj, lll;
   jjj = lll = 0;
 
-#if defined(USE_AVX)
-  lll = len & ~AVX_CUT_LEN_MASK;
-  __m256x mm_alpha = _mm256_broadcast_sx(&alpha);
-  for (jjj = 0; jjj < lll; jjj += AVX_STEP_SIZE) {
-    _mm256_store_px(y + jjj, _mm256_add_px(mm_alpha, _mm256_load_px(x + jjj)));
-  }
-
-#elif defined(USE_SSE)
+#if defined(USE_SSE)
   lll = len & ~SSE_CUT_LEN_MASK;
   __m128x mm_alpha = _mm_load1_px(&alpha);
   for (jjj = 0; jjj < lll; jjj += SSE_STEP_SIZE) {
@@ -303,24 +225,7 @@ inline void sse_sum(const T* x, T* y, size_t len) {
   jjj = lll = 0;
   *y = 0.;
 
-#if defined(USE_AVX)
-  lll = len & ~AVX_CUT_LEN_MASK;
-
-  __m256x mm_result = _mm256_setzero_px();
-  for (jjj = 0; jjj < lll; jjj += AVX_STEP_SIZE) {
-    mm_result = _mm256_add_px(mm_result, _mm256_load_px(x + jjj));
-  }
-
-#if defined(TYPE_USE_FLOAT)
-  __m256x hsum = _mm256_hadd_px(mm_result, mm_result);
-#elif defined(TYPE_USE_DOUBLE)
-  __m256x hsum = mm_result;
-#endif
-  hsum = _mm256_add_px(hsum, _mm256_permute2f128_px(hsum, hsum, 0x1));
-  _mm_store_sx(y, _mm_hadd_px(_mm256_castpx256_px128(hsum),
-                              _mm256_castpx256_px128(hsum)));
-
-#elif defined(USE_SSE)
+#if defined(USE_SSE)
   lll = len & ~SSE_CUT_LEN_MASK;
 
   __m128x mm_result = _mm_setzero_px();
@@ -331,8 +236,6 @@ inline void sse_sum(const T* x, T* y, size_t len) {
 
 #if defined(TYPE_USE_FLOAT)
   _mm_store_sx(y, _mm_hadd_px(mm_tmp, mm_tmp));
-#elif defined(TYPE_USE_DOUBLE)
-  _mm_store_sx(y, mm_tmp);
 #endif
 
 #endif
@@ -346,15 +249,7 @@ inline void sse_scale(const T* x, T* y, size_t len, const T alpha) {
   unsigned int jjj, lll;
   jjj = lll = 0;
 
-#if defined(USE_AVX)
-  lll = len & ~AVX_CUT_LEN_MASK;
-  __m256x mm_alpha = _mm256_broadcast_sx(&alpha);
-
-  for (jjj = 0; jjj < lll; jjj += AVX_STEP_SIZE) {
-    _mm256_store_px(y + jjj, _mm256_mul_px(mm_alpha, _mm256_load_px(x + jjj)));
-  }
-
-#elif defined(USE_SSE)
+#if defined(USE_SSE)
   lll = len & ~SSE_CUT_LEN_MASK;
   __m128x mm_alpha = _mm_load1_px(&alpha);
   for (jjj = 0; jjj < lll; jjj += SSE_STEP_SIZE) {
@@ -372,28 +267,7 @@ inline void sse_ip(const T* vec1, const T* vec2, size_t len, T* result) {
   jjj = lll = 0;
   *result = 0.;
 
-#if defined(USE_AVX)
-  lll = len & ~AVX_CUT_LEN_MASK;
-
-  __m256x mm_result = _mm256_setzero_px();
-  for (jjj = 0; jjj < lll; jjj += AVX_STEP_SIZE) {
-    mm_result = _mm256_add_px(
-        mm_result,
-        _mm256_mul_px(_mm256_load_px(vec1 + jjj), _mm256_load_px(vec2 + jjj)));
-  }
-
-#if defined(TYPE_USE_FLOAT)
-  __m256x hsum = _mm256_hadd_px(mm_result, mm_result);
-#elif defined(TYPE_USE_DOUBLE)
-  __m256x hsum = mm_result;
-#endif
-
-  hsum = _mm256_add_px(hsum, _mm256_permute2f128_px(hsum, hsum, 0x1));
-
-  _mm_store_sx(result, _mm_hadd_px(_mm256_castpx256_px128(hsum),
-                                   _mm256_castpx256_px128(hsum)));
-
-#elif defined(USE_SSE)
+#if defined(USE_SSE)
   lll = len & ~SSE_CUT_LEN_MASK;
   __m128x mm_result = _mm_setzero_px();
   for (jjj = 0; jjj < lll; jjj += SSE_STEP_SIZE) {
@@ -403,8 +277,6 @@ inline void sse_ip(const T* vec1, const T* vec2, size_t len, T* result) {
   __m128x mm_tmp = _mm_hadd_px(mm_result, mm_result);
 #if defined(TYPE_USE_FLOAT)
   _mm_store_sx(result, _mm_hadd_px(mm_tmp, mm_tmp));
-#elif defined(TYPE_USE_DOUBLE)
-  _mm_store_sx(result, mm_tmp);
 #endif
 
 #endif
