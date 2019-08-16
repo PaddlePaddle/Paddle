56643be69b6f3afe10c18bda62d0f5e31bdb74d0
chengduozh <zhaochengduo@baidu.com>
2019-08-16 14:56:57 +0800

    follow the rules 'forbid using PADDLE_ENRODE' test=release/1.5

--- paddle/fluid/framework/op_registry.h
+++ paddle/fluid/framework/op_registry.h
@@ -54,8 +54,9 @@ class Registrar {
 template <typename... ARGS>
 struct OperatorRegistrar : public Registrar {
   explicit OperatorRegistrar(const char* op_type) {
-    PADDLE_ENFORCE(!OpInfoMap::Instance().Has(op_type),
-                   "'%s' is registered more than once.", op_type);
+    if (OpInfoMap::Instance().Has(op_type)) {
+      PADDLE_THROW("'%s' is registered more than once.", op_type);
+    }
     static_assert(sizeof...(ARGS) != 0,
                   "OperatorRegistrar should be invoked at least by OpClass");
     OpInfo info;
