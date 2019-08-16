ae068773a6972c10f0cefefb2ac00d36f7cd3d37
danleifeng <danleifeng@163.com>
2019-08-16 09:20:21 +0000

    add use_paddlecloud param; test=develop

--- python/paddle/fluid/tests/unittests/test_launch.sh
+++ python/paddle/fluid/tests/unittests/test_launch.sh
@@ -28,3 +28,27 @@ else
     echo "not find trainer 0"
     exit -1
 fi
+
+
+# use paddlecloud
+distributed_args="--use_paddlecloud True --selected_gpus=0,1 --log_dir testlog"
+python -m paddle.distributed.launch ${distributed_args} multi_process.py
+
+str1="selected_gpus:0 worker_endpoints:['127.0.0.1:6170', '127.0.0.1:6171'] trainers_num:2 current_endpoint:127.0.0.1:6170 trainer_id:0"
+str2="selected_gpus:1 worker_endpoints:['127.0.0.1:6170', '127.0.0.1:6171'] trainers_num:2 current_endpoint:127.0.0.1:6171 trainer_id:1"
+file="multi_process.check.log"
+
+echo "paddlecloud params test"
+if ! grep -q "$str1" "$file"; then
+    echo "find trainer 0"
+else
+    echo "not find trainer 0"
+    exit -1
+fi
+
+if ! grep -q "$str2" "$file"; then
+    echo "find trainer 1"
+else
+    echo "not find trainer 0"
+    exit -1
+fi
