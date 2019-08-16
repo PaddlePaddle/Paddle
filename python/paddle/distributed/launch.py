135d8738f93cc5b20c2a3784fc535271c8688fbe
danleifeng <danleifeng@163.com>
2019-08-16 09:41:07 +0000

    add use_paddlecloud param; test=develop

--- python/paddle/distributed/launch.py
+++ python/paddle/distributed/launch.py
@@ -44,6 +44,7 @@ import sys
 from sys import version
 import subprocess
 import os
+import warnings
 import six
 import copy
 from argparse import ArgumentParser, REMAINDER
@@ -149,7 +150,14 @@ def start_procs(args):
         current_node_ip = os.getenv("POD_IP", "127.0.0.1")
         node_ips = os.getenv("PADDLE_TRAINERS", current_node_ip).split(",")
         node_id = int(os.getenv("PADDLE_TRAINER_ID", "0"))
-
+        if current_node_ip != args.node_ip:
+            warnings.warn("Please NOTE: When using paddlecloud, current_node_ip is \
+automatically got from POD_IP. Your input node_ip: %s doesn't equals to \
+current_node_ip: %s from paddlecloud environment." % (args.node_ip, current_node_ip) )
+        if args.cluster_node_ips != ",".join(node_ips):
+            warnings.warn("Please NOTE: When using paddlecloud, cluster_node_ips is \
+automatically got from PADDLE_TRAINERS(multi nodes) or POD_IP(single node).Your input \
+cluster_node_ips: %s doesn't equals to IPs: %s from paddlecloud environment." % (args.cluster_node_ips, node_ips) )
     num_nodes = len(node_ips)
 
     if args.selected_gpus is None:
--- python/paddle/fluid/tests/unittests/test_launch.sh
+++ python/paddle/fluid/tests/unittests/test_launch.sh
@@ -31,7 +31,10 @@ fi
 
 
 # use paddlecloud
-distributed_args="--use_paddlecloud True --selected_gpus=0,1 --log_dir testlog"
+cluster_node_ips="10.0.0.1"
+node_ip="10.0.0.1"
+
+distributed_args="--use_paddlecloud True --cluster_node_ips ${cluster_node_ips} --node_ip ${node_ip} --selected_gpus=0,1 --log_dir testlog"
 python -m paddle.distributed.launch ${distributed_args} multi_process.py
 
 str1="selected_gpus:0 worker_endpoints:['127.0.0.1:6170', '127.0.0.1:6171'] trainers_num:2 current_endpoint:127.0.0.1:6170 trainer_id:0"
