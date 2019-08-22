52df85499b9412fe3e54fa59da7b5cd1039c3fe7
chengduo <30176695+chengduoZH@users.noreply.github.com>
2019-08-22 17:02:28 +0800

    Update CompiledProgram (#18919)
    * use PE for compiler
    test=develop

--- python/paddle/fluid/io.py
+++ python/paddle/fluid/io.py
@@ -111,6 +111,20 @@ def _clone_var_in_block_(block, var):
             persistable=True)
 
 
+def _get_valid_program(main_program):
+    if main_program is None:
+        main_program = default_main_program()
+    elif isinstance(main_program, CompiledProgram):
+        main_program = main_program._program
+        if main_program is None:
+            raise TypeError("program should be as Program type or None")
+        warnings.warn(
+            "The input is a CompiledProgram, this is not recommended.")
+    if not isinstance(main_program, Program):
+        raise TypeError("program should be as Program type or None")
+    return main_program
+
+
 def save_vars(executor,
               dirname,
               main_program=None,
@@ -193,13 +207,9 @@ def save_vars(executor,
             # saved in the same file named 'var_file' in the path "./my_paddle_vars".
     """
     save_dirname = os.path.normpath(dirname)
+    main_program = _get_valid_program(main_program)
 
     if vars is None:
-        if main_program is None:
-            main_program = default_main_program()
-        if not isinstance(main_program, Program):
-            raise TypeError("program should be as Program type or None")
-
         save_vars(
             executor,
             main_program=main_program,
@@ -209,11 +219,6 @@ def save_vars(executor,
     else:
         save_program = Program()
         save_block = save_program.global_block()
-
-        if main_program is None:
-            main_program = default_main_program()
-        if not isinstance(main_program, Program):
-            raise TypeError("program should be as Program type or None")
 
         save_var_map = {}
         for each_var in vars:
@@ -516,11 +521,9 @@ def save_persistables(executor, dirname, main_program=None, filename=None):
             fluid.io.save_persistables(executor=exe, dirname=param_path,
                                        main_program=prog)
     """
-
     if main_program and main_program._is_distributed:
         _save_distributed_persistables(
             executor, dirname=dirname, main_program=main_program)
-
     else:
         save_vars(
             executor,
@@ -1026,11 +1029,7 @@ def save_inference_model(dirname,
                 all(isinstance(var, Variable) for var in target_vars)):
             raise ValueError("'target_vars' should be a list of Variable.")
 
-    if main_program is None:
-        main_program = default_main_program()
-
-    elif not isinstance(main_program, Program):
-        raise TypeError("program should be as Program type or None")
+    main_program = _get_valid_program(main_program)
 
     # fix the bug that the activation op's output as target will be pruned.
     # will affect the inference performance.
