2d1380ae50d64b098515b9e53f9158d79b79d95c
Wojciech Uss <wojciech.uss@intel.com>
2019-08-22 06:37:10 +0200

    updated for new QAT models

--- python/paddle/fluid/contrib/slim/quantization/quantization_mkldnn_pass.py
+++ python/paddle/fluid/contrib/slim/quantization/quantization_mkldnn_pass.py
@@ -305,9 +305,7 @@ class TransformThroughFP32Pass(object):
             'fake_quantize_moving_average_abs_max',
             'fake_quantize_dequantize_moving_average_abs_max'
         ]
-        self.fake_dequantize_types = [
-            'fake_dequantize_max_abs'
-        ]
+        self.fake_dequantize_types = ['fake_dequantize_max_abs']
         self._conv_ops = ['conv2d', 'depthwise_conv2d']
         self._pool_ops = ['pool2d']
         self._mul_ops = ['mul']
@@ -358,13 +356,32 @@ class TransformThroughFP32Pass(object):
     def _load_param(self, scope, param_name):
         return np.array(scope.find_var(param_name).get_tensor())
 
+    #  def _remove_fake_ops(self, graph):
+    #  for op in graph.all_op_nodes():
+    #  if op.name() in self.fake_quantize_types:
+    #  self._remove_fake_quantize(graph, op)
+
+    #  for op in graph.all_op_nodes():
+    #  if op.name() in self.fake_dequantize_types:
+    #  self._remove_fake_dequantize(graph, op)
+    #  return graph
+
     def _remove_fake_ops(self, graph):
         for op in graph.all_op_nodes():
             if op.name() in self.fake_quantize_types:
+                op_out = graph._find_node_by_name(op.outputs,
+                                                  op.output("Out")[0])
+                next_op = op_out.outputs[0]
+                if next_op.name() == 'mul':
+                    continue
                 self._remove_fake_quantize(graph, op)
 
         for op in graph.all_op_nodes():
             if op.name() in self.fake_dequantize_types:
+                op_in = graph._find_node_by_name(op.inputs, op.input("X")[0])
+                prev_op = op_in.inputs[0]
+                if prev_op.name() == 'mul':
+                    continue
                 self._remove_fake_dequantize(graph, op)
         return graph
 
@@ -411,8 +428,8 @@ class TransformThroughFP32Pass(object):
         for op in graph.all_op_nodes():
             if op.name() in self._conv_ops:
                 self._dequantize_conv_weights(graph, op)
-            elif op.name() in self._mul_ops:
-                self._dequantize_mul_weights(graph, op)
+            #  elif op.name() in self._mul_ops:
+            #  self._dequantize_mul_weights(graph, op)
         return graph
 
     def _dequantize_conv_weights(self, graph, op_node):
@@ -425,8 +442,7 @@ class TransformThroughFP32Pass(object):
         # Convert int8 range weights to fp32 range weights
         scales = self.WeightScales[output_name]
         weight = self._load_param(self._scope, weight_name)
-        w_fp32 = np.divide(
-            np.multiply(weight, self.s8_max), scales)
+        w_fp32 = np.divide(np.multiply(weight, self.s8_max), scales)
         w_fp32 = w_fp32.reshape(weight.shape)
         self._restore_var(weight_name, w_fp32)
 
@@ -435,8 +451,7 @@ class TransformThroughFP32Pass(object):
         output_name = op_node.output("Out")[0]
         scales = self.WeightScales[output_name]
         weight = self._load_param(self._scope, weight_name)
-        w_fp32 = np.divide(
-            np.multiply(weight, self.s8_max), scales)
+        w_fp32 = np.divide(np.multiply(weight, self.s8_max), scales)
         w_fp32 = w_fp32.reshape(weight.shape)
         self._restore_var(weight_name, w_fp32)
 
