8367e10cb9b5b73e3df014d7ab6d688c3afa5029
Tao Luo <luotao02@baidu.com>
2019-08-27 12:56:40 +0800

    try another update example

--- paddle/fluid/framework/async_executor.cc
+++ paddle/fluid/framework/async_executor.cc
@@ -87,7 +87,7 @@ void AsyncExecutor::RunFromFile(const ProgramDesc& main_program,
 
   actual_thread_num_ = thread_num;
   int file_cnt = filelist.size();
-  PADDLE_ENFORCE(file_cnt > 0, "File list cannot be empty");
+  PADDLE_ENFORCE_GT(file_cnt, 0, "File list cannot be empty");
 
   if (actual_thread_num_ > file_cnt) {
     VLOG(1) << "Thread num = " << thread_num << ", file num = " << file_cnt
