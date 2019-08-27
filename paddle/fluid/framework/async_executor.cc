d2901063ef75c304c2e310a12885106fcdaf363a
Tao Luo <luotao02@baidu.com>
2019-08-27 13:13:58 +0800

    try another update example2

--- paddle/fluid/framework/async_executor.cc
+++ paddle/fluid/framework/async_executor.cc
@@ -82,7 +82,7 @@ void AsyncExecutor::RunFromFile(const ProgramDesc& main_program,
 
   DataFeedDesc data_feed_desc;
   bool success = data_feed_desc.ParseFromString(data_feed_desc_str);
-  PADDLE_ENFORCE(success, "Fail to parse DataFeedDesc from string:\n%s",
+  PADDLE_ENFORCE(success, "Change, Fail to parse DataFeedDesc from string:\n%s",
                  data_feed_desc_str.c_str());
 
   actual_thread_num_ = thread_num;
