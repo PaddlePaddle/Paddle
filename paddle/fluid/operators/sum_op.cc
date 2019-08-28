b6d1d8901fe267cc7661935b073d4a496b5db6dd
chengduo <30176695+chengduoZH@users.noreply.github.com>
2019-08-29 06:18:54 +0800

    Increase num_iteration_per_drop_scope  (#19075)
    * increase num_iteration_per_drop_scope
    test=develop
    
    * Fix bug of while_op
    test=develop
    
    * fix bug of whileOp
    test=develop

--- paddle/fluid/operators/sum_op.cc
+++ paddle/fluid/operators/sum_op.cc
@@ -141,7 +141,7 @@ class SumOp : public framework::OperatorWithKernel {
       for (auto& x_var : x_vars) {
         auto& array = x_var->Get<framework::LoDTensorArray>();
         for (auto& each : array) {
-          if (each.numel() != 0) {
+          if (each.numel() != 0 && each.IsInitialized()) {
             return framework::OpKernelType(each.type(), ctx.device_context(),
                                            layout, library);
           }
