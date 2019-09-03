7c23347f4d47cb8148c3385bc63fed9e474a8e62
malin10 <malin10@baidu.com>
2019-09-03 11:07:03 +0800

    test=develop, unittest

--- python/paddle/fluid/tests/unittests/test_fleet_utils.py
+++ python/paddle/fluid/tests/unittests/test_fleet_utils.py
@@ -15,12 +15,20 @@
 from __future__ import print_function
 import paddle.fluid as fluid
 import unittest
+import paddle.fluid.incubate.fleet.base.role_maker as role_maker
+from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler import fleet
 from paddle.fluid.incubate.fleet.utils.fleet_barrier_util import check_all_trainers_ready
 
 
 class TestFleetUtils(unittest.TestCase):
     def test_fleet_barrier(self):
-        check_all_trainers_ready("unittest", 0)
+        role = role_maker.UserDefinedRoleMaker(
+            current_id=0,
+            role=role_maker.Role.WORKER,
+            worker_num=1,
+            server_endpoints=['127.0.0.1'])
+        fleet.init(role)
+        check_all_trainers_ready("/ready_path/", 0)
 
 
 if __name__ == '__main__':
