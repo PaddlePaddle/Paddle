c5548178b0a7dc428d545a532bf2bfcc74ffde3d
Yiqun Liu <liuyiqun01@baidu.com>
2019-09-03 19:26:36 +0800

    A a pass to enable the use of cudnn (#19346)
    * Add a interface to enable cudnn for inference.
    
    * Add cudnn_placement_pass.
    test=develop
    
    * Set the default value of cudnn_enabled_op_types to null.
    test=develop
    
    * Write the common basic class, placement_pass_base, to refine the codes.
    test=develop
    
    * Call EnableCUDNN in unittest.
    test=develop
    
    * Refine cudnn_placement_pass tester.
    
    * Enable the testing of cudnn_placement_pass in inference's unittest.
    test=develop
    
    * Add the check of op kernels.
    test=develop

--- paddle/fluid/framework/ir/cudnn_placement_pass.cc
+++ paddle/fluid/framework/ir/cudnn_placement_pass.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/framework/ir/mkldnn/mkldnn_placement_pass.h"
+#include "paddle/fluid/framework/ir/cudnn_placement_pass.h"
 
-REGISTER_PASS(mkldnn_placement_pass, paddle::framework::ir::MKLDNNPlacementPass)
-    .RequirePassAttr("mkldnn_enabled_op_types");
+REGISTER_PASS(cudnn_placement_pass, paddle::framework::ir::CUDNNPlacementPass)
+    .RequirePassAttr("cudnn_enabled_op_types");
