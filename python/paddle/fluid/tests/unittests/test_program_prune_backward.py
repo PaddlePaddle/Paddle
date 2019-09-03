cb46bf7da05d60ccd7cf6672753300ba7690ea9b
Chen Weihang <chenweihang@baidu.com>
2019-09-03 12:12:34 +0000

    add regularization for optimizer & program compare function, test=develop

--- python/paddle/fluid/tests/unittests/test_program_prune_backward.py
+++ python/paddle/fluid/tests/unittests/test_program_prune_backward.py
@@ -26,13 +26,35 @@ from test_parallel_executor_transformer import transformer, get_feed_data_reader
 
 
 class TestProgramPruneBackward(unittest.TestCase):
+    def program_compare(self, program_a, program_b):
+        assert isinstance(
+            program_a, fluid.framework.
+            Program), "The first argument should be fluid.framework.Program."
+        assert isinstance(
+            program_b, fluid.framework.
+            Program), "The second argument should be fluid.framework Program."
+
+        self.assertEqual(len(program_a.blocks), len(program_b.blocks))
+        for idx in range(len(program_a.blocks)):
+            block_a = program_a.blocks[idx]
+            block_b = program_b.blocks[idx]
+            self.assertEqual(len(block_a.ops), len(block_b.ops))
+            # self.assertEqual(len(block_a.vars), len(block_b.vars))
+            for op_idx in range(len(block_a.ops)):
+                self.assertEqual(block_a.ops[op_idx].type,
+                                 block_b.ops[op_idx].type)
+            for var_key in list(block_a.vars.keys()):
+                if block_a.vars[var_key].type == core.VarDesc.VarType.RAW:
+                    continue
+                self.assertTrue(block_b.has_var(var_key))
+
     def check_prune_correctness(self, method, feed_dict, optimizer):
         with self.program_scope_guard():
             loss = method(use_feed=False)
 
             main_program = fluid.default_main_program()
             test_prog_orig = main_program.clone(for_test=True)
-            optimizer(learning_rate=0.001).minimize(loss)
+            optimizer().minimize(loss)
             test_prog_prune = main_program.clone(for_test=True)
 
             place = core.CPUPlace()
@@ -46,6 +68,7 @@ class TestProgramPruneBackward(unittest.TestCase):
                                        feed=feed_dict,
                                        fetch_list=[loss.name])
 
+            self.program_compare(test_prog_orig, test_prog_prune)
             self.assertEqual(loss_data_orig, loss_data_prune)
 
     def _init_fc_data(self):
@@ -55,20 +78,32 @@ class TestProgramPruneBackward(unittest.TestCase):
         return img, label
 
     def test_simple_fc_net(self):
+        def optimizer():
+            optimizer = fluid.optimizer.SGD(
+                learning_rate=0.001,
+                regularization=fluid.regularizer.L2Decay(1e-4))
+            return optimizer
+
         img, label = self._init_fc_data()
         self.check_prune_correctness(
             method=simple_fc_net,
             feed_dict={"image": img,
                        "label": label},
-            optimizer=fluid.optimizer.SGD)
+            optimizer=optimizer)
 
     def test_batchnorm_fc(self):
+        def optimizer():
+            optimizer = fluid.optimizer.SGD(
+                learning_rate=0.001,
+                regularization=fluid.regularizer.L2Decay(1e-4))
+            return optimizer
+
         img, label = self._init_fc_data()
         self.check_prune_correctness(
             method=fc_with_batchnorm,
             feed_dict={"image": img,
                        "label": label},
-            optimizer=fluid.optimizer.SGD)
+            optimizer=optimizer)
 
     def test_seresnet(self):
         self.check_prune_correctness(
@@ -77,13 +112,17 @@ class TestProgramPruneBackward(unittest.TestCase):
             optimizer=seresnext_net.optimizer)
 
     def test_transformer(self):
+        def optimizer():
+            optimizer = fluid.optimizer.Adam(
+                learning_rate=0.001,
+                regularization=fluid.regularizer.L2Decay(1e-4))
+            return optimizer
+
         # the program argument is used to distinguish Program and CompiledProgram
         feed_dict = get_feed_data_reader().get_next(
             fluid.Executor(core.CPUPlace()), fluid.default_main_program())
         self.check_prune_correctness(
-            method=transformer,
-            feed_dict=feed_dict,
-            optimizer=fluid.optimizer.Adam)
+            method=transformer, feed_dict=feed_dict, optimizer=optimizer)
 
     @contextlib.contextmanager
     def program_scope_guard(self):
