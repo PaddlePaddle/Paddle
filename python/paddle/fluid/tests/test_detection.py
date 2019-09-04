b5eb1aebbb38fd96b4579cdb0b91fa4ab6a488df
jerrywgz <jerrywgz@126.com>
2019-09-04 09:34:20 +0000

    fix test_detection.py, test=develop

--- python/paddle/fluid/tests/test_detection.py
+++ python/paddle/fluid/tests/test_detection.py
@@ -528,7 +528,18 @@ class TestMulticlassNMS(unittest.TestCase):
                 name='bboxes', shape=[-1, 10, 4], dtype='float32')
             scores = layers.data(name='scores', shape=[-1, 10], dtype='float32')
             output = layers.multiclass_nms(bboxes, scores, 0.3, 400, 200, 0.7)
-            output2, index = layers.multiclass_nms(
+            self.assertIsNotNone(output)
+
+
+class TestMulticlassNMS2(unittest.TestCase):
+    def test_multiclass_nms2(self):
+        program = Program()
+        with program_guard(program):
+            bboxes = layers.data(
+                name='bboxes', shape=[-1, 10, 4], dtype='float32')
+            scores = layers.data(name='scores', shape=[-1, 10], dtype='float32')
+            output = layers.multiclass_nms2(bboxes, scores, 0.3, 400, 200, 0.7)
+            output2, index = layers.multiclass_nms2(
                 bboxes, scores, 0.3, 400, 200, 0.7, return_index=True)
             self.assertIsNotNone(output)
             self.assertIsNotNone(output2)
