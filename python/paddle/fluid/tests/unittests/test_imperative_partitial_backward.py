f3b27ba294297b6ff538a8bbd7b2eabfecbf0341
songyouwei <youwei0314@gmail.com>
2019-09-09 14:55:13 +0800

    bug fix for list equals, test=develop

--- python/paddle/fluid/tests/unittests/test_imperative_partitial_backward.py
+++ python/paddle/fluid/tests/unittests/test_imperative_partitial_backward.py
@@ -41,8 +41,9 @@ class TestImperativePartitialBackward(unittest.TestCase):
             optimizer = fluid.optimizer.AdamOptimizer()
             _, params_grads = optimizer.minimize(loss)
 
-            self.assertListEqual([p.name for p in fc1.parameters()],
-                                 [p_g[0].name for p_g in params_grads])
+            self.assertListEqual(
+                sorted([p.name for p in fc1.parameters()]),
+                sorted([p_g[0].name for p_g in params_grads]))
 
             fc1.clear_gradients()
             fc2.clear_gradients()
