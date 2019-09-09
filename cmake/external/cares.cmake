8047ff5d1484d5e0061b4b24e3c02a0e88656799
peizhilin <wopeizl@163.com>
2019-09-10 00:38:57 +0800

    t

--- cmake/external/cares.cmake
+++ cmake/external/cares.cmake
@@ -25,6 +25,12 @@ SET(CARES_SOURCES_DIR ${THIRD_PARTY_PATH}/cares)
 SET(CARES_INSTALL_DIR ${THIRD_PARTY_PATH}/install/cares)
 SET(CARES_INCLUDE_DIR "${CARES_INSTALL_DIR}/include/" CACHE PATH "cares include directory." FORCE)
 
+if(NOT APPLE AND NOT WIN32)
+    if(${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER 8.0)
+        set(PATCH_COMMAND_CARES cp ${PADDLE_SOURCE_DIR}/patches/cares/ares_parse_ptr_reply.c.txt ${CARES_SOURCES_DIR}/src/extern_cares/ares_parse_ptr_reply.c)
+    endif()
+endif()
+
 ExternalProject_Add(
     extern_cares
     GIT_REPOSITORY "https://github.com/c-ares/c-ares.git"
@@ -33,6 +39,7 @@ ExternalProject_Add(
     UPDATE_COMMAND  ""
     CONFIGURE_COMMAND ./buildconf && ./configure --disable-shared --prefix=${CARES_INSTALL_DIR}
     BUILD_IN_SOURCE 1
+    PATCH_COMMAND ${PATCH_COMMAND_CARES}
     BUILD_COMMAND   make -j8
     INSTALL_COMMAND make install
 )
