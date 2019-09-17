dea212e89a759d33dd278bfd01a65935bef29845
LDOUBLEV <liuvv0203@outlook.com>
2019-09-17 02:27:56 +0000

    use the fluid.layer.utils.convert_to_list instead, test=develop

--- python/paddle/fluid/functional.py
+++ python/paddle/fluid/functional.py
@@ -17,55 +17,10 @@ from __future__ import print_function
 import sys
 
 from .layer_helper import LayerHelper
-
+from .layers.utils import *
 import numpy as np
 
 __all__ = ['conv2d']
-
-
-def convert_to_list(value, n, name, dtype=np.int):
-    """
-    Converts a single numerical type or iterable of numerical
-    types into an numerical type list.
-
-    Arguments:
-      value: The value to validate and convert. Could an int, or any iterable
-        of ints.
-      n: The size of the list to be returned.
-      name: The name of the argument being validated, e.g. "stride" or
-        "filter_size". This is only used to format error messages.
-      dtype: the numerical type of the element of the list to be returned.
-
-    Returns:
-      A list of n dtypes.
-
-    Raises:
-      ValueError: If something else than an int/long or iterable thereof was
-        passed.
-    """
-    if isinstance(value, dtype):
-        return [value, ] * n
-    else:
-        try:
-            value_list = list(value)
-        except TypeError:
-            raise ValueError("The " + name +
-                             "'s type must be list or tuple. Received: " + str(
-                                 value))
-        if len(value_list) != n:
-            raise ValueError("The " + name + "'s length must be " + str(n) +
-                             ". Received: " + str(value))
-        for single_value in value_list:
-            try:
-                dtype(single_value)
-            except (ValueError, TypeError):
-                raise ValueError(
-                    "The " + name + "'s type must be a list or tuple of " + str(
-                        n) + " " + str(dtype) + " . Received: " + str(
-                            value) + " "
-                    "including element " + str(single_value) + " of type" + " "
-                    + str(type(single_value)))
-        return value_list
 
 
 def conv2d(input,
@@ -116,9 +71,9 @@ def conv2d(input,
           data = np.random.random((2, 3, 4, 4)).astype(np.float32)
           filter = np.random.random((8, 3, 3, 3)).astype(np.float32)
           with fluid.dygraph.guard():
-	      data = fluid.dygraph.to_variable(data)
-	      filter = fluid.dygraph.to_variable(filter)
-	      res = fluid.functional.conv2d(data, filter)
+              data = fluid.dygraph.to_variable(data)
+              filter = fluid.dygraph.to_variable(filter)
+              res = fluid.functional.conv2d(data, filter)
     """
     helper = LayerHelper("conv2d_with_filter", **locals())
     num_channels = input.shape[1]
