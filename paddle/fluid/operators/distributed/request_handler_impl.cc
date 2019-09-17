4dbe544f70f434586645d458649d18af5624e117
chengmo <chengmo@baidu.com>
2019-09-17 20:53:37 +0800

    add log to check why recv lodtensor

--- paddle/fluid/operators/distributed/request_handler_impl.cc
+++ paddle/fluid/operators/distributed/request_handler_impl.cc
@@ -116,6 +116,7 @@ bool RequestGetHandler::Handle(const std::string& varname,
         VLOG(3) << "copying " << varname << " to " << param_bak_name;
         framework::TensorCopy(t_orig, dev_ctx_->GetPlace(), t);
       }
+      VLOG(1)<<"AsyncSparseParamUpdateRecorder "<<varname <<AsyncSparseParamUpdateRecorder::GetInstance()->HasParam(varname);
       if (AsyncSparseParamUpdateRecorder::GetInstance()->HasParam(varname) &&
           !table_name.empty()) {
         std::vector<int64_t> updated_rows;
