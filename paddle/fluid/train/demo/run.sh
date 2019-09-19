51b1d4848ba688ae3322de2d474794846d8f90d0
mapingshuo <mps2012@yeah.net>
2019-09-19 16:46:20 +0800

    code cleaning

--- paddle/fluid/train/demo/run.sh
+++ paddle/fluid/train/demo/run.sh
@@ -7,8 +7,6 @@ PADDLE_ROOT=$1
 
 # download models
 function download() {
-    #wget yq01-sys-hic-v100-box-a225-0169.yq01.baidu.com:8123/main_program
-    #wget yq01-sys-hic-v100-box-a225-0169.yq01.baidu.com:8123/startup_program
     echo "downloading is currently not supported"
 }
 
