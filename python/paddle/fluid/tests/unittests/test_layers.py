1f0b19615cd64c57dd1c3029604225e8127a9000
songyouwei <youwei0314@gmail.com>
2019-09-23 13:51:07 +0800

    update test_custom_layer_with_kwargs, test=develop

--- python/paddle/fluid/tests/unittests/test_layers.py
+++ python/paddle/fluid/tests/unittests/test_layers.py
@@ -84,18 +84,31 @@ class LayerTest(unittest.TestCase):
 class TestLayer(LayerTest):
     def test_custom_layer_with_kwargs(self):
         class CustomLayer(fluid.Layer):
-            def __init__(self, name_scope):
+            def __init__(self, name_scope, fc1_size=4):
                 super(CustomLayer, self).__init__(name_scope)
+                self.fc1 = nn.FC('fc1',
+                                 size=fc1_size,
+                                 bias_attr=False,
+                                 num_flatten_dims=1)
+                self.fc2 = nn.FC('fc2',
+                                 size=1,
+                                 bias_attr=False,
+                                 num_flatten_dims=1)
 
-            def forward(self, x, b=0, multiples=1):
-                return x * multiples + b
+            def forward(self, x, do_fc2=False):
+                ret = self.fc1(x)
+                if do_fc2:
+                    ret = self.fc2(ret)
+                return ret
 
         with self.dynamic_graph():
             inp = np.ones([3, 3], dtype='float32')
             x = base.to_variable(inp)
-            cust_layer = CustomLayer('cust')
-            ret = cust_layer(x, multiples=2)
-            self.assertTrue(np.array_equal(ret.numpy(), inp * 2))
+            custom = CustomLayer('custom', fc1_size=2)
+            ret = custom(x, do_fc2=False)
+            self.assertTrue(np.array_equal(ret.numpy().shape, [3, 2]))
+            ret = custom(x, do_fc2=True)
+            self.assertTrue(np.array_equal(ret.numpy().shape, [3, 1]))
 
     def test_fc(self):
         inp = np.ones([3, 32, 32], dtype='float32')
