15aadfa81cc3f5a9e8068e0bbb1c550ad998cbe9
songyouwei <youwei0314@gmail.com>
2019-09-23 14:00:40 +0800

    fix UserList compatibility, test=develop

--- python/paddle/fluid/dygraph/nn.py
+++ python/paddle/fluid/dygraph/nn.py
@@ -24,7 +24,7 @@ from ..param_attr import ParamAttr
 from ..initializer import Normal, Constant, NumpyArrayInitializer
 import numpy as np
 import logging
-from collections import UserList
+from future.moves.collections import UserList
 
 __all__ = [
     'Conv2D', 'Conv3D', 'Pool2D', 'FC', 'BatchNorm', 'Embedding', 'GRUUnit',
