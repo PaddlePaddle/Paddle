bb85dbc0ca684070ec36d1ed6bbf62dbf20d4393
ForFishes <2282912238@qq.com>
2019-09-24 11:26:45 +0800

    treat broadcast as non-initial, test=develop

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -1833,6 +1833,11 @@ class Block(object):
                 init_ops = []
                 for op in block.ops:
                     if var.name in op.output_arg_names:
+                        #In startup_program, "c_broadcast" and "c_sync_comm_stream"
+                        #are treated as initialization ops that cause error. 
+                        #Think of "c_broadcast" and "c_sync_comm_stream" as a special case here.
+                        if op.type in ["c_broadcast", "c_sync_comm_stream"]:
+                            continue
                         init_ops.append(op)
                 return init_ops
 
