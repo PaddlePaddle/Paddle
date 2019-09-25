4aa517f9f9d8497dbd132e91ff0f5ac55343f145
ForFishes <2282912238@qq.com>
2019-09-25 19:45:40 +0800

    fix indentation, test=develop test=document_fix test=document_preview

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -1015,15 +1015,15 @@ def eye(num_rows, num_columns=None, batch_shape=None, dtype='float32'):
 
           import paddle.fluid as fluid
  	      data = fluid.layers.eye(3, dtype='int32')
-	      # [[1, 0, 0]
+          # [[1, 0, 0]
           #  [0, 1, 0]
-	      #  [0, 0, 1]]
-    
+          #  [0, 0, 1]]
+
           data = fluid.layers.eye(2, 3, dtype='int32')
-	      # [[1, 0, 0]
+          # [[1, 0, 0]
           #  [0, 1, 0]]
-    
-	      data = fluid.layers.eye(2, batch_shape=[3])
+
+          data = fluid.layers.eye(2, batch_shape=[3])
           # Construct a batch of 3 identity tensors, each 2 x 2.
           # data[i, :, :] is a 2 x 2 identity tensor, i = 0, 1, 2.
 
