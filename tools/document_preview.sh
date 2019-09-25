4673517e01981ca901ef67b16487854d52e8340e
tianshuo78520a <707759223@qq.com>
2019-09-25 15:22:34 +0800

    fix PR_CI_Document_Preview CI;test=develop;test=document_fix;test=document_preview

--- tools/document_preview.sh
+++ tools/document_preview.sh
@@ -5,11 +5,13 @@ cd ${PADDLE_ROOT}
 pip install /paddle/build/opt/paddle/share/wheels/*.whl
 git clone https://github.com/PaddlePaddle/FluidDoc
 git clone https://github.com/tianshuo78520a/PaddlePaddle.org.git
-cd  ${PADDLE_ROOT}/PaddlePaddle.org
-git reset 3feaa68376d8423e41d076814e901e6bf108c705
+cd ${PADDLE_ROOT}/FluidDoc
+export GIT_SSL_NO_VERIFY=1
+git pull
 cd ${PADDLE_ROOT}/FluidDoc/doc/fluid/api
 sh gen_doc.sh
-apt-get update && apt-get install -y python-dev build-essential
+cd  ${PADDLE_ROOT}/PaddlePaddle.org
+git reset 3feaa68376d8423e41d076814e901e6bf108c705
 cd ${PADDLE_ROOT}/PaddlePaddle.org/portal
 pip install -r requirements.txt
 #If the default port is not occupied, you can use port 8000, you need to replace it with a random port on the CI.
