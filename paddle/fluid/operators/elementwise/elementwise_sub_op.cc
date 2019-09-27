6b68f75adb4f82b939a64929d935858d15016e40
danleifeng <52735331+danleifeng@users.noreply.github.com>
2019-09-27 11:08:01 +0800

    Polish English docs of elementwise_add/sub/mul/div (#20027)
    Polish English docs of elementwise_add/sub/mul/div

--- paddle/fluid/operators/elementwise/elementwise_sub_op.cc
+++ paddle/fluid/operators/elementwise/elementwise_sub_op.cc
@@ -20,6 +20,28 @@ limitations under the License. */
 namespace paddle {
 namespace operators {
 
+class ElementwiseSubOpMaker : public ElementwiseOpMaker {
+ protected:
+  std::string GetName() const override { return "Sub"; }
+  std::string GetEquation() const override { return "Out = X - Y"; }
+
+  void AddInputX() override {
+    AddInput("X",
+             "(Variable), Tensor or LoDTensor of any dimensions. Its dtype "
+             "should be int32, int64, float32, float64.");
+  }
+
+  void AddInputY() override {
+    AddInput("Y",
+             "(Variable), Tensor or LoDTensor of any dimensions. Its dtype "
+             "should be int32, int64, float32, float64.");
+  }
+
+  std::string GetOpFuntionality() const override {
+    return "Substract two tensors element-wise";
+  }
+};
+
 class ElementwiseSubDoubleGradDescMaker
     : public framework::SingleGradOpDescMaker {
  public:
@@ -46,8 +68,7 @@ class ElementwiseSubDoubleGradDescMaker
 
 namespace ops = paddle::operators;
 REGISTER_ELEMWISE_GRAD_MAKER(elementwise_sub, Sub);
-REGISTER_ELEMWISE_EXPLICIT_OP_WITHOUT_GRAD(elementwise_sub, "Sub",
-                                           "Out = X - Y");
+REGISTER_ELEMWISE_EXPLICIT_OP_WITHOUT_GRAD(elementwise_sub, Sub);
 
 REGISTER_OPERATOR(elementwise_sub_grad, ops::ElementwiseOpExplicitGrad,
                   ops::ElementwiseGradOpInplace,
