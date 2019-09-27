5ebf758210c97e41be96c3bda78b1c9e32fb0fc2
tangwei <tangwei12@baidu.com>
2019-09-27 10:15:43 +0800

    add async test, test=develop

--- python/paddle/fluid/tests/unittests/test_dist_ctr.py
+++ python/paddle/fluid/tests/unittests/test_dist_ctr.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from __future__ import print_function
 
 import os
@@ -60,7 +61,32 @@ class TestDistCTR2x2_ASYNC(TestDistBase):
         self._enforce_place = "CPU"
 
     def test_dist_ctr(self):
-        self.check_with_place("dist_ctr.py", delta=100, check_error_log=True)
+        need_envs = {
+            "FLAGS_communicator_send_queue_size": "2",
+            "FLAGS_communicator_max_merge_var_num": "2",
+            "FLAGS_communicator_max_send_grad_num_before_recv": "2",
+        }
+
+        self.check_with_place(
+            "dist_ctr.py", delta=100, check_error_log=True, need_envs=need_envs)
+
+
+class TestDistCTR2x2_ASYNC2(TestDistBase):
+    def _setup_config(self):
+        self._sync_mode = False
+        self._hogwild_mode = True
+        self._enforce_place = "CPU"
+
+    def test_dist_ctr(self):
+        need_envs = {
+            "FLAGS_communicator_send_queue_size": "2",
+            "FLAGS_communicator_max_merge_var_num": "2",
+            "FLAGS_communicator_max_send_grad_num_before_recv": "2",
+            "FLAGS_communicator_independent_recv_thread": "1"
+        }
+
+        self.check_with_place(
+            "dist_ctr.py", delta=100, check_error_log=True, need_envs=need_envs)
 
 
 if __name__ == "__main__":
