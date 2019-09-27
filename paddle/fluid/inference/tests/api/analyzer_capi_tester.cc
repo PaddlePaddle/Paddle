29e01e5625c0a303ce251a424442b9ce440a81eb
FrostML <380185688@qq.com>
2019-09-27 16:27:25 +0800

    123

--- paddle/fluid/inference/tests/api/analyzer_capi_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_capi_tester.cc
@@ -20,6 +20,7 @@ limitations under the License. */
 #include <stdio.h>
 
 #include "paddle/fluid/inference/capi/c_api.h"
+#include "paddle/fluid/inference/tests/api/tester_helper.h"
 
 namespace paddle {
 namespace inference {
