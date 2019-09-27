f60a59e31a225e0e772a3793926035aba92a0d3c
石晓伟 <39303645+Shixiaowei02@users.noreply.github.com>
2019-09-27 21:00:11 +0800

    update operator compatible info, test=develop (#19978)
    * update operator compatible info, test=develop
    
    * revert cmake/version.cmake, test=develop
    
    * add unit_tests and fix bugs, test=develop
    
    * update ../paddle/fluid/framework/framework.proto, test=develop
    
    * fix bug of paddle/fluid/inference/api/analysis_predictor.cc, test=develop
    
    * update paddle/fluid/framework/version_test.cc, test=develop
    
    * add comments and rename interfaces, test=develop

--- python/paddle/fluid/io.py
+++ python/paddle/fluid/io.py
@@ -1103,6 +1103,8 @@ def save_inference_model(dirname,
         prepend_feed_ops(main_program, feeded_var_names)
         append_fetch_ops(main_program, fetch_var_names)
 
+        main_program.desc._set_version()
+        paddle.fluid.core.save_op_compatible_info(main_program.desc)
         with open(model_basename, "wb") as f:
             f.write(main_program.desc.serialize_to_string())
     else:
