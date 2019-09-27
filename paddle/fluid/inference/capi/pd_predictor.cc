c030b2a42206dd5c09d299d786808f4ed96abbbf
FrostML <380185688@qq.com>
2019-09-27 20:06:06 +0800

    123

--- paddle/fluid/inference/capi/pd_predictor.cc
+++ paddle/fluid/inference/capi/pd_predictor.cc
@@ -91,7 +91,7 @@ char** PD_GetPredictorOutputNames(PD_Predictor* predictor) {
 
 PD_ZeroCopyTensor* PD_GetPredictorInputTensor(PD_Predictor* predictor,
                                               const char* name) {
-  PD_ZeroCopyTensor* ret = nullptr;
+  PD_ZeroCopyTensor* ret;
   ret->tensor =
       *(predictor->predictor->GetInputTensor(std::string(name)).get());
   return ret;
@@ -99,7 +99,7 @@ PD_ZeroCopyTensor* PD_GetPredictorInputTensor(PD_Predictor* predictor,
 
 PD_ZeroCopyTensor* PD_GetPredictorOutputTensor(PD_Predictor* predictor,
                                                const char* name) {
-  PD_ZeroCopyTensor* ret = nullptr;
+  PD_ZeroCopyTensor* ret;
   ret->tensor =
       *(predictor->predictor->GetOutputTensor(std::string(name)).get());
   return ret;
