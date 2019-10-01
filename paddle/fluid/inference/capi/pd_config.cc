fc961c6e54572d8cef3c10da476dd468d173d075
FrostML <380185688@qq.com>
2019-10-02 00:02:53 +0800

    add the PADDLE_ENFORCE_NOT_NULL(config) for config in case the input is nullptr, test=develop

--- paddle/fluid/inference/capi/pd_config.cc
+++ paddle/fluid/inference/capi/pd_config.cc
@@ -39,6 +39,7 @@ void PD_DeleteAnalysisConfig(PD_AnalysisConfig* config) {
 void PD_SetModel(PD_AnalysisConfig* config, const char* model_dir,
                  const char* params_path) {
   LOG(INFO) << model_dir;
+  PADDLE_ENFORCE_NOT_NULL(config);
   LOG(INFO) << std::string(model_dir);
   if (!params_path) {
     config->config.SetModel(std::string(model_dir));
@@ -48,79 +49,103 @@ void PD_SetModel(PD_AnalysisConfig* config, const char* model_dir,
 }
 
 void PD_SetProgFile(PD_AnalysisConfig* config, const char* x) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SetProgFile(std::string(x));
 }
 
 void PD_SetParamsFile(PD_AnalysisConfig* config, const char* x) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SetParamsFile(std::string(x));
 }
 
 void PD_SetOptimCacheDir(PD_AnalysisConfig* config, const char* opt_cache_dir) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SetOptimCacheDir(std::string(opt_cache_dir));
 }
 
 const char* PD_ModelDir(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.model_dir().c_str();
 }
 
 const char* PD_ProgFile(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.prog_file().c_str();
 }
 
 const char* PD_ParamsFile(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.params_file().c_str();
 }
 
 void PD_EnableUseGpu(PD_AnalysisConfig* config,
                      uint64_t memory_pool_init_size_mb, int device_id) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.EnableUseGpu(memory_pool_init_size_mb, device_id);
 }
 
-void PD_DisableGpu(PD_AnalysisConfig* config) { config->config.DisableGpu(); }
+void PD_DisableGpu(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
+  config->config.DisableGpu();
+}
 
 bool PD_UseGpu(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.use_gpu();
 }
 
 int PD_GpuDeviceId(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.gpu_device_id();
 }
 
 int PD_MemoryPoolInitSizeMb(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.memory_pool_init_size_mb();
 }
 
 float PD_FractionOfGpuMemoryForPool(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.fraction_of_gpu_memory_for_pool();
 }
 
-void PD_EnableCUDNN(PD_AnalysisConfig* config) { config->config.EnableCUDNN(); }
+void PD_EnableCUDNN(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
+  config->config.EnableCUDNN();
+}
 
 bool PD_CudnnEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.cudnn_enabled();
 }
 
 void PD_SwitchIrOptim(PD_AnalysisConfig* config, bool x) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SwitchIrOptim(x);
 }
 
 bool PD_IrOptim(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.ir_optim();
 }
 
 void PD_SwitchUseFeedFetchOps(PD_AnalysisConfig* config, bool x) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SwitchUseFeedFetchOps(x);
 }
 
 bool PD_UseFeedFetchOpsEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.use_feed_fetch_ops_enabled();
 }
 
 void PD_SwitchSpecifyInputNames(PD_AnalysisConfig* config, bool x) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SwitchSpecifyInputNames(x);
 }
 
 bool PD_SpecifyInputName(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.specify_input_name();
 }
 
@@ -128,12 +153,14 @@ void PD_EnableTensorRtEngine(PD_AnalysisConfig* config, int workspace_size,
                              int max_batch_size, int min_subgraph_size,
                              Precision precision, bool use_static,
                              bool use_calib_mode) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.EnableTensorRtEngine(
       workspace_size, max_batch_size, min_subgraph_size,
       paddle::ConvertToACPrecision(precision), use_static, use_calib_mode);
 }
 
 bool PD_TensorrtEngineEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.tensorrt_engine_enabled();
 }
 
@@ -143,6 +170,7 @@ void PD_EnableAnakinEngine(PD_AnalysisConfig* config, int max_batch_size,
                            Precision precision, bool auto_config_layout,
                            char** passes_filter, int passes_filter_size,
                            char** ops_filter, int ops_filter_size) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   std::map<std::string, std::vector<int>> mis;
   if (max_input_shape) {
     for (int i = 0; i < max_input_shape_size; ++i) {
@@ -168,80 +196,101 @@ void PD_EnableAnakinEngine(PD_AnalysisConfig* config, int max_batch_size,
 }
 
 bool PD_AnakinEngineEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.anakin_engine_enabled();
 }
 
 void PD_SwitchIrDebug(PD_AnalysisConfig* config, bool x) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SwitchIrDebug(x);
 }
 
 void PD_EnableNgraph(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.EnableNgraph();
 }
 
 bool PD_NgraphEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.ngraph_enabled();
 }
 
 void PD_EnableMKLDNN(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.EnableMKLDNN();
 }
 
 void PD_SetMkldnnCacheCapacity(PD_AnalysisConfig* config, int capacity) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SetMkldnnCacheCapacity(capacity);
 }
 
 bool PD_MkldnnEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.mkldnn_enabled();
 }
 
 void PD_SetCpuMathLibraryNumThreads(PD_AnalysisConfig* config,
                                     int cpu_math_library_num_threads) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SetCpuMathLibraryNumThreads(cpu_math_library_num_threads);
 }
 
 int PD_CpuMathLibraryNumThreads(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.cpu_math_library_num_threads();
 }
 
 void PD_EnableMkldnnQuantizer(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.EnableMkldnnQuantizer();
 }
 
 bool PD_MkldnnQuantizerEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.mkldnn_quantizer_enabled();
 }
 
 void PD_SetModelBuffer(PD_AnalysisConfig* config, const char* prog_buffer,
                        size_t prog_buffer_size, const char* params_buffer,
                        size_t params_buffer_size) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.SetModelBuffer(prog_buffer, prog_buffer_size, params_buffer,
                                 params_buffer_size);
 }
 
 bool PD_ModelFromMemory(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.model_from_memory();
 }
 
 void PD_EnableMemoryOptim(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.EnableMemoryOptim();
 }
 
 bool PD_MemoryOptimEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.enable_memory_optim();
 }
 
 void PD_EnableProfile(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   config->config.EnableProfile();
 }
 
 bool PD_ProfileEnabled(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.profile_enabled();
 }
 
-void PD_SetInValid(PD_AnalysisConfig* config) { config->config.SetInValid(); }
+void PD_SetInValid(PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
+  config->config.SetInValid();
+}
 
 bool PD_IsValid(const PD_AnalysisConfig* config) {
+  PADDLE_ENFORCE_NOT_NULL(config);
   return config->config.is_valid();
 }
 }  // extern "C"
