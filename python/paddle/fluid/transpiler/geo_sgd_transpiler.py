7d4afaddd06306fcd613b0bd06e3b35cd416b311
chengmo <cmchengmo@163.com>
2019-10-03 22:57:45 +0800

    test=develop,add wait port strategy

--- python/paddle/fluid/transpiler/geo_sgd_transpiler.py
+++ python/paddle/fluid/transpiler/geo_sgd_transpiler.py
@@ -179,9 +179,8 @@ class GeoSgdTranspiler(DistributeTranspiler):
         return self.vars_info
 
     def get_trainer_program(self, wait_port=True):
-        # Todo: add wait mehtod
-        # if wait_port
-        #     wait_server_ready(self.pserver_endpoints)
+        if wait_port:
+            wait_server_ready(self.pserver_endpoints)
         return self.origin_program
 
     def get_pserver_programs(self, endpoint):
