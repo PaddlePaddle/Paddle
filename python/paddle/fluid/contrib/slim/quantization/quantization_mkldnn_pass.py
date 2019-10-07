3b64c46812134a0cc42d3a5563c84d36cd1b68ed
Michal Gallus <michal.gallus@intel.com>
2019-10-07 15:10:09 -0400

    Disable int8 mul and restore fake mul
    test=develop

--- python/paddle/fluid/contrib/slim/quantization/quantization_mkldnn_pass.py
+++ python/paddle/fluid/contrib/slim/quantization/quantization_mkldnn_pass.py
@@ -381,13 +381,13 @@ class FakeQAT2MkldnnINT8PerfPass(object):
                 next_op = op_out.outputs[0]
                 if next_op.name() not in self._mul_ops:
                     self._remove_fake_quantize(graph, op)
-                else:
-                    quant_op = self._transform_to_quantize_mkldnn(graph, op)
-                    self._transform_to_mul_mkldnn(graph, next_op, quant_op)
 
         for op in graph.all_op_nodes():
             if op.name() in self._fake_dequantize_types:
-                self._remove_fake_dequantize(graph, op)
+                op_in = graph._find_node_by_name(op.inputs, op.input("X")[0])
+                prev_op = op_in.inputs[0]
+                if prev_op.name() not in self._mul_ops:
+                    self._remove_fake_dequantize(graph, op)
         return graph
 
     def _remove_fake_quantize(self, graph, op):
