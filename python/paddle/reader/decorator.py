7877af7c0158ccad24c8e117747dad61be006786
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2019-10-08 14:01:25 +0000

    Fix api's doc, test=develop, test=document_preview, test=document_fix

--- python/paddle/reader/decorator.py
+++ python/paddle/reader/decorator.py
@@ -202,12 +202,33 @@ def buffered(reader, size):
     buffer. Reading from the buffered data reader will proceed as long
     as the buffer is not empty.
 
-    :param reader: the data reader to read from.
-    :type reader: callable
-    :param size: max buffer size.
-    :type size: int
+    Args:
+        reader (callable): The data reader to read from.
+        size (int): Max buffer size.
 
-    :returns: the buffered data reader.
+    Return:
+        Variable: The buffered data reader.
+
+    Examples:
+        .. code-block:: python
+
+            import paddle.reader as reader
+            import time
+
+            def reader_creator_10(dur):
+                def reader():
+                    for i in range(10):
+                        time.sleep(dur)
+                        yield i
+                return reader
+
+            for size in range(20):
+                b = reader.buffered(reader_creator_10(0), size)
+                c = 0
+                for i in b():
+                    assert i == c
+                    c += 1
+                assert c == 10
     """
 
     class EndSignal():
@@ -269,16 +290,51 @@ def xmap_readers(mapper, reader, process_num, buffer_size, order=False):
     """
     Use multi-threads to map samples from reader by a mapper defined by user.
 
-    Args:
-        mapper (callable): a function to map the data from reader.
-        reader (callable): a data reader which yields the data. 
-        process_num (int): thread number to handle original sample.
-        buffer_size (int): size of the queue to read data in. 
-        order (bool): whether to keep the data order from original reader. 
+    Parameters:
+        mapper (callable): A function to map the data from reader.
+        reader (callable): A data reader which yields the data. 
+        process_num (int): Thread number to handle original sample.
+        buffer_size (int): Size of the queue to read data in. 
+        order (bool): Whether to keep the data order from original reader. 
             Default False.
 
     Returns:
-        callable: a decorated reader with data mapping. 
+        A decorated reader with data mapping.
+
+    Example:
+
+        .. code-block:: python
+
+            import paddle.reader as reader
+            import time
+
+            def reader_creator_10(dur):
+                def reader():
+                    for i in range(10):
+                        time.sleep(dur)
+                        yield i
+                return reader
+
+            def mapper(x):
+                return (x + 1)
+
+            orders = (True, False)
+            thread_num = (1, 2, 4, 8, 16)
+            buffer_size = (1, 2, 4, 8, 16)
+            for order in orders:
+                for t_num in thread_num:
+                    for size in buffer_size:
+                        user_reader = reader.xmap_readers(mapper,
+                                                          reader_creator_10(0),
+                                                          t_num, size, order)
+                        for n in range(3):
+                            result = list()
+                            for i in user_reader():
+                                result.append(i)
+                            if not order:
+                                result.sort()
+                            for idx, e in enumerate(result):
+                                assert e == mapper(idx)
     """
     end = XmapEndSignal()
 
