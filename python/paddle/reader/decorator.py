d3fb5bdd45ea0f149be3cf081d50e31729da02e7
DannyIsFunny <912790387@qq.com>
2019-10-08 13:23:14 +0000

    test=develop

--- python/paddle/reader/decorator.py
+++ python/paddle/reader/decorator.py
@@ -71,7 +71,7 @@ def map_readers(func, *readers):
     Args:
         func: a function to read data and compute result, the output of this function 
               will be set as the output of the resulted data reader.
-        readers : list of readers whose outputs will be used as arguments of func,
+        readers : list of readers whose outputs will be used as arguments of func.
               For more information about Reader, please refer to :ref:`api_guide_python_reader` .
  
     Returns:
