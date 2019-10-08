89b3b8866adf60092e289af10003aa04e8750b52
FrostML <380185688@qq.com>
2019-10-08 09:25:32 +0800

    remove gflags, test=develop

--- paddle/fluid/inference/capi/pd_tensor.cc
+++ paddle/fluid/inference/capi/pd_tensor.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <gflags/gflags.h>
-#include <glog/logging.h>
 #include <algorithm>
 #include <vector>
 #include "paddle/fluid/inference/capi/c_api.h"
