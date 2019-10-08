abea26aa289fa4fae74a203ef35ed43e01b66d53
cjt222 <806512756@qq.com>
2019-10-08 13:10:28 +0000

    fix fluid.data code example, test=develop, test=document_preview, test=document_fix

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -15290,24 +15290,24 @@ def deformable_roi_pooling(input,
                            shape=[2, 192, 64, 64], 
                            dtype='float32')                   
         rois = fluid.data(name="rois",
-                          shape=[4],
+                          shape=[10, 4],
                           dtype='float32', 
                           lod_level=1)
         trans = fluid.data(name="trans",
                            shape=[2, 384, 64, 64], 
                            dtype='float32') 
-        x = fluid.layers.nn.deformable_roi_pooling(input=input, 
-                                                   rois=rois, 
-                                                   trans=trans, 
-                                                   no_trans=False,
-                                                   spatial_scale=1.0, 
-                                                   group_size=(1, 1),
-                                                   pooled_height=8,
-                                                   pooled_width=8,
-                                                   part_size=(8, 8),
-                                                   sample_per_part=4, 
-                                                   trans_std=0.1,
-                                                   position_sensitive=True)
+        x = fluid.layers.deformable_roi_pooling(input=input, 
+                                                rois=rois, 
+                                                trans=trans, 
+                                                no_trans=False,
+                                                spatial_scale=1.0, 
+                                                group_size=(1, 1),
+                                                pooled_height=8,
+                                                pooled_width=8,
+                                                part_size=(8, 8),
+                                                sample_per_part=4, 
+                                                trans_std=0.1,
+                                                position_sensitive=True)
   
         # position_sensitive=False
         import paddle.fluid as fluid
@@ -15315,24 +15315,24 @@ def deformable_roi_pooling(input,
                            shape=[2, 192, 64, 64], 
                            dtype='float32')                   
         rois = fluid.data(name="rois",
-                          shape=[4],
+                          shape=[10, 4],
                           dtype='float32', 
                           lod_level=1)
         trans = fluid.data(name="trans",
                            shape=[2, 384, 64, 64], 
                            dtype='float32') 
-        x = fluid.layers.nn.deformable_roi_pooling(input=input, 
-                                                   rois=rois, 
-                                                   trans=trans, 
-                                                   no_trans=False,
-                                                   spatial_scale=1.0, 
-                                                   group_size=(1, 1),
-                                                   pooled_height=8,
-                                                   pooled_width=8,
-                                                   part_size=(8, 8),
-                                                   sample_per_part=4, 
-                                                   trans_std=0.1,
-                                                   position_sensitive=False)
+        x = fluid.layers.deformable_roi_pooling(input=input, 
+                                                rois=rois, 
+                                                trans=trans, 
+                                                no_trans=False,
+                                                spatial_scale=1.0, 
+                                                group_size=(1, 1),
+                                                pooled_height=8,
+                                                pooled_width=8,
+                                                part_size=(8, 8),
+                                                sample_per_part=4, 
+                                                trans_std=0.1,
+                                                position_sensitive=False)
     """
 
     input_channels = input.shape[1]
