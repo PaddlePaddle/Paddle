642b98780551ab7cdc4a01a11c767afbc35be8f0
shippingwang <shipeng1108@163.com>
2019-10-09 13:04:30 +0000

    add density prior box doc

--- python/paddle/fluid/layers/detection.py
+++ python/paddle/fluid/layers/detection.py
@@ -1725,18 +1725,61 @@ def density_prior_box(input,
 
     Examples:
         .. code-block:: python
+            #declarative mode
+	    import paddle.fluid as fluid
+            import numpy as np
+	    input = fluid.data(name="input", shape=[None,3,6,9])
+	    image = fluid.data(name="image", shape=[None,3,9,12])
+	    box, var = fluid.layers.density_prior_box(
+                 input=input,
+                 image=image,
+                 densities=[4, 2, 1],
+                 fixed_sizes=[32.0, 64.0, 128.0],
+                 fixed_ratios=[1.],
+                 clip=True,
+                 flatten_to_2d=True)
 
-            import paddle.fluid as fluid
-            input = fluid.layers.data(name="input", shape=[3,6,9])
-            images = fluid.layers.data(name="images", shape=[3,9,12])
-            box, var = fluid.layers.density_prior_box(
-                input=input,
-                image=images,
-                densities=[4, 2, 1],
-                fixed_sizes=[32.0, 64.0, 128.0],
-                fixed_ratios=[1.],
-                clip=True,
-                flatten_to_2d=True)
+            place = fluid.CPUPlace()
+	    exe = fluid.Executor(place)
+	    exe.run(fluid.default_startup_program())
+ 
+	    # prepare a batch of data
+	    input_data = np.random.rand(1,3,6,9).astype("float32")
+	    image_data = np.random.rand(1,3,9,12).astype("float32")
+ 
+	    box_out, var_out = exe.run(
+	        fluid.default_main_program(),
+                feed={"input":input_data,
+	              "image":image_data},
+                fetch_list=[box,var],
+                return_numpy=True)
+ 
+	    print(mask_out.shape)
+	    # (1134, 4)
+            print(z_out.shape)
+	    # (1134, 4)
+
+
+	    #imperative mode
+	    import paddle.fluid.dygraph as dg
+
+	    with dg.guard(place) as g:
+    		input = dg.to_variable(input_data)
+    		image = dg.to_variable(image_data)
+    		box, var = fluid.layers.density_prior_box(
+		    input=input,
+		    image=image,
+		    densities=[4, 2, 1],
+		    fixed_sizes=[32.0, 64.0, 128.0],
+		    fixed_ratios=[1.],
+		    clip=True)
+
+    		print(box.shape)
+		# [6L, 9L, 21L, 4L]
+		print(var.shape)
+		# [6L, 9L, 21L, 4L]
+
+
     """
     helper = LayerHelper("density_prior_box", **locals())
     dtype = helper.input_dtype()
