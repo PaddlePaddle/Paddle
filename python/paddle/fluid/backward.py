0313a240d06f2acd82d9c6c4502a4aa7ea69f7b6
songyouwei <youwei0314@gmail.com>
2019-10-09 11:11:53 +0800

    polish append_backward, test=document_fix, test=develop

--- python/paddle/fluid/backward.py
+++ python/paddle/fluid/backward.py
@@ -977,8 +977,8 @@ def append_backward(loss,
         .. code-block:: python
 
             import paddle.fluid as fluid
-            x = fluid.layers.data(name='x', shape=[13], dtype='float32')
-            y = fluid.layers.data(name='y', shape=[1], dtype='float32')
+            x = fluid.data(name='x', shape=[None, 13], dtype='float32')
+            y = fluid.data(name='y', shape=[None, 1], dtype='float32')
 
             y_predict = fluid.layers.fc(input=x, size=1, act=None)
             loss = fluid.layers.square_error_cost(input=y_predict, label=y)
