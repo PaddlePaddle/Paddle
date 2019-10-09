506afffc80cb16f3358e791ec3c329e2b1b7d96d
xyzhou-puck <xiyzhouang@gmail.com>
2019-10-09 11:45:20 +0000

    test=develop

--- python/paddle/fluid/tests/unittests/test_debugger.py
+++ python/paddle/fluid/tests/unittests/test_debugger.py
@@ -60,52 +60,58 @@ class TestDebugger(unittest.TestCase):
     def test_fast_nan_debug(self):
         p = Program()
         with fluid.program_guard(p):
-            x = fluid.layers.data(name="x", shape=[4], dtype='float32')
-            y = fluid.layers.data(name="y", shape=[1], dtype='float32')
+            with fluid.unique_name.guard():
+                x = fluid.layers.data(name="x", shape=[4], dtype='float32')
+                y = fluid.layers.data(name="y", shape=[1], dtype='float32')
 
-            hidden = x
-            for i in range(10):
-                hidden = fluid.layers.fc(input=hidden, size=200, act="sigmoid")
+                hidden = x
+                for i in range(10):
+                    hidden = fluid.layers.fc(input=hidden,
+                                             size=200,
+                                             act="sigmoid")
 
-            y_predict = fluid.layers.fc(input=hidden, size=1, act=None)
+                y_predict = fluid.layers.fc(input=hidden, size=1, act=None)
 
-        debugger.prepare_fast_nan_inf_debug(p)
+            debugger.prepare_fast_nan_inf_debug(p)
 
-        for _block in p.blocks:
-            _vars_in_prog = [_var_name for _var_name in _block.vars]
-            for _var_name in _vars_in_prog:
-                is_good = _var_name.startswith("debug_var_") or (
-                    ("debug_var_" + _var_name + "_0") in _vars_in_prog)
-                assert is_good
-
-    def test_fast_nan_debug_skiplist(self):
-        p = Program()
-        with fluid.program_guard(p):
-            x = fluid.layers.data(name="x", shape=[4], dtype='float32')
-            y = fluid.layers.data(name="y", shape=[1], dtype='float32')
-
-            hidden = x
-            for i in range(10):
-                hidden = fluid.layers.fc(input=hidden,
-                                         size=200,
-                                         act="sigmoid",
-                                         name="fc_" + str(i))
-
-            y_predict = fluid.layers.fc(input=hidden, size=1, act=None)
-
-        debugger.prepare_fast_nan_inf_debug(p)
-
-        for _block in p.blocks:
-            _vars_in_prog = [_var_name for _var_name in _block.vars]
-            for _var_name in _vars_in_prog:
-                if "fc_0" not in _var_name:
-                    print(_var_name)
+            for _block in p.blocks:
+                _vars_in_prog = [_var_name for _var_name in _block.vars]
+                for _var_name in _vars_in_prog:
                     is_good = _var_name.startswith("debug_var_") or (
                         ("debug_var_" + _var_name + "_0") in _vars_in_prog)
                     assert is_good
 
-                else:
-                    assert "debug_var_" not in _var_name
+    def test_fast_nan_debug_skiplist(self):
+        q = Program()
+        with fluid.program_guard(q):
+            with fluid.unique_name.guard():
+                x = fluid.layers.data(name="x", shape=[4], dtype='float32')
+                y = fluid.layers.data(name="y", shape=[1], dtype='float32')
+
+                hidden = x
+                for i in range(10):
+                    hidden = fluid.layers.fc(input=hidden,
+                                             size=200,
+                                             act="sigmoid",
+                                             name="fc_" + str(i))
+
+                y_predict = fluid.layers.fc(input=hidden,
+                                            size=1,
+                                            act=None,
+                                            name="final")
+
+            debugger.prepare_fast_nan_inf_debug(q, skip_list=["final"])
+
+            for _block in q.blocks:
+                _vars_in_prog = [_var_name for _var_name in _block.vars]
+                for _var_name in _vars_in_prog:
+                    if "final" not in _var_name:
+                        is_good = _var_name.startswith("debug_var_") or (
+                            ("debug_var_" + _var_name + "_1") in _vars_in_prog)
+                        assert is_good
+
+                    else:
+                        assert "debug_var_" not in _var_name
 
 
 if __name__ == '__main__':
