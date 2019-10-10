6538537c09f05cea5a88da25a16275ab1175893e
gfwm2013 <2449465946@qq.com>
2019-10-10 03:20:38 +0000

    Add English doc of reader
    test=develop

--- python/paddle/fluid/reader.py
+++ python/paddle/fluid/reader.py
@@ -615,6 +615,11 @@ class PyReader(DataLoaderBase):
            EPOCH_NUM = 3
            ITER_NUM = 5
            BATCH_SIZE = 3
+           
+           def network(image, label):
+               # User-defined network, here is an example of softmax regression.
+               predict = fluid.layers.fc(input=image, size=10, act='softmax')           
+               return fluid.layers.cross_entropy(input=predict, label=label)
 
            def reader_creator_random_image_and_label(height, width):
                def reader():
@@ -626,8 +631,8 @@ class PyReader(DataLoaderBase):
                        yield fake_image, fake_label
                return reader
 
-           image = fluid.layers.data(name='image', shape=[784, 784], dtype='float32')
-           label = fluid.layers.data(name='label', shape=[1], dtype='int64')
+           image = fluid.data(name='image', shape=[None, 784, 784], dtype='float32')
+           label = fluid.data(name='label', shape=[None, 1], dtype='int64')
 
            reader = fluid.io.PyReader(feed_list=[image, label],
                                       capacity=4,
@@ -636,8 +641,8 @@ class PyReader(DataLoaderBase):
            user_defined_reader = reader_creator_random_image_and_label(784, 784)
            reader.decorate_sample_list_generator(
                paddle.batch(user_defined_reader, batch_size=BATCH_SIZE))
-           # definition of network is omitted
-           executor = fluid.Executor(fluid.CUDAPlace(0))
+           loss = network(image, label)
+           executor = fluid.Executor(fluid.CPUPlace())
            executor.run(fluid.default_startup_program())
            for i in range(EPOCH_NUM):
                reader.start()
@@ -664,27 +669,36 @@ class PyReader(DataLoaderBase):
            EPOCH_NUM = 3
            ITER_NUM = 5
            BATCH_SIZE = 10
-
+           
+           def network(image, label):
+               # User-defined network, here is an example of softmax regression.
+               predict = fluid.layers.fc(input=image, size=10, act='softmax')           
+               return fluid.layers.cross_entropy(input=predict, label=label)
+           
            def reader_creator_random_image(height, width):
                def reader():
                    for i in range(ITER_NUM):
-                       yield np.random.uniform(low=0, high=255, size=[height, width]),
+                       fake_image = np.random.uniform(low=0, high=255, size=[height, width])
+                       fake_label = np.ones([1])
+                       yield fake_image, fake_label
                return reader
 
-           image = fluid.layers.data(name='image', shape=[784, 784], dtype='float32')
-           reader = fluid.io.PyReader(feed_list=[image], capacity=4, iterable=True, return_list=False)
+           image = fluid.data(name='image', shape=[None, 784, 784], dtype='float32')
+           label = fluid.data(name='label', shape=[None, 1], dtype='int64')
+           reader = fluid.io.PyReader(feed_list=[image, label], capacity=4, iterable=True, return_list=False)
 
            user_defined_reader = reader_creator_random_image(784, 784)
            reader.decorate_sample_list_generator(
                paddle.batch(user_defined_reader, batch_size=BATCH_SIZE),
-               fluid.core.CUDAPlace(0))
-           # definition of network is omitted
-           executor = fluid.Executor(fluid.CUDAPlace(0))
-           executor.run(fluid.default_main_program())
+               fluid.core.CPUPlace())
+           
+           loss = network(image, label)
+           executor = fluid.Executor(fluid.CPUPlace())
+           executor.run(fluid.default_startup_program())
 
            for _ in range(EPOCH_NUM):
                for data in reader():
-                   executor.run(feed=data)
+                   executor.run(feed=data, fetch_list=[loss])
 
 
         3. If return_list=True, the return values would be presented as list instead of dict. 
@@ -745,8 +759,8 @@ class PyReader(DataLoaderBase):
         Start the data feeding thread. 
         Can only call when the reader object is not iterable.  
         
-	    Example:
-	        .. code-block:: python
+	Example:
+	    .. code-block:: python
     
                 import paddle
                 import paddle.fluid as fluid
@@ -758,12 +772,12 @@ class PyReader(DataLoaderBase):
                     for i in range(5):
                         yield np.random.uniform(low=0, high=255, size=[784, 784]),
 
-                image = fluid.layers.data(name='image', shape=[784, 784], dtype='float32')
+                image = fluid.data(name='image', shape=[None, 784, 784], dtype='float32')
                 reader = fluid.io.PyReader(feed_list=[image], capacity=4, iterable=False)
                 reader.decorate_sample_list_generator(
                     paddle.batch(generator, batch_size=BATCH_SIZE))
 
-                executor = fluid.Executor(fluid.CUDAPlace(0))
+                executor = fluid.Executor(fluid.CPUPlace())
                 executor.run(fluid.default_startup_program())
                 for i in range(3):
                     reader.start()
@@ -795,12 +809,12 @@ class PyReader(DataLoaderBase):
                     for i in range(5):
                         yield np.random.uniform(low=0, high=255, size=[784, 784]),
 
-                image = fluid.layers.data(name='image', shape=[784, 784], dtype='float32')
+                image = fluid.data(name='image', shape=[None, 784, 784], dtype='float32')
                 reader = fluid.io.PyReader(feed_list=[image], capacity=4, iterable=False)
                 reader.decorate_sample_list_generator(
                     paddle.batch(generator, batch_size=BATCH_SIZE))
 
-                executor = fluid.Executor(fluid.CUDAPlace(0))
+                executor = fluid.Executor(fluid.CPUPlace())
                 executor.run(fluid.default_startup_program())
                 for i in range(3):
                     reader.start()
@@ -849,6 +863,11 @@ class PyReader(DataLoaderBase):
                 ITER_NUM = 15
                 BATCH_SIZE = 3
 
+                def network(image, label):
+                    # User-defined network, here is an example of softmax regression.
+                    predict = fluid.layers.fc(input=image, size=10, act='softmax')           
+                    return fluid.layers.cross_entropy(input=predict, label=label) 
+
                 def random_image_and_label_generator(height, width):
                     def generator():
                         for i in range(ITER_NUM):
@@ -859,21 +878,21 @@ class PyReader(DataLoaderBase):
                             yield fake_image, fake_label
                     return generator
 
-                image = fluid.layers.data(name='image', shape=[784, 784], dtype='float32')
-                label = fluid.layers.data(name='label', shape=[1], dtype='int32')
+                image = fluid.data(name='image', shape=[None, 784, 784], dtype='float32')
+                label = fluid.data(name='label', shape=[None, 1], dtype='int64')
                 reader = fluid.io.PyReader(feed_list=[image, label], capacity=4, iterable=True)
 
                 user_defined_generator = random_image_and_label_generator(784, 784)
                 reader.decorate_sample_generator(user_defined_generator,
                                                  batch_size=BATCH_SIZE,
-                                                 places=[fluid.CUDAPlace(0)])
-                # definition of network is omitted
-                executor = fluid.Executor(fluid.CUDAPlace(0))
-                executor.run(fluid.default_main_program())
+                                                 places=[fluid.CPUPlace()])
+                loss = network(image, label)
+                executor = fluid.Executor(fluid.CPUPlace())
+                executor.run(fluid.default_startup_program())
 
                 for _ in range(EPOCH_NUM):
                     for data in reader():
-                        executor.run(feed=data)
+                        executor.run(feed=data, fetch_list=[loss])
     
         '''
         self._loader.set_sample_generator(sample_generator, batch_size,
@@ -905,6 +924,11 @@ class PyReader(DataLoaderBase):
                 ITER_NUM = 15
                 BATCH_SIZE = 3
 
+                def network(image, label):
+                    # User-defined network, here is an example of softmax regression.
+                    predict = fluid.layers.fc(input=image, size=10, act='softmax')           
+                    return fluid.layers.cross_entropy(input=predict, label=label)
+
                 def random_image_and_label_generator(height, width):
                     def generator():
                         for i in range(ITER_NUM):
@@ -915,21 +939,22 @@ class PyReader(DataLoaderBase):
                             yield fake_image, fake_label
                     return generator
 
-                image = fluid.layers.data(name='image', shape=[784, 784], dtype='float32')
-                label = fluid.layers.data(name='label', shape=[1], dtype='int32')
+                image = fluid.data(name='image', shape=[None, 784, 784], dtype='float32')
+                label = fluid.data(name='label', shape=[None, 1], dtype='int64')
                 reader = fluid.io.PyReader(feed_list=[image, label], capacity=4, iterable=True)
 
                 user_defined_generator = random_image_and_label_generator(784, 784)
                 reader.decorate_sample_list_generator(
                     paddle.batch(user_defined_generator, batch_size=BATCH_SIZE),
-                    fluid.core.CUDAPlace(0))
-                # definition of network is omitted
-                executor = fluid.Executor(fluid.core.CUDAPlace(0))
-                executor.run(fluid.default_main_program())
+                    fluid.core.CPUPlace())
+                
+                loss = network(image, label)
+                executor = fluid.Executor(fluid.core.CPUPlace())
+                executor.run(fluid.default_startup_program())
 
                 for _ in range(EPOCH_NUM):
                     for data in reader():
-                        executor.run(feed=data)
+                        executor.run(feed=data, fetch_list=[loss])
                  
         '''
         self._loader.set_sample_list_generator(reader, places)
@@ -959,6 +984,11 @@ class PyReader(DataLoaderBase):
                 ITER_NUM = 15
                 BATCH_SIZE = 3
 
+                def network(image, label):
+                    # User-defined network, here is an example of softmax regression.
+                    predict = fluid.layers.fc(input=image, size=10, act='softmax')           
+                    return fluid.layers.cross_entropy(input=predict, label=label)
+
                 def random_image_and_label_generator(height, width):
                     def generator():
                         for i in range(ITER_NUM):
@@ -966,22 +996,25 @@ class PyReader(DataLoaderBase):
                                                             high=255,
                                                             size=[BATCH_SIZE, height, width])
                             batch_label = np.ones([BATCH_SIZE, 1])
+                            batch_image = batch_image.astype('float32')
+                            batch_label = batch_label.astype('int64')
                             yield batch_image, batch_label
                     return generator
 
-                image = fluid.layers.data(name='image', shape=[784, 784], dtype='float32')
-                label = fluid.layers.data(name='label', shape=[1], dtype='int32')
+                image = fluid.data(name='image', shape=[None, 784, 784], dtype='float32')
+                label = fluid.data(name='label', shape=[None, 1], dtype='int64')
                 reader = fluid.io.PyReader(feed_list=[image, label], capacity=4, iterable=True)
 
                 user_defined_generator = random_image_and_label_generator(784, 784)
-                reader.decorate_batch_generator(user_defined_generator, fluid.CUDAPlace(0))
-                # definition of network is omitted
-                executor = fluid.Executor(fluid.CUDAPlace(0))
-                executor.run(fluid.default_main_program())
+                reader.decorate_batch_generator(user_defined_generator, fluid.CPUPlace())
+                
+                loss = network(image, label)
+                executor = fluid.Executor(fluid.CPUPlace())
+                executor.run(fluid.default_startup_program())
 
                 for _ in range(EPOCH_NUM):
                     for data in reader():
-                        executor.run(feed=data)
+                        executor.run(feed=data, fetch_list=[loss])
 
         '''
         self._loader.set_batch_generator(reader, places)
