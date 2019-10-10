9ab70f6d155dfbad333751a1622e02b0cc6c8314
GaoWei8 <catherin.gao@foxmail.com>
2019-10-10 11:48:19 +0000

    test=develop test=document_fix

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -11185,11 +11185,11 @@ def elu(x, alpha=1.0, name=None):
          
             input_elu = np.array([[-1,6],[1,15.6]])
             with fluid.dygraph.guard():
-            x = fluid.dygraph.to_variable(input_elu)
-            y = fluid.layers.elu(x, alpha=0.2)
-            print(y.numpy())
-            # [[-0.12642411  6.        ]
-            # [ 1.          15.6       ]]
+                x = fluid.dygraph.to_variable(input_elu)
+                y = fluid.layers.elu(x, alpha=0.2)
+                print(y.numpy())
+                # [[-0.12642411  6.        ]
+                # [ 1.          15.6       ]]
     """
     helper = LayerHelper('elu', **locals())
     if not isinstance(x, Variable):
