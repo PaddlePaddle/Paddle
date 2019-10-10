553cfd5b60ba9269618cff4dec6872947489a597
tangwei12 <tangwei12@baidu.com>
2019-10-10 22:51:02 +0800

    test=develop

--- python/paddle/fluid/incubate/fleet/parameter_server/distribute_transpiler/__init__.py
+++ python/paddle/fluid/incubate/fleet/parameter_server/distribute_transpiler/__init__.py
@@ -129,7 +129,8 @@ class DistributedTranspiler(Fleet):
         Returns:
             None
         """
-        self._communicator.stop()
+        if not self._transpile_config.sync_mode:
+            self._communicator.stop()
         self._executor.close()
         if isinstance(self._role_maker, MPISymetricRoleMaker):
             self._role_maker._finalize()
