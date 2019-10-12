22823df2e28f7ba863bdcd69fcaffc3cc6b0cc48
Aurelius84 <zhangliujie@baidu.com>
2019-10-12 08:19:56 +0800

    enhance embedding error message test=develop (#20246)
    * enhance embedding error message test=develop
    
    * enforce .h error test=develop
    
    * fix unittest code test=develop
    
    * Fix fp16 dtype in embedding test=develop
    
    * add import warnings test=develop

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -601,6 +601,21 @@ def embedding(input,
     """
 
     helper = LayerHelper('embedding', **locals())
+    if not isinstance(input, Variable):
+        raise TypeError(
+            "The type of 'input' in layers.embedding must be Variable, but received %s"
+            % (type(input)))
+    if convert_dtype(input.dtype) not in ['int64']:
+        raise TypeError(
+            "The data type of 'input' in layers.embedding must be int64, but received %s."
+            % (convert_dtype(input.dtype)))
+    if convert_dtype(dtype) in ['float16']:
+        warnings.warn(
+            "The 'dtype' of layers.embedding only support float16 in GPU now.")
+    if convert_dtype(dtype) not in ['float16', 'float32', 'float64']:
+        raise TypeError(
+            "The 'dtype' of layers.embedding must be float16, float32 or float64, but received %s."
+            % (convert_dtype(dtype)))
     remote_prefetch = is_sparse and (not is_distributed)
     if remote_prefetch:
         assert is_sparse is True and is_distributed is False
