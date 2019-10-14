3b9e30378ea46e1417a0df9e3469ef439ef33fb7
Shixiaowei02 <shixiaowei02@baidu.com>
2019-10-14 20:55:40 +0800

    update

--- cmake/external/lite.cmake
+++ cmake/external/lite.cmake
@@ -12,62 +12,82 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include(ExternalProject)
+if (LITE_SUBGRAPH_PATH)
+    # Print path of subgraph.
+    message(STATUS "==== Build LITE_SUBGRAPH! ====")
+    message(STATUS "LITE_SUBGRAPH_PATH: ${LITE_SUBGRAPH_PATH}")
 
-set(LITE_PROJECT extern_lite)
+    # Set the LITE_SUBGRAPH_LIB_PATH variable.
+    find_library(LITE_SUBGRAPH_LIB_PATH NAMES liblite_full_static.a
+        PATHS ${LITE_SUBGRAPH_PATH}
+        NO_DEFAULT_PATH
+        DOC "Path to lite library.")
 
-set(LITE_SOURCES_DIR ${THIRD_PARTY_PATH}/lite)
-set(LITE_INSTALL_DIR ${THIRD_PARTY_PATH}/install/lite)
+    # Add library lite_full_static by path LITE_SUBGRAPH_LIB_PATH.
+    add_library(lite_full_static STATIC IMPORTED GLOBAL)
+    SET_PROPERTY(TARGET lite_full_static PROPERTY IMPORTED_LOCATION ${LITE_SUBGRAPH_LIB_PATH})
 
-# No quotes, so cmake can resolve it as a command with arguments.
-set(LITE_BUILD_COMMAND $(MAKE) -j)
-set(LITE_OPTIONAL_ARGS -DWITH_MKL=ON
-                       -DWITH_GPU=OFF
-                       -DWITH_MKLDNN=ON
-                       -DLITE_WITH_X86=ON
-                       -DLITE_WITH_PROFILE=OFF
-                       -DWITH_LITE=ON
-                       -DLITE_WITH_LIGHT_WEIGHT_FRAMEWORK=OFF
-                       -DWITH_PYTHON=OFF
-                       -DWITH_TESTING=ON
-                       -DLITE_WITH_ARM=OFF)
+    # Make target model_optimize_tool. The sequence of glog gflags needs to be attention.
+    message(STATUS "LITE_SUBGRAPH_LIB_PATH: ${LITE_SUBGRAPH_LIB_PATH}")
+    include_directories(${LITE_SUBGRAPH_PATH}/include)
 
-ExternalProject_Add(
-    ${LITE_PROJECT}
-    ${EXTERNAL_PROJECT_LOG_ARGS}
-    GIT_REPOSITORY      "https://github.com/Shixiaowei02/Paddle-Lite.git"
-    GIT_TAG             "2a6362da32a2250ec7f7941b2263ad5ccd444241"
-    PREFIX              ${LITE_SOURCES_DIR}
-    UPDATE_COMMAND      ""
-    BUILD_COMMAND       ${LITE_BUILD_COMMAND}
-    INSTALL_COMMAND     ""
-    CMAKE_ARGS          -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-                        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-                        -DCMAKE_CXX_FLAGS=${LITE_CMAKE_CXX_FLAGS}
-                        -DCMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS_RELEASE}
-                        -DCMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS_DEBUG}
-                        -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-                        -DCMAKE_C_FLAGS_DEBUG=${CMAKE_C_FLAGS_DEBUG}
-                        -DCMAKE_C_FLAGS_RELEASE=${CMAKE_C_FLAGS_RELEASE}
-                        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-                        -DCMAKE_BUILD_TYPE=${THIRD_PARTY_BUILD_TYPE}
-                        ${EXTERNAL_OPTIONAL_ARGS}
-                        ${LITE_OPTIONAL_ARGS}
-)
+else()
+  include(ExternalProject)
+  set(LITE_PROJECT extern_lite)
+  set(LITE_SOURCES_DIR ${THIRD_PARTY_PATH}/lite)
+  set(LITE_INSTALL_DIR ${THIRD_PARTY_PATH}/install/lite)
 
-ExternalProject_Get_property(${LITE_PROJECT} BINARY_DIR)
-ExternalProject_Get_property(${LITE_PROJECT} SOURCE_DIR)
+  # No quotes, so cmake can resolve it as a command with arguments.
+  set(LITE_BUILD_COMMAND $(MAKE) -j)
+  set(LITE_OPTIONAL_ARGS -DWITH_MKL=ON
+                         -DWITH_GPU=OFF
+                         -DWITH_MKLDNN=ON
+                         -DLITE_WITH_X86=ON
+                         -DLITE_WITH_PROFILE=OFF
+                         -DWITH_LITE=ON
+                         -DLITE_WITH_LIGHT_WEIGHT_FRAMEWORK=OFF
+                         -DWITH_PYTHON=OFF
+                         -DWITH_TESTING=ON
+                         -DLITE_WITH_ARM=OFF)
 
-message(STATUS "Paddle-lite BINARY_DIR: ${BINARY_DIR}")
-message(STATUS "Paddle-lite SOURCE_DIR: ${SOURCE_DIR}")
+  ExternalProject_Add(
+      ${LITE_PROJECT}
+      ${EXTERNAL_PROJECT_LOG_ARGS}
+      GIT_REPOSITORY      "https://github.com/Shixiaowei02/Paddle-Lite.git"
+      GIT_TAG             "2a6362da32a2250ec7f7941b2263ad5ccd444241"
+      PREFIX              ${LITE_SOURCES_DIR}
+      UPDATE_COMMAND      ""
+      BUILD_COMMAND       ${LITE_BUILD_COMMAND}
+      INSTALL_COMMAND     ""
+      CMAKE_ARGS          -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+                          -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+                          -DCMAKE_CXX_FLAGS=${LITE_CMAKE_CXX_FLAGS}
+                          -DCMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS_RELEASE}
+                          -DCMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS_DEBUG}
+                          -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+                          -DCMAKE_C_FLAGS_DEBUG=${CMAKE_C_FLAGS_DEBUG}
+                          -DCMAKE_C_FLAGS_RELEASE=${CMAKE_C_FLAGS_RELEASE}
+                          -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+                          -DCMAKE_BUILD_TYPE=${THIRD_PARTY_BUILD_TYPE}
+                          ${EXTERNAL_OPTIONAL_ARGS}
+                          ${LITE_OPTIONAL_ARGS}
+  )
 
-function(external_lite_static_libs alias path)
-  add_library(${alias} STATIC IMPORTED GLOBAL) 
-  SET_PROPERTY(TARGET ${alias} PROPERTY IMPORTED_LOCATION 
-               ${path}) 
-  add_dependencies(${alias} ${LITE_PROJECT})
-endfunction()
+  ExternalProject_Get_property(${LITE_PROJECT} BINARY_DIR)
+  ExternalProject_Get_property(${LITE_PROJECT} SOURCE_DIR)
 
-include_directories(${SOURCE_DIR})
-include_directories(${BINARY_DIR})
-external_lite_static_libs(lite_full_static ${BINARY_DIR}/lite/api/libapi_full_static.a)
+  message(STATUS "Paddle-lite BINARY_DIR: ${BINARY_DIR}")
+  message(STATUS "Paddle-lite SOURCE_DIR: ${SOURCE_DIR}")
+
+  function(external_lite_static_libs alias path)
+    add_library(${alias} STATIC IMPORTED GLOBAL) 
+    SET_PROPERTY(TARGET ${alias} PROPERTY IMPORTED_LOCATION 
+                 ${path}) 
+    add_dependencies(${alias} ${LITE_PROJECT})
+  endfunction()
+
+  include_directories(${SOURCE_DIR})
+  include_directories(${BINARY_DIR})
+  external_lite_static_libs(lite_full_static ${BINARY_DIR}/lite/api/libapi_full_static.a)
+
+endif()
