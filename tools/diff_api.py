8493097c109fa5290aa23a3b07844ad3c3522be1
Zeng Jinle <32832641+sneaxiy@users.noreply.github.com>
2019-10-15 17:12:53 +0800

    Release/1.6 refine api spec (#20633)
    * refine API.spec, test=develop
    
    * add nn.py to test,test=develop
    
    * fix by removing new.spec, test=develop, test=document_fix
    
    * refine scripts when PYTHON_ABI is empty, follow partial comments, add extra cpp api doc, test=develop, test=document_fix
    
    * remove extra doc changes, remove API.spec, test=release/1.6
    
    * remove API.spec in release/1.6, test=release/1.6

--- tools/diff_api.py
+++ tools/diff_api.py
@@ -15,7 +15,7 @@ differ = difflib.Differ()
 result = differ.compare(origin, new)
 
 error = False
-print('API Difference is: ')
+diffs = []
 for each_diff in result:
     if each_diff[0] in ['-', '?']:  # delete or change API is not allowed
         error = True
@@ -23,13 +23,16 @@ for each_diff in result:
         error = True
 
     if each_diff[0] != ' ':
-        print(each_diff)
+        diffs.append(each_diff)
+'''
+If you modify/add/delete the API files, including code and comment, 
+please follow these steps in order to pass the CI:
 
+  1. cd ${paddle_path}, compile paddle;
+  2. pip install build/python/dist/(build whl package);
+  3. run "python tools/print_signatures.py paddle.fluid> paddle/fluid/API.spec"
+'''
 if error:
-    print(
-        '''If you modify/add/delete the API files, including code and comment, please follow these steps in order to pass the CI:
-    1. cd ${paddle_path}, compile paddle;
-    2. pip install build/python/dist/(build whl package);
-    3. run "python tools/print_signatures.py paddle.fluid> paddle/fluid/API.spec"'''
-    )
-    sys.exit(1)
+    print('API Difference is: ')
+    for each_diff in diffs:
+        print(each_diff)
