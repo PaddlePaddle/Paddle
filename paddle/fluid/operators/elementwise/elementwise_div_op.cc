11b1ad83801ee48fef509a944577ab1b3de4999b
danleifeng <danleifeng@163.com>
2019-10-18 06:18:31 +0000

    add div divison zero log for int type;test=release/1.6

--- paddle/fluid/operators/elementwise/elementwise_div_op.cc
+++ paddle/fluid/operators/elementwise/elementwise_div_op.cc
@@ -16,6 +16,7 @@ limitations under the License. */
 #include <memory>
 #include <string>
 #include "paddle/fluid/operators/elementwise/elementwise_op.h"
+#include "paddle/fluid/operators/elementwise/elementwise_op_function.h"
 
 namespace paddle {
 namespace operators {
@@ -32,6 +33,8 @@ struct SameDimsElemwiseDiv<
   }
 };
 
+// use default div function for int32/int64 type because of divison zero
+// checking.
 template <typename T>
 struct SameDimsElemwiseDiv<
     platform::CPUDeviceContext, T,
@@ -39,12 +42,7 @@ struct SameDimsElemwiseDiv<
   void operator()(const framework::ExecutionContext &ctx,
                   const framework::Tensor *x, const framework::Tensor *y,
                   framework::Tensor *z) {
-    auto eigen_x = framework::EigenVector<T>::Flatten(*x);
-    auto eigen_y = framework::EigenVector<T>::Flatten(*y);
-    auto eigen_z = framework::EigenVector<T>::Flatten(*z);
-    auto &place = *ctx.template device_context<platform::CPUDeviceContext>()
-                       .eigen_device();
-    eigen_z.device(place) = eigen_x / eigen_y;
+    default_elementwise_div<platform::CPUDeviceContext, T>(ctx, x, y, z);
   }
 };
 
