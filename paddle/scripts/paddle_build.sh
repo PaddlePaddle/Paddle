6188bd8beae2c5249ca699ff888719f0ddb9c62f
Tao Luo <luotao02@baidu.com>
2019-10-22 13:24:00 +0800

    update example script
    test=develop test=document_fix

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -1011,7 +1011,8 @@ function build_document_preview() {
 function example() {
     pip install ${PADDLE_ROOT}/build/python/dist/*.whl
     paddle version
-    cd ${PADDLE_ROOT}/tools
+    cp ${PADDLE_ROOT}/tools/sampcd_processor.py ${PADDLE_ROOT}/python/paddle/fluid
+    cd ${PADDLE_ROOT}/python/paddle/fluid
     python sampcd_processor.py cpu 
     if [ "$?" != "0" ];then
       echo "Code instance execution failed"
