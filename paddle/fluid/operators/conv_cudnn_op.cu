669ee9b59ab52f9438db6145acc33a51640d1cf0
lvmengsi <ceci3@users.noreply.github.com>
2019-10-23 16:19:53 +0800

    Revert "fix_depthwise_conv_cudnn, test=develop (#20712)" (#20782)
    This reverts commit dc229b4195052c67ceb28cba4859f10d7a49aa64.

--- paddle/fluid/operators/conv_cudnn_op.cu
+++ paddle/fluid/operators/conv_cudnn_op.cu
@@ -265,16 +265,6 @@ class CUDNNConvOpKernel : public framework::OpKernel<T> {
     algo = search::Find<T>(args, exhaustive_search, false, 0, ctx);
     workspace_size = search::GetWorkspaceSize(args, algo);
 
-#if CUDNN_VERSION_MIN(7, 0, 1)
-    // when groups > 1, SearchAlgorithm find algo is CUDNN_CONVOLUTION_\
-    // FWD_ALGO_WINOGRAD_NONFUSED, but this kind of algorithm is unstable
-    // in forward computation, so change the algorithm to CUDNN_CONVOLUTION_\
-    // FWD_ALGO_IMPLICIT_GEMM manually.
-    if (ctx.Attr<int>("groups") > 1) {
-      algo = static_cast<cudnnConvolutionFwdAlgo_t>(0);
-    }
-#endif
-
     // ------------------- cudnn conv forward ---------------------
     ScalingParamType<T> alpha = 1.0f, beta = 0.0f;
     for (int i = 0; i < groups; i++) {
@@ -815,7 +805,6 @@ class CUDNNConvDoubleGradOpKernel : public framework::OpKernel<T> {
 #if CUDNN_VERSION_MIN(7, 0, 1)
     iwo_group = 1;
     c_group = groups;
-    groups = 1;
 #endif
     auto dtype = platform::CudnnDataType<T>::type;
 
