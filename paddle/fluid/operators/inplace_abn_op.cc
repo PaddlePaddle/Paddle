7970d5f53f514ef7c770d52806d24f13d00dadbe
peizhilin <wopeizl@163.com>
2019-10-28 15:08:28 +0800

    t

--- paddle/fluid/operators/inplace_abn_op.cc
+++ paddle/fluid/operators/inplace_abn_op.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "paddle/fluid/operators/inplace_abn_op.h"
+#include <memory>
 #include <string>
 #include <vector>
 #include "paddle/fluid/framework/framework.pb.h"
