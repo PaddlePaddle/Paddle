fa91c88210d8d46b734e0cddb73a457fc0b5b837
root <root@gzhxy-inf-bce5ff6842.gzhxy.baidu.com>
2019-10-28 07:30:32 +0000

    move memory copy

--- paddle/fluid/operators/math/fc.cc
+++ paddle/fluid/operators/math/fc.cc
@@ -27,19 +27,20 @@ class FCFunctor<platform::CPUDeviceContext, T> {
                   const int N, const int K, const T* X, const T* W, T* Y,
                   const T* B = nullptr, bool relu = false) {
     auto blas = math::GetBlas<platform::CPUDeviceContext, T>(context);
-    framework::Tensor Y1;
-    Y1.Resize({M * (N + 4)});
-    T* Y1_data = Y1.mutable_data<T>(platform::CPUPlace());
-    framework::Tensor X1;
-    X1.Resize({M * (K + 4)});
-    T* X1_data = X1.mutable_data<T>(platform::CPUPlace());
+    if (N % 128 == 0 && K % 128 == 0) {
+      framework::Tensor Y1;
+      Y1.Resize({M * (N + 4)});
+      T* Y1_data = Y1.mutable_data<T>(platform::CPUPlace());
+      framework::Tensor X1;
+      X1.Resize({M * (K + 4)});
+      T* X1_data = X1.mutable_data<T>(platform::CPUPlace());
 #ifdef PADDLE_WITH_MKLML
 #pragma omp parallel for
 #endif
-    for (int i = 0; i < M; i++) {
-      memcpy(X1_data + i * (K + 4), X + i * K, K * sizeof(X[0]));
-    }
-    if (N % 128 == 0 && K % 128 == 0) {
+      for (int i = 0; i < M; i++) {
+        memcpy(X1_data + i * (K + 4), X + i * K, K * sizeof(X[0]));
+      }
+
       blas.MatMul(M, N, K, X1_data, W, Y1_data);
     } else {
       blas.MatMul(M, N, K, X, W, Y);
