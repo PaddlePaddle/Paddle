3967181d2ada9a2e1a482ad3e79680616532d3aa
gongweibao <weibao.gong@gmail.com>
2019-10-29 07:27:29 +0000

    add custom black varname test=develop

--- python/paddle/fluid/contrib/tests/test_image_classification_fp16.py
+++ python/paddle/fluid/contrib/tests/test_image_classification_fp16.py
@@ -134,8 +134,10 @@ def train(net_type, use_cuda, save_dirname, is_local):
 
         optimizer = fluid.optimizer.Lamb(learning_rate=0.001)
 
+        amp_lists = AutoMixedPrecisionLists(custom_black_varnames={"loss"})
         mp_optimizer = fluid.contrib.mixed_precision.decorate(
             optimizer=optimizer,
+            amp_lists=amp_lists,
             init_loss_scaling=8.0,
             use_dynamic_loss_scaling=True)
 
