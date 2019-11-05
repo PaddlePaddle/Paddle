da76ac0e96d34e2afcbfe2413a4ba3c732a6dad8
GaoWei8 <catherin.gao@foxmail.com>
2019-11-05 09:48:14 +0000

    Add ernie unit test test=develop

--- paddle/fluid/inference/tests/api/analyzer_ernie_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_ernie_tester.cc
@@ -17,6 +17,8 @@
 namespace paddle {
 namespace inference {
 
+using paddle::PaddleTensor;
+
 template <typename T>
 void GetValueFromStream(std::stringstream *ss, T *t) {
   (*ss) >> (*t);
@@ -89,23 +91,12 @@ bool ParseLine(const std::string &line,
   tensors->reserve(4);
 
   int i = 0;
-  // src_ids
-  paddle::PaddleTensor src_ids;
-  ParseTensor<int64_t>(fields[i++], &src_ids);
-  src_ids.name = "placeholder_0";
-  tensors->push_back(src_ids);
-
-  // pos_ids
-  paddle::PaddleTensor pos_ids;
-  ParseTensor<int64_t>(fields[i++], &pos_ids);
-  pos_ids.name = "placeholder_1";
-  tensors->push_back(pos_ids);
-
-  // sent_ids
-  paddle::PaddleTensor sent_ids;
-  ParseTensor<int64_t>(fields[i++], &sent_ids);
-  sent_ids.name = "placeholder_2";
-  tensors->push_back(sent_ids);
+  for (; i < 3; i++) {
+    paddle::PaddleTensor temp;
+    ParseTensor<int64_t>(fields[i], &temp);
+    temp.name = "placeholder_" + std::to_string(i);
+    tensors->push_back(temp);
+  }
 
   // input_mask
   paddle::PaddleTensor input_mask;
@@ -124,19 +115,17 @@ bool LoadInputData(std::vector<std::vector<paddle::PaddleTensor>> *inputs) {
 
   std::ifstream fin(FLAGS_infer_data);
   std::string line;
+  int sample = 0;
 
-  int lineno = 0;
+  // The unit-test dataset only have 10 samples, each sample have 5 feeds.
   while (std::getline(fin, line)) {
     std::vector<paddle::PaddleTensor> feed_data;
-    if (!ParseLine(line, &feed_data)) {
-      LOG(ERROR) << "Parse line[" << lineno << "] error!";
-    } else {
-      inputs->push_back(std::move(feed_data));
-    }
-    lineno++;
+    ParseLine(line, &feed_data);
+    inputs->push_back(std::move(feed_data));
+    sample++;
+    if (!FLAGS_test_all_data && sample == FLAGS_batch_size) break;
   }
-
-  LOG(INFO) << "Load " << lineno << " samples from " << FLAGS_infer_data;
+  LOG(INFO) << "number of samples: " << sample;
   return true;
 }
 
@@ -151,6 +140,48 @@ void SetConfig(AnalysisConfig *cfg) {
   }
 }
 
+void profile(bool use_mkldnn = false, bool use_ngraph = false) {
+  AnalysisConfig config;
+  SetConfig(&config);
+
+  if (use_mkldnn) {
+    config.EnableMKLDNN();
+    config.pass_builder()->AppendPass("fc_mkldnn_pass");
+  }
+
+  if (use_ngraph) {
+    config.EnableNgraph();
+  }
+
+  std::vector<std::vector<PaddleTensor>> outputs;
+  std::vector<std::vector<PaddleTensor>> inputs;
+  LoadInputData(&inputs);
+  TestPrediction(reinterpret_cast<const PaddlePredictor::Config *>(&config),
+                 inputs, &outputs, FLAGS_num_threads);
+}
+
+TEST(Analyzer_ernie, profile) { profile(); }
+#ifdef PADDLE_WITH_MKLDNN
+TEST(Analyzer_ernie, profile_mkldnn) { profile(true, false); }
+#endif
+
+#ifdef PADDLE_WITH_NGRAPH
+TEST(Analyzer_ernie, profile_ngraph) { profile(false, true); }
+#endif
+
+// Check the model by gpu
+TEST(Analyzer_ernie, profile_gpu) {
+  AnalysisConfig config;
+  SetConfig(&config);
+
+  config.EnableUseGpu(100, 0);
+  std::vector<std::vector<PaddleTensor>> outputs;
+  std::vector<std::vector<PaddleTensor>> inputs;
+  LoadInputData(&inputs);
+  TestPrediction(reinterpret_cast<const PaddlePredictor::Config *>(&config),
+                 inputs, &outputs, FLAGS_num_threads);
+}
+
 // Check the fuse status
 TEST(Analyzer_Ernie, fuse_statis) {
   AnalysisConfig cfg;
@@ -160,7 +191,40 @@ TEST(Analyzer_Ernie, fuse_statis) {
   auto predictor = CreatePaddlePredictor<AnalysisConfig>(cfg);
   auto fuse_statis = GetFuseStatis(
       static_cast<AnalysisPredictor *>(predictor.get()), &num_ops);
+  LOG(INFO) << "num_ops: " << num_ops;
 }
+
+// Compare result of NativeConfig and AnalysisConfig
+void compare(bool use_mkldnn = false, bool use_ngraph = false) {
+  AnalysisConfig cfg;
+  SetConfig(&cfg);
+  if (use_mkldnn) {
+    cfg.EnableMKLDNN();
+    cfg.pass_builder()->AppendPass("fc_mkldnn_pass");
+  }
+
+  if (use_ngraph) {
+    cfg.EnableNgraph();
+  }
+
+  std::vector<std::vector<PaddleTensor>> inputs;
+  LoadInputData(&inputs);
+  CompareNativeAndAnalysis(
+      reinterpret_cast<const PaddlePredictor::Config *>(&cfg), inputs);
+}
+
+TEST(Analyzer_ernie, compare) { compare(); }
+#ifdef PADDLE_WITH_MKLDNN
+TEST(Analyzer_ernie, compare_mkldnn) {
+  compare(true, false /* use_mkldnn, no use_ngraph */);
+}
+#endif
+
+#ifdef PADDLE_WITH_NGRAPH
+TEST(Analyzer_ernie, compare_ngraph) {
+  compare(false, true /* no use_mkldnn, use_ngraph */);
+}
+#endif
 
 // Compare Deterministic result
 TEST(Analyzer_Ernie, compare_determine) {
@@ -173,20 +237,38 @@ TEST(Analyzer_Ernie, compare_determine) {
                        input_slots_all);
 }
 
+// Compare result of AnalysisConfig and AnalysisConfig + ZeroCopy
+TEST(Analyzer_ernie, compare_zero_copy) {
+  AnalysisConfig cfg;
+  SetConfig(&cfg);
+
+  AnalysisConfig cfg1;
+  SetConfig(&cfg1);
+
+  std::vector<std::vector<PaddleTensor>> input_slots_all;
+  LoadInputData(&input_slots_all);
+  std::vector<std::string> outputs_name;
+  outputs_name.emplace_back("fc_73.tmp_1");
+  CompareAnalysisAndZeroCopy(reinterpret_cast<PaddlePredictor::Config *>(&cfg),
+                             reinterpret_cast<PaddlePredictor::Config *>(&cfg1),
+                             input_slots_all, outputs_name);
+}
+
 // Compare results
-TEST(Analyzer_Ernie, compare) {
+TEST(Analyzer_Ernie, compare_results) {
   AnalysisConfig cfg;
   SetConfig(&cfg);
 
   std::vector<std::vector<PaddleTensor>> input_slots_all;
   LoadInputData(&input_slots_all);
 
-  std::vector<float> ref = {
-      2.2881,    -0.919723, -1.63951,  -1.42595, 2.1902,    -0.699561,
-      1.20926,   -1.62216,  0.202339,  -0.51834, -0.255833, 0.403484,
-      -1.61329,  2.44492,   -0.621071, 1.93191,  -2.43418,  0.252749,
-      3.9044,    -2.59725,  -0.988252, -1.72084, 2.54846,   -0.660083,
-      -0.907154, -0.903408, 1.83843,   0.94278,  -2.09705,  0.819825};
+  std::ifstream fin(FLAGS_refer_result);
+  std::string line;
+  std::vector<float> ref;
+
+  while (std::getline(fin, line)) {
+    Split(line, ' ', &ref);
+  }
 
   auto predictor = CreateTestPredictor(
       reinterpret_cast<const PaddlePredictor::Config *>(&cfg),
@@ -199,7 +281,8 @@ TEST(Analyzer_Ernie, compare) {
     auto outputs_size = outputs.front().data.length() / (sizeof(float));
     for (size_t j = 0; j < outputs_size; ++j) {
       EXPECT_NEAR(ref[i * outputs_size + j],
-                  static_cast<float *>(outputs[0].data.data())[j], 1e-5);
+                  static_cast<float *>(outputs[0].data.data())[j],
+                  FLAGS_accuracy);
     }
   }
 }
