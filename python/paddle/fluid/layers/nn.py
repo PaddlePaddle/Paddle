843e40b05b02bd609096f68d1db1f4bc51286af1
bingyanghuang <bingyang.huang@intel.com>
2019-11-06 16:26:44 +0800

    cherry-pick PR 21028, test=release/1.6

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -4120,7 +4120,6 @@ def batch_norm(input,
                moving_mean_name=None,
                moving_variance_name=None,
                do_model_average_for_mean_and_var=True,
-               fuse_with_relu=False,
                use_global_stats=False):
     """
     **Batch Normalization Layer**
@@ -4205,7 +4204,6 @@ def batch_norm(input,
             will save global variance with the string.
         do_model_average_for_mean_and_var(bool, Default True): Whether parameter mean and variance should do model
             average when model average is enabled.
-        fuse_with_relu (bool): if True, this OP performs relu after batch norm.
         use_global_stats(bool, Default False): Whether to use global mean and
             variance. In inference or test mode, set use_global_stats to true
             or is_test to true, and the behavior is equivalent.
@@ -4321,7 +4319,7 @@ def batch_norm(input,
             "is_test": is_test,
             "data_layout": data_layout,
             "use_mkldnn": False,
-            "fuse_with_relu": fuse_with_relu,
+            "fuse_with_relu": False,
             "use_global_stats": use_global_stats
         })
 
