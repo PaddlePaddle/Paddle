77c208358681ae334b825dd45c6fdc09673c8b88
joanna.wozna.intel <joanna.wozna@intel.com>
2019-11-08 15:04:36 +0100

    Add transpose2 INT8 for mkl-dnn (#19424)
    * Add transpose2 INT8 for mkl-dnn
    
    test=develop
    
    * Fix test_transpose_int8_mkldnn
    
    test=develop
    
    * Revert "Merge branch 'develop' into transpose_int8_mkldnn_2"
    
    This reverts commit 34011bdba4c859abb945e062ab13124f70508054, reversing
    changes made to 2ce6473f144da298aba4a43d46918f27d463cf7c.
    
    * Revert "Revert "Merge branch 'develop' into transpose_int8_mkldnn_2""
    
    This reverts commit 23754dd78ca47ae56881161172b2aacd349aba90.
    
    * Add template to TransposeMKLDNNHandler
    
    test=develop
    
    * Resolve conflict
    
    test=develop
    
    * Restore get_size and refactor
    
    test=develop

--- paddle/fluid/framework/ir/mkldnn/cpu_quantize_pass.h
+++ paddle/fluid/framework/ir/mkldnn/cpu_quantize_pass.h
@@ -52,6 +52,8 @@ class CPUQuantizePass : public FusePassBase {
 
   void QuantizePriorBox(Graph* graph) const;
 
+  void QuantizeTranspose(Graph* graph) const;
+
   void QuantizeInput(Graph* g, Node* op, Node* input, std::string input_name,
                      double scale_to_one, bool is_unsigned,
                      std::string scale_attr_name = "") const;
