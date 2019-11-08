245e44b4a3d01262e395026a29b2a57ef6ca0364
liym27 <33742067+liym27@users.noreply.github.com>
2019-11-08 13:04:23 +0800

    fix bug in pool/conv/conv_transpose: UpdatePaddingAndDilation, _get_padding_with_SAME and conv2dtranspose_forward_naive. (#20997)
    * fix bug in pool/conv/conv_transpose:
    1. It should be stride[i] not stride[0] in UpdatePaddingAndDilation;
    2. fix bug of func  _get_padding_with_SAME in test_conv/conv_transpose_op.py;
    3. fix bug of the computation process in function conv2dtranspose_forward_naive.
    test=develop
    
    * change test to make the data of different dimensions different. test=develop

--- python/paddle/fluid/tests/unittests/test_conv2d_op.py
+++ python/paddle/fluid/tests/unittests/test_conv2d_op.py
@@ -73,11 +73,7 @@ def conv2d_forward_naive(input,
         pad = [0, 0, 0, 0]
     elif padding_algorithm == "SAME":
         dilation = [1, 1]
-        input_data_shape = []
-        if data_format == "NCHW":
-            input_data_shape = input.shape[2:4]
-        elif data_format == "NHWC":
-            input_data_shape = input.shape[1:3]
+        input_data_shape = input.shape[2:4]
         pad = _get_padding_with_SAME(input_data_shape, ksize, stride)
 
     pad_h_0, pad_h_1 = pad[0], pad[0]
@@ -775,11 +771,11 @@ class TestConv2dOp_v2(OpTest):
 
     def init_test_case(self):
         self.pad = [0, 0]
-        self.stride = [1, 1]
+        self.stride = [1, 2]
         self.input_size = [2, 3, 5, 5]  # NCHW
         assert np.mod(self.input_size[1], self.groups) == 0
         f_c = self.input_size[1] // self.groups
-        self.filter_size = [6, f_c, 3, 3]
+        self.filter_size = [6, f_c, 4, 3]
 
     def init_dilation(self):
         self.dilations = [1, 1]
