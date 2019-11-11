9091f8cdf9a7a524b6b17fd8cdae436d5dee91b0
Yiqun Liu <liuyiqun01@baidu.com>
2019-11-11 09:59:59 +0800

    Support generating code for grad_op (#21066)
    * Add the definition of operation in fusion_group.
    
    * Use operations in OperationMap to detect fusion_group of elementwise pattern.
    
    * Add namespace fusion_group in code_generator.
    
    * Use operations recorded in OperationMap to generate code.
    
    * Remove implementation codes to .cc file.
    
    * Refine Operation and CodeGenerator to make it easier to generate code for grad_op.
    Refine the unittest for better reuse.
    
    * Avoid recording the template's keyword in a array.
    
    * Support the generating of code for grad_op and add unittest.
    test=develop
    
    * Remove replaced_element_in_order and use use number instead.
    test=develop

--- paddle/fluid/framework/ir/fusion_group/code_generator_helper.cc
+++ paddle/fluid/framework/ir/fusion_group/code_generator_helper.cc
@@ -17,53 +17,66 @@ limitations under the License. */
 #include <sstream>
 #include <string>
 #include <vector>
+#include "glog/logging.h"
+#include "paddle/fluid/framework/ir/fusion_group/operation.h"
 
 namespace paddle {
 namespace framework {
 namespace ir {
+namespace fusion_group {
 
-OperationExpression::OperationExpression(std::vector<int> input_ids,
-                                         int output_id, std::string op) {
-  input_ids_ = input_ids;
-  output_id_ = output_id;
-  op_ = op;
+template <typename T>
+static T StringTo(const std::string& str) {
+  std::istringstream is(str);
+  T value;
+  is >> value;
+  return value;
 }
 
-std::string OperationExpression::GetRHSTemplate() {
-  std::stringstream ret;
-  std::string rhs_end = ";";
-  auto rhs = support_table[op_];
-  for (size_t i = 0; i < input_ids_.size(); i++) {
-    auto replaced_str = replaced_element_in_order[i];
-    auto pos = rhs.find(replaced_str);
-    auto index = input_ids_[i];
-    rhs.replace(pos, replaced_str.length(), std::to_string(index) + R"([idx])");
+std::string OperationExpression::GetRHS(size_t i) {
+  auto rhs = OperationMap::Instance().Get(op_).exprs[i];
+  for (size_t i = 0; i < rhs.size(); i++) {
+    size_t pos = i;
+    if (rhs[pos] == '$' && rhs[pos + 1] == '{') {
+      int length = 0;
+      while (rhs[pos + 2 + length] != '}') {
+        length++;
+      }
+      std::string index_str = rhs.substr(pos + 2, length);
+      int index = StringTo<int>(index_str);
+      PADDLE_ENFORCE_LT(index, input_ids_.size(),
+                        "Only %d inputs are provided, but need %d.",
+                        input_ids_.size(), index + 1);
+      rhs.replace(pos, length + 3, VarName(input_ids_[index]) + R"([idx])");
+    }
   }
-  ret << rhs << rhs_end;
-  return ret.str();
+  return rhs;
 }
 
-std::string OperationExpression::GetLHSTemplate() {
+std::string OperationExpression::GetLHS(size_t i) {
   std::stringstream ret;
-  ret << "var" << output_id_ << R"([idx] = )";
+  ret << VarName(output_ids_[i]) << R"([idx])";
   return ret.str();
 }
 
-bool OperationExpression::SupportState() {
-  return (support_table.find(op_) == support_table.end());
+bool OperationExpression::IsSupport() {
+  return OperationMap::Instance().Has(op_);
 }
 
 // we Traverse the graph and get the group , all input id and output id is
 // unique for the node which belong the group
 std::string OperationExpression::GetExpression() {
   std::stringstream ret;
-  if (!SupportState()) {
-    ret << GetLHSTemplate() << GetRHSTemplate();
+  if (IsSupport()) {
+    for (size_t i = 0; i < output_ids_.size(); ++i) {
+      ret << GetLHS(i) << " = " << GetRHS(i) << ";";
+    }
   }
 
   return ret.str();
 }
 
+}  // namespace fusion_group
 }  // namespace ir
 }  // namespace framework
 }  // namespace paddle
