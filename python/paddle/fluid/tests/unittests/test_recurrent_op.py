Fix the error of init variable in StaticRNN when stop_gradient=ON
test=develop

python/paddle/fluid/tests/unittests/test_recurrent_op.py
@@ -123,8 +123,8 @@ class RecurrentOpTest1(unittest.TestCase):
 
     def setUp(self):
         self.setup_program()
-        self.data_field = {"x", "h_boot"}
-        self.fetch_data_field = {"x", "h_boot"}
+        self.feed_data_field = {"x", "h_boot"}
+        self.grad_data_field = self.feed_data_field
 
         self.input_shape = (self.sent_len, self.batch_size, self.input_dim)
         self.output_shape = (self.sent_len, self.batch_size, self.input_dim)
@@ -162,7 +162,7 @@ class RecurrentOpTest1(unittest.TestCase):
     def forward(self):
         self.feed_map = {
             x: create_tensor(getattr(self.py_rnn, x), self.place)
-            for x in self.data_field
+            for x in self.feed_data_field
         }
         exe = Executor(self.place)
         out = exe.run(self.main_program,
@@ -174,11 +174,11 @@ class RecurrentOpTest1(unittest.TestCase):
     def backward(self):
         self.feed_map = {
             x: create_tensor(getattr(self.py_rnn, x), self.place)
-            for x in self.data_field
+            for x in self.feed_data_field
         }
         fetch_list = [
             self.main_program.global_block().var(grad_var_name(x))
-            for x in self.fetch_data_field
+            for x in self.grad_data_field
         ]
 
         exe = Executor(self.place)
@@ -196,7 +196,7 @@ class RecurrentOpTest1(unittest.TestCase):
         ana_grad = [np.array(x) for x in self.backward()]
 
         num_grad = self.get_numerical_gradient()
-        for idx, name in enumerate(self.fetch_data_field):
+        for idx, name in enumerate(self.grad_data_field):
             self.assertEqual(num_grad[idx].shape, ana_grad[idx].shape)
             self.assertTrue(
                 np.isclose(
@@ -213,7 +213,7 @@ class RecurrentOpTest1(unittest.TestCase):
 
     def get_numerical_gradient(self, delta=0.005):
         dloss_dout = 1.0
-        feed_list = [getattr(self.py_rnn, x) for x in self.fetch_data_field]
+        feed_list = [getattr(self.py_rnn, x) for x in self.grad_data_field]
         grad_list = [np.zeros_like(x) for x in feed_list]
         for feed, grad in zip(feed_list, grad_list):
             for f, g in np.nditer([feed, grad], op_flags=['readwrite']):
@@ -254,8 +254,8 @@ class RecurrentOpTest2(RecurrentOpTest1):
     def setUp(self):
         self.setup_program()
 
-        self.data_field = {"x", "h_boot", "W", "U"}
-        self.fetch_data_field = {"x", "h_boot", "W", "U"}
+        self.feed_data_field = {"x", "h_boot", "W", "U"}
+        self.grad_data_field = self.feed_data_field
 
         self.input_shape = (self.sent_len, self.batch_size, self.input_dim)
         self.output_shape = (self.sent_len, self.batch_size, self.input_dim)
@@ -354,8 +354,8 @@ class RecurrentOpMultipleMemoryTest(RecurrentOpTest1):
     def setUp(self):
         self.setup_program()
 
-        self.data_field = {"x", "h_boot1", "h_boot2"}
-        self.fetch_data_field = {"x", "h_boot1", "h_boot2"}
+        self.feed_data_field = {"x", "h_boot1", "h_boot2"}
+        self.grad_data_field = self.feed_data_field
 
         self.input_shape = (self.sent_len, self.batch_size, self.input_dim)
         self.output_shape = (self.sent_len, self.batch_size, self.input_dim)
@@ -438,8 +438,8 @@ class RecurrentOpNoMemBootTest(RecurrentOpTest1):
     def setUp(self):
         self.setup_program()
 
-        self.data_field = {"x"}
-        self.fetch_data_field = {"x"}
+        self.feed_data_field = {"x"}
+        self.grad_data_field = self.feed_data_field
 
         self.input_shape = (self.sent_len, self.batch_size, self.input_dim)
         self.output_shape = (self.sent_len, self.batch_size, self.input_dim)
@@ -539,8 +539,8 @@ class RecurrentOpSubBlockTest(RecurrentOpTest1):
     def setUp(self):
         self.setup_program()
 
-        self.data_field = {"x", "emb", "w1", "w2"}
-        self.fetch_data_field = {"x", "emb", "w1", "w2"}
+        self.feed_data_field = {"x", "emb", "w1", "w2"}
+        self.grad_data_field = self.feed_data_field
 
         self.input_shape = (self.sent_len, self.batch_size, self.input_dim)
         self.output_shape = (self.sent_len, self.batch_size, self.input_dim)
@@ -607,7 +607,7 @@ class RecurrentOpSubBlockTest(RecurrentOpTest1):
         return rnn()
 
 
-class RecurrentOpTest3(RecurrentOpTest1):
+class RecurrentOpStopGradientTest(RecurrentOpTest1):
     """
     Test RNNOp with stop_gradient = True
     equation:
@@ -629,8 +629,8 @@ class RecurrentOpTest3(RecurrentOpTest1):
 
     def setUp(self):
         self.setup_program()
-        self.data_field = {"x", "h_boot", "W", "U"}
-        self.fetch_data_field = {"x", "W", "U"}
+        self.feed_data_field = {"x", "h_boot", "W", "U"}
+        self.grad_data_field = {"x", "W", "U"}
 
         self.input_shape = (self.sent_len, self.batch_size, self.input_dim)
         self.output_shape = (self.sent_len, self.batch_size, self.input_dim)
@@ -652,7 +652,7 @@ class RecurrentOpTest3(RecurrentOpTest1):
 
         rnn = layers.StaticRNN()
         with rnn.step():
-            h_pre = rnn.memory(init=h_boot)
+            h_pre = rnn.memory(init=h_boot)  # init doesn't have gradient
             x_t = rnn.step_input(x)
 
             temp_l = layers.fc(