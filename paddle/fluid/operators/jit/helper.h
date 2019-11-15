f2e6ca7dd2cb0ae54474e7d3c99858db7f4f2b9b
Yihua Xu <yihua.xu@intel.com>
2019-11-15 13:54:38 +0800

    Fix jit tls issue (#21151)

--- paddle/fluid/operators/jit/helper.h
+++ paddle/fluid/operators/jit/helper.h
@@ -15,6 +15,7 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <string>
 #include <unordered_map>
 #include <utility>  // for std::move
@@ -175,13 +176,25 @@ typename KernelTuple::func_type GetDefaultBestFunc(
   return funcs[0];
 }
 
+extern std::unordered_map<std::string, std::shared_ptr<void>>&
+GetFuncCacheMap();
+
 template <typename KernelTuple, typename PlaceType>
 class KernelFuncs {
  public:
   KernelFuncs() = default;
   static KernelFuncs& Cache() {
-    static thread_local KernelFuncs<KernelTuple, PlaceType> g_func_cache;
-    return g_func_cache;
+    auto& func_cache_map = GetFuncCacheMap();
+    std::string key = typeid(KernelFuncs<KernelTuple, PlaceType>).name();
+    auto iter = func_cache_map.find(key);
+    if (iter != func_cache_map.end()) {
+      return *(KernelFuncs<KernelTuple, PlaceType>*)(iter->second.get());
+    } else {
+      std::shared_ptr<void> cache =
+          std::make_shared<KernelFuncs<KernelTuple, PlaceType>>();
+      func_cache_map.emplace(key, cache);
+      return *(KernelFuncs<KernelTuple, PlaceType>*)(cache.get());
+    }
   }
 
   // the exposed interface to use
