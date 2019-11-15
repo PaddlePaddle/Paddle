76433ffdf2886dfdf03b17de684b4bde6a796d7f
Thunderbrook <a754913769@163.com>
2019-11-15 17:54:52 +0800

    code style test=develop

--- paddle/fluid/framework/dist_multi_trainer.cc
+++ paddle/fluid/framework/dist_multi_trainer.cc
@@ -78,8 +78,8 @@ void DistMultiTrainer::DumpWork(int tid) {
       "%s/part-%03d-%05d", dump_fields_path_.c_str(), mpi_rank_, tid);
   
   if (user_define_dump_filename_ != "") {
-    path = string::format_string(
-        "%s/part-%s-%05d", dump_fields_path_.c_str(), user_define_dump_filename_.c_str(), tid);
+    path = string::format_string("%s/part-%s-%05d", dump_fields_path_.c_str(),
+                                 user_define_dump_filename_.c_str(), tid);
   }
 
   std::shared_ptr<FILE> fp = fs_open_write(path, &err_no, dump_converter_);
