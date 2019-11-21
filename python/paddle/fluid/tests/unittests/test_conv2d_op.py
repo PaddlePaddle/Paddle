24325deadd534c634d1b65409ac324394497bc1b
zhongpu <2013000149@qq.com>
2019-11-21 15:23:08 +0800

    open dygraph op test, test=develop (#19787)
    * open dygraph op test, test=develop
    
    * modify to_variable, test=develop
    
    * modify input and output for dygraph, test=develop
    
    * modify input and output for dygraph(fix bug), test=develop
    
    * fix input processing of dygraph op test, test=develop
    
    * fix bug, test=develop
    
    * fix op test, test=develop
    
    * fix forward bug for dygraph, test=develop
    
    * fix mkldnn op test for forward, test=develop
    
    * update nn.py for dygraph, test=develop
    
    * fix crop_tensor_op, test=develop
    
    * fix elementwise_mul_op, test=develop
    
    * fix fill_op, test=develop
    
    * fix some mkldnn op, test=develop
    
    * open backward op test for dygraph, test=develop
    
    * delete log, test=develop
    
    * close backward op test for dygraph, test=develop
    
    * fix bug for edit_distance_op and test_lstm_cudnn_op, test=develop
    
    * fix optest backward bug for dygraph, test=develop
    
    * fix optest backward bug for dygraph, test=develop
    
    * close backward op test for dygraph, test=develop
    
    * close backward op test for dygraph, test=develop
    
    * open dygraph op test, test=develop
    
    * fix op test for dygraph, fix GradOpDescMaker, test=develop
    
    * fix bug for linear_chain_crf_op.h, test=develop
    
    * remove log, test=develop
    
    * remove log, test=develop
    
    * remove log for op_test.py, test=develop
    
    * remove log for op_test.py, test=develop
    
    * fix bug for var_conv_2d_op, change PADDLE_ENFORCE, test=develop
    
    * fix PADDLE_ENFORCE_EQ for hierarchical_sigmoid_op.cc, test=develop
    
    * fix bug for test_increment_ngraph_op.py, test=develop
    
    * fix lod for op test in dygraph, test=develop
    
    * refactor op_test.py to reduce redundant code, test=develop
    
    * fix lod optest, modify InputVar/OutputVar to HasInput/HasOutput, test=develop
    
    * remove debug log, test=develop
    
    * remove redundant code in base.py, test=develop
    
    * fix some error in optest, test=develop
    
    * fix ClearNoNeedBufferInputs function's bug for LoDTensor, test=develop
    
    * refactor op_test.py, test=develop
    
    * remove redundant writing, test=develop
    
    * fix error(get tensor of the grad variable), test=develop
    
    * fix test_concat_mkldnn test_conv2d_mkldnn, test=develop
    
    * fix optest.py for get tensor of LoDTensor, test=develop
    
    * fix optest.py for get tensor of LoDTensor, test=develop
    
    * fix optest.py for get tensor of LoDTensor, test=develop
    
    * fix some redundant code, test=develop
    
    * reslove conflict and rewrite paddle error message, test=develop

--- python/paddle/fluid/tests/unittests/test_conv2d_op.py
+++ python/paddle/fluid/tests/unittests/test_conv2d_op.py
@@ -319,34 +319,44 @@ class TestConv2dOp(OpTest):
 
     def test_check_output(self):
         place = core.CUDAPlace(0) if self.has_cuda() else core.CPUPlace()
-        self.check_output_with_place(place, atol=1e-5)
+        # TODO(wangzhongpu): support mkldnn op in dygraph mode
+        self.check_output_with_place(
+            place, atol=1e-5, check_dygraph=(self.use_mkldnn == False))
 
     def test_check_grad(self):
         if self.dtype == np.float16:
             return
         place = core.CUDAPlace(0) if self.has_cuda() else core.CPUPlace()
+        # TODO(wangzhongpu): support mkldnn op in dygraph mode
         self.check_grad_with_place(
-            place, {'Input', 'Filter'}, 'Output', max_relative_error=0.02)
+            place, {'Input', 'Filter'},
+            'Output',
+            max_relative_error=0.02,
+            check_dygraph=(self.use_mkldnn == False))
 
     def test_check_grad_no_filter(self):
         if self.dtype == np.float16:
             return
         place = core.CUDAPlace(0) if self.has_cuda() else core.CPUPlace()
+        # TODO(wangzhongpu): support mkldnn op in dygraph mode
         self.check_grad_with_place(
             place, ['Input'],
             'Output',
             max_relative_error=0.02,
-            no_grad_set=set(['Filter']))
+            no_grad_set=set(['Filter']),
+            check_dygraph=(self.use_mkldnn == False))
 
     def test_check_grad_no_input(self):
         if self.dtype == np.float16:
             return
         place = core.CUDAPlace(0) if self.has_cuda() else core.CPUPlace()
+        # TODO(wangzhongpu): support mkldnn op in dygraph mode
         self.check_grad_with_place(
             place, ['Filter'],
             'Output',
             max_relative_error=0.02,
-            no_grad_set=set(['Input']))
+            no_grad_set=set(['Input']),
+            check_dygraph=(self.use_mkldnn == False))
 
     def init_test_case(self):
         self.pad = [0, 0]
@@ -739,17 +749,24 @@ class TestConv2dOp_v2(OpTest):
                                                  self.use_cuda)
 
     def test_check_output(self):
+        # TODO(wangzhongpu): support mkldnn op in dygraph mode
         place = core.CUDAPlace(0) if self.has_cuda() else core.CPUPlace()
-        self.check_output_with_place(place, atol=1e-5)
+        self.check_output_with_place(
+            place, atol=1e-5, check_dygraph=(self.use_mkldnn == False))
 
     def test_check_grad(self):
+        # TODO(wangzhongpu): support mkldnn op in dygraph mode
         if self.dtype == np.float16:
             return
         place = core.CUDAPlace(0) if self.has_cuda() else core.CPUPlace()
         self.check_grad_with_place(
-            place, {'Input', 'Filter'}, 'Output', max_relative_error=0.02)
+            place, {'Input', 'Filter'},
+            'Output',
+            max_relative_error=0.02,
+            check_dygraph=(self.use_mkldnn == False))
 
     def test_check_grad_no_filter(self):
+        # TODO(wangzhongpu): support mkldnn op in dygraph mode
         if self.dtype == np.float16:
             return
         place = core.CUDAPlace(0) if self.has_cuda() else core.CPUPlace()
@@ -757,9 +774,11 @@ class TestConv2dOp_v2(OpTest):
             place, ['Input'],
             'Output',
             max_relative_error=0.02,
-            no_grad_set=set(['Filter']))
+            no_grad_set=set(['Filter']),
+            check_dygraph=(self.use_mkldnn == False))
 
     def test_check_grad_no_input(self):
+        # TODO(wangzhongpu): support mkldnn op in dygraph mode
         if self.dtype == np.float16:
             return
         place = core.CUDAPlace(0) if self.has_cuda() else core.CPUPlace()
@@ -767,7 +786,8 @@ class TestConv2dOp_v2(OpTest):
             place, ['Filter'],
             'Output',
             max_relative_error=0.02,
-            no_grad_set=set(['Input']))
+            no_grad_set=set(['Input']),
+            check_dygraph=(self.use_mkldnn == False))
 
     def init_test_case(self):
         self.pad = [0, 0]
