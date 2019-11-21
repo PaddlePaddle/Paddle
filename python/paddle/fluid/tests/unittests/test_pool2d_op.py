7ab85396b79d63acb1a86fc38706f589a0a3ee0b
liym27 <33742067+liym27@users.noreply.github.com>
2019-11-21 17:41:45 +0800

    [cherry-pick]fix bug in pool/conv/conv_transpose: UpdatePaddingAndDilation, _get_padding_with_SAME and conv2dtranspose_forward_naive. (#20997) (#21225)
    * fix bug in pool/conv/conv_transpose:
        1. It should be stride[i] not stride[0] in UpdatePaddingAndDilation;
        2. fix bug of func  _get_padding_with_SAME in test_conv/conv_transpose_op.py;
        3. fix bug of the computation process in function conv2dtranspose_forward_naive.
        test=release/1.6

--- python/paddle/fluid/tests/unittests/test_pool2d_op.py
+++ python/paddle/fluid/tests/unittests/test_pool2d_op.py
@@ -950,6 +950,20 @@ create_test_cudnn_padding_VALID_class(TestCase4_channel_last)
 create_test_cudnn_padding_VALID_class(TestCase5_channel_last)
 
 
+class TestCase1_strides(TestCase1):
+    def init_test_case(self):
+        self.ksize = [3, 3]
+        self.strides = [1, 2]
+
+    def init_shape(self):
+        self.shape = [2, 3, 4, 5]
+
+
+create_test_cudnn_class(TestCase1_strides)
+create_test_padding_SAME_class(TestCase1_strides)
+create_test_cudnn_padding_SAME_class(TestCase1_strides)
+
+
 # ----- test API
 class TestPool2dAPI(OpTest):
     def test_api(self):
