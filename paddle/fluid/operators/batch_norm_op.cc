90072050972770b94d527c128eead51186f54199
dengkaipeng <dengkaipeng@baidu.com>
2019-11-27 05:33:42 +0000

    fix PADDLE_ENFORCE ci. test=develop

--- paddle/fluid/operators/batch_norm_op.cc
+++ paddle/fluid/operators/batch_norm_op.cc
@@ -60,7 +60,8 @@ void BatchNormOp::InferShape(framework::InferShapeContext *ctx) const {
 
   if (ctx->IsRuntime() && ctx->HasInput("MomentumTensor")) {
     auto mom = ctx->Inputs("MomentumTensor");
-    PADDLE_ENFORCE_EQ(mom.size(), 1, "Input(MomentumTensor) size must be 1");
+    PADDLE_ENFORCE_EQ(mom.size(), 1, 
+        platform::errors::InvalidArgument("Input(MomentumTensor) size must be 1"));
   }
 
   PADDLE_ENFORCE_GE(
