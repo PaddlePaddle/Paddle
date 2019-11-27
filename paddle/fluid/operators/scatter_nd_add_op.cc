5d96e62b495614cf6f958fed855053860c802e84
ForFishes <2282912238@qq.com>
2019-11-27 17:54:29 +0800

    add lod, test=develop

--- paddle/fluid/operators/scatter_nd_add_op.cc
+++ paddle/fluid/operators/scatter_nd_add_op.cc
@@ -86,10 +86,12 @@ class ScatterNdAddGradOp : public framework::OperatorWithKernel {
     if (ctx->HasOutput(framework::GradVarName("Updates"))) {
       ctx->SetOutputDim(framework::GradVarName("Updates"),
                         ctx->GetInputDim("Updates"));
+      ctx->ShareLoD("Updates", /*-->*/ framework::GradVarName("Updates"));
     }
     if (ctx->HasOutput(framework::GradVarName("X"))) {
       ctx->SetOutputDim(framework::GradVarName("X"),
                         ctx->GetInputDim(framework::GradVarName("Out")));
+      ctx->ShareLoD("X", /*-->*/ framework::GradVarName("Out"));
     }
   }
 
