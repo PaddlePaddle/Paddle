c0aa13672edf484b280988f3400636b1a3aff050
lidanqing <danqing.li@intel.com>
2019-11-28 07:22:22 +0100

    Fp32 vs int8 qat C++ performance (#21244)
    * add ut for comparing FP32 and QAT INT8
    
    * add save qat transformed model python script
    test=develop
    
    * updated
    
    * added missing file
    
    * add "with_label"
    test=develop
    
    * performance benchmark as unit test
    test=develop
    
    * change names of unnecessary thing
    
    * Change CMakeList.txt for model downloading and UT
    test=develop
    
    * change names of functions and params for more readable code
    test=develop
    
    * Change PADDLE_ENFORCE messages
    test=develop
    
    * fix indent problems
    test=develop
    
    * indent problems
    test=develop

--- paddle/fluid/inference/tests/api/analyzer_qat_image_classification_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_qat_image_classification_tester.cc
@@ -21,10 +21,10 @@ namespace paddle {
 namespace inference {
 namespace analysis {
 
-void SetConfig(AnalysisConfig *cfg) {
-  cfg->SetModel(FLAGS_infer_model);
+void SetConfig(AnalysisConfig *cfg, std::string model_path) {
+  cfg->SetModel(model_path);
   cfg->DisableGpu();
-  cfg->SwitchIrOptim();
+  cfg->SwitchIrOptim(false);
   cfg->SwitchSpecifyInputNames();
   cfg->SetCpuMathLibraryNumThreads(FLAGS_paddle_num_threads);
   cfg->EnableMKLDNN();
@@ -66,51 +66,8 @@ class TensorReader {
   size_t numel_;
 };
 
-std::shared_ptr<std::vector<PaddleTensor>> GetWarmupData(
-    const std::vector<std::vector<PaddleTensor>> &test_data,
-    int num_images = FLAGS_warmup_batch_size) {
-  int test_data_batch_size = test_data[0][0].shape[0];
-  auto iterations = test_data.size();
-  auto all_test_data_size = iterations * test_data_batch_size;
-  PADDLE_ENFORCE_LE(static_cast<size_t>(num_images), all_test_data_size,
-                    platform::errors::InvalidArgument(
-                        "The requested quantization warmup data size must be "
-                        "smaller than the test data size. But received warmup "
-                        "size is %d and test data size is %d",
-                        num_images, all_test_data_size));
-
-  PaddleTensor images;
-  images.name = "image";
-  images.shape = {num_images, 3, 224, 224};
-  images.dtype = PaddleDType::FLOAT32;
-  images.data.Resize(sizeof(float) * num_images * 3 * 224 * 224);
-
-  PaddleTensor labels;
-  labels.name = "label";
-  labels.shape = {num_images, 1};
-  labels.dtype = PaddleDType::INT64;
-  labels.data.Resize(sizeof(int64_t) * num_images);
-
-  for (int i = 0; i < num_images; i++) {
-    auto batch = i / test_data_batch_size;
-    auto element_in_batch = i % test_data_batch_size;
-    std::copy_n(static_cast<float *>(test_data[batch][0].data.data()) +
-                    element_in_batch * 3 * 224 * 224,
-                3 * 224 * 224,
-                static_cast<float *>(images.data.data()) + i * 3 * 224 * 224);
-
-    std::copy_n(static_cast<int64_t *>(test_data[batch][1].data.data()) +
-                    element_in_batch,
-                1, static_cast<int64_t *>(labels.data.data()) + i);
-  }
-
-  auto warmup_data = std::make_shared<std::vector<PaddleTensor>>(2);
-  (*warmup_data)[0] = std::move(images);
-  (*warmup_data)[1] = std::move(labels);
-  return warmup_data;
-}
-
 void SetInput(std::vector<std::vector<PaddleTensor>> *inputs,
+              bool with_accuracy_layer = FLAGS_with_accuracy_layer,
               int32_t batch_size = FLAGS_batch_size) {
   std::ifstream file(FLAGS_infer_data, std::ios::binary);
   if (!file) {
@@ -124,49 +81,47 @@ void SetInput(std::vector<std::vector<PaddleTensor>> *inputs,
   std::vector<int> image_batch_shape{batch_size, 3, 224, 224};
   std::vector<int> label_batch_shape{batch_size, 1};
   auto images_offset_in_file = static_cast<size_t>(file.tellg());
-  auto labels_offset_in_file =
-      images_offset_in_file + sizeof(float) * total_images * 3 * 224 * 224;
 
   TensorReader<float> image_reader(file, images_offset_in_file,
                                    image_batch_shape, "image");
-  TensorReader<int64_t> label_reader(file, labels_offset_in_file,
-                                     label_batch_shape, "label");
 
   auto iterations_max = total_images / batch_size;
   auto iterations = iterations_max;
   if (FLAGS_iterations > 0 && FLAGS_iterations < iterations_max) {
     iterations = FLAGS_iterations;
   }
+
+  auto labels_offset_in_file =
+      images_offset_in_file + sizeof(float) * total_images * 3 * 224 * 224;
+
+  TensorReader<int64_t> label_reader(file, labels_offset_in_file,
+                                     label_batch_shape, "label");
   for (auto i = 0; i < iterations; i++) {
     auto images = image_reader.NextBatch();
-    auto labels = label_reader.NextBatch();
-    inputs->emplace_back(
-        std::vector<PaddleTensor>{std::move(images), std::move(labels)});
+    std::vector<PaddleTensor> tmp_vec;
+    tmp_vec.push_back(std::move(images));
+    if (with_accuracy_layer) {
+      auto labels = label_reader.NextBatch();
+      tmp_vec.push_back(std::move(labels));
+    }
+    inputs->push_back(std::move(tmp_vec));
   }
 }
 
-TEST(Analyzer_int8_image_classification, quantization) {
-  AnalysisConfig cfg;
-  SetConfig(&cfg);
+TEST(Analyzer_qat_image_classification, quantization) {
+  AnalysisConfig fp32_cfg;
+  SetConfig(&fp32_cfg, FLAGS_fp32_model);
 
-  AnalysisConfig q_cfg;
-  SetConfig(&q_cfg);
+  AnalysisConfig int8_cfg;
+  SetConfig(&int8_cfg, FLAGS_int8_model);
 
   // read data from file and prepare batches with test data
   std::vector<std::vector<PaddleTensor>> input_slots_all;
   SetInput(&input_slots_all);
 
-  // prepare warmup batch from input data read earlier
-  // warmup batch size can be different than batch size
-  std::shared_ptr<std::vector<PaddleTensor>> warmup_data =
-      GetWarmupData(input_slots_all);
-
-  // configure quantizer
-  q_cfg.EnableMkldnnQuantizer();
-  q_cfg.mkldnn_quantizer_config()->SetWarmupData(warmup_data);
-  q_cfg.mkldnn_quantizer_config()->SetWarmupBatchSize(FLAGS_warmup_batch_size);
-
-  CompareQuantizedAndAnalysis(&cfg, &q_cfg, input_slots_all);
+  // 0 is avg_cost, 1 is top1_accuracy, 2 is top5_accuracy or mAP
+  CompareAnalysisAndAnalysis(&fp32_cfg, &int8_cfg, input_slots_all,
+                             FLAGS_with_accuracy_layer, 1);
 }
 
 }  // namespace analysis
