cd43c4440ef5aec02c3eb242b6f0cade225bb0e2
Jacek Czaja <jacek.czaja@intel.com>
2019-11-29 05:06:07 +0100

    [MKL-DNN] LRN and Pool2d (FWD) NHWC support (#21375)

--- paddle/fluid/operators/pool_op.h
+++ paddle/fluid/operators/pool_op.h
@@ -35,6 +35,10 @@ class PoolOp : public framework::OperatorWithKernel {
  protected:
   framework::OpKernelType GetExpectedKernelType(
       const framework::ExecutionContext& ctx) const override;
+
+  framework::OpKernelType GetKernelTypeForVar(
+      const std::string& var_name, const Tensor& tensor,
+      const framework::OpKernelType& expected_kernel_type) const;
 };
 
 class PoolOpGrad : public framework::OperatorWithKernel {
