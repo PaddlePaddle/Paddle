b94a1ec11038e5dbf19d60c848d14fa8df40129a
zhangting2020 <709968123@qq.com>
2019-12-02 10:40:03 +0000

    fix PythonAPI test of group_norm for random failure, test=develop

--- python/paddle/fluid/tests/unittests/test_group_norm_op.py
+++ python/paddle/fluid/tests/unittests/test_group_norm_op.py
@@ -15,6 +15,7 @@
 from __future__ import print_function
 import unittest
 import numpy as np
+import random
 
 from operator import mul
 import paddle.fluid.core as core
@@ -194,6 +195,10 @@ class TestGroupNormOpLargeData_With_NHWC(TestGroupNormOp):
 
 
 class TestGroupNormAPI_With_NHWC(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(123)
+        random.seed(124)
+
     def test_case1(self):
         data1 = fluid.data(name='data1', shape=[None, 3, 3, 4], dtype='float32')
         out1 = fluid.layers.group_norm(
