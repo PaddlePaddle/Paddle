9c356d41279ae13c2a36ff2b7d3fc68bcbb9d667
FrostML <380185688@qq.com>
2019-12-04 11:22:46 +0800

    refine and check the github approver's id, test=develop

--- tools/check_api_approvals.sh
+++ tools/check_api_approvals.sh
@@ -142,10 +142,10 @@ if [ "${NEW_OP_ADDED}" != "" ] && [ "${GIT_PR_ID}" != "" ]; then
     fi
 fi
 
-INPLACE_TESTS_ADDED=`git diff --name-only --diff-filter=A upstream/$BRANCH |grep -oE "inplace_atol[[:space:]]*=.*" || true`
-if [ "${INPLACE_TESTS_ADDED}" != "" ] && [ "${GIT_PR_ID}" != "" ]; then
+HAS_INPLACE_TESTS=`git diff --name-only --diff-filter=A upstream/$BRANCH |grep -oE "inplace_atol[[:space:]]*=.*" || true`
+if [ "${HAS_INPLACE_TESTS}" != "" ] && [ "${GIT_PR_ID}" != "" ]; then
     echo_line="The calculation results of inplace is enabled and disabled must be equal, that is, it's not recommended to set inplace_atol.\n If you do need to use inplace_atol, you must have one RD (XiaoguangHu01, lanxianghit, phlrain, luotao1) approval for the usage of inplace_atol.\n"
-    check_approval 1 46782768 6836917 47554610 43953930
+    check_approval 1 46782768 47554610 43953930 6836917
 fi
 
 if [ -n "${echo_list}" ];then
