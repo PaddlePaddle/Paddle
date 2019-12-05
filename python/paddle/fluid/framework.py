37500040e03845eff978725e37f62d1e9cd5a105
JepsonWong <2013000149@qq.com>
2019-12-05 04:24:30 +0000

    fix Variable's gradient api in framework.py, test=develop

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -1074,6 +1074,7 @@ class Variable(object):
                 import paddle.fluid as fluid
                 import numpy as np
 
+                # example1: return ndarray
                 x = np.ones([2, 2], np.float32)
                 with fluid.dygraph.guard():
                     inputs2 = []
@@ -1087,6 +1088,20 @@ class Variable(object):
                     backward_strategy.sort_sum_gradient = True
                     loss2.backward(backward_strategy)
                     print(loss2.gradient())
+
+                # example2: return tuple of ndarray
+                with fluid.dygraph.guard():
+                    embedding = fluid.dygraph.Embedding(
+                        name_scope='embedding',
+                        size=[20, 32],
+                        param_attr='emb.w',
+                        is_sparse=True)
+                    x_data = np.arange(12).reshape(4, 3).astype('int64')
+                    x_data = x_data.reshape((-1, 3, 1))
+                    x = fluid.dygraph.base.to_variable(x_data)
+                    out = embedding(x)
+                    out.backward()
+                    print(embedding._w.gradient())
 
         """
         pass
