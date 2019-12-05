a40d9041a66b8304f058ab7facaefca833b92da4
Aurelius84 <liujiezhangbupt@gmail.com>
2019-12-05 20:42:11 +0800

    add sub-branch in test_backward.py test=develop

--- python/paddle/fluid/tests/unittests/test_backward.py
+++ python/paddle/fluid/tests/unittests/test_backward.py
@@ -16,21 +16,116 @@ from __future__ import print_function
 
 import unittest
 import paddle.fluid as fluid
-import collections
-from simple_nets import init_data
+import numpy as np
 
 
-class Simple_Net():
+class BackwardNet(object):
     def __init__(self):
-        self.stop_gradient_vars = []
-        self.no_grad_set = []
+        self.stop_gradient_grad_vars = []
+        self.no_grad_vars = []
+        self.params_names = []
+        self.op_path = []
+
+    def build_model(self):
+        raise NotImplementedError
+
+    def init_data(self):
+        raise NotImplementedError
+
+    def check_backward(self, loss, main_program):
+        params_grads = self.check_params_grad(loss)
+        # 1.1 get_stop_gradients
+        no_grad_dict = self.check_stop_gradient(main_program)
+        # 1.2 find_op_path
+        op_path, block_no_grad_set = self.check_op_path(
+            main_program.block(0), [loss], [], no_grad_dict)
+        # 1.3 _find_no_grad_vars
+        no_grad_vars = self.check_find_no_grad_vars(
+            main_program.block(0), op_path, [loss], block_no_grad_set)
+        # update no_grad_dict
+        block_no_grad_set.update(no_grad_vars)
+        no_grad_dict[0].update(
+            list(map(fluid.backward._append_grad_suffix_, block_no_grad_set)))
+
+    def check_params_grad(self, loss, parameter_list=None, no_grad_set=None):
+        params_grads = fluid.backward.append_backward(loss, parameter_list,
+                                                      no_grad_set)
+        params_names = set([var[0].name for var in params_grads])
+        assert len(params_names - set(self.params_names)) == 0
+
+        return params_grads
+
+    def check_stop_gradient(self, program):
+        no_grad_dict = fluid.backward._get_stop_gradients_(program)
+        if no_grad_dict is not None and isinstance(no_grad_dict, dict):
+            assert len(no_grad_dict[0] - set(self.stop_gradient_grad_vars)) == 0
+
+        return no_grad_dict
+
+    def check_op_path(self, root_block, outputs, inputs=[], no_grad_dict=None):
+        if no_grad_dict is None or not isinstance(no_grad_dict, dict):
+            block_no_grad_set = None
+        else:
+            block_no_grad_set = set(
+                map(fluid.backward._strip_grad_suffix_, no_grad_dict[0]))
+        op_path = fluid.backward._find_op_path_(root_block, outputs, inputs,
+                                                block_no_grad_set)
+        op_types = [op.type for op in op_path]
+        assert op_types == self.op_path
+
+        return op_path, block_no_grad_set
+
+    def check_find_no_grad_vars(self, root_block, op_path, targets,
+                                block_no_grad_set):
+        no_grad_vars = fluid.backward._find_no_grad_vars(
+            root_block, op_path, targets, block_no_grad_set)
+        assert len(no_grad_vars) == len(self.no_grad_vars)
+
+        return no_grad_vars
+
+
+class SimpleNet(BackwardNet):
+    def __init__(self):
+        super(BackwardNet, self).__init__()
+        self.stop_gradient_grad_vars = [
+            u'x_no_grad@GRAD', u'x2_no_grad@GRAD', u'x3_no_grad@GRAD',
+            u'label_no_grad@GRAD'
+        ]
+        self.no_grad_vars = []
+        self.params_names = [u'w2v', u'fc_predict.b_0', u'fc_w']
+        self.op_path = [
+            u'lookup_table_v2',
+            u'lookup_table_v2',  # embedding
+            u'elementwise_add',  # merge
+            u'mul',
+            u'elementwise_add',
+            u'softmax',  # fc
+            u'elementwise_sub',
+            u'square',
+            u'mean'
+        ]  # loss
+        self.shape = [16, 50]
+
+    def init_data(self):
+        assert len(self.shape) == 2
+        x = np.random.randint(0, 90, self.shape).astype('int64')
+        x2 = np.random.randint(0, 90, self.shape).astype('int64')
+        x3 = np.random.randint(0, 90, self.shape).astype('int64')
+        label = np.random.random([self.shape[0], 1]).astype('float32')
+        return {
+            'x_no_grad': x,
+            'x2_no_grad': x2,
+            'x3_no_grad': x3,
+            'label_no_grad': label
+        }
 
     def build_model(self):
         # stop_gradient = True in input
-        x = fluid.data(name='x_no_grad', shape=[13], dtype='int64')
-        x2 = fluid.data(name='x2_no_grad', shape=[13], dtype='int64')
-        x3 = fluid.data(name='x3_no_grad', shape=[13], dtype='int64')
-        label = fluid.data(name='label_no_grad', shape=[13, 1], dtype='int64')
+        x = fluid.data(name='x_no_grad', shape=self.shape, dtype='int64')
+        x2 = fluid.data(name='x2_no_grad', shape=self.shape, dtype='int64')
+        x3 = fluid.data(name='x3_no_grad', shape=self.shape, dtype='int64')
+        label = fluid.data(
+            name='label_no_grad', shape=[self.shape[0], 1], dtype='float32')
         # shared layer, the grad of 'w2v' will be summed and renamed.
         # To test  _addup_repetitive_outputs_
         x_emb = fluid.embedding(
@@ -61,88 +156,15 @@ class Simple_Net():
 
         return loss
 
-    def check_params_grad(self, loss, parameter_list=None, no_grad_set=None):
-        params_grads = fluid.backward.append_backward(loss, parameter_list,
-                                                      no_grad_set)
-        gt = [u'w2v', u'fc_predict.b_0', u'fc_w']
-        params_names = set([var[0].name for var in params_grads])
-        assert len(params_names - set(gt)) == 0
 
-        return params_grads
-
-    def check_stop_gradient(self, program):
-        no_grad_dict = fluid.backward._get_stop_gradients_(program)
-        gt = [
-            u'x_no_grad@GRAD', u'x2_no_grad@GRAD', u'x3_no_grad@GRAD',
-            u'label_no_grad@GRAD'
-        ]
-        if no_grad_dict is not None and isinstance(no_grad_dict, dict):
-            assert len(no_grad_dict[0] - set(gt)) == 0
-
-        return no_grad_dict
-
-    def check_op_path(self, root_block, outputs, inputs=[], no_grad_dict=None):
-        if no_grad_dict is None or not isinstance(no_grad_dict, dict):
-            block_no_grad_set = None
-        else:
-            block_no_grad_set = set(
-                map(fluid.backward._strip_grad_suffix_, no_grad_dict[0]))
-        op_path = fluid.backward._find_op_path_(root_block, outputs, inputs,
-                                                block_no_grad_set)
-        op_types = [op.type for op in op_path]
-        gt = [
-            u'lookup_table_v2',
-            u'lookup_table_v2',  # embedding
-            u'elementwise_add',  # merge
-            u'mul',
-            u'elementwise_add',
-            u'softmax',  # fc
-            u'elementwise_sub',
-            u'square',
-            u'mean'
-        ]  # loss
-        assert op_types == gt
-
-        return op_path, block_no_grad_set
-
-    def check_find_no_grad_vars(self, root_block, op_path, targets,
-                                block_no_grad_set):
-        no_grad_vars = fluid.backward._find_no_grad_vars(
-            root_block, op_path, targets, block_no_grad_set)
-        assert len(no_grad_vars) == 0
-
-        return no_grad_vars
-
-
-def case2_prune_no_grad_branch():
-    x = fluid.layers.data(name='image', shape=[784], dtype='float32')
-    label = fluid.layers.data(name='label', shape=[1], dtype='int64')
-    feature = fluid.layers.fc(input=x, size=10, act=None)
-    label = fluid.layers.cast(label, dtype="float32")
-    label = fluid.layers.cast(label, dtype='int64')
-    # Note that the label is not persistable in fluid.layers.cross_entropy.
-    loss = fluid.layers.cross_entropy(input=feature, label=label)
-    loss = fluid.layers.mean(loss)
-    return loss
-
-
-def case3_prune_no_grad_branch2():
-    label = fluid.layers.data(name='label', shape=[1], dtype='int64')
-    label = fluid.layers.cast(label, dtype="float32")
-    label = fluid.layers.cast(label, dtype='int64')
-    out = fluid.layers.one_hot(input=label, depth=100)
-    loss = fluid.layers.mean(out)
-    return loss
-
-
-def case4_with_no_grad_op_maker():
-    out = fluid.layers.gaussian_random(shape=[20, 30])
-    loss = fluid.layers.mean(out)
-    return loss
+# NotImplemented
+class ConditionalNet(BackwardNet):
+    def __init__(self):
+        super(BackwardNet, self).__init__()
 
 
 class TestBackward(unittest.TestCase):
-    def check_backward(self, net, feed_dict):
+    def check_backward(self, net):
         place = fluid.CPUPlace()
         exe = fluid.Executor(place)
 
@@ -151,44 +173,16 @@ class TestBackward(unittest.TestCase):
 
         with fluid.program_guard(main, startup):
             loss = net.build_model()
+            net.check_backward(loss, main)
 
             optimizer = fluid.optimizer.SGD(learning_rate=0.1)
-            # test each step of minimize
-            # step 1: append_backward
-
-            params_grads = net.check_params_grad(loss)
-            print(params_grads)
-            # 1.1 get_stop_gradients
-            no_grad_dict = net.check_stop_gradient(main)
-            print(no_grad_dict)
-            # 1.2 find_op_path
-            op_path, block_no_grad_set = net.check_op_path(
-                main.block(0), [loss], [], no_grad_dict)
-            print(op_path)
-            # 1.3 _find_no_grad_vars
-            no_grad_vars = net.check_find_no_grad_vars(
-                main.block(0), op_path, [loss], block_no_grad_set)
-            print(no_grad_vars)
-            #
-            block_no_grad_set.update(no_grad_vars)
-            no_grad_dict[0].update(
-                list(
-                    map(fluid.backward._append_grad_suffix_,
-                        block_no_grad_set)))
-
-            # run whole net
             optimizer.minimize(loss)
-            # exe.run(feed=feed_dict)
+            exe.run(startup)
+            exe.run(feed=net.init_data())
 
     def test_backward(self):
-        # batch_size = 2
-        # img, label = init_data(batch_size, img_shape=[784], label_range=9)
-        # feed_dict = {'image': img, 'label': label}
-        simple_net = Simple_Net()
-        self.check_backward(simple_net, {})
-        # self.check_backward(case2_prune_no_grad_branch, feed_dict)
-        # self.check_backward(case3_prune_no_grad_branch2, {'label': label})
-        # self.check_backward(case4_with_no_grad_op_maker, {})
+        simple_net = SimpleNet()
+        self.check_backward(simple_net)
 
 
 if __name__ == '__main__':
