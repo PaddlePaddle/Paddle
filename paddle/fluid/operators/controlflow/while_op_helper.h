86cbbba34443fdd6bdfe1f681d016cef9dde9cf5
gfwm2013 <2449465946@qq.com>
2019-12-05 23:39:15 +0000

    Make while op can run on GPU
    test=develop

--- paddle/fluid/operators/controlflow/while_op_helper.h
+++ paddle/fluid/operators/controlflow/while_op_helper.h
@@ -17,7 +17,9 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/framework/operator.h"
+#include "paddle/fluid/platform/device_context.h"
 #include "paddle/fluid/platform/variant.h"
 
 namespace paddle {
@@ -39,6 +41,22 @@ void PrepareSafeEagerDeletionOnWhileOpAndWhileGradOp(
     const framework::ProgramDesc &program,
     const std::vector<framework::OperatorBase *> &while_ops,
     const std::vector<framework::OperatorBase *> &while_grad_ops);
+
+inline bool GetCondData(const framework::LoDTensor &cond) {
+  if (platform::is_cpu_place(cond.place())) {
+    return cond.data<bool>()[0];
+  }
+  // when platform::is_gpu_place(cond.place()) is true
+  std::unique_ptr<framework::LoDTensor> cpu_cond{new framework::LoDTensor()};
+#ifdef PADDLE_WITH_CUDA
+  framework::TensorCopySync(cond, platform::CPUPlace(), cpu_cond.get());
+#else
+  PADDLE_THROW(
+      "This version of PaddlePaddle doen NOT support GPU but got GPU tensor "
+      "Cond in WhileOp. Please compile WITH_GPU option");
+#endif
+  return cpu_cond->data<bool>()[0];
+}
 
 }  // namespace operators
 }  // namespace paddle
