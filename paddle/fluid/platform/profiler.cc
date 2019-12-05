df7142ca40ede0fb7320dd3983c199e2b3b731e4
wangchaochaohu <wangchao66@baidu.com>
2019-12-05 03:09:37 +0000

    revise warning message test=develop

--- paddle/fluid/platform/profiler.cc
+++ paddle/fluid/platform/profiler.cc
@@ -86,6 +86,7 @@ double Event::CudaElapsedMs(const Event &e) const {
 #ifdef PADDLE_WITH_CUPTI
   return gpu_ns_ / 1000000.0;
 #else
+  LOG_FIRST_N(WARNING, 1) << "CUDA CUPTI is not enabled";
   return 0;
 #endif
 }
