b8344ba1df14eb831d7c5fe7dcde545ea49c9f62
Aurelius84 <liujiezhangbupt@gmail.com>
2019-12-06 14:48:59 +0800

    decrease lodLevel in seq_pool_op.cc test=develop (#21604)

--- paddle/fluid/operators/sequence_ops/sequence_pool_op.cc
+++ paddle/fluid/operators/sequence_ops/sequence_pool_op.cc
@@ -28,6 +28,16 @@ class SequencePoolOp : public framework::OperatorWithKernel {
                       "Input(X) of SequencePoolOp should not be null.");
     PADDLE_ENFORCE_EQ(ctx->HasOutput("Out"), true,
                       "Output(Out) of SequencePoolOp should not be null.");
+    if (!ctx->IsRuntime()) {
+      // Check the lod_level for compile-time.
+      framework::VarDesc* x_desc =
+          boost::get<framework::VarDesc*>(ctx->GetInputVarPtrs("X")[0]);
+      auto in_lod_level = x_desc->GetLoDLevel();
+      PADDLE_ENFORCE_GT(
+          in_lod_level, 0,
+          "The LoD level Input(X) of sequence_pool should be larger than 0.");
+      ctx->DecreaseLoDLevel("X", "Out");
+    }
     ctx->SetOutputDim("Out", ctx->GetInputDim("X"));
     if (ctx->Attrs().Get<std::string>("pooltype") == "MAX") {
       PADDLE_ENFORCE_EQ(
