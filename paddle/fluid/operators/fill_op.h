fde47a12b06a8c9afbc94a51066d9ba80ea716f8
gfwm2013 <2449465946@qq.com>
2019-12-09 09:32:47 +0000

    Add CI checking for ShareDatawith or ShareBufferWith
    test=develop

--- paddle/fluid/operators/fill_op.h
+++ paddle/fluid/operators/fill_op.h
@@ -59,7 +59,6 @@ class FillKernel : public framework::OpKernel<T> {
 
     if (force_cpu || platform::is_cpu_place(ctx.GetPlace())) {
       tensor.ShareDataWith(out);
-      tensor.ShareDataWith(out);
     } else {
       // Always make tensor in CPU memory.
       tensor.Resize(out.dims());
