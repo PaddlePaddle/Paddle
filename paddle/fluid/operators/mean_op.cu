d65afef1000b6e0e7b2790ae27361b66939f2ce4
wangchaochaohu <wangchao66@baidu.com>
2019-12-09 09:19:57 +0000

    accelerate mean op test=develop

--- paddle/fluid/operators/mean_op.cu
+++ paddle/fluid/operators/mean_op.cu
@@ -11,15 +11,60 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
+#include "cub/cub.cuh"
 #include "paddle/fluid/operators/mean_op.h"
 #include "paddle/fluid/platform/float16.h"
+
+namespace paddle {
+namespace operators {
+
+template <typename T>
+struct DivideFunctor {
+  HOSTDEVICE explicit inline DivideFunctor(int n) : n_inv((T)(1.0 / n)) {}
+
+  HOSTDEVICE inline T operator()(const T& x) const { return x * n_inv; }
+
+ private:
+  T n_inv;
+};
+
+template <typename DeviceContext, typename T>
+class MeanCUDAKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& context) const override {
+    auto* input = context.Input<Tensor>("X");
+    auto* output = context.Output<Tensor>("Out");
+
+    output->mutable_data<T>(context.GetPlace());
+    auto size_prob = input->numel();
+    const T* in_data = input->data<T>();
+    T* out_data = output->mutable_data<T>(context.GetPlace());
+    auto stream = context.cuda_device_context().stream();
+
+    DivideFunctor<T> transformer(size_prob);
+    cub::TransformInputIterator<T, DivideFunctor<T>, const T*> trans_x(
+        in_data, transformer);
+    size_t temp_storage_bytes = 0;
+
+    cub::DeviceReduce::Sum(nullptr, temp_storage_bytes, trans_x, out_data,
+                           size_prob, stream);
+    framework::Tensor tmp;
+    auto* temp_storage = tmp.mutable_data<uint8_t>(
+        framework::make_ddim({static_cast<int64_t>(temp_storage_bytes)}),
+        context.GetPlace());
+    cub::DeviceReduce::Sum(temp_storage, temp_storage_bytes, trans_x, out_data,
+                           size_prob, stream);
+  }
+};
+}  // namespace operators
+}  // namespace paddle
 
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
 REGISTER_OP_CUDA_KERNEL(
-    mean, ops::MeanKernel<paddle::platform::CUDADeviceContext, float>,
-    ops::MeanKernel<paddle::platform::CUDADeviceContext, double>,
-    ops::MeanKernel<paddle::platform::CUDADeviceContext, plat::float16>);
+    mean, ops::MeanCUDAKernel<paddle::platform::CUDADeviceContext, float>,
+    ops::MeanCUDAKernel<paddle::platform::CUDADeviceContext, double>,
+    ops::MeanCUDAKernel<paddle::platform::CUDADeviceContext, plat::float16>);
 REGISTER_OP_CUDA_KERNEL(
     mean_grad, ops::MeanGradKernel<paddle::platform::CUDADeviceContext, float>,
     ops::MeanGradKernel<paddle::platform::CUDADeviceContext, double>,
