74c59e7e28480e496592b57984f65a2c6b6ad78e
baojun-nervana <baojun.liu@intel.com>
2019-12-10 22:44:32 +0000

    reduce file changes test=develop

--- python/paddle/fluid/tests/unittests/ngraph/test_softmax_ngraph_op.py
+++ python/paddle/fluid/tests/unittests/ngraph/test_softmax_ngraph_op.py
@@ -13,9 +13,8 @@
 # limitations under the License.
 
 from __future__ import print_function
-import unittest, sys
-sys.path.append("../")
-from test_softmax_op import TestSoftmaxOp, TestSoftmaxOp2, TestSoftmaxOp3, TestSoftmaxOp4, TestSoftmaxOp5
+import unittest
+from paddle.fluid.tests.unittests.test_softmax_op import TestSoftmaxOp, TestSoftmaxOp2, TestSoftmaxOp3, TestSoftmaxOp4, TestSoftmaxOp5
 
 if __name__ == "__main__":
     unittest.main()
