aced06a42dca9f3f99037dda523f0c9ec248abf1
juncaipeng <13006307475@163.com>
2019-12-10 12:54:08 +0800

    add dtype for TestLoDTensorAndSelectedRowsOp, test=develop

--- python/paddle/fluid/tests/unittests/test_sum_op.py
+++ python/paddle/fluid/tests/unittests/test_sum_op.py
@@ -144,6 +144,7 @@ class TestLoDTensorAndSelectedRowsOp(TestSelectedRowsSumOp):
         self.height = 10
         self.row_numel = 12
         self.rows = [0, 1, 2, 2, 4, 5, 6]
+        self.dtype = np.float32
 
     def check_with_place(self, place, inplace):
         scope = core.Scope()
