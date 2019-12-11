fix input shape of op tests for elementwise_sub, gather, pad2d, transpose, softmax, scale, elementwise_max, hierarchical_sigmoid, reshape2, sign, squeeze, reduce_sum, sum, squeeze2, unsqueeze, unsqueeze2, cast, reverse
test=develop

python/paddle/fluid/tests/unittests/test_cast_op.py
@@ -24,7 +24,7 @@ from paddle.fluid import compiler, Program, program_guard
 
 class TestCastOp1(op_test.OpTest):
     def setUp(self):
-        ipt = np.random.random(size=[10, 10])
+        ipt = np.random.random(size=[10, 12])
         self.inputs = {'X': ipt.astype('float32')}
         self.outputs = {'Out': ipt.astype('float64')}
         self.attrs = {
@@ -42,7 +42,7 @@ class TestCastOp1(op_test.OpTest):
 
 class TestCastOp2(op_test.OpTest):
     def setUp(self):
-        ipt = np.random.random(size=[10, 10])
+        ipt = np.random.random(size=[10, 12])
         self.inputs = {'X': ipt.astype('float16')}
         self.outputs = {'Out': ipt.astype('float32')}
         self.attrs = {
@@ -57,7 +57,7 @@ class TestCastOp2(op_test.OpTest):
 
 class TestCastOp3(op_test.OpTest):
     def setUp(self):
-        ipt = np.random.random(size=[10, 10])
+        ipt = np.random.random(size=[10, 12])
         self.inputs = {'X': ipt.astype('float32')}
         self.outputs = {'Out': ipt.astype('float16')}
         self.attrs = {

python/paddle/fluid/tests/unittests/test_scale_op.py
@@ -26,7 +26,7 @@ class TestScaleOp(OpTest):
         self.op_type = "scale"
         self.dtype = np.float32
         self.init_dtype_type()
-        self.inputs = {'X': np.random.random((10, 10)).astype(self.dtype)}
+        self.inputs = {'X': np.random.random((10, 12)).astype(self.dtype)}
         self.attrs = {'scale': -2.3}
         self.outputs = {
             'Out': self.inputs['X'] * self.dtype(self.attrs['scale'])
@@ -49,7 +49,7 @@ class TestScaleOpScaleVariable(OpTest):
         self.init_dtype_type()
         self.scale = -2.3
         self.inputs = {
-            'X': np.random.random((10, 10)).astype(self.dtype),
+            'X': np.random.random((10, 12)).astype(self.dtype),
             'ScaleTensor': np.array([self.scale]).astype('float32')
         }
         self.attrs = {}

python/paddle/fluid/tests/unittests/test_sum_op.py
@@ -27,9 +27,9 @@ class TestSumOp(OpTest):
         self.init_kernel_type()
         self.use_mkldnn = False
         self.init_kernel_type()
-        x0 = np.random.random((3, 4)).astype(self.dtype)
-        x1 = np.random.random((3, 4)).astype(self.dtype)
-        x2 = np.random.random((3, 4)).astype(self.dtype)
+        x0 = np.random.random((3, 40)).astype(self.dtype)
+        x1 = np.random.random((3, 40)).astype(self.dtype)
+        x2 = np.random.random((3, 40)).astype(self.dtype)
         self.inputs = {"X": [("x0", x0), ("x1", x1), ("x2", x2)]}
         y = x0 + x1 + x2
         self.outputs = {'Out': y}