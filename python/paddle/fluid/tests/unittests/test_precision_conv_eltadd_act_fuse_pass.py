dc3e0917460ba3454ac577852173614403f1e481
FrostML <380185688@qq.com>
2019-12-11 12:55:13 +0800

    add test for passes' precision, test=develop

---
+++ python/paddle/fluid/tests/unittests/test_precision_conv_eltadd_act_fuse_pass.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import unittest
+import paddle.fluid as fluid
+from paddle.fluid.core import PaddleTensor
+from paddle.fluid.core import AnalysisConfig
+from paddle.fluid.core import PaddleDType
+from paddle.fluid.core import create_paddle_predictor
+
+
+class TestSeqconvEltaddReluPass(unittest.TestCase):
+    '''This pass only enabled on gpu now. 
+    '''
+
+    def test_conv_eltadd_pass_gpu_precision(self):
+        x = fluid.data(name='x', shape=[-1, 3, 100, 100])
+        conv_res = fluid.layers.conv2d(
+            x, num_filters=3, filter_size=3, act='relu')
+        place = fluid.CUDAPlace(0)
+        exe = fluid.Executor(place)
+        exe.run(fluid.default_startup_program())
+        np_x = np.array([i % 255 for i in range(30000)]).reshape(
+            [1, 3, 100, 100]).astype('float32')
+        fw_output = exe.run(feed={"x": np_x}, fetch_list=[conv_res])
+        print(fw_output[0])
+        path = "./tmp/inference_model"
+        fluid.io.save_inference_model(
+            dirname=path,
+            feeded_var_names=['x'],
+            target_vars=[conv_res],
+            executor=exe)
+        config = AnalysisConfig(path)
+        config.enable_use_gpu(100, 0)
+        predictor = create_paddle_predictor(config)
+        data = np.array([i % 255 for i in range(30000)]).reshape(
+            [1, 3, 100, 100]).astype('float32')
+        inputs = PaddleTensor(data)
+        outputs = predictor.run([inputs])
+        output = outputs[0]
+        output_data = output.as_ndarray()
+        print(output_data)
+        self.assertEqual(output_data.shape, np.array(fw_output[0]).shape)
+        self.assertTrue(
+            np.allclose(
+                np.array(fw_output[0]).ravel(), output_data.ravel(),
+                rtol=1e-05))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python/paddle/fluid/tests/unittests/test_precision_fc_fuse_pass.py
@@ -0,0 +1,109 @@
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import unittest
+import paddle.fluid as fluid
+from paddle.fluid.core import PaddleTensor
+from paddle.fluid.core import AnalysisConfig
+from paddle.fluid.core import PaddleDType
+from paddle.fluid.core import create_paddle_predictor
+
+
+class TestSeqconvEltaddReluPass(unittest.TestCase):
+    def test_conv_eltadd_pass_cpu_precision(self):
+        x = fluid.data(name='x', shape=[-1, 3, 10, 10])
+
+        weight_param1 = fluid.ParamAttr(
+            name='fc_weight1',
+            initializer=fluid.initializer.NormalInitializer())
+        bias_param1 = fluid.ParamAttr(
+            name='fc_bias1', initializer=fluid.initializer.NormalInitializer())
+        fc1_res = fluid.layers.fc(x,
+                                  size=100,
+                                  act='relu',
+                                  param_attr=weight_param1,
+                                  bias_attr=bias_param1)
+
+        place = fluid.CPUPlace()
+        exe = fluid.Executor(place)
+        exe.run(fluid.default_startup_program())
+        np_x = np.array([float(i % 255 / 255) for i in range(300)]).reshape(
+            [1, 3, 10, 10]).astype('float32')
+        fw_output = exe.run(feed={"x": np_x}, fetch_list=[fc1_res])
+        path = "./tmp/inference_model"
+        fluid.io.save_inference_model(
+            dirname=path,
+            feeded_var_names=['x'],
+            target_vars=[fc1_res],
+            executor=exe)
+        config = AnalysisConfig(path)
+        config.disable_gpu()
+        predictor = create_paddle_predictor(config)
+        data = np.array([float(i % 255 / 255) for i in range(300)]).reshape(
+            [1, 3, 10, 10]).astype('float32')
+        inputs = PaddleTensor(data)
+        outputs = predictor.run([inputs])
+        output = outputs[0]
+        output_data = output.as_ndarray()
+        self.assertEqual(output_data.shape, np.array(fw_output[0]).shape)
+        self.assertTrue(
+            np.allclose(
+                np.array(fw_output[0]).ravel(), output_data.ravel(),
+                rtol=1e-05))
+
+    def test_conv_eltadd_pass_gpu_precision(self):
+        x = fluid.data(name='x', shape=[-1, 3, 10, 10])
+
+        weight_param1 = fluid.ParamAttr(
+            name='fc_weight1',
+            initializer=fluid.initializer.NormalInitializer())
+        bias_param1 = fluid.ParamAttr(
+            name='fc_bias1', initializer=fluid.initializer.NormalInitializer())
+        fc1_res = fluid.layers.fc(x,
+                                  size=100,
+                                  act='relu',
+                                  param_attr=weight_param1,
+                                  bias_attr=bias_param1)
+
+        place = fluid.CUDAPlace(0)
+        exe = fluid.Executor(place)
+        exe.run(fluid.default_startup_program())
+        np_x = np.array([float(i % 255 / 255) for i in range(300)]).reshape(
+            [1, 3, 10, 10]).astype('float32')
+        fw_output = exe.run(feed={"x": np_x}, fetch_list=[fc1_res])
+        path = "./tmp/inference_model"
+        fluid.io.save_inference_model(
+            dirname=path,
+            feeded_var_names=['x'],
+            target_vars=[fc1_res],
+            executor=exe)
+        config = AnalysisConfig(path)
+        config.enable_use_gpu(100, 0)
+        predictor = create_paddle_predictor(config)
+        data = np.array([float(i % 255 / 255) for i in range(300)]).reshape(
+            [1, 3, 10, 10]).astype('float32')
+        inputs = PaddleTensor(data)
+        outputs = predictor.run([inputs])
+        output = outputs[0]
+        output_data = output.as_ndarray()
+        self.assertEqual(output_data.shape, np.array(fw_output[0]).shape)
+        self.assertTrue(
+            np.allclose(
+                np.array(fw_output[0]).ravel(), output_data.ravel(),
+                rtol=1e-05))
+
+
+if __name__ == '__main__':
+    unittest.main()
