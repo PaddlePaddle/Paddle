f6ee2cbac94fa5b248481dd765ce0e22cdfdf72c
GaoWei8 <catherin.gao@foxmail.com>
2019-12-13 09:03:24 +0000

    add cast test=develop

--- python/paddle/fluid/tests/unittests/test_cast_op.py
+++ python/paddle/fluid/tests/unittests/test_cast_op.py
@@ -52,7 +52,7 @@ class TestCastOp2(op_test.OpTest):
         self.op_type = 'cast'
 
     def test_check_output(self):
-        self.check_output()
+        self.check_output(atol=1e-3)
 
 
 class TestCastOp3(op_test.OpTest):
@@ -67,7 +67,7 @@ class TestCastOp3(op_test.OpTest):
         self.op_type = 'cast'
 
     def test_check_output(self):
-        self.check_output()
+        self.check_output(atol=1e-3)
 
 
 class TestCastOpError(unittest.TestCase):
