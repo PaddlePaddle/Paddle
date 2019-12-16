507834de109417e5bd1e1915e56351faebb69a86
GaoWei8 <catherin.gao@foxmail.com>
2019-12-16 02:47:49 +0000

    Remove self-set accuracy parameters of op tests test=develop

--- python/paddle/fluid/tests/unittests/test_log_loss_op.py
+++ python/paddle/fluid/tests/unittests/test_log_loss_op.py
@@ -42,7 +42,7 @@ class TestLogLossOp(OpTest):
         self.check_output()
 
     def test_check_grad(self):
-        self.check_grad(['Predicted'], 'Loss')
+        self.check_grad(['Predicted'], 'Loss', max_relative_error=0.03)
 
 
 if __name__ == '__main__':
