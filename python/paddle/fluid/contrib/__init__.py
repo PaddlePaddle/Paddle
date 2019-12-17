49eab558cc03f208a1825eda180b037e18d1e32d
JesseyXujin <516770320@qq.com>
2019-12-17 08:22:10 +0000

    python install

--- python/paddle/fluid/contrib/__init__.py
+++ python/paddle/fluid/contrib/__init__.py
@@ -24,14 +24,10 @@ from . import quantize
 from .quantize import *
 from . import reader
 from .reader import *
-from . import slim
-from .slim import *
 from . import utils
 from .utils import *
 from . import extend_optimizer
 from .extend_optimizer import *
-from . import model_stat
-from .model_stat import *
 from . import mixed_precision
 from .mixed_precision import *
 from . import layers
@@ -43,7 +39,6 @@ __all__ += memory_usage_calc.__all__
 __all__ += op_frequence.__all__
 __all__ += quantize.__all__
 __all__ += reader.__all__
-__all__ += slim.__all__
 __all__ += utils.__all__
 __all__ += extend_optimizer.__all__
 __all__ += ['mixed_precision']
--- python/paddle/utils/__init__.py
+++ python/paddle/utils/__init__.py
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .plot import Ploter
 __all__ = ['dump_config', 'Ploter']
