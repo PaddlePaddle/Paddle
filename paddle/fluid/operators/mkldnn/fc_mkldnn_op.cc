e90952218cd98755aa7655e2f3dad6466b874da5
Michal Gallus <michal.gallus@intel.com>
2019-12-18 13:04:03 +0100

    Remove unnecessary header
    test=develop

--- paddle/fluid/operators/mkldnn/fc_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/fc_mkldnn_op.cc
@@ -12,7 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include <mkldnn/include/mkldnn_debug.h>
 #include <mkldnn/include/mkldnn_types.h>
 #include <memory>
 #include "paddle/fluid/framework/tensor.h"
