5e108d33c9c92fb85451baa21e5094d041ef59e7
GaoWei8 <catherin.gao@foxmail.com>
2019-12-18 03:28:12 +0000

    fix error test=develop

--- python/paddle/fluid/tests/unittests/test_lstm_op.py
+++ python/paddle/fluid/tests/unittests/test_lstm_op.py
@@ -188,7 +188,7 @@ class TestLstmOp(OpTest):
         }
 
     def test_check_output(self):
-        self.check_output(ctol=1e-8, heck_dygraph=False)
+        self.check_output(atol=1e-8, check_dygraph=False)
 
     def test_check_grad(self):
         # TODO(qingqing) remove folowing lines after the check_grad is refined.
