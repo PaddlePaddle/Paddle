a5b3cd7104d7fcd351669e877c78e928fbd3183e
iclementine <ijasmine@yeah.net>
2019-12-18 15:41:47 +0000

    use defaut max_relative_error, test=develop

--- python/paddle/fluid/tests/unittests/test_erf_op.py
+++ python/paddle/fluid/tests/unittests/test_erf_op.py
@@ -34,7 +34,7 @@ class TestErfOp(OpTest):
         self.check_output()
 
     def test_check_grad(self):
-        self.check_grad(['X'], 'Out', max_relative_error=0.007)
+        self.check_grad(['X'], 'Out')
 
 
 if __name__ == '__main__':
