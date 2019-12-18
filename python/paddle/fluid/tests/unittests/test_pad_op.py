5a4e297c29aebe0266d19c6cfd66ae6930793bc2
GaoWei8 <catherin.gao@foxmail.com>
2019-12-18 12:47:47 +0000

    Remove self-set accuracy parameters of op tests: max_relative_error test=develop

--- python/paddle/fluid/tests/unittests/test_pad_op.py
+++ python/paddle/fluid/tests/unittests/test_pad_op.py
@@ -43,7 +43,7 @@ class TestPadOp(OpTest):
         self.check_output()
 
     def test_check_grad_normal(self):
-        self.check_grad(['X'], 'Out', max_relative_error=0.006)
+        self.check_grad(['X'], 'Out')
 
     def initTestCase(self):
         self.shape = (16, 16)
@@ -83,7 +83,7 @@ def create_test_fp16(parent):
             return np.float16
 
         def test_check_grad_normal(self):
-            self.check_grad(['X'], 'Out', max_relative_error=0.3)
+            self.check_grad(['X'], 'Out')
 
     cls_name = "{0}_{1}".format(parent.__name__, "Fp16")
     TestPadFp16.__name__ = cls_name
