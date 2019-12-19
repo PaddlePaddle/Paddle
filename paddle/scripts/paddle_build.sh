d97e28fa9f3c05cfd67a9f94820956d07e50dcbe
lidanqing-intel <danqing.li@intel.com>
2019-12-19 13:39:47 +0100

    move position of dmesg test=develop

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -750,6 +750,7 @@ set +x
         card_test "$single_card_tests" 1    # run cases with single GPU
         card_test "$multiple_card_tests" 2  # run cases with two GPUs
         card_test "$exclusive_tests"        # run cases exclusively, in this cases would be run with 4/8 GPUs
+        dmesg
         if [[ "$EXIT_CODE" != "0" ]]; then
             exit 8;
         fi
@@ -1145,7 +1146,6 @@ function main() {
         build ${parallel_number}
         enable_unused_var_check
         parallel_test
-        dmesg
         ;;
       cicheck_brpc)
         cmake_gen ${PYTHON_ABI:-""}
