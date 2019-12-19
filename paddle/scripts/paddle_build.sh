3896ca89838ed669e44236b914f8b9d82b62d02a
lidanqing-intel <danqing.li@intel.com>
2019-12-19 19:18:56 +0100

    only leave one test test=develop

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -1170,7 +1170,9 @@ function main() {
         cmake_gen ${PYTHON_ABI:-""}
         build ${parallel_number}
         enable_unused_var_check
-        parallel_test
+        #parallel_test
+        single_test test_qat2_int8_resnet50_mkldnn
+        dmesg
         ;;
       cicheck_coverage)
         check_approvals_of_unittest 1
