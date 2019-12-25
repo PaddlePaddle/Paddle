57451bfde7045502cbc0dc7e7ff158784f863f54
Liu Yiqun <liuyiqun01@baidu.com>
2019-12-25 14:55:21 +0800

    Improve the unittest. test=develop

--- paddle/fluid/platform/parallel_test.cc
+++ paddle/fluid/platform/parallel_test.cc
@@ -13,10 +13,52 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/platform/parallel.h"
-
+#include <vector>
 #include "gtest/gtest.h"
 
-TEST(Parallel, SetNumThreads) {
-  paddle::platform::SetNumThreads(1);
-  paddle::platform::SetNumThreads(4);
+void FillRandom(std::vector<float>* x) {
+  static unsigned int seed = 100;
+  std::mt19937 rng(seed++);
+  std::uniform_real_distribution<double> uniform_dist(0, 1);
+  for (size_t i = 0; i < x->size(); ++i) {
+    x->at(i) = static_cast<float>(uniform_dist(rng)) - 0.5;
+  }
+}
+
+void FillZero(std::vector<float>* x) {
+  for (size_t i = 0; i < x->size(); ++i) {
+    x->at(i) = 0;
+  }
+}
+
+TEST(Parallel, compute) {
+  std::vector<float> x;
+  std::vector<float> y;
+  std::vector<float> z;
+
+  auto parallel_add = [&](int64_t begin, int64_t end) {
+    for (int64_t i = begin; i < end; ++i) {
+      z[i] = x[i] + y[i];
+    }
+  };
+
+  for (int64_t length : {1, 2, 3, 4, 255}) {
+    x.resize(length);
+    y.resize(length);
+    z.resize(length);
+
+    FillRandom(&x);
+    FillRandom(&y);
+    FillZero(&z);
+
+    for (int num_threads : {1, 2, 3, 4}) {
+      paddle::platform::SetNumThreads(num_threads);
+      ASSERT_EQ(paddle::platform::GetMaxThreads(), num_threads);
+      paddle::platform::RunParallelFor(0, length, parallel_add);
+
+      for (int64_t i = 0; i < length; ++i) {
+        ASSERT_NEAR(z[i], x[i] + y[i], 1e-5);
+      }
+    }
+  }
 }
