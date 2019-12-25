9117364bb83afac4a8d145e33e653d63e53568da
songyouwei <youwei0314@gmail.com>
2019-12-25 14:13:24 +0800

    Move sequence op unittest files to a separate folder sequence (#21842)
    * move sequence op unittests to a separate folder
    test=develop
    
    * add missing CMakeList file
    test=develop
    
    * fix relative path import
    test=develop
    
    * fix relative import
    test=develop
    
    * use sys.path.append
    test=develop

--- python/paddle/fluid/tests/unittests/sequence/test_seq_pool.py
+++ python/paddle/fluid/tests/unittests/sequence/test_seq_pool.py
@@ -16,6 +16,8 @@ from __future__ import print_function
 
 import unittest
 import numpy as np
+import sys
+sys.path.append("../")
 from op_test import OpTest, skip_check_grad_ci
 from test_reorder_lod_tensor import convert_to_offset
 
--- python/paddle/fluid/tests/unittests/sequence/test_sequence_enumerate_op.py
+++ python/paddle/fluid/tests/unittests/sequence/test_sequence_enumerate_op.py
@@ -16,6 +16,8 @@ from __future__ import print_function
 
 import unittest
 import numpy as np
+import sys
+sys.path.append("../")
 from op_test import OpTest
 
 
