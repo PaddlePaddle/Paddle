754a3ba333c53f4e0769cfab1f63b60605a75d7d
sandyhouse <lilong12@baidu.com>
2019-12-26 19:32:14 +0800

    do nothing on PY3, test=develop

--- python/paddle/fluid/tests/unittests/test_plsc.sh
+++ python/paddle/fluid/tests/unittests/test_plsc.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+ver=`python -c "import platform;print(platform.python_version())"`
+ver=${ver:0:1}
+
+if [ $ver == '3' ]; then
+   echo "PLSC only support py2 now."
+   exit 0
+fi
+
 # install plsc
 python -m pip install plsc
 
