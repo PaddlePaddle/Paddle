e6e657a180fab3d27afc510e7f65933c093d6de4
sandyhouse <lilong12@baidu.com>
2019-12-26 10:03:56 +0800

    cleanup, test=develop

--- python/paddle/fluid/tests/unittests/test_plsc.sh
+++ python/paddle/fluid/tests/unittests/test_plsc.sh
@@ -2,9 +2,13 @@
 set -e
 
 # install plsc
-python -m pip install plsc --force
+python -m pip install plsc
 
 # test
 python -m paddle.distributed.launch \
         --selected_gpus="0,1" \
         --log_dir="/tmp/plsc.log" do_plsc.py
+
+# cleanup
+rm -rf /tmp/plsc.log
+python -m pip uninstall -y plsc
