7f41497524b6486446d4c805354156a681be0940
tianshuo78520a <707759223@qq.com>
2019-12-26 11:27:32 +0800

    test=py35

--- tools/manylinux1/build_scripts/install_nccl2.sh
+++ tools/manylinux1/build_scripts/install_nccl2.sh
@@ -16,7 +16,7 @@ DIR="/nccl2"
 mkdir -p $DIR
 # we cached the nccl2 deb package in BOS, so we can download it with wget
 # install nccl2: http://docs.nvidia.com/deeplearning/sdk/nccl-install-guide/index.html#down
-wget -O $DIR/$DEB $URL
+wget -O --no-proxy $DIR/$DEB $URL
 
 cd $DIR && ar x $DEB && tar xf data.tar.xz
 DEBS=$(find ./var/ -name "*.deb")
