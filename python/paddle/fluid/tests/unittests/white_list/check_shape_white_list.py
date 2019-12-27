911e4c7db6efd9e78a7ee6accc5b070e85a75417
pkpk <xiyzhouang@gmail.com>
2019-12-27 09:10:43 +0800

    Fix the bug in sequence_scatter unitest (#21960)
    * test=develop
    
    * test=develop
    
    * test=develop
    
    * change the check_shape_white_list in web
    
    * test=develop
    
    * test=develop

--- python/paddle/fluid/tests/unittests/white_list/check_shape_white_list.py
+++ python/paddle/fluid/tests/unittests/white_list/check_shape_white_list.py
@@ -18,7 +18,6 @@ NOT_CHECK_OP_LIST = [
 ]
 
 NEED_TO_FIX_OP_LIST = [
-    'sequence_scatter', 'sequence_topk_avg_pooling', 'matmul',
-    'add_position_encoding', 'margin_rank_loss', 'elementwise_pow',
-    'fused_elemwise_activation', 'mul'
+    'sequence_topk_avg_pooling', 'matmul', 'add_position_encoding',
+    'margin_rank_loss', 'elementwise_pow', 'fused_elemwise_activation', 'mul'
 ]
