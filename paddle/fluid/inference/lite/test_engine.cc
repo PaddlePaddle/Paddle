ba4381ba3023bd2af59b87f320b7a6ec69a6650b
jiweibo <jiweibo@baidu.com>
2019-12-29 03:06:43 +0000

    fix unittest error test=develop

--- paddle/fluid/inference/lite/test_engine.cc
+++ paddle/fluid/inference/lite/test_engine.cc
@@ -13,8 +13,6 @@
 // limitations under the License.
 
 #include <gtest/gtest.h>
-#include <fstream>
-#include <ios>
 
 #include "lite/api/paddle_use_kernels.h"
 #include "lite/api/paddle_use_ops.h"
@@ -22,6 +20,7 @@
 
 #include "paddle/fluid/inference/lite/engine.h"
 #include "paddle/fluid/inference/utils/singleton.h"
+#include "paddle/fluid/operators/lite/ut_helper.h"
 
 #include "paddle/fluid/framework/block_desc.h"
 #include "paddle/fluid/framework/op_desc.h"
@@ -29,61 +28,87 @@
 #include "paddle/fluid/framework/scope.h"
 
 namespace paddle {
+namespace inference {
 namespace lite {
 
-namespace {
-
-void AddTensorToBlockDesc(framework::proto::BlockDesc* block,
-                          const std::string& name,
-                          const std::vector<int64_t>& shape) {
-  using framework::proto::VarType;
-  auto* var = block->add_vars();
-  framework::VarDesc desc(name);
-  desc.SetType(VarType::LOD_TENSOR);
-  desc.SetDataType(VarType::FP32);
-  desc.SetShape(shape);
-  *var = *desc.Proto();
-}
+using inference::lite::AddTensorToBlockDesc;
+using inference::lite::CreateTensor;
+using inference::lite::serialize_params;
 
 void make_fake_model(std::string* model, std::string* param) {
   framework::ProgramDesc program;
+  LOG(INFO) << "program.block size is " << program.Size();
   auto* block_ = program.Proto()->mutable_blocks(0);
   LOG(INFO) << "create block desc";
   framework::BlockDesc block_desc(&program, block_);
-  LOG(INFO) << "create feed op";
   auto* feed0 = block_desc.AppendOp();
   feed0->SetType("feed");
   feed0->SetInput("X", {"feed"});
   feed0->SetOutput("Out", {"x"});
-  feed0->SetAttr("col", 1);
-  AddTensorToBlockDesc(block_, "x", std::vector<int64_t>({2, 4, 1, 1}));
+  feed0->SetAttr("col", 0);
+  auto* feed1 = block_desc.AppendOp();
+  feed1->SetType("feed");
+  feed1->SetInput("X", {"feed"});
+  feed1->SetOutput("Out", {"y"});
+  feed1->SetAttr("col", 1);
+  LOG(INFO) << "create elementwise_add op";
+  auto* elt_add = block_desc.AppendOp();
+  elt_add->SetType("elementwise_add");
+  elt_add->SetInput("X", std::vector<std::string>({"x"}));
+  elt_add->SetInput("Y", std::vector<std::string>({"y"}));
+  elt_add->SetOutput("Out", std::vector<std::string>({"z"}));
+  elt_add->SetAttr("axis", -1);
+  LOG(INFO) << "create fetch op";
+  auto* fetch = block_desc.AppendOp();
+  fetch->SetType("fetch");
+  fetch->SetInput("X", std::vector<std::string>({"z"}));
+  fetch->SetOutput("Out", std::vector<std::string>({"out"}));
+  fetch->SetAttr("col", 0);
+  // Set inputs' variable shape in BlockDesc
+  AddTensorToBlockDesc(block_, "x", std::vector<int64_t>({2, 4}), true);
+  AddTensorToBlockDesc(block_, "y", std::vector<int64_t>({2, 4}), true);
+  AddTensorToBlockDesc(block_, "z", std::vector<int64_t>({2, 4}), false);
+  AddTensorToBlockDesc(block_, "out", std::vector<int64_t>({2, 4}), false);
+
   *block_->add_ops() = *feed0->Proto();
-  ASSERT_EQ(block_->ops_size(), 1);
+  *block_->add_ops() = *feed1->Proto();
+  *block_->add_ops() = *elt_add->Proto();
+  *block_->add_ops() = *fetch->Proto();
+
   framework::Scope scope;
+#ifdef PADDLE_WITH_CUDA
+  platform::CUDAPlace place;
+  platform::CUDADeviceContext ctx(place);
+#else
   platform::CPUPlace place;
   platform::CPUDeviceContext ctx(place);
+#endif
+  // Prepare variables.
+  std::vector<std::string> repetitive_params{"x", "y"};
+  CreateTensor(&scope, "x", std::vector<int64_t>({2, 4}));
+  CreateTensor(&scope, "y", std::vector<int64_t>({2, 4}));
+  ASSERT_EQ(block_->ops_size(), 4);
   *model = program.Proto()->SerializeAsString();
+  serialize_params(param, &scope, repetitive_params);
 }
 
-}  // namespace
-
-TEST(EngineManager, manual) {
+TEST(EngineManager, engine) {
   ASSERT_EQ(
       inference::Singleton<inference::lite::EngineManager>::Global().Empty(),
       true);
 
   inference::lite::EngineConfig config;
   make_fake_model(&(config.model), &(config.param));
+  LOG(INFO) << "prepare config";
 
   const std::string unique_key("engine_0");
   config.model_from_memory = true;
-  config.prefer_place = {TARGET(kX86), PRECISION(kFloat)};
   config.valid_places = {
-      paddle::lite::Place({TARGET(kX86), PRECISION(kFloat)}),
-      paddle::lite::Place({TARGET(kHost), PRECISION(kAny)}),
 #ifdef PADDLE_WITH_CUDA
       paddle::lite::Place({TARGET(kCUDA), PRECISION(kFloat)}),
 #endif
+      paddle::lite::Place({TARGET(kX86), PRECISION(kFloat)}),
+      paddle::lite::Place({TARGET(kHost), PRECISION(kAny)}),
   };
 
   LOG(INFO) << "Create EngineManager";
@@ -99,7 +124,6 @@ TEST(EngineManager, manual) {
   paddle::lite::Predictor* engine_0 =
       inference::Singleton<inference::lite::EngineManager>::Global().Get(
           unique_key);
-
   CHECK_NOTNULL(engine_0);
   inference::Singleton<inference::lite::EngineManager>::Global().DeleteAll();
   CHECK(inference::Singleton<inference::lite::EngineManager>::Global().Get(
@@ -108,4 +132,5 @@ TEST(EngineManager, manual) {
 }
 
 }  // namespace lite
+}  // namespace inference
 }  // namespace paddle
--- paddle/fluid/operators/lite/ut_helper.h
+++ paddle/fluid/operators/lite/ut_helper.h
@@ -14,6 +14,10 @@
 #pragma once
 
 #include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+
 #include "paddle/fluid/framework/block_desc.h"
 #include "paddle/fluid/framework/lod_tensor.h"
 #include "paddle/fluid/framework/op_registry.h"
@@ -24,6 +28,36 @@ namespace paddle {
 namespace inference {
 namespace lite {
 
+void AddTensorToBlockDesc(framework::proto::BlockDesc* block,
+                          const std::string& name,
+                          const std::vector<int64_t>& shape,
+                          bool persistable = false) {
+  using framework::proto::VarType;
+  auto* var = block->add_vars();
+  framework::VarDesc desc(name);
+  desc.SetType(VarType::LOD_TENSOR);
+  desc.SetDataType(VarType::FP32);
+  desc.SetShape(shape);
+  desc.SetPersistable(persistable);
+  *var = *desc.Proto();
+}
+void serialize_params(std::string* str, framework::Scope* scope,
+                      const std::vector<std::string>& params) {
+  std::ostringstream os;
+#ifdef PADDLE_WITH_CUDA
+  platform::CUDAPlace place;
+  platform::CUDADeviceContext ctx(place);
+#else
+  platform::CPUDeviceContext ctx;
+#endif
+  for (const auto& param : params) {
+    PADDLE_ENFORCE_NOT_NULL(scope->FindVar(param),
+                            "Block should already have a '%s' variable", param);
+    auto* tensor = scope->FindVar(param)->GetMutable<framework::LoDTensor>();
+    framework::SerializeToStream(os, *tensor, ctx);
+  }
+  *str = os.str();
+}
 /*
  * Get a random float value between [low, high]
  */
@@ -33,24 +67,41 @@ float random(float low, float high) {
   std::uniform_real_distribution<double> dist(low, high);
   return dist(mt);
 }
-
 void RandomizeTensor(framework::LoDTensor* tensor,
                      const platform::Place& place) {
   auto dims = tensor->dims();
   size_t num_elements = analysis::AccuDims(dims, dims.size());
   PADDLE_ENFORCE_GT(num_elements, 0);
-
   platform::CPUPlace cpu_place;
   framework::LoDTensor temp_tensor;
   temp_tensor.Resize(dims);
   auto* temp_data = temp_tensor.mutable_data<float>(cpu_place);
-
   for (size_t i = 0; i < num_elements; i++) {
     *(temp_data + i) = random(0., 1.);
+    // LOG(INFO) << "weights: " << *(temp_data + i);
   }
-
   TensorCopySync(temp_tensor, place, tensor);
 }
+
+void CreateTensor(framework::Scope* scope, const std::string& name,
+                  const std::vector<int64_t>& shape, bool in_cuda = true) {
+  auto* var = scope->Var(name);
+  auto* tensor = var->GetMutable<framework::LoDTensor>();
+  auto dims = framework::make_ddim(shape);
+  tensor->Resize(dims);
+  platform::Place place;
+  if (in_cuda) {
+#ifdef PADDLE_WITH_CUDA
+    place = platform::CUDAPlace(0);
+#else
+    LOG(FATAL) << "You must define PADDLE_WITH_CUDA for using CUDAPlace.";
+#endif
+  } else {
+    place = platform::CPUPlace();
+  }
+  RandomizeTensor(tensor, place);
+}
+
 }  // namespace lite
 }  // namespace inference
 }  // namespace paddle
