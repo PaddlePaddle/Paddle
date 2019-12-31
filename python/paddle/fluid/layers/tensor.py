1a29c30aba0f79729b6d0ba8f57ab7467e669c29
mapingshuo <mps2012@yeah.net>
2019-12-31 15:26:58 +0800

    allow VarType as input, test=develop

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -1158,7 +1158,7 @@ def range(start, end, step, dtype):
                                  it is a 1-D Tensor with shape [1].
         step(float32 | float64 | int32 | int64 | Variable): Spacing between values. For any output out, this is the
                                   distance between two adjacent values, out[i+1] - out[i].
-        dtype(str): the data type of the output tensor, can be float32, float64, int32, int64.
+        dtype(str|core.VarDesc.VarType): the data type of the output tensor, can be float32, float64, int32, int64.
 
     Returns: a 1-D Tensor which is evenly spaced values within a given interval. Its data type is set by dtype.
     
@@ -1174,9 +1174,15 @@ def range(start, end, step, dtype):
     """
     helper = LayerHelper("range", **locals())
 
+    check_dtype(dtype, 'create data type',
+                ['float32', 'float64', 'int32', 'int64'], 'range')
+
+    dtype = convert_dtype(dtype)
     if not isinstance(start, Variable):
         start = fill_constant([1], dtype, start)
     elif convert_dtype(start.dtype) != dtype:
+        # make sure that start, end, step has the same dtype as
+        # `dtype`
         start = cast(x=start, dtype=dtype)
 
     if not isinstance(end, Variable):
