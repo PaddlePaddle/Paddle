7e6110d4aa4e2f2095a96b75eaf99518c79d9fff
chengmo <cmchengmo@163.com>
2019-12-31 09:50:56 +0800

    test=develop , fix variable 'p' referenced before assignment bug

--- python/paddle/fluid/clip.py
+++ python/paddle/fluid/clip.py
@@ -532,17 +532,20 @@ def append_gradient_clip_ops(param_grads):
 
     # change wrong mapping relation between param & grad in clip op
     clip_flag = '_clip'
-    for op in p.block.program.global_block().ops:
-        if 'op_namescope' in op.all_attrs() and clip_flag in op.attr(
-                "op_namescope"):
-            if op.attr('op_role_var'):
-                param_name = op.attr('op_role_var')[0]
-                index = 0
-                for i in range(len(res)):
-                    if res[i][0].name == param_name:
-                        index = i
-                correct_p_g = [param_name, res[index][1].name]
-                op._set_attr('op_role_var', correct_p_g)
+    for p, g in param_grads:
+        if g is None:
+            continue
+        for op in p.block.program.global_block().ops:
+            if 'op_namescope' in op.all_attrs() and clip_flag in op.attr(
+                    "op_namescope"):
+                if op.attr('op_role_var'):
+                    param_name = op.attr('op_role_var')[0]
+                    index = 0
+                    for i in range(len(res)):
+                        if res[i][0].name == param_name:
+                            index = i
+                    correct_p_g = [param_name, res[index][1].name]
+                    op._set_attr('op_role_var', correct_p_g)
     return res
 
 
