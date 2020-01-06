0a3e7ca6bc09112518eec5fce77d9e25a0ecd5b1
chengmo <cmchengmo@163.com>
2020-01-06 17:10:23 +0800

    test=develop, fix unittest

--- python/paddle/fluid/tests/unittests/test_dist_fleet_grad_clip.py
+++ python/paddle/fluid/tests/unittests/test_dist_fleet_grad_clip.py
@@ -54,32 +54,6 @@ class TestDistGeoClipByGlobalNormTranspiler(unittest.TestCase):
         pserver_startup_program = fleet.startup_program
         pserver_mian_program = fleet.main_program
 
-    def _setup_config(self):
-        self._sync_mode = False
-        self._geo_sgd = True
-        self._geo_sgd_need_push_nums = 5
-        self._grad_clip_mode = 2
-
-    def check_with_place(self,
-                         model_file,
-                         delta=1e-3,
-                         check_error_log=False,
-                         need_envs={}):
-        required_envs = {
-            "PATH": os.getenv("PATH", ""),
-            "PYTHONPATH": os.getenv("PYTHONPATH", ""),
-            "LD_LIBRARY_PATH": os.getenv("LD_LIBRARY_PATH", ""),
-            "FLAGS_rpc_deadline": "5000",  # 5sec to fail fast
-            "http_proxy": ""
-        }
-        required_envs.update(need_envs)
-
-        tr0_losses, tr1_losses = self._run_cluster(model_file, required_envs)
-
-    def test_dist_train(self):
-        self.check_with_place(
-            "dist_fleet_ctr.py", delta=1e-5, check_error_log=True)
-
 
 class TestDistGeoClipByGlobalNorm(TestFleetBase):
     def _setup_config(self):
