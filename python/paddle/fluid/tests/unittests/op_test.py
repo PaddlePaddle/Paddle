170d7b5881ca66803741937549079d8d0dbb5781
HappyAngel <chenjiaobuaa@126.com>
2020-01-08 12:21:25 +0800

    fix conflict, test=develop

--- python/paddle/fluid/tests/unittests/op_test.py
+++ python/paddle/fluid/tests/unittests/op_test.py
@@ -33,7 +33,7 @@ from paddle.fluid.executor import Executor
 from paddle.fluid.framework import Program, OpProtoHolder, Variable
 from testsuite import create_op, set_input, append_input_output, append_loss_ops
 from paddle.fluid import unique_name
-from white_list import op_accuracy_white_list, check_shape_white_list, compile_vs_runtime_white_list, no_check_set_white_list, no_grad_set_white_
+from white_list import op_accuracy_white_list, check_shape_white_list, compile_vs_runtime_white_list, no_check_set_white_list, no_grad_set_white_list
 from white_list import op_threshold_white_list
 
 
