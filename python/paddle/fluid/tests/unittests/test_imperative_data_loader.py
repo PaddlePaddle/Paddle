62ddf0dc9987f2a2961a74927290b11e68cc1bcc
Chen Weihang <chenweihang@baidu.com>
2020-01-10 03:48:07 +0000

    split normal and exception unittests to diff class, test=develop

--- python/paddle/fluid/tests/unittests/test_imperative_data_loader.py
+++ python/paddle/fluid/tests/unittests/test_imperative_data_loader.py
@@ -19,11 +19,6 @@ import paddle.fluid as fluid
 from paddle.fluid import core
 import paddle.compat as cpt
 
-if sys.version_info[0] == 2:
-    import Queue as queue
-else:
-    import queue
-
 
 def get_random_images_and_labels(image_shape, label_shape):
     image = np.random.random(size=image_shape).astype('float32')
@@ -36,6 +31,19 @@ def sample_generator_creator(batch_size, batch_num):
         for _ in range(batch_num * batch_size):
             image, label = get_random_images_and_labels([784], [1])
             yield image, label
+
+    return __reader__
+
+
+def sample_list_generator_creator(batch_size, batch_num):
+    def __reader__():
+        for _ in range(batch_num):
+            sample_list = []
+            for _ in range(batch_size):
+                image, label = get_random_images_and_labels([784], [1])
+                sample_list.append([image, label])
+
+            yield sample_list
 
     return __reader__
 
@@ -86,8 +94,18 @@ class TestDygraphhDataLoader(unittest.TestCase):
                     self.assertEqual(label.shape, [self.batch_size, 1])
                     self.assertEqual(relu.shape, [self.batch_size, 784])
 
-    # NOTE: sample_list_generator already has multiple unittests in dygraph mode.
-    # Such as test_imperative_mnist/resnet/se_resnext.
+    def test_sample_list_generator(self):
+        with fluid.dygraph.guard():
+            loader = fluid.io.DataLoader.from_generator(capacity=self.capacity)
+            loader.set_sample_list_generator(
+                sample_list_generator_creator(self.batch_size, self.batch_num),
+                places=fluid.CPUPlace())
+            for _ in range(self.epoch_num):
+                for image, label in loader():
+                    relu = fluid.layers.relu(image)
+                    self.assertEqual(image.shape, [self.batch_size, 784])
+                    self.assertEqual(label.shape, [self.batch_size, 1])
+                    self.assertEqual(relu.shape, [self.batch_size, 784])
 
     def test_batch_genarator(self):
         with fluid.dygraph.guard():
@@ -102,19 +120,17 @@ class TestDygraphhDataLoader(unittest.TestCase):
                     self.assertEqual(label.shape, [self.batch_size, 1])
                     self.assertEqual(relu.shape, [self.batch_size, 784])
 
-    # NOTE: coverage CI can't cover child process code, so need this test.
-    # It test child process loop function in main process
-    def test_reader_process_loop(self):
+
+class TestDygraphhDataLoaderWithException(unittest.TestCase):
+    def setUp(self):
+        self.batch_num = 4
+        self.capacity = 2
+
+    def test_not_capacity(self):
         with fluid.dygraph.guard():
-            loader = fluid.io.DataLoader.from_generator(
-                capacity=self.batch_num + 1)
-            loader.set_batch_generator(
-                batch_generator_creator(self.batch_size, self.batch_num),
-                places=fluid.CPUPlace())
-            loader._data_queue = queue.Queue(self.batch_num + 1)
-            loader._reader_process_loop()
-            for _ in range(self.batch_num):
-                loader._data_queue.get(timeout=10)
+            with self.assertRaisesRegexp(ValueError,
+                                         "Please give value to capacity."):
+                fluid.io.DataLoader.from_generator()
 
     def test_single_process_with_thread_expection(self):
         def error_sample_genarator(batch_num):
