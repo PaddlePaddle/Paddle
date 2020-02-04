be528bf2b34349e9c87673d21ee0717661a083a9
xujiaqi01 <173596896@qq.com>
2020-02-04 20:46:51 +0800

    add collective communication library in fleet (#22211) (#22435)
    * add collective communication library in fleet to replace mpi
    * test=develop

--- paddle/fluid/framework/fleet/fleet_wrapper.cc
+++ paddle/fluid/framework/fleet/fleet_wrapper.cc
@@ -107,6 +107,16 @@ uint64_t FleetWrapper::RunServer() {
 #endif
 }
 
+uint64_t FleetWrapper::RunServer(const std::string& ip, uint32_t port) {
+#ifdef PADDLE_WITH_PSLIB
+  VLOG(3) << "Going to run server with ip " << ip << " port " << port;
+  auto ret = pslib_ptr_->run_server(ip, port);
+  return ret;
+#else
+  return 0;
+#endif
+}
+
 void FleetWrapper::GatherServers(const std::vector<uint64_t>& host_sign_list,
                                  int node_num) {
 #ifdef PADDLE_WITH_PSLIB
