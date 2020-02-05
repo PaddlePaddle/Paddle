7bc4b095008058fdde05e3e9337e31845f1ce9b5
Wilber <jiweibo@baidu.com>
2020-02-05 14:01:10 +0800

    add WITH_NCCL option for cmake. (#22384)
    cmake选项中添加了WITH_NCCL，显示指定是否编译NCCL的部分代码，WITH_NCCL默认打开，但如果WITH_GPU为OFF，则关闭WITH_NCCL
    
    添加了PADDLE_WITH_NCCL定义
    
    单机单卡能够关闭NCCL编译，多卡的话需要默认打开NCCL，如果关闭NCCL，则只能使用单卡
    
    Co-authored-by: 石晓伟 <39303645+Shixiaowei02@users.noreply.github.com>

--- paddle/fluid/framework/device_worker_factory.cc
+++ paddle/fluid/framework/device_worker_factory.cc
@@ -61,7 +61,7 @@ std::shared_ptr<DeviceWorker> DeviceWorkerFactory::CreateDeviceWorker(
 
 REGISTER_DEVICE_WORKER_CLASS(HogwildWorker);
 REGISTER_DEVICE_WORKER_CLASS(DownpourWorker);
-#if defined(PADDLE_WITH_CUDA) && !defined(_WIN32)
+#if defined(PADDLE_WITH_NCCL)
 REGISTER_DEVICE_WORKER_CLASS(SectionWorker);
 #endif
 }  // namespace framework
