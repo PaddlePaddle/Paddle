fb98116cd7ceb22021a3fd2c2cf9bdf3d1d3708d
Wilber <jiweibo@baidu.com>
2020-02-05 17:22:29 +0800

    cherry-pick 22384 and 22371. test=develop test=release/1.7 (#22453)
    [cherry-pick] #22384 and #22371
    
    22384增加了WITH_NCCL开关
    
    22371修改了fluid依赖lite的commit id

--- cmake/external/lite.cmake
+++ cmake/external/lite.cmake
@@ -43,7 +43,7 @@ if (NOT LITE_SOURCE_DIR OR NOT LITE_BINARY_DIR)
       ${LITE_PROJECT}
       ${EXTERNAL_PROJECT_LOG_ARGS}
       GIT_REPOSITORY      "https://github.com/PaddlePaddle/Paddle-Lite.git"
-      GIT_TAG             d8143103f9fb63dd362d7b0d72ca3c0ac6e10f72
+      GIT_TAG             922ace19a45f30075618f71428523e7a2d5898d6
       PREFIX              ${LITE_SOURCES_DIR}
       UPDATE_COMMAND      ""
       BUILD_COMMAND       ${LITE_BUILD_COMMAND}
