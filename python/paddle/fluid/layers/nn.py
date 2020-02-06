f45744043bba6c6fe1488bfbef7ed60a03dc6a0e
FDInSky <fdinsky@qq.com>
2020-02-06 11:28:42 +0800

    test=develop fix some errors

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -6275,7 +6275,7 @@ def label_smooth(label,
 @templatedoc()
 def roi_pool(input,
              rois,
-             rois_lod=[],
+             rois_lod=None,
              pooled_height=1,
              pooled_width=1,
              spatial_scale=1.0):
@@ -6294,7 +6294,7 @@ def roi_pool(input,
     Args:
         input (Variable): Input feature, 4D-Tensor with the shape of [N,C,H,W], where N is the batch size, C is the input channel, H is Height, W is weight. The data type is float32 or float64.
         rois (Variable): ROIs (Regions of Interest) to pool over. 2D-LoDTensor with the shape of [num_rois,4], the lod level is 1. Given as [[x1, y1, x2, y2], ...], (x1, y1) is the top left coordinates, and (x2, y2) is the bottom right coordinates.
-        rois_lod(List): the rois lod info that is the number of rois of every batch. Default: empty list 
+        rois_lod(Variable): the lod info of rois. Default: None 
         pooled_height (int, optional): The pooled output height, data type is int32. Default: 1
         pooled_width (int, optional): The pooled output height, data type is int32. Default: 1
         spatial_scale (float, optional): Multiplicative spatial scale factor to translate ROI coords from their input scale to the scale used when pooling. Default: 1.0
@@ -6320,10 +6320,11 @@ def roi_pool(input,
     
         x = fluid.data(name='input', shape=[None,1,4,4], dtype=DATATYPE)
         rois = fluid.data(name='roi', shape=[None,4], dtype=DATATYPE)
-    
+        rois_lod = fluid.data(name="rois_lod", shape=[None, ], dtype='int32') 
         pool_out = fluid.layers.roi_pool(
                 input=x,
                 rois=rois,
+                rois_lod=rois_lod,
                 pooled_height=1,
                 pooled_width=1,
                 spatial_scale=1.0)
@@ -6355,7 +6356,7 @@ def roi_pool(input,
 @templatedoc()
 def roi_align(input,
               rois,
-              rois_lod=[],
+              rois_lod=None,
               pooled_height=1,
               pooled_width=1,
               spatial_scale=1.0,
@@ -6371,7 +6372,7 @@ def roi_align(input,
             data type is float32 or float64. Given as [[x1, y1, x2, y2], ...], 
             (x1, y1) is the top left coordinates, and (x2, y2) is the bottom
             right coordinates. 
-        rois_lod(List): the rois lod info that is the number of rois of every batch. Default: empty list 
+        rois_lod(Variable): the lod info of rois. Default: None 
         pooled_height (int32, optional): ${pooled_height_comment} Default: 1
         pooled_width (int32, optional): ${pooled_width_comment} Default: 1
         spatial_scale (float32, optional): ${spatial_scale_comment} Default: 1.0
@@ -6394,8 +6395,10 @@ def roi_align(input,
                 name='data', shape=[None, 256, 32, 32], dtype='float32')
             rois = fluid.data(
                 name='rois', shape=[None, 4], dtype='float32')
+            rois_lod = fluid.data(name="rois_lod", shape=[None, ], dtype='int32') 
             align_out = fluid.layers.roi_align(input=x,
                                                rois=rois,
+                                               rois_lod=rois_lod,
                                                pooled_height=7,
                                                pooled_width=7,
                                                spatial_scale=0.5,
