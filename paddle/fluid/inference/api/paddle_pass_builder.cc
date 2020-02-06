e01dc974c0835e6374b462d4aaa0b509564e8f08
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2020-02-06 02:33:15 +0000

    disable_fc_gru_fusion, test=develop

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -153,9 +153,9 @@ CpuPassStrategy::CpuPassStrategy() : PassStrategy({}) {
                   // "seqpool_concat_fuse_pass",    //
                   "seqpool_cvm_concat_fuse_pass",  //
                   // "embedding_fc_lstm_fuse_pass", //
-                  "fc_lstm_fuse_pass",                       //
-                  "mul_lstm_fuse_pass",                      //
-                  "fc_gru_fuse_pass",                        //
+                  "fc_lstm_fuse_pass",   //
+                  "mul_lstm_fuse_pass",  //
+                  // "fc_gru_fuse_pass",                     //
                   "mul_gru_fuse_pass",                       //
                   "seq_concat_fc_fuse_pass",                 //
                   "fc_fuse_pass",                            //
