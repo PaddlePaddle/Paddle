4e29397fe95b0f94dd98ec620089b697eef0d494
zhiqiu <chenqiuliang@baidu.com>
2020-02-07 12:28:54 +0800

    add unittest for prune, test=develop

--- python/paddle/fluid/tests/unittests/test_prune.py
+++ python/paddle/fluid/tests/unittests/test_prune.py
@@ -96,5 +96,40 @@ class TestPrune(unittest.TestCase):
                 cpt.get_exception_message(e))
 
 
+class TestExecutorRunAutoPrune(unittest.TestCase):
+    def net1(self):
+        x = fluid.layers.data(name='x', shape=[2], dtype='float32')
+        label = fluid.layers.data(name="label", shape=[1], dtype="int64")
+        y = fluid.layers.fc(input=[x], size=2, act="softmax")
+        loss1 = fluid.layers.cross_entropy(input=y, label=label)
+        loss1 = fluid.layers.mean(x=loss1)
+        loss2 = fluid.layers.square_error_cost(input=y, label=label)
+        loss2 = fluid.layers.mean(x=loss2)
+        #sgd_optimizer = fluid.optimizer.SGD(learning_rate=0.05)
+        #sgd_optimizer.minimize(loss1)
+        return x, y, label, loss1, loss2
+
+    def test_prune_by_fetches_without_optimize():
+        """
+        Prune operators and operators which are not needed to generate 'fetches'. 
+        In train mode, the operators and operators in backward and optimization should be kept.
+        """
+        program = framework.Program()
+        startup_program = framework.Program()
+        block = program.global_block()
+        with fluid.program_guard(program, startup_program):
+            (x, y, label, loss1, loss2) = self.net()
+            exe = fluid.Executor(fluid.CPUPlace())
+            exe.run(startup_program)
+            x_np = np.random.randint(5, size=(10, 2))
+            label_np = np.random.randint(1, size=(10, 1))
+            exe.run(program,
+                    feed={'x': x_np,
+                          'label': label_np},
+                    fetch_list=[loss1.name],
+                    use_prune=True)
+            ## checkout loss2 not calculated
+
+
 if __name__ == '__main__':
     unittest.main()
