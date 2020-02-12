ab4da8de63274371ed4900dbe601ea6710e9fcb0
wangjiawei04 <wangjiawei04@baidu.com>
2020-02-12 03:04:48 +0000

    Add TopK Op Grad, modify grad op maker test=develop

--- paddle/fluid/operators/top_k_op.cc
+++ paddle/fluid/operators/top_k_op.cc
@@ -14,6 +14,7 @@ limitations under the License. */
 
 #include "paddle/fluid/operators/top_k_op.h"
 #include <memory>
+
 namespace paddle {
 namespace operators {
 
@@ -110,13 +111,13 @@ class TopkOpGrad : public framework::OperatorWithKernel {
 };
 
 template <typename T>
-class TopkGradOpMaker : public framework::SingleGradOpDescMaker<T> {
+class TopkGradOpMaker : public framework::SingleGradOpMaker<T> {
  public:
-  using framework::SingleGradOpDescMaker::SingleGradOpDescMaker;
+  using framework::SingleGradOpMaker<T>::SingleGradOpMaker;
 
  protected:
-  std::unique_ptr<framework::OpDesc> Apply() const override {
-    std::unique_ptr<framework::OpDesc> op(new framework::OpDesc());
+  std::unique_ptr<T> Apply() const override {
+    std::unique_ptr<T> op(new T());
     op->SetType("top_k_grad");
     op->SetInput(framework::GradVarName("Out"), this->OutputGrad("Out"));
     op->SetInput("X", this->Input("X"));
