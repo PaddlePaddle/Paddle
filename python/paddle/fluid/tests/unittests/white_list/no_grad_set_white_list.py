c43e02a500625abf0f164aa5eee99c6fbac2a6f2
HappyAngel <chenjiaobuaa@126.com>
2020-02-12 18:52:24 +0800

    add not_check list, test=develop

--- python/paddle/fluid/tests/unittests/white_list/no_grad_set_white_list.py
+++ python/paddle/fluid/tests/unittests/white_list/no_grad_set_white_list.py
@@ -13,41 +13,25 @@
 # limitations under the License.
 
 # check no_grad_set is None
-NOT_CHECK_OP_LIST = []
-# TODO(Shixiaowei02): Check if the items do not need fix.
-# no_grad_set has value in NEED_TO_FIX_OP_LIST
-# yapf: disable
-NEED_TO_FIX_OP_LIST = [
+NOT_CHECK_OP_LIST = [
     'affine_channel',
     'affine_grid',
-    'backward',
     'batch_norm',
     'conv_shift',
-    'conv2d',
-    'conv2d_transpose',
-    'conv3d',
-    'conv3d_transpose',
     'cos_sim',
     'cross_entropy',
-    'cross_entropy2',
     'data_norm',
     'deformable_conv',
     'deformable_conv_v1',
-    'depthwise_conv2d',
-    'depthwise_conv2d_transpose',
     'elementwise_add',
     'elementwise_div',
     'elementwise_max',
     'elementwise_min',
     'elementwise_mul',
     'elementwise_sub',
-    'filter_by_instag',
     'fused_elemwise_activation',
     'fused_emb_seq_pool',
     'fused_embedding_seq_pool',
-    'gru_unit',
-    'hierarchical_sigmoid',
-    'hsigmoid',
     'huber_loss',
     'instance_norm',
     'kldiv_loss',
@@ -61,11 +45,28 @@ NEED_TO_FIX_OP_LIST = [
     'matmul',
     'mul',
     'multiplex',
-    'prelu',
     'rank_loss',
-    'row_conv',
-    'sequence_conv',
     'smooth_l1_loss',
     'spectral_norm'
+]
+# TODO(Shixiaowei02): Check if the items do not need fix.
+# no_grad_set has value in NEED_TO_FIX_OP_LIST
+# yapf: disable
+NEED_TO_FIX_OP_LIST = [
+    'backward',
+    'conv2d',
+    'conv2d_transpose',
+    'conv3d',
+    'conv3d_transpose',
+    'cross_entropy2',
+    'depthwise_conv2d',
+    'depthwise_conv2d_transpose',
+    'filter_by_instag',
+    'gru_unit',
+    'hierarchical_sigmoid',
+    'hsigmoid',
+    'prelu',
+    'row_conv',
+    'sequence_conv'
 ]
 # yapf: enable
