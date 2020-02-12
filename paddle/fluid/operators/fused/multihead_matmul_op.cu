720df627dfff2488f96f9786260ed8eab4bf218f
nhzlx <nhzlx.dragon@gmail.com>
2020-02-12 08:40:14 +0000

    fix comments test=develop

--- paddle/fluid/operators/fused/multihead_matmul_op.cu
+++ paddle/fluid/operators/fused/multihead_matmul_op.cu
@@ -330,9 +330,19 @@ __device__ float2 add_func<float2>(float2 a, float2 b) {
   return c;
 }
 
+template <>
+__device__ float4 add_func<float4>(float4 a, float4 b) {
+  float4 c;
+  c.x = a.x + b.x;
+  c.y = a.y + b.y;
+  c.z = a.z + b.z;
+  c.w = a.w + b.w;
+  return c;
+}
+
 template <typename T>
-__global__ void transpose_qkv_kernel(const int H /*head_size*/, const T *input,
-                                     const T *bias, T *output) {
+__global__ void transpose_qkv_kernel(const int H, const T *input, const T *bias,
+                                     T *output) {
   // Input: BxSx3xNxH
   // Bias: 3xSxB
   // Output: 3xBxNxSxH
@@ -342,9 +352,9 @@ __global__ void transpose_qkv_kernel(const int H /*head_size*/, const T *input,
   int m = blockIdx.z;
 
   const int N = blockDim.y;
-
   const int S = gridDim.x;
   const int B = gridDim.y;
+
   const int NH = N * H;
   const int NHS = NH * S;
   const int in_offset = n * H + m * NH + s * 3 * NH + b * NHS * 3;
@@ -352,10 +362,8 @@ __global__ void transpose_qkv_kernel(const int H /*head_size*/, const T *input,
   const int out_offset = s * H + n * S * H + b * NHS + m * NHS * B;
 
   const int i = threadIdx.x;
-  if (i < H) {
-    output[out_offset + i] =
-        add_func(input[in_offset + i], bias[bias_offset + i]);
-  }
+  output[out_offset + i] =
+      add_func(input[in_offset + i], bias[bias_offset + i]);
 }
 
 void TransQKVWithBias(const int batch, const int seq_len, const int head_size,
@@ -363,16 +371,40 @@ void TransQKVWithBias(const int batch, const int seq_len, const int head_size,
                       float *output, cudaStream_t stream) {
   // BxSx3xNxH + 3xNxH -> 3xBxNxSxH
   const dim3 grid(seq_len, batch, 3);
-  if (head_size % 2 == 0) {
+  if (head_size % 4 == 0) {
+    const int h = head_size / 4;
+    const float4 *input4 = reinterpret_cast<const float4 *>(input);
+    const float4 *bias4 = reinterpret_cast<const float4 *>(bias);
+    float4 *output4 = reinterpret_cast<float4 *>(output);
+    const dim3 block(h, head_num, 1);
+
+    // limit h * head_num to max block size(1024).
+    PADDLE_ENFORCE_LE(h * head_num, 1024,
+                      platform::errors::InvalidArgument(
+                          "head_num (%d) * head_size (%d) should <= %d",
+                          head_num, head_size, 1024 * 4));
+    transpose_qkv_kernel<float4><<<grid, block, 0, stream>>>(h, input4, bias4,
+                                                             output4);
+  } else if (head_size % 2 == 0) {
     const int h = head_size / 2;
     const float2 *input2 = reinterpret_cast<const float2 *>(input);
     const float2 *bias2 = reinterpret_cast<const float2 *>(bias);
     float2 *output2 = reinterpret_cast<float2 *>(output);
     const dim3 block(h, head_num, 1);
+    // limit h * head_num to max block size(1024).
+    PADDLE_ENFORCE_LE(h * head_num, 1024,
+                      platform::errors::InvalidArgument(
+                          "head_num (%d) * head_size (%d) should <= %d",
+                          head_num, head_size, 1024 * 2));
     transpose_qkv_kernel<float2><<<grid, block, 0, stream>>>(h, input2, bias2,
                                                              output2);
   } else {
     const dim3 block(head_size, head_num, 1);
+    // limit head_size * head_num to max block size(1024).
+    PADDLE_ENFORCE_LE(head_size * head_num, 1024,
+                      platform::errors::InvalidArgument(
+                          "head_num (%d) * head_size (%d) should <= %d",
+                          head_num, head_size, 1024));
     transpose_qkv_kernel<float><<<grid, block, 0, stream>>>(head_size, input,
                                                             bias, output);
   }
