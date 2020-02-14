3eaebaa80e76a371de8d6e2f6c09221903af15de
HappyAngel <chenjiaobuaa@126.com>
2020-02-14 16:28:59 +0800

    fix format， test=develop

--- python/paddle/fluid/tests/unittests/white_list/no_grad_set_white_list.py
+++ python/paddle/fluid/tests/unittests/white_list/no_grad_set_white_list.py
@@ -13,9 +13,8 @@
 # limitations under the License.
 
 # check no_grad_set is None
-NOT_CHECK_OP_LIST = [
-    'deformable_conv'
-]
+NOT_CHECK_OP_LIST = ['deformable_conv']
+
 # TODO(Shixiaowei02): Check if the items do not need fix.
 # no_grad_set has value in NEED_TO_FIX_OP_LIST
 # yapf: disable
