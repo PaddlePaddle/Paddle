c60ff2c2de4ed4c0b70600218c0be34c5f2ddabf
gongweibao <weibao.gong@gmail.com>
2020-02-23 10:18:12 +0000

    add job server

--- python/paddle/fluid/tests/unittests/job_server_demo.py
+++ python/paddle/fluid/tests/unittests/job_server_demo.py
@@ -3,8 +3,13 @@ from flask import Flask, jsonify, abort, request, make_response, url_for
 from contextlib import closing
 import socket
 from flask import request
+import random
+
+random.seed(10)
 
 app = Flask(__name__, static_url_path="")
+
+job_manager = JobInfoManager()
 
 
 @app.errorhandler(400)
@@ -17,22 +22,53 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-def _get_job_pods(job_id):
-    job = {"job_id": "test_job_id_1234", "pods": []}
+class JobInfoManager(object):
+    def __init__(self):
+        self.job["test_job_id_1234"] = self._init_job_pods()
+        self._t_id = None
+        self._lock = threading.Lock()
 
-    pods = []
-    port = 7070
-    for i in range(0, 16, 2):
-        pod = {}
-        pod["pod_id"] = i / 2
-        pod["running"] = True
-        pod["addr"] = "127.0.0.1"
-        pod["pod_port"] = port + i
-        pod["trainer_ports"] = [port + i + 1]
-        pods.append(pod)
+    def _init_job_pods(self):
+        #job = {"job_id": "test_job_id_1234", "pods": []}
+        pods = []
+        port = 7070
+        for i in range(0, 16, 2):
+            pod = {}
+            pod["pod_id"] = i / 2
+            pod["running"] = True
+            pod["addr"] = "127.0.0.1"
+            pod["pod_port"] = port + i
+            pod["trainer_ports"] = [port + i + 1]
+            pods.append(pod)
+        #job["pods"] = pods
+        return pods
 
-    job["pods"] = pods
-    return job
+    def start(self):
+        assert self._t_id is None, "thread has been started"
+
+        thread = Thread(target=self.run)
+        thread.start()
+        return str(thread)
+
+    def get_job_pods(self, job_id):
+        with self._lock:
+            return self.jobs[job_id]
+
+    def del_tail(self, pods_num):
+        with self._lock:
+            pods = self.job["pods"]
+            assert pods_num < len(pods), "can't delete pods_num:%d".format(
+                pods_num)
+            #return pods[:-pods_num]
+
+    def add_tail(self, pods_num):
+        pass
+
+    def run(self):
+        while (True):
+            with self._lock:
+                generate_job_pods()
+            time.sleep(300)
 
 
 @app.route('/rest/1.0/get/query_pods', methods=['GET'])
@@ -42,9 +78,9 @@ def get_job_pods():
         print("job_id:", job_id)
     except:
         return jsonify({'job_id': {}})
-    job_pods = _get_job_pods(job_id)
+    job_pods = job_manager.get_job_pods(job_id)
     print("job_pods:", job_pods)
-    return jsonify({'job_id': job_pods})
+    return jsonify({'job_id': "job_id", "pods": job_pods})
 
 
 @app.route('/rest/1.0/post/job_runtime_static', methods=['POST'])
@@ -55,4 +91,5 @@ def update_job_static():
 
 
 if __name__ == '__main__':
+    job_manager.start()
     app.run(host='0.0.0.0', port=8180)
