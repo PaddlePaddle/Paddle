a794861b3f30008ca06d3fd81983ec45c598d36d
tangwei <tangwei12@baidu.com>
2020-02-28 23:48:40 +0800

    fix bug for lib, test=develop

--- python/paddle/fluid/tests/unittests/test_communicator_sync.py
+++ python/paddle/fluid/tests/unittests/test_communicator_sync.py
@@ -25,7 +25,7 @@ from paddle.fluid.communicator import Communicator
 
 import paddle.fluid.incubate.fleet.base.role_maker as role_maker
 from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler import fleet
-from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler.distributed_strategy import StrategyFactory
+from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler import StrategyFactory
 
 
 class TestCommunicator(unittest.TestCase):
