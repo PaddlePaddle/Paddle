f5e63bb618fd752d7276c93764aa5bb76777ee15
WangXi <wangxi16@baidu.com>
2020-02-28 10:09:01 +0000

    Split fp16 compression into two for loops, test=develop

--- python/paddle/fluid/contrib/compression/fp16_compressor.py
+++ python/paddle/fluid/contrib/compression/fp16_compressor.py
@@ -19,6 +19,7 @@ from ... import default_main_program
 from ... import framework
 from ... import layers
 from ... import optimizer
+from ... import unique_name
 
 __all__ = ["fp16_compression"]
 
@@ -40,18 +41,23 @@ class FP16Compressor(optimizer.Optimizer):
         Compress fp32 gradients to fp16 during allreduce.
         """
         main_program = default_main_program()
-        block = main_program.global_block()
         op_maker = core.op_proto_and_checker_maker
 
+        new_param_and_grads = []  # param, grad, is_cast
+        # cast grad from fp32->fp16 before allreduce,
         for param, grad in param_and_grads:
             if grad is None or grad.dtype != core.VarDesc.VarType.FP32:
+                new_param_and_grads.append((param, grad, False))
                 continue
 
             op = grad.op
+            block = grad.block
             var_attr = op.all_attrs()[op_maker.kOpRoleVarAttrName()]
             if param.name not in var_attr:
+                new_param_and_grads.append((param, grad, False))
                 continue
 
+            # remove (param, grad) from op_role_var
             var_attr.remove(param.name)
             var_attr.remove(grad.name)
             if len(var_attr) > 1:
@@ -59,20 +65,17 @@ class FP16Compressor(optimizer.Optimizer):
             else:
                 op._remove_attr(op_maker.kOpRoleVarAttrName())
 
-            cast_name = grad.name + '.cast_fp16'
-            out_var = block.vars.get(cast_name)
-            if out_var is None or out_var.dtype != core.VarDesc.VarType.FP16:
-                out_var = block.create_var(
-                    name=cast_name,
-                    dtype=core.VarDesc.VarType.FP16,
-                    persistable=False)
+            new_grad = block.create_var(
+                name=unique_name.generate(grad.name + ".cast_fp16"),
+                dtype=core.VarDesc.VarType.FP16,
+                persistable=False,
+                stop_gradient=True)
 
-            # cast grad from fp32->fp16 before allreduce,
             with block.program._backward_role_guard():
                 cast_op = block.append_op(
                     type="cast",
                     inputs={"X": grad},
-                    outputs={"Out": out_var},
+                    outputs={"Out": new_grad},
                     attrs={
                         "in_dtype": core.VarDesc.VarType.FP32,
                         "out_dtype": core.VarDesc.VarType.FP16
@@ -82,27 +85,49 @@ class FP16Compressor(optimizer.Optimizer):
                 backward = op_maker.OpRole.Backward
                 cast_op._set_attr(op_maker.kOpRoleAttrName(), backward)
                 cast_op._set_attr(op_maker.kOpRoleVarAttrName(),
-                                  [param.name, out_var.name])
+                                  [param.name, new_grad.name])
+                new_grad.op = cast_op
 
-            # cast grad from fp16->fp32 after allreduce.
-            with param.block.program._optimized_guard(
+            new_param_and_grads.append((param, new_grad, True))
+
+        ret_param_and_grads = []
+        # cast grad from fp16->fp32 after allreduce.
+        for param, grad, cast in new_param_and_grads:
+            if not cast:
+                ret_param_and_grads.append((param, grad))
+                continue
+
+            block = grad.block
+            new_grad = block.create_var(
+                name=unique_name.generate(grad.name + ".cast_fp32"),
+                dtype=core.VarDesc.VarType.FP32,
+                persistable=False,
+                stop_gradient=True)
+
+            with block.program._optimized_guard(
                 [param, grad]), framework.name_scope('fp16_compressor'):
                 cast_op = block.append_op(
                     type="cast",
-                    inputs={"X": out_var},
-                    outputs={"Out": grad},
+                    inputs={"X": grad},
+                    outputs={"Out": new_grad},
                     attrs={
                         "in_dtype": core.VarDesc.VarType.FP16,
                         "out_dtype": core.VarDesc.VarType.FP32
                     },
                     stop_gradient=True)
+            ret_param_and_grads.append((param, new_grad))
+
+        return ret_param_and_grads
 
     def backward(self, **kargs):
         return self._optimizer.backward(**kargs)
 
-    def apply_optimize(self, **kargs):
-        self._append_cast_ops(kargs['params_grads'])
-        return self._optimizer.apply_optimize(**kargs)
+    def apply_optimize(self, loss, startup_program, params_grads):
+        new_params_grads = self._append_cast_ops(params_grads)
+        return self._optimizer.apply_optimize(
+            loss,
+            startup_program=startup_program,
+            params_grads=new_params_grads)
 
     def minimize(self,
                  loss,
