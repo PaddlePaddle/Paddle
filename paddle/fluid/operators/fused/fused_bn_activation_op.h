c9120dbbc18095498c36822615dc46cfdc774cbd
liuwei1031 <liuwei12@baidu.com>
2020-02-28 02:06:36 +0000

    fix error of fused_bn_activation_op, test=develop

--- paddle/fluid/operators/fused/fused_bn_activation_op.h
+++ paddle/fluid/operators/fused/fused_bn_activation_op.h
@@ -85,9 +85,9 @@ class FusedBatchNormActGradOpMaker : public framework::SingleGradOpMaker<T> {
 class FusedBatchNormActOpInferVarType
     : public framework::PassInDtypeAndVarTypeToOutput {
  protected:
-  std::unordered_map<std::string, std::string> GetInputOutputWithSameType()
+  std::unordered_map<std::string, std::string>& GetInputOutputWithSameType()
       const override {
-    std::unordered_map<std::string, std::string> m{{"X", /*->*/ "Y"}};
+    static std::unordered_map<std::string, std::string> m{{"X", /*->*/ "Y"}};
     return m;
   }
 };
