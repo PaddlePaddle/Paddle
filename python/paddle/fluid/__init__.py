e551076aa249894a3fbe92cefcf1810bb75cffc5
Zeng Jinle <32832641+sneaxiy@users.noreply.github.com>
2020-03-04 01:34:20 -0600

    Add flags to limit gpu memory (#22793)
    * add recorded cuda memory apis, fix typo, test=develop
    
    * add more ut, test=develop
    
    * follow comments, test=develop
    
    * fix py35 incompatible issues, test=develop

--- python/paddle/fluid/__init__.py
+++ python/paddle/fluid/__init__.py
@@ -214,7 +214,7 @@ def __bootstrap__():
             'enable_cublas_tensor_op_math', 'conv_workspace_size_limit',
             'cudnn_exhaustive_search', 'selected_gpus', 'sync_nccl_allreduce',
             'cudnn_batchnorm_spatial_persistent', 'gpu_allocator_retry_time',
-            'local_exe_sub_scope_limit'
+            'local_exe_sub_scope_limit', 'gpu_memory_limit_mb'
         ]
     core.init_gflags([sys.argv[0]] +
                      ["--tryfromenv=" + ",".join(read_env_flags)])
