5d634154b5d9cab90af8f619e774a246c03ab17a
liuwei1031 <liuwei12@baidu.com>
2020-03-04 07:29:11 +0000

    reduce default attrs for dynamic graph, test=develop

--- paddle/fluid/framework/attribute.h
+++ paddle/fluid/framework/attribute.h
@@ -339,9 +339,11 @@ class OpAttrChecker {
     return *(checker.target<TypedAttrChecker<T>>());
   }
 
-  void Check(AttributeMap* attr_map) const {
-    for (const auto& checker : attr_checkers_) {
-      checker(attr_map, false);
+  void Check(AttributeMap* attr_map, bool explicit_only = false) const {
+    auto checker_num = attr_checkers_.size();
+    if (explicit_only) checker_num = explicit_checker_num_;
+    for (size_t i = 0; i < checker_num; ++i) {
+      attr_checkers_[i](attr_map, false);
     }
   }
 
@@ -353,8 +355,13 @@ class OpAttrChecker {
     return default_values_map;
   }
 
+  void RecordExplicitCheckerNum() {
+    explicit_checker_num_ = attr_checkers_.size();
+  }
+
  private:
   std::vector<AttrChecker> attr_checkers_;
+  size_t explicit_checker_num_;
 };
 
 }  // namespace framework
