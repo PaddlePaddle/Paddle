2955e88c0c14b3f202a87fd5cb57909e7da14903
chengmo <cmchengmo@163.com>
2020-03-05 13:53:33 +0800

    fix index bug

--- paddle/fluid/operators/find_by_index_op.h
+++ paddle/fluid/operators/find_by_index_op.h
@@ -56,7 +56,7 @@ class FindByIndexKernel : public framework::OpKernel<T> {
     auto *input_data = input_tensor.data<T>();
     auto *index_data = index_tensor.data<int64_t>();
     for (int i = 0; i < index_ids_num; i++) {
-      int b = floor(i / batch_size);
+      int b = floor(i / index_length);
       int v_i = b * batch_size + static_cast<int>(index_data[i]);
       T v = input_data[v_i];
       VLOG(1) << "Find by index: b= " << b << " v_i= " << v_i << " v= " << v;
