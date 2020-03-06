a00aa87095d7fde823e6d8c7ad064447f39c606d
Huihuang Zheng <zhhsplendid@gmail.com>
2020-03-06 10:05:22 +0800

    Fix NameVisitor bugs (#22847)
    1. copy.deepcopy in NameVisitor should be changed to copy.copy to make hash or set work
    2. read_context should be type of gast.Load()/gast.AugLoad(), not gast.Load/gast.AugLoad

--- python/paddle/fluid/dygraph/dygraph_to_static/loop_transformer.py
+++ python/paddle/fluid/dygraph/dygraph_to_static/loop_transformer.py
@@ -80,11 +80,11 @@ class NameVisitor(gast.NodeVisitor):
         return True
 
     def get_loop_var_names(self, node):
-        assert isinstance(node, gast.While) or isinstance(
-            while_node, gast.For), "Input node is not gast loop node"
+        assert isinstance(node, (gast.While,
+                                 gast.For)), "Input node is not gast loop node"
         loop_var_names = set()
         create_var_names = set()
-        read_context = {type(gast.Load), type(gast.AugLoad)}
+        read_context = {type(gast.Load()), type(gast.AugLoad())}
 
         in_loop_vars = self.in_loop_vars[node]
         in_loop_name_strs = set(name.id for name in in_loop_vars)
@@ -114,13 +114,13 @@ class NameVisitor(gast.NodeVisitor):
 
     def visit_For(self, node):
         self.current_loop.append(node)
-        self.before_loop_vars[node] = copy.deepcopy(self.current_seen_vars)
+        self.before_loop_vars[node] = copy.copy(self.current_seen_vars)
         self.generic_visit(node)
         self.current_loop.pop()
 
     def visit_While(self, node):
         self.current_loop.append(node)
-        self.before_loop_vars[node] = copy.deepcopy(self.current_seen_vars)
+        self.before_loop_vars[node] = copy.copy(self.current_seen_vars)
         self.generic_visit(node)
         self.current_loop.pop()
 
