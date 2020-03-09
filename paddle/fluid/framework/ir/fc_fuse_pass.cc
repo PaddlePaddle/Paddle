8d8319fa2d0e6df802b589988388fe55c1623efb
FrostML <380185688@qq.com>
2020-03-09 04:51:39 +0000

    delete useless node, test=develop

--- paddle/fluid/framework/ir/fc_fuse_pass.cc
+++ paddle/fluid/framework/ir/fc_fuse_pass.cc
@@ -124,6 +124,7 @@ int FCFusePass::ApplyFCPattern(Graph* graph, bool with_relu) const {
         delete[] weight_data_tmp;
         desc.SetInput("W", {w_name});
         desc.SetAttr("padding_weights", true);
+        desc.Flush();
       }
     }
 
@@ -156,9 +157,10 @@ int FCFusePass::ApplyFCPattern(Graph* graph, bool with_relu) const {
 
     IR_NODE_LINK_TO(subgraph.at(x), fc_node);
     if (desc.GetAttrIfExists<bool>("padding_weights")) {
+      GraphSafeRemoveNodes(graph, {w});
       IR_NODE_LINK_TO(w_node, fc_node);
     } else {
-      w_key.SetPersistable(false);
+      GraphSafeRemoveNodes(g, {w_node});
       IR_NODE_LINK_TO(w, fc_node);
     }
     IR_NODE_LINK_TO(bias, fc_node);
