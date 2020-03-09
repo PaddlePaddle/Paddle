48ad8517af72b1aa4fe47162529aef7e99ed8e90
yaoxuefeng <yaoxuefeng@baidu.com>
2020-03-09 13:27:09 +0800

    add attr default test=develop

--- paddle/fluid/operators/filter_by_instag_op.cc
+++ paddle/fluid/operators/filter_by_instag_op.cc
@@ -61,7 +61,8 @@ class FilterByInstagOpMaker : public framework::OpProtoAndCheckerMaker {
     AddInput("Filter_tag", "(1D Tensor) filter tag list");
     AddAttr<bool>("is_lod", "is Ins with LoD info or not, default True");
     AddAttr<int64_t>("out_val_ifempty",
-                     "if the output after filter is empty, the output value");
+                     "if the output after filter is empty, the output value")
+        .SetDefault(0);
     AddOutput("Out", "(LoDTensor) embeded tensor filtered by instag");
     AddOutput("LossWeight", "(Tensor) loss weight.");
     AddOutput("IndexMap", "(LoDTensor) mapping from Out rows to X1 rows");
