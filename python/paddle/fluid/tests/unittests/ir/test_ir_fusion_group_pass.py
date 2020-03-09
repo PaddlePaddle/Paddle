2f71dcd0b7630f07f39154b11bb53ea9e154f05e
wangchaochaohu <wangchao66@baidu.com>
2020-03-09 08:17:00 +0000

    add fusion group test for backward test=develop

--- python/paddle/fluid/tests/unittests/ir/test_ir_fusion_group_pass.py
+++ python/paddle/fluid/tests/unittests/ir/test_ir_fusion_group_pass.py
@@ -35,10 +35,15 @@ class FusionGroupPassTest(PassTest):
             # subgraph with 2 op nodes
             tmp_2 = layers.relu(tmp_0 + tmp_1)
 
-        self.fetch_list = [tmp_2]
         self.num_fused_ops = 1
+        self.fetch_list = [tmp_2.name, tmp_1.name + "@GRAD"]
+
+        if self.backward:
+            self.append_gradinets(tmp_2)
+            self.num_fused_ops = 2
 
     def setUp(self):
+        self.backward = True
         self.build_program("float32")
         self.feeds = self._feed_random_data(self.feed_vars)
         self.pass_names = "fusion_group_pass"
@@ -86,11 +91,16 @@ class FusionGroupPassTest1(FusionGroupPassTest):
                 self.feed_vars[2]) * layers.tanh(self.feed_vars[3])
             tmp_2 = layers.tanh(tmp_1) + layers.sigmoid(self.feed_vars[4])
 
-        self.fetch_list = [tmp_1, tmp_2]
-        self.num_fused_ops = 1
+        if self.backward:
+            self.append_gradinets(tmp_2)
+            self.num_fused_ops = 2
+        else:
+            self.num_fused_ops = 1
+
+        self.fetch_list = [tmp_2.name, tmp_1.name + "@GRAD"]
 
 
-class FusionGroupPassTest2(FusionGroupPassTest):
+class FusionGroupPassTest2(PassTest):
     def build_program(self, dtype):
         with fluid.program_guard(self.main_program, self.startup_program):
             self.feed_vars = self._prepare_feed_vars([32, 128], dtype, 3)
@@ -98,15 +108,58 @@ class FusionGroupPassTest2(FusionGroupPassTest):
                 fluid.data(
                     name="data3", shape=[128, 32], dtype=dtype))
 
-            # subgraph with 3 op nodes
-            tmp_1 = layers.relu(
-                (self.feed_vars[0] - self.feed_vars[1]) * self.feed_vars[2])
+            # subgraph with 3 op node
+            tmp_0 = layers.assign(self.feed_vars[2])
+            tmp_1 = layers.relu((self.feed_vars[0] + self.feed_vars[1]) * tmp_0)
             # subgraph with 2 op nodes
             tmp_2 = layers.relu(layers.sigmoid(self.feed_vars[3]))
             tmp_3 = layers.mul(tmp_1, tmp_2)
 
-        self.fetch_list = [tmp_1, tmp_2, tmp_3]
         self.num_fused_ops = 2
+        self.fetch_list = [
+            tmp_1.name, tmp_2.name, tmp_3.name, tmp_1.name + "@GRAD"
+        ]
+
+        if self.backward:
+            self.append_gradinets(tmp_3)
+            self.num_fused_ops = 3
+
+    def setUp(self):
+        self.backward = True
+        self.build_program("float32")
+        self.feeds = self._feed_random_data(self.feed_vars)
+        self.pass_names = "fusion_group_pass"
+        self.fused_op_type = "fusion_group"
+
+    def _prepare_feed_vars(self, shape, dtype, num_data):
+        feed_vars = []
+        for i in range(num_data):
+            var = fluid.data(name=("data" + str(i)), shape=shape, dtype=dtype)
+            feed_vars.append(var)
+        return feed_vars
+
+    def _feed_random_data(self, feed_vars):
+        feeds = {}
+        for var in feed_vars:
+            if var.type != fluid.core.VarDesc.VarType.LOD_TENSOR:
+                raise TypeError("Feed data of non LoDTensor is not supported.")
+
+            shape = var.shape
+            if var.dtype == fluid.core.VarDesc.VarType.FP32:
+                dtype = "float32"
+            elif var.dtype == fluid.core.VarDesc.VarType.FP64:
+                dtype = "float64"
+            elif var.dtype == fluid.core.VarDesc.VarType.FP16:
+                dtype = "float16"
+            else:
+                raise ValueError("Unsupported dtype %s" % var.dtype)
+            feeds[var.name] = np.random.random(shape).astype(dtype)
+        return feeds
+
+    def test_check_output(self):
+        if core.is_compiled_with_cuda():
+            self.pass_attrs = {"fusion_group_pass": {"use_gpu": True}}
+            self.check_output_with_place(fluid.CUDAPlace(0))
 
 
 class FusionGroupPassTestFP64(FusionGroupPassTest):
@@ -134,8 +187,12 @@ class FusionGroupPassTestFP16(FusionGroupPassTest):
             tmp_4 = layers.relu(tmp_2 + tmp_3)
             tmp_5 = layers.cast(tmp_4, dtype=dtype)
 
-        self.fetch_list = [tmp_5]
         self.num_fused_ops = 1
+        self.fetch_list = [tmp_5]
+
+        if self.backward:
+            self.num_fused_ops = 2
+            self.append_gradinets(tmp_5)
 
 
 class FusionGroupPassSumTest(FusionGroupPassTest):
