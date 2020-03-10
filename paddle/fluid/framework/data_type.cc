ee5b69f6db8952df662cac2ef3aae5c33e867cb5
chalsliu <craziecharles@gmail.com>
2020-03-10 19:05:12 +0800

    Test coverage
    test=develop

--- paddle/fluid/framework/data_type.cc
+++ paddle/fluid/framework/data_type.cc
@@ -79,11 +79,11 @@ std::type_index ToTypeIndex(proto::VarType::Type type) {
 
 std::string DataTypeToString(const proto::VarType::Type type) {
   auto it = gDataTypeMap().proto_to_str_.find(static_cast<int>(type));
-  if (it != gDataTypeMap().proto_to_str_.end()) {
+  if (it == gDataTypeMap().proto_to_str_.end()) {
     PADDLE_THROW("Not support proto::VarType::Type(%d) as tensor type",
                  static_cast<int>(type));
-    return it->second;
   }
+  return it->second;
 }
 
 size_t SizeOfType(proto::VarType::Type type) {
