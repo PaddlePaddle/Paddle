cc7ec7d64bf968dce3da013f7e3782f00a3699b7
sandyhouse <lilong12@baidu.com>
2020-03-16 14:07:08 +0800

    add model_parallel device_worker and trainer

---
+++ paddle/fluid/framework/model_parallel_trainer.cc
@@ -0,0 +1,164 @@
+// Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "paddle/fluid/framework/data_feed_factory.h"
+#include "paddle/fluid/framework/device_worker_factory.h"
+#include "paddle/fluid/framework/trainer.h"
+#include "paddle/fluid/framework/trainer_desc.pb.h"
+
+namespace paddle {
+namespace framework {
+
+void ModelParallelTrainer::Initialize(const TrainerDesc& trainer_desc,
+                                      Dataset* dataset) {
+  // auto device_num_ = trainer_desc.thread_num();
+  // Todo: (lilong12) set device_num_ correctly
+  int device_num_ = 2;
+  VLOG(3) << "device num: " << device_num_;
+
+  SetDataset(dataset);
+  const std::vector<paddle::framework::DataFeed*> readers =
+      dataset->GetReaders();
+  VLOG(3) << "readers num: " << readers.size();
+  // Todo: (lilong12) add assert(readers.size() == 1)
+  auto reader = readers[0];
+  feed_var_names_ = reader->GetUseSlotAlias();
+
+  pipeline_config_ = trainer_desc.section_param();
+
+  section_num_ = device_num_;
+  workers_.resize(section_num_);
+
+  for (int i = 0; i < section_num_; ++i) {
+    const auto& section_config = pipeline_config_.section_config(i);
+    // Todo: (lilong12) set concurrency_ correctly.
+    concurrency_ = 1;
+    platform::Place place;
+    switch (section_config.place()) {
+      case SectionConfig::CPUPlace:
+        place = platform::CPUPlace();
+        break;
+      case SectionConfig::CUDAPlace:
+        place = platform::CUDAPlace(i);
+        break;
+      case SectionConfig::CUDAPinnedPlace:
+        place = platform::CUDAPinnedPlace();
+        break;
+      default:
+        PADDLE_ENFORCE(false, "Unkown place type in SectionConfig: %d",
+                       section_config.place());
+    }
+    VLOG(3) << "place: " << place;
+
+    workers_[i].resize(concurrency_);
+    for (int j = 0; j < concurrency_; ++j) {
+      workers_[i][j] = DeviceWorkerFactory::CreateDeviceWorker(
+          trainer_desc.device_worker_name());
+      auto this_worker =
+          std::dynamic_pointer_cast<paddle::framework::ModelParallelWorker>(
+              workers_[i][j]);
+      if (i == 0 && j == 0) {
+        this_worker->SetDataFeed(reader);
+        this_worker->SetReaderPlace(place);
+      }
+      this_worker->SetPlace(place);
+      this_worker->Initialize(trainer_desc);
+    }
+  }
+  // set debug here
+  SetDebug(trainer_desc.debug());
+}
+
+void ModelParallelTrainer::CopyParameters(Scope& scope, int macrobatch_id,
+                                          const ProgramDesc& main_program) {
+  auto& global_block = main_program.Block(0);
+  for (auto& var : global_block.AllVars()) {
+    int is_feed_var =
+        std::count(feed_var_names_.begin(), feed_var_names_.end(), var->Name());
+    if ((var->Persistable() || is_feed_var) && macrobatch_id == 0) {
+      auto* ptr = scope.Var(var->Name());
+      InitializeVariable(ptr, var->GetType());
+      VLOG(3) << "Create variable " << var->Name()
+              << " globally for root scope";
+    } else {
+      auto* ptr = macrobatch_scopes_[macrobatch_id]->Var(var->Name());
+      InitializeVariable(ptr, var->GetType());
+      VLOG(3) << "Create variable " << var->Name() << " for macrobatch "
+              << macrobatch_id;
+    }
+  }
+}
+
+void ModelParallelTrainer::InitTrainerEnv(const ProgramDesc& main_program,
+                                          const platform::Place& place) {
+  PADDLE_ENFORCE(root_scope_, "Null root_scope pointer");
+  // Todo: (lilong12) set num_macrobatches correctly
+  int num_macrobatches_ = 1;
+  macrobatch_scopes_.resize(num_macrobatches_);
+  for (auto& var : main_program.Block(0).AllVars()) {
+    if (var->Persistable()) {
+      persistable_vars_.push_back(var->Name());
+    }
+  }
+
+  VLOG(3) << "create all scopes";
+  // auto* scope = &root_scope_->NewScope();
+  for (int i = 0; i < num_macrobatches_; ++i) {
+    macrobatch_scopes_[i] = &root_scope_->NewScope();
+    CopyParameters(*root_scope_, i, main_program);
+  }
+
+  for (int i = 0; i < section_num_; ++i) {
+    for (size_t j = 0; j < workers_[i].size(); ++j) {
+      auto this_worker =
+          std::dynamic_pointer_cast<paddle::framework::ModelParallelWorker>(
+              workers_[i][j]);
+      this_worker->SetRootScope(root_scope_);
+      this_worker->SetMacrobatchScopes(macrobatch_scopes_);
+    }
+  }
+}
+
+void ModelParallelTrainer::Run() {
+  VLOG(3) << "Going to run";
+  for (int i = 0; i < section_num_; ++i) {
+    for (size_t j = 0; j < workers_[i].size(); ++j) {
+      // if (!debug_) {
+      if (true) {
+        section_threads_.push_back(
+            std::thread(&DeviceWorker::TrainFiles, workers_[i][j].get()));
+      }
+      // else {
+      //  section_threads_.push_back(std::thread(
+      //      &DeviceWorker::TrainFilesWithProfiler, workers_[i][j].get()));
+      //}
+      //}
+    }
+  }
+}
+
+void ModelParallelTrainer::Finalize() {
+  VLOG(3) << "calling finalized. ";
+  for (auto& th : section_threads_) {
+    th.join();
+  }
+  root_scope_->DropKids();
+}
+
+Scope* ModelParallelTrainer::GetWorkerScope(int thread_id) {
+  return macrobatch_scopes_[thread_id];
+}
+
+}  // end namespace framework
+}  // end namespace paddle
---
+++ paddle/fluid/framework/model_parallel_worker.cc
@@ -0,0 +1,106 @@
+/* Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#include "google/protobuf/io/zero_copy_stream_impl.h"
+#include "google/protobuf/message.h"
+#include "google/protobuf/text_format.h"
+
+#include "paddle/fluid/framework/device_worker.h"
+#include "paddle/fluid/framework/fleet/box_wrapper.h"
+#include "paddle/fluid/framework/tensor_util.h"
+#include "paddle/fluid/framework/trainer_desc.pb.h"
+#include "paddle/fluid/platform/cpu_helper.h"
+#include "paddle/fluid/platform/device_context.h"
+#include "paddle/fluid/platform/lodtensor_printer.h"
+
+namespace paddle {
+namespace framework {
+
+std::atomic<int> ModelParallelWorker::cpu_id_(0);
+void ModelParallelWorker::Initialize(const TrainerDesc& trainer_desc) {
+  VLOG(3) << "ModelParallelWorker::Initialize, place " << place_;
+  dev_ctx_ = platform::DeviceContextPool::Instance().Get(place_);
+  std::shared_ptr<framework::ProgramDesc> program;
+  program.reset(new ProgramDesc(
+      trainer_desc.section_param().section_config(0).program_desc()));
+  for (auto& op_desc : program->Block(0).AllOps()) {
+    ops_.push_back(OpRegistry::CreateOp(*op_desc));
+  }
+}
+
+void ModelParallelWorker::AutoSetCPUAffinity(bool reuse) {
+  int thread_cpu_id = cpu_id_.fetch_add(1);
+
+  unsigned concurrency_cap = std::thread::hardware_concurrency();
+  unsigned proc = thread_cpu_id;
+
+  if (proc >= concurrency_cap) {
+    if (reuse) {
+      proc %= concurrency_cap;
+    } else {
+      LOG(INFO) << "All " << concurrency_cap
+                << " CPUs have been set affinities. Fail to set the "
+                << thread_cpu_id << "th thread";
+      return;
+    }
+  }
+
+  cpu_set_t mask;
+  CPU_ZERO(&mask);
+  CPU_SET(proc, &mask);
+
+  if (-1 == sched_setaffinity(0, sizeof(mask), &mask)) {
+    LOG(WARNING) << "Fail to set thread affinity to CPU " << proc;
+    return;
+  }
+
+  CPU_ZERO(&mask);
+  if ((0 != sched_getaffinity(0, sizeof(mask), &mask)) ||
+      (0 == CPU_ISSET(proc, &mask))) {
+    LOG(WARNING) << "Fail to set thread affinity to CPU " << proc;
+  }
+  VLOG(3) << "Set " << thread_cpu_id << "th thread affinity to CPU " << proc;
+}
+
+void ModelParallelWorker::TrainFiles() {
+  AutoSetCPUAffinity(true);
+
+  int batch_size = 0;
+  VLOG(3) << "before set data reader_ ";
+  if (device_reader_ != nullptr) {
+    VLOG(3) << "set data reader_ ";
+    VLOG(3) << "start data reader_ ";
+    device_reader_->Start();
+    // const std::vector<std::string>& input_feed =
+    //    device_reader_->GetUseSlotAlias();
+    // for (auto name : input_feed) {
+    //  device_reader_->AddFeedVar(root_scope_->Var(name), name);
+    //}
+    VLOG(3) << "assign var for data reader_ ";
+    device_reader_->AssignFeedVar(*root_scope_);
+    VLOG(3) << "get batch_size for data reader_ ";
+    batch_size = device_reader_->Next();
+    VLOG(3) << "read batch size: " << batch_size;
+  }
+
+  VLOG(3) << "begin running ops";
+  for (auto& op : ops_) {
+    op->Run(*macrobatch_scopes_[0], place_);
+  }
+  dev_ctx_->Wait();
+}
+
+void ModelParallelWorker::TrainFilesWithProfiler() {
+  VLOG(3) << "Not implemented now.";
+}
+
+}  // namespace framework
+}  // namespace paddle
