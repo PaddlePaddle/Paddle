af9d4a6fc8bd6b0b1167415af659d5d7fb8da2c3
--global <--global>
2020-03-21 14:21:33 +0800

    add template to reorganize python api

--- python/paddle/framework/framework.py
+++ python/paddle/framework/framework.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .plot import Ploter
-__all__ = ['dump_config', 'Ploter']
 
-#TODO: define new api under this directory
-# __all__ = ['profiler',
-#            '	cuda_profiler',
-#            '	profiler',
-#            '	reset_profiler',
-#            '	start_profiler',
-#            '	stop_profiler',
-#            'unique_name',
-#            'load_op_library',
-#            'require_version',
-#            'is_compiled_with_cuda']
+
+
+# TODO: define framework api 
+# __all__ = ['set_default_dtype',
+#            'get_default_dtype']
 
--- python/paddle/metric/__init__.py
+++ python/paddle/metric/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .plot import Ploter
-__all__ = ['dump_config', 'Ploter']
 
-#TODO: define new api under this directory
-# __all__ = ['profiler',
-#            '	cuda_profiler',
-#            '	profiler',
-#            '	reset_profiler',
-#            '	start_profiler',
-#            '	stop_profiler',
-#            'unique_name',
-#            'load_op_library',
-#            'require_version',
-#            'is_compiled_with_cuda']
 
+
+# TODO: define the functions to calculate metric in this directory 
+# __all__ = ['Accuracy',
+#            'Auc',
+#            'ChunkEvaluator',
+#            'CompositeMetric',
+#            'DetectionMAP',
+#            'EditDistance',
+#            'Precesion',
+#            'Recall',
+#            'accuracy',
+#            'auc',
+#            'chunk_eval',
+#            'cos_sim',
+#            'mean_iou']
