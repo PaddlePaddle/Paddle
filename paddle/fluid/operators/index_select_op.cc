3198527751c0f14893ca42ab5c9fc6ed69fc1635
malin10 <malin10@baidu.com>
2020-03-22 00:11:13 +0800

    debug for index_select

--- paddle/fluid/operators/index_select_op.cc
+++ paddle/fluid/operators/index_select_op.cc
@@ -1,21 +1,24 @@
-// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #include "paddle/fluid/operators/index_select_op.h"
+#include <memory>
 
 namespace paddle {
 namespace operators {
+
+using framework::Tensor;
 
 class IndexSelectOp : public framework::OperatorWithKernel {
  public:
@@ -42,23 +45,17 @@ class IndexSelectOp : public framework::OperatorWithKernel {
                       "ShapeError: dim must be greater or equal to 0. "
                       "But received select dim = %d.",
                       dim);
-
-    PADDLE_ENFORCE_EQ(index_dim.size() == 1 ||
-                          (index_dim.size() == 2 && index_dim[1] == 1),
-                      true,
-                      "ShapeError: index must be 1-D tensor, "
-                      "But received: the shape of index is [%s],the dimension "
-                      "of index is [%d]",
-                      index_dim, index_dim.size());
+    PADDLE_ENFORCE_EQ(
+        index_dim.size() == 1 || (index_dim.size() == 2 && index_dim[1] == 1),
+        true,
+        "ShapeError: index must be 1-D tensor, "
+        "But received: the shape of index is [%s],the dimension "
+        "of index is [%d]",
+        index_dim, index_dim.size());
 
     auto output_dim = framework::vectorize(input_dim);
     output_dim[dim] = index_dim[0];
     ctx->SetOutputDim("Out", framework::make_ddim(output_dim));
-
-    if (ctx->GetOutputsVarType("Out")[0] ==
-        framework::proto::VarType::LOD_TENSOR) {
-      ctx->ShareLoD("X", /*->*/ "Out");
-    }
   }
 
  protected:
@@ -74,18 +71,22 @@ class IndexSelectGradOp : public framework::OperatorWithKernel {
   using framework::OperatorWithKernel::OperatorWithKernel;
 
   void InferShape(framework::InferShapeContext* ctx) const override {
+    PADDLE_ENFORCE(ctx->HasInput("Index"), "Input(Index) should be not null.");
+    PADDLE_ENFORCE(ctx->HasInput(framework::GradVarName("Out")),
+                   "Input(Out@GRAD) should be not null.");
+    PADDLE_ENFORCE(ctx->HasOutput(framework::GradVarName("X")),
+                   "Output(X@GRAD) should be not null.");
+
     ctx->SetOutputDim(framework::GradVarName("X"), ctx->GetInputDim("X"));
-    ctx->ShareLoD("X", /*-->*/ framework::GradVarName("X"));
   }
 
  protected:
   framework::OpKernelType GetExpectedKernelType(
       const framework::ExecutionContext& ctx) const override {
     return framework::OpKernelType(OperatorWithKernel::IndicateVarDataType(
-                                       ctx, framework::GradVarName("Out")),
-                                   ctx.device_context());
-  }
-};
+                                       ctx, framework::GradVarName("Out"));
+  };
+}
 
 class IndexSelectOpMaker : public framework::OpProtoAndCheckerMaker {
  public:
@@ -95,14 +96,14 @@ class IndexSelectOpMaker : public framework::OpProtoAndCheckerMaker {
     AddOutput("Out", "the output tensor.");
     AddAttr<int>("dim", "the dimension in which we index.").SetDefault(0);
     AddComment(R"DOC(
-        Returns a new tensor which indexes the input tensor
-        along dimension dim using the entries in index which
-        is a Tensor.
+    Returns a new tensor which indexes the input tensor
+    along dimension dim using the entries in index which
+    is a Tensor.
 
-        The returned tensor has the same number of dimensions
-        as the original tensor (input). The dimth dimension
-        has the same size as the length of index; other dimensions
-        have the same size as in the original tensor.
+    The returned tensor has the same number of dimensions
+    as the original tensor (input). The dimth dimension
+    has the same size as the length of index; other dimensions
+    have the same size as in the original tensor.
     )DOC");
   }
 };
@@ -119,13 +120,10 @@ class IndexSelectGradMaker : public framework::SingleGradOpMaker<T> {
     op->SetInput("X", this->Input("X"));
     op->SetInput("Index", this->Input("Index"));
     op->SetInput(framework::GradVarName("Out"), this->OutputGrad("Out"));
-
     op->SetOutput(framework::GradVarName("X"), this->InputGrad("X"));
-
     op->SetAttrMap(this->Attrs());
   }
-};
-
+}
 }  // namespace operators
 }  // namespace paddle
 
@@ -134,12 +132,9 @@ REGISTER_OPERATOR(index_select, ops::IndexSelectOp, ops::IndexSelectOpMaker,
                   ops::IndexSelectGradMaker<paddle::framework::OpDesc>,
                   ops::IndexSelectGradMaker<paddle::imperative::OpBase>);
 REGISTER_OPERATOR(index_select_grad, ops::IndexSelectGradOp);
-
 REGISTER_OP_CPU_KERNEL(
     index_select,
-    ops::IndexSelectOpKernel<paddle::platform::CPUDeviceContext, float>,
-    ops::IndexSelectOpKernel<paddle::platform::CPUDeviceContext, double>);
+    ops::IndexSelectKernel<paddle::platform::CPUDeviceContext, float>);
 REGISTER_OP_CPU_KERNEL(
     index_select_grad,
-    ops::IndexSelectGradOpKernel<paddle::platform::CPUDeviceContext, float>,
-    ops::IndexSelectGradOpKernel<paddle::platform::CPUDeviceContext, double>);
+    ops::IndexSelectGradKernel<paddle::platform::CPUDeviceContext, float>);
--- paddle/fluid/operators/index_select_op.h
+++ paddle/fluid/operators/index_select_op.h
@@ -1,19 +1,16 @@
-// Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
 
-#ifndef PADDLE_FLUID_OPERATORS_INDEX_SELECT_OP_H_
-#define PADDLE_FLUID_OPERATORS_INDEX_SELECT_OP_H_
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #pragma once
 #include "paddle/fluid/framework/op_registry.h"
@@ -21,181 +18,17 @@
 namespace paddle {
 namespace operators {
 
-using Tensor = framework::Tensor;
-using LoDTensor = framework::LoDTensor;
-using DDim = framework::DDim;
-
-template <typename T, typename IndexT = int>
-void IndexSelectInner(const framework::ExecutionContext& context,
-                      const Tensor& input, const Tensor& index, Tensor* output,
-                      int dim) {
-  auto index_size = index.dims()[0];
-
-  const T* p_input = input.data<T>(context.GetPlace());
-  const IndexT* p_index = index.data<IndexT>(context.GetPlace());
-  T* p_output = output->data<T>(context.GetPlace());
-
-  auto input_dim = input.dims();
-  auto input_dim_size = input_dim.size();
-  auto output_dim = output->dims();
-
-  auto slice_size = 1;
-  for (size_t i = dim + 1; i < input_dim_size; i++) {
-    slice_size *= input_dim[i];
-  }
-  const size_t slice_bytes = slice_size * sizeof(T);
-
-  auto input_width = slice_size * input_dim[dim];
-  auto output_width = slice_size * output_dim[dim];
-
-  auto outer_nums = 1;
-  for (auto i = 0; i < dim; i++) {
-    outer_nums *= input_dim[i];
-  }
-
-  VLOG(1) << "outer_nums: " << outer_nums << "; slice_size: " << slice_size
-          << "; input_width: " << input_width
-          << "; output_width: " << output_width;
-
-  for (auto i = 0; i < outer_nums; i++) {
-    auto input_start_offset = i * input_width;
-    auto output_start_offset = i * output_width;
-
-    for (auto j = 0; j < index_size; j++) {
-      IndexT index_value = p_index[j];
-      memcpy(p_output + output_start_offset + j * slice_size,
-             p_input + input_start_offset + index_value * slice_size,
-             slice_bytes);
-    }
-  }
-}
-
 template <typename DeviceContext, typename T>
-class IndexSelectOpKernel : public framework::OpKernel<T> {
+class IndexSelectKernel : public framework::OpKernel<T> {
  public:
-  void Compute(const framework::ExecutionContext& context) {
-    auto* inputs_var = context.InputVar("X");
-    auto* index_var = context.InputVar("Index");
-    auto* output_var = context.OutputVar("Out");
-
-    auto& inputs = inputs_var->Get<LoDTensor>();
-    auto& index = index_var->Get<LoDTensor>();
-    auto* output = output_var->GetMutable<framework::LoDTensor>();
-    int dim = context.Attr<int>("dim");
-
-    if (index.dims().size() == 2) {
-      PADDLE_ENFORCE_EQ(
-          index.dims()[1], 1,
-          "index.dims()[1] should be 1 when index.dims().size() == "
-          "2 in index_select_op.");
-    } else {
-      PADDLE_ENFORCE_EQ(
-          index.dims().size(), 1,
-          "index.dims().size() should be 1 or 2 in index_select_op.");
-    }
-
-    const auto& index_type = index->type();
-    bool index_type_match = index_type == framework::proto::VarType::INT32 ||
-                            index_type == framework::proto::VarType::INT64;
-    PADDLE_ENFORCE(
-        index_type_match,
-        "Index holds the wrong type, it holds %s, but desires to be %s or %s",
-        paddle::framework::DataTypeToString(index_type),
-        paddle::framework::DataTypeToString(framework::proto::VarType::INT32),
-        paddle::framework::DataTypeToString(framework::proto::VarType::INT64));
-
-    if (index_type == framework::proto::VarType::INT32) {
-      IndexSelectInner<T, int>(context, *inputs, *index, output, dim);
-    } else if (index_type == framework::proto::VarType::INT64) {
-      IndexSelectInner<T, int64_t>(context, *inputs, *index, output, dim);
-    }
-  }
+  void Compute(const framework::ExecutionContext &context) const override {}
 };
 
-template <typename T, typename IndexT = int>
-void IndexSelectGradInner(const framework::ExecutionContext& context,
-                          const Tensor& out_grad, const Tensor& index,
-                          Tensor* x_grad, int dim) {
-  auto index_size = index.dims()[0];
-
-  const T* p_input = out_grad.data<T>(context.GetPlace());
-  const IndexT* p_index = index.data<IndexT>(context.GetPlace());
-  T* p_output = x_grad->data<T>(context.GetPlace());
-
-  auto input_dim = out_grad.dims();
-  auto input_dim_size = input_dim.size();
-  auto output_dim = x_grad->dims();
-
-  auto slice_size = 1;
-  for (size_t i = dim + 1; i < input_dim_size; i++) {
-    slice_size *= input_dim[i];
-  }
-  const size_t slice_bytes = slice_size * sizeof(T);
-
-  auto input_width = slice_size * input_dim[dim];
-  auto output_width = slice_size * output_dim[dim];
-
-  auto outer_nums = 1;
-  for (auto i = 0; i < dim; i++) {
-    outer_nums *= input_dim[i];
-  }
-
-  VLOG(1) << "index_select_grad: outer_nums: " << outer_nums
-          << "; slice_size: " << slice_size << "; input_width: " << input_width
-          << "; output_width: " << output_width;
-
-  for (auto i = 0; i < outer_nums; i++) {
-    auto input_start_offset = i * input_width;
-    auto output_start_offset = i * output_width;
-
-    for (auto j = 0; j < index_size; ++j) {
-      IndexT index_value = p_index[i];
-      memset(p_output + output_start_offset + slice_size * index_value, 0,
-             slice_bytes);
-    }
-    for (auto j = 0; j < index_size; j++) {
-      IndexT index_value = p_index[j];
-      T* ou = p_output + output_start_offset + index_value * slice_size;
-      T* in = p_input + input_start_offset + j * slice_size;
-      for (auto k = 0; k < slice_size; k++) {
-        *(ou + k) += *(in + k);
-      }
-    }
-  }
-}
-
 template <typename DeviceContext, typename T>
-class IndexSelectGradOpKernel : public framework::OpKernel<T> {
+class IndexSelectGradKernel : public framework::OpKernel<T> {
  public:
-  void Compute(const framework::ExecutionContext& ctx) const override {
-    auto* index_var = ctx.InputVar("Index");
-    auto* x_grad_var = ctx.OutputVar(framework::GradVarName("X"));
-    auto* out_grad_var = ctx.InputVar(framework::GradVarName("Out"));
-
-    auto& index = index_var->Get<LoDTensor>();
-    auto& x_grad = x_grad_var->GetMutable<framework::LoDTensor>();
-    auto* out_grad = out_grad_var->Get<LoDTensor>();
-
-    int dim = ctx.Attr<int>("dim");
-
-    const auto& index_type = index->type();
-    bool index_type_match = index_type == framework::proto::VarType::INT32 ||
-                            index_type == framework::proto::VarType::INT64;
-    PADDLE_ENFORCE(
-        index_type_match,
-        "Index holds the wrong type, it holds %s, but desires to be %s or %s",
-        paddle::framework::DataTypeToString(index_type),
-        paddle::framework::DataTypeToString(framework::proto::VarType::INT32),
-        paddle::framework::DataTypeToString(framework::proto::VarType::INT64));
-    if (index_type == framework::proto::VarType::INT32) {
-      IndexSelectGradInner<T, int>(ctx, *out_grad, *index, x_grad, dim);
-    } else if (index_type == framework::proto::VarType::INT64) {
-      IndexSelectGradInner<T, int64_t>(ctx, *out_grad, *index, x_grad, dim);
-    }
-  }
+  void Compute(const framework::ExecutionContext &context) const override {}
 };
 
 }  // namespace operators
 }  // namespace paddle
-
-#endif  // PADDLE_FLUID_OPERATORS_INDEX_SELECT_OP_H_
