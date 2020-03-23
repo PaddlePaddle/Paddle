620908ba2f3f756e08529f363876f4ba3816d6e3
malin10 <malin10@baidu.com>
2020-03-23 12:37:06 +0800

    debug

--- paddle/fluid/operators/roll_op.h
+++ paddle/fluid/operators/roll_op.h
@@ -31,41 +31,43 @@ inline void shift_along_dim(const T* input_data, T* output_data,
   for (auto i = 0; i < dim; i++) {
     outer_loops *= input_dim[i];
   }
-  int64_t slice_width = 1;
+  auto slice_width = 1;
   for (auto i = dim + 1; i < input_dim.size(); i++) {
     slice_width *= input_dim[i];
   }
+  const size_t slice_bytes = slice_width * sizeof(T);
 
   for (auto i = 0; i < outer_loops; i++) {
     auto input_pos = i * slice_width;
     auto output_pos = ((i + dim) % input_dim[dim]) * slice_width;
-    memcpy(output_pos, input_pos, slice_width * sizeof(T));
+    memcpy(output_data + output_pos, input_data + input_pos, slice_bytes);
   }
 }
 
 template <typename DeviceContext, typename T>
 class RollKernel : public framework::OpKernel<T> {
  public:
-  void Compute(const framework::ExecutionContext& context) const override {}
-  auto* input_var = context.InputVar("X");
-  auto* output_var = context.OutputVar("Out");
-  auto& input = input_var -> Get<LoDTensor>();
-  auto* output = input_var -> GetMutable<LoDTensor>();
-  std::vector<int64_t> shifts = context.Attr<std::vecotr<int64_t>>("shifts");
-  std::vector<int64_t> dims = context.Attr<std::vecotr<int64_t>>("dims");
+  void Compute(const framework::ExecutionContext& context) const override {
+    auto* input_var = context.InputVar("X");
+    auto* output_var = context.OutputVar("Out");
+    auto& input = input_var->Get<LoDTensor>();
+    auto* output = output_var->GetMutable<LoDTensor>();
+    std::vector<int64_t> shifts = context.Attr<std::vector<int64_t>>("shifts");
+    std::vector<int64_t> dims = context.Attr<std::vector<int64_t>>("dims");
 
-  PADDLE_ENFORCE_EQ(shifts.size(), dims.size(),
-                    "AttrError: Attr<shifts>.size() must be equal "
-                    "to Attr<dims>.size(). But received: Attr<shift>"
-                    ".size = %d, Attr<dims>.size() = %d",
-                    shift.size(), dims.size());
-  size_t nums = shifts.size();
-  const T* input_data = input.data<T>();
-  T* output_data = output->mutable_data<T>(context.GetPlace());
-  const DDim input_dim = input.dims();
+    PADDLE_ENFORCE_EQ(shifts.size(), dims.size(),
+                      "AttrError: Attr<shifts>.size() must be equal "
+                      "to Attr<dims>.size(). But received: Attr<shift>"
+                      ".size = %d, Attr<dims>.size() = %d",
+                      shifts.size(), dims.size());
+    size_t nums = shifts.size();
+    const T* input_data = input.data<T>();
+    T* output_data = output->mutable_data<T>(context.GetPlace());
+    const DDim input_dim = input.dims();
 
-  for (size_t i = 0; i < num; i++) {
-    shift_along_dim(input_data, output_data, input_dim, dims[i], shifts[i]);
+    for (size_t i = 0; i < nums; i++) {
+      shift_along_dim(input_data, output_data, input_dim, dims[i], shifts[i]);
+    }
   }
 };
 
@@ -76,21 +78,21 @@ class RollGradKernel : public framework::OpKernel<T> {
     auto* input_var = context.InputVar(framework::GradVarName("Out"));
     auto* output_var = context.OutputVar(framework::GradVarName("X"));
     auto& input = input_var->Get<LoDTensor>();
-    auto* output = input_var->GetMutable<LoDTensor>();
-    std::vector<int64_t> shifts = context.Attr<std::vecotr<int64_t>>("shifts");
-    std::vector<int64_t> dims = context.Attr<std::vecotr<int64_t>>("dims");
+    auto* output = output_var->GetMutable<LoDTensor>();
+    std::vector<int64_t> shifts = context.Attr<std::vector<int64_t>>("shifts");
+    std::vector<int64_t> dims = context.Attr<std::vector<int64_t>>("dims");
 
     PADDLE_ENFORCE_EQ(shifts.size(), dims.size(),
                       "AttrError: Attr<shifts>.size() must be equal "
                       "to Attr<dims>.size(). But received: Attr<shift>"
                       ".size = %d, Attr<dims>.size() = %d",
-                      shift.size(), dims.size());
+                      shifts.size(), dims.size());
     size_t nums = shifts.size();
     const T* input_data = input.data<T>();
     T* output_data = output->mutable_data<T>(context.GetPlace());
     const DDim input_dim = input.dims();
 
-    for (size_t i = 0; i < num; i++) {
+    for (size_t i = 0; i < nums; i++) {
       shift_along_dim(input_data, output_data, input_dim, dims[i],
                       0 - shifts[i]);
     }
