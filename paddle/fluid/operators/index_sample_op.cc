5d6520a281ec7bb6e0467c80f7321509440b0e1e
chengmo <cmchengmo@163.com>
2020-03-24 20:30:17 +0800

    fix compile bug

--- paddle/fluid/operators/index_sample_op.cc
+++ paddle/fluid/operators/index_sample_op.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/operators/index_sample.h"
+#include "paddle/fluid/operators/index_sample_op.h"
 #include <vector>
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/platform/enforce.h"
