7f8710a1b1914f8d8939fcc66deae8ad292f4fef
chengmo <cmchengmo@163.com>
2020-03-25 10:19:21 +0800

    add inner function

--- paddle/fluid/operators/index_sample_op.h
+++ paddle/fluid/operators/index_sample_op.h
@@ -30,6 +30,50 @@ using Tensor = framework::Tensor;
 using LoDTensor = framework::LoDTensor;
 using DDim = framework::DDim;
 
+template <typename T, typename IndexT = int>
+void IndexSampleInner(const framework::ExecutionContext &context,
+                      const LoDTensor &input, const LoDTensor &index,
+                      LoDTensor *output) {
+  auto input_dims = input.dims();
+  auto index_dims = index.dims();
+
+  int batch_size = input_dims[0];
+  auto value_length = input_dims[1];
+  auto index_length = index_dims[1];
+  int index_ids_num = index_tensor.numel();
+  auto *input_data = input.data<T>();
+  auto *index_data = index.data<IndexT>();
+
+  std::vector<T> res{};
+  for (int i = 0; i < index_ids_num; i++) {
+    int b = floor(i / index_length);
+    PADDLE_ENFORCE_LT(
+        -1, index_data[i],
+        "Variable value (index) of OP(index_sample) "
+        "expected >= 0 and < %ld, but got %ld. Please check input "
+        "value.",
+        value_length, index_data[i]);
+    PADDLE_ENFORCE_LT(
+        index_data[i], value_length,
+        "Variable value (index) of OP(index_sample) "
+        "expected >= 0 and < %ld, but got %ld. Please check input "
+        "value.",
+        value_length, index_data[i]);
+
+    int v_i = b * value_length + static_cast<int>(index_data[i]);
+    T v = input_data[v_i];
+    VLOG(4) << "Index Sample: batch = " << b << " index = " << v_i
+            << " value = " << v;
+    res.push_back(v);
+  }
+
+  auto ddim = framework::make_ddim({batch_size, index_length});
+  output->Resize(ddim);
+  T *out_data = output->mutable_data<T>(context.GetPlace());
+
+  memcpy(out_data, &res[0], sizeof(T) * index_ids_num);
+}
+
 template <typename DeviceContext, typename T>
 class IndexSampleKernel : public framework::OpKernel<T> {
  public:
@@ -39,15 +83,9 @@ class IndexSampleKernel : public framework::OpKernel<T> {
 
     auto &input_tensor = input_var->Get<LoDTensor>();
     auto &index_tensor = index_var->Get<LoDTensor>();
-    auto input_dims = input_tensor.dims();
-    auto index_dims = index_tensor.dims();
 
-    int batch_size = input_dims[0];
-    auto value_length = input_dims[1];
-    auto index_length = index_dims[1];
-    int index_ids_num = index_tensor.numel();
-
-    auto *input_data = input_tensor.data<T>();
+    auto *out_var = ctx.OutputVar("Out");
+    auto *out_tensor = out_var->GetMutable<framework::LoDTensor>();
 
     const auto &index_type = index_tensor.type();
     bool index_type_match = index_type == framework::proto::VarType::INT32 ||
@@ -61,43 +99,51 @@ class IndexSampleKernel : public framework::OpKernel<T> {
                               framework::proto::VarType::INT32),
                           paddle::framework::DataTypeToString(
                               framework::proto::VarType::INT64)));
-
-    auto *index_data = index_type == framework::proto::VarType::INT32
-                           ? index_tensor.data<int>()
-                           : index_tensor.data<int64_t>();
-
-    std::vector<T> res{};
-    for (int i = 0; i < index_ids_num; i++) {
-      int b = floor(i / index_length);
-      PADDLE_ENFORCE_LT(
-          -1, index_data[i],
-          "Variable value (index) of OP(index_sample) "
-          "expected >= 0 and < %ld, but got %ld. Please check input "
-          "value.",
-          value_length, index_data[i]);
-      PADDLE_ENFORCE_LT(
-          index_data[i], value_length,
-          "Variable value (index) of OP(index_sample) "
-          "expected >= 0 and < %ld, but got %ld. Please check input "
-          "value.",
-          value_length, index_data[i]);
-
-      int v_i = b * value_length + static_cast<int>(index_data[i]);
-      T v = input_data[v_i];
-      VLOG(4) << "Index Sample: batch = " << b << " index = " << v_i
-              << " value = " << v;
-      res.push_back(v);
+    if (index_type == framework::proto::VarType::INT32) {
+      IndexSampleInner<T, int>(ctx, input_tensor, index_tensor, out_tensor);
+    } else if (index_type == framework::proto::VarType::INT64) {
+      IndexSampleInner<T, int64_t>(ctx, input_tensor, index_tensor, out_tensor);
     }
-
-    auto *out_var = ctx.OutputVar("Out");
-    auto *out_tensor = out_var->GetMutable<framework::LoDTensor>();
-    auto ddim = framework::make_ddim({batch_size, index_length});
-    out_tensor->Resize(ddim);
-    auto *out_data = out_tensor->mutable_data<T>(ctx.GetPlace());
-
-    memcpy(out_data, &res[0], sizeof(T) * index_ids_num);
   }
 };
+
+template <typename T, typename IndexT = int>
+void IndexSampleGradInner(const framework::ExecutionContext &context,
+                          const LoDTensor &out_grad, const LoDTensor &index,
+                          LoDTensor *x_grad) {
+  auto index_dims = index.dims();
+  auto out_grad_dims = out_grad.dims();
+  auto x_grad_dims = x_grad->dims();
+
+  int batch_size = x_grad_dims[0];
+  auto value_length = x_grad_dims[1];
+  auto index_length = index_dims[1];
+  int index_ids_num = index.numel();
+
+  T *x_grad_data = x_grad->mutable_data<T>(context.GetPlace());
+  auto *out_grad_data = out_grad.data<T>();
+  auto *index_data = index.data<IndexT>();
+
+  memset(x_grad_data, 0, batch_size * value_length * sizeof(T));
+
+  for (int i = 0; i < index_ids_num, i++) {
+    int b = floor(i / index_length);
+    PADDLE_ENFORCE_LT(
+        -1, index_data[i],
+        "Variable value (index) of OP(index_sample_grad) "
+        "expected >= 0 and < %ld, but got %ld. Please check input "
+        "value.",
+        value_length, index_data[i]);
+    PADDLE_ENFORCE_LT(
+        index_data[i], value_length,
+        "Variable value (index) of OP(index_sample_grad) "
+        "expected >= 0 and < %ld, but got %ld. Please check input "
+        "value.",
+        value_length, index_data[i]);
+    int v_i = b * value_length + static_cast<int>(index_data[i]);
+    x_grad_data[v_i] += out_grad_data[i];
+  }
+}
 
 template <typename DeviceContext, typename T>
 class IndexSampleGradKernel : public framework::OpKernel<T> {
@@ -111,18 +157,6 @@ class IndexSampleGradKernel : public framework::OpKernel<T> {
     auto &out_grad_tensor = out_grad_var->Get<LoDTensor>();
     auto *x_grad_tensor = x_grad_var->GetMutable<framework::LoDTensor>();
 
-    auto index_dims = index_tensor.dims();
-    auto out_grad_dims = out_grad_tensor.dims();
-    auto x_grad_dims = x_grad_tensor.dims();
-
-    int batch_size = x_grad_dims[0];
-    auto value_length = x_grad_dims[1];
-    auto index_length = index_dims[1];
-    int index_ids_num = index_tensor.numel();
-
-    auto *x_grad_data = x_grad_tensor.data<T>();
-    auto *out_grad_data = out_grad_tensor.data<T>();
-
     const auto &index_type = index_tensor.type();
     bool index_type_match = index_type == framework::proto::VarType::INT32 ||
                             index_type == framework::proto::VarType::INT64;
@@ -135,28 +169,12 @@ class IndexSampleGradKernel : public framework::OpKernel<T> {
                               framework::proto::VarType::INT32),
                           paddle::framework::DataTypeToString(
                               framework::proto::VarType::INT64)));
-    auto *index_data = index_type == framework::proto::VarType::INT32
-                           ? index_tensor.data<int>()
-                           : index_tensor.data<int64_t>();
-
-    memset(x_grad_data, 0, batch_size * value_length * sizeof(T));
-
-    for (int i = 0; i < index_ids_num, i++) {
-      int b = floor(i / index_length);
-      PADDLE_ENFORCE_LT(
-          -1, index_data[i],
-          "Variable value (index) of OP(index_sample_grad) "
-          "expected >= 0 and < %ld, but got %ld. Please check input "
-          "value.",
-          value_length, index_data[i]);
-      PADDLE_ENFORCE_LT(
-          index_data[i], value_length,
-          "Variable value (index) of OP(index_sample_grad) "
-          "expected >= 0 and < %ld, but got %ld. Please check input "
-          "value.",
-          value_length, index_data[i]);
-      int v_i = b * value_length + static_cast<int>(index_data[i]);
-      x_grad_data[v_i] += out_grad_data[i];
+    if (index_type == framework::proto::VarType::INT32) {
+      IndexSampleGradInner<T, int>(context, out_grad_tensor, index_tensor,
+                                   x_grad_tensor);
+    } else if (index_type == framework::proto::VarType::INT64) {
+      IndexSampleGradInner<T, int64_t>(context, out_grad_tensor, index_tensor,
+                                       x_grad_tensor);
     }
   }
 };
