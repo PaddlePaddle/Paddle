104057ed5905d1f42144f5157dd65b116b5a2a4e
gfwm2013 <2449465946@qq.com>
2020-03-25 06:34:47 +0000

    Add new API : randn
    test=develop

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -24,7 +24,7 @@ import os
 import inspect
 from ..layer_helper import LayerHelper
 from ..initializer import Normal, Constant, NumpyArrayInitializer
-from ..framework import Variable, OpProtoHolder, in_dygraph_mode, dygraph_only, _dygraph_tracer, default_main_program
+from ..framework import Variable, OpProtoHolder, in_dygraph_mode, dygraph_only, _dygraph_tracer, default_main_program, device_guard
 from .. import dygraph_utils
 from ..param_attr import ParamAttr
 from .layer_function_generator import autodoc, templatedoc, _generate_doc_string_
@@ -36,6 +36,7 @@ from .. import core
 from ..data_feeder import convert_dtype, check_variable_and_dtype, check_type, check_dtype
 
 __all__ = [
+    'randn',
     'fc',
     'embedding',
     'linear_chain_crf',
@@ -9582,13 +9583,18 @@ def uniform_random_batch_size_like(input,
 
 
 @templatedoc()
-def gaussian_random(shape, mean=0.0, std=1.0, seed=0, dtype='float32'):
+def gaussian_random(shape, out=None, mean=0.0, std=1.0, seed=0,
+                    dtype='float32'):
     """
     Generate a random tensor whose data is drawn from a Gaussian distribution.
 
     Args:
         shape (Tuple[int] | List[int]): Shape of the generated random tensor.
         
+        out(Variable, optional): Optional output which can be any created Variable that meets
+            the requirements to store the result of operation. If the out is `None`, a new Variable
+            wiil be create to store the result. Default is None.
+
         mean (float): Mean of the random tensor, defaults to 0.0.
             
         std (float): Standard deviation of the random tensor, defaults to 1.0.
@@ -9638,7 +9644,10 @@ def gaussian_random(shape, mean=0.0, std=1.0, seed=0, dtype='float32'):
     """
 
     helper = LayerHelper('gaussian_random', **locals())
-    out = helper.create_variable_for_type_inference(dtype)
+    check_type(shape, 'shape', (list, tuple), 'gaussian_random')
+
+    if out is None:
+        out = helper.create_variable_for_type_inference(dtype)
     c_dtype = convert_np_dtype_to_dtype_(dtype)
     helper.append_op(
         type='gaussian_random',
@@ -13931,3 +13940,68 @@ def uniform_random(shape, dtype='float32', min=-1.0, max=1.0, seed=0):
         outputs={"Out": out})
 
     return helper.append_activation(out)
+
+
+def randn(shape,
+          out=None,
+          dtype=None,
+          device=None,
+          stop_gradient=True,
+          name=None):
+    """
+    This function returns a tensor filled with random numbers from a normal distribution with mean 0 and variance 1 (also called the standard normal
+    distribution).
+
+    Args:
+        shape(list|tuple): Shape of the generated random tensor.
+        out(Variable, optional): Optional output which can be any created Variable that meets the requirements to store the result of operation. If
+            the out is `None`, a new Variable wiil be create to store the result. Default is None.
+        dtype(np.dtype|core.VarDesc.VarType|str, optional): Data type of the output tensor, which can be float16, float32, float64, int32, int64, bool.
+            if dtype is `None`, the data type of output tensor is `float32`. Default is None.
+        device(str, optional): This parameter specifies that the Tensor is created on the GPU or CPU. Default is None.
+        stop_gradient(bool, optional): Indicating if we stop gradient from current(out) Variable. Default is None.
+        name(str, optional): Normally there is no need for user to set this property. For more information, please refer to :refer:`api_guide_Name` .
+            Default is None.
+
+    Returns:
+        Random tensor whose data is drawn from a Gaussian distribution, dtype: flaot32 or float64 as specified.
+
+    Return type:
+        Variable
+
+    Examples:
+        .. code-block:: python
+
+            import paddle.fluid as fluid
+
+            data = fluid.tensor.randn([2, 4])
+            exe = fluid.Executor()
+            res = exe.run(default_main_program, feed={}, fetch_list=[data])
+            print(res[0])
+            # array([[2.3060477 , 2.676496  , 3.9911983 , 0.9990833 ],
+            #        [2.8675377 , 2.2279181 , 0.79029655, 2.8447366 ]], dtype=float32)
+
+    """
+    helper = LayerHelper("randn", **locals())
+    check_type(shape, 'shape', (list, tuple), 'randn')
+    assert len(shape) > 0, ("The size of argument(shape) can't be zero.")
+
+    if dtype is None:
+        dtype = 'float32'
+
+    check_dtype(dtype, 'create data type', ['float32', 'float64'], 'randn')
+
+    if out is None:
+        out = helper.create_variable_for_type_inference(dtype=dtype)
+
+    out.stop_gradient = stop_gradient
+
+    seed = np.random.randint(0, 100)
+
+    if device is None:
+        gaussian_random(shape=shape, out=out, seed=seed, dtype=dtype)
+    else:
+        with device_guard(device):
+            gaussian_random(shape=shape, out=out, seed=seed, dtype=dtype)
+
+    return out
---
+++ python/paddle/fluid/tests/unittests/test_randn_op.py
@@ -0,0 +1,110 @@
+#   Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import unittest
+import numpy as np
+
+import paddle.fluid.core as core
+from paddle.fluid.op import Operator
+import paddle.fluid as fluid
+from paddle.fluid import compiler, Program, program_guard
+
+
+class TestRandnOp(unittest.TestCase):
+    def test_api(self):
+        x1 = fluid.layers.randn(shape=[1000, 784], dtype='float32')
+        x2 = fluid.layers.randn(shape=[1000, 784], dtype='float64')
+        x3 = fluid.layers.fill_constant(
+            shape=[1000, 784], dtype='float32', value=0)
+        fluid.layers.randn(shape=[1000, 784], out=x3, dtype='float32')
+        x4 = fluid.layers.randn(
+            shape=[1000, 784], dtype='float32', device='cpu')
+        x5 = fluid.layers.randn(
+            shape=[1000, 784], dtype='float32', device='gpu')
+        x6 = fluid.layers.randn(
+            shape=[1000, 784],
+            dtype='float32',
+            device='gpu',
+            stop_gradient=False)
+
+        exe = fluid.Executor(fluid.CUDAPlace(1))
+        res = exe.run(fluid.default_main_program(),
+                      feed={},
+                      fetch_list=[x1, x2, x3, x4, x5, x6])
+
+        self.assertAlmostEqual(np.mean(res[0]), .0, delta=0.1)
+        self.assertAlmostEqual(np.std(res[0]), 1., delta=0.1)
+        self.assertAlmostEqual(np.mean(res[1]), .0, delta=0.1)
+        self.assertAlmostEqual(np.std(res[1]), 1., delta=0.1)
+        self.assertAlmostEqual(np.mean(res[2]), .0, delta=0.1)
+        self.assertAlmostEqual(np.std(res[2]), 1., delta=0.1)
+        self.assertAlmostEqual(np.mean(res[3]), .0, delta=0.1)
+        self.assertAlmostEqual(np.std(res[3]), 1., delta=0.1)
+        self.assertAlmostEqual(np.mean(res[4]), .0, delta=0.1)
+        self.assertAlmostEqual(np.std(res[4]), 1., delta=0.1)
+        self.assertAlmostEqual(np.mean(res[5]), .0, delta=0.1)
+        self.assertAlmostEqual(np.std(res[5]), 1., delta=0.1)
+
+
+class TestRandnOpError(unittest.TestCase):
+    def test_error(self):
+        with program_guard(Program(), Program()):
+
+            # The argument shape's size of randn_op should not be 0.
+            def test_shape_size():
+                out = fluid.layers.randn(shape=[])
+
+            self.assertRaises(AssertionError, test_shape_size)
+
+            # The argument shape's type of randn_op should be list or tuple.
+            def test_shape_type():
+                out = fluid.layers.randn(shape=1)
+
+            self.assertRaises(TypeError, test_shape_type)
+
+            # The argument dtype of randn_op should be float32 or float64.
+            def test_dtype_float16():
+                out = fluid.layers.randn(shape=[1, 2], dtype='float16')
+
+            self.assertRaises(TypeError, test_dtype_float16)
+
+            # The argument dtype of randn_op should be float32 or float64.
+            def test_dtype_int32():
+                out = fluid.layers.randn(shape=[1, 2], dtype='int32')
+
+            self.assertRaises(TypeError, test_dtype_int32)
+
+            # The argument dtype of randn_op should be float32 or float64.
+            def test_dtype_int64():
+                out = fluid.layers.randn(shape=[1, 2], dtype='int64')
+
+            self.assertRaises(TypeError, test_dtype_int64)
+
+            # The argument dtype of randn_op should be float32 or float64.
+            def test_dtype_uint8():
+                out = fluid.layers.randn(shape=[1, 2], dtype='uint8')
+
+            self.assertRaises(TypeError, test_dtype_uint8)
+
+            # The argument dtype of randn_op should be float32 or float64.
+            def test_dtype_bool():
+                out = fluid.layers.randn(shape=[1, 2], dtype='bool')
+
+            self.assertRaises(TypeError, test_dtype_bool)
+
+
+if __name__ == "__main__":
+    unittest.main()
