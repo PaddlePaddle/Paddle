23b9afc886ff214b5dbcf5df9b0e2230d623b3a9
Aurelius84 <liujiezhangbupt@gmail.com>
2020-03-27 16:25:46 +0800

    add TODO test=develop

--- python/paddle/fluid/dygraph/dygraph_to_static/ifelse_transformer.py
+++ python/paddle/fluid/dygraph/dygraph_to_static/ifelse_transformer.py
@@ -147,6 +147,8 @@ def is_candidate_node(node):
     """
     is_compare_node = isinstance(node,
                                  (gast.Compare, gast.BoolOp, gast.UnaryOp))
+    # TODO(Aurelius84): `.numpy()` may be an customized function,
+    # and should consider a more elegant way to solve this problem。
     has_numpy_attr = ".numpy()" in ast_to_source_code(node)
     return is_compare_node or has_numpy_attr
 
