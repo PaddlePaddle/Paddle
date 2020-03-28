3d3003ba95a97add45d5deeb1475fa64925dbd32
gongweibao <weibao.gong@gmail.com>
2020-03-28 07:58:31 +0000

    fix testlaunch test=develop

--- python/paddle/fluid/tests/unittests/test_command_line_flags.sh
+++ python/paddle/fluid/tests/unittests/test_command_line_flags.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 python -c "import paddle.fluid;print(\"test command line flag ok now!\")"
-python -m paddle.distributed.launch dummy.py 
+
+if [[ "${WITH_GPU}"x == "ON"x && "${WITH_DISTRIBUTE}"x == "ON"x ]]; then
+    python -m paddle.distributed.launch dummy.py 
+fi
