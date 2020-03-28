52b97694ced662c6dcdc09bac1ab660d02029b7e
gongweibao <weibao.gong@gmail.com>
2020-03-28 08:08:10 +0000

    fix with distribute test=develop

--- python/paddle/fluid/tests/unittests/test_command_line_flags.sh
+++ python/paddle/fluid/tests/unittests/test_command_line_flags.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 python -c "import paddle.fluid;print(\"test command line flag ok now!\")"
 
+echo "WITH_GPU:${WITH_GPU}"
+echo "WITH_DISTRIBUTE:${WITH_DISTRIBUTE}"
+
 if [[ "${WITH_GPU}"x == "ON"x && "${WITH_DISTRIBUTE}"x == "ON"x ]]; then
     python -m paddle.distributed.launch dummy.py 
 fi
