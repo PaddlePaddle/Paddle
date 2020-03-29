223d127dc493d8e70b33e2a757a4a682b10a8343
gongweibao <weibao.gong@gmail.com>
2020-03-29 12:15:05 +0000

    fix

--- python/paddle/fluid/tests/unittests/test_command_line_flags.sh
+++ python/paddle/fluid/tests/unittests/test_command_line_flags.sh
@@ -4,6 +4,5 @@ python -c "import paddle.fluid;print(\"test command line flag ok now!\")"
 echo "WITH_GPU:${WITH_GPU}"
 echo "WITH_DISTRIBUTE:${WITH_DISTRIBUTE}"
 
-if [[ "${WITH_GPU}"x == "ON"x && "${WITH_DISTRIBUTE}"x == "ON"x ]]; then
-    python -m paddle.distributed.launch dummy.py 
-fi
+echo "import paddle.fluid as fluid" > ../paddle_mod_dummy.py
+python -m paddle.fluid.tests.paddle_mod_dummy
