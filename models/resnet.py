7ac1c7649fc963aaafa57c48063ad0685a22a749
LielinJiang <jianglielin@baidu.com>
2020-03-30 02:37:59 +0000

    remove noused code

--- models/resnet.py
+++ models/resnet.py
@@ -48,47 +48,6 @@ class ConvBNLayer(fluid.dygraph.Layer):
         return x
 
 
-class BasicBlock(fluid.dygraph.Layer):
-    expansion = 1
-
-    def __init__(self, num_channels, num_filters, stride, shortcut=True):
-        super(BasicBlock, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError(
-                'BasicBlock only supports groups=1 and base_width=64')
-        if dilation > 1:
-            raise NotImplementedError(
-                "Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
 class BottleneckBlock(fluid.dygraph.Layer):
     def __init__(self, num_channels, num_filters, stride, shortcut=True):
         super(BottleneckBlock, self).__init__()
