4e8683f51e00e28f1e91e31cb6fd887cfc3109c5
JepsonWong <2013000149@qq.com>
2020-03-31 19:31:14 +0000

    add instance_norm op for dygraph, test=develop

--- python/paddle/nn/__init__.py
+++ python/paddle/nn/__init__.py
@@ -16,6 +16,11 @@
 # including layers, linear, conv, rnn etc.
 # __all__ = [ ]
 
+from .layer import norm
+
+__all__ = []
+__all__ += norm.__all__
+
 # TODO: define alias in nn directory
 # from .clip import ErrorClipByValue   #DEFINE_ALIAS
 # from .clip import GradientClipByGlobalNorm   #DEFINE_ALIAS
@@ -70,6 +75,7 @@
 # from .layer.norm import BatchNorm   #DEFINE_ALIAS
 # from .layer.norm import GroupNorm   #DEFINE_ALIAS
 # from .layer.norm import LayerNorm   #DEFINE_ALIAS
+from .layer.norm import InstanceNorm  #DEFINE_ALIAS
 # from .layer.norm import SpectralNorm   #DEFINE_ALIAS
 # from .layer.activation import PReLU   #DEFINE_ALIAS
 # from .layer.activation import ReLU   #DEFINE_ALIAS
