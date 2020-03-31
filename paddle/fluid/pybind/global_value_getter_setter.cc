9e068223293149f0c0f2a2a6b0e4b45412a3f15b
gfwm2013 <2449465946@qq.com>
2020-03-31 06:52:51 +0000

    Add a function to update FLAGS
    test=develop

--- paddle/fluid/pybind/global_value_getter_setter.cc
+++ paddle/fluid/pybind/global_value_getter_setter.cc
@@ -344,7 +344,7 @@ static void RegisterGlobalVarGetterSetter() {
       FLAGS_fuse_parameter_memory_size, FLAGS_init_allocated_mem,
       FLAGS_initial_cpu_memory_in_mb, FLAGS_memory_fraction_of_eager_deletion,
       FLAGS_use_pinned_memory, FLAGS_benchmark, FLAGS_inner_op_parallelism,
-      FLAGS_tracer_profile_fname);
+      FLAGS_tracer_profile_fname, FLAGS_paddle_num_threads);
 
 #ifdef PADDLE_WITH_CUDA
   REGISTER_PUBLIC_GLOBAL_VAR(
