8858958a9e9f1a7cf793ecbcbdc099ab0526eff0
chulutao <chulutao@baidu.com>
2020-04-01 18:40:27 +0800

    1. add gpu and dygraph unittests.\n 2. Performance optimization: scale op is not called when alpha=1. \n 3. op error message optimization.

--- python/paddle/common_ops_import.py
+++ python/paddle/common_ops_import.py
@@ -17,11 +17,11 @@ common import
 from six.moves import reduce
 from paddle.fluid.layer_helper import LayerHelper
 from paddle.fluid.param_attr import ParamAttr
-from paddle.fluid.framework import convert_np_dtype_to_dtype_, in_dygraph_mode, _varbase_creator
+from paddle.fluid.framework import convert_np_dtype_to_dtype_, OpProtoHolder, in_dygraph_mode, dygraph_only, _dygraph_tracer, default_main_program, _varbase_creator
 from paddle.fluid.framework import Variable
 from paddle.fluid.initializer import Constant
 from paddle.fluid.core import VarDesc
-from paddle.fluid import core
+from paddle.fluid import core, dygraph_utils
 from paddle.fluid.data_feeder import convert_dtype, check_variable_and_dtype, check_type, check_dtype
 from paddle.fluid.layers import utils, scale
 import numpy
