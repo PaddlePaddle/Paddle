761efd1cff024b3ce5b4771cbf43c6b54540e3ff
zhangting2020 <709968123@qq.com>
2020-04-01 08:52:21 +0000

    add code comments, test=develop

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -71,7 +71,7 @@ if(WITH_GPU)
     add_definitions(-DEIGEN_USE_GPU)
     # The compiler fully support const expressions since c++14,
     # but Eigen use some const expressions such as std::max and std::min, which are not supported in c++11
-    # set EIGEN_HAS_CONSTEXPR=0 to avoid compilation error in c++11
+    # use following definition to set EIGEN_HAS_CONSTEXPR=0 to avoid compilation error in c++11
     add_definitions(-DEIGEN_MAX_CPP_VER=11)
 
     FIND_PACKAGE(CUDA REQUIRED)
