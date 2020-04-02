f55eaba86e5a78ee129105271411c071a6cb78f1
zhongpu <2013000149@qq.com>
2020-04-02 11:24:00 +0800

    Revert "Exhaustive search (#22821)", test=develop
    This reverts commit 48144e40995e5ecc77a9649e4f2e4f180249ce2d.

--- paddle/fluid/operators/elementwise/test_elementwise_mul_op_dim.cc
+++ paddle/fluid/operators/elementwise/test_elementwise_mul_op_dim.cc
@@ -58,7 +58,8 @@ void MainTest(const TestData& test_data) {
 
   RuntimeContext runtime_ctx =
       RuntimeContext(op->Inputs(), op->Outputs(), scope);
-  ExecutionContext ctx = ExecutionContext(*op, scope, *dev_ctx, runtime_ctx);
+  ExecutionContext ctx =
+      ExecutionContext(*op, scope, *dev_ctx, runtime_ctx, nullptr);
   bool result = ElementwiseMulOp::AreDimsAndFormatCorrect(
       ctx, 16, MKLDNNMemoryFormat::nChw16c);
   if (test_data.supposed_to_fail)
