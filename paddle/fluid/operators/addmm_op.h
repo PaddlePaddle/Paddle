fc8a74d933228da7b4ba1577b4e49e7b66711a39
littletomatodonkey <dazhiningsibuqu@163.com>
2020-04-03 11:50:25 +0000

    fix bug, test=develop

--- paddle/fluid/operators/addmm_op.h
+++ paddle/fluid/operators/addmm_op.h
@@ -28,8 +28,6 @@ namespace paddle {
 namespace operators {
 
 using Tensor = framework::Tensor;
-template <typename T, int MajorType = Eigen::RowMajor,
-          typename IndexType = Eigen::DenseIndex>
 template <typename T, size_t D, int MajorType = Eigen::RowMajor,
           typename IndexType = Eigen::DenseIndex>
 using EigenTensor = framework::EigenTensor<T, D, MajorType, IndexType>;
