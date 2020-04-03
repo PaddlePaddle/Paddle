4770149e304597e9fa6c99f6181e9da6128e64ae
Aurelius84 <liujiezhangbupt@gmail.com>
2020-04-03 14:40:40 +0800

    API/OP (append_backward) error message enhancement test=develop

--- python/paddle/fluid/tests/unittests/test_backward.py
+++ python/paddle/fluid/tests/unittests/test_backward.py
@@ -287,6 +287,44 @@ class TestSimpleNetWithErrorNoGradSet(TestBackward):
             self._check_error_no_grad_set(self.net, [test, "test", 3])
 
 
+class TestAppendBackwardWithError(unittest.TestCase):
+    def build_net(self):
+        x = fluid.data(name='x', shape=[None, 13], dtype='int64')
+        y = fluid.data(name='y', shape=[None, 1], dtype='float32')
+        x_emb = fluid.embedding(x, size=[100, 256])
+        y_predict = fluid.layers.fc(input=x_emb, size=1, name='my_fc')
+        loss = fluid.layers.square_error_cost(input=y_predict, label=y)
+        avg_loss = fluid.layers.mean(loss)
+        param_names = [
+            param.name
+            for param in fluid.default_main_program().block(0).all_parameters()
+        ]
+
+        return avg_loss, param_names
+
+    def setUp(self):
+        self.avg_loss, self.param_names = self.build_net()
+
+    def test_loss_type_error(self):
+        with self.assertRaises(TypeError):
+            fluid.backward.append_backward(loss=self.avg_loss.name)
+
+    def test_parameter_list_type_error(self):
+        with self.assertRaises(TypeError):
+            self.param_names[0] = np.random.random([10])
+            fluid.backward.append_backward(
+                loss=self.avg_loss, parameter_list=self.param_names)
+
+    def test_callback_type_error(self):
+        with self.assertRaises(TypeError):
+
+            def callback(block, context):
+                return
+
+            fluid.backward.append_backward(
+                loss=self.avg_loss, callbacks=callback)
+
+
 # TODO(Aurelius84): add conditional network test
 class ConditionalNet(BackwardNet):
     def __init__(self):
