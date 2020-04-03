d380712254707d747e6b2c127026badd77b1d872
sneaxiy <sneaxiy@126.com>
2020-04-03 09:36:00 +0800

    test xly coverage, test=develop

--- paddle/fluid/imperative/layer.cc
+++ paddle/fluid/imperative/layer.cc
@@ -28,7 +28,6 @@
 #include "paddle/fluid/platform/device_context.h"
 #include "paddle/fluid/platform/enforce.h"
 #include "paddle/fluid/platform/profiler.h"
-
 namespace paddle {
 namespace imperative {
 
