994d5b9377335a4aa35001469112cfd599d4d953
LutaoChu <30695251+LutaoChu@users.noreply.github.com>
2020-04-04 22:11:53 +0800

    Add 2 new ops: paddle.tensor.div() and paddle.tensor.add() to API2.0(#23352)
    * add new op paddle.tensor.div(x, y, out=None, name=None) 
    * add gpu and dygraph unittests.
    * Performance optimization: scale op is not called when alpha=1. 
    * op error message optimization.

--- python/paddle/tensor/__init__.py
+++ python/paddle/tensor/__init__.py
@@ -109,8 +109,8 @@ from .math import tanh  #DEFINE_ALIAS
 # from .math import max   #DEFINE_ALIAS
 # from .math import min   #DEFINE_ALIAS
 # from .math import mm   #DEFINE_ALIAS
-# from .math import div   #DEFINE_ALIAS
-# from .math import add   #DEFINE_ALIAS
+from .math import div  #DEFINE_ALIAS
+from .math import add  #DEFINE_ALIAS
 # from .math import atan   #DEFINE_ALIAS
 # from .math import logsumexp   #DEFINE_ALIAS
 # from .math import inverse   #DEFINE_ALIAS
