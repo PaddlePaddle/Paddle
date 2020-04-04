fc6cfcebe7f87696c8f6e172d83a87ceed148885
littletomatodonkey <dazhiningsibuqu@163.com>
2020-04-04 19:32:33 +0000

    fix init docs, test=develop

--- python/paddle/fluid/regularizer.py
+++ python/paddle/fluid/regularizer.py
@@ -134,8 +134,8 @@ class L2DecayRegularizer(WeightDecayRegularizer):
             main_prog = fluid.Program()
             startup_prog = fluid.Program()
             with fluid.program_guard(main_prog, startup_prog):
-                data = fluid.layers.data(name='image', shape=[3, 28, 28], dtype='float32')
-                label = fluid.layers.data(name='label', shape=[1], dtype='int64')
+                data = fluid.data(name='image', shape=[256, 3, 28, 28], dtype='float32')
+                label = fluid.data(name='label', shape=[256, 1], dtype='int64')
                 hidden = fluid.layers.fc(input=data, size=128, act='relu')
                 prediction = fluid.layers.fc(input=hidden, size=10, act='softmax')
                 loss = fluid.layers.cross_entropy(input=prediction, label=label)
@@ -213,8 +213,8 @@ class L1DecayRegularizer(WeightDecayRegularizer):
             main_prog = fluid.Program()
             startup_prog = fluid.Program()
             with fluid.program_guard(main_prog, startup_prog):
-                data = fluid.layers.data(name='image', shape=[3, 28, 28], dtype='float32')
-                label = fluid.layers.data(name='label', shape=[1], dtype='int64')
+                data = fluid.data(name='image', shape=[256, 3, 28, 28], dtype='float32')
+                label = fluid.data(name='label', shape=[256, 1], dtype='int64')
                 hidden = fluid.layers.fc(input=data, size=128, act='relu')
                 prediction = fluid.layers.fc(input=hidden, size=10, act='softmax')
                 loss = fluid.layers.cross_entropy(input=prediction, label=label)
