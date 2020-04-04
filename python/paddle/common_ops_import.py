f5e45ec565bb15de1a4e85de204af3799ecc2676
mamingjie-China <mamingjie@baidu.com>
2020-04-04 11:42:00 +0800

    add a new op paddle.sort(), test=develop

--- python/paddle/common_ops_import.py
+++ python/paddle/common_ops_import.py
@@ -11,18 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from six.moves import reduce
 from paddle.fluid.layer_helper import LayerHelper
 from paddle.fluid.param_attr import ParamAttr
 from paddle.fluid.framework import convert_np_dtype_to_dtype_, in_dygraph_mode, _varbase_creator
-from paddle.fluid.framework import OpProtoHolder, dygraph_only, default_main_program, _dygraph_tracer
-from paddle.fluid.framework import Variable, device_guard
+from paddle.fluid.framework import device_guard, default_main_program, dygraph_only, _dygraph_tracer
+from paddle.fluid.framework import OpProtoHolder, Variable
 from paddle.fluid.initializer import Constant
 from paddle.fluid.core import VarDesc
 from paddle.fluid import core, dygraph_utils
-from paddle.fluid.data_feeder import check_type, check_dtype, convert_dtype, check_variable_and_dtype
-from paddle.fluid.layers import utils, scale, fill_constant
+from paddle.fluid.data_feeder import check_type, check_dtype, check_variable_and_dtype, convert_dtype
+from paddle.fluid.layers import fill_constant, utils, scale
 from paddle.fluid.layers.layer_function_generator import templatedoc
 import paddle.fluid as fluid
 import numpy
