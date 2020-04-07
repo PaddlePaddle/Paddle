a68ed92e60b3b9b4a0d38e7f5798af79a181d796
jingqinghe <jingqinghe@baidu.com>
2020-04-07 16:32:55 +0800

    add bmm op and move serval ops from fluid.layers to tensor test=develop

--- python/paddle/tensor/linalg.py
+++ python/paddle/tensor/linalg.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from paddle.common_ops_import import *
-
 from __future__ import print_function
 
 from paddle.common_ops_import import *
