b11340a43770abd720f815a562ceee492426a80f
songyouwei <youwei0314@gmail.com>
2020-04-09 18:50:21 +0800

    support Layer level train/eval mode (#22463)
    * Layer add training attr, add Dropout Layer
    
    * add unit test for Dropout
    test=develop
    
    * minor fix
    test=develop
    
    * add missing args
    test=develop
    
    * support global flag in dropout, add docs
    test=develop
    
    * minor fix
    test=develop
    
    * minor fix
    test=develop
    
    * refine code comments
    test=develop
    
    * refine Dropout
    test=develop
    
    * fix ut
    test=develop
    
    * arg expansion
    test=develop
    
    * sample code update
    test=develop
    
    * prop -> p
    test=develop
    
    * fix ut
    test=develop
    
    * minor check fix
    test=develop

--- python/paddle/fluid/tests/unittests/test_layers.py
+++ python/paddle/fluid/tests/unittests/test_layers.py
@@ -106,6 +106,33 @@ class TestLayer(LayerTest):
             ret = custom(x, do_linear2=True)
             self.assertTrue(np.array_equal(ret.numpy().shape, [3, 1]))
 
+    def test_dropout(self):
+        inp = np.ones([3, 32, 32], dtype='float32')
+        with self.static_graph():
+            t = layers.data(
+                name='data',
+                shape=[3, 32, 32],
+                dtype='float32',
+                append_batch_size=False)
+            dropout = nn.Dropout(p=0.35, seed=1, is_test=False)
+            ret = dropout(t)
+            ret2 = fluid.layers.dropout(
+                t, dropout_prob=0.35, seed=1, is_test=False)
+            static_ret, static_ret2 = self.get_static_graph_result(
+                feed={'data': inp}, fetch_list=[ret, ret2])
+        with self.dynamic_graph():
+            t = base.to_variable(inp)
+            dropout = nn.Dropout(p=0.35, seed=1, is_test=False)
+            dy_ret = dropout(t)
+            dy_ret2 = fluid.layers.dropout(
+                t, dropout_prob=0.35, seed=1, is_test=False)
+            dy_ret_value = dy_ret.numpy()
+            dy_ret2_value = dy_ret2.numpy()
+
+        self.assertTrue(np.array_equal(static_ret, static_ret2))
+        self.assertTrue(np.array_equal(dy_ret_value, dy_ret2_value))
+        self.assertTrue(np.array_equal(static_ret, dy_ret_value))
+
     def test_linear(self):
         inp = np.ones([3, 32, 32], dtype='float32')
         with self.static_graph():
