69806734019b3b2c5f74c30f07c2d97ac90f6591
mamingjie <mamingjie@baidu.com>
2020-04-10 16:07:38 +0000

    API(argsort, argmax, argmin, cast, diag) error message enhancement, test=develop

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -1377,8 +1377,13 @@ def diag(diagonal):
           # diagonal.shape=(3,) data.shape=(3, 3)
 
     """
-    check_dtype(diagonal, 'diagonal', ['float32', 'float64', 'int32', 'int64'],
-                'diag')
+    if not isinstance(diagonal, Variable):
+        check_dtype(diagonal, 'diagonal',
+                    ['float32', 'float64', 'int32', 'int64'], 'diag')
+    else:
+        check_variable_and_dtype(diagonal, 'diagonal',
+                                 ['float32', 'float64', 'int32', 'int64'],
+                                 'diag')
     helper = LayerHelper("diag", **locals())
 
     if not isinstance(diagonal, Variable):
