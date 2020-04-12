91868e425eb0d204b48211089b7938a00583312d
suytingwan <suytingwan@163.com>
2020-04-12 09:08:14 +0000

    test=develop fix test api doc

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -685,19 +685,30 @@ def meshgrid(x, name=None):
     x = fluid.data(name='x', shape=[100], dtype='int32')
     y = fluid.data(name='y', shape=[200], dtype='int32')
 
+    input_1 = np.random.randint(0, 100, [100, ]).astype('int32')
+    input_2 = np.random.randint(0, 100, [200, ]).astype('int32')
+
+    exe = fluid.Executor(place=fluid.CPUPlace())
     grid_x, grid_y = paddle.tensor.meshgrid([x, y])
+    res_1, res_2 = exe.run(fluid.default_main_program(),
+                           feed={'x': input_1,
+                                 'y': input_2},
+                           fetch_list=[grid_x, grid_y])
      
-    #the shape of grid_x is (100, 200)
-    #the shape of grid_y is (100, 200)
+    #the shape of res_1 is (100, 200)
+    #the shape of res_2 is (100, 200)
 
     #in dygraph mode
 
-    x = np.random.random([100, ]).astype('int32')
-    y = np.random.random([200, ]).astype('int32')
+    x = np.random.randint(0, 100, [100, ]).astype('int32')
+    y = np.random.randint(0, 100, [200, ]).astype('int32')
     with fluid.dygraph.guard():
         x = fluid.dygraph.to_variable(input_3)
         y = fluid.dygraph.to_variable(input_4)
         grid_x, grid_y = paddle.tensor.meshgrid([tensor_3, tensor_4])
+
+    #the shape of grid_x is (100, 200)
+    #the shape of grid_y is (100, 200)
 
     """
 
