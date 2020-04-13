8b183d0efce2d388cde814f32209f90350c3f095
MyPandaShaoxiang <txg4794@163.com>
2020-04-13 15:16:07 +0800

    fix: fix param_attr check_type test=develop

--- python/paddle/fluid/param_attr.py
+++ python/paddle/fluid/param_attr.py
@@ -78,7 +78,7 @@ class ParamAttr(object):
                  regularizer=None,
                  trainable=True,
                  do_model_average=True):
-        check_type(name, "name", (str, type(None), type(unicode)), "ParamAttr")
+        check_type(name, "name", (str, type(None), unicode), "ParamAttr")
         check_type(learning_rate, "learning_rate", (float, int), "ParamAttr")
         check_type(trainable, "trainable", (bool), "ParamAttr")
         check_type(do_model_average, "do_model_average", (bool), "ParamAttr")
