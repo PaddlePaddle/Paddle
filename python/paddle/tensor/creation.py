222a5137b381f50b840b400aba81366b1dd9d638
danleifeng <52735331+danleifeng@users.noreply.github.com>
2020-04-14 00:19:01 +0800

    Add new tensor in API2.0: max,min,t,eye,log1p (#23228)
    add new tensor: max,min,t,eye,log1p; test=develop

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -38,7 +38,7 @@ __all__ = [
     'zeros',
     'zeros_like',
     #            'arrange',
-    #            'eye',
+    'eye',
     'full',
     'full_like',
     'triu',
@@ -393,6 +393,66 @@ def zeros_like(input, dtype=None, device=None, name=None):
         attrs=attrs,
         outputs={'Out': [out]})
     out.stop_gradient = True
+    return out
+
+
+def eye(num_rows,
+        num_columns=None,
+        out=None,
+        dtype='float32',
+        stop_gradient=True,
+        name=None):
+    """
+    **eye**
+    This function constructs an identity tensor, or a batch of tensor.
+    Args:
+        num_rows(int): the number of rows in each batch tensor.
+        num_columns(int, optional): the number of columns in each batch tensor.
+                          If None, default: num_rows.
+        out(Variable, optional): Optional output which can be any created 
+            Variable that meets the requirements to store the result of operation.
+            if out is None, a new Varibale will be create to store the result.
+        dtype(string, optional): The data type of the returned tensor.
+                       It should be int32, int64, float16, float32, float64.
+        stop_gradient(bool, optional): Whether stop calculating gradients. Default:True.
+        name(str, optional): The default value is None.  Normally there is no need for 
+            user to set this property.  For more information, please refer to :ref:`api_guide_Name`
+    Returns:
+        Variable: An identity Tensor or LoDTensor of shape [num_rows, num_columns].
+    Examples:
+        .. code-block:: python
+          import paddle
+          data = paddle.eye(3, dtype='int32')
+          # [[1, 0, 0]
+          #  [0, 1, 0]
+          #  [0, 0, 1]]
+          data = paddle.eye(2, 3, dtype='int32')
+          # [[1, 0, 0]
+          #  [0, 1, 0]]
+    """
+
+    helper = LayerHelper("eye", **locals())
+    if not isinstance(num_rows, int) or num_rows < 0:
+        raise TypeError("num_rows should be a non-negative int")
+    if num_columns is not None:
+        if not isinstance(num_columns, int) or num_columns < 0:
+            raise TypeError("num_columns should be a non-negative int")
+    else:
+        num_columns = num_rows
+    if out is None:
+        out = helper.create_variable_for_type_inference(dtype=dtype)
+    c_dtype = convert_np_dtype_to_dtype_(dtype)
+    helper.append_op(
+        type='eye',
+        inputs={},
+        outputs={'Out': [out]},
+        attrs={
+            'num_rows': num_rows,
+            'num_columns': num_columns,
+            'dtype': c_dtype
+        },
+        stop_gradient=True)
+    out.stop_gradient = stop_gradient
     return out
 
 
