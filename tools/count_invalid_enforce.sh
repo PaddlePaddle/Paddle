fd5ab029948a7644cbde049a99ec9b36b824bb5b
Chen Weihang <chenweihang@baidu.com>
2020-04-13 11:09:05 +0000

    enrich count enforce scripts, test=develop, test=document_fix

--- tools/count_invalid_enforce.sh
+++ tools/count_invalid_enforce.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
-ALL_PADDLE_CHECK=`grep -r -zoE "(PADDLE_ENFORCE[A-Z_]*|PADDLE_THROW)\(.[^,\);]*.[^;]*\);\s" ../paddle/fluid || true`
-ALL_PADDLE_CHECK_CNT=`echo "$ALL_PADDLE_CHECK" | grep -cE "(PADDLE_ENFORCE|PADDLE_THROW)" || true`
-VALID_PADDLE_CHECK_CNT=`echo "$ALL_PADDLE_CHECK" | grep -zoE '(PADDLE_ENFORCE[A-Z_]*|PADDLE_THROW)\((.[^,;]+,)*.[^";]*(errors::).[^"]*".[^";]{20,}.[^;]*\);\s' | grep -cE "(PADDLE_ENFORCE|PADDLE_THROW)" || true`
+
+ROOT_DIR=../paddle/fluid
+ALL_PADDLE_CHECK_CNT=0
+VALID_PADDLE_CHECK_CNT=0
+
+function enforce_scan(){
+    paddle_check=`grep -r -zoE "(PADDLE_ENFORCE[A-Z_]*|PADDLE_THROW)\(.[^,\);]*.[^;]*\);\s" $1 || true`
+    total_check_cnt=`echo "$paddle_check" | grep -cE "(PADDLE_ENFORCE|PADDLE_THROW)" || true`
+    valid_check_cnt=`echo "$paddle_check" | grep -zoE '(PADDLE_ENFORCE[A-Z_]*|PADDLE_THROW)\((.[^,;]+,)*.[^";]*(errors::).[^"]*".[^";]{20,}.[^;]*\);\s' | grep -cE "(PADDLE_ENFORCE|PADDLE_THROW)" || true`
+    eval $2=$total_check_cnt
+    eval $3=$valid_check_cnt
+}
+
+function walk_dir(){
+    for file in `ls $1`
+    do
+        if [ -d $1"/"$file ];then
+            level=$(($2+1))
+            if [ $level -le 1 ]; then
+                enforce_scan $1"/"$file total_check_cnt valid_check_cnt
+                echo $1"/"$file" - total: ${total_check_cnt}, valid: ${valid_check_cnt}, invalid: $(($total_check_cnt-$valid_check_cnt))"
+                ALL_PADDLE_CHECK_CNT=$(($ALL_PADDLE_CHECK_CNT+$total_check_cnt))
+                VALID_PADDLE_CHECK_CNT=$(($VALID_PADDLE_CHECK_CNT+$valid_check_cnt))
+                walk_dir $1"/"$file $level
+            fi
+        fi
+    done
+}
+
+walk_dir $ROOT_DIR 0
 
 echo "----------------------------"
 echo "PADDLE ENFORCE & THROW COUNT"
