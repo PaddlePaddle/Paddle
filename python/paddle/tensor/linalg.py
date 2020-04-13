1083c3463bed601e8cb05d53de304dcc165595dd
danleifeng <danleifeng@163.com>
2020-04-13 10:58:20 +0800

    reserve other apis' blank line; test=develop

--- python/paddle/tensor/linalg.py
+++ python/paddle/tensor/linalg.py
@@ -33,25 +33,32 @@ __all__ = [
 def matmul(x, y, transpose_x=False, transpose_y=False, alpha=1.0, name=None):
     """
     Applies matrix multiplication to two tensors.
+
     Currently, the input tensors' rank can be any, but when the rank of any
     inputs is bigger than 3, this two inputs' rank should be equal.
+
     The actual behavior depends on the shapes of :math:`x`, :math:`y` and the
     flag values of :attr:`transpose_x`, :attr:`transpose_y`. Specifically:
+
     - If a transpose flag is specified, the last two dimensions of the tensor
       are transposed. If the tensor is rank-1 of shape :math:`[D]`, then for
       :math:`x` it is treated as :math:`[1, D]` in nontransposed form and as
       :math:`[D, 1]` in transposed form, whereas for :math:`y` it is the
       opposite: It is treated as :math:`[D, 1]` in nontransposed form and as
       :math:`[1, D]` in transposed form.
+
     - After transpose, the two tensors are 2-D or n-D and matrix multiplication
       performs in the following way.
+
       - If both are 2-D, they are multiplied like conventional matrices.
       - If either is n-D, it is treated as a stack of matrices residing in the
         last two dimensions and a batched matrix multiply supporting broadcast
         applies on the two tensors.
+
     Also note that if the raw tensor :math:`x` or :math:`y` is rank-1 and
     nontransposed, the prepended or appended dimension :math:`1` will be
     removed after matrix multiplication.
+
     Args:
         x (Variable): The input variable which is a Tensor or LoDTensor.
         y (Variable): The input variable which is a Tensor or LoDTensor.
@@ -60,25 +67,35 @@ def matmul(x, y, transpose_x=False, transpose_y=False, alpha=1.0, name=None):
         alpha (float): The scale of output. Default 1.0.
         name(str|None): A name for this layer(optional). If set None, the layer
             will be named automatically.
+
     Returns:
         Variable: The product Tensor (or LoDTensor) variable.
+
     Examples:
         .. code-block:: python
+
             # Examples to clarify shapes of the inputs and output
             # x: [B, ..., M, K], y: [B, ..., K, N]
             # paddle.matmul(x, y)  # out: [B, ..., M, N]
+
             # x: [B, M, K], y: [B, K, N]
             # paddle.matmul(x, y)  # out: [B, M, N]
+
             # x: [B, M, K], y: [K, N]
             # paddle.matmul(x, y)  # out: [B, M, N]
+
             # x: [M, K], y: [K, N]
             # paddle.matmul(x, y)  # out: [M, N]
+
             # x: [B, M, K], y: [K]
             # paddle.matmul(x, y)  # out: [B, M]
+
             # x: [K], y: [K]
             # paddle.matmul(x, y)  # out: [1]
+
             # x: [M], y: [N]
             # paddle.matmul(x, y, True, True)  # out: [M, N]
+
             import paddle.fluid as fluid
             x = fluid.data(name='x', shape=[2, 3], dtype='float32')
             y = fluid.data(name='y', shape=[3, 2], dtype='float32')
@@ -147,39 +164,60 @@ def dist(x, y, p=2):
     """
     This OP returns the p-norm of (x - y). It is not a norm in a strict sense, only as a measure
     of distance. The shapes of x and y must be broadcastable.
+
     Where, z = x - y,
+
     When p = 0, defining $0^0=0$, the zero-norm of z is simply the number of non-zero elements of z.
+
     .. math::
+
         ||z||_{0}=\lim_{p \\rightarrow 0}\sum_{i=1}^{m}|z_i|^{p}
+
     When p = inf, the inf-norm of z is the maximum element of z.
+
     .. math::
+
         ||z||_\infty=\max_i |z_i|
+
     When p = -inf, the negative-inf-norm of z is the minimum element of z.
+
     .. math::
+
         ||z||_{-\infty}=\min_i |z_i|
+
     Otherwise, the p-norm of z follows the formula,
+
     .. math::
+
         ||z||_{p}=(\sum_{i=1}^{m}|z_i|^p)^{\\frac{1}{p}}
+
     Args:
         x (Variable): 1-D to 6-D Tensor, its data type is float32 or float64.
         y (Variable): 1-D to 6-D Tensor, its data type is float32 or float64.
         p (float, optional): The norm to be computed, its data type is float32 or float64. Default: 2.
+
     Returns:
         Variable: Tensor that is the p-norm of (x - y).
+
     Examples:
         .. code-block:: python
+
             import paddle
             import paddle.fluid as fluid
             import numpy as np
+
             with fluid.dygraph.guard():
                 x = fluid.dygraph.to_variable(np.array([[3, 3],[3, 3]]).astype(np.float32))
                 y = fluid.dygraph.to_variable(np.array([[3, 3],[3, 1]]).astype(np.float32))
                 out = paddle.dist(x, y, 0)
                 print(out.numpy()) # out = [1.]
+
                 out = paddle.dist(x, y, 2)
                 print(out.numpy()) # out = [2.]
+
                 out = paddle.dist(x, y, float("inf"))
                 print(out.numpy()) # out = [2.]
+
                 out = paddle.dist(x, y, float("-inf"))
                 print(out.numpy()) # out = [0.]
     """
@@ -194,6 +232,55 @@ def dist(x, y, p=2):
     attrs = {"p": float(p)}
     helper.append_op(
         type='dist', inputs=inputs, outputs={'Out': out}, attrs=attrs)
+    return out
+
+
+def dot(x, y, name=None):
+    """
+    This operator calculates inner product for vectors.
+   
+    .. note::
+       Only support 1-d Tensor(vector).
+
+    Parameters:
+    
+        x(Variable): 1-D ``Tensor`` or ``LoDTensor``. Its datatype should be ``float32``, ``float64``, ``int32``, ``int64``
+        y(Variable): 1-D ``Tensor`` or ``LoDTensor``. Its datatype soulde be ``float32``, ``float64``, ``int32``, ``int64``
+        name(str, optional): Name of the output. Default is None. It's used to print debug info for developers. Details: :ref:`api_guide_Name`
+
+    Examples:
+
+    .. code-block:: python
+
+        import paddle
+        import paddle.fluid as fluid
+        import numpy as np
+        
+        with fluid.dygraph.guard():
+          x = fluid.dygraph.to_variable(np.random.uniform(0.1, 1, [10]).astype(np.float32))
+          y = fluid.dygraph.to_variable(np.random.uniform(1, 3, [10]).astype(np.float32))
+          z = paddle.dot(x, y)
+          print(z.numpy())
+
+    """
+    op_type = 'dot'
+    assert x is not None, 'x cannot be None in {}'.format(op_type)
+    assert y is not None, 'y cannot be None in {}'.format(op_type)
+
+    check_variable_and_dtype(x, 'x', ['float32', 'float64', 'int32', 'int64'],
+                             op_type)
+    check_variable_and_dtype(y, 'y', ['float32', 'float64', 'int32', 'int64'],
+                             op_type)
+
+    helper = LayerHelper(op_type, **locals())
+    if name is None:
+        out = helper.create_variable_for_type_inference(dtype=x.dtype)
+    else:
+        out = helper.create_variable(
+            name=name, dtype=x.dtype, persistable=False)
+    helper.append_op(
+        type="dot", inputs={'X': x,
+                            'Y': y}, attrs={}, outputs={"Out": out})
     return out
 
 
@@ -265,48 +352,4 @@ def t(input, name=None):
             outputs={'Out': [out],
                      'XShape': [input_shape]},
             attrs={'axis': [1, 0]})
-    return out
-
-
-def dot(x, y, name=None):
-    """
-    This operator calculates inner product for vectors.
-   
-    .. note::
-       Only support 1-d Tensor(vector).
-    Parameters:
-    
-        x(Variable): 1-D ``Tensor`` or ``LoDTensor``. Its datatype should be ``float32``, ``float64``, ``int32``, ``int64``
-        y(Variable): 1-D ``Tensor`` or ``LoDTensor``. Its datatype soulde be ``float32``, ``float64``, ``int32``, ``int64``
-        name(str, optional): Name of the output. Default is None. It's used to print debug info for developers. Details: :ref:`api_guide_Name`
-    Examples:
-    .. code-block:: python
-        import paddle
-        import paddle.fluid as fluid
-        import numpy as np
-        
-        with fluid.dygraph.guard():
-          x = fluid.dygraph.to_variable(np.random.uniform(0.1, 1, [10]).astype(np.float32))
-          y = fluid.dygraph.to_variable(np.random.uniform(1, 3, [10]).astype(np.float32))
-          z = paddle.dot(x, y)
-          print(z.numpy())
-    """
-    op_type = 'dot'
-    assert x is not None, 'x cannot be None in {}'.format(op_type)
-    assert y is not None, 'y cannot be None in {}'.format(op_type)
-
-    check_variable_and_dtype(x, 'x', ['float32', 'float64', 'int32', 'int64'],
-                             op_type)
-    check_variable_and_dtype(y, 'y', ['float32', 'float64', 'int32', 'int64'],
-                             op_type)
-
-    helper = LayerHelper(op_type, **locals())
-    if name is None:
-        out = helper.create_variable_for_type_inference(dtype=x.dtype)
-    else:
-        out = helper.create_variable(
-            name=name, dtype=x.dtype, persistable=False)
-    helper.append_op(
-        type="dot", inputs={'X': x,
-                            'Y': y}, attrs={}, outputs={"Out": out})
     return out
