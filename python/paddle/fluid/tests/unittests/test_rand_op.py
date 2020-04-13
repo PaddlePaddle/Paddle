89451c3f68bf82e2163fc4f4ccfba2d0b1ca431f
wuxing03 <wuxing03@baidu.com>
2020-04-13 03:40:55 +0000

    add more unittest code

--- python/paddle/fluid/tests/unittests/test_rand_op.py
+++ python/paddle/fluid/tests/unittests/test_rand_op.py
@@ -18,7 +18,7 @@ import numpy as np
 from op_test import OpTest
 
 import paddle.fluid.core as core
-from paddle import tensor
+from paddle import rand
 import paddle.fluid as fluid
 from paddle.fluid import compiler, Program, program_guard
 
@@ -32,16 +32,49 @@ class TestRandOpError(unittest.TestCase):
             def test_Variable():
                 x1 = fluid.create_lod_tensor(
                     np.zeros((4, 784)), [[1, 1, 1, 1]], fluid.CPUPlace())
-                tensor.rand(x1)
+                rand(x1)
 
             self.assertRaises(TypeError, test_Variable)
 
             def test_dtype():
                 dim_1 = fluid.layers.fill_constant([1], "int64", 3)
                 dim_2 = fluid.layers.fill_constant([1], "int32", 5)
-                tensor.rand(shape=[dim_1, dim_2], dtype='int32')
+                rand(shape=[dim_1, dim_2], dtype='int32')
 
             self.assertRaises(TypeError, test_dtype)
+
+            def test_device():
+                rand(shape=[3, 4], device='device')
+
+            self.assertRaises(ValueError, test_device)
+
+
+class TestRandOp(unittest.TestCase):
+    def test_run(self):
+        use_cuda = False
+        place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
+        exe = fluid.Executor(place)
+
+        train_program = fluid.Program()
+        startup_program = fluid.Program()
+        with fluid.program_guard(train_program, startup_program):
+            result_1 = rand(shape=[3, 4])
+            dim_1 = fluid.layers.fill_constant([1], "int64", 3)
+            dim_2 = fluid.layers.fill_constant([1], "int32", 5)
+            result_2 = rand(shape=[dim_1, dim_2])
+            var_shape = fluid.data(name='var_shape', shape=[2], dtype="int64")
+            result_3 = rand(var_shape)
+            var_shape_int32 = fluid.data(
+                name='var_shape_int32', shape=[2], dtype="int32")
+            result_4 = rand(var_shape_int32)
+        exe.run(startup_program)
+
+        x1 = np.array([3, 2]).astype('int64')
+        x2 = np.array([4, 3]).astype('int32')
+        ret = exe.run(train_program,
+                      feed={"var_shape": x1,
+                            "var_shape_int32": x2},
+                      fetch_list=[result_1, result_2, result_3, result_4])
 
 
 if __name__ == "__main__":
