4237f64c3a08b642a08d851dc01356f32aa0db18
jingqinghe <jingqinghe@baidu.com>
2020-04-13 13:13:14 +0800

    fix bug in reduce op test=develop

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -4268,14 +4268,16 @@ def reduce_sum(input, dim=None, keep_dim=False, name=None):
         dim = [dim]
 
     if in_dygraph_mode():
-        reduce_all = True if dim == None or dim == [] else False
+        reduce_all = True if dim == None or dim == [] or len(dim) == len(
+            input.shape) else False
         dim = dim if dim != None and dim != [] else [0]
         return core.ops.reduce_sum(input, 'dim', dim, 'keep_dim', keep_dim,
                                    'reduce_all', reduce_all)
     attrs = {
         'dim': dim if dim != None and dim != [] else [0],
         'keep_dim': keep_dim,
-        'reduce_all': True if dim == None or dim == [] else False
+        'reduce_all': True
+        if dim == None or dim == [] or len(dim) == len(input.shape) else False
     }
     check_variable_and_dtype(
         input, 'input', ['float32', 'float64', 'int32', 'int64'], 'reduce_sum')
@@ -4343,14 +4345,16 @@ def reduce_mean(input, dim=None, keep_dim=False, name=None):
         dim = [dim]
 
     if in_dygraph_mode():
-        reduce_all = True if dim == None or dim == [] else False
+        reduce_all = True if dim == None or dim == [] or len(dim) == len(
+            input.shape) else False
         dim = dim if dim != None and dim != [] else [0]
         return core.ops.reduce_mean(input, 'dim', dim, 'keep_dim', keep_dim,
                                     'reduce_all', reduce_all)
     attrs = {
         'dim': dim if dim != None and dim != [] else [0],
         'keep_dim': keep_dim,
-        'reduce_all': True if dim == None or dim == [] else False
+        'reduce_all': True
+        if dim == None or dim == [] or len(dim) == len(input.shape) else False
     }
     check_variable_and_dtype(
         input, 'input', ['float32', 'float64', 'int32', 'int64'], 'reduce_mean')
@@ -4420,7 +4424,8 @@ def reduce_max(input, dim=None, keep_dim=False, name=None):
         attrs={
             'dim': dim if dim != None and dim != [] else [0],
             'keep_dim': keep_dim,
-            'reduce_all': True if dim == None or dim == [] else False
+            'reduce_all': True if dim == None or dim == [] or
+            len(dim) == len(input.shape) else False
         })
     return out
 
@@ -4481,7 +4486,8 @@ def reduce_min(input, dim=None, keep_dim=False, name=None):
         attrs={
             'dim': dim if dim != None and dim != [] else [0],
             'keep_dim': keep_dim,
-            'reduce_all': True if dim == None or dim == [] else False
+            'reduce_all': True if dim == None or dim == [] or
+            len(dim) == len(input.shape) else False
         })
     return out
 
@@ -4543,7 +4549,8 @@ def reduce_prod(input, dim=None, keep_dim=False, name=None):
         attrs={
             'dim': dim if dim != None and dim != [] else [0],
             'keep_dim': keep_dim,
-            'reduce_all': True if dim == None or dim == [] else False
+            'reduce_all': True if dim == None or dim == [] or
+            len(dim) == len(input.shape) else False
         })
     return out
 
@@ -4601,7 +4608,8 @@ def reduce_all(input, dim=None, keep_dim=False, name=None):
         attrs={
             'dim': dim if dim != None and dim != [] else [0],
             'keep_dim': keep_dim,
-            'reduce_all': True if dim == None or dim == [] else False
+            'reduce_all': True if dim == None or dim == [] or
+            len(dim) == len(input.shape) else False
         })
     return out
 
@@ -4659,7 +4667,8 @@ def reduce_any(input, dim=None, keep_dim=False, name=None):
         attrs={
             'dim': dim if dim != None and dim != [] else [0],
             'keep_dim': keep_dim,
-            'reduce_all': True if dim == None or dim == [] else False
+            'reduce_all': True if dim == None or dim == [] or
+            len(dim) == len(input.shape) else False
         })
     return out
 
