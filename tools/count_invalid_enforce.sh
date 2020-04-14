9704a69ed5ddc93bae258a524e41e33495b6bc44
Chen Weihang <chenweihang@baidu.com>
2020-04-14 03:38:07 +0000

    add usage & result, test=develop, test=document_fix

--- tools/count_invalid_enforce.sh
+++ tools/count_invalid_enforce.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# This script is used to count all PADDLE checks in the paddle/fluid directory,
+#   including the total PADDLE check number, the valid check number and the
+#   invalid check number under paddle/fluid and its main sub-directories.
+
+# Usage: bash count_invalid_enforce.sh (run in tools directory)
+
+# Result Example:
+
+#     ../paddle/fluid/framework - total: 1065, valid: 267, invalid: 798
+#     ../paddle/fluid/imperative - total: 135, valid: 118, invalid: 17
+#     ../paddle/fluid/inference - total: 449, valid: 158, invalid: 291
+#     ../paddle/fluid/memory - total: 60, valid: 10, invalid: 50
+#     ../paddle/fluid/operators - total: 4225, valid: 1061, invalid: 3164
+#     ../paddle/fluid/platform - total: 240, valid: 39, invalid: 201
+#     ../paddle/fluid/pybind - total: 98, valid: 53, invalid: 45
+#     ../paddle/fluid/string - total: 0, valid: 0, invalid: 0
+#     ../paddle/fluid/testdata - total: 0, valid: 0, invalid: 0
+#     ../paddle/fluid/train - total: 6, valid: 0, invalid: 6
+#     ----------------------------
+#     PADDLE ENFORCE & THROW COUNT
+#     ----------------------------
+#     All PADDLE_ENFORCE{_**} & PADDLE_THROW Count: 6278
+#     Valid PADDLE_ENFORCE{_**} & PADDLE_THROW Count: 1706
+#     Invalid PADDLE_ENFORCE{_**} & PADDLE_THROW Count: 4572
+
 ROOT_DIR=../paddle/fluid
 ALL_PADDLE_CHECK_CNT=0
 VALID_PADDLE_CHECK_CNT=0
