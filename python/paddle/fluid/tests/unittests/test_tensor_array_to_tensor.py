5bfa86aca783ff5ce914f9c367d69bb5862c0835
HappyAngel <chenjiaobuaa@126.com>
2020-04-15 19:43:12 +0800

    fix ut, test=develop

--- python/paddle/fluid/tests/unittests/test_tensor_array_to_tensor.py
+++ python/paddle/fluid/tests/unittests/test_tensor_array_to_tensor.py
@@ -30,9 +30,9 @@ class TestTensorArrayToTensorError(unittest.TestCase):
         with program_guard(Program()):
             input_data = numpy.random.random((2, 4)).astype("float32")
 
-            def test_input_list():
-                # The input type must be List.
-                fluid.layers.tensor_array_to_tensor(input=[input_data])
+            def est_Variable():
+                # The input type must be Variable.
+                fluid.layers.tensor_array_to_tensor(input=input_data)
 
             self.assertRaises(TypeError, test_input_list)
 
