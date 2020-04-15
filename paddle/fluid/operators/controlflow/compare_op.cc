bd5f1c23cef42b24762a202a7f853f6918ee0ff4
gfwm0502 <63550764+gfwm0502@users.noreply.github.com>
2020-04-15 10:33:43 +0800

    OP(compare/get_places/shrink_rnn_memory) error message enhancement (#23780)
    As the title.

--- paddle/fluid/operators/controlflow/compare_op.cc
+++ paddle/fluid/operators/controlflow/compare_op.cc
@@ -81,7 +81,7 @@ class CompareOp : public framework::OperatorWithKernel {
   void InferShape(framework::InferShapeContext* context) const override {
     OpComment comment;
     OP_INOUT_CHECK(context->HasInput("X"), "Input", "X", comment.type);
-    OP_INOUT_CHECK(context->HasInput("Y"), "Output", "Y", comment.type);
+    OP_INOUT_CHECK(context->HasInput("Y"), "Input", "Y", comment.type);
     auto dim_x = context->GetInputDim("X");
     auto dim_y = context->GetInputDim("Y");
 
