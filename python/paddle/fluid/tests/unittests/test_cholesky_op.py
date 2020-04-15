55dccc3a23ad98d184d89da5893ea4a8819e2b4d
guosheng <guosheng@baidu.com>
2020-04-16 00:45:09 +0800

    Add grad_check unit test for cholesky_op. test=develop

--- python/paddle/fluid/tests/unittests/test_cholesky_op.py
+++ python/paddle/fluid/tests/unittests/test_cholesky_op.py
@@ -16,34 +16,65 @@ from __future__ import print_function
 
 import unittest
 import numpy as np
-from op_test import OpTest
+import paddle
+import paddle.fluid as fluid
+import paddle.fluid.layers as layers
+import paddle.fluid.core as core
+from op_test import OpTest, skip_check_grad_ci
+from gradient_checker import grad_check
+from decorator_helper import prog_scope
 
 
+@skip_check_grad_ci(
+    reason="The input of cholesky_op should always be symmetric positive-definite. "
+    "However, OpTest calculates the numeric gradient of each element in input "
+    "via small finite difference, which makes the input no longer symmetric "
+    "positive-definite thus can not compute the Cholesky decomposition. "
+    "While we can use the gradient_checker.grad_check to perform gradient "
+    "check of cholesky_op, since it supports check gradient with a program "
+    "and we can construct symmetric positive-definite matrices in the program")
 class TestCholeskyOp(OpTest):
     def setUp(self):
         self.op_type = "cholesky"
-        self._input_shape = (8, 32, 32)
+        self._input_shape = (2, 32, 32)
         self._upper = True
         self.init_config()
-        trans_dims = list(range(len(self._input_shape) - 2)) + [
+        self.trans_dims = list(range(len(self._input_shape) - 2)) + [
             len(self._input_shape) - 1, len(self._input_shape) - 2
         ]
-        l = np.random.random(self._input_shape).astype("float64")
+        self.root_data = np.random.random(self._input_shape).astype("float64")
         # construct symmetric positive-definite matrice
-        input_data = np.matmul(l, l.transpose(trans_dims)) + 1e-03
+        input_data = np.matmul(
+            self.root_data, self.root_data.transpose(self.trans_dims)) + 1e-05
         output_data = np.linalg.cholesky(input_data).astype("float64")
         if self._upper:
-            output_data = output_data.transpose(trans_dims)
+            output_data = output_data.transpose(self.trans_dims)
         self.inputs = {"X": input_data}
         self.attrs = {"upper": self._upper}
         self.outputs = {"Out": output_data}
-        self.gradient = calc_gradient(output_data)
 
     def test_check_output(self):
         self.check_output()
 
     def test_check_grad(self):
-        self.check_grad(["X"], "Out", user_defined_grads=self.gradient)
+        places = [fluid.CPUPlace()]
+        if core.is_compiled_with_cuda():
+            places.append(fluid.CUDAPlace(0))
+        for p in places:
+            self.func(p)
+
+    @prog_scope()
+    def func(self, place):
+        # use small size since Jacobian gradients is time consuming
+        root_data = self.root_data[..., :3, :3]
+        prog = fluid.Program()
+        with fluid.program_guard(prog):
+            root = layers.create_parameter(
+                dtype=root_data.dtype, shape=root_data.shape)
+            root_t = layers.transpose(root, self.trans_dims)
+            x = layers.matmul(x=root, y=root_t) + 1e-05
+            out = paddle.cholesky(x, upper=self.attrs["upper"])
+            grad_check(root, out, x_init=root_data, place=place)
 
     def init_config(self):
         self._upper = True
