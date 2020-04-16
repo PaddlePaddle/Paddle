331fa0b69547199f8229dcf8642e0d6c75379bff
sneaxiy <sneaxiy@126.com>
2020-04-16 03:08:28 +0000

    refine inlined_vector, test=develop

--- paddle/fluid/framework/inlined_vector.h
+++ paddle/fluid/framework/inlined_vector.h
@@ -13,7 +13,9 @@
 // limitations under the License.
 
 #pragma once
+#include <algorithm>
 #include <cstdint>
+#include <utility>
 #include <vector>
 #include "paddle/fluid/platform/enforce.h"
 
@@ -25,43 +27,184 @@ class InlinedVector {
   static_assert(N > 0, "N must be larger than 0");
 
  public:
-  inline InlinedVector() { len_ = 0; }
+  InlinedVector() {}
 
-  inline size_t size() const { return len_; }
+  explicit InlinedVector(size_t size) { resize(size); }
 
-  inline T& operator[](size_t i) { return i < N ? head_[i] : tail_[i - N]; }
-
-  inline const T& operator[](size_t i) const {
-    return i < N ? head_[i] : tail_[i - N];
-  }
-
-  inline void emplace_back(const T& item) {
-    if (LIKELY(len_ < N)) {
-      head_[len_++] = item;
-    } else {
-      tail_.emplace_back(item);
-      ++len_;
+  template <typename U>
+  InlinedVector(std::initializer_list<U> list) {
+    reserve(list.size());
+    for (const auto& item : list) {
+      push_back(item);
     }
   }
 
-  inline void pop_back() {
-    if (UNLIKELY(len_ > N)) {
+  InlinedVector(const InlinedVector<T, N>& other)
+      : len_(other.len_), tail_(other.tail_) {
+    size_t head_size = HeadSize();
+    for (size_t i = 0; i < head_size; ++i) {
+      new (MutableHead(i)) T(other.Head(i));
+    }
+  }
+
+  InlinedVector(InlinedVector<T, N>&& other)
+      : len_(other.len_), tail_(std::move(other.tail_)) {
+    other.len_ = 0;
+    size_t head_size = HeadSize();
+    for (size_t i = 0; i < head_size; ++i) {
+      auto* other_item = other.MutableHead(i);
+      new (MutableHead(i)) T(std::move(*other_item));
+      other_item->~T();
+    }
+  }
+
+  ~InlinedVector() {
+    size_t head_size = HeadSize();
+    for (size_t i = 0; i < head_size; ++i) {
+      MutableHead(i)->~T();
+    }
+  }
+
+  InlinedVector<T, N>& operator=(const InlinedVector<T, N>& other) {
+    if (LIKELY(this != &other)) {
+      size_t head_size = HeadSize();
+      size_t other_head_size = other.HeadSize();
+      size_t min_size = std::min(head_size, other_head_size);
+      for (size_t i = 0; i < min_size; ++i) {
+        *MutableHead(i) = other.Head(i);
+      }
+
+      for (size_t i = min_size; i < other_head_size; ++i) {
+        new (MutableHead(i)) T(other.Head(i));
+      }
+
+      for (size_t i = other_head_size; i < head_size; ++i) {
+        MutableHead(i)->~T();
+      }
+
+      len_ = other.len_;
+      tail_ = other.tail_;
+    }
+    return *this;
+  }
+
+  InlinedVector<T, N>& operator=(InlinedVector<T, N>&& other) {
+    if (LIKELY(this != &other)) {
+      size_t head_size = HeadSize();
+      size_t other_head_size = other.HeadSize();
+      size_t min_size = std::min(head_size, other_head_size);
+      for (size_t i = 0; i < min_size; ++i) {
+        *MutableHead(i) = std::move(*other.MutableHead(i));
+      }
+
+      for (size_t i = min_size; i < other_head_size; ++i) {
+        new (MutableHead(i)) T(std::move(*other.MutableHead(i)));
+      }
+
+      for (size_t i = other_head_size; i < head_size; ++i) {
+        MutableHead(i)->~T();
+      }
+
+      for (size_t i = 0; i < other_head_size; ++i) {
+        other.MutableHead(i)->~T();
+      }
+
+      len_ = other.len_;
+      other.len_ = 0;
+      tail_ = std::move(other.tail_);
+    }
+    return *this;
+  }
+
+  bool empty() const { return len_ == 0; }
+
+  size_t size() const { return len_; }
+
+  size_t capacity() const { return N + tail_.capactiy(); }
+
+  void reserve(size_t size) {
+    if (size > N) {
+      tail_.reserve(size - N);
+    }
+  }
+
+  void resize(size_t size) {
+    if (size > N) {
+      tail_.resize(size - N);
+    } else {
+      tail_.clear();
+    }
+
+    size_t head_size = HeadSize();
+    size_t max_head_size = std::min(size, N);
+
+    if (head_size <= max_head_size) {
+      for (size_t i = head_size; i < max_head_size; ++i) {
+        new (MutableHead(i)) T();
+      }
+    } else {
+      for (size_t i = max_head_size; i < head_size; ++i) {
+        MutableHead(i)->~T();
+      }
+    }
+    len_ = size;
+  }
+
+  T& operator[](size_t i) { return i < N ? *MutableHead(i) : tail_[i - N]; }
+
+  const T& operator[](size_t i) const { return i < N ? Head(i) : tail_[i - N]; }
+
+  template <typename... Args>
+  void emplace_back(Args&&... args) {
+    if (LIKELY(len_ < N)) {
+      new (MutableHead(len_)) T(std::forward<Args>(args)...);
+    } else {
+      tail_.emplace_back(std::forward<Args>(args)...);
+    }
+    ++len_;
+  }
+
+  void push_back(const T& item) { emplace_back(item); }
+
+  void push_back(T&& item) { emplace_back(std::move(item)); }
+
+  void pop_back() {
+    if (LIKELY(--len_ < N)) {
+      MutableHead(len_)->~T();
+    } else {
       tail_.pop_back();
     }
-    --len_;
   }
 
-  inline T& back() {
-    if (LIKELY(len_ <= N)) {
-      return head_[len_ - 1];
-    } else {
-      return tail_.back();
+  T& front() { return (*this)[0]; }
+
+  const T& front() const { return (*this)[0]; }
+
+  T& back() { return (*this)[len_ - 1]; }
+
+  const T& back() const { return (*this)[len_ - 1]; }
+
+  void clear() {
+    size_t head_size = HeadSize();
+    for (size_t i = 0; i < head_size; ++i) {
+      MutableHead(i)->~T();
     }
+    len_ = 0;
+    tail_.clear();
   }
 
  private:
-  T head_[N];
-  size_t len_;
+  T* MutableHead(size_t i) { return reinterpret_cast<T*>(&head_[0]) + i; }
+
+  const T& Head(size_t i) const {
+    return *(reinterpret_cast<const T*>(&head_[0]) + i);
+  }
+
+  size_t HeadSize() const { return len_ < N ? len_ : N; }
+
+ private:
+  uint8_t head_[N * sizeof(T)];
+  size_t len_{0};
   std::vector<T> tail_;
 };
 
--- paddle/fluid/framework/inlined_vector_test.cc
+++ paddle/fluid/framework/inlined_vector_test.cc
@@ -67,7 +67,7 @@ void InlinedVectorCheck(size_t n) {
   CHECK_EQ(vec.size(), static_cast<size_t>(0));
 }
 
-TEST(inlined_vector, inlined_vector) {
+TEST(inlined_vector, test_emplace_back_and_pop_back) {
   for (size_t i = 0; i < 20; ++i) {
     InlinedVectorCheck<1>(i);
     InlinedVectorCheck<10>(i);
@@ -76,6 +76,145 @@ TEST(inlined_vector, inlined_vector) {
     InlinedVectorCheck<21>(i);
     InlinedVectorCheck<25>(i);
   }
+}
+
+class StubObject {
+ public:
+  StubObject() { ++alived_obj_num_; }
+
+  StubObject(const StubObject &other) {
+    rand_num_ = other.rand_num_;
+    ++alived_obj_num_;
+  }
+
+  StubObject(StubObject &&other) {
+    rand_num_ = other.rand_num_;
+    other.rand_num_ = -1;
+    ++alived_obj_num_;
+  }
+
+  ~StubObject() { --alived_obj_num_; }
+
+  StubObject &operator=(const StubObject &other) {
+    rand_num_ = other.rand_num_;
+    return *this;
+  }
+
+  StubObject &operator=(StubObject &&other) {
+    rand_num_ = other.rand_num_;
+    other.rand_num_ = -1;
+    return *this;
+  }
+
+  static size_t AlivedObjectNum() { return alived_obj_num_; }
+
+  void SetRandomNumber() { rand_num_ = rand(); }  // NOLINT
+
+  int RandomNumber() const { return rand_num_; }
+
+  bool operator==(const StubObject &other) const {
+    return rand_num_ == other.rand_num_;
+  }
+
+ private:
+  int rand_num_;
+  static size_t alived_obj_num_;
+};
+
+size_t StubObject::alived_obj_num_ = 0;
+
+static std::ostream &operator<<(std::ostream &out, const StubObject &obj) {
+  out << obj.RandomNumber();
+  return out;
+}
+
+template <size_t N>
+static void TestInlinedVectorCopyAndMove() {
+  using VecType = InlinedVector<StubObject, N>;
+  VecType vec;
+  ASSERT_EQ(vec.size(), 0UL);
+  ASSERT_EQ(vec.empty(), true);
+
+  for (size_t i = 0; i < 3 * N; ++i) {
+    vec.resize(i);
+    for (size_t j = 0; j < i; ++j) {
+      vec[j].SetRandomNumber();
+    }
+
+    ASSERT_EQ(vec.size(), i);
+    ASSERT_EQ(vec.empty(), (i == 0));
+    {
+      auto copy_vec = vec;
+      ASSERT_EQ(copy_vec.size(), vec.size());
+      ASSERT_EQ(StubObject::AlivedObjectNum(), 2 * i);
+      for (size_t j = 0; j < i; ++j) {
+        ASSERT_EQ(vec[j], copy_vec[j]);
+      }
+
+      {
+        VecType another_copy_vec(rand() % 100);  // NOLINT
+        another_copy_vec = vec;
+        ASSERT_EQ(another_copy_vec.size(), vec.size());
+        ASSERT_EQ(StubObject::AlivedObjectNum(), 3 * i);
+        for (size_t j = 0; j < i; ++j) {
+          ASSERT_EQ(vec[j], another_copy_vec[j]);
+        }
+      }
+
+      auto move_vec = std::move(copy_vec);
+      ASSERT_EQ(copy_vec.size(), 0UL);
+      ASSERT_EQ(move_vec.size(), vec.size());
+      ASSERT_EQ(StubObject::AlivedObjectNum(), 2 * i);
+      for (size_t j = 0; j < i; ++j) {
+        ASSERT_EQ(vec[j], move_vec[j]);
+      }
+
+      {
+        VecType another_move_vec(rand() % 100);  // NOLINT
+        another_move_vec = std::move(move_vec);
+        ASSERT_EQ(another_move_vec.size(), vec.size());
+        ASSERT_EQ(move_vec.empty(), true);
+        ASSERT_EQ(StubObject::AlivedObjectNum(), 2 * i);
+        for (size_t j = 0; j < i; ++j) {
+          ASSERT_EQ(vec[j], another_move_vec[j]);
+        }
+      }
+    }
+
+    ASSERT_EQ(StubObject::AlivedObjectNum(), i);
+  }
+}
+
+TEST(inlined_vector, test_copy_and_move) {
+  TestInlinedVectorCopyAndMove<1>();
+  TestInlinedVectorCopyAndMove<10>();
+  TestInlinedVectorCopyAndMove<15>();
+  TestInlinedVectorCopyAndMove<20>();
+  TestInlinedVectorCopyAndMove<21>();
+  TestInlinedVectorCopyAndMove<25>();
+}
+
+TEST(inlined_vector, test_clear) {
+  InlinedVector<StubObject, 8> vec;
+  for (size_t i = 0; i < 20; ++i) {
+    vec.resize(i);
+    ASSERT_EQ(StubObject::AlivedObjectNum(), i);
+    vec.clear();
+    ASSERT_EQ(StubObject::AlivedObjectNum(), 0UL);
+  }
+}
+
+TEST(inlined_vector, test_front_back) {
+  InlinedVector<int, 3> vec = {4, 3, 2, 10};
+  ASSERT_EQ(vec.front(), 4);
+  ASSERT_EQ(vec.back(), 10);
+
+  ASSERT_EQ(vec[0], vec.front());
+  ASSERT_EQ(vec[vec.size() - 1], vec.back());
+
+  const auto const_vec = vec;
+  ASSERT_EQ(const_vec.front(), vec[0]);
+  ASSERT_EQ(const_vec.back(), vec[vec.size() - 1]);
 }
 
 }  // namespace framework
