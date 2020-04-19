49eb4c4e8ae2b869ff820b304012cb57d420c484
GaoWei8 <53294385+GaoWei8@users.noreply.github.com>
2020-04-19 17:25:35 +0800

    add paddle.tensor interface (#23801)
    * add paddle.tensor
    test=develop
    
    * polish gpu where codes
    test=develop
    
    * polish test code
    test=develop

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -191,7 +191,7 @@ from .tensor.search import argmax  #DEFINE_ALIAS
 # from .tensor.search import has_nan   #DEFINE_ALIAS
 # from .tensor.search import masked_select   #DEFINE_ALIAS
 # from .tensor.search import topk   #DEFINE_ALIAS
-# from .tensor.search import where   #DEFINE_ALIAS
+from .tensor.search import where  #DEFINE_ALIAS
 from .tensor.search import index_select  #DEFINE_ALIAS
 from .tensor.search import index_sample  #DEFINE_ALIAS
 from .tensor.search import nonzero  #DEFINE_ALIAS
