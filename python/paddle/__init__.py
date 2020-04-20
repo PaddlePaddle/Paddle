17ff46b6a3f4e9e2c2df65d1cb842aded667e274
0YuanZhang0 <953963890@qq.com>
2020-04-20 19:37:44 +0800

    Cherry-pick Manual seed op into paddle.framework (#23537)
    add manual_seed op into paddle.framework.random

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -36,6 +36,7 @@ batch = batch.batch
 import paddle.sysconfig
 import paddle.tensor
 import paddle.nn
+import paddle.framework
 import paddle.imperative
 
 # TODO: define alias in tensor and framework directory
@@ -198,7 +199,7 @@ from .tensor.search import nonzero  #DEFINE_ALIAS
 from .tensor.search import sort  #DEFINE_ALIAS
 # from .framework.framework import set_default_dtype   #DEFINE_ALIAS
 # from .framework.framework import get_default_dtype   #DEFINE_ALIAS
-# from .framework.random import manual_seed   #DEFINE_ALIAS
+from .framework.random import manual_seed  #DEFINE_ALIAS
 # from .framework import append_backward   #DEFINE_ALIAS
 # from .framework import gradients   #DEFINE_ALIAS
 # from .framework import Executor   #DEFINE_ALIAS
