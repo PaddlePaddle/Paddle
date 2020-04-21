10721c6209a19c422543a7e2d0f2facd2b50ada0
Liufang Sang <slf12thuss@163.com>
2020-04-21 06:25:08 -0500

    【cherry-pick】 enhance error message for smooth_l1, mean_iou, accuracy... etc (#24006)
    * cherry-pick OP Normal, Uniform, Xavier Initializer, smooth_l1, mean_iou error message enhancement (#23751)  test=release/2.0

--- paddle/fluid/operators/uniform_random_op.cc
+++ paddle/fluid/operators/uniform_random_op.cc
@@ -74,12 +74,13 @@ class CPUUniformRandomKernel : public framework::OpKernel<T> {
         static_cast<unsigned int>(ctx.Attr<int>("diag_step"));
     auto diag_val = static_cast<T>(ctx.Attr<float>("diag_val"));
     if (diag_num > 0) {
-      PADDLE_ENFORCE_GT(size, (diag_num - 1) * (diag_step + 1),
-                        "ShapeError: the diagonal's elements is equal (num-1) "
-                        "* (step-1) with num %d, step %d,"
-                        "It should be smaller than %d, but received %d",
-                        diag_num, diag_step, (diag_num - 1) * (diag_step + 1),
-                        size);
+      PADDLE_ENFORCE_GT(
+          size, (diag_num - 1) * (diag_step + 1),
+          platform::errors::InvalidArgument(
+              "ShapeInvalid: the diagonal's elements is equal (num-1) "
+              "* (step-1) with num %d, step %d,"
+              "It should be smaller than %d, but received %d",
+              diag_num, diag_step, (diag_num - 1) * (diag_step + 1), size));
       for (int64_t i = 0; i < diag_num; ++i) {
         int64_t pos = i * diag_step + i;
         data[pos] = diag_val;
@@ -117,9 +118,10 @@ class UniformRandomOp : public framework::OperatorWithKernel {
       auto inputs_name = ctx->Inputs("ShapeTensorList");
       PADDLE_ENFORCE_GT(
           inputs_name.size(), 0,
-          "Input(ShapeTensorList)'size of Op(uniform_random) can't be zero."
-          "Please check the Attr(shape)'s size of"
-          "Op(fluid.layers.uniform_random).)");
+          platform::errors::InvalidArgument(
+              "Input(ShapeTensorList)'size of Op(uniform_random) can't be zero."
+              "Please check the Attr(shape)'s size of"
+              "Op(fluid.layers.uniform_random).)"));
       auto out_dims = std::vector<int>(inputs_name.size(), -1);
       ctx->SetOutputDim("Out", framework::make_ddim(out_dims));
 
@@ -130,10 +132,11 @@ class UniformRandomOp : public framework::OperatorWithKernel {
       auto shape_dims = ctx->GetInputDim("ShapeTensor");
       PADDLE_ENFORCE_EQ(
           shape_dims.size(), 1,
-          "ShapeError: Input(ShapeTensor)' dimension size of "
-          "Op(uniform_random) must be 1."
-          "But received ShapeTensor's dimensions = %d, shape = [%s]",
-          shape_dims.size(), shape_dims);
+          platform::errors::InvalidArgument(
+              "ShapeError: Input(ShapeTensor)' dimension size of "
+              "Op(uniform_random) must be 1."
+              "But received ShapeTensor's dimensions = %d, shape = [%s]",
+              shape_dims.size(), shape_dims));
       int num_ele = 1;
       for (int i = 0; i < shape_dims.size(); ++i) {
         num_ele *= shape_dims[i];
@@ -144,11 +147,12 @@ class UniformRandomOp : public framework::OperatorWithKernel {
       return;
     }
 
-    PADDLE_ENFORCE_EQ(
-        shape.empty(), false,
-        "if there is no Input(ShapeTensorList) and no Input(ShapeTensor),the "
-        "attr(shape) information must "
-        "be set by Attr(shape).");
+    PADDLE_ENFORCE_EQ(shape.empty(), false,
+                      platform::errors::InvalidArgument(
+                          "if there is no Input(ShapeTensorList) and no "
+                          "Input(ShapeTensor),the "
+                          "attr(shape) information must "
+                          "be set by Attr(shape)."));
     std::vector<int64_t> tensor_shape;
     tensor_shape.reserve(shape.size());
     for (auto dim : shape) {
