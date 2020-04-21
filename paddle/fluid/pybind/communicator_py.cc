c044ec11886c59d9708ef41a3357c263bab52bfb
tangwei <tangwei12@baidu.com>
2020-04-21 16:17:44 +0800

    rename split_var_names -> split_varnames, add origin_varnames

--- paddle/fluid/pybind/communicator_py.cc
+++ paddle/fluid/pybind/communicator_py.cc
@@ -40,9 +40,10 @@ namespace pybind {
 
 void BindCommunicatorContext(py::module* m) {
   py::class_<CommContext>(*m, "CommContext")
-      .def(py::init<const std::string&, const std::vector<std::string>&,
-                    const std::vector<std::string>&,
-                    const std::vector<int64_t>&, int, bool, bool>())
+      .def(
+          py::init<const std::string&, const std::vector<std::string>&,
+                   const std::vector<std::string>&, const std::vector<int64_t>&,
+                   const std::vector<std::string>&, int, bool, bool>())
       .def_property("trainer_id",
                     [](const CommContext& self) { return self.trainer_id; });
 }
