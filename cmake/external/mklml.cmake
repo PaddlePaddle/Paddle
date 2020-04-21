8946c3a899202f5d23252b58d0086652b456efc4
Tao Luo <luotao02@baidu.com>
2020-04-21 17:48:02 +0800

    update mklml.cmake with new version
    test=develop

--- cmake/external/mklml.cmake
+++ cmake/external/mklml.cmake
@@ -33,7 +33,7 @@ IF(WIN32)
 ELSE()
     #TODO(intel-huying):
     #  Now enable csrmm function in mklml library temporarily, it will be updated as offical version later.
-    SET(MKLML_VER "mklml_lnx_2019.0.5.20190502" CACHE STRING "" FORCE)
+    SET(MKLML_VER "Csrmm_mklml_lnx_2019.0.5.20190502" CACHE STRING "" FORCE)
     SET(MKLML_URL "http://paddlepaddledeps.bj.bcebos.com/${MKLML_VER}.tgz" CACHE STRING "" FORCE)
     SET(MKLML_LIB                 ${MKLML_LIB_DIR}/libmklml_intel.so)
     SET(MKLML_IOMP_LIB            ${MKLML_LIB_DIR}/libiomp5.so)
