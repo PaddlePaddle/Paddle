a0c7157e8566a8831005158e1267a3ad95096bb8
0YuanZhang0 <953963890@qq.com>
2020-04-21 04:08:04 +0000

    test=develop

--- python/paddle/fluid/tests/unittests/sequence/test_sequence_concat.py
+++ python/paddle/fluid/tests/unittests/sequence/test_sequence_concat.py
@@ -83,29 +83,33 @@ class TestSequenceConcatOpError(unittest.TestCase):
         def test_input_list():
             # the input type must be list
             x_data = fluid.layers.data(name='x', shape=[4], dtype='float32')
-            fluid.layers.sequence_concat(x=x_data)
+            fluid.layers.sequence_concat(input=x_data)
 
         self.assertRaises(TypeError, test_input_list)
 
-        def test_variable():
+        def test_variable1():
             # the input element type must be Variable
-            input_list = []
-            for i in range(2):
-                x_data = np.array([[3, 5]]).astype('float32')
-                input_list.append(x_data)
-            fluid.layers.sequence_concat(x=input_list)
+            x1_data = np.array([[3, 5]]).astype('float32')
+            y1_data = fluid.layers.data(name='y1', shape=[4], dtype='float32')
+            fluid.layers.sequence_concat(input=[x1_data, y1_data])
 
-        self.assertRaises(TypeError, test_variable)
+        def test_variable2():
+            x2_data = np.array([[3, 5]]).astype('float32')
+            y2_data = fluid.layers.data(name='y2', shape=[4], dtype='float32')
+            fluid.layers.sequence_concat(input=[y2_data, x2_data])
+
+        for i in range(2):
+            if i == 0:
+                self.assertRaises(TypeError, test_variable1)
+            else:
+                self.assertRaises(TypeError, test_variable2)
 
         def test_dtype():
             # dtype must be 'float32', 'float64', 'int64'
-            input_list = []
-            for i in range(2):
-                name = "x_%s" % i
-                x_data = fluid.layers.data(
-                    name=name, shape=[3, 5], dtype='int32')
-                input_list.append(x_data)
-            fluid.layers.sequence_concat(x=input_list)
+            x3_data = fluid.layers.data(name="x3", shape=[3, 5], dtype='int32')
+            y3_data = fluid.layers.data(name="y3", shape=[3, 5], dtype='int16')
+            input_list = [x3_data, y3_data]
+            fluid.layers.sequence_concat(input=input_list)
 
         self.assertRaises(TypeError, test_dtype)
 
