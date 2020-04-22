3149b8f34999b2a1198716d97ba4ee15901c97c7
Yang Zhang <yangzhang@live.com>
2020-04-22 18:56:24 +0800

    Indent MSELoss example docs

--- python/paddle/nn/layer/loss.py
+++ python/paddle/nn/layer/loss.py
@@ -166,37 +166,38 @@ class MSELoss(fluid.dygraph.layers.Layer):
 
     Examples:
         .. code-block:: python
-        import numpy as np
-        import paddle
-        from paddle import fluid
-        import paddle.fluid.dygraph as dg
 
-        mse_loss = paddle.nn.loss.MSELoss()
-        input = fluid.data(name="input", shape=[1])
-        label = fluid.data(name="label", shape=[1])
-        place = fluid.CPUPlace()
-        input_data = np.array([1.5]).astype("float32")
-        label_data = np.array([1.7]).astype("float32")
+            import numpy as np
+            import paddle
+            from paddle import fluid
+            import paddle.fluid.dygraph as dg
 
-        # declarative mode
-        output = mse_loss(input,label)
-        exe = fluid.Executor(place)
-        exe.run(fluid.default_startup_program())
-        output_data = exe.run(
-            fluid.default_main_program(),
-            feed={"input":input_data, "label":label_data},
-            fetch_list=[output],
-            return_numpy=True)
-        print(output_data)
-        # [array([0.04000002], dtype=float32)]
+            mse_loss = paddle.nn.loss.MSELoss()
+            input = fluid.data(name="input", shape=[1])
+            label = fluid.data(name="label", shape=[1])
+            place = fluid.CPUPlace()
+            input_data = np.array([1.5]).astype("float32")
+            label_data = np.array([1.7]).astype("float32")
 
-        # imperative mode
-        with dg.guard(place) as g:
-            input = dg.to_variable(input_data)
-            label = dg.to_variable(label_data)
-            output = mse_loss(input, label)
-            print(output.numpy())
-            # [0.04000002]
+            # declarative mode
+            output = mse_loss(input,label)
+            exe = fluid.Executor(place)
+            exe.run(fluid.default_startup_program())
+            output_data = exe.run(
+                fluid.default_main_program(),
+                feed={"input":input_data, "label":label_data},
+                fetch_list=[output],
+                return_numpy=True)
+            print(output_data)
+            # [array([0.04000002], dtype=float32)]
+
+            # imperative mode
+            with dg.guard(place) as g:
+                input = dg.to_variable(input_data)
+                label = dg.to_variable(label_data)
+                output = mse_loss(input, label)
+                print(output.numpy())
+                # [0.04000002]
     """
 
     def __init__(self, reduction='mean'):
