5766c821bf24f091b8d6de9e208650b5a4ec56a1
arlesniak <artur.lesniak@intel.com>
2020-04-24 05:26:45 +0200

    added fusing matmul-transpose-reshape pass (#23866)

--- paddle/fluid/operators/matmul_op.cc
+++ paddle/fluid/operators/matmul_op.cc
@@ -407,7 +407,45 @@ class MatMulOp : public framework::OperatorWithKernel {
     if (dim_out.empty()) {
       dim_out = {1};
     }
-    context->SetOutputDim("Out", framework::make_ddim(dim_out));
+
+    framework::DDim ddim_out = framework::make_ddim(dim_out);
+
+#ifdef PADDLE_WITH_MKLDNN
+    //  if mkldnn matmul+transpose+reshape fuse activated
+    auto reshape_out =
+        context->Attrs().Get<std::vector<int>>("fused_reshape_Out");
+    auto transpose_out =
+        context->Attrs().Get<std::vector<int>>("fused_transpose_Out");
+
+    if (!reshape_out.empty() && !transpose_out.empty()) {
+      auto reshape_out_size = reshape_out.size();
+      auto transpose_out_size = transpose_out.size();
+      PADDLE_ENFORCE_EQ(transpose_out_size, 4,
+                        platform::errors::InvalidArgument(
+                            "transpose_out supported rank is 4, "
+                            "received %d",
+                            transpose_out_size));
+      const std::vector<int> supported_axis{0, 2, 1, 3};
+      const bool supported_transpose_axis = std::equal(
+          transpose_out.begin(), transpose_out.end(), supported_axis.begin());
+      PADDLE_ENFORCE_EQ(
+          supported_transpose_axis, true,
+          platform::errors::InvalidArgument(
+              "supported transpose axis for the fuse are {0, 2, 1, 3}"));
+      PADDLE_ENFORCE_EQ(
+          reshape_out_size, 3,
+          platform::errors::InvalidArgument("reshape_out supported rank is 3, "
+                                            "received %d",
+                                            reshape_out_size));
+      framework::DDim shape_out =
+          ddim_out.transpose(transpose_out).reshape(reshape_out);
+      context->SetOutputDim("Out", shape_out);
+    } else {
+      context->SetOutputDim("Out", ddim_out);
+    }
+#else
+    context->SetOutputDim("Out", ddim_out);
+#endif
     context->ShareLoD("X", /*->*/ "Out");
   }
 
@@ -446,6 +484,16 @@ class MatMulOpMaker : public framework::OpProtoAndCheckerMaker {
         "use_mkldnn",
         "(bool, default false) Indicates if MKL-DNN kernel will be used")
         .SetDefault(false);
+    AddAttr<std::vector<int>>(
+        "fused_reshape_Out",
+        R"DOC(When MKLDNN MatMul_transpose_reshape fuse activated, "
+              "it's a shape atribute of fused reshape for `Out` output.)DOC")
+        .SetDefault({});
+    AddAttr<std::vector<int>>(
+        "fused_transpose_Out",
+        R"DOC(When MKLDNN MatMul_transpose_reshape fuse activated, "
+              "it's a axis atribute of fused transpose for `Out` output.)DOC")
+        .SetDefault({});
     /* int8 parameters */
     AddAttr<bool>("use_quantizer",
                   "(bool, default false) "
@@ -466,6 +514,7 @@ class MatMulOpMaker : public framework::OpProtoAndCheckerMaker {
                   "(bool, default false) Force INT8 kernel output FP32, only "
                   "used in MKL-DNN INT8")
         .SetDefault(false);
+
 #if defined(PADDLE_WITH_MKLML) && !defined(PADDLE_WITH_CUDA)
     AddAttr<int>("head_number", "The number of heads of the matrix")
         .SetDefault(1);
