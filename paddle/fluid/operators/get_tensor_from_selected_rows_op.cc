1b3df9327121d827a68f854fd8663a5f0b51e6dc
HappyAngel <chenjiaobuaa@126.com>
2020-04-24 12:06:12 +0800

    fix

--- paddle/fluid/operators/get_tensor_from_selected_rows_op.cc
+++ paddle/fluid/operators/get_tensor_from_selected_rows_op.cc
@@ -28,20 +28,20 @@ class GetTensorFromSelectedRowsOp : public framework::OperatorWithKernel {
     OP_INOUT_CHECK(ctx->HasOutput("Out"), "Output", "Out",
                    "GetTensorFromSelectedRows");
 
-    PADDLE_ENFORCE_EQ(ctx->GetInputsVarType("X").front() ==
-                          framework::proto::VarType::SELECTED_ROWS,
-                      true, platform::errors::InvalidArgument(
-                                "The input X's type should be SelectedRows, "
-                                "but the received is %s",
-                                ctx->Inputs("X").front(),
-                                ctx->GetInputsVarType("X").front()));
-    PADDLE_ENFORCE_EQ(ctx->GetOutputsVarType("Out").front() ==
-                          framework::proto::VarType::LOD_TENSOR,
-                      true, platform::errors::InvalidArgument(
-                                "The output Out's type should be LoDTensor, "
-                                "but the received is %s",
-                                ctx->Outputs("Out").front(),
-                                ctx->GetOutputsVarType("Out").front()));
+    PADDLE_ENFORCE_EQ(ctx->GetInputsVarType("X").front(),
+                      framework::proto::VarType::SELECTED_ROWS,
+                      platform::errors::InvalidArgument(
+                          "The input X's type should be SelectedRows, "
+                          "but the received is %s",
+                          ctx->Inputs("X").front(),
+                          ctx->GetInputsVarType("X").front()));
+    PADDLE_ENFORCE_EQ(ctx->GetOutputsVarType("Out").front(),
+                      framework::proto::VarType::LOD_TENSOR,
+                      platform::errors::InvalidArgument(
+                          "The output Out's type should be LoDTensor, "
+                          "but the received is %s",
+                          ctx->Outputs("Out").front(),
+                          ctx->GetOutputsVarType("Out").front()));
     ctx->SetOutputDim("Out", ctx->GetInputDim("X"));
   }
 
