a8ff0872a3f3ca8c518448435871c8c189f8982f
wuxing03 <wuxing03@baidu.com>
2020-04-24 17:22:02 +0000

    remove unused import

--- python/paddle/fluid/dygraph/rnn.py
+++ python/paddle/fluid/dygraph/rnn.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import paddle
+
 from . import Layer
 from ..layers import sigmoid, tanh, concat, fill_constant, matmul, elementwise_add, elementwise_mul, split
 import copy
