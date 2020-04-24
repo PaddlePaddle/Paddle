1f413ad25644eca24a7d7775bd29eacc903f64e8
HappyAngel <chenjiaobuaa@126.com>
2020-04-24 12:09:03 +0800

    add note, test=develop

--- paddle/fluid/operators/tensor_array_to_tensor_op.cc
+++ paddle/fluid/operators/tensor_array_to_tensor_op.cc
@@ -97,7 +97,8 @@ class LoDTensorArray2TensorOp : public framework::OperatorBase {
 
     const size_t n = inx.size();
     PADDLE_ENFORCE_GT(n, 0, platform::errors::InvalidArgument(
-                                "Input tensorarray size should > 0."));
+                                "Input tensorarray size should > 0,"
+                                "But received n: %d", n));
 
     std::string base_name = Inputs("X")[0];
     std::vector<std::string> names;
@@ -234,7 +235,8 @@ class LoDTensorArray2TensorGradOp : public framework::OperatorBase {
     auto &inx = scope.FindVar(Input("X"))->Get<framework::LoDTensorArray>();
     const size_t n = inx.size();
     PADDLE_ENFORCE_GT(n, 0, platform::errors::InvalidArgument(
-                                "Input tensorarray size should > 0."));
+                                "Input tensorarray size should > 0, "
+                                "But received n: %d. ", n));
 
     std::string base_name = Inputs("X")[0];
     std::vector<std::string> names;
