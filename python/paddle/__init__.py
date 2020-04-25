7aa6b9ec9b6af686b2aca423274caae1371792b0
Feiyu Chan <chenfeiyu@baidu.com>
2020-04-25 14:11:01 +0800

    add kron op (#24105)
    * add kron op and its python API, doc and unittests.
    * add kron in paddle.complex

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -139,6 +139,7 @@ from .tensor.math import min  #DEFINE_ALIAS
 from .tensor.math import mm  #DEFINE_ALIAS
 from .tensor.math import div  #DEFINE_ALIAS
 from .tensor.math import add  #DEFINE_ALIAS
+from .tensor.math import kron  #DEFINE_ALIAS
 # from .tensor.math import atan   #DEFINE_ALIAS
 from .tensor.math import logsumexp  #DEFINE_ALIAS
 # from .tensor.math import inverse   #DEFINE_ALIAS
