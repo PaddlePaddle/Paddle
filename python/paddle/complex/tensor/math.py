e01262e691dc8d847f5152b84bf0fc12be3cb5db
Feiyu Chan <chenfeiyu@baidu.com>
2020-04-25 14:11:01 +0800

    add kron op (#24105)
    * add kron op and its python API, doc and unittests.
    * add kron in paddle.complex

--- python/paddle/complex/tensor/math.py
+++ python/paddle/complex/tensor/math.py
@@ -12,12 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from paddle.common_ops_import import *
 from ..helper import is_complex, is_real, complex_variable_exists
 from ...fluid.framework import ComplexVariable
 from ...fluid import layers
+from ...tensor import math
 
 __all__ = [
-    'elementwise_add', 'elementwise_sub', 'elementwise_mul', 'elementwise_div'
+    'elementwise_add', 'elementwise_sub', 'elementwise_mul', 'elementwise_div',
+    'kron'
 ]
 
 
@@ -37,6 +40,9 @@ def elementwise_add(x, y, axis=-1, name=None):
             with any number of dimensions. The supported data types include float32 
             and float64 when it is a Variable. Otherwise the supported data types 
             are complex64 or complex128.
+        name(str, optional): The default value is None.  Normally there is no 
+            need for user to set this property.  For more information, please 
+            refer to :ref:`api_guide_Name`.
 
     Examples:
         .. code-block:: python
@@ -85,6 +91,9 @@ def elementwise_sub(x, y, axis=-1, name=None):
             with any number of dimensions. The supported data types include float32 
             and float64 when it is a Variable. Otherwise the supported data types 
             are complex64 or complex128.
+        name(str, optional): The default value is None.  Normally there is no 
+            need for user to set this property.  For more information, please 
+            refer to :ref:`api_guide_Name`.
 
     Examples:
         .. code-block:: python
@@ -133,6 +142,9 @@ def elementwise_mul(x, y, axis=-1, name=None):
             with any number of dimensions. The supported data types include float32 
             and float64 when it is a Variable. Otherwise the supported data types 
             are complex64 or complex128.
+        name(str, optional): The default value is None.  Normally there is no 
+            need for user to set this property.  For more information, please 
+            refer to :ref:`api_guide_Name`.
 
     Examples:
         .. code-block:: python
@@ -184,6 +196,9 @@ def elementwise_div(x, y, axis=-1, name=None):
             with any number of dimensions. The supported data types include float32 
             and float64 when it is a Variable. Otherwise the supported data types 
             are complex64 or complex128.
+        name(str, optional): The default value is None.  Normally there is no 
+            need for user to set this property.  For more information, please 
+            refer to :ref:`api_guide_Name`.
 
     Examples:
         .. code-block:: python
@@ -214,3 +229,71 @@ def elementwise_div(x, y, axis=-1, name=None):
         e,
         axis=axis,
         name=name)
+
+
+def kron(x, y, name=None):
+    """
+    The kronecker product of two complex tensors. At least one of inputs :attr:`x` 
+    and :attr:`y` must be a ComplexVariable. See the detailed description for 
+    the function and other arguments in :ref:`api_paddle_tensor_kron` . 
+
+    Let $x = a + ib$, and $y = c + id$, the euqation is 
+
+    .. math::
+       kron(x, y) = kron(a, c) - kron(b, d) + i(kron(a, d) + kron(b, c))
+
+    Args:
+        x (Variable|ComplexVariable): The first input Variable or ComplexVariable 
+            with any number of dimensions. The supported data types include float32 
+            and float64 when it is a Variable. Otherwise the supported data types 
+            are complex64 or complex128.
+        y (Variable|ComplexVariable): The second input Variable or ComplexVariable 
+            with any number of dimensions. The supported data types include float32 
+            and float64 when it is a Variable. Otherwise the supported data types 
+            are complex64 or complex128.
+        name(str, optional): The default value is None.  Normally there is no 
+            need for user to set this property.  For more information, please 
+            refer to :ref:`api_guide_Name`.
+
+    Returns:
+        ComplexVariable: The kronecker product, data type: complex64 or complex128, depending on the data type of x and y. If the data types of x and y are float32/complex64, the data type of the output is complex64, else if the data types of x and y are float64/complex128, the data type of the output is complex128.
+
+    Examples:
+        .. code-block:: python
+    
+            import numpy as np
+            import paddle
+            import paddle.fluid.dygraph as dg
+
+            a = np.array([[1.0+1.0j, 2.0+1.0j], [3.0+1.0j, 4.0+1.0j]])
+            b = np.array([[5.0+2.0j, 6.0+2.0j], [7.0+2.0j, 8.0+2.0j]])
+
+            place = fluid.CPUPlace()
+            with dg.guard(place):
+                x = dg.to_variable(a)
+                y = dg.to_variable(b)
+                out = paddle.complex.kron(x, y)
+                print(out.numpy())
+            # [[ 3. +7.j  4. +8.j  8. +9.j 10.+10.j]
+            #  [ 5. +9.j  6.+10.j 12.+11.j 14.+12.j]
+            #  [13.+11.j 16.+12.j 18.+13.j 22.+14.j]
+            #  [19.+13.j 22.+14.j 26.+15.j 30.+16.j]]
+    """
+    complex_variable_exists([x, y], "kron")
+
+    # X = A + Bi, Y = C+Di
+    # kron(A, B) = kron(A, C) - kron(B, D) + (kron(A, D) + kron(B, C))i
+    (a, b) = (x.real, x.imag) if is_complex(x) else (x, None)
+    (c, d) = (y.real, y.imag) if is_complex(y) else (y, None)
+
+    if is_real(b) and is_real(d):
+        real = math.kron(a, c) - math.kron(b, d)
+        imag = math.kron(a, d) + math.kron(b, c)
+    elif is_real(b):
+        real = math.kron(a, c)
+        imag = math.kron(b, c)
+    else:
+        # is_real(d)
+        real = math.kron(a, c)
+        imag = math.kron(a, d)
+    return ComplexVariable(real, imag)
