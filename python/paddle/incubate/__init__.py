a345ed901a3260c92cca2354791cef301acb39b3
Yibing Liu <liuyibing01@baidu.com>
2020-04-29 04:57:24 +0000

    Forbid import functions from paddle.incubate directly, test=develop

--- python/paddle/incubate/__init__.py
+++ python/paddle/incubate/__init__.py
@@ -11,8 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from . import complex
-from .complex import *
-
-__all__ = complex.__all__
