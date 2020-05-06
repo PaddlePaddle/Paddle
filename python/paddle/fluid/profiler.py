5db8c39f6df1aaa53361d735a7de2668a5ebde7b
GaoWei8 <catherin.gao@foxmail.com>
2020-05-06 06:14:10 +0000

    polish codes test=develop

--- python/paddle/fluid/profiler.py
+++ python/paddle/fluid/profiler.py
@@ -15,16 +15,17 @@
 from __future__ import print_function
 
 from . import core
-from .wrapped_decorator import signature_safe_contextmanager
 from .data_feeder import convert_dtype
-import json
-import os
-import six
-import numpy as np
 from sys import version_info
 from paddle.fluid import framework
-
 from .framework import Variable
+from .wrapped_decorator import signature_safe_contextmanager
+import os
+import six
+import json
+import inspect
+import importlib
+import numpy as np
 
 __all__ = [
     'cuda_profiler', 'reset_profiler', 'profiler', 'start_profiler',
@@ -42,13 +43,9 @@ NVPROF_CONFIG = [
 ]
 
 not_record_op = [
-    'data', 'creat_tensor', 'creat_parameter', 'global_var', 'while',
-    'Optimizer'
+    'data', 'creat_tensor', 'creat_parameter', 'global_var', 'Optimizer'
 ]
-not_record_param = [
-    'name', 'op_type', 'attrs', 'bias_attr', 'param_attr', 'l_type',
-    'channel_last'
-]
+not_record_param = ['name', 'op_type', 'attrs', 'bias_attr', 'param_attr']
 
 op_alias = dict()
 op_alias['max_pool2d_with_index'] = 'adaptive_pool2d'
@@ -69,6 +66,38 @@ class APIStr(object):
         if param_j != None:
             op_params_json_list.append(param_j)
 
+    def import_fluid_module(self, api_name):
+        act_api_name = api_name
+
+        try:
+            if act_api_name in ["embedding", "ont_hot"]:
+                module_name = "paddle.fluid"
+            else:
+                module_name = "paddle.fluid.layers"
+            module = importlib.import_module(module_name)
+            return getattr(module, act_api_name)
+        except Exception:
+            print("Cannot immport %s.%s." % (module_name, act_api_name))
+            module = None
+
+    def import_paddle_module(self, api_name):
+        act_api_name = api_name
+
+        try:
+            module = importlib.import_module("paddle")
+            return getattr(module, act_api_name)
+        except Exception:
+            print("Cannot immport %s.%s." % (module_name, act_api_name))
+            module = None
+
+    def is_variable(self, type):
+        if isinstance(
+                type,
+                Variable) or str(type).find('Variable') != -1 or isinstance(
+                    type,
+                    framework.Parameter) or str(type).find('Parameter') != -1:
+            return True
+
     def op_trans(self, op, params):
         op_json = {}
         params_json = {}
@@ -84,71 +113,70 @@ class APIStr(object):
             print("API_LOG: return as not valid op:", op)
             return None
 
-        if len(params) == 0:
-            op_json["param_info"] = ''
-        for param in params:
-            key = param[0]
-            values = param[1]
-            if key != None and key not in not_record_param and not callable(
-                    values):
-                data_type = ""
-                dict_temp = {}
-                if hasattr(values, 'dtype'):
-                    if isinstance(values.dtype, Variable) or str(
-                            values.dtype).find('Variable') != -1:
-                        data_type = "Variable"
-                    elif isinstance(values.dtype, framework.Parameter) or str(
-                            values.dtype).find('Parameter') != -1:
-                        data_type = "Variable"
-                    else:
-                        data_type = convert_dtype(values.dtype)
+        func = self.import_fluid_module(op)
+        if func is None:
+            func = self.import_paddle_module(op)
 
-                vtype = self.type_to_string(values, key, op_json["op"])
-                if values is None:
-                    dict_temp = {'type': 'string', 'value': str(None)}
-                elif vtype == "Variable":
-                    dict_temp = {
-                        'type': vtype,
-                        'dtype': data_type,
-                        'shape': str(list(values.shape))
-                    }
-                elif vtype == "dict":
-                    dict_temp = None
-                elif vtype == "list" and type(values[0]) is Variable:
-                    v = values[0]
-                    if hasattr(v, 'dtype'):
-                        data_type_l = convert_dtype(v.dtype)
-                    for i in range(len(values)):
-                        dict_temp[key + str(i)] = {
-                            'type': 'Variable',
-                            'dtype': data_type_l,
-                            'shape': str(list(values[i].shape))
+        if func is not None:
+            argspec = inspect.getargspec(func)
+
+            if len(params) == 0:
+                op_json["param_info"] = ''
+            for param in params:
+                key = param[0]
+                values = param[1]
+                if key != None and key in argspec.args and key not in not_record_param and not callable(
+                        values):
+                    data_type = ""
+                    dict_temp = {}
+                    if hasattr(values, 'dtype'):
+                        if self.is_variable(values.dtype):
+                            data_type = "Variable"
+                        else:
+                            data_type = convert_dtype(values.dtype)
+
+                    vtype = self.type_to_string(values, key, op_json["op"])
+                    if values is None:
+                        dict_temp = {'type': 'string', 'value': str(None)}
+                    elif vtype == "Variable":
+                        dict_temp = {
+                            'type': vtype,
+                            'dtype': data_type,
+                            'shape': str(list(values.shape))
                         }
-                    dict_temp['type'] = 'list<Variable>'
-                elif vtype == "VarType":
-                    dict_temp = {
-                        'type': 'string',
-                        'value': convert_dtype(values)
-                    }
-                elif vtype == "unicode":
-                    dict_temp = {
-                        'type': 'string',
-                        'value': str(values.encode('utf-8'))
-                    }
-                else:
-                    dict_temp = {'type': vtype, 'value': str(values)}
-                if dict_temp != None:
-                    params_json[key] = dict_temp
-            op_json["param_info"] = params_json
+                    elif vtype == "dict":
+                        dict_temp = None
+                    elif vtype == "list" and type(values[0]) is Variable:
+                        v = values[0]
+                        if hasattr(v, 'dtype'):
+                            data_type_l = convert_dtype(v.dtype)
+                        for i in range(len(values)):
+                            dict_temp[key + str(i)] = {
+                                'type': 'Variable',
+                                'dtype': data_type_l,
+                                'shape': str(list(values[i].shape))
+                            }
+                        dict_temp['type'] = 'list<Variable>'
+                    elif vtype == "VarType":
+                        dict_temp = {
+                            'type': 'string',
+                            'value': convert_dtype(values)
+                        }
+                    elif vtype == "unicode":
+                        dict_temp = {
+                            'type': 'string',
+                            'value': str(values.encode('utf-8'))
+                        }
+                    else:
+                        dict_temp = {'type': vtype, 'value': str(values)}
+                    if dict_temp != None:
+                        params_json[key] = dict_temp
+                op_json["param_info"] = params_json
         return op_json
 
     def type_to_string(self, values, key, op):
         vtype = ""
-        if isinstance(type(values),
-                      Variable) or str(type(values)).find('Variable') != -1:
-            vtype = "Variable"
-        elif isinstance(type(values), framework.Parameter) or str(
-                type(values)).find('Parameter') != -1:
+        if self.is_variable(type(values)):
             vtype = "Variable"
         elif type(values) is float:
             vtype = "float"
