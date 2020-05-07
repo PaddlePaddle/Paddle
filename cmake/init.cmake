307593c45925c66852069ab50d8f87165b27ffac
taoshibo <taoshibo@baidu.com>
2020-05-07 20:57:51 +0800

    OPT ccache support CUDA. test=develop

---
+++ cmake/init.cmake
@@ -0,0 +1,26 @@
+# default: "-g"
+set(CMAKE_C_FLAGS_DEBUG "-g")
+# default: "-O3 -DNDEBUG"
+set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG")
+# default: "-O2 -g -DNDEBUG"
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g -DNDEBUG")
+# default: "-Os -DNDEBUG"
+set(CMAKE_C_FLAGS_MINSIZEREL "-Os -DNDEBUG")
+
+# default: "-g"
+set(CMAKE_CXX_FLAGS_DEBUG "-g")
+# default: "-O3 -DNDEBUG"
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
+# default: "-O2 -g -DNDEBUG"
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -DNDEBUG")
+# default: "-Os -DNDEBUG"
+set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG")
+
+# default: "-g"
+set(CMAKE_CUDA_FLAGS_DEBUG "-g")
+# default: "-O3 -DNDEBUG"
+set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -DNDEBUG")
+# default: "-O2 -g -DNDEBUG"
+set(CMAKE_CUDA_FLAGS_RELWITHDEBINFO "-O2 -g -DNDEBUG")
+# default: "-O1 -DNDEBUG"
+set(CMAKE_CUDA_FLAGS_MINSIZEREL "-O1 -DNDEBUG")
