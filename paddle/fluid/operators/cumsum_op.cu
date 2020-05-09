d2337cc23cba9d202d44c2d3488bc2d838ba1304
wangchaochaohu <wangchao66@baidu.com>
2020-05-09 05:07:10 +0000

    add exclusive test=develop

--- paddle/fluid/operators/cumsum_op.cu
+++ paddle/fluid/operators/cumsum_op.cu
@@ -60,11 +60,12 @@ __global__ void OuterScan(const T* in, T* out, int inner_dim_size,
   }
 }
 
-// exclusive scan
+// inclusive scan
 template <typename T, int num_threads_x, int num_threads_y>
-__global__ void InnerMostDimScan(const T* in, T* out, int inner_dim_size,
-                                 int outer_dim_size, int scan_dim_size,
-                                 bool reverse) {
+__global__ void InnerMostDimInclusiveScan(const T* in, T* out,
+                                          int inner_dim_size,
+                                          int outer_dim_size, int scan_dim_size,
+                                          bool reverse) {
   __shared__ T share_data[num_threads_y][num_threads_x * 2];
   T* share_row = share_data[threadIdx.y];
   int forward_direction = 1;
@@ -141,6 +142,78 @@ __global__ void InnerMostDimScan(const T* in, T* out, int inner_dim_size,
   }
 }
 
+// exclusive block scan and store block sum for large scan
+template <typename T>
+__global__ void InnerMostDimExclusiveScan(const T* in, T* out, T* sum_data,
+                                          int inner_dim_size,
+                                          int outer_dim_size, int scan_dim_size,
+                                          int block_scan_size, bool reverse) {
+  // https://stackoverflow.com/questions/27570552/templated-cuda-kernel-with-dynamic-shared-memory
+  extern __shared__ __align__(sizeof(T)) unsigned char raw_tmp[];
+  T* share_tmp = reinterpret_cast<T*>(raw_tmp);
+  int threadId = threadIdx.x;
+  int blockId = blockIdx.x;
+  int col1 = threadId;
+  int col2 = threadId + (block_scan_size) / 2;
+  int index1 = blockIdx.y * (scan_dim_size) + blockId * block_scan_size + col1;
+  int index2 = blockIdx.y * (scan_dim_size) + blockId * block_scan_size + col2;
+
+  // to avoid bank confilict in share memory
+  int bank_offset1 = col1 >> 5;
+  int bank_offset2 = col2 >> 5;
+  if (threadId < block_scan_size / 2) {
+    share_tmp[col1] = in[index1];
+    share_tmp[col2] = in[index2];
+  } else {
+    share_tmp[col1] = 0;
+    share_tmp[col2] = 0;
+  }
+
+  // Up-Sweep
+  int offset = 1;
+  for (int d = (block_scan_size / 2); d > 0; d >>= 1) {
+    __syncthreads();
+    if (threadId < d) {
+      int tmp_index1 = offset * (2 * threadId + 1) - 1;
+      int tmp_index2 = offset * (2 * threadId + 2) - 1;
+      tmp_index1 += (tmp_index1 >> 5);
+      tmp_index2 += (tmp_index2 >> 5);
+      share_tmp[tmp_index2] += share_tmp[tmp_index1];
+    }
+    offset *= 2;
+  }
+  __syncthreads();
+
+  if (threadId == 0) {
+    sum_data[blockId] = share_tmp[block_scan_size - 1];
+    share_tmp[block_scan_size - 1] = 0;
+  }
+
+  // Down Sweep
+  for (int d = 1; d < block_scan_size; d *= 2) {
+    __syncthreads();
+    if (threadId < d) {
+      int tmp_index1 = offset * (2 * threadId + 1) - 1;
+      int tmp_index2 = offset * (2 * threadId + 2) - 1;
+      tmp_index1 += (tmp_index1 >> 5);
+      tmp_index2 += (tmp_index2 >> 5);
+
+      T tmp = share_tmp[tmp_index1];
+      share_tmp[tmp_index1] = share_tmp[tmp_index2];
+      share_tmp[tmp_index2] += tmp;
+    }
+    offset >>= 1;
+  }
+
+  __syncthreads();
+
+  out[index1] = share_tmp[col1];
+  out[index2] = share_tmp[col2];
+}
+
+template <typename T>
+__global__ void AddBlokScan(T* in, T* sum, int size) {}
+
 template <typename DeviceContext, typename T>
 class CumCUDAKernel : public framework::OpKernel<T> {
  public:
@@ -165,7 +238,7 @@ class CumCUDAKernel : public framework::OpKernel<T> {
 
     int scan_dim_size = in_dims[axis];
     bool optimize_condition = (axis == (in_dims.size() - 1)) ? true : false;
-    optimize_condition = optimize_condition && !exclusive;
+    optimize_condition = optimize_condition && (exclusive || !reverse);
     int outer_dim_size = 1;
     int inner_dim_size = 1;
     // treat all dim index < axis as outer_dim_size
@@ -182,11 +255,38 @@ class CumCUDAKernel : public framework::OpKernel<T> {
 
     auto& dev_ctx = context.template device_context<DeviceContext>();
     if (optimize_condition) {
-      dim3 block(32, 16);
-      dim3 grid((outer_dim_size + block.y - 1) / block.y);
-      InnerMostDimScan<T, 32, 16><<<grid, block, 0, dev_ctx.stream()>>>(
-          in_data, out_data, inner_dim_size, outer_dim_size, scan_dim_size,
-          reverse);
+      auto nextPowerOfTwo = [](int x) -> int {
+        int ret = 1;
+        while (ret < x) ret <<= 1;
+        return ret;
+      };
+      if (exclusive) {
+        int element_per_block = nextPowerOfTwo(scan_dim_size) / 2;
+        if (element_per_block > 1024 || element_per_block < 32) {
+          element_per_block = 32;
+        }
+        dim3 block(element_per_block);
+        dim3 grid((scan_dim_size + block.x - 1) / block.x, outer_dim_size);
+        int share_mem_size = element_per_block * 2 * sizeof(T);
+        int block_scan_size = (scan_dim_size % (element_per_block * 2));
+        Tensor scan_sum;
+        paddle::framework::DDim dims{(scan_dim_size + block.x - 1) / block.x,
+                                     outer_dim_size};
+        scan_sum.Resize(dims);
+        T* sum_data = scan_sum.mutable_data<T>(platform::CUDAPlace());
+        InnerMostDimExclusiveScan<
+            T><<<grid, block, share_mem_size, dev_ctx.stream()>>>(
+            in_data, out_data, sum_data, inner_dim_size, outer_dim_size,
+            scan_dim_size, block_scan_size, reverse);
+
+      } else {
+        dim3 block(32, 16);
+        dim3 grid((outer_dim_size + block.y - 1) / block.y);
+        InnerMostDimInclusiveScan<T, 32,
+                                  16><<<grid, block, 0, dev_ctx.stream()>>>(
+            in_data, out_data, inner_dim_size, outer_dim_size, scan_dim_size,
+            reverse);
+      }
     } else {
       dim3 block(std::min(512, inner_dim_size));
       dim3 grid(outer_dim_size, (inner_dim_size + block.x - 1) / block.x);
