43625bdabd172893542daa76d104f7a783f4381f
qingqing01 <dangqingqing@baidu.com>
2020-05-11 05:14:13 -0500

    Add a high-level API with traning and inference into Paddle. (#24293)
    * Merge hapi into Paddle
    
    Hapi is a high level API for training and inference.
    The main modules include Model, Loss, Metrics, Dataset.
    Also includes common modules and models in NLP and computer vision, such as BERT, ResNet.
    
    These modules are developed by:
    0YuanZhang0, guoshengCS heavengate, LielinJiang, qingqing01, xyzhou-puck huangjun12, wangxiao1021, zhangyang.

--- python/paddle/incubate/hapi/vision/__init__.py
+++ python/paddle/incubate/hapi/vision/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import hapi
+from . import models
+from .models import *
 
-__all__ = []
-__all__ += hapi.__all__
+__all__ = models.__all__
