db8f4082f4cba5d374c97fa398aac3c5457727d2
danleifeng <52735331+danleifeng@users.noreply.github.com>
2020-05-13 23:57:39 +0800

    fix check info for flatten hash is_empty op; test=develop (#24499)

--- python/paddle/fluid/tests/unittests/test_is_empty_op.py
+++ python/paddle/fluid/tests/unittests/test_is_empty_op.py
@@ -17,6 +17,7 @@ from __future__ import print_function
 import unittest
 import numpy as np
 from op_test import OpTest
+import paddle.fluid as fluid
 
 
 class TestEmpty(OpTest):
@@ -34,6 +35,43 @@ class TestNotEmpty(TestEmpty):
         self.op_type = "is_empty"
         self.inputs = {'X': np.array([])}
         self.outputs = {'Out': np.array([True])}
+
+
+class TestIsEmptyOpError(unittest.TestCase):
+    def test_errors(self):
+        with fluid.program_guard(fluid.Program(), fluid.Program()):
+            input_data = np.random.random((3, 2)).astype("float64")
+
+            def test_Variable():
+                # the input type must be Variable
+                fluid.layers.is_empty(x=input_data)
+
+            self.assertRaises(TypeError, test_Variable)
+
+            def test_cond_Variable():
+                # cond type must be Variable or None
+                x2 = fluid.layers.data(name="x2", shape=[3, 2], dtype="float32")
+                cond_data = np.random.random((3, 2)).astype("float32")
+                fluid.layers.is_empty(x=x2, cond=cond_data)
+
+            self.assertRaises(TypeError, test_cond_Variable)
+
+            def test_type():
+                # dtype must be float32, float64, int32, int64
+                x3 = fluid.layers.data(
+                    name="x3", shape=[4, 32, 32], dtype="bool")
+                res = fluid.layers.is_empty(x=x3)
+
+            self.assertRaises(TypeError, test_type)
+
+            def test_cond_type():
+                # cond dtype must be bool.
+                x4 = fluid.layers.data(name="x4", shape=[3, 2], dtype="float32")
+                cond = fluid.layers.data(
+                    name="cond", shape=[1], dtype="float32")
+                fluid.layers.is_empty(x=x4, cond=cond)
+
+            self.assertRaises(TypeError, test_cond_type)
 
 
 if __name__ == "__main__":
