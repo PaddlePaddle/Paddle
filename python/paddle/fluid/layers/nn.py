2f37e4bcadc1b5557f1eba0f59fdc5680945be1b
FrostML <380185688@qq.com>
2020-05-13 13:31:23 +0000

    fix doc for chunk_eval, test=develop

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -1110,12 +1110,12 @@ def chunk_eval(input,
             dict_size = 10000
             label_dict_len = 7
             sequence = fluid.data(
-                name='id', shape=[-1, 1], lod_level=1, dtype='int64')
+                name='id', shape=[None, 1], lod_level=1, dtype='int64')
             embedding = fluid.embedding(
                 input=sequence, size=[dict_size, 512])
             hidden = fluid.layers.fc(input=embedding, size=512)
             label = fluid.layers.data(
-                name='label', shape=[1], lod_level=1, dtype='int64')
+                name='label', shape=[None, 1], lod_level=1, dtype='int64')
             crf = fluid.layers.linear_chain_crf(
                 input=hidden, label=label, param_attr=fluid.ParamAttr(name="crfw"))
             crf_decode = fluid.layers.crf_decoding(
@@ -1124,7 +1124,7 @@ def chunk_eval(input,
                 input=crf_decode,
                 label=label,
                 chunk_scheme="IOB",
-                num_chunk_types=(label_dict_len - 1) / 2)
+                num_chunk_types=int((label_dict_len - 1) / 2))
     """
     helper = LayerHelper("chunk_eval", **locals())
 
