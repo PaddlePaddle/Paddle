d0262777a2eb4856b5f522609829e4ad89a05a62
Aurelius84 <liujiezhangbupt@gmail.com>
2020-05-19 14:46:38 +0800

    [dy2stat] Support len

---
+++ python/paddle/fluid/dygraph/dygraph_to_static/convert_builtins_func.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+from paddle.fluid import framework
+from paddle.fluid import core
+from paddle.fluid import unique_name
+
+
+def converted_len(var):
+    print("invoke variable.__len__ in math_op_path:")
+    # return variable from length ops based on var.type
+    if isinstance(var, framework.Variable):
+        block = current_block(var)
+        out = create_new_var(block, 'int32', name='shape')
+        if var.type == core.VarDesc.VarType.LOD_TENSOR:
+            # Note: Length of var may be known ahead of time in dygraph,
+            # but it probably represents batch size which is variant.
+            # so we return a variable dynamically inferred from var.shape.
+            block.append_op(
+                type='shape', inputs={'Input': var}, outputs={'Out': out})
+        elif var.type == core.VarDesc.VarType.SELECTED_ROWS:
+            block.append_op(
+                type='shape', inputs={'Input': var}, outputs={'Out': out})
+        elif var.type == core.VarDesc.VarType.LOD_TENSOR_ARRAY:
+            block.append_op(
+                type='lod_array_length',
+                inputs={'X': [var]},
+                outputs={'Out': [out]})
+            return out
+        else:
+            raise TypeError(
+                'len(var) only supports LoDTensor/LoDTensorArray/SelectedRows, but received %s.'
+                % type(var))
+        # return shape[0] as length.
+        len_var = out[0]
+        return len_var
+    else:
+        return builtin_len(var)
+
+
+def builtin_len(x):
+    return len(x)
+
+
+def current_block(var):
+    return var.block.program.current_block()
+
+
+def create_new_var(block, dtype, name):
+    tmp_name = unique_name.generate(name)
+    return block.create_var(name=tmp_name, dtype=dtype)
