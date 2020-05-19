175a28f413a86abacd8ca22f8568a9388f2619c6
Aurelius84 <liujiezhangbupt@gmail.com>
2020-05-19 16:00:39 +0800

    fix import error

--- python/paddle/fluid/dygraph/dygraph_to_static/call_transformer.py
+++ python/paddle/fluid/dygraph/dygraph_to_static/call_transformer.py
@@ -36,7 +36,7 @@ class CallTransformer(gast.NodeTransformer):
         assert isinstance(node, gast.Call)
         func_str = ast_to_source_code(node.func).strip()
         try:
-            from convert_call_func import is_builtin, is_builtin_len
+            from paddle.fluid.dygraph.dygraph_to_static.convert_call_func import is_builtin, is_builtin_len
             is_builtin_func = eval("is_builtin({})".format(func_str))
             is_len_func = eval("is_builtin_len({})".format(func_str))
             return not is_len_func and is_builtin_func
