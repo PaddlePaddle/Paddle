2d020af46712663fd3ce7f11599a7c5d90e5c719
Leo Chen <chenqiuliang@baidu.com>
2020-05-20 20:53:09 +0800

    fix bug of varbase.__getitem__, test=develop (#24642)
    * fix bug of varbase.__getitem__, test=develop
    
    * fix bug of float and other type, test=develop

--- python/paddle/fluid/tests/unittests/test_slice_op.py
+++ python/paddle/fluid/tests/unittests/test_slice_op.py
@@ -634,5 +634,34 @@ class TestSliceApiWithLoDTensorArray(unittest.TestCase):
         self.assertTrue(np.array_equal(self.g_x2, np.ones_like(self.data)))
 
 
+class TestImperativeVarBaseGetItem(unittest.TestCase):
+    def test_getitem_with_long(self):
+        with fluid.dygraph.guard():
+            data = np.random.random((2, 80, 16128)).astype('float32')
+            var = fluid.dygraph.to_variable(data)
+            sliced = var[:, 10:, :var.shape[1]]  # var.shape[1] is 80L here
+            self.assertEqual(sliced.shape, [2, 70, 80])
+
+            sliced = var[:, var.shape[0]:, var.shape[0]:var.shape[1]]
+            self.assertEqual(sliced.shape, [2, 78, 78])
+
+    def test_getitem_with_float(self):
+        def test_float_in_slice_item():
+            with fluid.dygraph.guard():
+                data = np.random.random((2, 80, 16128)).astype('float32')
+                var = fluid.dygraph.to_variable(data)
+                sliced = var[:, 1.1:, :var.shape[1]]
+
+        self.assertRaises(Exception, test_float_in_slice_item)
+
+        def test_float_in_index():
+            with fluid.dygraph.guard():
+                data = np.random.random((2, 80, 16128)).astype('float32')
+                var = fluid.dygraph.to_variable(data)
+                sliced = var[1.1]
+
+        self.assertRaises(Exception, test_float_in_index)
+
+
 if __name__ == '__main__':
     unittest.main()
