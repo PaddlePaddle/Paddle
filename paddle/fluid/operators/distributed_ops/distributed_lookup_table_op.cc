98cf8f80215588d56d5900d1416d4ade883bb959
chengmo <cmchengmo@163.com>
2020-05-20 19:46:40 +0800

    add lookuptable.h

--- paddle/fluid/operators/distributed_ops/distributed_lookup_table_op.cc
+++ paddle/fluid/operators/distributed_ops/distributed_lookup_table_op.cc
@@ -12,12 +12,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#include "paddle/fluid/operators/distributed_ops/distributed_lookup_table_op.h"
 #include <algorithm>
 #include <string>
+#include <vector>
 #include "paddle/fluid/framework/data_type.h"
 #include "paddle/fluid/framework/op_registry.h"
-#include "paddle/fluid/operators/distributed/parameter_prefetch.h"
-#include "paddle/fluid/operators/math/math_function.h"
 
 namespace paddle {
 namespace operators {
@@ -135,28 +135,6 @@ if the Id is not in the sparse table, this operator will return a
 random value and set the value into the table for the next looking up.
 
 )DOC");
-  }
-};
-
-template <typename DeviceContext, typename T>
-class DistributedLookupTableKernel : public framework::OpKernel<T> {
- public:
-  void Compute(const framework::ExecutionContext &context) const override {
-    auto ids_vars = context.MultiInputVar("Ids");
-    auto emb_vars = context.MultiOutput<framework::Tensor>("Embeddings");
-
-    auto id_names = context.InputNames("Ids");
-    auto embedding_name = context.InputNames("W").front();
-    auto out_names = context.OutputNames("Outputs");
-
-    auto lookup_tables = context.Attr<std::vector<std::string>>("table_names");
-    auto height_sections =
-        context.Attr<std::vector<int64_t>>("height_sections");
-    auto endpoints = context.Attr<std::vector<std::string>>("endpoints");
-
-    operators::distributed::prefetchs(
-        id_names, out_names, embedding_name, false, lookup_tables, endpoints,
-        height_sections, context, context.scope());
   }
 };
 
