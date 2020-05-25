e718a1d3a82cd7aad6fea0c5e992c6c5d935323c
Zhen Wang <wangzhen31@baidu.com>
2020-05-25 08:18:49 +0000

    use quantized_counterpart instead.

--- python/paddle/fluid/contrib/slim/quantization/dygraph_quantization.py
+++ python/paddle/fluid/contrib/slim/quantization/dygraph_quantization.py
@@ -13,18 +13,17 @@
 # limitations under the License.
 
 import logging
-import six
+import sys
 from ....dygraph import layers
-from ....layer_helper import LayerHelper
-from ....dygraph.nn import Conv2D, Pool2D, BatchNorm, Linear
-from ....dygraph.base import to_variable
+from ....dygraph.nn import Conv2D, Linear
 from .... import core
-from .... import framework
+from .... import dygraph_utils
 from .... import unique_name
 from ....param_attr import ParamAttr
-from ....framework import _varbase_creator
+from ....framework import _varbase_creator, in_dygraph_mode
 from ....initializer import Constant
 from ....log_helper import get_logger
+from ....data_feeder import check_variable_and_dtype
 
 __all__ = ['quantize_qat']
 
@@ -85,10 +84,140 @@ class FakeQuant(layers.Layer):
         attrs = ('moving_rate', self._moving_rate, 'bit_length',
                  self._quant_bits, 'is_test', self._is_test)
 
-        out, out_scale, _, _ = core.ops.fake_quantize_dequantize_moving_average_abs_max(
+        out, _, _, _ = core.ops.fake_quantize_dequantize_moving_average_abs_max(
             input, self.scale, self.accum, self.state, quant_out, self.scale,
             self.state, self.accum, *attrs)
-        return out, out_scale
+        return out
+
+
+class QuantizedConv2D(layers.Layer):
+    def __init__(self, layer, weight_bits=8, activation_bits=8,
+                 moving_rate=0.9):
+        super(QuantizedConv2D, self).__init__()
+        # For Conv2D
+        self._groups = getattr(layer, '_groups')
+        self._stride = getattr(layer, '_stride')
+        self._padding = getattr(layer, '_padding')
+        self._dilation = getattr(layer, '_dilation')
+        self._act = getattr(layer, '_act')
+        self._use_cudnn = getattr(layer, '_use_cudnn')
+        self._dtype = getattr(layer, '_dtype')
+        self._l_type = getattr(layer, '_l_type')
+        self.weight = getattr(layer, 'weight')
+        self.bias = getattr(layer, 'bias')
+        # For FakeQuant
+        is_test = not layer.training
+        self._fake_quant_input = FakeQuant(layer.full_name(), moving_rate,
+                                           activation_bits, self._dtype,
+                                           is_test)
+
+        self._fake_quant_weight = FakeQuant(self.weight.name, moving_rate,
+                                            weight_bits, self._dtype, is_test)
+
+    def forward(self, input):
+        quant_input = self._fake_quant_input(input)
+        quant_weight = self._fake_quant_input(self.weight)
+
+        if self._l_type == 'conv2d':
+            attrs = ('strides', self._stride, 'paddings', self._padding,
+                     'dilations', self._dilation, 'groups', self._groups
+                     if self._groups else 1, 'use_cudnn', self._use_cudnn)
+            pre_bias = core.ops.conv2d(quant_input, quant_weight, *attrs)
+
+            pre_act = dygraph_utils._append_bias_in_dygraph(pre_bias, self.bias,
+                                                            1)
+            return dygraph_utils._append_activation_in_dygraph(pre_act,
+                                                               self._act)
+        attrs = {
+            'strides': self._stride,
+            'paddings': self._padding,
+            'dilations': self._dilation,
+            'groups': self._groups if self._groups else 1,
+            'use_cudnn': self._use_cudnn,
+            'use_mkldnn': False,
+        }
+
+        check_variable_and_dtype(quant_input, 'input',
+                                 ['float16', 'float32', 'float64'], 'Conv2D')
+        pre_bias = self._helper.create_variable_for_type_inference(
+            dtype=self._dtype)
+
+        self._helper.append_op(
+            type=self._l_type,
+            inputs={
+                'Input': quant_input,
+                'Filter': quant_weight,
+            },
+            outputs={"Output": pre_bias},
+            attrs=attrs)
+
+        if self.bias is not None:
+            pre_act = self._helper.create_variable_for_type_inference(
+                dtype=self._dtype)
+            self._helper.append_op(
+                type='elementwise_add',
+                inputs={'X': [pre_bias],
+                        'Y': [self.bias]},
+                outputs={'Out': [pre_act]},
+                attrs={'axis': 1})
+        else:
+            pre_act = pre_bias
+
+        return self._helper.append_activation(pre_act, act=self._act)
+
+
+class QuantizedLinear(layers.Layer):
+    def __init__(self, layer, weight_bits=8, activation_bits=8,
+                 moving_rate=0.9):
+        super(QuantizedLinear, self).__init__()
+        # For Linear
+        self._act = getattr(layer, '_act')
+        self._dtype = getattr(layer, '_dtype')
+        self.weight = getattr(layer, 'weight')
+        self.bias = getattr(layer, 'bias')
+        # For FakeQuant
+        is_test = not layer.training
+        self._fake_quant_input = FakeQuant(layer.full_name(), moving_rate,
+                                           activation_bits, self._dtype,
+                                           is_test)
+
+        self._fake_quant_weight = FakeQuant(self.weight.name, moving_rate,
+                                            weight_bits, self._dtype, is_test)
+
+    def forward(self, input):
+        quant_input = self._fake_quant_input(input)
+        quant_weight = self._fake_quant_input(self.weight)
+        pre_bias = _varbase_creator(dtype=input.dtype)
+        core.ops.matmul(quant_input, quant_weight, pre_bias, 'transpose_X',
+                        False, 'transpose_Y', False, "alpha", 1)
+        pre_act = dygraph_utils._append_bias_in_dygraph(
+            pre_bias, self.bias, axis=len(input.shape) - 1)
+
+        return dygraph_utils._append_activation_in_dygraph(pre_act, self._act)
+
+
+def _get_quantized_counterpart(layer,
+                               weight_bits=8,
+                               activation_bits=8,
+                               moving_rate=0.9):
+    quant_layers = tuple(_quant_layers_map.values())
+    quantized_counterpart = tuple('Quantized' + k
+                                  for k in _quant_layers_map.keys())
+
+    predicate = lambda value: isinstance(layer, value)
+    index_generator = (i for i, v in enumerate(quant_layers) if predicate(v))
+
+    try:
+        index = next(index_generator)
+    except StopIteration:
+        _logger.fatal("Don't find any quantized layer for {}.".format(
+            layer.full_name()))
+        sys.exit(-1)
+
+    module = __import__(__name__)
+    quantized_layer = getattr(module, quantized_counterpart[index])(
+        layer, weight_bits, activation_bits, moving_rate)
+    return quantized_layer
 
 
 def quantize_qat(model,
@@ -96,38 +225,6 @@ def quantize_qat(model,
                  activation_bits=8,
                  moving_rate=0.9,
                  quantizable_layer_type=['Conv2D', 'Linear']):
-    def _add_fake_quant(layer, input):
-        _logger.info("------------------------------------------------{}".
-                     format(layer.full_name()))
-        is_test = not layer.training
-        quant_inputs = []
-
-        # add fake_quant for activation inputs
-        for layer_in in input:
-            _logger.info("***********************************************{}".
-                         format(layer_in.name))
-            # prevent pruning the fake_quant_grad op
-            layer_in.stop_gradient = False
-            dtype = 'float64' if layer_in.dtype == core.VarDesc.VarType.FP64 else 'float32'
-            fake_quant_input = FakeQuant(layer_in.name, moving_rate,
-                                         activation_bits, dtype, is_test)
-            quant_input, _ = fake_quant_input(layer_in)
-            quant_inputs.append(quant_input)
-
-        # add fake_quant for weight inputs
-        params = layer._parameters.items()
-        for key, param in params:
-            _logger.info("key-------------------------:{}".format(key))
-            _logger.info("name-------------------------:{}".format(param.name))
-            if key.find('weight') != -1:
-                dtype = 'float64' if param.dtype == core.VarDesc.VarType.FP64 else 'float32'
-                fake_quant_weight = FakeQuant(param.name, moving_rate,
-                                              weight_bits, dtype, is_test)
-                quant_param, _ = fake_quant_weight(param)
-                layer._parameters[key] = quant_param
-
-        return tuple(quant_inputs)
-
     quant_layers = tuple(_quant_layers_map[layer]
                          if layer in _quant_layers_map else layer
                          for layer in quantizable_layer_type)
@@ -135,7 +232,18 @@ def quantize_qat(model,
         assert not isinstance(
             layer, str), "{} is unspported to be quantized.".format(layer)
 
-    processed_layers = filter(lambda layer: isinstance(layer, quant_layers),
-                              model.sublayers())
-    for layer in processed_layers:
-        layer.register_forward_pre_hook(_add_fake_quant)
+    for name, layer in model.named_sublayers():
+        if not isinstance(layer, quant_layers):
+            continue
+
+        scopes = name.split('.')
+        target = scopes[-1]
+        obj = model
+        parent = model
+        for i in range(len(scopes) - 1):
+            obj = getattr(parent, scopes[i])
+            parent = obj
+
+        quant_layer = _get_quantized_counterpart(layer, weight_bits,
+                                                 activation_bits, moving_rate)
+        setattr(obj, target, quant_layer)
