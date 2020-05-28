5f604c8cf0ce67d3b9e89cd4cdf9b39b9964941f
malin10 <malin10@baidu.com>
2020-05-28 11:14:52 +0800

    test=develop, bug fix

--- paddle/fluid/framework/downpour_worker.cc
+++ paddle/fluid/framework/downpour_worker.cc
@@ -776,20 +776,22 @@ void DownpourWorker::TrainFiles() {
         } catch (std::exception& e) {
           fprintf(stderr, "error message: %s\n", e.what());
           auto& ins_id_vec = device_reader_->GetInsIdVec();
+          size_t batch_size = device_reader_->GetCurBatchSize();
           std::string s = "";
           for (auto& ins_id : ins_id_vec) {
             if (s != "") s += ",";
             s += ins_id;
           }
-          fprintf(stderr, "ins_ids_vec: %s\n", s.c_str());
+          fprintf(stderr, "batch_size: %d, ins_ids_vec: %s\n", batch_size,
+                  s.c_str());
           s = "";
-          for (auto& param : all_param_) {
+          for (auto& param : *all_param_) {
             Variable* var = thread_scope_->FindVar(param);
             if (var == nullptr) {
               continue;
             }
-            Tensor* tensor;
-            int64_t len;
+            Tensor* tensor = nullptr;
+            int64_t len = 0;
             if (var->IsType<framework::LoDTensor>()) {
               tensor = var->GetMutable<LoDTensor>();
               len = tensor->numel();
@@ -804,6 +806,7 @@ void DownpourWorker::TrainFiles() {
             s += param + ":" + std::to_string(len) + ":";
             s += PrintLodTensor(tensor, 0, len);
             fprintf(stderr, "%s\n", s.c_str());
+            fflush(stderr);
             s = "";
           }
           throw e;
