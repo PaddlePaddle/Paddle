7c81f9f6cba464188a5d38d20377a15c522ea286
yangqingyou <yangqingyou@baidu.com>
2020-05-28 11:20:40 +0000

    Revert "Add crypto api (#24694)"
    This reverts commit 5a7a517cdeb099b1cba37f8b94da3917aed64948.
    
    Conflicts:
    	cmake/external/cryptopp.cmake

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -305,8 +305,4 @@ if (WITH_LITE)
     include(external/lite)
 endif (WITH_LITE)
 
-if (WITH_CRYPTO)
-    include(external/cryptopp)   # download, build, install cryptopp
-endif (WITH_CRYPTO)
-
 add_custom_target(third_party ALL DEPENDS ${third_party_deps})
