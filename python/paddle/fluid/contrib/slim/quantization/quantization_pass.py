30c72a0fdff6490dab0b3e4f34335b3d4fc5e303
slf12 <slf12thuss@163.com>
2020-05-29 04:45:17 +0000

    change 2018 to 2020 test=develop

--- python/paddle/fluid/contrib/slim/quantization/quantization_pass.py
+++ python/paddle/fluid/contrib/slim/quantization/quantization_pass.py
@@ -267,6 +267,151 @@ class QuantizationTransformPass(object):
         self._is_test = None
         self._global_step = None
 
+        self.create_var_map = {}
+        self.create_op_map = {}
+
+    def _create_new_node(self, graph, in_node):
+        key = ''
+        for inp in in_node.inputs:
+            key = key + inp.name()
+        key = key + in_node.name()
+        for inp in in_node.outputs:
+            key = key + inp.name()
+
+        if key in self.create_var_map.keys():
+            new_node = self.create_var_map[key]
+        elif in_node.is_ctrl_var():
+            new_node = graph.create_control_dep_var()
+            self.create_var_map[key] = new_node
+        else:
+            new_node = graph.create_var_node_from_desc(in_node.node.var())
+            self.create_var_map[key] = new_node
+        return new_node
+
+    def _copy_graph(self, graph, source_graph, op_node):
+        key = ''
+        for inp in op_node.inputs:
+            key = key + inp.name()
+        key = key + op_node.name()
+        for inp in op_node.outputs:
+            key = key + inp.name()
+        has_created = False
+        if key in self.create_op_map.keys():
+            new_op_node = self.create_op_map[key]
+            has_created = True
+        else:
+            new_op_node = graph.create_op_node_from_desc(op_node.node.op())
+            self.create_op_map[key] = new_op_node
+        if has_created:
+            return
+        for in_node in op_node.inputs:
+            new_node = self._create_new_node(graph, in_node)
+            graph.link_to(new_node, new_op_node)
+        for in_node in op_node.outputs:
+            new_node = self._create_new_node(graph, in_node)
+            graph.link_to(new_op_node, new_node)
+        for var_node in op_node.outputs:
+            for next_op_node in var_node.outputs:
+                self._copy_graph(graph, source_graph, next_op_node)
+        return
+
+    def _insert_func(self, graph, func, var_node, op):
+        tmp_program = Program()
+        startup_program = Program()
+        with program_guard(tmp_program, startup_program):
+            with unique_name.guard(var_node.name() + "_"):
+                in_node = data(
+                    var_node.name() + '_tmp_input',
+                    shape=var_node.shape(),
+                    dtype='float32')
+                out_node = func(in_node)
+                # loss shape must be 1 when minimize
+                loss = mean(out_node)
+                if not self._for_test:
+                    assert self._optimizer, "optimizer_func must be set when for_test is False"
+                    in_node.stop_gradient = False
+                    optimizer = self._optimizer()
+                    optimizer.minimize(loss)
+        with scope_guard(self._scope):
+            self._exe.run(startup_program)
+        tmp_graph = IrGraph(
+            core.Graph(tmp_program.desc), for_test=self._for_test)
+        in_node = tmp_graph._find_node_by_name(tmp_graph.all_var_nodes(),
+                                               in_node.name)
+        out_node = tmp_graph._find_node_by_name(tmp_graph.all_var_nodes(),
+                                                out_node.name)
+
+        in_node_params = []
+        in_op_node = []
+        for node in tmp_graph.all_var_nodes():
+            if node.inputs == [] and node.persistable():
+                in_node_params.append(node)
+        for node in tmp_graph.all_op_nodes():
+            if node.inputs == []:
+                in_op_node.append(node)
+        for node in in_node.outputs:
+            self._copy_graph(graph, tmp_graph, node)
+        for node in in_node_params:
+            for op_node in node.outputs:
+                self._copy_graph(graph, tmp_graph, op_node)
+        for node in in_op_node:
+            self._copy_graph(graph, tmp_graph, node)
+
+        target_in_node = graph._find_node_by_name(graph.all_var_nodes(),
+                                                  in_node.name())
+        target_out_node = graph._find_node_by_name(graph.all_var_nodes(),
+                                                   out_node.name())
+        loss_node = graph._find_node_by_name(graph.all_var_nodes(), loss.name)
+        outputs = target_in_node.outputs
+        for node in outputs:
+            graph.update_input_link(target_in_node, var_node, node)
+        graph.update_input_link(var_node, target_out_node, op)
+
+        # update grad
+        if not self._for_test:
+            op_out = op.outputs[0]
+            op_out_grad = graph._find_node_by_name(graph.all_var_nodes(),
+                                                   op_out.name() + "@GRAD")
+            # find op's gradient op, such as conv2d_grad
+            op_grad = op_out_grad.outputs[0]
+            target_out_grad_node = graph._find_node_by_name(
+                graph.all_var_nodes(), target_out_node.name() + "@GRAD")
+            in_node_grad = graph._find_node_by_name(
+                graph.all_var_nodes(), target_in_node.name() + "@GRAD")
+            in_node_grad_op = in_node_grad.inputs
+            # update op_grad's input
+            graph.update_input_link(var_node, target_out_node, op_grad)
+
+            op_grad_out = None
+            # find var_node's corresponding grad node
+            for node in op_grad.outputs:
+                if var_node.name() + "@GRAD" in node.name():
+                    op_grad_out = node
+            # update op_grad's output
+            if op_grad_out is not None:
+                graph.update_output_link(op_grad_out, target_out_grad_node,
+                                         op_grad)
+            else:
+                graph.link_to(op_grad, target_out_grad_node)
+
+            for node in in_node_grad_op:
+                graph.update_input_link(target_in_node, var_node, node)
+                if op_grad_out:
+                    graph.update_output_link(in_node_grad, op_grad_out, node)
+            # remove useless nodes
+            mean_grad = target_out_grad_node.inputs[0]
+            mean_out_grad = mean_grad.inputs[0]
+            fill_constant_node = mean_out_grad.inputs[0]
+            graph.safe_remove_nodes(mean_grad)
+            graph.safe_remove_nodes(mean_out_grad)
+            graph.safe_remove_nodes(fill_constant_node)
+            graph.safe_remove_nodes(in_node_grad)
+
+        graph.safe_remove_nodes(loss_node.inputs[0])
+        graph.safe_remove_nodes(loss_node)
+        graph.safe_remove_nodes(target_in_node)
+        return target_out_node
+
     def apply(self, graph):
         """
         Quantize the graph for training process. According to weight and
@@ -284,8 +429,6 @@ class QuantizationTransformPass(object):
         # marked the variable which has been dequantized.
         dequantized_vars = collections.OrderedDict()
         persistable_vars = [p.name() for p in graph.all_persistable_nodes()]
-        create_var_map = {}
-        create_op_map = {}
         processed_vars = collections.OrderedDict()
 
         def _quant_preprocess(op_node):
@@ -300,150 +443,6 @@ class QuantizationTransformPass(object):
             if user_skipped:
                 op_node.op()._set_attr("skip_quant", True)
 
-        def _create_new_node(in_node):
-            key = ''
-            for inp in in_node.inputs:
-                key = key + inp.name()
-            key = key + in_node.name()
-            for inp in in_node.outputs:
-                key = key + inp.name()
-
-            if key in create_var_map.keys():
-                new_node = create_var_map[key]
-            elif in_node.is_ctrl_var():
-                new_node = graph.create_control_dep_var()
-                create_var_map[key] = new_node
-            else:
-                new_node = graph.create_var_node_from_desc(in_node.node.var())
-                create_var_map[key] = new_node
-            return new_node
-
-        def _copy_graph(source_graph, op_node):
-            key = ''
-            for inp in op_node.inputs:
-                key = key + inp.name()
-            key = key + op_node.name()
-            for inp in op_node.outputs:
-                key = key + inp.name()
-            has_created = False
-            if key in create_op_map.keys():
-                new_op_node = create_op_map[key]
-                has_created = True
-            else:
-                new_op_node = graph.create_op_node_from_desc(op_node.node.op())
-                create_op_map[key] = new_op_node
-            if has_created:
-                return
-            for in_node in op_node.inputs:
-                new_node = _create_new_node(in_node)
-                graph.link_to(new_node, new_op_node)
-            for in_node in op_node.outputs:
-                new_node = _create_new_node(in_node)
-                graph.link_to(new_op_node, new_node)
-            for var_node in op_node.outputs:
-                for next_op_node in var_node.outputs:
-                    _copy_graph(source_graph, next_op_node)
-            return
-
-        def _insert_func(graph, func, var_node, op):
-            tmp_program = Program()
-            startup_program = Program()
-            with program_guard(tmp_program, startup_program):
-                with unique_name.guard(var_node.name() + "_"):
-                    in_node = data(
-                        var_node.name() + '_tmp_input',
-                        shape=var_node.shape(),
-                        dtype='float32')
-                    out_node = func(in_node)
-                    # loss shape must be 1 when minimize
-                    loss = mean(out_node)
-                    if not self._for_test:
-                        assert self._optimizer, "optimizer_func must be set when for_test is False"
-                        in_node.stop_gradient = False
-                        optimizer = self._optimizer()
-                        optimizer.minimize(loss)
-            with scope_guard(self._scope):
-                self._exe.run(startup_program)
-            tmp_graph = IrGraph(
-                core.Graph(tmp_program.desc), for_test=self._for_test)
-            in_node = tmp_graph._find_node_by_name(tmp_graph.all_var_nodes(),
-                                                   in_node.name)
-            out_node = tmp_graph._find_node_by_name(tmp_graph.all_var_nodes(),
-                                                    out_node.name)
-
-            in_node_params = []
-            in_op_node = []
-            for node in tmp_graph.all_var_nodes():
-                if node.inputs == [] and node.persistable():
-                    in_node_params.append(node)
-            for node in tmp_graph.all_op_nodes():
-                if node.inputs == []:
-                    in_op_node.append(node)
-            for node in in_node.outputs:
-                _copy_graph(tmp_graph, node)
-            for node in in_node_params:
-                for op_node in node.outputs:
-                    _copy_graph(tmp_graph, op_node)
-            for node in in_op_node:
-                _copy_graph(tmp_graph, node)
-
-            target_in_node = graph._find_node_by_name(graph.all_var_nodes(),
-                                                      in_node.name())
-            target_out_node = graph._find_node_by_name(graph.all_var_nodes(),
-                                                       out_node.name())
-            loss_node = graph._find_node_by_name(graph.all_var_nodes(),
-                                                 loss.name)
-            outputs = target_in_node.outputs
-            for node in outputs:
-                graph.update_input_link(target_in_node, var_node, node)
-            graph.update_input_link(var_node, target_out_node, op)
-
-            # update grad
-            if not self._for_test:
-                op_out = op.outputs[0]
-                op_out_grad = graph._find_node_by_name(graph.all_var_nodes(),
-                                                       op_out.name() + "@GRAD")
-                # find op's gradient op, such as conv2d_grad
-                op_grad = op_out_grad.outputs[0]
-                target_out_grad_node = graph._find_node_by_name(
-                    graph.all_var_nodes(), target_out_node.name() + "@GRAD")
-                in_node_grad = graph._find_node_by_name(
-                    graph.all_var_nodes(), target_in_node.name() + "@GRAD")
-                in_node_grad_op = in_node_grad.inputs
-                # update op_grad's input
-                graph.update_input_link(var_node, target_out_node, op_grad)
-
-                op_grad_out = None
-                # find var_node's corresponding grad node
-                for node in op_grad.outputs:
-                    if var_node.name() + "@GRAD" in node.name():
-                        op_grad_out = node
-                # update op_grad's output
-                if op_grad_out is not None:
-                    graph.update_output_link(op_grad_out, target_out_grad_node,
-                                             op_grad)
-                else:
-                    graph.link_to(op_grad, target_out_grad_node)
-
-                for node in in_node_grad_op:
-                    graph.update_input_link(target_in_node, var_node, node)
-                    if op_grad_out:
-                        graph.update_output_link(in_node_grad, op_grad_out,
-                                                 node)
-                # remove useless nodes
-                mean_grad = target_out_grad_node.inputs[0]
-                mean_out_grad = mean_grad.inputs[0]
-                fill_constant_node = mean_out_grad.inputs[0]
-                graph.safe_remove_nodes(mean_grad)
-                graph.safe_remove_nodes(mean_out_grad)
-                graph.safe_remove_nodes(fill_constant_node)
-                graph.safe_remove_nodes(in_node_grad)
-
-            graph.safe_remove_nodes(loss_node.inputs[0])
-            graph.safe_remove_nodes(loss_node)
-            graph.safe_remove_nodes(target_in_node)
-            return target_out_node
-
         def _transform_forward(graph, op):
             op.op()._set_attr("quantization_type", "qat_with_weight")
             inputs = op.inputs
@@ -453,30 +452,30 @@ class QuantizationTransformPass(object):
                 if var_node.name() in dequantized_vars:
                     dequant_var_node = dequantized_vars[var_node.name()]
                 else:
+
                     name = var_node.name()
+                    if name in processed_vars:
+                        continue
+
                     if var_node.name() in persistable_vars:
                         is_weight = True
                     else:
                         is_weight = False
 
                     if is_weight and self._weight_preprocess_func is not None:
-                        var_node = _insert_func(
+                        var_node = self._insert_func(
                             graph, self._weight_preprocess_func, var_node, op)
                     if not is_weight and self._act_preprocess_func is not None:
-                        var_node = _insert_func(
+                        var_node = self._insert_func(
                             graph, self._act_preprocess_func, var_node, op)
 
                     if is_weight and self._weight_quantize_func is not None:
-                        if name in processed_vars:
-                            continue
-                        target_out_node = _insert_func(
+                        target_out_node = self._insert_func(
                             graph, self._weight_quantize_func, var_node, op)
                         processed_vars[name] = target_out_node
                         continue
                     elif not is_weight and self._act_quantize_func is not None:
-                        if name in processed_vars:
-                            continue
-                        target_out_node = _insert_func(
+                        target_out_node = self._insert_func(
                             graph, self._act_quantize_func, var_node, op)
                         processed_vars[name] = target_out_node
                         continue
