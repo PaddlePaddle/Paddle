8ee9b7a7ce7d56a358340e4c7b7ccd30f6b6657e
Aurelius84 <liujiezhangbupt@gmail.com>
2020-06-02 09:27:26 +0800

    add example of LoDTensorArray test=develop

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -1136,6 +1136,16 @@ def reverse(x, axis):
           data = fluid.layers.assign(np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]], dtype='float32')) # [[0., 1., 2.], [3., 4., 5.], [6., 7., 8.]]
           result1 = fluid.layers.reverse(data, 0) # [[6., 7., 8.], [3., 4., 5.], [0., 1., 2.]]
           result2 = fluid.layers.reverse(data, [0, 1]) # [[8., 7., 6.], [5., 4., 3.], [2., 1., 0.]]
+
+          # example of LoDTensorArray
+          data1 = fluid.layers.assign(np.array([[0, 1, 2]], dtype='float32'))
+          data2 = fluid.layers.assign(np.array([[3, 4, 5]], dtype='float32'))
+          tensor_array = fluid.layers.create_array(dtype='float32')
+          i = fluid.layers.fill_constant(shape=[1], dtype='int64', value=0)
+          fluid.layers.array_write(data1, i, tensor_array)
+          fluid.layers.array_write(data1, i+1, tensor_array)
+
+          reversed_tensor_array = fluid.layers.reverse(tensor_array, 0) # {[[3, 4, 5]], [[0, 1, 2]]}
     """
     check_variable_and_dtype(
         x, 'x', ('float32', 'float64', 'int32', 'int64', 'uint8'), 'reverse')
