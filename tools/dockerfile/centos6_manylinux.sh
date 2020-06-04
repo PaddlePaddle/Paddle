a751e9ab72e2e5aea346786df34abd50b8ce2b37
tianshuo78520a <707759223@qq.com>
2020-06-04 19:25:12 +0800

    chang file

--- tools/dockerfile/centos6_manylinux.sh
+++ tools/dockerfile/centos6_manylinux.sh
@@ -9,13 +9,13 @@ sed 's/<NCCL_MAKE_OPTS>/NVCC_GENCODE="-gencode=arch=compute_35,code=sm_35 -genco
 #docker build -t ${REPO}/paddle_manylinux_devel:cuda9.0_cudnn7 -f Dockerfile.tmp .
 #docker push ${REPO}/paddle_manylinux_devel:cuda9.0_cudnn7
 
-#sed 's/<baseimg>/10.0-cudnn7-devel-centos6/g' Dockerfile.centos.gcc48 | \
-#sed 's/<NCCL_MAKE_OPTS>/NVCC_GENCODE="-gencode=arch=compute_35,code=sm_35 -gencode=arch=compute_50,code=sm_50 -gencode=arch=compute_52,code=sm_52 -gencode=arch=compute_60,code=sm_60 -gencode=arch=compute_60,code=compute_60 -gencode=arch=compute_61,code=sm_61 -gencode=arch=compute_62,code=sm_62 -gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75"/g'> Dockerfile.tmp
+sed 's/<baseimg>/10.0-cudnn7-devel-centos6/g' Dockerfile.centos.gcc48 | \
+sed 's/<NCCL_MAKE_OPTS>/NVCC_GENCODE="-gencode=arch=compute_35,code=sm_35 -gencode=arch=compute_50,code=sm_50 -gencode=arch=compute_52,code=sm_52 -gencode=arch=compute_60,code=sm_60 -gencode=arch=compute_60,code=compute_60 -gencode=arch=compute_61,code=sm_61 -gencode=arch=compute_62,code=sm_62 -gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75"/g'> Dockerfile.tmp
 #docker build -t ${REPO}/paddle_manylinux_devel:cuda10.0_cudnn7 -f Dockerfile.tmp .
 #docker push ${REPO}/paddle_manylinux_devel:cuda10.0_cudnn7
 
 sed 's/<baseimg>/10.1-cudnn7-devel-centos6/g' Dockerfile.centos.gcc82 | \
 sed 's/<NCCL_MAKE_OPTS>/NVCC_GENCODE="-gencode=arch=compute_35,code=sm_35 -gencode=arch=compute_50,code=sm_50 -gencode=arch=compute_52,code=sm_52 -gencode=arch=compute_60,code=sm_60 -gencode=arch=compute_60,code=compute_60 -gencode=arch=compute_61,code=sm_61 -gencode=arch=compute_62,code=sm_62 -gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75"/g'> Dockerfile.tmp
-docker build --no-cache -t tianshuo78520a/paddlepaddle:cuda10.1_cudnn7-manylinux -f Dockerfile.tmp .
+#docker build --no-cache -t tianshuo78520a/paddlepaddle:cuda10.1_cudnn7-manylinux -f Dockerfile.tmp .
 #docker build -t ${REPO}/paddle_manylinux_devel:cuda10.1_cudnn7 -f Dockerfile.tmp .
 #docker push ${REPO}/paddle_manylinux_devel:cuda10.1_cudnn7
--- tools/dockerfile/ci_dockerfile.sh
+++ tools/dockerfile/ci_dockerfile.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sed 's/<baseimg>/9.0-cudnn7-devel-ubuntu16.04/g' ./Dockerfile.ubuntu.gcc48 >Dockerfile.ci_ubuntu_gcc8
+sed 's/<baseimg>/9.0-cudnn7-devel-ubuntu16.04/g' ./Dockerfile.ubuntu.gcc48 >Dockerfile.ci_cuda10_cudnn7_gcc48_ubuntu16
 dockerfile_line=`wc -l Dockerfile.ci_ubuntu_gcc8|awk '{print $1}'`
 sed -i "${dockerfile_line}i RUN wget --no-check-certificate https://pslib.bj.bcebos.com/openmpi-1.4.5.tar.gz && tar -xzf openmpi-1.4.5.tar.gz && \
     cd openmpi-1.4.5 && ./configure --prefix=/usr/local && make all -j8 && make install -j8 && \
     export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH && export PATH=/usr/local/bin:$PATH && cd .. && \
     rm -rf openmpi-1.4.5.tar.gz && pip --no-cache-dir install mpi4py && ln -fs /bin/bash /bin/sh && \
     apt-get install libprotobuf-dev -y" Dockerfile.ci_ubuntu_gcc8
+
+
+sed 's/<baseimg>/9.0-cudnn7-devel-centos6/g' ./Dockerfile.ubuntu.gcc48 >Dockerfile.ci_cuda9_cudnn7_gcc48_py35_centos6
+sed -i 's#<install_gcc>#RUN apt-get update \
+      WORKDIR /usr/bin \
+      RUN apt install -y gcc-4.8 g++-4.8 \&\& cp gcc gcc.bak \&\& cp g++ g++.bak \&\& rm gcc \&\& rm g++ \&\& ln -s gcc-4.8 gcc \&\& ln -s g++-4.8 g++ #g' Dockerfile.ci_cuda9_cudnn7_gcc48_py35_centos6
