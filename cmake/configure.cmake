fdad55ac70c38ef17a165de25b9a1027ad2a3b01
silingtong123 <silingtong@baidu.com>
2020-06-04 06:43:29 +0000

    test=develop, remove the macro of PADDLE_DLL_INFERENCE

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -48,6 +48,7 @@ if(WIN32)
   SET(CMAKE_C_RESPONSE_FILE_LINK_FLAG "@")
   SET(CMAKE_CXX_RESPONSE_FILE_LINK_FLAG "@")
 
+  add_definitions(-DPADDLE_DLL_INFERENCE)
   # set definition for the dll export
   if (NOT MSVC)
     message(FATAL "Windows build only support msvc. Which was binded by the nvcc compiler of NVIDIA.")
