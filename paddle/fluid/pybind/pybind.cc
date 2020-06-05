b384d1323e0ef617185d9627d8e138f6e010242b
gongweibao <weibao.gong@gmail.com>
2020-06-05 09:18:29 +0000

    change interface test=develop

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -1501,14 +1501,15 @@ All parameter, weight, gradient are variables in Paddle.
   m.def("is_compiled_with_mkldnn", IsCompiledWithMKLDNN);
   m.def("is_compiled_with_brpc", IsCompiledWithBrpc);
   m.def("is_compiled_with_dist", IsCompiledWithDIST);
-  m.def("run_cmd",
-        [](const std::string &cmd, int time_out = 1 * 60 * 1000,
-           int sleep_inter = 3000) -> const std::string {
-          return paddle::framework::shell_get_command_output(cmd, time_out,
-                                                             sleep_inter);
-        },
-        py::arg("cmd"), py::arg("time_out") = 1 * 60 * 1000,
-        py::arg("sleep_inter") = -1, py::arg("print_cmd") = false);
+  m.def(
+      "run_cmd",
+      [](const std::string &cmd, int time_out = 1 * 60 * 1000,
+         int sleep_inter = 3000, bool print_cmd = false) -> const std::string {
+        return paddle::framework::shell_get_command_output(cmd, time_out,
+                                                           sleep_inter);
+      },
+      py::arg("cmd"), py::arg("time_out") = 1 * 60 * 1000,
+      py::arg("sleep_inter") = -1, py::arg("print_cmd") = false);
   m.def("shell_execute_cmd",
         [](const std::string &cmd, std::string &output,
            int time_out = 1 * 60 * 1000, int sleep_inter = 3000,
