2e526ed4b1bc2b5090e966d4714b5da00bf4afe2
tianshuo78520a <707759223@qq.com>
2020-06-08 16:24:19 +0800

    fix manylinux dockerfile

--- tools/dockerfile/centos6_manylinux.sh
+++ tools/dockerfile/centos6_manylinux.sh
@@ -4,17 +4,17 @@ set -xe
 REPO="${REPO:-paddledocker}"
 
 function make_cuda9cudnn7(){
-  sed 's/<baseimg>/9.0-cudnn7-devel-centos6/g' Dockerfile.centos >Dockerfile.tmp
+  sed 's/<baseimg>/9.0-cudnn7-devel-centos6/g' tools/dockerfile/Dockerfile.centos >Dockerfile.tmp
 }
 
 
 function make_cuda10cudnn7() {
-  sed 's/<baseimg>/10.0-cudnn7-devel-centos6/g' Dockerfile.centos >Dockerfile.tmp
+  sed 's/<baseimg>/10.0-cudnn7-devel-centos6/g' tools/dockerfile/Dockerfile.centos >Dockerfile.tmp
 }
 
 
 function make_cuda101cudnn7() {
-  sed 's/<baseimg>/10.1-cudnn7-devel-centos6/g' Dockerfile.centos >Dockerfile.tmp
+  sed 's/<baseimg>/10.1-cudnn7-devel-centos6/g' tools/dockerfile/Dockerfile.centos >Dockerfile.tmp
   sed -i "s#COPY build_scripts /build_scripts#COPY build_scripts /build_scripts \nRUN bash build_scripts/install_gcc.sh gcc82 \nENV PATH=/usr/local/gcc-8.2/bin:\$PATH#g" Dockerfile.tmp
 }
 
