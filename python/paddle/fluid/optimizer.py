c242b0cb38faf00bd01a3b5320f4eed51f81b29f
sandyhouse <lilong12@baidu.com>
2020-06-15 08:42:16 +0000

    refine, test=develop

--- python/paddle/fluid/optimizer.py
+++ python/paddle/fluid/optimizer.py
@@ -724,8 +724,8 @@ class Optimizer(object):
             target_block = framework.default_main_program().blocks[
                 current_block.backward_block_idx]
         self._update_param_device_map(params_grads, target_block)
-        params_grads = append_regularization_ops(
-            params_grads, self.regularization)
+        params_grads = append_regularization_ops(params_grads,
+                                                 self.regularization)
 
         optimize_ops = self._create_optimization_pass(params_grads)
         if table_optimize_op is not None:
