71f76f236db6fe4200056044f566c8c3b5a6e818
zhouwei25 <zhouwei25@baidu.com>
2020-06-16 04:45:44 +0000

    add API doc and example code for optimizer.set_lr,test=develop

--- python/paddle/fluid/optimizer.py
+++ python/paddle/fluid/optimizer.py
@@ -302,16 +302,60 @@ class Optimizer(object):
         """
         .. note::
           **This API is ONLY available in Dygraph mode**
-          
+        
+        Set the value of the learning rate manually in the optimizer. If the optimizer use LearningRateDecay,
+        this API cannot be invoked, because it will lead to conflict.
+
         Args:
-            value (float|Variable): 
+            value (float|Variable): the value of learning rate
+
+        Returns:
+            None
+          
+        Examples:
+            .. code-block:: python
+
+                import paddle.fluid as fluid
+                        
+                with fluid.dygraph.guard():
+                    linear = fluid.dygraph.nn.Linear(10, 10)
+
+                    adam = fluid.optimizer.Adam(0.1, parameter_list=linear.parameters())
+
+                    # set learning rate manually by python float value
+                    lr_list = [0.2, 0.3, 0.4, 0.5, 0.6]
+                    for i in range(5):
+                        adam.set_lr(lr_list[i])
+                        lr = adam.current_step_lr()
+                        print("current lr is {}".format(lr))
+                    # Print:
+                    #    current lr is 0.2
+                    #    current lr is 0.3
+                    #    current lr is 0.4
+                    #    current lr is 0.5
+                    #    current lr is 0.6
+
+
+                    # set learning rate manually by framework Variable
+                    lr_var = fluid.layers.create_global_var(
+                        shape=[1], value=0.7, dtype='float32')
+                    adam.set_lr(lr_var)
+                    lr = adam.current_step_lr()
+                    print("current lr is {}".format(lr))
+                    # Print:
+                    #    current lr is 0.7
+
+
+
         """
-        check_type(value, 'value', (framework.Variable, float), 'set_lr')
+        if not isinstance(value, (framework.Variable, float)):
+            raise TypeError(
+                "The type of 'value' in optimizer.set_lr must be (float, Variable), but received %s."
+                % (type(value)))
         if isinstance(self._learning_rate, LearningRateDecay):
             raise RuntimeError(
                 "optimizer's learning rate can't be LearningRateDecay when invoke this API, because this will lead to conflict."
             )
-
         if isinstance(value, float):
             self._learning_rate = value
             current_lr = self._global_learning_rate()
