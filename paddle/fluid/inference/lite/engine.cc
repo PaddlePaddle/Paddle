04ed80c107618618a03334a71f8dd5e4be6ced14
石晓伟 <39303645+Shixiaowei02@users.noreply.github.com>
2020-06-17 15:18:21 +0800

    fix repeat definitions in liengine.cc, test=develop (#25020)

--- paddle/fluid/inference/lite/engine.cc
+++ paddle/fluid/inference/lite/engine.cc
@@ -18,6 +18,10 @@
 
 #include "paddle/fluid/inference/lite/engine.h"
 
+#include "lite/api/paddle_use_kernels.h"
+#include "lite/api/paddle_use_ops.h"
+#include "lite/api/paddle_use_passes.h"
+
 namespace paddle {
 namespace inference {
 namespace lite {
