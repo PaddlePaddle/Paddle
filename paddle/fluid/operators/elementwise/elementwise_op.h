e06be51d4bd26c54ad5c7adb7974c545c711f926
Jacek Czaja <jacek.czaja@intel.com>
2020-06-18 12:54:07 +0200

    [oneDNN]elementwise_add and elementwise_mul int8 support (#24984)
    * Start implementing int8 eltwise add
    
    test=develop
    
    * - Fix to Michal PR
    
    * - Fix
    
    test=develop
    
    * - Lint fixes
    
    test=develop
    
    * - Added checking if elementwise_mul can be used
    
    test=develop
    
    * - Added attribs to skip_attrs_set
    
    test=develop
    
    * - Improved broadcasting
    
    test=develop
    
    - fixes to compilation
    
    - fix
    
    - fix
    
    - Lint fixes
    
    test=develop
    
    * - removed redundant condition
    
    test=develop
    
    Co-authored-by: Michal Gallus <michal.gallus@intel.com>

--- paddle/fluid/operators/elementwise/elementwise_op.h
+++ paddle/fluid/operators/elementwise/elementwise_op.h
@@ -100,15 +100,7 @@ class ElementwiseOp : public framework::OperatorWithKernel {
     auto input_data_type = OperatorWithKernel::IndicateVarDataType(ctx, "X");
 
 #ifdef PADDLE_WITH_MKLDNN
-    auto CanMKLDNNElementwiseAddBeUsed = [&]() {
-      int axis = ctx.Attr<int>("axis");
-      int rankdiff = ctx.Input<Tensor>("X")->dims().size() -
-                     ctx.Input<Tensor>("Y")->dims().size();
-      return (rankdiff == 0) || (axis == -1) || (axis == rankdiff);
-    };
-
-    if (platform::CanMKLDNNBeUsed(ctx) &&
-        (ctx.Type() != "elementwise_add" || CanMKLDNNElementwiseAddBeUsed())) {
+    if (platform::CanMKLDNNBeUsed(ctx)) {
       return framework::OpKernelType(input_data_type, ctx.GetPlace(),
                                      framework::DataLayout::kMKLDNN,
                                      framework::LibraryType::kMKLDNN);
@@ -148,6 +140,21 @@ class ElementwiseOpMaker : public framework::OpProtoAndCheckerMaker {
         .SetDefault("");
     AddAttr<std::string>("y_data_format", "This parameter is no longer used.")
         .SetDefault("");
+    /* int8 parameters */
+    AddAttr<bool>("use_quantizer",
+                  "(bool, default false) "
+                  "Set to true for operators that should be quantized and use "
+                  "int8 kernel. Only used on CPU.")
+        .SetDefault(false);
+    AddAttr<float>("Scale_x",
+                   "(float, default 1.0f), The quantize scale of X tensor")
+        .SetDefault(1.0f);
+    AddAttr<float>("Scale_y",
+                   "(float, default 1.0f), The quantize scale of Y tensor")
+        .SetDefault(1.0f);
+    AddAttr<float>("Scale_out",
+                   "(float, default 1.0f), The quantize scale of output data")
+        .SetDefault(1.0f);
     AddOpComment();
   }
 
