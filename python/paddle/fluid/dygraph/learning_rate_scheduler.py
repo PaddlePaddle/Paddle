97386ef7eafffe7d3532a70d34d14084c8567f9e
zhouwei25 <zhouwei25@baidu.com>
2020-06-22 06:31:52 +0000

    fix sample code error of fluid.dygraph.NaturalExpDecay,test=release/1.8

--- python/paddle/fluid/dygraph/learning_rate_scheduler.py
+++ python/paddle/fluid/dygraph/learning_rate_scheduler.py
@@ -171,15 +171,18 @@ class NaturalExpDecay(LearningRateDecay):
     Examples:
         .. code-block:: python
 
-          import paddle.fluid as fluid
-          base_lr = 0.1
-          with fluid.dygraph.guard():
-              sgd_optimizer = fluid.optimizer.SGD(
-        	      learning_rate=fluid.dygraph.NaturalExpDecay(
-	    	            learning_rate=base_lr,
-        		    decay_steps=10000,
-		            decay_rate=0.5,
-		            staircase=True))
+            import paddle.fluid as fluid
+            base_lr = 0.1
+            with fluid.dygraph.guard():
+                emb = fluid.dygraph.Embedding([10, 10])
+                sgd_optimizer = fluid.optimizer.SGD(
+                        learning_rate=fluid.dygraph.NaturalExpDecay(
+                            learning_rate=base_lr,
+                            decay_steps=10000,
+                            decay_rate=0.5,
+                            staircase=True),
+                        parameter_list=emb.parameters())
+
 
     """
 
