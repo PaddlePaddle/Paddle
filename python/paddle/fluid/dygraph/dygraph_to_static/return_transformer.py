aa825cf225b7d4e3d2c2df1287372581b697fff9
zhhsplendid <zhhsplendid@163.com>
2020-06-23 03:07:45 +0000

    Add return_transformer, not tested. test=develop

---
+++ python/paddle/fluid/dygraph/dygraph_to_static/return_transformer.py
@@ -0,0 +1,225 @@
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import gast
+
+from paddle.fluid import unique_name
+from paddle.fluid.dygraph.dygraph_to_static.utils import index_in_list
+from paddle.fluid.dygraph.dygraph_to_static.break_continue_transformer import ForToWhileTransformer
+
+__all__ = ['ReturnTransformer']
+
+# Constant for the name of the variable which stores the boolean state that we
+# should return
+RETURN_PREFIX = '__return'
+
+# Constant for the name of the variable which stores the final return value
+RETURN_VALUE_PREFIX = '__return_value'
+
+
+class ReturnPreAnalysisVisitor(gast.NodeVisitor):
+    """
+    Visits gast Tree and pre-analyze the information about 'return'.
+    """
+
+    def __init__(self, wrapper_root):
+        self.wrapper_root = wrapper_root
+        self.root = wrapper_root.node
+
+        # A list to store where the current function is.
+        self.function_def = []
+
+        # Mapping from gast.FunctionDef node to the number of return statements
+        # Python allows define function inside function so we have to handle it
+        self.count_return = {}
+        self.visit(self.root)
+
+    def visit_FunctionDef(self, node):
+        self.function_def.append(node)
+        self.generic_visit(node)
+        self.function_def.pop()
+        return node
+
+    def visit_Return(self, node):
+        assert len(
+            self.function_def) > 0, "Found 'return' statement out of function."
+        cur_func = self.function_def[-1]
+        if cur_func in self.count_return:
+            self.count_return[cur_func] += 1
+        else:
+            self.count_return[cur_func] = 1
+        self.generic_visit(node)
+
+    def get_func_return_count(self, func_node):
+        return self.count_return[func_node]
+
+
+class ReturnTransformer(gast.NodeTransformer):
+    """
+    Transforms return statements into equivalent python statements containing
+    only one return statement at last. The basic idea is using a return value
+    variable to store the early return statements. For example:
+
+    # Before transforming:
+    if some_condition:
+        statements_in_condition
+        return foo
+    statements_out_condition
+    return bar
+
+    # After transforming
+
+    if some_condition:
+        statements_in_condition
+        return_value = foo
+    else
+        statements_out_condition
+        return_value = bar
+    return return_value
+    """
+
+    def __init__(self, wrapper_root):
+        self.wrapper_root = wrapper_root
+        self.root = wrapper_root.node
+        self.pre_visitor = ReturnPreAnalysisVisitor(self.pre_visitor)
+        self.ancestor_nodes = []
+
+        # The name of the variable which stores the final return value
+        # Mapping from FunctionDef node to string
+        self.return_value_name = {}
+        # The names of the variable which stores the boolean state that skip
+        # statments. Mapping from FunctionDef node to list
+        self.return_name = {}
+        # A list of FunctionDef to store where the current function is.
+        self.function_def = []
+
+    def transform(self):
+        if self.pre_visitor.get_count_return() >= 2:
+            # We only have to transform when there are >= 2 return statements
+            self.visit(self.root)
+
+    def generic_visit(self, node):
+        # Because we change ancestor nodes during visit_Return, not current
+        # node, original generic_visit of NodeTransformer will visit node
+        # which may be deleted. To prevent that node being added into
+        # transformed AST, We self-write a generic_visit and visit
+        for field, value in gast.iter_fields(node):
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item, gast.AST):
+                        self.visit(item)
+            elif isinstance(value, gast.AST):
+                self.visit(value)
+
+    def visit(self, node):
+        """
+        Self-defined visit for appending ancestor
+        """
+        self.ancestor_nodes.append(node)
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        ret = visitor(node)
+        self.ancestor_nodes.pop()
+        return ret
+
+    def visit_FunctionDef(self, node):
+        self.function_def.append(node)
+        self.return_value_name[node] = None
+        self.return_name[node] = []
+
+        self.generic_visit(node)
+
+        # append final return statement
+        value_name = self.return_value_name[node]
+        if value_name is not None:
+            node.body.append(
+                gast.Return(value=gast.Name(
+                    id=value_name,
+                    ctx=Load(),
+                    annotation=None,
+                    type_comment=None)))
+        # Prepend control flow boolean nodes such as '__return@1 = False'
+        for name in self.return_name[node]:
+            assign_false_node = create_fill_constant_node(name, False)
+            node.body.insert(0, assign_false_node)
+        self.function_def.pop()
+        return node
+
+    def visit_Return(self, node):
+        cur_func_node = self.function_def[-1]
+        if self.pre_visitor.get_func_return_count(cur_func_node) <= 1:
+            return node
+        return_name = unique_name.generate(RETURN_PREFIX)
+        self.return_name[cur_func_node].append(return_name)
+        for ancestor_index in reversed(range(len(self.ancestor_nodes) - 1)):
+            ancestor = self.ancestor_nodes[ancestor_index]
+            cur_node = self.ancestor_nodes[ancestor_index + 1]
+            if hasattr(ancestor,
+                       "body") and index_in_list(ancestor.body, cur_node) != -1:
+                if cur_node == node:
+                    self._replace_return_in_stmt_list(ancestor.body, cur_node,
+                                                      return_name)
+                self._replace_after_node_to_if_in_stmt_list(
+                    ancestor.body, cur_node, return_name)
+            elif hasattr(ancestor, "orelse") and index_in_list(ancestor.orelse,
+                                                               cur_node) != -1:
+                if cur_node == node:
+                    self._replace_return_in_stmt_list(ancestor.body, cur_node,
+                                                      return_name)
+                self._replace_after_node_to_if_in_stmt_list(
+                    ancestor.orelse, cur_node, return_name)
+
+    def _replace_return_in_stmt_list(self, stmt_list, return_node, return_name):
+        i = index_in_list(stmt_list, return_node)
+        if i == -1:
+            return False
+        assign_nodes = [create_fill_constant_node(return_name, True)]
+        if return_node.value is not None:
+            cur_func_node = self.function_def[-1]
+            if self.return_value_name[cur_func_node] is None:
+                self.return_value_name[cur_func_node] = unique_name.generate(
+                    RETURN_VALUE_PREFIX)
+            assign_nodes.append(
+                gast.Assign(
+                    targets=[
+                        Name(
+                            id=self.return_value_name[cur_func_node],
+                            ctx=Store(),
+                            annotation=None)
+                    ],
+                    value=return_node.value))
+        stmt_list[i:] = assign_nodes
+        return True
+
+    def _replace_after_node_to_if_in_stmt_list(self, stmt_list, node,
+                                               return_name):
+        i = index_in_list(stmt_list, node)
+        if i < 0 or i >= len(stmt_list):
+            return False
+        if i == len(stmt_list) - 1:
+            # No need to add, we consider this as added successfully
+            return True
+        if_stmt = gast.If(test=gast.UnaryOp(
+            op=gast.Not(),
+            operand=gast.Name(
+                id=return_name,
+                ctx=gast.Store(),
+                annotation=None,
+                type_comment=None)),
+                          body=stmt_list[i + 1:],
+                          orelse=[])
+        stmt_list[i + 1:] = [if_stmt]
+        return True
