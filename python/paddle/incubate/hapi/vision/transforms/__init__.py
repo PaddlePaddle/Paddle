402b611ddd7cd4c5f97e257f778284239636e0e2
root <root@yq01-gpu-255-125-22-01.epc.baidu.com>
2020-06-23 07:41:24 +0000

    add transform, test=develop

--- python/paddle/incubate/hapi/vision/transforms/__init__.py
+++ python/paddle/incubate/hapi/vision/transforms/__init__.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,10 @@
 # limitations under the License.
 
 from . import transforms
-from .transforms import *
+from . import functional
 
-__all__ = transforms.__all__
+from .transforms import *
+from .functional import *
+
+__all__ = transforms.__all__ \
+        + functional.__all__
