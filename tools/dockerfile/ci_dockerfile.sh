96afc27fc2ffbc8e8ca1b86095e00ccd508d5da9
tianshuo78520a <707759223@qq.com>
2020-06-25 16:36:27 +0800

    change ci dockerfile of gcc82

--- tools/dockerfile/ci_dockerfile.sh
+++ tools/dockerfile/ci_dockerfile.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 function make_ubuntu_dockerfile(){
-  sed 's/<baseimg>/10.1-cudnn7-devel-ubuntu16.04/g' ./Dockerfile.ubuntu >Dockerfile.cuda10_cudnn7_gcc48_ubuntu16
+  sed 's/<baseimg>/10.1-cudnn7-devel-ubuntu16.04/g' ./Dockerfile.ubuntu >Dockerfile.cuda10_cudnn7_gcc82_ubuntu16
   dockerfile_line=`wc -l Dockerfile.cuda10_cudnn7_gcc48_ubuntu16|awk '{print $1}'`
-  sed -i "${dockerfile_line}i RUN wget --no-check-certificate https://pslib.bj.bcebos.com/openmpi-1.4.5.tar.gz && tar -xzf openmpi-1.4.5.tar.gz && \
-      cd openmpi-1.4.5 && ./configure --prefix=/usr/local && make all -j8 && make install -j8 && \
-      export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH && export PATH=/usr/local/bin:$PATH && cd .. && \
-      rm -rf openmpi-1.4.5.tar.gz && pip --no-cache-dir install mpi4py && ln -fs /bin/bash /bin/sh && \
-      apt-get install libprotobuf-dev -y" Dockerfile.cuda10_cudnn7_gcc48_ubuntu16
-  sed -i 's#<install_gcc>#RUN apt-get update \
-      WORKDIR /usr/bin \
-      RUN apt install -y gcc-4.8 g++-4.8 \&\& cp gcc gcc.bak \&\& cp g++ g++.bak \&\& rm gcc \&\& rm g++ \&\& ln -s gcc-4.8 gcc \&\& ln -s g++-4.8 g++ #g' Dockerfile.cuda10_cudnn7_gcc48_ubuntu16
+
+  sed -i 's#liblzma-dev#liblzma-dev openmpi-bin openmpi-doc libopenmpi-dev#g' Dockerfile.cuda10_cudnn7_gcc82_ubuntu16
+
+  sed -i 's#<install_gcc>#WORKDIR /usr/bin \
+      COPY tools/dockerfile/build_scripts /build_scripts \
+      RUN bash /build_scripts/install_gcc.sh gcc82 \&\& rm -rf /build_scripts \
+      RUN cp gcc gcc.bak \&\& cp g++ g++.bak \&\& rm gcc \&\& rm g++ \
+      RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/local/bin/gcc \
+      RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/local/bin/g++ \
+      RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/bin/gcc \
+      RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/bin/g++ \
+      ENV PATH=/usr/local/gcc-8.2/bin:$PATH #g' Dockerfile.cuda10_cudnn7_gcc82_ubuntu16
+
 }
 
 
