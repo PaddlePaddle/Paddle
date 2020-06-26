eee5914a5ee97b737e728420c46c991116304eb1
ReeseWang <rewang@nvidia.com>
2020-06-26 19:15:48 +0800

    use Ex API when using fp16, test=develop

--- paddle/fluid/operators/math/blas_impl.cu.h
+++ paddle/fluid/operators/math/blas_impl.cu.h
@@ -428,30 +428,36 @@ void Blas<platform::CUDADeviceContext>::BatchedGEMM(
   const int64_t strideC = M * N;
 
 #if CUDA_VERSION >= 9010
-  if (FLAGS_enable_cublas_tensor_op_math && std::is_same<T, float>::value) {
+  bool tensor_core_avail = context_.tensor_core_available();
+  if ((tensor_core_avail && std::is_same<T, __half>::value) ||
+      std::is_same<T, float>::value) {
     cublasGemmAlgo_t algo = CUBLAS_GEMM_DFALT;
-    bool use_tensor_op_math = context_.tensor_core_available();
-    if (use_tensor_op_math) {
+
+    if (std::is_same<T, float>::value && tensor_core_avail &&
+        FLAGS_enable_cublas_tensor_op_math) {
       algo = CUBLAS_GEMM_DFALT_TENSOR_OP;
+      VLOG(5) << "use_tensor_op_math: True";
+    } else if (std::is_same<T, __half>::value) {
+      algo = CUBLAS_GEMM_DFALT_TENSOR_OP;
+      VLOG(5) << "use_tensor_op_math: True";
+    } else {
+      VLOG(5) << "use_tensor_op_math: False";
     }
-    VLOG(5) << "use_tensor_op_math: "
-            << (use_tensor_op_math ? "True" : "False");
+
+    auto fp = std::is_same<T, float>::value ? CUDA_R_32F : CUDA_R_16F;
 
     context_.TensorCoreCublasCallIfAvailable([&](cublasHandle_t handle) {
       PADDLE_ENFORCE_CUDA_SUCCESS(platform::dynload::cublasGemmStridedBatchedEx(
-          handle, cuTransB, cuTransA, N, M, K, &alpha, B, CUDA_R_32F, ldb,
-          strideB, A, CUDA_R_32F, lda, strideA, &beta, C, CUDA_R_32F, ldc,
-          strideC, batchCount, CUDA_R_32F, algo));
+          handle, cuTransB, cuTransA, N, M, K, &alpha, B, fp, ldb, strideB, A,
+          fp, lda, strideA, &beta, C, fp, ldc, strideC, batchCount, fp, algo));
     });
   } else {
-#endif  // CUDA_VERSION >= 9010
-
+#endif  // CUDA_VERSION < 9010
     context_.CublasCall([&](cublasHandle_t handle) {
       CUBlas<T>::GEMM_STRIDED_BATCH(handle, cuTransB, cuTransA, N, M, K, &alpha,
                                     B, ldb, strideB, A, lda, strideA, &beta, C,
                                     ldc, strideC, batchCount);
     });
-
 #if CUDA_VERSION >= 9010
   }
 #endif  // CUDA_VERSION >= 9010
