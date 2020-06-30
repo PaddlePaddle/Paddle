d1029ce61ee721139a575711ddaa981a2141c1aa
wangchaochaohu <wangchao66@baidu.com>
2020-06-30 13:48:24 +0000

    refine the linspace Op test=develop

--- python/paddle/fluid/tests/unittests/test_linspace.py
+++ python/paddle/fluid/tests/unittests/test_linspace.py
@@ -71,17 +71,6 @@ class TestLinspaceOpNumOneCase(OpTest):
 
 
 class TestLinspaceAPI(unittest.TestCase):
-    def test_out(self):
-        with program_guard(fluid.Program()):
-            out_1 = fluid.data(name="out_1", shape=[5], dtype="float32")
-            out_2 = paddle.tensor.linspace(0, 10, 5, dtype='float32', out=out_1)
-            exe = fluid.Executor(place=fluid.CPUPlace())
-            ipt = {'out_1': np.random.random([5]).astype('float32')}
-            res_1, res_2 = exe.run(fluid.default_main_program(),
-                                   feed=ipt,
-                                   fetch_list=[out_1, out_2])
-            assert np.array_equal(res_1, res_2)
-
     def test_name(self):
         with fluid.program_guard(fluid.Program()):
             out = paddle.linspace(
@@ -92,12 +81,6 @@ class TestLinspaceAPI(unittest.TestCase):
 class TestLinspaceOpError(unittest.TestCase):
     def test_errors(self):
         with program_guard(Program(), Program()):
-            # for ci coverage
-            # The device of fill_constant must be in 'cpu', 'gpu' or None
-            def test_device_value():
-                paddle.linspace(0, 10, 1, dtype="float32", device='xxxpu')
-
-            self.assertRaises(ValueError, test_device_value)
 
             def test_start_type():
                 fluid.layers.linspace([0], 10, 1, dtype="float32")
