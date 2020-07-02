d5854e934452fe14b3238fb48aef5fcc32fc6c78
tianshuo78520a <707759223@qq.com>
2020-07-02 19:00:53 +0800

    add trt

--- tools/dockerfile/build_scripts/install_trt.sh
+++ tools/dockerfile/build_scripts/install_trt.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 version=$1
 
-if [ "$version" -eq "cuda10.1cudnn7" ];then
+if [[ "$version" == "cuda10.1cudnn7" ]];then
   wget -q https://paddle-ci.gz.bcebos.com/TRT/TensorRT6-cuda10.1-cudnn7.tar.gz --no-check-certificate
   tar -zxf TensorRT6-cuda10.1-cudnn7.tar.gz -C /usr/local
   cp -rf /usr/local/TensorRT6-cuda10.1-cudnn7/include/* /usr/include/ && cp -rf /usr/local/TensorRT6-cuda10.1-cudnn7/lib/* /usr/lib/
-elif [ "$version" -eq "cuda10.0cudnn7" ];then
+elif [[ "$version" == "cuda10.0cudnn7" ]];then
   wget -q https://paddle-ci.gz.bcebos.com/TRT/TensorRT6-cuda10.0-cudnn7.tar.gz --no-check-certificate
   tar -zxf TensorRT6-cuda10.0-cudnn7.tar.gz -C /usr/local
   cp -rf /usr/local/TensorRT6-cuda10.0-cudnn7/include/* /usr/include/ && cp -rf /usr/local/TensorRT6-cuda10.0-cudnn7/lib/* /usr/lib/
-elif [ "$version" -eq "cuda9.0cudnn7" ];then
+elif [[ "$version" == "cuda9.0cudnn7" ]];then
   wget -q https://paddle-ci.gz.bcebos.com/TRT/TensorRT6-cuda9.0-cudnn7.tar.gz --no-check-certificate
-  tar -zxf TensorRT6-cuda9.0-cudnn7 -C /usr/local
+  tar -zxf TensorRT6-cuda9.0-cudnn7.tar.gz -C /usr/local
   cp -rf /usr/local/TensorRT6-cuda9.0-cudnn7/include/* /usr/include/ && cp -rf /usr/local/TensorRT6-cuda9.0-cudnn7/lib/* /usr/lib/
 fi
