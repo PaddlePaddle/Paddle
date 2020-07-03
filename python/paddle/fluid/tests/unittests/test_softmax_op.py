test=develop, fix softmax

python/paddle/fluid/tests/unittests/test_softmax_op.py
@@ -46,7 +46,8 @@ class TestSoftmaxOp(OpTest):
         self.init_kernel_type()
         self.shape = self.get_x_shape()
         self.axis = self.get_axis()
-
+        
+        np.random.seed(0)
         x = np.random.uniform(0.1, 1, self.shape).astype(self.dtype)
         out = np.apply_along_axis(stable_softmax, self.axis, x)
 