33115e956aa8f754f55a25adba42702b0b86e1fc
wangchaochaohu <wangchao66@baidu.com>
2020-07-07 11:46:21 +0000

    refine the code test=develop

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -709,9 +709,8 @@ def fill_constant(shape, dtype, value, force_cpu=False, out=None, name=None):
                 ['bool', 'float16', 'float32', 'float64', 'int32', 'int64'],
                 'fill_constant')
     check_type(shape, 'shape', (Variable, list, tuple), 'fill_constant')
-    if isinstance(shape, Variable):
-        check_variable_and_dtype(shape, 'shape', ['int32', 'int64'],
-                                 'fill_constant')
+    check_variable_and_dtype(shape, 'shape', ['int32', 'int64'],
+                             'fill_constant')
     if out is not None:
         check_variable_and_dtype(out, 'out', [convert_dtype(dtype)],
                                  'fill_constant')
