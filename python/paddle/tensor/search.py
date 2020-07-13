8dce4491305738f93509e65d8bb2e1fba13cb966
wangchaochaohu <wangchao66@baidu.com>
2020-07-13 03:24:32 +0000

    fix the doc test=develop

--- python/paddle/tensor/search.py
+++ python/paddle/tensor/search.py
@@ -170,7 +170,7 @@ def index_select(x, index, axis=0, name=None):
                              [9.0, 10.0, 11.0, 12.0]])
             data_index = np.array([0, 1, 1]).astype('int32')
 
-            x = paddle.to_variable(data)
+            x = paddle.imperative.to_variable(data)
             index = paddle.to_variable(data_index)
             out_z1 = paddle.index_select(x=x, index=index)
             #[[1. 2. 3. 4.]
