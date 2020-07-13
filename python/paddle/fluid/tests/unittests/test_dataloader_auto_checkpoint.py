f05ebab69a6b80fdfc8f5dd4143022c2d6692573
gongweibao <weibao.gong@gmail.com>
2020-07-13 12:33:44 +0000

    add test=develop

---
+++ python/paddle/fluid/tests/unittests/test_dataloader_auto_checkpoint.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import paddle
+import paddle.fluid as fluid
+import paddle.fluid.incubate.fleet.base.role_maker as role_maker
+from paddle.fluid.incubate.fleet.collective import CollectiveOptimizer, fleet, TrainStatus
+import os
+import sys
+
+from paddle.fluid.incubate.fleet.utils.fs import LocalFS
+from paddle.fluid.incubate.fleet.utils.hdfs import HDFSClient
+import paddle.fluid.incubate.checkpointer.auto_checkpoint as acp
+from paddle.fluid.incubate.checkpointer.checkpointer import PaddleModel
+from paddle.fluid.framework import program_guard
+from paddle.fluid import unique_name
+
+import numpy as np
+from paddle.io import Dataset, BatchSampler, DataLoader
+
+from .auto_checkpoint_utils import AutoCheckpointBase, logger
+
+
+class DataLoaderAutoCheckpointTest(AutoCheckpointBase):
+    def _run_save_0(self):
+        pass
+
+    def _run_load_0(self):
+        pass
+
+    def test_basic(self):
+        pass
+
+    def test_coreno_epoch(self):
+        pass
+
+    def test_distributed_basic(self):
+        pass
+
+    def test_multiple(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
