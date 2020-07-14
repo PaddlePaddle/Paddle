3b50e02dc447a9c0f218d6cec562522416f7463d
seiriosPlus <tangwei12@baidu.com>
2020-07-14 10:52:46 +0800

    fix import package, test=develop

--- python/paddle/fluid/tests/unittests/test_dist_fleet_base.py
+++ python/paddle/fluid/tests/unittests/test_dist_fleet_base.py
@@ -35,9 +35,8 @@ import tempfile
 
 import paddle.fluid as fluid
 import paddle.fluid.incubate.fleet.base.role_maker as role_maker
-from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler import fleet
-from paddle.fluid.transpiler.distribute_transpiler import DistributeTranspilerConfig
-from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler.distributed_strategy import StrategyFactory
+from paddle.fluid.incubate.fleet.parameter_server import fleet
+from paddle.fluid.incubate.fleet.parameter_server import StrategyFactory
 
 __all__ = ['FleetDistRunnerBase', 'TestFleetBase', 'runtime_main']
 
