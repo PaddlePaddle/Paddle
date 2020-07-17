ee28c14ea7d98eeaf58c9c3a1ea564d664cb1e30
zhiqiu <chenqiuliang@baidu.com>
2020-07-17 17:51:26 +0800

    add ci check for changing op-related api without core.ops, test=develop

--- tools/count_api_without_core_ops.py
+++ tools/count_api_without_core_ops.py
@@ -11,12 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-List all operator-raleated APIs that contains append_op but not core.ops.xx.
 
-Usage:
-    python ./count_api_without_ops.py paddle
-"""
 from __future__ import print_function
 
 import importlib
@@ -28,8 +23,6 @@ import hashlib
 import six
 import functools
 
-visited_modules = set()
-
 # APIs that should not be printed into API.spec 
 omitted_list = [
     "paddle.fluid.LoDTensor.set",  # Do not know why it should be omitted
@@ -37,11 +30,14 @@ omitted_list = [
     "paddle.fluid.io.ComposeNotAligned.__init__",
 ]
 
-api_with_ops = []
-api_without_ops = []
+
+def md5(doc):
+    hash = hashlib.md5()
+    hash.update(str(doc).encode('utf-8'))
+    return hash.hexdigest()
 
 
-def queue_dict(member, cur_name):
+def split_with_and_without_core_ops(member, cur_name):
     if cur_name in omitted_list:
         return
 
@@ -60,18 +56,34 @@ def queue_dict(member, cur_name):
             pass
 
 
-def visit_member(parent_name, member):
+def get_md5_of_func(member, cur_name):
+    if cur_name in omitted_list:
+        return
+
+    doc_md5 = md5(member.__doc__)
+
+    if inspect.isclass(member):
+        pass
+    else:
+        try:
+            source = inspect.getsource(member)
+            func_dict[cur_name] = md5(source)
+        except TypeError:  # special for PyBind method
+            pass
+
+
+def visit_member(parent_name, member, func):
     cur_name = ".".join([parent_name, member.__name__])
     if inspect.isclass(member):
-        queue_dict(member, cur_name)
+        func(member, cur_name)
         for name, value in inspect.getmembers(member):
             if hasattr(value, '__name__') and (not name.startswith("_") or
                                                name == "__init__"):
-                visit_member(cur_name, value)
+                visit_member(cur_name, value, func)
     elif inspect.ismethoddescriptor(member):
         return
     elif callable(member):
-        queue_dict(member, cur_name)
+        func(member, cur_name)
     elif inspect.isgetsetdescriptor(member):
         return
     else:
@@ -94,7 +106,7 @@ def is_primitive(instance):
         return False
 
 
-def visit_all_module(mod):
+def visit_all_module(mod, visited, func):
     mod_name = mod.__name__
     if mod_name != 'paddle' and not mod_name.startswith('paddle.'):
         return
@@ -102,10 +114,10 @@ def visit_all_module(mod):
     if mod_name.startswith('paddle.fluid.core'):
         return
 
-    if mod in visited_modules:
+    if mod in visited:
         return
 
-    visited_modules.add(mod)
+    visited.add(mod)
 
     for member_name in (
             name
@@ -122,19 +134,51 @@ def visit_all_module(mod):
             continue
 
         if inspect.ismodule(instance):
-            visit_all_module(instance)
+            visit_all_module(instance, visited, func)
         else:
-            visit_member(mod.__name__, instance)
+            visit_member(mod.__name__, instance, func)
 
 
-modules = sys.argv[1].split(",")
-for m in modules:
-    visit_all_module(importlib.import_module(m))
+def get_apis_with_and_without_core_ops(modules):
+    global api_with_ops, api_without_ops
+    api_with_ops = []
+    api_without_ops = []
+    for m in modules:
+        visit_all_module(
+            importlib.import_module(m), set(), split_with_and_without_core_ops)
+    return api_with_ops, api_without_ops
 
-print('api_with_ops:', len(api_with_ops))
-print('\n'.join(api_with_ops))
 
-print('\n==============\n')
+def get_api_source_desc(modules):
+    global func_dict
+    func_dict = collections.OrderedDict()
+    for m in modules:
+        visit_all_module(importlib.import_module(m), set(), get_md5_of_func)
+    return func_dict
 
-print('api_without_ops:', len(api_without_ops))
-print('\n'.join(api_without_ops))
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        modules = sys.argv[2].split(",")
+        if sys.argv[1] == '-c':
+            api_with_ops, api_without_ops = get_apis_with_and_without_core_ops(
+                modules)
+
+            print('api_with_ops:', len(api_with_ops))
+            print('\n'.join(api_with_ops))
+            print('\n==============\n')
+            print('api_without_ops:', len(api_without_ops))
+            print('\n'.join(api_without_ops))
+
+        if sys.argv[1] == '-p':
+            func_dict = get_api_source_desc(modules)
+            for name in func_dict:
+                print(name, func_dict[name])
+
+    else:
+        print("""Usage: 
+            1. Count and list all operator-raleated APIs that contains append_op but not core.ops.xx. 
+                python ./count_api_without_core_ops.py -c paddle
+            2. Print api and the md5 of source code of the api.
+                python ./count_api_without_core_ops.py -p paddle
+            """)
