d5766bec433017c49eec9959fa230c77d3cbbca7
WuHaobo <wuhaobo1994@gmail.com>
2020-07-17 13:16:17 +0000

    test=develop, remove all  the out args

--- python/paddle/fluid/tests/unittests/test_activation_op.py
+++ python/paddle/fluid/tests/unittests/test_activation_op.py
@@ -892,21 +892,18 @@ class TestPow_factor_tensor(TestActivation):
         factor_2 = fluid.layers.fill_constant([1], "float32", 3.0)
         out_1 = fluid.layers.pow(x, factor=factor_1)
         out_2 = fluid.layers.pow(x, factor=factor_2)
-        out_3 = paddle.pow(x, factor_1, out=res)
         out_4 = paddle.pow(x, factor_1, name='pow_res')
-        out_5 = paddle.pow(x, factor_1, out=res, name='pow_res')
         out_6 = paddle.pow(x, factor_2)
         self.assertEqual(('pow_res' in out_4.name), True)
 
         exe = fluid.Executor(place=fluid.CPUPlace())
-        res_1, res_2, res_3, res, res_6 = exe.run(
+        res_1, res_2, res, res_6 = exe.run(
             fluid.default_main_program(),
             feed={"x": input},
-            fetch_list=[out_1, out_2, out_3, res, out_6])
+            fetch_list=[out_1, out_2, res, out_6])
 
         assert np.array_equal(res_1, np.power(input, 2))
         assert np.array_equal(res_2, np.power(input, 3))
-        assert np.array_equal(res_3, res)
         assert np.array_equal(res_6, np.power(input, 3))
 
     def test_error(self):
