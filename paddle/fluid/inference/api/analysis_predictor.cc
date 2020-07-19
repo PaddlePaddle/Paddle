6a87f61bfc58887e31ef7d53e3b517eb8ff95b83
ReeseWang <rewang@nvidia.com>
2020-07-19 22:55:45 +0800

    add trt stack op, test=develop

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -1035,4 +1035,5 @@ USE_TRT_CONVERTER(fused_embedding_eltwise_layernorm);
 USE_TRT_CONVERTER(skip_layernorm);
 USE_TRT_CONVERTER(slice);
 USE_TRT_CONVERTER(scale);
+USE_TRT_CONVERTER(stack);
 #endif
