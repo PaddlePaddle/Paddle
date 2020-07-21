474f1e753d58745c94dd58d81388e940130a3919
LielinJiang <jianglielin@baidu.com>
2020-07-21 03:03:13 +0000

    fix docs, test=develop

--- python/paddle/incubate/hapi/utils.py
+++ python/paddle/incubate/hapi/utils.py
@@ -19,8 +19,6 @@ from paddle import fluid
 from paddle.fluid.framework import Variable
 from paddle.fluid.executor import global_scope
 
-fluid.layers.load_combine
-
 
 def to_list(value):
     if value is None:
