4058c8adb184db405074b0f114d6a858754f58f6
tianshuo78520a <707759223@qq.com>
2020-07-22 17:56:28 +0800

    notest

--- tools/dockerfile/build_scripts/install_gcc.sh
+++ tools/dockerfile/build_scripts/install_gcc.sh
@@ -5,10 +5,12 @@
 set -ex
 
 if [ -f "/etc/redhat-release" ];then
-  lib_faile=/usr/lib64/libstdc++.so.6
+  lib_so_5=/usr/lib64/libstdc++.so.6
+  lib_so_6=/usr/lib64/libstdc++.so.6
   lib_path=/usr/lib64
 else
-  lib_faile=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+  lib_so_5=/usr/lib/x86_64-linux-gnu/libstdc++.so.5
+  lib_so_6=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
   lib_path=/usr/lib/x86_64-linux-gnu
 fi
 
@@ -23,7 +25,7 @@ if [ "$1" == "gcc82" ]; then
   make -j8 && make install
   cd .. && rm -rf temp_gcc82
   cp ${lib_faile} ${lib_faile}.bak  && rm -f ${lib_faile} && 
-  ln -s /usr/local/gcc-8.2/lib64/libstdc++.so.6 ${lib_faile} && \
-  ln -s /usr/local/gcc-8.2/lib64/libgfortran.so.5 ${lib_faile} && \
+  ln -s /usr/local/gcc-8.2/lib64/libgfortran.so.5 ${lib_so_5} && \
+  ln -s /usr/local/gcc-8.2/lib64/libstdc++.so.6 ${lib_so_6} && \
   cp /usr/local/gcc-8.2/lib64/libstdc++.so.6.0.25 ${lib_path}
 fi
