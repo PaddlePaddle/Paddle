0a266e131c81dcdae67c4c8404cd540258ec9105
tianshuo78520a <707759223@qq.com>
2020-07-28 20:26:03 +0800

    test=document_fix

--- paddle/scripts/conda_build.py
+++ paddle/scripts/conda_build.py
@@ -71,7 +71,6 @@ requirements:
     - gast>=0.3.3
     - py-cpuinfo==5.0.0
 """
-
         self.test = r"""
 test:
   import:
@@ -222,13 +221,13 @@ package:
     if not (cuda_str == None):
         meta_str = meta_str + cuda_str
     meta_str = meta_str + var.test + var.about
-    blt_str = var.blt_const + blt_var
     if (python_str == var.python27):
         blt_str = blt_str + """
-    - pip install C:\package\opencv_python-4.2.0.32-cp27-cp27m-win32.whl"""
+    - pip install C:\package\opencv_python-4.2.0.32-cp27-cp27m-win_amd64.whl"""
     else:
         meta_str = meta_str + """
     - opencv>=3.4.2"""
+    blt_str = var.blt_const + blt_var
 
     meta_filename = "meta.yaml"
     build_filename = "bld.bat"
