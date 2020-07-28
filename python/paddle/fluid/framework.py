920d998f1ee941ed8facf41f34674e17e07b4377
Dong Daxiang <35550832+guru4elephant@users.noreply.github.com>
2020-07-28 12:47:46 +0800

    add more settings for distributed strategy (#25685)
    * add more settings for distributed strategy
    Basically, DistributedStrategy has several parts of configurations:
    - BuildStrategy: the same as paddle.fluid.BuildStrategy, but the distributed arguments are moved out of BuildStrategy
    - ExecutionStrategy: the same as paddle.fluid.ExecutionStrategy
    - collective communication configs: nccl_comm_num, hierarchical allreduce and so on
    - distributed algorithms: async_update(mainly used in PS), lars, lamb and so on

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -3936,6 +3936,9 @@ class Program(object):
         # appending gradients times
         self._appending_grad_times = 0
 
+        # compiled program, i.e. Graph
+        self._graph = None
+
     def global_seed(self, seed=0):
         """
         Set global seed for Program
