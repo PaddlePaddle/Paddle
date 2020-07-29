703c7caf3ce2d01b742e54872f25c2ecc9646c85
ForFishes <2282912238@qq.com>
2020-07-29 12:02:48 +0800

    fix bost_get, test=develop

--- paddle/fluid/operators/math/matrix_inverse.cu.cc
+++ paddle/fluid/operators/math/matrix_inverse.cu.cc
@@ -81,7 +81,7 @@ class MatrixInverseFunctor<platform::CUDADeviceContext, T> {
                          reinterpret_cast<const T**>(tmp_gpu_ptrs_data->ptr()),
                          gpu_inv_ptrs, gpu_info_ptr, batch_size);
       memory::Copy(platform::CPUPlace(), info.data(),
-                   boost::get<platform::CUDAPlace>(context.GetPlace()),
+                   BOOST_GET_CONST(platform::CUDAPlace, context.GetPlace()),
                    gpu_info_ptr, sizeof(int) * batch_size, context.stream());
       for (int i = 0; i < batch_size; ++i) {
         PADDLE_ENFORCE_EQ(
@@ -102,7 +102,7 @@ class MatrixInverseFunctor<platform::CUDADeviceContext, T> {
                         reinterpret_cast<const T**>(tmp_gpu_ptrs_data->ptr()),
                         gpu_pivot_ptr, gpu_inv_ptrs, gpu_info_ptr, batch_size);
       memory::Copy(platform::CPUPlace(), info.data(),
-                   boost::get<platform::CUDAPlace>(context.GetPlace()),
+                   BOOST_GET_CONST(platform::CUDAPlace, context.GetPlace()),
                    gpu_info_ptr, sizeof(int) * batch_size, context.stream());
       for (int i = 0; i < batch_size; ++i) {
         PADDLE_ENFORCE_EQ(
