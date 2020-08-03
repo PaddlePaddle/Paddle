8ae96b636df85ca9de7feda226c8863948542435
Zhen Wang <wangzhen31@baidu.com>
2020-08-03 12:20:24 +0000

    add cast cuda kernel for cast op. test=develop

--- paddle/fluid/operators/cast_op.cu
+++ paddle/fluid/operators/cast_op.cu
@@ -15,6 +15,39 @@ limitations under the License. */
 #include "paddle/fluid/operators/cast_op.h"
 #include "paddle/fluid/platform/float16.h"
 
+namespace paddle {
+namespace operators {
+template <typename InT, typename OutT>
+__global__ void DoCastKernel(const InT* in, const int n, OutT* out) {
+  int bid = threadIdx.x + blockIdx.x * blockDim.x;
+  for (int i = bid; i < n; i += blockDim.x * gridDim.x) {
+    out[i] = static_cast<OutT>(in[i]);
+  }
+}
+
+template <typename InT>
+struct CastOpFunctor<platform::CUDADeviceContext, InT> {
+  const framework::Tensor* in_;
+  framework::Tensor* out_;
+  const platform::CUDADeviceContext& ctx_;
+  CastOpFunctor(const framework::Tensor* in, framework::Tensor* out,
+                const platform::CUDADeviceContext& ctx)
+      : in_(in), out_(out), ctx_(ctx) {}
+
+  template <typename OutT>
+  void apply() const {
+    auto* in = in_->data<InT>();
+    auto num = in_->numel();
+    auto* out = out_->mutable_data<OutT>(ctx_.GetPlace());
+    int block = 1024;
+    int grid = (block - 1 + num) / block;
+    DoCastKernel<InT, OutT><<<grid, block, 0, ctx_.stream()>>>(in, num, out);
+  }
+};
+
+}  // namespace operators
+}  // namespace paddle
+
 template <typename T>
 using CastOpKernel =
     paddle::operators::CastOpKernel<paddle::platform::CUDADeviceContext, T>;
