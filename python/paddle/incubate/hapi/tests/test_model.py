c7cc35fa3f1ff7005150cd48a4f3de105a6bd9b6
LiuChiachi <709153940@qq.com>
2020-08-04 10:10:44 +0800

    test, test=develop

--- python/paddle/incubate/hapi/tests/test_model.py
+++ python/paddle/incubate/hapi/tests/test_model.py
@@ -437,47 +437,35 @@ class TestModelFunction(unittest.TestCase):
             fluid.disable_dygraph() if dynamic else None
 
     def test_export_deploy_model(self):
-        from paddle.fluid.dygraph import to_variable
+        net = LeNet()
         inputs = [Input('image', [-1, 1, 28, 28], 'float32')]
+        model = Model(net, inputs)
+        model.prepare()
+        save_dir = tempfile.mkdtemp()
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
         tensor_img = np.array(
             np.random.random((1, 1, 28, 28)), dtype=np.float32)
-        tensor_img1 = tensor_img
-        device = hapi.set_device('cpu')
-        for dynamic in [True, False]:
-            save_dir = tempfile.mkdtemp()
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir)
-            if dynamic:
-                fluid.enable_dygraph(device)
-                net = LeNet()
-                model = Model(net, inputs)
-                model.prepare()
-                ori_results = model.test_batch(tensor_img)
-                in_var_list = [to_variable(tensor_img)]
-                model.save_inference_model(save_dir, net, in_var_list)
-            else:
-                fluid.disable_dygraph() if fluid.in_dygraph_mode() else None
-                net = LeNet()
-                model = Model(net, inputs)
-                model.prepare()
-                ori_results = model.test_batch(tensor_img1)
-                model.save_inference_model(save_dir)
+        ori_results = model.test_batch(tensor_img)
 
-            fluid.disable_dygraph() if dynamic else None
-            place = fluid.CPUPlace() if not fluid.is_compiled_with_cuda(
-            ) else fluid.CUDAPlace(0)
-            exe = fluid.Executor(place)
-            [inference_program, feed_target_names, fetch_targets] = (
-                fluid.io.load_inference_model(
-                    dirname=save_dir, executor=exe))
+        model.save_inference_model(save_dir)
 
-            results = exe.run(inference_program,
-                              feed={feed_target_names[0]: tensor_img},
-                              fetch_list=fetch_targets)
+        place = fluid.CPUPlace() if not fluid.is_compiled_with_cuda(
+        ) else fluid.CUDAPlace(0)
+        exe = fluid.Executor(place)
+        [inference_program, feed_target_names, fetch_targets] = (
+            fluid.io.load_inference_model(
+                dirname=save_dir, executor=exe))
 
-            np.testing.assert_allclose(results, ori_results, rtol=1e-6)
-            shutil.rmtree(save_dir)
+        results = exe.run(inference_program,
+                          feed={feed_target_names[0]: tensor_img},
+                          fetch_list=fetch_targets)
+
+        np.testing.assert_allclose(results, ori_results, rtol=1e-6)
+        shutil.rmtree(save_dir)
 
 
 if __name__ == '__main__':
     unittest.main()
+
