ec3f6f97978708209171286c20e3e61f8ad7ad79
pangyoki <pangyoki@126.com>
2020-08-05 11:18:50 +0000

    Add examples of prepare_context API, test=develop;test=document_fix

--- python/paddle/fluid/dygraph/parallel.py
+++ python/paddle/fluid/dygraph/parallel.py
@@ -27,9 +27,55 @@ ParallelStrategy = core.ParallelStrategy
 
 
 def prepare_context(strategy=None):
-    '''
+    """
     :api_attr: imperative
-    '''
+
+    This function is an environment configuration interface for multi-process multi-card
+    training, and accepts a ParallelStrategy structure variable as input.
+
+    Examples:
+      .. code-block:: python
+
+        # This example needs to run with paddle.distributed.launch, The usage is:
+        #   python -m paddle.distributed.launch --selected_gpus=0,1 example.py
+        # And the content of `example.py` is the code of following example.
+
+        import numpy as np
+        import paddle.fluid as fluid
+        import paddle.fluid.dygraph as dygraph
+        from paddle.fluid.optimizer import AdamOptimizer
+        from paddle.fluid.dygraph.nn import Linear
+        from paddle.fluid.dygraph.base import to_variable
+
+        place = fluid.CUDAPlace(fluid.dygraph.ParallelEnv().dev_id)
+        with fluid.dygraph.guard(place=place):
+
+            # prepare the data parallel context
+            strategy=dygraph.prepare_context()
+
+            linear = Linear(1, 10, act="softmax")
+            adam = fluid.optimizer.AdamOptimizer()
+
+            # make the module become the data parallelism module
+            linear = dygraph.DataParallel(linear, strategy)
+
+            x_data = np.random.random(size=[10, 1]).astype(np.float32)
+            data = to_variable(x_data)
+
+            hidden = linear(data)
+            avg_loss = fluid.layers.mean(hidden)
+
+            # scale the loss according to the number of trainers.
+            avg_loss = linear.scale_loss(avg_loss)
+
+            avg_loss.backward()
+
+            # collect the gradients of trainers.
+            linear.apply_collective_grads()
+
+            adam.minimize(avg_loss)
+            linear.clear_gradients()
+    """
     if strategy is None:
         strategy = ParallelStrategy()
         strategy.nranks = Env().nranks
