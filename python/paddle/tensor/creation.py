8e79ccfe075b0211b7d895ab44711558a8498ce3
wangchaochaohu <wangchao66@baidu.com>
2020-08-06 11:42:33 +0000

    refine the doc test=develop

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -248,7 +248,7 @@ def zeros(shape, dtype=None, name=None):
           
           # shape is a Tensor
           shape = paddle.fill_constant(shape=[2], dtype='int32', value=2)
-          data3 = paddle.ones(shape=shape, dtype='int32') 
+          data3 = paddle.zeros(shape=shape, dtype='int32') 
           # [[0 0]
           #  [0 0]]
     """
