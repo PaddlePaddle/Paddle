d3d0cf42d1f3ea920488f5a56fe6e39c4eb8ceab
seiriosPlus <tangwei12@baidu.com>
2020-08-07 11:41:07 +0800

    add paddle.fleet UT

--- python/paddle/fluid/tests/unittests/test_communicator_geo.py
+++ python/paddle/fluid/tests/unittests/test_communicator_geo.py
@@ -63,7 +63,7 @@ class TestCommunicatorGeoEnd2End(unittest.TestCase):
 
     def run_pserver(self, role, strategy):
         fleet.init(role)
-        avg_cost, x, y = self.net()
+        avg_cost, x, z, y = self.net()
         optimizer = fluid.optimizer.SGD(0.01)
         optimizer = fleet.distributed_optimizer(optimizer, strategy)
         optimizer.minimize(avg_cost)
