23eb82a19fb1e28b21d054c4582df74e728e42c3
sandyhouse <lilong12@baidu.com>
2020-08-07 03:57:40 +0000

    fix ut, test=develop

--- python/paddle/fluid/tests/unittests/test_c_comm_init_op.sh
+++ python/paddle/fluid/tests/unittests/test_c_comm_init_op.sh
@@ -2,5 +2,5 @@
 set -e
 # use default values
 # FIXME: random fails on Unknown command lines -c (or -m).
-#launch_py=${PADDLE_BINARY_DIR}/python/paddle/distributed/launch.py
-python -m paddle.distributed.launch --selected_gpus="0,1" c_comm_init_op.py
+launch_py=${PADDLE_BINARY_DIR}/python/paddle/distributed/launch.py
+CUDA_VISIBLE_DEVICES=0,1 python ${launch_py} c_comm_init_op.py
