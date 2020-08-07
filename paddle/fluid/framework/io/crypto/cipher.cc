3675df99b423954b108f423582ebffef4e6ea4c0
MRXLT <xlt2024@gmail.com>
2020-08-07 15:48:22 +0800

    bug fix

--- paddle/fluid/framework/io/crypto/cipher.cc
+++ paddle/fluid/framework/io/crypto/cipher.cc
@@ -16,9 +16,6 @@
 #include "paddle/fluid/framework/io/crypto/aes_cipher.h"
 #include "paddle/fluid/framework/io/crypto/cipher_utils.h"
 #include "paddle/fluid/platform/enforce.h"
-#ifdef PADDLE_ON_INFERENCE
-#include "paddle/fluid/inference/api/paddle_api.h"
-#endif
 namespace paddle {
 namespace framework {
 
