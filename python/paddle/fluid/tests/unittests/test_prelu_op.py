0682535d121364dfbf42dca2c1775ab195af0714
zhiqiu <chenqiuliang@baidu.com>
2020-08-08 17:45:23 +0800

    reshape alpha

--- python/paddle/fluid/tests/unittests/test_prelu_op.py
+++ python/paddle/fluid/tests/unittests/test_prelu_op.py
@@ -57,9 +57,16 @@ class PReluTest(OpTest):
 
         self.inputs = {'X': x_np, 'Alpha': alpha_np}
 
+        # NOTE(zhiqu): reshape inputs['Alpha'] from [1, 100, 1, 1] to [1, 100] + [1]*len(x.shape[2:])
+        # since np operands could not be broadcast together with shapes (1,100,2,2,2,3) (1,100,1,1) 	
+        reshaped_alpha = self.inputs['Alpha']
+        if self.attrs == {'mode': "channel"}:
+            reshaped_alpha = np.reshape(
+                self.inputs['Alpha'],
+                [1, self.x_shape[1]] + [1] * len(self.x_shape[2:]))
+
         out_np = np.maximum(self.inputs['X'], 0.)
-        out_np = out_np + np.minimum(self.inputs['X'],
-                                     0.) * self.inputs['Alpha']
+        out_np = out_np + np.minimum(self.inputs['X'], 0.) * reshaped_alpha
         assert out_np is not self.inputs['X']
         self.outputs = {'Out': out_np}
 
