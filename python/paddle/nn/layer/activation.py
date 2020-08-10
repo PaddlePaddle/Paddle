5c94345e400d77e3d6fa0347784b27097641178a
zhupengyang <zhu_py@qq.com>
2020-08-10 16:12:50 +0800

    fix docs

--- python/paddle/nn/layer/activation.py
+++ python/paddle/nn/layer/activation.py
@@ -234,9 +234,9 @@ class LeakyReLU(layers.Layer):
 
         paddle.enable_imperative()
 
-        lrelu = nn.LeakyReLU()
-        x = paddle.imperative.to_variable(np.array([-2, 0, 1]))
-        res = lrelu(x)  # [-0.02, 0, 1]
+        lrelu = paddle.nn.LeakyReLU()
+        x = paddle.imperative.to_variable(np.array([-2, 0, 1], 'float32'))
+        out = lrelu(x)  # [-0.02, 0, 1]
     """
 
     def __init__(self, alpha=1e-2, name=None):
