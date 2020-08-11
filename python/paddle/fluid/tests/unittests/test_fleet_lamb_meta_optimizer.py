25eb5d79f07f35fd1cbdc664ebd3ee7bd35ed670
JZ-LIANG <jianzhongliang10@gmail.com>
2020-08-11 11:57:35 +0800

    revise unitest for lars and lamb

--- python/paddle/fluid/tests/unittests/test_fleet_lamb_meta_optimizer.py
+++ python/paddle/fluid/tests/unittests/test_fleet_lamb_meta_optimizer.py
@@ -14,6 +14,7 @@
 
 import unittest
 import paddle
+from paddle import fluid
 import os
 import paddle.fleet as fleet
 import paddle.fluid.incubate.fleet.base.role_maker as role_maker
@@ -27,31 +28,40 @@ class TestFleetLambMetaOptimizer(unittest.TestCase):
         os.environ["PADDLE_PSERVERS_IP_PORT_LIST"] = \
                        "127.0.0.1:36001,127.0.0.2:36001"
 
-    def net(self):
-        role = role_maker.PaddleCloudRoleMaker(is_collective=True)
-        fleet.init(role)
-        input_x = paddle.fluid.layers.data(
-            name="x", shape=[32], dtype='float32')
-        input_y = paddle.fluid.layers.data(name="y", shape=[1], dtype='int64')
+    def net(self, main_prog, startup_prog):
+        with fluid.program_guard(main_prog, startup_prog):
+            with fluid.unique_name.guard():
+                input_x = paddle.fluid.layers.data(
+                    name="x", shape=[32], dtype='float32')
+                input_y = paddle.fluid.layers.data(
+                    name="y", shape=[1], dtype='int64')
 
-        fc_1 = paddle.fluid.layers.fc(input=input_x, size=64, act='tanh')
-        fc_2 = paddle.fluid.layers.fc(input=fc_1, size=256, act='tanh')
-        prediction = paddle.fluid.layers.fc(input=[fc_2], size=2, act='softmax')
-        cost = paddle.fluid.layers.cross_entropy(
-            input=prediction, label=input_y)
-        avg_cost = paddle.fluid.layers.mean(x=cost)
+                fc_1 = paddle.fluid.layers.fc(input=input_x,
+                                              size=64,
+                                              act='tanh')
+                fc_2 = paddle.fluid.layers.fc(input=fc_1, size=256, act='tanh')
+                prediction = paddle.fluid.layers.fc(input=[fc_2],
+                                                    size=2,
+                                                    act='softmax')
+                cost = paddle.fluid.layers.cross_entropy(
+                    input=prediction, label=input_y)
+                avg_cost = paddle.fluid.layers.mean(x=cost)
 
-        strategy = paddle.fleet.DistributedStrategy()
-        strategy.lamb = True
-        strategy.lamb_configs = {
-            'lamb_weight_decay': 0.01,
-            'exclude_from_weight_decay': [],
-        }
+                strategy = paddle.fleet.DistributedStrategy()
+                strategy.lamb = True
+                strategy.lamb_configs = {
+                    'lamb_weight_decay': 0.01,
+                    'exclude_from_weight_decay': [],
+                }
 
         return avg_cost, strategy
 
     def test_lamb_optimizer(self):
-        avg_cost, strategy = self.net()
+        role = role_maker.PaddleCloudRoleMaker(is_collective=True)
+        fleet.init(role)
+        startup_prog = fluid.Program()
+        train_prog = fluid.Program()
+        avg_cost, strategy = self.net(train_prog, startup_prog)
         optimizer = paddle.optimizer.Adam(learning_rate=0.01)
         optimizer = fleet.distributed_optimizer(optimizer, strategy=strategy)
         optimizer.minimize(avg_cost)
@@ -60,7 +70,11 @@ class TestFleetLambMetaOptimizer(unittest.TestCase):
         self.assertIn('lamb', ops)
 
     def test_lamb_not_apply_with_momentum(self):
-        avg_cost, strategy = self.net()
+        role = role_maker.PaddleCloudRoleMaker(is_collective=True)
+        fleet.init(role)
+        startup_prog = fluid.Program()
+        train_prog = fluid.Program()
+        avg_cost, strategy = self.net(train_prog, startup_prog)
         optimizer = paddle.optimizer.Momentum(learning_rate=0.1, momentum=0.9)
         optimizer = fleet.distributed_optimizer(optimizer, strategy=strategy)
         optimizer.minimize(avg_cost)
@@ -69,17 +83,25 @@ class TestFleetLambMetaOptimizer(unittest.TestCase):
         self.assertNotIn('lamb', ops)
 
     def test_lamb_exclude_fn(self):
-        avg_cost, strategy = self.net()
+        role = role_maker.PaddleCloudRoleMaker(is_collective=True)
+        fleet.init(role)
+        startup_prog = fluid.Program()
+        train_prog = fluid.Program()
+        avg_cost, strategy = self.net(train_prog, startup_prog)
         optimizer = paddle.optimizer.Adam(learning_rate=0.01)
-        strategy.lamb_configs['exclude_from_weight_decay'] = ['.b_0']
+        strategy.lamb_configs = {
+            'lamb_weight_decay': 0.01,
+            'exclude_from_weight_decay': ['.b_0'],
+        }
         optimizer = fleet.distributed_optimizer(optimizer, strategy=strategy)
         optimizer.minimize(avg_cost)
 
-        for op in avg_cost.block.ops:
-            print(op.type)
-            print(op)
-        # ops = [op.type for op in avg_cost.block.ops]
-        # self.assertIn('lamb', ops)
+        ops_with_bias = [
+            op for op in avg_cost.block.ops
+            if op.type == 'lamb' and op.attr('op_role_var')[0].endswith('.b_0')
+        ]
+        for op in ops_with_bias:
+            self.assertEqual(op.attr('weight_decay'), 0)
 
 
 if __name__ == "__main__":
