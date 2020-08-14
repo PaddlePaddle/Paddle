4e226a9db88b1971cd724f1f018e75245bcb440e
wangchaochaohu <wangchao66@baidu.com>
2020-08-14 02:11:03 +0000

    refine the code test=develop

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -232,7 +232,7 @@ from .tensor.stat import reduce_mean  #DEFINE_ALIAS
 from .tensor.stat import std  #DEFINE_ALIAS
 from .tensor.stat import var  #DEFINE_ALIAS
 from .fluid.data import data
-from .device import cudnn_version
+from .device import get_cudnn_version
 # from .tensor.tensor import Tensor        #DEFINE_ALIAS
 # from .tensor.tensor import LoDTensor        #DEFINE_ALIAS
 # from .tensor.tensor import LoDTensorArray        #DEFINE_ALIAS
