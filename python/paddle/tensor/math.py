b89a7971eb0e3f271b6fe8726dee5fc23a8ba38e
Yang Zhang <yangzhang@live.com>
2020-08-17 09:49:46 +0800

    Remove `imperative`
    also trigger CI

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -1359,7 +1359,7 @@ def clip(x, min=None, max=None, name=None):
 
             paddle.disable_static()
             x = np.array([[1.2,3.5], [4.5,6.4]]).astype('float32')
-            x1 = paddle.imperative.to_variable(x)
+            x1 = paddle.to_variable(x)
             out1 = paddle.clip(x1, min=3.5, max=5.0)
             out2 = paddle.clip(x1, min=2.5)
             print(out1.numpy())
