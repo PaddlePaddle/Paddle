2a4d486d6ae353e978f2366178f58655b0905791
dengkaipeng <dengkaipeng@baidu.com>
2020-08-17 13:01:04 +0000

    add Sampler/SequenceSampler/RandomSampler. test=develop

--- python/paddle/fluid/dataloader/__init__.py
+++ python/paddle/fluid/dataloader/__init__.py
@@ -23,6 +23,10 @@ from .batch_sampler import *
 from . import dataloader_iter
 from .dataloader_iter import *
 
+from . import sampler
+from .sampler import *
+
 __all__ = dataset.__all__ \
         + batch_sampler.__all__ \
-        + dataloader_iter.__all__
+        + dataloader_iter.__all__ \
+        + sampler.__all__
