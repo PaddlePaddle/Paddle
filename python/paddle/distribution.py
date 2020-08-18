7e529e77876abb14c546a2d1016aea63dbef5ee6
pangyoki <pangyoki@126.com>
2020-08-18 19:16:40 +0000

    adjust format

--- python/paddle/distribution.py
+++ python/paddle/distribution.py
@@ -166,13 +166,10 @@ class Uniform(Distribution):
           # Without broadcasting, a single uniform distribution [3, 4]:
           u1 = Uniform(low=3.0, high=4.0)
           # 2 distributions [1, 3], [2, 4]
-          u2 = Uniform(low=[1.0, 2.0],
-                        high=[3.0, 4.0])
+          u2 = Uniform(low=[1.0, 2.0], high=[3.0, 4.0])
           # 4 distributions
-          u3 = Uniform(low=[[1.0, 2.0],
-                    [3.0, 4.0]],
-               high=[[1.5, 2.5],
-                     [3.5, 4.5]])
+          u3 = Uniform(low=[[1.0, 2.0], [3.0, 4.0]],
+                    high=[[1.5, 2.5], [3.5, 4.5]])
 
           # With broadcasting:
           u4 = Uniform(low=3.0, high=[5.0, 6.0, 7.0])
@@ -344,7 +341,6 @@ class Normal(Distribution):
 
           normal_a = Normal([0.], [1.])
           normal_b = Normal([0.5], [2.])
-
           sample = normal_a.sample([2])
           # a random tensor created by normal distribution with shape: [2, 1]
           entropy = normal_a.entropy()
