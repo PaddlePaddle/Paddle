4916400be6138be761771f60a74bb23e5756c0de
zhangting2020 <709968123@qq.com>
2020-08-18 03:38:45 +0000

    use paddle.data instead of fluid.data, test=develop

--- python/paddle/fluid/tests/unittests/test_allclose_op.py
+++ python/paddle/fluid/tests/unittests/test_allclose_op.py
@@ -16,7 +16,6 @@ import unittest
 import numpy as np
 from op_test import OpTest
 import paddle
-import paddle.fluid as fluid
 
 
 class TestAllcloseOp(OpTest):
@@ -94,24 +93,26 @@ class TestAllcloseDygraph(unittest.TestCase):
 class TestAllcloseError(unittest.TestCase):
     def test_input_dtype(self):
         def test_x_dtype():
-            with fluid.program_guard(fluid.Program()):
-                x = fluid.data(name='x', shape=[10, 10], dtype='float16')
-                y = fluid.data(name='y', shape=[10, 10], dtype='float64')
+            with paddle.static.program_guard(paddle.static.Program(),
+                                             paddle.static.Program()):
+                x = paddle.data(name='x', shape=[10, 10], dtype='float16')
+                y = paddle.data(name='y', shape=[10, 10], dtype='float64')
                 result = paddle.allclose(x, y)
 
         self.assertRaises(TypeError, test_x_dtype)
 
         def test_y_dtype():
-            with fluid.program_guard(fluid.Program()):
-                x = fluid.data(name='x', shape=[10, 10], dtype='float64')
-                y = fluid.data(name='y', shape=[10, 10], dtype='int32')
+            with paddle.static.program_guard(paddle.static.Program(),
+                                             paddle.static.Program()):
+                x = paddle.data(name='x', shape=[10, 10], dtype='float64')
+                y = paddle.data(name='y', shape=[10, 10], dtype='int32')
                 result = paddle.allclose(x, y)
 
         self.assertRaises(TypeError, test_y_dtype)
 
     def test_attr(self):
-        x = fluid.data(name='x', shape=[10, 10], dtype='float64')
-        y = fluid.data(name='y', shape=[10, 10], dtype='float64')
+        x = paddle.data(name='x', shape=[10, 10], dtype='float64')
+        y = paddle.data(name='y', shape=[10, 10], dtype='float64')
 
         def test_rtol():
             result = paddle.allclose(x, y, rtol=True)
