d9daeefc07f2dd487e7f349a9551848f09d81570
wangchaochaohu <wangchao66@baidu.com>
2020-08-19 07:39:28 +0000

    add cuda implementation for gather op

--- paddle/fluid/operators/gather_v2_op.cu
+++ paddle/fluid/operators/gather_v2_op.cu
@@ -13,86 +13,218 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/framework/eigen.h"
+#include "paddle/fluid/operators/gather.cu.h"
+#include "paddle/fluid/operators/gather_op.h"
 #include "paddle/fluid/operators/gather_v2_op.h"
+#include "paddle/fluid/operators/scatter.cu.h"
 
 namespace paddle {
 namespace operators {
 
+using Tensor = framework::Tensor;
+
+template <typename T, typename U>
+__global__ void GatherGPUKernel(const T* input, const U* index, T* out,
+                                int outer_dim_size, int inner_dim_size,
+                                int index_dim_size, int size) {
+  int idx = blockDim.x * blockIdx.x + threadIdx.x;
+  for (; idx < size; idx += blockDim.x * gridDim.x) {
+    int inner_dim_index = idx / (outer_dim_size * index_dim_size);
+    int out_dim_index = idx % outer_dim_size;
+    int input_dim_index = idx / outer_dim_size;
+    int input_index = inner_dim_index * (outer_dim_size * index_dim_size) +
+                      index[input_dim_index] * outer_dim_size + out_dim_index;
+    out[idx] = input[input_index];
+  }
+}
+
+template <typename T, typename U>
+__global__ void GatherGradGPUKernel(const T* input, const U* index, T* out,
+                                    int outer_dim_size, int inner_dim_size,
+                                    int index_dim_size, int size) {
+  int idx = blockDim.x * blockIdx.x + threadIdx.x;
+  for (; idx < size; idx += blockDim.x * gridDim.x) {
+    int inner_dim_index = idx / (outer_dim_size * index_dim_size);
+    int out_dim_index = idx % outer_dim_size;
+    int input_dim_index = idx / outer_dim_size;
+    int out_index = inner_dim_index * (outer_dim_size * index_dim_size) +
+                    index[input_dim_index] * outer_dim_size + out_dim_index;
+    paddle::platform::CudaAtomicAdd(out + out_index, *(input + idx));
+  }
+}
+
+template <typename T, typename U, typename V>
+void GatherV2CUDAFunction(const Tensor* input, const Tensor* index,
+                          const Tensor* axis, Tensor* out,
+                          const paddle::platform::Place& place,
+                          const framework::ExecutionContext& ctx) {
+  int axis_size = axis->numel();
+  int index_size = index->numel();
+  int input_size = input->numel();
+  auto input_dim = input->dims();
+  auto* input_data = input->data<T>();
+  auto* index_data = index->data<U>();
+
+  if (input->numel() == 0) return;
+  PADDLE_ENFORCE_EQ(axis_size, 1,
+                    platform::errors::InvalidArgument(
+                        "Axis size should be 1, but received %d", axis_size));
+  Tensor cpu_axis;
+  framework::TensorCopy(*axis, platform::CPUPlace(), &cpu_axis);
+  int axis_index = cpu_axis.data<V>()[0];
+  int index_dim_size = input_dim[axis_index];
+  PADDLE_ENFORCE_LE(
+      index_size, index_dim_size,
+      platform::errors::InvalidArgument(
+          "The size that index should be less equal than the dim size of "
+          "input,"
+          "but received index size:%d, the dim size of input %d.",
+          axis_size, index_dim_size));
+
+  int inner_dim_size = 1;
+  int outer_dim_size = 1;
+  std::vector<int> out_dim_vec{index_dim_size};
+
+  for (int i = 0; i < axis_index; i++) {
+    inner_dim_size *= input_dim[i];
+  }
+  for (int i = axis_index + 1; i < input_dim.size(); i++) {
+    outer_dim_size *= input_dim[i];
+    out_dim_vec.push_back(input_dim[i]);
+  }
+  auto out_dim = framework::make_ddim(out_dim_vec);
+
+  out->Resize(out_dim);
+  auto* out_data = out->mutable_data<T>(place);
+  int out_size = out->numel();
+
+  int threads = 512;
+  int grid = (out_size + threads - 1) / threads;
+  auto stream = ctx.cuda_device_context().stream();
+  GatherGPUKernel<T, U><<<grid, threads, 0, stream>>>(
+      input_data, index_data, out_data, outer_dim_size, inner_dim_size,
+      index_dim_size, out_size);
+}
+
+template <typename T, typename U, typename V>
+void GatherV2GradCUDAFunction(const Tensor* input, const Tensor* index,
+                              const Tensor* axis, Tensor* out,
+                              const paddle::platform::Place& place,
+                              const framework::ExecutionContext& ctx) {
+  auto* axis_data = axis->data<V>();
+  auto* index_data = index->data<U>();
+
+  int axis_size = axis->numel();
+  int index_size = index->numel();
+  int input_size = input->numel();
+  auto input_dim = input->dims();
+  auto* input_data = input->data<T>();
+
+  if (input->numel() == 0) return;
+  PADDLE_ENFORCE_EQ(axis_size, 1,
+                    platform::errors::InvalidArgument(
+                        "Axis size should be 1, but received %d", axis_size));
+  Tensor cpu_axis;
+  framework::TensorCopy(*axis, platform::CPUPlace(), &cpu_axis);
+  int axis_index = cpu_axis.data<V>()[0];
+  int index_dim_size = input_dim[axis_index];
+  PADDLE_ENFORCE_LE(
+      index_size, index_dim_size,
+      platform::errors::InvalidArgument(
+          "The size that index should be less equal than the dim size of "
+          "input,"
+          "but received index size:%d, the dim size of input %d.",
+          axis_size, index_dim_size));
+
+  int inner_dim_size = 1;
+  int outer_dim_size = 1;
+
+  for (int i = 0; i < axis_index; i++) {
+    inner_dim_size *= input_dim[i];
+  }
+  for (int i = axis_index + 1; i < input_dim.size(); i++) {
+    outer_dim_size *= input_dim[i];
+  }
+
+  auto* out_data = out->mutable_data<T>(place);
+  auto* dev_ctx = platform::DeviceContextPool::Instance().Get(place);
+  operators::math::set_constant(*dev_ctx, out, 0.0);
+
+  int threads = 512;
+  int grid = (input_size + threads - 1) / threads;
+  auto stream = ctx.cuda_device_context().stream();
+  GatherGradGPUKernel<T, U><<<grid, threads, 0, stream>>>(
+      input_data, index_data, out_data, outer_dim_size, inner_dim_size,
+      index_dim_size, input_size);
+}
+
 template <typename DeviceContext, typename T>
 class GatherV2OpCUDAKernel : public framework::OpKernel<T> {
  public:
-  void Compute(const framework::ExecutionContext &ctx) const override {
-    /*
-          PADDLE_ENFORCE_EQ(platform::is_gpu_place(ctx.GetPlace()), true,
-                      platform::errors::PreconditionNotMet(
-                          "This kernel only runs on GPU device."));
-    auto *x = ctx.Input<Tensor>("X");
-    auto *index = ctx.Input<Tensor>("Index");
-    auto *output = ctx.Output<Tensor>("Out");
+  void Compute(const framework::ExecutionContext& ctx) const override {
+    const Tensor* input = ctx.Input<Tensor>("X");
+    const Tensor* index = ctx.Input<Tensor>("Index");
+    const Tensor* axis = ctx.Input<Tensor>("Axis");
+    Tensor* out = ctx.Output<Tensor>("Y");
 
-    output->mutable_data<T>(ctx.GetPlace());
-    if (x->numel() == 0) return;
-    const auto &index_type = index->type();
-    bool index_type_match = index_type == framework::proto::VarType::INT32 ||
-                            index_type == framework::proto::VarType::INT64;
-    PADDLE_ENFORCE_EQ(index_type_match, true,
-                      platform::errors::InvalidArgument(
-                          "Index holds the wrong type, it holds [%s], but "
-                          "desires to be [%s] or [%s].",
-                          paddle::framework::DataTypeToString(index_type),
-                          paddle::framework::DataTypeToString(
-                              framework::proto::VarType::INT32),
-                          paddle::framework::DataTypeToString(
-                              framework::proto::VarType::INT64)));
-    if (index_type == framework::proto::VarType::INT32) {
-      GPUGatherNd<DeviceContext, T, int>(ctx, *x, *index, output);
-    } else if (index_type == framework::proto::VarType::INT64) {
-      GPUGatherNd<DeviceContext, T, int64_t>(ctx, *x, *index, output);
+    const auto& index_type = index->type();
+    const auto& axis_type = axis->type();
+    auto place = ctx.GetPlace();
+    if (index_type == framework::proto::VarType::INT32 &&
+        axis_type == framework::proto::VarType::INT32) {
+      GatherV2CUDAFunction<T, int32_t, int32_t>(input, index, axis, out, place,
+                                                ctx);
     }
-    */
+    if (index_type == framework::proto::VarType::INT32 &&
+        axis_type == framework::proto::VarType::INT64) {
+      GatherV2CUDAFunction<T, int32_t, int64_t>(input, index, axis, out, place,
+                                                ctx);
+    }
+    if (index_type == framework::proto::VarType::INT64 &&
+        axis_type == framework::proto::VarType::INT32) {
+      GatherV2CUDAFunction<T, int64_t, int32_t>(input, index, axis, out, place,
+                                                ctx);
+    }
+    if (index_type == framework::proto::VarType::INT64 &&
+        axis_type == framework::proto::VarType::INT64) {
+      GatherV2CUDAFunction<T, int64_t, int64_t>(input, index, axis, out, place,
+                                                ctx);
+    }
   }
 };
 
 template <typename DeviceContext, typename T>
 class GatherV2GradOpCUDAKernel : public framework::OpKernel<T> {
  public:
-  void Compute(const framework::ExecutionContext &ctx) const override {
-    /*
-          PADDLE_ENFORCE_EQ(platform::is_gpu_place(ctx.GetPlace()), true,
-                      platform::errors::PreconditionNotMet(
-                          "This kernel only runs on GPU device."));
-    auto *index = ctx.Input<Tensor>("Index");
-    auto *dX = ctx.Output<Tensor>(framework::GradVarName("X"));
-    auto *dO = ctx.Input<Tensor>(framework::GradVarName("Out"));
+  void Compute(const framework::ExecutionContext& ctx) const override {
+    const Tensor* input = ctx.Input<Tensor>("X");
+    const Tensor* index = ctx.Input<Tensor>("Index");
+    const Tensor* axis = ctx.Input<Tensor>("Axis");
+    Tensor* out = ctx.Output<Tensor>("Y");
 
-    dX->mutable_data<T>(ctx.GetPlace());
-    auto dxt = framework::EigenVector<T>::Flatten(*dX);
-    auto &place = *ctx.template device_context<platform::CUDADeviceContext>()
-                       .eigen_device();
-    dxt.device(place) = dxt.constant(static_cast<T>(0));
-    if (dO->numel() == 0) return;
-
-    const auto &index_type = index->type();
-    bool index_type_match = index_type == framework::proto::VarType::INT32 ||
-                            index_type == framework::proto::VarType::INT64;
-
-    PADDLE_ENFORCE_EQ(index_type_match, true,
-                      platform::errors::InvalidArgument(
-                          "Index holds the wrong type, it holds [%s],"
-                          "but desires to be [%s] or [%s].",
-                          paddle::framework::DataTypeToString(index_type),
-                          paddle::framework::DataTypeToString(
-                              framework::proto::VarType::INT32),
-                          paddle::framework::DataTypeToString(
-                              framework::proto::VarType::INT64)));
-
-    if (index_type == framework::proto::VarType::INT32) {
-      GPUScatterNdAdd<DeviceContext, T, int>(ctx, *dO, *index, dX);
-    } else if (index_type == framework::proto::VarType::INT64) {
-      GPUScatterNdAdd<DeviceContext, T, int64_t>(ctx, *dO, *index, dX);
+    const auto& index_type = index->type();
+    const auto& axis_type = axis->type();
+    auto place = ctx.GetPlace();
+    if (index_type == framework::proto::VarType::INT32 &&
+        axis_type == framework::proto::VarType::INT32) {
+      GatherV2GradCUDAFunction<T, int32_t, int32_t>(input, index, axis, out,
+                                                    place, ctx);
     }
-
-    */
+    if (index_type == framework::proto::VarType::INT32 &&
+        axis_type == framework::proto::VarType::INT64) {
+      GatherV2GradCUDAFunction<T, int32_t, int64_t>(input, index, axis, out,
+                                                    place, ctx);
+    }
+    if (index_type == framework::proto::VarType::INT64 &&
+        axis_type == framework::proto::VarType::INT32) {
+      GatherV2GradCUDAFunction<T, int64_t, int32_t>(input, index, axis, out,
+                                                    place, ctx);
+    }
+    if (index_type == framework::proto::VarType::INT64 &&
+        axis_type == framework::proto::VarType::INT64) {
+      GatherV2GradCUDAFunction<T, int64_t, int64_t>(input, index, axis, out,
+                                                    place, ctx);
+    }
   }
 };
 
--- paddle/fluid/operators/gather_v2_op.h
+++ paddle/fluid/operators/gather_v2_op.h
@@ -18,17 +18,19 @@ limitations under the License. */
 #include "paddle/fluid/framework/eigen.h"
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/operators/gather.h"
+#include "paddle/fluid/operators/math/math_function.h"
 
 namespace paddle {
 namespace operators {
 
 using Tensor = framework::Tensor;
+
 template <typename T, typename U, typename V>
 void GatherV2Function(const Tensor* input, const Tensor* index,
                       const Tensor* axis, Tensor* out,
                       const paddle::platform::Place& place) {
-  auto* axis_data = axis->data<U>();
-  auto* index_data = index->data<V>();
+  auto* axis_data = axis->data<V>();
+  auto* index_data = index->data<U>();
 
   int axis_size = axis->numel();
   int index_size = index->numel();
@@ -66,12 +68,14 @@ void GatherV2Function(const Tensor* input, const Tensor* index,
   out->Resize(out_dim);
   auto* out_data = out->mutable_data<T>(place);
 
+  int out_index = 0;
   for (int i = 0; i < inner_dim_size; i++) {
     for (int j = 0; j < index_size; j++) {
       for (int k = 0; k < outer_dim_size; k++) {
         int index = k + index_data[j] * outer_dim_size +
                     (i * input_size / inner_dim_size);
-        out_data[i] = input_data[index];
+        out_data[out_index] = input_data[index];
+        out_index++;
       }
     }
   }
@@ -108,6 +112,57 @@ class GatherV2OpKernel : public framework::OpKernel<T> {
   }
 };
 
+template <typename T, typename U, typename V>
+void GatherV2GradFunction(const Tensor* input, const Tensor* index,
+                          const Tensor* axis, Tensor* out,
+                          const paddle::platform::Place& place) {
+  auto* axis_data = axis->data<V>();
+  auto* index_data = index->data<U>();
+
+  int axis_size = axis->numel();
+  int index_size = index->numel();
+  auto input_dim = input->dims();
+  auto* input_data = input->data<T>();
+
+  if (input->numel() == 0) return;
+  PADDLE_ENFORCE_EQ(axis_size, 1,
+                    platform::errors::InvalidArgument(
+                        "Axis size should be 1, but received %d", axis_size));
+  int axis_index = axis_data[0];
+  int index_dim_size = input_dim[axis_index];
+  PADDLE_ENFORCE_LE(
+      index_size, index_dim_size,
+      platform::errors::InvalidArgument(
+          "The size that index should be less equal than the dim size of "
+          "input,"
+          "but received index size:%d, the dim size of input %d.",
+          axis_size, index_dim_size));
+
+  int inner_dim_size = 1;
+  int outer_dim_size = 1;
+
+  for (int i = 0; i < axis_index; i++) {
+    inner_dim_size *= input_dim[i];
+  }
+  for (int i = axis_index + 1; i < input_dim.size(); i++) {
+    outer_dim_size *= input_dim[i];
+  }
+
+  auto* out_data = out->mutable_data<T>(place);
+  auto* dev_ctx = platform::DeviceContextPool::Instance().Get(place);
+  operators::math::set_constant(*dev_ctx, out, 0.0);
+
+  for (int i = 0; i < index_size; i++) {
+    for (int j = 0; j < inner_dim_size; j++) {
+      for (int k = 0; k < outer_dim_size; k++) {
+        int index = k + index_data[i] * outer_dim_size +
+                    j * outer_dim_size * index_dim_size;
+        out_data[index] += input_data[i];
+      }
+    }
+  }
+}
+
 template <typename T>
 class GatherV2GradientOpKernel : public framework::OpKernel<T> {
  public:
@@ -116,6 +171,26 @@ class GatherV2GradientOpKernel : public framework::OpKernel<T> {
     auto* axis = ctx.Input<Tensor>("Axis");
     auto* out = ctx.Output<Tensor>(framework::GradVarName("X"));
     auto* input = ctx.Input<Tensor>(framework::GradVarName("Y"));
+
+    const auto& index_type = index->type();
+    const auto& axis_type = axis->type();
+    auto place = ctx.GetPlace();
+    if (index_type == framework::proto::VarType::INT32 &&
+        axis_type == framework::proto::VarType::INT32) {
+      GatherV2GradFunction<T, int32_t, int32_t>(input, index, axis, out, place);
+    }
+    if (index_type == framework::proto::VarType::INT32 &&
+        axis_type == framework::proto::VarType::INT64) {
+      GatherV2GradFunction<T, int32_t, int64_t>(input, index, axis, out, place);
+    }
+    if (index_type == framework::proto::VarType::INT64 &&
+        axis_type == framework::proto::VarType::INT32) {
+      GatherV2GradFunction<T, int64_t, int32_t>(input, index, axis, out, place);
+    }
+    if (index_type == framework::proto::VarType::INT64 &&
+        axis_type == framework::proto::VarType::INT64) {
+      GatherV2GradFunction<T, int64_t, int64_t>(input, index, axis, out, place);
+    }
   }
 };
 
