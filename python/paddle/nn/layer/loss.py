f500d7b39b58f2cac27aacdc258522668184faa3
ZHUI <zhonghui.net@gmail.com>
2020-08-21 01:42:51 +0800

    fix BCEWithLogitsLoss demo code.

--- python/paddle/nn/layer/loss.py
+++ python/paddle/nn/layer/loss.py
@@ -109,7 +109,8 @@ class BCEWithLogitsLoss(fluid.dygraph.Layer):
             paddle.disable_static()
             logit = paddle.to_tensor([5.0, 1.0, 3.0], dtype="float32")
             label = paddle.to_tensor([1.0, 0.0, 1.0], dtype="float32")
-            output = paddle.nn.functional.binary_cross_entropy_with_logits(logit, label)
+            bce_logit_loss = paddle.nn.BCEWithLogitsLoss()
+            output = bce_logit_loss(logit, label)
             print(output.numpy())  # [0.45618808]
 
     """
