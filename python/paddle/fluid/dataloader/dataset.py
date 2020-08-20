62762da829d09478bf79425cfa4464a7631bc6cb
dengkaipeng <dengkaipeng@baidu.com>
2020-08-20 07:27:35 +0000

    remove fluid in sample code. test=develop

--- python/paddle/fluid/dataloader/dataset.py
+++ python/paddle/fluid/dataloader/dataset.py
@@ -243,21 +243,21 @@ class TensorDataset(Dataset):
         .. code-block:: python
 
 	    import numpy as np
-	    import paddle.fluid as fluid
+            import paddle
 	    from paddle.io import TensorDataset
 
-	    place = fluid.CPUPlace()
-	    with fluid.dygraph.guard(place):
-                input_np = np.random.random([2, 3, 4]).astype('float32')
-                input = paddle.to_tensor(input_np)
-                label_np = np.random.random([2, 1]).astype('int32')
-                label = paddle.to_tensor(label_np)
-                
-                dataset = TensorDataset([input, label])
+            paddle.disable_static()
 
-                for i in range(len(dataset)):
-                    input, label = dataset[i]
-                    print(input, label)
+            input_np = np.random.random([2, 3, 4]).astype('float32')
+            input = paddle.to_tensor(input_np)
+            label_np = np.random.random([2, 1]).astype('int32')
+            label = paddle.to_tensor(label_np)
+            
+            dataset = TensorDataset([input, label])
+
+            for i in range(len(dataset)):
+                input, label = dataset[i]
+                print(input, label)
 
     """
 
