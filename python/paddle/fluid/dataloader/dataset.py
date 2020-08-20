99e9ca70a71a67c045b891799bd08525e6e7f85d
dengkaipeng <dengkaipeng@baidu.com>
2020-08-20 10:17:33 +0000

    fix indent. test=develop

--- python/paddle/fluid/dataloader/dataset.py
+++ python/paddle/fluid/dataloader/dataset.py
@@ -241,20 +241,20 @@ class TensorDataset(Dataset):
     Examples:
 
         .. code-block:: python
-
-	    import numpy as np
+        
+            import numpy as np
             import paddle
             from paddle.io import TensorDataset
             
             paddle.disable_static()
-            
+
             input_np = np.random.random([2, 3, 4]).astype('float32')
             input = paddle.to_tensor(input_np)
             label_np = np.random.random([2, 1]).astype('int32')
             label = paddle.to_tensor(label_np)
             
             dataset = TensorDataset([input, label])
-
+            
             for i in range(len(dataset)):
                 input, label = dataset[i]
                 print(input, label)
