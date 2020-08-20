9691cbfbb1353c903637823fac4196bbed2acc6a
shippingwang <shipeng1108@163.com>
2020-08-20 04:04:13 +0000

    add adaptive max pooling

--- python/paddle/nn/__init__.py
+++ python/paddle/nn/__init__.py
@@ -79,6 +79,10 @@ from .layer.common import Embedding  #DEFINE_ALIAS
 from .layer.common import Linear  #DEFINE_ALIAS
 from .layer.common import Flatten  #DEFINE_ALIAS
 from .layer.common import UpSample  #DEFINE_ALIAS
+
+from .layer.common import AdaptiveMaxPool2d  #DEFINE_ALIAS
+from .layer.common import AdaptiveMaxPool3d  #DEFINE_ALIAS
+
 from .layer.conv import Conv2D  #DEFINE_ALIAS
 from .layer.conv import Conv2DTranspose  #DEFINE_ALIAS
 from .layer.conv import Conv3D  #DEFINE_ALIAS
