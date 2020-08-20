ce3179552605d8c3bd79992438d591c209bea348
MRXLT <xlt2024@gmail.com>
2020-08-20 20:37:30 +0800

    add sample code for Optimizer

--- python/paddle/optimizer/optimizer.py
+++ python/paddle/optimizer/optimizer.py
@@ -74,6 +74,27 @@ class Optimizer(object):
 
     Returns:
        Base class for optimizer. 
+    
+    Examples:
+        .. code-block:: python
+
+            #Take the subclass adam as an example
+            #Optimizer 
+            import paddle
+            import numpy as np
+
+            paddle.disable_static()
+            inp = np.random.uniform(-0.1, 0.1, [10, 10]).astype("float32")
+            linear = paddle.nn.Linear(10, 10)
+            inp = paddle.to_tensor(inp)
+            out = linear(inp)
+            loss = paddle.mean(out)
+            adam = paddle.optimizer.Adam(learning_rate=0.1,
+                    parameters=linear.parameters())
+            out.backward()
+            adam.step()
+            adam.clear_grad()
+
     """
 
     @imperative_base.no_grad()
