16b5fb504f724d8341dedffafe70a55cff3ce2e8
Guanghua Yu <742925032@qq.com>
2020-08-20 10:48:08 +0800

    add paddle.nn.SmoothL1Loss,test=develop (#26398)

--- python/paddle/nn/__init__.py
+++ python/paddle/nn/__init__.py
@@ -101,6 +101,7 @@ from .layer.loss import NLLLoss  #DEFINE_ALIAS
 from .layer.loss import BCELoss  #DEFINE_ALIAS
 from .layer.loss import KLDivLoss  #DEFINE_ALIAS
 from .layer.loss import MarginRankingLoss  #DEFINE_ALIAS
+from .layer.loss import SmoothL1Loss  #DEFINE_ALIAS
 from .layer.norm import BatchNorm  #DEFINE_ALIAS
 from .layer.norm import SyncBatchNorm  #DEFINE_ALIAS
 from .layer.norm import GroupNorm  #DEFINE_ALIAS
