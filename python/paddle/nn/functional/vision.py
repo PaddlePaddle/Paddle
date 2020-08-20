6ceae74a0d03e52803cb0195c59dbfcfa8de0373
shippingwang <shipeng1108@163.com>
2020-08-20 05:48:38 +0000

    vision.py

--- python/paddle/nn/functional/vision.py
+++ python/paddle/nn/functional/vision.py
@@ -96,15 +96,8 @@ __all__ = [
 
 def pixel_shuffle(x, upscale_factor, data_format="NCHW", name=None):
     """
-    
-    This operator rearranges elements in a tensor of shape [N, C, H, W]
-    to a tensor of shape [N, C/upscale_factor**2, H*upscale_factor, W*upscale_factor],
-    or from shape [N, H, W, C] to [N, H*upscale_factor, W*upscale_factor, C/upscale_factor**2].
-    This is useful for implementing efficient sub-pixel convolution
-    with a stride of 1/upscale_factor.
-    Please refer to the paper: `Real-Time Single Image and Video Super-Resolution
-    Using an Efficient Sub-Pixel Convolutional Neural Network <https://arxiv.org/abs/1609.05158v2>`_ .
-    by Shi et. al (2016) for more details.
+    This API implements pixel shuffle operation.
+    See more details in :ref:`api_nn_vision_PixelShuffle` .
 
     Parameters:
 
