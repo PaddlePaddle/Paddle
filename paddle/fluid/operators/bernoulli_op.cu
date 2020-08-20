291fb4a8f74bb3be4aad06af44f7a02bffd8f11d
zhiqiu <chenqiuliang@baidu.com>
2020-08-21 02:45:33 +0800

    fix cuda kernel and add unit test

--- paddle/fluid/operators/bernoulli_op.cu
+++ paddle/fluid/operators/bernoulli_op.cu
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#include <thrust/execution_policy.h>
 #include <thrust/random.h>
 #include <thrust/transform.h>
 
@@ -19,6 +20,7 @@ limitations under the License. */
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/framework/operator.h"
 #include "paddle/fluid/operators/bernoulli_op.h"
+#include "paddle/fluid/platform/transform.h"
 
 namespace paddle {
 namespace operators {
@@ -28,10 +30,11 @@ struct BernoulliCudaFunctor {
   unsigned int seed_;
   __host__ __device__ BernoulliCudaFunctor(int seed) : seed_(seed) {}
 
-  __host__ __device__ T operator()(T p) const {
+  __host__ __device__ T operator()(const unsigned int n, const T p) const {
     thrust::minstd_rand rng;
     rng.seed(seed_);
     thrust::uniform_real_distribution<T> dist(0.0, 1.0);
+    rng.discard(n);
     return static_cast<T>(dist(rng) < p);
   }
 };
@@ -40,19 +43,21 @@ template <typename T>
 class BernoulliOpKernel<platform::CUDADeviceContext, T>
     : public framework::OpKernel<T> {
  public:
-  void Compute(const framework::ExecutionContext& context) const override {
+  void Compute(const framework::ExecutionContext& ctx) const override {
     std::random_device rd;
-    seed = rd();
+    auto seed = rd();
     const auto x = ctx.Input<framework::Tensor>("X");
     auto out = ctx.Output<framework::Tensor>("Out");
     auto* in_data = x->data<T>();
     auto* out_data = out->mutable_data<T>(ctx.GetPlace());
 
     int64_t size = x->numel();
-
-    thrust::transform(
-        thrust::device_ptr<T>(in_data), thrust::device_ptr<T>(in_data) + size,
-        thrust::device_ptr<T>(out_data), GaussianGenerator<T>(seed));
+    thrust::counting_iterator<unsigned int> index_sequence_begin(0);
+    platform::Transform<platform::CUDADeviceContext> trans;
+    auto* context =
+        static_cast<const platform::CUDADeviceContext*>(&ctx.device_context());
+    trans(*context, index_sequence_begin, index_sequence_begin + size, in_data,
+          out_data, BernoulliCudaFunctor<T>(seed));
   }
 };
 
@@ -62,6 +67,6 @@ class BernoulliOpKernel<platform::CUDADeviceContext, T>
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
 
-REGISTER_OP_CPU_KERNEL(bernoulli,
-                       ops::BernoulliOpKernel<plat::CUDADeviceContext, float>,
-                       ops::BernoulliOpKernel<plat::CUDADeviceContext, double>);
+REGISTER_OP_CUDA_KERNEL(
+    bernoulli, ops::BernoulliOpKernel<plat::CUDADeviceContext, float>,
+    ops::BernoulliOpKernel<plat::CUDADeviceContext, double>);
