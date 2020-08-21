29b12d6f6ab4040073166f1da43c0f6f0c81e68a
Dong Daxiang <35550832+guru4elephant@users.noreply.github.com>
2020-08-21 13:27:08 +0800

    【paddle.fleet】Meta from optimizer (#26392)
    * consider the combination of different strategies to work together

--- python/paddle/distributed/fleet/meta_optimizers/lars_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/lars_optimizer.py
@@ -24,7 +24,8 @@ class LarsOptimizer(MetaOptimizerBase):
         self.inner_opt = optimizer
         self.lars_opt = None
         # we do not allow meta optimizer to be inner optimizer currently
-        self.meta_optimizers_white_list = []
+        self.meta_optimizers_white_list = ["GraphExecutionOptimizer"]
+        self.meta_optimizers_black_list = []
 
     def _set_basic_info(self, loss, role_maker, user_defined_optimizer,
                         user_defined_strategy):
