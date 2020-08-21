e7d68bbd050906984dd549b3b2963a7f154f092b
shippingwang <shipeng1108@163.com>
2020-08-21 12:18:46 +0000

    fix unittestgit status!

--- python/paddle/fluid/tests/unittests/test_pixel_shuffle.py
+++ python/paddle/fluid/tests/unittests/test_pixel_shuffle.py
@@ -16,8 +16,12 @@ from __future__ import print_function
 
 import unittest
 import numpy as np
+
 from op_test import OpTest
 import paddle
+import paddle.nn.functional as F
+import paddle.fluid.core as core
+import paddle.fluid as fluid
 
 
 def pixel_shuffle_np(x, up_factor, data_format="NCHW"):
@@ -45,7 +49,7 @@ def pixel_shuffle_np(x, up_factor, data_format="NCHW"):
         return npresult
 
 
-class TestPixelShuffle(OpTest):
+class TestPixelShuffleOp(OpTest):
     def setUp(self):
         self.op_type = "pixel_shuffle"
         self.init_data_format()
@@ -75,13 +79,71 @@ class TestPixelShuffle(OpTest):
         self.check_grad(['X'], 'Out')
 
 
-class TestChannelLast(TestPixelShuffle):
+class TestChannelLast(TestPixelShuffleOp):
     def init_data_format(self):
         self.format = "NHWC"
 
 
-class TestPixelShuffleDygraph(unittest.TestCase):
-    def run_pixel_shuffle(self, up_factor, data_format):
+class TestPixelShuffleAPI(unittest.TestCase):
+    def setUp(self):
+        self.x_1_np = np.random.random([2, 9, 4, 4]).astype("float64")
+        self.x_2_np = np.random.random([2, 4, 4, 9]).astype("float64")
+        self.out_1_np = pixel_shuffle_np(self.x_1_np, 3)
+        self.out_2_np = pixel_shuffle_np(self.x_2_np, 3, "NHWC")
+
+    def test_static_graph_functional(self):
+        for use_cuda in ([False, True]
+                         if core.is_compiled_with_cuda() else [False]):
+            place = paddle.CUDAPlace(0) if use_cuda else paddle.CPUPlace()
+
+            paddle.enable_static()
+            x_1 = paddle.data(name="x", shape=[2, 9, 4, 4], dtype="float64")
+            x_2 = paddle.data(name="x2", shape=[2, 4, 4, 9], dtype="float64")
+            out_1 = F.pixel_shuffle(x_1, 3)
+            out_2 = F.pixel_shuffle(x_2, 3, "NHWC")
+
+            exe = paddle.static.Executor(place=place)
+            res_1 = exe.run(fluid.default_main_program(),
+                            feed={"x": self.x_1_np},
+                            fetch_list=out_1)
+
+            res_2 = exe.run(fluid.default_main_program(),
+                            feed={"x2": self.x_2_np},
+                            fetch_list=out_2)
+
+            assert np.allclose(res_1, self.out_1_np)
+            assert np.allclose(res_2, self.out_2_np)
+
+    # same test between layer and functional in this op.
+    def test_static_graph_layer(self):
+        for use_cuda in ([False, True]
+                         if core.is_compiled_with_cuda() else [False]):
+            place = paddle.CUDAPlace(0) if use_cuda else paddle.CPUPlace()
+
+            paddle.enable_static()
+            x_1 = paddle.data(name="x", shape=[2, 9, 4, 4], dtype="float64")
+            x_2 = paddle.data(name="x2", shape=[2, 4, 4, 9], dtype="float64")
+            # init instance
+            ps_1 = paddle.nn.PixelShuffle(3)
+            ps_2 = paddle.nn.PixelShuffle(3, "NHWC")
+            out_1 = ps_1(x_1)
+            out_2 = ps_2(x_2)
+            out_1_np = pixel_shuffle_np(self.x_1_np, 3)
+            out_2_np = pixel_shuffle_np(self.x_2_np, 3, "NHWC")
+
+            exe = paddle.static.Executor(place=place)
+            res_1 = exe.run(fluid.default_main_program(),
+                            feed={"x": self.x_1_np},
+                            fetch_list=out_1)
+
+            res_2 = exe.run(fluid.default_main_program(),
+                            feed={"x2": self.x_2_np},
+                            fetch_list=out_2)
+
+            assert np.allclose(res_1, out_1_np)
+            assert np.allclose(res_2, out_2_np)
+
+    def run_dygraph(self, up_factor, data_format):
 
         n, c, h, w = 2, 9, 4, 4
 
@@ -94,18 +156,27 @@ class TestPixelShuffleDygraph(unittest.TestCase):
 
         npresult = pixel_shuffle_np(x, up_factor, data_format)
 
-        paddle.disable_static()
-        pixel_shuffle = paddle.nn.PixelShuffle(
-            up_factor, data_format=data_format)
-        result = pixel_shuffle(paddle.to_tensor(x))
+        for use_cuda in ([False, True]
+                         if core.is_compiled_with_cuda() else [False]):
+            place = paddle.CUDAPlace(0) if use_cuda else paddle.CPUPlace()
 
-        self.assertTrue(np.allclose(result.numpy(), npresult))
+            paddle.disable_static(place=place)
 
-    def test_pixel_shuffle(self):
-        self.run_pixel_shuffle(3, "NCHW")
+            pixel_shuffle = paddle.nn.PixelShuffle(
+                up_factor, data_format=data_format)
+            result = pixel_shuffle(paddle.to_tensor(x))
 
-    def test_channel_last(self):
-        self.run_pixel_shuffle(3, "NHWC")
+            self.assertTrue(np.allclose(result.numpy(), npresult))
+
+            result_functional = F.pixel_shuffle(
+                paddle.to_tensor(x), 3, data_format)
+            self.assertTrue(np.allclose(result_functional.numpy(), npresult))
+
+    def test_dygraph1(self):
+        self.run_dygraph(3, "NCHW")
+
+    def test_dygraph2(self):
+        self.run_dygraph(3, "NHWC")
 
 
 if __name__ == '__main__':
