c4003e9ca78bfe8e9eb12afe0a44ed4c54caba40
zhupengyang <zhu_py@qq.com>
2020-08-22 14:16:42 +0800

    leaky_relu docs

--- python/paddle/nn/layer/activation.py
+++ python/paddle/nn/layer/activation.py
@@ -625,7 +625,7 @@ class LeakyReLU(layers.Layer):
             paddle.disable_static()
 
             m = paddle.nn.LeakyReLU()
-            x = paddle.to_tensor(np.array([-2, 0, 1]))
+            x = paddle.to_tensor(np.array([-2, 0, 1], 'float32'))
             out = m(x)  # [-0.02, 0., 1.]
     """
 
