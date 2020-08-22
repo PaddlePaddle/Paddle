4a1706ef3753485c65730798eb70aeadea01d8d6
huangjun12 <2399845970@qq.com>
2020-08-22 10:30:16 +0800

    [API2.0] add dropout, dropout2d and dropout3d in nn.functional and nn.layer (#26111)
    * [API2.0] add dropout, dropout2d and dropout3d, test=develop
    
    * refine Interface and assertion after review, test=develop
    
    * fix alias p=1 and use scale, test=develop
    
    * fix doc and training, test=develop
    
    * fix doc in Dropout2D, test=develop

--- python/paddle/nn/__init__.py
+++ python/paddle/nn/__init__.py
@@ -87,6 +87,9 @@ from .layer.common import Embedding  #DEFINE_ALIAS
 from .layer.common import Linear  #DEFINE_ALIAS
 from .layer.common import Flatten  #DEFINE_ALIAS
 from .layer.common import UpSample  #DEFINE_ALIAS
+from .layer.common import Dropout  #DEFINE_ALIAS
+from .layer.common import Dropout2D  #DEFINE_ALIAS
+from .layer.common import Dropout3D  #DEFINE_ALIAS
 from .layer.pooling import AdaptiveAvgPool2d  #DEFINE_ALIAS
 from .layer.pooling import AdaptiveAvgPool3d  #DEFINE_ALIAS
 from .layer.conv import Conv2D  #DEFINE_ALIAS
--- python/paddle/nn/layer/__init__.py
+++ python/paddle/nn/layer/__init__.py
@@ -52,6 +52,9 @@ from .common import Embedding  #DEFINE_ALIAS
 from .common import Linear  #DEFINE_ALIAS
 from .common import Flatten  #DEFINE_ALIAS
 from .common import UpSample  #DEFINE_ALIAS
+from .common import Dropout  #DEFINE_ALIAS
+from .common import Dropout2D  #DEFINE_ALIAS
+from .common import Dropout3D  #DEFINE_ALIAS
 from .pooling import AdaptiveAvgPool2d  #DEFINE_ALIAS
 from .pooling import AdaptiveAvgPool3d  #DEFINE_ALIAS
 from .conv import Conv2D  #DEFINE_ALIAS
