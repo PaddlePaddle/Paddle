c6a95a12f770c440392622f069a51d14add5db52
shenliang03 <shenliang03@baidu.com>
2020-08-22 12:58:49 +0800

    fix api

--- python/paddle/fluid/tests/unittests/test_data.py
+++ python/paddle/fluid/tests/unittests/test_data.py
@@ -56,7 +56,7 @@ class TestApiDataError(unittest.TestCase):
 
 
 class TestApiStaticDataError(unittest.TestCase):
-    def test_fluid_data(self):
+    def test_fluid_dtype(self):
         with program_guard(Program(), Program()):
             x1 = paddle.static.data(name="x1", shape=[2, 25])
             self.assertEqual(x1.dtype, core.VarDesc.VarType.FP32)
@@ -67,6 +67,36 @@ class TestApiStaticDataError(unittest.TestCase):
             paddle.set_default_dtype("float64")
             x3 = paddle.static.data(name="x3", shape=[2, 25])
             self.assertEqual(x3.dtype, core.VarDesc.VarType.FP64)
+
+    def test_fluid_data(self):
+        with program_guard(Program(), Program()):
+
+            # 1. The type of 'name' in fluid.data must be str.
+            def test_name_type():
+                paddle.static.data(name=1, shape=[2, 25], dtype="bool")
+
+            self.assertRaises(TypeError, test_name_type)
+
+            # 2. The type of 'shape' in fluid.data must be list or tuple.
+            def test_shape_type():
+                paddle.static.data(name='data1', shape=2, dtype="bool")
+
+            self.assertRaises(TypeError, test_shape_type)
+
+    def test_layers_data(self):
+        with program_guard(Program(), Program()):
+
+            # 1. The type of 'name' in layers.data must be str.
+            def test_name_type():
+                paddle.static.data(name=1, shape=[2, 25], dtype="bool")
+
+            self.assertRaises(TypeError, test_name_type)
+
+            # 2. The type of 'shape' in layers.data must be list or tuple.
+            def test_shape_type():
+                paddle.static.data(name='data1', shape=2, dtype="bool")
+
+            self.assertRaises(TypeError, test_shape_type)
 
 
 if __name__ == "__main__":
