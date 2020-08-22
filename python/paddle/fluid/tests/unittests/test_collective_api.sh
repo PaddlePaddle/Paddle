de9acb498f1d6a5ef5d2b957ade0826aa172d375
sandyhouse <lilong12@baidu.com>
2020-08-22 03:10:52 +0000

    fix ut, test=develop

--- python/paddle/fluid/tests/unittests/test_collective_api.sh
+++ python/paddle/fluid/tests/unittests/test_collective_api.sh
@@ -3,5 +3,5 @@ set -e
 # use default values
 # FIXME: random fails on Unknown command lines -c (or -m).
 launch_py=${PADDLE_BINARY_DIR}/python/paddle/distributed/launch.py
-python ${launch_py} --selected_gpus="0,1" paddle_distributed.py "nccl"
-python ${launch_py} --selected_gpus="0,1" paddle_distributed.py "gloo"
+CUDA_VISIBLE_DEVICE="0,1" python ${launch_py} --selected_gpus="0,1" paddle_distributed.py "nccl"
+CUDA_VISIBLE_DEVICE="0,1" python ${launch_py} --selected_gpus="0,1" paddle_distributed.py "gloo"
