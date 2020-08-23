1c7101a1d5702a65ac42fc71ad2d4e7fe1d6c82e
huangjun12 <2399845970@qq.com>
2020-08-23 21:45:10 +0800

    add alpha_dropout in nn.functional and nn.layer, test=develop (#26365)
    * add alpha_dropout in nn.functional and nn.layer, test=develop
    
    * refine Interface and assertion, test=develop
    
    * fix ci import error, test=develop
    
    * fix alias and use layers.scale, test=develop
    
    * fix doc and training params, test=develop
    
    * refine details in doc, test=develop
    
    * fix doc details, test=develop

--- python/paddle/nn/functional/__init__.py
+++ python/paddle/nn/functional/__init__.py
@@ -57,6 +57,7 @@ from .activation import log_softmax  #DEFINE_ALIAS
 from .common import dropout  #DEFINE_ALIAS
 from .common import dropout2d  #DEFINE_ALIAS
 from .common import dropout3d  #DEFINE_ALIAS
+from .common import alpha_dropout  #DEFINE_ALIAS
 # from .common import embedding        #DEFINE_ALIAS
 # from .common import fc  #DEFINE_ALIAS
 from .common import label_smooth  #DEFINE_ALIAS
