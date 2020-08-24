8a4248681e089790c963342a5974893d66c23bac
zhhsplendid <zhhsplendid@163.com>
2020-08-24 06:42:51 +0000

    Modify based on reviewer's comment, test=document_fix

--- python/paddle/fluid/dygraph/jit.py
+++ python/paddle/fluid/dygraph/jit.py
@@ -132,8 +132,8 @@ def _declarative_(dygraph_func):
     """
     Converts imperative dygraph APIs into declarative function APIs. Decorator
     @declarative handles the Program and Executor of static mode and returns
-    the result as a dygraph VarBase. Users could use the returned dygraph
-    Varbase to do imperative training, inference, or other operations. If the
+    the result as a dygraph Tensor. Users could use the returned dygraph
+    Tensor to do imperative training, inference, or other operations. If the
     decorated function calls other imperative function, the called one will be
     converted into declarative function as well.
 
@@ -141,7 +141,7 @@ def _declarative_(dygraph_func):
         dygraph_func (callable): callable imperative function.
 
     Returns:
-        VarBase: containing the numerical result.
+        Tensor: containing the numerical result.
 
     Examples:
         .. code-block:: python
