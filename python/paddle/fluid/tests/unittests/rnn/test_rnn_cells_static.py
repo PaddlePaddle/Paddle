779e2263610b0ca27b0c5421499a5a7b16baa275
chenfeiyu <chenfeiyu@baidu.com>
2020-08-25 09:39:57 +0800

    remove unnecessary imports

--- python/paddle/fluid/tests/unittests/rnn/test_rnn_cells_static.py
+++ python/paddle/fluid/tests/unittests/rnn/test_rnn_cells_static.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
-torch.set_default_dtype(torch.float64)
-
 import paddle
 paddle.framework.set_default_dtype("float64")
 
--- python/paddle/fluid/tests/unittests/rnn/test_rnn_nets.py
+++ python/paddle/fluid/tests/unittests/rnn/test_rnn_nets.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
-
 import paddle
 paddle.set_default_dtype("float64")
 from paddle.fluid.layers import sequence_mask
