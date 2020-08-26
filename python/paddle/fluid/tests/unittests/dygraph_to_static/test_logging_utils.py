51fc574003873c4a2f1dfc7abcbb599e0a55556a
liym27 <liym0923@126.com>
2020-08-26 17:34:14 +0800

    Expose interfaces set_code_level and set_verbosity in paddle.jit, fix doc of the two interface. test=develop
    Change-Id: Ia902416935464571eaaaa45a997ca95dc83a1cf5

--- python/paddle/fluid/tests/unittests/dygraph_to_static/test_logging_utils.py
+++ python/paddle/fluid/tests/unittests/dygraph_to_static/test_logging_utils.py
@@ -22,6 +22,8 @@ import unittest
 
 import gast
 import six
+
+import paddle
 from paddle.fluid.dygraph.dygraph_to_static import logging_utils
 
 # TODO(liym27): library mock needs to be installed separately in PY2,
@@ -40,30 +42,34 @@ class TestLoggingUtils(unittest.TestCase):
         self.translator_logger = logging_utils._TRANSLATOR_LOGGER
 
     def test_verbosity(self):
-        logging_utils.set_verbosity(None)
+        paddle.jit.set_verbosity(None)
         os.environ[logging_utils.VERBOSITY_ENV_NAME] = '3'
         self.assertEqual(logging_utils.get_verbosity(), 3)
 
-        logging_utils.set_verbosity(self.verbosity_level)
+        paddle.jit.set_verbosity(self.verbosity_level)
         self.assertEqual(self.verbosity_level, logging_utils.get_verbosity())
 
         # String is not supported
         with self.assertRaises(TypeError):
-            logging_utils.set_verbosity("3")
+            paddle.jit.set_verbosity("3")
 
         with self.assertRaises(TypeError):
-            logging_utils.set_verbosity(3.3)
+            paddle.jit.set_verbosity(3.3)
 
     def test_code_level(self):
 
-        logging_utils.set_code_level(self.code_level)
+        paddle.jit.set_code_level(None)
+        os.environ[logging_utils.CODE_LEVEL_ENV_NAME] = '2'
+        self.assertEqual(logging_utils.get_code_level(), 2)
+
+        paddle.jit.set_code_level(self.code_level)
         self.assertEqual(logging_utils.get_code_level(), self.code_level)
 
-        logging_utils.set_code_level(9)
+        paddle.jit.set_code_level(9)
         self.assertEqual(logging_utils.get_code_level(), 9)
 
         with self.assertRaises(TypeError):
-            logging_utils.set_code_level(3.3)
+            paddle.jit.set_code_level(3.3)
 
     def test_log(self):
         stream = io.BytesIO() if six.PY2 else io.StringIO()
@@ -98,11 +104,11 @@ class TestLoggingUtils(unittest.TestCase):
 
         if six.PY3:
             with mock.patch.object(sys, 'stdout', stream):
-                logging_utils.set_code_level(1)
+                paddle.jit.set_code_level(1)
                 logging_utils.log_transformed_code(1, ast_code,
                                                    "BasicApiTransformer")
 
-                logging_utils.set_code_level()
+                paddle.jit.set_code_level()
                 logging_utils.log_transformed_code(
                     logging_utils.LOG_AllTransformer, ast_code,
                     "All Transformers")
