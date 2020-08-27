b71b175295907c448237bb45dafdd34f822a2813
lilong12 <lilong12@baidu.com>
2020-08-27 12:55:06 +0800

    [api 2.0] add collective op for cpu using gloo and paddle.distributed.* apis (#26552)
    add collective op for cpu using gloo and paddle.distributed.* apis

--- paddle/fluid/operators/collective/barrier_op.h
+++ paddle/fluid/operators/collective/barrier_op.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,11 +19,12 @@ limitations under the License. */
 #include <vector>
 
 #include "paddle/fluid/framework/data_type.h"
+#include "paddle/fluid/framework/ddim.h"
 #include "paddle/fluid/framework/lod_tensor.h"
 #include "paddle/fluid/framework/op_registry.h"
 
 #if defined(PADDLE_WITH_GLOO)
-#include <gloo/scatter.h>
+#include <gloo/barrier.h>
 #include "paddle/fluid/framework/fleet/gloo_wrapper.h"
 #endif
 
@@ -31,38 +32,17 @@ namespace paddle {
 namespace operators {
 
 template <typename T>
-class CScatterOpCPUKernel : public framework::OpKernel<T> {
+class BarrierOpCPUKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
 #if defined(PADDLE_WITH_GLOO)
-    auto in = ctx.Input<framework::Tensor>("X");
-    auto out = ctx.Output<framework::Tensor>("Out");
-    auto root_id = ctx.Attr<int>("root");
-
     auto gloo = paddle::framework::GlooWrapper::GetInstance();
     PADDLE_ENFORCE_EQ(
         gloo->IsInitialized(), true,
         platform::errors::PreconditionNotMet(
             "You must initialize the gloo environment first to use it."));
-
-    int64_t send_numel = out->numel();
-    auto nranks = gloo->Size();
-    auto rank = gloo->Rank();
-    T* recv_buff = out->data<T>();
-    gloo::ScatterOptions opts(gloo->GetContext());
-    if (root_id == rank) {
-      T* send_buff = const_cast<T*>(in->data<T>());
-      std::vector<T*> ptrs(nranks);
-      for (int i = 0; i < nranks; ++i) {
-        ptrs[i] = send_buff;
-        send_buff += send_numel;
-      }
-      opts.setInputs(ptrs, send_numel);
-    }
-    opts.setOutput(recv_buff, send_numel);
-    opts.setRoot(root_id);
-
-    gloo::scatter(opts);
+    gloo::BarrierOptions opts(gloo->GetContext());
+    gloo::barrier(opts);
 #else
     PADDLE_THROW(platform::errors::Unavailable(
         "PaddlePaddle should compile with GLOO by setting WITH_GLOO=ON"));
