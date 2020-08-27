be5e27f193c8f7523a6fe3c5ffede337178f8c96
yongqiangma <xing.wo@163.com>
2020-08-27 04:29:01 +0000

    my ok. test=develop

--- python/paddle/tensor/linalg.py
+++ python/paddle/tensor/linalg.py
@@ -215,34 +215,35 @@ def norm(x, p='fro', axis=None, keepdim=False, out=None, name=None):
             paddle.disable_static()
             shape=[2, 3, 4]
             np_input = np.arange(24).astype('float32') - 12
+            np_input = np_input.reshape(shape)
             x = paddle.to_tensor(np_input)
             #[[[-12. -11. -10.  -9.] [ -8.  -7.  -6.  -5.] [ -4.  -3.  -2.  -1.]]
             # [[  0.   1.   2.   3.] [  4.   5.   6.   7.] [  8.   9.  10.  11.]]]
 
             # compute frobenius norm along last two dimensions.
             out_fro = paddle.norm(x, p='fro', axis=[0,1])
-            # out_fro = [17.43559577 16.91153453 16.73320053 16.91153453]
+            # out_fro.numpy() [17.435596 16.911535 16.7332   16.911535]
 
             # compute 2-order vector norm along last dimension.
             out_pnorm = paddle.norm(x, p=2, axis=-1)
-            #out_pnorm = [[21.11871208 13.19090596  5.47722558]
-            #             [ 3.74165739 11.22497216 19.13112647]]
+            #out_pnorm.numpy(): [[21.118711  13.190906   5.477226]
+            #                    [ 3.7416575 11.224972  19.131126]]
 
             # compute 2-order  norm along [0,1] dimension.
             out_pnorm = paddle.norm(x, p=2, axis=[0,1])
-            #out_pnorm = [17.43559577 16.91153453 16.73320053 16.91153453]
+            #out_pnorm.numpy(): [17.435596 16.911535 16.7332   16.911535]
 
             # compute inf-order  norm
             out_pnorm = paddle.norm(x, p=np.inf)
-            #out_pnorm = [12.]
+            #out_pnorm.numpy()  = [12.]
             out_pnorm = paddle.norm(x, p=np.inf, axis=0)
-            #out_pnorm = [[0. 1. 2. 3.] [4. 5. 6. 5.] [4. 3. 2. 1.]]
+            #out_pnorm.numpy(): [[12. 11. 10. 9.] [8. 7. 6. 7.] [8. 9. 10. 11.]]
 
             # compute -inf-order  norm
             out_pnorm = paddle.norm(x, p=-np.inf)
-            #out_pnorm = [0.]
+            #out_pnorm.numpy(): [0.]
             out_pnorm = paddle.norm(x, p=-np.inf, axis=0)
-            #out_pnorm = [[0. 1. 2. 3.] [4. 5. 6. 5.] [4. 3. 2. 1.]]
+            #out_pnorm.numpy(): [[0. 1. 2. 3.] [4. 5. 6. 5.] [4. 3. 2. 1.]]
     """
 
     def frobenius_norm(input, dim=None, keepdim=False, out=None, name=None):
@@ -259,7 +260,8 @@ def norm(x, p='fro', axis=None, keepdim=False, out=None, name=None):
                 "The dim of frobenius norm op should be None or two elements list!"
             )
         if in_dygraph_mode():
-            return core.ops.frobenius_norm(input, 'dim', axis, 'keepdim',
+            if dim is None: dim = [-1]
+            return core.ops.frobenius_norm(input, 'dim', dim, 'keepdim',
                                            keepdim)
         attrs = {
             'dim': dim if dim != None else [-2, -1],
