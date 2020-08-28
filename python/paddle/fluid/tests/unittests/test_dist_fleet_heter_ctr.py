a7abb870af1ecca30ca7942cb0508a8598cb6e90
MrChengmo <cmchengmo@163.com>
2020-08-28 17:09:14 +0800

    fix & code clean

--- python/paddle/fluid/tests/unittests/test_dist_fleet_heter_ctr.py
+++ python/paddle/fluid/tests/unittests/test_dist_fleet_heter_ctr.py
@@ -20,38 +20,6 @@ import tempfile
 from test_dist_fleet_heter_base import TestFleetHeterBase
 
 
-class TestDistHeterPyreaderAsync2x2(TestFleetHeterBase):
-    def _setup_config(self):
-        self._mode = "async"
-        self._reader = "pyreader"
-
-    def check_with_place(self,
-                         model_file,
-                         delta=1e-3,
-                         check_error_log=False,
-                         need_envs={}):
-        required_envs = {
-            "PATH": os.getenv("PATH", ""),
-            "PYTHONPATH": os.getenv("PYTHONPATH", ""),
-            "LD_LIBRARY_PATH": os.getenv("LD_LIBRARY_PATH", ""),
-            "FLAGS_rpc_deadline": "5000",  # 5sec to fail fast
-            "http_proxy": "",
-            "CPU_NUM": "2"
-        }
-
-        required_envs.update(need_envs)
-
-        if check_error_log:
-            required_envs["GLOG_v"] = "4"
-            required_envs["GLOG_logtostderr"] = "1"
-
-        tr0_losses, tr1_losses = self._run_cluster(model_file, required_envs)
-
-    def test_dist_train(self):
-        self.check_with_place(
-            "dist_fleet_heter_ctr.py", delta=1e-5, check_error_log=True)
-
-
 class TestDistHeterDatasetAsync2x2(TestFleetHeterBase):
     def _setup_config(self):
         self._mode = "async"
