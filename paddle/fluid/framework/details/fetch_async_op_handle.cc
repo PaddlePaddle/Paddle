988b2dae9972bdb9c17048ffcf288c8dccdd4dbd
Wang Huan <wanghuan29@baidu.com>
2020-08-28 09:26:32 +0000

    fix bug, double copy gpu memory, test=develop

--- paddle/fluid/framework/details/fetch_async_op_handle.cc
+++ paddle/fluid/framework/details/fetch_async_op_handle.cc
@@ -184,12 +184,6 @@ void FetchAsyncOpHandle::FetchMergedLodTensor(
     }
     auto dst = dst_lodtensor->Slice(begin, end);
     TransData(src, &dst, *dev_ctxes_[src->place()]);
-    if (platform::is_gpu_place(src_lodtensors[0]->place())) {
-      framework::TensorCopy(*src, platform::CUDAPinnedPlace(), &dst);
-    } else {
-      framework::TensorCopy(*src, platform::CPUPlace(), &dst);
-    }
-
     begin = end;
   }
 }
