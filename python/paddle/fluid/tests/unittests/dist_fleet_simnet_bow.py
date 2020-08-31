07e60f7c339ccd19b5d40d4ef64338a4384f8486
chengmo <chengmo@baidu.com>
2020-08-31 20:52:15 +0800

    fix unittest

--- python/paddle/fluid/tests/unittests/dist_fleet_simnet_bow.py
+++ python/paddle/fluid/tests/unittests/dist_fleet_simnet_bow.py
@@ -64,140 +64,139 @@ def fake_simnet_reader():
     return reader
 
 
+def get_acc(cos_q_nt, cos_q_pt, batch_size):
+    cond = fluid.layers.less_than(cos_q_nt, cos_q_pt)
+    cond = fluid.layers.cast(cond, dtype='float64')
+    cond_3 = fluid.layers.reduce_sum(cond)
+    acc = fluid.layers.elementwise_div(
+        cond_3,
+        fluid.layers.fill_constant(
+            shape=[1], value=batch_size * 1.0, dtype='float64'),
+        name="simnet_acc")
+    return acc
+
+
+def get_loss(cos_q_pt, cos_q_nt):
+    loss_op1 = fluid.layers.elementwise_sub(
+        fluid.layers.fill_constant_batch_size_like(
+            input=cos_q_pt, shape=[-1, 1], value=margin, dtype='float32'),
+        cos_q_pt)
+    loss_op2 = fluid.layers.elementwise_add(loss_op1, cos_q_nt)
+    loss_op3 = fluid.layers.elementwise_max(
+        fluid.layers.fill_constant_batch_size_like(
+            input=loss_op2, shape=[-1, 1], value=0.0, dtype='float32'),
+        loss_op2)
+    avg_cost = fluid.layers.mean(loss_op3)
+    return avg_cost
+
+
+def train_network(batch_size,
+                  is_distributed=False,
+                  is_sparse=False,
+                  is_self_contained_lr=False,
+                  is_pyreader=False):
+    # query
+    q = fluid.layers.data(
+        name="query_ids", shape=[1], dtype="int64", lod_level=1)
+    # label data
+    label = fluid.layers.data(name="label", shape=[1], dtype="int64")
+    # pt
+    pt = fluid.layers.data(
+        name="pos_title_ids", shape=[1], dtype="int64", lod_level=1)
+    # nt
+    nt = fluid.layers.data(
+        name="neg_title_ids", shape=[1], dtype="int64", lod_level=1)
+
+    datas = [q, label, pt, nt]
+
+    reader = None
+    if is_pyreader:
+        reader = fluid.io.PyReader(
+            feed_list=datas,
+            capacity=64,
+            iterable=False,
+            use_double_buffer=False)
+
+    # embedding
+    q_emb = fluid.embedding(
+        input=q,
+        is_distributed=is_distributed,
+        size=[dict_dim, emb_dim],
+        param_attr=fluid.ParamAttr(
+            initializer=fluid.initializer.Constant(value=0.01), name="__emb__"),
+        is_sparse=is_sparse)
+    q_emb = fluid.layers.reshape(q_emb, [-1, emb_dim])
+    # vsum
+    q_sum = fluid.layers.sequence_pool(input=q_emb, pool_type='sum')
+    q_ss = fluid.layers.softsign(q_sum)
+    # fc layer after conv
+    q_fc = fluid.layers.fc(
+        input=q_ss,
+        size=hid_dim,
+        param_attr=fluid.ParamAttr(
+            initializer=fluid.initializer.Constant(value=0.01),
+            name="__q_fc__",
+            learning_rate=base_lr), )
+
+    # embedding
+    pt_emb = fluid.embedding(
+        input=pt,
+        is_distributed=is_distributed,
+        size=[dict_dim, emb_dim],
+        param_attr=fluid.ParamAttr(
+            initializer=fluid.initializer.Constant(value=0.01),
+            name="__emb__",
+            learning_rate=emb_lr),
+        is_sparse=is_sparse)
+    pt_emb = fluid.layers.reshape(pt_emb, [-1, emb_dim])
+    # vsum
+    pt_sum = fluid.layers.sequence_pool(input=pt_emb, pool_type='sum')
+    pt_ss = fluid.layers.softsign(pt_sum)
+    # fc layer
+    pt_fc = fluid.layers.fc(
+        input=pt_ss,
+        size=hid_dim,
+        param_attr=fluid.ParamAttr(
+            initializer=fluid.initializer.Constant(value=0.01), name="__fc__"),
+        bias_attr=fluid.ParamAttr(name="__fc_b__"))
+
+    # embedding
+    nt_emb = fluid.embedding(
+        input=nt,
+        is_distributed=is_distributed,
+        size=[dict_dim, emb_dim],
+        param_attr=fluid.ParamAttr(
+            initializer=fluid.initializer.Constant(value=0.01), name="__emb__"),
+        is_sparse=is_sparse)
+    nt_emb = fluid.layers.reshape(nt_emb, [-1, emb_dim])
+    # vsum
+    nt_sum = fluid.layers.sequence_pool(input=nt_emb, pool_type='sum')
+    nt_ss = fluid.layers.softsign(nt_sum)
+    # fc layer
+    nt_fc = fluid.layers.fc(
+        input=nt_ss,
+        size=hid_dim,
+        param_attr=fluid.ParamAttr(
+            initializer=fluid.initializer.Constant(value=0.01), name="__fc__"),
+        bias_attr=fluid.ParamAttr(name="__fc_b__"))
+    cos_q_pt = fluid.layers.cos_sim(q_fc, pt_fc)
+    cos_q_nt = fluid.layers.cos_sim(q_fc, nt_fc)
+    # loss
+    avg_cost = get_loss(cos_q_pt, cos_q_nt)
+    # acc
+    acc = get_acc(cos_q_nt, cos_q_pt, batch_size)
+    return avg_cost, acc, cos_q_pt, reader
+
+
 class TestDistSimnetBow2x2(FleetDistRunnerBase):
     """
     For test SimnetBow model, use Fleet api
     """
 
-    def train_network(self,
-                      args,
-                      batch_size,
-                      is_distributed=False,
-                      is_sparse=False,
-                      is_self_contained_lr=False):
-        # query
-        q = fluid.layers.data(
-            name="query_ids", shape=[1], dtype="int64", lod_level=1)
-        # label data
-        label = fluid.layers.data(name="label", shape=[1], dtype="int64")
-        # pt
-        pt = fluid.layers.data(
-            name="pos_title_ids", shape=[1], dtype="int64", lod_level=1)
-        # nt
-        nt = fluid.layers.data(
-            name="neg_title_ids", shape=[1], dtype="int64", lod_level=1)
-
-        datas = [q, label, pt, nt]
-
-        if args.reader == "pyreader":
-            self.reader = fluid.io.PyReader(
-                feed_list=datas,
-                capacity=64,
-                iterable=False,
-                use_double_buffer=False)
-
-        # embedding
-        q_emb = fluid.embedding(
-            input=q,
-            is_distributed=is_distributed,
-            size=[dict_dim, emb_dim],
-            param_attr=fluid.ParamAttr(
-                initializer=fluid.initializer.Constant(value=0.01),
-                name="__emb__"),
-            is_sparse=is_sparse)
-        q_emb = fluid.layers.reshape(q_emb, [-1, emb_dim])
-        # vsum
-        q_sum = fluid.layers.sequence_pool(input=q_emb, pool_type='sum')
-        q_ss = fluid.layers.softsign(q_sum)
-        # fc layer after conv
-        q_fc = fluid.layers.fc(
-            input=q_ss,
-            size=hid_dim,
-            param_attr=fluid.ParamAttr(
-                initializer=fluid.initializer.Constant(value=0.01),
-                name="__q_fc__",
-                learning_rate=base_lr), )
-
-        # embedding
-        pt_emb = fluid.embedding(
-            input=pt,
-            is_distributed=is_distributed,
-            size=[dict_dim, emb_dim],
-            param_attr=fluid.ParamAttr(
-                initializer=fluid.initializer.Constant(value=0.01),
-                name="__emb__",
-                learning_rate=emb_lr),
-            is_sparse=is_sparse)
-        pt_emb = fluid.layers.reshape(pt_emb, [-1, emb_dim])
-        # vsum
-        pt_sum = fluid.layers.sequence_pool(input=pt_emb, pool_type='sum')
-        pt_ss = fluid.layers.softsign(pt_sum)
-        # fc layer
-        pt_fc = fluid.layers.fc(
-            input=pt_ss,
-            size=hid_dim,
-            param_attr=fluid.ParamAttr(
-                initializer=fluid.initializer.Constant(value=0.01),
-                name="__fc__"),
-            bias_attr=fluid.ParamAttr(name="__fc_b__"))
-
-        # embedding
-        nt_emb = fluid.embedding(
-            input=nt,
-            is_distributed=is_distributed,
-            size=[dict_dim, emb_dim],
-            param_attr=fluid.ParamAttr(
-                initializer=fluid.initializer.Constant(value=0.01),
-                name="__emb__"),
-            is_sparse=is_sparse)
-        nt_emb = fluid.layers.reshape(nt_emb, [-1, emb_dim])
-        # vsum
-        nt_sum = fluid.layers.sequence_pool(input=nt_emb, pool_type='sum')
-        nt_ss = fluid.layers.softsign(nt_sum)
-        # fc layer
-        nt_fc = fluid.layers.fc(
-            input=nt_ss,
-            size=hid_dim,
-            param_attr=fluid.ParamAttr(
-                initializer=fluid.initializer.Constant(value=0.01),
-                name="__fc__"),
-            bias_attr=fluid.ParamAttr(name="__fc_b__"))
-        cos_q_pt = fluid.layers.cos_sim(q_fc, pt_fc)
-        cos_q_nt = fluid.layers.cos_sim(q_fc, nt_fc)
-        # loss
-        avg_cost = self.get_loss(cos_q_pt, cos_q_nt)
-        # acc
-        acc = self.get_acc(cos_q_nt, cos_q_pt, batch_size)
-        return [avg_cost, acc, cos_q_pt]
-
-    def get_acc(self, cos_q_nt, cos_q_pt, batch_size):
-        cond = fluid.layers.less_than(cos_q_nt, cos_q_pt)
-        cond = fluid.layers.cast(cond, dtype='float64')
-        cond_3 = fluid.layers.reduce_sum(cond)
-        acc = fluid.layers.elementwise_div(
-            cond_3,
-            fluid.layers.fill_constant(
-                shape=[1], value=batch_size * 1.0, dtype='float64'),
-            name="simnet_acc")
-        return acc
-
-    def get_loss(self, cos_q_pt, cos_q_nt):
-        loss_op1 = fluid.layers.elementwise_sub(
-            fluid.layers.fill_constant_batch_size_like(
-                input=cos_q_pt, shape=[-1, 1], value=margin, dtype='float32'),
-            cos_q_pt)
-        loss_op2 = fluid.layers.elementwise_add(loss_op1, cos_q_nt)
-        loss_op3 = fluid.layers.elementwise_max(
-            fluid.layers.fill_constant_batch_size_like(
-                input=loss_op2, shape=[-1, 1], value=0.0, dtype='float32'),
-            loss_op2)
-        avg_cost = fluid.layers.mean(loss_op3)
-        return avg_cost
-
     def net(self, args, batch_size=4, lr=0.01):
-        avg_cost, _, predict = \
-            self.train_network(args, batch_size=batch_size, is_distributed=False,
-                               is_sparse=True, is_self_contained_lr=False)
+        avg_cost, _, predict, self.reader = \
+            train_network(batch_size=batch_size, is_distributed=False,
+                               is_sparse=True, is_self_contained_lr=False, is_pyreader=(args.reader == "pyreader"))
         self.avg_cost = avg_cost
         self.predict = predict
 
