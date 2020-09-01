afe3288b60b684ca0de25f579606692838fea274
gfwm2013 <2449465946@qq.com>
2020-09-01 12:52:07 +0000

    [Not merge]Support python3.8
    test=notest

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -192,7 +192,7 @@ function cmake_base() {
                 export LD_LIBRARY_PATH=/opt/_internal/cpython-3.8.0/lib/:${LD_LIBRARY_PATH}
                 export PATH=/opt/_internal/cpython-3.8.0/bin/:${PATH}
                 export PYTHON_FLAGS="-DPYTHON_EXECUTABLE:FILEPATH=/opt/_internal/cpython-3.8.0/bin/python3.8
-            -DPYTHON_INCLUDE_DIR:PATH=/opt/_internal/cpython-3.8.0/include/python3.8m
+            -DPYTHON_INCLUDE_DIR:PATH=/opt/_internal/cpython-3.8.0/include/python3.8
             -DPYTHON_LIBRARIES:FILEPATH=/opt/_internal/cpython-3.8.0/lib/libpython3.so"
                 pip3.8 install -r ${PADDLE_ROOT}/python/requirements.txt
            fi
