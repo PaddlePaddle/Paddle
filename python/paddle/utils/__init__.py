a3956f2785ab07e847ca348d37ff5b3650dfaa80
LielinJiang <jianglielin@baidu.com>
2020-09-02 04:20:01 +0000

    rm unused code

--- python/paddle/utils/__init__.py
+++ python/paddle/utils/__init__.py
@@ -17,7 +17,7 @@ from .profiler import ProfilerOptions
 from .profiler import Profiler
 from .profiler import get_profiler
 from .deprecated import deprecated
-# from .model_summary import summary
+
 from . import download
 
 __all__ = ['dump_config', 'Ploter', 'deprecated', 'download']
