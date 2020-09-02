f3432cc7d3abf45dc4c2c83aaf52d54a479db61c
joanna.wozna.intel <joanna.wozna@intel.com>
2020-09-02 09:17:16 +0200

    Restore "Add mkldnn bfloat16 option to C-API " (#26882)
    * Add mkldnn bfloat16 option to C-API
    
    * Add test for bfloat16 gpu
    
    * Change coverage test
    
    * Repair capi_gpu test

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -143,6 +143,10 @@ void GpuPassStrategy::EnableMkldnnQuantizer() {
   LOG(ERROR) << "GPU not support MKL-DNN quantization";
 }
 
+void GpuPassStrategy::EnableMkldnnBfloat16() {
+  LOG(ERROR) << "GPU not support MKL-DNN bfloat16";
+}
+
 CpuPassStrategy::CpuPassStrategy() : PassStrategy({}) {
   // NOTE the large fusions should be located in the front, so that they will
   // not be damaged by smaller ones.
@@ -222,6 +226,14 @@ void CpuPassStrategy::EnableMkldnnQuantizer() {
   use_mkldnn_quantizer_ = true;
 #else
   use_mkldnn_quantizer_ = false;
+#endif
+}
+
+void CpuPassStrategy::EnableMkldnnBfloat16() {
+#ifdef PADDLE_WITH_MKLDNN
+  use_mkldnn_bfloat16_ = true;
+#else
+  use_mkldnn_bfloat16_ = false;
 #endif
 }
 
