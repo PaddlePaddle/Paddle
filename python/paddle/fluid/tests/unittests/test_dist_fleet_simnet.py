b42dc818e3af18c0477b2158fc8709f61817a6e9
Chengmo <cmchengmo@163.com>
2020-09-02 09:39:47 +0800

    supplement bug fix of parameter server (#26217)
    * fix fluid.embedding

--- python/paddle/fluid/tests/unittests/test_dist_fleet_simnet.py
+++ python/paddle/fluid/tests/unittests/test_dist_fleet_simnet.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,19 +16,14 @@ from __future__ import print_function
 
 import os
 import unittest
-import paddle.fluid as fluid
-import paddle.fluid.incubate.fleet.base.role_maker as role_maker
-from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler import fleet
-from paddle.fluid.incubate.fleet.parameter_server.distribute_transpiler.distributed_strategy import StrategyFactory
+import tempfile
 from test_dist_fleet_base import TestFleetBase
-from dist_fleet_simnet_bow import train_network
 
 
-class TestDistGeoCtr_2x2(TestFleetBase):
+class TestDistSimnetASync2x2(TestFleetBase):
     def _setup_config(self):
-        self._mode = "geo"
+        self._mode = "async"
         self._reader = "pyreader"
-        self._geo_sgd_need_push_nums = 5
 
     def check_with_place(self,
                          model_file,
@@ -40,46 +35,21 @@ class TestDistGeoCtr_2x2(TestFleetBase):
             "PYTHONPATH": os.getenv("PYTHONPATH", ""),
             "LD_LIBRARY_PATH": os.getenv("LD_LIBRARY_PATH", ""),
             "FLAGS_rpc_deadline": "5000",  # 5sec to fail fast
-            "http_proxy": ""
+            "http_proxy": "",
+            "CPU_NUM": "2"
         }
 
         required_envs.update(need_envs)
 
         if check_error_log:
-            required_envs["GLOG_v"] = "4"
+            required_envs["GLOG_v"] = "3"
             required_envs["GLOG_logtostderr"] = "1"
 
         tr0_losses, tr1_losses = self._run_cluster(model_file, required_envs)
 
     def test_dist_train(self):
         self.check_with_place(
-            "dist_fleet_ctr.py", delta=1e-5, check_error_log=True)
-
-
-class TestGeoSgdTranspiler(unittest.TestCase):
-    def test_pserver(self):
-        role = role_maker.UserDefinedRoleMaker(
-            current_id=0,
-            role=role_maker.Role.SERVER,
-            worker_num=2,
-            server_endpoints=["127.0.0.1:36011", "127.0.0.1:36012"])
-
-        fleet.init(role)
-
-        batch_size = 128
-        is_sparse = True
-        is_distribute = False
-
-        strategy = StrategyFactory.create_geo_strategy(5)
-
-        avg_cost, _, _, _ = train_network(batch_size, is_distribute, is_sparse)
-
-        optimizer = fluid.optimizer.SGD(0.1)
-        optimizer = fleet.distributed_optimizer(optimizer, strategy)
-        optimizer.minimize(avg_cost)
-
-        pserver_startup_program = fleet.startup_program
-        pserver_mian_program = fleet.main_program
+            "dist_fleet_simnet_bow.py", delta=1e-5, check_error_log=True)
 
 
 if __name__ == "__main__":
