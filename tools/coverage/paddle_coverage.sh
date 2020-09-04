88e07a8771dae5aa881ffab8eae5be63b91eeae9
XieYunshen <1084314248@qq.com>
2020-09-04 15:42:43 +0800

    resolve the issue of curl having same exit code with paddle build failed

--- tools/coverage/paddle_coverage.sh
+++ tools/coverage/paddle_coverage.sh
@@ -5,7 +5,10 @@ set -xe
 PADDLE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")/../../" && pwd )"
 
 # install lcov
-curl -o /lcov-1.14.tar.gz -x "" -s https://paddle-ci.gz.bcebos.com/coverage/lcov-1.14.tar.gz
+curl -o /lcov-1.14.tar.gz -x "" -s https://paddle-ci.gz.bcebos.com/coverage/lcov-1.14.tar.gz;curl_lcov_error=$?
+if [ "$curl_lcov_error" != 0 ];then
+    exit 10;
+fi
 tar -xf /lcov-1.14.tar.gz -C /
 cd /lcov-1.14
 make install
