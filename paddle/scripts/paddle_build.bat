5868416a4b75c621595ac7fd39972a66c47f56af
XieYunshen <1084314248@qq.com>
2020-09-07 10:34:30 +0800

    modified the value of retry times

--- paddle/scripts/paddle_build.bat
+++ paddle/scripts/paddle_build.bat
@@ -223,7 +223,7 @@ dir %THIRD_PARTY_PATH:/=\%\install\mkldnn\bin
 dir %THIRD_PARTY_PATH:/=\%\install\warpctc\bin
 
 set PATH=%THIRD_PARTY_PATH:/=\%\install\openblas\lib;%THIRD_PARTY_PATH:/=\%\install\openblas\bin;%THIRD_PARTY_PATH:/=\%\install\zlib\bin;%THIRD_PARTY_PATH:/=\%\install\mklml\lib;%THIRD_PARTY_PATH:/=\%\install\mkldnn\bin;%THIRD_PARTY_PATH:/=\%\install\warpctc\bin;%PATH%
-ctest.exe --output-on-failure -C Release -j 8 --repeat until-pass:3
+ctest.exe --output-on-failure -C Release -j 8 --repeat until-pass:4
 goto:eof
 
 :unit_test_error
