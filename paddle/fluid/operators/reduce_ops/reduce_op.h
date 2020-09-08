5c71a031a2f01b13fe553048ec1052f546a82ef9
Jack <136876878@qq.com>
2020-09-08 07:47:31 +0000

    remove mutable data

--- paddle/fluid/operators/reduce_ops/reduce_op.h
+++ paddle/fluid/operators/reduce_ops/reduce_op.h
@@ -118,13 +118,6 @@ void HandleLargeDimGrad(const framework::ExecutionContext& context,
   const int64_t reduced = x->numel() / unreduced;
   DDim out_dim(out->dims());
   DDim x_dim(x->dims());
-  framework::Tensor mutable_out;
-  framework::Tensor mutable_dout;
-  mutable_out.ShareDataWith(*out);
-  mutable_dout.ShareDataWith(*dout);
-  // reshape dOut, Out {unreduced}
-  mutable_dout.Resize({unreduced});
-  mutable_out.Resize({unreduced});
   // transpose and reshape X
   Tensor shuffled_x;
   GetShuffledInput<DeviceContext, T>(context, x, &shuffled_x, dims);
@@ -145,9 +138,6 @@ void HandleLargeDimGrad(const framework::ExecutionContext& context,
   math::TransposeNormal<DeviceContext, T> trans;
   trans(context.template device_context<DeviceContext>(), dx_tmp, dx,
         origin_axis);
-
-  mutable_dout.Resize(out_dim);
-  mutable_out.Resize(out_dim);
 }
 
 template <typename DeviceContext, typename T, typename Functor>
