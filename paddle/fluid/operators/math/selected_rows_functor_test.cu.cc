8faf4e828a2b5fb2b2de4a63a2e344f15efd1d17
Steffy-zxf <zhangxuefei@baidu.com>
2020-09-09 06:44:54 +0000

    fix coding style

--- paddle/fluid/operators/math/selected_rows_functor_test.cu.cc
+++ paddle/fluid/operators/math/selected_rows_functor_test.cu.cc
@@ -39,7 +39,7 @@ TEST(selected_rows_functor, gpu_add) {
       gpu_place);
   functor(ctx, in1_value, 1.0);
   PADDLE_ENFORCE_EQ(cudaDeviceSynchronize(), true,
-                    platform::errors::PreconditionNotMet(
+                    paddle::platform::errors::PreconditionNotMet(
                         "The all synchronization on the cuda is error!"));
 
   std::vector<int64_t> rows2{0, 5, 7, 9};
