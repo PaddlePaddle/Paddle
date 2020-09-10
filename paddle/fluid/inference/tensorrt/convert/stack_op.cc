f9b957c8ae78e671aa2eaabfac8d3b7d42ba37fd
Wang Huan <wanghuan29@baidu.com>
2020-09-10 02:52:30 +0000

    fix compilation error 3, test=develop

--- paddle/fluid/inference/tensorrt/convert/stack_op.cc
+++ paddle/fluid/inference/tensorrt/convert/stack_op.cc
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/inference/tensorrt/convert/op_converter.h"
+#include "paddle/fluid/inference/tensorrt/plugin/stack_op_plugin.h"
 
 namespace paddle {
 namespace inference {
