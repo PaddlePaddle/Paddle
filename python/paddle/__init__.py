2e59769612f6c9680cdfe89db6c56adc18693739
furnace <34057289+windstamp@users.noreply.github.com>
2020-09-11 09:02:04 +0800

    add empty op (c++, python, unit test) (#26659)

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -75,6 +75,7 @@ from .tensor.creation import full_like  #DEFINE_ALIAS
 from .tensor.creation import triu  #DEFINE_ALIAS
 from .tensor.creation import tril  #DEFINE_ALIAS
 from .tensor.creation import meshgrid  #DEFINE_ALIAS
+from .tensor.creation import empty  #DEFINE_ALIAS
 from .tensor.linalg import matmul  #DEFINE_ALIAS
 from .tensor.linalg import dot  #DEFINE_ALIAS
 # from .tensor.linalg import einsum        #DEFINE_ALIAS
