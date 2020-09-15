15ac6ab9fac7fe1554dd973815f6fc4e824e2516
chengmo <chengmo@baidu.com>
2020-09-15 16:24:13 +0800

    fix error report

--- paddle/fluid/operators/optimizers/dpsgd_op.h
+++ paddle/fluid/operators/optimizers/dpsgd_op.h
@@ -30,15 +30,17 @@ class DpsgdOpKernel : public framework::OpKernel<T> {
     PADDLE_ENFORCE_EQ(param_var->IsType<framework::LoDTensor>(), true,
                       "The Var(%s)'s type should be LoDTensor, "
                       "but the received is %s",
-                      ctx.InputNames("Param").front(),
-                      framework::ToTypeName(param_var->Type()));
+                      platform::errors::InvalidArgument(
+                          ctx.InputNames("Param").front(),
+                          framework::ToTypeName(param_var->Type())));
 
     const auto *grad_var = ctx.InputVar("Grad");
     PADDLE_ENFORCE_EQ(grad_var->IsType<framework::LoDTensor>(), true,
                       "The Var(%s)'s type should be LoDTensor, "
                       "but the received is %s",
-                      ctx.InputNames("Grad").front(),
-                      framework::ToTypeName(grad_var->Type()));
+                      platform::errors::InvalidArgument(
+                          ctx.InputNames("Grad").front(),
+                          framework::ToTypeName(grad_var->Type())));
 
     const auto *learning_rate = ctx.Input<framework::Tensor>("LearningRate");
 
