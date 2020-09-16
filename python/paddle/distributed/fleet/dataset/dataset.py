7c953b34f87cfc8ea40036aeacf905ee162547c4
yaoxuefeng6 <yaoxuefeng@baidu.com>
2020-09-16 16:40:09 +0800

    deprecated dataset api in fluid and mark static in new dataset api

--- python/paddle/distributed/fleet/dataset/dataset.py
+++ python/paddle/distributed/fleet/dataset/dataset.py
@@ -240,6 +240,8 @@ class DatasetBase(object):
 
 class InMemoryDataset(DatasetBase):
     """
+    :api_attr: Static Graph
+
     InMemoryDataset, it will load data into memory
     and shuffle data before training.
 
@@ -265,6 +267,8 @@ class InMemoryDataset(DatasetBase):
 
     def _init_distributed_settings(self, **kwargs):
         """
+        :api_attr: Static Graph
+
         should be called only once in user's python scripts to initialize distributed-related setings of dataset instance
         Args:
             kwargs: Keyword arguments. Currently, we support following keys in **kwargs:
@@ -323,6 +327,8 @@ class InMemoryDataset(DatasetBase):
 
     def update_settings(self, **kwargs):
         """
+        :api_attr: Static Graph
+
         should be called in user's python scripts to update setings of dataset instance
         Args:
             kwargs: Keyword arguments. Currently, we support following keys in **kwargs,
@@ -400,6 +406,8 @@ class InMemoryDataset(DatasetBase):
 
     def init(self, **kwargs):
         """
+        :api_attr: Static Graph
+
         should be called only once in user's python scripts to initialize setings of dataset instance
         Args:
             kwargs: Keyword arguments. Currently, we support following keys in **kwargs:
@@ -639,6 +647,8 @@ class InMemoryDataset(DatasetBase):
 
     def load_into_memory(self):
         """
+        :api_attr: Static Graph
+        
         Load data into memory
 
         Examples:
@@ -655,6 +665,8 @@ class InMemoryDataset(DatasetBase):
 
     def preload_into_memory(self, thread_num=None):
         """
+        :api_attr: Static Graph
+
         Load data into memory in async mode
 
         Args:
@@ -679,6 +691,8 @@ class InMemoryDataset(DatasetBase):
 
     def wait_preload_done(self):
         """
+        :api_attr: Static Graph
+
         Wait preload_into_memory done
 
         Examples:
@@ -696,6 +710,8 @@ class InMemoryDataset(DatasetBase):
 
     def local_shuffle(self):
         """
+        :api_attr: Static Graph
+
         Local shuffle
 
         Examples:
@@ -712,6 +728,8 @@ class InMemoryDataset(DatasetBase):
 
     def global_shuffle(self, fleet=None, thread_num=12):
         """
+        :api_attr: Static Graph
+
         Global shuffle.
         Global shuffle can be used only in distributed mode. i.e. multiple
         processes on single machine or multiple machines training together.
@@ -781,6 +799,8 @@ class InMemoryDataset(DatasetBase):
 
     def get_memory_data_size(self, fleet=None):
         """
+        :api_attr: Static Graph
+
         Get memory data size, user can call this function to know the num
         of ins in all workers after load into memory.
 
@@ -817,6 +837,8 @@ class InMemoryDataset(DatasetBase):
 
     def get_shuffle_data_size(self, fleet=None):
         """
+        :api_attr: Static Graph
+
         Get shuffle data size, user can call this function to know the num
         of ins in all workers after local/global shuffle.
 
@@ -901,6 +923,8 @@ class InMemoryDataset(DatasetBase):
 
 class QueueDataset(DatasetBase):
     """
+    :api_attr: Static Graph
+    
     QueueDataset, it will process data streamly.
 
     Examples:
@@ -920,6 +944,8 @@ class QueueDataset(DatasetBase):
 
     def init(self, **kwargs):
         """
+        :api_attr: Static Graph
+
         should be called only once in user's python scripts to initialize setings of dataset instance
         """
         super(QueueDataset, self).init(**kwargs)
