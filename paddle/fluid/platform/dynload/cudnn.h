02c10ca92bde5661aeae9a48bc35419263b08a4c
GaoWei8 <catherin.gao@foxmail.com>
2020-09-16 10:26:17 +0000

    polish codes

--- paddle/fluid/platform/dynload/cudnn.h
+++ paddle/fluid/platform/dynload/cudnn.h
@@ -181,16 +181,16 @@ CUDNN_DNN_ROUTINE_EACH_R7(DECLARE_DYNAMIC_LOAD_CUDNN_WRAP)
 #endif
 
 #if CUDNN_VERSION >= 7201
-#define CUDNN_DNN_ROUTINE_EACH_AFTER_R72(__macro) \
-  __macro(cudnnCreateRNNDataDescriptor);          \
-  __macro(cudnnDestroyRNNDataDescriptor);         \
-  __macro(cudnnSetRNNDataDescriptor);             \
-  __macro(cudnnRNNForwardTrainingEx);             \
-  __macro(cudnnSetRNNPaddingMode);                \
-  __macro(cudnnRNNBackwardDataEx);                \
-  __macro(cudnnRNNBackwardWeightsEx);             \
+#define CUDNN_DNN_ROUTINE_EACH_AFTER_TWO_R7(__macro) \
+  __macro(cudnnCreateRNNDataDescriptor);             \
+  __macro(cudnnDestroyRNNDataDescriptor);            \
+  __macro(cudnnSetRNNDataDescriptor);                \
+  __macro(cudnnSetRNNPaddingMode);                   \
+  __macro(cudnnRNNForwardTrainingEx);                \
+  __macro(cudnnRNNBackwardDataEx);                   \
+  __macro(cudnnRNNBackwardWeightsEx);                \
   __macro(cudnnRNNForwardInferenceEx);
-CUDNN_DNN_ROUTINE_EACH_AFTER_R72(DECLARE_DYNAMIC_LOAD_CUDNN_WRAP)
+CUDNN_DNN_ROUTINE_EACH_AFTER_TWO_R7(DECLARE_DYNAMIC_LOAD_CUDNN_WRAP)
 #endif
 
 #if CUDNN_VERSION >= 7401
