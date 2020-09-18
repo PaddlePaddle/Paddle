4283be5271c4e40693341e9661cd418ea6173ce6
Pei Yang <peiyang@baidu.com>
2020-09-18 13:39:56 +0800

    [cherry-pick][Paddle-TRT] Stack op plugin (#25605) (#27365)
    * [Paddle-TRT] Stack op plugin (#25605)
    
    * add stack_op to CMakeLists
    
    * add dim=3 support for scale op
    
    * add trt stack op, test=develop
    
    * remove debug message
    
    * add stack plugin serialize
    
    * remove slice, scale op, will add later
    
    * enhence error message
    
    * revise trt ernie test to conver the stack op CI testi, test=develop
    
    * add stack op serialization
    
    * fix test shape after adding stack op
    
    * remove slice op, will add after implementing serialization
    
    * roll back to min_graph=5 to avoid using slice op
    
    * fix scale op output layer
    
    * implement stack op createPlugin
    
    * use workspace and move the defination to .cu
    
    * move stack plugin creator definition to .cu, test=develop
    
    * sync ut with develop
    
    Co-authored-by: zlsh80826 <zlsh80826@gmail.com>

--- paddle/fluid/inference/tensorrt/engine.cc
+++ paddle/fluid/inference/tensorrt/engine.cc
@@ -186,6 +186,7 @@ void TensorRTEngine::FreezeNetwork() {
           Vec2TRT_Dims(optim_input_shape_[input.first], input.first, true));
     }
     infer_builder_config_->addOptimizationProfile(optim_profile_);
+    infer_builder_config_->setMaxWorkspaceSize(max_workspace_);
     if (WithFp16()) {
       infer_builder_config_->setFlag(nvinfer1::BuilderFlag::kFP16);
       if (disable_trt_plugin_fp16()) {
