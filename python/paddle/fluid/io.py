5c90f8b4f5c034548648b0628a2fb63b21e1c175
dengkaipeng <dengkaipeng@baidu.com>
2020-09-21 06:28:08 +0000

    remain fluid.io alias paddle.reader. test=develop

--- python/paddle/fluid/io.py
+++ python/paddle/fluid/io.py
@@ -31,6 +31,8 @@ from paddle.fluid.executor import Executor, global_scope
 from paddle.fluid.evaluator import Evaluator
 from paddle.fluid.framework import Program, Parameter, default_main_program, default_startup_program, Variable, \
     program_guard, dygraph_not_support
+from paddle.reader import cache, map_readers, buffered, compose, chain, shuffle, \
+    ComposeNotAligned, firstn, xmap_readers, multiprocess_reader
 from .wrapped_decorator import signature_safe_contextmanager
 from paddle.fluid.compiler import CompiledProgram
 from paddle.fluid.log_helper import get_logger
