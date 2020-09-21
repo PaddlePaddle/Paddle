ea2ad88f036d21fc492018076f81aaa771baedae
zhhsplendid <zhhsplendid@163.com>
2020-09-21 11:38:28 +0000

    Add function, test=develop

--- python/paddle/fluid/dygraph/dygraph_to_static/logical_transformer.py
+++ python/paddle/fluid/dygraph/dygraph_to_static/logical_transformer.py
@@ -67,7 +67,7 @@ class LogicalTransformer(gast.NodeTransformer):
             nodes = [pre_logic_node] + [post_logic_node]
 
         args = [ast_to_source_code(child) for child in nodes]
-        new_node_str = "fluid.dygraph.dygraph_to_static.convert_operators.convert_logical_{}(x={}, y={})".format(
+        new_node_str = "paddle.jit.dygraph_to_static.convert_operators.convert_logical_{}(x={}, y={})".format(
             api_type, args[0], args[1])
         # NOTE: gast.parse return Module(body=[expr(...)])
         new_node = gast.parse(new_node_str).body[0].value
