87e092f0d93f4d5386b78c2428d311499981f7d8
MRXLT <xlt2024@gmail.com>
2020-09-22 16:19:02 +0800

    update error info

--- paddle/fluid/operators/collective/c_allgather_op.cu.cc
+++ paddle/fluid/operators/collective/c_allgather_op.cu.cc
@@ -37,7 +37,10 @@ class CAllGatherOpCUDAKernel : public framework::OpKernel<T> {
     int rid = ctx.Attr<int>("ring_id");
     auto place = ctx.GetPlace();
     auto comm = platform::NCCLCommContext::Instance().Get(rid, place);
-    PADDLE_ENFORCE_EQ(nranks, comm->nranks());
+    PADDLE_ENFORCE_EQ(
+        nranks, comm->nranks(),
+        platform::errors::InvalidArgument("nranks: %s should equal to %s",
+                                          nranks, comm->nranks()));
 
     framework::DDim out_dims = in->dims();
     out_dims[0] *= nranks;
