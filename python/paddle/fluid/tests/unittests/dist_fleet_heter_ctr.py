27451c968e32197adade00dc85c0ec44cea7b87c
tangwei12 <tangwei12@baidu.com>
2020-09-23 14:26:37 +0800

    large scale kv speedup (#26510)
    * rename communicator meet->BatchesCounter
    
    * fix parame recv for sparse
    
    * geo sparse init from pserver
    
    * optimize init from pserver
    
    * add large scale optimizer fuse(SGD/ADAM)
    
    * rectification init_worker and exe.run startup program

--- python/paddle/fluid/tests/unittests/dist_fleet_heter_ctr.py
+++ python/paddle/fluid/tests/unittests/dist_fleet_heter_ctr.py
@@ -152,8 +152,9 @@ class TestHeterPsCTR2x2(FleetDistHeterRunnerBase):
         """
 
         exe = fluid.Executor(fluid.CPUPlace())
-        fleet.init_worker()
         exe.run(fluid.default_startup_program())
+        fleet.init_worker()
+
         batch_size = 4
         train_reader = paddle.batch(fake_ctr_reader(), batch_size=batch_size)
         self.reader.decorate_sample_list_generator(train_reader)
@@ -176,8 +177,8 @@ class TestHeterPsCTR2x2(FleetDistHeterRunnerBase):
 
         exe = fluid.Executor(fluid.CPUPlace())
 
-        fleet.init_worker()
         exe.run(fluid.default_startup_program())
+        fleet.init_worker()
 
         thread_num = int(os.getenv("CPU_NUM", 2))
         batch_size = 128
