b80f73b1dde1f6d9684fa402f8c3ac6f2ec473d6
LielinJiang <jianglielin@baidu.com>
2020-09-25 07:35:25 +0000

    add visualdl callback

--- python/paddle/hapi/callbacks.py
+++ python/paddle/hapi/callbacks.py
@@ -15,6 +15,7 @@
 import os
 
 from paddle.fluid.dygraph.parallel import ParallelEnv
+from paddle.utils import try_import
 
 from .progressbar import ProgressBar
 
@@ -469,3 +470,111 @@ class ModelCheckpoint(Callback):
             path = '{}/final'.format(self.save_dir)
             print('save checkpoint at {}'.format(os.path.abspath(path)))
             self.model.save(path)
+
+
+class VisualDL(Callback):
+    """VisualDL callback function
+    Args:
+        log_dir (str): The directory to save visualdl log file.
+
+    Examples:
+        .. code-block:: python
+
+            import paddle
+            from paddle.static import InputSpec
+
+            inputs = [InputSpec([-1, 1, 28, 28], 'float32', 'image')]
+            labels = [InputSpec([None, 1], 'int64', 'label')]
+
+            train_dataset = paddle.vision.datasets.MNIST(mode='train')
+            eval_dataset = paddle.vision.datasets.MNIST(mode='test')
+
+            net = paddle.vision.LeNet()
+            model = paddle.Model(net, inputs, labels)
+
+            optim = paddle.optimizer.Adam(0.001, parameters=net.parameters())
+            model.prepare(optimizer=optim,
+                        loss=paddle.nn.CrossEntropyLoss(),
+                        metrics=paddle.metric.Accuracy())
+
+            callback = paddle.callbacks.VisualDL(log_dir='visualdl_log_dir')
+            model.fit(train_dataset, eval_dataset, batch_size=64, callbacks=callback)
+            
+    """
+
+    def __init__(self, log_dir):
+        self.log_dir = log_dir
+        self.epochs = None
+        self.steps = None
+
+    def _is_write(self):
+        return ParallelEnv().local_rank == 0
+
+    def on_train_begin(self, logs=None):
+        self.epochs = self.params['epochs']
+        assert self.epochs
+        self.train_metrics = self.params['metrics']
+        assert self.train_metrics
+        self._is_fit = True
+
+    def on_epoch_begin(self, epoch=None, logs=None):
+        visualdl = try_import('visualdl')
+        self.steps = self.params['steps']
+        self.epoch = epoch
+        self.train_step = 0
+        self.train_writer = visualdl.LogWriter(self.log_dir)
+
+    def _updates(self, logs, mode):
+        metrics = getattr(self, '%s_metrics' % (mode))
+        writer = getattr(self, '%s_writer' % (mode))
+        current_step = getattr(self, '%s_step' % (mode))
+        if mode == 'train':
+            total_step = self.epoch * self.steps + current_step
+        else:
+            total_step = self.epoch
+
+        for k in metrics:
+            if k in logs:
+                temp_tag = mode + '/' + k
+
+                if isinstance(logs[k], (list, tuple)):
+                    temp_value = logs[k][0]
+                elif isinstance(logs[k], numbers.Number):
+                    temp_value = logs[k]
+                else:
+                    continue
+                writer.add_scalar(
+                    tag=temp_tag, step=total_step, value=temp_value)
+
+    def on_train_batch_end(self, step, logs=None):
+        logs = logs or {}
+        self.train_step += 1
+
+        if self._is_write():
+            if self.steps is None or self.train_step < self.steps:
+                self._updates(logs, 'train')
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        if self._is_write() and (self.steps is not None):
+            self._updates(logs, 'train')
+
+    def on_eval_begin(self, logs=None):
+        visualdl = try_import('visualdl')
+        self.eval_steps = logs.get('steps', None)
+        self.eval_metrics = logs.get('metrics', [])
+        self.eval_step = 0
+        self.evaled_samples = 0
+        self.eval_writer = visualdl.LogWriter(self.log_dir)
+
+    def on_train_end(self, logs=None):
+        if hasattr(self, 'train_writer'):
+            self.train_writer.close()
+        if hasattr(self, 'eval_writer'):
+            self.eval_writer.close()
+
+    def on_eval_end(self, logs=None):
+        self._updates(logs, 'eval')
+
+        if (not hasattr(self, '_is_fit')) and hasattr(self, 'eval_writer'):
+            self.eval_writer.close()
