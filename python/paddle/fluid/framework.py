b0ee1405f74e0c598f84694f91cfd331a1ab10ca
joanna.wozna.intel <joanna.wozna@intel.com>
2020-09-26 07:32:39 +0200

    Add conv2d bfloat16 support (#27325)

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -613,7 +613,9 @@ def convert_np_dtype_to_dtype_(np_dtype):
     elif dtype == np.bool:
         return core.VarDesc.VarType.BOOL
     elif dtype == np.uint16:
-        return core.VarDesc.VarType.INT16
+        # since there is still no support for bfloat16 in NumPy,
+        # uint16 is used for casting bfloat16
+        return core.VarDesc.VarType.BF16
     elif dtype == np.uint8:
         return core.VarDesc.VarType.UINT8
     elif dtype == np.int8:
