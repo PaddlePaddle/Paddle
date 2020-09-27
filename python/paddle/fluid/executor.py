0dfc404ec8b0272740a035ad1dd6f0b5afc19afd
jiweibo <jiweibo@baidu.com>
2020-09-27 06:12:22 +0000

    update.

--- python/paddle/fluid/executor.py
+++ python/paddle/fluid/executor.py
@@ -99,7 +99,7 @@ def scope_guard(scope):
 
             new_scope = paddle.static.Scope()
             with paddle.static.scope_guard(new_scope):
-                 paddle.static.global_scope().var("data").get_tensor().set(paddle.ones((2, 2)), paddle.CPUPlace())
+                 paddle.static.global_scope().var("data").get_tensor().set(numpy.ones((2, 2)), paddle.CPUPlace())
             numpy.array(new_scope.find_var("data").get_tensor())
     """
 
