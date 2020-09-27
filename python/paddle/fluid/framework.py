becf7dfb8193faccc96e4091f8ff57858a12abc8
Aurelius84 <zhangliujie@baidu.com>
2020-09-27 05:56:46 +0000

    fix indent

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -5415,11 +5415,11 @@ def program_guard(main_program, startup_program=None):
 
           import paddle
 
-           paddle.enable_static()
-           main_program = paddle.static.Program()
-           # does not care about startup program. Just pass a temporary value.
-           with paddle.static.program_guard(main_program, paddle.static.Program()):
-               data = paddle.static.data(name='image', shape=[None, 784, 784], dtype='float32')
+          paddle.enable_static()
+          main_program = paddle.static.Program()
+          # does not care about startup program. Just pass a temporary value.
+          with paddle.static.program_guard(main_program, paddle.static.Program()):
+              data = paddle.static.data(name='image', shape=[None, 784, 784], dtype='float32')
     
     """
     from .data_feeder import check_type
