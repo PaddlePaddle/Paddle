f48f8d9bd62e6fe9ec5a0181f5813b7b0f103364
Wang Huan <wanghuan29@baidu.com>
2020-09-28 01:10:15 +0000

    delete CPUPlace and XPUPlace, test=develop

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -1663,13 +1663,7 @@ All parameter, weight, gradient are variables in Paddle.
   m.def("is_compiled_with_mkldnn", IsCompiledWithMKLDNN);
   m.def("is_compiled_with_brpc", IsCompiledWithBrpc);
   m.def("is_compiled_with_dist", IsCompiledWithDIST);
-  m.def("_cuda_synchronize", [](const platform::XPUPlace &place) {
-    platform::DeviceContextPool::Instance().Get(place)->Wait();
-  });
   m.def("_cuda_synchronize", [](const platform::CUDAPlace &place) {
-    platform::DeviceContextPool::Instance().Get(place)->Wait();
-  });
-  m.def("_cuda_synchronize", [](const platform::CPUPlace &place) {
     platform::DeviceContextPool::Instance().Get(place)->Wait();
   });
 
