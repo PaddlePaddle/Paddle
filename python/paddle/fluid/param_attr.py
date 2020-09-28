3ddf3eb44ee37f7a20b840e44fa7a9e5ec95d55f
jiweibo <jiweibo@baidu.com>
2020-09-28 12:43:57 +0000

    update for dynamic demo.

--- python/paddle/fluid/param_attr.py
+++ python/paddle/fluid/param_attr.py
@@ -64,13 +64,12 @@ class ParamAttr(object):
             import paddle
             paddle.enable_static()
 
-            w_param_attrs = paddle.ParamAttr(name="fc_weight",
-                                             learning_rate=0.5,
-                                             regularizer=paddle.regularizer.L2Decay(1.0),
-                                             trainable=True)
-            print(w_param_attrs.name) # "fc_weight"
-            x = paddle.static.data(name='X', shape=[None, 1], dtype='float32')
-            y_predict = paddle.static.nn.fc(input=x, size=10, param_attr=w_param_attrs)
+            weight_attr = paddle.ParamAttr(name="weight",
+                                           learning_rate=0.5,
+                                           regularizer=paddle.regularizer.L2Decay(1.0),
+                                           trainable=True)
+            print(weight_attr.name) # "weight"
+            paddle.nn.Linear(3, 4, weight_attr=weight_attr)
     """
 
     def __init__(self,
