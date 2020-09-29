97df129dc2306c1024667147f2ac30ccfe21668b
Wang Huan <wanghuan29@baidu.com>
2020-09-29 05:32:42 +0000

    refine, test=develop

--- python/paddle/fluid/dygraph/layers.py
+++ python/paddle/fluid/dygraph/layers.py
@@ -481,7 +481,7 @@ class Layer(core.Layer):
 
                 layer_list = list(model.children())
 
-                print(layer_list)
+                print(layer_list)   # [<paddle.nn.layer.common.Linear object at 0x7f7b8113f830>, <paddle.nn.layer.common.Linear object at 0x7f7b8113f950>]
 
         """
         for _, layer in self.named_children():
@@ -499,11 +499,13 @@ class Layer(core.Layer):
 
                 import paddle
 
-                fc1 = paddle.nn.Linear(10, 3)
-                fc2 = paddle.nn.Linear(3, 10, bias_attr=False)
-                model = paddle.nn.Sequential(fc1, fc2)
+                linear1 = paddle.nn.Linear(10, 3)
+                linear2 = paddle.nn.Linear(3, 10, bias_attr=False)
+                model = paddle.nn.Sequential(linear1, linear2)
                 for prefix, layer in model.named_children():
                     print(prefix, layer)
+                    # ('0', <paddle.nn.layer.common.Linear object at 0x7fb61ed85830>)
+                    # ('1', <paddle.nn.layer.common.Linear object at 0x7fb61ed85950>)
 
         """
         memo = set()
