b7972d55a050fa0127243353d79beb4c077b685b
ysh329@sina.com <ysh329@sina.com>
2020-09-29 02:11:57 +0000

    test=develop

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -12414,8 +12414,10 @@ def clip_by_norm(x, max_norm, name=None):
         .. code-block:: python
 
             import paddle
-            input = paddle.data(
-                name='data', shape=[None, 1], dtype='float32')
+            import numpy as np
+
+            paddle.disable_static()
+            input = paddle.to_tensor(data=np.array([[0.1, 0.2], [0.3, 0.4]]), dtype="float32")
             reward = paddle.nn.clip_by_norm(x=input, max_norm=1.0)
     """
 
