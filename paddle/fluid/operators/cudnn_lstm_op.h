56b6b6f154b3e858587fe37ca790bb4ccab06ad8
wawltor <fangzeyang0904@hotmail.com>
2020-09-30 08:58:36 +0000

    temp update

--- paddle/fluid/operators/cudnn_lstm_op.h
+++ paddle/fluid/operators/cudnn_lstm_op.h
@@ -31,160 +31,6 @@ using Tensor = framework::Tensor;
 using TensorList = std::vector<framework::Tensor>;
 
 template <typename T>
-struct Cell {
-  virtual ~Cell() {}
-  // virtual void operator()(const Tensor* input, const TensorList& vec,
-  //                        const Tensor* init_h, const Tensor* init_c,
-  //                        Tensor* last_h, Tensor* last_c, Tensor* output,
-  //                        const int& layer_idx, const int& init_offset,
-  //                        const int& time_step) {}
-  virtual void operator()(const platform::CPUDeviceContext* device_ctx,
-                          Tensor* input, const Tensor* weight_hh,
-                          const Tensor* bias_hh, const Tensor* init_h,
-                          const Tensor* init_c, Tensor* last_h, Tensor* last_c,
-                          Tensor* output) {}
-};
-
-template <typename T>
-struct LSTMCell : Cell<T> {
-  void operator()(const platform::CPUDeviceContext* device_ctx, Tensor* input,
-                  const Tensor* weight_hh, const Tensor* bias_hh,
-                  const Tensor* init_h, Tensor* init_c, Tensor* last_h,
-                  Tensor* last_c, Tensor* output) {
-    // input + w_hh * h_{t-1} + b_hh
-    // auto blas = math::GetBlas<DeviceContext, T>(device_ctx);
-    // blas.MatMul(init_h, false, weight_hh, false, static_cast<T>(1.0), &input,
-    //             static_cast<T>(1.0));
-    math::FCFunctor<platform::CPUDeviceContext, T> fc;
-    fc(device_ctx, init_h->dims()[0], weight_hh->dims()[1],
-       weight_hh->dims()[0], init_h->data<T>(), weight_hh->data<T>(),
-       input->data<T>(), bias_hh->data<T>());
-
-    math::LstmMetaValue<T> lstm_value;
-    lstm_value.check_ig = nullptr;
-    lstm_value.check_fg = nullptr;
-    lstm_value.check_og = nullptr;
-
-    auto gate_act = math::detail::GetActivationType("sigmoid");
-    auto cell_act = math::detail::GetActivationType("tanh");
-    auto cand_act = math::detail::GetActivationType("tanh ");
-
-    size_t frame_size = init_h->dims()[1];
-    size_t batch_size = init_h->dims()[0];
-
-    Tensor cell_pre_act;
-    cell_pre_act.mutable_data<T>(init_h->dims(), device_ctx->GetPlace());
-
-    lstm_value.prev_state_value = init_c->data<T>();
-    lstm_value.gate_value = input->data<T>();
-    lstm_value.output_value = output->data<T>();
-    lstm_value.state_value = last_c->data<T>();
-    lstm_value.state_active_value = cell_pre_act.data<T>();
-    T cell_clip = 0.0;
-    math::LstmUnitFunctor<platform::CPUDeviceContext, T>::compute(
-        &device_ctx, lstm_value, frame_size, batch_size, cell_clip, gate_act,
-        cell_act, cand_act);
-  }
-};
-
-// input_w = input * w_ih + b_ih
-// input_tensors = unbind(input_w)
-// input_tensors[i], w_hh, b_hh
-// init_h N*D
-
-template <typename T>
-struct Layer {
-  virtual ~Layer() {}
-  Tensor preprocess(const framework::ExecutionContext& context,
-                    const Tensor* input, const TensorList& vec,
-                    const int& hidden_size, const int& gate_num,
-                    const int& layer_idx) {
-    const TensorList& parameter = vec[layer_idx];
-    // crate the temp input for the X * W_ih^T + Bias_ih
-    Tensor cache_input;
-    cache_input.Resize(
-        framework::make_ddim{input->dims[0], input->dims[1], hidden_size});
-    cache_input.mutable_data<T>(ctx.GetPlace());
-    const auto& weight = parameter[0];
-    const auto& bias = parameter[1];
-    auto blas = math::GetBlas<DeviceContext, T>(device_ctx);
-    auto mat_dim_a = math::CreateMatrixDescriptor(
-        RowMatrixFromVector(cache.dims()), 0, false);
-    auto mat_dim_b =
-        math::CreateMatrixDescriptor(ColumnMatrixFromVector(y.dims()), 0, true);
-    mat_dim_a.height_ *= mat_dim_a.batch_size_;
-    mat_dim_a.batch_size_ = 0;
-    blas.MatMul(*input, mat_dim_a, weight, mat_dim_b, static_cast<T>(1.0),
-                &cache_input, T(0));
-
-    return cache_input;
-  }
-  virtual void operator()(Tensor* input) const = 0;
-};
-
-template <typename T>
-struct SingleLayer {
-  explicit SingleLayer(Cell<T>& cell) : cell_(cell) {}
-  void operator()(const framework::ExecutionContext& context,
-                  const Tensor* input, const TensorList& vec,
-                  const Tensor* init_h, const Tensor* init_c, Tensor* last_h,
-                  Tensor* last_c, Tensor* output, const int& layer_idx,
-                  const int& gate_num) {
-    // first step, we could calcalute the W_ih * input + Bias_ih to more faster
-    const int& hidden_size = init_h->dims()[2];
-    const int& time_step = input->dims()[0];
-    Tensor input_w;
-    auto dims = input->dims();
-    dims[0] = vec[0].dims()[0];
-    input_w.mutable_data<T>(dims, context.GetPlace());
-    // TensorList output_tensors;
-    // output_tensors.reserve(time_step);
-
-    TensorList step_hiddens;
-    // TensorList step_cells
-    // input_w = input * w_hi + b_hi
-    // or input_w = input * w_hi + b_hi + b_hh
-    auto& dev_ctx =
-        context.template device_context<platform::CPUDeviceContext>();
-    // auto blas = math::GetBlas<platform::CPUDeviceContext, T>(dev_ctx);
-    math::SetConstant<platform::CPUDeviceContext, T> set_zero;
-    set_zero(dev_ctx, &input_w, static_cast<T>(0));
-
-    auto weight_hi = vec[0];
-    auto bias_hi = vec[2];
-    math::FCFunctor<platform::CPUDeviceContext, T> fc;
-    fc(dev_ctx, input->dims()[0], weight_hi.dims()[1], weight_hi.dims()[0],
-       input->data<T>(), weight_hi.data<T>(), input_w.data<T>(),
-       bias_hi.data<T>());
-
-    auto input_tensors = Unbind(input_w);
-    for (int i = 0; i < time_step; i++) {
-      cell_(&dev_ctx, &input_tensors[i], &vec[1], &vec[3], init_h, init_c,
-            last_h, last_c, output);
-      // step_hiddens.emplace_back(last_h);
-      // step_cells.emplace_back(last_c);
-      init_h = last_h;
-      init_c = last_c;
-    }
-    // TODO(wawltor)
-  }
-
-  // Cell for the rnn module
-  Cell<T> cell_;
-};
-
-template <typename T>
-struct BidirLayer {
-  explicit BidirLayer(Cell<T>& cell) : cell_(cell) {}
-  void operator()(const framework::ExecutionContext& context,
-                  const Tensor* input, const TensorList& vec,
-                  const Tensor* init_h, const Tensor* init_c, Tensor* last_h,
-                  Tensor* last_c, Tensor* output, const int& layer_idx,
-                  const int& gate_num) {}
-  Cell<T> cell_;
-};
-
-template <typename T>
 void dropout_cpu_function_inplace(const framework::ExecutionContext& context,
                                   Tensor* x, Tensor* mask,
                                   const float& dropout_prob,
@@ -309,17 +155,167 @@ std::vector<TensorList> parameter_split(
   return params_vec;
 }
 
-template <typename CellType, template <typename> class LayerT,
+template <typename T>
+struct Cell {
+  virtual ~Cell() {}
+  virtual void operator()(const platform::CPUDeviceContext* device_ctx,
+                          Tensor* input, const Tensor* weight_hh,
+                          const Tensor* bias_hh, const Tensor* init_h,
+                          const Tensor* init_c, Tensor* last_h, Tensor* last_c,
+                          Tensor* output) {}
+};
+
+template <typename T>
+struct LSTMCell : Cell<T> {
+  void operator()(const platform::CPUDeviceContext* device_ctx, Tensor* input,
+                  const Tensor* weight_hh, const Tensor* bias_hh,
+                  const Tensor* init_h, Tensor* init_c, Tensor* last_h,
+                  Tensor* last_c, Tensor* output) {
+    math::FCFunctor<platform::CPUDeviceContext, T> fc;
+    fc(device_ctx, init_h->dims()[0], weight_hh->dims()[1],
+       weight_hh->dims()[0], init_h->data<T>(), weight_hh->data<T>(),
+       input->data<T>(), bias_hh->data<T>());
+
+    math::LstmMetaValue<T> lstm_value;
+    lstm_value.check_ig = nullptr;
+    lstm_value.check_fg = nullptr;
+    lstm_value.check_og = nullptr;
+
+    auto gate_act = math::detail::GetActivationType("sigmoid");
+    auto cell_act = math::detail::GetActivationType("tanh");
+    auto cand_act = math::detail::GetActivationType("tanh ");
+
+    size_t frame_size = init_h->dims()[1];
+    size_t batch_size = init_h->dims()[0];
+
+    Tensor cell_pre_act;
+    cell_pre_act.mutable_data<T>(init_h->dims(), device_ctx->GetPlace());
+
+    lstm_value.prev_state_value = init_c->data<T>();
+    lstm_value.gate_value = input->data<T>();
+    lstm_value.output_value = output->data<T>();
+    lstm_value.state_value = last_c->data<T>();
+    lstm_value.state_active_value = cell_pre_act.data<T>();
+    T cell_clip = 0.0;
+    math::LstmUnitFunctor<platform::CPUDeviceContext, T>::compute(
+        &device_ctx, lstm_value, frame_size, batch_size, cell_clip, gate_act,
+        cell_act, cand_act);
+  }
+};
+
+template <typename T>
+struct Layer {
+  virtual ~Layer() {}
+  Tensor preprocess(const framework::ExecutionContext& context,
+                    const Tensor* input, const Tensor& weight,
+                    const Tensor& bias) {
+    // crate the temp input for the X * W_ih^T + Bias_ih
+    auto& dev_ctx =
+        context.template device_context<platform::CPUDeviceContext>();
+    const int& hidden_size = weight.dims()[0];
+    Tensor cache_input;
+    cache_input.Resize(framework::make_ddim(
+        {input->dims()[0], input->dims()[1], hidden_size}));
+    cache_input.mutable_data<T>(context.GetPlace());
+    auto blas = math::GetBlas<platform::CPUDeviceContext, T>(dev_ctx);
+    auto mat_dim_a = math::CreateMatrixDescriptor(
+        RowMatrixFromVector(cache_input.dims()), 0, false);
+    auto mat_dim_b = math::CreateMatrixDescriptor(
+        ColumnMatrixFromVector(weight.dims()), 0, true);
+    // convert the batch matmul to matmul, this operator could be speed faster
+    mat_dim_a.height_ *= mat_dim_a.batch_size_;
+    mat_dim_a.batch_size_ = 0;
+    blas.MatMul(*input, mat_dim_a, weight, mat_dim_b, static_cast<T>(1.0),
+                &cache_input, T(0));
+
+    auto eigen_in = framework::EigenMatrix<T>::Reshape(
+        cache_input, cache_input.dims().size() - 1);
+    auto eigen_bias = framework::EigenVector<T>::Flatten(bias);
+    // use the eigen faster the add
+    const int& row_num =
+        framework::product(cache_input.dims()) / cache_input.dims()[2];
+    for (int64_t i = 0; i < row_num; ++i) {
+      eigen_in.chip(i, 0) = eigen_in.chip(i, 0) + eigen_bias;
+    }
+
+    return cache_input;
+  }
+  virtual void operator()(const framework::ExecutionContext& context,
+                          const Tensor* input, const TensorList& vec,
+                          const Tensor* init_h, const Tensor* init_c,
+                          Tensor* last_h, Tensor* last_c, Tensor* output,
+                          const int& layer_idx, const int& gate_num) const = 0;
+};
+
+template <typename T>
+struct SingleLayer : Layer<T> {
+  explicit SingleLayer(Cell<T>& cell) : cell_(cell) {}
+  void operator()(const framework::ExecutionContext& context,
+                  const Tensor* input, const TensorList& vec,
+                  const Tensor* init_h, const Tensor* init_c, Tensor* last_h,
+                  Tensor* last_c, Tensor* output, const int& layer_idx,
+                  const int& gate_num) {
+    auto& dev_ctx =
+        context.template device_context<platform::CPUDeviceContext>();
+    // first step, we could calcalute the W_ih * input + Bias_ih to more faster
+    const int& hidden_size = init_h->dims()[2];
+    const int& time_step = input->dims()[0];
+    // vec[0] is parameter of w_hi, vec[2] is bias of b_hi
+    const Tensor& input_w = preprocess<T>(context, input, vec[0], vec[2]);
+    auto input_tensors = Unbind(input_w);
+    for (int i = 0; i < time_step; i++) {
+      cell_(&dev_ctx, &input_tensors[i], &vec[1], &vec[3], init_h, init_c,
+            last_h, last_c, output);
+      init_h = last_h;
+      init_c = last_c;
+    }
+  }
+
+  // Cell for the rnn module
+  Cell<T> cell_;
+};
+
+template <typename T>
+struct BidirLayer : Layer<T> {
+  explicit BidirLayer(Cell<T>& cell) : cell_(cell) {}
+  void operator()(const framework::ExecutionContext& context,
+                  const Tensor* input, const TensorList& vec,
+                  const Tensor* init_h, const Tensor* init_c, Tensor* last_h,
+                  Tensor* last_c, Tensor* output, const int& layer_idx,
+                  const int& gate_num) {
+    auto& dev_ctx =
+        context.template device_context<platform::CPUDeviceContext>();
+    // first step, calculate the fw_ih * input + fb_ih to more faster
+    const int& time_step = input->dims()[0];
+    const Tensor& forward_input_w =
+        preprocess<T>(context, input, vec[0], vec[2]);
+    auto input_tensors = Unbind(forward_input_w);
+    for (int i = 0; i < time_step; i++) {
+      cell_(&dev_ctx, &input_tensors[i], &vec[1], &vec[3], init_h, init_c,
+            last_h, last_c, output);
+    }
+    // second step, we calcluate the bw_ih * reverse_input + bw_ih
+    const Tensor& backward_input_w = preprocess(context, input, vec[4], vec[6]);
+    auto backward_input_tensors = Unbind(backward_input_w);
+    std::reverse(backward_input_tensors.begin(), backward_input_tensors.end());
+    for (int i = 0; i < time_step; i++) {
+      cell_(&dev_ctx, &backward_input_tensors[i], &vec[1], &vec[3], init_h,
+            init_c, last_h, last_c, output);
+    }
+  }
+
+  Cell<T> cell_;
+};
+
+template <typename CellType, template <typename> class SingleLayerT,
           template <typename> class BidirLayerT, typename T>
-void CacluateLSTMLayer(const framework::ExecutionContext& ctx,
-                       const Tensor* input, const Tensor* weight,
-                       const Tensor* init_h, const Tensor* init_c,
-                       Tensor* last_h, Tensor* last_c, Tensor* output,
-                       Tensor* dropout_mask, const int& num_layers,
-                       const int& gate_num, const int& input_size,
-                       const int& hidden_size, const bool& is_bidirec,
-                       const std::string& cell_type, const float& dropout_prob,
-                       const bool& is_test, const int& seed) {
+void RnnFunc(const framework::ExecutionContext& ctx, const Tensor* input,
+             const Tensor* weight, const Tensor* init_h, const Tensor* init_c,
+             Tensor* last_h, Tensor* last_c, Tensor* output,
+             Tensor* dropout_mask, const int& num_layers, const int& gate_num,
+             const int& input_size, const int& hidden_size,
+             const bool& is_bidirec, const std::string& cell_type,
+             const float& dropout_prob, const bool& is_test, const int& seed) {
   // check the dim message of init state
   const auto& init_h_dims = init_h->dims();
   const auto& init_c_dims = init_c->dims();
@@ -384,15 +380,15 @@ void CacluateLSTMLayer(const framework::ExecutionContext& ctx,
               last_c, output_holder, i, gate_num);
       }
     } else {
-      LayerT<T> layer(cell);
+      SingleLayerT<T> layer(cell);
       if (i == 0) {
         layer(ctx, input, parameter_lists[i], &init_h_unbind[i],
               &init_c_unbind[i], &last_h_unbind[i], &last_c_unbind[i],
-              output_holder, i, init_offset);
+              output_holder, i, gate_num);
       } else {
         layer(ctx, input_holder, parameter_lists[i], &init_h_unbind[i],
               &init_c_unbind[i], &last_h_unbind[i], &last_c_unbind[i],
-              output_holder, i, init_offset);
+              output_holder, i, gate_num);
       }
     }
   }
@@ -428,9 +424,10 @@ class CudnnLSTMCPUKernel : public framework::OpKernel<T> {
 
     // init the output and allocate the memory
     output->mutable_data<T>(ctx.GetPlace());
-    // dropout_mask->mutable_data<T>(ctx.GetPlace());
+    last_h->mutable_data<T>(ctx.GetPlace());
+    last_c->mutable_data<T>(ctx.GetPlace());
     if (cell_type == "lstm") {
-      CacluateLSTMLayer<LSTMCell<T>, SingleLayer, BidirLayer, T>(
+      RnnFunc<LSTMCell<T>, SingleLayer, BidirLayer, T>(
           ctx, input, weight, init_h, init_c, last_h, last_c, output,
           dropout_mask, num_layers, 4, input_size, hidden_size, is_bidirec,
           cell_type, dropout_prob, is_test, seed);
