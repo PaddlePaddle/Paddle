abb6dafba4e43f97b80aa452637e27923287d08e
Jacek Czaja <jacek.czaja@intel.com>
2020-10-06 10:25:56 +0200

    - antoher compilation fix
    test=develop

--- paddle/fluid/operators/mkldnn/pool_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/pool_mkldnn_op.cc
@@ -126,7 +126,7 @@ class PoolMKLDNNGradOpKernel : public paddle::framework::OpKernel<T> {
     UpdatePadding(&paddings, global_pooling, 0, padding_algorithm, data_dims,
                   strides, ksize);
 
-    platform::PoolingMKLDNNHandler::ComputeAdaptivePoolParameters(
+    platform::PoolingMKLDNNHandler<T>::ComputeAdaptivePoolParameters(
         ctx, paddle::framework::vectorize(in_x->dims()), ksize, strides);
 
     auto& dev_ctx =
