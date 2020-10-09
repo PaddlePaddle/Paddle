ba074659c816b32c35c3a30242857637c9b1b7d0
mapingshuo <mps2012@yeah.net>
2020-10-09 13:03:23 +0800

    correct _var_to_use_op, zero-0.5.1

--- python/paddle/distributed/fleet/meta_optimizers/zero_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/zero_optimizer.py
@@ -60,7 +60,7 @@ class ProgramDeps(object):
         # vars where to stop to build the deps
         self._end_vars = end_vars
         # var name -> op idxs which depends on this var
-        self._var_deps = {}
+        self._var_to_use_op = {}
         # sub block deps which is a subset of this topo
         self._sub_block_deps = {}
         # var name -> op idxs which generate var
@@ -76,15 +76,15 @@ class ProgramDeps(object):
             return None
 
     def get_var_deps(self, var_name):
-        if var_name in self._var_deps:
-            return self._var_deps[var_name]
+        if var_name in self._var_to_use_op:
+            return self._var_to_use_op[var_name]
         else:
             return None
 
     def _build_deps(self, ):
         for var_name in self._start_vars:
-            self._var_deps[var_name] = [-1]
-            self._var_to_generate_op[var_name] = [-1]
+            self._var_to_use_op[var_name] = []
+            self._var_to_generate_op[var_name] = []
 
         for idx, op in enumerate(self._block.ops):
             if op.type in [
@@ -96,40 +96,48 @@ class ProgramDeps(object):
             output_vars = op.desc.output_arg_names()
             deps_reduce = False
             for input_name in input_vars:
-                if input_name in self._var_deps:
+                if input_name in self._var_to_use_op:
                     deps_reduce = True
-            if deps_reduce:
-                for input_name in input_vars:
-                    if input_name in self._var_deps:
-                        self._var_deps[input_name].append(idx)
-                for output_name in output_vars:
-                    self._var_deps[output_name] = []
-                    if output_name not in self._var_to_generate_op:
-                        self._var_to_generate_op[output_name] = [idx]
-                    else:
-                        self._var_to_generate_op[output_name].append(idx)
-                if op.type == "conditional_block":
-                    # subblock
-                    assert (op.desc.has_attr("sub_block"))
-                    subblock_idx = op.desc.attr("sub_block").id
-                    subblock_deps = ProgramDeps(
-                        self._block.program.block(subblock_idx),
-                        op.desc.input_arg_names(), op.desc.output_arg_names())
-                    self._sub_block_deps[subblock_idx] = subblock_deps
-                    subblock_deps._father_block_deps = self
+            if not deps_reduce:
+                continue
+            for input_name in input_vars:
+                if input_name in self._var_to_use_op:
+                    self._var_to_use_op[input_name].append(idx)
+            for output_name in output_vars:
+                if output_name not in self._var_to_use_op:
+                    self._var_to_use_op[output_name] = []
+                if output_name not in self._var_to_generate_op:
+                    self._var_to_generate_op[output_name] = [idx]
+                else:
+                    self._var_to_generate_op[output_name].append(idx)
+            if op.type == "conditional_block":
+                # subblock
+                assert (op.desc.has_attr("sub_block"))
+                subblock_idx = op.desc.attr("sub_block").id
+                subblock_deps = ProgramDeps(
+                    self._block.program.block(subblock_idx),
+                    op.desc.input_arg_names(), op.desc.output_arg_names())
+                self._sub_block_deps[subblock_idx] = subblock_deps
+                subblock_deps._father_block_deps = self
 
     def crop_input_var_from_op(self, op_idx, var_name):
-        if var_name in self._var_deps:
+        if var_name in self._var_to_use_op:
             # update var -> dep_var_op
-            if self._var_deps[var_name] != []:
-                assert (op_idx in self._var_deps[var_name])
-                self._var_deps[var_name].remove(op_idx)
+            if self._var_to_use_op[var_name] != []:
+                if op_idx not in self._var_to_use_op[var_name]:
+                    raise ValueError(
+                        "op_idx: {} is not in self._var_to_use_op[{}], "
+                        "self._var_to_use_op[{}] is {}".format(
+                            op_idx, var_name, var_name, self._var_to_use_op[
+                                var_name]))
+                self._var_to_use_op[var_name].remove(op_idx)
             # update _should_removed_var
             if var_name in self._start_vars:
                 self._should_removed_var.discard(var_name)
-            elif self._var_deps[var_name] == []:  # no more deps of this var
+            elif self._var_to_use_op[
+                    var_name] == []:  # no more deps of this var
                 self._should_removed_var.add(var_name)
-            elif self._var_to_generate_op[var_name][-1] >= self._var_deps[
+            elif self._var_to_generate_op[var_name][-1] >= self._var_to_use_op[
                     var_name][-1]:
                 # there are circle in the graph
                 self._should_removed_var.add(var_name)
