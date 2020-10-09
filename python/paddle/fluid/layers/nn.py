64682021961f4cf9b4898ab169c6011e3ac76554
Steffy-zxf <zhangxuefei@baidu.com>
2020-10-09 08:21:53 +0000

    test=document_fix

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -8856,7 +8856,7 @@ def mean_iou(input, label, num_classes):
 
             iou_shape = [64, 32, 32]
             num_classes = 5
-            predict = paddle.randint(low=0, high=, shape=iou_shape, dtype='int64')
+            predict = paddle.randint(low=0, high=255, shape=iou_shape, dtype='int64')
             label = paddle.randint(low=0, high=255, shape=iou_shape, dtype='int64')
             mean_iou, out_wrong, out_correct = paddle.metric.mean_iou(predict, label, num_classes)
     """
