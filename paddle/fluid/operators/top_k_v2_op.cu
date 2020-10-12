fb7c9ef53e35b68b9f1f8e4a5a130bd6ebefb14f
wawltor <fangzeyang0904@hotmail.com>
2020-10-12 13:26:18 +0000

    update the code for the topk message optimize, test=develop

--- paddle/fluid/operators/top_k_v2_op.cu
+++ paddle/fluid/operators/top_k_v2_op.cu
@@ -38,8 +38,10 @@ template <typename DeviceContext, typename T>
 class TopkV2OpCUDAKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
-    PADDLE_ENFORCE(platform::is_gpu_place(ctx.GetPlace()),
-                   "It must use CUDAPlace.");
+    PADDLE_ENFORCE(
+        platform::is_gpu_place(context.GetPlace()),
+        platform::errors::InvalidArgument(
+            "It must use CUDAPlace, you must check your device set."));
     auto* input = ctx.Input<Tensor>("X");
     auto* output = ctx.Output<Tensor>("Out");
     auto* indices = ctx.Output<Tensor>("Indices");
@@ -192,9 +194,10 @@ template <typename DeviceContext, typename T>
 class TopkV2OpGradCUDAKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& context) const override {
-    PADDLE_ENFORCE_EQ(
-        platform::is_gpu_place(context.GetPlace()), true,
-        platform::errors::InvalidArgument("It must use CUDAPlace."));
+    PADDLE_ENFORCE(
+        platform::is_gpu_place(context.GetPlace()),
+        platform::errors::InvalidArgument(
+            "It must use CUDAPlace, you must check your device set."));
     auto* x = context.Input<Tensor>("X");
     auto* out_grad = context.Input<Tensor>(framework::GradVarName("Out"));
     auto* indices = context.Input<Tensor>("Indices");
