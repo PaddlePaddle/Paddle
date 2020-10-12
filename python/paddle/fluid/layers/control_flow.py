90d48f3b518a88bb41cc19e8c7671c723edbc2f6
Steffy-zxf <zhangxuefei@baidu.com>
2020-10-12 02:35:01 +0000

    update docs

--- python/paddle/fluid/layers/control_flow.py
+++ python/paddle/fluid/layers/control_flow.py
@@ -1431,6 +1431,7 @@ def increment(x, value=1.0, in_place=True):
 
     Examples:
         .. code-block:: python
+
           import paddle.fluid as fluid
           counter = fluid.layers.zeros(shape=[1], dtype='float32') # [0.]
           fluid.layers.increment(counter) # [1.]
