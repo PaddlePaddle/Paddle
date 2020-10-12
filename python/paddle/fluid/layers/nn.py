a45809b7b12bf685db000f71188cacba256f606b
chentianyu03 <chentianyu03@baidu.com>
2020-10-12 13:25:21 +0000

    modify removed alias apis to raw api in doc and unittests

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -9289,7 +9289,7 @@ def pad2d(input,
             x_shape = (1, 1, 3, 4)
             x = np.arange(np.prod(x_shape), dtype=np.float32).reshape(x_shape) + 1
             tensor_x = paddle.to_tensor(x)
-            y = F.pad2d(tensor_x, paddings=[1, 2, 2, 1], pad_value=1, mode='constant')
+            y = paddle.fluid.layers.pad2d(tensor_x, paddings=[1, 2, 2, 1], pad_value=1, mode='constant')
             print(y.numpy())
             # [[[[ 1.  1.  1.  1.  1.  1.  1.]
             #    [ 1.  1.  1.  2.  3.  4.  1.]
@@ -9302,7 +9302,7 @@ def pad2d(input,
             x_shape = (1, 1, 2, 3)
             x = np.arange(np.prod(x_shape), dtype=np.float32).reshape(x_shape) + 1
             tensor_x = paddle.to_tensor(x)
-            y = F.pad2d(tensor_x, paddings=[1, 1, 1, 1], mode='reflect')
+            y = paddle.fluid.layers.pad2d(tensor_x, paddings=[1, 1, 1, 1], mode='reflect')
             print(y.numpy())
             # [[[[5. 4. 5. 6. 5.]
             #    [2. 1. 2. 3. 2.]
