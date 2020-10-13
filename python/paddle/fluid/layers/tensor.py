2508dbdd0d6260550cf1c107da325eea41296b83
liuyuhui <liuyuhui@baidu.com>
2020-10-13 17:31:46 +0800

    Revert "fix dygraph support for assign,test=develop"
    This reverts commit 195dd41ff37a609eaa2c3fb745819c2cbf4a68d6.

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -595,12 +595,8 @@ def assign(input, output=None):
             return core.ops.assign(input)
         else:
             attrs = get_attrs(input)
-            print("attrs:{}".format(attrs))
-            attrs_ = []
-            for k, v in attrs.items():
-                attrs_.append(k)
-                attrs_.append(v)
-            return core.ops.assign_value(*attrs_)
+            print("attrs:{}".format('dtype', list(input.shape)))
+            return core.ops.assign_value(*attrs)
     helper = LayerHelper('assign', **locals())
 
     if isinstance(input, Variable):
@@ -621,9 +617,7 @@ def assign(input, output=None):
             output = helper.create_variable_for_type_inference(
                 dtype=input.dtype)
         helper.append_op(
-            type='assign_value',
-            outputs={'Out': [output]},
-            attrs=get_attrs(input))
+            type='assign_value', outputs={'Out': [output]}, attrs=get_attrs())
 
     return output
 
