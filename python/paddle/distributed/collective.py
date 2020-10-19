f2a6f86ecad9298feb554c10d9a00f6c15320756
sandyhouse <root@yq01-sys-hic-p40-box-a12-0071.yq01.baidu.com>
2020-10-19 13:41:37 +0800

    update, test=document_fix

--- python/paddle/distributed/collective.py
+++ python/paddle/distributed/collective.py
@@ -47,6 +47,25 @@ class ReduceOp:
         ReduceOp.MIN
 
         ReduceOp.PROD
+
+    Examples:
+        .. code-block:: python
+
+            import numpy as np
+            import paddle
+            from paddle.distributed import ReduceOp
+            from paddle.distributed import init_parallel_env
+
+            paddle.set_device('gpu:%d'%paddle.distributed.ParallelEnv().dev_id)
+            init_parallel_env()
+            if paddle.distributed.ParallelEnv().local_rank == 0:
+                np_data = np.array([[4, 5, 6], [4, 5, 6]])
+            else:
+                np_data = np.array([[1, 2, 3], [1, 2, 3]])
+            data = paddle.to_tensor(np_data)
+            paddle.distributed.all_reduce(data, op=ReduceOp.SUM)
+            out = data.numpy()
+            # [[5, 7, 9], [5, 7, 9]]
     """
     SUM = 0
     MAX = 1
