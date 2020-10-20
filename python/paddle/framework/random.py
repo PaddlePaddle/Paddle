e54e58a58dacd61e65849f5326f10fcce99d177d
liuhui29 <liuhui29@baidu.com>
2020-10-20 11:27:28 +0800

    rename manual_seed --> seed

--- python/paddle/framework/random.py
+++ python/paddle/framework/random.py
@@ -45,10 +45,10 @@ def seed(seed):
     if core.is_compiled_with_cuda():
         for i in range(core.get_cuda_device_count()):
             core.default_cuda_generator(i)._is_init_py = True
-            core.default_cuda_generator(i).seed(seed)
+            core.default_cuda_generator(i).manual_seed(seed)
 
     core.default_cpu_generator()._is_init_py = True
-    return core.default_cpu_generator().seed(seed)
+    return core.default_cpu_generator().manual_seed(seed)
 
 
 def get_cuda_rng_state():
