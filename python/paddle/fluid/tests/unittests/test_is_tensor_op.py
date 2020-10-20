21bb9b9354b4b67487fbcc22a89ef57ab22b9423
Ray2020BD <563755780@qq.com>
2020-10-20 05:54:35 +0000

    Add new api: is_tensor

---
+++ python/paddle/fluid/tests/unittests/test_is_tensor_op.py
@@ -0,0 +1,56 @@
+#   Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import unittest
+import paddle
+
+DELTA = 0.00001
+
+
+class Test_is_tensor_op(unittest.TestCase):
+    def test_is_tensor_real(self, dtype="float32"):
+        """Test is_tensor api with a real tensor
+        """
+        x = paddle.rand([3, 2, 4], dtype=dtype)
+        res_expect = True
+        self.assertEqual(paddle.is_tensor(x), res_expect)
+
+    def test_is_tensor_complex(self, dtype="float32"):
+        """Test is_tensor api with a complex tensor
+        """
+        r = paddle.to_tensor(1)
+        i = paddle.to_tensor(2)
+        x = paddle.ComplexTensor(r, i)
+        res_expect = True
+        self.assertEqual(paddle.is_tensor(x), res_expect)
+
+    def test_is_tensor_list(self, dtype="float32"):
+        """Test is_tensor api with a list
+        """
+        x = [1, 2, 3]
+        res_expect = False
+        self.assertEqual(paddle.is_tensor(x), res_expect)
+
+    def test_is_tensor_number(self, dtype="float32"):
+        """Test is_tensor api with a number
+        """
+        x = 5
+        res_expect = False
+        self.assertEqual(paddle.is_tensor(x), res_expect)
+
+
+if __name__ == '__main__':
+    unittest.main()
