a91d7d4265b8401a95ffe4ed176783688722c017
tianshuo78520a <707759223@qq.com>
2020-10-20 10:13:34 +0800

    test=document_fix

--- tools/dockerfile/build_scripts/install_trt.sh
+++ tools/dockerfile/build_scripts/install_trt.sh
@@ -20,10 +20,6 @@ if [[ "$VERSION" == "10.1" ]];then
   wget -q https://paddle-ci.gz.bcebos.com/TRT/TensorRT6-cuda10.1-cudnn7.tar.gz --no-check-certificate
   tar -zxf TensorRT6-cuda10.1-cudnn7.tar.gz -C /usr/local
   cp -rf /usr/local/TensorRT6-cuda10.1-cudnn7/include/* /usr/include/ && cp -rf /usr/local/TensorRT6-cuda10.1-cudnn7/lib/* /usr/lib/
-elif [[ "$VERSION" == "10.2" ]];then
-  wget -q https://paddle-ci.cdn.bcebos.com/TRT/TensorRT-7.1.3.4.tar --no-check-certificate
-  tar -xf build_scripts/TensorRT-7.1.3.4.tar -C /usr/local
-  cp -rf /usr/local/TensorRT-7.1.3.4/include/* /usr/include/ && cp -rf /usr/local/TensorRT-7.1.3.4/lib/* /usr/lib/
 elif [[ "$VERSION" == "10.0" ]];then
   wget -q https://paddle-ci.gz.bcebos.com/TRT/TensorRT6-cuda10.0-cudnn7.tar.gz --no-check-certificate
   tar -zxf TensorRT6-cuda10.0-cudnn7.tar.gz -C /usr/local
