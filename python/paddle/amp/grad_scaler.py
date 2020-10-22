0ea03916535845863ffc6f49486fca9c70a83a01
pangyoki <pangyoki@126.com>
2020-10-22 07:30:51 +0000

    change Conv2d to Conv2D

--- python/paddle/amp/grad_scaler.py
+++ python/paddle/amp/grad_scaler.py
@@ -50,7 +50,7 @@ class GradScaler(AmpScaler):
 
             import paddle
 
-            model = paddle.nn.Conv2d(3, 2, 3, bias_attr=True)
+            model = paddle.nn.Conv2D(3, 2, 3, bias_attr=True)
             optimizer = paddle.optimizer.SGD(learning_rate=0.01, parameters=model.parameters())
             scaler = paddle.amp.GradScaler(init_loss_scaling=1024)
             data = paddle.rand([10, 3, 32, 32])
@@ -90,7 +90,7 @@ class GradScaler(AmpScaler):
 
                 import paddle
 
-                model = paddle.nn.Conv2d(3, 2, 3, bias_attr=True)
+                model = paddle.nn.Conv2D(3, 2, 3, bias_attr=True)
                 optimizer = paddle.optimizer.SGD(learning_rate=0.01, parameters=model.parameters())
                 scaler = paddle.amp.GradScaler(init_loss_scaling=1024)
                 data = paddle.rand([10, 3, 32, 32])
@@ -122,7 +122,7 @@ class GradScaler(AmpScaler):
 
                 import paddle
 
-                model = paddle.nn.Conv2d(3, 2, 3, bias_attr=True)
+                model = paddle.nn.Conv2D(3, 2, 3, bias_attr=True)
                 optimizer = paddle.optimizer.SGD(learning_rate=0.01, parameters=model.parameters())
                 scaler = paddle.amp.GradScaler(init_loss_scaling=1024)
                 data = paddle.rand([10, 3, 32, 32])
