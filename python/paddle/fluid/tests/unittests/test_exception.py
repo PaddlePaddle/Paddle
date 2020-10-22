dd62b73bcef46bb2f046623ba743cc205220646a
Chen Weihang <chenweihang@baidu.com>
2020-10-22 09:47:03 +0000

    add unittest for coverage

--- python/paddle/fluid/tests/unittests/test_exception.py
+++ python/paddle/fluid/tests/unittests/test_exception.py
@@ -14,9 +14,13 @@
 
 from __future__ import print_function
 
+import numpy
+import unittest
+
+import paddle
+import paddle.fluid as fluid
 import paddle.compat as cpt
 import paddle.fluid.core as core
-import unittest
 
 
 class TestException(unittest.TestCase):
@@ -30,6 +34,35 @@ class TestException(unittest.TestCase):
             exception = ex
 
         self.assertIsNotNone(exception)
+
+
+class TestExceptionStatic(unittest.TestCase):
+    def setUp(self):
+        paddle.enable_static()
+        # test no C++ stack format
+        fluid.set_flags({'FLAGS_call_stack_level': 1})
+
+    def test_exception_in_static_model(self):
+        x = fluid.layers.data(name='X', shape=[-1, 13], dtype='float32')
+        y = fluid.layers.data(name='Y', shape=[-1, 1], dtype='float32')
+        predict = fluid.layers.fc(input=x, size=1, act=None)
+        loss = fluid.layers.square_error_cost(input=predict, label=y)
+        avg_loss = fluid.layers.mean(loss)
+
+        fluid.optimizer.SGD(learning_rate=0.01).minimize(avg_loss)
+
+        place = fluid.CPUPlace()
+        exe = fluid.Executor(place)
+        exe.run(fluid.default_startup_program())
+
+        x = numpy.random.random(size=(8, 12)).astype('float32')
+        y = numpy.random.random(size=(8, 1)).astype('float32')
+
+        with self.assertRaises(ValueError):
+            exe.run(fluid.default_main_program(),
+                    feed={'X': x,
+                          'Y': y},
+                    fetch_list=[avg_loss.name])
 
 
 if __name__ == "__main__":
