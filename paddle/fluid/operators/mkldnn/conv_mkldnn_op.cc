305bbf4b6b0173a66af57a13bf51d1bb45fecb93
Jacek Czaja <jacek.czaja@intel.com>
2020-10-22 17:15:02 +0200

    - lint fix

--- paddle/fluid/operators/mkldnn/conv_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/conv_mkldnn_op.cc
@@ -1072,7 +1072,7 @@ class ConvMKLDNNGradOpKernel : public paddle::framework::OpKernel<T> {
         // for 2d conv with groups (five dimensional data reorder to goihw)
         mkldnn::memory::format_tag out_format =
             weights_tz.size() == 6 ? mkldnn::memory::format_tag::goidhw
-                                    : mkldnn::memory::format_tag::goihw;
+                                   : mkldnn::memory::format_tag::goihw;
         const std::string key =
             platform::CreateKey(weights_tz, filter_fmt, out_format, in_type);
 
@@ -1094,7 +1094,7 @@ class ConvMKLDNNGradOpKernel : public paddle::framework::OpKernel<T> {
         // because filter_grad shape is set for OIHW (OIDHW for conv3d)
         mkldnn::memory::format_tag target_format =
             weights_tz.size() == 6 ? mkldnn::memory::format_tag::oidhw
-                                    : mkldnn::memory::format_tag::oihw;
+                                   : mkldnn::memory::format_tag::oihw;
         filter_grad->set_format(target_format);
       } else {
         filter_grad->set_format(filter_fmt);
