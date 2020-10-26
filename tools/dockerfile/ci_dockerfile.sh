f6931f753417c5095b3369c8ffa7200c14ffe6ef
tianshuo78520a <707759223@qq.com>
2020-10-26 19:56:17 +0800

    test=exitcode1

--- tools/dockerfile/ci_dockerfile.sh
+++ tools/dockerfile/ci_dockerfile.sh
@@ -22,17 +22,17 @@ function make_ubuntu_dockerfile(){
   sed -i "${dockerfile_line}i RUN wget --no-check-certificate -q https://paddle-edl.bj.bcebos.com/hadoop-2.7.7.tar.gz && \
      tar -xzf  hadoop-2.7.7.tar.gz && mv hadoop-2.7.7 /usr/local/" ${dockerfile_name} 
   sed -i "s#<install_gcc>#WORKDIR /usr/bin \\
-      COPY tools/dockerfile/build_scripts /build_scripts \\
-      RUN bash /build_scripts/install_gcc.sh gcc82 \&\& rm -rf /build_scripts \\
-      RUN cp gcc  gcc.bak \&\& cp g++  g++.bak \&\& rm gcc \&\& rm g++ \\
-      RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/local/bin/gcc \\
-      RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/local/bin/g++ \\
-      RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/bin/gcc \\
-      RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/bin/g++ \\
-      ENV PATH=/usr/local/gcc-8.2/bin:\$PATH #g" ${dockerfile_name}
-  sed -i "s#bash /build_scripts/install_nccl2.sh#wget --no-proxy https://nccl2-deb.cdn.bcebos.com/nccl-repo-ubuntu1604-2.7.8-ga-cuda10.1_1-1_amd64.deb && \\
-      RUN dpkg -i nccl-repo-ubuntu1604-2.7.8-ga-cuda10.1_1-1_amd64.deb && \\
-      RUN apt-get install -y libnccl2=2.7.8-1+cuda10.1 libnccl-dev=2.7.8-1+cuda10.1 --allow-change-held-packages #g" ${dockerfile_name}
+    COPY tools/dockerfile/build_scripts /build_scripts \\
+    RUN bash /build_scripts/install_gcc.sh gcc82 \&\& rm -rf /build_scripts \\
+    RUN cp gcc  gcc.bak \&\& cp g++  g++.bak \&\& rm gcc \&\& rm g++ \\
+    RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/local/bin/gcc \\
+    RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/local/bin/g++ \\
+    RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/bin/gcc \\
+    RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/bin/g++ \\
+    ENV PATH=/usr/local/gcc-8.2/bin:\$PATH #g" ${dockerfile_name}
+  sed -i "s#bash /build_scripts/install_nccl2.sh#wget --no-proxy https://nccl2-deb.cdn.bcebos.com/nccl-repo-ubuntu1604-2.7.8-ga-cuda10.1_1-1_amd64.deb \\
+    RUN dpkg -i nccl-repo-ubuntu1604-2.7.8-ga-cuda10.1_1-1_amd64.deb \\
+    RUN apt-get install -y libnccl2=2.7.8-1+cuda10.1 libnccl-dev=2.7.8-1+cuda10.1 --allow-change-held-packages #g" ${dockerfile_name}
 }
 
 
@@ -42,10 +42,10 @@ function make_centos_dockerfile(){
   sed -i "s#COPY build_scripts /build_scripts#COPY tools/dockerfile/build_scripts ./build_scripts#g" ${dockerfile_name} 
   dockerfile_line=$(wc -l ${dockerfile_name}|awk '{print $1}')
   sed -i "${dockerfile_line}i RUN ln -s /usr/lib64/libz.so /usr/local/lib/libz.so \\
-     RUN ln -s /usr/local/lib/libnccl.so /usr/local/cuda/lib64/ \\
-     RUN rm -rf /usr/include/NvInfer*" ${dockerfile_name}
+    RUN ln -s /usr/local/lib/libnccl.so /usr/local/cuda/lib64/ \\
+    RUN rm -rf /usr/include/NvInfer*" ${dockerfile_name}
   sed -i $"${dockerfile_line}i RUN wget --no-check-certificate -q https://paddle-edl.bj.bcebos.com/hadoop-2.7.7.tar.gz \\
-     RUN tar -xzf  hadoop-2.7.7.tar.gz && mv hadoop-2.7.7 /usr/local/" ${dockerfile_name}
+    RUN tar -xzf  hadoop-2.7.7.tar.gz && mv hadoop-2.7.7 /usr/local/" ${dockerfile_name}
   sed -i "s#RUN bash build_scripts/install_nccl2.sh##g" ${dockerfile_name}
   sed -i "${dockerfile_line}i RUN wget --no-check-certificate -q https://nccl2-deb.cdn.bcebos.com/libnccl-2.7.8-1+cuda10.2.x86_64.rpm \\
     RUN wget --no-check-certificate -q https://nccl2-deb.cdn.bcebos.com/libnccl-devel-2.7.8-1+cuda10.2.x86_64.rpm \\
@@ -54,14 +54,14 @@ function make_centos_dockerfile(){
     RUN rpm -ivh libnccl-devel-2.7.8-1+cuda10.2.x86_64.rpm \\
     RUN rpm -ivh libnccl-static-2.7.8-1+cuda10.2.x86_64.rpm && rm -f /usr/local/include/nccl.h " ${dockerfile_name}
   sed -i "s#<install_gcc>#WORKDIR /usr/bin \\
-      COPY tools/dockerfile/build_scripts /build_scripts \\
-      RUN bash /build_scripts/install_gcc.sh gcc82 \&\& rm -rf /build_scripts \\
-      RUN cp gcc  gcc.bak \&\& cp g++  g++.bak \&\& rm gcc \&\& rm g++ \\
-      RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/local/bin/gcc \\
-      RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/local/bin/g++ \\
-      RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/bin/gcc \\
-      RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/bin/g++ \\
-      ENV PATH=/usr/local/gcc-8.2/bin:\$PATH #g" ${dockerfile_name}
+    COPY tools/dockerfile/build_scripts /build_scripts \\
+    RUN bash /build_scripts/install_gcc.sh gcc82 \&\& rm -rf /build_scripts \\
+    RUN cp gcc  gcc.bak \&\& cp g++  g++.bak \&\& rm gcc \&\& rm g++ \\
+    RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/local/bin/gcc \\
+    RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/local/bin/g++ \\
+    RUN ln -s /usr/local/gcc-8.2/bin/gcc /usr/bin/gcc \\
+    RUN ln -s /usr/local/gcc-8.2/bin/g++ /usr/bin/g++ \\
+    ENV PATH=/usr/local/gcc-8.2/bin:\$PATH #g" ${dockerfile_name}
 }
 
 
