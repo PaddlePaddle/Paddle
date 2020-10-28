864f6ced6397ef8fb4b3495fe7de5a0c85356237
wozna <joanna.wozna@intel.com>
2020-10-28 16:21:42 +0100

    Revert reshape ifdef

--- paddle/fluid/operators/reshape_op.cc
+++ paddle/fluid/operators/reshape_op.cc
@@ -622,11 +622,9 @@ REGISTER_OP_CPU_KERNEL_FUNCTOR(reshape2, float, ops::ReshapeKernel, double,
                                ops::ReshapeKernel, int8_t, ops::ReshapeKernel,
                                uint8_t, ops::ReshapeKernel, int,
                                ops::ReshapeKernel, int64_t, ops::ReshapeKernel,
-                               bool, ops::ReshapeKernel
-#ifdef PADDLE_WITH_MKLDNN
-                               , paddle::platform::bfloat16, ops::ReshapeKernel // NOLINT
-#endif
-                               );  // NOLINT
+                               bool, ops::ReshapeKernel,
+                               paddle::platform::bfloat16, ops::ReshapeKernel);
+
 REGISTER_OP_CPU_KERNEL_FUNCTOR(reshape2_grad, float, ops::ReshapeGradKernel,
                                double, ops::ReshapeGradKernel, int,
                                ops::ReshapeGradKernel, int64_t,
