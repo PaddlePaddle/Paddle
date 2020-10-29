1fa06a76569dce82558f24e8809b9931329adf3e
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2020-10-29 16:18:55 +0000

    update, test=develop

--- paddle/scripts/paddle_build.bat
+++ paddle/scripts/paddle_build.bat
@@ -248,7 +248,7 @@ echo Build third_party successfully!
 set build_times=1
 :build_paddle
 echo Build Paddle the %build_times% time:
-msbuild /m:%PARALLEL_PROJECT_COUNT% /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=%PYTHON_ROOT%\Scripts /p:Configuration=Release /verbosity:minimal paddle.sln
+msbuild /m:2 /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=%PYTHON_ROOT%\Scripts /p:Configuration=Release /verbosity:minimal paddle.sln
 if %ERRORLEVEL% NEQ 0 (
     set /a build_times=%build_times%+1
     if %build_times% GTR 1 (
