24a54b8bdf8d6620aa06ab87bfe551d0df849603
jerrywgz <jerrywgz@126.com>
2020-10-29 03:06:29 +0000

    fix format

--- paddle/fluid/pybind/op_function_generator.cc
+++ paddle/fluid/pybind/op_function_generator.cc
@@ -53,9 +53,8 @@ std::map<std::string, std::set<std::string>> op_ins_map = {
      {"X", "W", "Label", "PathTable", "PathCode", "Bias"}},
     {"moving_average_abs_max_scale", {"X", "InAccum", "InState"}},
     {"multiclass_nms3", {"BBoxes", "Scores", "RoisNum"}},
-    {"box_coder", {"PriorBox", "PriorBoxVar", "TargetBox"}}};
-}
-;
+    {"box_coder", {"PriorBox", "PriorBoxVar", "TargetBox"}},
+};
 
 // NOTE(zhiqiu): Like op_ins_map.
 // Commonly, the outputs in auto-generated OP function are determined by the
