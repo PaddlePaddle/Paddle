f2ec966781b8a75c8a46c295afef900028ef23d5
Shang Zhizhou <shangzhizhou@baidu.com>
2020-11-03 18:44:11 +0800

    TensorRT中ernie模型推理性能优化，支持变长输入 (#28367)
    * fp16 result ok
    
    * change -DWITH_NVINFER_PLUGIN toconfig.EnableTensorRtOSS
    
    * auto detect special slice op converter for ernie with trt oss
    
    * ernie oss only support fp16
    
    * fix special_slice_plugin serialize bug
    
    * matmul in tensorrt ok
    
    * ernie unittest ok
    
    * add matmul tensorrt unittest
    
    * remove demo code

--- paddle/fluid/inference/api/analysis_config.cc
+++ paddle/fluid/inference/api/analysis_config.cc
@@ -121,6 +121,7 @@ AnalysisConfig::AnalysisConfig(const AnalysisConfig &other) {
   CP_MEMBER(tensorrt_precision_mode_);
   CP_MEMBER(trt_use_static_engine_);
   CP_MEMBER(trt_use_calib_mode_);
+  CP_MEMBER(trt_use_oss_);
   // MKLDNN related.
   CP_MEMBER(use_mkldnn_);
   CP_MEMBER(mkldnn_enabled_op_types_);
@@ -272,6 +273,10 @@ void AnalysisConfig::SetTRTDynamicShapeInfo(
   max_input_shape_ = max_input_shape;
   optim_input_shape_ = optim_input_shape;
   disable_trt_plugin_fp16_ = disable_trt_plugin_fp16;
+}
+
+void AnalysisConfig::EnableTensorRtOSS() {
+    trt_use_oss_ = true;
 }
 
 // TODO(Superjomn) refactor this, buggy.
