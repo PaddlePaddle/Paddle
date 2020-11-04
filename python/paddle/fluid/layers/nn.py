9eadc710d92f1fbd0fbab647fb71915621bf8ab9
wujionghao <wujionghao@baidu.com>
2020-11-04 07:42:55 +0000

    clean doc fluid

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -8753,11 +8753,10 @@ def log2(x, name=None):
         .. code-block:: python
         
             import paddle
-            import paddle.fluid as fluid
 
             # example 1: x is a float
             x_i = paddle.to_tensor([[1.0], [2.0]])
-            res = paddle.log2(x_i)
+            res = paddle.log2(x_i) # [[0.],[1.0]]
 
             # example 2: x is float32
             x_i = paddle.full(shape=[1], fill_value=2, dtype='float32')
