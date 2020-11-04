8b2436a776e5cb5d0ebc8bc06f9624ecd87c9189
Leo Chen <chenqiuliang@baidu.com>
2020-11-04 10:43:33 +0800

    Add broadcast_shape api (#28257)
    * add broadcast_shape api
    
    * add ut
    
    * follow comments
    
    * add example code, test=dodument_fix
    
    * update example code, test=document_fix

--- python/paddle/tensor/__init__.py
+++ python/paddle/tensor/__init__.py
@@ -164,6 +164,8 @@ from .math import isnan  #DEFINE_ALIAS
 from .math import prod  #DEFINE_ALIAS
 from .math import all  #DEFINE_ALIAS
 from .math import any  #DEFINE_ALIAS
+from .math import broadcast_shape  #DEFINE_ALIAS
+
 from .random import multinomial  #DEFINE_ALIAS
 from .random import standard_normal
 from .random import normal
@@ -194,4 +196,4 @@ from .stat import median  #DEFINE_ALIAS
 # from .tensor import Tensor        #DEFINE_ALIAS
 # from .tensor import LoDTensor        #DEFINE_ALIAS
 # from .tensor import LoDTensorArray        #DEFINE_ALIAS
-from .to_string import set_printoptions
+from .to_string import set_printoptions  #DEFINE_ALIAS
