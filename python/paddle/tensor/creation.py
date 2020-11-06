3c7f4b4cd015fc146a31df5270394c142f6e8314
wangchaochaohu <wangchao66@baidu.com>
2020-11-06 09:59:47 +0000

    refine

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -288,6 +288,7 @@ def ones(shape, dtype=None, name=None):
           #  [1 1]]
           
           # shape is a Tensor
+          shape = paddle.full(shape=[2], dtype='int32', fill_value=2)
           data3 = paddle.ones(shape=shape, dtype='int32') 
           # [[1 1]
           #  [1 1]]
