d5bd99985d6e302da485bb0679403ebe8d7f5768
wangchaochaohu <wangchao66@baidu.com>
2020-11-09 05:33:25 +0000

    refine

--- python/paddle/device.py
+++ python/paddle/device.py
@@ -118,7 +118,7 @@ def set_device(device):
             
         import paddle
 
-        paddle.device.set_device("cpu")
+        paddle.set_device("cpu")
         x1 = paddle.ones(name='x1', shape=[1, 2], dtype='int32')
         x2 = paddle.zeros(name='x2', shape=[1, 2], dtype='int32')
         data = paddle.stack([x1,x2], axis=1)
