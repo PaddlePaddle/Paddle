1c2d9cbf9c27af58db73c6d3cc38312613cc4fe3
zhiqiu <chenqiuliang@baidu.com>
2020-11-10 16:44:58 +0800

    update ut

--- python/paddle/fluid/tests/unittests/rnn/test_rnn_nets.py
+++ python/paddle/fluid/tests/unittests/rnn/test_rnn_nets.py
@@ -272,6 +272,8 @@ class TestLSTM(unittest.TestCase):
 
     def test_predict(self):
         predict_test_util(self.place, "LSTM")
+        predict_test_util(self.place, "LSTM", "float64", stop_gradient=False)
+        predict_test_util(self.place, "LSTM", "float32", stop_gradient=False)
 
     def runTest(self):
         self.test_with_initial_state()
@@ -280,7 +282,7 @@ class TestLSTM(unittest.TestCase):
         self.test_predict()
 
 
-def predict_test_util(place, mode):
+def predict_test_util(place, mode, dtype="float32", stop_gradient=True):
     place = paddle.set_device(place)
     paddle.seed(123)
     np.random.seed(123)
@@ -297,8 +299,8 @@ def predict_test_util(place, mode):
         def forward(self, input):
             return self.rnn(input)
 
-    x = paddle.randn((4, 10, 16))
-    x.stop_gradient = False
+    x = paddle.randn((4, 10, 16), dtype=dtype)
+    x.stop_gradient = stop_gradient
     seq_len = paddle.to_tensor(np.array([10, 6, 8, 5]))
     mask = sequence_mask(seq_len, maxlen=10, dtype=x.dtype)
     mask = paddle.unsqueeze(mask, [2])
