f00deef35b2e3209b851410281aa0b573efd67bc
pangyoki <pangyoki@126.com>
2020-11-11 07:59:08 +0000

    fix Categorical en doc

--- python/paddle/distribution.py
+++ python/paddle/distribution.py
@@ -672,13 +672,13 @@ class Categorical(Distribution):
 
             paddle.seed(100) # on CPU device
             x = paddle.rand([6])
-            print(x.numpy())
+            print(x)
             # [0.5535528  0.20714243 0.01162981
             #  0.51577556 0.36369765 0.2609165 ]
 
             paddle.seed(200) # on CPU device
             y = paddle.rand([6])
-            print(y.numpy())
+            print(y)
             # [0.77663314 0.90824795 0.15685187
             #  0.04279523 0.34468332 0.7955718 ]
 
@@ -746,7 +746,7 @@ class Categorical(Distribution):
 
                 paddle.seed(100) # on CPU device
                 x = paddle.rand([6])
-                print(x.numpy())
+                print(x)
                 # [0.5535528  0.20714243 0.01162981
                 #  0.51577556 0.36369765 0.2609165 ]
 
@@ -793,13 +793,13 @@ class Categorical(Distribution):
 
                 paddle.seed(100) # on CPU device
                 x = paddle.rand([6])
-                print(x.numpy())
+                print(x)
                 # [0.5535528  0.20714243 0.01162981
                 #  0.51577556 0.36369765 0.2609165 ]
 
                 paddle.seed(200) # on CPU device
                 y = paddle.rand([6])
-                print(y.numpy())
+                print(y)
                 # [0.77663314 0.90824795 0.15685187
                 #  0.04279523 0.34468332 0.7955718 ]
 
@@ -844,7 +844,7 @@ class Categorical(Distribution):
 
                 paddle.seed(100) # on CPU device
                 x = paddle.rand([6])
-                print(x.numpy())
+                print(x)
                 # [0.5535528  0.20714243 0.01162981
                 #  0.51577556 0.36369765 0.2609165 ]
 
@@ -889,7 +889,7 @@ class Categorical(Distribution):
 
                 paddle.seed(100) # on CPU device
                 x = paddle.rand([6])
-                print(x.numpy())
+                print(x)
                 # [0.5535528  0.20714243 0.01162981
                 #  0.51577556 0.36369765 0.2609165 ]
 
@@ -955,7 +955,7 @@ class Categorical(Distribution):
 
                 paddle.seed(100) # on CPU device
                 x = paddle.rand([6])
-                print(x.numpy())
+                print(x)
                 # [0.5535528  0.20714243 0.01162981
                 #  0.51577556 0.36369765 0.2609165 ]
 
