614f20f6bad5b158fc13452474dfac2e62ce1de6
Yang Zhang <yangzhang@live.com>
2020-11-12 17:05:05 +0800

    Update `add` `clip` `MSELoss` and `no_grad` (#28530)
    also fix `no_grad` alias

--- python/paddle/framework/__init__.py
+++ python/paddle/framework/__init__.py
@@ -38,7 +38,7 @@ from ..fluid.core import CUDAPinnedPlace  #DEFINE_ALIAS
 from ..fluid.core import VarBase  #DEFINE_ALIAS
 
 from paddle.fluid import core  #DEFINE_ALIAS
-from ..fluid.dygraph.base import no_grad  #DEFINE_ALIAS
+from ..fluid.dygraph.base import no_grad_ as no_grad  #DEFINE_ALIAS
 from ..fluid.dygraph.base import to_variable  #DEFINE_ALIAS
 from ..fluid.dygraph.base import grad  #DEFINE_ALIAS
 from .io import save
