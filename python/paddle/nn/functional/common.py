3422608bb05934e5defd278bfe483f6fb6bb9277
littletomatodonkey <dazhiningsibuqu@163.com>
2020-11-16 03:07:18 +0000

    fix comment and rm fluid import

--- python/paddle/nn/functional/common.py
+++ python/paddle/nn/functional/common.py
@@ -14,7 +14,6 @@
 
 import warnings
 import paddle
-from paddle import fluid
 from ...fluid.framework import in_dygraph_mode, default_main_program
 from paddle.fluid.layer_helper import LayerHelper
 from paddle.fluid.layers.tensor import Variable, fill_constant, zeros, concat
@@ -1159,6 +1158,9 @@ def alpha_dropout(x, p=0.5, training=True, name=None):
 def pad(x, pad, mode='constant', value=0, data_format="NCHW", name=None):
     """
     Pad tensor according to 'pad' and 'mode'.
+    If mode is 'constant' and length of pad is twice as length of x dimension,
+    then the padding will be started from the last dimension and moved forward onto x
+    according to 'pad' and 'value'.
     If mode is 'reflect', pad[0] and pad[1] must be no greater
     than width-1. The height and depth dimension has the same condition.
 
@@ -1275,6 +1277,9 @@ def pad(x, pad, mode='constant', value=0, data_format="NCHW", name=None):
     unsqueezed_dim = []
 
     if mode == "constant" and isinstance(pad, list) and len(pad) == x_dim * 2:
+        print(
+            "When the mode is constant and pad length is twice as x dimension, the padding will be started from the last dimension and moved forward onto x."
+        )
         return layers.pad(x, pad, pad_value=value)
 
     if isinstance(pad, Variable):
