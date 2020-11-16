eee189ae049b72f658ef9b169eb8d48f2456cb1b
littletomatodonkey <dazhiningsibuqu@163.com>
2020-11-16 10:42:08 +0000

    fix note

--- python/paddle/nn/functional/common.py
+++ python/paddle/nn/functional/common.py
@@ -1159,7 +1159,7 @@ def pad(x, pad, mode='constant', value=0, data_format="NCHW", name=None):
     """
     Pad tensor according to 'pad' and 'mode'.
     If mode is 'constant' and length of pad is twice as length of x dimension,
-    then the padding will be started from the last dimension and moved forward onto x
+    then the padding will be started from the first dimension and moved back onto x
     according to 'pad' and 'value'.
     If mode is 'reflect', pad[0] and pad[1] must be no greater
     than width-1. The height and depth dimension has the same condition.
