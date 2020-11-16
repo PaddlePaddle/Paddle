37a2c64395c4d4a482df7755de92c1fcf121094b
baiyfbupt <me@ethanbai.com>
2020-11-16 11:39:22 +0800

    fix code example, test=document_fix

--- python/paddle/fluid/layers/loss.py
+++ python/paddle/fluid/layers/loss.py
@@ -329,7 +329,7 @@ def square_error_cost(input, label):
             input = paddle.to_tensor([1.1, 1.9])
             label = paddle.to_tensor([1.0, 2.0])
             output = paddle.nn.functional.square_error_cost(input, label)
-            print(output.numpy())
+            print(output)
             # [0.01, 0.01]
 
     """
