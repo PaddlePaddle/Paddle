0f84cc3f7900ddbbe98506273c151dcd2d5562fd
TeslaZhao <zhaolisoftware@163.com>
2020-11-17 18:53:11 +0800

    Fix two english api documents, transpose and strided_slice

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -1503,6 +1503,7 @@ def strided_slice(x, axes, starts, ends, strides, name=None):
                 strides = [1, 3]
             Then:
                 result = [ [2], ]
+
     Args:
         x (Tensor): An N-D ``Tensor``. The data type is ``float32``, ``float64``, ``int32`` or ``int64``.
         axes (list|tuple): The data type is ``int32`` . Axes that `starts` and `ends` apply to.
@@ -1534,7 +1535,7 @@ def strided_slice(x, axes, starts, ends, strides, name=None):
             # sliced_1 is x[:, 1:3:1, 0:2:1, 2:4:1].                                
             # example 2:
             # attr starts is a list which contain tensor Tensor.
-            minus_3 = paddle.fill_constant([1], "int32", -3)
+            minus_3 = paddle.full(shape=[1], fill_value=-3, dtype='int32')
             sliced_2 = paddle.strided_slice(x, axes=axes, starts=[minus_3, 0, 2], ends=ends, strides=strides_2)
             # sliced_2 is x[:, 1:3:1, 0:2:1, 2:4:2].
     """
