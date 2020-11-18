f80840692c664a6e67d14163f8e32b8a5d0e9095
wujionghao <wujionghao@baidu.com>
2020-11-18 03:14:48 +0000

    clean code

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -448,12 +448,12 @@ def multiply(x, y, name=None):
             x = paddle.to_tensor([[1, 2], [3, 4]])
             y = paddle.to_tensor([[5, 6], [7, 8]])
             res = paddle.multiply(x, y)
-            print(res.numpy()) # [[5, 12], [21, 32]]
+            print(res) # [[5, 12], [21, 32]]
 
             x = paddle.to_tensor([[[1, 2, 3], [1, 2, 3]]])
             y = paddle.to_tensor([1, 2])
             res = paddle.multiply(x, y)
-            print(res.numpy()) # [[[1, 2, 3], [2, 4, 6]]]
+            print(res) # [[[1, 2, 3], [2, 4, 6]]]
 
     """
     op_type = 'elementwise_mul'
