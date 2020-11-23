96724b99e474cf5342cece113820a7bf1333677f
Jiawei Wang <wangjiawei04@baidu.com>
2020-11-23 13:52:48 +0800

    Update lamb.py

--- python/paddle/optimizer/lamb.py
+++ python/paddle/optimizer/lamb.py
@@ -80,7 +80,7 @@ class Lamb(Optimizer):
             def exclude_fn(param):
                 return param.name.endswith('.b_0')
 
-            optimizer = fluid.optimizer.Lamb(learning_rate=0.002,
+            optimizer = paddle.optimizer.Lamb(learning_rate=0.002,
                                              exclude_from_weight_decay_fn=exclude_fn)
             optimizer.minimize(cost)
     """
