75f28e9f7c9fe3fd734242e5df384531bec65039
yghstill <742925032@qq.com>
2020-11-23 11:35:51 +0000

    fix box_clip_op.cu error

--- paddle/fluid/operators/detection/box_clip_op.cu
+++ paddle/fluid/operators/detection/box_clip_op.cu
@@ -32,9 +32,9 @@ static __global__ void GPUBoxClip(const T *input, const size_t *lod,
                                   T *output) {
   T im_w = im_shape[blockIdx.x * ImShapeSize + 1];
   T im_h = im_shape[blockIdx.x * ImShapeSize];
-  for (int i = threadIdx.x; i < (lod[blockIdx.x + 1] - lod[blockIdx.x]) * width;
-       i += BlockSize) {
-    int idx = lod[blockIdx.x] * width + i;
+  for (size_t i = threadIdx.x;
+       i < (lod[blockIdx.x + 1] - lod[blockIdx.x]) * width; i += BlockSize) {
+    size_t idx = lod[blockIdx.x] * width + i;
     T im_size = (idx % 2 == 0) ? im_w : im_h;
     output[idx] = max(min(input[idx], im_size - 1), T(0.));
   }
@@ -52,28 +52,38 @@ class GPUBoxClipKernel : public framework::OpKernel<T> {
 
     auto &dev_ctx = context.template device_context<DeviceContext>();
     auto stream = dev_ctx.stream();
-    // LOG(ERROR)<<"DEBUG111111111111";
-    T *output_data = output->mutable_data<T>(dev_ctx.GetPlace());
+    T *output_data = output->mutable_data<T>(input->dims(), dev_ctx.GetPlace());
+
+    framework::LoD lod;
     if (context.HasInput("RoisNum")) {
       auto *rois_num = context.Input<Tensor>("RoisNum");
-      // LOG(ERROR)<<"DEBUG rois_num"<<rois_num;
-      const size_t batch_size = rois_num->numel();
-      const size_t *rois_num_data =
-          reinterpret_cast<const size_t *>(rois_num->data<int>());
-      GPUBoxClip<T, 512><<<batch_size, 512, 0, stream>>>(
-          input->data<T>(), rois_num_data, bbox_width, im_shape->data<T>(),
-          output_data);
-      // LOG(ERROR)<<"DEBUG33333333333";
-      // LOG(ERROR)<<"DEBUG output_data:"<<output_data;
+      std::vector<size_t> offset_v(1, 0);
+      const int *rois_num_data = rois_num->data<int>();
+
+      size_t offset = 0;
+      auto place = BOOST_GET_CONST(platform::CUDAPlace, dev_ctx.GetPlace());
+      auto cpu_place = platform::CPUPlace();
+      auto temp_val_ptr =
+          memory::Alloc(platform::CPUPlace(), rois_num->numel() * sizeof(int));
+      int *cpu_data = reinterpret_cast<int *>(temp_val_ptr->ptr());
+
+      memory::Copy(cpu_place, cpu_data, place, rois_num_data,
+                   sizeof(int) * rois_num->numel(), dev_ctx.stream());
+      dev_ctx.Wait();
+
+      for (int i = 0; i < rois_num->numel(); ++i) {
+        offset += cpu_data[i];
+        offset_v.emplace_back(offset);
+      }
+      lod.emplace_back(offset_v);
     } else {
-      auto lod = input->lod();
-      framework::LoD abs_offset_lod = framework::ToAbsOffset(lod);
-      const size_t batch_size = lod.back().size() - 1;
-      GPUBoxClip<T, 512><<<batch_size, 512, 0, stream>>>(
-          input->data<T>(),
-          abs_offset_lod[0].CUDAMutableData(dev_ctx.GetPlace()), bbox_width,
-          im_shape->data<T>(), output_data);
+      lod = input->lod();
     }
+    framework::LoD abs_offset_lod = framework::ToAbsOffset(lod);
+    const size_t batch_size = lod.back().size() - 1;
+    GPUBoxClip<T, 512><<<batch_size, 512, 0, stream>>>(
+        input->data<T>(), abs_offset_lod[0].CUDAMutableData(dev_ctx.GetPlace()),
+        bbox_width, im_shape->data<T>(), output_data);
   }
 };
 
