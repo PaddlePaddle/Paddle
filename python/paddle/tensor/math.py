391e183ec66a843d8fa92f318b085a293236d3d6
shenliang03 <shenliang03@baidu.com>
2020-11-24 22:40:38 +0800

    fix doc

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -400,7 +400,7 @@ def remainder(x, y, name=None):
 
             x = paddle.to_tensor([2, 3, 8, 7])
             y = paddle.to_tensor([1, 5, 3, 3])
-            z = paddle.mod(x, y)
+            z = paddle.remainder(x, y)
             print(z)  # [0, 3, 2, 1]
 
     """
