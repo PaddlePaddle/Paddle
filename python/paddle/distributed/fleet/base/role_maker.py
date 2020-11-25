242e2ac96d9ca76c61e1e87489f38420e8a27e64
sandyhouse <lilong12@baidu.com>
2020-11-25 23:01:27 +0800

    update, test=develop

--- python/paddle/distributed/fleet/base/role_maker.py
+++ python/paddle/distributed/fleet/base/role_maker.py
@@ -176,7 +176,7 @@ class Gloo(object):
             http_server.start()
             wait_seconds = 5
             while http_server_d.get("running",
-                                    False) and http_server.should_stop():
+                                    False) or not http_server.should_stop():
                 time.sleep(wait_seconds)
             http_server.stop()
 
