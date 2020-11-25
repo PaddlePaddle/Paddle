64915f64bd79e820435e3c83fe978e1b82bbaace
gongweibao <weibao.gong@gmail.com>
2020-11-25 02:31:01 +0000

    coverage test=develop

--- python/paddle/distributed/utils.py
+++ python/paddle/distributed/utils.py
@@ -36,6 +36,10 @@ def get_cluster_from_args(args, selected_gpus):
     logger.debug("parsed from args:node_ips:{} node_ip:{} node_rank:{}".format(
         node_ips, node_ip, node_rank))
 
+    started_port = 6070
+    if args.started_port is not None:
+        started_port = args.started_port
+
     free_ports = None
     if not args.use_paddlecloud and len(
             node_ips) <= 1 and args.started_port is None:
@@ -43,10 +47,6 @@ def get_cluster_from_args(args, selected_gpus):
         if free_ports is not None:
             free_ports = list(free_ports)
     else:
-        started_port = 6070
-        if args.started_port is not None:
-            started_port = args.started_port
-
         free_ports = [
             x for x in range(started_port, started_port + len(selected_gpus))
         ]
@@ -55,43 +55,6 @@ def get_cluster_from_args(args, selected_gpus):
     for ip in node_ips:
         trainer_endpoints.append(["%s:%d" % (ip, port) for port in free_ports])
     return get_cluster(node_ips, node_ip, trainer_endpoints, selected_gpus)
-
-
-def get_gpus(selected_gpus):
-    if selected_gpus is None:
-        from paddle.fluid import core
-        gpus_num = core.get_cuda_device_count()
-        gpus = [str(x) for x in range(0, gpus_num)]
-    else:
-        cuda_visible_devices = os.getenv("CUDA_VISIBLE_DEVICES")
-        if cuda_visible_devices is None or cuda_visible_devices == "":
-            gpus = [x.strip() for x in selected_gpus.split(',')]
-        else:
-            # change selected_gpus into relative values
-            # e.g. CUDA_VISIBLE_DEVICES=4,5,6,7; args.selected_gpus=4,5,6,7;
-            # therefore selected_gpus=0,1,2,3
-            cuda_visible_devices_list = cuda_visible_devices.split(',')
-            for x in selected_gpus.split(','):
-                assert x in cuda_visible_devices_list, "Can't find " \
-                                                       "your selected_gpus %s in CUDA_VISIBLE_DEVICES[%s]." \
-                                                       % (x, cuda_visible_devices)
-            gpus = [
-                cuda_visible_devices_list.index(x.strip())
-                for x in selected_gpus.split(',')
-            ]
-            logger.info("Change selected_gpus into reletive values. --ips:{} "
-                        "will change into relative_ips:{} according to your "
-                        "CUDA_VISIBLE_DEVICES:{}".format(
-                            selected_gpus, gpus, cuda_visible_devices_list))
-
-    return gpus
-
-
-def _print_arguments(args):
-    print("-----------  Configuration Arguments -----------")
-    for arg, value in sorted(six.iteritems(vars(args))):
-        print("%s: %s" % (arg, value))
-    print("------------------------------------------------")
 
 
 class Hdfs(object):
