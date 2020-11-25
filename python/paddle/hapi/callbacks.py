2780738da0ea6575d7cf0653c52c4314a42c1fd3
LielinJiang <jianglielin@baidu.com>
2020-11-25 12:49:41 +0000

    add tips

--- python/paddle/hapi/callbacks.py
+++ python/paddle/hapi/callbacks.py
@@ -359,6 +359,10 @@ class ProgBarLogger(Callback):
             'count': 0,
             'samples': 0,
         }
+        if self._is_print():
+            print(
+                "The loss value printed in the log is the current batch, and the metric is the average value of previous step."
+            )
 
     def on_epoch_begin(self, epoch=None, logs=None):
         self.steps = self.params['steps']
@@ -436,6 +440,9 @@ class ProgBarLogger(Callback):
             num=self.eval_steps, verbose=self.verbose)
         if self._is_print():
             print('Eval begin...')
+            print(
+                "The loss value printed in the log is the current batch, and the metric is the average value of previous step."
+            )
 
         self._eval_timer['batch_start_time'] = time.time()
 
