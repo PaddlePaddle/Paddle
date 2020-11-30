2318b083c475888ea9808b54f806bf1a78be49a5
Feiyu <chenfeiyu@baidu.com>
2020-11-30 14:41:24 +0800

    fix multiple documentation error, test=document_fix

--- python/paddle/nn/layer/rnn.py
+++ python/paddle/nn/layer/rnn.py
@@ -273,12 +273,11 @@ class SimpleRNNCell(RNNCellBase):
     The formula used is as follows:
 
     .. math::
-        h_{t} & = act(W_{ih}x_{t} + b_{ih} + W_{hh}h{t-1} + b_{hh})
+        h_{t} & = act(W_{ih}x_{t} + b_{ih} + W_{hh}h_{t-1} + b_{hh})
 
         y_{t} & = h_{t}
     
-    where :math:`act` is for :attr:`activation` , and * is the elemetwise
-    multiplication operator.
+    where :math:`act` is for :attr:`activation`.
 
     Please refer to `Finding Structure in Time 
     <https://crl.ucsd.edu/~elman/Papers/fsit.pdf>`_ for more details.
@@ -289,17 +288,17 @@ class SimpleRNNCell(RNNCellBase):
         activation (str, optional): The activation in the SimpleRNN cell. 
             It can be `tanh` or `relu`. Defaults to `tanh`.
         weight_ih_attr (ParamAttr, optional): The parameter attribute for 
-            `weight_ih`. Default: None.
+            :math:`weight_ih`. Default: None.
         weight_hh_attr(ParamAttr, optional): The parameter attribute for 
-            `weight_hh`. Default: None.
+            :math:`weight_hh`. Default: None.
         bias_ih_attr (ParamAttr, optional): The parameter attribute for the 
-            `bias_ih`. Default: None.
+            :math:`bias_ih`. Default: None.
         bias_hh_attr (ParamAttr, optional): The parameter attribute for the 
-            `bias_hh`. Default: None.
+            :math:`bias_hh`. Default: None.
         name (str, optional): Name for the operation (optional, default is 
             None). For more information, please refer to :ref:`api_guide_Name`.
 
-    Attributes:
+    Variables:
         weight_ih (Parameter): shape (hidden_size, input_size), input to hidden 
             weight, corresponding to :math:`W_{ih}` in the formula.
         weight_hh (Parameter): shape (hidden_size, hidden_size), hidden to 
@@ -310,25 +309,15 @@ class SimpleRNNCell(RNNCellBase):
             corresponding to :math:`b_{hh}` in the formula.
     
     Inputs:
-        inputs (Tensor): shape `[batch_size, input_size]`, the input, 
-                corresponding to :math:`x_t` in the formula.
-        states (Tensor, optional): shape `[batch_size, hidden_size]`, the
-            previous hidden state, corresponding to :math:`h_{t-1}` in the 
-            formula. When states is None, zero state is used. Defaults to 
-            None.
+        inputs (Tensor): shape `[batch_size, input_size]`, the input, corresponding to :math:`x_{t}` in the formula.
+        states (Tensor, optional): shape `[batch_size, hidden_size]`, the previous hidden state, corresponding to :math:`h_{t-1}` in the formula. When states is None, zero state is used. Defaults to None.
 
     Returns:
-        (outputs, new_states)
-        outputs (Tensor): shape `[batch_size, hidden_size]`, the output, 
-            corresponding to :math:`h_{t}` in the formula.
-        states (Tensor): shape `[batch_size, hidden_size]`, the new hidden 
-            state, corresponding to :math:`h_{t}` in the formula.
+        - outputs (Tensor): shape `[batch_size, hidden_size]`, the output, corresponding to :math:`h_{t}` in the formula.
+        - states (Tensor): shape `[batch_size, hidden_size]`, the new hidden state, corresponding to :math:`h_{t}` in the formula.
     
     Notes:
-        All the weights and bias are initialized with `Uniform(-std, std)` by 
-        default. Where std = :math:`\frac{1}{\sqrt{hidden_size}}`. For more 
-        information about parameter initialization, please refer to
-         :ref:`api_fluid_ParamAttr`.
+        All the weights and bias are initialized with `Uniform(-std, std)` by default. Where std = :math:`\frac{1}{\sqrt{hidden\_size}}`. For more information about parameter initialization, please refer to :ref:`api_fluid_ParamAttr`.
 
     Examples:
 
@@ -448,41 +437,28 @@ class LSTMCell(RNNCellBase):
         name (str, optional): Name for the operation (optional, default is 
             None). For more information, please refer to :ref:`api_guide_Name`.
 
-    Attributes:
+    Variables:
         weight_ih (Parameter): shape (4 * hidden_size, input_size), input to 
-            hidden weight, which corresponds to the concatenation of
-             :math:`W_{ii}, W_{if}, W_{ig}, W_{io}` in the formula.
+            hidden weight, which corresponds to the concatenation of :math:`W_{ii}, W_{if}, W_{ig}, W_{io}` in the formula.
         weight_hh (Parameter): shape (4 * hidden_size, hidden_size), hidden to 
-            hidden weight, which corresponds to the concatenation of
-             :math:`W_{hi}, W_{hf}, W_{hg}, W_{ho}` in the formula.
+            hidden weight, which corresponds to the concatenation of :math:`W_{hi}, W_{hf}, W_{hg}, W_{ho}` in the formula.
         bias_ih (Parameter): shape (4 * hidden_size, ), input to hidden bias, 
-            which corresponds to the concatenation of
-             :math:`b_{ii}, b_{if}, b_{ig}, b_{io}` in the formula.
+            which corresponds to the concatenation of :math:`b_{ii}, b_{if}, b_{ig}, b_{io}` in the formula.
         bias_hh (Parameter): shape (4 * hidden_size, ), hidden to hidden bias, 
-            which corresponds to the concatenation of
-             :math:`b_{hi}, b_{hf}, b_{hg}, b_{ho}` in the formula.
+            which corresponds to the concatenation of :math:`b_{hi}, b_{hf}, b_{hg}, b_{ho}` in the formula.
 
     Inputs:
-        inputs (Tensor): shape `[batch_size, input_size]`, the input, 
-            corresponding to :math:`x_t` in the formula.
-        states (tuple, optional): a tuple of two tensors, each of shape 
-            `[batch_size, hidden_size]`, the previous hidden state, 
-            corresponding to :math:`h_{t-1}, c_{t-1}` in the formula. 
-            When states is None, zero state is used. Defaults to None.
+        inputs (Tensor): shape `[batch_size, input_size]`, the input, corresponding to :math:`x_t` in the formula.
+        states (tuple, optional): a tuple of two tensors, each of shape `[batch_size, hidden_size]`, the previous hidden state, corresponding to :math:`h_{t-1}, c_{t-1}` in the formula. When states is None, zero state is used. Defaults to None.
 
     Returns:
-        (outputs, new_states)
-        outputs (Tensor): shape `[batch_size, hidden_size]`, the output, 
-            corresponding to :math:`h_{t}` in the formula.
-        states (tuple): a tuple of two tensors, each of shape 
-            `[batch_size, hidden_size]`, the new hidden states,
-            corresponding to :math:`h_{t}, c_{t}` in the formula.
+        outputs (Tensor): shape `[batch_size, hidden_size]`, the output, corresponding to :math:`h_{t}` in the formula.
+        states (tuple): a tuple of two tensors, each of shape `[batch_size, hidden_size]`, the new hidden states, corresponding to :math:`h_{t}, c_{t}` in the formula.
 
     Notes:
         All the weights and bias are initialized with `Uniform(-std, std)` by 
-        default. Where std = :math:`\frac{1}{\sqrt{hidden_size}}`. For more 
-        information about parameter initialization, please refer to
-         :ref:`api_fluid_ParamAttr`.
+        default. Where std = :math:`\frac{1}{\sqrt{hidden\_size}}`. For more 
+        information about parameter initialization, please refer to :ref:`api_fluid_ParamAttr`.
 
     Examples:
 
@@ -582,11 +558,11 @@ class GRUCell(RNNCellBase):
 
     ..  math::
 
-        r_{t} & = \sigma(W_{ir}x_{t} + b_{ir} + W_{hr}x_{t-1} + b_{hr})
+        r_{t} & = \sigma(W_{ir}x_{t} + b_{ir} + W_{hr}h_{t-1} + b_{hr})
 
-        z_{t} & = \sigma(W_{iz}x_{t} + b_{iz} + W_{hz}x_{t-1} + b_{hz})
+        z_{t} & = \sigma(W_{iz}x_{t} + b_{iz} + W_{hz}h_{t-1} + b_{hz})
 
-        \widetilde{h}_{t} & = \tanh(W_{ic}x_{t} + b_{ic} + r_{t} * (W_{hc}x_{t-1} + b_{hc}))
+        \widetilde{h}_{t} & = \tanh(W_{ic}x_{t} + b_{ic} + r_{t} * (W_{hc}h_{t-1} + b_{hc}))
 
         h_{t} & = z_{t} * h_{t-1} + (1 - z_{t}) * \widetilde{h}_{t}
 
@@ -599,7 +575,7 @@ class GRUCell(RNNCellBase):
     <http://proceedings.mlr.press/v37/jozefowicz15.pdf>`_ for more details.
 
     Parameters:
-        input_size (int): The input size..
+        input_size (int): The input size.
         hidden_size (int): The hidden size.
         weight_ih_attr(ParamAttr, optional): The parameter attribute for 
             `weight_ih`. Default: None.
@@ -612,38 +588,28 @@ class GRUCell(RNNCellBase):
         name (str, optional): Name for the operation (optional, default is 
             None). For more information, please refer to :ref:`api_guide_Name`.
 
-    Attributes:
+    Variables:
         weight_ih (Parameter): shape (3 * hidden_size, input_size), input to 
-            hidden weight, which corresponds to the concatenation of
-             :math:`W_{ir}, W_{iz}, W_{ic}` in the formula.
+            hidden weight, which corresponds to the concatenation of :math:`W_{ir}, W_{iz}, W_{ic}` in the formula.
         weight_hh (Parameter): shape (3 * hidden_size, hidden_size), hidden to 
-            hidden weight, which corresponds to the concatenation of
-             :math:`W_{hr}, W_{hz}, W_{hc}` in the formula.
+            hidden weight, which corresponds to the concatenation of :math:`W_{hr}, W_{hz}, W_{hc}` in the formula.
         bias_ih (Parameter): shape (3 * hidden_size, ), input to hidden bias, 
-            which corresponds to the concatenation of
-             :math:`b_{ir}, b_{iz}, b_{ic}` in the formula.
+            which corresponds to the concatenation of :math:`b_{ir}, b_{iz}, b_{ic}` in the formula.
         bias_hh (Parameter): shape (3 * hidden_size, ), hidden to hidden bias, 
-            which corresponds to the concatenation of
-             :math:`b_{hr}, b_{hz}, b_{hc}` in the formula.
+            which corresponds to the concatenation of :math:`b_{hr}, b_{hz}, b_{hc}` in the formula.
 
     Inputs:
-        inputs (Tensor): A tensor with shape `[batch_size, input_size]`,
-            corresponding to :math:`x_t` in the formula.
-        states (Tensor): A tensor with shape `[batch_size, hidden_size]`.
-            corresponding to :math:`h_{t-1}` in the formula.
+        inputs (Tensor): A tensor with shape `[batch_size, input_size]`, corresponding to :math:`x_t` in the formula.
+        states (Tensor): A tensor with shape `[batch_size, hidden_size]`, corresponding to :math:`h_{t-1}` in the formula.
 
     Returns:
-        (outputs, new_states)
-        outputs (Tensor): shape `[batch_size, hidden_size]`, the output, 
-            corresponding to :math:`h_{t}` in the formula.
-        states (Tensor): shape `[batch_size, hidden_size]`, the new hidden 
-            state, corresponding to :math:`h_{t}` in the formula.
+        outputs (Tensor): shape `[batch_size, hidden_size]`, the output, corresponding to :math:`h_{t}` in the formula.
+        states (Tensor): shape `[batch_size, hidden_size]`, the new hidden state, corresponding to :math:`h_{t}` in the formula.
     
     Notes:
         All the weights and bias are initialized with `Uniform(-std, std)` by 
-        default. Where std = :math:`\frac{1}{\sqrt{hidden_size}}`. For more 
-        information about parameter initialization, please refer to
-         :ref:`api_fluid_ParamAttr`.
+        default. Where std = :math:`\frac{1}{\sqrt{hidden\_size}}`. For more 
+        information about parameter initialization, please refer to s:ref:`api_fluid_ParamAttr`.
 
     Examples:
 
@@ -759,18 +725,11 @@ class RNN(Layer):
             If `sequence_length` is not None, the inputs are treated as 
             padded sequences. In each input sequence, elements whose time step 
             index are not less than the valid length are treated as paddings.
-        **kwargs: Additional keyword arguments to pass to `forward` of the cell. 
+        kwargs: Additional keyword arguments to pass to `forward` of the cell. 
 
     Returns:
-        (outputs, final_states)
-        outputs (Tensor|list|tuple): the output sequences.
-            If `time_major` is True, the shape is 
-            `[time_steps, batch_size, hidden_size]`, else 
-            `[batch_size, time_steps, hidden_size]`.
-        final_states (Tensor|list|tuple): final states of the cell. Tensor or 
-            a possibly nested structure of tensors which has the same structure 
-            with intial state. Each tensor in final states has the same shape 
-            and dtype as the corresponding tensor in initial states.
+        outputs (Tensor|list|tuple): the output sequences. If `time_major` is True, the shape is `[time_steps, batch_size, hidden_size]`, else `[batch_size, time_steps, hidden_size]`.
+        final_states (Tensor|list|tuple): final states of the cell. Tensor or a possibly nested structure of tensors which has the same structure with intial state. Each tensor in final states has the same shape and dtype as the corresponding tensor in initial states.
     
     Notes:
         This class is a low level API for wrapping rnn cell into a RNN network.
@@ -852,11 +811,9 @@ class BiRNN(Layer):
             If `sequence_length` is not None, the inputs are treated as 
             padded sequences. In each input sequence, elements whose time step 
             index are not less than the valid length are treated as paddings.
-        **kwargs: Additional keyword arguments. Arguments passed to `forward` 
-            for each cell.
+        kwargs: Additional keyword arguments. Arguments passed to `forward` for each cell.
 
     Outputs:
-        (outputs, final_states)
         outputs (Tensor): the outputs of the bidirectional RNN. It is the 
             concatenation of the outputs from the forward RNN and backward 
             RNN along the last axis. 
@@ -1150,12 +1107,11 @@ class SimpleRNN(RNNBase):
 
     .. math::
 
-        h_{t} & = act(W_{ih}x_{t} + b_{ih} + W_{hh}h{t-1} + b_{hh})
+        h_{t} & = act(W_{ih}x_{t} + b_{ih} + W_{hh}h_{t-1} + b_{hh})
 
         y_{t} & = h_{t}
     
-    where :math:`act` is for :attr:`activation` , and * is the elemetwise
-    multiplication operator.
+    where :math:`act` is for :attr:`activation`.
 
     Using key word arguments to construct is recommended.
 
@@ -1412,11 +1368,11 @@ class GRU(RNNBase):
 
     .. math::
 
-        r_{t} & = \sigma(W_{ir}x_{t} + b_{ir} + W_{hr}x_{t-1} + b_{hr})
+        r_{t} & = \sigma(W_{ir}x_{t} + b_{ir} + W_{hr}h_{t-1} + b_{hr})
 
-        z_{t} & = \sigma(W_{iz}x_{t} + b_{iz} + W_{hz}x_{t-1} + b_{hz})
+        z_{t} & = \sigma(W_{iz}x_{t} + b_{iz} + W_{hz}h_{t-1} + b_{hz})
 
-        \widetilde{h}_{t} & = \tanh(W_{ic}x_{t} + b_{ic} + r_{t} * (W_{hc}x_{t-1} + b_{hc}))
+        \widetilde{h}_{t} & = \tanh(W_{ic}x_{t} + b_{ic} + r_{t} * (W_{hc}h_{t-1} + b_{hc}))
 
         h_{t} & = z_{t} * h_{t-1} + (1 - z_{t}) * \widetilde{h}_{t}
 
