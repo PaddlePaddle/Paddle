d8eef4e4a466c8008dbe518a2df71ffc8d0acdcc
LielinJiang <50691816+LielinJiang@users.noreply.github.com>
2020-11-30 21:51:42 +0800

    Remove dependence of scipy (#29121)
    * lazy import for scipy
    
    * rm unused check

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -13,9 +13,6 @@
 # limitations under the License.
 
 import os
-from paddle.check_import_scipy import check_import_scipy
-
-check_import_scipy(os.name)
 
 try:
     from paddle.version import full_version as __version__
