e4c2ddf207704d7dac52559288fd6f89fa6e949a
mls1999725 <43207078+mls1999725@users.noreply.github.com>
2020-11-30 17:19:34 +0800

    Update voc2012.py

--- python/paddle/vision/datasets/voc2012.py
+++ python/paddle/vision/datasets/voc2012.py
@@ -73,12 +73,12 @@ class VOC2012(Dataset):
             normalize = Normalize(mean=[0.5, 0.5, 0.5],
                                   std=[0.5, 0.5, 0.5],
                                   data_format='HWC')
-            voc2012 = VOC2012(mode='train', transform=normalize)
+            voc2012 = VOC2012(mode='train', transform=normalize, backend='cv2')
 
             for i in range(10):
                 image, label= voc2012[i]
                 image = paddle.cast(paddle.to_tensor(image), 'float32')
-                label = paddle.to_tensor(np.array(label))
+                label = paddle.to_tensor(label)
 
                 model = SimpleNet()
                 image, label= model(image, label)
