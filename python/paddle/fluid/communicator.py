686f69fc4923ff736a1147e14b37a704cc8baf50
seiriosPlus <tangwei12@baidu.com>
2020-12-01 22:27:58 +0800

    bug fix

--- python/paddle/fluid/communicator.py
+++ python/paddle/fluid/communicator.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,7 +32,6 @@ Communicator is used for async distribute training in distribute_transpiler mode
 It's a wrapper of a cpp class Communicator and should be used inside fleet API.
 """
 from . import core
-from paddle.fluid.framework import Program
 from paddle.fluid.incubate.fleet.parameter_server.mode import DistributedMode
 
 __all__ = ['Communicator', 'LargeScaleKV']
@@ -158,6 +157,9 @@ class Communicator(object):
 
     def recv(self):
         self.communicator_.recv()
+
+    def init_params(self, context):
+        self.communicator_.init_params(context)
 
 
 class LargeScaleKV(object):
