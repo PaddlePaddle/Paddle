38727a15da74fb4b1062d50650ea75ddb632784f
zhangkeliang <zhangkeliang@baidu.com>
2020-12-02 04:05:42 +0000

    1. fix with_mkldnn compile error, 2. add TODO for unit test

--- paddle/fluid/operators/layer_norm_op.cc
+++ paddle/fluid/operators/layer_norm_op.cc
@@ -247,6 +247,14 @@ class LayerNormGradOp : public framework::OperatorWithKernel {
     framework::LibraryType library = framework::LibraryType::kPlain;
     framework::DataLayout layout = framework::DataLayout::kAnyLayout;
 
+#ifdef PADDLE_WITH_MKLDNN
+    if (library == framework::LibraryType::kPlain &&
+        this->CanMKLDNNBeUsed(ctx)) {
+      library = framework::LibraryType::kMKLDNN;
+      layout = framework::DataLayout::kMKLDNN;
+    }
+#endif
+
     return framework::OpKernelType(
         OperatorWithKernel::IndicateVarDataType(ctx, "X"), ctx.GetPlace(),
         layout, library);
