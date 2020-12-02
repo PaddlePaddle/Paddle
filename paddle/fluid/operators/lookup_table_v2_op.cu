9044ab320426eac70ebf41d475ce2662a13310e3
seiriosPlus <tangwei12@baidu.com>
2020-12-02 16:54:53 +0800

    fix gpu emb out of range
    Change-Id: I5794ac73bd634d5ea069a6fbbd914274b6d6b7bf

--- paddle/fluid/operators/lookup_table_v2_op.cu
+++ paddle/fluid/operators/lookup_table_v2_op.cu
@@ -110,14 +110,16 @@ class LookupTableV2CUDAKernel : public framework::OpKernel<T> {
     for (int64_t i = 0; i < K; ++i) {
       PADDLE_ENFORCE_GE(
           ids[i], 0,
-          "Variable value (input) of OP(paddle.nn.embedding) "
-          "expected >= 0 and < %ld, but got %ld. Please check input value.",
-          N, ids[i]);
+          platform::errors::InvalidArgument(
+              "Variable value (input) of OP(paddle.nn.embedding) "
+              "expected >= 0 and < %ld, but got %ld. Please check input value.",
+              N, ids[i]));
       PADDLE_ENFORCE_LT(
           ids[i], N,
-          "Variable value (input) of OP(paddle.nn.embedding) "
-          "expected >= 0 and < %ld, but got %ld. Please check input value.",
-          N, ids[i]);
+          platform::errors::InvalidArgument(
+              "Variable value (input) of OP(paddle.nn.embedding) "
+              "expected >= 0 and < %ld, but got %ld. Please check input value.",
+              N, ids[i]));
     }
 
     auto *table = table_t->data<T>();
