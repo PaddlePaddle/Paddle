f616daaa58fb872d5f2d224d4caf2c9d88ab07c8
LielinJiang <50691816+LielinJiang@users.noreply.github.com>
2020-12-03 20:25:54 +0800

    Move temporal_shift to paddle.nn.functional (#29261) (#29315)
    * move temporal_shift to functional

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -13332,7 +13332,7 @@ def temporal_shift(x, seg_num, shift_ratio=0.25, name=None):
             import paddle.nn.functional as F
 
             input = paddle.randn([6, 4, 2, 2])
-            out = paddle.fluid.layers.temporal_shift(x=input, seg_num=2, shift_ratio=0.2)
+            out = F.temporal_shift(x=input, seg_num=2, shift_ratio=0.2)
     """
     helper = LayerHelper("temporal_shift", **locals())
     check_variable_and_dtype(x, 'x', ['float32', 'float64'], 'temporal_shift')
