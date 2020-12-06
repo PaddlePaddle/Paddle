d93c04a04c94c8d157e7036e99a5d926bd1241a8
zlsh80826 <zlsh80826@gmail.com>
2020-12-06 08:13:03 +0000

    vec softmax fw

--- paddle/fluid/operators/softmax_cudnn_op.cu
+++ paddle/fluid/operators/softmax_cudnn_op.cu
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/framework/op_registry.h"
+#include "paddle/fluid/operators/math/math_cuda_utils.h"
 #include "paddle/fluid/operators/softmax_op.h"
 #include "paddle/fluid/platform/cudnn_helper.h"
 
@@ -38,6 +39,34 @@ static inline int SizeOutAxis(const int axis, DDim dims) {
   return size;
 }
 
+template <typename T, typename VECT, int VPT, int WARP_PER_BLOCK>
+__global__ void VecSoftmaxKernel(T* dst, const T* src, const int batch_size,
+                                 const int softmax_ele) {
+  int offset = blockIdx.x * softmax_ele * WARP_PER_BLOCK;
+  int idx = threadIdx.x * VPT;
+
+  VECT buf = reinterpret_cast<const VECT*>(&src[offset + idx])[0];
+  T* bufp = reinterpret_cast<T*>(&buf);
+  float4 val4;
+  float* val4p = reinterpret_cast<float*>(&val4);
+  for (int i = 0; i < VPT; ++i) {
+    val4p[i] = static_cast<float>(bufp[i]);
+  }
+  float val = val4.x + val4.y + val4.z + val4.w;
+  float max_val = math::warpReduceMax<float>(
+      max(max(val4.x, val4.y), max(val4.z, val4.w)), 0xffffffff);
+  float4 tmp4 = make_float4(__expf(val4.x - max_val), __expf(val4.y - max_val),
+                            __expf(val4.z - max_val), __expf(val4.w - max_val));
+  float* tmp4p = reinterpret_cast<float*>(&tmp4);
+  float invsum = 1.f / (math::warpReduceSum<float>(
+                            tmp4.x + tmp4.y + tmp4.z + tmp4.w, 0xffffffff) +
+                        1e-6f);
+  for (int i = 0; i < VPT; ++i) {
+    bufp[i] = static_cast<T>(tmp4p[i] * invsum);
+  }
+  reinterpret_cast<VECT*>(&dst[offset + idx])[0] = buf;
+}
+
 template <typename T>
 class SoftmaxCUDNNKernel : public framework::OpKernel<T> {
  public:
@@ -54,20 +83,42 @@ class SoftmaxCUDNNKernel : public framework::OpKernel<T> {
     const int N = SizeToAxis(axis, dims);
     const int D = SizeOutAxis(axis, dims);
 
-    ScopedTensorDescriptor desc;
-    std::vector<int> tensor_dims = {N, dim, D, 1};
-    DataLayout layout = DataLayout::kNCHW;
-    cudnnTensorDescriptor_t desc_ = desc.descriptor<T>(layout, tensor_dims);
+    if (D == 1 && dim == 128 && N % 4 == 0 && sizeof(T) <= 4) {
+      // a warp for a batch, 4 elements for a thread, only support the softmax
+      // dim size = 128 currently
+      constexpr int warps_per_block = 4;
+      if (sizeof(T) == 2) {
+        VecSoftmaxKernel<
+            T, int2, 4,
+            warps_per_block><<<N / warps_per_block, warps_per_block * WARP_SIZE,
+                               0, ctx.cuda_device_context().stream()>>>(
+            out_data, x->data<T>(), N, dim);
+      } else if (sizeof(T) == 4) {
+        VecSoftmaxKernel<
+            T, int4, 4,
+            warps_per_block><<<N / warps_per_block, warps_per_block * WARP_SIZE,
+                               0, ctx.cuda_device_context().stream()>>>(
+            out_data, x->data<T>(), N, dim);
+      } else {
+        assert(false && "not support");
+      }
+    } else {
+      ScopedTensorDescriptor desc;
+      std::vector<int> tensor_dims = {N, dim, D, 1};
+      DataLayout layout = DataLayout::kNCHW;
+      cudnnTensorDescriptor_t desc_ = desc.descriptor<T>(layout, tensor_dims);
 
-    auto& dev_ctx = ctx.template device_context<platform::CUDADeviceContext>();
-    auto handle = dev_ctx.cudnn_handle();
-    auto mode = axis == rank - 1 ? CUDNN_SOFTMAX_MODE_INSTANCE
-                                 : CUDNN_SOFTMAX_MODE_CHANNEL;
+      auto& dev_ctx =
+          ctx.template device_context<platform::CUDADeviceContext>();
+      auto handle = dev_ctx.cudnn_handle();
+      auto mode = axis == rank - 1 ? CUDNN_SOFTMAX_MODE_INSTANCE
+                                   : CUDNN_SOFTMAX_MODE_CHANNEL;
 
-    PADDLE_ENFORCE_CUDA_SUCCESS(platform::dynload::cudnnSoftmaxForward(
-        handle, CUDNN_SOFTMAX_ACCURATE, mode,
-        platform::CudnnDataType<T>::kOne(), desc_, x->data<T>(),
-        platform::CudnnDataType<T>::kZero(), desc_, out_data));
+      PADDLE_ENFORCE_CUDA_SUCCESS(platform::dynload::cudnnSoftmaxForward(
+          handle, CUDNN_SOFTMAX_ACCURATE, mode,
+          platform::CudnnDataType<T>::kOne(), desc_, x->data<T>(),
+          platform::CudnnDataType<T>::kZero(), desc_, out_data));
+    }
   }
 };
 
