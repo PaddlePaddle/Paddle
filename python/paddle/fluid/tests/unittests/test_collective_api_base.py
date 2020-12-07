469dd127d07f21bf28b7615025243f030eb9b748
sandyhouse <lilong12@baidu.com>
2020-12-07 17:55:52 +0800

    update, test=develop

--- python/paddle/fluid/tests/unittests/test_collective_api_base.py
+++ python/paddle/fluid/tests/unittests/test_collective_api_base.py
@@ -177,6 +177,7 @@ class TestDistBase(unittest.TestCase):
             "LD_LIBRARY_PATH": os.getenv("LD_LIBRARY_PATH", ""),
             "LD_PRELOAD": os.getenv("LD_PRELOAD", ""),
             "GLOG_v": "0",
+            "PADDLE_WITH_GLOO": "1",
             "NCCL_P2P_DISABLE": "1",
             "BACKEND": backend,
             "PATH_ID": path_id
