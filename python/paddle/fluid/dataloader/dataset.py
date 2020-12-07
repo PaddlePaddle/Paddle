97b6e87fd4e142d503ea409c30bbc786d33a1ecb
wujionghao <wujionghao@baidu.com>
2020-12-07 06:49:27 +0000

    polish example code

--- python/paddle/fluid/dataloader/dataset.py
+++ python/paddle/fluid/dataloader/dataset.py
@@ -416,9 +416,15 @@ class Subset(Dataset):
         import numpy as np
         from paddle.io import Subset
 
+        # example 1:
         a = paddle.io.Subset(dataset=range(1, 4), indices=[0, 2])
         print(list(a))
         # [1, 3]
+
+        # example 2:
+        b = paddle.io.Subset(dataset=range(1, 4), indices=[1, 1])
+        print(list(b))
+        # [2, 2]
     """
 
     def __init__(self, dataset, indices):
