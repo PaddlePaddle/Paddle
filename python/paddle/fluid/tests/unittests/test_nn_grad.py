0f2574783497e7f33f671ec0c805c03c149e26f9
lijianshe02 <jslict09@gmail.com>
2020-12-15 02:28:14 +0000

    fix conficts test=develop

--- python/paddle/fluid/tests/unittests/test_nn_grad.py
+++ python/paddle/fluid/tests/unittests/test_nn_grad.py
@@ -355,7 +355,6 @@ class TestTransposeDoubleGradCheck(unittest.TestCase):
     def func(self, place):
         x_shape = [3, 40]
         perm = [1, 0]
-        eps = 0.005
         dtype = np.float64
 
         x = layers.data('x', x_shape, False, dtype)
@@ -363,8 +362,29 @@ class TestTransposeDoubleGradCheck(unittest.TestCase):
         out = paddle.transpose(x, perm)
         x_arr = np.random.uniform(-1, 1, x_shape).astype(dtype)
 
-        gradient_checker.double_grad_check(
-            [x], out, x_init=x_arr, place=place, eps=eps)
+        gradient_checker.double_grad_check([x], out, x_init=x_arr, place=place)
+
+    def test_grad(self):
+        places = [fluid.CPUPlace()]
+        if core.is_compiled_with_cuda():
+            places.append(fluid.CUDAPlace(0))
+        for p in places:
+            self.func(p)
+
+
+class TestTransposeDoubleGradCheckCase1(unittest.TestCase):
+    @prog_scope()
+    def func(self, place):
+        x_shape = [2, 3, 4, 5]
+        perm = [0, 2, 3, 1]
+        dtype = np.float64
+
+        x = layers.data('x', x_shape, False, dtype)
+        x.persistable = True
+        out = paddle.transpose(x, perm)
+        x_arr = np.random.uniform(-1, 1, x_shape).astype(dtype)
+
+        gradient_checker.double_grad_check([x], out, x_init=x_arr, place=place)
 
     def test_grad(self):
         places = [fluid.CPUPlace()]
