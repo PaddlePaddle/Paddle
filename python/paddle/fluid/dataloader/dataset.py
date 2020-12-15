20473188ffe95d3f1ab329c9726f859fdde5f8ee
wujionghao <wujionghao@baidu.com>
2020-12-15 02:50:22 +0000

    fix example code

--- python/paddle/fluid/dataloader/dataset.py
+++ python/paddle/fluid/dataloader/dataset.py
@@ -561,8 +561,28 @@ class BufferedShuffleDataset(IterableDataset):
     
         .. code-block:: python
 
+            import numpy as np
+
             import paddle
-            from paddle.io import BufferedShuffleDataset
+            from paddle.io import BufferedShuffleDataset, Dataset
+
+                        
+            # define a random dataset
+            class RandomDataset(Dataset):
+                def __init__(self, num_samples):
+                    self.num_samples = num_samples
+            
+                def __getitem__(self, idx):
+                    image = np.random.random([784]).astype('float32')
+                    label = np.random.randint(0, 9, (1, )).astype('int64')
+                    return image, label
+                
+                def __len__(self):
+                    return self.num_samples
+            
+            dataset = RandomDataset(10)
+            for i in range(len(dataset)):
+                print(dataset[i])
 
             # Example 1:
             # For single-process mode (:attr:`num_workers == 0`), the random seed is required to
