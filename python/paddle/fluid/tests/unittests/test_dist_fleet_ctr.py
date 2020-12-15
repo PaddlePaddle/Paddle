05558e2563ae64a9079634d803b62d8f13cbcf86
seiriosPlus <tangwei12@baidu.com>
2020-12-15 15:24:31 +0800

    fix ut
    Change-Id: If26406883ad8fe09e393bf6f4f881afacf288aba

--- python/paddle/fluid/tests/unittests/test_dist_fleet_ctr.py
+++ python/paddle/fluid/tests/unittests/test_dist_fleet_ctr.py
@@ -116,7 +116,7 @@ class TestDistMnistAsync2x2(TestFleetBase):
             "dist_fleet_ctr.py", delta=1e-5, check_error_log=True)
 
 
-@unittest.skip(reason="Skip unstable ut, reader need to be rewrite")
+# @unittest.skip(reason="Skip unstable ut, reader need to be rewrite")
 class TestDistMnistAsyncDataset2x2(TestFleetBase):
     def _setup_config(self):
         self._mode = "async"
