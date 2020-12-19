f3ddbbad0c6a5990401cc071dd4fba092c66d18c
chalsliu <craziecharles@gmail.com>
2020-12-19 21:09:02 +0800

    fix

--- tools/check_added_ut.sh
+++ tools/check_added_ut.sh
@@ -14,7 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set +e
+set -x
 if [ -z ${BRANCH} ]; then
     BRANCH="develop"
 fi
