2ec3c1d279bb9e835d227759003ee7b16a38700a
qili93 <qili93@qq.com>
2020-12-21 10:05:59 +0800

    [ROCM] fix code stype failure, test=document_fix

--- tools/dockerfile/rocm_dev.sh
+++ tools/dockerfile/rocm_dev.sh
@@ -2,7 +2,7 @@
 
 
 function rocm() {
-  # # ROCM 3.3 - not work as rocthrust build fail without AMD GPU
+  # ROCM 3.3 - not work as rocthrust build fail without AMD GPU
   # sed 's#<rocm_repo_version>#3.3#g'  Dockerfile.rocm >test/rocm33.dockerfile
   # sed -ri 's#<rocprim_version>#3.3.0#g' test/rocm33.dockerfile
   # sed -ri 's#<rocthrust_version>#3.3.0#g' test/rocm33.dockerfile
