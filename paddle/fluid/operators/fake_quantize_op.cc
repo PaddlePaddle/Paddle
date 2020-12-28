e63f589c346b53f32f721f02398bcf0c78e0d1d8
cc <52520497+juncaipeng@users.noreply.github.com>
2020-12-28 14:06:23 +0800

    add op version for fake_quant and fake_dequant ops, test=op_version (#29923)
    * add op version for fake_quant and fake_dequant ops, test=op_version, test=develop

--- paddle/fluid/operators/fake_quantize_op.cc
+++ paddle/fluid/operators/fake_quantize_op.cc
@@ -16,6 +16,7 @@ limitations under the License. */
 #include <algorithm>
 #include <string>
 #include "paddle/fluid/framework/eigen.h"
+#include "paddle/fluid/framework/op_version_registry.h"
 #include "paddle/fluid/operators/clip_op.h"
 #include "paddle/fluid/platform/transform.h"
 
@@ -805,3 +806,10 @@ REGISTER_OPERATOR(fake_channel_wise_quantize_dequantize_abs_max,
 REGISTER_OP_CPU_KERNEL(
     fake_channel_wise_quantize_dequantize_abs_max,
     ops::FakeChannelWiseQuantizeDequantizeAbsMaxKernel<CPU, float>);
+
+REGISTER_OP_VERSION(fake_channel_wise_quantize_abs_max)
+    .AddCheckpoint(
+        R"ROC(add new attributes [quant_axis] for applying per-channel "
+        "quantization to conv2d_tranpose and mul ops.)ROC",
+        paddle::framework::compatible::OpVersionDesc().NewAttr(
+            "quant_axis", "The axis for quantization.", 0));
