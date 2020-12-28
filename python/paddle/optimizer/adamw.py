d3a7e2caf7dadfd8b880afca203ce042c184b77b
Leo Chen <chenqiuliang@baidu.com>
2020-12-28 10:28:23 +0800

    fix optimizer dtype (#29917)

--- python/paddle/optimizer/adamw.py
+++ python/paddle/optimizer/adamw.py
@@ -210,7 +210,6 @@ class AdamW(Adam):
     @framework.dygraph_only
     @imperative_base.no_grad
     def step(self):
-        self._dtype = None
         params_grads = []
         for param in self._parameter_list:
             if not param.trainable:
