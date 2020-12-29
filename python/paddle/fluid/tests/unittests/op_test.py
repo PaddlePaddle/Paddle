91ebc4608139a44521b133a3783a20da76502876
Chen Weihang <chenweihang@baidu.com>
2020-12-29 06:17:30 -0600

    [Cherry-pick] Complex network execute support (#29905)
    * [Complex] Add support for complex grad accumulated (#29889)
    
    * add support for complex grad accumulated
    
    * add unittest for coverage
    
    * update test dtype
    
    * remove useless blank line
    
    * [Complex] Handle complex to real after type promotion (#29855)
    
    * try to add fwd op input dtypes
    
    * refactor base impl
    
    * return tmp_ins after dygraph prepare data
    
    * fix typo found in debug
    
    * polish comment & add complex net test
    
    * revert detail change
    
    * fix unittest failed
    
    * add complex kernel condition control
    
    * fix xpu test failed & polish comment
    
    * polish details by review comments
    
    * Complex op test (#29753)
    
    * delete no need to calculate inputs in dygraph op_test
    
    * delete no need to calculate inputs in dygraph op_test
    
    * change grad elementwise_mul for complex types (#29757)
    
    * add conj op for complex types
    
    * add conj for complex types
    
    * add more test case
    
    * add conj_op test
    
    * modify conj api and impl
    
    * add complex type for fill_constant_op xpu
    
    * add setConstant for complex type
    
    * remove complex conj test file
    
    * user define grad for test_conj_op
    
    * add test case for static mode of conj api
    
    * modify conj doc
    
    * change input args name to x
    
    * remove useless codes
    
    * conj support real types
    
    * add conj test case for real number
    
    * delete no need to calculate inputs in dygraph op_test
    
    * delete no need to calculate inputs in dygraph op_test
    
    * modify grad of mul for complex types
    
    * fix the grads of inputs args order not match bug
    
    * change the grad of div when complex types (#29804)
    
    * change the grad of div when complex types
    
    * fix the grads of inputs args order not match bug
    
    Co-authored-by: chentianyu03 <chentianyu03@baidu.com>

--- python/paddle/fluid/tests/unittests/op_test.py
+++ python/paddle/fluid/tests/unittests/op_test.py
@@ -855,7 +855,7 @@ class OpTest(unittest.TestCase):
                                place,
                                no_check_set=None,
                                inplace_atol=None):
-        """Chech the inplace correctness of given op (self.op_type).
+        """Check the inplace correctness of given op (self.op_type).
         Run the op twice with same inputs, one enable inplace and another disable, compare their outputs.
 
         Args:
@@ -935,7 +935,7 @@ class OpTest(unittest.TestCase):
                             fwd_res,
                             grad_op_desc,
                             inplace_atol=None):
-        """Chech the inplace correctness of given grad_op_desc.
+        """Check the inplace correctness of given grad_op_desc.
 
         Run the grad op twice with same inputs, one enable inplace and another disable, compare their outputs.
         It works like _check_forward_inplace, but the way to construct program and feed_map differs.
@@ -1291,7 +1291,6 @@ class OpTest(unittest.TestCase):
 
     def _assert_is_close(self, numeric_grads, analytic_grads, names,
                          max_relative_error, msg_prefix):
-
         for a, b, name in six.moves.zip(numeric_grads, analytic_grads, names):
             # It asserts np.abs(a - b) / np.abs(a) < max_relative_error, in which
             # max_relative_error is 1e-7. According to the value of np.abs(a), we
@@ -1544,6 +1543,10 @@ class OpTest(unittest.TestCase):
                 grad_outputs = []
                 for grad_out_value in user_defined_grad_outputs:
                     grad_outputs.append(paddle.to_tensor(grad_out_value))
+                # delete the inputs which no need to calculate grad
+                for no_grad_val in no_grad_set:
+                    del (inputs[no_grad_val])
+
                 grad_inputs = paddle.grad(
                     outputs=fluid.layers.utils.flatten(outputs),
                     inputs=fluid.layers.utils.flatten(inputs),
@@ -1612,7 +1615,7 @@ class OpTest(unittest.TestCase):
             targets = [
                 outputs[name] for name in outputs if name in output_names
             ]
-            inputs = [inputs[name] for name in inputs if name in input_to_check]
+            inputs = [inputs[name] for name in input_to_check if name in inputs]
             grad_inputs = paddle.static.gradients(targets, inputs, grad_outputs,
                                                   no_grad_set)
             fetch_list = grad_inputs
