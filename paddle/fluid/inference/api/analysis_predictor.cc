6537bf9c92c3af091ec0ff19a633c748395d3e1c
Reese Wang <rewang@nvidia.com>
2020-12-31 15:06:22 +0800

    tensorrt nearest_interp converter

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -1101,6 +1101,7 @@ USE_TRT_CONVERTER(slice);
 USE_TRT_CONVERTER(scale);
 USE_TRT_CONVERTER(stack);
 USE_TRT_CONVERTER(clip);
+USE_TRT_CONVERTER(nearest_interp);
 #endif
 
 namespace paddle_infer {
