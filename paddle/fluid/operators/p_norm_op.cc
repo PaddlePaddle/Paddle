bf8639cbf77f9f600936e0a03b08cf81385858db
yongqiangma <xing.wo@163.com>
2020-12-31 07:44:41 +0000

    p_norm fix op version info. test=develop

--- paddle/fluid/operators/p_norm_op.cc
+++ paddle/fluid/operators/p_norm_op.cc
@@ -16,6 +16,7 @@ limitations under the License. */
 #include <memory>
 #include <string>
 #include <vector>
+#include "paddle/fluid/framework/op_version_registry.h"
 
 namespace paddle {
 namespace operators {
