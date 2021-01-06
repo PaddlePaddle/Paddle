162fe58da439c8675fe85138a6ea75fa0f1c19d4
zhhsplendid <zhhsplendid@163.com>
2021-01-06 06:22:28 +0000

    Make AsyncVariable::GetMutable Behave Like a Setter, test=develop

--- paddle/fluid/framework/async_variable.h
+++ paddle/fluid/framework/async_variable.h
@@ -18,6 +18,7 @@
 #include <thread>  // NOLINT
 #include <utility>
 
+#include "paddle/fluid/framework/var_type_traits.h"
 #include "paddle/fluid/platform/enforce.h"
 
 #pragma once
@@ -25,6 +26,9 @@
 namespace paddle {
 namespace framework {
 
+/**
+ * Variable class for asynchronized ops.
+ **/
 class AsyncVariable {
  public:
   AsyncVariable();
@@ -39,34 +43,39 @@ class AsyncVariable {
       std::unique_lock<std::mutex> lock(mutex_);
       cv_.wait(lock, [this] { return state_ == EnumState::kAvailable; });
     }
-    return *static_cast<const T*>(holder_);
+    return *static_cast<const T*>(holder_->Ptr());
   }
 
   template <typename T>
   T* GetMutable() {
+    std::lock_guard<std::mutex> lock(mutex_);
     if (state_ != EnumState::kAvailable) {
-      std::unique_lock<std::mutex> lock(mutex_);
-      cv_.wait(lock, [this] { return state_ == EnumState::kAvailable; });
+      state_ = EnumState::kAvailable;
+      holder_.reset(new PlaceholderImpl<T>());
+    } else {
+      PADDLE_ENFORCE_EQ(
+          holder_->Type(), VarTypeTrait<T>::kId,
+          platform::errors::InvalidArgument(
+              "The Variable type must be %s, but the type it holds is %s.",
+              ToTypeName(VarTypeTrait<T>::kId), ToTypeName(holder_->Type())));
     }
-    return static_cast<T*>(holder_);
+    cv_.notify_all();
+    return static_cast<T*>(holder_->Ptr());
   }
 
-  template <typename T, typename... Args>
-  void Emplace(Args&&... args) {
-    PADDLE_ENFORCE_NE(
-        state_, EnumState::kAvailable,
+  template <typename T>
+  void Emplace(T&& arg) {
+    PADDLE_ENFORCE_EQ(
+        state_, EnumState::kNotAvailable,
         platform::errors::Unimplemented("Emplace AsyncVariable for multiple "
                                         "times is not implemented now."));
-    if (state_ == EnumState::kNotAvailable) {
-      {
-        std::lock_guard<std::mutex> lock(mutex_);
-        state_ = EnumState::kAvailable;
-        holder_ = new T(std::forward<Args>(args)...);
-      }
-      cv_.notify_all();
-      return;
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      state_ = EnumState::kAvailable;
+      holder_.reset(new PlaceholderImpl<T>(arg));
     }
-    holder_ = new T(std::forward<Args>(args)...);
+    cv_.notify_all();
+    return;
   }
 
   template <typename WaiterT>
@@ -87,8 +96,44 @@ class AsyncVariable {
   };
 
  private:
+  // class to hold data member for AsyncVariable
+  class Placeholder {
+   public:
+    virtual ~Placeholder() PADDLE_MAY_THROW {}
+
+    inline int Type() const { return type_; }
+    inline const void* Ptr() const { return ptr_; }
+    inline void* Ptr() { return ptr_; }
+
+   protected:
+    inline void Init(void* p, int type) {
+      ptr_ = p;
+      type_ = type;
+    }
+
+    void* ptr_;
+    int type_;
+  };
+
+  // Placeholder hides type T, so it doesn't appear as a template
+  // parameter of Variable.
+  template <typename T>
+  class PlaceholderImpl : public Placeholder {
+   public:
+    static_assert(
+        IsRegisteredVarType<T>(),
+        "Not registered type. Please register T inside var_type_traits.h");
+    PlaceholderImpl() { this->Init(&obj_, VarTypeTrait<T>::kId); }
+    explicit PlaceholderImpl(const T& obj) : obj_(obj) {
+      this->Init(&obj_, VarTypeTrait<T>::kId);
+    }
+
+   private:
+    T obj_;
+  };
+
   // Holder for data member
-  void* holder_;
+  std::shared_ptr<Placeholder> holder_;
   // Enum state of the current AsyncVariable
   EnumState state_;
   // Condition variable used for wait-notify
