9e2edc674d86c4e58729a0cf66d8e9444ca8ed62
LDOUBLEV <liuvv0203@outlook.com>
2021-01-06 13:25:27 +0000

    fix unitest,test=develop

--- python/paddle/fluid/tests/unittests/test_activation_nn_grad.py
+++ python/paddle/fluid/tests/unittests/test_activation_nn_grad.py
@@ -78,7 +78,7 @@ class TestLeakyReluDoubleGradCheck(unittest.TestCase):
 class TestELUDoubleGradCheck(unittest.TestCase):
     @prog_scope()
     def func(self, place):
-        shape = [2, 3, 4, 4]
+        shape = [2, 4, 4, 4]
         eps = 1e-6
         alpha = 0.2
         dtype = np.float64
