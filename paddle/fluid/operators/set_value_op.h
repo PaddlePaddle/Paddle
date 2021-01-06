fc9bc691b154cb361288e944ee6bfb8495d44cbc
liym27 <liym0923@126.com>
2021-01-06 13:13:08 +0800

    check rank in kernel of set_value op

--- paddle/fluid/operators/set_value_op.h
+++ paddle/fluid/operators/set_value_op.h
@@ -83,7 +83,11 @@ template <typename DeviceContext, typename T>
 class SetValueKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const {
-    const int rank = ctx.Output<framework::LoDTensor>("Out")->dims().size();
+    const int rank = ctx.Input<framework::LoDTensor>("Input")->dims().size();
+    PADDLE_ENFORCE_LT(
+        rank, 7,
+        platform::errors::InvalidArgument(
+            "The rank of input should be less than 7, but received %d.", rank));
 
     // TODO(liym27): A more elegent code to do this. C++ has to make template
     //  integer as constant, but we had better have alternative writing in the
