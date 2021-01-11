70cbde83b3ef96c70e8e5cf7ec7c4bd1bbad41bd
XiaoguangHu <46782768+XiaoguangHu01@users.noreply.github.com>
2021-01-10 20:28:15 -0600

    [cherry-pick] clean redundant API alias in 2.0 - part 2 (#30244)
    * fix dynamic to static error
    
    * delete paddle.nn.functional.assign

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -563,9 +563,9 @@ def assign(input, output=None):
                             [3, 4],
                             [1, 3]]).astype(np.int64)
           result1 = paddle.zeros(shape=[3, 3], dtype='float32')
-          paddle.nn.functional.assign(array, result1) # result1 = [[1, 1], [3 4], [1, 3]]
-          result2 = paddle.nn.functional.assign(data)  # result2 = [[2.5, 2.5], [2.5, 2.5], [2.5, 2.5]]
-          result3 = paddle.nn.functional.assign(np.array([[2.5, 2.5], [2.5, 2.5], [2.5, 2.5]], dtype='float32')) # result3 = [[2.5, 2.5], [2.5, 2.5], [2.5, 2.5]]
+          paddle.assign(array, result1) # result1 = [[1, 1], [3 4], [1, 3]]
+          result2 = paddle.assign(data)  # result2 = [[2.5, 2.5], [2.5, 2.5], [2.5, 2.5]]
+          result3 = paddle.assign(np.array([[2.5, 2.5], [2.5, 2.5], [2.5, 2.5]], dtype='float32')) # result3 = [[2.5, 2.5], [2.5, 2.5], [2.5, 2.5]]
     """
     helper = LayerHelper('assign', **locals())
     check_type(input, 'input', (Variable, numpy.ndarray), 'assign')
