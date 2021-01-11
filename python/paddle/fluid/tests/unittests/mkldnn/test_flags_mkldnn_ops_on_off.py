493d0e204e0b70b88c58f94146303aa127f819f1
Wojciech Uss <wojciech.uss@intel.com>
2021-01-11 05:50:41 +0100

    fix regex

--- python/paddle/fluid/tests/unittests/mkldnn/test_flags_mkldnn_ops_on_off.py
+++ python/paddle/fluid/tests/unittests/mkldnn/test_flags_mkldnn_ops_on_off.py
@@ -32,7 +32,7 @@ class TestFlagsUseMkldnn(unittest.TestCase):
         self.env[str("FLAGS_use_mkldnn")] = str("1")
 
         self.relu_regex = b"^dnnl_verbose,exec,cpu,eltwise,.+alg:eltwise_relu alpha:0 beta:0,10x20x20"
-        self.ew_add_regex = b"^dnnl_verbose,exec,cpu,binary.+alg:binary_add,10x20x30:10x20x30 10x20x30"
+        self.ew_add_regex = b"^dnnl_verbose,exec,cpu,binary.+alg:binary_add,10x20x30:10x20x30"
         self.matmul_regex = b"^dnnl_verbose,exec,cpu,matmul,.*10x20x30:10x30x20:10x20x20"
 
     def flags_use_mkl_dnn_common(self, e):
