3d52a3e99c686c3b76a48daf025e5b7349562f38
swtkiwi <1208425345@qq.com>
2021-01-12 14:46:10 +0800

    test=develop

--- paddle/fluid/operators/softmax_with_cross_entropy_op.cc
+++ paddle/fluid/operators/softmax_with_cross_entropy_op.cc
@@ -141,7 +141,7 @@ class SoftmaxWithCrossEntropyOp : public framework::OperatorWithKernel {
         if (ctx->IsRuntime() || (logits_dims[i] > 0 && labels_dims[i] > 0)) {
           PADDLE_ENFORCE_EQ(logits_dims[i], labels_dims[i],
                             platform::errors::InvalidArgument(
-                                "The dims of Input(Logits) should be the same as Input(Label).  "
+                                "The dimension of Input(Logits) should be the same as Input(Label).  "
                                 "But recieved Logits dims: [%s], Label dims: [%s].",
                                 logits_dims[i],labels_dims[i]));
         }
