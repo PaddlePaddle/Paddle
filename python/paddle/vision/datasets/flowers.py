452513fbec31e98b0d73f189df219ce7d0f599a1
liuhui29 <liuhui29@baidu.com>
2021-01-13 21:53:21 +0800

    update document of paddle.vision.dataset, test=document

--- python/paddle/vision/datasets/flowers.py
+++ python/paddle/vision/datasets/flowers.py
@@ -47,15 +47,14 @@ class Flowers(Dataset):
 
     Args:
         data_file(str): path to data file, can be set None if
-            :attr:`download` is True. Default None
+            :attr:`download` is True. Default None, default data path: ~/.cache/paddle/dataset/flowers/
         label_file(str): path to label file, can be set None if
             :attr:`download` is True. Default None
         setid_file(str): path to subset index file, can be set
             None if :attr:`download` is True. Default None
         mode(str): 'train', 'valid' or 'test' mode. Default 'train'.
-        transform(callable): transform to perform on image, None for on transform.
-        download(bool): whether to download dataset automatically if
-            :attr:`data_file` is not set. Default True
+        transform(callable): transform to perform on image, None for no transform.
+        download(bool): download dataset automatically if :attr:`data_file` is None. Default True
         backend(str, optional): Specifies which type of image to be returned: 
             PIL.Image or numpy.ndarray. Should be one of {'pil', 'cv2'}. 
             If this option is not set, will get backend from ``paddle.vsion.get_image_backend`` ,
