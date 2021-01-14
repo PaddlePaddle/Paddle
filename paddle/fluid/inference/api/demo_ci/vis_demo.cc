1b9ead8331ff68fde277323dbd5aabd9ead35f17
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2021-01-14 13:33:04 +0000

    export global google flags to users, test=develop

--- paddle/fluid/inference/api/demo_ci/vis_demo.cc
+++ paddle/fluid/inference/api/demo_ci/vis_demo.cc
@@ -82,7 +82,7 @@ void Main(bool use_gpu) {
 }  // namespace paddle
 
 int main(int argc, char** argv) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
+  ::GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
   if (FLAGS_use_gpu) {
     paddle::demo::Main(true /*use_gpu*/);
   } else {
