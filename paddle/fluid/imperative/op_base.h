0fa088fda94d7d85dc70801011465f8365aaaef6
pangyoki <pangyoki@126.com>
2021-01-14 17:11:32 +0000

    fix head file error

--- paddle/fluid/imperative/op_base.h
+++ paddle/fluid/imperative/op_base.h
@@ -21,6 +21,7 @@
 #include <utility>
 #include <vector>
 #include "paddle/fluid/framework/type_defs.h"
+#include "paddle/fluid/imperative/saved_variable_wrapper_list.h"
 #include "paddle/fluid/imperative/type_defs.h"
 #include "paddle/fluid/imperative/variable_wrapper.h"
 #include "paddle/fluid/platform/place.h"
