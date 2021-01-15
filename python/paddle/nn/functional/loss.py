f7fa30a782c89d63adfb10484ccba87e233037d6
pangyoki <pangyoki@126.com>
2021-01-15 11:19:07 +0000

    save reshape update v2 in pr 30180

--- python/paddle/nn/functional/loss.py
+++ python/paddle/nn/functional/loss.py
@@ -1226,7 +1226,7 @@ def cross_entropy(input,
             weight_gather = core.ops.gather_nd(
                 weight, label)  #trans weight from class to sample, shape:N
             input_shape = list(label.shape)
-            weight_gather_reshape, _ = reshape(weight_gather, shape=input_shape)
+            weight_gather_reshape = reshape(weight_gather, shape=input_shape)
             out = core.ops.elementwise_mul(out, weight_gather_reshape)
 
         if reduction == "sum":
