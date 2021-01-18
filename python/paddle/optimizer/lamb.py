2195c20e5006276226855ac75be8232df3151d3b
gfwm2013 <2449465946@qq.com>
2021-01-18 03:17:49 +0000

    Fix the document errors of lamb

--- python/paddle/optimizer/lamb.py
+++ python/paddle/optimizer/lamb.py
@@ -37,9 +37,9 @@ class Lamb(Optimizer):
 
         v_t &= \\beta_2 v_{t - 1}  + (1 - \\beta_2)g_t^2
 
-        m_t &= \\frac{m_t}{\\beta_1^t}
+        m_t &= \\frac{m_t}{1 - \\beta_1^t}
 
-        v_t &= \\frac{v_t}{\\beta_2^t}
+        v_t &= \\frac{v_t}{1 - \\beta_2^t}
 
         r_t &= \\frac{m_t}{\\sqrt{v_t}+\\epsilon}
 
@@ -66,6 +66,9 @@ class Lamb(Optimizer):
             ( :ref:`api_paddle_fluid_clip_ClipGradByGlobalNorm` , :ref:`api_paddle_fluid_clip_ClipGradByNorm` ,
             :ref:`api_paddle_fluid_clip_ClipGradByValue` ). If you want better convergence, it is recommended
             to use :ref:`api_paddle_fluid_clip_ClipGradByGlobalNorm` . Default None, meaning there is no gradient clipping.
+        exclude_from_weight_decay_fn (function|None): Exclude a parameter from weight 
+            decay when **exclude_from_weight_decay_fn(parameter)** returns true. 
+            Default None.
         name(str|None): For detailed information, please refer to
             :ref:`api_guide_Name` . Usually name is no need to set and None by default.
     Examples:
