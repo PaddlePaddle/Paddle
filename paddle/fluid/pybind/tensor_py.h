5bea9b907e4e9fba087f4e485c0e17bf4f8c3ff8
Wang Huan <wanghuan29@baidu.com>
2021-01-19 07:27:54 +0000

    fix, test=develop

--- paddle/fluid/pybind/tensor_py.h
+++ paddle/fluid/pybind/tensor_py.h
@@ -292,10 +292,10 @@ void SetTensorFromPyArrayT(
     if (paddle::platform::is_gpu_place(place)) {
       // TODO(zhiqiu): set SetDeviceId before calling cuda APIs.
       auto dst = self->mutable_data<T>(place);
-      // paddle::memory::Copy(place, dst, platform::CPUPlace(), array.data(),
-      //                      array.nbytes());
-      paddle::platform::GpuMemcpySync(dst, array.data(), array.nbytes(),
-                                            cudaMemcpyHostToDevice);
+      auto p = BOOST_GET_CONST(platform::CUDAPlace, place);
+      paddle::memory::Copy(p, dst, platform::CPUPlace(), array.data(),
+                           array.nbytes(), nullptr);
+
     } else if (paddle::platform::is_cuda_pinned_place(place)) {
       auto dst = self->mutable_data<T>(place);
       std::memcpy(dst, array.data(), array.nbytes());
@@ -709,11 +709,9 @@ inline py::array TensorToPyArray(const framework::Tensor &tensor,
             "or double free would occur"));
 
     size_t copy_bytes = sizeof_dtype * numel;
-    // auto p = BOOST_GET_CONST(platform::CUDAPlace, tensor.place());
-    // paddle::memory::Copy(platform::CPUPlace(), py_arr.mutable_data(), p,
-    //                      tensor_buf_ptr, copy_bytes);
-    paddle::platform::GpuMemcpySync(py_arr.mutable_data(), tensor_buf_ptr,
-                                        copy_bytes, cudaMemcpyDeviceToHost);
+    auto p = BOOST_GET_CONST(platform::CUDAPlace, tensor.place());
+    paddle::memory::Copy(platform::CPUPlace(), py_arr.mutable_data(), p,
+                         tensor_buf_ptr, copy_bytes, nullptr);
     return py_arr;
 #else
     PADDLE_THROW(platform::errors::PermissionDenied(
