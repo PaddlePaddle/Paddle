287bf2fdd1caff00e7e53f9334c746c38f68ea67
gongweibao <gongweibao@gmail.com>
2021-01-21 21:37:45 +0800

    add

--- python/paddle/fluid/tests/unittests/test_ascend_group.py
+++ python/paddle/fluid/tests/unittests/test_ascend_group.py
@@ -15,15 +15,26 @@
 import os
 import sys
 import time
+import paddle.fluid as fluid
+from paddle.fluid import unique_name
+import paddle.fluid.core as core
+import paddle
+from paddle.fluid.layer_helper import LayerHelper
 
-def init_communicator(program, main_program, current_endpoint, endpoints, ring_id):
+paddle.enable_static()
+
+OpRole = core.op_proto_and_checker_maker.OpRole
+OP_ROLE_KEY = core.op_proto_and_checker_maker.kOpRoleAttrName()
+OP_ROLE_VAR_KEY = core.op_proto_and_checker_maker.kOpRoleVarAttrName()
+
+def init_communicator(startup_program, main_program, current_endpoint, endpoints, ring_id):
     nranks = len(endpoints)
     other_endpoints = endpoints[:]
     other_endpoints.remove(current_endpoint)
     group_rank=endpoints.index(current_endpoint)
     assert group_rank >=0
 
-    block = program.global_block()
+    block = startup_program.global_block()
     nccl_id_var = block.create_var(
         name=unique_name.generate('nccl_id'),
         persistable=True,
@@ -54,7 +65,9 @@ def init_communicator(program, main_program, current_endpoint, endpoints, ring_i
         dtype='float32')
 
     with fluid.program_guard(main_program):
-        data=fluid.layers.fill_constant(shape=[1, positive_2], dtype='float32', value=2.5)
+        op_type="c_allreduce_sum"
+        data=fluid.layers.fill_constant(shape=[1], dtype='float32', value=2.5)
+        helper = LayerHelper(op_type, **locals())
         helper.append_op(
             type=op_type,
             inputs={'X': [data]},
@@ -66,20 +79,26 @@ def train():
     startup_programs=[]
     main_programs=[]
 
+
     trainer_endpoints=["127.0.0.1:6071","127.0.0.1:6072","127.0.0.1:6073","127.0.0.1:6074"]
-    groups=[]
+    groups=[[], [], []]
     groups[0]=[trainer_endpoints[0], trainer_endpoints[1]]
     groups[1]=[trainer_endpoints[2], trainer_endpoints[3]]
-    groups[3]=[trainer_endpoints[0], trainer_endpoints[2]]
+    groups[2]=[trainer_endpoints[0], trainer_endpoints[2]]
+
+    for i in range(len(trainer_endpoints)):
+        startup_programs.append(fluid.Program())
+        main_programs.append(fluid.Program())
 
     for idx, group in enumerate(groups):
         for te in group:
-            startup_program = fluid.program()
-            init_communicator(startup_program, main_program, te, group, str(idx))
-            startup_programs.append(startup_program)
-            main_programs.append(main_program)
+            te_idx = trainer_endpoints.index(te)
+            startup_program = startup_programs[te_idx]
+            main_program=main_programs[te_idx]
+            init_communicator(startup_program, main_program, te, group, idx)
 
-    print(startup_program[0])
-    print(main_program[0])
+    print(len(startup_programs))
+    print(startup_programs[0])
+    print(main_programs[0])
 
-
+train()
