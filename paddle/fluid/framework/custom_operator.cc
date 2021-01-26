fdbe86bc8ea606d588140ccfab5d088717c952b0
Chen Weihang <chenweihang@baidu.com>
2021-01-26 07:34:26 +0000

    add grap op simple demo

--- paddle/fluid/framework/custom_operator.cc
+++ paddle/fluid/framework/custom_operator.cc
@@ -16,8 +16,11 @@ limitations under the License. */
 
 #include <functional>
 #include <iostream>
+#include <map>
 #include <string>
 #include <tuple>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -53,18 +56,49 @@ static T* DynLoad(void* handle, std::string name) {
 
 // custom op kernel define
 template <typename Func>
-static void CallKernelFunc(const framework::ExecutionContext& ctx,
-                           Func&& func) {
+static void CallKernelFunc(const framework::ExecutionContext& ctx, Func&& func);
+
+template <>
+void CallKernelFunc<const std::function<std::vector<Tensor>(const Tensor&)>&>(
+    const framework::ExecutionContext& ctx,
+    const std::function<std::vector<Tensor>(const Tensor&)>& func) {
   VLOG(0) << "start run in CallKernelFunc";
   auto* x = ctx.Input<Tensor>(detail::kCustomOpInputPrefix + std::to_string(0));
   PADDLE_ENFORCE_NOT_NULL(x, "input x is nullptr.");
   PADDLE_ENFORCE(x->IsInitialized(), "input x is not initialized.");
 
-  VLOG(0) << "run func in CallKernelFunc";
+  VLOG(0) << "run forward func in CallKernelFunc";
   auto outs = func(*x);
 
   VLOG(0) << "share output in CallKernelFunc";
   auto true_outs = ctx.MultiOutput<Tensor>(detail::kCustomOpOutputPrefix);
+  for (size_t i = 0; i < true_outs.size(); ++i) {
+    (true_outs)[i]->ShareDataWith(outs.at(i));
+  }
+}
+
+template <>
+void CallKernelFunc<const std::function<
+    std::vector<Tensor>(const Tensor&, const Tensor&, const Tensor&)>&>(
+    const framework::ExecutionContext& ctx,
+    const std::function<std::vector<Tensor>(const Tensor&, const Tensor&,
+                                            const Tensor&)>& func) {
+  std::vector<const Tensor*> ins;
+  for (auto name : ctx.InNameList()) {
+    VLOG(0) << "input name: " << name;
+    auto* x = ctx.Input<Tensor>(name);
+    PADDLE_ENFORCE_NOT_NULL(x, "input %s is nullptr.", name);
+    PADDLE_ENFORCE(x->IsInitialized(), "input %s is not initialized.", name);
+    ins.emplace_back(x);
+  }
+
+  VLOG(0) << "run forward func in CallKernelFunc";
+  auto outs = func(*ins[0], *ins[1], *ins[2]);
+
+  VLOG(0) << "share output in CallKernelFunc";
+  auto out_names = ctx.OutNameList();
+  PADDLE_ENFORCE_EQ(out_names.size(), 1, "only can hold 1 out in custom op.");
+  auto true_outs = ctx.MultiOutput<Tensor>(out_names[0]);
   for (size_t i = 0; i < true_outs.size(); ++i) {
     (true_outs)[i]->ShareDataWith(outs.at(i));
   }
@@ -112,7 +146,17 @@ class CustomOpMaker : public OpProtoAndCheckerMaker {
   size_t input_num_;
 };
 
+class CustomGradOperator : public OperatorWithKernel {
+ public:
+  using OperatorWithKernel::OperatorWithKernel;
+
+  void InferShape(framework::InferShapeContext* ctx) const override {
+    VLOG(0) << "Infer shape of custom grad operator.";
+  }
+};
+
 void RegisterOperator(const std::string& name, size_t input_num) {
+  /* Op register */
   OpInfo info;
 
   // Op
@@ -121,6 +165,11 @@ void RegisterOperator(const std::string& name, size_t input_num) {
                      const AttributeMap& attrs) {
     return new CustomOperator(type, inputs, outputs, attrs);
   };
+
+  // InferShape
+  OperatorWithKernel* op = dynamic_cast<OperatorWithKernel*>(info.creator_(
+      std::string{}, VariableNameMap{}, VariableNameMap{}, AttributeMap{}));
+  info.infer_shape_ = [op](InferShapeContext* ctx) { op->InferShape(ctx); };
 
   // OpMaker
   info.proto_ = new proto::OpProto;
@@ -135,16 +184,58 @@ void RegisterOperator(const std::string& name, size_t input_num) {
           "Fail to initialize %s's OpProto, because %s is not initialized.",
           name, info.proto_->InitializationErrorString()));
 
-  // GradOpMaker
+  // GradOpDescMaker
+  info.grad_op_maker_ = [](
+      const OpDesc& fwd_op, const std::unordered_set<std::string>& no_grad_set,
+      std::unordered_map<std::string, std::string>* grad_to_var,
+      const std::vector<BlockDesc*>& grad_block) {
+    DefaultGradOpMaker<paddle::framework::OpDesc, true> maker(
+        fwd_op, no_grad_set, grad_to_var, grad_block);
+    return maker();
+  };
+  // GradOpBaseMaker
+  info.dygraph_grad_op_maker_ = [](
+      const std::string& type,
+      const imperative::NameVarBaseMap& var_base_map_in,
+      const imperative::NameVarBaseMap& var_base_map_out,
+      const framework::AttributeMap& attrs,
+      const std::map<std::string, std::string>& inplace_map) {
+    DefaultGradOpMaker<paddle::imperative::OpBase, true> maker(
+        type, var_base_map_in, var_base_map_out, attrs, inplace_map);
+    return maker();
+  };
+  info.use_default_grad_op_desc_maker_ = true;
+
+  /* Grad op register */
+  OpInfo grad_info;
+
+  // Op
+  grad_info.creator_ = [](
+      const std::string& type, const VariableNameMap& inputs,
+      const VariableNameMap& outputs, const AttributeMap& attrs) {
+    return new CustomGradOperator(type, inputs, outputs, attrs);
+  };
+
+  // InferShape
+  OperatorWithKernel* grad_op =
+      dynamic_cast<OperatorWithKernel*>(grad_info.creator_(
+          std::string{}, VariableNameMap{}, VariableNameMap{}, AttributeMap{}));
+  grad_info.infer_shape_ = [grad_op](InferShapeContext* ctx) {
+    grad_op->InferShape(ctx);
+  };
+
+  // Last Step: insert
   OpInfoMap::Instance().Insert(name, info);
+  OpInfoMap::Instance().Insert(name + "_grad", grad_info);
 }
 
-void RegisterOperatorKernel(
-    const std::string& name,
-    const std::function<std::vector<Tensor>(const Tensor&)>& func) {
+template <typename Func>
+void RegisterOperatorKernel(const std::string& name, TensorFunction* func) {
+  // 0. unwrap func
+  Func true_func = std::move(func->UnWrap<Func>());
+
   // 1. construct kernel func
-  CustomOpKernel<float, std::function<std::vector<Tensor>(const Tensor&)>>
-      custom_kernel(func);
+  CustomOpKernel<float, Func> custom_kernel(true_func);
 
   // 2. insert kernel
   std::string library_type = "CPU";
@@ -174,17 +265,19 @@ void LoadCustomOperator(const std::string& dso_name) {
   VLOG(0) << "size of op funcs map: " << op_funcs.size();
   for (auto& pair : op_funcs) {
     VLOG(0) << "pair first - op name: " << pair.first;
-    // 1. forward op
+    // 1. op func info
     auto forward_in_num = pair.second.forward_in_num();
+    // auto backward_in_num = pair.second.backward_in_num();
     auto forward_func = pair.second.forward_func();
-    std::function<std::vector<Tensor>(const Tensor&)> func = std::move(
-        forward_func
-            .UnWrap<std::function<std::vector<Tensor>(const Tensor&)>>());
+    auto backward_func = pair.second.backward_func();
     // 2. register op
-    VLOG(0) << "forward_in_num: " << forward_in_num;
     RegisterOperator(pair.first, forward_in_num);
     // 3. register op kernel
-    RegisterOperatorKernel(pair.first, func);
+    RegisterOperatorKernel<std::function<std::vector<Tensor>(const Tensor&)>>(
+        pair.first, &forward_func);
+    RegisterOperatorKernel<std::function<std::vector<Tensor>(
+        const Tensor&, const Tensor&, const Tensor&)>>(pair.first + "_grad",
+                                                       &backward_func);
   }
 }
 
