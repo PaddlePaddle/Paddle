20cf1be861ee9438a0972d448a8e4b03adc37032
FrostML <380185688@qq.com>
2021-01-26 06:34:08 +0000

    update sample code, test=document_fix

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -15000,14 +15000,13 @@ def gather_tree(ids, parents):
         .. code-block:: python
 
             import paddle
-            import numpy as np
 
-            ids = paddle.to_tensor(np.asarray([[[2, 2], [6, 1]], [[3, 9], [6, 1]], [[0, 1], [9, 0]]]))
-    
-            parents = paddle.to_tensor(np.asarray([[[0, 0], [1, 1]], [[1, 0], [1, 0]], [[0, 0], [0, 1]]]))
-            
-            # [[[2, 2], [1, 6]], [[3, 3], [6, 1]], [[0, 1], [9, 0]]]
+            ids = paddle.to_tensor([[[2, 2], [6, 1]], [[3, 9], [6, 1]], [[0, 1], [9, 0]]])
+
+            parents = paddle.to_tensor([[[0, 0], [1, 1]], [[1, 0], [1, 0]], [[0, 0], [0, 1]]])
+
             final_sequences = paddle.nn.functional.gather_tree(ids, parents)
+            # [[[2, 2], [1, 6]], [[3, 3], [6, 1]], [[0, 1], [9, 0]]]
 
     """
     helper = LayerHelper('gather_tree', **locals())
