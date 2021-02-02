505349a4e6beeb4d1a02d7be6fb3ca0aa3948ec2
jiweibo <jiweibo@baidu.com>
2021-02-02 12:23:08 +0000

    update paddle_fluid.so to paddle_inference.so

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -126,11 +126,15 @@ GpuPassStrategy::GpuPassStrategy() : PassStrategy({}) {
         "fc_elementwise_layernorm_fuse_pass",        //
 #if CUDNN_VERSION >= 7100  // To run conv_fusion, the version of cudnn must be
                            // guaranteed at least v7
+// TODO(wilber): cudnn8 has memory leak problem in conv + eltwise + act, so we
+// disable the pass temporarily.
+#if CUDNN_VERSION < 8000
         "conv_elementwise_add_act_fuse_pass",   //
         "conv_elementwise_add2_act_fuse_pass",  //
-        "conv_elementwise_add_fuse_pass",       //
-#endif                                          //
-        "transpose_flatten_concat_fuse_pass",   //
+#endif
+        "conv_elementwise_add_fuse_pass",      //
+#endif                                         //
+        "transpose_flatten_concat_fuse_pass",  //
         // following pass should be located in the last, since it will
         // work on all fused ops.
         "runtime_context_cache_pass"
@@ -226,7 +230,7 @@ void CpuPassStrategy::EnableMKLDNN() {
              "batch_norm_act_fuse_pass",
              // TODO(intel): Please fix the bug on windows.
              // https://github.com/PaddlePaddle/Paddle/issues/29710
-             //"mkldnn_inplace_pass",  // This pass should be activated after
+             // "mkldnn_inplace_pass",  // This pass should be activated after
              // fuses. Disabled by default due to
              // little gain and lots of problems
          })) {
