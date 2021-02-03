c79158cf71ea6d4041531a470d071243c0e7994d
liuyuhui <liuyuhui@baidu.com>
2021-02-03 14:40:44 +0000

    add fleet api unittest for xpu

--- python/paddle/fluid/tests/unittests/test_parallel_dygraph_mnist.py
+++ python/paddle/fluid/tests/unittests/test_parallel_dygraph_mnist.py
@@ -71,13 +71,30 @@ class TestFleetDygraphMnist(TestDistBase):
         self._sync_mode = False
         self._nccl2_mode = True
         self._dygraph = True
-        self._gpu_fleet_api = True
+        self._device_fleet_api = True
 
     def test_mnist(self):
         if fluid.core.is_compiled_with_cuda():
             self.check_with_place(
                 "parallel_dygraph_mnist.py",
                 delta=1e-5,
+                check_error_log=True,
+                log_name=flag_name)
+
+
+class TestFleetDygraphMnistXPU(TestDistBase):
+    def _setup_config(self):
+        self._sync_mode = False
+        self._bkcl_mode = True
+        self._dygraph = True
+        self._enforce_place = "XPU"
+        self._device_fleet_api = True
+
+    def test_mnist(self):
+        if fluid.core.is_compiled_with_xpu():
+            self.check_with_place(
+                "parallel_dygraph_mnist.py",
+                delta=3,
                 check_error_log=True,
                 log_name=flag_name)
 
