30718e10297d105a6aecb99e99e746e9916a52c1
Wang Huan <wanghuan29@baidu.com>
2021-02-03 07:16:23 +0000

    refine, test=develop

--- paddle/fluid/framework/ir/transpose_flatten_concat_fuse_pass.cc
+++ paddle/fluid/framework/ir/transpose_flatten_concat_fuse_pass.cc
@@ -13,21 +13,7 @@
 // limitations under the License.
 
 #include "paddle/fluid/framework/ir/transpose_flatten_concat_fuse_pass.h"
-#include "paddle/fluid/framework/ir/graph_pattern_detector.h"
-#include "paddle/fluid/framework/ir/pass.h"
 #include "paddle/fluid/framework/op_version_registry.h"
-#include "paddle/fluid/platform/enforce.h"
-
-namespace paddle {
-namespace paddle {
-namespace framework {
-namespace ir {
-class Graph;
-class Node;
-}  // namespace ir
-}  // namespace framework
-}  // namespace paddle
-}  // namespace paddle
 
 namespace paddle {
 namespace framework {
