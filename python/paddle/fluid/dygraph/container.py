7f0df10f077cb31852e340fdc9dde9068d4dcb96
lyuwenyu <wenyu.lyu@gmail.com>
2021-02-05 15:51:21 +0800

    fix format error for py2

--- python/paddle/fluid/dygraph/container.py
+++ python/paddle/fluid/dygraph/container.py
@@ -82,7 +82,7 @@ class Sequential(Layer):
 
     def __setitem__(self, idx, layer): 
         if isinstance(idx, str):
-            setattr(self, str(idx), layer)
+            setattr(self, idx, layer)
         else:
             key = list(self._sub_layers.keys())[self._get_abs_index(idx)]
             setattr(self, key, layer)
