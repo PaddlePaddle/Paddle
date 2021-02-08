d3849dcdf8480ce8fd4e2a5dbb425bd51ffe9767
shenliang03 <shenliang03@baidu.com>
2021-02-09 00:14:51 +0800

    fix bckl

--- paddle/fluid/imperative/reducer.cc
+++ paddle/fluid/imperative/reducer.cc
@@ -576,6 +576,8 @@ void Reducer::MarkVarReady(const size_t var_index, const bool is_used_var) {
           {static_cast<int64_t>(length)});
     } else {
       if (!group_tensor.IsInitialized()) {
+        group_tensor.Resize({static_cast<int64_t>(length)});
+        group_tensor.mutable_data(place_, group.dtype_);
 #ifdef PADDLE_WITH_XPU_BKCL
         if (platform::is_xpu_place(group_tensor.place())) {
           // TODO(liuyuhui) support XPU set constant
@@ -583,8 +585,6 @@ void Reducer::MarkVarReady(const size_t var_index, const bool is_used_var) {
         }
 #else
         auto *dev_ctx = platform::DeviceContextPool::Instance().Get(place_);
-        group_tensor.Resize({static_cast<int64_t>(length)});
-        group_tensor.mutable_data(place_, group.dtype_);
         operators::math::set_constant(*dev_ctx, &group_tensor, 0.0);
 #endif
       }
