1b8a2afc00cc467e4d1ed44101a37e6d1d2f3694
zhhsplendid <zhhsplendid@163.com>
2021-02-10 03:07:41 +0000

    Refine fake_interface Error Message. test=develop

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -248,7 +248,8 @@ def _fake_interface_only_(func):
         raise AssertionError(
             "'%s' should be called by imperative Varible in imperative mode, please run it in dygraph "
             "mode. You can turn off paddle.enable_static() if you are in static mode, or turn off "
-            "ProgramTranslator if you are using @paddle.jit.to_static" %
+            "ProgramTranslator if you are using @paddle.jit.to_static. If you have to run ProgramTranslator, "
+            "please use other API to replace '%s'" % func.__name__,
             func.__name__)
 
     return __impl__
