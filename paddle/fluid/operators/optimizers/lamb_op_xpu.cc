94b74167c82b9fd925135c4b954829a7a5c7e355
Gradie <1099562076@qq.com>
2021-02-18 06:35:57 +0000

    modify lamb_op_xpu.cc;test=kunlun

--- paddle/fluid/operators/optimizers/lamb_op_xpu.cc
+++ paddle/fluid/operators/optimizers/lamb_op_xpu.cc
@@ -27,22 +27,14 @@ class LambOpXPUKernel : public framework::OpKernel<T> {
   void Compute(const framework::ExecutionContext& ctx) const override {
     using paddle::framework::LoDTensor;
     const auto* param_var = ctx.InputVar("Param");
-    PADDLE_ENFORCE_EQ(param_var->IsType<LoDTensor>(), true,
+    PADDLE_ENFORCE_EQ(param_var->IsType<framework::LoDTensor>(), true,
                       platform::errors::InvalidArgument(
-                          "Tensor holds the wrong type，Expected Var(%s)'s "
-                          "type is LoDTensor, "
+                          "The Var(%s)'s type should be LoDTensor, "
                           "but the received is %s",
                           ctx.InputNames("Param").front(),
                           framework::ToTypeName(param_var->Type())));
 
-    const auto* grad_var = ctx.InputVar("Grad");
-    PADDLE_ENFORCE_EQ(grad_var->IsType<LoDTensor>(), true,
-                      platform::errors::InvalidArgument(
-                          "Tensor holds the wrong type，Expected Var(%s)'s "
-                          "type is LoDTensor, "
-                          "but the received is %s",
-                          ctx.InputNames("Grad").front(),
-                          framework::ToTypeName(grad_var->Type())));
+    using paddle::framework::LoDTensor;
 
     // inputs
     T epsilon = static_cast<T>(ctx.Attr<float>("epsilon"));
@@ -51,9 +43,7 @@ class LambOpXPUKernel : public framework::OpKernel<T> {
     T beta2 = static_cast<T>(ctx.Attr<float>("beta2"));
     auto& param = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Param"), "Input",
                                   "Param", "Lamb");
-    // auto* grad_var = ctx.InputVar("Grad");
-    auto& grad =
-        GET_DATA_SAFELY(ctx.Input<LoDTensor>("Grad"), "Input", "Grad", "Lamb");
+    auto* grad_var = ctx.InputVar("Grad");
     auto& mom1 = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Moment1"), "Input",
                                  "Moment1", "Lamb");
     auto& mom2 = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Moment2"), "Input",
@@ -78,52 +68,58 @@ class LambOpXPUKernel : public framework::OpKernel<T> {
                                           "Output", "Beta2PowOut", "Lamb");
     auto& dev_ctx = ctx.template device_context<DeviceContext>();
 
-    int r = xpu::lamb(
-        dev_ctx.x_context(), grad.template data<T>(), mom1.template data<T>(),
-        mom2.template data<T>(), param.template data<T>(),
-        beta1_pow.template data<T>(), beta2_pow.template data<T>(), beta1,
-        beta2, epsilon, weight_decay, lr.template data<T>(),
-        mom1_out.template mutable_data<T>(ctx.GetPlace()),
-        mom2_out.template mutable_data<T>(ctx.GetPlace()),
-        param_out.template mutable_data<T>(ctx.GetPlace()),
-        beta1_pow_out.template mutable_data<T>(ctx.GetPlace()),
-        beta2_pow_out.template mutable_data<T>(ctx.GetPlace()), param.numel());
+    if (grad_var->IsType<framework::LoDTensor>()) {
+      auto& grad = *ctx.Input<LoDTensor>("Grad");
+      int r = xpu::lamb(dev_ctx.x_context(), grad.template data<T>(),
+                        mom1.template data<T>(), mom2.template data<T>(),
+                        param.template data<T>(), beta1_pow.template data<T>(),
+                        beta2_pow.template data<T>(), beta1, beta2, epsilon,
+                        weight_decay, lr.template data<T>(),
+                        mom1_out.template mutable_data<T>(ctx.GetPlace()),
+                        mom2_out.template mutable_data<T>(ctx.GetPlace()),
+                        param_out.template mutable_data<T>(ctx.GetPlace()),
+                        beta1_pow_out.template mutable_data<T>(ctx.GetPlace()),
+                        beta2_pow_out.template mutable_data<T>(ctx.GetPlace()),
+                        param.numel());
 
-    if (r == xpu::Error_t::INVALID_PARAM) {
-      PADDLE_ENFORCE_EQ(
-          r, xpu::Error_t::SUCCESS,
-          platform::errors::InvalidArgument(
-              "XPU kernel error of RmspropOp, error message: INVALID_PARAM, "
-              "please check your input & output."));
-    } else if (r == xpu::Error_t::RUNTIME_ERROR) {
-      PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
-                        platform::errors::Unavailable(
-                            "XPU kernel error of RmspropOp, error message: "
-                            "RUNTIME_ERROR, please check whether Baidu "
-                            "Kunlun Card is properly installed."));
-    } else if (r == xpu::Error_t::NO_ENOUGH_WORKSPACE) {
-      PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
-                        platform::errors::ResourceExhausted(
-                            "XPU kernel error of RmspropOp, error "
-                            "message: NO_ENOUGH_WORKSPACE, XPU "
-                            "has no enough memory."));
+      if (r == xpu::Error_t::INVALID_PARAM) {
+        PADDLE_ENFORCE_EQ(
+            r, xpu::Error_t::SUCCESS,
+            platform::errors::InvalidArgument(
+                "XPU kernel error of LambOp, error message: INVALID_PARAM, "
+                "please check your input & output."));
+      } else if (r == xpu::Error_t::RUNTIME_ERROR) {
+        PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
+                          platform::errors::Unavailable(
+                              "XPU kernel error of LambOp, error message: "
+                              "RUNTIME_ERROR, please check whether Baidu "
+                              "Kunlun Card is properly installed."));
+      } else if (r == xpu::Error_t::NO_ENOUGH_WORKSPACE) {
+        PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
+                          platform::errors::ResourceExhausted(
+                              "XPU kernel error of LambOp, error "
+                              "message: NO_ENOUGH_WORKSPACE, XPU "
+                              "has no enough memory."));
+      } else {
+        PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
+                          platform::errors::ResourceExhausted(
+                              "XPU kernel error of LambOp, error "
+                              "message: OTHER "
+                              "XPU API returns error code: %d.",
+                              r));
+      }
     } else {
-      PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
-                        platform::errors::ResourceExhausted(
-                            "XPU kernel error of RmspropOp, error "
-                            "message: OTHER "
-                            "XPU API returns error code: %d.",
-                            r));
+      PADDLE_THROW(platform::errors::InvalidArgument(
+          "Variable type not supported by lamb_op. Expect LoDTensor, "
+          "but got %s",
+          framework::ToTypeName(param_var->Type())));
     }
   }
 };
-#endif
-
 }  // namespace operators
 }  // namespace paddle
 
 namespace ops = paddle::operators;
-#ifdef PADDLE_WITH_XPU
 REGISTER_OP_XPU_KERNEL(
     lamb, ops::LambOpXPUKernel<paddle::platform::XPUDeviceContext, float>);
 #endif
