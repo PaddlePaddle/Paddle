d9b90fb195565a9f92d4f4cf2138e1a3748a19c1
frankwhzhang <frankwhzhang@126.com>
2021-02-20 17:31:29 +0800

    fix

--- paddle/fluid/operators/shape_op_npu.cc
+++ paddle/fluid/operators/shape_op_npu.cc
@@ -26,8 +26,8 @@ template <typename DeviceContext, typename T>
 class ShapeNPUKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
-    auto* x = ctx.Input<framework::LoDTensor>("Input");
-    auto* out = ctx.Output<framework::LoDTensor>("Out");
+    auto* x = ctx.Input<Tensor>("Input");
+    auto* out = ctx.Output<Tensor>("Out");
     
     out->mutable_data<T>(ctx.GetPlace());
 
