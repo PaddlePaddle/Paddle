1c2784ea40796bd3817fcdb62fdf4343ba4f3d71
Qi Li <qili93@qq.com>
2021-02-20 13:47:28 +0800

    [ROCM] update fluid inference for rocm (part1), test=develop (#31018)

--- paddle/fluid/inference/lite/engine.cc
+++ paddle/fluid/inference/lite/engine.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifdef PADDLE_WITH_CUDA
+#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
 #define LITE_WITH_CUDA 1
 #endif
 
