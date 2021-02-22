b00958e759526799fa790f96c583d02a830db314
wangchaochaohu <wangchao66@baidu.com>
2021-02-22 06:36:06 +0000

    refie comment

--- paddle/fluid/operators/elementwise/elementwise_add_op.h
+++ paddle/fluid/operators/elementwise/elementwise_add_op.h
@@ -315,7 +315,7 @@ class ElementwiseAddGradKernel : public ElemwiseGradKernel<T> {
     // skip out
     auto *out = dout;
 
-// TODO(@zhouwei25, wangchaochaohu): Fix conv_transpose2d API(dataformat NHWC)
+// TODO(@wangchaochaohu, zhouwei35): Fix conv_transpose2d API(dataformat NHWC)
 // error in Windows
 #if defined(PADDLE_WITH_CUDA) && defined(_LINUX)
 #ifdef __NVCC__
