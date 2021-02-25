dee255173dd50bd20a109d934acdff2a44a52283
Adam Osewski <adam.osewski@intel.com>
2021-02-25 10:11:42 +0100

    Enable bfloat16 kernel for lookup_table and grad.

--- paddle/fluid/operators/lookup_table_op.cc
+++ paddle/fluid/operators/lookup_table_op.cc
@@ -222,9 +222,11 @@ REGISTER_OPERATOR(lookup_table_grad, ops::LookupTableOpGrad,
 
 REGISTER_OP_CPU_KERNEL(lookup_table, ops::LookupTableKernel<float>,
                        ops::LookupTableKernel<double>,
-                       ops::LookupTableKernel<int8_t>);
+                       ops::LookupTableKernel<int8_t>,
+                       ops::LookupTableKernel<paddle::platform::bfloat16>);
 REGISTER_OP_CPU_KERNEL(lookup_table_grad, ops::LookupTableGradKernel<float>,
-                       ops::LookupTableGradKernel<double>);
+                       ops::LookupTableGradKernel<double>,
+                       ops::LookupTableGradKernel<paddle::platform::bfloat16>);
 
 /* ==========================  register checkpoint ===========================*/
 
