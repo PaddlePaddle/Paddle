29622aeb2e123a69a43fdb18b9d709d070b491bf
frankwhzhang <frankwhzhang@126.com>
2021-02-25 18:14:22 +0800

    update mean op

---
+++ paddle/fluid/operators/mean_op_npu.cc
@@ -0,0 +1,88 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+//#include "cub/cub.cuh"
+#include "paddle/fluid/operators/mean_op.h"
+#include "paddle/fluid/platform/float16.h"
+#include "paddle/fluid/operators/npu_op_runner.h"
+
+
+namespace paddle {
+namespace operators {
+
+template <typename DeviceContext, typename T>
+class MeanNPUKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
+    auto* x = ctx.Input<framework::LoDTensor>("X");
+    auto* out = ctx.Output<framework::LoDTensor>("Out");
+
+    // DEBUG
+    /*
+    framework::Tensor cpu_tensor;
+    TensorCopySync(*x, platform::CPUPlace(), &cpu_tensor);
+    auto* data = cpu_tensor.data<T>();
+    auto vec_data = std::vector<T>(data, data + x->numel());
+    for(int i=0; i< static_cast<int>(vec_data.size()); ++i){
+        VLOG(3) << " vec_data["<< i << "] = " << vec_data[i];
+    } */
+
+    auto reduce_ndim = x->dims().size();
+    std::vector<int> axes;
+    for (auto i = 0; i < reduce_ndim; ++i) {
+      axes.push_back(i);
+      // VLOG(3) << " axes " << i ;
+    }
+
+    framework::AttributeMap attr_input = {{"keep_dims", false}, {"axes", axes}};
+
+    std::vector<int64_t> out_dims;
+    out_dims.push_back(1);
+    out->Resize(framework::make_ddim(out_dims));
+    out->mutable_data<T>(ctx.GetPlace());
+
+    Tensor reduced_out(x->type());
+    std::vector<int64_t> reduced_dout_dims;
+    reduced_dout_dims.push_back(1);
+    reduced_out.Resize(framework::make_ddim(reduced_dout_dims));
+    reduced_out.mutable_data<T>(ctx.GetPlace());
+
+    auto runner = NpuOpRunner("ReduceMeanD", {*x}, {*out}, attr_input);
+    auto stream =
+      ctx.template device_context<paddle::platform::NPUDeviceContext>()
+                .stream();
+    runner.Run(stream);
+    /*
+    framework::Tensor cpu_tensor2;
+    TensorCopySync(*out, platform::CPUPlace(), &cpu_tensor2);
+    framework::Tensor cpu_tensor2;
+    TensorCopySync(reduced_out, platform::CPUPlace(), &cpu_tensor2);
+    data = cpu_tensor2.data<T>();
+    framework::TensorCopySync(*tmp_dout, ctx.GetPlace(), dx);
+    */
+
+  }
+};
+
+
+}  // namespace operators
+}  // namespace paddle
+
+namespace ops = paddle::operators;
+namespace plat = paddle::platform;
+REGISTER_OP_NPU_KERNEL(
+    mean, 
+    ops::MeanNPUKernel<paddle::platform::NPUDeviceContext, int>,
+    ops::MeanNPUKernel<paddle::platform::NPUDeviceContext, float>,
+    ops::MeanNPUKernel<paddle::platform::NPUDeviceContext, double>)
+
