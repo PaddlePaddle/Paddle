6330fc948d15499220d28a3911aaff3dd01e2d59
Wilber <jiweibo@baidu.com>
2021-03-01 11:28:28 +0800

    cherry-pick (#31279)

--- paddle/fluid/inference/api/api_tester.cc
+++ paddle/fluid/inference/api/api_tester.cc
@@ -91,4 +91,9 @@ TEST(paddle_inference_api, AnalysisConfigCopyCtor) {
     CHECK_NE(ps, delete_pass);
   }
 }
+
+#ifdef PADDLE_WITH_CRYPTO
+TEST(paddle_inference_api, crypto) { paddle::MakeCipher(""); }
+#endif
+
 }  // namespace paddle
