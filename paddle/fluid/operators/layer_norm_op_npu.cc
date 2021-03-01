8d0f8f40b419923ec9753a793d1c1b4965b971b2
zhiqiu <chenqiuliang@baidu.com>
2021-03-01 07:27:25 +0000

    fix typo

--- paddle/fluid/operators/layer_norm_op_npu.cc
+++ paddle/fluid/operators/layer_norm_op_npu.cc
@@ -15,6 +15,7 @@ limitations under the License. */
 #ifdef PADDLE_WITH_ASCEND_CL
 
 #include "paddle/fluid/operators/layer_norm_op.h"
+#include "paddle/fluid/operators/npu_op_runner.h"
 
 namespace paddle {
 namespace operators {
@@ -34,10 +35,8 @@ class LayerNormNPUKernel : public framework::OpKernel<T> {
     auto* y = ctx.Output<Tensor>("Y");
     auto* mean = ctx.Output<Tensor>("Mean");
     auto* variance = ctx.Output<Tensor>("Variance");
-    const auto* x_data = x->data<T>();
     const auto& x_dims = x->dims();
     auto matrix_dim = framework::flatten_to_2d(x_dims, begin_norm_axis);
-    int left = static_cast<int>(matrix_dim[0]);
     int right = static_cast<int>(matrix_dim[1]);
 
     auto place = ctx.GetPlace();
@@ -54,7 +53,7 @@ class LayerNormNPUKernel : public framework::OpKernel<T> {
                        ctx.device_context(), &value);
       auto runner =
           NpuOpRunner("FillD", {value}, {default_scale}, {{"dims", {right}}});
-      runner.run();
+      runner.Run(stream);
       scale = &default_scale;
     }
     if (!bias) {
@@ -66,7 +65,7 @@ class LayerNormNPUKernel : public framework::OpKernel<T> {
                        &value);
       auto runner =
           NpuOpRunner("FillD", {value}, {default_bias}, {{"dims", {right}}});
-      runner.run();
+      runner.Run(stream);
       bias = &default_bias;
     }
     y->mutable_data<T>(ctx.GetPlace());
@@ -74,11 +73,11 @@ class LayerNormNPUKernel : public framework::OpKernel<T> {
     variance->mutable_data<T>(ctx.GetPlace());
 
     auto runner =
-        NpuOpRunner("LayerNorm", {*x, *scale, *bias}, {*out, *mean, *variance},
+        NpuOpRunner("LayerNorm", {*x, *scale, *bias}, {*y, *mean, *variance},
                     {{"begin_norm_axis", begin_norm_axis},
                      {"begin_param_axis", begin_norm_axis},
-                     {"eposilon", epsilon}});
-    runner.run();
+                     {"epsilon", epsilon}});
+    runner.Run(stream);
   }
 };
 
@@ -86,11 +85,9 @@ template <typename T>
 class LayerNormGradNPUKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
+    const auto begin_norm_axis = ctx.Attr<int>("begin_norm_axis");
     const auto* x = ctx.Input<Tensor>("X");
     const auto& x_dims = x->dims();
-    auto matrix_dim = framework::flatten_to_2d(x_dims, begin_norm_axis);
-    int left = static_cast<int>(matrix_dim[0]);
-    int right = static_cast<int>(matrix_dim[1]);
     const auto* mean = ctx.Input<Tensor>("Mean");
     const auto* variance = ctx.Input<Tensor>("Variance");
     const auto* scale = ctx.Input<Tensor>("Scale");
@@ -99,15 +96,21 @@ class LayerNormGradNPUKernel : public framework::OpKernel<T> {
     auto* dscale = ctx.Output<Tensor>(framework::GradVarName("Scale"));
     auto* dbias = ctx.Output<Tensor>(framework::GradVarName("Bias"));
 
-    auto& dev_ctx = ctx.template device_context<DeviceContext>();
+    auto matrix_dim = framework::flatten_to_2d(x_dims, begin_norm_axis);
+    int right = static_cast<int>(matrix_dim[1]);
+
+    auto place = ctx.GetPlace();
+    auto stream =
+        ctx.template device_context<paddle::platform::NPUDeviceContext>()
+            .stream();
 
     // No need to compute any gradient, jusr return
     if (!dx && !dscale && !dbias) {
       return;
     }
 
+    Tensor default_scale(x->type());
     if (!scale) {
-      Tensor default_scale(x->type());
       default_scale.mutable_data<T>({right}, place);
       Tensor value(x->type());
       value.mutable_data<T>({1}, place);
@@ -115,24 +118,23 @@ class LayerNormGradNPUKernel : public framework::OpKernel<T> {
                        ctx.device_context(), &value);
       auto runner =
           NpuOpRunner("FillD", {value}, {default_scale}, {{"dims", {right}}});
-      runner.run();
+      runner.Run(stream);
       scale = &default_scale;
     }
 
-    dx = (dx == nullptr) ? &Tensor(dy->type()) : dx;
-    dscale = (dscale == nullptr) ? &Tensor(dy->type()) : dscale;
-    dbias = (dbias == nullptr) ? &Tensor(dy->type()) : dbias;
+    Tensor dx_(dy->type()), dscale_(dy->type()), dbias_(dy->type());
+    dx = (dx == nullptr) ? &dx_ : dx;
+    dscale = (dscale == nullptr) ? &dscale_ : dscale;
+    dbias = (dbias == nullptr) ? &dbias_ : dbias;
 
     dscale->mutable_data<T>(ctx.GetPlace());
     dbias->mutable_data<T>(ctx.GetPlace());
     dx->mutable_data<T>(ctx.GetPlace());
 
-    auto runner = NpuOpRunner(
-        "LayerNormGrad", {*dy, *x, *variance, *mean, *scale},
-        {*dx, *dscale, *dbias}, {{"begin_norm_axis", begin_norm_axis},
-                                 {"begin_param_axis", begin_norm_axis},
-                                 {"eposilon", epsilon}});
-    runner.run();
+    auto runner =
+        NpuOpRunner("LayerNormGrad", {*dy, *x, *variance, *mean, *scale},
+                    {*dx, *dscale, *dbias}, {});
+    runner.Run(stream);
   }
 };
 
