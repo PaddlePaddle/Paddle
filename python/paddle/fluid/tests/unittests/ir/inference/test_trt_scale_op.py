cd58282244e045d22426dff461def1d819624a76
cryoco <peiyang@baidu.com>
2021-03-01 21:22:39 +0800

    add flatten for ut

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_scale_op.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_scale_op.py
@@ -43,7 +43,7 @@ class TRTScaleTest(InferencePassTest):
     def test_check_output(self):
         if core.is_compiled_with_cuda():
             use_gpu = True
-            self.check_output_with_option(use_gpu)
+            self.check_output_with_option(use_gpu, flatten=True)
             self.assertTrue(
                 PassVersionChecker.IsCompatible('tensorrt_subgraph_pass'))
 
