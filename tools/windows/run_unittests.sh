34eda5ff076e9baf0238eabc4a039abdecd38494
zhouwei25 <zhouwei25@baidu.com>
2021-03-02 12:07:49 +0000

    [Parallel UT]improve Parallel UT level on Windows/Linux

--- tools/windows/run_unittests.sh
+++ tools/windows/run_unittests.sh
@@ -211,9 +211,10 @@ UT_list=$(ctest -N | awk -F ': ' '{print $2}' | sed '/^$/d' | sed '$d')
 num=$(ctest -N | awk -F ': ' '{print $2}' | sed '/^$/d' | sed '$d' | wc -l)
 echo "Windows 1 card TestCases count is $num"
 output=$(python ${PADDLE_ROOT}/tools/parallel_UT_rule.py "${UT_list}")
-eight_parallel_job=$(echo $output | cut -d ";" -f 1)
-tetrad_parallel_jog=$(echo $output | cut -d ";" -f 2)
-non_parallel_job=$(echo $output | cut -d ";" -f 3)
+cpu_parallel_job=$(echo $output | cut -d ";" -f 1)
+tetrad_parallel_job=$(echo $output | cut -d ";" -f 2)
+two_parallel_job=$(echo $output | cut -d ";" -f 3)
+non_parallel_job=$(echo $output | cut -d ";" -f 4)
 
 non_parallel_job_1=$(echo $non_parallel_job | cut -d "," -f 1)
 non_parallel_job_2=$(echo $non_parallel_job | cut -d "," -f 2)
@@ -237,10 +238,11 @@ function collect_failed_tests() {
 function run_unittest() {
     test_case=$1
     parallel_job=$2
+    parallel_level_base=${CTEST_PARALLEL_LEVEL:-1}
     if [ "$2" == "" ]; then
-        parallel_job=1
+        parallel_job=$parallel_level_base
     else
-        parallel_job=$2
+        parallel_job=`expr $2 \* $parallel_level_base`
     fi
     echo "************************************************************************"
     echo "********These unittests run $parallel_job job each time with 1 GPU**********"
@@ -308,7 +310,7 @@ function show_ut_retry_result() {
             echo "========================================"
             echo "There are failed tests, which have been successful after re-run:"
             echo "========================================"
-            echo "The following tests have been re-ran:"
+            echo "The following tests have been re-run:"
             echo "${retry_unittests_record}"
         else
             failed_ut_re=$(echo "${retry_unittests_record_judge}" | awk 'BEGIN{ all_str=""}{if (all_str==""){all_str=$1}else{all_str=all_str"|"$1}} END{print all_str}')
@@ -324,8 +326,9 @@ function show_ut_retry_result() {
 }
 
 set +e
-run_unittest $eight_parallel_job 8
-run_unittest $tetrad_parallel_jog 4
+run_unittest $cpu_parallel_job 20
+run_unittest $tetrad_parallel_job 4
+run_unittest $two_parallel_job 2
 run_unittest $non_parallel_job_1
 run_unittest $non_parallel_job_2
 collect_failed_tests
