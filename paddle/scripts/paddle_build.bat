44440dccd21188aa0c8a395d907e55d677e605e1
XieYunshen <1084314248@qq.com>
2021-03-02 16:25:18 +0800

    notest,test=model_benchmark

--- paddle/scripts/paddle_build.bat
+++ paddle/scripts/paddle_build.bat
@@ -187,7 +187,7 @@ call :build || goto build_error
 call :test_whl_pacakage || goto test_whl_pacakage_error
 call :test_unit || goto test_unit_error
 call :test_inference || goto test_inference_error
-:: call :check_change_of_unittest || goto check_change_of_unittest_error
+call :check_change_of_unittest || goto check_change_of_unittest_error
 goto:success
 
 rem ------Build windows avx whl package------
