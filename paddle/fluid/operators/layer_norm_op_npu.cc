6a72e5346bfc695e88c0473940f88e47f8958a92
zhiqiu <chenqiuliang@baidu.com>
2021-03-02 10:41:53 +0000

    fix bug

--- paddle/fluid/operators/layer_norm_op_npu.cc
+++ paddle/fluid/operators/layer_norm_op_npu.cc
@@ -12,8 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#ifdef PADDLE_WITH_ASCEND_CL
-
 #include "paddle/fluid/operators/layer_norm_op.h"
 #include "paddle/fluid/operators/npu_op_runner.h"
 
@@ -36,37 +34,48 @@ class LayerNormNPUKernel : public framework::OpKernel<T> {
     auto* mean = ctx.Output<Tensor>("Mean");
     auto* variance = ctx.Output<Tensor>("Variance");
     const auto& x_dims = x->dims();
+    std::vector<int> axes;
     auto matrix_dim = framework::flatten_to_2d(x_dims, begin_norm_axis);
     int right = static_cast<int>(matrix_dim[1]);
 
+    // The shape of scale and bias should be equal to x.shape[begin_norm_axis:],
+    // required by Ascend.
+    for (auto i = begin_norm_axis; i < x_dims.size(); ++i) {
+      axes.push_back(x_dims[i]);
+    }
     auto place = ctx.GetPlace();
     auto stream =
         ctx.template device_context<paddle::platform::NPUDeviceContext>()
             .stream();
 
+    Tensor default_scale(x->type());
     if (!scale) {
-      Tensor default_scale(x->type());
-      default_scale.mutable_data<T>({right}, place);
+      default_scale.mutable_data<T>(framework::make_ddim(axes), place);
       Tensor value(x->type());
       value.mutable_data<T>({1}, place);
       TensorFromVector(std::vector<T>{static_cast<T>(1.0)},
                        ctx.device_context(), &value);
       auto runner =
-          NpuOpRunner("FillD", {value}, {default_scale}, {{"dims", {right}}});
+          NpuOpRunner("FillD", {value}, {default_scale}, {{"dims", axes}});
       runner.Run(stream);
       scale = &default_scale;
+    } else {
+      const_cast<Tensor*>(scale)->Resize(framework::make_ddim(axes));
     }
+
+    Tensor default_bias(x->type());
     if (!bias) {
-      Tensor default_bias(x->type());
-      default_bias.mutable_data<T>({right}, place);
+      default_bias.mutable_data<T>(framework::make_ddim(axes), place);
       Tensor value(x->type());
       value.mutable_data<T>({1}, place);
       TensorFromVector(std::vector<T>{static_cast<T>(0)}, ctx.device_context(),
                        &value);
       auto runner =
-          NpuOpRunner("FillD", {value}, {default_bias}, {{"dims", {right}}});
+          NpuOpRunner("FillD", {value}, {default_bias}, {{"dims", axes}});
       runner.Run(stream);
       bias = &default_bias;
+    } else {
+      const_cast<Tensor*>(bias)->Resize(framework::make_ddim(axes));
     }
     y->mutable_data<T>(ctx.GetPlace());
     mean->mutable_data<T>(ctx.GetPlace());
@@ -78,6 +87,11 @@ class LayerNormNPUKernel : public framework::OpKernel<T> {
                      {"begin_params_axis", begin_norm_axis},
                      {"epsilon", epsilon}});
     runner.Run(stream);
+    // revert shape of scale and bias
+    // TODO(zhiqiu): better implementation, use tmp tensor to avoid write input
+    // tensor.
+    const_cast<Tensor*>(scale)->Resize(framework::make_ddim({right}));
+    const_cast<Tensor*>(bias)->Resize(framework::make_ddim({right}));
   }
 };
 
@@ -99,6 +113,11 @@ class LayerNormGradNPUKernel : public framework::OpKernel<T> {
     auto matrix_dim = framework::flatten_to_2d(x_dims, begin_norm_axis);
     int right = static_cast<int>(matrix_dim[1]);
 
+    std::vector<int> axes;
+    for (auto i = begin_norm_axis; i < x_dims.size(); ++i) {
+      axes.push_back(x_dims[i]);
+    }
+
     auto place = ctx.GetPlace();
     auto stream =
         ctx.template device_context<paddle::platform::NPUDeviceContext>()
@@ -109,17 +128,32 @@ class LayerNormGradNPUKernel : public framework::OpKernel<T> {
       return;
     }
 
+    // The rank of mean should be equal to x, required by Ascend.
+    std::vector<int> new_shape;
+    for (auto i = 0; i < begin_norm_axis; ++i) {
+      new_shape.push_back(x_dims[i]);
+    }
+    for (auto i = begin_norm_axis; i < x_dims.size(); ++i) {
+      new_shape.push_back(1);
+    }
+
+    auto mean_dims = mean->dims();
+    const_cast<Tensor*>(mean)->Resize(framework::make_ddim({new_shape}));
+    const_cast<Tensor*>(variance)->Resize(framework::make_ddim({new_shape}));
+
     Tensor default_scale(x->type());
     if (!scale) {
-      default_scale.mutable_data<T>({right}, place);
+      default_scale.mutable_data<T>(framework::make_ddim(axes), place);
       Tensor value(x->type());
       value.mutable_data<T>({1}, place);
       TensorFromVector(std::vector<T>{static_cast<T>(1.0)},
                        ctx.device_context(), &value);
       auto runner =
-          NpuOpRunner("FillD", {value}, {default_scale}, {{"dims", {right}}});
+          NpuOpRunner("FillD", {value}, {default_scale}, {{"dims", axes}});
       runner.Run(stream);
       scale = &default_scale;
+    } else {
+      const_cast<Tensor*>(scale)->Resize(framework::make_ddim(axes));
     }
 
     Tensor dx_(dy->type()), dscale_(dy->type()), dbias_(dy->type());
@@ -127,14 +161,25 @@ class LayerNormGradNPUKernel : public framework::OpKernel<T> {
     dscale = (dscale == nullptr) ? &dscale_ : dscale;
     dbias = (dbias == nullptr) ? &dbias_ : dbias;
 
+    dscale->Resize(framework::make_ddim(axes));
     dscale->mutable_data<T>(ctx.GetPlace());
+
+    dbias->Resize(framework::make_ddim(axes));
     dbias->mutable_data<T>(ctx.GetPlace());
+
+    dx->Resize(x->dims());
     dx->mutable_data<T>(ctx.GetPlace());
 
     auto runner =
         NpuOpRunner("LayerNormGrad", {*dy, *x, *variance, *mean, *scale},
                     {*dx, *dscale, *dbias}, {});
     runner.Run(stream);
+
+    const_cast<Tensor*>(mean)->Resize(mean_dims);
+    const_cast<Tensor*>(variance)->Resize(mean_dims);
+    const_cast<Tensor*>(scale)->Resize(framework::make_ddim({right}));
+    dscale->Resize(framework::make_ddim({right}));
+    dbias->Resize(framework::make_ddim({right}));
   }
 };
 
@@ -148,5 +193,3 @@ REGISTER_OP_NPU_KERNEL(layer_norm, ops::LayerNormNPUKernel<float>,
                        ops::LayerNormNPUKernel<plat::float16>);
 REGISTER_OP_NPU_KERNEL(layer_norm_grad, ops::LayerNormGradNPUKernel<float>,
                        ops::LayerNormGradNPUKernel<plat::float16>);
-
-#endif  // PADDLE_WITH_ASCEND_CL
