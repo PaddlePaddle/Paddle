626b599560f0d93e9b516b5a8390b739be763406
XieYunshen <1084314248@qq.com>
2021-03-02 21:05:39 +0800

    fix format error

--- tools/windows/run_unittests.sh
+++ tools/windows/run_unittests.sh
@@ -221,7 +221,7 @@ if [ ${PRECISION_TEST:-OFF} == "ON" ]; then
     fi
 fi
 
-if [ ${PRECISION_TEST:-OFF} == "ON"] && [[ "$precision_cases" != ""]];then
+if [ ${PRECISION_TEST:-OFF} == "ON" ] && [[ "$precision_cases" != "" ]];then
     UT_list_prec=''
     re=$(cat ut_list|awk -F ' ' '{print }' | awk 'BEGIN{ all_str=""}{if (all_str==""){all_str=$1}else{all_str=all_str"$|^"$1}} END{print "^"all_str"$"}')
     for case in $UT_list; do
