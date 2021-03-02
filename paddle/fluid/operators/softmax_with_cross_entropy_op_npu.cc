9b2045a2c97dc659c732c066026bf627afc8c3e4
frankwhzhang <frankwhzhang@126.com>
2021-03-02 06:59:59 +0000

    add softmax_with_cross_entropy_grad

--- paddle/fluid/operators/softmax_with_cross_entropy_op_npu.cc
+++ paddle/fluid/operators/softmax_with_cross_entropy_op_npu.cc
@@ -106,6 +106,70 @@ class SoftmaxWithCrossEntropyNPUKernel : public framework::OpKernel<T> {
   }
 };
 
+template <typename DeviceContext, typename T>
+class SoftmaxWithCrossEntropyGradNPUKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
+    auto* labels = ctx.Input<Tensor>("Label");
+    auto* softmax = ctx.Input<Tensor>("Softmax");
+    auto* loss_grad = ctx.Input<Tensor>(framework::GradVarName("Loss"));
+    auto* logits_grad = ctx.Output<Tensor>(framework::GradVarName("Logits"));
+
+    int cls_num = softmax->dims()[1];
+
+    auto stream =
+        ctx.template device_context<paddle::platform::NPUDeviceContext>()
+            .stream();
+
+    // cast label
+    Tensor tmp_labels(labels->type());
+    tmp_labels.Resize(labels->dims());
+    tmp_labels.mutable_data<T>(ctx.GetPlace());
+    auto runner_cast_label = NpuOpRunner("Cast", {*labels}, {tmp_labels},
+                                         {{"dst_type", static_cast<int>(3)}});
+    runner_cast_label.Run(stream);
+
+    // on and off
+    Tensor on_tensor(framework::proto::VarType::INT32);
+    on_tensor.mutable_data<int>({1}, ctx.GetPlace());
+    TensorFromVector(std::vector<int>{static_cast<int>(1)},
+                     ctx.device_context(), &on_tensor);
+    Tensor off_tensor(framework::proto::VarType::INT32);
+    off_tensor.mutable_data<int>({1}, ctx.GetPlace());
+    TensorFromVector(std::vector<int>{static_cast<int>(0)},
+                     ctx.device_context(), &off_tensor);
+
+    // one_hot
+    Tensor tmp_onehot(on_tensor.type());
+    tmp_onehot.Resize(logits->dims());
+    tmp_onehot.mutable_data<int>(ctx.GetPlace());
+
+    auto runner_onehot =
+        NpuOpRunner("OneHotD", {tmp_labels, on_tensor, off_tensor},
+                    {tmp_onehot}, {{"depth", cls_num}});
+    runner_onehot.Run(stream);
+
+    // to do squeeze, infer shape first
+    PADDLE_ENFORCE_NE(
+        logits->dims()[0], 1,
+        platform::errors::InvalidArgument("logits first dim should not be 1"));
+    PADDLE_ENFORCE_NE(
+        logits->dims()[1], 1,
+        platform::errors::InvalidArgument("logits secend dim should not be 1"));
+
+    // sub
+    Tensor tmp_sub(softmax->.type());
+    tmp_sub.Resize(softmax->dims());
+    tmp_sub.mutable_data<T>(ctx.GetPlace());
+    auto runner_sub = NpuOpRunner("Sub", {*softmax, tmp_onehot}, {tmp_sub}, {});
+
+    runner_sub.Run(stream);
+    // mul
+    auto runner_mul =
+        NpuOpRunner("Mul", {*loss_grad, tmp_sub}, {*logit_grad}, {});
+    runner_mul.Run(stream);
+  }
+};
 }  // namespace operators
 }  // namespace paddle
 
@@ -117,4 +181,10 @@ REGISTER_OP_NPU_KERNEL(
                                           float>,
     ops::SoftmaxWithCrossEntropyNPUKernel<paddle::platform::NPUDeviceContext,
                                           paddle::platform::float16>);
+REGISTER_OP_NPU_KERNEL(
+    softmax_with_cross_entropy_grad,
+    ops::SoftmaxWithCrossEntropyGradNPUKernel<
+        paddle::platform::NPUDeviceContext, float>,
+    ops::SoftmaxWithCrossEntropyGradNPUKernel<
+        paddle::platform::NPUDeviceContext, paddle::platform::float16>);
 #endif
