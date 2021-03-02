d1075df2e8965bf995585e36277308c5a6e365a7
danleifeng <52735331+danleifeng@users.noreply.github.com>
2021-03-02 11:20:46 +0800

    topo and memory performance for heterps (#30440)
    * topo and memory performance for heterps; test=develop
    * add trainwithprofiler in heter trainier; test=develop

--- paddle/fluid/framework/fleet/ps_gpu_wrapper.h
+++ paddle/fluid/framework/fleet/ps_gpu_wrapper.h
@@ -227,7 +227,7 @@ class PSGPUWrapper {
   std::vector<int> heter_devices_;
   std::unordered_set<std::string> gpu_ps_config_keys_;
   HeterObjectPool<HeterContext> gpu_task_pool_;
-  std::vector<std::vector<std::vector<uint64_t>>> thread_keys_;
+  std::vector<std::vector<std::unordered_set<uint64_t>>> thread_keys_;
   int thread_keys_thread_num_ = 37;
   int thread_keys_shard_num_ = 37;
   uint64_t max_fea_num_per_pass_ = 5000000000;
