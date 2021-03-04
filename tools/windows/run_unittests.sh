8458a9a77ee91618d29a0bb3d67bcf237b123ede
XieYunshen <1084314248@qq.com>
2021-03-04 14:43:18 +0800

    fix retry bug on windows

--- tools/windows/run_unittests.sh
+++ tools/windows/run_unittests.sh
@@ -221,6 +221,7 @@ non_parallel_job_2=$(echo $non_parallel_job | cut -d "," -f 2)
 failed_test_lists=''
 tmp_dir=`mktemp -d`
 function collect_failed_tests() {
+    set +e
     for file in `ls $tmp_dir`; do
         grep -q 'The following tests FAILED:' $tmp_dir/$file
         exit_code=$?
@@ -232,6 +233,7 @@ function collect_failed_tests() {
             ${failuretest}"
         fi
     done
+    set -e
 }
 
 function run_unittest() {
@@ -283,7 +285,8 @@ function unittests_retry(){
                     echo "========================================="
                     rm -f $tmp_dir/*
                     failed_test_lists=''
-                    ctest -R "($retry_unittests_regular)" --output-on-failure -C Release -j $parallel_job| tee $tmpfile
+                    (ctest -R "($retry_unittests_regular)" --output-on-failure -C Release -j $parallel_job| tee $tmpfile ) &
+                    wait;
                     collect_failed_tests
                     exec_times=$(echo $exec_times | awk '{print $0+1}')
                 done
