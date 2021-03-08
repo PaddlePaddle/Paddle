eea89c124c9b47f646129ceffe62a76e82d0b28b
xiayanming <xiayanming@baidu.com>
2021-03-08 15:14:15 +0800

    code review done

--- paddle/fluid/operators/gather_op_npu.cc
+++ paddle/fluid/operators/gather_op_npu.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#ifdef PADDLE_WITH_ASCEND_CL
 #include <memory>
 #include <string>
 #include <vector>
@@ -115,4 +114,4 @@ REGISTER_OP_NPU_KERNEL(
     ops::GatherGradOpNPUKernel<paddle::platform::NPUDeviceContext, float>,
     ops::GatherGradOpNPUKernel<paddle::platform::NPUDeviceContext, 
                                paddle::platform::float16>);
-#endif
+
--- python/paddle/fluid/tests/unittests/test_gather_op_npu.py
+++ python/paddle/fluid/tests/unittests/test_gather_op_npu.py
@@ -107,3 +107,4 @@ class TestGatherAPI(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
