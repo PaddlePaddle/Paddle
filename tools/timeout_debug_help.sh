d433a4e6617ddb1fe4ca10b73d6940de8ea7771f
XieYunshen <1084314248@qq.com>
2021-03-09 20:02:18 +0800

    added show information

--- tools/timeout_debug_help.sh
+++ tools/timeout_debug_help.sh
@@ -18,7 +18,10 @@ failed_uts=$1
 need_debug_ut_re='test_dist_fleet'
 cat_log_judge=$(echo "${failed_uts}" | grep 'Timeout' |  grep -oEi "$need_debug_ut_re" )
 if [[ "$cat_log_judge" != "" ]];then 
-    cat tmp/tr0_err.log tmp/tr1_err.log tmp/ps0_err.log tmp/ps1_err.log
-    cat tmp/heter0_err.log tmp/heter1_err.log
+    echo "=============================================="
+    echo "show timeout ut logs"
+    echo "=============================================="
+    cat /tmp/tr0_err.log /tmp/tr1_err.log /tmp/ps0_err.log /tmp/ps1_err.log
+    cat /tmp/heter0_err.log /tmp/heter1_err.log
 fi
 set -e
