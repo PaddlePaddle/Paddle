0e9ec7cee6448fad7a7d92fb70e80988a87fee75
XieYunshen <1084314248@qq.com>
2021-03-09 13:32:19 +0800

    fix code format error

--- tools/get_pr_ut.py
+++ tools/get_pr_ut.py
@@ -214,6 +214,8 @@ class PRChecker(object):
             if platform.system() == "Darwin":
                 f=f.replace(PADDLE_ROOT,'/paddle/', 1)
                 f=f.replace('//', '/')
+
+
             if f not in file_ut_map:
                 if f.endswith('.md'):
                     ut_list.append('md_placeholder')
