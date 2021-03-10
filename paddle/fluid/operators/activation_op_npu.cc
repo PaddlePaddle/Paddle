97dce685c7e72042a31f480440d652bfe385236f
frankwhzhang <frankwhzhang@126.com>
2021-03-10 13:17:34 +0000

    fixed

--- paddle/fluid/operators/activation_op_npu.cc
+++ paddle/fluid/operators/activation_op_npu.cc
@@ -133,13 +133,13 @@ class ReluGradNPUKernel : public framework::OpKernel<T> {
     auto* dout = ctx.Input<Tensor>(framework::GradVarName("Out"));
     auto* dx = ctx.Output<Tensor>(framework::GradVarName("X"));
 
-    dx->mutable_data<T>(ctx.GetPlace());
-
-    auto runner = NpuOpRunner("ReluGrad", {*dout, *x}, {*dx}, {});
-
     auto stream =
         ctx.template device_context<paddle::platform::NPUDeviceContext>()
             .stream();
+
+    dx->mutable_data<T>(ctx.GetPlace());
+    auto runner = NpuOpRunner("ReluGrad", {*dout, *x}, {*dx}, {});
+
     runner.Run(stream);
   }
 };
@@ -166,5 +166,5 @@ REGISTER_OP_NPU_KERNEL(
 REGISTER_OP_NPU_KERNEL(
     relu_grad,
     ops::ReluGradNPUKernel<paddle::platform::NPUDeviceContext, float>,
-    ops::ReluNPUKernel<paddle::platform::NPUDeviceContext,
-                       paddle::platform::float16>);
+    ops::ReluGradNPUKernel<paddle::platform::NPUDeviceContext,
+                           paddle::platform::float16>);
