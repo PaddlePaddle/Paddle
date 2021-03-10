213737a9ebdf09b3b851f6c0c8470da58dcbd24e
arlesniak <artur.lesniak@intel.com>
2021-03-10 11:27:51 +0100

    Refactor

--- python/paddle/fluid/contrib/mixed_precision/__init__.py
+++ python/paddle/fluid/contrib/mixed_precision/__init__.py
@@ -20,8 +20,6 @@ from . import fp16_lists
 from .fp16_lists import *
 from . import fp16_utils
 from .fp16_utils import *
-from . import bf16
-from .bf16 import *
 
 __all__ = decorator.__all__
 __all__ += fp16_lists.__all__
