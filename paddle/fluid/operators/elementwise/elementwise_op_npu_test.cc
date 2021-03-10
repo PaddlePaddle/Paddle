9e25c37fae93b0aa880638363b6132d452e7babd
oyjxer <1728722986@qq.com>
2021-03-10 16:40:21 +0800

    Support npu op elementwise_mul, elementwise_div, elementwise_max, elementwise_min, elementwise_pow, elementwise_mul_grad and elementwise_div_grad

--- paddle/fluid/operators/elementwise/elementwise_op_npu_test.cc
+++ paddle/fluid/operators/elementwise/elementwise_op_npu_test.cc
@@ -36,6 +36,16 @@ USE_OP(elementwise_add);
 USE_OP_DEVICE_KERNEL(elementwise_add, NPU);
 USE_OP(elementwise_sub);
 USE_OP_DEVICE_KERNEL(elementwise_sub, NPU);
+USE_OP(elementwise_mul);
+USE_OP_DEVICE_KERNEL(elementwise_mul, NPU);
+USE_OP(elementwise_div);
+USE_OP_DEVICE_KERNEL(elementwise_div, NPU);
+USE_OP(elementwise_max);
+USE_OP_DEVICE_KERNEL(elementwise_max, NPU);
+USE_OP(elementwise_min);
+USE_OP_DEVICE_KERNEL(elementwise_min, NPU);
+USE_OP(elementwise_pow);
+USE_OP_DEVICE_KERNEL(elementwise_pow, NPU);
 
 template <typename T>
 void Compare(f::Scope* scope, const p::DeviceContext& ctx,
@@ -62,31 +72,58 @@ void Compare(f::Scope* scope, const p::DeviceContext& ctx,
   TensorFromVector(init_y, ctx, tensor_y);
   tensor_y->Resize({10, 10});
 
-  ctx.Wait();
-
-  auto place = ctx.GetPlace();
   auto out = scope->Var("Out");
   auto tensor_out = out->GetMutable<f::LoDTensor>();
 
-  // run
   f::AttributeMap attrs;
-  auto op = f::OpRegistry::CreateOp(op_type, {{"X", {"X"}}, {"Y", {"Y"}}},
-                                    {{"Out", {"Out"}}}, attrs);
 
-  op->Run(*scope, place);
   ctx.Wait();
 
+  // run
+  auto place = ctx.GetPlace();
+
+  auto op = f::OpRegistry::CreateOp(op_type, {{"X", {"X"}}, {"Y", {"Y"}}},
+                                    {{"Out", {"Out"}}}, attrs);
+  op->Run(*scope, place);
+
+  ctx.Wait();
+
+  // eval time
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
+
+  for (int i = 0; i < 100; i++) {
+    op->Run(*scope, place);
+  }
+
+  ctx.Wait();
+
+  gettimeofday(&end, NULL);
+  int micros = (((end.tv_sec - start.tv_sec) * 1000000) +
+                  end.tv_usec) - (start.tv_usec);
+  printf("used time: %d\n", micros / 100);
+
+  // eval value
   std::vector<T> out_vec;
   TensorToVector(*tensor_out, ctx, &out_vec);
 
-  ctx.Wait();
   float expected;
   if (op_type == "elementwise_add") {
     expected = 3.0;
   } else if (op_type == "elementwise_sub") {
     expected = -1.0;
+  } else if (op_type == "elementwise_mul") {
+    expected = 2.0;
+  } else if (op_type == "elementwise_div") {
+    expected = 0.5;
+  } else if (op_type == "elementwise_max") {
+    expected = 2.0;
+  } else if (op_type == "elementwise_min") {
+    expected = 1.0;
+  } else if (op_type == "elementwise_pow") {
+    expected = 1.0;
   }
-  EXPECT_EQ(out_vec.size(), init_x.size());
+
   for (uint32_t i = 0; i < out_vec.size(); i++) {
     EXPECT_EQ(out_vec[i], static_cast<T>(expected));
   }
@@ -98,15 +135,52 @@ void CompareGrad(f::Scope* scope, const p::DeviceContext& ctx,
   // init
   auto dout = scope->Var("DOut");
   auto tensor_dout = dout->GetMutable<f::LoDTensor>();
-  tensor_dout->Resize({2, 3, 5});
 
   auto x = scope->Var("X");
   auto tensor_x = x->GetMutable<f::LoDTensor>();
-  tensor_x->Resize({2, 3, 5});
 
   auto y = scope->Var("Y");
   auto tensor_y = y->GetMutable<f::LoDTensor>();
-  tensor_y->Resize({1, 5});
+
+  auto out = scope->Var("Out");
+  auto tensor_out = out->GetMutable<f::LoDTensor>();
+
+  std::vector<T> init_dout;
+  for (int64_t i = 0; i < 10 * 10; ++i) {
+    init_dout.push_back(static_cast<T>(1.0));
+  }
+
+  std::vector<T> init_x;
+  for (int64_t i = 0; i < 10 * 10; ++i) {
+    init_x.push_back(static_cast<T>(2.0));
+  }
+
+  std::vector<T> init_y;
+  for (int64_t i = 0; i < 10 * 10; ++i) {
+    init_y.push_back(static_cast<T>(3.0));
+  }
+
+  std::vector<T> init_out;
+  for (int64_t i = 0; i < 10 * 10; ++i) {
+    if (op_type == "elementwise_add_grad") {
+      init_out.push_back(static_cast<T>(5.0));
+    } else if (op_type == "elementwise_sub_grad") {
+      init_out.push_back(static_cast<T>(-1.0));
+    } else if (op_type == "elementwise_mul_grad") {
+      init_out.push_back(static_cast<T>(6.0));
+    } else if (op_type == "elementwise_div_grad") {
+      init_out.push_back(static_cast<T>(2.0/3.0));
+    }
+  }
+
+  TensorFromVector(init_dout, ctx, tensor_dout);
+  tensor_dout->Resize({10, 10});
+  TensorFromVector(init_x, ctx, tensor_x);
+  tensor_x->Resize({10, 10});
+  TensorFromVector(init_y, ctx, tensor_y);
+  tensor_y->Resize({10, 10});
+  TensorFromVector(init_out, ctx, tensor_out);
+  tensor_out->Resize({10, 10});
 
   auto dx = scope->Var("DX");
   auto tensor_dx = dx->GetMutable<f::LoDTensor>();
@@ -114,76 +188,158 @@ void CompareGrad(f::Scope* scope, const p::DeviceContext& ctx,
   auto dy = scope->Var("DY");
   auto tensor_dy = dy->GetMutable<f::LoDTensor>();
 
-  std::vector<T> init_dout;
-  for (int64_t i = 0; i < tensor_dout->numel(); ++i) {
-    init_dout.push_back(static_cast<T>(1.0));
-  }
-
-  TensorFromVector(init_dout, ctx, tensor_dout);
-  tensor_dout->Resize({2, 3, 5});
+  f::AttributeMap attrs;
 
   ctx.Wait();
 
   // run
-  f::AttributeMap attrs;
-  auto op = f::OpRegistry::CreateOp(
-      op_type, {{"Out@GRAD", {"DOut"}}, {"X", {"X"}}, {"Y", {"Y"}}},
-      {{"X@GRAD", {"DX"}}, {"Y@GRAD", {"DY"}}}, attrs);
-
   auto place = ctx.GetPlace();
+
+  auto op = f::OpRegistry::CreateOp(op_type,
+    {{"Out@GRAD", {"DOut"}}, {"X", {"X"}}, {"Y", {"Y"}}, {"Out", {"Out"}}},
+    {{"X@GRAD", {"DX"}}, {"Y@GRAD", {"DY"}}}, attrs);
   op->Run(*scope, place);
+
   ctx.Wait();
 
+  // eval time
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
+
+  for (int i = 0; i < 100; i++) {
+    op->Run(*scope, place);
+  }
+
+  ctx.Wait();
+
+  gettimeofday(&end, NULL);
+  int micros = (((end.tv_sec - start.tv_sec) * 1000000) +
+                  end.tv_usec) - (start.tv_usec);
+  printf("used time: %d\n", micros / 100);
+
+  // eval value
   std::vector<T> dx_vec;
   TensorToVector(*tensor_dx, ctx, &dx_vec);
 
   std::vector<T> dy_vec;
   TensorToVector(*tensor_dy, ctx, &dy_vec);
 
-  ctx.Wait();
-  float expected_x, expected_y;
+  float expected_dx;
+  float expected_dy;
   if (op_type == "elementwise_add_grad") {
-    expected_x = 1.0;
-    expected_y = 6.0;
+    expected_dx = 1.0;
+    expected_dy = 1.0;
   } else if (op_type == "elementwise_sub_grad") {
-    expected_x = 1.0;
-    expected_y = -6.0;
+    expected_dx = 1.0;
+    expected_dy = -1.0;
+  } else if (op_type == "elementwise_mul_grad") {
+    expected_dx = 3.0;
+    expected_dy = 2.0;
+  } else if (op_type == "elementwise_div_grad") {
+    expected_dx = 1.0/3.0;
+    expected_dy = 2.0/9.0;
   }
 
   for (uint32_t i = 0; i < dx_vec.size(); i++) {
-    EXPECT_EQ(dx_vec[i], static_cast<T>(expected_x));
+    EXPECT_FLOAT_EQ(dx_vec[i], static_cast<T>(expected_dx));
   }
   for (uint32_t i = 0; i < dy_vec.size(); i++) {
-    EXPECT_EQ(dy_vec[i], static_cast<T>(expected_y));
+    EXPECT_FLOAT_EQ(dy_vec[i], static_cast<T>(expected_dy));
   }
 }
 
 TEST(elementwise_add, NPU_fp32) {
-  f::Scope scope;
-  p::NPUDeviceContext ctx(p::NPUPlace(0));
-  Compare<float>(&scope, ctx, "elementwise_add");
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<float>(&scope, ctx, "elementwise_add");
 }
 
 TEST(elementwise_sub, NPU_fp32) {
-  f::Scope scope;
-  p::NPUDeviceContext ctx(p::NPUPlace(0));
-  Compare<float>(&scope, ctx, "elementwise_sub");
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<float>(&scope, ctx, "elementwise_sub");
+}
+
+TEST(elementwise_mul, NPU_fp32) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<float>(&scope, ctx, "elementwise_mul");
+}
+
+TEST(elementwise_div, NPU_fp32) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<float>(&scope, ctx, "elementwise_div");
+}
+
+TEST(elementwise_max, NPU_fp32) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<float>(&scope, ctx, "elementwise_max");
+}
+
+TEST(elementwise_min, NPU_fp32) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<float>(&scope, ctx, "elementwise_min");
+}
+
+TEST(elementwise_pow, NPU_fp32) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<float>(&scope, ctx, "elementwise_pow");
 }
 
 TEST(elementwise_sub, NPU_fp16) {
-  f::Scope scope;
-  p::NPUDeviceContext ctx(p::NPUPlace(0));
-  Compare<p::float16>(&scope, ctx, "elementwise_sub");
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<p::float16>(&scope, ctx, "elementwise_sub");
+}
+
+TEST(elementwise_mul, NPU_fp16) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<p::float16>(&scope, ctx, "elementwise_mul");
+}
+
+TEST(elementwise_div, NPU_fp16) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<p::float16>(&scope, ctx, "elementwise_div");
+}
+
+TEST(elementwise_max, NPU_fp16) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<p::float16>(&scope, ctx, "elementwise_max");
+}
+
+TEST(elementwise_min, NPU_fp16) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<p::float16>(&scope, ctx, "elementwise_min");
+}
+
+TEST(elementwise_pow, NPU_fp16) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    Compare<p::float16>(&scope, ctx, "elementwise_pow");
 }
 
 TEST(elementwise_sub_grad, NPU) {
-  f::Scope scope;
-  p::NPUDeviceContext ctx(p::NPUPlace(0));
-  CompareGrad<float>(&scope, ctx, "elementwise_sub_grad");
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    CompareGrad<float>(&scope, ctx, "elementwise_sub_grad");
 }
 
-TEST(elementwise_add_grad, NPU) {
-  f::Scope scope;
-  p::NPUDeviceContext ctx(p::NPUPlace(0));
-  CompareGrad<float>(&scope, ctx, "elementwise_add_grad");
+TEST(elementwise_mul_grad, NPU) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    CompareGrad<float>(&scope, ctx, "elementwise_mul_grad");
+}
+
+TEST(elementwise_div_grad, NPU) {
+    f::Scope scope;
+    p::NPUDeviceContext ctx(p::NPUPlace(0));
+    CompareGrad<float>(&scope, ctx, "elementwise_div_grad");
 }
