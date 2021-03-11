5fcc12390fd3c1f2ec696313c5947428ccf801af
jiweibo <jiweibo@baidu.com>
2021-03-11 11:20:57 +0000

    update

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -11255,15 +11255,19 @@ def shape(input, axes=None):
     Examples:
         .. code-block:: python
 
-            import paddle
-            ndim_3_tensor = paddle.to_tensor([[[1, 2, 3, 4, 5],
-                                               [6, 7, 8, 9, 10]],
-                                              [[11, 12, 13, 14, 15],
-                                               [16, 17, 18, 19, 20]]])
-            print(ndim_3_tensor.size()) # Tensor(shape=[3], dtype=int32, place=CPUPlace, stop_gradient=True, [2, 2, 5])
-            print(ndim_3_tensor.size(2)) # Tensor(shape=[1], dtype=int32, place=CPUPlace, stop_gradient=True, [5])
-            print(ndim_3_tensor.size([-1, 0, 1])) # Tensor(shape=[3], dtype=int32, place=CPUPlace, stop_gradient=True, [5, 2, 2])
-            print(paddle.shape(ndim_3_tensor)) # Tensor(shape=[3], dtype=int32, place=CPUPlace, stop_gradient=True, [2, 2, 5])
+            import paddle.fluid as fluid
+            import numpy as np
+
+            inputs = fluid.data(name="x", shape=[3, 100, 100], dtype="float32")
+            output = fluid.layers.shape(inputs)
+
+            exe = fluid.Executor(fluid.CPUPlace())
+            exe.run(fluid.default_startup_program())
+
+            img = np.ones((3, 100, 100)).astype(np.float32)
+
+            res = exe.run(fluid.default_main_program(), feed={'x':img}, fetch_list=[output])
+            print(res) # [array([  3, 100, 100], dtype=int32)]
     """
     check_variable_and_dtype(
         input, 'input',
