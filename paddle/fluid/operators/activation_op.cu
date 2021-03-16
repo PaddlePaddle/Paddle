702bb64cc5e7e0f7035517d4c1f7323b28d824ca
niuililing123 <sdust_niull@163.com>
2021-03-16 09:42:59 +0000

    modified the name for kernel

--- paddle/fluid/operators/activation_op.cu
+++ paddle/fluid/operators/activation_op.cu
@@ -20,60 +20,119 @@ namespace operators {
 using Tensor = framework::Tensor;
 using float16 = paddle::platform::float16;
 
-template <typename T, int N>
+template <typename T>
 struct GetVecType;
 
-template <typename T>
-struct GetVecType<T, 1> {
-  using type = T;
+template <>
+struct GetVecType<double> {
+  using type = double;
 };
 
 template <>
-struct GetVecType<platform::float16, 2> {
+struct GetVecType<platform::float16> {
   using type = __half2;
 };
 
 template <>
-struct GetVecType<float, 4> {
+struct GetVecType<float> {
   using type = float4;
 };
 
 template <typename T>
-class BaseActivationGPUFunctor {
+class BaseGPUFunctor {
  public:
-  using ELEMENT_TYPE = T;
+  using ELEMENT_TYPE_ = T;
 };
 
+/* ========================================================================== */
+
+/* ===========================    relu forward   ============================ */
 template <typename Type>
-class ReluGPUFunctor : public BaseActivationGPUFunctor<Type> {
+class ReluGPUFuctor : public BaseGPUFunctor<Type> {
  public:
   // for relu forward and backward when T is double
   template <typename T>
-  __device__ __forceinline__ T compute(const T* a, const T* b) {
-#ifdef __CUDA_ARCH__ >= 350 || HIP_VERSION >= 300
-    return __ldg(a) > static_cast<T>(0) ? __ldg(b) : static_cast<T>(0);
-#else
-    return (*a) > static_cast<T>(0) ? (*b) : static_cast<T>(0);
-#endif
-  }
-
+  __device__ __forceinline__ T Compute(const T* a);
   // when num % vecsize != 0 this func will be used
   template <typename T>
-  __device__ __forceinline__ T computeReminder(const T a, const T b) {
-    return a > static_cast<T>(0) ? b : static_cast<T>(0);
+  __device__ __forceinline__ T ComputeReminder(const T a) {
+    return a > static_cast<T>(0.0f) ? a : static_cast<T>(0.0f);
   }
 };
 
 template <>
+__device__ __forceinline__ double ReluGPUFuctor<double>::Compute<double>(
+    const double* a) {
+#ifdef __CUDA_ARCH__ >= 350 || HIP_VERSION >= 300
+  return __ldg(a) > static_cast<double>(0.0f) ? __ldg(a)
+                                              : static_cast<double>(0.0f);
+#else
+  return (*a) > static_cast<double>(0.0f) ? (*a) : static_cast<double>(0.0f);
+#endif
+}
+
+template <>
 __device__ __forceinline__ float4
-ReluGPUFunctor<float>::compute<float4>(const float4* a, const float4* b) {
+ReluGPUFuctor<float>::Compute<float4>(const float4* a) {
+  return make_float4((a->x > 0.0f) * (a->x), (a->y > 0.0f) * (a->y),
+                     (a->z > 0.0f) * (a->z), (a->w > 0.0f) * (a->w));
+}
+
+template <>
+__device__ __forceinline__ __half2
+ReluGPUFuctor<float16>::Compute<__half2>(const __half2* a) {
+#ifdef __CUDA_ARCH__ >= 530 || CUDA_VERSION >= 300
+  const half2 kzero = __float2half2_rn(0.0f);
+  return __hmul2(__hgt2(__ldg(a), kzero), __ldg(a));
+#else
+  const float2 xx = __half22float2(*a);
+  return __floats2half2_rn((xx.x > 0.0f) * static_cast<float>(xx.x),
+                           (xx.y > 0.0f) * static_cast<float>(xx.y));
+#endif
+}
+/* ========================================================================== */
+
+/* ===========================    relu backward   ============================
+ */
+
+template <typename Type>
+class ReluGradGPUFunctor : public BaseGPUFunctor<Type> {
+ public:
+  // for relu forward and backward when T is double
+  template <typename T>
+  __device__ __forceinline__ T Compute(const T* a, const T* b);
+
+  // when num % vecsize != 0 this func will be used
+  template <typename T>
+  __device__ __forceinline__ T ComputeReminder(const T a, const T b) {
+    return a > static_cast<T>(0) ? b : static_cast<T>(0);
+  }
+
+  static constexpr ActBwdOpFwdDeps FwdDeps() { return kDepOut; }
+};
+
+template <>
+__device__ __forceinline__ double ReluGradGPUFunctor<double>::Compute<double>(
+    const double* a, const double* b) {
+#ifdef __CUDA_ARCH__ >= 350 || HIP_VERSION >= 300
+  return __ldg(a) > static_cast<double>(0.0f) ? __ldg(b)
+                                              : static_cast<double>(0.0f);
+#else
+  return (*a) > static_cast<double>(0.0f) ? (*b) : static_cast<double>(0.0f);
+#endif
+}
+
+template <>
+__device__ __forceinline__ float4
+ReluGradGPUFunctor<float>::Compute<float4>(const float4* a, const float4* b) {
   return make_float4((a->x > 0.0f) * (b->x), (a->y > 0.0f) * (b->y),
                      (a->z > 0.0f) * (b->z), (a->w > 0.0f) * (b->w));
 }
 
 template <>
 __device__ __forceinline__ __half2
-ReluGPUFunctor<float16>::compute<__half2>(const __half2* a, const __half2* b) {
+ReluGradGPUFunctor<float16>::Compute<__half2>(const __half2* a,
+                                              const __half2* b) {
 #ifdef __CUDA_ARCH__ >= 530 || CUDA_VERSION >= 300
   const half2 kzero = __float2half2_rn(0.0f);
   return __hmul2(__hgt2(__ldg(a), kzero), __ldg(b));
@@ -85,36 +144,60 @@ ReluGPUFunctor<float16>::compute<__half2>(const __half2* a, const __half2* b) {
 #endif
 }
 
-template <typename T, typename VecType, int VecSize, typename Functor>
-__global__ void reluKernelCudaVec(const T* src1, const T* src2, T* dst, int num,
-                                  Functor functor) {
+/* ========================================================================== */
+
+template <typename T, typename VecType, typename Functor>
+__global__ void ActivationGradKernelVec(const T* forward_data, const T* dout,
+                                        T* dx, int num, int vecsize,
+                                        Functor functor) {
   int idx = threadIdx.x + blockIdx.x * blockDim.x;
   int stride = blockDim.x * gridDim.x;
-  int loop = num / VecSize;
-  int tail = num % VecSize;
-  const VecType* in1 = reinterpret_cast<const VecType*>(src1);
-  const VecType* in2 = reinterpret_cast<const VecType*>(src2);
-  VecType* out = reinterpret_cast<VecType*>(dst);
+  int loop = num / vecsize;
+  int tail = num % vecsize;
+  const VecType* in_forward = reinterpret_cast<const VecType*>(forward_data);
+  const VecType* in_dout = reinterpret_cast<const VecType*>(dout);
+  VecType* out = reinterpret_cast<VecType*>(dx);
 
   for (int i = idx; i < loop; i += stride) {
-    out[i] = functor.compute((in1 + i), (in2 + i));
+    out[i] = functor.Compute((in_forward + i), (in_dout + i));
   }
 
   while (idx == loop && tail) {
-    dst[num - tail] =
-        functor.computeReminder(src1[num - tail], src2[num - tail]);
+    dx[num - tail] =
+        functor.ComputeReminder(forward_data[num - tail], dout[num - tail]);
     --tail;
   }
 }
 
-template <typename DeviceContext, typename Functor, int VecSize>
+template <typename T, typename VecType, typename Functor>
+__global__ void ActivationKerenlVec(const T* src, T* dst, int num, int vecsize,
+                                    Functor functor) {
+  int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+  int loop = num / vecsize;
+  int tail = num % vecsize;
+  const VecType* in = reinterpret_cast<const VecType*>(src);
+  VecType* out = reinterpret_cast<VecType*>(dst);
+
+  for (int i = idx; i < loop; i += stride) {
+    out[i] = functor.Compute((in + i));
+  }
+
+  while (idx == loop && tail) {
+    dst[num - tail] = functor.ComputeReminder(src[num - tail]);
+    --tail;
+  }
+}
+
+template <typename DeviceContext, typename Functor>
 class ActivationGPUKernel
-    : public framework::OpKernel<typename Functor::ELEMENT_TYPE> {
+    : public framework::OpKernel<typename Functor::ELEMENT_TYPE_> {
  public:
-  using T = typename Functor::ELEMENT_TYPE;
+  using T = typename Functor::ELEMENT_TYPE_;
   void Compute(const framework::ExecutionContext& context) const override {
-    const Tensor* in_x = context.Input<Tensor>("X");
-    Tensor* out = context.Output<Tensor>("Out");
+    const framework::Tensor* in_x = nullptr;
+    framework::Tensor* out = nullptr;
+    ExtractActivationTensor(context, &in_x, &out);
     auto& dev_ctx = context.template device_context<DeviceContext>();
 
     int num = in_x->numel();
@@ -122,50 +205,62 @@ class ActivationGPUKernel
     T* output_data = out->mutable_data<T>(dev_ctx.GetPlace(),
                                           static_cast<size_t>(num * sizeof(T)));
 
+    using VecType = typename GetVecType<T>::type;
+    // int vecsize = typename GetVecType<T>::vecsize;
+    int vecsize = sizeof(VecType) / sizeof(T);
     int block = 512;
     int grid = (num + block - 1) / block;
-    if (num > VecSize) {
+    if (num > vecsize) {
       // update grid for float16 and float32
-      grid = (num / VecSize + block - 1) / block;
+      grid = (num / vecsize + block - 1) / block;
     }
 
     Functor functor;
-    using VecType = typename GetVecType<T, VecSize>::type;
-    reluKernelCudaVec<T, VecType, VecSize, Functor><<<grid, block>>>(
-        input_data, input_data, output_data, num, functor);
+    ActivationKerenlVec<T, VecType, Functor><<<grid, block>>>(
+        input_data, output_data, num, vecsize, functor);
   }
 };
 
-template <typename DeviceContext, typename Functor, int VecSize>
+template <typename DeviceContext, typename Functor>
 class ActivationGradGPUKernel
-    : public framework::OpKernel<typename Functor::ELEMENT_TYPE> {
+    : public framework::OpKernel<typename Functor::ELEMENT_TYPE_> {
  public:
-  using T = typename Functor::ELEMENT_TYPE;
-  void Compute(const framework::ExecutionContext& ctx) const override {
-    const framework::Tensor* out = ctx.Input<framework::Tensor>("Out");
-    const framework::Tensor* d_out =
-        ctx.Input<framework::Tensor>(framework::GradVarName("Out"));
-    framework::Tensor* d_x =
-        ctx.Output<framework::Tensor>(framework::GradVarName("X"));
-
-    auto& dev_ctx = ctx.template device_context<DeviceContext>();
-    auto* out_data = out->data<T>();
+  using T = typename Functor::ELEMENT_TYPE_;
+  void Compute(const framework::ExecutionContext& context) const override {
+    const framework::Tensor *x, *out, *d_out;
+    framework::Tensor* d_x = nullptr;
+    x = out = d_out = nullptr;
+    ExtractActivationGradTensor<Functor::FwdDeps()>(context, &x, &out, &d_out,
+                                                    &d_x);
     auto numel = d_out->numel();
-    auto* dout_data = d_out->data<T>();
+    auto& dev_ctx = context.template device_context<DeviceContext>();
     auto* dx_data = d_x->mutable_data<T>(
         dev_ctx.GetPlace(), static_cast<size_t>(numel * sizeof(T)));
+    auto* dout_data = d_out->data<T>();
 
+    auto* forward_data = dout_data;
+    if (static_cast<int>(Functor::FwdDeps()) == static_cast<int>(kDepOut)) {
+      forward_data = out->data<T>();
+      // only out
+    } else if (static_cast<int>(Functor::FwdDeps()) ==
+               static_cast<int>(kDepX)) {
+      // only input
+      forward_data = x->data<T>();
+    }
+
+    using VecType = typename GetVecType<T>::type;
+    // int vecsize = typename GetVecType<T>::vecsize;
+    int vecsize = sizeof(VecType) / sizeof(T);
     int block = 512;
     int grid = (numel + block - 1) / block;
-    if (numel >= VecSize) {
+    if (numel >= vecsize) {
       // update grid for float16 and float32
-      grid = (numel / VecSize + block - 1) / block;
+      grid = (numel / vecsize + block - 1) / block;
     }
 
     Functor functor;
-    using VecType = typename GetVecType<T, VecSize>::type;
-    reluKernelCudaVec<T, VecType, VecSize, Functor><<<grid, block>>>(
-        out_data, dout_data, dx_data, numel, functor);
+    ActivationGradKernelVec<T, VecType, Functor><<<grid, block>>>(
+        forward_data, dout_data, dx_data, numel, vecsize, functor);
   }
 };
 
@@ -174,6 +269,7 @@ class ActivationGradGPUKernel
 
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
+
 #define REGISTER_ACTIVATION_CUDA_KERNEL(act_type, op_name, functor,         \
                                         grad_functor)                       \
   REGISTER_OP_CUDA_KERNEL(                                                  \
@@ -221,19 +317,19 @@ REGISTER_OP_CUDA_KERNEL(
 /* ===========================    relu register  ============================ */
 REGISTER_OP_CUDA_KERNEL(
     relu, ops::ActivationGPUKernel<paddle::platform::CUDADeviceContext,
-                                   ops::ReluGPUFunctor<float>, 4>,
+                                   ops::ReluGPUFuctor<float>>,
     ops::ActivationGPUKernel<paddle::platform::CUDADeviceContext,
-                             ops::ReluGPUFunctor<double>, 1>,
+                             ops::ReluGPUFuctor<double>>,
     ops::ActivationGPUKernel<plat::CUDADeviceContext,
-                             ops::ReluGPUFunctor<plat::float16>, 2>);
+                             ops::ReluGPUFuctor<plat::float16>>);
 
 REGISTER_OP_CUDA_KERNEL(
     relu_grad, ops::ActivationGradGPUKernel<paddle::platform::CUDADeviceContext,
-                                            ops::ReluGPUFunctor<float>, 4>,
+                                            ops::ReluGradGPUFunctor<float>>,
     ops::ActivationGradGPUKernel<paddle::platform::CUDADeviceContext,
-                                 ops::ReluGPUFunctor<double>, 1>,
+                                 ops::ReluGradGPUFunctor<double>>,
     ops::ActivationGradGPUKernel<plat::CUDADeviceContext,
-                                 ops::ReluGPUFunctor<plat::float16>, 2>);
+                                 ops::ReluGradGPUFunctor<plat::float16>>);
 
 REGISTER_OP_CUDA_KERNEL(
     relu_grad_grad,
