a549aebbb79c2616060087f8cd45e738ed65301c
zhiqiu <chenqiuliang@baidu.com>
2021-03-16 03:12:09 +0000

    modify atol

--- python/paddle/fluid/tests/unittests/npu/test_adam_op_npu.py
+++ python/paddle/fluid/tests/unittests/npu/test_adam_op_npu.py
@@ -75,7 +75,7 @@ class TestSGD(OpTest):
         self.dtype = np.float32
 
     def test_check_output(self):
-        self.check_output_with_place(self.place, check_dygraph=False)
+        self.check_output_with_place(self.place, atol=1e-5, check_dygraph=False)
 
 
 '''
