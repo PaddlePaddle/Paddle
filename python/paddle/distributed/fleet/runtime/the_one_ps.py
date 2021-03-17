287eaec46ddb5f5062bef5b2d8b58f48ff208d21
MrChengmo <cmchengmo@163.com>
2021-03-17 11:05:46 +0800

    add unittest

--- python/paddle/distributed/fleet/runtime/the_one_ps.py
+++ python/paddle/distributed/fleet/runtime/the_one_ps.py
@@ -150,7 +150,8 @@ class CommonAccessor:
         oop = None
 
         for op in optimizer_ops:
-            if "Param" in op.input_names and op.input("Param")[0] == param_name:
+            if ("Param" in op.input_names) and (
+                    op.input("Param")[0] == param_name):
                 oop = op
                 break
 
@@ -187,8 +188,7 @@ class CommonAccessor:
                     shape = self.get_shard(total_dims, pserver_num, pserver_id)
             dims.append(shape)
 
-            initializer = self.get_initializer_attr(
-                param.name, startup_program)
+            initializer = self.get_initializer_attr(param.name, startup_program)
             initializers.append(initializer)
 
         for (attr_varname, type_) in attr_varnames:
@@ -544,8 +544,7 @@ class TheOnePSRuntime(RuntimeBase):
             # for ps-heter mode, wait heter worker ready
             if self.role_maker._is_heter_parameter_server_mode and self.role_maker._is_worker(
             ):
-                wait_server_ready(
-                    self.role_maker._get_heter_worker_endpoints())
+                wait_server_ready(self.role_maker._get_heter_worker_endpoints())
 
                 self._heter_client = HeterClient(
                     self.role_maker._get_heter_worker_endpoints(),
@@ -786,8 +785,7 @@ class TheOnePSRuntime(RuntimeBase):
         from paddle.fluid.incubate.fleet.parameter_server.ir.public import get_sparse_tablenames
 
         dist_varnames = get_sparse_tablenames(self.origin_main_program, True)
-        sparse_varnames = get_sparse_tablenames(
-            self.origin_main_program, False)
+        sparse_varnames = get_sparse_tablenames(self.origin_main_program, False)
 
         distributed_varnames = dist_varnames + sparse_varnames
 
