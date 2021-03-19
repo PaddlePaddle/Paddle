aa50d2a0af0b8360e4c0af724712910cad7933af
chajchaj <306536853@qq.com>
2021-03-19 07:16:14 +0000

    fix readme format, test=develop

--- python/paddle/nn/layer/loss.py
+++ python/paddle/nn/layer/loss.py
@@ -157,80 +157,80 @@ class CrossEntropyLoss(fluid.dygraph.Layer):
 
     The calculation of this operator includes the following two steps.
 
-    -  I. softmax cross entropy 
+    -  **I.softmax cross entropy** 
 
-    1. Hard label (each sample can only be assigned into one category)
+        1. Hard label (each sample can only be assigned into one category)
 
-    .. math::
-      \\loss_j=-\text{logits}_{label_j}+\log\left(\sum_{i=0}^{C}\exp(\text{logits}_i)\right) , j = 1,...,N
+        .. math::
+          \\loss_j=-\text{logits}_{label_j}+\log\left(\sum_{i=0}^{C}\exp(\text{logits}_i)\right) , j = 1,...,N
 
-    where, N is the number of samples and C is the number of categories.
+        where, N is the number of samples and C is the number of categories.
 
-    2. Soft label (each sample is assigned to multiple categories with a certain probability, 
-    and the probability sum is 1).
+        2. Soft label (each sample is assigned to multiple categories with a certain probability, 
+        and the probability sum is 1).
 
-    .. math::
-      \\loss_j=-\sum_{i=0}^{C}\text{label}_i\left(\text{logits}_i-\log\left(\sum_{i=0}^{C}\exp(\text{logits}_i)\right)\right) , j = 1,...,N
+        .. math::
+          \\loss_j=-\sum_{i=0}^{C}\text{label}_i\left(\text{logits}_i-\log\left(\sum_{i=0}^{C}\exp(\text{logits}_i)\right)\right) , j = 1,...,N
 
-    where, N is the number of samples and C is the number of categories.
-
-
-    -  II. Weight and reduction processing 
-
-    1. Weight
-
-    If the ``weight`` parameter is ``none`` , go to the next step directly.
-
-    If the ``weight`` parameter is not ``none`` , the cross entropy of each sample is weighted by weight
-    according to soft_label = False or True as follows.
-
-    1.1. Hard labels (soft_label = False)
-
-    .. math::
-        \\loss_j=loss_j*weight[label_j] 
+        where, N is the number of samples and C is the number of categories.
 
 
-    1.2. Soft labels (soft_label = True)
+    -  **II.Weight and reduction processing** 
 
-     .. math::
-        \\loss_j=loss_j*\sum_{i}\left(weight[label_i]*logits_i\right)
+        1. Weight
 
-    2. reduction
+        If the ``weight`` parameter is ``none`` , go to the next step directly.
 
-    2.1 if the ``reduction`` parameter is ``none`` 
+        If the ``weight`` parameter is not ``none`` , the cross entropy of each sample is weighted by weight
+        according to soft_label = False or True as follows.
 
-    Return the previous result directly
+        1.1. Hard labels (soft_label = False)
 
-    2.2 if the ``reduction`` parameter is ``sum`` 
+        .. math::
+            \\loss_j=loss_j*weight[label_j] 
 
-    Return the sum of the previous results
 
-    .. math::
-       \\loss=\sum_{j}loss_j
+        1.2. Soft labels (soft_label = True)
 
-    2.3 if the ``reduction`` parameter is ``mean`` , it will be processed according to 
-    the ``weight`` parameter as follows. 
+         .. math::
+            \\loss_j=loss_j*\sum_{i}\left(weight[label_i]*logits_i\right)
 
-    2.3.1. If the  ``weight``  parameter is ``none`` 
+        2. reduction
 
-    Return the average value of the previous results
+        2.1 if the ``reduction`` parameter is ``none`` 
 
-     .. math::
-        \\loss=\sum_{j}loss_j/N
+        Return the previous result directly
 
-    where, N is the number of samples and C is the number of categories.
+        2.2 if the ``reduction`` parameter is ``sum`` 
 
-    2.3.2. If the 'weight' parameter is not 'none', the weighted average value of the previous result will be returned
+        Return the sum of the previous results
 
-    (1) Hard labels (soft_label = False)
+        .. math::
+           \\loss=\sum_{j}loss_j
 
-     .. math::
-        \\loss=\sum_{j}loss_j/\sum_{j}weight[label_j] 
+        2.3 if the ``reduction`` parameter is ``mean`` , it will be processed according to 
+        the ``weight`` parameter as follows. 
 
-    (2) Soft labels (soft_label = True)
+        2.3.1. If the  ``weight``  parameter is ``none`` 
 
-     .. math::
-        \\loss=\sum_{j}loss_j/\sum_{j}\left(\sum_{i}weight[label_i]\right)
+        Return the average value of the previous results
+
+         .. math::
+            \\loss=\sum_{j}loss_j/N
+
+        where, N is the number of samples and C is the number of categories.
+
+        2.3.2. If the 'weight' parameter is not 'none', the weighted average value of the previous result will be returned
+
+        (1) Hard labels (soft_label = False)
+
+         .. math::
+            \\loss=\sum_{j}loss_j/\sum_{j}weight[label_j] 
+
+        (2) Soft labels (soft_label = True)
+
+         .. math::
+            \\loss=\sum_{j}loss_j/\sum_{j}\left(\sum_{i}weight[label_i]\right)
  
  
     Parameters:
@@ -274,6 +274,7 @@ class CrossEntropyLoss(fluid.dygraph.Layer):
 
             The name of the operator. Default is ``None`` .
             For more information, please refer to :ref:`api_guide_Name` .
+
 
     Shape:
 
