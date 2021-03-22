bfced39eb6c7e1c65cd704bc638798c25c2004bb
zlsh80826 <rewang@nvidia.com>
2021-03-22 14:49:50 +0800

    [Paddle-TRT] nearest_interp op (#31626)
    * nearest_interp op converter w/ dynamic/static
    
    * fix data_layout include
    
    * add trt nearest unit_test
    
    * add nearest_interp NHWC test
    
    * update trt nearest interp nhwc testcase
    
    * remove asterisk for python2 compatibility
    
    * add empty line to prevent conflict
    
    * nearest_interp op converter w/ dynamic/static
    
    * fix data_layout include
    
    * add trt nearest unit_test
    
    * add nearest_interp NHWC test
    
    * update trt nearest interp nhwc testcase
    
    * remove asterisk for python2 compatibility
    
    * add empty line to prevent conflict
    
    * change the priority of out_h, out_w

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -14,6 +14,7 @@
 
 #include "paddle/fluid/inference/tensorrt/op_teller.h"
 #include "paddle/fluid/framework/block_desc.h"
+#include "paddle/fluid/framework/data_layout.h"
 
 namespace paddle {
 namespace framework {
@@ -110,6 +111,8 @@ struct SimpleOpTypeSetTeller : public Teller {
       "flatten2",
       "flatten",
       "gather",
+
+      "nearest_interp",
   };
 };
 
@@ -187,10 +190,29 @@ bool OpTeller::Tell(const framework::ir::Node* node, bool use_no_calib_int8,
         if (axis != 1) return false;
       }
     }
+
     if (op_type == "gather") {
       // current not support axis from input, use default 0
       if (!with_dynamic_shape || desc.Input("Axis").size() > 0) return false;
     }
+
+    if (op_type == "nearest_interp") {
+      std::vector<std::string> attrs{"data_layout",   "interp_method",
+                                     "align_corners", "scale",
+                                     "out_h",         "out_w"};
+      for (auto const attr : attrs) {
+        if (!desc.HasAttr(attr)) return false;
+      }
+      auto data_layout = framework::StringToDataLayout(
+          BOOST_GET_CONST(std::string, desc.GetAttr("data_layout")));
+      if (data_layout != framework::DataLayout::kNCHW &&
+          data_layout != framework::DataLayout::kNHWC)
+        return false;
+      auto interp_method =
+          BOOST_GET_CONST(std::string, desc.GetAttr("interp_method"));
+      if (interp_method != "nearest") return false;
+    }
+
     if ((*teller)(op_type, desc, use_no_calib_int8)) return true;
   }
   return false;
