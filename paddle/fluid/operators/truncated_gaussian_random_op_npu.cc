e87258bd7889299db31c728d0f3eeabc20669fe0
zhiqiu <chenqiuliang@baidu.com>
2021-03-25 10:16:18 +0000

    add npu kernel for truncated_gaussian_random

--- paddle/fluid/operators/truncated_gaussian_random_op_npu.cc
+++ paddle/fluid/operators/truncated_gaussian_random_op_npu.cc
@@ -1,8 +1,11 @@
 /* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -68,14 +71,41 @@ class TruncatedGaussianRandomNPUKernel : public framework::OpKernel<T> {
   }
 };
 
+// NOTE(zhiqiu): actually, this is cpu version kernel, and we need to make the
+// above
+// npu version work in the future.
+template <typename T>
+class NPUTruncatedGaussianRandomKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& context) const override {
+    float mean = context.Attr<float>("mean");
+    float std = context.Attr<float>("std");
+    auto* tensor = context.Output<framework::Tensor>("Out");
+    tensor->mutable_data<T>(context.GetPlace());
+
+    Tensor cpu_tensor(tensor->type());
+    cpu_tensor.Resize(tensor->dims());
+    T* cpu_data = cpu_tensor.mutable_data<T>(platform::CPUPlace());
+    std::uniform_real_distribution<T> dist(std::numeric_limits<float>::min(),
+                                           1.0);
+    TruncatedNormal<T> truncated_normal(mean, std);
+    int64_t size = tensor->numel();
+
+    unsigned int seed = static_cast<unsigned int>(context.Attr<int>("seed"));
+    auto engine = framework::GetCPURandomEngine(seed);
+    for (int64_t i = 0; i < size; ++i) {
+      cpu_data[i] = truncated_normal(dist(*engine));
+    }
+    framework::TensorCopy(
+        cpu_tensor, context.GetPlace(),
+        context.template device_context<platform::DeviceContext>(), tensor);
+  }
+};
+
 }  // namespace operators
 }  // namespace paddle
 
 namespace ops = paddle::operators;
 
-REGISTER_OP_NPU_KERNEL(
-    truncated_gaussian_random,
-    ops::TruncatedGaussianRandomNPUKernel<paddle::platform::NPUDeviceContext,
-                                          float>,
-    ops::TruncatedGaussianRandomNPUKernel<paddle::platform::NPUDeviceContext,
-                                          paddle::platform::float16>);
+REGISTER_OP_NPU_KERNEL(truncated_gaussian_random,
+                       ops::NPUTruncatedGaussianRandomKernel<float>);
--- python/paddle/fluid/tests/unittests/npu/test_truncated_gaussian_random_op_npu.py
+++ python/paddle/fluid/tests/unittests/npu/test_truncated_gaussian_random_op_npu.py
@@ -31,60 +31,37 @@ SEED = 2021
 
 @unittest.skipIf(not paddle.is_compiled_with_npu(),
                  "core is not compiled with NPU")
-class TestTrunctedGaussianRandom(OpTest):
-    def setUp(self):
-        self.set_npu()
-        self.op_type = "truncated_gaussian_random"
-        self.place = paddle.NPUPlace(0)
-        self.inputs = {}
-        self.attrs = {
-            "shape": [10],
-            "mean": .0,
-            "std": 1.,
-            "seed": 10,
-        }
+class TestPowNet(unittest.TestCase):
+    def _test(self, run_npu=True):
+        main_prog = paddle.static.Program()
+        startup_prog = paddle.static.Program()
+        main_prog.random_seed = SEED
+        startup_prog.random_seed = SEED
+        np.random.seed(SEED)
+        paddle.seed(SEED)
 
-        self.outputs = {"Out": np.random.random(10).astype(self.dtype)}
+        with paddle.static.program_guard(main_prog, startup_prog):
+            weight_attr = paddle.framework.ParamAttr(
+                name="linear_weight",
+                initializer=paddle.nn.initializer.TruncatedNormal(
+                    mean=0.0, std=2.0))
+            linear = paddle.nn.Linear(
+                2, 2, weight_attr=weight_attr, bias_attr=False)
 
-    def set_npu(self):
-        self.__class__.use_npu = True
+        if run_npu:
+            place = paddle.NPUPlace(0)
+        else:
+            place = paddle.CPUPlace()
 
-    def init_dtype(self):
-        self.dtype = np.float32
+        exe = paddle.static.Executor(place)
+        w = exe.run(startup_prog, fetch_list=['linear_weight'])
+        return w
 
-    def test_check_output(self):
-        self.check_output_with_place(self.place, check_dygraph=False)
+    def test_npu(self):
+        cpu_w = self._test(False)
+        npu_w = self._test(True)
 
-    #def test_npu(self):
-    #    self.gaussian_random_test(place=paddle.NPUPlace(0))
-
-    #def gaussian_random_test(self, place):
-
-    #    program = fluid.Program()
-    #    block = program.global_block()
-    #    vout = block.create_var(name="Out")
-    #    op = block.append_op(
-    #        type=self.op_type, outputs={"Out": vout}, attrs=self.attrs)
-
-    #    op.desc.infer_var_type(block.desc)
-    #    op.desc.infer_shape(block.desc)
-
-    #    fetch_list = []
-    #    for var_name in self.outputs:
-    #        fetch_list.append(block.var(var_name))
-
-    #    exe = Executor(place)
-    #    outs = exe.run(program, fetch_list=fetch_list)
-    #    tensor = outs[0]
-    #    self.assertAlmostEqual(numpy.mean(tensor), .0, delta=0.1)
-    #    self.assertAlmostEqual(numpy.var(tensor), 0.773, delta=0.1)
-
-    # TODO(ascendrc): Add grad test
-    # def test_check_grad(self):
-    #     if self.dtype == np.float16:
-    #         return
-    #     self.check_grad(['X'], 'Out')
-    #
+        self.assertTrue(np.allclose(npu_w, cpu_w))
 
 
 if __name__ == '__main__':
