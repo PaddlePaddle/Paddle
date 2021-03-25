0c57909102f481ff83c93d46ee7cedf17b741d9f
Ren Wei <wadefelix@gmail.com>
2021-03-25 15:39:30 +0800

    add required

--- python/paddle/distributed/parallel.py
+++ python/paddle/distributed/parallel.py
@@ -67,6 +67,7 @@ def init_parallel_env():
         
     Examples:
         .. code-block:: python
+            # required: gpu, distributed
 
             import paddle
             import paddle.nn as nn
