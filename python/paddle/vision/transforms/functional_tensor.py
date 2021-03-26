b24c29316aad8d68aac69c7c41e9cfaaf182f59b
lyuwenyu <wenyu.lyu@gmail.com>
2021-03-26 11:07:22 +0800

    add to_grayscale, normalize

--- python/paddle/vision/transforms/functional_tensor.py
+++ python/paddle/vision/transforms/functional_tensor.py
@@ -31,10 +31,48 @@ def normalize(img, mean, std, data_format='CHW'):
         Tensor: Normalized mage.
 
     """
-    if data_format == 'CHW':
-        mean = paddle.to_tensor(mean).reshape([-1, 1, 1])
-        std = paddle.to_tensor(std).reshape([-1, 1, 1])
-    else:
-        mean = paddle.to_tensor(mean)
-        std = paddle.to_tensor(std)
+    assert data_format.lower() in ('chw', 'hwc'
+                                   ), "data_format should in ('chw', 'hwc')"
+
+    mean = paddle.to_tensor(mean, place=img.place)
+    std = paddle.to_tensor(std, place=img.place)
+
+    if data_format.lower == 'chw':
+        mean = mean.reshape([-1, 1, 1])
+        std = std.reshape([-1, 1, 1])
+
     return (img - mean) / std
+
+
+def to_grayscale(img, num_output_channels=1, data_format='CHW'):
+    """Converts image to grayscale version of image.
+
+    Args:
+        img (paddel.Tensor): Image to be converted to grayscale.
+        num_output_channels (int, optionl[1, 3]):
+            if num_output_channels = 1 : returned image is single channel
+            if num_output_channels = 3 : returned image is 3 channel 
+        data_format (str, optional): Data format of img, should be 'HWC' or 
+            'CHW'. Default: 'CHW'.
+
+    Returns:
+        PIL.Image: Grayscale version of the image.
+    """
+    assert data_format.lower() in ('chw', 'hwc'
+                                   ), "data_format should in ('chw', 'hwc')"
+    assert num_output_channels in (1, 3), ""
+
+    rgb_weights = paddle.to_tensor(
+        [0.2989, 0.5870, 0.1140], place=img.place).astype(img.dtype)
+
+    if data_format.lower() == 'chw':
+        _c_index = -3
+        rgb_weights = rgb_weights.reshape((-1, 1, 1))
+    else:
+        _c_index = -1
+
+    img = (img * rgb_weights).sum(axis=_c_index, keepdim=True)
+    _shape = img.shape
+    _shape[_c_index] = num_output_channels
+
+    return img.expand(_shape)
