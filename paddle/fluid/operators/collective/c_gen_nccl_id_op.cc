09ef66f1f4ecd209aea7439b096d430c49921be5
WangXi <wangxi16@baidu.com>
2021-03-26 12:09:51 +0800

    fix port reuse when create multi group

--- paddle/fluid/operators/collective/c_gen_nccl_id_op.cc
+++ paddle/fluid/operators/collective/c_gen_nccl_id_op.cc
@@ -65,6 +65,9 @@ class CGenNCCLIdOp : public framework::OperatorBase {
       return Output("Out");
     };
 
+    std::string endpoint = Attr<std::string>("endpoint");
+    int server_fd = platform::SocketServer::GetInstance(endpoint).socket();
+
     std::vector<ncclUniqueId> nccl_ids;
     nccl_ids.resize(1);
 
@@ -74,8 +77,6 @@ class CGenNCCLIdOp : public framework::OperatorBase {
           Attr<std::vector<std::string>>("other_endpoints");
       platform::SendBroadCastCommID(endpoint_list, &nccl_ids);
     } else {
-      std::string endpoint = Attr<std::string>("endpoint");
-      int server_fd = platform::SocketServer::GetInstance(endpoint).socket();
       platform::RecvBroadCastCommID(server_fd, endpoint, &nccl_ids);
     }
 
