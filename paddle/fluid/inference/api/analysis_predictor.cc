2e70d7b757479930697c8bcf318cb0ea844f96f0
zlsh80826 <rewang@nvidia.com>
2021-03-29 14:16:56 +0800

    [Paddle-TRT] roi_align_plugin (#31732)
    * add roi_align_plugin
    
    * add roi align unit_test
    
    * add roi align serialization
    
    * remove roi align static plugin because of batch dim issue
    
    * refine roi align unittest and add fp16/serialization
    
    * add trt roi align condition to op_teller
    
    * refine error message
    
    * remove unnecessary reshape layer

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -1192,6 +1192,7 @@ USE_TRT_CONVERTER(scale);
 USE_TRT_CONVERTER(stack);
 USE_TRT_CONVERTER(clip);
 USE_TRT_CONVERTER(gather);
+USE_TRT_CONVERTER(roi_align);
 USE_TRT_CONVERTER(affine_channel);
 USE_TRT_CONVERTER(multiclass_nms);
 USE_TRT_CONVERTER(nearest_interp);
