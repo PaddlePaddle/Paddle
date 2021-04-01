90f214d936349a0fac1552602374093fdd90354a
frankwhzhang <frankwhzhang@126.com>
2021-04-01 03:18:04 +0000

    fix, test=develop

--- cmake/external/warpctc.cmake
+++ cmake/external/warpctc.cmake
@@ -21,10 +21,10 @@ ENDIF()
 SET(WARPCTC_PREFIX_DIR  ${THIRD_PARTY_PATH}/warpctc)
 SET(WARPCTC_SOURCE_DIR  ${THIRD_PARTY_PATH}/warpctc/src/extern_warpctc)
 SET(WARPCTC_INSTALL_DIR ${THIRD_PARTY_PATH}/install/warpctc)
-set(WARPCTC_REPOSITORY  https://gitee.com/tianjianhe/warp-ctc.git)
+# in case of low internet speed  
+#set(WARPCTC_REPOSITORY  https://gitee.com/tianjianhe/warp-ctc.git)
 set(WARPCTC_REPOSITORY  ${GIT_URL}/baidu-research/warp-ctc.git)
 set(WARPCTC_TAG         c690fc5755abbdbdc98ef78d51ec10a6748a8cd1)
-
 
 SET(WARPCTC_INCLUDE_DIR "${WARPCTC_INSTALL_DIR}/include"
     CACHE PATH "Warp-ctc Directory" FORCE)
