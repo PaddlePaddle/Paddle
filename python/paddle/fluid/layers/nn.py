bf1de59c5b266d18ade0cab91800b33a080d4706
danleifeng <danleifeng@163.com>
2021-04-07 15:59:06 +0800

    add uint8 type for flatten;test=develop

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -9962,7 +9962,9 @@ def flatten(x, axis=1, name=None):
 
         .. code-block:: python
 
+            import paddle
             import paddle.fluid as fluid
+            paddle.enable_static()
             x = fluid.data(name="x", shape=[4, 4, 3], dtype="float32")
             # x shape is [4, 4, 3]
             out = fluid.layers.flatten(x=x, axis=2)
