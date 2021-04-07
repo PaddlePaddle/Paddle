510629ef5b44795ee8ce81611ffb7194c98a3f66
XieYunshen <1084314248@qq.com>
2021-04-07 15:58:58 +0800

    test,test,notest,test=windows_ci

--- tools/windows/run_unittests.sh
+++ tools/windows/run_unittests.sh
@@ -225,20 +225,7 @@ if [ ${WITH_GPU:-OFF} == "ON" ];then
 
     set +e
     if [ ${PRECISION_TEST:-OFF} == "ON" ] && [[ "$precision_cases" != "" ]];then
-        UT_list_prec=''
-        re=$(cat ut_list|awk -F ' ' '{print }' | awk 'BEGIN{ all_str=""}{if (all_str==""){all_str=$1}else{all_str=all_str"$|^"$1}} END{print "^"all_str"$"}')
-        for case in $UT_list; do
-            flag=$(echo $case|grep -oE $re)
-            if [ -n "$flag" ];then
-                if [ -z "$UT_list_prec" ];then
-                    UT_list_prec=$case
-                else
-                    UT_list_prec=$UT_list_prec'\n'$case
-                fi
-            else
-                echo $case "won't run in PRECISION_TEST mode."
-            fi
-        done
+        UT_list_prec=$(python ${PADDLE_ROOT}/tools/windows/get_prec_ut_list.py "$UT_list" "$precision_cases" )
         UT_list=$UT_list_prec
     fi
     set -e
