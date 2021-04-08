e35a185620e266f1a787fe293c98215f1bbb4232
xiemoyuan <xiemoyuan@baidu.com>
2021-04-08 05:03:15 +0000

    Modified the return value of tensor.grad from numpy as tensor.

--- python/paddle/fluid/dygraph/varbase_patch_methods.py
+++ python/paddle/fluid/dygraph/varbase_patch_methods.py
@@ -337,10 +337,24 @@ def monkey_patch_varbase():
     @property
     def grad(self):
         """
-        The alias of gradient().
-        """
+        Get the Gradient of Current Tensor.
 
-        return self.gradient()
+        Returns:
+            Tensor: the gradient of current Tensor
+
+        Examples:
+            .. code-block:: python
+
+                import paddle
+
+                x = paddle.to_tensor(5., stop_gradient=False)
+                y = paddle.pow(x, 4.0)
+                y.backward()
+                print("grad of x: {}".format(x.grad))
+                # Tensor(shape=[1], dtype=float32, place=CUDAPlace(0), stop_gradient=False, [500.])
+
+        """
+        return self._grad_ivar()
 
     def clear_grad(self):
         """
