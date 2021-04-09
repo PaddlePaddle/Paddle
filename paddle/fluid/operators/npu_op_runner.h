ccf5709d3f9958dccedf1b79e3c834fc2398b9c2
Leo Chen <chenqiuliang@baidu.com>
2021-04-09 17:29:20 +0800

    [NPU] cherry-pick basic NPU components/allocator/operator/executor supports from ascendrc  (#32144)
    * [feature] support npu allocator (#30840)
    
    [feature] support npu allocator
    
    * [feature] support npu operator (#30951)
    
    [feature] support npu operator
    
    * [feature] support npu allocator, part 2 (#30972)
    
    * support npu allocator
    
    * add npu device context
    
    * fix some compile problem
    
    * fix some compile problem
    
    * add npu info
    
    * compile ok
    
    * fix include dir
    
    * support naive_best_fit_allocator
    
    * run ut ok, bug failed to exit
    
    * call aclrtResetDevice before exit
    
    * fix aclFinilize
    
    * add system allocatot test
    
    * add selected_gpus in gtest
    
    * add tensor_test for npu
    
    * support npu op, initial commit
    
    * add npu stream
    
    * add elementwise_add_op
    
    * compile ok
    
    * fix typo
    
    * fix elementwise_add_op_npu_test
    
    * support op run
    
    * test can run but failed
    
    * change aclopExecuteV2 to aclopCompileAndExecute
    
    * support parsing ascend rank table file (#31000)
    
    support parsing ascend rank table file
    
    * Fix reshape on GE graph. (#31084)
    
    Fix reshape on GE graph
    
    * add npu kernel for elementwise_sub and elementwise_sub_grad (#30973)
    
    * add npu sub op
    
    * fix typo
    
    * rename test
    
    * fix bug
    
    * fix bug
    
    * add fp16 kernel
    
    * fix typo
    
    * support sub grad op
    
    * support elementwise_sub_grad op
    
    Co-authored-by: frankwhzhang <frankwhzhang@126.com>
    
    * Fix compilation problem (#31100)
    
    Fix compilation problem (#31100)
    
    * fix compile
    
    * fix code stype
    
    * remove const_cast
    
    * support adding correct npu op in pybind.h (#31143)
    
    * support adding correct npu op in pybind.h
    
    * refine code
    
    * [NPU] Support executor with NPU (#31057)
    
    * [NPU] Support executor with NPU
    
    * Fix code according to reviews
    
    * Fix code
    
    * Add unittest for sub op npu
    
    * refactor npu device manager (#31154)
    
    refactor npu device manager (#31154)
    
    * fix selected npus
    
    * fix compile
    
    * fix reading flags from env
    
    * format
    
    Co-authored-by: xiayanming <41795079@qq.com>
    Co-authored-by: gongweibao <weibao.gong@gmail.com>
    Co-authored-by: frankwhzhang <frankwhzhang@126.com>
    Co-authored-by: liym27 <33742067+liym27@users.noreply.github.com>

---
+++ paddle/fluid/operators/npu_op_runner.h
@@ -0,0 +1,84 @@
+/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+#include <paddle/fluid/framework/operator.h>
+
+#include <string>
+#include <vector>
+
+#include "acl/acl.h"
+#include "paddle/fluid/operators/npu_op_runner.h"
+
+namespace paddle {
+namespace operators {
+
+using Tensor = framework::Tensor;
+using DataLayout = framework::DataLayout;
+using Attribute = framework::Attribute;
+using AttributeMap = framework::AttributeMap;
+
+class NpuOpRunner {
+ public:
+  explicit NpuOpRunner(std::string op_type);
+  explicit NpuOpRunner(std::string op_type,
+                       const std::vector<Tensor> &inputs = {},
+                       const std::vector<Tensor> &outputs = {},
+                       const AttributeMap &attrs = {});
+
+  ~NpuOpRunner();
+
+  const std::string &Type();
+
+  NpuOpRunner &AddAttr(const std::string &name, const Attribute &attr);
+
+  NpuOpRunner &AddAttrs(const AttributeMap &attrs);
+
+  NpuOpRunner &AddInput(const Tensor &tensor);
+
+  NpuOpRunner &AddOutput(const Tensor &tensor);
+
+  NpuOpRunner &AddInputs(const std::vector<Tensor> &tensors);
+
+  NpuOpRunner &AddOutputs(const std::vector<Tensor> &tensors);
+
+  aclTensorDesc *GetInputDesc(size_t index);
+
+  aclTensorDesc *GetOutputDesc(size_t index);
+
+  std::vector<aclTensorDesc *> &GetInputDescs();
+
+  std::vector<aclTensorDesc *> &GetOutputDescs();
+
+  std::vector<aclDataBuffer *> &GetInputBuffers();
+
+  std::vector<aclDataBuffer *> &GetOutputBuffers();
+
+  void Run(aclrtStream stream);
+
+ private:
+  aclTensorDesc *CreateTensorDesc(Tensor tensor);
+  aclDataBuffer *CreateDataBuffer(Tensor tensor);
+
+ private:
+  std::string op_type_;
+  std::vector<aclDataBuffer *> input_buffers_;
+  std::vector<aclDataBuffer *> output_buffers_;
+  std::vector<aclTensorDesc *> input_descs_;
+  std::vector<aclTensorDesc *> output_descs_;
+  aclopAttr *attr_{nullptr};
+};
+
+}  // namespace operators
+}  // namespace paddle
