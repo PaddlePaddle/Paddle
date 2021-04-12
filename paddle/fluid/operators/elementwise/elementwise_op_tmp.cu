bb80296deb8c02699dcff0dca723c61f8fc06fd8
JamesLim-sy <santamonic@sina.com>
2021-04-12 20:00:34 +0000

    Adding the load logics

--- paddle/fluid/operators/elementwise/elementwise_op_tmp.cu
+++ paddle/fluid/operators/elementwise/elementwise_op_tmp.cu
@@ -36,282 +36,95 @@ class ElementwiseKernel : public framework::OpKernel<T> {
   void Compute(const framework::ExecutionContext &ctx) const override {
     auto in_tensors = ctx.MultiInput<framework::LoDTensor>("Input");
     auto *out_tensor = ctx.Output<framework::LoDTensor>("Out");
-    bool is_broadcast;
+    bool no_broadcast = true;
 
     using vec_input_t = std::vector<const framework::Tensor *>;
-    vec_input_t in_array;
-    in_array.reserve(in_tensors.size());
+    vec_input_t in_tensor_arr;
+    in_tensor_arr.reserve(in_tensors.size());
     for (auto *in_tensor : in_tensors) {
-      is_broadcast = in_tensor->dims() == out_tensor->dims();
-      in_array.emplace_back(*in_tensor);
+      no_broadcast &= in_tensor->dims() == out_tensor->dims();
+      in_tensor_arr.emplace_back(in_tensor);
     }
-
-    if (!is_broadcast && (in_tensors.size() == 1)) {
-      SameDimsElemwise<DeviceContext, T, Functor, vec_input_t>(ctx, &in_array,
-                                                               out_tensor);
+    if (no_broadcast || (in_tensors.size() == 1)) {
+      SameDimsElemwise<DeviceContext, T, Functor, vec_input_t>(
+          ctx, in_tensor_arr, out_tensor);
     } else {
       BroadcastElementwise<DeviceContext, T, Functor, vec_input_t>(
-          ctx, &in_array, out_tensor);
+          ctx, in_tensor_arr, out_tensor);
     }
   }
 };
 
-template <int N, typename vec_input_t>
-void MergeDims(vec_input_t *ins, framework::DDim *out_dims) {
-  PADDLE_ENFORCE_GE(
-      out_dims->size(), MAX_DIMS,
-      platform::errors::InvalidArgument(
-          "Output tensor`s dim is %d, bigger than upper limitation %d\n",
-          paddle::framework::DataTypeToString(framework::proto::VarType::INT32),
-          paddle::framework::DataTypeToString(
-              framework::proto::VarType::INT32)));
-
-  for (*in_tensor : ins) {
-    PADDLE_ENFORCE_GE(
-        in_tensor->dims().size(), MAX_DIMS,
-        platform::errors::InvalidArgument(
-            "Input tensor`s dim is %d, bigger than upper limitation %d\n",
-            paddle::framework::DataTypeToString(
-                framework::proto::VarType::INT32),
-            paddle::framework::DataTypeToString(
-                framework::proto::VarType::INT32)));
-  }
-}
-
-template <typename vec_t, typename T, int N>
-void DimsReorganise(std::vector<vec_t> *in_tenosr_data, vec_t *out_tensor_data,
-                    int *out_size, int *max_idx, int low_idx, int cnt) {
-  auto VectorReorganise = [](vec_t *vec, int l_idx, int m_idx) {
-    (*vec)[m_idx - 1] = std::accumulate(
-        vec->begin() + l_idx, vec->begin() + m_idx, 1, std::multiplies<T>());
-    vec->erase(vec->begin() + l_idx, vec->begin() + m_idx - 1);
-  };
-
-  if (cnt > 1) {
-#pragma unroll
-    for (int j = 0; j < N; ++j) {
-      VectorReorganise(&((*in_tenosr_data)[j]), low_idx, *max_idx);
-    }
-    VectorReorganise(out_tensor_data, low_idx, *max_idx);
-    (*out_size) -= --cnt;
-    (*max_idx) -= cnt;
-  } else if (cnt < 1) {
-    (*max_idx)++;
-  }
-}
-
-/*
-* 1. To compensate the lackage of input_tensors dimension;
-* 2. To Merge the dimensions of input_tensors while the consequtive
-* equal-dimensions appear;
-* 3. To Merge the dimension of input_tensors while the consequtive
-* 1-value-dimensions appear;
-* 4. To calculate the strides of each input_tensor.
-*/
-template <typename vec_t, typename T, int N>
-void MergeDimsTemp(std::vector<vec_t> *in_arr, vec_t *out) {
-  std::vector<vec_t> a = *in_arr;
-  int out_size = out->size();
-
-  for (int j = 0; j < N; ++j) {
-    std::reverse(a[j].begin(), a[j].end());
-    if (a[j].size() < out_size) {
-      vec_t vec_temp(out_size, 1);
-      int idx_in = 0, idx_out = 0;
-      a[j].resize(out_size, 1);
-
-      do {
-        if (a[j][idx_in] == (*out)[idx_out] || a[j][idx_in] == 1) {
-          vec_temp[idx_out++] = a[j][idx_in++];
-        } else {
-          idx_out++;
-        }
-      } while (idx_out < out_size);
-      std::copy(vec_temp.begin(), vec_temp.end(), a[j].begin());
-    }
-  }
-
-  int i = 0;
-  while (i < out_size) {
-    int cnt = 0;
-    int low_idx = i;
-    bool equal_flag = true;
-    do {
-#pragma unroll
-      for (int j = 1; j < N; j++) {
-        equal_flag &= a[0][i] == a[j][i];
-      }
-      if (equal_flag) {
-        i++;
-        cnt++;
-      } else {
-        break;
-      }
-    } while (i < out_size);
-    DimsReorganise<vec_t, T, N>(&a, out, &out_size, &i, low_idx, cnt);
-  }
-
-  int min_idx = 0;
-  T min_val =
-      std::accumulate(a[0].begin(), a[0].end(), 1, std::multiplies<T>());
-#pragma unroll
-  for (int j = 1; j < N; ++j) {
-    T temp = std::accumulate(a[j].begin(), a[j].end(), 1, std::multiplies<T>());
-    min_val = min_val > temp ? temp : min_val;
-    if (min_val == temp) {
-      min_idx = j;
-    }
-  }
-  std::swap(a[0], a[min_idx]);
-
-  i = 0;
-  while (i < out_size) {
-    int cnt = 0;
-    int low_idx = i;
-    bool equal_flag = true;
-    do {
-      equal_flag &= a[0][i] == 1;
-      if (equal_flag) {
-#pragma unroll
-        for (int j = 1; j < N; ++j) {
-          equal_flag &= a[j][i] == (*out)[i];
-        }
-      }
-      if (equal_flag) {
-        i++;
-        cnt++;
-      } else {
-        break;
-      }
-    } while (i < out_size);
-    DimsReorganise<vec_t, T, N>(&a, out, &out_size, &i, low_idx, cnt);
-  }
-}
-
-template <int N, typename vec_input_t>
-void StrideCalculate() {
-  auto out_size =
-      out_dims->size() std::vector<vec_t> in_stride(N, vec_t(out_size, 1));
-#pragma unroll
-  for (int j = 0; j < N; j++) {
-    for (int i = 0; i < out_size; ++i) {
-      if (a[j][i] == 1) {
-        in_stride[j][i] = 0;
-      } else if (i != 1) {
-        auto temp = std::accumulate(a[j].begin(), a[j].begin() + i, 1,
-                                    std::multiplies<T>());
-        in_stride[j][i] = temp;
-      }
-    }
-  }
-}
-
-template <int N, typename vec_input_t>  // N : input tensors.
-static OffsetCalculator<N, vec_input_t> input_offset_calculator(
-    vec_input_t *ins, framework::Tensor *out) {
-  constexpr int input_num = std::max<int>(N, 1);
-  std::array<int, input_num> shift_array;
-  std::array<int, input_num> mul_array;
-
-  std::reverse(out->dims().begin(), out->dims().end());
-  for (*in : ins) {
-    std::reverse(in->dims().begin(), in->dims().end());
-  }
-  MergeDims(ins, out->dims());
-  StrideCalculate(ins, out->dims());
-}
-
-template <typename T, typename Functor, typename inp_calc_t, int N, int nDims>
-__devide__ void CommonElementwiseKernel(vec_data *in_data_arr[], out_data) {
-  T *args[N];
-
-#pragma unroll
-  for (int i = 0; i < N; ++i) {
-    (loader[i])(in_data_arr[i], args[i]);
-  }
-  args[N - 1] = Functor(args);
-}
-
-template <typename T, typename Functor, typename inp_calc_t, int N>
-void CommonElementwiseCore(vec_input_t ins, framework::Tensor *out) {
-  constexpr int out_dim_size = out->dims().size();
-  using vec_data = std::vector<T *>;
-  vec_data in_data_arr;
-  in_data_arr.reserve(N);
-  for (auto *in_tensor : ins) {
-    in_data_arr.emplace_back(in_tensor->data());
-  }
-  T *out_data = out->data();
-
-  switch
-    out_dim_size {
-      case 2:
-        CommonElementwiseKernel<T, N, 2, vec_data>(in_data_arr, out_data);
-        break;
-      case 3:
-        CommonElementwiseKernel<T, N, 3, vec_data>(in_data_arr, out_data);
-        break;
-      case 4:
-        CommonElementwiseKernel<T, N, 4, vec_data>(in_data_arr, out_data);
-        break;
-      case 5:
-        CommonElementwiseKernel<T, N, 5, vec_data>(in_data_arr, out_data);
-        break;
-      default:
-        CommonElementwiseKernel<T, N, 5, vec_data>(in_data_arr, out_data);
-    }
-}  // 这里还是用元模板编程switch到 MAX_DIMS 吧
-
 template <typename T, typename Functor, typename vec_input_t>
 void BroadcastElementwise(const framework::ExecutionContext &ctx,
                           vec_input_t *ins, framework::Tensor *out) {
-  auto in_tensor_num = ins->size();
-  switch (in_tensor_num) {
+  auto input_num = ins->size();
+  auto merged_dims = MergeDims<T, vec_input_t>(ins, &(out->dims()), input_num);
+  auto offset_pre_cal =
+      OffsetPreCalculator<T, decltype(merged_dims)>(merged_dims);
+
+  switch (input_num) {
     case 2: {
-      auto input_calc = OffsetCalculator<vec_input_t, 2>(ins, out);
-      CommonElementwiseCore<T, Functor, input_calc, 2>(ins, out, in_tensor_num,
-                                                       dims);
+      CommonElementwiseCore<T, Functor, decltype(offset_pre_cal), 2>(
+          ins, out, offset_pre_cal);
       break;
     }
     case 3: {
-      auto input_calc = OffsetCalculator<vec_input_t, 3>(ins, out);
-      CommonElementwiseCore<T, Functor, input_calc, 3>(ins, out, in_tensor_num,
-                                                       dims);
+      CommonElementwiseCore<T, Functor, decltype(offset_pre_cal), 3>(
+          ins, out, offset_pre_cal);
       break;
     }
-    default: {
-      auto input_calc = OffsetCalculator<vec_input_t>(ins, out);
-      CommonElementwiseCore<T, Functor, input_calc>(ins, out, in_tensor_num,
-                                                    dims);
-    }
+    default: { ; }
   }
 }
 
-// template<typename func_t, typename array_t, typename inp_calc_t, typename
-// out_calc_t, typename loader_t, typename storer_t>
-// __global__ void BroadcastElementwiseKernel(int N, func_t f, array_t data,
-//                                             inp_calc_t ic, out_calc_t oc,
-//                                             loader_t l, storer_t s)
-// {
-//   int remaining = N - block_work_size * blockIdx.x;
-//   auto policy = memory::policies::unroll<array_t, inp_calc_t, out_calc_t,
-//   loader_t, storer_t>(data, remaining, ic, oc, l, s);
-//   ElementwiseCalculator(f, policy);
-// }
+template <typename T, typename func_t, typename offset_pre_t, int N>
+void CommonElementwiseCore(vec_input_t *ins, framework::Tensor *out,
+                           offset_pre_t offset_pre_cal) {
+  constexpr int dim_sizes = offset_pre_cal.strides.size();
+  auto in_loader = TensorLoader<vec_input_t, decltype(offset_pre_cal), N, 1>;
+  switch (dim_sizes) {
+    case 2:
+      in_loader = TensorLoader<vec_input_t, decltype(offset_pre_cal), N, 2>(
+          ins, offset_pre_cal);
+      CommonElementwiseKernel<T, decltype(in_loader), func_t, N, 2>(in_loader,
+                                                                    out);
+      break;
+    case 3:
+      in_loader = TensorLoader<vec_input_t, decltype(offset_pre_cal), N, 3>(
+          ins, offset_pre_cal);
+      CommonElementwiseKernel<T, decltype(in_loader), func_t, N, 3>(in_loader,
+                                                                    out);
+      break;
+    case 4:
+      in_loader = TensorLoader<vec_input_t, decltype(offset_pre_cal), N, 4>(
+          ins, offset_pre_cal);
+      CommonElementwiseKernel<T, decltype(in_loader), func_t, N, 4>(in_loader,
+                                                                    out);
+      break;
+    case 5:
+      in_loader = TensorLoader<vec_input_t, decltype(offset_pre_cal), N, 5>(
+          ins, offset_pre_cal);
+      CommonElementwiseKernel<T, decltype(in_loader), func_t, N, 4>(in_loader,
+                                                                    out);
+      break;
+    default:
+      CommonElementwiseKernel<T, decltype(in_loader), func_t, N, 1>(in_loader,
+                                                                    out);
+      break;
+  }
+}
 
-// template <template <int i> typename func, int end, int current = 0>
-// template <typename... Args>
-// static inline void with_args(Args &&... args) {
-//   func<current>::apply(std::forward<Args>(args)...);
-//   static_unroll<func, end, current + 1>::with_args(args...);
-// }
+template <typename T, typename loader_t, typename func_t, int N, int nDims>
+__devide__ void CommonElementwiseKernel(vec_data *in_data_arr[], out_data) {
+  // T *args[N];
 
-// template <typename args_t>
-// void load(args_t *args, int idx) {
-//   constexpr int arity = std::tuple_size<args_t>::value;
-//   detail::static_unroll<detail::vectorized_load_helper, MAX_DIMS>::with_args(
-//       *this, args, idx);
-// }
+  // #pragma unroll
+  // for (int i = 0; i < N; ++i) {
+  //   (loader[i])(in_data_arr[i], args[i]);
+  // }
+  // args[N - 1] = Functor(args);
+}
 
 }  // namespace operators
 }  // namespace paddle
--- paddle/fluid/operators/elementwise/elementwise_op_tmp.h
+++ paddle/fluid/operators/elementwise/elementwise_op_tmp.h
@@ -17,22 +17,15 @@
 #include <algorithm>
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
+#include "paddle/fluid/operators/elementwise/elementwise_op.h"
 
-template <typename INDEX_T>
-struct DivMod {
-  INDEX_T div, mod;
-
-  __forceinline__ __device__ DivMod(INDEX_T div, INDEX_T mod)
-      : div(div), mod(mod) {}
-};
-
-template <typename INDEX_T>
+template <typename index_t, typename N>
 struct FastDivMod {
   FastDivMod() {}
 
-  explicit FastDivMod(INDEX_T d) : divisor(d) {
-    // assert(divisor >= 1 && divisor <= std::numeric_limits<INDEX_T>::max);
+  explicit FastDivMod(index_t d) : divisor(d) {
     for (shift_val = 0; shift_val < INT_BITS; ++shift_val) {
       if ((1 << shift_val) >= divisor) {
         break;
@@ -46,17 +39,297 @@ struct FastDivMod {
     multiplier = temp_div;
   }
 
-  __forceinline__ __device__ INDEX_T div(INDEX_T n) const {
-    INDEX_T t = __umulhi(n, multiplier);
+  __forceinline__ __device__ index_t div(index_t n) const {
+    index_t t = __umulhi(n, multiplier);
     return (t + n) >> shift_val;
   }
 
-  __forceinline__ __device__ DivMod<INDEX_T> divmod(INDEX_T n) const {
-    INDEX_T q = div(n);
-    return DivMod<INDEX_T>(q, n - q * divisor);
+  __forceinline__ __device__ index_t *divmod(index_t n) const {
+    index_t q = div(n);
+    index_t arr[N] = {q, n - q * divisor};
+    // return DivMod<index_t>(q, n - q * divisor);
+    return arr;
   }
 
-  INDEX_T divisor;
-  INDEX_T multiplier;
-  INDEX_T shift_val;
+  index_t divisor;
+  index_t multiplier;
+  index_t shift_val;
 };
+
+/*
+* 1. To compensate the lackage of input_tensors dimension;
+* 2. To Merge the dimensions of input_tensors while the consequtive
+*    equal-dimensions appears;
+* 3. To Merge the dimension of input_tensors while the consequtive
+*    1-value-dimensions appears;
+* 4. To calculate the strides of each input_tensor.
+*/
+template <typename T, typename vec_input_t>
+struct MergeDims {
+  using vec_t = std::vector<int>;
+
+  __inline__ void DimsCompensate(int out_size) {
+    for (int j = 0; j < N; ++j) {
+      std::reverse(in_strides[j].begin(), in_strides[j].end());
+      if (in_strides[j].size() < out_size) {
+        vec_t tmp_dims(out_size, 1);
+        int idx_in = 0, idx_out = 0;
+        in_strides[j].resize(out_size, 1);
+        do {
+          if (in_strides[j][idx_in] == out_dims[idx_out] ||
+              in_strides[j][idx_in] == 1) {
+            tmp_dims[idx_out++] = in_strides[j][idx_in++];
+          } else {
+            idx_out++;
+          }
+        } while (idx_out < out_size);
+        std::copy(tmp_dims.begin(), tmp_dims.end(), in_strides[j].begin());
+      }
+    }
+  }
+
+  __inline__ void DimsReorganise(int *out_size, func_t merge_func) {
+    int i = 0;
+    auto VectorReorganise = [](vec_t *vec, int l_idx, int m_idx) {
+      (*vec)[m_idx - 1] = std::accumulate(
+          vec->begin() + l_idx, vec->begin() + m_idx, 1, std::multiplies<T>());
+      vec->erase(vec->begin() + l_idx, vec->begin() + m_idx - 1);
+    };
+    while (i < *out_size) {
+      int cnt = 0;
+      int low_idx = i;
+      bool equal_flag = true;
+      do {
+        merge_func(&equal_flag, &in_strides, &out_dims, i, N);
+        if (equal_flag) {
+          i++;
+          cnt++;
+        } else {
+          break;
+        }
+      } while (i < *out_size);
+      if (cnt > 1) {
+#pragma unroll
+        for (int j = 0; j < N; ++j) {
+          VectorReorganise(&(in_strides[j]), low_idx, i);
+        }
+        VectorReorganise(&out_dims, low_idx, i);
+        *out_size -= --cnt;
+        i -= cnt;
+      } else if (cnt < 1) {
+        i++;
+      }
+    }
+  }
+
+  template <typename T, typename vec_t>
+  __inline__ void StirdeCalculator(int out_size) {
+    std::vector<vec_t> tmp_stride(N, vec_t(out_size, 1));
+    for (int j = 0; j < N; ++j) {
+      for (int i = 0; i < out_size; ++i) {
+        if (in_strides[j][i] == 1) {
+          tmp_stride[j][i] = 0;
+        } else if (i != 1) {
+          tmp_stride[j][i] =
+              std::accumulate(in_strides[j].begin(), in_strides[j].begin() + i,
+                              1, std::multiplies<T>());
+        }
+      }
+      std::copy(tmp_stride[j].begin(), tmp_stride[j].end(),
+                in_strides[j].begin());
+    }
+  }
+
+  template <typename vec_input_t>
+  MergeDims(vec_input_t *ins, framework::DDim *dims, int N) {
+    auto out_size = *dims.size();
+    PADDLE_ENFORCE_GE(
+        out_size, MAX_DIMS,
+        platform::errors::InvalidArgument(
+            "Output tensor`s dim is %d, bigger than upper limitation %d\n",
+            out_size, MAX_DIMS));
+    for (auto *in_tensor : *ins) {
+      PADDLE_ENFORCE_GE(
+          in_tensor->dims().size(), MAX_DIMS,
+          platform::errors::InvalidArgument(
+              "Input tensor`s dim is %d, bigger than upper limitation %d\n",
+              in_tensor->dims().size(), MAX_DIMS));
+    }
+
+    in_strides.resize(N);
+    out_dims.resize(out_size);
+    for (auto &x : *out_dims) {
+      out_dims.emplace_back(x);
+    }
+    for (int j = 0; j < N; ++j) {
+      in_strides[j].resize(out_size);
+      for (auto &x : (*ins)[j]->dims()) {
+        in_strides[j].emplace_back(x);
+      }
+    }
+    DimsCompensate(out_size);
+
+    auto merge_same = [](bool *equal, std::vector<vec_t> *in_arr, vec_t out,
+                         int i, int num) -> void {
+      for (int j = 1; j < num; ++j) {
+        *equal &= (*in_arr)[0][i] == (*in_arr)[j][i];
+      }
+    };
+
+    auto merge_one = [](bool *equal, std::vector<vec_t> *in_arr, vec_t *out,
+                        int i, int num) -> void {
+      *equal &= (*in_arr)[0][i] == 1;
+      if (*equal) {
+        bool at_least = false;
+        for (int j = 1; j < num; ++j) {
+          if ((*in_arr)[j][i] == (*out)[i]) {
+            *equal &= true;
+            at_least = true;
+          } else if ((*in_arr)[j][i] == 1) {
+            continue;
+          } else {
+            PADDLE_ENFORCE_NE((*in_arr[j])[i], (*out)[i]),
+              platform::errors::InvalidArgument(
+                  "%d th input tensor %d th dimension(%d) is neither equal to"
+                  "the output tensor %d th dimension(%d), nor equal to 1.\n",
+                  j, i, (*in_arr)[j][i], (*out)[i]) );
+          }
+        }
+        PADDLE_ENFORCE_NE(
+            at_least, false,
+            platform::errors::InvalidArgument(
+                "All input tensors %d th dimension(%d) is equal to 1, none of "
+                "them equals to output tensor %d th dimension(%d).\n",
+                i, i, (*out)[i]));
+      }
+    };
+
+    typedef void (*func_t)(bool *, std::vector<vec_t> *, vec_t *, int, int);
+    func_t merge_ptr = merge_same;
+    DimsReorganise<T, vec_t, N, func_t>(&out_size, merge_ptr);
+
+    int min_idx = 0;
+    int min_val = std::accumulate(in_strides[0].begin(), in_strides[0].end(), 1,
+                                  std::multiplies<T>());
+    for (int j = 1; j < N; ++j) {
+      int temp = std::accumulate(in_strides[j].begin(), in_strides[j].end(), 1,
+                                 std::multiplies<T>());
+      min_val = min_val > temp ? temp : min_val;
+      if (min_val == temp) {
+        min_idx = j;
+      }
+    }
+    std::swap(in_strides[0], in_strides[min_idx]);
+
+    merge_ptr = merge_one;
+    DimsReorganise<T, vec_t, N, func_t>(out_size, merge_ptr);
+
+    StirdeCalculator<T, vec_t, N>(&vec_dims, out_size);
+    dims_size = out_size;
+  }
+
+  int dims_size;
+  vec_t out_dims;
+  std::vector<vec_t> in_strides;
+};
+
+template <typename merge_t, typename index_t = uint32_t>
+struct OffsetPreCalculator {
+  OffsetFastCal(merge_t merge_dims) {
+    auto N = merge_dims.in_strides.size();
+    auto dim_size = merge_dims.dims_size;
+    divmoder.resize(dim_size);
+    strides.resize(N, std::vector<index_t>(dim_size));
+    is_broadcast.resize(N, true);
+
+    for (int i = 0; i < dim_size; ++i) {
+      divmoder[i] = FastDivMod<index_t, N>(merge_dims.out_dims[i]);
+    }
+    for (int i = 0; i < N; ++i) {
+      std::copy(merge_dims.in_strides[i].begin(),
+                merge_dims.in_strides[i].end(), strides[i].begin());
+    }
+
+    std::vector<std::vector<index_t>> strides;
+    std::vector<FastDivMod<index_t>> divmoder;
+  }
+};
+
+template <typename offset_pre_t, int nDims, typename index_t = uint32_t>
+struct LoadScalar {
+  template <typename offset_pre_t, int nDims, typename index_t = uint32_t>
+  LoadScalar(offset_pre_t offset_pre_cal, int i) {
+    memcpy(static_cast<void *> strides, offset_pre_cal.strides[i].val(),
+           sizeof(index_t) * nDims);
+    memcpy(static_cast<void *> stridesdivmoder, offset_pre_cal.divmoder.val(),
+           sizeof(FastDivMod<index_t>) * nDims);
+  }
+
+  __forceinline__ __device__ index_t get_offset(int tid) {
+    index_t offset = 0;
+#pragma unroll
+    for (int i = 0; i < nDims; ++i) {
+      auto fast_divmoder = divmoder[i].divmod(tid);
+      tid = fast_divmoder[0];
+      offset += fast_divmoder[1] * strides[i];
+    }
+    return offset;
+  }
+
+  template <typename T>
+  __forceinline__ __device__ void load(T args, int idx);
+
+  index_t stride[MAX_DIM];
+  FastDivMod<index_t> divmoder[MAX_DIM];
+};
+
+template <typename T>
+struct LoadVector {
+  template <typename T, int vec_size>
+  __forceinline__ __device__ void load(T args, int idx);
+
+  template <typename T>
+  explicit LoadVector(int vec_size) {
+    switch (vec_size) {
+      case 2: {
+        return load<T, 2>();
+        break;
+      }
+      case 4: {
+        return load<T, 2>();
+        break;
+      }
+      case 8: {
+        return load<T, 2>();
+        break;
+      }
+      default:
+        return load<T, 1>();
+    }
+  }
+};
+
+template <typename T, typename vec_input_t, typename func_t, int N, int nDims,
+          typename offset_pre_t>
+struct TensorLoader {
+  template <typename T, int N, typename vec_input_t, typename offset_pre_t>
+  TensorLoader(vec_input_t *ins, framework::Tensor *out,
+               offset_pre_t *offset_pre = nullptr) {
+    for (int j = 0; j < N; ++j) {
+      in_data[j] = (*ins)[j].data();
+
+      if ((*ins)[j]->dims() == out->dims()) {
+        // constexpr vec_size = 8;
+        // auto load_vector = LoadVector<T>(vec_size);
+        // Funcp[j] = load_vector<T>.load();
+        // ;
+      } else {
+        auto load_scalar = LoadScalar<offset_pre_t, nDims>(*offset_pre, j);
+        Funcp[j] = (load_scalar<T>).load();
+      }
+    }
+  }
+
+  T *in_data[N];
+  std::function<void(vec_t)> Funcp[N];
+}
