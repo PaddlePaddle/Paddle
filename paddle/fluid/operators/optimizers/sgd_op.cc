5c136de41a25a73f4bb792a926b67a6e975a3f38
Adam Osewski <adam.osewski@intel.com>
2021-04-12 09:48:46 +0200

    Refactor SGD op kernels to leverage some common code.
    * Make easier to add new kerne invoke code.

--- paddle/fluid/operators/optimizers/sgd_op.cc
+++ paddle/fluid/operators/optimizers/sgd_op.cc
@@ -12,8 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/operators/optimizers/sgd_op.h"
 #include <string>
+
+#include "paddle/fluid/operators/optimizers/sgd_op.h"
 
 namespace paddle {
 namespace operators {
