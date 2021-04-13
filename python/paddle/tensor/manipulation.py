a5103fb32f307e66d241d6069d822272a79d2de2
HexToString <506181616@qq.com>
2021-04-13 15:50:27 +0800

    fix expand op check_dtype missing float16

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -1432,7 +1432,7 @@ def expand(x, shape, name=None):
                     'Elements in shape must be 1-D Tensors or integers.')
 
     check_variable_and_dtype(
-        x, 'x', ['bool', 'float32', 'float64', 'int32', 'int64'], 'expand')
+        x, 'x', ['bool', 'float16', 'float32', 'float64', 'int32', 'int64'], 'expand')
     check_type(shape, 'shape', (list, tuple, Variable), 'expand')
     if convert_dtype(x.dtype) == 'bool' and x.stop_gradient == False:
         raise ValueError("When the data type of input 'x' for expand is bool, "
