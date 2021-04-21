ed141696c7cbb70534dc6ea50d9111a401b93b28
xiemoyuan <71377852+xiemoyuan@users.noreply.github.com>
2021-04-21 08:40:01 +0800

    remove fluid for auto_checkpoint. (#32157)
    * remove fluid for auto_checkpoint.
    
    * fix bug.

--- python/paddle/incubate/checkpoint/__init__.py
+++ python/paddle/incubate/checkpoint/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,12 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import optimizer
-from . import checkpoint
-from ..fluid.contrib import reader
-from ..fluid.layer_helper import LayerHelper
+from ...fluid.incubate.checkpoint import auto_checkpoint
 
-__all__ = []
-__all__ += ["reader"]
-__all__ += optimizer.__all__
-__all__ += checkpoint.__all__
+__all__ = ["auto_checkpoint"]
