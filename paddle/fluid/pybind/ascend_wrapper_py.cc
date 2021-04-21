3bf60287b5bbd6c85195142ec66e2e9cb40e6c9b
zhang wenhui <frankwhzhang@126.com>
2021-04-21 20:52:50 +0800

    【NPU】Merge NPU ccl code (#32381)
    * add allreduce and broadcast without test (#31024)
    
    add allreduce and broadcast without test
    
    * Refactor HCCLCommContext to be compatible with Paddle (#31359)
    
    Refactor HCCLCommContext to be compatible with Paddle (#31359)
    
    * [NPU] add npu kernel for communication op (#31437)
    
    * add allreduce and broadcast without test
    
    * add c_broadcast_test case
    
    * build c_comm_init and c_create_group operators
    
    * make the whole thing compile
    
    * add broadcast and init op test case but run failed
    
    * make unit test compile
    
    * fix broadcast test bug and change into hcom for ccl
    
    * change c_comm_init and c_create_group ops accordingly
    
    * make tests compile
    
    * transfer code to 27
    
    * compiled successfully in 28, but run failed
    
    * test broadcast in 28, but failed
    
    * make hcom primitives work
    
    * change hccl data type for base.h
    
    * fix broadcast bug
    
    * make attributes work
    
    * fix group name bug
    
    * add allreduce but test failed
    
    * allreduce bug for qiuliang
    
    * allreduce finished
    
    * add allgather and reducescatter
    
    * merge all op code
    
    * add allgather test
    
    * finish run all ccl op test exclude send/recv
    
    * all all op and test exclude send/recv
    
    * send_v2_npu.cc recv_v2_npiu.cc compiled
    
    * fix ccl core dump bug and test allgather, reducescatter, broadcast op
    
    * fix allreduce bug just for test
    
    * hcom send&recv test pass, without hcom_destroy
    
    * for qiuliang test
    
    * Ascend Send&Recv Test Pass
    
    * all op (ex send/recv) ok
    
    * fix bug
    
    * merge all ccl op
    
    * style merge to PaddlePaddle
    
    * merge style
    
    * new merge style
    
    * merge style 2
    
    * insert an empty at the end
    
    * disable ctest for hcom to pass ci
    
    Co-authored-by: void-main <voidmain1313113@gmail.com>
    Co-authored-by: f2hkop <f2huestc@outlook.com>
    
    * Add auto-increasing tag id for Hcom OPs (#31702)
    
    * add c_reduce_sum op (#31793)
    
    add c_reduce_sum op
    
    * update Ascendrc hccl to 20.3 (#32126)
    
    update Ascendrc hccl to 20.3 (#32126)
    
    * fix merge code
    
    * change cmake.txt1
    
    * [NPU] Support npu kernel for c sync stream op (#31386)
    
    * sync stream npu op
    
    * add with_ascend_acl
    
    * update c++ unittest
    
    * compile all failed
    
    * try to pre commit
    
    * after pre commit
    
    * merge&compile&test hccl successfully!
    
    * fix code style
    
    * fix code style
    
    * fix bugs about hccl
    
    * fix some bugs
    
    * fix code style
    
    * fix style
    
    * fix style
    
    * fix
    
    * fixed
    
    * merge develop
    
    Co-authored-by: lw921014 <liuwei921014@yeah.net>
    Co-authored-by: Void Main <voidmain1313113@gmail.com>
    Co-authored-by: f2hkop <f2huestc@outlook.com>
    Co-authored-by: xiayanming <41795079@qq.com>

--- paddle/fluid/pybind/ascend_wrapper_py.cc
+++ paddle/fluid/pybind/ascend_wrapper_py.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#ifdef PADDLE_WITH_ASCEND
+#ifdef PADDLE_WITH_ASCEND_CL
 #include <fcntl.h>
 
 #ifdef _POSIX_C_SOURCE
