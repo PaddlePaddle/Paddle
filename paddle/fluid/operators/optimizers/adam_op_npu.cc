f2da540fdecb2a48d20f924d55a3c6ee681bd33a
pangyoki <pangyoki@126.com>
2021-04-21 04:23:11 +0000

    Revert "change FillNpuTensorWithConstant to TensorFromVector"
    This reverts commit 8577dfc62ae8f401b7b603f871a790bce9747414.

--- paddle/fluid/operators/optimizers/adam_op_npu.cc
+++ paddle/fluid/operators/optimizers/adam_op_npu.cc
@@ -64,19 +64,17 @@ class AdamNPUKernel : public framework::OpKernel<T> {
 
     // NOTE(zhiqiu): beta1_pow and beta2_pow may on CPU and not transform place.
     if (beta1_pow->place() == platform::CPUPlace()) {
-      float beta1 = *beta1_pow->data<float>();
+      T beta1 = *beta1_pow->data<T>();
       // `mutable_data` operation needs to be done after getting data
       beta1_pow_out->mutable_data<T>(ctx.GetPlace());
-      TensorFromVector(std::vector<float>{beta1}, ctx.device_context(),
-                       beta1_pow_out);
+      FillNpuTensorWithConstant<T>(beta1_pow_out, beta1);
     } else {
       beta1_pow_out->mutable_data<T>(ctx.GetPlace());
     }
     if (beta2_pow->place() == platform::CPUPlace()) {
-      float beta2 = *beta2_pow->data<float>();
+      T beta2 = *beta2_pow->data<T>();
       beta2_pow_out->mutable_data<T>(ctx.GetPlace());
-      TensorFromVector(std::vector<float>{beta2}, ctx.device_context(),
-                       beta2_pow_out);
+      FillNpuTensorWithConstant<T>(beta2_pow_out, beta2);
     } else {
       beta2_pow_out->mutable_data<T>(ctx.GetPlace());
     }
@@ -117,18 +115,15 @@ class AdamNPUKernel : public framework::OpKernel<T> {
 
     // reshape
     Tensor beta1_tensor(framework::proto::VarType::FP32);
-    beta1_tensor.mutable_data<float>({1}, ctx.GetPlace());
-    TensorFromVector(std::vector<T>{beta1}, ctx.device_context(),
-                     &beta1_tensor);
+    beta1_tensor.mutable_data<T>({1}, ctx.GetPlace());
+    FillNpuTensorWithConstant<T>(&beta1_tensor, beta1);
     Tensor beta2_tensor(framework::proto::VarType::FP32);
-    beta2_tensor.mutable_data<float>({1}, ctx.GetPlace());
-    TensorFromVector(std::vector<T>{beta2}, ctx.device_context(),
-                     &beta2_tensor);
+    beta2_tensor.mutable_data<T>({1}, ctx.GetPlace());
+    FillNpuTensorWithConstant<T>(&beta2_tensor, beta2);
 
     Tensor epsilon_tensor(framework::proto::VarType::FP32);
     epsilon_tensor.mutable_data<T>({1}, ctx.GetPlace());
-    TensorFromVector(std::vector<T>{epsilon}, ctx.device_context(),
-                     &epsilon_tensor);
+    FillNpuTensorWithConstant<T>(&epsilon_tensor, epsilon);
     auto stream =
         ctx.template device_context<paddle::platform::NPUDeviceContext>()
             .stream();
