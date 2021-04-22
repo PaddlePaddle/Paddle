82ab600e28c46b0e4bb40b8785616672624e39bc
ShenLiang <1422485404@qq.com>
2021-04-22 13:49:21 +0800

    [HybridParallel] Add ClipGradByGlobalNorm & check_finite_and_unscale in Dygraph (#32354)
    * add clip/check
    
    * add amp & clip grad in dygraph
    
    * add logging

--- python/paddle/distributed/fleet/meta_optimizers/__init__.py
+++ python/paddle/distributed/fleet/meta_optimizers/__init__.py
@@ -26,3 +26,4 @@ from .lamb_optimizer import LambOptimizer
 from .fp16_allreduce_optimizer import FP16AllReduceOptimizer
 from .sharding_optimizer import ShardingOptimizer
 from .dygraph_optimizer import HybridParallelOptimizer
+from .dygraph_optimizer import HybridParallelGradScaler
