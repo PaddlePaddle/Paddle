f1897955d2eee4de1bd09b998b3b40b90424ecb1
Jakub Piasecki <jakpia21@gmail.com>
2021-04-22 16:33:56 +0200

    base changes for fix

--- paddle/fluid/platform/mkldnn_reuse.h
+++ paddle/fluid/platform/mkldnn_reuse.h
@@ -639,7 +639,7 @@ class BroadcastDataMKLDNNHandler
                              const mkldnn::engine engine,
                              platform::Place cpu_place, const Tensor* x,
                              const Tensor* y, float scale_x, float scale_y,
-                             const std::string& uniq_name)
+                             const std::string& uniq_name, std::vector<int64_t>& input_dims)
       : platform::MKLDNNHandlerT<T, dnnl::binary>(
             dev_ctx, engine, cpu_place,
             platform::CreateKey(dev_ctx, framework::vectorize(x->dims()),
@@ -659,24 +659,25 @@ class BroadcastDataMKLDNNHandler
           y->format(), MKLDNNMemoryFormat::undef,
           platform::errors::InvalidArgument("Wrong format set for Y tensor."));
 
-      auto src1_tz = framework::vectorize(y->dims());
+      //auto src1_tz = std::vector<int64_t>(src0_tz);
       const auto src0_tz = framework::vectorize(x->dims());
+      
 
       // GetExpectedKernelType checks if smaller vector is a subvector with all
       // the dims in correct order on the rightmost part of the bigger vector,
       // i.e. a correct vector for broadcasting:
       //  x = 5, 7, 3, 2, 4, 8
       //  y = 4, 8
-      src1_tz.reserve(src0_tz.size());
-
-      for (size_t i = src1_tz.size(); i < src0_tz.size(); ++i) {
-        src1_tz.insert(src1_tz.begin(), 1L);
-      }
+      //src1_tz.reserve(src0_tz.size());
+//
+      //for (size_t i = src1_tz.size(); i < src0_tz.size(); ++i) {
+      //  src1_tz.insert(src1_tz.begin(), 1L);
+      //}
 
       const auto src0_md = dnnl::memory::desc(
           src0_tz, platform::MKLDNNGetDataType<T>(), x->format());
       const auto src1_md = dnnl::memory::desc(
-          src1_tz, platform::MKLDNNGetDataType<T>(), x->format());
+          input_dims, platform::MKLDNNGetDataType<T>(), x->format());
 
       dnnl::primitive_attr attributes;
       attributes.set_scales(DNNL_ARG_SRC_0, 0, {scale_x});
