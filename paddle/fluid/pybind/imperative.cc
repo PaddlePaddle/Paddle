7e2e308dd77bc1c2c64674f45eacd70ef770c0f2
zhiqiu <chenqiuliang@baidu.com>
2021-04-25 10:29:17 +0000

    skip op has no fp16 kernel

--- paddle/fluid/pybind/imperative.cc
+++ paddle/fluid/pybind/imperative.cc
@@ -1487,7 +1487,7 @@ void BindImperative(py::module *m_ptr) {
                  allow_ops);
              imperative::AmpOperators::Instance().GetMutableBlockOps()->swap(
                  block_ops);
-             VLOG(4) << "AMP operators changed, "
+             VLOG(5) << "AMP operators changed, "
                      << imperative::AmpOperators::Instance();
            })
       .def("_get_amp_op_list",
