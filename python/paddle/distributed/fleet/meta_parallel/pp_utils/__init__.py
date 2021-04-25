561dc719da979f25287cdf57f7d90572ad5eae81
lilong12 <lilong12@baidu.com>
2021-04-25 22:45:32 +0800

    add pipeline for dynamic graph (#32511)
    * add pp dygraph, test=develop

--- python/paddle/distributed/fleet/meta_parallel/pp_utils/__init__.py
+++ python/paddle/distributed/fleet/meta_parallel/pp_utils/__init__.py
@@ -12,6 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .parallel_layers import *
-from .model_parallel import ModelParallel
-from .pipeline_parallel import PipelineParallel
+from .utils import *
