4e460d7b6475d76dcf27581714bea1cf83571d26
Wenyu <wenyu.lyu@gmail.com>
2021-04-25 14:22:30 +0800

    Add hub Module for easy to use pre-trained models.  (#31873)
    * add Hub Module for easy to use pre-trained models.
    *   support list, load, help fucntions.
    *   support load models by github, gitee, local 
    
    Co-authored-by: LielinJiang <jianglielin@baidu.com>

--- python/paddle/tests/hubconf.py
+++ python/paddle/tests/hubconf.py
@@ -1,28 +1,24 @@
-# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-#
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import logger
-from . import callbacks
-from . import model_summary
-from . import hub
+dependencies = ['paddle']
 
-from . import model
-from .model import *
-from .model_summary import summary
-from .dynamic_flops import flops
+import paddle
+from test_hapi_hub_model import MM as _MM
 
-logger.setup_logger()
 
-__all__ = ['callbacks'] + model.__all__ + ['summary']
-__all__ = model.__all__ + ['flops']
+def MM(out_channels=8, pretrained=False):
+    '''This is a test demo for paddle hub
+    '''
+    return _MM(out_channels)
