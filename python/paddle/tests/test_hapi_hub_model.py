46aec95b5cee4150a74a34aedcb64659d275e782
Wenyu <wenyu.lyu@gmail.com>
2021-04-25 14:22:30 +0800

    Add hub Module for easy to use pre-trained models.  (#31873)
    * add Hub Module for easy to use pre-trained models.
    *   support list, load, help fucntions.
    *   support load models by github, gitee, local 
    
    Co-authored-by: LielinJiang <jianglielin@baidu.com>

--- python/paddle/tests/test_hapi_hub_model.py
+++ python/paddle/tests/test_hapi_hub_model.py
@@ -1,28 +1,29 @@
-# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-#
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import logger
-from . import callbacks
-from . import model_summary
-from . import hub
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
 
-from . import model
-from .model import *
-from .model_summary import summary
-from .dynamic_flops import flops
 
-logger.setup_logger()
+class MM(nn.Layer):
+    def __init__(self, out_channels):
+        super(MM, self).__init__()
+        self.conv = nn.Conv2D(3, out_channels, 3, 2, 1)
 
-__all__ = ['callbacks'] + model.__all__ + ['summary']
-__all__ = model.__all__ + ['flops']
+    def forward(self, x):
+        out = self.conv(x)
+        out = F.relu(out)
+
+        return out
