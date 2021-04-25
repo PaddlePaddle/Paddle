7b5f60d08a5d67505fb1dbb64584916326bfd928
xiemoyuan <xiemoyuan@baidu.com>
2021-04-25 11:21:23 +0000

    test=document_fix. Fixed examples.

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -429,6 +429,8 @@ def cuda_places(device_ids=None):
             import paddle
             import paddle.static as static
 
+            # required: gpu
+            
             paddle.enable_static()
 
             cuda_places = static.cuda_places()
