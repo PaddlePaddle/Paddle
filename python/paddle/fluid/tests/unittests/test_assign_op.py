9e65ab384fb75973c3e3cdf178ad329ee8878281
zhhsplendid <zhhsplendid@163.com>
2021-04-25 09:08:55 +0000

    Add test and fix xpu unittest

--- python/paddle/fluid/tests/unittests/test_assign_op.py
+++ python/paddle/fluid/tests/unittests/test_assign_op.py
@@ -94,8 +94,8 @@ class TestAssignOpError(unittest.TestCase):
             x3 = fluid.layers.data(name='x3', shape=[4], dtype="uint8")
             self.assertRaises(TypeError, fluid.layers.assign, x3)
             # When the type of input is numpy.ndarray, the dtype of input must be float32, int32.
-            x5 = np.array([[2.5, 2.5]], dtype='uint8')
-            self.assertRaises(TypeError, fluid.layers.assign, x5)
+            x4 = np.array([[2.5, 2.5]], dtype='uint8')
+            self.assertRaises(TypeError, fluid.layers.assign, x4)
 
 
 class TestAssignOApi(unittest.TestCase):
@@ -162,6 +162,16 @@ class TestAssignOApi(unittest.TestCase):
         self.assertTrue(np.allclose(result.numpy(), np.array(l)))
         paddle.enable_static()
 
+    def test_assign_BasicTypes(self):
+        paddle.disable_static()
+        result1 = paddle.assign(2)
+        result2 = paddle.assign(3.0)
+        result3 = paddle.assign(True)
+        self.assertTrue(np.allclose(result1.numpy(), np.array([2])))
+        self.assertTrue(np.allclose(result2.numpy(), np.array([3.0])))
+        self.assertTrue(np.allclose(result3.numpy(), np.array([1])))
+        paddle.enable_static()
+
 
 class TestAssignOpErrorApi(unittest.TestCase):
     def test_errors(self):
@@ -174,8 +184,8 @@ class TestAssignOpErrorApi(unittest.TestCase):
             x3 = fluid.layers.data(name='x3', shape=[4], dtype="uint8")
             self.assertRaises(TypeError, paddle.assign, x3)
             # When the type of input is numpy.ndarray, the dtype of input must be float32, int32.
-            x5 = np.array([[2.5, 2.5]], dtype='uint8')
-            self.assertRaises(TypeError, paddle.assign, x5)
+            x4 = np.array([[2.5, 2.5]], dtype='uint8')
+            self.assertRaises(TypeError, paddle.assign, x4)
 
 
 if __name__ == '__main__':
