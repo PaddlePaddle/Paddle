67b02e7da26ce43a6a845c0f895b0550fc09f8a4
Thunderbrook <a754913769@163.com>
2021-04-26 20:49:32 +0800

    solve brpc depend

--- paddle/fluid/framework/trainer.h
+++ paddle/fluid/framework/trainer.h
@@ -26,7 +26,6 @@ limitations under the License. */
 #include "paddle/fluid/framework/data_feed.h"
 #include "paddle/fluid/framework/data_set.h"
 #include "paddle/fluid/framework/device_worker.h"
-#include "paddle/fluid/framework/fleet/heter_context.h"
 #include "paddle/fluid/framework/fleet/heter_wrapper.h"
 #include "paddle/fluid/framework/heter_service.h"
 #include "paddle/fluid/framework/lod_tensor.h"
