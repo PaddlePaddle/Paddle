ae071ce0c784d9b4482abe886c10908350f5bf83
xiemoyuan <xiemoyuan@baidu.com>
2021-04-26 03:49:22 +0000

    add require gpu for example.

--- python/paddle/distributed/collective.py
+++ python/paddle/distributed/collective.py
@@ -871,6 +871,8 @@ def split(x,
             import paddle
             from paddle.distributed import init_parallel_env
 
+            # required: gpu
+
             paddle.set_device('gpu:%d'%paddle.distributed.ParallelEnv().dev_id)
             init_parallel_env()
             data = paddle.randint(0, 8, shape=[10,4])
