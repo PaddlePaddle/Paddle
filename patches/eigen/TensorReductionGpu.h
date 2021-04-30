90eb4509ce8ccf16be2ef567537c6f74028d7830
Ray2020BD <563755780@qq.com>
2021-04-30 08:04:27 +0000

    [Rocm] fix test_expand_as_op

--- patches/eigen/TensorReductionGpu.h
+++ patches/eigen/TensorReductionGpu.h
@@ -1,18 +1,3 @@
-// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// clang-format off
 // This file is part of Eigen, a lightweight C++ template library
 // for linear algebra.
 //
@@ -21,7 +6,7 @@
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+// clang-format off
 #ifndef EIGEN_CXX11_TENSOR_TENSOR_REDUCTION_GPU_H
 #define EIGEN_CXX11_TENSOR_TENSOR_REDUCTION_GPU_H
 
@@ -1010,3 +995,4 @@ struct OuterReducer<Self, Op, GpuDevice> {
 } // end namespace Eigen
 
 #endif // EIGEN_CXX11_TENSOR_TENSOR_REDUCTION_GPU_H
+// clang-format on
