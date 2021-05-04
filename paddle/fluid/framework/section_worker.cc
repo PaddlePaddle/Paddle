e5fcb199f04627903f381961a3eeeb3d2c00e439
gongweibao <weibao.gong@gmail.com>
2021-05-04 10:12:19 +0800

    fix piplineopt

--- paddle/fluid/framework/section_worker.cc
+++ paddle/fluid/framework/section_worker.cc
@@ -10,7 +10,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #if defined(PADDLE_WITH_NCCL) || defined(PADDLE_WITH_RCCL) || \
-    defined(WITH_ASCEND_CL)
+    defined(PADDLE_WITH_ASCEND_CL)
 #include <float.h>
 #include "paddle/fluid/framework/device_worker.h"
 #include "paddle/fluid/framework/executor_gc_helper.h"
