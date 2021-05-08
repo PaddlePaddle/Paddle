5b68b881fb924930ab39e462c6ac593992bc892d
XieYunshen <1084314248@qq.com>
2021-05-08 16:08:06 +0800

    fix error

--- paddle/scripts/conda_build.py
+++ paddle/scripts/conda_build.py
@@ -116,7 +116,7 @@ about:
 
         self.cuda_info = [(self.cuda101, "cuda10.1", ".post101"),
                           (self.cuda102, "cuda10.2", ""),
-                          (self.cuda110, "cuda11.2", ".post112")]
+                          (self.cuda112, "cuda11.2", ".post112")]
         self.py_str = ["py36", "py37", "py38","py39"]
         self.pip_end = ".whl --no-deps"
         self.pip_prefix_linux = "pip install /package/paddlepaddle"
