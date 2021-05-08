b0043d0d216ba483698cc0b8c3e53e0a556e7e99
XieYunshen <1084314248@qq.com>
2021-05-08 15:56:07 +0800

    update conda_build

--- paddle/scripts/conda_build.py
+++ paddle/scripts/conda_build.py
@@ -92,24 +92,16 @@ about:
         self.blt_const = r""" 
 """
 
-        self.python35 = r"    - python>=3.5, <3.6"
         self.python36 = r"    - python>=3.6, <3.7"
         self.python37 = r"    - python>=3.7, <3.8"
         self.python38 = r"    - python>=3.8, <3.9"
+        self.python39 = r"    - python>=3.9, <3.10"
 
         self.python_version = [
-            self.python35, self.python36, self.python37,
-            self.python38
+            self.python36, self.python37,
+            self.python38, self.python39
         ]
 
-        self.cuda90 = r"""
-    - cudatoolkit>=9.0, <9.1
-    - cudnn>=7.6, <7.7
-    """
-        self.cuda100 = r"""
-    - cudatoolkit>=10.0, <10.1
-    - cudnn>=7.6, <7.7
-    """
         self.cuda101 = r"""
     - cudatoolkit>=10.1, <10.2
     - cudnn>=7.6, <7.7
@@ -118,31 +110,30 @@ about:
     - cudatoolkit>=10.2, <10.3
     - cudnn>=7.6, <7.7
     """
-        self.cuda110 = r"""
-    - cudatoolkit>=11.0, <11.1
+        self.cuda112 = r"""
+    - cudatoolkit>=11.2, <11.3
     """
-        self.cuda_info = [(self.cuda90, "cuda9.0", ".post90"),
-                          (self.cuda100, "cuda10.0", ".post100"),
-                          (self.cuda101, "cuda10.1", ".post101"),
+
+        self.cuda_info = [(self.cuda101, "cuda10.1", ".post101"),
                           (self.cuda102, "cuda10.2", ""),
-                          (self.cuda110, "cuda11.0", ".post110")]
-        self.py_str = ["py35", "py36", "py37", "py38"]
+                          (self.cuda110, "cuda11.2", ".post112")]
+        self.py_str = ["py36", "py37", "py38","py39"]
         self.pip_end = ".whl --no-deps"
         self.pip_prefix_linux = "pip install /package/paddlepaddle"
         self.pip_prefix_windows = r"pip install C:\package\paddlepaddle"
         self.pip_gpu = "_gpu-"
         self.pip_cpu = "-"
         self.mac_pip = [
-            "-cp35-cp35m-macosx_10_6_intel", "-cp36-cp36m-macosx_10_6_intel",
-            "-cp37-cp37m-macosx_10_6_intel", "-cp38-cp38-macosx_10_14_x86_64"
+            "-cp36-cp36m-macosx_10_6_intel", "-cp37-cp37m-macosx_10_6_intel", 
+            "-cp38-cp38-macosx_10_14_x86_64", "-cp39-cp39-macosx_10_14_x86_64"
         ]
         self.linux_pip = [
-            "-cp35-cp35m-linux_x86_64", "-cp36-cp36m-linux_x86_64",
-            "-cp37-cp37m-linux_x86_64", "-cp38-cp38-linux_x86_64"
+            "-cp36-cp36m-linux_x86_64", "-cp37-cp37m-linux_x86_64", 
+            "-cp38-cp38-linux_x86_64", "-cp39-cp39-linux_x86_64"
         ]
         self.windows_pip = [
-            "-cp35-cp35m-win_amd64", "-cp36-cp36m-win_amd64", 
-            "-cp37-cp37m-win_amd64", "-cp38-cp38-win_amd64"
+            "-cp36-cp36m-win_amd64", "-cp37-cp37m-win_amd64", 
+            "-cp38-cp38-win_amd64", "-cp39-cp39-win_amd64"
         ]
 
 
