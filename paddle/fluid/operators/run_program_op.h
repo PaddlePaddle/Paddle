f6a257e9e0edacab6aa8095eccf5467ceaed2502
Aurelius84 <zhangliujie@baidu.com>
2021-05-10 09:26:26 +0000

    test for windows CI

--- paddle/fluid/operators/run_program_op.h
+++ paddle/fluid/operators/run_program_op.h
@@ -222,6 +222,7 @@ class RunProgramOpKernel : public framework::OpKernel<T> {
       auto skip_eager_vars = framework::details::ParseSafeEagerDeletionSkipVars(
           *program, end_op_index, output_var_names);
       parallel_executor->RunWithoutFetch(skip_eager_vars);
+      framework::ExecutorInfoCache::Instance().Finalize();
     }
     // Step 4. Get Output
     details::ShareVarsFromScope(output_vars, output_var_names, &scope);
@@ -314,6 +315,7 @@ class RunProgramGradOpKernel : public framework::OpKernel<T> {
 
       // Step 3. run ops
       parallel_executor->RunWithoutFetch(/*skip_eager_vars*/ skip_eager_vars);
+      framework::ExecutorInfoCache::Instance().Finalize();
     }
 
     // Step 4. get outputs
