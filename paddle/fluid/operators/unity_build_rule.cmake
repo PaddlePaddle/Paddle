a1f9503b2f47daea9697833b439b0004d74241ed
Jakub Piasecki <jakpia21@gmail.com>
2021-05-21 19:06:00 +0200

    CI fix

--- paddle/fluid/operators/unity_build_rule.cmake
+++ paddle/fluid/operators/unity_build_rule.cmake
@@ -233,6 +233,8 @@ register_unity_group(cc
     sampling_id_op.cc
     save_combine_op.cc
     save_op.cc
+    scale_op.cc
+    mkldnn/scale_op.cc
     scatter_nd_add_op.cc
     scatter_op.cc
     seed_op.cc
