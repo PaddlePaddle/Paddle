4015a0eb20aea7707eeba85c90d5ebfe0766bb1a
JamesLim-sy <santamonic@sina.com>
2021-05-21 17:33:20 +0000

    First_Commit.

--- paddle/fluid/operators/elementwise/elementwise_mod_op.cu
+++ paddle/fluid/operators/elementwise/elementwise_mod_op.cu
@@ -12,13 +12,43 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 #include "paddle/fluid/operators/elementwise/elementwise_mod_op.h"
+#include "paddle/fluid/operators/elementwise/elementwise_op_broadcast.cu.h"
+#include "paddle/fluid/operators/elementwise/elementwise_op_function.cu.h"
 #include "paddle/fluid/platform/float16.h"
 
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
 
+namespace paddle {
+namespace operators {
+
+template <typename T>
+class ElementwiseModKernel<platform::CUDADeviceContext, T>
+    : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
+    auto* x = ctx.Input<framework::LoDTensor>("X");
+    auto* y = ctx.Input<framework::LoDTensor>("Y");
+    auto* z = ctx.Output<framework::LoDTensor>("Out");
+    z->mutable_data<T>(ctx.GetPlace());
+    int axis = ctx.Attr<int>("axis");
+    axis = axis == -1 ? std::abs(x->dims().size() - y->dims().size()) : axis;
+
+    std::vector<const framework::Tensor*> ins = {x, y};
+    std::vector<framework::Tensor*> outs = {z};
+    const auto& cuda_ctx =
+        ctx.template device_context<platform::CUDADeviceContext>();
+
+    LaunchElementwiseCudaKernel<ElementwiseType::kBinary, T, T>(
+        cuda_ctx, ins, &outs, axis, CudaModFunctor<T>());
+  }
+};
+
+}  // namespace operators
+}  // namespace paddle
+
 REGISTER_OP_CUDA_KERNEL(
     elementwise_mod, ops::ElementwiseModKernel<plat::CUDADeviceContext, int>,
     ops::ElementwiseModKernel<plat::CUDADeviceContext, int64_t>,
-    ops::ElementwiseModFPKernel<plat::CUDADeviceContext, float>,
-    ops::ElementwiseModFPKernel<plat::CUDADeviceContext, double>);
+    ops::ElementwiseModKernel<plat::CUDADeviceContext, float>,
+    ops::ElementwiseModKernel<plat::CUDADeviceContext, double>);
