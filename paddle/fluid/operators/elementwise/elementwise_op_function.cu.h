62b221aeb6c838f74ec17f7ff60be142cd993620
JamesLim-sy <santamonic@sina.com>
2021-05-23 09:10:07 +0000

    Update the flood_div op

--- paddle/fluid/operators/elementwise/elementwise_op_function.cu.h
+++ paddle/fluid/operators/elementwise/elementwise_op_function.cu.h
@@ -229,7 +229,7 @@ DEFINE_SIMPLE_CUDA_BINARY_KERNEL(Div, /, half2_div)
 
 template <typename T, typename Enable = void>
 struct CudaDivFunctor {
-  inline HOSTDEVICE T operator()(const T* args) const {
+  inline __device__ T operator()(const T* args) const {
     return args[0] / args[1];
   }
 };
@@ -237,7 +237,7 @@ struct CudaDivFunctor {
 template <typename T>
 struct CudaDivFunctor<T,
                       typename std::enable_if_t<std::is_integral<T>::value>> {
-  inline HOSTDEVICE T operator()(const T* args) const {
+  inline __device__ T operator()(const T* args) const {
     PADDLE_ENFORCE(args[1] != 0, DIV_ERROR_INFO);
     return args[0] / args[1];
   }
@@ -245,7 +245,7 @@ struct CudaDivFunctor<T,
 
 template <typename T, typename Enable = void>
 struct CudaFloorDivFunctor {
-  inline HOSTDEVICE T operator()(const T* args) const {
+  inline __device__ T operator()(const T* args) const {
 #if defined(__HIPCC__) || defined(__CUDA_ARCH__)
     if (args[1] == 0) {
       printf(
