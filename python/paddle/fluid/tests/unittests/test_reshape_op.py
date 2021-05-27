be4ce281b1281b540f52f98cfce9abfd09b2cd33
yghstill <742925032@qq.com>
2021-05-27 07:35:48 +0000

    add reshape int shape unittests

--- python/paddle/fluid/tests/unittests/test_reshape_op.py
+++ python/paddle/fluid/tests/unittests/test_reshape_op.py
@@ -48,6 +48,30 @@ class TestReshapeOp(OpTest):
         self.check_grad(["X"], "Out")
 
 
+# situation 1: have shape(int, no tensor), no actual shape(Tensor)
+class TestReshapeOpIntShape(OpTest):
+    def setUp(self):
+        self.init_data()
+        self.op_type = "reshape2"
+        self.inputs = {"X": np.random.random(self.ori_shape).astype("float32")}
+        self.attrs = {"shape": self.new_shape}
+        self.outputs = {
+            "Out": self.inputs["X"].reshape(self.infered_shape),
+            'XShape': np.random.random(self.ori_shape).astype("float32")
+        }
+
+    def init_data(self):
+        self.ori_shape = (2, 60)
+        self.new_shape = -1
+        self.infered_shape = -1
+
+    def test_check_output(self):
+        self.check_output(no_check_set=['XShape'])
+
+    def test_check_grad(self):
+        self.check_grad(["X"], "Out")
+
+
 class TestReshapeOpDimInfer1(TestReshapeOp):
     def init_data(self):
         self.ori_shape = (5, 25)
