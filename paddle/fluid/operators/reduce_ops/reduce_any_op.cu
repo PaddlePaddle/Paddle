4bd964486133ff0c1d7f3567f1e56c322165e8a4
niuliling123 <root@yq01-qianmo-com-255-129-13.yq01.baidu.com>
2021-05-28 11:26:17 +0000

    reset reduce_any and reduce_all

--- paddle/fluid/operators/reduce_ops/reduce_any_op.cu
+++ paddle/fluid/operators/reduce_ops/reduce_any_op.cu
@@ -13,45 +13,7 @@
 // limitations under the License.
 
 #include "paddle/fluid/operators/reduce_ops/reduce_any_op.h"
-#include "paddle/fluid/operators/reduce_ops/reduce_functor_op.h"
-#include "paddle/fluid/operators/reduce_ops/reduce_op.cuh"
-#include "paddle/fluid/operators/reduce_ops/reduce_op.h"
 
-namespace paddle {
-namespace operators {
-
-template <typename T>
-class BoolReduceAnyKernel : public framework::OpKernel<T> {
- public:
-  void Compute(const framework::ExecutionContext& context) const override {
-    bool reduce_all = context.Attr<bool>("reduce_all");
-    auto* input = context.Input<Tensor>("X");
-    auto* output = context.Output<Tensor>("Out");
-
-    auto dims = context.Attr<std::vector<int>>("dim");
-    bool keep_dim = context.Attr<bool>("keep_dim");
-
-    std::vector<int> reduce_dims;
-    if (reduce_all) {
-      reduce_dims.resize(input->dims().size());
-      for (int i = 0; i < reduce_dims.size(); ++i) {
-        reduce_dims[i] = i;
-      }
-
-    } else {
-      for (auto e : dims) {
-        reduce_dims.push_back(e >= 0 ? e : e + input->dims().size());
-      }
-    }
-
-    auto stream = context.cuda_device_context().stream();
-    TensorReduceFunc<T, T, CustomLogicalOr<T>, detail::IdentityFunctor<T>>(
-        *input, output, reduce_dims, static_cast<T>(false),
-        CustomLogicalOr<T>(), detail::IdentityFunctor<T>(), stream);
-  }
-};
-
-}  // namespace operators
-}  // namespace paddle
-
-REGISTER_OP_CUDA_KERNEL(reduce_any, ops::BoolReduceAnyKernel<bool>);
+REGISTER_OP_CUDA_KERNEL(
+    reduce_any, ops::BoolReduceKernel<paddle::platform::CUDADeviceContext, bool,
+                                      ops::AnyFunctor>);
