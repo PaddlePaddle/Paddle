b2c29e290f39ba0a13a77b83e570dd82a0dcbf79
pangyoki <pangyoki@126.com>
2021-05-31 07:09:16 +0000

    add npu format support in tensor impl

--- paddle/fluid/framework/tensor.cc
+++ paddle/fluid/framework/tensor.cc
@@ -144,6 +144,15 @@ const DDim& Tensor::dims() const { return dims_; }
 
 int64_t Tensor::numel() const { return product(dims_); }
 
+Tensor& ResizeNPUDims(const DDim& npu_storage_dims) {
+  npu_storage_dims_ = npu_storage_dims;
+  return *this;
+}
+
+const DDim& npu_storage_dims() const { return npu_storage_dims_; }
+
+int64_t npu_storage_numel() { return product(npu_storage_dims_); }
+
 void Tensor::ResetHolder(std::shared_ptr<memory::Allocation> holder) {
   PADDLE_ENFORCE_EQ(
       offset_, 0,
