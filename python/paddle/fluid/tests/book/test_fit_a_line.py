29f252fc1c74b689bd3fbd9a12db08888f9f331c
wozna <joanna.wozna@intel.com>
2021-06-01 11:05:37 +0200

    Correct comment

--- python/paddle/fluid/tests/book/test_fit_a_line.py
+++ python/paddle/fluid/tests/book/test_fit_a_line.py
@@ -127,7 +127,7 @@ def infer(use_cuda, save_dirname=None, use_bf16=False):
 
     inference_scope = fluid.core.Scope()
     with fluid.scope_guard(inference_scope):
-        # Use fluid.io.load_inference_model to obtain the inference program desc,
+        # Use paddle.static.load_inference_model to obtain the inference program desc,
         # the feed_target_names (the names of variables that will be fed
         # data using feed operators), and the fetch_targets (variables that
         # we want to obtain data from using fetch operators).
