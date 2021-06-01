790173af23957990286edf1bc8d198684af3ced3
niuliling123 <51102941+niuliling123@users.noreply.github.com>
2021-06-01 20:55:18 +0800

    Update reduce_functor_op.h

--- paddle/fluid/operators/reduce_ops/reduce_functor_op.h
+++ paddle/fluid/operators/reduce_ops/reduce_functor_op.h
@@ -12,8 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#pragma once
-#include <cuda.h>
 #include <float.h>
 #include <limits.h>
 #include <math.h>
