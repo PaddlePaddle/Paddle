18c4413ff7dc1cdc17f685a002f5f9b0de31888d
zhangbo9674 <zhangbo54@baidu.com>
2021-06-02 06:27:24 +0000

    addequaldim for cuda, test=develop

--- paddle/fluid/operators/add_equal_dim_op.cu
+++ paddle/fluid/operators/add_equal_dim_op.cu
@@ -19,16 +19,13 @@ limitations under the License. */
 #include <hipcub/hipcub.hpp>
 namespace cub = hipcub;
 #endif
-#include <cuda_runtime.h>
 #include "paddle/fluid/operators/add_equal_dim_op.h"
-#include "paddle/fluid/platform/cuda_primitives.h"
-#include "paddle/fluid/platform/float16.h"
 
 namespace paddle {
 namespace operators {
 
 template <typename T>
-__global__ void add(T* x, T* y, T* z, int N) {
+__global__ void add(const T* x, const T* y, T* z, int N) {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   int stride = blockDim.x * gridDim.x;
   for (int i = index; i < N; i += stride) {
@@ -41,35 +38,20 @@ class AddEqualDimCUDAKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& context) const override {
     VLOG(3) << "AddEqualDim_CUDA_kernel_start\n";
-    // intputs / outputs
+
     auto* x = context.Input<Tensor>("X");
     auto* y = context.Input<Tensor>("Y");
     auto* out = context.Output<Tensor>("Out");
 
-    const T* h_x = x->data<T>();
-    const T* h_y = y->data<T>();
-    T* d_o = out->mutable_data<T>(context.GetPlace());
+    const T* x_ptr = x->data<T>();
+    const T* y_ptr = y->data<T>();
+    T* out_ptr = out->mutable_data<T>(context.GetPlace());
 
     int numel = x->numel();
-    int nBytes = numel * sizeof(T);
-
-    T* d_x;
-    T* d_y;
-    cudaMalloc(reinterpret_cast<void**>(&d_x), nBytes);
-    cudaMalloc(reinterpret_cast<void**>(&d_y), nBytes);
-
-    cudaMemcpy(reinterpret_cast<void*>(d_x), reinterpret_cast<void*>(h_x),
-               nBytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(reinterpret_cast<void*>(d_y), reinterpret_cast<void*>(h_y),
-               nBytes, cudaMemcpyHostToDevice);
 
     dim3 blockSize(256);
     dim3 gridSize((numel + blockSize.x - 1) / blockSize.x);
-
-    add<<<gridSize, blockSize>>>(d_x, d_y, d_o, numel);
-
-    cudaFree(d_x);
-    cudaFree(d_y);
+    add<<<gridSize, blockSize>>>(x_ptr, y_ptr, out_ptr, numel);
   }
 };
 
@@ -78,25 +60,30 @@ class AddEqualDimCUDAGradKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& context) const override {
     VLOG(3) << "AddEqualDimGrad_CUDA_kernel_start\n";
-    // 输入
+
     auto* dout = context.Input<Tensor>(framework::GradVarName("Out"));
-    // 输出
+
     auto* dx = context.Output<Tensor>(framework::GradVarName("X"));
     auto* dy = context.Output<Tensor>(framework::GradVarName("Y"));
 
-    const T* h_dout = dout->data<T>();
-    T* d_dx = dx->mutable_data<T>(context.GetPlace());
-    T* d_dy = dy->mutable_data<T>(context.GetPlace());
+    const T* dout_const_ptr = dout->data<T>();
+    T* dout_ptr = const_cast<T*>(dout_const_ptr);
+
+    T* dx_ptr = dx->mutable_data<T>(context.GetPlace());
+    T* dy_ptr = dy->mutable_data<T>(context.GetPlace());
 
     int numel = dout->numel();
     int nBytes = numel * sizeof(T);
 
-    cudaMemcpy(reinterpret_cast<void*>(d_dx), reinterpret_cast<void*>(h_dout),
-               nBytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(reinterpret_cast<void*>(d_dy), reinterpret_cast<void*>(h_dout),
-               nBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(reinterpret_cast<void*>(dx_ptr),
+               reinterpret_cast<void*>(dout_ptr), nBytes,
+               cudaMemcpyDeviceToDevice);
+    cudaMemcpy(reinterpret_cast<void*>(dy_ptr),
+               reinterpret_cast<void*>(dout_ptr), nBytes,
+               cudaMemcpyDeviceToDevice);
   }
 };
+
 }  // namespace operators
 }  // namespace paddle
 
