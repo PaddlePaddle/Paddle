cd9d555f5cf9b53a8bb66911aa608c6cda4dc6c7
wozna <joanna.wozna@intel.com>
2021-06-02 10:23:10 +0200

    Change op_type to op_version

--- python/paddle/fluid/tests/unittests/test_lookup_table_bf16_op.py
+++ python/paddle/fluid/tests/unittests/test_lookup_table_bf16_op.py
@@ -25,19 +25,19 @@ from paddle.fluid.op import Operator
 from paddle import enable_static
 
 
-def _lookup(weights, ids, flat_ids, op_type="lookup_table"):
+def _lookup(weights, ids, flat_ids, op_version="lookup_table"):
     w_shape = weights.shape
-    out_shape = list(ids.shape[:-1]) if op_type is "lookup_table" else list(
+    out_shape = list(ids.shape[:-1]) if op_version is "lookup_table" else list(
         ids.shape)
     out_shape.append(w_shape[-1])
     out = weights[flat_ids].reshape(out_shape)
     return out
 
 
-def _get_grad(weights, ids, flat_ids, op_type="lookup_table"):
+def _get_grad(weights, ids, flat_ids, op_version="lookup_table"):
     w_shape = weights.shape
     w_grad = np.zeros((w_shape), dtype=weights.dtype)
-    out_shape = list(ids.shape[:-1]) if op_type is "lookup_table" else list(
+    out_shape = list(ids.shape[:-1]) if op_version is "lookup_table" else list(
         ids.shape)
     out_grad_shape = (np.prod(out_shape), w_shape[-1])
     out_grad = weights[flat_ids].reshape(out_grad_shape)
