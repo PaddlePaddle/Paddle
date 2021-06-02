ce1e2caafd30039eefe1b7504ded04ff3ce534ab
zhangbo9674 <zhangbo54@baidu.com>
2021-06-02 06:45:42 +0000

    addequaldim for cuda, test=develop

--- paddle/fluid/operators/add_equal_dim_op.cu
+++ paddle/fluid/operators/add_equal_dim_op.cu
@@ -12,13 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#ifdef __NVCC__
-#include "cub/cub.cuh"
-#endif
-#ifdef __HIPCC__
-#include <hipcub/hipcub.hpp>
-namespace cub = hipcub;
-#endif
 #include "paddle/fluid/operators/add_equal_dim_op.h"
 
 namespace paddle {
