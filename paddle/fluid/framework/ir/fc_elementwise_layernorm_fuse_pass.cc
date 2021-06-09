788693cd9aee3556d2ed3c900ced33b7acacb6bf
fengshuai03 <fengshuai03@baidu.com>
2021-06-09 11:25:56 +0000

    fc_elementwise_layer_fuse_pass

--- paddle/fluid/framework/ir/fc_elementwise_layernorm_fuse_pass.cc
+++ paddle/fluid/framework/ir/fc_elementwise_layernorm_fuse_pass.cc
@@ -136,6 +136,108 @@ static bool IsEqual(const std::vector<T> &x, const std::vector<T> &y) {
   return true;
 }
 
+FCElementwiseLayerNormFusePass::FCElementwiseLayerNormFusePass() {
+  AddOpCompat(OpCompat("fc"))
+      .AddInput("Input")
+      .IsTensor()
+      .End()
+      .AddInput("W")
+      .IsTensor()
+      .End()
+      .AddInput("Bias")
+      .IsTensor()
+      .End()
+      .AddOutput("Out")
+      .IsTensor()
+      .End()
+      .AddAttr("in_num_col_dims")
+      .IsNumGE(1)
+      .End()
+      .AddAttr("activation_type")
+      .End();
+
+  AddOpCompat(OpCompat("layer_norm"))
+      .AddInput("X")
+      .IsTensor()
+      .End()
+      .AddInput("Scale")
+      .IsTensor()
+      .End()
+      .AddInput("Bias")
+      .IsTensor()
+      .End()
+      .AddOutput("Y")
+      .IsTensor()
+      .End()
+      .AddOutput("Mean")
+      .IsOptional()
+      .End()
+      .AddOutput("Variance")
+      .IsOptional()
+      .End()
+
+      .AddAttr("epsilon")
+      .IsNumGE(0.0f)
+      .IsNumLE(0.001f)
+      .End()
+      .AddAttr("begin_norm_axis")
+      .IsNumGT(0)
+      .End();
+
+  AddOpCompat(OpCompat("elementwise_add"))
+      .AddInput("X")
+      .IsTensor()
+      .End()
+      .AddInput("Y")
+      .IsTensor()
+      .End()
+      .AddInput("Out")
+      .IsTensor()
+      .End();
+
+  AddOpCompat(OpCompat("fc_elementwise_layernorm"))
+      .AddInput("X")
+      .IsTensor()
+      .End()
+      .AddInput("W")
+      .IsTensor()
+      .End()
+      .AddInput("Bias0")
+      .IsTensor()
+      .End()
+      .AddInput("Y")
+      .IsTensor()
+      .End()
+      .AddInput("Scale")
+      .IsTensor()
+      .End()
+      .AddInput("Bias1")
+      .IsTensor()
+      .End()
+
+      .AddOutput("Out")
+      .IsTensor()
+      .End()
+      .AddOutput("Mean")
+      .IsOptional()
+      .End()
+      .AddOutput("Variance")
+      .IsOptional()
+      .End()
+
+      .AddAttr("x_num_col_dims")
+      .IsNumGE(1)
+      .End()
+      .AddAttr("epsilon")
+      .IsNumGE(0.0f)
+      .IsNumLE(0.001f)
+      .End()
+      .AddAttr("begin_norm_axis")
+      .End()
+      .AddAttr("activation_type")
+      .End();
+}
+
 void FCElementwiseLayerNormFusePass::ApplyImpl(ir::Graph *graph) const {
   PADDLE_ENFORCE_NOT_NULL(graph,
                           platform::errors::InvalidArgument(
@@ -156,6 +258,11 @@ void FCElementwiseLayerNormFusePass::ApplyImpl(ir::Graph *graph) const {
                      Graph *graph) {
     if (subgraph.count(x) <= 0) {
       LOG(WARNING) << "The subgraph is empty.";
+      return;
+    }
+
+    if (!IsCompat(subgraph, graph)) {
+      LOG(WARNING) << "Pass in op compat failed.";
       return;
     }
 
@@ -232,6 +339,11 @@ void FCElementwiseLayerNormFusePass::ApplyImpl(ir::Graph *graph) const {
     new_desc.SetAttr("begin_norm_axis",
                      layer_norm->Op()->GetAttr("begin_norm_axis"));
     new_desc.SetAttr("activation_type", fc->Op()->GetAttr("activation_type"));
+
+    if (!IsCompat(new_desc)) {
+      LOG(WARNING) << "Fc fuse pass in out fc op compat failed.";
+      return;
+    }
 
     auto fused_node = graph->CreateOpNode(&new_desc);  // OpDesc will be copied.
 
