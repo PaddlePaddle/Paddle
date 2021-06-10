8158a58c1be27e00ca413c936aa8a5f3b2ef942a
zhangbo9674 <zhangbo54@baidu.com>
2021-06-10 01:33:45 +0000

    new api paddle.trunc, test=develop

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -871,17 +871,12 @@ def trunc(input, name=None):
 
             import paddle
 
-            input = paddle.rand([2,2],'float32')
-            print(input)
-            # Tensor(shape=[2, 2], dtype=float32, place=CUDAPlace(0), stop_gradient=True,
-            #         [[0.02331470, 0.42374918],
-            #         [0.79647720, 0.74970269]])
-            
+            input = paddle.to_tensor([[1.45, 3.54], [0.23, -4.21]], dtype='float32')
             output = paddle.trunc(input)
             print(output)
             # Tensor(shape=[2, 2], dtype=float32, place=CUDAPlace(0), stop_gradient=True,
-            #         [[0., 0.],
-            #         [0., 0.]])
+            #       [[1., 3.],
+            #        [0., -4.]])
     '''
     if in_dygraph_mode():
         return core.ops.trunc(input)
