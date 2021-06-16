dc6ec18476f5b6879db4caeeb42e93b6361d1feb
Wangzheee <634486483@qq.com>
2021-06-16 07:44:16 +0000

    pass_enhance

--- paddle/fluid/framework/ir/pass_tester_helper.h
+++ paddle/fluid/framework/ir/pass_tester_helper.h
@@ -67,6 +67,13 @@ struct Layers {
     op->SetInput("Filter", {filter->Name()});
     op->SetInput("Bias", {bias->Name()});
     op->SetOutput("Output", {out->Name()});
+    op->SetAttr("use_cudnn", use_cudnn);
+    op->SetAttr("groups", 1);
+    op->SetAttr("strides", std::vector<int>{1, 1});
+    op->SetAttr("paddings", std::vector<int>{0, 0});
+    op->SetAttr("padding_algorithm", "EXPLICIT");
+    op->SetAttr("dilations", std::vector<int>{1, 1});
+    op->SetAttr("data_format", "NCHW");
     op->SetAttr(OpProtoAndCheckerMaker::OpRoleAttrName(),
                 static_cast<int>(OpRole::kForward));
     return out;
