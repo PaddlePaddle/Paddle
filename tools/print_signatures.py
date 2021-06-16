0b4a7f1aa3ee54bc32c4bd82f4640f09dfa36b70
tianshuo78520a <707759223@qq.com>
2021-06-16 17:26:08 +0800

    del python2 code (#33556)

--- tools/print_signatures.py
+++ tools/print_signatures.py
@@ -50,10 +50,7 @@ console.setFormatter(
 def md5(doc):
     try:
         hashinst = hashlib.md5()
-        if platform.python_version()[0] == "2":
-            hashinst.update(str(doc))
-        else:
-            hashinst.update(str(doc).encode('utf-8'))
+        hashinst.update(str(doc).encode('utf-8'))
         md5sum = hashinst.hexdigest()
     except UnicodeDecodeError as e:
         md5sum = None
@@ -156,7 +153,7 @@ def visit_member(parent_name, member, member_name=None):
 
 
 def is_primitive(instance):
-    int_types = (int, long) if platform.python_version()[0] == "2" else (int, )
+    int_types = (int, )
     pritimitive_types = int_types + (float, str)
     if isinstance(instance, pritimitive_types):
         return True
