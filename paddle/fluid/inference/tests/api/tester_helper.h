67bec55cbd42df12ab4f531c4126db1ec81a07d6
Wilber <jiweibo@baidu.com>
2021-06-17 12:54:14 +0800

    [Inference Tensorrt] Add attr for trt engine and handle the input seq problem for ernie var len. (#33575)

--- paddle/fluid/inference/tests/api/tester_helper.h
+++ paddle/fluid/inference/tests/api/tester_helper.h
@@ -33,6 +33,7 @@
 #include "paddle/fluid/inference/analysis/ut_helper.h"
 #include "paddle/fluid/inference/api/analysis_predictor.h"
 #include "paddle/fluid/inference/api/helper.h"
+#include "paddle/fluid/inference/api/paddle_inference_api.h"
 #include "paddle/fluid/inference/api/paddle_inference_pass.h"
 #include "paddle/fluid/inference/tests/api/config_printer.h"
 #include "paddle/fluid/inference/tests/test_helper.h"
