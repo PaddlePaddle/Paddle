8d24aa8de275c113d673b3c808a00f62ec4013b3
Pei Yang <peiyang@baidu.com>
2021-06-21 20:21:13 +0800

    fix emb_eltwise_ln gpu_id bug (#33701)

--- paddle/fluid/inference/tensorrt/plugin/emb_eltwise_layernorm_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/emb_eltwise_layernorm_plugin.cu
@@ -134,7 +134,7 @@ int EmbEltwiseLayernormPluginDynamicImpl<T>::enqueue(
   int batch = id_dims.d[0];
   int seq_len = id_dims.d[1];
   int input_num = embs_.size();
-
+  cudaGetDevice(&device_id_);
   auto in_ptr_gpu_d =
       in_ptr_tensor_.mutable_data<int64_t>(platform::CUDAPlace(device_id_));
   auto emb_ptr_gpu_d =
