aad5fdd0a4de407ac48bff111d9daeb19af98627
Jiangxinz <jiangxinz@foxmail.com>
2021-06-21 15:20:27 +0800

    fix undef var

--- python/paddle/optimizer/momentum.py
+++ python/paddle/optimizer/momentum.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import warnings
+
 from .optimizer import Optimizer
 from ..fluid import core
 from ..fluid import framework
