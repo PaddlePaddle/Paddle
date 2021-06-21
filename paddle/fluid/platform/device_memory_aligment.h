a361ea56baa6ce496101a3aac09043ed796bad6b
Leo Chen <chenqiuliang@baidu.com>
2021-06-21 11:56:18 +0800

    [NPU] flatten params and grads, fuse grad_clip and optimizer op (#33461)
    * enable npu alignment
    
    * support flatten_params/grads
    
    * support clip by global norm
    
    * remove memset in coalesce_tensor_op
    
    * fix npu kernel of sum op when input is one tensor
    
    * add ut for flatten_param_grads+regularizer
    
    * fix ut
    
    * fix typo

--- paddle/fluid/platform/device_memory_aligment.h
+++ paddle/fluid/platform/device_memory_aligment.h
@@ -22,9 +22,13 @@ limitations under the License. */
 #elif defined(PADDLE_WITH_ASCEND_CL)
 #include "paddle/fluid/platform/npu_info.h"
 #endif
+#if defined(PADDLE_WITH_ASCEND_CL)
+#include "paddle/fluid/platform/npu_info.h"
+#endif
 
 namespace paddle {
 namespace platform {
-size_t Alignment(size_t size, const platform::Place &place);
+size_t Alignment(size_t size, const platform::Place &place,
+                 int align_size = -1);
 }  // namespace platform
 }  // namespace paddle
