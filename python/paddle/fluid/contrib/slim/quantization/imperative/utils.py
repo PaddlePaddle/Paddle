76c88e7fc0fc7bd87e2bff3929f39e834aaf9c3c
Jiangxinz <jiangxinz@foxmail.com>
2021-06-22 15:00:39 +0800

    fix undef var

--- python/paddle/fluid/contrib/slim/quantization/imperative/utils.py
+++ python/paddle/fluid/contrib/slim/quantization/imperative/utils.py
@@ -16,6 +16,10 @@ import math
 import numpy as np
 
 import paddle
+from paddle.fluid.log_helper import get_logger
+
+_logger = get_logger(
+    __name__, logging.INFO, fmt='%(asctime)s-%(levelname)s: %(message)s')
 
 from . import quant_nn
 
