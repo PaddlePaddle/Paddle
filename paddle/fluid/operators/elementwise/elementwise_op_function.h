ecc86748cefe28e61680593b97a24871977f5f8a
zhiqiu <chenqiuliang@baidu.com>
2021-07-02 08:21:56 +0000

    format code

--- paddle/fluid/operators/elementwise/elementwise_op_function.h
+++ paddle/fluid/operators/elementwise/elementwise_op_function.h
@@ -255,7 +255,7 @@ void CommonForwardBroadcastCPU(const framework::Tensor *x,
   PADDLE_ENFORCE_NOT_NULL(x_data, platform::errors::InvalidArgument(
                      "The input X should not be empty.")); 
   PADDLE_ENFORCE_NOT_NULL(y_data, platform::errors::InvalidArgument(
-                       "The input Y should not be empty."));
+                         "The input Y should not be empty."));
   OutType *out_data = z->mutable_data<OutType>(ctx.GetPlace());
 
   const int out_size = std::accumulate(out_dims_array, out_dims_array + max_dim,
