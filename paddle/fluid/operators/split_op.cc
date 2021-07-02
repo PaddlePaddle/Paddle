96dd03700c44c3b81eed52f696ec7a0b364c1b12
Jakub Piasecki <jakpia21@gmail.com>
2021-07-02 20:57:45 +0200

    minor change

--- paddle/fluid/operators/split_op.cc
+++ paddle/fluid/operators/split_op.cc
@@ -85,9 +85,9 @@ class SplitOp : public framework::OperatorWithKernel {
       // 8 or 16(depending on which blocking format is used) submemory cannot be
       // created, so in that scenario a fallback is needed
       auto tmp_md = dnnl::memory::desc(
-          framework::vectorize(ctx.Input<Tensor>("X")->dims()), dnnl::data_type
-          : f32, ctx.Input<Tensor>("X")->format());
-      if (tmp_md.inner_blks != 0)
+          framework::vectorize(ctx.Input<Tensor>("X")->dims()),
+          dnnl::memory::data_type::f32, ctx.Input<Tensor>("X")->format());
+      if (tmp_md.data.format_desc.blocking.inner_nblks == 0)
         return framework::OpKernelType(input_data_type, ctx.GetPlace(),
                                        framework::DataLayout::kMKLDNN,
                                        framework::LibraryType::kMKLDNN);
