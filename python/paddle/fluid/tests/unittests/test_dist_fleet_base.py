f696df40a0b4a15a516af3151e2cabc6112a2270
seiriosPlus <tangwei12@baidu.com>
2021-07-05 16:47:44 +0800

    fix ut error
    Change-Id: Ib84c09f3e3d93f32ffd04c7412b10eeb07eeb309

--- python/paddle/fluid/tests/unittests/test_dist_fleet_base.py
+++ python/paddle/fluid/tests/unittests/test_dist_fleet_base.py
@@ -392,7 +392,7 @@ class TestFleetBase(unittest.TestCase):
                   format(basename))
 
         if tr0_ret != 0 or tr1_ret != 0:
-            if is_listen_failed(ps0_err) or is_listen_failed(ps1_err):
+            if is_listen_failed(ps0_err_log) or is_listen_failed(ps1_err_log):
                 print("find parameter server port bind failed, skip the error")
                 tr0_ret, tr1_ret = 0, 0
             else:
