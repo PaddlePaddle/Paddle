16ecf244bcbecbe6ef4a984140e9e8ce9aa0781b
Artur Lesniak <artur.lesniak@intel.com>
2021-07-07 19:08:06 +0200

    reverted template compilation fix

--- paddle/fluid/operators/math/selected_rows_functor.cc
+++ paddle/fluid/operators/math/selected_rows_functor.cc
@@ -307,7 +307,6 @@ elementwise_add_to(BlasT<platform::CPUDeviceContext, T>* blas, size_t data_len,
                    const T* in, T* out) {
   onednn_handler_axpy(data_len, T(1.f), in, out);
 }
-#endif
 
 template <typename T>
 typename std::enable_if<std::is_same<T, float>::value ||
@@ -318,6 +317,16 @@ elementwise_add_to(BlasT<platform::CPUDeviceContext, T>* blas, size_t data_len,
                    const T* in, T* out) {
   blas->AXPY(data_len, T(1.f), in, out);
 }
+#else
+template <typename T>
+typename std::enable_if<std::is_floating_point<T>::value ||
+                        std::is_same<T, platform::complex<float>>::value ||
+                        std::is_same<T, platform::complex<double>>::value>::type
+elementwise_add_to(BlasT<platform::CPUDeviceContext, T>* blas, size_t data_len,
+                   const T* in, T* out) {
+  blas->AXPY(data_len, T(1.f), in, out);
+}
+#endif
 
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value>::type elementwise_add_to(
@@ -561,10 +570,8 @@ template struct MergeAdd<platform::CPUDeviceContext,
                          paddle::platform::complex<float>>;
 template struct MergeAdd<platform::CPUDeviceContext,
                          paddle::platform::complex<double>>;
-#ifdef PADDLE_WITH_MKLDNN
 template struct MergeAdd<platform::CPUDeviceContext,
                          paddle::platform::bfloat16>;
-#endif
 
 template struct MergeAverage<platform::CPUDeviceContext, int>;
 template struct MergeAverage<platform::CPUDeviceContext, int64_t>;
