9cda0596e69c2038171ea2d13bf5e818ba1f2fa9
houj04 <35131887+houj04@users.noreply.github.com>
2021-07-12 19:46:46 +0800

    [NPU ]add npu kernel for gaussian random (#33983)
    * add npu operator for gaussian random.
    
    * bugfix: add wait after memory copy.
    
    * update gaussian random op: use TensorCopy.

--- paddle/fluid/operators/uniform_random_op_npu.cc
+++ paddle/fluid/operators/uniform_random_op_npu.cc
@@ -12,11 +12,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/operators/uniform_random_op.h"
 #include <string>
+
 #include "paddle/fluid/framework/generator.h"
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/framework/operator.h"
+#include "paddle/fluid/operators/uniform_random_op.h"
 
 namespace paddle {
 namespace operators {
