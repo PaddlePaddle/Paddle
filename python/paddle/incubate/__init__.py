44bdbe932d161dc65d49efac683986ce191c80a6
Yuang Liu <liuyuang@baidu.com>
2021-07-16 10:57:35 +0800

    softmax mask fuse op, test=develop (#33841)

--- python/paddle/incubate/__init__.py
+++ python/paddle/incubate/__init__.py
@@ -17,7 +17,8 @@ from .optimizer import ModelAverage  # noqa: F401
 from .checkpoint import auto_checkpoint  # noqa: F401
 from ..fluid.layer_helper import LayerHelper  # noqa: F401
 from .operators import softmax_mask_fuse_upper_triangle  # noqa: F401
+from .operators import softmax_mask_fuse  # noqa: F401
 
 __all__ = [  # noqa
-    'LookAhead', 'ModelAverage', 'softmax_mask_fuse_upper_triangle'
+    'LookAhead', 'ModelAverage', 'softmax_mask_fuse_upper_triangle', 'softmax_mask_fuse'
 ]
