9dc6325aa06fd8517fe9ffdaee2473f311846225
wenbin <wang3323032@qq.com>
2021-07-16 11:11:37 +0000

    correct UT.test=develop

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_reduce_mean_op.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_reduce_mean_op.py
@@ -27,21 +27,21 @@ class TRTReduceMeanTest(InferencePassTest):
     def setUp(self):
         with fluid.program_guard(self.main_program, self.startup_program):
             data = fluid.data(
-                name="data", shape=[-1, 3, 10, 768], dtype="float32")
+                name="data", shape=[-1, 3, 224, 224], dtype="float32")
             reduce_mean = fluid.layers.reduce_mean(
                 data, dim=[2, -1], keep_dim=True)
             out = fluid.layers.batch_norm(reduce_mean, is_test=True)
 
         self.feeds = {
-            "data": np.random.random([3, 3, 10, 768]).astype("float32"),
+            "data": np.random.random([3, 3, 224, 224]).astype("float32"),
         }
         self.enable_trt = True
         self.trt_parameters = TRTReduceMeanTest.TensorRTParam(
             1 << 30, 32, 1, AnalysisConfig.Precision.Float32, False, False)
         self.fetch_list = [out]
         self.dynamic_shape_params = TRTReduceMeanTest.DynamicShapeParam({
-            'data': [1, 3, 8, 8]
-        }, {'data': [3, 3, 10, 768]}, {'data': [3, 3, 10, 768]}, False)
+            'data': [1, 3, 224, 224]
+        }, {'data': [3, 3, 224, 224]}, {'data': [3, 3, 224, 224]}, False)
 
     def test_check_output(self):
         if core.is_compiled_with_cuda():
@@ -55,20 +55,20 @@ class TRTReduceMeanAllTest(InferencePassTest):
     def setUp(self):
         with fluid.program_guard(self.main_program, self.startup_program):
             data = fluid.data(
-                name="data", shape=[-1, 3, 10, 768], dtype="float32")
+                name="data", shape=[-1, 3, 224, 224], dtype="float32")
             reduce_mean = fluid.layers.reduce_mean(data, keep_dim=True)
             out = fluid.layers.batch_norm(reduce_mean, is_test=True)
 
         self.feeds = {
-            "data": np.random.random([3, 3, 10, 768]).astype("float32"),
+            "data": np.random.random([3, 3, 224, 224]).astype("float32"),
         }
         self.enable_trt = True
         self.trt_parameters = TRTReduceMeanAllTest.TensorRTParam(
             1 << 30, 32, 1, AnalysisConfig.Precision.Float32, False, False)
         self.fetch_list = [out]
         self.dynamic_shape_params = TRTReduceMeanAllTest.DynamicShapeParam({
-            'data': [1, 3, 8, 8]
-        }, {'data': [3, 3, 10, 768]}, {'data': [3, 3, 10, 768]}, False)
+            'data': [1, 3, 224, 224]
+        }, {'data': [3, 3, 224, 224]}, {'data': [3, 3, 224, 224]}, False)
 
     def test_check_output(self):
         if core.is_compiled_with_cuda():
