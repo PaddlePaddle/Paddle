d909897c302f75ba70011265f30711c82271c3d6
Zjq9409 <15205085056@163.com>
2021-07-18 07:46:59 +0000

    dropout optimization forward by mkl

--- paddle/fluid/operators/dropout_op.h
+++ paddle/fluid/operators/dropout_op.h
@@ -153,7 +153,7 @@ class CPUDropoutKernel : public framework::OpKernel<T> {
       }
       auto engine = framework::GetCPURandomEngine(seed_data);
       std::uniform_real_distribution<float> dist(0, 1);
-      for (size_t i = 0; i < size; ++i) {
+      for (int i = 0; i < size; ++i) {
         if (dist(*engine) < dropout_prob) {
           mask_data[i] = 0;
           y_data[i] = 0;
