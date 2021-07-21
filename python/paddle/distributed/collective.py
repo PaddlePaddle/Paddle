7aba1c363180f911d2c331f31f581f78ac96a506
youth123 <2042519524@qq.com>
2021-07-21 16:44:50 +0800

    fix the bug in paddle.distributed.split demo

--- python/paddle/distributed/collective.py
+++ python/paddle/distributed/collective.py
@@ -1343,18 +1343,18 @@ def split(x,
         .. code-block:: python
 
             import paddle
-            from paddle.distributed import init_parallel_env
+            import paddle.distributed.fleet as fleet
 
-            # required: gpu
-
+            paddle.enable_static()
             paddle.set_device('gpu:%d'%paddle.distributed.ParallelEnv().dev_id)
-            init_parallel_env()
+            fleet.init(is_collective=True)
             data = paddle.randint(0, 8, shape=[10,4])
             emb_out = paddle.distributed.split(
                 data,
                 (8, 8),
                 operation="embedding",
                 num_partitions=2)
+
     """
     assert isinstance(size, (list, tuple)), (
         "The type of size for "
