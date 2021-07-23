577fdde579bc9caf06cafe2318b4e673e900ba98
Aurelius84 <liujiezhangbupt@gmail.com>
2021-07-23 22:12:55 +0800

    Revert "[Dy2Stat] Refactor ExecutorCache logic and pre-support BuildStrategy for pass (#34181)" (#34348)
    This reverts commit 609f8225281e1c200516df3dd4fc26842eff3590.

--- paddle/fluid/operators/run_program_op.cc
+++ paddle/fluid/operators/run_program_op.cc
@@ -103,10 +103,6 @@ class RunProgramOpMaker : public framework::OpProtoAndCheckerMaker {
                   "(bool, default false) Set to true for inference only, false "
                   "for training.")
         .SetDefault(false);
-    AddAttr<int64_t>(
-        "program_id",
-        "(int64_t)"
-        "The unique hash id used as cache key for ExecutorInfoCache.");
     AddComment(R"DOC(
 RunProgram operator.
 
