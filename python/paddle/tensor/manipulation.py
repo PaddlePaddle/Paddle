b8271994be40513a7f2c2bf39d067c83b3d880f7
Roc <30228238+sljlp@users.noreply.github.com>
2021-07-26 15:42:23 +0800

    Update manipulation.py
    fix doc

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -241,10 +241,10 @@ def flip(x, axis, name=None):
           x = x.astype('float32')
           img = paddle.to_tensor(x)
           tmp = paddle.flip(img, [0,1])
-          print(tmp) # [[[10,11][8, 9]],[[6, 7],[4, 5]] [[2, 3],[0, 1]]]
+          print(tmp) # [[[10,11],[8, 9]], [[6, 7],[4, 5]], [[2, 3],[0, 1]]]
 
           out = paddle.flip(tmp,-1)
-          print(out) # [[[11,10][9, 8]],[[7, 6],[5, 4]] [[3, 2],[1, 0]]]
+          print(out) # [[[11,10],[9, 8]], [[7, 6],[5, 4]], [[3, 2],[1, 0]]]
     """
     if isinstance(axis, int):
         axis = [axis]
