ccae6ae0b50c5a7ab98cb93a9fb47344105da1d3
sandyhouse <lilong12@baidu.com>
2021-07-26 21:49:39 +0800

    update

--- python/paddle/distributed/auto_parallel/interface.py
+++ python/paddle/distributed/auto_parallel/interface.py
@@ -12,6 +12,80 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numpy as np
+
+__all__ = []
+
+g_process_mesh_map = dict()
+
+
+class ProcessMesh(object):
+    """
+    A class to describe the logical topology of processes.
+
+    Args:
+        topology (list): a list to describe the process topology
+        process_group (list): a list of processes belonging to this group
+        parent_index (int): the index of the parent ProcessMesh. None means
+            that it has no parent ProcessMesh.
+
+    Examples:
+        .. code-block:: python
+
+            import paddle
+            import paddle.distributed as dist
+            
+            dp_degree = 2
+            pp_degree = 2
+            mp_degree = 2
+            mesh = ProcessMesh([dp_degree, pp_degree, mp_degree])
+    """
+
+    def __init__(self, topology, process_group=None, parent_index=None):
+        assert topology, "You must specify the topology for ProcessMesh."
+        process_num = np.prod(mesh)
+        if process_group is None:
+            process_group = list(range(process_num))
+        assert len(process_group) == process_num
+
+        if parent_index is None: parent_index = -1
+
+        self.desc = core.ProcessMesh(topology, process_group, parent_index)
+        cur_idx = self.desc.id
+        assert cur_idx not in g_process_mesh_map, "%d already exists." % cur_idx
+        g_process_mesh_map[cur_idx] = self
+
+    @property
+    def topology(self):
+        return self.desc.topology
+
+    @property
+    def process_group(self):
+        return self.desc.process_group
+
+    @property
+    def rank(self):
+        return len(self.desc.topology)
+
+    @property
+    def id(self):
+        return self.desc.id
+
+    @property
+    def parent(self):
+        parent_idx = self.desc.parent_idx
+        if parent_idx == -1: return None
+        assert parent_idx in g_process_mesh_map, \
+            "parent (%d) does not exist."%parent_idx
+        return g_process_mesh_map[parent_idx]
+
+    def __eq__(self, other):
+        assert other
+        return self.desc.equal(other.desc)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
 
 def validate_check():
     pass
@@ -28,8 +102,7 @@ def shard_tensor(tensor, mesh, dims_mapping):
         The tensor itself.
     """
     validate_check()
-    tensor.desc._set_distributed_attr('mesh_topology', mesh.get_mesh())
-    tensor.desc._set_distributed_attr('mesh_group', mesh.get_process_group())
+    tensor.desc._set_distributed_attr('mesh_id', mesh.id)
     tensor.desc._set_distributed_attr('dims_mapping', dims_mapping)
     return tensor
 
@@ -44,8 +117,7 @@ def set_shard_mask(tensor, mask):
         The tensor itself.
     """
     validate_check()
-    tensor.desc._set_distributed_attr('mask_shape', mask.shape)
-    tensor.desc._set_distributed_attr('mask_value', mask.tolist())
+    tensor.desc._set_distributed_attr('mask', mask)
     return tensor
 
 
@@ -62,8 +134,7 @@ def shard_op(op_name, mesh, input_dims_mapping, output_dims_mapping):
     """
     validate_check()
     # op_mapping[op_name](parameter list from input_dims_mapping)
-    op.desc._set_distributed_attr('mesh_topology', mesh.get_mesh())
-    op.desc._set_distributed_attr('mesh_group', mesh.get_process_group())
+    op.desc._set_distributed_attr('mesh_id', mesh.id)
     op.desc._set_distributed_attr('input_dims_mapping', input_dims_mapping)
     op.desc._set_distributed_attr('output_dims_mapping', output_dims_mapping)
     # input_dims_mapping = {index: {'name': in_name, 'dims_mapping': dims_mapping}}
@@ -89,5 +160,4 @@ def set_pipeline_stage(stage):
     Returns:
         None.
     """
-    global _CURRENT_PIPELINE_STAGE_
-    _CURRENT_PIPELINE_STAGE_ = stage
+    op.desc._set_distributed_attr('pipeline_stage', stage)
