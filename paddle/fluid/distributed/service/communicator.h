fdac4b62c7580a43e82e84accbd08ef87c1434a1
seemingwang <zsasuke@qq.com>
2021-07-28 19:00:54 +0800

    fix style

--- paddle/fluid/distributed/service/communicator.h
+++ paddle/fluid/distributed/service/communicator.h
@@ -152,9 +152,8 @@ template <typename T>
 inline void MergeVars(const std::string &var_name,
                       const std::vector<std::shared_ptr<Variable>> &vars,
                       Scope *scope, bool merge_add = true) {
-  PADDLE_ENFORCE_NE(
-      vars.empty(), true,
-      platform::errors::InvalidArgument("vector vars are empty."));
+  PADDLE_ENFORCE_NE(vars.empty(), true, platform::errors::InvalidArgument(
+                                              "vector vars are empty."));
   auto cpu_place = platform::CPUPlace();
   auto &var0 = vars[0];
   auto *out_var = scope->Var(var_name);
