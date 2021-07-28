1818bc612c80e5bd13e52b77039fc7a1516ed556
shiyutang <1574572981@qq.com>
2021-07-28 19:50:45 +0800

    test=document_fix

--- python/paddle/nn/layer/common.py
+++ python/paddle/nn/layer/common.py
@@ -53,8 +53,8 @@ class Identity(Layer):
 
           import paddle
 
-          input_tensor = paddle.to_tensor(paddle.randn(shape=[3, 2]))
-          layer = paddle.nn.Identity(54)
+          input_tensor = paddle.randn(shape=[3, 2])
+          layer = paddle.nn.Identity()
           out = layer(input_tensor)
           # input_tensor: [[-0.32342386 -1.200079  ]
           #                [ 0.7979031  -0.90978354]
