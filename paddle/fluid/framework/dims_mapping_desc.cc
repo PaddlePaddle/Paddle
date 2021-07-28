f5ef24572029f0d363e5dcaf0450144428db7398
sandyhouse <lilong12@baidu.com>
2021-07-28 14:30:23 +0800

    update, test=develop

---
+++ paddle/fluid/framework/dims_mapping_desc.cc
@@ -0,0 +1,39 @@
+/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#include "paddle/fluid/framework/dims_mapping_desc.h"
+
+namespace paddle {
+namespace framework {
+
+DimsMappingDesc::DimsMappingDesc(const std::string& name,
+                                 const proto::DimsMappingDesc::Type type,
+                                 const std::vector<int32_t>& mapping) {
+  desc_.set_name(name);
+  desc_.set_type(type);
+  for (size_t i = 0; i != mapping.size(); ++i) {
+    desc_.add_dims_mapping(mapping[i]);
+  }
+}
+
+std::vector<int32_t> DimsMappingDesc::DimsMapping() const {
+  size_t size = desc_.dims_mapping_size();
+  std::vector<int32_t> ret(size);
+  for (size_t i = 0; i != size; ++i) {
+    ret[i] = desc_.dims_mapping(i);
+  }
+  return ret;
+}
+}  // namespace framework
+}  // namespace paddle
---
+++ paddle/fluid/framework/dims_mapping_desc.h
@@ -0,0 +1,42 @@
+/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "paddle/fluid/framework/framework.pb.h"
+#include "paddle/fluid/framework/proto_desc.h"
+#include "paddle/fluid/platform/macros.h"
+
+namespace paddle {
+namespace framework {
+
+class DimsMappingDesc {
+ public:
+  DimsMappingDesc(const std::string& name,
+                  const proto::DimsMappingDesc::Type type,
+                  const std::vector<int32_t>& mapping);
+
+  std::string Name() const { return desc_.name(); }
+  proto::DimsMappingDesc::Type Type() const { return desc_.type(); }
+
+  std::vector<int32_t> DimsMapping() const;
+
+ private:
+  proto::DimsMappingDesc desc_;  // not_own
+};
+}  // namespace framework
+}  // namespace paddle
