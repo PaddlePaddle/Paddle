c636a3199ac798901624483c033c413ed05895b2
shenliang03 <shenliang03@baidu.com>
2021-07-28 11:09:49 +0800

    support tuple

--- python/paddle/distributed/fleet/meta_parallel/pp_utils/p2p_communication.py
+++ python/paddle/distributed/fleet/meta_parallel/pp_utils/p2p_communication.py
@@ -23,7 +23,7 @@ _tensor_shape = (2, 1024, 768)
 class SendRecvMeta:
     def __init__(self):
         self.send_shape_message = None
-        self.send_dtype_messgae = None
+        self.send_dtype_message = None
 
         self.recv_shape_message = None
         self.recv_dtype_message = None
@@ -31,51 +31,85 @@ class SendRecvMeta:
         self.has_send_meta = False
         self.has_recv_meta = False
 
+    def _recv_shape_dtype(self, group):
+        # recv len(shape)
+        dims = paddle.to_tensor([0])
+        paddle.distributed.recv(dims, src=0, group=group)
+        dims = dims.item()
+
+        # recv shape
+        shape = paddle.to_tensor([0] * dims)
+        paddle.distributed.recv(shape, src=0, group=group)
+
+        # recv dtype
+        dtype = paddle.to_tensor([0])
+        paddle.distributed.recv(dtype, src=0, group=group)
+        return shape.numpy().tolist(), dtype.item()
+
     def recv_meta(self, group):
         tensor_type = paddle.to_tensor([0])
         paddle.distributed.recv(tensor_type, src=0, group=group)
-
         tensor_type = tensor_type.item()
+
         if tensor_type == 0:
-            # recv len(shape)
-            dims = paddle.to_tensor([0])
-            paddle.distributed.recv(dims, src=0, group=group)
-            dims = dims.item()
-
-            # recv shape
-            shape = paddle.to_tensor([0] * dims)
-            paddle.distributed.recv(shape, src=0, group=group)
-            shape = shape.numpy().tolist()
-
-            # recv dtype
-            dtype = paddle.to_tensor([0])
-            paddle.distributed.recv(dtype, src=0, group=group)
-
+            shape, dtype = self._recv_shape_dtype(group)
             self.recv_shape_message = shape
-            self.recv_dtype_message = dtype.item()
+            self.recv_dtype_message = dtype
+
+        elif tensor_type == 1:
+            num = paddle.to_tensor([0])
+            paddle.distributed.recv(num, src=0, group=group)
+            num = num.item()
+            shapes = []
+            dtypes = []
+            for i in range(num):
+                shape, dtype = self._recv_shape_dtype()
+                shapes.append(shape)
+                dtypes.append(dtype)
+
+            self.recv_shape_message = tuple(shapes)
+            self.recv_dtype_message = tuple(dtypes)
+
+    def _send_dims_shape_dtype(self, tensor, group):
+        # send len(shape)
+        dims = paddle.to_tensor(len(tensor.shape))
+        paddle.distributed.send(dims, dst=1, group=group)
+
+        # send shape
+        shape = paddle.to_tensor(tensor.shape)
+        paddle.distributed.send(shape, dst=1, group=group)
+
+        # send dtype
+        dtype = paddle.to_tensor(paddle_2_number(tensor.dtype))
+        paddle.distributed.send(dtype, dst=1, group=group)
 
     def send_meta(self, tensor, group):
         if isinstance(tensor, paddle.Tensor):
             tensor_type = paddle.to_tensor([0])
-
             # send tensor type
             paddle.distributed.send(tensor_type, dst=1, group=group)
 
-            # send len(shape)
-            dims = paddle.to_tensor(len(tensor.shape))
-            paddle.distributed.send(dims, dst=1, group=group)
+            self._send_dims_shape_dtype(tensor, group)
+        elif isinstance(tensor, tuple):
+            tensor_type = paddle.to_tensor([1])
+            # send tensor type
+            paddle.distributed.send(tensor_type, dst=1, group=group)
 
-            # send shape
-            shape = paddle.to_tensor(tensor.shape)
-            paddle.distributed.send(shape, dst=1, group=group)
+            nums = paddle.to_tensor(len(tensor))
+            paddle.distributed.send(nums, dst=1, group=group)
 
-            # send dtype
-            dtype = paddle.to_tensor(paddle_2_number(tensor.dtype))
-            paddle.distributed.send(dtype, dst=1, group=group)
+            for d in tensor:
+                assert isinstance(d, paddle.Tensor)
+                self._send_dims_shape_dtype(d)
 
     def set_send_message(self, tensor):
-        self.send_shape_message = tensor.shape
-        self.send_dtype_message = paddle_2_number(tensor.dtype)
+        if isinstance(tensor, paddle.Tensor):
+            self.send_shape_message = tensor.shape
+            self.send_dtype_message = paddle_2_number(tensor.dtype)
+        elif isinstance(tensor, tuple):
+            self.send_shape_message = tuple([d.shape for d in tensor])
+            self.send_dtype_message = tuple(
+                [paddle_2_number(d.dtype) for d in tensor])
 
 
 _send_recv_meta = SendRecvMeta()
@@ -86,100 +120,90 @@ def initialize_p2p_groups(hcg):
     _hcg = hcg
 
 
-def send(tensor, dest_stage):
-    global _groups, _hcg
-    src_stage = _hcg.get_stage_id()
-    _is_valid_communciate(src_stage, dest_stage)
-    group = _get_send_recv_group(src_stage, dest_stage)
-    return paddle.distributed.send(
-        tensor, dst=1 if dest_stage > src_stage else 0, group=group)
-
-
-def recv(tensor, src_stage):
-    global _groups, _hcg
-    dest_stage = _hcg.get_stage_id()
-
-    _is_valid_communciate(src_stage, dest_stage)
-    group = _get_send_recv_group(src_stage, dest_stage)
-    return paddle.distributed.recv(
-        tensor, src=0 if dest_stage > src_stage else 1, group=group)
-
-
-def _get_send_recv_group(src_stage, dest_stage):
-    global _groups, _hcg
-    stage_id = None
-    first_stage = 0
-    last_stage = _hcg.get_pipe_parallel_world_size() - 1
-    group_id = _hcg.get_rank_from_stage(stage_id=src_stage)
-    return _groups[group_id]
-
-
 def _communicate(tensor_send_next, tensor_send_prev, recv_prev, recv_next):
+    global _groups, _hcg
 
     tensor_recv_prev = None
     tensor_recv_next = None
 
-    global _tensor_shape, _groups, _hcg
-    tensor_chunk_shape = _tensor_shape
-    dtype = "float32"
-
-    current_stage = _hcg.get_stage_id()
-    prev_stage = current_stage - 1
-    next_stage = current_stage + 1
+    recv_shape_msg = _send_recv_meta.recv_shape_message
+    recv_dtype_msg = _send_recv_meta.recv_dtype_message
+    send_shape_msg = _send_recv_meta.send_shape_message
+    send_dtype_msg = _send_recv_meta.send_dtype_message
 
     if recv_prev:
-        # tensor_recv_prev = paddle.empty(shape=tensor_chunk_shape, dtype=dtype)
-        tensor_recv_prev = paddle.empty(
-            shape=_send_recv_meta.recv_shape_message,
-            dtype=number_2_dtype(_send_recv_meta.recv_dtype_message))
+        if isinstance(recv_shape_msg, tuple):
+            for idx, shape in enumerate(recv_shape_msg):
+                tensor_recv_prev = tuple([
+                    paddle.empty(
+                        shape=shape, dtype=number_2_dtype(recv_dtype_msg[idx]))
+                ])
+        else:
+            tensor_recv_prev = paddle.empty(
+                shape=recv_shape_msg, dtype=number_2_dtype(recv_dtype_msg))
 
     if recv_next:
-        # tensor_recv_next = paddle.empty(shape=tensor_chunk_shape, dtype=dtype)
-        tensor_recv_next = paddle.empty(
-            shape=_send_recv_meta.send_shape_message,
-            dtype=number_2_dtype(_send_recv_meta.send_dtype_message))
+        if isinstance(send_shape_msg, tuple):
+            for idx, shape in enumerate(send_shape_msg):
+                tensor_recv_next = tuple([
+                    paddle.empty(
+                        shape=shape, dtype=number_2_dtype(send_dtype_msg[idx]))
+                ])
+        else:
+            tensor_recv_next = paddle.empty(
+                shape=send_shape_msg, dtype=number_2_dtype(send_dtype_msg))
 
     if tensor_send_prev is not None:
-        # group = _get_send_recv_group(
-        # src_stage=current_stage, dest_stage=prev_stage)
-        #print("group msg:", group)
-        paddle.distributed.wait(tensor_send_prev, use_calc_stream=True)
-        paddle.distributed.send(
-            tensor_send_prev,
-            dst=0,
-            group=_hcg.send_prev_group,
-            use_calc_stream=False)
+        if isinstance(tensor_send_prev, tuple):
+            for d in tensor_send_prev:
+                paddle.distributed.wait(d, use_calc_stream=True)
+                paddle.distributed.send(
+                    d, dst=0, group=_hcg.send_prev_group, use_calc_stream=False)
+        else:
+            paddle.distributed.wait(tensor_send_prev, use_calc_stream=True)
+            paddle.distributed.send(
+                tensor_send_prev,
+                dst=0,
+                group=_hcg.send_prev_group,
+                use_calc_stream=False)
+
     if tensor_recv_prev is not None:
-        # group = _get_send_recv_group(
-        # src_stage=prev_stage, dest_stage=current_stage)
-        #print("group msg:", group)
-        paddle.distributed.recv(
-            tensor_recv_prev,
-            src=0,
-            group=_hcg.recv_prev_group,
-            use_calc_stream=True)
-        #print("tensor_recv_prev", tensor_recv_prev.numpy())
+        if isinstance(tensor_recv_prev, tuple):
+            for d in tensor_recv_prev:
+                paddle.distributed.recv(
+                    d, src=0, group=_hcg.recv_prev_group, use_calc_stream=True)
+        else:
+            paddle.distributed.recv(
+                tensor_recv_prev,
+                src=0,
+                group=_hcg.recv_prev_group,
+                use_calc_stream=True)
 
     if tensor_send_next is not None:
-        # group = _get_send_recv_group(
-        # src_stage=current_stage, dest_stage=next_stage)
-        #print("group msg:", group)
-        paddle.distributed.wait(tensor_send_next, use_calc_stream=True)
-        paddle.distributed.send(
-            tensor_send_next,
-            dst=1,
-            group=_hcg.send_next_group,
-            use_calc_stream=False)
+        if isinstance(tensor_send_next, tuple):
+            for d in tensor_send_next:
+                paddle.distributed.wait(d, use_calc_stream=True)
+                paddle.distributed.send(
+                    d, dst=1, group=_hcg.send_next_group, use_calc_stream=False)
+        else:
+            paddle.distributed.wait(tensor_send_next, use_calc_stream=True)
+            paddle.distributed.send(
+                tensor_send_next,
+                dst=1,
+                group=_hcg.send_next_group,
+                use_calc_stream=False)
+
     if tensor_recv_next is not None:
-        # group = _get_send_recv_group(
-        # src_stage=next_stage, dest_stage=current_stage)
-        #print("group msg:", group)
-        paddle.distributed.recv(
-            tensor_recv_next,
-            src=1,
-            group=_hcg.recv_next_group,
-            use_calc_stream=True)
-        #print("tensor_recv_next", tensor_recv_next.numpy())
+        if isinstance(tensor_recv_next, tuple):
+            for d in tensor_recv_next:
+                paddle.distributed.recv(
+                    d, src=1, group=_hcg.recv_next_group, use_calc_stream=True)
+        else:
+            paddle.distributed.recv(
+                tensor_recv_next,
+                src=1,
+                group=_hcg.recv_next_group,
+                use_calc_stream=True)
 
     return tensor_recv_prev, tensor_recv_next
 
@@ -188,7 +212,6 @@ def recv_forward():
     if _hcg.is_first_stage:
         input_tensor = None
     else:
-        # check recv forward
         if not _send_recv_meta.has_recv_meta:
             _send_recv_meta.recv_meta(_hcg.recv_prev_group)
             _send_recv_meta.has_recv_meta = True
@@ -215,7 +238,6 @@ def recv_backward():
 
 def send_forward(output_tensor):
     if not _hcg.is_last_stage:
-
         if not _send_recv_meta.has_send_meta:
             _send_recv_meta.set_send_message(output_tensor)
             _send_recv_meta.send_meta(output_tensor, _hcg.send_next_group)
@@ -241,11 +263,6 @@ def send_forward_recv_backward(output_tensor):
     if _hcg.is_last_stage:
         output_tensor_grad = None
     else:
-        if not _send_recv_meta.has_send_meta:
-            _send_recv_meta.set_send_message(output_tensor)
-            _send_recv_meta.send_meta(output_tensor, _hcg.send_next_group)
-            _send_recv_meta.has_send_meta = True
-
         _, output_tensor_grad = _communicate(
             tensor_send_next=output_tensor,
             tensor_send_prev=None,
