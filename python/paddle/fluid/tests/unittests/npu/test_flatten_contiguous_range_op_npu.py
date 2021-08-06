0da867c3d04deffd2e2a379d1640bdcaa7a8859c
Liu-xiandong <liuxiandong@ncic.ac.cn>
2021-08-06 13:00:18 +0800

    [NPU] Support npu kernel for flatten_contiguous_range op, test=develop

--- python/paddle/fluid/tests/unittests/npu/test_flatten_contiguous_range_op_npu.py
+++ python/paddle/fluid/tests/unittests/npu/test_flatten_contiguous_range_op_npu.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -50,7 +50,6 @@ class TestFlattenOp(OpTest):
 
     def test_check_grad(self):
         pass
-        #self.check_grad_with_place(self.place, ['X'], 'Out')
 
     def init_test_case(self):
         self.in_shape = (3, 2, 5, 4)
