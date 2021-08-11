089cabd4d524ad096b1559440fd94174553a6642
lipeihan01 <lipeihan01@baidu.com>
2021-08-11 19:04:51 +0800

    fix LeViT model dir bugs

--- paddle/fluid/inference/tests/infer_ut/run.sh
+++ paddle/fluid/inference/tests/infer_ut/run.sh
@@ -72,7 +72,7 @@ for model_name in $ocr_download_list; do
 done
 
 clas_download_list='LeViT'
-for model_name in $ocr_download_list; do
+for model_name in $clas_download_list; do
     url_prefix="https://paddle-qa.bj.bcebos.com/inference_model/2.1.1/class"
     download $url_prefix $model_name
 done
