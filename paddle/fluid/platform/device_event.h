33e677f74314242d564cfd983663bdf326e2e407
Aurelius <zhangliujie@baidu.com>
2021-08-12 11:03:29 +0000

    add device_context

---
+++ paddle/fluid/platform/device_event.h
@@ -0,0 +1,144 @@
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+#include <memory>
+#include "paddle/fluid/platform/enforce.h"
+#include "paddle/fluid/platform/place.h"
+
+namespace paddle {
+namespace platform {
+
+class DeviceOption;
+class DeviceEvent;
+
+enum class DeviceType {
+  kCPU = 0,
+  kCUDA = 1,  // CUDA.
+  kHIP = 2,   // AMD HIP
+
+  MAX_DEVICE_TYPES = 3,
+};
+
+constexpr int MaxDeviceTypes = static_cast<int>(DeviceType::MAX_DEVICE_TYPES);
+
+typedef void (*EventCreateFunction)(DeviceEvent*, const DeviceOption&);
+typedef void (*EventRecordFunction)(DeviceEvent*, const platform::Place&,
+                                    const void*);
+typedef bool (*EventQueryFunction)(const DeviceEvent*);
+
+class DeviceOption {
+ public:
+  explicit DeviceOption(int device_type) : device_type_(device_type) {}
+
+  int device_type() const { return device_type_; }
+
+ private:
+  int device_type_;
+};
+
+class DeviceEvent {
+ public:
+  explicit DeviceEvent(const DeviceOption& device_option)
+      : event_(),
+        type_(device_option.device_type()),
+        device_option_(device_option) {
+    PADDLE_ENFORCE_LT(type_, MaxDeviceTypes,
+                      platform::errors::PreconditionNotMet(
+                          "Required type < %d, but received type = %d",
+                          MaxDeviceTypes, type_));
+    PADDLE_ENFORCE_NOT_NULL(
+        event_creator_[type_],
+        platform::errors::Unavailable(
+            "event_creator_[%d] shall not be nullptr.", type_));
+    event_creator_[type_](this, device_option_);
+  }
+
+  ~DeviceEvent() {}
+
+  void Record(const platform::Place& place, const void* dev_ctx) {
+    PADDLE_ENFORCE_NOT_NULL(
+        event_recorder_[type_],
+        platform::errors::Unavailable(
+            "event_recorder_[%d] shall not be nullptr.", type_));
+    event_recorder_[type_](this, place, dev_ctx);
+  }
+
+  bool Query() {
+    PADDLE_ENFORCE_NOT_NULL(
+        event_querier_[type_],
+        platform::errors::Unavailable(
+            "event_querier_[%d] shall not be nullptr.", type_));
+    event_querier_[type_](this);
+  }
+
+ private:
+  std::shared_ptr<void> event_;
+  int type_;
+  DeviceOption device_option_;
+
+  static EventCreateFunction event_creator_[MaxDeviceTypes];
+  static EventRecordFunction event_recorder_[MaxDeviceTypes];
+  static EventQueryFunction event_querier_[MaxDeviceTypes];
+
+  template <DeviceType device_typ>
+  friend struct EventCreateFunctionRegisterer;
+
+  template <DeviceType device_typ>
+  friend struct EventRecordFunctionRegisterer;
+
+  template <DeviceType device_typ>
+  friend struct EventQueryFunctionRegisterer;
+};
+
+template <DeviceType device_type>
+struct EventCreateFunctionRegisterer {
+  explicit EventCreateFunctionRegisterer(EventCreateFunction func) {
+    auto type_idx = DeviceTypeToId(device_type);
+    DeviceEvent::event_creator_[type_idx] = func;
+  }
+};
+#define REGISTER_EVENT_CREATE_FUNCTION(device_type, func) \
+  namespace {                                             \
+  static EventCreateFunctionRegisterer<device_type>       \
+      g_device_event_create_##type_idx(func)              \
+  }
+
+template <DeviceType device_type>
+struct EventRecordFunctionRegisterer {
+  explicit EventRecordFunctionRegisterer(EventRecordFunction func) {
+    auto type_idx = DeviceTypeToId(device_type);
+    DeviceEvent::event_recorder_[type_idx] = func;
+  }
+};
+#define REGISTER_EVENT_RECORD_FUNCTION(device_type, func) \
+  namespace {                                             \
+  static EventRecordFunctionRegisterer<device_type>       \
+      g_device_event_record_##type_idx(func)              \
+  }
+
+template <DeviceType device_type>
+struct EventQueryFunctionRegisterer {
+  explicit EventQueryFunctionRegisterer(EventQueryFunction func) {
+    auto type_idx = DeviceTypeToId(device_type);
+    DeviceEvent::event_querier_[type_idx] = func;
+  }
+};
+#define REGISTER_EVENT_QUERY_FUNCTION(device_type, func) \
+  namespace {                                            \
+  static EventQueryFunctionRegisterer<device_type>       \
+      g_device_event_query_##type_idx(func)              \
+  }
+
+}  // namespace platform
+}  // namespace paddle
