18732d7c19615a5084fc4ea7f294d8e6a2ffb676
baoachun <962571062@qq.com>
2021-08-13 01:59:17 +0000

    add elementwise max grad op for npu

--- paddle/fluid/operators/elementwise/elementwise_max_op_npu.cc
+++ paddle/fluid/operators/elementwise/elementwise_max_op_npu.cc
@@ -12,8 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/framework/tensor_util.h"
-#include "paddle/fluid/operators/elementwise/elementwise_min_op.h"
+#include "paddle/fluid/operators/elementwise/elementwise_max_op.h"
 #include "paddle/fluid/operators/elementwise/elementwise_npu.h"
 #include "paddle/fluid/operators/npu_op_runner.h"
 
