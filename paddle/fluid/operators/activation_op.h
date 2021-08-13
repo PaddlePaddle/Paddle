da38b7d5a3eae575b83fccc447f787eee97feac1
Zjq9409 <15205085056@163.com>
2021-08-13 02:09:25 +0000

    opt relu

--- paddle/fluid/operators/activation_op.h
+++ paddle/fluid/operators/activation_op.h
@@ -403,9 +403,7 @@ template <typename T>
 struct ReluCPUFunctor : public BaseActivationFunctor<T> {
   template <typename Device, typename X, typename Out>
   void operator()(Device d, X x, Out out) const {
-    out.device(d) = x.unaryExpr([] HOSTDEVICE(T v) {
-      return v > static_cast<T>(0) ? v : static_cast<T>(0);
-    });
+    out.device(d) = x.cwiseMax(static_cast<T>(0));
   }
 };
 
