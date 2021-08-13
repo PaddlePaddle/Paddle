d9e77d1bbb7346b2cec1a12c257d5d931edba25b
baoachun <962571062@qq.com>
2021-08-13 01:50:43 +0000

    add elementwise max grad op for npu

--- paddle/fluid/operators/elementwise/elementwise_max_op_npu.cc
+++ paddle/fluid/operators/elementwise/elementwise_max_op_npu.cc
@@ -1,24 +1,25 @@
 /* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-#include <memory>
-#include <string>
 
 #include "paddle/fluid/framework/tensor_util.h"
 #include "paddle/fluid/operators/elementwise/elementwise_min_op.h"
 #include "paddle/fluid/operators/elementwise/elementwise_npu.h"
 #include "paddle/fluid/operators/npu_op_runner.h"
-#include "paddle/fluid/platform/enforce.h"
 
 namespace paddle {
 namespace operators {
+
 using Tensor = framework::Tensor;
 
 template <typename DeviceContext, typename T>
@@ -34,10 +35,6 @@ class ElementwiseMaxNPUKernel : public framework::OpKernel<T> {
     out->mutable_data<T>(ctx.GetPlace());
     int axis = ctx.Attr<int>("axis");
 
-    auto stream =
-        ctx.template device_context<paddle::platform::NPUDeviceContext>()
-            .stream();
-
     bool direct_compute = false;
     auto x_dims = x->dims();
     auto y_dims = y->dims();
@@ -49,6 +46,10 @@ class ElementwiseMaxNPUKernel : public framework::OpKernel<T> {
       direct_compute =
           x_dims == framework::slice_ddim(y_dims, axis, y_dims.size());
     }
+
+    auto stream =
+        ctx.template device_context<paddle::platform::NPUDeviceContext>()
+            .stream();
 
     if (direct_compute) {
       const auto& runner = NpuOpRunner("Maximum", {*x, *y}, {*out}, {});
