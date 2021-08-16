6ad7f26a2f71020b2ce7a4ce82f5953595f985a1
chenruibiao <chenruibiao@baidu.com>
2021-08-16 11:21:55 +0800

    Modify codes according to the review comments

--- paddle/fluid/operators/interpolate_op_npu.cc
+++ paddle/fluid/operators/interpolate_op_npu.cc
@@ -1,13 +1,16 @@
-/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-   http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License. */
+/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 #include "paddle/fluid/operators/interpolate_op.h"
 #include <string>
@@ -102,7 +105,7 @@ template <typename T>
 class InterpolateNPUKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
-    // Note(Ruibiao):
+    // NOTE(Ruibiao):
     // this kernel only support nearest interpolotion for 2D images
     // the Ascend 'ResizeNearestNeighborV2' used in this kernle has diff
     // when 'align_corners' is 'true' or data type is 'double'
@@ -153,7 +156,7 @@ template <typename T>
 class InterpolateGradNPUKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
-    // Note(Ruibiao):
+    // NOTE(Ruibiao):
     // this kernel only support nearest interpolotion for 2D images
     // the Ascend 'ResizeNearestNeighborV2' used in this kernle has diff
     // when 'align_corners' is 'true' or data type is 'double'
