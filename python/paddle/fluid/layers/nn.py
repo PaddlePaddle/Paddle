2a4ff29e102224a242517d5709c2fb3b6b5eef1e
Chen Long <1300851984@qq.com>
2021-08-16 14:50:02 +0800

    fix shuffle_channel bugs test=document_fix

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -13355,7 +13355,9 @@ def shuffle_channel(x, group, name=None):
     Examples:
         .. code-block:: python
 
-            import paddle.fluid as fluid
+            import paddle
+	    import paddle.fluid as fluid
+	    paddle.enable_static()
             input = fluid.data(name='input', shape=[None,4,2,2], dtype='float32')
             out = fluid.layers.shuffle_channel(x=input, group=2)
     """
