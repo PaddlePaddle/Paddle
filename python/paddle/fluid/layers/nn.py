76eb2f2f74c1d83332b477dd4dddb1da5cfc14b8
Chen Long <1300851984@qq.com>
2021-08-16 14:51:14 +0800

    fix code style test=document_fix

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -13356,8 +13356,8 @@ def shuffle_channel(x, group, name=None):
         .. code-block:: python
 
             import paddle
-	    import paddle.fluid as fluid
-	    paddle.enable_static()
+	      import paddle.fluid as fluid
+	      paddle.enable_static()
             input = fluid.data(name='input', shape=[None,4,2,2], dtype='float32')
             out = fluid.layers.shuffle_channel(x=input, group=2)
     """
