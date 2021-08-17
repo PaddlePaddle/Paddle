efe7f243fdf49a7b4826d359660ad950352c7eb7
zhiqiu <chenqiuliang@baidu.com>
2021-08-17 13:19:46 +0000

    reduce retry interval

--- paddle/fluid/operators/collective/c_gen_hccl_id_op.cc
+++ paddle/fluid/operators/collective/c_gen_hccl_id_op.cc
@@ -33,7 +33,7 @@ namespace operators {
 
 static void GenHCCLID(std::vector<HcclRootInfo>* hccl_ids) {
   constexpr int timeout = 2 * 60 + 10;  // 2MSL+10s
-  constexpr int retry_time = 2;
+  constexpr int retry_time = 1;
   for (size_t i = 0; i < hccl_ids->size(); ++i) {
     bool failed = true;
     for (auto retry_times = 0; retry_times * retry_time < timeout;
