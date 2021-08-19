89d2aa30c5edfe946bf8385e205a9120ee521036
JZZ-NOTE <jzz2446071400@163.com>
2021-08-19 19:40:07 +0800

    add sin op npu

--- python/paddle/fluid/tests/unittests/npu/test_sin_op_npu.py
+++ python/paddle/fluid/tests/unittests/npu/test_sin_op_npu.py
@@ -12,19 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from __future__ import print_function
 import unittest
 
@@ -60,9 +47,6 @@ def test_class(op_type, typename):
             self.check_output_with_place(self.place)
 
         def test_check_grad(self):
-            pass
-
-        def init_kernel_type(self):
             pass
 
         def test_out_name(self):
