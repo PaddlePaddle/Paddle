ab4b034f71803392907b3d0d690e14d7b808f4e5
Thunderbrook <a754913769@163.com>
2021-08-19 21:21:31 +0800

    log level

--- paddle/fluid/framework/ps_gpu_trainer.cc
+++ paddle/fluid/framework/ps_gpu_trainer.cc
@@ -114,7 +114,7 @@ void PSGPUTrainer::InitTrainerEnv(const ProgramDesc& main_program,
     if (var->Persistable()) {
       auto it = std::find(need_merge_var_names_.begin(), need_merge_var_names_.end(), var->Name());
       if (it == need_merge_var_names_.end()) {
-        VLOG(1) << "train param: " << var->Name();
+        VLOG(2) << "train param: " << var->Name();
         trainable_param_.push_back(var->Name());
       }
     }
