75549af6d7cb8b7894e62a3e6fb726b108298cbb
GuoxiaWang <mingzilaochongtu@gmail.com>
2021-08-20 19:48:04 +0800

    refactor unittest

--- python/paddle/fluid/tests/unittests/test_sparse_momentum_op.py
+++ python/paddle/fluid/tests/unittests/test_sparse_momentum_op.py
@@ -77,6 +77,9 @@ class TestSparseMomentumOp(OpTest):
         self.init_multi_precision()
         self.init_use_nesterov()
 
+        if self.multi_precision:
+            assert self.dtype == np.float16
+
         param = np.random.random(
             (self.batch_size, self.num_classes)).astype(self.dtype)
         grad = np.random.random(
@@ -98,6 +101,7 @@ class TestSparseMomentumOp(OpTest):
         velocity = np.random.random(
             (self.batch_size, self.num_classes)).astype(self.dtype)
         learning_rate = np.array([0.001]).astype(self.dtype)
+
         mu = 0.9
         regularization_method = "l2_decay"
         regularization_coeff = 1.0
@@ -112,7 +116,7 @@ class TestSparseMomentumOp(OpTest):
             regularization_method=regularization_method,
             regularization_coeff=regularization_coeff,
             index=index,
-            axis=self.axis, )
+            axis=self.axis)
 
         self.attrs = {
             'mu': mu,
@@ -124,20 +128,27 @@ class TestSparseMomentumOp(OpTest):
         }
 
         self.inputs = {
-            'Param': param,
-            'Velocity': velocity,
-            'LearningRate': learning_rate,
-            'Grad': grad,
+            'Param': param.astype("float16") if self.multi_precision else param,
+            'Velocity': velocity.astype("float32")
+            if self.multi_precision else velocity,
+            'LearningRate': learning_rate.astype("float32")
+            if self.multi_precision else learning_rate,
+            'Grad': grad.astype("float16") if self.multi_precision else grad,
             'Index': index,
+            'Axis': np.array(self.axis).astype(np.int32),
         }
         self.outputs = {
-            'ParamOut': param_out,
-            'VelocityOut': velocity_out,
+            'ParamOut': param_out.astype("float16")
+            if self.multi_precision else param_out,
+            'VelocityOut': velocity_out.astype("float32")
+            if self.multi_precision else velocity_out,
         }
 
         if self.multi_precision:
-            self.inputs['MasterParam'] = param
-            self.outputs['MasterParamOut'] = param_out
+            self.inputs['MasterParam'] = param.astype(
+                "float32") if self.multi_precision else param
+            self.outputs['MasterParamOut'] = param_out.astype(
+                "float32") if self.multi_precision else param_out
 
     def init_dtype(self):
         pass
@@ -152,7 +163,7 @@ class TestSparseMomentumOp(OpTest):
         pass
 
     def test_check_output(self):
-        self.check_output()
+        self.check_output(atol=5e-3 if self.multi_precision else 1e-5)
 
 
 class TestSparseMomentumOpDtype1(TestSparseMomentumOp):
@@ -183,137 +194,49 @@ class TestSparseMomentumOpNesterov(TestSparseMomentumOp):
         self.use_nesterov = True
 
 
-class TestSparseMomentumOpMultiPrecision(unittest.TestCase):
-    def setUp(self):
-        self.dtype = np.float32
-        self.index_dtype = np.int32
-        self.axis = 0
-        self.multi_precision = True
-        self.use_nesterov = False
-        self.batch_size = 20
-        self.num_classes = 20
-        self.init_dtype()
-        self.init_axis()
-        self.init_multi_precision()
-        self.init_use_nesterov()
-
-    def check_with_place(self, place):
-        scope = core.Scope()
-
-        param = np.full((self.batch_size, self.num_classes),
-                        5.0).astype(self.dtype)
-        grad = np.full((self.batch_size, self.num_classes),
-                       1.0).astype(self.dtype)
-        velocity = np.full((self.batch_size, self.num_classes),
-                           1.0).astype(self.dtype)
-
-        if self.axis == 0:
-            index = np.random.randint(
-                0,
-                self.batch_size,
-                size=(self.batch_size // 2, ),
-                dtype=self.index_dtype)
-            grad = grad[index]
-        else:
-            index = np.random.randint(
-                0,
-                self.num_classes,
-                size=(self.num_classes // 2, ),
-                dtype=self.index_dtype)
-            grad = grad[:, index]
-
-        learning_rate = np.array([2.0]).astype(self.dtype)
-        mu = 1.0
-        regularization_method = "l2_decay"
-        regularization_coeff = 1.0
-
-        param_tensor = scope.var('Param').get_tensor()
-        param_tensor.set(param.astype("float16"), place)
-        master_param_tensor = scope.var('MasterParam').get_tensor()
-        master_param_tensor.set(param, place)
-
-        grad_tensor = scope.var('Grad').get_tensor()
-        grad_tensor.set(grad.astype("float16"), place)
-
-        param_out_array = np.full((self.batch_size, self.num_classes),
-                                  0.0).astype(self.dtype)
-        param_out_tensor = scope.var("ParamOut").get_tensor()
-        param_out_tensor.set(param_out_array.astype("float16"), place)
-        master_param_out_tensor = scope.var("MasterParamOut").get_tensor()
-        master_param_out_tensor.set(param_out_array, place)
-
-        velocity_tensor = scope.var('Velocity').get_tensor()
-        velocity_tensor.set(velocity, place)
-        velocity_out_array = np.full((self.batch_size, self.num_classes),
-                                     0.0).astype(self.dtype)
-        velocity_out_tensor = scope.var("VelocityOut").get_tensor()
-        velocity_out_tensor.set(velocity_out_array, place)
-
-        index_tensor = scope.var('Index').get_tensor()
-        index_tensor.set(index, place)
-
-        lr = scope.var('LearningRate').get_tensor()
-        lr.set(learning_rate, place)
-
-        op = Operator(
-            "sparse_momentum",
-            Param='Param',
-            Grad='Grad',
-            Velocity='Velocity',
-            MasterParam='MasterParam',
-            ParamOut='ParamOut',
-            VelocityOut='VelocityOut',
-            MasterParamOut='MasterParamOut',
-            LearningRate='LearningRate',
-            Index='Index',
-            mu=mu,
-            use_nesterov=self.use_nesterov,
-            regularization_method=regularization_method,
-            regularization_coeff=regularization_coeff,
-            multi_precision=True,
-            axis=self.axis)
-        op.run(scope, place)
-
-        param_out, velocity_out = calculate_sparse_momentum_by_numpy(
-            param=param.astype("float16"),
-            grad=grad.astype("float16"),
-            mu=np.array(
-                mu, dtype="float16"),
-            velocity=velocity.astype("float16"),
-            use_nesterov=self.use_nesterov,
-            learning_rate=np.array(
-                learning_rate, dtype="float16"),
-            regularization_method=regularization_method,
-            regularization_coeff=np.array(
-                regularization_coeff, dtype="float16"),
-            index=index,
-            axis=self.axis, )
-
-        self.assertTrue((param_out == np.array(param_out_tensor)).all())
-        self.assertTrue((velocity_out == np.array(velocity_out_tensor)).all())
-
-    def test_sparse_momentum(self):
-        if core.is_compiled_with_cuda():
-            self.check_with_place(fluid.CUDAPlace(0))
-
+class TestSparseMomentumOpMultiPrecision(TestSparseMomentumOp):
     def init_dtype(self):
-        pass
-
-    def init_axis(self):
-        pass
+        self.dtype = np.float16
+        self.index_dtype = np.int32
 
     def init_multi_precision(self):
-        pass
+        self.multi_precision = True
 
-    def init_use_nesterov(self):
-        pass
-
-
-class TestSparseMomentumOpMultiPrecision1(TestSparseMomentumOpMultiPrecision):
     def init_use_nesterov(self):
         self.use_nesterov = True
 
 
-class TestSparseMomentumOpMultiPrecision2(TestSparseMomentumOpMultiPrecision):
-    def init_axis(self):
-        self.axis = 1
+class TestSparseMomentumOpMultiPrecision1(TestSparseMomentumOp):
+    def init_dtype(self):
+        self.dtype = np.float16
+        self.index_dtype = np.int64
+
+    def init_multi_precision(self):
+        self.multi_precision = True
+
+    def init_use_nesterov(self):
+        self.use_nesterov = True
+
+
+class TestSparseMomentumOpMultiPrecision2(TestSparseMomentumOp):
+    def init_dtype(self):
+        self.dtype = np.float16
+        self.index_dtype = np.int32
+
+    def init_multi_precision(self):
+        self.multi_precision = True
+
+    def init_use_nesterov(self):
+        self.use_nesterov = False
+
+
+class TestSparseMomentumOpMultiPrecision3(TestSparseMomentumOp):
+    def init_dtype(self):
+        self.dtype = np.float16
+        self.index_dtype = np.int64
+
+    def init_multi_precision(self):
+        self.multi_precision = True
+
+    def init_use_nesterov(self):
+        self.use_nesterov = False
