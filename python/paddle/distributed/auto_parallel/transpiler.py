67abec3abb2dab1b5d25cf3acd1786257b72ccbf
JZ-LIANG <jianzhongliang10@gmail.com>
2021-08-24 16:37:01 +0800

    add transpiler forward

--- python/paddle/distributed/auto_parallel/transpiler.py
+++ python/paddle/distributed/auto_parallel/transpiler.py
@@ -222,3 +222,286 @@ class AutoParallelTranspiler(object):
                                                 dist_startup_program)
 
         return optimize_ops
+
+    def transpile_forward_impl(self, main_program, startup_program):
+
+        if not isinstance(main_program, (Program)):
+            raise TypeError(
+                "dist_strategy be paddle.fluid.framework.program, got %s here" %
+                type(main_program))
+
+        if not isinstance(startup_program, (Program)):
+            raise TypeError(
+                "auto_parallel_context be paddle.fluid.framework.program, got %s here"
+                % type(startup_program))
+
+        # check if shard annotated serial program valid
+        if not self._is_valid_annotated_program(main_program):
+            raise RuntimeError(
+                "Not all vars or ops are annotated in main program !")
+
+        # determine parallelism mode
+        self._determine_parallel_mode(main_program)
+
+        # dist op & partition vars
+        new_main_prog, new_startup_program = self._dist_var_op_forward_transpile(
+            main_program, startup_program)
+
+        # AMP
+        if self._dist_strategy.amp:
+            new_main_prog, new_startup_program = self._amp_forward_transpile(
+                new_main_prog, new_startup_program)
+
+        # Recompute
+        if self._dist_strategy.recompute:
+            new_main_prog, new_startup_program = self._recompute_forward_transpile(
+                new_main_prog, new_startup_program)
+
+        # Pipeline
+        # TODO in auto parallel scenario, pipeline enable or not will be determined by distributed attribute 
+        # of corresponding activation vars, therefore the triggle condition should be modification here in future.
+        if self._dist_strategy.pipeline:
+            new_main_prog, new_startup_program = self._pipeline_forward_transpile(
+                new_main_prog, new_startup_program)
+
+        # Sharding
+        if self._dist_strategy.sharding:
+            new_main_prog, new_startup_program = self._sharding_forward_transpile(
+                new_main_prog, new_startup_program)
+
+        return new_main_prog, new_startup_program
+
+    def _dist_var_op_forward_transpile(self,
+                                       serial_main_program,
+                                       serial_startup_program=None):
+        """
+        1. partition variables
+        2. replace local op with corresponding dist op
+        """
+
+        partitioned_main_prog = fluid.Program()
+        partitioned_global_block = partitioned_main_prog.global_block()
+        serial_global_block = serial_main_program.global_block()
+        serial_ops = serial_main_program.global_block().ops
+
+        # transpile main program
+        for op in serial_ops:
+
+            # partititon input variables
+            for serial_input_varname in op.desc.input_arg_names():
+                if serial_input_varname not in self._serial2dist_varname_mapping:
+                    new_varname = serial_input_varname + self._dist_varname_suffix
+                    if serial_global_block.has_var(serial_input_varname):
+                        _partition_var(self._auto_parallel_context,
+                                       serial_global_block,
+                                       partitioned_global_block,
+                                       serial_input_varname, new_varname)
+                    else:
+                        assert serial_input_varname in __varname_not_in_block__
+
+                    self._serial2dist_varname_mapping[
+                        serial_input_varname] = new_varname
+
+            # partition output vars
+            for serial_output_varname in op.desc.output_arg_names():
+                if serial_output_varname not in self._serial2dist_varname_mapping:
+                    new_varname = serial_output_varname + self._dist_varname_suffix
+                    _partition_var(self._auto_parallel_context,
+                                   serial_global_block,
+                                   partitioned_global_block,
+                                   serial_output_varname, new_varname)
+                    self._serial2dist_varname_mapping[
+                        serial_output_varname] = new_varname
+
+            # partition op
+            if _found_match_dist_op(self._auto_parallel_context, op):
+                # replace with corresponding dist op
+                _insert_dist_op(op, partitioned_global_block,
+                                self._serial2dist_varname_mapping,
+                                self._auto_parallel_context, self._rank_id)
+            else:
+                # replicate op
+                _insert_src_op(op, partitioned_global_block,
+                               self._serial2dist_varname_mapping)
+
+        # transpile startup program
+        if serial_startup_program == None:
+            partitioned_startup_prog = None
+        else:
+            partitioned_startup_prog = fluid.Program()
+            # create parameter
+            partitioned_startup_global_block = partitioned_startup_prog.global_block(
+            )
+            param2shape = {}
+            for var in partitioned_main_prog.list_vars():
+                if isinstance(var, Parameter):
+                    _partition_parameter(self._auto_parallel_context, var,
+                                         partitioned_startup_global_block,
+                                         var.name, var.shape)
+                    param2shape[var.name] = var.shape
+
+            # copy initializer
+            for op in serial_startup_program.global_block().ops:
+                output_vars = op.desc.output_arg_names()
+                assert len(
+                    output_vars
+                ) == 1, "initializer should output only ONE variable, but got [{}]".format(
+                    str(op.desc))
+                assert self._serial2dist_varname_mapping[output_vars[
+                    0]] in param2shape, "try to initialize [{}] which is not a Parameter".format(
+                        output_vars[0])
+                new_op_desc = partitioned_startup_global_block.desc.append_op()
+                new_op_desc.copy_from(op.desc)
+                new_op_desc._rename_output(
+                    output_vars[0],
+                    self._serial2dist_varname_mapping[output_vars[0]])
+                new_op_desc._set_attr("shape", param2shape[
+                    self._serial2dist_varname_mapping[output_vars[0]]])
+                partitioned_startup_global_block._sync_with_cpp()
+
+            # MP broadcast not split parameter
+            # NOTE Theoretically, the MP param init broadcast should be handled by
+            # each dist op itself. but if we insert the broadcast op at that moment, the broadcast
+            # will before the initializer, which lead to a undertermined case.
+            if self._enable_tensor_parallel:
+                param_to_sync = []
+                for param in partitioned_startup_prog.all_parameters():
+                    if not self._is_var_distributed(param):
+                        param_to_sync.append(param)
+                        # FIXME the ring id should be set by autoparallel.mapping module
+                        # it should be determined by dp groups butfixed it here for hacking
+                        partitioned_startup_global_block.append_op(
+                            type='c_broadcast',
+                            inputs={'X': param},
+                            outputs={'Out': param},
+                            attrs={
+                                'ring_id': self._tp_group.id,
+                                'root': 0,
+                                'use_calc_stream': True,
+                                OP_ROLE_KEY: OpRole.Forward
+                            })
+                partitioned_startup_global_block.append_op(
+                    type='c_sync_comm_stream',
+                    inputs={'X': param_to_sync},
+                    outputs={'Out': param_to_sync},
+                    attrs={
+                        'ring_id': self._tp_group.id,
+                        OP_ROLE_KEY: OpRole.Forward
+                    })
+                partitioned_startup_global_block._sync_with_cpp()
+
+            # DP init param broadcast
+            if self._enable_data_parallel:
+                # parameters initialization synchronization 
+                param_to_sync = []
+
+                for param in partitioned_startup_global_block.all_parameters():
+                    param_to_sync.append(param)
+
+                    # FIXME the ring id should be set by autoparallel.mapping module
+                    # it should be determined by dp groups butfixed it here for hacking
+                    partitioned_startup_global_block.append_op(
+                        type='c_broadcast',
+                        inputs={'X': param},
+                        outputs={'Out': param},
+                        attrs={
+                            'ring_id': self._dp_group.id,
+                            'root': 0,
+                            'use_calc_stream': True,
+                            OP_ROLE_KEY: OpRole.Forward
+                        })
+                partitioned_startup_global_block.append_op(
+                    type='c_sync_comm_stream',
+                    inputs={'X': param_to_sync},
+                    outputs={'Out': param_to_sync},
+                    attrs={
+                        'ring_id': self._dp_group.id,
+                        OP_ROLE_KEY: OpRole.Forward
+                    })
+                partitioned_startup_global_block._sync_with_cpp()
+
+        return partitioned_main_prog, partitioned_startup_prog
+
+    def _amp_forward_transpile(self, main_prog, startup_program):
+        """
+        this transpile conduct the modification in forward program need by amp strategy
+        which majorly include:
+            1. insert cast op for parameter
+
+        NOTE the transpile modification is inplace on the input program
+        """
+
+        raise NotImplementedError("Amp is NOT support in AutoParallel yet!")
+
+    def _amp_backward_transpile(self, main_prog, startup_program):
+        """
+        this transpile conduct the modification in backward program need by amp strategy
+        which majorly include:
+            1. conduct loss scaling
+
+        NOTE the transpile modification is inplace on the input program
+        """
+
+        raise NotImplementedError("Amp is NOT support in AutoParallel yet!")
+
+    def _recompute_forward_transpile(self, main_prog, startup_program):
+        """
+        this transpile conduct the modification in forward program need by recompute strategy
+        which majorly include:
+            1. insert seed op for dropout
+            2. modify dropout 
+
+        NOTE the transpile modification is inplace on the input program
+        """
+
+        raise NotImplementedError(
+            "Recompute is NOT support in AutoParallel yet!")
+
+    def _pipeline_forward_transpile(self, main_prog, startup_program):
+        """
+        this transpile conduct the modification in forward program need by pipeline strategy
+        which majorly include:
+            1. insert send & recv op for activation
+            2. insert sync op 
+
+        NOTE the transpile modification is inplace on the input program
+        """
+
+        raise NotImplementedError(
+            "Pipeline is NOT support in AutoParallel yet!")
+
+    def _sharding_forward_transpile(self, main_prog, startup_program):
+        """
+        this transpile conduct the modification in forward program need by sharding strategy
+        which majorly include:
+            1. partition the parameter
+            2. insert broadcast op
+            3. insert sync op 
+
+        NOTE the transpile modification is inplace on the input program
+        """
+
+        raise NotImplementedError(
+            "Sharding is NOT support in AutoParallel yet!")
+
+    def _sharding_backward_transpile(self, main_prog, startup_program):
+        """
+        this transpile conduct the modification in backward program need by sharding strategy
+        which majorly include:
+            1. partition the gradient
+            2. insert broadcast op
+            3. insert sync op 
+
+        NOTE the transpile modification is inplace on the input program
+        """
+
+        raise NotImplementedError(
+            "Sharding is NOT support in AutoParallel yet!")
+
+    def _sharding_optimize_transpile(self, params_grads, dist_main_program,
+                                     dist_startup_program):
+        """
+        shard params_grads
+        append the broadcast to sync parameters 
+        """
+        raise RuntimeError("sharding transpile is NOT implemented !")
