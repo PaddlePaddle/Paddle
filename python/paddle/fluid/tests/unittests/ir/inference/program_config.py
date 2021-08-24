0fb9d9a0aebb7be512461c72665e4f76b48de1e4
xiaoxiaohehe001 <hiteezsf@163.com>
2021-08-24 12:32:33 +0000

    add_split_teller

--- python/paddle/fluid/tests/unittests/ir/inference/program_config.py
+++ python/paddle/fluid/tests/unittests/ir/inference/program_config.py
@@ -137,10 +137,8 @@ def create_fake_model(program_config):
             op_desc._set_attr(name, values)
         for name, values in op_config.outputs.items():
             op_desc.set_output(name, values)
-            for v in values:
-                var_desc = main_block_desc.var(cpt.to_bytes(v))
-                var_desc.set_type(core.VarDesc.VarType.LOD_TENSOR)
-                var_desc.set_dtype(core.VarDesc.VarType.FP32)
+            var_desc = main_block_desc.var(cpt.to_bytes(name))
+            var_desc.set_type(core.VarDesc.VarType.LOD_TENSOR)
         op_desc.infer_var_type(main_block_desc)
         op_desc.infer_shape(main_block_desc)
 
