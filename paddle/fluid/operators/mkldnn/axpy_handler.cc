4a8c477b62ba63be47cbe22bb82c3d5cc7159599
Adam Osewski <adam.osewski@intel.com>
2021-08-26 16:34:17 +0200

    Create stateful OneDNNAXPYHandler object.
    This makes it possible to call it multiple times without recreating the
    oneDNN primitives every time.

--- paddle/fluid/operators/mkldnn/axpy_handler.cc
+++ paddle/fluid/operators/mkldnn/axpy_handler.cc
@@ -34,11 +34,11 @@ namespace plat = paddle::platform;
 namespace {
 
 template <typename T>
-class AXPYMKLDNNHandler : public plat::MKLDNNHandlerT<T, dnnl::reorder> {
+class AXPYHandler : public plat::MKLDNNHandlerT<T, dnnl::reorder> {
  public:
-  AXPYMKLDNNHandler(const plat::MKLDNNDeviceContext &dev_ctx,
-                    const dnnl::engine mkldnn_engine, plat::Place cpu_place,
-                    int n, float alpha)
+  AXPYHandler(const plat::MKLDNNDeviceContext &dev_ctx,
+              const dnnl::engine mkldnn_engine, plat::Place cpu_place, int n,
+              float alpha)
       : plat::MKLDNNHandlerT<T, dnnl::reorder>(
             dev_ctx, mkldnn_engine, cpu_place,
             plat::CreateKey(dev_ctx, static_cast<int64_t>(n),
@@ -47,7 +47,7 @@ class AXPYMKLDNNHandler : public plat::MKLDNNHandlerT<T, dnnl::reorder> {
         n_(n) {}
 
   std::shared_ptr<dnnl::memory> AcquireMemory(void *ptr,
-                                              const std::string &suffix) {
+                                              const std::string &suffix) const {
     /*Generate key*/
     auto local_key = this->key_ + suffix;
     auto mem_p = std::static_pointer_cast<dnnl::memory>(
@@ -63,17 +63,17 @@ class AXPYMKLDNNHandler : public plat::MKLDNNHandlerT<T, dnnl::reorder> {
     return mem_p;
   }
 
-  std::shared_ptr<dnnl::memory> AcquireSrcMemory(const T *x) {
+  std::shared_ptr<dnnl::memory> AcquireSrcMemory(const T *x) const {
     return this->AcquireMemory(plat::to_void_cast(x), "@user_src_mem_p");
   }
 
-  std::shared_ptr<dnnl::memory> AcquireDstMemory(T *y) {
+  std::shared_ptr<dnnl::memory> AcquireDstMemory(T *y) const {
     return this->AcquireMemory(y, "@user_dst_mem_p");
   }
 
   std::shared_ptr<dnnl::reorder> AcquireReorder(
       std::shared_ptr<dnnl::memory> dst_memory_p,
-      std::shared_ptr<dnnl::memory> src_memory_p) {
+      std::shared_ptr<dnnl::memory> src_memory_p) const {
     auto prim_key = this->key_ + "@reorder_p";
     auto reorder_p = std::static_pointer_cast<dnnl::reorder>(
         this->dev_ctx_.GetBlob(prim_key));
@@ -95,15 +95,12 @@ class AXPYMKLDNNHandler : public plat::MKLDNNHandlerT<T, dnnl::reorder> {
     return reorder_p;
   }
 
- private:
   float alpha_;
   int n_;
 };
 
-template class AXPYMKLDNNHandler<float>;
-template class AXPYMKLDNNHandler<plat::bfloat16>;
-
-}  // anonnymouse namespace
+template class AXPYHandler<float>;
+template class AXPYHandler<plat::bfloat16>;
 
 template <typename T>
 static void naive_axpy(int n, T alpha, const T *x, T *y) {
@@ -114,27 +111,42 @@ static void naive_axpy(int n, T alpha, const T *x, T *y) {
   }
 }
 
-template <typename T>
-void onednn_handler_axpy(int n, T alpha, const T *x, T *y) {
-  // fallback to naive version
-  if (n < 100) {
-    naive_axpy(n, alpha, x, y);
-    return;
-  }
+}  // anonnymouse namespace
 
+template <typename T>
+class OneDNNAXPYHandler<T>::Impl {
+ public:
+  Impl(int n, T alpha);
+  void operator()(const T *x, T *y);
+
+ private:
+  std::unique_ptr<AXPYHandler<T>> handler_;
+  int n_;
+  T alpha_;
+};
+
+template <typename T>
+OneDNNAXPYHandler<T>::Impl::Impl(int n, T alpha) : n_{n}, alpha_{alpha} {
   auto &pool = plat::DeviceContextPool::Instance();
   auto cpu_place = plat::CPUPlace();
   auto *dev_ctx =
       dynamic_cast<plat::MKLDNNDeviceContext *>(pool.Get(cpu_place));
   auto &cpu_engine = dev_ctx->GetEngine();
+  handler_ = std::make_unique<AXPYHandler<T>>(*dev_ctx, cpu_engine, cpu_place,
+                                              n, static_cast<float>(alpha));
+}
 
-  AXPYMKLDNNHandler<T> handler(*dev_ctx, cpu_engine, cpu_place, n,
-                               static_cast<float>(alpha));
+template <typename T>
+void OneDNNAXPYHandler<T>::Impl::operator()(const T *x, T *y) {
+  if (this->n_ < 100) {
+    naive_axpy(this->n_, this->alpha_, x, y);
+    return;
+  }
 
-  auto reorder_src_memory_p = handler.AcquireSrcMemory(x);
-  auto reorder_dst_memory_p = handler.AcquireDstMemory(y);
+  auto reorder_src_memory_p = handler_->AcquireSrcMemory(x);
+  auto reorder_dst_memory_p = handler_->AcquireDstMemory(y);
   auto reorder_p =
-      handler.AcquireReorder(reorder_dst_memory_p, reorder_src_memory_p);
+      handler_->AcquireReorder(reorder_dst_memory_p, reorder_src_memory_p);
 
   auto &astream = plat::MKLDNNDeviceContext::tls().get_stream();
   plat::RecordEvent record_reorder("axpy_int_reorder",
@@ -143,10 +155,17 @@ void onednn_handler_axpy(int n, T alpha, const T *x, T *y) {
   astream.wait();
 }
 
-template void onednn_handler_axpy<float>(int, float, const float *, float *);
-template void onednn_handler_axpy<plat::bfloat16>(int, plat::bfloat16,
-                                                  const plat::bfloat16 *,
-                                                  plat::bfloat16 *);
+template <typename T>
+OneDNNAXPYHandler<T>::OneDNNAXPYHandler(int n, T alpha)
+    : pimpl_{new Impl{n, alpha}, [](Impl *impl) { delete impl; }} {}
+
+template <typename T>
+void OneDNNAXPYHandler<T>::operator()(const T *x, T *y) {
+  pimpl_->operator()(x, y);
+}
+
+template class OneDNNAXPYHandler<float>;
+template class OneDNNAXPYHandler<plat::bfloat16>;
 
 }  // namespace operators
 }  // namespace paddle
