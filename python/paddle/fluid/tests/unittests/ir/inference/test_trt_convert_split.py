f83cd0b92c5e01eeb83202b92db2e1c1007c0337
xiaoxiaohehe001 <hiteezsf@163.com>
2021-08-27 11:21:16 +0000

    add_split_teller

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_split.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_split.py
@@ -19,7 +19,7 @@ import numpy as np
 import paddle.inference as paddle_infer
 
 
-class TrtConvertSplitTest(TrtLayerAutoScanTest):
+class TrtConvertSplitDims2Test(TrtLayerAutoScanTest):
     def setUp(self):
         self.set_params()
         self.ops_config = [{
@@ -46,7 +46,7 @@ class TrtConvertSplitTest(TrtLayerAutoScanTest):
         self.paddle = 3
 
     def update_program_input_and_weight_with_attr(self, op_attr_list):
-        split_input = TensorConfig(shape=[4, 3, 64, 64])
+        split_input = TensorConfig(shape=[1, 3, 64, 64])
         self.program_inputs = {"split_input": split_input}
         self.program_outputs = ["output_var0", "output_var1"]
         self.program_weights = {}
@@ -79,7 +79,7 @@ class TrtConvertSplitTest(TrtLayerAutoScanTest):
             trt_engine_num=self.trt, paddle_op_num=self.paddle, threshold=1e-2)
 
 
-class TrtConvertSplitAxisTest(TrtConvertSplitTest):
+class TrtConvertSplitDims2AxisTest(TrtConvertSplitDims2Test):
     def set_params(self):
         self.sections = [[10, 54]]
         self.num = [0]
@@ -88,13 +88,73 @@ class TrtConvertSplitAxisTest(TrtConvertSplitTest):
         self.paddle = 3
 
 
-class TrtConvertSplitNumTest(TrtConvertSplitTest):
+class TrtConvertSplitDims2NumTest(TrtConvertSplitDims2Test):
     def set_params(self):
         self.sections = [[]]
         self.num = [2]
         self.axis = [2]
         self.trt = 1
         self.paddle = 3
+
+
+class TrtConvertSplitDims3Test(TrtLayerAutoScanTest):
+    def setUp(self):
+        self.set_params()
+        self.ops_config = [{
+            "op_type": "split",
+            "op_inputs": {
+                "X": ["split_input"]
+            },
+            "op_outputs": {
+                "Out": ["output_var0", "output_var1", "output_var2"]
+            },
+            "op_attrs": {
+                "sections": self.sections,
+                "num": self.num,
+                "axis": self.axis
+            }
+        }]
+        self.batch_size_set = [1, 2, 4]
+
+    def set_params(self):
+        self.sections = [[3, 7, 54]]
+        self.num = [0]
+        self.axis = [2]
+        self.trt = 1
+        self.paddle = 4
+
+    def update_program_input_and_weight_with_attr(self, op_attr_list):
+        split_input = TensorConfig(shape=[1, 3, 64, 64])
+        self.program_inputs = {"split_input": split_input}
+        self.program_outputs = ["output_var0", "output_var1", "output_var2"]
+        self.program_weights = {}
+
+    def test_check_fp32_output(self):
+        self.trt_param.precision == paddle_infer.PrecisionType.Float32
+        # the fused tensorrt engine num is 1, and paddle op num is 2(feed and fetch).
+        self.run_test(
+            trt_engine_num=self.trt, paddle_op_num=self.paddle, threshold=1e-5)
+
+    def test_check_fp16_output(self):
+        self.trt_param.precision == paddle_infer.PrecisionType.Half
+        self.run_test(
+            trt_engine_num=self.trt, paddle_op_num=self.paddle, threshold=1e-2)
+
+    def test_dynamic_shape_fp32_check_output(self):
+        self.trt_param.precision = paddle_infer.PrecisionType.Float32
+        self.dynamic_shape.min_input_shape = {"split_input": [1, 3, 64, 32]}
+        self.dynamic_shape.max_input_shape = {"split_input": [4, 3, 64, 64]}
+        self.dynamic_shape.opt_input_shape = {"split_input": [1, 3, 64, 64]}
+        self.run_test(
+            trt_engine_num=self.trt, paddle_op_num=self.paddle, threshold=1e-5)
+
+    def test_dynamic_shape_fp16_check_output(self):
+        self.trt_param.precision = paddle_infer.PrecisionType.Half
+        self.dynamic_shape.min_input_shape = {"split_input": [1, 3, 64, 32]}
+        self.dynamic_shape.max_input_shape = {"split_input": [4, 3, 64, 64]}
+        self.dynamic_shape.opt_input_shape = {"split_input": [1, 3, 64, 64]}
+        self.run_test(
+            trt_engine_num=self.trt, paddle_op_num=self.paddle, threshold=1e-2)
 
 
 if __name__ == "__main__":
