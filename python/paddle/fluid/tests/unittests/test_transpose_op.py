b29143d9ff18ee80c4864ff0d13955cc2e4d4869
limin2021 <1121099234@qq.com>
2021-08-31 08:27:30 +0000

    Fix test_transpose_mkldnn_op bug about import.

--- python/paddle/fluid/tests/unittests/test_transpose_op.py
+++ python/paddle/fluid/tests/unittests/test_transpose_op.py
@@ -16,7 +16,7 @@ from __future__ import print_function
 
 import unittest
 import numpy as np
-from op_test import OpTest
+from paddle.fluid.tests.unittests.op_test import OpTest
 import paddle
 import paddle.fluid as fluid
 from paddle.fluid import Program, program_guard
