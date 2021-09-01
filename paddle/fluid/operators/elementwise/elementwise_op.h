7f0da55cee52dff04a73583a45aed4ee1e0a3b3f
wangxinxin08 <wangxinxin08@baidu.com>
2021-09-01 03:33:35 +0000

    remove wrong check

--- paddle/fluid/operators/elementwise/elementwise_op.h
+++ paddle/fluid/operators/elementwise/elementwise_op.h
@@ -86,15 +86,6 @@ class ElementwiseOp : public framework::OperatorWithKernel {
       int max_dim = std::max(x_dims.size(), y_dims.size());
       int axis = ctx->Attrs().Get<int>("axis");
       if (x_dims.size() == y_dims.size()) {
-        for (int i = 0; i < x_dims.size(); i++) {
-          PADDLE_ENFORCE_EQ(
-              x_dims[i], y_dims[i],
-              platform::errors::InvalidArgument(
-                  "the size of tensor X (%s) must be equal to the size of "
-                  "tensor Y (%s) at axis %s while the dimension of tensor X is "
-                  "equal to the dimension of tensor Y",
-                  x_dims[i], y_dims[i], i));
-        }
         PADDLE_ENFORCE_EQ((axis == -1) || (axis == 0), true,
                           platform::errors::InvalidArgument(
                               "axis should be -1 or 0 while the dimension of "
