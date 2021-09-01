e6ea5c3acc03e542a9a464fd800850583a2ee0d2
huangjun12 <12272008@bjtu.edu.cn>
2021-09-01 03:38:05 +0000

    add as_extra to dropout op and lrn op

--- paddle/fluid/operators/dropout_op.cc
+++ paddle/fluid/operators/dropout_op.cc
@@ -65,15 +65,17 @@ class DropoutOpMaker : public framework::OpProtoAndCheckerMaker {
     AddAttr<bool>("is_test",
                   "(bool, default false) Set to true for inference only, false "
                   "for training. Some layers may run faster when this is true.")
-        .SetDefault(false);
+        .SetDefault(false)
+        .AsExtra();
     AddAttr<bool>("fix_seed",
                   "A flag indicating whether to use a fixed seed to generate "
                   "random mask. NOTE: DO NOT set this flag to true in "
                   "training. Setting this flag to true is only useful in "
                   "unittest or for debug that always the same output units "
                   "will be dropped.")
-        .SetDefault(false);
-    AddAttr<int>("seed", "Dropout random seed.").SetDefault(0);
+        .SetDefault(false)
+        .AsExtra();
+    AddAttr<int>("seed", "Dropout random seed.").SetDefault(0).AsExtra();
     AddAttr<std::string>(
         "dropout_implementation",
         "[\"downgrade_in_infer\"|\"upscale_in_train\"]"
