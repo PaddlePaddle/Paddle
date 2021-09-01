fcd344830a6e178537900cb33b18e7f31f66dcab
wjj19950828 <wjjisloser@163.com>
2021-09-01 20:38:45 +0800

    deal with comments

--- paddle/fluid/operators/elementwise/elementwise_pow_op_npu.cc
+++ paddle/fluid/operators/elementwise/elementwise_pow_op_npu.cc
@@ -95,117 +95,7 @@ class ElementwisePowGradNPUKernel : public framework::OpKernel<T> {
     auto stream = dev_ctx.stream();
     // Reshape info vector.
     std::vector<int> reduce_axes;
-
-    if (dx && dy) {
-      dx->mutable_data<T>(place);
-      dy->mutable_data<T>(place);
-      Tensor tmp_dx;
-      tmp_dx.mutable_data<T>(dout_dims, place);
-      Tensor tmp_dy;
-      tmp_dy.mutable_data<T>(dout_dims, place);
-
-      // dx = dout * y * pow(x, y - 1);
-      Tensor PowGrad_dx_temp1(dout->type());
-      PowGrad_dx_temp1.mutable_data<T>(dout->dims(), place);
-      const auto& runner_PowGrad_dx_temp1 =
-          NpuOpRunner("Mul", {*dout, transformed_y}, {PowGrad_dx_temp1}, {});
-      runner_PowGrad_dx_temp1.Run(stream);
-
-      Tensor one_dx(transformed_y.type());
-      one_dx.mutable_data<T>(transformed_y.dims(), place);
-      const auto& runner_one_dx =
-          NpuOpRunner("OnesLike", {transformed_y}, {one_dx}, {});
-      runner_one_dx.Run(stream);
-
-      Tensor sub_dx(transformed_y.type());
-      sub_dx.mutable_data<T>(transformed_y.dims(), place);
-      const auto& runner_sub_dx =
-          NpuOpRunner("Sub", {transformed_y, one_dx}, {sub_dx}, {});
-      runner_sub_dx.Run(stream);
-
-      Tensor PowGrad_dx_temp2(transformed_x.type());
-      PowGrad_dx_temp2.mutable_data<T>(transformed_x.dims(), place);
-      const auto& runner_PowGrad_dx_temp2 =
-          NpuOpRunner("Pow", {transformed_x, sub_dx}, {PowGrad_dx_temp2}, {});
-      runner_PowGrad_dx_temp2.Run(stream);
-
-      const auto& runner_dx = NpuOpRunner(
-          "Mul", {PowGrad_dx_temp1, PowGrad_dx_temp2}, {tmp_dx}, {});
-      runner_dx.Run(stream);
-
-      // dy = dout * log(x) * pow(x, y)
-      Tensor PowGrad_dy_temp1(transformed_x.type());
-      PowGrad_dy_temp1.mutable_data<T>(transformed_x.dims(), place);
-      const auto& runner_PowGrad_dy_temp1 = NpuOpRunner(
-          "Pow", {transformed_x, transformed_y}, {PowGrad_dy_temp1}, {});
-      runner_PowGrad_dy_temp1.Run(stream);
-
-      Tensor one_dy(transformed_x.type());
-      one_dy.mutable_data<T>(transformed_x.dims(), place);
-      const auto& runner_one_dy =
-          NpuOpRunner("OnesLike", {transformed_x}, {one_dy}, {});
-      runner_one_dy.Run(stream);
-
-      Tensor sub_dy(transformed_x.type());
-      sub_dy.mutable_data<T>(transformed_x.dims(), place);
-      const auto& runner_sub_dy =
-          NpuOpRunner("Sub", {transformed_x, one_dy}, {sub_dy}, {});
-      runner_sub_dy.Run(stream);
-
-      Tensor log_dy(transformed_x.type());
-      log_dy.mutable_data<T>(transformed_x.dims(), place);
-      const auto& runner_log_dy = NpuOpRunner("Log1p", {sub_dy}, {log_dy}, {});
-      runner_log_dy.Run(stream);
-
-      Tensor PowGrad_dy_temp2(transformed_x.type());
-      PowGrad_dy_temp2.mutable_data<T>(transformed_x.dims(), place);
-      const auto& runner_PowGrad_dy_temp2 = NpuOpRunner(
-          "Mul", {log_dy, PowGrad_dy_temp1}, {PowGrad_dy_temp2}, {});
-      runner_PowGrad_dy_temp2.Run(stream);
-
-      const auto& runner_dy =
-          NpuOpRunner("Mul", {*dout, PowGrad_dy_temp2}, {tmp_dy}, {});
-      runner_dy.Run(stream);
-
-      if (x_dims != dout_dims) {
-        reduce_axes.clear();
-        int src_axis = (x_dims.size() < dout_dims.size() ? axis : 0);
-        for (int ax = 0; ax < dout_dims.size(); ++ax) {
-          if ((ax < src_axis || ax >= src_axis + x_dims.size()) ||
-              (dout_dims[ax] > 1 && x_dims[ax - src_axis] == 1)) {
-            reduce_axes.push_back(ax);
-          }
-        }
-        if (!reduce_axes.empty()) {
-          const auto& runner =
-              NpuOpRunner("ReduceSumD", {tmp_dx}, {*dx},
-                          {{"axes", reduce_axes}, {"keep_dims", false}});
-          runner.Run(stream);
-        }
-      } else {
-        framework::TensorCopy(tmp_dx, place, dev_ctx, dx);
-      }
-
-      if (y_dims != dout_dims) {
-        reduce_axes.clear();
-        int src_axis = (y_dims.size() < dout_dims.size() ? axis : 0);
-        for (int ax = 0; ax < dout_dims.size(); ++ax) {
-          if ((ax < src_axis || ax >= src_axis + y_dims.size()) ||
-              (dout_dims[ax] > 1 && y_dims[ax - src_axis] == 1)) {
-            reduce_axes.push_back(ax);
-          }
-        }
-        if (!reduce_axes.empty()) {
-          const auto& runner =
-              NpuOpRunner("ReduceSumD", {tmp_dy}, {*dy},
-                          {{"axes", reduce_axes}, {"keep_dims", false}});
-          runner.Run(stream);
-        }
-      } else {
-        framework::TensorCopy(tmp_dy, place, dev_ctx, dy);
-      }
-
-    } else if (dx) {
+    if (dx) {
       Tensor zero_tensor(dout->type());
       zero_tensor.mutable_data<T>(dout_dims, place);
       FillNpuTensorWithConstant<T>(&zero_tensor, static_cast<T>(0));
@@ -262,8 +152,8 @@ class ElementwisePowGradNPUKernel : public framework::OpKernel<T> {
       } else {
         framework::TensorCopy(tmp_dx, place, dev_ctx, dx);
       }
-
-    } else if (dy) {
+    }
+    if (dy) {
       Tensor zero_tensor(dout->type());
       zero_tensor.mutable_data<T>(dout_dims, place);
       FillNpuTensorWithConstant<T>(&zero_tensor, static_cast<T>(0));
@@ -325,7 +215,8 @@ class ElementwisePowGradNPUKernel : public framework::OpKernel<T> {
       } else {
         framework::TensorCopy(tmp_dy, place, dev_ctx, dy);
       }
-    } else {
+    }
+    if (!dx && !dy) {
       PADDLE_THROW(platform::errors::Unavailable(
           "Not support all outputs to be empty."));
     }
