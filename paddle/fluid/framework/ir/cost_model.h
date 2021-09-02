dbae0d96bc145da78836d1bd54c98a7c09a26511
huangxu96 <huangxu_app@163.com>
2021-09-02 03:47:58 +0000

    add class CostData

--- paddle/fluid/framework/ir/cost_model.h
+++ paddle/fluid/framework/ir/cost_model.h
@@ -33,13 +33,34 @@ namespace framework {
 
 class Graph;
 
+class CostData {
+  public:
+    std::map<int, int64> GetTimeMap();
+    std::map<int, int64> GetMemMap();
+    int64 GetWholeTime();
+    int64 GetWholeMem();
+
+  private:
+    Graph* graph_;
+    Program* program_;
+    std::map<int, int64> time_; // from Op Node id to time
+    std::map<int, int64> memory_; // from Op Node id to total memory bytes
+    std::map<int, int64> comm_; // from Op Node id to communicate cost
+    int64 whole_time_; // time cost of the whole program or graph
+    int64 whole_memory; // memory cost of the whole program or graph
+    int64 whole_comm_;// communication cost of the whole program or graph
+    const static int NOT_MEASURED = -1;
+
+};
 class CostModel {
  public:
+  CostData profile_measure(Program* program, std::string device, fetch_cost_list);
+
  private:
   // Number of times Node has been executed
   std::vector<int> count_;
   // Total execution time
-  std::vector<Microseconds>
+  std::vector<int>
   // Total Bytes output on each channel
 }
 
