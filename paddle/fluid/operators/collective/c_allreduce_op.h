651b7d73265153f60be199f0afdd06733103f12d
gongweibao <weibao.gong@gmail.com>
2021-09-04 20:44:28 +0800

    cleanup test=develop

--- paddle/fluid/operators/collective/c_allreduce_op.h
+++ paddle/fluid/operators/collective/c_allreduce_op.h
@@ -144,7 +144,8 @@ inline bool ContainsNan(const paddle::platform::NPUDeviceContext& dev_ctx,
   try {
     const auto& runner_mean = paddle::operators::NpuOpRunner(
         "ReduceMeanD", {*in}, {mean}, {{"axes", axes}, {"keep_dims", false}});
-    runner_mean.Run(stream);
+    // FIXME(gongwb): not need to open this.
+    // runner_mean.Run(stream);
     TensorToVector(mean, dev_ctx, &vec);
   } catch (...) {
     LOG(WARNING) << "ContainsNan catch exception";
@@ -240,7 +241,7 @@ class CAllReduceOpASCENDKernel : public framework::OpKernel<T> {
       case framework::proto::VarType::FP32: {
         if (FLAGS_hccl_check_nan) {
           VLOG(3) << "prepare to FoundNanInf";
-          found_nan = ContainsNan(*dev_ctx, dev_ctx->stream(), in);
+          ContainsNan(*dev_ctx, dev_ctx->stream(), in);
           VLOG(3) << "check_numerics:" << found_nan;
         }
         break;
