b772cd5ff02453e5471e772aa63f440e55034487
liutiexing <liutiexing@gmail.com>
2021-09-05 04:33:53 +0000

    add unittest

--- paddle/fluid/framework/new_executor/workqueue_test.cc
+++ paddle/fluid/framework/new_executor/workqueue_test.cc
@@ -86,6 +86,10 @@ TEST(WorkQueue, TestWorkQueueGroup) {
   using paddle::framework::WorkQueueOptions;
   using paddle::framework::WorkQueueGroup;
   using paddle::framework::CreateWorkQueueGroup;
+  std::atomic<bool> finished{false};
+  std::atomic<unsigned> counter{0};
+  constexpr unsigned kExternalLoopNum = 100;
+  constexpr unsigned kLoopNum = 1000000;
   // CreateMultiThreadedWorkQueue
   WorkQueueOptions sq_options;
   sq_options.num_threads = 1;
@@ -95,4 +99,24 @@ TEST(WorkQueue, TestWorkQueueGroup) {
   mq_options.track_task = true;
   auto queue_group = CreateWorkQueueGroup({sq_options, mq_options});
   // NumThreads
+  EXPECT_EQ(queue_group->QueueNumThreads(0), 1u);
+  EXPECT_EQ(queue_group->QueueNumThreads(1), 10u);
+  EXPECT_EQ(queue_group->QueueGroupNumThreads(), 11u);
+  // AddTask
+  EXPECT_EQ(counter.load(), 0u);
+  for (unsigned i = 0; i < kExternalLoopNum; ++i) {
+    queue_group->AddTask(1, [&counter, &finished, kLoopNum]() {
+      for (unsigned i = 0; i < kLoopNum; ++i) {
+        ++counter;
+      }
+    });
+  }
+  queue_group->AddTask(0, [&counter, &finished, kLoopNum]() {
+    for (unsigned i = 0; i < kLoopNum; ++i) {
+      ++counter;
+    }
+  });
+  //  WaitQueueGroupEmpty()
+  queue_group->WaitQueueGroupEmpty();
+  EXPECT_EQ(counter.load(), kLoopNum * kExternalLoopNum + kLoopNum);
 }
