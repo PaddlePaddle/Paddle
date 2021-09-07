fd8a5546639787c0e35d9dd07da0f1fa503fee8a
xiaoxiaohehe001 <hiteezsf@163.com>
2021-09-07 06:34:27 +0000

    add_softmax_teller

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_softmax.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_softmax.py
@@ -12,124 +12,138 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from trt_layer_auto_scan_test import TrtLayerAutoScanTest
-from program_config import TensorConfig
+from trt_layer_auto_scan_test import TrtLayerAutoScanTest, SkipReasons
+from program_config import TensorConfig, ProgramConfig
 import numpy as np
 import paddle.inference as paddle_infer
-import unittest
+from functools import partial
+from typing import Optional, List, Callable, Dict, Any, Set
 
 
 class TrtConvertSoftmaxTest(TrtLayerAutoScanTest):
-    def setUp(self):
-        self.ops_config = [{
-            "op_type": "softmax",
-            "op_inputs": {
-                "X": ["softmax_channel_input_data"]
-            },
-            "op_outputs": {
-                "Out": ["softmax_channel_output_data"]
-            },
-            "op_attrs": {
-                "axis": [1, 2, 3]
-            }
-        }]
-        self.batch_size_set = [1, 2, 4]
+    def is_program_valid(self, program_config: ProgramConfig) -> bool:
+        # TODO: This is just the example to remove the wrong attrs.
+        inputs = program_config.inputs
+        weights = program_config.weights
+        outputs = program_config.outputs
 
-    def update_program_input_and_weight_with_attr(self, op_attr_list):
-        softmax_channel_input_data = TensorConfig(
-            shape=[1, 3, 64, 64], dtype="float32")
-        self.program_weights = {}
-        self.program_inputs = {
-            "softmax_channel_input_data": softmax_channel_input_data
-        }
-        self.program_outputs = ["softmax_channel_output_data"]
+        attrs = [
+            program_config.ops[i].attrs
+            for i in range(len(program_config.ops))
+        ]
+        if len(inputs['softmax_input'].shape) <= attrs[0]['axis']:
+            return False
 
-    def test_check_fp32_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Float32
-        # the fused tensorrt engine num is 1, and paddle op num is 2(feed and fetch).
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-5)
+        return True
 
-    def test_check_fp16_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Half
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-2)
+    def sample_program_configs(self):
+        def generate_input1(attrs: List[Dict[str, Any]], batch):
+            # TODO: This is just the example to illustrate the releation between axis and input.
+            # for each attr, can generate different datas
+            if self.dims == 4:
+                return np.ones([batch, 3, 24, 24]).astype(np.float32)
+            elif self.dims == 3:
+                return np.ones([batch, 3, 24]).astype(np.float32)
+            elif self.dims == 2:
+                return np.ones([batch, 32]).astype(np.float32)
 
-    def test_dynamic_shape_fp32_check_output(self):
+        for dims in [2, 3, 4]:
+            for batch in [1, 2, 4]:
+                for axis in [-1, 0, 1, 2, 3]:
+                    self.dims = dims
+                    dics = [{"axis": axis}, {}]
+                    ops_config = [{
+                        "op_type": "softmax",
+                        "op_inputs": {
+                            "X": ["softmax_input"]
+                        },
+                        "op_outputs": {
+                            "Out": ["softmax_out"]
+                        },
+                        "op_attrs": dics[0]
+                    }]
+                    ops = self.generate_op_config(ops_config)
+                    program_config = ProgramConfig(
+                        ops=ops,
+                        weights={},
+                        inputs={
+                            "softmax_input": TensorConfig(data_gen=partial(
+                                generate_input1, dics, batch))
+                        },
+                        outputs=["softmax_out"])
+
+                    yield program_config
+
+    def sample_predictor_configs(
+            self, program_config) -> (paddle_infer.Config, List[int], float):
+        def generate_dynamic_shape(attrs):
+            if self.dims == 4:
+                self.dynamic_shape.min_input_shape = {
+                    "softmax_input": [1, 3, 24, 24]
+                }
+                self.dynamic_shape.max_input_shape = {
+                    "softmax_input": [4, 3, 48, 48]
+                }
+                self.dynamic_shape.opt_input_shape = {
+                    "softmax_input": [1, 3, 24, 48]
+                }
+            elif self.dims == 3:
+                self.dynamic_shape.min_input_shape = {
+                    "softmax_input": [1, 3, 24]
+                }
+                self.dynamic_shape.max_input_shape = {
+                    "softmax_input": [4, 3, 48]
+                }
+                self.dynamic_shape.opt_input_shape = {
+                    "softmax_input": [1, 3, 48]
+                }
+            elif self.dims == 2:
+                self.dynamic_shape.min_input_shape = {"softmax_input": [1, 32]}
+                self.dynamic_shape.max_input_shape = {"softmax_input": [4, 64]}
+                self.dynamic_shape.opt_input_shape = {"softmax_input": [1, 32]}
+
+        def clear_dynamic_shape():
+            self.dynamic_shape.min_input_shape = {}
+            self.dynamic_shape.max_input_shape = {}
+            self.dynamic_shape.opt_input_shape = {}
+
+        def generate_trt_nodes_num(attrs, dynamic_shape):
+            # TODO: This is just the example, need to be fixed.
+            return 1, 2
+
+        attrs = [
+            program_config.ops[i].attrs
+            for i in range(len(program_config.ops))
+        ]
+
+        # for static_shape
+        clear_dynamic_shape()
+        if attrs[0]['axis'] == 0:
+            pass
+        else:
+            self.trt_param.precision = paddle_infer.PrecisionType.Float32
+            yield self.create_inference_config(), generate_trt_nodes_num(
+                attrs, False), 1e-5
+            self.trt_param.precision = paddle_infer.PrecisionType.Half
+            yield self.create_inference_config(), generate_trt_nodes_num(
+                attrs, False), 1e-2
+
+        # for dynamic_shape
+        generate_dynamic_shape(attrs)
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        self.dynamic_shape.min_input_shape = {
-            "softmax_channel_input_data": [1, 3, 32, 32]
-        }
-        self.dynamic_shape.max_input_shape = {
-            "softmax_channel_input_data": [4, 3, 64, 64]
-        }
-        self.dynamic_shape.opt_input_shape = {
-            "softmax_channel_input_data": [1, 3, 64, 64]
-        }
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-5)
-
-    def test_dynamic_shape_fp16_check_output(self):
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs,
+                                                                     True), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
-        self.dynamic_shape.min_input_shape = {
-            "softmax_channel_input_data": [1, 3, 32, 32]
-        }
-        self.dynamic_shape.max_input_shape = {
-            "softmax_channel_input_data": [4, 3, 64, 64]
-        }
-        self.dynamic_shape.opt_input_shape = {
-            "softmax_channel_input_data": [1, 3, 64, 64]
-        }
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-2)
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs,
+                                                                     True), 1e-2
 
+    def add_skip_trt_case(self):
+        # TODO(wilber): This is just the example to illustrate the skip usage.
+        pass
 
-class DynamicTrtConvertSoftmaxTest(TrtLayerAutoScanTest):
-    def setUp(self):
-        self.ops_config = [{
-            "op_type": "softmax",
-            "op_inputs": {
-                "X": ["softmax_channel_input_data"]
-            },
-            "op_outputs": {
-                "Out": ["softmax_channel_output_data"]
-            },
-            "op_attrs": {
-                "axis": [0]
-            }
-        }]
-        self.batch_size_set = [1, 2, 4]
-
-    def update_program_input_and_weight_with_attr(self, op_attr_list):
-        softmax_channel_input_data = TensorConfig(
-            shape=[1, 3, 64, 64], dtype="float32")
-        self.program_weights = {}
-        self.program_inputs = {
-            "softmax_channel_input_data": softmax_channel_input_data
-        }
-        self.program_outputs = ["softmax_channel_output_data"]
-
-    def test_dynamic_shape_fp32_check_output(self):
-        self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        self.dynamic_shape.min_input_shape = {
-            "softmax_channel_input_data": [1, 3, 32, 32]
-        }
-        self.dynamic_shape.max_input_shape = {
-            "softmax_channel_input_data": [4, 3, 64, 64]
-        }
-        self.dynamic_shape.opt_input_shape = {
-            "softmax_channel_input_data": [1, 3, 64, 64]
-        }
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-5)
-
-    def test_dynamic_shape_fp16_check_output(self):
-        self.trt_param.precision = paddle_infer.PrecisionType.Half
-        self.dynamic_shape.min_input_shape = {
-            "softmax_channel_input_data": [1, 3, 32, 32]
-        }
-        self.dynamic_shape.max_input_shape = {
-            "softmax_channel_input_data": [4, 3, 64, 64]
-        }
-        self.dynamic_shape.opt_input_shape = {
-            "softmax_channel_input_data": [1, 3, 64, 64]
-        }
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-2)
+    def test(self):
+        self.add_skip_trt_case()
+        self.run_test()
 
 
 if __name__ == "__main__":
