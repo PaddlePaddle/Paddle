6c35a857492eb3354a4cb076560c5e74ee610eac
Wang Huan <wanghuan29@baidu.com>
2021-09-07 09:05:45 +0000

    refine, test=develop

--- paddle/fluid/framework/new_executor/interpretercore.cc
+++ paddle/fluid/framework/new_executor/interpretercore.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define GLOG_NO_ABBREVIATED_SEVERITIES  // msvc conflict logging with windows.h
+
 #if !defined(_WIN32)
 #include <sched.h>
 #else
