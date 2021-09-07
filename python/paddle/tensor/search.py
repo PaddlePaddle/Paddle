0fcc53d75f73394677de98802ff212a072f9fba5
Yanxing-Shi <shiyanxing2008@qq.com>
2021-09-07 13:41:32 +0000

    modify doc after mentor review

--- python/paddle/tensor/search.py
+++ python/paddle/tensor/search.py
@@ -776,13 +776,12 @@ def searchsorted(sorted_sequence,
     This OP is used to find the index of the corresponding `sorted_sequence` in the innermost dimension based on the given `values`.
 
     Args:
-        sorted_sequence(Tensor): An input N-D or 1-D tensor with type int32, int64, float32, float64. The value of the tensor monotonically increases in the innermost dimension. 
-        values(Tensor or Scalar): An input N-D tensor or a Scalar value with type int32, int64, float32, float64.
+        sorted_sequence(Tensor): An input N-D tensor with type int32, int64, float32, float64. The value of the tensor monotonically increases in the innermost dimension. 
+        values(Tensor): An input N-D tensor value with type int32, int64, float32, float64.
         out_int32(bool, optional): Data type of the output tensor which can be int32, int64. The default value is False, and it indicates that the output data type is int64.
-        right(bool, optional): Find the upper and lower bounds of the sorted_sequence range in the innermost dimension based on the given `values`. If the sorted_sequence value is nan or inf, return the size of the innermost dimension
+        right(bool, optional): Find the upper or lower bounds of the sorted_sequence range in the innermost dimension based on the given `values`. If the value of the sorted_sequence is nan or inf, return the size of the innermost dimension.
                                The default value is False and it shows the lower bounds.  
-        name(str, optional): The default value is None. Normally there is no need for user to set this property. For more information, please
-                             refer to :ref:`api_guide_Name`.
+        name(str, optional): The default value is None. Normally there is no need for user to set this property. For more information, please refer to :ref:`api_guide_Name`.
         
     Returns:
         An N-D Tensor the same sizes of the `values`, return the tensor of `int32` if set :attr:`out_int32` is True, otherwise return the tensor of `int64`.  
@@ -812,6 +811,7 @@ def searchsorted(sorted_sequence,
             # Tensor(shape=[2, 4], dtype=int64, place=CUDAPlace(0), stop_gradient=True,
             #        [[1, 3, 4, 5],
             #         [1, 3, 4, 5]])
+            
     """
 
     if in_dygraph_mode():
