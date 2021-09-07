cb928af8f8d6295ba9ed160bf3f0849b673c9fec
xiaoxiaohehe001 <hiteezsf@163.com>
2021-09-07 07:31:56 +0000

    add_shuffle_teller

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_shuffle_channel.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_shuffle_channel.py
@@ -12,72 +12,111 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from trt_layer_auto_scan_test import TrtLayerAutoScanTest
-from program_config import TensorConfig
+from trt_layer_auto_scan_test import TrtLayerAutoScanTest, SkipReasons
+from program_config import TensorConfig, ProgramConfig
 import numpy as np
 import paddle.inference as paddle_infer
-import unittest
+from functools import partial
+from typing import Optional, List, Callable, Dict, Any, Set
 
 
 class TrtConvertShuffleChannelTest(TrtLayerAutoScanTest):
-    def setUp(self):
-        self.ops_config = [{
-            "op_type": "shuffle_channel",
-            "op_inputs": {
-                "X": ["shuffle_channel_input_data"]
-            },
-            "op_outputs": {
-                "Out": ["shuffle_channel_output_data"]
-            },
-            "op_attrs": {
-                "group": [1, 2, 3]
+    def is_program_valid(self, program_config: ProgramConfig) -> bool:
+        # TODO: This is just the example to remove the wrong attrs.
+        inputs = program_config.inputs
+        weights = program_config.weights
+        outputs = program_config.outputs
+
+        attrs = [
+            program_config.ops[i].attrs
+            for i in range(len(program_config.ops))
+        ]
+        return True
+
+    def sample_program_configs(self):
+        def generate_input1(attrs: List[Dict[str, Any]], batch):
+            return np.ones([batch, 6, 24, 24]).astype(np.float32)
+
+        for batch in [1, 2, 4]:
+            for group in [1, 2, 3]:
+                dics = [{"group": group}, {}]
+                ops_config = [{
+                    "op_type": "shuffle_channel",
+                    "op_inputs": {
+                        "X": ["shuffle_channel_input"]
+                    },
+                    "op_outputs": {
+                        "Out": ["shuffle_channel_out"]
+                    },
+                    "op_attrs": dics[0]
+                }]
+                ops = self.generate_op_config(ops_config)
+                program_config = ProgramConfig(
+                    ops=ops,
+                    weights={},
+                    inputs={
+                        "shuffle_channel_input": TensorConfig(data_gen=partial(
+                            generate_input1, dics, batch))
+                    },
+                    outputs=["shuffle_channel_out"])
+
+                yield program_config
+
+    def sample_predictor_configs(
+            self, program_config) -> (paddle_infer.Config, List[int], float):
+        def generate_dynamic_shape(attrs):
+            self.dynamic_shape.min_input_shape = {
+                "shuffle_channel_input": [1, 6, 24, 24]
             }
-        }]
-        self.batch_size_set = [1, 2, 4]
+            self.dynamic_shape.max_input_shape = {
+                "shuffle_channel_input": [4, 6, 48, 48]
+            }
+            self.dynamic_shape.opt_input_shape = {
+                "shuffle_channel_input": [1, 6, 24, 48]
+            }
 
-    def update_program_input_and_weight_with_attr(self, op_attr_list):
-        shuffle_channel_input_data = TensorConfig(
-            shape=[2, 6, 32, 32], dtype="float32")
-        self.program_weights = {}
-        self.program_inputs = {
-            "shuffle_channel_input_data": shuffle_channel_input_data
-        }
-        self.program_outputs = ["shuffle_channel_output_data"]
+        def clear_dynamic_shape():
+            self.dynamic_shape.min_input_shape = {}
+            self.dynamic_shape.max_input_shape = {}
+            self.dynamic_shape.opt_input_shape = {}
 
-    def test_check_fp32_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Float32
-        # the fused tensorrt engine num is 1, and paddle op num is 2(feed and fetch).
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-5)
+        def generate_trt_nodes_num(attrs, dynamic_shape):
+            # TODO: This is just the example, need to be fixed.
+            if dynamic_shape == True:
+                return 0, 3
+            else:
+                return 1, 2
 
-    def test_check_fp16_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Half
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-2)
-
-    def test_dynamic_shape_fp32_check_output(self):
+        attrs = [
+            program_config.ops[i].attrs
+            for i in range(len(program_config.ops))
+        ]
+        self.trt_param.max_batch_size = 9
+        # for static_shape
+        clear_dynamic_shape()
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        self.dynamic_shape.min_input_shape = {
-            "scale_input_data": [1, 6, 32, 32]
-        }
-        self.dynamic_shape.max_input_shape = {
-            "scale_input_data": [4, 6, 64, 64]
-        }
-        self.dynamic_shape.opt_input_shape = {
-            "scale_input_data": [1, 6, 64, 64]
-        }
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-5)
-
-    def test_dynamic_shape_fp16_check_output(self):
+        yield self.create_inference_config(), generate_trt_nodes_num(
+            attrs, False), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
-        self.dynamic_shape.min_input_shape = {
-            "scale_input_data": [1, 6, 32, 32]
-        }
-        self.dynamic_shape.max_input_shape = {
-            "scale_input_data": [4, 6, 64, 64]
-        }
-        self.dynamic_shape.opt_input_shape = {
-            "scale_input_data": [1, 6, 64, 64]
-        }
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-2)
+        yield self.create_inference_config(), generate_trt_nodes_num(
+            attrs, False), 1e-2
+
+        # for dynamic_shape
+        generate_dynamic_shape(attrs)
+        self.trt_param.precision = paddle_infer.PrecisionType.Float32
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs,
+                                                                     True), 1e-5
+        self.trt_param.precision = paddle_infer.PrecisionType.Half
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs,
+                                                                     True), 1e-2
+
+    def add_skip_trt_case(self):
+        # TODO(wilber): This is just the example to illustrate the skip usage.
+        pass
+
+    def test(self):
+        self.add_skip_trt_case()
+        self.run_test()
 
 
 if __name__ == "__main__":
