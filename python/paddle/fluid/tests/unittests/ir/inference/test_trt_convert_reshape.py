7c967c92eb38ac83f500634043c117a55806d243
xiaoxiaohehe001 <hiteezsf@163.com>
2021-09-07 10:52:19 +0000

    add_reshape_teller

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_reshape.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_reshape.py
@@ -12,199 +12,183 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from trt_layer_auto_scan_test import TrtLayerAutoScanTest
-from program_config import TensorConfig
+from trt_layer_auto_scan_test import TrtLayerAutoScanTest, SkipReasons
+from program_config import TensorConfig, ProgramConfig
 import numpy as np
 import paddle.inference as paddle_infer
-import unittest
+from functools import partial
+from typing import Optional, List, Callable, Dict, Any, Set
 
 
 class TrtConvertReshapeTest(TrtLayerAutoScanTest):
-    def setUp(self):
-        self.ops_config = [{
-            "op_type": "reshape",
-            "op_inputs": {
-                "X": ["input_data"]
-            },
-            "op_outputs": {
-                "Out": ["shape_output_data"]
-            },
-            "op_attrs": {
-                "shape": [[1, 6, 8], [1, 2, 4, 6]]
-            }
-        }]
-        self.batch_size_set = [1]
+    def is_program_valid(self, program_config: ProgramConfig) -> bool:
+        # TODO: This is just the example to remove the wrong attrs.
+        inputs = program_config.inputs
+        weights = program_config.weights
+        outputs = program_config.outputs
 
-    def update_program_input_and_weight_with_attr(self, op_attr_list):
-        input_data = TensorConfig(shape=[1, 48])
-        self.program_weights = {}
-        self.program_inputs = {"input_data": input_data}
-        self.program_outputs = ["shape_output_data"]
+        attrs = [
+            program_config.ops[i].attrs
+            for i in range(len(program_config.ops))
+        ]
+        return True
 
-    def test_check_fp32_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Float32
-        # the fused tensorrt engine num is 1, and paddle op num is 2(feed and fetch).
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-5)
+    def sample_program_configs(self):
+        def generate_input1(attrs: List[Dict[str, Any]]):
+            if self.dims == 4:
+                return np.ones([1, 2, 4, 6]).astype(np.float32)
+            elif self.dims == 3:
+                return np.ones([1, 8, 6]).astype(np.float32)
+            elif self.dims == 2:
+                return np.ones([1, 48]).astype(np.float32)
+            elif self.dims == 1:
+                return np.ones([48]).astype(np.float32)
 
-    def test_check_fp16_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Half
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-2)
+        def generate_weight1(attrs: List[Dict[str, Any]]):
+            return np.array([1, 48]).astype(np.int32)
 
-    def test_dynamic_shape_fp32_check_output(self):
+        def generate_shapeT1_data(attrs: List[Dict[str, Any]]):
+            return np.array([2]).astype(np.int32)
+
+        def generate_shapeT2_data(attrs: List[Dict[str, Any]]):
+            return np.array([24]).astype(np.int32)
+
+        for dims in [4, 3, 2, 1]:
+            for num_input in [0, 1, 2, 3]:
+                for shape in [[1, 6, 8], [1, 2, 4, 6], [1, 6, 8], [2, 24],
+                              [3, 4, 4]]:
+                    dics = [{"shape": shape, }, {}]
+                    self.num_input = num_input
+                    self.dims = dims
+                    dics_intput = [{
+                        "X": ["reshape_input"],
+                        "Shape": ["shape_data"],
+                        "ShapeTensor": ["shapeT1_data", "shapeT2_data"],
+                    }, {
+                        "X": ["reshape_input"],
+                        "Shape": ["shape_data"],
+                    }, {
+                        "X": ["reshape_input"],
+                        "ShapeTensor": ["shapeT1_data", "shapeT2_data"],
+                    }, {
+                        "X": ["reshape_input"]
+                    }]
+
+                    dics_weight = [{
+                        "shape_data":
+                        TensorConfig(data_gen=partial(generate_weight1, dics)),
+                        "shapeT1_data": TensorConfig(data_gen=partial(
+                            generate_shapeT1_data, dics)),
+                        "shapeT2_data": TensorConfig(data_gen=partial(
+                            generate_shapeT2_data, dics))
+                    }, {
+                        "shape_data":
+                        TensorConfig(data_gen=partial(generate_weight1, dics))
+                    }, {
+                        "shapeT1_data": TensorConfig(data_gen=partial(
+                            generate_shapeT1_data, dics)),
+                        "shapeT2_data": TensorConfig(data_gen=partial(
+                            generate_shapeT2_data, dics))
+                    }, {}]
+
+                    ops_config = [{
+                        "op_type": "reshape",
+                        "op_inputs": dics_intput[num_input],
+                        "op_outputs": {
+                            "Out": ["reshape_out"]
+                        },
+                        "op_attrs": dics[0]
+                    }]
+                    ops = self.generate_op_config(ops_config)
+                    program_config = ProgramConfig(
+                        ops=ops,
+                        weights=dics_weight[num_input],
+                        inputs={
+                            "reshape_input": TensorConfig(data_gen=partial(
+                                generate_input1, dics))
+                        },
+                        outputs=["reshape_out"])
+
+                    yield program_config
+
+    def sample_predictor_configs(
+            self, program_config) -> (paddle_infer.Config, List[int], float):
+        def generate_dynamic_shape(attrs):
+            if self.dims == 4:
+                self.dynamic_shape.min_input_shape = {
+                    "reshape_input": [1, 2, 4, 6]
+                }
+                self.dynamic_shape.max_input_shape = {
+                    "reshape_input": [4, 2, 4, 6]
+                }
+                self.dynamic_shape.opt_input_shape = {
+                    "reshape_input": [1, 2, 4, 6]
+                }
+            elif self.dims == 3:
+                self.dynamic_shape.min_input_shape = {
+                    "reshape_input": [1, 8, 6]
+                }
+                self.dynamic_shape.max_input_shape = {
+                    "reshape_input": [4, 8, 6]
+                }
+                self.dynamic_shape.opt_input_shape = {
+                    "reshape_input": [1, 8, 6]
+                }
+            elif self.dims == 2:
+                self.dynamic_shape.min_input_shape = {"reshape_input": [1, 48]}
+                self.dynamic_shape.max_input_shape = {"reshape_input": [4, 48]}
+                self.dynamic_shape.opt_input_shape = {"reshape_input": [1, 48]}
+            elif self.dims == 1:
+                self.dynamic_shape.min_input_shape = {"reshape_input": [48]}
+                self.dynamic_shape.max_input_shape = {"reshape_input": [48]}
+                self.dynamic_shape.opt_input_shape = {"reshape_input": [48]}
+
+        def clear_dynamic_shape():
+            self.dynamic_shape.min_input_shape = {}
+            self.dynamic_shape.max_input_shape = {}
+            self.dynamic_shape.opt_input_shape = {}
+
+        def generate_trt_nodes_num(attrs, dynamic_shape):
+            # TODO: This is just the example, need to be fixed.
+            if self.num_input == 3:
+                return 1, 2
+            else:
+                return 0, 3
+
+        attrs = [
+            program_config.ops[i].attrs
+            for i in range(len(program_config.ops))
+        ]
+        self.trt_param.max_batch_size = 9
+
+        if attrs[0]['shape'][0] != 1 or self.dims == 1:
+            pass
+        else:
+            # for static_shape
+            clear_dynamic_shape()
+            self.trt_param.precision = paddle_infer.PrecisionType.Float32
+            yield self.create_inference_config(), generate_trt_nodes_num(
+                attrs, False), 1e-5
+            self.trt_param.precision = paddle_infer.PrecisionType.Half
+            yield self.create_inference_config(), generate_trt_nodes_num(
+                attrs, False), 1e-2
+
+        # for dynamic_shape
+        generate_dynamic_shape(attrs)
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        self.dynamic_shape.min_input_shape = {"input_data": [1, 48]}
-        self.dynamic_shape.max_input_shape = {"input_data": [4, 48]}
-        self.dynamic_shape.opt_input_shape = {"input_data": [1, 48]}
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-5)
-
-    def test_dynamic_shape_fp16_check_output(self):
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs,
+                                                                     True), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
-        self.dynamic_shape.min_input_shape = {"input_data": [1, 48]}
-        self.dynamic_shape.max_input_shape = {"input_data": [4, 48]}
-        self.dynamic_shape.opt_input_shape = {"input_data": [1, 48]}
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-2)
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs,
+                                                                     True), 1e-2
 
+    def add_skip_trt_case(self):
+        # TODO(wilber): This is just the example to illustrate the skip usage.
+        pass
 
-class TrtConvertDynamicReshapeTest(TrtLayerAutoScanTest):
-    def setUp(self):
-        self.ops_config = [{
-            "op_type": "reshape",
-            "op_inputs": {
-                "X": ["input_data"]
-            },
-            "op_outputs": {
-                "Out": ["shape_output_data"]
-            },
-            "op_attrs": {
-                "shape": [[2, 24], [6, 8]]
-            }
-        }]
-        self.batch_size_set = [1]
-
-    def update_program_input_and_weight_with_attr(self, op_attr_list):
-        input_data = TensorConfig(shape=[1, 12, 4])
-        self.program_weights = {}
-        self.program_inputs = {"input_data": input_data}
-        self.program_outputs = ["shape_output_data"]
-
-    def test_dynamic_shape_fp32_check_output(self):
-        self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        self.dynamic_shape.min_input_shape = {"input_data": [1, 12, 4]}
-        self.dynamic_shape.max_input_shape = {"input_data": [4, 12, 4]}
-        self.dynamic_shape.opt_input_shape = {"input_data": [1, 12, 4]}
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-5)
-
-    def test_dynamic_shape_fp16_check_output(self):
-        self.trt_param.precision = paddle_infer.PrecisionType.Half
-        self.dynamic_shape.min_input_shape = {"input_data": [1, 12, 4]}
-        self.dynamic_shape.max_input_shape = {"input_data": [4, 12, 4]}
-        self.dynamic_shape.opt_input_shape = {"input_data": [1, 12, 4]}
-        self.run_test(trt_engine_num=1, paddle_op_num=2, threshold=1e-2)
-
-
-class TrtConvertReshapeInputShapeTest(TrtLayerAutoScanTest):
-    def setUp(self):
-        self.ops_config = [{
-            "op_type": "reshape",
-            "op_inputs": {
-                "X": ["input_data"],
-                "Shape": ["shape_data"],
-            },
-            "op_outputs": {
-                "Out": ["shape_output_data"]
-            },
-            "op_attrs": {
-                "shape": [[1, 48]]
-            }
-        }]
-        self.batch_size_set = [1]
-
-    def update_program_input_and_weight_with_attr(self, op_attr_list):
-        input_data = TensorConfig(shape=[1, 2, 4, 6])
-        shape_data = TensorConfig(
-            shape=[1, 2],
-            dtype='int32',
-            data=np.array([1, 48]).astype(np.int32))
-        self.program_weights = {"shape_data": shape_data}
-        self.program_inputs = {"input_data": input_data}
-        self.program_outputs = ["shape_output_data"]
-
-    def test_check_fp32_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Float32
-        # the fused tensorrt engine num is 1, and paddle op num is 2(feed and fetch).
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-5)
-
-    def test_check_fp16_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Half
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-2)
-
-    def test_dynamic_shape_fp32_check_output(self):
-        self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        self.dynamic_shape.min_input_shape = {"input_data": [1, 2, 4, 6]}
-        self.dynamic_shape.max_input_shape = {"input_data": [4, 2, 4, 6]}
-        self.dynamic_shape.opt_input_shape = {"input_data": [1, 2, 4, 6]}
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-5)
-
-    def test_dynamic_shape_fp16_check_output(self):
-        self.trt_param.precision = paddle_infer.PrecisionType.Half
-        self.dynamic_shape.min_input_shape = {"input_data": [1, 2, 4, 6]}
-        self.dynamic_shape.max_input_shape = {"input_data": [4, 2, 4, 6]}
-        self.dynamic_shape.opt_input_shape = {"input_data": [1, 2, 4, 6]}
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-2)
-
-
-class TrtConvertReshapeInputShapeTensorTest(TrtLayerAutoScanTest):
-    def setUp(self):
-        self.ops_config = [{
-            "op_type": "reshape",
-            "op_inputs": {
-                "X": ["input_data"],
-                "ShapeTensor": ["shapeT1_data", "shapeT2_data"]
-            },
-            "op_outputs": {
-                "Out": ["shape_output_data"]
-            },
-            "op_attrs": {
-                "shape": [[1, 48]]
-            }
-        }]
-        self.batch_size_set = [1]
-
-    def update_program_input_and_weight_with_attr(self, op_attr_list):
-        input_data = TensorConfig(shape=[1, 2, 4, 6])
-        shapeT1_data = TensorConfig(
-            shape=[1], dtype='int32', data=np.array([2]).astype(np.int32))
-        shapeT2_data = TensorConfig(
-            shape=[1], dtype='int32', data=np.array([24]).astype(np.int32))
-        self.program_weights = {
-            "shapeT1_data": shapeT1_data,
-            "shapeT2_data": shapeT2_data
-        }
-        self.program_inputs = {"input_data": input_data}
-        self.program_outputs = ["shape_output_data"]
-
-    def test_check_fp32_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Float32
-        # the fused tensorrt engine num is 1, and paddle op num is 2(feed and fetch).
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-5)
-
-    def test_check_fp16_output(self):
-        self.trt_param.precision == paddle_infer.PrecisionType.Half
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-2)
-
-    def test_dynamic_shape_fp32_check_output(self):
-        self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        self.dynamic_shape.min_input_shape = {"input_data": [1, 2, 4, 6]}
-        self.dynamic_shape.max_input_shape = {"input_data": [4, 2, 4, 6]}
-        self.dynamic_shape.opt_input_shape = {"input_data": [1, 2, 4, 6]}
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-5)
-
-    def test_dynamic_shape_fp16_check_output(self):
-        self.trt_param.precision = paddle_infer.PrecisionType.Half
-        self.dynamic_shape.min_input_shape = {"input_data": [1, 2, 4, 6]}
-        self.dynamic_shape.max_input_shape = {"input_data": [4, 2, 4, 6]}
-        self.dynamic_shape.opt_input_shape = {"input_data": [1, 2, 4, 6]}
-        self.run_test(trt_engine_num=0, paddle_op_num=3, threshold=1e-2)
+    def test(self):
+        self.add_skip_trt_case()
+        self.run_test()
 
 
 if __name__ == "__main__":
