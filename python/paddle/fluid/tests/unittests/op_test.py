db6242e945ac679dd06e1b97570cc7b45a682c23
Leo Chen <chenqiuliang@baidu.com>
2021-09-08 10:39:38 +0800

    [NPU] release gil before op run (#35370)
    * release gil before op run
    
    * support npu grad test
    
    * fix op_test

--- python/paddle/fluid/tests/unittests/op_test.py
+++ python/paddle/fluid/tests/unittests/op_test.py
@@ -1491,18 +1491,9 @@ class OpTest(unittest.TestCase):
         if not type(output_names) is list:
             output_names = [output_names]
 
-        # FIXME: Replace numeric_place with place to calculate numeric_grads.
-        # NOTE(liym27): There is an unknown error when call op.run() on NPUPlace, which
-        # needs to be fixed.
-        if hasattr(self.__class__,
-                   "use_npu") and self.__class__.use_npu == True:
-            numeric_place = paddle.CPUPlace()
-        else:
-            numeric_place = place
-
         numeric_grads = user_defined_grads or [
             get_numeric_gradient(
-                numeric_place,
+                place,
                 self.scope,
                 self.op,
                 self.inputs,
