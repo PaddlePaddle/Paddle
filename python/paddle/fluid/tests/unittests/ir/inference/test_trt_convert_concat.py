0ff7ba67b0679e050e4ed036e00a69ea8d088ab0
xiaoxiaohehe001 <hiteezsf@163.com>
2021-09-09 03:19:53 +0000

    add_concat_teller

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_concat.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_concat.py
@@ -22,7 +22,6 @@ from typing import Optional, List, Callable, Dict, Any, Set
 
 class TrtConvertConcatTest(TrtLayerAutoScanTest):
     def is_program_valid(self, program_config: ProgramConfig) -> bool:
-        # TODO: This is just the example to remove the wrong attrs.
         inputs = program_config.inputs
         weights = program_config.weights
         outputs = program_config.outputs
@@ -275,7 +274,6 @@ class TrtConvertConcatTest(TrtLayerAutoScanTest):
             self.dynamic_shape.opt_input_shape = {}
 
         def generate_trt_nodes_num(attrs, dynamic_shape):
-            # TODO: This is just the example, need to be fixed.
             if dynamic_shape == True:
                 if attrs[0]['axis'] >= 0:
                     return 1, 4
