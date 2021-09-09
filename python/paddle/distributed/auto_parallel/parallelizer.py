617a5641242f4ee28a4f0418fda8b08494a20fdd
sandyhouse <lilong12@baidu.com>
2021-09-09 10:24:55 +0800

    update

--- python/paddle/distributed/auto_parallel/parallelizer.py
+++ python/paddle/distributed/auto_parallel/parallelizer.py
@@ -14,6 +14,7 @@
 
 import paddle
 from paddle.distributed.fleet import cloud_utils
+import paddle.core as core
 from .context import DistributedContext
 from .context import get_default_distributed_context
 from .completion import complete_annotation
