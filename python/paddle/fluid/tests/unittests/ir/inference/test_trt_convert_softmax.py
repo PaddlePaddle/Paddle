16f4e419ecb050709ab891e7a69783eb96c4fb28
xiaoxiaohehe001 <hiteezsf@163.com>
2021-09-10 04:25:58 +0000

    add_softmax_teller

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_softmax.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_softmax.py
@@ -30,6 +30,8 @@ class TrtConvertSoftmaxTest(TrtLayerAutoScanTest):
             program_config.ops[i].attrs
             for i in range(len(program_config.ops))
         ]
+
+        #The input dimension should be less than or equal to the set axis.
         if len(inputs['softmax_input'].shape) <= attrs[0]['axis']:
             return False
 
@@ -121,7 +123,7 @@ class TrtConvertSoftmaxTest(TrtLayerAutoScanTest):
                 attrs, False), 1e-5
             self.trt_param.precision = paddle_infer.PrecisionType.Half
             yield self.create_inference_config(), generate_trt_nodes_num(
-                attrs, False), 1e-2
+                attrs, False), 1e-5
 
         # for dynamic_shape
         generate_dynamic_shape(attrs)
@@ -130,7 +132,7 @@ class TrtConvertSoftmaxTest(TrtLayerAutoScanTest):
                                                                      True), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
         yield self.create_inference_config(), generate_trt_nodes_num(attrs,
-                                                                     True), 1e-2
+                                                                     True), 1e-5
 
     def add_skip_trt_case(self):
         pass
