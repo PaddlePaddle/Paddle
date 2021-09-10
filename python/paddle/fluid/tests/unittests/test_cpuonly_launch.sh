25a05bb630240a79bb8fefeffcf7e139b1364277
xiongkun <xiongkun03@baidu.com>
2021-09-10 11:00:35 +0000

    add 2 testcase: test_cpuonly_spawn and test_cpuonly_launch

---
+++ python/paddle/fluid/tests/unittests/test_cpuonly_launch.sh
@@ -0,0 +1,36 @@
+#!/bkjjin/bash
+
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+function test_launch_cpuonly(){
+    python -m paddle.distributed.launch --nproc_per_node=24  \
+        parallel_dygraph_gradient_check.py 2>ut.elog
+    if grep -q "ABORT" ut.elog; then
+        echo "test cpu only failed"
+        exit -1
+    else
+        if grep -q "CPUONLY" ut.elog; then
+            echo "test cpu only successfully"
+        else 
+            echo "test cpu only failed"
+            exit -1
+        fi
+    fi
+}
+
+echo "in out loop test mode"
+test_launch_cpuonly
---
+++ python/paddle/fluid/tests/unittests/test_cpuonly_spawn.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+import unittest
+
+import paddle
+import paddle.nn as nn
+import paddle.optimizer as opt
+import paddle.distributed as dist
+
+
+class LinearNet(nn.Layer):
+    def __init__(self):
+        super(LinearNet, self).__init__()
+        self._linear1 = nn.Linear(10, 10)
+        self._linear2 = nn.Linear(10, 1)
+
+    def forward(self, x):
+        return self._linear2(self._linear1(x))
+
+
+def train(print_result=False):
+    # 1. initialize parallel environment
+    dist.init_parallel_env()
+
+    # 2. create data parallel layer & optimizer
+    layer = LinearNet()
+    dp_layer = paddle.DataParallel(layer)
+
+    loss_fn = nn.MSELoss()
+    adam = opt.Adam(learning_rate=0.001, parameters=dp_layer.parameters())
+
+    # 3. run layer
+    inputs = paddle.randn([10, 10], 'float32')
+    outputs = dp_layer(inputs)
+    labels = paddle.randn([10, 1], 'float32')
+    loss = loss_fn(outputs, labels)
+
+    if print_result is True:
+        print("loss:", loss.numpy())
+
+    loss.backward()
+    print("Grad is", layer._linear1.weight.grad)
+    adam.step()
+    adam.clear_grad()
+
+
+# Usage 1: only pass function.
+# If your training method no need any argument, and
+# use all visible devices for parallel training.
+class TestSpawn(unittest.TestCase):
+    def test_spawn(self):
+        dist.spawn(train)
+        #train(print_result=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+## Usage 2: pass function and arguments.
+## If your training method need some arguments, and
+## use all visible devices for parallel training.
+#if __name__ == '__main__':
+#dist.spawn(train, args=(True,))
+
+## Usage 3: pass function, arguments and nprocs.
+## If your training method need some arguments, and
+## only use part of visible devices for parallel training.
+## If your machine hold 8 cards {0,1,2,3,4,5,6,7},
+## this case will use cards {0,1}; If you set
+## CUDA_VISIBLE_DEVICES=4,5,6,7, this case will use
+## cards {4,5}
+#if __name__ == '__main__':
+#dist.spawn(train, args=(True,), nprocs=2)
+
+## Usage 4: pass function, arguments, nprocs and gpus.
+## If your training method need some arguments, and
+## only use part of visible devices for parallel training,
+## but you can't set your machine's environment variable
+## CUDA_VISIBLE_DEVICES, such as it is None or all cards
+## {0,1,2,3,4,5,6,7}, you can pass `gpus` to
+## select the GPU cards you want to use. For example,
+## this case will use cards {4,5} if your machine hold 8 cards.
+#if __name__ == '__main__':
+#dist.spawn(train, args=(True,), nprocs=2, gpus='4,5')
