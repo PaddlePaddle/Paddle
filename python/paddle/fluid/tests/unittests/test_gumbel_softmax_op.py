6779ffd0e73bc55482734ed27e140a81a0f8c0a8
YuanRisheng <yuanrisheng@baidu.com>
2021-09-13 07:53:00 +0000

    delete reletive error in unit test

--- python/paddle/fluid/tests/unittests/test_gumbel_softmax_op.py
+++ python/paddle/fluid/tests/unittests/test_gumbel_softmax_op.py
@@ -129,7 +129,7 @@ class TestGumbelSoftmaxOpSampleDistribution(OpTest):
         self.assertLess(np.max(np.abs(z)).item(), 2.58)
 
     def test_check_grad(self):
-        self.check_grad(["X"], "Out", max_relative_error=0.01)
+        self.check_grad(["X"], "Out")
 
 
 class TestGumbelSoftmaxOpGrad(unittest.TestCase):
