75d5e3bf327f963f1ffb547ce9b7205ebc84de5f
baoachun <962571062@qq.com>
2021-09-13 12:51:54 +0800

    add gather trt converter test case (#35523)

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -319,6 +319,17 @@ bool OpTeller::Tell(const framework::ir::Node* node, bool use_no_calib_int8,
 
     if (op_type == "gather") {
       if (!with_dynamic_shape) return false;
+
+      if (with_dynamic_shape) {
+        auto* block = desc.Block();
+        auto* x_var_desc = block->FindVar(desc.Input("X")[0]);
+        const auto x_shape = x_var_desc->GetShape();
+        if (x_shape.size() == 1) {
+          VLOG(3) << "Gather does not support 1-dimensional input in tensorrt";
+          return false;
+        }
+      }
+
       auto inputs = desc.InputArgumentNames();
       for (auto& input : inputs) {
         if (input == "Axis" && desc.Input("Axis").size() > 0) return false;
