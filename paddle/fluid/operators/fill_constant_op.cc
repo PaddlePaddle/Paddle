c1ca9bfcb0cbdb6b31c1081f8c34755244651571
Zhang Zheng <32410583+ZzSean@users.noreply.github.com>
2021-09-13 13:21:44 +0800

    Support int16_t in fill_constant_op (#35619)

--- paddle/fluid/operators/fill_constant_op.cc
+++ paddle/fluid/operators/fill_constant_op.cc
@@ -149,8 +149,8 @@ REGISTER_OPERATOR(
 REGISTER_OP_CPU_KERNEL(
     fill_constant, ops::FillConstantKernel<float>,
     ops::FillConstantKernel<double>, ops::FillConstantKernel<uint8_t>,
-    ops::FillConstantKernel<int64_t>, ops::FillConstantKernel<int>,
-    ops::FillConstantKernel<bool>,
+    ops::FillConstantKernel<int16_t>, ops::FillConstantKernel<int>,
+    ops::FillConstantKernel<int64_t>, ops::FillConstantKernel<bool>,
     ops::FillConstantKernel<paddle::platform::float16>,
     ops::FillConstantKernel<paddle::platform::bfloat16>,
     ops::FillConstantKernel<paddle::platform::complex<float>>,
