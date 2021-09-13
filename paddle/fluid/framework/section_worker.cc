2e78b44df067a7b7d84e694365af50910dad91a0
liuyuang <liuyuang@baidu.com>
2021-09-13 14:45:58 +0800

    replace unique_ptr with ptr

--- paddle/fluid/framework/section_worker.cc
+++ paddle/fluid/framework/section_worker.cc
@@ -57,22 +57,21 @@ void SectionWorker::Initialize(const TrainerDesc &desc) {
 
     // cache the op type during the init part
     // reduce unnecessary op visit during 1F1B
-    int op_role = op->Attr<int>(std::string("op_role"));
     if ((op_role == static_cast<int>(OpRole::kForward)) ||
         (op_role == (static_cast<int>(OpRole::kForward) |
                      static_cast<int>(OpRole::kLoss))) ||
         (op_role == static_cast<int>(OpRole::kLRSched))) {
-      forward_first_ops_.push_back(op);
+      forward_first_ops_.push_back(op.get());
     } else if ((op_role == static_cast<int>(OpRole::kForward)) ||
                (op_role == (static_cast<int>(OpRole::kForward) |
                             static_cast<int>(OpRole::kLoss)))) {
-      forward_other_ops_.push_back(op);
+      forward_other_ops_.push_back(op.get());
     } else if ((op_role == static_cast<int>(OpRole::kBackward)) ||
                (op_role == (static_cast<int>(OpRole::kBackward) |
                             static_cast<int>(OpRole::kLoss)))) {
-      backward_ops_.push_back(op);
+      backward_ops_.push_back(op.get());
     } else if (op_role == static_cast<int>(OpRole::kOptimize)) {
-      optimizer_ops_.push_back(op);
+      optimizer_ops_.push_back(op.get());
     }
   }
 }
@@ -86,14 +85,14 @@ void SectionWorker::RunForward(
     int micro_id, std::unique_ptr<GarbageCollector> &gc,
     std::unordered_map<const OperatorBase *, std::vector<std::string>>
         &unused_vars_) {
-  std::vector<std::unique_ptr<OperatorBase>> &forward_tmp =
+  std::vector<OperatorBase *> &forward_tmp =
       micro_id == 0 ? forward_first_ops_ : forward_other_ops_;
   for (auto &op : forward_tmp) {
     VLOG(3) << "Forward: running op " << op->Type() << " for micro-batch "
             << micro_id;
     op->Run(*microbatch_scopes_[micro_id], place_);
     if (gc) {
-      DeleteUnusedTensors(*microbatch_scopes_[micro_id], op.get(), unused_vars_,
+      DeleteUnusedTensors(*microbatch_scopes_[micro_id], op, unused_vars_,
                           gc.get());
     }
   }
@@ -108,7 +107,7 @@ void SectionWorker::RunBackward(
             << micro_id;
     op->Run(*microbatch_scopes_[micro_id], place_);
     if (gc) {
-      DeleteUnusedTensors(*microbatch_scopes_[micro_id], op.get(), unused_vars_,
+      DeleteUnusedTensors(*microbatch_scopes_[micro_id], op, unused_vars_,
                           gc.get());
     }
   }
@@ -122,7 +121,7 @@ void SectionWorker::RunUpdate(
     VLOG(3) << "Update: running op " << op->Type();
     op->Run(*microbatch_scopes_[num_microbatches_ - 1], place_);
     if (gc) {
-      DeleteUnusedTensors(*microbatch_scopes_[num_microbatches_ - 1], op.get(),
+      DeleteUnusedTensors(*microbatch_scopes_[num_microbatches_ - 1], op,
                           unused_vars_, gc.get());
     }
   }
