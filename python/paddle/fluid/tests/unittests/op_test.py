b97af7d025caf14d785166a9ef944c2aa6b137c3
Qi Li <qili93@qq.com>
2021-09-14 17:10:52 +0800

    [NPU] conv2d_transpose_grad, test=develop (#35553)
    * [NPU] conv2d_transpose_grad, test=develop
    
    * remove debug files, test=develop
    
    * fix bug, test=develop
    
    * [NPU] fix test_conv2d_transpose_op_npu, test=develop

--- python/paddle/fluid/tests/unittests/op_test.py
+++ python/paddle/fluid/tests/unittests/op_test.py
@@ -1435,7 +1435,8 @@ class OpTest(unittest.TestCase):
                               max_relative_error=0.005,
                               user_defined_grads=None,
                               user_defined_grad_outputs=None,
-                              check_dygraph=True):
+                              check_dygraph=True,
+                              numeric_place=None):
         self.scope = core.Scope()
         op_inputs = self.inputs if hasattr(self, "inputs") else dict()
         op_outputs = self.outputs if hasattr(self, "outputs") else dict()
@@ -1492,9 +1493,12 @@ class OpTest(unittest.TestCase):
         if not type(output_names) is list:
             output_names = [output_names]
 
+        if numeric_place is None:
+            numeric_place = place
+
         numeric_grads = user_defined_grads or [
             get_numeric_gradient(
-                place,
+                numeric_place,
                 self.scope,
                 self.op,
                 self.inputs,
