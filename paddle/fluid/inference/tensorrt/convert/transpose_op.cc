c8944b3987ad8687fbfd37e23057eae2960362e3
xiaoxiaohehe001 <hiteezsf@163.com>
2021-09-14 12:22:08 +0000

    add_transpose_teller

--- paddle/fluid/inference/tensorrt/convert/transpose_op.cc
+++ paddle/fluid/inference/tensorrt/convert/transpose_op.cc
@@ -9,7 +9,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include <bitset>
 #include "paddle/fluid/inference/tensorrt/convert/op_converter.h"
 
 namespace paddle {
