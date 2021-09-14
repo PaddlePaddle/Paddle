618bfe7f9c5f6d8a73ad622ca343ed6063b4c28e
haohongxiang <haohongxiang@baidu.com>
2021-09-14 20:25:59 +0800

    modify docs

--- python/paddle/fluid/optimizer.py
+++ python/paddle/fluid/optimizer.py
@@ -3959,62 +3959,58 @@ class ExponentialMovingAverage(object):
 
 
     Args:
-	decay (float, optional): The exponential decay rate, usually close to 1, such as 
-        0.999, 0.9999, ... . Default 0.999.
-    thres_steps (Variable|None, optional): If not `None`, schedule the decay rate. 
-        Default None.
-    name (str|None, optional): For detailed information, please refer to 
-        :ref:`api_guide_Name`. Usually name is no need to set and None by 
-        default.
+        decay (float, optional): The exponential decay rate, usually close to 1, such as 0.999, 0.9999, ... . Default 0.999.
+        thres_steps (Variable|None, optional): If not `None`, schedule the decay rate. Default None.
+        name (str|None, optional): For detailed information, please refer to :ref:`api_guide_Name`. Usually name is no need to set and None by default.
 
 
     Examples:
 
-	.. code-block:: python
+        .. code-block:: python
 
-	    import numpy
-        import paddle
-        import paddle.static as static
-        from paddle.static import ExponentialMovingAverage
+            import numpy
+            import paddle
+            import paddle.static as static
+            from paddle.static import ExponentialMovingAverage
 
-        paddle.enable_static()
+            paddle.enable_static()
 
-        data = static.data(name='x', shape=[-1, 5], dtype='float32')
-        hidden = static.nn.fc(x=data, size=10)
-        cost = paddle.mean(hidden)
+            data = static.data(name='x', shape=[-1, 5], dtype='float32')
+            hidden = static.nn.fc(x=data, size=10)
+            cost = paddle.mean(hidden)
 
-        test_program = static.default_main_program().clone(for_test=True)
-        optimizer = paddle.optimizer.Adam(learning_rate=0.001)
-        optimizer.minimize(cost)
+            test_program = static.default_main_program().clone(for_test=True)
+            optimizer = paddle.optimizer.Adam(learning_rate=0.001)
+            optimizer.minimize(cost)
 
-        ema = ExponentialMovingAverage(0.999)
-        ema.update()
+            ema = ExponentialMovingAverage(0.999)
+            ema.update()
 
-        place = paddle.CPUPlace()
-        exe = static.Executor(place)
-        exe.run(static.default_startup_program())
+            place = paddle.CPUPlace()
+            exe = static.Executor(place)
+            exe.run(static.default_startup_program())
 
-        for pass_id in range(3):
-            for batch_id in range(6):
-                data = numpy.random.random(size=(10, 5)).astype('float32')
-                exe.run(program=static.default_main_program(),
-                feed={'x': data}, 
-                fetch_list=[cost.name])
-
-            # usage 1
-            with ema.apply(exe):
-                data = numpy.random.random(size=(10, 5)).astype('float32')
-                exe.run(program=test_program,
+            for pass_id in range(3):
+                for batch_id in range(6):
+                    data = numpy.random.random(size=(10, 5)).astype('float32')
+                    exe.run(program=static.default_main_program(),
                     feed={'x': data}, 
-                    fetch_list=[hidden.name])
+                    fetch_list=[cost.name])
 
-            # usage 2
-            with ema.apply(exe, need_restore=False):
-                data = numpy.random.random(size=(10, 5)).astype('float32')
-                exe.run(program=test_program,
-                    feed={'x': data}, 
-                    fetch_list=[hidden.name])
-            ema.restore(exe)
+                # usage 1
+                with ema.apply(exe):
+                    data = numpy.random.random(size=(10, 5)).astype('float32')
+                    exe.run(program=test_program,
+                        feed={'x': data}, 
+                        fetch_list=[hidden.name])
+
+                # usage 2
+                with ema.apply(exe, need_restore=False):
+                    data = numpy.random.random(size=(10, 5)).astype('float32')
+                    exe.run(program=test_program,
+                        feed={'x': data}, 
+                        fetch_list=[hidden.name])
+                ema.restore(exe)
 
     """
 
