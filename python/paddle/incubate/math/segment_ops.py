8fd0c2ab64a582f8c5e3da349949706b9631cedd
Zhong Hui <zhonghui.net@gmail.com>
2021-09-15 16:19:33 +0800

    fix doc

--- python/paddle/incubate/math/segment_ops.py
+++ python/paddle/incubate/math/segment_ops.py
@@ -99,6 +99,7 @@ def segment_mean(data, segment_ids, name=None):
         .. code-block:: python
 
             import paddle
+            data = paddle.to_tensor([[1, 2, 3], [3, 2, 1], [4, 5, 6]], dtype='float32')
             segment_ids = paddle.to_tensor([0, 0, 1], dtype='int32')
             out = paddle.incubate.segment_mean(data, segment_ids)
             #Outputs: [[2., 2., 2.], [4., 5., 6.]]
