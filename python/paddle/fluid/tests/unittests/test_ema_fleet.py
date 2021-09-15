5abc7e216bd2c27cc72a861a5194f53b5a1e7c45
haohongxiang <haohongxiang@baidu.com>
2021-09-15 17:44:29 +0800

    update

--- python/paddle/fluid/tests/unittests/test_ema_fleet.py
+++ python/paddle/fluid/tests/unittests/test_ema_fleet.py
@@ -27,10 +27,9 @@ def gen_data():
     return np.random.random(size=(10, 5)).astype('float32')
 
 
-class TestParallelExecutorEMA(unittest.TestCase):
+class TestFleetStaticEMA(unittest.TestCase):
     def setUp(self):
-        self._place = paddle.CUDAPlace(
-            int(os.environ.get('FLAGS_selected_gpus', 0)))
+        self._place = paddle.CUDAPlace(0)
         self._ema_decay = 0.999
         self._param_name = "fc.weight"
         self._train_program = static.Program()
@@ -58,7 +57,7 @@ class TestParallelExecutorEMA(unittest.TestCase):
                 self._ema = static.ExponentialMovingAverage(self._ema_decay)
                 self._ema.update()
 
-    def train(self, place):
+    def train(self, place, restore):
         exe = static.Executor(place)
         exe.run(self._startup_prog)
 
@@ -70,21 +69,25 @@ class TestParallelExecutorEMA(unittest.TestCase):
                     self._param_name).get_tensor())
                 params.append(tmp_param)
 
-        with self._ema.apply(exe):
-            final_ema = np.array(static.global_scope().find_var(
-                self._param_name).get_tensor())
-            exe.run(program=self._test_program, feed={'x': gen_data()})
+            with self._ema.apply(exe, restore):
+                final_ema = np.array(static.global_scope().find_var(
+                    self._param_name).get_tensor())
+                exe.run(program=self._test_program, feed={'x': gen_data()})
+            if not restore:
+                self._ema.restore(exe)
+
         return params, final_ema
 
     def test_check_ema(self):
-        params, final_ema = self.train(self._place)
-        manu_ema = np.zeros_like(final_ema)
-        if len(params) > 0:
-            for param in params:
-                manu_ema = self._ema_decay * manu_ema + (1 - self._ema_decay
-                                                         ) * param
-            manu_ema = manu_ema / (1.0 - self._ema_decay**len(params))
-        self.assertTrue(np.allclose(manu_ema, final_ema))
+        for restore in (True, False):
+            params, final_ema = self.train(self._place, restore)
+            manu_ema = np.zeros_like(final_ema)
+            if len(params) > 0:
+                for param in params:
+                    manu_ema = self._ema_decay * manu_ema + (1 - self._ema_decay
+                                                             ) * param
+                manu_ema = manu_ema / (1.0 - self._ema_decay**len(params))
+            self.assertTrue(np.allclose(manu_ema, final_ema))
 
 
 if __name__ == '__main__':
