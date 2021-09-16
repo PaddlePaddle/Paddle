d19ee008125a1dedeafb9aa8222066e8502e72f0
WeiXin <weixin10@baidu.com>
2021-09-16 04:50:58 +0000

    polish code.

--- python/paddle/fluid/dygraph/jit.py
+++ python/paddle/fluid/dygraph/jit.py
@@ -1061,6 +1061,7 @@ def load(path, **configs):
                 batch_size=BATCH_SIZE,
                 shuffle=True,
                 drop_last=True,
+                return_list=False,
                 num_workers=2)
 
             # 1. train and save inference model
