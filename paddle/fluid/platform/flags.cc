6e97351272622bb8b129696c50458fa7715f38c5
sneaxiy <sneaxiy@126.com>
2021-09-18 02:44:57 +0000

    update new FLAGS coding style

--- paddle/fluid/platform/flags.cc
+++ paddle/fluid/platform/flags.cc
@@ -674,8 +674,8 @@ PADDLE_DEFINE_EXPORTED_bool(
     apply_pass_to_program, false,
     "It controls whether to apply IR pass to program when using Fleet APIs");
 
-DEFINE_bool(enable_cache_op_run, false, "");
-DEFINE_bool(enable_cache_tensor_address, false, "");
-DEFINE_int64(cuda_graph_op_num, 0, "");
-DEFINE_int64(cuda_graph_warmup_iteration, 3, "");
-DEFINE_bool(build_cuda_graph_once, false, "");
+PADDLE_DEFINE_EXPORTED_bool(enable_cache_op_run, false, "");
+PADDLE_DEFINE_EXPORTED_bool(enable_cache_tensor_address, false, "");
+PADDLE_DEFINE_EXPORTED_int64(cuda_graph_op_num, 0, "");
+PADDLE_DEFINE_EXPORTED_int64(cuda_graph_warmup_iteration, 3, "");
+PADDLE_DEFINE_EXPORTED_bool(build_cuda_graph_once, false, "");
