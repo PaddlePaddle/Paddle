57fe56177ff549b90f06e336e671820c05eb5974
piotrekobiIntel <piotr.paturej@intel.com>
2021-09-21 10:20:16 +0200

    Fix format error

--- paddle/fluid/platform/mkldnn_reuse.h
+++ paddle/fluid/platform/mkldnn_reuse.h
@@ -1539,11 +1539,10 @@ static void SetDstMemoryQuantized(
   T* output_data = output->mutable_data<T>(ctx.GetPlace());
   const size_t dst_dims = dst_tz.size();
   MKLDNNMemoryFormat dst_fmt;
-  PADDLE_ENFORCE_LE(dst_dims, 5,
-                    platform::errors::InvalidArgument(
-                        "Dst memory for quantization can not have "
-                        "dims > 5. But received dst_dims is %d.",
-                        dst_dims));
+  PADDLE_ENFORCE_LE(dst_dims, 5, platform::errors::InvalidArgument(
+                                    "Dst memory for quantization can not have "
+                                    "dims > 5. But received dst_dims is %d.",
+                                    dst_dims));
   dst_fmt = platform::MKLDNNFormatForSize(dst_dims, output_format);
 
   auto tmp_dst_md =
