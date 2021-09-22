897d4460cf9c4146dbd8c1be55978aae41e32ac2
JingZhuangzhuang <75348594+JZZ-NOTE@users.noreply.github.com>
2021-09-21 23:36:19 -0500

    fix conv2d convert test (#35627)
    * support nnadapter and ascend310
    
    * modify code
    
    * add anchor_generator convert test
    
    * add gelu convert test
    
    * add conv2d convert test
    
    * modify anchor_operator convert test
    
    * modify conv2d test
    
    * modify con2d convert test
    
    * modify conv2d convert test
    
    * modify conv2d convert test
    
    * modify conv2d test
    
    * fix WITH_PYTHON compile error
    
    * modify test file
    
    * modify test file
    
    * modify test file
    
    * modify test file
    
    * modify test file
    
    * modify test file
    
    * modify test file
    
    * modify test file
    
    Co-authored-by: xiaoxiaohehe001 <hiteezsf@163.com>
    Co-authored-by: jiweibo <jiweibo@baidu.com>

--- paddle/fluid/inference/tensorrt/convert/conv2d_op.cc
+++ paddle/fluid/inference/tensorrt/convert/conv2d_op.cc
@@ -86,7 +86,20 @@ void ConvertConv2d(TensorRTEngine* engine, const framework::proto::OpDesc& op,
   nvinfer1::DimsHW nv_ksize(filter_h, filter_w);
   nvinfer1::DimsHW nv_dilations(dilations[0], dilations[1]);
   nvinfer1::DimsHW nv_strides(strides[0], strides[1]);
-  nvinfer1::DimsHW nv_paddings(paddings[0], paddings[1]);
+  nvinfer1::DimsHW nv_paddings;
+  nvinfer1::Dims nv_pre_paddings;
+  nvinfer1::Dims nv_post_paddings;
+  if (paddings.size() == 2) {
+    nv_paddings.d[0] = paddings[0];
+    nv_paddings.d[1] = paddings[1];
+  } else {
+    nv_pre_paddings.nbDims = 2;
+    nv_post_paddings.nbDims = 2;
+    nv_pre_paddings.d[0] = paddings[0];
+    nv_pre_paddings.d[1] = paddings[2];
+    nv_post_paddings.d[0] = paddings[1];
+    nv_post_paddings.d[1] = paddings[3];
+  }
 
   TensorRTEngine::Weight weight{nvinfer1::DataType::kFLOAT,
                                 static_cast<void*>(weight_data),
@@ -116,7 +129,13 @@ void ConvertConv2d(TensorRTEngine* engine, const framework::proto::OpDesc& op,
       layer, platform::errors::Fatal("TensorRT create conv2d/conv2d_transpose"
                                      " layer failed."));
   layer->setStride(nv_strides);
-  layer->setPadding(nv_paddings);
+  if (paddings.size() == 2) {
+    layer->setPadding(nv_paddings);
+  } else {
+    layer->setPrePadding(nv_pre_paddings);
+    layer->setPostPadding(nv_post_paddings);
+  }
+
   layer->setNbGroups(groups);
   if (padding_algorithm == "SAME") {
     layer->setPaddingMode(nvinfer1::PaddingMode::kSAME_UPPER);
