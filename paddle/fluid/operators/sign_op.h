be15b0215a2063f6cd442cf63e7d50574d163f72
Chen Weihang <chenweihang@baidu.com>
2021-09-23 06:27:20 +0000

    revert sign, mean, scale, dot kernel removing

---
+++ paddle/fluid/operators/sign_op.h
@@ -0,0 +1,48 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#pragma once
+
+#include "paddle/fluid/framework/eigen.h"
+#include "paddle/fluid/framework/op_registry.h"
+#include "paddle/fluid/framework/tcmpt_utils.h"
+#include "paddle/fluid/operators/eigen/eigen_function.h"
+
+// only can include the headers in paddle/tcmpt/api dirs
+#include "paddle/tcmpt/api/include/dev/core.h"
+#include "paddle/tcmpt/api/include/dev/math.h"
+
+namespace paddle {
+namespace operators {
+template <typename DeviceContext, typename T>
+class SignKernel : public framework::OpKernel<T> {
+ public:
+  virtual void Compute(const framework::ExecutionContext& context) const {
+    auto* x = context.Input<framework::Tensor>("X");
+    auto* out = context.Output<framework::Tensor>("Out");
+    auto& dev_ctx = context.device_context<DeviceContext>();
+    out->mutable_data<T>(x->place(), x->type());
+
+    auto pt_x =
+        framework::MakeTensorImpl<pt::DenseTensor>(*x, x->place(), x->type());
+    auto pt_out =
+        framework::MakeTensorImpl<pt::DenseTensor>(*out, x->place(), x->type());
+
+    // call new kernel
+    pt::Sign<T>(dev_ctx, *pt_x.get(), pt_out.get());
+  }
+};
+
+}  // namespace operators
+}  // namespace paddle
