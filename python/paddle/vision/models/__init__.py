02672a4855efff4f37f74332e3196f80396171d6
SigureMo <sigure.qaq@gmail.com>
2021-09-24 01:44:47 +0800

    add googlenet
    * update AvgPool2D to AdaptiveAvgPool2D
    * class_num -> num_classes
    * add en doc
    * add googlenet to pretrained test
    * remove weights name
    * add parameter with_pool
    * update en doc
    * fix googlenet out shape
    * 2020 -> 2021
    
    Co-authored-by: Ainavo <ainavo@163.com>
    Co-authored-by: pithygit <pyg20200403@163.com>

--- python/paddle/vision/models/__init__.py
+++ python/paddle/vision/models/__init__.py
@@ -45,6 +45,8 @@ from .resnext import resnext152_32x4d  # noqa: F401
 from .resnext import resnext152_64x4d  # noqa: F401
 from .inceptionv3 import InceptionV3  # noqa: F401
 from .inceptionv3 import inception_v3  # noqa: F401
+from .googlenet import GoogLeNet  # noqa: F401
+from .googlenet import googlenet  # noqa: F401
 
 __all__ = [ #noqa
     'ResNet',
@@ -79,5 +81,7 @@ __all__ = [ #noqa
     'resnext152_32x4d',
     'resnext152_64x4d',
     'InceptionV3',
-    'inception_v3'
+    'inception_v3',
+    'GoogLeNet',
+    'googlenet',
 ]
