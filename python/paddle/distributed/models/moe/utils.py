44f1064aa3bc559365e309c921e6a065abce035b
hlygit66666 <2570058140@qq.com>
2021-09-24 08:18:35 +0000

    # This is a combination of 10 commits. # The first commit's message is: add expert count op
    add ut for expert_count
    
    # This is the 2nd commit message:
    
    update UT only for cuda
    
    # This is the 3rd commit message:
    
    fix for rocm
    
    # This is the 4th commit message:
    
    update ut
    
    # This is the 5th commit message:
    
    add moe module
    
    # This is the 6th commit message:
    
    add expert count op
    
    add ut for expert_count
    
    # This is the 7th commit message:
    
    update UT only for cuda
    
    # This is the 8th commit message:
    
    update ut
    
    # This is the 9th commit message:
    
    add moe module
    
    # This is the 10th commit message:
    
    make expert count private

--- python/paddle/distributed/models/moe/utils.py
+++ python/paddle/distributed/models/moe/utils.py
@@ -17,11 +17,11 @@ from paddle.fluid.layer_helper import LayerHelper
 from paddle.fluid.framework import in_dygraph_mode
 
 
-def _number_count(gate_idx, upper_range):
+def _number_count(numbers, upper_range):
     """
     calculate the expert count according to the gate index.
     Args:
-        gate_idx (Tensor): Tensor. The input gate index whose data type should be int32 or int64.
+        numbers (Tensor): Tensor. The input gate index whose data type should be int32 or int64.
         upper_range (int): The number of the experts.
     Returns:
         out (Tensor): The output expert count.
@@ -30,26 +30,26 @@ def _number_count(gate_idx, upper_range):
             # required: distributed
             import paddle
 
-            gate_idx = [
+            numbers = [
                 [0, 2],
                 [0, 2]
             ]
             upper_range = 6
-            gate_idx = paddle.to_tensor(gate_idx, dtype="int32")
-            number_count = paddle.distributed.utils.number_count(gate_idx, upper_range)
+            numbers = paddle.to_tensor(numbers, dtype="int32")
+            number_count = paddle.distributed.utils.number_count(numbers, upper_range)
             print(number_count) # the result: [2, 0, 2, 0, 0, 0]
     """
     if in_dygraph_mode():
-        return core.ops.number_count(gate_idx, 'upper_range', upper_range)
+        return core.ops.number_count(numbers, 'upper_range', upper_range)
     else:
         op_type = 'number_count'
 
         helper = LayerHelper(op_type, **locals())
-        out = helper.create_variable_for_type_inference(dtype=gate_idx.dtype)
+        out = helper.create_variable_for_type_inference(dtype=numbers.dtype)
 
         helper.append_op(
             type=op_type,
-            inputs={'gate_idx': gate_idx},
+            inputs={'numbers': numbers},
             outputs={'Out': out},
             attrs={'upper_range': upper_range})
         return out
