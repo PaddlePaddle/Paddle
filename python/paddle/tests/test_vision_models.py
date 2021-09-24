d92e1c5299cc4a7d715bd8cd7af04abc27d03028
SigureMo <sigure.qaq@gmail.com>
2021-09-24 19:03:34 +0800

    add unit test

--- python/paddle/tests/test_vision_models.py
+++ python/paddle/tests/test_vision_models.py
@@ -71,6 +71,9 @@ class TestVisonModels(unittest.TestCase):
     def test_resnet152(self):
         self.models_infer('resnet152')
 
+    def test_resnext152_32x4d(self):
+        self.models_infer('ResNeXt152_32x4d')
+
     def test_vgg16_num_classes(self):
         vgg16 = models.__dict__['vgg16'](pretrained=False, num_classes=10)
 
