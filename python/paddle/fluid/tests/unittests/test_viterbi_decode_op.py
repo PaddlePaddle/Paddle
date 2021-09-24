d543b215ad745cff036a1e16211cfbe996e96775
zhoushunjie <zhoushunjie@baidu.com>
2021-09-24 21:00:31 +0800

    add viterbi unittest

---
+++ python/paddle/fluid/tests/unittests/test_viterbi_decode_op.py
@@ -0,0 +1,99 @@
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import numpy as np
+from op_test import OpTest
+import paddle.fluid.core as core
+import paddle
+paddle.enable_static()
+
+
+class Decoder(object):
+    def __init__(self, transitions, with_start_stop_tag=True):
+        self.transitions = transitions
+        self.with_start_stop_tag = with_start_stop_tag
+        if with_start_stop_tag:
+            self.start_idx, self.stop_idx = -1, -2
+        self.num_tags = transitions.shape[0]
+
+    def __call__(self, inputs, length):
+        batch_size, seq_len, n_label = inputs.shape
+        inputs_t = np.transpose(inputs, (1, 0, 2))
+        trans_exp = np.expand_dims(self.transitions, axis=0)
+        historys = []
+        left_length = np.array(length)
+        max_seq_len = np.amax(left_length)
+        left_length = np.expand_dims(left_length, 1)
+        if self.with_start_stop_tag:
+            alpha = np.full((batch_size, self.num_tags), -1e4, dtype='float32')
+            alpha[:, -1] = 0
+        else:
+            alpha = np.zeros((batch_size, self.num_tags), dtype='float32')
+        for i, logit in enumerate(inputs_t[:max_seq_len]):
+            if i == 0 and not self.with_start_stop_tag:
+                alpha = logit
+                left_length = left_length - 1
+                continue
+            alpha_exp = np.expand_dims(alpha, 2)
+            alpha_trn_sum = alpha_exp + trans_exp
+            alpha_max = np.amax(alpha_trn_sum, axis=1)
+            if i >= 1:
+                alpha_argmax = np.argmax(alpha_trn_sum, 1)
+                historys.append(alpha_argmax)
+            alpha_nxt = alpha_max + logit
+            mask = (left_length > 0)
+            alpha = mask * alpha_nxt + (1 - mask) * alpha
+            if self.with_start_stop_tag:
+                alpha += (left_length == 1) * trans_exp[:, self.stop_idx]
+            left_length = left_length - 1
+        scores, last_ids = np.amax(alpha, 1), np.argmax(alpha, 1)
+        historys.reverse()
+        left_length = left_length[:, 0]
+        last_ids_update = last_ids * (left_length >= 0)
+        batch_path = [last_ids_update]
+        batch_offset = np.arange(batch_size) * n_label
+        for hist in historys:
+            left_length = left_length + 1
+            tag_mask = (left_length >= 0)
+            gather_idx = batch_offset + last_ids
+            last_ids_update = np.take(hist, gather_idx) * tag_mask
+            batch_path.insert(0, last_ids_update)
+            last_ids = last_ids_update + (1 - tag_mask) * last_ids
+        batch_path = np.stack(batch_path, 1)
+        return scores, batch_path
+
+
+class TestViterbiOp(OpTest):
+    def set_attr(self):
+        self.dtype = np.float32 if core.is_compiled_with_rocm() else np.float64
+        self.with_start_stop_tag = True
+        self.bz = 4
+        self.len = 8
+        self.ntags = 10
+
+    def setUp(self):
+        self.op_type = "viterbi_decode"
+        self.set_attr()
+        bz, ntags = self.bz, self.ntags
+        self.input = np.random.randn(bz, self.len, ntags).astype(self.dtype)
+        self.transitions = np.random.randn(ntags, ntags).astype(self.dtype)
+        self.length = np.random.randint(1, self.len + 1, [self.bz])
+        decoder = Decoder(self.transitions, self.with_start_stop_tag)
+        scores, path = decoder(self.input, self.length)
+        self.inputs = {
+            'Input': self.input,
+            'Transition': self.transitions,
+            'Length': self.length,
+        }
+        self.attrs = {'with_start_stop_tag': self.with_start_stop_tag, }
+        self.outputs = {'Scores': scores, 'Path': path}
+
+    def test_output(self):
+        self.check_output()
