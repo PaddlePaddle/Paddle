a59f3f33955b2e1cb31f0ba2a5c01a3fda5645e9
ZzSean <18818272991@163.com>
2021-09-24 09:12:34 +0000

    support bn+add+relu backward and compile success

--- paddle/fluid/operators/fused/resnet_unit_op.cu
+++ paddle/fluid/operators/fused/resnet_unit_op.cu
@@ -22,6 +22,8 @@ limitations under the License. */
 namespace paddle {
 namespace operators {
 using Tensor = framework::Tensor;
+#define MALLOC_AND_GET_PTR(TR, Dtype, Place) \
+  Dtype *TR##_ptr = TR->mutable_data<Dtype>(Place);
 
 template <typename T>
 class ResNetUnitKernel<platform::CUDADeviceContext, T>
@@ -75,9 +77,6 @@ class ResNetUnitKernel<platform::CUDADeviceContext, T>
         std::accumulate(output_shape.begin(), output_shape.end(), 1,
                         std::multiplies<int>()) /
         output_channel;
-
-#define MALLOC_AND_GET_PTR(TR, Dtype, Place) \
-  Dtype *TR##_ptr = TR->mutable_data<Dtype>(Place);
 
     MALLOC_AND_GET_PTR(conv_out_x, T, place)
     MALLOC_AND_GET_PTR(sum_x, float, place)
@@ -188,7 +187,6 @@ class ResNetUnitKernel<platform::CUDADeviceContext, T>
                          equiv_bias_x_ptr, output_ptr, bitmask_ptr);
       }
     }
-#undef MALLOC_AND_GET_PTR
   }
 };
 
@@ -200,9 +198,154 @@ class ResNetUnitGradKernel<platform::CUDADeviceContext, T>
     PADDLE_ENFORCE_EQ(
         platform::is_gpu_place(ctx.GetPlace()), true,
         platform::errors::PreconditionNotMet("It must use CUDAPlace."));
+
+    // backward input
+    const Tensor *doutput = ctx.Input<Tensor>(framework::GradVarName("Y"));
+    // forward input (backward input)
+    const Tensor *input_x = ctx.Input<Tensor>("X");
+    const Tensor *filter_x = ctx.Input<Tensor>("FilterX");
+    const Tensor *scale_x = ctx.Input<Tensor>("ScaleX");
+    const Tensor *bias_x = ctx.Input<Tensor>("BiasX");
+    const Tensor *saved_mean_x = ctx.Input<Tensor>("SavedMeanX");
+    const Tensor *saved_invstd_x = ctx.Input<Tensor>("SavedInvstdX");
+    // forward output (backward input)
+    const Tensor *conv_out_x = ctx.Input<Tensor>("ConvX");
+    const Tensor *output = ctx.Input<Tensor>("Y");
+    const Tensor *bitmask = ctx.Input<Tensor>("BitMask");
+
+    // backward output
+    Tensor *dinput_x = ctx.Output<Tensor>(framework::GradVarName("X"));
+    Tensor *dfilter_x = ctx.Output<Tensor>(framework::GradVarName("FilterX"));
+    Tensor *dscale_x = ctx.Output<Tensor>(framework::GradVarName("ScaleX"));
+    Tensor *dbias_x = ctx.Output<Tensor>(framework::GradVarName("BiasX"));
+    Tensor *dconv_out_x = ctx.Output<Tensor>(framework::GradVarName("ConvX"));
+
+    // attrs
+    int pad = ctx.Attr<int>("pad");
+    int stride = ctx.Attr<int>("stride");
+    int dilate = ctx.Attr<int>("dilate");
+    int group = ctx.Attr<int>("group");
+    double eps = static_cast<double>(ctx.Attr<float>("epsilon"));
+    double momentum = static_cast<double>(ctx.Attr<float>("momentum"));
+    bool has_shortcut = ctx.Attr<bool>("has_shortcut");
+    bool fused_add = ctx.Attr<bool>("fused_add");
+    bool use_global_stats = ctx.Attr<bool>("use_global_stats");
+    std::string act_type = ctx.Attr<std::string>("act_type");
+
+    // tensor shape
+    auto input_x_shape = framework::vectorize<int>(input_x->dims());
+    auto filter_x_shape = framework::vectorize<int>(filter_x->dims());
+    auto param_shape = framework::vectorize<int>(scale_x->dims());
+    auto output_shape = framework::vectorize<int>(output->dims());
+    auto bitmask_shape = framework::vectorize<int>(bitmask->dims());
+
+    auto place = input_x->place();
+    auto &dev_ctx = ctx.template device_context<platform::CUDADeviceContext>();
+
+    // get ptr
+    // input ptr
+    T *doutput_ptr = const_cast<T *>(doutput->data<T>());
+    T *input_x_ptr = const_cast<T *>(input_x->data<T>());
+    T *filter_x_ptr = const_cast<T *>(filter_x->data<T>());
+    float *scale_x_ptr = const_cast<float *>(scale_x->data<float>());
+    float *bias_x_ptr = const_cast<float *>(bias_x->data<float>());
+    float *saved_mean_x_ptr = const_cast<float *>(saved_mean_x->data<float>());
+    float *saved_invstd_x_ptr =
+        const_cast<float *>(saved_invstd_x->data<float>());
+    T *conv_out_x_ptr = const_cast<T *>(conv_out_x->data<T>());
+    T *output_ptr = const_cast<T *>(output->data<T>());
+    // output ptr
+    MALLOC_AND_GET_PTR(dinput_x, T, place)
+    MALLOC_AND_GET_PTR(dfilter_x, T, place)
+    MALLOC_AND_GET_PTR(dscale_x, float, place)
+    MALLOC_AND_GET_PTR(dbias_x, float, place)
+    MALLOC_AND_GET_PTR(dconv_out_x, T, place)
+
+    // 1. bn add relu backward, get dconv_out_x, dscale_x, dbias_x
+    std::shared_ptr<CudnnScaleBiasAddReluOp<T>> sbar_x_op(
+        new CudnnScaleBiasAddReluOp<T>(fused_add, has_shortcut));
+    if (has_shortcut) {
+      // get dconv_out_z, dz, dscale_z and dbias_z
+      // forward input (backward input)
+      const Tensor *input_z = ctx.Input<Tensor>("Z");
+      const Tensor *filter_z = ctx.Input<Tensor>("FilterZ");
+      const Tensor *scale_z = ctx.Input<Tensor>("ScaleZ");
+      const Tensor *bias_z = ctx.Input<Tensor>("BiasZ");
+      const Tensor *saved_mean_z = ctx.Input<Tensor>("SavedMeanZ");
+      const Tensor *saved_invstd_z = ctx.Input<Tensor>("SavedInvstdZ");
+      // forward output (backward input)
+      const Tensor *conv_out_z = ctx.Input<Tensor>("ConvZ");
+      // backward output
+      Tensor *dinput_z = ctx.Output<Tensor>(framework::GradVarName("Z"));
+      Tensor *dfilter_z = ctx.Output<Tensor>(framework::GradVarName("FilterZ"));
+      Tensor *dscale_z = ctx.Output<Tensor>(framework::GradVarName("ScaleZ"));
+      Tensor *dbias_z = ctx.Output<Tensor>(framework::GradVarName("BiasZ"));
+      Tensor *dconv_out_z = ctx.Output<Tensor>(framework::GradVarName("ConvZ"));
+
+      // get ptr
+      // input ptr
+      T *input_z_ptr = const_cast<T *>(input_z->data<T>());
+      T *filter_z_ptr = const_cast<T *>(filter_z->data<T>());
+      float *scale_z_ptr = const_cast<float *>(scale_z->data<float>());
+      float *bias_z_ptr = const_cast<float *>(bias_z->data<float>());
+      float *saved_mean_z_ptr =
+          const_cast<float *>(saved_mean_z->data<float>());
+      float *saved_invstd_z_ptr =
+          const_cast<float *>(saved_invstd_z->data<float>());
+      T *conv_out_z_ptr = const_cast<T *>(conv_out_z->data<T>());
+      // output ptr
+      MALLOC_AND_GET_PTR(dinput_z, T, place)
+      MALLOC_AND_GET_PTR(dfilter_z, T, place)
+      MALLOC_AND_GET_PTR(dscale_z, float, place)
+      MALLOC_AND_GET_PTR(dbias_z, float, place)
+      MALLOC_AND_GET_PTR(dconv_out_z, T, place)
+      // 1.1 bn add relu backward for x, get dconv_out_x, dscale_x, dbias_x and
+      // temp grad for z
+      Tensor dz_temp;
+      T *dz_temp_ptr = dz_temp.mutable_data<T>(conv_out_z->dims(), place);
+      sbar_x_op->Init(dev_ctx, act_type, output_shape, bitmask_shape,
+                      output_shape, param_shape, output_shape);
+      sbar_x_op->Backward(dev_ctx, doutput_ptr, conv_out_x_ptr, output_ptr,
+                          scale_x_ptr, bias_x_ptr, saved_mean_x_ptr,
+                          saved_invstd_x_ptr, dconv_out_x_ptr, dz_temp_ptr,
+                          dscale_x_ptr, dbias_x_ptr, eps);
+      // 1.2 bn backward for z, get dconv_out_z, dscale_z, dbias_z
+      std::shared_ptr<CudnnScaleBiasAddReluOp<T>> sbar_z_op(
+          new CudnnScaleBiasAddReluOp<T>(false, false));
+      sbar_z_op->Init(dev_ctx, "", output_shape, bitmask_shape, output_shape,
+                      param_shape);
+      sbar_z_op->Backward(dev_ctx, dz_temp_ptr, conv_out_z_ptr, nullptr,
+                          scale_z_ptr, bias_z_ptr, saved_mean_z_ptr,
+                          saved_invstd_z_ptr, dconv_out_z_ptr, nullptr,
+                          dscale_z_ptr, dbias_z_ptr, eps);
+      // 1.3 conv backward for z, get dinput_z and dfilter_z
+    } else {
+      if (fused_add) {
+        // 1.1 bn add relu backward for x, get dconv_out_x, dscale_x, dbias_x
+        // and dinput_z
+        Tensor *dinput_z = ctx.Output<Tensor>(framework::GradVarName("Z"));
+        MALLOC_AND_GET_PTR(dinput_z, T, place)
+        sbar_x_op->Init(dev_ctx, act_type, output_shape, bitmask_shape,
+                        output_shape, param_shape, output_shape);
+        sbar_x_op->Backward(dev_ctx, doutput_ptr, conv_out_x_ptr, output_ptr,
+                            scale_x_ptr, bias_x_ptr, saved_mean_x_ptr,
+                            saved_invstd_x_ptr, dconv_out_x_ptr, dinput_z_ptr,
+                            dscale_x_ptr, dbias_x_ptr, eps);
+      } else {
+        // 1.1 bn add relu backward for x, get dconv_out_x, dscale_x, dbias_x
+        sbar_x_op->Init(dev_ctx, act_type, output_shape, bitmask_shape,
+                        output_shape, param_shape);
+        sbar_x_op->Backward(dev_ctx, doutput_ptr, conv_out_x_ptr, output_ptr,
+                            scale_x_ptr, bias_x_ptr, saved_mean_x_ptr,
+                            saved_invstd_x_ptr, dconv_out_x_ptr, nullptr,
+                            dscale_x_ptr, dbias_x_ptr, eps);
+      }
+    }
+    // 2. conv backward for x, get dinput_x and dfilter_x
   }
 };
 
+#undef MALLOC_AND_GET_PTR
 }  // namespace operators
 }  // namespace paddle
 
