247a50c78e5927ff22d4fbd806f1f61d7c0b285c
Liu Yiqun <liuyiqun01@baidu.com>
2021-09-25 12:38:15 +0000

    Enhance OpTest for bfloat16.

--- paddle/fluid/operators/cast_op.cu
+++ paddle/fluid/operators/cast_op.cu
@@ -79,37 +79,25 @@ struct CastOpFunctor<platform::CUDADeviceContext, InT> {
 }  // namespace paddle
 
 namespace ops = paddle::operators;
+namespace plat = paddle::platform;
 
-#ifdef PADDLE_WITH_HIP
-REGISTER_OP_CUDA_KERNEL(
-    cast, ops::CastOpKernel<paddle::platform::CUDADeviceContext, float>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, double>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, int>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, int64_t>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, int16_t>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, bool>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, uint8_t>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext,
-                      paddle::platform::float16>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext,
-                      paddle::platform::complex<float>>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext,
-                      paddle::platform::complex<double>>);
+#define REGISTER_CAST_CUDA_BASE(op_name, ...)                            \
+  REGISTER_OP_CUDA_KERNEL(                                               \
+      op_name, ops::CastOpKernel<plat::CUDADeviceContext, float>,        \
+      ops::CastOpKernel<plat::CUDADeviceContext, double>,                \
+      ops::CastOpKernel<plat::CUDADeviceContext, int>,                   \
+      ops::CastOpKernel<plat::CUDADeviceContext, int64_t>,               \
+      ops::CastOpKernel<plat::CUDADeviceContext, int16_t>,               \
+      ops::CastOpKernel<plat::CUDADeviceContext, bool>,                  \
+      ops::CastOpKernel<plat::CUDADeviceContext, uint8_t>,               \
+      ops::CastOpKernel<plat::CUDADeviceContext, plat::float16>,         \
+      ops::CastOpKernel<plat::CUDADeviceContext, plat::complex<float>>,  \
+      ops::CastOpKernel<plat::CUDADeviceContext, plat::complex<double>>, \
+      ##__VA_ARGS__);
+
+#if !defined(PADDLE_WITH_HIP)
+REGISTER_CAST_CUDA_BASE(
+    cast, ops::CastOpKernel<plat::CUDADeviceContext, plat::bfloat16>)
 #else
-REGISTER_OP_CUDA_KERNEL(
-    cast, ops::CastOpKernel<paddle::platform::CUDADeviceContext, float>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, double>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, int>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, int64_t>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, int16_t>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, bool>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext, uint8_t>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext,
-                      paddle::platform::float16>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext,
-                      paddle::platform::bfloat16>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext,
-                      paddle::platform::complex<float>>,
-    ops::CastOpKernel<paddle::platform::CUDADeviceContext,
-                      paddle::platform::complex<double>>);
+REGISTER_CAST_CUDA_BASE(cast)
 #endif
