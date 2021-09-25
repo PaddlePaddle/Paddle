620f0d2eff182ed53f08db6f18bc7672ff03129e
AshburnLee <1578034415@qq.com>
2021-09-25 15:18:20 +0000

    try to solve the unittest

--- python/paddle/fluid/tests/unittests/test_eig_op.py
+++ python/paddle/fluid/tests/unittests/test_eig_op.py
@@ -20,8 +20,7 @@ from op_test import OpTest, skip_check_grad_ci
 import unittest
 from paddle.fluid.op import Operator
 from paddle.fluid import compiler, Program, program_guard
-
-
+'''
 @skip_check_grad_ci(reason="temp")
 class TestEigOp(OpTest):
     def setUp(self):
@@ -44,20 +43,17 @@ class TestEigOp(OpTest):
     # if __name__ == "__main__":
     #     paddle.enable_static()
     #     unittest.main()
-
-
 '''
-@skip_check_grad_ci(reason="temp") # no grad test
+
+
+@skip_check_grad_ci(reason="temp")  # no grad test
 class TestEigOp(OpTest):
     def setUp(self):
         self.op_type = "eig"
         self.__class__.op_type = self.op_type
         self.init_input()
         self.inputs = {'X': OpTest.np_dtype_to_fluid_dtype(self.x)}
-        self.outputs = {
-            'Eigenvalues': self.out[0],
-            'Eigenvectors': self.out[1]
-        }
+        self.outputs = {'Eigenvalues': self.out[0], 'Eigenvectors': self.out[1]}
 
     def init_input(self):
         self.shape = (3, 3)
@@ -66,32 +62,36 @@ class TestEigOp(OpTest):
         self.out = np.linalg.eig(self.x)
 
     def compare_results(self, expect, actual, rtol, atol, place):
-        self.assertTrue(np.allclose(
-            np.abs(expect[0]), paddle.abs(actual[0]), rtol, atol, place),
-            "Eigen values has diff at " + str(place) +
-            "\nExpect " + str(expect[0]) + "\n" + "But Got" +
-            str(actual[0]) + " in class " + self.__class__.__name__)
-        
-        self.assertTrue(np.allclose(
-            np.abs(expect[1]), paddle.abs(actual[1]), rtol, atol, place),
-            "Eigen vectors has diff at " + str(place) +
-            "\nExpect " + str(expect[1]) + "\n" + "But Got" +
-            str(actual[1]) + " in class " + self.__class__.__name__)
+        self.assertTrue(
+            np.allclose(
+                np.abs(expect[0]), paddle.abs(actual[0]), rtol, atol,
+                place), "Eigen values has diff at " + str(place) + "\nExpect " +
+            str(expect[0]) + "\n" + "But Got" + str(actual[0]) + " in class " +
+            self.__class__.__name__)
+
+        self.assertTrue(
+            np.allclose(
+                np.abs(expect[1]), paddle.abs(actual[1]), rtol, atol,
+                place), "Eigen vectors has diff at " + str(place) + "\nExpect "
+            + str(expect[1]) + "\n" + "But Got" + str(actual[1]) + " in class "
+            + self.__class__.__name__)
 
     def test_check_output_with_place(self):
         paddle.disable_static()
         place = fluid.CPUPlace()
         x = paddle.to_tensor(self.x)
+        paddle.device.set_device("cpu")
         actual = paddle.linalg.eig(paddle.to_tensor(self.x))
         self.compare_results(self.out, actual, 1e-6, 1e-6, place)
         paddle.enable_static()
 
     def test_grad(self):
-        paddle.disable_static()
-        #self.check_grad(["X"], "Eigenvalues")
-        paddle.enable_static()
-'''
-'''
+        pass
+        # paddle.disable_static()
+        # self.check_grad(["X"], ["Eigenvalues","Eigenvectors"])
+        # paddle.enable_static()
+
+
 class TestEigBatchMarices(TestEigOp):
     def init_input(self):
         self.shape = (3, 3, 3)
@@ -104,24 +104,25 @@ class TestEigStatic(TestEigOp):
     def test_check_output_with_place(self):
         paddle.enable_static()
         place = core.CPUPlace()
-        input_np = np.random.random([3,3]).astype('float32')
+        input_np = np.random.random([3, 3]).astype('float32')
         expect_val, expect_vec = np.linalg.eig(input_np)
         with fluid.program_guard(fluid.Program(), fluid.Program()):
-            input = fluid.data(name="input", shape=[3,3], dtype='float32')
+            input = fluid.data(name="input", shape=[3, 3], dtype='float32')
             act_val, act_vec = paddle.linalg.eig(input)
 
             exe = fluid.Executor(place)
             fetch_val, fetch_vec = exe.run(fluid.default_main_program(),
-                                            feed={"input": input_np},
-                                            fetch_list=[act_val, act_vec])
+                                           feed={"input": input_np},
+                                           fetch_list=[act_val, act_vec])
 
-        self.assertTrue(np.allclose(expect_val, fetch_val, 1e-6, 1e-6), 
-                        "The eigen values have diff ")
-        self.assertTrue(np.allclose(np.abs(expect_vec), np.abs(fetch_vec), 1e-6, 1e-6), 
-                        "The eigen vectors have diff ")
+        self.assertTrue(
+            np.allclose(expect_val, fetch_val, 1e-6, 1e-6),
+            "The eigen values have diff ")
+        self.assertTrue(
+            np.allclose(np.abs(expect_vec), np.abs(fetch_vec), 1e-6, 1e-6),
+            "The eigen vectors have diff ")
 
         paddle.disable_static()
-'''
 
 
 class TestEigWrongDimsError(unittest.TestCase):
@@ -155,5 +156,5 @@ class TestEigUnsupportedDtypeError(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    paddle.enable_static()
+    #paddle.enable_static()
     unittest.main()
