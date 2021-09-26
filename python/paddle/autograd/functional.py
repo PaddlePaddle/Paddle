ae0f8839145124cea07aeb2b70dcd8a09d0d1096
levi131 <limaolin01@baidu.com>
2021-09-26 09:41:13 +0000

    add utils.py

--- python/paddle/autograd/functional.py
+++ python/paddle/autograd/functional.py
@@ -13,37 +13,8 @@
 # limitations under the License.
 
 from paddle.fluid import framework
+from .utils import _check_tensors, _stack_tensor_or_return_none, _replace_none_with_zero_tensor
 import paddle
-
-
-def _check_tensors(in_out_list, name):
-    assert in_out_list is not None, "{} should not be None".format(name)
-
-    if isinstance(in_out_list, (list, tuple)):
-        assert len(in_out_list) > 0, "{} connot be empyt".format(name)
-        for each_var in in_out_list:
-            assert isinstance(
-                each_var,
-                paddle.Tensor), "Elements of {} must be paddle.Tensor".format(
-                    name)
-        return in_out_list
-    else:
-        assert isinstance(
-            in_out_list,
-            paddle.Tensor), "{} must be Tensor or list of Tensor".format(name)
-        return [in_out_list]
-
-
-def _stack_tensor_or_return_none(origin_list):
-    assert len(origin_list) > 0, "Can't not stack an empty list"
-    return paddle.stack(
-        origin_list, axis=0) if isinstance(origin_list[0],
-                                           paddle.Tensor) else None
-
-
-def _replace_none_with_zero_tensor(t, spec_t):
-    return paddle.zeros(
-        shape=spec_t.shape, dtype=spec_t.dtype) if t is None else t
 
 
 @framework.dygraph_only
--- python/paddle/fluid/tests/unittests/autograd/test_hessian.py
+++ python/paddle/fluid/tests/unittests/autograd/test_hessian.py
@@ -17,54 +17,6 @@ import numpy as np
 import paddle
 
 
-def _jac_func()
-
-def _compute_numerical_hessian(func, xs, delta, np_dtype):
-    xs = _check_tensors(xs, "xs")
-    ys = _check_tensors(func(*xs), "ys")
-    fin_size = len(xs)
-    hessian = list([] for _ in range(fin_size))
-    for i in range(fin_size):
-        hessian_i = list([] for _ in range(fin_size))
-        for j in range(fin_size):
-            hessian_i[j] = np.zeros(
-                (_product(xs[i].shape), _product(xs[j].shape)), dtype=np_dtype)
-        hessian[i] = hessian_i
-
-    for i in range(fin_size):
-        for p in range(_product(xs[i].shape)):
-            orig_i = _get_item(xs[i], p)
-            x_pos_i = orig_i + delta
-            x_neg_i = orig_i - delta
-            for j in range(fin_size):
-                for q in range(_product(xs[j].shape)):
-                    orig_j = _get_item(xs[j], q)
-                    x_pos_j = orig_j + delta
-                    x_neg_j = orig_j - delta
-                    xs[i] = _set_item(xs[i], p, x_pos_i)
-
-
-    for j in range(fin_size):
-        for q in range(_product(xs[j].shape)):
-            orig = _get_item(xs[j], q)
-            x_pos = orig + delta
-            xs[j] = _set_item(xs[j], q, x_pos)
-            ys_pos = _check_tensors(func(*xs), "ys_pos")
-
-            x_neg = orig - delta
-            xs[j] = _set_item(xs[j], q, x_neg)
-            ys_neg = _check_tensors(func(*xs), "ys_neg")
-
-            xs[j] = _set_item(xs[j], q, orig)
-
-            for i in range(fout_size):
-                for p in range(_product(ys[i].shape)):
-                    y_pos = _get_item(ys_pos[i], p)
-                    y_neg = _get_item(ys_neg[i], p)
-                    jacobian[i][j][p][q] = (y_pos - y_neg) / delta / 2.
-    return hessian
-
-
 class TestHessian(unittest.TestCase):
     @classmethod
     def setUpClass(self):
