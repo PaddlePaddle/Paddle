7ff226f05a930b81890b65e8f02e4c562ced6b31
crystal <62974595+Zjq9409@users.noreply.github.com>
2021-09-26 12:23:22 +0800

    CPU forward calculation replaces Eigen with Lapack;Modify linalg exposure rules (#35916)

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -106,7 +106,6 @@ from .tensor.linalg import slogdet  # noqa: F401
 from .tensor.linalg import multi_dot  # noqa: F401
 from .tensor.linalg import matrix_power  # noqa: F401
 from .tensor.linalg import svd  # noqa: F401
-from .tensor.linalg import eigh  # noqa: F401
 from .tensor.linalg import pinv  # noqa: F401
 from .tensor.linalg import solve  # noqa: F401
 from .tensor.logic import equal  # noqa: F401
