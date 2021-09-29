d200d0bc63256e6f5d6606eb9e381ef421be612d
Yanxing-Shi <shiyanxing2008@qq.com>
2021-09-29 08:46:16 +0000

    fix error for the gate_idx dim

--- python/paddle/fluid/tests/unittests/test_prune_gate_by_capacity_op.py
+++ python/paddle/fluid/tests/unittests/test_prune_gate_by_capacity_op.py
@@ -19,10 +19,10 @@ import numpy as np
 
 class TestPruneGateByCapacityOp(unittest.TestCase):
     def init_test_case(self):
-        self.n_expert = 8
+        self.n_expert = 5
         self.n_worker = 1
-        self.gate_idx = np.array([1, 3, 3, 3, 3, 2, 1, 1])
-        self.expert_count = np.array([0, 3, 1, 3, 0, 0, 0, 0])
+        self.gate_idx = np.array([1, 3, 3, 3, 3, 2, 1, 1]).astype("int64")
+        self.expert_count = np.array([0, 3, 1, 3, 0]).astype("int64")
 
     def setUp(self):
         self.init_test_case()
@@ -34,16 +34,16 @@ class TestPruneGateByCapacityOp(unittest.TestCase):
         def run(place):
             with paddle.static.program_guard(paddle.static.Program()):
                 gate_idx_tensor = paddle.static.data(
-                    'GateIdx', shape=self.gate_idx.shape, dtype="int32")
+                    'GateIdx', shape=self.gate_idx.shape, dtype="int64")
                 expert_count_tensor = paddle.static.data(
                     'ExpertCount', shape=self.expert_count.shape, dtype="int64")
                 out = paddle.distributed.utils.prune_gate_by_capacity(
                     gate_idx_tensor, expert_count_tensor, self.n_expert,
                     self.n_worker)
-                exe = paddle.static.Executor(place)
+                exe = paddle.static.Executor(self.place)
                 res = exe.run(feed={
-                    'GateIdx': self.gate_idx.astype("int32"),
-                    'ExpertCount': self.expert_count.astype("int64"),
+                    'GateIdx': self.gate_idx,
+                    'ExpertCount': self.expert_count,
                 },
                               fetch_list=out)
 
@@ -59,9 +59,8 @@ class TestPruneGateByCapacityOp(unittest.TestCase):
     def test_dygraph_api(self):
         def run(place):
             paddle.disable_static(place)
-            gate_idx_tensor = paddle.to_tensor(self.gate_idx.astype('int32'))
-            expert_count_tensor = paddle.to_tensor(
-                self.expert_count.astype('int64'))
+            gate_idx_tensor = paddle.to_tensor(self.gate_idx)
+            expert_count_tensor = paddle.to_tensor(self.expert_count)
             out = paddle.distributed.utils.prune_gate_by_capacity(
                 gate_idx_tensor, expert_count_tensor, self.n_expert,
                 self.n_worker)
