77a4503cbc831d8ccf1825677c7733ad583adffd
yaoxuefeng6 <yaoxuefeng@baidu.com>
2021-09-29 16:05:31 +0800

    fix

--- paddle/fluid/framework/data_feed.h
+++ paddle/fluid/framework/data_feed.h
@@ -384,7 +384,7 @@ class CustomParser {
   CustomParser() {}
   virtual ~CustomParser() {}
   virtual void Init(const std::vector<SlotConf>& slots) = 0;
-  virtual bool Init(const std::vector<AllSlotInfo>& slots) = 0;
+  virtual bool Init(const std::vector<AllSlotInfo>& slots);
   virtual void ParseOneInstance(const char* str, Record* instance) = 0;
   virtual bool ParseOneInstance(
       const std::string& line,
