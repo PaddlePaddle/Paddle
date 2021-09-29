bf0f99b4313448c015fa1a25bedd3beae1d25061
Chen Weihang <chenweihang@baidu.com>
2021-09-29 11:36:16 +0000

    fix ifdef error

--- paddle/tcmpt/core/convert_utils.cc
+++ paddle/tcmpt/core/convert_utils.cc
@@ -92,20 +92,30 @@ paddle::platform::Place TransToFluidPlace(const Backend& backend) {
   switch (backend) {
     case pt::Backend::kCPU:
       return paddle::platform::CPUPlace();
+#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
     case pt::Backend::kCUDA:
       return paddle::platform::CUDAPlace(
           paddle::platform::GetCurrentDeviceId());
+#endif
+#ifdef PADDLE_WITH_XPU
     case pt::Backend::kXPU:
       // TODO(chenweihang): add device id
       return paddle::platform::XPUPlace();
+#endif
+#ifdef PADDLE_WITH_NPU
     case pt::Backend::kNPU:
       // TODO(chenweihang): add device id
       return paddle::platform::NPUPlace();
+#endif
+#ifdef PADDLE_WITH_MKLDNN
     case pt::Backend::kMKLDNN:
       return paddle::platform::CPUPlace();
+#endif
+#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
     case pt::Backend::kCUDNN:
       return paddle::platform::CUDAPlace(
           paddle::platform::GetCurrentDeviceId());
+#endif
     default:
       PADDLE_THROW(paddle::platform::errors::Unimplemented(
           "Unsupported backend `%s` when casting it to paddle place type.",
