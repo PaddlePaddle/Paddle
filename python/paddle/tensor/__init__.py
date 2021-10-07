5de7fa0415b5c0041064fec25890aae3ae27b31c
chenruibiao <chenruibiao@baidu.com>
2021-10-07 14:50:31 +0000

    Add new API tensordot

--- python/paddle/tensor/__init__.py
+++ python/paddle/tensor/__init__.py
@@ -104,6 +104,7 @@ from .manipulation import flip  # noqa: F401
 from .manipulation import unbind  # noqa: F401
 from .manipulation import roll  # noqa: F401
 from .manipulation import chunk  # noqa: F401
+from .manipulation import tensordot  # noqa: F401
 from .math import abs  # noqa: F401
 from .math import acos  # noqa: F401
 from .math import asin  # noqa: F401
@@ -345,6 +346,7 @@ tensor_method_func  = [ #noqa
            'slice',
            'split',
            'chunk',
+           'tensordot',
            'squeeze',
            'squeeze_',
            'stack',
