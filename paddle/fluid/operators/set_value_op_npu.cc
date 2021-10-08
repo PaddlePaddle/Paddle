d7b2f32a2755f0490db505741b2d48397d27c0cc
qili93 <qili93@qq.com>
2021-10-09 01:14:24 +0800

    fix typo, test=develop

--- paddle/fluid/operators/set_value_op_npu.cc
+++ paddle/fluid/operators/set_value_op_npu.cc
@@ -18,7 +18,6 @@ limitations under the License. */
 namespace paddle {
 namespace operators {
 
-using Tensor = framework::Tensor;
 using NPUDeviceContext = platform::NPUDeviceContext;
 
 template <typename T>
