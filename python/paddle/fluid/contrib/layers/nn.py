91ec06c7e2f38786680d05847a247c631773dcc7
Yanxing-Shi <shiyanxing2008@qq.com>
2021-10-08 11:12:55 +0000

    fix error for sample code

--- python/paddle/fluid/contrib/layers/nn.py
+++ python/paddle/fluid/contrib/layers/nn.py
@@ -1068,8 +1068,8 @@ def sparse_embedding(input,
             entry = paddle.distributed.CountFilterEntry(10)
 
             input = paddle.static.data(name='ins', shape=[1], dtype='int64')
-
-            emb = paddle.static.nn.sparse_embedding((
+        
+            emb = paddle.static.nn.sparse_embedding(
                 input=input,
                 size=[sparse_feature_dim, embedding_size],
                 is_test=False,
