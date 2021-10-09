0a13415d7002770a63dbfbafae509b3cd6b2841c
wangxinxin08 <wangxinxin08@baidu.com>
2021-10-09 06:22:56 +0000

    add check and modify unittest while converting mish to trt plugin

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_mish.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_mish.py
@@ -26,37 +26,93 @@ class TrtConvertMishTest(TrtLayerAutoScanTest):
         return True
 
     def sample_program_configs(self):
-        def generate_input():
-            return np.ones([1, 3, 64, 64]).astype(np.float32)
+        def generate_input(batch, dim1, dim2, dim3):
+            shape = [batch]
+            if dim1 != 0:
+                shape.append(dim1)
+            if dim2 != 0:
+                shape.append(dim2)
+            if dim3 != 0:
+                shape.append(dim3)
+            return np.random.random(shape).astype(np.float32)
 
-        ops_config = [{
-            "op_type": "mish",
-            "op_inputs": {
-                "X": ["input_data"]
-            },
-            "op_outputs": {
-                "Out": ["mish_output_data"]
-            },
-            "op_attrs": {
-                "threshold": 20.0
-            }
-        }]
+        for batch in [1, 4]:
+            for dim1 in [0, 3]:
+                for dim2 in [0, 16]:
+                    for dim3 in [0, 32]:
+                        for thre in [5.0, 20.0]:
+                            self.dim1 = dim1
+                            self.dim2 = dim2
+                            self.dim3 = dim3
 
-        ops = self.generate_op_config(ops_config)
-        program_config = ProgramConfig(
-            ops=ops,
-            weights={},
-            inputs={"input_data": TensorConfig(data_gen=generate_input)},
-            outputs=["mish_output_data"])
+                            if dim1 == 0 and dim2 != 0:
+                                continue
+                            if dim1 == 0 and dim2 == 0 and dim3 != 0:
+                                continue
 
-        yield program_config
+                            ops_config = [{
+                                "op_type": "mish",
+                                "op_inputs": {
+                                    "X": ["input_data"]
+                                },
+                                "op_outputs": {
+                                    "Out": ["mish_output_data"]
+                                },
+                                "op_attrs": {
+                                    "threshold": thre
+                                }
+                            }]
 
-    def sample_predictor_configs(
-            self, program_config) -> (paddle_infer.Config, List[int], float):
+                            ops = self.generate_op_config(ops_config)
+                            program_config = ProgramConfig(
+                                ops=ops,
+                                weights={},
+                                inputs={
+                                    "input_data": TensorConfig(
+                                        data_gen=partial(generate_input, batch,
+                                                         dim1, dim2, dim3))
+                                },
+                                outputs=["mish_output_data"])
+
+                            yield program_config
+
+    def sample_predictor_configs(self, program_config):
         def generate_dynamic_shape(attrs):
-            self.dynamic_shape.min_input_shape = {"input_data": [1, 3, 32, 32]}
-            self.dynamic_shape.max_input_shape = {"input_data": [4, 3, 64, 64]}
-            self.dynamic_shape.opt_input_shape = {"input_data": [1, 3, 64, 64]}
+            if self.dim1 == 0:
+                self.dynamic_shape.min_input_shape = {"input_data": [1], }
+                self.dynamic_shape.max_input_shape = {"input_data": [4], }
+                self.dynamic_shape.opt_input_shape = {"input_data": [2], }
+            else:
+                if self.dim2 == 0 and self.dim3 == 0:
+                    self.dynamic_shape.min_input_shape = {
+                        "input_data": [1, 1],
+                    }
+                    self.dynamic_shape.max_input_shape = {
+                        "input_data": [4, 64],
+                    }
+                    self.dynamic_shape.opt_input_shape = {
+                        "input_data": [2, 3],
+                    }
+                elif self.dim2 != 0 and self.dim3 != 0:
+                    self.dynamic_shape.min_input_shape = {
+                        "input_data": [1, 1, 1, 1],
+                    }
+                    self.dynamic_shape.max_input_shape = {
+                        "input_data": [4, 64, 128, 128],
+                    }
+                    self.dynamic_shape.opt_input_shape = {
+                        "input_data": [2, 3, 16, 32],
+                    }
+                elif self.dim3 == 0:
+                    self.dynamic_shape.min_input_shape = {
+                        "input_data": [1, 1, 1],
+                    }
+                    self.dynamic_shape.max_input_shape = {
+                        "input_data": [4, 64, 256],
+                    }
+                    self.dynamic_shape.opt_input_shape = {
+                        "input_data": [2, 3, 128],
+                    }
 
         def clear_dynamic_shape():
             self.dynamic_shape.min_input_shape = {}
@@ -78,7 +134,7 @@ class TrtConvertMishTest(TrtLayerAutoScanTest):
             attrs, False), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
         yield self.create_inference_config(), generate_trt_nodes_num(
-            attrs, False), 1e-2
+            attrs, False), 1e-5
 
         # for dynamic_shape
         generate_dynamic_shape(attrs)
@@ -87,9 +143,30 @@ class TrtConvertMishTest(TrtLayerAutoScanTest):
                                                                      True), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
         yield self.create_inference_config(), generate_trt_nodes_num(attrs,
-                                                                     True), 1e-2
+                                                                     True), 1e-5
+
+    def add_skip_trt_case(self):
+        def teller1(program_config, predictor_config):
+            if self.dim1 == 0 and self.dim2 == 0 and self.dim3 == 0:
+                return True
+            return False
+
+        self.add_skip_case(teller1, SkipReasons.TRT_NOT_SUPPORT,
+                           "Trt does not support 1-dimensional input.")
+
+        def teller2(program_config, predictor_config):
+            if (len(self.dynamic_shape.min_input_shape) == 0):
+                if self.dim1 != 0 and self.dim2 == 0 and self.dim3 == 0:
+                    return True
+            return False
+
+        self.add_skip_case(
+            teller2, SkipReasons.TRT_NOT_SUPPORT,
+            "Need to repair the case: the output of GPU and tensorrt has diff when the input dimension is 2 in static shape mode."
+        )
 
     def test(self):
+        self.add_skip_trt_case()
         self.run_test()
 
 
