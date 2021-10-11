3cc19dafbe11d72824a66145448152d80bb70424
HydrogenSulfate <490868991@qq.com>
2021-10-11 23:13:41 +0800

    Update test_cross_entropy_loss.py

--- python/paddle/fluid/tests/unittests/test_cross_entropy_loss.py
+++ python/paddle/fluid/tests/unittests/test_cross_entropy_loss.py
@@ -1208,7 +1208,7 @@ class CrossEntropyLoss(unittest.TestCase):
             self.assertIsNotNone(static_ret)
         with fluid.dygraph.guard():
             cross_entropy_loss = paddle.nn.loss.CrossEntropyLoss(
-                weight=fluid.dygraph.to_variable(weight_np), reduction='mean')
+                weight=fluid.dygraph.to_variable(weight_np), reduction='mean', axis=1)
             dy_ret = cross_entropy_loss(
                 fluid.dygraph.to_variable(input_np),
                 fluid.dygraph.to_variable(label_np))
