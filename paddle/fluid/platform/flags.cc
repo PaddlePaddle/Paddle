7b45a46e13fe057ca12a001dac7b8d6d24d9f211
Zeng Jinle <32832641+sneaxiy@users.noreply.github.com>
2021-10-11 19:59:16 +0800

    Add FLAGS_allreduce_record_one_event to remove event waiting number (#36263)
    * add FLAGS_allreduce_record_one_event
    
    * add more comments
    
    * fix ut
    
    * improve coverage
    
    * fix ut, improve coverage

--- paddle/fluid/platform/flags.cc
+++ paddle/fluid/platform/flags.cc
@@ -682,6 +682,23 @@ PADDLE_DEFINE_EXPORTED_bool(
     "It controls whether to apply IR pass to program when using Fleet APIs");
 
 /**
+ * Distributed related FLAG
+ * Name: FLAGS_allreduce_record_one_event
+ * Since Version: 2.2.0
+ * Value Range: bool, default=false
+ * Example: FLAGS_allreduce_record_one_event=true makes the allreduce
+ *          operations would only wait one event instead of multiple events.
+ * Note: Make the allreduce operations would only wait one event instead of
+ *       multiple events. Currently, only fuse allreduce supports this.
+ *       Otherwise, the precision may be wrong.
+ */
+PADDLE_DEFINE_EXPORTED_bool(allreduce_record_one_event, false,
+                            "It controls whether the allreduce operations "
+                            "would only wait one event instead of multiple "
+                            "events. Currently, only fuse allreduce supports "
+                            "this. Otherwise, the precision may be wrong.");
+
+/*
  * CINN related FLAG
  * Name: FLAGS_use_cinn
  * Since Version: 2.3
