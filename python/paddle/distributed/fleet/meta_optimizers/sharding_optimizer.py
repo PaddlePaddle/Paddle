8487d91cff32e86440caf50d0f3231c55b31380c
liuyuang <liuyuang@baidu.com>
2021-10-12 16:13:41 +0800

    add flag to control the avg position for grad merge under pipeline mode
    rename the flag name
    
    change the default value of avg_loss
    
    fix ut
    
    optimize for amp
    
    update ut
    
    add no amp ut

--- python/paddle/distributed/fleet/meta_optimizers/sharding_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/sharding_optimizer.py
@@ -18,7 +18,7 @@ import paddle.fluid as fluid
 from paddle.static import default_startup_program, device_guard
 from paddle.fluid import layers
 
-from .common import OpRole, OP_ROLE_VAR_KEY, CollectiveHelper
+from .common import OpRole, OP_ROLE_VAR_KEY, CollectiveHelper, OP_ROLE_KEY
 from .common import is_backward_op, is_optimizer_op, is_update_op
 from .meta_optimizer_base import MetaOptimizerBase
 from .sharding.shard import Shard, ProgramSegment
@@ -193,6 +193,14 @@ class ShardingOptimizer(MetaOptimizerBase):
         else:
             gm_mode = "pp_gm"
             gm_acc_step = strategy.pipeline_configs['accumulate_steps']
+            gradient_scale_configs = strategy.gradient_scale_configs
+            assert gradient_scale_configs['scale_strategy'] == 'avg', \
+                'For pipeline mode, the ' 'gradient scale mode should ' \
+                'be "avg", but got {}'.format(gradient_scale_configs['scale_strategy'])
+            # Note (Yuang Liu): this avg_loss flag determines where to do the average op for grad merge.
+            # If True, will do sum firstly for gradient merge, then do scale by gm_acc_step.
+            # If False, will scale loss by gm_acc_step first, then do sum for gradient merge.
+            self.avg_loss = gradient_scale_configs['avg_loss']
         if gm_acc_step > 1:
             logger.info("Gradient merge in [{}], acc step = [{}]".format(
                 gm_mode, gm_acc_step))
@@ -241,6 +249,7 @@ class ShardingOptimizer(MetaOptimizerBase):
                 'global_ring_id': 3,
                 'mp_degree': self.mp_degree,
                 'mp_rank': global_rank % self.mp_degree,
+                'avg_loss': self.avg_loss
             }
             main_program = loss.block.program
             main_program._pipeline_opt = pipeline_opt
@@ -362,7 +371,11 @@ class ShardingOptimizer(MetaOptimizerBase):
             main_block, strategy=strategy, shard=shard)
 
         len_of_ops = len(main_block.ops)
-        first_optimize_op_index = get_first_optimize_op_idx(main_block)
+        if self.avg_loss:
+            first_optimize_op_index = get_first_optimize_op_idx(main_block)
+        else:
+            first_optimize_op_index = self._avg_grad_merge_after_sum(
+                main_block, accumulated_grad_names)
 
         if self.pp_allreduce_in_optimize:
             logger.info("Pipeline Persistable grad is {}".format(
@@ -428,6 +441,52 @@ class ShardingOptimizer(MetaOptimizerBase):
             len_of_ops = len(main_block.ops)
 
         # FIXME(wangxi): if fp16_allreduce, put cast fp16->fp32 to there?
+
+    def _avg_grad_merge_after_sum(self, main_block, accumulated_grad_names):
+        tmp_first_opt_idx = get_first_optimize_op_idx(main_block)
+        if self.user_defined_strategy.amp:
+            # For AMP, the avg operation can be simple done by modify the
+            # LossScaling op.
+            for idx, op in enumerate(main_block.ops):
+                if op.type == 'check_finite_and_unscale':
+                    loss_scale_name = op.input('Scale')[0]
+                    loss_scaling_var = main_block.var(loss_scale_name)
+                    loss_scale_tmp_var = main_block.create_var(
+                        name="loss_scale_tmp_var",
+                        shape=loss_scaling_var.shape,
+                        dtype=loss_scaling_var.dtype)
+                    main_block._insert_op(
+                        idx,
+                        type='scale',
+                        inputs={'X': loss_scaling_var},
+                        outputs={'Out': loss_scale_tmp_var},
+                        attrs={
+                            'scale': self._gradient_merge_acc_step,
+                            'bias': 0.0,
+                            'bias_after_scale': False,
+                            OP_ROLE_KEY: OpRole.Optimize
+                        })
+                    op._rename_input(loss_scale_name, loss_scale_tmp_var.name)
+                    break
+        else:
+            # For pp, do the avg operation for gradient merge after merging
+            # the gradient to meet the logic for gradient merge under pure dp.
+            for grad in accumulated_grad_names:
+                main_block._insert_op_without_sync(
+                    tmp_first_opt_idx,
+                    type='scale',
+                    inputs={'X': grad},
+                    outputs={'Out': grad},
+                    attrs={
+                        'scale': 1.0 / self._gradient_merge_acc_step,
+                        'bias': 0.0,
+                        'bias_after_scale': False,
+                        OP_ROLE_KEY: OpRole.Optimize
+                    })
+            # Note(Yuang Liu): Need to move the first opt idx a little bit after
+            # to make sure the c_allreudce_sum ops are inserted behind scale ops
+            tmp_first_opt_idx += len(accumulated_grad_names)
+        return tmp_first_opt_idx
 
     def _adapt_amp_clip_without_sharding(self):
         # if not use sharding, adapt amp/clip, for remain parallelism.
