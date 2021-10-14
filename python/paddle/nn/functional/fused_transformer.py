2d9f7278c035b4fb0a1f37bb398eef061988b249
limin2021 <1121099234@qq.com>
2021-10-14 05:54:11 +0000

    Minors.

--- python/paddle/nn/functional/fused_transformer.py
+++ python/paddle/nn/functional/fused_transformer.py
@@ -12,29 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#import warnings
 import paddle
-#from ...fluid.framework import in_dygraph_mode, default_main_program
-#from paddle.fluid.layer_helper import LayerHelper
-#from paddle.fluid.layers.tensor import fill_constant
-#from ...tensor import concat
-#from ...tensor.creation import zeros
-#from paddle.static import Variable
-#from ...fluid.layers import core
-#from ...fluid import dygraph_utils
-#from ...fluid.layers import unfold  # noqa: F401
-#from ...tensor.manipulation import squeeze
-#from ...tensor.manipulation import unsqueeze
-#from ...tensor import clip
-#from ...tensor import sum
-#from ...tensor import sqrt
-#from ...fluid.data_feeder import check_variable_and_dtype, check_dtype
-#from ...fluid.framework import in_dygraph_mode, _varbase_creator
-
 from ...fluid.framework import in_dygraph_mode
-#from ...fluid import core, dygraph_utils
-#from ...fluid import core, layers
-#from ...fluid.data_feeder import check_variable_and_dtype
 from paddle import _C_ops
 
 __all__ = []
@@ -59,8 +38,9 @@ def fused_multihead_attention(x,
     r"""
     """
     if in_dygraph_mode():
-        # pre_ln_mean, pre_ln_variance, pre_ln_out, qkv_out, qkv_bias_out, transpose_out, qk_out, qktv_out, softmax_out, attn_dropout_mask_out, 
-        # attn_dropout_out, attn_mask_out, fmha_out, linear_out, dropout_mask_out, ln_mean_out, ln_var_out, bias_dropout_residual_out, final_out
+        # pre_ln_mean, pre_ln_variance, pre_ln_out, qkv_out, qkv_bias_out, transpose_out, qk_out, 
+        # qktv_out, softmax_out, attn_dropout_mask_out, attn_dropout_out, attn_mask_out, fmha_out, 
+        # linear_out, dropout_mask_out, ln_mean_out, ln_var_out, bias_dropout_residual_out, final_out
         _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, final_out = _C_ops.fused_attention(
             x, pre_ln_scale, pre_ln_bias, qkv_weight, qkv_bias, attn_mask,
             linear_weight, linear_bias, ln_scale, ln_bias, 'pre_layer_norm',
