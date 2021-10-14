e262f9180a6f06386569ee7ce2297982d5211a42
DesmonDay <908660116@qq.com>
2021-10-14 10:50:58 +0000

    notest,test=windows_ci

--- paddle/fluid/pybind/imperative.cc
+++ paddle/fluid/pybind/imperative.cc
@@ -2352,7 +2352,7 @@ void BindImperative(py::module *m_ptr) {
       
       .. code-block:: python
 
-          # required gpu
+          # required: gpu
           import paddle
           from paddle.fluid import core  
           from paddle.device import cuda
@@ -2558,7 +2558,7 @@ void BindImperative(py::module *m_ptr) {
   Examples:
       .. code-block:: python
 
-          # required gpu
+          # required: gpu
           import paddle
           from paddle.fluid import core
           from paddle.device import cuda
