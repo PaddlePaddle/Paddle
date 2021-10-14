d37c1d1bac1f8beb593c54675070430f7c4ee619
zhaocaibei123 <zhaocaibei@baidu.com>
2021-10-14 07:49:31 +0000

    add ctr table depends

--- paddle/fluid/distributed/table/depends/sparse_utils.h
+++ paddle/fluid/distributed/table/depends/sparse_utils.h
@@ -31,8 +31,8 @@ struct PullSparseValue {
         feasigns_(nullptr),
         frequencies_(nullptr) {}
 
-  explicit PullSparseValue(std::vector<uint64_t> feasigns,
-                           std::vector<uint32_t> frequencies, int dim) {
+  explicit PullSparseValue(std::vector<uint64_t>& feasigns,
+                           std::vector<uint32_t>& frequencies, int dim) {
     numel_ = feasigns.size();
     dim_ = dim;
     is_training_ = true;
