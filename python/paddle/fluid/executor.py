41a96b37e56dca88fe4cb48fe4c9bd416469da39
zmx <zmxdream@pku.edu.cn>
2021-10-17 13:19:36 +0800

    fix  _run_from_dataset
    set fetch_info = None in _run_from_dataset

--- python/paddle/fluid/executor.py
+++ python/paddle/fluid/executor.py
@@ -1667,6 +1667,7 @@ class Executor(object):
                         'op_role',
                         core.op_proto_and_checker_maker.OpRole.Optimize)
             fetch_list = None
+            fetch_info = None
 
         scope, trainer = self._prepare_trainer(
             program=program,
