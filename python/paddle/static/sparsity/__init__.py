22336af54f4fafe1ab999ed64006d6e7df1d0403
Ming Huang <mingh@nvidia.com>
2021-10-19 10:13:04 +0000

    Code format fix

--- python/paddle/static/sparsity/__init__.py
+++ python/paddle/static/sparsity/__init__.py
@@ -13,17 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ...fluid.contrib.sparsity import calculate_density #noqa: F401
-from ...fluid.contrib.sparsity import check_mask_1d #noqa: F401
-from ...fluid.contrib.sparsity import get_mask_1d #noqa: F401
-from ...fluid.contrib.sparsity import check_mask_2d #noqa: F401
-from ...fluid.contrib.sparsity import get_mask_2d_greedy #noqa: F401
-from ...fluid.contrib.sparsity import get_mask_2d_best #noqa: F401
-from ...fluid.contrib.sparsity import create_mask #noqa: F401
-from ...fluid.contrib.sparsity import check_sparsity #noqa: F401
-from ...fluid.contrib.sparsity import MaskAlgo #noqa: F401
-from ...fluid.contrib.sparsity import CheckMethod #noqa: F401
-from ...fluid.contrib.sparsity import decorate #noqa: F401
-from ...fluid.contrib.sparsity import prune_model #noqa: F401
-from ...fluid.contrib.sparsity import set_excluded_layers #noqa: F401
-from ...fluid.contrib.sparsity import reset_excluded_layers #noqa: F401
+from ...fluid.contrib.sparsity import calculate_density  #noqa: F401
+from ...fluid.contrib.sparsity import check_mask_1d  #noqa: F401
+from ...fluid.contrib.sparsity import get_mask_1d  #noqa: F401
+from ...fluid.contrib.sparsity import check_mask_2d  #noqa: F401
+from ...fluid.contrib.sparsity import get_mask_2d_greedy  #noqa: F401
+from ...fluid.contrib.sparsity import get_mask_2d_best  #noqa: F401
+from ...fluid.contrib.sparsity import create_mask  #noqa: F401
+from ...fluid.contrib.sparsity import check_sparsity  #noqa: F401
+from ...fluid.contrib.sparsity import MaskAlgo  #noqa: F401
+from ...fluid.contrib.sparsity import CheckMethod  #noqa: F401
+from ...fluid.contrib.sparsity import decorate  #noqa: F401
+from ...fluid.contrib.sparsity import prune_model  #noqa: F401
+from ...fluid.contrib.sparsity import set_excluded_layers  #noqa: F401
+from ...fluid.contrib.sparsity import reset_excluded_layers  #noqa: F401
