4c5a2e1921737cf40764757b09c53a5c5e0b8c9f
ForFishes <1422485404@qq.com>
2021-10-19 06:59:17 +0000

    rm cast for moe

--- paddle/fluid/operators/expert_count_op.cu
+++ paddle/fluid/operators/expert_count_op.cu
@@ -24,45 +24,20 @@ namespace operators {
 #define PERTHREAD_EXPERTS 256
 #define WARP_SIZE 32
 
-<<<<<<< HEAD
-const int CUDA_NUM_THREADS = 512;
-static inline int GET_BLOCKS(const int N) {
-  return (N + CUDA_NUM_THREADS - 1) / CUDA_NUM_THREADS;
-}
-
-=======
->>>>>>> 80b7f7a... add expert_count_op and limit_by_capacity_op
 using LoDTensor = framework::LoDTensor;
 using Tensor = framework::Tensor;
 
 template <typename T>
-<<<<<<< HEAD
-__global__ void initialize_zero_kernel(T* data, const int length) {
-  CUDA_KERNEL_LOOP(idx, length) { data[idx] = static_cast<T>(0); }
-}
-
-template <typename T>
-__global__ void ExpertCount(const T* gate_idx, T* expert_count,
-                            int64_t batch_size, int n_expert) {
-  int res_tmp[PERTHREAD_EXPERTS] = {0};
-  int expert_min = blockIdx.x * PERTHREAD_EXPERTS;
-  int expert_max = expert_min + PERTHREAD_EXPERTS;
-=======
 __global__ void expert_count_impl(const T* gate_idx, int* expert_count,
                                   int64_t batch_size, int n_expert) {
   int res_tmp[PERTHREAD_EXPERTS] = {0};
   int64_t expert_min = blockIdx.x * PERTHREAD_EXPERTS;
   int64_t expert_max = expert_min + PERTHREAD_EXPERTS;
->>>>>>> 80b7f7a... add expert_count_op and limit_by_capacity_op
   if (expert_max > n_expert) {
     expert_max = n_expert;
   }
   for (int i = threadIdx.x; i < batch_size; i += blockDim.x) {
-<<<<<<< HEAD
-    T idx = gate_idx[i];
-=======
     int64_t idx = gate_idx[i];
->>>>>>> 80b7f7a... add expert_count_op and limit_by_capacity_op
     if (idx == -1) {
       continue;
     }
@@ -78,7 +53,7 @@ __global__ void expert_count_impl(const T* gate_idx, int* expert_count,
       x = x + __shfl_down_sync(-1u, x, j);
     }
     if (threadIdx.x % WARP_SIZE == 0) {
-      atomicAdd(expert_count + i, x);
+      platform::CudaAtomicAdd(expert_count + i, x);
     }
   }
 }
@@ -87,38 +62,20 @@ template <typename T>
 class ExpertCountOpCUDAKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& context) const override {
-<<<<<<< HEAD
-    auto gate_idx = context.Input<LoDTensor>("gate_idx");
-    auto n_expert = context.Attr<int>("n_expert");
-    auto expert_count = context.Output<LoDTensor>("Out");
-
-    int64_t batch_size = gate_idx->numel();
-=======
     auto gate_idx = context.Input<Tensor>("gate_idx");
     auto n_expert = context.Attr<int>("n_expert");
     auto expert_count = context.Output<Tensor>("Out");
 
     auto batch_size = gate_idx->numel();
->>>>>>> 80b7f7a... add expert_count_op and limit_by_capacity_op
     auto place = context.GetPlace();
     const auto& dev_ctx =
         context.template device_context<platform::CUDADeviceContext>();
 
     framework::DDim out_dims = framework::make_ddim({n_expert});
-<<<<<<< HEAD
-    auto out_data = expert_count->mutable_data<T>(out_dims, place);
-    const T* gate_data = gate_idx->data<T>();
-
-    initialize_zero_kernel<
-        T><<<GET_BLOCKS(n_expert), CUDA_NUM_THREADS, 0, dev_ctx.stream()>>>(
-        out_data, n_expert);
-    ExpertCount<
-=======
     auto out_data = expert_count->mutable_data<int>(out_dims, place);
     const T* gate_data = gate_idx->data<T>();
 
     expert_count_impl<
->>>>>>> 80b7f7a... add expert_count_op and limit_by_capacity_op
         T><<<CEIL(n_expert, PERTHREAD_EXPERTS), 256, 0, dev_ctx.stream()>>>(
         gate_data, out_data, batch_size, n_expert);
   }
@@ -130,9 +87,5 @@ class ExpertCountOpCUDAKernel : public framework::OpKernel<T> {
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
 
-<<<<<<< HEAD
-REGISTER_OP_CUDA_KERNEL(expert_count, ops::ExpertCountOpCUDAKernel<int>)
-=======
 REGISTER_OP_CUDA_KERNEL(expert_count, ops::ExpertCountOpCUDAKernel<int>,
                         ops::ExpertCountOpCUDAKernel<int64_t>);
->>>>>>> 80b7f7a... add expert_count_op and limit_by_capacity_op
