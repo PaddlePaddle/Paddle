8dca05425afa46fdd574fd466dd1a463baa13886
Ming Huang <mingh@nvidia.com>
2021-10-20 01:58:55 +0000

    Fixed code example errors.

--- python/paddle/fluid/contrib/sparsity/utils.py
+++ python/paddle/fluid/contrib/sparsity/utils.py
@@ -446,7 +446,7 @@ def get_mask_2d_best(mat, n, m):
                           [5, 6, 3, 9],
                           [2, 4, 6, 9]])
           mask_greedy = sparsity.get_mask_2d_greedy(mat, 2, 4)
-          mask_greedy = sparsity.get_mask_2d_best(mat, 2, 4)
+          mask_best = sparsity.get_mask_2d_best(mat, 2, 4)
           print("L1 norm of `greedy` sparse matrix", np.multiply(mat, mask_greedy).sum()) # 56
           print("L1 norm of `best` sparse matrix", np.multiply(mat, mask_best).sum()) # 61
     """
