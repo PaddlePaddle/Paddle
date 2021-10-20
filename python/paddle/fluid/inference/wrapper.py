22628716096789bc61af46ab4f87e14ac0028a0e
Steffy-zxf <48793257+Steffy-zxf@users.noreply.github.com>
2021-10-20 14:55:14 +0800

    Add FasterTokenizer Operator (#34491)
    Add Tokenizer related functionalities for Transformer model in order that the process of training and predicting is consistent.
    
    * support the text string as an input Tensor
    * support the "VOCAB"unordered_map<wstring, int> as an input Tensor to lookup tokens
    * Tokenizer used for BERT. This tokenizer applies an end-to-end, text string to wordpiece tokenization.
    * It first applies basic tokenization, followed by wordpiece tokenization.

--- python/paddle/fluid/inference/wrapper.py
+++ python/paddle/fluid/inference/wrapper.py
@@ -21,3 +21,20 @@ PrecisionType = AnalysisConfig.Precision
 Config = AnalysisConfig
 Tensor = PaddleInferTensor
 Predictor = PaddleInferPredictor
+
+
+def tensor_copy_from_cpu(self, data):
+    '''
+    Support input type check based on tensor.copy_from_cpu.
+    '''
+    if isinstance(data, np.ndarray) or (isinstance(data, list) and
+                                        len(data) > 0 and
+                                        isinstance(data[0], str)):
+        self.copy_from_cpu_bind(data)
+    else:
+        raise TypeError(
+            "In copy_from_cpu, we only support numpy ndarray and list[str] data type."
+        )
+
+
+Tensor.copy_from_cpu = tensor_copy_from_cpu
