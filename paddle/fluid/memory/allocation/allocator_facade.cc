b82a059a62003403a7c76b649c857463a7dba301
Wang Huan <wanghuan29@baidu.com>
2021-10-22 10:14:47 +0000

    refine, test=develop

--- paddle/fluid/memory/allocation/allocator_facade.cc
+++ paddle/fluid/memory/allocation/allocator_facade.cc
@@ -56,7 +56,7 @@ PADDLE_DEFINE_EXPORTED_bool(
     "Whether to use system allocator to allocate CPU and GPU memory. "
     "Only used for unittests.");
 
-PADDLE_DEFINE_EXPORTED_bool(use_virtual_memory_auto_growth, true,
+PADDLE_DEFINE_EXPORTED_bool(use_virtual_memory_auto_growth, false,
                             "Use VirtualMemoryAutoGrowthBestFitAllocator.");
 
 DECLARE_string(allocator_strategy);
