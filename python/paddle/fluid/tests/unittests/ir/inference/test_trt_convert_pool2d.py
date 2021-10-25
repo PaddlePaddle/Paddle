e01369edcdf10bf884e03c047516ced25abe506b
jiweibo <jiweibo@baidu.com>
2021-10-25 13:17:02 +0000

    add pool2d

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_pool2d.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_pool2d.py
@@ -18,6 +18,7 @@ import numpy as np
 import paddle.inference as paddle_infer
 from functools import partial
 from typing import Optional, List, Callable, Dict, Any, Set
+import unittest
 
 
 class TrtConvertPool2dTest(TrtLayerAutoScanTest):
@@ -32,6 +33,10 @@ class TrtConvertPool2dTest(TrtLayerAutoScanTest):
                 for index in range(len(ksize)):
                     if ksize[index] <= paddings[index]:
                         return False
+        ver = paddle_infer.get_trt_compile_version()
+        if ver[0] * 1000 + ver[1] * 100 + ver[0] * 10 < 7000:
+            if program_config.ops[0].attrs['pooling_type'] == 'avg':
+                return False
         return True
 
     def is_program_valid(self, program_config: ProgramConfig) -> bool:
@@ -156,6 +161,28 @@ class TrtConvertPool2dTest(TrtLayerAutoScanTest):
         self.add_skip_case(
             teller2, SkipReasons.TRT_NOT_IMPLEMENTED,
             "It is not support that global_pooling is true for trt now.")
+
+    def teller3(program_config, predictor_config):
+        if self.dynamic_shape.min_input_shape == {} and program_config.ops[
+                0].attrs['ceil_mode'] == True:
+            return True
+        return False
+
+    self.add_skip_case(
+        teller3, SkipReasons.TRT_NOT_IMPLEMENTED,
+        "It is not support that ceil_mode is true in static mode for trt now.")
+
+    def teller4(program_config, predictor_config):
+        if self.dynamic_shape.min_input_shape != {} and (
+                program_config.ops[0].attrs['strides'] == [1, 2] or
+                program_config.ops[0].attrs['strides'] == [2, 2]):
+            return True
+        return False
+
+    self.add_skip_case(
+        teller4, SkipReasons.TRT_NOT_IMPLEMENTED,
+        "It is not support that strides is not equal [1, 1] in dynamic mode for trt now."
+    )
 
     def test(self):
         self.add_skip_trt_case()
