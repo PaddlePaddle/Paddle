40c376f9cea073a48d91d4b50cda4909f0a4ca00
Tongxin Bai <waffle.bai@gmail.com>
2021-10-25 09:40:45 +0000

    [BFGS]: add utils, re-organize code structure.

--- python/paddle/optimizer/functional/bfgs.py
+++ python/paddle/optimizer/functional/bfgs.py
@@ -15,40 +15,11 @@
 import contextlib
 import paddle
 from ...autograd import vjp
+from .bfgs_utils import as_float_tensor, make_state, update_state
+from .bfgs_utils import any_active, active, converged, failed
+from .bfgs_utils import vnorm_inf
 from .linesearch import hz_linesearch as linesearch
 
-def as_float_or_double_tensor(input, dtype=None):
-    r"""Generates a float or double typed tensor from `input`. The data
-    type of `input` is either float or double.
-
-    Args:
-        input(Scalar | List | Tensor): a scalar or a list of floats, or 
-            a tensor with dtype float or double.
-        dtype('float' | 'float32' | 'float64' | 'double', Optional): the data
-            type of the result tensor. The default value is None.
-
-    Returns:
-        A tensor with the required dtype.
-    """
-    assert isinstance(input, (float, list, paddle.Tensor)), (
-        f'Input `{input}` should be float, list or paddle.Tensor but found 
-        f'{type(input)}.'
-    )
-
-    if dtype in ('float', 'float32'):
-        dtype = 'float32'
-    elif dtype in ['float64', 'double']:
-        dtype = 'float64'
-    else:
-        assert dtype is None
-    try:
-        output = paddle.to_tensor(input, dtype=dtype)
-        if output.dtype not in paddle.float32, paddle.float64:
-            raise TypeError
-    except TypeError:
-        raise TypeError(f'The data type of {input} is {type(input)}, which is not supported.')
-    
-    return output
 
 def verify_symmetric_positive_definite_matrix(H):
     r"""
@@ -77,17 +48,6 @@ def verify_symmetric_positive_definite_matrix(H):
         f"(Batched) matrix {H} is not symmetric."
     )
 
-def vnorm_p(x, p=2):
-    r"""p vector norm."""
-    return paddle.norm(x, p=p, axis=-1)
-
-def vnorm_inf(x):
-    r"""Infinum vector norm."""
-    return paddle.norm(x, p='inf', axis=-1)
-
-def matnorm(x):
-    r"""Matrix norm."""
-    return paddle.norm(x, 'fro')
 
 class BFGS_State(object):
     r"""
@@ -116,22 +76,16 @@ class BFGS_State(object):
         self.k = k
         self.nf = nf
         self.ng = ng
-        self.state = paddle.zeros_like(fk, dtype='int8')
-
-def _any_active(state):
-    return paddle.any(state == 0)
-
-def _converged(state):
-    return state == 1
-
-def _failed(state):
-    return state == 2
+        self.state = make_state(fk)
+        self._StateConverged = make_state(fk, 'converged')
+        self._StateFailed = make_state(fk, 'failed')
 
 def iterates(func,
              x0, 
              dtype='float',
              H0=None,
              gtol=1e-8,
+             xtol=0,
              iters=50,
              ls_iters=50):
     r"""
@@ -167,12 +121,12 @@ def iterates(func,
     Returns:
         A generator which returns a BFGS_State per iteration.
     """
-    x0 = as_float_or_double_tensor(x0, dtype)
+    # Proprocesses inputs and control parameters 
+    x0 = as_float_tensor(x0, dtype)
     dtype = x0.dtype
 
-    # Tensorizes control parameters
-    gtol = as_float_or_double_tensor(gtol, dtype)
-    xtol = as_float_or_double_tensor(xtol, dtype)
+    gtol = as_float_tensor(gtol, dtype)
+    xtol = as_float_tensor(xtol, dtype)
 
     # Evaluates the starting points
     f0, g0 = vjp(func, x0)
@@ -180,43 +134,34 @@ def iterates(func,
     # If function is applied to batched input, the last axis of the input
     # tensor holds the input dimensions.
     input_dim = x0.shape[-1]
-    hessian_shape = input_shape + [input_dim]
+    hessian_shape = x0.shape + [input_dim]
     
     # The initial approximation of the inverse Hessian.
     if H0 is None:
         I = paddle.eye(input_dim, dtype=dtype)
         H0 = paddle.broadcast_to(I, hessian_shape)
     else:
-        H0 = as_float_or_double_tensor(H0, dtype)        
+        H0 = as_float_tensor(H0, dtype)        
         verify_symmetric_positive_definite_matrix(H0)
 
     # Puts the starting points in the initial state and kicks off the
     # minimization process.
     gnorm = vnorm_inf(g0)
     state = BFGS_State(x0, f0, g0, H0)
-    
-    # Builds tensor boilers for later use as one of the where arguments.
-    converged_boiler = paddle.ones_like(f0, dtype='int8')
-    failed_boiler = converged_boiler + 1
 
-    while _any_active(state.state) and state.k < iters:
-        # Performs line search
-        ls, alpha_k = linesearch(state,
-                                 func, 
-                                 gtol=gtol,
-                                 xtol=xtol,
-                                 max_iters=ls_iters)
+    while any_active(state.state) and state.k < iters:
+        # Performs line search and updates the state 
+        linesearch(state,
+                   func, 
+                   gtol=gtol,
+                   xtol=xtol,
+                   max_iters=ls_iters)
         
         # Calculates the gradient norms
         gnorm = vnorm_inf(state.gk)
-        
-        # Newly converged are the active batch elements whose gradient norms are
-        # smaller than the tolerance value. 
-        new_converged = paddle.logical_and(_active(state.state), gnorm < gtol)
-        
+
         # Updates the state tensor on the newly converged elements.
-        state.state = paddle.where(new_converged, converged_boiler, state.state)
-       
+        state.state = update_state(state.state, gnorm < gtol, 'converged')
         
 
         
---
+++ python/paddle/optimizer/functional/bfgs_utils.py
@@ -0,0 +1,123 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import paddle
+
+
+def vnorm_p(x, p=2):
+    r"""p vector norm."""
+    return paddle.norm(x, p=p, axis=-1)
+
+def vnorm_inf(x):
+    r"""Infinum vector norm."""
+    return paddle.norm(x, p='inf', axis=-1)
+
+def matnorm(x):
+    r"""Matrix norm."""
+    return paddle.norm(x, 'fro')
+
+def any_active(state):
+    return paddle.any(state == 0)
+
+def active(state):
+    return state == 0
+
+def converged(state):
+    return state == 1
+
+def failed(state):
+    return state == 2
+
+def make_state(tensor_like, value='active'):
+    r"""Makes BFGS state tensor. Default is all zeros.
+    
+    args:
+        tensor_like (Tensor): provides the shape of the result tensor.
+        value (str, optional): indicates the default value of the result
+            tensor. If `value` is 'active' then the result tensor is all
+            zeros. If `value` is 'converged' then the result tensor is all
+            ones. If `value` is 'failed' then the result tensor is all twos.
+            Default value is 'active'.
+
+    Returns:
+        Tensor wiht the same shape of `tensor_like`, of dtype `int8`.
+    """
+    if value is 'active':
+        state = paddle.zeros_like(tensor_like, dtype='int8')
+    elif value is 'converged':
+        state = paddle.ones_like(tensor_like, dtype='int8')
+    else:
+        assert value is 'failed':
+        state = paddle.ones_like(tensor_like, dtype='int8') + 1
+    
+    return state
+
+def update_state(input_state, predicate, new_state)
+    r"""Change some BFGS states to new specified state.
+    
+    Updates the state on the locations where the old value is 0 and 
+    corresponding predicate is True.
+
+    Args:
+        input_state (Tensor): the original state tensor.
+        predicate (Tensor): a tensor with the same shape of `input_state`, of 
+        boolean type, indicating which locations should be updated.
+        new_state (str): specifies the new state, either 'converged' or 
+        'failed'.
+
+    Returns:
+        Tensor updated on the specified locations.
+    """
+    assert new_state in 'converged', 'failed'
+        
+    if new_state is 'converge':
+        increments = paddle.to_tensor(predicate, dtype='int8')
+    else:
+        increments = paddle.to_tensor(predicate, dtype='int8') * 2
+    
+    output_state = paddle.where(state == 0, increments, state)
+    return output_state
+
+def as_float_tensor(input, dtype=None):
+    r"""Generates a float or double typed tensor from `input`. The data
+    type of `input` is either float or double.
+
+    Args:
+        input(Scalar | List | Tensor): a scalar or a list of floats, or 
+            a tensor with dtype float or double.
+        dtype('float' | 'float32' | 'float64' | 'double', Optional): the data
+            type of the result tensor. The default value is None.
+
+    Returns:
+        A tensor with the required dtype.
+    """
+    assert isinstance(input, (float, list, paddle.Tensor)), (
+        f'Input `{input}` should be float, list or paddle.Tensor but found 
+        f'{type(input)}.'
+    )
+
+    if dtype in ('float', 'float32'):
+        dtype = 'float32'
+    elif dtype in ['float64', 'double']:
+        dtype = 'float64'
+    else:
+        assert dtype is None
+    try:
+        output = paddle.to_tensor(input, dtype=dtype)
+        if output.dtype not in paddle.float32, paddle.float64:
+            raise TypeError
+    except TypeError:
+        raise TypeError(f'The data type of {input} is {type(input)}, which is not supported.')
+    
+    return output
