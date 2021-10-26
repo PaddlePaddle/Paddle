25e38a83dfd4172c0b806caefc080d95bbbee0b8
zmxdream <zmxdream@pku.edu.cn>
2021-10-26 10:49:17 +0800

    update

--- python/paddle/fluid/communicator.py
+++ python/paddle/fluid/communicator.py
@@ -189,8 +189,7 @@ class LargeScaleKV(object):
 
 class HeterClient(object):
     def __init__(self, endpoint, previous_endpoint, cur_endpoint, trainer_id):
-        self.heter_client_ = core.HeterClient(endpoint, previous_endpoint,
-                                              cur_endpoint, trainer_id)
+        self.heter_client_ = core.HeterClient(endpoint, previous_endpoint, trainer_id)
 
     def stop(self):
         self.heter_client_.stop()
