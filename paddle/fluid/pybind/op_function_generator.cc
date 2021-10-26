edff5b7975dcbf5ed6996f952de79be8ca15b49a
Steffy-zxf <48793257+Steffy-zxf@users.noreply.github.com>
2021-10-26 20:27:08 +0800

    [Cherry-pick] Add FasterTokenizer Operator (#36716)
    * Add FasterTokenizer Operator (#34491)
    
    Add Tokenizer related functionalities for Transformer model in order that the process of training and predicting is consistent.
    
    * support the text string as an input Tensor
    * support the "VOCAB"unordered_map<wstring, int> as an input Tensor to lookup tokens
    * Tokenizer used for BERT. This tokenizer applies an end-to-end, text string to wordpiece tokenization.
    * It first applies basic tokenization, followed by wordpiece tokenization.
    
    * optimize fast tokenizer
    
    * remove const_cast
    
    Co-authored-by: zhoushunjie <zhoushunjie@baidu.com>
    Co-authored-by: wawltor <fangzeyang0904@hotmail.com>

--- paddle/fluid/pybind/op_function_generator.cc
+++ paddle/fluid/pybind/op_function_generator.cc
@@ -72,6 +72,7 @@ std::map<std::string, std::set<std::string>> op_ins_map = {
     {"sparse_momentum", {"Param", "Grad", "Velocity", "Index", "LearningRate"}},
     {"rnn", {"Input", "PreState", "WeightList", "SequenceLength"}},
     {"run_program", {"X", "Params"}},
+    {"faster_tokenizer", {"Text", "Vocab", "TextPair"}},
     {"fused_feedforward",
      {"Dropout1Seed", "Dropout2Seed", "Linear1Bias", "Linear2Bias", "Ln1Scale",
       "Ln1Bias", "Ln2Scale", "Ln2Bias"}},
