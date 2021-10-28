6d1cc1c319d93452b74572741fb87afa81f86b98
zhaoyingli <zhaoyingli@baidu.com>
2021-10-28 10:26:48 +0800

    param auto convert

--- python/paddle/distributed/auto_parallel/utils.py
+++ python/paddle/distributed/auto_parallel/utils.py
@@ -19,6 +19,8 @@ import numpy as np
 
 import warnings
 import logging
+from functools import reduce
+
 import paddle.fluid.core as core
 from paddle.fluid.io import is_parameter, is_belong_to_optimizer
 from paddle.framework.io import _to_LodTensor
@@ -429,8 +431,7 @@ def save_distributed_checkpoint(program,
         logging.info("Already save model to {}".format(checkpoint_path))
 
         if dist_attr_path:
-            raise NotImplementedError(
-                "Save distributed attribute has not been implemented.")
+            save_distributed_attribute(program, dist_attr_path)
     else:
         # TODO: integrate param before save
         raise NotImplementedError(
@@ -465,9 +466,53 @@ def load_distributed_checkpoint(checkpoint_path,
     dist_attr_path = _check_valid_path(dist_attr_path)
 
     if checkpoint_path and dist_attr_path:
-        raise NotImplementedError(
-            "Merge&Slice parameter with dist_attr has not been implemented.")
+        # integrate param value to 'total_state_dict'
+        total_state_dict = {}
+        for ckpt_file in checkpoint_path:
+            state_dict_info = paddle.load(ckpt_file)
+            pre_ranks = state_dict_info["ranks"]
+            assert pre_ranks == len(checkpoint_path), \
+                "The number of checkpoint_path must equal to the number of last training ranks."
+            assert pre_ranks == len(dist_attr_path), \
+                "The number of distributed attribute file should equal to the number of last training ranks."
 
+            for name, value in state_dict_info["model"].items():
+                if name in total_state_dict:
+                    total_state_dict[name].append(value)
+                else:
+                    total_state_dict[name] = [value]
+
+        param_not_in_dist_attr = []
+        param_not_in_program = []
+        total_pre_dist_attr = load_distributed_attribute(dist_attr_path)
+        total_cur_dist_attr = _get_dist_attr(program)
+        for name in total_pre_dist_attr:
+            if name not in total_cur_dist_attr:
+                param_not_in_program.append(name)
+
+        logging.info("Start merge and slice parameter")
+        state_dict = {}
+        for var in program.list_vars():
+            if is_parameter(var) or is_belong_to_optimizer(var):
+                if var.name not in total_pre_dist_attr:
+                    param_not_in_dist_attr.append(var.name)
+                    continue
+
+                pre_slice_param = total_state_dict[var.name]
+                pre_dist_attr = total_pre_dist_attr[var.name]
+                cur_dist_attr = total_cur_dist_attr[var.name]
+                cur_slice_param = _merge_and_slice_parameter(
+                    pre_slice_param, pre_dist_attr, cur_dist_attr)
+                state_dict[var.name] = cur_slice_param
+
+        if param_not_in_dist_attr:
+            warnings.warn(
+                "Parameters '{}' are not found in pre dist_attr."\
+                    .format(str(param_not_in_dist_attr)))
+        if param_not_in_program:
+            warnings.warn(
+                "Parameters '{}' are not found in current program."\
+                    .format(str(param_not_in_program)))
     elif checkpoint_path:
         assert len(checkpoint_path) == paddle.distributed.get_world_size(), \
             "The number of checkpoint_path must equal to the number of ranks"
@@ -484,3 +529,284 @@ def load_distributed_checkpoint(checkpoint_path,
         return
 
     return state_dict_info["addition_info"]
+
+
+def save_distributed_attribute(program, dist_attr_path):
+    """
+    Save distributed attribute of all parameters
+    """
+    # TODO: just save a complete distributed attribute file
+    rank = paddle.distributed.get_rank()
+    dist_attr_name = os.path.join(dist_attr_path,
+                                  "dist_attr_rank{}.pdattr".format(rank))
+
+    dist_attr_dict = _get_dist_attr(program)
+    paddle.save(dist_attr_dict, dist_attr_name)
+    logging.info("Already save distributed attribute to {}".format(
+        dist_attr_path))
+
+
+def load_distributed_attribute(dist_attr_path):
+    """
+    Load distributed attribute of all parameters
+    """
+    total_dist_attr = {}
+    for dist_attr_file in dist_attr_path:
+        dist_attr = paddle.load(dist_attr_file)
+        for name in dist_attr:
+            if name not in total_dist_attr:
+                total_dist_attr[name] = dist_attr[name]
+
+    return total_dist_attr
+
+
+def _get_dist_attr(program):
+    """
+    Get distributed attribute of current rank
+    """
+    from .context import get_default_distributed_context
+
+    dist_context = get_default_distributed_context()
+    dist_attr = {}
+    for var in program.list_vars():
+        if is_parameter(var) or is_belong_to_optimizer(var):
+            tensor_dist_attr = dist_context.get_tensor_distributed_attr_for_program(
+                var)
+            process_mesh = tensor_dist_attr.get_process_mesh()
+            dims_mapping = tensor_dist_attr.get_dims_mapping()
+
+            dist_attr[var.name] = {
+                "process_shape": process_mesh.topology,
+                "process_group": process_mesh.process_group,
+                "dims_mapping": dims_mapping
+            }
+
+    return dist_attr
+
+
+def _merge_and_slice_parameter(slice_param_list, pre_dist_attr, cur_dist_attr):
+    """
+    Merge parameters with previous dist_attr and slice parameters with current dist_attr
+
+    Arags:
+        slice_param_list(list[LodTensor]): a parameter's value of all ranks
+        pre_dist_attr(dict): a parameter's dist_attr of last training
+        cur_dist_attr(dict): a parameter's dist_attr of current rank
+
+    Returns:
+        sliced parameter(LodTensor) of current rank
+    """
+    from .reshard import _compute_complete_shape, _compute_partition_index
+
+    pre_dims_mapping = pre_dist_attr["dims_mapping"]
+    pre_process_shape = pre_dist_attr["process_shape"]
+    pre_process_group = pre_dist_attr["process_group"]
+
+    cur_dims_mapping = cur_dist_attr["dims_mapping"]
+    cur_process_shape = cur_dist_attr["process_shape"]
+    cur_process_group = cur_dist_attr["process_group"]
+
+    # get the complete shape of parameter
+    slice_params = [np.array(param) for param in slice_param_list]
+    complete_shape = _compute_complete_shape(
+        slice_params[0].shape, pre_process_shape, pre_dims_mapping)
+
+    # merge parameter with previous dist_attr
+    partition_param_list = []
+    for process in pre_process_group:
+        partition_index = _compute_partition_index(
+            process, complete_shape, pre_dims_mapping, pre_process_shape,
+            pre_process_group)
+        index = pre_process_group.index(process)
+        _merge_parameter(partition_param_list, slice_params[index],
+                         partition_index)
+    assert len(partition_param_list) == 1 or not partition_param_list, \
+        "Fail to merge parameter"
+
+    # slice parameter with current dist_attr
+    complete_param = partition_param_list[0][0]
+    partition_index_list = _get_split_indices(
+        complete_param.shape, cur_dims_mapping, cur_process_shape,
+        cur_process_group)
+    slice_param_list = _slice_parameter(complete_param, partition_index_list,
+                                        len(partition_index_list))
+
+    # get the current param index in slice_param_list
+    rank = paddle.distributed.get_rank()
+    slice_param_index = _partition_index_to_slice_param_index(
+        rank, complete_param.shape, cur_dims_mapping, cur_process_shape,
+        cur_process_group)
+
+    return _to_LodTensor(slice_param_list[slice_param_index])
+
+
+def _merge_parameter(partition_param_list, param, partition_index):
+    """
+    Merge partitial parameters to a complete one.
+
+    Args:
+        partition_param_list(list(tuple))
+        param(numpy.ndarray)
+        partition_index(list)
+
+    Returns:
+        None
+
+    Examples:
+        .. code-block:: python
+
+            import numpy as np
+            partition_param_list = [(np.array([[[1.11, 1.12]]]), [[0,1],[0,1],[0,2]])]
+            param = np.array([[[1.13, 1.14]]])
+            partition_index = [[0,1],[0,1],[2,4]]
+
+            _merge_parameter(partition_param_list, param, partition_index)
+            # partition_param_list: [(np.array([[[1.11, 1.12, 1.13, 1.14]]]), [[0,1],[0,1],[0,4]])]
+    """
+    from .reshard import _compute_concat_info
+
+    if not partition_param_list:
+        partition_param_list.append((param, partition_index))
+    else:
+        i = 0
+        has_concat = False
+        while i < len(partition_param_list):
+            concat_axis, first_order, new_partition = _compute_concat_info(
+                partition_param_list[i][1], partition_index)
+            if concat_axis != -1:
+                has_concat = True
+                new_param = np.concatenate((partition_param_list[i][0], param), axis=concat_axis) \
+                    if first_order == 0 else np.concatenate((param, partition_param_list[i][0]), axis=concat_axis)
+
+                partition_param_list.pop(i)
+                _merge_parameter(partition_param_list, new_param, new_partition)
+                break
+            i += 1
+
+        if not has_concat:
+            need_append = True
+            for i in range(len(partition_param_list)):
+                if partition_index == partition_param_list[i][1]:
+                    need_append = False
+                    break
+            if need_append:
+                partition_param_list.append((param, partition_index))
+
+
+def _slice_parameter(complete_param, partition_index_list, length):
+    """
+    Slice complete parameter with current dist_attr.
+
+    Returns:
+        slice_param_list(list)
+
+    Examples:
+        .. code-block:: python
+
+            import numpy as np
+            complete_param = np.array([[[1.11, 1.12, 1.13, 1.14, 1.15, 1.16]]])
+            rank = 2
+            complete_shape = [1, 1, 6]
+            dims_mapping = [-1, -1, 0]
+            process_shape = [3]
+            process_group = [0, 1, 2]
+
+            slice_param_list = _slice_parameter(complete_param, [[], [], [2, 4]], 3)
+            # [array([[[1.11, 1.12]]]), array([[[1.13, 1.14]]]), array([[[1.15, 1.16]]])]
+    """
+    slice_param_list = []
+    axis = len(complete_param.shape) - length
+    slice_param = np.split(
+        complete_param, partition_index_list[axis], axis=axis)
+    if length == 1:
+        return slice_param
+    for param in slice_param:
+        slice_param_list.extend(
+            _slice_parameter(param, partition_index_list, length - 1))
+
+    return slice_param_list
+
+
+def _partition_index_to_slice_param_index(rank, complete_shape, dims_mapping,
+                                          process_shape, process_group):
+    """
+    Get slice param index of complete param with current rank
+
+    Returns:
+        slice_param_index(int): the index of slice param in slice_param_list
+
+    Examples:
+        .. code-block:: python
+
+            import numpy as np
+            complete_param = np.array([[[1.11, 1.12, 1.13, 1.14, 1.15, 1.16]]])
+            rank = 2
+            complete_shape = [1, 1, 6]
+            dims_mapping = [-1, -1, 0]
+            process_shape = [3]
+            process_group = [0, 1, 2]
+
+            slice_param = _slice_parameter(complete_param, [[], [], [2, 4]], 3)
+            # slice_param: 
+            # [array([[[1.11, 1.12]]]), array([[[1.13, 1.14]]]), array([[[1.15, 1.16]]])]
+
+            index = _partition_index_to_slice_param_index(rank, complete_shape, dims_mapping
+                                                            process_shape, process_group)
+            # index: 2
+    """
+    from .reshard import _compute_partition_index
+
+    partition_index = _compute_partition_index(
+        rank, complete_shape, dims_mapping, process_shape, process_group)
+
+    slice_param_index = 0
+    for i, shape in enumerate(complete_shape):
+        slice_shape = shape if dims_mapping[i] == -1 else \
+            shape // process_shape[dims_mapping[i]]
+
+        index = 0 if shape == 1 else \
+            (partition_index[i][0] + 1) // slice_shape
+
+        slice_param_index = slice_param_index * (shape // slice_shape) + index
+
+    return slice_param_index
+
+
+def _get_split_indices(complete_shape, dims_mapping, process_shape,
+                       process_group):
+    """
+    Get split indices of every dimension
+
+    Returns:
+        split_indices_list(list): the split indices of every dimension of current param
+
+    Examples:
+        .. code-block:: python
+
+            import numpy as np
+            complete_param = np.array([[[1.11, 1.12, 1.13, 1.14, 1.15, 1.16]]])
+            complete_shape = [1, 1, 6]
+            dims_mapping = [-1, -1, 0]
+            process_shape = [3]
+            process_group = [0, 1, 2]
+
+            index = _get_split_indices(complete_shape, dims_mapping, process_shape, process_group)
+            # index: [[], [], [2, 4]]
+    """
+    from .reshard import _compute_partition_index
+
+    split_indices_list = []
+    for process in process_group:
+        partition_index = _compute_partition_index(
+            process, complete_shape, dims_mapping, process_shape, process_group)
+        if split_indices_list:
+            for dim in range(len(partition_index)):
+                split_indices_list[dim].extend(partition_index[dim])
+        else:
+            split_indices_list = partition_index
+
+    split_indices_list = list(map(lambda x, y: list(set(x)-set([y])-set([0])), \
+                                                split_indices_list, complete_shape))
+    split_indices_list = [sorted(x) for x in split_indices_list]
+
+    return split_indices_list
