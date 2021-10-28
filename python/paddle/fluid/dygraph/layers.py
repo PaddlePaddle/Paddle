5ae9546ad26125ed93f049954142358100840988
zhangbo9674 <zhangbo54@baidu.com>
2021-10-28 13:37:18 +0000

    set layer.to is inplace

--- python/paddle/fluid/dygraph/layers.py
+++ python/paddle/fluid/dygraph/layers.py
@@ -122,9 +122,6 @@ class Layer(core.Layer):
         self._forward_pre_hooks = collections.OrderedDict()
         self._forward_post_hooks = collections.OrderedDict()
 
-        self._parameters_transform_map = {}
-        self._buffers_transform_map = {}
-
         self._casted_by_pure_fp16 = False
 
         self._state_dict_hooks = collections.OrderedDict()
@@ -1473,27 +1470,12 @@ class Layer(core.Layer):
             if param is not None:
                 with no_grad():
                     param_applied = func(param, device, dtype, blocking)
-                    assert param.is_leaf
-                    param_applied.stop_gradient = param.stop_gradient
-                    if hasattr(param_applied, 'is_distributed'):
-                        param_applied.is_distributed = param.is_distributed
-                    self._parameters[key] = param_applied
                 if param.grad is not None:
                     with no_grad():
                         grad_applied = func(param._grad_ivar(), device, dtype,
                                             blocking)
-
-                        grad_applied.stop_gradient = param._grad_ivar(
-                        ).stop_gradient
-                        if hasattr(param._grad_ivar(), 'is_distributed'):
-                            grad_applied.is_distributed = param._grad_ivar(
-                            ).is_distributed
-                        self._parameters[key]._set_grad_ivar(grad_applied)
-
-            self._parameters_transform_map[id(param)] = [param_applied, key]
         for key, buf in self._buffers.items():
             self._buffers[key] = func(buf, device, dtype, blocking)
-            self._buffers_transform_map[id(buf)] = [self._buffers[key], key]
 
     def to(self, device=None, dtype=None, blocking=None):
         '''
@@ -1570,22 +1552,34 @@ class Layer(core.Layer):
                 device = t.place
             if dtype is None:
                 dtype = t.dtype
+
             if isinstance(t, framework.ParamBase):
                 helper = LayerHelper("cast", **locals())
-                new_t = helper.create_variable_for_type_inference(dtype=dtype)
+                new_dtype_t = helper.create_variable_for_type_inference(
+                    dtype=dtype)
                 if dtype is not None and dtype != t.dtype:
                     framework._dygraph_tracer().trace_op(
                         type='cast',
                         inputs={'X': t},
-                        outputs={'Out': new_t},
+                        outputs={'Out': new_dtype_t},
                         attrs={
                             'in_dtype': t.dtype,
                             'out_dtype': convert_np_dtype_to_dtype_(dtype)
                         })
             else:
                 if dtype is not None and dtype != t.dtype:
-                    new_t = t.cast(dtype=dtype)
-            return new_t
+                    new_dtype_t = t.cast(dtype=dtype)
+
+            new_deivce_dtype_t = new_dtype_t._copy_to(device, blocking)
+
+            if isinstance(t, framework.ParamBase):
+                dst_tensor = t.value().get_tensor()
+                src_tensor = new_deivce_dtype_t.value().get_tensor()
+                dst_tensor._share_data_with(src_tensor)
+            else:
+                t._share_data_with(new_deivce_dtype_t)
+
+            return new_deivce_dtype_t
 
         self._apply(transform, device, dtype, blocking)
         self._dtype = dtype
