10579297a36b86da24473d3b474ebe50f43a958b
fuqianya <fuqian_918@163.com>
2021-10-29 14:03:36 +0800

    fix CI

--- python/paddle/vision/models/squeezenet.py
+++ python/paddle/vision/models/squeezenet.py
@@ -176,7 +176,6 @@ class SqueezeNet(nn.Layer):
             x = self._conv6(x)
             x = self._conv7(x)
             x = self._conv8(x)
-
         if self.num_classes > 0:
             x = self._drop(x)
             x = self._conv9(x)
