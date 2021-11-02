e5f9ac8eab17233a78b44a07574a8b6d26462442
SmirnovKol <1945340016@qq.com>
2021-11-03 03:10:58 +0800

    add wide resnet and its test

--- python/paddle/vision/models/__init__.py
+++ python/paddle/vision/models/__init__.py
@@ -11,7 +11,6 @@
 #WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #See the License for the specific language governing permissions and
 #limitations under the License.
-
 from .resnet import ResNet  # noqa: F401
 from .resnet import resnet18  # noqa: F401
 from .resnet import resnet34  # noqa: F401
