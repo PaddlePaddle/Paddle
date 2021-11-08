2c85f1a22a023a83acd772cfae96d912436628ea
piotrekobiIntel <piotr.paturej@intel.com>
2021-11-08 10:00:00 +0100

    Fix memset errors

--- paddle/fluid/framework/hogwild_worker.cc
+++ paddle/fluid/framework/hogwild_worker.cc
@@ -97,7 +97,7 @@ void HogwildWorker::CreateThreadScope(const ProgramDesc &program) {
 template <typename T>
 void HogwildWorker::SetZero(LoDTensor *tensor, LoDTensor *root_tensor,
                             int tensor_dim) {
-  if (typeid(T) == typeid(platform::bfloat16)) {
+  if (typeid(T) != typeid(platform::bfloat16)) {
     T *ptr = tensor->mutable_data<T>(root_tensor->dims(), platform::CPUPlace());
     memset(ptr, 0, sizeof(T) * tensor_dim);
   }
