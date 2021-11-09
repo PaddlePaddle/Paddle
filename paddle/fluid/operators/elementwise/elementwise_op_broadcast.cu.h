96df1b2f8ff60c83ba1e18d8c78148c05656e6d7
niuliling123 <sdust_niull@163.com>
2021-11-09 06:32:19 +0000

    modified the elementwise_op_broadcast and elementwise_op_impl for xpu2

--- paddle/fluid/operators/elementwise/elementwise_op_broadcast.cu.h
+++ paddle/fluid/operators/elementwise/elementwise_op_broadcast.cu.h
@@ -162,6 +162,151 @@ struct DimensionsTransform {
   }
 };
 
+<<<<<<< 9c59170353fcd17b44c7de560bd56760cbd5786b
+=======
+template <typename T, int VecSize, int Rank, bool IsBoundary = false>
+__device__ __forceinline__ void LoadData(
+    T *dst, const T *__restrict__ src, uint32_t block_offset,
+    const kps::details::BroadcastConfig<Rank> &config, int numel, int num,
+    bool need_broadcast) {
+  // numel : whole num of output
+  // num: how many data will be deal with in this time
+  if (need_broadcast) {
+    kps::ReadDataBc<T, VecSize, 1, 1, Rank, IsBoundary>(dst, src, block_offset,
+                                                        config, numel);
+  } else {
+    kps::ReadData<T, VecSize, 1, 1, IsBoundary>(dst, src + block_offset, num);
+  }
+}
+
+template <typename InT, typename OutT, typename Functor, int Arity, int VecSize,
+          int Rank, bool IsBoundary = false>
+__device__ void BroadcastKernelImpl(
+    const framework::Array<const InT *__restrict__, Arity> &ins, OutT *out,
+    const framework::Array<bool, Arity> &use_broadcast, uint32_t numel,
+    const framework::Array<kps::details::BroadcastConfig<Rank>, Arity> &configs,
+    int num, int block_offset, Functor func) {
+  InT args[Arity][VecSize];
+  OutT result[VecSize];
+
+#pragma unroll
+  for (int i = 0; i < Arity; i++) {
+    kps::Init<InT, VecSize>(args[i], static_cast<InT>(1.0f));
+    LoadData<InT, VecSize, Rank, IsBoundary>(args[i], ins[i], block_offset,
+                                             configs[i], numel, num,
+                                             use_broadcast[i]);
+  }
+
+  const bool kCallElementwiseAny =
+      platform::FunctionTraits<Functor>::has_pointer_args;
+  ElementwisePrimitiveCaller<InT, OutT, VecSize, Functor, Arity,
+                             kCallElementwiseAny>()(func, args, result);
+  kps::WriteData<OutT, VecSize, 1, 1, IsBoundary>(out + block_offset, result,
+                                                  num);
+}
+
+template <typename InT, typename OutT, typename Functor, int Arity, int VecSize,
+          int Rank>
+__global__ void BroadcastKernel(
+    framework::Array<const InT *__restrict__, Arity> ins, OutT *out,
+    framework::Array<bool, Arity> use_broadcast, uint32_t numel,
+    framework::Array<kps::details::BroadcastConfig<Rank>, Arity> configs,
+    int main_offset, int tail_tid, Functor func) {
+  int block_offset = BLOCK_ID_X * BLOCK_NUM_X * VecSize;
+  int stride = BLOCK_NUM_X * GRID_NUM_X * VecSize;
+  // data offset of this block
+  for (; block_offset < main_offset; block_offset += stride) {
+    BroadcastKernelImpl<InT, OutT, Functor, Arity, VecSize, Rank, false>(
+        ins, out, use_broadcast, numel, configs, BLOCK_NUM_X * VecSize,
+        block_offset, func);
+  }
+
+  if (tail_tid) {
+    BroadcastKernelImpl<InT, OutT, Functor, Arity, VecSize, Rank, true>(
+        ins, out, use_broadcast, numel, configs, tail_tid, block_offset, func);
+  }
+}
+
+template <typename InT, typename OutT, typename Functor, int Arity, int VecSize,
+          int Rank>
+void LaunchKernel(const platform::CUDADeviceContext &ctx,
+                  const std::vector<const framework::Tensor *> &ins,
+                  framework::Tensor *out, Functor func,
+                  DimensionsTransform merge_dims) {
+  int numel = out->numel();
+  const int threads = 256;
+  int blocks = ((numel + VecSize - 1) / VecSize + threads - 1) / threads;
+
+  int main_offset = (numel / (VecSize * threads)) * VecSize * threads;
+  int tail_tid = numel % (VecSize * threads);
+  auto stream = ctx.stream();
+  OutT *out_data = out->data<OutT>();
+
+  framework::Array<kps::details::BroadcastConfig<Rank>, Arity> configs;
+  framework::Array<bool, Arity> use_broadcast;
+  framework::Array<const InT *__restrict__, Arity> ins_data;
+
+  for (int i = 0; i < Arity; i++) {
+    use_broadcast[i] = (ins[i]->numel() != numel);
+    ins_data[i] = ins[i]->data<InT>();
+    if (use_broadcast[i]) {
+      // get the broadcast config,
+      // if data shape is[m, n], then you should set data_dim = {n, m}
+      // eg: out's shape [3, 45, 1]. then out_dims = {1, 45, 3}
+      configs[i] = kps::details::BroadcastConfig<Rank>(
+          merge_dims.out_dims, merge_dims.in_dims[i], merge_dims.dim_size);
+    }
+  }
+#ifdef PADDLE_WITH_XPU2
+  threads = 128;
+  blocks = 8;
+  main_offset = (numel / (VecSize * threads)) * VecSize * threads;
+  tail_tid = numel % (VecSize * threads);
+  BroadcastKernel<InT, OutT, Functor, Arity, VecSize,
+                  Rank><<<blocks, threads, 0, stream>>>(
+      ins_data, out_data, use_broadcast, numel, configs, main_offset, tail_tid,
+      func);
+#else
+  BroadcastKernel<InT, OutT, Functor, Arity, VecSize,
+                  Rank><<<blocks, threads, 0, stream>>>(
+      ins_data, out_data, use_broadcast, numel, configs, main_offset, tail_tid,
+      func);
+#endif
+}
+
+template <typename InT, typename OutT, typename Functor, int Arity, int VecSize>
+void LaunchBroadcastKernelForDifferentVecSize(
+    const platform::CUDADeviceContext &ctx,
+    const std::vector<const framework::Tensor *> &ins, framework::Tensor *out,
+    int axis, Functor func) {
+  const auto merge_dims = DimensionsTransform(ins, out->dims(), axis);
+
+#define CALL_BROADCAST_FOR_DIM_SIZE(rank)                                     \
+  case rank: {                                                                \
+    LaunchKernel<InT, OutT, Functor, Arity, VecSize, rank>(ctx, ins, out,     \
+                                                           func, merge_dims); \
+  } break;
+
+  switch (merge_dims.dim_size) {
+    CALL_BROADCAST_FOR_DIM_SIZE(1);
+    CALL_BROADCAST_FOR_DIM_SIZE(2);
+    CALL_BROADCAST_FOR_DIM_SIZE(3);
+    CALL_BROADCAST_FOR_DIM_SIZE(4);
+    CALL_BROADCAST_FOR_DIM_SIZE(5);
+    CALL_BROADCAST_FOR_DIM_SIZE(6);
+    CALL_BROADCAST_FOR_DIM_SIZE(7);
+    CALL_BROADCAST_FOR_DIM_SIZE(8);
+    default: {
+      PADDLE_THROW(platform::errors::InvalidArgument(
+          "The maximum dimension of input tensor is expected to be less than "
+          "%d, but recieved %d.\n",
+          merge_dims.dim_size, framework::DDim::kMaxRank));
+    }
+  }
+#undef CALL_BROADCAST_FOR_DIM_SIZE
+}
+
+>>>>>>> modified the elementwise_op_broadcast and elementwise_op_impl for xpu2
 template <ElementwiseType ET, typename InT, typename OutT, typename Functor>
 void LaunchBroadcastElementwiseCudaKernel(
     const platform::CUDADeviceContext &ctx,
--- paddle/fluid/operators/elementwise/elementwise_op_impl.cu.h
+++ paddle/fluid/operators/elementwise/elementwise_op_impl.cu.h
@@ -36,7 +36,166 @@ namespace operators {
 
 namespace kps = paddle::operators::kernel_primitives;
 
+<<<<<<< 9c59170353fcd17b44c7de560bd56760cbd5786b
 using ElementwiseType = pten::ElementwiseType;
+=======
+enum ElementwiseType { kUnary = 1, kBinary = 2, kTernary = 3, kAny = -1 };
+
+/*
+* According to NVIDIA, if number of threads per block is 64/128/256/512,
+* cuda performs better. And number of blocks should be greater (at least
+* 2x~4x) than number of SMs. Hence, SM count is took into account within
+* this function to determine the right number of threads per block.
+*/
+inline int GetThreadsConfig(const platform::CUDADeviceContext &ctx,
+                            int64_t numel, int vec_size) {
+  int threads = ELEMENTWISE_BLOCK_SIZE;
+  int sm_count = ctx.GetSMCount();
+  int active_threads_num = numel / vec_size;
+  if (active_threads_num / (sm_count << 1) < ELEMENTWISE_BLOCK_SIZE) {
+    // Round up threads number into an exponential multiple of 2, while number
+    // of acitve blocks is about twice of SM, to acquire better performance.
+    threads = platform::RoundToPowerOfTwo(active_threads_num / (sm_count << 1));
+  } else if (active_threads_num / (sm_count << 2) < ELEMENTWISE_BLOCK_SIZE) {
+    // Round up threads number into an exponential multiple of 2, while number
+    // of acitve blocks is about 4 times of SM, to acquire better performance.
+    threads = platform::RoundToPowerOfTwo(active_threads_num / (sm_count << 2));
+  }
+  // Number of threads per block shall be larger than 64.
+  return std::max(64, threads);
+}
+
+template <typename InT, typename OutT>
+int GetVectorizedSizeForTensors(
+    const std::vector<const framework::Tensor *> &ins,
+    const std::vector<framework::Tensor *> &outs) {
+  int vec_size = 4;
+  for (auto iter = ins.begin(); iter != ins.end(); ++iter) {
+    vec_size = std::min<int>(vec_size,
+                             platform::GetVectorizedSize((*iter)->data<InT>()));
+  }
+  for (auto iter = outs.begin(); iter != outs.end(); ++iter) {
+    vec_size = std::min<int>(
+        vec_size, platform::GetVectorizedSize((*iter)->data<OutT>()));
+  }
+  return vec_size;
+}
+
+template <typename InT, typename OutT, int VecSize, typename Functor, int Arity,
+          bool CallElementwiseAny = false>
+struct ElementwisePrimitiveCaller {
+  __device__ inline void operator()(Functor func, InT (*args)[VecSize],
+                                    OutT *result);
+};
+
+template <typename InT, typename OutT, int VecSize, typename Functor, int Arity>
+struct ElementwisePrimitiveCaller<InT, OutT, VecSize, Functor, Arity, true> {
+  __device__ inline void operator()(Functor func, InT (*args)[VecSize],
+                                    OutT *result) {
+    kps::ElementwiseAny<InT, OutT, VecSize, 1, 1, Arity, Functor>(result, args,
+                                                                  func);
+  }
+};
+
+template <typename InT, typename OutT, int VecSize, typename Functor>
+struct ElementwisePrimitiveCaller<InT, OutT, VecSize, Functor, 1, false> {
+  __device__ inline void operator()(Functor func, InT (*args)[VecSize],
+                                    OutT *result) {
+    kps::ElementwiseUnary<InT, OutT, VecSize, 1, 1, Functor>(result, args[0],
+                                                             func);
+  }
+};
+
+template <typename InT, typename OutT, int VecSize, typename Functor>
+struct ElementwisePrimitiveCaller<InT, OutT, VecSize, Functor, 2, false> {
+  __device__ inline void operator()(Functor func, InT (*args)[VecSize],
+                                    OutT *result) {
+    kps::ElementwiseBinary<InT, OutT, VecSize, 1, 1, Functor>(result, args[0],
+                                                              args[1], func);
+  }
+};
+
+template <typename InT, typename OutT, int VecSize, typename Functor>
+struct ElementwisePrimitiveCaller<InT, OutT, VecSize, Functor, 3, false> {
+  __device__ inline void operator()(Functor func, InT (*args)[VecSize],
+                                    OutT *result) {
+    kps::ElementwiseTernary<InT, OutT, VecSize, 1, 1, Functor>(
+        result, args[0], args[1], args[2], func);
+  }
+};
+
+template <typename InT, typename OutT, typename Functor, int Arity, int VecSize,
+          bool IsBoundary>
+__device__ void ElementVectorizeKernelImpl(
+    const framework::Array<const InT *__restrict__, Arity> &in, OutT *out,
+    int num, int data_offset, Functor func) {
+  InT args[Arity][VecSize];
+  OutT result[VecSize];
+
+#pragma unroll
+  for (int i = 0; i < Arity; i++) {
+    kps::Init<InT, VecSize>(args[i], static_cast<InT>(1.0f));
+    kps::ReadData<InT, VecSize, 1, 1, IsBoundary>(args[i], in[i] + data_offset,
+                                                  num);
+  }
+
+  const bool kCallElementwiseAny =
+      platform::FunctionTraits<Functor>::has_pointer_args;
+  ElementwisePrimitiveCaller<InT, OutT, VecSize, Functor, Arity,
+                             kCallElementwiseAny>()(func, args, result);
+  kps::WriteData<OutT, VecSize, 1, 1, IsBoundary>(out + data_offset, result,
+                                                  num);
+}
+
+template <typename InT, typename OutT, typename Functor, int Arity, int VecSize>
+__global__ void ElementVectorizeKernel(
+    framework::Array<const InT *__restrict__, Arity> ins, OutT *out, int size,
+    int main_offset, Functor func) {
+  int data_offset = BLOCK_ID_X * BLOCK_NUM_X * VecSize;
+  int stride = BLOCK_NUM_X * GRID_NUM_X * VecSize;
+  for (; data_offset < main_offset; data_offset += stride) {
+    ElementVectorizeKernelImpl<InT, OutT, Functor, Arity, VecSize, false>(
+        ins, out, VecSize * BLOCK_NUM_X, data_offset, func);
+  }
+
+  int num = size - data_offset;
+  if (num > 0) {
+    ElementVectorizeKernelImpl<InT, OutT, Functor, Arity, VecSize, true>(
+        ins, out, num, data_offset, func);
+  }
+}
+
+template <typename InT, typename OutT, typename Functor, int Arity, int VecSize>
+void ElementwiseCudaKernel(const platform::CUDADeviceContext &ctx,
+                           const std::vector<const framework::Tensor *> &ins,
+                           std::vector<framework::Tensor *> *outs,
+                           Functor func) {
+  auto numel = ins[0]->numel();
+  int block_size = GetThreadsConfig(ctx, numel, VecSize);
+  int grid_size =
+      ((numel + VecSize - 1) / VecSize + block_size - 1) / block_size;
+
+  auto stream = ctx.stream();
+  OutT *out_data = (*outs)[0]->data<OutT>();
+  framework::Array<const InT *__restrict__, Arity> ins_data;
+  for (int i = 0; i < Arity; i++) {
+    ins_data[i] = ins[i]->data<InT>();
+  }
+#ifdef PADDLE_WITH_XPU2
+  block_size = 128;
+  grid_size = 8;
+  int main_tid = numel / (VecSize * block_size);
+  ElementVectorizeKernel<InT, OutT, Functor, Arity,
+                         VecSize><<<grid_size, block_size, stream>>>(
+      ins_data, out_data, numel, main_tid * block_size * VecSize, func);
+#else
+  int main_tid = numel / (VecSize * block_size);
+  ElementVectorizeKernel<InT, OutT, Functor, Arity,
+                         VecSize><<<grid_size, block_size, 0, stream>>>(
+      ins_data, out_data, numel, main_tid * block_size * VecSize, func);
+#endif
+}
+>>>>>>> modified the elementwise_op_broadcast and elementwise_op_impl for xpu2
 
 template <ElementwiseType ET, typename InT, typename OutT, typename Functor>
 void LaunchSameDimsElementwiseCudaKernel(
