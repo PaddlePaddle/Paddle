21913c1975ae08d724be080409e5eda184f5a322
YuanRisheng <yuanrisheng@baidu.com>
2021-11-10 08:48:45 +0000

    fix bugs when run ci

--- paddle/pten/api/lib/math.cc
+++ paddle/pten/api/lib/math.cc
@@ -69,7 +69,7 @@ Tensor add(const Tensor& x, const Tensor& y) {
 
   // 2. Get Device Context
   auto* dev_ctx = GetDeviceContextByBackend(kernel_key.backend());
-  auto kernel_context = pten::KernelContext(*dev_ctx);
+  auto kernel_context = pten::KernelContext(dev_ctx);
 
   // 3. Auto data transform
   auto dense_x = std::dynamic_pointer_cast<pten::DenseTensor>(x.impl());
