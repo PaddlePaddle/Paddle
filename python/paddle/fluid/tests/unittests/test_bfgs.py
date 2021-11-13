[BFGS] fix hessian update bugs.

python/paddle/fluid/tests/unittests/test_bfgs.py
@@ -25,6 +25,8 @@ from paddle.optimizer.functional.bfgs_utils import (
     vjp
 )
 
+import tensorflow as tf
+
 def jacfn_gen(f, create_graph=False):
     r"""Returns a helper function for computing the jacobians.
     
@@ -65,6 +67,31 @@ def hesfn_gen(f):
         return h
     
     return hess
+
+
+def update_inv_hessian_proper(H, s, y):
+    batch = len(s.shape) > 1
+    dtype = H.dtype
+    dim = s.shape[-1]
+    if not batch:
+        rho = 1. / paddle.dot(s, y)
+        l = paddle.eye(dim, dtype=dtype) - rho * s.unsqueeze(-1) * y.unsqueeze(0)
+        r = paddle.eye(dim, dtype=dtype) - rho * y.unsqueeze(-1) * s.unsqueeze(0)
+        lH = paddle.matmul(l, H)
+        lHr = paddle.matmul(lH, r)
+        H_next = lHr + rho * s.unsqueeze(-1) * s.unsqueeze(0)
+    else:
+        rho = 1. / paddle.einsum('...i, ...i', s, y)
+        l = paddle.eye(dim) - paddle.einsum('...,...i,...j->...ij', rho, s, y)
+        r = paddle.eye(dim) - paddle.einsum('...,...i,...j->...ij', rho, y, s)
+        lH = paddle.matmul(l, H)
+        lHr = paddle.matmul(lH, r)
+        H_next = lHr + paddle.einsum('...,...i,...j->...ij', rho, s, s)
+    
+    return H_next
+
+def _tensor_product(t1, t2):
+    return tf.linalg.matmul(t1[..., tf.newaxis], t2[..., tf.newaxis, :])
 
 class TestBFGS(unittest.TestCase):
 
@@ -163,17 +190,61 @@ class TestBFGS(unittest.TestCase):
 shape = [2]
 dtype = 'float32'
 
-center = paddle.rand(shape, dtype=dtype)
+center = np.random.rand(2)
+scales = np.array([0.2, 1.5])
+x0 = np.ones_like(center)
+s = np.array([-0.5, -0.5])
+
 # scales = paddle.square(paddle.rand(shape, dtype=dtype))
-scales = paddle.to_tensor([0.2, 1.5])
+
+# TF results as reference
+center_tf = tf.convert_to_tensor(center)
+x0_tf = tf.convert_to_tensor(x0)
+s_tf = tf.convert_to_tensor(s)
+
+def f_tf(x):
+    return tf.reduce_sum(tf.square(x - center_tf), axis=-1)
+
+with tf.GradientTape() as tape:
+    tape.watch(x0_tf)
+    y = f_tf(x0_tf)
+
+g0_tf = tape.gradient(y, x0_tf)
+
+x1_tf = x0_tf + s_tf
+with tf.GradientTape() as tape:
+    tape.watch(x1_tf)
+    y = f_tf(x1_tf)
+
+g1_tf = tape.gradient(y, x1_tf)
+
+y_tf = g1_tf - g0_tf
+h0_tf = tf.linalg.inv(tf.linalg.diag(g0_tf))
+normalization_factor = tf.tensordot(s_tf, y_tf, 1)
+h1_tf = tf_inv_hessian_update(y_tf, s_tf, normalization_factor, h0_tf)
+
+# Applies the proper update rules.
+center_pp = paddle.to_tensor(center)
+scales_pp = paddle.to_tensor(scales)
+x0_pp = paddle.to_tensor(x0)
+s_pp = paddle.to_tensor(s)
+
 def f(x):
-    return paddle.sum(paddle.square(x - center), axis=-1)
+    return paddle.sum(paddle.square(x - center_pp), axis=-1)
 
-x0 = paddle.ones_like(center, dtype=dtype)
+f0_pp, g0_pp = vjp(f, x0_pp)
+h0_pp = paddle.inverse(paddle.diag(g0_pp))
+state = SearchState(x0_pp, f0_pp, g0_pp, h0_pp)
 
-# The true inverse hessian value at x0
-hess = hesfn_gen(f)(x0)
-inv_hess = paddle.inverse(hess)
+x1_pp = x0_pp + s_pp
+f1_pp, g1_pp = vjp(f, x1_pp)
+y_pp = g1_pp - g0_pp
+
+h1_pp = update_approx_inverse_hessian(state, h0_pp, s_pp, y_pp)
+
+h1_pp_proper = update_inv_hessian_proper(h0_pp, s_pp, y_pp)
+
+
 
 if __name__ == "__main__":
     unittest.main()