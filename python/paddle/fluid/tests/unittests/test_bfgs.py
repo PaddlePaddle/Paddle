[Einsum] fix wrong dimension number for single element tensors.

python/paddle/fluid/tests/unittests/test_bfgs.py
@@ -90,8 +90,28 @@ def update_inv_hessian_proper(H, s, y):
     
     return H_next
 
-def _tensor_product(t1, t2):
-    return tf.linalg.matmul(t1[..., tf.newaxis], t2[..., tf.newaxis, :])
+def quadratic_gen(shape, dtype):
+    paddle.seed(12345)
+    center = paddle.rand(shape, dtype=dtype)
+    hessian_shape = shape + shape[-1:]
+    rotation = paddle.rand(hessian_shape, dtype=dtype)
+    hessian = paddle.einsum('...ik, ...jk', rotation, rotation)
+
+    if len(hessian.shape) > 1:
+        verify_symmetric_positive_definite_matrix(hessian)
+    else:
+        hessian = paddle.abs(hessian)
+        f = lambda x: x * hessian * x
+        return f, center
+
+    def f(x):
+        y = paddle.einsum('...i, ...ij, ...j',
+                          x - center,
+                          hessian,
+                          x - center)
+        return y
+    
+    return f, center
 
 class TestBFGS(unittest.TestCase):
 
@@ -102,149 +122,110 @@ class TestBFGS(unittest.TestCase):
         dtypes = ['float32', 'float64']
         shapes = {
             '1d1v': [1],
-            '1d2v': [2],
-            '2d1v': [2, 1],
-            '2d2v': [2, 2],
-            '1d100v': [100],
-            '10d10v': [10, 10]
+            # '1d2v': [2],
+            # '2d1v': [2, 1],
+            # '2d2v': [2, 2],
+            # '1d100v': [100],
+            # '10d10v': [10, 10]
         }
         for shape, dtype in zip(shapes.values(), dtypes):
             yield shape, dtype
 
-    def test_update_approx_inverse_hessian(self):
-        paddle.seed(12345)
-        for shape, dtype in self.gen_configs():
-            center = paddle.rand(shape, dtype=dtype)
-            scales = paddle.exp(paddle.rand(shape, dtype=dtype))
-            scales = paddle.square(paddle.rand(shape, dtype=dtype))
-            def f(x):
-                return paddle.sum(scales * paddle.square(x - center), axis=-1)
-            x0 = paddle.ones_like(center, dtype=dtype)
+    # def test_update_approx_inverse_hessian(self):
+    #     paddle.seed(12345)
+    #     for shape, dtype in self.gen_configs():
+    #         print(f'update_inv_hession for shape= {shape},  dtype= {dtype}')
+
+    #         f, center = quadratic_gen(shape, dtype)
+    #         x0 = paddle.ones(shape, dtype=dtype)
             
-            # The true inverse hessian value at x0
-            hess = hesfn_gen(f)(x0)
-            inv_hess = paddle.inverse(hess)
+    #         # The true inverse hessian value at x0
+    #         hess = hesfn_gen(f)(x0)
+    #         h0 = paddle.inverse(hess)
+    #         f0, g0 = vjp(f, x0)
+    #         state = SearchState(x0, f0, g0, h0)
             
-            f0, g0 = vjp(f, x0)
-            state = SearchState(x0, f0, g0, inv_hess)
-            
-            # Verifies the estimated invese Hessian at the center equals the 
-            # true value. 
-            s = center - x0
-            y = -g0
+    #         # Verifies the estimated invese Hessian at the center equals the 
+    #         # true value. 
+    #         s = center - x0
+    #         y = -g0
 
-            H1 = update_approx_inverse_hessian(state, inv_hess, s, y)
-            hess = hesfn_gen(f)(center)
-            inv_hess = paddle.inverse(hess)
+    #         h1 = update_approx_inverse_hessian(state, h0, s, y)
+    #         h1_proper = update_inv_hessian_proper(h0, s, y)
 
-            self.assertTrue(paddle.allclose(H1, inv_hess))
-
-    def _regular_quadratic(self, input_shape, dtype):
-        minimum = paddle.rand(input_shape, dtype=dtype)
-        scales = paddle.exp(paddle.rand(input_shape, dtype=dtype))
-
-        def f(x):
-            return paddle.sum(scales * paddle.square(x - minimum), axis=-1)
-
-        x0 = paddle.ones_like(minimum, dtype=dtype)
-
-        result = bfgs_optimize(f, x0, dtype=dtype)
-
-        self.assertTrue(paddle.all(result.converged))
-        self.assertTrue(paddle.allclose(result.location, minimum))
+    #         self.assertTrue(paddle.allclose(h1, h1_proper))
 
     def test_quadratic(self):
         paddle.seed(12345)
         for shape, dtype in self.gen_configs():
-            self._regular_quadratic(shape, dtype)
-
-    def _general_quadratic(self, input_shape, dtype):
-        minimum = paddle.rand(input_shape, dtype=dtype)
-        hessian_shape = input_shape + input_shape[-1:]
-        rotation = paddle.rand(hessian_shape, dtype=dtype)
-        hessian = paddle.einsum('...ik, ...jk', rotation, rotation)
-
-        verify_symmetric_positive_definite_matrix(hessian)
-
-        def f(x):
-            y = paddle.einsum('...i, ...ij, ...j',
-                              x - minimum,
-                              hessian,
-                              x - minimum)
-
-            return y
-        
-        x0 = paddle.ones_like(minimum, dtype=dtype)
-
-        result = bfgs_optimize(f, x0, dtype=dtype)
-
-        self.assertTrue(paddle.all(result.converged))
-        self.assertTrue(paddle.allclose(result.location, minimum))
-
-    def test_general_quadratic(self):
-        paddle.seed(12345)
-        for shape, dtype in self.gen_configs():
-            self._general_quadratic(shape, dtype)
+            f, center = quadratic_gen(shape, dtype)
+            x0 = paddle.ones(shape, dtype=dtype)
+            result = bfgs_optimize(f, x0, dtype=dtype)
+            self.assertTrue(paddle.all(result.converged))
+            self.assertTrue(paddle.allclose(result.location, center))
 
 
-shape = [2]
-dtype = 'float32'
+# shape = [2]
+# dtype = 'float32'
 
-center = np.random.rand(2)
-scales = np.array([0.2, 1.5])
-x0 = np.ones_like(center)
-s = np.array([-0.5, -0.5])
+# center = np.random.rand(2)
+# scales = np.array([0.2, 1.5])
+# x0 = np.ones_like(center)
+# s = np.array([-0.5, -0.5])
 
-# scales = paddle.square(paddle.rand(shape, dtype=dtype))
+# # scales = paddle.square(paddle.rand(shape, dtype=dtype))
 
-# # TF results as reference
-# center_tf = tf.convert_to_tensor(center)
-# x0_tf = tf.convert_to_tensor(x0)
-# s_tf = tf.convert_to_tensor(s)
+# # # TF results as reference
+# # center_tf = tf.convert_to_tensor(center)
+# # x0_tf = tf.convert_to_tensor(x0)
+# # s_tf = tf.convert_to_tensor(s)
 
-# def f_tf(x):
-#     return tf.reduce_sum(tf.square(x - center_tf), axis=-1)
+# # def f_tf(x):
+# #     return tf.reduce_sum(tf.square(x - center_tf), axis=-1)
 
-# with tf.GradientTape() as tape:
-#     tape.watch(x0_tf)
-#     y = f_tf(x0_tf)
+# # with tf.GradientTape() as tape:
+# #     tape.watch(x0_tf)
+# #     y = f_tf(x0_tf)
 
-# g0_tf = tape.gradient(y, x0_tf)
+# # g0_tf = tape.gradient(y, x0_tf)
 
-# x1_tf = x0_tf + s_tf
-# with tf.GradientTape() as tape:
-#     tape.watch(x1_tf)
-#     y = f_tf(x1_tf)
+# # x1_tf = x0_tf + s_tf
+# # with tf.GradientTape() as tape:
+# #     tape.watch(x1_tf)
+# #     y = f_tf(x1_tf)
 
-# g1_tf = tape.gradient(y, x1_tf)
+# # g1_tf = tape.gradient(y, x1_tf)
 
-# y_tf = g1_tf - g0_tf
-# h0_tf = tf.linalg.inv(tf.linalg.diag(g0_tf))
-# normalization_factor = tf.tensordot(s_tf, y_tf, 1)
-# h1_tf = tf_inv_hessian_update(y_tf, s_tf, normalization_factor, h0_tf)
+# # y_tf = g1_tf - g0_tf
+# # h0_tf = tf.linalg.inv(tf.linalg.diag(g0_tf))
+# # normalization_factor = tf.tensordot(s_tf, y_tf, 1)
+# # h1_tf = tf_inv_hessian_update(y_tf, s_tf, normalization_factor, h0_tf)
 
-# Applies the proper update rules.
-center_pp = paddle.to_tensor(center)
-scales_pp = paddle.to_tensor(scales)
-x0_pp = paddle.to_tensor(x0)
-s_pp = paddle.to_tensor(s)
+# # Applies the proper update rules.
+# center_pp = paddle.to_tensor(center)
+# scales_pp = paddle.to_tensor(scales)
+# x0_pp = paddle.to_tensor(x0)
+# s_pp = paddle.to_tensor(s)
 
-def f(x):
-    return paddle.sum(paddle.square(x - center_pp), axis=-1)
+# def f(x):
+#     return paddle.sum(paddle.square(x - center_pp), axis=-1)
 
-f0_pp, g0_pp = vjp(f, x0_pp)
-h0_pp = paddle.inverse(paddle.diag(g0_pp))
-state = SearchState(x0_pp, f0_pp, g0_pp, h0_pp)
+# f0_pp, g0_pp = vjp(f, x0_pp)
+# h0_pp = paddle.inverse(paddle.diag(g0_pp))
+# state = SearchState(x0_pp, f0_pp, g0_pp, h0_pp)
 
-x1_pp = x0_pp + s_pp
-f1_pp, g1_pp = vjp(f, x1_pp)
-y_pp = g1_pp - g0_pp
+# x1_pp = x0_pp + s_pp
+# f1_pp, g1_pp = vjp(f, x1_pp)
+# y_pp = g1_pp - g0_pp
 
-h1_pp = update_approx_inverse_hessian(state, h0_pp, s_pp, y_pp)
+# h1_pp = update_approx_inverse_hessian(state, h0_pp, s_pp, y_pp)
 
-h1_pp_proper = update_inv_hessian_proper(h0_pp, s_pp, y_pp)
+# h1_pp_proper = update_inv_hessian_proper(h0_pp, s_pp, y_pp)
 
+x = paddle.rand([1, 1, 1])
+y = paddle.rand([1, 1])
 
+z = paddle.einsum('...ij, ...j', x, y)
 
 if __name__ == "__main__":
     unittest.main()