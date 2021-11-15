319b4811b0954e8ba0746b35ac2bf93b2051241d
piotrekobiIntel <piotr.paturej@intel.com>
2021-11-15 12:00:31 +0100

    Revert "Change oneDNN version to 2.3 for Jenkins test"
    This reverts commit 0b176defc3b63f65dd0ba85873a018534f287000.

--- cmake/external/mkldnn.cmake
+++ cmake/external/mkldnn.cmake
@@ -20,7 +20,7 @@ SET(MKLDNN_SOURCE_DIR     ${THIRD_PARTY_PATH}/mkldnn/src/extern_mkldnn)
 SET(MKLDNN_INSTALL_DIR    ${THIRD_PARTY_PATH}/install/mkldnn)
 SET(MKLDNN_INC_DIR        "${MKLDNN_INSTALL_DIR}/include" CACHE PATH "mkldnn include directory." FORCE)
 SET(MKLDNN_REPOSITORY     ${GIT_URL}/oneapi-src/oneDNN.git)
-SET(MKLDNN_TAG            e2d45252ae9c3e91671339579e3c0f0061f81d49)
+SET(MKLDNN_TAG            8488d014aefcbe82e61c2bc494b94e593fe0b669)
 
 
 # Introduce variables:
