5520f57f79b7ed18a71f89a13772b159828f5763
gsq7474741 <gsq7474741@icloud.com>
2021-11-15 19:58:33 +0800

    add zeropad2d v0.3

--- python/paddle/fluid/tests/unittests/test_zeropad2d.py
+++ python/paddle/fluid/tests/unittests/test_zeropad2d.py
@@ -41,7 +41,7 @@ class TestZeroPad2dOpError(unittest.TestCase):
             pad = 2
             x = np.random.randint(-255, 255, size=self.shape)
             x_tensor = to_tensor(x).astype(dtype)
-            self.assertRaises(TypeError, zeropad2d, x=x_tensor, pad=pad)
+            self.assertRaises(TypeError, zeropad2d, x=x_tensor, padding=pad)
 
 
 class TestZeroPadOp2d(unittest.TestCase):
@@ -104,8 +104,8 @@ class TestZeroPadOp2d(unittest.TestCase):
         expect_res = np.pad(
             x, [[0, 0], [0, 0], [pad[2], pad[3]], [pad[0], pad[1]]])
 
-        x_tensor = to_tensor(x, dtype='int32')
-        pad_tensor = to_tensor(pad)
+        x_tensor = to_tensor(x)
+        pad_tensor = to_tensor(pad, dtype='int32')
         ret_res = zeropad2d(x_tensor, pad_tensor).numpy()
         self.assertTrue(np.allclose(expect_res, ret_res))
 
