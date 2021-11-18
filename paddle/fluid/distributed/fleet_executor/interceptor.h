b35fe4d66e3b72c4b5c98cc6dd2ae61dca1faa6e
liuyuang <liuyuang@baidu.com>
2021-11-18 11:40:03 +0800

    update

--- paddle/fluid/distributed/fleet_executor/interceptor.h
+++ paddle/fluid/distributed/fleet_executor/interceptor.h
@@ -97,8 +97,8 @@ class Interceptor {
   // read by PoolTheMailbox()
   std::queue<InterceptorMessage> local_mailbox_;
 
-  int64_t already_run_times_{0};
   int64_t used_slot_nums_{0};
+  int64_t already_run_times_{0};
 };
 
 }  // namespace distributed
