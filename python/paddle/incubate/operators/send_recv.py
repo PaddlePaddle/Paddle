6c59c790993f6dd5ee119374f0cb3bf673e43688
DesmonDay <908660116@qq.com>
2021-11-18 02:26:47 +0000

    delete numpy in examples

--- python/paddle/incubate/operators/send_recv.py
+++ python/paddle/incubate/operators/send_recv.py
@@ -65,11 +65,10 @@ def send_recv(x, src_index, dst_index, pool_type="sum", name=None):
 
         .. code-block:: python
 
-            import numpy as np
             import paddle
 
-            x = paddle.to_tensor(np.array([[0, 2, 3], [1, 4, 5], [2, 6, 7]]), dtype="float32")
-            indexes = paddle.to_tensor(np.array([[0, 1], [1, 2], [2, 1], [0, 0]]), dtype="int32")
+            x = paddle.to_tensor([[0, 2, 3], [1, 4, 5], [2, 6, 7]], dtype="float32")
+            indexes = paddle.to_tensor([[0, 1], [1, 2], [2, 1], [0, 0]], dtype="int32")
             src_index = indexes[:, 0]
             dst_index = indexes[:, 1]
             out = paddle.incubate.send_recv(x, src_index, dst_index, pool_type="sum")
