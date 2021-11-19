7ff5608678391f1079b07dad194dce7d775666c9
Wangzheee <634486483@qq.com>
2021-11-19 08:43:47 +0000

    fix_nearest

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_nearest_interp.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_nearest_interp.py
@@ -124,6 +124,9 @@ class TrtConvertNearestInterpTest(TrtLayerAutoScanTest):
             if program_config.ops[0].attrs[
                     'scale'] <= 0 and self.dynamic_shape.min_input_shape:
                 return True
+            if program_config.ops[0].attrs[
+                    'align_corners'] == True and self.dynamic_shape.min_input_shape:
+                return True
             return False
 
         self.add_skip_case(
