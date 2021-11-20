9a880bd902b180fac3edf4efad7c871278ca70a0
levi131 <limaolin01@baidu.com>
2021-11-20 08:07:39 +0000

    add batch jacobian and batch hessian

--- python/paddle/autograd/functional.py
+++ python/paddle/autograd/functional.py
@@ -244,7 +244,7 @@ def jvp(func, inputs, v=None, create_graph=False, allow_unused=False):
 
 
 @framework.dygraph_only
-def jacobian(func, inputs, create_graph=False, allow_unused=False, batch=False):
+def jacobian(func, inputs, create_graph=False, allow_unused=False):
     ''' 
     .. note::
         **This API is ONLY available in the imperative mode.**
@@ -355,12 +355,6 @@ def jacobian(func, inputs, create_graph=False, allow_unused=False, batch=False):
     '''
     inputs = _tensors(inputs, "inputs")
     outputs = _tensors(func(*inputs), "outputs")
-    if batch:
-        batch_size = inputs[0].shape[0]
-        for input in inputs:
-            assert input.shape[0] == batch_size
-        for output in outputs:
-            assert output.shape[0] == batch_size
     fin_size = len(inputs)
     fout_size = len(outputs)
     flat_outputs = tuple(reshape(output, shape=[-1]) for output in outputs)
@@ -392,7 +386,74 @@ def jacobian(func, inputs, create_graph=False, allow_unused=False, batch=False):
 
 
 @framework.dygraph_only
-def hessian(func, inputs, create_graph=False, allow_unused=False, batch=False):
+def batch_jacobian(func, inputs, create_graph=False, allow_unused=False):
+    inputs = _tensors(inputs, "inputs")
+    outputs = _tensors(func(*inputs), "outputs")
+    batch_size = inputs[0].shape[0]
+    for input in inputs:
+        assert input.shape[0] == batch_size
+    for output in outputs:
+        assert output.shape[0] == batch_size
+    fin_size = len(inputs)
+    fout_size = len(outputs)
+    flat_outputs = tuple(
+        reshape(
+            output, shape=[batch_size, -1]) for output in outputs)
+    jacobian = tuple()
+    for i, flat_output in enumerate(flat_outputs):
+        jac_i = list([] for _ in range(fin_size))
+        for k in range(flat_output.shape[1]):
+            row_k = grad(
+                flat_output[:, k],
+                inputs,
+                create_graph=create_graph,
+                retain_graph=True,
+                allow_unused=allow_unused)
+            for j in range(fin_size):
+                jac_i[j].append(
+                    reshape(
+                        row_k[j], shape=[-1])
+                    if isinstance(row_k[j], paddle.Tensor) else None)
+        jacobian += (tuple(
+            _stack_tensor_or_return_none(jac_i_j) for jac_i_j in jac_i), )
+    if fin_size == 1 and fout_size == 1:
+        return jacobian[0][0]
+    elif fin_size == 1 and fout_size != 1:
+        return tuple(jacobian[i][0] for i in range(fout_size))
+    elif fin_size != 1 and fout_size == 1:
+        return jacobian[0]
+    else:
+        return jacobian
+
+
+@framework.dygraph_only
+def batch_hessian(func, inputs, create_graph=False, allow_unused=False):
+    inputs = _tensors(inputs, "inputs")
+    outputs = func(*inputs)
+    batch_size = inputs[0].shape[0]
+    for input in inputs:
+        assert input.shape[0] == batch_size
+    assert isinstance(outputs, paddle.Tensor) and outputs.shape == [
+        batch_size, 1
+    ], "The function to compute batched Hessian matrix should return a Tensor of shape [batch_size, 1]"
+
+    def jac_func(*ins):
+        grad_inputs = grad(
+            outputs,
+            ins,
+            create_graph=True,
+            retain_graph=True,
+            allow_unused=allow_unused)
+        return tuple(
+            _replace_none_with_zero_tensor(grad_inputs[i], inputs[i])
+            for i in range(len(inputs)))
+
+    return batch_jacobian(
+        jac_func, inputs, create_graph=create_graph, allow_unused=allow_unused)
+
+
+@framework.dygraph_only
+def hessian(func, inputs, create_graph=False, allow_unused=False):
     ''' 
     .. note::
         **This API is ONLY available in the imperative mode.**
@@ -498,17 +559,9 @@ def hessian(func, inputs, create_graph=False, allow_unused=False, batch=False):
     '''
     inputs = _tensors(inputs, "inputs")
     outputs = func(*inputs)
-    if batch:
-        batch_size = inputs[0].shape[0]
-        for input in inputs:
-            assert input.shape[0] == batch_size
-        assert isinstance(outputs, paddle.Tensor) and outputs.shape == [
-            batch_size, 1
-        ], "The function to compute batched Hessian matrix should return a Tensor of shape [batch_size, 1]"
-    else:
-        assert isinstance(outputs, paddle.Tensor) and outputs.shape == [
-            1
-        ], "The function to compute Hessian matrix should return a Tensor with a single element"
+    assert isinstance(outputs, paddle.Tensor) and outputs.shape == [
+        1
+    ], "The function to compute Hessian matrix should return a Tensor with a single element"
 
     def jac_func(*ins):
         grad_inputs = grad(
@@ -522,11 +575,7 @@ def hessian(func, inputs, create_graph=False, allow_unused=False, batch=False):
             for i in range(len(inputs)))
 
     return jacobian(
-        jac_func,
-        inputs,
-        create_graph=create_graph,
-        allow_unused=allow_unused,
-        batch=batch)
+        jac_func, inputs, create_graph=create_graph, allow_unused=allow_unused)
 
 
 @framework.dygraph_only
