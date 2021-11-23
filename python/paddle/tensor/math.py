c3ce05924803b93285a075f570a66167ddc7267c
andyjpaddle <jiangkaitao@baidu.com>
2021-11-23 02:23:12 +0000

    update sample code

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -2645,28 +2645,28 @@ def diff(x, n=1, dim=-1, prepend=None, append=None, name=None):
     Examples:
         .. code-block:: python
 
-        import paddle
-        x = paddle.to_tensor([1, 4, 5, 2])
-        out = paddle.diff(x)
-        print(out)
-        # out:
-        # [3, 1, -3]
+            import paddle
+            x = paddle.to_tensor([1, 4, 5, 2])
+            out = paddle.diff(x)
+            print(out)
+            # out:
+            # [3, 1, -3]
 
-        y = paddle.to_tensor([7, 9])
-        out = paddle.diff(x, append=y)
-        print(out)
-        # out: 
-        # [3, 1, -3, 5, 2]
+            y = paddle.to_tensor([7, 9])
+            out = paddle.diff(x, append=y)
+            print(out)
+            # out: 
+            # [3, 1, -3, 5, 2]
 
-        z = paddle.to_tensor([[1, 2, 3], [4, 5, 6]])
-        out = paddle.diff(z, dim=0)
-        print(out)
-        # out:
-        # [3, 3, 3]
-        out = paddle.diff(z, dim=1)
-        print(out)
-        # out:
-        # [[1, 1], [1, 1]]
+            z = paddle.to_tensor([[1, 2, 3], [4, 5, 6]])
+            out = paddle.diff(z, dim=0)
+            print(out)
+            # out:
+            # [3, 3, 3]
+            out = paddle.diff(z, dim=1)
+            print(out)
+            # out:
+            # [[1, 1], [1, 1]]
 
 
     """
