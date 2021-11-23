1984b481c023fcb7b1301ceb3498a60d1da3df8f
Chen Weihang <chenweihang@baidu.com>
2021-11-22 20:07:28 -0600

    [PTen] Adapt to inference api dir for pten (#37415)
    * adapt to inference api dir for pten
    
    * fix conflit with develop
    
    * fix test_egr_ds_eager_tensor compile failed

--- paddle/pten/api/lib/manipulation.cc
+++ paddle/pten/api/lib/manipulation.cc
@@ -17,7 +17,7 @@ limitations under the License. */
 #include <memory>
 
 #include "glog/logging.h"
-#include "paddle/pten/api/include/registry.h"
+#include "paddle/pten/api/lib/api_registry.h"
 #include "paddle/pten/api/lib/kernel_dispatch.h"
 #include "paddle/pten/api/lib/utils/allocator.h"
 #include "paddle/pten/core/kernel_registry.h"
