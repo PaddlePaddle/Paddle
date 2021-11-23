b4845e20be499c458e8bc10354bd27ef8a7bec9a
tianshuo78520a <707759223@qq.com>
2021-11-23 22:55:55 +0800

    fix

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 <p align="center">
 <img align="center" src="doc/imgs/logo.png", width=1600>
 <p>
--- tools/ci_op_benchmark.sh
+++ tools/ci_op_benchmark.sh
@@ -92,7 +92,6 @@ function load_CHANGE_OP_FILES {
     fi
   done
   [ ${#CHANGE_OP_FILES[@]} -eq 0 ] && LOG "[INFO] No op to test, skip this ci." && \
-  echo "cpu_benchmark=ON" >${cfs_dir}/op_benchmark/${AGILE_PULL_ID}/${AGILE_REVISION}/pass.txt && \
   exit 0
 }
 
