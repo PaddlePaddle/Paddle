cf3da6f0308d62876fa9bfab00ddf81b87eccb63
piotrekobiIntel <piotr.paturej@intel.com>
2021-11-25 09:08:53 +0100

    Add third batch of deprecated mkldnn namespace name changes

--- paddle/fluid/platform/device_context.h
+++ paddle/fluid/platform/device_context.h
@@ -48,7 +48,6 @@ limitations under the License. */
 #ifdef PADDLE_WITH_MKLDNN
 #include "dnnl.hpp"
 #include "paddle/fluid/framework/data_layout.h"
-namespace mkldnn = dnnl;
 #endif
 
 #include <map>
@@ -63,10 +62,6 @@ namespace mkldnn = dnnl;
 #include "paddle/fluid/platform/stream/npu_stream.h"
 #endif
 #include "unsupported/Eigen/CXX11/Tensor"
-
-// This aias is required for now so that namespace name changes can be made to
-// less than 20 files at a time. After all the names are changed it will be
-// removed.
 
 namespace Eigen {
 struct DefaultDevice;
