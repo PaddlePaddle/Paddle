c658354a1987fbe2e2065844dbaa612bdfdb89b3
skrBang <s_huabang@163.com>
2021-11-26 15:26:22 +0800

    fix Example doc

--- python/paddle/nn/functional/loss.py
+++ python/paddle/nn/functional/loss.py
@@ -2110,21 +2110,19 @@ def hinge_embedding_loss(input, label, delta=1.0, reduction='mean', name=None):
         .. code-block:: python
 
             import paddle
-            import paddle.nn as nn
+            import paddle.nn.functional as F
 
             input = paddle.to_tensor([[1, -2, 3], [0, -1, 2], [1, 0, 1]], dtype=paddle.float32)
             # label elements in {1., -1.}
             label = paddle.to_tensor([[-1, 1, -1], [1, 1, 1], [1, -1, 1]], dtype=paddle.float32)
 
-            hinge_embedding_loss = nn.HingeEmbeddingLoss(delta=1.0, reduction='none')
-            loss = hinge_embedding_loss(input, label)
+            loss = F.hinge_embedding_loss(input, label, delta=1.0, reduction='none')
             print(loss)
             # Tensor([[0., -2., 0.],
             #         [0., -1., 2.],
             #         [1., 1., 1.]])
 
-            hinge_embedding_loss = nn.HingeEmbeddingLoss(delta=1.0, reduction='mean')
-            loss = hinge_embedding_loss(input, label)
+            loss = F.hinge_embedding_loss(input, label, delta=1.0, reduction='mean')
             print(loss)
             # Tensor([0.22222222])
     """
