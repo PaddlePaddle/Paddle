893c244d7386cd643f0b4169f983de6fa69f4c96
chenfeiyu <chenfeiyu@baidu.com>
2021-11-26 07:43:56 +0000

    fix cmath import, for nvcc

--- paddle/fluid/operators/angle_op.cu
+++ paddle/fluid/operators/angle_op.cu
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define _USE_MATH_DEFINES
 #include "paddle/fluid/operators/angle_op.h"
 #include "paddle/fluid/platform/complex.h"
 
