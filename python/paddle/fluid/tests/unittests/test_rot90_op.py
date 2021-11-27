7cbc6b3ddfe43bf774d0a76f4e15fc889fe61587
zmxdream <zmxdream@pku.edu.cn>
2021-11-27 18:04:44 +0800

    fix ut. test=develop

--- python/paddle/fluid/tests/unittests/test_rot90_op.py
+++ python/paddle/fluid/tests/unittests/test_rot90_op.py
@@ -20,10 +20,8 @@ import paddle
 import paddle.fluid as fluid
 import paddle.fluid.core as core
 from paddle.fluid import Program, program_guard
-from op_test import OpTest
 
-
-class TestRot90Op_API(unittest.TestCase):
+class TestRot90_API(unittest.TestCase):
     """Test rot90 api."""
 
     def test_static_graph(self):
@@ -66,87 +64,6 @@ class TestRot90Op_API(unittest.TestCase):
             self.assertTrue(
                 (ret.numpy() == out_ref).all(),
                 msg='rot90 output is wrong, out =' + str(ret.numpy()))
-
-
-class TestRot90Op(OpTest):
-    def setUp(self):
-        self.op_type = 'rot90'
-        self.init_test_case()
-        self.inputs = {'X': np.random.random(self.in_shape).astype('float64')}
-        self.init_attrs()
-        self.outputs = {'Out': self.calc_ref_res()}
-
-    def init_attrs(self):
-        self.attrs = {"k": self.k, "dims": self.dims}
-
-    def test_check_output(self):
-        self.check_output()
-
-    def test_check_grad(self):
-        self.check_grad(["X"], "Out")
-
-    def init_test_case(self):
-        self.in_shape = (6, 4, 2, 3)
-        self.k = 1
-        self.dims = [0, 1]
-
-    def calc_ref_res(self):
-        res = self.inputs['X']
-        res = np.rot90(res, self.k, self.dims)
-        #if isinstance(self.axis, int):
-        #    return np.flip(res, self.axis)
-        #for axis in self.axis:
-        #    res = np.flip(res, axis)
-        return res
-
-
-class TestRot90OpK1(TestRot90Op):
-    def init_test_case(self):
-        self.in_shape = (2, 4, 4)
-        self.k = 1
-        self.dims = [0, 1]
-
-
-class TestRot90OpK2(TestRot90Op):
-    def init_test_case(self):
-        self.in_shape = (4, 4, 6, 3)
-        self.k = 2
-        self.dims = [0, 1]
-
-
-class TestRot90OpK3(TestRot90Op):
-    def init_test_case(self):
-        self.in_shape = (4, 3, 1)
-        self.k = 3
-        self.dims = [0, 1]
-
-
-class TestRot90OpKNeg1(TestRot90Op):
-    def init_test_case(self):
-        self.in_shape = (6, 4, 2, 2)
-        self.k = -1
-        self.dims = [0, 1]
-
-
-class TestRot90OpKNeg2(TestRot90Op):
-    def init_test_case(self):
-        self.in_shape = (6, 4, 2, 2)
-        self.k = -2
-        self.dims = [0, 1]
-
-
-class TestRot90OpKNeg3(TestRot90Op):
-    def init_test_case(self):
-        self.in_shape = (6, 4, 2, 2)
-        self.k = -3
-        self.dims = [0, 1]
-
-
-class TestRot90OpK0(TestRot90Op):
-    def init_test_case(self):
-        self.in_shape = (6, 4, 2, 2)
-        self.k = 0
-        self.dims = [0, 1]
 
 
 if __name__ == "__main__":
