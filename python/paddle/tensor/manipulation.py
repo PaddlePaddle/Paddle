1dd77f5ca7dbb87883dda5126986c5f249b9cdf9
zmxdream <zmxdream@pku.edu.cn>
2021-11-27 19:42:35 +0800

    fix sample code. test=develop

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -523,6 +523,7 @@ def rot90(x, k, dims, name=None):
           y= paddle.rot90(data, -1, [0, 1])
           print(y) #[[2, 0],[3, 1]]
           data2 = paddle.arange(8)
+          data2 = paddle.reshape(data2, (2,2,2))
           print(data2) ###[[[0, 1],[2, 3]],[[4, 5],[6, 7]]]
           y = paddle.rot90(data2, 1, [1, 2])
           print(y)   ### [[[1, 3],[0, 2]],[[5, 7],[4, 6]]]
