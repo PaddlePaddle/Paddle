a23da761682d4fe111246dcb24c6fe7677f51550
chenfeiyu <chenfeiyu@baidu.com>
2021-11-29 06:53:52 +0000

    remove redundant _USE_MATH_DEFINES

--- paddle/fluid/operators/angle_op.cu
+++ paddle/fluid/operators/angle_op.cu
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define _USE_MATH_DEFINES
 #include "paddle/fluid/operators/angle_op.h"
 #include "paddle/fluid/platform/complex.h"
 
