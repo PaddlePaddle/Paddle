dc004f297b0f2f8cc143a8374b93bac94fc6710e
chenxin33 <1448148955@qq.com>
2021-12-01 08:27:17 +0000

    only deliver trt_root in run.sh

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -2237,8 +2237,7 @@ EOF
     demo_ci_startTime_s=`date +%s`
     cd ${PADDLE_ROOT}/paddle/fluid/inference/api/demo_ci
     ./run.sh ${PADDLE_ROOT} ${WITH_MKL:-ON} ${WITH_GPU:-OFF} ${INFERENCE_DEMO_INSTALL_DIR} \
-             ${TENSORRT_INCLUDE_DIR:-/usr/local/TensorRT/include} \
-             ${TENSORRT_LIB_DIR:-/usr/local/TensorRT/lib}
+             ${TENSORRT_ROOT_DIR:-/usr}
     DEMO_EXIT_CODE=$?
     ./clean.sh
     demo_ci_endTime_s=`date +%s`
