7a56b73a45dcaa0241942e041a7153918fef4324
user.name <hopewj@163.com>
2021-12-03 19:07:30 +0800

    and util path

--- python/paddle/distributed/auto_parallel/operators/common.py
+++ python/paddle/distributed/auto_parallel/operators/common.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
-from .utils import compute_compatible_dim_mapping
+from ..utils import compute_compatible_dim_mapping
 
 _g_distributed_operator_impl_registries = {}
 
