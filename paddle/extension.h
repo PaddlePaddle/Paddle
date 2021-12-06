9f638f8c53a9524106865c363a776e213da3e2f6
piotrekobiIntel <piotr.paturej@intel.com>
2021-12-06 12:02:41 +0100

    Change cmake eigen include directory

--- paddle/extension.h
+++ paddle/extension.h
@@ -11,8 +11,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-#define EXTENSION
 #pragma once
-
 // All paddle apis in C++ frontend
 #include "paddle/pten/api/all.h"
