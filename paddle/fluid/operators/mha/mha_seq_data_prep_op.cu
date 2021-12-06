2d92ce2c96767eb5f424bdcebf944ce4a30dc888
Ming Huang <mingh@nvidia.com>
2021-12-06 07:34:09 +0000

    Added a subdictionary for mha related ops

--- paddle/fluid/operators/mha/mha_seq_data_prep_op.cu
+++ paddle/fluid/operators/mha/mha_seq_data_prep_op.cu
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/operators/mha_seq_data_prep_op.h"
+#include "paddle/fluid/operators/mha/mha_seq_data_prep_op.h"
 #include "paddle/fluid/platform/float16.h"
 
 namespace ops = paddle::operators;
