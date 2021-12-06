4ff89e556b39f2955615c330e476e8ae16e5e7f5
YuanRisheng <yuanrisheng@baidu.com>
2021-12-06 08:26:14 +0000

    add alias kernel name

--- paddle/fluid/operators/reshape_op.cc
+++ paddle/fluid/operators/reshape_op.cc
@@ -555,13 +555,13 @@ class Reshape2Op : public ReshapeOp {
       const framework::ExecutionContext &ctx) const override {
     auto multi_inputs = ctx.MultiInput<framework::Tensor>("ShapeTensor");
     if (multi_inputs.size() > 0) {
-      return framework::KernelSignature("reshape2.mulhost",
-                                        {"X", "ShapeTensor"}, {}, {"Out"});
+      return framework::KernelSignature("reshape.mulhost", {"X", "ShapeTensor"},
+                                        {}, {"Out"});
     } else if (ctx.HasInput("Shape")) {
-      return framework::KernelSignature("reshape2.host", {"X", "Shape"}, {},
+      return framework::KernelSignature("reshape.host", {"X", "Shape"}, {},
                                         {"Out"});
     } else {
-      return framework::KernelSignature("reshape2", {"X"}, {"shape"}, {"Out"});
+      return framework::KernelSignature("reshape", {"X"}, {"shape"}, {"Out"});
     }
   }
 };
--- paddle/pten/core/convert_utils.cc
+++ paddle/pten/core/convert_utils.cc
@@ -20,6 +20,26 @@ limitations under the License. */
 
 namespace pten {
 
+// the map between kernel name in fluid and pten
+const std::unordered_map<std::string, std::string> kernel_alias_name_map = {
+    {"reshape2", "reshape"},
+    {"fill_any_like", "full_like"},
+    {"fill_constant", "full"},
+    {"matmul_v2", "matmul"},
+    {"flatten_contiguous_range", "flatten"},
+    {"reduce_mean", "mean"},
+    {"reduce_sum", "sum"},
+    {"elementwise_add", "add"},
+    {"elementwise_sub", "subtract"},
+    {"elementwise_mul", "muliply"},
+    {"elementwise_div", "divide"},
+    // fluid kernel "mean/reshape/matmul/flatten/sum" should be deprecated
+    {"mean", "deprecated"},
+    {"reshape", "deprecated"},
+    {"matmul", "deprecated"},
+    {"flatten", "deprecated"},
+    {"sum", "deprecated"}};
+
 // TODO(chenweihang): Add other place trans cases later
 Backend TransToPtenBackend(const paddle::platform::Place& place) {
   if (paddle::platform::is_cpu_place(place)) {
@@ -302,6 +322,14 @@ int TensorDtype2NumpyDtype(pten::DataType dtype) {
           static_cast<int>(dtype)));
       return 0;
   }
+}
+
+std::string TransToPtenKernelName(const std::string& fluid_op_name) {
+  if (kernel_alias_name_map.find(fluid_op_name) !=
+      kernel_alias_name_map.end()) {
+    return kernel_alias_name_map.at(fluid_op_name);
+  }
+  return fluid_op_name;
 }
 
 }  // namespace pten
