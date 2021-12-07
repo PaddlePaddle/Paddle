d432116161b1dee67b4ed912fbab09e7453dd2ac
liyurui <liyuruijx@pku.edu.cn>
2021-12-07 14:20:17 +0800

    fix for converage

--- python/paddle/fluid/executor.py
+++ python/paddle/fluid/executor.py
@@ -1951,18 +1951,17 @@ class Executor(object):
         from google.protobuf import text_format
         assert program, "Program for fleet executor should not be None"
         assert fleet_opt, "Configurations for fleet executor should not be None"
-        trainer_endpoints_str = os.getenv("PADDLE_TRAINER_ENDPOINTS")
+        trainer_endpoints_str = os.getenv("PADDLE_TRAINER_ENDPOINTS", "")
+        trainer_endpoints = trainer_endpoints_str.split(',')
         fleet_exe_desc = fleet_executor_desc_pb2.FleetExecutorDesc()
         fleet_exe_desc.cur_rank = os.getenv("PADDLE_TRAINER_ID", 0)
-        nrank = 1
-        if trainer_endpoints_str:
-            trainer_endpoints = trainer_endpoints_str.split(',')
+        nrank = len(trainer_endpoints) if trainer_endpoints else 1
+        if trainer_endpoints:
             for rank, endpoint in enumerate(trainer_endpoints):
                 rank_info = fleet_executor_desc_pb2.RankInfo()
                 rank_info.rank = rank
                 rank_info.ip_port = endpoint
                 fleet_exe_desc.cluster_info.append(rank_info)
-            nrank = len(trainer_endpoints)
         else:
             rank_info = fleet_executor_desc_pb2.RankInfo()
             rank_info.rank = 0
