52cc26610d02897359a358269e38e6a14dd4b0ed
piotrekobiIntel <piotr.paturej@intel.com>
2021-12-07 12:06:07 +0100

    Revert unnecessary file changes

--- paddle/extension.h
+++ paddle/extension.h
@@ -11,6 +11,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
+
 #pragma once
+
 // All paddle apis in C++ frontend
 #include "paddle/pten/api/all.h"
