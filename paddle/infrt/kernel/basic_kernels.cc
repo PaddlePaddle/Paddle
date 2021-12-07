547886ed1eb82437a71670d3c4b12b4f3b1c6ff4
Yan Chunwei <yanchunwei@outlook.com>
2021-12-07 18:40:30 +0800

    introduce INF-RT (#37669)
    * add infrt code
    
    refined with Paddle's code style.
    
    * rename CinnRtConfig to InfRtConfig
    
    * rename CinnRt to InfRt of some code
    
    * rename CINNRT to INFRT
    
    * remove unnecessary code
    
    * replace CINN to INFRT in the source code
    
    * replace all "cinn" in code to "infrt"
    
    * remove some const_cast

---
+++ paddle/infrt/kernel/basic_kernels.cc
@@ -0,0 +1,85 @@
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "paddle/infrt/kernel/basic_kernels.h"
+
+#include <iostream>
+#include <string>
+
+#include "llvm/Support/raw_ostream.h"
+#include "paddle/infrt/host_context/kernel_registry.h"
+#include "paddle/infrt/host_context/kernel_utils.h"
+
+using infrt::host_context::Attribute;
+
+namespace infrt::kernel {
+
+template <typename T>
+T add(T a, T b) {
+  return a + b;
+}
+
+template <typename T>
+T sub(T a, T b) {
+  return a - b;
+}
+
+template <typename T>
+T mul(T a, T b) {
+  return a * b;
+}
+
+template <typename T>
+T div(T a, T b) {
+  return a / b;
+}
+
+template <typename T>
+void print(T a) {
+  std::cout << a << std::endl;
+}
+
+static std::string GetString(Attribute<std::string> value) {
+  return value.get();
+}
+
+static void PrintString(const std::string &str) {
+  llvm::outs() << "string = " << str << '\n';
+  llvm::outs().flush();
+}
+
+void RegisterBasicKernels(host_context::KernelRegistry *registry) {
+  RegisterIntBasicKernels(registry);
+  RegisterFloatBasicKernels(registry);
+  registry->AddKernel("infrt.get_string", INFRT_KERNEL(GetString));
+  registry->AddKernel("infrt.print_string", INFRT_KERNEL(PrintString));
+}
+
+void RegisterIntBasicKernels(host_context::KernelRegistry *registry) {
+  registry->AddKernel("infrt.add.i32", INFRT_KERNEL(add<int32_t>));
+  registry->AddKernel("infrt.sub.i32", INFRT_KERNEL(sub<int32_t>));
+  registry->AddKernel("infrt.mul.i32", INFRT_KERNEL(mul<int32_t>));
+  registry->AddKernel("infrt.div.i32", INFRT_KERNEL(div<int32_t>));
+  registry->AddKernel("infrt.print.i32", INFRT_KERNEL(print<int32_t>));
+}
+
+void RegisterFloatBasicKernels(host_context::KernelRegistry *registry) {
+  registry->AddKernel("infrt.add.f32", INFRT_KERNEL(add<float>));
+  registry->AddKernel("infrt.sub.f32", INFRT_KERNEL(sub<float>));
+  registry->AddKernel("infrt.mul.f32", INFRT_KERNEL(mul<float>));
+  registry->AddKernel("infrt.div.f32", INFRT_KERNEL(div<float>));
+  registry->AddKernel("infrt.print.f32", INFRT_KERNEL(print<float>));
+}
+
+}  // namespace infrt::kernel
---
+++ paddle/infrt/tensor/dense_host_tensor.cc
@@ -0,0 +1,86 @@
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "paddle/infrt/tensor/dense_host_tensor.h"
+
+#include <llvm/Support/raw_os_ostream.h>
+
+#include "paddle/infrt/common/buffer.h"
+
+namespace infrt::tensor {
+
+DenseHostTensor::DenseHostTensor(const TensorShape& shape, DType dtype)
+    : HostTensor(TensorMetadata{dtype, shape}) {
+  CHECK(metadata().IsValid()) << "Tensor construct get invalid metadata";
+  buffer_.reset(new infrt::Buffer(infrt::common::DefaultHostTarget()));
+  buffer_->ResizeLazy(dtype.GetHostSize() * shape.GetNumElements());
+}
+
+const TensorShape& DenseHostTensor::shape() const { return metadata().shape; }
+
+void DenseHostTensor::Init(const std::vector<int64_t>& shape, DType dtype) {
+  auto shape_array = llvm::ArrayRef<int64_t>(shape.data(), shape.size());
+  auto metadata = TensorMetadata(dtype, shape_array);
+  setTensorMetadata(metadata);
+  buffer_.reset(new infrt::Buffer(infrt::common::DefaultHostTarget()));
+  buffer_->ResizeLazy(dtype.GetHostSize() * metadata.shape.GetNumElements());
+}
+
+const infrt::Buffer* DenseHostTensor::buffer() const { return buffer_.get(); }
+
+template <typename T>
+void DisplayArray(std::ostream& os, T* data, int num_elements) {
+  for (int i = 0; i < num_elements - 1; i++) os << data[i] << ", ";
+  if (num_elements > 0) os << data[num_elements - 1];
+}
+
+std::ostream& operator<<(std::ostream& os, const DenseHostTensor& instance) {
+  CHECK(instance.metadata().IsValid())
+      << "Cann't print tensor with invalid metadata";
+  llvm::raw_os_ostream oos(os);
+  oos << "tensor: ";
+  oos << "shape=";
+  oos << instance.shape();
+  oos << ", values=[";
+
+  oos.flush();
+
+  if (instance.metadata().dtype == GetDType<float>()) {
+    auto* data = reinterpret_cast<float*>(instance.buffer()->data()->memory);
+    DisplayArray(os, data, instance.shape().GetNumElements());
+  } else if (instance.metadata().dtype == GetDType<double>()) {
+    auto* data = reinterpret_cast<double*>(instance.buffer()->data()->memory);
+    DisplayArray(os, data, instance.shape().GetNumElements());
+  } else if (instance.metadata().dtype == GetDType<int32_t>()) {
+    auto* data = reinterpret_cast<int32_t*>(instance.buffer()->data()->memory);
+    DisplayArray(os, data, instance.shape().GetNumElements());
+  } else if (instance.metadata().dtype == GetDType<int64_t>()) {
+    auto* data = reinterpret_cast<int64_t*>(instance.buffer()->data()->memory);
+    DisplayArray(os, data, instance.shape().GetNumElements());
+  } else {
+    LOG(FATAL) << "Not supported dtype [" << instance.metadata().dtype.name()
+               << " " << static_cast<int>(instance.metadata().dtype.kind())
+               << "] in print";
+  }
+
+  os << "]";
+
+  return os;
+}
+
+DenseHostTensor::~DenseHostTensor() {}
+
+void* DenseHostTensor::raw_data() const { return buffer_->data()->memory; }
+
+}  // namespace infrt::tensor
