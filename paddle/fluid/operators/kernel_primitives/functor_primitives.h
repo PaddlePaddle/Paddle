eafdf1619f540de607feeb54d7763c831cd29a3f
crystal <62974595+Zjq9409@users.noreply.github.com>
2021-12-08 14:07:59 +0800

    implementation of broadcast sub backward by reduce (#37754)
    * add boardcast_sub
    
    * add boardcast_sub

--- paddle/fluid/operators/kernel_primitives/functor_primitives.h
+++ paddle/fluid/operators/kernel_primitives/functor_primitives.h
@@ -101,6 +101,20 @@ struct InverseFunctor {
 };
 
 /**
+ * @brief Default inverse functor
+ */
+template <typename Tx, typename Ty = Tx>
+struct InverseFunctor {
+  HOSTDEVICE inline InverseFunctor() {}
+
+  HOSTDEVICE explicit inline InverseFunctor(int n) {}
+
+  HOSTDEVICE inline Ty operator()(const Tx& x) const {
+    return static_cast<Ty>(-x);
+  }
+};
+
+/**
  * @brief Default unary square functor
  */
 template <typename Tx, typename Ty = Tx>
