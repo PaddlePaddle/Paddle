e848b716fd3e71b9c9a180143862187b0f33d677
CtfGo <ctfeng66@163.com>
2021-12-08 02:34:05 +0000

    add a subdirectory named cinn in operators and move releated files into it

--- paddle/fluid/operators/cinn/cinn_launch_op.cc
+++ paddle/fluid/operators/cinn/cinn_launch_op.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/operators/cinn_launch_op.h"
+#include "paddle/fluid/operators/cinn/cinn_launch_op.h"
 
 #include <functional>
 #include <vector>
