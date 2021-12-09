820766297f48ba747c2719ff209d5c860bc84bb2
Wangzheee <634486483@qq.com>
2021-12-09 04:07:03 +0000

    fix fc_op when ernie_oss(x_dim.nbDims - x_num_col_dims ==)

--- paddle/fluid/inference/tensorrt/convert/fc_op.cc
+++ paddle/fluid/inference/tensorrt/convert/fc_op.cc
@@ -67,13 +67,7 @@ class FcOpConverter : public OpConverter {
                                      nvinfer1::Dims x_dim, int x_num_col_dims) {
     // add shuffle after fc
     nvinfer1::Dims reshape_after_fc_dim;
-    if (engine_->use_oss() && engine_->with_ernie() && x_dim.nbDims == 4 &&
-        x_dim.d[2] == 1 && x_dim.d[3] == 1 && x_num_col_dims == 1) {
-      // If use tensorrt'oss, the x_dim and x_num_col_dims need change
-      reshape_after_fc_dim.nbDims = 4;
-    } else {
-      reshape_after_fc_dim.nbDims = x_num_col_dims + 1;
-    }
+    reshape_after_fc_dim.nbDims = x_num_col_dims + 1;
     for (int i = 0; i < reshape_after_fc_dim.nbDims; i++) {
       reshape_after_fc_dim.d[i] = 0;
     }
@@ -178,6 +172,8 @@ class FcOpConverter : public OpConverter {
               ("fc_op_int8_reshape_after_fc: Shuffle (Output: " + output_name +
                ")")
                   .c_str());
+          engine_->SetTensorDynamicRange(fc_after_reshape_int8->getOutput(0),
+                                         out_scale);
           nvinfer1::IActivationLayer* relu_layer_int8 = TRT_ENGINE_ADD_LAYER(
               engine_, Activation, *(fc_after_reshape_int8->getOutput(0)),
               nvinfer1::ActivationType::kRELU);
@@ -242,23 +238,72 @@ class FcOpConverter : public OpConverter {
     }
     // If use tensorrt'oss, the x_dim and x_num_col_dims need change
     if (engine_->use_oss() && engine_->with_ernie() && x_dim.nbDims == 4 &&
-        x_dim.d[2] == 1 && x_dim.d[3] == 1 && x_num_col_dims == 2) {
+        x_dim.d[3] == 1 && x_num_col_dims == 2) {
       x_num_col_dims = 1;
     }
-    PADDLE_ENFORCE_GT(
-        x_dim.nbDims, x_num_col_dims,
-        platform::errors::InvalidArgument(
-            "Params and input dims mismatch. Paddle-TRT FC "
-            "converter expects x_dim.nbDims > x_num_col_dims, but "
-            "x_dim.nbDims : %d, x_num_col_dims : %d.",
-            x_dim.nbDims, x_num_col_dims));
-    auto* reshape_before_fc_layer =
-        reshape_before_fc(X, x_dim, x_num_col_dims, output_name);
-    auto* reshape_itensor = reshape_before_fc_layer->getOutput(0);
-    if (enable_int8) {
-      engine_->SetTensorDynamicRange(reshape_itensor, in_scale);
+    if (x_dim.nbDims - x_num_col_dims ==
+        3) {  // needn't reshape input before and after fc
+      if (enable_int8) {
+        // add conv layer
+        nvinfer1::DimsHW nv_ksize(1, 1);
+        auto* fc_layer_int8 =
+            TRT_ENGINE_ADD_LAYER(engine_, Convolution, *X, n_output, nv_ksize,
+                                 weight.get(), bias.get());
+        if (activation_type == "relu") {
+          fc_layer_int8->setName(
+              ("fc_op_int8: Convolution (Output: " + output_name + ")")
+                  .c_str());
+          PADDLE_ENFORCE_EQ(
+              op_desc.HasAttr("out_threshold"), true,
+              platform::errors::InvalidArgument(
+                  "must have out threshold in fc layers in int8 mode"));
+          float out_scale =
+              BOOST_GET_CONST(float, op_desc.GetAttr("out_threshold"));
+          engine_->SetTensorDynamicRange(fc_layer_int8->getOutput(0),
+                                         out_scale);
+          nvinfer1::IActivationLayer* relu_layer_int8 = TRT_ENGINE_ADD_LAYER(
+              engine_, Activation, *(fc_layer_int8->getOutput(0)),
+              nvinfer1::ActivationType::kRELU);
+          RreplenishLayerAndOutput(relu_layer_int8, "relu_after_fc_int8",
+                                   {output_name}, test_mode);
+        } else {
+          RreplenishLayerAndOutput(fc_layer_int8, "fc_op_int8: Convolution",
+                                   {output_name}, test_mode);
+        }
+      } else {
+        // add fc layer
+        auto* fc_layer_float = TRT_ENGINE_ADD_LAYER(
+            engine_, FullyConnected, *X, n_output, weight.get(), bias.get());
+        if (activation_type == "relu") {
+          fc_layer_float->setName(
+              ("fc_op_float: FullyConnected (Output: " + output_name + ")")
+                  .c_str());
+          nvinfer1::IActivationLayer* relu_layer_float = TRT_ENGINE_ADD_LAYER(
+              engine_, Activation, *(fc_layer_float->getOutput(0)),
+              nvinfer1::ActivationType::kRELU);
+          RreplenishLayerAndOutput(relu_layer_float, "relu_after_fc_float",
+                                   {output_name}, test_mode);
+        } else {
+          RreplenishLayerAndOutput(fc_layer_float, "shuffle_after_fc",
+                                   {output_name}, test_mode);
+        }
+      }
+    } else {  // need reshape input before and after fc
+      PADDLE_ENFORCE_GT(
+          x_dim.nbDims, x_num_col_dims,
+          platform::errors::InvalidArgument(
+              "Params and input dims mismatch. Paddle-TRT FC "
+              "converter expects x_dim.nbDims > x_num_col_dims, but "
+              "x_dim.nbDims : %d, x_num_col_dims : %d.",
+              x_dim.nbDims, x_num_col_dims));
+      auto* reshape_before_fc_layer =
+          reshape_before_fc(X, x_dim, x_num_col_dims, output_name);
+      auto* reshape_itensor = reshape_before_fc_layer->getOutput(0);
+      if (enable_int8) {
+        engine_->SetTensorDynamicRange(reshape_itensor, in_scale);
+      }
+      regist_fc(reshape_itensor, n_output, weight, bias);
     }
-    regist_fc(reshape_itensor, n_output, weight, bias);
   }
 };
 
