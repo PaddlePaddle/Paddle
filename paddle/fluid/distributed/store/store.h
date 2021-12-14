ef755a0cd6a65b7ea08f83b26ba5a4ed03c0c850
sandyhouse <lilong12@baidu.com>
2021-12-14 17:29:22 +0800

    add store

---
+++ paddle/fluid/distributed/store/store.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace paddle {
+namespace distributed {
+
+class Store {
+ public:
+  static constexpr std::chrono::milliseconds kDefaultTimeout =
+      std::chrono::seconds(360);
+  static constexpr std::chrono::milliseconds kDefaultTimeout =
+      std::chrono::zero();
+
+  Store() : timeout_(kDefaultTimeout) {}
+  explicit Store(const std::chrono::milliseconds& timeout)
+      : timeout_(timeout) {}
+  virtual ~Store() {}
+
+  virtual void set(const std::string& key,
+                   const std::vector<uint8_t>& value) = 0;
+  virtual std::vector<uint8_t> get(const std::string& key) = 0;
+
+  virtual int64_t add(const std::string& key, int64_t value) = 0;
+  virtual bool removeKey(const std::string& key) = 0;
+  virtual void wait(const std::vector<std::string>& keys) = 0;
+  virtual void wait(const std::vector<std::string>& keys,
+                    const std::chrono::milliseconds& timeout) = 0;
+
+  virtual const std::chrono::milliseconds& getTimeout() const {
+    return timeout_;
+  }
+  virtual void setTimeout(const std::chrono::milliseconds& timeout) {
+    timeout_ = timeout;
+  }
+
+ private:
+  std::chrono::milliseconds timeout_;
+}
+
+}  // namespace distributed
+}  // namespace paddle
