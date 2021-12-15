8f7675d4932e01de1b0be2a24f6ad41d0ef1fb93
limin2021 <1121099234@qq.com>
2021-12-15 08:16:28 +0000

    Code format.

--- python/paddle/fluid/tests/unittests/test_scatter_op.py
+++ python/paddle/fluid/tests/unittests/test_scatter_op.py
@@ -276,22 +276,22 @@ class TestScatterOpFp16(OpTest):
         self.__class__.op_type = "scatter"
         # compute grad in the following code handly.
         self.__class__.no_need_check_grad = True
-        self.x_type='float16'
+        self.x_type = 'float16'
         self.x_np = np.ones((3, 3)).astype(self.x_type)
         self.index_np = np.array([1, 2]).astype("int32")
         self.updates_np = np.random.random((2, 3)).astype(self.x_type)
         self.output_np = np.copy(self.x_np)
         self.output_np[self.index_np] = self.updates_np
         self.dout_np = np.random.random((3, 3)).astype(self.x_type)
-        
+
         # compute ref_dx
         self.ref_dx = np.copy(self.dout_np)
         zero_np = np.zeros((2, 3)).astype(self.x_type)
         self.ref_dx[self.index_np] = zero_np
 
     def compute_ref_grad_updates(self):
-        ref_grad_updates = paddle.gather(paddle.to_tensor(self.dout_np), 
-                             paddle.to_tensor(self.index_np))
+        ref_grad_updates = paddle.gather(
+            paddle.to_tensor(self.dout_np), paddle.to_tensor(self.index_np))
         return ref_grad_updates
 
     def test_scatter_fp16(self):
@@ -304,8 +304,10 @@ class TestScatterOpFp16(OpTest):
             [out_tensor], [paddle.to_tensor(self.dout_np)], retain_graph=True)
         ref_grad_updates = self.compute_ref_grad_updates()
         np.testing.assert_allclose(
-            ref_grad_updates.numpy(), updates_tensor.grad.numpy(), 
-            rtol=1e-5, atol=1e-5)
+            ref_grad_updates.numpy(),
+            updates_tensor.grad.numpy(),
+            rtol=1e-5,
+            atol=1e-5)
         np.testing.assert_allclose(
             self.ref_dx, x_tensor.grad.numpy(), rtol=1e-5, atol=1e-5)
 
