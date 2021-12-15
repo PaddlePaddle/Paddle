9250e6f974d9291357175f9f435b77c10b60f4c8
zmxdream <zmxdream@pku.edu.cn>
2021-12-16 00:51:06 +0800

    add ut for ins tag cuda kernel

--- python/paddle/fluid/tests/unittests/test_filter_by_instag_op.py
+++ python/paddle/fluid/tests/unittests/test_filter_by_instag_op.py
@@ -284,5 +284,60 @@ class TestFilterByInstagOp7(OpTest):
         pass
 
 
+class TestFilterByInstagOp_GPU(OpTest):
+    def setUp(self):
+        self.op_type = 'filter_by_instag'
+        x1 = np.zeros((36, 4), dtype=np.float64)
+        for i in range(36):
+            for j in range(4):
+                x1[i, j] = i
+        x1_lod = [[1, 2, 3, 4, 5, 6, 7, 8]]
+
+        x2 = np.array([[1], [2], [1], [2], [1], [2], [1], [2]]).astype('int64')
+        x2_lod = [[1, 1, 1, 1, 1, 1, 1, 1]]
+
+        x3 = np.array([2]).astype('int64')
+
+        out = np.zeros((20, 4), dtype=np.float64)
+        out_lod = [[2, 4, 6, 8]]
+        start_num_lst = [1, 6, 15, 28]
+
+        ln = 0
+        for i in range(4):
+            start = start_num_lst[i]
+            len = out_lod[0][i]
+            for j in range(len):
+                cur = start + j
+                for k in range(4):
+                    out[ln, k] = cur
+                ln += 1
+
+        mmap = np.array(
+            [[0, 1, 2], [2, 6, 4], [6, 15, 6], [12, 28, 8]]).astype('int64')
+        mmap_lod = [[1, 1, 1, 1]]
+
+        loss_weight = np.array([[1], [1], [1], [1]]).astype('double')
+
+        self.inputs = {
+            'Ins': (x1, x1_lod),
+            'Ins_tag': (x2, x2_lod),
+            'Filter_tag': x3,
+        }
+        self.outputs = {
+            'Out': (out, out_lod),
+            'LossWeight': (loss_weight, mmap_lod),
+            'IndexMap': (mmap, mmap_lod)
+        }
+
+        self.attrs = {'is_lod': True, 'out_val_if_empty': 0}
+
+    def test_check_output(self):
+        self.check_output()
+
+    def test_check_grad(self):
+        self.check_grad(
+            ['Ins'], 'Out', no_grad_set=set(['Ins_tag', 'Filter_tag']))
+
+
 if __name__ == '__main__':
     unittest.main()
