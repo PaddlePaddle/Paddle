77ef8690e937c1397566bf4445ba64337641917b
Chen Weihang <chenweihang@baidu.com>
2021-12-15 13:13:55 +0000

    fix header match error

--- cmake/inference_lib.cmake
+++ cmake/inference_lib.cmake
@@ -218,16 +218,16 @@ include_directories(${CMAKE_BINARY_DIR}/../paddle/fluid/framework/io)
 
 # copy api headers for pten & custom op
 copy(inference_lib_dist
-        SRCS  ${PADDLE_SOURCE_DIR}/paddle/pten/api/ext/*
+        SRCS  ${PADDLE_SOURCE_DIR}/paddle/pten/api/ext/*.h
         DSTS  ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/experimental/pten/api/ext/)
 copy(inference_lib_dist
-        SRCS  ${PADDLE_SOURCE_DIR}/paddle/pten/api/include/*
+        SRCS  ${PADDLE_SOURCE_DIR}/paddle/pten/api/include/*.h
         DSTS  ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/experimental/pten/api/include/)
 copy(inference_lib_dist
         SRCS  ${PADDLE_SOURCE_DIR}/paddle/pten/api/all.h
         DSTS  ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/experimental/pten/api/)
 copy(inference_lib_dist
-        SRCS  ${PADDLE_SOURCE_DIR}/paddle/pten/common/*
+        SRCS  ${PADDLE_SOURCE_DIR}/paddle/pten/common/*.h
               ${PADDLE_SOURCE_DIR}/paddle/fluid/platform/bfloat16.h
               ${PADDLE_SOURCE_DIR}/paddle/fluid/platform/complex.h
               ${PADDLE_SOURCE_DIR}/paddle/fluid/platform/float16.h
