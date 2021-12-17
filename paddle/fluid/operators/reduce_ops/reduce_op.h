4d6f1bae21a8b9683ab2149d7188095d88747a54
chentianyu03 <chentianyu03@baidu.com>
2021-12-17 15:21:52 +0800

    [pten] modify reduce_sum reduce_mean args (#38216)
    * modify sum mean args
    
    * add GetExpectedPtenKernelArgs for redcue_op
    
    * modify kernel args number
    
    * modify kernel args number

--- paddle/fluid/operators/reduce_ops/reduce_op.h
+++ paddle/fluid/operators/reduce_ops/reduce_op.h
@@ -546,6 +546,25 @@ class ReduceOp : public framework::OperatorWithKernel {
     }
     return framework::OpKernelType(input_data_type, ctx.GetPlace());
   }
+
+  framework::KernelSignature GetExpectedPtenKernelArgs(
+      const framework::ExecutionContext& ctx) const override {
+    if (Type() == "reduce_sum") {
+      if (ctx.InputVar("X")->IsType<framework::LoDTensor>()) {
+        return framework::KernelSignature(
+            "sum", {"X"}, {"dim", "keep_dim", "reduce_all", "out_dtype"},
+            {"Out"});
+      }
+    }
+    if (Type() == "reduce_mean") {
+      if (ctx.InputVar("X")->IsType<framework::LoDTensor>()) {
+        return framework::KernelSignature(
+            "mean", {"X"}, {"dim", "keep_dim", "reduce_all"}, {"Out"});
+      }
+    }
+    // TODO(chentianyu03): support other cases after selected rows added
+    return framework::KernelSignature("reduce.unregistered", {}, {}, {});
+  }
 };
 
 class ReduceOpUseInputPlace : public ReduceOp {
