fc701369095706ce7f6d7282b60de37b4759cba5
Yuang Liu <liuyuang@baidu.com>
2021-12-17 10:36:40 +0800

    [fleet_executor] run time graph on python side (#38164)

--- paddle/fluid/distributed/fleet_executor/fleet_executor.cc
+++ paddle/fluid/distributed/fleet_executor/fleet_executor.cc
@@ -17,6 +17,9 @@
 #include "paddle/fluid/distributed/fleet_executor/message_bus.h"
 #include "paddle/fluid/distributed/fleet_executor/runtime_graph.h"
 #include "paddle/fluid/distributed/fleet_executor/task_node.h"
+#include "paddle/fluid/framework/executor_gc_helper.h"
+#include "paddle/fluid/framework/op_desc.h"
+#include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/framework/operator.h"
 #include "paddle/fluid/framework/program_desc.h"
 #include "paddle/fluid/framework/scope.h"
@@ -38,16 +41,30 @@ void FleetExecutor::Init(
     const platform::Place& place, const std::vector<TaskNode*>& task_nodes,
     const std::unordered_map<int64_t, int64_t>& task_id_to_rank) {
   if (task_nodes.size() == 0) {
+    LOG(INFO) << "fleet executor will use c++ side scheduler construction.";
     runtime_graph_ = std::make_shared<RuntimeGraph>(program_desc, exe_desc_);
   } else {
+    LOG(INFO) << "fleet executor has been set dependency on python side.";
+    // TODO(fleet_exe devs): the unused_vars should be got from run time graph
+    std::vector<std::unique_ptr<framework::OperatorBase>> ops;
+    for (auto task_node : task_nodes) {
+      for (auto op : task_node->ops()) {
+        ops.emplace_back(std::unique_ptr<framework::OperatorBase>(op));
+      }
+    }
+    auto unused_vars = framework::GetUnusedVars(program_desc.Block(0), ops, {});
     runtime_graph_ = std::make_shared<RuntimeGraph>();
     std::unordered_map<int64_t, TaskNode*> interceptor_id_to_task;
     for (auto task_node : task_nodes) {
+      task_node->SetUnusedVars(unused_vars);
       int64_t interceptor_id = task_node->task_id();
       interceptor_id_to_task.emplace(interceptor_id, task_node);
     }
     runtime_graph_->SetInterceptorIdToRank(task_id_to_rank);
     runtime_graph_->SetInterceptorIdToNode(interceptor_id_to_task);
+    for (auto& unique_op : ops) {
+      unique_op.release();
+    }
   }
   root_scope_ = scope;
   place_ = place;
