bf6f48fcc72a166481c542713944e447e6b9ab75
pangyoki <pangyoki@126.com>
2021-12-20 12:48:39 +0000

    fix cuda and nccl, wrong

--- tools/dockerfile/performance_docker/ubuntu16_perf.sh
+++ tools/dockerfile/performance_docker/ubuntu16_perf.sh
@@ -41,18 +41,40 @@ function install_whl(){
 
 function set_cuda_env(){
   if [[ ${ref_CUDA_MAJOR} == "11.2" ]];then
-      sed -i 's#<set_cuda_en>#RUN ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.8 /usr/lib/x86_64-linux-gnu/libcudnn.so \&\& \\ \
-    ln -s /usr/lib/x86_64-linux-gnu/libnccl.so.2 /usr/lib/x86_64-linux-gnu/libnccl.so \&\& \\ \
-    ln -s /usr/local/cuda-11.2/targets/x86_64-linux/lib/libcusolver.so.10 /usr/local/cuda-11.2/targets/x86_64-linux/lib/libcusolver.so \&\& \\ \
+      sed -i 's#<set_cuda_env>#RUN ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.8 /usr/lib/x86_64-linux-gnu/libcudnn.so \&\& \\ \
+    ln -s /usr/local/cuda-11.2/targets/x86_64-linux/lib/libcusolver.so.11 /usr/local/cuda-11.2/targets/x86_64-linux/lib/libcusolver.so \&\& \\ \
     ln -s /usr/local/cuda-11.2/targets/x86_64-linux/lib/libcublas.so.11 /usr/local/cuda-11.2/targets/x86_64-linux/lib/libcublas.so \
-ENV LD_LIBRARY_PATH=/usr/local/cuda-11.2/targets/x86_64-linux/lib/:${LD_LIBRARY_PATH} #g' Dockerfile.tmp
+ENV LD_LIBRARY_PATH=/usr/local/lib/:/usr/local/cuda-11.2/targets/x86_64-linux/lib/:${LD_LIBRARY_PATH} #g' Dockerfile.tmp
   elif [[ ${ref_CUDA_MAJOR} == "10.2" ]];then
-      sed -i 's#<set_cuda_en>#RUN rm -f /usr/lib/x86_64-linux-gnu/libcublas.so \&\& \\ \
+      sed -i 's#<set_cuda_env>#RUN rm -f /usr/lib/x86_64-linux-gnu/libcublas.so \&\& \\ \
     ln -s /usr/lib/x86_64-linux-gnu/libcublas.so.10 /usr/lib/x86_64-linux-gnu/libcublas.so \
 RUN ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.7 /usr/lib/x86_64-linux-gnu/libcudnn.so \&\& \\ \
     ln -s /usr/lib/x86_64-linux-gnu/libnccl.so.2 /usr/lib/x86_64-linux-gnu/libnccl.so \&\& \\ \
     ln -s /usr/local/cuda-10.2/targets/x86_64-linux/lib/libcusolver.so.10 /usr/local/cuda-10.2/targets/x86_64-linux/lib/libcusolver.so \
 ENV LD_LIBRARY_PATH=/usr/local/cuda-10.2/targets/x86_64-linux/lib/:${LD_LIBRARY_PATH} #g' Dockerfile.tmp
+  fi
+}
+
+# wrong code, bash file
+function install_nccl2(){
+  if [[ ${ref_CUDA_MAJOR} == "11.2" ]];then
+      sed -i 's#<install_nccl2>#DEB="nccl-repo-ubuntu1604-2.7.8-ga-cuda10.2_1-1_amd64.deb" \
+URL="http://nccl2-deb.cdn.bcebos.com/$DEB" \
+DIR="/nccl2" \
+mkdir -p $DIR \
+wget -q -O $DIR/$DEB $URL \
+cd $DIR \&\& ar x $DEB \&\& tar xf data.tar.xz \
+DEBS=$(find ./var/ -name "*.deb") \
+for sub_deb in $DEBS; do \
+  echo $sub_deb \
+  ar x $sub_deb \&\& tar xf data.tar.xz \
+done \
+mv -f usr/include/nccl.h /usr/local/include/ \
+mv -f usr/lib/x86_64-linux-gnu/libnccl* /usr/local/lib/ \
+rm /usr/include/nccl.h \
+rm -rf $DIR #g' Dockerfile.tmp
+  else
+      sed -i 's#<install_nccl2># #g' Dockerfile.tmp
   fi
 }
 
@@ -79,6 +101,7 @@ function main(){
   ref_whl
   make_dockerfile
   install_dali
+  install_nccl2
   set_cuda_env
   install_whl
 }
