c8021277217a1389988517807c1d0078b84ac69b
zhangjingyu06 <zhangjingyu06@baidu.com>
2021-12-20 15:08:29 +0800

    [XPU]add split op for kunlun,*test=kunlun

--- paddle/fluid/operators/split_op_xpu.cc
+++ paddle/fluid/operators/split_op_xpu.cc
@@ -1,21 +1,21 @@
-/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
-
+/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
+
 #ifdef PADDLE_WITH_XPU
 #include "paddle/fluid/operators/split_op.h"
 #include <string>
 #include <vector>
-#include "paddle/fluid/platform/device/xpu/xpu_header.h
+#include "paddle/fluid/platform/device/xpu/xpu_header.h"
 
 namespace paddle {
 namespace operators {
@@ -33,7 +33,7 @@ class SplitXPUKernel : public framework::OpKernel<T> {
     int axis = ctx.Attr<int>("axis");
     auto& dev_ctx = ctx.template device_context<DeviceContext>();
     auto in_dims = input->dims();
-    
+
     auto input_shape = framework::vectorize<int>(in_dims);
     std::vector<int> split_lists;
     std::vector<T*> out_ptrs;
@@ -46,11 +46,12 @@ class SplitXPUKernel : public framework::OpKernel<T> {
       split_lists.push_back(output[i]->dims()[axis]);
     }
 
-    int r = xpu::split<T>(dev_ctx.x_context(), input->data<T>(), out_ptrs, input_shape, split_lists, axis);
-    PADDLE_ENFORCE_EQ(r, XPU_SUCCESS,
-                      platform::errors::External(
-                          "XPU split kernel return wrong value[%d %s]", r,
-                          XPUAPIErrorMsg[r]));
+    int r = xpu::split<T>(dev_ctx.x_context(), input->data<T>(), out_ptrs,
+                          input_shape, split_lists, axis);
+    PADDLE_ENFORCE_EQ(
+        r, XPU_SUCCESS,
+        platform::errors::External("XPU split kernel return wrong value[%d %s]",
+                                   r, XPUAPIErrorMsg[r]));
   }
 };
 
@@ -58,7 +59,7 @@ class SplitXPUKernel : public framework::OpKernel<T> {
 }  // namespace paddle
 
 namespace ops = paddle::operators;
-REGISTER_OP_XPU_KERNEL(split, 
-                       ops::SplitXPUKernel<paddle::platform::XPUDeviceContext, float>,
-                       ops::SplitXPUKernel<paddle::platform::XPUDeviceContext, int>);
+REGISTER_OP_XPU_KERNEL(
+    split, ops::SplitXPUKernel<paddle::platform::XPUDeviceContext, float>,
+    ops::SplitXPUKernel<paddle::platform::XPUDeviceContext, int>);
 #endif
--- python/paddle/fluid/tests/unittests/xpu/test_split_op_xpu.py
+++ python/paddle/fluid/tests/unittests/xpu/test_split_op_xpu.py
@@ -1,10 +1,10 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#  Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,14 +18,14 @@ sys.path.append("..")
 import unittest
 import numpy as np
 import paddle.fluid.core as core
-from op_test import OpTest, skip_check_grad_ci
+from op_test import OpTest
 from op_test_xpu import XPUOpTest
 import paddle
 import paddle.fluid as fluid
 from paddle.fluid import Program, program_guard
 
+
 # test with attr(num)
-@skip_check_grad_ci(reason="No support for split_grad_xpu op.")
 class TestSplitOp(XPUOpTest):
     def initDefaultParameters(self):
         self.dtype = 'float32'
@@ -57,6 +57,7 @@ class TestSplitOp(XPUOpTest):
             place = paddle.XPUPlace(0)
             self.check_output_with_place(place)
 
+
 # attr(axis) is Tensor
 class TestSplitOp_2(XPUOpTest):
     def initDefaultParameters(self):
@@ -87,6 +88,7 @@ class TestSplitOp_2(XPUOpTest):
             paddle.enable_static()
             place = paddle.XPUPlace(0)
             self.check_output_with_place(place)
+
 
 # attr(sections) is list containing Tensor
 class TestSplitOp_3(XPUOpTest):
@@ -128,6 +130,8 @@ class TestSplitOp_3(XPUOpTest):
             paddle.enable_static()
             place = paddle.XPUPlace(0)
             self.check_output_with_place(place)
+
+
 # unknow section
 class TestSplitOp_4(XPUOpTest):
     def initDefaultParameters(self):
@@ -153,6 +157,46 @@ class TestSplitOp_4(XPUOpTest):
         out = np.split(self.x, self.indices_or_sections, self.axis)
         self.outputs = {'Out': [('out%d' % i, out[i]) \
                                 for i in range(len(out))]}
+
+    def test_check_output(self):
+        if paddle.is_compiled_with_xpu():
+            paddle.enable_static()
+            place = paddle.XPUPlace(0)
+            self.check_output_with_place(place)
+
+
+# test with int32
+class TestSplitOp_5(XPUOpTest):
+    def initDefaultParameters(self):
+        self.dtype = 'int32'
+        self.x = np.random.random((4, 5, 6)).astype(self.dtype)
+        self.axis = 2
+        self.sections = []
+        self.num = 3
+        self.indices_or_sections = 3
+
+    def setUp(self):
+        self.__class__.op_type = 'split'
+        self.use_xpu = True
+        self.use_mkldnn = False
+        self.initDefaultParameters()
+        self.inputs = {'X': self.x}
+        self.attrs = {
+            'axis': self.axis,
+            'sections': self.sections,
+            'num': self.num
+        }
+
+        out = np.split(self.x, self.indices_or_sections, self.axis)
+        self.outputs = {'Out': [('out%d' % i, out[i]) \
+                                for i in range(len(out))]}
+
+    def test_check_output(self):
+        if paddle.is_compiled_with_xpu():
+            paddle.enable_static()
+            place = paddle.XPUPlace(0)
+            self.check_output_with_place(place)
+
 
 if __name__ == '__main__':
     unittest.main()
