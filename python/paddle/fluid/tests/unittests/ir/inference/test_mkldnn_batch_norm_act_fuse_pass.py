c425c6275f3bcbf7486fc952c1cc0c86a786d95d
baoachun <962571062@qq.com>
2021-12-21 12:47:16 +0000

    update ut

--- python/paddle/fluid/tests/unittests/ir/inference/test_mkldnn_batch_norm_act_fuse_pass.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_mkldnn_batch_norm_act_fuse_pass.py
@@ -114,8 +114,7 @@ class TestScaleMatmulMkldnnFusePass(PassAutoScanTest):
         yield config, ["batch_norm"], (1e-5, 1e-5)
 
     def test(self):
-        self.run_and_statis(
-            max_examples=10, quant=False, passes=["batch_norm_act_fuse_pass"])
+        self.run_and_statis(quant=False, passes=["batch_norm_act_fuse_pass"])
 
 
 if __name__ == "__main__":
