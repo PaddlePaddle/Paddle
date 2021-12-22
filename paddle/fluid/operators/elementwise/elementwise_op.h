4bc5ccdcd14d6d9580ded20f571b3756dc3d9b90
jakpiase <jakpia21@gmail.com>
2021-12-22 02:48:09 +0100

    switched to tensor from lodtensor

--- paddle/fluid/operators/elementwise/elementwise_op.h
+++ paddle/fluid/operators/elementwise/elementwise_op.h
@@ -120,7 +120,7 @@ class ElementwiseOp : public framework::OperatorWithKernel {
 #ifdef PADDLE_WITH_MKLDNN
     // if dims rank is larger than 5 we fallback to native kernel because oneDNN
     // is always choosing reference implementation which is extremely slow
-    if (ctx.Input<paddle::framework::LoDTensor>("X")->dims().size() > 5)
+    if (ctx.Input<paddle::framework::Tensor>("X")->dims().size() > 5)
       return framework::OpKernelType(input_data_type, ctx.GetPlace());
 
     if (this->CanMKLDNNBeUsed(ctx, input_data_type)) {
