2ad8d670a0ffcfc48424aa7e0054afd81617a28a
yaoxuefeng6 <yaoxuefeng@baidu.com>
2021-12-22 15:38:28 +0800

    add test

--- paddle/fluid/operators/pull_gpups_sparse_op.h
+++ paddle/fluid/operators/pull_gpups_sparse_op.h
@@ -39,7 +39,7 @@ static void PullGpuPSSparseFunctor(const framework::ExecutionContext &ctx) {
     slot_lengths[i] = slot->numel();
     auto *output = outputs[i]->mutable_data<T>(ctx.GetPlace());
     // double type is not fully supported now
-    all_values[i] = reinterpret_cast<float*>(output);
+    all_values[i] = reinterpret_cast<float *>(output);
   }
 #ifdef PADDLE_WITH_HETERPS
   auto gpu_ps_ptr = paddle::framework::PSGPUWrapper::GetInstance();
