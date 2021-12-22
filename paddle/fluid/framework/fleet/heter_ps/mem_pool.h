96fc3fe38460641862f06c056394e47178a51647
yaoxuefeng6 <yaoxuefeng@baidu.com>
2021-12-22 22:55:09 +0800

    fix

--- paddle/fluid/framework/fleet/heter_ps/mem_pool.h
+++ paddle/fluid/framework/fleet/heter_ps/mem_pool.h
@@ -15,71 +15,64 @@ limitations under the License. */
 #pragma once
 
 #ifdef PADDLE_WITH_HETERPS
-// #include "paddle/fluid/framework/fleet/heter_ps/cudf/concurrent_unordered_map.cuh.h"
-#include "paddle/fluid/framework/fleet/heter_ps/cudf/managed.cuh"
+// #include
+// "paddle/fluid/framework/fleet/heter_ps/cudf/concurrent_unordered_map.cuh.h"
 #include <iostream>
+#include "paddle/fluid/framework/fleet/heter_ps/cudf/managed.cuh"
 
 namespace paddle {
 namespace framework {
 
-
 class MemoryPool {
  public:
-  MemoryPool(size_t capacity, size_t block_size) : capacity_(capacity), block_size_(block_size) {
-    VLOG(3) << "mem_pool init with block_size: " << block_size << " capacity: " << capacity;
+  MemoryPool(size_t capacity, size_t block_size)
+      : capacity_(capacity), block_size_(block_size) {
+    VLOG(3) << "mem_pool init with block_size: " << block_size
+            << " capacity: " << capacity;
     mem_ = (char*)malloc(block_size * capacity_);
   }
   ~MemoryPool() {
     VLOG(3) << "mem pool delete";
     free(mem_);
   }
-  size_t block_size() {
-    return block_size_;
+  size_t block_size() { return block_size_; }
+  char* mem() { return mem_; }
+
+  size_t capacity() { return capacity_; }
+  size_t byte_size() { return capacity_ * block_size_; }
+  void* mem_address(const uint32_t& idx) {
+    return (void*)&mem_[(idx)*block_size_];
   }
-  char *mem() {
-    return mem_;
-  }
-  
-  size_t capacity() {
-    return capacity_;
-  }
-  size_t byte_size() {
-    return capacity_ * block_size_;
-  }
-  void* mem_address(const uint32_t &idx) {
-    return (void*)&mem_[(idx) * block_size_];
-  }
+
  private:
   char* mem_ = NULL;
   size_t capacity_;
   size_t block_size_;
 };
 
-
 class HBMMemoryPool : public managed {
  public:
-  HBMMemoryPool(size_t capacity, size_t block_size) : capacity_(capacity), block_size_(block_size) {}
+  HBMMemoryPool(size_t capacity, size_t block_size)
+      : capacity_(capacity), block_size_(block_size) {}
   HBMMemoryPool(MemoryPool* mem_pool) {
     capacity_ = mem_pool->capacity();
     block_size_ = mem_pool->block_size();
-    VLOG(3) << "hbm memory pool with capacity" << capacity_ << " bs: " << block_size_; 
+    VLOG(3) << "hbm memory pool with capacity" << capacity_
+            << " bs: " << block_size_;
     cudaMalloc(&mem_, block_size_ * capacity_);
-    cudaMemcpy(mem_, mem_pool->mem(), mem_pool->byte_size(), cudaMemcpyHostToDevice);
+    cudaMemcpy(mem_, mem_pool->mem(), mem_pool->byte_size(),
+               cudaMemcpyHostToDevice);
   }
-    
+
   ~HBMMemoryPool() {
     VLOG(3) << "delete hbm memory pool";
     cudaFree(mem_);
   }
- 
-  size_t block_size() {
-    return block_size_;
-  }
 
-  void clear(void) {
-    cudaMemset(mem_, 0, block_size_ * capacity_);
-  }
-  
+  size_t block_size() { return block_size_; }
+
+  void clear(void) { cudaMemset(mem_, 0, block_size_ * capacity_); }
+
   void reset(size_t capacity) {
     cudaFree(mem_);
     mem_ = NULL;
@@ -92,7 +85,8 @@ class HBMMemoryPool : public managed {
     for (size_t k = 0; k < 5; k++) {
       auto x = (FeatureValue*)(p.mem() + k * p.capacity());
       out << "show: " << x->show << " clk: " << x->clk << " slot: " << x->slot
-        << " lr: " << x->lr <<" mf_dim: " << x->mf_size <<  " mf_size: " << x->mf_size << " mf:";
+          << " lr: " << x->lr << " mf_dim: " << x->mf_size
+          << " mf_size: " << x->mf_size << " mf:";
       for (int i = 0; i < x->mf_dim + 1; ++i) {
         out << " " << x->mf[i];
       }
@@ -101,23 +95,18 @@ class HBMMemoryPool : public managed {
     return out;
   }
 
-  char *mem() {
-    return mem_;
+  char* mem() { return mem_; }
+
+  size_t capacity() { return capacity_; }
+  __forceinline__ __device__ void* mem_address(const uint32_t& idx) {
+    return (void*)&mem_[(idx)*block_size_];
   }
-  
-  size_t capacity() {
-    return capacity_;
-  }
-  __forceinline__
-  __device__ void* mem_address(const uint32_t &idx) {
-    return (void*)&mem_[(idx) * block_size_];
-  }
+
  private:
   char* mem_ = NULL;
   size_t capacity_;
   size_t block_size_;
 };
-
 
 }  // end namespace framework
 }  // end namespace paddle
