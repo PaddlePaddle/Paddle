ebff863a7997e7b1aaf8a46b3a52da3812c76d06
zhangbo9674 <zhangbo54@baidu.com>
2021-12-23 12:03:25 +0000

    fix bug

--- python/paddle/fluid/dygraph/amp/auto_cast.py
+++ python/paddle/fluid/dygraph/amp/auto_cast.py
@@ -128,12 +128,10 @@ def pure_fp16_initialize(models):
     for idx in range(len(models)):
         for layer in models[idx].sublayers(include_self=True):
             layer._casted_by_pure_fp16 = True
-            if len(layer._sub_layers) is 0:
-
-                if (layer._dtype is 'float16') or isinstance(layer, (
-                        paddle.nn.BatchNorm, paddle.nn.LayerNorm)):
-                    continue
-                layer.to(dtype='float16')
+            if (layer._dtype is 'float16') or isinstance(layer, (
+                    paddle.nn.BatchNorm, paddle.nn.LayerNorm)):
+                continue
+            layer.to(dtype='float16')
     return models
 
 
