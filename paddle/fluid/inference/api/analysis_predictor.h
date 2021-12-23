15ad7ee4f36f93edb979f2af75216a3df15afae8
Wilber <jiweibo@baidu.com>
2021-12-23 15:45:05 +0800

    Support external stream. (#38373)
    * support external stream.
    
    * update
    
    * update
    
    * update

--- paddle/fluid/inference/api/analysis_predictor.h
+++ paddle/fluid/inference/api/analysis_predictor.h
@@ -25,6 +25,7 @@
 #include "paddle/fluid/inference/api/details/reset_tensor_array.h"
 #include "paddle/fluid/inference/api/helper.h"
 #include "paddle/fluid/inference/api/paddle_inference_api.h"
+#include "paddle/fluid/platform/device/gpu/gpu_types.h"
 #include "paddle/fluid/platform/float16.h"
 #include "paddle/fluid/string/printf.h"
 #ifdef PADDLE_WITH_TESTING
@@ -171,6 +172,11 @@ class AnalysisPredictor : public PaddlePredictor {
   /// \return Whether the function executed successfully
   ///
   bool ZeroCopyRun() override;
+
+#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
+  // Note: Can only be used under thread_local semantics.
+  bool ExpRunWithExternalStream(const gpuStream_t stream);
+#endif
 
   ///
   /// \brief Create feed fetch variables
