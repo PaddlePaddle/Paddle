57eacfc1a47107b0cdf689ac0ddabe098686eabd
Baibaifan <yangfanbai@yahoo.com>
2021-12-23 14:46:42 +0000

    fix share buffer to

--- paddle/fluid/pybind/imperative.cc
+++ paddle/fluid/pybind/imperative.cc
@@ -1984,6 +1984,7 @@ void BindImperative(py::module *m_ptr) {
                  platform::errors::InvalidArgument(
                      "Tensor %s has not been initialized!", self->Name()));
              dst_->ShareBufferWith(*src);
+             dst_->ShareDataTypeWith(*src);
            })
       .def("_is_shared_buffer_with",
            [](const std::shared_ptr<imperative::VarBase> &self,
