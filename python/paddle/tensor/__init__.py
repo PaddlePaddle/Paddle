d7425495f375a23d313c8918eeb7d6f4ba70cae9
seemingwang <zsasuke@qq.com>
2021-12-23 13:49:42 +0000

    fix import

--- python/paddle/tensor/__init__.py
+++ python/paddle/tensor/__init__.py
@@ -214,6 +214,7 @@ from .math import diff  # noqa: F401
 from .math import angle  # noqa: F401
 from .math import fmax  # noqa: F401
 from .math import fmin  # noqa: F401
+
 from .random import multinomial  # noqa: F401
 from .random import standard_normal  # noqa: F401
 from .random import normal  # noqa: F401
