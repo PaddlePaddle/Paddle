80a6eb5c713ff83408f4534a20e0148905c9bad4
JiabinYang <360788950@qq.com>
2021-12-24 07:09:12 +0000

    fix windows error

--- paddle/fluid/pybind/imperative.cc
+++ paddle/fluid/pybind/imperative.cc
@@ -763,10 +763,6 @@ void BindImperative(py::module *m_ptr) {
         []() { imperative::SetLoadProcessSignalHandler(); });
   m.def("_throw_error_if_process_failed",
         []() { imperative::ThrowErrorIfLoadProcessFailed(); });
-  m.def("_set_eager_tracer",
-        [](const std::shared_ptr<imperative::Tracer> &tracer) {
-          egr::Controller::Instance().SetCurrentTracer(tracer);
-        });
   // Dygraph DataLoader reader process & thread related functions
   m.def(
       "_convert_to_tensor_list",
@@ -869,7 +865,10 @@ void BindImperative(py::module *m_ptr) {
 
   m.def("start_imperative_gperf_profiler",
         []() { imperative::StartProfile(); });
-
+  m.def("_set_eager_tracer",
+        [](const std::shared_ptr<imperative::Tracer> &tracer) {
+          egr::Controller::Instance().SetCurrentTracer(tracer);
+        });
   m.def("stop_imperative_gperf_profiler", []() { imperative::StopProfile(); });
 
   m.def("_is_dygraph_debug_enabled",
