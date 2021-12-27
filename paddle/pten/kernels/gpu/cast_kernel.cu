16ef1c70cbb4048a1a30eab692a4fe3504f3faf4
chentianyu03 <chentianyu03@baidu.com>
2021-12-27 13:04:15 +0000

    rm unused code in cast_kernel.cu

--- paddle/pten/kernels/gpu/cast_kernel.cu
+++ paddle/pten/kernels/gpu/cast_kernel.cu
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#pragma once
-
 #include "paddle/pten/kernels/cast_kernel.h"
 
 #include "paddle/pten/api/ext/dispatch.h"
