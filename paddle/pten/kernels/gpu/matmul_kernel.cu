982bf444af6b9d3ef58512b8f15d5f845db028fc
zyfncg <zhangyunfei07@baidu.com>
2021-12-28 15:22:44 +0800

    refactor matmul directory in pten (#38227)
    * refactor matmul directory in pten
    
    * fix merge conflict

--- paddle/pten/kernels/gpu/matmul_kernel.cu
+++ paddle/pten/kernels/gpu/matmul_kernel.cu
@@ -12,16 +12,20 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#pragma once
+#include "paddle/pten/kernels/matmul_kernel.h"
 
+#include "paddle/pten/backends/gpu/gpu_context.h"
 #include "paddle/pten/core/kernel_registry.h"
 
-// TODO(chenweihang) After the kernel is split into a single file,
-// the kernel declare statement is automatically generated according to the
-// file name of the kernel, and this header file will be removed
+#include "paddle/fluid/platform/complex.h"
+#include "paddle/pten/kernels/impl/matmul_kernel_impl.h"
 
-PT_DECLARE_KERNEL(mean, CPU, ALL_LAYOUT);
-
-#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
-PT_DECLARE_KERNEL(mean, GPU, ALL_LAYOUT);
-#endif
+PT_REGISTER_CTX_KERNEL(matmul,
+                       GPU,
+                       ALL_LAYOUT,
+                       pten::MatmulKernel,
+                       float,
+                       double,
+                       paddle::platform::float16,
+                       paddle::platform::complex<float>,
+                       paddle::platform::complex<double>) {}
