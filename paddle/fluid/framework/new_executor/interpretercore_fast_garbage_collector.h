5f8bef4c93e7b675c3c18796087d9139a24eb718
From00 <chenruibiao@baidu.com>
2021-12-28 19:44:32 +0800

    Utilize StreamSafeCUDAAllocator to support fast GC in new executor (#37642)
    * fix reshape move storage error
    
    * remove needless set type
    
    * alloc tensor by shared storage
    
    * Utilize StreamSafeCUDAAllocator to support fast GC in new executor
    
    * Fix compile error for Windows and ROCm
    
    * Fix compile error for Windows
    
    * Modify UT stream_safe_cuda_alloc_test
    
    * Modify UT stream_safe_cuda_alloc_test
    
    * Rewrite fast GC
    
    * Rewrite fast GC
    
    * Fix compile error for BOOST_GET_CONST
    
    * Fix compile error for BOOST_GET_CONST
    
    * Changes default stream for StreamSafeCUDAAllocator
    
    * Fix a small CI error
    
    * Remove some redundant code
    
    * Fix conflict
    
    * Fix compile error for ROCm
    
    * Fix Windoes CI error
    
    * Fix CI error
    
    * Remove some unnecessary code
    
    * Fix CI error
    
    * Add UT for fast GC
    
    * Fix CI error
    
    * add device-agnostic stream class
    
    * add stream.h
    
    * fix ut
    
    * fix cpu compile
    
    * Use RWLock in GetAllocator
    
    * Fix CI error
    
    Co-authored-by: Chen Weihang <chenweihang@baidu.com>
    Co-authored-by: zhiqiu <chenqiuliang@baidu.com>

--- paddle/fluid/framework/new_executor/interpretercore_fast_garbage_collector.h
+++ paddle/fluid/framework/new_executor/interpretercore_fast_garbage_collector.h
@@ -13,31 +13,18 @@
 // limitations under the License.
 #pragma once
 
-#include <queue>
-#include "paddle/fluid/memory/allocation/spin_lock.h"
-#include "paddle/fluid/platform/device_event.h"
+#include "paddle/fluid/framework/new_executor/interpretercore_garbage_collector.h"
 
 namespace paddle {
 namespace framework {
 
-using Garbage = std::shared_ptr<memory::Allocation>;
-using GarbageQueue = std::deque<Garbage>;
-
-class InterpreterCoreGarbageCollector {
+class InterpreterCoreFastGarbageCollector
+    : public InterpreterCoreGarbageCollector {
  public:
-  InterpreterCoreGarbageCollector();
-  virtual ~InterpreterCoreGarbageCollector(){};
-  virtual void Add(Variable* var);
-  virtual void Add(Variable* var, platform::DeviceEvent& event,
-                   const platform::DeviceContext* ctx);
-  DISABLE_COPY_AND_ASSIGN(InterpreterCoreGarbageCollector);
+  virtual void Add(Variable* var) override;
 
- protected:
-  std::unique_ptr<GarbageQueue> garbages_;
-  int64_t max_memory_size_;
-  int64_t cur_memory_size_;
-  memory::SpinLock spinlock_;
+ private:
+  void Add(Garbage garbage);
 };
-
 }  // namespace framework
 }  // namespace paddle
