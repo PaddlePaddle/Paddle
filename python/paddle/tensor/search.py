4a7337e7e823273108c5e1397624c765bc47e826
zoooo0820 <zoooo0820@qq.com>
2021-12-28 03:04:56 +0000

    fix example code error

--- python/paddle/tensor/search.py
+++ python/paddle/tensor/search.py
@@ -871,7 +871,7 @@ def kthvalue(x, k, axis=None, keepdim=False, name=None):
             #        [[ 0.15104349, -0.93965352],
             #         [ 0.14745511,  0.98209465],
             #         [ 0.10732264, -0.55859774]]])           
-            y = paddle.kthvalue(2, 1)    
+            y = paddle.kthvalue(x, 2, 1)    
             # (Tensor(shape=[2, 2], dtype=float32, place=CUDAPlace(0), stop_gradient=True,
             # [[ 0.22954939, -0.17573971],
             #  [ 0.14745511, -0.55859774]]), Tensor(shape=[2, 2], dtype=int64, place=CUDAPlace(0), stop_gradient=True,
