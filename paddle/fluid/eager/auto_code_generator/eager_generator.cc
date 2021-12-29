8972b38837e034f536a9ddd8700101ddd2560cb8
xiongkun <xiongkun03@baidu.com>
2021-12-29 06:56:04 +0000

    const cast

--- paddle/fluid/eager/auto_code_generator/eager_generator.cc
+++ paddle/fluid/eager/auto_code_generator/eager_generator.cc
@@ -2047,7 +2047,8 @@ static void PrepareAttrMapForOps() {
   // Handle "run_program_op"
   static framework::ProgramDesc fake_prog;
   operators_with_attrs["run_program"] = {};
-  operators_with_attrs["run_program"]["global_block"] = &fake_prog.Block(0);
+  operators_with_attrs["run_program"]["global_block"] =
+      const_cast<BlockDesc*>(&fake_prog.Block(0));
   // Handle "fused_elemwise_add_activation"
   std::vector<std::string> functor_list = {"a", "b"};
   operators_with_attrs["fused_elemwise_add_activation"] = {};
