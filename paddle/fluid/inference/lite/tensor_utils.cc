08e1220c26ff310803f843c0244419bf6f2381aa
jim19930609 <jim19930609@gmail.com>
2022-01-04 12:23:21 +0000

    Fixed lite issues

--- paddle/fluid/inference/lite/tensor_utils.cc
+++ paddle/fluid/inference/lite/tensor_utils.cc
@@ -230,7 +230,7 @@ void TensorCopyAsync(framework::LoDTensor* dst,
   const platform::Place& dst_place = dst->place();
   int64_t src_numel = GetLiteTensorNumel(src);
   const size_t bytes = src_numel * framework::SizeOfType(dst->type());
-  const void* src_data = src.data();
+  const void* src_data = src.data<void>();
   // When Lite is ready, the source type needs to be modified here.
   void* dst_data = dst->mutable_data(dst_place, dst->type());
   VLOG(3) << "[CopyAsync lite -> fluid] Bytes = " << bytes << ", src = " << &src
@@ -242,7 +242,7 @@ void TensorCopyAsync(framework::LoDTensor* dst,
 template <>
 void TensorDataShare(paddle::lite_api::Tensor* dst, framework::LoDTensor* src) {
   dst->Resize(framework::vectorize(src->dims()));
-  dst->ShareExternalMemory(src->data(), src->memory_size(),
+  dst->ShareExternalMemory(src->data<void>(), src->memory_size(),
                            GetLiteTargetType(src->place()));
   dst->SetPrecision(GetLitePrecisionType(src->type()));
   paddle::lite::LoD lite_lod;
