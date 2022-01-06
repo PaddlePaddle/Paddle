a2280c5d6b00575b249d45cde2e9f2496db98571
andyjpaddle <jiangkaitao@baidu.com>
2022-01-06 02:15:44 +0000

    update sample code for maxunpool3d

--- python/paddle/nn/functional/pooling.py
+++ python/paddle/nn/functional/pooling.py
@@ -843,11 +843,11 @@ def max_unpool3d(x,
             import paddle
             import paddle.nn.functional as F
 
-            data = paddle.rand(shape=[1, 1, 6, 6, 6])
+            data = paddle.rand(shape=[1, 1, 4, 4, 6])
             pool_out, indices = F.max_pool3d(data, kernel_size=2, stride=2, padding=0, return_mask=True)
-            # pool_out shape: [1, 1, 3, 3, 3],  indices shape: [1, 1, 3, 3, 3]
+            # pool_out shape: [1, 1, 2, 2, 3],  indices shape: [1, 1, 2, 2, 3]
             unpool_out = F.max_unpool3d(pool_out, indices, kernel_size=2, padding=0)
-            # unpool_out shape: [1, 1, 6, 6, 6]
+            # unpool_out shape: [1, 1, 4, 4, 6]
 
     """
     kernel_size = utils.convert_to_list(kernel_size, 3, 'pool_size')
--- python/paddle/nn/layer/pooling.py
+++ python/paddle/nn/layer/pooling.py
@@ -1266,12 +1266,12 @@ class MaxUnPool3D(Layer):
             import paddle.nn.functional as F
             import numpy as np
 
-            data = paddle.rand(shape=[1, 1, 7, 7, 7])
+            data = paddle.rand(shape=[1, 1, 4, 4, 6])
             pool_out, indices = F.max_pool3d(data, kernel_size=2, stride=2, padding=0, return_mask=True)
-            # pool_out shape: [1, 1, 3, 3, 3],  indices shape: [1, 1, 3, 3, 3]
+            # pool_out shape: [1, 1, 2, 2, 3],  indices shape: [1, 1, 2, 2, 3]
             Unpool3D = paddle.nn.MaxUnPool3D(kernel_size=2, padding=0)
             unpool_out = Unpool3D(pool_out, indices)
-            # unpool_out shape: [1, 1, 6, 6, 6]
+            # unpool_out shape: [1, 1, 4, 4, 6]
 
     """
 
