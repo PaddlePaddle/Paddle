2c5159a0a853c184b8b59bfe4e17a3372391a694
WorgenZhang <frank08081993@gmail.com>
2022-01-07 02:34:32 +0000

    [PSLIB] Clean Metric Module Unused Code

--- paddle/fluid/framework/fleet/metrics.h
+++ paddle/fluid/framework/fleet/metrics.h
@@ -112,19 +112,10 @@ class BasicAucCalculator {
   double _bucket_error = 0;
   std::unordered_set<uint64_t> _uid_keys;
 
-  // std::vector<std::shared_ptr<memory::Allocation>> _d_positive;
-  // std::vector<std::shared_ptr<memory::Allocation>> _d_negative;
-  // std::vector<std::shared_ptr<memory::Allocation>> _d_abserr;
-  // std::vector<std::shared_ptr<memory::Allocation>> _d_sqrerr;
-  // std::vector<std::shared_ptr<memory::Allocation>> _d_pred;
-
  private:
   void set_table_size(int table_size) { _table_size = table_size; }
   int _table_size;
   std::vector<double> _table[2];
-  // std::unordered_map<uint64_t,
-  //                    std::map<uint32_t, std::pair<uint64_t, uint64_t>>>
-  //     _uid_prob_cntmap;
   std::vector<WuaucRecord> wuauc_records_;
   static constexpr double kRelativeErrorBound = 0.05;
   static constexpr double kMaxSpan = 0.01;
@@ -652,24 +643,14 @@ class Metric {
                       platform::errors::InvalidArgument(
                           "The metric name you provided is not registered."));
     VLOG(0) << "begin GetWuAucMetricMsg";
-    // paddle::platform::Timer timeline;
-    // double total_time = 0.0;
-    // double compute_time = 0.0;
-    // double allreduce_time = 0.0;
-    // double reset_time = 0.0;
     std::vector<float> metric_return_values_(6, 0.0);
     auto* auc_cal_ = iter->second->GetCalculator();
-    // timeline.Start();
     auc_cal_->computeWuAuc();
     metric_return_values_[0] = auc_cal_->user_cnt();
     metric_return_values_[1] = auc_cal_->size();
     metric_return_values_[2] = auc_cal_->uauc();
     metric_return_values_[3] = auc_cal_->wuauc();
-    // timeline.Pause();
-    // compute_time += timeline.ElapsedSec();
-    // total_time += timeline.ElapsedSec();
 
-    // timeline.Start();
     auto gloo_wrapper = paddle::framework::GlooWrapper::GetInstance();
     auto global_metric_return_values_ =
         gloo_wrapper->AllReduce(metric_return_values_, "sum");
@@ -677,19 +658,7 @@ class Metric {
                                       (global_metric_return_values_[0] + 1e-10);
     global_metric_return_values_[5] = global_metric_return_values_[3] /
                                       (global_metric_return_values_[1] + 1e-10);
-    // timeline.Pause();
-    // allreduce_time += timeline.ElapsedSec();
-    // total_time += timeline.ElapsedSec();
-
-    // timeline.Start();
     auc_cal_->reset_records();
-    // timeline.Pause();
-    // reset_time += timeline.ElapsedSec();
-    // total_time += timeline.ElapsedSec();
-    // VLOG(0) << "wuauc total time: " << total_time << "s\n";
-    // VLOG(0) << "wuauc compute time: " << compute_time << "s\n";
-    // VLOG(0) << "wuauc allreduce time: " << allreduce_time << "s\n";
-    // VLOG(0) << "wuauc reset time: " << reset_time << "s\n";
     return global_metric_return_values_;
   }
 
