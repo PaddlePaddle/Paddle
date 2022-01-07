37a9d19b0e68238c3120ec3a0f3cc2d62ba0038a
ZzSean <18818272991@163.com>
2022-01-07 07:22:18 +0000

    Add pten change file check for op benchmark

--- paddle/pten/kernels/gpu/elementwise.h
+++ paddle/pten/kernels/gpu/elementwise.h
@@ -14,6 +14,7 @@ limitations under the License. */
 
 #pragma once
 
+
 #include "paddle/fluid/operators/kernel_primitives/kernel_primitives.h"
 #include "paddle/fluid/platform/aligned_vector.h"
 #include "paddle/fluid/platform/function_traits.h"
