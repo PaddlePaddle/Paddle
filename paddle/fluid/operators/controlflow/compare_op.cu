ca16ec8dd19cf8ee94b794d9a2e91b4fe6c482ec
JamesLim-sy <santamonic@sina.com>
2022-01-09 09:40:26 +0000

    for ci approval

--- paddle/fluid/operators/controlflow/compare_op.cu
+++ paddle/fluid/operators/controlflow/compare_op.cu
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/operators/controlflow/compare_op.h"
+#include "paddle/fluid/operators/elementwise/elementwise_op_broadcast.cu.h"
 
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
