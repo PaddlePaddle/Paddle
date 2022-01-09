fcf88a495a8b20d6c444e0054f4a01cfae8460df
liutiexing <liutiexing@google.com>
2022-01-09 13:15:23 +0000

    profiler skeleton

--- paddle/fluid/platform/profiler/host_event_recorder.cc
+++ paddle/fluid/platform/profiler/host_event_recorder.cc
@@ -9,7 +9,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/platform/host_event_recorder.h"
+#include "paddle/fluid/platform/profiler/host_event_recorder.h"
 #include "paddle/fluid/platform/os_info.h"
 
 namespace paddle {
