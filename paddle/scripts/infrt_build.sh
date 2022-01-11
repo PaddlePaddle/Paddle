d5f889b94e9eb4d9968d46a1d4d0402f01cd321d
DannyIsFunny <912790387@qq.com>
2022-01-11 07:26:15 +0000

    share the same build dir

--- paddle/scripts/infrt_build.sh
+++ paddle/scripts/infrt_build.sh
@@ -31,7 +31,8 @@ tmp_dir=`mktemp -d`
 function update_pd_ops() {
    PADDLE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")/../../" && pwd )"
    # compile and install paddle
-   cd ${PADDLE_ROOT} && rm -rf paddle_build && mkdir paddle_build && cd paddle_build
+   mkdir -p ${PADDLE_ROOT}/build
+   cd ${PADDLE_ROOT}/build
    cmake .. -DWITH_PYTHON=ON -DWITH_GPU=OFF -DPYTHON_EXECUTABLE=`which python3`
    make -j ${parallel_number}
    cd ${PADDLE_ROOT}/paddle_build
@@ -78,13 +79,13 @@ function infrt_gen_and_build() {
     fi
     startTime_s=`date +%s`
     set +e
+
+    # step2. compile infrt
+    mkdir -p ${PADDLE_ROOT}/build
     # step1. reinstall paddle and generate pd_ops.td
     if [ "${UPDATE_PD_OPS}" != "false" ]; then
         update_pd_ops
     fi
-
-    # step2. compile infrt
-    mkdir -p ${PADDLE_ROOT}/build
     cd ${PADDLE_ROOT}/build
     rm -f infrt_summary.txt
     cmake ..  -DWITH_MKL=OFF -DWITH_GPU=OFF -DWITH_CRYPTO=OFF -DCMAKE_BUILD_TYPE=Release -DWITH_INFRT=ON -DWITH_PYTHON=OFF -DWITH_TESTING==${WITH_TESTING:-ON}; build_error=$?
