b85d65dc29ebea322d2c7a2ee74ada8c9314043a
Allen Guo <alleng@graphcore.ai>
2022-01-11 17:39:02 +0800

    remove ipu_pass_base

--- paddle/fluid/framework/ir/ipu/transfer_cast_op_pass.h
+++ paddle/fluid/framework/ir/ipu/transfer_cast_op_pass.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,13 +14,13 @@
 
 #pragma once
 
-#include "paddle/fluid/framework/ir/ipu/ipu_pass_base.h"
+#include "paddle/fluid/framework/ir/pass.h"
 
 namespace paddle {
 namespace framework {
 namespace ir {
 
-class TransferCastOpPass : public IPUPassBase {
+class TransferCastOpPass : public Pass {
  protected:
   void ApplyImpl(ir::Graph* graph) const override;
 };
