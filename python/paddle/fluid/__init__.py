d4f05966326a0d7eee2ee07704941dcd8638ad6a
jim19930609 <jim19930609@gmail.com>
2022-01-12 00:55:48 +0000

    Patched python level LoDTensor

--- python/paddle/fluid/__init__.py
+++ python/paddle/fluid/__init__.py
@@ -32,6 +32,10 @@ if os.path.exists(legacy_core):
     except Exception as e:
         raise e
 
+# Patch LoDTensor
+from . import core
+core.LoDTensor = core.Tensor
+
 # import all class inside framework into fluid module
 from . import framework
 from .framework import *
@@ -70,10 +74,7 @@ from . import distribute_lookup_table
 from .param_attr import ParamAttr, WeightNormParamAttr
 from .data_feeder import DataFeeder
 
-import core
-core.LoDTensor = core.Tensor
-
-from .core import LoDTensorArray, Scope, _Scope
+from .core import LoDTensor, LoDTensorArray, Scope, _Scope
 from .core import CPUPlace, XPUPlace, CUDAPlace, CUDAPinnedPlace, NPUPlace, IPUPlace, MLUPlace
 from .incubate import fleet
 from .transpiler import DistributeTranspiler, \
