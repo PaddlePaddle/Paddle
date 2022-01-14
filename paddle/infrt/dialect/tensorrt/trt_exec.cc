0de8a805a89eb70203163a34858ff504afff30df
王明冬 <78149749+winter-wang@users.noreply.github.com>
2022-01-14 16:05:00 +0800

    [infrt] update the version of llvm. test=develop (#38843)

--- paddle/infrt/dialect/tensorrt/trt_exec.cc
+++ paddle/infrt/dialect/tensorrt/trt_exec.cc
@@ -11,10 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <llvm/Support/CommandLine.h>
+#include <mlir/Pass/PassManager.h>
 #include <iostream>
 #include <string>
-#include "llvm/Support/CommandLine.h"
-#include "mlir/Pass/PassManager.h"
 #include "paddle/infrt/common/global.h"
 #include "paddle/infrt/dialect/mlir_loader.h"
 #include "paddle/infrt/dialect/tensorrt/trt_graph_fuse_pass.h"
