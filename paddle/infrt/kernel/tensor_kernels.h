4e0e31ad1a35c776b254f04c5937f96ec90e288b
王明冬 <78149749+winter-wang@users.noreply.github.com>
2022-01-14 16:05:00 +0800

    [infrt] update the version of llvm. test=develop (#38843)

--- paddle/infrt/kernel/tensor_kernels.h
+++ paddle/infrt/kernel/tensor_kernels.h
@@ -14,12 +14,16 @@
 
 #pragma once
 
-namespace infrt::host_context {
+namespace infrt {
+namespace host_context {
 struct KernelRegistry;
-}  // namespace infrt::host_context
+}  // namespace host_context
+}  // namespace infrt
 
-namespace infrt::kernel {
+namespace infrt {
+namespace kernel {
 
 void RegisterTensorKernels(host_context::KernelRegistry* registry);
 
-}  // namespace infrt::kernel
+}  // namespace kernel
+}  // namespace infrt
