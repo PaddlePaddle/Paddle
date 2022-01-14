2936903a5240cb1e5d715b8700c000f1027691e6
Artur Lesniak <artur.lesniak@intel.com>
2022-01-14 15:12:39 +0100

    fix for isnan not  was not declared in this scope error

--- paddle/fluid/operators/elementwise/elementwise_min_op.h
+++ paddle/fluid/operators/elementwise/elementwise_min_op.h
@@ -128,7 +128,7 @@ class ElementwiseMinGradKernel : public ElemwiseGradKernel<T> {
 template <typename T>
 struct FMinGradDx {
   HOSTDEVICE T operator()(T x, T y, T out, T dout) const {
-    return dout * static_cast<T>((x <= y) || isnan(y));
+    return dout * static_cast<T>((x <= y) || std::isnan(y));
   }
 };
 
@@ -160,7 +160,7 @@ struct FMinGradDx<int64_t> {
 template <typename T>
 struct FMinGradDy {
   HOSTDEVICE T operator()(T x, T y, T out, T dout) const {
-    return dout * static_cast<T>(!((x <= y) || isnan(y)));
+    return dout * static_cast<T>(!((x <= y) || std::isnan(y)));
   }
 };
 
