8c20d6681b7eecceac591f49b1e700818316ddb0
sneaxiy <32832641+sneaxiy@users.noreply.github.com>
2022-01-18 10:20:17 +0800

    Speedup FP16 Gelu op using fast math and vectorized 8 kernel (#38980)
    * speedup gelu using fast math
    
    * add bwd part

--- paddle/fluid/platform/flags.cc
+++ paddle/fluid/platform/flags.cc
@@ -652,6 +652,9 @@ PADDLE_DEFINE_EXPORTED_bool(
 #if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
 PADDLE_DEFINE_EXPORTED_bool(conv2d_disable_cudnn, false,
                             "Disable cudnn in conv2d");
+
+PADDLE_DEFINE_EXPORTED_bool(use_fast_math, false,
+                            "Whether to use fast math GPU functions.");
 #endif
 
 /**
