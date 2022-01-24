4e99d31179744342f9c1e1ac708e060140266d0d
houj04 <houj04@foxmail.com>
2022-01-23 19:09:40 -0800

    fix unit tests. test=kunlun

--- paddle/fluid/imperative/tests/test_gradient_accmulator.cc
+++ paddle/fluid/imperative/tests/test_gradient_accmulator.cc
@@ -108,6 +108,7 @@ TEST(test_add_functor, add_functor) {
 
 #if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
   int gpu_res = 1;
+  // normal
   gpu_res = TensorddTest(gpu_place, gpu_place, 1.0, 0.0);
   EXPECT_EQ(gpu_res, 0);
   gpu_res = TensorddTest(gpu_place, gpu_place, static_cast<double>(1.0),
@@ -115,6 +116,21 @@ TEST(test_add_functor, add_functor) {
   EXPECT_EQ(gpu_res, 0);
   gpu_res =
       TensorddTest(gpu_place, gpu_place, static_cast<platform::float16>(1.0),
+                   static_cast<platform::float16>(2.0));
+  EXPECT_EQ(gpu_res, 0);
+  // different places
+  gpu_res = TensorddTest(cpu_place, gpu_place, static_cast<float>(1.0),
+                         static_cast<float>(2.0));
+  EXPECT_EQ(gpu_res, 0);
+  gpu_res = TensorddTest(gpu_place, cpu_place, static_cast<float>(1.0),
+                         static_cast<float>(2.0));
+  EXPECT_EQ(gpu_res, 0);
+  gpu_res =
+      TensorddTest(cpu_place, gpu_place, static_cast<platform::float16>(1.0),
+                   static_cast<platform::float16>(2.0));
+  EXPECT_EQ(gpu_res, 0);
+  gpu_res =
+      TensorddTest(gpu_place, cpu_place, static_cast<platform::float16>(1.0),
                    static_cast<platform::float16>(2.0));
   EXPECT_EQ(gpu_res, 0);
 #endif
