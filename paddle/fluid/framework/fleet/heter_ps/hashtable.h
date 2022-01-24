7e09cdf8bd5d3876881459dcf709ec90aedca8db
yaoxuefeng6 <yaoxuefeng@baidu.com>
2022-01-25 00:25:33 +0800

    reconstruct directory of ps

--- paddle/fluid/framework/fleet/heter_ps/hashtable.h
+++ paddle/fluid/framework/fleet/heter_ps/hashtable.h
@@ -21,7 +21,7 @@ limitations under the License. */
 #include "common_value.h"  // NOLINT
 #endif
 #ifdef PADDLE_WITH_PSCORE
-#include "paddle/fluid/distributed/table/depends/large_scale_kv.h"
+#include "paddle/fluid/distributed/ps/table/depends/large_scale_kv.h"
 #endif
 #include "paddle/fluid/framework/rw_lock.h"
 #include "thrust/pair.h"
