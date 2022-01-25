5234b6365e7f140c984052f5587b6a66da417718
limin2021 <1121099234@qq.com>
2022-01-25 06:02:32 +0000

    Optimize layer_norm bwd kernel when cols is 1024.

--- paddle/fluid/operators/layer_norm_kernel.cu.h
+++ paddle/fluid/operators/layer_norm_kernel.cu.h
@@ -383,6 +383,314 @@ __inline__ __device__ void cuLoadAddStridedInputs(
   }
 }
 
+// 2*sms blocks
+// 32 * 4 threads per block.
+#define COLS_ 1024
+template <typename T, typename U, typename ScaleT = U, int VecSize = 8,
+          int WARPS_M = 4, int WARPS_N = 1, int BYTES_PER_LDG = 16,
+          int ELTS_PER_ROW = 1024, int THREADS_PER_WARP = 32,
+          int THREADS_PER_ROW = WARPS_N *THREADS_PER_WARP,
+          int THREADS_PER_CTA = WARPS_M *THREADS_PER_ROW,
+          int ROWS_PER_CTA = WARPS_M,
+          int ELTS_PER_ROW_PER_CTA = THREADS_PER_ROW *VecSize,
+          int LDGS = ELTS_PER_ROW / ELTS_PER_ROW_PER_CTA>
+__global__ __launch_bounds__(THREADS_PER_CTA) void ln_bwd_1024_kernel(
+    const int rows, const T *__restrict__ x_ptr,
+    const ScaleT *__restrict__ gamma_ptr, const U *__restrict__ mu_ptr,
+    const U *__restrict__ rs_ptr, const T *__restrict__ dout_ptr,
+    U *__restrict__ dgamma_temp_ptr, U *__restrict__ dbeta_temp_ptr,
+    T *__restrict__ dx_ptr) {
+  using Vec = platform::AlignedVector<T, VecSize>;
+  using Vec_scale = platform::AlignedVector<ScaleT, VecSize>;
+
+  const int tidx = threadIdx.x;
+  const int bidx = blockIdx.x;
+  const int lane = tidx % THREADS_PER_WARP;            // 0, 1, ..., 31
+  const int warp = tidx / THREADS_PER_WARP;            // 0, 1, 2, 3
+  const int warp_m = warp / WARPS_N;                   // 0, 1, 2, 3
+  const int warp_n = warp % WARPS_N;                   // 0
+  const int tid_r = warp_n * THREADS_PER_WARP + lane;  // 0, 1, ..., 31
+
+  const int r = bidx * ROWS_PER_CTA + warp_m;
+  const int c = warp_n * THREADS_PER_WARP + lane;
+
+  static_assert(COLS_ == THREADS_PER_ROW * LDGS * VecSize, "");
+
+  // smem for final reduction
+  // __shared__ U smem_[ROWS_PER_CTA * COLS];
+  extern __shared__ U smem_[];
+  // static_assert(sizeof(smem_dw_sum) == 32*1024,"");
+  // Using the grid stride loop we can assign multiple rows to each thread
+  // by using a number of CTAs smaller than rows / ROWS_PER_CTA
+  // We accumulate them here, one in smem, one in registers, because the smem
+  // capacity is limited U * dw_sum = &smem_dw_sum[warp_m * COLS + tid_r
+  // * LDGS * VecSize];
+  U dwy_sum[LDGS * VecSize];
+  U dw_sum[LDGS * VecSize];
+
+  memset(dwy_sum, 0, sizeof(U) * LDGS * VecSize);
+  memset(dw_sum, 0, sizeof(U) * LDGS * VecSize);
+  // Debug 8 rows, 4B, 1024 cols
+
+  __shared__ U smem_mdy[ROWS_PER_CTA * WARPS_N];   // 4
+  __shared__ U smem_mdyy[ROWS_PER_CTA * WARPS_N];  // 4
+  U *mdy_shared = &smem_mdy[warp_m * WARPS_N];     //
+  U *mdyy_shared = &smem_mdyy[warp_m * WARPS_N];   //
+
+  constexpr float rn = 1.f / static_cast<float>(COLS_);
+  Vec_scale gamma[LDGS];
+  int col = c;
+#pragma unroll
+  for (int it = 0; it < LDGS; it++) {
+    // gamma[it].load_from(g_ptr + col * BYTES_PER_LDG);
+    platform::Load<ScaleT, VecSize>(gamma_ptr + col * VecSize, &gamma[it]);
+    col += THREADS_PER_ROW;
+  }
+// if ROWS_PER_CTA does not divice rows, we might get divergence in the
+// last blocks with syncthreads!
+// grid stride over rows
+#pragma unroll 1
+  for (int row = r; row < rows; row += gridDim.x * ROWS_PER_CTA) {
+    const U mu_r = mu_ptr[row];
+    const U rs_r = rs_ptr[row];
+    Vec dout[LDGS], x[LDGS], dx[LDGS];
+    int col = c;
+#pragma unroll
+    for (int it = 0; it < LDGS; it++) {
+      // dw[it].load_from(dw_ptr + row * BYTES_PER_ROW + col * BYTES_PER_LDG);
+      // x[it].load_from(x_ptr + row * BYTES_PER_ROW + col * BYTES_PER_LDG);
+      platform::Load<T, VecSize>(dout_ptr + row * COLS_ + col * VecSize,
+                                 &dout[it]);
+      platform::Load<T, VecSize>(x_ptr + row * COLS_ + col * VecSize, &x[it]);
+      col += THREADS_PER_ROW;
+    }
+    // local reductions
+    U dy[LDGS * VecSize];
+    U y[LDGS * VecSize];
+
+    U mdy_local = 0.f;
+    U mdyy_local = 0.f;
+#pragma unroll
+    for (int it = 0; it < LDGS; it++) {
+#pragma unroll
+      for (int jt = 0; jt < VecSize; jt++) {
+        // U x_tmp = x[it].data.elt[jt];
+        U x_tmp = x[it][jt];
+        U y_tmp = rs_r * (x_tmp - mu_r);
+        // U dy_tmp = gamma[it].data.elt[jt] * dw[it].data.elt[jt]; // scale *
+        // dy
+        // U dw_tmp = dw[it].data.elt[jt]; // dy
+        U dy_tmp = gamma[it][jt] * dout[it][jt];  // scale * dy
+        U dw_tmp = dout[it][jt];                  // dy
+
+        // 求dx的，后面进行行规约
+        mdy_local += dy_tmp;           // scale * dy, sum_1
+        mdyy_local += dy_tmp * y_tmp;  // scale * dy * y, sum_2
+
+        dy[it * VecSize + jt] = dy_tmp;  // scale* dy
+        y[it * VecSize + jt] = y_tmp;    // y
+
+        // 求dscale和dbias的，列规约.
+        dwy_sum[it * VecSize + jt] += dw_tmp * y_tmp;  // dy * y
+        dw_sum[it * VecSize + jt] += dw_tmp;           // dy
+      }
+    }
+
+    // reduction across row for mdy, mdyy
+    if (WARPS_N == 1) {  // no need to go through smem!
+#pragma unroll
+      // 32个线程的结果进行行规约，
+      for (int it = 1; it < THREADS_PER_WARP; it *= 2) {
+        mdy_local += __shfl_xor_sync(uint32_t(-1), mdy_local, it);
+        mdyy_local += __shfl_xor_sync(uint32_t(-1), mdyy_local, it);
+      }
+
+      mdy_local *= rn;
+      mdyy_local *= rn;
+
+    } else {
+#pragma unroll
+      for (int it = 16; it > 0; it /= 2) {
+        mdy_local += __shfl_down_sync(uint32_t(-1), mdy_local, it);
+        mdyy_local += __shfl_down_sync(uint32_t(-1), mdyy_local, it);
+      }  // lane 0 holds the result!
+
+      if (lane == 0) {
+        mdy_shared[warp_n] = mdy_local;
+        mdyy_shared[warp_n] = mdyy_local;
+      }
+
+      __syncthreads();
+      if (warp_n == 0 && lane == 0) {
+        mdy_local = 0.f;
+        mdyy_local = 0.f;
+        for (int it = 0; it < WARPS_N; it++) {
+          mdy_local += mdy_shared[it];
+          mdyy_local += mdyy_shared[it];
+        }
+        mdy_shared[0] = mdy_local;
+        mdyy_shared[0] = mdyy_local;
+      }
+      __syncthreads();
+
+      mdy_local = mdy_shared[0] * rn;
+      mdyy_local = mdyy_shared[0] * rn;
+    }
+
+//
+#pragma unroll
+    for (int it = 0; it < LDGS; it++) {
+#pragma unroll
+      for (int jt = 0; jt < VecSize; jt++) {
+        U dy_tmp = dy[it * VecSize + jt];  // scale* dy
+        U y_tmp = y[it * VecSize + jt];    // y
+        // var * (scale*dy - sum_2 * y - sum_1)
+        U dx_tmp = U(rs_r) * (dy_tmp - mdyy_local * y_tmp - mdy_local);
+        // dx[it].data.elt[jt] = dx_tmp;
+        // todo: static_cast<T>
+        dx[it][jt] = dx_tmp;
+      }
+    }
+
+    col = c;  // 0, ... 31
+              // 将dx的计算结果存回.
+#pragma unroll
+    for (int it = 0; it < LDGS; it++) {
+      // dx[it].store_to(dx_ptr + row * BYTES_PER_ROW + col * BYTES_PER_LDG);
+      platform::Store<T, VecSize>(dx[it], dx_ptr + row * COLS_ + col * VecSize);
+      col += THREADS_PER_ROW;
+    }
+  }  // end: grid stride loop
+
+  // Finalize reduction of part dgamma and dbeta for this CTA
+  // by reducing over the rows held across the WARPS_M warps
+
+  enum { NUM_RES = COLS_ / THREADS_PER_CTA };  // 1024/128 = 8
+  static_assert(NUM_RES * THREADS_PER_CTA == COLS_, "");
+
+  U *smem_write;
+
+  // tid_r: 0, ..., 31(lane id)
+  smem_write = &smem_[warp_m * COLS_ + tid_r * VecSize];  // 4 * 1024大小
+#pragma unroll
+  for (int it = 0; it < LDGS; it++) {
+#pragma unroll
+    for (int jt = 0; jt < VecSize; jt++) {
+      smem_write[jt] = dw_sum[it * VecSize + jt];
+    }
+    smem_write += THREADS_PER_ROW * VecSize;  // 32*8
+  }
+  __syncthreads();
+  U cta_dw_sum[NUM_RES];
+  memset(cta_dw_sum, 0, sizeof(U) * NUM_RES);
+  // 对smem中的元素按照列进行规约: 4*1024 -> 1*1024.
+  for (int it = 0; it < ROWS_PER_CTA; it++) {
+    for (int jt = 0; jt < NUM_RES; jt++) {
+      cta_dw_sum[jt] += smem_[it * COLS_ + tidx + jt * THREADS_PER_CTA];
+    }
+  }
+  __syncthreads();
+
+  smem_write = &smem_[warp_m * COLS_ + tid_r * VecSize];
+#pragma unroll
+  for (int it = 0; it < LDGS; it++) {
+#pragma unroll
+    for (int jt = 0; jt < VecSize; jt++) {
+      smem_write[jt] = dwy_sum[it * VecSize + jt];
+    }
+    smem_write += THREADS_PER_ROW * VecSize;
+  }
+  __syncthreads();
+  U cta_dwy_sum[NUM_RES];
+  memset(cta_dwy_sum, 0, sizeof(U) * NUM_RES);
+  for (int it = 0; it < ROWS_PER_CTA; it++) {
+    for (int jt = 0; jt < NUM_RES; jt++) {
+      cta_dwy_sum[jt] += smem_[it * COLS_ + tidx + jt * THREADS_PER_CTA];
+    }
+  }
+
+  // 结果的shape：(#blocks, 1024)
+  U *dgamma_part = static_cast<U *>(dgamma_temp_ptr) + bidx * COLS_ + tidx;
+  for (int jt = 0; jt < NUM_RES; jt++) {
+    *dgamma_part = cta_dwy_sum[jt];  //
+    dgamma_part += THREADS_PER_CTA;  // 128
+  }
+
+  U *dbeta_part = static_cast<U *>(dbeta_temp_ptr) + bidx * COLS_ + tidx;
+  for (int jt = 0; jt < NUM_RES; jt++) {
+    *dbeta_part = cta_dw_sum[jt];
+    dbeta_part += THREADS_PER_CTA;
+  }
+}
+
+// todo: driver routine
+template <typename T, typename U, typename ScaleT = U, int VecSize = 8>
+void ln_bwd_1024_kernel_driver(const platform::CUDADeviceContext &dev_ctx,
+                               cudaStream_t stream, const int rows,
+                               const int cols, const T *x_ptr,
+                               const ScaleT *gamma_ptr, const U *mean_ptr,
+                               const U *var_ptr, const T *dout_ptr, T *dx_ptr,
+                               ScaleT *dgamma_ptr, ScaleT *dbeta_ptr) {
+  if (cols == 1024) {
+    // step-1: compute dx and reduced intermediate results of dscale and dbias.
+    const int WARPS_M = 4;
+    const int WARPS_N = 1;
+    const int BYTES_PER_LDG = 16;
+    const int THREADS_PER_WARP = 32;
+    const int THREADS_PER_ROW = WARPS_N * THREADS_PER_WARP;
+    const int THREADS_PER_CTA = WARPS_M * THREADS_PER_ROW;
+    const int ROWS_PER_CTA = WARPS_M;
+    // int ELTS_PER_ROW = 1024;
+    // int ELTS_PER_ROW_PER_CTA=THREADS_PER_ROW*VecSize,
+    // int LDGS = ELTS_PER_ROW / ELTS_PER_ROW_PER_CTA;
+
+    // 4 * 1024 * 4
+    const int SMEM_BYTES = ROWS_PER_CTA * cols * sizeof(U);
+#if 0
+    if (SMEM_BYTES >= 48 * 1024) {
+      AT_CUDA_CHECK(cudaFuncSetAttribute(
+          ln_bwd_kernel<Ktraits>, cudaFuncAttributeMaxDynamicSharedMemorySize,
+          Ktraits::SMEM_BYTES));
+    }
+#endif
+
+    // blocks数目：2*SM数目
+    int gridx = 216;
+
+    // get temp space for dgamma and dbeta.
+    framework::Tensor dgamma_temp;
+    dgamma_temp.Resize({gridx, cols});
+    dgamma_temp.mutable_data<U>(dev_ctx.GetPlace());
+    U *dgamma_temp_ptr = dgamma_temp.data<U>();
+
+    framework::Tensor dbeta_temp;
+    dbeta_temp.Resize({gridx, cols});
+    dbeta_temp.mutable_data<U>(dev_ctx.GetPlace());
+    U *dbeta_temp_ptr = dbeta_temp.data<U>();
+
+    ln_bwd_1024_kernel<
+        T, U, ScaleT, VecSize, WARPS_M, WARPS_N,
+        BYTES_PER_LDG><<<gridx, THREADS_PER_CTA, SMEM_BYTES, stream>>>(
+        rows, x_ptr, gamma_ptr, mean_ptr, var_ptr, dout_ptr, dgamma_temp_ptr,
+        dbeta_temp_ptr, dx_ptr);
+
+    // using Ktraits2 = Kernel_traits<float, 1024, 16, 1, 4>;
+    // // 向量化长度为1.
+    // constexpr int grid2 =
+    //     DIVUP(1024, Ktraits2::THREADS_PER_ROW * Ktraits2::Vec::VecSize); //
+    //     1024/(32*1)= 32
+    // // 32个block，块内512个线程.
+    // ln_bwd_finalize_kernel<Ktraits2, scalar_t>
+    //     <<<grid2, Ktraits2::THREADS_PER_CTA, 0, stream>>>(
+    //         dgamma_ptr, dbeta_ptr, dgamma_temp_ptr,
+    //         dbeta_temp_ptr, gridx);
+  } else {
+    PADDLE_THROW(platform::errors::InvalidArgument(
+        "Fast layer_norm kernel is only used when feature_size is 1024"));
+  }
+  // AT_CUDA_CHECK(cudaPeekAtLastError());
+}
+
 template <typename T, typename U, int BDIMX, int BDIMY, int VPTX>
 __global__ void LayerNormBackwardPartGradGammaBeta(
     const T *__restrict__ dout, const T *__restrict__ input, const int64_t n1,
