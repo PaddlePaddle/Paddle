99e10f5552dd7acb39466bd6a3e601f08f62c40f
Leo Chen <chenqiuliang@baidu.com>
2022-01-26 21:07:03 +0800

    [pten]  remove deprecated fluid op kernel for pten  (#38842)
    * update cmake file to remove fluid kernel
    
    * add pten declaration.h to where pybind.h used
    
    * fix sync_bn and tensorrt_engine
    
    * refine detection_library
    
    * fix interpreter_core
    
    * support eager legacy
    
    * fit eager legacy for pten
    
    * fall back to cpu if not found kernel
    
    * fix compile problem
    
    * fix compile problem
    
    * refine fallback logic
    
    * fit operator.run()
    
    * fix xpu compile
    
    * fit for new_exec
    
    * add REGISTER_OP_WITHOUT_GRADIENT
    
    * un-cache pt_kernel_context
    
    * fix compile
    
    * fix cudnn
    
    * fix compiling with on_infer
    
    * fix mkldnn
    
    * fix isfinite_v2
    
    * fix xpu problem
    
    * fix op_device
    
    * refine fallback for xpu
    
    * fix xpu compile
    
    * merge develop
    
    * refine code format
    
    * fix compile
    
    * fix compile
    
    * add data_transfer
    
    * fix PreparePtenData
    
    * fix cpu context
    
    * merge develop
    
    * fix compile
    
    * fix error device context
    
    * fix xpu
    
    * fix dev_ctx

--- paddle/fluid/operators/cast_op.cu
+++ paddle/fluid/operators/cast_op.cu
@@ -30,10 +30,8 @@ using CUDA = paddle::platform::CUDADeviceContext;
       ops::CastOpKernel<CUDA, plat::complex<double>>, ##__VA_ARGS__);
 
 #if !defined(PADDLE_WITH_HIP)
-REGISTER_CAST_CUDA_BASE(cast, ops::CastOpKernel<CUDA, plat::bfloat16>)
 // See [ why register transfer_dtype_op alias with cast_op? ] in cast_op.cc
 REGISTER_CAST_CUDA_BASE(transfer_dtype, ops::CastOpKernel<CUDA, plat::bfloat16>)
 #else
-REGISTER_CAST_CUDA_BASE(cast)
 REGISTER_CAST_CUDA_BASE(transfer_dtype)
 #endif
