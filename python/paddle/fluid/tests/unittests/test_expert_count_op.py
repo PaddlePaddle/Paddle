ac9e1e496777d07e99d972b2c9cb8fe71b97024e
sljlp <lrslj@live.com>
2022-01-26 11:08:09 +0800

    update UT only for cuda

--- python/paddle/fluid/tests/unittests/test_expert_count_op.py
+++ python/paddle/fluid/tests/unittests/test_expert_count_op.py
@@ -43,6 +43,8 @@ class TestExpertCountOpInt64(op_test.OpTest):
         self.attrs = {"n_expert": expert_num}
 
     def test_forward(self):
+        if not paddle.is_compiled_with_cuda():
+            return
         self.check_output_with_place(paddle.CUDAPlace(0))
 
 
@@ -55,6 +57,8 @@ class TestExpertCountAPI(unittest.TestCase):
         self.place = paddle.CUDAPlace(0)
 
     def test_api_static(self):
+        if not paddle.is_compiled_with_cuda():
+            return
         paddle.enable_static()
         with paddle.static.program_guard(paddle.static.Program()):
             x = paddle.fluid.data('x', self.x.shape, dtype="int64")
@@ -64,6 +68,8 @@ class TestExpertCountAPI(unittest.TestCase):
             assert np.allclose(res, self.out)
 
     def test_api_dygraph(self):
+        if not paddle.is_compiled_with_cuda():
+            return
         paddle.disable_static()
         x = paddle.to_tensor(self.x)
         out = paddle.distributed.utils.expert_count(x, self.n_expert)
