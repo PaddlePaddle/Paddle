5446765ecc595f2d29a72b9da22128301c9ae725
phlrain <phliuhongyu@126.com>
2022-01-27 00:50:38 +0000

    remove useless code; test=develop

--- paddle/pten/kernels/impl/digamma_kernel_impl.h
+++ paddle/pten/kernels/impl/digamma_kernel_impl.h
@@ -38,7 +38,6 @@ struct DigammaFunctor {
 template <typename T, typename Context>
 void DigammaKernel(const Context& ctx, const DenseTensor& x, DenseTensor* out) {
   out->mutable_data<T>(ctx.GetPlace());
-
   auto* x_data = x.data<T>();
   auto* out_data = out->data<T>();
   auto numel = x.numel();
