b52b9e3be618fc65cc3c9765b6cf20dd6547c3a3
weishengyu <weishengyu@baidu.com>
2022-01-28 15:54:58 +0800

    empty commit to rerun ci

--- python/paddle/nn/functional/pooling.py
+++ python/paddle/nn/functional/pooling.py
@@ -1144,6 +1144,7 @@ def max_pool3d(x,
                                           return_mask=True)
             # output.shape [None, 3, 16, 16, 16], max_indices.shape [None, 3, 16, 16, 16],
     """
+    
     kernel_size = utils.convert_to_list(kernel_size, 3, 'pool_size')
     if stride is None:
         stride = kernel_size
