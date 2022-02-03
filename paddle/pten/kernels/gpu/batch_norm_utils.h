3f6c67f0891c7e0e534f4ea14468a4732a8e275a
phlrain <phliuhongyu@126.com>
2022-02-03 15:51:09 +0000

    move batch norm to pten; test=develop

--- paddle/pten/kernels/gpu/batch_norm_utils.h
+++ paddle/pten/kernels/gpu/batch_norm_utils.h
@@ -1,8 +1,21 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+#pragma once
 
-
-#include "paddle/fluid/operators/math/math_function.h"
 #include "paddle/fluid/framework/op_registry.h"
+#include "paddle/fluid/operators/math/math_function.h"
 
 namespace pten {
 
@@ -106,10 +119,10 @@ inline void TransToChannelFirst(const DeviceContext& context,
   }
 }
 
-
 template <typename DeviceContext, typename T>
 inline void TransToChannelLast(const DeviceContext& context,
-                               const Tensor* input, Tensor* transformed_input) {
+                               const Tensor* input,
+                               Tensor* transformed_input) {
   int dim = input->dims().size() - 2;
   if (dim == 3) {
     std::vector<int> axis{0, 2, 3, 4, 1};
@@ -127,4 +140,4 @@ inline void TransToChannelLast(const DeviceContext& context,
   }
 }
 
-} //namespace pten
+}  // namespace pten
