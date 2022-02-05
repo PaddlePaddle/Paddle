c4755323b2719692b938e5cb5b6900e2cdd116d3
jakpiase <jakpia21@gmail.com>
2022-02-05 16:36:33 +0100

    added attributes as extra

--- paddle/fluid/operators/shape_op.cc
+++ paddle/fluid/operators/shape_op.cc
@@ -75,12 +75,14 @@ Return the shape of the input.
 )DOC");
     AddAttr<bool>("use_mkldnn",
                   "(bool, default false) Only used in mkldnn kernel")
-        .SetDefault(false);
+        .SetDefault(false)
+        .AsExtra();
     AddAttr<std::string>(
         "mkldnn_data_type",
         "(string, default \"float32\"). Data type of mkldnn kernel")
         .SetDefault("float32")
-        .InEnum({"float32", "bfloat16", "int8"});
+        .InEnum({"float32", "bfloat16", "int8"})
+        .AsExtra();
   }
 };
 
