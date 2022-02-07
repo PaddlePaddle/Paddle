a40f220b7d62b2e3e1dd6b76b78b8306ffe5d8b9
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2022-02-07 07:12:08 +0000

    update

--- paddle/pten/backends/cpu/cpu_context.h
+++ paddle/pten/backends/cpu/cpu_context.h
@@ -1,8 +1,11 @@
 /* Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,7 +31,7 @@ class CPUContext : public DeviceContext {
   explicit CPUContext(const Place&);
   virtual ~CPUContext();
   Eigen::DefaultDevice* eigen_device() const;
-  Place GetPlace() const override;
+  const Place& GetPlace() const override;
 
  public:
   // NOTE: DeviceContext hold resources. Used in training scenarios.
