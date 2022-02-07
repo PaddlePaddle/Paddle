38da4e9fbbb2e1fa3cec5bb910a961f50ce7e776
Wangzheee <634486483@qq.com>
2022-02-07 10:13:14 +0000

    rebuild matmul pass: trt and gpu_cpu

--- paddle/fluid/framework/ir/trt_map_matmul_to_mul_pass.cc
+++ paddle/fluid/framework/ir/trt_map_matmul_to_mul_pass.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/framework/ir/trt_trt_map_matmul_to_mul_pass.h"
+#include "paddle/fluid/framework/ir/trt_map_matmul_to_mul_pass.h"
 
 #include <cmath>
 #include <string>
