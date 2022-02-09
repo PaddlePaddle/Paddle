818b6be0dc7c3d4ec2c18bd6f893ef9edff6cb9d
Jacek Czaja <jacek.czaja@intel.com>
2022-02-09 10:01:36 +0100

    - update of oneDNN to 2.5

--- cmake/external/mkldnn.cmake
+++ cmake/external/mkldnn.cmake
@@ -19,7 +19,7 @@ SET(MKLDNN_PREFIX_DIR     ${THIRD_PARTY_PATH}/mkldnn)
 SET(MKLDNN_INSTALL_DIR    ${THIRD_PARTY_PATH}/install/mkldnn)
 SET(MKLDNN_INC_DIR        "${MKLDNN_INSTALL_DIR}/include" CACHE PATH "mkldnn include directory." FORCE)
 SET(MKLDNN_REPOSITORY     ${GIT_URL}/oneapi-src/oneDNN.git)
-SET(MKLDNN_TAG            145c4b50196ac90ec1b946fb80cb5cef6e7d2d35)
+SET(MKLDNN_TAG            543b8f86f4659d36dddcff1d2bddb810d3740889)
 
 
 # Introduce variables:
