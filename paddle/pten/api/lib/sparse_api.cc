52b72656c71493a0b3cfd4ff30b38e7b2066f290
Chen Weihang <chenweihang@baidu.com>
2022-02-09 03:31:47 +0000

    resolve conflict

--- paddle/pten/api/lib/sparse_api.cc
+++ paddle/pten/api/lib/sparse_api.cc
@@ -144,15 +144,15 @@ PADDLE_API Tensor to_sparse_csr(const Tensor& x, Backend backend) {
   // create empty SparseCooTensor
   pten::DenseTensor non_zero_crows(
       pten::make_intrusive<paddle::experimental::SharedStorage>(
-          pten::TransToFluidPlace(backend)),
+          pten::TransToPtenPlace(backend)),
       std::move(crows_meta));
   pten::DenseTensor non_zero_cols(
       pten::make_intrusive<paddle::experimental::SharedStorage>(
-          pten::TransToFluidPlace(backend)),
+          pten::TransToPtenPlace(backend)),
       std::move(cols_meta));
   pten::DenseTensor non_zero_elements(
       pten::make_intrusive<paddle::experimental::SharedStorage>(
-          pten::TransToFluidPlace(backend)),
+          pten::TransToPtenPlace(backend)),
       std::move(elements_meta));
   auto csr = std::make_shared<pten::SparseCsrTensor>(
       non_zero_crows, non_zero_cols, non_zero_elements, x.dims());
