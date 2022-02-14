f7b88cf0ea9696389bcb88bd2a56f91b2bc45aec
linjieccc <623543001@qq.com>
2022-02-14 08:56:39 +0000

    fix tensorcopy

--- paddle/pten/kernels/impl/expand_grad_kernel_impl.h
+++ paddle/pten/kernels/impl/expand_grad_kernel_impl.h
@@ -88,6 +88,8 @@ void ExpandGradKernel(const Context& ctx,
   }
   // no need reduce, just copy
   if (just_copy) {
+    ctx.template Alloc<T>(in_grad);
+    in_grad->data<T>();
     pten::Copy(ctx, x, false, in_grad);
   } else {
     PADDLE_ENFORCE_GE(dims,
