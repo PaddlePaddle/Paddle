572849068bfa5c906df5f38995fe48baa346be37
chenfeiyu <chenfeiyu@baidu.com>
2022-02-15 06:06:13 +0000

    update API names for new files

--- paddle/pten/kernels/funcs/complex_functors.h
+++ paddle/pten/kernels/funcs/complex_functors.h
@@ -13,7 +13,10 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
-
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+#include <cmath>
 #include <type_traits>
 
 #include "paddle/pten/common/complex.h"
