553e3ec82e18adeaa21f825e71e03b597e5904db
ZzSean <18818272991@163.com>
2022-02-15 02:39:25 +0000

    Modify the implementation of BlockYReduce to fit more scenes

--- paddle/pten/kernels/primitive/compute_primitives.h
+++ paddle/pten/kernels/primitive/compute_primitives.h
@@ -110,7 +110,11 @@ __device__ __forceinline__ T BlockXReduce(T val, ReduceOp reducer) {
     T temp = paddle::platform::CudaShuffleDownSync(mask, val, stride);
     val = reducer(val, temp);
   }
-  return val;
+  if (threadIdx.x == 0) {
+    shared[0] = val;
+  }
+  __syncthreads();
+  return shared[0];
 }
 
 /**
