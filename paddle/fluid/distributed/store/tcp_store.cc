9b7feefb2993bbed26d63bb6f9d630d3804f3971
sandyhouse <lilong12@baidu.com>
2022-02-15 12:46:10 +0000

    update

--- paddle/fluid/distributed/store/tcp_store.cc
+++ paddle/fluid/distributed/store/tcp_store.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -26,6 +26,8 @@ namespace distributed {
 
 namespace detail {
 
+constexpr int INFTIME = -1;
+
 std::unique_ptr<MasterDaemon> MasterDaemon::start(Socket sock) {
   auto master = std::make_unique<MasterDaemon>(sock);
   return master;
@@ -46,7 +48,6 @@ void MasterDaemon::doSet(int sock) {}
 void MasterDaemon::doRemove(int sock) {}
 
 void MasterDaemon::doAdd(int sock) {
-  VLOG(0) << "doAdd:here1";
   std::string key = tcputils::recvString(sock);
   int64_t incr = tcputils::recvValue<int64_t>(sock);
   int64_t new_value = incr;
@@ -58,13 +59,11 @@ void MasterDaemon::doAdd(int sock) {
     size_t len = old_value.size();
     new_value += std::stoll(std::string(buffer, len));
   }
-  VLOG(0) << "doAdd:here2";
 
   std::string new_value_str = std::to_string(new_value);
   _store[key] =
       std::vector<uint8_t>(new_value_str.begin(), new_value_str.end());
   tcputils::sendValue<int64_t>(sock, new_value);
-  VLOG(0) << "doAdd:here3";
 }
 
 void MasterDaemon::doWait(int sock) {}
@@ -72,26 +71,18 @@ void MasterDaemon::doWait(int sock) {}
 void MasterDaemon::run() {
   std::vector<struct pollfd> fds;
   auto sock_fd = _socket.get_socket_fd();
-  struct pollfd listen_fd;
-  listen_fd.fd = sock_fd;
-  listen_fd.events = POLLIN | POLLRDNORM;
-  // fds.push_back({.fd = sock_fd, .events = POLLIN, .revents = 0});
-  fds.push_back(listen_fd);
+  fds.push_back({.fd = sock_fd, .events = POLLIN, .revents = 0});
 
   bool done = false;
   while (!done) {
-    // for(size_t i = 0; i < fds.size(); i++) {
-    //  fds[i].revents = 0;
-    //}
-    VLOG(0) << "run:here1_0_3";
-    VLOG(0) << "run:fds[0]: " << fds[0].fd;
-    VLOG(0) << "run:fds[0].events: " << fds[0].events;
-    ::poll(fds.data(), fds.size(), -1);
-    VLOG(0) << "run:here1_0_4";
+    for (size_t i = 0; i < fds.size(); i++) {
+      fds[i].revents = 0;
+    }
+
+    ::poll(fds.data(), fds.size(), INFTIME);
 
     if (fds[0].revents != 0) {
       Socket socket = _socket.accept();
-      VLOG(0) << "run:here1_0_5";
       _sockets.emplace_back(socket);
       auto new_sock_fd = socket.get_socket_fd();
       fds.push_back({.fd = new_sock_fd, .events = POLLIN, .revents = 0});
@@ -99,14 +90,11 @@ void MasterDaemon::run() {
 
     for (size_t i = 1; i < fds.size(); i++) {
       if (fds[i].revents == 0) {
-        VLOG(0) << "run:here1_1";
         continue;
       }
 
       Command command;
-      VLOG(0) << "run:here1_2";
       tcputils::recvBytes<Command>(fds[i].fd, &command, 1);
-      VLOG(0) << "run:here2";
 
       switch (command) {
         case Command::GET:
@@ -116,7 +104,6 @@ void MasterDaemon::run() {
         case Command::REMOVE:
           doRemove(fds[i].fd);
         case Command::ADD:
-          VLOG(0) << "run:here3";
           doAdd(fds[i].fd);
         case Command::WAIT:
           doWait(fds[i].fd);
@@ -145,7 +132,6 @@ std::unique_ptr<TCPClient> TCPClient::connect(
 
 void TCPClient::sendCommandForKey(Command type, const std::string& key) {
   int sock_fd = _socket.get_socket_fd();
-  VLOG(0) << "client socket: " << sock_fd;
   tcputils::sendValue<Command>(sock_fd, type);
   if (key.empty()) {
     return;
@@ -213,18 +199,13 @@ void TCPStore::waitWorkers() {
     return;
   }
 
-  VLOG(0) << "here0";
   add(_init_key, 1);
-  VLOG(0) << "here0_1";
   auto begin = std::chrono::steady_clock::now();
   if (_server) {
     do {
-      VLOG(0) << "here1";
       auto value = get(_init_key);
       int completed = std::stoi(std::string(value.begin(), value.end()));
-      VLOG(0) << "completed: " << completed;
       if (completed >= _num_workers) {
-        VLOG(0) << "here2";
         break;
       }
       const auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(
@@ -251,13 +232,9 @@ std::vector<uint8_t> TCPStore::get(const std::string& key) {
 }
 
 int64_t TCPStore::add(const std::string& key, int64_t value) {
-  VLOG(0) << "add::here1";
   const std::lock_guard<std::mutex> lock(_lock);
-  VLOG(0) << "add::here2";
   _client->sendCommandForKey(Command::ADD, _key_prefix + key);
-  VLOG(0) << "add::here3";
   _client->sendValue<std::int64_t>(value);
-  VLOG(0) << "add::here4";
   return _client->recvValue<std::int64_t>();
 }
 
