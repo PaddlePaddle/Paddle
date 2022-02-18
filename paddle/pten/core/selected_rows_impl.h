7fc040701023f43a100d6c9ae64694c8d3e4479f
Jiabin Yang <360788950@qq.com>
2022-02-18 19:03:15 +0800

    Shared selected rows (#39608)
    * merge legacy to fluid
    
    * Remove legacy code
    
    * Remove legacy code
    
    * Remove DataType test
    
    * Using Tensor directly instead of using EagerTensor
    
    * support gradient_accumulation
    
    * make test_imperative_lod_tensor_to_selected_rows longer
    
    * make test_imperative_lod_tensor_to_selected_rows longer
    
    * refine code
    
    * Rename all EagerTensor to Tensor
    
    * Rename some EagerTensor to Tensor
    
    * rename EagerTensor to EagerVariable
    
    * add more test
    
    * Support copiable selected rows and merge develop

--- paddle/pten/core/selected_rows_impl.h
+++ paddle/pten/core/selected_rows_impl.h
@@ -21,13 +21,19 @@ limitations under the License. */
 #include <utility>
 #include <vector>
 
-#include "paddle/pten/core/selected_rows_impl.h"
+#include "paddle/pten/common/place.h"
+#include "paddle/pten/core/ddim.h"
+#include "paddle/pten/core/dense_tensor.h"
+#include "paddle/pten/core/enforce.h"
+#include "paddle/pten/core/utils/rw_lock.h"
 
+// See Note [ Why still include the fluid headers? ]
+#include "paddle/fluid/framework/mixed_vector.h"
 namespace pten {
-class SelectedRows : public TensorBase,
-                     public TypeInfoTraits<TensorBase, SelectedRows> {
+class SelectedRowsImpl {
   /*
-   * @brief We can use the SelectedRows structure to reproduce a sparse table.
+   * @brief We can use the SelectedRowsImpl structure to reproduce a sparse
+   * table.
    *  A sparse table is a key-value structure that the key is an `int64_t`,
    *  and the value is a Tensor which the first dimension is 0.
    *  You can use the following interface to operate the sparse table, and you
@@ -42,29 +48,32 @@ class SelectedRows : public TensorBase,
    *
    */
  public:
-  SelectedRows(const std::vector<int64_t>& rows, const int64_t& height)
-      : impl_(std::make_shared<pten::SelectedRowsImpl>(rows, height)) {}
-
-  SelectedRows() : impl_(std::make_shared<pten::SelectedRowsImpl>()) {}
-
-  const DenseTensor& value() const { return impl_->value(); }
-
-  DenseTensor* mutable_value() { return impl_->mutable_value(); }
-
-  int64_t height() const { return impl_->height(); }
-
-  void set_height(int64_t height) { impl_->set_height(height); }
-
-  const paddle::framework::Vector<int64_t>& rows() const {
-    return impl_->rows();
+  SelectedRowsImpl(const std::vector<int64_t>& rows, const int64_t& height)
+      : rows_(rows), height_(height) {
+    value_.reset(new DenseTensor());
+    rwlock_.reset(new RWLock);
   }
 
-  paddle::framework::Vector<int64_t>* mutable_rows() {
-    return impl_->mutable_rows();
+  SelectedRowsImpl() {
+    height_ = 0;
+    value_.reset(new DenseTensor());
+    rwlock_.reset(new RWLock);
   }
+
+  const DenseTensor& value() const { return *value_; }
+
+  DenseTensor* mutable_value() { return value_.get(); }
+
+  int64_t height() const { return height_; }
+
+  void set_height(int64_t height) { height_ = height; }
+
+  const paddle::framework::Vector<int64_t>& rows() const { return rows_; }
+
+  paddle::framework::Vector<int64_t>* mutable_rows() { return &rows_; }
 
   void set_rows(const paddle::framework::Vector<int64_t>& rows) {
-    impl_->set_rows(rows);
+    rows_ = rows;
   }
 
   /*
@@ -72,14 +81,21 @@ class SelectedRows : public TensorBase,
    *
    * @return -1 if the key does not exists.
    */
-  int64_t Index(int64_t key) const { return impl_->Index(key); }
+  int64_t Index(int64_t key) const {
+    auto it = std::find(rows_.begin(), rows_.end(), key);
+    if (it == rows_.end()) {
+      PADDLE_THROW(paddle::platform::errors::NotFound(
+          "Input id (%lld) is not in current rows table.", key));
+    }
+    return static_cast<int64_t>(std::distance(rows_.begin(), it));
+  }
 
   /*
    * @brief whether has the specified key in the table.
    *
    * @return true if the key is exists.
    */
-  bool HasKey(int64_t key) const { return impl_->HasKey(key); }
+  bool HasKey(int64_t key) const;
 
   /*
    * @brief Get value by the key list.
@@ -93,15 +109,11 @@ class SelectedRows : public TensorBase,
   void Get(const DenseTensor& ids,
            DenseTensor* value,
            bool auto_grown = false,
-           bool is_test = false) {
-    impl_->Get(ids, value, auto_grown, is_test);
-  }
+           bool is_test = false);
 
   void* AllocateFrom(Allocator* allocator,
                      DataType dtype,
-                     size_t requested_size = 0) override {
-    return impl_->AllocateFrom(allocator, dtype, requested_size);
-  }
+                     size_t requested_size = 0);
 
   /*
    * @brief Get the index of the key from id_to_index_ map. If the key not
@@ -114,62 +126,71 @@ class SelectedRows : public TensorBase,
    *
    * @return index of the key.
    */
-  int64_t AutoGrownIndex(int64_t key, bool auto_grown, bool is_test = false) {
-    return impl_->AutoGrownIndex(key, auto_grown, is_test);
-  }
+  int64_t AutoGrownIndex(int64_t key, bool auto_grown, bool is_test = false);
 
   /*
    * @brief Get the index of the key from id_to_index_ map.
    */
   inline int64_t GetIndexFromId(int64_t key) const {
-    return impl_->GetIndexFromId(key);
+    auto iter = id_to_index_.find(key);
+    if (iter == id_to_index_.end()) {
+      return -1;
+    } else {
+      return iter->second;
+    }
   }
 
-  void SyncIndex() { impl_->SyncIndex(); }
+  void SyncIndex();
   /*
    * @brief Get complete Dims before
    */
   pten::framework::DDim GetCompleteDims() const {
-    return impl_->GetCompleteDims();
+    std::vector<int64_t> dims = vectorize(value_->dims());
+    dims[0] = height_;
+    return pten::framework::make_ddim(dims);
   }
-
-  /// \brief Returns the name of the class for type traits.
-  /// \return The name of the class.
-  static const char* name() { return "SelectedRows"; }
 
   /// \brief Returns the number of elements contained in tensor.
   /// \return The number of elements contained in tensor.
-  int64_t numel() const override { return impl_->numel(); };
+  int64_t numel() const { return value_->numel(); }
 
   /// \brief Returns the dims of the tensor.
   /// \return The dims of the tensor.
-  const DDim& dims() const noexcept override {
-    return impl_->dims();
+  const DDim& dims() const noexcept {
+    return value_->dims();
     // return paddle::framework::make_ddim(dims);
   }
 
   /// \brief Returns the data type of the tensor.
   /// \return The data type of the tensor.
-  DataType dtype() const noexcept override { return impl_->dtype(); }
+  DataType dtype() const noexcept { return value_->dtype(); }
 
   /// \brief Returns the data layout of the tensor.
   /// \return The data layout of the tensor.
-  DataLayout layout() const noexcept override { return impl_->layout(); }
+  DataLayout layout() const noexcept { return value_->layout(); }
 
   /// \brief Returns the data place of the tensor.
   /// \return The data place of the tensor.
-  const Place& place() const override { return impl_->place(); };
+  const Place& place() const { return value_->place(); }
 
   /// \brief Test whether the metadata is valid.
   /// \return Whether the metadata is valid.
-  bool valid() const noexcept override { return impl_->valid(); }
+  bool valid() const noexcept { return value_->valid(); }
 
   /// \brief Test whether the storage is allocated.
   /// return Whether the storage is allocated.
-  bool initialized() const override { return impl_->initialized(); }
+  bool initialized() const { return value_->initialized(); }
 
  private:
-  std::shared_ptr<pten::SelectedRowsImpl> impl_{nullptr};
+  // Notice: rows can be duplicate. We can have {0, 4, 7, 0, 5, 7, 9} here.
+  // SelectedRowsImpl are simply concated when adding together. Until a
+  // SelectedRowsImpl add a Tensor, will the duplicate rows be handled.
+  paddle::framework::Vector<int64_t> rows_;
+  std::unordered_map<int64_t, int64_t>
+      id_to_index_;  // should not be used when rows_ has duplicate member
+  std::unique_ptr<DenseTensor> value_{nullptr};
+  int64_t height_;  // height indicates the underline tensor's height
+  std::unique_ptr<RWLock> rwlock_{nullptr};
 };
 
 }  // namespace pten
