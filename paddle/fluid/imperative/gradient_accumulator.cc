a7cbd3ef6312c095d344a3483be82a36dd8f9ee7
Jiabin Yang <360788950@qq.com>
2022-02-18 09:44:49 +0800

    [Bug Fix]Fix gradient accumulator (#39577)
    * merge legacy to fluid
    
    * Remove legacy code
    
    * Remove legacy code
    
    * Remove DataType test
    
    * Using Tensor directly instead of using EagerTensor
    
    * support gradient_accumulation
    
    * make test_imperative_lod_tensor_to_selected_rows longer
    
    * make test_imperative_lod_tensor_to_selected_rows longer
    
    * refine code
    
    * Rename all EagerTensor to Tensor
    
    * Rename some EagerTensor to Tensor
    
    * rename EagerTensor to EagerVariable
    
    * add more test
    
    * fix different device gradient_accmulator bug
    
    * merge develop
    
    * remove useless tests

--- paddle/fluid/imperative/gradient_accumulator.cc
+++ paddle/fluid/imperative/gradient_accumulator.cc
@@ -300,13 +300,10 @@ void TensorAdd(const VarType& src, VarType* dst) {
                         "should be equal, Otherwise, the calculation results "
                         "will be incorrect."));
 
-#ifdef PADDLE_WITH_XPU
   // if src and dst are in different place, copy dst to src's place
   if (dst_tensor->place() != place) {
     paddle::framework::TensorCopySync(*dst_tensor, place, dst_tensor);
   }
-#endif
-
 #define PADDLE_TENSOR_ADD(cpp_type)                                  \
   if (data_type == framework::DataTypeTrait<cpp_type>::DataType()) { \
     TensorAddFunctor<cpp_type> func(                                 \
