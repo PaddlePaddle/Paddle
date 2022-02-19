b97e9b096223194b53586dd7157674fa906fbe9d
Aurelius84 <zhangliujie@baidu.com>
2022-02-19 06:51:53 +0000

    fix conflict

--- paddle/fluid/eager/api/utils/tensor_utils.cc
+++ paddle/fluid/eager/api/utils/tensor_utils.cc
@@ -42,8 +42,8 @@ paddle::experimental::Tensor CreateTensorWithValue(
     const pten::DataType& dtype, const pten::DataLayout& layout, float value,
     bool is_leaf) {
   paddle::experimental::Tensor out = paddle::experimental::full(
-      pten::vectorize(ddim), paddle::experimental::Scalar(value),
-      dtype, pten::TransToPtenBackend(place));
+      pten::vectorize(ddim), paddle::experimental::Scalar(value), dtype,
+      pten::TransToPtenBackend(place));
 
   auto meta = EagerUtils::autograd_meta(&out);
   if (is_leaf) {
