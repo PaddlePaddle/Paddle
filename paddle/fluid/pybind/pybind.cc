4877bbad9e4e2fc790549200ab88c06e5cb6a462
Zhong Hui <zhonghui.net@gmail.com>
2022-02-21 11:21:11 +0000

    fix merge issues.

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -1199,7 +1199,7 @@ PYBIND11_MODULE(core_noavx, m) {
              size_t size = t[0].cast<size_t>();
              auto dtype =
                  static_cast<paddle::experimental::DataType>(t[1].cast<int>());
-             auto dims = make_ddim(t[2].cast<std::vector<int>>());
+             auto dims = phi::make_ddim(t[2].cast<std::vector<int>>());
              auto lod_info = t[3].cast<framework::LoD>();
              auto device_id = t[4].cast<int>();
 
@@ -1313,7 +1313,7 @@ PYBIND11_MODULE(core_noavx, m) {
              tensor.ResetHolderWithType(
                  shared_reader_holder,
                  static_cast<paddle::experimental::DataType>(t[3].cast<int>()));
-             tensor.Resize(make_ddim(t[4].cast<std::vector<int>>()));
+             tensor.Resize(phi::make_ddim(t[4].cast<std::vector<int>>()));
              tensor.set_lod(t[5].cast<framework::LoD>());
 
              return tensor;
@@ -1424,7 +1424,7 @@ PYBIND11_MODULE(core_noavx, m) {
              tensor.ResetHolderWithType(
                  shared_holder,
                  static_cast<paddle::experimental::DataType>(t[2].cast<int>()));
-             tensor.Resize(make_ddim(t[3].cast<std::vector<int>>()));
+             tensor.Resize(phi::make_ddim(t[3].cast<std::vector<int>>()));
              tensor.set_lod(t[4].cast<framework::LoD>());
 
              return tensor;
