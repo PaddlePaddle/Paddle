a47ecef3474c59f0c072648e1081af879508598b
xiongkun <xiongkun03@baidu.com>
2022-02-21 10:50:24 +0000

    transfer kldiv_loss and kldiv_loss_grad; pass the unitest

--- paddle/fluid/operators/kldiv_loss_op_npu.cc
+++ paddle/fluid/operators/kldiv_loss_op_npu.cc
@@ -12,8 +12,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the Licnse. */
 
-#include "paddle/fluid/operators/kldiv_loss_op.h"
+#include <string>
+#include "paddle/fluid/framework/eigen.h"
+#include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/platform/device/npu/npu_op_runner.h"
+#include "paddle/phi/core/hostdevice.h"
 
 namespace paddle {
 namespace operators {
