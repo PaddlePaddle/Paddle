d6b243e36a1521ff912d852372d00152554d8a53
maxhuiy <1508399706@qq.com>
2022-02-22 07:26:50 +0000

    [MLU] fix the cncl_context_test

--- paddle/fluid/imperative/tests/cncl_context_test.cc
+++ paddle/fluid/imperative/tests/cncl_context_test.cc
@@ -54,7 +54,7 @@ void Broadcast(int local_rank, int device_id) {
 
   framework::Variable* src_dev_var(new framework::Variable());
   auto* src_dev_tensor = src_dev_var->GetMutable<framework::LoDTensor>();
-  src_dev_tensor->mutable_data<float>(framework::make_ddim({data_size}), place);
+  src_dev_tensor->mutable_data<float>(phi::make_ddim({data_size}), place);
 
   // fill data for rank 0 only
   std::vector<float> src_vec;
@@ -101,7 +101,7 @@ void AllReduceByStream(int local_rank, int device_id) {
   // input data
   framework::Variable* src_dev_var(new framework::Variable());
   auto* src_dev_tensor = src_dev_var->GetMutable<framework::LoDTensor>();
-  src_dev_tensor->mutable_data<float>(framework::make_ddim({data_size}), place);
+  src_dev_tensor->mutable_data<float>(phi::make_ddim({data_size}), place);
 
   // fill input data
   std::vector<float> src_vec;
@@ -114,7 +114,7 @@ void AllReduceByStream(int local_rank, int device_id) {
   // output data
   framework::Variable* dst_dev_var(new framework::Variable());
   auto* dst_dev_tensor = dst_dev_var->GetMutable<framework::LoDTensor>();
-  dst_dev_tensor->mutable_data<float>(framework::make_ddim({data_size}), place);
+  dst_dev_tensor->mutable_data<float>(phi::make_ddim({data_size}), place);
 
   // call allreduce
   cpc.AllReduceByStream(*src_dev_var, dst_dev_var, 0, false);
