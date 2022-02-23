a54d34834f78d95ad05909d45c3a6020dc2564d6
Ren Wei <wadefelix@gmail.com>
2022-02-23 09:40:30 +0800

    don't merge, testing pipeline

--- python/paddle/fluid/framework.py
+++ python/paddle/fluid/framework.py
@@ -195,6 +195,9 @@ def require_version(min_version, max_version=None):
 
                 # if 0.1.0 <= version <= 10.0.0, it is acceptable.
                 fluid.require_version(min_version='0.1.0', max_version='10.0.0')
+
+                # placeholder
+                print('Done')
         """
     if not isinstance(min_version, str):
         raise TypeError(
