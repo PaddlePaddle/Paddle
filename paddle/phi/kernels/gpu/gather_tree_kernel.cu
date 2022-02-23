3855b0b8ee75e5a516320ed0fc8a2a0e94e31526
zhoujianqian <15205085056@163.com>
2022-02-23 06:17:03 +0000

    migrate gather_tree_op into phi

--- paddle/phi/kernels/gpu/gather_tree_kernel.cu
+++ paddle/phi/kernels/gpu/gather_tree_kernel.cu
@@ -12,11 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/backends/cpu/cpu_context.h"
+#include <algorithm>
+#include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/device_context.h"
 #include "paddle/phi/core/kernel_registry.h"
 
 namespace phi {
+
+template <typename T>
+__global__ void GatherTree(const T *ids_data,
+                           const T *parents_data,
+                           T *out_data,
+                           const int64_t max_length,
+                           const int64_t batch_size,
+                           const int64_t beam_size) {
+  CUDA_KERNEL_LOOP(i, batch_size * beam_size) {
+    int batch = i / beam_size;
+    int beam = i % beam_size;
+    auto idx =
+        (max_length - 1) * batch_size * beam_size + batch * beam_size + beam;
+    out_data[idx] = ids_data[idx];
+    auto parent = parents_data[idx];
+    for (int step = max_length - 2; step >= 0; step--) {
+      idx = step * batch_size * beam_size + batch * beam_size;
+      out_data[idx + beam] = ids_data[idx + parent];
+      parent = parents_data[idx + parent];
+    }
+  }
+}
 
 template <typename T, typename Context>
 void GatherTreeKernel(const Context &dev_ctx,
@@ -25,13 +48,7 @@ void GatherTreeKernel(const Context &dev_ctx,
                       DenseTensor *out) {
   const auto *ids_data = ids.data<T>();
   const auto *parents_data = parents.data<T>();
-
   T *out_data = dev_ctx.template Alloc<T>(out);
-
-  auto &ids_dims = ids.dims();
-  auto max_length = ids_dims[0];
-  auto batch_size = ids_dims[1];
-  auto beam_size = ids_dims[2];
 
   PADDLE_ENFORCE_NOT_NULL(ids_data,
                           phi::errors::InvalidArgument(
@@ -42,22 +59,21 @@ void GatherTreeKernel(const Context &dev_ctx,
       phi::errors::InvalidArgument(
           "Input(Parents) of gather_tree should not be null."));
 
-  for (int batch = 0; batch < batch_size; batch++) {
-    for (int beam = 0; beam < beam_size; beam++) {
-      auto idx =
-          (max_length - 1) * batch_size * beam_size + batch * beam_size + beam;
-      out_data[idx] = ids_data[idx];
-      auto parent = parents_data[idx];
-      for (int step = max_length - 2; step >= 0; step--) {
-        idx = step * batch_size * beam_size + batch * beam_size;
-        out_data[idx + beam] = ids_data[idx + parent];
-        parent = parents_data[idx + parent];
-      }
-    }
-  }
+  auto &ids_dims = ids.dims();
+  int64_t max_length = ids_dims[0];
+  int64_t batch_size = ids_dims[1];
+  int64_t beam_size = ids_dims[2];
+
+  const int block = 512;
+  int max_threads =
+      std::min(static_cast<int64_t>(dev_ctx.GetMaxPhysicalThreadCount()),
+               batch_size * beam_size);
+  const int grid = std::max(max_threads / block, 1);
+  GatherTree<<<grid, block>>>(
+      ids_data, parents_data, out_data, max_length, batch_size, beam_size);
 }
 
 }  // namespace phi
 
 PD_REGISTER_KERNEL(
-    gather_tree, CPU, ALL_LAYOUT, phi::GatherTreeKernel, int, int64_t) {}
+    gather_tree, GPU, ALL_LAYOUT, phi::GatherTreeKernel, int, int64_t) {}
