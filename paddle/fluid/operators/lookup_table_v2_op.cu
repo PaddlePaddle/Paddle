4c5e0c4981cbb1688c14aa0b4622028fbad0ba55
limin2021 <1121099234@qq.com>
2022-02-23 09:06:49 +0000

    Polish.

--- paddle/fluid/operators/lookup_table_v2_op.cu
+++ paddle/fluid/operators/lookup_table_v2_op.cu
@@ -200,6 +200,7 @@ struct LookupTableV2GradCUDAFunctor {
       const T *d_output = d_output_t->template data<T>();
       const auto *ids = ids_t_->template data<IdT>();
       T *d_table = d_table_t->mutable_data<T>(context_.GetPlace());
+
 #ifdef PADDLE_WITH_HIP
       PADDLE_ENFORCE_GPU_SUCCESS(
           hipMemsetAsync(d_table, 0, N * D * sizeof(T), dev_ctx.stream()));
