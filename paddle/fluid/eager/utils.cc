99988c64d6be1753be0b376d4fdd07b1bf8dc441
jim19930609 <jim19930609@gmail.com>
2022-02-23 12:30:55 +0000

    Removed restriction on AccumulationNode modification

--- paddle/fluid/eager/utils.cc
+++ paddle/fluid/eager/utils.cc
@@ -123,11 +123,12 @@ void EagerUtils::SetHistory(std::vector<AutogradMeta*>* autograd_metas,
                             const std::shared_ptr<GradNodeBase>& grad_node) {
   for (const auto& autograd_meta : *autograd_metas) {
     if (dynamic_cast<GradNodeAccumulation*>(autograd_meta->GradNode())) {
-      PADDLE_THROW(paddle::platform::errors::Fatal(
-          "Error: Reseting GradNodeAccumulation for leaf tensor is not allowed."
-          "You're not expected to hit this error, likely an internal issue "
-          "with Paddle."
-          "Please contact Paddle developers for more details."));
+      VLOG(6)
+          << "Warning: Reseting GradNodeAccumulation for leaf tensor is not "
+             "allowed."
+             "You're not expected to hit this error, likely an internal issue "
+             "with Paddle."
+             "Please contact Paddle developers for more details.";
     }
     autograd_meta->SetGradNode(grad_node);
   }
@@ -136,11 +137,12 @@ void EagerUtils::SetHistory(std::vector<AutogradMeta*>* autograd_metas,
 void EagerUtils::SetHistory(AutogradMeta* autograd_meta,
                             const std::shared_ptr<GradNodeBase>& grad_node) {
   if (dynamic_cast<GradNodeAccumulation*>(autograd_meta->GradNode())) {
-    PADDLE_THROW(paddle::platform::errors::Fatal(
-        "Error: Reseting GradNodeAccumulation for leaf tensor is not allowed."
-        "You're not expected to hit this error, likely an internal issue with "
-        "Paddle."
-        "Please contact Paddle developers for more details."));
+    VLOG(6)
+        << "Warning: Reseting GradNodeAccumulation for leaf tensor is not "
+           "allowed."
+           "You're not expected to hit this error, likely an internal issue "
+           "with Paddle."
+           "Please contact Paddle developers for more details.";
   }
 
   autograd_meta->SetGradNode(grad_node);
