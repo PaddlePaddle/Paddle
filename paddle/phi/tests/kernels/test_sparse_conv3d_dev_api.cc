84e317e88745d34f867830bb70b5ee2b62eb476b
zkh2016 <zhangkaihuo@baidu.com>
2022-02-24 03:14:06 +0000

    only cpu

--- paddle/phi/tests/kernels/test_sparse_conv3d_dev_api.cc
+++ paddle/phi/tests/kernels/test_sparse_conv3d_dev_api.cc
@@ -15,7 +15,6 @@ limitations under the License. */
 #include <gtest/gtest.h>
 #include <memory>
 
-#include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/common/place.h"
 #include "paddle/phi/kernels/copy_kernel.h"
 #include "paddle/phi/kernels/sparse/convolution_kernel.h"
@@ -120,81 +119,6 @@ void TestConv3d(const std::vector<int>& indices,
       ASSERT_LT(tmp, diff);
     }
   }
-
-// test gpu
-#if defined(PADDLE_WITH_CUDA)
-  phi::GPUContext dev_ctx_gpu;
-  dev_ctx_gpu.PartialInitWithoutAllocator();
-  dev_ctx_gpu.SetAllocator(
-      paddle::memory::allocation::AllocatorFacade::Instance()
-          .GetAllocator(dev_ctx_gpu.GetPlace(), dev_ctx_gpu.stream())
-          .get());
-  dev_ctx_gpu.SetHostAllocator(
-      paddle::memory::allocation::AllocatorFacade::Instance()
-          .GetAllocator(phi::CPUPlace())
-          .get());
-  dev_ctx_gpu.PartialInitWithAllocator();
-
-  const auto cuda_alloc =
-      std::make_shared<paddle::experimental::DefaultAllocator>(
-          paddle::platform::CUDAPlace());
-  DenseTensor d_indices_tensor(
-      cuda_alloc.get(),
-      DenseTensorMeta(DataType::INT32, {4, non_zero_num}, DataLayout::NCHW));
-  phi::Copy(dev_ctx_gpu, indices_tensor, true, &d_indices_tensor);
-  DenseTensor d_features_tensor(
-      cuda_alloc.get(),
-      DenseTensorMeta(paddle::experimental::CppTypeToDataType<T>::Type(),
-                      {non_zero_num, in_channels},
-                      DataLayout::NHWC));
-  phi::Copy(dev_ctx_gpu, features_tensor, true, &d_features_tensor);
-
-  SparseCooTensor d_x_tensor(d_indices_tensor, d_features_tensor, x_dims);
-
-  // TODO(zhangkaihuo) change layout to DHWCOC
-  DenseTensor d_kernel_tensor(
-      cuda_alloc.get(),
-      DenseTensorMeta(paddle::experimental::CppTypeToDataType<T>::Type(),
-                      kernel_dims,
-                      DataLayout::NHWC));
-  phi::Copy(dev_ctx_gpu, kernel_tensor, true, &d_kernel_tensor);
-
-  SparseCooTensor d_out = sparse::Conv3d<T>(dev_ctx_gpu,
-                                            d_x_tensor,
-                                            d_kernel_tensor,
-                                            paddings,
-                                            dilations,
-                                            strides,
-                                            1);
-
-  ASSERT_EQ(correct_out_dims.size(), d_out.dims().size());
-  ASSERT_EQ((int64_t)correct_out_features.size() / out_channels, d_out.nnz());
-  for (int i = 0; i < correct_out_dims.size(); i++) {
-    ASSERT_EQ(correct_out_dims[i], d_out.dims()[i]);
-  }
-
-  DenseTensor h_indices_tensor(
-      alloc.get(),
-      DenseTensorMeta(DataType::INT32, {4, d_out.nnz()}, DataLayout::NCHW));
-  phi::Copy(dev_ctx_gpu, d_out.non_zero_indices(), true, &h_indices_tensor);
-
-  int cmp_indices2 = memcmp(correct_out_indices.data(),
-                            h_indices_tensor.data<int>(),
-                            correct_out_indices.size() * sizeof(int));
-  ASSERT_EQ(cmp_indices2, 0);
-
-  DenseTensor h_features_tensor(
-      alloc.get(),
-      DenseTensorMeta(paddle::experimental::CppTypeToDataType<T>::Type(),
-                      {d_out.nnz()},
-                      d_out.layout()));
-  phi::Copy(dev_ctx_gpu, d_out.non_zero_elements(), true, &h_features_tensor);
-  for (uint64_t i = 0; i < correct_out_features.size(); i++) {
-    float tmp = std::fabs(static_cast<float>(correct_out_features[i] -
-                                             h_features_tensor.data<T>()[i]));
-    ASSERT_LT(tmp, diff);
-  }
-#endif
 }
 
 TEST(DEV_API, sparse_conv3d) {
@@ -553,5 +477,70 @@ TEST(DEV_API, sparse_conv3d_padding) {
                       dilations);
 }
 
+TEST(DEV_API, sparse_conv2d) {
+  const int in_channels = 1;
+  const int out_channels = 1;
+  DDim x_dims = {1, 1, 5, 5, in_channels};
+  DDim kernel_dims = {1, 3, 3, in_channels, out_channels};
+  DDim out_dims = {1, 1, 3, 3, out_channels};
+  std::vector<int> paddings = {0, 0, 0};
+  std::vector<int> strides = {1, 1, 1};
+  std::vector<int> dilations = {1, 1, 1};
+
+  const int non_zero_num = 3;
+  std::vector<int> indices_flatten = {0, 0, 0, 0, 0, 0, 0, 4, 0, 3, 2, 4};
+
+  std::vector<float> features = {-0.79394531, -0.3125, -0.55029297};
+  // 3*3*3=27
+  std::vector<float> kernel = {0.65820312,
+                               0.75048828,
+                               0.21411133,
+                               0.17370605,
+                               0.85546875,
+                               0.53076172,
+                               0.28833008,
+                               0.71044922,
+                               0.00659943};
+
+  std::vector<int> out_indices_flatten = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 2, 2, 2, 1, 2, 0, 1, 2};
+
+  std::vector<float> out_features = {
+      -0.17004, -0.71338, -0.00206, -0.22205, -0.09009};
+
+  TestConv3d<float>(indices_flatten,
+                    features,
+                    x_dims,
+                    kernel,
+                    kernel_dims,
+                    out_indices_flatten,
+                    out_features,
+                    out_dims,
+                    non_zero_num,
+                    paddings,
+                    strides,
+                    dilations);
+
+  // test fp16
+  using phi::dtype::float16;
+  std::vector<float16> features_fp16(features.size()),
+      out_features_fp16(out_features.size()), kernel_fp16(kernel.size());
+  cast<float, float16>(features, &features_fp16);
+  cast<float, float16>(out_features, &out_features_fp16);
+  cast<float, float16>(kernel, &kernel_fp16);
+
+  TestConv3d<float16>(indices_flatten,
+                      features_fp16,
+                      x_dims,
+                      kernel_fp16,
+                      kernel_dims,
+                      out_indices_flatten,
+                      out_features_fp16,
+                      out_dims,
+                      non_zero_num,
+                      paddings,
+                      strides,
+                      dilations);
+}
 }  // namespace tests
 }  // namespace phi
