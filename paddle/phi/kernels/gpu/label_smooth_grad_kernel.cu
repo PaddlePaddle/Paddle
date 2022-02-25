035151872c9f752f2bfcf2af9fecabc976861557
seemingwang <zsasuke@qq.com>
2022-02-25 06:10:56 +0000

    fix

--- paddle/phi/kernels/gpu/label_smooth_grad_kernel.cu
+++ paddle/phi/kernels/gpu/label_smooth_grad_kernel.cu
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/framework/tensor_util.h"
 #include "paddle/fluid/operators/elementwise/elementwise_op_impl.cu.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
