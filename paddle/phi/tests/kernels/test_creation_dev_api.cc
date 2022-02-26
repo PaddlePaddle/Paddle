98fa938ffa73684e3cad5d7ef41117aeddee4e54
zyfncg <zhangyunfei07@baidu.com>
2022-02-26 10:18:22 +0000

    fix empyt bug

--- paddle/phi/tests/kernels/test_creation_dev_api.cc
+++ paddle/phi/tests/kernels/test_creation_dev_api.cc
@@ -39,7 +39,7 @@ TEST(DEV_API, empty) {
   dev_ctx.Init();
 
   // 2. test API
-  auto out = phi::Empty<float>(dev_ctx, {3, 2}, phi::DataType::INT32);
+  auto out = phi::Empty<int>(dev_ctx, {3, 2}, phi::DataType::INT32);
 
   // 3. check result
   ASSERT_EQ(out.dims().size(), 2);
