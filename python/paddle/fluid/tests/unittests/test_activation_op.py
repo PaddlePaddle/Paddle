934032454b3e17d75da655db870e9aa56af7bd1f
zhangbo9674 <zhangbo54@baidu.com>
2022-03-02 08:10:13 +0000

    refine unittest

--- python/paddle/fluid/tests/unittests/test_activation_op.py
+++ python/paddle/fluid/tests/unittests/test_activation_op.py
@@ -18,7 +18,7 @@ import unittest
 import numpy as np
 from scipy.special import expit, erf
 
-from paddle.fluid.tests.unittests.op_test import OpTest, convert_float_to_uint16, skip_check_grad_ci, convert_float_to_uint16
+from paddle.fluid.tests.unittests.op_test import OpTest, convert_float_to_uint16, skip_check_grad_ci
 import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
