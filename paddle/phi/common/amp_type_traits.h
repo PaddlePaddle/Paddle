704a37a13bea2a98f774c0adf4a4187feb4ce3c8
zhangbo9674 <82555433+zhangbo9674@users.noreply.github.com>
2022-03-02 16:14:31 +0800

    [bf16] add bf16 kernel: softmax & log_softmax (#39999)
    * add softmax log_softmax
    
    * refine rocm
    
    * refine unittest

--- paddle/phi/common/amp_type_traits.h
+++ paddle/phi/common/amp_type_traits.h
@@ -4,7 +4,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,19 +12,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/phi/kernels/softmax_kernel.h"
+#pragma once
 
-#include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/common/float16.h"
-#include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/impl/softmax_kernel_impl.h"
 
-PD_REGISTER_KERNEL(softmax,
-                   GPU,
-                   ALL_LAYOUT,
-                   phi::SoftmaxRawKernel,
-                   float,
-                   double,
-                   phi::dtype::float16,
-                   phi::dtype::bfloat16) {}
+namespace phi {
+namespace dtype {
+
+template <typename T>
+class MPTypeTrait {
+ public:
+  using Type = T;
+};
+
+template <>
+class MPTypeTrait<phi::dtype::float16> {
+ public:
+  using Type = float;
+};
+
+template <>
+class MPTypeTrait<phi::dtype::bfloat16> {
+ public:
+  using Type = float;
+};
+
+}  // namespace dtype
+}  // namespace phi
