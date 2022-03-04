2142891bdc0adadd569be31d87279c6211bc64a3
tink2123 <y_tink@163.com>
2022-03-04 10:06:52 +0000

    fix fold dygraph mode, test=develop

--- python/paddle/nn/functional/common.py
+++ python/paddle/nn/functional/common.py
@@ -1915,7 +1915,7 @@ def fold(x,
             "Unexpected type of paddings, it should be either an integer or a list"
             "of 2 or 4 integers")
 
-    if in_dygraph_mode():
+    if in_dynamic_mode():
         out = _C_ops.fold(x, "output_sizes", output_sizes, "kernel_sizes",
                           kernel_sizes, "strides", strides, "paddings",
                           paddings, "dilations", dilations)
