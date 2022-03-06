312ef41fea1cebebfdd684140e6de4e0bd408284
wjj19950828 <wjjisloser@163.com>
2022-03-06 17:29:41 +0800

    replace pt by pd

--- paddle/fluid/operators/is_empty_op.cc
+++ paddle/fluid/operators/is_empty_op.cc
@@ -52,8 +52,8 @@ It will just return product(tensor.ddims()) > 0;
 }  // namespace paddle
 
 namespace ops = paddle::operators;
-DELCARE_INFER_SHAPE_FUNCTOR(is_empty, IsEmptyInferShapeFunctor,
-                            PT_INFER_META(phi::IsEmptyInferMeta));
+DECLARE_INFER_SHAPE_FUNCTOR(is_empty, IsEmptyInferShapeFunctor,
+                            PD_INFER_META(phi::IsEmptyInferMeta));
 REGISTER_OPERATOR(
     is_empty, ops::IsEmptyOp, ops::IsEmptyOpMaker,
     paddle::framework::EmptyGradOpMaker<paddle::framework::OpDesc>,
