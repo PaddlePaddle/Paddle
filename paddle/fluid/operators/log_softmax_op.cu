bc565055362346b1e1af6f7a39ec62424bcb8927
ZzSean <18818272991@163.com>
2022-03-08 06:36:37 +0000

    fixfixfixfix

--- paddle/fluid/operators/log_softmax_op.cu
+++ paddle/fluid/operators/log_softmax_op.cu
@@ -14,6 +14,45 @@
 
 #include "paddle/fluid/operators/log_softmax_op.h"
 #include "paddle/fluid/platform/float16.h"
+#include "paddle/phi/kernels/gpudnn/softmax_gpudnn.h"
+
+namespace paddle {
+namespace operators {
+
+using Tensor = framework::Tensor;
+
+template <typename T>
+class LogSoftmaxCUDNNKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
+    auto* x = ctx.Input<Tensor>("X");
+    auto* out = ctx.Output<Tensor>("Out");
+    out->mutable_data<T>(ctx.GetPlace());
+
+    int input_axis = ctx.Attr<int>("axis");
+    auto& dev_ctx = ctx.template device_context<platform::CUDADeviceContext>();
+    phi::SoftmaxForwardCUDAKernelDriver<T, true>(dev_ctx, *x, input_axis, out);
+  }
+};
+
+template <typename T>
+class LogSoftmaxGradCUDNNKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
+    auto* out = ctx.Input<Tensor>("Out");
+    auto* dout = ctx.Input<Tensor>(framework::GradVarName("Out"));
+    auto* dx = ctx.Output<Tensor>(framework::GradVarName("X"));
+    dx->mutable_data<T>(ctx.GetPlace());
+
+    int input_axis = ctx.Attr<int>("axis");
+    auto& dev_ctx = ctx.template device_context<platform::CUDADeviceContext>();
+    phi::SoftmaxBackwardCUDAKernelDriver<T, true>(dev_ctx, *out, *dout,
+                                                  input_axis, dx);
+  }
+};
+
+}  // namespace operators
+}  // namespace paddle
 
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
@@ -27,3 +66,35 @@ REGISTER_OP_CUDA_KERNEL(
     ops::LogSoftmaxGradKernel<plat::CUDADeviceContext, double>,
     ops::LogSoftmaxGradKernel<plat::CUDADeviceContext, plat::float16>,
     ops::LogSoftmaxGradKernel<plat::CUDADeviceContext, plat::bfloat16>);
+#ifdef PADDLE_WITH_HIP
+REGISTER_OP_KERNEL(log_softmax, CUDNN, plat::CUDAPlace,
+                   ops::LogSoftmaxCUDNNKernel<float>,
+                   ops::LogSoftmaxCUDNNKernel<plat::float16>,
+                   ops::LogSoftmaxCUDNNKernel<plat::bfloat16>);
+REGISTER_OP_KERNEL(log_softmax_grad, CUDNN, plat::CUDAPlace,
+                   ops::LogSoftmaxGradCUDNNKernel<float>,
+                   ops::LogSoftmaxGradCUDNNKernel<plat::float16>,
+                   ops::LogSoftmaxGradCUDNNKernel<plat::bfloat16>);
+#else
+#if CUDNN_VERSION_MIN(8, 1, 0)
+REGISTER_OP_KERNEL(log_softmax, CUDNN, plat::CUDAPlace,
+                   ops::LogSoftmaxCUDNNKernel<float>,
+                   ops::LogSoftmaxCUDNNKernel<double>,
+                   ops::LogSoftmaxCUDNNKernel<plat::float16>,
+                   ops::LogSoftmaxCUDNNKernel<plat::bfloat16>);
+REGISTER_OP_KERNEL(log_softmax_grad, CUDNN, plat::CUDAPlace,
+                   ops::LogSoftmaxGradCUDNNKernel<float>,
+                   ops::LogSoftmaxGradCUDNNKernel<double>,
+                   ops::LogSoftmaxGradCUDNNKernel<plat::float16>,
+                   ops::LogSoftmaxGradCUDNNKernel<plat::bfloat16>);
+#else
+REGISTER_OP_KERNEL(log_softmax, CUDNN, plat::CUDAPlace,
+                   ops::LogSoftmaxCUDNNKernel<float>,
+                   ops::LogSoftmaxCUDNNKernel<double>,
+                   ops::LogSoftmaxCUDNNKernel<plat::float16>);
+REGISTER_OP_KERNEL(log_softmax_grad, CUDNN, plat::CUDAPlace,
+                   ops::LogSoftmaxGradCUDNNKernel<float>,
+                   ops::LogSoftmaxGradCUDNNKernel<double>,
+                   ops::LogSoftmaxGradCUDNNKernel<plat::float16>);
+#endif
+#endif
