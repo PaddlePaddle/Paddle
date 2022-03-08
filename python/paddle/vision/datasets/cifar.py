1e30b78ed67fde44d5cbb53e6a05103350a2e97a
dengkaipeng <dengkaipeng@baidu.com>
2022-03-08 14:20:45 +0000

    fix assert for Cifar10/Cifar100. test=develop

--- python/paddle/vision/datasets/cifar.py
+++ python/paddle/vision/datasets/cifar.py
@@ -102,8 +102,8 @@ class Cifar10(Dataset):
                  transform=None,
                  download=True,
                  backend=None):
-        assert mode.lower() in ['train', 'test', 'train', 'test'], \
-            "mode should be 'train10', 'test10', 'train100' or 'test100', but got {}".format(mode)
+        assert mode.lower() in ['train', 'test'], \
+            "mode should be train' or 'test', but got {}".format(mode)
         self.mode = mode.lower()
 
         if backend is None:
