d9733aaf7c286dbb434dba8f617579aa52a97e72
Linjie Chen <40840292+linjieccc@users.noreply.github.com>
2022-03-09 22:38:14 +0800

    [phi] move viterbi_decode to phi (#40186)
    * move viterbi to phi
    
    * move infershape to phi
    
    * update infershape
    
    * fix
    
    * resolve conflicts

--- paddle/phi/infermeta/ternary.h
+++ paddle/phi/infermeta/ternary.h
@@ -53,6 +53,14 @@ void ScatterNdAddInferMeta(const MetaTensor& x,
                            const MetaTensor& updates,
                            MetaTensor* out);
 
+void ViterbiDecodeInferMeta(const MetaTensor& input,
+                            const MetaTensor& transition,
+                            const MetaTensor& length,
+                            bool include_bos_eos_tag,
+                            MetaTensor* scores,
+                            MetaTensor* path,
+                            MetaConfig config = MetaConfig());
+
 void LerpInferMeta(const MetaTensor& x,
                    const MetaTensor& y,
                    const MetaTensor& weight,
