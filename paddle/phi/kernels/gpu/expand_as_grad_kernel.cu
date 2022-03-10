1f749587aaadab3732a2552e363e20aeec41b890
ZzSean <18818272991@163.com>
2022-03-10 09:15:51 +0000

    fix

--- paddle/phi/kernels/gpu/expand_as_grad_kernel.cu
+++ paddle/phi/kernels/gpu/expand_as_grad_kernel.cu
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/backends/gpu/gpu_context.h"
-#include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/expand_as_grad_kernel.h"
 #include "paddle/phi/kernels/impl/expand_as_grad_kernel_impl.h"
+
+#include "paddle/phi/backends/gpu/gpu_context.h"
+#include "paddle/phi/core/kernel_registry.h"
 
 PD_REGISTER_KERNEL(expand_as_grad,
                    GPU,
--- paddle/phi/kernels/gpu/expand_as_kernel.cu
+++ paddle/phi/kernels/gpu/expand_as_kernel.cu
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/backends/gpu/gpu_context.h"
-#include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/expand_as_kernel.h"
 #include "paddle/phi/kernels/impl/expand_as_kernel_impl.h"
+
+#include "paddle/phi/backends/gpu/gpu_context.h"
+#include "paddle/phi/core/kernel_registry.h"
 
 PD_REGISTER_KERNEL(expand_as,
                    GPU,
