8315f00c0d87f95276ee2354ecfcaa0ddb55f329
phlrain <phliuhongyu@126.com>
2022-03-10 03:17:31 +0000

    fix compare error; test=develop

--- python/paddle/fluid/tests/unittests/test_compare_op.py
+++ python/paddle/fluid/tests/unittests/test_compare_op.py
@@ -36,7 +36,7 @@ def create_test_class(op_type, typename, callback):
             self.op_type = op_type
 
         def test_output(self):
-            self.check_output(check_eager=True)
+            self.check_output(check_eager=False)
 
         def test_errors(self):
             paddle.enable_static()
