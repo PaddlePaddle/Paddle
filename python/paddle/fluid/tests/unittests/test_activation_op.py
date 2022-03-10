c0b3d600478073e6756ea359dd5d12864456295c
pangyoki <pangyoki@126.com>
2022-03-10 07:48:54 +0000

    fix CI Coverage error

--- python/paddle/fluid/tests/unittests/test_activation_op.py
+++ python/paddle/fluid/tests/unittests/test_activation_op.py
@@ -147,7 +147,7 @@ class TestParameter(object):
             exe = fluid.Executor(place)
             result, = exe.run(feed={"X": np_x}, fetch_list=[out])
             expected = eval("np.%s(np_x)" % self.op_type)
-            self.assertEqual(result, expected)
+            self.assertTrue(np.allclose(result, expected))
 
     def test_dygraph(self):
         with fluid.dygraph.guard():
@@ -156,10 +156,7 @@ class TestParameter(object):
             z = eval("paddle.%s(x).numpy()" % self.op_type)
             z_expected = eval("np.%s(np_x)" % self.op_type)
             # ROCM platform will fail in assertEqual
-            if core.is_compiled_with_rocm():
-                self.assertTrue(np.allclose(z, z_expected))
-            else:
-                self.assertEqual(z, z_expected)
+            self.assertTrue(np.allclose(z, z_expected))
 
 
 class TestSigmoid(TestActivation):
