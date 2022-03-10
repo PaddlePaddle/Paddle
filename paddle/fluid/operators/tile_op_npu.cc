0de44a092eb9ac2ef18d7b2cf2356a3abc9fc4ec
Aurelius84 <zhangliujie@baidu.com>
2022-03-10 11:33:28 +0000

    fix include headers

--- paddle/fluid/operators/tile_op_npu.cc
+++ paddle/fluid/operators/tile_op_npu.cc
@@ -12,6 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/framework/op_registry.h"
+#include "paddle/fluid/operators/tile_op_functor.h"
 #include "paddle/fluid/platform/device/npu/npu_op_runner.h"
 
 namespace paddle {
--- paddle/fluid/operators/tile_op_xpu.cc
+++ paddle/fluid/operators/tile_op_xpu.cc
@@ -12,6 +12,7 @@ limitations under the License. */
 #ifdef PADDLE_WITH_XPU
 
 #include "paddle/fluid/framework/op_registry.h"
+#include "paddle/fluid/operators/tile_op_functor.h"
 
 namespace paddle {
 namespace operators {
