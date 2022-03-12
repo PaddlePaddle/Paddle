1f168bd5ac5f9193885460b41347e499e8342760
SigureMo <sigure.qaq@gmail.com>
2022-03-12 13:04:19 +0800

    align to cn docs

--- python/paddle/optimizer/sgd.py
+++ python/paddle/optimizer/sgd.py
@@ -59,16 +59,14 @@ class SGD(Optimizer):
         .. code-block:: python
 
             import paddle
-            import numpy as np
-            inp = np.random.uniform(-0.1, 0.1, [10, 10]).astype("float32")
+
+            inp = paddle.uniform(min=-0.1, max=0.1, shape=[10, 10], dtype='float32')
             linear = paddle.nn.Linear(10, 10)
             inp = paddle.to_tensor(inp)
             out = linear(inp)
             loss = paddle.mean(out)
-            beta1 = paddle.to_tensor([0.9], dtype="float32")
-            beta2 = paddle.to_tensor([0.99], dtype="float32")
-            sgd = paddle.optimizer.SGD(learning_rate=0.1, parameters=linear.parameters(), weight_decay=0.01)
-            back = out.backward()
+            sgd = paddle.optimizer.SGD(learning_rate=0.1, parameters=linear.parameters())
+            out.backward()
             sgd.step()
             sgd.clear_grad()
 
