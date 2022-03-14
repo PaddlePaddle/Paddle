6184dd3822527c362316ac0dcdd94f4fa3aeeb5d
ZzSean <18818272991@163.com>
2022-03-14 06:36:06 +0000

    fix

--- paddle/phi/kernels/cpu/kron_grad_kernel.cc
+++ paddle/phi/kernels/cpu/kron_grad_kernel.cc
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 #include "paddle/phi/kernels/kron_grad_kernel.h"
-#include "paddle/phi/kernels/impl/kron_grad_kernel_impl.h"
 
 #include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/impl/kron_grad_kernel_impl.h"
 
 PD_REGISTER_KERNEL(kron_grad,
                    CPU,
