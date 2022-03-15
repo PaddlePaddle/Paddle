3d105ad1754eafb7167fe374b9ccdc4713c3eee6
Chen Weihang <chenweihang@baidu.com>
2022-03-15 02:15:33 +0000

    fix xpu compile failed

--- paddle/fluid/operators/gather_op_xpu.cc
+++ paddle/fluid/operators/gather_op_xpu.cc
@@ -23,6 +23,8 @@ limitations under the License. */
 namespace paddle {
 namespace operators {
 
+using Tensor = framework::Tensor;
+
 template <typename T>
 class GatherOpXPUKernel : public framework::OpKernel<T> {
   using XPUType = typename XPUTypeTrait<T>::Type;
