2b925d442b0a4079f21cfd5df727df94aa22aaf4
SigureMo <sigure.qaq@gmail.com>
2022-03-15 16:27:48 +0800

    add parameter `weight_decay`

--- python/paddle/optimizer/sgd.py
+++ python/paddle/optimizer/sgd.py
@@ -65,7 +65,7 @@ class SGD(Optimizer):
             inp = paddle.to_tensor(inp)
             out = linear(inp)
             loss = paddle.mean(out)
-            sgd = paddle.optimizer.SGD(learning_rate=0.1, parameters=linear.parameters())
+            sgd = paddle.optimizer.SGD(learning_rate=0.1, parameters=linear.parameters(), weight_decay=0.01)
             out.backward()
             sgd.step()
             sgd.clear_grad()
