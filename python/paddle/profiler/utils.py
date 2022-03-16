a7d8b0357584f244913dc0908a1890f653c8010a
chenjian <chenjian26@baidu.com>
2022-03-16 13:03:09 +0000

    fix

--- python/paddle/profiler/utils.py
+++ python/paddle/profiler/utils.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,7 +33,6 @@ class RecordEvent(ContextDecorator):
 
     Parameters:
     name(str): Name of the record event
-    event_type(TracerEventType): Type of the record event, can be used for statistics.
 
     Examples:
     .. code-block:: python
