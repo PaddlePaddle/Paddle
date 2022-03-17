0dbe2f70383c8848d88d2c02ad205fa48619d734
ForFishes <2282912238@qq.com>
2022-03-17 11:11:23 +0000

    fix bug

--- paddle/fluid/pybind/eager_method.cc
+++ paddle/fluid/pybind/eager_method.cc
@@ -277,7 +277,7 @@ static PyObject* tensor_method_copy_(TensorObject* self, PyObject* args,
             egr::EagerUtils::autograd_meta(&(src_tensor))->Persistable());
   }
 
-  self->tensor.copy_(src_tensor, blocking, self->tensor.inner_place());
+  self->tensor.copy_(src_tensor, self->tensor.inner_place(), blocking);
 
   VLOG(6) << "Finish Copy Tensor " << src_tensor.name() << " to "
           << self->tensor.name();
