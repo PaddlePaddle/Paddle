89e36a0bc8e9094fb8734f441104d7c11c524d8b
BrilliantYuKaimin <91609464+BrilliantYuKaimin@users.noreply.github.com>
2022-03-20 12:40:42 +0800

    Update test_pixel_unshuffle.py
    增加对extra_repr的测试

--- python/paddle/fluid/tests/unittests/test_pixel_unshuffle.py
+++ python/paddle/fluid/tests/unittests/test_pixel_unshuffle.py
@@ -209,6 +209,11 @@ class TestPixelUnshuffleAPI(unittest.TestCase):
                 paddle.to_tensor(x), 3, data_format)
             self.assertTrue(np.allclose(result_functional.numpy(), npresult))
 
+            pixel_unshuffle_str = 'downscale_factor={}, data_format={}'.format(
+                pixel_unshuffle._downscale_factor,
+                pixel_unshuffle._data_format)
+            self.assertTrue(pixel_unshuffle.extra_repr(), pixel_unshuffle_str)
+
     def test_dygraph1(self):
         '''test_dygraph1'''
 
