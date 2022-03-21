d2fbba3ef1398b08c6d2a24b28f367a97e962a3b
From00 <chenruibiao@baidu.com>
2022-03-21 10:58:26 +0800

    Move conv-transpose OPs to phi (#40675)
    * Move conv-transpose OPs to phi
    
    * Fix CI errors
    
    * Fix CI errors

--- paddle/phi/infermeta/binary.h
+++ paddle/phi/infermeta/binary.h
@@ -83,6 +83,19 @@ void ConvInferMeta(const MetaTensor& input,
                    MetaTensor* out,
                    MetaConfig config = MetaConfig());
 
+void ConvTransposeInferMeta(const MetaTensor& x,
+                            const MetaTensor& filter,
+                            const std::vector<int>& strides,
+                            const std::vector<int>& paddings,
+                            const std::vector<int>& output_padding,
+                            const std::vector<int>& output_size,
+                            const std::string& padding_algorithm,
+                            int groups,
+                            const std::vector<int>& dilations,
+                            const std::string& data_format,
+                            MetaTensor* out,
+                            MetaConfig config = MetaConfig());
+
 void CrossInferMeta(const MetaTensor& x,
                     const MetaTensor& y,
                     int axis,
