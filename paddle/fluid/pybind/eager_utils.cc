24a853e961c11c78246f2aac2ac4618b527323d3
phlrain <phliuhongyu@126.com>
2022-03-21 06:06:33 +0000

    add some line; test=document_fix

--- paddle/fluid/pybind/eager_utils.cc
+++ paddle/fluid/pybind/eager_utils.cc
@@ -31,6 +31,7 @@ limitations under the License. */
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/core/compat/convert_utils.h"
 #include "paddle/phi/core/dense_tensor.h"
+
 namespace paddle {
 namespace pybind {
 
