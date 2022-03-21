f78644ce4ce127d1d6149bb26eae66be97d3a4e2
kuizhiqing <kuizhiqing@baidu.com>
2022-03-21 12:48:42 +0000

    enable continuous log; update doc

--- python/paddle/distributed/__init__.py
+++ python/paddle/distributed/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from .spawn import spawn  # noqa: F401
-from .fleet.launch import launch  # noqa: F401
+from .launch.main import launch  # noqa: F401
 
 from .parallel import init_parallel_env  # noqa: F401
 from .parallel import get_rank  # noqa: F401
