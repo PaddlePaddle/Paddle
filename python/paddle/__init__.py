819d9ab20cd965741bc30740ec50ceba8fc53983
BrilliantYuKaimin <91609464+BrilliantYuKaimin@users.noreply.github.com>
2022-03-21 19:40:37 +0800

    在python中增加logspace API

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -85,6 +85,7 @@ from .tensor.creation import diag  # noqa: F401
 from .tensor.creation import diagflat  # noqa: F401
 from .tensor.creation import eye  # noqa: F401
 from .tensor.creation import linspace  # noqa: F401
+from .tensor.creation import logspace  # noqa: F401
 from .tensor.creation import ones  # noqa: F401
 from .tensor.creation import ones_like  # noqa: F401
 from .tensor.creation import zeros  # noqa: F401
@@ -582,6 +583,7 @@ __all__ = [  # noqa
            'sqrt',
            'randperm',
            'linspace',
+           'logspace',
            'reshape',
            'reshape_',
            'reverse',
