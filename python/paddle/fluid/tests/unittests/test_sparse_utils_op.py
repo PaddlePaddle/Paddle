1e751443858887c3526b51ea810b239ee009a3f3
zkh2016 <zhangkaihuo@baidu.com>
2022-03-22 06:19:05 +0000

    add a functions to create sparse tensor in eager

--- python/paddle/fluid/tests/unittests/test_sparse_utils_op.py
+++ python/paddle/fluid/tests/unittests/test_sparse_utils_op.py
@@ -17,10 +17,40 @@ import unittest
 import numpy as np
 import paddle
 from paddle import _C_ops
+from paddle.fluid import core
 from paddle.fluid.framework import _test_eager_guard
 
 
 class TestSparseUtils(unittest.TestCase):
+    def test_create_sparse_coo_tensor(self):
+        with _test_eager_guard():
+            non_zero_indices = [[0, 0, 1, 2, 2], [1, 3, 2, 0, 1]]
+            non_zero_elements = [1, 2, 3, 4, 5]
+            dense_shape = [3, 4]
+            dense_indices = paddle.to_tensor(non_zero_indices)
+            dense_elements = paddle.to_tensor(
+                non_zero_elements, dtype='float32')
+            stop_gradient = False
+            coo = core.eager.sparse_coo_tensor(dense_indices, dense_elements,
+                                               dense_shape, stop_gradient)
+            print(coo)
+
+    def test_create_sparse_csr_tensor(self):
+        with _test_eager_guard():
+            non_zero_crows = [0, 2, 3, 5]
+            non_zero_cols = [1, 3, 2, 0, 1]
+            non_zero_elements = [1, 2, 3, 4, 5]
+            dense_shape = [3, 4]
+            dense_crows = paddle.to_tensor(non_zero_crows)
+            dense_cols = paddle.to_tensor(non_zero_cols)
+            dense_elements = paddle.to_tensor(
+                non_zero_elements, dtype='float32')
+            stop_gradient = False
+            csr = core.eager.sparse_csr_tensor(dense_crows, dense_cols,
+                                               dense_elements, dense_shape,
+                                               stop_gradient)
+            print(csr)
+
     def test_to_sparse_coo(self):
         with _test_eager_guard():
             x = [[0, 1, 0, 2], [0, 0, 3, 0], [4, 5, 0, 0]]
