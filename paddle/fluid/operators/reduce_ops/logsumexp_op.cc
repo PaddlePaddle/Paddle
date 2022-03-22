0e19fe9491008cb9198b3f0fff9b678535f5bf07
xingjing1 <2907374824@qq.com>
2022-03-22 07:07:57 +0000

    fix bugs

--- paddle/fluid/operators/reduce_ops/logsumexp_op.cc
+++ paddle/fluid/operators/reduce_ops/logsumexp_op.cc
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/operators/reduce_ops/logsumexp_op.h"
 #include <algorithm>
 #include <string>
 #include <vector>
