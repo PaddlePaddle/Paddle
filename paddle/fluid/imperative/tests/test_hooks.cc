3c99c8289d4bd543a63062d79ad27ddc7a79bbc4
Chen Weihang <chenweihang@baidu.com>
2022-03-23 06:21:00 +0000

    fix dygraph test failed

--- paddle/fluid/imperative/tests/test_hooks.cc
+++ paddle/fluid/imperative/tests/test_hooks.cc
@@ -28,6 +28,8 @@
 
 PD_DECLARE_KERNEL(add, CPU, ALL_LAYOUT);
 PD_DECLARE_KERNEL(add_grad, CPU, ALL_LAYOUT);
+PD_DECLARE_KERNEL(matmul_with_flatten, CPU, ALL_LAYOUT);
+PD_DECLARE_KERNEL(matmul_with_flatten_grad, CPU, ALL_LAYOUT);
 
 namespace platform = paddle::platform;
 namespace framework = paddle::framework;
