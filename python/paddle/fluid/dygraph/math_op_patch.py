8cb1b77439cfde6e1f2f3558011fbed9d83aee5e
linjieccc <623543001@qq.com>
2022-03-24 06:30:28 +0000

    fix outpu dtype of elementwise_div

--- python/paddle/fluid/dygraph/math_op_patch.py
+++ python/paddle/fluid/dygraph/math_op_patch.py
@@ -220,6 +220,8 @@ def monkey_patch_math_varbase():
                 pass
 
             # 2. create varbase for scalar
+            if op_type == 'elementwise_div' and self.dtype in _supported_int_dtype_:
+                self = astype(self, 'float32')
             lhs_dtype = self.dtype
             if _in_eager_mode():
                 other_var_should_be = core.eager.Tensor
