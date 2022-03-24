844e6363efb3603d5a37c68f7cef7993fb350d94
Aurelius84 <zhangliujie@baidu.com>
2022-03-24 06:12:52 +0000

    fix npu problem

--- paddle/fluid/operators/strided_slice_op_npu.cc
+++ paddle/fluid/operators/strided_slice_op_npu.cc
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#include "paddle/phi/kernels/funcs/strided_slice.h"
 #include "paddle/fluid/operators/slice_op.h"
 #include "paddle/fluid/platform/device/npu/npu_op_runner.h"
 
