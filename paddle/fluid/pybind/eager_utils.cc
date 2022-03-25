dabd104b96d7338a973e19fa5ace0bd8e1e7540a
0x45f <wangzhen45@baidu.com>
2022-03-25 09:30:32 +0000

    Refine test_lac.py for eager mode

--- paddle/fluid/pybind/eager_utils.cc
+++ paddle/fluid/pybind/eager_utils.cc
@@ -633,6 +633,8 @@ static paddle::experimental::Tensor& GetTensorFromPyObject(
     ssize_t arg_idx, bool dispensable) {
   if (PyTuple_Check(obj)) {
     obj = PyTuple_GET_ITEM(obj, 0);
+  } else if (PyList_Check(obj)) {
+    obj = PyList_GET_ITEM(obj, 0);
   }
 
   if (obj == nullptr || obj == Py_None) {
