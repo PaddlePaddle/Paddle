89b58aae0beb4f0adb05448f6661bbbc78b34949
Chen Weihang <chenweihang@baidu.com>
2022-03-25 02:14:48 +0000

    share layout for mkldnn

--- paddle/phi/infermeta/unary.cc
+++ paddle/phi/infermeta/unary.cc
@@ -825,6 +825,7 @@ void MaxPoolWithIndexInferMeta(const MetaTensor& x,
 void MeanAllInferMeta(const MetaTensor& x, MetaTensor* out) {
   out->set_dims(phi::make_ddim({1}));
   out->set_dtype(x.dtype());
+  out->set_layout(x.layout());
 }
 
 void ModeInferMeta(const MetaTensor& x,
