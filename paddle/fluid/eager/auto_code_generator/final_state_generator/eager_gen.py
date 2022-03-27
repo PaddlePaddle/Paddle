855c2b82ce6379515f3936676dc2f7b71c61a8c8
pangyoki <pangyoki@126.com>
2022-03-27 08:39:48 +0800

    fix inplace bug in final_state eager_gen (#40979)
    * fix inplace bug in final_state eager_gen
    
    * fix python_c_gen

--- paddle/fluid/eager/auto_code_generator/final_state_generator/eager_gen.py
+++ paddle/fluid/eager/auto_code_generator/final_state_generator/eager_gen.py
@@ -807,7 +807,8 @@ class DygraphSingleFunctionGenerator(FunctionGeneratorBase):
                         f"auto NEW_{name} = ({name}.get_ptr() != nullptr) ? paddle::make_optional<const paddle::experimental::Tensor&>(egr::EagerAmpAutoCast(\"{name}\", *({name}.get_ptr()), amp_dst_dtype, op_name)) : {name};\n"
                     )
                 else:
-                    if inplace_map and name in inplace_map.keys():
+                    if is_inplaced and inplace_map and name in inplace_map.keys(
+                    ):
                         arg_str = f"paddle::experimental::Tensor& {name}"
                         amp_tensors_vector_list.append(f"{{{name}}}")
                         amp_autocast_list.append(
@@ -881,7 +882,7 @@ class DygraphSingleFunctionGenerator(FunctionGeneratorBase):
             returns_str = ", ".join(returns_list)
             returns_str = f"std::make_tuple({returns_str})"
 
-        self.GenerateNodeCreationCodes(forward_call_str)
+        self.GenerateNodeCreationCodes(forward_call_str, is_inplaced)
 
         node_creation_str = self.node_creation_str
         dygraph_event_str = f"paddle::platform::RecordEvent dygraph_entrance_record_event(\"{forward_api_name} dygraph\", paddle::platform::TracerEventType::Operator, 1);"
@@ -917,7 +918,7 @@ class DygraphSingleFunctionGenerator(FunctionGeneratorBase):
         logging.info(
             f"Generated Forward Declaration: {self.forward_declaration_str}")
 
-    def GenerateNodeCreationCodes(self, forward_call_str):
+    def GenerateNodeCreationCodes(self, forward_call_str, is_inplaced):
         forward_api_name = self.forward_api_name
         forward_inputs_position_map = self.forward_inputs_position_map
         forward_outputs_position_map = self.forward_outputs_position_map
@@ -980,12 +981,13 @@ class DygraphSingleFunctionGenerator(FunctionGeneratorBase):
         # Check Inplace
         check_inplace_str = ""
         bump_inplace_version_str = ""
-        for inplace_name in inplace_map.keys():
-            inplace_autograd_meta_name = GetAutoGradMetaName(inplace_name)
-            check_inplace_str += CHECK_INPLACE_TEMPLATE.format(
-                inplace_name, inplace_autograd_meta_name)
-            bump_inplace_version_str += BUMP_INPLACE_VERSION_TEMPLATE.format(
-                inplace_name, inplace_name)
+        if is_inplaced:
+            for inplace_name in inplace_map.keys():
+                inplace_autograd_meta_name = GetAutoGradMetaName(inplace_name)
+                check_inplace_str += CHECK_INPLACE_TEMPLATE.format(
+                    inplace_name, inplace_autograd_meta_name)
+                bump_inplace_version_str += BUMP_INPLACE_VERSION_TEMPLATE.format(
+                    inplace_name, inplace_name)
 
         # Node Construction        
         num_backward_inputs = len(forward_outputs_position_map.keys())
