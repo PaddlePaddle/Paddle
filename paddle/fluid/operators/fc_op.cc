8b074b3f1381ed34c35abbe490ffaf0f184fab1d
Sławomir Siwek <slawomir.siwek@intel.com>
2022-03-28 11:22:53 -0700

    Remove input from base op

--- paddle/fluid/operators/fc_op.cc
+++ paddle/fluid/operators/fc_op.cc
@@ -137,10 +137,6 @@ class FCOpMaker : public framework::OpProtoAndCheckerMaker {
     AddInput("W", "(Tensor), The weight fc op with shape (I, O).");
     AddInput("Bias", "(Tensor, optional) Bias vector with shape (1 x O")
         .AsDispensable();
-    AddInput("ResidualData",
-             "(Tensor) Residual data which will be added to fc output")
-        .AsDispensable()
-        .AsExtra();
     AddOutput("Out",
               "(Tensor) The output tensor of fully connected operator. ");
     AddAttr<int>("in_num_col_dims",
