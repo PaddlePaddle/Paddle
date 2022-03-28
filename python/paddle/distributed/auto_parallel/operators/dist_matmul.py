f5fe29bf3c2d999e0ef87ffefa649f2a7c3f5e87
JZ-LIANG <jianzhongliang10@gmail.com>
2022-03-28 10:36:25 +0800

    reverse dist matmul

--- python/paddle/distributed/auto_parallel/operators/dist_matmul.py
+++ python/paddle/distributed/auto_parallel/operators/dist_matmul.py
@@ -1506,7 +1506,6 @@ class DistributedMulImpl0(DistributedOperatorImpl):
         y_name = op_desc.input('Y')[0]
         x_dims_mapping = op_dist_attr.get_input_dims_mapping(x_name)
         y_dims_mapping = op_dist_attr.get_input_dims_mapping(y_name)
-
         if is_dim_shard(x_dims_mapping[-1]):
             return False
         if is_dim_shard(y_dims_mapping[-2]) or is_dim_replicate(y_dims_mapping[
