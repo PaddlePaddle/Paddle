a14efc13df96880846a75ea93180fae4368ef74f
zmxdream <zhangminxu01@baidu.com>
2022-03-30 21:11:04 +0800

    add optimizer kernel. test=develop

--- paddle/fluid/framework/fleet/heter_ps/optimizer.cuh.h
+++ paddle/fluid/framework/fleet/heter_ps/optimizer.cuh.h
@@ -134,65 +134,96 @@ class Optimizer {
   }
 #elif defined(PADDLE_WITH_XPU_KP)
 
-  __device__ void update_lr(float& w, float& g2sum, float g,  // NOLINT
+  __device__ void update_lr(float* w, float* g2sum, float g,  // NOLINT
                             float scale) {
+    __local__ local_learning_rate;
+    __local__ local_initial_g2sum;
+    __local__ local_min_bound;
+    __local__ local_max_bound;
+
+    GM2LM(optimizer_config::learning_rate, &local_learning_rate, sizeof(float));
+    GM2LM(optimizer_config::initial_g2sum, &local_initial_g2sum, sizeof(float));
+    GM2LM(optimizer_config::min_bound, &local_min_bound, sizeof(float));
+    GM2LM(optimizr_config::max_bound, &local_max_bound, sizeof(float));
+
     double add_g2sum = 0;
-    double ratio = optimizer_config::learning_rate *
-                   sqrt(optimizer_config::initial_g2sum /
-                        (optimizer_config::initial_g2sum + g2sum));
+    double ratio = local_learning_rate *
+                   sqrt(local_initial_g2sum / (local_initial_g2sum + g2sum));
     double scaled_grad = g / scale;
 
-    w += scaled_grad * ratio;
+    (*w) += scaled_grad * ratio;
 
-    if (w < optimizer_config::min_bound) w = optimizer_config::min_bound;
-    if (w > optimizer_config::max_bound) w = optimizer_config::max_bound;
+    if (w < local_min_bound) w = local_min_bound;
+    if (w > local_max_bound) w = local_max_bound;
 
     add_g2sum += scaled_grad * scaled_grad;
 
-    g2sum += add_g2sum;
+    (*g2sum) += add_g2sum;
   }
 
-  __device__ void update_mf(int n, float* w, float& g2sum,  // NOLINT
-                            const float* g, float scale) {
+  __device__ void update_mf(int n, float* w, float* g2sum, const float* g,
+                            float scale) {
+    __local__ local_mf_learning_rate;
+    __local__ local_mf_initial_g2sum;
+    __local__ local_mf_min_bound;
+    __local__ local_mf_max_bound;
+
+    GM2LM(optimizer_config::mf_learning_rate, &local_mf_learning_rate,
+          sizeof(float));
+    GM2LM(optimizer_config::mf_initial_g2sum, &local_mf_initial_g2sum,
+          sizeof(float));
+    GM2LM(optimizer_config::mf_min_bound, &local_mf_min_bound, sizeof(float));
+    GM2LM(optimizer_config::mf_max_bound, &local_mf_max_bound, sizeof(float));
+
     double add_g2sum = 0;
-    double ratio = optimizer_config::mf_learning_rate *
-                   sqrt(optimizer_config::mf_initial_g2sum /
-                        (optimizer_config::mf_initial_g2sum + g2sum));
+    double ratio =
+        local_mf_learning_rate *
+        sqrt(local_mf_initial_g2sum / (local_mf_initial_g2sum + g2sum));
     for (int i = 0; i < n; ++i) {
       double scaled_grad = g[i] / scale;
-
       w[i] += scaled_grad * ratio;
 
-      if (w[i] < optimizer_config::mf_min_bound)
-        w[i] = optimizer_config::mf_min_bound;
-      if (w[i] > optimizer_config::mf_max_bound)
-        w[i] = optimizer_config::mf_max_bound;
+      if (w[i] < local_mf_min_bound) w[i] = local_mf_min_bound;
+      if (w[i] > local_mf_max_bound) w[i] = local_mf_max_bound;
       add_g2sum += scaled_grad * scaled_grad;
     }
 
-    g2sum += add_g2sum / n;
+    (*g2sum) += add_g2sum / n;
   }
 
-  __device__ void update_value(ValType& val, const GradType& grad) {  // NOLINT
-    val.slot = grad.slot;
-    val.show += grad.show;
-    val.clk += grad.clk;
+  __device__ void xpu_rand_uniform(float* ret_val) { *ret_val = 0.1; }
 
-    val.delta_score += optimizer_config::nonclk_coeff * (grad.show - grad.clk) +
-                       optimizer_config::clk_coeff * grad.clk;
+  __device__ void update_value(ValType* val, const GradType* grad) {  // NOLINT
+    (*val).slot = (*grad).slot;
+    (*val).show += (*grad).show;
+    (*val).clk += (*grad).clk;
 
-    update_lr(val.lr, val.lr_g2sum, grad.lr_g, grad.show);
+    __local__ float local_nonclk_coeff;
+    __local__ float local_clk_coeff;
+
+    __local__ float local_mf_create_thresholds;
+    __local__ float local_mf_initial_range;
+
+    GM2LM(optimizer_config::nonclk_coeff, &local_nonclk_coeff, sizeof(float));
+    GM2LM(optimizer_config::clk_coeff, &local_clk_coeff, sizeof(float));
+    GM2LM(optimizer_config::mf_create_thresholds, &local_mf_create_thresholds,
+          sizeof(float));
+
+    val.delta_score += local_nonclk_coeff * ((*grad).show - (*grad).clk) +
+                       local_clk_coeff * (*grad).clk;
+
+    update_lr(&(*val).lr, &(*val).lr_g2sum, (*grad).lr_g, (*grad).show);
 
     if (val.mf_size == 0) {
-      if (optimizer_config::mf_create_thresholds <=
-          optimizer_config::nonclk_coeff * (val.show - val.clk) +
-              optimizer_config::clk_coeff * val.clk) {
+      if (local_mf_create_thresholds <=
+          local_nonclk_coeff * ((*val).show - (*val).clk) +
+              local_clk_coeff * (*val).clk) {
         val.mf_size = MF_DIM + 1;
         val.mf[0] = 0;
 
-        for (int i = 0; i < MF_DIM; ++i) {
-          val.mf[i + 1] =
-              (xpurand_uniform(&state)) * optimizer_config::mf_initial_range;
+        float ret_val;
+        xpu_rand_uniform(&ret_val) for (int i = 0; i < MF_DIM; ++i) {
+          (*val).mf[i + 1] = (ret_val)*local_mf_initial_range;
         }
 
         // int tid_x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -204,7 +235,7 @@ class Optimizer {
         // }
       }
     } else {
-      update_mf(MF_DIM, &val.mf[1], val.mf[0], grad.mf_g, grad.show);
+      update_mf(MF_DIM, &val.mf[1], &val.mf[0], (*grad).mf_g, (*grad).show);
     }
   }
 
