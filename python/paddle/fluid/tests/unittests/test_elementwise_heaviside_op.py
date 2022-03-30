5397eb844d3a82eb7edae4cbc185bf9427de87f4
BrilliantYuKaimin <91609464+BrilliantYuKaimin@users.noreply.github.com>
2022-03-30 15:25:20 +0800

    Update test_elementwise_heaviside_op.py

--- python/paddle/fluid/tests/unittests/test_elementwise_heaviside_op.py
+++ python/paddle/fluid/tests/unittests/test_elementwise_heaviside_op.py
@@ -12,14 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import print_function
-
 import unittest
 import numpy as np
 from op_test import OpTest
 import paddle
-
-paddle.enable_static()
+import paddle.fluid.core as core
+import paddle.fluid as fluid
 
 
 class TestElementwiseOp(OpTest):
@@ -31,82 +29,106 @@ class TestElementwiseOp(OpTest):
         self.outputs = {'Out': np.heaviside(self.inputs['X'], self.inputs['Y'])}
 
     def test_check_output(self):
+        paddle.enable_static()
         self.check_output()
+        paddle.disable_static()
 
     def test_check_grad_normal(self):
+        paddle.enable_static()
         self.check_grad(['X', 'Y'], 'Out')
-
-
-class TestElementwiseHeavisideOp_Vector(TestElementwiseOp):
-    def setUp(self):
-        self.op_type = "elementwise_heaviside"
-        x = np.random.random((100, )).astype("float64")
-        y = np.random.random((100, )).astype("float64")
-        self.inputs = {'X': x, 'Y': y}
-        self.outputs = {'Out': np.heaviside(self.inputs['X'], self.inputs['Y'])}
-
-
-class TestElementwiseHeavisideOp_broadcast_0(TestElementwiseOp):
-    def setUp(self):
-        self.op_type = "elementwise_heaviside"
-        x = np.random.random((100, 5, 2)).astype(np.float64)
-        y = np.random.random((100, 1, 1)).astype(np.float64)
-        self.inputs = {'X': x, 'Y': y}
-
-        self.attrs = {'axis': 0}
-        self.outputs = {'Out': np.heaviside(self.inputs['X'], self.inputs['Y'])}
-
+        paddle.disable_static()
 
 class TestElementwiseHeavisideOp_broadcast_1(TestElementwiseOp):
     def setUp(self):
         self.op_type = "elementwise_heaviside"
-        x = np.random.random((2, 100, 3)).astype(np.float64)
-        y = np.random.random((100, )).astype(np.float64)
+        x = np.random.random((100, 5)).astype(np.float64)
+        y = np.random.random((100, 1)).astype(np.float64)
         self.inputs = {'X': x, 'Y': y}
 
-        self.attrs = {'axis': 1}
-        self.outputs = {
-            'Out': np.heaviside(self.inputs['X'],
-                                self.inputs['Y'].reshape(1, 100, 1))
-        }
+        self.outputs = {'Out': np.heaviside(self.inputs['X'], self.inputs['Y'])}
 
 
 class TestElementwiseHeavisideOp_broadcast_2(TestElementwiseOp):
     def setUp(self):
         self.op_type = "elementwise_heaviside"
-        x = np.random.random((1, 3, 100)).astype(np.float64)
-        y = np.random.random((100, )).astype(np.float64)
+        x = np.random.random((5, 100)).astype(np.float64)
+        y = np.random.random((1, 100)).astype(np.float64)
         self.inputs = {'X': x, 'Y': y}
 
-        self.outputs = {
-            'Out':
-            np.heaviside(self.inputs['X'], self.inputs['Y'].reshape(1, 1, 100))
-        }
+        self.outputs = {'Out':np.heaviside(self.inputs['X'], self.inputs['Y'])}
 
 
-class TestElementwiseHeavisideOp_broadcast_3(TestElementwiseOp):
+class TestHeavisideAPI_float64(unittest.TestCase):
     def setUp(self):
-        self.op_type = "elementwise_heaviside"
-        x = np.random.random((2, 50, 2, 1)).astype(np.float64)
-        y = np.random.random((50, 2)).astype(np.float64)
-        self.inputs = {'X': x, 'Y': y}
+        self.x_np = np.random.random((13, 17)).astype("float64")
+        self.y_np = np.random.random((13, 17)).astype("float64")
+        self.out_np = np.heaviside(self.x_np, self.y_np)
+        self.dtype = "float64"
 
-        self.attrs = {'axis': 1}
-        self.outputs = {
-            'Out': np.heaviside(self.inputs['X'],
-                                self.inputs['Y'].reshape(1, 50, 2, 1))
-        }
+    def test_static(self):
+        for use_cuda in ([False, True]
+                         if core.is_compiled_with_cuda() else [False]):
+            place = paddle.CUDAPlace(0) if use_cuda else paddle.CPUPlace()
+
+            paddle.enable_static()
+            x = paddle.fluid.data(
+                name=f"x_{self.dtype}", shape=[13, 17], dtype=self.dtype)
+            y = paddle.fluid.data(
+                name=f"y_{self.dtype}", shape=[13, 17], dtype=self.dtype)
+            out = paddle.heaviside(x, y)
+
+            exe = paddle.static.Executor(place=place)
+            res = exe.run(fluid.default_main_program(),
+                            feed={f"x_{self.dtype}": self.x_np, f"y_{self.dtype}": self.y_np},
+                            fetch_list=out,
+                            use_prune=True)
+
+            self.assertTrue(np.allclose(res, self.out_np))
+
+    def test_dygraph(self):
+        for use_cuda in ([False, True]
+                         if core.is_compiled_with_cuda() else [False]):
+            place = paddle.CUDAPlace(0) if use_cuda else paddle.CPUPlace()
+            paddle.disable_static(place=place)
+            result = paddle.heaviside(paddle.to_tensor(self.x_np), paddle.to_tensor(self.y_np))
+
+            self.assertTrue(np.allclose(result.numpy(), self.out_np))
 
 
-class TestElementwiseHeavisideOp_broadcast_4(TestElementwiseOp):
+class TestHeavisideAPI_float32(TestHeavisideAPI_float64):
     def setUp(self):
-        self.op_type = "elementwise_heaviside"
-        x = np.random.random((2, 3, 4, 50)).astype(np.float64)
-        y = np.random.random((2, 3, 1, 50)).astype(np.float64)
-        self.inputs = {'X': x, 'Y': y}
+        self.x_np = np.random.random((13, 17)).astype("float32")
+        self.y_np = np.random.random((13, 17)).astype("float32")
+        self.out_np = np.heaviside(self.x_np, self.y_np)
+        self.dtype = "float32"
 
-        self.outputs = {'Out': np.heaviside(self.inputs['X'], self.inputs['Y'])}
 
+class TestHeavisideAPI_int64(TestHeavisideAPI_float64):
+    def setUp(self):
+        self.x_np = np.random.random((13, 17)).astype("int64")
+        self.y_np = np.random.random((13, 17)).astype("int64")
+        self.out_np = np.heaviside(self.x_np, self.y_np)
+        self.dtype = "int64"
+
+
+class TestHeavisideAPI_int32(TestHeavisideAPI_float64):
+    def setUp(self):
+        self.x_np = np.random.random((13, 17)).astype("int32")
+        self.y_np = np.random.random((13, 17)).astype("int32")
+        self.out_np = np.heaviside(self.x_np, self.y_np)
+        self.dtype = "int32"
+
+class TestHeavisideError(unittest.TestCase):
+    def test_input(self):
+        def test_input_x():
+            with paddle.fluid.dygraph.guard():
+                paddle.heaviside(1, paddle.randn([100]))
+        self.assertRaises(ValueError, test_input_x)
+
+        def test_input_y():
+            with paddle.fluid.dygraph.guard():
+                paddle.heaviside(paddle.randn([100]), 1)
+        self.assertRaises(ValueError, test_input_y)
 
 if __name__ == '__main__':
     unittest.main()
