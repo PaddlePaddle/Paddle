1822fefde6f174520446d5e019e6aa2dc399e911
Tongxin Bai <waffle.bai@gmail.com>
2022-03-31 12:45:47 +0000

    Add python primitive wrappers.

--- python/paddle/autograd/primops.py
+++ python/paddle/autograd/primops.py
@@ -52,8 +52,6 @@ def _manipulation_unop(helper):
 
     if out is None:
         out = helper.create_variable_for_type_inference(dtype=x.dtype)
-    
-    attrs = {}
 
     helper.append_op(
         type=optype,
@@ -65,50 +63,136 @@ def _manipulation_unop(helper):
 
 # Each primitive op is given a Python constructor for sake of convenience.
 def add(x, y, out=None):
-    return _simple_binop(LayerHelper('prim_add', **locals()))    
+    return _simple_binop(LayerHelper('add_p', **locals()))    
 
 
 def sub(x, y, out=None):
-    return _simple_binop(LayerHelper('prim_sub', **locals()))
+    return _simple_binop(LayerHelper('sub_p', **locals()))
 
 
 def mul(x, y, out=None):
-    return _simple_binop(LayerHelper('prim_mul', **locals()))
+    return _simple_binop(LayerHelper('mul_p', **locals()))
 
 
 def div(x, y, out=None):
-    return _simple_binop(LayerHelper('prim_div', **locals()))
+    return _simple_binop(LayerHelper('div_p', **locals()))
 
 
 def sqrt(x, out=None):
-    return _simple_unop(LayerHelper('prim_sqrt', **locals()))
+    return _simple_unop(LayerHelper('sqrt_p', **locals()))
 
 
 def tanh(x, out=None):
-    return _simple_unop(LayerHelper('prim_tanh', **locals()))
+    return _simple_unop(LayerHelper('tanh_p', **locals()))
 
 
 def reshape(x, shape, out=None):
-    return _manipulation_unop(LayerHelper('prim_reshape', **locals()))
+    return _manipulation_unop(LayerHelper('reshape_p', **locals()))
 
 
 def broadcast(x, shape, out=None):
-    return _manipulation_unop(LayerHelper('prim_broadcast', **locals()))
+    return _manipulation_unop(LayerHelper('broadcast_p', **locals()))
 
 
 def transpose(x, axis=None, out=None):
-    return _manipulation_unop(LayerHelper('prim_transpose', **locals()))
+    return _manipulation_unop(LayerHelper('transpose_p', **locals()))
 
 
 def split(x, num_or_sections, axis=0, out=None):
+    assert isinstance(num_or_sections, (int, list, tuple))
     
+    if isinstance(num_or_sections, int):
+        n = num_or_sections
+        attrs = {'axis': axis, 'num': n}
+    else:
+        n = len(num_or_sections)
+        attrs = {'axis': axis, 'sections': list(num_or_sections)}
+    helper = LayerHelper('split_p', **locals())
+    outs = [
+        helper.create_variable_for_type_inference(dtype=x.dtype)
+        for i in range(n)
+    ]
+    helper.append_op(
+        type=helper.layer_type,
+        inputs={'X': x},
+        outputs={'Out': outs},
+        attrs=attrs)
+    return outs
 
 
-def concat(xs, out=None):
+def concat(xs, axis=0, out=None):
+    assert isinstance(xs, (list, tuple)) and len(xs) > 0
+    attrs = {'axis': axis}
+    helper = LayerHelper('split_p', **locals())
+    if out is None:
+        out = helper.create_variable_for_type_inference(dtype=xs[0].dtype)
+    helper.append_op(
+        type=helper.layer_type,
+        inputs={'X': xs},
+        outputs={'Out': out},
+        attrs=attrs)
+    return out
 
 
+def reduce(x, dim, keepdim, out=None):
+    assert isinstance(dim, (int, tuple, list))
+    assert isinstance(keepdim, bool)
 
-def reduce(x, axis, out=None):
+    attrs = {'dim': dim, 'keepdim': keepdim}
+
+    helper = LayerHelper('reduce_p', **locals())
+    if out is None:
+        out = helper.create_variable_for_type_inference(dtype=x.dtype)
+
+    helper.append_op(
+        type=helper.layer_type,
+        inputs={'X': x},
+        outputs={'Out': out},
+        attrs=attrs)
+    return out
+
+def matmul(x, y, out=None):
+    return _simple_binop(LayerHelper('matmul_p', **locals()))
 
 
+def slice_select(x, axis, starts, ends, strides):
+    assert isinstance(axis, (list, tuple)), (
+        f'Argument type error. `axis` is supposed to be int, list or'
+        f' tuple but found {type(axis)}.')
+    assert isinstance(starts, (list, tuple))
+    assert isinstance(ends, (list, tuple))
+    assert len(axis) == len(starts) == len(ends) == len(strides)
+
+    attrs = {'axis': axis, 'starts': starts, 'ends': ends, 'strides': strides}
+    helper = LayerHelper('slice_select_p', **locals())
+    if out is None:
+        out = helper.create_variable_for_type_inference(dtype=x.dtype)
+    helper.append_op(
+        type=helper.layer_type,
+        inputs={'X': x},
+        outputs={'Out': out},
+        attrs=attrs)
+    return out
+
+
+def slice_assign(x, y, axis, starts, ends, strides):
+    assert len(y.shape) == len(starts) == len(ends) == len(strides)
+    assert len(y.shape) <= len(x.shape)
+    
+    attrs = {'axis': axis, 'starts': starts, 'ends': ends, 'strides': strides}
+    helper = LayerHelper('slice_assign_p', **locals())
+    if out is None:
+        out = helper.create_variable_for_type_inference(dtype=x.dtype)
+    helper.append_op(
+        type=helper.layer_type,
+        inputs={'X': x, 'Y': y},
+        outputs={'Out': out},
+        attrs=attrs)
+    return out
+
+
+if __name__ == '__main__':
+    paddle.enable_static()
+    A = paddle.rand([2, 3])
+    B = paddle.rand([3, 2])
 
