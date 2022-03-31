999b5020a41bbcfcb09633f7b7d6c903a355b4a5
tianshuo78520a <707759223@qq.com>
2022-03-31 11:44:17 +0800

    test=document_fix

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -2778,7 +2778,8 @@ function exec_samplecode_test() {
     if [ "$1" = "cpu" ] ; then
         python sampcd_processor.py cpu; example_error=$?
     elif [ "$1" = "gpu" ] ; then
-        python sampcd_processor.py --threads=16 gpu; example_error=$?
+        samplecode_gpu=${samplecode_gpu:-2}
+        python sampcd_processor.py --threads=${samplecode_gpu} gpu; example_error=$?
     fi
     if [ "$example_error" != "0" ];then
       echo "Code instance execution failed" >&2
