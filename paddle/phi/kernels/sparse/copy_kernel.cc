f1a3dbec1186d863a15eaf36fd7de516b4f558d9
zkh2016 <zhangkaihuo@baidu.com>
2022-03-31 06:52:24 +0000

    rename sparse copy filename

--- paddle/phi/kernels/sparse/copy_kernel.cc
+++ paddle/phi/kernels/sparse/copy_kernel.cc
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/phi/kernels/sparse/sparse_copy_kernel.h"
+#include "paddle/phi/kernels/sparse/copy_kernel.h"
 
 #include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
