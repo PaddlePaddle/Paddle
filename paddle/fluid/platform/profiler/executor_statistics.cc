dcacc731ef9be287ca53848ac8a6af96d9784c07
liutiexing <liutiexing@gmail.com>
2022-03-31 10:56:45 +0000

    update

--- paddle/fluid/platform/profiler/executor_statistics.cc
+++ paddle/fluid/platform/profiler/executor_statistics.cc
@@ -134,6 +134,18 @@ void StatisticsHostEvents(const TraceEventCollector& collector) {
             << " threads:" << idx2threads[kv.second].size();
   }
 
+  uint64_t total_cost = 0;
+  uint64_t python_cost = 0;
+  uint64_t cplusplus_cost = 0;
+  uint64_t run_op_cost = 0;
+/*uint64_t infershape_cost = 0;
+uint64_t luanch_kernel_cost = 0;
+uint64_t gc_cost = 0;
+uint64_t nexts_cost = 0;
+uint64_t allocator_cost = 0;
+uint64_t add_task_cost = 0;
+uint64_t data_transform = 0;*/
+
 #define GET_EVENT_TATALTIME(event) idx2total_ns[name2idx[#event]]
 #define GET_EVENT_COUNT(event) idx2cnt[name2idx[#event]]
   std::vector<std::string> executor_type_names{
@@ -143,68 +155,99 @@ void StatisticsHostEvents(const TraceEventCollector& collector) {
     executor_type = 1;  // executor
   } else if (GET_EVENT_COUNT(ParallelExecutor::Run) > 0) {
     executor_type = 2;  // PE
-  } else if (GET_EVENT_COUNT(StandaloneExecutor : run) > 0) {
+  } else if (GET_EVENT_COUNT(StandaloneExecutor::run) > 0) {
     executor_type = 3;  // new executor
   }
+
   VLOG(1) << "========Executor analysis(" << executor_type_names[executor_type]
           << ")========";
-  VLOG(1) << "Step: " << GET_EVENT_TATALTIME(ProfileStep) << "ns "
-          << GET_EVENT_COUNT(ProfileStep) << "times";
-  uint64_t allocator_cost = 0;
-  if (executor_type <= 2) {
-    VLOG(1) << "thread model==========";
-    VLOG(1) << "threadpool AddTask: " << GET_EVENT_TATALTIME(WorkQueue::AddTask)
-            << "ns " << GET_EVENT_COUNT(WorkQueue::AddTask) << "times";
-    allocator_cost = GET_EVENT_TATALTIME(StreamSafeCUDAAllocator::Allocate) +
-                     GET_EVENT_TATALTIME(StreamSafeCUDAAllocator::Free);
-    VLOG(1) << "Allocator: " << allocator_cost << "ns "
-            << GET_EVENT_COUNT(StreamSafeCUDAAllocator::Allocate) +
-                   GET_EVENT_COUNT(StreamSafeCUDAAllocator::Free)
-            << "times";
-    uint64_t autogrowth_allocator_cost =
-        GET_EVENT_TATALTIME(AutoGrowthBestFitAllocator::Allocate) +
-        GET_EVENT_TATALTIME(AutoGrowthBestFitAllocator::Free);
-    VLOG(1) << "AutoGrowth Allocator: " << autogrowth_allocator_cost << "ns "
-            << GET_EVENT_COUNT(AutoGrowthBestFitAllocator::Allocate) +
-                   GET_EVENT_COUNT(AutoGrowthBestFitAllocator::Free)
-            << "times";
-    uint64_t kernel_luanch = CalcOverlapTotal(collector,
-                                              [](const HostTraceEvent& evt) {
-                                                return evt.name == "compute";
-                                              }) -
-                             allocator_cost;
-    VLOG(1) << "kernel luanch: " << kernel_luanch << "ns ";
-  } else {  // old executor
-    VLOG(1) << "static kernel=========";
-    VLOG(1) << "data transform: " << GET_EVENT_TATALTIME(prepare_data) << "ns "
-            << GET_EVENT_COUNT(prepare_data) << "times";
-    VLOG(1) << "thread model==========";
-    VLOG(1) << "threadpool AddTask: " << GET_EVENT_TATALTIME(WorkQueue::AddTask)
-            << "ns " << GET_EVENT_COUNT(WorkQueue::AddTask) << "times";
-    allocator_cost = GET_EVENT_TATALTIME(AutoGrowthBestFitAllocator::Allocate) +
-                     GET_EVENT_TATALTIME(AutoGrowthBestFitAllocator::Free);
-    VLOG(1) << "Allocator: " << allocator_cost << "ns "
-            << GET_EVENT_COUNT(AutoGrowthBestFitAllocator::Allocate) +
-                   GET_EVENT_COUNT(AutoGrowthBestFitAllocator::Free)
-            << "times";
+  if (executor_type == 1) {
+    total_cost = GET_EVENT_TATALTIME(ProfileStep);
+    cplusplus_cost = GET_EVENT_TATALTIME(Executor::RunPartialPreparedContext);
+    python_cost = total_cost - cplusplus_cost;
+    run_op_cost = CalcOverlapTotal(collector, [](const HostTraceEvent& evt) {
+      return evt.type == TracerEventType::Operator;
+    });
+  } else if (executor_type == 2) {
+    total_cost = GET_EVENT_TATALTIME(ProfileStep);
+    cplusplus_cost = GET_EVENT_TATALTIME(ParallelExecutor::Run);
+    python_cost = total_cost - cplusplus_cost;
+    run_op_cost = CalcOverlapTotal(collector, [](const HostTraceEvent& evt) {
+      return evt.type == TracerEventType::Operator;
+    });
+  } else if (executor_type == 3) {
+    total_cost = GET_EVENT_TATALTIME(ProfileStep);
+    cplusplus_cost = GET_EVENT_TATALTIME(StandaloneExecutor::run);
+    python_cost = total_cost - cplusplus_cost;
+    run_op_cost = CalcOverlapTotal(collector, [](const HostTraceEvent& evt) {
+      return evt.type == TracerEventType::Operator;
+    });
   }
-  // common
-  uint64_t op_run = CalcOverlapTotal(collector, [](const HostTraceEvent& evt) {
-    return evt.type == TracerEventType::Operator;
-  });
-  VLOG(1) << "run op(overlap): " << op_run << "ns ";
-  uint64_t infer_shape = CalcOverlapTotal(
-      collector,
-      [](const HostTraceEvent& evt) { return evt.name == "infer_shape"; });
-  VLOG(1) << "infershape(overlap): " << infer_shape << "ns ";
+  VLOG(1) << "total_cost"
+          << "\t"
+          << "python_cost"
+          << "\t"
+          << "cplusplus_cost"
+          << "\t"
+          << "run_op_cost";
+
+  VLOG(1) << total_cost << "\t" << python_cost << "\t" << cplusplus_cost << "\t"
+          << run_op_cost;
+/*uint64_t allocator_cost = 0;
+if (executor_type <= 2) {
+  VLOG(1) << "thread model==========";
+  VLOG(1) << "threadpool AddTask: " << GET_EVENT_TATALTIME(WorkQueue::AddTask)
+          << "ns " << GET_EVENT_COUNT(WorkQueue::AddTask) << "times";
+  allocator_cost = GET_EVENT_TATALTIME(StreamSafeCUDAAllocator::Allocate) +
+                   GET_EVENT_TATALTIME(StreamSafeCUDAAllocator::Free);
+  VLOG(1) << "Allocator: " << allocator_cost << "ns "
+          << GET_EVENT_COUNT(StreamSafeCUDAAllocator::Allocate) +
+                 GET_EVENT_COUNT(StreamSafeCUDAAllocator::Free)
+          << "times";
+  uint64_t autogrowth_allocator_cost =
+      GET_EVENT_TATALTIME(AutoGrowthBestFitAllocator::Allocate) +
+      GET_EVENT_TATALTIME(AutoGrowthBestFitAllocator::Free);
+  VLOG(1) << "AutoGrowth Allocator: " << autogrowth_allocator_cost << "ns "
+          << GET_EVENT_COUNT(AutoGrowthBestFitAllocator::Allocate) +
+                 GET_EVENT_COUNT(AutoGrowthBestFitAllocator::Free)
+          << "times";
   uint64_t kernel_luanch = CalcOverlapTotal(collector,
                                             [](const HostTraceEvent& evt) {
                                               return evt.name == "compute";
                                             }) -
                            allocator_cost;
-  VLOG(1) << "kernel luanch(overlap): " << kernel_luanch << "ns ";
-  VLOG(1) << "op count: " << GET_EVENT_COUNT(compute);
-  VLOG(1) << "op gaps: " << CalcOverlapGaps(collector);
+  VLOG(1) << "kernel luanch: " << kernel_luanch << "ns ";
+} else {  // old executor
+  VLOG(1) << "static kernel=========";
+  VLOG(1) << "data transform: " << GET_EVENT_TATALTIME(prepare_data) << "ns "
+          << GET_EVENT_COUNT(prepare_data) << "times";
+  VLOG(1) << "thread model==========";
+  VLOG(1) << "threadpool AddTask: " << GET_EVENT_TATALTIME(WorkQueue::AddTask)
+          << "ns " << GET_EVENT_COUNT(WorkQueue::AddTask) << "times";
+  allocator_cost = GET_EVENT_TATALTIME(AutoGrowthBestFitAllocator::Allocate) +
+                   GET_EVENT_TATALTIME(AutoGrowthBestFitAllocator::Free);
+  VLOG(1) << "Allocator: " << allocator_cost << "ns "
+          << GET_EVENT_COUNT(AutoGrowthBestFitAllocator::Allocate) +
+                 GET_EVENT_COUNT(AutoGrowthBestFitAllocator::Free)
+          << "times";
+}
+// common
+uint64_t op_run = CalcOverlapTotal(collector, [](const HostTraceEvent& evt) {
+  return evt.type == TracerEventType::Operator;
+});
+VLOG(1) << "run op(overlap): " << op_run << "ns ";
+uint64_t infer_shape = CalcOverlapTotal(
+    collector,
+    [](const HostTraceEvent& evt) { return evt.name == "infer_shape"; });
+VLOG(1) << "infershape(overlap): " << infer_shape << "ns ";
+uint64_t kernel_luanch = CalcOverlapTotal(collector,
+                                          [](const HostTraceEvent& evt) {
+                                            return evt.name == "compute";
+                                          }) -
+                         allocator_cost;
+VLOG(1) << "kernel luanch(overlap): " << kernel_luanch << "ns ";
+VLOG(1) << "op count: " << GET_EVENT_COUNT(compute);
+VLOG(1) << "op gaps: " << CalcOverlapGaps(collector);*/
 #undef GET_EVENT_TATALTIME
 #undef GET_EVENT_COUNT
 }
