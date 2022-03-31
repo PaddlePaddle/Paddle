9b7e3823fe4dc228efe68cfbff9c7ba7f7e5ec52
jakpiase <jakpia21@gmail.com>
2022-03-31 16:28:00 +0200

    tmp save

--- paddle/fluid/pybind/paddle_bfloat/bfloat16.cc
+++ paddle/fluid/pybind/paddle_bfloat/bfloat16.cc
@@ -1,11 +1,11 @@
 // Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -909,7 +909,8 @@ struct BinaryUFunc2 {
 // 			auto x = *reinterpret_cast<const typename
 // TypeDescriptor<InType>::T *>(i0);
 // 			bfloat16 y = *reinterpret_cast<bfloat16 *>(i1);
-// 			*reinterpret_cast<typename TypeDescriptor<OutType>::T *>(o)
+// 			*reinterpret_cast<typename TypeDescriptor<OutType>::T
+// *>(o)
 // =
 // 				Functor()(x, y);
 // 			i0 += steps[0];
