2a941d9a29aa89889e9c7736ec1ce4b6c1d586aa
shangzhizhou <shangzhizhou@baidu.com>
2022-04-01 16:59:21 +0800

    notest,test=shangzhizhou

--- paddle/scripts/infrt_build.sh
+++ paddle/scripts/infrt_build.sh
@@ -98,6 +98,7 @@ function infrt_gen_and_build() {
         exit 7;
     fi
 
+    #add trt
     make -j ${parallel_number} infrt infrtopt infrtexec test_infrt_exec trt-exec phi-exec infrt_lib_dist paddle-mlir-convert;build_error=$?
     if [ "$build_error" != 0 ];then
         exit 7;
