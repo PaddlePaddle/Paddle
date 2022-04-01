db9483738b061c09466c7a14054ed1f5e1411358
Sing_chan <51314274+betterpig@users.noreply.github.com>
2022-04-01 11:40:17 +0800

    fix bug of bfgs example code;test=document_fix (#41195)

--- python/paddle/incubate/optimizer/functional/bfgs.py
+++ python/paddle/incubate/optimizer/functional/bfgs.py
@@ -100,7 +100,7 @@ def minimize_bfgs(objective_func,
                 return paddle.dot(x, x)
 
             x0 = paddle.to_tensor([1.3, 2.7])
-            results = paddle.optimizer.functional.minimize_bfgs(func, x0)
+            results = paddle.incubate.optimizer.functional.minimize_bfgs(func, x0)
             print("is_converge: ", results[0])
             print("the minimum of func is: ", results[2])
             # is_converge:  is_converge:  Tensor(shape=[1], dtype=bool, place=Place(gpu:0), stop_gradient=True,
