5ef757c3e14f790179f198a9ad0d94d74388d617
pangyoki <pangyoki@126.com>
2022-04-01 04:09:45 +0000

    add cuda11.5 manylinux docker

--- tools/dockerfile/centos7_manylinux.sh
+++ tools/dockerfile/centos7_manylinux.sh
@@ -100,6 +100,11 @@ function make_cuda112cudnn821trt8034gcc54() {
   sed -i '/CMD/iRUN ldconfig' Dockerfile.tmp
 }
 
+function make_cuda115cudnn8() {
+  sed 's/<baseimg>/11.5.1-cudnn8-devel-centos7/g' Dockerfile.centos >Dockerfile.tmp
+  sed -i "s#RUN bash build_scripts/build.sh#RUN bash build_scripts/install_gcc.sh gcc82 \nRUN mv /usr/bin/cc /usr/bin/cc.bak \&\& ln -s /usr/local/gcc-8.2/bin/gcc /usr/bin/cc \nENV PATH=/usr/local/gcc-8.2/bin:\$PATH \nRUN bash build_scripts/build.sh#g" Dockerfile.tmp
+}
+
 function main() {
   local CMD=$1 
   case $CMD in
@@ -145,6 +150,9 @@ function main() {
     cuda112cudnn821trt8034gcc54)
       make_cuda112cudnn821trt8034gcc54
      ;; 
+    cuda115cudnn8)
+      make_cuda115cudnn8
+     ;;
     *)
       echo "Make dockerfile error, Without this paramet."
       exit 1
