a2a1c634e5a26c43eba7b66794276912cafc2709
ziyoujiyi <997620387@qq.com>
2022-04-01 07:34:48 +0000

    .

--- cmake/external/arm_brpc.cmake
+++ cmake/external/arm_brpc.cmake
@@ -25,51 +25,34 @@ INCLUDE(ExternalProject)
 #ADD_LIBRARY(crypto SHARED IMPORTED GLOBAL)
 #SET_PROPERTY(TARGET crypto PROPERTY IMPORTED_LOCATION ${OPENSSL_CRYPTO_LIBRARY})
 
-SET(ARM_BRPC_PROJECT       "extern_arm_brpc")
 IF((NOT DEFINED ARM_BRPC_NAME) OR (NOT DEFINED ARM_BRPC_URL))
   MESSAGE(STATUS "use pre defined download url")
-  SET(PSLIB_BRPC_VER "0.1.0" CACHE STRING "" FORCE)
+  SET(ARM_BRPC_VER "0.1.0" CACHE STRING "" FORCE)
   SET(ARM_BRPC_NAME "arm_brpc" CACHE STRING "" FORCE)
   SET(ARM_BRPC_URL "https://arm_brpc.bj.bcebos.com/arm_brpc.tar.gz" CACHE STRING "" FORCE)
 ENDIF()
+
 MESSAGE(STATUS "ARM_BRPC_NAME: ${ARM_BRPC_NAME}, ARM_BRPC_URL: ${ARM_BRPC_URL}")
 SET(ARM_BRPC_PREFIX_DIR    "${THIRD_PARTY_PATH}/arm_brpc")
+SET(ARM_BRPC_PROJECT       "extern_arm_brpc")
 SET(ARM_BRPC_DOWNLOAD_DIR  "${ARM_BRPC_PREFIX_DIR}/src/${ARM_BRPC_PROJECT}")
-SET(ARM_BRPC_DST_DIR       "output")
+SET(ARM_BRPC_DST_DIR       "base/baidu-rpc/output")
 SET(ARM_BRPC_INSTALL_ROOT  "${THIRD_PARTY_PATH}/install")
 SET(ARM_BRPC_INSTALL_DIR   ${ARM_BRPC_INSTALL_ROOT}/${ARM_BRPC_DST_DIR})
 SET(ARM_BRPC_ROOT          ${ARM_BRPC_INSTALL_DIR})
 SET(ARM_BRPC_INC_DIR       ${ARM_BRPC_ROOT}/include/baidu/rpc)
 SET(ARM_BRPC_LIB_DIR       ${ARM_BRPC_ROOT}/lib)
 SET(ARM_BRPC_LIB           ${ARM_BRPC_LIB_DIR}/libbdrpc.a)
-SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH}" "${ARM_BRPC_ROOT}/lib")
-
-INCLUDE_DIRECTORIES(${ARM_BRPC_INC_DIR})
 
 FILE(WRITE ${ARM_BRPC_DOWNLOAD_DIR}/CMakeLists.txt
   "PROJECT(ARM_BRPC)\n"
-  "cmake_minimum_required(VERSION 3.0)\n"
-  "install(DIRECTORY ${ARM_BRPC_NAME}/include ${ARM_BRPC_NAME}/lib \n"
-  "        DESTINATION ${ARM_BRPC_DST_DIR})\n")
+  "cmake_minimum_required(VERSION 3.0)\n")
 
-ExternalProject_Add(
-    ${ARM_BRPC_PROJECT}
-    ${EXTERNAL_PROJECT_LOG_ARGS}
-    PREFIX                ${ARM_BRPC_PREFIX_DIR}
-    DOWNLOAD_DIR          ${ARM_BRPC_DOWNLOAD_DIR}
-    #DOWNLOAD_COMMAND      wget --no-check-certificate ${ARM_BRPC_URL} -c -q -O ${ARM_BRPC_NAME}.tar.gz
-    DOWNLOAD_COMMAND      cp /home/wangbin44/Paddle/build/arm_brpc.tar.gz .
-                          && tar zxvf ${ARM_BRPC_NAME}.tar.gz && cp -r base ./install/
-    DOWNLOAD_NO_PROGRESS  1
-    UPDATE_COMMAND        ""
-    CMAKE_ARGS            -DCMAKE_INSTALL_PREFIX=${ARM_BRPC_INSTALL_ROOT}
-                          -DCMAKE_BUILD_TYPE=${THIRD_PARTY_BUILD_TYPE}
-    CMAKE_CACHE_ARGS      -DCMAKE_INSTALL_PREFIX:PATH=${ARM_BRPC_INSTALL_ROOT}
-                          -DCMAKE_BUILD_TYPE=${THIRD_PARTY_BUILD_TYPE}
-    BUILD_BYPRODUCTS      ${ARM_BRPC_LIB}
-)
+execute_process(COMMAND cp /home/wangbin44/Paddle/build/arm_brpc.tar.gz .)
+execute_process(COMMAND tar zxvf ${ARM_BRPC_NAME}.tar.gz)
+execute_process(COMMAND mv base ./install/base)
 
-ADD_LIBRARY(arm_brpc STATIC IMPORTED GLOBAL)
+ADD_LIBRARY(arm_brpc STATIC IMPORTED GLOBAL)  # 直接导入已经生成的库
 SET_PROPERTY(TARGET arm_brpc PROPERTY IMPORTED_LOCATION ${ARM_BRPC_LIB})
 ADD_DEPENDENCIES(arm_brpc ${ARM_BRPC_PROJECT})
 
