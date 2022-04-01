fa1a1008b4037689ab4aee707f115f138b778c4d
Aurelius84 <zhangliujie@baidu.com>
2022-04-01 02:22:41 +0000

    format code

--- python/paddle/fluid/layers/control_flow.py
+++ python/paddle/fluid/layers/control_flow.py
@@ -971,17 +971,18 @@ def get_inputs_outputs_in_block(current_block, inner_inputs, inner_outputs,
     :param inner_outputs: Output var name of ops in current block.
     :return: inner_inputs, inner_outputs
     """
-    # NOTE(dev): There are some persistable var created in some non-standard API
-    # such as "contrib.layers.shuffle_batch". It create a "Seed" used both in
-    # Input and Output. This var shall not be considered as a loop_var in
-    # control_flow.
-    IGNORE_VAR_NAMES = ["shuffle_batch_seed"]
 
-    def is_ignore_vars(var):
-        for name in IGNORE_VAR_NAMES:
-            if name in var.name:
-                return True
-
+    def is_ignore_vars(op, var_name):
+        # NOTE(dev): There are some persistable var created in some non-standard API
+        # such as "contrib.layers.shuffle_batch". It create a "Seed" used both in
+        # Input and Output. This var shall not be considered as a loop_var in
+        # control_flow.
+        IGNORE_VAR_NAMES = {"shuffle_batch": ["shuffle_batch_seed"]}
+        if op.type in IGNORE_VAR_NAMES:
+            var_names = IGNORE_VAR_NAMES[op.type]
+            for name in var_names:
+                if name in var_name:
+                    return True
         return False
 
     # Step1: update inner_inputs and inner_outputs
@@ -992,7 +993,8 @@ def get_inputs_outputs_in_block(current_block, inner_inputs, inner_outputs,
         assert isinstance(op, Operator)
         for iname in op.input_names:
             for in_var_name in op.input(iname):
-                if in_var_name not in inner_outputs:
+                if in_var_name not in inner_outputs and not is_ignore_vars(
+                        op, in_var_name):
                     inner_inputs.add(in_var_name)
 
         for oname in op.output_names:
@@ -1009,7 +1011,7 @@ def get_inputs_outputs_in_block(current_block, inner_inputs, inner_outputs,
         if current_block.has_var(in_var_name):
             current_block_var = current_block.var(in_var_name)
         if not parent_block_var and current_block_var and \
-                (current_block_var.type == core.VarDesc.VarType.LOD_TENSOR_ARRAY or is_ignore_vars(current_block_var)):
+                current_block_var.type == core.VarDesc.VarType.LOD_TENSOR_ARRAY:
             remove_inner_inputs.add(in_var_name)
 
     inner_inputs = inner_inputs - remove_inner_inputs
