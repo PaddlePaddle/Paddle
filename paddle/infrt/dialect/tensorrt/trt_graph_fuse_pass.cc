c70760aafc239855edeba883fc69d49f1c72bdae
shangzhizhou <shangzhizhou@baidu.com>
2022-04-02 14:21:50 +0800

    notest

--- paddle/infrt/dialect/tensorrt/trt_graph_fuse_pass.cc
+++ paddle/infrt/dialect/tensorrt/trt_graph_fuse_pass.cc
@@ -29,38 +29,11 @@
 
 namespace infrt {
 namespace trt {
-namespace {
-// ReverseDfs
-// do reverse dfs. calls "func" to search when visit a node.
-// The elements in 'source' can't be nullptr.
-// Reference the function nameed "FlexibleDFS" but defined in:
-// paddle/fluid/framework/ir/subgraph_detector.cc.
-
-bool reverseDfs(std::vector<mlir::Operation *> source,
-                const std::function<bool(const mlir::Operation *)> &func) {
-  std::unordered_set<const mlir::Operation *> visited;
-  while (!source.empty()) {
-    auto node = source.back();
-    source.pop_back();
-    if (visited.count(node)) continue;
-    visited.insert(node);
-    if (func(node)) return true;
-    auto values = node->getOperands();
-    for (auto value : values) {
-      // if the value is a block argument, the node is nullptr.
-      mlir::Operation *node = value.getDefiningOp();
-      if (node != nullptr && !visited.count(node)) {
-        source.emplace_back(node);
-      }
-    }
-  }
-  return false;
-}
 
 // merge the first&second graph op to a new graph op.
-void mergeTwoAdjacentGraphOp(mlir::OpBuilder &builder,  // NOLINT
-                             ::infrt::GraphOp first,
-                             ::infrt::GraphOp second) {
+static void mergeTwoAdjacentGraphOp(mlir::OpBuilder &builder,  // NOLINT
+                                    ::infrt::GraphOp first,
+                                    ::infrt::GraphOp second) {
   // comput inputs and outputs
   ::llvm::SmallVector<mlir::Value, 4> inputs(first.getOperands()), outputs;
   for (mlir::Value input : second.getOperands()) {
@@ -126,65 +99,9 @@ void mergeTwoAdjacentGraphOp(mlir::OpBuilder &builder,  // NOLINT
   }
   second.replaceAllUsesWith(
       graph_op.getResults().take_back(second.getNumResults()));
-  first.erase();
-  second.erase();
+  // first.erase();
+  // second.erase();
 }
-
-// Topological sort the function op.
-void topoSortBlock(mlir::Block &body) {  // NOLINT
-  llvm::SetVector<mlir::Operation *> toSort;
-  if (body.empty()) return;
-  for (auto it = body.rbegin(); it != body.rend(); ++it) {
-    toSort.insert(&*it);
-  }
-  llvm::SetVector<mlir::Operation *> result = mlir::topologicalSort(toSort);
-  for (auto *op : result) {
-    op->moveBefore(body.getTerminator());
-  }
-}
-
-}  // namespace
-/*
-// Implementation of the trtGraphFusePass.
-void TRTGraphFusePass::runOnFunction() {
-  mlir::Block &body = getFunction().front();
-  mlir::OpBuilder builder(&body, body.begin());
-  bool changed = false;
-  do {
-    changed = false;
-    for (auto &op : body) {
-      ::infrt::GraphOp graph_op =
-          ::llvm::dyn_cast_or_null<::infrt::GraphOp>(&op);
-      if (nullptr == graph_op) continue;
-
-      for (auto user_op : op.getUsers()) {
-        ::infrt::GraphOp user_graph_op =
-            ::llvm::dyn_cast_or_null<::infrt::GraphOp>(user_op);
-        if (nullptr == user_graph_op) continue;
-        // get all dst input nodes except src.
-        std::vector<mlir::Operation *> source_nodes;
-        for (auto operand : user_op->getOperands()) {
-          auto input = operand.getDefiningOp();
-          if (input != &op && input != nullptr) {
-            source_nodes.push_back(input);
-          }
-        }
-        // Reverse DFS from the source_nodes.
-        if (!reverseDfs(source_nodes,
-                        [&op](const mlir::Operation *n) { return n == &op; })) {
-          mergeTwoAdjacentGraphOp(builder, graph_op, user_graph_op);
-          changed = true;
-          break;
-        }
-      }
-      if (changed) break;
-    }
-  } while (changed);
-
-  // TODO(wilber): Implement a toposort for efficiency.
-  // topoSortBlock(body);
-}
-*/
 
 struct TRT_Graph_Fuse : public ::mlir::RewritePattern {
   explicit TRT_Graph_Fuse(::mlir::MLIRContext *context)
@@ -192,62 +109,53 @@ struct TRT_Graph_Fuse : public ::mlir::RewritePattern {
   ::mlir::LogicalResult matchAndRewrite(
       ::mlir::Operation *op, ::mlir::PatternRewriter &rewriter) const override {
     auto graph_op = ::llvm::dyn_cast<::infrt::GraphOp>(op);
-    for (::mlir::Value input : graph_op.getOperands()) {
-      std::string str;
-      llvm::raw_string_ostream os(str);
-      input.print(os);
-      LOG(INFO) << "cur op   ==>   " << str << "\n";
-    }
-    /*
-    ::mlir::SmallVector<::mlir::Value, 4> operands;
-    ::mlir::Operation::operand_range Input = casted_op.getODSOperands(0);
-    ::mlir::Operation::operand_range Scale = casted_op.getODSOperands(1);
-    ::mlir::Operation::operand_range Bias = casted_op.getODSOperands(2);
+    ::infrt::GraphOp father_graph_op{nullptr};
+    for (::mlir::Value input : graph_op.inputs()) {
+      if (input.getDefiningOp()) {
+        if (auto father_graph_op =
+                ::llvm::dyn_cast<::infrt::GraphOp>(input.getDefiningOp())) {
+          // check mergeable
+          bool mergeable{true};
+          for (::mlir::Value output : father_graph_op->getOpResults()) {
+            for (auto user : output.getUsers()) {
+              if (user->getName().getIdentifier().str() == "infrt.graph" &&
+                  user != graph_op) {
+                mergeable = false;
+                break;
+              }
+            }
+            if (!mergeable) break;
+          }
 
-    // TODO(weishengying) : recompute this via params
-    operands.push_back((*Input.begin()));
-    operands.push_back((*Scale.begin()));
-    operands.push_back((*Bias.begin()));
-    operands.push_back((*Bias.begin()));
+          if (mergeable) {
+            // for debug
+            LOG(INFO) << ".......................................mergeable....."
+                         "..................................";
+            LOG(INFO) << "graph Op.name: "
+                      << graph_op->getName().getIdentifier().str();
+            for (auto xx : graph_op.inputs()) {
+              std::string str;
+              llvm::raw_string_ostream os(str);
+              xx.print(os);
+              LOG(INFO) << str;
+            }
+            LOG(INFO) << "father_graph Op.name: "
+                      << father_graph_op->getName().getIdentifier().str();
+            for (auto xx : father_graph_op.inputs()) {
+              std::string str;
+              llvm::raw_string_ostream os(str);
+              xx.print(os);
+              LOG(INFO) << str;
+            }
 
-    trt::ScaleNdOp scaleNd_op;
-    // inputs
-    ::mlir::SmallVector<::mlir::Value, 4> trt_inputs;
-    for (auto v : operands) {
-      trt_inputs.push_back(v);
-    }
-
-    // resultTypes
-    ::mlir::SmallVector<::mlir::Type, 4> resultTypes;
-    for (auto v : casted_op.getODSResults(0)) {
-      resultTypes.push_back(v.getType());
-    }
-
-    // attributes
-    ::mlir::SmallVector<::mlir::NamedAttribute, 8> attributes;
-    {
-      auto mode_attr = rewriter.getI32IntegerAttr(1);
-      attributes.emplace_back(rewriter.getStringAttr("mode"), mode_attr);
-    }
-
-    {
-      auto axis_attr = rewriter.getI32IntegerAttr(-1);
-      attributes.emplace_back(rewriter.getStringAttr("axis"), axis_attr);
-    }
-    auto result = rewriter
-                      .create<trt::ScaleNdOp>(
-                          op->getLoc(), resultTypes, operands, attributes)
-                      .getODSResults(0);
-    ::llvm::SmallVector<::mlir::Value, 4> tblgen_repl_values;
-    // TODO(weishengying) : update it
-    for (uint32_t i = 0; i < casted_op.getNumResults(); i++) {
-      for (auto v : ::llvm::SmallVector<::mlir::Value, 4>{result}) {
-        tblgen_repl_values.push_back(v);
+            // mergeTwoAdjacentGraphOp(rewriter, father_graph_op, graph_op);
+            // return ::mlir::success();
+            break;
+          }
+        }
       }
     }
-    rewriter.replaceOp(op, tblgen_repl_values);
-    */
-    // return ::mlir::success();
+
     return rewriter.notifyMatchFailure(
         op, [&](::mlir::Diagnostic &diag) { diag << "infrt.graph not match"; });
   }
