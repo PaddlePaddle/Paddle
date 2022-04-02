2da04b93d04e4a387f0de15ad007250a8e3d5645
phlrain <phliuhongyu@126.com>
2022-04-02 00:47:05 +0000

    fix context default value error

--- paddle/fluid/framework/op_desc.cc
+++ paddle/fluid/framework/op_desc.cc
@@ -780,7 +780,14 @@ void OpDesc::CheckAttrs() {
 void OpDesc::InferShape(const BlockDesc &block) const {
   try {
     VLOG(3) << "CompileTime infer shape on " << Type();
-    auto &infer_shape = OpInfoMap::Instance().Get(this->Type()).infer_shape_;
+    auto &op_info = OpInfoMap::Instance().Get(this->Type());
+    auto *checker = op_info.Checker();
+    if (checker == nullptr) {
+      // set dafault value here
+      VLOG(10) << "begin to check attribute of " << Type();
+      checker->Check(&attrs_);
+    }
+    auto &infer_shape = on_info.infer_shape_;
     PADDLE_ENFORCE_EQ(
         static_cast<bool>(infer_shape), true,
         platform::errors::NotFound(
