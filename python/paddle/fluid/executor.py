0271d701519b313efc5793f42466bc1c51309218
zhiqiu <chenqiuliang@baidu.com>
2022-04-03 15:37:04 +0800

    skip unsupported place

--- python/paddle/fluid/executor.py
+++ python/paddle/fluid/executor.py
@@ -1377,6 +1377,10 @@ class Executor(object):
             program = pruned_program
 
         def _can_use_interpreter_core(program, place):
+            if core.is_compiled_with_npu() or core.is_compiled_with_xpu(
+            ) or core.is_compiled_with_mlu() or core.is_compiled_with_ipu():
+                return False
+
             compiled = isinstance(program, compiler.CompiledProgram)
             # NOTE(zhiqiu): do not support compiled program now
             if compiled:
