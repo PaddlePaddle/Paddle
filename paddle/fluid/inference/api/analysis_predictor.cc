f27ea98e912b9370bd23a44f9f719dcaf7a24d51
JZZ-NOTE <jzz2446071400@163.com>
2022-04-05 15:57:04 +0000

    gpu memory strategy

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -772,12 +772,12 @@ std::unique_ptr<PaddlePredictor> CreatePaddlePredictor<
         gflags.push_back("--cudnn_deterministic=True");
       }
 
-// TODO(wilber): jetson tx2 may fail to run the model due to insufficient memory
-// under the native_best_fit strategy. Modify the default allocation strategy to
-// auto_growth. todo, find a more appropriate way to solve the problem.
-#ifdef WITH_NV_JETSON
+      // TODO(wilber): jetson tx2 may fail to run the model due to insufficient
+      // memory
+      // under the native_best_fit strategy. Modify the default allocation
+      // strategy to
+      // auto_growth. todo, find a more appropriate way to solve the problem.
       gflags.push_back("--allocator_strategy=auto_growth");
-#endif
 
       // TODO(Shixiaowei02): Add a mandatory scheme to use the thread local
       // allocator when multi-stream is enabled.
