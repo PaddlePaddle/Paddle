0372a4d64cef3bb54db9f19314156006636f4c2a
Zhou Wei <1183042833@qq.com>
2022-04-07 22:15:52 +0800

    remove FLAGS_use_curand and change all random op CUDA implementation (#41308)

--- python/paddle/fluid/tests/unittests/test_exponential_op.py
+++ python/paddle/fluid/tests/unittests/test_exponential_op.py
@@ -100,9 +100,6 @@ class TestExponentialAPI(unittest.TestCase):
         if not "V100" in paddle.device.cuda.get_device_name():
             return
 
-        if os.getenv("FLAGS_use_curand", None) in ('0', 'False', None):
-            return
-
         print("Test Fixed Random number on V100 GPU------>")
         paddle.disable_static()
         paddle.set_device('gpu')
