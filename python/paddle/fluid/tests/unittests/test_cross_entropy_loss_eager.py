aaa026cb636cc4efa561c49842a964d3938c5c38
Chen Weihang <chenweihang@baidu.com>
2022-04-07 13:59:19 +0000

    fix import error

--- python/paddle/fluid/tests/unittests/test_cross_entropy_loss_eager.py
+++ python/paddle/fluid/tests/unittests/test_cross_entropy_loss_eager.py
@@ -14,46 +14,113 @@
 
 from __future__ import print_function
 
-import test_cross_entropy_loss
+from test_cross_entropy_loss import CrossEntropyLoss
 from paddle.fluid.framework import _test_eager_guard
 
 
 class CrossEntropyLossEager(CrossEntropyLoss):
-    def test_soft_1d_dygraph_final_state_api(self):
+    def test_cross_entropy_loss_soft_1d_eager(self):
         with _test_eager_guard():
             self.test_cross_entropy_loss_soft_1d()
+
+    def test_cross_entropy_loss_soft_1d_weight_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_soft_1d_weight()
+
+    def test_cross_entropy_loss_soft_1d_mean_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_soft_1d_mean()
+
+    def test_cross_entropy_loss_soft_1d_weight_mean_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_soft_1d_weight_mean()
 
-    # put all testcases in one test will be failed
-    def test_soft_2d_dygraph_final_state_api(self):
+    def test_cross_entropy_loss_soft_2d_eager(self):
         with _test_eager_guard():
             self.test_cross_entropy_loss_soft_2d()
+
+    def test_cross_entropy_loss_soft_2d_weight_mean_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_soft_2d_weight_mean()
 
-    def test_other_dygraph_final_state_api(self):
+    def test_cross_entropy_loss_1d_with_mean_ignore_eager(self):
         with _test_eager_guard():
             self.test_cross_entropy_loss_1d_with_mean_ignore()
+
+    def test_cross_entropy_loss_1d_with_mean_ignore_negative_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_with_mean_ignore_negative()
+
+    def test_cross_entropy_loss_1d_with_weight_mean_ignore_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_with_weight_mean_ignore()
+
+    def test_cross_entropy_loss_1d_with_weight_mean_ignore_exceedlabel_eager(
+            self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_with_weight_mean_ignore_exceedlabel(
             )
+
+    def test_cross_entropy_loss_1d_with_weight_mean_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_with_weight_mean()
+
+    def test_cross_entropy_loss_1d_with_weight_sum_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_with_weight_sum()
+
+    def test_cross_entropy_loss_1d_with_weight_none_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_with_weight_none()
+
+    def test_cross_entropy_loss_1d_with_weight_none_func_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_with_weight_none_func()
+
+    def test_cross_entropy_loss_1d_mean_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_mean()
+
+    def test_cross_entropy_loss_1d_sum_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_sum()
+
+    def test_cross_entropy_loss_1d_none_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_1d_none()
+
+    def test_cross_entropy_loss_2d_with_weight_none_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_2d_with_weight_none()
+
+    def test_cross_entropy_loss_2d_with_weight_axis_change_mean_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_2d_with_weight_axis_change_mean()
+
+    def test_cross_entropy_loss_2d_with_weight_mean_ignore_exceedlabel_eager(
+            self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_2d_with_weight_mean_ignore_exceedlabel(
             )
+
+    def test_cross_entropy_loss_2d_with_weight_mean_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_2d_with_weight_mean()
+
+    def test_cross_entropy_loss_2d_with_weight_sum_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_2d_with_weight_sum()
+
+    def test_cross_entropy_loss_2d_none_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_2d_none()
+
+    def test_cross_entropy_loss_2d_mean_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_2d_mean()
+
+    def test_cross_entropy_loss_2d_sum_eager(self):
+        with _test_eager_guard():
             self.test_cross_entropy_loss_2d_sum()
 
 
