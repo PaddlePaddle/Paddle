4c896df5cf6f008fde5579b368e27c7ca04a2d28
sandyhouse <lilong12@baidu.com>
2022-04-08 05:21:38 +0000

    update

--- paddle/fluid/distributed/collective/ProcessGroupHCCL.h
+++ paddle/fluid/distributed/collective/ProcessGroupHCCL.h
@@ -46,7 +46,7 @@ class ProcessGroupHCCL : public ProcessGroup {
                    public std::enable_shared_from_this<HCCLTask> {
    public:
     HCCLTask(const std::vector<Place>& places, int rank, CommType CommType,
-             const std::vector<Tensor>& inputs);
+             const std::vector<phi::DenseTensor>& inputs);
 
     bool IsCompleted();
 
@@ -65,7 +65,7 @@ class ProcessGroupHCCL : public ProcessGroup {
    protected:
     std::vector<Place> places_;
     std::vector<std::shared_ptr<HCCLCommManager>> hcclComms_;
-    std::shared_ptr<std::vector<Tensor>> outputs_;
+    std::shared_ptr<std::vector<phi::DenseTensor>> outputs_;
 
    private:
   };
@@ -78,17 +78,19 @@ class ProcessGroupHCCL : public ProcessGroup {
   }
 
   std::shared_ptr<ProcessGroup::Task> AllReduce(
-      std::vector<Tensor>& tensors,
+      std::vector<phi::DenseTensor>& in_tensors,
+      std::vector<phi::DenseTensor>& out_tensors,
       const AllreduceOptions& = AllreduceOptions()) override;
 
   std::shared_ptr<ProcessGroup::Task> Broadcast(
-      std::vector<Tensor>& tensors,
+      std::vector<phi::DenseTensor>& in_tensors,
+      std::vector<phi::DenseTensor>& out_tensors,
       const BroadcastOptions& = BroadcastOptions()) override;
 
  protected:
   virtual std::shared_ptr<ProcessGroupHCCL::HCCLTask> CreateTask(
       std::vector<Place> places, int rank, CommType opType,
-      const std::vector<Tensor>& inputs);
+      const std::vector<phi::DenseTensor>& inputs);
 
   std::shared_ptr<Store> store_;
   std::shared_ptr<HCCLCommManager> hccl_comm_;
@@ -113,8 +115,8 @@ class ProcessGroupHCCL : public ProcessGroup {
 
   template <typename Fn>
   std::shared_ptr<ProcessGroup::Task> Collective(
-      std::vector<Tensor>& inputs,   // NOLINT
-      std::vector<Tensor>& outputs,  // NOLINT
+      std::vector<phi::DenseTensor>& inputs,   // NOLINT
+      std::vector<phi::DenseTensor>& outputs,  // NOLINT
       Fn fn, CommType op_type);
 
   void CreateHCCLManagerCache(const std::string& places_key,
