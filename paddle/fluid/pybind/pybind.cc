614431f3cfbfd64ac2993776f7cc0a9780fcc889
zhaocaibei123 <zhaocaibei@baidu.com>
2022-04-09 07:03:27 +0000

    remove

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -4504,7 +4504,6 @@ All parameter, weight, gradient are variables in Paddle.
   BindTreeIndex(&m);
   BindIndexWrapper(&m);
   BindIndexSampler(&m);
-//  BindSparseShardingTools(&m);
 #endif
 }
 }  // namespace pybind
