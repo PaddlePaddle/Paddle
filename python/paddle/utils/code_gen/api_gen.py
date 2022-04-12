c47b33a91a48ece55fa4d52ba3288b55798d3fac
Chen Weihang <chenweihang@baidu.com>
2022-04-12 13:05:51 +0000

    test declarations block, test=document_fix

--- python/paddle/utils/code_gen/api_gen.py
+++ python/paddle/utils/code_gen/api_gen.py
@@ -177,6 +177,7 @@ def source_include(header_file_path):
 #include "paddle/phi/infermeta/nullary.h"
 #include "paddle/phi/infermeta/unary.h"
 #include "paddle/phi/infermeta/ternary.h"
+#include "paddle/phi/kernels/declarations.h"
 
 #include "paddle/fluid/platform/profiler/event_tracing.h"
 
