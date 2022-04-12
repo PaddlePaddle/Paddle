901b26a045fdd5d5f1b17fa930cade6b449aaddc
Chen Weihang <chenweihang@baidu.com>
2022-04-12 05:09:06 +0000

    add comment

--- paddle/phi/common/place.cc
+++ paddle/phi/common/place.cc
@@ -116,7 +116,9 @@ phi::Place PlaceType::kUNK = phi::Place();
 phi::Place PlaceType::kCPU = phi::Place(phi::AllocationType::CPU);
 // GPU Place contains device id, here we use default value 0, so it cannot
 // use for multi-casd cases, but because it is static variable, it is difficult
-// to get the exact device id at all time
+// to get the exact device id at all time.
+// NOTE: Please DO NOT use this place in the framework!!!
+// It only for external compatibility
 phi::Place PlaceType::kGPU = phi::Place(phi::AllocationType::GPU);
 
 }  // namespace paddle
