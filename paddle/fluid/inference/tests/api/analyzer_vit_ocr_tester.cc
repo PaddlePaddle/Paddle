acc5db2cff04fc0c2db0f750ab4c6189635c613d
Sławomir Siwek <slawomir.siwek@intel.com>
2022-04-13 02:05:33 -0700

    VIT-OCR accuracy check

--- paddle/fluid/inference/tests/api/analyzer_vit_ocr_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_vit_ocr_tester.cc
@@ -13,10 +13,56 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/inference/tests/api/tester_helper.h"
+#include <fstream>
+#include <iostream>
 
 namespace paddle {
 namespace inference {
 namespace analysis {
+
+struct Record {
+  std::vector<float> data;
+  std::vector<int32_t> shape;
+};
+
+Record ProcessALine(const std::string &line) {
+  std::vector<std::string> columns;
+  split(line, '\t', &columns);
+  CHECK_EQ(columns.size(), 2UL)
+      << "data format error, should be <data>\t<shape>";
+
+  Record record;
+  std::vector<std::string> data_strs;
+  split(columns[0], ' ', &data_strs);
+  for (auto &d : data_strs) {
+    record.data.push_back(std::stof(d));
+  }
+  
+  std::vector<std::string> shape_strs;
+  split(columns[1], ' ', &shape_strs);
+  for (auto &s : shape_strs) {
+    record.shape.push_back(std::stoi(s));
+  }
+  
+  return record;
+}  
+
+void SetInput(std::vector<std::vector<PaddleTensor>> *inputs) {
+  std::string line;
+  std::ifstream file(FLAGS_infer_data);
+  std::getline(file, line);
+  auto record = ProcessALine(line);
+
+  PaddleTensor input;
+  input.shape = record.shape;
+  input.dtype = PaddleDType::FLOAT32;
+  size_t input_size = record.data.size() * sizeof(float);
+  input.data.Resize(input_size);
+  memcpy(input.data.data(), record.data.data(), input_size);
+  std::vector<PaddleTensor> input_slots;
+  input_slots.assign({input});
+  (*inputs).emplace_back(input_slots);
+}
 
 void SetConfig(AnalysisConfig *cfg, bool use_mkldnn = false) {
   cfg->SetModel(FLAGS_infer_model + "/inference.pdmodel",
@@ -32,6 +78,23 @@ void SetConfig(AnalysisConfig *cfg, bool use_mkldnn = false) {
     cfg->pass_builder()->InsertPass(insertingIndex, "fc_mkldnn_pass");
   }
 }
+
+// Compare results of NativeConfig and AnalysisConfig
+void compare(bool use_mkldnn = false) {
+  AnalysisConfig cfg;
+  SetConfig(&cfg, use_mkldnn);
+
+  std::vector<std::vector<PaddleTensor>> input_slots_all;
+  SetInput(&input_slots_all);
+  CompareNativeAndAnalysis(
+      reinterpret_cast<const PaddlePredictor::Config *>(&cfg), input_slots_all);
+}
+
+TEST(Analyzer_vit_ocr, compare) { compare(); }
+
+#ifdef PADDLE_WITH_MKLDNN
+TEST(Analyzer_vit_ocr, compare_mkldnn) { compare(true /* use_mkldnn */); }
+#endif
 
 #ifdef PADDLE_WITH_MKLDNN
 // Check the fuse status
