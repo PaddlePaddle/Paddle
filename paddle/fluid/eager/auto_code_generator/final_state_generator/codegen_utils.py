10ad7cb74cf8f936647df7b0968ea197803fad2c
Zhanlue Yang <jim19930609@gmail.com>
2022-04-13 14:09:43 +0800

    [DoubleGrad] Enabled test_imperative_triple_grad test cases under eager_mode (#41612)
    * [DoubleGrad] Enabled double grad test cases in eager_mode for test_imperative_double_grad
    
    * Fixed elementwise issue
    
    * Addressed CI failures
    
    * [DoubleGrad] Enabled test_imperative_triple_grad test cases under eager_mode
    
    * Fixed minor issues

--- paddle/fluid/eager/auto_code_generator/final_state_generator/codegen_utils.py
+++ paddle/fluid/eager/auto_code_generator/final_state_generator/codegen_utils.py
@@ -23,7 +23,8 @@ import os
 ########################
 ops_to_fill_zero_for_empty_grads = set([
     "split_grad", "rnn_grad", "matmul_double_grad", "matmul_triple_grad",
-    "sigmoid_triple_grad, add_double_grad"
+    "sigmoid_double_grad", "sigmoid_triple_grad", "add_double_grad",
+    "add_triple_grad"
 ])
 
 # For API dispatch used at python-level
