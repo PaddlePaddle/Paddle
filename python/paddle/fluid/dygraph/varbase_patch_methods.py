168233045d67e3ebeaf015dc007575a898b9325e
zkh2016 <zhangkaihuo@baidu.com>
2022-04-13 02:54:19 +0000

    fix example

--- python/paddle/fluid/dygraph/varbase_patch_methods.py
+++ python/paddle/fluid/dygraph/varbase_patch_methods.py
@@ -888,7 +888,7 @@ def monkey_patch_varbase():
                     indices = [[0, 0, 1, 2, 2], [1, 3, 2, 0, 1]]
                     values = [1, 2, 3, 4, 5]
                     dense_shape = [3, 4]
-                    sparse_x = paddle.sparse.sparse_coo_tensor(paddle.to_tensor(indices, dtype='int32'), paddle.to_tensor(values, dtype='float32'), dense_shape=dense_shape)
+                    sparse_x = paddle.sparse.sparse_coo_tensor(paddle.to_tensor(indices, dtype='int32'), paddle.to_tensor(values, dtype='float32'), shape=dense_shape)
                     print(sparse_x.values())
                     #[1, 2, 3, 4, 5]
         """
