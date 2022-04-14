9c151c0eac252a376a0b39716a0d78252b091617
jakpiase <jakpia21@gmail.com>
2022-04-14 21:04:51 +0200

    added missing file

---
+++ cmake/external/paddle_bfloat.cmake
@@ -0,0 +1,22 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+file(DOWNLOAD
+    https://raw.githubusercontent.com/jakpiase/paddle_bfloat/107f3e1324ef33765900759c298fd7685f0d7a2e/bfloat16.cc
+    ${CMAKE_SOURCE_DIR}/paddle/fluid/pybind/paddle_bfloat/bfloat16.cc)
+
+file(DOWNLOAD
+    https://raw.githubusercontent.com/jakpiase/paddle_bfloat/107f3e1324ef33765900759c298fd7685f0d7a2e/bfloat16.h
+    ${CMAKE_SOURCE_DIR}/paddle/fluid/pybind/paddle_bfloat/bfloat16.h)
