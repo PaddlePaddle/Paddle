a7a81906b1e7bd468fd2d29d1494b6834c8d88d3
YuanRisheng <yuanrisheng@baidu.com>
2022-04-14 02:06:53 +0000

    fix unit test bugs

--- paddle/phi/api/lib/scalar.cc
+++ paddle/phi/api/lib/scalar.cc
@@ -30,11 +30,12 @@ ScalarBase<Tensor>::ScalarBase(const Tensor& tensor_in)
                         "The Scalar only supports Tensor with 1 element, but "
                         "now Tensor has `%d` elements",
                         tensor_in.numel()));
-  if (tensor_in.place().GetType() == phi::AllocationType::CPU) {
+  auto tensor_in_place = tensor_in.place().GetType();
+  if (tensor_in_place == phi::AllocationType::GPU) {
     Tensor dst_tensor;
     copy(tensor_in, phi::CPUPlace(), true, &dst_tensor);
     GetDataFromTensor(dst_tensor);
-  } else if (tensor_in.place() == PlaceType::kCPU) {
+  } else if (tensor_in_place == phi::AllocationType::CPU) {
     GetDataFromTensor(tensor_in);
   } else {
     PADDLE_THROW(phi::errors::Unimplemented(
