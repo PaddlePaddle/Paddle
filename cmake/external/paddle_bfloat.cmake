e626f3104f39d6adb0134d82cf488780c17fc4b1
jakpiase <jakpia21@gmail.com>
2022-04-14 22:14:31 +0200

    minor change

--- cmake/external/paddle_bfloat.cmake
+++ cmake/external/paddle_bfloat.cmake
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 
-file(DOWNLOAD
-    https://raw.githubusercontent.com/jakpiase/paddle_bfloat/107f3e1324ef33765900759c298fd7685f0d7a2e/bfloat16.cc
-    ${CMAKE_SOURCE_DIR}/paddle/fluid/pybind/paddle_bfloat/bfloat16.cc)
-
-file(DOWNLOAD
-    https://raw.githubusercontent.com/jakpiase/paddle_bfloat/107f3e1324ef33765900759c298fd7685f0d7a2e/bfloat16.h
-    ${CMAKE_SOURCE_DIR}/paddle/fluid/pybind/paddle_bfloat/bfloat16.h)
+#file(DOWNLOAD
+#    https://raw.githubusercontent.com/jakpiase/paddle_bfloat/107f3e1324ef33765900759c298fd7685f0d7a2e/bfloat16.cc
+#    ${CMAKE_SOURCE_DIR}/paddle/fluid/pybind/paddle_bfloat/bfloat16.cc)
+#
+#file(DOWNLOAD
+#    https://raw.githubusercontent.com/jakpiase/paddle_bfloat/107f3e1324ef33765900759c298fd7685f0d7a2e/bfloat16.h
+#    ${CMAKE_SOURCE_DIR}/paddle/fluid/pybind/paddle_bfloat/bfloat16.h)
