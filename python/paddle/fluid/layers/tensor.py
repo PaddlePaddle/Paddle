921a6fb78a2038cca9dd501be4e0f9c88fda3ef0
chentianyu03 <chentianyu03@baidu.com>
2022-04-14 11:16:19 +0800

    Cherry pick final state ops (#41755)
    * [Yaml]add exp yaml (#41217)
    
    * add exp yaml
    
    * add exp api in test case
    
    * add determinant yaml
    
    * fix exp op unittest
    
    * change test class name
    
    * modify api name
    
    * compacted with raw api
    
    * fix det api
    
    * add python_api
    
    * add test eager for determinant op
    
    * [Yaml] Add assign yaml (#41428)
    
    * add assign yaml
    
    * add assign api
    
    * add assign backward api
    
    * add assign
    
    * add assign yaml
    
    * add assign
    
    * assign yaml
    
    * add assign raw kernel and use assign_raw in yaml
    
    * merge develop branch
    
    * add missing python_api
    
    * exchange assign and assign_raw kernel name (#41625)
    
    * exchange assign and assign_raw kernel name
    
    * fix register error
    
    * [Yaml]add gaussian_random yaml and test case (#41312)
    
    * add guassian random yaml
    
    * add gaussian_random yaml and test case
    
    * fix error modify of full yaml
    
    * import in_dygraph_mode
    
    * import _in_legacy_dygraph
    
    * add place arg in api
    
    * import __current_expected_place
    
    * fix test_egr_python_api failed case
    
    * add test case
    
    * add cast for NormalInitializer
    
    * fix test error
    
    * fix test error
    
    * rm unsed check code
    
    * fix test error in test_initializer_nn
    
    * modify by review
    
    * [Phi]fix split error when sections has 0 size and add test case (#41708)
    
    * fix split error when sections has 0 size and add test case
    
    * fix test case

--- python/paddle/fluid/layers/tensor.py
+++ python/paddle/fluid/layers/tensor.py
@@ -622,12 +622,15 @@ def assign(input, output=None):
     # after this api.
     if isinstance(input, (Variable, core.VarBase)):
         if _non_static_mode():
-            if output is None:
-                if _in_legacy_dygraph():
-                    output = core.VarBase()
-                else:
-                    output = core.eager.Tensor()
-            _C_ops.assign(input, output)
+            if in_dygraph_mode() and output is None:
+                output = _C_ops.final_state_assign(input)
+            else:
+                if output is None:
+                    if _in_legacy_dygraph():
+                        output = core.VarBase()
+                    else:
+                        output = core.eager.Tensor()
+                _C_ops.assign(input, output)
         else:
             check_dtype(input.dtype, 'input', [
                 'float16', 'uint16', 'float32', 'float64', 'int32', 'int64',
