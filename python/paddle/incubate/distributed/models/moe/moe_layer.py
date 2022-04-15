063f475fd107bb1900fe7b8abe3caf1a941548bf
ForFishes <2282912238@qq.com>
2022-04-15 07:48:08 +0000

    fix moe

--- python/paddle/incubate/distributed/models/moe/moe_layer.py
+++ python/paddle/incubate/distributed/models/moe/moe_layer.py
@@ -12,6 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Copyright 2021, Jiaao He
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# The file has been adapted from
+# https://github.com/laekov/fastmoe/blob/master/fmoe/functions.py
+# commit: 59bcec8edf2b7fb6fa86baa0e2d561a838730e8a
+
 import collections
 import math
 
