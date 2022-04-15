f5ae211a998dbaa6ccd2ebab9d02b58c6b195541
AshburnLee <1578034415@qq.com>
2022-04-15 05:40:25 +0000

    rm header

--- paddle/phi/kernels/funcs/reduce_function.h
+++ paddle/phi/kernels/funcs/reduce_function.h
@@ -36,7 +36,6 @@ namespace cub = hipcub;
 #ifndef PADDLE_WITH_XPU_KP
 #include "paddle/fluid/platform/device/gpu/gpu_device_function.h"
 #include "paddle/fluid/platform/device/gpu/gpu_launch_config.h"
-// #include "paddle/fluid/platform/fast_divmod.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/backends/gpu/gpu_info.h"
 #endif
