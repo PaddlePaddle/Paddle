9294ba25424727d3197c1af9be100e7a7cf652cf
zhangkaihuo <zhangkaihuo@baidu.com>
2022-04-15 11:16:17 +0800

    Add paddle.sparse and three Sparse API (#41276) (#41803)
    cherry-pick PR#41276 到 release/2.3
    
        添加路径：paddle.sparse，paddle.sparse.functional，paddle.sparse.layer
        添加API：paddle.sparse.sparse_coo_tensor（用于创建COO格式的Sparse Tensor ）和 paddle.sparse.sparse_csr_tensor（用于创建CSR格式的Sparse Tensor ）
        添加API：paddle.sparse.ReLU

--- python/paddle/fluid/tests/unittests/test_sparse_activation_op.py
+++ python/paddle/fluid/tests/unittests/test_sparse_activation_op.py
@@ -16,7 +16,6 @@ from __future__ import print_function
 import unittest
 import numpy as np
 import paddle
-from paddle import _C_ops
 from paddle.fluid.framework import _test_eager_guard
 
 
@@ -24,16 +23,18 @@ class TestSparseActivation(unittest.TestCase):
     def test_sparse_relu(self):
         with _test_eager_guard():
             x = [[0, -1, 0, 2], [0, 0, -3, 0], [4, 5, 0, 0]]
-            dense_x = paddle.to_tensor(x, dtype='float32')
-            dense_shape = [3, 4]
-            stop_gradient = True
+            dense_x = paddle.to_tensor(x, dtype='float32', stop_gradient=False)
             sparse_dim = 2
-            sparse_coo_x = dense_x.to_sparse_coo(sparse_dim)
-            #TODO(zhangkaihuo): change to test the corresponding API: paddle.sparse.relu(sparse_coo_x)
-            sparse_act_out = _C_ops.final_state_sparse_relu(sparse_coo_x)
-            correct_result = [0, 2, 0, 4, 5]
-            actual_result = sparse_act_out.non_zero_elements().numpy()
-            assert np.array_equal(correct_result, actual_result)
+            sparse_x = dense_x.to_sparse_coo(sparse_dim)
+            sparse_relu = paddle.sparse.ReLU()
+            sparse_out = sparse_relu(sparse_x)
+            dense_relu = paddle.nn.ReLU()
+            #TODO: replace non_zero_elements() as values()
+            dense_out = dense_relu(sparse_x.non_zero_elements())
+            actual_result = sparse_out.non_zero_elements().numpy()
+            assert np.array_equal(dense_out.numpy(), actual_result)
+            dense_out.backward(dense_out)
+            sparse_out.backward(sparse_out)
 
 
 if __name__ == "__main__":
