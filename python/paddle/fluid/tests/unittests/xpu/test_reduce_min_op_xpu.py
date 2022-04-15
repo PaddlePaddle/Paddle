585f905dc9c15a9a1b9c2bb6227484a0684bd5cb
AshburnLee <1578034415@qq.com>
2022-04-15 21:24:05 +0000

    init commit,add files

--- python/paddle/fluid/tests/unittests/xpu/test_reduce_min_op_xpu.py
+++ python/paddle/fluid/tests/unittests/xpu/test_reduce_min_op_xpu.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,18 +27,18 @@ from xpu.get_test_cover_info import create_test_class, get_xpu_op_support_types,
 paddle.enable_static()
 
 
-class XPUTestReduceMaxOp(XPUOpTestWrapper):
+class XPUTestReduceMinOp(XPUOpTestWrapper):
     def __init__(self):
-        self.op_name = 'reduce_max'
+        self.op_name = 'reduce_min'
 
-    class XPUTestReduceMaxBase(XPUOpTest):
+    class XPUTestReduceMinBase(XPUOpTest):
         def setUp(self):
             self.place = paddle.XPUPlace(0)
             self.init_case()
             self.set_case()
 
         def set_case(self):
-            self.op_type = 'reduce_max'
+            self.op_type = 'reduce_min'
             self.attrs = {
                 'use_xpu': True,
                 'reduce_all': self.reduce_all,
@@ -46,10 +46,10 @@ class XPUTestReduceMaxOp(XPUOpTestWrapper):
             }
             self.inputs = {'X': np.random.random(self.shape).astype("float32")}
             if self.attrs['reduce_all']:
-                self.outputs = {'Out': self.inputs['X'].max()}
+                self.outputs = {'Out': self.inputs['X'].min()}
             else:
                 self.outputs = {
-                    'Out': self.inputs['X'].max(axis=self.axis,
+                    'Out': self.inputs['X'].min(axis=self.axis,
                                                 keepdims=self.attrs['keep_dim'])
                 }
 
@@ -65,7 +65,7 @@ class XPUTestReduceMaxOp(XPUOpTestWrapper):
         def test_check_grad(self):
             pass
 
-    class XPUTestReduceSumCase1(XPUTestReduceMaxBase):
+    class XPUTestReduceSumCase1(XPUTestReduceMinBase):
         def init_case(self):
             self.shape = (5, 6, 10)
             self.axis = (0, )
@@ -73,9 +73,9 @@ class XPUTestReduceMaxOp(XPUOpTestWrapper):
             self.keep_dim = True
 
 
-support_types = get_xpu_op_support_types('reduce_max')
+support_types = get_xpu_op_support_types('reduce_min')
 for stype in support_types:
-    create_test_class(globals(), XPUTestReduceMaxOp, stype)
+    create_test_class(globals(), XPUTestReduceMinOp, stype)
 
 if __name__ == '__main__':
     unittest.main()
