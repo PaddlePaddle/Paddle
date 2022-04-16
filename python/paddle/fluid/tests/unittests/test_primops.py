61cc5766ce4ff7b2034a7122556c956063d8feec
levi131 <limaolin01@baidu.com>
2022-04-16 09:13:55 +0000

    50% for adam.minimize

--- python/paddle/fluid/tests/unittests/test_primops.py
+++ python/paddle/fluid/tests/unittests/test_primops.py
@@ -22,6 +22,8 @@ from paddle.autograd.primops import (
     fill_const)
 from paddle.autograd.primx import Transform, topo_path, orig2prim, prim2orig
 
+from paddle.autograd.new_adam_optimizer import AdamOptimizer
+
 
 class TestPyPrimOps(unittest.TestCase):
     """ Test Python wrappers of primitive ops. """
@@ -195,6 +197,30 @@ class TestPyPrimOps(unittest.TestCase):
 
             print(f'-------test_orig2prim: orig-------')
             print(x.block)
+
+    # def test_minimize(self):
+    #     place = paddle.CUDAPlace(0)
+    #     exe = paddle.static.Executor(place)
+    #     main = paddle.static.Program()
+    #     startup = paddle.static.Program()
+    #     with paddle.static.program_guard(main, startup):
+    #         x = paddle.static.data('x', shape=[2, 20], dtype='float32')
+    #         x.stop_gradient = False
+    #         w = paddle.static.create_parameter(
+    #             shape=[20, 2], dtype='float32', is_bias=False)
+    #         bias = paddle.static.create_parameter(
+    #             shape=[2], dtype='float32', is_bias=True)
+    #         y = paddle.tanh(paddle.matmul(x, w) + bias)
+    #         loss = paddle.sum(y)
+    #         opt = AdamOptimizer(0.01)
+    #         opt.minimize(loss)
+    #         # prim2orig(x.block, update_var_list=[loss])
+
+    #         # print(f'-------test_minimize: orig-------')
+    #         # print(x.block)
+    #     # exe.run(startup)
+    #     # for i in range(10):
+    #     #     y_d = exe.run(main, feed = {'x': np.rand((2, 20))}, fetch_list=[y.name])
 
 
 if __name__ == '__main__':
