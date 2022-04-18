f06dcad6af019b7c4cd10d6e081310a61a8b0b5c
phlrain <phliuhongyu@126.com>
2022-04-18 01:15:13 +0000

    remove useless code

--- paddle/phi/kernels/gpudnn/conv_kernel.cu
+++ paddle/phi/kernels/gpudnn/conv_kernel.cu
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <algorithm>
-#include <map>
-
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/kernels/conv_kernel.h"
 
