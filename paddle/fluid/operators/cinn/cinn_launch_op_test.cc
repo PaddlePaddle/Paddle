2cb19d8fdc9651c2d3f9250163412b1a063c11aa
YuanRisheng <yuanrisheng@baidu.com>
2022-04-19 11:53:16 +0800

    [Phi]Separate AddKernel/DivideKernel/SubtractKernel/MultiplyKernel from ElementwiseKernel（Part1） (#41806)
    * seperate add/div/sub/mul from elementwise
    
    * delete code
    
    * fix compile bugs
    
    * deal with conflict
    
    * fix bugs when compile
    
    * fix windows unit test bug
    
    * fix ci converage bugs

--- paddle/fluid/operators/cinn/cinn_launch_op_test.cc
+++ paddle/fluid/operators/cinn/cinn_launch_op_test.cc
@@ -36,7 +36,7 @@ DECLARE_bool(enable_pe_launch_cinn);
 
 PD_DECLARE_KERNEL(add, CPU, ALL_LAYOUT);
 #ifdef PADDLE_WITH_CUDA
-PD_DECLARE_KERNEL(add, GPU, ALL_LAYOUT);
+PD_DECLARE_KERNEL(add, KPS, ALL_LAYOUT);
 #endif
 
 namespace paddle::operators {
