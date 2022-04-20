ef78c9c2807cb9ba56dc403fff3cf5b72540ebee
seemingwang <seemingwang@users.noreply.github.com>
2022-04-20 20:07:17 +0800

    cherry pick recent updates in graph-engine to release2.3 (#42027)
    * gpu_graph engine optimization+ (#41455)
    
    * extract sub-graph
    
    * graph-engine merging
    
    * fix
    
    * fix
    
    * fix heter-ps config
    
    * test performance
    
    * test performance
    
    * test performance
    
    * test
    
    * test
    
    * update bfs
    
    * change cmake
    
    * test
    
    * test gpu speed
    
    * gpu_graph_engine optimization
    
    * add ssd layer to graph_engine
    
    * fix allocation
    
    * fix syntax error
    
    * fix syntax error
    
    * fix pscore class
    
    * fix
    
    * recover test
    
    * recover test
    
    * fix spelling
    
    * recover
    
    * fix
    
    * Cpu gpu graph engine (#41942)
    
    * extract sub-graph
    
    * graph-engine merging
    
    * fix
    
    * fix
    
    * fix heter-ps config
    
    * test performance
    
    * test performance
    
    * test performance
    
    * test
    
    * test
    
    * update bfs
    
    * change cmake
    
    * test
    
    * test gpu speed
    
    * gpu_graph_engine optimization
    
    * add ssd layer to graph_engine
    
    * fix allocation
    
    * fix syntax error
    
    * fix syntax error
    
    * fix pscore class
    
    * fix
    
    * recover test
    
    * recover test
    
    * fix spelling
    
    * recover
    
    * fix
    
    * fix linking problem
    
    * remove comment

--- paddle/fluid/framework/fleet/heter_ps/heter_comm.h
+++ paddle/fluid/framework/fleet/heter_ps/heter_comm.h
@@ -211,11 +211,11 @@ class HeterComm {
   std::vector<std::vector<Path>> path_;
   float load_factor_{0.75};
   int block_size_{256};
+  std::unique_ptr<HeterCommKernel> heter_comm_kernel_;
 
  private:
-  std::unique_ptr<HeterCommKernel> heter_comm_kernel_;
-  std::vector<LocalStorage> storage_;
   int topo_aware_{0};
+  std::vector<LocalStorage> storage_;
   int feanum_{1800 * 2048};
   int multi_node_{0};
   int node_size_;
