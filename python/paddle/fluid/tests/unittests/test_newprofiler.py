a8600fd6c9c613a31884a9e1cbc8ad44009b92a9
chenjian <chenjian26@baidu.com>
2022-04-21 16:06:32 +0000

    improve coverage

--- python/paddle/fluid/tests/unittests/test_newprofiler.py
+++ python/paddle/fluid/tests/unittests/test_newprofiler.py
@@ -152,6 +152,24 @@ class TestProfiler(unittest.TestCase):
             simple_net.clear_gradients()
             prof.step()
         prof.stop()
+        prof.summary()
+        prof = None
+        dataset = RandomDataset(10 * 4)
+        simple_net = SimpleNet()
+        loader = DataLoader(dataset, batch_size=4, shuffle=True, drop_last=True)
+        opt = paddle.optimizer.Adam(
+            learning_rate=1e-3, parameters=simple_net.parameters())
+        prof = profiler.Profiler(on_trace_ready=lambda prof: None)
+        prof.start()
+        for i, (image, label) in enumerate(loader()):
+            out = simple_net(image)
+            loss = F.cross_entropy(out, label)
+            avg_loss = paddle.mean(loss)
+            avg_loss.backward()
+            opt.step()
+            simple_net.clear_gradients()
+            prof.step()
+        prof.stop()
 
 
 class TestNvprof(unittest.TestCase):
