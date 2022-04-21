56774d9d4760699a44db3a97996088d5a7aa2498
zkh2016 <zhangkaihuo@baidu.com>
2022-04-21 10:22:47 +0000

    fix examples

--- python/paddle/sparse/creation.py
+++ python/paddle/sparse/creation.py
@@ -111,7 +111,7 @@ def sparse_coo_tensor(indices,
         with _test_eager_guard():
             indices = [[0, 1, 2], [1, 2, 0]]
             values = [1.0, 2.0, 3.0]
-            dense_shape = [2, 3]
+            dense_shape = [3, 3]
             coo = paddle.sparse.sparse_coo_tensor(indices, values, dense_shape)
             # print(coo)
             # Tensor(shape=[2, 3], dtype=paddle.float32, place=Place(gpu:0), stop_gradient=True,
