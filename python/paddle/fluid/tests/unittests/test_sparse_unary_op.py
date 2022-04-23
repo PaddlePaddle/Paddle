f59fa26ea11278305819ece6f4c2aa1aea69450b
tiancaishaonvjituizi <452565578@qq.com>
2022-04-23 16:34:11 +0800

    add test about error, reformat

--- python/paddle/fluid/tests/unittests/test_sparse_unary_op.py
+++ python/paddle/fluid/tests/unittests/test_sparse_unary_op.py
@@ -22,32 +22,34 @@ from paddle import _C_ops
 
 
 class TestSparseUnary(unittest.TestCase):
+    def assert_raises_on_dense_tensor(self, sparse_func):
+        dense_x = paddle.ones((2, 3))
+        with self.assertRaises(ValueError):
+            sparse_func(dense_x)
+
     def compare_with_dense(
-        self,
-        x,
-        to_sparse: Callable[[paddle.Tensor], paddle.Tensor],
-        dense_func: Callable[[paddle.Tensor], paddle.Tensor],
-        sparse_func: Callable[[paddle.Tensor], paddle.Tensor],
-        test_gradient: bool,
-    ):
-        def tensor_allclose(dense_tensor: paddle.Tensor, sparse_tensor: paddle.Tensor):
+            self,
+            x,
+            to_sparse: Callable[[paddle.Tensor], paddle.Tensor],
+            dense_func: Callable[[paddle.Tensor], paddle.Tensor],
+            sparse_func: Callable[[paddle.Tensor], paddle.Tensor],
+            test_gradient: bool, ):
+        def tensor_allclose(dense_tensor: paddle.Tensor,
+                            sparse_tensor: paddle.Tensor):
             dense_numpy = dense_tensor.numpy()
             mask = ~np.isnan(dense_numpy)
-            return np.allclose(
-                dense_numpy[mask], sparse_tensor.to_dense().numpy()[mask]
-            )
+            return np.allclose(dense_numpy[mask],
+                               sparse_tensor.to_dense().numpy()[mask])
 
         with _test_eager_guard():
             dense_x = paddle.to_tensor(
-                x, dtype="float32", stop_gradient=not test_gradient
-            )
+                x, dtype="float32", stop_gradient=not test_gradient)
 
             sparse_x = to_sparse(dense_x)
             sparse_out = sparse_func(sparse_x)
 
             dense_x = paddle.to_tensor(
-                x, dtype="float32", stop_gradient=not test_gradient
-            )
+                x, dtype="float32", stop_gradient=not test_gradient)
             dense_out = dense_func(dense_x)
 
             assert tensor_allclose(dense_out, sparse_out)
@@ -65,15 +67,14 @@ class TestSparseUnary(unittest.TestCase):
             lambda x: x.to_sparse_coo(sparse_dim),
             paddle.nn.ReLU(),
             paddle.sparse.ReLU(),
-            True,
-        )
+            True, )
         self.compare_with_dense(
             x,
             lambda x: x.to_sparse_csr(),
             paddle.nn.ReLU(),
             paddle.sparse.ReLU(),
-            False,
-        )
+            False, )
+        self.assert_raises_on_dense_tensor(paddle.sparse.ReLU())
 
     def test_sparse_sqrt(self):
         x = [[0, 16, 0, 0], [0, 0, 0, 0], [0, 4, 2, 0]]
@@ -83,15 +84,14 @@ class TestSparseUnary(unittest.TestCase):
             lambda x: x.to_sparse_coo(sparse_dim),
             paddle.sqrt,
             paddle.sparse.functional.sqrt,
-            True,
-        )
+            True, )
         self.compare_with_dense(
             x,
             lambda x: x.to_sparse_csr(),
             paddle.sqrt,
             paddle.sparse.functional.sqrt,
-            False,
-        )
+            False, )
+        self.assert_raises_on_dense_tensor(paddle.sparse.functional.sqrt)
 
     def test_sparse_sin(self):
         x = [[0, 16, 0, 0], [0, 0, 0, 0], [0, 4, 2, 0]]
@@ -101,15 +101,14 @@ class TestSparseUnary(unittest.TestCase):
             lambda x: x.to_sparse_coo(sparse_dim),
             paddle.sin,
             paddle.sparse.functional.sin,
-            True,
-        )
+            True, )
         self.compare_with_dense(
             x,
             lambda x: x.to_sparse_csr(),
             paddle.sin,
             paddle.sparse.functional.sin,
-            False,
-        )
+            False, )
+        self.assert_raises_on_dense_tensor(paddle.sparse.functional.sin)
 
     def test_sparse_tanh(self):
         x = [[0, 16, 0, 0], [0, 0, 0, 0], [0, -4, 2, 0]]
@@ -119,15 +118,15 @@ class TestSparseUnary(unittest.TestCase):
             lambda x: x.to_sparse_coo(sparse_dim),
             paddle.tanh,
             paddle.sparse.functional.tanh,
-            True,
-        )
+            True, )
         self.compare_with_dense(
             x,
             lambda x: x.to_sparse_csr(),
             paddle.tanh,
             paddle.sparse.functional.tanh,
-            False,
-        )
+            False, )
+        self.assert_raises_on_dense_tensor(paddle.sparse.functional.tanh)
+
 
 if __name__ == "__main__":
     unittest.main()
