763150262cfde8f42869dd2dc40bac8735714af4
tiancaishaonvjituizi <452565578@qq.com>
2022-04-23 23:59:25 +0800

    fix conflict with master

--- paddle/phi/core/compat/arg_map_context.h
+++ paddle/phi/core/compat/arg_map_context.h
@@ -59,7 +59,10 @@ struct KernelSignature {
   // TODO(chenweihang): add assign constructor to solve windows compile
   // problem, remove it later
   KernelSignature(const KernelSignature& other)
-      : name(other.name), args(other.args) {}
+      : name(other.name),
+        input_names(other.input_names),
+        attr_names(other.attr_names),
+        output_names(other.output_names) {}
   KernelSignature& operator=(const KernelSignature& other) {
     name = other.name;
     input_names = other.input_names;
