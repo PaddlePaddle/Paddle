3a9efa3d8f95810075df142866847e73f3a96b19
tianshuo78520a <707759223@qq.com>
2022-04-23 14:15:24 +0800

    test=document_fix

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3087,6 +3087,7 @@ function main() {
         check_diff_file_for_coverage
         cmake_gen_and_build ${PYTHON_ABI:-""} ${parallel_number}
         enable_unused_var_check
+
         #check_coverage_build
         ;;
       gpu_cicheck_coverage)
