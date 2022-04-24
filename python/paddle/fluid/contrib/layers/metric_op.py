f9a4ca5bc55a558682b5ab793c7c44d35ee29654
wangzhen38 <wangzhen38@baidu.com>
2022-04-24 06:49:53 +0000

    update metric for real data

--- python/paddle/fluid/contrib/layers/metric_op.py
+++ python/paddle/fluid/contrib/layers/metric_op.py
@@ -155,6 +155,16 @@ def ctr_metric_bundle(input, label, ins_tag_weight=None):
         type="sigmoid",
         inputs={"X": [input]},
         outputs={"Out": [tmp_res_sigmoid]})
+
+    #if data is fake, return 0
+    axis = helper.kwargs.get('axis', 0)
+    helper.append_op(
+        type="elementwise_mul",
+        inputs={"X": [tmp_res_sigmoid],
+                "Y": [ins_tag_weight]},
+        outputs={"Out": [tmp_res_sigmoid]},
+        attrs={'axis': axis})
+
     helper.append_op(
         type="reduce_sum",
         inputs={"X": [tmp_res_sigmoid]},
@@ -188,22 +198,20 @@ def ctr_metric_bundle(input, label, ins_tag_weight=None):
         type="reduce_sum",
         inputs={"X": [tmp_ones]},
         outputs={"Out": [batch_ins_num]})
+
+    #if data is fake, return 0
+    axis = helper.kwargs.get('axis', 0)
+    helper.append_op(
+        type="elementwise_mul",
+        inputs={"X": [batch_ins_num],
+                "Y": [ins_tag_weight]},
+        outputs={"Out": [batch_ins_num]},
+        attrs={'axis': axis})
+
     helper.append_op(
         type="elementwise_add",
         inputs={"X": [batch_ins_num],
                 "Y": [local_ins_num]},
         outputs={"Out": [local_ins_num]})
-
-    #if data is fake, return 0
-    helper.append_op(
-        type="elementwise_mul",
-        inputs={"X": [ins_tag_weight],
-                "Y": [local_ins_num]},
-        outputs={"Out": [local_ins_num]})
-    helper.append_op(
-        type="elementwise_mul",
-        inputs={"X": [ins_tag_weight],
-                "Y": [local_q]},
-        outputs={"Out": [local_q]})
 
     return local_sqrerr, local_abserr, local_prob, local_q, local_pos_num, local_ins_num
