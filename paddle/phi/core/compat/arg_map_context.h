5c06947efbeaa1b716afc1050f923df99b781764
zyfncg <zhangyunfei07@baidu.com>
2022-04-27 10:09:51 +0800

    Add move construct for KernelSignature (#42253)
    * add move construct for KernelSignature
    
    * add noexcept

--- paddle/phi/core/compat/arg_map_context.h
+++ paddle/phi/core/compat/arg_map_context.h
@@ -58,11 +58,31 @@ struct KernelSignature {
 
   // TODO(chenweihang): add assign constructor to solve windows compile
   // problem, remove it later
+  KernelSignature(const KernelSignature& other)
+      : name(other.name),
+        input_names(other.input_names),
+        attr_names(other.attr_names),
+        output_names(other.output_names) {}
+
+  KernelSignature(KernelSignature&& other) noexcept
+      : name(other.name),
+        input_names(std::move(other.input_names)),
+        attr_names(std::move(other.attr_names)),
+        output_names(std::move(other.output_names)) {}
+
   KernelSignature& operator=(const KernelSignature& other) {
     name = other.name;
     input_names = other.input_names;
     attr_names = other.attr_names;
     output_names = other.output_names;
+    return *this;
+  }
+
+  KernelSignature& operator=(KernelSignature&& other) noexcept {
+    name = other.name;
+    input_names.swap(other.input_names);
+    attr_names.swap(other.attr_names);
+    output_names.swap(other.output_names);
     return *this;
   }
 };
