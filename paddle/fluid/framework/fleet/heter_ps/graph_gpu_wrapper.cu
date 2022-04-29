878c38fe3c2271cc3b9547a87a5a8f17f88a07bb
seemingwang <seemingwang@users.noreply.github.com>
2022-04-29 14:08:33 +0800

    enable graph-engine to return all id (#42319)
    * enable graph-engine to return all id
    
    * change vector's dimension
    
    * change vector's dimension
    
    * enlarge returned ids dimensions

--- paddle/fluid/framework/fleet/heter_ps/graph_gpu_wrapper.cu
+++ paddle/fluid/framework/fleet/heter_ps/graph_gpu_wrapper.cu
@@ -58,6 +58,11 @@ void GraphGpuWrapper::set_device(std::vector<int> ids) {
     device_id_mapping.push_back(device_id);
   }
 }
+std::vector<std::vector<int64_t>> GraphGpuWrapper::get_all_id(int type, int idx,
+                                                              int slice_num) {
+  return ((GpuPsGraphTable *)graph_table)
+      ->cpu_graph_table->get_all_id(type, idx, slice_num);
+}
 void GraphGpuWrapper::set_up_types(std::vector<std::string> &edge_types,
                                    std::vector<std::string> &node_types) {
   id_to_edge = edge_types;
