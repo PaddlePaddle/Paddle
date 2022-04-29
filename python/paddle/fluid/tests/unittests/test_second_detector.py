17381386d3c4bd8ed590559877556f0cf565512c
zkh2016 <zhangkaihuo@baidu.com>
2022-04-29 08:50:33 +0000

    opt the test

--- python/paddle/fluid/tests/unittests/test_second_detector.py
+++ python/paddle/fluid/tests/unittests/test_second_detector.py
@@ -18,68 +18,39 @@ class MiddleExtractor(paddle.nn.Layer):
                 name='MiddleExtractor'):
         super(MiddleExtractor, self).__init__()
         self.name = name
-        self.middle_conv = paddle.nn.Sequential(
-            #nn.Pad3D(1),
-            nn.Conv3D(num_input_features, 64, 3, stride=(2, 1, 1), data_format='NDHWC'),
-            #nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
-            nn.ReLU(),
-            #nn.Pad3D([1, 1, 1, 1, 0, 0]),
-            nn.Conv3D(64, 64, 3, stride=(1, 1, 1), data_format='NDHWC'),
-            #nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
-            nn.ReLU(),
-            #nn.Pad3D(1),
-            nn.Conv3D(64, 64, 3, stride=(2, 1, 1), data_format='NDHWC'),
-            #nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
-            nn.ReLU(),
-        )
+        if not use_norm:
+            self.middle_conv = paddle.nn.Sequential(
+                #nn.Pad3D(1),
+                nn.Conv3D(num_input_features, 64, 3, stride=(2, 1, 1), data_format='NDHWC'),
+                #nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
+                nn.ReLU(),
+                #nn.Pad3D([1, 1, 1, 1, 0, 0]),
+                nn.Conv3D(64, 64, 3, stride=(1, 1, 1), data_format='NDHWC'),
+                #nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
+                nn.ReLU(),
+                #nn.Pad3D(1),
+                nn.Conv3D(64, 64, 3, stride=(2, 1, 1), data_format='NDHWC'),
+                #nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
+                nn.ReLU(),
+            )
+        else:
+            self.middle_conv = paddle.nn.Sequential(
+                #nn.Pad3D(1),
+                nn.Conv3D(num_input_features, 64, 3, stride=(2, 1, 1), data_format='NDHWC'),
+                nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
+                nn.ReLU(),
+                #nn.Pad3D([1, 1, 1, 1, 0, 0]),
+                nn.Conv3D(64, 64, 3, stride=(1, 1, 1), data_format='NDHWC'),
+                nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
+                nn.ReLU(),
+                #nn.Pad3D(1),
+                nn.Conv3D(64, 64, 3, stride=(2, 1, 1), data_format='NDHWC'),
+                nn.BatchNorm3D(64, epsilon=1e-3, momentum=0.001, data_format='NDHWC'),
+                nn.ReLU(),
+            )
     def forward(self, x):
         return self.middle_conv(x)
 
-
-class SparseMiddleExtractor(paddle.nn.Layer):
-    def __init__(self,
-                #output_shape,
-                use_norm=True,
-                num_input_features=128,
-                num_filters_down1=[64],
-                num_filters_down2=[64, 64],
-                name='SparseMiddleExtractor'):
-        super(SparseMiddleExtractor, self).__init__()
-        self.name = name
-
-        middle_layers = []
-        num_filters = [num_input_features] + num_filters_down1
-        filters_pairs_d1 = [[num_filters[i], num_filters[i + 1]] for i in range(len(num_filters) - 1)]
-        for i, o in filters_pairs_d1:
-            middle_layers.append(sparse.SubmConv3D(i, o, 3, bias_attr=False))
-            #middle_layers.append(sparse.BatchNorm(o, epsilon=1e-3, momentum=0.99))
-            middle_layers.append(sparse.ReLU())
-
-        middle_layers.append(sparse.Conv3D(num_filters[-1], num_filters[-1], (3, 1, 1), (2, 1, 1), bias_attr=False))
-        #middle_layers.append(sparse.BatchNorm(num_filters[-1], epsilon=1e-3, momentum=0.99))
-        middle_layers.append(sparse.ReLU())
-
-        if len(num_filters_down1) == 0:
-            num_filters = [num_filters[-1]] + num_filters_down2
-        else:
-            num_filters = [num_filters_down1[-1]] + num_filters_down2
-
-        filters_pairs_d2 = [[num_filters[i], num_filters[i + 1]] for i in range(len(num_filters) - 1)]
-
-        for i, o in filters_pairs_d2:
-            middle_layers.append(sparse.SubmConv3D(i, o, 3, bias_attr=False))
-            #middle_layers.append(sparse.BatchNorm(o, epsilon=1e-3, momentum=0.99))
-            middle_layers.append(sparse.ReLU())
-
-        middle_layers.append(sparse.Conv3D(num_filters[-1], num_filters[-1], (3, 1, 1), (2, 1, 1), bias_attr=False))
-        #middle_layers.append(sparse.BatchNorm(num_filters[-1], epsilon=1e-3, momentum=0.99))
-        middle_layers.append(sparse.ReLU())
-
-        self.middle_conv = nn.Sequential(*middle_layers)
-        
-    def forward(self, x):
-        sparse_out = self.middle_conv(x)
-        return sparse_out.to_dense()
 
 def get_pos_to_kw_map(func):
     pos_to_kw = {}
@@ -106,6 +77,17 @@ def change_default_args(**kwargs):
 
     return layer_wrapper
 
+class Empty(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super(Empty, self).__init__()
+
+    def forward(self, *args, **kwargs):
+        if len(args) == 1:
+            return args[0]
+        elif len(args) == 0:
+            return None
+        return args
+
 class SpconvMiddleExtractor(torch.nn.Module):
     def __init__(self,
                 #output_shape,
@@ -131,7 +113,8 @@ class SpconvMiddleExtractor(torch.nn.Module):
 
         for i, o in filters_pairs_d1:
             middle_layers.append(spconv.SubMConv3d(i, o, 3, bias=False))
-            #middle_layers.append(BatchNorm1d(o))
+            if use_norm:
+                middle_layers.append(BatchNorm1d(o))
             middle_layers.append(torch.nn.ReLU())
 
         middle_layers.append(
@@ -139,8 +122,9 @@ class SpconvMiddleExtractor(torch.nn.Module):
                 num_filters[-1],
                 num_filters[-1], (3, 1, 1), (2, 1, 1),
                 bias=False))
-        #middle_layers.append(
-        #    BatchNorm1d(num_filters[-1]))
+        if use_norm:
+            middle_layers.append(
+                BatchNorm1d(num_filters[-1]))
         middle_layers.append(torch.nn.ReLU())
         # assert len(num_filters_down2) > 0
         if len(num_filters_down1) == 0:
@@ -151,15 +135,17 @@ class SpconvMiddleExtractor(torch.nn.Module):
                             for i in range(len(num_filters) - 1)]
         for i, o in filters_pairs_d2:
             middle_layers.append(spconv.SubMConv3d(i, o, 3, bias=False))
-            #middle_layers.append(BatchNorm1d(o))
+            if use_norm:
+                middle_layers.append(BatchNorm1d(o))
             middle_layers.append(torch.nn.ReLU())
         middle_layers.append(
             spconv.SparseConv3d(
                 num_filters[-1],
                 num_filters[-1], (3, 1, 1), (2, 1, 1),
                 bias=False))
-        #middle_layers.append(
-        #    BatchNorm1d(num_filters[-1]))
+        if use_norm:
+            middle_layers.append(
+                BatchNorm1d(num_filters[-1]))
         middle_layers.append(torch.nn.ReLU())
         #middle_layers.append(scn.SparseToDense(3, num_filters[-1]))
         middle_layers.append(spconv.ToDense())
@@ -169,12 +155,62 @@ class SpconvMiddleExtractor(torch.nn.Module):
         out = self.middle_conv(x)
         return out
 
+class SparseMiddleExtractor(paddle.nn.Layer):
+    def __init__(self,
+                #output_shape,
+                use_norm=True,
+                num_input_features=128,
+                num_filters_down1=[64],
+                num_filters_down2=[64, 64],
+                name='SparseMiddleExtractor'):
+        super(SparseMiddleExtractor, self).__init__()
+        self.name = name
+
+        middle_layers = []
+        num_filters = [num_input_features] + num_filters_down1
+        filters_pairs_d1 = [[num_filters[i], num_filters[i + 1]] for i in range(len(num_filters) - 1)]
+        for i, o in filters_pairs_d1:
+            middle_layers.append(sparse.SubmConv3D(i, o, 3, bias_attr=False))
+            if use_norm:
+                middle_layers.append(sparse.BatchNorm(o, epsilon=1e-3, momentum=0.99))
+            middle_layers.append(sparse.ReLU())
+
+        middle_layers.append(sparse.Conv3D(num_filters[-1], num_filters[-1], (3, 1, 1), (2, 1, 1), bias_attr=False))
+        if use_norm:
+            middle_layers.append(sparse.BatchNorm(num_filters[-1], epsilon=1e-3, momentum=0.99))
+        middle_layers.append(sparse.ReLU())
+
+        if len(num_filters_down1) == 0:
+            num_filters = [num_filters[-1]] + num_filters_down2
+        else:
+            num_filters = [num_filters_down1[-1]] + num_filters_down2
+
+        filters_pairs_d2 = [[num_filters[i], num_filters[i + 1]] for i in range(len(num_filters) - 1)]
+
+        for i, o in filters_pairs_d2:
+            middle_layers.append(sparse.SubmConv3D(i, o, 3, bias_attr=False))
+            if use_norm:
+                middle_layers.append(sparse.BatchNorm(o, epsilon=1e-3, momentum=0.99))
+            middle_layers.append(sparse.ReLU())
+
+        middle_layers.append(sparse.Conv3D(num_filters[-1], num_filters[-1], (3, 1, 1), (2, 1, 1), bias_attr=False))
+        if use_norm:
+            middle_layers.append(sparse.BatchNorm(num_filters[-1], epsilon=1e-3, momentum=0.99))
+        middle_layers.append(sparse.ReLU())
+
+        self.middle_conv = nn.Sequential(*middle_layers)
+        
+    def forward(self, x):
+        sparse_out = self.middle_conv(x)
+        return sparse_out.to_dense()
+
+
 def test():
     with _test_eager_guard():
         in_channels = 128 
-        shape = [20, 200, 352]
+        shape = [20, 400, 352]
         batch_size = 1
-        sparsity = 0.95
+        sparsity = 0.9
 
         full_shape = [batch_size] + shape + [in_channels]
         print(full_shape)
@@ -202,9 +238,17 @@ def test():
 
         spconv_x = spconv.SparseConvTensor.from_dense(torch_x)
 
-        dense_model = MiddleExtractor(num_input_features=in_channels)
-        sparse_model = SparseMiddleExtractor(num_input_features=in_channels)
-        spconv_model = SpconvMiddleExtractor(num_input_features=in_channels).to(device)
+        use_norm = False
+        dense_model = MiddleExtractor(use_norm=use_norm, num_input_features=in_channels)
+        spconv_model = SpconvMiddleExtractor(use_norm=use_norm, num_input_features=in_channels).to(device)
+        sparse_model = SparseMiddleExtractor(use_norm=use_norm, num_input_features=in_channels)
+        layer_nums = len(sparse_model.middle_conv)
+        block_size = 3 if use_norm else 2
+        layer_nums = int(layer_nums / block_size)
+
+        for i in range(0, layer_nums):
+            weight = paddle.to_tensor(spconv_model.middle_conv[i * block_size].weight.detach().cpu().numpy())
+            sparse_model.middle_conv[i * block_size].weight.set_value(paddle.transpose(weight, [1,2,3,4,0]))
 
         print(dense_model)
         print(sparse_model)
@@ -219,6 +263,7 @@ def test():
         paddle.device.cuda.synchronize()
         out3 = spconv_model(spconv_x)
         torch.cuda.synchronize(device)
+        #warm up
 
         iters = 1
         t0 = time.time()
@@ -247,11 +292,12 @@ def test():
             torch.cuda.synchronize(device)
         t3 = time.time()
 
+        print(out3)
         print("dense time: ", t1 - t0)
         print("sparse time: ", t2 - t1)
         print("spconv time: ", t3 - t2)
 
-        np.allclose(paddle.transpose(out2, [0, 4, 1, 2, 3]).numpy(), out3.detach().cpu().numpy(), atol=1e-3, rtol=1e-3)
-        np.allclose(spconv_x.features.grad.cpu().numpy(), sparse_x.grad.values().numpy(), atol=1e-3, rtol=1e-3)
+        assert np.allclose(paddle.transpose(out2, [0, 4, 1, 2, 3]).numpy(), out3.detach().cpu().numpy(), atol=1e-3, rtol=1e-3)
+        #np.allclose(spconv_x.features.grad.cpu().numpy(), sparse_x.grad.values().numpy(), atol=1e-3, rtol=1e-3)
 
 test()
