c125a02fb5a25234ed1ec85430ede174b530cc71
SmirnovKol <31559413+SmirnovKol@users.noreply.github.com>
2022-05-02 14:38:56 +0800

    Add files via upload

--- paddle/phi/kernels/cpu/rrelu_grad_kernel.cc
+++ paddle/phi/kernels/cpu/rrelu_grad_kernel.cc
@@ -16,29 +16,48 @@
 
 #include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/funcs/eigen/common.h"
 
 namespace phi {
 
 template <typename T, typename Context>
 void RReluGradKernel(const Context& dev_ctx,
                      const DenseTensor& x,
-                     const DenseTensor& noise,
+                     const DenseTensor& mask,
                      const DenseTensor& out_grad,
                      DenseTensor* x_grad) {
-  const T* n_ptr = noise.data<T>();
-  const T* x_ptr = x.data<T>();
-  const T* out_grad_ptr = out_grad.data<T>();
-  int numel = x.numel();
-  if (!x_grad) return;
+  // const T* n_ptr = noise.data<T>();
+  // const T* x_ptr = x.data<T>();
+  // const T* out_grad_ptr = out_grad.data<T>();
+  // int numel = x.numel();
+  // if (!x_grad) return;
 
-  int i = 0;
-  T* x_grad_ptr = dev_ctx.template Alloc<T>(x_grad);
-  for (i = 0; i < numel; i++) {
-    x_grad_ptr[i] = x_ptr[i] > 0 ? out_grad_ptr[i] : n_ptr[i] * out_grad_ptr[i];
-  }
+  // int i = 0;
+  // T* x_grad_ptr = dev_ctx.template Alloc<T>(x_grad);
+  // for (i = 0; i < numel; i++) {
+  //   x_grad_ptr[i] = x_ptr[i] > 0 ? out_grad_ptr[i] : n_ptr[i] * out_grad_ptr[i];
+  // }
+
+  x_grad->mutable_data<T>(dev_ctx.GetPlace());
+
+  auto dX = EigenVector<T>::Flatten(*x_grad);
+  auto dY = EigenVector<T>::Flatten(out_grad);
+  auto M = EigenVector<T>::Flatten(mask);
+
+  auto& place = *dev_ctx.eigen_device();
+
+  // Can the following be changed to :
+  // dX.device(place) = dY * M ;
+  // dX.device(place) = dY * M.cast<T>();
+  dX.device(place) = dY * M;
 }
 
 }  // namespace phi
 
 PD_REGISTER_KERNEL(
-    rrelu_grad, CPU, ALL_LAYOUT, phi::RReluGradKernel, float, double) {}
+    rrelu_grad, 
+    CPU, 
+    ALL_LAYOUT, 
+    phi::RReluGradKernel, 
+    float, 
+    double) {}
