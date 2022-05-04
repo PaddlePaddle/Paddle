6b5867d6a060f475f581b80b3a98113a64523b49
Guanghua Yu <742925032@qq.com>
2022-05-04 10:01:02 +0800

    fix PTQ unittest timeout (#42450)

--- python/paddle/fluid/contrib/slim/tests/test_post_training_quantization_mnist.py
+++ python/paddle/fluid/contrib/slim/tests/test_post_training_quantization_mnist.py
@@ -338,6 +338,27 @@ class TestPostTrainingmseAdaroundForMnist(TestPostTrainingQuantization):
                       infer_iterations, quant_iterations)
 
 
+class TestPostTrainingKLAdaroundForMnist(TestPostTrainingQuantization):
+    def test_post_training_kl(self):
+        model_name = "mnist_model"
+        data_url = "http://paddle-inference-dist.bj.bcebos.com/int8/mnist_model.tar.gz"
+        data_md5 = "be71d3997ec35ac2a65ae8a145e2887c"
+        algo = "KL"
+        round_type = "adaround"
+        quantizable_op_type = ["conv2d", "depthwise_conv2d", "mul"]
+        is_full_quantize = False
+        is_use_cache_file = False
+        is_optimize_model = True
+        diff_threshold = 0.01
+        batch_size = 10
+        infer_iterations = 50
+        quant_iterations = 5
+        self.run_test(model_name, data_url, data_md5, algo, round_type,
+                      quantizable_op_type, is_full_quantize, is_use_cache_file,
+                      is_optimize_model, diff_threshold, batch_size,
+                      infer_iterations, quant_iterations)
+
+
 class TestPostTrainingmseForMnistONNXFormat(TestPostTrainingQuantization):
     def test_post_training_mse_onnx_format(self):
         model_name = "mnist_model"
