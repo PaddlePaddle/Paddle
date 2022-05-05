ae4a8ba17094611071017969c646db42078d58e7
JZZ-NOTE <jzz2446071400@163.com>
2022-05-05 05:02:12 +0000

    compressed size=0

--- paddle/fluid/inference/tensorrt/test_engine.cc
+++ paddle/fluid/inference/tensorrt/test_engine.cc
@@ -11,7 +11,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-//#define _GLIBCXX_USE_CXX11_ABI 0
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 
@@ -75,207 +74,30 @@ class TensorRTEngineTest : public ::testing::Test {
   platform::CUDADeviceContext *ctx_;
 };
 
-TEST_F(TensorRTEngineTest, add_layer) {
-  const int size = 1;
-
-  float raw_weight[size] = {2.};  // Weight in CPU memory.
-  float raw_bias[size] = {3.};
-
-  std::vector<void *> buffers(2);  // TRT binded inputs
-
-  LOG(INFO) << "create weights";
-  TensorRTEngine::Weight weight(nvinfer1::DataType::kFLOAT, raw_weight, size);
-  TensorRTEngine::Weight bias(nvinfer1::DataType::kFLOAT, raw_bias, size);
-  auto *x = engine_->DeclareInput("x", nvinfer1::DataType::kFLOAT,
-                                  nvinfer1::Dims3{1, 1, 1});
-  auto *fc_layer = TRT_ENGINE_ADD_LAYER(engine_, FullyConnected, *x, size,
-                                        weight.get(), bias.get());
-  PADDLE_ENFORCE_NOT_NULL(fc_layer,
-                          platform::errors::InvalidArgument(
-                              "TRT fully connected layer building failed."));
-
-  engine_->DeclareOutput(fc_layer, 0, "y");
-  LOG(INFO) << "freeze network";
-  engine_->FreezeNetwork();
-  ASSERT_EQ(engine_->engine()->getNbBindings(), 2);
-
-  // fill in real data
-  std::vector<float> x_v = {1234};
-  std::vector<float> y_cpu;
-  PrepareInputOutput(x_v, {1});
-
-  auto *x_v_gpu_data = input_.mutable_data<float>(ctx_->GetPlace());
-  auto *y_gpu_data = output_.mutable_data<float>(ctx_->GetPlace());
-
-  buffers[0] = reinterpret_cast<void *>(x_v_gpu_data);
-  buffers[1] = reinterpret_cast<void *>(y_gpu_data);
-
-  LOG(INFO) << "Set attr";
-  engine_->Set("test_attr", new std::string("test_attr"));
-  if (engine_->Has("test_attr")) {
-    auto attr_val = engine_->Get<std::string>("test_attr");
-    engine_->Erase("test_attr");
-  }
-  std::string *attr_key = new std::string("attr_key");
-  engine_->SetNotOwned("attr1", attr_key);
-
-  LOG(INFO) << "to execute";
-  engine_->Execute(1, &buffers, ctx_->stream());
-
-  LOG(INFO) << "to get output";
-  GetOutput(&y_cpu);
-
-  LOG(INFO) << "to checkout output";
-  ASSERT_EQ(y_cpu[0], x_v[0] * 2 + 3);
-
-  delete attr_key;
-}
-
-TEST_F(TensorRTEngineTest, add_layer_multi_dim) {
-  // Weight in CPU memory.
-  // It seems tensorrt FC use col-major: [[1.0, 3.3], [1.1, 4.4]]
-  // instead of row-major, which is [[1.0, 1.1], [3.3, 4.4]]
-  float raw_weight[4] = {1.0, 1.1, 3.3, 4.4};
-  float raw_bias[2] = {1.3, 2.4};
-  std::vector<void *> buffers(2);  // TRT binded inputs
-
-  TensorRTEngine::Weight weight(nvinfer1::DataType::kFLOAT, raw_weight, 4);
-  TensorRTEngine::Weight bias(nvinfer1::DataType::kFLOAT, raw_bias, 2);
-  auto *x = engine_->DeclareInput("x", nvinfer1::DataType::kFLOAT,
-                                  nvinfer1::Dims3{1, 2, 1});
-  auto *fc_layer = TRT_ENGINE_ADD_LAYER(engine_, FullyConnected, *x, 2,
-                                        weight.get(), bias.get());
-  PADDLE_ENFORCE_NOT_NULL(fc_layer,
-                          platform::errors::InvalidArgument(
-                              "TRT fully connected layer building failed."));
-
-  engine_->DeclareOutput(fc_layer, 0, "y");
-  engine_->FreezeNetwork();
-  ASSERT_EQ(engine_->engine()->getNbBindings(), 2);
-
-  // fill in real data
-  std::vector<float> x_v = {1.0, 2.0};
-  std::vector<float> y_cpu;
-  PrepareInputOutput(x_v, {2});
-
-  auto *x_v_gpu_data = input_.mutable_data<float>(ctx_->GetPlace());
-  auto *y_gpu_data = output_.mutable_data<float>(ctx_->GetPlace());
-
-  buffers[0] = reinterpret_cast<void *>(x_v_gpu_data);
-  buffers[1] = reinterpret_cast<void *>(y_gpu_data);
-
-  engine_->Execute(1, &buffers, ctx_->stream());
-
-  LOG(INFO) << "to get output";
-  GetOutput(&y_cpu);
-
-  auto dims = engine_->GetITensor("y")->getDimensions();
-  ASSERT_EQ(dims.nbDims, 3);
-  ASSERT_EQ(dims.d[0], 2);
-  ASSERT_EQ(dims.d[1], 1);
-
-  ASSERT_EQ(y_cpu[0], 4.5);
-  ASSERT_EQ(y_cpu[1], 14.5);
-}
-
-TEST_F(TensorRTEngineTest, test_conv2d) {
-  // Weight in CPU memory.
-  float raw_weight[9] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-  float raw_bias[1] = {0};
-  std::vector<void *> buffers(2);  // TRT binded inputs
-
-  TensorRTEngine::Weight weight(nvinfer1::DataType::kFLOAT, raw_weight, 9);
-  TensorRTEngine::Weight bias(nvinfer1::DataType::kFLOAT, raw_bias, 1);
-  auto *x = engine_->DeclareInput("x", nvinfer1::DataType::kFLOAT,
-                                  nvinfer1::Dims3{1, 3, 3});
-  auto *conv_layer =
-      TRT_ENGINE_ADD_LAYER(engine_, Convolution, *x, 1, nvinfer1::DimsHW{3, 3},
-                           weight.get(), bias.get());
-  PADDLE_ENFORCE_NOT_NULL(conv_layer,
-                          platform::errors::InvalidArgument(
-                              "TRT convolution layer building failed."));
-  conv_layer->setStride(nvinfer1::DimsHW{1, 1});
-  conv_layer->setPadding(nvinfer1::DimsHW{1, 1});
-
-  engine_->DeclareOutput(conv_layer, 0, "y");
-  engine_->FreezeNetwork();
-  ASSERT_EQ(engine_->engine()->getNbBindings(), 2);
-
-  // fill in real data
-  std::vector<float> x_v = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                            1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-  std::vector<float> y_cpu;
-  PrepareInputOutput(x_v, {18});
-
-  auto *x_v_gpu_data = input_.mutable_data<float>(ctx_->GetPlace());
-  auto *y_gpu_data = output_.mutable_data<float>(ctx_->GetPlace());
-
-  buffers[0] = reinterpret_cast<void *>(x_v_gpu_data);
-  buffers[1] = reinterpret_cast<void *>(y_gpu_data);
-
-  engine_->Execute(2, &buffers, ctx_->stream());
-
-  LOG(INFO) << "to get output";
-  GetOutput(&y_cpu);
-
-  ASSERT_EQ(y_cpu[0], 4.0);
-  ASSERT_EQ(y_cpu[1], 6.0);
-}
-
-TEST_F(TensorRTEngineTest, test_pool2d) {
-  // Weight in CPU memory.
-  auto *x = engine_->DeclareInput("x", nvinfer1::DataType::kFLOAT,
-                                  nvinfer1::Dims3{1, 2, 2});
-
-  std::vector<void *> buffers(2);  // TRT binded inputs
-  nvinfer1::PoolingType pool_t = nvinfer1::PoolingType::kAVERAGE;
-  auto *pool_layer = TRT_ENGINE_ADD_LAYER(engine_, Pooling, *x, pool_t,
-                                          nvinfer1::DimsHW{2, 2});
-
-  PADDLE_ENFORCE_NOT_NULL(
-      pool_layer,
-      platform::errors::InvalidArgument("TRT pooling layer building failed."));
-  pool_layer->setStride(nvinfer1::DimsHW{1, 1});
-  pool_layer->setPadding(nvinfer1::DimsHW{0, 0});
-
-  engine_->DeclareOutput(pool_layer, 0, "y");
-  engine_->FreezeNetwork();
-  ASSERT_EQ(engine_->engine()->getNbBindings(), 2);
-
-  // fill in real data
-  std::vector<float> x_v = {1.0, 2.0, 5.0, 0.0, 2.0, 3.0, 5.0, 10.0};
-  std::vector<float> y_cpu;
-  PrepareInputOutput(x_v, {2});
-
-  auto *x_v_gpu_data = input_.mutable_data<float>(ctx_->GetPlace());
-  auto *y_gpu_data = output_.mutable_data<float>(ctx_->GetPlace());
-
-  buffers[0] = reinterpret_cast<void *>(x_v_gpu_data);
-  buffers[1] = reinterpret_cast<void *>(y_gpu_data);
-
-  engine_->Execute(2, &buffers, ctx_->stream());
-
-  LOG(INFO) << "to get output";
-  GetOutput(&y_cpu);
-
-  ASSERT_EQ(y_cpu[0], 2.0);
-  ASSERT_EQ(y_cpu[1], 5.0);
-}
-
 TEST_F(TensorRTEngineTest, test_sparse_fc) {
   // Weight in CPU memory.
-  float raw_weight[4] = {1.0, 0, 0, 4.4};
-  float raw_bias[1] = {0.0, };
+  // float raw_weight[16] = {1.0, 0, 0, 4.4,
+  //                        1.0, 0, 0, 4.4,
+  //                        1.0, 0, 0, 4.4,
+  //                        1.0, 0, 0, 4.4};
+  float raw_weight[256];
+  for (int i=0; i<64; i++) {
+    raw_weight[4*i] = 1.0;
+    raw_weight[4*i+1] = 0.0;
+    raw_weight[4*i+2] = 0.0;
+    raw_weight[4*i+3] = 4.4;
+  }
+  float raw_bias[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   std::vector<void *> buffers(2);  // TRT binded inputs
-
-  TensorRTEngine::Weight weight(nvinfer1::DataType::kFLOAT, raw_weight, 4);
-  TensorRTEngine::Weight bias(nvinfer1::DataType::kFLOAT, raw_bias, 1);
+  // builder->setMaxBatchSize(1)
+  TensorRTEngine::Weight weight(nvinfer1::DataType::kFLOAT, raw_weight, 256);
+  TensorRTEngine::Weight bias(nvinfer1::DataType::kFLOAT, raw_bias, 16);
   auto *x = engine_->DeclareInput("x", nvinfer1::DataType::kFLOAT,
-                                  nvinfer1::Dims4{4, 1, 4, 1});
+                                  nvinfer1::Dims4{16, 1, 16, 1});
 
-  plugin::SpmmPluginDynamic::Activation act = plugin::SpmmPluginDynamic::Activation::kGelu;
+  plugin::SpmmPluginDynamic::Activation act = plugin::SpmmPluginDynamic::Activation::kNone;
 
-  plugin::SpmmPluginDynamic* plugin = new plugin::SpmmPluginDynamic("CustomSpmmPluginDynamic", nvinfer1::DataType::kFLOAT, 1, weight.get(), bias.get(), act);
+  plugin::SpmmPluginDynamic* plugin = new plugin::SpmmPluginDynamic("CustomSpmmPluginDynamic", nvinfer1::DataType::kFLOAT, 16, weight.get(), bias.get(), act);
   std::vector<nvinfer1::ITensor*> plugin_inputs;
   plugin_inputs.emplace_back(x);
   auto fc_layer = engine_->network()->addPluginV2(
@@ -283,19 +105,27 @@ TEST_F(TensorRTEngineTest, test_sparse_fc) {
 
   PADDLE_ENFORCE_NOT_NULL(fc_layer,
                           platform::errors::InvalidArgument(
-                              "TRT fully connected layer building failed."));
+                              "TRT SPARSE FC layer building failed."));
 
   engine_->DeclareOutput(fc_layer, 0, "y");
   engine_->FreezeNetwork();
   ASSERT_EQ(engine_->engine()->getNbBindings(), 2);
 
   // fill in real data
-  std::vector<float> x_v = {1.0, 2.0, 3.0, 4.0,
-                            1.0, 2.0, 3.0, 4.0,
-                            1.0, 2.0, 3.0, 4.0,
-                            1.0, 2.0, 3.0, 4.0};
+  // std::vector<float> x_v = {1.0, 2.0, 3.0, 4.0,
+  //                           1.0, 2.0, 3.0, 4.0,
+  //                           1.0, 2.0, 3.0, 4.0,
+  //                           1.0, 2.0, 3.0, 4.0};
+  std::vector<float> x_v(256);
+  for (int i=0; i<64; i++) {
+    x_v[4*i] = 1.0;
+    x_v[4*i+1] = 2.0;
+    x_v[4*i+2] = 3.0;
+    x_v[4*i+3] = 4.0;
+  }
+
   std::vector<float> y_cpu;
-  PrepareInputOutput(x_v, {2});
+  PrepareInputOutput(x_v, {16});
 
   auto *x_v_gpu_data = input_.mutable_data<float>(ctx_->GetPlace());
   auto *y_gpu_data = output_.mutable_data<float>(ctx_->GetPlace());
@@ -303,14 +133,14 @@ TEST_F(TensorRTEngineTest, test_sparse_fc) {
   buffers[0] = reinterpret_cast<void *>(x_v_gpu_data);
   buffers[1] = reinterpret_cast<void *>(y_gpu_data);
 
-  engine_->Execute(4, &buffers, ctx_->stream());
+  engine_->Execute(16, &buffers, ctx_->stream());
 
   LOG(INFO) << "to get output";
   GetOutput(&y_cpu);
 
   auto dims = engine_->GetITensor("y")->getDimensions();
   ASSERT_EQ(dims.nbDims, 2);
-  ASSERT_EQ(dims.d[0], 1);
+  ASSERT_EQ(dims.d[0], 16);
 
   ASSERT_EQ(y_cpu[0], 18.6);
 }
