fd01cbf92d6a8cd991c9dbc94aff37ae644f6437
Allen Guo <alleng@graphcore.ai>
2022-05-05 14:24:21 +0800

    update UTs 2

--- python/paddle/fluid/tests/unittests/ipu/test_not_equal_op_ipu.py
+++ python/paddle/fluid/tests/unittests/ipu/test_not_equal_op_ipu.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#  Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,21 +31,23 @@ class TestBase(IPUOpTest):
         self.set_op_attrs()
 
     def set_data_feed(self):
-        x = np.random.uniform(size=[2, 5])
-        y = np.random.uniform(size=[5, 3])
-        self.feed_fp32 = {"x": x.astype(np.float32), "y": y.astype(np.float32)}
-        self.feed_fp16 = {"x": x.astype(np.float16), "y": y.astype(np.float16)}
+        x = np.ones([1, 10])
+        y = np.zeros([1, 10])
+        self.feed_fp32 = {
+            "x": x.astype(np.float32),
+            "y": y.astype(np.float32),
+        }
+        self.feed_fp16 = {
+            "x": x.astype(np.float16),
+            "y": y.astype(np.float16),
+        }
 
     def set_feed_attr(self):
         self.feed_shape = [x.shape for x in self.feed_fp32.values()]
         self.feed_list = list(self.feed_fp32.keys())
-        self.feed_dtype = [x.dtype for x in self.feed_fp32.values()]
 
     def set_op_attrs(self):
-        self.attrs = {
-            "x_num_col_dims": 1,
-            "y_num_col_dims": 1,
-        }
+        self.attrs = {}
 
     @IPUOpTest.static_graph
     def build_model(self):
@@ -53,7 +55,7 @@ class TestBase(IPUOpTest):
             name=self.feed_list[0], shape=self.feed_shape[0], dtype='float32')
         y = paddle.static.data(
             name=self.feed_list[1], shape=self.feed_shape[1], dtype='float32')
-        out = paddle.fluid.layers.mul(x, y, **self.attrs)
+        out = paddle.fluid.layers.not_equal(x, y, **self.attrs)
         self.fetch_list = [out.name]
 
     def run_model(self, exec_mode):
@@ -69,30 +71,59 @@ class TestBase(IPUOpTest):
 
 class TestCase1(TestBase):
     def set_data_feed(self):
-        x = np.random.uniform(size=[1, 2, 5])
-        y = np.random.uniform(size=[5, 3])
+        x = np.ones([1, 10])
+        y = np.ones([1, 10])
         self.feed_fp32 = {"x": x.astype(np.float32), "y": y.astype(np.float32)}
         self.feed_fp16 = {"x": x.astype(np.float16), "y": y.astype(np.float16)}
-
-    def set_op_attrs(self):
-        self.attrs = {
-            "x_num_col_dims": 2,
-            "y_num_col_dims": 1,
-        }
 
 
 class TestCase2(TestBase):
     def set_data_feed(self):
-        x = np.random.uniform(size=[3, 4, 2, 9])
-        y = np.random.uniform(size=[3, 6, 1, 2, 3])
+        x = np.ones([1, 10])
+        y = np.arange(0, 10).reshape([1, 10])
         self.feed_fp32 = {"x": x.astype(np.float32), "y": y.astype(np.float32)}
         self.feed_fp16 = {"x": x.astype(np.float16), "y": y.astype(np.float16)}
 
+
+@unittest.skipIf(not paddle.is_compiled_with_ipu(),
+                 "core is not compiled with IPU")
+class TestScalar(IPUOpTest):
+    def setUp(self):
+        self.set_atol()
+        self.set_training()
+        self.set_data_feed()
+        self.set_feed_attr()
+        self.set_op_attrs()
+
+    def set_data_feed(self):
+        x = np.ones([1, 10])
+        y = 0.5
+        self.feed_fp32 = {"x": x.astype(np.float32), }
+        self.feed_fp16 = {"x": x.astype(np.float16), }
+
+    def set_feed_attr(self):
+        self.feed_shape = [x.shape for x in self.feed_fp32.values()]
+        self.feed_list = list(self.feed_fp32.keys())
+
     def set_op_attrs(self):
-        self.attrs = {
-            'x_num_col_dims': 2,
-            'y_num_col_dims': 2,
-        }
+        self.attrs = {}
+
+    @IPUOpTest.static_graph
+    def build_model(self):
+        x = paddle.static.data(
+            name=self.feed_list[0], shape=self.feed_shape[0], dtype='float32')
+        out = (x != 0.5)
+        self.fetch_list = [out.name]
+
+    def run_model(self, exec_mode):
+        self.run_op_test(exec_mode)
+
+    def test(self):
+        for m in IPUOpTest.ExecutionMode:
+            if not self.skip_mode(m):
+                self.build_model()
+                self.run_model(m)
+        self.check()
 
 
 if __name__ == "__main__":
