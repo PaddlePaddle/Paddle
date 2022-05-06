b560736add84532f9cd23f05526711c6266f7704
SmirnovKol <31559413+SmirnovKol@users.noreply.github.com>
2022-05-06 14:38:05 +0800

    Add files via upload

--- paddle/phi/kernels/gpu/rrelu_grad_kernel.cu
+++ paddle/phi/kernels/gpu/rrelu_grad_kernel.cu
@@ -12,23 +12,60 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/kernels/gpu/rrelu_impl.cu.h"
+// #include "paddle/phi/kernels/gpu/rrelu_impl.cu.h"
 #include "paddle/phi/kernels/rrelu_grad_kernel.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/funcs/for_range.h"
 
 namespace phi {
 
+// template <typename T, typename Context>
+// void RReluGradKernel(const Context& dev_ctx,
+//                      const DenseTensor& mask,
+//                      const DenseTensor& out_grad,
+//                      DenseTensor* x_grad) {
+//   x_grad->mutable_data<T>(dev_ctx.GetPlace());
+//   auto size = mask.numel();
+//   paddle::operators::RReluGradGPUKernelDriver<T>(
+//       dev_ctx, out_grad, mask, x_grad);
+// }
+
+
+template <typename T>
+struct RReluGradCudaFunctor {
+ public:
+  RReluGradCudaFunctor(const T* mask,
+                       const T* out_grad,
+                       T* x_grad)
+      : mask_(mask), out_grad_(out_grad), x_grad_(x_grad) {}
+
+  __device__ void operator()(int64_t idx) {
+      x_grad_[idx] = mask_[idx] * out_grad_[idx];
+  }
+
+ private:
+  const T* mask_;
+  const T* out_grad_;
+  T* x_grad_;
+};
+
 template <typename T, typename Context>
-void RReluGradKernel(const Context& dev_ctx,
+void RReluGradKernel(const Context& ctx, 
                      const DenseTensor& mask,
                      const DenseTensor& out_grad,
                      DenseTensor* x_grad) {
-  x_grad->mutable_data<T>(dev_ctx.GetPlace());
+  const T* mask_data = mask.data<T>();
+  const T* out_grad_data = out_grad.data<T>();
+  T* x_grad_data = ctx.template Alloc<T>(x_grad);
   auto size = mask.numel();
-  paddle::operators::RReluGradGPUKernelDriver<T>(
-      dev_ctx, out_grad, mask, x_grad);
+
+  phi::funcs::ForRange<Context> for_range(ctx, size);
+
+  RReluGradCudaFunctor<T> functor(mask_data, out_grad_data, x_grad_data);
+  for_range(functor);
 }
+
 
 }  // namespace phi
 
--- paddle/phi/kernels/gpu/rrelu_kernel.cu
+++ paddle/phi/kernels/gpu/rrelu_kernel.cu
@@ -12,39 +12,116 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef __NVCC__
+#include <curand_kernel.h>
+#endif
+#ifdef __HIPCC__
+#include <hiprand_kernel.h>
+#endif
+
 #include "paddle/phi/kernels/rrelu_kernel.h"
-#include "paddle/phi/kernels/gpu/rrelu_impl.cu.h"
+// #include "paddle/phi/kernels/gpu/rrelu_impl.cu.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/funcs/for_range.h"
+
 
 namespace phi {
 
+// template <typename T, typename Context>
+// void RReluKernel(const Context& dev_ctx,
+//                  const DenseTensor& x,
+//                  const float lower,
+//                  const float upper,
+//                  bool is_test,
+//                //   bool fix_seed,
+//                //   int seed,
+//                  DenseTensor* out,
+//                  DenseTensor* mask) {
+//   out->mutable_data<T>(dev_ctx.GetPlace());
+//   mask->mutable_data<T>(dev_ctx.GetPlace());
+
+//   paddle::operators::RReluFwGPUKernelDriver<T>(dev_ctx,
+//                                                is_test,
+//                                                lower,
+//                                                upper, 
+//                                                fix_seed,
+//                                                seed,
+//                                                x,
+//                                             //    seed_tensor.get_ptr(),
+//                                                nullptr,
+//                                                mask,
+//                                                out);
+
+template <typename T>
+struct RReluCudaFunctor {
+ public:
+  RReluCudaFunctor(const T* in,
+                   T* out,
+                   T* mask,
+                   unsigned int seed,
+                   unsigned int offset)
+      : in_(in), out_(out), mask_(mask), seed_(seed), offset_(offset) {}
+
+  using MT = typename kps::details::MPTypeTrait<T>::Type;
+
+   auto zero = static_cast<T>(0);
+   auto one = static_cast<T>(1);
+
+  __device__ void operator()(int64_t idx) {
+   if (in_[idx] < zero) {
+#ifdef __NVCC__
+      curandStatePhilox4_32_10_t state;
+      curand_init(seed_, idx, offset_, &state);
+      // random_sampled_value should be in [0, 1]
+      float random_sampled_value = static_cast<float>(curand_uniform(&state));
+#elif __HIPCC__
+      hiprandStatePhilox4_32_10_t state;
+      hiprand_init(seed_, idx, offset_, &state);
+      // random_sampled_value should be in [0, 1]
+      float random_sampled_value = static_cast<float>(hiprand_uniform(&state));
+#endif
+      random_sampled_value = random_sampled_value * (upper - lower) + lower;
+      mask_[idx] = static_cast<T>(random_sampled_value);
+      out_[idx] = static_cast<T>(static_cast<MT>(in_[idx]) * static_cast<MT>(random_sampled_value));
+   } else {
+      mask_[idx] = one;
+      out_[idx] = in_[idx];
+   }
+  }
+
+ private:
+  const T* in_;
+  T* out_;
+  T* mask_;
+  const unsigned int seed_;
+  const unsigned int offset_;
+};
+
 template <typename T, typename Context>
-void RReluKernel(const Context& dev_ctx,
-                 const DenseTensor& x,
+void RReluKernel(const Context& ctx, 
+                 const DenseTensor& x, 
                  const float lower,
                  const float upper,
                  bool is_test,
-                 bool fix_seed,
-                 int seed,
                  DenseTensor* out,
                  DenseTensor* mask) {
-  out->mutable_data<T>(dev_ctx.GetPlace());
-  mask->mutable_data<T>(dev_ctx.GetPlace());
+  const T* x_data = x.data<T>();
+  T* out_data = ctx.template Alloc<T>(out);
+  T* mask_data = ctx.template Alloc<T>(mask);
+  auto size = x.numel();
 
-  paddle::operators::RReluFwGPUKernelDriver<T>(dev_ctx,
-                                               is_test,
-                                               lower,
-                                               upper, 
-                                               fix_seed,
-                                               seed,
-                                               x,
-                                            //    seed_tensor.get_ptr(),
-                                               nullptr,
-                                               mask,
-                                               out);
+  auto gen_cuda = ctx.GetGenerator();
+  auto seed_offset = gen_cuda->IncrementOffset(20);
+  uint64_t seed = seed_offset.first;
+  uint64_t offset = seed_offset.second;
 
+  phi::funcs::ForRange<Context> for_range(ctx, size);
+
+  RReluCudaFunctor<T> functor(x_data, out_data, mask_data, seed, offset);
+  for_range(functor);
 }
+
 
 }  // namespace phi
 
