7ea2b71a71e02a786770d80874d45805d8c07c41
zyfncg <zhangyunfei07@baidu.com>
2022-05-07 09:56:53 +0000

    fix eager_gen bug

--- paddle/fluid/eager/auto_code_generator/final_state_generator/eager_gen.py
+++ paddle/fluid/eager/auto_code_generator/final_state_generator/eager_gen.py
@@ -1324,11 +1324,12 @@ class DygraphNodeGenerator(DygraphFunctionGeneratorBase):
         slot_num_bwd_outputs = len(self.forward_inputs_position_map.keys())
         grad_api_namespace = f"paddle::experimental::{namespace}"
         grad_function_call_str = f"""
-  paddle::small_vector<std::vector<paddle::experimental::Tensor>, egr::kSlotSmallVectorSize> returns({slot_num_bwd_outputs});
   const auto& out_metas = OutputMeta();
+  paddle::small_vector<std::vector<paddle::experimental::Tensor>, egr::kSlotSmallVectorSize> returns({slot_num_bwd_outputs});
   paddle::small_vector<std::vector<paddle::experimental::Tensor*>, egr::kSlotSmallVectorSize> api_output({slot_num_bwd_outputs});
   for (int i = 0; i < {slot_num_bwd_outputs}; ++i) {{
-    api_output[i].reserve(returns.size());
+    returns[i].resize(out_metas[i].size());
+    api_output[i].reserve(out_metas[i].size());
     for (size_t j = 0; j < returns[i].size(); ++j) {{
       if (out_metas[i][j].IsStopGradient()) {{
         api_output[i].push_back(nullptr);
