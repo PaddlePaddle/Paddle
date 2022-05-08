6f512698ecd2bafdec41c6629dd92ca5b688fa6f
SmirnovKol <31559413+SmirnovKol@users.noreply.github.com>
2022-05-08 11:10:26 +0800

    Add files via upload

--- paddle/phi/kernels/elementwise_kernel.cc
+++ paddle/phi/kernels/elementwise_kernel.cc
@@ -152,7 +152,7 @@ PD_REGISTER_KERNEL(elementwise_pow,
                    int,
                    int64_t) {}
 PD_REGISTER_KERNEL(elementwise_heaviside,
-                   KPS,
+                   GPU,
                    ALL_LAYOUT,
                    phi::ElementwiseHeavisideKernel,
                    float,
