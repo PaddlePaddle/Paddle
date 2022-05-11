da1bcccdb13c0106de5b050c2cc50544a799746b
Zuza Gawrysiak <gawrysiak.zuzanna@gmail.com>
2022-05-11 12:20:13 +0200

    Move weights and biases scale computing into pass (#42241)
    * Add int8 scales gathering pass for convolution
    
    * Fix typo
    
    * Add unittest
    
    * Add corrected unit test
    
    * Change test name
    
    * Remove enabling mkldnn in test
    
    * Speed up test
    
    * Change max examples
    
    * Add functional test
    
    * Change test name
    
    * Add new test case
    
    * Rename pass

--- paddle/fluid/framework/ir/graph_pattern_detector.h
+++ paddle/fluid/framework/ir/graph_pattern_detector.h
@@ -1057,7 +1057,7 @@ struct Pool : public PatternBase {
 
 // Elementwise ops
 // Forward pass for element-wise operators (add, mul)
-// elementwise_mul_out is the result of the operator
+// elementwise_out is the result of the operator
 struct Elementwise : public PatternBase {
   Elementwise(PDPattern* pattern, const std::string& name_scope)
       : PatternBase(pattern, name_scope, "elementwise") {}
