8a1e8e9ae08a194c36d6bf702447be5d94d40d8f
0x45f <wangzhen45@baidu.com>
2022-05-11 07:08:28 +0000

    import paddle in primops.py

--- python/paddle/incubate/autograd/primops.py
+++ python/paddle/incubate/autograd/primops.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import paddle
 from paddle.fluid.layer_helper import LayerHelper
 from .primreg import REGISTER_FN
 
