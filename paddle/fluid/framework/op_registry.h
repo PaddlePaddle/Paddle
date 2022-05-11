cfc07fb1af01549d3ff97a6745aa120730e2ee82
betterpig <chenxin33@baidu.com>
2022-05-11 09:55:12 +0000

    generate glob_op_shared.so with op.cc

--- paddle/fluid/framework/op_registry.h
+++ paddle/fluid/framework/op_registry.h
@@ -279,7 +279,7 @@ struct OpKernelRegistrarFunctorEx<PlaceType, false, I,
   STATIC_ASSERT_GLOBAL_NAMESPACE(                                        \
       __reg_op__##op_type,                                               \
       "REGISTER_OPERATOR must be called in global namespace");           \
-  __declspec(dllexport) static ::paddle::framework::OperatorRegistrar    \
+  static ::paddle::framework::OperatorRegistrar    \
   <op_class, ##__VA_ARGS__> __op_registrar_##op_type##__(#op_type);      \
   __declspec(dllexport) int TouchOpRegistrar_##op_type() {               \
     __op_registrar_##op_type##__.Touch();                                \
@@ -301,7 +301,7 @@ struct OpKernelRegistrarFunctorEx<PlaceType, false, I,
       __reg_op_kernel_##op_type##_##library_type##_##customized_name##__,      \
                                  "REGISTER_OP_KERNEL must be called in "       \
                                  "global namespace");                          \
-  __declspec(dllexport) static ::paddle::framework::OpKernelRegistrar          \
+  static ::paddle::framework::OpKernelRegistrar          \
       <place_class, __VA_ARGS__>                                               \
       __op_kernel_registrar_##op_type##_##library_type##_##customized_name##__(\
           #op_type, #library_type, customized_type_value);                     \
