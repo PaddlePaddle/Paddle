2bcd77688f2123db0c8b3a409d4d01f1f8297c18
SmirnovKol <31559413+SmirnovKol@users.noreply.github.com>
2022-05-12 21:00:52 +0800

    Add files via upload

--- python/paddle/fluid/tests/unittests/test_index_add_op.py
+++ python/paddle/fluid/tests/unittests/test_index_add_op.py
@@ -19,25 +19,29 @@ import numpy as np
 from op_test import OpTest
 
 import paddle
-import paddle.fluid as fluid
+# import paddle.fluid as fluid
 import paddle.fluid.core as core
+from typing import List
 
-np.random.seed(102)
+# np.random.seed(102)
 
 
-def index_add_np(data, axis, index, added_value):
-    # as the calculation will modify the elements inplace,
+def index_add_np(data: np.array, axis: int, index: List[int], add_value: float):
+    # as the calculation will modify the elements of the input data inplace,
     # so copying the input data is important
     data = data.copy()
-    if isinstance(index, np.ndarray):
-        index = list(index.flatten())
-    added_value = float(added_value)
+    # if isinstance(index, np.ndarray):
+    #     index = list(index.flatten())
+    # added_value = float(added_value)
+    if axis == 0:
+        data[index] += add_value
+        return data
     outer_loop = int(np.prod(data.shape[:axis]))
     x_reshape = [outer_loop] + list(data.shape[axis:])
-    x_np_reshape = data.reshape(tuple(x_reshape))
+    x_np_reshape = data.reshape(x_reshape)
     for i in range(outer_loop):
         for j in index:
-            x_np_reshape[i, j] += added_value
+            x_np_reshape[i, j] += add_value
     return x_np_reshape.reshape(data.shape)
 
 
@@ -57,23 +61,27 @@ class TestIndexAddOp(OpTest):
         self.python_api = paddle.index_add
         self.op_type = "index_add"
         self.init_data()
-        self.inputs = {'X': self.x_np, 'Index': self.index_np}
-        self.attrs = {'axis': self.axis, 'added_value': self.added_value}
+        self.inputs = {'X': self.x_np}
+        self.attrs = {'axis': self.axis, 
+                        'index': self.index,
+                        'add_value': self.add_value,
+                        }
         self.outputs = {'Out': self.out_np}
 
     def init_data(self):
         self.axis = 1
         self.x_type = np.float64
-        self.index_type = np.int64
+        # self.index_type = np.int64
         self.x_shape = (4, 5, 6)
-        self.index_size = 4
-        self.added_value = 9.0
+        # self.index_size = 4
+        self.add_value = 9.0
         self.x_np = np.random.random(self.x_shape).astype(self.x_type)
-        self.index_np = np.random.randint(
-            low=0, high=self.x_shape[self.axis], size=self.index_size)
+        # self.index_np = np.random.randint(
+        #     low=0, high=self.x_shape[self.axis], size=self.index_size)
         # self.index_np = np.array([0, 3], dtype=self.index_type)
-        self.out_np = index_add_np(self.x_np, self.axis, self.index_np,
-                                    self.added_value)
+        self.index = [0, 4, 1]
+        self.out_np = index_add_np(self.x_np, self.axis, self.index,
+                                    self.add_value)
 
     def test_check_output(self):
         self.check_output(check_eager=True)
@@ -82,18 +90,21 @@ class TestIndexAddOp(OpTest):
         self.check_grad(['X'], 'Out', check_eager=True)
 
 
-class API_IndexAdd(unittest.TestCase):
+class TestStaticIndexAddAPI(unittest.TestCase):
     def setUp(self):
-        self.place = paddle.CUDAPlace(0) if core.is_compiled_with_cuda() \
-            else paddle.CPUPlace()
+        # self.place = paddle.CUDAPlace(0) if core.is_compiled_with_cuda() \
+        #     else paddle.CPUPlace()
+        self.places = [paddle.CPUPlace()]
+        if paddle.device.is_compiled_with_cuda():
+            self.places += [paddle.CUDAPlace(0)]
         self.executed_api()
         self.init_data()
 
     def init_data(self):
         self.data_np = np.random.random((3, 4, 5)).astype(np.float32)
         self.axis = 0
-        self.added_value = 7.0
-        self.index = np.asarray([1, 2]).astype(np.int64)
+        self.add_value = 7.0
+        self.index = [1, 2]
 
     def executed_api(self):
         self.op_run = paddle.index_add
@@ -101,144 +112,162 @@ class API_IndexAdd(unittest.TestCase):
     def test_out(self):
         paddle.enable_static()
         with paddle.static.program_guard(paddle.static.Program()):
-            x = paddle.fluid.data('X', np.asarray(self.data_np).shape)
-            idx = paddle.fluid.data(
-                'Index', np.asarray(self.index).shape, dtype="int64")
+            x = paddle.static.data('X', self.data_np.shape)
+            # idx = paddle.static.data(
+            #     'index', np.asarray(self.index).shape, dtype="int64")
             result_pd = self.op_run(
-                x, idx, axis=self.axis, added_value=self.added_value)
-            exe = paddle.static.Executor(self.place)
-            result, = exe.run(feed={"X": self.data_np,
-                                    "Index": self.index},
-                              fetch_list=[result_pd])
+                x, axis=self.axis, index=self.index, add_value=self.add_value)
             result_np = index_add_np(self.data_np, self.axis, self.index,
-                                      self.added_value)
-            self.assertTrue(np.allclose(result_np, result))
+                        self.add_value)
+            for place in self.places:
+                exe = paddle.static.Executor(place)
+                result, = exe.run(feed={"X": self.data_np},
+                                fetch_list=[result_pd])
+                self.assertTrue(np.allclose(result_np, result))
 
 
-class API_TestStaticIndexAdd_(API_IndexAdd):
+class TestStaticIndexAddInplaceAPI(TestStaticIndexAddAPI):
     def executed_api(self):
         self.op_run = paddle.index_add_
 
     def init_data(self):
         self.data_np = np.random.random((2, 3, 4, 5)).astype(np.float32)
         self.axis = 3
-        self.added_value = 8.0
-        self.index = np.asarray([0, 2, 3]).astype(np.int64)
-        self.place = paddle.CPUPlace()
+        self.add_value = 8.0
+        self.index = [0, 2, 3]
+        # self.place = paddle.CPUPlace()
 
 
-class API_TestDygraphIndexAdd(unittest.TestCase):
+class TestDygraphIndexAddAPI(unittest.TestCase):
     def setUp(self):
         self.executed_api()
         self.init_data()
-        self.place = paddle.CUDAPlace(0) if core.is_compiled_with_cuda() \
-            else paddle.CPUPlace()
+        # self.place = paddle.CUDAPlace(0) if core.is_compiled_with_cuda() \
+        #     else paddle.CPUPlace()
+        self.places = [paddle.CPUPlace()]
+        if paddle.device.is_compiled_with_cuda():
+            self.places += [paddle.CUDAPlace(0)]
 
     def init_data(self):
         self.data_np = np.random.random((3, 4, 5)).astype(np.float32)
         self.axis = 0
-        self.added_value = 11.0
-        self.index = np.asarray([1, 2]).astype(np.int64)
+        self.add_value = -11.0
+        self.index = [1, 2]
 
     def executed_api(self):
         self.op_run = paddle.index_add
 
     def test_out(self):
-        paddle.disable_static(self.place)
-        input_1 = paddle.to_tensor(self.data_np)
-        input = paddle.to_tensor(input_1)
-        index = paddle.to_tensor(self.index)
-        output = self.op_run(
-            input, index, axis=self.axis, added_value=self.added_value)
-        out_np = output.numpy()
-        expected_out = index_add_np(self.data_np, self.axis, self.index,
-                                     self.added_value)
-        self.assertTrue(np.allclose(expected_out, out_np))
+        # index is 1-D tensor
+        for place in self.places:
+            paddle.disable_static(place)
+            input = paddle.to_tensor(self.data_np)
+            index_tensor = paddle.to_tensor(self.index)
+            output = self.op_run(
+                input, axis=self.axis, index=index_tensor, add_value=self.add_value)
+            out_np = output.numpy()
+            expected_out = index_add_np(self.data_np, self.axis, self.index,
+                                        self.add_value)
+            self.assertTrue(np.allclose(expected_out, out_np))
+
+        # index is a list of ints
+        for place in self.places:
+            paddle.disable_static(place)
+            input = paddle.to_tensor(self.data_np)
+            output = self.op_run(
+                input, axis=self.axis, index=self.index, add_value=self.add_value)
+            out_np = output.numpy()
+            expected_out = index_add_np(self.data_np, self.axis, self.index,
+                                        self.add_value)
+            self.assertTrue(np.allclose(expected_out, out_np))
 
 
-class API_TestDygraphIndexAddInplace(API_TestDygraphIndexAdd):
+class TestDygraphIndexAddInplaceAPI(TestDygraphIndexAddAPI):
     def executed_api(self):
         self.op_run = paddle.index_add_
 
     def init_data(self):
         self.data_np = np.random.random((3, 4, 5)).astype(np.float32)
         self.axis = 2
-        self.added_value = 12.0
-        self.index = np.asarray([0, 2]).astype(np.int32)
+        self.add_value = 12.0
+        self.index = [0, 2]
 
 
-class API_TestDygraphIndexAdd2(API_TestDygraphIndexAdd):
+class TestDygraphIndexAddAPI_2(TestDygraphIndexAddAPI):
     def init_data(self):
         self.data_np = np.random.random((10)).astype(np.float32)
         self.axis = 0
-        self.added_value = 13.0
-        self.index = np.asarray([0]).astype(np.int32)
+        self.add_value = -13.0
+        self.index = [0, 2, 9]
 
 
-class API_TestDygraphIndexAdd3(API_TestDygraphIndexAdd):
+class TestDygraphIndexAddAPI_3(TestDygraphIndexAddAPI):
     def init_data(self):
         self.data_np = np.random.random((3, 4, 5, 6)).astype(np.float64)
         self.axis = 2
-        self.added_value = paddle.to_tensor([14.0])
-        self.index = np.asarray([0, 1, 2, 3]).astype(np.int64)
+        self.add_value = paddle.to_tensor([-14.0])
+        self.index = [0, 1, 2, 3]
 
 
-class API_IndexAdd2(unittest.TestCase):
+class TestIndexAddError(unittest.TestCase):
     def setUp(self):
-        self.place = paddle.CUDAPlace(0) if core.is_compiled_with_cuda() \
-            else paddle.CPUPlace()
+        self.places = [paddle.CPUPlace()]
+        if paddle.device.is_compiled_with_cuda():
+            self.places += [paddle.CUDAPlace(0)]
 
     def test_errors(self):
+        paddle.disable_static()
         def error_axis_dtype():
-            with paddle.fluid.dygraph.guard():
-                x = paddle.rand((2, 3))
-                index = paddle.to_tensor([0])
-                paddle.index_add(x, index, axis=0.5, added_value=0.1)
+            # with paddle.fluid.dygraph.guard():
+            x = paddle.rand((2, 3))
+            index = paddle.to_tensor([0])
+            paddle.index_add(x, axis=0.5, index=index, add_value=0.1)
 
         def error_axis_range():
-            with paddle.fluid.dygraph.guard():
-                x = paddle.rand((2, 3))
-                index = paddle.to_tensor([0])
-                paddle.index_add(x, index, axis=10, added_value=0.1)
+            # with paddle.fluid.dygraph.guard():
+            x = paddle.rand((2, 3))
+            index = paddle.to_tensor([0, 1])
+            paddle.index_add(x, axis=10, index=index, add_value=0.1)
 
         def error_index_dtype():
-            with paddle.fluid.dygraph.guard():
-                x = paddle.rand((2, 3))
-                paddle.index_add(x, [0, 1], axis=0, added_value=0.1)
+            # with paddle.fluid.dygraph.guard():
+            x = paddle.rand((2, 3))
+            index = [[0, 1]]
+            paddle.index_add(x, axis=0, index=index, add_value=0.1)
 
         def error_index_dtype2():
-            with paddle.fluid.dygraph.guard():
-                x = paddle.rand((2, 3))
-                index = paddle.to_tensor([0.2, 0.3])
-                paddle.index_add(x, index, axis=1, added_value=0.1)
+            # with paddle.fluid.dygraph.guard():
+            x = paddle.rand((2, 3))
+            index = [0.2, 0.3]
+            paddle.index_add(x, axis=1, index=index, add_value=0.1)
 
         def error_index_dtype3():
-            with paddle.fluid.dygraph.guard():
-                x = paddle.rand((2, 3))
-                index = paddle.to_tensor([])
-                paddle.index_add(x, index, axis=1, added_value=0.1)
+            # with paddle.fluid.dygraph.guard():
+            x = paddle.rand((2, 3))
+            index = paddle.to_tensor([])
+            paddle.index_add(x, axis=1, index=index, add_value=0.1)
 
         def error_index_value():
-            with paddle.fluid.dygraph.guard():
-                x = paddle.rand((2, 3))
-                index = paddle.to_tensor([[1], [2]])
-                added_value = paddle.to_tensor([[9.5], [9.8]])
-                paddle.index_add(x, index, axis=1, added_value=added_value)
+            # with paddle.fluid.dygraph.guard():
+            x = paddle.rand((2, 3, 4))
+            index = paddle.to_tensor([[1], [2]])
+            add_value = paddle.to_tensor(-23)
+            paddle.index_add(x, axis=1, index=index, add_value=add_value)
 
-        def error_added_value():
-            with paddle.fluid.dygraph.guard():
-                x = paddle.rand((2, 3))
-                index = paddle.to_tensor([1])
-                added_value = paddle.to_tensor([[9.5], [9.8]])
-                paddle.index_add(x, index, axis=1, added_value=added_value)
+        def error_add_value():
+            # with paddle.fluid.dygraph.guard():
+            x = paddle.rand((2, 5, 7))
+            index = paddle.to_tensor([1])
+            add_value = paddle.to_tensor([[9.5], [9.8]])
+            paddle.index_add(x, axis=1, index=index, add_value=add_value)
 
-        self.assertRaises(ValueError, error_axis_dtype)
+        self.assertRaises(TypeError, error_axis_dtype)
         self.assertRaises(ValueError, error_axis_range)
         self.assertRaises(TypeError, error_index_dtype)
         self.assertRaises(TypeError, error_index_dtype2)
         self.assertRaises(TypeError, error_index_dtype3)
-        self.assertRaises(ValueError, error_index_value)
-        self.assertRaises(ValueError, error_added_value)
+        self.assertRaises(TypeError, error_index_value)
+        self.assertRaises(TypeError, error_add_value)
 
     # def test_check_grad(self):
     #     paddle.disable_static(place=self.place)
