c017dac9d7e3f291ea6dc952e91ba3d2b9f19747
SmirnovKol <31559413+SmirnovKol@users.noreply.github.com>
2022-05-12 20:59:52 +0800

    Add files via upload

--- paddle/phi/ops/compat/index_add_sig.cc
+++ paddle/phi/ops/compat/index_add_sig.cc
@@ -18,14 +18,15 @@ namespace phi {
 
 KernelSignature IndexAddOpArgumentMapping(const ArgumentMappingContext& ctx) {
   return KernelSignature(
-      "index_add", {"X", "Index"}, {"axis", "added_value"}, {"Out"});
+      "index_add", {"X"}, {"axis", "index", "add_value"}, {"Out"});
 }
 
 KernelSignature IndexAddGradOpArgumentMapping(
     const ArgumentMappingContext& ctx) {
   return KernelSignature("index_add_grad",
                          {"Out@GRAD"},
-                         {"axis", "added_value"},
+                        //  {"axis", "added_value"},
+                         {},
                          {"X@GRAD"});
 }
 
