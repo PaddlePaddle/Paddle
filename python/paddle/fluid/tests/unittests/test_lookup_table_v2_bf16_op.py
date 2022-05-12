5c0a5a51b226290ec950ed022d1a20a22e5c29a1
jakpiase <jakpia21@gmail.com>
2022-05-12 23:05:07 +0200

    another fix for tests

--- python/paddle/fluid/tests/unittests/test_lookup_table_v2_bf16_op.py
+++ python/paddle/fluid/tests/unittests/test_lookup_table_v2_bf16_op.py
@@ -17,8 +17,8 @@ from __future__ import print_function
 import unittest
 import numpy as np
 import paddle
-from paddle.fluid.tests.unittests.op_test import (skip_check_grad_ci,
-                                                  convert_uint16_to_float)
+from paddle.fluid.tests.unittests.op_test import (
+    skip_check_grad_ci, OpTestTool, convert_uint16_to_float)
 from paddle.fluid.tests.unittests.test_lookup_table_bf16_op import (
     _lookup, TestLookupTableBF16Op, TestLookupTableBF16OpIds4D,
     TestLookupTableBF16OpWIsSelectedRows,
@@ -75,6 +75,7 @@ class TestLookupTableBF16OpIds4DPadding(TestLookupTableV2BF16OpIds4D):
         self.check_output_with_place(core.CPUPlace())
 
 
+@OpTestTool.skip_if_not_cpu_bf16()
 class TestEmbeddingLayerBF16ConstantInitializer(unittest.TestCase):
     """
     Test embedding layer from input api and results for bfloat16
