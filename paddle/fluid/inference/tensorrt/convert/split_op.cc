08bdc2b77429a2bf34263f1798f5afdea3b8b1ec
zhoutianzi666 <17801055074@163.com>
2022-05-13 11:24:49 +0000

    huipu commit

--- paddle/fluid/inference/tensorrt/convert/split_op.cc
+++ paddle/fluid/inference/tensorrt/convert/split_op.cc
@@ -59,11 +59,58 @@ class SplitOpConverter : public OpConverter {
 
     nvinfer1::ILayer* layer = nullptr;
     if (engine_->with_dynamic_shape()) {
-      bool with_fp16 =
-          engine_->WithFp16() && !engine_->disable_trt_plugin_fp16();
-      plugin::SplitPluginDynamic* plugin =
-          new plugin::SplitPluginDynamic(axis, output_lengths, with_fp16);
-      layer = engine_->AddDynamicPlugin(&input, input_num, plugin);
+      nvinfer1::Dims trt_step_dims;
+      trt_step_dims.nbDims = input->getDimensions().nbDims;
+      for (int i = 0; i < trt_step_dims.nbDims; i++) trt_step_dims.d[i] = 1;
+      auto shape_tensor = TRT_ENGINE_ADD_LAYER(engine_, Shape, *input)->getOutput(0);
+
+      std::vector<int32_t> gather_indices;
+      gather_indices.resize(trt_step_dims.nbDims);
+      std::iota(gather_indices.begin(), gather_indices.end(), 0);
+      gather_indices[axis] = gather_indices.size();
+
+      auto gather_indices_tensor = Add1DConstantLayer(gather_indices, "_add_split_op_" + "gather_indices");
+      std::vector<int32_t> zeros(trt_step_dims.nbDims, 0);
+      auto zeros_tensor = Add1DConstantLayer(zeros, "_add_split_op_" + "zeros");
+
+      std::vector<int32_t> each_len(1, out_axis_dim);
+      auto each_len_tensor = Add1DConstantLayer(each_len, "_add_split_op_" + "each_len");
+
+      // input : [N,C,H,W]
+      for (size_t i = 0; i < output_num; i++) {
+
+        auto i_tensor = Add1DConstantLayer({i}, "_add_split_op_" + "i");
+
+        auto start_tensor = TRT_ENGINE_ADD_LAYER(engine_, ElementWise, *i_tensor, *each_len_tensor,
+            nvinfer1::ElementWiseOperation::kPROD)->getOutput(0);
+        std::vector<nvinfer1::ITensor*> concat_inputs1 = {zeros_tensor, start_tensor};
+        std::vector<nvinfer1::ITensor*> concat_inputs2 = {shape_tensor, each_len_tensor};
+        start_tensor = TRT_ENGINE_ADD_LAYER(engine_, Gather,
+                 *zeros_tensor,
+                *TRT_ENGINE_ADD_LAYER(engine_, Concatenation, concat_inputs.data() ,2)->getOutput(0),
+                0);
+        auto size_tensor = TRT_ENGINE_ADD_LAYER(engine_, Gather,
+                 *shape_tensor,
+                *TRT_ENGINE_ADD_LAYER(engine_, Concatenation, concat_inputs.data() ,2)->getOutput(0),
+                0);
+
+        layer = TRT_ENGINE_ADD_LAYER(engine_, Slice, *input, trt_step_dims,
+                                     trt_step_dims, trt_step_dims);
+        layer->setInput(1, *start_tensor);
+        layer->setInput(2, *size_tensor);
+        
+        
+        auto output_name = op_desc.Output("Out")[i];
+        layer->getOutput(0)->setName(output_name.c_str());
+        engine_->SetITensor(output_name, layer->getOutput());
+        std::string layer_name = "split (Output: ";
+        layer_name += output_name;
+        if (test_mode) {
+          engine_->DeclareOutput(output_name);
+        }
+        layer->setName((layer_name + ")").c_str());
+      }
+
     } else {
 #if IS_TRT_VERSION_GE(7130)
 
@@ -71,21 +118,27 @@ class SplitOpConverter : public OpConverter {
       nvinfer1::Dims trt_start_dims;
       trt_start_dims.nbDims = chw_input_dims.nbDims;
       memset(trt_start_dims.d, 0, sizeof(int32_t) * chw_input_dims.nbDims);
-      nvinfer1::Dims trt_out_dims = chw_input_dims;
+      nvinfer1::Dims trt_size_dims = chw_input_dims;
       nvinfer1::Dims trt_step_dims;
       trt_step_dims.nbDims = chw_input_dims.nbDims;
       for (int i = 0; i < trt_step_dims.nbDims; i++) trt_step_dims.d[i] = 1;
 
-      std::string layer_name = "split (Output: ";
       // input : [C,H,W]
       for (size_t i = 0; i < output_num; i++) {
         trt_start_dims.d[axis] = std::accumulate(output_lengths.begin(),
                                                  output_lengths.begin() + i, 0);
-        trt_out_dims.d[axis] = output_lengths[i];
+        trt_size_dims.d[axis] = output_lengths[i];
         layer = TRT_ENGINE_ADD_LAYER(engine_, Slice, *input, trt_start_dims,
-                                     trt_out_dims, trt_step_dims);
+                                     trt_size_dims, trt_step_dims);
         auto output_name = op_desc.Output("Out")[i];
-        RreplenishLayerAndOutput(layer, "slice", {output_name}, test_mode);
+        layer->getOutput(i)->setName(output_name.c_str());
+        engine_->SetITensor(output_name, layer->getOutput(i));
+        std::string layer_name = "split (Output: ";
+        layer_name += output_name;
+        if (test_mode) {
+          engine_->DeclareOutput(output_name);
+        }
+        layer->setName((layer_name + ")").c_str());
       }
 #else
       bool with_fp16 =
