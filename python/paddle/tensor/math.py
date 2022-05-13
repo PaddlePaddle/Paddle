942cda9794db583ec65fe2a3e74e5e8fc594fbfc
Avin0323 <mr.avin0323@gmail.com>
2022-05-13 08:02:50 +0000

    fix sample code error of paddle.lerp, test=document_fix

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -3561,7 +3561,7 @@ def lerp(x, y, weight, name=None):
             x = paddle.arange(1., 5., dtype='float32')
             y = paddle.empty([4], dtype='float32')
             y.fill_(10.)
-            out = paddle.lerp(start, end, 0.5)
+            out = paddle.lerp(x, y, 0.5)
             # out: [5.5., 6., 6.5, 7.]
 
     """
