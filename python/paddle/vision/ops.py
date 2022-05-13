6ba3a4769a389da896757b23daacfbb538edd04e
LielinJiang <jianglielin@baidu.com>
2022-05-13 05:44:08 +0000

    fix decode_jpeg example code

--- python/paddle/vision/ops.py
+++ python/paddle/vision/ops.py
@@ -896,6 +896,7 @@ def decode_jpeg(x, mode='unchanged', name=None):
     Examples:
         .. code-block:: python
 
+            # required: gpu
             import cv2
             import numpy as np
             import paddle
