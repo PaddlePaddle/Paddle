6b58de954cbf776cddbf05154f86e8782ee4feca
zhupengyang <zhu_py@qq.com>
2022-05-17 19:26:30 +0800

    add yolo_box_fuse_pass, yolo_box_head_op, yolo_box_post_op (#42641)

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -1777,6 +1777,7 @@ USE_TRT_CONVERTER(clip);
 USE_TRT_CONVERTER(gather);
 USE_TRT_CONVERTER(anchor_generator);
 USE_TRT_CONVERTER(yolo_box);
+USE_TRT_CONVERTER(yolo_box_head);
 USE_TRT_CONVERTER(roi_align);
 USE_TRT_CONVERTER(affine_channel);
 USE_TRT_CONVERTER(multiclass_nms);
