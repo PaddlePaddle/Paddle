5fed3a538af9df85ef1849cd464379f82ee51889
jiweibo <jiweibo@baidu.com>
2022-05-17 08:31:31 +0000

    fix ci problem

--- paddle/fluid/inference/api/resource_manager.h
+++ paddle/fluid/inference/api/resource_manager.h
@@ -16,6 +16,7 @@
 #include <functional>
 #include <map>
 #include <memory>
+#include <mutex>
 #include "paddle/fluid/platform/macros.h"
 #include "paddle/phi/api/include/tensor.h"
 #include "paddle/phi/backends/cpu/forwards.h"
