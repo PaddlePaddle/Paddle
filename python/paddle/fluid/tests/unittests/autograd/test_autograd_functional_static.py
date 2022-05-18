f842bbd2033992614b9abd06f0f2836fadf8bbca
Xiaoxu Chen <chenxx_id@163.com>
2022-05-18 08:47:48 +0000

    enable Jacobian/Hessian for supporting new autograd

--- python/paddle/fluid/tests/unittests/autograd/test_autograd_functional_static.py
+++ python/paddle/fluid/tests/unittests/autograd/test_autograd_functional_static.py
@@ -18,6 +18,7 @@ import unittest
 import numpy as np
 import paddle
 import paddle.fluid as fluid
+from paddle.autograd.functional import forward_gradients
 
 import config
 import utils
@@ -125,10 +126,10 @@ def gen_static_data_and_feed(xs, v, stop_gradient=True):
     if isinstance(v, typing.Sequence):
         static_v = []
         for i, e in enumerate(v):
-            e = paddle.static.data(f'v{idx}', v.shape, v.dtype)
+            e = paddle.static.data(f'v{i}', e.shape, e.dtype)
             e.stop_gradient = stop_gradient
             static_v.append(e)
-        feed.update({f'v{idx}': value for idx, value in v})
+        feed.update({f'v{i}': value for i, value in enumerate(v)})
     elif v is not None:
         static_v = paddle.static.data('v', v.shape, v.dtype)
         static_v.stop_gradient = stop_gradient
@@ -366,6 +367,36 @@ class TestJacobianFloat32(unittest.TestCase):
         self.run_test_by_rows(pd_f, np_f, [self.D, self.E], batch=True)
         self.run_test_by_entries(pd_f, np_f, [self.D, self.E], batch=True)
 
+    def test_prim_mode(self):
+        def fun(x, y):
+            main = paddle.static.Program()
+            startup = paddle.static.Program()
+            with paddle.static.program_guard(main, startup):
+                static_x = paddle.static.data('x', x.shape, x.dtype)
+                static_y = paddle.static.data('y', y.shape, y.dtype)
+                jac = paddle.incubate.autograd.Jacobian(paddle.matmul,
+                                                        [static_x, static_y])[:]
+                if paddle.incubate.autograd.prim_enabled():
+                    paddle.incubate.autograd.prim2orig(main.block(0))
+            feed = {'x': x, 'y': y}
+            fetch_list = [jac]
+            exe = paddle.static.Executor()
+            exe.run(startup)
+            [jac] = exe.run(main, feed=feed, fetch_list=fetch_list)
+            return jac
+
+        x = np.random.rand(3, 3).astype(self.np_dtype)
+        y = np.random.rand(3, 3).astype(self.np_dtype)
+
+        paddle.incubate.autograd.disable_prim()
+        orig_jac = fun(x, y)
+        paddle.incubate.autograd.enable_prim()
+        prim_jac = fun(x, y)
+        paddle.incubate.autograd.disable_prim()
+
+        np.testing.assert_allclose(
+            orig_jac, prim_jac, rtol=self.rtol, atol=self.atol)
+
 
 class TestJacobianFloat64(TestJacobianFloat32):
     @classmethod
@@ -432,6 +463,36 @@ class TestHessianFloat32(unittest.TestCase):
 
         self.run_test_by_fullmatrix(pd_f, self.B, np_hess(self.B))
 
+    def test_prim_mode(self):
+        def fun(x, y):
+            main = paddle.static.Program()
+            startup = paddle.static.Program()
+            with paddle.static.program_guard(main, startup):
+                static_x = paddle.static.data('x', x.shape, x.dtype)
+                static_y = paddle.static.data('y', y.shape, y.dtype)
+                hessian = paddle.incubate.autograd.Hessian(
+                    lambda x, y: paddle.matmul(x, y), [static_x, static_y])[:]
+                if paddle.incubate.autograd.prim_enabled():
+                    paddle.incubate.autograd.prim2orig(main.block(0))
+            feed = {'x': x, 'y': y}
+            fetch_list = [hessian]
+            exe = paddle.static.Executor()
+            exe.run(startup)
+            [hessian] = exe.run(main, feed=feed, fetch_list=fetch_list)
+            return hessian
+
+        x = np.random.rand(1).astype(self.dtype)
+        y = np.random.rand(1).astype(self.dtype)
+
+        paddle.incubate.autograd.disable_prim()
+        orig_hessian = fun(x, y)
+        paddle.incubate.autograd.enable_prim()
+        prim_hessian = fun(x, y)
+        paddle.incubate.autograd.disable_prim()
+
+        np.testing.assert_allclose(
+            orig_hessian, prim_hessian, rtol=self.rtol, atol=self.atol)
+
 
 class TestHessianFloat64(TestHessianFloat32):
     @classmethod
@@ -449,6 +510,58 @@ class TestHessianFloat64(TestHessianFloat32):
             'second_order_grad').get('rtol')
         self.atol = config.TOLERANCE.get(self.dtype).get(
             'second_order_grad').get('atol')
+
+
+@utils.place(config.DEVICES)
+@utils.parameterize((utils.TEST_CASE_NAME, 'fun', 'xs', 'v'), (
+    ('matmul', paddle.matmul, (np.random.rand(2, 3), np.random.rand(3, 2)),
+     None), ('multiply', paddle.multiply,
+             (np.random.rand(2, 3), np.random.rand(2, 3)), None),
+    ('add', paddle.add, (np.random.rand(2, 3), np.random.rand(2, 3)),
+     None), ('input_not_sequence', paddle.tanh, np.random.rand(5, 5), None),
+    ('input_gradients_not_none', paddle.matmul,
+     (np.random.rand(3, 3), np.random.rand(3, 3)),
+     (np.random.rand(3, 3), np.random.rand(3, 3)))))
+class TestForwardGradients(unittest.TestCase):
+    def setUp(self):
+        self.dtype = str(self.xs[0].dtype) if isinstance(
+            self.xs, typing.Sequence) else str(self.xs.dtype)
+        self._rtol = config.TOLERANCE.get(str(self.dtype)).get(
+            "first_order_grad").get("rtol")
+        self._atol = config.TOLERANCE.get(str(self.dtype)).get(
+            "first_order_grad").get("atol")
+
+    def test_forward_gradients(self):
+        def expected():
+            sp = paddle.static.Program()
+            mp = paddle.static.Program()
+            with paddle.static.program_guard(mp, sp):
+                feed, static_xs, static_v = gen_static_data_and_feed(
+                    self.xs, self.v, stop_gradient=False)
+                _, ys_grad = paddle.autograd.jvp(self.fun, static_xs, static_v)
+            exe = paddle.static.Executor()
+            exe.run(sp)
+            return exe.run(mp, feed=feed, fetch_list=ys_grad)
+
+        def actual():
+            sp = paddle.static.Program()
+            mp = paddle.static.Program()
+            with paddle.static.program_guard(mp, sp):
+                feed, static_xs, static_v = gen_static_data_and_feed(
+                    self.xs, self.v, stop_gradient=False)
+                ys = self.fun(*static_xs) if isinstance(
+                    static_xs, typing.Sequence) else self.fun(static_xs)
+                ys_grad = forward_gradients(ys, static_xs, static_v)
+                paddle.incubate.autograd.prim2orig(mp.block(0))
+            exe = paddle.static.Executor()
+            exe.run(sp)
+            return exe.run(mp, feed=feed, fetch_list=ys_grad)
+
+        actual = actual()
+        expected = expected()
+        self.assertEqual(type(actual), type(expected))
+        np.testing.assert_allclose(
+            np.concatenate(actual), np.concatenate(expected))
 
 
 if __name__ == "__main__":
