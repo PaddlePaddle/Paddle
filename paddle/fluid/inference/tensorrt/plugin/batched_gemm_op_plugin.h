67adf795de50d024f2e5d0d7ae9d732d681513ee
yangzg216 <624788535@qq.com>
2022-05-19 00:00:42 +0800

    fp16 batched gemm plugin

--- paddle/fluid/inference/tensorrt/plugin/batched_gemm_op_plugin.h
+++ paddle/fluid/inference/tensorrt/plugin/batched_gemm_op_plugin.h
@@ -1,32 +1,24 @@
-/*
- * Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-//#include <NvInfer.h>
-//#include <cassert>
-//#include <cublas_v2.h>
-//#include <iostream>
-//#include <string>
-//#include <vector>
 #pragma once
 #include <algorithm>
 #include <string>
 #include <vector>
 #include "paddle/fluid/inference/tensorrt/engine.h"
 #include "paddle/fluid/inference/tensorrt/plugin/trt_plugin.h"
-//#include "paddle/fluid/platform/dynload/cublasLt.h"
+#include "paddle/fluid/inference/tensorrt/plugin/trt_plugin_utils.h"
 #include "paddle/fluid/platform/dynload/cublas.h"
 #include "paddle/fluid/platform/enforce.h"
 
@@ -41,26 +33,6 @@
     #define WHERE_AM_I()
 #endif // ifdef DEBUG
 
-/*
-inline void check(cublasStatus_t ret, int line)
-{
-    if (ret != CUBLAS_STATUS_SUCCESS)
-    {
-        std::cerr << "cuBLAS Error: " << ret << ", line: " << line << std::endl;
-    }
-}
-
-inline void check(cudaError_t ret, int line)
-{
-    if (ret != cudaSuccess)
-    {
-        std::cerr << "CUDA Error: " << cudaGetErrorString(ret) << ", line: " << line << std::endl;
-    }
-}
-
-#define CHECK(_x) check((_x), __LINE__)
-*/
-
 namespace paddle {
 namespace inference {
 namespace tensorrt {
@@ -70,23 +42,22 @@ class CuBLASBatchedGemmPlugin : public DynamicPluginTensorRT
 {
 private:
     const std::string name_;
-    std::string       namespace_;
+    std::string namespace_;
 
 public:
-    bool           bOwnWeight_ {false};
-    bool           has_relu_ {false};
-    int            nK_ {0}; // shape of the weight, B_{nK,nN}
-    int            nN_ {0};
-    int            batchcount_ {0};
-    float *        pGPUWeight_ {nullptr};
-    nvinfer1::Weights        weight_ {nvinfer1::DataType::kFLOAT, nullptr, 0};
-    float *        pGPUBias_ {nullptr};
-    nvinfer1::Weights        bias_ {nvinfer1::DataType::kFLOAT, nullptr, 0};
-    cublasHandle_t handle_ {nullptr};
+    bool has_relu_ {false};
+    int nK_ {0}; // shape of the weight, B_{nK,nN}
+    int nN_ {0};
+    int batchcount_{0};
+    cuda_unique_ptr<void> pGPUWeight_;
+    WeightsWithOwnership weight_;
+    cuda_unique_ptr<void> pGPUBias_;
+    WeightsWithOwnership bias_;
+    cublasHandle_t handle_{nullptr};
 
 public:
     CuBLASBatchedGemmPlugin() = delete;
-    CuBLASBatchedGemmPlugin(const std::string &name, nvinfer1::Weights weight, nvinfer1::Weights bias, int k, int n, int batchcount, bool has_relu, bool needDeepCopy);
+    CuBLASBatchedGemmPlugin(const std::string &name, const nvinfer1::Weights& weight, const nvinfer1::Weights& bias, const int k, const int n, const int batchcount, const bool has_relu, const bool with_fp16);
     CuBLASBatchedGemmPlugin(const std::string &name, const void *buffer, size_t length);
     ~CuBLASBatchedGemmPlugin();
 
@@ -104,8 +75,6 @@ public:
 
     // Method inherited from IPluginV2Ext
     nvinfer1::DataType getOutputDataType(int32_t index, nvinfer1::DataType const *inputTypes, int32_t nbInputs) const noexcept override;
-    //void     attachToContext(cudnnContext *contextCudnn, cublasContext *contextCublas, nvinfer1::IGpuAllocator *gpuAllocator) noexcept override;
-    //void     detachFromContext() noexcept override;
 
     //Method inherited from IPluginV2DynamicExt
     nvinfer1::IPluginV2DynamicExt *clone() const noexcept override;
@@ -127,34 +96,27 @@ private:
 public:
     CuBLASBatchedGemmPluginCreator() {}
     //~CuBLASBatchedGemmPluginCreator() {}
-    const char *                 getPluginName() const noexcept override
-    {
+    const char* getPluginName() const noexcept override {
         return "batchedgemmplugin";
     }
-    const char *                 getPluginVersion() const noexcept override
-    {
+    const char* getPluginVersion() const noexcept override {
         return "1";
     }
-    const nvinfer1::PluginFieldCollection *getFieldNames() noexcept override
-    {
+    const nvinfer1::PluginFieldCollection *getFieldNames() noexcept override {
         return &fc_;
     }
-    nvinfer1::IPluginV2 *                  createPlugin(const char *name, const nvinfer1::PluginFieldCollection *fc) noexcept override
-    {
+    nvinfer1::IPluginV2* createPlugin(const char *name, const nvinfer1::PluginFieldCollection *fc) noexcept override {
         return nullptr;
     }
-    nvinfer1::IPluginV2 *                  deserializePlugin(const char *name, const void *serialData, size_t serialLength) noexcept override
-    {
+    nvinfer1::IPluginV2* deserializePlugin(const char *name, const void *serialData, size_t serialLength) noexcept override {
         WHERE_AM_I()
         return new CuBLASBatchedGemmPlugin(name, serialData, serialLength);
     }
-    void                         setPluginNamespace(const char *pluginNamespace) noexcept override
-    {
+    void setPluginNamespace(const char *pluginNamespace) noexcept override {
         WHERE_AM_I()
         namespace_ = pluginNamespace;
     }
-    const char *                 getPluginNamespace() const noexcept override
-    {
+    const char* getPluginNamespace() const noexcept override {
         return namespace_.c_str();
     }
 };
