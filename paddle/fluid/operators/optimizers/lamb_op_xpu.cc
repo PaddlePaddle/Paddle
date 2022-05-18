96501bbdd14e1833d2e3b7d856eaad1ef94ab0cc
tiancaitzp <tiancaitzp@163.com>
2022-05-18 12:48:09 +0000

    remove old XDNN implementation.

--- paddle/fluid/operators/optimizers/lamb_op_xpu.cc
+++ paddle/fluid/operators/optimizers/lamb_op_xpu.cc
@@ -26,75 +26,75 @@ template <typename DeviceContext, typename T>
 class LambOpXPUKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
-    using paddle::framework::LoDTensor;
-    const auto* param_var = ctx.InputVar("Param");
-    PADDLE_ENFORCE_EQ(param_var->IsType<framework::LoDTensor>(), true,
-                      platform::errors::InvalidArgument(
-                          "The Var(%s)'s type should be LoDTensor, "
-                          "but the received is %s",
-                          ctx.InputNames("Param").front(),
-                          framework::ToTypeName(param_var->Type())));
+    // using paddle::framework::LoDTensor;
+    // const auto* param_var = ctx.InputVar("Param");
+    // PADDLE_ENFORCE_EQ(param_var->IsType<framework::LoDTensor>(), true,
+    //                   platform::errors::InvalidArgument(
+    //                       "The Var(%s)'s type should be LoDTensor, "
+    //                       "but the received is %s",
+    //                       ctx.InputNames("Param").front(),
+    //                       framework::ToTypeName(param_var->Type())));
 
-    using paddle::framework::LoDTensor;
+    // using paddle::framework::LoDTensor;
 
-    // inputs
-    T epsilon = static_cast<T>(ctx.Attr<float>("epsilon"));
-    T weight_decay = static_cast<T>(ctx.Attr<float>("weight_decay"));
-    T beta1 = static_cast<T>(ctx.Attr<float>("beta1"));
-    T beta2 = static_cast<T>(ctx.Attr<float>("beta2"));
-    auto& param = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Param"), "Input",
-                                  "Param", "Lamb");
-    auto* grad_var = ctx.InputVar("Grad");
-    auto& mom1 = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Moment1"), "Input",
-                                 "Moment1", "Lamb");
-    auto& mom2 = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Moment2"), "Input",
-                                 "Moment2", "Lamb");
-    auto& lr = GET_DATA_SAFELY(ctx.Input<LoDTensor>("LearningRate"), "Input",
-                               "LearningRate", "Lamb");
+    // // inputs
+    // T epsilon = static_cast<T>(ctx.Attr<float>("epsilon"));
+    // T weight_decay = static_cast<T>(ctx.Attr<float>("weight_decay"));
+    // T beta1 = static_cast<T>(ctx.Attr<float>("beta1"));
+    // T beta2 = static_cast<T>(ctx.Attr<float>("beta2"));
+    // auto& param = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Param"), "Input",
+    //                               "Param", "Lamb");
+    // auto* grad_var = ctx.InputVar("Grad");
+    // auto& mom1 = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Moment1"), "Input",
+    //                              "Moment1", "Lamb");
+    // auto& mom2 = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Moment2"), "Input",
+    //                              "Moment2", "Lamb");
+    // auto& lr = GET_DATA_SAFELY(ctx.Input<LoDTensor>("LearningRate"), "Input",
+    //                            "LearningRate", "Lamb");
 
-    auto& beta1_pow = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Beta1Pow"), "Input",
-                                      "Beta1Pow", "Lamb");
-    auto& beta2_pow = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Beta2Pow"), "Input",
-                                      "Beta2Pow", "Lamb");
+    // auto& beta1_pow = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Beta1Pow"), "Input",
+    //                                   "Beta1Pow", "Lamb");
+    // auto& beta2_pow = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Beta2Pow"), "Input",
+    //                                   "Beta2Pow", "Lamb");
 
-    auto& param_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("ParamOut"),
-                                      "Output", "ParamOut", "Lamb");
-    auto& mom1_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("Moment1Out"),
-                                     "Output", "Moment1Out", "Lamb");
-    auto& mom2_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("Moment2Out"),
-                                     "Output", "Moment2Out", "Lamb");
-    auto& beta1_pow_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("Beta1PowOut"),
-                                          "Output", "Beta1PowOut", "Lamb");
-    auto& beta2_pow_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("Beta2PowOut"),
-                                          "Output", "Beta2PowOut", "Lamb");
-    auto& dev_ctx = ctx.template device_context<DeviceContext>();
+    // auto& param_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("ParamOut"),
+    //                                   "Output", "ParamOut", "Lamb");
+    // auto& mom1_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("Moment1Out"),
+    //                                  "Output", "Moment1Out", "Lamb");
+    // auto& mom2_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("Moment2Out"),
+    //                                  "Output", "Moment2Out", "Lamb");
+    // auto& beta1_pow_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("Beta1PowOut"),
+    //                                       "Output", "Beta1PowOut", "Lamb");
+    // auto& beta2_pow_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("Beta2PowOut"),
+    //                                       "Output", "Beta2PowOut", "Lamb");
+    // auto& dev_ctx = ctx.template device_context<DeviceContext>();
 
-    if (grad_var->IsType<framework::LoDTensor>()) {
-      auto& grad = *ctx.Input<LoDTensor>("Grad");
-      int r = xpu::lamb(
-          dev_ctx.x_context(), grad.template data<T>(), mom1.template data<T>(),
-          mom2.template data<T>(), param.template data<T>(),
-          beta1_pow.template data<T>(), beta2_pow.template data<T>(),
-          mom1_out.template mutable_data<T>(ctx.GetPlace()),
-          mom2_out.template mutable_data<T>(ctx.GetPlace()),
-          param_out.template mutable_data<T>(ctx.GetPlace()),
-          beta1_pow_out.template mutable_data<T>(ctx.GetPlace()),
-          beta2_pow_out.template mutable_data<T>(ctx.GetPlace()), beta1, beta2,
-          epsilon, weight_decay, lr.template data<T>(), param.numel());
+    // if (grad_var->IsType<framework::LoDTensor>()) {
+    //   auto& grad = *ctx.Input<LoDTensor>("Grad");
+    //   int r = xpu::lamb(
+    //       dev_ctx.x_context(), grad.template data<T>(), mom1.template data<T>(),
+    //       mom2.template data<T>(), param.template data<T>(),
+    //       beta1_pow.template data<T>(), beta2_pow.template data<T>(),
+    //       mom1_out.template mutable_data<T>(ctx.GetPlace()),
+    //       mom2_out.template mutable_data<T>(ctx.GetPlace()),
+    //       param_out.template mutable_data<T>(ctx.GetPlace()),
+    //       beta1_pow_out.template mutable_data<T>(ctx.GetPlace()),
+    //       beta2_pow_out.template mutable_data<T>(ctx.GetPlace()), beta1, beta2,
+    //       epsilon, weight_decay, lr.template data<T>(), param.numel());
 
-      PADDLE_ENFORCE_XDNN_SUCCESS(r, "lamb");
-    } else {
-      PADDLE_THROW(platform::errors::InvalidArgument(
-          "Variable type not supported by lamb_op. Expect LoDTensor, "
-          "but got %s",
-          framework::ToTypeName(param_var->Type())));
-    }
+    //   PADDLE_ENFORCE_XDNN_SUCCESS(r, "lamb");
+    // } else {
+    //   PADDLE_THROW(platform::errors::InvalidArgument(
+    //       "Variable type not supported by lamb_op. Expect LoDTensor, "
+    //       "but got %s",
+    //       framework::ToTypeName(param_var->Type())));
+    // }
   }
 };
 }  // namespace operators
 }  // namespace paddle
 
-namespace ops = paddle::operators;
-REGISTER_OP_XPU_KERNEL(
-    lamb, ops::LambOpXPUKernel<paddle::platform::XPUDeviceContext, float>);
+// namespace ops = paddle::operators;
+// REGISTER_OP_XPU_KERNEL(
+//     lamb, ops::LambOpXPUKernel<paddle::platform::XPUDeviceContext, float>);
 #endif
--- paddle/fluid/operators/optimizers/rmsprop_op_xpu.cc
+++ paddle/fluid/operators/optimizers/rmsprop_op_xpu.cc
@@ -41,88 +41,88 @@ template <typename DeviceContext, typename T>
 class RmspropOpXPUKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
-    using paddle::framework::LoDTensor;
+    // using paddle::framework::LoDTensor;
 
-    // check Param & Grad tensor type
-    const auto* param_var = ctx.InputVar("Param");
-    PADDLE_ENFORCE_EQ(param_var->IsType<LoDTensor>(), true,
-                      platform::errors::InvalidArgument(
-                          "Tensor holds the wrong type，Expected Var(%s)'s "
-                          "type is LoDTensor, "
-                          "but the received is %s",
-                          ctx.InputNames("Param").front(),
-                          framework::ToTypeName(param_var->Type())));
+    // // check Param & Grad tensor type
+    // const auto* param_var = ctx.InputVar("Param");
+    // PADDLE_ENFORCE_EQ(param_var->IsType<LoDTensor>(), true,
+    //                   platform::errors::InvalidArgument(
+    //                       "Tensor holds the wrong type，Expected Var(%s)'s "
+    //                       "type is LoDTensor, "
+    //                       "but the received is %s",
+    //                       ctx.InputNames("Param").front(),
+    //                       framework::ToTypeName(param_var->Type())));
 
-    const auto* grad_var = ctx.InputVar("Grad");
-    PADDLE_ENFORCE_EQ(grad_var->IsType<LoDTensor>(), true,
-                      platform::errors::InvalidArgument(
-                          "Tensor holds the wrong type，Expected Var(%s)'s "
-                          "type is LoDTensor, "
-                          "but the received is %s",
-                          ctx.InputNames("Grad").front(),
-                          framework::ToTypeName(grad_var->Type())));
+    // const auto* grad_var = ctx.InputVar("Grad");
+    // PADDLE_ENFORCE_EQ(grad_var->IsType<LoDTensor>(), true,
+    //                   platform::errors::InvalidArgument(
+    //                       "Tensor holds the wrong type，Expected Var(%s)'s "
+    //                       "type is LoDTensor, "
+    //                       "but the received is %s",
+    //                       ctx.InputNames("Grad").front(),
+    //                       framework::ToTypeName(grad_var->Type())));
 
-    // inputs
-    auto& param = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Param"), "Input",
-                                  "Param", "Rmsprop");
-    auto& meanSquare = GET_DATA_SAFELY(ctx.Input<LoDTensor>("MeanSquare"),
-                                       "Input", "MeanSquare", "Rmsprop");
-    auto& grad = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Grad"), "Input", "Grad",
-                                 "Rmsprop");
-    auto& mom = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Moment"), "Input",
-                                "Moment", "Rmsprop");
+    // // inputs
+    // auto& param = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Param"), "Input",
+    //                               "Param", "Rmsprop");
+    // auto& meanSquare = GET_DATA_SAFELY(ctx.Input<LoDTensor>("MeanSquare"),
+    //                                    "Input", "MeanSquare", "Rmsprop");
+    // auto& grad = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Grad"), "Input", "Grad",
+    //                              "Rmsprop");
+    // auto& mom = GET_DATA_SAFELY(ctx.Input<LoDTensor>("Moment"), "Input",
+    //                             "Moment", "Rmsprop");
 
-    auto* learning_rate = ctx.Input<Tensor>("LearningRate");
-    PADDLE_ENFORCE_EQ(learning_rate->dims().size(), 1,
-                      platform::errors::InvalidArgument(
-                          "learining rate should have dimension = 1."
-                          " But received learning rate dim [%s] ",
-                          learning_rate->dims().size()));
-    T lr = static_cast<T>(GetAttrFromTensor(learning_rate));
+    // auto* learning_rate = ctx.Input<Tensor>("LearningRate");
+    // PADDLE_ENFORCE_EQ(learning_rate->dims().size(), 1,
+    //                   platform::errors::InvalidArgument(
+    //                       "learining rate should have dimension = 1."
+    //                       " But received learning rate dim [%s] ",
+    //                       learning_rate->dims().size()));
+    // T lr = static_cast<T>(GetAttrFromTensor(learning_rate));
 
-    // constants
-    T epsilon = static_cast<T>(ctx.Attr<float>("epsilon"));
-    T decay = static_cast<T>(ctx.Attr<float>("decay"));
-    T momentum = static_cast<T>(ctx.Attr<float>("momentum"));
+    // // constants
+    // T epsilon = static_cast<T>(ctx.Attr<float>("epsilon"));
+    // T decay = static_cast<T>(ctx.Attr<float>("decay"));
+    // T momentum = static_cast<T>(ctx.Attr<float>("momentum"));
 
-    // outputs
-    auto& param_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("ParamOut"),
-                                      "Output", "ParamOut", "Rmsprop");
-    auto& mom_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("MomentOut"),
-                                    "Output", "MomentOut", "Rmsprop");
-    auto& mom_sqrt_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("MeanSquareOut"),
-                                         "Output", "MeanSquareOut", "Rmsprop");
-    auto& dev_ctx = ctx.template device_context<DeviceContext>();
+    // // outputs
+    // auto& param_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("ParamOut"),
+    //                                   "Output", "ParamOut", "Rmsprop");
+    // auto& mom_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("MomentOut"),
+    //                                 "Output", "MomentOut", "Rmsprop");
+    // auto& mom_sqrt_out = GET_DATA_SAFELY(ctx.Output<LoDTensor>("MeanSquareOut"),
+    //                                      "Output", "MeanSquareOut", "Rmsprop");
+    // auto& dev_ctx = ctx.template device_context<DeviceContext>();
 
-    ///// rmsprop优化算法
-    ///
-    /// ms_out[i] = rho * ms[i] + (1 - rho) * (g[i] * g[i]);
-    ///
-    /// mom_out[i] = momentum * mom[i] + lr *
-    /// (g[i] / ((float)sqrt(ms_out[i] + epsilon)));
-    ///
-    /// p_out[i] = p[i] - mom_out[i];
-    /// DLL_EXPORT int rmsprop(Context* ctx, const float* p,
-    /// const float* ms, const float* g, const float* mom,
-    /// float epsilon, float rho, float momentum, float lr,
-    /// float *ms_out, float *mom_out, float *p_out, int n)
-    int r = xpu::rmsprop(dev_ctx.x_context(), grad.template data<T>(),
-                         param.template data<T>(),
-                         meanSquare.template data<T>(), mom.template data<T>(),
-                         param_out.template mutable_data<T>(ctx.GetPlace()),
-                         mom_sqrt_out.template mutable_data<T>(ctx.GetPlace()),
-                         mom_out.template mutable_data<T>(ctx.GetPlace()),
-                         epsilon, decay, momentum, lr, param.numel());
+    // ///// rmsprop优化算法
+    // ///
+    // /// ms_out[i] = rho * ms[i] + (1 - rho) * (g[i] * g[i]);
+    // ///
+    // /// mom_out[i] = momentum * mom[i] + lr *
+    // /// (g[i] / ((float)sqrt(ms_out[i] + epsilon)));
+    // ///
+    // /// p_out[i] = p[i] - mom_out[i];
+    // /// DLL_EXPORT int rmsprop(Context* ctx, const float* p,
+    // /// const float* ms, const float* g, const float* mom,
+    // /// float epsilon, float rho, float momentum, float lr,
+    // /// float *ms_out, float *mom_out, float *p_out, int n)
+    // int r = xpu::rmsprop(dev_ctx.x_context(), grad.template data<T>(),
+    //                      param.template data<T>(),
+    //                      meanSquare.template data<T>(), mom.template data<T>(),
+    //                      param_out.template mutable_data<T>(ctx.GetPlace()),
+    //                      mom_sqrt_out.template mutable_data<T>(ctx.GetPlace()),
+    //                      mom_out.template mutable_data<T>(ctx.GetPlace()),
+    //                      epsilon, decay, momentum, lr, param.numel());
 
-    PADDLE_ENFORCE_XDNN_SUCCESS(r, "rmsprop");
+    // PADDLE_ENFORCE_XDNN_SUCCESS(r, "rmsprop");
   }
 };
 
 }  // namespace operators
 }  // namespace paddle
 
-namespace ops = paddle::operators;
-REGISTER_OP_XPU_KERNEL(
-    rmsprop,
-    ops::RmspropOpXPUKernel<paddle::platform::XPUDeviceContext, float>);
+// namespace ops = paddle::operators;
+// REGISTER_OP_XPU_KERNEL(
+//     rmsprop,
+//     ops::RmspropOpXPUKernel<paddle::platform::XPUDeviceContext, float>);
 #endif
