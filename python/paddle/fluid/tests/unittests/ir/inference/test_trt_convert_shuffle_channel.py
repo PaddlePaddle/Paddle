28ae5b05c84fafa7c0a3997731136df860888d16
xiaoxiaohehe001 <hitzsf@163.com>
2022-05-19 11:53:11 +0000

    add_shuffle

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_shuffle_channel.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_shuffle_channel.py
@@ -73,10 +73,7 @@ class TrtConvertShuffleChannelTest(TrtLayerAutoScanTest):
             self.dynamic_shape.opt_input_shape = {}
 
         def generate_trt_nodes_num(attrs, dynamic_shape):
-            if dynamic_shape == True:
-                return 0, 3
-            else:
-                return 1, 2
+            return 1, 2
 
         attrs = [
             program_config.ops[i].attrs
