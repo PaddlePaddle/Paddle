484ca4cd1463b6caebb0d085bbc613174d4cc734
danleifeng <52735331+danleifeng@users.noreply.github.com>
2022-05-25 12:32:07 +0800

    [GPUPS]fix gpups pscore (#42967)

--- paddle/fluid/framework/hogwild_worker.cc
+++ paddle/fluid/framework/hogwild_worker.cc
@@ -230,6 +230,10 @@ void HogwildWorker::TrainFiles() {
   device_reader_->Start();
   int cur_batch;
   int batch_cnt = 0;
+
+#if defined(PADDLE_WITH_HETERPS) && defined(PADDLE_WITH_CUDA)
+  platform::SetDeviceId(thread_id_);
+#endif
   while ((cur_batch = device_reader_->Next()) > 0) {
     for (auto &op : ops_) {
       bool need_skip = false;
@@ -255,6 +259,9 @@ void HogwildWorker::TrainFiles() {
     ++batch_cnt;
     PrintFetchVars();
     thread_scope_->DropKids();
+#ifdef PADDLE_WITH_HETERPS
+    dev_ctx_->Wait();
+#endif
   }
   timeline.Pause();
   VLOG(0) << "worker " << thread_id_ << " train cost " << timeline.ElapsedSec()
