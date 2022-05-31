3b365b13d37df4b580b613675fb73427290daf56
xingjing1 <2907374824@qq.com>
2022-05-31 14:00:29 +0000

    fix bugs

--- paddle/fluid/inference/tensorrt/convert/test_activation_op.cc
+++ paddle/fluid/inference/tensorrt/convert/test_activation_op.cc
@@ -48,21 +48,6 @@ TEST(SigmoidOpConverter, main) { test_activation("sigmoid"); }
 TEST(TanhOpConverter, main) { test_activation("tanh"); }
 
 TEST(Relu6OpConverter, main) { test_activation("relu6"); }
-
-TEST(EluOpConverter, main) { test_activation("elu"); }
-
-TEST(seluOpConverter, main) { test_activation("selu"); }
-
-TEST(SoftsignOpConverter, main) { test_activation("softsign"); }
-
-TEST(SoftplusOpConverter, main) { test_activation("softplus"); }
-
-TEST(ScaledTanhOpConverter, main) { test_activation("scaled_tanh"); }
-
-TEST(ThreasholdedReluOpConverter, main) {
-  test_activation("threasholded_relu");
-}
-
 }  // namespace tensorrt
 }  // namespace inference
 }  // namespace paddle
@@ -70,10 +55,4 @@ TEST(ThreasholdedReluOpConverter, main) {
 USE_OP_ITSELF(relu);
 USE_OP_ITSELF(sigmoid);
 USE_OP_ITSELF(tanh);
-USE_OP_ITSELF(elu);
-USE_OP_ITSELF(selu);
-USE_OP_ITSELF(softsign);
-USE_OP_ITSELF(softplus);
-USE_OP_ITSELF(scaled_tanh);
-USE_OP_ITSELF(threasholded_relu);
 USE_OP(relu6);
