96761d2ccbebf9bf2bbfc3e0c977c3c805d60f2b
Weilong Wu <veyron_wu@163.com>
2022-06-02 00:24:57 +0000

    import paddle.fluid

--- python/paddle/fluid/tests/unittests/autograd/test_autograd_functional_dynamic.py
+++ python/paddle/fluid/tests/unittests/autograd/test_autograd_functional_dynamic.py
@@ -18,6 +18,7 @@ import unittest
 
 import numpy as np
 import paddle
+import paddle.fluid as fluid
 import paddle.compat as cpt
 import paddle.nn.functional as F
 from paddle.autograd.functional import _as_tensors
