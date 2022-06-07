36b7de1e527df70fd71c689fa2c6a19ccd22af07
Wangzheee <634486483@qq.com>
2022-06-07 04:22:34 +0000

    support matmulv2 in multihead

--- paddle/fluid/framework/ir/trt_multihead_matmul_fuse_pass.cc
+++ paddle/fluid/framework/ir/trt_multihead_matmul_fuse_pass.cc
@@ -866,7 +866,7 @@ int TrtMultiHeadMatmulV2FusePass::BuildFusionV2(Graph* graph,
     auto* mul0_op_desc = mul0->Op();
 
     // all mul op has same input.
-    if (multihead_op_desc.HasAttr("Input_scale")) {
+    if (mul0_op_desc->HasAttr("Input_scale")) {
       multihead_op_desc.SetAttr("Input_scale",
                                 mul0_op_desc->GetAttr("Input_scale"));
     }
