463b42d0099a1fa99b9b0b6f28f15b6e06b9bfb6
yangguohao <70266361+yangguohao@users.noreply.github.com>
2022-06-07 18:16:55 +0800

    Update loss.py

--- python/paddle/nn/layer/loss.py
+++ python/paddle/nn/layer/loss.py
@@ -1370,12 +1370,11 @@ class TripletMarginLoss(Layer):
         .. code-block:: python
 
             import paddle
-            import paddle.nn.TripletMarginLoss
 
             input = paddle.to_tensor([[1, 5, 3], [0, 3, 2], [1, 4, 1]], dtype=paddle.float32)
             positive= paddle.to_tensor([[5, 1, 2], [3, 2, 1], [3, -1, 1]], dtype=paddle.float32)
             negative = paddle.to_tensor([[2, 1, -3], [1, 1, -1], [4, -2, 1]], dtype=paddle.float32)
-            triplet_margin_loss = TripletMarginLoss()
+            triplet_margin_loss = paddle.nn.TripletMarginLoss()
             loss = triplet_margin_loss(input, positive, negative, reduction='none')
             print(loss)
             # Tensor([0.        , 0.57496738, 0.        ])
