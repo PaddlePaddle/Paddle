54e9f28d2de324c2972f899bd5e7c083c281935b
zhaoyingli <86812880+zhaoyinglia@users.noreply.github.com>
2022-06-09 16:07:29 +0800

    use tempfile to place temporary files (#43316)

--- python/paddle/fluid/tests/unittests/auto_parallel/test_relaunch_with_gpt_planner.py
+++ python/paddle/fluid/tests/unittests/auto_parallel/test_relaunch_with_gpt_planner.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import tempfile
 import unittest
 import os
 import sys
@@ -23,14 +24,29 @@ from paddle.distributed.fleet.launch_utils import run_with_coverage
 
 class TestPlannerReLaunch(unittest.TestCase):
 
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
+
     def test_relaunch_with_planner(self):
-        from test_auto_parallel_relaunch import cluster_json
-        file_dir = os.path.dirname(os.path.abspath(__file__))
-        cluster_json_path = os.path.join(file_dir, "auto_parallel_cluster.json")
+        from test_auto_parallel_relaunch import cluster_json, mapping_josn
+
+        cluster_json_path = os.path.join(self.temp_dir.name,
+                                         "auto_parallel_cluster.json")
+        mapping_json_path = os.path.join(self.temp_dir.name,
+                                         "auto_parallel_rank_mapping.json")
+
         cluster_json_object = json.loads(cluster_json)
         with open(cluster_json_path, "w") as cluster_json_file:
             json.dump(cluster_json_object, cluster_json_file)
 
+        mapping_json_object = json.loads(mapping_josn)
+        with open(mapping_json_path, "w") as mapping_json_file:
+            json.dump(mapping_json_object, mapping_json_file)
+
+        file_dir = os.path.dirname(os.path.abspath(__file__))
         launch_model_path = os.path.join(
             file_dir, "auto_parallel_relaunch_with_gpt_planner.py")
 
@@ -40,27 +56,14 @@ class TestPlannerReLaunch(unittest.TestCase):
             coverage_args = []
 
         cmd = [sys.executable, "-u"] + coverage_args + [
-            "-m", "launch", "--cluster_topo_path", cluster_json_path,
-            "--enable_auto_mapping", "True", launch_model_path
+            "-m", "launch", "--log_dir", self.temp_dir.name,
+            "--cluster_topo_path", cluster_json_path, "--rank_mapping_path",
+            mapping_json_path, "--enable_auto_mapping", "True",
+            launch_model_path
         ]
         process = subprocess.Popen(cmd)
         process.wait()
         self.assertEqual(process.returncode, 0)
-
-        # Remove unnecessary files
-        if os.path.exists(cluster_json_path):
-            os.remove(cluster_json_path)
-        rank_mapping_json_path = os.path.join(
-            file_dir, "auto_parallel_rank_mapping.json")
-        if os.path.exists(rank_mapping_json_path):
-            os.remove(rank_mapping_json_path)
-        files_path = [path for path in os.listdir('.') if '.pkl' in path]
-        for path in files_path:
-            if os.path.exists(path):
-                os.remove(path)
-        log_path = os.path.join(file_dir, "log")
-        if os.path.exists(log_path):
-            shutil.rmtree(log_path)
 
 
 if __name__ == "__main__":
