4a0aa0cba87cc407444cafa65b8c07905e3dea96
chalsliu <craziecharles@gmail.com>
2022-06-10 21:10:58 +0800

    install tensorrt

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3303,6 +3303,9 @@ function check_coverage_build() {
 
 
 function update_cuda() {
+wget https://sys-p0.bj.bcebos.com/paddle-qa/fullchain_ce_test/trt/TensorRT-7.2.3.4.Ubuntu-16.04.x86_64-gnu.cuda-10.2.cudnn8.1.tar.gz --no-check-certificate
+tar -zxf TensorRT-7.2.3.4.Ubuntu-16.04.x86_64-gnu.cuda-10.2.cudnn8.1.tar.gz -C /usr/local
+cp -rf /usr/local/TensorRT-7.2.3.4/include/* /usr/include/ && cp -rf /usr/local/TensorRT-7.2.3.4/lib/* /usr/lib/
     return || exit 0
 #apt-get remove -y --no-install-recommends     cuda-cudart-10-1=${NV_CUDA_CUDART_VERSION}     cuda-compat-10-1
 #
