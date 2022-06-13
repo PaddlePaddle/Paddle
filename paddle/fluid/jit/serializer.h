564d3cd358215f0da0655c9fd6b121d2719628ee
0x45f <wangzhen45@baidu.com>
2022-06-13 11:22:35 +0000

    Using DeserializeFromStream to load tensor

--- paddle/fluid/jit/serializer.h
+++ paddle/fluid/jit/serializer.h
@@ -22,7 +22,9 @@
 #include <string>
 
 #include "paddle/fluid/framework/data_type.h"
+#include "paddle/fluid/framework/lod_tensor.h"
 #include "paddle/fluid/jit/layer.h"
+#include "paddle/fluid/platform/device_context.h"
 #include "paddle/phi/core/dense_tensor.h"
 
 namespace paddle {
@@ -126,66 +128,19 @@ class Deserializer {
                                  const std::set<std::string>& var_name) const {
     VLOG(3) << "ReadTensorData from: " << file_name;
     std::ifstream fin(file_name, std::ios::binary);
+    platform::DeviceContextPool& pool = platform::DeviceContextPool::Instance();
+    // TODO(dev): Support other devices
+    auto& dev_ctx = *pool.Get(phi::CPUPlace());
     VariableNameMap res;
     for (auto it = var_name.begin(); it != var_name.end(); it++) {
       VLOG(3) << "load Tensor: " << *it;
-      // TODO(dev): support other tensor type
-      DenseTensor dense_tensor;
-      LoadTensorFromBuffer(fin, &dense_tensor);
       Variable v;
-      auto* p = v.GetMutable<DenseTensor>();
-      *p = dense_tensor;
+      // TODO(dev): Support framework::Vocab
+      DenseTensor* dense_tesnor = v.GetMutable<DenseTensor>();
+      framework::DeserializeFromStream(fin, dense_tesnor, dev_ctx);
       res[*it] = v;
     }
     return res;
-  }
-
-  void LoadTensorFromBuffer(std::istream& buffer, DenseTensor* tensor) const {
-    {
-      // the 1st field, unit32_t version for LoDTensor
-      uint32_t version;
-      buffer.read(reinterpret_cast<char*>(&version), sizeof(version));
-      VLOG(3) << "version: " << version;
-    }
-    {
-      // the 2st field, LoD information
-      uint64_t lod_level;
-      buffer.read(reinterpret_cast<char*>(&lod_level), sizeof(lod_level));
-      auto& lod = *tensor->mutable_lod();
-      // TODO(dev): support lod;
-      lod.resize(lod_level);
-      VLOG(3) << "lod_level: " << lod_level;
-    }
-    {
-      // the 3st filed, Tensor
-      {
-        uint32_t version;
-        buffer.read(reinterpret_cast<char*>(&version), sizeof(version));
-        VLOG(3) << "tensor version: " << version;
-      }
-      framework::proto::VarType::TensorDesc desc;
-      {
-        int32_t size;
-        buffer.read(reinterpret_cast<char*>(&size), sizeof(size));
-        std::unique_ptr<char[]> buf(new char[size]);
-        buffer.read(reinterpret_cast<char*>(buf.get()), size);
-        desc.ParseFromArray(buf.get(), size);
-      }
-      {
-        // read tensor
-        std::vector<int64_t> dims;
-        dims.reserve(desc.dims().size());
-        std::copy(desc.dims().begin(), desc.dims().end(),
-                  std::back_inserter(dims));
-        VLOG(3) << "dims: " << phi::make_ddim(dims);
-        tensor->Resize(phi::make_ddim(dims));
-        void* buf;
-        size_t size = tensor->numel() * framework::SizeOfType(desc.data_type());
-        // TODO(dev): support other devices
-        buf = tensor->mutable_data(phi::CPUPlace());
-        buffer.read(static_cast<char*>(buf), size);
-      }
-    }
   }
 
   // void ReadExtraInfo(const std::string& file_name) const;
