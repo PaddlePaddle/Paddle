2dafbcc5df8584dd27f0191f31fd7bd1fd67d2af
JiabinYang <360788950@qq.com>
2022-06-13 11:05:41 +0000

    fix edvr_starganv2

--- paddle/phi/kernels/impl/elementwise_grad_kernel_impl.h
+++ paddle/phi/kernels/impl/elementwise_grad_kernel_impl.h
@@ -443,7 +443,7 @@ void MultiplyDoubleGradKernel(const Context& dev_ctx,
   if (ddout) {
     auto& place = *dev_ctx.eigen_device();
     // size(ddout) > size(ddx), ddout can't use memory of ddx using inplace
-    if (ddout->numel() > ddx.get_ptr()->numel()) {
+    if (ddx.get_ptr() && (ddout->numel() > ddx.get_ptr()->numel())) {
       phi::funcs::ElemwiseGradCompute<Context, T, MulGradDX<T>, MulGradDY<T>>(
           dev_ctx,
           ddx_safe,
