695a5708463a8feb641a688b044b5631aae53f73
Sławomir Siwek <slawomir.siwek@intel.com>
2022-06-14 05:33:02 -0700

    python code format

--- python/paddle/fluid/tests/unittests/ir/inference/test_mkldnn_matmul_activation_fuse_pass.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_mkldnn_matmul_activation_fuse_pass.py
@@ -72,22 +72,22 @@ class TestMatmulActivationMkldnnFusePass(PassAutoScanTest):
                                      inputs={"X": ["matmul_output"]},
                                      outputs={"Out": ["activation_output"]},
                                      threshold=draw(
-                                        st.floats(min_value=1.0,
-                                                  max_value=10.0)))
+                                         st.floats(min_value=1.0,
+                                                   max_value=10.0)))
         elif activation_type == "leaky_relu":
             activation_op = OpConfig(activation_type,
                                      inputs={"X": ["matmul_output"]},
                                      outputs={"Out": ["activation_output"]},
                                      alpha=draw(
-                                        st.floats(min_value=0.1,
-                                                  max_value=1.0)))
+                                         st.floats(min_value=0.1,
+                                                   max_value=1.0)))
         elif activation_type == "swish":
             activation_op = OpConfig(activation_type,
                                      inputs={"X": ["matmul_output"]},
                                      outputs={"Out": ["activation_output"]},
                                      beta=draw(
-                                        st.floats(min_value=0.1,
-                                                  max_value=1.0)))
+                                         st.floats(min_value=0.1,
+                                                   max_value=1.0)))
         elif activation_type == "clip":
             activation_op = OpConfig(
                 activation_type,
