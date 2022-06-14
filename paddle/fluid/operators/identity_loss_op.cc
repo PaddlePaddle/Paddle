6392d6de58619f673470684287f3182721452692
zhaorui chen <zhaoruic@graphcore.ai>
2022-06-14 13:26:36 +0800

    fix(): identity_loss uses cpu place as expected kernel type.

--- paddle/fluid/operators/identity_loss_op.cc
+++ paddle/fluid/operators/identity_loss_op.cc
@@ -25,6 +25,14 @@ namespace operators {
 class IdentityLossOp : public framework::OperatorWithKernel {
  public:
   using framework::OperatorWithKernel::OperatorWithKernel;
+
+ protected:
+  framework::OpKernelType GetExpectedKernelType(
+      const framework::ExecutionContext& ctx) const override {
+    return framework::OpKernelType(
+        OperatorWithKernel::IndicateVarDataType(ctx, "X"),
+        platform::CPUPlace());
+  }
 };
 
 class IdentityLossOpMaker : public framework::OpProtoAndCheckerMaker {
@@ -55,7 +63,7 @@ class IdentityLossGradOp : public framework::OperatorWithKernel {
       const framework::ExecutionContext& ctx) const override {
     auto input_data_type = OperatorWithKernel::IndicateVarDataType(
         ctx, framework::GradVarName("Out"));
-    return framework::OpKernelType(input_data_type, ctx.GetPlace());
+    return framework::OpKernelType(input_data_type, platform::CPUPlace());
   }
 };
 
