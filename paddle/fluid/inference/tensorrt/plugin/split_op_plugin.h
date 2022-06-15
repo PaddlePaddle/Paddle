536d062600a7fb14df4a5a50c6b1afa5bb623ad7
zhoutianzi666 <17801055074@163.com>
2022-06-15 13:25:10 +0000

    fix compile fail in cuda11.6

--- paddle/fluid/inference/tensorrt/plugin/split_op_plugin.h
+++ paddle/fluid/inference/tensorrt/plugin/split_op_plugin.h
@@ -14,7 +14,6 @@
 
 #pragma once
 
-#include <thrust/device_vector.h>
 #include <string>
 #include <utility>
 #include <vector>
@@ -92,8 +91,6 @@ class SplitPlugin : public PluginTensorRTV2Ext {
   bool same_shape_;
   std::vector<int> output_length_;
   std::vector<int> segment_offsets_;
-  thrust::device_vector<int> d_segment_offsets_;
-  thrust::device_vector<float*> d_output_ptrs_;
 
  private:
   void shareData(const SplitPlugin* another);
