2353db3a34528e582b671509b9d8f2bcc4e21941
Allen Guo <alleng@graphcore.ai>
2022-06-21 11:02:13 +0800

    [IPU] add activation ops (#43662)
    * add argmin and argsort ops (#800)
    
    * add argmin and arsort ops
    
    * Add dot bmm ops (#803)
    
    * add bmm
    
    * add dot op
    
    * clean CreateConst
    
    * clean CreateCast
    
    * add activation ops (#808)
    
    * add activation ops
    
    * fix 1function-redefined error

--- paddle/fluid/platform/device/ipu/popart_canonicalization/search_ops.cc
+++ paddle/fluid/platform/device/ipu/popart_canonicalization/search_ops.cc
@@ -83,7 +83,30 @@ Node *topk_handler(Graph *graph, Node *node) {
                 {"largest", int64_t{largest}},
                 {"sorted", int64_t{sorted}}});
   return CreateCast(graph, node, {var_i}, {GetOutputVarNode("Indices", node)},
-                    static_cast<int>(framework::proto::VarType::INT32));
+                    VarType::INT32);
+}
+
+Node *argsort_handler(Graph *graph, Node *node) {
+  auto *op = node->Op();
+  auto x_shape = GetInputVarNode("X", node)->Var()->GetShape();
+  auto axis_ = BOOST_GET_CONST(int, op->GetAttr("axis"));
+  auto descending_ = BOOST_GET_CONST(bool, op->GetAttr("descending"));
+  if (axis_ < 0) {
+    axis_ = axis_ + x_shape.size();
+  }
+  auto *dim_size =
+      CreateConst(graph, node, std::vector<int64_t>{x_shape[axis_]}, {1},
+                  ONNXDataType::INT64)
+          ->outputs.front();
+  int64_t largest = descending_ ? 1 : 0;
+  return CreateBaseOp(
+      graph, node, "popart_topk", {GetInputVarNode("X", node), dim_size},
+      {GetOutputVarNode("Out", node), GetOutputVarNode("Indices", node)},
+      {
+          {"axis", int64_t{axis_}},
+          {"largest", int64_t{largest}},
+          {"sorted", int64_t{0}},
+      });
 }
 
 }  // namespace
@@ -93,3 +116,4 @@ Node *topk_handler(Graph *graph, Node *node) {
 
 REGISTER_HANDLER(top_k, topk_handler);
 REGISTER_HANDLER(top_k_v2, topk_handler);
+REGISTER_HANDLER(argsort, argsort_handler);
