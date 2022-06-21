faced82db0a2308aaa3f9e475fd80d4b56eec2f0
zhoutianzi666 <17801055074@163.com>
2022-06-21 10:26:02 +0000

    revert to 7000

--- paddle/fluid/inference/tensorrt/convert/slice_op.cc
+++ paddle/fluid/inference/tensorrt/convert/slice_op.cc
@@ -77,7 +77,7 @@ class SliceOpConverter : public OpConverter {
 // failing to infer desired value，we can solve by adding infer shape in fc and
 // multihead_mul，but fc int8 can't run. so we have to let slice trt layer run
 // in >= 7234
-#if IS_TRT_VERSION_GE(7234)
+#if IS_TRT_VERSION_GE(7000)
       auto nchw_input_dims = input->getDimensions();
       nvinfer1::Dims trt_start_dims;
       trt_start_dims.nbDims = nchw_input_dims.nbDims;
