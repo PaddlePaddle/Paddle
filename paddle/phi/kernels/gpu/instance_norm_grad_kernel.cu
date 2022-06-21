829723f2361c17eb4bdf530a3c978d9878f4a058
Sing_chan <51314274+betterpig@users.noreply.github.com>
2022-06-21 14:52:47 +0800

    resort .cu headers, set clang-format not sort include block and consider .cu as main source file (#43633)

--- paddle/phi/kernels/gpu/instance_norm_grad_kernel.cu
+++ paddle/phi/kernels/gpu/instance_norm_grad_kernel.cu
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "paddle/phi/kernels/instance_norm_grad_kernel.h"
+
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/common/layout.h"
 #include "paddle/phi/core/kernel_registry.h"
@@ -19,7 +21,6 @@
 #include "paddle/phi/kernels/funcs/math_function.h"
 #include "paddle/phi/kernels/funcs/norm_utils.h"
 #include "paddle/phi/kernels/gpu/instance_norm_utils.h"
-#include "paddle/phi/kernels/instance_norm_grad_kernel.h"
 
 namespace phi {
 template <typename T, int BlockDim>
