3c314538fa726b60134589694b5790a6479ce114
chenjian <chenjian26@baidu.com>
2022-06-21 07:00:20 +0000

    fix a bug

--- paddle/fluid/memory/allocation/stat_allocator.h
+++ paddle/fluid/memory/allocation/stat_allocator.h
@@ -32,7 +32,7 @@ class StatAllocator : public Allocator {
  protected:
   void FreeImpl(phi::Allocation* allocation) override {
     if (platform::is_cpu_place(allocation->place()) ||
-        platform::is_cuda_pinned_place(place)) {
+        platform::is_cuda_pinned_place(allocation->place())) {
       HOST_MEMORY_STAT_UPDATE(Allocated, allocation->place().GetDeviceId(),
                               -allocation->size());
     } else {
