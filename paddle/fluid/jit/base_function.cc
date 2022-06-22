7673b39acd6e16c26560b7149519f012d56ab5eb
WangZhen <23097963+0x45f@users.noreply.github.com>
2022-06-22 11:57:17 +0800

    [JITLayer]Polish Layer implement and refine interface code (#43607)
    * Fix some TODO and polish code
    
    * Support GPU place
    
    * Fix layer_test ci error
    
    * Polish some code
    
    * Make GetFunction as const function
    
    * Remove COMMAND tar to fix CI error
    
    * Fix comments
    
    * Merge develop to fix codestyle error

--- paddle/fluid/jit/base_function.cc
+++ paddle/fluid/jit/base_function.cc
@@ -38,29 +38,28 @@ std::vector<std::string> FunctionSchema::GetOutputArgNames() {
   return output_arg_names;
 }
 
-void FunctionSchema::AddInputArg(std::string name, bool is_output) {
-  input_args.emplace_back(name, is_output);
+void FunctionSchema::AddInputArg(std::string name) {
+  input_args.emplace_back(name, false);
 }
 
-void FunctionSchema::AddOutputArg(std::string name, bool is_output) {
-  output_args.emplace_back(name, is_output);
+void FunctionSchema::AddOutputArg(std::string name) {
+  output_args.emplace_back(name, true);
 }
 
-BaseFunction::BaseFunction(
-    const framework::ProgramDesc &program_desc,
-    const std::vector<std::string> param_names_for_program,
-    const VariableNameMap &params_dict)
-    : program_desc_(program_desc) {
+BaseFunction::BaseFunction(const framework::ProgramDesc &program_desc,
+                           const std::vector<std::string> &param_names,
+                           const VariableNameMap &params_dict,
+                           const phi::Place &place)
+    : program_desc_(program_desc), place_(place) {
   // Parse FunctionSchema
-  // skip_var_name_ = program_desc_.GetFetchTargetNames();
   for (auto &in_name : program_desc_.GetFeedTargetNames()) {
-    schema_.AddInputArg(in_name, false);
+    schema_.AddInputArg(in_name);
   }
   for (auto &out_name : program_desc_.GetFetchTargetNames()) {
-    schema_.AddOutputArg(out_name, true);
+    schema_.AddOutputArg(out_name);
   }
   // share params into scope
-  SharePartialIntoScope(param_names_for_program, params_dict);
+  ShareParamsIntoScope(param_names, params_dict);
   VLOG(6) << framework::GenScopeTreeDebugInfo(&scope_);
   // remove feed fetch op
   RemoveFeedFetch();
@@ -70,7 +69,9 @@ void BaseFunction::FetchOutput(std::vector<Variable> *outs) {
   for (auto &out_name : schema_.GetOutputArgNames()) {
     VLOG(3) << "fetch out: " << out_name;
     auto *var = scope_.FindVar(out_name);
+    VLOG(3) << "after scope_.FindVar(out_name);";
     auto &src_tensor = var->Get<phi::DenseTensor>();
+    VLOG(3) << "var->Get<phi::DenseTensor>();";
     Variable v;
     auto *p = v.GetMutable<DenseTensor>();
     *p = src_tensor;
@@ -78,23 +79,30 @@ void BaseFunction::FetchOutput(std::vector<Variable> *outs) {
   }
 }
 
-void BaseFunction::ShareIntoScope(const VariableNameMap &ivals) {
-  VLOG(3) << "ivals size: " << ivals.size();
-  for (auto it = ivals.begin(); it != ivals.end(); ++it) {
-    VLOG(3) << "share into scope: " << it->first;
-    DenseTensor dense_tensor = it->second.Get<DenseTensor>();
-    auto *var = scope_.Var(it->first);
+void BaseFunction::ShareInputsIntoScope(const std::vector<Variable> &vars) {
+  VLOG(3) << "vars size: " << vars.size();
+  std::vector<std::string> ordered_input_names = schema_.GetInputArgNames();
+  PADDLE_ENFORCE_EQ(
+      vars.size(),
+      ordered_input_names.size(),
+      platform::errors::InvalidArgument(
+          "vars.size() should be equal to ordered_input_names.size()."));
+
+  for (size_t i = 0; i < vars.size(); i++) {
+    VLOG(3) << "share into scope: " << ordered_input_names[i];
+    auto &dense_tensor = vars[i].Get<DenseTensor>();
+    auto *var = scope_.Var(ordered_input_names[i]);
     auto *dst_tensor = var->GetMutable<DenseTensor>();
     *dst_tensor = dense_tensor;
   }
 }
 
-void BaseFunction::SharePartialIntoScope(
-    const std::vector<std::string> param_names_for_program,
+void BaseFunction::ShareParamsIntoScope(
+    const std::vector<std::string> &param_names,
     const VariableNameMap &params_dict) {
-  VLOG(3) << "ivals size: " << param_names_for_program.size();
-  for (size_t i = 0; i < param_names_for_program.size(); ++i) {
-    std::string name = param_names_for_program[i];
+  VLOG(3) << "param_names size: " << param_names.size();
+  for (size_t i = 0; i < param_names.size(); ++i) {
+    std::string name = param_names[i];
     Variable val = params_dict.find(name)->second;
     auto &dense_tensor = val.Get<DenseTensor>();
     VLOG(3) << "share into scope: " << name;
@@ -112,8 +120,15 @@ void BaseFunction::RemoveFeedFetch() {
     VLOG(3) << "op_size: " << op_size;
     for (int i = op_size - 1; i >= 0; i--) {
       auto op = all_ops[i];
-      if (op->Type() == "feed" || op->Type() == "fetch") {
-        VLOG(3) << "remove op type: " << op->Type() << ", index: " << i;
+      if (op->Type() == "feed") {
+        VLOG(3) << "remove op type: " << op->Type() << ", index: " << i
+                << ", var name: " << op->Input("X")[0];
+        block->RemoveVar(op->Input("X")[0]);
+        block->RemoveOp(i, i + 1);
+      } else if (op->Type() == "fetch") {
+        VLOG(3) << "remove op type: " << op->Type() << ", index: " << i
+                << ", var name: " << op->Output("Out")[0];
+        block->RemoveVar(op->Output("Out")[0]);
         block->RemoveOp(i, i + 1);
       }
     }
