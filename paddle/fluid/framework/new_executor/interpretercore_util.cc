e516cf407597a7a4d8817901dbdd8e6052edb600
zhiqiu <chenqiuliang@baidu.com>
2022-06-22 10:53:37 +0800

    separate variable scope and scope

--- paddle/fluid/framework/new_executor/interpretercore_util.cc
+++ paddle/fluid/framework/new_executor/interpretercore_util.cc
@@ -225,6 +225,7 @@ void build_variable_scope(const framework::BlockDesc& block,
     if (var_name == framework::kEmptyVarName) {
       continue;
     }
+
     if (var_desc->Persistable()) {
       auto* ptr = inner_scope->Var(var_name);
 
@@ -241,7 +242,7 @@ void build_variable_scope(const framework::BlockDesc& block,
               << ptr << "Variable Type "
               << static_cast<int>(var_desc->GetType());
     }
-    var_scope->SetVarDesc(var_name, var_desc);
+    var_scope->AddVar(var_name, var_desc);
   }
 }
 
@@ -279,6 +280,7 @@ void create_all_ops(const framework::BlockDesc& block,
 std::tuple<VariableValueMap, VariableIdMap> build_variable_map(
     const VariableNameMap& var_name_map,
     VariableScope* var_scope,
+    Scope* local_scope,
     bool enforce_exist = true) {
   VariableValueMap name2var;
   VariableIdMap name2id;
@@ -288,14 +290,14 @@ std::tuple<VariableValueMap, VariableIdMap> build_variable_map(
     vars.reserve(item.second.size());
 
     for (auto& var_name : item.second) {
-      if (!enforce_exist && !var_scope->HasVar(var_name)) {
+      if (!enforce_exist && !local_scope->FindVar(var_name)) {
         // skip the non-exist variable: such as recurrent_grad
         VLOG(4) << var_name << " don't exist in variable scope, skip it!";
         continue;
       }
+      auto* var = local_scope->FindVar(var_name);
       auto var_id = var_scope->VarId(var_name);
-      auto* in_var = var_scope->Var(var_id);
-      vars.push_back(in_var);
+      vars.push_back(var);
       ids.push_back(var_id);
     }
     name2var[item.first] = std::move(vars);
@@ -421,12 +423,12 @@ void build_op_func_list(const platform::Place& place,
       enforce_exist = false;
     }
     std::tie(ins_map, ins_name2id) =
-        build_variable_map(inputs_names, var_scope, enforce_exist);
+        build_variable_map(inputs_names, var_scope, local_scope, enforce_exist);
 
     VariableValueMap outs_map;
     VariableIdMap outs_name2id;
-    std::tie(outs_map, outs_name2id) =
-        build_variable_map(outputs_names, var_scope, enforce_exist);
+    std::tie(outs_map, outs_name2id) = build_variable_map(
+        outputs_names, var_scope, local_scope, enforce_exist);
 
     // step 1: build OpFuncNode
     OpFuncNode op_func_node;
@@ -573,9 +575,9 @@ void build_op_func_list(const platform::Place& place,
         for (auto& p : m) {
           auto* transformed_tensor =
               GetMutableLoDTensorOrSelectedRowsValueFromVar(
-                  var_scope->Var(p.first));
+                  local_scope->FindVar(var_scope->GetNameById(p.first)));
           auto* original_tensor = GetMutableLoDTensorOrSelectedRowsValueFromVar(
-              var_scope->Var(p.second));
+              local_scope->FindVar(var_scope->GetNameById(p.second)));
           original_tensor->ShareDataWith(*transformed_tensor);
           VLOG(4) << "Transfer inplace variable back form "
                   << var_scope->GetNameById(p.first) << " to "
@@ -600,7 +602,7 @@ void build_op_func_list(const platform::Place& place,
         new std::deque<std::shared_ptr<memory::Allocation>>();
 
     for (auto& var_name : delete_vars) {
-      auto* var = var_scope->FindVar(var_name);
+      auto* var = local_scope->FindVar(var_name);
       if (var == nullptr || skip_gc_vars.find(var_name) != skip_gc_vars.end()) {
         continue;
       }
