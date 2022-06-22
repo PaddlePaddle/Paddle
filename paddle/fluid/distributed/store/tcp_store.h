9896f259ffcbb1f4c52025171ef6795fe741da12
gongweibao <gongweibao@baidu.com>
2022-06-22 08:56:32 +0000

    fix windows compile

--- paddle/fluid/distributed/store/tcp_store.h
+++ paddle/fluid/distributed/store/tcp_store.h
@@ -75,7 +75,11 @@ class MasterDaemon {
   void InitControlFd();
   void CloseControlFd();
   void StopByControlFd();
-  std::array<SocketType, 2> _control_fd{{static_cast<SocketType>(-1), -1}};
+#ifndef _WIN32
+  std::array<SocketType, 2> _control_fd{{INVALID_SOCKET, -1}};
+#else
+  std::array<SocketType, 2> _control_fd{{-1, -1}};
+#endif
 };
 
 class TCPServer {
