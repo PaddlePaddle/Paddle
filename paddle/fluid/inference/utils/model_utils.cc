060de4b1695074932f3e8cf2c8912ab1fc1c9e90
jiweibo <jiweibo@baidu.com>
2022-06-24 14:04:11 +0000

    fix ci problem

--- paddle/fluid/inference/utils/model_utils.cc
+++ paddle/fluid/inference/utils/model_utils.cc
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 #include "paddle/fluid/inference/utils/model_utils.h"
-#include <unordered_set>
+#include <set>
 #include "paddle/fluid/framework/framework.pb.h"
 #include "paddle/fluid/framework/var_type_inference.h"
 #include "paddle/phi/common/data_type.h"
@@ -25,7 +25,7 @@ using paddle::framework::proto::VarType;
 
 // Get all model's weights and return the data_type, e.g., fp16/bf16 or fp32.
 phi::DataType GetModelPrecision(const framework::ProgramDesc& program) {
-  std::unordered_set<VarType::Type> model_types{
+  std::set<VarType::Type> model_types{
       VarType::FP32,
       VarType::FP16,
       VarType::BF16,
