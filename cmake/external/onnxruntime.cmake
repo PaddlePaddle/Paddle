cd8e4805cd34c25a1f08607bc5a8f85f911e250a
heliqi <1101791222@qq.com>
2022-06-24 13:17:06 +0000

    add message test

--- cmake/external/onnxruntime.cmake
+++ cmake/external/onnxruntime.cmake
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+message(
+  "-------------------------------------------------------------------------------------"
+)
+
 if(NOT WITH_ONNXRUNTIME)
   return()
 endif()
