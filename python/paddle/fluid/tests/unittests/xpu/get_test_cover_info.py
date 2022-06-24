44c9c3e2cba5ab53a6a06d22686e01f3995bc3f0
helen88 <z8hanghuan@126.com>
2022-06-24 02:52:51 +0000

    modify xpu unittest to support fp64, *test=kunlun

--- python/paddle/fluid/tests/unittests/xpu/get_test_cover_info.py
+++ python/paddle/fluid/tests/unittests/xpu/get_test_cover_info.py
@@ -83,7 +83,8 @@ type_dict_str_to_numpy = {
 }
 
 xpu_test_op_white_list = []
-xpu_test_type_white_list = ['float64']
+xpu1_test_type_white_list = ['float64']
+xpu2_test_type_white_list = []
 xpu_test_op_type_white_list = []
 xpu_test_device_op_white_list = []
 xpu_test_device_op_type_white_list = []
@@ -108,7 +109,7 @@ def get_op_white_list():
 def get_type_white_list():
     xpu_version = core.get_xpu_device_version(0)
     version_str = "xpu2" if xpu_version == core.XPUVersion.XPU2 else "xpu1"
-    type_white_list = xpu_test_type_white_list if version_str == "xpu1" else []
+    type_white_list = xpu1_test_type_white_list if version_str == "xpu1" else xpu2_test_type_white_list
     if os.getenv('XPU_TEST_TYPE_WHITE_LIST') is not None:
         type_white_list.extend(
             os.getenv('XPU_TEST_TYPE_WHITE_LIST').strip().split(','))
