dab4609996c85e0e5005cbdff138d19ac502ae82
zkh2016 <zhangkaihuo@baidu.com>
2022-06-28 07:54:12 +0000

    resolve conflict

--- paddle/phi/kernels/sparse/cpu/convolution_kernel.cc
+++ paddle/phi/kernels/sparse/cpu/convolution_kernel.cc
@@ -14,8 +14,8 @@ limitations under the License. */
 
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/tensor_meta.h"
+#include "paddle/phi/core/tensor_utils.h"
 #include "paddle/phi/core/visit_type.h"
-#include "paddle/phi/kernels/copy_kernel.h"
 #include "paddle/phi/kernels/funcs/blas/blas.h"
 #include "paddle/phi/kernels/sparse/cpu/convolution.h"
 
