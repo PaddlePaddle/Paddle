2642ef1c887dee82f456913edd00167287e61009
zmxdream <zhangminxu01@baidu.com>
2022-06-29 21:22:25 +0800

    revert optimize dymf kernel. test=develop

--- paddle/fluid/framework/fleet/heter_ps/hashtable_kernel.cu
+++ paddle/fluid/framework/fleet/heter_ps/hashtable_kernel.cu
@@ -90,7 +90,6 @@ __global__ void dy_mf_search_kernel(Table* table,
                                     size_t len,
                                     size_t pull_feature_value_size) {
   const size_t i = blockIdx.x * blockDim.x + threadIdx.x;
-  // return;
   if (i < len) {
     auto it = table->find(keys[i]);
 
