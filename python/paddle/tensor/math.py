0a18d51833f173eff0b9c1e866a980f10ae4de02
mrcangye <mrcangye@88.com>
2022-07-03 20:03:17 +0800

    Modify the output result annotation under the lerp function

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -3878,7 +3878,7 @@ def lerp(x, y, weight, name=None):
             y = paddle.empty([4], dtype='float32')
             y.fill_(10.)
             out = paddle.lerp(x, y, 0.5)
-            # out: [5.5., 6., 6.5, 7.]
+            # out: [5.5, 6., 6.5, 7.]
 
     """
     if in_dygraph_mode():
