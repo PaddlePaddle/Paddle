36593928be2a3cfe9e615979ad832c201cd4b2db
0x45f <wangzhen45@baidu.com>
2022-07-04 11:19:45 +0000

    Polish runtime error code

--- python/paddle/fluid/dygraph/dygraph_to_static/error.py
+++ python/paddle/fluid/dygraph/dygraph_to_static/error.py
@@ -274,10 +274,17 @@ class ErrorData(object):
         bottom_error_message = error_value_lines[empty_line_idx + 1:]
         revise_suggestion = self._create_revise_suggestion(bottom_error_message)
 
-        user_filepath = ''
         error_traceback = []
         user_code_traceback_index = []
         pattern = 'File "(?P<filepath>.+)", line (?P<lineno>.+), in (?P<function_name>.+)'
+
+        # Distinguish user code and framework code using static_info_map
+        static_info_map = {}
+        for k, v in self.origin_info_map.items():
+            origin_filepath = v.location.filepath
+            origin_lineno = v.location.lineno
+            static_info_map[(origin_filepath, origin_lineno)] = k
+
         for i in range(0, len(error_value_lines_strip), 2):
             if error_value_lines_strip[i].startswith("File "):
                 re_result = re.search(pattern, error_value_lines_strip[i])
@@ -285,16 +292,11 @@ class ErrorData(object):
                 code = error_value_lines_strip[
                     i + 1] if i + 1 < len(error_value_lines_strip) else ''
 
-                orig_info = self.origin_info_map.get(
-                    (tmp_filepath, int(lineno_str)))
-                if orig_info is not None:
+                if static_info_map.get((tmp_filepath, int(lineno_str))):
                     user_code_traceback_index.append(len(error_traceback))
-                    error_traceback.append(
-                        (orig_info.location.filepath, orig_info.location.lineno,
-                         orig_info.function_name, orig_info.source_code))
-                else:
-                    error_traceback.append(
-                        (tmp_filepath, int(lineno_str), function_name, code))
+
+                error_traceback.append(
+                    (tmp_filepath, int(lineno_str), function_name, code))
 
         error_frame = []
         # Add user code traceback
