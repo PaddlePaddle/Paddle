7e525168d9710ccbcce46a7c8fdcc4052924fd1e
XieYunshen <1084314248@qq.com>
2022-07-05 21:05:07 +0800

    fix code format

--- paddle/scripts/conda_build.py
+++ paddle/scripts/conda_build.py
@@ -1,13 +1,13 @@
 #!/bin/python
 
 # Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,8 +24,7 @@ import time
 
 def parse_args():
     parser = argparse.ArgumentParser("conda build for paddlepaddle version")
-    parser.add_argument(
-        "--paddle_version",
+    parser.add_argument("--paddle_version",
         type=str,
         required=True,
         help="paddle version for conda build.")
@@ -35,6 +34,7 @@ def parse_args():
 
 
 class ConstantVar:
+
     def __init__(self):
         self.build = r"""
 build:
