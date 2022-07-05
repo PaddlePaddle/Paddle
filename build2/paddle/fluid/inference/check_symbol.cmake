7b56bfa077605ec8b6c367a0982dfb1d82430760
seemingwang <zsasuke@qq.com>
2022-07-05 04:00:16 +0000

    add config

--- build2/paddle/fluid/inference/check_symbol.cmake
+++
@@ -1,4 +0,0 @@
-execute_process(COMMAND sh -c "/wangxianming/myPro/Paddle/paddle/fluid/inference/check_symbol.sh /wangxianming/myPro/Paddle/build2/paddle/fluid/inference/libpaddle_inference.so" RESULT_VARIABLE symbol_res)
-if(NOT "${symbol_res}" STREQUAL "0")
-  message(FATAL_ERROR "Check symbol failed.")
-endif()
