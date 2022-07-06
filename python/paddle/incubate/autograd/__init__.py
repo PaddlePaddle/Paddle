dff4b0c0239feea5108b04528ce68cfa718578ab
Xiaoxu Chen <chenxx_id@163.com>
2022-07-06 03:17:29 +0000

    move _gradients to primapi and rename to grad

--- python/paddle/incubate/autograd/__init__.py
+++ python/paddle/incubate/autograd/__init__.py
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from paddle.autograd.functional import Hessian, Jacobian, jvp, vjp
-from .primx import prim2orig
-from .utils import enable_prim, disable_prim, prim_enabled
+from paddle.incubate.autograd.primapi import forward_grad, grad
+from paddle.incubate.autograd.primx import prim2orig
+from paddle.incubate.autograd.utils import (disable_prim, enable_prim,
+                                            prim_enabled)
 
 __all__ = [  # noqa
-    'vjp', 'jvp', 'Jacobian', 'Hessian', 'prim2orig', 'enable_prim',
-    'disable_prim', 'prim_enabled'
+    'vjp', 'jvp', 'Jacobian', 'Hessian', 'enable_prim', 'disable_prim',
+    'forward_grad', 'grad'
 ]
--- python/paddle/incubate/autograd/primapi.py
+++ python/paddle/incubate/autograd/primapi.py
@@ -16,26 +16,26 @@ import typing
 
 import paddle.autograd.utils as tensor_utils
 import paddle.incubate.autograd.utils as prim_utils
-from paddle.fluid import framework
+from paddle.fluid import backward, framework
 from paddle.incubate.autograd import primx
 
 
 @framework.static_only
-def forward_gradients(targets, inputs, input_gradients=None):
+def forward_grad(outputs, inputs, grad_inputs=None):
     """Forward mode of automatic differentiation.
 
     .. note::
         **ONLY available in the static mode and primitive operators.**
 
     Args:
-        targets: The target tensor or tensors
+        outputs: The output tensor or tensors
         inputs: The input tensor or tensors
-        input_gradients: The gradient Tensor or Tensors of inputs which has 
+        grad_inputs: The gradient Tensor or Tensors of inputs which has 
             the same shape with inputs, Defaults to None, in this case is 
             equivalent to all ones .
 
     Returns:
-        target_gradients (Tensor|Sequence[Tensor]): The gradients for targets.
+        grad_outputs (Tensor|Sequence[Tensor]): The gradients for outputs.
 
     Examples:
 
@@ -53,7 +53,7 @@ def forward_gradients(targets, inputs, input_gradients=None):
             with paddle.static.program_guard(main_program, startup_program):
                 x = paddle.static.data('x', shape=[1], dtype='float32')
                 y = x * x 
-                y_grad = paddle.incubate.autograd.forward_gradients(y, x)
+                y_grad = paddle.incubate.autograd.forward_grad(y, x)
                 paddle.incubate.autograd.prim2orig()
 
             exe = paddle.static.Executor()
@@ -66,10 +66,10 @@ def forward_gradients(targets, inputs, input_gradients=None):
             paddle.disable_static()
     """
     if not prim_utils.prim_enabled():
-        raise RuntimeError('forward_gradients must be running on primitive'
+        raise RuntimeError('forward_grad must be running on primitive'
                            'operators, use enable_prim to turn it on.')
 
-    if not isinstance(targets, (framework.Variable, typing.Sequence)):
+    if not isinstance(outputs, (framework.Variable, typing.Sequence)):
         raise TypeError(f'Expected targets is Tensor|Sequence[Tesnor], '
                         f'but got {type(targets)}.')
 
@@ -77,8 +77,8 @@ def forward_gradients(targets, inputs, input_gradients=None):
         raise TypeError(f'Expected inputs is Tensor|Sequence[Tesnor], '
                         f'but got {type(inputs)}.')
 
-    ys, xs, xs_dot = tensor_utils.as_tensors(targets), tensor_utils.as_tensors(
-        inputs), tensor_utils.as_tensors(input_gradients)
+    ys, xs, xs_dot = tensor_utils.as_tensors(outputs), tensor_utils.as_tensors(
+        inputs), tensor_utils.as_tensors(grad_inputs)
 
     block = framework.default_main_program().current_block()
     if any(x.block != block for x in xs + ys):
@@ -90,4 +90,95 @@ def forward_gradients(targets, inputs, input_gradients=None):
     ad = primx.Transform(ys[0].block)
     _, ys_dot = ad.linearize(xs, ys, xs_dot)
 
-    return ys_dot[0] if isinstance(targets, framework.Variable) else ys_dot
+    return ys_dot[0] if isinstance(outputs, framework.Variable) else ys_dot
+
+
+@framework.static_only
+def grad(outputs, inputs, grad_outputs=None):
+    """Reverse mode of automatic differentiation.
+
+    .. note::
+        **ONLY available in the static mode and primitive operators**
+
+    Args:
+        outputs (Tensor|Sequence[Tensor]): The output Tensor or Tensors.
+        inputs (Tensor|Sequence[Tensor]): The input Tensor or Tensors.
+        grad_outputs (Tensor|Sequence[Tensor]): The gradient Tensor or 
+            Tensors of outputs which has the same shape with outputs, Defaults 
+            to None, in this case is equivalent to all ones .
+
+    Returns:
+        grad_inputs (Tensor|Tensors): The gradients for inputs. 
+
+    Examples:
+
+        .. code-block:: python
+
+            import numpy as np
+            import paddle
+            paddle.enable_static()
+            paddle.incubate.autograd.enable_prim()
+            startup_program = paddle.static.Program()
+            main_program = paddle.static.Program()
+            with paddle.static.program_guard(main_program, startup_program):
+                x = paddle.static.data('x', shape=[1], dtype='float32')
+                x.stop_gradients = False
+                y = x * x 
+                x_grad = paddle.incubate.autograd.grad(y, x)
+                paddle.incubate.autograd.prim2orig()
+            exe = paddle.static.Executor()
+            exe.run(startup_program)
+            x_grad = exe.run(main_program, feed={'x': np.array([2.]).astype('float32')}, fetch_list=[x_grad])
+            print(x_grad)
+            # [array([4.], dtype=float32)]
+            paddle.incubate.autograd.disable_prim()
+            paddle.disable_static()
+    """
+
+    if not utils.prim_enabled():
+        return backward.gradients(outputs, inputs, grad_outputs)
+
+    if not isinstance(outputs, (framework.Variable, typing.Sequence)):
+        raise TypeError(f'Expected outputs is Tensor|Sequence[Tesnor], '
+                        f'but got {type(outputs)}.')
+
+    if not isinstance(inputs, (framework.Variable, typing.Sequence)):
+        raise TypeError(f'Expected inputs is Tensor|Sequence[Tesnor], '
+                        f'but got {type(inputs)}.')
+
+    ys, xs, ys_bar = as_tensors(outputs), as_tensors(inputs), as_tensors(
+        grad_outputs)
+    block = default_main_program().current_block()
+    if any((x is not None or x.block != block) for x in xs + ys):
+        raise RuntimeError(
+            'Variable in inputs and outputs should be None or in current block of main program'
+        )
+
+    # TODO(Tongxin) without any prior knowledge about whether the program
+    # is completely lowered to primitive ops, it's mandatory to run the lowering
+    # pass once and again. This is obviously inefficient and needs to be
+    # optimized.
+    primx.orig2prim(block)
+    ad = primx.Transform(block)
+    xs_dot, ys_dot = ad.linearize(xs, ys)
+    if any(var is None for var in ys_dot):
+        raise RuntimeError(
+            'Grads cannot be computed. The given outputs does not depend on inputs'
+        )
+    ys_bar, xs_bar = ad.transpose(ys_dot, xs_dot, ys_bar)
+
+    # remove xs_dot and their constructor ops
+    op_indexes = []
+    for var in xs_dot:
+        if var is not None:
+            op_index = block.ops.index(var.op)
+            if op_index < 0:
+                raise ValueError(
+                    f'op_index should be greater than or equal to 0, but op_index={op_index}.'
+                )
+            op_indexes.append(op_index)
+
+    ad.erase_ops(sorted(op_indexes))
+    ad.erase_dots(xs_dot)
+
+    return xs_bar[0] if isinstance(inputs, framework.Vairable) else xs_bar
