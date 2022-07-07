e35f0628e6bb854bb75228b5a0e09df4a6bb61de
tianshuo78520a <707759223@qq.com>
2022-07-07 16:39:55 +0800

    recover coverage build size check (#44153)

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3454,6 +3454,7 @@ function main() {
         check_diff_file_for_coverage
         cmake_gen_and_build ${PYTHON_ABI:-""} ${parallel_number}
         enable_unused_var_check
+        check_coverage_build
         ;;
       gpu_cicheck_coverage)
         parallel_test
