6984fbca87ffab9659bc411bac756c2c83f369f9
Allen Guo <alleng@graphcore.ai>
2022-07-07 13:09:25 +0800

    [IPU] support dy2static for IPU merge code (#43770)
    * feat(): dynamic_to_static support for ipu.
    
    * fix(): format fix.
    
    * fix format
    
    * fix cpplint error
    
    * use phi::errors
    
    * fix format
    
    * fix format
    
    * fix(): add api to restore patched function.
    
    * fix(): identity_loss uses cpu place as expected kernel type.
    
    * doc(): add IPU dy2static related docs.
    
    * fix(): combine test cases.
    
    * fix format
    
    * fix comment
    
    * fix format
    
    * apply comment
    
    * fix compiling
    
    * fix(): align docs.
    
    * fix(): fix identity_loss function docs.
    
    * fix(): adjust mean and sum in identity_loss.
    
    * fix(): minor docs.
    
    * move API to paddle.incubate.identity_loss
    
    * fix UT
    
    Co-authored-by: zhaorui chen <zhaoruic@graphcore.ai>

--- paddle/fluid/platform/device/ipu/ipu_executor.cc
+++ paddle/fluid/platform/device/ipu/ipu_executor.cc
@@ -213,8 +213,13 @@ void Executor::Run(const std::vector<const Tensor *> &inputs,
       optimizer = compiler_resources_->eval_optimizer.get();
     } else {
       VLOG(10) << "Update learning_rate";
-      auto new_lr =
-          GetSingleVarFromScope<float>(scope_, compiler_resources_->lr_var);
+      float new_lr;
+      if (ipu_strategy_->is_dynamic) {
+        new_lr = ipu_strategy_->lr;
+      } else {
+        new_lr =
+            GetSingleVarFromScope<float>(scope_, compiler_resources_->lr_var);
+      }
       VLOG(10) << "New Lr: " << new_lr;
       optimizer = compiler_resources_->UpdateOptimizer(new_lr);
     }
