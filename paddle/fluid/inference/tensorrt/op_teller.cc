7a2ce3b653183c7fa2c35559e233a813727a11a4
zhoutianzi666 <17801055074@163.com>
2022-07-07 03:32:20 +0000

    fix_convtranspose

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -1949,6 +1949,18 @@ bool OpTeller::Tell(const framework::ir::Node* node,
       }
 
       if (op_type == "conv3d_transpose") {
+        // trt doen't support output_padding,
+        // output_padding is set when stride > 1
+        if (desc.HasAttr("output_padding")) {
+          const std::vector<int> output_padding =
+              BOOST_GET_CONST(std::vector<int>, desc.GetAttr("output_padding"));
+          if (output_padding.size() > 0) {
+            int max_padding =
+                *std::max_element(output_padding.begin(), output_padding.end());
+            if (max_padding > 0) return false;
+          }
+        }
+
         if (!desc.HasAttr("dilations")) {
           return false;
         } else {
