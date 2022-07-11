22c3dbe956270ff468d056eee14f4df2ce90c7f4
chenruibiao <chenruibiao@baidu.com>
2022-07-11 20:43:01 +0800

    Disable windows inference test

--- tools/windows/run_unittests.sh
+++ tools/windows/run_unittests.sh
@@ -183,7 +183,8 @@ disable_win_inference_test="^trt_quant_int8_yolov3_r50_test$|\
 ^test_unsqueeze2_eltwise_fuse_pass$|\
 ^test_parallel_executor_seresnext_with_fuse_all_reduce_gpu$|\
 ^test_parallel_executor_seresnext_with_reduce_gpu$|\
-^test_api_impl$"
+^test_api_impl\
+test_tensordot$"
 
 
 # /*==========Fixed Disabled Windows CPU OPENBLAS((PR-CI-Windows-OPENBLAS)) unittests==============================*/
