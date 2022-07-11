0970952e9d59bf32d5c506c2cb6d2d4b30185184
zhaoyingli <zhaoyingli@baidu.com>
2022-07-11 12:01:46 +0800

    fix cmakelist

--- python/paddle/distributed/auto_parallel/engine.py
+++ python/paddle/distributed/auto_parallel/engine.py
@@ -82,6 +82,7 @@ class Engine:
         self._feed_vars = {}
         self._fetch_vars = {}
         self._planners = {}
+
         self._planned_mode = None
         self._gradient_scale = True
         self._optimizer = None
