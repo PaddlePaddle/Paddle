4fd224e7895d8c65e51311446d0d913cdfdd4767
zkh2016 <zhangkaihuo@baidu.com>
2022-07-12 07:49:11 +0000

    fix example

--- python/paddle/incubate/sparse/unary.py
+++ python/paddle/incubate/sparse/unary.py
@@ -491,7 +491,7 @@ def coalesce(x):
             indices = [[0, 0, 1], [1, 1, 2]]
             values = [1.0, 2.0, 3.0]
             sp_x = sparse.sparse_coo_tensor(indices, values)
-            sp_x = sparse.coalesced(sp_x)
+            sp_x = sparse.coalesce(sp_x)
             print(sp_x.indices())
             #[[0, 1], [1, 2]]
             print(sp_x.values())
