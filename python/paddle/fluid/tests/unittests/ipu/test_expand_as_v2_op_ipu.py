e47e82d0a31f2febe5161373fbaa930de30b1e03
Allen Guo <alleng@graphcore.ai>
2022-07-12 11:18:17 +0800

    [IPU] add more UTs 1/N (#44207)
    * add authors
    
    Co-authored-by: Allen Guo <alleng@graphcore.ai>
    Co-authored-by: Zhixin Yao <zhixiny@graphcore.ai>
    Co-authored-by: Zhaorui Chen <zhaoruic@graphcore.ai>
    
    * squash py changes 1/N
    
    Co-authored-by: Zhixin Yao <zhixiny@graphcore.ai>
    Co-authored-by: Zhaorui Chen <zhaoruic@graphcore.ai>

--- python/paddle/fluid/tests/unittests/ipu/test_expand_as_v2_op_ipu.py
+++ python/paddle/fluid/tests/unittests/ipu/test_expand_as_v2_op_ipu.py
@@ -29,27 +29,33 @@ class TestBase(IPUOpTest):
         self.set_training()
         self.set_data_feed()
         self.set_feed_attr()
-        self.set_op_attrs()
 
     def set_data_feed(self):
-        data = np.random.uniform(size=[2, 3, 1])
-        self.feed_fp32 = {'in_0': data.astype(np.float32)}
-        self.feed_fp16 = {'in_0': data.astype(np.float16)}
+        data_x = np.random.uniform(size=[1, 3])
+        data_y = np.random.uniform(size=[2, 2, 3])
+        self.feed_fp32 = {
+            'x': data_x.astype(np.float32),
+            'y': data_y.astype(np.float32)
+        }
+        self.feed_fp16 = {
+            'x': data_x.astype(np.float16),
+            'y': data_y.astype(np.float16)
+        }
 
     def set_feed_attr(self):
         self.feed_shape = [x.shape for x in self.feed_fp32.values()]
         self.feed_list = list(self.feed_fp32.keys())
-
-    def set_op_attrs(self):
-        self.attrs = {'fill_value': 0.3, 'dtype': 'float32'}
+        self.feed_dtype = [x.dtype for x in self.feed_fp32.values()]
 
     @IPUOpTest.static_graph
     def build_model(self):
         x = paddle.static.data(name=self.feed_list[0],
                                shape=self.feed_shape[0],
-                               dtype='float32')
-        x_fill = paddle.full_like(x, **self.attrs)
-        out = paddle.fluid.layers.elementwise_add(x_fill, x_fill)
+                               dtype="float32")
+        y = paddle.static.data(name=self.feed_list[1],
+                               shape=self.feed_shape[1],
+                               dtype="float32")
+        out = paddle.expand_as(x, y)
         self.fetch_list = [out.name]
 
     def run_model(self, exec_mode):
@@ -65,27 +71,33 @@ class TestBase(IPUOpTest):
 
 class TestCase1(TestBase):
 
-    def set_op_attrs(self):
-        self.attrs = {'fill_value': 3, 'dtype': 'int32'}
+    def set_data_feed(self):
+        data_x = np.random.uniform(size=[2, 3])
+        data_y = np.random.uniform(size=[2, 4, 2, 3])
+        self.feed_fp32 = {
+            'x': data_x.astype(np.float32),
+            'y': data_y.astype(np.float32)
+        }
+        self.feed_fp16 = {
+            'x': data_x.astype(np.float16),
+            'y': data_y.astype(np.float16)
+        }
 
 
-class TestError(TestBase):
+@unittest.skip("corresponding dimensions must have the same value.")
+class TestCase2(TestBase):
 
-    @IPUOpTest.static_graph
-    def build_model(self):
-        x = paddle.fluid.data('x', [-1, 3, 13], 'float32')
-        x_fill = paddle.full_like(x, **self.attrs)
-        out = paddle.fluid.layers.elementwise_add(x_fill, x_fill)
-        self.fetch_list = [out.name]
-
-    def test(self):
-        self.build_model()
-
-        def test_error():
-            self.run_op_test(IPUOpTest.ExecutionMode.IPU_FP32)
-
-        self.assertRaisesRegex(Exception, "Please check tensor shape setting",
-                               test_error)
+    def set_data_feed(self):
+        data_x = np.random.uniform(size=[2, 3])
+        data_y = np.random.uniform(size=[2, 4, 3, 3])
+        self.feed_fp32 = {
+            'x': data_x.astype(np.float32),
+            'y': data_y.astype(np.float32)
+        }
+        self.feed_fp16 = {
+            'x': data_x.astype(np.float16),
+            'y': data_y.astype(np.float16)
+        }
 
 
 if __name__ == "__main__":
