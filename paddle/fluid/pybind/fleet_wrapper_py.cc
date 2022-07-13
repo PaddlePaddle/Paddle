4a3569a671a24610bdd016a400375a7d4135e640
zmxdream <zhangminxu01@baidu.com>
2022-07-13 13:51:49 +0800

    fix code format

--- paddle/fluid/pybind/fleet_wrapper_py.cc
+++ paddle/fluid/pybind/fleet_wrapper_py.cc
@@ -41,7 +41,8 @@ namespace py = pybind11;
 namespace paddle {
 namespace pybind {
 void BindFleetWrapper(py::module* m) {
-  py::class_<framework::FleetWrapper, std::shared_ptr<framework::FleetWrapper>>(*m, "Fleet")
+  py::class_<framework::FleetWrapper, std::shared_ptr<framework::FleetWrapper>>(
+      *m, "Fleet")
       .def(py::init([](){ return framework::FleetWrapper::GetInstance(); }))
       .def("push_dense", &framework::FleetWrapper::PushDenseVarsSync)
       .def("pull_dense", &framework::FleetWrapper::PullDenseVarsSync)
