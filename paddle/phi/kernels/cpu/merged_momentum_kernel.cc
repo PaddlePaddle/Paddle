b63ff683b49c7e650b94cf0127ae72e43dba5cf6
liuyuang <liuyuang@baidu.com>
2022-07-13 10:36:08 +0800

    migrate kernel impl

---
+++ paddle/phi/kernels/cpu/merged_momentum_kernel.cc
@@ -0,0 +1,73 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "paddle/phi/kernels/merged_momentum_kernel.h"
+#include "paddle/phi/common/amp_type_traits.h"
+#include "paddle/phi/kernels/cpu/merged_momentum_functor.h"
+
+namespace phi {
+
+template <typename T, typename Context>
+void MergedMomentumKernel(const Context& dev_ctx,
+                          const std::vector<const DenseTensor*>& param,
+                          const std::vector<const DenseTensor*>& grad,
+                          const std::vector<const DenseTensor*>& velocity,
+                          const std::vector<const DenseTensor*>& learning_rate,
+                          const std::vector<const DenseTensor*>& master_param,
+                          float mu,
+                          bool use_nesterov,
+                          const std::vector<std::string>& regularization_method,
+                          const std::vector<float>& regularization_coeff,
+                          bool multi_precision,
+                          float rescale_grad,
+                          std::vector<DenseTensor*> param_out,
+                          std::vector<DenseTensor*> velocity_out,
+                          std::vector<DenseTensor*> master_param_out) {
+  using MPType = typename phi::dtype::MPTypeTrait<T>::Type;
+  if (multi_precision) {
+    InnerCompute<MPType, Context, MPType, T>(dev_ctx,
+                                             param,
+                                             grad,
+                                             velocity,
+                                             learning_rate,
+                                             master_param,
+                                             mu,
+                                             use_nesterov,
+                                             regularization_method,
+                                             regularization_coeff,
+                                             rescale_grad,
+                                             multi_precision,
+                                             param_out,
+                                             velocity_out,
+                                             master_param_out);
+  } else {
+    InnerCompute<T, Context, MPType, T>(dev_ctx,
+                                        param,
+                                        grad,
+                                        velocity,
+                                        learning_rate,
+                                        master_param,
+                                        mu,
+                                        use_nesterov,
+                                        regularization_method,
+                                        regularization_coeff,
+                                        rescale_grad,
+                                        multi_precision,
+                                        param_out,
+                                        velocity_out,
+                                        master_param_out);
+  }
+}
+
+}  // namespace phi
