f35d79435ee02e57e5dadcc554f44287c3db1918
Sławomir Siwek <slawomir.siwek@intel.com>
2022-07-14 05:05:13 -0700

    UT

---
+++ python/paddle/fluid/tests/unittests/ir/inference/test_conv2d_transpose_act_mkldnn_fuse_pass.py
@@ -0,0 +1,128 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from auto_scan_test import PassAutoScanTest
+from program_config import TensorConfig, ProgramConfig, OpConfig
+import numpy as np
+from functools import partial
+import unittest
+import hypothesis.strategies as st
+
+
+class TestConvTransposeActivationMkldnnFusePass(PassAutoScanTest):
+
+    def sample_program_config(self, draw):
+        batch_size = draw(st.integers(min_value=1, max_value=4))
+        groups = draw(st.sampled_from([1, 2, 4, 8]))
+        dilations = draw(st.sampled_from([[1, 1], [2, 2], [1, 2]]))
+        paddings = draw(st.sampled_from([[0, 3], [1, 2, 3, 4]]))
+        strides = draw(st.sampled_from([[1, 1], [2, 2], [1, 2]]))
+        data_format = draw(st.sampled_from(["NCHW", "NHWC"]))
+        padding_algorithm = draw(st.sampled_from(["EXPLICIT", "SAME", "VALID"]))
+        activation_type = draw(
+            st.sampled_from([
+                'relu', 'gelu', 'tanh', 'sigmoid', 'swish', 'mish', 'sqrt',
+                'hard_swish', 'sigmoid', 'abs', 'relu6', 'clip', 'tanh',
+                'hard_sigmoid', 'leaky_relu'
+            ]))
+
+        def generate_input():
+            input_shape = [
+                batch_size, 16, 64, 64
+            ] if data_format == "NCHW" else [batch_size, 64, 64, 16]
+            return np.random.random(input_shape).astype(np.float32)
+
+        def generate_weights():
+            return np.random.random([16, 16, 3, 3]).astype(np.float32)
+
+        conv2d_transpose_op = OpConfig(type="conv2d_transpose",
+                                       inputs={
+                                           "Input": ["input_data"],
+                                           "Filter": ["conv2d_weights"]
+                                       },
+                                       outputs={"Output": ["conv2d_output"]},
+                                       attrs={
+                                           "data_format": data_format,
+                                           "dilations": dilations,
+                                           "padding_algorithm":
+                                           padding_algorithm,
+                                           "groups": groups,
+                                           "paddings": paddings,
+                                           "strides": strides,
+                                           "output_size": [],
+                                           "output_padding": [],
+                                           "is_test": True
+                                       })
+
+        activation_op = None
+        if activation_type == "relu6":
+            activation_op = OpConfig(activation_type,
+                                     inputs={"X": ["conv2d_output"]},
+                                     outputs={"Out": ["activation_output"]},
+                                     threshold=draw(
+                                         st.floats(min_value=1.0,
+                                                   max_value=10.0)))
+        elif activation_type == "leaky_relu":
+            activation_op = OpConfig(activation_type,
+                                     inputs={"X": ["conv2d_output"]},
+                                     outputs={"Out": ["activation_output"]},
+                                     alpha=draw(
+                                         st.floats(min_value=0.1,
+                                                   max_value=1.0)))
+        elif activation_type == "swish":
+            activation_op = OpConfig(activation_type,
+                                     inputs={"X": ["conv2d_output"]},
+                                     outputs={"Out": ["activation_output"]},
+                                     beta=draw(
+                                         st.floats(min_value=0.1,
+                                                   max_value=1.0)))
+        elif activation_type == "clip":
+            activation_op = OpConfig(
+                activation_type,
+                inputs={"X": ["conv2d_output"]},
+                outputs={"Out": ["activation_output"]},
+                min=draw(st.floats(min_value=0.1, max_value=0.49)),
+                max=draw(st.floats(min_value=0.5, max_value=1.0)))
+        else:
+            activation_op = OpConfig(activation_type,
+                                     inputs={"X": ["conv2d_output"]},
+                                     outputs={"Out": ["activation_output"]})
+
+        model_net = [conv2d_transpose_op, activation_op]
+
+        program_config = ProgramConfig(
+            ops=model_net,
+            weights={
+                "conv2d_weights":
+                TensorConfig(data_gen=partial(generate_weights))
+            },
+            inputs={
+                "input_data": TensorConfig(data_gen=partial(generate_input))
+            },
+            outputs=["activation_output"])
+
+        return program_config
+
+    def sample_predictor_configs(self, program_config):
+        config = self.create_inference_config(use_mkldnn=True)
+        yield config, ['conv2d_transpose'], (1e-5, 1e-5)
+
+    def test(self):
+        self.run_and_statis(quant=False,
+                            max_examples=30,
+                            passes=["conv_activation_mkldnn_fuse_pass"])
+
+
+if __name__ == "__main__":
+    unittest.main()
