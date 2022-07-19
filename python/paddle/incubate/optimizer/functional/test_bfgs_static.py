2bd7750328ad06d13bce0c9e457b1dccbae08188
betterpig <chenxin33@baidu.com>
2022-07-19 11:34:41 +0000

    static still has problem

--- python/paddle/incubate/optimizer/functional/test_bfgs_static.py
+++ python/paddle/incubate/optimizer/functional/test_bfgs_static.py
@@ -18,6 +18,9 @@ import paddle
 import paddle.nn.functional as F
 from bfgs import minimize_bfgs
 
+# static
+paddle.enable_static()
+
 
 class LossAndFlatGradient():
     """A helper class to create a function required by tfp.optimizer.lbfgs_minimize.
@@ -28,10 +31,17 @@ class LossAndFlatGradient():
 
     def __init__(self, build_loss):
         if not paddle.in_dynamic_mode():
+            print(paddle.fluid.framework.default_main_program())
             params = paddle.fluid.framework.default_main_program().global_block(
             ).all_parameters()
-            self.weights = [param.name for param in params if param.trainable]
-            print(self.weights)
+            parameters = [param.name for param in params if param.trainable]
+            self.weights = []
+            self.shapes = [param.shape for param in params if param.trainable]
+            for param in parameters:
+                param_var = paddle.fluid.framework.default_main_program(
+                ).global_block().var(param)
+                self.weights.append(param_var)
+
         self.build_loss = build_loss
 
     def __call__(self, weights_1d):
@@ -45,17 +55,28 @@ class LossAndFlatGradient():
 
         self.set_flat_weights(weights_1d)
         loss = self.build_loss()
-        if paddle.in_dynamic_mode():
-            loss.backward()
-            grad = self.dynamic_stitch([param.grad for param in self.weights])
-            for weight in self.weights:
-                weight.clear_grad()
+        with paddle.fluid.program_guard(
+                paddle.fluid.framework.default_main_program(),
+                paddle.fluid.framework.default_startup_program()):
+            params_grads = paddle.static.append_backward(loss)
 
+        grad = self.dynamic_stitch([pair[1].detach() for pair in params_grads])
+        print("grad: ", grad)
         return loss, grad
 
-    def dynamic_stitch(self, inputs):
-        flattened_weights = [paddle.flatten(weight) for weight in inputs]
-        concat_weights = paddle.concat(flattened_weights)
+    def dynamic_stitch(self, inputs=None):
+        print("inputs: ", inputs)
+        if inputs is None:
+            inputs = self.weights
+        with paddle.no_grad():
+            print("inputs[0]: ", inputs[0])
+            paddle.fluid.layers.flatten(inputs[0], 0)
+            flattened_weights = [
+                paddle.fluid.layers.squeeze(
+                    paddle.fluid.layers.flatten(weight, 0), [0])
+                for weight in inputs
+            ]
+            concat_weights = paddle.concat(flattened_weights)
         return concat_weights
 
     def set_flat_weights(self, weights_1d):
@@ -63,15 +84,16 @@ class LossAndFlatGradient():
         Args:
             weights_1d: a 1D tf.Tensor representing the trainable variables.
         """
-        #weights = self.dynamic_partition(weights_1d, self.partitions,
-        #self.n_tensors)
+        with paddle.no_grad():
+            sections = [np.prod(list(shape)) for shape in self.shapes]
+            weights = paddle.split(weights_1d, sections)
+
         #for i, (shape, param) in enumerate(zip(self.shapes, weights)):
         #paddle.assign(self.net.parameters()[i], paddle.reshape(param, shape))
 
-        with paddle.no_grad():
-            if paddle.in_dynamic_mode():
-                for i in range(len(self._flat_weight)):
-                    self._flat_weight[i] = weights_1d[i]
+        # with paddle.no_grad():
+        #     for i in range(len(self.weights)):
+        #         paddle.assign(weights_1d[i], self.weights[])
 
     def to_flat_weights(self):
         """Returns a 1D tf.Tensor representing the `weights`.
@@ -80,14 +102,17 @@ class LossAndFlatGradient():
         Returns:
             A 1D tf.Tensor representing the `weights`.
         """
-        shape = [np.prod(param.shape) for param in self.weights]
-
+        shape = [
+            paddle.prod(paddle.fluid.layers.shape(param))
+            for param in self.weights
+        ]
+        print("shape: ", shape)
         _all_weights = [None] * len(self.weights)
         _all_weights = [w for w in self.weights]
 
         self._flat_weight = paddle.create_parameter(
-            shape=[np.sum(shape)], dtype=self.weights[0].dtype)
-        if paddle.fluid.in_dygraph_mode():
+            shape=[paddle.fluid.layers.sum(shape)], dtype='float32')
+        if not paddle.fluid.in_dygraph_mode():
             with paddle.no_grad():
                 _C_ops.coalesce_tensor(
                     _all_weights, _all_weights, self._flat_weight, "copy_data",
@@ -95,36 +120,32 @@ class LossAndFlatGradient():
         return self._flat_weight
 
 
-# static
-paddle.enable_static()
-
-
-def StaticNet(x_):
+def StaticNet():
+    X = paddle.static.data(name='x', shape=[1, 1], dtype=dtype)
     x1 = paddle.static.nn.fc(x=X, size=8)
     x2 = paddle.static.nn.fc(x=x1, size=1)
     x3 = paddle.fluid.layers.relu(x=x2)
     return x3
 
 
+def loss_fn():
+    y_predict = StaticNet()
+    Y = paddle.static.data(name='y', shape=[1, 1], dtype=dtype)
+    loss = paddle.fluid.layers.mse_loss(y_predict, Y)
+    return loss
+
+
 dtype = 'float32'
-X = paddle.static.data(name='x', shape=[1, 1], dtype=dtype)
-Y = paddle.static.data(name='y', shape=[1, 1], dtype=dtype)
+loss = loss_fn()
+print("loss: ", loss)
+func = LossAndFlatGradient(lambda: loss_fn())
+initial_position = func.dynamic_stitch()
+bfgs_op = minimize_bfgs(func, initial_position=initial_position)
 
-loss_fn = paddle.nn.L1Loss()
-
-func = LossAndFlatGradient(lambda: loss_fn(StaticNet(X), Y))
-initial_position = func.to_flat_weights()
-results = minimize_bfgs(func, initial_position=initial_position)
-
-main = paddle.static.Program()
-startup = paddle.static.Program()
 exe = paddle.static.Executor()
-exe.run(startup)
+exe.run(paddle.fluid.framework.default_startup_program())
 
 for epoch in range(5):
-    x_data = paddle.rand([1]).unsqueeze(1)  # 训练数据
+    x_data = np.rand([1])  # 训练数据
     y_data = x_data + 2.0  # 训练数据标签
-    exe.run(main,
-            feed={'x': x_data.numpy(),
-                  'y': y_data.numpy()},
-            fetch_list=[results])
+    results = exe.run(bfgs_op, feed={'x': x_data, 'y': y_data})
