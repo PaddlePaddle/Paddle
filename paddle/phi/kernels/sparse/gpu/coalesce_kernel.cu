a51402a82941977e11a009b488431e200dc02583
zkh2016 <zhangkaihuo@baidu.com>
2022-07-19 08:29:31 +0000

    fix codestyle

--- paddle/phi/kernels/sparse/gpu/coalesce_kernel.cu
+++ paddle/phi/kernels/sparse/gpu/coalesce_kernel.cu
@@ -155,7 +155,6 @@ void CoalesceGPUKernel(const GPUContext& dev_ctx,
                                out_values.data<T>());
   }
 
-
   // 6. convert index to coordinate
   Dim<DDim::kMaxRank> const_dims;
   for (int i = 0; i < x.dims().size(); i++) {
