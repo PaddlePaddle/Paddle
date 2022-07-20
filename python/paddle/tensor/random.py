889bdde3a6e7515cb07a4b00531fccc0ee31bc2a
Zhou Wei <1183042833@qq.com>
2022-07-20 16:52:18 +0800

    [Phi] migrate exponential kernel to phi (#44376)
    * [Phi] migrate exponential kernel to phi
    
    * fix comment
    
    * fix CI

--- python/paddle/tensor/random.py
+++ python/paddle/tensor/random.py
@@ -1052,7 +1052,9 @@ def exponential_(x, lam=1.0, name=None):
             #  [0.72520673, 0.45208144, 0.30234432]]
 
     """
-    if paddle.in_dynamic_mode():
+    if in_dygraph_mode():
+        return _C_ops.final_state_exponential_(x, lam)
+    elif paddle.in_dynamic_mode():
         return _C_ops.exponential_(x, "lambda", lam)
 
     check_variable_and_dtype(x, "x", ["float32", "float64"], "exponential")
