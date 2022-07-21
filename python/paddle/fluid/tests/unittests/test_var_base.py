cc92f4950b6880e0aa45996b7c790a65bd05f9a2
limin2021 <1121099234@qq.com>
2022-07-21 03:14:10 +0000

    comment in test_var_base.

--- python/paddle/fluid/tests/unittests/test_var_base.py
+++ python/paddle/fluid/tests/unittests/test_var_base.py
@@ -1776,8 +1776,7 @@ class TestEagerTensorGradNameValue(unittest.TestCase):
             b = a**2
             self.assertEqual(a._grad_value(), None)
             b.backward()
-            #self.assertEqual('eager_in_tmp' in a._grad_name(), True)
-            #print("a._grad_name = ", a._grad_name())
+            # Note, for new dygraph, there are no generated grad name, so we skip the name check.
             self.assertNotEqual(a._grad_value(), None)
 
 
