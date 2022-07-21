0b6eef49385146943356d9201ff3244c2a3d3ac4
zhaocaibei123 <48509226+zhaocaibei123@users.noreply.github.com>
2022-07-21 14:51:23 +0800

    add slot attr for push sparse op (#44422)
    * add slot attr for push sparse op
    
    * add pybind
    
    * remove fleet
    
    * add unittest
    
    * fix

--- python/paddle/fluid/contrib/layers/nn.py
+++ python/paddle/fluid/contrib/layers/nn.py
@@ -1073,7 +1073,8 @@ def sparse_embedding(input,
                      entry=None,
                      table_class="MemorySparseTable",
                      param_attr=None,
-                     dtype='float32'):
+                     dtype='float32',
+                     slot=None):
     r"""
     :api_attr: Static Graph
 
@@ -1220,6 +1221,9 @@ def sparse_embedding(input,
             )
         entry_str = entry._to_attr()
 
+    if slot == None:
+        slot = 0
+
     helper.append_op(type='lookup_table',
                      inputs={
                          'Ids': input,
@@ -1233,9 +1237,9 @@ def sparse_embedding(input,
                          'remote_prefetch': True,
                          'is_test': is_test,
                          'entry': entry_str,
-                         'table_class': table_class
+                         'table_class': table_class,
+                         'slot': slot
                      })
-
     return tmp
 
 
