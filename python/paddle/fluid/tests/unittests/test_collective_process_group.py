3f4aa5df0b9766edbe013c1dc424d0922fd7f9b0
wuhuachaocoding <huachaowu_ck@163.com>
2022-07-27 11:57:43 +0000

    add mpi test.

--- python/paddle/fluid/tests/unittests/test_collective_process_group.py
+++ python/paddle/fluid/tests/unittests/test_collective_process_group.py
@@ -25,6 +25,9 @@ class TestProcessGroup(TestMultipleGpus):
     def test_process_group_gloo(self):
         self.run_mnist_2gpu('process_group_gloo.py')
 
+    def test_process_group_mpi(self):
+        self.run_mnist_2gpu('process_group_mpi.py')
+
     def test_init_process_group(self):
         self.run_mnist_2gpu('init_process_group.py')
 
