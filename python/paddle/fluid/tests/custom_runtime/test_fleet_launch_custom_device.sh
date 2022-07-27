39c0fcd78c64fe92bb11cba3e52ca7e838760846
ronny1996 <ronny1996@163.com>
2022-07-27 07:07:00 +0000

    update

--- python/paddle/fluid/tests/custom_runtime/test_fleet_launch_custom_device.sh
+++ python/paddle/fluid/tests/custom_runtime/test_fleet_launch_custom_device.sh
@@ -16,13 +16,12 @@
 
 set -e
 
-
-
 rm -rf PaddleCustomDevice && git clone https://github.com/PaddlePaddle/PaddleCustomDevice.git && pushd PaddleCustomDevice/backends/custom_cpu && mkdir build && pushd build && cmake .. && make -j8 && popd && popd
 
 echo "begin test use custom_cpu"
 
 export FLAGS_selected_custom_cpus=0,1
+export CUSTOM_DEVICE_ROOT=PaddleCustomDevice/backends/custom_cpu/build
 
 distributed_args="--ips=127.0.0.1 --backend=xccl --custom_device_type=custom_cpu --custom_devices=0,1 --run_mode=collective --log_dir=testlog"
 python -m paddle.distributed.fleet.launch ${distributed_args} custom_device_multi_process_collective.py fleetlaunch_custom_cpu
