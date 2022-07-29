7329858d8d160a0d74de928a4e1baa0caba5d516
Yulong Ao <aoyulong@baidu.com>
2022-07-29 10:59:32 +0800

    [Phi] Add yaml for assign_value (#44596)
    * [Phi] Add yaml for assign_value
    
    * [Phi] Fix the bug of the assign api and modify the unittest
    
    * [Phi] Fix the bug when the tensor does not have the backend info
    
    * [Phi] Replace the functional-style cast init by the brace-init
    
    * [Phi] Cast the data explicitly

--- paddle/fluid/pybind/eager_utils.h
+++ paddle/fluid/pybind/eager_utils.h
@@ -191,6 +191,10 @@ paddle::experimental::Scalar CastNumpy2Scalar(PyObject* obj,
                                               const std::string& op_type,
                                               ssize_t arg_pos);
 
+std::vector<phi::Scalar> CastPyArg2ScalarArray(PyObject* obj,
+                                               const std::string& op_type,
+                                               ssize_t arg_pos);
+
 paddle::experimental::IntArray CastPyArg2IntArray(PyObject* obj,
                                                   const std::string& op_type,
                                                   ssize_t arg_pos);
