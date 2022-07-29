ee3ed417dbcf78f42564e5f2b5edb749aba1bb16
Jacek Czaja <jacek.czaja@intel.com>
2022-07-29 15:54:07 +0200

    - another fix

--- paddle/fluid/operators/mkldnn/matmul_v2_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/matmul_v2_mkldnn_op.cc
@@ -602,6 +602,21 @@ std::vector<int64_t> GetInputStrides(const ExecutionContext &ctx,
   auto input_dims = ctx.Input<Tensor>(input_name)->dims();
   auto new_dims = input_dims;
   if (!shape.empty() && !axis.empty()) {
+    auto it_zero = std::find(shape.begin(), shape.end(), 0);
+    if (it_zero != shape.end()) {
+      for (uint64_t i = 0; i < shape.size(); i++) {
+        if (shape[i] == 0) {
+          PADDLE_ENFORCE_LT(
+              i, input_dims.size(),
+              paddle::platform::errors::InvalidArgument(
+                  "The index of 0 in fused_reshape_%s ",
+                  "should be less than output dim size, ",
+                  "but the index is %d and output dim size is %d", input_name,
+                  i, input_dims.size()));
+          shape[i] = input_dims.at(i);
+        }
+      }
+    }
     new_dims = input_dims.reshape(shape).transpose(axis);
   }
 
