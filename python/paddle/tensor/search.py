52ddedf7f66b4eb91d3b18fd5d4a058e6a5b9490
Ligoml <39876205+Ligoml@users.noreply.github.com>
2022-08-01 18:34:52 +0800

    update paddle.where docs;test=document_fix

--- python/paddle/tensor/search.py
+++ python/paddle/tensor/search.py
@@ -588,9 +588,21 @@ def where(condition, x=None, y=None, name=None):
     Returns:
         Tensor: A Tensor with the same shape as :attr:`condition` and same data type as :attr:`x` and :attr:`y`.
 
-        Examples:
+    Examples:
+        
         .. code-block:: python
-            :name:where-example
+
+            import paddle
+            x = paddle.to_tensor([0.9383, 0.1983, 3.2, 1.2])
+            y = paddle.to_tensor([1.0, 1.0, 1.0, 1.0])
+            out = paddle.where(x>1, x, y)
+            print(out)
+            #out: [1.0, 1.0, 3.2, 1.2]
+            out = paddle.where(x>1)
+            print(out)
+            #out: (Tensor(shape=[2, 1], dtype=int64, place=CPUPlace, stop_gradient=True,
+            #            [[2],
+            #             [3]]),)
     """
     if np.isscalar(x):
         x = paddle.full([1], x, np.array([x]).dtype.name)
