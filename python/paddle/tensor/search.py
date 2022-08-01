799523941e699db1eb281f939dd7930a1dc0400f
Ligoml <39876205+Ligoml@users.noreply.github.com>
2022-08-01 18:40:12 +0800

    for ci;test=document_fix

--- python/paddle/tensor/search.py
+++ python/paddle/tensor/search.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -593,11 +593,14 @@ def where(condition, x=None, y=None, name=None):
         .. code-block:: python
 
             import paddle
+            
             x = paddle.to_tensor([0.9383, 0.1983, 3.2, 1.2])
             y = paddle.to_tensor([1.0, 1.0, 1.0, 1.0])
+            
             out = paddle.where(x>1, x, y)
             print(out)
             #out: [1.0, 1.0, 3.2, 1.2]
+            
             out = paddle.where(x>1)
             print(out)
             #out: (Tensor(shape=[2, 1], dtype=int64, place=CPUPlace, stop_gradient=True,
