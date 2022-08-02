510124ffa90e6ee7c6d5ff7abc5d89d041955abc
zhoutianzi666 <17801055074@163.com>
2022-08-02 13:35:27 +0000

    commit

--- paddle/fluid/inference/tensorrt/convert/rnn_op.cc
+++ paddle/fluid/inference/tensorrt/convert/rnn_op.cc
@@ -24,7 +24,7 @@ class RnnNativeOpConverter : public OpConverter {
                   const framework::Scope& scope,
                   bool test_mode) override {
 #if IS_TRT_VERSION_GE(7000)
-    VLOG(4) << "convert a fluid rnn op to tensorrt rnn native layer";
+    VLOG(4) << "convert a fluid rnn op to tensorrt rnn layer";
 
     framework::OpDesc op_desc(op, nullptr);
     // [seq_len, batch ,in_size],
