978f13966557f7603e521189363c3ee5f9f27f8f
DesmonDay <908660116@qq.com>
2022-08-02 06:29:18 +0000

    change api file name

--- python/paddle/geometric/message_passing/__init__.py
+++ python/paddle/geometric/message_passing/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .send_u_recv import send_u_recv  # noqa: F401
+from .send_recv import send_u_recv  # noqa: F401
