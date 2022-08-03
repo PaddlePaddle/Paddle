94c71e800c5a3e622d80dca2a948c75235b8fcfc
xiaoting <31891223+tink2123@users.noreply.github.com>
2022-08-03 17:52:16 +0800

    Update lu_unpack_kernel.cc

--- paddle/phi/kernels/cpu/lu_unpack_kernel.cc
+++ paddle/phi/kernels/cpu/lu_unpack_kernel.cc
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
 
