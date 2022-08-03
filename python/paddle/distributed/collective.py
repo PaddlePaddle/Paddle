2f2aad81c9774441e507ace79f23c2572c42023c
ronnywang <ronny1996@163.com>
2022-08-03 18:52:16 +0800

    [CustomDevice] add custom ccl  2/2 (#44650)
    * [CustomDevice] add custom ccl 2/2
    
    * update
    
    * update
    
    * update launch

--- python/paddle/distributed/collective.py
+++ python/paddle/distributed/collective.py
@@ -146,7 +146,7 @@ _group_map_backend = {}
 # Name of the default group for init_parallel_env
 _default_group_name = "_default_pg"
 
-_valid_backend_list = ['nccl', 'gloo', 'hccl', 'heter']
+_valid_backend_list = ['nccl', 'gloo', 'hccl', 'heter', 'xccl']
 _default_store = None  # the default tcp store
 _default_backend = None
 
@@ -271,6 +271,9 @@ def _new_process_group_impl(backend,
     elif backend == "hccl":
         place = core.NPUPlace(genv.device_id)
         pg = core.ProcessGroupHCCL(store, rank, world_size, place, group_id)
+    elif backend == "xccl":
+        place = core.CustomPlace(genv.device_type, genv.device_id)
+        pg = core.ProcessGroupCustom(store, rank, world_size, place, group_id)
     elif backend == "heter":
         place = None
         if core.is_compiled_with_cuda():
