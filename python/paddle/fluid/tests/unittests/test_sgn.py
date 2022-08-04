75fe6ce938f3c1baae2500c65b3daadd2f51c273
peachlcy <289854656@qq.com>
2022-08-04 11:52:02 +0800

    add paddle.sgn

--- python/paddle/fluid/tests/unittests/test_sgn.py
+++ python/paddle/fluid/tests/unittests/test_sgn.py
@@ -63,9 +63,8 @@ class TestSignAPI(unittest.TestCase):
 
     def test_complex(self):
         for dtype in ['complex64', 'complex128']:
-            np_x = np.array(
-                [[3 + 4j, 7 - 24j, 0, 1 + 2j], [6 + 8j, 3, 0, -2]],
-                dtype=dtype)
+            np_x = np.array([[3 + 4j, 7 - 24j, 0, 1 + 2j], [6 + 8j, 3, 0, -2]],
+                            dtype=dtype)
             x = paddle.to_tensor(np_x)
             z = paddle.sgn(x)
             np_z = z.numpy()
