65e32c337a84711c63c198d3404e455a8da7a30c
peachlcy <289854656@qq.com>
2022-08-06 21:23:30 +0800

    add paddle.sgn

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -4720,11 +4720,8 @@ def sgn(x, name=None):
         .. code-block:: Python
 
             import paddle
-            import numpy as np
 
-            np_x = np.array(
-                [[3 + 4j, 7 - 24j, 0, 1 + 2j], [6 + 8j, 3, 0, -2]], dtype='complex64')
-            x = paddle.to_tensor(np_x)
+            x = paddle.to_tensor([[3 + 4j, 7 - 24j, 0, 1 + 2j], [6 + 8j, 3, 0, -2]])
             print(paddle.sgn(x))
             #[[0.6+0.8j       0.28-0.96j      0.+0.j      0.4472136+0.8944272j]
             # [0.6+0.8j       1.+0.j          0.+0.j      -1.+0.j]]
