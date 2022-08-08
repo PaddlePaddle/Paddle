65ac8d4b87f730200492200956112a70b50f4727
wuhuachaocoding <huachaowu_ck@163.com>
2022-08-08 08:39:15 +0000

    update fleet

--- python/paddle/distributed/fleet/__init__.py
+++ python/paddle/distributed/fleet/__init__.py
@@ -28,7 +28,6 @@ from .data_generator.data_generator import MultiSlotStringDataGenerator  # noqa:
 from . import metrics  # noqa: F401
 from .base.topology import CommunicateTopology
 from .base.topology import HybridCommunicateGroup  # noqa: F401
-#from .base.fleet_base import Fleet
 from .Fleet import Fleet
 from .model import distributed_model
 from .optimizer import distributed_optimizer
