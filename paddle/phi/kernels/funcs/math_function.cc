534408e0da3bd6f316e61061b649e8ad3ccc08e5
helen88 <z8hanghuan@126.com>
2022-08-09 09:39:01 +0000

    support warpctc for xpu,*test=kunlun

--- paddle/phi/kernels/funcs/math_function.cc
+++ paddle/phi/kernels/funcs/math_function.cc
@@ -30,7 +30,6 @@ limitations under the License. */
 #include "paddle/fluid/platform/bfloat16.h"
 #include "paddle/fluid/platform/float16.h"
 #include "paddle/phi/backends/cpu/cpu_context.h"
-#include "paddle/phi/backends/xpu/xpu_context.h"
 #include "paddle/phi/kernels/funcs/eigen/common.h"
 #include "paddle/phi/kernels/funcs/math_function_impl.h"
 #include "unsupported/Eigen/CXX11/Tensor"
