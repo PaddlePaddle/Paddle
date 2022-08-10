ce0ed9fbef8b08137ec8170795e6e99005e7b26a
Wangzheee <634486483@qq.com>
2022-08-10 09:36:00 +0000

    disable_skip_layernorm_fp16

--- paddle/fluid/inference/tensorrt/convert/skip_layernorm.cc
+++ paddle/fluid/inference/tensorrt/convert/skip_layernorm.cc
@@ -166,6 +166,7 @@ class SkipLayerNormOpConverter : public OpConverter {
       // bool with_fp16 =
       //     engine_->WithFp16() && !engine_->disable_trt_plugin_fp16();
       bool with_fp16 = false;
+
       plugin::SkipLayerNormPluginDynamic* plugin =
           new plugin::SkipLayerNormPluginDynamic(
               static_cast<const float*>(bias_weight.values),
