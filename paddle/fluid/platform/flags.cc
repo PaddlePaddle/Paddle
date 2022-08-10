3827b9c061cc37f59654f0ebab8e55a42b83e6f9
wwbitejotunn <wang_bojun@outlook.com>
2022-08-10 13:12:02 +0000

    fix percision problem in fp16

--- paddle/fluid/platform/flags.cc
+++ paddle/fluid/platform/flags.cc
@@ -453,7 +453,7 @@ PADDLE_DEFINE_EXPORTED_double(
  * Example:
  * Note: For selecting allocator policy of PaddlePaddle.
  */
-static constexpr char kDefaultAllocatorStrategy[] = "naive_best_fit";
+static constexpr char kDefaultAllocatorStrategy[] = "auto_growth";
 PADDLE_DEFINE_EXPORTED_string(
     allocator_strategy,
     kDefaultAllocatorStrategy,
