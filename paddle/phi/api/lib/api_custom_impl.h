1d8097bed698f763e0bf2b8a73be4045c03a6156
zyfncg <zhangyunfei07@baidu.com>
2022-08-11 07:45:05 +0000

    delete useless code

--- paddle/phi/api/lib/api_custom_impl.h
+++ paddle/phi/api/lib/api_custom_impl.h
@@ -45,18 +45,6 @@ std::tuple<Tensor, Tensor, Tensor, Tensor, Tensor, Tensor> batch_norm_impl(
     bool trainable_statistics,
     bool fuse_with_relu);
 
-Tensor conv3d_impl(const Tensor& input,
-                   const Tensor& filter,
-                   const std::vector<int>& strides,
-                   const std::vector<int>& paddings,
-                   const std::string& paddding_algorithm,
-                   int groups,
-                   const std::vector<int>& dilations,
-                   const std::string& data_format,
-                   bool use_addto,
-                   int workspace_size_MB,
-                   bool exhaustive_search);
-
 Tensor copy_to_impl(const Tensor& x, Place place, bool blocking);
 
 Tensor embedding_impl(const Tensor& x,
@@ -82,36 +70,6 @@ std::tuple<Tensor, Tensor, Tensor> momentum_impl(
     float rescale_grad);
 
 ////////////////// Backward(grad) api impls //////////////////////
-
-void conv2d_grad_impl(const Tensor& input,
-                      const Tensor& filter,
-                      const Tensor& out_grad,
-                      const std::vector<int>& strides,
-                      const std::vector<int>& paddings,
-                      const std::string& paddding_algorithm,
-                      int groups,
-                      const std::vector<int>& dilations,
-                      const std::string& data_format,
-                      bool use_addto,
-                      int workspace_size_MB,
-                      bool exhaustive_search,
-                      Tensor* input_grad,
-                      Tensor* filter_grad);
-
-void conv3d_grad_impl(const Tensor& input,
-                      const Tensor& filter,
-                      const Tensor& out_grad,
-                      const std::vector<int>& strides,
-                      const std::vector<int>& paddings,
-                      const std::string& paddding_algorithm,
-                      int groups,
-                      const std::vector<int>& dilations,
-                      const std::string& data_format,
-                      bool use_addto,
-                      int workspace_size_MB,
-                      bool exhaustive_search,
-                      Tensor* input_grad,
-                      Tensor* filter_grad);
 
 void imag_grad_impl(const Tensor& out_grad, Tensor* x_grad);
 
