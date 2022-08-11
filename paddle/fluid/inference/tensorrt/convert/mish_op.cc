113bab198b7e5d1d320ce244f95ca5bc3637f689
zhangjun <ewalker@live.cn>
2022-08-11 11:55:31 +0000

    add trt mish

--- paddle/fluid/inference/tensorrt/convert/mish_op.cc
+++ paddle/fluid/inference/tensorrt/convert/mish_op.cc
@@ -41,27 +41,42 @@ class MishOpConverter : public OpConverter {
 
     framework::OpDesc op_desc(op, nullptr);
     // Declare inputs
-    int input_num = op_desc.Input("X").size();
     auto* input = engine_->GetITensor(op_desc.Input("X")[0]);
 
     const float threshold =
         op_desc.HasAttr("threshold")
             ? PADDLE_GET_CONST(float, op_desc.GetAttr("threshold"))
             : 20.0f;
-
     nvinfer1::ILayer* layer = nullptr;
-    if (engine_->with_dynamic_shape()) {
-      bool with_fp16 =
-          engine_->WithFp16() && !engine_->disable_trt_plugin_fp16();
-      plugin::MishPluginDynamic* plugin =
-          new plugin::MishPluginDynamic(threshold, with_fp16);
-      layer = engine_->AddDynamicPlugin(&input, input_num, plugin);
-    } else {
-      bool with_fp16 =
-          engine_->WithFp16() && !engine_->disable_trt_plugin_fp16();
-      plugin::MishPlugin* plugin = new plugin::MishPlugin(threshold, with_fp16);
-      layer = engine_->AddPlugin(&input, input_num, plugin);
-    }
+
+#if IS_TRT_VERSION_GE(5130)
+    // mish -> clip + softplus + tanh + prod
+    auto* layer_clip = TRT_ENGINE_ADD_LAYER(
+        engine_, Activation, *input, nvinfer1::ActivationType::kCLIP);
+    layer_clip->setAlpha(-3.40282e+038);
+    layer_clip->setBeta(threshold);
+    auto* softplus_layer =
+        TRT_ENGINE_ADD_LAYER(engine_,
+                             Activation,
+                             *layer_clip->getOutput(0),
+                             nvinfer1::ActivationType::kSOFTPLUS);
+    softplus_layer->setAlpha(1.0f);
+    softplus_layer->setBeta(1.0f);
+    auto* tan_layer = TRT_ENGINE_ADD_LAYER(engine_,
+                                           Activation,
+                                           *softplus_layer->getOutput(0),
+                                           nvinfer1::ActivationType::kTANH);
+    layer = TRT_ENGINE_ADD_LAYER(engine_,
+                                 ElementWise,
+                                 *input,
+                                 *(tan_layer->getOutput(0)),
+                                 nvinfer1::ElementWiseOperation::kPROD);
+#else
+    int input_num = op_desc.Input("X").size();
+    bool with_fp16 = engine_->WithFp16() && !engine_->disable_trt_plugin_fp16();
+    plugin::MishPlugin* plugin = new plugin::MishPlugin(threshold, with_fp16);
+    layer = engine_->AddPlugin(&input, input_num, plugin);
+#endif
 
     auto output_name = op_desc.Output("Out")[0];
     RreplenishLayerAndOutput(layer, "mish", {output_name}, test_mode);
