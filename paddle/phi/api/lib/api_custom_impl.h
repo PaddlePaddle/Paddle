fb604b5bc6f8101052ccfb7a4000c25a3db9e36d
zyfncg <zhangyunfei07@baidu.com>
2022-08-11 07:34:35 +0000

    remove some custom_impl api and make them generated by yaml completely

--- paddle/phi/api/lib/api_custom_impl.h
+++ paddle/phi/api/lib/api_custom_impl.h
@@ -31,27 +31,6 @@ namespace experimental {
 
 ////////////////// Forward api impls //////////////////////
 
-std::tuple<Tensor, Tensor, Tensor, Tensor, Tensor, Tensor> adamw_impl(
-    const Tensor& param,
-    const Tensor& grad,
-    const Tensor& learning_rate,
-    const Tensor& moment1,
-    const Tensor& moment2,
-    const Tensor& beta1_pow,
-    const Tensor& beta2_pow,
-    const paddle::optional<Tensor>& master_param,
-    const paddle::optional<Tensor>& skip_update,
-    const Scalar& beta1,
-    const Scalar& beta2,
-    const Scalar& epsilon,
-    float lr_ratio,
-    float coeff,
-    bool with_decay,
-    bool lazy_mode,
-    int64_t min_row_size_to_use_multithread,
-    bool multi_precision,
-    bool use_global_beta_pow);
-
 std::tuple<Tensor, Tensor, Tensor, Tensor, Tensor, Tensor> batch_norm_impl(
     const Tensor& x,
     const Tensor& scale,
@@ -65,18 +44,6 @@ std::tuple<Tensor, Tensor, Tensor, Tensor, Tensor, Tensor> batch_norm_impl(
     bool use_global_stats,
     bool trainable_statistics,
     bool fuse_with_relu);
-
-Tensor conv2d_impl(const Tensor& input,
-                   const Tensor& filter,
-                   const std::vector<int>& strides,
-                   const std::vector<int>& paddings,
-                   const std::string& paddding_algorithm,
-                   int groups,
-                   const std::vector<int>& dilations,
-                   const std::string& data_format,
-                   bool use_addto,
-                   int workspace_size_MB,
-                   bool exhaustive_search);
 
 Tensor conv3d_impl(const Tensor& input,
                    const Tensor& filter,
