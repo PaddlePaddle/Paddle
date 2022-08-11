2527e89e4c8c64f07d6e10549aa72c57c99b438f
Wang Huan <wanghuan29@baidu.com>
2022-08-11 07:39:29 +0000

    pylayer detach output tensor if it is equal with input

--- paddle/fluid/pybind/eager_py_layer.cc
+++ paddle/fluid/pybind/eager_py_layer.cc
@@ -428,6 +428,7 @@ PyObject* pylayer_method_apply(PyObject* cls,
   if (outputs_size == 1) {
     Py_XDECREF(outputs);
     outputs = PyTuple_GetItem(outputs_tuple, 0);
+    Py_INCREF(outputs);
     Py_XDECREF(outputs_tuple);
   }
   Py_XDECREF(forward_args);
