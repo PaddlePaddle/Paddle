8624f3b104ea497ae8b2bebfdcea5dd1887e17e7
caozhou <48191911+Caozhou1995@users.noreply.github.com>
2022-08-12 19:05:27 +0800

    [Auto Parallel] Update reshard for auto search (#45002)
    * update reshard for auto search
    
    * fix unittest bug
    
    * update dist tensor
    
    * update reshard output
    
    * fix unittests bug
    
    * merge develop

--- python/paddle/distributed/auto_parallel/utils.py
+++ python/paddle/distributed/auto_parallel/utils.py
@@ -1419,7 +1419,10 @@ def get_standalone_cost_data(distributed_programs):
     }
 
     standalone_cost_data = []
-    not_enum_ops = ["create_py_reader", "create_double_buffer_reader", "read"]
+    # skip ops
+    not_enum_ops = [
+        "create_py_reader", "create_double_buffer_reader", "read", "assign"
+    ]
     for distributed_program in distributed_programs:
         cost_data = {}
         vars = distributed_program.global_block().vars
