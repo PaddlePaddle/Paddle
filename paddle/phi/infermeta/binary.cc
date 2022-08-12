adb61b7bbd936b56509f4a744fd06419ade30c12
zyfncg <zhangyunfei07@baidu.com>
2022-08-12 14:34:18 +0800

    Remove some custom_impl api (#45066)
    * remove some custom_impl api and make them generated by yaml completely
    
    * delete useless code
    
    * fix adamw bug
    
    * fix infermeta
    
    * revert adamw
    
    * polish code
    
    * fix bug

--- paddle/phi/infermeta/binary.cc
+++ paddle/phi/infermeta/binary.cc
@@ -2736,5 +2736,4 @@ void Unpool3dInferMeta(const MetaTensor& x,
 }  // namespace phi
 
 PD_REGISTER_INFER_META_FN(add_raw, phi::ElementwiseRawInferMeta);
-PD_REGISTER_INFER_META_FN(conv2d, phi::ConvInferMeta);
 PD_REGISTER_INFER_META_FN(conv2d_infer, phi::ConvInferInferMeta);
