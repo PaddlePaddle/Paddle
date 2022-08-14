9d6eaae45082f4d3403c7fa451c7b085f3463251
Yulv-git <yulvchi@qq.com>
2022-08-14 13:57:07 +0800

    Add CI for assert-allclose.

--- tools/check_file_diff_approvals.sh
+++ tools/check_file_diff_approvals.sh
@@ -253,6 +253,13 @@ if [ "${INVALID_PADDLE_CHECK}" != "" ] && [ "${GIT_PR_ID}" != "" ]; then
     check_approval 1 6836917 47554610 22561442
 fi
 
+ALL_ADDED_LINES=`git diff -U0 upstream/$BRANCH | grep "^+" || true`
+INVALID_PADDLE_CHECK=`echo "$ALL_ADDED_LINES" | grep -En "self\.assert(True|Equal)\(\s*(np|numpy)\.(allclose|array_equal)" python/paddle/fluid/tests/unittests/ python/paddle/tests/ python/paddle/fluid/contrib/ || true`
+if [ "${INVALID_PADDLE_CHECK}" != "" ] && [ "${GIT_PR_ID}" != "" ]; then
+    echo_line="建议使用np.testing.assert_allclose(...)替换self.assertTrue(np.allclose(...))或self.assertEqual(np.allclose(...)):\n${INVALID_PADDLE_CHECK}\n"
+    check_approval 1 6836917 47554610 22561442
+fi
+
 EMPTY_GRAD_OP_REGISTERED=`echo $ALL_ADDED_LINES |grep -zoE "REGISTER_OP_WITHOUT_GRADIENT\([^;.]*\)[;\s]" || echo $ALL_ADDED_LINES |grep -zoE "[[:graph:]]*EmptyGradOpMaker<[[:graph:]]*>" || true`
 if [ "${EMPTY_GRAD_OP_REGISTERED}" != "" ] && [ "${GIT_PT_ID}" != "" ]; then
     echo_line="You must have one RD (phlrain, XiaoguangHu01, kolinwei or JiabinYang) approval for the usage of REGISTER_OP_WITHOUT_GRADIENT or EmptyGradOpMaker.\nThe code that do not meet the specification are as follows:\n${EMPTY_GRAD_OP_REGISTERED}\n"
