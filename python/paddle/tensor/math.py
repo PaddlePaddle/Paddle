5314325c9a21d3e1e5f0f92329098a8269595a7e
wawltor <fangzeyang0904@hotmail.com>
2022-08-15 11:31:30 +0000

    update the document for the api add

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -500,16 +500,19 @@ def add(x, y, name=None):
 
         Out=X+Y
 
-        X : a tensor of any dimension.
-        Y: a tensor whose dimensions must be less than or equal to the dimensions of X.
+    $X$ the tensor of any dimension.
+    $Y$ the tensor whose dimensions must be less than or equal to the dimensions of $X$.
 
     There are two cases for this operator:
-    1. The shape of Y is the same with X.
-    2. The shape of Y is a continuous subsequence of X.
+
+    1. The shape of $Y$ is the same with $X$.
+    2. The shape of $Y$ is a continuous subsequence of $X$.
+
     For case 2:
-    1. Broadcast Y to match the shape of X, where axis is the start dimension index for broadcasting Y onto X.
-    2. If axis is -1 (default), axis=rank(X)−rank(Y).
-    3. The trailing dimensions of size 1 for Y will be ignored for the consideration of subsequence, such as shape(Y) = (2, 1) => (2).
+
+    1. Broadcast $Y$ to match the shape of $X$, where axis is the start dimension index for broadcasting $Y$ onto $X$.
+    2. If $axis$ is -1 (default), $axis$=rank($X$)−rank($Y$).
+    3. The trailing dimensions of size 1 for $Y$ will be ignored for the consideration of subsequence, such as shape($Y$) = (2, 1) => (2).
 
         For example:
 
