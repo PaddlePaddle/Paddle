7715360bf0e991c560fd183762485948b6df6ed0
SigureMo <sigure.qaq@gmail.com>
2022-08-16 13:00:03 +0800

    remove test files, test=document_fix

--- python/paddle/fluid/tests/unittests/CI_test/test_executor_and_mul.py
+++
@@ -1,7 +0,0 @@
-'''
-        self.assertEqual((100, 100), res.shape)
-        self.assertTrue(numpy.allclose(res, numpy.dot(a_np, b_np)))
-        self.assertTrue(numpy.allclose(res_array[0], a_np))
-        self.assertTrue(numpy.allclose(res_array[1], b_np))
-        self.assertTrue(numpy.allclose(res_array[2], res))
-'''
--- python/paddle/fluid/tests/unittests/CI_test/test_take_along_axis_op.py
+++
@@ -1,6 +0,0 @@
-'''
-            self.assertEqual(np.allclose(out, out_ref, rtol=1e-03), True)
-
-
-        self.assertEqual(np.allclose(out.numpy(), out_ref, rtol=1e-03), True)
-'''
