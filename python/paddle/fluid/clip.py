09cb0264c5ae107fe0941cfd1b735617f7125084
phlrain <phliuhongyu@126.com>
2022-08-17 09:25:03 +0000

    revert impl, final state mul not support selected rows

--- python/paddle/fluid/clip.py
+++ python/paddle/fluid/clip.py
@@ -555,7 +555,7 @@ class ClipGradByGlobalNorm(ClipGradBase):
             if need_clip:
                 clip_input = (clip_var.astype('float16') if g.dtype
                               == core.VarDesc.VarType.FP16 else clip_var)
-                new_grad = layers.elementwise_mul(g, clip_input)
+                new_grad = _C_ops.elementwise_mul(g, clip_input)
                 params_and_grads.append((p, new_grad))
             else:
                 params_and_grads.append((p, g))
