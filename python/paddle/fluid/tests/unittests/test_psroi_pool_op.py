54fb53260d2c6411d46735b038d15223d70a95c3
SigureMo <sigure.qaq@gmail.com>
2022-08-18 01:47:48 +0800

    fix test_psroi_pool_op, unpack static graph results

--- python/paddle/fluid/tests/unittests/test_psroi_pool_op.py
+++ python/paddle/fluid/tests/unittests/test_psroi_pool_op.py
@@ -334,12 +334,12 @@ class TestPSROIPoolStaticAPI(unittest.TestCase):
             exe = paddle.static.Executor(place)
             boxes_lod_data = paddle.fluid.create_lod_tensor(
                 self.boxes, [[1, 2]], place)
-            out_res = exe.run(paddle.static.default_main_program(),
-                              feed={
-                                  'x': self.x,
-                                  'boxes': boxes_lod_data
-                              },
-                              fetch_list=[out.name])
+            out_res, = exe.run(paddle.static.default_main_program(),
+                               feed={
+                                   'x': self.x,
+                                   'boxes': boxes_lod_data
+                               },
+                               fetch_list=[out.name])
             np.testing.assert_allclose(out_res, expect_out, rtol=1e-05)
 
 
