1bb56d821bf9b05b6e1fc49430a9eed87e7311aa
wanghao107 <wanghao107@baidu.com>
2022-08-18 11:04:09 +0000

    modify code style

--- paddle/fluid/eager/auto_code_generator/final_state_generator/codegen_utils.py
+++ paddle/fluid/eager/auto_code_generator/final_state_generator/codegen_utils.py
@@ -447,10 +447,10 @@ class FunctionGeneratorBase:
         for i in range(len(forward_returns_list)):
             forward_return = forward_returns_list[i]
             if len(forward_return[0]) == 0:
-                if(len(forward_returns_list) == 1):
+                if (len(forward_returns_list) == 1):
                     return_name = "out"
                 else:
-                    return_name = "out_{}".format(i+1)
+                    return_name = "out_{}".format(i + 1)
             else:
                 return_name = forward_return[0]
             return_type = forward_return[1]
