c7f4f6c3d6d52461b6be79e00043e68e11311516
wuhuachaocoding <huachaowu_ck@163.com>
2022-08-18 08:24:44 +0000

    update some include mpi.h.

--- paddle/fluid/distributed/collective/MPITools.h
+++ paddle/fluid/distributed/collective/MPITools.h
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 #pragma once
-
 #include <error.h>
 #include <mpi.h>
 #include <iostream>
--- paddle/fluid/distributed/collective/ProcessGroupMPI.h
+++ paddle/fluid/distributed/collective/ProcessGroupMPI.h
@@ -14,7 +14,6 @@
 
 #pragma once
 
-#include <mpi.h>
 #include <chrono>
 #include <map>
 #include <string>
