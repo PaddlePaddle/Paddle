714605baf107caa5218b250ef3d42b473d6ba527
jiahongyu <jiahongyu@baidu.com>
2022-08-19 03:13:50 +0000

    polish default param of XXX_interp_test, the same default value with XXX_interp_np

--- python/paddle/fluid/tests/unittests/test_trilinear_interp_v2_op.py
+++ python/paddle/fluid/tests/unittests/test_trilinear_interp_v2_op.py
@@ -30,14 +30,14 @@ def trilinear_interp_test(x,
                           OutSize=None,
                           SizeTensor=None,
                           Scale=None,
-                          data_layout='NCHW',
+                          data_layout='NCDHW',
                           out_d=-1,
                           out_h=-1,
                           out_w=-1,
                           scale=[],
-                          interp_method='linear',
-                          align_corners=False,
-                          align_mode=1):
+                          interp_method='trilinear',
+                          align_corners=True,
+                          align_mode=0):
     if isinstance(scale, float) or isinstance(scale, int):
         scale_list = []
         for _ in range(len(x.shape) - 2):
