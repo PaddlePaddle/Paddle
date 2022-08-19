d968d48359f6ed654e3fc8e8d59285a2464e713c
xiongkun <xiongkun03@baidu.com>
2022-08-19 03:38:45 +0000

    add unittest for item interface

--- python/paddle/fluid/tests/unittests/dygraph_to_static/test_tensor_methods.py
+++ python/paddle/fluid/tests/unittests/dygraph_to_static/test_tensor_methods.py
@@ -64,5 +64,30 @@ class TestTensorDygraphOnlyMethodError(unittest.TestCase):
             static_res = self._run(to_static=True)
 
 
+@paddle.jit.to_static
+def tensor_item(x):
+    x = paddle.to_tensor(x)
+    y = x.clone()
+    return y.item()
+
+
+class TestTensorItem(unittest.TestCase):
+
+    def _run(self, to_static):
+        prog_trans = paddle.jit.ProgramTranslator()
+        prog_trans.enable(to_static)
+        x = paddle.ones([1])
+        if to_static:
+            return tensor_item(x).numpy()
+        return tensor_item(x)
+
+    def test_tensor_clone(self):
+        dygraph_res = self._run(to_static=False)
+        static_res = self._run(to_static=True)
+        self.assertTrue(numpy.allclose(dygraph_res, static_res),
+                        msg='dygraph res is {}\nstatic_res is {}'.format(
+                            dygraph_res, static_res))
+
+
 if __name__ == '__main__':
     unittest.main()
