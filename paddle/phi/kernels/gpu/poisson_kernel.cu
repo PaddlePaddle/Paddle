b7466077126d2fbcb58dbed8a50971fc333f8f93
ray96 <lqr5259@outlook.com>
2022-08-19 17:46:27 +0800

    modify code style

--- paddle/phi/kernels/gpu/poisson_kernel.cu
+++ paddle/phi/kernels/gpu/poisson_kernel.cu
@@ -31,16 +31,16 @@ template <typename T>
 __global__ void GetPoisson(
     const T* in, T* out, const int N, unsigned int seed, unsigned int offset) {
     CUDA_KERNEL_LOOP_TYPE(idx, N, int64_t){
-      #ifdef __NVCC__
-          curandStatePhilox4_32_10_t state;
-          curand_init(seed, idx, offset, &state);
-          out[idx] = static_cast<T>(curand_poisson(&state, in[idx]));
-      #elif __HIPCC__
-          hiprandStatePhilox4_32_10_t state;
-          hiprand_init(seed, idx, offset, &state);
-          out[idx] = static_cast<T>(hiprand_poisson(&state, in[idx]));
-      #endif
-    }
+#ifdef __NVCC__
+    curandStatePhilox4_32_10_t state;
+    curand_init(seed, idx, offset, &state);
+    out[idx] = static_cast<T>(curand_poisson(&state, in[idx]));
+#elif __HIPCC__
+    hiprandStatePhilox4_32_10_t state;
+    hiprand_init(seed, idx, offset, &state);
+    out[idx] = static_cast<T>(hiprand_poisson(&state, in[idx]));
+#endif
+  }
 }
 
 template <typename T, typename Context>
