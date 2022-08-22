505bf9bc5560bae260bf7edf05faffc0bc0a0a22
wuhuachaocoding <huachaowu_ck@163.com>
2022-08-22 08:04:57 +0000

    update test.

--- python/paddle/fluid/tests/unittests/test_mpi_comm.sh
+++ python/paddle/fluid/tests/unittests/test_mpi_comm.sh
@@ -19,10 +19,9 @@ set -e
 # use default values
 export PADDLE_DISTRI_BACKEND="mpi"
 cmd=`which mpirun`
-cmd="${cmd} -x PADDLE_DISTRI_BACKEND"
 if [ ${#cmd} -eq 0 ]
 then
     echo "Warning! mpirun command not found!"
 else
-    ${cmd} -np 2 --allow-run-as-root python3.8 process_group_mpi.py
+    ${cmd} -x PADDLE_DISTRI_BACKEND -np 2 --allow-run-as-root python3 process_group_mpi.py
 fi
