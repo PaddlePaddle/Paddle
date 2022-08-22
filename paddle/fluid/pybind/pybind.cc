c5fa8017ef8ed6ed370fe3759acea3cd7ea72fba
SmirnovKol <31559413+OccupyMars2025@users.noreply.github.com>
2022-08-22 23:21:35 +0800

    Update pybind.cc

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -397,7 +397,7 @@ struct finfo {
           eps = std::numeric_limits<float>::epsilon();
           max = std::numeric_limits<float>::max();
           min = std::numeric_limits<float>::lowest();
-          // smallest_normal = 
+          // smallest_normal = std::numeric_limits<float>::
           // tiny = 
           resolution = std::pow(10, -std::numeric_limits<float>::digits10);
           // dtype = "float32";
