5d32c521c2862d618988370d0fb6772f171c4f49
S-HuaBomb <s_huabang@163.com>
2022-08-22 22:00:52 +0800

    add example code

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -4828,6 +4828,7 @@ def take(x, index, mode='raise', name=None):
 
             idx_pos = paddle.arange(4, 10).reshape([2, 3])  # positive index
             idx_neg = paddle.arange(-2, 4).reshape([2, 3])  # negative index
+            idx_err = paddle.arange(-2, 13).reshape([3, 5])  # index out of range
 
             paddle.take(x_int, idx_pos)
             # Tensor(shape=[2, 3], dtype=int64, place=Place(cpu), stop_gradient=True,
@@ -4848,6 +4849,19 @@ def take(x, index, mode='raise', name=None):
             # Tensor(shape=[2, 3], dtype=int64, place=Place(cpu), stop_gradient=True,
             #        [[4, 5, 6],
             #         [7, 8, 9]])
+
+            paddle.take(x_int, idx_err, mode='wrap')
+            # Tensor(shape=[3, 5], dtype=int32, place=Place(cpu), stop_gradient=True,
+            #        [[10, 11, 0 , 1 , 2 ],
+            #         [3 , 4 , 5 , 6 , 7 ],
+            #         [8 , 9 , 10, 11, 0 ]])
+
+            paddle.take(x_int, idx_err, mode='clip')
+            # Tensor(shape=[3, 5], dtype=int32, place=Place(cpu), stop_gradient=True,
+            #        [[0 , 0 , 0 , 1 , 2 ],
+            #         [3 , 4 , 5 , 6 , 7 ],
+            #         [8 , 9 , 10, 11, 11]])
+
     """
     if mode not in ['raise', 'wrap', 'clip']:
         raise ValueError(
