e49704e9add3bdb9faa3f1a251b5c022c4022bf3
wuhuachaocoding <huachaowu_ck@163.com>
2022-08-23 07:31:48 +0000

    update.

--- paddle/phi/backends/dynload/mpi_comm.h
+++ paddle/phi/backends/dynload/mpi_comm.h
@@ -13,6 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 #pragma once
 
+#ifdef HOST
+#undef HOST
+#endif
 #include <mpi.h>
 
 #include <mutex>  // NOLINT
