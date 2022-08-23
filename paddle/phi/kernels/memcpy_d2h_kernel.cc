d7df51f6e7e0fee59e255f479de9c24e6297b0e7
kangguangli <kangguangli@hotmail.com>
2022-08-23 18:09:36 +0800

    polish error message

--- paddle/phi/kernels/memcpy_d2h_kernel.cc
+++ paddle/phi/kernels/memcpy_d2h_kernel.cc
@@ -69,9 +69,9 @@ void MemcpyD2HMultiIOKernel(const Context& dev_ctx,
     PADDLE_ENFORCE_NOT_NULL(
         array[i],
         errors::PreconditionNotMet("input tesnor %d should not be nullptr", i));
-    PADDLE_ENFORCE_NOT_NULL(
-        out_array[i],
-        errors::PreconditionNotMet("input tesnor %d should not be nullptr", i));
+    PADDLE_ENFORCE_NOT_NULL(out_array[i],
+                            errors::PreconditionNotMet(
+                                "output tesnor %d should not be nullptr", i));
 
     const auto& x = *(array[i]);
     MemcpyD2HKernel<T, Context>(dev_ctx, x, dst_place_type, out_array[i]);
