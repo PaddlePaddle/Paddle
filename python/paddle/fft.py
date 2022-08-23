6f16b13dab70db794e3cdaefdfec3886c13c11bc
Liyulingyue <83450930+Liyulingyue@users.noreply.github.com>
2022-08-23 20:52:32 +0800

    fft; test=document_fix

--- python/paddle/fft.py
+++ python/paddle/fft.py
@@ -303,11 +303,13 @@ def rfft(x, n=None, axis=-1, norm="backward", name=None):
             the forward/backward  pair of transforms is scaled and with what 
             normalization factor. Include {"backward", "ortho", "forward"}, 
             default value is "backward".
+            
                 - "backward": The factor of forward direction and backward direction are ``1`` 
                 and ``1/n`` respectively;
                 - "forward": The factor of forward direction and backward direction are ``1/n`` 
                 and ``1`` respectively;
                 - "ortho": The factor of forward direction and backword direction are both ``1/sqrt(n)``.
+                
             Where ``n`` is the multiplication of each element in  ``s`` .
         name(str, optional): The default value is None.  Normally there is no 
             need for user to set this property. For more information, please 
@@ -649,11 +651,13 @@ def rfftn(x, s=None, axes=None, norm="backward", name=None):
             normalization factor. Include {"backward", "ortho", "forward"}, 
             default value is "backward". The details of 
             three operations are shown below:
+            
                 - "backward": The factor of forward direction and backward direction are ``1`` 
                 and ``1/n`` respectively;
                 - "forward": The factor of forward direction and backward direction are ``1/n`` 
                 and ``1`` respectively;
                 - "ortho": The factor of forward direction and backword direction are both ``1/sqrt(n)``.
+                
             Where ``n`` is the multiplication of each element in  ``s`` .
         name(str, optional): The default value is None.  Normally there is no 
             need for user to set this property. For more information, please 
@@ -725,11 +729,13 @@ def irfftn(x, s=None, axes=None, norm="backward", name=None):
             pair and what normalization factor to use. The parameter value must be one 
             of "forward" or "backward" or "ortho". Default is "backward". The details of 
             three operations are shown below:
+            
                 - "backward": The factor of forward direction and backward direction are ``1`` 
                 and ``1/n`` respectively;
                 - "forward": The factor of forward direction and backward direction are ``1/n`` 
                 and ``1`` respectively;
                 - "ortho": The factor of forward direction and backword direction are both ``1/sqrt(n)``.
+                
             Where ``n`` is the multiplication of each element in  ``s`` .
         name (str, optional): The default value is None.  Normally there is no need for user to set 
             this property. For more information, please refer to :ref:`api_guide_Name`. 
@@ -1007,11 +1013,13 @@ def rfft2(x, s=None, axes=(-2, -1), norm="backward", name=None):
             forward/backward pair of transforms is scaled and with what 
             normalization factor. The details of 
             three operations are shown below:
+            
                 - "backward": The factor of forward direction and backward direction are ``1`` 
                 and ``1/n`` respectively;
                 - "forward": The factor of forward direction and backward direction are ``1/n`` 
                 and ``1`` respectively;
                 - "ortho": The factor of forward direction and backword direction are both ``1/sqrt(n)``.
+                
             Where ``n`` is the multiplication of each element in  ``s`` .
         name(str, optional): The default value is None.  Normally there is no 
             need for user to set this property. For more information, please 
@@ -1062,11 +1070,13 @@ def irfft2(x, s=None, axes=(-2, -1), norm="backward", name=None):
             pair and what normalization factor to use. The parameter value must be one 
             of "forward" or "backward" or "ortho". Default is "backward". The details of 
             three operations are shown below:
+            
                 - "backward": The factor of forward direction and backward direction are ``1`` 
                 and ``1/n`` respectively;
                 - "forward": The factor of forward direction and backward direction are ``1/n`` 
                 and ``1`` respectively;
                 - "ortho": The factor of forward direction and backword direction are both ``1/sqrt(n)``.
+                
             Where ``n`` is the multiplication of each element in  ``s`` .
         name (str, optional): The default value is None.  Normally there is no need for user to set 
             this property. For more information, please refer to :ref:`api_guide_Name` . 
