26398a4ba1389881cfb2bd93b3f9c879229d257c
zhoutianzi666 <17801055074@163.com>
2022-08-24 13:38:11 +0000

    clean code

--- paddle/fluid/framework/ir/constant_folding_pass.cc
+++ paddle/fluid/framework/ir/constant_folding_pass.cc
@@ -51,74 +51,91 @@ void ConstantFoldingPass::ApplyImpl(ir::Graph *graph) const {
       graph, platform::errors::PreconditionNotMet("graph should not be null."));
   FusePassBase::Init("constant_folding", graph);
   auto *scope = param_scope();
+  // Now, I don't want to fold fill_constant op in Paddle-TRT
+  std::vector<std::string> blacklist{"fill_constant1"};
 
   auto op_node_sorted = framework::ir::TopologyVarientSort(
       *graph, static_cast<framework::ir::SortKind>(0));
   for (auto *op_node : op_node_sorted) {
     if (!op_node->IsOp()) continue;
-    if (op_node->Op()->Type() == "feed") continue;
-    if (op_node->Op()->Type() != "unsqueeze2") continue;
+    if (op_node->Name() == "feed") continue;
+    if (std::find(blacklist.begin(), blacklist.end(), op_node->Name()) !=
+        blacklist.end())
+      continue;
     bool input_persis = true;
+
+    // map is used to record how many time a name string occures in the whole
+    // model
+    std::map<std::string, int> map;
     for (auto in_node : op_node->inputs) {
+      map[in_node->Name()] = 0;
       if (!in_node->Var()->Persistable()) {
         input_persis = false;
       }
     }
-    framework::Scope *new_scope = new framework::Scope();
-    std::unordered_set<const paddle::framework::ir::Node *> remove_nodes;
-    std::unique_ptr<OperatorBase> op;
-
     for (auto out_node : op_node->outputs) {
-      for (auto next_op : out_node->outputs) {
-        if (next_op->Op()->Type() == "set_value") {
-          input_persis = false;
+      map[out_node->Name()] = 0;
+    }
+    // Forbide other node in graph having the same name with nodes in map
+    for (auto iter : map) {
+      for (auto node : graph->Nodes()) {
+        if (node->IsVar() && node->Name() == iter.first) {
+          map[node->Name()]++;
+          if (map[node->Name()] > 1) {
+            input_persis = false;
+          }
         }
       }
     }
 
+    framework::Scope *local_scope = new framework::Scope();
+    std::unordered_set<const paddle::framework::ir::Node *> remove_nodes;
+    std::unique_ptr<OperatorBase> op;
+
     if (input_persis) {
+      std::cout << op_node->Name() << std::endl;
       for (auto in_node : op_node->inputs) {
-        new_scope->Var(in_node->Var()->Name());
-        new_scope->FindVar(in_node->Var()->Name())->GetMutable<LoDTensor>();
+        local_scope->Var(in_node->Var()->Name());
+        local_scope->FindVar(in_node->Var()->Name())->GetMutable<LoDTensor>();
         // This persistable input node is exclusive, and can be removed
         if (in_node->outputs.size() == 1L) remove_nodes.emplace(in_node);
 
         auto in_shape = in_node->Var()->GetShape();
-        auto *persis_x_tensor =
+        auto *global_persis_x_tensor =
             scope->FindVar(in_node->Name())->GetMutable<LoDTensor>();
-        auto *x_tensor =
-            new_scope->FindVar(in_node->Name())->GetMutable<LoDTensor>();
-        x_tensor->Resize(persis_x_tensor->dims());
-        *x_tensor = *persis_x_tensor;
+        auto *local_x_tensor =
+            local_scope->FindVar(in_node->Name())->GetMutable<LoDTensor>();
+        local_x_tensor->Resize(global_persis_x_tensor->dims());
+        *local_x_tensor = *global_persis_x_tensor;
       }
 
       op = paddle::framework::OpRegistry::CreateOp(*op_node->Op());
       remove_nodes.emplace(op_node);
       for (auto out_node : op_node->outputs) {
-        new_scope->Var(out_node->Var()->Name());
-        new_scope->FindVar(out_node->Var()->Name())->GetMutable<LoDTensor>();
+        local_scope->Var(out_node->Var()->Name());
+        local_scope->FindVar(out_node->Var()->Name())->GetMutable<LoDTensor>();
         if (out_node->outputs.size() == 0L) remove_nodes.emplace(out_node);
       }
-      op->Run(*new_scope, platform::CPUPlace());
+      op->Run(*local_scope, platform::CPUPlace());
       for (auto out_node : op_node->outputs) {
         // this out_node is useless, do not set it persistable
         if (out_node->outputs.size() == 0L) continue;
         auto out_desc = out_node->Var();
         auto out_name = out_desc->Name();
         auto *local_out_tensor =
-            new_scope->FindVar(out_name)->GetMutable<LoDTensor>();
+            local_scope->FindVar(out_name)->GetMutable<LoDTensor>();
         std::vector<int64_t> out_shape;
         for (int64_t i = 0; i < local_out_tensor->dims().size(); i++) {
           out_shape.push_back(local_out_tensor->dims()[i]);
         }
         out_desc->SetShape(out_shape);
         out_desc->SetPersistable(true);
-        auto *out_tensor = scope->Var(out_name)->GetMutable<LoDTensor>();
-        *out_tensor = *local_out_tensor;
+        auto *global_out_tensor = scope->Var(out_name)->GetMutable<LoDTensor>();
+        *global_out_tensor = *local_out_tensor;
       }
       GraphSafeRemoveNodes(graph, remove_nodes);
     }
-    delete new_scope;
+    delete local_scope;
   }
 }
 
