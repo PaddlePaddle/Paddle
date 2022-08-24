78916a7a26563166fea783c1040e782ad0e3bee3
Leo Chen <chenqiuliang@baidu.com>
2022-08-24 19:53:28 +0800

    make tensor_util contains no cuda code (#45256)
    * make tensor_util contains no cuda code
    
    * refine isfinite
    
    * revert ut
    
    * move isfinite function to its op
    
    * fix test
    
    * fix compile
    
    * std::isnan is not defined for int type on windows
    
    * fix windows compile
    
    * fix fp16
    
    * fix rocm compile
    
    * revert gradient node

--- paddle/phi/kernels/gpu/isfinite_kernel.cu
+++ paddle/phi/kernels/gpu/isfinite_kernel.cu
@@ -18,19 +18,6 @@
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/impl/isfinite_kernel_impl.h"
 
-namespace phi {
-
-template <typename T, typename Context, typename Functor>
-inline void IsfiniteKernelImpl(const Context& dev_ctx,
-                               const DenseTensor& x,
-                               DenseTensor* out) {
-  dev_ctx.template Alloc<T>(out);
-  Functor functor;
-  functor(x, out);
-}
-
-}  // namespace phi
-
 PD_REGISTER_KERNEL(isinf,
                    GPU,
                    ALL_LAYOUT,
