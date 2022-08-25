91110661eacebaa86845bbd1bc693d54a27d55e2
Aurelius84 <zhangliujie@baidu.com>
2022-08-25 20:30:55 +0800

    [OpAttr]axis of Reverse Support Tensor type (#45391)
    * [OpAttr]axis of Reverse Support Tensor type
    
    * fix coverage
    
    * fix unittest

--- paddle/phi/infermeta/unary.h
+++ paddle/phi/infermeta/unary.h
@@ -388,12 +388,14 @@ void ReshapeWithXShapeInferMeta(const MetaTensor& x,
                                 MetaConfig config = MetaConfig());
 
 void ReverseInferMeta(const MetaTensor& x,
-                      const std::vector<int>& axis,
-                      MetaTensor* out);
+                      const IntArray& axis,
+                      MetaTensor* out,
+                      MetaConfig config = MetaConfig());
 
 void ReverseArrayInferMeta(const std::vector<const phi::MetaTensor*>& x,
-                           const std::vector<int>& axis,
-                           std::vector<phi::MetaTensor*> out);
+                           const IntArray& axis,
+                           std::vector<phi::MetaTensor*> out,
+                           MetaConfig config = MetaConfig());
 
 void RollInferMeta(const MetaTensor& x,
                    const IntArray& shifts,
