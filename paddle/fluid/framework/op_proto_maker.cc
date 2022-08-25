5dac640dc7509dc03f88b69b9c49895a765fa6b4
zyfncg <zhangyunfei07@baidu.com>
2022-08-25 02:24:04 +0000

    polish code

--- paddle/fluid/framework/op_proto_maker.cc
+++ paddle/fluid/framework/op_proto_maker.cc
@@ -12,10 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/framework/op_proto_maker.h"
-#include <iostream>
 #include "paddle/fluid/operators/ops_extra_info.h"
-
-#include <string>
 
 #include "paddle/fluid/platform/enforce.h"
 
