42a54853d8ae5e2b1c6634508a4809000645aed9
zhoutianzi666 <17801055074@163.com>
2022-08-25 01:59:36 +0000

    clean code

--- paddle/fluid/framework/ir/constant_folding_pass.cc
+++ paddle/fluid/framework/ir/constant_folding_pass.cc
@@ -34,9 +34,10 @@ class Node;
 }  // namespace paddle
 
 /*
- * When a op's inputs and outputs is determined before feeding data to the model, we
- * can remove this op from the model. This ConstantFolding pass can remove all
- * these like ops.
+ * When a op's inputs and outputs is determined before feeding data to the
+ * model, we can remove this op from the model. This ConstantFolding pass can
+ * remove all these like ops.
+ *
  */
 
 namespace paddle {
