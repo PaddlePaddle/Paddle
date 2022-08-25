c7941395036563cc7fde575c26ec6fb08835a3cc
kangguangli <kangguangli@hotmail.com>
2022-08-25 07:37:13 +0000

    fix code style check

--- paddle/phi/kernels/memcpy_kernel.cc
+++ paddle/phi/kernels/memcpy_kernel.cc
@@ -1,9 +1,23 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "paddle/phi/kernels/memcpy_kernel.h"
 
 #include <vector>
 
-#include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/common/place.h"
+#include "paddle/phi/core/kernel_registry.h"
 
 namespace phi {
 
@@ -54,7 +68,8 @@ void MemcpyD2HKernel(const Context& dev_ctx,
 
     default:
       PADDLE_THROW(errors::InvalidArgument(
-          "Arugment 'dst_place_type' only support 0-1, but got: %d", dst_place_type));
+          "Arugment 'dst_place_type' only support 0-1, but got: %d",
+          dst_place_type));
       break;
   }
 }
@@ -82,7 +97,6 @@ void MemcpyD2HMultiIOKernel(const Context& dev_ctx,
     MemcpyD2HKernel<Context>(dev_ctx, x, dst_place_type, out_array[i]);
   }
 }
-
 
 }  // namespace phi
 
