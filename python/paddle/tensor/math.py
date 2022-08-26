4b80859e487f4f1c149f273ad0dce5b7c3d36099
zhengbicheng <a192161647@163.com>
2022-08-26 15:43:21 +0800

    代码风格更变

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -4812,7 +4812,6 @@ def frexp(x, dtype=paddle.float32):
     input_x = paddle.abs(x)
     exponent = paddle.floor(paddle.log2(input_x))
     exponent = paddle.where(paddle.isinf(exponent), paddle.full_like(exponent, 0), exponent)
-
     # 0填充
     mantissa = paddle.divide(input_x, 2 ** exponent)
     # 计算exponent
