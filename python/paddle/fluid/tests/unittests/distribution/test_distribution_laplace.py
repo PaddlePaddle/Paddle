748936ef30c92819b60ad7d993e3305d0287b8ac
nihao <nihao@baidu.com>
2022-08-27 22:20:06 +0800

    bugfix

--- python/paddle/fluid/tests/unittests/distribution/test_distribution_laplace.py
+++ python/paddle/fluid/tests/unittests/distribution/test_distribution_laplace.py
@@ -66,7 +66,7 @@ class TestLaplace(unittest.TestCase):
 
     def test_sample(self):
 
-        sample_shape = (1000, )
+        sample_shape = (100000, )
         samples = self._dist.sample(sample_shape)
         sample_values = samples.numpy()
 
