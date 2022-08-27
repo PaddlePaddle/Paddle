145ee68192d44c1440a60e07665e40aa11278354
SmirnovKol <31559413+OccupyMars2025@users.noreply.github.com>
2022-08-27 22:36:08 +0800

    Update test_iinfo_and_finfo.py

--- python/paddle/fluid/tests/unittests/test_iinfo_and_finfo.py
+++ python/paddle/fluid/tests/unittests/test_iinfo_and_finfo.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import paddle
 import unittest
 import numpy as np
@@ -5,7 +19,6 @@ import numpy as np
 class TestIInfoAndFInfoAPI(unittest.TestCase):
     def test_invalid_input(self):
         for dtype in [paddle.float16, paddle.float32, paddle.float64, paddle.bfloat16, paddle.complex64, paddle.complex128, paddle.bool]:
-            # I think it's best to raise TypeError, not  ValueError
             with self.assertRaises(ValueError):
                 _ = paddle.iinfo(dtype)
 
