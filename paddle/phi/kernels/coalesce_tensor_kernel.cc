cd534cae0054acd5b5fc4ac1d85ec187b45bc206
jiahongyu <jiahongyu@baidu.com>
2022-08-29 08:17:08 +0000

    restore XPU kernel temporarily

--- paddle/phi/kernels/coalesce_tensor_kernel.cc
+++ paddle/phi/kernels/coalesce_tensor_kernel.cc
@@ -22,9 +22,6 @@
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/funcs/math_function.h"
-#ifdef PADDLE_WITH_XPU
-#include "paddle/phi/backends/xpu/xpu_context.h"
-#endif
 
 namespace phi {
 
@@ -274,19 +271,6 @@ PD_REGISTER_KERNEL(coalesce_tensor,
 #if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
 PD_REGISTER_KERNEL(coalesce_tensor,
                    GPU,
-                   ALL_LAYOUT,
-                   phi::CoalesceTensorKernel,
-                   phi::dtype::float16,
-                   int,
-                   float,
-                   double) {
-  kernel->InputAt(0).SetBackend(phi::Backend::ALL_BACKEND);
-}
-#endif
-
-#ifdef PADDLE_WITH_XPU
-PD_REGISTER_KERNEL(coalesce_tensor,
-                   XPU,
                    ALL_LAYOUT,
                    phi::CoalesceTensorKernel,
                    phi::dtype::float16,
