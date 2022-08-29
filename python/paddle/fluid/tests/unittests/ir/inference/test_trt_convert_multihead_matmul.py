95321c7f0663e96897dd20fb38e0417d0ff51e27
fengshuai <fengshuai03@baidu.com>
2022-08-29 08:17:41 +0000

    just support trt8

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_multihead_matmul.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_multihead_matmul.py
@@ -1114,7 +1114,8 @@ class TrtConvertVitToMultiHeadMatmulTest(TrtLayerAutoScanTest):
         yield self.create_inference_config(), generate_trt_nodes_num(), (1e-3,
                                                                          1e-3)
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        yield self.create_inference_config(), (1, 2), (1e-3, 1e-3)
+        yield self.create_inference_config(), generate_trt_nodes_num(), (1e-3,
+                                                                         1e-3)
 
     def add_skip_trt_case(self):
         pass
