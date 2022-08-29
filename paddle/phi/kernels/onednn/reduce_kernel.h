0171abe0a6ef8a08ce02e1a9d67a0a0432c1cb92
Piotr Paturej <piotr.paturej@intel.com>
2022-08-29 19:13:28 +0200

    Remove unnecessary header

--- paddle/phi/kernels/onednn/reduce_kernel.h
+++ paddle/phi/kernels/onednn/reduce_kernel.h
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
-#include "paddle/fluid/framework/convert_utils.h"
 #include "paddle/phi/kernels/funcs/onednn/onednn_reuse.h"
 
 namespace phi {
