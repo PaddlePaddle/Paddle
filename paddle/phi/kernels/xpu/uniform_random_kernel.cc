553736641061b27e09032a9cb1d4c1a0b376c38e
jiahongyu <jiahongyu@baidu.com>
2022-08-30 12:09:34 +0000

    migrate uniform_random, test=kunlun

--- paddle/phi/kernels/xpu/uniform_random_kernel.cc
+++ paddle/phi/kernels/xpu/uniform_random_kernel.cc
@@ -16,44 +16,43 @@ limitations under the License. */
 
 #include <string>
 
-#include "paddle/fluid/framework/generator.h"
-#include "paddle/fluid/framework/operator.h"
+#include "paddle/phi/backends/xpu/xpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/funcs/uniform_real_distribution.h"
 
 namespace phi {
 
 template <typename T, typename Context>
-void UniformRandomKernel(const Context& dev_ctx,
-                         const IntArray& shape,
-                         DataType dtype,
-                         const Scalar& min,
-                         const Scalar& max,
-                         int seed,
-                         DenseTensor* out) {
+void UniformRandomRawKernel(const Context &dev_ctx,
+                            const IntArray &shape,
+                            DataType dtype,
+                            const Scalar &min,
+                            const Scalar &max,
+                            int seed,
+                            int diag_num,
+                            int diag_step,
+                            float diag_val,
+                            DenseTensor *out) {
   out->Resize(phi::make_ddim(shape.GetData()));
-  T* data = dev_ctx.template Alloc<T>(out);
+  dev_ctx.template Alloc<T>(out);
   int64_t size = out->numel();
 
   std::unique_ptr<T[]> data_cpu(new T[size]);
-  std::uniform_real_distribution<T> dist(
-      static_cast<T>(ctx.Attr<float>("min")),
-      static_cast<T>(ctx.Attr<float>("max")));
-  unsigned int seed = static_cast<unsigned int>(ctx.Attr<int>("seed"));
-  auto engine = framework::GetCPURandomEngine(seed);
 
-  for (int64_t i = 0; i < size; ++i) {
-    data_cpu[i] = dist(*engine);
+  std::shared_ptr<std::mt19937_64> engine;
+  if (seed) {
+    engine = std::make_shared<std::mt19937_64>();
+    engine->seed(seed);
+  } else {
+    engine = dev_ctx.GetGenerator()->GetCPUEngine();
   }
-
-  unsigned int diag_num = static_cast<unsigned int>(ctx.Attr<int>("diag_num"));
-  unsigned int diag_step =
-      static_cast<unsigned int>(ctx.Attr<int>("diag_step"));
-  auto diag_val = static_cast<T>(ctx.Attr<float>("diag_val"));
+  UniformRealDistribution<T>(
+      data_cpu, size, min.to<float>(), max.to<float>(), engine);
   if (diag_num > 0) {
     PADDLE_ENFORCE_GT(
         size,
         (diag_num - 1) * (diag_step + 1),
-        platform::errors::InvalidArgument(
+        phi::errors::InvalidArgument(
             "ShapeInvalid: the diagonal's elements is equal (num-1) "
             "* (step-1) with num %d, step %d,"
             "It should be smaller than %d, but received %d",
@@ -67,14 +66,14 @@ void UniformRandomKernel(const Context& dev_ctx,
     }
   }
 
-  memory::Copy(ctx.GetPlace(),
-               data,
-               platform::CPUPlace(),
-               reinterpret_cast<void*>(data_cpu.get()),
-               size * sizeof(T));
+  paddle::memory::Copy(ctx.GetPlace(),
+                       out,
+                       phi::CPUPlace(),
+                       reinterpret_cast<void *>(data_cpu.get()),
+                       size * sizeof(T));
 }
 
 }  // namespace phi
 
-REGISTER_OP_XPU_KERNEL(uniform_random,
-                       paddle::operators::XPUUniformRandomKernel<float>);
+PD_REGISTER_KERNEL(
+    uniform_random_raw, XPU, ALL_LAYOUT, phi::UniformRandomRawKernel, float) {}
