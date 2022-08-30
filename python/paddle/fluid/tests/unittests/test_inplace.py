a5b8a5a888e78cfb2f2c8fa784a9fda96f94a176
zrr1999 <2742392377@qq.com>
2022-08-30 15:49:23 +0800

    fix bug

--- python/paddle/fluid/tests/unittests/test_inplace.py
+++ python/paddle/fluid/tests/unittests/test_inplace.py
@@ -483,7 +483,7 @@ class TestDygraphInplaceSubtract(TestDygraphInplaceAdd):
         return var.subtract_(input_var_2)
 
 
-class TestDygraphInplaceRemainder(TestDygraphInplace):
+class TestDygraphInplaceRemainder(TestDygraphInplaceAdd):
 
     def non_inplace_api_processing(self, var):
         input_var_2 = paddle.to_tensor(self.input_var_numpy_2)
