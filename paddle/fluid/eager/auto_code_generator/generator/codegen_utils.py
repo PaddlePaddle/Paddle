1fdfe8fe19b406046e4f24fd953adee671745023
Charles-hit <56987902+Charles-hit@users.noreply.github.com>
2022-08-31 10:35:10 +0800

    Fix split api bug (#45396)
    * fix split bug
    
    * solve function redefine
    
    * fix fluid.layers.split and add unit test
    
    * delete splitInferMeta register in unary.cc
    
    * modify test_split_op GPU unit test
    
    * modify test_split_op GPU unit test place param
    
    * refactor split op and fix infershape bugs
    
    * add () in && and ||
    
    * fix split C++ unit test
    
    * fix split infershape

--- paddle/fluid/eager/auto_code_generator/generator/codegen_utils.py
+++ paddle/fluid/eager/auto_code_generator/generator/codegen_utils.py
@@ -22,16 +22,16 @@ import os
 ### Global Variables ###
 ########################
 ops_to_fill_zero_for_empty_grads = set([
-    "split_grad", "rnn_grad", "matmul_double_grad", "matmul_triple_grad",
-    "sigmoid_double_grad", "sigmoid_triple_grad", "add_double_grad",
-    "add_triple_grad", "multiply_grad", "multiply_double_grad",
-    "multiply_triple_grad", "conv2d_grad_grad", "batch_norm_double_grad",
-    "tanh_double_grad", "tanh_triple_grad", "subtract_double_grad",
-    "divide_double_grad", "log_double_grad", "elu_double_grad",
-    "leaky_relu_double_grad", "sqrt_double_grad", "rsqrt_double_grad",
-    "square_double_grad", "celu_double_grad", "pad_double_grad",
-    "pad3d_double_grad", "squeeze_double_grad", "unsqueeze_double_grad",
-    "instance_norm_double_grad", "conv3d_double_grad",
+    "split_grad", "split_with_num_grad", "rnn_grad", "matmul_double_grad",
+    "matmul_triple_grad", "sigmoid_double_grad", "sigmoid_triple_grad",
+    "add_double_grad", "add_triple_grad", "multiply_grad",
+    "multiply_double_grad", "multiply_triple_grad", "conv2d_grad_grad",
+    "batch_norm_double_grad", "tanh_double_grad", "tanh_triple_grad",
+    "subtract_double_grad", "divide_double_grad", "log_double_grad",
+    "elu_double_grad", "leaky_relu_double_grad", "sqrt_double_grad",
+    "rsqrt_double_grad", "square_double_grad", "celu_double_grad",
+    "pad_double_grad", "pad3d_double_grad", "squeeze_double_grad",
+    "unsqueeze_double_grad", "instance_norm_double_grad", "conv3d_double_grad",
     "depthwise_conv2d_grad_grad", "concat_double_grad", "expand_grad",
     "argsort_grad"
 ])
