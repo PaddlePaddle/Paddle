7d1a6a8cf28bc1c21236680acc3507e24d07bdf0
Hui Zhang <zhtclz@foxmail.com>
2022-09-01 03:57:35 +0000

    using with mkldnn macro

--- paddle/fluid/framework/details/build_strategy.h
+++ paddle/fluid/framework/details/build_strategy.h
@@ -150,7 +150,11 @@ struct BuildStrategy {
   // Inference pass
   bool enable_inference_pass_{false};  // switch for infernce pass
   bool delete_dropout_{true};          // delte dropout op
-  bool use_mkldnn_{false};             // use mkdnn to do inference
+#ifdef PADDLE_WITH_MKLDNN
+  bool use_mkldnn_{true};  // use mkdnn to do inference
+#else
+  bool use_mkldnn_{false};  // use mkdnn to do inference
+#endif
 
   // FIXME(zcd): is_distribution_ is a temporary field, because in pserver mode,
   // num_trainers is 1, so the current fields of build_strategy doesn't tell if
