c6693b96fc9c6a0f8719897203f809f0f3700452
xieyunshen <xieyunshen@baidu.com>
2022-09-06 14:32:07 +0800

    移除build目录大小限制

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3560,7 +3560,6 @@ function main() {
         check_diff_file_for_coverage
         cmake_gen_and_build ${PYTHON_ABI:-""} ${parallel_number}
         enable_unused_var_check
-        check_coverage_build
         ;;
       gpu_cicheck_coverage)
         parallel_test
