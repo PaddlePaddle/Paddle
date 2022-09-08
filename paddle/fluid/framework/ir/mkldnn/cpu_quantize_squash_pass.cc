0f1faf2fe98b7b4b9c09b398e16b01fc9ad22f1e
Sylwester Fraczek <sylwester.fraczek@intel.com>
2022-09-08 18:31:33 +0200

    fix MultipleQuantizeSquash for fc+residual

--- paddle/fluid/framework/ir/mkldnn/cpu_quantize_squash_pass.cc
+++ paddle/fluid/framework/ir/mkldnn/cpu_quantize_squash_pass.cc
@@ -25,6 +25,24 @@ namespace paddle {
 namespace framework {
 namespace ir {
 
+namespace {
+std::string FindInputNameByVarName(OpDesc* op,
+                                   const std::string& searched_name) {
+  for (auto name : op->InputNames())
+    for (auto input_name : op->Input(name))
+      if (input_name == searched_name) return name;
+  return std::string{};
+}
+
+std::string FindOutputNameByVarName(OpDesc* op,
+                                    const std::string& searched_name) {
+  for (auto name : op->OutputNames())
+    for (auto output_name : op->Output(name))
+      if (output_name == searched_name) return name;
+  return std::string{};
+}
+}  // namespace
+
 using string::PrettyLogDetail;
 
 CPUQuantizeSquashPass::CPUQuantizeSquashPass() {
@@ -375,10 +393,7 @@ void CPUQuantizeSquashPass::OpDequantSquash(Graph* graph) const {
           return;
       }
       // Find the name of the output linking any_op to dequant_in
-      std::string output_name;
-      for (auto name : any_op->Op()->OutputNames())
-        for (auto out_name : any_op->Op()->Output(name))
-          if (out_name == dequant_in->Name()) output_name = name;
+      std::string output_name = FindOutputNameByVarName(dequant_in->Name());
 
       if (output_name.empty()) return;
 
@@ -431,29 +446,51 @@ void CPUQuantizeSquashPass::MultipleQuantizeSquash(Graph* graph) const {
           quant_op->Op()->GetAttrIfExists<float>("Shift") == shift) {
         auto quant_out = quant_op->outputs[0];
         auto last_op = quant_out->outputs[0];
+        auto last_op_op = last_op->Op();
+        bool residual_fc = false;
 
-        std::string last_op_input_name;
-        for (auto name : last_op->Op()->InputNames())
-          for (auto input_name : last_op->Op()->Input(name))
-            if (input_name == quant_out->Name()) last_op_input_name = name;
+        std::string last_op_input_name =
+            FindInputNameByVarName(last_op_op, quant_out->Name());
+        if (last_op_input_name.empty()) {
+          last_op_input_name =
+              FindOutputNameByVarName(last_op_op, quant_out->Name());
+          PADDLE_ENFORCE_EQ(last_op_input_name,
+                            "ResidualData",
+                            platform::errors::InvalidArgument(
+                                "Only the ResidualData output is allowed to be "
+                                "linked to earlier op."));
+          PADDLE_ENFORCE_EQ(last_op_op->Type(),
+                            "fc",
+                            platform::errors::InvalidArgument(
+                                "Only fc operator supports ResidualData output "
+                                "linked as input."));
+          residual_fc = true;
+        }
 
         PADDLE_ENFORCE_NE(
             last_op_input_name.empty(),
             true,
             platform::errors::NotFound("Operator after quantize operator(%s) "
-                                       "should has quantize output as input.",
+                                       "should have quantize output as input.",
                                        quant_out->Name()));
 
         // update the next operator input,
         // by replacing quant_out with first_quant_out
-        auto last_op_names = last_op->Op()->Input(last_op_input_name);
+        auto last_op_names = residual_fc
+                                 ? last_op_op->Output(last_op_input_name)
+                                 : last_op_op->Input(last_op_input_name);
         last_op_names.erase(
             std::remove(
                 last_op_names.begin(), last_op_names.end(), quant_out->Name()),
             last_op_names.end());
         last_op_names.push_back(first_quant_out->Name());
-        last_op->Op()->SetInput(last_op_input_name,
+        if (residual_fc) {
+          last_op_op->SetOutput(last_op_input_name,
                                 std::vector<std::string>(last_op_names));
+        } else {
+          last_op_op->SetInput(last_op_input_name,
+                               std::vector<std::string>(last_op_names));
+        }
 
         IR_NODE_LINK_TO(first_quant_out, last_op);
         GraphSafeRemoveNodes(graph, {quant_op, quant_out});
