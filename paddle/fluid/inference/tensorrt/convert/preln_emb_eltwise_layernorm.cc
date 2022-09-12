00974c1e69be06c66fd3edd07c11342dcb80540f
Wangzheee <634486483@qq.com>
2022-09-12 14:30:19 +0000

    support n lookup_tables fuse to embeddinglayernorm

--- paddle/fluid/inference/tensorrt/convert/preln_emb_eltwise_layernorm.cc
+++ paddle/fluid/inference/tensorrt/convert/preln_emb_eltwise_layernorm.cc
@@ -192,7 +192,9 @@ class PrelnEmbEltwiseLayerNormOpConverter : public OpConverter {
     std::vector<nvinfer1::ITensor*> plugin_inputs = input_ids;
     plugin_inputs.emplace_back(engine_->GetITensor(
         "max_seqlen_tensor"));  // max_seqlen, eval_placeholder_3
-    auto creator = GetPluginRegistry()->getPluginCreator(
+    
+    nvinfer1::IPluginRegistry *plugin_register = static_cast<nvinfer1::IPluginRegistry *>(dy::getPluginRegistry());
+    auto creator = plugin_register->getPluginCreator(
         "ManyEmbLayerNormPluginDynamic", "3");
 
     auto plugin_obj =
