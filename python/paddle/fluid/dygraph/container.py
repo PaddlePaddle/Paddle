db9108aa05bc68aa829a683d555e8e29301235a7
firestonelib <firestonelib@gmail.com>
2022-09-13 05:32:15 +0000

    add index == 0

--- python/paddle/fluid/dygraph/container.py
+++ python/paddle/fluid/dygraph/container.py
@@ -297,6 +297,10 @@ class LayerList(Layer):
                 linears.insert(-1, another)
                 print(linears[-2] is another) # True
         """
+        assert isinstance(index, int) and \
+               -len(self._sub_layers) <= index <= len(self._sub_layers), \
+            "index should be an integer in range [{}, {})".format(-len(self), len(self))
+
         for i in range(len(self._sub_layers), index, -1):
             self._sub_layers[str(i)] = self._sub_layers[str(i - 1)]
         self._sub_layers[str(index)] = sublayer
