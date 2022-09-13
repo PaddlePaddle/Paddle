d7b320a0b85c9cbd70373cc050ca371cc2a6a276
JiabinYang <360788950@qq.com>
2022-09-13 05:27:59 +0000

    refactor code for better structure

--- paddle/phi/kernels/xpu/bmm_grad_kernel.cc
+++ paddle/phi/kernels/xpu/bmm_grad_kernel.cc
@@ -14,59 +14,9 @@
 
 #include "paddle/phi/kernels/bmm_grad_kernel.h"
 
-#include "paddle/phi/backends/xpu/xpu_context.h"
-#include "paddle/phi/core/enforce.h"
-#include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/impl/matmul_grad_kernel_impl.h"
+#include "paddle/phi/kernels/xpu/bmm_xpu_utils.h"
 
-// See Note [ Why still include the fluid headers? ]
-#include "paddle/fluid/platform/device/xpu/xpu_header.h"
-#include "paddle/phi/kernels/xpu/xpu_api_wrapper.h"
 namespace phi {
-
-template <typename T, typename FCT>
-static void MatMulXPUFunction(const DenseTensor& x,
-                              const DenseTensor& y,
-                              DenseTensor* out,
-                              bool trans_x,
-                              bool trans_y,
-                              xpu::Context* xpu_ctx) {
-  using XPUType = typename XPUTypeTrait<T>::Type;
-  const auto& x_dims = x.dims();
-  const auto& y_dims = y.dims();
-
-  auto mat_dim_a = phi::funcs::CreateMatrixDescriptor(
-      RowMatrixFromVector(x_dims), 0, trans_x);
-  auto mat_dim_b = phi::funcs::CreateMatrixDescriptor(
-      ColumnMatrixFromVector(y_dims), 0, trans_y);
-
-  T* data_c = out->data<T>();
-  int m = mat_dim_a.height_;
-  int n = mat_dim_b.width_;
-  int k = mat_dim_a.width_;
-  int batch_size = mat_dim_a.batch_size_;
-  // batch matmul
-  int r = xpu::fc_batched<XPUType, XPUType, XPUType, FCT>(
-      xpu_ctx,                                        // Context* ctx,
-      batch_size,                                     // int batch_size,
-      mat_dim_a.trans_,                               // bool x_trans,
-      mat_dim_b.trans_,                               // bool w_trans,
-      m,                                              // int m,
-      n,                                              // int n,
-      k,                                              // int k,
-      1.0,                                            // float alpha,
-      reinterpret_cast<const XPUType*>(x.data<T>()),  // const TX* x,
-      mat_dim_a.stride_,                              // int stride_a,
-      reinterpret_cast<const XPUType*>(y.data<T>()),  // const TW* w,
-      mat_dim_b.stride_,                              // int stride_b,
-      0.0,                                            // float beta,
-      reinterpret_cast<XPUType*>(data_c),             // TY* y,
-      m * n,                                          // int stride_c,
-      nullptr,                                        // const float* x_maxptr,
-      nullptr);                                       // const float* w_maxptr
-
-  PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_batched");
-}
 
 template <typename T, typename Context>
 void MatMul(const Context& dev_ctx,
