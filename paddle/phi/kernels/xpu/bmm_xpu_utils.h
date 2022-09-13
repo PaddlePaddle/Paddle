d84a20a17e2ef529ff8fb82e70d1bbd4f3d241f8
JiabinYang <360788950@qq.com>
2022-09-13 06:54:43 +0000

    test=kunlun

--- paddle/phi/kernels/xpu/bmm_xpu_utils.h
+++ paddle/phi/kernels/xpu/bmm_xpu_utils.h
@@ -16,6 +16,7 @@
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/impl/matmul_grad_kernel_impl.h"
 #include "paddle/phi/kernels/xpu/xpu_api_wrapper.h"
+
 namespace phi {
 template <typename T, typename FCT>
 static void MatMulXPUFunction(const DenseTensor& x,
