7deda3e985b29221b439813ffb4b4ff002954fa0
zhangbo9674 <zhangbo54@baidu.com>
2022-09-13 08:49:40 +0000

    refine code

--- python/paddle/fluid/dygraph/amp/auto_cast.py
+++ python/paddle/fluid/dygraph/amp/auto_cast.py
@@ -76,7 +76,7 @@ AMP_RELATED_FLAGS_SETTING = {
     'FLAGS_cudnn_batchnorm_spatial_persistent': 1,
 }
 
-PURE_FP16_WHITE_LIST = {' '}
+PURE_FP16_WHITE_LIST = {''}
 PURE_FP16_BLACK_LIST = {
     'lookup_table',
     'lookup_table_v2',
@@ -91,10 +91,10 @@ PURE_FP16_BLACK_LIST = {
 }
 
 BF16_WHITE_LIST = {'conv2d', 'matmul_v2'}
-BF16_BLACK_LIST = {' '}
+BF16_BLACK_LIST = {''}
 
-PURE_BF16_WHITE_LIST = {' '}
-PURE_BF16_BLACK_LIST = {' '}
+PURE_BF16_WHITE_LIST = {''}
+PURE_BF16_BLACK_LIST = {''}
 
 _g_amp_state_ = None
 
@@ -454,10 +454,10 @@ def amp_decorate(models,
         models(Layer|list of Layer, optional): The defined models by user, models must be either a single model or a list of models. Default is None.
         optimizers(Optimizer|list of Optimizer, optional): The defined optimizers by user, optimizers must be either a single optimizer or a list of optimizers. Default is None.
         level(str, optional): Auto mixed precision level. Accepted values are "O1" and "O2": O1 represent mixed precision, the decorator will do nothing; 
-             O2 represent Pure fp16, the decorator will cast all parameters of models to FP16, except BatchNorm and LayerNorm. Default is O1(amp)
+             O2 represent Pure fp16/bf16, the decorator will cast all parameters of models to FP16/BF16, except BatchNorm and LayerNorm. Default is O1(amp)
         dtype(str, optional): Whether to use 'float16' or 'bfloat16'. Default is 'float16'.
         master_weight(bool, optinal): For level='O2', whether to use multi-precision during weight updating. If master_weight is None, in O2 level optimizer will use multi-precision. Default is None.
-        save_dtype(float, optional): The save model parameter dtype when use `paddle.save` or `paddle.jit.save`,it should be float16, float32, float64 or None.
+        save_dtype(float, optional): The save model parameter dtype when use `paddle.save` or `paddle.jit.save`,it should be float16, bfloat16, float32, float64 or None.
              The save_dtype will not change model parameters dtype, it just change the state_dict dtype. When save_dtype is None, the save dtype is same as model dtype. Default is None.
 
     Examples:
