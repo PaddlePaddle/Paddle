0b82fb328e79662478545c3a84fbdf3cb8df037f
zhangkaihuo <zhangkaihuo@baidu.com>
2022-09-14 16:16:43 +0800

    [Sparse]Remove unused code (#46021)

--- paddle/phi/api/yaml/generator/sparse_bw_api_gen.py
+++ paddle/phi/api/yaml/generator/sparse_bw_api_gen.py
@@ -111,7 +111,6 @@ def source_include(header_file_path):
 #include "paddle/phi/api/include/sparse_api.h"
 #include "paddle/phi/api/lib/api_gen_utils.h"
 #include "paddle/phi/api/lib/kernel_dispatch.h"
-#include "paddle/phi/api/lib/sparse_api_custom_impl.h"
 #include "paddle/phi/core/kernel_registry.h"
 """
 
