84dd2569e807f86ae8eebf2ddd7b8fb5375b9ff6
RachelXu7 <molixu7@gmail.com>
2022-09-16 07:39:21 +0000

    Add LSQ/LSQ+ in QAT

--- python/paddle/fluid/contrib/slim/tests/test_imperative_qat_lsq.py
+++ python/paddle/fluid/contrib/slim/tests/test_imperative_qat_lsq.py
@@ -1,4 +1,4 @@
-#   copyright (c) 2018 paddlepaddle authors. all rights reserved.
+#   copyright (c) 2022 paddlepaddle authors. all rights reserved.
 #
 # licensed under the apache license, version 2.0 (the "license");
 # you may not use this file except in compliance with the license.
@@ -30,7 +30,6 @@ from paddle.fluid.contrib.slim.quantization import ImperativeQuantAware
 from paddle.fluid.dygraph.container import Sequential
 from paddle.nn import ReLU, ReLU6, LeakyReLU, Sigmoid, Softmax, PReLU
 from paddle.nn import Linear, Conv2D, Softmax, BatchNorm2D, MaxPool2D
-from paddle.nn import BatchNorm1D
 from paddle.fluid.log_helper import get_logger
 from paddle.fluid.dygraph.io import INFER_MODEL_SUFFIX, INFER_PARAMS_SUFFIX
 from paddle.nn.quant.quant_layers import QuantizedConv2D, QuantizedConv2DTranspose
@@ -106,8 +105,6 @@ class TestImperativeQatLSQ(unittest.TestCase):
         self.weight_quantize_type = 'lsq_weight'
         self.activation_quantize_type = 'lsq_act'
         self.onnx_format = False
-        self.check_export_model_accuracy = True
-        self.diff_threshold = 0.01
         self.fuse_conv_bn = False
 
     def func_qat(self):
@@ -118,100 +115,80 @@ class TestImperativeQatLSQ(unittest.TestCase):
             activation_quantize_type=self.activation_quantize_type,
             fuse_conv_bn=self.fuse_conv_bn)
 
-        with fluid.dygraph.guard():
-            # For CI coverage
-            seed = 1
-            np.random.seed(seed)
-            fluid.default_main_program().random_seed = seed
-            fluid.default_startup_program().random_seed = seed
+        seed = 100
+        np.random.seed(seed)
+        fluid.default_main_program().random_seed = seed
+        fluid.default_startup_program().random_seed = seed
+        paddle.disable_static()
+        lenet = ImperativeLenet()
+        lenet = fix_model_dict(lenet)
+        imperative_qat.quantize(lenet)
+        optimizer = MomentumOptimizer(learning_rate=0.1,
+                                      parameter_list=lenet.parameters(),
+                                      momentum=0.9)
 
-            lenet = ImperativeLenet()
-            lenet = fix_model_dict(lenet)
-            imperative_qat.quantize(lenet)
-            adam = MomentumOptimizer(learning_rate=0.1,
-                                     parameter_list=lenet.parameters(),
-                                     momentum=0.9)
+        train_reader = paddle.batch(paddle.dataset.mnist.train(),
+                                    batch_size=64,
+                                    drop_last=True)
+        test_reader = paddle.batch(paddle.dataset.mnist.test(), batch_size=32)
+        epoch_num = 2
+        for epoch in range(epoch_num):
+            lenet.train()
+            for batch_id, data in enumerate(train_reader()):
+                x_data = np.array([x[0].reshape(1, 28, 28)
+                                   for x in data]).astype('float32')
+                y_data = np.array([x[1] for x in data
+                                   ]).astype('int64').reshape(-1, 1)
 
-            train_reader = paddle.batch(paddle.dataset.mnist.train(),
-                                        batch_size=64,
-                                        drop_last=True)
-            test_reader = paddle.batch(paddle.dataset.mnist.test(),
-                                       batch_size=32)
-            epoch_num = 2
-            for epoch in range(epoch_num):
-                lenet.train()
-                for batch_id, data in enumerate(train_reader()):
+                img = fluid.dygraph.to_variable(x_data)
+                label = fluid.dygraph.to_variable(y_data)
+                out = lenet(img)
+                acc = fluid.layers.accuracy(out, label)
+                loss = fluid.layers.cross_entropy(out, label)
+                avg_loss = paddle.mean(loss)
+
+                avg_loss.backward()
+                optimizer.minimize(avg_loss)
+                lenet.clear_gradients()
+
+                if batch_id % 100 == 0:
+                    _logger.info(
+                        "Train | At epoch {} step {}: loss = {:}, acc= {:}".
+                        format(epoch, batch_id, avg_loss.numpy(), acc.numpy()))
+
+            lenet.eval()
+            eval_acc_top1_list = []
+            with paddle.no_grad():
+                for batch_id, data in enumerate(test_reader()):
+
                     x_data = np.array([x[0].reshape(1, 28, 28)
                                        for x in data]).astype('float32')
                     y_data = np.array([x[1] for x in data
                                        ]).astype('int64').reshape(-1, 1)
-
                     img = fluid.dygraph.to_variable(x_data)
                     label = fluid.dygraph.to_variable(y_data)
-                    out = lenet(img)
-                    acc = fluid.layers.accuracy(out, label)
-                    loss = fluid.layers.cross_entropy(out, label)
-                    avg_loss = paddle.mean(loss)
 
-                    avg_loss.backward()
-                    adam.minimize(avg_loss)
-                    lenet.clear_gradients()
+                    out = lenet(img)
+                    acc_top1 = fluid.layers.accuracy(input=out,
+                                                     label=label,
+                                                     k=1)
+                    acc_top5 = fluid.layers.accuracy(input=out,
+                                                     label=label,
+                                                     k=5)
 
                     if batch_id % 100 == 0:
+                        eval_acc_top1_list.append(float(acc_top1.numpy()))
                         _logger.info(
-                            "Train | At epoch {} step {}: loss = {:}, acc= {:}".
-                            format(epoch, batch_id, avg_loss.numpy(),
-                                   acc.numpy()))
-                    if batch_id == 1000:  # For shortening CI time
-                        break
+                            "Test | At epoch {} step {}: acc1 = {:}, acc5 = {:}"
+                            .format(epoch, batch_id, acc_top1.numpy(),
+                                    acc_top5.numpy()))
 
-                lenet.eval()
-                eval_acc_top1_list = []
-                with paddle.no_grad():
-                    for batch_id, data in enumerate(test_reader()):
-
-                        x_data = np.array([
-                            x[0].reshape(1, 28, 28) for x in data
-                        ]).astype('float32')
-                        y_data = np.array([x[1] for x in data
-                                           ]).astype('int64').reshape(-1, 1)
-                        img = fluid.dygraph.to_variable(x_data)
-                        label = fluid.dygraph.to_variable(y_data)
-
-                        out = lenet(img)
-                        acc_top1 = fluid.layers.accuracy(input=out,
-                                                         label=label,
-                                                         k=1)
-                        acc_top5 = fluid.layers.accuracy(input=out,
-                                                         label=label,
-                                                         k=5)
-
-                        if batch_id % 100 == 0:
-                            eval_acc_top1_list.append(float(acc_top1.numpy()))
-                            _logger.info(
-                                "Test | At epoch {} step {}: acc1 = {:}, acc5 = {:}"
-                                .format(epoch, batch_id, acc_top1.numpy(),
-                                        acc_top5.numpy()))
-
-                # check eval acc
-                eval_acc_top1 = sum(eval_acc_top1_list) / len(
-                    eval_acc_top1_list)
-                print('eval_acc_top1', eval_acc_top1)
-            self.assertTrue(eval_acc_top1 > 0.9,
-                            msg="The test acc {%f} is less than 0.9." %
-                            eval_acc_top1)
-
-            # test the correctness of `paddle.jit.save`
-            with paddle.no_grad():
-                data = next(test_reader())
-                test_data = np.array([x[0].reshape(1, 28, 28)
-                                      for x in data]).astype('float32')
-                y_data = np.array([x[1] for x in data
-                                   ]).astype('int64').reshape(-1, 1)
-                test_img = fluid.dygraph.to_variable(test_data)
-                label = fluid.dygraph.to_variable(y_data)
-                fp32_out = lenet(test_img)
-                fp32_acc = fluid.layers.accuracy(fp32_out, label).numpy()
+            # check eval acc
+            eval_acc_top1 = sum(eval_acc_top1_list) / len(eval_acc_top1_list)
+            print('eval_acc_top1', eval_acc_top1)
+        self.assertTrue(eval_acc_top1 > 0.9,
+                        msg="The test acc {%f} is less than 0.9." %
+                        eval_acc_top1)
 
     def test_qat(self):
         self.func_qat()
@@ -220,9 +197,9 @@ class TestImperativeQatLSQ(unittest.TestCase):
 class TestImperativeQatLSQChannelWise(TestImperativeQatLSQ):
 
     def set_vars(self):
+        paddle.enable_static()
         self.weight_quantize_type = 'channel_wise_lsq_weight'
         self.activation_quantize_type = 'lsq_act'
-        self.diff_threshold = 0.01
         self.onnx_format = False
         self.fuse_conv_bn = False
         print('weight_quantize_type', self.weight_quantize_type)
