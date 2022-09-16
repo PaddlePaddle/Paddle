84e2ee6040536fbd0d020852aa7bff7fb1c0b21b
Chen Weihang <chenweihang@baidu.com>
2022-09-16 13:33:11 +0800

    Unify core avx and core_noavx to libpaddle (#46095)
    * unify  core_avx and core_noavx
    
    * fix except error
    
    * revert mac compile logic
    
    * revert dylib to so
    
    * add core_noavx branch
    
    * remove core_noavx
    
    * replace paddle_core by lib paddle
    
    * polish var name
    
    * replace paddle_core by libpaddle
    
    * update custom device commit
    
    * polish code by comments

--- python/paddle/utils/cpp_extension/cpp_extension.py
+++ python/paddle/utils/cpp_extension/cpp_extension.py
@@ -440,7 +440,7 @@ class BuildExtension(build_ext, object):
 
                 # NOTE(Aurelius84): Since Paddle 2.0, we require gcc version > 5.x,
                 # so we add this flag to ensure the symbol names from user compiled
-                # shared library have same ABI suffix with core_(no)avx.so.
+                # shared library have same ABI suffix with libpaddle.so.
                 # See https://stackoverflow.com/questions/34571583/understanding-gcc-5s-glibcxx-use-cxx11-abi-or-the-new-abi
                 add_compile_flag(cflags, ['-D_GLIBCXX_USE_CXX11_ABI=1'])
                 # Append this macor only when jointly compiling .cc with .cu
