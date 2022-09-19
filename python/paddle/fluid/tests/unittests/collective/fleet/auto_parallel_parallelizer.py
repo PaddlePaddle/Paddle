12c174483df979a0ec5a30374440e31132dec272
Yulong Ao <aoyulong@baidu.com>
2022-09-19 14:56:34 +0800

    [Auto Parallel] Change the import way of Auto Parallel (#46115)

--- python/paddle/fluid/tests/unittests/collective/fleet/auto_parallel_parallelizer.py
+++ python/paddle/fluid/tests/unittests/collective/fleet/auto_parallel_parallelizer.py
@@ -23,7 +23,7 @@ import paddle.nn.functional as F
 import paddle.utils as utils
 from paddle.fluid import layers
 from paddle.distributed import fleet
-import paddle.distributed.auto_parallel as auto
+from paddle.distributed.fleet import auto
 from paddle.distributed.auto_parallel.utils import print_program_with_dist_attr
 import paddle.fluid.core as core
 
