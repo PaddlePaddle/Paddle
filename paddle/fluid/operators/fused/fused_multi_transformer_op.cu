39cd04c5fb7ba9d1b5f1f297f8c3eda0fca3ef99
carryyu <569782149@qq.com>
2022-09-19 12:46:06 +0000

    修改fused_multi_transformer_op.h文件名称，否则会出发CodeStyle检查：fused_multi_transformer_op.cc必须包含头文件fused_multi_transformer_op.h

--- paddle/fluid/operators/fused/fused_multi_transformer_op.cu
+++ paddle/fluid/operators/fused/fused_multi_transformer_op.cu
@@ -9,7 +9,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/operators/fused/fused_multi_transformer_op.h"
+#include "paddle/fluid/operators/fused/fused_multi_transformer.h"
 
 namespace paddle {
 namespace operators {
