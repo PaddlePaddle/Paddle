ae9cd15f6b9ce56a514023e613b342e416c10836
Wangzheee <634486483@qq.com>
2022-09-19 15:07:58 +0000

    support n lookup_tables fuse to embeddinglayernorm(2)

--- paddle/fluid/inference/tensorrt/plugin/many_emb_layernorm_varseqlen_plugin.cc
+++ paddle/fluid/inference/tensorrt/plugin/many_emb_layernorm_varseqlen_plugin.cc
@@ -1,5 +1,6 @@
 // Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-// SPDX-FileCopyrightText: Copyright (c) 1993-2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-FileCopyrightText: Copyright (c) 1993-2022 NVIDIA CORPORATION &
+// AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,6 +20,7 @@
 #include <vector>
 #include "NvInfer.h"
 #include "common/serialize.h"
+
 namespace paddle {
 namespace inference {
 namespace tensorrt {
--- paddle/fluid/inference/tensorrt/plugin/many_emb_layernorm_varseqlen_plugin.h
+++ paddle/fluid/inference/tensorrt/plugin/many_emb_layernorm_varseqlen_plugin.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-// SPDX-FileCopyrightText: Copyright (c) 1993-2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-FileCopyrightText: Copyright (c) 1993-2022 NVIDIA CORPORATION &
+// AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,10 +16,8 @@
 
 #pragma once
 #include <cuda.h>
-
 #include "NvInferPlugin.h"
 #include "NvInferRuntime.h"
-
 #include "common/bertCommon.h"
 #include "paddle/fluid/inference/tensorrt/plugin/trt_plugin.h"
 #include "paddle/fluid/platform/enforce.h"
