ed1b1bda04046add2cac1fd0f763b3d43100e8b3
niuliling123 <51102941+niuliling123@users.noreply.github.com>
2022-09-19 10:29:01 +0800

    Update layoutautotune for inplace (#45826)

--- paddle/fluid/imperative/tracer.cc
+++ paddle/fluid/imperative/tracer.cc
@@ -42,6 +42,8 @@ thread_local bool Tracer::enable_program_desc_tracing_ = false;
 
 thread_local bool Tracer::has_grad_ = true;
 
+thread_local bool Tracer::use_layout_autotune_ = false;
+
 thread_local AmpLevel Tracer::amp_level_ = AmpLevel::O0;
 
 thread_local phi::DataType Tracer::amp_dtype_ = phi::DataType::FLOAT32;
