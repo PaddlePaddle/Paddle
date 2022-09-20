1b4327ec6fe38774c5711b46cd564a834d973e18
wuhuachaocoding <huachaowu_ck@163.com>
2022-09-20 05:04:53 +0000

    update for py3.6

--- python/paddle/incubate/optimizer/distributed_fused_lamb.py
+++ python/paddle/incubate/optimizer/distributed_fused_lamb.py
@@ -19,7 +19,6 @@ from paddle.fluid.clip import ClipGradByGlobalNorm
 from paddle.fluid.initializer import Constant
 from paddle.fluid.layer_helper import LayerHelper
 from paddle.fluid.optimizer import Optimizer
-import paddle.distributed as dist
 from paddle.distributed.collective import new_group
 from paddle.fluid.executor import global_scope
 from paddle.fluid.framework import name_scope
@@ -288,8 +287,8 @@ class DistributedFusedLamb(Optimizer):
 
         step = self._get_or_create_step()
 
-        rank = dist.get_rank()
-        nranks = dist.get_world_size()
+        rank = paddle.distributed.get_rank()
+        nranks = paddle.distributed.get_world_size()
         if self._nproc_per_node is None:
             nproc_per_node = nranks
         else:
