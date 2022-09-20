78f8af66430881ab93f08db4a31aced4c2c69923
paulinagacek <paulina.gacek@intel.com>
2022-09-20 13:16:29 +0000

    refactoring

--- paddle/phi/kernels/onednn/transpose2_grad_kernel.cc
+++ paddle/phi/kernels/onednn/transpose2_grad_kernel.cc
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "paddle/phi/kernels/transpose_grad_kernel.h"
+
 #include "paddle/phi/backends/onednn/onednn_reuse.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/tensor_utils.h"
-#include "paddle/phi/kernels/transpose_grad_kernel.h"
-
 namespace phi {
 namespace funcs {
 
@@ -33,22 +33,22 @@ class TransposeOneDNNHandler {
         logical_axis_(dims.size(), 0),
         engine_(engine) {}
 
-  std::shared_ptr<dnnl::memory> AcquireSrcMemory(
-      const phi::funcs::OneDNNMemoryFormat& fmt, void* ptr) {
+  std::shared_ptr<dnnl::memory> AcquireSrcMemory(const OneDNNMemoryFormat& fmt,
+                                                 void* ptr) {
     // Make memory descriptor using input format, unless it
     // cannot be trusted (nchw) then make up memory fmt manually
     for (size_t i = 0; i < this->logical_axis_.size(); ++i) {
       this->logical_axis_[i] = i;
     }
 
-    auto src_md = fmt != phi::funcs::OneDNNMemoryFormat::nchw
+    auto src_md = fmt != OneDNNMemoryFormat::nchw
                       ? OneDNNMemDesc(dims_, oneDNNGetDataType<T>(), fmt)
                       : Axis2MemoryDesc(dims_, logical_axis_);
     return std::make_shared<dnnl::memory>(src_md, engine_, ptr);
   }
 
   std::shared_ptr<dnnl::memory> AcquireDstMemory(DenseTensor* output,
-                                                 phi::Place place) {
+                                                 Place place) {
     auto dst_md = Axis2MemoryDesc(dims_, axis_);
     output->Resize(make_ddim(dims_));
     auto dst_data = dev_ctx_.Alloc<T>(output);
@@ -74,8 +74,7 @@ class TransposeOneDNNHandler {
       strides[axis[i]] = total_stride;
       total_stride *= nchw_tz[axis[i]];
     }
-    dnnl::memory::desc mem_d(
-        nchw_tz, phi::funcs::oneDNNGetDataType<T>(), strides);
+    dnnl::memory::desc mem_d(nchw_tz, oneDNNGetDataType<T>(), strides);
 
     return mem_d;
   }
@@ -100,7 +99,7 @@ void TransposeGradKernel(const Context& dev_ctx,
   std::vector<int> reversed_axis(axis);
   int ndims = axis.size();
   if (ndims == 1) {
-    phi::Copy(dev_ctx, out_grad, out_grad.place(), false, x_grad);
+    Copy(dev_ctx, out_grad, out_grad.place(), false, x_grad);
     x_grad->set_format(out_grad.format());
     return;
   }
@@ -111,19 +110,19 @@ void TransposeGradKernel(const Context& dev_ctx,
 
   const T* out_grad_data = out_grad.data<T>();
   dev_ctx.template Alloc<T>(x_grad);
-  auto nchw_tz = phi::vectorize<int64_t>(out_grad.dims());
+  auto nchw_tz = vectorize<int64_t>(out_grad.dims());
 
-  phi::funcs::TransposeOneDNNHandler<T> handler(
+  funcs::TransposeOneDNNHandler<T> handler(
       dev_ctx, nchw_tz, reversed_axis, onednn_engine);
 
   auto transpose_src_memory_p = handler.AcquireSrcMemory(
-      out_grad.format(), phi::funcs::to_void_cast<T>(out_grad_data));
+      out_grad.format(), funcs::to_void_cast<T>(out_grad_data));
   auto transpose_dst_memory_p =
       handler.AcquireDstMemory(x_grad, dev_ctx.GetPlace());
   auto transpose_p =
       handler.AcquireTranspose(transpose_dst_memory_p, transpose_src_memory_p);
 
-  auto& astream = phi::OneDNNContext::tls().get_stream();
+  auto& astream = OneDNNContext::tls().get_stream();
   transpose_p->execute(
       astream, *transpose_src_memory_p, *transpose_dst_memory_p);
   astream.wait();
