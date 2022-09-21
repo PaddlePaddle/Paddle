b34bf6de068a252c0530a509d3b8585be17b3057
Piotr Paturej <piotr.paturej@intel.com>
2022-09-21 15:32:24 +0200

    Refactor licences

--- paddle/phi/backends/onednn/axpy_handler.cc
+++ paddle/phi/backends/onednn/axpy_handler.cc
@@ -1,16 +1,16 @@
-/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. */
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "paddle/phi/backends/onednn/axpy_handler.h"
 
@@ -127,7 +127,7 @@ void OneDNNAXPYHandler<T>::operator()(const T *x, T *y) {
 }
 
 template class OneDNNAXPYHandler<float>;
-template class OneDNNAXPYHandler<phi::dtype::bfloat16>;
+template class OneDNNAXPYHandler<dtype::bfloat16>;
 
 }  // namespace funcs
 }  // namespace phi
