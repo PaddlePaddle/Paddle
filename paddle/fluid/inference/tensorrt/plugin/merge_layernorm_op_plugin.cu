e3b08a3f7b540befb8ad713882aed6770ebabc2d
wwbitejotunn <wang_bojun@outlook.com>
2022-09-22 10:55:27 +0000

    add UT

--- paddle/fluid/inference/tensorrt/plugin/merge_layernorm_op_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/merge_layernorm_op_plugin.cu
@@ -1,4 +1,5 @@
 /* Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+   Copyright (c) 2019-2022, NVIDIA CORPORATION.  All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -11,8 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-
-// TODO wangbojun nv license
 
 #include <algorithm>
 
