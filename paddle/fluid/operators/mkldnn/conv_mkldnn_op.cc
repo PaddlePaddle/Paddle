fae473ee92db5289881fb2a2d371dce62b34f29a
Jacek Czaja <jacek.czaja@intel.com>
2022-09-23 17:09:45 +0200

    - compilation fix

--- paddle/fluid/operators/mkldnn/conv_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/conv_mkldnn_op.cc
@@ -1092,8 +1092,7 @@ class ConvMKLDNNGradOpKernel : public framework::OpKernel<T> {
         dnnl::memory::format_tag target_format =
             weights_tz.size() == 6 ? dnnl::memory::format_tag::oidhw
                                    : dnnl::memory::format_tag::oihw;
-        filter_grad->set_mem_desc(dnnl::memory::desc out_mem_desc(
-            phi::vectorize<int64_t>(in.dims()), in_type, target_format));
+        filter_grad->set_mem_desc(dnnl::memory::desc(phi::vectorize<int64_t>(filter_grad->dims()), in_type, target_format));
       } else {
         filter_grad->set_mem_desc(diff_weights_memory_p->get_desc());
       }
