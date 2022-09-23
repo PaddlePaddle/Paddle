2c7cd39c1fbf1f9ca6b02863777e96e1bc3eb75a
SigureMo <sigure.qaq@gmail.com>
2022-09-24 00:46:12 +0800

    test future import, test=document_fix

--- python/paddle/check_import_scipy.py
+++ python/paddle/check_import_scipy.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
+from __future__ import annotations
+
 
 def check_import_scipy(OsName):
     print_info = ""
