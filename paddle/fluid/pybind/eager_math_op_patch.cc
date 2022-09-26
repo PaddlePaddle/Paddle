34ffdd12d077205df17c0f5aded5b5e67934bbac
Weilong Wu <veyron_wu@163.com>
2022-09-26 04:08:16 +0000

    polish code

--- paddle/fluid/pybind/eager_math_op_patch.cc
+++ paddle/fluid/pybind/eager_math_op_patch.cc
@@ -461,6 +461,7 @@ static PyObject* tensor__mul__method(TensorObject* self,
       other = CastPyArg2AttrFloat(other_obj, 0);
       if (_supported_int_dtype_.find(self_tensor.dtype()) !=
           _supported_int_dtype_.end()) {
+        eager_gil_scoped_release guard;
         self_tensor = cast_ad_func(self_tensor, DataType::FLOAT32);
       }
     } else if (PyCheckInteger(other_obj) || IsNumpyType(other_obj)) {
@@ -476,7 +477,8 @@ static PyObject* tensor__mul__method(TensorObject* self,
   // 2. create or get tensor for other_obj
   paddle::experimental::Tensor other_tensor;
   if (!PyCheckTensor(other_obj)) {
-    paddle::experimental::Scalar value = CastPyArg2Scalar(other_obj, "full", 0);
+    paddle::experimental::Scalar value =
+        CastPyArg2Scalar(other_obj, "__mul__", 0);
     if (PyComplex_Check(other_obj)) {
       eager_gil_scoped_release guard;
       other_tensor = full_ad_func({1}, value, DataType::COMPLEX64, place);
@@ -503,9 +505,11 @@ static PyObject* tensor__mul__method(TensorObject* self,
               framework::TransToProtoVarType(rhs_dtype)));
       if (lhs_dtype != promote_dtype) {
         // cast
+        eager_gil_scoped_release guard;
         self_tensor = cast_ad_func(self_tensor, promote_dtype);
       }
       if (rhs_dtype != promote_dtype) {
+        eager_gil_scoped_release guard;
         other_tensor = cast_ad_func(other_tensor, promote_dtype);
       }
     } else {
@@ -514,6 +518,7 @@ static PyObject* tensor__mul__method(TensorObject* self,
              "dtype is "
           << lhs_dtype << ", but right dtype is " << rhs_dtype
           << ", the right dtype will convert to " << lhs_dtype;
+      eager_gil_scoped_release guard;
       other_tensor = cast_ad_func(other_tensor, lhs_dtype);
     }
   }
