1aab0c47a278e84582c701ec9771ecda6ab68f00
kangguangli <kangguangli@hotmail.com>
2022-09-26 19:29:11 +0800

    fix typo

--- paddle/fluid/framework/new_executor/interpreter/execution_config.h
+++ paddle/fluid/framework/new_executor/interpreter/execution_config.h
@@ -26,7 +26,7 @@ namespace interpreter {
 struct ExecutionConfig {
   bool used_for_jit{false};
   bool create_local_scope{true};
-  bool used_for_control_flow_op_{false};
+  bool used_for_control_flow_op{false};
 
   size_t host_num_threads;
   size_t deivce_num_threads;
