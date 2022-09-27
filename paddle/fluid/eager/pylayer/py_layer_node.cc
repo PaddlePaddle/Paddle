7ff13881509ee3188056b722ef64866f4fb37d3d
sneaxiy <sneaxiy@126.com>
2022-09-27 16:44:04 +0800

    update PR to pass CI

--- paddle/fluid/eager/pylayer/py_layer_node.cc
+++ paddle/fluid/eager/pylayer/py_layer_node.cc
@@ -155,11 +155,7 @@ GradNodePyLayer::operator()(
         if (ctx->forward_input_tensor_is_duplicable[i]) {
           grad_out.push_back(paddle::pybind::GetTensorListFromPyObject(obj));
         } else {
-          if (obj && obj != Py_None) {
-            grad_out.push_back({paddle::pybind::GetTensorFromPyObject(obj)});
-          } else {
-            grad_out.push_back({});
-          }
+          grad_out.push_back({paddle::pybind::GetTensorFromPyObject(obj)});
         }
       }
     } else {
--- paddle/phi/kernels/gpu/where_kernel.cu
+++ paddle/phi/kernels/gpu/where_kernel.cu
@@ -53,4 +53,5 @@ PD_REGISTER_KERNEL(where,
                    double,
                    int,
                    int64_t,
+                   phi::dtype::float16,
                    phi::dtype::bfloat16) {}
