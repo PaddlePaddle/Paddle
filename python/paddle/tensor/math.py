1e93c65225922dca5762ad660b5fde1664193437
thunder95 <290844930@qq.com>
2022-09-27 05:54:42 +0000

    fix code style

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -3196,7 +3196,8 @@ def logcumsumexp(x, axis=None, dtype=None, name=None):
         else:
             return _legacy_C_ops.logcumsumexp(x, 'axis', axis, 'flatten', flatten)
 
-    check_variable_and_dtype(x, 'x', ['float16', 'float32', 'float64'], "logcumsumexp")
+    check_variable_and_dtype(x, 'x', ['float16', 'float32', 'float64'],
+    "logcumsumexp")
 
     helper = LayerHelper('logcumsumexp', **locals())
     out = helper.create_variable_for_type_inference(x.dtype)
