4069f2d0c94463c27bbec277ce748bbfc0f50c32
risemeup1 <515586620@qq.com>
2022-09-28 14:51:20 +0000

    it is a test,test=ljdci

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -1859,7 +1859,14 @@ function precise_card_test_single {
         fi
         set -x
         find paddle/fluid -name '*.gcda'|xargs -I {} cp --path {} ut_map/$case
+        find paddle/phi -name '*.gcda'|xargs -I {} cp --path {} ut_map/$case
+        find paddle/utils -name '*.gcda'|xargs -I {} cp --path {} ut_map/$case
+
         find paddle/fluid -name '*.gcno'|xargs -I {} cp --path {} ut_map/$case
+        find paddle/phi -name '*.gcno'|xargs -I {} cp --path {} ut_map/$case
+        find paddle/utils -name '*.gcno'|xargs -I {} cp --path {} ut_map/$case
+        
+
         python ${PADDLE_ROOT}/tools/get_single_test_cov.py ${PADDLE_ROOT} $case &
         
         # python
@@ -1872,6 +1879,9 @@ function precise_card_test_single {
             mv python-coverage.data.* ${PADDLE_ROOT}/build/pytest/$case
         fi
         find paddle/fluid -name *.gcda | xargs rm -f #delete gcda
+        find paddle/phi -name *.gcda | xargs rm -f #delete gcda
+        find paddle/utils -name *.gcda | xargs rm -f #delete gcda
+
     done
 }
 
@@ -1929,6 +1939,7 @@ function get_precise_tests_map_file {
     ut_total_startTime_s=`date +%s`
     EXIT_CODE=0;
     test_cases=$(ctest -N -V) # get all test cases
+    ctest -N -V | grep "test_pass_"
     single_card_tests='' # all cases list which would take one graph card
     exclusive_tests=''        # cases list which would be run exclusively
     multiple_card_tests=''    # cases list which would take multiple GPUs, most cases would be two GPUs
@@ -1997,10 +2008,12 @@ set -x
     mkdir -p ${PADDLE_ROOT}/build/ut_map
     mkdir -p ${PADDLE_ROOT}/build/pytest
 
-    precise_card_test_single "$single_card_tests" 1
-    precise_card_test_single "$single_card_tests_1" 1
-    precise_card_test_single "$multiple_card_tests" 2
-    precise_card_test_single "$exclusive_tests"
+    #precise_card_test_single "$single_card_tests" 1
+    #precise_card_test_single "$single_card_tests_1" 1
+    #precise_card_test_single "$multiple_card_tests" 2
+    #precise_card_test_single "$exclusive_tests"
+    ljd_testcases='^test_op_signature$|^variant_test$'
+    precise_card_test_single "$ljd_testcases" 1
     wait;
     python ${PADDLE_ROOT}/tools/get_ut_file_map.py 'get_not_success_ut' ${PADDLE_ROOT}
 
--- tools/get_ut_file_map.py
+++ tools/get_ut_file_map.py
@@ -129,15 +129,6 @@ def notsuccessfuc(rootPath):
             utNotSuccess = utNotSuccess + '^%s$|' % ut
 
     # ut not exec
-    get_all_uts(rootPath)
-    with open("/paddle/build/all_uts_paddle", "r") as f:
-        data = f.readlines()
-    for ut in data:
-        ut = ut.replace('\n', '').strip()
-        if ut not in files:
-            print(ut)
-            count = count + 1
-            utNotSuccess = utNotSuccess + '^%s$|' % ut
 
     if utNotSuccess != '':
         print("utNotSuccess count: %s" % count)
@@ -148,35 +139,21 @@ def notsuccessfuc(rootPath):
 
 def ut_file_map_supplement(rootPath):
     ut_file_map_new = "%s/build/ut_file_map.json" % rootPath
-    os.system('mkdir /pre_test')
+    os.system('mkdir /pre_test_tmp')
     os.system(
-        'cd /pre_test && wget --no-proxy https://paddle-docker-tar.bj.bcebos.com/pre_test/ut_file_map.json --no-check-certificate'
+        'cd /pre_test_tmp && wget --no-proxy https://paddle-docker-tar.bj.bcebos.com/pre_test/ut_file_map.json --no-check-certificate'
     )
-    ut_file_map_old = "/pre_test/ut_file_map.json"
+    ut_file_map_old = "/pre_test_tmp/ut_file_map.json"
     with open(ut_file_map_new, 'r') as load_f:
         load_dict_new = json.load(load_f)
-    with open(ut_file_map_old, 'r') as f:
-        load_dict_old = json.load(f)
-
-    all_uts_paddle = '%s/build/all_uts_paddle' % rootPath
-    with open(all_uts_paddle, 'r') as f:
-        all_uts_paddle_list = []
-        for ut in f.readlines():
-            all_uts_paddle_list.append(ut.strip())
-        f.close()
-
-    for filename in load_dict_old:
-        if filename not in load_dict_new:
-            load_dict_new[filename] = load_dict_old[filename]
-
-    with open("/pre_test/ut_file_map.json", "w") as f:
+    with open("/pre_test_tmp/ut_file_map.json", "w") as f:
         json.dump(load_dict_new, f, indent=4)
         print("load_dict_new success!!")
 
     os.system(
-        'cd /pre_test && wget --no-proxy https://paddle-docker-tar.bj.bcebos.com/pre_test/prec_delta --no-check-certificate'
+        'cd /pre_test_tmp && wget --no-proxy https://paddle-docker-tar.bj.bcebos.com/pre_test/prec_delta --no-check-certificate'
     )
-    prec_delta_old = '/pre_test/prec_delta'
+    prec_delta_old = '/pre_test_tmp/prec_delta'
     prec_delta_new = "%s/build/prec_delta" % rootPath
     with open(prec_delta_old, 'r') as f:
         prec_delta_old_list = []
@@ -188,14 +165,9 @@ def ut_file_map_supplement(rootPath):
         for ut in f.readlines():
             prec_delta_new_list.append(ut.strip())
         f.close()
-    for ut in prec_delta_old_list:
-        filename = '%s/build/ut_map/%s/coverage.info.tmp' % (rootPath, ut)
-        if ut in all_uts_paddle_list:
-            if not os.path.exists(filename) and ut not in prec_delta_new_list:
-                prec_delta_new_list.append(ut)
     prec_delta_new_list.append(
         'test_py_reader_error_msg')  #add a python case for pycoverage
-    prec_delta_file = open("/pre_test/prec_delta", 'w')
+    prec_delta_file = open("/pre_test_tmp/prec_delta", 'w')
     for ut in prec_delta_new_list:
         prec_delta_file.write(ut + '\n')
     print("prec_delta_file success!!")
