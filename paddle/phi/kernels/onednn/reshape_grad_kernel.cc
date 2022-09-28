d970a89af1f0defefca7f2bbf4e197bce153c021
Sylwester Fraczek <sylwester.fraczek@intel.com>
2022-09-28 15:47:17 +0200

    fix build errors

--- paddle/phi/kernels/onednn/reshape_grad_kernel.cc
+++ paddle/phi/kernels/onednn/reshape_grad_kernel.cc
@@ -14,6 +14,7 @@
 
 #include "paddle/phi/kernels/reshape_grad_kernel.h"
 
+#include "paddle/phi/backends/onednn/onednn_reuse.h"
 #include "paddle/phi/core/kernel_registry.h"
 
 namespace phi {
@@ -24,16 +25,16 @@ void ReshapeGradKernel(const Context& dev_ctx,
                        DenseTensor* x_grad) {
   phi::DDim x_dims = x_grad->dims();
 
-  auto out_vec_dims = phi::vectorize(out_grad->dims());
+  auto out_vec_dims = phi::vectorize(out_grad.dims());
 
-  platform::ReorderMKLDNNHandler reorder_handler(
+  funcs::ReorderOneDNNHandler reorder_handler(
       out_vec_dims,
       out_grad.dtype(),
       funcs::ToOneDNNDataType(out_grad.dtype()),
       dev_ctx.GetEngine());
 
   auto reorder_src_memory_p = reorder_handler.AcquireSrcMemory(
-      out_grad->mem_desc(), funcs::to_void_cast(out_grad.data()));
+      out_grad.mem_desc(), funcs::to_void_cast(out_grad.data()));
   auto reorder_dst_memory_p = reorder_handler.AcquireDstMemory(
       x_grad,
       funcs::GetPlainOneDNNFormat(out_grad.dims().size()),
