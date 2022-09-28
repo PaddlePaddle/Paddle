3c6afe903e1b3f19572ba28ee33356e4cab90e40
SigureMo <sigure.qaq@gmail.com>
2022-09-28 21:56:48 +0800

    [CodeStyle][F401] remove unused import in unittests/collective

--- python/paddle/fluid/tests/unittests/collective/process_group_mpi.py
+++ python/paddle/fluid/tests/unittests/collective/process_group_mpi.py
@@ -15,17 +15,12 @@
 import unittest
 import random
 import numpy as np
-import os
-import shutil
 
 import paddle
 from paddle.fluid import core
-from datetime import timedelta
 import paddle.fluid.core as core
 from paddle.fluid.framework import _test_eager_guard
-from paddle.fluid.dygraph.parallel import ParallelEnv
 from paddle.distributed.collective import Group
-from paddle.distributed.collective import _group_map_by_name
 from paddle.distributed.collective import _default_group_name
 from paddle.distributed.collective import _set_group_map
 from paddle.distributed.collective import _set_group_map_by_name
