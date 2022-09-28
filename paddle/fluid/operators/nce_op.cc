b2e4211d5bb0c215ffd6ffa9e4454ed9265365c6
zyfncg <zhangyunfei07@baidu.com>
2022-09-28 14:42:07 +0800

    [cherry-pick] Clear extra attrs of some ops in OpMaker (#46150, #46321, #46418, #46451, #46457) (#46553)
    * Clear extra attributes of some Op in OpMaker (Part4) (#46060)
    
    * clear extra attr of some ops in opmaker
    
    * revert clear use_cudnn for pool
    
    * fix test_operator_desc
    
    * fix Attr interface of OperatorBase
    
    * clear extra attrs of condition op in opmaker (#46150)
    
    * Clear extra attrs of lookup_table_v2 in OpMaker (#46321)
    
    * clear extra attrs of look_up_table_v2 in opmaker
    
    * fix bug
    
    * clear extra attrs of quantize op in opmaker (#46418)
    
    * delete repeated item
    
    * clear extra attrs of distribute op in opmaker (#46451)
    
    * clear extra atts of sequence_softmax in opmaker (#46457)

--- paddle/fluid/operators/nce_op.cc
+++ paddle/fluid/operators/nce_op.cc
@@ -207,34 +207,6 @@ class NCEOpMaker : public framework::OpProtoAndCheckerMaker {
 
     // for parameter prefetch
     AddAttr<bool>("remote_prefetch", "").SetDefault(false);
-    AddAttr<int>("trainer_id", "trainer id from 0 ~ worker_num.")
-        .SetDefault(0)
-        .AsExtra();
-    AddAttr<std::vector<int64_t>>("height_sections",
-                                  "Height for each output SelectedRows.")
-        .SetDefault(std::vector<int64_t>({}))
-        .AsExtra();
-    AddAttr<std::vector<std::string>>(
-        "epmap",
-        "(string vector, default 127.0.0.1:6164)"
-        "Server endpoints in the order of input variables for mapping")
-        .SetDefault({})
-        .AsExtra();
-    AddAttr<std::vector<std::string>>(
-        "table_names",
-        "(string vector, the split table names that will be fetched from "
-        "parameter server)"
-        "in the order of input variables for mapping")
-        .SetDefault({})
-        .AsExtra();
-
-    AddAttr<std::vector<int>>("custom_neg_classes",
-                              "This attribute only be used in unitest. Classes "
-                              "in this list wiil be used as negative classes "
-                              "for every samples. Under normal conditions, "
-                              "user should avoid setting this attribute.")
-        .SetDefault({})
-        .AsExtra();
     AddAttr<bool>("is_test",
                   "(bool, default false) Set to true for inference "
                   "only, false for training.")
