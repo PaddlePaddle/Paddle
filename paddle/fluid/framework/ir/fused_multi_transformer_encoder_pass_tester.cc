fbe72d71b8e58da054a0425e2f94773e1597ef0f
dengkaipeng <dengkaipeng@baidu.com>
2022-09-28 15:56:43 +0000

    fix unittest. test=develop

--- paddle/fluid/framework/ir/fused_multi_transformer_encoder_pass_tester.cc
+++ paddle/fluid/framework/ir/fused_multi_transformer_encoder_pass_tester.cc
@@ -210,12 +210,12 @@ TEST(FusedMultiTransformerEncoderPass, basic) {
   int num_fused_nodes_after = GetNumOpNodes(graph, "fused_multi_transformer");
 
   PADDLE_ENFORCE_EQ(num_nodes_before,
-                    num_nodes_after + 69,
+                    num_nodes_after + 68,
                     platform::errors::InvalidArgument(
                         "After the fused_multi_transformer_encoder_pass, The "
                         "node num in graph "
                         "should be %d, but the result is %d",
-                        num_nodes_before - 69,
+                        num_nodes_before - 68,
                         num_nodes_after));
   PADDLE_ENFORCE_EQ(num_fused_nodes_after,
                     1,
@@ -366,11 +366,11 @@ TEST(FusedMultiTransformerEncoderFuseQKVPass, basic) {
 
   PADDLE_ENFORCE_EQ(
       num_nodes_before,
-      num_nodes_after + 57,
+      num_nodes_after + 56,
       platform::errors::InvalidArgument(
           "After the fused_multi_transformer_encoder_fuse_qkv_pass, "
           "The node num in graph should be %d, but the result is %d",
-          num_nodes_before - 57,
+          num_nodes_before - 56,
           num_nodes_after));
   PADDLE_ENFORCE_EQ(num_fused_nodes_after,
                     1,
