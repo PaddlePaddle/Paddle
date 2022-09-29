0e712f7c48d9dae3fccc6db4c72796b0a176d07a
Yulong Ao <aoyulong@baidu.com>
2022-09-29 03:58:25 +0000

    [Auto Parallel] Unify the logger and outputs of Engine API

--- python/paddle/distributed/passes/auto_parallel_amp.py
+++ python/paddle/distributed/passes/auto_parallel_amp.py
@@ -517,6 +517,7 @@ class AMPPass(PassBase):
         self.set_attr("use_dynamic_loss_scaling", False)
         self.set_attr("input_data", [])
         self.set_attr("params_grads", [])
+        self._loss = None
         self._loss_scaling = None
         self._num_good_steps = None
         self._num_bad_steps = None
