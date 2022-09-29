7453c334e9d31a6aaedaad71fe8015d00d950393
jakpiase <jakpia21@gmail.com>
2022-09-29 18:37:38 +0200

    fix for transpose

--- paddle/fluid/operators/mkldnn/transpose_mkldnn_op.cc
+++ paddle/fluid/operators/mkldnn/transpose_mkldnn_op.cc
@@ -34,7 +34,7 @@ class TransposeMKLDNNOpKernel : public paddle::framework::OpKernel<T> {
                           "Operator DNNL Transpose must use CPUPlace"));
     auto& dev_ctx =
         ctx.template device_context<paddle::platform::MKLDNNDeviceContext>();
-    const auto& mkldnn_engine = dev_ctx.GetEngine();
+    const auto& dnnl_engine = dev_ctx.GetEngine();
     std::vector<int> transpose_axis = ctx.Attr<std::vector<int>>("axis");
     int ndims = transpose_axis.size();
     auto* x = ctx.Input<Tensor>("X");
@@ -50,28 +50,30 @@ class TransposeMKLDNNOpKernel : public paddle::framework::OpKernel<T> {
 
     auto x_vec_dims = phi::vectorize(x->dims());
 
-    mkldnn::memory::data_type x_type = framework::ToMKLDNNDataType(x->type());
+    framework::proto::VarType::Type x_paddle_type =
+        framework::TransToProtoVarType(x->dtype());
+    dnnl::memory::data_type x_type = framework::ToMKLDNNDataType(x_paddle_type);
     platform::ReorderMKLDNNHandler reorder_handler(
-        x_vec_dims, x->type(), x_type, mkldnn_engine);
+        x_vec_dims, x_paddle_type, x_type, dnnl_engine);
 
     auto reorder_src_memory_p = reorder_handler.AcquireSrcMemory(
         x->mem_desc(), platform::to_void_cast(x->data<T>()));
 
     auto dst_md =
-        mkldnn::memory::desc(x_vec_dims,
-                             x->mem_desc().data_type(),
-                             platform::GetPlainMKLDNNFormat(x_vec_dims.size()));
+        dnnl::memory::desc(x_vec_dims,
+                           x->mem_desc().data_type(),
+                           platform::GetPlainMKLDNNFormat(x_vec_dims.size()));
     // a trick is used here to fake transpose of out_md, so later it will be
     // "untransposed", leaving output data in plain format tag
     auto dst_strides = FakeTranposeStrides(dst_md, transpose_axis);
 
-    dst_md = mkldnn::memory::desc(
-        x_vec_dims, x->mem_desc().data_type(), dst_strides);
+    dst_md =
+        dnnl::memory::desc(x_vec_dims, x->mem_desc().data_type(), dst_strides);
     auto dst_data =
         out->mutable_data(ctx.GetPlace(), x->type(), dst_md.get_size());
 
     auto reorder_dst_memory_p =
-        std::make_shared<mkldnn::memory>(dst_md, mkldnn_engine, dst_data);
+        std::make_shared<dnnl::memory>(dst_md, dnnl_engine, dst_data);
 
     auto reorder_p = reorder_handler.AcquireReorder(reorder_dst_memory_p,
                                                     reorder_src_memory_p);
@@ -86,7 +88,7 @@ class TransposeMKLDNNOpKernel : public paddle::framework::OpKernel<T> {
 
  private:
   // it is needed because oneDNN's permute axis understand axes order in
-  // different way than matmul's one
+  // different way PaddlePaddle's transpose
   std::vector<int> TransposeToPermuteAxis(
       const std::vector<int>& transpose_axis) const {
     std::vector<int> permute_axis(transpose_axis.size());
@@ -98,7 +100,7 @@ class TransposeMKLDNNOpKernel : public paddle::framework::OpKernel<T> {
   }
 
   std::vector<int64_t> FakeTranposeStrides(
-      const mkldnn::memory::desc& dst_md,
+      const dnnl::memory::desc& dst_md,
       const std::vector<int>& transpose_axis) const {
     std::vector<int64_t> fake_strides(transpose_axis.size());
     auto dims = dst_md.dims();
@@ -128,7 +130,7 @@ class TransposeMKLDNNGradOpKernel : public paddle::framework::OpKernel<T> {
     if (!dx) return;
     auto& dev_ctx =
         ctx.template device_context<paddle::platform::MKLDNNDeviceContext>();
-    const auto& mkldnn_engine = dev_ctx.GetEngine();
+    const auto& dnnl_engine = dev_ctx.GetEngine();
     std::vector<int> transpose_axis = ctx.Attr<std::vector<int>>("axis");
 
     auto& astream = platform::MKLDNNDeviceContext::tls().get_stream();
@@ -142,10 +144,13 @@ class TransposeMKLDNNGradOpKernel : public paddle::framework::OpKernel<T> {
 
     auto dout_vec_dims = phi::vectorize(dout->dims());
 
-    mkldnn::memory::data_type dout_type =
-        framework::ToMKLDNNDataType(dout->type());
+    framework::proto::VarType::Type dout_paddle_type =
+        framework::TransToProtoVarType(dout->dtype());
+    dnnl::memory::data_type dout_type =
+        framework::ToMKLDNNDataType(dout_paddle_type);
+
     platform::ReorderMKLDNNHandler reorder_handler(
-        dout_vec_dims, dout->type(), dout_type, mkldnn_engine);
+        dout_vec_dims, dout_paddle_type, dout_type, dnnl_engine);
 
     auto reorder_src_memory_p = reorder_handler.AcquireSrcMemory(
         dout->mem_desc(), platform::to_void_cast(dout->data<T>()));
@@ -153,8 +158,8 @@ class TransposeMKLDNNGradOpKernel : public paddle::framework::OpKernel<T> {
     auto reorder_dst_memory_p =
         reorder_handler.AcquireDstMemory(dx, dout->mem_desc(), ctx.GetPlace());
 
-    auto reorder_p = reorder_handler.AcquireReorder(reorder_src_memory_p,
-                                                    reorder_dst_memory_p);
+    auto reorder_p = reorder_handler.AcquireReorder(reorder_dst_memory_p,
+                                                    reorder_src_memory_p);
 
     reorder_p->execute(astream, *reorder_src_memory_p, *reorder_dst_memory_p);
     astream.wait();
