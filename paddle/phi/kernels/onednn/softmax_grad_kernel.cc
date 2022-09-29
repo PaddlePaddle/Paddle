b3aaf8660a3b9992cca0b2aa79be84546076e874
Silv3S <slawomir.siwek@intel.com>
2022-09-29 19:45:13 +0200

    remove redundant imports

--- paddle/phi/kernels/onednn/softmax_grad_kernel.cc
+++ paddle/phi/kernels/onednn/softmax_grad_kernel.cc
@@ -14,10 +14,7 @@
 
 #include "paddle/phi/kernels/softmax_grad_kernel.h"
 
-#include "paddle/phi/backends/onednn/onednn_context.h"
 #include "paddle/phi/backends/onednn/onednn_reuse.h"
-#include "paddle/phi/common/bfloat16.h"
-#include "paddle/phi/common/place.h"
 #include "paddle/phi/core/kernel_registry.h"
 
 namespace phi {
