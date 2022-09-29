a152f6c44c8f14b53b6302f2abe5b953d14d12de
tiancaitzp <tiancaitzp@163.com>
2022-09-29 07:55:35 +0000

    optimzie fusion_seqpool_cvm_concat_op.

--- paddle/fluid/operators/fused/fusion_seqpool_cvm_concat_op_xpu.cc
+++ paddle/fluid/operators/fused/fusion_seqpool_cvm_concat_op_xpu.cc
@@ -12,61 +12,62 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. */
 
-#include "paddle/fluid/operators/fused/fusion_seqpool_cvm_concat_op.h"
 #include <string>
 #include <vector>
 #include "paddle/fluid/operators/jit/kernels.h"
 #include "paddle/fluid/platform/device/xpu/xpu_info.h"
+#include "paddle/fluid/operators/fused/fusion_seqpool_cvm_concat_op.h"
 
 namespace paddle {
 namespace operators {
 template <typename DeviceContext, typename T>
 class FusionSeqPoolCVMConcatGradXPUKernel : public framework::OpKernel<T> {
- public:
-    void Compute(const framework::ExecutionContext& ctx) const override {
+public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
     const auto* dOut =
           ctx.Input<framework::LoDTensor>(framework::GradVarName("Out"));
-   const Tensor* cvm = ctx.Input<Tensor>("CVM");
-   auto dxs = ctx.MultiOutput<framework::LoDTensor>(framework::GradVarName("X"));
-   auto use_cvm = ctx.Attr<bool>("use_cvm");//TODO:
-   size_t n = dxs.size();
-   auto xpu_context =
-        ctx.template device_context<DeviceContext>().x_context();
-   auto cvm_offset = use_cvm ? 0 : 2;
-   for (size_t k = 0; k < n; k++) {
-       auto dx = dxs[k];
-       T* dx_data = dx->mutable_data<T>(ctx.GetPlace()); 
-       int tmp_cvm_dx_bs = dOut->dims()[0];
-       auto lod = dx->lod();
-       auto batch_size = dx->dims()[0];
-       dx->mutable_data<T>(ctx.GetPlace());
-       const T* cvm_data = cvm->data<T>();
-       auto lod_level_0 = dx->lod()[0];
-       auto item_size = dx->numel() / batch_size;
-       const T* dout_data = dOut->data<T>() + k * (item_size-cvm_offset);
-       int lod_size = lod_level_0.size();
-       std::vector<unsigned int> cpu_lodx(lod_size);
-       for (int i = 0; i < lod_size; i++) {
-          cpu_lodx[i] = lod_level_0[i];
-       }
-       const xpu::VectorParam<unsigned int> lodx = {reinterpret_cast<const unsigned int*>(cpu_lodx.data()),
-                  static_cast<int>(cpu_lodx.size()), nullptr};
-       xpu::ctx_guard RAII_GUARD(xpu_context);
-       auto dy_offset = dOut->dims()[1];
-       // use_cvm 0 item_size 11batch_size 4096*2  tmp_cvm_dx_bs 4096dout_offset 765
-       auto lodx_xpu = lodx.to_xpu(RAII_GUARD);
-       int r = xpu::sequence_sum_pool_cvm_grad<float, unsigned int>(xpu_context, dout_data, dx_data, cvm_data, lodx_xpu.xpu, item_size, tmp_cvm_dx_bs, dy_offset, use_cvm);
-       PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
-            platform::errors::External(
-               "The sequence_pool_cvm_grad XPU OP return wrong value[%d %s]",
-               r, XPUAPIErrorMsg[r]));
-   }
- }
+    const Tensor* cvm = ctx.Input<Tensor>("CVM");
+    auto dxs = ctx.MultiOutput<framework::LoDTensor>(framework::GradVarName("X"));
+    auto use_cvm = ctx.Attr<bool>("use_cvm");//TODO:
+    size_t n = dxs.size();
+    auto xpu_context =
+          ctx.template device_context<DeviceContext>().x_context();
+    auto cvm_offset = use_cvm ? 0 : 2;
+
+    for (size_t k = 0; k < n; k++) {
+        auto dx = dxs[k];
+        T* dx_data = dx->mutable_data<T>(ctx.GetPlace());
+        int tmp_cvm_dx_bs = dOut->dims()[0];
+        auto lod = dx->lod();
+        auto batch_size = dx->dims()[0];
+        dx->mutable_data<T>(ctx.GetPlace());
+        const T* cvm_data = cvm->data<T>();
+        auto lod_level_0 = dx->lod()[0];
+        auto item_size = dx->numel() / batch_size;
+        const T* dout_data = dOut->data<T>() + k * (item_size-cvm_offset);
+        int lod_size = lod_level_0.size();
+        std::vector<unsigned int> cpu_lodx(lod_size);
+        for (int i = 0; i < lod_size; i++) {
+            cpu_lodx[i] = lod_level_0[i];
+        }
+        const xpu::VectorParam<unsigned int> lodx = {reinterpret_cast<const unsigned int*>(cpu_lodx.data()),
+                    static_cast<int>(cpu_lodx.size()), nullptr};
+        xpu::ctx_guard RAII_GUARD(xpu_context);
+        auto dy_offset = dOut->dims()[1];
+        // use_cvm 0 item_size 11batch_size 4096*2  tmp_cvm_dx_bs 4096dout_offset 765
+        auto lodx_xpu = lodx.to_xpu(RAII_GUARD);
+        int r = xpu::sequence_sum_pool_cvm_grad<float, unsigned int>(xpu_context, dout_data, dx_data, cvm_data, lodx_xpu.xpu, item_size, tmp_cvm_dx_bs, dy_offset, use_cvm);
+        PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
+              platform::errors::External(
+                "The sequence_pool_cvm_grad XPU OP return wrong value[%d %s]",
+                r, XPUAPIErrorMsg[r]));
+    }
+  }
 };
 
 template <typename DeviceContext, typename T>
 class FusionSeqPoolCVMConcatXPUKernel : public framework::OpKernel<T> {
- public:
+public:
   void Compute(const framework::ExecutionContext& ctx) const override {
     auto ins = ctx.MultiInput<LoDTensor>("X");
     auto* out = ctx.Output<LoDTensor>("Out");
@@ -98,31 +99,45 @@ class FusionSeqPoolCVMConcatXPUKernel : public framework::OpKernel<T> {
                   paddle::platform::errors::InvalidArgument(
                       "The output of dims[1] should be dividable of (w-2)"));
     }
-    size_t n = ins.size();
-    for (size_t i = 0; i < n; ++i) {
-        auto x_dims = ins[i]->dims();
+
+    xpu::ctx_guard RAII_GUARD(xpu_context);
+
+    int slot_num = static_cast<int>(ins.size());
+
+    xpu::VectorParam<uint64_t> x_addr_VP_list = {nullptr, slot_num, nullptr};
+    std::vector<uint64_t> xpu_x_addr_vec(slot_num, 0);
+
+    xpu::VectorParam<uint64_t> lod_VP_list = {nullptr, slot_num, nullptr};
+    std::vector<uint64_t> xpu_lod_addr_vec(slot_num, 0);
+
+    for (int i = 0; i < slot_num; i++) {
+        xpu_x_addr_vec[i] = reinterpret_cast<uint64_t>(ins[i]->data<T>());
+
         auto x_lod = ins[i]->lod()[0];
-        const T* src = ins[i]->data<T>();
-        T* dst = y_data + i * (use_cvm ? w : w - 2);
-        PADDLE_ENFORCE_EQ(static_cast<int>(ins[i]->numel() / x_dims[0]), w,
-                        paddle::platform::errors::InvalidArgument(
-                            "Width of all inputs should be equal."));
-        PADDLE_ENFORCE_EQ(x_lod.size(), bs + 1,
-                        paddle::platform::errors::InvalidArgument(
-                             "Batchsize of all inputs should be equal."));
         std::vector<int> cpu_lodx(x_lod.size());
         for (size_t j = 0; j < x_lod.size(); j++) {
            cpu_lodx[j] = x_lod[j];
         }
-        xpu::VectorParam<unsigned int> x_lod_vec = {reinterpret_cast<const unsigned int*>(cpu_lodx.data()),
-                            static_cast<int>(cpu_lodx.size()), nullptr};
-        int r = xpu::sequence_sum_pool_cvm<T, unsigned int>(xpu_context,  src, dst, x_lod_vec,
-                    bs, x_dims[1], 0.00f, n, use_cvm);
+
+        int* xpu_lod_ptr = RAII_GUARD.alloc_l3_or_gm<int>(x_lod.size());
+        xpu_memcpy(xpu_lod_ptr, cpu_lodx.data(), cpu_lodx.size() * sizeof(int), XPU_HOST_TO_DEVICE);
+        xpu_lod_addr_vec[i] = reinterpret_cast<uint64_t>(xpu_lod_ptr);
+    }
+
+    uint64_t* xpu_x_addr_ptr = RAII_GUARD.alloc_l3_or_gm<uint64_t>(slot_num);
+    xpu_memcpy(xpu_x_addr_ptr, xpu_x_addr_vec.data(), slot_num * sizeof(uint64_t), XPU_HOST_TO_DEVICE);
+    x_addr_VP_list.xpu = reinterpret_cast<uint64_t*>(xpu_x_addr_ptr);
+
+    uint64_t* xpu_lod_addr_ptr = RAII_GUARD.alloc_l3_or_gm<uint64_t>(slot_num);
+    xpu_memcpy(xpu_lod_addr_ptr, xpu_lod_addr_vec.data(), slot_num * sizeof(uint64_t), XPU_HOST_TO_DEVICE);
+    lod_VP_list.xpu = reinterpret_cast<uint64_t*>(xpu_lod_addr_ptr);
+
+    int r = xpu::sequence_sum_pool_cvm<T>(xpu_context, x_addr_VP_list, y_data, lod_VP_list,
+                    bs, x0_dims[1], 0.00f, slot_num, use_cvm);
         PADDLE_ENFORCE_EQ(r, xpu::Error_t::SUCCESS,
                      platform::errors::External(
                          "The sequence_sum_pool XPU OP return wrong value[%d %s]",
                          r, XPUAPIErrorMsg[r]));
-    }
   }
 };
 
