9e25ce44a0c7e6ff360525eb0bf1877e51517a1e
wwbitejotunn <wang_bojun@outlook.com>
2022-09-30 03:30:04 +0000

    NOLINT for codestyle

--- paddle/fluid/inference/tensorrt/plugin/preln_residual_bias_plugin.h
+++ paddle/fluid/inference/tensorrt/plugin/preln_residual_bias_plugin.h
@@ -106,10 +106,11 @@ class PrelnResidualBiasPluginDynamic : public DynamicPluginTensorRT {
   size_t getSerializationSize() const TRT_NOEXCEPT override;
   void serialize(void* buffer) const TRT_NOEXCEPT override;
 
-  nvinfer1::DimsExprs getOutputDimensions(int output_index,
-                                          const nvinfer1::DimsExprs* inputs,
-                                          int nb_inputs,
-                                          nvinfer1::IExprBuilder& expr_builder)
+  nvinfer1::DimsExprs getOutputDimensions(
+      int output_index,
+      const nvinfer1::DimsExprs* inputs,
+      int nb_inputs,
+      nvinfer1::IExprBuilder& expr_builder)  // NOLINT
       TRT_NOEXCEPT override;
   bool supportsFormatCombination(int pos,
                                  const nvinfer1::PluginTensorDesc* in_out,
