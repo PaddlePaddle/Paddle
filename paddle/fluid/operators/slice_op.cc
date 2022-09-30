b187c0fc67309864f4688dea77cc691380339bb9
yeliang2258 <1047690002@qq.com>
2022-09-30 06:44:59 +0000

    fix slice bugs

--- paddle/fluid/operators/slice_op.cc
+++ paddle/fluid/operators/slice_op.cc
@@ -155,7 +155,14 @@ class SliceOp : public framework::OperatorWithKernel {
 #ifdef PADDLE_WITH_MKLDNN
       auto input_data_type =
           framework::OperatorWithKernel::IndicateVarDataType(ctx, "Input");
-
+      auto vec_dims = phi::vectorize(in_tensor.dims());
+      bool all_zeros = true;
+      for (auto &dim : vec_dims) {
+        if (dim != 0) {
+          all_zeros = false;
+          break;
+        }
+      }
       if (this->CanMKLDNNBeUsed(ctx, input_data_type)) {
         // OneDNN uses blocking format, which cannot be always supported with
         // reorders, because if blocked dimension is not divisible by 8 or
@@ -165,7 +172,7 @@ class SliceOp : public framework::OperatorWithKernel {
             phi::vectorize(ctx.Input<phi::DenseTensor>("Input")->dims()),
             dnnl::memory::data_type::f32,
             ctx.Input<phi::DenseTensor>("Input")->format());
-        if (tmp_md.data.format_desc.blocking.inner_nblks == 0)
+        if (tmp_md.data.format_desc.blocking.inner_nblks == 0 && !all_zeros)
           return framework::OpKernelType(input_data_type,
                                          ctx.GetPlace(),
                                          framework::DataLayout::kMKLDNN,
