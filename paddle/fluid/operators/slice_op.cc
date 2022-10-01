1d96782786d7592e9d963ca7de3e5a0f33b8a4d3
yeliang2258 <1047690002@qq.com>
2022-10-01 06:30:50 +0000

    update code

--- paddle/fluid/operators/slice_op.cc
+++ paddle/fluid/operators/slice_op.cc
@@ -156,9 +156,9 @@ class SliceOp : public framework::OperatorWithKernel {
       auto input_data_type =
           framework::OperatorWithKernel::IndicateVarDataType(ctx, "Input");
       auto vec_dims = phi::vectorize(in_tensor.dims());
-      bool not_all_zero_dims = std::all_of(
-          vec_dims.cbegin(), vec_dims.cend(), [](int64_t i) { return i != 0; });
-      if (!not_all_zero_dims && this->CanMKLDNNBeUsed(ctx, input_data_type)) {
+      bool all_zero_dims = std::all_of(
+          vec_dims.cbegin(), vec_dims.cend(), [](int64_t i) { return i == 0; });
+      if (!all_zero_dims && this->CanMKLDNNBeUsed(ctx, input_data_type)) {
         // OneDNN uses blocking format, which cannot be always supported with
         // reorders, because if blocked dimension is not divisible by 8 or
         // 16(depending on which blocking format is used) submemory cannot be
