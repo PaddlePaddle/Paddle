1713909e3e9c53c2efd14a652220dde5b4cd0e7c
SigureMo <sigure.qaq@gmail.com>
2022-10-03 04:16:57 +0800

    test the required import

--- python/paddle/fluid/tests/unittests/dist_sharding_save.py
+++ python/paddle/fluid/tests/unittests/dist_sharding_save.py
@@ -14,7 +14,7 @@
 
 import paddle
 import paddle.fluid as fluid
-from test_dist_base import TestDistRunnerBase
+# from test_dist_base import TestDistRunnerBase
 from dist_mnist import cnn_model
 # from paddle.fluid.incubate.fleet.collective import fleet
 import paddle.distributed.fleet as fleet
