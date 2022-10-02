d3318e81c6cf276f1611aaaede04afb19374b2f2
SigureMo <sigure.qaq@gmail.com>
2022-10-02 19:30:35 +0800

    revert a F401 false positive

--- python/paddle/fluid/tests/unittests/test_imperative_data_loader_exit_func.py
+++ python/paddle/fluid/tests/unittests/test_imperative_data_loader_exit_func.py
@@ -26,7 +26,7 @@ if sys.version_info[0] == 2:
 else:
     import queue
 
-from paddle.fluid.reader import CleanupFuncRegistrar, _cleanup
+from paddle.fluid.reader import multiprocess_queue_set, CleanupFuncRegistrar, _cleanup  # noqa: F401
 
 # NOTE: These special functions cannot be detected by the existing coverage mechanism,
 # so the following unittests are added for these internal functions.
