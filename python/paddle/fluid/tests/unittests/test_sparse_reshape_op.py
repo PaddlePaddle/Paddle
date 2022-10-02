2e913357fec2f7a5e3852a2e5c1a48403c36cc0f
SmirnovKol <31559413+OccupyMars2025@users.noreply.github.com>
2022-10-02 20:18:18 +0800

    add all test cases for all kinds of kernels

--- python/paddle/fluid/tests/unittests/test_sparse_reshape_op.py
+++ python/paddle/fluid/tests/unittests/test_sparse_reshape_op.py
@@ -21,92 +21,68 @@ import unittest
 class TestReshape(unittest.TestCase):
     # x: sparse, out: sparse
     def check_result(self, x_shape, new_shape, format):
-        # with _test_eager_guard():
-            # mask = paddle.randint(0, 2, x_shape).astype("float32")
-            # origin_x = paddle.rand(x_shape, dtype='float32') * mask
-            # mask = paddle.randint(0, 2, x_shape)
-            # origin_x = paddle.randint(-100, 100, x_shape) * mask
+        mask = np.random.randint( 0, 2, x_shape)
+        np_x = np.random.randint(-100, 100, x_shape) * mask 
+        
+        
+        ### cpu version
+        dense_x = paddle.to_tensor(np_x, place=paddle.CPUPlace())
+        dense_x.stop_gradient = False
+        # dense_out = paddle.transpose(dense_x, dims)
+        dense_out = paddle.reshape(dense_x, new_shape)
 
-            # dense_x = paddle.clone(origin_x.detach())
-            mask = np.random.randint( 0, 2, x_shape)
-            np_x = np.random.randint(-100, 100, x_shape) * mask 
-            
-            
-            ### cpu version
-            dense_x = paddle.to_tensor(np_x, place=paddle.CPUPlace())
+        if format == "coo":
+            # sp_x = origin_x.detach().to_sparse_coo(len(x_shape))
+            sp_x = paddle.to_tensor(np_x, place=paddle.CPUPlace()).to_sparse_coo(len(x_shape))
+        else:
+            # sp_x = origin_x.detach().to_sparse_csr()
+            sp_x = paddle.to_tensor(np_x, place=paddle.CPUPlace()).to_sparse_csr()
+        sp_x.stop_gradient = False
+        # sp_out = paddle.incubate.sparse.transpose(sp_x, dims)
+        sp_out = paddle.incubate.sparse.reshape(sp_x, new_shape)
+
+        np.testing.assert_allclose(sp_out.to_dense().numpy(),
+                                dense_out.numpy(),
+                                rtol=1e-05)
+
+        # check  cpu backward computation 
+        dense_out.backward()
+        sp_out.backward()
+        np.testing.assert_allclose(sp_x.grad.to_dense().numpy(),
+                                #    dense_x.grad.numpy() * mask,
+                                    dense_x.grad.numpy() * np_x.astype('bool').astype('int'),
+                                    rtol=1e-05)
+
+        # if paddle.is_compiled_with_cuda():
+        # if False:
+        if True:
+            ## cuda  version
+            dense_x = paddle.to_tensor(np_x, place=paddle.CUDAPlace(0))
             dense_x.stop_gradient = False
             # dense_out = paddle.transpose(dense_x, dims)
             dense_out = paddle.reshape(dense_x, new_shape)
 
             if format == "coo":
                 # sp_x = origin_x.detach().to_sparse_coo(len(x_shape))
-                sp_x = paddle.to_tensor(np_x, place=paddle.CPUPlace()).to_sparse_coo(len(x_shape))
+                sp_x = paddle.to_tensor(np_x, place=paddle.CUDAPlace(0)).to_sparse_coo(len(x_shape))
             else:
                 # sp_x = origin_x.detach().to_sparse_csr()
-                sp_x = paddle.to_tensor(np_x, place=paddle.CPUPlace()).to_sparse_csr()
+                sp_x = paddle.to_tensor(np_x, place=paddle.CUDAPlace(0)).to_sparse_csr()
             sp_x.stop_gradient = False
             # sp_out = paddle.incubate.sparse.transpose(sp_x, dims)
             sp_out = paddle.incubate.sparse.reshape(sp_x, new_shape)
 
-            # print(10*'=', "OccupyMars2025 the following is dense_out", 10*'=')
-            # print("dense_out.numpy():", dense_out.numpy())
-            # print("dense_out:", dense_out)
-            # print(10*'=', "OccupyMars2025 the following is sp_out", 10*'=')
-            # print("sp_out:", sp_out)
-            # print("sp_out.to_dense():", sp_out.to_dense())
-            # print("sp_out.to_dense().numpy():", sp_out.to_dense().numpy())
-            # print(10*'=', "OccupyMars2025 the end", 10*'=')
-            
             np.testing.assert_allclose(sp_out.to_dense().numpy(),
                                     dense_out.numpy(),
                                     rtol=1e-05)
-
-            # if paddle.is_compiled_with_cuda():
-            if False:
-            # if True:
-                ## cuda  version
-                dense_x = paddle.to_tensor(np_x, place=paddle.CUDAPlace(0))
-                dense_x.numpy()
-                print(dense_x.numpy())
-                dense_x.stop_gradient = False
-                dense_x.numpy()
-                # dense_out = paddle.transpose(dense_x, dims)
-                dense_out = paddle.reshape(dense_x, new_shape)
-                dense_out.numpy()
-                print(dense_out.numpy())
-
-                if format == "coo":
-                    # sp_x = origin_x.detach().to_sparse_coo(len(x_shape))
-                    sp_x = paddle.to_tensor(np_x, place=paddle.CUDAPlace(0)).to_sparse_coo(len(x_shape))
-                else:
-                    # sp_x = origin_x.detach().to_sparse_csr()
-                    sp_x = paddle.to_tensor(np_x, place=paddle.CUDAPlace(0)).to_sparse_csr()
-                sp_x.stop_gradient = False
-                # sp_out = paddle.incubate.sparse.transpose(sp_x, dims)
-                sp_out = paddle.incubate.sparse.reshape(sp_x, new_shape)
-
-                print(10*'=', "OccupyMars2025 the following is dense_out", 10*'=')
-                print("dense_out.numpy():", dense_out.numpy())
-                print("dense_out:", dense_out)
-                print(10*'=', "OccupyMars2025 the following is sp_out", 10*'=')
-                print("sp_out:", sp_out)
-                print("sp_out.to_dense():", sp_out.to_dense())
-                print("sp_out.to_dense().numpy():", sp_out.to_dense().numpy())
-                print(10*'=', "OccupyMars2025 the end", 10*'=')
-                
-                np.testing.assert_allclose(sp_out.to_dense().numpy(),
-                                        dense_out.numpy(),
-                                        rtol=1e-05)
-
-            # dense_out.backward()
-            # sp_out.backward()
-            # print("sp_x.grad.to_dense().numpy(): ", sp_x.grad.to_dense().numpy())
-            # print("dense_x.grad.numpy(): ", dense_x.grad.numpy())
-            # print("mask: ", mask)
-            # np.testing.assert_allclose(sp_x.grad.to_dense().numpy(),
-            #                            dense_x.grad.numpy() * mask,
-            #                        #    dense_x.grad.numpy(),
-            #                            rtol=1e-05)
+            
+            # check  gpu backward computation 
+            dense_out.backward()
+            sp_out.backward()
+            np.testing.assert_allclose(sp_x.grad.to_dense().numpy(),
+                                    #    dense_x.grad.numpy() * mask,
+                                    dense_x.grad.numpy() * np_x.astype('bool').astype('int'),
+                                    rtol=1e-05)
 
     def test_reshape_2d(self):
         self.check_result([2, 5], [10,], 'coo')
@@ -115,7 +91,7 @@ class TestReshape(unittest.TestCase):
         self.check_result([10, 5], [2, 25], 'csr')
         self.check_result([9, 8], [18, 4], 'csr')
 
-    def test_transpose_3d(self):
+    def test_reshape_3d(self):
         self.check_result([6, 2, 3], [6, 2, 3], 'coo')
         self.check_result([6, 2, 3], [2, 3, 3, 2], 'coo')
         self.check_result([6, 2, 3], [1, 18, 2], 'coo')
@@ -130,12 +106,9 @@ class TestReshape(unittest.TestCase):
         self.check_result([6, 2, 3], [4, 9, 1], 'csr')
         self.check_result([6, 2, 3], [12, 1, 3], 'csr')
 
-    def test_transpose_nd(self):
+    def test_reshape_nd(self):
         self.check_result([8, 3, 4, 4, 5, 3], [24, 8, 10, 3], 'coo')
-    #     # Randint now only supports access to dimension 0 to 9.
-    #     # self.check_result([i % 3 + 2 for i in range(9)],
-    #     #                   [(i + 2) % 9 for i in range(9)], 'coo')
-
+        self.check_result([3, 4, 4, 5, 7], [1, 12, 2, 5, 14], 'coo')
 
 if __name__ == "__main__":
     unittest.main()
