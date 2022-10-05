39e1a808f97a3b1ca83db5e95ed6213061bfed98
SigureMo <sigure.qaq@gmail.com>
2022-10-06 00:39:02 +0800

    [CodeStyle][F401] remove unused import in python/paddle/distributed

--- python/paddle/distributed/collective.py
+++ python/paddle/distributed/collective.py
@@ -19,39 +19,14 @@ import io
 import datetime
 import time
 from ..fluid.layer_helper import LayerHelper
-from ..fluid.framework import Variable
 from ..fluid.framework import in_dygraph_mode
-from ..fluid.framework import OpProtoHolder
 from ..fluid.framework import _non_static_mode
-from ..fluid.framework import _in_legacy_dygraph
-from ..fluid.framework import convert_np_dtype_to_dtype_
-from ..fluid.framework import _varbase_creator
-from ..fluid.data_feeder import convert_dtype
 from ..fluid.data_feeder import check_variable_and_dtype
-from ..fluid.data_feeder import check_type
-from ..fluid.data_feeder import check_dtype
 from ..fluid.layers.tensor import fill_constant
-from ..fluid.layers import utils
-from ..fluid.dygraph import layers
-from ..fluid.dygraph.parallel import prepare_context
 import paddle
-import paddle.fluid as fluid
 import paddle.fluid.core as core
-from paddle import _C_ops, _legacy_C_ops
-import paddle.fluid.dygraph_utils as dygraph_utils
+from paddle import _legacy_C_ops
 import contextlib
-from .fleet.layers.mpu.mp_ops import split
-from .fleet.layers.mpu.mp_ops import _c_identity
-from .fleet.layers.mpu.mp_ops import _c_concat
-from .fleet.layers.mpu.mp_ops import _c_split
-from .fleet.layers.mpu.mp_ops import _mp_allreduce
-from .fleet.layers.mpu.mp_ops import _c_lookup_table
-from .fleet.layers.mpu.mp_ops import _Linear
-from .fleet.layers.mpu.mp_ops import _set_var_distributed
-from .fleet.layers.mpu.mp_ops import _c_softmax_with_cross_entropy
-from .fleet.layers.mpu.mp_ops import _linear
-from .fleet.layers.mpu.mp_ops import _parallel_linear
-from .fleet.layers.mpu.mp_ops import _parallel_embedding
 from .communication.comm_utils import ReduceOp
 
 __all__ = []
