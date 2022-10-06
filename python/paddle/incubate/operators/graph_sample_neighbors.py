380f71d8fb727d9cc139b45c8ca424303a5e3b00
Yulv-git <yulvchi@qq.com>
2022-10-06 12:03:37 +0800

    [CodeStyle][F401] remove unused imports in python_paddle/inference_device_profiler_text_metric_incubate_quantization_libs_audio_amp_jit.
    Signed-off-by: Yulv-git <yulvchi@qq.com>

--- python/paddle/incubate/operators/graph_sample_neighbors.py
+++ python/paddle/incubate/operators/graph_sample_neighbors.py
@@ -12,12 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import paddle
 from paddle.fluid.layer_helper import LayerHelper
 from paddle.fluid.framework import _non_static_mode
 from paddle.fluid.data_feeder import check_variable_and_dtype
-from paddle.fluid import core
-from paddle import _C_ops, _legacy_C_ops
+from paddle import _legacy_C_ops
 import paddle.utils.deprecated as deprecated
 
 
--- python/paddle/incubate/sparse/multiary.py
+++ python/paddle/incubate/sparse/multiary.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from paddle import _C_ops, _legacy_C_ops
+from paddle import _C_ops
 from paddle.fluid.framework import dygraph_only
 
 __all__ = []
