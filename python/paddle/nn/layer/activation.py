780b50a5499c66bf16d8010b7187bb9563de3e54
Nyakku Shigure <sigure.qaq@gmail.com>
2022-10-06 18:29:42 +0800

    Update python/paddle/nn/layer/activation.py, test=document_fix

--- python/paddle/nn/layer/activation.py
+++ python/paddle/nn/layer/activation.py
@@ -820,7 +820,7 @@ class Softplus(Layer):
 
             import paddle
 
-            x = paddle.to_tensor(np.array([-0.4, -0.2, 0.1, 0.3]),dtype='float32')
+            x = paddle.to_tensor([-0.4, -0.2, 0.1, 0.3], dtype='float32')
             m = paddle.nn.Softplus()
             out = m(x) # [0.513015, 0.598139, 0.744397, 0.854355]
     """
