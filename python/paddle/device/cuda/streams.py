f017a5c0e28230af63ef3b869de5b92a8b6fb12e
Yulv-git <yulvchi@qq.com>
2022-10-06 12:33:39 +0800

    Fix import for streams.
    Signed-off-by: Yulv-git <yulvchi@qq.com>

--- python/paddle/device/cuda/streams.py
+++ python/paddle/device/cuda/streams.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from paddle.fluid.core import CUDAStream as Stream  # noqa: F401
+from paddle.fluid.core import CUDAEvent as Event  # noqa: F401
