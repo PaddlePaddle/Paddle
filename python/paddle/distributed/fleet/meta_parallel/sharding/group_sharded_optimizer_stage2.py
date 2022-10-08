69ae5d43f54e098ebd148980c2744a53740386a8
liuyuang <liuyuang@baidu.com>
2022-10-08 08:53:51 +0800

    update broadcast logic to fit not all param update

--- python/paddle/distributed/fleet/meta_parallel/sharding/group_sharded_optimizer_stage2.py
+++ python/paddle/distributed/fleet/meta_parallel/sharding/group_sharded_optimizer_stage2.py
@@ -504,8 +504,9 @@ class GroupShardedOptimizerStage2(Optimizer):
                 tasks = []
                 for param in layer.parameters():
                     if param.trainable:
-                        assert param.name in param2task
-                        tasks.append(param2task[param.name])
+                        if param.name in self._broadcast_order_params:
+                            assert param.name in param2task
+                            tasks.append(param2task[param.name])
                 self._forward_pre_hook_remove_helper.append(
                     layer.register_forward_pre_hook(
                         self._forward_pre_hook_function(tasks)))
