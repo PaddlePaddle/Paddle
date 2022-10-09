35b7e8520fea658a34846a33c25e0999297e9b86
Nyakku Shigure <sigure.qaq@gmail.com>
2022-10-09 18:00:46 +0800

    fix typo in typing, test=document_fix

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -4333,7 +4333,7 @@ def put_along_axis(arr, indices, values, axis, reduce='assign'):
         indices (Tensor) : Indices to put along each 1d slice of arr. This must match the dimension of arr,
             and need to broadcast against arr. Supported data type are int and int64.
         axis (int) : The axis to put 1d slices along.
-        reduce (string, optinal) : The reduce operation, default is 'assign', support 'add', 'assign', 'mul' and 'multiply'.
+        reduce (str, optional): The reduce operation, default is 'assign', support 'add', 'assign', 'mul' and 'multiply'.
 
     Returns:
         Tensor: The indexed element, same dtype with arr
