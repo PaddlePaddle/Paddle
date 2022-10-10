2d3b0c49e9dd65d83e298222fb2a63670ab8f442
Shuangchi He <34329208+Yulv-git@users.noreply.github.com>
2022-10-10 18:10:56 +0800

    [CodeStyle][F401] remove unused imports in python_paddle/io_costmodel_geometric_vision_static. (#46755)
    Signed-off-by: Yulv-git <yulvchi@qq.com>
    
    Signed-off-by: Yulv-git <yulvchi@qq.com>

--- python/paddle/geometric/sampling/neighbors.py
+++ python/paddle/geometric/sampling/neighbors.py
@@ -12,11 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import paddle
 from paddle.fluid.layer_helper import LayerHelper
 from paddle.fluid.framework import _non_static_mode
 from paddle.fluid.data_feeder import check_variable_and_dtype
-from paddle import _C_ops, _legacy_C_ops
+from paddle import _legacy_C_ops
 
 __all__ = []
 
