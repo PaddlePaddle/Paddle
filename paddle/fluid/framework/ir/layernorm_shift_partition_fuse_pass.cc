610097b938ea1bfc4a3c70024bb56b8cc85a7b36
wwbitejotunn <wang_bojun@outlook.com>
2022-10-11 05:15:43 +0000

    pass opt draft

--- paddle/fluid/framework/ir/layernorm_shift_partition_fuse_pass.cc
+++ paddle/fluid/framework/ir/layernorm_shift_partition_fuse_pass.cc
@@ -16,6 +16,7 @@
 
 #include <cmath>
 #include <string>
+#include <vector>
 
 #include "paddle/fluid/framework/ir/graph_pattern_detector.h"
 #include "paddle/fluid/framework/op_proto_maker.h"
@@ -85,123 +86,146 @@ LayerNormShiftPartitionFusePass::LayerNormShiftPartitionFusePass() {
       .AddAttr("axis")
       .IsType<std::vector<int>>()
       .End();
+    AddOpCompat(OpCompat("roll"))
+        .AddInput("X")
+        .IsTensor()
+        .End()
+        .AddOutput("Out")
+        .IsTensor()
+        .End()
+        .AddAttr("axis")
+        .IsType<std::vector<int64_t>>()
+        .End()
+        .AddAttr("shifts")
+        .IsType<std::vector<int64_t>>()
+        .End();  
 }
 
-void LayerNormShiftPartitionFusePass::ApplyImpl(ir::Graph* graph) const {
+int LayerNormShiftPartitionFusePass::ApplyPattern(ir::Graph* graph, bool with_roll) const {
   PADDLE_ENFORCE_NOT_NULL(
       graph,
       platform::errors::InvalidArgument(
           "The input graph of LayerNormShiftPartitionFusePass should not be "
           "nullptr."));
-
   FusePassBase::Init(scope_name_, graph);
-
   GraphPatternDetector gpd;
   patterns::LayernormShiftPartitionPattern shift_patition_pattern(
-      gpd.mutable_pattern(), scope_name_);
+      gpd.mutable_pattern(), scope_name_, /*with_roll=*/ false);
   shift_patition_pattern();
-
   int found_count = 0;
   auto handler = [&](const GraphPatternDetector::subgraph_t& subgraph,
-                     Graph* g) {
-    if (!IsCompat(subgraph, g)) {
-      LOG(WARNING) << "layernorm_shift_partition_fuse in op compat failed.";
-      return;
-    }
+                   Graph* g) {
+  if (!IsCompat(subgraph, g)) {
+    LOG(WARNING) << "layernorm_shift_partition_fuse in op compat failed.";
+    return;
+  }
 
-    VLOG(4) << "layernorm_shift_partition_fuse pass";
-    GET_IR_NODE_FROM_SUBGRAPH(
-        layer_norm_in, layer_norm_in, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        layer_norm_op, layer_norm_op, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        layer_norm_bias, layer_norm_bias, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        layer_norm_scale, layer_norm_scale, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        layer_norm_out, layer_norm_out, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(reshape1_op, reshape1_op, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        reshape1_out, reshape1_out, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(reshape2_op, reshape2_op, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        reshape2_out, reshape2_out, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        transpose_op, transpose_op, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        transpose_out, transpose_out, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(reshape3_op, reshape3_op, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        reshape3_out, reshape3_out, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(reshape4_op, reshape4_op, shift_patition_pattern);
-    GET_IR_NODE_FROM_SUBGRAPH(
-        reshape4_out, reshape4_out, shift_patition_pattern);
+  VLOG(4) << "layernorm_shift_partition_fuse pass";
+  GET_IR_NODE_FROM_SUBGRAPH(
+      layer_norm_in, layer_norm_in, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      layer_norm_op, layer_norm_op, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      layer_norm_bias, layer_norm_bias, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      layer_norm_scale, layer_norm_scale, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      layer_norm_out, layer_norm_out, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(reshape1_op, reshape1_op, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      reshape1_out, reshape1_out, shift_patition_pattern);
+  if(with_roll){
+    GET_IR_NODE_FROM_SUBGRAPH(roll1_op,roll1_op,shift_patition_pattern);
+    GET_IR_NODE_FROM_SUBGRAPH(roll1_out,roll1_out,GET_IR_NODE_FROM_SUBGRAPH);
+  }
+  GET_IR_NODE_FROM_SUBGRAPH(reshape2_op, reshape2_op, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      reshape2_out, reshape2_out, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      transpose_op, transpose_op, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      transpose_out, transpose_out, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(reshape3_op, reshape3_op, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      reshape3_out, reshape3_out, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(reshape4_op, reshape4_op, shift_patition_pattern);
+  GET_IR_NODE_FROM_SUBGRAPH(
+      reshape4_out, reshape4_out, shift_patition_pattern);
 
-    std::vector<int> shape_atr1 =
-        PADDLE_GET_CONST(std::vector<int>, reshape1_op->Op()->GetAttr("shape"));
-    std::vector<int> shape_atr2 =
-        PADDLE_GET_CONST(std::vector<int>, reshape2_op->Op()->GetAttr("shape"));
-    std::vector<int> shape_atr3 =
-        PADDLE_GET_CONST(std::vector<int>, reshape3_op->Op()->GetAttr("shape"));
-    std::vector<int> shape_atr4 =
-        PADDLE_GET_CONST(std::vector<int>, reshape4_op->Op()->GetAttr("shape"));
+  std::vector<int> shape_atr1 =
+      PADDLE_GET_CONST(std::vector<int>, reshape1_op->Op()->GetAttr("shape"));
+  std::vector<int> shape_atr2 =
+      PADDLE_GET_CONST(std::vector<int>, reshape2_op->Op()->GetAttr("shape"));
+  std::vector<int> shape_atr3 =
+      PADDLE_GET_CONST(std::vector<int>, reshape3_op->Op()->GetAttr("shape"));
+  std::vector<int> shape_atr4 =
+      PADDLE_GET_CONST(std::vector<int>, reshape4_op->Op()->GetAttr("shape"));
 
-    // emb dim should be same
-    if (!((shape_atr1.back() == shape_atr2.back()) &&
-          (shape_atr2.back() == shape_atr3.back()) &&
-          (shape_atr3.back() == shape_atr4.back()))) {
-      return;
-    }
+  // emb dim should be same
+  if (!((shape_atr1.back() == shape_atr2.back()) &&
+        (shape_atr2.back() == shape_atr3.back()) &&
+        (shape_atr3.back() == shape_atr4.back()))) {
+    return;
+  }
 
-    if (shape_atr1[1] != shape_atr1[2]) {
-      return;
-    }
-    int input_resolution = shape_atr1[1];
+  if (shape_atr1[1] != shape_atr1[2]) {
+    return;
+  }
+  int input_resolution = shape_atr1[1];
 
-    if (shape_atr3[1] != shape_atr3[2]) {
-      return;
-    }
-    int window_size = shape_atr2[2];
-    if (window_size < 0 || input_resolution < 0) {
-      return;
-    }
+  if (shape_atr3[1] != shape_atr3[2]) {
+    return;
+  }
+  int window_size = shape_atr2[2];
+  if (window_size < 0 || input_resolution < 0) {
+    return;
+  }
 
-    OpDesc new_op_desc;
-    new_op_desc.SetType("layernorm_shift_partition");
-    new_op_desc.SetInput("X", {layer_norm_in->Name()});
-    new_op_desc.SetInput("Bias", {layer_norm_bias->Name()});
-    new_op_desc.SetInput("Scale", {layer_norm_scale->Name()});
-    new_op_desc.SetOutput("Y", {reshape4_out->Name()});
-    new_op_desc.SetAttr("epsilon", layer_norm_op->Op()->GetAttr("epsilon"));
-    new_op_desc.SetAttr("begin_norm_axis",
-                        layer_norm_op->Op()->GetAttr("begin_norm_axis"));
-    new_op_desc.SetAttr("window_size", window_size);
-    new_op_desc.SetAttr("shift_size", 0); // if no circle_shift (roll), shift_size is 0
-    new_op_desc.SetAttr("input_resolution", input_resolution);
-    new_op_desc.Flush();
+  OpDesc new_op_desc;
+  new_op_desc.SetType("layernorm_shift_partition");
+  new_op_desc.SetInput("X", {layer_norm_in->Name()});
+  new_op_desc.SetInput("Bias", {layer_norm_bias->Name()});
+  new_op_desc.SetInput("Scale", {layer_norm_scale->Name()});
+  new_op_desc.SetOutput("Y", {reshape4_out->Name()});
+  new_op_desc.SetAttr("epsilon", layer_norm_op->Op()->GetAttr("epsilon"));
+  new_op_desc.SetAttr("begin_norm_axis",
+                      layer_norm_op->Op()->GetAttr("begin_norm_axis"));
+  new_op_desc.SetAttr("window_size", window_size);
+  new_op_desc.SetAttr("shift_size", 0); // if no circle_shift (roll), shift_size is 0
+  new_op_desc.SetAttr("input_resolution", input_resolution);
+  new_op_desc.Flush();
 
-    auto* layernorm_shift_partition = graph->CreateOpNode(&new_op_desc);
+  auto* layernorm_shift_partition = graph->CreateOpNode(&new_op_desc);
 
-    IR_NODE_LINK_TO(layer_norm_in, layernorm_shift_partition);
-    IR_NODE_LINK_TO(layer_norm_bias, layernorm_shift_partition);
-    IR_NODE_LINK_TO(layer_norm_scale, layernorm_shift_partition);
-    IR_NODE_LINK_TO(layernorm_shift_partition, reshape4_out);
-    GraphSafeRemoveNodes(graph,
-                         {layer_norm_op,
-                          layer_norm_out,
-                          reshape1_op,
-                          reshape1_out,
-                          reshape2_op,
-                          reshape2_out,
-                          transpose_op,
-                          transpose_out,
-                          reshape3_op,
-                          reshape3_out,
-                          reshape4_op});
-    ++found_count;
+  IR_NODE_LINK_TO(layer_norm_in, layernorm_shift_partition);
+  IR_NODE_LINK_TO(layer_norm_bias, layernorm_shift_partition);
+  IR_NODE_LINK_TO(layer_norm_scale, layernorm_shift_partition);
+  IR_NODE_LINK_TO(layernorm_shift_partition, reshape4_out);
+  GraphSafeRemoveNodes(graph,
+                       {layer_norm_op,
+                        layer_norm_out,
+                        reshape1_op,
+                        reshape1_out,
+                        reshape2_op,
+                        reshape2_out,
+                        transpose_op,
+                        transpose_out,
+                        reshape3_op,
+                        reshape3_out,
+                        reshape4_op});
+  ++found_count;
   };
 
   gpd(graph, handler);
+
+  return found_count;
+}
+
+void LayerNormShiftPartitionFusePass::ApplyImpl(ir::Graph* graph) const {
+
+
+  int found_count = 0;
+  found_count+=ApplyPattern(graph,false);
   AddStatis(found_count);
 }
 
