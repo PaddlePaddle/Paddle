fe716a0b829e4061d7f3ea9844fc57f757572a27
Nyakku Shigure <sigure.qaq@gmail.com>
2022-10-12 10:52:31 +0800

    [CodeStyle][F401] remove unused imports in python/paddle/distributed (#46758)
    * [CodeStyle][F401] remove unused import in python/paddle/distributed
    
    * remove pass
    
    * empty commit
    
    * Fix ValueError: list.remove(x): x not in list for meta_optimizer_names.
    
    Fix ValueError: list.remove(x): x not in list for meta_optimizer_names.
    
    * Fix split import.
    
    Fix split import.
    
    * add noqa after meta_optimizers in factory
    
    * restort collective ops
    
    * expand `import *`
    
    * add noqa after required imports
    
    * try to fix APIs without core.ops
    
    * Revert "try to fix APIs without core.ops"
    
    This reverts commit 6172beaf601e84bf61f2490c12c4739f0edaa5eb.
    
    * fix an increment
    
    * empty commit
    
    * add noqa after required imports
    
    * expand `import *`, fix ci error
    
    Co-authored-by: Shuangchi He <34329208+Yulv-git@users.noreply.github.com>

--- python/paddle/distributed/auto_parallel/cluster_v2.py
+++ python/paddle/distributed/auto_parallel/cluster_v2.py
@@ -12,15 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import copy
 import numpy as np
 from enum import IntEnum
 from enum import unique
 
-import paddle
 from paddle.fluid import core
-from paddle.fluid.core import Device
-from paddle.fluid.core import Link
+from paddle.fluid.core import Device  # noqa: F401
+from paddle.fluid.core import Link  # noqa: F401
 
 
 @unique
