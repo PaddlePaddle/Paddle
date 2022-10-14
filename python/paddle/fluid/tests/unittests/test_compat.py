a1062b10c18ae82b9af57ad8aaf52afbc36b73b4
SigureMo <sigure.qaq@gmail.com>
2022-10-15 02:20:11 +0800

    Revert "clean up to_bytes definition and unittests"
    This reverts commit e726539e1768172a411ff60e63fab82f164343cf.

--- python/paddle/fluid/tests/unittests/test_compat.py
+++ python/paddle/fluid/tests/unittests/test_compat.py
@@ -135,6 +135,107 @@ class TestCompatible(unittest.TestCase):
         self.assertEqual(l, l2)
         self.assertEqual({"": ""}, l2)
 
+    def test_to_bytes(self):
+        self.assertIsNone(cpt.to_bytes(None))
+
+        self.assertTrue(isinstance(cpt.to_bytes(str("")), bytes))
+        self.assertTrue(isinstance(cpt.to_bytes(str("123")), bytes))
+        self.assertTrue(isinstance(cpt.to_bytes(b""), bytes))
+        self.assertTrue(isinstance(cpt.to_bytes(b""), bytes))
+        self.assertTrue(isinstance(cpt.to_bytes(u""), bytes))
+        self.assertTrue(isinstance(cpt.to_bytes(u""), bytes))
+
+        self.assertEqual(b"", cpt.to_bytes(str("")))
+        self.assertEqual(b"123", cpt.to_bytes(str("123")))
+        self.assertEqual(b"", cpt.to_bytes(b""))
+        self.assertEqual(b"123", cpt.to_bytes(b"123"))
+        self.assertEqual(b"", cpt.to_bytes(u""))
+        self.assertEqual(b"123", cpt.to_bytes(u"123"))
+
+        # check list types, not inplace
+        l = [""]
+        l2 = cpt.to_bytes(l)
+        self.assertTrue(isinstance(l2, list))
+        self.assertFalse(l is l2)
+        self.assertNotEqual(l, l2)
+        self.assertEqual([b""], l2)
+        l = ["", "123"]
+        l2 = cpt.to_bytes(l)
+        self.assertTrue(isinstance(l2, list))
+        self.assertFalse(l is l2)
+        self.assertNotEqual(l, l2)
+        self.assertEqual([b"", b"123"], l2)
+        l = ["", b"123", u"321"]
+        l2 = cpt.to_bytes(l)
+        self.assertTrue(isinstance(l2, list))
+        self.assertFalse(l is l2)
+        self.assertNotEqual(l, l2)
+        self.assertEqual([b"", b"123", b"321"], l2)
+
+        # check list types, inplace
+        l = [""]
+        l2 = cpt.to_bytes(l, inplace=True)
+        self.assertTrue(isinstance(l2, list))
+        self.assertTrue(l is l2)
+        self.assertEqual(l, l2)
+        self.assertEqual([b""], l2)
+        l = ["", b"123"]
+        l2 = cpt.to_bytes(l, inplace=True)
+        self.assertTrue(isinstance(l2, list))
+        self.assertTrue(l is l2)
+        self.assertEqual(l, l2)
+        self.assertEqual([b"", b"123"], l2)
+        l = ["", b"123", u"321"]
+        l2 = cpt.to_bytes(l, inplace=True)
+        self.assertTrue(isinstance(l2, list))
+        self.assertTrue(l is l2)
+        self.assertEqual(l, l2)
+        self.assertEqual([b"", b"123", b"321"], l2)
+        for i in l2:
+            self.assertTrue(isinstance(i, bytes))
+
+        # check set types, not inplace
+        l = set([""])
+        l2 = cpt.to_bytes(l, inplace=False)
+        self.assertTrue(isinstance(l2, set))
+        self.assertFalse(l is l2)
+        self.assertNotEqual(l, l2)
+        self.assertEqual(set([b""]), l2)
+        l = set([u"", u"123"])
+        l2 = cpt.to_bytes(l, inplace=False)
+        self.assertTrue(isinstance(l2, set))
+        self.assertFalse(l is l2)
+        self.assertNotEqual(l, l2)
+        self.assertEqual(set([b"", b"123"]), l2)
+        l = set(["", b"123", u"321"])
+        l2 = cpt.to_bytes(l, inplace=False)
+        self.assertTrue(isinstance(l2, set))
+        self.assertFalse(l is l2)
+        self.assertNotEqual(l, l2)
+        self.assertEqual(set([b"", b"123", b"321"]), l2)
+
+        # check set types, inplace
+        l = set("")
+        l2 = cpt.to_bytes(l, inplace=True)
+        self.assertTrue(isinstance(l2, set))
+        self.assertTrue(l is l2)
+        self.assertEqual(l, l2)
+        self.assertEqual(set(b""), l2)
+        l = set([u"", u"123"])
+        l2 = cpt.to_bytes(l, inplace=True)
+        self.assertTrue(isinstance(l2, set))
+        self.assertTrue(l is l2)
+        self.assertEqual(l, l2)
+        self.assertEqual(set([b"", b"123"]), l2)
+        l = set(["", b"123", u"321"])
+        l2 = cpt.to_bytes(l, inplace=True)
+        self.assertTrue(isinstance(l2, set))
+        self.assertTrue(l is l2)
+        self.assertEqual(l, l2)
+        self.assertEqual(set([b"", b"123", b"321"]), l2)
+        for i in l2:
+            self.assertTrue(isinstance(i, bytes))
+
 
 if __name__ == "__main__":
     unittest.main()
