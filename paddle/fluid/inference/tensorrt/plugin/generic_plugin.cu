262bbaaf8e82f00c29ca99ae78a9f1dc150b2588
Leo Chen <chenqiuliang@baidu.com>
2022-10-14 09:04:22 +0800

    add athor (#46994)

--- paddle/fluid/inference/tensorrt/plugin/generic_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/generic_plugin.cu
@@ -287,15 +287,21 @@ bool GenericPlugin::supportsFormatCombination(
     const nvinfer1::PluginTensorDesc* in_out,
     int nb_inputs,
     int nb_outputs) TRT_NOEXCEPT {
+  bool is_linear_input = in_out[pos].format == nvinfer1::TensorFormat::kLINEAR;
   if (op_desc_.Type() == "gather_nd" || op_desc_.Type() == "yolo_box") {
-    if (pos == 0) return in_out[pos].type == nvinfer1::DataType::kFLOAT;
-    if (pos == 1) return in_out[pos].type == nvinfer1::DataType::kINT32;
+    if (pos == 0)
+      return in_out[pos].type == nvinfer1::DataType::kFLOAT && is_linear_input;
+    if (pos == 1)
+      return in_out[pos].type == nvinfer1::DataType::kINT32 && is_linear_input;
   } else if (op_desc_.Type() == "scatter_nd_add") {
-    if (pos == 0) return in_out[pos].type == nvinfer1::DataType::kFLOAT;
-    if (pos == 1) return in_out[pos].type == nvinfer1::DataType::kINT32;
-    if (pos == 2) return in_out[pos].type == nvinfer1::DataType::kFLOAT;
+    if (pos == 0)
+      return in_out[pos].type == nvinfer1::DataType::kFLOAT && is_linear_input;
+    if (pos == 1)
+      return in_out[pos].type == nvinfer1::DataType::kINT32 && is_linear_input;
+    if (pos == 2)
+      return in_out[pos].type == nvinfer1::DataType::kFLOAT && is_linear_input;
   } else {
-    return in_out[pos].type == nvinfer1::DataType::kFLOAT;
+    return in_out[pos].type == nvinfer1::DataType::kFLOAT && is_linear_input;
   }
 }
 
