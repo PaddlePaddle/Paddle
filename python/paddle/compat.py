b8461b79f71aa004a99fc7de5273277732540d2a
SigureMo <sigure.qaq@gmail.com>
2022-10-15 20:51:39 +0800

    Revert "remove to_text definition and unittest"
    This reverts commit a6b69cb8dca8b9b031ce10ea32d1040e7e0dd267.

--- python/paddle/compat.py
+++ python/paddle/compat.py
@@ -17,6 +17,103 @@ import six
 __all__ = []
 
 
+#  str and bytes related functions
+def to_text(obj, encoding='utf-8', inplace=False):
+    """
+    All string in PaddlePaddle should be represented as a literal string.
+
+    This function will convert object to a literal string without any encoding.
+    Especially, if the object type is a list or set container, we will iterate
+    all items in the object and convert them to literal string.
+
+    In Python3:
+        Decode the bytes type object to str type with specific encoding
+
+    In Python2:
+        Decode the str type object to unicode type with specific encoding
+
+    Args:
+        obj(unicode|str|bytes|list|set) : The object to be decoded.
+        encoding(str) : The encoding format to decode a string
+        inplace(bool) : If we change the original object or we create a new one
+
+    Returns:
+        Decoded result of obj
+
+    Examples:
+
+        .. code-block:: python
+
+            import paddle
+
+            data = "paddlepaddle"
+            data = paddle.compat.to_text(data)
+            # paddlepaddle
+
+    """
+    if obj is None:
+        return obj
+
+    if isinstance(obj, list):
+        if inplace:
+            for i in six.moves.xrange(len(obj)):
+                obj[i] = _to_text(obj[i], encoding)
+            return obj
+        else:
+            return [_to_text(item, encoding) for item in obj]
+    elif isinstance(obj, set):
+        if inplace:
+            for item in obj:
+                obj.remove(item)
+                obj.add(_to_text(item, encoding))
+            return obj
+        else:
+            return set([_to_text(item, encoding) for item in obj])
+    elif isinstance(obj, dict):
+        if inplace:
+            new_obj = {}
+            for key, value in six.iteritems(obj):
+                new_obj[_to_text(key, encoding)] = _to_text(value, encoding)
+            obj.update(new_obj)
+            return obj
+        else:
+            new_obj = {}
+            for key, value in six.iteritems(obj):
+                new_obj[_to_text(key, encoding)] = _to_text(value, encoding)
+            return new_obj
+    else:
+        return _to_text(obj, encoding)
+
+
+def _to_text(obj, encoding):
+    """
+    In Python3:
+        Decode the bytes type object to str type with specific encoding
+
+    In Python2:
+        Decode the str type object to unicode type with specific encoding,
+        or we just return the unicode string of object
+
+    Args:
+        obj(unicode|str|bytes) : The object to be decoded.
+        encoding(str) : The encoding format
+
+    Returns:
+        decoded result of obj
+    """
+    if obj is None:
+        return obj
+
+    if isinstance(obj, six.binary_type):
+        return obj.decode(encoding)
+    elif isinstance(obj, six.text_type):
+        return obj
+    elif isinstance(obj, (bool, float)):
+        return obj
+    else:
+        return six.u(obj)
+
+
 def to_bytes(obj, encoding='utf-8', inplace=False):
     """
     All string in PaddlePaddle should be represented as a literal string.
