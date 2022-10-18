4b669e830823429e73750f0ecddaba33fe44ebc2
SigureMo <sigure.qaq@gmail.com>
2022-10-18 19:01:46 +0800

    [CodeStyle] add more information when codestyle check failed

--- tools/codestyle/pre_commit.sh
+++ tools/codestyle/pre_commit.sh
@@ -1,11 +1,11 @@
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,7 +24,7 @@ fi
 readonly VERSION="13.0.0"
 version=$(clang-format -version)
 if ! [[ $(python -V 2>&1 | awk '{print $2}' | awk -F '.' '{print $1$2}') -ge 36 ]]; then
-    echo "clang-format installation by pip need python version great equal 3.6, 
+    echo "clang-format installation by pip need python version great equal 3.6,
           please change the default python to higher version."
     exit 1
 fi
@@ -37,7 +37,7 @@ pre-commit run --files ${diff_files};check_error=$?
 
 if test ! -z "$(git diff)"; then
     echo -e '\n************************************************************************************'
-    echo -e "These files have been formated by code format hook. You should use pre-commit to \
+    echo -e "These files have been formatted by code format hook. You should use pre-commit to \
 format them before git push."
     echo -e '************************************************************************************\n'
     git diff 2>&1
@@ -46,6 +46,17 @@ fi
 echo -e '\n***********************************'
 if [ ${check_error} != 0 ];then
     echo "Your PR code style check failed."
+    echo "Please install pre-commit locally and set up git hook scripts:"
+    echo ""
+    echo "    pip install pre-commit==2.17.0"
+    echo "    pre-commit install"
+    echo ""
+    echo "Then, run pre-commit to check codestyle issues in your PR:"
+    echo ""
+    echo "    pre-commit run --files $diff_files"
+    echo ""
+    echo "For more information, please refer to our codestyle check guide:"
+    echo "https://www.paddlepaddle.org.cn/documentation/docs/zh/develop/dev_guides/git_guides/codestyle_check_guide_cn.html"
 else
     echo "Your PR code style check passed."
 fi
