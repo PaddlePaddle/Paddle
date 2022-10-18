ad4c773b183d614112f5f66dfc6a11d3a249e557
Nyakku Shigure <sigure.qaq@gmail.com>
2022-10-18 10:27:15 +0800

    [CodeStyle][py2] remove `compat` module (to_text) (#47036)
    * [CodeStyle][py2] remove `compat` module (to_text)
    
    * remove some unnecessary decode
    
    * remove to_text definition and unittest
    
    * Revert "remove to_text definition and unittest"
    
    This reverts commit a6b69cb8dca8b9b031ce10ea32d1040e7e0dd267.
    
    * remove an assertion
    
    * empty commit

--- python/paddle/dataset/wmt14.py
+++ python/paddle/dataset/wmt14.py
@@ -24,7 +24,6 @@ import six
 import tarfile
 
 import paddle.dataset.common
-import paddle.compat as cpt
 import paddle.utils.deprecated as deprecated
 
 __all__ = []
@@ -52,7 +51,7 @@ def __read_to_dict(tar_file, dict_size):
         out_dict = dict()
         for line_count, line in enumerate(fd):
             if line_count < size:
-                out_dict[cpt.to_text(line.strip())] = line_count
+                out_dict[line.strip().decode()] = line_count
             else:
                 break
         return out_dict
@@ -84,7 +83,7 @@ def reader_creator(tar_file, file_name, dict_size):
             ]
             for name in names:
                 for line in f.extractfile(name):
-                    line = cpt.to_text(line)
+                    line = line.decode()
                     line_split = line.strip().split('\t')
                     if len(line_split) != 2:
                         continue
--- python/paddle/text/datasets/wmt14.py
+++ python/paddle/text/datasets/wmt14.py
@@ -17,7 +17,6 @@ import numpy as np
 import six
 
 from paddle.io import Dataset
-import paddle.compat as cpt
 from paddle.dataset.common import _check_exists_and_download
 
 __all__ = []
@@ -111,7 +110,7 @@ class WMT14(Dataset):
             out_dict = dict()
             for line_count, line in enumerate(fd):
                 if line_count < size:
-                    out_dict[cpt.to_text(line.strip())] = line_count
+                    out_dict[line.strip().decode()] = line_count
                 else:
                     break
             return out_dict
@@ -140,7 +139,7 @@ class WMT14(Dataset):
             ]
             for name in names:
                 for line in f.extractfile(name):
-                    line = cpt.to_text(line)
+                    line = line.decode()
                     line_split = line.strip().split('\t')
                     if len(line_split) != 2:
                         continue
