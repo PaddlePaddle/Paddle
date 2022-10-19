9fa70e4df0f4dc45e27b9b92190e2811d35d43de
xiongkun <xiongkun03@baidu.com>
2022-10-19 10:57:42 +0800

    [Dy2Static] Remove GradTransformer (#47063)
    * [Dy2Static] Remove GradTransformer
    1. fix einsum infershape bugs.
    2. remove grad_transformer and unify paddle.grad and paddle.static.gradient.
    3. add dygraph_and_dy2static_only decorator for dy2static.
    
    * fix bugs
    
    * rename

--- python/paddle/fluid/dygraph/dygraph_to_static/ast_transformer.py
+++ python/paddle/fluid/dygraph/dygraph_to_static/ast_transformer.py
@@ -105,7 +105,7 @@ class DygraphToStaticAst(BaseTransformer):
             PrintTransformer,  # print statement
             CallTransformer,  # transform call recursively
             CastTransformer,  # type casting statement
-            GradTransformer,  # transform paddle.grad to paddle.gradients
+            #GradTransformer,  # transform paddle.grad to paddle.gradients
             DecoratorTransformer,  # transform decorators to function call
             TypeHintTransformer,  # remove all typehint in gast.Name
         ]
