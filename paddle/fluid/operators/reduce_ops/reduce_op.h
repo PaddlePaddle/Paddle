7ff3053ae7ade2c382dbd40396ae25ce3d5f7224
jiahongyu <jiahongyu@baidu.com>
2022-10-20 09:11:31 +0000

    fix reduce_op fallback

--- paddle/fluid/operators/reduce_ops/reduce_op.h
+++ paddle/fluid/operators/reduce_ops/reduce_op.h
@@ -559,6 +559,10 @@ class ReduceOp : public framework::OperatorWithKernel {
         experimental::DataType::BFLOAT16)
       return true;
 
+    if (!ctx.HasAttr("dim") || !ctx.HasAttr("reduce_all")) {
+      return false;
+    }
+
     auto reduce_dims = ctx.Attr<std::vector<int>>("dim");
     const bool reduce_all = ctx.Attr<bool>("reduce_all");
     int ndims = ctx.Input<phi::DenseTensor>("X")->dims().size();
@@ -586,15 +590,12 @@ class ReduceOp : public framework::OperatorWithKernel {
     // choose cudnn kernel if the runtime supported.
     auto input_data_type = OperatorWithKernel::IndicateVarDataType(ctx, "X");
 
-    if (ctx.Input<phi::DenseTensor>("X")->dims().size() > 5)
-      return framework::OpKernelType(input_data_type, ctx.GetPlace());
-
     // NOTE(jiahongyu): Below codes originally enclosed by PADDLE_WITH_MKLDNN
-    if (!HasOptimizedOneDNNKernel(ctx)) {
+    if (ctx.Input<phi::DenseTensor>("X")->dims().size() > 5 ||
+        !HasOptimizedOneDNNKernel(ctx)) {
       dnn_fallback_ = true;
     }
-    // NOTE(jiahongyu): Above codes originally enclosed by
-    // PADDLE_WITH_MKLDNN
+    // NOTE(jiahongyu): Above codes originally enclosed by PADDLE_WITH_MKLDNN
 
     if (input_data_type == framework::proto::VarType::FP16) {
       PADDLE_ENFORCE_EQ(
@@ -673,9 +674,8 @@ class ReduceGradOp : public framework::OperatorWithKernel {
                   ctx, framework::GradVarName("Out"));
 
     // NOTE(jiahongyu): Below codes originally enclosed by PADDLE_WITH_MKLDNN
-    auto dx_dims = ctx.Input<phi::DenseTensor>("X")->dims();
-
-    if (dx_dims.size() > 5) {  // max 5D tensor is supported
+    // max 5D tensor is supported
+    if (ctx.Input<phi::DenseTensor>("X")->dims().size() > 5) {
       dnn_fallback_ = true;
     }
     // NOTE(jiahongyu): Above codes originally enclosed by PADDLE_WITH_MKLDNN
