a7cd4a04300fa1ecf270bc9e42ebd3cce10b5ef6
Manan Goel <manangoel1999@gmail.com>
2022-10-21 11:42:38 +0000

    Fixed code formatting in lazy import test

--- python/paddle/tests/test_utils_lazyimport.py
+++ python/paddle/tests/test_utils_lazyimport.py
@@ -15,8 +15,9 @@
 import unittest
 from paddle.utils.lazy_import import try_import
 
+
 class TestUtilsLazyImport(unittest.TestCase):
-    
+
     def setup(self):
         pass
 
@@ -26,18 +27,18 @@ class TestUtilsLazyImport(unittest.TestCase):
 
         with self.assertRaises(ImportError) as context:
             paddle2 = try_import('paddle2')
-        
-        self.assertTrue(
-            'require additional dependencies that have to be' in str(context.exception)
-        )
+
+        self.assertTrue('require additional dependencies that have to be' in
+                        str(context.exception))
 
         with self.assertRaises(ImportError) as context:
             paddle2 = try_import('paddle2', 'paddle2 is not installed')
-        
+
         self.assertTrue('paddle2 is not installed' in str(context.exception))
-    
+
     def test_lazy_import(self):
         self.func_test_lazy_import()
+
 
 if __name__ == "__main__":
     unittest.main()
