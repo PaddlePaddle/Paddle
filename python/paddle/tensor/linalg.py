958f5e3fbe6dfab893cbcb67d1028ca0bb40d74e
超级码牛 <54444805+SuperCodebull@users.noreply.github.com>
2022-10-21 21:24:10 +0800

    fix paddle.matmul 文档评估 #4407
    把函数的输出改成正确的

--- python/paddle/tensor/linalg.py
+++ python/paddle/tensor/linalg.py
@@ -186,35 +186,35 @@ def matmul(x, y, transpose_x=False, transpose_y=False, name=None):
             y = paddle.rand([10])
             z = paddle.matmul(x, y)
             print(z.shape)
-            # [1]
+            # (1,)
 
             # matrix * vector
             x = paddle.rand([10, 5])
             y = paddle.rand([5])
             z = paddle.matmul(x, y)
             print(z.shape)
-            # [10]
+            # (10,)
 
             # batched matrix * broadcasted vector
             x = paddle.rand([10, 5, 2])
             y = paddle.rand([2])
             z = paddle.matmul(x, y)
             print(z.shape)
-            # [10, 5]
+            # (10, 5)
 
             # batched matrix * batched matrix
             x = paddle.rand([10, 5, 2])
             y = paddle.rand([10, 2, 5])
             z = paddle.matmul(x, y)
             print(z.shape)
-            # [10, 5, 5]
+            # (10, 5, 5)
 
             # batched matrix * broadcasted matrix
             x = paddle.rand([10, 1, 5, 2])
             y = paddle.rand([1, 3, 2, 5])
             z = paddle.matmul(x, y)
             print(z.shape)
-            # [10, 3, 5, 5]
+            # (10, 3, 5, 5)
 
     """
     if in_dygraph_mode():
