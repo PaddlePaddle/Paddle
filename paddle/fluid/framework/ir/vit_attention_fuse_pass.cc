26933f88da589afc673ea5596859bf8b28d848f9
fengshuai <fengshuai03@baidu.com>
2022-10-23 09:03:56 +0000

    init

--- paddle/fluid/framework/ir/vit_attention_fuse_pass.cc
+++ paddle/fluid/framework/ir/vit_attention_fuse_pass.cc
@@ -56,6 +56,22 @@ namespace paddle {
 namespace framework {
 namespace ir {
 
+bool HasScale(OpDesc* const op_ptr,
+              std::string* name,
+              std::string regexp = "Input_scale_") {
+  name.clear();
+  std::unordered_map<std::string, Attribute> attr_map = op_ptr->GetAttrMap();
+  std::unordered_map<std::string, Attribute>::iterator iter;
+  int len = regexp.size();
+  for (iter = attr_map.begin(); iter != attr_map.end(); iter++) {
+    if (regexp == iter->first.substr(0, len)) {
+      name = iter->first;
+      return true;
+    }
+  }
+  return false;
+}
+
 void VitAttentionFusePass::ApplyImpl(ir::Graph* graph) const {
   GraphPatternDetector gpd;
   const std::string pattern_name = "vit_attention_fuse";
@@ -102,6 +118,21 @@ void VitAttentionFusePass::ApplyImpl(ir::Graph* graph) const {
     desc.SetAttr("head_number", static_cast<int>(shape[1]));
     float alpha = PADDLE_GET_CONST(float, scale1_op->Op()->GetAttr("scale"));
     desc.SetAttr("alpha", alpha);
+
+    // int8 start
+    std::string scale_name;
+    if (HasScale(matmul0_op->Op(), &scale_name)) {
+      desc.SetAttr("Input_scale", matmul0_op->Op()->GetAttr(scale_name));
+    }
+    if (HasScale(elementwise0_op->Op(), &scale_name, "Out")) {
+      desc.SetAttr("fc_out_threshold",
+                   elementwise0_op->Op()->GetAttr(scale_name));
+    }
+    // int8 for qkv
+    if (HasScale(matmul1_op->Op(), &scale_name)) {
+      desc.SetAttr("qkv2context_plugin_int8", true);
+    }
+    // int8 end
 
     // Create a new node for the fused op.
     auto vit_attention_node = graph->CreateOpNode(&desc);
