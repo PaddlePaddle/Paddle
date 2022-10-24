f7172d7bf976e32cb92b4f314db7b99eb70a7d86
james <zhangxiaoci@baidu.com>
2022-10-24 05:55:26 +0000

    optimize resnet_unit

--- paddle/fluid/imperative/amp_auto_cast.cc
+++ paddle/fluid/imperative/amp_auto_cast.cc
@@ -348,6 +348,9 @@ NameVarMap<VarType> AutoCastInputs(const std::string& op_type,
           pair.first != "X") {
         continue;
       }
+      if ((op_type == "max_pool2d_with_index_grad" || op_type == "max_pool2d_with_index") && pair.first == "Mask") {
+        continue;
+      }
 
       if ((op_type == "fused_attention" || op_type == "fused_feedforward")) {
         if (pair.first == "LnScale" || pair.first == "LnBias" ||
@@ -390,6 +393,9 @@ NameVarMap<VarType> AutoCastInputs(const std::string& op_type,
       if ((op_type == "batch_norm" || op_type == "layer_norm" ||
            op_type == "sync_batch_norm") &&
           pair.first == "X" && dst_type == framework::proto::VarType::FP32) {
+        continue;
+      }
+      if ((op_type == "max_pool2d_with_index_grad" || op_type == "max_pool2d_with_index") && pair.first != "Mask" && dst_type == framework::proto::VarType::FP32) {
         continue;
       }
       if ((op_type == "fused_attention" || op_type == "fused_feedforwad") &&
@@ -437,6 +443,9 @@ NameVarMap<VarType> CastPureFp16Inputs(const std::string& op_type,
     if ((op_type == "batch_norm" || op_type == "layer_norm" ||
          op_type == "sync_batch_norm") &&
         pair.first != "X") {
+      continue;
+    }
+    if ((op_type == "max_pool2d_with_index_grad" || op_type == "max_pool2d_with_index") && pair.first == "Mask") {
       continue;
     }
     if ((op_type == "fused_attention" || op_type == "fused_feedforward")) {
