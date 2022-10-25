8a010a570d1c2290f729145afaa24bc0f57b6a33
xiongkun <xiongkun03@baidu.com>
2022-10-25 04:00:14 +0000

    change flatten -> _is_sequence_except_dict

--- python/paddle/fluid/layers/control_flow.py
+++ python/paddle/fluid/layers/control_flow.py
@@ -2839,7 +2839,7 @@ def cond(pred, true_fn=None, false_fn=None, name=None, return_names=None):
     # Merge ture and false output if they are not None
     if return_names is None:
         is_dy2staic = False
-        return_names = ["no name"] * len(flatten(true_output))
+        return_names = ["no name"] * len(_to_sequence_except_dict(true_output))
     else:
         """
         dy2static will set the return_names and expand the return values to UndefinedVar.
@@ -2855,16 +2855,19 @@ def cond(pred, true_fn=None, false_fn=None, name=None, return_names=None):
             true_output, false_output, return_names
         )
 
-    if len(flatten(true_output)) != len(flatten(false_output)):
+    if len(_to_sequence_except_dict(true_output)) != len(
+        _to_sequence_except_dict(false_output)
+    ):
         raise ValueError(
             "true fn returns {} vars, but false fn returns {} vars, which is not equals".format(
-                len(to_sequence(true_output)), len(to_sequence(false_output))
+                len(_to_sequence_except_dict(true_output)),
+                len(_to_sequence_except_dict(false_output)),
             )
         )
     for true_out, false_out, return_name in zip(
-        flatten(true_output),
-        flatten(false_output),
-        flatten(return_names),
+        _to_sequence_except_dict(true_output),
+        _to_sequence_except_dict(false_output),
+        _to_sequence_except_dict(return_names),
     ):
         try:
             assert_same_structure(true_out, false_out, check_types=False)
@@ -2899,9 +2902,9 @@ def cond(pred, true_fn=None, false_fn=None, name=None, return_names=None):
                     )
 
     check_ret_none(
-        to_sequence(true_output),
-        to_sequence(false_output),
-        to_sequence(return_names),
+        _to_sequence_except_dict(true_output),
+        _to_sequence_except_dict(false_output),
+        _to_sequence_except_dict(return_names),
     )
 
     if is_dy2staic:
@@ -2922,9 +2925,9 @@ def cond(pred, true_fn=None, false_fn=None, name=None, return_names=None):
     merged_output = list(
         map(
             merge_every_var_list,
-            flatten(false_output),
-            flatten(true_output),
-            flatten(return_names),
+            _to_sequence_except_dict(false_output),
+            _to_sequence_except_dict(true_output),
+            _to_sequence_except_dict(return_names),
         )
     )
     merged_output = pack_sequence_as(false_output, flatten(merged_output))
@@ -2942,6 +2945,24 @@ def change_none_to_undefinedvar(nest1, nest2):
     nest1_out = pack_sequence_as(nest1, list(map(map_fn, flatten(nest1))))
     nest2_out = pack_sequence_as(nest2, list(map(map_fn, flatten(nest2))))
     return nest1_out, nest2_out
+
+
+def _to_sequence_except_dict(x):
+    """
+    In this function, dict is not viewed as sequence.
+    """
+    if isinstance(x, dict):
+        return [x]
+    return to_sequence(x)
+
+
+def _is_sequence_except_dict(x):
+    """
+    In this function, dict is not viewed as sequence.
+    """
+    if isinstance(x, dict):
+        return False
+    return is_sequence(x)
 
 
 def expand_undefined_var(nest1, nest2, names):
@@ -2987,24 +3008,24 @@ def expand_undefined_var(nest1, nest2, names):
     nest1_out = list(
         map(
             map_fn,
-            flatten(nest1),
-            flatten(nest2),
-            flatten(names),
-            [0 for i in flatten(names)],
+            _to_sequence_except_dict(nest1),
+            _to_sequence_except_dict(nest2),
+            _to_sequence_except_dict(names),
+            [0 for i in _to_sequence_except_dict(names)],
         )
     )
     nest2_out = list(
         map(
             map_fn,
-            flatten(nest2),
-            flatten(nest1),
-            flatten(names),
-            [1 for i in flatten(names)],
+            _to_sequence_except_dict(nest2),
+            _to_sequence_except_dict(nest1),
+            _to_sequence_except_dict(names),
+            [1 for i in _to_sequence_except_dict(names)],
         )
     )
-    if not is_sequence(nest1):
+    if not _is_sequence_except_dict(nest1):
         nest1_out = nest1_out[0]
-    if not is_sequence(nest2):
+    if not _is_sequence_except_dict(nest2):
         nest2_out = nest2_out[0]
     return nest1_out, nest2_out
 
