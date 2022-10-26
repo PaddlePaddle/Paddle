23b82a045e459a5e5de8f74fa6e1b14d15df3ca5
YangZhou <56786796+SmileGoat@users.noreply.github.com>
2022-10-26 12:37:46 +0800

    fix tess split fold

--- python/paddle/audio/datasets/esc50.py
+++ python/paddle/audio/datasets/esc50.py
@@ -136,12 +136,17 @@ class ESC50(AudioClassificationDataset):
         ('filename', 'fold', 'target', 'category', 'esc10', 'src_file', 'take'))
     audio_path = os.path.join('ESC-50-master', 'audio')
 
-    def __init__(self,
-                 mode: str = 'train',
-                 split: int = 1,
-                 feat_type: str = 'raw',
-                 archive=None,
-                 **kwargs):
+    def __init__(
+        self,
+        mode: str = 'train',
+        split: int = 1,
+        feat_type: str = 'raw',
+        archive=None,
+        **kwargs,
+    ):
+        assert split in range(
+            1, 6
+        ), f'The selected split should be integer, and 1 <= split <= 5, but got {split}'
         if archive is not None:
             self.archive = archive
         files, labels = self._get_data(mode, split)
