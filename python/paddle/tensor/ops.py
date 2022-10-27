92ba0197553b1fac7671d59eca45a8d1076ea861
zyfncg <zhangyunfei07@baidu.com>
2022-10-27 06:02:29 +0000

    fix example code of cosh

--- python/paddle/tensor/ops.py
+++ python/paddle/tensor/ops.py
@@ -640,12 +640,12 @@ def cosh(x, name=None):
      Examples:
          .. code-block:: python
 
-             import paddle
+            import paddle
 
-              x = paddle.to_tensor([-0.4, -0.2, 0.1, 0.3])
-             out = paddle.cosh(x)
-             print(out)
-             # [1.08107237 1.02006676 1.00500417 1.04533851]
+            x = paddle.to_tensor([-0.4, -0.2, 0.1, 0.3])
+            out = paddle.cosh(x)
+            print(out)
+            # [1.08107237 1.02006676 1.00500417 1.04533851]
 
     """
     if in_dygraph_mode():
