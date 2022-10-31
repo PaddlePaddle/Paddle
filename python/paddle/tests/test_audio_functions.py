1a495b3d58007f56eee2b80e7abdfc6d88ba1246
YangZhou <56786796+SmileGoat@users.noreply.github.com>
2022-10-31 10:43:40 +0800

    [audio] rm kaiser window in audio get_window function && rm audio utils (#47469)
    * rm kaiser window in audio window function
    
    * rm paddle audio utils which is redundant
    
    * rm kaiser in test_audio_functions.py
    Conflicts:
    	python/paddle/audio/utils/error.py
    	python/paddle/tests/test_audio_functions.py

--- python/paddle/tests/test_audio_functions.py
+++ python/paddle/tests/test_audio_functions.py
@@ -159,12 +159,6 @@ class TestAudioFuncitons(unittest.TestCase):
                                              window_paddle_exp.numpy(),
                                              decimal=5)
         try:
-            window_paddle = paddle.audio.functional.get_window(("kaiser", 1.0),
-                                                               self.n_fft)
-        except NotImplementedError:
-            pass
-
-        try:
             window_paddle = paddle.audio.functional.get_window("hann", -1)
         except ValueError:
             pass
