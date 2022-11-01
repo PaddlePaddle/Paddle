601626ac254fec6f7b238e818136591274eb6350
zyfncg <zhangyunfei07@baidu.com>
2022-11-01 16:12:39 +0800

    [cherry-pick][code-gen] Support code-gen for opmaker of sparse op (#46993) (#47417)
    * support generating code of opmaker for backward op invoke forward op (#46912)
    
    * [code-gen] Support code-gen for opmaker of sparse op (#46993)
    
    * support generating code of opmaker for backward op invoke forward op
    
    * gsupport code-gen of opmaker for sparse op
    
    * refind logic of choose phi kernrel
    
    * fix complie budg
    
    * fix code_gen bug
    
    * fix bug
    
    * fix kernel signature code-gen
    
    * fix complie bug of VarType
    
    * fix complie bug of VarType
    
    * fix test_sparse_conv_op
    
    * fix test_sparse_norm_op
    
    * [Phi] Refactor logic of judging whether having a phi kernrel (#46920)
    
    * refind logic of choose phi kernrel
    
    * fix complie budg
    
    * update cmake

--- python/paddle/sparse/nn/layer/norm.py
+++ python/paddle/sparse/nn/layer/norm.py
@@ -190,19 +190,20 @@ class BatchNorm(paddle.nn.BatchNorm1D):
             reserve_space = helper.create_variable_for_type_inference(
                 dtype=dtype, stop_gradient=True
             )
-            y = helper.create_sparse_variable_for_type_inference(dtype)
+            out = helper.create_sparse_variable_for_type_inference(dtype)
             outputs = {
-                "y": y,
+                "out": out,
                 "mean_out": mean_out,
                 "variance_out": variance_out,
                 "saved_mean": saved_mean,
                 "saved_variance": saved_variance,
                 "reserve_space": reserve_space,
             }
+
             helper.append_op(
                 type=op_type, inputs=inputs, outputs=outputs, attrs=attrs
             )
-            return y
+            return out
 
 
 class SyncBatchNorm(paddle.nn.SyncBatchNorm):
