8dc25c480a0f33e991b04857ed1a40ebb4625246
Wang Xin <xinwang614@gmail.com>
2022-11-02 10:05:55 +0800

    remove unused-variable warning in linux

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -153,7 +153,7 @@ if(NOT WIN32)
       -Wno-error=terminate # Warning in PADDLE_ENFORCE
       -Wno-error=int-in-bool-context # Warning in Eigen gcc 7.2
       -Wimplicit-fallthrough=0 # Warning in tinyformat.h
-      -Wno-error=maybe-uninitialized # Warning in boost gcc 7.2
+      -Wno-error=maybe-uninitialized # Warning in Paddle-Lite
       ${fsanitize})
 
   if(WITH_IPU)
