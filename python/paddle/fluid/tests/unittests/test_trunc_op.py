25d5ffbbc88f9f810c70fcfd27fab3b74d89d5cb
risemeup1 <515586620@qq.com>
2022-11-04 01:50:01 +0000

    fix bug ci

--- python/paddle/fluid/tests/unittests/test_trunc_op.py
+++ python/paddle/fluid/tests/unittests/test_trunc_op.py
@@ -1,3 +1,9 @@
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
 #  Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
