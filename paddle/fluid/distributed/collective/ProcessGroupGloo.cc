e1a1c35493d57035c857b7e1c5c9e8dff4ac9408
Wen Sun <35923278+HermitSun@users.noreply.github.com>
2022-11-07 10:19:53 +0800

    Refactor collective communication all_gather, all_reduce, broadcast & barrier C++ API (#47481)

--- paddle/fluid/distributed/collective/ProcessGroupGloo.cc
+++ paddle/fluid/distributed/collective/ProcessGroupGloo.cc
@@ -229,6 +229,17 @@ class BroadcastGlooTask : public ProcessGroupGloo::GlooTask {
   }
 };
 
+// TODO(sunyilun): for compatibility, will be updated later
+std::shared_ptr<ProcessGroup::Task> ProcessGroupGloo::Broadcast(
+    phi::DenseTensor* out_tensor,
+    const phi::DenseTensor& in_tensor,
+    const BroadcastOptions& opts,
+    bool sync_op) {
+  std::vector<phi::DenseTensor> in_wrapper = {in_tensor};
+  std::vector<phi::DenseTensor> out_wrapper = {*out_tensor};
+  return Broadcast(in_wrapper, out_wrapper, opts, true);
+}
+
 std::shared_ptr<ProcessGroup::Task> ProcessGroupGloo::Broadcast(
     std::vector<phi::DenseTensor>& inputs,
     std::vector<phi::DenseTensor>& outputs,
