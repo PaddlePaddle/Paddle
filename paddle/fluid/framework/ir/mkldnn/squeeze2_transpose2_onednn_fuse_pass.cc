13f7a2d27dac8f7b467e88194b8f1fbdb47a1c6a
Hui Zhang <zhtclz@foxmail.com>
2022-11-07 09:09:43 +0000

    format

--- paddle/fluid/framework/ir/mkldnn/squeeze2_transpose2_onednn_fuse_pass.cc
+++ paddle/fluid/framework/ir/mkldnn/squeeze2_transpose2_onednn_fuse_pass.cc
@@ -23,9 +23,10 @@ namespace ir {
 using string::PrettyLogDetail;
 
 void FuseSqueeze2Transpose2OneDNNPass::ApplyImpl(Graph *graph) const {
-  PADDLE_ENFORCE_NOT_NULL(graph,
-                          platform::errors::InvalidArgument(
-                              "Input graph pointer argument should not be nullptr."));
+  PADDLE_ENFORCE_NOT_NULL(
+      graph,
+      platform::errors::InvalidArgument(
+          "Input graph pointer argument should not be nullptr."));
 
   FusePassBase::Init("squeeze2_transpose2_onednn_fuse_pass", graph);
 
