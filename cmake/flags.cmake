ca33f8ab7d657348ca0aa72070e59682294bf505
Weilong Wu <veyron_wu@163.com>
2022-11-07 07:37:31 +0000

    CPUAllocator and fix mac compile

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -221,7 +221,7 @@ if(APPLE)
       -Werror=tautological-constant-out-of-range-compare
       -Werror=literal-conversion
       -Werror=pragma-pack
-      -lm)
+      -ll)
 endif()
 
 if(WITH_HETERPS AND WITH_PSLIB)
