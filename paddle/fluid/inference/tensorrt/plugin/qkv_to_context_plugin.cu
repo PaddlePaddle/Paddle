2ad07d380be6995047c5261e6569b437d5dc90e5
zhangxin81 <zhangxin81@baidu.com>
2022-11-08 10:23:24 +0800

    fix code style

--- paddle/fluid/inference/tensorrt/plugin/qkv_to_context_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/qkv_to_context_plugin.cu
@@ -21,12 +21,12 @@
 #include "glog/logging.h"
 #include "paddle/fluid/framework/tensor.h"
 #include "paddle/fluid/framework/tensor_util.h"
+#include "paddle/fluid/inference/tensorrt/plugin/common/common.cuh"
 #include "paddle/fluid/inference/tensorrt/plugin/qkv_to_context_plugin.h"
 #include "paddle/fluid/inference/tensorrt/plugin/trt_plugin_utils.h"
 #include "paddle/fluid/operators/math/bert_encoder_functor.h"
 #include "paddle/fluid/platform/device_context.h"
 #include "paddle/phi/kernels/funcs/blas/blas.h"
-#include "paddle/fluid/inference/tensorrt/plugin/common/common.cuh"
 
 namespace paddle {
 namespace inference {
