bc9bd786777121892729f9c2e2c7ae8d490283e4
Ryan <44900829+DrRyanHuang@users.noreply.github.com>
2022-11-14 13:22:24 +0800

    Update utils.py

--- python/paddle/vision/models/utils.py
+++ python/paddle/vision/models/utils.py
@@ -57,7 +57,7 @@ class IntermediateLayerGetter(nn.LayerDict):
 
         >>> m = paddle.vision.models.resnet18(pretrained=True)
         >>> # extract layer1 and layer3, giving as names `feat1` and feat2`
-        >>> new_m = paddle.vision.utils.IntermediateLayerGetter(m,
+        >>> new_m = paddle.vision.models.utils.IntermediateLayerGetter(m,
         >>>     {'layer1': 'feat1', 'layer3': 'feat2'})
         >>> out = new_m(paddle.rand([1, 3, 224, 224]))
         >>> print([(k, v.shape) for k, v in out.items()])
