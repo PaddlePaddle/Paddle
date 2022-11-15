d01db8b02cb0e6fe8c8211e03afe33596223d9e1
Wang Xin <xinwang614@gmail.com>
2022-11-15 19:35:43 +0800

    move "gpu_primitives.h" to phi

--- paddle/phi/kernels/gpu/nll_loss.h
+++ paddle/phi/kernels/gpu/nll_loss.h
@@ -20,7 +20,7 @@
 #include <string>
 
 #include "paddle/fluid/operators/math.h"
-#include "paddle/fluid/platform/device/gpu/gpu_primitives.h"
+#include "paddle/phi/backends/gpu/gpu_primitives.h"
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/hostdevice.h"
 
@@ -270,8 +270,8 @@ __global__ void GPUNLLLossForward2D_with_reduce(T* out_data,
       partial_sums, blockDim.x, acc_weight, thrust::plus<T>(), (T)0);
 
   if (threadIdx.x == 0) {
-    paddle::platform::CudaAtomicAdd(total_weight_data, acc_weight);
-    paddle::platform::CudaAtomicAdd(out_data, input_sum);
+    phi::CudaAtomicAdd(total_weight_data, acc_weight);
+    phi::CudaAtomicAdd(out_data, input_sum);
   }
 }
 
