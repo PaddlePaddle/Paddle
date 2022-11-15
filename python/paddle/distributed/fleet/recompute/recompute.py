dad79664f223372e19debb772f5b801e654025f7
wuhuachaocoding <huachaowu_ck@163.com>
2022-11-15 03:15:54 +0000

    upgrade recompute.

--- python/paddle/distributed/fleet/recompute/recompute.py
+++ python/paddle/distributed/fleet/recompute/recompute.py
@@ -619,6 +619,8 @@ def recompute(function, *args, **kwargs):
     """
     # Hack to mix *args with **kwargs in a python 2.7-compliant way
     preserve = kwargs.pop('preserve_rng_state', True)
+
+    # whether to use reentrant method to implement recompute
     use_reentrant = kwargs.pop('use_reentrant', True)
 
     if kwargs and use_reentrant:
