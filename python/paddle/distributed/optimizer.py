2611a251bd5734250cad9e826f21c0a4d5f41564
wangzhen38 <wangzhen38@baidu.com>
2022-11-16 03:17:17 +0000

    update

--- python/paddle/distributed/optimizer.py
+++ python/paddle/distributed/optimizer.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,11 +15,9 @@
 from paddle import framework
 from paddle.framework import core
 from paddle.common_ops_import import LayerHelper
-from paddle.fluid.clip import (
-    GradientClipByNorm,
-    append_gradient_clip_ops
-)
+from paddle.fluid.clip import GradientClipByNorm, append_gradient_clip_ops
 from paddle.optimizer import Optimizer
+
 
 class DGCMomentumOptimizer(Optimizer):
     r"""
@@ -400,7 +398,7 @@ class DGCMomentumOptimizer(Optimizer):
         encoded_var,
         gather_var,
     ):
-        block = paddle.static..default_main_program().global_block()
+        block = paddle.static.default_main_program().global_block()
         op_maker = core.op_proto_and_checker_maker
 
         regular_type = self.regular_type
