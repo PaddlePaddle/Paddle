d797b23e66ae48fbe43bf93f8ad3d0aa6dfb7be2
yunyaoXYY <imylovex1@163.com>
2022-11-16 12:59:17 +0000

    Clean fluid.layers.roi_pool

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -110,7 +110,6 @@ __all__ = [
     'pad',
     'pad_constant_like',
     'label_smooth',
-    'roi_pool',
     'roi_align',
     'dice_loss',
     'image_resize',
@@ -7218,123 +7217,6 @@ def label_smooth(
         attrs={"epsilon": float(epsilon)},
     )
     return smooth_label
-
-
-@templatedoc()
-def roi_pool(
-    input,
-    rois,
-    pooled_height=1,
-    pooled_width=1,
-    spatial_scale=1.0,
-    rois_num=None,
-    name=None,
-):
-    """
-
-    This operator implements the roi_pooling layer.
-    Region of interest pooling (also known as RoI pooling) is to perform max pooling on inputs of nonuniform sizes to obtain fixed-size feature maps (e.g. 7*7).
-
-    The operator has three steps:
-
-        1. Dividing each region proposal into equal-sized sections with the pooled_width and pooled_height;
-        2. Finding the largest value in each section;
-        3. Copying these max values to the output buffer.
-
-    For more information, please refer to https://stackoverflow.com/questions/43430056/what-is-roi-layer-in-fast-rcnn
-
-    Args:
-        input (Variable): Input feature, 4D-Tensor with the shape of [N,C,H,W], where N is the batch size, C is the input channel, H is Height, W is weight. The data type is float32 or float64.
-        rois (Variable): ROIs (Regions of Interest) to pool over. 2D-LoDTensor with the shape of [num_rois,4], the lod level is 1. Given as [[x1, y1, x2, y2], ...], (x1, y1) is the top left coordinates, and (x2, y2) is the bottom right coordinates.
-        pooled_height (int, optional): The pooled output height, data type is int32. Default: 1
-        pooled_width (int, optional): The pooled output height, data type is int32. Default: 1
-        spatial_scale (float, optional): Multiplicative spatial scale factor to translate ROI coords from their input scale to the scale used when pooling. Default: 1.0
-        rois_num (Tensor): The number of RoIs in each image. Default: None
-        name(str, optional): For detailed information, please refer
-            to :ref:`api_guide_Name`. Usually name is no need to set and
-            None by default.
-
-
-    Returns:
-        Variable: The pooled feature, 4D-Tensor with the shape of [num_rois, C, pooled_height, pooled_width].
-
-
-    Examples:
-
-    ..  code-block:: python
-
-        import paddle.fluid as fluid
-        import numpy as np
-        import paddle
-        paddle.enable_static()
-
-        DATATYPE='float32'
-
-        place = fluid.CPUPlace()
-        #place = fluid.CUDAPlace(0)
-
-        input_data = np.array([i for i in range(1,17)]).reshape(1,1,4,4).astype(DATATYPE)
-        roi_data =fluid.create_lod_tensor(np.array([[1., 1., 2., 2.], [1.5, 1.5, 3., 3.]]).astype(DATATYPE),[[2]], place)
-        rois_num_data = np.array([2]).astype('int32')
-
-        x = fluid.data(name='input', shape=[None,1,4,4], dtype=DATATYPE)
-        rois = fluid.data(name='roi', shape=[None,4], dtype=DATATYPE)
-        rois_num = fluid.data(name='rois_num', shape=[None], dtype='int32')
-
-        pool_out = fluid.layers.roi_pool(
-                input=x,
-                rois=rois,
-                pooled_height=1,
-                pooled_width=1,
-                spatial_scale=1.0,
-                rois_num=rois_num)
-
-        exe = fluid.Executor(place)
-        out, = exe.run(feed={'input':input_data ,'roi':roi_data, 'rois_num': rois_num_data}, fetch_list=[pool_out.name])
-        print(out)   #array([[[[11.]]], [[[16.]]]], dtype=float32)
-        print(np.array(out).shape)  # (2, 1, 1, 1)
-    """
-    if _non_static_mode():
-        assert (
-            rois_num is not None
-        ), "rois_num should not be None in dygraph mode."
-        pool_out, argmaxes = _legacy_C_ops.roi_pool(
-            input,
-            rois,
-            rois_num,
-            "pooled_height",
-            pooled_height,
-            "pooled_width",
-            pooled_width,
-            "spatial_scale",
-            spatial_scale,
-        )
-        return pool_out, argmaxes
-
-    check_variable_and_dtype(input, 'input', ['float32'], 'roi_pool')
-    check_variable_and_dtype(rois, 'rois', ['float32'], 'roi_pool')
-    helper = LayerHelper('roi_pool', **locals())
-    dtype = helper.input_dtype()
-    pool_out = helper.create_variable_for_type_inference(dtype)
-    argmaxes = helper.create_variable_for_type_inference(dtype='int32')
-
-    inputs = {
-        "X": input,
-        "ROIs": rois,
-    }
-    if rois_num is not None:
-        inputs['RoisNum'] = rois_num
-    helper.append_op(
-        type="roi_pool",
-        inputs=inputs,
-        outputs={"Out": pool_out, "Argmax": argmaxes},
-        attrs={
-            "pooled_height": pooled_height,
-            "pooled_width": pooled_width,
-            "spatial_scale": spatial_scale,
-        },
-    )
-    return pool_out
 
 
 @templatedoc()
--- python/paddle/fluid/tests/unittests/test_layers.py
+++ python/paddle/fluid/tests/unittests/test_layers.py
@@ -4375,41 +4375,6 @@ class TestBook(LayerTest):
             )
             return out
 
-    def test_roi_pool(self):
-        x_np = np.random.rand(2, 3, 8, 8).astype('float32')
-        rois_np = np.random.rand(3, 4).astype('float32')
-        rois_num_np = np.array([1, 2]).astype('int32')
-
-        with self.static_graph():
-            x = layers.data(name="x", shape=[3, 8, 8], dtype="float32")
-            rois = layers.data(name="rois", shape=[4], dtype="float32")
-            rois_num = fluid.data(name="rois_num", shape=[None], dtype="int32")
-            output = layers.roi_pool(x, rois, 4, 4, 0.5, rois_num=rois_num)
-            static_res = self.get_static_graph_result(
-                feed={'x': x_np, 'rois': rois_np, 'rois_num': rois_num_np},
-                fetch_list=[output],
-            )[0]
-
-        with self.dynamic_graph():
-            with _test_eager_guard():
-                x_dy = base.to_variable(x_np)
-                rois_dy = base.to_variable(rois_np)
-                rois_num_dy = base.to_variable(rois_num_np)
-                dy_eager_res = layers.roi_pool(
-                    x_dy, rois_dy, 4, 4, 0.5, rois_num=rois_num_dy
-                )
-                dy_eager_res_value = dy_eager_res[0].numpy()
-
-            x_dy = base.to_variable(x_np)
-            rois_dy = base.to_variable(rois_np)
-            rois_num_dy = base.to_variable(rois_num_np)
-            dy_res = layers.roi_pool(
-                x_dy, rois_dy, 4, 4, 0.5, rois_num=rois_num_dy
-            )
-            dy_res_value = dy_res[0].numpy()
-        np.testing.assert_array_equal(static_res, dy_res_value)
-        np.testing.assert_array_equal(static_res, dy_eager_res_value)
-
     def test_sequence_enumerate(self):
         # TODO(minqiyang): dygraph do not support lod now
         with self.static_graph():
