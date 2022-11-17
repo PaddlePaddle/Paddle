c0203f89f1f940e3551d94ffd3ecf259e2ec9450
wangzhen38 <wangzhen38@baidu.com>
2022-11-17 02:17:33 +0000

    [remove fluid] under fleet meta_optimizers_wz

--- python/paddle/distributed/fleet/meta_optimizers/meta_optimizer_base.py
+++ python/paddle/distributed/fleet/meta_optimizers/meta_optimizer_base.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from paddle.optimizer import Optimizer
+from paddle.fluid.optimizer import Optimizer
 
 __all__ = []
 
