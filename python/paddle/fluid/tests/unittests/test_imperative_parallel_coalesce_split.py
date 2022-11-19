5eee353c2d452e22fab61ebb55fe0b0639212cc4
GhostScreaming <mofengshenjieII@163.com>
2022-11-19 04:43:11 +0000

    Fix some bugs.

--- python/paddle/fluid/tests/unittests/test_imperative_parallel_coalesce_split.py
+++ python/paddle/fluid/tests/unittests/test_imperative_parallel_coalesce_split.py
@@ -21,11 +21,6 @@ from paddle.fluid import core
 from paddle.fluid.dygraph.parallel import DataParallel
 from paddle.fluid.dygraph.base import to_variable
 
-# from paddle.fluid.dygraph.parallel import (
-#     _coalesce_tensors,
-#     _split_tensors,
-#     _reshape_inplace,
-# )
 from paddle.distributed.parallel_env import (
     _coalesce_tensors,
     _split_tensors,
