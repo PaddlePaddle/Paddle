4b454fb27cc30951a7e97b1603bb845f53334f64
liuyuang <liuyuang@baidu.com>
2022-11-22 08:41:37 +0800

    add cudnn version check for bfloat16

--- paddle/phi/kernels/gpudnn/conv_kernel.cu
+++ paddle/phi/kernels/gpudnn/conv_kernel.cu
@@ -374,11 +374,15 @@ void ConvCudnnKernel(const Context& ctx,
   // HIP MIOPEN ONLY SUPPORT NCHW format
   auto compute_format = paddle::platform::DataLayout::kNCHW;
 #else
+#if CUDNN_VERSION_MIN(8, 1, 0)
   // Tensor Core introduced from Volta GPUs supports more faster conv op
   // with FP16 in NHWC data format.
   const bool compute_in_nhwc =
       (dtype == CUDNN_DATA_HALF || dtype == CUDNN_DATA_BFLOAT16) &&
       IsVoltaOrLater(ctx);
+#else
+  const bool compute_in_nhwc = dtype == CUDNN_DATA_HALF && IsVoltaOrLater(ctx);
+#endif
   // We will only do data format conversion from NHWC to NCHW.
   // cudnn will convert NCHW to NHWC automatically on Tensor Core.
   auto compute_format = compute_in_nhwc && channel_last
