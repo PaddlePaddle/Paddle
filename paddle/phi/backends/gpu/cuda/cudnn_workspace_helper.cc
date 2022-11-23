448fc315d48a59d7dd8b520e0b6bb4fb2b39c21e
PuQing <me@puqing.work>
2022-11-23 18:00:02 +0800

    move helper to gpu/cuda

--- paddle/phi/backends/gpu/cuda/cudnn_workspace_helper.cc
+++ paddle/phi/backends/gpu/cuda/cudnn_workspace_helper.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/core/cudnn_workspace_helper.h"
+#include "paddle/phi/backends/gpu/cuda/cudnn_workspace_helper.h"
 
 #include <cstdlib>
 #include <string>
