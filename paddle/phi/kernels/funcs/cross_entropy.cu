eb69017ef6b969eeab85c37335292fea3a8b2210
sneaxiy <sneaxiy@126.com>
2022-11-23 23:54:42 +0800

    fix windows compile error

--- paddle/phi/kernels/funcs/cross_entropy.cu
+++ paddle/phi/kernels/funcs/cross_entropy.cu
@@ -14,6 +14,7 @@ limitations under the License. */
 
 #include "paddle/phi/kernels/funcs/cross_entropy.h"
 
+#include "paddle/fluid/platform/device/gpu/gpu_dnn.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/backends/gpu/gpu_device_function.h"
 #include "paddle/phi/backends/gpu/gpu_primitives.h"
