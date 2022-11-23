2863f7d05ccac6137e496eb2079a2390a99672c9
“201716010711” <3030364995@qq.com>
2022-11-23 13:59:05 +0000

    transfer slice api2

--- python/paddle/fluid/contrib/layers/nn.py
+++ python/paddle/fluid/contrib/layers/nn.py
@@ -36,7 +36,6 @@ from paddle.fluid import core
 from paddle.fluid.param_attr import ParamAttr
 
 from paddle.fluid.framework import Variable, convert_np_dtype_to_dtype_
-from paddle.fluid.layers import slice
 import paddle
 import warnings
 from paddle import _C_ops, _legacy_C_ops
