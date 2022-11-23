d70aaaaae4a813fda4abe69ab6e3fb60897d581a
sneaxiy <sneaxiy@126.com>
2022-11-23 18:44:28 +0800

    fix ci compile

--- paddle/phi/kernels/funcs/cross_entropy.h
+++ paddle/phi/kernels/funcs/cross_entropy.h
@@ -15,6 +15,7 @@ limitations under the License. */
 #pragma once
 #include <limits>
 
+#include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/common/float16.h"
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/hostdevice.h"
