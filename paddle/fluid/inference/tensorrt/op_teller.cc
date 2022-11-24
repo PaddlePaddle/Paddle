8dca558ad56a7b93686ef5a789ffbec8d64c40b5
linsheng01 <linsheng01@baidu.com>
2022-11-24 04:04:39 +0000

    add less_than and greater_than trt converter

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -2190,6 +2190,32 @@ struct SimpleOpTypeSetTeller : public Teller {
     }
 #endif
 
+    if (op_type == "less_than" || op_type == "greater_than") {
+#if !IS_TRT_VERSION_GE(8400)
+      std::cout << "trt version -------------" << std::endl;
+      VLOG(3)
+          << "less_than and greater_than is not supported when TensorRT < 8.4";
+      return false;
+#else
+      if (!with_dynamic_shape) {
+        VLOG(3) << "the less_than and greater_than does not support "
+                   "static shape yet";
+        return false;
+      }
+      int axis = PADDLE_GET_CONST(int, desc.GetAttr("axis"));
+      if (axis == 0) {
+        return false;
+      }
+      auto* block = desc.Block();
+      if (block == nullptr) {
+        VLOG(3) << "The block desc is nullptr, we can't continue to analyze. "
+                   "Developers need to check whether block_desc is passed in "
+                   "the pass.";
+        return false;
+      }
+#endif
+    }
+
     if (op_type == "equal") {
 #if !IS_TRT_VERSION_GE(8000)
       VLOG(3) << "compare is not supported when TensorRT < 8.0";
@@ -2327,6 +2353,8 @@ struct SimpleOpTypeSetTeller : public Teller {
       "elementwise_min",
       "elementwise_max",
       "elementwise_floordiv",
+      "less_than",
+      "greater_than",
       "equal",
       "dropout",
       "fill_any_like",
@@ -2454,6 +2482,8 @@ struct SimpleOpTypeSetTeller : public Teller {
       "elementwise_min",
       "elementwise_max",
       "elementwise_floordiv",
+      "less_than",
+      "greater_than",
       "equal",
       "dropout",
       "fill_any_like",
