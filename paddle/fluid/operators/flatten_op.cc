c377bb09f78d804d57513ce26bdd40de3515c971
jakpiase <jakpia21@gmail.com>
2022-11-24 22:19:28 +0100

    re-enabled reshape, squeeze and flatten kernels

--- paddle/fluid/operators/flatten_op.cc
+++ paddle/fluid/operators/flatten_op.cc
@@ -87,6 +87,15 @@ class FlattenOp : public framework::OperatorWithKernel {
       const framework::ExecutionContext &ctx) const override {
     auto input_data_type =
         framework::OperatorWithKernel::IndicateVarDataType(ctx, "X");
+
+    #ifdef PADDLE_WITH_MKLDNN
+       if (this->CanMKLDNNBeUsed(ctx, input_data_type)) {
+         return framework::OpKernelType(input_data_type, ctx.GetPlace(),
+                                        phi::DataLayout::ONEDNN,
+                                        framework::LibraryType::kMKLDNN);
+       }
+    #endif
+
     return framework::OpKernelType(input_data_type, ctx.GetPlace());
   }
 };
@@ -159,6 +168,15 @@ class FlattenGradOp : public framework::OperatorWithKernel {
       const framework::ExecutionContext &ctx) const override {
     auto input_data_type = framework::OperatorWithKernel::IndicateVarDataType(
         ctx, framework::GradVarName("Out"));
+
+    #ifdef PADDLE_WITH_MKLDNN
+       if (this->CanMKLDNNBeUsed(ctx, input_data_type)) {
+         return framework::OpKernelType(input_data_type, ctx.GetPlace(),
+                                        phi::DataLayout::ONEDNN,
+                                        framework::LibraryType::kMKLDNN);
+       }
+    #endif
+
     return framework::OpKernelType(input_data_type, ctx.GetPlace());
   }
 };
@@ -223,6 +241,15 @@ class Flatten2Op : public framework::OperatorWithKernel {
       const framework::ExecutionContext &ctx) const override {
     auto input_data_type =
         framework::OperatorWithKernel::IndicateVarDataType(ctx, "X");
+
+    #ifdef PADDLE_WITH_MKLDNN
+       if (this->CanMKLDNNBeUsed(ctx, input_data_type)) {
+         return framework::OpKernelType(input_data_type, ctx.GetPlace(),
+                                        phi::DataLayout::ONEDNN,
+                                        framework::LibraryType::kMKLDNN);
+       }
+    #endif
+
     return framework::OpKernelType(input_data_type, ctx.GetPlace());
   }
 };
@@ -275,6 +302,15 @@ class Flatten2GradOp : public framework::OperatorWithKernel {
       const framework::ExecutionContext &ctx) const override {
     auto input_data_type = framework::OperatorWithKernel::IndicateVarDataType(
         ctx, framework::GradVarName("Out"));
+
+    #ifdef PADDLE_WITH_MKLDNN
+       if (this->CanMKLDNNBeUsed(ctx, input_data_type)) {
+         return framework::OpKernelType(input_data_type, ctx.GetPlace(),
+                                        phi::DataLayout::ONEDNN,
+                                        framework::LibraryType::kMKLDNN);
+       }
+    #endif
+
     return framework::OpKernelType(input_data_type, ctx.GetPlace());
   }
 };
--- paddle/fluid/operators/reshape_op.cc
+++ paddle/fluid/operators/reshape_op.cc
@@ -258,6 +258,14 @@ class ReshapeOp : public framework::OperatorWithKernel {
     auto input_data_type =
         framework::OperatorWithKernel::IndicateVarDataType(ctx, "X");
 
+    #ifdef PADDLE_WITH_MKLDNN
+       if (this->CanMKLDNNBeUsed(ctx, input_data_type)) {
+         return framework::OpKernelType(input_data_type, ctx.GetPlace(),
+                                        phi::DataLayout::ONEDNN,
+                                        framework::LibraryType::kMKLDNN);
+       }
+    #endif
+
     return framework::OpKernelType(input_data_type, ctx.GetPlace());
   }
 
@@ -617,6 +625,14 @@ class Reshape2GradOp : public framework::OperatorWithKernel {
       const framework::ExecutionContext &ctx) const override {
     auto input_data_type = framework::OperatorWithKernel::IndicateVarDataType(
         ctx, framework::GradVarName("Out"));
+
+    #ifdef PADDLE_WITH_MKLDNN
+       if (this->CanMKLDNNBeUsed(ctx, input_data_type)) {
+         return framework::OpKernelType(input_data_type, ctx.GetPlace(),
+                                        phi::DataLayout::ONEDNN,
+                                        framework::LibraryType::kMKLDNN);
+       }
+    #endif
 
     return framework::OpKernelType(input_data_type, ctx.GetPlace());
   }
