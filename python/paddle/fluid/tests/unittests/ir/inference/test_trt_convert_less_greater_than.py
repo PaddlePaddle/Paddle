ba77f73e6514b2d44a35a64db44cc407429239aa
linsheng01 <linsheng01@baidu.com>
2022-11-24 21:04:29 +0000

    fix2 less_than greater_than trt converter

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_less_greater_than.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_less_greater_than.py
@@ -104,7 +104,7 @@ class TrtConvertElementwiseTest_one_input_special_case0(TrtLayerAutoScanTest):
             if dynamic_shape:
                 ver = paddle_infer.get_trt_compile_version()
                 if ver[0] * 1000 + ver[1] * 100 + ver[0] * 10 < 8400:
-                    return 0, 2
+                    return 0, 4
                 return 1, 2
             return 0, 4
 
