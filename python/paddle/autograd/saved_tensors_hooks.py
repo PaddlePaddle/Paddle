931daad22c7dd8c44d70105d48d20cf8a9107f28
dmgy000 <60970491+dmgy000@users.noreply.github.com>
2022-11-24 16:15:31 +0800

    test grep; test=document_fix

--- python/paddle/autograd/saved_tensors_hooks.py
+++ python/paddle/autograd/saved_tensors_hooks.py
@@ -21,7 +21,7 @@ class saved_tensors_hooks:
     """
     Dynamic graph, registers a pair of pack / unpack hooks for saved tensors. change here
     
-    documentation preview where's paddle-bot preview
+    documentation preview where's paddle-bot preview. test grep
 
     Parameters:
         pack_hook (function): The pack hook will be called every time the forward
