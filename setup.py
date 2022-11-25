9a545a844cc0ca75ce435b7dbc39b8af0681fbeb
risemeup1 <515586620@qq.com>
2022-11-25 14:30:51 +0000

    debug

--- setup.py
+++ setup.py
@@ -1406,9 +1406,6 @@ def main():
     if os.path.exists('${SETUP_LOG_FILE}'):
         os.system('grep -v "purelib" ${SETUP_LOG_FILE}')
     # print_info_of_reminding(build_help_message)
-    import paddle
-
-    paddle.utils.run_check()
 
 
 if __name__ == '__main__':
