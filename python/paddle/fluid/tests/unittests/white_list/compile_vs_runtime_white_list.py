301465d17d3fe6e9824cf170e29211567e1b6b15
HydrogenSulfate <490868991@qq.com>
2022-11-25 05:02:05 +0000

    remove retinanet_detection_output

--- python/paddle/fluid/tests/unittests/white_list/compile_vs_runtime_white_list.py
+++ python/paddle/fluid/tests/unittests/white_list/compile_vs_runtime_white_list.py
@@ -23,7 +23,6 @@ COMPILE_RUN_OP_WHITE_LIST = [
     'sequence_slice',
     'generate_proposals',
     'mine_hard_examples',
-    'retinanet_detection_output',
     'ctc_align',
     'fusion_seqpool_cvm_concat',
     'gru',
