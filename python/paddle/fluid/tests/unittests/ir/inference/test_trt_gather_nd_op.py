533c50301449293efa4c63afeaddcd557e1f0e84
Vvsmile <17864154871@163.com>
2022-11-25 09:10:10 +0000

    fix code style of gather_nd

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_gather_nd_op.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_gather_nd_op.py
@@ -17,10 +17,10 @@ import unittest
 import numpy as np
 from inference_pass_test import InferencePassTest
 
+import paddle
 import paddle.fluid as fluid
 import paddle.fluid.core as core
 from paddle.fluid.core import AnalysisConfig, PassVersionChecker
-import paddle
 
 
 class TRTGatherNdTest(InferencePassTest):
