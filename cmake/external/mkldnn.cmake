09e7c39e8d1aa35db6fc6499e2cca720e8147c66
PaulinaGacek <paulina.gacek@intel.com>
2022-11-25 15:29:28 +0100

    OneDNN version to 2.7.2

--- cmake/external/mkldnn.cmake
+++ cmake/external/mkldnn.cmake
@@ -21,7 +21,7 @@ set(MKLDNN_INC_DIR
     "${MKLDNN_INSTALL_DIR}/include"
     CACHE PATH "mkldnn include directory." FORCE)
 set(MKLDNN_REPOSITORY ${GIT_URL}/oneapi-src/oneDNN.git)
-set(MKLDNN_TAG 9b186765dded79066e0cd9c17eb70b680b76fb8e)
+set(MKLDNN_TAG fbec3e25a559ee252022ae066817b204e106a6ba)
 
 # Introduce variables:
 # * CMAKE_INSTALL_LIBDIR
