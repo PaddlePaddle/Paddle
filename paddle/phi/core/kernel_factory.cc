5e6eebfbda80c45dc482376384dad7abfa5311e0
huangjiyi <947613776@qq.com>
2022-11-27 22:02:09 +0800

    fix PR-CI-Kunlun

--- paddle/phi/core/kernel_factory.cc
+++ paddle/phi/core/kernel_factory.cc
@@ -151,7 +151,7 @@ KernelResult KernelFactory::SelectKernelOrThrowError(
 #if defined(PADDLE_WITH_XPU) && !defined(PADDLE_WITH_XPU_KP)
   VLOG(6) << "fluid_op_name: " << TransToFluidOpName(kernel_name);
   if ((FLAGS_enable_api_kernel_fallback && kernel_iter == iter->second.end()) ||
-      paddle::platform::is_in_xpu_black_list(TransToFluidOpName(kernel_name))
+      phi::backends::xpu::is_in_xpu_black_list(TransToFluidOpName(kernel_name))
 #else
   if ((FLAGS_enable_api_kernel_fallback && kernel_iter == iter->second.end())
 #endif
