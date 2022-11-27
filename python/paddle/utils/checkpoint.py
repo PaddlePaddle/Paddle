e35e20e29b9d0e14fb3e2991f5d6ebfd79c63a0c
GGBond8488 <857631483@qq.com>
2022-11-27 15:54:32 +0000

    remove fluid in save_dygrapg

--- python/paddle/utils/checkpoint.py
+++ python/paddle/utils/checkpoint.py
@@ -73,19 +73,17 @@ def save_dygraph(state_dict, model_path):
         .. code-block:: python
 
             import paddle
-            import paddle.fluid as fluid
 
-            with fluid.dygraph.guard():
-                emb = fluid.dygraph.Embedding([10, 10])
+            emb = paddle.nn.Embedding([10, 10])
 
-                state_dict = emb.state_dict()
-                paddle.utils.save_dygraph( state_dict, "paddle_dy")
+            state_dict = emb.state_dict()
+            paddle.utils.save_dygraph( state_dict, "paddle_dy")
 
-                adam = fluid.optimizer.Adam( learning_rate = fluid.layers.noam_decay( 100, 10000),
-                                             parameter_list = emb.parameters() )
+            adam = paddle.optimizer.Adam(learning_rate = paddle.nn.layers.noam_decay( 100, 10000),
+                                         parameter_list = emb.parameters() )
 
-                state_dict = adam.state_dict()
-                paddle.utils.save_dygraph( state_dict, "paddle_dy")
+            state_dict = adam.state_dict()
+            paddle.utils.save_dygraph( state_dict, "paddle_dy")
 
     '''
 
