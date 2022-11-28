0389e887ad3a36f4bf58584367a5697c8c3d1569
CjhHa1 <cjh18671720497@outlook.com>
2022-11-28 11:21:45 +0800

    add annotations

--- python/paddle/utils/flops.py
+++ python/paddle/utils/flops.py
@@ -62,6 +62,10 @@ def register_flops(op_type):
 
 @register_flops("c_embedding")
 def _c_embedding_flops(input_shapes, **attrs):
+    """FLOPs computation for c_embedding op.
+    For c_embedding(input):
+        equation: flops = 0
+    """
     return 0
 
 
