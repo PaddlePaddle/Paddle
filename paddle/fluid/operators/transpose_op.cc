c26e9790e21562983dd6403b6cb1f6372a54d1b2
Netpunk <2327994230@qq.com>
2022-11-28 15:08:43 +0800

    delete transpose_op.h

--- paddle/fluid/operators/transpose_op.cc
+++ paddle/fluid/operators/transpose_op.cc
@@ -12,7 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/operators/transpose_op.h"
+#include "paddle/fluid/framework/op_registry.h"
+#include "paddle/phi/kernels/funcs/transpose_functor.h"
 
 #include <memory>
 #include <string>
