bd425104f7b42074ae494f60d935cc48b7d30dda
SigureMo <sigure.qaq@gmail.com>
2022-11-29 15:28:48 +0800

    revert conflicting files

--- python/paddle/fluid/tests/unittests/test_pool3d_op.py
+++ python/paddle/fluid/tests/unittests/test_pool3d_op.py
@@ -13,13 +13,12 @@
 # limitations under the License.
 
 import unittest
-
 import numpy as np
-from op_test import OpTest
 
 import paddle
-import paddle.fluid as fluid
 import paddle.fluid.core as core
+from op_test import OpTest
+import paddle.fluid as fluid
 
 
 def adaptive_start_index(index, input_size, output_size):
