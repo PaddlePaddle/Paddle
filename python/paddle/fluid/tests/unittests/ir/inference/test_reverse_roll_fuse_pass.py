a1461c385c64c2aadd893b6bbae33226c2d91814
wwbitejotunn <wang_bojun@outlook.com>
2022-11-29 07:25:11 +0000

    ut code style update

--- python/paddle/fluid/tests/unittests/ir/inference/test_reverse_roll_fuse_pass.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_reverse_roll_fuse_pass.py
@@ -12,14 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from auto_scan_test import PassAutoScanTest
-from program_config import TensorConfig, ProgramConfig, OpConfig
-import numpy as np
-import paddle.inference as paddle_infer
-from functools import partial
-import unittest
-import hypothesis.strategies as st
 import math
+import unittest
+from functools import partial
+
+import hypothesis.strategies as st
+import numpy as np
+from auto_scan_test import PassAutoScanTest
+from program_config import OpConfig, ProgramConfig, TensorConfig
+
+import paddle.inference as paddle_infer
 
 
 class ReverseRollPass(PassAutoScanTest):
@@ -48,14 +50,19 @@ class ReverseRollPass(PassAutoScanTest):
             min_subgraph_size=0,
             precision_mode=paddle_infer.PrecisionType.Float32,
             use_static=False,
-            use_calib_mode=False)
-        config.set_trt_dynamic_shape_info({
-            "input0": [64, 9, 96],
-        }, {
-            "input0": [512, 144, 768],
-        }, {
-            "input0": [64, 49, 96],
-        })
+            use_calib_mode=False,
+        )
+        config.set_trt_dynamic_shape_info(
+            {
+                "input0": [64, 9, 96],
+            },
+            {
+                "input0": [512, 144, 768],
+            },
+            {
+                "input0": [64, 49, 96],
+            },
+        )
 
         yield config, ['reverse_roll'], (1e-5, 1e-5)
 
@@ -67,14 +74,19 @@ class ReverseRollPass(PassAutoScanTest):
             min_subgraph_size=0,
             precision_mode=paddle_infer.PrecisionType.Half,
             use_static=False,
-            use_calib_mode=False)
-        config.set_trt_dynamic_shape_info({
-            "input0": [64, 9, 96],
-        }, {
-            "input0": [512, 144, 768],
-        }, {
-            "input0": [64, 49, 96],
-        })
+            use_calib_mode=False,
+        )
+        config.set_trt_dynamic_shape_info(
+            {
+                "input0": [64, 9, 96],
+            },
+            {
+                "input0": [512, 144, 768],
+            },
+            {
+                "input0": [64, 49, 96],
+            },
+        )
 
         yield config, ['reverse_roll'], (1e-3, 1e-3)
 
@@ -85,102 +97,124 @@ class ReverseRollPass(PassAutoScanTest):
         window_number = 64
 
         def generate_input(attrs):
-            return np.random.random([
-                attrs[0]["batch_size"] * attrs[1]["window_number"],
-                attrs[1]["window_size"] * attrs[1]["window_size"],
-                attrs[1]["dim"]
-            ]).astype(np.float32)
+            return np.random.random(
+                [
+                    attrs[0]["batch_size"] * attrs[1]["window_number"],
+                    attrs[1]["window_size"] * attrs[1]["window_size"],
+                    attrs[1]["dim"],
+                ]
+            ).astype(np.float32)
 
-        attrs = [{
-            "batch_size": batch_size
-        }, {
-            "window_number": window_number,
-            "window_size": window_size,
-            "dim": dim
-        }]
+        attrs = [
+            {"batch_size": batch_size},
+            {
+                "window_number": window_number,
+                "window_size": window_size,
+                "dim": dim,
+            },
+        ]
         reshape2_00 = OpConfig(
             type="reshape2",
             inputs={"X": ["input0"]},
             outputs={
                 "Out": ["reshape2_00_out"],
-                "XShape": ["reshape2_00_outXshape"]
+                "XShape": ["reshape2_00_outXshape"],
             },
-            attrs={"shape": [-1, window_size, window_size, dim]})
-        reshape2_10 = OpConfig(type="reshape2",
-                               inputs={"X": ["reshape2_00_out"]},
-                               outputs={
-                                   "Out": ["reshape2_10_out"],
-                                   "XShape": ["reshape2_10_outXshape"]
-                               },
-                               attrs={
-                                   "shape": [
-                                       -1,
-                                       int(math.sqrt(window_number)),
-                                       int(math.sqrt(window_number)),
-                                       window_size, window_size, dim
-                                   ]
-                               })
-        transpose2_20 = OpConfig(type="transpose2",
-                                 inputs={"X": ["reshape2_10_out"]},
-                                 outputs={
-                                     "Out": ["transpose2_20_out"],
-                                     "XShape": ["transpose2_20_outXshape"]
-                                 },
-                                 attrs={"axis": [0, 1, 3, 2, 4, 5]})
+            attrs={"shape": [-1, window_size, window_size, dim]},
+        )
+        reshape2_10 = OpConfig(
+            type="reshape2",
+            inputs={"X": ["reshape2_00_out"]},
+            outputs={
+                "Out": ["reshape2_10_out"],
+                "XShape": ["reshape2_10_outXshape"],
+            },
+            attrs={
+                "shape": [
+                    -1,
+                    int(math.sqrt(window_number)),
+                    int(math.sqrt(window_number)),
+                    window_size,
+                    window_size,
+                    dim,
+                ]
+            },
+        )
+        transpose2_20 = OpConfig(
+            type="transpose2",
+            inputs={"X": ["reshape2_10_out"]},
+            outputs={
+                "Out": ["transpose2_20_out"],
+                "XShape": ["transpose2_20_outXshape"],
+            },
+            attrs={"axis": [0, 1, 3, 2, 4, 5]},
+        )
         reshape2_30 = OpConfig(
             type="reshape2",
             inputs={"X": ["transpose2_20_out"]},
             outputs={
                 "Out": ["reshape2_30_out"],
-                "XShape": ["reshape2_30_outXshape"]
+                "XShape": ["reshape2_30_outXshape"],
             },
             attrs={
                 "shape": [
                     -1,
                     int(math.sqrt(window_number)) * window_size,
-                    int(math.sqrt(window_number)) * window_size, dim
+                    int(math.sqrt(window_number)) * window_size,
+                    dim,
                 ]
-            })
+            },
+        )
         roll_30_1 = OpConfig(
             type="roll",
             inputs={"X": ["reshape2_30_out"]},
             outputs={"Out": ["roll_30_1_out"]},
             attrs={
                 "axis": [1, 2],
-                "shifts":
-                [math.floor(window_size // 2),
-                 math.floor(window_size // 2)]
-            })
+                "shifts": [
+                    math.floor(window_size // 2),
+                    math.floor(window_size // 2),
+                ],
+            },
+        )
         reshape2_40 = OpConfig(
             type="reshape2",
             inputs={"X": ["roll_30_1_out"]},
             outputs={
                 "Out": ["reshape2_40_out"],
-                "XShape": ["reshape2_40_outXshape"]
+                "XShape": ["reshape2_40_outXshape"],
             },
             attrs={
                 "shape": [-1, window_number * window_size * window_size, dim]
-            })
+            },
+        )
 
         program_config = ProgramConfig(
             ops=[
-                reshape2_00, reshape2_10, transpose2_20, reshape2_30, roll_30_1,
-                reshape2_40
+                reshape2_00,
+                reshape2_10,
+                transpose2_20,
+                reshape2_30,
+                roll_30_1,
+                reshape2_40,
             ],
             weights={},
             inputs={
                 "input0": TensorConfig(data_gen=partial(generate_input, attrs)),
             },
-            outputs=["reshape2_40_out"])
+            outputs=["reshape2_40_out"],
+        )
 
         return program_config
 
     def test(self):
-        self.run_and_statis(quant=False,
-                            max_examples=50,
-                            passes=["reverse_roll_fuse_pass"],
-                            max_duration=250,
-                            min_success_num=50)
+        self.run_and_statis(
+            quant=False,
+            max_examples=50,
+            passes=["reverse_roll_fuse_pass"],
+            max_duration=250,
+            min_success_num=50,
+        )
 
 
 class ReverseRoll2Pass(PassAutoScanTest):
@@ -206,14 +240,19 @@ class ReverseRoll2Pass(PassAutoScanTest):
             min_subgraph_size=0,
             precision_mode=paddle_infer.PrecisionType.Float32,
             use_static=False,
-            use_calib_mode=False)
-        config.set_trt_dynamic_shape_info({
-            "input0": [64, 9, 96],
-        }, {
-            "input0": [512, 144, 768],
-        }, {
-            "input0": [64, 49, 96],
-        })
+            use_calib_mode=False,
+        )
+        config.set_trt_dynamic_shape_info(
+            {
+                "input0": [64, 9, 96],
+            },
+            {
+                "input0": [512, 144, 768],
+            },
+            {
+                "input0": [64, 49, 96],
+            },
+        )
 
         yield config, ['reverse_roll'], (1e-5, 1e-5)
 
@@ -225,14 +264,19 @@ class ReverseRoll2Pass(PassAutoScanTest):
             min_subgraph_size=0,
             precision_mode=paddle_infer.PrecisionType.Half,
             use_static=False,
-            use_calib_mode=False)
-        config.set_trt_dynamic_shape_info({
-            "input0": [64, 9, 96],
-        }, {
-            "input0": [512, 144, 768],
-        }, {
-            "input0": [64, 49, 96],
-        })
+            use_calib_mode=False,
+        )
+        config.set_trt_dynamic_shape_info(
+            {
+                "input0": [64, 9, 96],
+            },
+            {
+                "input0": [512, 144, 768],
+            },
+            {
+                "input0": [64, 49, 96],
+            },
+        )
 
         yield config, ['reverse_roll'], (1e-3, 1e-3)
 
@@ -243,92 +287,111 @@ class ReverseRoll2Pass(PassAutoScanTest):
         window_number = 64
 
         def generate_input(attrs):
-            return np.random.random([
-                attrs[0]["batch_size"] * attrs[1]["window_number"],
-                attrs[1]["window_size"] * attrs[1]["window_size"],
-                attrs[1]["dim"]
-            ]).astype(np.float32)
+            return np.random.random(
+                [
+                    attrs[0]["batch_size"] * attrs[1]["window_number"],
+                    attrs[1]["window_size"] * attrs[1]["window_size"],
+                    attrs[1]["dim"],
+                ]
+            ).astype(np.float32)
 
-        attrs = [{
-            "batch_size": batch_size
-        }, {
-            "window_number": window_number,
-            "window_size": window_size,
-            "dim": dim
-        }]
+        attrs = [
+            {"batch_size": batch_size},
+            {
+                "window_number": window_number,
+                "window_size": window_size,
+                "dim": dim,
+            },
+        ]
         reshape2_00 = OpConfig(
             type="reshape2",
             inputs={"X": ["input0"]},
             outputs={
                 "Out": ["reshape2_00_out"],
-                "XShape": ["reshape2_00_outXshape"]
+                "XShape": ["reshape2_00_outXshape"],
             },
-            attrs={"shape": [-1, window_size, window_size, dim]})
-        reshape2_10 = OpConfig(type="reshape2",
-                               inputs={"X": ["reshape2_00_out"]},
-                               outputs={
-                                   "Out": ["reshape2_10_out"],
-                                   "XShape": ["reshape2_10_outXshape"]
-                               },
-                               attrs={
-                                   "shape": [
-                                       -1,
-                                       int(math.sqrt(window_number)),
-                                       int(math.sqrt(window_number)),
-                                       window_size, window_size, dim
-                                   ]
-                               })
-        transpose2_20 = OpConfig(type="transpose2",
-                                 inputs={"X": ["reshape2_10_out"]},
-                                 outputs={
-                                     "Out": ["transpose2_20_out"],
-                                     "XShape": ["transpose2_20_outXshape"]
-                                 },
-                                 attrs={"axis": [0, 1, 3, 2, 4, 5]})
+            attrs={"shape": [-1, window_size, window_size, dim]},
+        )
+        reshape2_10 = OpConfig(
+            type="reshape2",
+            inputs={"X": ["reshape2_00_out"]},
+            outputs={
+                "Out": ["reshape2_10_out"],
+                "XShape": ["reshape2_10_outXshape"],
+            },
+            attrs={
+                "shape": [
+                    -1,
+                    int(math.sqrt(window_number)),
+                    int(math.sqrt(window_number)),
+                    window_size,
+                    window_size,
+                    dim,
+                ]
+            },
+        )
+        transpose2_20 = OpConfig(
+            type="transpose2",
+            inputs={"X": ["reshape2_10_out"]},
+            outputs={
+                "Out": ["transpose2_20_out"],
+                "XShape": ["transpose2_20_outXshape"],
+            },
+            attrs={"axis": [0, 1, 3, 2, 4, 5]},
+        )
         reshape2_30 = OpConfig(
             type="reshape2",
             inputs={"X": ["transpose2_20_out"]},
             outputs={
                 "Out": ["reshape2_30_out"],
-                "XShape": ["reshape2_30_outXshape"]
+                "XShape": ["reshape2_30_outXshape"],
             },
             attrs={
                 "shape": [
                     -1,
                     int(math.sqrt(window_number)) * window_size,
-                    int(math.sqrt(window_number)) * window_size, dim
+                    int(math.sqrt(window_number)) * window_size,
+                    dim,
                 ]
-            })
+            },
+        )
         reshape2_40 = OpConfig(
             type="reshape2",
             inputs={"X": ["reshape2_30_out"]},
             outputs={
                 "Out": ["reshape2_40_out"],
-                "XShape": ["reshape2_40_outXshape"]
+                "XShape": ["reshape2_40_outXshape"],
             },
             attrs={
                 "shape": [-1, window_number * window_size * window_size, dim]
-            })
+            },
+        )
 
         program_config = ProgramConfig(
             ops=[
-                reshape2_00, reshape2_10, transpose2_20, reshape2_30,
-                reshape2_40
+                reshape2_00,
+                reshape2_10,
+                transpose2_20,
+                reshape2_30,
+                reshape2_40,
             ],
             weights={},
             inputs={
                 "input0": TensorConfig(data_gen=partial(generate_input, attrs)),
             },
-            outputs=["reshape2_40_out"])
+            outputs=["reshape2_40_out"],
+        )
 
         return program_config
 
     def test(self):
-        self.run_and_statis(quant=False,
-                            max_examples=50,
-                            passes=["reverse_roll_fuse_pass"],
-                            max_duration=250,
-                            min_success_num=50)
+        self.run_and_statis(
+            quant=False,
+            max_examples=50,
+            passes=["reverse_roll_fuse_pass"],
+            max_duration=250,
+            min_success_num=50,
+        )
 
 
 if __name__ == "__main__":
