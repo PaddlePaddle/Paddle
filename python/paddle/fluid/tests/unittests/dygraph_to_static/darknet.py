8c3b4fd2476322dec65c3ea1cd402a9930b2203f
wangzhen38 <wangzhen38@baidu.com>
2022-11-29 12:30:06 +0000

    [remove fluid] under unittesets of batch norm

--- python/paddle/fluid/tests/unittests/dygraph_to_static/darknet.py
+++ python/paddle/fluid/tests/unittests/dygraph_to_static/darknet.py
@@ -14,7 +14,6 @@
 
 import paddle
 import paddle.fluid as fluid
-from paddle.fluid.dygraph.nn import BatchNorm
 from paddle.fluid.param_attr import ParamAttr
 from paddle.fluid.regularizer import L2Decay
 
