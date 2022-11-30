9662d1342765d8e4660926145f7892070cebc791
heyanru01 <429520051@qq.com>
2022-11-30 04:29:40 +0000

    fix

--- python/paddle/jit/dy2static/convert_operators.py
+++ python/paddle/jit/dy2static/convert_operators.py
@@ -29,8 +29,6 @@ from paddle.fluid.layers import (
 from paddle.fluid.layers import (
     assign,
     fill_constant,
-    reduce_all,
-    reduce_any,
 )
 from paddle.fluid.layers import (
     cast,
@@ -653,7 +651,7 @@ def convert_shape_compare(left, *args):
         def reduce_compare(x, op_str, y):
             element_wise_result = eval("x " + op_str + " y")
             if op_str == "!=":
-                return reduce_any(element_wise_result)
+                return paddle.reduce_any(element_wise_result)
             elif (
                 op_str == "is"
                 or op_str == "is not"
@@ -662,7 +660,7 @@ def convert_shape_compare(left, *args):
             ):
                 return element_wise_result
             else:
-                return reduce_all(element_wise_result)
+                return paddle.reduce_all(element_wise_result)
 
         final_result = reduce_compare(left, args[0], args[1])
         for i in range(1, num_cmp):
