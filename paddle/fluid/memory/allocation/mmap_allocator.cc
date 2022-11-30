4ebffe6f7a7cea2ebd20a85887653d7b20be4ec3
Wang Huan <wanghuan29@baidu.com>
2022-11-30 01:29:47 +0000

    refine

--- paddle/fluid/memory/allocation/mmap_allocator.cc
+++ paddle/fluid/memory/allocation/mmap_allocator.cc
@@ -172,11 +172,7 @@ void RefcountedMemoryMapAllocation::close() {
   void *data = map_ptr_;
   CountInfo *info = reinterpret_cast<CountInfo *>(data);
   if (--info->refcount == 0) {
-    PADDLE_ENFORCE_NE(
-        shm_unlink(ipc_name_.c_str()),
-        -1,
-        platform::errors::Unavailable(
-            "could not unlink the shared memory file ", ipc_name_));
+    shm_unlink(ipc_name_.c_str());
     VLOG(6) << "shm_unlink file: " << ipc_name_;
   }
 
