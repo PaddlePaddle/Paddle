e3631978f24ece1c36581ea7af046cb99055eaba
zhoutianzi666 <17801055074@163.com>
2022-11-30 07:23:30 +0000

    add switch

--- paddle/fluid/pybind/inference_api.cc
+++ paddle/fluid/pybind/inference_api.cc
@@ -644,7 +644,7 @@ void BindAnalysisConfig(py::module *m) {
            &AnalysisConfig::EnableUseGpu,
            py::arg("memory_pool_init_size_mb"),
            py::arg("device_id") = 0)
-      .def("enable_use_cutlass", &AnalysisConfig::EnableUseCutlass)
+      .def("exp_enable_use_cutlass", &AnalysisConfig::Exp_EnableUseCutlass)
 #if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
       .def("set_exec_stream",
            [](AnalysisConfig &self, phi::CUDAStream &stream) {
