b3254e4b394f2e3079ab205c741b6a4c25ab1602
heyanru01 <429520051@qq.com>
2022-11-30 06:04:10 +0000

    fix

--- python/paddle/fluid/tests/unittests/dygraph_to_static/test_cycle_gan.py
+++ python/paddle/fluid/tests/unittests/dygraph_to_static/test_cycle_gan.py
@@ -105,7 +105,7 @@ class Cycle_Gan(fluid.dygraph.Layer):
         G = g_A_loss + g_B_loss
         idt_A = self.build_generator_resnet_9blocks_a(input_B)
         idt_loss_A = (
-            fluid.layers.reduce_mean(
+            paddle.mean(
                 paddle.abs(fluid.layers.elementwise_sub(x=input_B, y=idt_A))
             )
             * lambda_B
@@ -114,7 +114,7 @@ class Cycle_Gan(fluid.dygraph.Layer):
 
         idt_B = self.build_generator_resnet_9blocks_b(input_A)
         idt_loss_B = (
-            fluid.layers.reduce_mean(
+            paddle.mean(
                 paddle.abs(fluid.layers.elementwise_sub(x=input_A, y=idt_B))
             )
             * lambda_A
