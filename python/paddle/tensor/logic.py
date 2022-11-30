c2dc4696fb796551db52b23388594261e779344a
Ligoml <39876205+Ligoml@users.noreply.github.com>
2022-11-30 10:54:59 +0800

    Update logic.py

--- python/paddle/tensor/logic.py
+++ python/paddle/tensor/logic.py
@@ -227,6 +227,11 @@ def logical_not(x, out=None, name=None):
 
         out = !x
 
+    Note:
+        ``paddle.logical_not`` supports broadcasting. If you want know more about broadcasting, please refer to `Introduction to Tensor`_ .
+
+        .. _Introduction to Tensor: ../../guides/beginner/tensor_en.html#chapter5-broadcasting-of-tensor
+
     Args:
         x(Tensor):  Operand of logical_not operator. Must be a Tensor of type bool, int8, int16, in32, in64, float32, or float64.
         out(Tensor): The ``Tensor`` that specifies the output of the operator, which can be any ``Tensor`` that has been created in the program. The default value is None, and a new ``Tensor` will be created to save the output.
