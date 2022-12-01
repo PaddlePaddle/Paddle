c56c512c09e67637a888ac5bb05d1204c202b867
wangruting <wangruting@baidu.com>
2022-12-01 08:07:55 +0000

    modify api

--- python/paddle/static/__init__.py
+++ python/paddle/static/__init__.py
@@ -73,6 +73,8 @@ from ..fluid.layers.metric_op import accuracy  # noqa: F401
 from ..fluid.contrib.layers import ctr_metric_bundle  # noqa: F401
 from ..fluid.layers import exponential_decay  # noqa: F401
 
+from .nn.common import batch_norm  # noqa: F401
+
 __all__ = [  # noqa
     'append_backward',
     'batch_norm',
