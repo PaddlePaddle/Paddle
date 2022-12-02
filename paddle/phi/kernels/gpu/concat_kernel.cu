7351661e3818ce8a807962fd454b6baefaf0acf7
sljlp <lrslj@live.com>
2022-12-02 21:04:59 +0800

    update for benchmark

--- paddle/phi/kernels/gpu/concat_kernel.cu
+++ paddle/phi/kernels/gpu/concat_kernel.cu
@@ -34,27 +34,43 @@ void ConcatKernel(const Context& dev_ctx,
                   DenseTensor* out) {
   int64_t axis = axis_scalar.to<int64_t>();
 
-  axis = phi::funcs::ComputeAxis(axis, x[0]->dims().size());
+  if (UNLIKELY(x[0]->dims().size() == 0)) {
+    // for dims is 0 specially
+    phi::DDim tmp_1dim, out_dims;
+    out_dims[0] = x.size();
+    tmp_1dim[0] = 1;
 
-  VLOG(10) << "axis: " << axis << " x.size: " << x.size()
-           << "x[0].dim: " << x[0]->dims().to_str();
+    out->Resize(out_dims);
+    dev_ctx.template Alloc<T>(out);
+
+    size_t output_offset = 0;
+    for (auto* in : x) {
+      if (in->numel() == 0UL) {
+        continue;
+      }
+      auto in_stride = phi::stride_numel(tmp_1dim);
+      auto out_stride = phi::stride_numel(out->dims());
+      paddle::operators::StridedNumelCopyWithAxis<T>(
+          dev_ctx,
+          axis,
+          out->data<T>() + output_offset,
+          out_stride,
+          in->data<T>(),
+          in_stride,
+          in_stride[axis]);
+      output_offset += in_stride[axis];
+    }
+    return;
+  }
+
+  axis = phi::funcs::ComputeAxis(axis, x[0]->dims().size());
 
   std::vector<phi::DDim> x_dims;
   for (size_t i = 0; i < x.size(); ++i) {
     x_dims.push_back(x[i]->dims());
   }
 
-  phi::DDim tmp_1dim, out_dims;
-  bool dim_is_0 = (x[0]->dims().size() == 0);
-
-  if (dim_is_0) {
-    out_dims[0] = x.size();
-    tmp_1dim[0] = 1;
-  } else {
-    out_dims = phi::funcs::ComputeAndCheckShape(true, x_dims, axis);
-  }
-  VLOG(10) << "out dims: " << out_dims.to_str() << "out num:" << out->numel();
-
+  phi::DDim out_dims = phi::funcs::ComputeAndCheckShape(true, x_dims, axis);
   out->Resize(out_dims);
   out->mutable_data<T>(dev_ctx.GetPlace());
 
@@ -96,7 +112,7 @@ void ConcatKernel(const Context& dev_ctx,
       if (in->numel() == 0UL) {
         continue;
       }
-      auto in_stride = phi::stride_numel(dim_is_0 ? tmp_1dim : in->dims());
+      auto in_stride = phi::stride_numel(in->dims());
       auto out_stride = phi::stride_numel(out->dims());
       paddle::operators::StridedNumelCopyWithAxis<T>(
           dev_ctx,
@@ -115,11 +131,6 @@ void ConcatKernel(const Context& dev_ctx,
         inputs.push_back(*x[j]);
       } else {
         continue;
-      }
-    }
-    if (dim_is_0) {
-      for (auto it = inputs.begin(); it != inputs.end(); it++) {
-        it->Resize(tmp_1dim);
       }
     }
     phi::funcs::ConcatFunctor<Context, T> functor;
