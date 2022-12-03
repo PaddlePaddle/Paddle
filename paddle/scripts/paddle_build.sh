861728b077e98a1f214eed83cab25acb0fffd0f4
risemeup1 <515586620@qq.com>
2022-12-03 10:01:48 +0000

    debug

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3665,6 +3665,8 @@ function run_setup(){
     ccache -z
 
     python setup.py install;build_error=$?
+    cp -r /opt/_internal/cpython-3.7.0/lib/python3.7/site-packages/paddlepaddle-0.0.0-py3.7-linux-x86_64.egg/paddle  /opt/_internal/cpython-3.7.0/lib/python3.7/site-packages/
+    
     # ci will collect ccache hit rate
     collect_ccache_hits
 
@@ -3885,10 +3887,10 @@ function main() {
         build_mac
         ;;
       cicheck_py37)
-        cmake_gen_and_build ${PYTHON_ABI:-""} ${parallel_number}
-        run_linux_cpu_test ${PYTHON_ABI:-""} ${PROC_RUN:-1}
-        # run_setup ${PYTHON_ABI:-""} ${parallel_number}
+        # cmake_gen_and_build ${PYTHON_ABI:-""} ${parallel_number}
         # run_linux_cpu_test ${PYTHON_ABI:-""} ${PROC_RUN:-1}
+        run_setup ${PYTHON_ABI:-""} ${parallel_number}
+        run_linux_cpu_test ${PYTHON_ABI:-""} ${PROC_RUN:-1}
         ;;
       test_cicheck_py37)
         run_linux_cpu_test ${PYTHON_ABI:-""} ${PROC_RUN:-1}
