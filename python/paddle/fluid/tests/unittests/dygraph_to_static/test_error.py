81f1fe148a08958cc2b8b435cf3601c76325a472
SigureMo <sigure.qaq@gmail.com>
2022-12-05 23:11:49 +0800

    [CodeStyle][isort][Dy2St] sort imports in test_error

--- python/paddle/fluid/tests/unittests/dygraph_to_static/test_error.py
+++ python/paddle/fluid/tests/unittests/dygraph_to_static/test_error.py
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import inspect
+import os
 import unittest
+
 import numpy as np
+
 import paddle
 import paddle.fluid as fluid
 from paddle.jit.dy2static import error
