924361d051f50247f7ee9f1f77a5da44bcf9a3ce
wangzhen38 <wangzhen38@baidu.com>
2022-12-05 02:52:53 +0000

    [remove fluid] Pool2D

--- python/paddle/fluid/tests/unittests/test_imperative_resnet.py
+++ python/paddle/fluid/tests/unittests/test_imperative_resnet.py
@@ -216,9 +216,7 @@ class ResNet(fluid.Layer):
                 )
                 self.bottleneck_block_list.append(bottleneck_block)
                 shortcut = True
-        self.pool2d_avg = paddle.fluid.dygraph.nn.Pool2D(
-            pool_size=7, pool_type='avg', global_pooling=True
-        )
+        self.pool2d_avg = paddle.nn.AdaptiveAvgPool2D(1)
 
         self.pool2d_avg_output = num_filters[-1] * 4 * 1 * 1
 
