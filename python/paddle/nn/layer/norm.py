f383b37cc4f4adf92a1313bce5143c517e286b2d
risemeup1 <515586620@qq.com>
2022-12-05 14:20:13 +0000

    modify static-check ci error

--- python/paddle/nn/layer/norm.py
+++ python/paddle/nn/layer/norm.py
@@ -846,7 +846,7 @@ class BatchNorm(Layer):
           x = np.random.random(size=(3, 10, 3, 7)).astype('float32')
           with fluid.dygraph.guard():
               x = to_variable(x)
-              batch_norm = fluid.BatchNorm(10)
+              batch_norm = nn.layer.norm.BatchNorm(10)
               hidden1 = batch_norm(x)
     """
 
