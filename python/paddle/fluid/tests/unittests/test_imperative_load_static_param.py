6e6719fdd801a4515a588e7d0fdd2e1082b8968b
wangzhen38 <wangzhen38@baidu.com>
2022-12-05 02:19:44 +0000

    [remove fluid] PRelu BilinearTensorProduct Conv2DTranspose SequenceConv RowConv

--- python/paddle/fluid/tests/unittests/test_imperative_load_static_param.py
+++ python/paddle/fluid/tests/unittests/test_imperative_load_static_param.py
@@ -21,13 +21,7 @@ import numpy as np
 import paddle
 import paddle.fluid as fluid
 import paddle.fluid.framework as framework
-from paddle.fluid.dygraph.nn import (
-    NCE,
-    BatchNorm,
-    Embedding,
-    GroupNorm,
-    LayerNorm,
-)
+from paddle.fluid.dygraph.nn import NCE, BatchNorm, Embedding, GroupNorm
 from paddle.nn import Linear
 
 
@@ -211,8 +205,8 @@ class TestDygraphLoadStatic(unittest.TestCase):
                     self.emb1 = Embedding([1000, 100])
                     self.emb2 = Embedding([2000, 200])
 
-                    self.layer_norm_1 = LayerNorm([10])
-                    self.layer_norm_2 = LayerNorm(10)
+                    self.layer_norm_1 = paddle.nn.LayerNorm([10])
+                    self.layer_norm_2 = paddle.nn.LayerNorm(10)
 
                     self.nce1 = NCE(10000, 100)
                     self.nce2 = NCE(10000, 100)
