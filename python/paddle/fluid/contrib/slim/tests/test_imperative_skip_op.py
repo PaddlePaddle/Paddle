60af22c25538fa487f2095a2c83602031151f835
yjjiang11 <yjjiang11@gmail.com>
2022-12-06 07:51:43 +0000

    rm imperative op eager guard tests

--- python/paddle/fluid/contrib/slim/tests/test_imperative_skip_op.py
+++ python/paddle/fluid/contrib/slim/tests/test_imperative_skip_op.py
@@ -33,7 +33,6 @@ from imperative_test_utils import (
     train_lenet,
     ImperativeLenetWithSkipQuant,
 )
-from paddle.fluid.framework import _test_eager_guard
 
 os.environ["CPU_NUM"] = "1"
 if core.is_compiled_with_cuda():
@@ -45,7 +44,7 @@ _logger = get_logger(
 
 
 class TestImperativeOutSclae(unittest.TestCase):
-    def func_out_scale_acc(self):
+    def test_out_scale_acc(self):
         paddle.disable_static()
         seed = 1000
         lr = 0.1
@@ -140,11 +139,6 @@ class TestImperativeOutSclae(unittest.TestCase):
             self.assertTrue(conv2d_skip_count == 1)
         if find_matmul:
             self.assertTrue(matmul_skip_count == 1)
-
-    def test_out_scale_acc(self):
-        with _test_eager_guard():
-            self.func_out_scale_acc()
-        self.func_out_scale_acc()
 
 
 if __name__ == '__main__':
