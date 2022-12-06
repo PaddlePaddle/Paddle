8c94362b19ec79af8c53981d46cecba6cc84369f
rewang <rewang@nvidia.com>
2022-12-06 11:55:55 +0000

    Add a persistent ibuilder to speedup unit test

--- paddle/fluid/inference/tensorrt/engine.h
+++ paddle/fluid/inference/tensorrt/engine.h
@@ -852,6 +852,15 @@ class TRTEngineManager {
   size_t max_ctx_mem_size_{0};
   std::unordered_map<PredictorID, AllocationPtr> context_memorys_;
   std::unordered_map<std::string, std::unique_ptr<TensorRTEngine>> engines_;
+  // createInferBuilder loads trt kernels and take a few second
+  // But as long as one IBuilder lives, trt kernel will not be unloaded
+  // Hence, a persistent IBuilder to avoid TensorRT unload/reload kernels
+  std::unique_ptr<nvinfer1::IBuilder, std::function<void(nvinfer1::IBuilder*)>>
+      holder{createInferBuilder(&NaiveLogger::Global()), [](auto* ptr) {
+               if (ptr) {
+                 ptr->destroy();
+               }
+             }};
 };
 
 }  // namespace tensorrt
