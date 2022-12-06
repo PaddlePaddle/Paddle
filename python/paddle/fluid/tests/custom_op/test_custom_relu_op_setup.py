425526760c3af46c5feb995380a52606dfb42c03
yjjiang11 <yjjiang11@gmail.com>
2022-12-06 07:08:03 +0000

    remove custom op eager guard tests

--- python/paddle/fluid/tests/custom_op/test_custom_relu_op_setup.py
+++ python/paddle/fluid/tests/custom_op/test_custom_relu_op_setup.py
@@ -21,7 +21,6 @@ import numpy as np
 
 import paddle
 import paddle.static as static
-from paddle.fluid.framework import _test_eager_guard
 from paddle.utils.cpp_extension.extension_utils import run_cmd
 from paddle.vision.transforms import Compose, Normalize
 
@@ -251,7 +250,7 @@ class TestNewCustomOpSetUpInstall(unittest.TestCase):
                         ),
                     )
 
-    def func_dynamic(self):
+    def test_dynamic(self):
         for device in self.devices:
             for dtype in self.dtypes:
                 if device == 'cpu' and dtype == 'float16':
@@ -278,11 +277,6 @@ class TestNewCustomOpSetUpInstall(unittest.TestCase):
                             x_grad, pd_x_grad
                         ),
                     )
-
-    def test_dynamic(self):
-        with _test_eager_guard():
-            self.func_dynamic()
-        self.func_dynamic()
 
     def test_static_save_and_load_inference_model(self):
         paddle.enable_static()
