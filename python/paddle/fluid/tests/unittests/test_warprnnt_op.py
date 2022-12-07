8af49a51eaa7a71ac5172d7b80b855b1f7d828a4
Hui Zhang <zhtclz@foxmail.com>
2022-12-07 08:04:48 +0000

    rnnt test all pass

--- python/paddle/fluid/tests/unittests/test_warprnnt_op.py
+++ python/paddle/fluid/tests/unittests/test_warprnnt_op.py
@@ -368,123 +368,226 @@ class TestWarpRNNTOpError(unittest.TestCase):
         paddle.enable_static()
 
 
-# class TestCTCLossAPICase(unittest.TestCase):
-#     def test_functinal_api(self):
-#         self.batch_size = 4
-#         self.num_classes = CUDA_BLOCK_SIZE + 2
-#         self.logits_length = np.array([4, 1, 3, 3], dtype=np.int64)
-#         self.labels_length = np.array([3, 1, 4, 4], dtype=np.int64)
-#         self.blank = self.num_classes - 1
-#         self.norm_by_times = False
+class TestRNNTLossAPICase(unittest.TestCase):
+    def set_act(self):
+        # logsoftmax
+        self.acts = np.array(
+            [
+                [
+                    [
+                        [-1.40493705, -0.68276381, -1.38870219],
+                        [-1.25243963, -1.03148021, -1.02802034],
+                        [-1.19624572, -0.93786934, -1.18347801],
+                    ],
+                    [
+                        [-1.03417513, -0.84465814, -1.53815849],
+                        [-0.96884241, -1.01432347, -1.35545407],
+                        [-0.82076925, -1.10135010, -1.48067081],
+                    ],
+                    [
+                        [-1.43828803, -1.16579869, -0.79630424],
+                        [-1.38401855, -0.83654478, -1.15129927],
+                        [-1.05188255, -1.29604414, -0.97522265],
+                    ],
+                    [
+                        [-1.34330978, -0.86678589, -1.14344457],
+                        [-0.72518815, -1.32106859, -1.39063758],
+                        [-1.09984781, -1.00059987, -1.20590993],
+                    ],
+                ],
+                [
+                    [
+                        [-1.02221057, -1.47617485, -0.88748174],
+                        [-1.18362952, -0.78488945, -1.43689575],
+                        [-1.00784739, -1.28566450, -1.02574476],
+                    ],
+                    [
+                        [-1.02589709, -1.13153743, -1.14260096],
+                        [-1.09942215, -1.12238913, -1.07459704],
+                        [-1.09359647, -0.89829379, -1.35585602],
+                    ],
+                    [
+                        [-1.07782876, -0.84361953, -1.47178440],
+                        [-1.23424792, -1.00248783, -1.07299990],
+                        [-0.96521771, -1.19895815, -1.14698912],
+                    ],
+                    [
+                        [-1.50722446, -1.15380039, -0.76994115],
+                        [-1.19125975, -0.89919308, -1.24041594],
+                        [-0.91301359, -1.19665577, -1.21576258],
+                    ],
+                ],
+                [
+                    [
+                        [-1.02221057, -1.47617485, -0.88748174],
+                        [-1.18362952, -0.78488945, -1.43689575],
+                        [-1.00784739, -1.28566450, -1.02574476],
+                    ],
+                    [
+                        [-1.02589709, -1.13153743, -1.14260096],
+                        [-1.09942215, -1.12238913, -1.07459704],
+                        [-1.09359647, -0.89829379, -1.35585602],
+                    ],
+                    [
+                        [-1.07782876, -0.84361953, -1.47178440],
+                        [-1.23424792, -1.00248783, -1.07299990],
+                        [-0.96521771, -1.19895815, -1.14698912],
+                    ],
+                    [
+                        [-1.50722446, -1.15380039, -0.76994115],
+                        [-1.19125975, -0.89919308, -1.24041594],
+                        [-0.91301359, -1.19665577, -1.21576258],
+                    ],
+                ],
+            ],
+            dtype=np.float32,
+        )
 
-#         logits = np.random.uniform(
-#             0.1,
-#             1.0,
-#             [max(self.logits_length), self.batch_size, self.num_classes],
-#         ).astype("float32")
-#         softmax = np.apply_along_axis(stable_softmax, -1, logits)
-#         # labels should not be blank
-#         labels = np.random.randint(
-#             0,
-#             self.num_classes - 1,
-#             [self.batch_size, max(self.labels_length)],
-#             dtype="int32",
-#         )
+    def set_gradient(self):
+        self.gradient = np.array(
+            [
+                [
+                    [
+                        [-0.43222645, -0.56777355, 0.0],
+                        [-0.3656501, 0.0, -0.20212345],
+                        [-0.20212345, 0.0, 0.0],
+                    ],
+                    [
+                        [-0.16521672, -0.26700973, 0.0],
+                        [-0.39436539, 0.0, -0.23829444],
+                        [-0.44041789, 0.0, 0.0],
+                    ],
+                    [
+                        [-0.05212979, -0.11308693, 0.0],
+                        [-0.18313787, 0.0, -0.32431445],
+                        [-0.76473234, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, -0.05212979, 0.0],
+                        [0.0, 0.0, -0.23526766],
+                        [-1.0, 0.0, 0.0],
+                    ],
+                ],
+                [
+                    [
+                        [-0.71614241, -0.28385759, 0.0],
+                        [-0.18382932, -0.10002826, 0.0],
+                        [-0.10002826, 0.0, 0.0],
+                    ],
+                    [
+                        [-0.41121795, -0.30492447, 0.0],
+                        [-0.32957594, -0.15917785, 0.0],
+                        [-0.25920611, 0.0, 0.0],
+                    ],
+                    [
+                        [-0.11607642, -0.29514153, 0.0],
+                        [-0.28653336, -0.3381841, 0.0],
+                        [-0.59739022, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, -0.11607642, 0.0],
+                        [0.0, -0.40260978, 0.0],
+                        [-1.0, 0.0, 0.0],
+                    ],
+                ],
+                [
+                    [
+                        [-0.71614241, -0.28385759, 0.0],
+                        [-0.18382932, -0.10002826, 0.0],
+                        [-0.10002826, 0.0, 0.0],
+                    ],
+                    [
+                        [-0.41121795, -0.30492447, 0.0],
+                        [-0.32957594, -0.15917785, 0.0],
+                        [-0.25920611, 0.0, 0.0],
+                    ],
+                    [
+                        [-0.11607642, -0.29514153, 0.0],
+                        [-0.28653336, -0.3381841, 0.0],
+                        [-0.59739022, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, -0.11607642, 0.0],
+                        [0.0, -0.40260978, 0.0],
+                        [-1.0, 0.0, 0.0],
+                    ],
+                ],
+            ],
+            dtype=np.float64,
+        )
 
-#         ctc = CTCForward(
-#             softmax,
-#             self.logits_length,
-#             labels,
-#             self.labels_length,
-#             self.num_classes,
-#             self.batch_size,
-#             self.blank,
-#             self.norm_by_times,
-#         )
-#         loss_np = ctc.forward()
+    def config(self):
+        self.blank = 0
+        self.fastemit_lambda = 0.0
+        self.set_act()
+        self.labels = np.array([[1, 2], [1, 1], [1, 1]], dtype=np.int32)
+        self.logit_lens = np.array([4, 4, 4], dtype=np.int32)
+        self.label_lens = np.array([2, 2, 2], dtype=np.int32)
 
-#         paddle.disable_static()
-#         softmax = paddle.to_tensor(logits)
-#         labels = paddle.to_tensor(labels)
-#         logits_length = paddle.to_tensor(self.logits_length)
-#         labels_length = paddle.to_tensor(self.labels_length)
-#         loss_pd_mean = F.ctc_loss(
-#             softmax,
-#             labels,
-#             logits_length,
-#             labels_length,
-#             blank=self.blank,
-#             reduction='mean',
-#         )
-#         loss_pd_mean = loss_pd_mean.numpy()
+        self.loss = np.array(
+            [4.2806528590890736, 3.9384369822503591, 3.9384369822503591],
+            dtype=np.float64,
+        )
+        self.set_gradient()
 
-#         loss_pd_sum = F.ctc_loss(
-#             softmax,
-#             labels,
-#             logits_length,
-#             labels_length,
-#             blank=self.blank,
-#             reduction='sum',
-#         )
-#         loss_pd_sum = loss_pd_sum.numpy()
-#         paddle.enable_static()
-#         loss_np = np.squeeze(loss_np, axis=-1)
-#         loss_np_mean = (loss_np / labels_length.numpy()).mean()
-#         loss_np_sum = loss_np.sum()
+    def test_functinal_api(self):
+        self.config()
 
-#         np.testing.assert_allclose(
-#             loss_pd_mean, loss_np_mean, rtol=1e-05, atol=1
-#         )
-#         np.testing.assert_allclose(loss_pd_sum, loss_np_sum, rtol=1e-05, atol=1)
+        paddle.disable_static()
 
-#     def test_class_api(self):
-#         self.batch_size = 3
-#         self.num_classes = 15
-#         self.logits_length = np.array([3, 3, 3], dtype=np.int64)
-#         self.labels_length = np.array([0, 1, 2], dtype=np.int64)
-#         self.blank = 0
-#         self.norm_by_times = False
+        acts = paddle.to_tensor(self.acts)
+        labels = paddle.to_tensor(self.labels)
+        logit_lens = paddle.to_tensor(self.logit_lens)
+        label_lens = paddle.to_tensor(self.label_lens)
 
-#         logits = np.random.uniform(
-#             0.1,
-#             1.0,
-#             [max(self.logits_length), self.batch_size, self.num_classes],
-#         ).astype("float32")
-#         softmax = np.apply_along_axis(stable_softmax, -1, logits)
-#         # labels should not be blank
-#         labels = np.random.randint(
-#             1,
-#             self.num_classes,
-#             [self.batch_size, max(self.labels_length)],
-#             dtype="int32",
-#         )
+        loss_pd_mean = paddle.nn.functional.rnnt_loss(
+            acts,
+            labels,
+            logit_lens,
+            label_lens,
+            blank=self.blank,
+            reduction='mean',
+            fastemit_lambda=self.fastemit_lambda,
+        )
+        loss_pd_mean = loss_pd_mean.numpy()
 
-#         ctc = CTCForward(
-#             softmax,
-#             self.logits_length,
-#             labels,
-#             self.labels_length,
-#             self.num_classes,
-#             self.batch_size,
-#             self.blank,
-#             self.norm_by_times,
-#         )
-#         loss_np = ctc.forward()
+        loss_pd_sum = paddle.nn.functional.rnnt_loss(
+            acts,
+            labels,
+            logit_lens,
+            label_lens,
+            blank=self.blank,
+            reduction='sum',
+            fastemit_lambda=self.fastemit_lambda,
+        )
+        loss_pd_sum = loss_pd_sum.numpy()
 
-#         paddle.disable_static()
-#         softmax = paddle.to_tensor(logits)
-#         labels = paddle.to_tensor(labels)
-#         logits_length = paddle.to_tensor(self.logits_length)
-#         labels_length = paddle.to_tensor(self.labels_length)
+        paddle.enable_static()
+        B = self.loss.shape[0]
+        loss_np_mean = self.loss.sum() / B
+        loss_np_sum = self.loss.sum()
 
-#         loss_pd = paddle.nn.CTCLoss(self.blank, 'none')(
-#             softmax, labels, logits_length, labels_length
-#         )
-#         loss_pd = loss_pd.numpy()
-#         paddle.enable_static()
-#         loss_np = np.squeeze(loss_np, axis=-1)
+        np.testing.assert_allclose(
+            loss_pd_mean, loss_np_mean, rtol=1e-05, atol=1
+        )
+        np.testing.assert_allclose(loss_pd_sum, loss_np_sum, rtol=1e-05, atol=1)
 
-#         np.testing.assert_allclose(loss_pd, loss_np, rtol=1e-05, atol=1)
+    def test_class_api(self):
+        self.config()
+
+        paddle.disable_static()
+
+        acts = paddle.to_tensor(self.acts)
+        labels = paddle.to_tensor(self.labels)
+        logit_lens = paddle.to_tensor(self.logit_lens)
+        label_lens = paddle.to_tensor(self.label_lens)
+
+        loss_pd = paddle.nn.RNNTLoss(self.blank, self.fastemit_lambda, 'none')(
+            acts, labels, logit_lens, label_lens
+        )
+        loss_pd = loss_pd.numpy()
+        paddle.enable_static()
+        np.testing.assert_allclose(loss_pd, self.loss, rtol=1e-05, atol=1)
 
 
 if __name__ == "__main__":
