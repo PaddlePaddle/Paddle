80bdacac3e917093727c0676506df1a93ec06631
Jacek Czaja <jacek.czaja@intel.com>
2022-12-07 20:29:01 +0100

    - fix

--- python/paddle/fluid/tests/unittests/mkldnn/test_reshape_mkldnn_op.py
+++ python/paddle/fluid/tests/unittests/mkldnn/test_reshape_mkldnn_op.py
@@ -173,6 +173,7 @@ def create_reshape_bf16_test_classes(parent):
         def setUp(self):
             super().setUp()
             self.dtype = np.uint16
+            self.x = np.random.random(self.ori_shape).astype("float32")
             self.inputs = {"X": convert_float_to_uint16(self.x)}
 
         def calculate_grads(self):
