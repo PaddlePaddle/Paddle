b9046f4c6f7d0270453501d10b90c8296680d09d
Silv3S <slawomir.siwek@intel.com>
2022-12-07 11:18:04 +0100

    remove header

--- paddle/phi/kernels/onednn/reshape_kernel.cc
+++ paddle/phi/kernels/onednn/reshape_kernel.cc
@@ -9,8 +9,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/phi/kernels/reshape_kernel.h"
-
 #include "paddle/phi/backends/onednn/onednn_reuse.h"
 #include "paddle/phi/core/kernel_registry.h"
 
