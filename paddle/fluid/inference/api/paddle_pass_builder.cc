f41809b9721176946caeac56625307c2b5b09d55
Wangzheee <634486483@qq.com>
2022-12-08 15:43:13 +0800

    [Paddle Inference] General optimization for no_varlen embedding layernorm (#48580)
    * general optimization no_varlen embedding layernorm

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -95,39 +95,33 @@ const std::vector<std::string> kTRTSubgraphPasses({
       "identity_scale_op_clean_pass",          //
       "add_support_int8_pass",                 //
       // "fc_fuse_pass",                        //
-      "simplify_with_basic_ops_pass",  //
-
-#if defined _WIN32
-#else
+      "simplify_with_basic_ops_pass",                 //
       "trt_embedding_eltwise_layernorm_fuse_pass",    //
       "preln_embedding_eltwise_layernorm_fuse_pass",  //
-#endif
-
-      "delete_c_identity_op_pass",            //
-      "trt_multihead_matmul_fuse_pass_v2",    //
-      "trt_multihead_matmul_fuse_pass_v3",    //
-      "multihead_matmul_roformer_fuse_pass",  //
-      "constant_folding_pass",                //
-      "vit_attention_fuse_pass",              //
-      "trt_skip_layernorm_fuse_pass",         //
-      "preln_skip_layernorm_fuse_pass",       //
-      "layernorm_shift_partition_fuse_pass",  //
-      "merge_layernorm_fuse_pass",            //
-      "preln_residual_bias_fuse_pass",        //
-      "preln_layernorm_x_fuse_pass",          //
-      "reverse_roll_fuse_pass",               //
-      // "set_transformer_input_convert_pass",       //
-      "conv_bn_fuse_pass",                           //
-      "unsqueeze2_eltwise_fuse_pass",                //
-      "trt_squeeze2_matmul_fuse_pass",               //
-      "trt_flatten2_matmul_fuse_pass",               //
-      "trt_map_matmul_v2_to_mul_pass",               //
-      "trt_map_matmul_v2_to_matmul_pass",            //
-      "trt_map_matmul_to_mul_pass",                  //
-      "fc_fuse_pass",                                //
-      "conv_elementwise_add_fuse_pass",              //
-      "remove_padding_recover_padding_pass",         //
-      "delete_remove_padding_recover_padding_pass",  //
+      "delete_c_identity_op_pass",                    //
+      "trt_multihead_matmul_fuse_pass_v2",            //
+      "trt_multihead_matmul_fuse_pass_v3",            //
+      "multihead_matmul_roformer_fuse_pass",          //
+      "constant_folding_pass",                        //
+      "vit_attention_fuse_pass",                      //
+      "trt_skip_layernorm_fuse_pass",                 //
+      "preln_skip_layernorm_fuse_pass",               //
+      "layernorm_shift_partition_fuse_pass",          //
+      "merge_layernorm_fuse_pass",                    //
+      "preln_residual_bias_fuse_pass",                //
+      "preln_layernorm_x_fuse_pass",                  //
+      "reverse_roll_fuse_pass",                       //
+      "conv_bn_fuse_pass",                            //
+      "unsqueeze2_eltwise_fuse_pass",                 //
+      "trt_squeeze2_matmul_fuse_pass",                //
+      "trt_flatten2_matmul_fuse_pass",                //
+      "trt_map_matmul_v2_to_mul_pass",                //
+      "trt_map_matmul_v2_to_matmul_pass",             //
+      "trt_map_matmul_to_mul_pass",                   //
+      "fc_fuse_pass",                                 //
+      "conv_elementwise_add_fuse_pass",               //
+      "remove_padding_recover_padding_pass",          //
+      "delete_remove_padding_recover_padding_pass",   //
       // "yolo_box_fuse_pass",      //
       "dense_fc_to_sparse_pass",                //
       "dense_multihead_matmul_to_sparse_pass",  //
