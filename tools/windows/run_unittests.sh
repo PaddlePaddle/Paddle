d718519a7878ea7eb2263ffb4fa8fe964e600cad
wangxn12138 <wangxn12138@gmail.com>
2022-12-08 21:02:33 +0800

    Revert "remove unitest"
    This reverts commit 9146bbd92523f59cf753ceabb23327b8ea061d4b.

--- tools/windows/run_unittests.sh
+++ tools/windows/run_unittests.sh
@@ -39,6 +39,7 @@ disable_wingpu_test="^test_model$|\
 ^test_reader_reset$|\
 ^test_imperative_se_resnext$|\
 ^test_sync_batch_norm_op$|\
+^test_imperative_static_runner_while$|\
 ^test_dataloader_keep_order$|\
 ^test_dataloader_unkeep_order$|\
 ^test_multiprocess_dataloader_iterable_dataset_static$|\
@@ -157,6 +158,7 @@ disable_win_inference_test="^trt_quant_int8_yolov3_r50_test$|\
 ^test_decoupled_py_reader$|\
 ^test_generator_dataloader$|\
 ^test_py_reader_using_executor$|\
+^test_imperative_static_runner_while$|\
 ^test_dataloader_keep_order$|\
 ^test_dataloader_unkeep_order$|\
 ^test_sync_batch_norm_op$|\
@@ -193,6 +195,7 @@ disable_wincpu_test="^jit_kernel_test$|\
 ^test_imperative_resnet$|\
 ^test_imperative_resnet_sorted_gradient$|\
 ^test_imperative_se_resnext$|\
+^test_imperative_static_runner_mnist$|\
 ^test_bmn$|\
 ^test_mobile_net$|\
 ^test_resnet_v2$|\
