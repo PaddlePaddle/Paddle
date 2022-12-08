29acb9975e1e40faf1918ca22b32304935ba293d
lxsbupt <luoxsbupt@163.com>
2022-12-08 16:05:38 +0800

    merge gpugraph to develop, fix the_one_ps.py for gpups

--- python/paddle/distributed/ps/the_one_ps.py
+++ python/paddle/distributed/ps/the_one_ps.py
@@ -1090,8 +1090,9 @@ class TheOnePSRuntime(RuntimeBase):
         self.string_hosts = []
         for idx, ep in enumerate(self.endpoints):
             host, port = ep.split(":")
-            pshost = core.PSHost(host, int(port), idx)
-            self.string_hosts.append(pshost.serialize_to_string())
+            #pshost = core.PSHost(host, int(port), idx)
+            #self.string_hosts.append(pshost.serialize_to_string())
+            self.string_hosts.append(":".join([host, port, str(idx)]))
 
         self.with_coordinator = self.role_maker._with_coordinator
         self.coordinator_hosts = []
