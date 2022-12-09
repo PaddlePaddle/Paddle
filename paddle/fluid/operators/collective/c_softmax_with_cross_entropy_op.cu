737ce47a18191e2bfdaf9131af45d9a37ed84cd1
huangjiyi <947613776@qq.com>
2022-12-09 20:44:51 +0800

    fix namespace

--- paddle/fluid/operators/collective/c_softmax_with_cross_entropy_op.cu
+++ paddle/fluid/operators/collective/c_softmax_with_cross_entropy_op.cu
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/operators/collective/c_softmax_with_cross_entropy_op.h"
+
+#include "paddle/fluid/framework/eigen.h"
 #include "paddle/fluid/platform/collective_helper.h"
 #include "paddle/fluid/platform/device/gpu/nccl_helper.h"
 #include "paddle/fluid/string/string_helper.h"
