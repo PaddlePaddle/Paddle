a27901d0c835a3e96314a1ca1bc5f84ac3a5b98e
MARD1NO <359521840@qq.com>
2022-12-09 05:43:45 +0000

    fix header file path

--- paddle/phi/kernels/fusion/cutlass/moe_kernel.cu
+++ paddle/phi/kernels/fusion/cutlass/moe_kernel.cu
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/kernels/moe_kernel.h"
+#include "paddle/phi/kernels/fusion/moe_kernel.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/kernel_registry.h"
