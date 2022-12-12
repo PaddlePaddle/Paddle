00195b295cbf48e7ea58f1ebe905788565c1633f
yuchen202 <103028470+yuchen202@users.noreply.github.com>
2022-12-13 00:52:39 +0800

    Update __init__.py

--- python/paddle/device/cuda/__init__.py
+++ python/paddle/device/cuda/__init__.py
@@ -396,7 +396,7 @@ def get_device_properties(device=None):
     Return the properties of given device.
 
     Args:
-        device(paddle.CUDAPlace or int or str): The device, the id of the device or
+        device(paddle.CUDAPlace or int or str, optional): The device, the id of the device or
             the string name of device like 'gpu:x' which to get the properties of the
             device from. If device is None, the device is the current device.
             Default: None.
