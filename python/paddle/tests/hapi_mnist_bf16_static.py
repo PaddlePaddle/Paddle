481be5fc9bba5903477c7c4ba2a487ad714bebc4
xu98bin <xuyubin@baidu.com>
2022-12-16 13:37:03 +0800

    Undo Modification python/paddle/tests/hapi_mnist_bf16_static.py

--- python/paddle/tests/hapi_mnist_bf16_static.py
+++ python/paddle/tests/hapi_mnist_bf16_static.py
@@ -105,7 +105,7 @@ def main(args):
                     'slice',
                     'reduce_mean',
                     'conv2d',
-                }
+                },
             ),
         )
 
