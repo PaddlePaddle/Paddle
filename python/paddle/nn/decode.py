75600d1f532b1d6b8a49dea02544a74118df6e9f
FrostML <380185688@qq.com>
2022-12-19 02:56:25 +0000

    examples update, test=document_fix

--- python/paddle/nn/decode.py
+++ python/paddle/nn/decode.py
@@ -1045,7 +1045,6 @@ def dynamic_decode(
 
         .. code-block:: python
 
-            import numpy as np
             import paddle
             from paddle.nn import BeamSearchDecoder, dynamic_decode
             from paddle.nn import GRUCell, Linear, Embedding
