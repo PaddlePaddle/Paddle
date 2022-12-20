d710b4689847184eb3ee509aeed2d8f9ab901452
zhoutianzi666 <17801055074@163.com>
2022-12-20 13:32:22 +0000

    nhwc_conv2d_bias -> conv2d_bias_impl

--- paddle/phi/kernels/fusion/cutlass/conv2d/conv2d_bias_relu.cu
+++ paddle/phi/kernels/fusion/cutlass/conv2d/conv2d_bias_relu.cu
@@ -22,7 +22,7 @@ namespace phi {
 namespace fusion {
 namespace cutlass_internal {
 template <typename TShape, typename WShape, int Alignment = 8>
-cutlass::Status nhwc_conv2d_bias_relu(ConvAllParams params) {
+cutlass::Status conv2d_bias_relu_impl(ConvAllParams params) {
   using ElementAccumulator = float;
   using ElementComputeEpilogue = float;
   using ElementInputA = cutlass::half_t;
@@ -133,60 +133,60 @@ cutlass::Status nhwc_conv2d_bias_relu(ConvAllParams params) {
 
 // config 0
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 64, 64>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 64, 64>,
                           cutlass::gemm::GemmShape<32, 32, 64>>(ConvAllParams);
 // config 1
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 32, 64>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 32, 64>,
                           cutlass::gemm::GemmShape<32, 32, 64>>(ConvAllParams);
 // config 2
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<128, 32, 64>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<128, 32, 64>,
                           cutlass::gemm::GemmShape<32, 32, 64>>(ConvAllParams);
 // config 3
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<128, 64, 64>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<128, 64, 64>,
                           cutlass::gemm::GemmShape<32, 32, 64>>(ConvAllParams);
 // config 4
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 64, 32>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 64, 32>,
                           cutlass::gemm::GemmShape<32, 32, 32>>(ConvAllParams);
 // config 5
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 128, 32>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 128, 32>,
                           cutlass::gemm::GemmShape<32, 64, 32>>(ConvAllParams);
 // config 6
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 128, 64>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 128, 64>,
                           cutlass::gemm::GemmShape<64, 64, 32>>(ConvAllParams);
 // config 7
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 256, 32>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 256, 32>,
                           cutlass::gemm::GemmShape<64, 64, 32>>(ConvAllParams);
 // config 8
 template cutlass::Status
-    nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<128, 64, 32>,
+    conv2d_bias_relu_impl<cutlass::gemm::GemmShape<128, 64, 32>,
                           cutlass::gemm::GemmShape<64, 32, 32>>(ConvAllParams);
 
 std::vector<std::function<cutlass::Status(ConvAllParams)>>
     conv2d_bias_relu_all_func = {
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 64, 64>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 64, 64>,
                               cutlass::gemm::GemmShape<32, 32, 64>>,
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 32, 64>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 32, 64>,
                               cutlass::gemm::GemmShape<32, 32, 64>>,
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<128, 32, 64>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<128, 32, 64>,
                               cutlass::gemm::GemmShape<32, 32, 64>>,
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<128, 64, 64>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<128, 64, 64>,
                               cutlass::gemm::GemmShape<32, 32, 64>>,
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 64, 32>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 64, 32>,
                               cutlass::gemm::GemmShape<32, 32, 32>>,
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 128, 32>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 128, 32>,
                               cutlass::gemm::GemmShape<32, 64, 32>>,
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 128, 64>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 128, 64>,
                               cutlass::gemm::GemmShape<64, 64, 32>>,
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<64, 256, 32>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<64, 256, 32>,
                               cutlass::gemm::GemmShape<64, 64, 32>>,
-        nhwc_conv2d_bias_relu<cutlass::gemm::GemmShape<128, 64, 32>,
+        conv2d_bias_relu_impl<cutlass::gemm::GemmShape<128, 64, 32>,
                               cutlass::gemm::GemmShape<64, 32, 32>>};
 std::map<std::vector<int>, int> map_problem_conv2d_bias_relu;
 
