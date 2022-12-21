96c4c6d2b4514acdecc4928defdff9adff8b7ca5
cyber-pioneer <chenzhuo@tju.edu.cn>
2022-12-21 07:38:29 +0000

    support aux and high-level function

--- python/paddle/fluid/tests/unittests/autograd/test_primapi.py
+++ python/paddle/fluid/tests/unittests/autograd/test_primapi.py
@@ -811,11 +811,120 @@ class TestGrad(unittest.TestCase):
             paddle.incubate.autograd.disable_prim()
             return out
 
+        def actual2():
+            """test usage type of grad like grad(outputs)(inputs)"""
+            paddle.incubate.autograd.enable_prim()
+            sp = paddle.static.Program()
+            mp = paddle.static.Program()
+            with paddle.static.program_guard(mp, sp):
+                feed, static_xs, static_v = utils.gen_static_data_and_feed(
+                    self.xs, self.v, stop_gradient=False
+                )
+                ys = (
+                    self.fun(*static_xs)
+                    if isinstance(static_xs, typing.Sequence)
+                    else self.fun(static_xs)
+                )
+                ys_grad = paddle.incubate.autograd.grad(ys)(static_xs, static_v)
+                paddle.incubate.autograd.prim2orig(mp.block(0))
+            exe = paddle.static.Executor()
+            exe.run(sp)
+            out = exe.run(mp, feed=feed, fetch_list=ys_grad)
+            paddle.incubate.autograd.disable_prim()
+            return out
+
         actual = actual()
         expected = expected()
         self.assertEqual(type(actual), type(expected))
         for i, j in zip(actual, expected):
             np.testing.assert_allclose(i, j, rtol=self._rtol, atol=self._atol)
+
+        actual2 = actual2()
+        self.assertEqual(type(actual2), type(expected))
+        for i, j in zip(actual2, expected):
+            np.testing.assert_allclose(i, j, rtol=self._rtol, atol=self._atol)
+
+    def test_grad_aux(self):
+        input1 = np.random.rand(2, 3).astype("float32")
+        input2 = np.random.rand(2, 3).astype("float32")
+        xv1 = np.ones((2, 3)).astype("float32")
+        xv2 = np.zeros((2, 3)).astype("float32")
+
+        def multi_outputs(x, y):
+            m = x * y
+            n = x + y
+            return m, n
+
+        def expected():
+            paddle.incubate.autograd.disable_prim()
+            sp = paddle.static.Program()
+            mp = paddle.static.Program()
+            with paddle.static.program_guard(mp, sp):
+                static_input1 = paddle.static.data(
+                    name="input1", shape=[2, 3], dtype="float32"
+                )
+                static_input2 = paddle.static.data(
+                    name="input2", shape=[2, 3], dtype="float32"
+                )
+                v1 = paddle.static.data(
+                    name="v1", shape=[2, 3], dtype="float32"
+                )
+                v2 = paddle.static.data(
+                    name="v2", shape=[2, 3], dtype="float32"
+                )
+                res, ys_grad = paddle.incubate.autograd.vjp(
+                    multi_outputs, (static_input1, static_input2), (v1, v2)
+                )
+            feed = {'input1': input1, 'input2': input2, 'v1': xv1, 'v2': xv2}
+            exe = paddle.static.Executor()
+            exe.run(sp)
+            res = exe.run(mp, feed=feed, fetch_list=[res, ys_grad])
+            paddle.incubate.autograd.enable_prim()
+            return res
+
+        def actual():
+            paddle_grad = paddle.incubate.autograd.grad
+            paddle.incubate.autograd.enable_prim()
+            main = paddle.static.Program()
+            startup = paddle.static.Program()
+            with paddle.static.program_guard(main, startup):
+                static_input1 = paddle.static.data(
+                    name="input1", shape=[2, 3], dtype="float32"
+                )
+                static_input2 = paddle.static.data(
+                    name="input2", shape=[2, 3], dtype="float32"
+                )
+                ys = multi_outputs(static_input1, static_input2)
+
+                res = paddle_grad(
+                    ys, (static_input1, static_input2), has_aux=True
+                )
+                paddle.incubate.autograd.prim2orig()
+
+            place = paddle.CPUPlace()
+            if paddle.device.is_compiled_with_cuda():
+                place = paddle.CUDAPlace(0)
+            exe = paddle.static.Executor(place)
+            exe.run(startup)
+            outs = exe.run(
+                main, feed={'input1': input1, 'input2': input2}, fetch_list=res
+            )
+            paddle.incubate.autograd.disable_prim()
+            return outs
+
+        expected = expected()
+        expected_gradients = expected[2:]
+        expected_aux = expected[1]
+        actual = actual()
+        actual_gradients = actual[:1]
+        actual_aux = actual[2]
+
+        self.assertEqual(type(expected_gradients), type(actual_gradients))
+        for i, j in zip(expected_gradients, actual_gradients):
+            np.testing.assert_allclose(i, j, rtol=self._rtol, atol=self._atol)
+        np.testing.assert_allclose(
+            expected_aux, actual_aux, rtol=self._rtol, atol=self._atol
+        )
 
     def test_illegal_param(self):
         paddle.incubate.autograd.enable_prim()
@@ -828,6 +937,40 @@ class TestGrad(unittest.TestCase):
             paddle.incubate.autograd.grad(
                 paddle.static.data('targets', shape=[1]), 1
             )
+
+        with self.assertRaises(TypeError):
+            paddle.incubate.autograd.grad(1)(
+                paddle.static.data('inputs', shape=[1])
+            )
+
+        with self.assertRaises(TypeError):
+            paddle.incubate.autograd.grad(
+                paddle.static.data('targets', shape=[1])
+            )(1)
+
+        paddle.incubate.autograd.disable_prim()
+
+    def test_illegal_case(self):
+        paddle.incubate.autograd.enable_prim()
+        free_var = paddle.static.data('free_var', shape=[1], dtype='float32')
+
+        def fn2(x):
+            y = x * x
+            return y, free_var
+
+        with self.assertRaises(RuntimeError):
+            startup_program = paddle.static.Program()
+            main_program = paddle.static.Program()
+            with paddle.static.program_guard(main_program, startup_program):
+                x = paddle.static.data('x', shape=[1], dtype='float32')
+                res = fn2(x)
+                paddle.incubate.autograd.grad(res)(x)
+
+        with self.assertRaises(RuntimeError):
+            x = paddle.static.data('x', shape=[1], dtype='float32')
+            res = fn2(x)
+            paddle.incubate.autograd.grad(res)(x)
+
         paddle.incubate.autograd.disable_prim()
 
     def test_disable_prim(self):
@@ -845,6 +988,27 @@ class TestGrad(unittest.TestCase):
                     else self.fun(static_xs)
                 )
                 ys_grad = paddle.incubate.autograd.grad(ys, static_xs, static_v)
+            exe = paddle.static.Executor()
+            exe.run(sp)
+            out = exe.run(mp, feed=feed, fetch_list=ys_grad)
+            paddle.incubate.autograd.enable_prim()
+            return out
+
+        def expected2():
+            """test usage type of grad like grad(outputs)(inputs)"""
+            paddle.incubate.autograd.disable_prim()
+            sp = paddle.static.Program()
+            mp = paddle.static.Program()
+            with paddle.static.program_guard(mp, sp):
+                feed, static_xs, static_v = utils.gen_static_data_and_feed(
+                    self.xs, self.v, stop_gradient=False
+                )
+                ys = (
+                    self.fun(*static_xs)
+                    if isinstance(static_xs, typing.Sequence)
+                    else self.fun(static_xs)
+                )
+                ys_grad = paddle.incubate.autograd.grad(ys)(static_xs, static_v)
             exe = paddle.static.Executor()
             exe.run(sp)
             out = exe.run(mp, feed=feed, fetch_list=ys_grad)
@@ -875,6 +1039,11 @@ class TestGrad(unittest.TestCase):
         expected = expected()
         self.assertEqual(type(actual), type(expected))
         for i, j in zip(actual, expected):
+            np.testing.assert_allclose(i, j, rtol=self._rtol, atol=self._atol)
+
+        expected2 = expected2()
+        self.assertEqual(type(actual), type(expected2))
+        for i, j in zip(actual, expected2):
             np.testing.assert_allclose(i, j, rtol=self._rtol, atol=self._atol)
 
 
@@ -1027,10 +1196,49 @@ class TestGradWithHigherOrder(unittest.TestCase):
             paddle.incubate.autograd.disable_prim()
             return outs
 
+        def actual2():
+            """test usage type of grad like grad(outputs)(inputs)"""
+            paddle_grad = paddle.incubate.autograd.grad
+            paddle.incubate.autograd.enable_prim()
+            main = paddle.static.Program()
+            startup = paddle.static.Program()
+            with paddle.static.program_guard(main, startup):
+                feed, static_xs, static_v = utils.gen_static_data_and_feed(
+                    self.xs, self.v, stop_gradient=False
+                )
+                ys = (
+                    self.fun_pd(*static_xs)
+                    if isinstance(static_xs, typing.Sequence)
+                    else self.fun_pd(static_xs)
+                )
+
+                grad1 = paddle_grad(ys)(static_xs, static_v)
+                grad2 = paddle_grad(grad1)(static_xs, static_v)
+                grad3 = paddle_grad(grad2)(static_xs, static_v)
+                grad4 = paddle_grad(grad3)(static_xs, static_v)
+                grad5 = paddle_grad(grad4)(static_xs, static_v)
+                paddle.incubate.autograd.prim2orig()
+
+            fetch_list = [grad3, grad4, grad5]
+
+            place = paddle.CPUPlace()
+            if paddle.device.is_compiled_with_cuda():
+                place = paddle.CUDAPlace(0)
+            exe = paddle.static.Executor(place)
+            exe.run(startup)
+            outs = exe.run(main, feed=feed, fetch_list=fetch_list)
+            paddle.incubate.autograd.disable_prim()
+            return outs
+
         actual = actual()
         expected = expected()
         self.assertEqual(type(actual), type(expected))
         for i, j in zip(actual, expected):
+            np.testing.assert_allclose(i, j, rtol=self._rtol, atol=self._atol)
+
+        actual2 = actual2()
+        self.assertEqual(type(actual2), type(expected))
+        for i, j in zip(actual2, expected):
             np.testing.assert_allclose(i, j, rtol=self._rtol, atol=self._atol)
 
 
