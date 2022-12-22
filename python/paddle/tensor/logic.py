d328b5a6442da3b9d34d5874381495b690d9f294
lizhiyu02 <1528794076@qq.com>
2022-12-22 13:15:32 +0000

    change the API doc of isclose

--- python/paddle/tensor/logic.py
+++ python/paddle/tensor/logic.py
@@ -958,19 +958,27 @@ def bitwise_not(x, out=None, name=None):
 @templatedoc()
 def isclose(x, y, rtol=1e-05, atol=1e-08, equal_nan=False, name=None):
     """
-    ${comment}
+    This operator checks if all :math:`x` and :math:`y` satisfy the condition:
+
+    .. math::
+
+        | x - y | <= atol + rtol X | y |
+
+    elementwise, for all elements of :math:`x` and :math:`y`. The behaviour of this
+    operator is analogous to :math:`numpy.isclose`, namely that it returns :math:`True` if
+    two tensors are elementwise equal within a tolerance.
 
     Args:
-        x(Tensor): ${input_comment}.
-        y(Tensor): ${other_comment}.
+        x(Tensor): The input tensor, it's data type should be float32, float64.
+        y(Tensor): The input tensor, it's data type should be float32, float64.
         rtol(rtoltype, optional): The relative tolerance. Default: :math:`1e-5` .
         atol(atoltype, optional): The absolute tolerance. Default: :math:`1e-8` .
-        equal_nan(equalnantype, optional): ${equal_nan_comment}.
+        equal_nan(equalnantype, optional): If :math:`True` , then two :math:`NaNs` will be compared as equal. Default: :math:`False` .
         name (str, optional): Name for the operation. For more information, please
             refer to :ref:`api_guide_Name`. Default: None.
 
     Returns:
-        Tensor: ${out_comment}.
+        Tensor: The output tensor, it's data type is bool.
 
     Examples:
         .. code-block:: python
