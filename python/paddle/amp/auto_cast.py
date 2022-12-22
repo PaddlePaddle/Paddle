fa77d99818899838eecd9b5fa2e3c562481fb77b
zkh2016 <zhangkaihuo@baidu.com>
2022-12-22 03:13:16 +0000

    fix example

--- python/paddle/amp/auto_cast.py
+++ python/paddle/amp/auto_cast.py
@@ -318,7 +318,7 @@ def amp_guard(
 
         data = np.random.uniform(-1, 1, [10, 3, 32, 32]).astype('float32')
         with paddle.fluid.dygraph.guard():
-            conv2d = paddle.fluid.dygraph.Conv2D(3, 2, 3)
+            conv2d = paddle.nn.Conv2D(3, 2, 3)
             data = paddle.fluid.dygraph.to_variable(data)
             with paddle.amp.amp_guard():
                 conv = conv2d(data)
