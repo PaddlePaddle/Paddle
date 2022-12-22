f1716c9dcb8f3b2fd1d974b985babb7faa11a4f0
JakubHulek <jakub.hulek@intel.com>
2022-12-22 14:08:58 +0100

    change mkldnn to onednn

--- python/paddle/fluid/tests/unittests/ir/inference/test_onednn_batch_norm_act_fuse_pass.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_onednn_batch_norm_act_fuse_pass.py
@@ -21,7 +21,7 @@ from auto_scan_test import PassAutoScanTest
 from program_config import OpConfig, ProgramConfig, TensorConfig
 
 
-class TestScaleMatmulMkldnnFusePass(PassAutoScanTest):
+class TestScaleOneDNNFusePass(PassAutoScanTest):
     def is_program_valid(self, program_config: ProgramConfig) -> bool:
         return True
 
