efec54804c5209bf902510dad31d09e7c25ae8ea
SigureMo <sigure.qaq@gmail.com>
2022-12-24 14:40:46 +0800

    remove duplicate license

--- paddle/fluid/inference/tensorrt/convert/c_allreduce_op.cc
+++ paddle/fluid/inference/tensorrt/convert/c_allreduce_op.cc
@@ -12,28 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/*
- * @Author: physico
- * @Date: 2022-12-19 16:59:04
- * @LastEditTime: 2022-12-19 17:02:33
- * @FilePath: /Paddle/paddle/fluid/inference/tensorrt/convert/c_allreduce_op.cc
- * @Description:
- * @Function List:
- */
-/* Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. */
-
 #include "paddle/fluid/inference/tensorrt/convert/op_converter.h"
 #include "paddle/fluid/inference/tensorrt/plugin/c_allreduce_op_plugin.h"
 
