4d3465d4511dd4925167c2d93b31c553a60af692
Sanbu <96160062+sanbuphy@users.noreply.github.com>
2022-12-28 16:48:35 +0800

    Update hybrid_parallel_inference.py

--- python/paddle/distributed/fleet/utils/hybrid_parallel_inference.py
+++ python/paddle/distributed/fleet/utils/hybrid_parallel_inference.py
@@ -207,7 +207,7 @@ class HybridParallelInferenceHelper:
             self._device = "gpu"
         assert self._device, "Only gpu and npu are supported."
 
-        assert not in_dygraph_mode(), "Only static mode is supported."
+        assert not in_dygraph_mode(), "Only static graph mode is supported."
 
         op_maker = core.op_proto_and_checker_maker
         self._op_role = op_maker.OpRole
