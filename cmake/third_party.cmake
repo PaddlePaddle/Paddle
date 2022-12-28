c60098211fc6aa193379c1f072a7d7f2b14167c5
zhengqiwen1997 <zhengqiwen_au@163.com>
2022-12-28 08:43:47 +0000

    test if linux cuda11.6 and above need cub

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -321,8 +321,7 @@ endif()
 
 if(WITH_GPU)
   if(${CMAKE_CUDA_COMPILER_VERSION} LESS 11.0
-     OR (${CMAKE_CUDA_COMPILER_VERSION} GREATER_EQUAL 11.6
-         AND ${CMAKE_CUDA_COMPILER_VERSION} LESS 11.8))
+     OR (WIN32 AND ${CMAKE_CUDA_COMPILER_VERSION} GREATER_EQUAL 11.6))
     include(external/cub) # download cub
     list(APPEND third_party_deps extern_cub)
   endif()
