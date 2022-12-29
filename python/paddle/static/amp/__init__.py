1ecdcffab32a79c968de2d4ed9df46252cbf3ed2
zhangkaihuo <zhangkaihuo@baidu.com>
2022-12-29 09:15:29 +0000

    fix

--- python/paddle/static/amp/__init__.py
+++ python/paddle/static/amp/__init__.py
@@ -13,8 +13,11 @@
 # limitations under the License.
 
 from . import decorator
+from .decorator import decorate
 from . import fp16_lists
+from .fp16_lists import CustomOpLists, AutoMixedPrecisionLists
 from . import fp16_utils
+from .fp16_utils import fp16_guard, cast_model_to_fp16, cast_parameters_to_fp16
 from . import bf16
 
 __all__ = []
