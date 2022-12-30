45905f35d98f912bb0f71f470599b16997e12b4a
yuanlehome <yuanlehome@163.com>
2022-12-30 11:57:08 +0000

    fix fc kerenl diff

--- paddle/phi/kernels/funcs/fc_functor.cu
+++ paddle/phi/kernels/funcs/fc_functor.cu
@@ -149,12 +149,13 @@ __global__ void bias_relu_v2(const int num,
 #if __CUDA_ARCH__ >= 800
       packed_val = __hmax2(__half2(0, 0), packed_val);
 #elif __CUDA_ARCH__ >= 530
-      packed_val = __hmul2(__hgt2(__half2(0, 0), packed_val), packed_val);
+      half zero = 0.0f;
+      packed_val.x = __hmul(packed_val.x > zero, dpacked_val.x);
+      packed_val.y = __hmul(packed_val.y > zero, dpacked_val.y);
 #else
-      packed_val.x = static_cast<int>(static_cast<float>(packed_val.x) > 0) *
-                     static_cast<float>(packed_val.x);
-      packed_val.y = static_cast<int>(static_cast<float>(packed_val.y) > 0) *
-                     static_cast<float>(packed_val.y);
+      half zero = 0.0f;
+      packed_val.x = static_cast<half>(packed_val.x > zero) * packed_val.x;
+      packed_val.y = static_cast<half>(packed_val.y > zero) * packed_val.y;
 #endif
     }
     data[tid] = packed_val;
