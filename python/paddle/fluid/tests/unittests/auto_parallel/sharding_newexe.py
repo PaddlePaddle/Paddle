4e1cf9bf512585d61da8cd5a6d9d4fa188405f97
JZ-LIANG <jianzhongliang10@gmail.com>
2022-12-30 11:52:14 +0800

    new unitest

---
+++ python/paddle/fluid/tests/unittests/auto_parallel/sharding_newexe.py
@@ -0,0 +1,113 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+import unittest
+
+import numpy as np
+from get_gpt_model import FakeDataset, generate_model
+
+import paddle
+from paddle.distributed.fleet import auto
+from paddle.fluid.dygraph.parallel import ParallelEnv
+
+paddle.enable_static()
+
+
+def apply_pass(use_sharding=False):
+    strategy = auto.Strategy()
+    strategy.auto_mode = "semi"
+    strategy.reinit = True
+
+    if use_sharding:
+        os.environ["FLAGS_CONVERT_GRAPH_TO_PROGRAM"] = str(1)
+        os.environ["FLAGS_add_dependency_for_communication_op"] = 'false'
+        sharding = strategy.sharding
+        sharding.enable = True
+        sharding.degree = 2
+        sharding.stage = 2
+        sharding.enable_overlap = True
+        sharding.param_comm_stream_num = 2
+        sharding.grad_comm_stream_num = 2
+        sharding.param_bucket_size_numel = 2
+        sharding.grad_bucket_size_numel = 2
+        sharding.partition_algor = 'use_order'
+
+    return strategy
+
+
+def reset_prog():
+    paddle.fluid.framework.switch_main_program(paddle.static.Program())
+    paddle.fluid.framework.switch_startup_program(paddle.static.Program())
+
+
+class TestShardingStage2WithNewEXE(unittest.TestCase):
+    def setUp(self):
+        self.batch_size = 2
+        self.batch_num = 1
+        self.clip_norm = 0.2
+        self.dataset = FakeDataset(self.batch_size * self.batch_num)
+
+    def init(self, engine):
+        paddle.seed(2022)
+        np.random.seed(2022)
+        random.seed(2022)
+        place = paddle.fluid.CUDAPlace(ParallelEnv().dev_id)
+        engine._executor = paddle.static.Executor(place)
+
+    def get_engine(self, use_sharding=False):
+        reset_prog()
+
+        strategy = apply_pass(use_sharding)
+        clip = paddle.nn.ClipGradByNorm(self.clip_norm)
+        opt = paddle.optimizer.AdamW(learning_rate=0.00001, grad_clip=clip)
+        model, loss = generate_model("dp")
+        engine = auto.Engine(model, loss, opt, strategy=strategy)
+        self.init(engine)
+        return engine
+
+    def check_result(self, dp_params, sharding_params):
+        assert len(dp_params) == len(sharding_params)
+        for dp_p, sharding_p in zip(dp_params, sharding_params):
+            np.testing.assert_allclose(
+                dp_p,
+                sharding_p,
+                rtol=1e-05,
+                atol=1e-08,
+                err_msg='gradient clip by global norm has wrong results!, \nu={}\nv={}\ndiff={}'.format(
+                    dp_p, sharding_p, dp_p - sharding_p
+                ),
+            )
+
+    def test_grad_clip(self):
+        # dp2 training
+        dp_engine = self.get_engine(True)
+        dp_engine.fit(self.dataset, 3, batch_size=self.batch_size)
+        with open(
+            "/main_prog.txt.{}".format(paddle.distributed.get_rank()), "w+"
+        ) as f:
+            f.write(str(dp_engine.main_program))
+
+        # # dp2sharding2 training
+        # sharding_engine = self.get_engine(True)
+        # sharding_engine.fit(self.dataset, 3, batch_size=self.batch_size)
+        # sharding_param_values = get_parameter_value(
+        #     sharding_engine.main_program
+        # )
+
+        # self.check_result(dp_param_values, sharding_param_values)
+
+
+if __name__ == "__main__":
+    unittest.main()
