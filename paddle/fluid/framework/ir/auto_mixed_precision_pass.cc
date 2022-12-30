419058f054a386bdd0dbf185122157daad8873f3
yuanlehome <yuanlehome@163.com>
2022-12-30 06:08:03 +0000

    disable scale op in amp pass

--- paddle/fluid/framework/ir/auto_mixed_precision_pass.cc
+++ paddle/fluid/framework/ir/auto_mixed_precision_pass.cc
@@ -178,6 +178,7 @@ void AutoMixedPrecisionPass::SetDefaultBlacklist() const {
       "rsqrt",
       "sum",
       "cos_sim",
+      "scale",
       "softmax_with_cross_entropy",
       "sigmoid_cross_entropy_with_logits",
       "c_softmax_with_cross_entropy",
