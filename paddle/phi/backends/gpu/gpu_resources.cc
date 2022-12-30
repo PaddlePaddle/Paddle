f06e2bc8fb045597796d30afab798dc4d19e5a46
MARD1NO <359521840@qq.com>
2022-12-30 03:37:55 +0000

    refine codestyle

--- paddle/phi/backends/gpu/gpu_resources.cc
+++ paddle/phi/backends/gpu/gpu_resources.cc
@@ -59,6 +59,7 @@ void InitGpuProperties(Place place,
 
   const gpuDeviceProp& prop =
       backends::gpu::GetDeviceProperties(place.GetDeviceId());
+
   static const std::set<int> compiled_archs{CUDA_REAL_ARCHS};
   // Make sure compiled cuda arch is as same as runtime cuda arch.
   if (compiled_archs.find(*compute_capability) == compiled_archs.cend() &&
