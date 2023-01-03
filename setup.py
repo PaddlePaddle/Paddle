fcb6c1e20e5caf4153f11b03cd83dbafbd93fd29
risemeup1 <515586620@qq.com>
2023-01-03 03:53:56 +0000

    modify setup.py

--- setup.py
+++ setup.py
@@ -703,12 +703,12 @@ def build_steps():
         os.remove(cmake_cache_file_path)
 
     CMAKE_GENERATOR = get_cmake_generator()
-    bool_ninja = (CMAKE_GENERATOR == "Ninja")
+    bool_ninja = CMAKE_GENERATOR == "Ninja"
     build_ninja_file_path = os.path.join(build_path, "build.ninja")
     if os.path.exists(cmake_cache_file_path) and not (
         bool_ninja and not os.path.exists(build_ninja_file_path)
     ):
-        print("Do not need rerun camke,everything is ready,run build now")
+        print("Do not need rerun camke, everything is ready, run build now")
     else:
         cmake_run(build_path)
     # make
