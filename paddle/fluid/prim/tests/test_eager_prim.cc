f442b43db352ae21eb96a7b68704273163e7610e
JiabinYang <360788950@qq.com>
2023-01-03 04:49:02 +0000

    fix dygraph error

--- paddle/fluid/prim/tests/test_eager_prim.cc
+++ paddle/fluid/prim/tests/test_eager_prim.cc
@@ -16,29 +16,16 @@
 
 #include "glog/logging.h"
 #include "gtest/gtest.h"
-#include "paddle/fluid/eager/accumulation/accumulation_node.h"
-#include "paddle/fluid/eager/api/all.h"
-#include "paddle/fluid/eager/autograd_meta.h"
+#include "paddle/fluid/eager/api/generated/eager_generated/forwards/dygraph_functions.h"
+#include "paddle/fluid/eager/api/utils/hook_utils.h"
 #include "paddle/fluid/eager/backward.h"
-#include "paddle/fluid/eager/grad_node_info.h"
-#include "paddle/fluid/eager/hooks.h"
 #include "paddle/fluid/eager/tests/test_utils.h"
+#include "paddle/fluid/prim/utils/utils.h"
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/tensor_meta.h"
 
-PD_DECLARE_KERNEL(tanh, CPU, ALL_LAYOUT);
-PD_DECLARE_KERNEL(tanh_grad, CPU, ALL_LAYOUT);
-PD_DECLARE_KERNEL(pow, CPU, ALL_LAYOUT);
-PD_DECLARE_KERNEL(scale, CPU, ALL_LAYOUT);
-PD_DECLARE_KERNEL(multiply, CPU, ALL_LAYOUT);
-#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
-PD_DECLARE_KERNEL(tanh, GPU, ALL_LAYOUT);
-PD_DECLARE_KERNEL(tanh_grad, KPS, ALL_LAYOUT);
-PD_DECLARE_KERNEL(pow, CPU, ALL_LAYOUT);
-PD_DECLARE_KERNEL(scale, CPU, ALL_LAYOUT);
-PD_DECLARE_KERNEL(multiply, CPU, ALL_LAYOUT);
-#endif
+DECLARE_bool(prim_enabled);
 
 namespace paddle {
 namespace prim {
@@ -48,30 +35,60 @@ TEST(EagerPrim, TanhBackwardTest) {
   eager_test::InitEnv(paddle::platform::CPUPlace());
   // 2. pre
   paddle::framework::DDim ddim = phi::make_ddim({4, 16, 16, 32});
-  paddle::experimental::Tensor tensor =
-      egr_utils_api::CreateTensorWithValue(ddim,
-                                           paddle::platform::CPUPlace(),
-                                           phi::DataType::FLOAT32,
-                                           phi::DataLayout::NCHW,
-                                           5.0 /*value*/,
-                                           true /*is_leaf*/);
-  egr_utils_api::RetainGradForTensor(tensor);
-
-  // 3. Run Forward
-  paddle::experimental::Tensor out = tanh_ad_func(
-      tensor, scale, bias, true /*bias_after_scale*/, true /*trace_backward*/);
-
-  std::vector<paddle::experimental::Tensor> outs = {out};
-
+  paddle::experimental::Tensor tensor0 =
+      ::egr::egr_utils_api::CreateTensorWithValue(ddim,
+                                                  paddle::platform::CPUPlace(),
+                                                  phi::DataType::FLOAT32,
+                                                  phi::DataLayout::NCHW,
+                                                  5.0 /*value*/,
+                                                  true /*is_leaf*/);
+  ::egr::egr_utils_api::RetainGradForTensor(tensor0);
+  paddle::experimental::Tensor tensor1 =
+      ::egr::egr_utils_api::CreateTensorWithValue(ddim,
+                                                  paddle::platform::CPUPlace(),
+                                                  phi::DataType::FLOAT32,
+                                                  phi::DataLayout::NCHW,
+                                                  5.0 /*value*/,
+                                                  true /*is_leaf*/);
+  ::egr::egr_utils_api::RetainGradForTensor(tensor1);
+  // 3. Run Forward once
+  paddle::experimental::Tensor out0 = tanh_ad_func(tensor0);
+  std::vector<paddle::experimental::Tensor> outs0 = {out0};
+  // Disable prim
+  FLAGS_prim_enabled = false;
   // 4. Run Backward
-  Backward(outs, {});
+  egr::Backward(outs0, {}, false);
 
-  VLOG(7) << "Target Grad is: "
-          << std::static_pointer_cast<phi::DenseTensor>(
-                 EagerUtils::unsafe_autograd_meta(tensor)->Grad().impl())
-                 ->data<float>()[0];
+  paddle::experimental::Tensor out1 = tanh_ad_func(tensor1);
+  std::vector<paddle::experimental::Tensor> outs1 = {out1};
+  // Disable prim
+  FLAGS_prim_enabled = true;
+  ASSERT_TRUE(PrimCommonUtils::IsPrimEnabled());
+  // 4. Run Backward
+  ::egr::Backward(outs1, {}, false);
+  VLOG(7)
+      << "Target Grad is: "
+      << std::static_pointer_cast<phi::DenseTensor>(
+             ::egr::EagerUtils::unsafe_autograd_meta(tensor0)->Grad().impl())
+             ->data<float>()[0];
+  VLOG(7)
+      << "Result Grad is: "
+      << std::static_pointer_cast<phi::DenseTensor>(
+             ::egr::EagerUtils::unsafe_autograd_meta(tensor1)->Grad().impl())
+             ->data<float>()[0];
   // Examine Backward Grad
-  eager_test::CompareGradTensorWithValue<float>(tensor, 2.0);
+  eager_test::CompareGradTensorWithValue<float>(
+      tensor1,
+      std::static_pointer_cast<phi::DenseTensor>(
+          ::egr::EagerUtils::unsafe_autograd_meta(tensor0)->Grad().impl())
+          ->data<float>()[0]);
+}
+
+TEST(EagerPrim, TestFlags) {
+  FLAGS_prim_enabled = true;
+  ASSERT_TRUE(PrimCommonUtils::IsPrimEnabled());
+  FLAGS_prim_enabled = false;
+  ASSERT_FALSE(PrimCommonUtils::IsPrimEnabled());
 }
 
 }  // namespace prim
