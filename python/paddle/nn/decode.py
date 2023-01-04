d3be70315abca4dbae62180651c914392ea4fc39
zhengqiwen1997 <zhengqiwen_au@163.com>
2023-01-04 12:20:06 +0000

    default_main_program should import from common_ops_import

--- python/paddle/nn/decode.py
+++ python/paddle/nn/decode.py
@@ -19,7 +19,8 @@ import warnings
 import numpy as np
 
 import paddle
-from paddle.framework import _non_static_mode, default_main_program
+from paddle.common_ops_import import default_main_program
+from paddle.framework import _non_static_mode
 
 from ..fluid.data_feeder import convert_dtype
 from ..fluid.layers.utils import flatten, map_structure
