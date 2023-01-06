36e3be89a1bccef5411af34d45c39e9694109ae8
risemeup1 <515586620@qq.com>
2023-01-06 14:24:52 +0000

    debug

--- paddle/fluid/distributed/fleet_executor/dist_model.cc
+++ paddle/fluid/distributed/fleet_executor/dist_model.cc
@@ -437,9 +437,9 @@ bool DistModel::PrepareFleetExe() {
     RankInfo *rank_info = executor_desc_.add_cluster_info();
     rank_info->set_rank(i);
     VLOG(3) << "start rank_info";
-    rank_info->set_ip_port(config_.trainer_endpoints[i]);
+    // rank_info->set_ip_port(config_.trainer_endpoints[i]);
+    *(rank_info->mutable_ip_port()) = config_.trainer_endpoints[i];
     VLOG(3) << "stop rank_info";
-    // *(rank_info->mutable_ip_port()) = config_.trainer_endpoints[i];
     id_to_rank.insert({i, i});
   }
   fleet_exe.reset(new FleetExecutor(executor_desc_));
