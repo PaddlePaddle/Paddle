8db2c3b24e14b81b327a770eda338cd8015dbd88
duanyanhui <duanyanhui@baidu.com>
2023-01-06 10:57:20 +0800

    remove unused comments

--- paddle/phi/backends/stream.cc
+++ paddle/phi/backends/stream.cc
@@ -84,8 +84,6 @@ void Stream::WaitCallback() const { callback_manager_->Wait(); }
 void Stream::Destroy() {
   if (own_data_ && stream_ != nullptr) {
     phi::DeviceManager::SetDevice(place_);
-    // Wait();
-    // WaitCallback();
     device_->DestroyStream(this);
     own_data_ = false;
     stream_ = nullptr;
