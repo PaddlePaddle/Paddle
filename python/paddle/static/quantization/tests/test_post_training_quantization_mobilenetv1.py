1f1cb66d930061635c7d7a033fc4238ce768de60
yghstill <742925032@qq.com>
2023-01-06 07:14:08 +0000

    fix UT

--- python/paddle/static/quantization/tests/test_post_training_quantization_mobilenetv1.py
+++ python/paddle/static/quantization/tests/test_post_training_quantization_mobilenetv1.py
@@ -575,5 +575,131 @@ class TestPostTrainingAvgONNXFormatForMobilenetv1(TestPostTrainingQuantization):
         )
 
 
+class TestPostTrainingAvgONNXFormatForMobilenetv1TensorRT(
+    TestPostTrainingQuantization
+):
+    def test_post_training_onnx_format_mobilenetv1_tensorrt(self):
+        model = "MobileNet-V1"
+        algo = "avg"
+        round_type = "round"
+        data_urls = [
+            'https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/MobileNetV1_infer.tar'
+        ]
+        data_md5s = ['5ee2b1775b11dc233079236cdc216c2e']
+        quantizable_op_type = [
+            "conv2d",
+            "depthwise_conv2d",
+            "mul",
+        ]
+        is_full_quantize = False
+        is_use_cache_file = False
+        is_optimize_model = False
+        onnx_format = True
+        diff_threshold = 0.05
+        batch_nums = 10
+        deploy_backend = "tensorrt"
+        self.run_test(
+            model,
+            'inference.pdmodel',
+            'inference.pdiparams',
+            algo,
+            round_type,
+            data_urls,
+            data_md5s,
+            quantizable_op_type,
+            is_full_quantize,
+            is_use_cache_file,
+            is_optimize_model,
+            diff_threshold,
+            onnx_format=onnx_format,
+            batch_nums=batch_nums,
+            deploy_backend=deploy_backend,
+        )
+
+
+class TestPostTrainingKLONNXFormatForMobilenetv1MKLDNN(
+    TestPostTrainingQuantization
+):
+    def test_post_training_onnx_format_mobilenetv1_mkldnn(self):
+        model = "MobileNet-V1"
+        algo = "ptf"
+        round_type = "round"
+        data_urls = [
+            'https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/MobileNetV1_infer.tar'
+        ]
+        data_md5s = ['5ee2b1775b11dc233079236cdc216c2e']
+        quantizable_op_type = [
+            "conv2d",
+            "depthwise_conv2d",
+            "mul",
+        ]
+        is_full_quantize = False
+        is_use_cache_file = False
+        is_optimize_model = False
+        onnx_format = True
+        diff_threshold = 0.05
+        batch_nums = 2
+        deploy_backend = "mkldnn"
+        self.run_test(
+            model,
+            'inference.pdmodel',
+            'inference.pdiparams',
+            algo,
+            round_type,
+            data_urls,
+            data_md5s,
+            quantizable_op_type,
+            is_full_quantize,
+            is_use_cache_file,
+            is_optimize_model,
+            diff_threshold,
+            onnx_format=onnx_format,
+            batch_nums=batch_nums,
+            deploy_backend=deploy_backend,
+        )
+
+
+class TestPostTrainingAvgONNXFormatForMobilenetv1ARMCPU(
+    TestPostTrainingQuantization
+):
+    def test_post_training_onnx_format_mobilenetv1_armcpu(self):
+        model = "MobileNet-V1"
+        algo = "avg"
+        round_type = "round"
+        data_urls = [
+            'https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/MobileNetV1_infer.tar'
+        ]
+        data_md5s = ['5ee2b1775b11dc233079236cdc216c2e']
+        quantizable_op_type = [
+            "conv2d",
+            "depthwise_conv2d",
+            "mul",
+        ]
+        is_full_quantize = False
+        is_use_cache_file = False
+        is_optimize_model = True
+        onnx_format = True
+        diff_threshold = 0.05
+        batch_nums = 3
+        deploy_backend = "arm"
+        self.run_test(
+            model,
+            'inference.pdmodel',
+            'inference.pdiparams',
+            algo,
+            round_type,
+            data_urls,
+            data_md5s,
+            quantizable_op_type,
+            is_full_quantize,
+            is_use_cache_file,
+            is_optimize_model,
+            diff_threshold,
+            onnx_format=onnx_format,
+            batch_nums=batch_nums,
+            deploy_backend=deploy_backend,
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
