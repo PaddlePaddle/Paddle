af3c0c492a15ddc775fa6aa612969aa08508e155
knamg <117444637+knamg@users.noreply.github.com>
2023-01-08 10:11:05 +0800

    delete numpy,test=document_fix,test=docs_preview

--- python/paddle/nn/layer/common.py
+++ python/paddle/nn/layer/common.py
@@ -1178,11 +1178,10 @@ class ZeroPad2D(Layer):
 
             import paddle
             import paddle.nn as nn
-            import numpy as np
 
-            input_shape = (1, 1, 2, 3)
+            input_shape = paddle.to_tensor([1, 1, 2, 3])
             pad = [1, 0, 1, 2]
-            data = paddle.arange(np.prod(input_shape), dtype="float32").reshape(input_shape) + 1
+            data = paddle.arange(paddle.prod(input_shape), dtype="float32").reshape(input_shape) + 1
 
             my_pad = nn.ZeroPad2D(padding=pad)
             result = my_pad(data)
