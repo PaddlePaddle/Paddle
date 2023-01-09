5e835d3698c60f0d51382226b297bfbba3d81f99
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-01-09 14:07:34 +0800

    [Dy2St] RandomHorizontalFlip (#49590)
    * Dy2St_RandomHorizontalFlip
    
    * Dy2St_RandomHorizontalFlip

--- python/paddle/tests/test_transforms_static.py
+++ python/paddle/tests/test_transforms_static.py
@@ -100,6 +100,16 @@ class TestRandomVerticalFlip1(TestTransformUnitTestBase):
         self.api = transforms.RandomVerticalFlip(prob=1)
 
 
+class TestRandomHorizontalFlip0(TestTransformUnitTestBase):
+    def set_trans_api(self):
+        self.api = transforms.RandomHorizontalFlip(0)
+
+
+class TestRandomHorizontalFlip1(TestTransformUnitTestBase):
+    def set_trans_api(self):
+        self.api = transforms.RandomHorizontalFlip(1)
+
+
 class TestRandomCrop_random(TestTransformUnitTestBase):
     def get_shape(self):
         return (3, 240, 240)
