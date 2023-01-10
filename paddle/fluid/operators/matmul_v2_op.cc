60b2eecce6c387d4b682034c2dbd3cc9d043824d
Silv3S <slawomir.siwek@intel.com>
2023-01-10 16:36:57 +0100

    codestyle

--- paddle/fluid/operators/matmul_v2_op.cc
+++ paddle/fluid/operators/matmul_v2_op.cc
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "paddle/fluid/operators/matmul_v2_op.h"
+
 #include <string>
 #include <vector>
 
 #include "paddle/fluid/framework/infershape_utils.h"
-#include "paddle/fluid/framework/op_registry.h"
 #include "paddle/phi/core/infermeta_utils.h"
 #include "paddle/phi/infermeta/backward.h"
 
