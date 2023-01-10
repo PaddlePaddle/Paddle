2f6acb7098954e49e807f01b01922a90b65baef0
wenbin <wang3323032@qq.com>
2023-01-10 07:21:33 +0000

    fix compile

--- paddle/fluid/inference/tensorrt/plugin/preln_groupnorm_act_op_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/preln_groupnorm_act_op_plugin.cu
@@ -16,7 +16,7 @@ limitations under the License. */
 
 #include "paddle/fluid/inference/tensorrt/plugin/preln_groupnorm_act_op_plugin.h"
 #include <cub/cub.cuh>
-#include "paddle/fluid/inference/tensorrt/plugin/common/groupNormPluginCommon.h"
+#include "paddle/fluid/inference/tensorrt/plugin/common/common.cuh"
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/common/layout.h"
 #include "paddle/phi/kernels/funcs/math_function.h"
