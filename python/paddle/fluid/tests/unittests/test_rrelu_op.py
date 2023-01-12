a8f20057f389c66589bd7d4ff82012e15606edbb
Weilong Wu <veyron_wu@163.com>
2023-01-12 12:39:55 +0000

    [PHI] rrelu add yaml

--- python/paddle/fluid/tests/unittests/test_rrelu_op.py
+++ python/paddle/fluid/tests/unittests/test_rrelu_op.py
@@ -311,6 +311,12 @@ class TestFunctionalRReluAPI(unittest.TestCase):
         self.assertRaises(ValueError, error_lower_upper)
 
 
+def rrelu(x, lower=1.0 / 8.0, upper=1.0 / 3.0, training=True, name=None):
+    return paddle.nn.functional.rrelu(
+        x, lower, upper, training=not training, name=None
+    )
+
+
 class RReluTest(OpTest):
     def setUp(self):
         self.op_type = "rrelu"
@@ -318,6 +324,10 @@ class RReluTest(OpTest):
         self.upper = 0.3
         self.is_test = True
         self.init_params()
+        self.python_api = rrelu
+        self.python_out_sig = [
+            "Out"
+        ]  # python out sig is customized output signature.
 
     def init_params(self):
         self.dtype = "float64"
@@ -337,10 +347,12 @@ class RReluTest(OpTest):
         }
 
     def test_check_output(self):
-        self.check_output()
+        self.check_output(
+            no_check_set=['Noise'], check_eager=True
+        )  # no_check_set=['Noise'],
 
     def test_check_grad(self):
-        self.check_grad(['X'], 'Out')
+        self.check_grad(['X'], 'Out', check_eager=True)
 
 
 class RReluTrainingTest(RReluTest):
@@ -350,6 +362,10 @@ class RReluTrainingTest(RReluTest):
         self.upper = 0.300000009
         self.is_test = False
         self.init_params()
+        self.python_api = rrelu
+        self.python_out_sig = [
+            "Out"
+        ]  # python out sig is customized output signature.
 
 
 if __name__ == "__main__":
