7e98716aa55edead5c96f905fa1c7d124f4ffd55
JiabinYang <360788950@qq.com>
2023-01-12 15:31:59 +0000

    support sub vjp

--- paddle/fluid/prim/api/manual/backward/composite_backward_api.h
+++ paddle/fluid/prim/api/manual/backward/composite_backward_api.h
@@ -28,6 +28,40 @@ void tanh_grad(const Tensor& out, const Tensor& grad_out, Tensor* grad_x) {
   grad_x->set_impl(grad_x_tmp.impl());
 }
 template <typename T>
+void subtract_grad(const Tensor& x,
+                   const Tensor& y,
+                   const Tensor& out_grad,
+                   int axis,
+                   Tensor* dx,
+                   Tensor* dy) {
+  if (dy) {
+    auto scale_out_grad = scale<T>(out_grad, -1.0, 0.0, true);
+    if (phi::product(x.dims()) > phi::product(y.dims())) {
+      // Maybe need reduce here
+      phi::DDim reduce_dim = get_reduce_dims(x.dims(), y.dims());
+      auto dy_reduce_res =
+          sum<T>(scale_out_grad, phi::vectorize(reduce_dim), y.dtype(), false);
+      auto dy_tmp = reshape<T>(dy_reduce_res, phi::vectorize(y.dims()));
+      dy->set_impl(dy_tmp.impl());
+    } else {
+      by_pass<T>(scale_out_grad, dy);
+    }
+  }
+  if (dx) {
+    if (phi::product(y.dims()) > phi::product(x.dims())) {
+      // Maybe need reduce here
+      auto reduce_dim = get_reduce_dims(y.dims(), x.dims());
+      auto dx_reduce_res =
+          sum<T>(out_grad, phi::vectorize(reduce_dim), x.dtype(), false);
+      auto dx_tmp = reshape<T>(dx_reduce_res, phi::vectorize(x.dims()));
+      dx->set_impl(dx_tmp.impl());
+    } else {
+      by_pass<T>(out_grad, dx);
+    }
+  }
+}
+
+template <typename T>
 void add_grad(const Tensor& x,
               const Tensor& y,
               const Tensor& out_grad,
