77de2ee36694a56a684c388e092b80124078c63b
wangxn12138 <wangxn12138@gmail.com>
2023-01-13 15:49:44 +0800

    fix codestyle

--- paddle/fluid/distributed/ps/table/graph/graph_weighted_sampler.h
+++ paddle/fluid/distributed/ps/table/graph/graph_weighted_sampler.h
@@ -56,9 +56,9 @@ class WeightedSampler : public Sampler {
 
  private:
   int sample(float query_weight,
-             std::unordered_map<WeightedSampler *, float> &subtract_weight_map,
-             std::unordered_map<WeightedSampler *, int> &subtract_count_map,
-             float &subtract);
+             std::unordered_map<WeightedSampler *, float> &subtract_weight_map,  // NOLINT
+             std::unordered_map<WeightedSampler *, int> &subtract_count_map,     // NOLINT
+             float &subtract);                                                   // NOLINT
 };
 }  // namespace distributed
 }  // namespace paddle
--- paddle/fluid/distributed/test/graph_node_split_test.cc
+++ paddle/fluid/distributed/test/graph_node_split_test.cc
@@ -157,7 +157,8 @@ void GetDownpourSparseTableProto(
 
 /*-------------------------------------------------------------------------*/
 
-std::string ip_ = "127.0.0.1", ip2 = "127.0.0.1";
+const char* ip_ = "127.0.0.1";
+const char* ip2 = "127.0.0.1";
 uint32_t port_ = 5209, port2 = 5210;
 
 std::vector<std::string> host_sign_list_;
@@ -204,7 +205,7 @@ void RunServer2() {
 }
 
 void RunClient(
-    std::map<uint64_t, std::vector<paddle::distributed::Region>>& dense_regions,
+    std::map<uint64_t, std::vector<paddle::distributed::Region>>& dense_regions,  // NOLINT
     int index,
     paddle::distributed::PsBaseService* service) {
   ::paddle::distributed::PSParameter worker_proto = GetWorkerProto();
