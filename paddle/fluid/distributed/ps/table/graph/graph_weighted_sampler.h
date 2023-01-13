32c7acc7df2acc4794ce72b3e1b520cb71297f8b
wangxn12138 <wangxn12138@gmail.com>
2023-01-13 16:17:48 +0800

    fix codestyle

--- paddle/fluid/distributed/ps/table/graph/graph_weighted_sampler.h
+++ paddle/fluid/distributed/ps/table/graph/graph_weighted_sampler.h
@@ -55,10 +55,12 @@ class WeightedSampler : public Sampler {
                                     const std::shared_ptr<std::mt19937_64> rng);
 
  private:
-  int sample(float query_weight,
-             std::unordered_map<WeightedSampler *, float> &subtract_weight_map,  // NOLINT
-             std::unordered_map<WeightedSampler *, int> &subtract_count_map,     // NOLINT
-             float &subtract);                                                   // NOLINT
+  int sample(
+      float query_weight,
+      std::unordered_map<WeightedSampler *, float>
+          &subtract_weight_map,                                        // NOLINT
+      std::unordered_map<WeightedSampler *, int> &subtract_count_map,  // NOLINT
+      float &subtract);                                                // NOLINT
 };
 }  // namespace distributed
 }  // namespace paddle
