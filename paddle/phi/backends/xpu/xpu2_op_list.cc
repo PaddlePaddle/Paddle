PaddlePaddle/Paddle

commit 30f5e39b6c6031da2116488b03d7a5e23f04a4f7

[PHI]Rename some PHI Kernel (#49470)
* rename kernel

* delete sig

* modify code according comment

* fix ci bugs

paddle/phi/backends/xpu/xpu2_op_list.cc
@@ -270,7 +270,7 @@ XPUOpMap& get_kl2_ops() {
                      phi::DataType::INT8,
                      phi::DataType::FLOAT16,
                      phi::DataType::FLOAT32})},
-      {"flatten_with_xshape",
+      {"flatten",
        XPUKernelSet({phi::DataType::INT64,
                      phi::DataType::INT32,
                      phi::DataType::INT8,
@@ -450,7 +450,7 @@ XPUOpMap& get_kl2_ops() {
                      phi::DataType::INT32,
                      phi::DataType::BOOL,
                      phi::DataType::FLOAT32})},
-      {"reshape_with_xshape",
+      {"reshape",
        XPUKernelSet({phi::DataType::FLOAT64,
                      phi::DataType::FLOAT16,
                      phi::DataType::INT64,
@@ -541,7 +541,7 @@ XPUOpMap& get_kl2_ops() {
                      phi::DataType::INT8,
                      phi::DataType::UINT8,
                      phi::DataType::FLOAT32})},
-      {"squeeze_with_xshape",
+      {"squeeze",
        XPUKernelSet({phi::DataType::FLOAT64,
                      phi::DataType::INT64,
                      phi::DataType::INT32,
@@ -655,7 +655,7 @@ XPUOpMap& get_kl2_ops() {
                      phi::DataType::UINT8,
                      phi::DataType::FLOAT16,
                      phi::DataType::FLOAT32})},
-      {"unsqueeze_with_xshape",
+      {"unsqueeze",
        XPUKernelSet({phi::DataType::FLOAT64,
                      phi::DataType::INT64,
                      phi::DataType::INT32,

paddle/fluid/operators/reshape_op.cc
@@ -424,27 +424,27 @@ class ReshapeKernel {
     }
     if (platform::is_cpu_place(ctx.GetPlace())) {
       auto &dev_ctx = ctx.device_context<phi::CPUContext>();
-      phi::ReshapeKernel(static_cast<const phi::CPUContext &>(dev_ctx),
-                         *in,
-                         pt_scalar_shape,
-                         out);
+      phi::ReshapeInferKernel(static_cast<const phi::CPUContext &>(dev_ctx),
+                              *in,
+                              pt_scalar_shape,
+                              out);
     }
 #if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
     if (platform::is_gpu_place(ctx.GetPlace())) {
       auto &dev_ctx = ctx.device_context<phi::GPUContext>();
-      phi::ReshapeKernel(static_cast<const phi::GPUContext &>(dev_ctx),
-                         *in,
-                         pt_scalar_shape,
-                         out);
+      phi::ReshapeInferKernel(static_cast<const phi::GPUContext &>(dev_ctx),
+                              *in,
+                              pt_scalar_shape,
+                              out);
     }
 #endif
 #ifdef PADDLE_WITH_XPU
     if (platform::is_xpu_place(ctx.GetPlace())) {
       auto &dev_ctx = ctx.device_context<platform::XPUDeviceContext>();
-      phi::ReshapeKernel(static_cast<const phi::XPUContext &>(dev_ctx),
-                         *in,
-                         pt_scalar_shape,
-                         out);
+      phi::ReshapeInferKernel(static_cast<const phi::XPUContext &>(dev_ctx),
+                              *in,
+                              pt_scalar_shape,
+                              out);
     }
 #endif
   }

paddle/phi/api/yaml/legacy_ops.yaml
@@ -561,7 +561,7 @@
     func : EinsumRawInferMeta
     param : [x, equation]
   kernel :
-    func : einsum_raw
+    func : einsum
   backward : einsum_grad
 
 - op : elementwise_pow
@@ -677,7 +677,7 @@
   infer_meta :
     func : FlattenWithXShapeInferMeta
   kernel :
-    func : flatten_with_xshape
+    func : flatten
     backend : x
   inplace : (x -> out)
   view : (x -> out)
@@ -1391,7 +1391,7 @@
   infer_meta :
     func : ReduceIntArrayAxisInferMetaBase
   kernel :
-    func : prod_raw
+    func : prod
   backward : prod_grad
 
 - op : psroi_pool
@@ -1473,7 +1473,7 @@
   infer_meta :
     func : ReshapeWithXShapeInferMeta
   kernel :
-    func : reshape_with_xshape
+    func : reshape
   inplace : (x -> out)
   view: (x -> out)
   intermediate : xshape

paddle/phi/api/yaml/ops.yaml
@@ -1150,7 +1150,7 @@
   infer_meta :
     func : SqueezeWithXShapeInferMeta
   kernel :
-    func : squeeze_with_xshape
+    func : squeeze
     data_type : x
   inplace : (x -> out)
   view: (x -> out)
@@ -1258,7 +1258,7 @@
   infer_meta :
     func : UnsqueezeWithXShapeInferMeta
   kernel :
-    func : unsqueeze_with_xshape
+    func : unsqueeze
     data_type : x
   inplace : (x -> out)
   view: (x -> out)

paddle/phi/kernels/cpu/einsum_kernel.cc
@@ -18,19 +18,19 @@
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/impl/einsum_impl.h"
 
-PD_REGISTER_KERNEL(einsum_raw,
+PD_REGISTER_KERNEL(einsum,
                    CPU,
                    ALL_LAYOUT,
-                   phi::EinsumKernelRaw,
+                   phi::EinsumKernel,
                    float,
                    double,
                    phi::dtype::complex<float>,
                    phi::dtype::complex<double>) {}
 
-PD_REGISTER_KERNEL(einsum,
+PD_REGISTER_KERNEL(einsum_infer,
                    CPU,
                    ALL_LAYOUT,
-                   phi::EinsumKernel,
+                   phi::EinsumInferKernel,
                    float,
                    double,
                    phi::dtype::complex<float>,

paddle/phi/kernels/cpu/prod_kernel.cc
@@ -22,12 +22,12 @@
 namespace phi {
 
 template <typename T, typename Context>
-void ProdRawKernel(const Context& dev_ctx,
-                   const DenseTensor& x,
-                   const IntArray& dims,
-                   bool keep_dim,
-                   bool reduce_all,
-                   DenseTensor* out) {
+void ProdKernel(const Context& dev_ctx,
+                const DenseTensor& x,
+                const IntArray& dims,
+                bool keep_dim,
+                bool reduce_all,
+                DenseTensor* out) {
   reduce_all = recompute_reduce_all(x, dims, reduce_all);
   auto out_dtype = x.dtype();
   phi::Reduce<CPUContext, T, phi::funcs::ProdFunctor>(
@@ -36,11 +36,5 @@ void ProdRawKernel(const Context& dev_ctx,
 
 }  // namespace phi
 
-PD_REGISTER_KERNEL(prod_raw,
-                   CPU,
-                   ALL_LAYOUT,
-                   phi::ProdRawKernel,
-                   float,
-                   double,
-                   int,
-                   int64_t) {}
+PD_REGISTER_KERNEL(
+    prod, CPU, ALL_LAYOUT, phi::ProdKernel, float, double, int, int64_t) {}

paddle/phi/kernels/einsum_kernel.h
@@ -19,17 +19,17 @@
 namespace phi {
 
 template <typename T, typename Context>
+void EinsumInferKernel(const Context& dev_ctx,
+                       const std::vector<const DenseTensor*>& inputs,
+                       const std::string& equation,
+                       DenseTensor* out);
+
+template <typename T, typename Context>
 void EinsumKernel(const Context& dev_ctx,
                   const std::vector<const DenseTensor*>& inputs,
                   const std::string& equation,
-                  DenseTensor* out);
-
-template <typename T, typename Context>
-void EinsumKernelRaw(const Context& dev_ctx,
-                     const std::vector<const DenseTensor*>& inputs,
-                     const std::string& equation,
-                     DenseTensor* out,
-                     std::vector<DenseTensor*> inner_cache,
-                     std::vector<DenseTensor*> xshape);
+                  DenseTensor* out,
+                  std::vector<DenseTensor*> inner_cache,
+                  std::vector<DenseTensor*> xshape);
 
 }  // namespace phi

paddle/phi/kernels/flatten_kernel.cc
@@ -23,11 +23,11 @@
 namespace phi {
 
 template <typename T, typename Context>
-void FlattenKernel(const Context& dev_ctx,
-                   const DenseTensor& x,
-                   int start_axis,
-                   int stop_axis,
-                   DenseTensor* out) {
+void FlattenInferKernel(const Context& dev_ctx,
+                        const DenseTensor& x,
+                        int start_axis,
+                        int stop_axis,
+                        DenseTensor* out) {
   dev_ctx.Alloc(out, x.dtype());
   auto out_dims = out->dims();
   phi::Copy(dev_ctx, x, dev_ctx.GetPlace(), false, out);
@@ -38,21 +38,21 @@ void FlattenKernel(const Context& dev_ctx,
 // Output Tensor，
 // is there a more flexible way to deal with this case?
 template <typename T, typename Context>
-void FlattenWithXShape(const Context& dev_ctx,
-                       const DenseTensor& x,
-                       int start_axis,
-                       int stop_axis,
-                       DenseTensor* out,
-                       DenseTensor* xshape) {
-  FlattenKernel<T, Context>(dev_ctx, x, start_axis, stop_axis, out);
+void FlattenKernel(const Context& dev_ctx,
+                   const DenseTensor& x,
+                   int start_axis,
+                   int stop_axis,
+                   DenseTensor* out,
+                   DenseTensor* xshape) {
+  FlattenInferKernel<T, Context>(dev_ctx, x, start_axis, stop_axis, out);
 }
 
 }  // namespace phi
 
-PD_REGISTER_KERNEL(flatten,
+PD_REGISTER_KERNEL(flatten_infer,
                    CPU,
                    ALL_LAYOUT,
-                   phi::FlattenKernel,
+                   phi::FlattenInferKernel,
                    float,
                    phi::dtype::bfloat16,
                    double,
@@ -62,10 +62,10 @@ PD_REGISTER_KERNEL(flatten,
                    int,
                    int64_t) {}
 
-PD_REGISTER_KERNEL(flatten_with_xshape,
+PD_REGISTER_KERNEL(flatten,
                    CPU,
                    ALL_LAYOUT,
-                   phi::FlattenWithXShape,
+                   phi::FlattenKernel,
                    float,
                    phi::dtype::bfloat16,
                    double,
@@ -76,10 +76,10 @@ PD_REGISTER_KERNEL(flatten_with_xshape,
                    int64_t) {}
 
 #if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
-PD_REGISTER_KERNEL(flatten,
+PD_REGISTER_KERNEL(flatten_infer,
                    GPU,
                    ALL_LAYOUT,
-                   phi::FlattenKernel,
+                   phi::FlattenInferKernel,
                    float,
                    phi::dtype::float16,
                    phi::dtype::bfloat16,
@@ -90,10 +90,10 @@ PD_REGISTER_KERNEL(flatten,
                    int,
                    int64_t) {}
 
-PD_REGISTER_KERNEL(flatten_with_xshape,
+PD_REGISTER_KERNEL(flatten,
                    GPU,
                    ALL_LAYOUT,
-                   phi::FlattenWithXShape,
+                   phi::FlattenKernel,
                    float,
                    phi::dtype::float16,
                    phi::dtype::bfloat16,
@@ -106,10 +106,10 @@ PD_REGISTER_KERNEL(flatten_with_xshape,
 #endif
 
 #ifdef PADDLE_WITH_XPU
-PD_REGISTER_KERNEL(flatten,
+PD_REGISTER_KERNEL(flatten_infer,
                    XPU,
                    ALL_LAYOUT,
-                   phi::FlattenKernel,
+                   phi::FlattenInferKernel,
                    float,
                    phi::dtype::float16,
                    int8_t,
@@ -117,10 +117,10 @@ PD_REGISTER_KERNEL(flatten,
                    int,
                    int64_t) {}
 
-PD_REGISTER_KERNEL(flatten_with_xshape,
+PD_REGISTER_KERNEL(flatten,
                    XPU,
                    ALL_LAYOUT,
-                   phi::FlattenWithXShape,
+                   phi::FlattenKernel,
                    float,
                    phi::dtype::float16,
                    int8_t,

commit 6a5ffb6e611d675544db4753ea9520e90ad66840

deal with leftout issue introduced by #49470

paddle/phi/backends/xpu/xpu2_op_list.cc
@@ -269,12 +269,6 @@ XPUOpMap& get_kl2_ops() {
                      phi::DataType::INT8,
                      phi::DataType::FLOAT16,
                      phi::DataType::FLOAT32})},
-      {"flatten",
-       XPUKernelSet({phi::DataType::INT64,
-                     phi::DataType::INT32,
-                     phi::DataType::INT8,
-                     phi::DataType::FLOAT16,
-                     phi::DataType::FLOAT32})},
       {"flatten_grad",
        XPUKernelSet({phi::DataType::INT64,
                      phi::DataType::INT32,