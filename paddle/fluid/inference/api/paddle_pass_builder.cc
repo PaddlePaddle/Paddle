07514139347328aff3b01f19301dac2ab601ea06
Yuanle Liu <yuanlehome@163.com>
2023-01-16 10:40:41 +0800

    add gpu_cpu_map_matmul_to_mul_pass to kGpuLowerPrecisionPasses (#49753)
    * add gpu_cpu_map_matmul_to_mul_pass to kGpuLowerPrecisionPasses
    
    * disable fc_elementwise_layernorm_fuse_pass in mixed precision

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -193,8 +193,9 @@ const std::vector<std::string> kGpuLowerPrecisionPasses{
     "fuse_multi_transformer_layer_pass",
     "gpu_cpu_map_matmul_v2_to_mul_pass",
     "gpu_cpu_map_matmul_v2_to_matmul_pass",
+    "gpu_cpu_map_matmul_to_mul_pass",
     "fc_fuse_pass",
-    "fc_elementwise_layernorm_fuse_pass",
+    // "fc_elementwise_layernorm_fuse_pass",
     "embedding_eltwise_layernorm_fuse_pass",
     "inplace_op_var_pass"};
 
