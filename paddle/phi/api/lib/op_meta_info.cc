2395fcef8dda9c4aeab6c92e3d47cd4871991ce2
jiahongyu <jiahongyu@baidu.com>
2023-01-17 02:21:32 +0000

    decouple phi custom_op, remove codes

--- paddle/phi/api/lib/op_meta_info.cc
+++ paddle/phi/api/lib/op_meta_info.cc
@@ -19,7 +19,6 @@ limitations under the License. */
 #include <vector>
 
 #include "glog/logging.h"
-#include "paddle/fluid/framework/custom_operator.h"
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/enforce.h"
 
