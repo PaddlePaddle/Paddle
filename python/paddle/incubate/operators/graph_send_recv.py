a4c38d968ff48994d884b9c8e7cf1645dea469af
mhy-666 <2367290636@qq.com>
2023-01-19 11:58:50 +0000

    fix

--- python/paddle/incubate/operators/graph_send_recv.py
+++ python/paddle/incubate/operators/graph_send_recv.py
@@ -14,6 +14,7 @@
 
 import numpy as np
 
+import paddle
 import paddle.utils.deprecated as deprecated
 from paddle import _C_ops
 from paddle.fluid.data_feeder import (
@@ -24,7 +25,6 @@ from paddle.fluid.data_feeder import (
 )
 from paddle.fluid.framework import Variable, in_dygraph_mode
 from paddle.fluid.layer_helper import LayerHelper
-from paddle.fluid.layers.tensor import cast
 
 
 @deprecated(
@@ -205,7 +205,7 @@ def get_out_size_tensor_inputs(inputs, attrs, out_size, op_type):
             '(When type of out_size in' + op_type + ' is Variable.)',
         )
         if convert_dtype(out_size.dtype) == 'int64':
-            out_size = cast(out_size, 'int32')
+            out_size = paddle.cast(out_size, 'int32')
         inputs["Out_size"] = out_size
     else:
         raise TypeError("Out_size only supports Variable or int.")
