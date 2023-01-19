83d0278fcc3b6054918c4ef6fd58c3eaa3566486
liyulingyue <852433440@qq.com>
2023-01-19 22:13:59 +0800

    bug fix

--- paddle/phi/kernels/impl/matrix_power_kernel_impl.h
+++ paddle/phi/kernels/impl/matrix_power_kernel_impl.h
@@ -189,12 +189,10 @@ void MatrixPowerKernel(const Context& ctx,
   const auto& x_dims = X->dims();
   const int x_ndim = x_dims.size();
 
-  for (int i = 0; i < x_ndim; ++i) {
-    PADDLE_ENFORCE_NE(
-        x_dims[i],
-        0,
-        errors::InvalidArgument("The size of Input(X) should not be 0."));
-  }
+  PADDLE_ENFORCE_NE(
+      x.numel(),
+      0,
+      errors::InvalidArgument("The size of Input(X) should not be 0."));
 
   PADDLE_ENFORCE_EQ(
       x_dims[x_ndim - 2],
