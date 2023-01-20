f5838705b02c6f9ffc06ecc7ec4034a2f92ac21f
jameszhang <zhangxiaoci@baidu.com>
2023-01-20 09:52:55 +0800

    [KUNLUN] update xccl lib & use native Reduce in dygraph (#49941)
    * update xccl lib & use native Reduce in dygraph
    
    * minor

--- cmake/external/xpu.cmake
+++ cmake/external/xpu.cmake
@@ -16,7 +16,7 @@ else()
 endif()
 
 set(XPU_XCCL_BASE_URL
-    "https://klx-sdk-release-public.su.bcebos.com/xccl/release/1.0.6")
+    "https://klx-sdk-release-public.su.bcebos.com/xccl/release/1.0.7")
 
 if(WITH_AARCH64)
   set(XPU_XRE_DIR_NAME "xre-kylin_aarch64")
