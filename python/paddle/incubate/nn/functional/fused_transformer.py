7dba928e336007babd50b451b1e0f9905cfa33f0
RedContritio <RedContritio@qq.com>
2023-01-21 19:21:28 +0000

    add shape check for fused_multi_head_attention

--- python/paddle/incubate/nn/functional/fused_transformer.py
+++ python/paddle/incubate/nn/functional/fused_transformer.py
@@ -615,6 +615,8 @@ def fused_multi_head_attention(
         'downgrade_in_infer' if mode == 'downscale_in_infer' else mode
     )  # semantic transfer
 
+    assert x.ndim == 3, "The dims of the x should be 3."
+
     if _non_static_mode():
         if default_main_program().random_seed != 0:
             seed = default_main_program().random_seed
