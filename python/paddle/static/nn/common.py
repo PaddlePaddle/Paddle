e5acbc734017cb5b079cac7b696134644eec6d12
RedContritio <RedContritio@qq.com>
2023-01-21 19:48:27 +0000

    add dim check for spectral_norm

--- python/paddle/static/nn/common.py
+++ python/paddle/static/nn/common.py
@@ -3416,11 +3416,15 @@ def spectral_norm(weight, dim=0, power_iters=1, eps=1e-12, name=None):
     # create intput and parameters
     input_shape = weight.shape
     assert weight.numel() > 0, "Any dimension of input cannot be equal to 0."
-    assert dim < len(input_shape), (
-        "The input `dim` should be less than the "
-        "rank of `weight`, but received dim="
-        "{}".format(dim)
+    assert dim in range(
+        -weight.ndim, weight.ndim
+    ), "The input `dim` should be in range [-rank(`weight`), rank(`weight`) - 1], but received dim={}".format(
+        dim
     )
+
+    if dim < 0:
+        dim += weight.ndim
+
     h = input_shape[dim]
     w = np.prod(input_shape) // h
 
