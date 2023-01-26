32a87d011fd77abd3bafab0fa733055c9293ef39
SigureMo <sigure.qaq@gmail.com>
2023-01-26 16:09:39 +0800

    remove extra indent in TensorDoc; test=docs_preview

--- paddle/fluid/pybind/eager.cc
+++ paddle/fluid/pybind/eager.cc
@@ -1143,9 +1143,9 @@ Tensor is the basic data structure in PaddlePaddle. There are some ways to creat
 
 - Use the exsiting ``data`` to create a Tensor, please refer to :ref:`api_paddle_to_tensor`.
 - Create a Tensor with a specified ``shape``, please refer to :ref:`api_paddle_ones`,
-    :ref:`api_paddle_zeros`, :ref:`api_paddle_full`.
+  :ref:`api_paddle_zeros`, :ref:`api_paddle_full`.
 - Create a Tensor with the same ``shape`` and ``dtype`` as other Tensor, please refer to
-    :ref:`api_paddle_ones_like`, :ref:`api_paddle_zeros_like` , :ref:`api_paddle_full_like`.
+  :ref:`api_paddle_ones_like`, :ref:`api_paddle_zeros_like` , :ref:`api_paddle_full_like`.
 )DOC");
 
 extern struct PyGetSetDef variable_properties[];
