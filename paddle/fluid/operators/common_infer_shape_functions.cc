9c016cb268743f9c8857d63cf3f0f34e5c8a8fd8
PuQing <me@puqing.work>
2023-01-26 13:31:47 +0800

    remove GetBroadcastDimsArrays and use phi function
    Signed-off-by: PuQing <me@puqing.work>

--- paddle/fluid/operators/common_infer_shape_functions.cc
+++ paddle/fluid/operators/common_infer_shape_functions.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/operators/common_infer_shape_functions.h"
-
+#include "paddle/phi/kernels/funcs/common_shape.h"
 namespace paddle {
 namespace framework {
 class InferShapeContext;
@@ -27,65 +27,7 @@ namespace paddle {
 namespace operators {
 namespace details {
 
-inline void GetBroadcastDimsArrays(const framework::DDim &x_dims,
-                                   const framework::DDim &y_dims,
-                                   int *x_dims_array,
-                                   int *y_dims_array,
-                                   int *out_dims_array,
-                                   const int max_dim,
-                                   const int axis) {
-  PADDLE_ENFORCE_GE(
-      axis,
-      0,
-      platform::errors::InvalidArgument(
-          "Axis should be great than or equal to 0, but received axis is %d.",
-          axis));
-  PADDLE_ENFORCE_LE(
-      axis,
-      max_dim,
-      platform::errors::InvalidArgument(
-          "Axis should be less than or equal to %d, but received axis is %d.",
-          max_dim,
-          axis));
-  if (x_dims.size() > y_dims.size()) {
-    std::fill(y_dims_array, y_dims_array + axis, 1);
-    if (axis + y_dims.size() < max_dim) {
-      std::fill(y_dims_array + axis + y_dims.size(), y_dims_array + max_dim, 1);
-    }
-    std::copy(x_dims.Get(), x_dims.Get() + x_dims.size(), x_dims_array);
-    std::copy(y_dims.Get(), y_dims.Get() + y_dims.size(), y_dims_array + axis);
-  } else {
-    std::fill(x_dims_array, x_dims_array + axis, 1);
-    if (axis + x_dims.size() < max_dim) {
-      std::fill(x_dims_array + axis + x_dims.size(), x_dims_array + max_dim, 1);
-    }
-    std::copy(x_dims.Get(), x_dims.Get() + x_dims.size(), x_dims_array + axis);
-    std::copy(y_dims.Get(), y_dims.Get() + y_dims.size(), y_dims_array);
-  }
-
-  for (int i = 0; i < max_dim; i++) {
-    PADDLE_ENFORCE_EQ(
-        x_dims_array[i] == y_dims_array[i] || x_dims_array[i] <= 1 ||
-            y_dims_array[i] <= 1,
-        true,
-        platform::errors::InvalidArgument(
-            "Broadcast dimension mismatch. Operands could "
-            "not be broadcast together with the shape of X = [%s] and "
-            "the shape of Y = [%s]. Received [%d] in X is not equal to "
-            "[%d] in Y at i:%d.",
-            x_dims,
-            y_dims,
-            x_dims_array[i],
-            y_dims_array[i],
-            i));
-    if ((x_dims_array[i] > 1 || y_dims_array[i] > 1) ||
-        (x_dims_array[i] == 1 && y_dims_array[i] == 1)) {
-      out_dims_array[i] = std::max(x_dims_array[i], y_dims_array[i]);
-    } else {
-      out_dims_array[i] = -1;
-    }
-  }
-}
+using phi::funcs::GetBroadcastDimsArrays;
 
 framework::DDim BroadcastTwoDims(const framework::DDim &x_dims,
                                  const framework::DDim &y_dims,
