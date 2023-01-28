e669af9f05e97839c0c3c390348649893d6be44d
Liu Yiqun <liuyiqun01@baidu.com>
2023-01-28 14:23:37 +0800

    Use GpuMemcpyAsync instead of memory::Copy.

--- paddle/phi/kernels/funcs/segmented_array.h
+++ paddle/phi/kernels/funcs/segmented_array.h
@@ -89,12 +89,11 @@ struct ArraySetterBase {
         ctx.GetPlace(),
         num_bytes,
         phi::Stream(reinterpret_cast<phi::StreamId>(ctx.stream())));
-    paddle::memory::Copy(ctx.GetPlace(),
-                         allocation->ptr(),
-                         phi::CPUPlace(),
-                         src,
-                         num_bytes,
-                         ctx.stream());
+    phi::backends::gpu::GpuMemcpyAsync(allocation->ptr(),
+                                       src,
+                                       num_bytes,
+                                       phi::gpuMemcpyHostToDevice,
+                                       ctx.stream());
     return allocation->ptr();
   }
 
