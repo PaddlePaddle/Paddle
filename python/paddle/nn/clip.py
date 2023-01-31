57b53603a7cc7a8ddb0ad6cbdd6bd98e5bd4b3ac
“201716010711” <3030364995@qq.com>
2023-01-31 02:11:32 +0000

    fix squared_l2_norm op test_case6

--- python/paddle/nn/clip.py
+++ python/paddle/nn/clip.py
@@ -208,6 +208,11 @@ def _squared_l2_norm(x):
 
     x = _cast_to_mp_type_if_enabled(x)
 
+    if core.is_compiled_with_xpu():
+        square = paddle.square(x)
+        sum_square = paddle.sum(square)
+        return sum_square
+
     if in_dygraph_mode():
         return _C_ops.squared_l2_norm(x)
 
