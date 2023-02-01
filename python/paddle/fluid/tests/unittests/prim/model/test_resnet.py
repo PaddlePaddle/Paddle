dfbd51703bf1eee0aa76b51d809b56e7d1f7a05c
0x45f <wangzhen45@baidu.com>
2023-02-01 12:09:08 +0000

    Clean code

--- python/paddle/fluid/tests/unittests/prim/model/test_resnet.py
+++ python/paddle/fluid/tests/unittests/prim/model/test_resnet.py
@@ -20,6 +20,7 @@ import numpy as np
 
 import paddle
 import paddle.fluid as fluid
+import paddle.fluid.core as core
 from paddle.vision.models import resnet50
 
 SEED = 2020
@@ -28,12 +29,9 @@ momentum_rate = 0.9
 l2_decay = 1e-4
 batch_size = 2
 epoch_num = 1
-place = (
-    fluid.CUDAPlace(0) if fluid.is_compiled_with_cuda() else fluid.CPUPlace()
-)
 
-if fluid.is_compiled_with_cuda():
-    fluid.set_flags({'FLAGS_cudnn_deterministic': True})
+if core.is_compiled_with_cuda():
+    paddle.set_flags({'FLAGS_cudnn_deterministic': True})
 
 
 def reader_decorator(reader):
@@ -58,83 +56,81 @@ def optimizer_setting(parameter_list=None):
 
 
 def train(to_static, enable_prim, enable_cinn):
-    """
-    Tests model decorated by `dygraph_to_static_output` in static graph mode. For users, the model is defined in dygraph mode and trained in static graph mode.
-    """
-    with fluid.dygraph.guard(place):
-        np.random.seed(SEED)
-        paddle.seed(SEED)
-        paddle.framework.random._manual_program_seed(SEED)
-        fluid.core._set_prim_all_enabled(
-            enable_prim and platform.system() == 'Linux'
-        )
+    if core.is_compiled_with_cuda():
+        paddle.set_device('gpu')
+    else:
+        paddle.set_device('cpu')
+    np.random.seed(SEED)
+    paddle.seed(SEED)
+    paddle.framework.random._manual_program_seed(SEED)
+    fluid.core._set_prim_all_enabled(
+        enable_prim and platform.system() == 'Linux'
+    )
 
-        train_reader = paddle.batch(
-            reader_decorator(paddle.dataset.flowers.train(use_xmap=False)),
-            batch_size=batch_size,
-            drop_last=True,
-        )
-        data_loader = fluid.io.DataLoader.from_generator(
-            capacity=5, iterable=True
-        )
-        data_loader.set_sample_list_generator(train_reader)
+    train_reader = paddle.batch(
+        reader_decorator(paddle.dataset.flowers.train(use_xmap=False)),
+        batch_size=batch_size,
+        drop_last=True,
+    )
+    data_loader = fluid.io.DataLoader.from_generator(capacity=5, iterable=True)
+    data_loader.set_sample_list_generator(train_reader)
 
-        resnet = resnet50(False)
-        if to_static:
-            build_strategy = paddle.static.BuildStrategy()
-            if enable_cinn:
-                build_strategy.build_cinn_pass = True
-            resnet = paddle.jit.to_static(resnet, build_strategy=build_strategy)
-        optimizer = optimizer_setting(parameter_list=resnet.parameters())
+    resnet = resnet50(False)
+    if to_static:
+        build_strategy = paddle.static.BuildStrategy()
+        if enable_cinn:
+            build_strategy.build_cinn_pass = True
+        resnet = paddle.jit.to_static(resnet, build_strategy=build_strategy)
+    optimizer = optimizer_setting(parameter_list=resnet.parameters())
 
-        for epoch in range(epoch_num):
-            total_acc1 = 0.0
-            total_acc5 = 0.0
-            total_sample = 0
-            losses = []
+    for epoch in range(epoch_num):
+        total_acc1 = 0.0
+        total_acc5 = 0.0
+        total_sample = 0
+        losses = []
 
-            for batch_id, data in enumerate(data_loader()):
-                start_time = time.time()
-                img, label = data
+        for batch_id, data in enumerate(data_loader()):
+            start_time = time.time()
+            img, label = data
 
-                pred = resnet(img)
-                loss = paddle.nn.functional.cross_entropy(
-                    input=pred,
-                    label=label,
-                    reduction='none',
-                    use_softmax=True,
-                )
+            pred = resnet(img)
+            loss = paddle.nn.functional.cross_entropy(
+                input=pred,
+                label=label,
+                reduction='none',
+                use_softmax=True,
+            )
 
-                avg_loss = paddle.mean(x=loss)
-                acc_top1 = paddle.static.accuracy(input=pred, label=label, k=1)
-                acc_top5 = paddle.static.accuracy(input=pred, label=label, k=5)
+            avg_loss = paddle.mean(x=loss)
+            acc_top1 = paddle.static.accuracy(input=pred, label=label, k=1)
+            acc_top5 = paddle.static.accuracy(input=pred, label=label, k=5)
 
-                avg_loss.backward()
-                optimizer.minimize(avg_loss)
-                resnet.clear_gradients()
+            avg_loss.backward()
+            optimizer.minimize(avg_loss)
+            resnet.clear_gradients()
 
-                total_acc1 += acc_top1
-                total_acc5 += acc_top5
-                total_sample += 1
-                losses.append(avg_loss.numpy())
+            total_acc1 += acc_top1
+            total_acc5 += acc_top5
+            total_sample += 1
+            losses.append(avg_loss.numpy())
 
-                end_time = time.time()
-                if batch_id % 1 == 0:
-                    print(
-                        "epoch %d | batch step %d, loss %0.8f, acc1 %0.3f, acc5 %0.3f, time %f"
-                        % (
-                            epoch,
-                            batch_id,
-                            avg_loss,
-                            total_acc1.numpy() / total_sample,
-                            total_acc5.numpy() / total_sample,
-                            end_time - start_time,
-                        )
+            end_time = time.time()
+            if batch_id % 1 == 0:
+                print(
+                    "epoch %d | batch step %d, loss %0.8f, acc1 %0.3f, acc5 %0.3f, time %f"
+                    % (
+                        epoch,
+                        batch_id,
+                        avg_loss,
+                        total_acc1.numpy() / total_sample,
+                        total_acc5.numpy() / total_sample,
+                        end_time - start_time,
                     )
-                if batch_id == 10:
-                    # avoid dataloader throw abort signaal
-                    data_loader._reset()
-                    break
+                )
+            if batch_id == 10:
+                # avoid dataloader throw abort signaal
+                data_loader._reset()
+                break
 
     return losses
 
