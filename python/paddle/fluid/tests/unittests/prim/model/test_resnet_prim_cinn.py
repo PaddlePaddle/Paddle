9f74c7230270efcb2045d36359f86b180668e908
0x45f <wangzhen45@baidu.com>
2023-02-02 06:35:17 +0000

    Rename UT

--- python/paddle/fluid/tests/unittests/prim/model/test_resnet_prim_cinn.py
+++ python/paddle/fluid/tests/unittests/prim/model/test_resnet_prim_cinn.py
@@ -143,17 +143,19 @@ class TestResnet(unittest.TestCase):
         dy2st_prim = self.train(
             to_static=True, enable_prim=True, enable_cinn=False
         )
+        np.testing.assert_allclose(dy2st, dy2st_prim, rtol=1e-6)
+
         dy2st_cinn = self.train(
             to_static=True, enable_prim=False, enable_cinn=True
         )
+        np.testing.assert_allclose(dy2st[0:2], dy2st_cinn[0:2], rtol=1e-3)
+        np.testing.assert_allclose(dy2st, dy2st_cinn, rtol=1e-1)
+
         dy2st_prim_cinn = self.train(
             to_static=True, enable_prim=True, enable_cinn=True
         )
-        np.testing.assert_allclose(dy2st, dy2st_prim, rtol=1e-6)
-        np.testing.assert_allclose(dy2st, dy2st_cinn, rtol=1e-1)
-
         np.testing.assert_allclose(dy2st[0:2], dy2st_prim_cinn[0:2], rtol=1e-2)
-        np.testing.assert_allclose(dy2st[2:], dy2st_prim_cinn[2:], rtol=1e-1)
+        np.testing.assert_allclose(dy2st, dy2st_prim_cinn, rtol=1e-1)
 
 
 if __name__ == '__main__':
