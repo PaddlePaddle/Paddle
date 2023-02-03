aa8cef4a97f59778473c5d39ff3c2ee8f1f9c5e8
Paulina Gacek <paulina.gacek@intel.com>
2023-02-03 10:42:16 +0100

    Rewrite conv testers from cpp to python (#49582)
    * conv_bias_mkldnn_fuse_pass_tester rewritten
    
    * conv_concat_relu_mkldnn_fuse_pass_tester rewritten
    
    * conv_elementwise_add_fuse_pass_tester rewritten
    
    * mkldnn changed to onednn
    
    * tests added to cmakeLists, style fix
    
    * got rid of unnecessary UT, some style changes
    
    * changes in naming convention
    
    * max_examples reduced
    
    * time out added

--- python/paddle/fluid/tests/unittests/ir/inference/test_onednn_conv_elementwise_add_fuse_pass.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_onednn_conv_elementwise_add_fuse_pass.py
@@ -21,22 +21,21 @@ from auto_scan_test import PassAutoScanTest
 from program_config import OpConfig, ProgramConfig, TensorConfig
 
 
-# the two inputs of elementwise_add are tensor
-class TestConvElementwiseAddMkldnnFusePass(PassAutoScanTest):
+class TestOneDNNConvElementwiseAddFusePass(PassAutoScanTest):
     def is_program_valid(self, program_config: ProgramConfig) -> bool:
         attrs = [
             program_config.ops[i].attrs for i in range(len(program_config.ops))
         ]
-        if attrs[1]['data_format'] == "NHWC" and attrs[3]['axis'] == 0:
+        if attrs[1]['data_format'] == 'NHWC' and attrs[3]['axis'] == 0:
             return False
-        if attrs[1]['data_format'] == "NCHW" and attrs[3]['axis'] == -1:
+        if attrs[1]['data_format'] == 'NCHW' and attrs[3]['axis'] == -1:
             return False
         return True
 
     def sample_program_config(self, draw):
-        data_format = draw(st.sampled_from(["NCHW", "NHWC"]))
+        data_format = draw(st.sampled_from(['NCHW', 'NHWC']))
         dilations = draw(st.sampled_from([[1, 1], [2, 2], [1, 2]]))
-        padding_algorithm = draw(st.sampled_from(["EXPLICIT", "SAME", "VALID"]))
+        padding_algorithm = draw(st.sampled_from(['EXPLICIT', 'SAME', 'VALID']))
         groups = draw(st.sampled_from([1, 2, 4]))
         paddings = draw(st.sampled_from([[0, 3], [1, 1], [1, 2, 3, 4]]))
         strides = draw(st.sampled_from([[1, 1], [2, 2], [1, 2]]))
@@ -44,7 +43,7 @@ class TestConvElementwiseAddMkldnnFusePass(PassAutoScanTest):
         batch_size = draw(st.integers(min_value=1, max_value=4))
 
         def generate_input():
-            if data_format == "NCHW":
+            if data_format == 'NCHW':
                 return np.random.random([batch_size, 48, 64, 64]).astype(
                     np.float32
                 )
@@ -59,44 +58,44 @@ class TestConvElementwiseAddMkldnnFusePass(PassAutoScanTest):
             )
 
         relu_op = OpConfig(
-            type="relu",
-            inputs={"X": ["input_data"]},
-            outputs={"Out": ["relu_out"]},
+            type='relu',
+            inputs={'X': ['input_data']},
+            outputs={'Out': ['relu_out']},
             attrs={},
         )
 
         conv2d_op1 = OpConfig(
-            type="conv2d",
-            inputs={"Input": ["relu_out"], "Filter": ["conv_weight1"]},
-            outputs={"Output": ["conv_output1"]},
+            type='conv2d',
+            inputs={'Input': ['relu_out'], 'Filter': ['conv_weight1']},
+            outputs={'Output': ['conv_output1']},
             attrs={
-                "data_format": data_format,
-                "dilations": dilations,
-                "padding_algorithm": padding_algorithm,
-                "groups": groups,
-                "paddings": paddings,
-                "strides": strides,
+                'data_format': data_format,
+                'dilations': dilations,
+                'padding_algorithm': padding_algorithm,
+                'groups': groups,
+                'paddings': paddings,
+                'strides': strides,
             },
         )
 
         conv2d_op2 = OpConfig(
-            type="conv2d",
-            inputs={"Input": ["input_data"], "Filter": ["conv_weight2"]},
-            outputs={"Output": ["conv_output2"]},
+            type='conv2d',
+            inputs={'Input': ['input_data'], 'Filter': ['conv_weight2']},
+            outputs={'Output': ['conv_output2']},
             attrs={
-                "data_format": data_format,
-                "dilations": dilations,
-                "padding_algorithm": padding_algorithm,
-                "groups": groups,
-                "paddings": paddings,
-                "strides": strides,
+                'data_format': data_format,
+                'dilations': dilations,
+                'padding_algorithm': padding_algorithm,
+                'groups': groups,
+                'paddings': paddings,
+                'strides': strides,
             },
         )
 
         elt_op = OpConfig(
-            type="elementwise_add",
-            inputs={"X": ["conv_output1"], "Y": ["conv_output2"]},
-            outputs={"Out": ["elementwise_output"]},
+            type='elementwise_add',
+            inputs={'X': ['conv_output1'], 'Y': ['conv_output2']},
+            outputs={'Out': ['elementwise_output']},
             attrs={'axis': axis},
         )
 
@@ -105,26 +104,26 @@ class TestConvElementwiseAddMkldnnFusePass(PassAutoScanTest):
         program_config = ProgramConfig(
             ops=model_net,
             weights={
-                "conv_weight1": TensorConfig(data_gen=partial(generate_weight)),
-                "conv_weight2": TensorConfig(data_gen=partial(generate_weight)),
+                'conv_weight1': TensorConfig(data_gen=partial(generate_weight)),
+                'conv_weight2': TensorConfig(data_gen=partial(generate_weight)),
             },
             inputs={
-                "input_data": TensorConfig(data_gen=partial(generate_input))
+                'input_data': TensorConfig(data_gen=partial(generate_input))
             },
-            outputs=["elementwise_output"],
+            outputs=['elementwise_output'],
         )
 
         return program_config
 
     def sample_predictor_configs(self, program_config):
         config = self.create_inference_config(use_mkldnn=True)
-        yield config, ["relu", "conv2d", "fused_conv2d"], (1e-5, 1e-5)
+        yield config, ['relu', 'conv2d', 'fused_conv2d'], (1e-5, 1e-5)
 
     def test(self):
         self.run_and_statis(
-            quant=False, passes=["conv_elementwise_add_mkldnn_fuse_pass"]
+            quant=False, passes=['conv_elementwise_add_mkldnn_fuse_pass']
         )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
