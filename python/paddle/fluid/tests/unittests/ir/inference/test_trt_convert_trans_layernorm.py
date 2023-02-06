ebc255b52ded4c45f46b8d662a3d77d75d7dda16
wwbitejotunn <wang_bojun@outlook.com>
2023-02-06 18:19:45 +0000

    ut fix

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_trans_layernorm.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_trans_layernorm.py
@@ -47,7 +47,12 @@ class TrtConvertTransLayernormTest(TrtLayerAutoScanTest):
             return x.astype(np.float32)
 
         def conv2d_input_datagen(dics):
-            x = (np.random.random([dics["c"], dics["c"], 1, 1]) - 0.5) * 0.1
+            x = (
+                np.random.random(
+                    [dics["batch"], dics["c"], dics["h"], dics["w"]]
+                )
+                - 0.5
+            ) * 0.1
 
             return x.astype(np.float32)
 
