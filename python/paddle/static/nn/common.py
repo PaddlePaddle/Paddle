15e561afeb7478f29a4cd1e51eda71b72db52583
GGBond8488 <857631483@qq.com>
2023-02-06 02:00:34 +0000

    fxi sample code error

--- python/paddle/static/nn/common.py
+++ python/paddle/static/nn/common.py
@@ -3702,7 +3702,7 @@ def embedding(
             paddle.enable_static()
 
             x = paddle.static.data(name="x", shape = [2, 4], dtype=np.int64)
-            embedding = paddle.static.nn.Embedding(10, 3,
+            embedding = paddle.static.nn.embedding(10, 3,
                         weight_attr=paddle.nn.initializer.Constant(value=1.0))
             adam = paddle.optimizer.SGD(parameters=[embedding.weight], learning_rate=0.01)
             output = embedding(x)
@@ -3749,7 +3749,7 @@ def embedding(
             x = paddle.to_tensor(x_data, stop_gradient=False)
 
             # embedding weight shape = [10, 3]
-            embedding = paddle.static.nn.Embedding(10, 3, sparse=True)
+            embedding = paddle.static.nn.embedding(10, 3, sparse=True)
 
             # embedding weight data = [10, 3]
             w0 = np.full(shape=(10, 3), fill_value=2).astype(np.float32)
