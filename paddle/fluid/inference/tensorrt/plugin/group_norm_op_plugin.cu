3db7f08291d064d9240b2c46ba3d6ba135ec7574
zhoutianzi666 <17801055074@163.com>
2023-02-06 03:29:40 +0000

    add GN int8 fake kernel

--- paddle/fluid/inference/tensorrt/plugin/group_norm_op_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/group_norm_op_plugin.cu
@@ -902,7 +902,7 @@ int GroupNormPluginDynamic::enqueue(
       groupNormNHWCScale(params_, stream);
     } else {
       PADDLE_THROW(platform::errors::Fatal(
-          "The Groupnorm TRT Plugin's only support nchw or nhwc8 input"));
+          "The Groupnorm TRT int8 Plugin only support nchw32 input"));
     }
   } else if (input_type == nvinfer1::DataType::kINT8) {
     const int8_t *input = reinterpret_cast<const int8_t *>(inputs[0]);
