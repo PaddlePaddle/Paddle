7cde7a26291ed850c9f8380b4dea6bff1eb74236
zhoutianzi666 <17801055074@163.com>
2023-02-06 12:09:33 +0000

    put constant_folding forward

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -101,11 +101,12 @@ const std::vector<std::string> kTRTSubgraphPasses({
       "delete_c_identity_op_pass",                    //
       "trt_multihead_matmul_fuse_pass_v2",            //
       "trt_multihead_matmul_fuse_pass_v3",            //
-      "vit_attention_fuse_pass",                      //
-      "trt_skip_layernorm_fuse_pass",                 //
-      "preln_skip_layernorm_fuse_pass",               //
-      "preln_residual_bias_fuse_pass",                //
-      "layernorm_shift_partition_fuse_pass",          //
+      "constant_folding_pass",
+      "vit_attention_fuse_pass",              //
+      "trt_skip_layernorm_fuse_pass",         //
+      "preln_skip_layernorm_fuse_pass",       //
+      "preln_residual_bias_fuse_pass",        //
+      "layernorm_shift_partition_fuse_pass",  //
       // "set_transformer_input_convert_pass",           //
       "conv_bn_fuse_pass",                           //
       "unsqueeze2_eltwise_fuse_pass",                //
