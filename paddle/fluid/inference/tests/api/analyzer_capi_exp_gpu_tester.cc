70f18a1758b944f628e627c39668bccdf9ec39ca
dengkaipeng <dengkaipeng@baidu.com>
2023-02-07 08:02:40 +0000

    update API. test=develop

--- paddle/fluid/inference/tests/api/analyzer_capi_exp_gpu_tester.cc
+++ paddle/fluid/inference/tests/api/analyzer_capi_exp_gpu_tester.cc
@@ -178,7 +178,7 @@ TEST(PD_Config, trt_static_path) {
   std::string model_dir = FLAGS_infer_model + "/mobilenet";
   std::string static_path = FLAGS_infer_model + "/trt_engine";
   PD_Config* config = PD_ConfigCreate();
-  PD_ConfigEnableUseGpu(config, 100, 0);
+  PD_ConfigEnableUseGpu(config, 100, 0, 0);
   PD_ConfigEnableTensorRtEngine(config,
                                 1 << 20,
                                 1,
