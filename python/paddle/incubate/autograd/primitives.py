8b7afc1d77f54bfe3cdd1cd06a177e8f5a0bc377
mhy-666 <2367290636@qq.com>
2023-02-07 11:25:25 +0000

    fix

--- python/paddle/incubate/autograd/primitives.py
+++ python/paddle/incubate/autograd/primitives.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+from paddle.fluid.layers.tensor import fill_constant  # noqa: F401
 from paddle.tensor import abs  # noqa: F401
 from paddle.tensor import acos  # noqa: F401
 from paddle.tensor import acosh  # noqa: F401
@@ -107,6 +107,7 @@ sub_prim = [
 
 others = [
     'broadcast_to',
+    'fill_constant',
     'reshape',
 ]
 
