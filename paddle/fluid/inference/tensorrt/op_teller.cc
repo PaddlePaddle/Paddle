0282b008f5ca5c2b3fef7fdbe9bf1fccca95cc79
zhoutianzi666 <17801055074@163.com>
2023-02-08 03:21:50 +0000

    disable bilinear_interp_v2

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -793,7 +793,7 @@ struct SimpleOpTypeSetTeller : public Teller {
 
     if (op_type == "bilinear_interp_v2") {
 // trt 7011 result in test_solov2_trt_fp32.py TRT fp32 diff
-#if !IS_TRT_VERSION_GE(1000)
+#if !IS_TRT_VERSION_GE(7100)
       return false;
 #endif
       std::vector<std::string> attrs{"data_layout",
