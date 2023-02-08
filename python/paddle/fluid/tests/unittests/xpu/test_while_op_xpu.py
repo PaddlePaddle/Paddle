fc9982af87f33e0b5003b7890125bc3c1a1259b3
mhy-666 <2367290636@qq.com>
2023-02-08 07:04:56 +0000

    fix paddle.sum

--- python/paddle/fluid/tests/unittests/xpu/test_while_op_xpu.py
+++ python/paddle/fluid/tests/unittests/xpu/test_while_op_xpu.py
@@ -54,7 +54,7 @@ class TestWhileOp(unittest.TestCase):
         with while_op.block():
             d = paddle.tensor.array_read(array=data_array, i=i)
             prev = paddle.tensor.array_read(array=mem_array, i=i)
-            result = paddle.sums(input=[d, prev])
+            result = paddle.sum([d, prev])
 
             i = paddle.increment(x=i)
             paddle.tensor.array_write(result, i=i, array=mem_array)
@@ -63,7 +63,7 @@ class TestWhileOp(unittest.TestCase):
             with while_op2.block():
                 d2 = paddle.tensor.array_read(array=data_array, i=j)
                 prev2 = paddle.tensor.array_read(array=mem_array, i=j)
-                result2 = paddle.sums(input=[d2, prev2])
+                result2 = paddle.sum([d2, prev2])
 
                 j = paddle.increment(x=j)
                 paddle.tensor.array_write(result2, i=j, array=mem_array)
