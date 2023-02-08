2bdc221c728981b285e843af386fd75458d7550e
zhangbo9674 <zhangbo54@baidu.com>
2023-02-08 07:59:18 +0000

    refine code by comment

--- paddle/ir/type/type_support.h
+++ paddle/ir/type/type_support.h
@@ -18,74 +18,88 @@
 
 namespace ir {
 
-/*
- * TypeId is the unique identification of Type, each Type corresponds to a
- * unique TypeId, the same id indicates the same Type class. TypeId provides an
- * instantiation interface: TypeId::get.
- * For example:
- *   class TypeA {};
- *   TypeId type_a_id = TypeId::get<TypeA>();
- */
+/// \brief TypeId is the unique identification of Type, each Type corresponds to
+/// a unique TypeId, the same id indicates the same Type class. TypeId provides
+/// an instantiation interface: TypeId::get. For example: \code{cpp}
+///   class TypeA {};
+///   TypeId type_a_id = TypeId::get<TypeA>();
+/// \endcode
 class TypeId {
   struct Storage {};
 
  public:
+  /// \brief Returns the unique TypeId of Type T.
+  /// \return The unique TypeId of Type T.
   template <typename T>
   static TypeId get() {
     static Storage instance;
     return TypeId(&instance);
   }
+
+  /// \brief Comparison operations.
   inline bool operator==(const TypeId &other) const {
     return storage_ == other.storage_;
   }
+
+  /// \brief Comparison operations.
   inline bool operator!=(const TypeId &other) const {
     return !(*this == other);
   }
+
+  /// \brief Enable hashing TypeId instances.
   friend struct std::hash<TypeId>;
 
  private:
+  /// \brief Construct a TypeId and initialize storage.
+  /// \param storage The storage of this TypeId.
   explicit TypeId(const Storage *storage) : storage_(storage) {}
+
   const Storage *storage_;
 };
 
-/*
- * Abstract the properties and behaviors common to all Type classes into an
- * AbstractType class. There are two types in Type system:
- * non-parameter/singleton type and parameter-type. The common attributes of all
- * types is TypeId (and possibly others). Therefore, construct a class with
- * TypeId as its member
- */
+/// \brief Abstract the properties and behaviors common to all Type classes into
+/// an AbstractType class. There are two types in Type system:
+/// on-parameter/singleton type and parameter-type. The common attributes of all
+/// types is TypeId (and possibly others). Therefore, construct a class with
+/// TypeId as its member.
 class AbstractType {
  public:
-  // Construct an AbstractType by TypeId directly.
+  /// \brief Construct an AbstractType by TypeId directly.
+  /// \param type_id The type id of the AbstractType.
   static AbstractType get(TypeId type_id) { return AbstractType(type_id); }
 
+  /// \brief Returns the type id of the AbstractType.
+  /// \return The type id of the AbstractType.
   TypeId type_id() const { return type_id_; }
 
-  // TODO(zhangbo9674): After the IRContext is designed, AbstractType will be
-  // cached to IRContext with TypeId as key. static const AbstractType&
-  // LookUp(IRContext* ctx, TypeId type_id);
+  /* TODO(zhangbo9674): After the IRContext is designed, AbstractType will be
+   * cached to IRContext with TypeId as key.
+   */
 
  private:
-  // The constructor is set to private and provides the user with the Get method
-  // and LookUp method to obtain and manage the AstractType.
+  /// \brief The constructor is set to private and provides the user with the
+  /// Get method and LookUp method to obtain and manage the AstractType. \param
+  /// type_id The type id of the AbstractType.
   explicit AbstractType(TypeId type_id) : type_id_(type_id) {}
+
   TypeId type_id_;
 };
 
-/*
- * TypeStorage is used to store all information of a Type. A Type object
- * contains a TypeStorage. For non-parameter type, the information includes:
- * TypeId, so TypeStorage only needs to include AbstractType; For parameter
- * type, in addition to AbstractType/TypeId, parameter information needs to be
- * included. So that, non-parameter type can be constructed by TypeStorage
- * directly but parameter type should be constructed by Derived TypeStorage
- */
+/// \brief TypeStorage is used to store all information of a Type. A Type object
+/// contains a TypeStorage. For non-parameter type, the information includes:
+/// TypeId, so TypeStorage only needs to include AbstractType; For parameter
+/// type, in addition to AbstractType/TypeId, parameter information needs to be
+/// included. So that, non-parameter type can be constructed by TypeStorage
+/// directly but parameter type should be constructed by Derived TypeStorage.
 class TypeStorage {
  public:
+  /// \brief Construct a TypeStorage and initialize abstract_type.
+  /// \param abstract_type The abstract_type of this TypeStorage.
   explicit TypeStorage(AbstractType *abstract_type)
       : abstract_type_(abstract_type) {}
 
+  /// \brief Returns the AbstractType of the TypeStorage.
+  /// \return The AbstractType of the TypeStorage.
   const AbstractType &abstract_type() { return *abstract_type_; }
 
  private:
@@ -96,10 +110,11 @@ class TypeStorage {
 
 // Custom specialization of std::hash can be injected in namespace std.
 namespace std {
+/// \brief Enable hashing TypeId instances.
 template <>
 struct hash<ir::TypeId> {
   std::size_t operator()(const ir::TypeId &obj) const {
-    return std::hash<const void *>()(static_cast<const void *>(obj.storage_));
+    return std::hash<const ir::TypeId::Storage *>()(obj.storage_);
   }
 };
 }  // namespace std
