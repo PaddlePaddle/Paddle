fadb11fde1a96abb0d22c0367e2a2b298ea798e7
zhoutianzi666 <17801055074@163.com>
2023-02-08 07:02:26 +0000

    add verison check in  py UT

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -793,7 +793,7 @@ struct SimpleOpTypeSetTeller : public Teller {
 
     if (op_type == "bilinear_interp_v2") {
 // trt 7011 result in test_solov2_trt_fp32.py TRT fp32 diff
-#if !IS_TRT_VERSION_GE(7100)
+#if IS_TRT_VERSION_LT(7100)
       return false;
 #endif
       std::vector<std::string> attrs{"data_layout",
