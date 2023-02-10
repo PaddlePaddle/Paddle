08f4b66457bff223c9d9b07a5d0f85350ae1c0a1
chenruibiao <chenruibiao@baidu.com>
2023-02-10 18:46:50 +0800

    Fix test_layer_new errors

--- paddle/fluid/jit/engine/interpreter_engine.cc
+++ paddle/fluid/jit/engine/interpreter_engine.cc
@@ -60,6 +60,7 @@ void InterpreterEngine::CreateInterpreterCore() {
   GraphToProgram(graph, &converted_prog_, nullptr);
 
   framework::interpreter::ExecutionConfig execution_config;
+  execution_config.create_local_scope = false;
   execution_config.used_for_jit = true;
 
   auto in_names = info_->InputArgNames();
