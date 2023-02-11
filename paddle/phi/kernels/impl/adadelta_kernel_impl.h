eb11ec23dde90cce6824d1a9b9f80edc04d7782e
wangzhen38 <wangzhen38@baidu.com>
2023-02-11 08:43:24 +0000

    fix acc update order

--- paddle/phi/kernels/impl/adadelta_kernel_impl.h
+++ paddle/phi/kernels/impl/adadelta_kernel_impl.h
@@ -13,10 +13,12 @@
 // limitations under the License.
 
 #pragma once
-
+#include "paddle/fluid/framework/eigen.h"
+#include "paddle/fluid/framework/op_registry.h"
+#include "paddle/fluid/operators/eigen/eigen_function.h"
 #include "paddle/phi/kernels/adadelta_kernel.h"
 #include "paddle/phi/kernels/funcs/eigen/common.h"
-#include "paddle/phi/kernels/funcs/eigen/eigen_function.h"
+#include "paddle/phi/kernels/funcs/math_function.h"
 
 namespace phi {
 
@@ -42,7 +44,7 @@ void AdadeltaKernel(const Context& dev_ctx,
   auto eigen_param = EigenVector<T>::Flatten(param);
   auto eigen_grad = EigenVector<T>::Flatten(grad);
   // Squared gradient accumulator
-  // auto eigen_avg_squared_grad = EigenVector<T>::Flatten(avg_squared_grad);
+  auto eigen_avg_squared_grad = EigenVector<T>::Flatten(avg_squared_grad);
   // Squared updates accumulator
   auto eigen_avg_squared_update = EigenVector<T>::Flatten(avg_squared_update);
   auto eigen_param_out = EigenVector<T>::Flatten(*param_out);
@@ -51,14 +53,11 @@ void AdadeltaKernel(const Context& dev_ctx,
   auto eigen_avg_squared_update_out =
       EigenVector<T>::Flatten(*avg_squared_update_out);
   auto& place = *dev_ctx.eigen_device();
-
-  eigen_avg_squared_grad_out.device(place) = (1 - rho_) * eigen_grad.square();
-  //    rho_ * eigen_avg_squared_grad + (1 - rho_) * eigen_grad.square();
+  eigen_avg_squared_grad_out.device(place) =
+      rho_ * eigen_avg_squared_grad + (1 - rho_) * eigen_grad.square();
   auto update =
-      -(eigen_avg_squared_update + epsilon_ - eigen_avg_squared_update).sqrt() *
-      eigen_grad / (eigen_avg_squared_grad_out + epsilon_).sqrt();
-  eigen_avg_squared_update_out.device(place) =
-      rho_ * eigen_avg_squared_update + (1 - rho_) * update.square();
+      -(((eigen_avg_squared_update + epsilon_).sqrt()) /
+        ((eigen_avg_squared_grad_out + epsilon_).sqrt()) * eigen_grad);
   Eigen::DSizes<int, 1> m_dsize(avg_squared_update_out->numel());
   if (paddle::platform::is_cpu_place(dev_ctx.GetPlace())) {
     auto* lr = learning_rate.data<T>();
@@ -68,6 +67,8 @@ void AdadeltaKernel(const Context& dev_ctx,
     eigen_param_out.device(place) =
         eigen_param + lr.broadcast(m_dsize) * update;
   }
+  eigen_avg_squared_update_out.device(place) =
+      rho_ * eigen_avg_squared_update + (1 - rho_) * update.square();
 }
 
 }  // namespace phi
