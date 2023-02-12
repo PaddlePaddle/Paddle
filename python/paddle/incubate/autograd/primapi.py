b9784fc31f329e9b394c6fd09654fd3b55bb21a2
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-02-12 11:14:20 +0000

    polish log

--- python/paddle/incubate/autograd/primapi.py
+++ python/paddle/incubate/autograd/primapi.py
@@ -24,45 +24,34 @@ from paddle.incubate.autograd import primx, utils
 @framework.static_only
 def forward_grad(outputs, inputs, grad_inputs=None):
     """Forward mode of automatic differentiation.
-
     Note:
         **ONLY available in the static graph mode and primitive operators.**
-
     Args:
         outputs(Tensor|Sequence[Tensor]): The output tensor or tensors.
         inputs(Tensor|Sequence[Tensor]): The input tensor or tensors.
         grad_inputs(Tensor|Sequence[Tensor]): Optional, the gradient Tensor or
             Tensors of inputs which has the same shape with inputs, Defaults to
             None, in this case is equivalent to all ones.
-
     Returns:
         grad_outputs(Tensor|Sequence[Tensor]): The gradients for outputs.
-
     Examples:
-
         .. code-block:: python
-
             import numpy as np
             import paddle
-
             paddle.enable_static()
             paddle.incubate.autograd.enable_prim()
-
             startup_program = paddle.static.Program()
             main_program = paddle.static.Program()
-
             with paddle.static.program_guard(main_program, startup_program):
                 x = paddle.static.data('x', shape=[1], dtype='float32')
                 y = x * x
                 y_grad = paddle.incubate.autograd.forward_grad(y, x)
                 paddle.incubate.autograd.prim2orig()
-
             exe = paddle.static.Executor()
             exe.run(startup_program)
             y_grad = exe.run(main_program, feed={'x': np.array([2.]).astype('float32')}, fetch_list=[y_grad])
             print(y_grad)
             # [array([4.], dtype=float32)]
-
             paddle.incubate.autograd.disable_prim()
             paddle.disable_static()
     """
@@ -107,30 +96,22 @@ def forward_grad(outputs, inputs, grad_inputs=None):
 @framework.static_only
 def grad(outputs, inputs, grad_outputs=None):
     """Reverse mode of automatic differentiation.
-
     Note:
         **ONLY available in the static graph mode and primitive operators**
-
     Args:
         outputs(Tensor|Sequence[Tensor]): The output Tensor or Tensors.
         inputs(Tensor|Sequence[Tensor]): The input Tensor or Tensors.
         grad_outputs(Tensor|Sequence[Tensor]): Optional, the gradient Tensor or
             Tensors of outputs which has the same shape with outputs, Defaults
             to None, in this case is equivalent to all ones.
-
     Returns:
         grad_inputs(Tensor|Tensors): The gradients for inputs.
-
     Examples:
-
         .. code-block:: python
-
             import numpy as np
             import paddle
-
             paddle.enable_static()
             paddle.incubate.autograd.enable_prim()
-
             startup_program = paddle.static.Program()
             main_program = paddle.static.Program()
             with paddle.static.program_guard(main_program, startup_program):
@@ -139,13 +120,11 @@ def grad(outputs, inputs, grad_outputs=None):
                 y = x * x
                 x_grad = paddle.incubate.autograd.grad(y, x)
                 paddle.incubate.autograd.prim2orig()
-
             exe = paddle.static.Executor()
             exe.run(startup_program)
             x_grad = exe.run(main_program, feed={'x': np.array([2.]).astype('float32')}, fetch_list=[x_grad])
             print(x_grad)
             # [array([4.], dtype=float32)]
-
             paddle.incubate.autograd.disable_prim()
             paddle.disable_static()
     """
