6e75a69d8e8840c5293f26c1acff93eef4c3fda1
sljlp <lrslj@live.com>
2023-02-13 15:31:16 +0800

    update rpc apis

--- python/paddle/static/nn/rpc_utils.py
+++ python/paddle/static/nn/rpc_utils.py
@@ -13,21 +13,40 @@
 # limitations under the License.
 
 from paddle import fluid
+import paddle
 
 
-def rpc_call(src_ids=None, url_id=None, url_list=[], voc_path=""):
+class IDGen:
+    def __init__(self) -> None:
+        self.ids = {}
+
+    def gen_name_with_idx(self, name):
+        if name not in self.ids:
+            self.ids[name] = -1
+        self.ids[name] += 1
+        return name + "_" + str(self.ids[name])
+
+    def __call__(self, name) -> str:
+        return self.gen_name_with_idx(name)
+
+
+id_gen = IDGen()
+
+
+def rpc_call(src_ids=None, url_id=None, url_list=[], voc_path="", cvt2str=True):
     request_id = (
         fluid.default_main_program()
         .block(0)
         .create_var(
-            name="request_id",
+            name=id_gen("rpc_request_id"),
             dtype="int32",
             shape=[1],
             persistable=False,
             stop_gradient=True,
         )
     )
-
+    if url_id is None:
+        url_id = paddle.assign(0).astype("int32")
     fluid.default_main_program().block(0).append_op(
         type="rpc_call",
         inputs={
@@ -35,17 +54,21 @@ def rpc_call(src_ids=None, url_id=None, url_list=[], voc_path=""):
             'url_id': [url_id],
         },
         outputs={"Out": [request_id]},
-        attrs={"url_list": url_list, "vocab_path": voc_path},
+        attrs={
+            "url_list": url_list,
+            "vocab_path": voc_path,
+            "use_ids": not cvt2str,
+        },
     )
     return request_id
 
 
-def rpc_result(request_id):
+def rpc_result(request_id, result_dtype):
     res = (
         fluid.default_main_program()
         .block(0)
         .create_var(
-            name="rpc_res",
+            name=id_gen("rpc_res"),
             dtype="float32",
             shape=[1000],
             persistable=False,
@@ -56,7 +79,7 @@ def rpc_result(request_id):
         fluid.default_main_program()
         .block(0)
         .create_var(
-            name="rpc_success",
+            name=id_gen("rpc_success"),
             dtype="bool",
             shape=[1],
             persistable=False,
@@ -67,5 +90,6 @@ def rpc_result(request_id):
         type="rpc_result",
         inputs={"X": [request_id]},
         outputs={"Out": [res], "succeed": [success]},
+        attrs={"res_type": result_dtype},
     )
     return res, success
