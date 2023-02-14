f76630da9d93738aac9cee68ef29526007816019
huangjiyi <947613776@qq.com>
2023-02-14 07:02:16 +0000

    fix bugs

--- paddle/phi/common/transform.h
+++ paddle/phi/common/transform.h
@@ -21,7 +21,7 @@ limitations under the License. */
 #include "paddle/phi/core/enforce.h"
 #include "paddle/phi/core/hostdevice.h"
 
-#if defined(__NVCC__) || defined(__HIPCC__) xxxxxxw
+#if defined(__NVCC__) || defined(__HIPCC__)
 #include <thrust/execution_policy.h>
 #include <thrust/transform.h>
 
