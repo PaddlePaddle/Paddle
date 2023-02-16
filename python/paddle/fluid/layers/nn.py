f7f67b7295235836acbb88221fdf20f57e427755
zqw_1997 <118182234+zhengqiwen1997@users.noreply.github.com>
2023-02-16 09:42:23 +0800

    Add mean composite rule (#50298)
    * beta
    
    * small commit
    
    * add batch_norm composite rule
    
    move composite test case
    
    remove unuseful var
    
    add composite op blacklist
    
    * small change v2
    
    * finish the test_composite_mean and test_composite_mean_grad
    
    * add ops assertion to the tests
    
    * add cinn test
    
    * fix the error and inappropriate usage in func: mean_composite
    
    * remove the ref of outer lib in primtives.py
    
    * modify sample code of reduce_sum
    
    * fix composite mean op map
    
    * modify testcases to test more float type
    
    * remove cpu float16 test
    
    * cinn test fix
    
    * remove reduce_max
    
    * change the name sum to sum_x
    
    * change the use of reduce_sum to sum
    
    ---------
    
    Co-authored-by: cyber-pioneer <chenzhuo@tju.edu.cn>

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -574,18 +574,18 @@ def reduce_sum(input, dim=None, keep_dim=False, name=None):
             #     [0.1, 0.2, 0.6, 0.7]]
             # Each example is followed by the corresponding output tensor.
             x = fluid.data(name='x', shape=[2, 4], dtype='float32')
-            fluid.layers.reduce_sum(x)  # [3.5]
-            fluid.layers.reduce_sum(x, dim=0)  # [0.3, 0.5, 1.1, 1.6]
-            fluid.layers.reduce_sum(x, dim=-1)  # [1.9, 1.6]
-            fluid.layers.reduce_sum(x, dim=1, keep_dim=True)  # [[1.9], [1.6]]
+            fluid.layers.nn.reduce_sum(x)  # [3.5]
+            fluid.layers.nn.reduce_sum(x, dim=0)  # [0.3, 0.5, 1.1, 1.6]
+            fluid.layers.nn.reduce_sum(x, dim=-1)  # [1.9, 1.6]
+            fluid.layers.nn.reduce_sum(x, dim=1, keep_dim=True)  # [[1.9], [1.6]]
 
             # y is a Tensor variable with shape [2, 2, 2] and elements as below:
             #      [[[1, 2], [3, 4]],
             #      [[5, 6], [7, 8]]]
             # Each example is followed by the corresponding output tensor.
             y = fluid.data(name='y', shape=[2, 2, 2], dtype='float32')
-            fluid.layers.reduce_sum(y, dim=[1, 2]) # [10, 26]
-            fluid.layers.reduce_sum(y, dim=[0, 1]) # [16, 20]
+            fluid.layers.nn.reduce_sum(y, dim=[1, 2]) # [10, 26]
+            fluid.layers.nn.reduce_sum(y, dim=[0, 1]) # [16, 20]
 
     """
     reduce_all, dim = _get_reduce_dim(dim, input)
