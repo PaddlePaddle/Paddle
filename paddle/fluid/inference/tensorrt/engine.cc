4020a927bb121f10641babd0ce7ae53e86a680b4
zhoutianzi666 <17801055074@163.com>
2023-02-16 03:26:13 +0000

    allow fall back to fp16 when int8

--- paddle/fluid/inference/tensorrt/engine.cc
+++ paddle/fluid/inference/tensorrt/engine.cc
@@ -161,6 +161,7 @@ void TensorRTEngine::FreezeNetwork() {
   if (enable_fp16) {
     bool support_fp16 = infer_builder_->platformHasFastFp16();
     infer_builder_config_->setFlag(nvinfer1::BuilderFlag::kFP16);
+    with_fp16_ = true;
     if (!support_fp16) {
       LOG(INFO) << "You specify FP16 mode, but the hardware do not support "
                    "FP16 speed up, use FP32 instead.";
@@ -173,8 +174,10 @@ void TensorRTEngine::FreezeNetwork() {
   if (enable_int8) {
     if (!use_dla_) {
       infer_builder_config_->setFlag(nvinfer1::BuilderFlag::kFP16);
+      with_fp16_ = true;
     }
     infer_builder_config_->setFlag(nvinfer1::BuilderFlag::kINT8);
+    with_int8_ = true;
 
     if (calibrator_) {
       infer_builder_config_->setInt8Calibrator(calibrator_);
