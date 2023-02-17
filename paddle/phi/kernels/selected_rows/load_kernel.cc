63f309fa80e540c1a1345b8b649368a61120ade2
huangjiyi <43315610+huangjiyi@users.noreply.github.com>
2023-02-17 15:30:03 +0800

    move_load_kernel

--- paddle/phi/kernels/selected_rows/load_kernel.cc
+++ paddle/phi/kernels/selected_rows/load_kernel.cc
@@ -20,35 +20,5 @@
 #include "paddle/phi/core/serialization.h"
 
 namespace phi {
-namespace sr {
-
-template <typename T, typename Context>
-void LoadKernel(const Context& dev_ctx,
-                const std::string& file_path,
-                int64_t seek,
-                const std::vector<int64_t>& shape,
-                bool load_as_fp16,
-                SelectedRows* out) {
-  // FIXME(yuyang18): We save variable to local file now, but we should change
-  // it to save an output stream.
-  std::ifstream fin(file_path, std::ios::binary);
-  PADDLE_ENFORCE_EQ(static_cast<bool>(fin),
-                    true,
-                    phi::errors::Unavailable(
-                        "Load operator fail to open file %s, please check "
-                        "whether the model file is complete or damaged.",
-                        file_path));
-  PADDLE_ENFORCE_NOT_NULL(out,
-                          phi::errors::InvalidArgument(
-                              "The variable to be loaded cannot be found."));
-
-  DeserializeFromStream(fin, out, dev_ctx);
-}
-
-}  // namespace sr
+namespace sr {}  // namespace sr
 }  // namespace phi
-
-PD_REGISTER_KERNEL(load_sr, CPU, ALL_LAYOUT, phi::sr::LoadKernel, float) {}
-#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
-PD_REGISTER_KERNEL(load_sr, GPU, ALL_LAYOUT, phi::sr::LoadKernel, float) {}
-#endif
