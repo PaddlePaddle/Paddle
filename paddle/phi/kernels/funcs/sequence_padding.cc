56a7598cc2e0f4e1616fc31b1e1c763c88d296d6
huangjiyi <947613776@qq.com>
2023-02-19 09:57:13 +0800

    fix buga

--- paddle/phi/kernels/funcs/sequence_padding.cc
+++ paddle/phi/kernels/funcs/sequence_padding.cc
@@ -15,6 +15,7 @@ limitations under the License. */
 #include "paddle/phi/kernels/funcs/sequence_padding.h"
 
 #include "paddle/phi/backends/cpu/cpu_context.h"
+#include "paddle/phi/backends/xpu/enforce_xpu.h"
 
 namespace phi {
 namespace funcs {
