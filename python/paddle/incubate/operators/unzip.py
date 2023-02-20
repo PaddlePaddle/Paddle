78424afac63035ba6013a703b1707a281af7629a
chao9527 <33347532+chao9527@users.noreply.github.com>
2023-02-20 12:37:07 +0800

    update unzip op for save token mem cost (#216)
    Co-authored-by: yangjunchao <yangjunchao@baidu.com>

--- python/paddle/incubate/operators/unzip.py
+++ python/paddle/incubate/operators/unzip.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from paddle.fluid.data_feeder import check_variable_and_dtype
+from paddle.fluid.data_feeder import check_variable_and_dtype, check_type
 from paddle.fluid.layer_helper import LayerHelper
 
 
-def unzip(input, lod):
+def unzip(input, lod, len):
     r"""
 
     **unzip layers**
@@ -24,11 +24,11 @@ def unzip(input, lod):
     unzip 'input' accroding to 'lod'
 
     Args:
-        input (Variable): The zipped input, 2-D LodTensor with shape [N, M].
+        input (Variable): The zipped input
         lod (Variable): The original lod of unzipped input, 1-D LodTensor with shape[K].
 
     Returns:
-        Variable: The original unzipped tensor, 2-D LodTensor with shape[K-1, M].
+        Variable: The original unzipped tensor, 2-D LodTensor with shape[K-1, len].
 
     Examples:
 
@@ -37,28 +37,20 @@ def unzip(input, lod):
           import paddle
           import paddle.fluid as fluid
           paddle.enable_static()
-          input_np = np.array([
-                        [1.0, 2.0, 3.0, 4.0],
-                        [10.0, 20.0, 30.0, 40.0],
-                        [100.0, 200.0, 300.0, 400.0]
-                    ])
-          lod_np = np.array([0, 4, 4, 8, 8, 8, 8, 12, 12, 12, 12])
+          input_np = np.array([1, 2, 3, 1, 2, 4])
+          lod_np = np.array([0, 3, 3, 3, 4, 6])
+          len = 4
           input = paddle.to_tensor(input_np, "int64")
           lod = paddle.to_tensor(lod_np, "int64")
 
-          unzipped_input = paddle.incubate.unzip(input, lod)
+          unzipped_input = paddle.incubate.operators.unzip(input, lod, len)
           '''
           unzipped_input is [
-                        [1.0, 2.0, 3.0, 4.0],
-                        [0.0, 0.0, 0.0, 0.0],
-                        [10.0, 20.0, 30.0, 40.0],
-                        [0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0],
-                        [100.0, 200.0, 300.0, 400.0],
-                        [0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0]
+                        [1, 2, 3, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0],
+                        [1, 0, 0, 0],
+                        [2, 4, 0, 0]
                     ]
           '''
     """
@@ -71,7 +63,9 @@ def unzip(input, lod):
         'unzip',
     )
     check_variable_and_dtype(lod, 'lod', ['int', 'int64'], 'unzip')
+    check_type(len, 'len', (int), 'unzip')
+    attrs = {'len': len}
     helper.append_op(
-        type='unzip', inputs={'X': [input], 'lod': [lod]}, outputs={'Y': [out]}
+        type='unzip', inputs={'X': [input], 'lod': [lod]}, outputs={'Y': [out]}, attrs=attrs
     )
     return out
