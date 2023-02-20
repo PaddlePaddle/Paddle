f320f66135a546ba94cc153a668a1cd345cc86a8
WorgenZhang <frank08081993@gmail.com>
2023-02-20 15:16:02 +0800

    Revert "23.2.20 modify accoding to CI conflicts"
    This reverts commit e288483f9a04734b6bdcdfc5a5494d2494a37ee0.

--- cmake/external/pslib.cmake
+++ cmake/external/pslib.cmake
@@ -54,10 +54,8 @@ ExternalProject_Add(
   ${EXTERNAL_PROJECT_LOG_ARGS}
   PREFIX ${PSLIB_PREFIX_DIR}
   DOWNLOAD_DIR ${PSLIB_DOWNLOAD_DIR}
-  # DOWNLOAD_COMMAND wget --no-check-certificate ${PSLIB_URL} -c -q -O
-  #                  ${PSLIB_NAME}.tar.gz && tar zxvf ${PSLIB_NAME}.tar.gz
-  DOWNLOAD_COMMAND cp /paddle/PSLIB/baidu/paddlepaddle/pslib_tar_bakup/pslib.tar.gz ./
-                   && tar zxvf ${PSLIB_NAME}.tar.gz
+  DOWNLOAD_COMMAND wget --no-check-certificate ${PSLIB_URL} -c -q -O
+                   ${PSLIB_NAME}.tar.gz && tar zxvf ${PSLIB_NAME}.tar.gz
   DOWNLOAD_NO_PROGRESS 1
   UPDATE_COMMAND ""
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${PSLIB_INSTALL_ROOT}
