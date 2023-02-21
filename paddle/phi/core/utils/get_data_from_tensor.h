f58156a676edaf99116d6d3fec6ffb0923f85a77
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-02-21 08:13:47 +0800

    Update get_data_from_tensor.h

--- paddle/phi/core/utils/get_data_from_tensor.h
+++ paddle/phi/core/utils/get_data_from_tensor.h
@@ -23,23 +23,23 @@ namespace funcs {
 template <typename T = int32_t>
 inline std::vector<T> GetDataFromDenseTensor(const phi::DenseTensor* x) {
   std::vector<T> vec_new_data;
-  if (framework::TransToProtoVarType(x->dtype()) ==
-      framework::proto::VarType::INT32) {
+  if (paddle::framework::TransToProtoVarType(x->dtype()) ==
+      paddle::framework::proto::VarType::INT32) {
     auto* data = x->data<int>();
     phi::DenseTensor cpu_attr_tensor;
-    if (!platform::is_cpu_place(x->place())) {
+    if (!paddle::platform::is_cpu_place(x->place())) {
       paddle::framework::TensorCopySync(
-          *x, platform::CPUPlace(), &cpu_attr_tensor);
+          *x, paddle::platform::CPUPlace(), &cpu_attr_tensor);
       data = cpu_attr_tensor.data<int>();
     }
     vec_new_data = std::vector<T>(data, data + x->numel());
-  } else if (framework::TransToProtoVarType(x->dtype()) ==
-             framework::proto::VarType::INT64) {
+  } else if (paddle::framework::TransToProtoVarType(x->dtype()) ==
+             paddle::framework::proto::VarType::INT64) {
     auto* data = x->data<int64_t>();
     phi::DenseTensor cpu_attr_tensor;
-    if (!platform::is_cpu_place(x->place())) {
+    if (!paddle::platform::is_cpu_place(x->place())) {
       paddle::framework::TensorCopySync(
-          *x, platform::CPUPlace(), &cpu_attr_tensor);
+          *x, paddle::platform::CPUPlace(), &cpu_attr_tensor);
       data = cpu_attr_tensor.data<int64_t>();
     }
     // NOTE: Converting int64 to int32 may cause data overflow.
@@ -47,7 +47,7 @@ inline std::vector<T> GetDataFromDenseTensor(const phi::DenseTensor* x) {
   } else {
     PADDLE_THROW(phi::errors::InvalidArgument(
         "The dtype of Tensor must be int32 or int64, but received: %s",
-        framework::TransToProtoVarType(x->dtype())));
+        paddle::framework::TransToProtoVarType(x->dtype())));
   }
   return vec_new_data;
 }
