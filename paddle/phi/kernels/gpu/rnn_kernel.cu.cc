e7357456c52b9260e935bf6853345860f2691b00
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-02-21 12:58:35 +0800

    Update rnn_kernel.cu.cc

--- paddle/phi/kernels/gpu/rnn_kernel.cu.cc
+++ paddle/phi/kernels/gpu/rnn_kernel.cu.cc
@@ -15,9 +15,9 @@
 #include "paddle/phi/kernels/rnn_kernel.h"
 
 #include "paddle/phi/backends/gpu/gpu_context.h"
-#include "paddle/phi/core/utils/get_data_from_tensor.h"
 #include "paddle/phi/core/generator.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/core/utils/get_data_from_tensor.h"
 #include "paddle/phi/kernels/empty_kernel.h"
 #include "paddle/phi/kernels/gpu/rnn_functor.h"
 
