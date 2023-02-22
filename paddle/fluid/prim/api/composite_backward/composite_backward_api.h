f559336112f2774f346a6f310cb2a1ddd13385ab
zhengqiwen1997 <zhengqiwen_au@163.com>
2023-02-22 09:31:27 +0000

    to make the syntax right

--- paddle/fluid/prim/api/composite_backward/composite_backward_api.h
+++ paddle/fluid/prim/api/composite_backward/composite_backward_api.h
@@ -348,8 +348,9 @@ void topk_grad(const Tensor& x,
     if (tmp_axis + 1 == in_dims.size()) {
       // the scatter_nd_add does not support 1-D indices
       if (out_dims.size() == 1) {
-        int num = out_dims[0];
-        std::vector<int64_t> new_dim = phi::vectorize<int64_t>{num, 1};
+        one_dim = phi::make_ddim(1);
+        phi::DDim reduce_dim = get_reduce_dims(out_dims, one_dim);
+        std::vector<int64_t> new_dim = phi::vectorize<int64_t>(reduce_dim);
         auto indices_reshape = reshape<T>(indices, IntArray(new_dim));
         x_grad_tmp =
          scatter_nd_add<T>(zero_tensor, indices_reshape, out_grad, false);
