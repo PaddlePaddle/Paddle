de02e585f87e8ac114190508205c68afc875873e
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-02-22 22:59:14 +0800

    Update rnn_grad_kernel.cc

--- paddle/phi/kernels/xpu/rnn_grad_kernel.cc
+++ paddle/phi/kernels/xpu/rnn_grad_kernel.cc
@@ -163,7 +163,7 @@ void RnnGradKernel(const Context& dev_ctx,
   int state_offset = pre_state[0]->dims()[1] * pre_state[0]->dims()[2];
 
   bool has_seq_length = sequence_length.is_initialized();
-  std::vector<int64_t> seq_len_tensor(batch_size, seq_len);
+  std::vector<int> seq_len_tensor(batch_size, seq_len);
   if (has_seq_length) {
     phi::TensorToVector(*sequence_length, dev_ctx, &seq_len_tensor);
   }
