50b81f9c43a16632c3f923fa8ad7c9c9df88f8ca
peter5232 <1434555239@qq.com>
2023-02-22 15:26:27 +0800

    change

--- python/paddle/framework/dtype.py
+++ python/paddle/framework/dtype.py
@@ -62,7 +62,7 @@ def iinfo(dtype):
 
             iinfo_uint8 = paddle.iinfo(paddle.uint8)
             print(iinfo_uint8)
-            # paddle.iinfo(min=0, max=255, bits=8, dtype=uint8)     
+            # paddle.iinfo(min=0, max=255, bits=8, dtype=uint8)
             print(iinfo_uint8.min) # 0
             print(iinfo_uint8.max) # 255
             print(iinfo_uint8.bits) # 8
