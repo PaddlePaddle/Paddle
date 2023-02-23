864a11a3b9ea5e233bab66310c8b57fbaa9eb24f
huwei02 <53012141+huwei02@users.noreply.github.com>
2023-02-23 22:20:23 +0800

    fix FillInferBuf (#218)
    Co-authored-by: root <root@yq01-inf-hic-k8s-a100-ab2-0009.yq01.baidu.com>

--- paddle/fluid/framework/data_feed.cu
+++ paddle/fluid/framework/data_feed.cu
@@ -2127,8 +2127,9 @@ int GraphDataGenerator::FillInferBuf() {
     global_infer_node_type_start[infer_cursor] += total_row_;
     infer_node_end_ = global_infer_node_type_start[infer_cursor];
     cursor_ = infer_cursor;
+    return 1;
   }
-  return 1;
+  return 0;
 }
 
 void GraphDataGenerator::ClearSampleState() {
