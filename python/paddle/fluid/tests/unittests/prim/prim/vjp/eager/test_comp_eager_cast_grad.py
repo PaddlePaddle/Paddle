b0a98dc47228a6e1c45307095208d07f94a8d021
JiabinYang <360788950@qq.com>
2023-02-23 05:36:27 +0000

    fix ut

--- python/paddle/fluid/tests/unittests/prim/prim/vjp/eager/test_comp_eager_cast_grad.py
+++ python/paddle/fluid/tests/unittests/prim/prim/vjp/eager/test_comp_eager_cast_grad.py
@@ -70,7 +70,7 @@ class TestCastGradComp(unittest.TestCase):
         from paddle.fluid.data_feeder import _PADDLE_DTYPE_2_NUMPY_DTYPE
 
         self.assertEqual(
-            _PADDLE_DTYPE_2_NUMPY_DTYPE(actual[0].dtype), desired[0].dtype
+            _PADDLE_DTYPE_2_NUMPY_DTYPE[actual[0].dtype], desired[0].dtype
         )
         np.testing.assert_allclose(
             actual=actual[0],
