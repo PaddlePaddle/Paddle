4ee264d7fdd843848d25d3a41ff9dbd43c6f78bb
Miracle2333 <kangzhaogeo@gmail.com>
2023-02-23 11:41:11 +0000

    feat: add relu composite rule, commit hook

--- python/paddle/incubate/autograd/composite_rules.py
+++ python/paddle/incubate/autograd/composite_rules.py
@@ -227,6 +227,6 @@ def bernoulli(shape, dtype, p, seed=0):
 
 @REGISTER_COMPOSITE('relu')
 def relu_composite(x):
-    """define composite rule of op relu"""
+    """define composite rule of op relu."""
     # relu(x) = max(x, 0)
     return maximum(0, x)
