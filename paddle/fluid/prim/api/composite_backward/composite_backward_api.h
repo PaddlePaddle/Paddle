ebfd683a49b24fdf85ffcd30603788c97820ebc3
JiabinYang <360788950@qq.com>
2023-02-24 06:22:30 +0000

    fix no grad branch

--- paddle/fluid/prim/api/composite_backward/composite_backward_api.h
+++ paddle/fluid/prim/api/composite_backward/composite_backward_api.h
@@ -26,8 +26,10 @@ using IntArray =
 //  paddle/phi/api/backward/backward_api.h
 template <typename T>
 void cast_grad(const Tensor& out_grad, DataType dtype, Tensor* x_grad) {
-  auto res = cast<T>(out_grad, dtype);
-  set_output<T>(res, x_grad);
+  if (x_grad) {
+    auto res = cast<T>(out_grad, dtype);
+    set_output<T>(res, x_grad);
+  }
 }
 template <typename T>
 void gather_grad(const Tensor& x,
@@ -74,21 +76,25 @@ void tanh_grad(const Tensor& out, const Tensor& grad_out, Tensor* grad_x) {
 
 template <typename T>
 void reshape_grad(const Tensor& x, const Tensor& grad_out, Tensor* grad_x) {
-  auto grad_x_tmp = reshape<T>(grad_out, phi::vectorize(x.dims()));
-  set_output<T>(grad_x_tmp, grad_x);
+  if (grad_x) {
+    auto grad_x_tmp = reshape<T>(grad_out, phi::vectorize(x.dims()));
+    set_output<T>(grad_x_tmp, grad_x);
+  }
 }
 
 template <typename T>
 void transpose_grad(const Tensor& grad_out,
                     const std::vector<int>& perm,
                     Tensor* grad_x) {
-  std::vector<int> reverse_perm(perm);
-  // make origin ranks
-  for (int i = 0; i < static_cast<int>(perm.size()); ++i) {
-    reverse_perm[perm[i]] = i;
+  if (grad_x) {
+    std::vector<int> reverse_perm(perm);
+    // make origin ranks
+    for (int i = 0; i < static_cast<int>(perm.size()); ++i) {
+      reverse_perm[perm[i]] = i;
+    }
+    auto grad_x_tmp = transpose<T>(grad_out, reverse_perm);
+    set_output<T>(grad_x_tmp, grad_x);
   }
-  auto grad_x_tmp = transpose<T>(grad_out, reverse_perm);
-  set_output<T>(grad_x_tmp, grad_x);
 }
 
 template <typename T>
