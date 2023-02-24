1573f14630e75917976bf480c3fac96ea602dc51
Liyulingyue <852433440@qq.com>
2023-02-24 23:20:01 +0800

    rollback unsqueeze_op.h and utils.h

--- paddle/fluid/operators/utils.h
+++ paddle/fluid/operators/utils.h
@@ -24,6 +24,38 @@ namespace paddle {
 namespace operators {
 
 template <typename T = int32_t>
+inline std::vector<T> GetDataFromTensor(const phi::DenseTensor* x) {
+  std::vector<T> vec_new_data;
+  if (framework::TransToProtoVarType(x->dtype()) ==
+      framework::proto::VarType::INT32) {
+    auto* data = x->data<int>();
+    phi::DenseTensor cpu_attr_tensor;
+    if (!platform::is_cpu_place(x->place())) {
+      paddle::framework::TensorCopySync(
+          *x, platform::CPUPlace(), &cpu_attr_tensor);
+      data = cpu_attr_tensor.data<int>();
+    }
+    vec_new_data = std::vector<T>(data, data + x->numel());
+  } else if (framework::TransToProtoVarType(x->dtype()) ==
+             framework::proto::VarType::INT64) {
+    auto* data = x->data<int64_t>();
+    phi::DenseTensor cpu_attr_tensor;
+    if (!platform::is_cpu_place(x->place())) {
+      paddle::framework::TensorCopySync(
+          *x, platform::CPUPlace(), &cpu_attr_tensor);
+      data = cpu_attr_tensor.data<int64_t>();
+    }
+    // NOTE: Converting int64 to int32 may cause data overflow.
+    vec_new_data = std::vector<T>(data, data + x->numel());
+  } else {
+    PADDLE_THROW(platform::errors::InvalidArgument(
+        "The dtype of Tensor must be int32 or int64, but received: %s",
+        framework::TransToProtoVarType(x->dtype())));
+  }
+  return vec_new_data;
+}
+
+template <typename T = int32_t>
 inline std::vector<T> GetDataFromTensorList(
     const std::vector<const phi::DenseTensor*>& list_tensor) {
   std::vector<T> vec_new_data;
