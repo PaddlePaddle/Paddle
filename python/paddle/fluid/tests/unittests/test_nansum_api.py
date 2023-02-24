200d40c1714c307677d3d3340d264f287b91fd10
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-02-24 12:56:43 +0800

    Add NotImplementedError unittest

--- python/paddle/fluid/tests/unittests/test_nansum_api.py
+++ python/paddle/fluid/tests/unittests/test_nansum_api.py
@@ -74,56 +74,58 @@ class API_Test_Nansum(unittest.TestCase):
 
     # test nansum api with float16
     def test_static_graph_fp16(self):
-        paddle.enable_static()
-        startup_program = fluid.Program()
-        train_program = fluid.Program()
-        with fluid.program_guard(train_program, startup_program):
-            input = fluid.data(name='input', dtype='float16', shape=[2, 4])
-            out1 = paddle.nansum(input)
-            out2 = paddle.nansum(input, axis=0)
-            out3 = paddle.nansum(input, axis=-1)
-            out4 = paddle.nansum(input, axis=1, keepdim=True)
-            place = fluid.CPUPlace()
-            if fluid.core.is_compiled_with_cuda():
-                place = fluid.CUDAPlace(0)
-            exe = fluid.Executor(place)
-            exe.run(startup_program)
+        def run1():
+            paddle.enable_static()
+            startup_program = fluid.Program()
+            train_program = fluid.Program()
+            with fluid.program_guard(train_program, startup_program):
+                input = fluid.data(name='input', dtype='float16', shape=[2, 4])
+                out1 = paddle.nansum(input)
+                out2 = paddle.nansum(input, axis=0)
+                out3 = paddle.nansum(input, axis=-1)
+                out4 = paddle.nansum(input, axis=1, keepdim=True)
+                place = fluid.CPUPlace()
+                if fluid.core.is_compiled_with_cuda():
+                    place = fluid.CUDAPlace(0)
+                exe = fluid.Executor(place)
+                exe.run(startup_program)
 
-            x = np.array(
-                [[float('nan'), 3, 5, 9], [1, 2, float('-nan'), 7]]
-            ).astype(np.float16)
-            res = exe.run(
-                train_program,
-                feed={'input': x},
-                fetch_list=[out1, out2, out3, out4],
-            )
+                x = np.array(
+                    [[float('nan'), 3, 5, 9], [1, 2, float('-nan'), 7]]
+                ).astype(np.float16)
+                res = exe.run(
+                    train_program,
+                    feed={'input': x},
+                    fetch_list=[out1, out2, out3, out4],
+                )
 
-            out1_np = np.array(res[0])
-            out2_np = np.array(res[1])
-            out3_np = np.array(res[2])
-            out4_np = np.array(res[3])
-            out1_ref = np.array([27]).astype(np.float16)
-            out2_ref = np.array([1, 5, 5, 16]).astype(np.float16)
-            out3_ref = np.array([17, 10]).astype(np.float16)
-            out4_ref = np.array([[17], [10]]).astype(np.float16)
+                out1_np = np.array(res[0])
+                out2_np = np.array(res[1])
+                out3_np = np.array(res[2])
+                out4_np = np.array(res[3])
+                out1_ref = np.array([27]).astype(np.float16)
+                out2_ref = np.array([1, 5, 5, 16]).astype(np.float16)
+                out3_ref = np.array([17, 10]).astype(np.float16)
+                out4_ref = np.array([[17], [10]]).astype(np.float16)
 
-            self.assertTrue(
-                (out1_np == out1_ref).all(),
-                msg='nansum output is wrong, out =' + str(out1_np),
-            )
-            self.assertTrue(
-                (out2_np == out2_ref).all(),
-                msg='nansum output is wrong, out =' + str(out2_np),
-            )
-            self.assertTrue(
-                (out3_np == out3_ref).all(),
-                msg='nansum output is wrong, out =' + str(out3_np),
-            )
-            self.assertTrue(
-                (out4_np == out4_ref).all(),
-                msg='nansum output is wrong, out =' + str(out4_np),
-            )
-
+                self.assertTrue(
+                    (out1_np == out1_ref).all(),
+                    msg='nansum output is wrong, out =' + str(out1_np),
+                )
+                self.assertTrue(
+                    (out2_np == out2_ref).all(),
+                    msg='nansum output is wrong, out =' + str(out2_np),
+                )
+                self.assertTrue(
+                    (out3_np == out3_ref).all(),
+                    msg='nansum output is wrong, out =' + str(out3_np),
+                )
+                self.assertTrue(
+                    (out4_np == out4_ref).all(),
+                    msg='nansum output is wrong, out =' + str(out4_np),
+                )
+        self.assertRaises(NotImplementedError, run1)
+        
     def test_dygraph(self):
         x = np.array(
             [[float('nan'), 3, 5, 9], [1, 2, float('-nan'), 7]]
