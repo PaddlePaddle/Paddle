099de4891c80e77957759ad6bd8c500974a8a204
mrcangye <chenloong@88.com>
2023-02-27 00:23:05 +0800

    rewrite test_api_fp16

--- python/paddle/fluid/tests/unittests/test_compare_op.py
+++ python/paddle/fluid/tests/unittests/test_compare_op.py
@@ -474,7 +474,7 @@ class API_TestElementwise_Equal(unittest.TestCase):
             limit = paddle.assign(np.array([3, 2], dtype="float16"))
             out = paddle.equal(x=label, y=limit)
             if core.is_compiled_with_cuda():
-                place = fluid.CPUPlace()
+                place = paddle.CUDAPlace(0)
                 exe = fluid.Executor(place)
                 (res,) = exe.run(fetch_list=[out])
         self.assertEqual((res == np.array([True, False])).all(), True)
@@ -484,21 +484,10 @@ class API_TestElementwise_Equal(unittest.TestCase):
             limit = paddle.assign(np.array([3, 3], dtype="float16"))
             out = paddle.equal(x=label, y=limit)
             if core.is_compiled_with_cuda():
-                place = fluid.CPUPlace()
+                place = paddle.CUDAPlace(0)
                 exe = fluid.Executor(place)
                 (res,) = exe.run(fetch_list=[out])
         self.assertEqual((res == np.array([True, True])).all(), True)
-
-    def test_fp16(self):
-        x_np = np.random.random((10, 16)).astype('float16')
-        with paddle.static.program_guard(paddle.static.Program()):
-            x = paddle.static.data(shape=[10, 16], name='x', dtype='float16')
-            out = paddle.equal(x=label, y=limit)
-            if core.is_compiled_with_cuda():
-                place = paddle.CUDAPlace(0)
-                exe = paddle.static.Executor(place)
-                exe.run(paddle.static.default_startup_program())
-                out = exe.run(feed={'x': x_np}, fetch_list=[out])
 
 
 class TestCompareOpPlace(unittest.TestCase):
