57f6a4695ba07dd48c553374f57afa3ce7280628
Yiqun Liu <Xreki@users.noreply.github.com>
2023-02-26 09:40:45 +0800

    Enable matmul + bias fusion in fused_gat_attention. (#50755)
    * Enable matmul + bias fusion in fused_gat_attention.
    
    * Add a variable to control whether using fused matmul + bias.

--- paddle/fluid/operators/fused/fused_gemm_epilogue_op.cc
+++ paddle/fluid/operators/fused/fused_gemm_epilogue_op.cc
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/fluid/operators/fused/fused_gemm_epilogue_op.h"
-
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/framework/op_version_registry.h"
 
