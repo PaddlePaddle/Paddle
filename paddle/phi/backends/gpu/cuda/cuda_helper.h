5bd92de2dad0e0f5b5638445cbdee03669711841
Liu Yiqun <liuyiqun01@baidu.com>
2023-02-26 10:17:19 +0000

    Add the missing header file.

--- paddle/phi/backends/gpu/cuda/cuda_helper.h
+++ paddle/phi/backends/gpu/cuda/cuda_helper.h
@@ -20,6 +20,7 @@
 #include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/common/float16.h"
+#include "paddle/phi/core/enforce.h"
 
 namespace phi {
 namespace backends {
