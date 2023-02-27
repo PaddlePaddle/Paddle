1303eaa3833de26902cdbd761dadd0f291a74339
mrcangye <chenloong@88.com>
2023-02-27 12:05:55 +0800

    rewrite test_api_fp16

--- python/paddle/fluid/tests/unittests/test_compare_op.py
+++ python/paddle/fluid/tests/unittests/test_compare_op.py
@@ -470,14 +470,14 @@ class API_TestElementwise_Equal(unittest.TestCase):
     def test_api_fp16(self):
         paddle.enable_static()
         with fluid.program_guard(fluid.Program(), fluid.Program()):
-            label = paddle.to_tensor([3, 2], dtype="float16")
+            label = paddle.to_tensor([3, 3], dtype="float16")
             limit = paddle.to_tensor([3, 2], dtype="float16")
             out = paddle.equal(x=label, y=limit)
             if core.is_compiled_with_cuda():
                 place = paddle.CUDAPlace(0)
                 exe = fluid.Executor(place)
                 (res,) = exe.run(fetch_list=[out])
-        self.assertEqual((res == np.array([True, False])).all(), True)
+                self.assertEqual((res == np.array([True, False])).all(), True)
 
         with fluid.program_guard(fluid.Program(), fluid.Program()):
             label = paddle.to_tensor([3, 3], dtype="float16")
@@ -487,7 +487,7 @@ class API_TestElementwise_Equal(unittest.TestCase):
                 place = paddle.CUDAPlace(0)
                 exe = fluid.Executor(place)
                 (res,) = exe.run(fetch_list=[out])
-        self.assertEqual((res == np.array([True, True])).all(), True)
+                self.assertEqual((res == np.array([True, True])).all(), True)
 
 
 class TestCompareOpPlace(unittest.TestCase):
