160a1f3e23399d019d680efdef9292a6a22e2835
wangzhen38 <wangzhen38@baidu.com>
2023-02-27 09:01:47 +0000

    [cinn] concat_grad

--- paddle/fluid/operators/concat_op.cc
+++ paddle/fluid/operators/concat_op.cc
@@ -158,19 +158,18 @@ class ConcatCompositeGradOpMaker : public prim::CompositeGradOpMakerBase {
 
  public:
   void Apply() override {
-    std::vector<paddle::experimental::Tensor> input = this->GetSingleForwardInput("X");
+    std::vector<paddle::experimental::Tensor> input =
+        this->GetSingleForwardInput("X");
     paddle::experimental::Tensor out_grad = this->GetSingleOutputGrad("Out");
-    std::vector<paddle::experimental::Tensor> input_grad = this->GetSingleInputGrad("X");
+    std::vector<paddle::experimental::Tensor> input_grad =
+        this->GetSingleInputGrad("X");
 
     int axis = static_cast<int>(this->Attr<int>("axis"));
     auto dx_ptr = this->GetOutputPtr(&input_grad);
     std::string dx_name = this->GetOutputName(input_grad);
 
     VLOG(6) << "Runing concat_grad composite func";
-    prim::concat_grad<prim::DescTensor>(input,
-                                       out_grad,
-                                       axis,
-                                       dx_ptr);
+    prim::concat_grad<prim::DescTensor>(input, out_grad, axis, dx_ptr);
     this->RecoverOutputName(input_grad, dx_name);
   }
 };
