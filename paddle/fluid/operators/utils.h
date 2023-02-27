c382d1cea4e0efb771cac0b7205bc7d7d3d8da1f
iLeGend <824040212@qq.com>
2023-02-27 15:15:42 +0800

    refer hears by quote

--- paddle/fluid/operators/utils.h
+++ paddle/fluid/operators/utils.h
@@ -13,11 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
-#include <paddle/fluid/framework/operator.h>
-#include <paddle/phi/core/tensor_utils.h>
 
 #include <string>
 #include <vector>
+
+#include "paddle/fluid/framework/operator.h"
+#include "paddle/phi/core/tensor_utils.h"
 
 namespace paddle {
 namespace operators {
