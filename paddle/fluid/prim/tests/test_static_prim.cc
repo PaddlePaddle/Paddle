a0c473f429a4872a48eaf80ef8307b0e68abf93c
xiaoguoguo626807 <100397923+xiaoguoguo626807@users.noreply.github.com>
2023-02-28 16:55:12 +0800

    【prim】Matmul double grad composite api (#50452)
    * modify name
    
    * merge develop
    
    * original code
    
    * build modify
    
    * success 2*2
    
    * fused dim=1 failed
    
    * success
    
    * modify static
    
    * success for static except dim=1
    
    * delete log
    
    * tmp modify
    
    * success
    
    * success
    
    * add fp1664
    
    * delete fp16 cpu test
    
    * stop windows test
    
    * review modify
    
    * modify tanh test
    
    * modify tanh
    
    * fix_conflixt
    
    * modift static prim
    
    * fix_conflict
    
    * Update test_static_prim.cc
    
    * update
    
    * bug fix

--- paddle/fluid/prim/tests/test_static_prim.cc
+++ paddle/fluid/prim/tests/test_static_prim.cc
@@ -194,7 +194,7 @@ TEST(StaticPrim, TanhBackwardComposite) {
                                        target_block,
                                        grad_sub_block));
   ASSERT_EQ(target_block->AllOps().size(), static_cast<std::size_t>(1));
-  ASSERT_EQ(grad_ops.size(), static_cast<std::size_t>(5));
+  ASSERT_EQ(grad_ops.size(), static_cast<std::size_t>(4));
   ASSERT_EQ(target_block->AllOps()[0]->Type(), "tanh");
   ASSERT_EQ(target_block->AllOps()[0]->Inputs().at("X").size(),
             static_cast<std::size_t>(1));
@@ -204,40 +204,33 @@ TEST(StaticPrim, TanhBackwardComposite) {
   ASSERT_EQ(target_block->AllOps()[0]->Outputs().at("Out")[0], "b");
   ASSERT_EQ(target_block->AllOps()[0]->Outputs().at("Out")[0], "b");
 
-  ASSERT_EQ(grad_ops[0]->Type(), "fill_constant");
-  ASSERT_EQ(PADDLE_GET_CONST(int, grad_ops[0]->GetAttr("dtype")),
+  ASSERT_EQ(grad_ops[0]->Type(), "elementwise_mul");
+  ASSERT_EQ(grad_ops[0]->Inputs().at("X").size(), static_cast<std::size_t>(1));
+  ASSERT_EQ(grad_ops[0]->Inputs().at("Y").size(), static_cast<std::size_t>(1));
+  ASSERT_EQ(grad_ops[0]->Inputs().at("Y")[0], "b");
+  ASSERT_EQ(grad_ops[0]->Inputs().at("X")[0], "b");
+
+  ASSERT_EQ(grad_ops[1]->Type(), "fill_constant");
+  ASSERT_EQ(PADDLE_GET_CONST(int, grad_ops[1]->GetAttr("dtype")),
             static_cast<int>(5));  // ProtoDataType::FP32
-  ASSERT_EQ(grad_ops[0]->Outputs().at("Out").size(),
+  ASSERT_EQ(grad_ops[1]->Outputs().at("Out").size(),
             static_cast<std::size_t>(1));
 
-  ASSERT_EQ(grad_ops[1]->Type(), "elementwise_pow");
-  ASSERT_EQ(grad_ops[1]->Inputs().at("X").size(), static_cast<std::size_t>(1));
-  ASSERT_EQ(grad_ops[1]->Inputs().at("Y").size(), static_cast<std::size_t>(1));
-  ASSERT_EQ(grad_ops[1]->Inputs().at("X")[0], "b");
-  ASSERT_EQ(grad_ops[0]->Outputs().at("Out").size(),
-            static_cast<std::size_t>(1));
-
-  ASSERT_EQ(grad_ops[2]->Type(), "fill_constant");
-  ASSERT_EQ(PADDLE_GET_CONST(int, grad_ops[2]->GetAttr("dtype")),
-            static_cast<int>(5));  // ProtoDataType::FP32
+  ASSERT_EQ(grad_ops[2]->Type(), "elementwise_sub");
+  ASSERT_EQ(grad_ops[2]->Inputs().at("X").size(), static_cast<std::size_t>(1));
+  ASSERT_EQ(grad_ops[2]->Inputs().at("Y").size(), static_cast<std::size_t>(1));
+  ASSERT_EQ(grad_ops[2]->Inputs().at("X")[0],
+            grad_ops[1]->Outputs().at("Out")[0]);
   ASSERT_EQ(grad_ops[2]->Outputs().at("Out").size(),
             static_cast<std::size_t>(1));
 
-  ASSERT_EQ(grad_ops[3]->Type(), "elementwise_sub");
+  ASSERT_EQ(grad_ops[3]->Type(), "elementwise_mul");
   ASSERT_EQ(grad_ops[3]->Inputs().at("X").size(), static_cast<std::size_t>(1));
   ASSERT_EQ(grad_ops[3]->Inputs().at("Y").size(), static_cast<std::size_t>(1));
-  ASSERT_EQ(grad_ops[3]->Inputs().at("X")[0],
+  ASSERT_EQ(grad_ops[3]->Inputs().at("Y")[0],
             grad_ops[2]->Outputs().at("Out")[0]);
+  ASSERT_EQ(grad_ops[3]->Inputs().at("X")[0], "b@GRAD");
   ASSERT_EQ(grad_ops[3]->Outputs().at("Out").size(),
-            static_cast<std::size_t>(1));
-
-  ASSERT_EQ(grad_ops[4]->Type(), "elementwise_mul");
-  ASSERT_EQ(grad_ops[4]->Inputs().at("X").size(), static_cast<std::size_t>(1));
-  ASSERT_EQ(grad_ops[4]->Inputs().at("Y").size(), static_cast<std::size_t>(1));
-  ASSERT_EQ(grad_ops[4]->Inputs().at("Y")[0],
-            grad_ops[3]->Outputs().at("Out")[0]);
-  ASSERT_EQ(grad_ops[4]->Inputs().at("X")[0], "b@GRAD");
-  ASSERT_EQ(grad_ops[4]->Outputs().at("Out").size(),
             static_cast<std::size_t>(1));
 }
 
