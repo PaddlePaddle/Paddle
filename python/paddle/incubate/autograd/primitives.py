5474dcf9fbfe0bce17326849dbea2ffdb59a3ee5
liupeng51 <liupeng51@baidu.com>
2023-02-28 14:52:14 +0800

    update apis calling

--- python/paddle/incubate/autograd/primitives.py
+++ python/paddle/incubate/autograd/primitives.py
@@ -59,7 +59,10 @@ from paddle.tensor import tanh  # noqa: F401
 from paddle.tensor import uniform  # noqa: F401
 from paddle.tensor import zeros  # noqa: F401
 from paddle.tensor.creation import assign  # noqa: F401
+from paddle.tensor.creation import to_tensor  # noqa: F401
 from paddle.tensor.manipulation import cast  # noqa: F401
+from paddle.tensor.math import maximum  # noqa: F401
+from paddle.tensor.math import minimum  # noqa: F401
 
 """
 math_op = [
@@ -87,6 +90,8 @@ math_op = [
     'logit',
     'max',
     'min',
+    'minimum',
+    'maximum'
 ]
 
 trigonometric_op = [
@@ -120,5 +125,6 @@ others = [
     'full',
     'uniform',
     'greater_equal',
+    'to_tensor'
 ]
 """
