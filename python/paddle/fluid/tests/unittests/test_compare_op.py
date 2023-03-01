a9c88957110751f3c86a9ef411d001e7998a9af0
wqgo <1552367872@qq.com>
2023-03-02 00:01:26 +0800

    add float16 to greater_than

--- python/paddle/fluid/tests/unittests/test_compare_op.py
+++ python/paddle/fluid/tests/unittests/test_compare_op.py
@@ -479,6 +479,18 @@ class API_TestElementwise_Equal(unittest.TestCase):
                 (res,) = exe.run(fetch_list=[out])
                 self.assertEqual((res == np.array([True, False])).all(), True)
 
+class API_Test_Greater_Than(unittest.TestCase):
+    def test_api_fp16(self):
+        paddle.enable_static()
+        with fluid.program_guard(fluid.Program(), fluid.Program()):
+            label = paddle.to_tensor([3, 3], dtype="float16")
+            limit = paddle.to_tensor([3, 2], dtype="float16")
+            out = paddle.greater_than(x=label, y=limit)
+            if core.is_compiled_with_cuda():
+                place = paddle.CUDAPlace(0)
+                exe = fluid.Executor(place)
+                (res,) = exe.run(fetch_list=[out])
+                self.assertEqual((res == np.array([False, True])).all(), True)
 
 class TestCompareOpPlace(unittest.TestCase):
     def test_place_1(self):
