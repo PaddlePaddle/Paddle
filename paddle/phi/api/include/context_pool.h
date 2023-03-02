534cdd3e51cc6196b1c26674bead53c2902f63de
huangjiyi <947613776@qq.com>
2023-03-02 11:58:51 +0000

    fix bugs

--- paddle/phi/api/include/context_pool.h
+++ paddle/phi/api/include/context_pool.h
@@ -17,7 +17,6 @@ limitations under the License. */
 #include <mutex>
 
 #include "paddle/phi/api/include/dll_decl.h"
-#include "paddle/phi/backends/all_context.h"
 #include "paddle/phi/common/place.h"
 #include "paddle/phi/core/macros.h"
 #include "paddle/utils/flat_hash_map.h"
--- paddle/phi/kernels/xpu/label_smooth_kernel.cc
+++ paddle/phi/kernels/xpu/label_smooth_kernel.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include "paddle/phi/kernels/label_smooth_kernel.h"
+#include "paddle/phi/backends/xpu/xpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
 namespace phi {
 
