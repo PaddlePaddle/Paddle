cce2b94df86b9df3a9bfed46ad07fdcd5fa8c894
HongyuJia <jiahongyu@baidu.com>
2023-03-02 14:08:28 +0800

    [GetCurrentCUDAStream] Add C++ API GetCurrentCUDAStream (#51027)
    * polish codes according #50813
    
    * [getCurrentCUDAStream] Add C++ API getCurrentCUDAStream
    
    * change get->Get
    
    * wrap with macro
    
    * use Get instead of get

--- paddle/phi/tests/api/test_cuda_stream.cu
+++ paddle/phi/tests/api/test_cuda_stream.cu
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,6 +12,15 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/phi/core/allocator.h"
+#include <gtest/gtest.h>
 
-namespace phi {}  // namespace phi
+#include "paddle/phi/api/include/context_pool.h"
+#include "paddle/phi/core/cuda_stream.h"
+
+TEST(CUDAStream, GPU) {
+  phi::GPUPlace gpu0(0);
+  phi::CUDAStream* stream = paddle::GetCurrentCUDAStream(gpu0);
+  EXPECT_TRUE(stream != nullptr);
+  gpuStream_t raw_stream = stream->raw_stream();
+  EXPECT_TRUE(raw_stream != nullptr);
+}
