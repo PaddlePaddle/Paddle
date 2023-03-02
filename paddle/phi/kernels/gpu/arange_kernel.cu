a021e444efec40d4cad663edcab04f9f3805bf2d
yangjianfengo1 <yangjianfeng01@baidu.com>
2023-03-02 09:56:57 +0000

    fix ci

--- paddle/phi/kernels/gpu/arange_kernel.cu
+++ paddle/phi/kernels/gpu/arange_kernel.cu
@@ -15,14 +15,14 @@
 #include "paddle/phi/kernels/arange_kernel.h"
 
 #include "paddle/phi/backends/gpu/gpu_context.h"
+#include "paddle/phi/common/amp_type_traits.h"
+#include "paddle/phi/common/bfloat16.h"
+#include "paddle/phi/common/float16.h"
 #include "paddle/phi/core/enforce.h"
 #include "paddle/phi/core/errors.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/tensor_utils.h"
 #include "paddle/phi/kernels/funcs/range_function.h"
-#include "paddle/phi/common/amp_type_traits.h"
-#include "paddle/phi/common/float16.h"
-#include "paddle/phi/common/bfloat16.h"
 
 namespace phi {
 
@@ -38,12 +38,13 @@ void ArangeKernel(const Context& dev_ctx,
                   const DenseTensor& step,
                   DenseTensor* out) {
   using MPType = typename phi::dtype::MPTypeTrait<T>::Type;
-  MPType start_value = static_cast<MPType>(GetValue<T, Context>(dev_ctx, start));
+  MPType start_value =
+      static_cast<MPType>(GetValue<T, Context>(dev_ctx, start));
   MPType end_value = static_cast<MPType>(GetValue<T, Context>(dev_ctx, end));
   MPType step_value = static_cast<MPType>(GetValue<T, Context>(dev_ctx, step));
 
   int64_t size = 0;
-  
+
   phi::funcs::GetSize(start_value, end_value, step_value, &size);
   out->Resize(phi::make_ddim({size}));
   MPType* out_data = dev_ctx.template Alloc<MPType>(out);
@@ -54,13 +55,22 @@ void ArangeKernel(const Context& dev_ctx,
     return;
   }
   int64_t grid = (size + block - 1) / block;
-  Range<MPType><<<grid, block, 0, stream>>>(start_value, step_value, size, out_data);
+  Range<MPType>
+      <<<grid, block, 0, stream>>>(start_value, step_value, size, out_data);
 }
 
 }  // namespace phi
 
-PD_REGISTER_KERNEL(
-    arange, GPU, ALL_LAYOUT, phi::ArangeKernel, float, double, int64_t, int, phi::dtype::float16, phi::dtype::bfloat16) {
+PD_REGISTER_KERNEL(arange,
+                   GPU,
+                   ALL_LAYOUT,
+                   phi::ArangeKernel,
+                   float,
+                   double,
+                   int64_t,
+                   int,
+                   phi::dtype::float16,
+                   phi::dtype::bfloat16) {
   kernel->InputAt(0).SetBackend(phi::Backend::ALL_BACKEND);
   kernel->InputAt(1).SetBackend(phi::Backend::ALL_BACKEND);
   kernel->InputAt(2).SetBackend(phi::Backend::ALL_BACKEND);
