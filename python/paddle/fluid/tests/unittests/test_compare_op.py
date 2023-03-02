bf926e0bdec761f352083f7d9ce2e662061b14b9
wqgo <1552367872@qq.com>
2023-03-02 15:54:59 +0800

    update

--- python/paddle/fluid/tests/unittests/test_compare_op.py
+++ python/paddle/fluid/tests/unittests/test_compare_op.py
@@ -482,13 +482,13 @@ class API_TestElementwise_Equal(unittest.TestCase):
 class API_TestElementwise_Less_Than(unittest.TestCase):
     def test_api_fp16(self):
         paddle.enable_static()
-        with fluid.program_guard(fluid.Program(), fluid.Program()):
+        with paddle.static.program_guard(paddle.static.Program(), paddle.static.Program()):
             label = paddle.to_tensor([3, 3], dtype="float16")
             limit = paddle.to_tensor([3, 2], dtype="float16")
             out = paddle.less_than(x=label, y=limit)
             if core.is_compiled_with_cuda():
                 place = paddle.CUDAPlace(0)
-                exe = fluid.Executor(place)
+                exe = paddle.static.Executor(place)
                 (res,) = exe.run(fetch_list=[out])
                 self.assertEqual((res == np.array([False, False])).all(), True)
 
