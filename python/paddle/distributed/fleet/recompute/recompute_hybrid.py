fd867fc7a6b6623acca3e3bc1ca23a5a13905423
GhostScreaming <mofengshenjieII@163.com>
2023-03-03 08:20:55 +0000

    Fix bug of amp_dtype.

--- python/paddle/distributed/fleet/recompute/recompute_hybrid.py
+++ python/paddle/distributed/fleet/recompute/recompute_hybrid.py
@@ -128,14 +128,7 @@ class _HPRecomputeFunction(PyLayer):
             raise ValueError(
                 "unsupported amp level: {}".format(tracer._amp_level)
             )
-        if tracer._amp_dtype == 'float16':
-            ctx.amp_dtype = 'float16'
-        elif tracer._amp_dtype in ('bfloat16', 'float32'):
-            ctx.amp_dtype = 'bfloat16'
-        else:
-            raise ValueError(
-                "unsupported amp dtype: {}".format(tracer._amp_dtype)
-            )
+        ctx.amp_dtype = tracer._amp_dtype
         ctx.amp_white_list, ctx.amp_black_list = tracer._get_amp_op_list()
 
         with paddle.no_grad():
