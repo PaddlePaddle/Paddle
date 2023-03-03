f62afbded6d3e2510b67276316e9ff95af360067
qizhaoaoe <zhaoqi10@baidu.com>
2023-03-03 17:33:01 +0800

    fluid clean: remove fluid.ir to framework.ir and some funcs form fluid.layer.io to incubate.

--- python/paddle/fluid/tests/unittests/test_apply_pass_to_program.py
+++ python/paddle/fluid/tests/unittests/test_apply_pass_to_program.py
@@ -19,7 +19,7 @@ import numpy as np
 import paddle
 import paddle.fluid as fluid
 from paddle.fluid.framework import _apply_pass
-from paddle.fluid.ir import apply_build_strategy
+from paddle.framework.ir import apply_build_strategy
 from paddle.nn import CrossEntropyLoss
 from paddle.vision.models import resnet50
 
