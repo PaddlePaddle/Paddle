a800bc538c6a2a259e6519f8d74071ccd81e1d01
Ainavo <Ainavo@163.com>
2023-03-04 20:00:50 +0800

    fix

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -294,6 +294,7 @@ from .tensor.math import frac  # noqa: F401
 from .tensor.math import sgn  # noqa: F401
 from .tensor.math import take  # noqa: F401
 from .tensor.math import frexp  # noqa: F401
+from .tensor.math import trapezoid  # noqa: F401
 
 from .tensor.random import bernoulli  # noqa: F401
 from .tensor.random import poisson  # noqa: F401
