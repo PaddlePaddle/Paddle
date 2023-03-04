302e68a337786438c841fa845b34c7f19b924ae4
enkilee <luhputu0815@gmail.com>
2023-03-04 03:54:47 +0000

    fix

--- python/paddle/fluid/tests/unittests/test_atan2_op.py
+++ python/paddle/fluid/tests/unittests/test_atan2_op.py
@@ -134,7 +134,7 @@ class TestAtan2OpBfp16(OpTest):
     def setUp(self):
         self.op_type = "atan2"
         self.dtype = np.uint16
-        input_x = np.random.uniform(0.1, 1).astype(np.float32)
+        input_x = np.random.rand(0.1, 1).astype(np.float32)
         input_y = np.random.rand(-1, -0.1).astype(np.float32)
         output_np = paddle.atan2(input_x, input_y)
         output_ref = np.arctan2(input_x, input_y)
