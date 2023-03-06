5103d83c77b030bff6b5be8c8d6d0cc6e7c6a708
mengziheng <hitmzh@163.com>
2023-03-06 08:01:00 +0000

    modify test file

--- python/paddle/fluid/tests/unittests/test_unsqueeze2_op.py
+++ python/paddle/fluid/tests/unittests/test_unsqueeze2_op.py
@@ -33,13 +33,15 @@ class TestUnsqueezeOp(OpTest):
         self.inputs = {"X": np.random.random(self.ori_shape).astype("float64")}
         self.init_attrs()
         self.prim_op_type = "comp"
-	self.outputs = {
+        self.outputs = {
             "Out": self.inputs["X"].reshape(self.new_shape),
             "XShape": np.random.random(self.ori_shape).astype("float64"),
         }
 
     def test_check_output(self):
-        self.check_output(check_prim=True, no_check_set=["XShape"], check_eager=True)
+        self.check_output(
+            check_prim=True, no_check_set=["XShape"], check_eager=True
+        )
 
     def test_check_grad(self):
         self.check_grad(["X"], "Out", check_eager=True)
@@ -87,7 +89,7 @@ class TestUnsqueezeOp4(TestUnsqueezeOp):
 
 class TestUnsqueezeOp_ZeroDim1(TestUnsqueezeOp):
     def init_test_case(self):
-	self.enable_cinn = False
+        self.enable_cinn = False
         self.ori_shape = ()
         self.axes = (-1,)
         self.new_shape = 1
@@ -96,7 +98,7 @@ class TestUnsqueezeOp_ZeroDim1(TestUnsqueezeOp):
 class TestUnsqueezeOp_ZeroDim2(TestUnsqueezeOp):
     def init_test_case(self):
         self.enable_cinn = False
-	self.ori_shape = ()
+        self.ori_shape = ()
         self.axes = (-1, 1)
         self.new_shape = (1, 1)
 
@@ -104,7 +106,7 @@ class TestUnsqueezeOp_ZeroDim2(TestUnsqueezeOp):
 class TestUnsqueezeOp_ZeroDim3(TestUnsqueezeOp):
     def init_test_case(self):
         self.enable_cinn = False
-	self.ori_shape = ()
+        self.ori_shape = ()
         self.axes = (0, 1, 2)
         self.new_shape = (1, 1, 1)
 
