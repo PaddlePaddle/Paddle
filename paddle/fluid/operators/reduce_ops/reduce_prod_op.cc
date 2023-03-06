65642d1ffe0cf3057f212c5d9a8418ff8534deef
chenjian <chenjian26@baidu.com>
2023-03-06 09:35:08 +0000

    add registry

--- paddle/fluid/operators/reduce_ops/reduce_prod_op.cc
+++ paddle/fluid/operators/reduce_ops/reduce_prod_op.cc
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 #include "paddle/fluid/operators/reduce_ops/reduce_prod_op.h"
+#include "paddle/fluid/prim/api/composite_backward/composite_backward_api.h"
+#include "paddle/fluid/prim/utils/static/composite_grad_desc_maker.h"
+#include "paddle/fluid/prim/utils/static/desc_tensor.h"
 
 #include "paddle/fluid/framework/infershape_utils.h"
 #include "paddle/phi/core/infermeta_utils.h"
@@ -35,6 +38,44 @@ class ReduceProdOpMaker : public ops::ReduceOpMaker {
   virtual std::string GetOpType() const { return "Reduce reduce_prod"; }
 };
 
+
+class ReduceProdCompositeGradOpMaker : public prim::CompositeGradOpMakerBase {
+  using prim::CompositeGradOpMakerBase::CompositeGradOpMakerBase;
+
+ public:
+  void Apply() override {
+    // get inputs
+    paddle::experimental::Tensor x = this->GetSingleForwardInput("X");
+    paddle::experimental::Tensor out = this->GetSingleForwardOutput("Out");
+    paddle::experimental::Tensor out_grad = this->GetSingleOutputGrad("Out");
+    
+    // get attr
+    std::vector<int> axis = this->Attr<std::vector<int>>("dim");
+    bool keep_dim = this->Attr<bool>("keep_dim");
+    bool reduce_all = this->Attr<bool>("reduce_all");
+
+    // get output
+    paddle::experimental::Tensor x_grad_t = this->GetSingleInputGrad("X");
+
+    // get output ptr
+    auto x_grad = this->GetOutputPtr(&x_grad_t);
+
+    // get output orginal name
+    std::string x_grad_name = this->GetOutputName(x_grad_t);
+    VLOG(6) << "Runing prod_grad composite func";
+    // call composite backward func
+    prim::prod_grad<prim::DescTensor>(x,
+                                      out,
+                                      out_grad,
+                                       axis,
+                                       keep_dim,
+                                       reduce_all,
+                                       x_grad);
+    // recover output name
+    this->RecoverOutputName(x_grad_t, x_grad_name);
+  }
+};
+
 DECLARE_INFER_SHAPE_FUNCTOR(
     reduce_prod,
     ReduceProdInferShapeFunctor,
@@ -46,5 +87,6 @@ REGISTER_OPERATOR(
     ReduceProdOpMaker,
     paddle::framework::DefaultGradOpMaker<paddle::framework::OpDesc, true>,
     paddle::framework::DefaultGradOpMaker<paddle::imperative::OpBase, true>,
+    ops::ReduceProdCompositeGradOpMaker,
     ReduceProdInferShapeFunctor);
 REGISTER_OPERATOR(reduce_prod_grad, ops::ReduceGradOp);
