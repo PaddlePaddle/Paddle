4a29e9f58ad4ceb9783a000e8a45544059af2f8b
zhangbo9674 <zhangbo54@baidu.com>
2023-03-06 03:31:54 +0000

    refine cast_utils

--- paddle/ir/cast_utils.h
+++ paddle/ir/cast_utils.h
@@ -19,7 +19,7 @@
 namespace ir {
 template <typename Target, typename From, typename Enabler = void>
 struct isa_impl {
-  static inline bool doit(const From &Val) { return Target::classof(Val); }
+  static inline bool call(const From &Val) { return Target::classof(Val); }
 };
 
 template <typename Target, typename From>
@@ -27,69 +27,77 @@ struct isa_impl<
     Target,
     From,
     typename std::enable_if<std::is_base_of<Target, From>::value>::type> {
-  static inline bool doit(const From &) { return true; }
+  static inline bool call(const From &) { return true; }
 };
 
+///
+/// \brief The template function actually called by isa.
+///
 template <typename Target, typename From>
 struct isa_wrap {
-  static inline bool doit(const From &Val) {
-    return isa_impl<Target, From>::doit(Val);
+  static inline bool call(const From &Val) {
+    return isa_impl<Target, From>::call(Val);
   }
 };
 
+///
+/// \brief typequalified specialization of the isa_wrap template parameter From.
+/// Specialized types include: const T, T*, const T*, T* const, const T* const.
+///
 template <typename Target, typename From>
 struct isa_wrap<Target, const From> {
-  static inline bool doit(const From &Val) {
-    return isa_impl<Target, From>::doit(Val);
+  static inline bool call(const From &Val) {
+    return isa_impl<Target, From>::call(Val);
   }
 };
 
 template <typename Target, typename From>
 struct isa_wrap<Target, From *> {
-  static inline bool doit(const From *Val) {
+  static inline bool call(const From *Val) {
     if (Val == nullptr) {
       throw("isa<> used on a null pointer");
     }
-    return isa_impl<Target, From>::doit(*Val);
+    return isa_impl<Target, From>::call(*Val);
   }
 };
 
 template <typename Target, typename From>
 struct isa_wrap<Target, From *const> {
-  static inline bool doit(const From *Val) {
+  static inline bool call(const From *Val) {
     if (Val == nullptr) {
       throw("isa<> used on a null pointer");
     }
-    return isa_impl<Target, From>::doit(*Val);
+    return isa_impl<Target, From>::call(*Val);
   }
 };
 
 template <typename Target, typename From>
 struct isa_wrap<Target, const From *> {
-  static inline bool doit(const From *Val) {
+  static inline bool call(const From *Val) {
     if (Val == nullptr) {
       throw("isa<> used on a null pointer");
     }
-    return isa_impl<Target, From>::doit(*Val);
+    return isa_impl<Target, From>::call(*Val);
   }
 };
 
 template <typename Target, typename From>
 struct isa_wrap<Target, const From *const> {
-  static inline bool doit(const From *Val) {
+  static inline bool call(const From *Val) {
     if (Val == nullptr) {
       throw("isa<> used on a null pointer");
     }
-    return isa_impl<Target, From>::doit(*Val);
+    return isa_impl<Target, From>::call(*Val);
   }
 };
 
 ///
-/// \brief isa func: if (isa<Type>(value)) { ... }.
+/// \brief isa template function, used to determine whether the value is a
+/// Target type. Using method: if (isa<Target_Type>(value)) { ... }.
 ///
 template <class Target, class From>
 inline bool isa(const From &Val) {
-  return isa_wrap<typename std::remove_pointer<Target>::type, From>::doit(Val);
+  return isa_wrap<typename std::remove_pointer<Target>::type, From>::call(Val);
 }
 
 ///
@@ -97,35 +105,35 @@ inline bool isa(const From &Val) {
 ///
 template <class To, class From>
 struct cast_type_deduction {
-  typedef To &ret_type;
+  typedef To &return_type;
 };
 template <class To, class From>
 struct cast_type_deduction<To, const From> {
-  typedef const To &ret_type;
+  typedef const To &return_type;
 };
 
 template <class To, class From>
 struct cast_type_deduction<To, From *> {
-  typedef To *ret_type;
+  typedef To *return_type;
 };
 
 template <class To, class From>
 struct cast_type_deduction<To, const From *> {
-  typedef const To *ret_type;
+  typedef const To *return_type;
 };
 
 template <class To, class From>
 struct cast_type_deduction<To, const From *const> {
-  typedef const To *ret_type;
+  typedef const To *return_type;
 };
 
 template <class To, class From>
 struct cast_value {
-  static typename cast_type_deduction<To, From>::ret_type doit(
+  static typename cast_type_deduction<To, From>::return_type call(
       const From &Val) {
-    typename cast_type_deduction<To, From>::ret_type Res2 =
-        (typename cast_type_deduction<To, From>::ret_type) const_cast<From &>(
-            Val);
+    typename cast_type_deduction<To, From>::return_type Res2 =
+        (typename cast_type_deduction<To, From>::return_type) const_cast<
+            From &>(Val);
     return Res2;
   }
 };
@@ -134,31 +142,32 @@ struct cast_value {
 /// \brief dyn_cast From to To.
 ///
 template <class To, class From>
-inline typename cast_type_deduction<To, From>::ret_type dyn_cast_impl(
+inline typename cast_type_deduction<To, From>::return_type dyn_cast_impl(
     From &Val) {  // NOLINT
   if (!isa<To>(Val)) {
     throw("dyn_cast_impl<To>() argument of incompatible type!");
   }
-  return cast_value<To, From>::doit(Val);
+  return cast_value<To, From>::call(Val);
 }
 
 template <class To, class From>
-inline typename cast_type_deduction<To, From *>::ret_type dyn_cast_impl(
+inline typename cast_type_deduction<To, From *>::return_type dyn_cast_impl(
     From *Val) {
   if (!isa<To>(Val)) {
     throw("dyn_cast_impl<To>() argument of incompatible type!");
   }
-  return cast_value<To, From *>::doit(Val);
+  return cast_value<To, From *>::call(Val);
 }
 
 template <class To, class From>
-inline typename cast_type_deduction<To, From>::ret_type dyn_cast(
+inline typename cast_type_deduction<To, From>::return_type dyn_cast(
     From &Val) {  // NOLINT
   return dyn_cast_impl<To>(Val);
 }
 
 template <class To, class From>
-inline typename cast_type_deduction<To, From *>::ret_type dyn_cast(From *Val) {
+inline typename cast_type_deduction<To, From *>::return_type dyn_cast(
+    From *Val) {
   return dyn_cast_impl<To>(Val);
 }
 
