769e24ceb6709d938af0c2ee4773f42188684ffa
Meteor Liu <liudongxue01@baidu.com>
2023-03-06 11:27:40 +0800

    implement floor_grad by primitive logic (#51059)
    * implement floor_grad by primitive logic
    
    * implement floor_grad by primitive logic
    
    * Merge branch 'develop' into floor_grad

--- python/paddle/fluid/tests/unittests/test_activation_op.py
+++ python/paddle/fluid/tests/unittests/test_activation_op.py
@@ -1434,6 +1434,49 @@ class TestFloor_ZeroDim(TestFloor):
         self.shape = []
 
 
+class TestFloorPrim(TestActivation):
+    def setUp(self):
+        self.op_type = "floor"
+        self.prim_op_type = "prim"
+
+        # the gradient on floor, ceil, round is undefined.
+        # we return zero as gradient, but the numpy return nan.
+        # for prim, we compare result with eager python api,
+        # so, we use only_prim flag to express we only test prim.
+        self.only_prim = True
+        self.check_eager = True
+        self.python_api = paddle.floor
+        self.init_dtype()
+        self.init_shape()
+
+        if len(self.shape) == 0:
+            # for 0-D tensor, skip cinn testing
+            self.enable_cinn = False
+
+        np.random.seed(1024)
+        x = np.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        out = np.floor(x)
+
+        self.inputs = {'X': OpTest.np_dtype_to_fluid_dtype(x)}
+        self.outputs = {'Out': out}
+
+    def init_shape(self):
+        self.shape = [10, 12]
+
+    def test_check_grad(self):
+        self.check_grad(['X'], 'Out', check_prim=True)
+
+
+class TestFloorPrim_ZeroDim(TestFloorPrim):
+    def init_shape(self):
+        self.shape = []
+
+
+class TestFloorPrimFp16(TestFloorPrim):
+    def init_dtype(self):
+        self.dtype = np.float16
+
+
 class TestCos(TestActivation):
     def setUp(self):
         self.op_type = "cos"
