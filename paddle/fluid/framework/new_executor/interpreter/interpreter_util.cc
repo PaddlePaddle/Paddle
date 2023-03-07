be8a579cf7c416e4001f60e4e0467ee52a9966fa
Liyulingyue <852433440@qq.com>
2023-03-07 20:56:49 +0800

    add REGISTER of real

--- paddle/fluid/framework/new_executor/interpreter/interpreter_util.cc
+++ paddle/fluid/framework/new_executor/interpreter/interpreter_util.cc
@@ -60,7 +60,6 @@ static std::set<std::string> OpsNeedSetOutputDtypeWhenRegisterPhiKernel = {
     "arg_sort",
     "argmax",
     "argmin",
-    "as_real",
     "atan2",
     "auc",
     "bincount",
