fix CI check: kl test failed

python/paddle/fluid/tests/unittests/distribution/test_distribution_geometric_static.py
@@ -304,12 +304,12 @@ class TestGeometricKL(unittest.TestCase):
             np.testing.assert_allclose(
                 kl_diver,
                 self._kl(),
-                rtol=0.3,
+                rtol=RTOL.get(str(self.probs1.dtype)),
                 atol=ATOL.get(str(self.probs1.dtype)),
             )
 
     def _kl(self):
-        temp = np.log(self.probs1, self.probs2)
+        temp = np.log(self.probs1 / self.probs2)
         kl_diff = self.probs1 * np.abs(temp)
 
         return np.sum(kl_diff, axis=-1)