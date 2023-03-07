cd428798f72f55828c4d6673b15871556ecf412c
enkilee <luhputu0815@gmail.com>
2023-03-07 01:56:47 +0000

    fix

--- paddle/phi/kernels/gpu/kthvalue_kernel.cu
+++ paddle/phi/kernels/gpu/kthvalue_kernel.cu
@@ -264,5 +264,4 @@ PD_REGISTER_KERNEL(kthvalue,
                    float,
                    double,
                    int,
-                   int64_t,
-                   phi::dtype::bfloat16) {}
+                   int64_t) {}
