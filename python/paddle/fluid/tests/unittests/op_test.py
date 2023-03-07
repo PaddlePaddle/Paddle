5f1524ee0b62dc7b388821c6d6f67aaf69cc1723
Weilong Wu <veyron_wu@163.com>
2023-03-07 10:00:03 +0000

    fix merge conflict

--- python/paddle/fluid/tests/unittests/op_test.py
+++ python/paddle/fluid/tests/unittests/op_test.py
@@ -67,17 +67,14 @@ g_disable_legacy_dygraph = (
 def check_out_dtype(api_fn, in_specs, expect_dtypes, target_index=0, **configs):
     """
     Determines whether dtype of output tensor is as expected.
-
     Args:
         api_fn(callable):  paddle api function
         in_specs(list[tuple]): list of shape and dtype information for constructing input tensor of api_fn, such as [(shape, dtype), (shape, dtype)].
         expected_dtype(list[str]): expected dtype of output tensor.
         target_index(int): indicate which one from in_specs to infer the dtype of output.
         config(dict): other arguments of paddle api function
-
     Example:
         check_out_dtype(fluid.layers.pad_constant_like, [([2,3,2,3], 'float64'), ([1, 3, 1,3], )], ['float32', 'float64', 'int64'], target_index=1, pad_value=0.)
-
     """
     paddle.enable_static()
     for i, expect_dtype in enumerate(expect_dtypes):
@@ -259,14 +256,11 @@ def get_numeric_gradient(
 
 def skip_check_grad_ci(reason=None):
     """Decorator to skip check_grad CI.
-
     Check_grad is required for Op test cases. However, there are some special
     cases that do not need to do check_grad. This decorator is used to skip the
     check_grad of the above cases.
-
     Note: the execution of unit test will not be skipped. It just avoids check_grad
     checking in tearDownClass method by setting a `no_need_check_grad` flag.
-
     Example:
         @skip_check_grad_ci(reason="For inference, check_grad is not required.")
         class TestInference(OpTest):
@@ -1093,14 +1087,12 @@ class OpTest(unittest.TestCase):
         self, place, fetch_list, expect_outs, actual_outs, inplace_atol=None
     ):
         """Compare expect outs and actual outs of an tested op.
-
         Args:
             place (CPUPlace | CUDAPlace): The place where the op runs.
             fetch_list (list): The outputs of tested op.
             expect_outs (list): The expect outs of tested op.
             actual_outs (list): The actual outs of tested op.
             inplace_atol (float): The tolerable error, only set when tested op doesn't ensure computational consistency, like group_norm op.
-
         Returns:
             None.
         """
@@ -1154,12 +1146,10 @@ class OpTest(unittest.TestCase):
         self, fwd_program, grad_op_desc, op_grad_to_var
     ):
         """Generate grad_program which contains the grad_op.
-
         Args:
             fwd_program (tuple): The program that contains grad_op_desc's corresponding forward op.
             grad_op_desc (OpDesc): The OpDesc of grad op.
             op_grad_to_var (dict): The relation of variables in grad op and its forward op.
-
         Returns:
             grad_program (program): The program which contains the grad_op.
         """
@@ -1201,17 +1191,14 @@ class OpTest(unittest.TestCase):
         self, place, fwd_res, grad_op_desc, op_grad_to_var
     ):
         """Generate grad_feed_map for grad_program.
-
         since we don`t really check gradient accuracy, but check the consistency when using and not using inplace,
         we use fwd outs (also inputs sometimes) to construct grad inputs.
-
         Args:
             place (CPUPlace | CUDAPlace): The place where the op runs.
             fwd_res (tuple): The outputs of its forward op, in the same form as returns of _calc_outputs() when for_inplace_test is True.
                 i.e., tuple(fwd_outs, fwd_fetch_list, fwd_feed_map, fwd_program, fwd_op_desc)
             grad_op_desc (OpDesc): The OpDesc of grad op.
             op_grad_to_var (dict): The relation of variables in grad op and its fwd_op.
-
         Returns:
             grad_feed_map (dict): The feed_map of grad_op.
         """
@@ -1249,12 +1236,10 @@ class OpTest(unittest.TestCase):
         An op needs to run druing inplace check if,
         (1) it has infer_inplace,
         (2) it has infer_inplace in its grad descendants. (since we need its outputs as to construct its grad's inputs)
-
         Args:
             op_desc (OpDesc): The op_desc of current op.
             fwd_op_desc (OpDesc): The op_desc of current op's forward op, None if current op has no forward op.
                 Eg. relu's fwd_op is None, relu_grad's fwd_op is relu, relu_grad_grad's fwd_op is relu_grad, etc.
-
         Returns:
             need_run_ops (list[(op_desc, fwd_op_desc)]): The ops that need to run during inplace test.
         """
@@ -1296,12 +1281,10 @@ class OpTest(unittest.TestCase):
     ):
         """Check the inplace correctness of given op (self.op_type).
         Run the op twice with same inputs, one enable inplace and another disable, compare their outputs.
-
         Args:
             place (CPUPlace | CUDAPlace): The place where the op runs.
             no_check_set (list): The names of outputs that needn't check, like XShape of reshape op.
             inplace_atol (float): The tolerable error, only set when op doesn't ensure computational consistency, like group_norm op.
-
         Returns:
             expect_res (tuple(outs, fetch_list, feed_map, program, op_desc)): The results of given op.
                 We return this to construct grad_program and grad_feed_map for grad inplace check.
@@ -1333,17 +1316,14 @@ class OpTest(unittest.TestCase):
         self, place, fwd_res, grad_op_desc, enable_inplace=None
     ):
         """Calculate grad_output for given grad_op_desc.
-
         since we don`t really check gradient accuracy, but check the consistency when using and not using inplace,
         we use fwd outs (also inputs sometimes) to construct grad inputs.
-
         Args:
             place (CPUPlace | CUDAPlace): The place where the op runs.
             fwd_res (tuple): The outputs of its forward op, in the same form as returns of _calc_outputs() when for_inplace_test is True.
                 i.e., tuple(fwd_outs, fwd_fetch_list, fwd_feed_map, fwd_program, fwd_op_desc).
             grad_op_desc (OpDesc): The OpDesc of grad op.
             enable_inplace (bool): Enable inplace or not.
-
         Returns:
             res (tuple(outs, fetch_list, feed_map, program, op_desc)): The results of given grad_op_desc.
         """
@@ -1389,18 +1369,15 @@ class OpTest(unittest.TestCase):
         self, place, fwd_res, grad_op_desc, inplace_atol=None
     ):
         """Check the inplace correctness of given grad_op_desc.
-
         Run the grad op twice with same inputs, one enable inplace and another disable, compare their outputs.
         It works like _check_forward_inplace, but the way to construct program and feed_map differs.
         So we define a new function for grad, grad_grad, etc.
-
         Args:
             place (CPUPlace | CUDAPlace): The place where the op runs.
             fwd_res (tuple): The outputs of its forward op, in the same form as returns of _calc_outputs() when for_inplace_test is True.
                 i.e., tuple(fwd_outs, fwd_fetch_list, fwd_feed_map, fwd_program, fwd_op_desc).
             grad_op_desc (OpDesc): The OpDesc of grad op.
             inplace_atol (float): The tolerable error, only set when op doesn't ensure computational consistency, like group_norm op.
-
         Returns:
             expect_res (tuple(outs, fetch_list, feed_map, program, op_desc)): The results of given op.
                 We return this to construct grad_program and grad_feed_map for grad inplace check.
@@ -1425,15 +1402,12 @@ class OpTest(unittest.TestCase):
         self, place, no_check_set=None, inplace_atol=None
     ):
         """Chech the inplace correctness of given op, its grad op, its grad_grad op, etc.
-
         (1) Get all ops need to run. (see conditions in _get_need_run_ops())
         (2) Run op in need_run_ops, and do inplace check if it has infer_inplace.
-
         Args:
             place (CPUPlace | CUDAPlace): The place where the op runs.
             no_check_set (list): The names of outputs that needn't check, like XShape of reshape op.
             inplace_atol (float): The tolerable error, only set when op doesn't ensure computational consistency, like group_norm op.
-
         Returns:
             None
         """
@@ -1666,7 +1640,6 @@ class OpTest(unittest.TestCase):
             def check(self):
                 """
                 return None means ok, raise Error means failed.
-
                 the main enter point of Checker class
                 """
                 self.init()
@@ -2062,6 +2035,7 @@ class OpTest(unittest.TestCase):
         check_eager=False,
         check_prim=False,
     ):
+
         # disable legacy dygraph check when check_eager is True
         if check_eager:
             check_dygraph = False
@@ -2086,8 +2060,6 @@ class OpTest(unittest.TestCase):
                 check_eager=check_eager,
                 check_prim=check_prim,
             )
-            if hasattr(self, 'only_prim') and self.only_prim:
-                continue
             if check_eager:
                 assert not check_dygraph
                 outs, eager_dygraph_outs, fetch_list = res
@@ -2211,6 +2183,7 @@ class OpTest(unittest.TestCase):
         check_dygraph=True,
         check_eager=False,
         check_prim=False,
+        only_check_prim=False,
     ):
         # disable legacy dygraph check when check_eager is True
         if check_eager:
@@ -2232,6 +2205,7 @@ class OpTest(unittest.TestCase):
                 check_dygraph,
                 check_eager=check_eager,
                 check_prim=check_prim,
+                only_check_prim=only_check_prim,
             )
 
     def check_grad_with_place(
@@ -2249,6 +2223,7 @@ class OpTest(unittest.TestCase):
         numeric_place=None,
         check_eager=False,
         check_prim=False,
+        only_check_prim=False,
     ):
         core._set_prim_all_enabled(False)
         if check_prim:
@@ -2264,8 +2239,7 @@ class OpTest(unittest.TestCase):
             # Support operators which not in the NO_FP64_CHECK_GRAD_OP_LIST list can be test prim with fp32
             setattr(self.__class__, 'check_prim', True)
             self._check_grad_helper()
-            if prim_grad_checker.is_only_check_prim():
-                self.only_prim = True
+            if only_check_prim:
                 return
         # disable legacy dygraph check when check_eager is True
         if check_eager:
