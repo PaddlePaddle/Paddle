73cc506cd1ac8488090ae8dfe1627c9f8214d4e0
Weilong Wu <veyron_wu@163.com>
2023-03-07 06:07:38 +0000

    polish input for check_output_with_place

--- python/paddle/fluid/tests/unittests/test_space_to_depth_op.py
+++ python/paddle/fluid/tests/unittests/test_space_to_depth_op.py
@@ -79,7 +79,9 @@ class TestSpaceToDepthOp(OpTest):
             if fluid.core.is_compiled_with_cuda()
             else fluid.core.CPUPlace()
         )
-        self.check_output_with_place(place, 1e-5, None, False)
+        self.check_output_with_place(
+            place=place, atol=1e-5, no_check_set=None, equal_nan=False
+        )
 
     def test_check_grad(self):
         place = (
