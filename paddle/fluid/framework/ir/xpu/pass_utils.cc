75733cbc18dfd2d6589fbdc47408c888ef8704df
zhupengyang <zhu_py@qq.com>
2023-03-08 02:58:41 +0000

    fix

--- paddle/fluid/framework/ir/xpu/pass_utils.cc
+++ paddle/fluid/framework/ir/xpu/pass_utils.cc
@@ -105,6 +105,31 @@ size_t HashTensor(const phi::DenseTensor& in) {
 template size_t HashTensor<int16_t>(const phi::DenseTensor& in);
 template size_t HashTensor<float>(const phi::DenseTensor& in);
 
+std::string GetPrefixWithoutHash(const std::string& name,
+                                 const phi::DenseTensor& tensor) {
+  size_t hash = 0;
+  switch (tensor.dtype()) {
+    case phi::DataType::FLOAT32:
+      hash = HashTensor<float>(tensor);
+      break;
+    case phi::DataType::INT16:
+      hash = HashTensor<int16_t>(tensor);
+      break;
+    case phi::DataType::FLOAT16:
+      // 1. std::hash can not be applied on paddle::experimental::float16
+      // 2. We only compute hash on transed_weight(int16)/transed_bias(float32).
+      // Float16 tensors are only in origin models.
+      return name;
+    default:
+      PADDLE_THROW(platform::errors::InvalidArgument(
+          "Not support hash tensor with type %s.",
+          phi::DataTypeToString(tensor.dtype())));
+      break;
+  }
+  std::size_t found = name.find(std::to_string(hash));
+  return found == std::string::npos ? name : name.substr(found);
+}
+
 template <typename T>
 void PrepareWeight(Graph* graph,
                    Scope* scope,
@@ -122,8 +147,9 @@ void PrepareWeight(Graph* graph,
 
   size_t dst_hash = HashTensor<T>(dst_tensor);
   size_t dst_max_hash = HashTensor<float>(dst_max_tensor);
-  std::string dst_name = src_name + "_" + std::to_string(dst_hash);
-  std::string dst_max_name = src_name + "_max_" + std::to_string(dst_max_hash);
+  std::string pre_name = GetPrefixWithoutHash(src_name, *src_tensor);
+  std::string dst_name = pre_name + "_" + std::to_string(dst_hash);
+  std::string dst_max_name = pre_name + "_max_" + std::to_string(dst_max_hash);
   *dst = FindNodeWithName(graph, dst_name);
   if (*dst == nullptr) {
     // Create dst node
@@ -199,7 +225,8 @@ void PrepareBias(
   phi::DenseTensor dst_tensor;
   CastToFp32(src_tensor, &dst_tensor);
   size_t dst_hash = HashTensor<float>(dst_tensor);
-  std::string dst_name = src_name + "_" + std::to_string(dst_hash);
+  std::string pre_name = GetPrefixWithoutHash(src_name, *src_tensor);
+  std::string dst_name = pre_name + "_" + std::to_string(dst_hash);
   *dst = FindNodeWithName(graph, dst_name);
   if (*dst == nullptr) {
     // Create dst node
