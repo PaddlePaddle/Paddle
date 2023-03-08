c35f94607d27a643ef6b367afaf95716b915f279
Liyulingyue <852433440@qq.com>
2023-03-08 19:23:00 +0800

    fix something

--- paddle/phi/kernels/cpu/accuracy_kernel.cc
+++ paddle/phi/kernels/cpu/accuracy_kernel.cc
@@ -94,7 +94,9 @@ void AccuracyRawKernel(const Context& dev_ctx,
 // TODO(add supported dtype.)
 PD_REGISTER_KERNEL(
     accuracy, CPU, ALL_LAYOUT, phi::AccuracyRawKernel, float, double) {
-  kernel->InputAt(0).SetDataType(phi::DataType::FLOAT32);
   kernel->InputAt(1).SetDataType(phi::DataType::INT64);
   kernel->InputAt(2).SetDataType(phi::DataType::INT64);
+  kernel->OutputAt(0).SetDataType(phi::DataType::FLOAT32);
+  kernel->OutputAt(1).SetDataType(phi::DataType::INT64);
+  kernel->OutputAt(2).SetDataType(phi::DataType::INT64);
 }
