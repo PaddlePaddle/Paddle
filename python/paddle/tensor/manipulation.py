e0335cc5fa53248171b498afa97673791f808355
yangjianfengo1 <yangjianfeng01@baidu.com>
2023-03-08 11:42:57 +0000

    update review

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -3150,7 +3150,7 @@ def tile(x, repeat_times, name=None):
             [
                 'bool',
                 'float16',
-                'bfloat16',
+                'uint16',
                 'float32',
                 'float64',
                 'int32',
