b6656c7e184f637be6bb3f541b239fea3b104860
andsonder <changlu@keter.top>
2023-03-09 16:10:21 +0800

    更新单测文件

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_temporal_shift.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_temporal_shift.py
@@ -30,7 +30,7 @@ class TrtConvertTemporalShiftTest(TrtLayerAutoScanTest):
     def sample_program_configs(self):
         def generate_input1(attrs):
             T = attrs[0]["seg_num"]
-            return np.ones([3 * T, 10, 64, 64]).astype(np.float32)
+            return np.random.rand(3 * T, 10, 64, 64).astype(np.float32)
 
         for shift_value in [0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.49]:
             for T in range(2, 5):
@@ -68,7 +68,7 @@ class TrtConvertTemporalShiftTest(TrtLayerAutoScanTest):
                     yield program_config
 
     def sample_predictor_configs(
-        self, program_config
+            self, program_config
     ) -> (paddle_infer.Config, List[int], float):
         def generate_dynamic_shape(attrs):
             self.dynamic_shape.min_input_shape = {"input_data": [6, 10, 64, 64]}
@@ -82,6 +82,9 @@ class TrtConvertTemporalShiftTest(TrtLayerAutoScanTest):
             self.dynamic_shape.min_input_shape = {}
             self.dynamic_shape.opt_input_shape = {}
 
+        def generate_trt_nodes_num(attrs, is_dynamic_shape):
+            return 0, 3
+
         attrs = [
             program_config.ops[i].attrs for i in range(len(program_config.ops))
         ]
@@ -89,16 +92,16 @@ class TrtConvertTemporalShiftTest(TrtLayerAutoScanTest):
         # for static_shape
         clear_dynamic_shape()
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        yield self.create_inference_config(), (0, 3), 1e-5
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs, False), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
-        yield self.create_inference_config(), (0, 3), 1e-3
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs, False), 1e-3
 
         # for dynamic_shape
         generate_dynamic_shape(attrs)
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        yield self.create_inference_config(), (0, 3), 1e-5
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs, True), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
-        yield self.create_inference_config(), (0, 3), 1e-3
+        yield self.create_inference_config(), generate_trt_nodes_num(attrs, True), 1e-3
 
     def test(self):
         self.run_test()
