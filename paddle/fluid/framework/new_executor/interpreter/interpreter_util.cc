39ceb794ae03b0aae111cfa70805813318e10eca
iLeGend <824040212@qq.com>
2023-03-09 13:38:38 +0800

    remove fused_matmul from list

--- paddle/fluid/framework/new_executor/interpreter/interpreter_util.cc
+++ paddle/fluid/framework/new_executor/interpreter/interpreter_util.cc
@@ -69,7 +69,6 @@ static std::set<std::string> OpsNeedSetOutputDtypeWhenRegisterPhiKernel = {
     "eigh",
     "ftt_c2r",
     "ftt_r2c",
-    "fused_matmul",
     "generate_proposals",
     "graph_sample_neighbors",
     "group_norm",
