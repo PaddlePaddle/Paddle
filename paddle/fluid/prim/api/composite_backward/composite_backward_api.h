a99eb4095dd328bb75ca3974ac8b6f7f94c1f286
SylarTiaNII <15840554235@163.com>
2023-03-09 18:04:17 +0800

    add assign composite backward op

--- paddle/fluid/prim/api/composite_backward/composite_backward_api.h
+++ paddle/fluid/prim/api/composite_backward/composite_backward_api.h
@@ -881,5 +881,13 @@ void gather_nd_grad(const Tensor& x,
   }
 }
 
+template <typename T>
+void assign_grad(const Tensor& out_grad,
+                 Tensor* x_grad) {
+  if (x_grad) {
+    set_output<T>(out_grad, x_grad);
+  }
+}
+
 }  // namespace prim
 }  // namespace paddle
