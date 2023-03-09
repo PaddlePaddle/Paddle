c191b707e8554a44bc923637da13e69040b49434
yu wentao <yuwentao126@126.com>
2023-03-09 10:46:11 +0800

    Add comm context manager, add phi broadcast op (#51072)
    * * add comm context for device context
    
    * add broadcast phi operator kernel and api
    
    * add broadcast support dtype, update ut
    
    * fix broadcast bfloat16 type
    
    * fix ut
    
    * update test_collective_broadcast_api timeout to 300

--- paddle/phi/infermeta/unary.cc
+++ paddle/phi/infermeta/unary.cc
@@ -356,6 +356,11 @@ void BatchSizeLikeInferMeta(const MetaTensor& x,
   out->set_dims(output_dim);
 }
 
+void BroadcastBaseInferMeta(const MetaTensor& x, MetaTensor* out) {
+  out->set_dtype(x.dtype());
+  out->set_dims(x.dims());
+}
+
 void CastInferMeta(const MetaTensor& x, DataType out_dtype, MetaTensor* out) {
   out->set_dims(x.dims());
   out->set_dtype(out_dtype);
