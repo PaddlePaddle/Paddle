c900583955bf1b1dc4051c8ee486bbfa18a95f56
sanbuphy <physicoada@gmail.com>
2023-03-09 15:23:06 +0800

    Add output defs for all_close all_raw  kernel

--- paddle/fluid/framework/new_executor/interpreter/interpreter_util.cc
+++ paddle/fluid/framework/new_executor/interpreter/interpreter_util.cc
@@ -53,8 +53,6 @@ static std::set<std::string> OpsNeedSetOutputDtypeWhenRegisterPhiKernel = {
     "abs",
     "adam",
     "adamw",
-    "all_close",
-    "all_raw",
     "angle",
     "any_raw",
     "arg_sort",
