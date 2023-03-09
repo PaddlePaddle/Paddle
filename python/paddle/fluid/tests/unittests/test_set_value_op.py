d73888dd1b429aeb65d2d3948d50ac5eb2e26c79
enkilee <luhputu0815@gmail.com>
2023-03-09 02:28:51 +0000

    fix

--- python/paddle/fluid/tests/unittests/test_set_value_op.py
+++ python/paddle/fluid/tests/unittests/test_set_value_op.py
@@ -18,9 +18,10 @@ import unittest
 from functools import reduce
 
 import numpy as np
-from op_test import convert_float_to_uint16
+from op_test import OpTest, convert_float_to_uint16
 
 import paddle
+import paddle.fluid.core as core
 from paddle.fluid.layer_helper import LayerHelper
 
 
@@ -1520,27 +1521,30 @@ class TestSetValueInplaceLeafVar(unittest.TestCase):
         paddle.enable_static()
 
 
-class TestSetValueOpBF16(unittest.TestCase):
-    def testsetvaluebf16(OpTest):
-        def setUp(self):
-            paddle.enable_static()
-            self.dtype = np.uint16
-            self.shape = [2, 3, 4]
-            self.data = np.ones(self.shape).astype(self.dtype)
-            self.program = paddle.static.Program()
-            x = np.random.rand([6]).astype('float32')
-            self.data[0, 0] = np.random.rand([6]).astype('float32')
-            out = self.data[0, 0]
-            self.inputs = {
-                'X': convert_float_to_uint16(x),
-            }
-            self.outputs = {'Out': convert_float_to_uint16(out)}
+@unittest.skipIf(
+    not core.is_compiled_with_cuda()
+    or not core.is_bfloat16_supported(core.CUDAPlace(0)),
+    "core is not complied with CUDA and not support the bfloat16",
+)
+class TestSetValueBFloat16(OpTest):
+    def setUp(self):
+        self.dtype = np.uint16
+        self.shape = [2, 3, 4]
+        self.__class__.op_type = self.op_type
+        self.data = np.ones(self.shape).astype(self.dtype)
+        x = np.random.rand([6]).astype('float32')
+        self.data[0, 0] = np.random.rand([6]).astype('float32')
+        out = self.data[0, 0]
+        self.inputs = {'X': convert_float_to_uint16(x)}
+        self.outputs = {'Out': convert_float_to_uint16(out)}
 
-        def test_check_output(self):
-            self.check_output(atol=1e-3)
+    def test_check_output(self):
+        place = core.CUDAPlace(0)
+        self.check_output_with_place(place)
 
-        def test_check_grad(self):
-            self.check_grad(['X'], 'Out', max_relative_error=1e-3)
+    def test_check_grad(self):
+        place = core.CUDAPlace(0)
+        self.check_grad_with_place(place, ['X'], 'Out')
 
 
 if __name__ == '__main__':
