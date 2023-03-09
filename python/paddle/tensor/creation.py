f3448977f30be15ac75f38f27bed426efca1923d
yangjianfengo1 <125249383+yangjianfengo1@users.noreply.github.com>
2023-03-09 20:33:12 +0800

    [AMP OP&Test] arange op support fp16/bf16 (#51106)
    * AMP arange & Test
    
    * fix arange bfloat16 dtype
    
    * update for review
    
    * update for review2
    
    * fix tile
    
    * update
    
    * fix ci
    
    * r
    
    * f
    
    * fix windows ci
    
    * update bfloat data
    
    * fix bloat16 input
    
    * add print
    
    * Update test_where_op.py
    
    * update kernel
    
    * del repeat
    
    * update review

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -1233,7 +1233,7 @@ def arange(start=0, end=None, step=1, dtype=None, name=None):
         check_dtype(
             dtype,
             'dtype',
-            ['float32', 'float64', 'int32', 'int64'],
+            ['float32', 'float64', 'int32', 'int64', 'float16', 'uint16'],
             'range/arange',
         )
         helper = LayerHelper('range', **locals())
