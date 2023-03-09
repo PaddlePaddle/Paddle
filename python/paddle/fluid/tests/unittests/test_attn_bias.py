59af096f4b2fa00ec522150a14f79c2af4ff7e08
sneaxiy <sneaxiy@126.com>
2023-03-09 18:54:24 +0800

    fix ci error

--- python/paddle/fluid/tests/unittests/test_attn_bias.py
+++ python/paddle/fluid/tests/unittests/test_attn_bias.py
@@ -29,7 +29,7 @@ from paddle.incubate.nn.attn_bias import (
 
 
 def all_dtypes():
-    dtypes = [paddle.float16, paddle.float32, paddle.float64]
+    dtypes = [paddle.float32, paddle.float64]
     if paddle.is_compiled_with_cuda() and not paddle.is_compiled_with_rocm():
         dtypes.append(paddle.float16)
         prop = paddle.device.cuda.get_device_properties()
