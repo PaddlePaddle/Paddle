3f4917f69ad4ad91afa7d8451992e305f78dd2b5
iLeGend <824040212@qq.com>
2023-03-10 10:38:59 +0800

    【Hackathon No.67】remove operator.h in blas.h (#50989)
    * remove operator.h from blas.h and remove paddle::framework::ExecutionContext
    
    * remove the deps for GetBlas(exe_ctx)
    
    * fix error

--- paddle/fluid/operators/fused/fusion_seqexpand_concat_fc_op.cc
+++ paddle/fluid/operators/fused/fusion_seqexpand_concat_fc_op.cc
@@ -239,9 +239,9 @@ class FusionSeqExpandConcatFCOpKernel : public framework::OpKernel<T> {
     T* out_data = out->mutable_data<T>(ctx.GetPlace());
     T* fc_out_data = fc_out->mutable_data<T>(ctx.GetPlace());
 
-    auto blas = phi::funcs::GetBlas<DeviceContext, T>(ctx);
-
     auto& dev_ctx = ctx.template device_context<phi::CPUContext>();
+    auto blas = phi::funcs::GetBlas<DeviceContext, T>(dev_ctx);
+
     phi::funcs::FCFunctor<DeviceContext, T> fc;
     fc(dev_ctx,
        total_T,
