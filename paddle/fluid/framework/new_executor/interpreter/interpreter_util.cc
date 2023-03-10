776a26fe5d67a8ddf50c01f2cffe5fbf8e7f6317
hellolllw <127483090+hellolllw@users.noreply.github.com>
2023-03-10 16:24:46 +0800

    Update interpreter_util.cc

--- paddle/fluid/framework/new_executor/interpreter/interpreter_util.cc
+++ paddle/fluid/framework/new_executor/interpreter/interpreter_util.cc
@@ -75,7 +75,6 @@ static std::set<std::string> OpsNeedSetOutputDtypeWhenRegisterPhiKernel = {
     "group_norm",
     "histogram",
     "instance_norm",
-    "is_empty",
     "kthvalue",
     "lamb",
     "layer_norm",
