715f2b31e980efee1221dc2d31bb38d2aa9195cc
heyanru01 <429520051@qq.com>
2023-03-11 04:53:03 +0000

    fix

--- python/paddle/incubate/autograd/composite_rules.py
+++ python/paddle/incubate/autograd/composite_rules.py
@@ -180,8 +180,8 @@ def instancenorm_composite(x, scale, bias, epsilon):
         bias_tile = reshape(bias, [1, c, 1, 1])
         out = out + bias_tile
 
-    mean_ = reshape(mean_, x.shape[:1])
-    variance = reshape(variance, x.shape[:1])
+    mean_ = reshape(mean_, x.shape[:2])
+    variance = reshape(variance, x.shape[:2])
     if len(x_shape) == 2:
         out = reshape(out, x_shape)
     return out, mean_, variance
