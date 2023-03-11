31a500dcd4c687e81909bfae5491cc237fc6ccae
zhoutianzi666 <17801055074@163.com>
2023-03-11 10:39:15 +0000

    use const & && use python script to generate conv2d/group=1 code

--- paddle/phi/kernels/fusion/cutlass/conv2d/conv2d_decl.h
+++ paddle/phi/kernels/fusion/cutlass/conv2d/conv2d_decl.h
@@ -53,11 +53,11 @@ typedef struct {
 } ConvAllParams;
 
 // Below functions are provided by cutlass, they are called by phi.
-void Conv2dBiasAddRelu(const ConvAllParams params);
-void Conv2dBiasRelu(const ConvAllParams params);
-void Conv2dBiasLeakyRelu(const ConvAllParams params);
-void Conv2dBiasSilu(const ConvAllParams params);
-void Conv2dBias(const ConvAllParams params);
+void Conv2dBiasAddRelu(const ConvAllParams &params);
+void Conv2dBiasRelu(const ConvAllParams &params);
+void Conv2dBiasLeakyRelu(const ConvAllParams &params);
+void Conv2dBiasSilu(const ConvAllParams &params);
+void Conv2dBias(const ConvAllParams &params);
 }  // namespace cutlass_internal
 }  // namespace fusion
 }  // namespace phi
