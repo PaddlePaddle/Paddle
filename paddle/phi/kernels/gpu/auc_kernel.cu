3ad7d68b09c182e1f0b83be1bf7f3fd46f7805c7
Little-chick <74541422+Little-chick@users.noreply.github.com>
2023-03-12 21:57:21 +0800

    Update auc_kernel.cu

--- paddle/phi/kernels/gpu/auc_kernel.cu
+++ paddle/phi/kernels/gpu/auc_kernel.cu
@@ -274,7 +274,7 @@ void AucKernel(const Context &dev_ctx,
 }  // namespace phi
 
 PD_REGISTER_KERNEL(auc, GPU, ALL_LAYOUT, phi::AucKernel, float) {
-  kernel->OutputAt(0).SetDataType(phi::DataType::DOUBLE);
+  kernel->OutputAt(0).SetDataType(phi::DataType::FLOAT64);
   kernel->OutputAt(1).SetDataType(phi::DataType::INT64);
   kernel->OutputAt(2).SetDataType(phi::DataType::INT64);
 }
