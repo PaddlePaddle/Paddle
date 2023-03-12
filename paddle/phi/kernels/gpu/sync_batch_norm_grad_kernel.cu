d58d1c1523da58a2b97d461c733f94d4c4dd3855
404988613 <86900071+404988613@users.noreply.github.com>
2023-03-12 20:28:55 +0800

    Update sync_batch_norm_grad_kernel.cu

--- paddle/phi/kernels/gpu/sync_batch_norm_grad_kernel.cu
+++ paddle/phi/kernels/gpu/sync_batch_norm_grad_kernel.cu
@@ -61,8 +61,7 @@ PD_REGISTER_KERNEL(sync_batch_norm_grad,
                    phi::SyncBatchNormGradKernel,
                    float,
                    phi::dtype::float16) {
-  kernel->OutputAt(1).SetDataType(phi::DataType::UNDEFINED);
-  kernel->OutputAt(2).SetDataType(phi::DataType::UNDEFINED);
+  kernel->OutputAt(0).SetDataType(phi::DataType::UNDEFINED);
 }
 #else
 PD_REGISTER_KERNEL(sync_batch_norm_grad,
@@ -72,7 +71,6 @@ PD_REGISTER_KERNEL(sync_batch_norm_grad,
                    float,
                    double,
                    phi::dtype::float16) {
-  kernel->OutputAt(1).SetDataType(phi::DataType::UNDEFINED);
-  kernel->OutputAt(2).SetDataType(phi::DataType::UNDEFINED);
+  kernel->OutputAt(0).SetDataType(phi::DataType::UNDEFINED);
 }
 #endif
