7b9ae3fa716c64fbd39a5d6d207ed091df486463
andsonder <changlu@keter.top>
2023-03-13 08:37:05 +0800

    delete compile version judge in test file

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_temporal_shift.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_temporal_shift.py
@@ -25,22 +25,6 @@ import paddle.inference as paddle_infer
 
 class TrtConvertTemporalShiftTest(TrtLayerAutoScanTest):
     def is_program_valid(self, program_config: ProgramConfig) -> bool:
-        compile_version = paddle_infer.get_trt_compile_version()
-        runtime_version = paddle_infer.get_trt_runtime_version()
-        if (
-            compile_version[0] * 1000
-            + compile_version[1] * 100
-            + compile_version[2] * 10
-            < 8200
-        ):
-            return False
-        if (
-            runtime_version[0] * 1000
-            + runtime_version[1] * 100
-            + runtime_version[2] * 10
-            < 8200
-        ):
-            return False
         return True
 
     def sample_program_configs(self):
