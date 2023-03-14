8ff8ad6a60a36241f8e7b0f37ba9bc1d3f1ce2c2
SylarTiaNII <15840554235@163.com>
2023-03-14 17:53:51 +0800

    replace assign with by_pass

--- paddle/fluid/prim/api/composite_backward/composite_backward_api.h
+++ paddle/fluid/prim/api/composite_backward/composite_backward_api.h
@@ -890,8 +890,7 @@ void gather_nd_grad(const Tensor& x,
 template <typename T>
 void assign_grad(const Tensor& out_grad, Tensor* x_grad) {
   if (x_grad) {
-    auto res = assign<T>(out_grad);
-    set_output<T>(res, x_grad);
+    by_pass<T>(out_grad, x_grad);
   }
 }
 
