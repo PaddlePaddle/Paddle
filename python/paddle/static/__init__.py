97364c8f35a8284d1c5969236e66e038f588dce6
kangguangli <kangguangli@hotmail.com>
2023-03-15 08:41:44 +0000

    remove api `class ParallelExecutor`

--- python/paddle/static/__init__.py
+++ python/paddle/static/__init__.py
@@ -68,7 +68,6 @@ from ..fluid.framework import Parameter  # noqa: F401
 from ..fluid.framework import ipu_shard_guard  # noqa: F401
 from ..fluid.framework import set_ipu_shard  # noqa: F401
 from ..fluid.layers.control_flow import Print  # noqa: F401
-from ..fluid.parallel_executor import ParallelExecutor  # noqa: F401
 from ..fluid.param_attr import WeightNormParamAttr  # noqa: F401
 from ..fluid.optimizer import Optimizer  # noqa: F401
 from ..fluid.optimizer import Adam  # noqa: F401
