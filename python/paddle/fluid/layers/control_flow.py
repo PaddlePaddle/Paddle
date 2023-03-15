7e26a8e9d02d9b9adba40e886db5986f66603d47
GGBond8488 <857631483@qq.com>
2023-03-15 08:07:35 +0000

    fix test error

--- python/paddle/fluid/layers/control_flow.py
+++ python/paddle/fluid/layers/control_flow.py
@@ -1574,9 +1574,9 @@ class Switch:
 
             with fluid.layers.control_flow.Switch() as switch:
                 with switch.case(global_step == zero_var):
-                    fluid.layers.assign(input=one_var, output=lr)
+                    paddle.assign(input=one_var, output=lr)
                 with switch.default():
-                    fluid.layers.assign(input=two_var, output=lr)
+                    paddle.assign(input=two_var, output=lr)
 
             exe = fluid.Executor(fluid.CPUPlace())
             exe.run(fluid.default_startup_program())
