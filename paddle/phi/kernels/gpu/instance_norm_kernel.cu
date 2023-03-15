379a8dbabf54bbf52e0945c90fd262d7f9648d2a
404988613 <86900071+404988613@users.noreply.github.com>
2023-03-15 19:46:03 +0800

    Update instance_norm_kernel.cu

--- paddle/phi/kernels/gpu/instance_norm_kernel.cu
+++ paddle/phi/kernels/gpu/instance_norm_kernel.cu
@@ -226,14 +226,8 @@ void InstanceNormKernel(const Context &dev_ctx,
 #ifdef PADDLE_WITH_HIP
 // MIOPEN do not support double
 PD_REGISTER_KERNEL(
-    instance_norm, GPU, ALL_LAYOUT, phi::InstanceNormKernel, float) {
-  kernel->OutputAt(1).SetDataType(phi::DataType::UNDEFINED);
-  kernel->OutputAt(2).SetDataType(phi::DataType::UNDEFINED);
-}
+    instance_norm, GPU, ALL_LAYOUT, phi::InstanceNormKernel, float) {}
 #else
 PD_REGISTER_KERNEL(
-    instance_norm, GPU, ALL_LAYOUT, phi::InstanceNormKernel, float, double) {
-  kernel->OutputAt(1).SetDataType(phi::DataType::UNDEFINED);
-  kernel->OutputAt(2).SetDataType(phi::DataType::UNDEFINED);
-}
+    instance_norm, GPU, ALL_LAYOUT, phi::InstanceNormKernel, float, double) {}
 #endif
