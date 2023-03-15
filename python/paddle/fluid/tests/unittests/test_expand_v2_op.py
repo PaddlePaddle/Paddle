6f4d6fa59103a52ae65699516cc9d6eac0ac5634
HexToString <506181616@qq.com>
2023-03-15 03:43:19 +0000

    fix bug

--- python/paddle/fluid/tests/unittests/test_expand_v2_op.py
+++ python/paddle/fluid/tests/unittests/test_expand_v2_op.py
@@ -44,7 +44,7 @@ class TestExpandV2OpRank1(OpTest):
         self.expand_times = [1]
 
     def test_check_output(self):
-        self.check_output(check_prim=True)
+        self.check_output()
 
     def test_check_grad(self):
         self.check_grad(['X'], 'Out', check_prim=True)
