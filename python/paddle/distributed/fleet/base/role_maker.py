3c444536c090307b7b4841d026f49c65b098de48
hitywt <yuwentao126@126.com>
2023-03-15 16:17:33 +0800

    launch support auto parallel tuner
    fix tuner_and_run mode
    
    fix cluster config

--- python/paddle/distributed/fleet/base/role_maker.py
+++ python/paddle/distributed/fleet/base/role_maker.py
@@ -1075,7 +1075,12 @@ class PaddleCloudRoleMaker(RoleMakerBase):
             self._cur_endpoint = self._worker_endpoints
             self._non_distributed = True
         self._worker_endpoints = self._worker_endpoints.split(",")
-        self._trainers_num = len(self._worker_endpoints)
+        trainers_num = os.getenv("PADDLE_TRAINERS_NUM", None)
+        if trainers_num is None:
+            raise ValueError(
+                "Can not find PADDLE_TRAINERS_NUM, please check your environment."
+            )
+        self._trainers_num = int(trainers_num)
         self._nodes_num = len({x.split(':')[0] for x in self._worker_endpoints})
         self._local_rank = os.getenv("PADDLE_RANK_IN_NODE")
         self._local_device_ids = os.getenv("PADDLE_LOCAL_DEVICE_IDS")
