c9d5fc28e6d5f027dddf83e421a62156d3c9e4d8
Zhang Ting <Douyaer2020@qq.com>
2023-03-15 11:24:59 +0800

    Update test_bce_loss.py

--- python/paddle/fluid/tests/unittests/test_bce_loss.py
+++ python/paddle/fluid/tests/unittests/test_bce_loss.py
@@ -264,7 +264,7 @@ class TestBceLossOp(OpTest):
         self.outputs = {'Out': output_np}
 
     def test_check_output(self):
-        self.check_output()
+        self.check_output(check_eager=True)
 
     def test_check_grad(self):
         self.check_grad(['X'], 'Out')
