3338e90d7315c9c756fb63eea983525a1731927f
Ainavo <Ainavo@163.com>
2023-03-15 12:25:52 +0800

    解决冲突

--- python/paddle/tensor/__init__.py
+++ python/paddle/tensor/__init__.py
@@ -243,6 +243,8 @@ from .math import frac  # noqa: F401
 from .math import sgn  # noqa: F401
 from .math import take  # noqa: F401
 from .math import frexp  # noqa: F401
+from .math import sigmoid  # noqa: F401
+from .math import sigmoid_  # noqa: F401
 from .math import trapezoid  # noqa: F401
 from .math import cumulative_trapezoid  # noqa: F401
 
@@ -527,6 +529,8 @@ tensor_method_func = [  # noqa
     'bucketize',
     'sgn',
     'frexp',
+    'sigmoid',
+    'sigmoid_',
     'trapezoid',
     'cumulative_trapezoid',
 ]
