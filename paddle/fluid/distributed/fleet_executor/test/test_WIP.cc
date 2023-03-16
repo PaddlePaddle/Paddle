1fd9045479e56d1c12d0c36349b940f560f556b3
liuyuang <liuyuang@baidu.com>
2023-03-16 08:06:42 +0800

    start up of ut

--- paddle/fluid/distributed/fleet_executor/test/test_WIP.cc
+++ paddle/fluid/distributed/fleet_executor/test/test_WIP.cc
@@ -11,3 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#include "gtest/gtest.h"
+
+namespace phi {
+
+TEST(WIPTest, WIP) { EXPECT_TRUE(true == false); }
+
+}  // namespace phi
