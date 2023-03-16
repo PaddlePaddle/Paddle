44352b9e03e5a5bee0ec73408aa5c900cee2707f
weisy11 <weisy11@163.com>
2023-03-16 05:12:54 +0000

    dbg

--- paddle/phi/kernels/funcs/pooling.h
+++ paddle/phi/kernels/funcs/pooling.h
@@ -385,7 +385,7 @@ inline int PoolOutputSize(int input_size,
   } else {
     output_size =
         (input_size - filter_size + padding_1 + padding_2 - 1) / stride + 1 +
-        (input_size - filter_size + padding_1 + padding_2) % stride;
+        (input_size - filter_size + padding_1 + padding_2) % stride > 0;
   }
   PADDLE_ENFORCE_GT(
       output_size,
