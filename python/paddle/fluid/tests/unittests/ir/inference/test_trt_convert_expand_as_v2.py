fd3146eab1f8dd2ff85a322f800c38b63b740663
gzy19990617 <m13689897706@163.com>
2023-03-16 12:43:11 +0000

    fix python test

--- python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_expand_as_v2.py
+++ python/paddle/fluid/tests/unittests/ir/inference/test_trt_convert_expand_as_v2.py
@@ -161,60 +161,47 @@ class TrtConvertExpandASV2Test(TrtLayerAutoScanTest):
         self.run_test()
 
 
-class TrtConvertExpandASV2Test2(TrtLayerAutoScanTest):
+class TrtConvertExpandV2Test2(TrtLayerAutoScanTest):
     def is_program_valid(self, program_config: ProgramConfig) -> bool:
         attrs = [
             program_config.ops[i].attrs for i in range(len(program_config.ops))
         ]
-        if len(attrs[0]['target_shape']) < self.dims:
-            return False
-        if self.dims == 1:
-            if len(attrs[0]['target_shape']) == 4:
-                return False
         return True
 
     def sample_program_configs(self):
         def generate_input1(attrs: List[Dict[str, Any]]):
-            if self.dims == 4:
-                self.input_shape = [1, 8, 1, 32]
-                return np.random.random([1, 8, 1, 32]).astype(np.float32)
-            elif self.dims == 3:
-                self.input_shape = [1, 32, 32]
-                return np.random.random([1, 32, 32]).astype(np.float32)
-            elif self.dims == 2:
-                self.input_shape = [1, 32]
-                return np.random.random([1, 32]).astype(np.float32)
-            elif self.dims == 1:
-                self.input_shape = [32]
-                return np.random.random([32]).astype(np.float32)
+            if self.dims == 1:
+                self.input_shape = [1]
+                return np.random.random([1]).astype(np.float32)
 
-        def generate_shapeT1_data(shape):
-            return np.array(shape).astype(np.int32)
-
-        for dims in [1, 2, 3, 4]:
-            for shape in [
-                [10, 8, 32, 32],
-                [2, 8, 32, 32],
-                [8, 32, 32],
-                [2, 32],
-                [32],
-            ]:
+        for dims in [1]:
+            for shape in [[10]]:
                 dics = [
                     {
                         "target_shape": shape,
                     },
                 ]
                 self.dims = dims
+                dics_intput = [
+                    {"X": ["expand_v2_input"], "Y": ["shapeT1_data"]},
+                ]
                 ops_config = [
                     {
-                        "op_type": "expand_as_v2",
-                        "op_inputs": {
-                            "X": ["expand_v2_input"],
-                            "Y": ["expand_shape"],
+                        "op_type": "fill_constant",
+                        "op_inputs": {},
+                        "op_outputs": {"Out": ["shapeT1_data"]},
+                        "op_attrs": {
+                            "dtype": 2,
+                            "str_value": "10",
+                            "shape": [1],
                         },
+                    },
+                    {
+                        "op_type": "expand_as_v2",
+                        "op_inputs": dics_intput[0],
                         "op_outputs": {"Out": ["expand_v2_out"]},
                         "op_attrs": dics[0],
-                    }
+                    },
                 ]
                 ops = self.generate_op_config(ops_config)
                 program_config = ProgramConfig(
@@ -223,10 +210,7 @@ class TrtConvertExpandASV2Test2(TrtLayerAutoScanTest):
                     inputs={
                         "expand_v2_input": TensorConfig(
                             data_gen=partial(generate_input1, dics)
-                        ),
-                        "expand_shape": TensorConfig(
-                            data_gen=partial(generate_shapeT1_data, shape)
-                        ),
+                        )
                     },
                     outputs=["expand_v2_out"],
                 )
@@ -237,57 +221,24 @@ class TrtConvertExpandASV2Test2(TrtLayerAutoScanTest):
         self, program_config
     ) -> (paddle_infer.Config, List[int], float):
         def generate_dynamic_shape():
-            if self.dims == 4:
-                self.dynamic_shape.min_input_shape = {
-                    "expand_v2_input": [1, 8, 1, 32]
-                }
-                self.dynamic_shape.max_input_shape = {
-                    "expand_v2_input": [10, 8, 1, 32]
-                }
-                self.dynamic_shape.opt_input_shape = {
-                    "expand_v2_input": [1, 8, 1, 32]
-                }
-            elif self.dims == 3:
-                self.dynamic_shape.min_input_shape = {
-                    "expand_v2_input": [1, 32, 32]
-                }
-                self.dynamic_shape.max_input_shape = {
-                    "expand_v2_input": [8, 32, 32]
-                }
-                self.dynamic_shape.opt_input_shape = {
-                    "expand_v2_input": [1, 32, 32]
-                }
-            elif self.dims == 2:
-                self.dynamic_shape.min_input_shape = {
-                    "expand_v2_input": [1, 32]
-                }
-                self.dynamic_shape.max_input_shape = {
-                    "expand_v2_input": [4, 32]
-                }
-                self.dynamic_shape.opt_input_shape = {
-                    "expand_v2_input": [1, 32]
-                }
-            elif self.dims == 1:
-                self.dynamic_shape.min_input_shape = {"expand_v2_input": [32]}
-                self.dynamic_shape.max_input_shape = {"expand_v2_input": [64]}
-                self.dynamic_shape.opt_input_shape = {"expand_v2_input": [32]}
+            if self.dims == 1:
+                self.dynamic_shape.min_input_shape = {"expand_v2_input": [1]}
+                self.dynamic_shape.max_input_shape = {"expand_v2_input": [1]}
+                self.dynamic_shape.opt_input_shape = {"expand_v2_input": [1]}
 
         def clear_dynamic_shape():
             self.dynamic_shape.min_input_shape = {}
             self.dynamic_shape.max_input_shape = {}
             self.dynamic_shape.opt_input_shape = {}
 
-        attrs = [
-            program_config.ops[i].attrs for i in range(len(program_config.ops))
-        ]
-
         clear_dynamic_shape()
         # for dynamic_shape
         generate_dynamic_shape()
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
-        yield self.create_inference_config(), (1, 3), 1e-5
+        # fill_constant will be folded by constnt folding pass!
+        yield self.create_inference_config(), (1, 2), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
-        yield self.create_inference_config(), (1, 3), 1e-3
+        yield self.create_inference_config(), (1, 2), 1e-3
 
     def add_skip_trt_case(self):
         pass
