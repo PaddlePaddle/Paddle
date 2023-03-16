8211e318b065e01bd80a4188c58c36f1acb25255
404988613 <86900071+404988613@users.noreply.github.com>
2023-03-16 09:11:29 +0800

    Update sync_batch_norm_grad_kernel.cu

--- paddle/phi/kernels/sparse/gpu/sync_batch_norm_grad_kernel.cu
+++ paddle/phi/kernels/sparse/gpu/sync_batch_norm_grad_kernel.cu
@@ -81,7 +81,6 @@ PD_REGISTER_KERNEL(sync_batch_norm_coo_grad,
                    double,
                    phi::dtype::float16) {
   if (kernel_key.dtype() == phi::DataType::FLOAT16) {
-    kernel->OutputAt(0).SetDataType(phi::DataType::FLOAT32);  // x_grad
     kernel->OutputAt(1).SetDataType(phi::DataType::FLOAT32);  // scale_grad
     kernel->OutputAt(2).SetDataType(phi::DataType::FLOAT32);  // bias_grad
   }
