0a1442ca0af6805f66d57c95c20eed1b8c90bb18
enkilee <luhputu0815@gmail.com>
2023-03-18 03:59:10 +0000

    fix

--- python/paddle/fluid/tests/unittests/test_logsumexp.py
+++ python/paddle/fluid/tests/unittests/test_logsumexp.py
@@ -87,7 +87,7 @@ class TestLogsumexp(OpTest):
         pass
 
     def test_check_output(self):
-        self.check_output(check_eager=True)
+        self.check_output()
 
     def test_check_grad(self):
         self.check_grad(
@@ -95,7 +95,6 @@ class TestLogsumexp(OpTest):
             ['Out'],
             user_defined_grads=self.user_defined_grads,
             user_defined_grad_outputs=self.user_defined_grad_outputs,
-            check_eager=True,
         )
 
     def calc_grad(self):
