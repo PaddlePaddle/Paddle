95791ddb7ca517d9e95fb74ed4c38e13b178fbdc
Ainavo <Ainavo@163.com>
2023-03-20 00:07:15 +0800

    remove_deprecated_unittest_alias

--- python/paddle/fluid/tests/unittests/prim/test_comp_get_grad_op_desc_prim_enabled.py
+++ python/paddle/fluid/tests/unittests/prim/test_comp_get_grad_op_desc_prim_enabled.py
@@ -80,7 +80,7 @@ class TestGetGradOpDescPrimEnabled(unittest.TestCase):
                 self.fwd, self.no_grad_var, self.grad_sub_block
             )[0]
         )
-        self.assertEquals(actual, self.desired_ops)
+        self.assertEqual(actual, self.desired_ops)
         core._set_prim_backward_enabled(False)
 
 
