c74aaf676bd28ae6dbd70f29e34fe17ec766ff33
Zhou Wei <1183042833@qq.com>
2023-03-21 01:22:30 +0800

    [Zero-Dim] Support 0D for numel/rank/size/optimizer/create_parameter/create_global_var, fix some usage to adapt 0D (#51566)

--- python/paddle/static/quantization/tests/imperative_test_utils.py
+++ python/paddle/static/quantization/tests/imperative_test_utils.py
@@ -89,7 +89,7 @@ def train_lenet(lenet, reader, optimizer):
         lenet.clear_gradients()
 
         if batch_id % 100 == 0:
-            loss_list.append(avg_loss.numpy()[0])
+            loss_list.append(float(avg_loss))
             _logger.info('{}: {}'.format('loss', avg_loss.numpy()))
 
     return loss_list
