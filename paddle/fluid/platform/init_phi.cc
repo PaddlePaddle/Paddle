62712aec847ed0e712e015402d78f8b37ec8657b
YuanRisheng <yuanrisheng@baidu.com>
2023-03-20 17:32:18 +0800

    [PHI Decoupling]Add PHI init for extension (#51511)
    * remove init
    
    * delete fluid in context pool
    
    * fix custom op bugs
    
    * fix profiler bugs
    
    * fix ci bugs
    
    * fix window compile bugs
    
    * fix windows bugs
    
    * fix window bugs

--- paddle/fluid/platform/init_phi.cc
+++ paddle/fluid/platform/init_phi.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -11,16 +11,17 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-
-#pragma once
-
-// All paddle apis in C++ frontend
-#include "paddle/phi/api/all.h"
-// Python bindings for the C++ frontend (includes Python.h)
-#if !defined(PADDLE_ON_INFERENCE) && !defined(PADDLE_NO_PYTHON)
-#include "paddle/utils/pybind.h"
-#endif
-// For initialization of DeviceContextPool and MemoryMethod
 #include "paddle/fluid/platform/init_phi.h"
+#include "glog/logging.h"
+#include "paddle/fluid/platform/init.h"
 
-static paddle::InitPhi g_init_phi;
+REGISTER_FILE_SYMBOLS(init_phi)
+
+namespace paddle {
+
+InitPhi::InitPhi() {
+  paddle::framework::InitMemoryMethod();
+  LOG(INFO) << "Init MemoryMethod success.";
+}
+
+}  // namespace paddle
