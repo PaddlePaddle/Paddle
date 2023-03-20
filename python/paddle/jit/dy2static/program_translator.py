d475c3e067285e8050d4d2340fe6b639e58c9725
Liyulingyue <852433440@qq.com>
2023-03-21 05:58:36 +0800

    MOVE INTO DEVELOP

--- python/paddle/jit/dy2static/program_translator.py
+++ python/paddle/jit/dy2static/program_translator.py
@@ -22,12 +22,12 @@ import weakref
 from paddle.amp.auto_cast import _in_amp_guard
 from paddle.fluid import _non_static_mode, core, framework
 from paddle.fluid.data_feeder import check_type
-from paddle.fluid.dygraph import layers
 from paddle.fluid.dygraph.base import (
     _switch_declarative_mode_guard_,
     param_guard,
     switch_to_static_graph,
 )
+from paddle.nn.layer import layers
 from paddle.utils import flatten, gast
 
 from . import error, logging_utils
