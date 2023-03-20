43b3d108f81c66edaadfc5c5f140836da2c844b0
ZzSean <18818272991@163.com>
2023-03-20 09:09:51 +0000

    fix

--- paddle/phi/kernels/funcs/blas/blaslt_impl.cu.h
+++ paddle/phi/kernels/funcs/blas/blaslt_impl.cu.h
@@ -15,7 +15,6 @@ limitations under the License. */
 #pragma once
 
 #if defined(PADDLE_WITH_CUDA) && CUDA_VERSION >= 11060
-
 #include <cuda_runtime_api.h>  // NOLINT
 #include "cuda.h"              // NOLINT
 #include "paddle/phi/backends/dynload/cublasLt.h"
