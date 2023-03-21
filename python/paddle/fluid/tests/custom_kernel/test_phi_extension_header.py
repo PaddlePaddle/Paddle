3e6694531faf4368a7939a2ea7f794f17e921f22
qili93 <qili93@qq.com>
2023-03-21 21:24:52 +0800

    only support linux

--- python/paddle/fluid/tests/custom_kernel/test_phi_extension_header.py
+++ python/paddle/fluid/tests/custom_kernel/test_phi_extension_header.py
@@ -14,6 +14,7 @@
 
 import os
 import re
+import sys
 import tempfile
 import unittest
 
@@ -58,4 +59,7 @@ class TestCustomCPUPlugin(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    if os.name == 'nt' or sys.platform.startswith('darwin'):
+        # only support Linux now
+        exit()
     unittest.main()
