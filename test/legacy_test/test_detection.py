be9ef67eb29a139dc514ed2372447fc60e77bdaf
Zheng-Bicheng <zheng_bicheng@outlook.com>
2023-03-22 04:46:20 +0800

    updata

--- test/legacy_test/test_detection.py
+++ test/legacy_test/test_detection.py
@@ -16,13 +16,13 @@ import contextlib
 import unittest
 
 import numpy as np
+from test_imperative_base import new_program_scope
 
 import paddle
 import paddle.fluid as fluid
 from paddle.fluid import core
 from paddle.fluid.dygraph import base
 from paddle.fluid.framework import Program, program_guard
-from paddle.fluid.tests.unittests.test_imperative_base import new_program_scope
 
 paddle.enable_static()
 
