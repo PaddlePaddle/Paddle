35b1a948b3542c3be8d4a9cae554b26da6bdc7f7
superwinner1 <82640284+superwinner1@users.noreply.github.com>
2023-03-21 18:09:40 +0800

    add fmax BF16 test

--- python/paddle/fluid/tests/unittests/test_fmax_op.py
+++ python/paddle/fluid/tests/unittests/test_fmax_op.py
@@ -241,5 +241,34 @@ class TestElementwiseFmax3Op(OpTest):
         self.check_grad(['X', 'Y'], 'Out')
 
 
+@unittest.skipIf(
+    not core.is_compiled_with_cuda()
+    or not core.is_bfloat16_supported(core.CUDAPlace(0)),
+    "core is not complied with CUDA and not support the bfloat16",
+)
+class TestFmaxBF16OP(OpTest):
+    def setUp(self):
+        self.op_type = "elementwise_fmax"
+        self.python_api = paddle.fmax
+        self.dtype = np.uint16
+        self.inputs = {
+            'X': np.random.randn(11, 17).astype(self.dtype),
+            'Y': np.random.randn(11, 17).astype(self.dtype),
+        }
+        self.outputs = {'Out': np.fmax(self.inputs['X'], self.inputs['Y'])}
+
+    def test_check_output(self):
+        if core.is_compiled_with_cuda():
+            place = core.CUDAPlace(0)
+            if core.is_bfloat16_supported(place):
+                self.check_output_with_place(place)
+
+    def test_check_grad(self):
+        if core.is_compiled_with_cuda():
+            place = core.CUDAPlace(0)
+            if core.is_bfloat16_supported(place):
+                self.check_grad_with_place(place, ['X', 'Y'], 'Out')
+
+
 if __name__ == "__main__":
     unittest.main()
