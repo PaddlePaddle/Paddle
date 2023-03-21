01c85b411a95872f7ce6d74e1bb3743949daa9c3
LoneRanger <836253168@qq.com>
2023-03-21 16:02:21 +0800

    Update test_image_classification_fp16.py

--- python/paddle/fluid/contrib/tests/test_image_classification_fp16.py
+++ python/paddle/fluid/contrib/tests/test_image_classification_fp16.py
@@ -24,7 +24,6 @@ import copy
 import numpy as np
 import tempfile
 from paddle.static.amp import decorate
-from paddle.fluid.tests.unittests.nets import img_conv_group
 
 paddle.enable_static()
 
@@ -76,7 +75,7 @@ def resnet_cifar10(input, depth=32):
 
 def vgg16_bn_drop(input):
     def conv_block(input, num_filter, groups, dropouts):
-        return img_conv_group(
+        return fluid.tests.unittests.nets.img_conv_group(
             input=input,
             pool_size=2,
             pool_stride=2,
