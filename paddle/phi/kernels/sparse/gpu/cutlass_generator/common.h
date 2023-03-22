197c59adc230f9deeb4d228084cf12d81d0389d0
umiswing <umiswing@foxmail.com>
2023-03-22 08:23:45 +0000

    using device::reduce, still not work yet. :(

--- paddle/phi/kernels/sparse/gpu/cutlass_generator/common.h
+++ paddle/phi/kernels/sparse/gpu/cutlass_generator/common.h
@@ -22,7 +22,7 @@
 #include "cutlass/gemm/gemm.h"
 #include "cutlass/gemm/threadblock/threadblock_swizzle.h"
 #include "cutlass/half.h"
-#include "cutlass/reduction/kernel/reduce_split_k.h"
+#include "cutlass/reduction/device/reduce_split_k.h"
 #include "cutlass/reduction/thread/reduction_operators.h"
 #include "cutlass/tensor_ref.h"
 #include "cutlass/util/device_memory.h"
@@ -51,114 +51,102 @@ namespace sparse {
       throw std::runtime_error(cutlassGetStatusString(error)); \
     }                                                          \
   }
-#define DEFINE_LAUNCH_KERNEL(dtype, cutlass_type)                              \
-  template <typename Config>                                                   \
-  void launchKernel(dtype const alpha,                                         \
-                    dtype const beta,                                          \
-                    const GPUContext& dev_ctx,                                 \
-                    const dtype* const a,                                      \
-                    const dtype* const b,                                      \
-                    const dtype* const c,                                      \
-                    dtype* const d,                                            \
-                    const int m,                                               \
-                    const int n,                                               \
-                    const int k,                                               \
-                    const int32_t* a_indices,                                  \
-                    const int32_t* b_indices,                                  \
-                    const int32_t* c_d_indices) {                              \
-    cutlass::gemm::GemmCoord problem_size_real({m, n, k});                     \
-    using Gemm = typename Config::Gemm;                                        \
-    int split_k_slices = std::max(std::min(128, k / 128), 1);                  \
-    typename Gemm::Arguments arguments{                                        \
-        Config::Mode,                                                          \
-        problem_size_real,                                                     \
-        split_k_slices,                                                        \
-        {static_cast<const cutlass_type>(static_cast<const float>(alpha)),     \
-         static_cast<const cutlass_type>(static_cast<const float>(beta))},     \
-        reinterpret_cast<const cutlass_type* const>(a),                        \
-        reinterpret_cast<const cutlass_type* const>(b),                        \
-        reinterpret_cast<const cutlass_type* const>(c),                        \
-        reinterpret_cast<cutlass_type* const>(d),                              \
-        typename Gemm::Base::LayoutA().capacity(problem_size_real.mk()),       \
-        typename Gemm::Base::LayoutB().capacity(problem_size_real.kn()),       \
-        typename Gemm::Base::LayoutC().capacity(problem_size_real.mn()),       \
-        typename Gemm::Base::LayoutC().capacity(problem_size_real.mn()),       \
-        std::is_same<typename Gemm::Base::LayoutA,                             \
-                     cutlass::layout::RowMajor>::value                         \
-            ? problem_size_real.k()                                            \
-            : problem_size_real.m(),                                           \
-        std::is_same<typename Gemm::Base::LayoutB,                             \
-                     cutlass::layout::RowMajor>::value                         \
-            ? problem_size_real.n()                                            \
-            : problem_size_real.k(),                                           \
-        std::is_same<typename Gemm::Base::LayoutC,                             \
-                     cutlass::layout::RowMajor>::value                         \
-            ? problem_size_real.n()                                            \
-            : problem_size_real.m(),                                           \
-        std::is_same<typename Gemm::Base::LayoutC,                             \
-                     cutlass::layout::RowMajor>::value                         \
-            ? problem_size_real.n()                                            \
-            : problem_size_real.m(),                                           \
-        a_indices,                                                             \
-        b_indices,                                                             \
-        c_d_indices};                                                          \
-    size_t workspace_size = Gemm::get_workspace_size(arguments);               \
-    cutlass::device_memory::allocation<uint8_t> workspace(workspace_size);     \
-    Gemm gemm_op;                                                              \
-    cutlass::Status status = gemm_op.can_implement(arguments);                 \
-    GATHER_GEMM_SCATTER_CHECK(status);                                         \
-    status = gemm_op.initialize(arguments, workspace.get());                   \
-    GATHER_GEMM_SCATTER_CHECK(status);                                         \
-    gemm_op(dev_ctx.stream());                                                 \
-    if (Config::Mode != cutlass::gemm::GemmUniversalMode::kGemmSplitKParallel) \
-      return;                                                                  \
-    using ReductionOp = cutlass::reduction::thread::ReduceAdd<                 \
-        typename Gemm::ElementAccumulator,                                     \
-        typename Gemm::EpilogueOutputOp::ElementAccumulator,                   \
-        Gemm::EpilogueOutputOp::kCount>;                                       \
-                                                                               \
-    using ReductionKernel = cutlass::reduction::kernel::ReduceSplitK<          \
-        cutlass::MatrixShape<4, 32 * Gemm::EpilogueOutputOp::kCount>,          \
-        typename Gemm::EpilogueOutputOp,                                       \
-        ReductionOp>;                                                          \
-                                                                               \
-    cutlass::gemm::threadblock::GemmSplitKHorizontalThreadblockSwizzle         \
-        threadblock_swizzle;                                                   \
-    typename cutlass::gemm::GemmCoord grid_shape =                             \
-        threadblock_swizzle.get_tiled_shape(problem_size_real,                 \
-                                            {Gemm::ThreadblockShape::kM,       \
-                                             Gemm::ThreadblockShape::kN,       \
-                                             Gemm::ThreadblockShape::kK},      \
-                                            split_k_slices);                   \
-                                                                               \
-    cutlass::TensorRef<typename Gemm::ElementAccumulator,                      \
-                       cutlass::layout::RowMajor>                              \
-        ref_workspace(reinterpret_cast<typename Gemm::ElementAccumulator*>(    \
-                          workspace.get()),                                    \
-                      problem_size_real.n());                                  \
-    cutlass::TensorRef<const typename Gemm::Base::ElementC,                    \
-                       typename Gemm::Base::LayoutC>                           \
-        ref_C(reinterpret_cast<const typename Gemm::Base::ElementC* const>(c), \
-              problem_size_real.n());                                          \
-    cutlass::TensorRef<typename Gemm::Base::ElementC,                          \
-                       typename Gemm::Base::LayoutC>                           \
-        ref_D(reinterpret_cast<typename Gemm::Base::ElementC* const>(d),       \
-              problem_size_real.n());                                          \
-    int64_t partition_stride =                                                 \
-        int64_t(problem_size_real.m()) * int64_t(problem_size_real.n());       \
-    typename ReductionKernel::Params reduction_params_;                        \
-    reduction_params_ =                                                        \
-        typename ReductionKernel::Params(problem_size_real.mn(),               \
-                                         grid_shape.k(),                       \
-                                         partition_stride,                     \
-                                         ref_workspace,                        \
-                                         ref_D,                                \
-                                         ref_C.non_const_ref());               \
-                                                                               \
-    dim3 block = ReductionKernel::block_shape();                               \
-    dim3 grid = ReductionKernel::grid_shape(problem_size_real.mn());           \
-    cutlass::Kernel<ReductionKernel>                                           \
-        <<<grid, block, 0, dev_ctx.stream()>>>(reduction_params_);             \
+#define DEFINE_LAUNCH_KERNEL(dtype, cutlass_type)                             \
+  template <typename Config>                                                  \
+  void launchKernel(dtype const alpha,                                        \
+                    dtype const beta,                                         \
+                    const GPUContext& dev_ctx,                                \
+                    const dtype* const a,                                     \
+                    const dtype* const b,                                     \
+                    const dtype* const c,                                     \
+                    dtype* const d,                                           \
+                    const int m,                                              \
+                    const int n,                                              \
+                    const int k,                                              \
+                    const int32_t* a_indices,                                 \
+                    const int32_t* b_indices,                                 \
+                    const int32_t* c_d_indices) {                             \
+    cutlass::gemm::GemmCoord problem_size_real({m, n, k});                    \
+    using Gemm = typename Config::Gemm;                                       \
+    int split_k_slices = std::max(std::min(128, k / 128), 1);                 \
+    typename Gemm::Arguments arguments{                                       \
+        Config::Mode,                                                         \
+        problem_size_real,                                                    \
+        split_k_slices,                                                       \
+        {static_cast<const cutlass_type>(static_cast<const float>(alpha)),    \
+         static_cast<const cutlass_type>(static_cast<const float>(beta))},    \
+        reinterpret_cast<const cutlass_type* const>(a),                       \
+        reinterpret_cast<const cutlass_type* const>(b),                       \
+        reinterpret_cast<const cutlass_type* const>(c),                       \
+        reinterpret_cast<cutlass_type* const>(d),                             \
+        typename Gemm::Base::LayoutA().capacity(problem_size_real.mk()),      \
+        typename Gemm::Base::LayoutB().capacity(problem_size_real.kn()),      \
+        typename Gemm::Base::LayoutC().capacity(problem_size_real.mn()),      \
+        typename Gemm::Base::LayoutC().capacity(problem_size_real.mn()),      \
+        std::is_same<typename Gemm::Base::LayoutA,                            \
+                     cutlass::layout::RowMajor>::value                        \
+            ? problem_size_real.k()                                           \
+            : problem_size_real.m(),                                          \
+        std::is_same<typename Gemm::Base::LayoutB,                            \
+                     cutlass::layout::RowMajor>::value                        \
+            ? problem_size_real.n()                                           \
+            : problem_size_real.k(),                                          \
+        std::is_same<typename Gemm::Base::LayoutC,                            \
+                     cutlass::layout::RowMajor>::value                        \
+            ? problem_size_real.n()                                           \
+            : problem_size_real.m(),                                          \
+        std::is_same<typename Gemm::Base::LayoutC,                            \
+                     cutlass::layout::RowMajor>::value                        \
+            ? problem_size_real.n()                                           \
+            : problem_size_real.m(),                                          \
+        a_indices,                                                            \
+        b_indices,                                                            \
+        c_d_indices};                                                         \
+    size_t workspace_size = Gemm::get_workspace_size(arguments);              \
+    cutlass::device_memory::allocation<uint8_t> workspace(workspace_size);    \
+    Gemm gemm_op;                                                             \
+    cutlass::Status status = gemm_op.can_implement(arguments);                \
+    GATHER_GEMM_SCATTER_CHECK(status);                                        \
+    status = gemm_op.initialize(arguments, workspace.get());                  \
+    GATHER_GEMM_SCATTER_CHECK(status);                                        \
+    gemm_op(dev_ctx.stream());                                                \
+    if (Config::Mode ==                                                       \
+        cutlass::gemm::GemmUniversalMode::kGemmSplitKParallel) {              \
+      using ReductionOp = cutlass::reduction::thread::ReduceAdd<              \
+          typename Gemm::ElementAccumulator,                                  \
+          typename Gemm::EpilogueOutputOp::ElementAccumulator,                \
+          Gemm::EpilogueOutputOp::kCount>;                                    \
+                                                                              \
+      using ReductionKernel = cutlass::reduction::kernel::ReduceSplitK<       \
+          cutlass::MatrixShape<4, 32 * Gemm::EpilogueOutputOp::kCount>,       \
+          typename Gemm::EpilogueOutputOp,                                    \
+          ReductionOp>;                                                       \
+      using ReductionDevice =                                                 \
+          typename cutlass::reduction::device::ReduceSplitK<ReductionKernel>; \
+      ReductionDevice reduction_op;                                           \
+      cutlass::TensorRef<typename Gemm::ElementAccumulator,                   \
+                         cutlass::layout::RowMajor>                           \
+          ref_workspace(reinterpret_cast<typename Gemm::ElementAccumulator*>( \
+                            workspace.get()),                                 \
+                        problem_size_real.n());                               \
+      cutlass::TensorRef<typename Gemm::Base::ElementC,                       \
+                         typename Gemm::Base::LayoutC>                        \
+          ref_c(reinterpret_cast<typename Gemm::Base::ElementC* const>(d),    \
+                problem_size_real.n());                                       \
+      cutlass::TensorRef<typename Gemm::Base::ElementC,                       \
+                         typename Gemm::Base::LayoutC>                        \
+          ref_d(reinterpret_cast<typename Gemm::Base::ElementC* const>(d),    \
+                problem_size_real.n());                                       \
+      typename ReductionDevice::Arguments reduction_args(                     \
+          problem_size_real.mn(),                                             \
+          split_k_slices,                                                     \
+          static_cast<size_t>(problem_size_real.m() * problem_size_real.n()), \
+          ref_workspace,                                                      \
+          ref_d,                                                              \
+          ref_c);                                                             \
+      status = reduction_op.initialize(reduction_args);                       \
+      status = reduction_op(dev_ctx.stream());                                \
+    }                                                                         \
   }
 
 TYPEDEF_KERNEL_POINTER(fp16_gather_gemm_scatter, phi::dtype::float16)
