471f97ce12facd7a7ef9e825de5edd6878950b58
Young-Flash <871946895@qq.com>
2023-03-23 11:51:38 +0000

    improve data type readability

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -1954,7 +1954,7 @@ struct SimpleOpTypeSetTeller : public Teller {
     }
 
     if (op_type == "bitwise_not") {
-#if !IS_TRT_VERSION_GE(8400)
+#if !IS_TRT_VERSION_GE(8500)
       auto* block = desc.Block();
       if (block == nullptr) {
         VLOG(3) << "The block desc is nullptr, we can't continue to analyze. "
@@ -1965,7 +1965,9 @@ struct SimpleOpTypeSetTeller : public Teller {
       auto x_var_name = desc.Input("X")[0];
       auto* x_var_desc = block->FindVar(x_var_name);
       auto dtype = x_var_desc->GetDataType();
-      if (dtype == 0 || dtype == 21 || dtype == 20) {
+      if (dtype == framework::proto::VarType::BOOL ||
+          dtype == framework::proto::VarType::INT8 ||
+          dtype == framework::proto::VarType::UINT8) {
         VLOG(3) << "BOOL / INT8 / UINT8 type support requires TensorRT 8.4";
         return false;
       }
