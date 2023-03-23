6c04252f6171b395806e75effecbdccf0b2c3f78
JiabinYang <360788950@qq.com>
2023-03-23 12:55:30 +0000

    fix flatten

--- python/paddle/incubate/autograd/composite_rules.py
+++ python/paddle/incubate/autograd/composite_rules.py
@@ -324,8 +324,10 @@ def flatten_contiguous_range_composite(x, start_axis, stop_axis):
     start_dim = start_axis if len(shape_in) != 0 else 0
     end_dim = stop_axis if len(shape_in) != 0 else 0
     assert start_dim <= end_dim
-    if len(shape_in) == 0 or start_dim == end_dim:
+    if len(shape_in) == 0:
         return reshape(x, shape=[1]), None
+    if start_dim == end_dim:
+        return reshape(x, shape=shape_in), None
     slice_numel = 1
     for i in range(start_dim, end_dim + 1):
         slice_numel *= shape_in[i]
