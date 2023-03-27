796149101bba30b898386780b6cf3a40d21cb1e7
kangguangli <kangguangli@hotmail.com>
2023-03-27 03:42:21 +0000

    fix

--- python/paddle/fluid/tests/unittests/dist_fleet_raw_program_optimizer.py
+++ python/paddle/fluid/tests/unittests/dist_fleet_raw_program_optimizer.py
@@ -110,6 +110,7 @@ class TestFleetMetaOptimizerPrecision(TestDistRunnerBase):
             fleet.init(role)
             strategy = paddle.distributed.fleet.DistributedStrategy()
             strategy.without_graph_optimization = True
+            strategy.fuse_all_reduce_ops = False
             optimizer = fleet.distributed_optimizer(
                 optimizer, strategy=strategy
             )
