0948bbf17aaa7c27df37588259b1cb46fcc11e73
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-03-28 01:20:28 +0000

    fix bug

--- python/paddle/jit/dy2static/utils.py
+++ python/paddle/jit/dy2static/utils.py
@@ -1479,9 +1479,9 @@ def _out_grad_names(program_desc, fwd_end_op_index, out_size):
         op = program_desc.block(0).op(i)
         # If prim forward op, fill_any_like will be decomposite as fill_constant.
         if core._is_fwd_prim_enabled():
-            target = 'fill_any_like'
-        else:
             target = ('fill_any_like', 'fill_constant')
+        else:
+            target = 'fill_any_like'
         if op.type() in target:
             var_name = op.output('Out')[0]
             names.append(var_name)
