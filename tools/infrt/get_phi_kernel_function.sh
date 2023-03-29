d48d8cba40148f69c51b34daa507dc843a639c11
jjyaoao <88936287+jjyaoao@users.noreply.github.com>
2023-03-29 04:27:03 +0000

    Clear the infrt-related code

--- tools/infrt/get_phi_kernel_function.sh
+++ tools/infrt/get_phi_kernel_function.sh
@@ -87,43 +87,9 @@ find  ${PADDLE_ROOT}/paddle/phi/ -name "*.cc" | xargs grep PD_REGISTER_INFER_MET
 
 #step 3:get ir's attr_name.
 ir_attr_name_info_file=`mktemp`
-# phi_cpu attr
-all_ir_name=`grep -Eo "PDTCPU_Kernel<.*\"" ${PADDLE_ROOT}/paddle/infrt/dialect/phi/ir/phi_cpu_kernels.td | awk -v FS="<" '{gsub(/\"/,"");print $2}'`
-for ir in $all_ir_name
-do
-  attr_name=`grep "<\"$ir" -A 3 ${PADDLE_ROOT}/paddle/infrt/dialect/phi/ir/phi_cpu_kernels.td  | grep -Eo "Attr:.*)" \
-  | awk '{gsub(/F32Attr/,"");gsub(/F64Attr/,"");gsub(/StrAttr/,"");gsub(/BoolAttr/,""); \
-  gsub(/SI1Attr/,"");gsub(/SI8Attr/,"");gsub(/SI16Attr/,"");gsub(/SI32Attr/,"");gsub(/SI64Attr/,""); \
-  gsub(/UI1Attr/,"");gsub(/UI8Attr/,"");gsub(/I16Attr/,"");gsub(/I32Attr/,"");gsub(/I64Attr/,""); \
-  gsub(/I1Attr/,"");gsub(/I8Attr/,"");gsub(/UI16Attr/,"");gsub(/UI32Attr/,"");gsub(/UI64Attr/,""); \
-  gsub(/I32ArrayAttr/,"");gsub(/SI32ArrayAttr/,""); \
-  gsub(/Attr/,"");gsub(/\)/,""); \
-  gsub(/[,:]/,"");print $a}'`
-  echo phi_cpu.$ir $attr_name >> $ir_attr_name_info_file
-done
-# phi_gpu attr
-all_ir_name=`grep -Eo "PDTGPU_Kernel<.*\"" ${PADDLE_ROOT}/paddle/infrt/dialect/phi/ir/phi_gpu_kernels.td | awk -v FS="<" '{gsub(/\"/,"");print $2}'`
-for ir in $all_ir_name
-do
-  attr_name=`grep "<\"$ir" -A 3 ${PADDLE_ROOT}/paddle/infrt/dialect/phi/ir/phi_gpu_kernels.td  | grep -Eo "Attr:.*)" \
-  | awk '{gsub(/F32Attr/,"");gsub(/F64Attr/,"");gsub(/StrAttr/,"");gsub(/BoolAttr/,""); \
-  gsub(/SI1Attr/,"");gsub(/SI8Attr/,"");gsub(/SI16Attr/,"");gsub(/SI32Attr/,"");gsub(/SI64Attr/,""); \
-  gsub(/UI1Attr/,"");gsub(/UI8Attr/,"");gsub(/I16Attr/,"");gsub(/I32Attr/,"");gsub(/I64Attr/,""); \
-  gsub(/I1Attr/,"");gsub(/I8Attr/,"");gsub(/UI16Attr/,"");gsub(/UI32Attr/,"");gsub(/UI64Attr/,""); \
-  gsub(/I32ArrayAttr/,"");gsub(/SI32ArrayAttr/,""); \
-  gsub(/Attr/,"");gsub(/\)/,"") \
-  gsub(/[,:]/,"");print $a}'`
-  echo phi_gpu.$ir $attr_name >> $ir_attr_name_info_file
-done
 
 #step 4: merge all infos
 #  @input1 => phi kernel infomation : kernel_name kernel_key(GPU/CPU, precision, layout)
 #  @input2 => information from api.yaml : kernel_name kernel_function_name inferMeta_function_name 
 #  @input3 => information from wrapped_infermeta_gen : ensure the inferMeta function has
 #             same signature with kernel function
-python3 ${PADDLE_ROOT}/tools/infrt/get_phi_kernel_info.py \
-  --paddle_root_path ${PADDLE_ROOT} \
-  --kernel_info_file $kernel_register_info_file \
-  --infermeta_wrap_file ${temp_path}/wrap_info.txt \
-  --attr_info_file $ir_attr_name_info_file \
-  --generate_file ${PADDLE_ROOT}/paddle/infrt/kernel/phi/infershaped/infershaped_kernel_launchers.cc
