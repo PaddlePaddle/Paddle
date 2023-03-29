e1fc873ec5dec1754877d3f3fd7d5f0626453671
Wangzheee <634486483@qq.com>
2023-03-29 11:07:52 +0000

    fix

--- paddle/fluid/inference/tensorrt/engine.cc
+++ paddle/fluid/inference/tensorrt/engine.cc
@@ -154,11 +154,16 @@ void TensorRTEngine::FreezeNetwork() {
 #if IS_TRT_VERSION_GE(8300)
   infer_builder_config_->setMemoryPoolLimit(
       nvinfer1::MemoryPoolType::kWORKSPACE, max_workspace_);
-  infer_builder_config_->setPreviewFeature(
-      nvinfer1::PreviewFeature::kFASTER_DYNAMIC_SHAPES_0805, true);
 #else
   infer_builder_config_->setMaxWorkspaceSize(max_workspace_);
 #endif
+
+#if IS_TRT_VERSION_GE(8400)
+  infer_builder_config_->setPreviewFeature(
+      nvinfer1::PreviewFeature::kFASTER_DYNAMIC_SHAPES_0805, true);
+#else
+#endif
+
   bool enable_fp16 = (precision_ == AnalysisConfig::Precision::kHalf);
   if (enable_fp16) {
     bool support_fp16 = infer_builder_->platformHasFastFp16();
