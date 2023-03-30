21da0b350b7383a35754f450482e1ad6769849ab
risemeup1 <515586620@qq.com>
2023-03-30 11:44:04 +0000

    fix_gcc12_error

--- paddle/phi/common/float16.h
+++ paddle/phi/common/float16.h
@@ -17,6 +17,8 @@
 #if defined(_M_X64) || defined(__x86_64__) || defined(_M_IX86) || \
     defined(__i386__)
 #define __PADDLE_x86__
+#if defined(___SSE2__)
+#undef __SSE2__
 #include <immintrin.h>
 #endif
 #include <stdint.h>
