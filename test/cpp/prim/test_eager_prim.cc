a445466f29b5e450c564a4110786e78f36cb546a
Zheng-Bicheng <58363586+Zheng-Bicheng@users.noreply.github.com>
2023-03-30 17:12:58 +0800

    [Move Test] Move prim cpp (#52173)
    * update
    
    * update
    
    * update

--- test/cpp/prim/test_eager_prim.cc
+++ test/cpp/prim/test_eager_prim.cc
@@ -21,11 +21,11 @@
 #include "paddle/fluid/eager/api/utils/hook_utils.h"
 #include "paddle/fluid/eager/backward.h"
 #include "paddle/fluid/eager/tests/test_utils.h"
-#include "paddle/fluid/prim/tests/init_env_utils.h"
 #include "paddle/fluid/prim/utils/utils.h"
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/tensor_meta.h"
+#include "test/cpp/prim/init_env_utils.h"
 
 DECLARE_string(tensor_operants_mode);
 
