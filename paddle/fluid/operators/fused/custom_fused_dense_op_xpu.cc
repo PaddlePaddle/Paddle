64e2c4b1f93d8a558d1474edf0e02f803451383f
Zhangjingyu06 <zhangjingyu06@baidu.com>
2023-03-31 08:46:23 +0000

    add l3 use in custom_fuse_dense

--- paddle/fluid/operators/fused/custom_fused_dense_op_xpu.cc
+++ paddle/fluid/operators/fused/custom_fused_dense_op_xpu.cc
@@ -43,9 +43,8 @@ class CustomFusedDenseXPUKernel : public framework::OpKernel<T> {
 
     bool transx = ctx.Attr<bool>("transx");
     bool transy = ctx.Attr<bool>("transy");
-    // bool use_addto = ctx.Attr<bool>("use_addto");
-    std::string activation = ctx.Attr<std::string>("activation");
 
+    std::string activation = ctx.Attr<std::string>("activation");
     VLOG(5) << "transx = " << transx << " , transy = " << transy
             << " , activation = " << activation;
 
@@ -80,7 +79,7 @@ class CustomFusedDenseXPUKernel : public framework::OpKernel<T> {
     xpu::ctx_guard RAII_GUARD(xpu_ctx);
     // XPUType* fc_out_ptr = RAII_GUARD.alloc_l3_or_gm<XPUType>(out->numel());
     // phi::MatMulXPUFunction<XPUType>(
-        // xpu_ctx, x_ptr, y_ptr, fc_out_ptr, fc_info, 1.0f);
+    // xpu_ctx, x_ptr, y_ptr, fc_out_ptr, fc_info, 1.0f);
     XPUType* bias_out_ptr = out_ptr;
     if (activation != "none" && reserve_space) {
       bias_out_ptr = reinterpret_cast<XPUType*>(
@@ -99,22 +98,126 @@ class CustomFusedDenseXPUKernel : public framework::OpKernel<T> {
     float* max_out = fc_info.max_out;
 
     const XPUType* bias_ptr = reinterpret_cast<const XPUType*>(bias->data<T>());
+    int nsdnn = xpu_ctx->nsdnn();
+    XPUType* x_ptr_l3 = RAII_GUARD.alloc_l3_or_gm<XPUType>(m * k);
+    float* max_x_ptr = RAII_GUARD.alloc<float>(nsdnn);
+    // move x_ptr to l3
+    int x_input_size = m * k;
+    int bias_size = bias->numel();
+    uint64_t l3_size = 63 * 1024 * 1024;
+    bool copy_to_l3 = (l3_size >= x_input_size * sizeof(XPUType));
+    bool copy_to_l3_bias = (l3_size >= (bias_size * sizeof(float) +
+                                        x_input_size * sizeof(XPUType)));
     if ((bias_ptr == nullptr) || std::is_same<T, float>::value) {
-      r = xpu::fc_fusion<XPUType, XPUType, XPUType, int16_t>(xpu_ctx, x_ptr, y_ptr, bias_out_ptr, m, n, k, trans_x,
-        trans_y, max_x, max_y, max_out, ldx, ldy, ldout, 1.0f, 0.0f,
-        reinterpret_cast<const float*>(bias_ptr), xpu::Activation_t::LINEAR);
-      PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_fusion");
+      if (copy_to_l3 && (k == ldx)) {
+        // r = xpu::findmax2d_copy_fusion<XPUType>(xpu_ctx, x_ptr, max_x_ptr,
+        // x_ptr_l3, m, k, k, k); PADDLE_ENFORCE_XDNN_SUCCESS(r,
+        // "findmax2d_copy_fusion");
+        r = xpu::findmax_copy_fusion<XPUType>(
+            xpu_ctx, x_ptr, max_x_ptr, x_ptr_l3, (m * k));
+        PADDLE_ENFORCE_XDNN_SUCCESS(r, "findmax_copy_fusion");
+
+        r = xpu::fc_fusion<XPUType, XPUType, XPUType, int16_t>(
+            xpu_ctx,
+            x_ptr_l3,
+            y_ptr,
+            bias_out_ptr,
+            m,
+            n,
+            k,
+            trans_x,
+            trans_y,
+            max_x_ptr,
+            max_y,
+            max_out,
+            ldx,
+            ldy,
+            ldout,
+            1.0f,
+            0.0f,
+            reinterpret_cast<const float*>(bias_ptr),
+            xpu::Activation_t::LINEAR);
+        PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_fusion");
+      } else {
+        r = xpu::fc_fusion<XPUType, XPUType, XPUType, int16_t>(
+            xpu_ctx,
+            x_ptr,
+            y_ptr,
+            bias_out_ptr,
+            m,
+            n,
+            k,
+            trans_x,
+            trans_y,
+            max_x,
+            max_y,
+            max_out,
+            ldx,
+            ldy,
+            ldout,
+            1.0f,
+            0.0f,
+            reinterpret_cast<const float*>(bias_ptr),
+            xpu::Activation_t::LINEAR);
+        PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_fusion");
+      }
     } else {
-      float* bias_tmp_ptr = RAII_GUARD.alloc_l3_or_gm<float>(bias->numel());
-      r = xpu::cast<XPUType, float>(dev_ctx.x_context(),
-                                    bias_ptr,
-                                    bias_tmp_ptr,
-                                    bias->numel());
+      float* bias_tmp_ptr = RAII_GUARD.alloc_l3_or_gm<float>(bias_size);
+      r = xpu::cast<XPUType, float>(
+          dev_ctx.x_context(), bias_ptr, bias_tmp_ptr, bias_size);
       PADDLE_ENFORCE_XDNN_SUCCESS(r, "cast");
-      r = xpu::fc_fusion<XPUType, XPUType, XPUType, int16_t>(xpu_ctx, x_ptr, y_ptr, bias_out_ptr, m, n, k, trans_x,
-        trans_y, max_x, max_y, max_out, ldx, ldy, ldout, 1.0f, 0.0f,
-        bias_tmp_ptr, xpu::Activation_t::LINEAR);
-      PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_fusion");
+      if (copy_to_l3_bias && (k == ldx)) {
+        // r = xpu::findmax2d_copy_fusion<XPUType>(xpu_ctx, x_ptr, max_x_ptr,
+        // x_ptr_l3, m, k, k, k); PADDLE_ENFORCE_XDNN_SUCCESS(r,
+        // "findmax2d_copy_fusion");
+        r = xpu::findmax_copy_fusion<XPUType>(
+            xpu_ctx, x_ptr, max_x_ptr, x_ptr_l3, (m * k));
+        PADDLE_ENFORCE_XDNN_SUCCESS(r, "findmax_copy_fusion");
+
+        r = xpu::fc_fusion<XPUType, XPUType, XPUType, int16_t>(
+            xpu_ctx,
+            x_ptr_l3,
+            y_ptr,
+            bias_out_ptr,
+            m,
+            n,
+            k,
+            trans_x,
+            trans_y,
+            max_x_ptr,
+            max_y,
+            max_out,
+            ldx,
+            ldy,
+            ldout,
+            1.0f,
+            0.0f,
+            bias_tmp_ptr,
+            xpu::Activation_t::LINEAR);
+        PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_fusion");
+      } else {
+        r = xpu::fc_fusion<XPUType, XPUType, XPUType, int16_t>(
+            xpu_ctx,
+            x_ptr,
+            y_ptr,
+            bias_out_ptr,
+            m,
+            n,
+            k,
+            trans_x,
+            trans_y,
+            max_x,
+            max_y,
+            max_out,
+            ldx,
+            ldy,
+            ldout,
+            1.0f,
+            0.0f,
+            bias_tmp_ptr,
+            xpu::Activation_t::LINEAR);
+        PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_fusion");
+      }
     }
 
     // r = xpu::broadcast_add(xpu_ctx,
@@ -144,6 +247,7 @@ class CustomFusedDenseXPUGradKernel : public framework::OpKernel<T> {
     int r = -1;
     bool transx = ctx.Attr<bool>("transx");
     bool transy = ctx.Attr<bool>("transy");
+    // bool use_addto = ctx.Attr<bool>("use_addto");
     auto& dev_ctx = ctx.template device_context<phi::XPUContext>();
     const phi::DenseTensor* dout = ctx.Input<phi::DenseTensor>("DOut");
     const phi::DenseTensor* x = ctx.Input<phi::DenseTensor>("X");
@@ -240,8 +344,29 @@ class CustomFusedDenseXPUGradKernel : public framework::OpKernel<T> {
                                         dout_fc_ptr);
     std::tie(info_dx, info_dy, a_1, b_1, a_2, b_2) = fc_info;
 
+    // const float beta_zero = 0.0f;
+    // const float beta_one = 1.0f;
+    // const float beta_dinput = (use_addto ? beta_one : beta_zero);
+
     if (dx) {
       phi::MatMulXPUFunction<XPUType>(xpu_ctx, a_1, b_1, c_1, info_dx, 1.0f);
+      // int dx_m = info_dx.m;
+      // int dx_n = info_dx.n;
+      // int dx_k = info_dx.k;
+      // int dx_ldx = info_dx.stride_x;
+      // int dx_ldy = info_dx.stride_y;
+      // int dx_ldout = info_dx.stride_out;
+      // bool dx_trans_x = info_dx.trans_x;
+      // bool dx_trans_y = info_dx.trans_y;
+      // float* dx_max_x = info_dx.max_x;
+      // float* dx_max_y = info_dx.max_y;
+      // float* dx_max_out = info_dx.max_out;
+
+      // r = xpu::fc_fusion<XPUType, XPUType, XPUType, int16_t>(xpu_ctx, a_1,
+      // b_1, c_1, dx_m, dx_n, dx_k, dx_trans_x,
+      //     dx_trans_y, dx_max_x, dx_max_y, dx_max_out, dx_ldx, dx_ldy,
+      //     dx_ldout, 1.0f, 0.0f, nullptr, xpu::Activation_t::LINEAR);
+      // PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_fusion");
     }
     if (dy) {
       phi::MatMulXPUFunction<XPUType>(xpu_ctx, a_2, b_2, c_2, info_dy, 1.0f);
@@ -256,27 +381,35 @@ class CustomFusedDenseXPUGradKernel : public framework::OpKernel<T> {
                             {0});
         PADDLE_ENFORCE_XDNN_SUCCESS(r, "reduce_sum");
       }
-      
-    	// int m = info_forward.k;
-    	// int n = info_forward.n;
-    	// int k = info_forward.m;
-    	// int ldx = info_forward.stride_x;
-    	// int ldy = info_forward.n;
-    	// int ldout = info_forward.n;
-    	// bool trans_x = !info_forward.trans_x;
-    	// bool trans_y = info_forward.trans_y;
-    	// float* max_x = nullptr;
-    	// float* max_y = nullptr;
-    	// float* max_out = nullptr;
-      // XPUType* dbias_ptr = reinterpret_cast<XPUType*>(dbias->mutable_data<T>(ctx.GetPlace()));
-      // if ((!dbias) || std::is_same<T, float>::value) {
-      //   r = xpu::fc_dbias_fusion<XPUType, XPUType, XPUType, float, int16_t>(xpu_ctx, reinterpret_cast<const XPUType*>(b_2), reinterpret_cast<const XPUType*>(a_2), reinterpret_cast<XPUType*>(c_2), m, n, k, trans_x,
+
+      // int m = info_forward.k;
+      // int n = info_forward.n;
+      // int k = info_forward.m;
+      // int ldx = info_forward.stride_x;
+      // int ldy = info_forward.n;
+      // int ldout = info_forward.n;
+      // bool trans_x = !info_forward.trans_x;
+      // bool trans_y = info_forward.trans_y;
+      // float* max_x = nullptr;
+      // float* max_y = nullptr;
+      // float* max_out = nullptr;
+      // XPUType* dbias_ptr =
+      // reinterpret_cast<XPUType*>(dbias->mutable_data<T>(ctx.GetPlace())); if
+      // ((!dbias) || std::is_same<T, float>::value) {
+      //   r = xpu::fc_dbias_fusion<XPUType, XPUType, XPUType, float,
+      //   int16_t>(xpu_ctx, reinterpret_cast<const XPUType*>(b_2),
+      //   reinterpret_cast<const XPUType*>(a_2),
+      //   reinterpret_cast<XPUType*>(c_2), m, n, k, trans_x,
       //       trans_y, max_x, max_y, max_out, ldx, ldy, ldout, 1.0f, 0.0f,
       //       nullptr, reinterpret_cast<float*>(dbias_ptr));
       //   PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_dbias_fusion");
       // } else {
-      //   float* dbias_tmp_ptr = RAII_GUARD.alloc_l3_or_gm<float>(dbias->numel());
-      //   r = xpu::fc_dbias_fusion<XPUType, XPUType, XPUType, float, int16_t>(xpu_ctx, reinterpret_cast<const XPUType*>(b_2), reinterpret_cast<const XPUType*>(a_2), reinterpret_cast<XPUType*>(c_2), m, n, k, trans_x,
+      //   float* dbias_tmp_ptr =
+      //   RAII_GUARD.alloc_l3_or_gm<float>(dbias->numel()); r =
+      //   xpu::fc_dbias_fusion<XPUType, XPUType, XPUType, float,
+      //   int16_t>(xpu_ctx, reinterpret_cast<const XPUType*>(b_2),
+      //   reinterpret_cast<const XPUType*>(a_2),
+      //   reinterpret_cast<XPUType*>(c_2), m, n, k, trans_x,
       //       trans_y, max_x, max_y, max_out, ldx, ldy, ldout, 1.0f, 0.0f,
       //       nullptr, dbias_tmp_ptr);
       //   PADDLE_ENFORCE_XDNN_SUCCESS(r, "fc_dbias_fusion");
@@ -287,7 +420,6 @@ class CustomFusedDenseXPUGradKernel : public framework::OpKernel<T> {
       //   PADDLE_ENFORCE_XDNN_SUCCESS(r, "cast");
       // }
     }
-
 
     if (std::getenv("XPU_PADDLE_FC_PRINT") != nullptr) {
       auto tag_end = std::chrono::steady_clock::now();
