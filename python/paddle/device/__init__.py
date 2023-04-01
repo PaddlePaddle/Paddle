b196784e283030588e858726e1517ad2907d877b
baocheny <baochen.yang@intel.com>
2023-04-01 13:38:18 +0800

    make lint happy

--- python/paddle/device/__init__.py
+++ python/paddle/device/__init__.py
@@ -351,7 +351,7 @@ def get_device():
         device = 'npu:' + str(device_id)
     elif isinstance(place, core.IPUPlace):
         num_devices = core.get_ipu_device_count()
-        device = "ipus:{{0-{}}}".format(num_devices - 1)
+        device = f"ipus:{{0-{num_devices - 1}}}"
         device = f"ipus:{{0-{num_devices - 1}}}"
     elif isinstance(place, core.CustomPlace):
         device_id = place.get_device_id()
