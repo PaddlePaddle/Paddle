23565176d4d7c01f0eb991fa686a3cbf82a09f39
gouzi <530971494@qq.com>
2023-04-02 14:13:46 +0800

    [phi] move sequence_pool kernel to phi

--- paddle/fluid/operators/sequence_ops/unity_build_rule.cmake
+++ paddle/fluid/operators/sequence_ops/unity_build_rule.cmake
@@ -13,7 +13,6 @@ register_unity_group(
   sequence_expand_op.cc
   sequence_mask_op.cc
   sequence_pad_op.cc
-  sequence_pool_op.cc
   sequence_expand_as_op.cc
   sequence_reshape_op.cc
   sequence_reverse_op.cc
@@ -31,7 +30,6 @@ register_unity_group(
   sequence_expand_op.cu
   sequence_mask_op.cu
   sequence_pad_op.cu
-  sequence_pool_op.cu
   sequence_expand_as_op.cu
   sequence_reshape_op.cu
   sequence_reverse_op.cu
