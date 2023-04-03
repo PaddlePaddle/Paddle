87ab3b7617cd37f026cf5d9d5f367cbdfc868613
wqgo <1552367872@qq.com>
2023-04-03 14:17:52 +0800

    support auto generate for unpool3d_op

--- paddle/fluid/operators/unpool_op.cc
+++ paddle/fluid/operators/unpool_op.cc
@@ -82,83 +82,12 @@ Paper: http://www.matthewzeiler.com/wp-content/uploads/2017/07/iccv2011.pdf
   }
 };
 
-class Unpool3dOpMaker : public framework::OpProtoAndCheckerMaker {
- public:
-  void Make() override {
-    AddInput(
-        "X",
-        "(Tensor) The input tensor of unpool operator. "
-        "The format of input tensor is NCDHW. Where N is batch size, C is the "
-        "number of channels, D, H and W is the depth, height and width of "
-        "feature.");
-    AddInput(
-        "Indices",
-        "(Tensor) The input tensor of the indices given out by MaxPool3d. "
-        "The format of input tensor is NCDHW. Where N is batch size, C is the "
-        "number of channels, D, H and W is the depth, height and width of "
-        "feature.");
-    AddOutput("Out",
-              "(Tensor) The output tensor of unpool operator."
-              "The format of output tensor is also NCDHW."
-              "Where N is batch size, C is "
-              "the number of channels, D, H and W is the depth, height and "
-              "width of feature.");
-    AddAttr<std::vector<int>>(
-        "ksize",
-        "(vector), the unpooling window size(depth, height, width) "
-        "of unpooling operator.");
-    AddAttr<std::vector<int>>(
-        "strides",
-        "(vector, default:{1, 1, 1}), "
-        "strides (depth, height, width) of unpooling operator.")
-        .SetDefault({1, 1, 1});
-    AddAttr<std::vector<int>>(
-        "paddings",
-        "(vector default:{0, 0,0}), "
-        "paddings (depth, height, width) of unpooling operator.")
-        .SetDefault({0, 0, 0});
-    AddAttr<std::string>(
-        "unpooling_type",
-        "(string), unpooling type, can be \"max\" for max-unpooling ")
-        .InEnum({"max"});
-    AddAttr<std::vector<int>>("output_size",
-                              "(vector, optional). The shape of output.")
-        .SetDefault({0, 0, 0});
-    AddAttr<std::string>(
-        "data_format",
-        "(string, default NCDHW)"
-        "Defaults to \"NCDHW\". Specify the data format of the output data, ")
-        .SetDefault("NCDHW");
-    AddComment(R"DOC(
-Input shape is: $(N, C_{in}, D_{in}, H_{in}, W_{in})$, Output shape is:
-$(N, C_{out}, D_{out}, H_{out}, W_{out})$, where
-$$
-D_{out} = (D_{in}-1) * strides[0] - 2 * paddings[0] + ksize[0] \\
-H_{out} = (H_{in}-1) * strides[1] - 2 * paddings[1] + ksize[1] \\
-W_{out} = (W_{in}-1) * strides[2] - 2 * paddings[2] + ksize[2]
-$$
-)DOC");
-  }
-};
-
 int UnpoolOutputSize(int input_size, int ksize, int padding, int stride) {
   int output_size = (input_size - 1) * stride - 2 * padding + ksize;
   return output_size;
 }
 
 class UnpoolOp : public framework::OperatorWithKernel {
- protected:
-  phi::KernelKey GetExpectedKernelType(
-      const framework::ExecutionContext& ctx) const override {
-    return phi::KernelKey(OperatorWithKernel::IndicateVarDataType(ctx, "X"),
-                          ctx.GetPlace());
-  }
-
- public:
-  using framework::OperatorWithKernel::OperatorWithKernel;
-};
-
-class Unpool3dOp : public framework::OperatorWithKernel {
  protected:
   phi::KernelKey GetExpectedKernelType(
       const framework::ExecutionContext& ctx) const override {
@@ -185,34 +114,7 @@ class UnpoolOpGradMaker : public framework::SingleGradOpMaker<T> {
   }
 };
 
-template <typename T>
-class Unpool3dOpGradMaker : public framework::SingleGradOpMaker<T> {
- public:
-  using framework::SingleGradOpMaker<T>::SingleGradOpMaker;
-  void Apply(GradOpPtr<T> op) const override {
-    op->SetType(this->ForwardOpType() + "_grad");
-    op->SetInput("X", this->Input("X"));
-    op->SetInput("Indices", this->Input("Indices"));
-    op->SetInput("Out", this->Output("Out"));
-    op->SetInput(framework::GradVarName("Out"), this->OutputGrad("Out"));
-    op->SetOutput(framework::GradVarName("X"), this->InputGrad("X"));
-    op->SetAttrMap(this->Attrs());
-  }
-};
-
 class UnpoolOpGrad : public framework::OperatorWithKernel {
- protected:
-  phi::KernelKey GetExpectedKernelType(
-      const framework::ExecutionContext& ctx) const override {
-    return phi::KernelKey(OperatorWithKernel::IndicateVarDataType(ctx, "X"),
-                          ctx.GetPlace());
-  }
-
- public:
-  using framework::OperatorWithKernel::OperatorWithKernel;
-};
-
-class Unpool3dOpGrad : public framework::OperatorWithKernel {
  protected:
   phi::KernelKey GetExpectedKernelType(
       const framework::ExecutionContext& ctx) const override {
@@ -243,22 +145,3 @@ DECLARE_INFER_SHAPE_FUNCTOR(unpool_grad,
                             PD_INFER_META(phi::UnchangedInferMeta));
 
 REGISTER_OPERATOR(unpool_grad, ops::UnpoolOpGrad, UnpoolGradInferShapeFunctor);
-
-DECLARE_INFER_SHAPE_FUNCTOR(unpool,
-                            Unpool3dInferShapeFunctor,
-                            PD_INFER_META(phi::Unpool3dInferMeta));
-
-REGISTER_OPERATOR(unpool3d,
-                  ops::Unpool3dOp,
-                  ops::Unpool3dOpMaker,
-                  ops::Unpool3dOpGradMaker<paddle::framework::OpDesc>,
-                  ops::Unpool3dOpGradMaker<paddle::imperative::OpBase>,
-                  Unpool3dInferShapeFunctor);
-
-DECLARE_INFER_SHAPE_FUNCTOR(unpool3d_grad,
-                            Unpool3dGradInferShapeFunctor,
-                            PD_INFER_META(phi::UnchangedInferMeta));
-
-REGISTER_OPERATOR(unpool3d_grad,
-                  ops::Unpool3dOpGrad,
-                  Unpool3dGradInferShapeFunctor);
