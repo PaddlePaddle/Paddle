3409ed9125c775475d78df42b844e5629937905c
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-04-03 06:46:55 +0000

    fix bug

--- python/paddle/incubate/autograd/primx.py
+++ python/paddle/incubate/autograd/primx.py
@@ -632,17 +632,17 @@ def _lower_composite(
                     f'when replace origin op {op_name} with composite rule, num of origin outs should be equal to new outs, '
                     f'but len(orig_outs) = {len(orig_outs)} and len(new_outs) = {len(new_outs)}'
                 )
-                if (None in orig_outs or None in new_outs) and (
-                    op_name not in ops_contain_none
-                ):
-                    raise ValueError(
-                        f"op {op_name} should not contain any None value. original outs={orig_outs} and its composite rule outs={new_outs}"
-                    )
 
                 for orig_out, new_out in zip(
                     orig_outs,
                     new_outs,
                 ):
+                    if (orig_out is None or new_out is None) and (
+                        op_name not in ops_contain_none
+                    ):
+                        raise ValueError(
+                            f"op {op_name} should not contain any None value. original outs={orig_outs} and its composite rule outs={new_outs}"
+                        )
                     if orig_out is None:
                         # to keep same as phi op definition, orig_out may receive None
                         continue
