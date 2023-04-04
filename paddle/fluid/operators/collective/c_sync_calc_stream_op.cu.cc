a496ab17c1d5350e248bd563463eb94180fdedb5
huangjiyi <43315610+huangjiyi@users.noreply.github.com>
2023-04-04 17:00:19 +0800

    update

--- paddle/fluid/operators/collective/c_sync_calc_stream_op.cu.cc
+++ paddle/fluid/operators/collective/c_sync_calc_stream_op.cu.cc
@@ -20,6 +20,8 @@ PD_REGISTER_STRUCT_KERNEL(c_sync_calc_stream,
                           GPU,
                           ALL_LAYOUT,
                           ops::CSyncCalcStreamKernel,
-                          float double,
+                          float,
+                          double,
                           int,
-                          int64_t plat::float16) {}
+                          int64_t,
+                          plat::float16) {}
