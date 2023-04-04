0d38e8f79c4dd3fde6e8b4d0df0346b39ee31249
wjj19950828 <wjjisloser@163.com>
2023-04-04 06:17:00 +0000

    deal with comments

--- python/paddle/fluid/tests/unittests/test_isfinite_op.py
+++ python/paddle/fluid/tests/unittests/test_isfinite_op.py
@@ -15,8 +15,7 @@
 import unittest
 
 import numpy as np
-from eager_op_test import OpTest
-from op_test import convert_float_to_uint16
+from eager_op_test import OpTest, convert_float_to_uint16
 
 from paddle.fluid import core
 
