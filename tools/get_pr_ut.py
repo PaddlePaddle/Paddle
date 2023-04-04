3b5f5439905bc081f4456f77cff052ced94d8642
risemeup1 <62429225+risemeup1@users.noreply.github.com>
2023-04-04 14:15:08 +0800

    Update get_pr_ut.py

--- tools/get_pr_ut.py
+++ tools/get_pr_ut.py
@@ -350,7 +350,7 @@ class PRChecker:
                     filterFiles.append(filename)
                 else:
                     file_list.append(filename)
-            if filename.startswith(PADDLE_ROOT + 'test/'):
+            elif filename.startswith(PADDLE_ROOT + 'test/'):
                 file_list.append(filename)
             else:
                 if file_dict[filename] == 'added':
