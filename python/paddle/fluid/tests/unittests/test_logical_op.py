4a7a077d36d6d808212866d2aa742f27027e4e21
wjj19950828 <wjjisloser@163.com>
2023-04-04 11:52:10 +0000

    fixed for ci

--- python/paddle/fluid/tests/unittests/test_logical_op.py
+++ python/paddle/fluid/tests/unittests/test_logical_op.py
@@ -15,7 +15,7 @@
 import unittest
 
 import numpy as np
-from op_test import convert_float_to_uint16
+from eager_op_test import convert_float_to_uint16
 
 import paddle
 from paddle.framework import _non_static_mode
