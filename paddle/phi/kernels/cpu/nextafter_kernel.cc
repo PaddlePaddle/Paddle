28781c98305b7cde53d51a77499f1aa758a4c32c
enkilee <luhputu0815@gmail.com>
2023-04-05 14:24:08 +0000

    fix

--- paddle/phi/kernels/cpu/nextafter_kernel.cc
+++ paddle/phi/kernels/cpu/nextafter_kernel.cc
@@ -13,32 +13,10 @@
 // limitations under the License.
 
 #include "paddle/phi/kernels/nextafter_kernel.h"
-#include <algorithm>
+
 #include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/funcs/math.h"
-
-namespace phi {
-
-template <typename T, typename Context>
-void NextafterKernel(const Context& ctx,
-                     const DenseTensor& x,
-                     const DenseTensor& y,
-                     DenseTensor* out) {
-  if (x.numel() == 0 || y.numel() == 0) {
-    return;
-  }
-  auto out_data = ctx.template Alloc<T>(out);
-  auto x_data = x.data<T>();
-  auto y_data = y.data<T>();
-  int x_numel = x.numel();
-
-  for (int i = 0; i < x_numel; ++i) {
-    out_data[i] = std::nextafter(x_data[i], y_data[i]);
-  }
-}
-
-}  // namespace phi
+#include "paddle/phi/kernels/impl/nextafter_kernel_impl.h"
 
 PD_REGISTER_KERNEL(
     nextafter, CPU, ALL_LAYOUT, phi::NextafterKernel, float, double) {}
