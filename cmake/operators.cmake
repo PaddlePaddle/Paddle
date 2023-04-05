341ba108511194fc83c2769364de10e6e1a60dae
jjyaoao <88936287+jjyaoao@users.noreply.github.com>
2023-04-05 11:44:40 +0000

    delete paddle/fluid/operators/*_npu.*

--- cmake/operators.cmake
+++ cmake/operators.cmake
@@ -62,7 +62,6 @@ function(op_library TARGET)
   set(hip_cc_srcs)
   set(xpu_cc_srcs)
   set(xpu_kp_cc_srcs)
-  set(npu_cc_srcs)
   set(mlu_cc_srcs)
   set(cudnn_cu_cc_srcs)
   set(miopen_cu_cc_srcs)
@@ -321,35 +320,25 @@ function(op_library TARGET)
       DEPS ${op_library_DEPS} ${op_common_deps})
   else()
     # Unity Build relies on global option `WITH_UNITY_BUILD` and local option `UNITY`.
-    if(WITH_UNITY_BUILD AND op_library_UNITY)
-      # Combine the cc source files.
-      compose_unity_target_sources(
-        ${UNITY_TARGET}
-        cc
-        ${cc_srcs}
-        ${mkldnn_cc_srcs}
-        ${xpu_cc_srcs}
-        ${npu_cc_srcs}
-        ${mlu_cc_srcs})
-      if(TARGET ${UNITY_TARGET})
-        # If `UNITY_TARGET` exists, add source files to `UNITY_TARGET`.
-        target_sources(${UNITY_TARGET} PRIVATE ${unity_target_cc_sources})
-      else()
-        # If `UNITY_TARGET` does not exist, create `UNITY_TARGET` with source files.
-        cc_library(
-          ${UNITY_TARGET}
-          SRCS ${unity_target_cc_sources}
-          DEPS ${op_library_DEPS} ${op_common_deps})
-      endif()
-      # Add alias library to handle dependencies.
-      add_library(${TARGET} ALIAS ${UNITY_TARGET})
+
+    # Combine the cc source files.
+    compose_unity_target_sources(
+      ${UNITY_TARGET} cc ${cc_srcs} ${mkldnn_cc_srcs} ${xpu_cc_srcs}
+      ${mlu_cc_srcs})
+    if(TARGET ${UNITY_TARGET})
+      # If `UNITY_TARGET` exists, add source files to `UNITY_TARGET`.
+      target_sources(${UNITY_TARGET} PRIVATE ${unity_target_cc_sources})
     else()
+      # If `UNITY_TARGET` does not exist, create `UNITY_TARGET` with source files.
       cc_library(
-        ${TARGET}
-        SRCS ${cc_srcs} ${mkldnn_cc_srcs} ${xpu_cc_srcs} ${npu_cc_srcs}
-             ${mlu_cc_srcs}
+        ${UNITY_TARGET}
+        SRCS ${unity_target_cc_sources}
         DEPS ${op_library_DEPS} ${op_common_deps})
     endif()
+    # Add alias library to handle dependencies.
+    add_library(${TARGET} ALIAS ${UNITY_TARGET})
+
+  endif()
   endif()
 
   list(LENGTH cu_srcs cu_srcs_len)
@@ -359,7 +348,6 @@ function(op_library TARGET)
   list(LENGTH mkldnn_cc_srcs mkldnn_cc_srcs_len)
   list(LENGTH xpu_cc_srcs xpu_cc_srcs_len)
   list(LENGTH miopen_cu_cc_srcs miopen_cu_cc_srcs_len)
-  list(LENGTH npu_cc_srcs npu_cc_srcs_len)
   list(LENGTH mlu_cc_srcs mlu_cc_srcs_len)
 
   # Define operators that don't need pybind here.
@@ -594,7 +582,6 @@ function(register_operators)
     "*_op.cc")
   string(REPLACE "_mkldnn" "" OPS "${OPS}")
   string(REPLACE "_xpu" "" OPS "${OPS}")
-  string(REPLACE "_npu" "" OPS "${OPS}")
   string(REPLACE "_mlu" "" OPS "${OPS}")
   string(REPLACE ".cc" "" OPS "${OPS}")
   list(REMOVE_DUPLICATES OPS)
