cf2c5afee2e7d9f8c82fde156c390bac4a823b6f
superwinner1 <82640284+superwinner1@users.noreply.github.com>
2023-04-06 23:23:55 +0800

    'fix'

--- paddle/phi/kernels/gpu/erf_kernel.cu
+++ paddle/phi/kernels/gpu/erf_kernel.cu
@@ -16,6 +16,7 @@ limitations under the License. */
 
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/common/float16.h"
+#include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/impl/erf_kernel_impl.h"
 
