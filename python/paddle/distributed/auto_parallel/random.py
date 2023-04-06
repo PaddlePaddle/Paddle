aa1e626cc4764407ef169db47a34c2d04ad08cf8
JZ-LIANG <jianzhongliang10@gmail.com>
2023-04-06 15:55:27 +0800

    fixed bug

--- python/paddle/distributed/auto_parallel/random.py
+++ python/paddle/distributed/auto_parallel/random.py
@@ -38,7 +38,7 @@ def enable_auto_rand_ctrl():
     _enable_random_control = True
 
 
-def auto_parallel_random_seed(seed):
+def paralle_manual_seed(seed):
     """Enable auto parallel random control.
     Random control maintain the randomness when tensor is distributed across devices on a Mesh(any order).
         * Independency: If tensor is **Sharded** on a Mesh dimension, Devices along that Mesh dimension should have Different randomness.
