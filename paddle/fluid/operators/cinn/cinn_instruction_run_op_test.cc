058ca61de56389f9577f764bc883caf3c328af7e
huangjiyi <43315610+huangjiyi@users.noreply.github.com>
2023-04-06 18:59:33 +0800

    register fluid kerenls to phi [part 3] (#52101)
    * update
    
    * fix compile bug
    
    * fix bug
    
    * fix bug
    
    * revert crop_op
    
    * fix xpu compile
    
    * fix cinn compile
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * update
    
    * update
    
    * update

--- paddle/fluid/operators/cinn/cinn_instruction_run_op_test.cc
+++ paddle/fluid/operators/cinn/cinn_instruction_run_op_test.cc
@@ -26,12 +26,16 @@ limitations under the License. */
 #include "paddle/fluid/platform/init.h"
 #include "paddle/phi/core/kernel_registry.h"
 
-USE_OP(cinn_launch);
-USE_OP(cinn_instruction_run);
+USE_OP_ITSELF(cinn_launch);
+USE_OP_ITSELF(cinn_instruction_run);
 USE_OP_ITSELF(elementwise_add);
 
+PD_DECLARE_KERNEL(cinn_launch, CPU, ALL_LAYOUT);
+PD_DECLARE_KERNEL(cinn_instruction_run, CPU, ALL_LAYOUT);
 PD_DECLARE_KERNEL(add, CPU, ALL_LAYOUT);
 #ifdef PADDLE_WITH_CUDA
+PD_DECLARE_KERNEL(cinn_launch, GPU, ALL_LAYOUT);
+PD_DECLARE_KERNEL(cinn_instruction_run, GPU, ALL_LAYOUT);
 PD_DECLARE_KERNEL(add, KPS, ALL_LAYOUT);
 #endif
 
