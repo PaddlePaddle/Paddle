c44cf549fb4bd326843974b3aa73740601cfbca0
jiahongyu <jiahongyu@baidu.com>
2023-04-06 11:57:32 +0000

    [enforce.h Decouple logging.h] Delete glog/logging.h from enforce.h

--- paddle/phi/kernels/cpu/adam_kernel.cc
+++ paddle/phi/kernels/cpu/adam_kernel.cc
@@ -16,6 +16,8 @@
 
 #include <vector>
 
+#include "glog/logging.h"
+
 #include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/tensor_utils.h"
--- paddle/phi/kernels/gpu/batch_norm_grad_kernel.cu
+++ paddle/phi/kernels/gpu/batch_norm_grad_kernel.cu
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "glog/logging.h"
+
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/backends/gpu/gpu_dnn.h"
 #include "paddle/phi/common/layout.h"
