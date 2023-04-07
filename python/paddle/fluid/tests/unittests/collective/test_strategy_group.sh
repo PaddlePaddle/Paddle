ca4e2ac35b3018bd52fc3cbed94a157699d140a4
haohongxiang <haohongxiang@baidu.com>
2023-04-07 08:21:01 +0000

    update codestyle

--- python/paddle/fluid/tests/unittests/collective/test_strategy_group.sh
+++ python/paddle/fluid/tests/unittests/collective/test_strategy_group.sh
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+
 set -e
 
 CUDA_VISIBLE_DEVICES=0,1 python -m paddle.distributed.launch --gpus=0,1 strategy_group.py
