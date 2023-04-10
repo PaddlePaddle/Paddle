ddd26dcaf0c4e46f6bc0da645c69a92a5848c258
zhangyuqin1998 <2368719370@qq.com>
2023-04-10 00:49:33 +0000

    fix

--- paddle/phi/kernels/cpu/gather_grad_kernel.cc
+++ paddle/phi/kernels/cpu/gather_grad_kernel.cc
@@ -51,11 +51,9 @@ void GatherGradKernel(const Context& dev_ctx,
   if (x_grad->numel() == 0) return;
 
   if (index_type == phi::DataType::INT32) {
-    phi::funcs::ScatterAssignAdd<T, int32_t>(
-        dev_ctx, out_grad, index, x_grad);
+    phi::funcs::ScatterAssignAdd<T, int32_t>(dev_ctx, out_grad, index, x_grad);
   } else if (index_type == phi::DataType::INT64) {
-    phi::funcs::ScatterAssignAdd<T, int64_t>(
-        dev_ctx, out_grad, index, x_grad);
+    phi::funcs::ScatterAssignAdd<T, int64_t>(dev_ctx, out_grad, index, x_grad);
   } else {
     PADDLE_THROW(phi::errors::InvalidArgument(
         "The data type of Input(Index) of gather_grad must be int32 or int64 "
