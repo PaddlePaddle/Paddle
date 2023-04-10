451c32df0c14c30ae5848dc139920254f13f66ce
jiahongyu <jiahongyu@baidu.com>
2023-04-10 12:48:59 +0000

    Add logging.h for all files in kernel directory

--- paddle/phi/kernels/gpu/top_k_kernel.cu
+++ paddle/phi/kernels/gpu/top_k_kernel.cu
@@ -14,6 +14,8 @@
 
 #include "paddle/phi/kernels/top_k_kernel.h"
 
+#include "glog/logging.h"
+
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/tensor_utils.h"
