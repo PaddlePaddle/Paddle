3c0b1795c413b55e582bcd5b0c2eb00082d793eb
HongyuJia <jiahongyu@baidu.com>
2023-04-10 13:08:31 +0800

    [enforce.h Decouple gflags.h] Move gflags.h from enforce.h to enforce.cc (#52573)
    * [enforce.h Decouple gflags.h] Move gflags.h from enforce.h to enforce.cc
    
    * Add gflags.h for other files
    
    * Add gflags.h for other files
    
    * Add gflags.h for blas_impl.hip.h
    
    * Add gflags.h for miopen_helper.h

--- paddle/phi/infermeta/unary.cc
+++ paddle/phi/infermeta/unary.cc
@@ -17,6 +17,7 @@ limitations under the License. */
 #include <algorithm>
 #include <set>
 
+#include "gflags/gflags.h"
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/common/type_traits.h"
 #include "paddle/phi/core/enforce.h"
