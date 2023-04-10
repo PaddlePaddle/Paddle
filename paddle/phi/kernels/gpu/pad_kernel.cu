3ee2b23713a139fcc5bce6d40b6b57e08621b0df
cyberslack_lee <luhputu0815@gmail.com>
2023-04-10 14:36:14 +0800

    【Hackathon4 No58】fix exponential and pad (#51300)

--- paddle/phi/kernels/gpu/pad_kernel.cu
+++ paddle/phi/kernels/gpu/pad_kernel.cu
@@ -28,5 +28,6 @@ PD_REGISTER_KERNEL(pad,
                    int,
                    int64_t,
                    phi::dtype::float16,
+                   phi::dtype::bfloat16,
                    phi::dtype::complex<float>,
                    phi::dtype::complex<double>) {}
