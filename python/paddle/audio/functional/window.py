1c6175ef13e389a3b13b609463f96621a1a8bc28
zhengqiwen1997 <zhengqiwen_au@163.com>
2023-04-11 07:55:39 +0000

    test=allcase

--- python/paddle/audio/functional/window.py
+++ python/paddle/audio/functional/window.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 import math
-from typing import List, Tuple, Union
+from typing import Tuple, Union
 
 import paddle
 from paddle import Tensor
