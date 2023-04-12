f617fb0df43b7bcce8804f7ae86ca66a63b56dff
huangjiyi <43315610+huangjiyi@users.noreply.github.com>
2023-04-12 20:09:29 +0800

    update

--- paddle/phi/tools/get_kernel_signatures.py
+++ paddle/phi/tools/get_kernel_signatures.py
@@ -55,19 +55,33 @@ class KernelSignatureSearcher:
     )
 
     srcs_dir = ['cpu', 'gpu', 'xpu', 'onednn', 'gpudnn', 'kps']
+    build_path = None
+
     filter = {"kernel_name": []}
 
-    def __init__(self, base_path, build_path):
-        self.base_path = base_path
-        self.build_path = build_path
+    def __init__(self, search_path):
+        self.search_path = search_path
         self.kernel_func_map = {}
         self.func_signature_map = {}
 
+        self.search_kernel_signature()
+        self.search_kernel_registration(search_path)
+        self.filter_result()
+
     @classmethod
-    def search(cls, base_path, build_path):
-        searcher = cls(base_path, build_path)
-        searcher.search_kernel_signature()
-        searcher.search_kernel_registration(base_path)
+    def filter_kernel(cls, kernel_name: str or list[str]):
+        if isinstance(kernel_name, str):
+            cls.filter["kernel_name"].append(kernel_name)
+        elif isinstance(kernel_name, list):
+            cls.filter["kernel_name"].extend(kernel_name)
+        else:
+            raise ValueError("kernel_name must be str or list[str].")
+
+    @classmethod
+    def search(cls, search_path):
+        if cls.build_path is None:
+            raise ValueError("Please set build_path first.")
+        searcher = cls(search_path)
         kernel_func_df = pd.DataFrame(
             list(searcher.kernel_func_map.items()),
             columns=['kernel_name', 'kernel_func'],
@@ -80,15 +94,19 @@ class KernelSignatureSearcher:
             kernel_func_df, func_signature_df, on='kernel_func', how='left'
         )[['kernel_name', 'kernel_signature']]
 
+    def filter_result(self):
+        for kernel_name in self.filter["kernel_name"]:
+            if kernel_name in self.kernel_func_map:
+                del self.kernel_func_map[kernel_name]
+
     def search_kernel_signature(self):
-        for file in os.listdir(self.base_path):
-            if file.endswith("_kernel.h"):
-                file_content = open(osp.join(self.base_path, file), 'r').read()
-                match_results = re.findall(
-                    self.kernel_sig_pattern, file_content
-                )
-                for match_result in match_results:
-                    self.func_signature_map[match_result[1]] = match_result[0]
+        for file in os.listdir(self.search_path):
+            if not file.endswith("_kernel.h"):
+                continue
+            file_content = open(osp.join(self.search_path, file), 'r').read()
+            match_results = re.findall(self.kernel_sig_pattern, file_content)
+            for match_result in match_results:
+                self.func_signature_map[match_result[1]] = match_result[0]
 
     def search_kernel_registration(self, path):
         self.processed_file_path = osp.join(
@@ -96,6 +114,7 @@ class KernelSignatureSearcher:
         )
         for file in os.listdir(path):
             file_path = osp.join(path, file)
+            # only search src files under specific srcs_dir
             if file in self.srcs_dir:
                 self.search_kernel_registration(file_path)
             if osp.isdir(file_path):
@@ -107,36 +126,37 @@ class KernelSignatureSearcher:
 
     def _search_kernel_registration(self, file_path, file):
         file_content = open(file_path, 'r').read()
-        self.file_preprocessed = False
+        self.header_content = None
         # if some kernel registration is in macro, preprocess macro first
+        self.file_preprocessed = False
         if re.search(self.macro_kernel_reg_pattern, file_content):
             file_content = preprocess_macro(
                 file_content, self.processed_file_path
             )
             self.file_preprocessed = True
+        # search kernel registration
         match_results = re.findall(self.kernel_reg_pattern, file_content)
-        self.header_context = None
         for match_result in match_results:
             kernel_name = match_result[1]
-            if (
-                kernel_name not in self.filter["kernel_name"]
-                and kernel_name not in self.kernel_func_map
-            ):
-                kernel_func = match_result[-1].split("<")[0].split("::")[-1]
-                self.kernel_func_map[kernel_name] = kernel_func
-                if kernel_func not in self.func_signature_map:
-                    kernel_signature = self.search_target_kernel_signature(
-                        kernel_func, file, file_content
-                    )
-                    self.func_signature_map[kernel_func] = kernel_signature
-                    if kernel_signature is None:
-                        warnings.warn(
-                            "Can't find kernel signature for kernel: "
-                            + kernel_func
-                            + ", which is registered in file: "
-                            + file_path
-                            + "."
-                        )
+            if kernel_name in self.kernel_func_map:
+                continue
+            kernel_func = match_result[-1].split("<")[0].split("::")[-1]
+            self.kernel_func_map[kernel_name] = kernel_func
+            if kernel_func in self.func_signature_map:
+                continue
+            # if target kernel signature is not found in header file, search
+            # it in current src file, or preprocess macro and search again
+            kernel_signature = self.search_target_kernel_signature(
+                kernel_func, file, file_content
+            )
+            self.func_signature_map[kernel_func] = kernel_signature
+            if kernel_signature is None:
+                warnings.warn(
+                    "Can't find kernel signature for kernel: "
+                    + kernel_func
+                    + ", which is registered in file: "
+                    + file_path
+                )
 
     def search_target_kernel_signature(self, kernel_func, file, file_content):
         target_kernel_signature_pattern = self.kernel_sig_pattern.replace(
@@ -159,29 +179,45 @@ class KernelSignatureSearcher:
             if kernel_signature is not None:
                 return kernel_signature
         # expand macro in according kernel header file and search again
-        if self.header_context is None:
-            header_path = osp.join(self.base_path, file.split('.')[0] + '.h')
+        if self.header_content is None:
+            header_path = osp.join(self.search_path, file.split('.')[0] + '.h')
             if osp.exists(header_path):
-                self.header_context = open(header_path, 'r').read()
-        if self.header_context is not None:
-            self.header_context = preprocess_macro(
-                self.header_context, self.processed_file_path
+                self.header_content = open(header_path, 'r').read()
+        if self.header_content is not None:
+            self.header_content = preprocess_macro(
+                self.header_content, self.processed_file_path
             )
             kernel_signature = search_pattern(
-                target_kernel_signature_pattern, self.header_context
+                target_kernel_signature_pattern, self.header_content
             )
             if kernel_signature is not None:
                 return kernel_signature
         return None
 
 
-if __name__ == "__main__":
+def get_kernel_signatures():
+    """
+    Get kernel signatures of all kernels registered in phi/kernels, and
+    generate a csv file named 'kernel_signatures.csv' in Paddle/build.
+
+    Example:
+        >>> # filter kernel signatures of max and bilinear
+        >>> KernelSignatureSearcher.filter_kernel(['reverse_array', 'max'])
+        >>> get_kernel_signatures()
+    """
     Paddle_path = osp.abspath(osp.join(osp.dirname(__file__), '../../..'))
+    if Paddle_path.split('/')[-1] != 'Paddle':
+        raise ValueError('Paddle path error.')
     build_path = osp.join(Paddle_path, 'build')
     os.makedirs(build_path, exist_ok=True)
-    base_path = osp.join(Paddle_path, 'paddle/phi/kernels')
+    KernelSignatureSearcher.build_path = build_path
 
-    kernel_signature_df = KernelSignatureSearcher.search(base_path, build_path)
+    base_path = osp.join(Paddle_path, 'paddle/phi/kernels')
+    kernel_signature_df = KernelSignatureSearcher.search(base_path)
+
+    # Because phi/kernels has some independent subdirs, whose kernel names
+    # (in different namespaces) may conflict with main directory or other
+    # subdirs, so we need to search them separately.
     indenpendent_subdir = [
         'fusion',
         'legacy',
@@ -191,10 +227,16 @@ if __name__ == "__main__":
     ]
     for subdir in indenpendent_subdir:
         sub_path = osp.join(base_path, subdir)
-        sub_df = KernelSignatureSearcher.search(sub_path, build_path)
+        sub_df = KernelSignatureSearcher.search(sub_path)
         kernel_signature_df = pd.concat(
             [kernel_signature_df, sub_df], ignore_index=True
         )
+
     output_csv_path = osp.join(build_path, 'kernel_signatures.csv')
     kernel_signature_df.to_csv(output_csv_path, index=False)
     print(kernel_signature_df)
+
+
+if __name__ == "__main__":
+    # KernelSignatureSearcher.filter_kernel(['reverse_array', 'max'])
+    get_kernel_signatures()
