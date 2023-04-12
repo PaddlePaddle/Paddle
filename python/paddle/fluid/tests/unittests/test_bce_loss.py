80804640306fb2d271d5d32fe26a5a1bcba8142e
mrcangye <chenloong@88.com>
2023-04-12 10:31:12 +0800

    update

--- python/paddle/fluid/tests/unittests/test_bce_loss.py
+++ python/paddle/fluid/tests/unittests/test_bce_loss.py
@@ -282,6 +282,7 @@ class TestBceLossOpCase2(OpTest):
 class TestBceLossOpFloat16(TestBceLossOp):
     def setUp(self):
         self.init_test_case()
+        self.python_api = bce_wrapper
         input_np = np.random.uniform(0.1, 0.8, self.shape).astype("float16")
         label_np = np.random.randint(0, 2, self.shape).astype("float16")
         output_np = bce_loss(input_np, label_np)
