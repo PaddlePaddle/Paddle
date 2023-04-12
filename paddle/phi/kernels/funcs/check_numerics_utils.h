bd18c4639f3e47227c952838b15a85ae51ae5137
Liu Yiqun <liuyiqun01@baidu.com>
2023-04-12 04:09:37 +0000

    Fix cmake and compiling error.

--- paddle/phi/kernels/funcs/check_numerics_utils.h
+++ paddle/phi/kernels/funcs/check_numerics_utils.h
@@ -17,6 +17,7 @@
 #include <omp.h>
 #include <fstream>
 
+#include "gflags/gflags.h"
 #include "paddle/phi/common/amp_type_traits.h"
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/core/enforce.h"
