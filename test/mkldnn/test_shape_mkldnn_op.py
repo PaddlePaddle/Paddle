608388f2b4e7e99496c6f229d043234efa9bc002
qunyang <qun.yang@intel.com>
2023-04-12 16:28:54 +0800

    support 0d tensor for shape and squeeze onednn kernel

--- test/mkldnn/test_shape_mkldnn_op.py
+++ test/mkldnn/test_shape_mkldnn_op.py
@@ -21,7 +21,6 @@ from paddle.fluid import core
 from paddle.fluid.tests.unittests.eager_op_test import OpTest, OpTestTool
 
 
-@OpTestTool.skip_if_not_cpu_bf16()
 class TestShape3DFP32OneDNNOp(OpTest):
     def setUp(self):
         self.op_type = "shape"
@@ -38,6 +37,13 @@ class TestShape3DFP32OneDNNOp(OpTest):
         self.check_output_with_place(core.CPUPlace())
 
 
+class TestShape0DFP32OneDNNOp(TestShape3DFP32OneDNNOp):
+    def config(self):
+        self.shape = []
+        self.dtype = np.float32
+
+
+@OpTestTool.skip_if_not_cpu_bf16()
 class TestShape6DBF16OneDNNOp(TestShape3DFP32OneDNNOp):
     def config(self):
         self.shape = [10, 2, 3, 4, 5, 2]
