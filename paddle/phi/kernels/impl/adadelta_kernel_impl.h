7140e8d29a0ee32a1bb757607570c3a2308cf038
wangzhen38 <wangzhen38@baidu.com>
2023-04-12 01:08:38 +0000

    del notes

--- paddle/phi/kernels/impl/adadelta_kernel_impl.h
+++ paddle/phi/kernels/impl/adadelta_kernel_impl.h
@@ -63,11 +63,6 @@ void AdadeltaKernel(const Context& dev_ctx,
       -(((eigen_avg_squared_update + epsilon_).sqrt()) /
         ((eigen_avg_squared_grad_out + epsilon_).sqrt()) * eigen_grad_cast);
   Eigen::DSizes<int, 1> m_dsize(avg_squared_update_out->numel());
-  // if (paddle::platform::is_cpu_place(dev_ctx.GetPlace())) {
-  //   auto* lr = learning_rate.data<T>();
-  //   eigen_param_out.device(place) =
-  //       eigen_param + lr[0] * update.template cast<T>();
-  // } else {
   auto lr = EigenVector<MPDType>::Flatten(learning_rate);
   if (multi_precision) {
     auto eigen_master_param_out =
@@ -83,7 +78,6 @@ void AdadeltaKernel(const Context& dev_ctx,
     eigen_param_out.device(place) =
         eigen_param + (lr.broadcast(m_dsize) * update).template cast<T>();
   }
-  //}
   eigen_avg_squared_update_out.device(place) =
       rho_ * eigen_avg_squared_update + (1 - rho_) * update.square();
 }
