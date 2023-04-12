42c3617699e98d7cc32221ee6ddc34d3b9ad1ce5
jjyaoao <88936287+jjyaoao@users.noreply.github.com>
2023-04-12 07:11:45 +0000

    delete WITH_ASCEND_CL

--- paddle/fluid/operators/controlflow/while_op_helper.cc
+++ paddle/fluid/operators/controlflow/while_op_helper.cc
@@ -235,7 +235,7 @@ bool GetCondData(const phi::DenseTensor &cond) {
   PADDLE_THROW(platform::errors::PreconditionNotMet(
       "This version of PaddlePaddle does NOT support GPU/NPU/XPU but got "
       "GPU/NPU/XPU tensor Cond in WhileOp. Please compile WITH_GPU or "
-      "WITH_ASCEND_CL or WITH_XPU option."));
+      "WITH_XPU option."));
 #endif
   return cpu_cond->data<bool>()[0];
 }
