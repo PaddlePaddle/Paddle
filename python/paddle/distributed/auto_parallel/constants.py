d8118d747667dfb39fe1760b16464f78f16bf804
JZ-LIANG <jianzhongliang10@gmail.com>
2023-04-13 15:32:01 +0800

    fixed unitest

--- python/paddle/distributed/auto_parallel/constants.py
+++ python/paddle/distributed/auto_parallel/constants.py
@@ -75,6 +75,7 @@ set_field_default_config(AMP, "custom_black_list", [])
 set_field_default_config(AMP, "custom_black_varnames", [])
 set_field_default_config(AMP, "use_fp16_guard", False)
 set_field_default_config(AMP, "use_bf16_guard", False)
+set_field_default_config(AMP, "use_optimizer_fp16", False)
 
 #########################################
 # sharding configuration
