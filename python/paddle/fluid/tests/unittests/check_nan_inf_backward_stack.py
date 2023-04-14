b4eb0c9b400a7f578515d6afd67d1df5a86034a2
niuliling123 <sdust_niull@163.com>
2023-04-14 09:23:45 +0000

    update

--- python/paddle/fluid/tests/unittests/check_nan_inf_backward_stack.py
+++ python/paddle/fluid/tests/unittests/check_nan_inf_backward_stack.py
@@ -17,14 +17,12 @@ import paddle
 
 
 def main():
-    paddle.disable_static()
     paddle.set_flags({"FLAGS_check_nan_inf": 1, "FLAGS_check_nan_inf_level": 0})
     cpu_place = paddle.CPUPlace()
     x = paddle.to_tensor([1, 0.0, 3], stop_gradient=False, place=cpu_place)
     y = paddle.to_tensor([0.2, 0.0, 0.5], place=cpu_place)
     z = paddle.pow(x, y)
     paddle.autograd.backward([z])
-    paddle.enable_static()
 
 
 if __name__ == "__main__":
