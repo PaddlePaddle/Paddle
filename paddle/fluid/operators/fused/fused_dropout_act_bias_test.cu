95c0bd2e2f81d482c1746291238db11a0f14e827
EmmonsCurse <1577972691@qq.com>
2023-04-14 17:18:18 +0800

    update fused_dropout_act_bias_test

--- paddle/fluid/operators/fused/fused_dropout_act_bias_test.cu
+++ paddle/fluid/operators/fused/fused_dropout_act_bias_test.cu
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include <time.h>
+#include <float.h>
 
 #include <random>
 #include <vector>
