6d5548286a2bca246875195bfb816bd765ae3f84
GGBond8488 <857631483@qq.com>
2023-04-17 03:14:56 +0000

    fix tets,test=allcase

--- python/paddle/fluid/tests/unittests/test_zero_dim_tensor.py
+++ python/paddle/fluid/tests/unittests/test_zero_dim_tensor.py
@@ -4324,7 +4324,7 @@ class TestSundryAPIStatic(unittest.TestCase):
         self.assertEqual(res[0].shape, ())
         np.testing.assert_array_equal(res[0], np.array(1))
 
-    @prog_scope
+    @prog_scope()
     def test_trace(self):
         x = paddle.to_tensor([[3, 2], [1, 9]], dtype="float32")
         x.stop_gradient = False
@@ -4336,7 +4336,7 @@ class TestSundryAPIStatic(unittest.TestCase):
         self.assertTrue(res[0].shape, ())
         np.testing.assert_allclose(out, np.array(12))
 
-    @prog_scope
+    @prog_scope()
     def test_cond(self):
         pass
         # use paddle.sum, paddle.max, paddle.min
