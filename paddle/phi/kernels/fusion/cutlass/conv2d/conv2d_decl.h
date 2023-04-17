188cb3a91488fcd127e66feca8d9e0d3e88461d7
zhoutianzi666 <17801055074@163.com>
2023-04-17 00:58:39 +0000

    modify decl.h and util.cu

--- paddle/phi/kernels/fusion/cutlass/conv2d/conv2d_decl.h
+++ paddle/phi/kernels/fusion/cutlass/conv2d/conv2d_decl.h
@@ -60,9 +60,9 @@ void Conv2dBiasSilu(const ConvAllParams &params);
 void Conv2dBias(const ConvAllParams &params);
 void Conv2dBiasSigmoid(const ConvAllParams &params);
 
+void Conv2dDepthwiseBias(const ConvAllParams &params);
 void Conv2dDepthwiseBiasRelu(const ConvAllParams &params);
 void Conv2dDepthwiseBiasSigmoid(const ConvAllParams &params);
-void Conv2dDepthwiseBias(const ConvAllParams &params);
 void Conv2dDepthwiseBiasSilu(const ConvAllParams &params);
 
 }  // namespace cutlass_internal
