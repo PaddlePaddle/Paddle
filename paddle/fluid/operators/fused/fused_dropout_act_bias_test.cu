eec11d33aec9925f8595366bb747d99e2f5ada79
YuBaoku <49938469+EmmonsCurse@users.noreply.github.com>
2023-04-18 21:06:00 +0800

    Update fused_dropout_act_bias_test.cu

--- paddle/fluid/operators/fused/fused_dropout_act_bias_test.cu
+++ paddle/fluid/operators/fused/fused_dropout_act_bias_test.cu
@@ -12,7 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include <float.h>
+#include <cfloat>
+#define FLT_MAX __FLT_MAX__
+
 #include <time.h>
 
 #include <random>
