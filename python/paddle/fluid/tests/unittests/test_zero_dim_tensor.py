9f3a5dc8205ae05b2cacc4469d28154305ed971a
zhengqiwen1997 <zhengqiwen_au@163.com>
2023-04-18 03:33:25 +0000

    test=allcase

--- python/paddle/fluid/tests/unittests/test_zero_dim_tensor.py
+++ python/paddle/fluid/tests/unittests/test_zero_dim_tensor.py
@@ -4296,8 +4296,6 @@ class TestSundryAPIStatic(unittest.TestCase):
     @prog_scope()
     def test_static_data(self):
         x1 = paddle.static.data(name="x1", shape=[])
-        x2 = paddle.static.data(name="x2", shape=[])
-
         prog = paddle.static.default_main_program()
         res = self.exe.run(
             prog,
@@ -4311,18 +4309,22 @@ class TestSundryAPIStatic(unittest.TestCase):
         self.assertEqual(res[0].shape, ())
         self.assertEqual(res[0], np.array(1.0))
 
+        x2 = paddle.static.data(name="x2", shape=[])
+        x3 = paddle.static.data(name="x3", shape=[])
+        y = x2 + x3
         prog = paddle.static.default_main_program()
         res = self.exe.run(
             prog,
             feed={
                 "x2": 100.5,
+                "x3": 200.5,
             },
             fetch_list=[
-                x2.name,
+                y.name,
             ],
         )
         self.assertEqual(res[0].shape, ())
-        self.assertEqual(res[0], 100.5)
+        self.assertEqual(res[0], 301.0)
 
     @prog_scope()
     def test_prelu(self):
