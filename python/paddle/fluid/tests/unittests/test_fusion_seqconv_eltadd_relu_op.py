34336e939af8e786841d993f06778b59e33cbad5
tianshuo78520a <707759223@qq.com>
2023-04-18 17:09:40 +0800

    fix

--- python/paddle/fluid/tests/unittests/test_fusion_seqconv_eltadd_relu_op.py
+++ python/paddle/fluid/tests/unittests/test_fusion_seqconv_eltadd_relu_op.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
 import sys
+import unittest
 
 import numpy as np
 from eager_op_test import OpTest
