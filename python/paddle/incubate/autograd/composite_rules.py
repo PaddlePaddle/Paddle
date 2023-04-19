c29dc34e801eadac4e99e82e78142b162eb2c51e
Jiabin Yang <360788950@qq.com>
2023-04-19 15:44:50 +0800

    optimzie reshape related fusion (#53066)

--- python/paddle/incubate/autograd/composite_rules.py
+++ python/paddle/incubate/autograd/composite_rules.py
@@ -164,10 +164,12 @@ def layernorm_composite(x, scale, bias, epsilon, begin_norm_axis):
     out = difference * rsqrt_var
 
     if scale is not None:
-        scale = reshape(scale, x.shape[begin_norm_axis:])
+        if x.shape[begin_norm_axis:] is not scale.shape:
+            scale = reshape(scale, x.shape[begin_norm_axis:])
         out = out * scale
     if bias is not None:
-        bias = reshape(bias, x.shape[begin_norm_axis:])
+        if x.shape[begin_norm_axis:] is not bias.shape:
+            bias = reshape(bias, x.shape[begin_norm_axis:])
         out = out + bias
 
     mean_ = reshape(mean_, [-1])
