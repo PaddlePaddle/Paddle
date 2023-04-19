4ea492cd6ec468636100924a9e42df5d23ddec3e
tianshuo78520a <707759223@qq.com>
2023-04-19 20:18:54 +0800

    fix build error

--- test/cpp/inference/api/analysis_predictor_tester.cc
+++ test/cpp/inference/api/analysis_predictor_tester.cc
@@ -27,7 +27,7 @@
 #include "paddle/fluid/inference/api/helper.h"
 #include "paddle/fluid/inference/api/paddle_api.h"
 #include "paddle/fluid/inference/api/paddle_inference_api.h"
-#include "paddle/fluid/inference/tests/api/tester_helper.h"
+#include "test/cpp/inference/api/tester_helper.h"
 #include "paddle/fluid/inference/utils/io_utils.h"
 #include "paddle/phi/backends/cpu/cpu_info.h"
 
