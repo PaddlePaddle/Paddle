8ab81fe3721d8ff69d9e6220a58dd50b82fb2782
Ainavo <Ainavo@163.com>
2023-04-20 10:13:39 +0800

    add nn.Unflatten test

--- python/paddle/nn/layer/__init__.py
+++ python/paddle/nn/layer/__init__.py
@@ -45,7 +45,9 @@ from .common import Dropout3D  # noqa: F401
 from .common import AlphaDropout  # noqa: F401
 from .common import UpsamplingBilinear2D  # noqa: F401
 from .common import UpsamplingNearest2D  # noqa: F401
-from .common import Fold
+from .common import Fold  # noqa: F401
+from .common import Unflatten  # noqa: F401
+
 from .pooling import AvgPool1D  # noqa: F401
 from .pooling import AvgPool2D  # noqa: F401
 from .pooling import AvgPool3D  # noqa: F401
