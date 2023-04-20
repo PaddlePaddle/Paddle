e0553c2d7020c76db5d7b63c0deea0489f30bd93
RedContritio <RedContritio@qq.com>
2023-04-20 09:36:28 +0000

    simplify layer_norm_op.cc

--- paddle/fluid/operators/layer_norm_op.cc
+++ paddle/fluid/operators/layer_norm_op.cc
@@ -32,92 +32,10 @@ class LayerNormOp : public framework::OperatorWithKernel {
  public:
   using framework::OperatorWithKernel::OperatorWithKernel;
 
-  void InferShape(framework::InferShapeContext *ctx) const override {
-    OP_INOUT_CHECK(ctx->HasInput("X"), "Input", "X", "LayerNorm");
-    OP_INOUT_CHECK(ctx->HasOutput("Y"), "Output", "Y", "LayerNorm");
-    OP_INOUT_CHECK(ctx->HasOutput("Mean"), "Output", "Mean", "LayerNorm");
-    OP_INOUT_CHECK(
-        ctx->HasOutput("Variance"), "Output", "Variance", "LayerNorm");
-
-    auto x_dim = ctx->GetInputDim("X");
-    auto begin_norm_axis = ctx->Attrs().Get<int>("begin_norm_axis");
-    PADDLE_ENFORCE_LT(
-        begin_norm_axis,
-        x_dim.size(),
-        platform::errors::InvalidArgument(
-            "'begin_norm_axis' must be less than the dimensions of X,"
-            "But received 'begin_norm_axis' is [%d],"
-            "received the dimensions of X is [%d].",
-            begin_norm_axis,
-            x_dim.size()));
-
-    auto matrix_dim = phi::flatten_to_2d(x_dim, begin_norm_axis);
-    int left = static_cast<int>(matrix_dim[0]);
-    int right = static_cast<int>(matrix_dim[1]);
-    if (ctx->HasInput("Scale")) {
-      PADDLE_ENFORCE_EQ(ctx->GetInputDim("Scale").size(),
-                        1,
-                        platform::errors::InvalidArgument(
-                            "The dimensions of Input(Scale) must be 1, but "
-                            "received dimensions of"
-                            "Input(Scale) is [%d]",
-                            ctx->GetInputDim("Scale").size()));
-
-      if (ctx->IsRuntime()) {
-        PADDLE_ENFORCE_EQ(
-            ctx->GetInputDim("Scale")[0],
-            right,
-            platform::errors::InvalidArgument(
-                "The first dimension value of Input(Scale) must equal to be the"
-                "second dimension value of the flattened 2D matrix of Input(X),"
-                "But received the first dimension value of Input(Scale) is"
-                "[%d], the second dimension value of the flattened 2D matrix of"
-                " Input(Scale) is [%d].",
-                ctx->GetInputDim("Scale")[0],
-                right));
-      }
-    }
-    if (ctx->HasInput("Bias")) {
-      PADDLE_ENFORCE_EQ(ctx->GetInputDim("Bias").size(),
-                        1,
-                        platform::errors::InvalidArgument(
-                            "The dimensions of Input(Bias) must be 1, but "
-                            "received dimensions of"
-                            "Input(Bias) is [%d]",
-                            ctx->GetInputDim("Bias").size()));
-      if (ctx->IsRuntime()) {
-        PADDLE_ENFORCE_EQ(
-            ctx->GetInputDim("Bias")[0],
-            right,
-            platform::errors::InvalidArgument(
-                "The first dimension value of Input(Bias) must equal to be the"
-                "second dimension value of the flattened 2D matrix of Input(X),"
-                "But received the first dimension value of Input(Bias) is"
-                "[%d], the second dimension value of the flattened 2D matrix of"
-                " Input(Bias) is [%d].",
-                ctx->GetInputDim("Scale")[0],
-                right));
-      }
-    }
-
-    ctx->SetOutputDim("Y", ctx->GetInputDim("X"));
-    ctx->SetOutputDim("Mean", {left});
-    ctx->SetOutputDim("Variance", {left});
-    ctx->ShareLoD("X", "Y");
-  }
-
  protected:
   phi::KernelKey GetExpectedKernelType(
       const framework::ExecutionContext &ctx) const override {
     auto input_data_type = OperatorWithKernel::IndicateVarDataType(ctx, "X");
-
-    // NOTE(jiahongyu): Below codes originally enclosed by PADDLE_WITH_MKLDNN
-    int begin_norm_axis = ctx.Attr<int>("begin_norm_axis");
-    if (begin_norm_axis !=
-        ctx.Input<phi::DenseTensor>("X")->dims().size() - 1) {
-      this->SetDnnFallback(true);
-    }
-    // NOTE(jiahongyu): Above codes originally enclosed by PADDLE_WITH_MKLDNN
 
     return phi::KernelKey(input_data_type, ctx.GetPlace());
   }
@@ -144,28 +62,12 @@ class LayerNormOpMaker : public framework::OpProtoAndCheckerMaker {
 
     AddAttr<float>("epsilon",
                    "Constant for numerical stability [default 1e-5].")
-        .SetDefault(1e-5)
-        .AddCustomChecker([](const float &epsilon) {
-          PADDLE_ENFORCE_EQ(epsilon >= 0.0f && epsilon <= 0.001f,
-                            true,
-                            platform::errors::InvalidArgument(
-                                "'epsilon' in Op(LayerNorm) should be between"
-                                "0.0 and 0.001, But received [%s].",
-                                epsilon));
-        });
+        .SetDefault(1e-5);
     AddAttr<int>("begin_norm_axis",
                  "the axis of `begin_norm_axis ... Rank(X) - 1` will be "
                  "normalized. `begin_norm_axis` splits the tensor(`X`) to a "
                  "matrix [N,H]. [default 1].")
-        .SetDefault(1)
-        .AddCustomChecker([](const int &begin_norm_axis) {
-          PADDLE_ENFORCE_GT(begin_norm_axis,
-                            0,
-                            platform::errors::InvalidArgument(
-                                "'begin_norm_axis' in Op(LayerNorm) should be"
-                                "greater than zero. But received [%d].",
-                                begin_norm_axis));
-        });
+        .SetDefault(1);
     AddComment(R"DOC(
 Assume feature vectors exist on dimensions
 :attr:`begin_norm_axis ... rank(input)` and calculate the moment statistics
@@ -182,31 +84,6 @@ Refer to `Layer Normalization <https://arxiv.org/pdf/1607.06450v1.pdf>`_
 class LayerNormGradOp : public framework::OperatorWithKernel {
  public:
   using framework::OperatorWithKernel::OperatorWithKernel;
-
-  void InferShape(framework::InferShapeContext *ctx) const override {
-    // check input
-    OP_INOUT_CHECK(ctx->HasInput("X"), "Input", "X", "LayerNormGrad");
-    OP_INOUT_CHECK(ctx->HasInput("Mean"), "Input", "Mean", "LayerNormGrad");
-    OP_INOUT_CHECK(
-        ctx->HasInput("Variance"), "Input", "Variance", "LayerNormGrad");
-    OP_INOUT_CHECK(ctx->HasInput(framework::GradVarName("Y")),
-                   "Input",
-                   framework::GradVarName("Y"),
-                   "LayerNormGrad");
-
-    // check output
-    if (ctx->HasOutput(framework::GradVarName("X"))) {
-      ctx->SetOutputDim(framework::GradVarName("X"), ctx->GetInputDim("X"));
-    }
-    if (ctx->HasOutput(framework::GradVarName("Scale"))) {
-      ctx->SetOutputDim(framework::GradVarName("Scale"),
-                        ctx->GetInputDim("Scale"));
-    }
-    if (ctx->HasOutput(framework::GradVarName("Bias"))) {
-      ctx->SetOutputDim(framework::GradVarName("Bias"),
-                        ctx->GetInputDim("Bias"));
-    }
-  }
 
  protected:
   phi::KernelKey GetExpectedKernelType(
