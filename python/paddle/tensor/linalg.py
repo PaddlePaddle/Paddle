f304d4c0616741ae2895e5166f25c085f641d2e2
GGBond8488 <857631483@qq.com>
2023-04-21 03:02:38 +0000

    test=allcase

--- python/paddle/tensor/linalg.py
+++ python/paddle/tensor/linalg.py
@@ -835,10 +835,7 @@ def cond(x, p=None, name=None):
         else:
             reduce_all = True if axis is None or axis == [] else False
             axis = axis if axis is not None and axis != [] else [0]
-            block = LayerHelper(
-                'norm',
-                **locals(),
-            )
+            block = LayerHelper('norm', **locals())
             abs_out = block.create_variable_for_type_inference(
                 dtype=block.input_dtype()
             )
