53cc56a801e377620668213b79ae9d878cc7763a
zhengqiwen1997 <zhengqiwen_au@163.com>
2023-04-23 07:43:19 +0000

    fix test cases, test=allcase

--- python/paddle/fluid/tests/unittests/test_zero_dim_tensor.py
+++ python/paddle/fluid/tests/unittests/test_zero_dim_tensor.py
@@ -2270,52 +2270,32 @@ class TestSundryAPI(unittest.TestCase):
     def test_unstack(self):
         x1 = paddle.full([1], 0)
         x2 = paddle.full([2], 2)
-        x1.retain_grads()
-        x2.retain_grads()
-        x1.stop_gradient = False
-        x2.stop_gradient = False
+
         [out1] = paddle.unstack(x1, 0)
-        out1.retain_grads()
-        out1.backward()
         [out2_1, out2_2] = paddle.unstack(x2, 0)
-        out2_1.retain_grads()
-        out2_1.backward()
-        out2_2.retain_grads()
-        out2_2.backward()
+
         self.assertEqual(out1.shape, [])
         self.assertEqual(out1.numpy(), 0)
-        self.assertEqual(out1.grad.shape, [])
+
         self.assertEqual(out2_1.shape, [])
         self.assertEqual(out2_1.numpy(), 2)
-        self.assertEqual(out2_1.grad.shape, [])
         self.assertEqual(out2_2.shape, [])
         self.assertEqual(out2_2.numpy(), 2)
-        self.assertEqual(out2_2.grad.shape, [])
 
     def test_unbind(self):
         x1 = paddle.full([1], 0)
         x2 = paddle.full([2], 2)
-        x1.retain_grads()
-        x2.retain_grads()
-        x1.stop_gradient = False
-        x2.stop_gradient = False
+
         [out1] = paddle.unbind(x1, 0)
-        out1.retain_grads()
-        out1.backward()
         [out2_1, out2_2] = paddle.unbind(x2, 0)
-        out2_1.retain_grads()
-        out2_1.backward()
-        out2_2.retain_grads()
-        out2_2.backward()
+
         self.assertEqual(out1.shape, [])
         self.assertEqual(out1.numpy(), 0)
-        self.assertEqual(out1.grad.shape, [])
+
         self.assertEqual(out2_1.shape, [])
         self.assertEqual(out2_1.numpy(), 2)
-        self.assertEqual(out2_1.grad.shape, [])
         self.assertEqual(out2_2.shape, [])
         self.assertEqual(out2_2.numpy(), 2)
-        self.assertEqual(out2_2.grad.shape, [])
 
     def test_maseked_select(self):
         x = paddle.rand([])
