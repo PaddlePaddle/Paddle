3325dbc492d492625f577ec5b54b94ed93f92317
shaojiewang <wsjmessi@163.com>
2023-04-24 09:33:09 +0800

    formatting

--- paddle/phi/kernels/gpu/embedding_grad_kernel.cu
+++ paddle/phi/kernels/gpu/embedding_grad_kernel.cu
@@ -18,13 +18,13 @@
 #include "glog/logging.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/backends/gpu/gpu_primitives.h"
+#include "paddle/phi/common/amp_type_traits.h"
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/common/memory_utils.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/mixed_vector.h"
 #include "paddle/phi/kernels/funcs/eigen/common.h"
 #include "paddle/phi/kernels/funcs/embedding_util.h"
-#include "paddle/phi/common/amp_type_traits.h"
 
 DECLARE_bool(embedding_deterministic);
 
