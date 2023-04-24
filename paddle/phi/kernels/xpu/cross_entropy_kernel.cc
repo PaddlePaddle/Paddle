d1894d46e65ca61f2134fe823b896441bd012486
lilujia <lilujia@baidu.com>
2023-04-24 09:33:04 +0000

    [Bug Fixs] fix bugs when using cast<int64_t, int32_t> in xpu/cross_entropy kernels, *test=kunlun

--- paddle/phi/kernels/xpu/cross_entropy_kernel.cc
+++ paddle/phi/kernels/xpu/cross_entropy_kernel.cc
@@ -133,39 +133,35 @@ void CrossEntropyWithSoftmaxKernel(const Context& dev_ctx,
                                          axis == rank - 1 ? d : t);
     PADDLE_ENFORCE_XDNN_SUCCESS(r, "soft_cross_entropy");
   } else {
-    DenseTensor labels_int32;
-    int* labels_int_ptr_l3 = RAII_GUARD.alloc_l3_or_gm<int32_t>(labels.numel());
-    PADDLE_ENFORCE_XDNN_NOT_NULL(labels_int_ptr_l3);
-
+    int* labels_int_ptr_l3 = nullptr;
     if (labels.dtype() == DataType::INT32) {
-      r = xpu::hard_cross_entropy<XPUType, int32_t>(
-          dev_ctx.x_context(),
-          softmax_data,
-          labels.data<int32_t>(),
-          loss_data,
-          nullptr,
-          axis == rank - 1 ? n : n * d / t,
-          axis == rank - 1 ? d : t,
-          ignore_index);
-      PADDLE_ENFORCE_XDNN_SUCCESS(r, "hard_cross_entropy");
-    } else {
+      labels_int_ptr_l3 = const_cast<int32_t*> labels.data<int32_t>();
+    } else if (labels.dtype() == DataType::INT64) {
+      labels_int_ptr_l3 = RAII_GUARD.alloc_l3_or_gm<int32_t>(labels.numel());
+      PADDLE_ENFORCE_XDNN_NOT_NULL(labels_int_ptr_l3);
+
       r = xpu::cast<int64_t, int32_t>(dev_ctx.x_context(),
                                       labels.data<int64_t>(),
                                       labels_int_ptr_l3,
                                       labels.numel());
       PADDLE_ENFORCE_XDNN_SUCCESS(r, "cast");
-
-      r = xpu::hard_cross_entropy<XPUType, int32_t>(
-          dev_ctx.x_context(),
-          softmax_data,
-          labels_int_ptr_l3,
-          loss_data,
-          nullptr,
-          axis == rank - 1 ? n : n * d / t,
-          axis == rank - 1 ? d : t,
-          ignore_index);
-      PADDLE_ENFORCE_XDNN_SUCCESS(r, "hard_cross_entropy");
+    } else {
+      // TODO(lilujia): other data types should be handled
+      errors::Unimplemented(
+          ("cross_entropy does not support data types other than int32 and "
+           "int64"));
     }
+
+    r = xpu::hard_cross_entropy<XPUType, int32_t>(
+        dev_ctx.x_context(),
+        softmax_data,
+        labels.data<int32_t>(),
+        loss_data,
+        nullptr,
+        axis == rank - 1 ? n : n * d / t,
+        axis == rank - 1 ? d : t,
+        ignore_index);
+    PADDLE_ENFORCE_XDNN_SUCCESS(r, "hard_cross_entropy");
   }
 }
 
