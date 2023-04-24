d2cb5e4a25083b17ca28206de8d7df81fd9985ab
zhangyuqin1998 <75946871+zhangyuqin1998@users.noreply.github.com>
2023-04-24 13:42:36 +0800

    Update uniform_kernel.cc

--- paddle/phi/kernels/legacy/cpu/uniform_kernel.cc
+++ paddle/phi/kernels/legacy/cpu/uniform_kernel.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#include "paddle/phi/kernels/uniform_kernel.h"
 
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/funcs/uniform_real_distribution.h"
