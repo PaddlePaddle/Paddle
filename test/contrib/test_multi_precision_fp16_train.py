f6d53f42813f9779c04df4ae8736836c1a625ef9
zhouwei25 <zhouwei25@baidu.com>
2023-04-24 14:06:52 +0000

    [Zero-Dim] Support paddle.sum/mean/loss api output 0D,test=allcase

--- test/contrib/test_multi_precision_fp16_train.py
+++ test/contrib/test_multi_precision_fp16_train.py
@@ -178,7 +178,7 @@ def train(use_pure_fp16=True, use_nesterov=False, optimizer=""):
                 (loss,) = exe.run(
                     train_program, feed=feeder.feed(data), fetch_list=[sum_cost]
                 )
-                loss_v = loss[0] if isinstance(loss, np.ndarray) else loss
+                loss_v = float(loss) if isinstance(loss, np.ndarray) else loss
                 print(
                     'PassID {:1}, Train Batch ID {:04}, train loss {:2.4}'.format(
                         pass_id, batch_id + 1, float(loss_v)
