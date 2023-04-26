8d6e219215903b495de431fe8d1021b75a1119d2
0x45f <wangzhen45@baidu.com>
2023-04-26 06:30:18 +0000

    Add high order grad ut

--- test/dygraph_to_static/test_gradname_parse.py
+++ test/dygraph_to_static/test_gradname_parse.py
@@ -67,40 +67,98 @@ class TestGradNameParse(unittest.TestCase):
         opt.minimize(loss)
 
 
-class TestXGradNameParse(unittest.TestCase):
-    def test_x_grad_name_parse(self):
-        def tanh_high_order_grad(x):
-            y = paddle.tanh(x)
-            return paddle.grad(y, x, create_graph=True)[0]
+def tanh_high_order_grad(x):
+    y = paddle.tanh(x)
+    return paddle.grad(y, x, create_graph=True)[0]
+
+
+class TestTanhHighOrderGrad(unittest.TestCase):
+    def setUp(self):
+        self.func = tanh_high_order_grad
 
         x1 = paddle.ones((1,))
         x1.stop_gradient = False
-        y1 = tanh_high_order_grad(x1)
-        g1 = paddle.grad(y1, x1)
+        self.dy_input = (x1,)
+        self.dy_grad_input = (x1,)
 
         x2 = paddle.ones((1,))
         x2.stop_gradient = False
-        y2 = paddle.jit.to_static(tanh_high_order_grad)(x2)
-        g2 = paddle.grad(y2, x2)
+        self.dy2st_input = (x2,)
+        self.dy2st_grad_input = (x2,)
 
-        np.testing.assert_equal(g1[0].numpy(), g2[0].numpy())
+    def test_run(self):
+        try:
+            dy_out = self.func(*self.dy_input)
+            dy_grad = paddle.grad(dy_out, self.dy_grad_input)
+        except:
+            dy_grad = [None for i in self.dy_grad_input]
+        dy_grad = [
+            t.numpy() if isinstance(t, paddle.Tensor) else t for t in dy_grad
+        ]
+
+        dy2st_out = paddle.jit.to_static(self.func)(*self.dy2st_input)
+        dy2st_grad = paddle.grad(dy2st_out, self.dy2st_grad_input)
+        dy2st_grad = [
+            t.numpy() if isinstance(t, paddle.Tensor) else t for t in dy_grad
+        ]
+        np.testing.assert_equal(dy_grad, dy2st_grad)
+
+        dy_input_grad = [
+            t.grad.numpy() if isinstance(t.grad, paddle.Tensor) else None
+            for t in self.dy_input
+        ]
+        dy2st_input_grad = [
+            t.grad.numpy() if isinstance(t.grad, paddle.Tensor) else None
+            for t in self.dy2st_input
+        ]
+        np.testing.assert_equal(dy_input_grad, dy2st_input_grad)
 
 
-class TestGradNone(unittest.TestCase):
-    def test_grad_none(self):
-        @paddle.jit.to_static
-        def matmul_high_order_grad(x, y):
-            z = paddle.matmul(x, y)
-            g = paddle.grad(z, [x, y], create_graph=False)
-            return g[0]
+def matmul_high_order_grad(x, y):
+    z = paddle.matmul(x, y)
+    g = paddle.grad(z, [x, y], create_graph=False)
+    return g[0]
 
-        x = paddle.ones([1])
-        x.stop_gradient = False
-        y = paddle.ones([1])
-        y.stop_gradient = False
-        g = matmul_high_order_grad(x, y)
-        o = paddle.grad(g, x)
-        np.testing.assert_equal(y.grad, None)
+
+class TestMatMulHighOrderGrad1(TestTanhHighOrderGrad):
+    def setUp(self):
+        self.func = matmul_high_order_grad
+
+        x1 = paddle.ones([1])
+        x1.stop_gradient = False
+        y1 = paddle.ones([1])
+        y1.stop_gradient = False
+        self.dy_input = (x1, y1)
+        self.dy_grad_input = (x1,)
+
+        x2 = paddle.ones([1])
+        x2.stop_gradient = False
+        y2 = paddle.ones([1])
+        y2.stop_gradient = False
+        self.dy2st_input = (x2, y2)
+        self.dy2st_grad_input = (x2,)
+
+
+class TestMatMulHighOrderGrad2(TestTanhHighOrderGrad):
+    def setUp(self):
+        self.func = matmul_high_order_grad
+
+        x = np.random.randn(5, 5)
+        y = np.random.randn(5, 5)
+
+        x1 = paddle.to_tensor(x)
+        x1.stop_gradient = False
+        y1 = paddle.to_tensor(y)
+        y1.stop_gradient = True
+        self.dy_input = (x1, y1)
+        self.dy_grad_input = (x1,)
+
+        x2 = paddle.to_tensor(x)
+        x2.stop_gradient = False
+        y2 = paddle.to_tensor(y)
+        y2.stop_gradient = True
+        self.dy2st_input = (x2, y2)
+        self.dy2st_grad_input = (x2,)
 
 
 if __name__ == "__main__":
