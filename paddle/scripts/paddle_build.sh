710684de0267860e6af9ea40fcc4707b38ef1420
tianshuo78520a <707759223@qq.com>
2023-04-26 20:46:14 +0800

    fix

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3937,11 +3937,6 @@ function main() {
         build_mac
         ;;
       cicheck_py37)
-        export PY_VERSION=3.9
-        pip3.9 config set global.cache-dir "/home/data/cfs/.cache/pip"
-        pip3.9 install -r "${work_dir}/python/requirements.txt"
-        pip3.9 install -r "${work_dir}/python/unittest_py/requirements.txt"
-        pip3.9 install bce-python-sdk==0.8.74
         run_setup ${PYTHON_ABI:-""} bdist_wheel ${parallel_number}
         run_linux_cpu_test ${PYTHON_ABI:-""} ${PROC_RUN:-1}
         ;;
