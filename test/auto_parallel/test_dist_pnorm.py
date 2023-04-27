254c31ff285fbf52fdabee694c5bf7ad765dbe98
GGBond8488 <857631483@qq.com>
2023-04-27 18:41:57 +0000

    test

--- test/auto_parallel/test_dist_pnorm.py
+++ test/auto_parallel/test_dist_pnorm.py
@@ -125,6 +125,8 @@ class TestDistPNormDP(TestDistPNorm):
                 for input_attr in op_dist_attr.inputs_dist_attrs.values():
                     assert set(input_attr.dims_mapping) == {-1}
                 for output_attr in op_dist_attr.outputs_dist_attrs.values():
+                    print("=======")
+                    print(output_attr.dims_mapping)
                     assert set(output_attr.dims_mapping) == {}
             if op.type == 'c_allgather':
                 for input_attr in op_dist_attr.inputs_dist_attrs.values():
