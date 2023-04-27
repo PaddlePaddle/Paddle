87ec584ee3c9dafdafba6ed7b1e176f6cec48058
superwinner1 <82640284+superwinner1@users.noreply.github.com>
2023-04-27 10:21:14 +0800

    【Hackathon No.55】add fmax BF16 test (#51925)

--- paddle/phi/kernels/gpu/elementwise_grad_kernel.cu
+++ paddle/phi/kernels/gpu/elementwise_grad_kernel.cu
@@ -98,6 +98,7 @@ PD_REGISTER_KERNEL(fmax_grad,
                    double,
                    int,
                    phi::dtype::float16,
+                   phi::dtype::bfloat16,
                    int64_t) {}
 
 PD_REGISTER_KERNEL(fmin_grad,
