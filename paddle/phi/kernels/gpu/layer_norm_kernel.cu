fd351a7aef99a6896c1eb96abfed021fd659db59
JamesLim-sy <santamonic@sina.com>
2023-05-02 17:42:26 +0000

    add test_perf_for_layernorm

--- paddle/phi/kernels/gpu/layer_norm_kernel.cu
+++ paddle/phi/kernels/gpu/layer_norm_kernel.cu
@@ -633,8 +633,8 @@ void LayerNormKernel(const Context &dev_ctx,
     }
   } else {
     // WarpShuffle intrinsics is involved in LaunchLayerNormKernel.
-    if (FLAGS_use_fast_math && feature_size <= 1024 &&
-        (!std::is_same<T, int8_t>::value)) {
+    // if (FLAGS_use_fast_math && feature_size <= 1024 &&
+    if (feature_size <= 1024 && (!std::is_same<T, int8_t>::value)) {
       LaunchLayerNormKernel<Context, T, U>(dev_ctx,
                                            x_data,
                                            y_data,
