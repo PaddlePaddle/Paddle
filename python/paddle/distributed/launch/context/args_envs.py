dadea2df29e06fd8d3972ab68bec3a8a2150a0a8
kuizhiqing <kuizhiqing@msn.com>
2023-05-08 10:59:37 +0000

    add log overwrite flag

--- python/paddle/distributed/launch/context/args_envs.py
+++ python/paddle/distributed/launch/context/args_envs.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import os
-from argparse import REMAINDER, ArgumentParser
+from argparse import REMAINDER, ArgumentParser, ArgumentTypeError
 
 env_args_mapping = {
     'POD_IP': 'host',
@@ -22,6 +22,7 @@ env_args_mapping = {
     'PADDLE_NNODES': 'nnodes',
     'PADDLE_RUN_MODE': 'run_mode',
     'PADDLE_LOG_LEVEL': 'log_level',
+    'PADDLE_LOG_OVERWRITE': 'log_overwrite',
     'PADDLE_NPROC_PER_NODE': 'nproc_per_node',
     'PADDLE_JOB_ID': 'job_id',
     'PADDLE_RANK': 'rank',
@@ -48,6 +49,15 @@ def fetch_envs():
     return os.environ.copy()
 
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise ArgumentTypeError('Unsupported value encountered.')
+
+
 def parse_args():
     parser = ArgumentParser()
 
@@ -61,7 +71,7 @@ def parse_args():
     )
 
     base_group.add_argument(
-        "--legacy", type=bool, default=False, help="use legacy launch"
+        "--legacy", type=str2bool, default=False, help="use legacy launch"
     )
 
     base_group.add_argument(
@@ -70,6 +80,13 @@ def parse_args():
 
     base_group.add_argument(
         "--log_level", type=str, default="INFO", help="log level. Default INFO"
+    )
+
+    base_group.add_argument(
+        "--log_overwrite",
+        type=str2bool,
+        default=False,
+        help="overwrite exits logfiles. Default False",
     )
 
     base_group.add_argument(
