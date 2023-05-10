0a56f9b65c9c1095c3e8cb727c0b31e05f2b14d3
PommesPeter <434596665@qq.com>
2023-05-10 13:52:47 +0800

    added base code for i0 and i0e

---
+++ paddle/phi/kernels/gpu/i0_kernel.cu
@@ -0,0 +1,38 @@
+/* Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#ifdef __NVCC__
+#include <curand_kernel.h>
+#endif
+#ifdef __HIPCC__
+#include <hiprand_kernel.h>
+#endif
+
+#include "paddle/phi/backends/gpu/gpu_context.h"
+#include "paddle/phi/backends/gpu/gpu_launch_config.h"
+#include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/funcs/for_range.h"
+#include "paddle/phi/kernels/i0_kernel.h"
+
+namespace phi {
+
+template <typename T, typename Context>
+void I0Kernel(const Context& ctx, const DenseTensor& x, DenseTensor* out) {
+
+}
+
+}  // namespace phi
+
+PD_REGISTER_KERNEL(
+    i0, GPU, ALL_LAYOUT, phi::I0Kernel, float, double) {}
