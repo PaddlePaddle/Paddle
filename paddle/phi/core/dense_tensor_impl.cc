61f6c7edeb21efeda86b408adfa8203b0a0585db
GGBond8488 <857631483@qq.com>
2023-05-12 08:02:47 +0000

    recovery some type error

--- paddle/phi/core/dense_tensor_impl.cc
+++ paddle/phi/core/dense_tensor_impl.cc
@@ -11,6 +11,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
+
 #include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/common/complex.h"
 #include "paddle/phi/common/float16.h"
