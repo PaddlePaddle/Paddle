3b5fd8475eeddd5a1c0c8377dc776cc80e17ba16
superwinner1 <82640284+superwinner1@users.noreply.github.com>
2023-05-12 10:07:54 +0800

    'fix'

--- python/paddle/fluid/tests/unittests/test_argsort_op.py
+++ python/paddle/fluid/tests/unittests/test_argsort_op.py
@@ -15,6 +15,7 @@
 import unittest
 
 import numpy as np
+from eager_op_test import OpTest, convert_float_to_uint16
 
 import paddle
 from paddle import fluid
@@ -501,17 +502,27 @@ class TestArgsortWithInputNaN(unittest.TestCase):
         paddle.enable_static()
 
 
-class TestArgsortOpFp16(unittest.TestCase):
-    def test_fp16(self):
+class TestArgsortFP16OP(OpTest):
+    def setUp(self):
+        self.op_type = 'argsort'
+        self.dtype = np.float16
         x_np = np.random.random((2, 8)).astype('float16')
-        with paddle.static.program_guard(paddle.static.Program()):
-            x = paddle.static.data(shape=[2, 8], name='x', dtype='float16')
-            out = paddle.argsort(x)
-            if core.is_compiled_with_cuda():
-                place = paddle.CUDAPlace(0)
-                exe = paddle.static.Executor(place)
-                exe.run(paddle.static.default_startup_program())
-                out = exe.run(feed={'x': x_np}, fetch_list=[out])
+        x = paddle.static.data(shape=[2, 8], name='x', dtype='float16')
+        out = np.argsort(x)
+        self.inputs = {'X': x_np}
+        self.outputs = {'Out': out}
+
+    def test_check_output(self):
+        place = core.CUDAPlace(0)
+        self.check_output_with_place(place)
+
+    def test_check_grad(self):
+        place = core.CUDAPlace(0)
+        self.check_grad_with_place(
+            place,
+            ['X'],
+            'Out',
+        )
 
 
 @unittest.skipIf(
@@ -519,16 +530,27 @@ class TestArgsortOpFp16(unittest.TestCase):
     or not core.is_bfloat16_supported(core.CUDAPlace(0)),
     "core is not compiled with CUDA and not support the bfloat16",
 )
-class TestArgsortBF16OP(unittest.TestCase):
-    def test_bf16(self):
-        x_np = np.random.random((2, 8)).astype(np.uint16)
-        with paddle.static.program_guard(paddle.static.Program()):
-            x = paddle.static.data(shape=[2, 8], name='x', dtype=np.uint16)
-            out = paddle.argsort(x)
-            place = paddle.CUDAPlace(0)
-            exe = paddle.static.Executor(place)
-            exe.run(paddle.static.default_startup_program())
-            out = exe.run(feed={'x': x_np}, fetch_list=[out])
+class TestArgsortBF16OP(OpTest):
+    def setUp(self):
+        self.op_type = 'argsort'
+        self.dtype = np.uint16
+        x_np = np.random.random((2, 8)).astype('float32')
+        x = paddle.static.data(shape=[2, 8], name='x', dtype='float32')
+        out = np.argsort(x)
+        self.inputs = {'X': convert_float_to_uint16(x_np)}
+        self.outputs = {'Out': convert_float_to_uint16(out)}
+
+    def test_check_output(self):
+        place = core.CUDAPlace(0)
+        self.check_output_with_place(place)
+
+    def test_check_grad(self):
+        place = core.CUDAPlace(0)
+        self.check_grad_with_place(
+            place,
+            ['X'],
+            'Out',
+        )
 
 
 if __name__ == "__main__":
