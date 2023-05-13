14c9fd319bb3b47d7d3243999e65067605db26dc
enkilee <luhputu0815@gmail.com>
2023-05-13 06:36:37 +0000

    fix

--- python/paddle/fluid/tests/unittests/test_elementwise_mod_op.py
+++ python/paddle/fluid/tests/unittests/test_elementwise_mod_op.py
@@ -163,7 +163,7 @@ class TestElementwiseModBF16Op(OpTest):
     def init_input_output(self):
         self.x = np.random.uniform(0, 10000, [10, 10]).astype(np.float32)
         self.y = np.random.uniform(0, 1000, [10, 10]).astype(np.float32)
-        self.out = np.mod(self.x, self.y)
+        self.out = np.fmod(self.y + np.fmod(self.x, self.y), self.y)
 
     def setUp(self):
         self.op_type = "elementwise_mod"
@@ -183,9 +183,7 @@ class TestElementwiseModBF16Op(OpTest):
             ),
         }
         self.attrs = {'axis': self.axis, 'use_mkldnn': self.use_mkldnn}
-        self.outputs = {
-            'Out': convert_uint16_to_float(convert_float_to_uint16(self.out))
-        }
+        self.outputs = {'Out': convert_float_to_uint16(self.out)}
 
     def test_check_output(self):
         place = core.CUDAPlace(0)
