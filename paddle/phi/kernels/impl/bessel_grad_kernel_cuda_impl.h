93b1f2693cb5fc49bac1bd59b47d164734567f5c
LyndonKong <kongrui.chn@outlook.com>
2023-05-13 08:58:17 +0800

    resolve merge conflicts

--- paddle/phi/kernels/impl/bessel_grad_kernel_cuda_impl.h
+++ paddle/phi/kernels/impl/bessel_grad_kernel_cuda_impl.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,18 +11,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-=======
-// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
->>>>>>> 3c1049b876 (Add i1 and i1e op)
 
 #pragma once
 
@@ -34,7 +21,6 @@ limitations under the License. */
 namespace phi {
 
 template <typename T>
-<<<<<<< HEAD
 struct CudaI0GradFunctor {
   __device__ __forceinline__ T operator()(const T _x, const T _out_grad) const {
     using MT = typename phi::dtype::MPTypeTrait<T>::Type;
@@ -67,9 +53,6 @@ struct CudaI0GradFunctor {
 
 template <typename T>
 struct CudaI0eGradFunctor {
-=======
-struct CudaI1GradFunctor {
->>>>>>> 3c1049b876 (Add i1 and i1e op)
   __device__ __forceinline__ T operator()(const T _x,
                                           const T _out,
                                           const T _out_grad) const {
@@ -77,7 +60,6 @@ struct CudaI1GradFunctor {
     const MT mp_x = static_cast<MT>(_x);
     const MT mp_out = static_cast<MT>(_out);
     const MT mp_out_grad = static_cast<MT>(_out_grad);
-<<<<<<< HEAD
     // get output of i1e
     MT x = std::abs(mp_x);
     if (x <= MT{8.0}) {
@@ -93,7 +75,27 @@ struct CudaI1GradFunctor {
                             mp_out_grad);
     }
     auto coeff_pair_B = ChebyshevCoefficientsI1e_B<MT>();
-=======
+    auto B = std::get<0>(coeff_pair_B);
+    auto len = std::get<1>(coeff_pair_B);
+    MT y = (MT{32.0} / x) - MT{2.0};
+
+    const MT i1e_out = Chbevl<MT>(y, B, len) / std::sqrt(x);
+    const MT i1e_data = (mp_x < MT{0.0}) ? -i1e_out : i1e_out;
+
+    return static_cast<T>((i1e_data - std::copysign(MT{1.0}, mp_x) * mp_out) *
+                          mp_out_grad);
+  }
+};
+
+template <typename T>
+struct CudaI1GradFunctor {
+  __device__ __forceinline__ T operator()(const T _x,
+                                          const T _out,
+                                          const T _out_grad) const {
+    using MT = typename phi::dtype::MPTypeTrait<T>::Type;
+    const MT mp_x = static_cast<MT>(_x);
+    const MT mp_out = static_cast<MT>(_out);
+    const MT mp_out_grad = static_cast<MT>(_out_grad);
     MT x = std::abs(mp_x);
     if (x <= MT{8.0}) {
       auto coeff_pair_A = ChebyshevCoefficientsI0e_A<MT>();
@@ -112,18 +114,10 @@ struct CudaI1GradFunctor {
       }
     }
     auto coeff_pair_B = ChebyshevCoefficientsI0e_B<MT>();
->>>>>>> 3c1049b876 (Add i1 and i1e op)
     auto B = std::get<0>(coeff_pair_B);
     auto len = std::get<1>(coeff_pair_B);
     MT y = (MT{32.0} / x) - MT{2.0};
 
-<<<<<<< HEAD
-    const MT i1e_out = Chbevl<MT>(y, B, len) / std::sqrt(x);
-    const MT i1e_data = (mp_x < MT{0.0}) ? -i1e_out : i1e_out;
-
-    return static_cast<T>((i1e_data - std::copysign(MT{1.0}, mp_x) * mp_out) *
-                          mp_out_grad);
-=======
     return static_cast<T>(
         (std::exp(x) * Chbevl<MT>(y, B, len) / std::sqrt(x) - mp_out / mp_x) *
         mp_out_grad);
@@ -166,7 +160,6 @@ struct CudaI1eGradFunctor {
         (Chbevl<T>(y, B, len) / std::sqrt(x) -
          mp_out * (std::copysign(MT{1.0}, mp_x) + (MT{1.0}) / mp_x)) *
         mp_out_grad);
->>>>>>> 3c1049b876 (Add i1 and i1e op)
   }
 };
 
--- paddle/phi/kernels/impl/bessel_kernel_impl.h
+++ paddle/phi/kernels/impl/bessel_kernel_impl.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,18 +11,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
-=======
-// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
->>>>>>> 3c1049b876 (Add i1 and i1e op)
 
 #pragma once
 
@@ -100,7 +87,6 @@ static inline T Chbevl(T x, const T array[], size_t len) {
 }
 
 template <typename T>
-<<<<<<< HEAD
 struct I0eFunctor {
   I0eFunctor(const T* input, T* output, int64_t numel)
       : input_(input), output_(output), numel_(numel) {}
@@ -162,8 +148,6 @@ struct I0Functor {
 };
 
 template <typename T>
-=======
->>>>>>> 3c1049b876 (Add i1 and i1e op)
 static inline typename std::enable_if<std::is_same<double, T>::value,
                                       std::tuple<const T*, size_t>>::type
 ChebyshevCoefficientsI1e_A() {
@@ -267,8 +251,6 @@ ChebyshevCoefficientsI1e_B() {
   return std::make_tuple(coeff, 7);
 }
 
-<<<<<<< HEAD
-=======
 template <typename T>
 struct I1Functor {
   I1Functor(const T* input, T* output, int64_t numel)
@@ -330,5 +312,4 @@ struct I1eFunctor {
   int64_t numel_;
 };
 
->>>>>>> 3c1049b876 (Add i1 and i1e op)
 }  // namespace phi
