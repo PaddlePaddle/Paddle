5d221be2a22568761c489fb55cf5dba00e0ebef6
andsonder <changlu@keter.top>
2023-05-14 14:04:35 +0000

    update onnx version

--- paddle/fluid/inference/tensorrt/convert/flip_op.cc
+++ paddle/fluid/inference/tensorrt/convert/flip_op.cc
@@ -23,61 +23,115 @@ class FlipOpConverter : public OpConverter {
   void operator()(const framework::proto::OpDesc& op,
                   const framework::Scope& scope,
                   bool test_mode) override {
-#if IS_TRT_VERSION_GE(8600)
-    VLOG(3) << "convert a flip op to tensorrt layer";
-    std::cout << "convert a flip op to tensorrt layer" << std::endl;
     framework::OpDesc op_desc(op, nullptr);
-
     auto* input = engine_->GetITensor(op_desc.Input("X")[0]);
-    auto input_dims = input->getDimensions();
+    vector<int> axis = op_desc.GetAttr("axis");
 
-    int rank = input_dims.nbDims;
-    int axis = PADDLE_GET_CONST(std::vector<int>, op_desc.GetAttr("axis"))[0];
-    if (axis < 0) {
-      axis += rank;
+    // Need to flip the data input along the given axis using the Slice
+    operator const auto input_shape = Shape(input);
+    int intput_dims = input->getDimensions().nbDims;
+    nvinfer1::ITensor* starts = Add1DConstantLayer(-1);
+    nvinfer1::ITensor* ends = Add1DConstantLayer(static_cast<int64_t>(INT_MIN));
+    nvinfer1::ITensor* axes = Add1DConstantLayer(axis);
+    nvinfer1::ITensor* steps = Add1DConstantLayer(-1);
+
+    if (axes->getDimensions().nbDims < intput_dims) {
+      // axes specify a subset of the dimensions, or out of order.
+      // Convert starts/ends/steps to complete in-order form.
+      const nvinfer1::ITensor* subscripts{
+          AxesToInterlaceSubscripts(axes, intput_dims)};
+      starts = interlace(Similar(input_shape, 0), starts, subscripts);
+      ends = interlace(input_shape, ends, subscripts);
+      steps = interlace(Similar(input_shape, 1), steps, subscripts);
     }
+    decodeOnnxStartsAndEnds(input_shape, steps, starts, ends);
+    // TensorRT uses sizes of the output dimensions instead of ends.
+    nvinfer1::ITensor* sizes =
+        computeSliceSizes(ctx, starts, ends, steps, input_shape);
 
-    // expand the batch dimension for static shape
+    nvinfer1::ISliceLayer* slice = addSlice(ctx, tensor, starts, sizes, steps);
+    nvinfer1::ITensor* flippedTensor = *slice->getOutput(0);
+
+    // Declare inputs
+    auto* input = engine_->GetITensor(op_desc.Input("X")[0]);
+    int dims = input->getDimensions().nbDims;
+    nvinfer1::IShuffleLayer* layer = nullptr;
     if (!engine_->with_dynamic_shape()) {
-      nvinfer1::Dims reshape_dim;
-      reshape_dim.nbDims = input_dims.nbDims + 1;
-      reshape_dim.d[0] = 1;
-      for (int i = 0; i < input_dims.nbDims; ++i) {
-        reshape_dim.d[i + 1] = input_dims.d[i];
+      int dim_prod = 1;
+      for (int i = 0; i < dims; i++) {
+        int dim_i = input->getDimensions().d[i];
+        PADDLE_ENFORCE_GT(
+            dim_i,
+            0,
+            platform::errors::InvalidArgument(
+                "flatten input dim should be > 0, but got %d.", dim_i));
+        dim_prod *= dim_i;
       }
-      auto* reshape_layer = TRT_ENGINE_ADD_LAYER(engine_, Shuffle, *input);
-      reshape_layer->setReshapeDimensions(reshape_dim);
-      input = reshape_layer->getOutput(0);
-    }
-
-    auto* input_shape = Shape(input);
-    // get the sequence length at the axis dimension
-    auto* sequence_lens = Gather(input_shape, std::vector<int>{axis});
-
-    auto output_name = op_desc.Output("Out")[0];
-    auto* reverse_layer =
-        TRT_ENGINE_ADD_LAYER(engine_, ReverseSequence, *input, *sequence_lens);
-    reverse_layer->setBatchAxis(axis);
-    reverse_layer->setSequenceAxis(axis);
-
-    // remove the batch dimension for static shape
-    if (!engine_->with_dynamic_shape()) {
-      auto* reshape_layer =
-          TRT_ENGINE_ADD_LAYER(engine_, Shuffle, *reverse_layer->getOutput(0));
-      reshape_layer->setReshapeDimensions(input_dims);
-      RreplenishLayerAndOutput(reshape_layer,
-                               "flip",
-                               {output_name + "_value", output_name},
-                               test_mode);
+      nvinfer1::Dims flatten_dim;
+      flatten_dim.nbDims = 1;
+      flatten_dim.d[0] = dim_prod;
+      layer = TRT_ENGINE_ADD_LAYER(engine_, Shuffle, *input);
+      layer->setReshapeDimensions(flatten_dim);
     } else {
-      RreplenishLayerAndOutput(reverse_layer,
-                               "flip",
-                               {output_name + "_value", output_name},
-                               test_mode);
+      auto* shape_layer = TRT_ENGINE_ADD_LAYER(engine_, Shape, *input);
+      nvinfer1::Dims start_dim, size_dim, stride_dim;
+      start_dim.nbDims = 1;
+      size_dim.nbDims = 1;
+      stride_dim.nbDims = 1;
+      start_dim.d[0] = 1;
+      size_dim.d[0] = dims - 1;
+      stride_dim.d[0] = 1;
+      auto* slice_layer = TRT_ENGINE_ADD_LAYER(engine_,
+                                               Slice,
+                                               *(shape_layer->getOutput(0)),
+                                               start_dim,
+                                               size_dim,
+                                               stride_dim);
+      uint32_t reduce_dim = 1;
+      auto* reduce_prod_layer =
+          TRT_ENGINE_ADD_LAYER(engine_,
+                               Reduce,
+                               *(slice_layer->getOutput(0)),
+                               nvinfer1::ReduceOperation::kPROD,
+                               reduce_dim,
+                               true);
+      int32_t* constant_weight_data = new int32_t[1];
+      constant_weight_data[0] = -1;
+      TensorRTEngine::Weight constant_weight{
+          nvinfer1::DataType::kINT32,
+          static_cast<void*>(constant_weight_data),
+          1};
+      nvinfer1::Dims constant_dims;
+      constant_dims.nbDims = 1;
+      constant_dims.d[0] = 1;
+      auto* constant_layer = TRT_ENGINE_ADD_LAYER(
+          engine_, Constant, constant_dims, constant_weight.get());
+      std::vector<nvinfer1::ITensor*> itensors;
+      itensors.push_back(constant_layer->getOutput(0));
+      itensors.push_back(reduce_prod_layer->getOutput(0));
+      auto* concat_layer =
+          TRT_ENGINE_ADD_LAYER(engine_, Concatenation, itensors.data(), 2);
+      concat_layer->setAxis(0);
+      layer = TRT_ENGINE_ADD_LAYER(engine_, Shuffle, *input);
+      layer->setInput(1, *(concat_layer->getOutput(0)));
     }
-#else
-    VLOG(3) << "Flip is not supported when TensorRT < 8.6";
-#endif
+    auto output_name = op_desc.Output("Out")[0];
+    RreplenishLayerAndOutput(layer, "flatten", {output_name}, test_mode);
+  }
+
+  privete :
+
+      nvinfer1::ITensor*
+      Similar(nvinfer1::ITensor* exemplar) {}
+
+  nvinfer1::ITensor* AxesToInterlaceSubscripts(const nvinfer1::ITensor* axes,
+                                               int nbDims) {
+    std::vector<int64_t> subscripts(nbDims);
+    std::iota(subscripts.begin(), subscripts.end(), 0);
+    for (int32_t i = 0; i < axes->getDimensions().nbDims; ++i) {
+      subscripts[axes[i]] = nbDims + i;
+    }
+    return Add1DConstantLayer(subscripts);
   }
 };
 
