827355746679aac5b76d837b5688218004648778
tianshuo78520a <707759223@qq.com>
2023-05-15 19:47:29 +0800

    fix test_async_read_write

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -2645,7 +2645,7 @@ function parallel_test() {
         pip install ${PADDLE_ROOT}/dist/*whl
     fi
     cp ${PADDLE_ROOT}/build/test/legacy_test/testsuite.py ${PADDLE_ROOT}/build/python
-    cp -r ${PADDLE_ROOT}/build/test/legacy_test/white_list ${PADDLE_ROOT}/build/python
+    cp -r ${PADDLE_ROOT}/build/test/white_list ${PADDLE_ROOT}/build/python
     ut_total_startTime_s=`date +%s`
     if [ "$WITH_CINN" == "ON" ];then
         parallel_test_base_cinn
