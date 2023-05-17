e8c2ea2397e5c8f7807b3e5d54be42b9bdf55596
houj04 <houj04@foxmail.com>
2023-05-17 13:53:32 +0800

    refine code.

--- python/paddle/distributed/fleet/base/topology.py
+++ python/paddle/distributed/fleet/base/topology.py
@@ -189,7 +189,7 @@ class HybridCommunicateGroup:
 
         # create p2p_groups
         if self._pp_degree > 1:
-            if not paddle.is_compiled_with_xpu():
+            if paddle.framework.core.is_compiled_with_nccl():
                 check_nccl_version_for_p2p()
             self._set_p2p_group()
 
