fb77c02a39137022761acbcdfae85af56277ab7a
zhuyipin <yipinzhu@outlook.com>
2023-05-17 16:30:04 +0000

    add axis for maximum

--- paddle/fluid/prim/api/composite_backward/composite_backward_api.h
+++ paddle/fluid/prim/api/composite_backward/composite_backward_api.h
@@ -1505,6 +1505,7 @@ template <typename T>
 void maximum_grad(const Tensor& x,
                   const Tensor& y,
                   const Tensor& out_grad,
+                  int axis,
                   Tensor* x_grad,
                   Tensor* y_grad) {
   if (x_grad) {
