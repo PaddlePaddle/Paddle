c643ec71f8aa6ddd3ea52fc0b712d7a2a67c4109
Scotty <jmhgchn@gmail.com>
2023-05-18 11:12:18 +0000

    add static test

--- python/paddle/fluid/tests/unittests/test_sparse_slice_op.py
+++ python/paddle/fluid/tests/unittests/test_sparse_slice_op.py
@@ -18,8 +18,31 @@ import numpy as np
 
 import paddle
 
+data_5d = [
+    [[2, 3, 4, 5, 6], [0, 1, 2, 4], [0, 1, 2, -4], [3, 3, 4, -2]],
+]
+data_4d = [
+    [[2, 3, 4, 5], [0, 1, 2, 3], [0, 1, 2, -4], [3, 3, 4, -2]],
+]
 
-class TestSlice(unittest.TestCase):
+data_3d = [
+    [[4, 4, 5], [-3, -2, -1], [1, -3, 2], [3, 3, 4]],
+    [[4, 4, 5], [0, 1, 2], [0, 1, 2], [3, 3, 4]],
+    [[4, 4, 5], [-1], [0], [2]],
+    [[4, 4, 5], [0], [1], [2]],
+    [[4, 4, 5], [1], [2], [3]],
+    [[4, 4, 5], [1, 2], [2, 2], [3, 4]],
+    [[4, 4, 5], [0, 2], [2, 2], [3, 4]],
+]
+
+data_2d = [
+    [[3, 4], [0], [0], [2]],
+    [[3, 4], [1], [-3], [2]],
+    [[3, 4], [-2, -1], [-3, 0], [2, -1]],
+]
+
+
+class TestSparseSlice(unittest.TestCase):
     """
     Test the API paddle.sparse.slice on some sparse tensors.
     x: sparse, out: sparse
@@ -65,63 +88,118 @@ class TestSlice(unittest.TestCase):
         self._check_result(np_x, axes, starts, ends, format)
 
     def test_coo_5d(self):
-        self.check_result_with_shape(
-            [2, 3, 4, 5, 6],
-            [0, 1, 2, 4],
-            [0, 1, 2, -4],
-            [3, 3, 4, -2],
-            format='coo',
-        )
+        for item in data_5d:
+            self.check_result_with_shape(*item, format='coo')
 
     def test_coo_4d(self):
-        self.check_result_with_shape(
-            [2, 3, 4, 5],
-            [0, 1, 2, 3],
-            [0, 1, 2, -4],
-            [3, 3, 4, -2],
-            format='coo',
-        )
+        for item in data_4d:
+            self.check_result_with_shape(*item, format='coo')
 
     def test_coo_3d(self):
-        self.check_result_with_shape(
-            [4, 4, 5], [0, 1, 2], [0, 1, 2], [3, 3, 4], format='coo'
-        )
-        self.check_result_with_shape([4, 4, 5], [0], [0], [2], format='coo')
-        self.check_result_with_shape([4, 4, 5], [1], [2], [3], format='coo')
-        self.check_result_with_shape(
-            [4, 4, 5], [1, 2], [2, 2], [3, 4], format='coo'
-        )
-        self.check_result_with_shape(
-            [4, 4, 5], [0, 2], [2, 2], [3, 4], format='coo'
-        )
+        for item in data_3d:
+            self.check_result_with_shape(*item, format='coo')
 
     def test_coo_2d(self):
-        self.check_result_with_shape([3, 4], [0], [0], [2], format='coo')
+        for item in data_2d:
+            self.check_result_with_shape(*item, format='coo')
 
     def test_coo_1d(self):
         x = [-49, 55, -5, 0, 3, 0, 0, -60, -21, 0, 0, 0]
         self.check_result_with_list(x, [0], [-3], [-1], format='coo')
 
     def test_csr_3d(self):
-        self.check_result_with_shape(
-            [4, 4, 5], [0, 1, 2], [0, 1, 2], [3, 3, 4], format='csr'
-        )
-        self.check_result_with_shape([4, 4, 5], [0], [0], [2], format='csr')
-        self.check_result_with_shape([4, 4, 5], [1], [2], [3], format='csr')
-        self.check_result_with_shape(
-            [4, 4, 5], [1, 2], [2, 2], [3, 4], format='csr'
-        )
-        self.check_result_with_shape(
-            [4, 4, 5], [0, 2], [2, 2], [3, 4], format='csr'
-        )
+        for item in data_3d:
+            self.check_result_with_shape(*item, format='csr')
 
     def test_csr_2d(self):
-        self.check_result_with_shape([3, 4], [0], [0], [2], format='csr')
-        self.check_result_with_shape([3, 4], [1], [2], [3], format='csr')
-        self.check_result_with_shape([3, 4], [1], [-3], [-1], format='csr')
-        self.check_result_with_shape(
-            [3, 4], [0, 1], [0, 2], [-1, 3], format='csr'
-        )
+        for item in data_2d:
+            self.check_result_with_shape(*item, format='csr')
+
+
+class TestSparseCooSliceStatic(unittest.TestCase):
+    def _check_result_coo(self, np_x, axes, starts, ends):
+        x_shape = np_x.shape
+        dense_x = paddle.to_tensor(np_x, place=paddle.CPUPlace())
+        dense_x.stop_gradient = False
+        dense_out = paddle.slice(dense_x, axes, starts, ends)
+        sp_x = paddle.to_tensor(
+            np_x,
+            place=paddle.CPUPlace(),
+        ).to_sparse_coo(len(x_shape))
+        indices_data = sp_x.detach().indices()
+        values_data = sp_x.detach().values()
+
+        paddle.enable_static()
+        mp = paddle.static.Program()
+        sp = paddle.static.Program()
+        with paddle.static.program_guard(mp, sp):
+            indices = paddle.static.data(
+                name='indices',
+                shape=indices_data.shape,
+                dtype=indices_data.dtype,
+            )
+            values = paddle.static.data(
+                name='values',
+                shape=values_data.shape,
+                dtype=values_data.dtype,
+            )
+            sp_x = paddle.sparse.sparse_coo_tensor(
+                indices,
+                values,
+                shape=dense_x.shape,
+                dtype=dense_x.dtype,
+            )
+            sp_out = paddle.sparse.slice(sp_x, axes, starts, ends)
+            sp_dense_out = sp_out.to_dense()
+
+            exe = paddle.static.Executor()
+            res = exe.run(
+                feed={
+                    'indices': indices_data.numpy(),
+                    'values': values_data.numpy(),
+                },
+                fetch_list=[sp_dense_out],
+                return_numpy=True,
+            )
+            np.testing.assert_allclose(
+                dense_out.numpy(),
+                res[0],
+                rtol=1e-5,
+            )
+        paddle.disable_static()
+
+    def check_result_with_shape(
+        self, x_shape, axes, starts, ends, format='coo'
+    ):
+        mask = np.random.randint(0, 2, x_shape)
+        np_x = np.random.randint(-100, 100, x_shape) * mask
+        if format == 'coo':
+            self._check_result_coo(np_x, axes, starts, ends)
+
+    def check_result_with_list(self, x, axes, starts, ends, format='coo'):
+        np_x = np.array(x)
+        if format == 'coo':
+            self._check_result_coo(np_x, axes, starts, ends)
+
+    def test_coo_5d(self):
+        for item in data_5d:
+            self.check_result_with_shape(*item, format='coo')
+
+    def test_coo_4d(self):
+        for item in data_4d:
+            self.check_result_with_shape(*item, format='coo')
+
+    def test_coo_3d(self):
+        for item in data_3d:
+            self.check_result_with_shape(*item, format='coo')
+
+    def test_coo_2d(self):
+        for item in data_2d:
+            self.check_result_with_shape(*item, format='coo')
+
+    def test_coo_1d(self):
+        x = [-49, 55, -5, 0, 3, 0, 0, -60, -21, 0, 0, 0]
+        self.check_result_with_list(x, [0], [-3], [-1], format='coo')
 
 
 if __name__ == "__main__":
