0614b194e63ade76f42b46fbdf5bc1e18011f4e0
zxcd <228587199@qq.com>
2023-05-18 03:14:49 +0000

    add unitest for new case.

--- python/paddle/fluid/tests/unittests/test_arange.py
+++ python/paddle/fluid/tests/unittests/test_arange.py
@@ -176,6 +176,10 @@ class TestArangeImperative(unittest.TestCase):
         for i in [x1, x2, x3, x4]:
             self.assertEqual((i.numpy() == expected_data).all(), True)
 
+        x5 = paddle.arange(start, np.array([0.5], 'float32'), step)
+        x5_expected_data = np.arange(0, 0.5, 1).astype(np.float32)
+        self.assertEqual((x5.numpy() == x5_expected_data).all(), True)
+
 
 if __name__ == "__main__":
     unittest.main()
