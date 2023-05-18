775c1d2bda72166c6e3ffa079c3ec46ddef51c34
kangguangli <kangguangli@hotmail.com>
2023-05-18 10:07:06 +0000

    revert

--- paddle/ir/program.cc
+++ paddle/ir/program.cc
@@ -13,11 +13,47 @@
 // limitations under the License.
 
 #include "paddle/ir/program.h"
+
+#include <unordered_map>
+#include <vector>
+
 #include "paddle/ir/ir_context.h"
+#include "paddle/ir/value.h"
 
 namespace ir {
 Program::~Program() {
+  enum Mark { Unvisited, Temp, Perm };
+  std::unordered_map<void*, int> visited;
   for (auto op : ops_) {
+    visited[static_cast<void*>(op)] = Mark::Unvisited;
+  }
+
+  std::vector<Operation*> topology_order_ops;
+  topology_order_ops.reserve(ops_.size());
+
+  std::function<void(Operation*)> dfs;
+
+  dfs = [&](Operation* op) -> void {
+    auto* key = static_cast<void*>(op);
+    if (visited[key] == Mark::Perm) return;
+    if (visited[key] == Mark::Temp) throw("has cycle");
+
+    visited[key] = Mark::Temp;
+    for (size_t idx = 0; idx < op->num_results(); idx++) {
+      auto ret = op->GetResultByIndex(idx);
+      for (auto iter = ret.begin(); iter != ret.end(); ++iter) {
+        dfs(iter.owner());
+      }
+    }
+
+    visited[key] = Mark::Perm;
+    topology_order_ops.push_back(op);
+  };
+  if (topology_order_ops.size() > 0) {
+    dfs(topology_order_ops[0]);
+  }
+  std::reverse(topology_order_ops.begin(), topology_order_ops.end());
+  for (auto op : topology_order_ops) {
     op->destroy();
   }
 }
