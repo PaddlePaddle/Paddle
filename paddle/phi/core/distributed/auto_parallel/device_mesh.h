be1152a413b56d1af4c4ed3eb9be9aa0d0d35a7f
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-05-23 15:32:43 +0800

    [dist attr 迁移到 phi]Dist attr (#53848)
    * merge code from forsish
    
    * polish
    
    * paddle/fluid/pybind/auto_parallel_py.cc
    
    * polish
    
    * polish
    
    * polish
    
    * polish
    
    * polish
    
    * polish
    
    * polish
    
    * polish
    
    * polish
    
    * polish
    
    * polish

--- paddle/phi/core/distributed/auto_parallel/device_mesh.h
+++ paddle/phi/core/distributed/auto_parallel/device_mesh.h
@@ -23,11 +23,11 @@ limitations under the License. */
 #include <string>
 #include <vector>
 
-#include "paddle/fluid/distributed/auto_parallel/auto_parallel.pb.h"
-#include "paddle/fluid/distributed/auto_parallel/utils.h"
-#include "paddle/fluid/platform/enforce.h"
+#include "paddle/phi/core/distributed/auto_parallel/auto_parallel.pb.h"
+#include "paddle/phi/core/distributed/auto_parallel/utils.h"
+#include "paddle/phi/core/enforce.h"
 
-namespace paddle {
+namespace phi {
 namespace distributed {
 namespace auto_parallel {
 struct DeviceCapability {
@@ -259,7 +259,7 @@ class DeviceMesh {
         return shape_[i];
       }
     }
-    PADDLE_THROW(platform::errors::InvalidArgument(
+    PADDLE_THROW(errors::InvalidArgument(
         "Cannot find the dimension of %s in this device mesh.", dim_name));
   }
 
@@ -298,4 +298,4 @@ inline bool operator!=(const DeviceMesh& lhs, const DeviceMesh& rhs) {
 
 }  // namespace auto_parallel
 }  // namespace distributed
-}  // namespace paddle
+}  // namespace phi
