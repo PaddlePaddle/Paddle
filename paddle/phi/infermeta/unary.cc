ebafbf7d06c5a374c691b8cd2fe19d5935180685
Wang Huan <wanghuan29@baidu.com>
2023-05-24 02:25:44 +0000

    refine

--- paddle/phi/infermeta/unary.cc
+++ paddle/phi/infermeta/unary.cc
@@ -3238,7 +3238,6 @@ void ReshapeWithXShapeInferMeta(const MetaTensor& x,
   xshape->set_dims(phi::make_ddim(xshape_dims));
   xshape->share_lod(x);
   ReshapeInferMeta(x, shape, out, config);
-  out->set_layout(static_cast<DataLayout>(x.layout()));
 }
 
 void ReverseInferMeta(const MetaTensor& x,
