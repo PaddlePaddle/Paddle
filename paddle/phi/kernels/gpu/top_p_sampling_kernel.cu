7a57bddadacd7ebf077802ab976e955288ce7102
carryyu <569782149@qq.com>
2023-05-26 19:11:19 +0800

    delete bf16 topp

--- paddle/phi/kernels/gpu/top_p_sampling_kernel.cu
+++ paddle/phi/kernels/gpu/top_p_sampling_kernel.cu
@@ -41,10 +41,10 @@ struct DataTypeTraits<phi::dtype::float16> {
   using DataType = half;
 };
 
-template <>
-struct DataTypeTraits<phi::dtype::bfloat16> {
-  using DataType = __nv_bfloat16;
-};
+// template <>
+// struct DataTypeTraits<phi::dtype::bfloat16> {
+//   using DataType = __nv_bfloat16;
+// };
 
 #define FINAL_MASK 0xFFFFFFFF
 
