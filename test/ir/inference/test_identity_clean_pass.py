901fd6e3952f02a1fe119b45d8485791c25f9755
zhoutianzi666 <17801055074@163.com>
2023-05-26 07:48:50 +0000

    commit

--- test/ir/inference/test_identity_clean_pass.py
+++ test/ir/inference/test_identity_clean_pass.py
@@ -18,8 +18,6 @@ import hypothesis.strategies as st
 from auto_scan_test import PassAutoScanTest
 from program_config import OpConfig, ProgramConfig, TensorConfig
 
-import paddle.inference as paddle_infer
-
 
 class TestIdentityScaleCleanPass(PassAutoScanTest):
     def sample_predictor_configs(self, program_config):
@@ -54,45 +52,6 @@ class TestIdentityScaleCleanPass(PassAutoScanTest):
 
     def test(self):
         self.run_and_statis(max_examples=25, passes=["identity_op_clean_pass"])
-
-
-class TestDeleteCIdentityPass(PassAutoScanTest):
-    def sample_predictor_configs(self, program_config):
-        config = self.create_trt_inference_config()
-        config.enable_tensorrt_engine(
-            max_batch_size=8,
-            workspace_size=0,
-            min_subgraph_size=0,
-            precision_mode=paddle_infer.PrecisionType.Float32,
-            use_static=False,
-            use_calib_mode=False,
-        )
-        yield config, ['relu'], (1e-5, 1e-5)
-
-    def sample_program_config(self, draw):
-        n = draw(st.integers(min_value=1, max_value=2))
-        relu_op = OpConfig(
-            "relu", inputs={"X": ["relu_x"]}, outputs={"Out": ["relu_out"]}
-        )
-        c_identity_op = OpConfig(
-            "c_identity",
-            inputs={"X": ["relu_out"]},
-            outputs={"Out": ["id_out"]},
-        )
-        program_config = ProgramConfig(
-            ops=[relu_op, c_identity_op],
-            weights={},
-            inputs={"relu_x": TensorConfig(shape=[n])},
-            outputs=["id_out"],
-        )
-        return program_config
-
-    def test(self):
-        self.run_and_statis(
-            max_examples=2,
-            min_success_num=2,
-            passes=["identity_op_clean_pass"],
-        )
 
 
 if __name__ == "__main__":
