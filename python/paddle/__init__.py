172f97d2d1c5180b9b26cd4d04ff2e7c7f74dac9
lzy <569782149@qq.com>
2023-05-29 10:31:58 +0800

    cherry-pick (#54127)

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -445,6 +445,7 @@ from .tensor.search import (
     sort,
     kthvalue,
     mode,
+    top_p_sampling,
 )
 
 from .tensor.to_string import set_printoptions
@@ -726,6 +727,7 @@ __all__ = [
     'zeros_like',
     'maximum',
     'topk',
+    'top_p_sampling',
     'index_select',
     'CPUPlace',
     'matmul',
