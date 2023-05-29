4fbc2fbe92bc8ed9c9839f5c894bfacb7649c6b9
huangjiyi <947613776@qq.com>
2023-05-29 16:38:22 +0800

    update

--- paddle/fluid/operators/generator/get_expected_kernel_func.h
+++ paddle/fluid/operators/generator/get_expected_kernel_func.h
@@ -44,6 +44,10 @@ phi::KernelKey GetSoftmaxGradExpectedKernelType(
     const framework::ExecutionContext& ctx,
     const framework::OperatorWithKernel* op_ptr);
 
+phi::KernelKey GetStridedSliceExpectedKernelType(
+    const framework::ExecutionContext& ctx,
+    const framework::OperatorWithKernel* op_ptr);
+
 phi::KernelKey GetUpdateLossScalingExpectedKernelType(
     const framework::ExecutionContext& ctx,
     const framework::OperatorWithKernel* op_ptr);
