a8db9533dc79b94d3fa2207e279170ddd59d148c
jiahongyu <jiahongyu@baidu.com>
2023-05-30 13:39:34 +0000

    [Polish Unittest] Polish test_zero_dim_tensor.py

--- python/paddle/fluid/tests/unittests/test_zero_dim_tensor.py
+++ python/paddle/fluid/tests/unittests/test_zero_dim_tensor.py
@@ -3723,7 +3723,7 @@ class TestSundryAPIStatic(unittest.TestCase):
 
         x1 = paddle.uniform([], None, -10, 10)
         x1.stop_gradient = False
-        out1 = paddle.clip(x1, paddle.full([], 5.0), paddle.full([], 5.0))
+        out1 = paddle.clip(x1, paddle.full([], -5.0), paddle.full([], 5.0))
         paddle.static.append_backward(out1)
 
         prog = paddle.static.default_main_program()
