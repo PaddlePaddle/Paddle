abcadd490b05e2be9f318159135cc76bcb3fc16d
hitywt <yuwentao126@126.com>
2023-05-31 15:10:17 +0800

    add all_to_all phi operator

--- test/collective/test_collective_alltoall_api.py
+++ test/collective/test_collective_alltoall_api.py
@@ -28,6 +28,28 @@ class TestCollectiveAllToAllAPI(TestDistBase):
     def test_alltoall_nccl(self):
         self.check_with_place("collective_alltoall_api.py", "alltoall", "nccl")
 
+    def test_alltoall_nccl_with_comm_context(self):
+        dtypes_to_test = [
+            "float16",
+            "float32",
+            "float64",
+            "int32",
+            "int64",
+            "int8",
+            "uint8",
+            "bool",
+        ]
+        if self._nccl_version >= 2100:
+            dtypes_to_test.append("bfloat16")
+        for dtype in dtypes_to_test:
+            self.check_with_place(
+                "collective_alltoall_api.py",
+                "alltoall",
+                "nccl",
+                dtype=dtype,
+                need_envs={"USE_COMM_CONTEXT": "1"},
+            )
+
     def test_alltoall_nccl_dygraph(self):
         dtypes_to_test = [
             "float16",
