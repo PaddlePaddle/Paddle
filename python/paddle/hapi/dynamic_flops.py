3128e347ef1fa80e46461ef899c890897ecb9110
tianshuo78520a <707759223@qq.com>
2023-05-31 14:07:45 +0800

    test=document_fix

--- python/paddle/hapi/dynamic_flops.py
+++ python/paddle/hapi/dynamic_flops.py
@@ -102,6 +102,7 @@ def flops(net, input_size, custom_ops=None, print_detail=False):
             #|   linear_2   |     [1, 84]     |     [1, 10]     |  850   |  840   |
             #+--------------+-----------------+-----------------+--------+--------+
             #Total Flops: 347560     Total Params: 61610
+            print(111)
     """
     if isinstance(net, nn.Layer):
         # If net is a dy2stat model, net.forward is StaticFunction instance,
