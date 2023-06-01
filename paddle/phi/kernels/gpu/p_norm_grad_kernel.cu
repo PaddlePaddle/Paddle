ac9a792f7a1a1cd60c127fbf62aaf735252be75e
WintersMontagne10335 <22251099@zju.edu.cn>
2023-06-01 14:26:39 +0000

    Modify p_norm_grad_kernel.cu

--- paddle/phi/kernels/gpu/p_norm_grad_kernel.cu
+++ paddle/phi/kernels/gpu/p_norm_grad_kernel.cu
@@ -23,68 +23,42 @@
 #include "paddle/phi/kernels/funcs/elementwise_base.h"
 
 namespace phi {
+
 template <typename T>
-__device__ __forceinline__ int sgn(T val) {
+__device__ __forceinline__ int inline_sgn(T val) {
   return (T(0) < val) - (val < T(0));
 }
 
-__device__ __forceinline__ int inline_sign(dtype::float16 x) {
-  return sgn<dtype::float16>(x);
-}
-__device__ __forceinline__ int inline_sign(dtype::bfloat16 x) {
-  return sgn<dtype::bfloat16>(x);
-}
-__device__ __forceinline__ int inline_sign(float x) { return sgn<float>(x); }
-__device__ __forceinline__ int inline_sign(double x) { return sgn<double>(x); }
-
-__device__ __forceinline__ dtype::float16 inline_abs(dtype::float16 x) {
-  return static_cast<dtype::float16>(abs(static_cast<float>(x)));
-}
-
-__device__ __forceinline__ dtype::bfloat16 inline_abs(dtype::bfloat16 x) {
-  return static_cast<dtype::bfloat16>(abs(static_cast<float>(x)));
-}
-
-__device__ __forceinline__ float inline_abs(float x) { return abs(x); }
-
-__device__ __forceinline__ double inline_abs(double x) { return abs(x); }
-
-__device__ __forceinline__ dtype::float16 inline_pow(dtype::float16 base,
-                                                     dtype::float16 exponent) {
-  return static_cast<dtype::float16>(
+template <typename T>
+__device__ __forceinline__ T inline_pow(T base, T exponent) {
+  return static_cast<T>(
       pow(static_cast<float>(base), static_cast<float>(exponent)));
 }
-__device__ __forceinline__ dtype::bfloat16 inline_pow(
-    dtype::bfloat16 base, dtype::bfloat16 exponent) {
-  return static_cast<dtype::bfloat16>(
-      pow(static_cast<float>(base), static_cast<float>(exponent)));
-}
-__device__ __forceinline__ float inline_pow(float base, float exponent) {
-  return pow(base, exponent);
-}
+
+template <>
 __device__ __forceinline__ double inline_pow(double base, double exponent) {
   return pow(base, exponent);
 }
 
-dtype::float16 host_pow(dtype::float16 base, dtype::float16 exponent) {
-  return static_cast<dtype::float16>(
-      pow(static_cast<float>(base), static_cast<float>(exponent)));
-}
-dtype::bfloat16 host_pow(dtype::bfloat16 base, dtype::bfloat16 exponent) {
-  return static_cast<dtype::bfloat16>(
-      pow(static_cast<float>(base), static_cast<float>(exponent)));
+template <typename T>
+__device__ __forceinline__ T inline_abs(T x) {
+  return static_cast<T>(abs(static_cast<float>(x)));
 }
 
-float host_pow(float base, float exponent) { return pow(base, exponent); }
-double host_pow(double base, double exponent) { return pow(base, exponent); }
+template <>
+__device__ __forceinline__ double inline_abs(double x) {
+  return abs(x);
+}
 
 template <typename T>
 struct PNormGradScalarDirectCUDAFunctor {
+ private:
   const T* y_;
   const T* dy_;
   const T epsilon_;
   const T porder_;
 
+ public:
   HOSTDEVICE inline PNormGradScalarDirectCUDAFunctor(const T* y,
                                                      const T* dy,
                                                      const T epsilon,
@@ -96,48 +70,53 @@ struct PNormGradScalarDirectCUDAFunctor {
 
   HOSTDEVICE inline T operator()(const T x) const {
     const T scalar =
-        dy_[0] * inline_pow(y_[0] + epsilon_, static_cast<T>(-1) * porder_);
-    return static_cast<T>(static_cast<T>(inline_sign(x)) *
-                          inline_pow(inline_abs(x), porder_) * scalar);
+        dy_[0] * inline_pow<T>(y_[0] + epsilon_, static_cast<T>(-1) * porder_);
+    return static_cast<T>(static_cast<T>(inline_sgn<T>(x)) *
+                          inline_pow<T>(inline_abs<T>(x), porder_) * scalar);
   }
 };
 
 template <typename T>
 struct InfinityNormGradScalarDirectCUDAFunctor {
+ private:
   const T* y_;
   const T* dy_;
 
+ public:
   HOSTDEVICE inline InfinityNormGradScalarDirectCUDAFunctor(const T* y,
                                                             const T* dy)
       : y_(y), dy_(dy) {}
 
   HOSTDEVICE inline T operator()(const T x) const {
-    return static_cast<T>(dy_[0] * static_cast<T>(inline_sign(x)) *
-                          static_cast<T>((inline_abs(x) == y_[0])));
+    return static_cast<T>(dy_[0] * static_cast<T>(inline_sgn<T>(x)) *
+                          static_cast<T>((inline_abs<T>(x) == y_[0])));
   }
 };
 
 template <typename T>
 struct InfinityNormGradTensorDirectCUDAFunctor {
   HOSTDEVICE inline T operator()(const T x, const T y, const T dy) const {
-    return static_cast<T>(dy * static_cast<T>(inline_sign(x)) *
-                          static_cast<T>(inline_abs(x) == y));
+    return static_cast<T>(dy * static_cast<T>(inline_sgn<T>(x)) *
+                          static_cast<T>(inline_abs<T>(x) == y));
   }
 };
 
 template <typename T>
 struct PNormGradTensorDirectCUDAFunctor {
+ private:
   const T epsilon_;
   const T porder_;
 
+ public:
   HOSTDEVICE inline PNormGradTensorDirectCUDAFunctor(const T epsilon,
                                                      const T porder)
       : epsilon_(epsilon), porder_(porder - static_cast<T>(1.)) {}
 
   HOSTDEVICE inline T operator()(const T x, const T y, const T dy) const {
     return static_cast<T>(
-        static_cast<T>(inline_sign(x)) * inline_pow(inline_abs(x), porder_) *
-        dy * inline_pow(y + epsilon_, static_cast<T>(-1.0) * porder_));
+        static_cast<T>(inline_sgn<T>(x)) *
+        inline_pow<T>(inline_abs<T>(x), porder_) * dy *
+        inline_pow<T>(y + epsilon_, static_cast<T>(-1.0) * porder_));
   }
 };
 
@@ -207,6 +186,7 @@ void PNormGradKernel(const Context& dev_ctx,
     }
   }
 }
+
 }  // namespace phi
 PD_REGISTER_KERNEL(p_norm_grad,
                    GPU,
