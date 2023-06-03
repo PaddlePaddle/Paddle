ee06eadd6c9341ca624c8e021b30ced8996aeb08
gouzi <530971494@qq.com>
2023-06-03 15:57:21 +0800

    [cmake] change lapack

--- cmake/external/lapack.cmake
+++ cmake/external/lapack.cmake
@@ -15,7 +15,8 @@
 include(ExternalProject)
 
 set(LAPACK_PREFIX_DIR ${THIRD_PARTY_PATH}/lapack)
-set(LAPACK_SOURCE_DIR ${THIRD_PARTY_PATH}/lapack/src/extern_lapack)
+set(LAPACK_DOWNLOAD_DIR
+    ${PADDLE_SOURCE_DIR}/third_party/lapack/${CMAKE_SYSTEM_NAME})
 set(LAPACK_INSTALL_DIR ${THIRD_PARTY_PATH}/install/lapack)
 set(LAPACK_LIB_DIR ${LAPACK_INSTALL_DIR}/lib)
 
@@ -23,10 +24,10 @@ set(LAPACK_LIB_DIR ${LAPACK_INSTALL_DIR}/lib)
 # use lapack tag v3.10.0 on 06/28/2021 https://github.com/Reference-LAPACK/lapack
 if(LINUX)
   set(LAPACK_VER
-      "lapack_lnx_v3.10.0.20210628"
+      "lapack_lnx_v3.10.0.20210628.tar.gz"
       CACHE STRING "" FORCE)
   set(LAPACK_URL
-      "https://paddlepaddledeps.bj.bcebos.com/${LAPACK_VER}.tar.gz"
+      "https://paddlepaddledeps.bj.bcebos.com/${LAPACK_VER}"
       CACHE STRING "" FORCE)
   set(LAPACK_URL_MD5 71f8cc8237a8571692f3e07f9a4f25f6)
   set(GNU_RT_LIB_1 "${LAPACK_LIB_DIR}/libquadmath.so.0")
@@ -36,10 +37,10 @@ if(LINUX)
 elseif(WIN32)
   # Refer to [lapack-for-windows] http://icl.cs.utk.edu/lapack-for-windows/lapack/#lapacke
   set(LAPACK_VER
-      "lapack_win_v3.10.0.20210628"
+      "lapack_win_v3.10.0.20210628.zip"
       CACHE STRING "" FORCE)
   set(LAPACK_URL
-      "https://paddlepaddledeps.bj.bcebos.com/${LAPACK_VER}.zip"
+      "https://paddlepaddledeps.bj.bcebos.com/${LAPACK_VER}"
       CACHE STRING "" FORCE)
   set(LAPACK_URL_MD5 590d080392dcd5abbd5dca767a50b63a)
   set(GNU_RT_LIB_1 "${LAPACK_LIB_DIR}/libquadmath-0.dll")
@@ -49,10 +50,10 @@ elseif(WIN32)
   set(LAPACK_LIB "${LAPACK_LIB_DIR}/liblapack.dll")
 else()
   set(LAPACK_VER
-      "lapack_mac_v3.10.0.20210628"
+      "lapack_mac_v3.10.0.20210628.tar.gz"
       CACHE STRING "" FORCE)
   set(LAPACK_URL
-      "https://paddlepaddledeps.bj.bcebos.com/${LAPACK_VER}.tar.gz"
+      "https://paddlepaddledeps.bj.bcebos.com/${LAPACK_VER}"
       CACHE STRING "" FORCE)
   set(LAPACK_URL_MD5 427aecf8dee8523de3566ca8e47944d7)
   set(GNU_RT_LIB_1 "${LAPACK_LIB_DIR}/libquadmath.0.dylib")
@@ -62,18 +63,44 @@ else()
   set(LAPACK_LIB "${LAPACK_LIB_DIR}/liblapack.3.dylib")
 endif()
 
+find_file(
+  LOCAL_LAPACK_LIB_ZIP
+  NAMES ${LAPACK_VER}
+  PATHS ${LAPACK_DOWNLOAD_DIR}
+  NO_DEFAULT_PATH)
+
+# Download lapack.
+if(NOT LOCAL_LAPACK_LIB_ZIP)
+  message(
+    STATUS "Downloading ${LAPACK_URL} to ${LAPACK_DOWNLOAD_DIR}/${LAPACK_VER}")
+  # NOTE: If the version is updated, consider emptying the folder; maybe add timeout
+  file(
+    DOWNLOAD ${LAPACK_URL} ${LAPACK_DOWNLOAD_DIR}/${LAPACK_VER}
+    EXPECTED_MD5 ${LAPACK_URL_MD5}
+    STATUS ERR)
+  if(ERR EQUAL 0)
+    message(STATUS "Download ${LAPACK_VER} success")
+  else()
+    message(
+      FATAL_ERROR
+        "Download failed, error: ${ERR}\n You can try downloading ${LAPACK_VER} again"
+    )
+  endif()
+endif()
+
 ExternalProject_Add(
   extern_lapack
   ${EXTERNAL_PROJECT_LOG_ARGS}
-  URL ${LAPACK_URL}
+  URL ${LAPACK_DOWNLOAD_DIR}/${LAPACK_VER}
   URL_MD5 ${LAPACK_URL_MD5}
+  DOWNLOAD_DIR ${LAPACK_DOWNLOAD_DIR}
+  SOURCE_DIR ${LAPACK_LIB_DIR}
   PREFIX ${LAPACK_PREFIX_DIR}
   DOWNLOAD_NO_PROGRESS 1
   PATCH_COMMAND ""
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory ${LAPACK_SOURCE_DIR}
-                  ${LAPACK_LIB_DIR}
+  INSTALL_COMMAND ${CMAKE_COMMAND}
   BUILD_BYPRODUCTS ${BLAS_LIB}
   BUILD_BYPRODUCTS ${LAPACK_LIB})
