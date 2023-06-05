ed604569b96774b970e52046d403c6c58fa7995c
PommesPeter <54879512+PommesPeter@users.noreply.github.com>
2023-06-05 17:24:31 +0800

    【Hackathon 4 No.19】Add polygamma API to Paddle (#53791)
    * feat: added polygamma init code
    
    * feat: added polygamma unittest code
    
    * test: added more test cases
    
    * refactor: added forward impl
    
    * refactor: added backward impl
    
    * test: updated cases
    
    * refactor: updated test cases
    
    * refactor: added more case and fixed some bugs
    
    * test: updated ref func
    
    * refactor: updated code style
    
    * refactor: move the code
    
    * refactor: updated test
    
    * refactor: updated test
    
    * docs: updated en doc
    
    Co-authored-by: zachary sun <70642955+sunzhongkai588@users.noreply.github.com>
    
    * docs: updated math eq
    
    ---------
    
    Co-authored-by: zachary sun <70642955+sunzhongkai588@users.noreply.github.com>

--- python/paddle/tensor/__init__.py
+++ python/paddle/tensor/__init__.py
@@ -261,6 +261,7 @@ from .math import i0  # noqa: F401
 from .math import i0e  # noqa: F401
 from .math import i1  # noqa: F401
 from .math import i1e  # noqa: F401
+from .math import polygamma  # noqa: F401
 
 from .random import multinomial  # noqa: F401
 from .random import standard_normal  # noqa: F401
@@ -560,6 +561,7 @@ tensor_method_func = [  # noqa
     'i0e',
     'i1',
     'i1e',
+    'polygamma',
 ]
 
 # this list used in math_op_patch.py for magic_method bind
