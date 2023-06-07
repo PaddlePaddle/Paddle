85683ba1286d9f66a2f95133f33b56fcc390096d
hitywt <yuwentao126@126.com>
2023-06-07 15:50:35 +0800

    prepare for collective communicate upgrade in dygraph

--- paddle/fluid/framework/new_executor/interpreter/interpreter_util.h
+++ paddle/fluid/framework/new_executor/interpreter/interpreter_util.h
@@ -66,7 +66,7 @@ class AsyncWorkQueue {
   std::unique_ptr<WorkQueueGroup> queue_group_;
 };
 
-bool IsCommunicationOp(const std::string& op_name);
+bool IsCommunicationOp(const OperatorBase* op);
 
 bool IsCommunicationOp(const Instruction& instr);
 
