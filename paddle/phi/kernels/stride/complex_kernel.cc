6d9f7b36f767098cd838dfbff39c9085d8427f51
Wang Huan <wanghuan29@baidu.com>
2023-06-07 04:32:45 +0000

    refine

---
+++ paddle/phi/kernels/stride/complex_kernel.cc
@@ -0,0 +1,53 @@
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "paddle/phi/kernels/complex_kernel.h"
+#include "paddle/phi/backends/all_context.h"
+#include "paddle/phi/core/kernel_registry.h"
+
+namespace phi {
+
+template <typename Context>
+void RealStridedKernel(const Context& dev_ctx,
+                       const DenseTensor& x,
+                       DenseTensor* out) {
+  DDim stride = out->stride();
+  for (int i = 0; i < stride.size(); i++) {
+    stride[i] = out->stride()[i] * 2;
+  }
+  out->set_stride(stride);
+  out->ResetHolder(x.Holder());
+}
+
+template <typename Context>
+void ImagStridedKernel(const Context& dev_ctx,
+                       const DenseTensor& x,
+                       DenseTensor* out) {
+  DDim stride = out->stride();
+  for (int i = 0; i < stride.size(); i++) {
+    stride[i] = out->stride()[i] * 2;
+  }
+  out->set_stride(stride);
+  out->set_offset(phi::SizeOf(out->dtype()));
+  out->ResetHolder(x.Holder());
+}
+
+}  // namespace phi
+
+PD_REGISTER_KERNEL_FOR_ALL_BACKEND_DTYPE(real,
+                                         STRIDED,
+                                         phi::RealStridedKernel) {}
+
+PD_REGISTER_KERNEL_FOR_ALL_BACKEND_DTYPE(imag,
+                                         STRIDED,
+                                         phi::ImagStridedKernel) {}
--- paddle/phi/kernels/stride/unsqueeze_grad_kernel.cc
+++ paddle/phi/kernels/stride/unsqueeze_grad_kernel.cc
@@ -1,5 +1,4 @@
-
-// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,22 +11,26 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#pragma once
-
-#include "paddle/phi/core/dense_tensor.h"
+#include "paddle/phi/backends/all_context.h"
+#include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/flatten_grad_kernel.h"
+#include "paddle/phi/kernels/reshape_kernel.h"
 
 namespace phi {
-
-template <typename T, typename Context>
-void UnsqueezeGradKernel(const Context& dev_ctx,
-                         const DenseTensor& x_shape,
-                         const DenseTensor& dout,
-                         DenseTensor* dx);
 
 template <typename Context>
 void UnsqueezeGradStridedKernel(const Context& dev_ctx,
                                 const DenseTensor& x_shape,
                                 const DenseTensor& dout,
-                                DenseTensor* dx);
+                                DenseTensor* dx) {
+  auto xshape_dims = xshape.dims();
+  auto x_dims = phi::slice_ddim(xshape_dims, 1, xshape_dims.size());
+  ReshapeStridedKernel<Context>(
+      dev_ctx, dout, IntArray(phi::vectorize<int64_t>(x_dims)), dx, nullptr);
+}
+
 }  // namespace phi
+
+PD_REGISTER_KERNEL_FOR_ALL_BACKEND_DTYPE(unsqueeze_grad,
+                                         STRIDED,
+                                         phi::UnsqueezeGradStridedKernel) {}
