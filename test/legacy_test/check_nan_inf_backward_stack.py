253ccbfcd1bc4553b74de535a2887d3301296450
niuliling123 <51102941+niuliling123@users.noreply.github.com>
2023-06-07 12:15:57 +0800

    Update check_nan_inf_backward_stack.py

--- test/legacy_test/check_nan_inf_backward_stack.py
+++ test/legacy_test/check_nan_inf_backward_stack.py
@@ -18,6 +18,7 @@ import paddle
 
 def main():
     paddle.set_flags({"FLAGS_check_nan_inf": 1, "FLAGS_check_nan_inf_level": 0})
+    paddle.set_flags({"FLAGS_check_nan_inf_stack_limit": 1})
     cpu_place = paddle.CPUPlace()
     x = paddle.to_tensor([1, 0.0, 3], stop_gradient=False, place=cpu_place)
     y = paddle.to_tensor([0.2, 0.0, 0.5], place=cpu_place)
