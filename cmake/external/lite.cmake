8765abfa0ffc1f40042066cf7ef73a28576e8843
enkilee <luhputu0815@gmail.com>
2023-06-08 16:12:12 +0000

    fix

--- cmake/external/lite.cmake
+++ cmake/external/lite.cmake
@@ -62,22 +62,40 @@ endif()
 
 if(NOT LITE_SOURCE_DIR OR NOT LITE_BINARY_DIR)
   include(ExternalProject)
+  set(LITE_SOURCE_DIR ${PADDLE_SOURCE_DIR}/third_party/lite)
+  set(LITE_GIT_TAG 81ef66554099800c143a0feff6e0a491b3b0d12e)
   set(LITE_PROJECT extern_lite)
   set(LITE_PREFIX_DIR ${THIRD_PARTY_PATH}/lite)
   set(LITE_INSTALL_DIR ${THIRD_PARTY_PATH}/install/lite)
-  set(LITE_BINARY_DIR ${LITE_PREFIX_DIR}/src/extern_lite-build)
-  set(LITE_SOURCE_DIR ${LITE_PREFIX_DIR}/src/extern_lite)
+  set(LITE_BINARY_DIR ${LITE_SOURCE_DIR}/src/extern_lite-build)
 
   set(LITE_SHARED_LIB
       ${LITE_BINARY_DIR}/${LITE_OUTPUT_BIN_DIR}/cxx/lib/libpaddle_full_api_shared.so
   )
 
-  if(NOT LITE_GIT_TAG)
-    set(LITE_GIT_TAG 81ef66554099800c143a0feff6e0a491b3b0d12e)
-  endif()
-
   if(NOT CUDA_ARCH_NAME)
     set(CUDA_ARCH_NAME "Auto")
+  endif()
+
+  file(GLOB LITE_SOURCE_FILE_LIST ${LITE_SOURCE_DIR})
+  list(LENGTH LITE_SOURCE_FILE_LIST RES_LEN)
+  if(RES_LEN EQUAL 0)
+    execute_process(
+      COMMAND
+        ${GIT_EXECUTABLE} clone -b ${LITE_GIT_TAG}
+        "https://github.com/PaddlePaddle/Paddle-Lite.git" ${LITE_SOURCE_DIR})
+  else()
+    # check git tag
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} describe --abbrev=6 --always --tags
+      OUTPUT_VARIABLE VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET
+      WORKING_DIRECTORY ${LITE_SOURCE_DIR})
+    if(NOT ${VERSION} STREQUAL ${LITE_TAG})
+      message(WARNING "lite version is not ${VERSION}, checkout to ${LITE_TAG}")
+      execute_process(COMMAND ${GIT_EXECUTABLE} checkout ${LITE_TAG}
+                      WORKING_DIRECTORY ${LITE_SOURCE_DIR})
+    endif()
   endif()
 
   # No quotes, so cmake can resolve it as a command with arguments.
@@ -108,8 +126,7 @@ if(NOT LITE_SOURCE_DIR OR NOT LITE_BINARY_DIR)
     ExternalProject_Add(
       ${LITE_PROJECT}
       ${EXTERNAL_PROJECT_LOG_ARGS}
-      GIT_REPOSITORY "${GIT_URL}/PaddlePaddle/Paddle-Lite.git"
-      GIT_TAG ${LITE_GIT_TAG}
+      SOURCE_DIR ${LITE_SOURCE_DIR}
       PREFIX ${LITE_PREFIX_DIR}
       PATCH_COMMAND
         mkdir -p ${LITE_PREFIX_DIR}/src/extern_lite-build/lite/gen_code && touch
@@ -161,8 +178,7 @@ if(NOT LITE_SOURCE_DIR OR NOT LITE_BINARY_DIR)
     ExternalProject_Add(
       ${LITE_PROJECT}
       ${EXTERNAL_PROJECT_LOG_ARGS}
-      GIT_REPOSITORY "${GIT_URL}/PaddlePaddle/Paddle-Lite.git"
-      GIT_TAG ${LITE_GIT_TAG}
+      SOURCE_DIR ${LITE_SOURCE_DIR}
       PREFIX ${LITE_PREFIX_DIR}
       UPDATE_COMMAND ""
       PATCH_COMMAND
