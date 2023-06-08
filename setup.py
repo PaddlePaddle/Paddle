d5f7f355ff755bd8b3fabb283f47fbc76d7ec566
YuanRisheng <yuanrisheng@baidu.com>
2023-06-08 07:50:31 +0000

    add phi option in cmake

--- setup.py
+++ setup.py
@@ -967,7 +967,7 @@ def get_package_data_and_package_dir():
     libs_path = paddle_binary_dir + '/python/paddle/libs'
     package_data['paddle.libs'] = []
 
-    if env_dict.get("WITH_PHI_SHARED") == "ON":
+    if env_dict.get("WITH_SHARED_PHI") == "ON":
         package_data['paddle.libs'] = [
             ('libphi' if os.name != 'nt' else 'phi') + ext_suffix
         ]
@@ -1211,7 +1211,7 @@ def get_package_data_and_package_dir():
                     + env_dict.get("FLUID_CORE_NAME")
                     + '.so'
                 )
-                if env_dict.get("WITH_PHI_SHARED") == "ON":
+                if env_dict.get("WITH_SHARED_PHI") == "ON":
                     commands.append(
                         "install_name_tool -add_rpath '@loader_path' "
                         + env_dict.get("PADDLE_BINARY_DIR")
@@ -1226,7 +1226,7 @@ def get_package_data_and_package_dir():
                     + env_dict.get("FLUID_CORE_NAME")
                     + '.so'
                 ]
-                if env_dict.get("WITH_PHI_SHARED") == "ON":
+                if env_dict.get("WITH_SHARED_PHI") == "ON":
                     commands.append(
                         "patchelf --set-rpath '$ORIGIN' "
                         + env_dict.get("PADDLE_BINARY_DIR")
