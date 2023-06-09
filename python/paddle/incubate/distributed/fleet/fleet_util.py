8f65f72e8932570ed4cfb028f724f00d83a2072c
Nyakku Shigure <sigure.qaq@gmail.com>
2023-06-09 10:14:23 +0800

    bump ruff to 0.0.272 and update config (#54449)
    * bump ruff to 0.0.271 and update config
    
    * exclude third_party
    
    * bump ruff to 0.0.272
    
    * refine config

--- python/paddle/incubate/distributed/fleet/fleet_util.py
+++ python/paddle/incubate/distributed/fleet/fleet_util.py
@@ -1640,10 +1640,9 @@ class FleetUtil:
             total_ins_num_name,
         )
         self.rank0_print(
-            "%s global AUC=%.6f BUCKET_ERROR=%.6f MAE=%.6f "
-            "RMSE=%.6f Actural_CTR=%.6f Predicted_CTR=%.6f "
-            "COPC=%.6f MEAN Q_VALUE=%.6f Ins number=%s"
-            % (
+            "{} global AUC={:.6f} BUCKET_ERROR={:.6f} MAE={:.6f} "
+            "RMSE={:.6f} Actural_CTR={:.6f} Predicted_CTR={:.6f} "
+            "COPC={:.6f} MEAN Q_VALUE={:.6f} Ins number={}".format(
                 print_prefix,
                 auc,
                 bucket_error,
