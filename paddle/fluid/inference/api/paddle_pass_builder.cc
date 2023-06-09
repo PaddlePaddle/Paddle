1851003e080e27aaa3ed53db2404809c9d97b0e7
wz1qqx <2997223960@qq.com>
2023-06-09 17:31:11 +0800

    support reshape_mul fuse

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -527,6 +527,8 @@ XpuPassStrategy::XpuPassStrategy() : PassStrategy({}) {
       "stack_fuse_pass",
       "fused_multi_transformer_xpu_pass",
       "sigmoid_elementmul_fuse_pass",
+      "gpu_cpu_map_matmul_v2_to_matmul_pass",
+      "gpu_cpu_reshape2_matmul_fuse_pass",
       "fc_xpu_fuse_pass",
       "conv2d_xpu_fuse_pass",
       "add_activation_xpu_fuse_pass",
