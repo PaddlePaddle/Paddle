1897ea898822ed0d25496293e626b9e9f7bb1c06
LiYuRio <liyuruijx@163.com>
2023-06-12 14:47:11 +0800

    remove p2p on npu and xpu

--- python/paddle/distributed/fleet/meta_parallel/pp_utils/p2p_communication.py
+++ python/paddle/distributed/fleet/meta_parallel/pp_utils/p2p_communication.py
@@ -12,21 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os
-
 import numpy as np
 
 import paddle
@@ -46,29 +31,6 @@ _hcg = None
 _use_cache = False
 _enable_partial_send_recv = True
 _timers = None
-
-_xpu_comm_group_started = False
-
-_sync_send = os.environ.get("PADDLE_P2P_SYNC_SEND", "0")
-_sync_send = _sync_send.lower() in ['1', 'true']
-
-
-def _xpu_comm_group_start():
-    if not paddle.is_compiled_with_xpu():
-        return
-    global _xpu_comm_group_started
-    assert not _xpu_comm_group_started
-    framework.core.ProcessGroupBKCL.group_start()
-    _xpu_comm_group_started = True
-
-
-def _xpu_comm_group_end():
-    if not paddle.is_compiled_with_xpu():
-        return
-    global _xpu_comm_group_started
-    if _xpu_comm_group_started:
-        framework.core.ProcessGroupBKCL.group_end()
-        _xpu_comm_group_started = False
 
 
 def initialize_p2p_groups(
