40c9c7feeeea9b426aee173e0bfb20bd9d15cc2a
Zhang Jun <ewalker@live.cn>
2023-06-13 07:42:09 +0800

    Update range_op.cc

--- paddle/fluid/inference/tensorrt/convert/range_op.cc
+++ paddle/fluid/inference/tensorrt/convert/range_op.cc
@@ -47,7 +47,9 @@ class RangeOpConverter : public OpConverter {
     }
     auto number_tensor = Max(Sub(zero_tensor, quotient_tensor), zero_tensor);
     auto* start1 = engine_->GetITensor(op_desc.Input("Start")[0]);
-    start1 = Reshape(start1, nvinfer1::Dims{0, {0}});
+    nvinfer1::Dims start_dims;
+    start_dims.nbDims = 0;
+    start1 = Reshape(start1, start_dims);
 
     layer = TRT_ENGINE_ADD_LAYER(
         engine_, Fill, nvinfer1::Dims{}, nvinfer1::FillOperation::kLINSPACE);
