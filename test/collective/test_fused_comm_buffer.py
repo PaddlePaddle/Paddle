2b067d2143d0fcbfe43f1d11ee92b87f6a166ed1
liuyuang <liuyuang@baidu.com>
2023-06-12 09:15:36 +0800

    update ut

--- test/collective/test_fused_comm_buffer.py
+++ test/collective/test_fused_comm_buffer.py
@@ -25,15 +25,19 @@ class TestFusedCommBufferGradChecker(unittest.TestCase):
     def test_fused_comm_buffer_grad_checker(self):
         linear = paddle.nn.Linear(10, 10)
         w = linear.weight
+        b = linear.bias
         w.main_grad = None
+        b.main_grad = None
         buffer = FusedCommBuffer(
             id=0,
-            params=[w],
+            params=[w, b],
             comm_group=None,
             acc_steps=10,
             act=HOOK_ACTION.ALL_REDUCE,
         )
+        assert buffer.use_main_grad
         buffer.add_grad(w)
+        buffer.add_grad(b)
         w.main_grad = paddle.to_tensor([1], stop_gradient=True, dtype="float32")
         try:
             buffer.add_grad(w)
