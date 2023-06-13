8f10206120fb7ba7ebc0b85266f2cb5adbcdf475
wz1qqx <2997223960@qq.com>
2023-06-13 11:27:19 +0800

    fix ci

--- paddle/fluid/framework/ir/xpu/matmul_weight_trans_pass_test.cc
+++ paddle/fluid/framework/ir/xpu/matmul_weight_trans_pass_test.cc
@@ -28,8 +28,7 @@ TEST(MatMulWeightTransPass, basic) {
   auto* reshape2_in = layers.data("reshape2_in", {64, 256, 1, 1});
   auto* reshape2_out = layers.reshape2(reshape2_in, std::vector<int>{-1, 256});
   auto* matmul_y = layers.data("matmul_y", {8, 256}, true);
-  auto* matmul_out =
-      layers.matmul_v2(reshape2_out, matmul_y, nullptr, false, true);
+  layers.matmul_v2(reshape2_out, matmul_y, nullptr, false, true);
 
   std::unique_ptr<ir::Graph> graph(new ir::Graph(layers.main_program()));
   auto pass = PassRegistry::Instance().Get("matmul_weight_trans_pass");
