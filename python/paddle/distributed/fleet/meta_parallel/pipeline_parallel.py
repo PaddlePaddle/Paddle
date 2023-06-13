8ce1e07c60e5450927bbe80007e3bee92891595f
liuyuang <liuyuang@baidu.com>
2023-06-13 14:10:10 +0800

    update

--- python/paddle/distributed/fleet/meta_parallel/pipeline_parallel.py
+++ python/paddle/distributed/fleet/meta_parallel/pipeline_parallel.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 import time
+import warnings
 
 import paddle
 from paddle import framework
