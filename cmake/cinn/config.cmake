87f6013c45b6044a983971b5e2f044155a10913b
6clc <chaoliu.lc@foxmail.com>
2023-06-15 14:52:32 +0800

    feat(cmake): add cmake of cinn

---
+++ cmake/cinn/config.cmake
@@ -0,0 +1,11 @@
+# The home path of ISL
+# Required!
+set(ISL_HOME "")
+
+# Whether enable NVidia CUDA support.
+# Possible values: ON, OFF
+set(WITH_GPU ON)
+
+set(WITH_MKL ON)
+set(WITH_MKLDNN ON)
+set(USE_OPENMP "intel")
