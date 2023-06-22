f9ecefc88e702f1a02c4613ad821cb29ddb08f06
phlrain <phliuhongyu@126.com>
2023-06-22 09:58:45 +0000

    add fetch kernel

--- test/ir/new_ir/test_standalone_new_ir.py
+++ test/ir/new_ir/test_standalone_new_ir.py
@@ -22,33 +22,7 @@ import paddle
 paddle.enable_static()
 
 
-# class TestNewIr(unittest.TestCase):
-#     def test_with_new_ir(self):
-#         place = paddle.CPUPlace()
-#         exe = paddle.static.Executor(place)
-
-#         x = paddle.ones([2, 2], dtype="float32")
-#         y = paddle.ones([2, 2], dtype="float32")
-
-#         z = x + y
-#         out = exe.run(
-#             paddle.static.default_main_program(), {}, fetch_list=[z.name]
-#         )
-
-#         gold_res = np.ones([2, 2], dtype="float32") * 2
-
-#         self.assertEqual(
-#             np.array_equal(
-#                 np.array(
-#                     paddle.static.global_scope().find_var(z.name).get_tensor()
-#                 ),
-#                 gold_res,
-#             ),
-#             True,
-#         )
-
-
-class TestCombineOp(unittest.TestCase):
+class TestNewIr(unittest.TestCase):
     def test_with_new_ir(self):
         place = paddle.CPUPlace()
         exe = paddle.static.Executor(place)
@@ -56,7 +30,7 @@ class TestCombineOp(unittest.TestCase):
         x = paddle.ones([2, 2], dtype="float32")
         y = paddle.ones([2, 2], dtype="float32")
 
-        z = paddle.linalg.multi_dot([x, y])
+        z = x + y
         out = exe.run(
             paddle.static.default_main_program(), {}, fetch_list=[z.name]
         )
@@ -72,6 +46,32 @@ class TestCombineOp(unittest.TestCase):
             ),
             True,
         )
+
+
+# class TestCombineOp(unittest.TestCase):
+#     def test_with_new_ir(self):
+#         place = paddle.CPUPlace()
+#         exe = paddle.static.Executor(place)
+
+#         x = paddle.ones([2, 2], dtype="float32")
+#         y = paddle.ones([2, 2], dtype="float32")
+
+#         z = paddle.linalg.multi_dot([x, y])
+#         out = exe.run(
+#             paddle.static.default_main_program(), {}, fetch_list=[z.name]
+#         )
+
+#         gold_res = np.ones([2, 2], dtype="float32") * 2
+
+#         self.assertEqual(
+#             np.array_equal(
+#                 np.array(
+#                     paddle.static.global_scope().find_var(z.name).get_tensor()
+#                 ),
+#                 gold_res,
+#             ),
+#             True,
+#         )
 
 
 if __name__ == "__main__":
