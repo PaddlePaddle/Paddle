cdb4ff44b355bb0ff520e6490a42506c1806952a
Xing-lil <836567328@qq.com>
2023-06-26 20:02:38 +0800

    update

--- paddle/phi/core/distributed/gloo_comm_context.cc
+++ paddle/phi/core/distributed/gloo_comm_context.cc
@@ -40,6 +40,41 @@ GlooCommContext::GlooCommContext(
   gloo_context_->connectFullMesh(*store, device);
 }
 
+typedef void (*reduce_func)(void*, const void*, const void*, size_t);
+enum class ReduceOp : std::uint8_t { SUM = 0, AVG, MAX, MIN, PRODUCT };
+template <typename T>
+reduce_func get_function(const int& r) {
+  switch (r) {
+    case 0:
+      return reduce_func(&::gloo::sum<T>);
+    case 4:
+      return reduce_func(&::gloo::product<T>);
+    case 3:
+      return reduce_func(&::gloo::min<T>);
+    case 2:
+      return reduce_func(&::gloo::max<T>);
+    case 1:
+      VLOG(0) << "Error: Unsupported ReduceOp::AVG.";
+      exit(-1);
+  }
+
+  VLOG(0) << "Error: Unknown ReduceOp.";
+  exit(-1);
+}
+
+template <typename T>
+void _get_function_impl(gloo::AllreduceOptions::Func& fn,  // NOLINT
+                        const int op) {
+  fn = get_function<T>(op);
+}
+
+gloo::AllreduceOptions::Func _get_function(const phi::DataType type,
+                                           const int op) {
+  gloo::AllreduceOptions::Func fn;
+  GENERATE_FUNC(type, _get_function_impl, fn, op);
+  return fn;
+}
+
 void GlooCommContext::Broadcast(phi::DenseTensor* out_tensor,
                                 const phi::DenseTensor& in_tensor,
                                 int root,
@@ -100,6 +135,7 @@ void GlooCommContext::AllReduce(phi::DenseTensor* out_tensor,
   GENERATE_FUNC(dtype, SetInput, &opts, in_tensor);
   GENERATE_FUNC(dtype, SetOutput, &opts, out_tensor);
   GENERATE_FUNC(dtype, SetReduceFunc, &opts, reduce_type);
+  opts.setReduceFunction(_get_function(dtype, reduce_type));
   gloo::allreduce(opts);
 }
 
@@ -116,6 +152,32 @@ void GlooCommContext::Reduce(phi::DenseTensor* out_tensor,
   GENERATE_FUNC(dtype, SetOutput, &opts, out_tensor);
   GENERATE_FUNC(dtype, SetReduceFunc, &opts, reduce_type);
   gloo::reduce(opts);
+}
+
+void GlooCommContext::Send(const phi::DenseTensor& in_tensor,
+                           int dst,
+                           uint32_t tag) {
+  SendRecvOptions opts(gloo_context_);
+  const auto& dtype = in_tensor.dtype();
+  GENERATE_FUNC(dtype, SetInput, &opts, in_tensor);
+  opts.setSrc(gloo_context_.get()->rank);
+  opts.setDst(dst);
+  opts.setTag(tag);
+  send_recv(&opts);
+}
+void GlooCommContext::Recv(phi::DenseTensor* out_tensor,
+                           int src,
+                           const phi::DenseTensor& in_tensor,
+                           uint32_t tag) {
+  SendRecvOptions opts(gloo_context_);
+  const auto& dtype = in_tensor.dtype();
+  // const auto& dtype = out_tensor->dtype();
+  GENERATE_FUNC(dtype, SetOutput, &opts, out_tensor);
+  opts.setTag(tag);
+  // opts.setSrc(rank);
+  opts.setSrc(gloo_context_.get()->rank);
+  opts.setDst(src);
+  send_recv(&opts);
 }
 
 }  // namespace distributed
