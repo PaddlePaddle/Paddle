b5997094d74e6263af3fec6c3d1f97d42d2bc7ec
chenkailun <chenkailun@baidu.com>
2023-06-27 21:09:11 +0800

    trt unbind op

--- paddle/fluid/inference/tensorrt/convert/unbind_op.cc
+++ paddle/fluid/inference/tensorrt/convert/unbind_op.cc
@@ -26,7 +26,7 @@ class UnbindOpConverter : public OpConverter {
                   bool test_mode) override{
     VLOG(3) << "convert a unbind op to tensorrt layer";
     framework::OpDesc op_desc(op, nullptr);
-    std::string input_x_name = op_desc.Input("X");
+    std::string input_x_name = op_desc.Input("X").front();
     auto* input_x_tensor = engine_->GetITensor(input_x_name);
     auto in_dims = input_x_tensor->getDimensions();
     auto rank = in_dims.nbDims;
@@ -44,8 +44,8 @@ class UnbindOpConverter : public OpConverter {
     nvinfer1::Dims stride;
     std::vector<int32_t> newDims_vec;
     start.nbDims = rank;
-    size.nDims = rank;
-    stride.nDims = rank;
+    size.nbDims = rank;
+    stride.nbDims = rank;
     for(int i = 0; i < rank; ++i){
         start.d[i] = 0;
         stride.d[i] = 1;
@@ -53,18 +53,20 @@ class UnbindOpConverter : public OpConverter {
             size.d[i] = 1;
         }else{
             size.d[i] = in_dims.d[i];
-            newDims_vec[i].push_back(in_dims.d[i]);
+            newDims_vec.push_back(in_dims.d[i]);
         }
     }
     // reshape tensor
-    auto newDims = Add1DConstantLayer(start_vec, input_x_name + "_newDims_tensor_");
+    auto newDims = Add1DConstantLayer(newDims_vec, input_x_name + "_newDims_tensor_");
     for(auto& output_name : op_desc.Output("Out")){
         // 1 slice
         auto inputSliced = TRT_ENGINE_ADD_LAYER(
-        engine_, Slice, *input_x_tensor, start, size, stride);
+        engine_, Slice, *input_x_tensor, start, size, stride)->getOutput(0);
         // 2 reshape
-        auto inputSliced_output = Reshape(inputSliced, newDims, ("unbind: reshape: (Output(" + output_name + ")").c_str());
-        RreplenishLayerAndOutput(inputSliced_output, "unbind", {output_name}, test_mode);
+        // auto inputReshaped = Reshape(inputSliced, newDims, ("unbind: reshape: (Output(" + output_name + ")").c_str());
+        auto inputReshaped = TRT_ENGINE_ADD_LAYER(engine_, Shuffle, *inputSliced);
+        inputReshaped->setInput(1, *newDims);
+        RreplenishLayerAndOutput(inputReshaped, "unbind", {output_name}, test_mode);
         // 3 revise start dims
         ++start.d[axis];
     }
