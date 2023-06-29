bb6f39403cd06ded5a1db9e46e9db3f5a892abce
Wang Huan <wanghuan29@baidu.com>
2023-06-29 03:31:44 +0000

    reifne

--- paddle/phi/kernels/xpu/strided_copy_kernel.cc
+++ paddle/phi/kernels/xpu/strided_copy_kernel.cc
@@ -19,32 +19,6 @@ limitations under the License. */
 
 namespace phi {
 
-template <typename T>
-void StridedCopyFunc(const T* input_data,
-                     T* out_data,
-                     const int64_t* input_stride,
-                     const int64_t* output_stride,
-                     const int64_t* dims,
-                     const int rank,
-                     const int64_t numel) {
-  int64_t tid = core_id() * cluster_num() + cluster_id();
-  int64_t nthreads = core_id() * cluster_num();
-
-  for (int64_t i = tid; i < numel; i += nthreads) {
-    int64_t input_offset = 0;
-    int64_t output_offset = 0;
-    int64_t index_tmp = i;
-    for (int dim = rank - 1; dim >= 0; --dim) {
-      int64_t mod = index_tmp % dims[dim];
-      index_tmp = index_tmp / dims[dim];
-      input_offset += mod * input_stride[dim];
-      output_offset += mod * output_stride[dim];
-    }
-
-    out_data[output_offset] = input_data[input_offset];
-  }
-}
-
 template <typename T, typename Context>
 void StridedCopyKernel(const Context& dev_ctx,
                        const DenseTensor& input,
@@ -52,6 +26,7 @@ void StridedCopyKernel(const Context& dev_ctx,
                        const std::vector<int64_t>& out_stride,
                        int64_t offset,
                        DenseTensor* out) {
+  using XPUT = typename XPUTypeTrait<T>::Type;
   phi::DenseTensorMeta meta = input.meta();
   meta.strides = phi::make_ddim(out_stride);
   meta.dims = phi::make_ddim(dims);
@@ -72,48 +47,17 @@ void StridedCopyKernel(const Context& dev_ctx,
                         input.numel(),
                         out->numel()));
 
-  if (input.numel() <= 0) {
-    return;
-  }
+  auto input_data = reinterpret_cast<const XPUT*>(input.data<T>());
+  auto output_data = reinterpret_cast<XPUT*>(dev_ctx.template Alloc<T>(out));
 
-  const T* input_data = input.data<T>();
-  T* output_data = dev_ctx.template Alloc<T>(out);
-  int rank = input.dims().size();
-  const int64_t* input_dims = input.dims().Get();
-  const int64_t* input_stride = input.strides().Get();
-  const int64_t* output_stride = meta.strides.Get();
-  auto numel = input.numel();
-  int64_t block = 512;
-  int64_t grid = (numel + block - 1) / block;
-
-  int64_t* tmp_data =
-      reinterpret_cast<int64_t*>(malloc(sizeof(int64_t) * rank * 3));
-  std::memcpy(tmp_data, input_dims, sizeof(int64_t) * rank);
-  std::memcpy(tmp_data + rank, input_stride, sizeof(int64_t) * rank);
-  std::memcpy(tmp_data + rank + rank, output_stride, sizeof(int64_t) * rank);
-
-  auto dims_stride = paddle::memory::Alloc(
-      dev_ctx.GetPlace(),
-      sizeof(int64_t) * rank * 3,
-      phi::Stream(reinterpret_cast<phi::StreamId>(dev_ctx.stream())));
-  int64_t* dims_stride_data = reinterpret_cast<int64_t*>(dims_stride->ptr());
-
-  paddle::memory::Copy(dev_ctx.GetPlace(),
-                       dims_stride_data,
-                       phi::CPUPlace(),
-                       tmp_data,
-                       sizeof(int64_t) * rank * 3);
-
-  int r = StridedCopyFunc<<<dev_ctx.x_context()->ncluster(),
-                            64,
-                            dev_ctx.stream()>>>(input_data,
-                                                output_data,
-                                                dims_stride_data + rank,
-                                                dims_stride_data + rank + rank,
-                                                dims_stride_data,
-                                                rank,
-                                                numel);
-  PADDLE_ENFORCE_XDNN_SUCCESS(r, "StridedCopyFunc");
+  int r = xpu::strided_copy<XPUT>(dev_ctx.x_context(),
+                                  input_data,
+                                  output_data,
+                                  phi::vectorize<int64_t>(input.dims()),
+                                  phi::vectorize<int64_t>(out->dims()),
+                                  phi::vectorize<int64_t>(input.strides()),
+                                  phi::vectorize<int64_t>(out->strides()));
+  PADDLE_ENFORCE_XDNN_SUCCESS(r, "strided_copy");
 }
 
 }  // namespace phi
