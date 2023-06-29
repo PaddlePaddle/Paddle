a7419ff5a5e6929c1a937016469052dfe8502c88
zhangbo9674 <82555433+zhangbo9674@users.noreply.github.com>
2023-06-29 16:12:45 +0800

    [IR] Refine PhiKernelOp attributes name and delete some unused code2 (#54944)
    * refine code
    
    * add some interface for phi kernel op
    
    * fix compile bug
    
    * delete unused code
    
    * support code
    
    * fix bug
    
    * refine code
    
    * delete unused code
    
    * fix compile bug
    
    * fix compile bug
    
    * delete unused code
    
    * add elementwise add op
    
    * fix compile bug
    
    * refine code
    
    * fix compile bug
    
    * add ut for attribute member function
    
    * delete unused code
    
    * refine ut

--- test/cpp/ir/kernel_dialect/ir_kernel_dialect_pass_test.cc
+++ test/cpp/ir/kernel_dialect/ir_kernel_dialect_pass_test.cc
@@ -20,6 +20,7 @@
 #include "paddle/fluid/framework/variable.h"
 #include "paddle/fluid/framework/variable_helper.h"
 #include "paddle/fluid/ir/dialect/kernel_dialect.h"
+#include "paddle/fluid/ir/dialect/kernel_op.h"
 #include "paddle/fluid/ir/dialect/pd_attribute.h"
 #include "paddle/fluid/ir/dialect/pd_dialect.h"
 #include "paddle/fluid/ir/dialect/pd_type.h"
@@ -34,6 +35,7 @@
 #include "paddle/ir/core/ir_context.h"
 #include "paddle/ir/core/program.h"
 #include "paddle/ir/core/utils.h"
+#include "paddle/phi/common/data_type.h"
 #include "paddle/phi/common/place.h"
 #include "paddle/phi/core/kernel_context.h"
 #include "paddle/phi/core/kernel_factory.h"
@@ -84,6 +86,24 @@ TEST(program_test, program) {
   EXPECT_EQ(res1, true);
   EXPECT_EQ(res2, true);
   EXPECT_EQ(res3, true);
+
+  EXPECT_EQ(kernel_program->block()->size(), 3u);
+  EXPECT_EQ(kernel_program->block()
+                ->front()
+                ->dyn_cast<paddle::dialect::PhiKernelOp>()
+                .op_name(),
+            "pd.full");
+  EXPECT_EQ(kernel_program->block()
+                ->front()
+                ->dyn_cast<paddle::dialect::PhiKernelOp>()
+                .kernel_name(),
+            "full");
+  EXPECT_EQ(kernel_program->block()
+                ->front()
+                ->dyn_cast<paddle::dialect::PhiKernelOp>()
+                .kernel_key()
+                .dtype(),
+            phi::DataType::FLOAT32);
 }
 
 TEST(dialect_attr, attr) {
