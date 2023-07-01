b918100a02a48bb47b606f652c959f200989727d
a_weng <54014960+Sahala08@users.noreply.github.com>
2023-07-01 20:44:52 +0800

    [CodeStyle][CINN] fix cinn cpplint codestyle about `[readability/todo]`, `[readability/check]` (#55022)

--- paddle/cinn/hlir/op/contrib/argmax.cc
+++ paddle/cinn/hlir/op/contrib/argmax.cc
@@ -157,9 +157,9 @@ std::shared_ptr<framework::OpStrategy> StrategyForArgmax(
     ir::IRSchedule ir_sch(mod_expr);
     ir_sch.MergeExprs();
     auto blocks = ir_sch.GetAllBlocks();
-    // TODO: It needs to be rewritten according to the reduction_max operator to
-    // improve performance. Do not use local variables, because the size will
-    // exceed the limit.
+    // TODO(zhhsplendid): It needs to be rewritten according to the
+    // reduction_max operator to improve performance. Do not use local
+    // variables, because the size will exceed the limit.
     ir_sch.SetBuffer(blocks[0], "local");
     ir_sch.SetBuffer(blocks[1], "local");
 
@@ -184,7 +184,7 @@ std::shared_ptr<framework::OpStrategy> StrategyForArgmax(
 std::vector<shape_t> InferShapeForArgmax(
     const std::vector<shape_t> &inputs_shape,
     const framework::AttrMapType &attrs) {
-  CHECK(inputs_shape.size() == 1UL);
+  CHECK_EQ(inputs_shape.size(), 1UL);
   auto ndim = inputs_shape[0].size();
   CHECK_GT(ndim, 0) << "tensor's dim must be more than 0";
   int axis;
