b991bae0b151c14c6012cc7b5078985d376dad21
hitywt <yuwentao126@126.com>
2023-07-02 15:48:23 +0800

    fix ut

--- python/paddle/distributed/passes/auto_parallel_data_parallel_optimization.py
+++ python/paddle/distributed/passes/auto_parallel_data_parallel_optimization.py
@@ -290,7 +290,6 @@ class DataParallelOptimizationPass(PassBase):
         # comm wait calc to finish
         for idx, op in reversed(list(enumerate(block.ops))):
             if is_data_parallel_reduce_op(op):
-                assert op.has_attr('use_calc_stream')
                 assert op.has_attr('ring_id')
 
                 op._set_attr('use_calc_stream', False)
@@ -444,7 +443,7 @@ class DataParallelOptimizationPass(PassBase):
     def _update_program(self, grad_groups):
         block = default_main_program().global_block()
 
-        remove_op_types = ['scale', 'c_allreduce_sum', 'c_wait_compute']
+        remove_op_types = ['scale', 'all_reduce', 'c_wait_compute']
 
         for i, group in enumerate(grad_groups[::-1]):
             # skip unfused big tensor
@@ -497,8 +496,8 @@ class DataParallelOptimizationPass(PassBase):
 
             allreduce_op = block.ops[group.allreduce_op_idx]
             assert (
-                allreduce_op.type == 'c_allreduce_sum'
-            ), "should found c_allreduce_sum op but found {}".format(
+                allreduce_op.type == 'all_reduce'
+            ), "should found all_reduce op but found {}".format(
                 str(allreduce_op)
             )
             allreduce_op_dist_attr = (
