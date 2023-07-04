ab53ab85e6cefc92fe6030f2e6f8d5aaf3881bc4
kangguangli <kangguangli@hotmail.com>
2023-07-04 09:42:19 +0000

    open only in py3 and mac
    (cherry picked from commit 6daa44da495afb0287c6b69ecefbe35bbc47cb50)

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -626,6 +626,8 @@ EOF
 
 
 function run_mac_test() {
+    export FLAGS_NEW_IR_OPTEST=True
+    export FLAGS_CI_PIPELINE=mac
     mkdir -p ${PADDLE_ROOT}/build
     cd ${PADDLE_ROOT}/build
     if [ ${WITH_TESTING:-ON} == "ON" ] ; then
@@ -771,6 +773,8 @@ EOF
 }
 
 function run_linux_cpu_test() {
+    export FLAGS_NEW_IR_OPTEST=True
+    export FLAGS_CI_PIPELINE=py3
     mkdir -p ${PADDLE_ROOT}/build
     cd ${PADDLE_ROOT}/build
     pip install hypothesis
