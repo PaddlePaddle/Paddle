4771e4e61dc67313f4d91007146b2b3a674f8aa2
JZ-LIANG <jianzhongliang10@gmail.com>
2023-07-04 16:01:02 +0800

    [Semi-Auto] Replicated Parallel Rule (#54810)
    * base rule
    
    * add sharidng merge
    
    * add sharidng axis merge
    
    * define unified data class for inferencing dist_attr
    
    ---------
    
    Co-authored-by: Yichen Zhang <zhangyichen03@baidu.com>

--- paddle/fluid/distributed/auto_parallel/spmd_rules/rules.h
+++ paddle/fluid/distributed/auto_parallel/spmd_rules/rules.h
@@ -16,6 +16,7 @@
 
 #include "paddle/fluid/distributed/auto_parallel/spmd_rules/common.h"
 #include "paddle/fluid/distributed/auto_parallel/spmd_rules/matmul_spmd_rule.h"
+#include "paddle/fluid/distributed/auto_parallel/spmd_rules/replicated_spmd_rule.h"
 
 // TODO(ljz) Automatic this process in cmake file.
 namespace paddle {
@@ -24,6 +25,9 @@ namespace auto_parallel {
 
 // matmul rule
 REGISTER_SPMD_RULE(matmul, MatmulSPMDRule);
+
+// replicated rule
+REGISTER_SPMD_RULE(replicated, ReplicatedSPMDRule);
 
 }  // namespace auto_parallel
 }  // namespace distributed
