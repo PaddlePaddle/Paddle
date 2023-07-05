993c163ea6b6a79ea4bd26b1d3793d38f277edb6
gouzil <66515297+gouzil@users.noreply.github.com>
2023-07-05 10:00:59 +0800

    [CodeStyle][CINN] fix Ruff lint errors (pylint rules) (#54990)
    * [codestyle] fix ruff PLR0402
    
    * [codestyle] fix ruff PLC0414
    
    * [codestyle] fix ruff PLE1205

--- test/cinn/fake_model/naive_mul.py
+++ test/cinn/fake_model/naive_mul.py
@@ -19,8 +19,7 @@ import numpy
 import numpy as np
 
 import paddle
-import paddle.fluid as fluid
-import paddle.static as static
+from paddle import fluid, static
 
 size = 30
 paddle.enable_static()
