ff80db313ecf72cb7acdd5209912628a876a3fbc
ckl117 <ckl117@163.com>
2023-07-07 08:43:30 +0000

    code check

--- paddle/fluid/inference/tensorrt/convert/unbind_op.cc
+++ paddle/fluid/inference/tensorrt/convert/unbind_op.cc
@@ -29,6 +29,7 @@ class UnbindOpConverter : public OpConverter {
     std::string input_x_name = op_desc.Input("X").front();
     auto* input_x_tensor = engine_->GetITensor(input_x_name);
     auto in_dims = input_x_tensor->getDimensions();
+    auto in_shape_tensor = Shape(input_x_tensor);
     auto rank = in_dims.nbDims;
     int axis = 0;
     std::cout << std::endl;
@@ -39,42 +40,46 @@ class UnbindOpConverter : public OpConverter {
       }
     }
     
-    nvinfer1::Dims start;
-    nvinfer1::Dims size;
+
+    std::vector<nvinfer1::ITensor*> in_shape_tensors;
+    std::vector<nvinfer1::ITensor*> newDims_tensors; 
+    for (int32_t i = 0; i < rank; ++i) {
+      in_shape_tensors.push_back(GetEleTensorOfShape(in_shape_tensor, i));
+      if (i != axis)
+      {
+        newDims_tensors.push_back(GetEleTensorOfShape(in_shape_tensor, i));
+      }
+    }
+    auto newDims_tensor = Concat(newDims_tensors);
+
+    std::vector<nvinfer1::ITensor*> start_tensors;
+    std::vector<nvinfer1::ITensor*> size_tensors = in_shape_tensors;
+
     nvinfer1::Dims stride;
-    std::vector<int32_t> newDims_vec;
-    std::vector<int32_t> start_vec(rank, 0);
-    std::vector<int32_t> size_vec(rank, 0);
-    std::vector<int32_t> stride_vec(rank, 1);
-    start.nbDims = rank;
-    size.nbDims = rank;
     stride.nbDims = rank;
     for(int i = 0; i < rank; ++i){
         if(axis == i){
-            size_vec[i] = 1;
-        }else{
-            size_vec[i] = in_dims.d[i];
-            newDims_vec.push_back(in_dims.d[i]);
+            size_tensors[i] = Add1DConstantLayer(1);
         }
+        start_tensors.push_back(Add1DConstantLayer(0));
+        stride.d[i] = 1;
     }
-    auto sizes_tensor = Add1DConstantLayer(size_vec, input_x_name + "_size_tensor_");
-    auto strides_tensor = Add1DConstantLayer(stride_vec, input_x_name + "_stride_tensor_");
-    auto newDims_tensor = Add1DConstantLayer(newDims_vec, input_x_name + "_newDims_tensor_");
+    int ii = 0;
     for(auto& output_name : op_desc.Output("Out")){
+
+        start_tensors[axis] = Add1DConstantLayer(ii++);
         // 1 slice
-        auto starts_tensor =Add1DConstantLayer(start_vec, output_name + "_start_tensor_");
         auto inputSliced = TRT_ENGINE_ADD_LAYER(
-        engine_, Slice, *input_x_tensor, start, size, stride);
-        inputSliced->setInput(1, *starts_tensor);
-        inputSliced->setInput(2, *sizes_tensor);
-        inputSliced->setInput(3, *strides_tensor);
+        engine_, Slice, *input_x_tensor, stride, stride, stride);
+        inputSliced->setInput(1, *Concat(start_tensors));
+        inputSliced->setInput(2, *Concat(size_tensors));
+
+
         auto inputSliced_out = inputSliced->getOutput(0);
         // 2 reshape
         auto inputReshaped = TRT_ENGINE_ADD_LAYER(engine_, Shuffle, *inputSliced_out);
         inputReshaped->setInput(1, *newDims_tensor);
         RreplenishLayerAndOutput(inputReshaped, "unbind", {output_name}, test_mode);
-        // 3 revise start dims
-        start_vec[axis]++;
     }
 
   }
