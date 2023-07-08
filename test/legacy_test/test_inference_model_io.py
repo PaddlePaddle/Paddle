a1f458ffe751b9e2a19e32f6dafb724fbba5f2b5
Difers <707065510@qq.com>
2023-07-08 13:44:14 +0000

    fix test_inference_model_io bug

--- test/legacy_test/test_inference_model_io.py
+++ test/legacy_test/test_inference_model_io.py
@@ -104,7 +104,7 @@ class TestBook(unittest.TestCase):
         importlib.reload(executor)  # reload to build a new scope
 
         model_0 = InferModel(load_inference_model(MODEL_DIR, exe))
-        with open(os.path.join(UNI_MODEL_DIR, '.pdmodel'), "rb") as f:
+        with open((UNI_MODEL_DIR + '.pdmodel'), "rb") as f:
             model_str = f.read()
         model_1 = InferModel(load_inference_model(UNI_MODEL_DIR, exe))
 
