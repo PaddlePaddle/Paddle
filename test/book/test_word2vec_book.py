770ebcd687839bddd055833eb3f1a5510b6ca553
Difers <707065510@qq.com>
2023-07-09 11:45:51 +0000

    fix io error

--- test/book/test_word2vec_book.py
+++ test/book/test_word2vec_book.py
@@ -274,6 +274,7 @@ def infer(target, save_dirname=None):
         infer_inputs = [to_infer_tensor(t) for t in infer_inputs]
 
         infer_config = fluid.core.NativeConfig()
+        infer_config.model_dir = save_dirname + ".pdmodel"
         if target == "cuda":
             infer_config.use_gpu = True
             infer_config.device = 0
