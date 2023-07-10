3c4018d5fdbafc82a7e17e127afa27b43dd815cc
Vvsmile <17864154871@163.com>
2023-07-10 13:15:12 +0800

    fix elementwise_div and pool_max

--- test/legacy_test/test_elementwise_div_op.py
+++ test/legacy_test/test_elementwise_div_op.py
@@ -124,7 +124,7 @@ class ElementwiseDivOp(OpTest):
                 'user_defined_grad_outputs': [self.grad_out],
                 'check_dygraph': self.check_dygraph,
                 'check_prim': self.check_prim,
-                'max_relative_error': 0.02
+                'max_relative_error': 0.02,
             }
             if self.place is None:
                 self.check_grad(*check_args, **check_kwargs)
@@ -390,6 +390,7 @@ class TestElementwiseDivOpXsizeLessThanYsize(ElementwiseDivOp):
         self.x_shape = [10, 12]
         self.y_shape = [2, 3, 10, 12]
         self.attrs = {'axis': 2}
+
     def compute_gradient_x(self, grad_out, y):
         return np.sum(grad_out / y, axis=(0, 1))
 
