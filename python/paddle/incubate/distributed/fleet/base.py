c3ef8912f5768e2ba374e4c606377b3ecce44269
longranger2 <836253168@qq.com>
2023-07-11 15:29:13 +0000

    fix bug

--- python/paddle/incubate/distributed/fleet/base.py
+++ python/paddle/incubate/distributed/fleet/base.py
@@ -290,8 +290,10 @@ class DistributedOptimizer(metaclass=abc.ABCMeta):
     """
 
     def __init__(self, optimizer, strategy=None):
-        if not isinstance(optimizer, SGD.__bases__) and not isinstance(
-            optimizer, OptimizerWithMixedPrecision
+        if (
+            not isinstance(optimizer, SGD.__bases__)
+            and not isinstance(optimizer, fluid.optimizer.Optimizer)
+            and not isinstance(optimizer, OptimizerWithMixedPrecision)
         ):
             raise TypeError("optimizer must be an instance of Optimizer")
 
