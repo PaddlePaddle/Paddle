2ce0d37cb17b569771b172c61809829659610f35
phlrain <phliuhongyu@126.com>
2023-07-11 13:08:30 +0000

    remove useless code

--- test/legacy_test/eager_op_test.py
+++ test/legacy_test/eager_op_test.py
@@ -1140,7 +1140,6 @@ class OpTest(unittest.TestCase):
         set_flags({"FLAGS_enable_new_ir_in_executor": True})
 
         executor = Executor(place)
-
         ir_outs = executor.run(
             program,
             feed=feed_map,
@@ -1244,7 +1243,6 @@ class OpTest(unittest.TestCase):
                 program = compiled_prog
 
             executor = Executor(place)
-
             outs = executor.run(
                 program,
                 feed=feed_map,
@@ -2956,7 +2954,6 @@ class OpTest(unittest.TestCase):
                 compiled_prog = fluid.CompiledProgram(prog, build_strategy)
                 prog = compiled_prog
             executor = fluid.Executor(place)
-
             res = list(
                 map(
                     np.array,
