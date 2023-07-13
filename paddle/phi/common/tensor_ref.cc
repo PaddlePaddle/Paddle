d7e4eeedb1f50cc28bd5a2f28b823b43c528dfcf
phlrain <phliuhongyu@126.com>
2023-07-13 04:23:44 +0000

    remove program once

--- paddle/phi/common/tensor_ref.cc
+++ paddle/phi/common/tensor_ref.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#pragma once
-
 #include "paddle/phi/common/tensor_ref.h"
 
 #include <cstdint>
