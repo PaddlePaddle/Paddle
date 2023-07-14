d1b74ba565f0f9d4b7d01e6e09937cf6a8de6739
HongyuJia <jiahongyu@baidu.com>
2023-07-14 10:50:08 +0800

    [0D-Tensor] CINN supports transpose, add special case to expand_zero_dim_pass (#55379)

--- test/cinn/ops/test_zero_dim_tensor.py
+++ test/cinn/ops/test_zero_dim_tensor.py
@@ -633,6 +633,44 @@ class TestScaleOp(OpTest):
 @OpTestTool.skip_if(
     not is_compiled_with_cuda(), "x86 test will be skipped due to timeout."
 )
+class TestTransposeOp(OpTest):
+    def setUp(self):
+        np.random.seed(2023)
+        self.dtype = "float32"
+        self.init_input()
+
+    def init_input(self):
+        self.inputs = {
+            "x": np.random.randint(-10, 10, []).astype(self.dtype),
+        }
+        self.target_shape = ()
+
+    def build_paddle_program(self, target):
+        x = paddle.to_tensor(self.inputs["x"], stop_gradient=False)
+        out = paddle.transpose(x, [])
+
+        self.paddle_outputs = [out]
+
+    def build_cinn_program(self, target):
+        builder = NetBuilder("transpose_op")
+        x = builder.create_input(
+            cinn_dtype_convert(self.dtype), self.inputs["x"].shape, "x"
+        )
+        out = builder.transpose(x, [])
+
+        prog = builder.build()
+        res = self.get_cinn_output(prog, target, [x], [self.inputs["x"]], [out])
+
+        self.cinn_outputs = res
+        self.assertEqual(res[0].shape, self.target_shape)
+
+    def test_check_results(self):
+        self.check_outputs_and_grads()
+
+
+@OpTestTool.skip_if(
+    not is_compiled_with_cuda(), "x86 test will be skipped due to timeout."
+)
 class TestBroadcastToOp1D(OpTest):
     def setUp(self):
         np.random.seed(2023)
