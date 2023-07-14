a0f4ffd92d84d2048e08372811f7f9bd0cee34f0
kangguangli <kangguangli@hotmail.com>
2023-07-14 06:18:53 +0000

    fix

--- paddle/fluid/ir/dialect/pd_op_verify.cc
+++ paddle/fluid/ir/dialect/pd_op_verify.cc
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "pd_op.h"  //NOLINT
+#include "paddle/fluid/ir/dialect/pd_op.h"
 
 void PrintOp::Verify() {}
