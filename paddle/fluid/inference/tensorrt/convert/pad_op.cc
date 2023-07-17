a0f0b123356a4488160b0a34f070aefe83bf283f
ckl117 <ckl117@163.com>
2023-07-17 08:04:48 +0000

    add trt pad support nhwc

--- paddle/fluid/inference/tensorrt/convert/pad_op.cc
+++ paddle/fluid/inference/tensorrt/convert/pad_op.cc
@@ -32,25 +32,97 @@ class PadOpConverter : public OpConverter {
     // Declare inputs
     auto* input = engine_->GetITensor(op_desc.Input("X")[0]);
 
-    const std::vector<int> paddings =
+    // const std::vector<int> paddings =
+    //     PADDLE_GET_CONST(std::vector<int>, op_desc.GetAttr("paddings"));
+
+    nvinfer1::ITensor* transpose_layer1_out = nullptr;
+    const std::vector<int> base_paddings =
         PADDLE_GET_CONST(std::vector<int>, op_desc.GetAttr("paddings"));
+    std::vector<int> paddings({base_paddings.begin(), base_paddings.end()});
+    // auto in_dims = input->getDimensions();
+    bool flag_nchw = true;
+    if (base_paddings.size() == 8) {
+      for (int i = 0; i < 4; ++i) {
+        if (paddings[i] != 0) {
+          flag_nchw = false;
+          break;
+        }
+      }
+      // transpose to nchw
+      if (!flag_nchw) {
+        paddings[6] = paddings[4];
+        paddings[7] = paddings[5];
+        paddings[4] = paddings[2];
+        paddings[5] = paddings[3];
+        nvinfer1::Permutation perm;
+        if (engine_->with_dynamic_shape()) {
+          perm.order[0] = 0;
+          perm.order[1] = 3;
+          perm.order[2] = 1;
+          perm.order[3] = 2;
+        } else {
+          perm.order[0] = 2;
+          perm.order[1] = 0;
+          perm.order[2] = 1;
+        }
+
+        auto* transpose_layer1 = TRT_ENGINE_ADD_LAYER(engine_, Shuffle, *input);
+        transpose_layer1->setFirstTranspose(perm);
+        transpose_layer1_out = transpose_layer1->getOutput(0);
+      }
+    }
 
     int pad_size = static_cast<int>(paddings.size());
 
     nvinfer1::DimsHW pre_pad(paddings[pad_size - 4], paddings[pad_size - 2]);
     nvinfer1::DimsHW post_pad(paddings[pad_size - 3], paddings[pad_size - 1]);
 
-    auto* layer = TRT_ENGINE_ADD_LAYER(engine_,
-                                       Padding,
-                                       *const_cast<nvinfer1::ITensor*>(input),
-                                       pre_pad,
-                                       post_pad);
+    auto* layer = TRT_ENGINE_ADD_LAYER(
+        engine_,
+        Padding,
+        transpose_layer1_out == nullptr ? *const_cast<nvinfer1::ITensor*>(input)
+                                        : *transpose_layer1_out,
+        pre_pad,
+        post_pad);
 
-    PADDLE_ENFORCE_NOT_NULL(layer,
-                            platform::errors::External(
-                                "add padding layer to tensorrt engine error"));
-    auto output_name = op_desc.Output("Out")[0];
-    RreplenishLayerAndOutput(layer, "pad", {output_name}, test_mode);
+    if (!flag_nchw) {
+      nvinfer1::Permutation perm;
+
+      if (engine_->with_dynamic_shape()) {
+        perm.order[0] = 0;
+        perm.order[1] = 2;
+        perm.order[2] = 3;
+        perm.order[3] = 1;
+      } else {
+        perm.order[0] = 1;
+        perm.order[1] = 2;
+        perm.order[2] = 0;
+      }
+      auto* transpose_layer2 =
+          TRT_ENGINE_ADD_LAYER(engine_, Shuffle, *(layer->getOutput(0)));
+      transpose_layer2->setFirstTranspose(perm);
+      PADDLE_ENFORCE_NOT_NULL(
+          transpose_layer2,
+          platform::errors::External(
+              "add padding layer to tensorrt engine error"));
+      auto output_name = op_desc.Output("Out")[0];
+      RreplenishLayerAndOutput(
+          transpose_layer2, "pad", {output_name}, test_mode);
+    } else {
+      PADDLE_ENFORCE_NOT_NULL(
+          layer,
+          platform::errors::External(
+              "add padding layer to tensorrt engine error"));
+      auto output_name = op_desc.Output("Out")[0];
+      RreplenishLayerAndOutput(layer, "pad", {output_name}, test_mode);
+    }
+
+    // PADDLE_ENFORCE_NOT_NULL(layer,
+    //                         platform::errors::External(
+    //                             "add padding layer to tensorrt engine
+    //                             error"));
+    // auto output_name = op_desc.Output("Out")[0];
+    // RreplenishLayerAndOutput(layer, "pad", {output_name}, test_mode);
   }
 };
 
