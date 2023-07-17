541a45c254ab13b89f953a1537823f922f2c46e4
zhoutianzi666 <17801055074@163.com>
2023-07-17 15:34:51 +0800

    remove std::cout

--- paddle/fluid/inference/analysis/passes/memory_optimize_pass.cc
+++ paddle/fluid/inference/analysis/passes/memory_optimize_pass.cc
@@ -92,7 +92,7 @@ void MemoryOptimizePass::CollectLifeCycle(
 
           auto in_shape = node->Var()->GetShape();
           for (auto i : in_shape) {
-            std::cout <<node->Name() << std::endl;
+            //std::cout <<node->Name() << std::endl;
             CHECK_GE(i, 0);
           }
           auto var_bytes = std::accumulate(in_shape.begin(),
