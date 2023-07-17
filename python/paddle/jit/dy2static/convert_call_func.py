e0888a8992462dd5c74c36c04ebaf560947d24e7
SigureMo <sigure.qaq@gmail.com>
2023-07-17 17:29:56 +0800

    clean code

--- python/paddle/jit/dy2static/convert_call_func.py
+++ python/paddle/jit/dy2static/convert_call_func.py
@@ -121,10 +121,7 @@ def is_unsupported(func):
         for v in m.__dict__.values():
             if not callable(v):
                 continue
-            func_in_dict = func == v
-            if isinstance(func_in_dict, (list, numpy.ndarray)):
-                func_in_dict = numpy.array(func_in_dict).any()
-            if func_in_dict:
+            if func is v:
                 translator_logger.log(
                     2,
                     "Whitelist: {} is part of built-in module and does not have to be transformed.".format(
