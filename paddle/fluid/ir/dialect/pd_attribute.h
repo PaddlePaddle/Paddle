3c71ee00d65b86a20e4e2475cf36024595d3faec
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-07-19 07:29:10 +0800

    Update pd_attribute.h

--- paddle/fluid/ir/dialect/pd_attribute.h
+++ paddle/fluid/ir/dialect/pd_attribute.h
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #pragma once
 
 #include "paddle/fluid/ir/dialect/pd_attribute_storage.h"
