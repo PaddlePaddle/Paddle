5a81262c3a591ae784a2dac1fe6405b7e838cc55
minghaipeng <minghaipeng@baidu.com>
2023-07-20 09:22:35 +0000

    paddle-TRT support float64

--- paddle/fluid/inference/tensorrt/convert/fill_any_like_op.cc
+++ paddle/fluid/inference/tensorrt/convert/fill_any_like_op.cc
@@ -33,7 +33,7 @@ class FillAnyLikeOpConverter : public OpConverter {
 
     const int dtype = PADDLE_GET_CONST(int, op_desc.GetAttr("dtype"));
     float value = PADDLE_GET_CONST(float, op_desc.GetAttr("value"));
-    if ((dtype == 2) ||
+    if ((dtype == 2 || dtype == 3) ||
         (dtype == -1 && input->getType() == nvinfer1::DataType::kINT32)) {
       value_tensor = Add1DConstantLayer(static_cast<int32_t>(value),
                                         output_name + "_value_tensor_");
