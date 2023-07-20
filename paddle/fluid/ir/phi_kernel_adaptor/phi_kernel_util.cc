5453c0bd183d99d17017d56e619d9ea2ecc61686
zhangbo9674 <82555433+zhangbo9674@users.noreply.github.com>
2023-07-20 09:42:52 +0800

    [IR] Add variable name prefix for BuildScope (#55536)
    * add interface
    
    * add code
    
    * add code
    
    * add code
    
    * add code
    
    * fix bug
    
    * fix bug
    
    * add var prefix

--- paddle/fluid/ir/phi_kernel_adaptor/phi_kernel_util.cc
+++ paddle/fluid/ir/phi_kernel_adaptor/phi_kernel_util.cc
@@ -66,8 +66,8 @@ paddle::framework::Variable* CreateVar(
   }
 
   paddle::framework::Variable* var = nullptr;
-  VLOG(6) << "var_name_prefix is: " << var_name_prefix;
-  std::string name = "inner_var_" + std::to_string(variable_2_var_name->size());
+  std::string name = var_name_prefix + "_inner_var_" +
+                     std::to_string(variable_2_var_name->size());
   if (force_persisable || is_persisable) {
     VLOG(6) << "Create var: " << name << " in scope " << inner_scope->root();
     var = const_cast<paddle::framework::Scope*>(inner_scope->root())->Var(name);
