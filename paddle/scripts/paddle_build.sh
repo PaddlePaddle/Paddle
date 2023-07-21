b01f13b9b1e93371f1d059d4596a9c23a585ad1b
ccsuzzh <1719571694@qq.com>
2023-07-21 21:44:06 +0800

    add clang-tidy check in the end of PR-CI-Build

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3238,6 +3238,53 @@ function trt_convert_test() {
     fi
 }
 
+function clang-tidy_check() {
+    set +x
+
+    diff_files=$(git diff --name-only --diff-filter=ACMR ${BRANCH})
+    num_diff_files=$(echo "$diff_files" | wc -l)
+    echo -e "diff files between pr and ${BRANCH}:\n${diff_files}"
+
+    echo "Checking code style by pre-commit ..."
+    startTime_s=`date +%s`
+    pre-commit run clang-tidy --files ${diff_files};check_error=$?
+    endTime_s=`date +%s`
+    [ -n "$startTime_firstBuild" ] && startTime_s=$startTime_firstBuild
+    echo "Files Num: $[ $num_diff_files ]"
+    echo "Check Time: $[ $endTime_s - $startTime_s ]s"
+
+    if test ! -z "$(git diff)"; then
+        echo -e '\n************************************************************************************'
+        echo -e "These files have been formatted by code format hook. You should use pre-commit to \
+    format them before git push."
+        echo -e '************************************************************************************\n'
+        git diff 2>&1
+    fi
+
+    echo -e '\n************************************************************************************'
+    if [ ${check_error} != 0 ];then
+        echo "Your PR code style check failed."
+        echo "Please install pre-commit locally and set up git hook scripts:"
+        echo ""
+        echo "    pip install pre-commit==2.17.0"
+        echo "    pre-commit install"
+        echo ""
+        if [[ $num_diff_files -le 100 ]];then
+            echo "Then, run pre-commit to check codestyle issues in your PR:"
+            echo ""
+            echo "    pre-commit run --files" $(echo ${diff_files} | tr "\n" " ")
+            echo ""
+        fi
+        echo "For more information, please refer to our codestyle check guide:"
+        echo "https://www.paddlepaddle.org.cn/documentation/docs/zh/develop/dev_guides/git_guides/codestyle_check_guide_cn.html"
+    else
+        echo "Your PR code style check passed."
+    fi
+    echo -e '************************************************************************************\n'
+
+    exit ${check_error} 
+}
+
 function build_pr_and_develop() {
     run_setup ${PYTHON_ABI:-""} bdist_wheel ${parallel_number}
     if [ ! -d "${PADDLE_ROOT}/build/python/dist/" ]; then
@@ -3273,7 +3320,8 @@ function build_pr_and_develop() {
         mkdir ${PADDLE_ROOT}/build/dev_whl && cp ${PADDLE_ROOT}/build/python/dist/*.whl ${PADDLE_ROOT}/build/dev_whl
     fi
     generate_api_spec "$1" "DEV"
-
+    
+    clang-tidy_check
 }
 
 function build_develop() {
