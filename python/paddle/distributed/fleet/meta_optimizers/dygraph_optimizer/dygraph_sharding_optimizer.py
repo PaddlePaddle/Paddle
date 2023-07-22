6f83ff50700f357ee846d79a60280f4bf7e767bd
sneaxiy <32832641+sneaxiy@users.noreply.github.com>
2023-07-22 10:20:15 +0800

    part-3 cherry from: make sharding reduce mode by default (#55529)
    * make sharding reduce mode by default
    
    * Update dygraph_sharding_optimizer.py
    
    * Update hybrid_parallel_optimizer.py
    
    * Update pipeline_parallel.py

--- python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/dygraph_sharding_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/dygraph_sharding_optimizer.py
@@ -39,10 +39,8 @@ if g_shard_norm_align_dp:
         not g_shard_use_reduce
     ), "g_shard_norm_align_dp is not supported if g_shard_use_reduce is true"
 
-g_shard_use_reduce = int(os.environ.get("FLAGS_shard_use_reduce", 0))
-logger.info(f"g_shard_use_reduce {g_shard_use_reduce}")
-g_shard_norm_align_dp = int(os.environ.get("FLAGS_shard_norm_align_dp", 1))
-logger.info(f"g_shard_norm_align_dp {g_shard_norm_align_dp}")
+g_shard_use_reduce = int(os.environ.get("FLAGS_shard_use_reduce", 1))
+g_shard_norm_align_dp = int(os.environ.get("FLAGS_shard_norm_align_dp", 0))
 
 if g_shard_norm_align_dp:
     assert (
