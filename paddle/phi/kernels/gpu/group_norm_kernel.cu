f3cb8a66ed4580ad09e0029a37c88e37b553c0c5
yangjianfengo1 <yangjianfeng01@baidu.com>
2023-07-23 03:02:37 +0000

    use is_same

--- paddle/phi/kernels/gpu/group_norm_kernel.cu
+++ paddle/phi/kernels/gpu/group_norm_kernel.cu
@@ -937,7 +937,7 @@ void GroupNormKernel(const Context& dev_ctx,
   }
 
 #ifdef PADDLE_CUDA_BF16
-  if (is_same_v<T, phi::dtype::float16>::value && data_layout_str == "NHWC") {
+  if (is_same<T, phi::dtype::float16>::value && data_layout_str == "NHWC") {
     GroupNormNHWCKernel<phi::dtype::bfloat16, Context>(dev_ctx,
                                                        x,
                                                        scale,
