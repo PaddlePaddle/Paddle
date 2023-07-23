a2aa7a9b0d0ea4c83dc1bb5403812a31edc223a8
phlrain <phliuhongyu@126.com>
2023-07-23 12:46:44 +0000

    remove usless code

--- python/paddle/jit/dy2static/partial_program.py
+++ python/paddle/jit/dy2static/partial_program.py
@@ -505,7 +505,6 @@ class PartialProgramLayer:
                 progs = self._train_pure_fp16_forward_backward_program
             else:
                 progs = self._train_forward_backward_program
-            print("forward prog", progs[0])
             return progs[0]
         else:
             return self.infer_program
@@ -519,7 +518,6 @@ class PartialProgramLayer:
                 progs = self._train_pure_fp16_forward_backward_program
             else:
                 progs = self._train_forward_backward_program
-            print("bwackward prog", progs[1])
             return progs[1]
         else:
             """
