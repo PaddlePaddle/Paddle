9bb6cdb0f1f174cf0eb0b4450d707f8afda3dd0e
RedContritio <RedContritio@qq.com>
2023-07-23 10:21:04 +0000

    used reinterpret_cast in paddle/fluid/pybind/eager_legacy_op_function_generator.cc to remove 1496 cppcoreguidelines-pro-type-cstyle-cast warnings

--- paddle/fluid/pybind/eager_legacy_op_function_generator.cc
+++ paddle/fluid/pybind/eager_legacy_op_function_generator.cc
@@ -129,7 +129,7 @@ static PyObject * %s(PyObject *self, PyObject *args, PyObject *kwargs)
   }
 })";
 
-const char* PYBIND_ITEM_TEMPLATE = R"(  {"%s", (PyCFunction)(void(*)(void))%s, METH_VARARGS | METH_KEYWORDS, "C++ interface function for %s in dygraph."},)";
+const char* PYBIND_ITEM_TEMPLATE = R"(  {"%s", reinterpret_cast<PyCFunction>(reinterpret_cast<void (*)(void)>(%s)), METH_VARARGS | METH_KEYWORDS, "C++ interface function for %s in dygraph."},)";
 
 // These operators will skip automatical code generation and
 // need to be handwritten in CUSTOM_HANDWRITE_OP_FUNC_FILE
@@ -512,14 +512,17 @@ int main(int argc, char* argv[]) {
   auto core_ops_infos = GenerateCoreOpsInfoMap();
   std::string core_ops_infos_registry =
       "  {\"get_core_ops_args_info\", "
-      "(PyCFunction)(void(*)(void))eager_get_core_ops_args_info, METH_NOARGS, "
+      "reinterpret_cast<PyCFunction>(reinterpret_cast<void "
+      "(*)(void)>(eager_get_core_ops_args_info)), METH_NOARGS, "
       "\"C++ interface function for eager_get_core_ops_args_info.\"},\n"
       "  {\"get_core_ops_args_type_info\", "
-      "(PyCFunction)(void(*)(void))eager_get_core_ops_args_type_info, "
+      "reinterpret_cast<PyCFunction>(reinterpret_cast<void "
+      "(*)(void)>(eager_get_core_ops_args_type_info)), "
       "METH_NOARGS, "
       "\"C++ interface function for eager_get_core_ops_args_type_info.\"},\n"
       "  {\"get_core_ops_returns_info\", "
-      "(PyCFunction)(void(*)(void))eager_get_core_ops_returns_info, "
+      "reinterpret_cast<PyCFunction>(reinterpret_cast<void "
+      "(*)(void)>(eager_get_core_ops_returns_info)), "
       "METH_NOARGS, \"C++ interface function for "
       "eager_get_core_ops_returns_info.\"},\n";
 
