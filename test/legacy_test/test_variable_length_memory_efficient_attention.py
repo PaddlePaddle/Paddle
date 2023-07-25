537ea894ec0a82cd10608da5d357deccb386069b
carryyu <569782149@qq.com>
2023-07-25 11:31:50 +0800

    update variable_length_memory_efficient_attention unittest

--- test/legacy_test/test_variable_length_memory_efficient_attention.py
+++ test/legacy_test/test_variable_length_memory_efficient_attention.py
@@ -68,8 +68,8 @@ def naive_attention_impl(query, key, value, mask, scale):
 
 
 @unittest.skipIf(
-    not core.is_compiled_with_cuda() or get_cuda_version() < 11030,
-    "core is not compiled with CUDA and cuda version need larger than or equal to 11.3",
+    not core.is_compiled_with_cuda() or get_cuda_version() < 11020,
+    "core is not compiled with CUDA and cuda version need larger than or equal to 11.2",
 )
 class TestMemEffAttentionVariableAPI(unittest.TestCase):
     def setUp(self):
@@ -204,6 +204,10 @@ class TestMemEffAPIVariableDtypeBF16(TestMemEffAttentionVariableAPI):
         self.scale = 1.0 / np.sqrt(self.shape[-1])
 
 
+@unittest.skipIf(
+    not core.is_compiled_with_cuda() or get_cuda_version() < 11020,
+    "core is not compiled with CUDA and cuda version need larger than or equal to 11.2",
+)
 class TestMemEffAPIVariableDtypeFP16Static(unittest.TestCase):
     def setUp(self):
         self.name = "MemEffAPIVariableStatic_fp16"
