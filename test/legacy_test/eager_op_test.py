254b08a156971cd4ceac9b7bad1622290ece35d4
kangguangli <kangguangli@hotmail.com>
2023-07-25 10:47:28 +0800

    [BugFix] fix random fail of test_bilinear_interp_v2_op (#55643)
    * fix random fail of test_bilinear_interp_v2_op
    
    * reset if compiledProgram

--- test/legacy_test/eager_op_test.py
+++ test/legacy_test/eager_op_test.py
@@ -1211,13 +1211,21 @@ class OpTest(unittest.TestCase):
             return
 
         set_flags({"FLAGS_enable_new_ir_in_executor": True})
-
+        new_scope = paddle.static.Scope()
         executor = Executor(place)
+        new_program = None
+        if isinstance(program, paddle.static.CompiledProgram):
+            new_program = fluid.CompiledProgram(
+                program._program, build_strategy=program._build_strategy
+            )
+        else:
+            new_program = program.clone()
         ir_outs = executor.run(
-            program,
+            new_program,
             feed=feed_map,
             fetch_list=fetch_list,
             return_numpy=False,
+            scope=new_scope,
         )
         assert len(outs) == len(
             ir_outs
