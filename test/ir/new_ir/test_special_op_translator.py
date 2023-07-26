9180bc7128e644fde260bbf4e8876fac019887b2
kangguangli <kangguangli@hotmail.com>
2023-07-26 15:17:24 +0800

    [NewIR]support attribute hook and fix_reduce_all (#55553)
    * support attribute hook and fix_reduce_all
    
    * resolve merge conflicts
    
    * fix coverage ci
    
    * trigger CI
    
    * trigger CI
    
    * fix coverage ci

--- test/ir/new_ir/test_special_op_translator.py
+++ test/ir/new_ir/test_special_op_translator.py
@@ -160,5 +160,39 @@ class TestOneHotOpTranscriber(unittest.TestCase):
         _ = ir.translate_to_new_ir(main_program.desc)
 
 
+class TestReduceOpTranscriber(unittest.TestCase):
+    def test_reduce_all(self):
+        place = core.Place()
+        place.set_place(paddle.CPUPlace())
+        exe = paddle.static.Executor(place)
+
+        new_scope = paddle.static.Scope()
+        main_program = paddle.static.Program()
+        with paddle.static.scope_guard(new_scope):
+            with paddle.static.program_guard(main_program):
+                arr = np.ones([2, 2], dtype="float32")
+                x = paddle.to_tensor(arr, dtype='int32')
+                out1 = paddle.all(x)
+
+                out = exe.run(main_program, {}, fetch_list=[out1.name])
+                np.testing.assert_array_equal(out[0], np.all(arr))
+
+    def test_with_axis(self):
+        place = core.Place()
+        place.set_place(paddle.CPUPlace())
+        exe = paddle.static.Executor(place)
+
+        new_scope = paddle.static.Scope()
+        main_program = paddle.static.Program()
+        with paddle.static.scope_guard(new_scope):
+            with paddle.static.program_guard(main_program):
+                arr = np.ones([2, 2], dtype="float32")
+                x = paddle.to_tensor(arr, dtype='int32')
+                out1 = paddle.all(x, axis=0)
+
+                out = exe.run(main_program, {}, fetch_list=[out1.name])
+                np.testing.assert_array_equal(out[0], np.all(arr, axis=0))
+
+
 if __name__ == "__main__":
     unittest.main()
