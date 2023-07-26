74c48d55586419b1f2985a96339c052f23b121c3
phlrain <phliuhongyu@126.com>
2023-07-26 01:46:17 +0000

    remove usless header

--- paddle/phi/kernels/funcs/tensor_formatter.cc
+++ paddle/phi/kernels/funcs/tensor_formatter.cc
@@ -16,8 +16,6 @@
 
 #include <string>
 
-#include "paddle/fluid/framework/convert_utils.h"
-
 #include "paddle/phi/backends/context_pool.h"
 #include "paddle/phi/common/place.h"
 #include "paddle/phi/core/tensor_utils.h"
