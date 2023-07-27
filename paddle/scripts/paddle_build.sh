1dd88fc497d7e7eb118289c0ce8a969df82dd9b5
Zhenghai Zhang <1719571694@qq.com>
2023-07-27 10:27:37 +0800

    fix bug

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3245,7 +3245,7 @@ function clang-tidy_check() {
     num_diff_files=$(echo "$diff_files" | wc -l)
     echo -e "diff files between pr and ${BRANCH}:\n${diff_files}"
 
-    echo "Checking code style by pre-commit ..."
+    echo "Checking code style by clang-tidy ..."
     startTime_s=`date +%s`
     pre-commit run clang-tidy --files ${diff_files};check_error=$?
     endTime_s=`date +%s`
@@ -3253,32 +3253,24 @@ function clang-tidy_check() {
     echo "Files Num: $[ $num_diff_files ]"
     echo "Check Time: $[ $endTime_s - $startTime_s ]s"
 
-    if test ! -z "$(git diff)"; then
-        echo -e '\n************************************************************************************'
-        echo -e "These files have been formatted by code format hook. You should use pre-commit to \
-    format them before git push."
-        echo -e '************************************************************************************\n'
-        git diff 2>&1
-    fi
-
     echo -e '\n************************************************************************************'
     if [ ${check_error} != 0 ];then
-        echo "Your PR code style check failed."
-        echo "Please install pre-commit locally and set up git hook scripts:"
+        echo "Your PR code style clang-tidy check failed."
+        echo "Please install clang-tidy locally:"
         echo ""
-        echo "    pip install pre-commit==2.17.0"
-        echo "    pre-commit install"
+        echo "    pip install clang-tidy==15.0.2.1"
+        echo ""
         echo ""
         if [[ $num_diff_files -le 100 ]];then
-            echo "Then, run pre-commit to check codestyle issues in your PR:"
+            echo "After the build is completed, run clang-tidy to check codestyle issues in your PR:"
             echo ""
-            echo "    pre-commit run --files" $(echo ${diff_files} | tr "\n" " ")
+            echo "    pre-commit run clang-tidy --files" $(echo ${diff_files} | tr "\n" " ")
             echo ""
         fi
         echo "For more information, please refer to our codestyle check guide:"
         echo "https://www.paddlepaddle.org.cn/documentation/docs/zh/develop/dev_guides/git_guides/codestyle_check_guide_cn.html"
     else
-        echo "Your PR code style check passed."
+        echo "Your PR code style clang-tidy check passed."
     fi
     echo -e '************************************************************************************\n'
 
