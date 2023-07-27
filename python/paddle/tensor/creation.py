58a03d417987e0979466547b0861044fbfcd2dcf
GGBond8488 <33050871+GGBond8488@users.noreply.github.com>
2023-07-27 15:33:52 +0800

    【inplace api】batch add inplace api paddle.log_, paddle.i0_, paddle.nn.functional.leaky_relu_... (#55576)
    * batch add inplace api
    
    * add inplace test
    
    * add activation inplace
    
    * fix test
    
    * remove atan2 ge, gt, le, lt, nq
    
    * remove atan2 ge, gt, le, lt, nq
    
    * fix windows ci error
    
    * rerun ci
    
    * fix typro
    
    * fix bugs
    
    ---------
    
    Co-authored-by: zhangrui34 <v_zhangrui34@baidu.com>

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -22,6 +22,7 @@ import numpy as np
 
 import paddle
 from paddle import _C_ops
+from paddle.utils.inplace_utils import inplace_apis_in_dygraph_only
 
 from ..fluid.data_feeder import (
     check_dtype,
@@ -1462,6 +1463,17 @@ def tril(x, diagonal=0, name=None):
         return _tril_triu_op(LayerHelper('tril', **locals()))
 
 
+@inplace_apis_in_dygraph_only
+def tril_(x, diagonal=0, name=None):
+    r"""
+    Inplace version of ``tril`` API, the output Tensor will be inplaced with input ``x``.
+    Please refer to :ref:`api_paddle_tril`.
+    """
+
+    if in_dynamic_mode():
+        return _C_ops.tril_(x, diagonal)
+
+
 def triu(x, diagonal=0, name=None):
     r"""
     Return the upper triangular part of a matrix (2-D tensor) or batch of matrices
@@ -1522,6 +1534,17 @@ def triu(x, diagonal=0, name=None):
         return _C_ops.triu(x, diagonal)
     else:
         return _tril_triu_op(LayerHelper('triu', **locals()))
+
+
+@inplace_apis_in_dygraph_only
+def triu_(x, diagonal=0, name=None):
+    r"""
+    Inplace version of ``triu`` API, the output Tensor will be inplaced with input ``x``.
+    Please refer to :ref:`api_paddle_triu`.
+    """
+
+    if in_dynamic_mode():
+        return _C_ops.triu_(x, diagonal)
 
 
 def meshgrid(*args, **kwargs):
