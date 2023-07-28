de550dbe494aa785e664a34c7c09e9879cbd9494
lzy <569782149@qq.com>
2023-07-29 03:19:25 +0800

    Update variable_length_memory_efficient_attention.cu

--- paddle/phi/kernels/fusion/cutlass/variable_length_memory_efficient_attention.cu
+++ paddle/phi/kernels/fusion/cutlass/variable_length_memory_efficient_attention.cu
@@ -88,13 +88,12 @@ void MultiHeadAttentionVariableForwardKernel(
       // not support mask_broad_cast
       return;
     }
-    if (reinterpret_cast<uintptr_t>(params.mask_ptr) % 16 == 0 &&
+    if (mask && reinterpret_cast<uintptr_t>(params.mask_ptr) % 16 == 0 &&
         params.ldm % (16 / sizeof(T)) == 0 && !KernelType::kMaskIsAligned) {
       return;
     }
-    if (!(reinterpret_cast<uintptr_t>(params.mask_ptr) % 16 == 0 &&
-          params.ldm % (16 / sizeof(T)) == 0) &&
-        KernelType::kMaskIsAligned) {
+    if (mask && !(reinterpret_cast<uintptr_t>(params.mask_ptr) % 16 == 0 &&
+        params.ldm % (16 / sizeof(T)) == 0) && KernelType::kMaskIsAligned) {
       return;
     }
     if (KernelType::kSingleValueIteration &&
