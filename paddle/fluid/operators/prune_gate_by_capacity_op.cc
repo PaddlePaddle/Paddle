91b719d9f27964991dee7fcd643f2f9b1e71a6e1
andsonder <changlu@keter.top>
2023-07-28 15:47:44 +0000

    remove useless codes

--- paddle/fluid/operators/prune_gate_by_capacity_op.cc
+++ paddle/fluid/operators/prune_gate_by_capacity_op.cc
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/operators/prune_gate_by_capacity_op.h"
+#include "paddle/fluid/framework/op_registry.h"
+#include "paddle/fluid/framework/operator.h"
 
 namespace paddle {
 namespace operators {
