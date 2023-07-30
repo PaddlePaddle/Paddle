2933b911763f960f1f0601606bd210c9c424b8a3
longranger2 <836253168@qq.com>
2023-07-30 22:52:20 +0800

    fix bug

--- python/paddle/distributed/fleet/meta_optimizers/qat_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/qat_optimizer.py
@@ -97,7 +97,7 @@ class QATOptimizer(MetaOptimizerBase):
         optimize_ops, params_grads = self.inner_opt.minimize(
             loss,
             startup_program=startup_program,
-            parameter=parameter_list,
+            parameters=parameter_list,
             no_grad_set=no_grad_set,
         )
         device = paddle.device.get_device()
