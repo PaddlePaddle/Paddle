37c3c345d985e4368a151ad797237429f4f59fb0
megemini <megemini@outlook.com>
2023-07-31 15:12:36 +0800

    Fix convert directive (#55811)
    * [Change] directive_pattern to all patterns
    
    * [Change] patch tensor place
    
    * [Change] patch float precision
    
    * [Change] print_summary
    
    * [Add] change uniform.py docstring
    
    * [Fix] fix print_summary whl_error
    
    * [Change] catch except

--- python/paddle/distribution/uniform.py
+++ python/paddle/distribution/uniform.py
@@ -62,33 +62,42 @@ class Uniform(distribution.Distribution):
     Examples:
         .. code-block:: python
 
-            import paddle
-            from paddle.distribution import Uniform
+            >>> import paddle
+            >>> from paddle.distribution import Uniform
+            >>> paddle.seed(2023)
 
-            # Without broadcasting, a single uniform distribution [3, 4]:
-            u1 = Uniform(low=3.0, high=4.0)
-            # 2 distributions [1, 3], [2, 4]
-            u2 = Uniform(low=[1.0, 2.0], high=[3.0, 4.0])
-            # 4 distributions
-            u3 = Uniform(low=[[1.0, 2.0], [3.0, 4.0]],
-                        high=[[1.5, 2.5], [3.5, 4.5]])
+            >>> # Without broadcasting, a single uniform distribution [3, 4]:
+            >>> u1 = Uniform(low=3.0, high=4.0)
+            >>> # 2 distributions [1, 3], [2, 4]
+            >>> u2 = Uniform(low=[1.0, 2.0], high=[3.0, 4.0])
+            >>> # 4 distributions
+            >>> u3 = Uniform(low=[[1.0, 2.0], [3.0, 4.0]],
+            ...             high=[[1.5, 2.5], [3.5, 4.5]])
+            ...
+            >>> # With broadcasting:
+            >>> u4 = Uniform(low=3.0, high=[5.0, 6.0, 7.0])
 
-            # With broadcasting:
-            u4 = Uniform(low=3.0, high=[5.0, 6.0, 7.0])
+            >>> # Complete example
+            >>> value_tensor = paddle.to_tensor([0.8], dtype="float32")
 
-            # Complete example
-            value_tensor = paddle.to_tensor([0.8], dtype="float32")
+            >>> uniform = Uniform([0.], [2.])
 
-            uniform = Uniform([0.], [2.])
+            >>> sample = uniform.sample([2])
+            >>> # a random tensor created by uniform distribution with shape: [2, 1]
+            >>> entropy = uniform.entropy()
+            >>> print(entropy)
+            Tensor(shape=[1], dtype=float32, place=Place(cpu), stop_gradient=True,
+                [0.69314718])
 
-            sample = uniform.sample([2])
-            # a random tensor created by uniform distribution with shape: [2, 1]
-            entropy = uniform.entropy()
-            # [0.6931472] with shape: [1]
-            lp = uniform.log_prob(value_tensor)
-            # [-0.6931472] with shape: [1]
-            p = uniform.probs(value_tensor)
-            # [0.5] with shape: [1]
+            >>> lp = uniform.log_prob(value_tensor)
+            >>> print(lp)
+            Tensor(shape=[1], dtype=float32, place=Place(cpu), stop_gradient=True,
+                [-0.69314718])
+
+            >>> p = uniform.probs(value_tensor)
+            >>> print(p)
+            Tensor(shape=[1], dtype=float32, place=Place(cpu), stop_gradient=True,
+                [0.50000000])
     """
 
     def __init__(self, low, high, name=None):
