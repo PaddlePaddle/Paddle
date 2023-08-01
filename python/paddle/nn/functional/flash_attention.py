29a118d3ccffcdab5936d45325483b026e049e57
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-08-01 02:32:42 +0000

    skip xtest

--- python/paddle/nn/functional/flash_attention.py
+++ python/paddle/nn/functional/flash_attention.py
@@ -451,10 +451,12 @@ def scaled_dot_product_attention(
     Examples:
         .. code-block:: python
             # required: skiptest
+            >>> # xdoctest: +SKIP()
             >>> import paddle
             >>> q = paddle.rand((1, 128, 2, 16), dtype=paddle.bfloat16)
             >>> output = paddle.nn.functional.scaled_dot_product_attention(q, q, q, None, 0.9, False)
             >>> print(output)
+            >>> # xdoctest: -SKIP
     """
     assert attn_mask is None, "attn_mask is not supported yet"
     out, _ = flash_attention(query, key, value, dropout_p, is_causal)
