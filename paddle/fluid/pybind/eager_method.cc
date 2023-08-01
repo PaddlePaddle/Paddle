b07747919759531e5d487b0c22fed42ecdc5a445
Wang Huan <wanghuan29@baidu.com>
2023-08-01 10:58:12 +0000

    refine

--- paddle/fluid/pybind/eager_method.cc
+++ paddle/fluid/pybind/eager_method.cc
@@ -921,31 +921,31 @@ Returns:
 Examples:
     .. code-block:: python
 
-        import paddle
+      import paddle
 
-        x = paddle.to_tensor(1.0, stop_gradient=False)
-        detach_x = x.detach()
-        detach_x[:] = 10.0
-        print(x)  # Tensor(shape=[1], dtype=float32, place=CPUPlace, stop_gradient=False,
+      x = paddle.to_tensor([1.0], stop_gradient=False)
+      detach_x = x.detach()
+      detach_x[0] = 10.0
+      print(x)  # Tensor(shape=[1], dtype=float32, place=CPUPlace, stop_gradient=False,
                   #        [10.])
-        y = x**2
-        y.backward()
-        print(x.grad)         # [20.0]
-        print(detach_x.grad)  # None, 'stop_gradient=True' by default
+      y = x**2
+      y.backward()
+      print(x.grad)         # [20.0]
+      print(detach_x.grad)  # None, 'stop_gradient=True' by default
 
-        detach_x.stop_gradient = False # Set stop_gradient to be False, supported auto-grad
-        z = detach_x**3
-        z.backward()
+      detach_x.stop_gradient = False # Set stop_gradient to be False, supported auto-grad
+      z = detach_x**3
+      z.backward()
 
-        print(x.grad)         # [20.0], detach_x is detached from x's graph, not affect each other
-        print(detach_x.grad)  # [300.0], detach_x has its own graph
+      print(x.grad)         # [20.0], detach_x is detached from x's graph, not affect each other
+      print(detach_x.grad)  # [300.0], detach_x has its own graph
 
-        # Due to sharing of data with origin Tensor, There are some unsafe operations:
-        # y = 2 * x
-        # detach_x[:] = 5.0
-        # y.backward()
-        # It will raise Error:
-        #   one of the variables needed for gradient computation has been modified by an inplace operation.
+      # Due to sharing of data with origin Tensor, There are some unsafe operations:
+      # y = 2 * x
+      # detach_x[:] = 5.0
+      # y.backward()
+      # It will raise Error:
+      #   one of the variables needed for gradient computation has been modified by an inplace operation.
 )DOC");
 
 static PyObject* tensor_method_detach(TensorObject* self,
