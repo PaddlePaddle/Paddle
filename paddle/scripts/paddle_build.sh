05d66e035bb3cce97e7003eaf49d2a88e9dcaba0
Wang Xin <xinwang614@gmail.com>
2023-08-02 16:24:25 +0000

    [WIP][clang-tidy]test clang-tidy CI time

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3238,6 +3238,45 @@ function trt_convert_test() {
     fi
 }
 
+function clang-tidy_check() {
+    set +x
+
+    diff_files=$(git diff --name-only --diff-filter=ACMR ${BRANCH})
+    num_diff_files=$(echo "$diff_files" | wc -l)
+    echo -e "diff files between pr and ${BRANCH}:\n${diff_files}"
+
+    echo "Checking code style by clang-tidy ..."
+    startTime_s=`date +%s`
+    pre-commit run clang-tidy --files ${diff_files};check_error=$?
+    endTime_s=`date +%s`
+    [ -n "$startTime_firstBuild" ] && startTime_s=$startTime_firstBuild
+    echo "Files Num: $[ $num_diff_files ]"
+    echo "Check Time: $[ $endTime_s - $startTime_s ]s"
+
+    echo -e '\n************************************************************************************'
+    if [ ${check_error} != 0 ];then
+        echo "Your PR code style clang-tidy check failed."
+        echo "Please install clang-tidy locally:"
+        echo ""
+        echo "    pip install clang-tidy==15.0.2.1"
+        echo ""
+        echo ""
+        if [[ $num_diff_files -le 100 ]];then
+            echo "After the build is completed, run clang-tidy to check codestyle issues in your PR:"
+            echo ""
+            echo "    pre-commit run clang-tidy --files" $(echo ${diff_files} | tr "\n" " ")
+            echo ""
+        fi
+        echo "For more information, please refer to our codestyle check guide:"
+        echo "https://www.paddlepaddle.org.cn/documentation/docs/zh/develop/dev_guides/git_guides/codestyle_check_guide_cn.html"
+    else
+        echo "Your PR code style clang-tidy check passed."
+    fi
+    echo -e '************************************************************************************\n'
+
+    exit ${check_error} 
+}
+
 function build_pr_and_develop() {
     run_setup ${PYTHON_ABI:-""} bdist_wheel ${parallel_number}
     if [ ! -d "${PADDLE_ROOT}/build/python/dist/" ]; then
@@ -3273,7 +3312,8 @@ function build_pr_and_develop() {
         mkdir ${PADDLE_ROOT}/build/dev_whl && cp ${PADDLE_ROOT}/build/python/dist/*.whl ${PADDLE_ROOT}/build/dev_whl
     fi
     generate_api_spec "$1" "DEV"
-
+    
+    clang-tidy_check
 }
 
 function build_develop() {
