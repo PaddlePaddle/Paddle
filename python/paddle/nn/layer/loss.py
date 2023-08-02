4498509603980732fbcc7204ef2efae5edab0dd9
enkilee <luhputu0815@gmail.com>
2023-08-02 05:14:49 +0000

    test=docs_preview

--- python/paddle/nn/layer/loss.py
+++ python/paddle/nn/layer/loss.py
@@ -844,18 +844,18 @@ class NLLLoss(Layer):
 
             >>> import paddle
 
-            >>>  nll_loss = paddle.nn.loss.NLLLoss()
-            >>>  log_softmax = paddle.nn.LogSoftmax(axis=1)
+            >>> nll_loss = paddle.nn.loss.NLLLoss()
+            >>> log_softmax = paddle.nn.LogSoftmax(axis=1)
 
-            >>>  input = paddle.to_tensor([[0.88103855, 0.9908683 , 0.6226845 ],
-            ...                            [0.53331435, 0.07999352, 0.8549948 ],
-            ...                            [0.25879037, 0.39530203, 0.698465  ],
-            ...                            [0.73427284, 0.63575995, 0.18827209],
-            ...                            [0.05689114, 0.0862954 , 0.6325046 ]], "float32")
-            >>>  log_out = log_softmax(input)
-            >>>  label = paddle.to_tensor([0, 2, 1, 1, 0], "int64")
-            >>>  result = nll_loss(log_out, label)
-            >>>  print(result)
+            >>> input = paddle.to_tensor([[0.88103855, 0.9908683 , 0.6226845 ],
+            ...                           [0.53331435, 0.07999352, 0.8549948 ],
+            ...                           [0.25879037, 0.39530203, 0.698465  ],
+            ...                           [0.73427284, 0.63575995, 0.18827209],
+            ...                           [0.05689114, 0.0862954 , 0.6325046 ]], "float32")
+            >>> log_out = log_softmax(input)
+            >>> label = paddle.to_tensor([0, 2, 1, 1, 0], "int64")
+            >>> result = nll_loss(log_out, label)
+            >>> print(result)
             Tensor(shape=[], dtype=float32, place=Place(cpu), stop_gradient=True,
             1.07202101)
 
