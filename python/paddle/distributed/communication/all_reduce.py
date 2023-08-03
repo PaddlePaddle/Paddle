4743843b7535f48262dd3be0cdacc007aa93f2db
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-08-03 20:54:57 +0800

    Update all_reduce.py

--- python/paddle/distributed/communication/all_reduce.py
+++ python/paddle/distributed/communication/all_reduce.py
@@ -43,17 +43,17 @@ def all_reduce(tensor, op=ReduceOp.SUM, group=None, sync_op=True):
         .. code-block:: python
 
             # required: distributed
-            import paddle
-            import paddle.distributed as dist
+            >>> import paddle
+            >>> import paddle.distributed as dist
 
-            dist.init_parallel_env()
-            if dist.get_rank() == 0:
-                data = paddle.to_tensor([[4, 5, 6], [4, 5, 6]])
-            else:
-                data = paddle.to_tensor([[1, 2, 3], [1, 2, 3]])
-            dist.all_reduce(data)
-            print(data)
-            # [[5, 7, 9], [5, 7, 9]] (2 GPUs)
+            >>> dist.init_parallel_env()
+            >>> if dist.get_rank() == 0:
+            ...     data = paddle.to_tensor([[4, 5, 6], [4, 5, 6]])
+            >>> else:
+            ...     data = paddle.to_tensor([[1, 2, 3], [1, 2, 3]])
+            >>> dist.all_reduce(data)
+            >>> print(data)
+            [[5, 7, 9], [5, 7, 9]] (2 GPUs)
     """
     return stream.all_reduce(
         tensor, op=op, group=group, sync_op=sync_op, use_calc_stream=False
