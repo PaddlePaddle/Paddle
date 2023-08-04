4649d4b8a06d3d216b1ba63c56c1680284cb3901
SigureMo <sigure.qaq@gmail.com>
2023-08-04 10:36:47 +0800

    stanh, test=docs_preview

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -324,7 +324,10 @@ def stanh(x, scale_a=0.67, scale_b=1.7159, name=None):
             >>> import paddle
 
             >>> x = paddle.to_tensor([1.0, 2.0, 3.0, 4.0])
-            >>> out = paddle.stanh(x, scale_a=0.67, scale_b=1.72) # [1.00616539, 1.49927628, 1.65933108, 1.70390463]
+            >>> out = paddle.stanh(x, scale_a=0.67, scale_b=1.72)
+            >>> print(out)
+            Tensor(shape=[4], dtype=float32, place=Place(cpu), stop_gradient=True,
+            [1.00616539, 1.49927628, 1.65933096, 1.70390463])
 
     """
 
