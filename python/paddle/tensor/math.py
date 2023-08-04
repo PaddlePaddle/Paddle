164c98c70ebb755a626cf8c8ae690440209c18c6
SigureMo <sigure.qaq@gmail.com>
2023-08-04 10:40:20 +0800

    pow, test=docs_preview

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -474,19 +474,19 @@ def pow(x, y, name=None):
             >>> # example 1: y is a float or int
             >>> res = paddle.pow(x, 2)
             >>> print(res)
-            >>> # Tensor(shape=[3], dtype=float32, place=CUDAPlace(0), stop_gradient=True,
-            >>> #        [1., 4., 9.])
+            Tensor(shape=[3], dtype=float32, place=Place(cpu), stop_gradient=True,
+            [1., 4., 9.])
             >>> res = paddle.pow(x, 2.5)
             >>> print(res)
-            >>> # Tensor(shape=[3], dtype=float32, place=CUDAPlace(0), stop_gradient=True,
-            >>> #        [1.         , 5.65685415 , 15.58845711])
+            Tensor(shape=[3], dtype=float32, place=Place(cpu), stop_gradient=True,
+            [1.         , 5.65685415 , 15.58845711])
 
             >>> # example 2: y is a Tensor
             >>> y = paddle.to_tensor([2], dtype='float32')
             >>> res = paddle.pow(x, y)
             >>> print(res)
-            >>> # Tensor(shape=[3], dtype=float32, place=CUDAPlace(0), stop_gradient=True,
-            >>> #        [1., 4., 9.])
+            Tensor(shape=[3], dtype=float32, place=Place(cpu), stop_gradient=True,
+            [1., 4., 9.])
 
     """
     # in dynamic graph mode
