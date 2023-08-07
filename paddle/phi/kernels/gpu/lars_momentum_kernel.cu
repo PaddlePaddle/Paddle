a3dd4618907b370ed0d6323a7dcd7524ab08f803
gouzi <530971494@qq.com>
2023-08-07 12:16:57 +0800

    Add Registration

--- paddle/phi/kernels/gpu/lars_momentum_kernel.cu
+++ paddle/phi/kernels/gpu/lars_momentum_kernel.cu
@@ -680,4 +680,10 @@ PD_REGISTER_KERNEL(lars_momentum,
                    phi::LarsMomentumKernel,
                    float,
                    double,
-                   phi::dtype::float16) {}
+                   phi::dtype::float16) {
+  if (kernel_key.dtype() == phi::DataType::FLOAT16) {
+    kernel->OutputAt(1).SetDataType(phi::DataType::FLOAT16);
+    kernel->OutputAt(2).SetDataType(phi::DataType::FLOAT16);
+    kernel->OutputAt(3).SetDataType(phi::DataType::FLOAT16);
+  }
+}
