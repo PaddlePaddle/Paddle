bd839999800cacb6ffa9edf002282c117c2849ec
Charles-hit <56987902+Charles-hit@users.noreply.github.com>
2023-08-08 11:18:01 +0800

    [PRIM][IR] Migrate vjp rules to new ir in non primitive mode (#55647)
    * [prim][newir] add basic framework for primitive
    
    * support desctensor in new ir
    
    * add vjp interface
    
    * support vjp in new ir
    
    * support vjp in new ir
    
    * polish vjp interface
    
    * fix stop_gradients set
    
    * fix vjp dispatch
    
    * add comment
    
    * add vjp test for new ir
    
    * add test for tanh vjp
    
    * [prim][newir] add basic framework for primitive
    
    * support desctensor in new ir
    
    * support vjp in new ir
    
    * support vjp in new ir
    
    * polish vjp interface
    
    * fix stop_gradients set
    
    * fix vjp dispatch
    
    * add comment
    
    * add vjp test for new ir
    
    * add test for tanh vjp
    
    * add eager and static backend for warp lower level api
    
    * support call_vjp pybind
    
    * polish code and add test for vjp
    
    * remove useless code
    
    * polish code
    
    * remove useless code
    
    * support mean vjp
    
    * add test for mean vjp and support has_vjp function
    
    * fix call_vjp
    
    * polish code
    
    * add primitive ops set for backend
    
    * add vjp test for tanh_
    
    * fix inference CI
    
    * fix inference ci
    
    * modify fluid cmake
    
    * remove useless deps
    
    * add cmake
    
    ---------
    
    Co-authored-by: cxxly <chenxx_id@163.com>
    Co-authored-by: zhangbo9674 <zhangbo54@baidu.com>

--- paddle/fluid/ir/interface/vjp.h
+++ paddle/fluid/ir/interface/vjp.h
@@ -20,21 +20,24 @@ namespace dialect {
 class VjpInterface : public ir::OpInterfaceBase<VjpInterface> {
  public:
   struct Concept {
-    explicit Concept(std::vector<std::vector<ir::Value>> (*vjp)(
-        std::vector<std::vector<ir::Value>> out_grads,
+    explicit Concept(std::vector<std::vector<ir::OpResult>> (*vjp)(
+        ir::Operation* op,
+        const std::vector<std::vector<ir::OpResult>>& out_grads,
         const std::vector<std::vector<int>>& stop_gradients))
         : vjp_(vjp) {}
-    std::vector<std::vector<ir::Value>> (*vjp_)(
-        std::vector<std::vector<ir::Value>> out_grads,
+    std::vector<std::vector<ir::OpResult>> (*vjp_)(
+        ir::Operation* op,
+        const std::vector<std::vector<ir::OpResult>>& out_grads,
         const std::vector<std::vector<int>>& stop_gradients);
   };
 
   template <class ConcreteOp>
   struct Model : public Concept {
-    static std::vector<std::vector<ir::Value>> Vjp(
-        std::vector<std::vector<ir::Value>> out_grads,
+    static std::vector<std::vector<ir::OpResult>> Vjp(
+        ir::Operation* op,
+        const std::vector<std::vector<ir::OpResult>>& out_grads,
         const std::vector<std::vector<int>>& stop_gradients) {
-      return ConcreteOp::Vjp(out_grads, stop_gradients);
+      return ConcreteOp::Vjp(op, out_grads, stop_gradients);
     }
 
     Model() : Concept(Vjp) {}
@@ -43,10 +46,11 @@ class VjpInterface : public ir::OpInterfaceBase<VjpInterface> {
   VjpInterface(ir::Operation* op, Concept* impl)
       : ir::OpInterfaceBase<VjpInterface>(op), impl_(impl) {}
 
-  std::vector<std::vector<ir::Value>> Vjp(
-      std::vector<std::vector<ir::Value>> out_grads,
+  std::vector<std::vector<ir::OpResult>> Vjp(
+      ir::Operation* op,
+      const std::vector<std::vector<ir::OpResult>>& out_grads,
       const std::vector<std::vector<int>>& stop_gradients) {
-    return impl_->vjp_(out_grads, stop_gradients);
+    return impl_->vjp_(op, out_grads, stop_gradients);
   }
 
  private:
