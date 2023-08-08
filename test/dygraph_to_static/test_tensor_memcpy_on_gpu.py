872d62bd3378c33a3f57c4107e51047647efc2a2
liudongxue01 <liudongxue01@baidu.com>
2023-08-08 14:47:38 +0000

    [dy2static]implement tensor.cuda() in static graph

--- test/dygraph_to_static/test_tensor_memcpy_on_gpu.py
+++ test/dygraph_to_static/test_tensor_memcpy_on_gpu.py
@@ -27,6 +27,13 @@ def tensor_copy_to_cpu(x):
     return y
 
 
+@paddle.jit.to_static
+def tensor_copy_to_cuda(x):
+    x = paddle.to_tensor(x)
+    y = x.cuda()
+    return y
+
+
 class TestTensorCopyToCpuOnDefaultGPU(unittest.TestCase):
     def _run(self, to_static):
         paddle.jit.enable_to_static(to_static)
@@ -51,6 +58,32 @@ class TestTensorCopyToCpuOnDefaultGPU(unittest.TestCase):
         self.assertTrue(static_x1_place.is_gpu_place())
         self.assertTrue(dygraph_place.is_cpu_place())
         self.assertTrue(static_place.is_cpu_place())
+
+
+class TestTensorCopyToCUDAOnDefaultGPU(unittest.TestCase):
+    def _run(self, to_static):
+        paddle.jit.enable_to_static(to_static)
+        x1 = paddle.ones([1, 2, 3])
+        x2 = tensor_copy_to_cuda(x1)
+        return x1.place, x2.place, x2.numpy()
+
+    def test_tensor_cuda_on_default_gpu(self):
+        if paddle.fluid.is_compiled_with_cuda():
+            place = paddle.CUDAPlace(
+                int(os.environ.get('FLAGS_selected_gpus', 0))
+            )
+        else:
+            return
+        paddle.fluid.framework._set_expected_place(place)
+        dygraph_x1_place, dygraph_place, dygraph_res = self._run(
+            to_static=False
+        )
+        static_x1_place, static_place, static_res = self._run(to_static=True)
+        np.testing.assert_allclose(dygraph_res, static_res, rtol=1e-05)
+        self.assertTrue(dygraph_x1_place.is_gpu_place())
+        self.assertTrue(static_x1_place.is_gpu_place())
+        self.assertTrue(dygraph_place.is_gpu_place())
+        self.assertTrue(static_place.is_gpu_place())
 
 
 if __name__ == '__main__':
