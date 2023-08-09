242d26d45e8fd12dfa3cc04320959f2c1e2e0155
Frank Lin <eee4017@gmail.com>
2023-08-08 20:28:11 -0700

    Set Input Type for TensorRT convert tests (#55823)
    * set input type to fp16

--- test/ir/inference/test_trt_convert_index_select.py
+++ test/ir/inference/test_trt_convert_index_select.py
@@ -171,10 +171,12 @@ class TrtConvertIndexSelectTest(TrtLayerAutoScanTest):
         # for static_shape
         clear_dynamic_shape()
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
+        program_config.set_input_type(np.float32)
         yield self.create_inference_config(), generate_trt_nodes_num(
             False
         ), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
+        program_config.set_input_type(np.float16)
         yield self.create_inference_config(), generate_trt_nodes_num(
             False
         ), 1e-3
@@ -182,8 +184,10 @@ class TrtConvertIndexSelectTest(TrtLayerAutoScanTest):
         # for dynamic_shape
         generate_dynamic_shape(attrs)
         self.trt_param.precision = paddle_infer.PrecisionType.Float32
+        program_config.set_input_type(np.float32)
         yield self.create_inference_config(), generate_trt_nodes_num(True), 1e-5
         self.trt_param.precision = paddle_infer.PrecisionType.Half
+        program_config.set_input_type(np.float16)
         yield self.create_inference_config(), generate_trt_nodes_num(True), 1e-3
 
     def test(self):
