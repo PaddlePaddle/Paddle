9780c01d95fc90f7a50e9984b2ac3c4d1909bc24
Courtesy-Xs <Xs1580802568@gmail.com>
2023-08-10 02:59:59 +0000

    rename need_optim arg name as use_optim

--- paddle/cinn/ir/module.h
+++ paddle/cinn/ir/module.h
@@ -41,7 +41,7 @@ class Module : public ir::IrNodeRef {
       module_->target = target;
     }
 
-    void AddFunction(const ir::LoweredFunc& func, bool need_optim = true);
+    void AddFunction(const ir::LoweredFunc& func, bool use_optim = true);
     void AddBuffer(ir::Buffer buffer);
     void Clear();
 
