5c2e057e84f55634290d129b749bd978a8dcb68c
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-08-10 11:42:08 +0000

    support ir api for prim

--- paddle/fluid/primitive/composite/composite.h
+++ paddle/fluid/primitive/composite/composite.h
@@ -20,5 +20,53 @@ namespace primitive {
 
 namespace experimental {}
 
+template <typename T>
+Tensor mean_grad(const Tensor& x,
+                 const Tensor& out_grad,
+                 std::vector<int64_t> axis = {},
+                 bool keepdim = false,
+                 bool reduce_all = false);
+
+template <typename T>
+Tensor divide(const Tensor& x, const Tensor& y);
+
+template <typename T>
+Tensor add(const Tensor& x, const Tensor& y);
+
+template <typename T>
+Tensor multiply(const Tensor& x, const Tensor& y);
+
+template <typename T>
+Tensor elementwise_pow(const Tensor& x, const Tensor& y);
+
+template <typename T>
+Tensor scale(const Tensor& x,
+             const Scalar& scale = 1.0,
+             float bias = 0.0,
+             bool bias_after_scale = true);
+
+template <typename T>
+Tensor sum(const Tensor& x,
+           const IntArray& axis = {},
+           phi::DataType dtype = phi::DataType::UNDEFINED,
+           bool keepdim = false);
+
+template <typename T>
+Tensor full(const IntArray& shape,
+            const Scalar& value,
+            phi::DataType dtype = phi::DataType::FLOAT32,
+            phi::Place place = phi::CPUPlace());
+
+template <typename T>
+Tensor reshape(const Tensor& x, const IntArray& shape);
+
+template <typename T>
+Tensor expand(const Tensor& x, const IntArray& shape);
+
+template <typename T>
+Tensor tile(const Tensor& x, const IntArray& repeat_times = {});
+
+}  // namespace experimental
+}  // namespace backend
 }  // namespace primitive
 }  // namespace paddle
