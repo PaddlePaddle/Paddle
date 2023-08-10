34eb3846e10abb73050fa149b350b86c5c76d132
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-08-10 11:36:20 +0000

    dp

--- python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/hybrid_parallel_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/hybrid_parallel_optimizer.py
@@ -442,22 +442,21 @@ class HybridParallelOptimizer:
     @no_grad()
     @framework.dygraph_only
     def step(self):
-        parameters_list = list(
-            obtain_optimizer_parameters_list(self._inner_opt)
-        )
+        parameter_list = list(obtain_optimizer_parameters_list(self._inner_opt))
+        dp_parameter_list = parameter_list
         if self._sharding_enable:
             assert isinstance(self._inner_opt, DygraphShardingOptimizer)
-            self._inner_opt.reduce_gradients(parameters_list, self._hcg)
+            self._inner_opt.reduce_gradients(parameter_list, self._hcg)
             # dp later do not need to use global parameter list
             if not g_shard_norm_align_dp:
-                parameters_list = self._inner_opt.filter_parameters(
-                    parameters_list, self._hcg
+                dp_parameter_list = self._inner_opt.filter_parameters(
+                    parameter_list, self._hcg
                 )
 
         if self._dp_enable:
-            fused_allreduce_gradients(parameters_list, self._hcg)
+            fused_allreduce_gradients(dp_parameter_list, self._hcg)
 
-        self._step(parameters_list)
+        self._step(parameter_list)
 
     @no_grad()
     def minimize(
@@ -471,6 +470,7 @@ class HybridParallelOptimizer:
             else obtain_optimizer_parameters_list(self._inner_opt)
         )
         parameter_list = list(parameter_list)
+        dp_parameter_list = parameter_list
         # Here sharding should use global parameter list
         if self._sharding_enable:
             assert isinstance(self._inner_opt, DygraphShardingOptimizer)
@@ -478,12 +478,12 @@ class HybridParallelOptimizer:
 
             # dp later do not need to use global parameter list
             if not g_shard_norm_align_dp:
-                parameter_list = self._inner_opt.filter_parameters(
+                dp_parameter_list = self._inner_opt.filter_parameters(
                     parameter_list, self._hcg
                 )
 
         if self._dp_enable:
-            fused_allreduce_gradients(parameter_list, self._hcg)
+            fused_allreduce_gradients(dp_parameter_list, self._hcg)
 
         return self._inner_opt.minimize(
             loss, startup_program, parameter_list, no_grad_set
