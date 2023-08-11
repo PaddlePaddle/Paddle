836a24e8a10e57db98932f0feb61f81d21bfe509
iLeGend <824040212@qq.com>
2023-08-11 12:50:17 +0800

    fix typo

--- paddle/phi/kernels/gpu/dist_kernel.cu
+++ paddle/phi/kernels/gpu/dist_kernel.cu
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <alogrithm>
+#include <algorithm>
 
 #include "paddle/phi/backends/gpu/gpu_launch_config.h"
 #include "paddle/phi/common/float16.h"
