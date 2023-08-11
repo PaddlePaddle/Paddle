f1ab5d71a979c2494a977157d87d8b6ddaddfa32
Candy2Tang <yuhanzhangTT@163.com>
2023-08-11 09:20:00 +0800

    fix comment test=docs_preview

--- python/paddle/optimizer/adamax.py
+++ python/paddle/optimizer/adamax.py
@@ -101,13 +101,14 @@ class Adamax(Optimizer):
             ...         parameters=linear.parameters(),
             ...         beta1=beta1,
             ...         beta2=beta2,
-            ...         weight_decay=0.01)
+            ...         weight_decay=0.01
+            ... )
             >>> out.backward()
             >>> adam.step()
             >>> adam.clear_grad()
 
 
-            >>> #Note that the learning_rate of linear_2 is 0.01.
+            >>> # Note that the learning_rate of linear_2 is 0.01.
             >>> linear_1 = paddle.nn.Linear(10, 10)
             >>> linear_2 = paddle.nn.Linear(10, 10)
             >>> inp = paddle.uniform(shape=[10, 10], min=-0.1, max=0.1)
@@ -125,7 +126,8 @@ class Adamax(Optimizer):
             ...         'beta1': 0.8
             ...     }],
             ...     weight_decay=0.01,
-            ...     beta1=0.9)
+            ...     beta1=0.9
+            ... )
             >>> out.backward()
             >>> adam.step()
             >>> adam.clear_grad()
