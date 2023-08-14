cbd671454f5e8b3969301ebfbb5c39c1be82ad39
Liyulingyue <852433440@qq.com>
2023-08-14 18:38:27 +0800

    add tanhshrink in cpu/gpu forward files

--- paddle/phi/kernels/gpu/activation_kernel.cu
+++ paddle/phi/kernels/gpu/activation_kernel.cu
@@ -284,6 +284,7 @@ PD_REGISTER_KERNEL(square,
                    phi::dtype::bfloat16) {}
 
 PD_REGISTER_ACTIVATION_KERNEL(hard_shrink, HardShrinkKernel)
+PD_REGISTER_ACTIVATION_KERNEL_WITH_COMPLEX(hard_shrink, HardShrinkKernel)
 PD_REGISTER_ACTIVATION_KERNEL(softshrink, SoftShrinkKernel)
 PD_REGISTER_ACTIVATION_KERNEL(tanh_shrink, TanhShrinkKernel)
 PD_REGISTER_ACTIVATION_KERNEL(elu, EluKernel)
