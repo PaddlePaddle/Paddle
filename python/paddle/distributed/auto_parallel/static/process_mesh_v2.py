48f25779087f5839f7eca145d37d9e26a7a2307c
Nyakku Shigure <sigure.qaq@gmail.com>
2023-08-16 01:42:26 +0800

    Update python/paddle/distributed/auto_parallel/static/process_mesh_v2.py

--- python/paddle/distributed/auto_parallel/static/process_mesh_v2.py
+++ python/paddle/distributed/auto_parallel/static/process_mesh_v2.py
@@ -39,6 +39,7 @@ class ProcessMesh(core.ProcessMesh):
 
             >>> mesh = dist.ProcessMesh([[2, 4, 5], [0, 1, 3]])
             >>> assert mesh.shape == [2, 3]
+            >>> assert mesh.process_ids == [2, 4, 5, 0, 1, 3]
 
     """
 
