5df8bf2f03c32812490f07695c721846b1f02a18
Aurelius84 <zhangliujie@baidu.com>
2023-08-15 08:34:59 +0000

    fix windows compile error

--- paddle/fluid/framework/new_executor/new_ir_interpreter.cc
+++ paddle/fluid/framework/new_executor/new_ir_interpreter.cc
@@ -36,7 +36,9 @@
 #include "paddle/fluid/platform/flags.h"
 #include "paddle/phi/backends/device_manager.h"
 
+#ifdef PADDLE_WITH_CINN
 #include "paddle/fluid/framework/new_executor/instruction/cinn_jit_instruction.h"
+#endif
 #include "paddle/fluid/framework/new_executor/instruction/legacy_kernel_instruction.h"
 #include "paddle/fluid/framework/new_executor/instruction/phi_kernel_instruction.h"
 #include "paddle/fluid/ir/phi_kernel_adaptor/phi_kernel_util.h"
@@ -462,9 +464,11 @@ void NewIRInterpreter::BuildInstruction() {
                                                    var_name_2_id_,
                                                    variable_2_var_name_));
       }
+#ifdef PADDLE_WITH_CINN
     } else if (op->dialect()->name() == "cinn") {
       vec_instruction_base_.emplace_back(
           std::make_unique<CinnJitInstruction>(op_idx++, place_, op, scope_));
+#endif
     } else {
       PADDLE_THROW(platform::errors::Unimplemented(
           "Now only support pd_kernel and cinn dialect."));
