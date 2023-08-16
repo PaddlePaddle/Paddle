ab5feb5aa76d816745af2052586aa47c53b46c93
Charles-hit <wanghao107@baidu.com>
2023-08-16 05:40:29 +0000

    support vjp prim mode in new ir

--- paddle/fluid/primitive/rule/vjp/static_utils.cc
+++ paddle/fluid/primitive/rule/vjp/static_utils.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,14 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#pragma once
+#include "paddle/fluid/primitive/rule/vjp/utils.h"
+#include "paddle/fluid/primitive/type/desc_tensor.h"
 
 namespace paddle {
-
 namespace primitive {
-
-namespace experimental {}
+template <>
+void set_output<DescTensor>(const paddle::Tensor& x_tmp, paddle::Tensor* x) {
+  x->set_impl(x_tmp.impl());
+}
 
 }  // namespace primitive
 }  // namespace paddle
