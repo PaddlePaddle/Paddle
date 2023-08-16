c0b331b4748165cd375d9b60afa8ee7fcdb0d847
huangjiyi <947613776@qq.com>
2023-08-16 20:08:17 +0800

    fix CI-Inference

--- cmake/inference_lib.cmake
+++ cmake/inference_lib.cmake
@@ -344,12 +344,15 @@ copy(
   DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/paddle/utils/)
 copy(
   inference_lib_dist
-  SRCS ${PADDLE_SOURCE_DIR}/paddle/utils/flags_native.h
-  DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/paddle/utils/)
-copy(
-  inference_lib_dist
   SRCS ${PADDLE_SOURCE_DIR}/paddle/extension.h
   DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/paddle/)
+
+if(NOT WITH_GFLAGS)
+  copy(
+    inference_lib_dist
+    SRCS ${PADDLE_SOURCE_DIR}/paddle/utils/flags_native.h
+    DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/paddle/utils/)
+endif()
 
 # the include path of phi needs to be changed to adapt to inference api path
 add_custom_command(
