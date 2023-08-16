838d06499a27305ac0b27d3600587ccd75f5885d
ylichen <ylichen@nvidia.com>
2023-08-16 02:35:45 -0700

    Correct the year in copyright

--- test/ir/inference/test_trt_convert_quantize_dequantize_linear.py
+++ test/ir/inference/test_trt_convert_quantize_dequantize_linear.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
 
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
