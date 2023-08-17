d1b6aace2107dd87a5eb12510de0ce0db7e34ba7
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-08-17 08:48:13 +0000

    change prim forward in New IR

--- python/paddle/decomposition/decomp.py
+++ python/paddle/decomposition/decomp.py
@@ -45,7 +45,7 @@ def _prepare_python_api_arguments(op):
     return tuple(api_arguments)
 
 
-def _check_op_results(op_name, orig_outs, new_outs):
+def _check_op_results(op_name, orig_outs, new_outs, orig_vars, dst_vars):
     """
     Check whether the replaced outputs are consistent with origin outputs.
 
@@ -53,6 +53,8 @@ def _check_op_results(op_name, orig_outs, new_outs):
     op_name (str): The name of operator.
     orig_outs (tuple): The outputs of original operator.
     new_outs (tuple): The outputs of replaced operator.
+    orig_vars (dict): Origin variables of original block.
+    dst_vars (list): Corresponding replaced variables of Origin variables.
     """
     assert len(orig_outs) == len(new_outs), (
         f'when replace origin op {op_name} with composite rule, num of origin outs should be equal to new outs, '
@@ -73,6 +75,8 @@ def _check_op_results(op_name, orig_outs, new_outs):
             # to keep same as phi op definition, orig_out may receive None
             continue
         elif new_out is not None:
+            if orig_out in orig_vars.keys():
+                dst_vars[orig_vars[orig_out]] = new_out
             orig_dtype = orig_out.dtype
             new_dtype = new_out.dtype
             orig_shape = orig_out.shape
@@ -96,6 +100,7 @@ def _check_op_results(op_name, orig_outs, new_outs):
 
 def decompose(
     program,
+    replaced_var,
     blacklist=frozenset(),
     whitelist=frozenset(),
 ):
@@ -110,8 +115,12 @@ def decompose(
 
     Args:
         program (Program): The program to be processed.
+        replaced_var (list[OpResult]): In program, once some operator is decomposed, its vars will be replaced by new ones. This argument means some vars will be used later and corresponding vars will be returned for later usage.
         blacklist (frozenset): The Operators that will be exclude when decomposed into primitives.
         whitelist (frozenset): Only the operators in whitelist will be decomposed into primitives.
+
+    Return:
+        new_var (list): A list contains all vars which replace origin ones in replaced_var.
     """
     if not isinstance(program, Program):
         raise TypeError(f"Expect type Program, but got type {type(program)}.")
@@ -140,25 +149,39 @@ def decompose(
         op_filter = lambda x: x.name() in whitelist
     else:
         op_filter = lambda x: True
+    dst_vars = [None] * len(replaced_var)
+    orig_vars = {}
+    for idx, item in enumerate(replaced_var):
+        orig_vars[item] = idx
     with ir.core.program_guard(program):
         _decompose_subgraph(
             block,
+            orig_vars,
+            dst_vars,
             op_filter,
         )
+    for item in dst_vars:
+        if not isinstance(item, ir.OpResult):
+            raise TypeError(
+                f"Each var in dst_vars should map corresponding var in replaced_var, but got type {type(item)} in {dst_vars}."
+            )
     logging.debug(
         "Decompose composite forward ops finish: {}".format(
             core.prim_config["composite_ops_record"]
         )
     )
+    return dst_vars
 
 
-def _decompose_subgraph(block, op_filter):
+def _decompose_subgraph(block, orig_vars, dst_vars, op_filter):
     """
     The operators in block wich satisfy the filter conditon will be decomposed into primitives.
 
     Args:
         block (Block|Sequence[Block]): The blocks of program to be processed.
         op_filter (function): The filter to specify which ops to be processed.
+        orig_vars (dict): Origin variables of original block.
+        dst_vars (list): Corresponding replaced variables of Origin variables.
     """
 
     if isinstance(block, Block):
@@ -176,7 +199,9 @@ def _decompose_subgraph(block, op_filter):
                 new_outs = _build_tensor_tuple(decom_rule(*input_args))
 
                 # Todo: To cover such case: some outputs are no longer needed after decomposition.
-                _check_op_results(op_name, orig_outs, new_outs)
+                _check_op_results(
+                    op_name, orig_outs, new_outs, orig_vars, dst_vars
+                )
 
                 op.replace_all_uses_with(new_outs)
                 block.remove_op(op)
@@ -184,7 +209,7 @@ def _decompose_subgraph(block, op_filter):
 
     elif isinstance(block, typing.Sequence):
         for item in block:
-            _decompose_subgraph(item, op_filter)
+            _decompose_subgraph(item, orig_vars, dst_vars, op_filter)
         return
     raise TypeError(
         f"Expect type Block or Sequence of Block, but got type {type(block)}"
