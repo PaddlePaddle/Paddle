6556c7641b23a0a59f45b12452bebff9fa59bc3d
enkilee <luhputu0815@gmail.com>
2023-08-17 06:29:21 +0000

    fix

--- paddle/phi/backends/cpu/cpu_context.h
+++ paddle/phi/backends/cpu/cpu_context.h
@@ -28,8 +28,8 @@ class PADDLE_API CPUContext : public DeviceContext,
                               public TypeInfoTraits<DeviceContext, CPUContext> {
  public:
   CPUContext();
-  CPUContext(CPUContext&&) noexcept = default;
-  CPUContext& operator=(CPUContext&&) noexcept = default;
+  CPUContext(CPUContext&&);
+  CPUContext& operator=(CPUContext&&);
   explicit CPUContext(const Place&);
   virtual ~CPUContext();
   Eigen::DefaultDevice* eigen_device() const;
--- paddle/phi/backends/gpu/gpu_context.h
+++ paddle/phi/backends/gpu/gpu_context.h
@@ -88,8 +88,8 @@ class PADDLE_API GPUContext : public DeviceContext,
                       bool init = true,
                       int stream_priority = 0);
 
-  GPUContext(GPUContext&&) noexcept;
-  GPUContext& operator=(GPUContext&&) noexcept;
+  GPUContext(GPUContext&&);
+  GPUContext& operator=(GPUContext&&);
 
   virtual ~GPUContext();
 
