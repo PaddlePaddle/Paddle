3981831d21b4245f3b5fe2ba43a75fc560511a5a
zhanglirong1999 <1695074375@qq.com>
2023-08-18 15:15:41 +0800

    add USE_OP_ITSELF

--- test/cpp/fluid/mkldnn/test_fc_mkldnn_different_shape.cc
+++ test/cpp/fluid/mkldnn/test_fc_mkldnn_different_shape.cc
@@ -18,9 +18,12 @@ limitations under the License. */
 #include <iostream>
 #include "paddle/fluid/framework/ir/pass_tester_helper.h"
 #include "paddle/fluid/framework/naive_executor.h"
-#include "paddle/fluid/framework/op_desc.h"
-#include "paddle/fluid/framework/program_desc.h"
+#include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/framework/scope.h"
+#include "paddle/phi/core/kernel_registry.h"
+
+USE_OP_ITSELF(fc)
+PD_DECLARE_KERNEL(fc, OneDNN, ONEDNN);
 
 paddle::framework::VarDesc *Data(
     paddle::framework::BlockDesc *block,
