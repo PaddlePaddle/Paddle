14d350c79466ad8af5da99b4468d83f90351e96d
Liyulingyue <852433440@qq.com>
2023-08-20 07:05:13 +0800

    enhance single int input in UpsamplingNearest2D and UpsamplingBilinear2D

--- python/paddle/nn/layer/common.py
+++ python/paddle/nn/layer/common.py
@@ -656,6 +656,8 @@ class UpsamplingNearest2D(Layer):
         self, size=None, scale_factor=None, data_format='NCHW', name=None
     ):
         super().__init__()
+        if isinstance(size, int):
+            size = [size, size]
         self.size = size
         self.scale_factor = scale_factor
         self.data_format = data_format
@@ -742,6 +744,8 @@ class UpsamplingBilinear2D(Layer):
         self, size=None, scale_factor=None, data_format='NCHW', name=None
     ):
         super().__init__()
+        if isinstance(size, int):
+            size = [size, size]
         self.size = size
         self.scale_factor = scale_factor
         self.data_format = data_format
