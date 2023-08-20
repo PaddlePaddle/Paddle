8bf987a717b57bb77936e2b3a4f9fb35070365a4
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-08-20 13:41:17 +0800

    codestyle

--- paddle/phi/kernels/gpu/activation_grad_kernel.cu
+++ paddle/phi/kernels/gpu/activation_grad_kernel.cu
@@ -402,7 +402,7 @@ PD_REGISTER_KERNEL(exp_grad,
 
 PD_REGISTER_ACTIVATION_GRAD_KERNEL(softshrink_grad, SoftShrinkGradKernel)
 PD_REGISTER_ACTIVATION_GRAD_KERNEL(hard_shrink_grad, HardShrinkGradKernel)
-PD_REGISTER_ACTIVATION_GRAD_KERNEL_WITH_COMPLEX(tanh_shrink_grad, 
+PD_REGISTER_ACTIVATION_GRAD_KERNEL_WITH_COMPLEX(tanh_shrink_grad,
                                                 TanhShrinkGradKernel)
 PD_REGISTER_ACTIVATION_GRAD_KERNEL(silu_grad, SiluGradKernel)
 PD_REGISTER_ACTIVATION_GRAD_KERNEL(elu_grad, EluGradKernel)
