3546dcaa608f22758c1228722c0ecc32daab450c
0x45f <wangzhen45@baidu.com>
2023-08-21 03:33:56 +0000

    Remove concat_grad

--- paddle/fluid/ir/dialect/pd_manual_api.cc
+++ paddle/fluid/ir/dialect/pd_manual_api.cc
@@ -19,18 +19,5 @@
 #include "paddle/ir/core/builtin_op.h"
 
 namespace paddle {
-namespace dialect {
-std::vector<ir::OpResult> concat_grad(std::vector<ir::OpResult> x,
-                                      ir::OpResult out_grad,
-                                      ir::OpResult axis) {
-  auto combine_op =
-      APIBuilder::Instance().GetBuilder()->Build<ir::CombineOp>(x);
-  paddle::dialect::ConcatGradOp concat_grad_op =
-      APIBuilder::Instance().GetBuilder()->Build<paddle::dialect::ConcatGradOp>(
-          combine_op.out(), out_grad, axis);
-  auto split_op = APIBuilder::Instance().GetBuilder()->Build<ir::SplitOp>(
-      concat_grad_op.result(0));
-  return split_op.outputs();
-}
-}  // namespace dialect
+namespace dialect {}  // namespace dialect
 }  // namespace paddle
