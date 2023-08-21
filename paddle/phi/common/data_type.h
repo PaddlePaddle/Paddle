0bfe911d90b599ca2cd7a584fa64c77c11199ee7
Wang Huan <wanghuan29@baidu.com>
2023-08-21 02:02:09 +0000

    refine

--- paddle/phi/common/data_type.h
+++ paddle/phi/common/data_type.h
@@ -253,7 +253,7 @@ inline std::string DataTypeToString(const DataType& dtype) {
   }
 }
 
-inline VarTypeToDataType(paddle::framework::proto::VarType_Type var_type) {
+inline VarTypeToDataType(::paddle::framework::proto::VarType_Type var_type) {
   swtich(var_type) {
     case paddle::framework::proto::VarType_Type::VarType_Type_BOOL:
       return DataType::BOOL;
