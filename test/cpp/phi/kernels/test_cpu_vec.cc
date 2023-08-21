b3698a7ad1b16a4e90bd2581a1df17c67aabaf2f
Chen Weihang <chenweihang@baidu.com>
2023-08-21 16:13:06 +0000

    fix vec compile error

--- test/cpp/phi/kernels/test_cpu_vec.cc
+++ test/cpp/phi/kernels/test_cpu_vec.cc
@@ -12,6 +12,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#define _BSD_SOURCE
+
+#include <sys/time.h>
+
 #include <cmath>
 #include <cstring>
 #include <random>
