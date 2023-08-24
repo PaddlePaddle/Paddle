7d6a6c147fd3e3c6e019110f54a99d29da69ab21
iLeGend <824040212@qq.com>
2023-08-24 19:25:20 +0800

    fix

--- test/legacy_test/test_logical_op.py
+++ test/legacy_test/test_logical_op.py
@@ -127,7 +127,9 @@ def np_data_generator(np_shape, dtype, *args, **kwargs):
         x = np.random.uniform(0.0, 1.0, np_shape).astype(np.float32)
         return convert_float_to_uint16(x)
     elif dtype == np.complex64 or dtype == np.complex128:
-        return np.random.randn(np_shape + [2]).view(dtype)
+        return np.random.normal(0, 1, np_shape).astype(
+            dtype
+        ) + 1.0j * np.random.normal(0, 1, np_shape).astype(dtype)
     else:
         return np.random.normal(0, 1, np_shape).astype(dtype)
 
