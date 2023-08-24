de3647326dd39fad3c8ea38e9b6bf5b0a9f424e6
phlrain <phliuhongyu@126.com>
2023-08-24 09:41:51 +0000

    fix batch norm grad args def

--- paddle/phi/kernels/gpu/batch_norm_grad_kernel.cu
+++ paddle/phi/kernels/gpu/batch_norm_grad_kernel.cu
@@ -1405,7 +1405,6 @@ PD_REGISTER_KERNEL(batch_norm_grad,
                    double,
                    phi::dtype::float16) {
   if (kernel_key.dtype() == phi::DataType::FLOAT16) {
-    kernel->OutputAt(0).SetDataType(phi::DataType::FLOAT32);  // x_grad
     kernel->OutputAt(1).SetDataType(phi::DataType::FLOAT32);  // scale_grad
     kernel->OutputAt(2).SetDataType(phi::DataType::FLOAT32);  // bias_grad
   }
