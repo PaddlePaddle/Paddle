94c6c17c690c88e4d7c013c190be036382d52178
enkilee <luhputu0815@gmail.com>
2023-08-24 05:35:09 +0000

    fix

--- paddle/phi/kernels/impl/ftrl_kernel_impl.h
+++ paddle/phi/kernels/impl/ftrl_kernel_impl.h
@@ -27,101 +27,9 @@ limitations under the License. */
 #include "paddle/phi/common/type_traits.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/ftrl_kernel.h"
-
 #include "paddle/phi/kernels/funcs/eigen/common.h"
 
 namespace phi {
-template <typename T,
-          int MajorType = Eigen::RowMajor,
-          typename IndexType = Eigen::DenseIndex>
-using EigenVector = phi::EigenVector<T, MajorType, IndexType>;
-
-template <typename T>
-class SparseFTRLFunctor {
- private:
-  const T* g_;
-  const T* p_;
-  const T* s_acc_;
-  const T* l_acc_;
-  const T* lr_;
-  const T l1_;
-  const T l2_;
-  const T lr_power_;
-  const int64_t* rows_;
-  const int64_t row_numel_;
-  T* p_out_;
-  T* s_acc_out_;
-  T* l_acc_out_;
-
- public:
-  SparseFTRLFunctor(const T* g,
-                    const T* p,
-                    const T* s_acc,
-                    const T* lr,
-                    const T l1,
-                    const T l2,
-                    const T lr_power,
-                    const int64_t* rows,
-                    int64_t row_numel,
-                    T* p_out,
-                    T* s_acc_out,
-                    T* l_acc_out)
-      : g_(g),
-        p_(p),
-        s_acc_(s_acc),
-        lr_(lr),
-        l1_(l1),
-        l2_(l2),
-        lr_power_(lr_power),
-        rows_(rows),
-        row_numel_(row_numel),
-        p_out_(p_out),
-        s_acc_out_(s_acc_out),
-        l_acc_out_(l_acc_out) {}
-
-  inline HOSTDEVICE void operator()(size_t i) {
-    auto j = rows_[i / row_numel_] * row_numel_ + i % row_numel_;
-    const T g = g_[i];
-    const T p = p_[j];
-    const T s_acc = s_acc_[j];
-    const T lr = lr_[0];
-
-    auto new_acc = s_acc + g * g;
-
-    if (lr_power_ == static_cast<T>(-0.5)) {
-      l_acc_out_[j] += g - (std::sqrt(new_acc) - std::sqrt(s_acc)) / lr * p;
-    } else {
-      l_acc_out_[j] +=
-          g - (std::pow(new_acc, -lr_power_) - std::pow(s_acc, -lr_power_)) /
-                  lr * p;
-    }
-
-    auto l_acc = l_acc_out_[j];
-
-    if (std::fabs(l_acc) > l1_) {
-      auto x = -l_acc;
-      if (l_acc >= static_cast<T>(0)) {
-        x += l1_;
-      } else {
-        x -= l1_;
-      }
-
-      auto y = static_cast<T>(2) * l2_;
-      if (lr_power_ == static_cast<T>(-0.5)) {
-        y += std::sqrt(new_acc) / lr;
-      } else {
-        y += std::pow(new_acc, -lr_power_) / lr;
-      }
-
-      auto pre_shrink = x / y;
-      p_out_[j] = pre_shrink;
-    } else {
-      p_out_[j] = static_cast<T>(0);
-    }
-
-    s_acc_out_[j] += g * g;
-  }
-};
 
 template <typename T, typename Context>
 void FTRLOpKernel(const Context& ctx,
@@ -186,5 +94,4 @@ void FTRLOpKernel(const Context& ctx,
   }
   s_acc_out.device(place) = sq_accum + g * g;
 }
-
 }  // namespace phi
