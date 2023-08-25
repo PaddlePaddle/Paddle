3483398c59238e871ba18fde4318eeb5cca5f9a1
JZ-LIANG <jianzhongliang10@gmail.com>
2023-08-25 20:38:48 +0800

    [Semi Auto] Matmul & Embedding InferBackward Rule  (#56257)
    * add embedding backward rule
    
    * update backward api
    
    * revert api
    
    * matmul inferbackward
    
    * update unitest

--- test/auto_parallel/spmd_rules/test_embedding_rule.py
+++ test/auto_parallel/spmd_rules/test_embedding_rule.py
@@ -26,6 +26,8 @@ class TestEmbeddingSPMDRule(unittest.TestCase):
     def setUp(self):
         self.rule1 = get_spmd_rule("lookup_table_v2")
 
+    def test_embedding_infer_forward(self):
+        # forward setup
         x_shape = [4, 1024]  # [B,S]
         table_shape = [512, 768]  # [V,H]
         process_mesh = auto.ProcessMesh(mesh=[[0, 1, 2, 3], [4, 5, 6, 7]])
@@ -45,7 +47,6 @@ class TestEmbeddingSPMDRule(unittest.TestCase):
             'sparse': False,
         }
 
-    def test_embedding_infer_forward(self):
         # data parallel
         self.x_dist_tensor_spec.set_dims_mapping([1, -1])
         self.table_dist_tensor_spec.set_dims_mapping([-1, -1])
@@ -88,6 +89,8 @@ class TestEmbeddingSPMDRule(unittest.TestCase):
         self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [1, -1])
         self.assertEqual(infered_input_dist_attrs[1].dims_mapping, [0, -1])
         self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [1, -1, -1])
+        self.assertEqual(infered_output_dist_attrs[0]._is_partial(), True)
+        self.assertEqual(infered_output_dist_attrs[0]._partial_dims(), {0})
 
         # table row-wise parallel & padding_idx
         self.x_dist_tensor_spec.set_dims_mapping([1, -1])
@@ -109,6 +112,89 @@ class TestEmbeddingSPMDRule(unittest.TestCase):
                 [self.x_dist_tensor_spec, self.table_dist_tensor_spec],
                 self.attrs,
             )
+
+    def test_embedding_infer_backward(self):
+        # backward setup
+        process_mesh = auto.ProcessMesh(mesh=[[0, 1, 2, 3], [4, 5, 6, 7]])
+
+        x_shape = [4, 1024]  # [B,S]
+        table_shape = [512, 768]  # [V,H]
+
+        x_tensor_dist_attr = TensorDistAttr()
+        x_tensor_dist_attr.process_mesh = (
+            process_mesh  # not set the dims mapping is ok.
+        )
+        self.x_dist_tensor_spec = DistTensorSpec(x_shape, x_tensor_dist_attr)
+
+        table_tensor_dist_attr = TensorDistAttr()
+        table_tensor_dist_attr.process_mesh = (
+            process_mesh  # not set the dims mapping is ok.
+        )
+        self.table_dist_tensor_spec = DistTensorSpec(
+            table_shape, table_tensor_dist_attr
+        )
+
+        out_shape = [4, 1024, 768]  # [B,S, H]
+        out_tensor_dist_attr = TensorDistAttr()
+        out_tensor_dist_attr.process_mesh = process_mesh
+        self.out_dist_tensor_spec = DistTensorSpec(
+            out_shape, out_tensor_dist_attr
+        )
+
+        self.attrs = {
+            'padding_idx': -1,
+            'sparse': False,
+        }
+
+        # data parallel
+        self.out_dist_tensor_spec.set_dims_mapping([1, -1, -1])
+        result_dist_attrs = self.rule1.infer_backward(
+            [self.x_dist_tensor_spec, self.table_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            self.attrs,
+        )
+        infered_input_dist_attrs = result_dist_attrs[0]
+        infered_output_dist_attrs = result_dist_attrs[1]
+
+        self.assertEqual(len(result_dist_attrs), 2)
+        self.assertEqual(len(infered_input_dist_attrs), 2)
+        self.assertEqual(len(infered_output_dist_attrs), 1)
+
+        self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [1, -1])
+        self.assertEqual(infered_input_dist_attrs[1].dims_mapping, [-1, -1])
+        self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [1, -1, -1])
+
+        # table col-wise parallel & dp
+        self.out_dist_tensor_spec.set_dims_mapping([-1, 0, 1])
+        result_dist_attrs = self.rule1.infer_backward(
+            [self.x_dist_tensor_spec, self.table_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            self.attrs,
+        )
+        infered_input_dist_attrs = result_dist_attrs[0]
+        infered_output_dist_attrs = result_dist_attrs[1]
+
+        self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [-1, 0])
+        self.assertEqual(infered_input_dist_attrs[1].dims_mapping, [-1, 1])
+        self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [-1, 0, 1])
+
+        # sharded on multiple broadcast axes
+        self.out_dist_tensor_spec.set_dims_mapping([1, 0, -1])
+
+        result_dist_attrs = self.rule1.infer_backward(
+            [self.x_dist_tensor_spec, self.table_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            self.attrs,
+        )
+        infered_input_dist_attrs = result_dist_attrs[0]
+        infered_output_dist_attrs = result_dist_attrs[1]
+
+        self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [1, 0])
+        self.assertEqual(infered_input_dist_attrs[1].dims_mapping, [-1, -1])
+        self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [1, 0, -1])
+
+        # table row-wise parallel
+        # skiped
 
 
 if __name__ == "__main__":
