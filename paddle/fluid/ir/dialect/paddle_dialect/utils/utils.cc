990a608814bb9a23b512699daff756eefcdd28d1
zhaoyingli <zhaoyingli@baidu.com>
2023-08-25 16:50:28 +0800

    fix format

--- paddle/fluid/ir/dialect/paddle_dialect/utils/utils.cc
+++ paddle/fluid/ir/dialect/paddle_dialect/utils/utils.cc
@@ -17,15 +17,16 @@
 namespace paddle {
 namespace dialect {
 
-const std::unordered_set<std::string> LegacyOpList = {"pd.load_combine",
-                                                      "pd.c_concat",
-                                                      "pd.c_broadcast_",
-                                                      "pd.fused_bn_add_activation_",
-                                                      "pd.fused_bn_add_activation_grad",
-                                                      "pd.c_sync_calc_stream_",
-                                                      "pd.c_sync_comm_stream_",
-                                                      "pd.send_v2",
-                                                      "pd.recv_v2"};
+const std::unordered_set<std::string> LegacyOpList = {
+    "pd.load_combine",
+    "pd.c_concat",
+    "pd.c_broadcast_",
+    "pd.fused_bn_add_activation_",
+    "pd.fused_bn_add_activation_grad",
+    "pd.c_sync_calc_stream_",
+    "pd.c_sync_comm_stream_",
+    "pd.send_v2",
+    "pd.recv_v2"};
 
 enum class AttrType {
   UNDEFINED = 0,
