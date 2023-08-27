452b1c6c4f4184be40f3fd0c439291cc24a9a659
megemini <megemini@outlook.com>
2023-08-27 14:52:41 +0800

    [Change] test timeout code

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -294,7 +294,9 @@ def linspace(start, stop, num, dtype=None, name=None):
     Examples:
         .. code-block:: python
 
-                >>> # doctest: +TIMEOUT(0.000001)
+                >>> # doctest: +TIMEOUT(0.1)
+                >>> import time
+                >>> time.sleep(2)
                 >>> import paddle
                 >>> data = paddle.linspace(0, 10, 5, 'float32') # [0.0,  2.5,  5.0,  7.5, 10.0]
                 >>> data = paddle.linspace(0, 10, 1, 'float32') # [0.0]
