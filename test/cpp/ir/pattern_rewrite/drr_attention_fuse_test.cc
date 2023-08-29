df6f824524e614a5bb6bc2965081c14e7ef43888
yuanlehome <yuanlehome@163.com>
2023-08-29 09:31:26 +0000

    add multihead_matmul fuse pattern

--- test/cpp/ir/pattern_rewrite/drr_attention_fuse_test.cc
+++ test/cpp/ir/pattern_rewrite/drr_attention_fuse_test.cc
@@ -27,64 +27,155 @@ class MultiHeadMatmulFusePattern
     : public ir::drr::DrrPatternBase<MultiHeadMatmulFusePattern> {
  public:
   void operator()(ir::drr::DrrPatternContext *ctx) const override {
+    //
+    // Source Pattern.
+    //
     ir::drr::SourcePattern src = ctx->SourcePattern();
     // The first path to matmul with scale (q).
-    const auto &matmul_1 = src.Op("pd.matmul");
+    const auto &matmul_1 =
+        src.Op("pd.matmul",
+               {{"transpose_x", src.Attr("matmul_1_transpose_x")},
+                {"transpose_y", src.Attr("matmul_1_transpose_y")}});
     src.Tensor("matmul_1_out") =
         matmul_1(src.Tensor("matmul_1_in_1"), src.Tensor("matmul_1_in_2"));
     const auto &add_1 = src.Op("pd.add");
     src.Tensor("add_1_out") =
         add_1(src.Tensor("matmul_1_out"), src.Tensor("add_1_in_2"));
+    const auto &full_int_array_1 = src.Op(
+        "pd.full_int_array", {{"value", src.Attr("full_int_array_1_value")}});
     const auto &reshape_1 = src.Op("pd.reshape");
-    src.Tensor("reshape_1_out") = reshape_1(src.Tensor("add_1_out"));
+    reshape_1({&src.Tensor("add_1_out"), &full_int_array_1()},
+              {&src.Tensor("reshape_1_out"), &src.Tensor("reshape_1_xshape")});
     const auto &transpose_1 = src.Op("pd.transpose");
     src.Tensor("transpose_1_out") = transpose_1(src.Tensor("reshape_1_out"));
     const auto &scale = src.Op("pd.scale");
     src.Tensor("scale_out") = scale(src.Tensor("transpose_1_out"));
 
     // The second path to matmul (k).
-    const auto &matmul_2 = src.Op("pd.matmul");
+    const auto &matmul_2 =
+        src.Op("pd.matmul",
+               {{"transpose_x", src.Attr("matmul_2_transpose_x")},
+                {"transpose_y", src.Attr("matmul_2_transpose_y")}});
     src.Tensor("matmul_2_out") =
         matmul_2(src.Tensor("matmul_1_in_1"), src.Tensor("matmul_2_in_2"));
     const auto &add_2 = src.Op("pd.add");
     src.Tensor("add_2_out") =
         add_2(src.Tensor("matmul_2_out"), src.Tensor("add_2_in_2"));
+    const auto &full_int_array_2 = src.Op("pd.full_int_array");
     const auto &reshape_2 = src.Op("pd.reshape");
-    src.Tensor("reshape_2_out") = reshape_2(src.Tensor("add_2_out"));
+    reshape_2({&src.Tensor("add_2_out"), &full_int_array_2()},
+              {&src.Tensor("reshape_2_out"), &src.Tensor("reshape_2_xshape")});
     const auto &transpose_2 = src.Op("pd.transpose");
     src.Tensor("transpose_2_out") = transpose_2(src.Tensor("reshape_2_out"));
 
     // The third path to matmul (v).
-    const auto &matmul_3 = src.Op("pd.matmul");
+    const auto &matmul_3 =
+        src.Op("pd.matmul",
+               {{"transpose_x", src.Attr("matmul_3_transpose_x")},
+                {"transpose_y", src.Attr("matmul_3_transpose_y")}});
     src.Tensor("matmul_3_out") =
         matmul_3(src.Tensor("matmul_1_in_1"), src.Tensor("matmul_3_in_2"));
     const auto &add_3 = src.Op("pd.add");
     src.Tensor("add_3_out") =
         add_3(src.Tensor("matmul_3_out"), src.Tensor("add_3_in_2"));
+    const auto &full_int_array_3 = src.Op("pd.full_int_array");
     const auto &reshape_3 = src.Op("pd.reshape");
-    src.Tensor("reshape_3_out") = reshape_3(src.Tensor("add_3_out"));
+    reshape_3({&src.Tensor("add_3_out"), &full_int_array_3()},
+              {&src.Tensor("reshape_3_out"), &src.Tensor("reshape_3_xshape")});
     const auto &transpose_3 = src.Op("pd.transpose");
     src.Tensor("transpose_3_out") = transpose_3(src.Tensor("reshape_3_out"));
 
     // softmax(qk)v + matmul
-    const auto &matmul_4 = src.Op("pd.matmul");
+    const auto &matmul_4 =
+        src.Op("pd.matmul",
+               {{"transpose_x", src.Attr("matmul_4_transpose_x")},
+                {"transpose_y", src.Attr("matmul_4_transpose_y")}});
     src.Tensor("matmul_4_out") =
         matmul_4(src.Tensor("scale_out"), src.Tensor("transpose_2_out"));
-    const auto &softmax = src.Op("pd.softmax");
+    const auto &softmax =
+        src.Op("pd.softmax", {{"axis", src.Attr("softmax_axis")}});
     src.Tensor("softmax_out") = softmax(src.Tensor("matmul_4_out"));
-    const auto &matmul_5 = src.Op("pd.matmul");
+    const auto &matmul_5 =
+        src.Op("pd.matmul",
+               {{"transpose_x", src.Attr("matmul_5_transpose_x")},
+                {"transpose_y", src.Attr("matmul_5_transpose_y")}});
     src.Tensor("matmul_5_out") =
         matmul_4(src.Tensor("softmax_out"), src.Tensor("transpose_3_out"));
     const auto &transpose_4 = src.Op("pd.transpose");
     src.Tensor("transpose_4_out") = transpose_4(src.Tensor("matmul_5_out"));
+    const auto &full_int_array_4 = src.Op("pd.full_int_array");
     const auto &reshape_4 = src.Op("pd.reshape");
-    src.Tensor("reshape_4_out") = reshape_4(src.Tensor("transpose_4_out"));
-    const auto &matmul_6 = src.Op("pd.matmul");
+    reshape_4({&src.Tensor("transpose_4_out"), &full_int_array_4()},
+              {&src.Tensor("reshape_4_out"), &src.Tensor("reshape_4_xshape")});
+    const auto &matmul_6 =
+        src.Op("pd.matmul",
+               {{"transpose_x", src.Attr("matmul_6_transpose_x")},
+                {"transpose_y", src.Attr("matmul_6_transpose_y")}});
     src.Tensor("matmul_6_out") =
         matmul_6(src.Tensor("reshape_4_out"), src.Tensor("matmul_6_in_2"));
     const auto &add_4 = src.Op("pd.add");
     src.Tensor("add_4_out") =
         add_4(src.Tensor("matmul_6_out"), src.Tensor("add_4_in_2"));
+
+    //
+    // Constraints.
+    //
+    src.RequireNativeCall([](const ir::drr::MatchContext &match_ctx) -> bool {
+      const auto &softmax_axis = match_ctx.Attr<int>("softmax_axis");
+      if (softmax_axis == -1 || softmax_axis == 3) return false;
+
+      const auto &matmul_1_transpose_x =
+          match_ctx.Attr<int>("matmul_1_transpose_x");
+      const auto &matmul_1_transpose_y =
+          match_ctx.Attr<int>("matmul_1_transpose_y");
+      if (matmul_1_transpose_x || matmul_1_transpose_y) retrun false;
+
+      const auto &matmul_2_transpose_x =
+          match_ctx.Attr<int>("matmul_2_transpose_x");
+      const auto &matmul_2_transpose_y =
+          match_ctx.Attr<int>("matmul_2_transpose_y");
+      if (matmul_2_transpose_x || matmul_2_transpose_y) retrun false;
+
+      const auto &matmul_3_transpose_x =
+          match_ctx.Attr<int>("matmul_3_transpose_x");
+      const auto &matmul_3_transpose_y =
+          match_ctx.Attr<int>("matmul_3_transpose_y");
+      if (matmul_3_transpose_x || matmul_3_transpose_y) retrun false;
+
+      const auto &matmul_4_transpose_x =
+          match_ctx.Attr<int>("matmul_4_transpose_x");
+      const auto &matmul_4_transpose_y =
+          match_ctx.Attr<int>("matmul_4_transpose_y");
+      if (matmul_4_transpose_x || !matmul_4_transpose_y) retrun false;
+
+      const auto &matmul_5_transpose_x =
+          match_ctx.Attr<int>("matmul_5_transpose_x");
+      const auto &matmul_5_transpose_y =
+          match_ctx.Attr<int>("matmul_5_transpose_y");
+      if (matmul_5_transpose_x || matmul_5_transpose_y) retrun false;
+
+      const auto &matmul_6_transpose_x =
+          match_ctx.Attr<int>("matmul_6_transpose_x");
+      const auto &matmul_6_transpose_y =
+          match_ctx.Attr<int>("matmul_6_transpose_y");
+      if (matmul_6_transpose_x || matmul_6_transpose_y) retrun false;
+
+      return true;
+    });
+
+    //
+    // Result Pattern.
+    //
+    ir::drr::ResultPattern res = src.ResultPattern();
+    const auto &head_number =
+        res.Attr([](const ir::drr::MatchContext &match_ctx) -> int {
+          const auto &full_int_array_1_value =
+              match_ctx.Attr<std::vector<int64_t>>("full_int_array_1_value");
+          return full_int_array_1_value.at(2);
+        });
+    const auto &multihead_matmul =
+        res.Op("pd.multihead_matmul", {{"head_number", head_number}});
+    res.Tensor("add_4_out") = multihead_matmul(res.Tensor("matmul_1_in_1"));
   }
 };
 
