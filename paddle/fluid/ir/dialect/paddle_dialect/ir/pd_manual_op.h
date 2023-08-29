0fcae1104e0e5d46b5a3ab1483601a2728a0775c
Wang Huan <wanghuan29@baidu.com>
2023-08-29 06:49:52 +0000

    refine

--- paddle/fluid/ir/dialect/paddle_dialect/ir/pd_manual_op.h
+++ paddle/fluid/ir/dialect/paddle_dialect/ir/pd_manual_op.h
@@ -121,5 +121,7 @@ class SplitGradOp : public ir::Op<SplitGradOp, OpYamlInfoInterface> {
 
 IR_DECLARE_EXPLICIT_TYPE_ID(paddle::dialect::AddNOp)
 IR_DECLARE_EXPLICIT_TYPE_ID(paddle::dialect::SplitGradOp)
+IR_DECLARE_EXPLICIT_TYPE_ID(paddle::dialect::AddN_Op)
+IR_DECLARE_EXPLICIT_TYPE_ID(paddle::dialect::AddNWithKernelOp)
 
 #endif
