d2bf2a80da628ce14a4d4380ecbdeddc91b25776
yuchen202 <103028470+yuchen202@users.noreply.github.com>
2023-08-30 15:35:47 +0800

    ... for i in range(2):
    将...改为>>>

--- python/paddle/nn/functional/common.py
+++ python/paddle/nn/functional/common.py
@@ -1257,7 +1257,7 @@ def dropout2d(x, p=0.5, training=True, data_format='NCHW', name=None):
             >>> x = paddle.randn(shape=(2, 3, 4, 5)).astype(paddle.float32)
             >>> y_train = paddle.nn.functional.dropout2d(x)  #train
             >>> y_test = paddle.nn.functional.dropout2d(x, training=False) #test
-            ... for i in range(2):
+            >>> for i in range(2):
             ...     for j in range(3):
             ...         print(x[i,j,:,:])
             ...         print(y_train[i,j,:,:]) # may all 0
