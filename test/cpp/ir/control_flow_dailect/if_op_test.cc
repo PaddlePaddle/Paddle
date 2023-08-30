fafd0da2cdcbf4cf83a2fd29a5b2bcf5a692bf86
winter-wang <1030748926@qq.com>
2023-08-30 09:17:32 +0000

    test

---
+++ test/cpp/ir/control_flow_dailect/if_op_test.cc
@@ -0,0 +1,34 @@
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <gtest/gtest.h>
+
+#include "paddle/fluid/ir/dialect/paddle_dialect/ir/pd_dialect.h"
+#include "paddle/fluid/ir/dialect/paddle_dialect/ir/pd_manual_op.h"
+#include "paddle/fluid/ir/dialect/paddle_dialect/ir/pd_op.h"
+#include "paddle/ir/core/builder.h"
+#include "paddle/ir/core/program.h"
+
+TEST(if_op_test, base) {
+  ir::IrContext ctx;
+  ctx.GetOrRegisterDialect<paddle::dialect::PaddleDialect>();
+
+  ir::Program program(&ctx);
+  ir::Block* block = program.block();
+
+  ir::Builder builder(&ctx, block);
+  auto data_op = builder.Build<paddle::dialect::DataOp>(ir::AttributeMap{});
+  builder.Build<paddle::dialect::IfOp>(data_op.out(), std::vector<ir::Type>{});
+  std::stringstream ss;
+  program.Print(ss);
+}
