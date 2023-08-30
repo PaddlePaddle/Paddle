2ef4ec71117f77bfe2021fbbf624e617098f3abd
huangjiyi <43315610+huangjiyi@users.noreply.github.com>
2023-08-30 16:58:24 +0800

    Add paddle custom flags support (#56256)
    * update
    
    * repalce gflags header
    
    * replace DEFINE_<type> with PD_DEFINE_<type>
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * update cmake
    
    * add :: before some paddle namespace
    
    * fix link error
    
    * fix CI-Py3
    
    * allow commandline parse
    
    * fix SetFlagsFromEnv
    
    * fix bug
    
    * fix bug
    
    * fix CI-CINN
    
    * fix CI-Coverage-build
    
    * fix CI-Windows-build
    
    * fix CI-Inference
    
    * fix bug
    
    * fix bug
    
    * fix CI-CINN
    
    * fix inference api test
    
    * fix infer_ut test
    
    * revert infer_ut gflags usage
    
    * update
    
    * fix inference
    
    * remove flags export macro
    
    * revert inference demo_ci gflags usage
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix bug when turn on WITH_GFLAGS
    
    * turn on WITH_GFLAGS
    
    * fix bug when turn on WITH_GFLAGS
    
    * fix bug when turn on WITH_GFLAGS
    
    * update
    
    * update and add unittest
    
    * add unittest
    
    * fix conflict
    
    * rerun ci
    
    * update
    
    * resolve conflict

--- paddle/phi/infermeta/unary.cc
+++ paddle/phi/infermeta/unary.cc
@@ -17,7 +17,6 @@ limitations under the License. */
 #include <algorithm>
 #include <set>
 
-#include "gflags/gflags.h"
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/common/type_traits.h"
 #include "paddle/phi/core/enforce.h"
@@ -31,6 +30,7 @@ limitations under the License. */
 #include "paddle/phi/kernels/funcs/unfold_functor.h"
 #include "paddle/phi/kernels/funcs/unsqueeze.h"
 #include "paddle/phi/kernels/impl/einsum_impl.h"
+#include "paddle/utils/flags.h"
 
 namespace phi {
 
