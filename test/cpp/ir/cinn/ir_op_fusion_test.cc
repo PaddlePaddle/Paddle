d5e434ecfba16e24ac09d398d3e69dda8346217e
phlrain <phliuhongyu@126.com>
2023-08-31 02:08:38 +0000

    fix header bug

--- test/cpp/ir/cinn/ir_op_fusion_test.cc
+++ test/cpp/ir/cinn/ir_op_fusion_test.cc
@@ -17,7 +17,7 @@
 #include <sstream>
 
 #include "paddle/cinn/hlir/dialect/cinn_dialect/ir/cinn_dialect.h"
-#include "paddle/cinn/hlir/dialect/cinn_op.h"
+#include "paddle/cinn/hlir/dialect/cinn_dialect/ir/cinn_op.h"
 #include "paddle/fluid/ir/dialect/paddle_dialect/ir/pd_dialect.h"
 #include "paddle/fluid/ir/dialect/paddle_dialect/ir/pd_op.h"
 #include "paddle/ir/core/ir_context.h"
