5c44f2e8854f33f041edf4a546a83761a605d77b
vivienfanghua <vivienfanghua@163.com>
2023-09-04 09:30:23 +0000

    delete rename ops output when normalize_program

--- python/paddle/static/io.py
+++ python/paddle/static/io.py
@@ -189,15 +189,16 @@ def normalize_program(program, feed_vars, fetch_vars):
     # fix the bug that the activation op's output as target will be pruned.
     # will affect the inference performance.
     # TODO(Superjomn) add an IR pass to remove 1-scale op.
-    with program_guard(program):
-        uniq_fetch_vars = []
-        for i, var in enumerate(fetch_vars):
-            if var.dtype != paddle.bool:
-                var = layers.scale(var,
-                                   1.,
-                                   name="save_infer_model/scale_{}".format(i))
-            uniq_fetch_vars.append(var)
-        fetch_vars = uniq_fetch_vars
+    # with program_guard(program):
+    #     uniq_fetch_vars = []
+    #     for i, var in enumerate(fetch_vars):
+    #         if var.dtype != paddle.bool:
+    #             var = layers.scale(var,
+    #                                1.,
+    #                                name="save_infer_model/scale_{}".format(i))
+    #         uniq_fetch_vars.append(var)
+    #     fetch_vars = uniq_fetch_vars
+    # TODO(vivienfanghuagood) maybe these codes is not needed 
 
     # serialize program
     copy_program = program.clone()
