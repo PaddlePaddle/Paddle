18599bb5ab05d6de4d9b9f09b8f0017fa30ba22c
6clc <chaoliu.lc@foxmail.com>
2023-09-05 11:26:03 +0800

    test=cinnunit

--- tools/cinn/build.sh
+++ tools/cinn/build.sh
@@ -85,7 +85,9 @@ function prepare_ci {
   python${py_version} -m pip install -U --no-cache-dir pip
   python${py_version} -m pip install wheel
   python${py_version} -m pip install sphinx==3.3.1 sphinx_gallery==0.8.1 recommonmark==0.6.0 exhale scipy breathe==4.24.0 matplotlib sphinx_rtd_theme
+  set -x
   python${py_version} -m pip install paddlepaddle-gpu==0.0.0.post118 -f https://www.paddlepaddle.org.cn/whl/linux/gpu/develop.html
+  set +x
 }
 
 
