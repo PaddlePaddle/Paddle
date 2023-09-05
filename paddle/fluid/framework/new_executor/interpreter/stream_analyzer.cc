a851eff31740053db3652e7e34bc8e324aba0147
chenruibiao <chenruibiao@baidu.com>
2023-09-05 09:52:51 +0000

    Fix event info bug in StreamAnalyzer

--- paddle/fluid/framework/new_executor/interpreter/stream_analyzer.cc
+++ paddle/fluid/framework/new_executor/interpreter/stream_analyzer.cc
@@ -346,7 +346,6 @@ void analyse_event_info_for_two_instructions<Instruction>(
 
   if (has_data_dependency<Instruction, std::string>(
           instructions[cur_instr_id], instructions[next_instr_id]) ||
-      !run_type_info[next_instr_id][DownstreamRunType::kEventRun].empty() ||
       instructions[next_instr_id]->OpBase()->Type() == "depend") {
     waiter_instr_ids->insert(next_instr_id);
     return;
@@ -406,7 +405,6 @@ void analyse_event_info_for_two_instructions<
 
   if (has_data_dependency<paddle::framework::InstructionBase, ir::Value>(
           instructions[cur_instr_id], instructions[next_instr_id]) ||
-      !run_type_info[next_instr_id][DownstreamRunType::kEventRun].empty() ||
       instructions[next_instr_id]->Name() == "pd.depend") {
     waiter_instr_ids->insert(next_instr_id);
     return;
