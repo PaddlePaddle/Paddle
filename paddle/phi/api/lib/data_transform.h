d2fedeac0a35e4df470fced3e19029edd8f13bf2
Ghost Screaming <mofengshenjieII@163.com>
2023-09-05 14:32:57 +0800

    [Auto Parallel]: Support std::vector<phi::Tensor> input and output for DistTensor. (#56602)
    * [WIP] Support std::vector<phi::Tensor> input and output for DistTensor.
    Concat forward and backward are verified.
    
    * Polish code for new dist tensor implementation.
    
    * Fix bug of DistTensor upgrade. Add support functions for std::vector<Tensor> -> std::vector<Tensor>.
    
    * Add support for DistTensor type of std::vector<phi::Tensor> as input or output of operators.
    Following testcases are passed.
    1. concat: std::vector<phi::Tensor> -> phi::Tensor
    2. unbind: phi::Tensor -> std::vector<phi::Tensor>
    3. broadcast_tensors: std::vector<phi::Tensor> -> std::vector<phi::Tensor>
    
    * Polish code. Remove useless comments.
    
    * Add update_loss_scaling in skip_op_lists.
    
    * Polish code.

--- paddle/phi/api/lib/data_transform.h
+++ paddle/phi/api/lib/data_transform.h
@@ -180,5 +180,11 @@ std::shared_ptr<phi::distributed::DistTensor> PrepareDataForDistTensor(
     const TransformFlag& transform_flag,
     bool is_stride_kernel);
 
+std::vector<std::shared_ptr<phi::distributed::DistTensor>>
+PrepareDataForDistTensor(const std::vector<Tensor>& input,
+                         const phi::TensorArgDef& target_args_def,
+                         const TransformFlag& transform_flag,
+                         bool is_stride_kernel);
+
 }  // namespace experimental
 }  // namespace paddle
