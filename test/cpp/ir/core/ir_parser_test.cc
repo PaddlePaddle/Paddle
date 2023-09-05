923d961b14ac7e93750e86f5a53554416a6c5e92
xingmingyyj <135400902+xingmingyyj@users.noreply.github.com>
2023-09-05 14:34:35 +0800

    Update test/cpp/ir/core/ir_parser_test.cc
    Co-authored-by: kangguangli <kangguangli@hotmail.com>

--- test/cpp/ir/core/ir_parser_test.cc
+++ test/cpp/ir/core/ir_parser_test.cc
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <fstream>
+#include <iostream>
+
 #include <gtest/gtest.h>
 #include <fstream>
 #include <iostream>
