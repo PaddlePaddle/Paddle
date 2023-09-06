50dc548bc2009ac3de05483c88cf8773593b152f
Charles-hit <56987902+Charles-hit@users.noreply.github.com>
2023-09-06 18:47:34 +0800

    Revert "support for checking op's inputs grad semantic (#56925)"
    This reverts commit d8122a23d5068d21f74c11eb4bb8e0a5b779d567.

--- paddle/fluid/ir/dialect/paddle_dialect/utils/op_yaml_info_util.h
+++ paddle/fluid/ir/dialect/paddle_dialect/utils/op_yaml_info_util.h
@@ -27,7 +27,6 @@ struct OpInputInfo {
   bool optional = false;
   bool no_need_buffer = false;
   bool is_mutable_attribute = false;
-  bool with_grad_semantic = true;
   OpInputInfo() = default;
   OpInputInfo(const OpInputInfo& input_info) = default;
 
@@ -35,14 +34,12 @@ struct OpInputInfo {
               const std::string& type_name,
               bool optional,
               bool no_need_buffer,
-              bool is_mutable_attribute,
-              bool with_grad_semantic)
+              bool is_mutable_attribute)
       : name(name),
         type_name(type_name),
         optional(optional),
         no_need_buffer(no_need_buffer),
-        is_mutable_attribute(is_mutable_attribute),
-        with_grad_semantic(with_grad_semantic) {}
+        is_mutable_attribute(is_mutable_attribute) {}
 };
 
 struct OpOutputInfo {
