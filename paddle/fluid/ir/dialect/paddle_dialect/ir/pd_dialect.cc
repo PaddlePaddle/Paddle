56532823fd705f246b7cf8172339283e520adbff
zyfncg <zhangyunfei07@baidu.com>
2023-09-06 11:15:30 +0000

    add FusedGemmEpilogueOp in new ir

--- paddle/fluid/ir/dialect/paddle_dialect/ir/pd_dialect.cc
+++ paddle/fluid/ir/dialect/paddle_dialect/ir/pd_dialect.cc
@@ -51,6 +51,8 @@ void PaddleDialect::initialize() {
   RegisterOps<paddle::dialect::AddNOp,
               paddle::dialect::AddN_Op,
               paddle::dialect::AddNWithKernelOp,
+              paddle::dialect::FusedGemmEpilogueOp,
+              paddle::dialect::FusedGemmEpilogueGradOp,
               paddle::dialect::SplitGradOp>();
 
   RegisterInterfaces<ParameterConvertInterface>();
