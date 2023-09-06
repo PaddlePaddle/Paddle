749ebce00edd94f3dd8da10c29008d3fda366bff
zyfncg <zhangyunfei07@baidu.com>
2023-09-06 09:22:18 +0000

    update reorder_block_ops_pass

--- paddle/ir/transforms/reorder_block_ops_pass.cc
+++ paddle/ir/transforms/reorder_block_ops_pass.cc
@@ -30,63 +30,67 @@ class ReorderBlockOpsPass : public ir::Pass {
   ReorderBlockOpsPass() : ir::Pass("ReorderBlockOpsPass", 0) {}
 
   void Run(ir::Operation *op) override {
-    auto module_op = op->dyn_cast<ir::ModuleOp>();
-    IR_ENFORCE(module_op, "ReorderBlockOpsPass should run on module op.");
-    auto *block = module_op.block();
-    std::list<ir::Operation *> res_op_list;
-    std::unordered_map<ir::Operation *, int>
-        reorder_op_dep_cnt;  // op -> dependent input count
-    std::unordered_set<ir::Value> visited_values;
-    std::queue<ir::Operation *> op_que;
+    IR_ENFORCE(op->num_regions() > 0,
+               "ReorderBlockOpsPass should run on Operation which regions "
+               "number greater than 0.");
+    for (size_t i = 0; i < op->num_regions(); ++i) {
+      for (auto *block : op->region(i)) {
+        std::list<ir::Operation *> res_op_list;
+        std::unordered_map<ir::Operation *, int>
+            reorder_op_dep_cnt;  // op -> dependent input count
+        std::unordered_set<ir::Value> visited_values;
+        std::queue<ir::Operation *> op_que;
 
-    auto update_op_que = [&](ir::Operation *op) {
-      for (size_t i = 0; i < op->results().size(); ++i) {
-        auto result = op->result(i);
-        visited_values.insert(result);
-        for (auto it = result.use_begin(); it != result.use_end(); ++it) {
-          if (reorder_op_dep_cnt.count(it->owner())) {
-            reorder_op_dep_cnt[it->owner()]--;
-            if (reorder_op_dep_cnt[it->owner()] == 0) {
-              op_que.push(it->owner());
+        auto update_op_que = [&](ir::Operation *op) {
+          for (size_t i = 0; i < op->results().size(); ++i) {
+            auto result = op->result(i);
+            visited_values.insert(result);
+            for (auto it = result.use_begin(); it != result.use_end(); ++it) {
+              if (reorder_op_dep_cnt.count(it->owner())) {
+                reorder_op_dep_cnt[it->owner()]--;
+                if (reorder_op_dep_cnt[it->owner()] == 0) {
+                  op_que.push(it->owner());
+                }
+              }
             }
           }
-        }
-      }
-    };
+        };
 
-    for (auto &op : *block) {
-      bool has_dependency = false;
-      if (op->num_operands() > 0) {
-        for (size_t i = 0; i < op->num_operands(); ++i) {
-          auto operand = op->operand_source(i);
-          if (operand && visited_values.count(op->operand_source(i)) == 0) {
-            reorder_op_dep_cnt[op]++;
-            has_dependency = true;
+        for (auto &op : *block) {
+          bool has_dependency = false;
+          if (op->num_operands() > 0) {
+            for (size_t i = 0; i < op->num_operands(); ++i) {
+              auto operand = op->operand_source(i);
+              if (operand && visited_values.count(op->operand_source(i)) == 0) {
+                reorder_op_dep_cnt[op]++;
+                has_dependency = true;
+              }
+            }
+          }
+          if (!has_dependency) {
+            res_op_list.push_back(op);
+            update_op_que(op);
           }
         }
-      }
-      if (!has_dependency) {
-        res_op_list.push_back(op);
-        update_op_que(op);
-      }
-    }
 
-    if (reorder_op_dep_cnt.empty()) {
-      return;
-    }
+        if (reorder_op_dep_cnt.empty()) {
+          return;
+        }
 
-    while (!op_que.empty()) {
-      auto *op = op_que.front();
-      op_que.pop();
-      res_op_list.push_back(op);
-      update_op_que(op);
+        while (!op_que.empty()) {
+          auto *op = op_que.front();
+          op_que.pop();
+          res_op_list.push_back(op);
+          update_op_que(op);
+        }
+        VLOG(4) << "ReorderBlockOpsPass is applied.";
+        block->ResetOpListOrder(res_op_list);
+      }
     }
-    VLOG(4) << "ReorderBlockOpsPass is applied.";
-    block->ResetOpListOrder(res_op_list);
   }
 
   bool CanApplyOn(ir::Operation *op) const override {
-    return op->name() == "builtin.module" && op->num_regions() > 0;
+    return op->num_regions() > 0;
   }
 };
 
