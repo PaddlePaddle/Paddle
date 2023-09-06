e458a64b28222f63db7924dab71bc477a3c25174
0x45f <wangzhen45@baidu.com>
2023-09-06 07:13:41 +0000

    Call _C_ops.full in new ir

--- python/paddle/_C_ops.py
+++ python/paddle/_C_ops.py
@@ -16,7 +16,7 @@ from paddle.fluid import core
 
 __all__ = []
 
-UNIFIED_APIS = ['mean']
+UNIFIED_APIS = ['mean', 'full', 'full_']
 
 for name in dir(core.eager.ops):
     globals()[name] = getattr(core.eager.ops, name)
