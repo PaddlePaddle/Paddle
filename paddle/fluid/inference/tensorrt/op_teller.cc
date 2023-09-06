2e9c209b0be3e05a62d628c1f208a5460e16d162
zhoutianzi666 <17801055074@163.com>
2023-09-06 22:47:42 +0800

    delete Trt below 8.6 not support conv2d's fil

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -387,11 +387,11 @@ struct SimpleOpTypeSetTeller : public Teller {
         auto* filter_var_desc = block->FindVar(desc.Input("Filter")[0]);
         if (!filter_var_desc->Persistable()) {
 #if IS_TRT_VERSION_GE(8600)
-#else
-          LOG(INFO)
-              << "Trt below 8.6 not support conv2d's filter is a intermedoate "
-                 "tensor in conv2d op, please upgarde your TenroRT.";
-          return false;
+#else     // for explict quantization 
+          // LOG(INFO)
+          //     << "Trt below 8.6 not support conv2d's filter is a intermedoate "
+          //        "tensor in conv2d op, please upgarde your TenroRT.";
+          // return false;
 #endif
         }
       }
