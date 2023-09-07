efa02005676d76ffb5caca0e41a6e1a457435b5f
megemini <megemini@outlook.com>
2023-09-07 12:54:22 +0800

    trigger ci

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -103,7 +103,6 @@ def create_global_var(
             >>> import paddle
             >>> print(1+1)
             2
-
     """
     # TODO:
     check_type(shape, 'shape', (list, tuple, np.ndarray), 'create_global_var')
