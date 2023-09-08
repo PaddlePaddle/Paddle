a7a1304a5f478c05aa86bcc5e3dc86afcea26e3d
yangguohao <1901212980@pku.edu.cn>
2023-09-08 13:26:07 +0000

    enable dy2stat newIR_3

--- test/dygraph_to_static/test_ptb_lm_v2.py
+++ test/dygraph_to_static/test_ptb_lm_v2.py
@@ -17,6 +17,7 @@ import time
 import unittest
 
 import numpy as np
+from dygraph_to_static_util import test_and_compare_with_new_ir
 
 import paddle
 
@@ -317,6 +318,7 @@ def train_dygraph(place):
     return train(place)
 
 
+@test_and_compare_with_new_ir(True)
 def train_static(place):
     paddle.jit.enable_to_static(True)
     return train(place)
