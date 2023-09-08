ef74e008dc820e5d7ac8508cbe742d731d2b8be2
xingmingyyj <zxm_3791@163.com>
2023-09-08 01:26:16 +0000

    add parser

--- paddle/fluid/ir/dialect/paddle_dialect/ir/pd_dialect.h
+++ paddle/fluid/ir/dialect/paddle_dialect/ir/pd_dialect.h
@@ -25,8 +25,8 @@ class PaddleDialect : public ir::Dialect {
 
   static const char* name() { return "pd"; }
 
-  ir::Type ParseType(ir::IrParser& parser);            // NOLINT
-  ir::Attribute ParseAttribute(ir::IrParser& parser);  // NOLINT
+  ir::Type ParseType(ir::IrParser& parser) override;            // NOLINT
+  ir::Attribute ParseAttribute(ir::IrParser& parser) override;  // NOLINT
 
   void PrintType(ir::Type type, std::ostream& os) const override;
   void PrintAttribute(ir::Attribute type, std::ostream& os) const override;
