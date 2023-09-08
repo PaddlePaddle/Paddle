cfffe9c8699769011f3d557b5166ce020ca27a5a
Charles-hit <56987902+Charles-hit@users.noreply.github.com>
2023-09-09 03:19:24 +0800

    support optional and invoke (#57084)

--- paddle/fluid/primitive/utils/static_utils.cc
+++ paddle/fluid/primitive/utils/static_utils.cc
@@ -21,5 +21,23 @@ void set_output<LazyTensor>(const paddle::Tensor& x_tmp, paddle::Tensor* x) {
   x->set_impl(x_tmp.impl());
 }
 
+std::vector<std::vector<Tensor>> ConstructVjpResultByStopGradients(
+    const std::vector<std::vector<Tensor>>& outputs,
+    const std::vector<std::vector<bool>>& stop_gradients) {
+  std::vector<std::vector<Tensor>> vjp_results(outputs.size());
+  for (size_t i = 0; i < outputs.size(); ++i) {
+    vjp_results[i].reserve(outputs[i].size());
+    for (size_t j = 0; j < outputs[i].size(); ++j) {
+      if (stop_gradients[i][j]) {
+        // Use Tensor's impl is nullptr to indicate it has no gradient
+        vjp_results[i].emplace_back(Tensor());
+      } else {
+        vjp_results[i].emplace_back(outputs[i][j]);
+      }
+    }
+  }
+  return vjp_results;
+}
+
 }  // namespace primitive
 }  // namespace paddle
