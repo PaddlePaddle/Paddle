6644ca8ed282e6c18a747b15ed9c9393f318d8d3
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-09-10 00:20:36 +0800

    Apply suggestions from code review

--- paddle/fluid/pybind/place.cc
+++ paddle/fluid/pybind/place.cc
@@ -215,7 +215,6 @@ void BindPlace(pybind11::module &m) {  // NOLINT
 
           >>> import paddle
           >>> fake_cpu_place = paddle.CustomPlace("FakeCPU", 0)
-          ...
 )DOC");
   g_customplace_pytype = reinterpret_cast<PyTypeObject *>(customplace.ptr());
   customplace
