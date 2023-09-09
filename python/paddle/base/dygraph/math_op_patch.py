81aaf083473eab068417f3251333115485773d9c
yoyoIcy <yoyosummer66@gmail.com>
2023-09-09 15:05:54 +0800

    [Doctest]fix No.334, test=docs_preview

--- python/paddle/base/dygraph/math_op_patch.py
+++ python/paddle/base/dygraph/math_op_patch.py
@@ -85,13 +85,13 @@ def monkey_patch_math_tensor():
         Examples:
             .. code-block:: python
 
-                import paddle
-                import numpy as np
+                >>> import paddle
+                >>> import numpy as np
 
-                original_tensor = paddle.ones([2, 2])
-                print("original tensor's dtype is: {}".format(original_tensor.dtype))
-                new_tensor = original_tensor.astype('float32')
-                print("new tensor's dtype is: {}".format(new_tensor.dtype))
+                >>> original_tensor = paddle.ones([2, 2])
+                >>> print("original tensor's dtype is: {}".format(original_tensor.dtype))
+                >>> new_tensor = original_tensor.astype('float32')
+                >>> print("new tensor's dtype is: {}".format(new_tensor.dtype))
 
         """
         if not isinstance(dtype, core.VarDesc.VarType):
