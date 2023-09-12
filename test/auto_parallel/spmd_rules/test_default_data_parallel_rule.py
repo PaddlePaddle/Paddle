29900a0b494849617120bf1453cfc40f1ac8ec1d
JZ-LIANG <jianzhongliang10@gmail.com>
2023-09-12 14:14:48 +0800

    [Semi Auto] Refactor Replicated Rule (#56839)
    * adapt general spmd rule
    
    * polish details
    
    * add new rules
    
    * bugfix for set_partial
    
    * bugfix
    
    * unitest
    
    * adapt for argument for tensor and vector of tensor
    
    ---------
    
    Co-authored-by: Chen Weihang <chenweihang@baidu.com>

--- test/auto_parallel/spmd_rules/test_default_data_parallel_rule.py
+++ test/auto_parallel/spmd_rules/test_default_data_parallel_rule.py
@@ -22,11 +22,11 @@ from paddle.distributed.fleet import auto
 from paddle.framework import core
 
 
-class TestReplicatedSPMDRule(unittest.TestCase):
+class TestDefaultDataParallelSPMDRule(unittest.TestCase):
     def setUp(self):
         # After replaced all spmd rules by phi impl, we can recover the
         # api name to `get_spmd_rule`
-        self.rule = core.get_phi_spmd_rule("replicated")
+        self.rule = core.get_phi_spmd_rule("unsqueeze")
 
         x_shape = [10, 10, 32, 48]
         y_shape = [32, 48]
@@ -45,19 +45,20 @@ class TestReplicatedSPMDRule(unittest.TestCase):
         self.y_dist_tensor_spec = DistTensorSpec(y_shape, y_tensor_dist_attr)
 
         out1_tensor_dist_attr = TensorDistAttr()
-        # out1_tensor_dist_attr.dims_mapping = [-1, 1, 0, -1] // unset on purpose, inferforward only need shape infer of output
-        # out1_tensor_dist_attr.process_mesh = process_mesh
+        out1_tensor_dist_attr.dims_mapping = [-1, 1, -1, -1]
+        out1_tensor_dist_attr.process_mesh = process_mesh
         self.out1_dist_tensor_spec = DistTensorSpec(
             out1_shape, out1_tensor_dist_attr
         )
         out2_tensor_dist_attr = TensorDistAttr()
+        out1_tensor_dist_attr.dims_mapping = [-1, 1, -1]
+        out1_tensor_dist_attr.process_mesh = process_mesh
         self.out2_dist_tensor_spec = DistTensorSpec(
             out2_shape, out2_tensor_dist_attr
         )
 
-    def test_replicated_infer_forward(self):
-        # return all -1
-        # 2 inputs 2 outputs
+    def test_default_dp_infer_forward(self):
+        # 2 inputs 2 outputs, sharded batch axis
         in_vec = [self.x_dist_tensor_spec, self.y_dist_tensor_spec]
         out_vec = [self.out1_dist_tensor_spec, self.out2_dist_tensor_spec]
         result_dist_attrs = self.rule.infer_forward(
@@ -69,58 +70,19 @@ class TestReplicatedSPMDRule(unittest.TestCase):
         self.assertEqual(len(result_dist_attrs[0]), 2)
         self.assertEqual(len(result_dist_attrs[1]), 2)
 
-        self.assertEqual(result_dist_attrs[0][0].dims_mapping, [-1, -1, -1, -1])
-        self.assertEqual(result_dist_attrs[0][1].dims_mapping, [-1, -1])
-        self.assertEqual(result_dist_attrs[1][0].dims_mapping, [-1, -1, -1, -1])
-        self.assertEqual(result_dist_attrs[1][1].dims_mapping, [-1, -1, -1])
+        self.assertEqual(result_dist_attrs[0][0].dims_mapping, [0, -1, -1, -1])
+        self.assertEqual(result_dist_attrs[0][1].dims_mapping, [0, -1])
+        self.assertEqual(result_dist_attrs[1][0].dims_mapping, [0, -1, -1, -1])
+        self.assertEqual(result_dist_attrs[1][1].dims_mapping, [0, -1, -1])
 
-        # 1 inputs 2 outputs
-        in_vec = [self.y_dist_tensor_spec]
-        out_vec = [self.out1_dist_tensor_spec, self.out2_dist_tensor_spec]
-        result_dist_attrs = self.rule.infer_forward(
-            [(0, len(in_vec)), (len(in_vec), len(in_vec) + len(out_vec))],
-            in_vec + out_vec,
-            [],
-        )
-        self.assertEqual(len(result_dist_attrs), 2)
-        self.assertEqual(len(result_dist_attrs[0]), 1)
-        self.assertEqual(len(result_dist_attrs[1]), 2)
-
-        self.assertEqual(result_dist_attrs[0][0].dims_mapping, [-1, -1])
-        self.assertEqual(result_dist_attrs[1][0].dims_mapping, [-1, -1, -1, -1])
-        self.assertEqual(result_dist_attrs[1][1].dims_mapping, [-1, -1, -1])
-
-    def test_replicated_infer_backward(self):
-        process_mesh = auto.ProcessMesh(mesh=[[0, 1, 2], [3, 4, 5]])
-        self.out1_dist_tensor_spec.set_dims_mapping([-1, 1, 0, -1])
-        self.out1_dist_tensor_spec.set_process_mesh(process_mesh)
-        self.out2_dist_tensor_spec.set_dims_mapping([1, -1, 0])
-        self.out2_dist_tensor_spec.set_process_mesh(process_mesh)
-
-        in_vec = [self.x_dist_tensor_spec, self.y_dist_tensor_spec]
-        out_vec = [self.out1_dist_tensor_spec, self.out2_dist_tensor_spec]
-        result_dist_attrs = self.rule.infer_backward(
-            [(0, len(in_vec)), (len(in_vec), len(in_vec) + len(out_vec))],
-            in_vec + out_vec,
-            [],
-        )
-        self.assertEqual(len(result_dist_attrs), 2)
-        self.assertEqual(len(result_dist_attrs[0]), 2)
-        self.assertEqual(len(result_dist_attrs[1]), 2)
-
-        self.assertEqual(result_dist_attrs[0][0].dims_mapping, [-1, -1, -1, -1])
-        self.assertEqual(result_dist_attrs[0][1].dims_mapping, [-1, -1])
-        self.assertEqual(result_dist_attrs[1][0].dims_mapping, [-1, -1, -1, -1])
-        self.assertEqual(result_dist_attrs[1][1].dims_mapping, [-1, -1, -1])
-
-        # 1 inputs 3 outputs
-        in_vec = [self.y_dist_tensor_spec]
+        # 1 inputs 3 outputs, unsharded batch axis
+        in_vec = [self.x_dist_tensor_spec]
         out_vec = [
-            self.x_dist_tensor_spec,
+            self.y_dist_tensor_spec,
             self.out1_dist_tensor_spec,
             self.out2_dist_tensor_spec,
         ]
-        result_dist_attrs = self.rule.infer_backward(
+        result_dist_attrs = self.rule.infer_forward(
             [(0, len(in_vec)), (len(in_vec), len(in_vec) + len(out_vec))],
             in_vec + out_vec,
             [],
@@ -129,10 +91,65 @@ class TestReplicatedSPMDRule(unittest.TestCase):
         self.assertEqual(len(result_dist_attrs[0]), 1)
         self.assertEqual(len(result_dist_attrs[1]), 3)
 
-        self.assertEqual(result_dist_attrs[0][0].dims_mapping, [-1, -1])
-        self.assertEqual(result_dist_attrs[1][0].dims_mapping, [-1, -1, -1, -1])
+        self.assertEqual(result_dist_attrs[0][0].dims_mapping, [-1, -1, -1, -1])
+        self.assertEqual(result_dist_attrs[1][0].dims_mapping, [-1, -1])
         self.assertEqual(result_dist_attrs[1][1].dims_mapping, [-1, -1, -1, -1])
         self.assertEqual(result_dist_attrs[1][2].dims_mapping, [-1, -1, -1])
+
+        # conflict
+        self.x_dist_tensor_spec.set_dims_mapping([0, -1, -1, -1])
+        self.y_dist_tensor_spec.set_dims_mapping([1, -1, -1, -1])
+
+        in_vec = [self.x_dist_tensor_spec, self.y_dist_tensor_spec]
+        out_vec = [self.out1_dist_tensor_spec, self.out2_dist_tensor_spec]
+        with self.assertRaises(NotImplementedError):
+            result_dist_attrs = self.rule.infer_forward(
+                [(0, len(in_vec)), (len(in_vec), len(in_vec) + len(out_vec))],
+                in_vec + out_vec,
+                [],
+            )
+
+    def test_default_dp_infer_backward(self):
+        # replicated out1 from [-1, 0, 1, -1]  to [0, -1, -1, -1]
+        # replicated out1 from [0, 1, -1]  to [0, -1, -1]
+        self.out1_dist_tensor_spec.set_dims_mapping([-1, 0, 1, -1])
+        self.out2_dist_tensor_spec.set_dims_mapping([0, 1, -1])
+
+        in_vec = [self.x_dist_tensor_spec, self.y_dist_tensor_spec]
+        out_vec = [self.out1_dist_tensor_spec, self.out2_dist_tensor_spec]
+        result_dist_attrs = self.rule.infer_backward(
+            [(0, len(in_vec)), (len(in_vec), len(in_vec) + len(out_vec))],
+            in_vec + out_vec,
+            [],
+        )
+        self.assertEqual(len(result_dist_attrs), 2)
+        self.assertEqual(len(result_dist_attrs[0]), 2)
+        self.assertEqual(len(result_dist_attrs[1]), 2)
+
+        self.assertEqual(result_dist_attrs[0][0].dims_mapping, [0, -1, -1, -1])
+        self.assertEqual(result_dist_attrs[0][1].dims_mapping, [0, -1])
+        self.assertEqual(result_dist_attrs[1][0].dims_mapping, [0, -1, -1, -1])
+        self.assertEqual(result_dist_attrs[1][1].dims_mapping, [0, -1, -1])
+
+        # batch axis unsharded
+        self.out1_dist_tensor_spec.set_dims_mapping([-1, 0, 1, -1])
+        self.out2_dist_tensor_spec.set_dims_mapping([-1, 1, -1])
+
+        in_vec = [self.x_dist_tensor_spec, self.y_dist_tensor_spec]
+        out_vec = [self.out1_dist_tensor_spec, self.out2_dist_tensor_spec]
+        result_dist_attrs = self.rule.infer_backward(
+            [(0, len(in_vec)), (len(in_vec), len(in_vec) + len(out_vec))],
+            in_vec + out_vec,
+            [],
+        )
+        self.assertEqual(len(result_dist_attrs), 2)
+        self.assertEqual(len(result_dist_attrs[0]), 2)
+        self.assertEqual(len(result_dist_attrs[1]), 2)
+
+        self.assertEqual(result_dist_attrs[0][0].dims_mapping, [-1, -1, -1, -1])
+        self.assertEqual(result_dist_attrs[0][1].dims_mapping, [-1, -1])
+        self.assertEqual(result_dist_attrs[1][0].dims_mapping, [-1, -1, -1, -1])
+        self.assertEqual(result_dist_attrs[1][1].dims_mapping, [-1, -1, -1])
 
 
 if __name__ == "__main__":
