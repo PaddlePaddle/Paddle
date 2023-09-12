8a521e11d1a41a867911d6031f9ca470bb8f2224
Yichen Zhang <zhangyichen03@baidu.com>
2023-09-12 20:45:24 +0800

    add partial status in infer_backward unit test

--- test/auto_parallel/spmd_rules/test_reduction_rule.py
+++ test/auto_parallel/spmd_rules/test_reduction_rule.py
@@ -43,7 +43,6 @@ class TestReductionSPMDRule(unittest.TestCase):
 
         self.attrs = OrderedDict([('axis', [0]), ('keep_dim', False)])
 
-    """
     def test_single_mesh_dim(self):
         # reduce on dim 0, keep_dim = false
         # [0, -1] --> [0, -1], [-1], partial_on_dim:[0]
@@ -142,7 +141,6 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [-1, -1])
         self.assertEqual(infered_output_dist_attrs[0]._is_partial(), True)
         self.assertEqual(infered_output_dist_attrs[0]._partial_dims(), {0})
-    """
 
     def test_multi_mesh_dim(self):
         process_mesh = auto.ProcessMesh(mesh=[[0, 1, 2], [3, 4, 5]])
@@ -166,75 +164,75 @@ class TestReductionSPMDRule(unittest.TestCase):
 
         self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [0, -1, -1])
         self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [0])
+        self.assertEqual(infered_output_dist_attrs[0]._is_partial(), False)
 
         # reduce on dim 1, 2, keep_dim = false
         # [-1, 0, 1] --> [-1, 0, 1], [-1], partial_on_dim:[0, 1]
-        # self.attrs['keep_dim'] = False
-        # self.attrs['axis'] = [1, 2]
-        # self.x_dist_tensor_spec.set_dims_mapping([-1, 0, 1])
-        # result_dist_attrs = self.rule.infer_forward(
-        #     [self.x_dist_tensor_spec], list(self.attrs.values())
-        # )
-        # infered_input_dist_attrs = result_dist_attrs[0]
-        # infered_output_dist_attrs = result_dist_attrs[1]
+        self.attrs['keep_dim'] = False
+        self.attrs['axis'] = [1, 2]
+        self.x_dist_tensor_spec.set_dims_mapping([-1, 0, 1])
+        result_dist_attrs = self.rule.infer_forward(
+            [self.x_dist_tensor_spec], list(self.attrs.values())
+        )
+        infered_input_dist_attrs = result_dist_attrs[0]
+        infered_output_dist_attrs = result_dist_attrs[1]
 
-        # self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [-1, 0, 1])
-        # self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [-1])
+        self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [-1, 0, 1])
+        self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [-1])
 
-        # self.assertEqual(infered_output_dist_attrs[0]._is_partial(), True)
-        # self.assertEqual(infered_output_dist_attrs[0]._partial_dims(), {0, 1})
-        # infered_output_dist_attrs[0]._clean_partial_status()
-        # self.assertEqual(infered_output_dist_attrs[0]._is_partial(), False)
-        # # reduction on dim 1, 2, keep_dim = false
-        # # [1, -1, -1] --> [1, -1, -1], [1], partial_on_dim:[]
-        # self.attrs['keep_dim'] = False
-        # self.attrs['axis'] = [1, 2]
-        # self.x_dist_tensor_spec.set_dims_mapping([1, -1, -1])
-        # result_dist_attrs = self.rule.infer_forward(
-        #     [self.x_dist_tensor_spec], list(self.attrs.values())
-        # )
-        # infered_input_dist_attrs = result_dist_attrs[0]
-        # infered_output_dist_attrs = result_dist_attrs[1]
+        self.assertEqual(infered_output_dist_attrs[0]._is_partial(), True)
+        self.assertEqual(infered_output_dist_attrs[0]._partial_dims(), {0, 1})
+        infered_output_dist_attrs[0]._clean_partial_status()
+        self.assertEqual(infered_output_dist_attrs[0]._is_partial(), False)
+        # reduction on dim 1, 2, keep_dim = false
+        # [1, -1, -1] --> [1, -1, -1], [1], partial_on_dim:[]
+        self.attrs['keep_dim'] = False
+        self.attrs['axis'] = [1, 2]
+        self.x_dist_tensor_spec.set_dims_mapping([1, -1, -1])
+        result_dist_attrs = self.rule.infer_forward(
+            [self.x_dist_tensor_spec], list(self.attrs.values())
+        )
+        infered_input_dist_attrs = result_dist_attrs[0]
+        infered_output_dist_attrs = result_dist_attrs[1]
 
-        # self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [1, -1, -1])
-        # self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [1])
-        # self.assertEqual(infered_output_dist_attrs[0]._is_partial(), False)
+        self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [1, -1, -1])
+        self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [1])
+        self.assertEqual(infered_output_dist_attrs[0]._is_partial(), False)
 
-        # # reduction on dim 1, 2, keep_dim = false
-        # # [0, 1, -1] --> [0, 1, -1], [0], partial_on_dim:[1]
-        # self.attrs['keep_dim'] = False
-        # self.attrs['axis'] = [1, 2]
-        # self.x_dist_tensor_spec.set_dims_mapping([0, 1, -1])
-        # result_dist_attrs = self.rule.infer_forward(
-        #     [self.x_dist_tensor_spec], list(self.attrs.values())
-        # )
-        # infered_input_dist_attrs = result_dist_attrs[0]
-        # infered_output_dist_attrs = result_dist_attrs[1]
+        # reduction on dim 1, 2, keep_dim = false
+        # [0, 1, -1] --> [0, 1, -1], [0], partial_on_dim:[1]
+        self.attrs['keep_dim'] = False
+        self.attrs['axis'] = [1, 2]
+        self.x_dist_tensor_spec.set_dims_mapping([0, 1, -1])
+        result_dist_attrs = self.rule.infer_forward(
+            [self.x_dist_tensor_spec], list(self.attrs.values())
+        )
+        infered_input_dist_attrs = result_dist_attrs[0]
+        infered_output_dist_attrs = result_dist_attrs[1]
 
-        # self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [0, 1, -1])
-        # self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [0])
-        # self.assertEqual(infered_output_dist_attrs[0]._is_partial(), True)
-        # self.assertEqual(infered_output_dist_attrs[0]._partial_dims(), {1})
-        # infered_output_dist_attrs[0]._clean_partial_status()
-        # self.assertEqual(infered_output_dist_attrs[0]._is_partial(), False)
+        self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [0, 1, -1])
+        self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [0])
+        self.assertEqual(infered_output_dist_attrs[0]._is_partial(), True)
+        self.assertEqual(infered_output_dist_attrs[0]._partial_dims(), {1})
+        infered_output_dist_attrs[0]._clean_partial_status()
+        self.assertEqual(infered_output_dist_attrs[0]._is_partial(), False)
 
-        # # reduction on dim 1, 2, keep_dim = true
-        # # [0, 1, -1] --> [0, 1, -1], [0, -1, -1], partial_on_dim:[1]
-        # self.attrs['keep_dim'] = True
-        # self.attrs['axis'] = [1, 2]
-        # self.x_dist_tensor_spec.set_dims_mapping([0, 1, -1])
-        # result_dist_attrs = self.rule.infer_forward(
-        #     [self.x_dist_tensor_spec], list(self.attrs.values())
-        # )
-        # infered_input_dist_attrs = result_dist_attrs[0]
-        # infered_output_dist_attrs = result_dist_attrs[1]
+        # reduction on dim 1, 2, keep_dim = true
+        # [0, 1, -1] --> [0, 1, -1], [0, -1, -1], partial_on_dim:[1]
+        self.attrs['keep_dim'] = True
+        self.attrs['axis'] = [1, 2]
+        self.x_dist_tensor_spec.set_dims_mapping([0, 1, -1])
+        result_dist_attrs = self.rule.infer_forward(
+            [self.x_dist_tensor_spec], list(self.attrs.values())
+        )
+        infered_input_dist_attrs = result_dist_attrs[0]
+        infered_output_dist_attrs = result_dist_attrs[1]
 
-        # self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [0, 1, -1])
-        # self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [0, -1, -1])
-        # self.assertEqual(infered_output_dist_attrs[0]._is_partial(), True)
-        # self.assertEqual(infered_output_dist_attrs[0]._partial_dims(), {1})
+        self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [0, 1, -1])
+        self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [0, -1, -1])
+        self.assertEqual(infered_output_dist_attrs[0]._is_partial(), True)
+        self.assertEqual(infered_output_dist_attrs[0]._partial_dims(), {1})
 
-    """
     def test_backward_single_mesh_dim(self):
         # reduce on dim 0, keep_dim = false
         # [-1] --> [-1, -1], [-1] (output --> input, output)
@@ -243,7 +241,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [32]
         self.out_dist_tensor_spec.set_dims_mapping([-1])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -262,7 +262,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [1, 32]
         self.out_dist_tensor_spec.set_dims_mapping([-1, -1])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -277,7 +279,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [64]
         self.out_dist_tensor_spec.set_dims_mapping([0])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -292,7 +296,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [64, 1]
         self.out_dist_tensor_spec.set_dims_mapping([0, -1])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -307,7 +313,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = []
         self.out_dist_tensor_spec.set_dims_mapping([])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -322,7 +330,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [1, 1]
         self.out_dist_tensor_spec.set_dims_mapping([-1, -1])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -343,7 +353,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [96]
         self.out_dist_tensor_spec.set_dims_mapping([0])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -362,7 +374,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [96]
         self.out_dist_tensor_spec.set_dims_mapping([-1])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -377,7 +391,9 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [96]
         self.out_dist_tensor_spec.set_dims_mapping([1])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
@@ -392,14 +408,46 @@ class TestReductionSPMDRule(unittest.TestCase):
         self.out_dist_tensor_spec.shape = [96, 1, 1]
         self.out_dist_tensor_spec.set_dims_mapping([0, -1, -1])
         result_dist_attrs = self.rule.infer_backward(
-            [self.x_dist_tensor_spec], [self.out_dist_tensor_spec], list(self.attrs.values())
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
         )
         infered_input_dist_attrs = result_dist_attrs[0]
         infered_output_dist_attrs = result_dist_attrs[1]
 
         self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [0, -1, -1])
         self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [0, -1, -1])
-    """
+
+    def test_backward_multi_mesh_dim_parital(self):
+        # reduction on dim 1, 2, keep_dim = true, partial_dim=[1]
+        # [0, -1, -1] --> [0, -1, -1], [0, -1, -1] (output --> input, output)
+        # output parital_dim: [1], input parital_dim: []
+        out_shape = [96, 1, 1]
+        process_mesh = auto.ProcessMesh(mesh=[[0, 1, 2], [3, 4, 5]])
+
+        self.x_dist_tensor_spec.set_process_mesh(process_mesh)
+        self.x_dist_tensor_spec.shape = [96, 24, 48]
+        out_tensor_dist_attr = TensorDistAttr()
+        out_tensor_dist_attr.dims_mapping = [0, -1, -1]
+        out_tensor_dist_attr.process_mesh = process_mesh
+        out_tensor_dist_attr._set_partial_dims([1])
+        self.out_dist_tensor_spec = DistTensorSpec(
+            out_shape, out_tensor_dist_attr
+        )
+
+        self.attrs['keep_dim'] = True
+        self.attrs['axis'] = [1, 2]
+        result_dist_attrs = self.rule.infer_backward(
+            [self.x_dist_tensor_spec],
+            [self.out_dist_tensor_spec],
+            list(self.attrs.values()),
+        )
+        infered_input_dist_attrs = result_dist_attrs[0]
+        infered_output_dist_attrs = result_dist_attrs[1]
+
+        self.assertEqual(infered_input_dist_attrs[0].dims_mapping, [0, -1, -1])
+        self.assertEqual(infered_output_dist_attrs[0].dims_mapping, [0, -1, -1])
+        self.assertEqual(infered_input_dist_attrs[0]._is_partial(), False)
 
 
 if __name__ == "__main__":
