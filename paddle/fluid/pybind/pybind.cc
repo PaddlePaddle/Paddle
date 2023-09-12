4d92b3a3c14e806db1409de0925bf6e80d081c1d
Ryan <44900829+DrRyanHuang@users.noreply.github.com>
2023-09-13 00:07:11 +0800

    xdoc

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -122,7 +122,7 @@ limitations under the License. */
 #include "paddle/fluid/pybind/imperative.h"
 #include "paddle/fluid/pybind/inference_api.h"
 #include "paddle/fluid/pybind/io.h"
-#include "paddle/fluid/pybind/ir.h"
+#include "paddle/fluid/pybind/pir.h"
 #include "paddle/fluid/pybind/jit.h"
 #include "paddle/fluid/pybind/metrics_py.h"
 #include "paddle/fluid/pybind/ps_gpu_wrapper_py.h"
