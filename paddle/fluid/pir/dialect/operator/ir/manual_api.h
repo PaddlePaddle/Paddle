ebfeae859b68d28d0095b49f3c72c2f4cabaddaa
wangruting <wangruting@baidu.com>
2023-09-12 08:43:04 +0000

    fix conflict

--- paddle/fluid/pir/dialect/operator/ir/manual_api.h
+++ paddle/fluid/pir/dialect/operator/ir/manual_api.h
@@ -24,14 +24,10 @@ namespace paddle {
 namespace dialect {
 pir::OpResult builtin_combine(std::vector<pir::OpResult> x);
 
-pir::OpResult split_grad(std::vector<pir::OpResult> out_grads,
-                         pir::OpResult axis);
-
-pir::OpResult split_grad(std::vector<pir::OpResult> out_grads, int axis);
-
 pir::OpResult get_parameter(const std::string& name,
                             phi::DataType dtype,
                             const std::vector<int64_t>& shape);
+
 void set_parameter(pir::OpResult parameter, const std::string& name);
 }  // namespace dialect
 }  // namespace paddle
