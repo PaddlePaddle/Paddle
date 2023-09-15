442f83c1d0e8bc208839b569ea42462768c2dba6
xiongkun <xiongkun03@baidu.com>
2023-09-15 05:33:49 +0000

    fix

--- python/paddle/jit/dy2static/newir_partial_program.py
+++ python/paddle/jit/dy2static/newir_partial_program.py
@@ -131,7 +131,7 @@ class ProgramInfo:
         if key not in self.programs:
             infer_prog = prog_creator(is_infer_mode=True)
             self.programs[key] = infer_prog
-            self.op_size[key] = infer_prog.desc.block(0).op_size()
+            self.op_size[key] = infer_prog.desc.global_block().op_size()
 
         return self.programs[key], self.op_size[key]
 
@@ -542,7 +542,7 @@ class PartialProgramLayer:
             whenever we call this method, a tmp Program() object is created and is gc immediatly
             after executed the following line in PartialProgramLayer.__call__.
 
-            >>> self.backward_program.desc.block(0),
+            >>> self.backward_program.desc.global_block(),
 
             When we access RunProgramAPI, it's possible to get an invalid backward_program address.
             """
@@ -586,7 +586,7 @@ class PartialProgramLayer:
                 return False
             if var.dtype not in [paddle.float32, paddle.float64]:
                 return False
-            for op in main_program.block(0).ops:
+            for op in main_program.global_block().ops:
                 for in_arg in op.input_arg_names:
                     if in_arg == var.name:
                         return True
@@ -603,7 +603,7 @@ class PartialProgramLayer:
                         out_arg == var_grad_name
                         for out_arg in x[1].output_arg_names
                     ),
-                    enumerate(target_program.block(0).ops),
+                    enumerate(target_program.global_block().ops),
                 )
             )
 
@@ -612,7 +612,7 @@ class PartialProgramLayer:
             if len(finded_ops) == 0:
                 return None
             # step1: create a new var named var.name@GRAD
-            target_program.block(0).create_var(
+            target_program.global_block().create_var(
                 name=new_grad_name,
                 type=var.type,
                 dtype=var.dtype,
@@ -624,7 +624,7 @@ class PartialProgramLayer:
                 op._rename_output(var_grad_name, new_grad_name)
             # step3: insert sum op to aggregate the gradient.
             #        var.name@GRAD = sum(var.name@dy2static@GRAD, var.name@GRAD)
-            target_program.block(0)._insert_op(
+            target_program.global_block()._insert_op(
                 finded_ops[-1][0] + 1,
                 type='sum',
                 inputs={'X': [var_grad_name, new_grad_name]},
@@ -668,7 +668,7 @@ class PartialProgramLayer:
                         forward_outputs_grads.append(None)
                         continue
                     opres = (
-                        program.block()
+                        program.global_block()
                         .ops[forward_end_idx + not_stop_gradient_num]
                         .results()[0]
                     )
@@ -742,9 +742,9 @@ class PartialProgramLayer:
     def _prepare_attributes(self):
         attrs = [
             'forward_global_block',
-            self.forward_program.block(),
+            self.forward_program.global_block(),
             'backward_global_block',
-            self.backward_program.block(),
+            self.backward_program.global_block(),
             'is_test',
             not self.training,
             'program_id',
@@ -798,7 +798,7 @@ class PartialProgramLayer:
         backward_start_op_index = forward_end_op_index + len(
             list(filter(lambda r: r.stop_gradient is False, self._outputs))
         )
-        backward_end_op_index = len(whole_program.block().ops)
+        backward_end_op_index = len(whole_program.global_block().ops)
         # For Backward process in CINN, all param@GRAD shoule be skipped for GC, because
         # they will be shared in scope and used by optimizer.
 
@@ -1057,7 +1057,9 @@ class PartialProgramLayer:
         # be user wanted result.
         for param in params:
             grad_name = param.name + core.grad_var_suffix()
-            grad_var = train_program.desc.block(0).find_var(grad_name.encode())
+            grad_var = train_program.desc.global_block().find_var(
+                grad_name.encode()
+            )
             # NOTE: cannot find var desc maybe no problem, such as in batch_norm
             if grad_var is None:
                 continue
