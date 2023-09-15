d19df7c7806ba48b63f8125c3904a62add9fb656
liangjianzhong <liangjianzhong@baidu.com>
2023-09-15 11:14:13 +0800

    bugfix in win

--- paddle/fluid/pybind/auto_parallel_py.cc
+++ paddle/fluid/pybind/auto_parallel_py.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <Python.h>
 #include <pybind11/operators.h>
 #include <pybind11/stl.h>
 #include <utility>
