e8aa01db5aee6b6e1c033a50ee483b9ae937bf74
zhanglirong1999 <1695074375@qq.com>
2023-09-15 16:20:29 +0800

    update

--- paddle/fluid/framework/ir/mkldnn/fc_elementwise_add_mkldnn_fuse_pass.cc
+++ paddle/fluid/framework/ir/mkldnn/fc_elementwise_add_mkldnn_fuse_pass.cc
@@ -115,6 +115,7 @@ GraphWithStats FCResidualConnectionMKLDNNFusePass::FuseFC(
 
     // Binary_add may have some error when scale in int8, thus skip
     proto::VarType::Type data_type = fc_input->Var()->GetDataType();
+    LOG(INFO) << fc_input->Var()->GetDataType();
     if (data_type == proto::VarType::INT8 ||
         data_type == proto::VarType::UINT8) {
       LOG(INFO) << "Skip fusion fc + elementwise_add with int8 data type";
