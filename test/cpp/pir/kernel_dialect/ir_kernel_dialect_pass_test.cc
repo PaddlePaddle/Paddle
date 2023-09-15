0fa2751dd136cd158cf53d60965c87e53e6571f5
phlrain <phliuhongyu@126.com>
2023-09-15 08:56:29 +0000

    update

--- test/cpp/pir/kernel_dialect/ir_kernel_dialect_pass_test.cc
+++ test/cpp/pir/kernel_dialect/ir_kernel_dialect_pass_test.cc
@@ -56,171 +56,124 @@ PD_DECLARE_KERNEL(add, CPU, ALL_LAYOUT);
 
 bool simple_cmp(float a, float b) { return std::abs((a - b) / a) < 1e-5; }
 
-// TEST(program_test, program) {
-//   // (1) Init environment.
-//   ir::IrContext* ctx = ir::IrContext::Instance();
-//   ir::Program program((ctx));
-
-//   ctx->GetOrRegisterDialect<paddle::dialect::PaddleDialect>();
-
-//   ir::Builder builder = ir::Builder(ctx, program.block());
-
-//   paddle::dialect::FullOp op1 = builder.Build<paddle::dialect::FullOp>(
-//       std::vector<int64_t>{2, 2}, 1.0, phi::DataType::FLOAT32,
-//       phi::CPUPlace());
-
-//   paddle::dialect::FullOp op2 = builder.Build<paddle::dialect::FullOp>(
-//       std::vector<int64_t>{2, 2}, 1.0, phi::DataType::FLOAT32,
-//       phi::CPUPlace());
-
-//   builder.Build<paddle::dialect::AddOp>(op1->result(0), op2->result(0));
-
-//   auto kernel_program = paddle::dialect::PdOpLowerToKernelPass(&program);
-
-//   paddle::framework::Scope scope;
-//   PhiKernelAdaptor phi_kernel_adaptor(&scope);
-//   phi_kernel_adaptor.run_kernel_prog(kernel_program.get());
-
-//   auto out_tensor =
-//       scope.Var(phi_kernel_adaptor.out_name)->Get<phi::DenseTensor>();
-
-//   bool res0 = simple_cmp(out_tensor.data<float>()[0], 2.0);
-//   bool res1 = simple_cmp(out_tensor.data<float>()[1], 2.0);
-//   bool res2 = simple_cmp(out_tensor.data<float>()[2], 2.0);
-//   bool res3 = simple_cmp(out_tensor.data<float>()[3], 2.0);
-
-//   EXPECT_EQ(res0, true);
-//   EXPECT_EQ(res1, true);
-//   EXPECT_EQ(res2, true);
-//   EXPECT_EQ(res3, true);
-
-//   EXPECT_EQ(kernel_program->block()->size(), 3u);
-//   EXPECT_EQ(kernel_program->block()
-//                 ->front()
-//                 ->dyn_cast<paddle::dialect::PhiKernelOp>()
-//                 .op_name(),
-//             "pd.full");
-//   EXPECT_EQ(kernel_program->block()
-//                 ->front()
-//                 ->dyn_cast<paddle::dialect::PhiKernelOp>()
-//                 .kernel_name(),
-//             "full");
-//   EXPECT_EQ(kernel_program->block()
-//                 ->front()
-//                 ->dyn_cast<paddle::dialect::PhiKernelOp>()
-//                 .kernel_key()
-//                 .dtype(),
-//             phi::DataType::FLOAT32);
-// }
-
-// TEST(dialect_attr, attr) {
-//   // (1) Init environment.
-//   ir::IrContext* ctx = ir::IrContext::Instance();
-//   ir::Program program((ctx));
-
-//   ctx->GetOrRegisterDialect<paddle::dialect::PaddleDialect>();
-//   auto kernel_dialect =
-//       ctx->GetOrRegisterDialect<paddle::dialect::PaddleKernelDialect>();
-
-//   phi::KernelKey kernel_key(
-//       phi::Backend::CPU, phi::DataLayout::ALL_LAYOUT,
-//       phi::DataType::FLOAT32);
-//   auto attr = paddle::dialect::KernelAttribute::get(ctx, kernel_key);
-
-//   std::stringstream ss;
-
-//   kernel_dialect->PrintAttribute(attr, ss);
-
-//   EXPECT_EQ(
-//       ss.str() == "<backend:CPU|layout:Undefined(AnyLayout)|dtype:float32>",
-//       true);
-// }
-
-// ir::AttributeMap CreateAttributeMap(std::vector<std::string> attribute_names,
-//                                     std::vector<std::string> attributes,
-//                                     std::string attr_name,
-//                                     phi::KernelKey kernel_key) {
-//   ir::IrContext* ctx = ir::IrContext::Instance();
-//   ir::AttributeMap attr_map;
-//   for (size_t i = 0; i < attribute_names.size(); i++) {
-//     ir::Attribute attr_value = ir::StrAttribute::get(ctx, attributes[i]);
-//     attr_map.insert(
-//         std::pair<std::string, ir::Attribute>(attribute_names[i],
-//         attr_value));
-//   }
-//   auto attr = paddle::dialect::KernelAttribute::get(ctx, kernel_key);
-//   attr_map.insert(std::pair<std::string, ir::Attribute>(attr_name, attr));
-//   return attr_map;
-// }
-
-// TEST(kernel_dialect, legacy_op_test) {
-//   // (1) Init environment.
-
-//   ir::IrContext* ctx = ir::IrContext::Instance();
-//   ir::Program program((ctx));
-
-//   ctx->GetOrRegisterDialect<paddle::dialect::PaddleDialect>();
-//   phi::KernelKey kernel_key(
-//       phi::Backend::CPU, phi::DataLayout::ALL_LAYOUT,
-//       phi::DataType::FLOAT32);
-
-//   ir::OpInfo kernel_op_info =
-//       ctx->GetRegisteredOpInfo(paddle::dialect::LegacyKernelOp::name());
-//   ir::OperationArgument argument(kernel_op_info);
-//   argument.attributes = CreateAttributeMap({"op_name", "kernel_name"},
-//                                            {"pd.kernel_op", "kernel_op"},
-//                                            "kernel_key",
-//                                            kernel_key);
-
-//   ir::Operation* op = ir::Operation::Create(std::move(argument));
-//   EXPECT_EQ("pd.kernel_op",
-//             op->dyn_cast<paddle::dialect::LegacyKernelOp>().op_name());
-//   EXPECT_EQ("kernel_op",
-//             op->dyn_cast<paddle::dialect::LegacyKernelOp>().kernel_name());
-//   EXPECT_EQ(kernel_key,
-//             op->dyn_cast<paddle::dialect::LegacyKernelOp>().kernel_key());
-// }
-
-TEST(kernel_dialect, legacy_op_test) {
+TEST(program_test, program) {
   // (1) Init environment.
-  std::cout << "start_test_end!" << std::endl;
-  pir::IrContext* ctx = pir::IrContext::Instance();
-  ctx->GetOrRegisterDialect<paddle::dialect::OperatorDialect>();
-  ctx->GetOrRegisterDialect<pir::ControlFlowDialect>();
+  ir::IrContext* ctx = ir::IrContext::Instance();
+  ir::Program program((ctx));
 
-  pir::Program program(ctx);
-  pir::Block* block = program.block();
-  pir::Builder builder(ctx, block);
+  ctx->GetOrRegisterDialect<paddle::dialect::PaddleDialect>();
 
-  auto full_op = builder.Build<paddle::dialect::FullOp>(
-      std::vector<int64_t>{1}, true, phi::DataType::BOOL);
+  ir::Builder builder = ir::Builder(ctx, program.block());
 
-  auto if_op = builder.Build<paddle::dialect::IfOp>(
-      full_op.out(), std::vector<pir::Type>{builder.bool_type()});
+  paddle::dialect::FullOp op1 = builder.Build<paddle::dialect::FullOp>(
+      std::vector<int64_t>{2, 2}, 1.0, phi::DataType::FLOAT32, phi::CPUPlace());
 
-  pir::Block* true_block = if_op.true_block();
+  paddle::dialect::FullOp op2 = builder.Build<paddle::dialect::FullOp>(
+      std::vector<int64_t>{2, 2}, 1.0, phi::DataType::FLOAT32, phi::CPUPlace());
 
-  builder.SetInsertionPointToStart(true_block);
-
-  auto full_op_1 = builder.Build<paddle::dialect::FullOp>(
-      std::vector<int64_t>{2}, true, phi::DataType::BOOL);
-  builder.Build<pir::YieldOp>(std::vector<pir::OpResult>{full_op_1.out()});
-
-  pir::Block* false_block = if_op.false_block();
-
-  builder.SetInsertionPointToStart(false_block);
-
-  auto full_op_2 = builder.Build<paddle::dialect::FullOp>(
-      std::vector<int64_t>{3}, true, phi::DataType::BOOL);
-  builder.Build<pir::YieldOp>(std::vector<pir::OpResult>{full_op_2.out()});
-
-  program.Print(std::cout);
-
-  std::cout << "test_end!" << std::endl;
+  builder.Build<paddle::dialect::AddOp>(op1->result(0), op2->result(0));
 
   auto kernel_program = paddle::dialect::PdOpLowerToKernelPass(&program);
 
-  kernel_program->Print(std::cout);
+  paddle::framework::Scope scope;
+  PhiKernelAdaptor phi_kernel_adaptor(&scope);
+  phi_kernel_adaptor.run_kernel_prog(kernel_program.get());
+
+  auto out_tensor =
+      scope.Var(phi_kernel_adaptor.out_name)->Get<phi::DenseTensor>();
+
+  bool res0 = simple_cmp(out_tensor.data<float>()[0], 2.0);
+  bool res1 = simple_cmp(out_tensor.data<float>()[1], 2.0);
+  bool res2 = simple_cmp(out_tensor.data<float>()[2], 2.0);
+  bool res3 = simple_cmp(out_tensor.data<float>()[3], 2.0);
+
+  EXPECT_EQ(res0, true);
+  EXPECT_EQ(res1, true);
+  EXPECT_EQ(res2, true);
+  EXPECT_EQ(res3, true);
+
+  EXPECT_EQ(kernel_program->block()->size(), 3u);
+  EXPECT_EQ(kernel_program->block()
+                ->front()
+                ->dyn_cast<paddle::dialect::PhiKernelOp>()
+                .op_name(),
+            "pd.full");
+  EXPECT_EQ(kernel_program->block()
+                ->front()
+                ->dyn_cast<paddle::dialect::PhiKernelOp>()
+                .kernel_name(),
+            "full");
+  EXPECT_EQ(kernel_program->block()
+                ->front()
+                ->dyn_cast<paddle::dialect::PhiKernelOp>()
+                .kernel_key()
+                .dtype(),
+            phi::DataType::FLOAT32);
+}
+
+TEST(dialect_attr, attr) {
+  // (1) Init environment.
+  ir::IrContext* ctx = ir::IrContext::Instance();
+  ir::Program program((ctx));
+
+  ctx->GetOrRegisterDialect<paddle::dialect::PaddleDialect>();
+  auto kernel_dialect =
+      ctx->GetOrRegisterDialect<paddle::dialect::PaddleKernelDialect>();
+
+  phi::KernelKey kernel_key(
+      phi::Backend::CPU, phi::DataLayout::ALL_LAYOUT, phi::DataType::FLOAT32);
+  auto attr = paddle::dialect::KernelAttribute::get(ctx, kernel_key);
+
+  std::stringstream ss;
+
+  kernel_dialect->PrintAttribute(attr, ss);
+
+  EXPECT_EQ(
+      ss.str() == "<backend:CPU|layout:Undefined(AnyLayout)|dtype:float32>",
+      true);
+}
+
+ir::AttributeMap CreateAttributeMap(std::vector<std::string> attribute_names,
+                                    std::vector<std::string> attributes,
+                                    std::string attr_name,
+                                    phi::KernelKey kernel_key) {
+  ir::IrContext* ctx = ir::IrContext::Instance();
+  ir::AttributeMap attr_map;
+  for (size_t i = 0; i < attribute_names.size(); i++) {
+    ir::Attribute attr_value = ir::StrAttribute::get(ctx, attributes[i]);
+    attr_map.insert(
+        std::pair<std::string, ir::Attribute>(attribute_names[i], attr_value));
+  }
+  auto attr = paddle::dialect::KernelAttribute::get(ctx, kernel_key);
+  attr_map.insert(std::pair<std::string, ir::Attribute>(attr_name, attr));
+  return attr_map;
+}
+
+TEST(kernel_dialect, legacy_op_test) {
+  // (1) Init environment.
+
+  ir::IrContext* ctx = ir::IrContext::Instance();
+  ir::Program program((ctx));
+
+  ctx->GetOrRegisterDialect<paddle::dialect::PaddleDialect>();
+  phi::KernelKey kernel_key(
+      phi::Backend::CPU, phi::DataLayout::ALL_LAYOUT, phi::DataType::FLOAT32);
+
+  ir::OpInfo kernel_op_info =
+      ctx->GetRegisteredOpInfo(paddle::dialect::LegacyKernelOp::name());
+  ir::OperationArgument argument(kernel_op_info);
+  argument.attributes = CreateAttributeMap({"op_name", "kernel_name"},
+                                           {"pd.kernel_op", "kernel_op"},
+                                           "kernel_key",
+                                           kernel_key);
+
+  ir::Operation* op = ir::Operation::Create(std::move(argument));
+  EXPECT_EQ("pd.kernel_op",
+            op->dyn_cast<paddle::dialect::LegacyKernelOp>().op_name());
+  EXPECT_EQ("kernel_op",
+            op->dyn_cast<paddle::dialect::LegacyKernelOp>().kernel_name());
+  EXPECT_EQ(kernel_key,
+            op->dyn_cast<paddle::dialect::LegacyKernelOp>().kernel_key());
 }
 
 TEST(kernel_dialect, cond_op_test) {
