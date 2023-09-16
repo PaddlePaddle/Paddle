faa459a079e0b12c0c1a9399c0d5c1a9206d72bf
andsonder <changlu@keter.top>
2023-09-16 12:26:25 +0000

    recover codes

--- test/legacy_test/test_masked_select_op.py
+++ test/legacy_test/test_masked_select_op.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,194 +15,259 @@
 import unittest
 
 import numpy as np
+from op_test import OpTest, convert_float_to_uint16
 
 import paddle
-from paddle import base
 from paddle.base import core
 
 
-def np_masked_fill(x, mask, value):
-    if not np.isscalar(value):
-        value = value[0]
-
+def np_masked_select(x, mask):
+    result = np.empty(shape=(0), dtype=x.dtype)
     x, mask = np.broadcast_arrays(x, mask)
-    result = np.copy(x)
-    for idx, m in np.ndenumerate(mask):
-        if m:
-            result[idx] = value
-    return result
+    for ele, ma in zip(np.nditer(x), np.nditer(mask)):
+        if ma:
+            result = np.append(result, ele)
+    return result.flatten()
 
 
-paddle.enable_static()
-
-
-class TestMaskedFillOp(unittest.TestCase):
+class TestMaskedSelectOp(OpTest):
     def setUp(self):
         self.init()
+        self.op_type = "masked_select"
+        self.python_api = paddle.masked_select
+        x = np.random.random(self.shape).astype("float64")
+        mask = np.array(np.random.randint(2, size=self.mask_shape, dtype=bool))
+        out = np_masked_select(x, mask)
+        self.inputs = {'X': x, 'Mask': mask}
+        self.outputs = {'Y': out}
 
-        self.x_np = np.random.random(self.x_shape).astype("float32")
-        self.mask_np = np.array(
-            np.random.randint(2, size=self.mask_shape), dtype="bool"
-        )
+    def test_check_output(self):
+        self.check_output()
 
-        self.value_np = np.random.randn(1).astype("float32")
-        self.out_np = np_masked_fill(self.x_np, self.mask_np, self.value_np)
+    def test_check_grad(self):
+        self.check_grad(['X'], 'Y')
 
     def init(self):
-        self.x_shape = (50, 3)
-        self.mask_shape = self.x_shape
+        self.shape = (50, 3)
+        self.mask_shape = self.shape
 
-    def test_static_graph(self):
-        paddle.enable_static()
-        startup_program = base.Program()
-        train_program = base.Program()
-        with base.program_guard(startup_program, train_program):
-            x = paddle.static.data(
-                name='x', dtype='float32', shape=self.x_shape
-            )
-            mask = paddle.static.data(
-                name='mask', dtype='bool', shape=self.mask_shape
-            )
-            value = paddle.static.data(
-                name='value', dtype='float32', shape=self.value_np.shape
-            )
-            out = paddle.masked_fill(x, mask, value)
 
-            place = (
-                base.CUDAPlace(0)
-                if core.is_compiled_with_cuda()
-                else base.CPUPlace()
-            )
-            exe = base.Executor(place)
-            res = exe.run(
-                base.default_main_program(),
-                feed={
-                    'x': self.x_np,
-                    'mask': self.mask_np,
-                    'value': self.value_np,
-                },
-                fetch_list=[out],
-            )
-            np.testing.assert_allclose(
-                res[0], self.out_np, atol=1e-5, rtol=1e-5
-            )
-            paddle.disable_static()
+class TestMaskedSelectOp1(TestMaskedSelectOp):
+    def init(self):
+        self.shape = (6, 8, 9, 18)
+        self.mask_shape = self.shape
 
-    def test_dygraph(self):
+
+class TestMaskedSelectOp2(TestMaskedSelectOp):
+    def init(self):
+        self.shape = (168,)
+        self.mask_shape = self.shape
+
+
+class TestMaskedSelectFP16Op(OpTest):
+    def setUp(self):
+        self.init()
+        self.op_type = "masked_select"
+        self.dtype = np.float16
+        self.python_api = paddle.masked_select
+        x = np.random.random(self.shape).astype("float16")
+        mask = np.array(np.random.randint(2, size=self.shape, dtype=bool))
+        out = np_masked_select(x, mask)
+        self.inputs = {'X': x, 'Mask': mask}
+        self.outputs = {'Y': out}
+
+    def test_check_output(self):
+        self.check_output()
+
+    def test_check_grad(self):
+        self.check_grad(['X'], 'Y')
+
+    def init(self):
+        self.shape = (50, 3)
+
+
+class TestMaskedSelectFP16Op1(TestMaskedSelectFP16Op):
+    def init(self):
+        self.shape = (6, 8, 9, 18)
+
+
+class TestMaskedSelectFP16Op2(TestMaskedSelectFP16Op):
+    def init(self):
+        self.shape = (168,)
+
+
+@unittest.skipIf(
+    not core.is_compiled_with_cuda()
+    or not core.is_bfloat16_supported(core.CUDAPlace(0)),
+    "core is not compiled with CUDA or not support bfloat16",
+)
+class TestMaskedSelectBF16Op(OpTest):
+    def setUp(self):
+        self.init()
+        self.op_type = "masked_select"
+        self.dtype = np.uint16
+        self.python_api = paddle.masked_select
+        x = np.random.random(self.shape).astype("float32")
+        mask = np.array(np.random.randint(2, size=self.shape, dtype=bool))
+        out = np_masked_select(x, mask)
+        self.inputs = {'X': convert_float_to_uint16(x), 'Mask': mask}
+        self.outputs = {'Y': convert_float_to_uint16(out)}
+
+    def test_check_output(self):
+        self.check_output_with_place(core.CUDAPlace(0))
+
+    def test_check_grad(self):
+        self.check_grad_with_place(core.CUDAPlace(0), ['X'], 'Y')
+
+    def init(self):
+        self.shape = (50, 3)
+
+
+class TestMaskedSelectBF16Op1(TestMaskedSelectBF16Op):
+    def init(self):
+        self.shape = (6, 8, 9, 2)
+
+
+class TestMaskedSelectBF16Op2(TestMaskedSelectBF16Op):
+    def init(self):
+        self.shape = (168,)
+
+
+class TestMaskedSelectAPI(unittest.TestCase):
+    def test_imperative_mode(self):
         paddle.disable_static()
-        x = paddle.to_tensor(self.x_np, dtype='float32')
-        mask = paddle.to_tensor(self.mask_np).astype('bool')
-        value = paddle.to_tensor(self.value_np, dtype='float32')
-        result = paddle.masked_fill(x, mask, value)
-        np.testing.assert_allclose(self.out_np, result.numpy(), rtol=1e-05)
-
+        shape = (88, 6, 8)
+        np_x = np.random.random(shape).astype('float32')
+        np_mask = np.array(np.random.randint(2, size=shape, dtype=bool))
+        x = paddle.to_tensor(np_x)
+        mask = paddle.to_tensor(np_mask)
+        out = paddle.masked_select(x, mask)
+        np_out = np_masked_select(np_x, np_mask)
+        np.testing.assert_allclose(out.numpy(), np_out, rtol=1e-05)
         paddle.enable_static()
 
+    def test_static_mode(self):
+        shape = [8, 9, 6]
+        x = paddle.static.data(shape=shape, dtype='float32', name='x')
+        mask = paddle.static.data(shape=shape, dtype='bool', name='mask')
+        np_x = np.random.random(shape).astype('float32')
+        np_mask = np.array(np.random.randint(2, size=shape, dtype=bool))
 
-class TestMaskedFillOp1(TestMaskedFillOp):
+        out = paddle.masked_select(x, mask)
+        np_out = np_masked_select(np_x, np_mask)
+
+        exe = paddle.static.Executor(place=paddle.CPUPlace())
+
+        (res,) = exe.run(
+            paddle.static.default_main_program(),
+            feed={"x": np_x, "mask": np_mask},
+            fetch_list=[out],
+        )
+        np.testing.assert_allclose(res, np_out, rtol=1e-05)
+
+
+class TestMaskedSelectError(unittest.TestCase):
+    def setUp(self):
+        paddle.enable_static()
+
+    def test_error(self):
+        with paddle.static.program_guard(
+            paddle.static.Program(), paddle.static.Program()
+        ):
+            shape = [8, 9, 6]
+            x = paddle.static.data(shape=shape, dtype='float32', name='x')
+            mask = paddle.static.data(shape=shape, dtype='bool', name='mask')
+            mask_float = paddle.static.data(
+                shape=shape, dtype='float32', name='mask_float'
+            )
+            np_x = np.random.random(shape).astype('float32')
+            np_mask = np.array(np.random.randint(2, size=shape, dtype=bool))
+
+            def test_x_type():
+                paddle.masked_select(np_x, mask)
+
+            self.assertRaises(TypeError, test_x_type)
+
+            def test_mask_type():
+                paddle.masked_select(x, np_mask)
+
+            self.assertRaises(TypeError, test_mask_type)
+
+            def test_mask_dtype():
+                paddle.masked_select(x, mask_float)
+
+            self.assertRaises(TypeError, test_mask_dtype)
+
+
+class TestMaskedSelectBroadcast(unittest.TestCase):
+    def setUp(self):
+        paddle.disable_static()
+
+    def test_broadcast(self):
+        shape = (3, 4)
+        np_x = np.random.random(shape).astype('float32')
+        np_mask = np.array([[True], [False], [False]])
+        x = paddle.to_tensor(np_x)
+        mask = paddle.to_tensor(np_mask)
+        out = paddle.masked_select(x, mask)
+        np_out = np_x[0]
+        np.testing.assert_allclose(out.numpy(), np_out, rtol=1e-05)
+
+    def test_broadcast_grad(self):
+        shape = (3, 4)
+        np_x = np.random.random(shape).astype('float32')
+        np_mask = np.array([[True], [False], [False]])
+        x = paddle.to_tensor(np_x, stop_gradient=False)
+        mask = paddle.to_tensor(np_mask)
+        out = paddle.masked_select(x, mask)
+        out.sum().backward()
+        np_out = np.zeros(shape)
+        np_out[0] = 1.0
+        np.testing.assert_allclose(x.grad.numpy(), np_out, rtol=1e-05)
+
+    def test_broadcast_zerodim(self):
+        shape = (3, 4)
+        np_x = np.random.random(shape).astype('float32')
+        x = paddle.to_tensor(np_x)
+        mask = paddle.to_tensor(True)
+        out = paddle.masked_select(x, mask)
+        np_out = np_x.reshape(-1)
+        np.testing.assert_allclose(out.numpy(), np_out, rtol=1e-05)
+
+    def test_broadcast_zerodim_grad(self):
+        shape = (3, 4)
+        np_x = np.random.random(shape).astype('float32')
+        np_mask = np.array(True)
+        x = paddle.to_tensor(np_x, stop_gradient=False)
+        mask = paddle.to_tensor(np_mask)
+        out = paddle.masked_select(x, mask)
+        out.sum().backward()
+        np_out = np.ones(shape)
+        np.testing.assert_allclose(x.grad.numpy(), np_out, rtol=1e-05)
+
+
+class TestMaskedSelectOpBroadcast(TestMaskedSelectOp):
     def init(self):
-        self.x_shape = (6, 8, 9, 18)
-        self.mask_shape = self.x_shape
-
-
-class TestMaskedFillOp2(TestMaskedFillOp):
-    def init(self):
-        self.x_shape = (168,)
-        self.mask_shape = self.x_shape
-
-
-class TestMaskedFillOpBroadcast(TestMaskedFillOp):
-    def init(self):
-        self.x_shape = (3, 40)
+        self.shape = (3, 40)
         self.mask_shape = (3, 1)
 
 
-class TestMaskedFillOpBroadcast2(TestMaskedFillOp):
+class TestMaskedSelectOpBroadcast2(TestMaskedSelectOp):
     def init(self):
-        self.x_shape = (300, 1)
+        self.shape = (300, 1)
         self.mask_shape = (300, 40)
 
 
-class TestMaskedFillOpBroadcast3(TestMaskedFillOp):
+class TestMaskedSelectOpBroadcast3(TestMaskedSelectOp):
     def init(self):
-        self.x_shape = (120,)
+        self.shape = (120,)
         self.mask_shape = (300, 120)
 
 
-class TestMaskedFillOpBroadcast4(TestMaskedFillOp):
+class TestMaskedSelectOpBroadcast4(TestMaskedSelectOp):
     def init(self):
-        self.x_shape = (300, 40)
-        self.mask_shape = (40,)
-
-
-# class TestMaskedFillFP16Op(OpTest):
-#     def setUp(self):
-#         self.init()
-#         self.op_type = "masked_fill"
-#         self.dtype = np.float16
-#         self.python_api = paddle.masked_fill
-#         x = np.random.random(self.shape).astype("float16")
-#         mask = np.array(np.random.randint(2, size=self.shape, dtype=bool))
-#         value = np.array(np.random.random(5).astype("float16"))
-#         out = np_masked_fill(x, mask, value)
-#         self.inputs = {'X': x, 'Mask': mask}
-#         self.outputs = {'Y': out}
-
-#     def test_check_output(self):
-#         self.check_output()
-
-#     def test_check_grad(self):
-#         self.check_grad(['X'], 'Y')
-
-#     def init(self):
-#         self.shape = (50, 3)
-
-
-# class TestMaskedFillFP16Op1(TestMaskedFillFP16Op):
-#     def init(self):
-#         self.shape = (6, 8, 9, 18)
-
-
-# class TestMaskedFillFP16Op2(TestMaskedFillFP16Op):
-#     def init(self):
-#         self.shape = (168,)
-
-
-# @unittest.skipIf(
-#     not core.is_compiled_with_cuda()
-#     or not core.is_bfloat16_supported(core.CUDAPlace(0)),
-#     "core is not compiled with CUDA or not support bfloat16",
-# )
-# class TestMaskedFillBF16Op(OpTest):
-#     def setUp(self):
-#         self.init()
-#         self.op_type = "masked_fill"
-#         self.dtype = np.uint16
-#         self.python_api = paddle.masked_fill
-#         x = np.random.random(self.shape).astype("float32")
-#         mask = np.array(np.random.randint(2, size=self.shape, dtype=bool))
-#         out = np_masked_fill(x, mask)
-#         self.inputs = {'X': convert_float_to_uint16(x), 'Mask': mask}
-#         self.outputs = {'Y': convert_float_to_uint16(out)}
-
-#     def test_check_output(self):
-#         self.check_output_with_place(core.CUDAPlace(0))
-
-#     def test_check_grad(self):
-#         self.check_grad_with_place(core.CUDAPlace(0), ['X'], 'Y')
-
-#     def init(self):
-#         self.shape = (50, 3)
-
-
-# class TestMaskedFillBF16Op1(TestMaskedFillBF16Op):
-#     def init(self):
-#         self.shape = (6, 8, 9, 2)
-
-
-# class TestMaskedFillBF16Op2(TestMaskedFillBF16Op):
-#     def init(self):
-#         self.shape = (168,)
+        self.shape = (300, 40)
+        self.mask_shape = 40
 
 
 if __name__ == '__main__':
