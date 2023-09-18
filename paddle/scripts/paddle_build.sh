8386e05585b3f22790747d075ba3599f558d6960
risemeup1 <515586620@qq.com>
2023-09-18 13:46:27 +0000

    fix cinn and build bug

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -987,7 +987,7 @@ function check_whl_size() {
     fi
 
     set +x
-    pr_whl_size=`du -m ${PADDLE_ROOT}/build/pr_whl/*.whl|awk '{print $1}'`
+    pr_whl_size=`du -m ${PADDLE_ROOT}/build/pr_whl/paddle*.whl|awk '{print $1}'`
     echo "pr_whl_size: ${pr_whl_size}"
 
     dev_whl_size=`du -m ${PADDLE_ROOT}/build/python/dist/*.whl|awk '{print $1}'`
