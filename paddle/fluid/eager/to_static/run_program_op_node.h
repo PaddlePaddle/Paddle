b9ee2068191ee8adfb6f625d6e871ecf6125f9cd
wanghuancoder <wanghuan29@baidu.com>
2023-09-19 11:11:08 +0800

    run program contiguous tensor first (#57431)
    * run program contiguous tensor first
    
    * refine

--- paddle/fluid/eager/to_static/run_program_op_node.h
+++ paddle/fluid/eager/to_static/run_program_op_node.h
@@ -24,6 +24,7 @@
 #include "paddle/fluid/pir/transforms/pd_op_to_kernel_pass.h"
 #include "paddle/fluid/platform/enforce.h"
 #include "paddle/fluid/platform/profiler/event_tracing.h"
+#include "paddle/phi/api/lib/data_transform.h"
 #include "paddle/pir/core/program.h"
 #include "paddle/pir/core/value.h"
 
@@ -31,6 +32,25 @@ PHI_DECLARE_bool(enable_new_ir_in_executor);
 
 namespace details {
 using Tensor = paddle::Tensor;
+
+static void Trans2ContiguousTensorsInplace(
+    const std::vector<paddle::Tensor> &tensors) {
+  std::vector<Tensor> res;
+  for (auto &t : tensors) {
+    if (t.is_initialized() && t.is_dense_tensor() &&
+        !std::dynamic_pointer_cast<phi::DenseTensor>(t.impl())
+             ->meta()
+             .is_contiguous()) {
+      auto tmp = paddle::experimental::Trans2Contiguous(
+          *(std::dynamic_pointer_cast<phi::DenseTensor>(t.impl())));
+      auto holder = tmp.MoveMemoryHolder();
+      std::dynamic_pointer_cast<phi::DenseTensor>(t.impl())->ResetHolder(
+          holder);
+      std::dynamic_pointer_cast<phi::DenseTensor>(t.impl())->set_meta(
+          tmp.meta());
+    }
+  }
+}
 
 static std::vector<Tensor> DereferenceTensors(
     const std::vector<Tensor *> &tensor_ptr) {
@@ -543,6 +563,8 @@ inline void RunProgramGradAPI(
   auto *backward_global_block = PADDLE_GET_CONST(
       paddle::framework::BlockDesc *, attrs.at("backward_global_block"));
   auto *backward_program = backward_global_block->Program();
+
+  details::Trans2ContiguousTensorsInplace(out_grad);
 
   auto out_grad_names = details::GetTensorsName(out_grad);
   auto &interpretercore_info_cache =
