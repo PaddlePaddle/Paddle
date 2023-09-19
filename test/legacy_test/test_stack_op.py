aba15a66413f538ffec7a9b132fd2a91db8085b3
liyongchao911 <11_258@163.com>
2023-09-19 11:21:19 +0800

    No.38 Migrate paddle.stack into pir

--- test/legacy_test/test_stack_op.py
+++ test/legacy_test/test_stack_op.py
@@ -63,10 +63,10 @@ class TestStackOpBase(OpTest):
         self.attrs = {'axis': self.axis}
 
     def test_check_output(self):
-        self.check_output(check_prim=True)
+        self.check_output(check_prim=True, check_new_ir=True)
 
     def test_check_grad(self):
-        self.check_grad(self.get_x_names(), 'Y', check_prim=True)
+        self.check_grad(self.get_x_names(), 'Y', check_prim=True, check_new_ir=True)
 
 
 class TestStackOp1(TestStackOpBase):
