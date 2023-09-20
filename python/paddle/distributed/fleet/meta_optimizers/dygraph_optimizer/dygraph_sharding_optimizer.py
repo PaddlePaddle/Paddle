df53ba4da9507ddabebbe35aae98dd2e715636d4
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-09-20 10:55:24 +0000

    polish

--- python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/dygraph_sharding_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/dygraph_sharding_optimizer.py
@@ -25,18 +25,22 @@ from ...utils.tensor_fusion_helper import fused_parameters
 
 g_shard_use_reduce = int(os.environ.get("FLAGS_shard_use_reduce", 1))
 g_shard_norm_align_dp = int(os.environ.get("FLAGS_shard_norm_align_dp", 0))
+g_shard_split_param = int(os.environ.get("FLAGS_shard_split_param", 0))
 
 if g_shard_norm_align_dp:
     assert (
         not g_shard_use_reduce
-    ), "g_shard_norm_align_dp is not support if g_shard_use_reduce is true"
+    ), "g_shard_norm_align_dp is not supported if g_shard_use_reduce is true"
+    assert (
+        not g_shard_split_param
+    ), "g_shard_norm_align_dp is not supported if g_shard_split_param is true"
 
 
 def _is_trainable(param):
     return not param.stop_gradient
 
 
-class DygraphShardingOptimizer:
+class DygraphShardingOptimizerV1:
     """
     A wrapper for Sharding Optimizer in Dygraph.
 
@@ -76,6 +80,7 @@ class DygraphShardingOptimizer:
         self.tensor_fusion = strategy.hybrid_configs[
             'sharding_configs'
         ].tensor_fusion
+
         self.accumulate_steps = strategy.hybrid_configs[
             'sharding_configs'
         ].accumulate_steps
@@ -422,3 +427,317 @@ class DygraphShardingOptimizer:
 
     def __getattr__(self, item):
         return getattr(self._inner_opt, item)
+
+
+class DygraphShardingOptimizerV2:
+    """
+    A wrapper for Sharding Optimizer in Dygraph, which split params
+
+    .. warning: DygraphShardingOptimizer is experimental and subject to change.
+
+    .. ZeRO: https://arxiv.org/abs/1910.02054
+
+    """
+
+    # TODO (JZ-LIANG)
+    # TO support following featrues in future:
+    # 1. fused update parameter sync
+    # 2. parameters_groups
+    # 3. dynamic trainable params, which is the case bewteen pretraining and finetuning
+    # 4. option to choose fuse comm (more GPU MEM need) or un-fuse comm
+    # 5. do not shard small params
+
+    def __init__(self, optimizer, hcg):
+        # TODO(pangengzheng): support param_groups
+        if isinstance(optimizer._parameter_list[0], dict):
+            raise TypeError(
+                "Do not support param_groups now, please set optimizer._parameter_list as a list of Parameter"
+            )
+        if not hasattr(optimizer, '_apply_optimize') or not callable(
+            optimizer._apply_optimize
+        ):
+            raise ValueError(
+                "the optimzier object should have _apply_optimize function"
+            )
+        # the self._parameter_list holds the whole model parameter slices
+        self._parameter_list = optimizer._parameter_list
+        # slice parameter list
+        self._local_parameter_list = [
+            self._partition_param(p) for p in optimizer._parameter_list
+        ]
+
+        self._inner_opt = optimizer
+        self._hcg = hcg
+        self._sharding_world_size = self._hcg.get_sharding_parallel_world_size()
+        self._sharding_rank = self._hcg.get_sharding_parallel_rank()
+
+        strategy = fleet.fleet._user_defined_strategy
+        self.tensor_fusion = strategy.hybrid_configs[
+            'sharding_configs'
+        ].tensor_fusion
+
+        assert not self.tensor_fusion, "not supported yet"
+
+        self.accumulate_steps = strategy.hybrid_configs[
+            'sharding_configs'
+        ].accumulate_steps
+        self.comm_overlap = strategy.hybrid_configs[
+            'sharding_configs'
+        ].comm_overlap
+        pp_overlap = strategy.hybrid_configs['pp_configs'].sharding_comm_overlap
+
+        assert not self.comm_overlap, "not supported yet"
+        assert not pp_overlap, "not supported yet"
+
+        self._set_inner_opt_attr('_parameter_list', self._local_parameter_list)
+        self._set_inner_opt_attr('_param_groups', self._local_parameter_list)
+
+    def clear_grad(self, set_to_zero=True):
+        """
+        should clear grad for all parameters in model
+        """
+
+        def clear_grad_func(p):
+            if hasattr(p, "main_grad") and p.main_grad is not None:
+                assert p._grad_ivar() is None
+                if set_to_zero:
+                    p.main_grad.zero_()
+                else:
+                    p.main_grad._clear()
+                    p.main_grad = None
+            elif not hasattr(p, "main_grad"):
+                if self.tensor_fusion:
+                    if set_to_zero:
+                        p.grad.zero_()
+                    else:
+                        p.grad._clear()
+                        p.grad = None
+                else:
+                    p.clear_gradient(set_to_zero)
+
+        for p in self._parameter_list:
+            clear_grad_func(p)
+            if hasattr(p, "shard_slice"):
+                clear_grad_func(p.shard_slice)
+
+    def reduce_gradients(self, parameter_list, hcg):
+        # TODO merge grad / nrank with dp
+        logger.debug("sharding start gradients sync")
+        with framework.no_grad():
+            sharding_nrank = hcg.get_sharding_parallel_group().nranks
+            for param in parameter_list:
+                g_var = None
+                if param.trainable and (param._grad_ivar() is not None):
+                    g_var = param._grad_ivar()
+                if param.trainable and hasattr(param, "main_grad"):
+                    assert (
+                        param._grad_ivar() is None
+                    ), "param.grad should be None when using main_grad"
+                    g_var = param.main_grad
+                if g_var is not None:
+                    g_var.scale_(1.0 / sharding_nrank)
+                    # extend the grad tensor to padded buffer
+                    g_var = self._create_padded_buffer(g_var)
+                    # reduce scatter
+                    assert g_var._numel() % self._sharding_world_size == 0
+                    reduce_scattered = paddle.empty(
+                        [g_var._numel() // self._sharding_world_size],
+                        dtype=g_var.dtype,
+                    )
+                    paddle.distributed.reduce_scatter(
+                        reduce_scattered,
+                        g_var,
+                        group=self._hcg.get_sharding_parallel_group(),
+                    )
+                    self._assign_slice_grad(param, reduce_scattered)
+
+    def _sharding_sync_parameters(self):
+        """
+        sync parameter across sharding group
+        """
+        # TODO speed up this functional
+
+        logger.debug("sharding start sync parameters")
+        with framework.no_grad():
+            params = zip(self._full_parameter_list, self._parameter_list)
+            for full, slice in params:
+                gather_list = []
+                paddle.distributed.all_gather(
+                    gather_list,
+                    slice,
+                    group=self._hcg.get_sharding_parallel_group(),
+                    sync_op=True,
+                )
+                padded_full = paddle.concat(gather_list, axis=0)
+                self._assign_param(full, padded_full)
+                # TODO(liuzhenhai): release shard slice
+
+    def _update_trainable(self):
+        """
+        allow user to update trainable parameters list during training
+        """
+        raise NotImplementedError
+
+    def minimize(
+        self, loss, startup_program=None, parameters=None, no_grad_set=None
+    ):
+        # NOTE in dygraph mode, the only different between step and minimize is that minimize
+        # allow user to customize the parameters for updating on each step
+
+        input_param_names = {param.name for param in parameters}
+        parameters = list(
+            filter(
+                lambda x: x.name in input_param_names,
+                self._rank2params[self._sharding_rank],
+            )
+        )
+        result = self._inner_opt.minimize(
+            loss, startup_program, parameters, no_grad_set
+        )
+
+        # sync parameters across sharding ranks
+        self._sharding_sync_parameters()
+
+        return result
+
+    def _partition_param(self, param):
+        # partition param to slice
+        size = param._numel()
+        shard_size = (
+            size + self._sharding_world_size - 1
+        ) // self._sharding_world_size
+        slice_begin = shard_size * self._sharding_rank
+        slice_end = min(slice_begin + shard_size, size)
+        with framework.no_grad():
+            param_shape = param.shape
+            param.flatten_()
+            param_slice = paddle.zero(shape=[shard_size], dtype=param.dtype)
+            param_slice[0 : slice_end - slice_end] = param[
+                slice_begin:slice_end
+            ]
+            param.reshape(param_shape)
+            param_slice.name = f"shard@{param.name}"
+            param.shard_slice = param_slice
+            return param_slice
+
+    def _create_padded_buffer(self, t):
+        # extend grad to padded buffer
+        size = t._numel()
+        padded_size = (
+            (size + self._sharding_world_size - 1) // self._sharding_world_size
+        ) * self._sharding_world_size
+        with framework.no_grad():
+            t_shape = t.shape
+            t.flatten_()
+            t_padded = paddle.zero(shape=[padded_size], dtype=t.dtype)
+            t_padded[0:size] = t
+            t.reshape(t_shape)
+            return t_padded
+
+    def _assign_param(self, param, full_param):
+        # recover param from padded buffer
+        size = param._numel()
+        padded_size = full_param._numel()
+        assert padded_size >= size
+        with framework.no_grad():
+            param_shape = param.shape
+            param.flatten_()
+            param[0:size] = full_param[0:size]
+            param.reshape(param_shape)
+
+    def _assign_slice_grad(self, param, slice_grad):
+        # assign slice grad to parameter
+        with framework.no_grad():
+            assert hasattr(param, "shard_slice")
+            param.shard_slice._copy_gradient_from(slice_grad)
+
+    def step(self):
+        # TODO Check whether the model trainable param changed and update state accordingly
+
+        # hack to grad_clip all parameters,
+        # otherwise the self._inner_opt will only grad_clip the self._rank2params[self._sharding_rank] params
+        # TODO(pangengzheng): remove the hacked grad_clip codes here when there is no diff in calculating global norm values in HybridParallelClipGrad compared to dp.
+        origin_clip = self._inner_opt._grad_clip
+        if not isinstance(self._parameter_list[0], dict):
+            params_grads = []
+            for param in self._parameter_list:
+                if (
+                    hasattr(param, "regularizer")
+                    and param.regularizer is not None
+                ):
+                    raise ValueError(
+                        "param {} should not has the regularizer attribute".format(
+                            param.name
+                        )
+                    )
+                if param.stop_gradient:
+                    continue
+                # update on slice
+                assert hasattr(param, "shard_slice")
+                param = param.shard_slice
+                grad_var = param._grad_ivar()
+                if hasattr(param, "main_grad") and param.main_grad is not None:
+                    grad_var = param.main_grad
+                params_grads.append((param, grad_var))
+
+            self._apply_optimize(
+                loss=None,
+                startup_program=None,
+                params_grads=params_grads,
+            )
+
+        # sync parameters across sharding ranks
+        self._sharding_sync_parameters()
+
+    @framework.dygraph_only
+    def set_state_dict(self, state_dict):
+        inner_state = {}
+        parameters = self._rank2params[self._sharding_rank]
+
+        if "LR_Scheduler" in state_dict:
+            inner_state["LR_Scheduler"] = state_dict.pop("LR_Scheduler")
+
+        if "master_weights" in state_dict:
+            master = state_dict.pop("master_weights")
+            inner_state["master_weights"] = {}
+            for p in parameters:
+                for k, v in master.items():
+                    if p.name == k:
+                        v.name = self._inner_opt._gen_master_weight_var_name(p)
+                        inner_state["master_weights"][k] = v
+
+        for p in parameters:
+            for k, v in state_dict.items():
+                if p.name in k:
+                    inner_state[k] = v
+
+        self._inner_opt.set_state_dict(inner_state)
+
+    def _set_inner_opt_attr(self, attr_name, value):
+        inner_opt = self._inner_opt
+        inner_opt_name = '_inner_opt'
+        if not isinstance(attr_name, str):
+            raise TypeError(
+                "attr_name should be str type, but is {}".format(
+                    type(attr_name)
+                )
+            )
+        while hasattr(inner_opt, attr_name):
+            setattr(inner_opt, attr_name, value)
+            if (
+                hasattr(inner_opt, inner_opt_name)
+                and getattr(inner_opt, inner_opt_name, None) is not None
+            ):
+                inner_opt = getattr(inner_opt, inner_opt_name, None)
+            else:
+                break
+
+    def __getattr__(self, item):
+        return getattr(self._inner_opt, item)
+
+
+DygraphShardingOptimizer = (
+    DygraphShardingOptimizerV2
+    if g_shard_split_param
+    else DygraphShardingOptimizerV1
+)
