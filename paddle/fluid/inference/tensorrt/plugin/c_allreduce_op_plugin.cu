13190788b8b1ec193af7506941aea5b408dcb93f
gouzi <530971494@qq.com>
2023-09-22 00:02:23 +0800

    try ci

--- paddle/fluid/inference/tensorrt/plugin/c_allreduce_op_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/c_allreduce_op_plugin.cu
@@ -203,7 +203,14 @@ int CAllReducePluginDynamic::enqueue(
                           "has ring_id attr."));
     auto stream = comm_ctx->GetStream();
     ncclRedOp_t nccl_red_type = ncclSum;
-    comm_ctx->AllReduce(&inputs[0], inputs[0], nccl_red_type, stream);
+    // comm_ctx->AllReduce(&inputs[0], inputs[0], nccl_red_type, stream);
+    phi::dynload::ncclAllReduce(sendbuff,
+                                recvbuff,
+                                numel,
+                                dtype,
+                                nccl_red_type,
+                                comm_ctx->GetNcclComm(),
+                                stream);
     VLOG(3) << "new NCCLCommContext has ring_id_ " << ring_id_;
   } else {
     auto comm = platform::NCCLCommContext::Instance().Get(ring_id_);
