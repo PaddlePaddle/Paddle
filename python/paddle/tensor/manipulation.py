460c0be1b526cc01dbe0254bd5546f15562330d9
xiongkun <xiongkun03@baidu.com>
2023-09-21 08:29:17 +0000

    success run gpt forward and backward.

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -1987,8 +1987,10 @@ def split(x, num_or_sections, axis=0, name=None):
                 )
 
         if isinstance(num_or_sections, int):
+            dim = dim if dim >= 0 else dim + len(input.shape)
             return _C_ops.split_with_num(input, num_or_sections, dim)
         else:
+            dim = dim if dim >= 0 else dim + len(input.shape)
             return _C_ops.split(input, num_or_sections, dim)
     else:
         check_variable_and_dtype(
