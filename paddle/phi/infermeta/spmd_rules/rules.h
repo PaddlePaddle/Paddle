65999a996ff3a7a9952d353cf15625c3c19355ac
Yichen Zhang <zhangyichen03@baidu.com>
2023-09-22 15:17:31 +0800

    fix code style

--- paddle/phi/infermeta/spmd_rules/rules.h
+++ paddle/phi/infermeta/spmd_rules/rules.h
@@ -18,8 +18,8 @@ limitations under the License. */
 
 #include "paddle/phi/infermeta/spmd_rules/default_data_parallel.h"
 #include "paddle/phi/infermeta/spmd_rules/elementwise.h"
-#include "paddle/phi/infermeta/spmd_rules/layer_norm.h"
 #include "paddle/phi/infermeta/spmd_rules/embedding.h"
+#include "paddle/phi/infermeta/spmd_rules/layer_norm.h"
 #include "paddle/phi/infermeta/spmd_rules/matmul.h"
 #include "paddle/phi/infermeta/spmd_rules/reduction.h"
 #include "paddle/phi/infermeta/spmd_rules/replicated.h"
