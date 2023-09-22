1542a9598a14f418e89674c0fd817789f2216f49
lisamhy <763334840@qq.com>
2023-09-22 02:07:52 +0000

    cpu forward ok

---
+++ paddle/phi/kernels/cpu/scatter_kernel_impl.h
@@ -0,0 +1,183 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "glog/logging.h"
+
+#include "paddle/phi/core/dense_tensor.h"
+#include "paddle/phi/core/tensor_utils.h"
+#include "paddle/phi/kernels/funcs/blas/blas.h"
+#include "paddle/phi/kernels/funcs/eigen/common.h"
+#include "paddle/phi/kernels/funcs/math_function.h"
+#include "paddle/phi/kernels/funcs/math_function.h"
+
+
+#include "paddle/phi/common/memory_utils.h"
+#include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/core/utils/data_type.h"
+#include "paddle/phi/kernels/cpu/index_add_impl.h"
+#include "paddle/phi/kernels/funcs/eigen/common.h"
+#include "paddle/phi/kernels/index_add_kernel.h"
+
+#include "paddle/phi/kernels/compare_kernel.h"
+#include "paddle/phi/kernels/elementwise_divide_kernel.h"
+#include "paddle/phi/kernels/full_kernel.h"
+#include "paddle/phi/kernels/funcs/math_function.h"
+#include "paddle/phi/kernels/index_add_kernel.h"
+#include "paddle/phi/kernels/where_kernel.h"
+
+
+namespace phi {
+
+
+template <typename Context, typename T, typename IndexT = int>
+void IndexReduceInner(const Context& ctx,
+                   DenseTensor* input,
+                   const DenseTensor& index,
+                   int axis,
+                   const std::string& reduce,
+                   bool include_self,
+                   DenseTensor* source,
+                   DenseTensor* output) {
+  auto input_dim = input->dims();
+  auto input_dim_size = input_dim.size();
+  auto output_dim = output->dims();
+  auto index_size = index.dims()[0];
+  auto source_dim = source->dims();
+
+  const IndexT* index_data = index.data<IndexT>();
+  ctx.template Alloc<T>(output);
+
+  auto zeros = Full<T, Context>(ctx, vectorize(input_dim), 0);
+  auto ones = Full<T, Context>(ctx, vectorize(input_dim), 1);
+  auto counts = include_self ? ones : zeros;
+
+  auto src_ones = Full<T, Context>(ctx, vectorize(source->dims()), 1);
+  auto src_cnts = IndexAdd<T, Context>(ctx, counts, index, src_ones, axis);
+  auto mask = Equal<T, Context>(ctx, src_cnts, zeros);
+
+  if (include_self) {
+     phi::Copy(ctx, *input, ctx.GetPlace(), false, output);
+  } else {
+    T init_val;
+    if (reduce == "mul" || reduce == "multiply") {
+      init_val = static_cast<T>(1);
+    } else if (reduce == "amin") {
+      init_val = std::numeric_limits<T>::has_infinity
+                     ? std::numeric_limits<T>::infinity()
+                     : std::numeric_limits<T>::max();
+    } else if (reduce == "amax") {
+      init_val = std::numeric_limits<T>::has_infinity
+                     ? -std::numeric_limits<T>::infinity()
+                     : std::numeric_limits<T>::lowest();
+    } else {
+      init_val = static_cast<T>(0);
+    }
+    auto init = Full<T, Context>(ctx, vectorize(input_dim), init_val);
+    *output = Where<T, Context>(ctx, mask, *input, init);
+  }
+
+  auto slice_size = 1;
+  for (auto i = axis + 1; i < input_dim_size; i++) {
+    slice_size *= input_dim[i];
+  }
+  auto outer_nums = 1;
+  for (auto i = 0; i < axis; i++) {
+    outer_nums *= input_dim[i];
+  }
+
+  for (int i = 0; i < index_size; i++) {
+    PADDLE_ENFORCE_GE(
+        index_data[i],
+        0,
+        phi::errors::InvalidArgument(
+            "Variable value (index) of OP(index_add) "
+            "expected >= 0 and < %ld, but got %ld. Please check input "
+            "value.",
+            input_dim[axis],
+            index_data[i]));
+    PADDLE_ENFORCE_LT(
+        index_data[i],
+        input_dim[axis],
+        phi::errors::InvalidArgument(
+            "Variable value (index) of OP(index_add) "
+            "expected >= 0 and < %ld, but got %ld. Please check input "
+            "value.",
+            input_dim[axis],
+            index_data[i]));
+  }
+
+  VLOG(3) << "Index_Add_Debug; outer_nums: " << outer_nums
+          << "; slice_size: " << slice_size << "; index_size: " << index_size;
+
+  output->Resize(phi::make_ddim({outer_nums, input_dim[axis], slice_size}));
+  source->Resize(phi::make_ddim({outer_nums, index_size, slice_size}));
+  VLOG(3) << "output.dims: " << output->dims()
+          << ", source.dims: " << source->dims();
+
+  auto source_tensor = EigenTensor<T, 3>::From(*source);
+  auto output_tensor = EigenTensor<T, 3>::From(*output);
+
+  auto& place = *ctx.eigen_device();
+  for (auto j = 0; j < index_size; j++) {
+    IndexT index_value = index_data[j];
+    auto output_t = output_tensor.chip(index_value, 1);
+    auto source_t = source_tensor.chip(j, 1);
+    if (reduce == "add" || reduce =="mean") {
+      output_t.device(place) = output_t + source_t;
+    } else if (reduce == "mul" || reduce =="muliply"){
+        output_t.device(place) = output_t * source_t;
+    } else if (reduce =="amin") {
+        output_t.device(place) = output_t.cwiseMin(source_t);
+    } else if (reduce =="amax"){
+        output_t.device(place) = output_t.cwiseMax(source_t);
+    } else if (reduce == "assign") {
+        output_t.device(place) = source_t;
+    }
+  }
+
+  output->Resize(output_dim);
+  source->Resize(source_dim);
+
+  if (reduce == "mean"){
+    auto src_cnts_wo_zeros = Where<T, Context>(ctx, mask, ones, src_cnts);
+    *output = Divide<T,Context>(ctx, *output, src_cnts_wo_zeros);
+  }
+}
+
+template <typename T, typename Context>
+void IndexReduceBaseKernel(const Context& dev_ctx,
+                        const DenseTensor& x,
+                        const DenseTensor& index,
+                        DenseTensor& source,
+                        int axis,
+                        const std::string& reduce,
+                        bool include_self,
+                        DenseTensor* output) {
+  const auto& index_type = index.dtype();
+  if (axis < 0) {
+    axis += x.dims().size();
+  }
+  auto inputs = x;
+  if (index_type == phi::DataType::INT32) {
+    IndexReduceInner<Context, T, int32_t>(
+        dev_ctx, &inputs, index, axis, reduce, include_self, &source, output);
+  } else if (index_type == phi::DataType::INT64) {
+    IndexReduceInner<Context, T, int64_t>(
+        dev_ctx, &inputs, index, axis, reduce, include_self, &source, output);
+  }
+}
+
+}  // namespace phi
--- paddle/phi/kernels/gpu/scatter_kernel.cu
+++ paddle/phi/kernels/gpu/scatter_kernel.cu
@@ -14,53 +14,163 @@
 
 #include "paddle/phi/kernels/scatter_kernel.h"
 
+#include "glog/logging.h"
 #include "paddle/phi/backends/gpu/gpu_context.h"
+#include "paddle/phi/backends/gpu/gpu_info.h"
+#include "paddle/phi/backends/gpu/gpu_launch_config.h"
+#include "paddle/phi/backends/gpu/gpu_primitives.h"
 #include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/tensor_utils.h"
+#include "paddle/phi/core/utils/data_type.h"
 #include "paddle/phi/kernels/funcs/scatter.cu.h"
+#include "paddle/utils/flags.h"
+
+PD_DECLARE_bool(cudnn_deterministic);
 
 namespace phi {
 
+using phi::PADDLE_CUDA_NUM_THREADS;
+
+template <typename T, typename IndexT>
+__global__ void index_reduce_cuda_kernel(const T* input,
+                                         const IndexT* index,
+                                         const T* add_value,
+                                         int64_t N,
+                                         int64_t stride,
+                                         int64_t size,
+                                         int64_t delta,
+                                         T* output) {
+  CUDA_KERNEL_LOOP_TYPE(idx, N, int64_t) {
+    int64_t pre_idx = idx / (stride * size);
+    int64_t dim_idx = idx % (stride * size) / stride;
+    IndexT src_dim_idx = index[dim_idx];
+    int64_t input_idx =
+        idx + (delta * pre_idx + src_dim_idx - dim_idx) * stride;
+    phi::CudaAtomicAdd(&output[input_idx], add_value[idx]);
+  }
+}
+
 template <typename T, typename Context>
-void ScatterKernel(const Context &ctx,
-                   const DenseTensor &x,
-                   const DenseTensor &index,
-                   const DenseTensor &updates,
+void IndexReduceKernel(const Context& ctx,
+                       const DenseTensor& x,
+                       const DenseTensor& index,
+                       const DenseTensor& add_value,
+                       int axis,
+                       const std::string& reduce,
+                       bool include_self,
+                       DenseTensor* output) {
+  auto input_dim = x.dims();
+  auto output_dim = output->dims();
+  auto add_value_dim = add_value.dims();
+
+  const auto& index_type = index.dtype();
+
+  int dim = axis;
+  dim = dim >= 0 ? dim : dim + input_dim.size();
+
+  auto stride_dim = phi::stride(input_dim);
+  int64_t stride = stride_dim[dim];
+  int64_t size = add_value_dim[dim];
+  int64_t delta = input_dim[dim] - size;
+
+  auto* in_data = x.data<T>();
+  T* out_data = ctx.template Alloc<T>(output);
+  auto* add_value_data = add_value.data<T>();
+
+  int64_t numel = add_value.numel();
+  if (numel == 0) {
+    return;
+  }
+
+  auto stream = ctx.stream();
+
+  unsigned int block_dim = PADDLE_CUDA_NUM_THREADS;
+  dim3 grid_dim = dim3((numel + block_dim - 1) / block_dim);
+  phi::backends::gpu::LimitGridDim(ctx, &grid_dim);
+
+  // copy input to output.
+  // todo(@limin29): inplace do not need copy.
+  phi::Copy(ctx, x, ctx.GetPlace(), false, output);
+
+  if (FLAGS_cudnn_deterministic) {
+    VLOG(2) << "Run grad kernel of index_add with single thread.";
+    block_dim = 1;
+    grid_dim.x = 1;
+  }
+
+  if (index_type == phi::DataType::INT64) {
+    const int64_t* index_data = index.data<int64_t>();
+    index_reduce_cuda_kernel<T, int64_t>
+        <<<grid_dim, block_dim, 0, stream>>>(in_data,
+                                             index_data,
+                                             add_value_data,
+                                             numel,
+                                             stride,
+                                             size,
+                                             delta,
+                                             out_data);
+  } else {
+    const int* index_data = index.data<int>();
+    index_reduce_cuda_kernel<T, int>
+        <<<grid_dim, block_dim, 0, stream>>>(in_data,
+                                             index_data,
+                                             add_value_data,
+                                             numel,
+                                             stride,
+                                             size,
+                                             delta,
+                                             out_data);
+  }
+}
+
+template <typename T, typename Context>
+void ScatterKernel(const Context& ctx,
+                   const DenseTensor& x,
+                   const DenseTensor& index,
+                   const DenseTensor& updates,
                    bool overwrite,
                    int axis,
-                   const std::string &reduce,
+                   const std::string& reduce,
                    bool include_self,
-                   DenseTensor *out) {
-  phi::Copy(ctx, x, ctx.GetPlace(), false, out);
-  // use template class to support int32_t and int64_t
+                   DenseTensor* out) {
   auto index_type = index.dtype();
-  bool index_type_match =
-      index_type == phi::DataType::INT32 || index_type == phi::DataType::INT64;
-  PADDLE_ENFORCE_EQ(index_type_match,
-                    true,
-                    phi::errors::InvalidArgument(
-                        "scatter_op Index holds the wrong type, it holds [%s],"
-                        "but desires to be [%s] or [%s].",
-                        index_type,
-                        phi::DataType::INT32,
-                        phi::DataType::INT64));
-  PADDLE_ENFORCE_EQ(reduce == "sum" || reduce == "mul",
-                    true,
-                    phi::errors::InvalidArgument(
-                        "scatter_op Reduce holds the wrong val, it holds [%s],"
-                        "but desires to be [%s] or [%s].",
-                        index_type,
-                        "sum",
-                        "mul"));
-  int reduce_type = reduce == "sum" ? 0 : 1;
-  if (index_type == phi::DataType::INT32) {
-    phi::funcs::GPUScatterAssign<T, int32_t>(
-        ctx, updates, index, out, overwrite, reduce_type);
-  } else {
-    phi::funcs::GPUScatterAssign<T, int64_t>(
-        ctx, updates, index, out, overwrite, reduce_type);
+  PADDLE_ENFORCE_EQ(
+      index_type == phi::DataType::INT32 || index_type == phi::DataType::INT64,
+      true,
+      phi::errors::InvalidArgument(
+          "scatter_op Index holds the wrong type, it holds [%s],"
+          "but desires to be [%s] or [%s].",
+          index_type,
+          phi::DataType::INT32,
+          phi::DataType::INT64));
+
+  PADDLE_ENFORCE_EQ(
+      reduce == "add" || reduce == "mul" || reduce == "muliply" ||
+          reduce == "mean" || reduce == "amin" || reduce == "amax",
+      true,
+      phi::errors::InvalidArgument(
+          "Reduce holds the wrong value, it holds [%s],"
+          "but desires to be add, mul, multiply, mean, amin, amax.",
+          reduce));
+
+  std::string reducer = reduce;
+  if (overwrite) {
+    reducer = "assign";
   }
+
+  IndexReduceKernel<T, Context>(
+      ctx, x, index, updates, axis, reducer, include_self, out);
+
+  // phi::Copy(ctx, x, ctx.GetPlace(), false, out);
+  // int reduce_type = reduce == "sum" ? 0 : 1;
+  // if (index_type == phi::DataType::INT32) {
+  //   phi::funcs::GPUScatterAssign<T, int32_t>(
+  //       ctx, updates, index, out, overwrite, reduce_type);
+  // } else {
+  //   phi::funcs::GPUScatterAssign<T, int64_t>(
+  //       ctx, updates, index, out, overwrite, reduce_type);
+  // }
 }
 
 }  // namespace phi
