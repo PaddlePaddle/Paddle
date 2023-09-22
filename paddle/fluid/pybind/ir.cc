c6d01632ee7f44bc17e05ae8b47670eb4fae5524
Ryan <44900829+DrRyanHuang@users.noreply.github.com>
2023-09-22 00:33:23 +0000

    pir => ir

--- paddle/fluid/pybind/ir.cc
+++ paddle/fluid/pybind/ir.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/pybind/pir.h"
+#include "paddle/fluid/pybind/ir.h"
 #include <Python.h>
 #include <algorithm>
 #include <memory>
