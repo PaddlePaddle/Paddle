7d11daa3bcf927c58037dcf3337fc2159fa86721
Wang Xin <xinwang614@gmail.com>
2023-09-25 03:38:42 +0000

    fix bug

--- test/legacy_test/test_rms_norm_op.py
+++ test/legacy_test/test_rms_norm_op.py
@@ -342,7 +342,7 @@ class TestRMSNormStaticOp(unittest.TestCase):
             )
         return out_s[0], paddle_naive_rmsnorm_out
 
-    def test_rmsnorm_newir(self):
+    def test_rmsnorm_pir(self):
         paddle.disable_static()
         x = paddle.to_tensor(self.x_np.astype("float32"))
         gamma = paddle.to_tensor(self.norm_weight_np.astype("float32"))
@@ -351,7 +351,7 @@ class TestRMSNormStaticOp(unittest.TestCase):
         paddle_naive_rmsnorm_out = naive_rms_norm(x, gamma, beta, self.epsilon)
         paddle.enable_static()
 
-        with paddle.new_ir_utils.IrGuard():
+        with paddle.pir_utils.IrGuard():
             x_static = paddle.static.data(
                 name="x_static", shape=[self.batch, self.cols], dtype="float32"
             )
@@ -361,7 +361,7 @@ class TestRMSNormStaticOp(unittest.TestCase):
             beta_static = paddle.static.data(
                 name="beta_static", shape=[self.cols], dtype="float32"
             )
-            outs = paddle.incubate.nn.functional.fused_rms_norm(
+            out, _ = paddle.incubate.nn.functional.fused_rms_norm(
                 x_static,
                 gamma_static,
                 beta_static,
@@ -375,11 +375,11 @@ class TestRMSNormStaticOp(unittest.TestCase):
                     "gamma_static": self.norm_weight_np.astype("float32"),
                     "beta_static": self.norm_bias_np.astype("float32"),
                 },
-                fetch_list=[outs],
+                fetch_list=[out],
             )
 
         np.testing.assert_allclose(
-            out_s[0].numpy(),
+            out_s[0],
             paddle_naive_rmsnorm_out.numpy(),
             rtol=1e-3,
             atol=1e-3,
