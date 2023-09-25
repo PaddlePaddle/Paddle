87b5c9f4b0e5c6dc610651ca3d149424edf2e9d4
iLeGend <824040212@qq.com>
2023-09-25 10:44:35 +0800

    [Doctest]fix No.219-223, test=docs_preview (#57601)

--- python/paddle/distributed/fleet/layers/mpu/mp_ops.py
+++ python/paddle/distributed/fleet/layers/mpu/mp_ops.py
@@ -809,19 +809,19 @@ def split(
     Examples:
         .. code-block:: python
 
-            # required: distributed
-            import paddle
-            import paddle.distributed.fleet as fleet
+            >>> # doctest: +REQUIRES(env:DISTRIBUTED)
+            >>> import paddle
+            >>> import paddle.distributed.fleet as fleet
 
-            paddle.enable_static()
-            paddle.set_device('gpu:%d'%paddle.distributed.ParallelEnv().dev_id)
-            fleet.init(is_collective=True)
-            data = paddle.randint(0, 8, shape=[10,4])
-            emb_out = paddle.distributed.split(
-                data,
-                (8, 8),
-                operation="embedding",
-                num_partitions=2)
+            >>> paddle.enable_static()
+            >>> paddle.set_device('gpu:%d'%paddle.distributed.ParallelEnv().dev_id)
+            >>> fleet.init(is_collective=True)
+            >>> data = paddle.randint(0, 8, shape=[10,4])
+            >>> emb_out = paddle.distributed.split(
+            ...     data,
+            ...     (8, 8),
+            ...     operation="embedding",
+            ...     num_partitions=2)
 
     """
     assert isinstance(size, (list, tuple)), (
