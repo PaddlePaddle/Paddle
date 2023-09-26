1cec81dc566d347bd582aa3aaba259137e651a10
andsonder <changlu@keter.top>
2023-09-26 09:13:10 +0000

    fix build error

--- paddle/phi/kernels/cpu/quantize_linear_kernel.cc
+++ paddle/phi/kernels/cpu/quantize_linear_kernel.cc
@@ -19,7 +19,84 @@
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/common/type_traits.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/funcs/eigen/common.h"
 #include "paddle/phi/kernels/impl/quantize_linear_impl.h"
+
+namespace phi {
+
+template <typename T>
+struct DequantizeFunctor<phi::CPUContext, T> {
+  void operator()(const phi::CPUContext& dev_ctx,
+                  const phi::DenseTensor* in,
+                  const phi::DenseTensor* scale,
+                  T max_range,
+                  phi::DenseTensor* out) {
+    auto in_e = phi::EigenVector<T>::Flatten(*in);
+    const T* scale_factor = scale->data<T>();
+    auto out_e = phi::EigenVector<T>::Flatten(*out);
+
+    auto& dev = *dev_ctx.eigen_device();
+    out_e.device(dev) = in_e * scale_factor[0] / max_range;
+  }
+};
+
+template <typename T>
+struct ChannelDequantizeFunctorV2<phi::CPUContext, T> {
+  void operator()(const phi::CPUContext& dev_ctx,
+                  const phi::DenseTensor* in,
+                  const phi::DenseTensor* scale,
+                  T max_range,
+                  const int quant_axis,
+                  phi::DenseTensor* out) {
+    // Dequant op is before quantized op
+    // Dequantize the weight of quantized op
+    auto in_dims = in->dims();
+    const int64_t channel = in_dims[quant_axis];
+    const T* scale_factor = scale->data<T>();
+    if (quant_axis == 0) {
+      for (int64_t i = 0; i < channel; i++) {
+        T s = scale_factor[i];
+        phi::DenseTensor one_channel_in = in->Slice(i, i + 1);
+        phi::DenseTensor one_channel_out = out->Slice(i, i + 1);
+        auto in_e = phi::EigenVector<T>::Flatten(one_channel_in);
+        auto out_e = phi::EigenVector<T>::Flatten(one_channel_out);
+        auto& dev = *dev_ctx.eigen_device();
+        out_e.device(dev) = in_e * s / max_range;
+      }
+    } else if (quant_axis == 1) {
+      int64_t out_iter = 1;
+      for (int i = 0; i < quant_axis; i++) {
+        out_iter *= in_dims[i];
+      }
+      int64_t step_i = in->numel() / out_iter;
+      int64_t step_j = in->numel() / (out_iter * channel);
+      auto* in_data = in->data<T>();
+      auto* out_data = dev_ctx.Alloc<T>(out, out->numel() * sizeof(T));
+      for (int64_t i = 0; i < out_iter; i++) {
+        for (int64_t j = 0; j < channel; j++) {
+          auto* cur_in = in_data + i * step_i + j * step_j;
+          auto* cur_out = out_data + i * step_i + j * step_j;
+          T s = scale_factor[j];
+          for (int64_t k = 0; k < step_j; k++) {
+            *cur_out = (*cur_in) * s / max_range;
+            ++cur_in;
+            ++cur_out;
+          }
+        }
+      }
+    }
+  }
+};
+
+template struct DequantizeFunctor<phi::CPUContext, phi::dtype::float16>;
+template struct DequantizeFunctor<phi::CPUContext, float>;
+template struct DequantizeFunctor<phi::CPUContext, double>;
+template struct ChannelDequantizeFunctorV2<phi::CPUContext,
+                                           phi::dtype::float16>;
+template struct ChannelDequantizeFunctorV2<phi::CPUContext, float>;
+template struct ChannelDequantizeFunctorV2<phi::CPUContext, double>;
+
+}  // namespace phi
 
 PD_REGISTER_KERNEL(dequantize_linear,
                    CPU,
--- paddle/phi/kernels/impl/quantize_linear_impl.h
+++ paddle/phi/kernels/impl/quantize_linear_impl.h
@@ -18,74 +18,26 @@
 
 #include "paddle/phi/kernels/quantize_linear_kernel.h"
 
-#include "paddle/phi/backends/gpu/gpu_context.h"
-#include "paddle/phi/backends/gpu/gpu_primitives.h"
+#include "paddle/phi/backends/gpu/gpu_launch_config.h"
+#include "paddle/phi/common/place.h"
+#include "paddle/phi/core/hostdevice.h"
 #include "paddle/phi/core/tensor_utils.h"
 #include "paddle/phi/kernels/cast_kernel.h"
 
 namespace phi {
 
-template <typename T>
-__global__ void KeDequantize(
-    const T* in, const T* scale, T max_range, int64_t num, T* out) {
-  int64_t idx = threadIdx.x + blockIdx.x * blockDim.x;
-  for (int64_t i = idx; i < num; i += blockDim.x * gridDim.x) {
-    out[i] = in[i] * scale[0] / max_range;
-  }
-}
-
-template <typename T>
-__global__ void DequantizeOneScaleQuantAxisN(const T* in,
-                                             const T* scale,
-                                             const T max_range,
-                                             const int64_t num,
-                                             const int n_scales,
-                                             const int quant_stride,
-                                             T* out) {
-  int64_t idx = blockDim.x * blockIdx.x + threadIdx.x;
-  for (int64_t i = idx; i < num; i += blockDim.x * gridDim.x) {
-    T s = scale[(i / quant_stride) % n_scales];
-    out[i] = in[i] * s / max_range;
-  }
-}
-
-template <typename DeviceContext, typename T>
+template <typename Context, typename T>
 struct DequantizeFunctor {
-  void operator()(const DeviceContext& dev_ctx,
+  void operator()(const Context& dev_ctx,
                   const phi::DenseTensor* in,
                   const phi::DenseTensor* scale,
                   T max_range,
                   phi::DenseTensor* out);
 };
 
-template <typename T>
-struct DequantizeFunctor<phi::GPUContext, T> {
-  void operator()(const phi::GPUContext& dev_ctx,
-                  const phi::DenseTensor* in,
-                  const phi::DenseTensor* scale,
-                  T max_range,
-                  phi::DenseTensor* out) {
-    const T* in_data = in->data<T>();
-    const T* scale_factor = scale->data<T>();
-    T* out_data = dev_ctx.Alloc<T>(out, out->numel() * sizeof(T));
-
-    int64_t num = in->numel();
-    int64_t block_size = std::min(
-        num, static_cast<int64_t>(dev_ctx.GetMaxThreadsPerBlock() / 4));
-    int64_t max_threads =
-        dev_ctx.GetMaxPhysicalThreadCount();  // SM * block_per_SM
-    const int64_t max_blocks =
-        std::max(((max_threads - 1) / block_size + 1), static_cast<int64_t>(1));
-    const int64_t grid_size =
-        std::min(max_blocks, (num + block_size - 1) / block_size);
-    KeDequantize<T><<<grid_size, block_size, 0, dev_ctx.stream()>>>(
-        in_data, scale_factor, max_range, num, out_data);
-  }
-};
-
-template <typename DeviceContext, typename T>
+template <typename Context, typename T>
 struct ChannelDequantizeFunctorV2 {
-  void operator()(const DeviceContext& dev_ctx,
+  void operator()(const Context& dev_ctx,
                   const phi::DenseTensor* in,
                   const phi::DenseTensor** scales,
                   const int scale_num,
@@ -93,51 +45,6 @@ struct ChannelDequantizeFunctorV2 {
                   const int quant_axis,
                   phi::DenseTensor* out);
 };
-
-template <typename T>
-struct ChannelDequantizeFunctorV2<phi::GPUContext, T> {
-  void operator()(const phi::GPUContext& dev_ctx,
-                  const phi::DenseTensor* in,
-                  const phi::DenseTensor* scale,
-                  T max_range,
-                  const int quant_axis,
-                  phi::DenseTensor* out) {
-    auto in_dims = in->dims();
-    const T* in_data = in->data<T>();
-    T* out_data = dev_ctx.Alloc<T>(out, out->numel() * sizeof(T));
-    int64_t num = in->numel();
-    const T* scale_factor = scale->data<T>();
-    int64_t block_size = std::min(
-        num, static_cast<int64_t>(dev_ctx.GetMaxThreadsPerBlock() / 4));
-    int64_t max_threads =
-        dev_ctx.GetMaxPhysicalThreadCount();  // SM * block_per_SM
-    const int64_t max_blocks =
-        std::max(((max_threads - 1) / block_size + 1), static_cast<int64_t>(1));
-    const int64_t grid_size =
-        std::min(max_blocks, (num + block_size - 1) / block_size);
-
-    int quant_stride = 1;
-    for (int i = quant_axis + 1; i < in_dims.size(); i++) {
-      quant_stride *= in_dims[i];
-    }
-
-    DequantizeOneScaleQuantAxisN<T>
-        <<<grid_size, block_size, 0, dev_ctx.stream()>>>(in_data,
-                                                         scale_factor,
-                                                         max_range,
-                                                         num,
-                                                         in_dims[quant_axis],
-                                                         quant_stride,
-                                                         out_data);
-  }
-};
-
-template struct DequantizeFunctor<phi::GPUContext, phi::dtype::float16>;
-template struct DequantizeFunctor<phi::GPUContext, float>;
-template struct DequantizeFunctor<phi::GPUContext, double>;
-template struct ChannelDequantizeFunctorV2<phi::GPUContext, float16>;
-template struct ChannelDequantizeFunctorV2<phi::GPUContext, float>;
-template struct ChannelDequantizeFunctorV2<phi::GPUContext, double>;
 
 template <typename T, typename Context, typename D>
 void DeQuantizeLinearImpl(const Context& dev_ctx,
