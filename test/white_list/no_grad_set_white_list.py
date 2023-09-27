f45f76d94ba96a72a64adc601351392795007e3e
Wang Huan <wanghuan29@baidu.com>
2023-09-27 08:12:55 +0000

    refine

--- test/white_list/no_grad_set_white_list.py
+++ test/white_list/no_grad_set_white_list.py
@@ -48,6 +48,7 @@ NEED_TO_FIX_OP_LIST = [
     'fused_elemwise_activation',
     'fused_emb_seq_pool',
     'fused_embedding_seq_pool',
+    'gru_unit',
     'hierarchical_sigmoid',
     'hsigmoid',
     'huber_loss',
