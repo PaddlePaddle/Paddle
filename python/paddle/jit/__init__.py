49933ae155b0714414ae0771d8302c697e6c3b14
SigureMo <sigure.qaq@gmail.com>
2023-09-28 01:28:34 +0800

    remove from . import sot

--- python/paddle/jit/__init__.py
+++ python/paddle/jit/__init__.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import sot
 from .api import save
 from .api import load
 from .api import to_static
