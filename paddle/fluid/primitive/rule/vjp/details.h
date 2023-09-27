e60d7eeb9b215c2413597bc4b18d801f09ae79df
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-09-27 09:42:08 +0000

    polish code

--- paddle/fluid/primitive/rule/vjp/details.h
+++ paddle/fluid/primitive/rule/vjp/details.h
@@ -205,9 +205,9 @@ void reshape_grad(const Tensor& xshape,
                   Tensor* grad_x) {
   if (grad_x) {
     // xshape: [0] + x.shape
-    auto shape = phi::vectorize(xshape.dims());
-    shape.erase(shape.begin());
-    auto grad_x_tmp = reshape<T>(grad_out, shape);
+    auto xshape_dims = xshape.dims();
+    auto x_dims = phi::slice_ddim(xshape_dims, 1, xshape_dims.size());
+    auto grad_x_tmp = reshape<T>(grad_out, phi::vectorize(x_dims););
     set_output<T>(grad_x_tmp, grad_x);
   }
 }
