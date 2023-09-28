780e10f1a7670f8e80c51ff1f6dca4b18dfcecdb
YuanRisheng <yuanrisheng@baidu.com>
2023-09-28 06:45:46 +0000

    fix windows bugs

--- cmake/external/cccl.cmake
+++ cmake/external/cccl.cmake
@@ -2,6 +2,10 @@ include(external/cpm)
 
 # This will automatically clone CCCL from GitHub and make the exported cmake targets available
 cpmaddpackage(
-  NAME CCCL GITHUB_REPOSITORY nvidia/cccl GIT_TAG
-  main # Fetches the latest commit on the main branch
+  NAME
+  CCCL
+  GITHUB_REPOSITORY
+  nvidia/cccl
+  GIT_TAG
+  1f6e4bcae0fbf1bbed87f88544d8d2161c490fc1 # The latest commit has bugs in windows, so we set a fix commit.
 )
