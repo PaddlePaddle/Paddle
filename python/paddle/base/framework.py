2b8d07088537e01a24a464e57c5c46b8031f27b5
Kaedeharai <Kaedehara92693@outlook.com>
2023-10-08 09:10:13 +0800

    [CodeStyle][task 8] enable Ruff C408 rule in python/paddle/base

--- python/paddle/base/framework.py
+++ python/paddle/base/framework.py
@@ -12,32 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import textwrap
 import collections
-import copy
-import functools
-import multiprocessing
+from collections.abc import Iterable
+from .wrapped_decorator import signature_safe_contextmanager, wrap_decorator
 import os
 import re
-import subprocess
-import sys
-import textwrap
-import threading
 import traceback
-import warnings
-from collections.abc import Iterable
-from types import FunctionType, MethodType
+import copy
+from types import MethodType, FunctionType
 
 import numpy as np
+import subprocess
+import multiprocessing
+import sys
 
-import paddle.version as fluid_version
-from paddle.base.libpaddle import DataType
-
-from .. import pir
-from . import core, unique_name
-from .proto import data_feed_pb2  # noqa: F401
 from .proto import framework_pb2
-from .variable_index import _getitem_static, _setitem_impl_, _setitem_static
-from .wrapped_decorator import signature_safe_contextmanager, wrap_decorator
+from .proto import data_feed_pb2  # noqa: F401
+
+from . import core
+from . import unique_name
+from .. import pir
+from paddle.base.libpaddle import DataType
+import paddle.version as fluid_version
+import warnings
+import functools
+from .variable_index import _getitem_static, _setitem_static, _setitem_impl_
 
 __all__ = []
 
