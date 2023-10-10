72f8c308b4ef067f56a0c49b3266cde98ca27b3d
bapijun <bapijun@gmail.com>
2023-10-10 20:50:24 +0800

    fix the bug

--- python/paddle/tensor/__init__.py
+++ python/paddle/tensor/__init__.py
@@ -328,6 +328,8 @@ from .math import i1  # noqa: F401
 from .math import i1e  # noqa: F401
 from .math import polygamma  # noqa: F401
 from .math import polygamma_  # noqa: F401
+from .math import renorm  # noqa: F401
+from .math import renorm_  # noqa: F401
 
 from .random import multinomial  # noqa: F401
 from .random import standard_normal  # noqa: F401
@@ -376,6 +378,9 @@ from .array import create_array  # noqa: F401
 
 from .einsum import einsum  # noqa: F401
 
+from ..signal import istft  # noqa: F401
+from ..signal import stft  # noqa: F401
+
 # this list used in math_op_patch.py for _binary_creator_
 tensor_method_func = [  # noqa
     'create_parameter',
@@ -409,6 +414,7 @@ tensor_method_func = [  # noqa
     'all',
     'any',
     'asin',
+    'asin_',
     'atan',
     'ceil',
     'ceil_',
@@ -492,8 +498,6 @@ tensor_method_func = [  # noqa
     'add_',
     'subtract',
     'subtract_',
-    'atan',
-    'logsumexp',
     'inverse',
     'log1p',
     'log1p_',
@@ -632,6 +636,7 @@ tensor_method_func = [  # noqa
     'triangular_solve',
     'asinh',
     'atanh',
+    'atanh_',
     'acosh',
     'lu',
     'lu_unpack',
@@ -687,6 +692,61 @@ tensor_method_func = [  # noqa
     'i1e',
     'polygamma',
     'polygamma_',
+    'atan2',
+    'diagflat',
+    'multinomial',
+    'pinv',
+    'renorm',
+    'renorm_',
+    'tan',
+    'tan_',
+    'tril',
+    'tril_',
+    'triu',
+    'triu_',
+    'stft',
+    'istft',
+    'abs_',
+    'acos_',
+    'atan_',
+    'cos_',
+    'cosh_',
+    'sin_',
+    'sinh_',
+    'acosh_',
+    'asinh_',
+    'diag',
+    'eye',
+    'linspace',
+    'fill_constant',
+    'ones',
+    'ones_like',
+    'zeros',
+    'zeros_like',
+    'arange',
+    'full',
+    'full_like',
+    'meshgrid',
+    'empty',
+    'empty_like',
+    'complex',
+    'eigh',
+    'standard_normal',
+    'normal',
+    'uniform',
+    'randn',
+    'rand',
+    'randint',
+    'randint_like',
+    'randperm',
+    'poisson',
+    'searchsorted',
+    'set_printoptions',
+    'array_length',
+    'array_read',
+    'array_write',
+    'create_array',
+    'einsum',
     'normal_',
 ]
 
