14b209f7e7ae6ed058b5614cd9331a9bf8af4285
xingmingyyj <zxm_3791@163.com>
2023-10-11 09:27:37 +0000

    fix_dpsgd_op

--- paddle/fluid/pir/dialect/operator/utils/utils.cc
+++ paddle/fluid/pir/dialect/operator/utils/utils.cc
@@ -26,6 +26,7 @@ const std::unordered_set<std::string> LegacyOpList = {
     "pd_op.fused_bn_add_activation_grad",
     "pd_op.c_sync_calc_stream_",
     "pd_op.c_sync_comm_stream_",
+    "pd_op.dpsgd",
     "pd_op.send_v2",
     "pd_op.recv_v2",
     "pd_op.c_allreduce_sum",
@@ -35,8 +36,7 @@ const std::unordered_set<std::string> LegacyOpList = {
     "pd_op.c_allreduce_max_",
     "pd_op.c_allgather",
     "pd_op.seed",
-    "pd_op.share_data",
-    "pd_op.dpsgd"};
+    "pd_op.share_data"};
 
 enum class AttrType {
   UNDEFINED = 0,
