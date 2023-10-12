9f7b7693d0e392cf3785b7d462a5fbc250e79dc2
yxy <ltyxy@buaa.edu.cn>
2023-10-12 13:05:32 +0000

    add new ir test

---
+++ test/ir/new_ir/pass/pass_test.py
@@ -0,0 +1,209 @@
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+import unittest
+import warnings
+
+import numpy as np
+
+import paddle
+from paddle import base, pir
+from paddle.base import core
+
+
+class PassTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.main_program = paddle.static.Program()
+        self.startup_program = paddle.static.Program()
+        self.feeds = None
+        self.fetch_list = None
+        self.pass_names = None
+        self.pass_attrs = {}
+        self.graph_attrs = {}
+        self.fused_op_type = None
+        self.num_fused_ops = -1
+
+        self.newir_program = None
+
+        np.random.seed(123)
+        random.seed(124)
+
+    def _get_places(self):
+        places = [base.CPUPlace()]
+        if core.is_compiled_with_cuda():
+            places.append(base.CUDAPlace(0))
+        return places
+
+    # def grad(self, var):
+    #     grad_name = var.name + "@GRAD"
+    #     return self.main_program.global_block().var(grad_name)
+
+    # def append_gradients(self, outs):
+    #     with base.program_guard(self.main_program, self.startup_program):
+    #         loss = paddle.mean(outs)
+    #         base.backward.append_backward(loss)
+
+    def check_output(self, startup_on_cpu=False, atol=1e-5):
+        '''
+        Check whether the fetched outputs of the origin program and the
+        optimized program are the same.
+
+        For inference model, the parameters are loaded to CPUPlace first,
+        after apply all specified passes, then copy the parameters to GPUPlace.
+        We can set startup_on_cpu to True to test inference pass.
+        '''
+        places = self._get_places()
+        for place in places:
+            self.check_output_with_place(place, startup_on_cpu, atol)
+
+    def _run_program(self, executor, program):
+        outs = executor.run(
+            program=program,
+            feed=self.feeds,
+            fetch_list=self.fetch_list,
+            return_numpy=False,
+        )
+        outs_np = []
+        outs_lod = []
+        for out in outs:
+            outs_np.append(np.array(out))
+            outs_lod.append(out.lod())
+        return outs_np, outs_lod
+
+    def _apply_ir_passes(self):
+        # graph = core.Graph(self.main_program.desc)
+        # graph.set_not_owned("__param_scope__", base.global_scope())
+        # for attr_name, attr_value in self.graph_attrs.items():
+        #     graph.set(attr_name, attr_value)
+
+        if not isinstance(self.pass_names, list):
+            self.pass_names = [self.pass_names]
+
+        pm = pir.PassManager()
+        for name in self.pass_names:
+            pm.add_pass(name)
+
+        opt_program = self.newir_program.clone()
+
+        pm.run(opt_program)
+
+        return opt_program
+
+    def check_output_with_place(self, place, startup_on_cpu=False, atol=1e-5):
+        '''
+        Check whether the fetched outputs of the origin program and the
+        optimized program are the same.
+
+        For inference model, the parameters are loaded to CPUPlace first,
+        after apply all specified passes, then copy the parameters to GPUPlace.
+        We can set startup_on_cpu to True to test inference pass.
+        '''
+        executor = paddle.static.Executor(place)
+        if startup_on_cpu:
+            # Initialize parameters on CPU
+            cpu_executor = paddle.static.Executor(base.CPUPlace())
+            cpu_executor.run(self.startup_program)
+            outs, lods = self._run_program(cpu_executor, self.main_program)
+        else:
+            executor.run(self.startup_program)
+            outs, lods = self._run_program(executor, self.main_program)
+        self.assertTrue(
+            len(self.fetch_list) == len(outs),
+            "Checking the number of fetchs failed. Expected: {}, Received: {}".format(
+                len(self.fetch_list), len(outs)
+            ),
+        )
+
+        # Parameters may be changed in ir passes.
+        opt_program = self._apply_ir_passes()
+
+        # TODO: check program, unfinished
+        # self.check_program(opt_program)
+
+        if startup_on_cpu and not isinstance(place, base.CPUPlace):
+            warnings.warn(
+                "Parameters are on CPU, and will be transferred to GPU "
+                "automatically by data transform."
+            )
+
+        outs_opt, lods_opt = self._run_program(executor, opt_program)
+        self.assertTrue(
+            len(self.fetch_list) == len(outs_opt),
+            "Checking the number of fetchs failed. Expected: {}, Received: {}".format(
+                len(self.fetch_list), len(outs_opt)
+            ),
+        )
+        for i in range(len(self.fetch_list)):
+            is_allclose = np.allclose(outs_opt[i], outs[i], atol=atol)
+            if not is_allclose:
+                a = outs_opt[i]
+                b = outs[i]
+                diff_mat = np.abs(a - b) / np.abs(a)
+                max_diff = np.max(diff_mat)
+                offset = np.argmax(diff_mat > atol)
+                self.assertTrue(
+                    is_allclose,
+                    "Output (name: %s, shape: %s, dtype: %s) has diff at %s. The maximum diff is %e, first error element is %d, expected %e, but got %e"
+                    % (
+                        self.fetch_list[i].name,
+                        str(self.fetch_list[i].shape),
+                        self.fetch_list[i].dtype,
+                        str(place),
+                        max_diff,
+                        offset,
+                        a.flatten()[offset],
+                        b.flatten()[offset],
+                    ),
+                )
+
+    # _check_fused_ops and check_program are unused in our pass_test.py
+    # so delete them temporary
+
+    def _find_op(self, specified_op, program, block_id):
+        is_find = False
+        for op in program.block(block_id).ops:
+            if specified_op.type == op.type:
+                for name in op.input_names:
+                    if op.input(name) != specified_op.input(name):
+                        break
+                for name in op.output_names:
+                    if op.output(name) != specified_op.output(name):
+                        break
+                for name in op.attr_names:
+                    if op.attr(name) != specified_op.attr(name):
+                        break
+                is_find = True
+                break
+
+        return is_find
+
+    def _find_var(self, specified_var, program, block_id):
+        if not program.block(block_id).has_var(specified_var.name):
+            return False
+
+        var = program.block(block_id).var(specified_var.name)
+        if var.type != specified_var.type:
+            return False
+        if var.dtype != specified_var.dtype:
+            return False
+        if var.lod_level != specified_var.lod_level:
+            return False
+        if var.shape != specified_var.shape:
+            return False
+        if var.persistable != specified_var.persistable:
+            return False
+
+        return True
---
+++ test/ir/new_ir/pass/test_ir_conv2d_bn_fuse_pass.py
@@ -0,0 +1,79 @@
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+from pass_test import PassTest
+
+import paddle
+from paddle import base, pir
+from paddle.base import core
+
+paddle.enable_static()
+
+
+class Conv2dBnFusePassTest(PassTest):
+    def setUp(self):
+        place = core.Place()
+        place.set_place(paddle.CPUPlace())
+        new_scope = paddle.static.Scope()
+        main_program = paddle.static.Program()
+        with paddle.static.scope_guard(new_scope):
+            with paddle.static.program_guard(main_program):
+                x = paddle.static.data(
+                    name='x', shape=[3, 1, 28, 28], dtype='float32'
+                )
+                conv1_1 = paddle.static.nn.conv2d(
+                    input=x,
+                    filter_size=3,
+                    num_filters=32,
+                    stride=1,
+                    padding=1,
+                    act=None,
+                    bias_attr=False,
+                    data_format='NHWC',
+                )
+                bn = paddle.static.nn.batch_norm(
+                    input=conv1_1, act=None, data_layout='NHWC'
+                )
+
+        self.newir_program = pir.translate_to_new_ir(main_program.desc)
+        print(self.newir_program)
+
+        # exe = paddle.static.Executor(place)
+        # out = exe.run(
+        #         main_program,
+        #         feed={"x": np_a},
+        #         fetch_list=[out0.name],
+        #     )
+
+        self.feeds = {"x": np.random.random((3, 1, 28, 28)).astype("float32")}
+        self.fetch_list = [bn]
+        self.pass_names = "conv2d_bn_fuse"
+        self.fused_op_type = "conv2d"
+        # self.num_fused_ops = 2
+
+    def test_check_output(self):
+        use_gpu_set = [False]
+        if core.is_compiled_with_cuda():
+            use_gpu_set.append(True)
+        for use_gpu in use_gpu_set:
+            self.pass_attrs = {"conv2d_bn_fuse": {"use_gpu": use_gpu}}
+            place = base.CUDAPlace(0) if use_gpu else base.CPUPlace()
+            self.check_output_with_place(place, startup_on_cpu=True)
+
+
+if __name__ == "__main__":
+    unittest.main()
