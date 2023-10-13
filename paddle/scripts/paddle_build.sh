ef3d1e1b171825ba7a23c764fb64e41fc810c16c
gouzi <530971494@qq.com>
2023-10-13 08:18:35 +0800

    fix title

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -937,7 +937,7 @@ function check_run_sot_ci() {
     # use "git commit -m 'message, test=sot'" to force ci to run
     COMMIT_RUN_CI=$(git log -1 --pretty=format:"%s"|grep -w "test=sot" || true)
     # check pr title
-    TITLE_RUN_CI= $(curl -s https://github.com/PaddlePaddle/Paddle/pull/{GIT_PR_ID} | grep "<title>" | grep -i "sot" || true)
+    TITLE_RUN_CI= $(curl -s https://github.com/PaddlePaddle/Paddle/pull/${GIT_PR_ID} | grep "<title>" | grep -i "sot" || true)
     if [[ ${COMMIT_RUN_CI} || ${TITLE_RUN_CI} ]];then
         set -x
         return
