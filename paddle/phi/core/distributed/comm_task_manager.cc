c80e71cf619aae04190bcd35534ba0824f237999
hitywt <yuwentao126@126.com>
2023-10-16 10:58:24 +0800

    fix compile

--- paddle/phi/core/distributed/comm_task_manager.cc
+++ paddle/phi/core/distributed/comm_task_manager.cc
@@ -29,12 +29,12 @@
 #include "glog/logging.h"
 #include "paddle/phi/backends/gpu/gpu_info.h"
 #include "paddle/phi/core/distributed/store/store.h"
-#include "paddle/phi/core/distributed/trace_utils.h"
 #include "paddle/phi/core/enforce.h"
 
 #if defined(PADDLE_WITH_NCCL) || defined(PADDLE_WITH_RCCL)
 #include "paddle/phi/core/distributed/comm_task_manager.h"
 #include "paddle/phi/core/distributed/nccl_comm_context.h"
+#include "paddle/phi/core/distributed/trace_utils.h"
 #endif
 
 namespace phi {
