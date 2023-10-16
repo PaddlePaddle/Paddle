66e63e7b471b7f6d836bf900c3109da4626cb40f
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-10-16 06:23:59 +0000

    decomp sink

---
+++ paddle/fluid/pir/dialect/operator/ir/manual_op_decomp.cc
@@ -0,0 +1,77 @@
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "paddle/fluid/pir/dialect/operator/ir/op_attribute.h"
+#include "paddle/fluid/pir/dialect/operator/ir/pd_op.h"
+#include "paddle/fluid/pir/dialect/operator/utils/utils.h"
+#include "paddle/fluid/primitive/composite/composite.h"
+#include "paddle/fluid/primitive/type/lazy_tensor.h"
+#include "paddle/phi/api/include/tensor.h"
+#include "paddle/phi/common/int_array.h"
+#include "paddle/pir/core/builtin_op.h"
+#include "paddle/pir/core/op_base.h"
+
+// TODO(chenzhuo)
+// this file will be generated in pd_op_decomp.cc
+
+namespace paddle {
+namespace dialect {
+using IntArray = paddle::experimental::IntArray;
+
+std::vector<std::vector<pir::OpResult>> MeanOp::Decomp(pir::Operation* op) {
+  MeanOp op_obj = op->dyn_cast<MeanOp>();
+  (void)op_obj;
+
+  VLOG(0) << "Decomp Prepare inputs of mean";
+
+  Tensor x(std::make_shared<primitive::LazyTensor>(op_obj.x()));
+
+  VLOG(0) << "Decomp prepare attributes of mean";
+
+  IntArray axis = op->attribute("axis")
+                      .dyn_cast<paddle::dialect::IntArrayAttribute>()
+                      .data();
+
+  bool keepdim = op->attribute("keepdim").dyn_cast<pir::BoolAttribute>().data();
+  VLOG(0) << "Decomp mean keep_dim " << keepdim;
+
+  VLOG(0) << "Decomp prepare call mean's decomp inteface";
+
+  Tensor op_res =
+      paddle::primitive::details::mean_decomp<primitive::LazyTensor>(
+          x, axis, keepdim);
+  // std::vector<Tensor> tensor_res;
+  // tensor_res.push_back(op_res);
+  auto org_res = op->results();
+  std::vector<std::vector<pir::OpResult>> res(org_res.size());
+  res[0].push_back(
+      std::static_pointer_cast<primitive::LazyTensor>(op_res.impl())
+          ->value()
+          .dyn_cast<pir::OpResult>());
+  // for (size_t i = 0; i < tensor_res.size(); ++i) {
+  //   res[i].resize(tensor_res[i].size());
+  //   for (size_t j = 0; j < tensor_res[i].size(); ++j) {
+  //     if (tensor_res[i][j].defined()) {
+  //       res[i][j] = std::static_pointer_cast<primitive::LazyTensor>(
+  //                       tensor_res[i][j].impl())
+  //                       ->value()
+  //                       .dyn_cast<pir::OpResult>();
+  //     }
+  //   }
+  // }
+  return res;
+}
+
+}  // namespace dialect
+}  // namespace paddle
