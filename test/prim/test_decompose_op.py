f3ed0ff203a8ca00eae8a27d13ae854b66b7e18d
lxd-cumt <1141051934@qq.com>
2023-10-16 10:00:32 +0000

    polish codes

--- test/prim/test_decompose_op.py
+++ test/prim/test_decompose_op.py
@@ -219,172 +219,6 @@ class TestDecomposeOp2(unittest.TestCase):
         np.random.seed(2023)
         self.shape_x = [2, 3]
         self.x = np.random.random(self.shape_x).astype("float32")
-        self.shape_y = [2, 3]
-        self.y = np.random.random(self.shape_y).astype("float32")
-        self.shape_z = [2, 3]
-        self.z = np.random.random(self.shape_z).astype("float32")
-
-    def gelu_net(self, flag=None):
-        mp = paddle.pir.core.Program()
-        sp = paddle.pir.core.Program()
-
-        with paddle.pir_utils.IrGuard(), paddle.pir.core.program_guard(mp, sp):
-            core._set_prim_backward_enabled(False)
-            core._set_prim_forward_enabled(False)
-
-            # construct graph
-            x = paddle.static.data('x', self.shape_x, dtype='float32')
-            x.stop_gradient = False
-            y = paddle.static.data('y', self.shape_y, dtype='float32')
-            y.stop_gradient = False
-            z = paddle.static.data('z', self.shape_z, dtype='float32')
-            z.stop_gradient = False
-            tmp1 = paddle.add(x, y)
-            tmp2 = paddle.multiply(tmp1, z)
-            out = paddle.nn.functional.gelu(tmp2, approximate=False)
-
-            # trace forward output and backward grad
-            global_outputs = [out]
-            global_grads = grad(out, [x, y, z])
-
-            # analysis the graph, and get op mapping infos and variable mapping infos
-            ops = mp.global_block().ops
-            simu_bwd_op_to_fwd_op_map = {  # without update during execution
-                ops[8]: ops[5],
-                ops[9]: ops[4],
-                ops[10]: ops[3],
-            }
-            simu_grad_var_to_var_map = {  # with update during execution
-                ops[8].operand(1).source(): ops[5].result(0),
-                ops[8].result(0): ops[5].operand(0).source(),
-                ops[9].result(0): ops[4].operand(0).source(),
-                ops[9].result(1): ops[4].operand(1).source(),
-                ops[10].result(0): ops[3].operand(0).source(),
-                ops[10].result(1): ops[3].operand(1).source(),
-            }
-
-            # get global outputs and grads info, when decomposing an op that corresponds to global outputs and grads, then update the global outputs and grads
-            (
-                related_fwd_ops,
-                related_fwd_ops_output_indexes,
-            ) = get_graph_outputs_infos(
-                mp.global_block(), global_outputs
-            )  # without update during execution
-            (
-                related_bwd_ops,
-                related_bwd_ops_output_indexes,
-            ) = get_graph_outputs_infos(mp.global_block(), global_grads)
-
-            # setting "decompose" flag means decompsing composite op into primitive ops
-            if flag == "decompose":
-                core._set_prim_forward_enabled(True)
-                core._set_prim_backward_enabled(True)
-                for bwd_op in simu_bwd_op_to_fwd_op_map:
-                    fwd_op = simu_bwd_op_to_fwd_op_map[bwd_op]
-                    fwd_inputs = [x.source() for x in fwd_op.operands()]
-                    fwd_outputs = fwd_op.results()
-
-                    # if bwd_op has custom_vjp rule, then decompose bwd_op firstly and decompose fwd_op secondly
-                    if core.has_custom_vjp(fwd_op):
-                        related_bwd_op_index = related_graph_outputs(
-                            global_grads, related_bwd_ops, bwd_op
-                        )
-                        new_grads = decompose_bwd_op(
-                            mp.global_block(),
-                            bwd_op,
-                            simu_grad_var_to_var_map,
-                            fwd_outputs,
-                            fwd_inputs,
-                        )
-                        if related_bwd_op_index is not None:
-                            replace_graph_outputs(
-                                global_grads,
-                                new_grads,
-                                related_bwd_op_index,
-                                related_bwd_ops_output_indexes,
-                            )
-
-                        related_fwd_op_index = related_graph_outputs(
-                            global_outputs, related_fwd_ops, fwd_op
-                        )
-                        new_fwd_outputs = decompose_fwd_op(
-                            mp.global_block(),
-                            fwd_op,
-                            simu_grad_var_to_var_map,
-                        )
-                        if related_fwd_op_index is not None:
-                            replace_graph_outputs(
-                                global_outputs,
-                                new_fwd_outputs,
-                                related_fwd_op_index,
-                                related_fwd_ops_output_indexes,
-                            )
-                    # if bwd_op has no custom_vjp rule, then decompose fwd_op into a set of primitive ops firstly and decompose bwd_op secondly
-                    else:
-                        related_fwd_op_index = related_graph_outputs(
-                            global_outputs, related_fwd_ops, fwd_op
-                        )
-                        new_fwd_outputs = decompose_fwd_op(
-                            mp.global_block(),
-                            fwd_op,
-                            simu_grad_var_to_var_map,
-                        )
-                        if related_fwd_op_index is not None:
-                            replace_graph_outputs(
-                                global_outputs,
-                                new_fwd_outputs,
-                                related_fwd_op_index,
-                                related_fwd_ops_output_indexes,
-                            )
-
-                        related_bwd_op_index = related_graph_outputs(
-                            global_grads, related_bwd_ops, bwd_op
-                        )
-                        new_grads = decompose_bwd_op(
-                            mp.global_block(),
-                            bwd_op,
-                            simu_grad_var_to_var_map,
-                            new_fwd_outputs,
-                            fwd_inputs,
-                        )
-                        if related_bwd_op_index is not None:
-                            replace_graph_outputs(
-                                global_grads,
-                                new_grads,
-                                related_bwd_op_index,
-                                related_bwd_ops_output_indexes,
-                            )
-
-            # execution
-            print("final graph: ", mp)
-            exe = paddle.static.Executor()
-            outs = exe.run(
-                mp,
-                feed={'x': self.x, 'y': self.y, 'z': self.z},
-                fetch_list=[
-                    global_outputs[0],
-                    global_grads[0],
-                    global_grads[1],
-                    global_grads[2],
-                ],
-            )
-
-        core._set_prim_backward_enabled(False)
-        core._set_prim_forward_enabled(False)
-        return outs
-
-    def test_decompose_gelu_op(self):
-        res_ref = self.gelu_net()
-        res = self.gelu_net("decompose")
-        for ref, actual in zip(res_ref, res):
-            np.testing.assert_allclose(ref, actual, atol=1e-4)
-
-
-class TestDecomposeOp3(unittest.TestCase):
-    def setUp(self):
-        np.random.seed(2023)
-        self.shape_x = [2, 3]
-        self.x = np.random.random(self.shape_x).astype("float32")
 
     def dropout_net(self, flag=None):
         mp = paddle.pir.core.Program()
@@ -526,6 +360,227 @@ class TestDecomposeOp3(unittest.TestCase):
         res = self.dropout_net("decompose")
         # for ref, actual in zip(res_ref, res):
         #     np.testing.assert_allclose(ref, actual, atol=1e-2)
+
+
+def check_param_mappings(param_mappings):
+    for VarDesc, Values in param_mappings.items():
+        if len(Values) < 0 or len(Values) > 1:
+            raise ValueError("currently only support one-to-one param_mappings")
+
+
+def get_new_ir_grad_var_to_var_map(param_mappings, old_ir_grad_var_to_var_map):
+    new_ir_grad_var_to_var_map = {}
+    for grad_var, var in old_ir_grad_var_to_var_map.items():
+        new_grad_var = param_mappings[grad_var][0].get_opresult()
+        new_var = param_mappings[var][0].get_opresult()
+        new_ir_grad_var_to_var_map[new_grad_var] = new_var
+    return new_ir_grad_var_to_var_map
+
+
+def get_fwd_op(bwd_op, grad_var_to_var_map):
+    bwd_op_input_names = bwd_op.get_input_names()
+    for idx, input_name in enumerate(bwd_op_input_names):
+        if input_name == "out_grad":
+            out_grad = bwd_op.operand(idx).source()
+            out = grad_var_to_var_map[out_grad]
+            fwd_op = out.get_defining_op()
+            return fwd_op
+
+    return None
+
+
+def get_gelu_pir_program_and_param_map():
+    shape = [2, 3]
+    mp = paddle.static.Program()
+    with paddle.static.program_guard(mp):
+        # construct forward graph
+        x = paddle.static.data('x', shape, dtype='float32')
+        x.stop_gradient = False
+        y = paddle.static.data('y', shape, dtype='float32')
+        y.stop_gradient = False
+        z = paddle.static.data('z', shape, dtype='float32')
+        z.stop_gradient = False
+        tmp1 = paddle.add(x, y)
+        tmp2 = paddle.multiply(tmp1, z)
+        out = paddle.nn.functional.gelu(tmp2, approximate=False)
+        # construct backward graph
+        gradients = paddle.static.gradients(out, [x, y, z])
+        # get the old_ir_grad_var_to_var map
+        old_ir_grad_var_to_var_map = {
+            'gelu_0.tmp_0@GRAD': 'gelu_0.tmp_0',
+            'elementwise_mul_0@GRAD': 'elementwise_mul_0',
+            'elementwise_add_0@GRAD': 'elementwise_add_0',
+            'z@GRAD': 'z',
+            'x@GRAD': 'x',
+            'y@GRAD': 'y',
+        }
+
+    (
+        newir_program,
+        param_mappings,
+    ) = paddle.pir.translate_to_new_ir_with_param_map(mp.desc)
+    check_param_mappings(param_mappings)
+    new_ir_grad_var_to_var_map = get_new_ir_grad_var_to_var_map(
+        param_mappings, old_ir_grad_var_to_var_map
+    )
+
+    return newir_program, new_ir_grad_var_to_var_map
+
+
+class TestDecomposeOp3(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(2023)
+        self.shape_x = [2, 3]
+        self.x = np.random.random(self.shape_x).astype("float32")
+        self.shape_y = [2, 3]
+        self.y = np.random.random(self.shape_y).astype("float32")
+        self.shape_z = [2, 3]
+        self.z = np.random.random(self.shape_z).astype("float32")
+        print("x: ", self.x)
+        print("y: ", self.y)
+        print("z: ", self.z)
+
+    def gelu_net(self, flag=None):
+        (
+            newir_program,
+            grad_var_to_var_map,
+        ) = get_gelu_pir_program_and_param_map()
+
+        with paddle.pir_utils.IrGuard(), paddle.pir.core.program_guard(
+            newir_program
+        ):
+            core._set_prim_forward_enabled(True)
+            core._set_prim_backward_enabled(True)
+
+            newir_ops = newir_program.global_block().ops
+            decompose_bwd_ops_names = [
+                "pd_op.gelu_grad",
+                "pd_op.add_grad",
+                "pd_op.multiply_grad",
+            ]
+
+            global_outputs = [newir_ops[5].result(0)]
+            global_grads = [
+                newir_ops[-1].result(0),
+                newir_ops[-1].result(1),
+                newir_ops[-2].result(1),
+            ]
+
+            # get global outputs and grads info, when decomposing an op that corresponds to global outputs and grads, then update the global outputs and grads
+            (
+                related_fwd_ops,
+                related_fwd_ops_output_indexes,
+            ) = get_graph_outputs_infos(
+                newir_program.global_block(), global_outputs
+            )  # without update during execution
+            (
+                related_bwd_ops,
+                related_bwd_ops_output_indexes,
+            ) = get_graph_outputs_infos(
+                newir_program.global_block(), global_grads
+            )
+
+            for bwd_op in newir_ops:
+                if (
+                    flag == "decompose"
+                    and bwd_op.name() in decompose_bwd_ops_names
+                ):
+                    fwd_op = get_fwd_op(bwd_op, grad_var_to_var_map)
+                    fwd_inputs = [x.source() for x in fwd_op.operands()]
+                    fwd_outputs = fwd_op.results()
+
+                    # if bwd_op has custom_vjp rule, then decompose bwd_op firstly and decompose fwd_op secondly
+                    if core.has_custom_vjp(fwd_op):
+                        related_bwd_op_index = related_graph_outputs(
+                            global_grads, related_bwd_ops, bwd_op
+                        )
+                        new_grads = decompose_bwd_op(
+                            newir_program.global_block(),
+                            bwd_op,
+                            grad_var_to_var_map,
+                            fwd_outputs,
+                            fwd_inputs,
+                        )
+                        if related_bwd_op_index is not None:
+                            replace_graph_outputs(
+                                global_grads,
+                                new_grads,
+                                related_bwd_op_index,
+                                related_bwd_ops_output_indexes,
+                            )
+
+                        related_fwd_op_index = related_graph_outputs(
+                            global_outputs, related_fwd_ops, fwd_op
+                        )
+                        new_fwd_outputs = decompose_fwd_op(
+                            newir_program.global_block(),
+                            fwd_op,
+                            grad_var_to_var_map,
+                        )
+                        if related_fwd_op_index is not None:
+                            replace_graph_outputs(
+                                global_outputs,
+                                new_fwd_outputs,
+                                related_fwd_op_index,
+                                related_fwd_ops_output_indexes,
+                            )
+
+                    # if bwd_op has no custom_vjp rule, then decompose fwd_op into a set of primitive ops firstly and decompose bwd_op secondly
+                    else:
+                        related_fwd_op_index = related_graph_outputs(
+                            global_outputs, related_fwd_ops, fwd_op
+                        )
+                        new_fwd_outputs = decompose_fwd_op(
+                            newir_program.global_block(),
+                            fwd_op,
+                            grad_var_to_var_map,
+                        )
+                        if related_fwd_op_index is not None:
+                            replace_graph_outputs(
+                                global_outputs,
+                                new_fwd_outputs,
+                                related_fwd_op_index,
+                                related_fwd_ops_output_indexes,
+                            )
+
+                        related_bwd_op_index = related_graph_outputs(
+                            global_grads, related_bwd_ops, bwd_op
+                        )
+                        new_grads = decompose_bwd_op(
+                            newir_program.global_block(),
+                            bwd_op,
+                            grad_var_to_var_map,
+                            new_fwd_outputs,
+                            fwd_inputs,
+                        )
+                        if related_bwd_op_index is not None:
+                            replace_graph_outputs(
+                                global_grads,
+                                new_grads,
+                                related_bwd_op_index,
+                                related_bwd_ops_output_indexes,
+                            )
+
+            # execution
+            print("final graph: ", newir_program)
+            exe = paddle.static.Executor()
+            outs = exe.run(
+                newir_program,
+                feed={'x': self.x, 'y': self.y, 'z': self.z},
+                fetch_list=[
+                    global_outputs[0],
+                    global_grads[0],
+                    global_grads[1],
+                    global_grads[2],
+                ],
+            )
+            core._set_prim_backward_enabled(False)
+            core._set_prim_forward_enabled(False)
+
+        return outs
+
+    def test_decompose_gelu_op(self):
+        res = self.gelu_net("decompose")
 
 
 if __name__ == "__main__":
