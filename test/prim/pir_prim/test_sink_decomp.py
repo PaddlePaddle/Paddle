802a585bf5028eb3f4590aac56c457f7dcd41d9b
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-10-17 01:38:47 +0000

    fix flag

--- test/prim/pir_prim/test_sink_decomp.py
+++ test/prim/pir_prim/test_sink_decomp.py
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import unittest
-
-os.environ["FLAGS_enable_pir_api"] = "true"
 
 import numpy as np
 
