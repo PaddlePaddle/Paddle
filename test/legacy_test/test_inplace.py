a69d1ff7a3b738d02a399b4f8eaeaa413dec264b
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-10-17 23:12:46 +0800

    Update test_inplace.py

--- test/legacy_test/test_inplace.py
+++ test/legacy_test/test_inplace.py
@@ -56,7 +56,7 @@ class TestInplace(unittest.TestCase):
             loss = paddle.nn.functional.relu(var_c + var_d)
             with self.assertRaisesRegex(
                 RuntimeError,
-                f"received tensor_version:{1} != wrapper_version_snapshot:{0}",
+                "received tensor_version:1 != wrapper_version_snapshot:0",
             ):
                 loss.backward()
 
@@ -171,7 +171,7 @@ class TestDygraphInplace(unittest.TestCase):
             loss = paddle.nn.functional.relu(var_c)
             with self.assertRaisesRegex(
                 RuntimeError,
-                f"received tensor_version:{1} != wrapper_version_snapshot:{0}",
+                "received tensor_version:1 != wrapper_version_snapshot:0",
             ):
                 loss.backward()
 
