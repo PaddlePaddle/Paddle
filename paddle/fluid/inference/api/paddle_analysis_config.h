95da675597ab71209a7a0759cdef2d040e155c14
yuanlehome <yuanlehome@163.com>
2023-10-18 06:29:31 +0000

    default to false

--- paddle/fluid/inference/api/paddle_analysis_config.h
+++ paddle/fluid/inference/api/paddle_analysis_config.h
@@ -1309,7 +1309,7 @@ struct PD_INFER_DECL AnalysisConfig {
   bool use_feed_fetch_ops_{true};
   bool ir_debug_{false};
 
-  bool use_new_executor_{true};
+  bool use_new_executor_{false};
 
   bool specify_input_name_{false};
 
