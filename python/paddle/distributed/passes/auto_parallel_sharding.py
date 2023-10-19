f81594f53ceee86491f12c665ac01d274abcb4cc
Leo Chen <chenqiuliang@baidu.com>
2023-10-19 10:48:15 +0800

    fit for flash_attn (#57784)
    * fit for flash_attn
    
    * stash
    
    * use DistributedElementwiseImpl0 to copy dist_attr from input to output
    
    * revert third_party

--- python/paddle/distributed/passes/auto_parallel_sharding.py
+++ python/paddle/distributed/passes/auto_parallel_sharding.py
@@ -1691,11 +1691,10 @@ def re_order_program(block, param_grads, dist_context):
     if is_optimize_op(last_op) and last_op.type in _supported_optimizer_type:
         # record optimizer
         for idx, op in reversed(list(enumerate(block.ops))):
-            if op.type not in _supported_optimizer_type:
-                break
-            assert len(op.input("Param")) == 1
-            pname_to_op[op.input("Param")[0]] = op
-            remove_op_indices.append(idx)
+            if op.type in _supported_optimizer_type:
+                assert len(op.input("Param")) == 1
+                pname_to_op[op.input("Param")[0]] = op
+                remove_op_indices.append(idx)
         assert len(use_order) == len(pname_to_op)
 
         # append new opts
