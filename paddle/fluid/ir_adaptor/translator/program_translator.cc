a9af25161a2caa7c3ef4538df317a4ae7911241f
kangguangli <kangguangli@hotmail.com>
2023-10-20 09:09:04 +0000

    remove debug code

--- paddle/fluid/ir_adaptor/translator/program_translator.cc
+++ paddle/fluid/ir_adaptor/translator/program_translator.cc
@@ -16,8 +16,6 @@
 
 #include <unordered_map>
 
-#include <iostream>
-
 #include "glog/logging.h"
 #include "paddle/fluid/framework/program_desc.h"
 #include "paddle/fluid/framework/var_desc.h"
@@ -287,9 +285,6 @@ void ProgramTranslator::Translate() {
                  legacy_program_->Block(0).OpSize(),
                  &param_map_,
                  program_->block());
-  std::cout << "----------[AllBlockDone]----------------\n";
-  std::cout << *program_ << std::endl;
-  std::cout << "----------[AllBlockDone]----------------\n";
 
   SetParameterFromSingleBlock(legacy_program_->Block(0));
 
@@ -298,18 +293,10 @@ void ProgramTranslator::Translate() {
     SetStopGradientAttributeForAllValue(block);
   }
 
-  std::cout << "----------[StopGradient]----------------\n";
-  std::cout << *program_ << std::endl;
-  std::cout << "----------[StopGradient]----------------\n";
-
   for (size_t block_idx = 0; block_idx < legacy_program_->Size(); block_idx++) {
     const BlockDesc& block = legacy_program_->Block(block_idx);
     SetIsPersisableAttributeForAllValue(block);
   }
-
-  std::cout << "----------[Persistable]----------------\n";
-  std::cout << *program_ << std::endl;
-  std::cout << "----------[Persistable]----------------\n";
 }
 
 void ProgramTranslator::TranslateBlock(
