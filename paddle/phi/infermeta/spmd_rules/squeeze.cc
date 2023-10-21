1fbb2cf5c647e8783b5eb6bca40102bd23accba2
WintersMontagne10335 <22251099@zju.edu.cn>
2023-10-21 11:37:10 +0000

    re-run CI

--- paddle/phi/infermeta/spmd_rules/squeeze.cc
+++ paddle/phi/infermeta/spmd_rules/squeeze.cc
@@ -95,6 +95,7 @@ SpmdInfo SqueezeInferSpmd(const DistMetaTensor& x,
   int x_ndim = x_shape.size();
   auto x_dist_attr_src = x.dist_attr();
   std::vector<int64_t> x_dims_mapping = x_dist_attr_src.dims_mapping();
+
   PADDLE_ENFORCE_EQ(
       x_ndim,
       x_dims_mapping.size(),
