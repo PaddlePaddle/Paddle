18cc3d23428e1f50372031197a6cfc2b8277ab63
phlrain <phliuhongyu@126.com>
2023-10-24 14:04:35 +0000

    fix bug

--- test/cpp/pir/cinn/pir_all_path_test.cc
+++ test/cpp/pir/cinn/pir_all_path_test.cc
@@ -20,7 +20,7 @@
 #include "paddle/cinn/hlir/dialect/operator/ir/manual_op.h"
 #include "paddle/cinn/hlir/dialect/operator/ir/op_dialect.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/cinn_group_lowering_pass.h"
-#include "paddle/cinn/hlir/dialect/operator/transforms/pd_op_to_cinn_op_convert_pass.h"
+#include "paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.h"
 #include "paddle/fluid/framework/new_executor/interpretercore.h"
 #include "paddle/fluid/pir/dialect/operator/ir/op_dialect.h"
 #include "paddle/fluid/pir/dialect/operator/ir/pd_api.h"
