3cc8c1371fa046e9d72af40613a91da46635f816
xingmingyyj <zxm_3791@163.com>
2023-10-24 13:44:46 +0000

    add flag FLAGS_FLAGS_NEW_IR_NO_CHECK

--- paddle/fluid/pir/dialect/operator/utils/utils.cc
+++ paddle/fluid/pir/dialect/operator/utils/utils.cc
@@ -25,7 +25,6 @@ const std::unordered_set<std::string> LegacyOpList = {
     "pd_op.c_sync_calc_stream_",
     "pd_op.c_sync_comm_stream_",
     "pd_op.dpsgd",
-    "pd_op.exponential",
     "pd_op.send_v2",
     "pd_op.recv_v2",
     "pd_op.c_allreduce_sum",
