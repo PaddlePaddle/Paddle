cf98331f519a825e45c20cdbc482b442b34a30da
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-10-25 02:46:19 +0000

    polish

--- paddle/phi/infermeta/spmd_rules/rules.h
+++ paddle/phi/infermeta/spmd_rules/rules.h
@@ -22,6 +22,7 @@ limitations under the License. */
 #include "paddle/phi/infermeta/spmd_rules/flatten.h"
 #include "paddle/phi/infermeta/spmd_rules/layer_norm.h"
 #include "paddle/phi/infermeta/spmd_rules/matmul.h"
+#include "paddle/phi/infermeta/spmd_rules/pow.h"
 #include "paddle/phi/infermeta/spmd_rules/reduction.h"
 #include "paddle/phi/infermeta/spmd_rules/replicated.h"
 #include "paddle/phi/infermeta/spmd_rules/reshape.h"
