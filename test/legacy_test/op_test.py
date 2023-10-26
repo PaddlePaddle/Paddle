e356fabead92c875e50a341375da9de2bc5c79ae
xingmingyyj <zxm_3791@163.com>
2023-10-26 02:15:00 +0000

    fix getenv

--- test/legacy_test/op_test.py
+++ test/legacy_test/op_test.py
@@ -1435,11 +1435,11 @@ class OpTest(unittest.TestCase):
             ), "Fetch result should have same length when executed in pir"
 
             check_method = np.testing.assert_array_equal
-            if os.getenv("FLAGS_NEW_IR_OPTEST_RELAX_CHECK", None):
+            if os.getenv("FLAGS_NEW_IR_OPTEST_RELAX_CHECK", None) == "True":
                 check_method = lambda x, y, z: np.testing.assert_allclose(
                     x, y, err_msg=z, atol=1e-6, rtol=1e-6
                 )
-            if os.getenv("FLAGS_NEW_IR_NO_CHECK", None):
+            if os.getenv("FLAGS_NEW_IR_NO_CHECK", None) == "True":
                 check_method = lambda x, y, err_msg: None
 
             for i in range(len(outs)):
@@ -3370,12 +3370,12 @@ class OpTest(unittest.TestCase):
             )
 
             check_method = np.testing.assert_array_equal
-            if os.getenv("FLAGS_NEW_IR_OPTEST_RELAX_CHECK", None):
+            if os.getenv("FLAGS_NEW_IR_OPTEST_RELAX_CHECK", None) == "True":
                 check_method = lambda x, y, z: np.testing.assert_allclose(
                     x, y, err_msg=z, atol=1e-6, rtol=1e-6
                 )
 
-            if os.getenv("FLAGS_NEW_IR_NO_CHECK", None):
+            if os.getenv("FLAGS_NEW_IR_NO_CHECK", None) == "True":
                 check_method = lambda x, y, err_msg: None
 
             for i in range(len(new_gradients)):
