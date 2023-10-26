853db61d6a82858d692cf24348547377cfe9197b
wanghuancoder <wanghuan29@baidu.com>
2023-10-26 14:10:25 +0800

    [AutoParallel] Support disttensor for Tensor.copy_ (#58369)
    * support disttensor for tensor.copy_

--- test/auto_parallel/test_semi_auto_parallel_single_strategy.py
+++ test/auto_parallel/test_semi_auto_parallel_single_strategy.py
@@ -61,6 +61,16 @@ class TestSemiAutoParallelSingleStrategy(test_base.CommunicationTestDistBase):
                 user_defined_envs=envs,
             )
 
+    def test_simple_net_recompute(self):
+        envs_list = test_base.gen_product_envs_list(
+            self._default_envs, self._changeable_envs
+        )
+        for envs in envs_list:
+            self.run_test_case(
+                "semi_auto_parallel_recompute.py",
+                user_defined_envs=envs,
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
