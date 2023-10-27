a5265fcd84a8288271e29f1280ddaa12dfd49820
luqi <luq@zju.edu.cn>
2023-10-27 07:51:05 +0000

    add acosh

--- python/paddle/tensor/ops.py
+++ python/paddle/tensor/ops.py
@@ -289,7 +289,7 @@ def acosh(x, name=None):
             Tensor(shape=[4], dtype=float32, place=Place(cpu), stop_gradient=True,
             [0.        , 1.76274717, 2.06343699, 2.29243159])
     """
-    if in_dynamic_mode():
+    if in_dynamic_or_pir_mode():
         return _C_ops.acosh(x)
     else:
         check_variable_and_dtype(
