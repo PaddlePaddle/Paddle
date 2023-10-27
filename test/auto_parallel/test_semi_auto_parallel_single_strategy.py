a024e6a10301de9bce8ffdd4a467963693478023
GhostScreaming <mofengshenjieII@163.com>
2023-10-27 17:10:05 +0800

    Remove useless modification.

--- test/auto_parallel/test_semi_auto_parallel_single_strategy.py
+++ test/auto_parallel/test_semi_auto_parallel_single_strategy.py
@@ -71,27 +71,6 @@ class TestSemiAutoParallelSingleStrategy(test_base.CommunicationTestDistBase):
                 user_defined_envs=envs,
             )
 
-    def test_simple_net_single_strategy_with_gradient_hook(self):
-        self._changeable_envs = {"backend": ["gpu"]}
-        envs_list = test_base.gen_product_envs_list(
-            self._default_envs, self._changeable_envs
-        )
-        for envs in envs_list:
-            self.run_test_case(
-                "semi_auto_parallel_simple_net_gradient_hook.py",
-                user_defined_envs=envs,
-            )
-
-    def test_simple_net_clear_gradient(self):
-        envs_list = test_base.gen_product_envs_list(
-            self._default_envs, self._changeable_envs
-        )
-        for envs in envs_list:
-            self.run_test_case(
-                "semi_auto_parallel_clear_gradient.py",
-                user_defined_envs=envs,
-            )
-
 
 if __name__ == "__main__":
     unittest.main()
