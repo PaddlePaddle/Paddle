6ad6f36f4d2c52f4fcad5d815b84d30705cc45e8
AndSonder <changlu@keter.top>
2023-10-27 06:54:36 +0000

    fix win build error

--- paddle/phi/kernels/autotune/gpu_timer.h
+++ paddle/phi/kernels/autotune/gpu_timer.h
@@ -40,12 +40,7 @@
 #define DELTA_EPOCH_IN_MICROSECS 11644473600000000ULL
 #endif
 
-struct timezone {
-  int tz_minuteswest; /* minutes W of Greenwich */
-  int tz_dsttime;     /* type of dst correction */
-};
-
-int gettimeofday(struct timeval *tv, struct timezone *tz) {
+int gettimeofday(struct timeval *tv, void *tz) {
   FILETIME ft;
   unsigned __int64 tmpres = 0;
   static int tzflag = 0;
@@ -62,15 +57,6 @@ int gettimeofday(struct timeval *tv, struct timezone *tz) {
     tmpres -= DELTA_EPOCH_IN_MICROSECS;
     tv->tv_sec = static_cast<int64_t>(tmpres / 1000000UL);
     tv->tv_usec = static_cast<int64_t>(tmpres % 1000000UL);
-  }
-
-  if (NULL != tz) {
-    if (!tzflag) {
-      _tzset();
-      tzflag++;
-    }
-    tz->tz_minuteswest = _timezone / 60;
-    tz->tz_dsttime = _daylight;
   }
 
   return 0;
