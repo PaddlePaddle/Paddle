cc25c8d820cd14eb1ef55317215dba189764840f
liangjianzhong <liangjianzhong@baidu.com>
2023-11-01 16:56:56 +0800

    code style

--- python/paddle/distributed/auto_parallel/static/reshard.py
+++ python/paddle/distributed/auto_parallel/static/reshard.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
+
 import copy
 from collections import OrderedDict
 from functools import reduce
