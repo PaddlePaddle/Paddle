1ceadc5fa003c674246ff82f8af79f38ef08e354
AndSonder <changlu@keter.top>
2023-11-02 11:37:24 +0000

    add step timeline and fix logic when job overlap

--- python/paddle/distributed/auto_parallel/static/profiler_helper_static.py
+++ python/paddle/distributed/auto_parallel/static/profiler_helper_static.py
@@ -19,6 +19,13 @@ from argparse import ArgumentParser
 
 import paddle
 
+color_map = {
+    "forward": "thread_state_running",  # RGB: 126, 200, 148
+    "backward": "rail_idle",  # RGB: 238, 142, 0
+    "optimizer": "rail_response",  # RGB: 238, 142, 0
+    "default": "thread_state_unknown",  # RGB: 199, 155, 125
+}
+
 
 def parse_args():
     parser = ArgumentParser()
@@ -32,92 +39,119 @@ def parse_args():
     return args
 
 
-def process_log_data(log_data, device_id):
+def process_job_log(log_data, device_id):
     log_pattern = r'.*?Profiler Info: Job \((\d+)\), type = (\w+), micro_batch_id = (\d+), job_start_time = (\d+.\d+), job_end_time = (\d+.\d+)'
     matches = re.findall(log_pattern, log_data)
     events = []
-    color_map = {
-        "forward": "thread_state_running",  # RGB: 126, 200, 148
-        "backward": "rail_idle",  # RGB: 238, 142, 0
-        "optimizer": "rail_response",  # RGB: 238, 142, 0
-        "default": "thread_state_unknown",  # RGB: 199, 155, 125
-    }
-    step = 0
+    last_end_time = None
+
     for match in matches:
         job_id, job_type, micro_batch_id, job_start_time, job_end_time = match
         if job_type in ["lr"]:
             continue
 
-        if job_type == "forward" and int(micro_batch_id) == 0:
-            if step > 0:
-                event_step_stop = {
-                    "name": "Step " + str(step - 1),
-                    "cat": "Step",
-                    "ph": "E",
-                    "ts": float(job_start_time.strip()) * 1000,
-                    "pid": "Main",
-                    "tid": "Step" + str(device_id),
-                    "cname": color_map["default"],
-                }
-                events.append(event_step_stop)
+        start_time = float(job_start_time.strip()) * 1000
+        end_time = float(job_end_time.strip()) * 1000
 
-            event_step_start = {
-                "name": "Step " + str(step),
-                "cat": "Step",
-                "ph": "B",
-                "ts": float(job_start_time.strip()) * 1000,
-                "pid": "Main",
-                "tid": "Step" + str(device_id),
-                "cname": color_map["default"],
-            }
-            events.append(event_step_start)
-
-            step += 1
+        if last_end_time is not None:
+            if start_time < last_end_time:
+                end_time = end_time + last_end_time - start_time
+                start_time = last_end_time
 
         event_start = {
-            "name": job_type[0].upper() + "_" + str(job_id),
+            "name": job_type + "_" + str(job_id),
             "cat": job_type,
             "ph": "B",
-            "ts": float(job_start_time.strip()) * 1000,
-            "pid": "Main",
+            "ts": start_time,
+            "pid": 0,
             "tid": "GPU" + str(device_id),
             "cname": color_map[job_type],
         }
         event_end = {
-            "name": job_type[0].upper() + "_" + str(job_id),
+            "name": job_type + "_" + str(job_id),
             "cat": job_type,
             "ph": "E",
-            "pid": "Main",
-            "ts": float(job_end_time.strip()) * 1000,
+            "pid": 0,
+            "ts": end_time,
             "tid": "GPU" + str(device_id),
             "cname": color_map[job_type],
         }
         events.append(event_start)
         events.append(event_end)
 
-    event_step_end = {
-        "name": "Step " + str(step),
-        "cat": "Step",
-        "ph": "E",
-        "ts": events[-1]["ts"],
-        "pid": "Main",
-        "tid": "Step" + str(device_id),
-        "cname": color_map["default"],
-    }
+        last_end_time = end_time
+
     return events
+
+
+def process_step_log(log_data, device_id):
+    start_pattern = r'.*?NVTX range push: (\d+), time: (\d+.\d+)'
+    end_pattern = r'.*?NVTX range pop, time: (\d+.\d+)'
+    start_matches = re.findall(start_pattern, log_data)
+    end_matches = re.findall(end_pattern, log_data)
+
+    step_info = []
+    for start_match, stop_match in zip(start_matches, end_matches):
+        step_id, start_time = start_match
+        stop_time = stop_match
+        if int(step_id) >= len(step_info):
+            for _ in range(int(step_id) - len(step_info) + 1):
+                step_info.append([float('inf'), 0])
+        step_info[int(step_id)] = [start_time, stop_time]
+    return step_info
 
 
 def main():
     args = parse_args()
     all_events = []
+    step_infos = []
     for device_id in args.devices.split(","):
         print(f"Process device {device_id}")
         device_id = int(device_id)
         log_file = os.path.join(args.log_dir, "workerlog." + str(device_id))
         with open(log_file, "r") as f:
             log_data = f.read()
-        events = process_log_data(log_data, device_id)
+        events = process_job_log(log_data, device_id)
+        step_info = process_step_log(log_data, device_id)
         all_events.extend(events)
+
+        if len(step_info) > len(step_infos):
+            for _ in range(len(step_info) - len(step_infos)):
+                step_infos.append([float('inf'), 0])
+        for i, info in enumerate(step_info):
+            start_time = float(info[0].strip()) * 1000
+            stop_time = float(info[1].strip()) * 1000
+
+            step_infos[i][0] = min(step_infos[i][0], start_time)
+            step_infos[i][1] = max(step_infos[i][1], stop_time)
+
+    for i, info in enumerate(step_infos):
+        if info[0] == float('inf'):
+            continue
+        start_time = info[0]
+        if i > 0:
+            start_time = max(start_time, step_infos[i - 1][1])
+        event_start = {
+            "name": "step" + str(i),
+            "cat": "step",
+            "ph": "B",
+            "ts": start_time,
+            "pid": 0,
+            "tid": "Step",
+            "cname": color_map["default"],
+        }
+        event_end = {
+            "name": "step" + str(i),
+            "cat": "step",
+            "ph": "E",
+            "ts": info[1],
+            "pid": 0,
+            "tid": "Step",
+            "cname": color_map["default"],
+        }
+
+        all_events.append(event_start)
+        all_events.append(event_end)
 
     save_path = os.path.join(args.log_dir, "pipeline_profile.json")
     with open(save_path, "w") as f:
@@ -126,33 +160,37 @@ def main():
 
     # support Perfetto format
     save_path = os.path.join(args.log_dir, "pipeline_profile_perfetto.json")
+    all_events.extend(
+        [
+            {
+                "args": {"name": "STEP"},
+                "cat": "__metadata",
+                "name": "thread_name",
+                "ph": "M",
+                "pid": 0,
+                "tid": 2333,
+                "ts": 0,
+            }
+        ]
+    )
     for i in range(len(args.devices.split(","))):
         all_events.extend(
             [
-                {
-                    "args": {"name": "Step"},
-                    "cat": "__metadata",
-                    "name": "thread_name",
-                    "ph": "M",
-                    "pid": "Main",
-                    "tid": i * 2,
-                    "ts": 0,
-                },
                 {
                     "args": {"name": f"GPU:{i}"},
                     "cat": "__metadata",
                     "name": "thread_name",
                     "ph": "M",
-                    "pid": "Main",
-                    "tid": i * 2 + 1,
+                    "pid": 0,
+                    "tid": i + 2334,
                     "ts": 0,
-                },
+                }
             ]
         )
     json_str = json.dumps({"traceEvents": all_events})
     for i in range(len(args.devices.split(","))):
-        json_str = json_str.replace(f'"Step{i}"', f'{i * 2}')
-        json_str = json_str.replace(f'"GPU{i}"', f'{i * 2 + 1}')
+        json_str = json_str.replace('"Step"', '2333')
+        json_str = json_str.replace(f'"GPU{i}"', f'{i + 2334}')
 
     with open(save_path, "w") as f:
         f.write(json_str)
