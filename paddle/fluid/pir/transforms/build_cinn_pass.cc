3aca78880d7fdac7c78dd5c71898c16e901dd32e
Aurelius84 <zhangliujie@baidu.com>
2023-11-03 03:13:38 +0000

    [PIR+CINN]Support all modules of PIR+CINN with Python unittest

--- paddle/fluid/pir/transforms/build_cinn_pass.cc
+++ paddle/fluid/pir/transforms/build_cinn_pass.cc
@@ -14,7 +14,6 @@
 
 #include "paddle/fluid/pir/transforms/build_cinn_pass.h"
 
-#include <algorithm>
 #include <queue>
 #include <regex>
 #include <set>
@@ -532,12 +531,10 @@ std::vector<pir::Value> AnalysisOutputs(GroupOpsVec& group_ops) {  // NOLINT
       outputs.emplace(result);
     }
   }
-  std::vector<pir::Value> results;
-  std::set_symmetric_difference(outputs.begin(),
-                                outputs.end(),
-                                inputs.begin(),
-                                inputs.end(),
-                                std::back_inserter(results));
+  for (auto& input : inputs) {
+    outputs.erase(input);
+  }
+  std::vector<pir::Value> results(outputs.begin(), outputs.end());
   VLOG(3) << "Outputs size for GroupOp " << results.size();
   return results;
 }
@@ -562,14 +559,14 @@ void ReplaceWithGroupOp(pir::Block* block,
   for (auto* op : group_ops) {
     op->MoveTo(group_block, group_block->begin());
   }
-  // step 3: Insert YieldOp for outputs
-  builder.SetInsertionPointToEnd(group_block);
-  builder.Build<::pir::YieldOp>(outputs);
-  // step 4: Replace outputs of inner ops
+  // step 3: Replace outputs of inner ops
   std::vector<pir::OpResult> group_outs = new_group_op->results();
   for (size_t i = 0; i < outputs.size(); ++i) {
     outputs[i].ReplaceAllUsesWith(group_outs[i]);
   }
+  // step 4: Insert YieldOp for outputs
+  builder.SetInsertionPointToEnd(group_block);
+  builder.Build<::pir::YieldOp>(outputs);
 }
 
 class BuildCinnPass : public pir::Pass {
