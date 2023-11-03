f24538f9370aa87a9b083b6964ced4a20e94feb6
Aurelius84 <zhangliujie@baidu.com>
2023-11-03 06:15:59 +0000

    fix conflict

--- python/paddle/jit/dy2static/newir_partial_program.py
+++ python/paddle/jit/dy2static/newir_partial_program.py
@@ -397,7 +397,7 @@ class PartialProgramLayer:
             infer_program = PirPassContext.apply(
                 infer_program, self._build_strategy
             )
-            # TODO(Aurelius84): Support this later
+            # TODO(Aurelius84): Support this later.
             # if self._hooker:
             #     infer_program = self._hooker.after_infer(infer_program)
             return infer_program
