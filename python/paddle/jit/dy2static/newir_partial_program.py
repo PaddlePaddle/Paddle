8073343aa02b5c2e4cd830a3e3c3255cbe8d2283
xiongkun <xiongkun03@baidu.com>
2023-11-05 00:57:31 +0800

    [compat] cached property is not support in 3.7, replace with our implementation (#58667)

--- python/paddle/jit/dy2static/newir_partial_program.py
+++ python/paddle/jit/dy2static/newir_partial_program.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import itertools
-from functools import cached_property
 
 import numpy as np
 
@@ -34,6 +33,20 @@ from . import logging_utils
 from .utils import RETURN_NO_VALUE_MAGIC_NUM, backend_guard
 
 __all__ = []
+
+
+class cached_property:
+    """
+    Descriptor to implement lazy initialization of property.
+    """
+
+    def __init__(self, function):
+        self.function = function
+
+    def __get__(self, instance, cls):
+        val = self.function(instance)
+        setattr(instance, self.function.__name__, val)
+        return val
 
 
 class NestSequence:
