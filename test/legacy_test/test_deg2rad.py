7c9ba019db8ff151fd28df6cf3eae39c64133997
enkilee <luhputu0815@gmail.com>
2023-11-07 09:15:20 +0000

    fix

--- test/legacy_test/test_deg2rad.py
+++ test/legacy_test/test_deg2rad.py
@@ -33,28 +33,26 @@ class TestDeg2radAPI(unittest.TestCase):
         self.out_np = np.deg2rad(self.x_np)
 
     def test_static_graph(self):
-        place = (
-            base.CUDAPlace(0)
-            if core.is_compiled_with_cuda()
-            else base.CPUPlace()
-        )
-        paddle.enable_static()
-        with paddle.static.program_guard(
-            paddle.static.Program(), paddle.static.Program()
-        ):
+        startup_program = base.Program()
+        train_program = base.Program()
+        with base.program_guard(startup_program, train_program):
             x = paddle.static.data(
                 name='input', dtype=self.x_dtype, shape=self.x_shape
             )
-
-            exe = paddle.static.Executor(place)
             out = paddle.deg2rad(x)
-            (res,) = exe.run(
-                paddle.static.default_main_program(),
+
+            place = (
+                base.CUDAPlace(0)
+                if core.is_compiled_with_cuda()
+                else base.CPUPlace()
+            )
+            exe = base.Executor(place)
+            res = exe.run(
+                base.default_main_program(),
                 feed={'input': self.x_np},
                 fetch_list=[out],
             )
-            self.assertTrue((np.array(res[0]) == self.out_np).all())
-        paddle.disable_static()
+            self.assertTrue((np.array(out[0]) == self.out_np).all())
 
     def test_dygraph(self):
         paddle.disable_static()
