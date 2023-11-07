c5a321a49a49d9a563f628486dabadacecdccad3
hitywt <yuwentao126@126.com>
2023-11-07 14:23:15 +0800

    update top_p_sampling api exapmples

--- python/paddle/tensor/search.py
+++ python/paddle/tensor/search.py
@@ -1255,24 +1255,31 @@ def top_p_sampling(x, ps, threshold=None, seed=None, name=None):
         .. code-block:: python
 
             >>> import paddle
+
             >>> x = paddle.randn([2,3])
             >>> print(x)
+            >>> # doctest: +SKIP('Different environments yield different output.')
             Tensor(shape=[2, 3], dtype=float32, place=Place(gpu:0), stop_gradient=True,
             [[ 0.36665046,  0.80056280,  1.20835602],
              [ 1.04112589, -1.37984419,  0.91245371]])
             >>> ps = paddle.randn([2])
             >>> print(ps)
+            >>> # doctest: +SKIP('Different environments yield different output.')
             Tensor(shape=[2], dtype=float32, place=Place(gpu:0), stop_gradient=True,
              [-0.20317549, -0.93559051])
+            >>> # doctest: -SKIP
             >>> value, index = paddle.tensor.top_p_sampling(x, ps)
             >>> print(value)
+            >>> # doctest: +SKIP('Different environments yield different output.')
             Tensor(shape=[2, 1], dtype=float32, place=Place(gpu:0), stop_gradient=True,
             [[1.20835602],
              [1.04112589]])
             >>> print(index)
+            >>> # doctest: +SKIP('Different environments yield different output.')
             Tensor(shape=[2, 1], dtype=int64, place=Place(gpu:0), stop_gradient=True,
             [[2],
              [0]])
+            >>> # doctest: -SKIP
     """
 
     if seed is None:
