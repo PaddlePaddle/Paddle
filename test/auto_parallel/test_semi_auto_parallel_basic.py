d0fdeecfbfcde672249de4b07d49a7a5439a6f91
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-11-07 08:59:30 +0000

    add test

--- test/auto_parallel/test_semi_auto_parallel_basic.py
+++ test/auto_parallel/test_semi_auto_parallel_basic.py
@@ -56,6 +56,16 @@ class TestSemiAutoParallelBasic(test_base.CommunicationTestDistBase):
                 user_defined_envs=envs,
             )
 
+    def test_layernorm_api(self):
+        envs_list = test_base.gen_product_envs_list(
+            self._default_envs, self._changeable_envs
+        )
+        for envs in envs_list:
+            self.run_test_case(
+                "semi_auto_parallel_for_layernorm.py",
+                user_defined_envs=envs,
+            )
+
     def test_reduction_api(self):
         envs_list = test_base.gen_product_envs_list(
             self._default_envs, self._changeable_envs
