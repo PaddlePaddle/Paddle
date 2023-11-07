d4f0721ba24e8d88b6cb1f1c1ed188ac54926a35
Zhan Rongrui <2742392377@qq.com>
2023-11-07 13:38:25 +0000

    add print

--- python/paddle/static/nn/control_flow.py
+++ python/paddle/static/nn/control_flow.py
@@ -16,7 +16,8 @@ import warnings
 from functools import partial, reduce
 
 import paddle
-from paddle.base import core
+from paddle import _C_ops
+from paddle.base import core, in_pir_mode
 from paddle.base.backward import _infer_var_data_type_shape_
 from paddle.base.framework import (
     Operator,
@@ -1745,8 +1746,24 @@ def Print(
         ['uint16', 'float16', 'float32', 'float64', 'int32', 'int64', 'bool'],
         'paddle.static.Print',
     )
+    message = message or ""
+    helper = LayerHelper('print', **locals())
 
-    helper = LayerHelper('print' + "_" + input.name, **locals())
+    if in_pir_mode():
+        return _C_ops.print(
+            input,
+            first_n,
+            message,
+            summarize,
+            print_tensor_name,
+            print_tensor_type,
+            print_tensor_shape,
+            print_tensor_layout,
+            print_tensor_lod,
+            print_phase.upper(),
+            True,
+        )
+
     output = helper.create_variable_for_type_inference(input.dtype)
     helper.append_op(
         type='print',
@@ -1755,7 +1772,7 @@ def Print(
         attrs={
             'first_n': first_n,
             'summarize': summarize,
-            'message': message or "",
+            'message': message,
             'print_tensor_name': print_tensor_name,
             'print_tensor_type': print_tensor_type,
             'print_tensor_shape': print_tensor_shape,
