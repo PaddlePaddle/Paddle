a6e58df67ff1e2f48403de0bff6b0504a52ed6ba
lugimzzz <zhenglujing@baidu.com>
2023-11-08 16:40:29 +0800

    script update

--- bf16_matmul.py
+++ bf16_matmul.py
@@ -1,4 +1,5 @@
 import paddle
+import paddle.nn.functional as F
 import numpy as np
 
 x_np = np.load('hs_0.npy')
@@ -6,6 +7,10 @@ x = paddle.to_tensor(x_np, dtype='bfloat16')
 w_np = np.load('w.npy')
 w = paddle.to_tensor(w_np, dtype='bfloat16')
 
-out = paddle.matmul(x, w, transpose_y=True)
+out = F.linear(x, w.T, bias=None)
+out1 = paddle.matmul(x, w, transpose_y=True)
 print(out)
+print(paddle.all((out-out1)==0))
 np.save('sdpa.npy', out.astype('float32'))
+np.save('sdpa1.npy', out1.astype('float32'))
+import pdb; pdb.set_trace()
--- t_bf16_matmul.py
+++ t_bf16_matmul.py
@@ -1,11 +1,15 @@
 import torch 
+import torch.nn.functional as F
 import numpy as np
 
 x_np = np.load('hs_0.npy')
-x = torch.tensor(x_np, dtype=torch.bfloat16)
+x = torch.tensor(x_np, dtype=torch.bfloat16).to("cuda:0")
 w_np = np.load('w.npy')
-w = torch.tensor(w_np, dtype=torch.bfloat16)
+w = torch.tensor(w_np, dtype=torch.bfloat16).to("cuda:0")
 
-out = torch.matmul(x, w.transpose(0,1))
+out1 = torch.matmul(x, w.transpose(0,1))
+out = F.linear(x, w, bias=None)
 print(out)
-np.save('t_sdpa.npy', out.to(torch.float32))
+print(torch.all((out1-out) == 0))
+np.save('t_sdpa.npy', out.cpu().detach().to(torch.float32))
+np.save('t_sdpa1.npy', out1.cpu().detach().to(torch.float32))
