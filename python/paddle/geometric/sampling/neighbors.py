21b64a1cc39eaa6bb31c7cf55f41f66fef50ce6b
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-11-08 12:59:27 +0800

    Update neighbors.py

--- python/paddle/geometric/sampling/neighbors.py
+++ python/paddle/geometric/sampling/neighbors.py
@@ -15,7 +15,7 @@
 from paddle import _C_ops, _legacy_C_ops
 from paddle.base.data_feeder import check_variable_and_dtype
 from paddle.base.layer_helper import LayerHelper
-from paddle.framework import in_dynamic_mode
+from paddle.framework import in_dynamic_mode, in_dynamic_or_pir_mode
 
 __all__ = []
 
