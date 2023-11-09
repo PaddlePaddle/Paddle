d25f08ef53efd82444b1ca0cc33872e2a93b5e54
0x45f <wangzhen45@baidu.com>
2023-11-09 08:36:05 +0000

    Fix ut

--- test/legacy_test/test_adam_op.py
+++ test/legacy_test/test_adam_op.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
 
-sys.path.append("/workspace/Paddle/test")
 import unittest
 
 import numpy as np
