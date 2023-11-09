76472428c3658ddf48314b5a1209169d4bb2f4eb
zhangxiaoci <zhangxiaoci@baidu.com>
2023-11-09 03:26:04 +0000

    [XPU] support fp16 for c_embedding and c_embedding_grad

--- paddle/phi/kernels/xpu/c_embedding_kernel_grad.cc
+++ paddle/phi/kernels/xpu/c_embedding_kernel_grad.cc
@@ -31,6 +31,7 @@ void CEmbeddingGradKernel(const Context& dev_ctx,
   w_grad->Resize(w.dims());
   dev_ctx.template Alloc(w_grad, w.dtype());
   T* table_grad_data = static_cast<T*>(w_grad->data());
+  using XPUType = typename XPUTypeTrait<T>::Type;
 
   size_t table_t_mem_size = w.numel() * phi::SizeOf(w_grad->dtype());
   size_t table_grad_t_mem_size = w_grad->numel() * phi::SizeOf(w_grad->dtype());
@@ -51,9 +52,9 @@ void CEmbeddingGradKernel(const Context& dev_ctx,
   const auto& index_type = ids.dtype();
   if (index_type == phi::DataType::INT32) {
     r = xpu::embedding_grad(dev_ctx.x_context(),
-                            d_output_data,
+                            reinterpret_cast<const XPUType*>(d_output_data),
                             ids.data<int32_t>(),
-                            table_grad_data,
+                            reinterpret_cast<XPUType*>(table_grad_data),
                             height,
                             width,
                             ids.numel(),
@@ -61,9 +62,9 @@ void CEmbeddingGradKernel(const Context& dev_ctx,
                             static_cast<int32_t>(start_index));
   } else if (index_type == phi::DataType::INT64) {
     r = xpu::embedding_grad(dev_ctx.x_context(),
-                            d_output_data,
+                            reinterpret_cast<const XPUType*>(d_output_data),
                             ids.data<int64_t>(),
-                            table_grad_data,
+                            reinterpret_cast<XPUType*>(table_grad_data),
                             height,
                             width,
                             ids.numel(),
@@ -78,5 +79,9 @@ void CEmbeddingGradKernel(const Context& dev_ctx,
 
 }  // namespace phi
 
-PD_REGISTER_KERNEL(
-    c_embedding_grad, XPU, ALL_LAYOUT, phi::CEmbeddingGradKernel, float) {}
+PD_REGISTER_KERNEL(c_embedding_grad,
+                   XPU,
+                   ALL_LAYOUT,
+                   phi::CEmbeddingGradKernel,
+                   float,
+                   phi::dtype::float16) {}
