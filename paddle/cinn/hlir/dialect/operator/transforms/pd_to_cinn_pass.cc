ae5f9854b4cafa9233425a5043c8d6474dd91048
phlrain <phliuhongyu@126.com>
2023-11-10 07:08:57 +0000

    pir_cinn_support_elementwise_pow_and_reshape

--- paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
@@ -78,12 +78,81 @@ class MaxOpPattern : public pir::drr::DrrPatternBase<MaxOpPattern> {
   }
 };
 
+// class ReshapeOpPattern : public pir::drr::DrrPatternBase<ReshapeOpPattern> {
+//  public:
+//   void operator()(pir::drr::DrrPatternContext *ctx) const override {
+//     // Source Pattern
+//     pir::drr::SourcePattern pattern = ctx->SourcePattern();
+//     const auto &full_int_array =
+//         pattern.Op(paddle::dialect::FullIntArrayOp::name(),
+//                    {{"value", pattern.Attr("axis_info")},
+//                     {"dtype", pattern.Attr("dtype_2")},
+//                     {"place", pattern.Attr("place_2")}});
+
+//     const auto &pd_reshape = pattern.Op(paddle::dialect::ReshapeOp::name(),
+//                                     {});
+//     pattern.Tensor("ret") = pd_reshape(pattern.Tensor("arg0"),
+//     full_int_array());
+
+//     // Result patterns
+//     pir::drr::ResultPattern res = pattern.ResultPattern();
+//     const auto &cinn_reshape =
+//         res.Op(cinn::dialect::ReshapeCOp::name(),
+//                {{"shape", pattern.Attr("axis_info")}});
+//     res.Tensor("ret") = cinn_reshape(res.Tensor("arg0"));
+//   }
+// };
+
+class ReshapeOpPattern
+    : public pir::OpRewritePattern<paddle::dialect::ReshapeOp> {
+ public:
+  using pir::OpRewritePattern<paddle::dialect::ReshapeOp>::OpRewritePattern;
+
+  bool MatchAndRewrite(paddle::dialect::ReshapeOp op,
+                       pir::PatternRewriter &rewriter) const override {
+    auto scale_factor_gen_op =
+        op->operand_source(1).dyn_cast<pir::OpResult>().owner();
+
+    if (auto full_op =
+            scale_factor_gen_op->dyn_cast<paddle::dialect::FullIntArrayOp>()) {
+      // sacle is generator by full op
+      // get attribute value from full op
+
+      auto out_shape_attr =
+          full_op.attribute("value").dyn_cast<pir::ArrayAttribute>().AsVector();
+
+      std::vector<int> vec_out_shape;
+      if (out_shape_attr.size() > 0) {
+        PADDLE_ENFORCE_EQ(
+            out_shape_attr[0].isa<::pir::Int64Attribute>(),
+            true,
+            phi::errors::Unimplemented(
+                "the 0th elementwise MUST be ir::Int64Attribute"));
+        for (size_t i = 0; i < out_shape_attr.size(); ++i) {
+          vec_out_shape.push_back(
+              out_shape_attr[i].dyn_cast<::pir::Int64Attribute>().data());
+        }
+      }
+
+      auto cinn_reshape = rewriter.Build<cinn::dialect::ReshapeCOp>(
+          op->operand_source(0).dyn_cast<pir::OpResult>(), vec_out_shape);
+      rewriter.ReplaceAllUsesWith(op.result(0), cinn_reshape.result(0));
+      rewriter.EraseOp(op);
+      rewriter.EraseOp(full_op);
+
+      return true;
+    }
+    return false;
+  }
+};
+
 PdOpToCinnOpPass::PdOpToCinnOpPass() : pir::Pass("pd_to_cinn_pass", 1) {}
 
 bool PdOpToCinnOpPass::Initialize(pir::IrContext *context) {
   pir::RewritePatternSet ps(context);
   ps.Add(SumOpPattern().Build(context));
   ps.Add(MaxOpPattern().Build(context));
+  ps.Add<ReshapeOpPattern>(context);
 
   patterns_ = ::pir::FrozenRewritePatternSet(std::move(ps));
   return true;
--- test/cpp/pir/cinn/pir_all_path_test.cc
+++ test/cpp/pir/cinn/pir_all_path_test.cc
@@ -222,3 +222,74 @@ TEST(GroupOp, TestBuildLayerNorm) {
   //   auto out_tensor =
   //       executor.local_scope()->FindVar("out@fetch")->Get<phi::DenseTensor>();
 }
+
+std::shared_ptr<::pir::Program> BuildPowerProgram() {
+  ::pir::IrContext* ctx = ::pir::IrContext::Instance();
+  ctx->GetOrRegisterDialect<paddle::dialect::OperatorDialect>();
+  auto program = std::make_shared<::pir::Program>(ctx);
+  ::pir::Builder builder = ::pir::Builder(ctx, program->block());
+
+  auto x = builder
+               .Build<paddle::dialect::FullOp>(std::vector<int64_t>({16, 16}),
+                                               2.0,
+                                               phi::DataType::FLOAT32,
+                                               phi::GPUPlace())
+               .result(0);
+
+  auto factor =
+      builder
+          .Build<paddle::dialect::FullOp>(std::vector<int64_t>({16, 16}),
+                                          2.0,
+                                          phi::DataType::FLOAT32,
+                                          phi::GPUPlace())
+          .result(0);
+
+  auto power =
+      builder.Build<paddle::dialect::ElementwisePowOp>(x, factor).result(0);
+  auto out =
+      builder
+          .Build<paddle::dialect::ReshapeOp>(power, std::vector<int64_t>({-1}))
+          .result(0);
+
+  builder.Build<paddle::dialect::FetchOp>(out, "out", 0);
+  return program;
+}
+
+TEST(GroupOp, TestBuildPower) {
+  // Step 1: Construct pir::Program
+  ::pir::IrContext* ctx = ::pir::IrContext::Instance();
+  std::shared_ptr<::pir::Program> program = BuildPowerProgram();
+  ctx->GetOrRegisterDialect<paddle::dialect::OperatorDialect>();
+  ctx->GetOrRegisterDialect<cinn::dialect::OperatorDialect>();
+
+  program->Print(std::cout);
+  cinn::dialect::ir::PdOp2CinnOpConverter(program.get());
+
+  program->Print(std::cout);
+  pir::PassManager pm(ctx);
+  pm.AddPass(
+      std::make_unique<cinn::dialect::ir::AddBroadcastToElementwisePass>());
+  pm.AddPass(pir::CreateBuildCinnPass());
+  CHECK_EQ(pm.Run(program.get()), true);
+
+  program->Print(std::cout);
+  auto res = cinn::dialect::ir::CINNGroupLoweringPass(program.get());
+
+  res->Print(std::cout);
+  paddle::platform::Place place = paddle::platform::CUDAPlace(0);
+
+  auto kernel_program =
+      paddle::dialect::PdOpLowerToKernelPass(res.get(), place);
+
+  paddle::framework::Scope exe_scope;
+
+  paddle::framework::InterpreterCore executor(
+      place, {"out@fetch"}, kernel_program->block(), &exe_scope);
+
+  executor.Run({}, true);
+
+  auto out_tensor =
+      executor.local_scope()->FindVar("out@fetch")->Get<phi::DenseTensor>();
+
+  std::cerr << out_tensor << std::endl;
+}
