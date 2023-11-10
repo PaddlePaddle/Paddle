8bfb8c416d4fee40be91829eed1e0f8c5a2e0539
AyaseNana <13659110308@163.com>
2023-11-10 21:44:56 +0800

    fix docs

--- python/paddle/vision/transforms/transforms.py
+++ python/paddle/vision/transforms/transforms.py
@@ -315,12 +315,11 @@ class ToTensor(BaseTransform):
 
         .. code-block:: python
 
-            >>> import numpy as np
             >>> from PIL import Image
             >>> import paddle.vision.transforms as T
             >>> import paddle.vision.transforms.functional as F
 
-            >>> fake_img = Image.fromarray((np.random.rand(4, 5, 3) * 255.).astype(np.uint8))
+            >>> fake_img = Image.fromarray((np.random.rand(4, 5, 3) * 255.).astype('uint8'))
             >>> transform = T.ToTensor()
             >>> tensor = transform(fake_img)
             >>> print(tensor.shape)
