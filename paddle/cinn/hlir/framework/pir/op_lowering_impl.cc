80ffd432a661b83909bca0892eee91dd116867cb
hong <43953930+phlrain@users.noreply.github.com>
2023-11-11 20:16:14 +0800

    Pir with cinn align ouptut order (#58905)
    * jit kernel op align cinnn output order
    
    * polish code
    
    * polish comment
    
    * [PIR+CINN]Replace FlattenLoops with Fuse
    
    * update
    
    ---------
    
    Co-authored-by: Aurelius84 <zhangliujie@baidu.com>

--- paddle/cinn/hlir/framework/pir/op_lowering_impl.cc
+++ paddle/cinn/hlir/framework/pir/op_lowering_impl.cc
@@ -288,6 +288,7 @@ std::vector<ir::LoweredFunc> OpLowererImpl::PostProcess(
 
   group->output_names.clear();
   VLOG(3) << "group->output_ops.size(): " << group->output_ops.size();
+  // TODO(phlrain): output values not stable here
   for (auto& op : group->output_ops) {
     // collect all output tensor.
     for (auto opresult : op->results()) {
@@ -298,6 +299,8 @@ std::vector<ir::LoweredFunc> OpLowererImpl::PostProcess(
       if (arg_name_set.count(tensor->buffer->name) != 0) {
         continue;
       }
+
+      group->output_values.push_back(opresult);
       // output arg tensors
       group_func_arg_tensors->push_back(tensor);
       // output args
@@ -403,6 +406,7 @@ std::vector<ir::LoweredFunc> OpLowererImpl::DoOpLower(
   for (const ir::Tensor& tensor : *op_func_arg_tensors) {
     cinn_inputs.push_back(common::CINNValue(ir::Expr(tensor)));
   }
+
   // set tensor name = operand hash name
   auto op_results = op->results();
   for (const auto& result : op_results) {
