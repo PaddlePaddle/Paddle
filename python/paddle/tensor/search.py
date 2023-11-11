fa15709d891903a0eeaf5d60a0cbc3030f64b7d4
coco <1228759711@qq.com>
2023-11-11 15:25:07 +0000

    pir fit for masked_select

--- python/paddle/tensor/search.py
+++ python/paddle/tensor/search.py
@@ -913,21 +913,19 @@ def masked_select(x, mask, name=None):
             >>> print(out.numpy())
             [1. 5. 6. 9.]
     """
-
-    if in_dynamic_mode():
+    check_variable_and_dtype(
+        x,
+        'x',
+        ['float16', 'float32', 'float64', 'int32', 'int64', 'uint16'],
+        'paddle.tensor.search.mask_select',
+    )
+    check_variable_and_dtype(
+        mask, 'mask', ['bool'], 'paddle.tensor.search.masked_select'
+    )
+    if in_dynamic_or_pir_mode():
         return _C_ops.masked_select(x, mask)
-
     else:
         helper = LayerHelper("masked_select", **locals())
-        check_variable_and_dtype(
-            x,
-            'x',
-            ['float16', 'float32', 'float64', 'int32', 'int64', 'uint16'],
-            'paddle.tensor.search.mask_select',
-        )
-        check_variable_and_dtype(
-            mask, 'mask', ['bool'], 'paddle.tensor.search.masked_select'
-        )
         out = helper.create_variable_for_type_inference(dtype=x.dtype)
         helper.append_op(
             type='masked_select',
