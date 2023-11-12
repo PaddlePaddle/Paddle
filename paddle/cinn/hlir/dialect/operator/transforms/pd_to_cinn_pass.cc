f512919dda13da6e01bb9b0e30fef7002ef25ed5
hong <43953930+phlrain@users.noreply.github.com>
2023-11-12 14:07:04 +0800

    [PIR]Pir cinn support elementwise pow and reshape (#58908)
    * pir cinn support scale op
    
    * revert code
    
    * fix bug
    
    * pir_cinn_support_elementwise_pow_and_reshape
    
    * update
    
    * rever some code
    
    * update
    
    * update

--- paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
@@ -130,6 +130,49 @@ class ScaleOpPattern : public pir::OpRewritePattern<paddle::dialect::ScaleOp> {
   }
 };
 
+class ReshapeOpPattern
+    : public pir::OpRewritePattern<paddle::dialect::ReshapeOp> {
+ public:
+  using pir::OpRewritePattern<paddle::dialect::ReshapeOp>::OpRewritePattern;
+
+  bool MatchAndRewrite(paddle::dialect::ReshapeOp op,
+                       pir::PatternRewriter &rewriter) const override {
+    auto scale_factor_gen_op =
+        op->operand_source(1).dyn_cast<pir::OpResult>().owner();
+
+    if (auto full_op =
+            scale_factor_gen_op->dyn_cast<paddle::dialect::FullIntArrayOp>()) {
+      // sacle is generator by full op
+      // get attribute value from full op
+
+      auto out_shape_attr =
+          full_op.attribute("value").dyn_cast<pir::ArrayAttribute>().AsVector();
+
+      std::vector<int> vec_out_shape;
+      if (out_shape_attr.size() > 0) {
+        PADDLE_ENFORCE_EQ(
+            out_shape_attr[0].isa<::pir::Int64Attribute>(),
+            true,
+            phi::errors::Unimplemented(
+                "the 0th elementwise MUST be ir::Int64Attribute"));
+        for (size_t i = 0; i < out_shape_attr.size(); ++i) {
+          vec_out_shape.push_back(
+              out_shape_attr[i].dyn_cast<::pir::Int64Attribute>().data());
+        }
+      }
+
+      auto cinn_reshape = rewriter.Build<cinn::dialect::ReshapeOp>(
+          op->operand_source(0).dyn_cast<pir::OpResult>(), vec_out_shape);
+      rewriter.ReplaceAllUsesWith(op.result(0), cinn_reshape.result(0));
+      rewriter.EraseOp(op);
+      rewriter.EraseOp(full_op);
+
+      return true;
+    }
+    return false;
+  }
+};
+
 class UniformOpPattern : public pir::drr::DrrPatternBase<UniformOpPattern> {
  public:
   void operator()(pir::drr::DrrPatternContext *ctx) const override {
@@ -186,6 +229,7 @@ bool PdOpToCinnOpPass::Initialize(pir::IrContext *context) {
       context);  // NOTE, scale op pattern should before AddBroadcastTo
   ps.Add(SumOpPattern().Build(context));
   ps.Add(MaxOpPattern().Build(context));
+  ps.Add<ReshapeOpPattern>(context);
   // ps.Add(UniformOpPattern().Build(context));
 
   patterns_ = ::pir::FrozenRewritePatternSet(std::move(ps));
