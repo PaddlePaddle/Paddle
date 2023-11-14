451f453612d6af2c5af8a33a71c34057248445eb
lizexu <2694294196@qq.com>
2023-11-14 03:20:07 +0000

    enqueueV3

--- paddle/fluid/operators/tensorrt/tensorrt_engine_op.h
+++ paddle/fluid/operators/tensorrt/tensorrt_engine_op.h
@@ -735,9 +735,16 @@ class TensorRTEngineOp : public framework::OperatorBase {
 #if IS_TRT_VERSION_GE(8500)
         auto x_name = engine->engine()->getBindingName(bind_index);
         auto dims = trt_context->getTensorShape(x_name);
+        int nb_dims = dims.nbDims;
+        for (; nb_dims > 0; nb_dims--) {
+          // some 'x 1' of shape is normal, no need to remove it
+          if (dims.d[nb_dims - 1] != 1 ||
+              nb_dims == origin_output_rank[output_index])
+            break;
+        }
+        for (int i = 0; i < nb_dims; i++) ddim.push_back(dims.d[i]);
 #else
         auto dims = trt_context->getBindingDimensions(bind_index);
-
         int nb_dims = dims.nbDims;
         for (; nb_dims > 0; nb_dims--) {
           // some 'x 1' of shape is normal, no need to remove it
