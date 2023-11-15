0923a15490da11d61ce8461c156c18edd91601cf
haohongxiang <haohongxiang@baidu.com>
2023-11-15 12:17:10 +0000

    add unittest

--- python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/hybrid_parallel_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/hybrid_parallel_optimizer.py
@@ -54,10 +54,10 @@ class HybridParallelClipGrad:
             os.getenv('FLAGS_force_align_vpp_grad_sum_order', '0')
         )
 
-        if hasattr(self._clip, "clip_norm"):
-            assert (
-                self._clip.clip_norm > 0.0
-            ), "It's not supported when clip_norm of grad clip is set to 0."
+        if hasattr(self._clip, "clip_norm") and self._clip.clip_norm <= 0:
+            raise ValueError(
+                "It's not supported when clip_norm of grad clip is set to 0 or negative number."
+            )
 
     def _get_vpp_chunk_num(self, params_grads):
         chunk_num = -1
