b1925681111c258e7f2835a9b9577a3c2f8bd5c3
xiaoye <50870160+xiaoyewww@users.noreply.github.com>
2023-11-15 11:27:21 +0800

    【PIR API adaptor No.172】 Migrate paddle.Tensor.put_along_axis (#58831)

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -4999,10 +4999,10 @@ def put_along_axis(arr, indices, values, axis, reduce='assign'):
         )
     axis = non_negative_axis(arr, axis)
     broadcast_shape = infer_broadcast_shape(arr, indices, axis)
-    if in_dynamic_mode():
+    if in_dynamic_or_pir_mode():
         values = (
             paddle.to_tensor(values)
-            if not isinstance(values, paddle.Tensor)
+            if not isinstance(values, (paddle.Tensor, paddle.pir.OpResult))
             else values
         )
         if broadcast_shape:
