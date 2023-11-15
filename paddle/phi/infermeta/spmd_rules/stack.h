21f1240e0b2f9d03f5cc300560a47431b0da41fa
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-11-15 17:35:33 +0800

    stack, split_with_num infer spmd (#58854)
    * format
    
    * polish
    
    * polish
    
    * polish
    
    * polish
    
    * yaml
    
    * polish
    
    * format
    
    * polish_test
    
    * polish
    
    * code gen
    
    * polish
    
    * polish
    
    * follow comment
    
    * fix ci
    
    * fix ci
    
    * polish
    
    * polish

--- paddle/phi/infermeta/spmd_rules/stack.h
+++ paddle/phi/infermeta/spmd_rules/stack.h
@@ -4,7 +4,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,11 +14,7 @@ limitations under the License. */
 
 #pragma once
 
-#include <iterator>
-#include <map>
-#include <string>
 #include <vector>
-
 #include "paddle/phi/common/scalar.h"
 #include "paddle/phi/core/distributed/auto_parallel/dist_meta_tensor.h"
 #include "paddle/phi/core/distributed/type_defs.h"
@@ -26,26 +22,13 @@ limitations under the License. */
 namespace phi {
 namespace distributed {
 
-SpmdInfo SplitInferSpmd(const DistMetaTensor& x,
-                        const std::vector<int>& sections,
-                        int axis);
+SpmdInfo StackInferSpmd(const std::vector<DistMetaTensor>& x, int axis);
 
-SpmdInfo SplitInferSpmdReverse(const DistMetaTensor& x,
-                               const std::vector<const DistMetaTensor*>& outs,
-                               const std::vector<int>& sections,
+SpmdInfo StackInferSpmdReverse(const std::vector<DistMetaTensor>& x,
+                               const DistMetaTensor& output,
                                int axis);
 
-SpmdInfo SplitWithNumInferSpmd(const DistMetaTensor& x, int num, int axis);
-
-SpmdInfo SplitWithNumInferSpmdDynamic(const DistMetaTensor& x,
-                                      int num,
-                                      const Scalar& axis);
-
-SpmdInfo SplitWithNumInferSpmdReverse(
-    const DistMetaTensor& x,
-    const std::vector<const DistMetaTensor*>& outs,
-    int num,
-    int axis);
+SpmdInfo StackGradInferSpmd(const DistMetaTensor& output_grad, int axis);
 
 }  // namespace distributed
 }  // namespace phi
