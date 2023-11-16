8cb3aa23cdf88ca1f576bb18caefd3ba173fc502
zeroRains <linjunlu@zerorains.top>
2023-11-16 13:08:14 +0000

    change the register name

--- paddle/phi/kernels/fusion/gpu/fused_bias_dropout_residual_layer_norm_grad_kernel.cu
+++ paddle/phi/kernels/fusion/gpu/fused_bias_dropout_residual_layer_norm_grad_kernel.cu
@@ -26,7 +26,7 @@
 namespace phi {
 namespace fusion {
 template <typename T, typename Context>
-void FusedBiasDropoutResidualLnKernel(
+void FusedBiasDropoutResidualLnGradKernel(
     const Context& dev_ctx,
     const DenseTensor& y_grad,
     const DenseTensor& x,
