bf467f98c8f17759f07a11839a3e3072c08075bf
wangruting <wangruting@baidu.com>
2023-11-17 10:59:52 +0000

    modify segment

--- paddle/phi/kernels/funcs/segment_pooling.cc
+++ paddle/phi/kernels/funcs/segment_pooling.cc
@@ -34,11 +34,17 @@ class SegmentPoolFunctor<phi::CPUContext, T, IndexT> {
                   DenseTensor* summed,
                   const std::string pooltype = "SUM") {
     const IndexT* segment_ids = segments.data<IndexT>();
+    std::cout << "00000000" << std::endl;
+    auto summed_ids = dev_ctx.Alloc<int64_t>(summed, 0);
+    std::cout << "11111111" << std::endl;
     auto curent_id = segment_ids[0];
-    summed->Resize(phi::make_ddim(
-        std::vector<int64_t>{segment_ids[segments.numel() - 1] + 1, 1}));
-    auto summed_ids =
-        dev_ctx.Alloc<int64_t>(summed, summed->numel() * sizeof(int64_t));
+    if (pooltype == "MEAN") {
+      summed->Resize(phi::make_ddim(
+          std::vector<int64_t>{segment_ids[segments.numel() - 1] + 1, 1}));
+      summed_ids =
+          dev_ctx.Alloc<int64_t>(summed, summed->numel() * sizeof(int64_t));
+    }
+    std::cout << "22222222" << std::endl;
     int64_t last_idx = 0;
     int64_t w = input.numel() / input.dims()[0];
     auto& place = *dev_ctx.eigen_device();
@@ -60,13 +66,13 @@ class SegmentPoolFunctor<phi::CPUContext, T, IndexT> {
       Tensor in_t = input.Slice(last_idx, idx);
 
       int64_t h = idx - last_idx;
-      summed_ids[curent_id] = h;
       auto in_e = EigenMatrix<T>::From(in_t, phi::make_ddim({h, w}));
       auto out_e = EigenVector<T>::Flatten(out_t);
 
       auto reduce_dim = Eigen::array<int, 1>({{0}});
       if (pooltype == "MEAN") {
         out_e.device(place) = in_e.mean(reduce_dim);
+        summed_ids[curent_id] = h;
       } else if (pooltype == "SUM") {
         out_e.device(place) = in_e.sum(reduce_dim);
       } else if (pooltype == "MAX") {
