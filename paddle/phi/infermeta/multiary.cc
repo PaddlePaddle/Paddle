4ec9d14aeb0685c60e6f3aac10c46ed3ba4063c4
nishirong <1847623494@qq.com>
2023-11-17 09:13:16 +0000

    add op encode_rotary_qk

--- paddle/phi/infermeta/multiary.cc
+++ paddle/phi/infermeta/multiary.cc
@@ -4258,6 +4258,17 @@ void MultiheadMatmulInferMeta(const MetaTensor& input,
   out->set_dtype(input.dtype());
   out->share_lod(input);
 }
+void  EncodeRotaryQKInferMeta(const MetaTensor& q,
+                              const MetaTensor& kv,
+                              const MetaTensor& rotary_emb,
+                              const MetaTensor& seq_lens,
+                              int rotary_emb_dims,
+                              bool use_neox,
+                              MetaTensor* rotary_q_out,
+                              MetaTensor* rotary_kv_out) {
+   rotary_q_out->share_meta(q);
+   rotary_kv_out->share_meta(kv);
+}
 
 void MaskedMultiheadAttentionInferMeta(const MetaTensor& x,
                                        const MetaTensor& cache_kv,
