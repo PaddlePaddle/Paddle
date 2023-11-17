9ec90a1456aec6078b5adf354f8f882a75278ff3
liuzhenhai93 <liuzhenhai93@outlook.com>
2023-11-17 08:28:45 +0000

    polish

--- test/auto_parallel/test_semi_auto_parallel_basic.py
+++ test/auto_parallel/test_semi_auto_parallel_basic.py
@@ -106,6 +106,16 @@ class TestSemiAutoParallelBasic(test_base.CommunicationTestDistBase):
                 user_defined_envs=envs,
             )
 
+    def test_triu_api(self):
+        envs_list = test_base.gen_product_envs_list(
+            self._default_envs, self._changeable_envs
+        )
+        for envs in envs_list:
+            self.run_test_case(
+                "semi_auto_parallel_for_triu.py",
+                user_defined_envs=envs,
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
