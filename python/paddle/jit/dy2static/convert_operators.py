1dfea313c20cf45e6ffd9ec06678cd0558573f09
SigureMo <sigure.qaq@gmail.com>
2023-11-20 13:50:32 +0000

    fix convert_len and convert_range

--- python/paddle/jit/dy2static/convert_operators.py
+++ python/paddle/jit/dy2static/convert_operators.py
@@ -556,7 +556,7 @@ def convert_len(var):
           operations are added in `len` transformation, such as appending
           `shape_op` in var.block.
     """
-    if isinstance(var, (Variable, OpResult)):
+    if isinstance(var, Variable):
         assert var.ndim > 0, "len() of a 0-D tensor is wrong"
         if var.type in [
             core.VarDesc.VarType.LOD_TENSOR,
@@ -575,8 +575,19 @@ def convert_len(var):
                 'len(var) only supports LoDTensor/LoDTensorArray/SelectedRows, but received %s.'
                 % type(var)
             )
+    elif isinstance(var, OpResult):
+        assert var.ndim > 0, "len() of a 0-D tensor is wrong"
+        if var.is_dense_tensor_type() or var.is_selected_row_type():
+            # Note: Length of var may be known ahead of time in dygraph,
+            # but it probably represents batch size which can be variant.
+            # so we return a variable dynamically inferred from var.shape.
+            if var.shape[0] > 0 and var.is_dense_tensor_type():
+                return var.shape[0]
+            return paddle.shape(var)[0]
+        elif var.is_dense_tensor_array_type():
+            return paddle.tensor.array_length(var)
     else:
-        if isinstance(var, (VariableTuple)):
+        if isinstance(var, VariableTuple):
             return var.__len__()
         return len(var)
 
@@ -625,11 +636,11 @@ def convert_range(*args):
     has_variable = any(isinstance(x, (Variable, OpResult)) for x in args)
     if has_variable:
         if len(args) == 1:
-            return paddle.arange(0, args[0], 1, paddle.int64)
+            return paddle.arange(0, args[0], 1, "int64")
         if len(args) == 2:
-            return paddle.arange(args[0], args[1], 1, paddle.int64)
+            return paddle.arange(args[0], args[1], 1, "int64")
         if len(args) == 3:
-            return paddle.arange(args[0], args[1], args[2], paddle.int64)
+            return paddle.arange(args[0], args[1], args[2], "int64")
     return range(*args)
 
 
