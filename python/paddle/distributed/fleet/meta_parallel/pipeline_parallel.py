9067919e8facc4bcff1ff16ec7a1f4bd930463ea
hitywt <yuwentao126@126.com>
2023-11-20 16:59:41 +0800

    fix comments

--- python/paddle/distributed/fleet/meta_parallel/pipeline_parallel.py
+++ python/paddle/distributed/fleet/meta_parallel/pipeline_parallel.py
@@ -178,7 +178,6 @@ class PipelineParallel(MetaParallelBase):
         self.pp_group = self._hcg.get_pipe_parallel_group()
 
         self.dp_group = self._hcg.get_data_parallel_group()
-        self.sharding_group = self._hcg.get_sharding_parallel_group()
 
         # fused sep and dp
         if self.use_sep_parallel:
@@ -240,10 +239,6 @@ class PipelineParallel(MetaParallelBase):
                 "After parsing, a new file 'pipeline_profile.json' will be generated. "
                 "Users can inspect this file by chrome://tracing website."
             )
-
-        assert (
-            not self._dp_comm_overlap and not self._sharding_comm_overlap
-        ), "Comm overlap is not supported now."
 
         if self._dp_comm_overlap:
             assert self.use_data_parallel and self.num_stages > 1
@@ -621,11 +616,6 @@ class PipelineParallel(MetaParallelBase):
         self.lr_scheduler = lr_scheduler
         self._layers.train()
         self.register_sharding_comm_overlap_hook(optimizer)
-
-        if self._sharding_comm_overlap and len(self._comm_buffers) == 0:
-            self.register_allreduce_overlap_hook(
-                self._layers, self.sharding_group, self.accumulate_steps, False
-            )
 
         return data
 
