032abb158b24fc122372f862a45f7374632bf224
bukejiyu <52310069+bukejiyu@users.noreply.github.com>
2023-11-20 17:44:40 +0800

    [Inference] Add conv_fuse_pass, support conv2d+bn -> conv2d (#58724)
    * add conv2d_bn_fuse_pass
    
    ---------
    
    Co-authored-by: yxy <ltyxy@buaa.edu.cn>

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -105,6 +105,7 @@
 #include "paddle/fluid/ir_adaptor/translator/translate.h"
 #include "paddle/fluid/pir/transforms/constant_folding_pass.h"
 #include "paddle/fluid/pir/transforms/dead_code_elimination_pass.h"
+#include "paddle/fluid/pir/transforms/fusion/conv2d_fuse_pass.h"
 #include "paddle/fluid/pir/transforms/inplace_pass.h"
 #include "paddle/fluid/pir/transforms/params_sync_among_devices_pass.h"
 #include "paddle/fluid/pir/transforms/pd_op_to_kernel_pass.h"
@@ -772,6 +773,7 @@ bool AnalysisPredictor::PrepareExecutor() {
       ::pir::PassManager pm_for_op_program(::pir::IrContext::Instance(), 2);
       // TODO(liuyuanle): Uncomment constant_folding_pass after fix it
       // pm_for_op_program.AddPass(::pir::CreateConstantFoldingPass(sub_scope_));
+      pm_for_op_program.AddPass(::pir::CreateConv2dFusePass());
       pm_for_op_program.AddPass(::pir::CreateDeadCodeEliminationPass());
       pm_for_op_program.AddPass(
           ::pir::CreateReplaceFetchWithShadowOutputPass());
