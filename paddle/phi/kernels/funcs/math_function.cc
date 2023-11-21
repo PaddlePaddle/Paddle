9060ef67244fc4bb969582f9b0941e82ffd89c12
danleifeng <danleifeng@163.com>
2023-11-21 12:07:44 +0000

    fix ci error; test=develop

--- paddle/phi/kernels/funcs/math_function.cc
+++ paddle/phi/kernels/funcs/math_function.cc
@@ -28,6 +28,7 @@ limitations under the License. */
 #include <vector>
 
 #include "paddle/phi/backends/cpu/cpu_context.h"
+#include "paddle/phi/backends/context_pool.h"
 #include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/common/float16.h"
