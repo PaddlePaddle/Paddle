74010e912870278c010232578d94ead8b80a1e14
zhangyuqin1998 <75946871+zhangyuqin1998@users.noreply.github.com>
2023-11-21 11:38:32 +0800

    Update run_program_op_node.h

--- paddle/fluid/eager/to_static/run_program_op_node.h
+++ paddle/fluid/eager/to_static/run_program_op_node.h
@@ -205,12 +205,6 @@ static auto GetNameFromValue(const ::pir::Block *block,
                  .dyn_cast<pir::StrAttribute>()
                  .AsString();
       value2name[op.result(0).Value::impl()] = name;
-    } else if (is_input && op.name() == "builtin.get_constant") {
-      name = op.attributes()
-                 .at("constant_name")
-                 .dyn_cast<pir::StrAttribute>()
-                 .AsString();
-      value2name[op.result(0).Value::impl()] = name;
     }
   }
   std::vector<std::string> names;
