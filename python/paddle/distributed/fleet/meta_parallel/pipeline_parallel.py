3bf0dd3bf17ddec6b3294e19f5957c394cb98b17
hitywt <yuwentao126@126.com>
2023-11-21 10:33:53 +0800

    fix comments

--- python/paddle/distributed/fleet/meta_parallel/pipeline_parallel.py
+++ python/paddle/distributed/fleet/meta_parallel/pipeline_parallel.py
@@ -980,12 +980,6 @@ class PipelineParallelWithInterleave(PipelineParallel):
             self._records = []
             self._reset_counter()
 
-    def _assign_vpp_info(self, chunks):
-        chunk_num = len(chunks)
-        for i, chunk in enumerate(chunks):
-            for p in chunk.parameters():
-                p._chunk_info = {"chunk_id": i, "chunk_num": chunk_num}
-
     def _get_virtual_pp_rank(self, micro_step, forward):
         virtual_pp_stage = micro_step % (
             self.num_stages * self.num_model_chunks
