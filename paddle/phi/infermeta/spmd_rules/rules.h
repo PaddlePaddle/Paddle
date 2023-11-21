506a79312873627aae83615ad5dd3938c4b3ad05
Yichen Zhang <zhangyichen03@baidu.com>
2023-11-21 16:22:18 +0800

    add registration of softmax_with_cross_entropy

--- paddle/phi/infermeta/spmd_rules/rules.h
+++ paddle/phi/infermeta/spmd_rules/rules.h
@@ -578,6 +578,10 @@ PD_REGISTER_SPMD_RULE(
     cross_entropy_with_softmax,
     PD_INFER_SPMD(phi::distributed::CrossEntropyWithSoftmaxInferSpmd),
     PD_INFER_SPMD(phi::distributed::CrossEntropyWithSoftmaxInferSpmdReverse));
+PD_REGISTER_SPMD_RULE(
+    softmax_with_cross_entropy,
+    PD_INFER_SPMD(phi::distributed::CrossEntropyWithSoftmaxInferSpmd),
+    PD_INFER_SPMD(phi::distributed::CrossEntropyWithSoftmaxInferSpmdReverse));
 
 }  // namespace distributed
 }  // namespace phi
