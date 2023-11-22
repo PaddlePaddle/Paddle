03c5a4c9ea06f65c66a3478a63d245248744daca
Aurelius84 <zhangliujie@baidu.com>
2023-11-22 15:55:19 +0800

    fix code-style

--- paddle/cinn/hlir/dialect/operator/transforms/group_merge/cinn_group_lowering_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/group_merge/cinn_group_lowering_pass.cc
@@ -161,8 +161,8 @@ class GroupOpPattern : public pir::OpRewritePattern<cinn::dialect::GroupOp> {
         cinn::dialect::ir::GeneralFusionMergePassInternal(op_fusion);
 
     for (auto group : group_list) {
-      auto ir_compiler = hlir::framework::PirCompilerManager::Create(
-            *program, target, scope);
+      auto ir_compiler =
+          hlir::framework::PirCompilerManager::Create(*program, target, scope);
       if (FLAGS_cinn_enable_map_expr) {
         cinn::adt::TryGenerateMapExprFromGroup(group);
       }
