295a55ac4328bb6ffeabf21cb1a18108f5f2e301
kangguangli <kangguangli@hotmail.com>
2023-11-22 16:49:05 +0000

    fix

--- test/dygraph_to_static/test_loop.py
+++ test/dygraph_to_static/test_loop.py
@@ -407,6 +407,7 @@ class TestTransformForLoop(Dy2StTestBase):
                 ret = self.dyfunc(self.len)
             return ret.numpy()
 
+    @test_sot_only
     def test_ast_to_func(self):
         np.testing.assert_allclose(
             self._run_dygraph(), self._run_static(), rtol=1e-05
