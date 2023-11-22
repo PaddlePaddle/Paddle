8a8a9e471916b82567625cecfaf0bb8b70c93f52
zeroRains <linjunlu@zerorains.top>
2023-11-22 02:25:12 +0000

    add in vjp_interface_black_list

--- paddle/fluid/pir/dialect/op_generator/ops_api_gen.py
+++ paddle/fluid/pir/dialect/op_generator/ops_api_gen.py
@@ -70,6 +70,7 @@ OPS_API_TEMPLATE = """
 
 NEED_GEN_STATIC_ONLY_APIS = [
     'fetch',
+    'fused_bias_dropout_residual_layer_norm',
     'fused_embedding_eltwise_layernorm',
     'fused_fc_elementwise_layernorm',
     'fused_multi_transformer_xpu',
