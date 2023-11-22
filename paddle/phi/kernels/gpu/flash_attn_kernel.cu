3056558bbc51e2b1f6963a554bfdeb822ada1447
hitywt <yuwentao126@126.com>
2023-11-22 21:32:06 +0800

    part-3 fix cherry pick PR (#56040)

--- paddle/phi/kernels/gpu/flash_attn_kernel.cu
+++ paddle/phi/kernels/gpu/flash_attn_kernel.cu
@@ -21,11 +21,11 @@
 #include "paddle/phi/core/tensor_utils.h"
 #include "paddle/phi/kernels/arange_kernel.h"
 #include "paddle/phi/kernels/empty_kernel.h"
+#include "paddle/phi/kernels/gpu/flash_attn_utils.h"
 #include "paddle/phi/kernels/reshape_kernel.h"
 
 #ifdef PADDLE_WITH_FLASHATTN
 #include "paddle/phi/backends/dynload/flashattn.h"
-#include "paddle/phi/kernels/gpu/flash_attn_utils.h"
 #endif
 
 namespace phi {
