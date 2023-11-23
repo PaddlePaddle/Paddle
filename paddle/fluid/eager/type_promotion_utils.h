76169122667e6cb4e1d3abe4af3b45304f9eaab9
zxcd <228587199@qq.com>
2023-11-23 06:04:17 +0000

    fix codestyle.

--- paddle/fluid/eager/type_promotion_utils.h
+++ paddle/fluid/eager/type_promotion_utils.h
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#pragma once
 #include "paddle/fluid/eager/api/generated/eager_generated/forwards/dygraph_functions.h"
 #include "paddle/fluid/eager/api/utils/global_utils.h"
 
