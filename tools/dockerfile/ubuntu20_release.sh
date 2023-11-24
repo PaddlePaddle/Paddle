0ac49e1ae53a66a960d86b4786f35c537fd89956
tianshuo78520a <707759223@qq.com>
2023-11-24 15:52:39 +0800

    add cpu dockerbuild

--- tools/dockerfile/ubuntu20_release.sh
+++ tools/dockerfile/ubuntu20_release.sh
@@ -44,6 +44,17 @@ function base_image(){
     sed -i 's#RUN bash /build_scripts/install_trt.sh#RUN bash /build_scripts/install_trt.sh trt8616#g' ${dockerfile_name}
     sed -i 's#cudnn841#cudnn891#g' ${dockerfile_name}
     sed -i 's#CUDNN_VERSION=8.4.1#CUDNN_VERSION=8.9.1#g' ${dockerfile_name}
+  elif [[ ${ref_CUDA_MAJOR} == "0" ]];then
+    dockerfile_name="Dockerfile-cpu"
+    sed "s#<baseimg>#ubuntu:20.04#g" ./Dockerfile.release.ubuntu20 >${dockerfile_name}
+    sed -i "s#<setcuda>##g" ${dockerfile_name}
+    sed -i 's#<install_cpu_package>##g' ${dockerfile_name}
+    sed -i "s#<install_gcc>#WORKDIR /usr/bin ENV PATH=/usr/local/gcc-8.2/bin:\$PATH #g" ${dockerfile_name}
+    sed -i "s#gcc121#gcc82#g" ${dockerfile_name}
+    sed -i "s#gcc-12.1#gcc-8.2#g" ${dockerfile_name}
+    sed -i 's#RUN bash /build_scripts/install_trt.sh##g' ${dockerfile_name}
+    sed -i 's#RUN bash /build_scripts/install_cudnn.sh cudnn841##g' ${dockerfile_name}
+    sed -i 's#ENV CUDNN_VERSION=8.4.1##g' ${dockerfile_name}
   else
     echo "Dockerfile ERROR!!!"
     exit 1
@@ -57,4 +68,6 @@ base_image
 export ref_CUDA_MAJOR=11.7
 base_image
 export ref_CUDA_MAJOR=12.0
+base_image
+export ref_CUDA_MAJOR=0
 base_image
