ca8caab127cda975eaac73da57c7a345b7a666e5
luq <luq@zju.edu.cn>
2023-11-28 08:41:20 +0000

    revert gradient_checker.py

--- test/legacy_test/gradient_checker.py
+++ test/legacy_test/gradient_checker.py
@@ -498,22 +498,16 @@ def double_grad_check(
     x_init = _as_list(x_init)
 
     if in_pir_mode():
-        main_program, op_map_main = paddle.base.libpaddle.pir.clone_program(
+        program, op_map = paddle.base.libpaddle.pir.clone_program(
             paddle.static.default_main_program()
-        )
-        (
-            startup_program,
-            op_map_startup,
-        ) = paddle.base.libpaddle.pir.clone_program(
-            paddle.static.default_startup_program()
         )
         clone_x = []
         for xi in x:
-            clone_x.append(op_map_main[xi])
+            clone_x.append(op_map[xi])
         clone_y = []
         for yi in y:
-            clone_y.append(op_map_main[yi])
-        with paddle.static.program_guard(main_program, startup_program):
+            clone_y.append(op_map[yi])
+        with paddle.static.program_guard(program):
             (
                 grad_res,
                 x,
@@ -863,7 +857,6 @@ def get_pir_static_double_grad(
     # only fetch not None dx in exe.run
     filted = [(i, dxi) for i, dxi in enumerate(ddx) if dxi is not None]
     filted_idx, filted_ddx = zip(*filted)
-    exe.run(paddle.static.default_startup_program())
     ddx_res = exe.run(
         program=program, feed=feeds, fetch_list=[filted_ddx, filted_dx]
     )
