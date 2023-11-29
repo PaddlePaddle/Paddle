933a8645adb11dc42fdd9f7bdaf004c4e7750901
xiaoxiaohehe001 <hiteezsf@163.com>
2023-11-29 13:35:25 +0800

    fix_fix_custom_op

--- python/paddle/utils/cpp_extension/extension_utils.py
+++ python/paddle/utils/cpp_extension/extension_utils.py
@@ -1077,7 +1077,7 @@ def _gen_output_content(
 {indent}    start_idx += 1"""
             static_content += f"""
 {indent}if {lower_in_names} is not None:
-{indent}    outs['{out_name}'] = [helper.create_variable(dtype='float32') for _ in range(len({lower_in_names}))]"""
+{indent}    outs['{out_name}'] = {lower_in_names}"""
         elif (
             in_idx != -1 and "@VECTOR" in in_names[in_idx]
         ):  # inplace vector<Tensor> output case
@@ -1086,7 +1086,7 @@ def _gen_output_content(
 {indent}res.append(outs[start_idx: start_idx + len({lower_in_names})])
 {indent}start_idx += len({lower_in_names})"""
             static_content += f"""
-{indent}outs['{out_name}'] = [helper.create_variable(dtype='float32') for _ in range(len({lower_in_names}))]"""
+{indent}outs['{out_name}'] = {lower_in_names}"""
         elif (
             in_idx != -1 and "@OPTIONAL" in in_names[in_idx]
         ):  # inplace optional Tensor output case, handle inplace None input
@@ -1099,8 +1099,17 @@ def _gen_output_content(
 {indent}start_idx += 1"""
             static_content += f"""
 {indent}if {lower_in_names} is not None:
-{indent}    outs['{out_name}'] = helper.create_variable(dtype='float32')"""
-        else:  # general/inplace Tensor output case
+{indent}    outs['{out_name}'] = {lower_in_names}"""
+        elif (
+            in_idx != -1
+        ):  # inplace Tensor output case, handle inplace None input
+            lower_in_names = in_names[in_idx].lower()
+            dynamic_content += f"""
+{indent}res.append(outs[start_idx])
+{indent}start_idx += 1"""
+            static_content += f"""
+{indent}outs['{out_name}'] = {lower_in_names}"""
+        else:  # general Tensor output case
             dynamic_content += f"""
 {indent}res.append(outs[start_idx])
 {indent}start_idx += 1"""
