de218db32b1ea2f04b60e66ee4b47bc3f578d9c7
xuxinyi04 <xuxinyi04@baidu.com>
2023-11-30 08:02:45 +0000

    fix

--- test/cpp/fluid/fused/fused_dropout_test.h
+++ test/cpp/fluid/fused/fused_dropout_test.h
@@ -18,7 +18,7 @@ limitations under the License. */
 #include <vector>
 
 #include "gtest/gtest.h"
-#include "paddle/common/macros.h"
+#include "paddle/common/macros.h"  // NOLINT
 #include "paddle/fluid/framework/op_registry.h"
 #include "paddle/fluid/framework/operator.h"
 #include "paddle/fluid/framework/program_desc.h"
