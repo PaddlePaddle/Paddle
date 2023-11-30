41aba9d1256600a6adfe022616cffd38a645ed29
zhangbopd <1299246947@qq.com>
2023-11-30 12:54:05 +0000

    infer

--- cmake/inference_lib.cmake
+++ cmake/inference_lib.cmake
@@ -312,6 +312,10 @@ copy(
   DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/paddle/phi/core/)
 copy(
   inference_lib_dist
+  SRCS ${PADDLE_SOURCE_DIR}/paddle/utils/string/printf.h
+  DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/paddle/utils/string/)
+copy(
+  inference_lib_dist
   SRCS ${PADDLE_SOURCE_DIR}/paddle/phi/core/visit_type.h
   DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/include/paddle/phi/core/)
 copy(
