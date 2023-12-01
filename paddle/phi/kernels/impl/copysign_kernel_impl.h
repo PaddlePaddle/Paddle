6b785c2519157de5d4bdec8536de7e63f88f4c36
coco <1228759711@qq.com>
2023-12-02 07:35:28 +0800

    static cast

--- paddle/phi/kernels/impl/copysign_kernel_impl.h
+++ paddle/phi/kernels/impl/copysign_kernel_impl.h
@@ -26,7 +26,8 @@ struct CopySignFunctor {
       : x_data_(x_data), y_data_(y_data), out_data_(out_data), numel_(numel) {}
 
   HOSTDEVICE void operator()(int64_t idx) const {
-    out_data_[idx] = static_cast<T>(std::copysign(x_data_[idx], y_data_[idx]));
+    out_data_[idx] = static_cast<T>(std::copysign(static_cast<T> x_data_[idx],
+                                                  static_cast<T> y_data_[idx]));
   }
 
   const T* x_data_;
