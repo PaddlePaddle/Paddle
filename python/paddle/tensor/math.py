41ef0e7d893b66da60da42a90b6291574cfd43a6
coco <1228759711@qq.com>
2023-12-02 14:36:08 +0800

    fix cast when grad

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -7073,8 +7073,8 @@ def copysign(x, y, name=None):
 
     """
     out_shape = broadcast_shape(x.shape, y.shape)
-    x = x.cast_(paddle.float64)
-    y = y.cast_(paddle.float64)
+    x = paddle.cast_(x, paddle.float64)
+    y = paddle.cast_(y, paddle.float64)
     x = paddle.broadcast_to(x, out_shape)
     y = paddle.broadcast_to(y, out_shape)
 
@@ -7096,8 +7096,8 @@ def copysign_(x, y):
     Please refer to :ref:`api_paddle_copysign`.
     """
     out_shape = broadcast_shape(x.shape, y.shape)
-    x = x.cast_(paddle.float64)
-    y = y.cast_(paddle.float64)
+    x = paddle.cast_(x, paddle.float64)
+    y = paddle.cast_(y, paddle.float64)
     y = paddle.broadcast_to(y, out_shape)
 
     return _C_ops.copysign_(x, y)
