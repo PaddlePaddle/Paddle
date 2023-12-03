652057c4fc16eefd751ad97fec603924091899f6
Yichen Zhang <zhangyichen03@baidu.com>
2023-12-03 22:52:42 +0800

    update static_decorate as comments

--- python/paddle/distributed/__init__.py
+++ python/paddle/distributed/__init__.py
@@ -85,6 +85,7 @@ from .auto_parallel.api import (
     reshard,
     shard_layer,
     shard_optimizer,
+    static_decorate,
 )
 
 from .fleet import BoxPSDataset  # noqa: F401
@@ -159,4 +160,5 @@ __all__ = [
     "Replicate",
     "Partial",
     "shard_optimizer",
+    "static_decorate",
 ]
