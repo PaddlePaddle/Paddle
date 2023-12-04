249dc762a28eea18e3919a9a1d9ad26cd2fab2a9
zeroRains <linjunlu@zerorains.top>
2023-12-04 11:02:23 +0000

    temp

--- paddle/fluid/operators/fused/fused_gemm_epilogue_op.cc
+++ paddle/fluid/operators/fused/fused_gemm_epilogue_op.cc
@@ -92,9 +92,63 @@ class FusedGemmEpilogueGradOp : public framework::OperatorWithKernel {
  protected:
   phi::KernelKey GetExpectedKernelType(
       const framework::ExecutionContext& ctx) const override {
-    auto data_type = OperatorWithKernel::IndicateVarDataType(
-        ctx, framework::GradVarName("Out"));
+    auto data_type = OperatorWithKernel::IndicateVarDataType(ctx, "DOut");
     return phi::KernelKey(data_type, ctx.GetPlace());
+  }
+};
+
+class FusedGemmEpilogueGradOpMaker : public framework::OpProtoAndCheckerMaker {
+ public:
+  void Make() override {
+    AddInput("DOut",
+             "The input grad tensor to Out of Out = (Act(X) * Y) + bias");
+    AddInput("X", "The input tensor X of Out = (Act(X) * Y) + bias");
+    AddInput("Y", "The input tensor Y of Out = (Act(X) * Y) + bias");
+    AddInput("ReserveSpace",
+             R"DOC(A GPU space to fetch
+        auxiliary data pointer. It is used to pass auxiliary data pointer
+        for fused_gemm_epilogue_grad op. If not given (empty string), the
+        auxiliary mode would not be enable.)DOC")
+        .AsDispensable();
+
+    AddOutput("DX", "The output grad tensor to X of Out = (Act(X) * Y) + bias.")
+        .AsDispensable();
+    AddOutput("DY",
+              "The output grad tensor to Y of Out = (Act(X) * Y) + bias.");
+    AddOutput("DBias",
+              "The output grad tensor to bias of Out = (Act(X) * Y) + bias.")
+        .AsDispensable();
+    AddAttr<bool>(
+        "trans_x",
+        R"DOC((bool, default false), Whether to transpose input tensor X
+    or not. The input tensor X coulbe be more than two dimension. When
+    set trans_x=true, it would fully reverse X. For instant: X with shpae
+    [d0, d1, d2, d3] -> [d3, d2, d1, d0].)DOC")
+        .SetDefault(false);
+    AddAttr<bool>(
+        "trans_y",
+        R"DOC((bool, default false), Whether to transpose input tensor Y
+    or not. The input tensor Y should be two dimension. When
+    set trans_y=true, it would transpose Y. For instant: Y with shpae
+    [d0, d1] -> [d1, d0].)DOC")
+        .SetDefault(false);
+
+    AddAttr<std::string>(
+        "activation_grad",
+        R"DOC((string, default none), The backward activation function. It could be
+    one of {none, relu_grad, gelu_grad}. When none is given, The backward Act would
+    be null operations)DOC")
+        .SetDefault("none");
+
+    AddComment(R"DOC(
+FusedGemmEpilogueGrad Operator
+This operator is used to perform backward of Elementwise_add(Matmul(Activeation(X), Y), bias).
+It is equal to Activation (None, ReLU or GeLU) + paddle.nn.Linear.
+
+Note:
+X could be more than two dimension and would be flatten to 2D for computing.
+X with shape [d0, d1, d2, d3] -> X_2D with shape [d0*d1*d2, d3]
+)DOC");
   }
 };
 
@@ -113,11 +167,11 @@ class FusedGemmEpilogueOpGradMaker : public framework::SingleGradOpMaker<T> {
     if (act_type != "none") {
       op->SetInput("ReserveSpace", this->Output("ReserveSpace"));
     }
-    op->SetInput(framework::GradVarName("Out"), this->OutputGrad("Out"));
+    op->SetInput("DOut", this->OutputGrad("Out"));
 
-    op->SetOutput(framework::GradVarName("X"), this->InputGrad("X"));
-    op->SetOutput(framework::GradVarName("Y"), this->InputGrad("Y"));
-    op->SetOutput(framework::GradVarName("Bias"), this->InputGrad("Bias"));
+    op->SetOutput("DX", this->InputGrad("X"));
+    op->SetOutput("DY", this->InputGrad("Y"));
+    op->SetOutput("DBias", this->InputGrad("Bias"));
 
     op->SetAttrMap(this->Attrs());
   }
@@ -141,4 +195,5 @@ REGISTER_OPERATOR(fused_gemm_epilogue,
                   FusedGemmEpilogueInferShapeFunctor);
 REGISTER_OPERATOR(fused_gemm_epilogue_grad,
                   ops::FusedGemmEpilogueGradOp,
+                  ops::FusedGemmEpilogueGradOpMaker,
                   FusedGemmEpilogueGradInferShapeFunctor);
