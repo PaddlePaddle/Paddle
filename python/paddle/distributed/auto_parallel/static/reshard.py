0a9798faad595abf6471dc547f25ea21ca1ba1c0
Yichen Zhang <zhangyichen03@baidu.com>
2023-12-05 14:37:37 +0800

    remove useless print and comments

--- python/paddle/distributed/auto_parallel/static/reshard.py
+++ python/paddle/distributed/auto_parallel/static/reshard.py
@@ -2470,13 +2470,6 @@ class Resharder:
                         and self.dist_context.process_meshes
                     ):
                         is_union_process_mesh_tensor = True
-                        print(
-                            "var:",
-                            var_name,
-                            "process meshes:",
-                            self.dist_context.process_meshes,
-                        )
-                        print("dist_attr:", dist_tensor.dist_attr)
                         assert dist_tensor.dist_attr.dims_mapping.count(
                             -1
                         ) == len(dist_tensor.dist_attr.dims_mapping)
