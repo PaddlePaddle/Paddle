29933ff585d9dde840a2c93599d4a79736dc5f77
lizexu <2694294196@qq.com>
2023-12-05 11:40:32 +0000

    argsort

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -3193,10 +3193,11 @@ struct GenericPluginTeller : public Teller {
         return false;
       }
     }
+    // TODO(lizexu123): the tensorrt version on Windows supports low-level
+    // and inconsistent supportformation
     if (op_type == "argsort") {
 #if IS_TRT_VERSION_LT(8100)
-      VLOG(3) << "argsort is not supported in TRT version <8.1,beacuse of the "
-                 "cuda version is too low.";
+      VLOG(3) << "argsort is not supported in TRT version <8.1";
       return false;
 #endif
       if (!with_dynamic_shape) {
