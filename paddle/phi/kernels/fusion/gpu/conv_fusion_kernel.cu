abe6e1589f2131176a56cb9fa5484e32528992ee
yuguo-Jack <948529990@qq.com>
2023-12-06 13:27:45 +0000

    fix rocm compile bugs

--- paddle/phi/kernels/fusion/gpu/conv_fusion_kernel.cu
+++ paddle/phi/kernels/fusion/gpu/conv_fusion_kernel.cu
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef PADDLE_WITH_CUDA
 #include <xxhash.h>
 
 #include <algorithm>
@@ -649,3 +650,4 @@ PD_REGISTER_KERNEL(conv2d_fusion,  // cuda_only
                    float,
                    double,
                    phi::dtype::float16) {}
+#endif
