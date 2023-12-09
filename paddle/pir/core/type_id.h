aa747cf14c647ec6aad2398a96f3795d24cfaa3c
Yangrl <2535184404@qq.com>
2023-12-09 14:48:18 +0000

    fix win

--- paddle/pir/core/type_id.h
+++ paddle/pir/core/type_id.h
@@ -83,7 +83,7 @@ class TypeId {
 };
 
 namespace detail {
-class alignas(8) UniqueingId {
+class alignas(8) TEST_API UniqueingId {
  public:
   UniqueingId() = default;
   UniqueingId(const UniqueingId &) = delete;
@@ -129,11 +129,11 @@ TypeId TypeId::get() {
   }                                                    \
   }  // namespace pir
 
-#define IR_DEFINE_EXPLICIT_TYPE_ID(TYPE_CLASS)               \
-  namespace pir {                                            \
-  namespace detail {                                         \
-  TEST_API UniqueingId TypeIdResolver<TYPE_CLASS>::id_ = {}; \
-  }                                                          \
+#define IR_DEFINE_EXPLICIT_TYPE_ID(TYPE_CLASS)      \
+  namespace pir {                                   \
+  namespace detail {                                \
+  UniqueingId TypeIdResolver<TYPE_CLASS>::id_ = {}; \
+  }                                                 \
   }  // namespace pir
 
 }  // namespace pir
