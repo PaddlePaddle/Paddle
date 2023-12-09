9555135e0dd6f9bb518b44d673f77ebca092bfab
SigureMo <sigure.qaq@gmail.com>
2023-12-09 20:43:22 +0800

    [Dy2St] Decrease `test_train_step` time

--- test/dygraph_to_static/test_train_step_resnet18_sgdx.py
+++ test/dygraph_to_static/test_train_step_resnet18_sgdx.py
@@ -15,7 +15,7 @@
 import platform
 import unittest
 
-from test_train_step import (
+from test_train_stepx import (
     TestTrainStepTinyModel,
     loss_fn_tiny_model,
     train_step_tiny_model,
@@ -25,12 +25,12 @@ import paddle
 from paddle.vision.models import resnet18
 
 
-class TestTrainStepResNet18Adam(TestTrainStepTinyModel):
+class TestTrainStepResNet18Sgd(TestTrainStepTinyModel):
     def setUp(self):
         self.input = paddle.randn([64, 3, 224, 224])
         self.net_creator = resnet18
         self.lr_creator = lambda: 0.001
-        self.optimizer_creator = paddle.optimizer.Adam
+        self.optimizer_creator = paddle.optimizer.SGD
         self.loss_fn = loss_fn_tiny_model
         self.train_step_func = train_step_tiny_model
         self.steps = 3
