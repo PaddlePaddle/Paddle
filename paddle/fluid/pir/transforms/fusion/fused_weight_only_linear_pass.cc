b49e4f4b286ed2dad75f3dca12626bb259212db0
Wanglongzhi2001 <583087864@qq.com>
2023-12-12 01:11:40 +0800

    fix: fix typo

--- paddle/fluid/pir/transforms/fusion/fused_weight_only_linear_pass.cc
+++ paddle/fluid/pir/transforms/fusion/fused_weight_only_linear_pass.cc
@@ -69,7 +69,7 @@ class FusedWeightOnlyLinearPattern
       }
 
       auto w_dims = match_ctx.Tensor("w").Shape();
-      if (w_dims.at(0) % 16 != 0 || w_dims.at(1) % 16 != 0) return false;
+      if (w_dims.at(0) % 64 != 0 || w_dims.at(1) % 16 != 0) return false;
 
       auto w_dtype = match_ctx.Tensor("w").Dtype();
       if (!w_dtype.dtype().get().isa<pir::Float16Type>() &&
