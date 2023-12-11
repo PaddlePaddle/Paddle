7b3ef681ee793bab35a444faaf95450d33e2046f
megemini <megemini@outlook.com>
2023-12-11 19:17:03 +0800

    [Change] fractional max 2d gpu pooling.cu grad with dim3

--- paddle/phi/kernels/funcs/pooling.cu
+++ paddle/phi/kernels/funcs/pooling.cu
@@ -2587,7 +2587,7 @@ template class MaxPool3dWithIndexGradFunctor<phi::GPUContext,
                                              int>;
 // fractional max pool
 template <typename T1, typename T2>
-__global__ void FractionalKernelMaxPool2dWithIdx(const int nthreads,
+__global__ void FractionalKernelMaxPool2dWithIdx(const int ncd,
                                                  const T1* input_data,
                                                  const int channels,
                                                  const int input_height,
@@ -2600,8 +2600,8 @@ __global__ void FractionalKernelMaxPool2dWithIdx(const int nthreads,
                                                  T1* output_data,
                                                  T2* mask_data,
                                                  FastDivModForPooling divmods) {
-  float alpha_height = 0, alpha_width = 0, alpha_depth = 0;
-  float u_height = 0, u_width = 0, u_depth = 0;
+  float alpha_height = 0, alpha_width = 0;
+  float u_height = 0, u_width = 0;
   float u = 0;
   if (random_u == 0) {
     size_t thread_idx =
@@ -2626,52 +2626,55 @@ __global__ void FractionalKernelMaxPool2dWithIdx(const int nthreads,
   u_height = FractionalRationalU(u, alpha_height, input_height, output_height);
   u_width = FractionalRationalU(u, alpha_width, input_width, output_width);
 
-  for (int index = blockIdx.x * blockDim.x + threadIdx.x; index < nthreads;
-       index += blockDim.x * gridDim.x) {
-    int hstart, hend, wstart, wend;
-    int w_offset, h_offset, c_offset, input_offset;
-    OffsetPreparationFor4Dimension<FastDivModForPooling>(index,
-                                                         false,
-                                                         divmods,
-                                                         0,
-                                                         0,
-                                                         input_width,
-                                                         input_height,
-                                                         &w_offset,
-                                                         &h_offset,
-                                                         &c_offset,
-                                                         &input_offset);
-    input_data += input_offset;
+  int w_offset, h_offset, nc_offset;
+  int hstart, hend, wstart, wend;
+  const T1* input_data_cur;
 
-    hstart = FractionalStartIndex(h_offset, alpha_height, u_height);
-    hend = FractionalEndIndex(h_offset, alpha_height, u_height);
-    hstart = std::max(hstart, 0);
-    hend = std::min(hend, input_height);
+  w_offset = blockIdx.x * blockDim.x + threadIdx.x;
 
-    wstart = FractionalStartIndex(w_offset, alpha_width, u_width);
-    wend = FractionalEndIndex(w_offset, alpha_width, u_width);
-    wstart = std::max(wstart, 0);
-    wend = std::min(wend, input_width);
+  if (w_offset < output_width) {
+    for (int index_y = blockIdx.y * blockDim.y + threadIdx.y; index_y < ncd;
+         index_y += gridDim.y * blockDim.y) {
+      auto output_height_divmod = divmods.height.Divmod(index_y);
+      h_offset = output_height_divmod.val[1];
+      nc_offset = output_height_divmod.val[0];
 
-    T1 ele = static_cast<T1>(-FLT_MAX);
-    int max_index = -1;
-    for (int h = hstart; h < hend; ++h) {
-      for (int w = wstart; w < wend; ++w) {
-        int input_index = h * input_width + w;
-        if (ele < input_data[input_index]) {
-          max_index = input_index;
-          ele = input_data[input_index];
+      int output_index = nc_offset * output_height * output_width +
+                         h_offset * output_width + w_offset;
+
+      int input_offset = nc_offset * input_height * input_width;
+      input_data_cur = input_data + input_offset;
+
+      hstart = FractionalStartIndex(h_offset, alpha_height, u_height);
+      hend = FractionalEndIndex(h_offset, alpha_height, u_height);
+      hstart = std::max(hstart, 0);
+      hend = std::min(hend, input_height);
+
+      wstart = FractionalStartIndex(w_offset, alpha_width, u_width);
+      wend = FractionalEndIndex(w_offset, alpha_width, u_width);
+      wstart = std::max(wstart, 0);
+      wend = std::min(wend, input_width);
+
+      T1 ele = static_cast<T1>(-FLT_MAX);
+      int max_index = -1;
+      for (int h = hstart; h < hend; ++h) {
+        for (int w = wstart; w < wend; ++w) {
+          if (ele < input_data_cur[h * input_width + w]) {
+            max_index = h * input_width + w;
+            ele = input_data_cur[max_index];
+          }
         }
       }
+
+      output_data[output_index] = ele;
+      mask_data[output_index] = max_index;
     }
-    output_data[index] = ele;
-    mask_data[index] = max_index;
   }
 }
 
 template <typename T1, typename T2>
 __global__ void FractionalKernelMaxPool2DWithIdxGrad(
-    const int nthreads,
+    const int ncd,
     const T1* output_grad,
     const T2* mask_data,
     const int channels,
@@ -2684,23 +2687,26 @@ __global__ void FractionalKernelMaxPool2DWithIdxGrad(
     uint64_t offset,
     T1* input_grad,
     FastDivModForPooling divmods) {
-  for (int index = blockIdx.x * blockDim.x + threadIdx.x; index < nthreads;
-       index += blockDim.x * gridDim.x) {
-    int w_offset, h_offset, c_offset;
+  int w_offset, h_offset, nc_offset;
 
-    auto input_width_divmod = divmods.width.Divmod(index);
-    auto input_height_divmod = divmods.height.Divmod(input_width_divmod.val[0]);
-    auto channel_divmod = divmods.channel.Divmod(input_height_divmod.val[0]);
-    w_offset = input_width_divmod.val[1];
-    h_offset = input_height_divmod.val[1];
-    c_offset = channel_divmod.val[1];
+  w_offset = blockIdx.x * blockDim.x + threadIdx.x;
 
-    int output_index = c_offset * output_height * output_width +
-                       h_offset * output_width + w_offset;
+  if (w_offset < output_width) {
+    for (int index_y = blockIdx.y * blockDim.y + threadIdx.y; index_y < ncd;
+         index_y += gridDim.y * blockDim.y) {
+      auto output_height_divmod = divmods.height.Divmod(index_y);
+      h_offset = output_height_divmod.val[1];
+      nc_offset = output_height_divmod.val[0];
 
-    int max_index = mask_data[output_index];
-    if (max_index != -1) {
-      phi::CudaAtomicAdd(&input_grad[index], output_grad[output_index]);
+      int output_index = nc_offset * output_height * output_width +
+                         h_offset * output_width + w_offset;
+
+      int max_index = mask_data[output_index];
+      if (max_index != -1) {
+        phi::CudaAtomicAdd(
+            &input_grad[nc_offset * input_height * input_width + max_index],
+            output_grad[output_index]);
+      }
     }
   }
 }
@@ -2729,17 +2735,22 @@ class FractionalMaxPool2dWithIndexFunctor<phi::GPUContext, T1, T2> {
     T1* output_data = context.template Alloc<T1>(output);
     T2* mask_data = context.template Alloc<T2>(mask);
 
-    int nthreads = batch_size * output_channels * output_height * output_width;
+    int ncd = batch_size * input_channels * output_height;
+
+    int thread_x = 32;
+    int thread_y = 1;
+    int thread_z = 1;
+    dim3 threads(thread_x, thread_y, thread_z);
+    std::array<int, 3> max_grid_dim = context.GetCUDAMaxGridDimSize();
+    int block_x = (output_width + threads.x - 1) / threads.x;
+    int block_y = (ncd > max_grid_dim[1] * threads.y)
+                      ? max_grid_dim[1]
+                      : (ncd + threads.y - 1) / threads.y;
+    int block_z = 1;
+    dim3 grid(block_x, block_y, block_z);
+
     auto pool_divmods =
         FastDivModForPooling(input_channels, output_width, output_height);
-
-    int thread_num = 1024;
-#ifdef WITH_NV_JETSON
-    backends::gpu::ChangeThreadNum(context, &thread_num);
-#endif
-    int blocks = (nthreads + thread_num - 1) / thread_num;
-    dim3 threads(thread_num, 1);
-    dim3 grid(blocks, 1);
 
     uint64_t seed = 0;
     uint64_t offset = 0;
@@ -2751,7 +2762,7 @@ class FractionalMaxPool2dWithIndexFunctor<phi::GPUContext, T1, T2> {
     offset = seed_offset.second;
 
     FractionalKernelMaxPool2dWithIdx<T1, T2>
-        <<<grid, threads, 0, context.stream()>>>(nthreads,
+        <<<grid, threads, 0, context.stream()>>>(ncd,
                                                  input_data,
                                                  input_channels,
                                                  input_height,
@@ -2790,13 +2801,22 @@ class FractionalMaxPool2dWithIndexGradFunctor<phi::GPUContext, T1, T2> {
     const T1* output_grad_data = output_grad.data<T1>();
     T1* input_grad_data = context.template Alloc<T1>(input_grad);
 
-    int nthreads = batch_size * input_channels * input_height * input_width;
-    int blocks = (nthreads + 1024 - 1) / 1024;
-    dim3 threads(1024, 1);
-    dim3 grid(blocks, 1);
+    int ncd = batch_size * input_channels * output_height;
+
+    int thread_x = 32;
+    int thread_y = 1;
+    int thread_z = 1;
+    dim3 threads(thread_x, thread_y, thread_z);
+    std::array<int, 3> max_grid_dim = context.GetCUDAMaxGridDimSize();
+    int block_x = (output_width + threads.x - 1) / threads.x;
+    int block_y = (ncd > max_grid_dim[1] * threads.y)
+                      ? max_grid_dim[1]
+                      : (ncd + threads.y - 1) / threads.y;
+    int block_z = 1;
+    dim3 grid(block_x, block_y, block_z);
 
     auto pool_divmods =
-        FastDivModForPooling(input_channels, input_width, input_height);
+        FastDivModForPooling(input_channels, output_width, output_height);
 
     uint64_t seed = 0;
     uint64_t offset = 0;
@@ -2808,7 +2828,7 @@ class FractionalMaxPool2dWithIndexGradFunctor<phi::GPUContext, T1, T2> {
     offset = seed_offset.second;
 
     FractionalKernelMaxPool2DWithIdxGrad<T1, T2>
-        <<<grid, threads, 0, context.stream()>>>(nthreads,
+        <<<grid, threads, 0, context.stream()>>>(ncd,
                                                  output_grad_data,
                                                  mask_data,
                                                  input_channels,
