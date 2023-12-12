6766b8dd8027e7769dc53286ac860631763cfe65
iosmers <yinwei_hust@163.com>
2023-12-12 17:56:24 +0800

    update

--- paddle/phi/kernels/fusion/gpu/fused_linear_param_grad_add_kernel.cu
+++ paddle/phi/kernels/fusion/gpu/fused_linear_param_grad_add_kernel.cu
@@ -136,10 +136,10 @@ void FusedLinearParamGradAdd(const Context &ctx,
   if (dweight_out) {
     if (dweight_out->dtype() == phi::DataType::FLOAT16) {
       LOG_FIRST_N(WARNING, 1)
-          << "There may be problems when master_grad is not enabled and use "
-             "fp16-O2 in stage2, users should pay attention to the correctness "
-             "of "
-             "the result of the grad accumulation in stage2.";
+          << "fused_linear_param_grad_add op may have problems when "
+             "master_grad is not enabled and use fp16-O2 in stage2, users "
+             "should pay attention to the correctness of the result of the "
+             "grad accumulation in stage2.";
     }
     if (dweight) {
       use_addto = true;
