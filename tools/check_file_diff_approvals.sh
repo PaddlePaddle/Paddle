33fcbf3eb683101c010c0eef1351c72baa70a78e
drryanhuang <zihaohuang@aliyun.com>
2023-12-15 06:55:57 +0000

    fix codestyle

--- tools/check_file_diff_approvals.sh
+++ tools/check_file_diff_approvals.sh
@@ -390,14 +390,12 @@ if [ "${DEPRECATED_FLAKE8}" != "" ] && [ "${GIT_PR_ID}" != "" ]; then
     check_approval 1 SigureMo gouzil
 fi
 
-
 TEST_FILE_ADDED_LINES=$(git diff test -U0 upstream/$BRANCH |grep "^+" || true)
 ENABLE_TO_STATIC_CHECK=`echo "$TEST_FILE_ADDED_LINES" | grep "enable_to_static(" || true`
 if [ "${ENABLE_TO_STATIC_CHECK}" != "" ] && [ "${GIT_PR_ID}" != "" ]; then
-    echo_line="You must have have one RD (SigureMo, Aurelius84 or 2742195759) approval for using `paddle.jit.enable_to_static`, we recommend using `enable_to_static_guard` in the related test files.\n"
+    echo_line="You must have one RD (SigureMo, Aurelius84 or 2742195759) approval for using `paddle.jit.enable_to_static`, we recommend using `enable_to_static_guard` in the related test files.\n"
     check_approval 1 SigureMo Aurelius84 2742195759
 fi
-
 
 HAS_MODIFIED_PHI_FILES=`git diff --name-only upstream/$BRANCH | grep "paddle/phi/" || true`
 PHI_INCLUDE_FLUID_FILES=""
