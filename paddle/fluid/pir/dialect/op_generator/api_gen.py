109afd118a0b6bb7f0bad56b99956ec9c08ff5e8
Zhan Rongrui <2742392377@qq.com>
2023-12-15 07:01:25 +0000

    enable grad

--- paddle/fluid/pir/dialect/op_generator/api_gen.py
+++ paddle/fluid/pir/dialect/op_generator/api_gen.py
@@ -571,8 +571,7 @@ class CodeGen:
         )
 
         if (
-            op_name.endswith(('_grad', '_grad_', '_grad_dense', '_grad_sparse'))
-            or op_name in ["print", "hardshrink", "det", "assign_out_"]
+            op_name in ["print", "hardshrink", "det", "assign_out_"]
             or len(mapping_name_to_type) == 0
         ):
             return ""
