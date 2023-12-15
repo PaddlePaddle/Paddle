306f37f144f9fc6dabfa0ae1066ddd82fac974a6
jiahongyu <jiahongyu@baidu.com>
2023-12-15 08:38:37 +0000

    Fix unit test

--- test/cpp/pir/shape_dialect/symbol_dim_expr_test.cc
+++ test/cpp/pir/shape_dialect/symbol_dim_expr_test.cc
@@ -61,7 +61,7 @@ TEST(DimExpr, equal) {
   ASSERT_EQ(sym0 - sym1, sym0 - sym1);
   ASSERT_NE(sym0 - sym1, sym1 - sym0);
   ASSERT_EQ(sym0 - constant1, DimExpr("S0") - constant1);
-  ASSERT_EQ(sym0 * sym1, sym0 * sym2);
+  ASSERT_EQ(sym0 * sym1, sym0 * sym1);
   ASSERT_NE(sym0 * sym1, sym1 * sym0);
   ASSERT_EQ(sym0 * constant1, DimExpr("S0") * constant1);
   ASSERT_EQ(sym0 / sym1, sym0 / sym1);
