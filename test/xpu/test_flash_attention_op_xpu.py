626aa654e623742f4ba9ec88b9dd17b0188a6ea9
houj04 <houj04@foxmail.com>
2023-12-18 12:59:03 +0800

    update ut.

--- test/xpu/test_flash_attention_op_xpu.py
+++ test/xpu/test_flash_attention_op_xpu.py
@@ -15,7 +15,6 @@
 import unittest
 
 import numpy as np
-from op_test import convert_uint16_to_float
 
 import paddle
 import paddle.nn.functional as F
@@ -33,11 +32,15 @@ def get_triangle_upper_mask(x):
     return mask
 
 
-def attention_naive(q_bf16, k_bf16, v_bf16):
-    q = paddle.cast(q_bf16, "float32")
-    k = paddle.cast(k_bf16, "float32")
-    v = paddle.cast(v_bf16, "float32")
-
+def attention_naive(q, k, v):
+    origin_dtype = q.dtype
+    assert k.dtype == origin_dtype
+    assert v.dtype == origin_dtype
+    if q.dtype != paddle.float32:
+        q = paddle.cast(q, "float32")
+        k = paddle.cast(k, "float32")
+        v = paddle.cast(v, "float32")
+    # real calculation
     qt = paddle.transpose(q, [0, 2, 1, 3])
     kt = paddle.transpose(k, [0, 2, 1, 3])
     vt = paddle.transpose(v, [0, 2, 1, 3])
@@ -48,7 +51,7 @@ def attention_naive(q_bf16, k_bf16, v_bf16):
     s = s + mask
     p = F.softmax(s)
     o = paddle.matmul(p, vt)
-    o = paddle.cast(o, "bfloat16")
+    o = paddle.cast(o, origin_dtype)
     return paddle.transpose(o, [0, 2, 1, 3])
 
 
@@ -69,16 +72,21 @@ class TestFlashAttentionAPI(unittest.TestCase):
     def setUp(self):
         self.place = paddle.XPUPlace(0)
         self.shape = (1, 128, 2, 32)
-        # TODO(houj04): add more dtypes
-        self.dtype = 'bfloat16'
+        self.dtype = 'float32'
         self.dropout = 0.0
         self.causal = True
         self.return_softmax = False
+        self.rtol = 1e-3
+        self.atol = 1e-3
 
     def test_all(self):
+        # TODO(houj04) remove debug codes after correctness check
+        print(f"Test case shape {self.shape} dtype {self.dtype}")
+
         # test dynamic
         paddle.disable_static()
 
+        np.random.seed(2023)
         query = np.random.random(self.shape)
         key = np.random.random(self.shape)
         value = np.random.random(self.shape)
@@ -113,10 +121,11 @@ class TestFlashAttentionAPI(unittest.TestCase):
         out_.backward()
 
         # forward result
-        float_out = convert_uint16_to_float(out)
-        float_out_ = convert_uint16_to_float(out_)
+        float_out = paddle.cast(out, "float32")
+        float_out_ = paddle.cast(out_, "float32")
+
         np.testing.assert_allclose(
-            float_out, float_out_, rtol=1e-02, atol=1e-02
+            float_out, float_out_, rtol=self.rtol, atol=self.atol
         )
 
         # backward shape
@@ -128,23 +137,76 @@ class TestFlashAttentionAPI(unittest.TestCase):
         self.assertEqual(v_.grad.shape, v.shape)
 
         # backward result
-        float_q_grad = convert_uint16_to_float(q.grad)
-        float_q_grad_ = convert_uint16_to_float(q_.grad)
-        float_k_grad = convert_uint16_to_float(k.grad)
-        float_k_grad_ = convert_uint16_to_float(k_.grad)
-        float_v_grad = convert_uint16_to_float(v.grad)
-        float_v_grad_ = convert_uint16_to_float(v_.grad)
+        float_q_grad = paddle.cast(q.grad, "float32")
+        float_q_grad_ = paddle.cast(q_.grad, "float32")
+        float_k_grad = paddle.cast(k.grad, "float32")
+        float_k_grad_ = paddle.cast(k_.grad, "float32")
+        float_v_grad = paddle.cast(v.grad, "float32")
+        float_v_grad_ = paddle.cast(v_.grad, "float32")
 
-        # TODO(houj04): fix ut threshold
+        # TODO(houj04) remove debug codes after correctness check
+        max_diff_q_grad = np.max(
+            np.abs(float_q_grad.numpy() - float_q_grad_.numpy())
+        )
+        mean_diff_q_grad = np.mean(
+            np.abs(float_q_grad.numpy() - float_q_grad_.numpy())
+        )
+        print("max_diff_q_grad:", max_diff_q_grad)
+        print("mean_diff_q_grad:", mean_diff_q_grad)
+
+        max_diff_k_grad = np.max(
+            np.abs(float_k_grad.numpy() - float_k_grad_.numpy())
+        )
+        mean_diff_k_grad = np.mean(
+            np.abs(float_k_grad.numpy() - float_k_grad_.numpy())
+        )
+        print("max_diff_k_grad:", max_diff_k_grad)
+        print("mean_diff_k_grad:", mean_diff_k_grad)
+
+        max_diff_v_grad = np.max(
+            np.abs(float_v_grad.numpy() - float_v_grad_.numpy())
+        )
+        mean_diff_v_grad = np.mean(
+            np.abs(float_v_grad.numpy() - float_v_grad_.numpy())
+        )
+        print("max_diff_v_grad:", max_diff_v_grad)
+        print("mean_diff_v_grad:", mean_diff_v_grad)
+
         np.testing.assert_allclose(
-            float_q_grad, float_q_grad_, rtol=1e-01, atol=1e-01
+            float_q_grad, float_q_grad_, rtol=self.rtol, atol=self.atol
         )
         np.testing.assert_allclose(
-            float_k_grad, float_k_grad_, rtol=1e-01, atol=1e-01
+            float_k_grad, float_k_grad_, rtol=self.rtol, atol=self.atol
         )
         np.testing.assert_allclose(
-            float_v_grad, float_v_grad_, rtol=1e-01, atol=1e-01
+            float_v_grad, float_v_grad_, rtol=self.rtol, atol=self.atol
         )
+
+
+class TestFlashAttentionAPITestFP16(TestFlashAttentionAPI):
+    def setUp(self):
+        self.place = paddle.XPUPlace(0)
+        self.shape = (1, 128, 2, 32)
+        self.dtype = 'float16'
+        self.dropout = 0.0
+        self.causal = True
+        self.return_softmax = False
+        # TODO(houj04) fix ut threshold after correctness check
+        self.rtol = 5e-3
+        self.atol = 5e-3
+
+
+class TestFlashAttentionAPITestBF16(TestFlashAttentionAPI):
+    def setUp(self):
+        self.place = paddle.XPUPlace(0)
+        self.shape = (1, 128, 2, 32)
+        self.dtype = 'bfloat16'
+        self.dropout = 0.0
+        self.causal = True
+        self.return_softmax = False
+        # TODO(houj04) fix ut threshold after correctness check
+        self.rtol = 1e-1
+        self.atol = 1e-1
 
 
 if __name__ == '__main__':
