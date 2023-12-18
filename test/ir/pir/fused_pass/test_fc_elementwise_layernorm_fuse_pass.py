0223416a5a5cc9c44d666af7706e547d6a3f0559
bukejiyu <395822456@qq.com>
2023-12-18 09:15:14 +0000

    bug fix

--- test/ir/pir/fused_pass/test_fc_elementwise_layernorm_fuse_pass.py
+++ test/ir/pir/fused_pass/test_fc_elementwise_layernorm_fuse_pass.py
@@ -42,58 +42,69 @@ class TestFcElementwiseLayerNormFusePattern(PassTest):
         for x_shape in [[3, 2]]:
             for w_shape in [[2, 3]]:
                 for y_shape in [[1, 3]]:
-                    for with_relu in [True, False]:
-                        with paddle.pir_utils.IrGuard():
-                            pir_program = paddle.static.Program()
-                            with paddle.pir.core.program_guard(pir_program):
-                                x = paddle.static.data(
-                                    name='x', shape=x_shape, dtype='float32'
-                                )
-                                w = paddle.static.data(
-                                    name='w', shape=w_shape, dtype='float32'
-                                )
-                                y = paddle.static.data(
-                                    name='y', shape=y_shape, dtype='float32'
-                                )
-                                if with_relu:
-                                    relu_op = paddle.nn.ReLU()
-                                    fc_out = relu_op(
-                                        paddle.add(paddle.matmul(x, w), y)
+                    for bias_shape in [[3, 3]]:
+                        for with_relu in [True, False]:
+                            with paddle.pir_utils.IrGuard():
+                                pir_program = paddle.static.Program()
+                                with paddle.pir.core.program_guard(pir_program):
+                                    x = paddle.static.data(
+                                        name='x', shape=x_shape, dtype='float32'
                                     )
-                                else:
-                                    fc_out = paddle.add(paddle.matmul(x, w), y)
+                                    w = paddle.static.data(
+                                        name='w', shape=w_shape, dtype='float32'
+                                    )
+                                    y = paddle.static.data(
+                                        name='y', shape=y_shape, dtype='float32'
+                                    )
+                                    if with_relu:
+                                        relu_op = paddle.nn.ReLU()
+                                        fc_out = relu_op(
+                                            paddle.add(paddle.matmul(x, w), y)
+                                        )
+                                    else:
+                                        fc_out = paddle.add(
+                                            paddle.matmul(x, w), y
+                                        )
 
-                                bias1 = paddle.static.data(
-                                    name='bias1', shape=[3, 3], dtype='float32'
-                                )
+                                    bias1 = paddle.static.data(
+                                        name='bias1',
+                                        shape=bias_shape,
+                                        dtype='float32',
+                                    )
 
-                                add_out = paddle.add(fc_out, bias1)
-                                layer_norm = paddle.nn.LayerNorm(
-                                    add_out.shape[-1:]
-                                )
-                                out = layer_norm(add_out)
-                                out = paddle.assign(out)
-                                self.pass_list.append('fc_fuse_pass')
-                                self.pass_list.append(
-                                    'fc_elementwise_layernorm_fuse_pass'
-                                )
-                                self.feeds = {
-                                    "x": np.random.random([1, 2, 2]).astype(
-                                        "float32"
-                                    ),
-                                    "w": np.random.random([3, 3]).astype(
-                                        "float32"
-                                    ),
-                                }
-                                self.fetch_list = [out]
-                                self.valid_op_map = {
-                                    "pd_op.add": 0,
-                                    "pd_op.relu": 0,
-                                    "pd_op.matmul": 0,
-                                    "pd_op.fused_fc_elementwise_layernorm": 1,
-                                }
+                                    add_out = paddle.add(fc_out, bias1)
+                                    layer_norm = paddle.nn.LayerNorm(
+                                        add_out.shape[-1:]
+                                    )
+                                    out = layer_norm(add_out)
+                                    out = paddle.assign(out)
+                                    self.pass_list.append('fc_fuse_pass')
+                                    self.pass_list.append(
+                                        'fc_elementwise_layernorm_fuse_pass'
+                                    )
+                                    self.feeds = {
+                                        "x": np.random.random(x_shape).astype(
+                                            "float32"
+                                        ),
+                                        "w": np.random.random(w_shape).astype(
+                                            "float32"
+                                        ),
+                                        "y": np.random.random(y_shape).astype(
+                                            "float32"
+                                        ),
+                                        "bias1": np.random.random(
+                                            bias_shape
+                                        ).astype("float32"),
+                                    }
+                                    self.fetch_list = [out]
+                                    self.valid_op_map = {
+                                        "pd_op.add": 0,
+                                        "pd_op.relu": 0,
+                                        "pd_op.matmul": 0,
+                                        "pd_op.fused_fc_elementwise_layernorm": 1,
+                                    }
 
-                                yield pir_program, False
+                                    yield pir_program, False
 
     def setUp(self):
         self.place_runtime = "gpu"
