cdc0cfe53a06fe02841fb8659b03b626305eb017
zhiqiu <chenqiuliang@baidu.com>
2023-12-20 16:31:57 +0800

    fix ut

--- test/auto_parallel/hybrid_strategy/semi_auto_llama.py
+++ test/auto_parallel/hybrid_strategy/semi_auto_llama.py
@@ -183,7 +183,7 @@ class TestLlamaAuto:
 
     def run_test_cases(self):
         self.run_llama(to_static=0)
-        self.run_llama(to_static=0)
+        self.run_llama(to_static=1)
 
 
 if __name__ == '__main__':
