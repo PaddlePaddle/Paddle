546752eb694e3de00dbfd313b46c7fa5c400c903
yuanlehome <yuanlehome@163.com>
2023-12-21 05:24:18 +0000

    rewrite dce pass

--- paddle/fluid/pir/transforms/dead_code_elimination_pass.cc
+++ paddle/fluid/pir/transforms/dead_code_elimination_pass.cc
@@ -15,63 +15,45 @@
 #include "paddle/fluid/pir/transforms/dead_code_elimination_pass.h"
 
 #include "paddle/fluid/pir/dialect/operator/ir/pd_op.h"
+#include "paddle/pir/core/block.h"
 #include "paddle/pir/core/builtin_op.h"
 #include "paddle/pir/core/op_trait.h"
-#include "paddle/pir/core/program.h"
-#include "paddle/pir/dialect/control_flow/ir/cf_op.h"
 #include "paddle/pir/pass/pass.h"
 #include "paddle/pir/pass/pass_registry.h"
-#include "paddle/pir/pattern_rewrite/frozen_rewrite_pattern_set.h"
-#include "paddle/pir/pattern_rewrite/pattern_match.h"
-#include "paddle/pir/pattern_rewrite/pattern_rewrite_driver.h"
 
 namespace {
-
-class DeadCodeEliminationPattern : public pir::RewritePattern {
- public:
-  DeadCodeEliminationPattern(
-      pir::IrContext* context,
-      pir::PatternBenefit benefit = 1,
-      const std::vector<std::string>& generated_names = {})
-      : RewritePattern(MatchAnyOpTypeTag(), benefit, context, generated_names) {
-  }
-
-  bool Match(pir::Operation* op) const override {
-    if (op->HasTrait<pir::SideEffectTrait>()) return false;
-
-    if (op->isa<paddle::dialect::DataOp>()) {
-      return false;
-    }
-    return op->use_empty();
-  }
-
-  void Rewrite(pir::Operation* op,
-               pir::PatternRewriter& rewriter) const override {  // NOLINT
-    rewriter.EraseOp(op);
-  }
-};
 
 class DeadCodeEliminationPass : public pir::Pass {
  public:
   DeadCodeEliminationPass() : pir::Pass("dead_code_elimination_pass", 0) {}
 
-  bool Initialize(pir::IrContext* context) override {
-    pir::RewritePatternSet ps(context);
-    ps.Add<DeadCodeEliminationPattern>(context);
-    patterns_ = pir::FrozenRewritePatternSet(std::move(ps));
-    return true;
-  }
-
   void Run(pir::Operation* op) override {
-    pir::GreedyRewriteConfig cfg;
-    cfg.use_top_down_traversal = true;
-    cfg.max_iterations = 10;
-    auto [_, num_rewrites] = pir::ApplyPatternsGreedily(op, patterns_, cfg);
-    PrintStatistics(num_rewrites);
+    VLOG(6) << "apply dead_code_elimination_pass";
+    int64_t num_erasers{0};
+    EraseOp(*op->GetParentProgram()->block(), &num_erasers);
+    PrintStatistics(num_erasers);
   }
 
  private:
-  pir::FrozenRewritePatternSet patterns_;
+  void EraseOp(const pir::Block& block, int64_t* num_erasers) {
+    for (auto& op : block) {
+      if (op.HasTrait<pir::SideEffectTrait>() ||
+          op.isa<paddle::dialect::DataOp>()) {
+        continue;
+      }
+      if (op.use_empty()) {
+        op.Erase();
+        (*num_erasers)++;
+      } else {
+        for (size_t i = 0; i < op.num_regions(); ++i) {
+          auto& inner_region = op.region(i);
+          for (auto& inner_block : inner_region) {
+            EraseOp(inner_block, num_erasers);
+          }
+        }
+      }
+    }
+  }
 };
 
 }  // namespace
