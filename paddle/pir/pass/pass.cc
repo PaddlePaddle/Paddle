6f91e772e0723deb00d3494341c7327d020aeba7
bukejiyu <395822456@qq.com>
2023-12-21 07:06:12 +0000

    code style

--- paddle/pir/pass/pass.cc
+++ paddle/pir/pass/pass.cc
@@ -35,16 +35,12 @@ Pass::~Pass() = default;
 bool Pass::CanApplyOn(Operation* op) const { return op->num_regions() > 0; }
 
 void Pass::PrintStatistics(int64_t match_count) const {
-  if (match_count != 0) {
-    LOG(INFO) << "--- detected [" << match_count << "] subgraphs!";
-  }
+  LOG(INFO) << "--- detected [" << match_count << "] subgraphs!";
 }
 
 void Pass::PrintStatistics(int64_t match_count, int64_t all_count) const {
-  if (all_count != 0 && match_count != 0) {
-    LOG(INFO) << "--- detected [" << match_count << "/" << all_count
-              << "] subgraphs!";
-  }
+  LOG(INFO) << "--- detected [" << match_count << "/" << all_count
+            << "] subgraphs!";
 }
 
 void Pass::PrintStatistics(const std::string& custom_log) const {
