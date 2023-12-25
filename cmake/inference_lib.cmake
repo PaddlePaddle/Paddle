7ad8acefef9e9e1be9ecf70d605712f77de6d0b2
risemeup1 <515586620@qq.com>
2023-12-25 06:28:35 +0000

    fix windows bug

--- cmake/inference_lib.cmake
+++ cmake/inference_lib.cmake
@@ -236,7 +236,11 @@ copy(
 copy_part_of_thrid_party(inference_lib_dist ${PADDLE_INFERENCE_INSTALL_DIR})
 
 set(src_dir "${PADDLE_SOURCE_DIR}/paddle/fluid")
-
+set(paddle_common_lib ${PADDLE_BINARY_DIR}/paddle/common/libcommon.*)
+copy(
+  inference_lib_dist
+  SRCS ${paddle_common_lib}
+  DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/lib)
 if(WIN32)
   if(WITH_STATIC_LIB)
     set(paddle_inference_lib
@@ -268,11 +272,6 @@ else()
       SRCS ${paddle_phi_lib}
       DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/lib)
   endif()
-  set(paddle_common_lib ${PADDLE_BINARY_DIR}/paddle/common/libcommon.*)
-  copy(
-    inference_lib_dist
-    SRCS ${paddle_common_lib}
-    DSTS ${PADDLE_INFERENCE_INSTALL_DIR}/paddle/lib)
 endif()
 
 copy(
