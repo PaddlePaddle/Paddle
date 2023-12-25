3b18fe659f0e0a444caafdec24aea57ec50a60e4
LiYuRio <liyuruijx@163.com>
2023-12-25 15:19:32 +0800

    remove test case in hybrid strategy

--- test/auto_parallel/hybrid_strategy/test_semi_auto_parallel_hybrid_strategy.py
+++ test/auto_parallel/hybrid_strategy/test_semi_auto_parallel_hybrid_strategy.py
@@ -126,51 +126,5 @@ class TestSemiAutoParallelHybridStrategyWithSP(
             ckpt_path.cleanup()
 
 
-class TestSemiAutoParallelCrossMeshReshard(test_base.CommunicationTestDistBase):
-    def setUp(self):
-        super().setUp(
-            num_of_devices=4,
-            timeout=120,
-            nnode=1,
-        )
-        self._default_envs = {
-            "dtype": "float32",
-            "seed": "2023",
-        }
-        self._changeable_envs = {"backend": ["gpu"]}
-
-    def test_simple_net_cross_mesh_reshard(self):
-        envs_list = test_base.gen_product_envs_list(
-            self._default_envs, self._changeable_envs
-        )
-        for envs in envs_list:
-            self.run_test_case(
-                "semi_auto_parallel_cross_mesh_reshard.py",
-                user_defined_envs=envs,
-            )
-
-
-class TestSemiAutoParallelNdCrossMeshReshard(
-    test_base.CommunicationTestDistBase
-):
-    def setUp(self):
-        super().setUp(num_of_devices=8, timeout=200, nnode=1)
-        self._default_envs = {
-            "dtype": "float32",
-            "seed": "2023",
-        }
-        self._changeable_envs = {"backend": ["gpu"]}
-
-    def test_simple_net_bybrid_strategy(self):
-        envs_list = test_base.gen_product_envs_list(
-            self._default_envs, self._changeable_envs
-        )
-        for envs in envs_list:
-            self.run_test_case(
-                "semi_auto_parallel_nd_cross_mesh_reshard.py",
-                user_defined_envs=envs,
-            )
-
-
 if __name__ == "__main__":
     unittest.main()
