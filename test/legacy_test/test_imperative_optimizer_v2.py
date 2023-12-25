1fe24f0d37637703e847e4eb75b592bd5be741bd
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2023-12-25 14:28:02 +0800

    【PIR API adaptor No.124】 optimizer.Lamb (#58881)

--- test/legacy_test/test_imperative_optimizer_v2.py
+++ test/legacy_test/test_imperative_optimizer_v2.py
@@ -22,6 +22,7 @@ import paddle
 from paddle import base
 from paddle.base import core
 from paddle.distributed.fleet.meta_optimizers import DGCMomentumOptimizer
+from paddle.pir_utils import test_with_pir_api
 
 # Note(wangzhongpu)
 # In dygraph, don't support ModelAverage, DGCMomentumOptimizer, ExponentialMovingAverage, PipelineOptimizer, LookaheadOptimizer, RecomputeOptimizer.
@@ -790,6 +791,7 @@ class TestImperativeLambOptimizer(TestImperativeOptimizerBase):
         return optimizer
 
     # should fix: may fail in CI-windows
+    @test_with_pir_api
     def _test_lamb(self):
         self._check_mlp()
 
