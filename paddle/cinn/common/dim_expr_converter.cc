1b0bdf874983d03774a02d119cb21ac05dc6dcc4
jiahongyu <jiahongyu@baidu.com>
2023-12-26 04:53:07 +0000

    Add Unittest

--- paddle/cinn/common/dim_expr_converter.cc
+++ paddle/cinn/common/dim_expr_converter.cc
@@ -1,20 +1,31 @@
+// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "paddle/cinn/common/dim_expr_converter.h"
 #include "paddle/cinn/common/ir_util.h"
 
 namespace cinn::common {
-using namespace symbol;
+using namespace symbol;  // NOLINT
 
 namespace {
 
 struct DimExprToIrExprVisitor {
-
   ir::Expr ConvertToIrExpr(const DimExpr& dim_expr) {
-    return std::visit(*this, dim_expr);
+    return std::visit(*this, dim_expr.variant());
   }
 
-  ir::Expr operator()(const int64_t& dim) {
-    return ir::Expr(dim);
-  }
+  ir::Expr operator()(const int64_t& dim) { return ir::Expr(dim); }
 
   ir::Expr operator()(const std::string& dim_expr) {
     Var x = ir::_Var_::Make(dim_expr, Int(64));
@@ -23,12 +34,12 @@ struct DimExprToIrExprVisitor {
 
   ir::Expr operator()(const Negative<DimExpr>& dim_expr) {
     const auto& [operand] = *dim_expr;
-    return ir::Sub::Make(ir::Expr(0), ConvertToIrExpr(operand));
+    return ir::Sub::Make(ir::Expr(std::int64_t(0)), ConvertToIrExpr(operand));
   }
 
   ir::Expr operator()(const Reciprocal<DimExpr>& dim_expr) {
     const auto& [operand] = *dim_expr;
-    return ir::Div::Make(ir::Expr(1), ConvertToIrExpr(operand));
+    return ir::Div::Make(ir::Expr(std::int64_t(1)), ConvertToIrExpr(operand));
   }
 
   ir::Expr operator()(const Add<DimExpr>& dim_expr) {
@@ -76,15 +87,15 @@ struct DimExprToIrExprVisitor {
   }
 
   ir::Expr operator()(const Broadcast<DimExpr>& dim_expr) {
-    LOG(FATAL) << "no support for converting from Broadcast<DimExpr> to ir::Expr";
+    LOG(FATAL)
+        << "no support for converting from Broadcast<DimExpr> to ir::Expr";
   }
-
 };
 
-}
+}  // namespace
 
 ir::Expr DimExprConverter::ConvertToIrExpr(const DimExpr& dim_expr) const {
   return DimExprToIrExprVisitor().ConvertToIrExpr(dim_expr);
 }
 
-}
+}  // namespace cinn::common
