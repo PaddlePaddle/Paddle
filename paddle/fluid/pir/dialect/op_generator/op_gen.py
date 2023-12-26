de19af9a1c84c2ba669e164453bf90f9be10f03a
Yangrl <2535184404@qq.com>
2023-12-26 05:04:36 +0000

    update

--- paddle/fluid/pir/dialect/op_generator/op_gen.py
+++ paddle/fluid/pir/dialect/op_generator/op_gen.py
@@ -89,7 +89,7 @@ IR_DECLARE_EXPLICIT_TYPE_ID({op_name})
 """
 
 OP_DECLARE_TEMPLATE = """
-class TEST_API {op_name} : public pir::Op<{op_name}{interfaces}{traits}> {{
+class {TEST_API} {op_name} : public pir::Op<{op_name}{interfaces}{traits}> {{
  public:
   using Op::Op;
   static const char *name() {{ return "{dialect_op_name}"; }}
@@ -1351,8 +1351,12 @@ def AutoCodeGen(op_info_items, all_op_info_items, namespaces, dialect_name):
                     )
 
                 # gen op_declare_str/op_defined_str
+                TEST_API = ""
+                if op_class_name in ["AbsOp", "FullOp"]:
+                    TEST_API = "TEST_API"
                 if len(op_non_mutable_attribute_name_list) == 0:
                     op_declare_str = OP_DECLARE_TEMPLATE.format(
+                        TEST_API=TEST_API,
                         op_name=op_class_name,
                         dialect_op_name=op_dialect_name,
                         interfaces=op_interfaces_str,
@@ -1372,6 +1376,7 @@ def AutoCodeGen(op_info_items, all_op_info_items, namespaces, dialect_name):
                     op_defined_str = ""
                 else:
                     op_declare_str = OP_DECLARE_TEMPLATE.format(
+                        TEST_API=TEST_API,
                         op_name=op_class_name,
                         dialect_op_name=op_dialect_name,
                         interfaces=op_interfaces_str,
