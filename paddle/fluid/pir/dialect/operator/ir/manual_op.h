8d03c690be216970179b9efca645d2993068cc5d
zhangbo9674 <zhangbo54@baidu.com>
2023-12-26 06:51:48 +0000

    fix

--- paddle/fluid/pir/dialect/operator/ir/manual_op.h
+++ paddle/fluid/pir/dialect/operator/ir/manual_op.h
@@ -314,8 +314,8 @@ class SliceArrayDenseOp
  public:
   using Op::Op;
   static const char *name() { return "pd_op.slice_array_dense"; }
-  static const char *attributes_name[1];
-  static constexpr uint32_t attributes_num = 1;
+  static constexpr const char **attributes_name = nullptr;
+  static constexpr uint32_t attributes_num = 0;
   static OpInfoTuple GetOpInfo();
   void VerifySig();
 
