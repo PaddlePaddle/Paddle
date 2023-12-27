69e1af87d49191d420d78f18b381d4fc8aa8e407
jiahongyu <jiahongyu@baidu.com>
2023-12-27 06:40:00 +0000

    Add IR_API

--- paddle/pir/dialect/shape/utils/dim_expr_util.h
+++ paddle/pir/dialect/shape/utils/dim_expr_util.h
@@ -16,20 +16,22 @@
 
 #include <optional>
 #include "paddle/pir/core/builder.h"
+#include "paddle/pir/core/dll_decl.h"
 #include "paddle/pir/dialect/shape/utils/dim_expr.h"
 
 namespace symbol {
 
-::pir::Attribute ConvertDimExprToAttribute(::pir::Builder* builder,
-                                           const DimExpr& dim_expr);
-std::optional<DimExpr> ConvertAttributeToDimExpr(::pir::Attribute attribute);
+IR_API ::pir::Attribute ConvertDimExprToAttribute(::pir::Builder* builder,
+                                                  const DimExpr& dim_expr);
+IR_API std::optional<DimExpr> ConvertAttributeToDimExpr(
+    ::pir::Attribute attribute);
 
-std::optional<DimExpr> SubstituteDimExpr(
+IR_API std::optional<DimExpr> SubstituteDimExpr(
     const DimExpr& dim_expr,
     const std::function<std::optional<DimExpr>(const std::string& symbol_name)>&
         DimExpr4SymbolName);
 
-std::function<std::optional<DimExpr>(const std::string& symbol_name)>
+IR_API std::function<std::optional<DimExpr>(const std::string& symbol_name)>
 MakeGetterDimExpr4SymbolName(
     const std::vector<std::tuple<std::string /*symbol_name*/,
                                  int /*in_tensor_idx*/,
