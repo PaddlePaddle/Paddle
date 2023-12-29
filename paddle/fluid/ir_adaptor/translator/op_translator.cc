be3b056d80479f130ea916c77c97899b5c53212e
kangguangli <kangguangli@hotmail.com>
2023-12-29 10:12:18 +0000

    fix

--- paddle/fluid/ir_adaptor/translator/op_translator.cc
+++ paddle/fluid/ir_adaptor/translator/op_translator.cc
@@ -535,8 +535,9 @@ std::vector<pir::Value> OpTranscriber::GenerateOperationInput(
                  info.name,
                  op_desc.Type());
       IR_ENFORCE(param_map->count(legacy_input_vars[0]),
-                 "Input [%s] of op [%s] not found in param map",
+                 "Input [%s: %s] of op [%s] not found in param map",
                  info.name,
+                 legacy_input_vars[0],
                  op_desc.Type());
       auto defining_info = (*param_map)[legacy_input_vars[0]];
       op_inputs.push_back(defining_info.value);
