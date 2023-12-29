b8d51ab3aff35e44c421eb7f0beb447ddb2957b1
gouzi <530971494@qq.com>
2023-12-29 15:14:15 +0800

    clean I001

--- python/paddle/distribution/transform.py
+++ python/paddle/distribution/transform.py
@@ -25,22 +25,6 @@ from paddle.distribution import (
     variable,
 )
 
-__all__ = [
-    'Transform',
-    'AbsTransform',
-    'AffineTransform',
-    'ChainTransform',
-    'ExpTransform',
-    'IndependentTransform',
-    'PowerTransform',
-    'ReshapeTransform',
-    'SigmoidTransform',
-    'SoftmaxTransform',
-    'StackTransform',
-    'StickBreakingTransform',
-    'TanhTransform',
-]
-
 
 class Type(enum.Enum):
     """Mapping type of a transformation."""
