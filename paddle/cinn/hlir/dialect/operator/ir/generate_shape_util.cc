08dd3c081ce6c1729a37b8d73c13fb552c939209
jiahy0825 <jiahongyu@baidu.com>
2024-01-02 13:48:16 +0000

    fix complier complaint

--- paddle/cinn/hlir/dialect/operator/ir/generate_shape_util.cc
+++ paddle/cinn/hlir/dialect/operator/ir/generate_shape_util.cc
@@ -598,9 +598,9 @@ std::vector<pir::Value> GetMinimalInputs(
     const std::vector<pir::Value>& input_tensors) {
   std::unordered_set<symbol::DimExpr> handdled_dim_exprs;
   std::unordered_set<pir::Value> first_occurred_input_tensors;
-  auto TryCollectFirstOcurredInput_tensor = [&](pir::Value input_tensor, const auto& dim_exprs) {
+  auto TryCollectFirstOcurredInput_tensor = [&](pir::Value input_tensor, const std::vector<symbol::DimExpr>& dim_exprs) {
     for (const auto& dim_expr : dim_exprs) {
-      if (dim_expr.Has<int64_t>()) continue;
+      if (dim_expr.isa<int64_t>()) continue;
       if (!handdled_dim_exprs.insert(dim_expr).second) {
         first_occurred_input_tensors.insert(input_tensor);
       }
