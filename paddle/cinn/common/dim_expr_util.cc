831bd25d971a6217566ac69860fa7d85a072a229
jiahongyu <jiahongyu@baidu.com>
2024-01-02 05:17:18 +0000

    Fix compile error

--- paddle/cinn/common/dim_expr_util.cc
+++ paddle/cinn/common/dim_expr_util.cc
@@ -1,22 +1,36 @@
+// Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "paddle/cinn/common/dim_expr_util.h"
 
 namespace cinn::common {
-using namespace symbol;
+using namespace symbol;  // NOLINT
 
 namespace {
 
 class SubstituteDimExprHelper final {
  public:
   explicit SubstituteDimExprHelper(
-    const std::unordered_map<symbol::DimExpr, symbol::DimExpr>& pattern_to_replacement)
-    : pattern_to_replacement_(pattern_to_replacement) {}
+      const std::unordered_map<symbol::DimExpr, symbol::DimExpr>&
+          pattern_to_replacement)
+      : pattern_to_replacement_(pattern_to_replacement) {}
 
   std::optional<DimExpr> Substitute(const DimExpr& dim_expr) {
     auto iter = pattern_to_replacement_.find(dim_expr);
     if (iter != pattern_to_replacement_.end()) return iter->second;
-    return std::visit([&](const auto& impl) {
-      return SubstituteImpl(impl);
-    }, dim_expr.variant());
+    return std::visit([&](const auto& impl) { return SubstituteImpl(impl); },
+                      dim_expr.variant());
   }
 
  private:
@@ -66,14 +80,15 @@ class SubstituteDimExprHelper final {
 
   template <typename T>
   std::optional<DimExpr> SubstituteVariadic(const T& dim_expr) {
-    const auto& [operands] = *dim_expr;
+    const auto& operands = *(dim_expr.operands);
     List<DimExpr> substituted_operands{};
     size_t replace_cnt = 0;
     for (const auto& operand : operands) {
       const auto& substituted_operand = Substitute(operand);
       replace_cnt += substituted_operand.has_value();
-      substituted_operands.push_back(
-        substituted_operand.has_value() ? substituted_operand.value() : operand);
+      substituted_operands->push_back(substituted_operand.has_value()
+                                          ? substituted_operand.value()
+                                          : operand);
     }
     if (replace_cnt == 0) return std::nullopt;
     return T{substituted_operands};
@@ -86,8 +101,10 @@ class SubstituteDimExprHelper final {
 
 symbol::DimExpr SubstituteDimExpr(
     const symbol::DimExpr& dim_expr,
-    const std::unordered_map<symbol::DimExpr, symbol::DimExpr>& pattern_to_replacement) {
-  const auto& opt_replaced = SubstituteDimExprHelper(pattern_to_replacement).Substitute(dim_expr);
+    const std::unordered_map<symbol::DimExpr, symbol::DimExpr>&
+        pattern_to_replacement) {
+  const auto& opt_replaced =
+      SubstituteDimExprHelper(pattern_to_replacement).Substitute(dim_expr);
   return opt_replaced.has_value() ? opt_replaced.value() : dim_expr;
 }
 
