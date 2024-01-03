0f8ea5a35b8365b6a9684f052372730f07ceb9cb
pangengzheng <pangengzheng@baidu.com>
2024-01-03 16:25:46 +0800

    static save dynamic load test

--- test/auto_parallel/hybrid_strategy/semi_auto_parallel_mutual_load_between_dynamic_and_static.py
+++ test/auto_parallel/hybrid_strategy/semi_auto_parallel_mutual_load_between_dynamic_and_static.py
@@ -35,7 +35,7 @@ class TestSemiAutoParallelMutualLoadBetweenDynamicAndStatic(
     TestSimpleNetForSemiAutoParallel
 ):
     def __init__(self):
-        self._dynamic_ckpt_path = os.environ.get("ckpt_path")
+        self._ckpt_path = os.environ.get("ckpt_path")
         self._seed = os.environ.get("seed", 123)
 
     def create_data_loader(self):
@@ -62,6 +62,23 @@ class TestSemiAutoParallelMutualLoadBetweenDynamicAndStatic(
                 opt.clear_grad()
         return np.array(loss_list)
 
+    def run_dy2static(self, layer, opt, data_loader):
+        # create loss
+        loss_fn = nn.MSELoss()
+
+        # static training
+        dist_model, dist_loader = dist.to_static(
+            layer, data_loader, loss_fn, opt
+        )
+        loss_list = []
+        dist_model.train()
+        for epoch in range(5):
+            for batch_id, (image, label) in enumerate(dist_loader()):
+                loss = dist_model(image, label)
+                loss_list.append(loss)
+
+        return np.array(loss_list), dist_model
+
     def set_random_seed(self, seed):
         random.seed(seed)
         np.random.seed(seed)
@@ -82,7 +99,10 @@ class TestSemiAutoParallelMutualLoadBetweenDynamicAndStatic(
         )
         dy_losses = self.run_dynamic(dy_layer, dy_opt, data_loader)
         saved_dy_layer_state_dict = dy_layer.state_dict()
-        dist.save_state_dict(saved_dy_layer_state_dict, self._dynamic_ckpt_path)
+        ckpt_path = os.path.join(
+            self._ckpt_path, "test_dygraph_save_static_load"
+        )
+        dist.save_state_dict(saved_dy_layer_state_dict, ckpt_path)
         dist.barrier()
 
         dy2static_opt = paddle.optimizer.SGD(
@@ -113,7 +133,7 @@ class TestSemiAutoParallelMutualLoadBetweenDynamicAndStatic(
                 )
             ), f"key:{k}, v:{v}, expected_state_dict[k]:{expected_state_dict[k]}"
 
-        dist.load_state_dict(state_dict_to_load, self._dynamic_ckpt_path)
+        dist.load_state_dict(state_dict_to_load, ckpt_path)
         dist_model.set_state_dict(state_dict_to_load)
 
         program_state_dict = dist_model.state_dict(mode="param")
@@ -127,8 +147,69 @@ class TestSemiAutoParallelMutualLoadBetweenDynamicAndStatic(
                 expected_state_dict[k].numpy(),
             )
 
+    def test_static_save_dynamic_load(self):
+        paddle.disable_static()
+        mesh = dist.ProcessMesh([0, 1], dim_names=["x"])
+        # set seed to promise the same input for different tp rank
+        self.set_random_seed(self._seed)
+        data_loader = self.create_data_loader()
+
+        dy_layer = dist.shard_layer(
+            DemoNet("dp_mp_hybrid_strategy"), mesh, self.shard_fn
+        )
+
+        dy2static_opt = paddle.optimizer.SGD(
+            learning_rate=0.1, parameters=dy_layer.parameters()
+        )
+        dy2static_losses, dist_model = self.run_dy2static(
+            dy_layer, dy2static_opt, data_loader
+        )
+
+        saved_static_layer_state_dict = dist_model.state_dict("param")
+        ckpt_path = os.path.join(
+            self._ckpt_path, "test_static_save_dynamic_load"
+        )
+        dist.save_state_dict(saved_static_layer_state_dict, ckpt_path)
+        dist.barrier()
+
+        paddle.disable_static()
+        need_load_state_dict = {}
+        expected_state_dict = {}
+        with paddle.base.dygraph.guard():
+            for k, v in saved_static_layer_state_dict.items():
+                expected_state_dict[k] = v._local_value().clone()
+                need_load_state_dict[k] = paddle.zeros_like(v)
+        dy_layer.set_state_dict(need_load_state_dict)
+        state_dict_to_load = dy_layer.state_dict()
+        assert len(state_dict_to_load) == len(expected_state_dict)
+        for k, v in state_dict_to_load.items():
+            assert (
+                k in expected_state_dict
+            ), f"key {k} not in expected_state_dict:{expected_state_dict}"
+            assert np.any(
+                np.not_equal(
+                    v._local_value().numpy(),
+                    expected_state_dict[k].numpy(),
+                )
+            ), f"key:{k}, v:{v}, expected_state_dict[k]:{expected_state_dict[k]}"
+
+        dist.load_state_dict(state_dict_to_load, ckpt_path)
+        dy_layer.set_state_dict(state_dict_to_load)
+
+        state_dict = dy_layer.state_dict()
+        assert len(expected_state_dict) == len(state_dict)
+        for k, v in state_dict.items():
+            assert (
+                k in expected_state_dict
+            ), f"key {k} not in expected_state_dict:{expected_state_dict}"
+            np.testing.assert_equal(
+                v._local_value().numpy(),
+                expected_state_dict[k].numpy(),
+            )
+
     def run_test_case(self):
         self.test_dygraph_save_static_load()
+        self.test_static_save_dynamic_load()
 
 
 if __name__ == "__main__":
