476244f5295edfcb2b2bf176771453fca22ab3f9
xiaoguoguo626807 <100397923+xiaoguoguo626807@users.noreply.github.com>
2024-01-03 14:00:36 +0800

    【pir】 add tensorarray op createarrylike, add_n (#60460)
    * optimize backward
    
    * [PIR] add vjp interface for while op
    
    * [PIR] fix ci error.
    
    * modify while stopgradient
    
    * merge
    
    * modify while grad bug
    
    * modify while grad op
    
    * modify
    
    * increment vp
    
    * [PIR] add get_used_external_value interface for block.
    
    * while case
    
    * delete print
    
    * delete print
    
    * Update python/paddle/autograd/ir_backward.py
    
    * [PIR] add unit_test for get_used_external_value
    
    * modify while_loop
    
    * code_style
    
    * modofy ci bug
    
    * modify while api
    
    * modify ci
    
    * modify array
    
    * Update python/paddle/autograd/ir_backward.py
    
    * Update test/legacy_test/test_cond.py
    
    * update
    
    * modify array_write grad info
    
    * merge
    
    * add_n and createarraylike
    
    * conflict
    
    * modify exe bug
    
    * modify kernel choose
    
    ---------
    
    Co-authored-by: winter-wang <1030748926@qq.com>

--- paddle/fluid/framework/new_executor/pir_adaptor/pir_adaptor_util.h
+++ paddle/fluid/framework/new_executor/pir_adaptor/pir_adaptor_util.h
@@ -213,9 +213,13 @@ void BuildPhiContext(pir::Operation* op,
         } else if (variable_array[i]->IsType<phi::SelectedRows>()) {
           inputs.emplace_back(InType(const_cast<phi::SelectedRows*>(
               &(variable_array[i]->Get<phi::SelectedRows>()))));
+        } else if (variable_array[i]->IsType<phi::TensorArray>()) {
+          inputs.emplace_back(InType(const_cast<phi::TensorArray*>(
+              &(variable_array[i]->Get<phi::TensorArray>()))));
         } else {
           PADDLE_THROW(phi::errors::Unimplemented(
-              "Only support Vector<DenseTensor> and vector<SelectedRows> now, "
+              "Only support Vector<DenseTensor> and vector<SelectedRows> "
+              "and vector<TensorArray> now "
               "not support vector<%d>.",
               variable_array[i]->Type()));
         }
