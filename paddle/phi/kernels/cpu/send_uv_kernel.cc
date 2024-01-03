d8900198c3268683a47a9517020fbf79c9fa8ffa
Yuanle Liu <yuanlehome@163.com>
2024-01-03 19:42:30 +0800

    [Inference] refine common/*.h for inference lib (#60513)

--- paddle/phi/kernels/cpu/send_uv_kernel.cc
+++ paddle/phi/kernels/cpu/send_uv_kernel.cc
@@ -14,8 +14,8 @@
 
 #include "paddle/phi/kernels/send_uv_kernel.h"
 
+#include "paddle/common/hostdevice.h"
 #include "paddle/phi/backends/cpu/cpu_context.h"
-#include "paddle/phi/core/hostdevice.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/cpu/graph_send_ue_recv_funcs.h"
 #include "paddle/phi/kernels/impl/graph_message_passing_impl.h"
