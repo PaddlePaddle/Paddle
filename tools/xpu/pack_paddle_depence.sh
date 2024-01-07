8b142b64410a5610a0a5506f22d527f6222ddf40
skywalker <weiyong02@baidu.com>
2024-01-07 23:27:56 +0800

    [XPU] provide sandbox as another way of compiling input

--- tools/xpu/pack_paddle_depence.sh
+++ tools/xpu/pack_paddle_depence.sh
@@ -17,12 +17,6 @@
 set -e
 set -x
 
-if [[ $# -eq 8 ]]; then
-  echo "Compiling Paddle with XHPC"
-  XHPC_URL=$7
-  XHPC_DIR_NAME=$8
-fi
-
 XRE_URL=$1
 XRE_DIR_NAME=$2
 
@@ -32,41 +26,87 @@ XDNN_DIR_NAME=$4
 XCCL_URL=$5
 XCCL_DIR_NAME=$6
 
-wget --no-check-certificate ${XRE_URL} -q -O xre.tar.gz
-tar xvf xre.tar.gz
+if [[ $# -eq 8 ]]; then
+  echo "Compiling Paddle with XHPC"
+  XHPC_URL=$7
+  XHPC_DIR_NAME=$8
+fi
 
-wget --no-check-certificate ${XDNN_URL} -q -O xdnn.tar.gz
-tar xvf xdnn.tar.gz
-
-wget --no-check-certificate ${XCCL_URL} -q -O xccl.tar.gz
-tar xvf xccl.tar.gz
-
+XPU_DOWNLOAD_DIR=$(pwd)
+SANDBOX_PATTERN="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+.*"
 mkdir -p xpu/include/xpu
 mkdir -p xpu/lib
 
-if ! [ -z ${XHPC_URL} ]; then
-  echo "Compiling Paddle with XHPC"
-  echo "XHPC_URL: ${XHPC_URL}"
-  wget --no-check-certificate ${XHPC_URL} -q -O xhpc.tar.gz
-  tar xvf xhpc.tar.gz
+function download_from_bos() {
+  wget --no-check-certificate ${XRE_URL} -q -O xre.tar.gz
+  tar xvf xre.tar.gz
 
-  mkdir -p xpu/include/xhpc/xblas
-  mkdir -p xpu/include/xhpc/xfa
+  wget --no-check-certificate ${XDNN_URL} -q -O xdnn.tar.gz
+  tar xvf xdnn.tar.gz
 
-  cp -r ${XHPC_DIR_NAME}/xblas/include/* xpu/include/xhpc/xblas
-  cp -r ${XHPC_DIR_NAME}/xblas/so/* xpu/lib/
+  wget --no-check-certificate ${XCCL_URL} -q -O xccl.tar.gz
+  tar xvf xccl.tar.gz
+}
 
-  cp -r ${XHPC_DIR_NAME}/xdnn/include/* xpu/include/
-  cp -r ${XHPC_DIR_NAME}/xdnn/so/* xpu/lib
+function xhpc_prepare() {
+    if ! [ -z ${XHPC_URL} ]; then
+      echo "XHPC_URL: ${XHPC_URL}"
+      wget --no-check-certificate ${XHPC_URL} -q -O xhpc.tar.gz
+      tar xvf xhpc.tar.gz
 
-  cp -r ${XHPC_DIR_NAME}/xfa/include/* xpu/include/xhpc/xfa
-  cp -r ${XHPC_DIR_NAME}/xfa/so/* xpu/lib/
+      mkdir -p xpu/include/xhpc/xblas
+      mkdir -p xpu/include/xhpc/xfa
+
+      cp -r ${XHPC_DIR_NAME}/xblas/include/* xpu/include/xhpc/xblas
+      cp -r ${XHPC_DIR_NAME}/xblas/so/* xpu/lib/
+
+      cp -r ${XHPC_DIR_NAME}/xdnn/include/* xpu/include/
+      cp -r ${XHPC_DIR_NAME}/xdnn/so/* xpu/lib
+
+      cp -r ${XHPC_DIR_NAME}/xfa/include/* xpu/include/xhpc/xfa
+      cp -r ${XHPC_DIR_NAME}/xfa/so/* xpu/lib/
+    else
+      cp -r $XDNN_DIR_NAME/include/xpu/* xpu/include/xpu/
+      cp -r $XDNN_DIR_NAME/so/* xpu/lib/
+    fi
+}
+
+if [[ $XRE_URL =~ $SANDBOX_PATTERN ]]; then
+    build_from="ipipe"
+
+    if [[ ${XRE_DIR_NAME} == *ubuntu* ]]; then
+        SANDBOX_MOD_PATH=${XPU_DOWNLOAD_DIR}/output/xpu_portfolio_ubuntu/toolkit/modules
+        tar -zxf  ${SANDBOX_MOD_PATH}/xre-ubuntu_1804_x86_64.tar.gz -C ${SANDBOX_MOD_PATH}
+        rm -rf ${SANDBOX_MOD_PATH}/$XRE_DIR_NAME
+        mv ${SANDBOX_MOD_PATH}/xre-ubuntu_1804_x86_64 ${SANDBOX_MOD_PATH}/$XRE_DIR_NAME
+    elif [[ ${XRE_DIR_NAME} == *bdcentos* ]]; then
+        SANDBOX_MOD_PATH=${XPU_DOWNLOAD_DIR}/output/xpu_portfolio_bdcentos/toolkit/modules
+    else
+        echo "unsupport platform from sandbox" 
+        exit 1
+    fi
+
+    # xre
+    cp -r ${SANDBOX_MOD_PATH}/$XRE_DIR_NAME/include/xpu/* xpu/include/xpu/
+    cp -r ${SANDBOX_MOD_PATH}/$XRE_DIR_NAME/so/libxpurt* xpu/lib/
+
+    # xdnn
+    cp -r ${SANDBOX_MOD_PATH}/$XDNN_DIR_NAME/include/xpu/* xpu/include/xpu/
+    cp -r ${SANDBOX_MOD_PATH}/$XDNN_DIR_NAME/so/* xpu/lib/
+
+    # xccl
+    cp -r ${SANDBOX_MOD_PATH}/$XCCL_DIR_NAME/include/* xpu/include/xpu/
+    cp -r ${SANDBOX_MOD_PATH}/$XCCL_DIR_NAME/so/* xpu/lib/
+elif [[ $XRE_URL != *"https://baidu-kunlun-product.su.bcebos.com"* ]]; then
+    build_from="local"
 else
-  cp -r $XDNN_DIR_NAME/include/xpu/* xpu/include/xpu/
-  cp -r $XDNN_DIR_NAME/so/* xpu/lib/
-fi
+    # below is normal way
+    build_from="bos"
+    download_from_bos
+    xhpc_prepare
 
-cp -r $XRE_DIR_NAME/include/xpu/* xpu/include/xpu/
-cp -r $XRE_DIR_NAME/so/libxpurt* xpu/lib/
-cp -r $XCCL_DIR_NAME/include/* xpu/include/xpu/
-cp -r $XCCL_DIR_NAME/so/* xpu/lib/
+    cp -r $XRE_DIR_NAME/include/xpu/* xpu/include/xpu/
+    cp -r $XRE_DIR_NAME/so/libxpurt* xpu/lib/
+    cp -r $XCCL_DIR_NAME/include/* xpu/include/xpu/
+    cp -r $XCCL_DIR_NAME/so/* xpu/lib/
+fi
