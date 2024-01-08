996537e603c9c32133c52cee47e15e46f484fa54
Ryan Jeng <rjeng@nvidia.com>
2024-01-08 00:43:21 -0800

    increase processor count to flashattn

--- cmake/external/flashattn.cmake
+++ cmake/external/flashattn.cmake
@@ -81,6 +81,9 @@ foreach(arch ${NVCC_ARCH_BIN})
   endif()
 endforeach()
 
+include(ProcessorCount)
+ProcessorCount(NUMBER_OF_CORES)
+
 ExternalProject_Add(
   extern_flashattn
   ${EXTERNAL_PROJECT_LOG_ARGS}
@@ -107,7 +110,7 @@ ExternalProject_Add(
              -DCMAKE_POSITION_INDEPENDENT_CODE=ON
              -DCMAKE_BUILD_TYPE=${THIRD_PARTY_BUILD_TYPE}
              -DCMAKE_JOB_POOL_COMPILE:STRING=compile
-             -DCMAKE_JOB_POOLS:STRING=compile=4
+             -DCMAKE_JOB_POOLS:STRING=compile=${NUMBER_OF_CORES}
              -DNVCC_ARCH_BIN=${FA_NVCC_ARCH_BIN}
              ${EXTERNAL_OPTIONAL_ARGS}
   CMAKE_CACHE_ARGS
