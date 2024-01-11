67758d13e9ba9be1f30303eae88816e8e93f4d26
Wang Huan <wanghuan29@baidu.com>
2024-01-11 08:56:30 +0000

    refine

--- test/ir/inference/test_mkldnn_pad3d_op.py
+++ test/ir/inference/test_mkldnn_pad3d_op.py
@@ -78,6 +78,21 @@ class TestOneDNNPad3DOp(MkldnnAutoScanTest):
     def test(self, *args, **kwargs):
         self.run_test(quant=False, *args, **kwargs)
 
+    @given(
+        data_format=st.sampled_from(['NCDHW', 'NDHWC']),
+        use_paddings_tensor=st.sampled_from([True, False]),
+        in_shape=st.sampled_from(
+            [[2, 3, 4, 5, 6], [1, 4, 1, 3, 2], [4, 3, 2, 1, 1], [1, 1, 1, 1, 1]]
+        ),
+        paddings=st.sampled_from(
+            [
+                [0, 0, 0, 0, 0, 0],
+                [1, 2, 0, 1, 2, 1],
+                [2, 5, 11, 3, 4, 3],
+                [0, 5, 0, 1, 0, 2],
+            ]
+        ),
+    )
     @compare_legacy_with_pt
     def test_pir(self, *args, **kwargs):
         self.run_test(quant=False, *args, **kwargs)
