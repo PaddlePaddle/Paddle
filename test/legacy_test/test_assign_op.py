600fc0bf9cb8b9bed5c8c1346195a8d470ad4ad8
SigureMo <sigure.qaq@gmail.com>
2024-01-15 06:50:12 +0000

    remove dup ut

--- test/legacy_test/test_assign_op.py
+++ test/legacy_test/test_assign_op.py
@@ -113,13 +113,13 @@ class TestAssignBFP16Op(op_test.OpTest):
         paddle.disable_static()
 
 
-class TestAssignOpWithLoDTensorArray(unittest.TestCase):
+class TestAssignOpWithTensorArray(unittest.TestCase):
     @test_with_pir_api
-    def test_assign_LoDTensorArray(self):
+    def test_assign_tensor_array(self):
         paddle.enable_static()
         main_program = paddle.static.Program()
         startup_program = paddle.static.Program()
-        with paddle.static.program_guard(main_program):
+        with paddle.static.program_guard(main_program, startup_program):
             x = paddle.static.data(name='x', shape=[100, 10], dtype='float32')
             x.stop_gradient = False
             y = paddle.tensor.fill_constant(
@@ -167,44 +167,8 @@ class TestAssignOpError(unittest.TestCase):
         paddle.disable_static()
 
 
-class TestAssignOApi(unittest.TestCase):
-    def test_assign_LoDTensorArray(self):
-        paddle.enable_static()
-        main_program = Program()
-        startup_program = Program()
-        with program_guard(main_program):
-            x = paddle.static.data(name='x', shape=[100, 10], dtype='float32')
-            x.stop_gradient = False
-            y = paddle.tensor.fill_constant(
-                shape=[100, 10], dtype='float32', value=1
-            )
-            z = paddle.add(x=x, y=y)
-            i = paddle.tensor.fill_constant(shape=[1], dtype='int64', value=0)
-            init_array = paddle.tensor.array_write(x=z, i=i)
-            array = paddle.assign(init_array)
-            sums = paddle.tensor.array_read(array=init_array, i=i)
-            mean = paddle.mean(sums)
-            append_backward(mean)
-
-        place = (
-            base.CUDAPlace(0)
-            if core.is_compiled_with_cuda()
-            else base.CPUPlace()
-        )
-        exe = base.Executor(place)
-        feed_x = np.random.random(size=(100, 10)).astype('float32')
-        ones = np.ones((100, 10)).astype('float32')
-        feed_add = feed_x + ones
-        res = exe.run(
-            main_program,
-            feed={'x': feed_x},
-            fetch_list=[sums.name, x.grad_name],
-        )
-        np.testing.assert_allclose(res[0], feed_add, rtol=1e-05)
-        np.testing.assert_allclose(res[1], ones / 1000.0, rtol=1e-05)
-        paddle.disable_static()
-
-    def test_assign_NumpyArray(self):
+class TestAssignOpApi(unittest.TestCase):
+    def test_assign_numpy_array(self):
         for dtype in [np.bool_, np.float32, np.int32, np.int64]:
             with base.dygraph.guard():
                 array = np.random.random(size=(100, 10)).astype(dtype)
@@ -260,7 +224,7 @@ class TestAssignOApi(unittest.TestCase):
 @unittest.skipIf(
     not paddle.is_compiled_with_cuda(), "FP16 test runs only on GPU"
 )
-class TestAssignOApiFP16(unittest.TestCase):
+class TestAssignOpApiFP16(unittest.TestCase):
     def test_assign_fp16(self):
         x = np.random.uniform(0, 10, [3, 3]).astype(np.float16)
         x = paddle.to_tensor(x)
