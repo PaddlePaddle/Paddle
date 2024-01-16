1897abbdcf0d6b9b4312b7d35fc45a6ee5633575
zyfncg <zhangyunfei07@baidu.com>
2024-01-16 09:37:34 +0000

    update broadcast tree

--- paddle/cinn/common/broadcast_tree.cc
+++ paddle/cinn/common/broadcast_tree.cc
@@ -137,7 +137,8 @@ std::optional<symbol::Broadcastable<symbol::DimExpr>> GetFirstCstrBroadcastable(
       }
     }
     if (lhs_symbol.has_value() && rhs_symbol.has_value()) {
-      CHECK(lhs_symbol != rhs_symbol);
+      CHECK(lhs_symbol != rhs_symbol)
+          << lhs_symbol.value() << " != " << rhs_symbol.value();
       ret = symbol::Broadcastable<symbol::DimExpr>{lhs_symbol.value(),
                                                    rhs_symbol.value()};
       return true;
