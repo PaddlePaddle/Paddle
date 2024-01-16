f3e325c1e3b72e8d7d924ff2f4c1a6466a246f94
zyfncg <zhangyunfei07@baidu.com>
2024-01-16 03:55:43 +0000

    polish code

--- paddle/pir/dialect/shape/utils/shape_utils.cc
+++ paddle/pir/dialect/shape/utils/shape_utils.cc
@@ -161,18 +161,6 @@ ShapeConstraintIRAnalysis::GetShapeOrDataForValue(Value val) {
   // TODO(lanxianghit): check this when all the Ops support InferSymbolicShape
   // CHECK(value_to_shape_or_data_.find(val) != value_to_shape_or_data_.end());
   return value_to_shape_or_data_[val];
-  // skip static shape temporarily
-  // const auto& shape = val.type().dyn_cast<ShapedTypeInterface>();
-  // CHECK(shape && !shape.IsDynamicShape())
-  //     << "Value(" << GetValueId(val)
-  //     << ") is not found in value_to_shape_or_data and also not static.";
-  // const pir::DDim& val_dims = shape.GetShape();
-  // std::vector<symbol::DimExpr> static_shape;
-  // static_shape.reserve(val_dims.size());
-  // for (int i = 0; i < val_dims.size(); ++i) {
-  //   static_shape.emplace_back(val_dims.at(i));
-  // }
-  // return symbol::ShapeOrDataDimExprs(static_shape);
 }
 
 void ShapeConstraintIRAnalysis::SetShapeOrDataForValue(
