8664d54b23172de673dfeed3948625301857b9bc
feifei-111 <2364819892@qq.com>
2024-01-16 11:42:50 +0000

    update

--- python/paddle/jit/sot/symbolic/export.py
+++ python/paddle/jit/sot/symbolic/export.py
@@ -185,7 +185,8 @@ class PyFileGen:
             "def train(self, net, to_static, with_prim=False, with_cinn=False):"
         )
         train.add_sub(
-            "paddle.set_flags('FLAGS_prim_all': with_prim})" "if to_static:",
+            "paddle.set_flags({'FLAGS_prim_all': with_prim})",
+            "if to_static:",
             "    if with_cinn:",
             "        build_strategy = paddle.static.BuildStrategy()",
             "        build_strategy.build_cinn_pass = True",
