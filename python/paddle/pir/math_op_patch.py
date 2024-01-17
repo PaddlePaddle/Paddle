e8cb250354e3ed528480c621194f27a1cca4d036
SigureMo <sigure.qaq@gmail.com>
2024-01-17 10:56:05 +0000

    [Dy2St] Make `bool(Value)` always throw error

--- python/paddle/pir/math_op_patch.py
+++ python/paddle/pir/math_op_patch.py
@@ -434,6 +434,13 @@ def monkey_patch_value():
 
         return _C_ops.transpose(self, perm)
 
+    def _bool_(self):
+        raise TypeError(
+            "bool(Value) is not supported in static graph mode. If you are using @to_static, you can try this:\n"
+            "1. If you want to get the value of Value, you can switch to non-fullgraph mode by setting @to_static(full_graph=True).\n"
+            "2. If you want to run it in full graph mode, you need use Value.astype(paddle.bool), and do not use bool(Value)."
+        )
+
     def clone(self):
         """
         Returns a new static Value, which is the clone of the original static
@@ -653,6 +660,7 @@ def monkey_patch_value():
                 '__ge__', paddle.tensor.greater_equal, False, None
             ),
         ),
+        ('__bool__', _bool_),
     ]
 
     global _already_patch_value
