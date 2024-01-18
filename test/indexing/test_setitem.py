001b3f3222bfc0611f15fbf4fe16360f4c079e1b
JYChen <zoooo0820@qq.com>
2024-01-18 17:25:56 +0800

    Optimize advanced setting by remove the last set_value (#60771)
    * pure-advanced setitem will not set_value back
    
    * fix multi output in tensor_array_pir
    
    * only in dynamic mode
    
    * add only advanced-setting case to fix coverage

--- test/indexing/test_setitem.py
+++ test/indexing/test_setitem.py
@@ -28,6 +28,21 @@ class TestSetitemInDygraph(unittest.TestCase):
         self.ndtype = np.float64
         self.dtype = 'float64'
 
+    def test_advanced_index(self):
+        np_data = np.zeros((3, 4, 5, 6), dtype='float32').astype(self.ndtype)
+        if self.dtype == 'bfloat16':
+            np_data = convert_uint16_to_float(convert_float_to_uint16(np_data))
+        if self.dtype == 'complex64' or self.dtype == 'complex128':
+            np_data = np_data + 1j * np_data
+
+        x = paddle.to_tensor(np_data, dtype=self.dtype)
+        np_data[[0, 1], [1, 2], [1]] = 10.0
+        x[[0, 1], [1, 2], [1]] = 10.0
+
+        if self.dtype == 'bfloat16':
+            x = paddle.cast(x, dtype='float32')
+        np.testing.assert_allclose(x.numpy(), np_data)
+
     def test_combined_index_1(self):
         np_data = np.zeros((3, 4, 5, 6), dtype='float32').astype(self.ndtype)
         if self.dtype == 'bfloat16':
