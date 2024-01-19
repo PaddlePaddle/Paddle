8001a8e8a4658d0135eec2b5a7f033456b62e9a0
lanxianghit <lanxiang@outlook.com>
2024-01-19 02:04:21 +0000

    fix ci

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape.cc
@@ -13,13 +13,39 @@
 // limitations under the License.
 
 #include "paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape.h"
-#include "paddle/cinn/hlir/dialect/operator/transforms/group_merge/op_with_group_merge_util.h"
 #include "paddle/fluid/pir/dialect/operator/ir/op_attribute.h"
 #include "paddle/fluid/pir/dialect/operator/ir/pd_op.h"
 #include "paddle/pir/core/builtin_attribute.h"
 #include "paddle/pir/core/builtin_type.h"
 #include "paddle/pir/core/builtin_type_interfaces.h"
 #include "paddle/pir/dialect/shape/ir/shape_attribute.h"
+
+template <typename T = int64_t>
+std::vector<T> GetVectorAttr(const ::pir::Operation *op,
+                             const std::string &name) {
+  auto &attr_map = op->attributes();
+  PADDLE_ENFORCE(
+      attr_map.count(name),
+      phi::errors::PreconditionNotMet(
+          "attr [%s] MUST in attribute map for [%s] op", name, op->name()));
+  auto &val = attr_map.at(name);
+
+  PADDLE_ENFORCE(val.isa<::pir::ArrayAttribute>(),
+                 phi::errors::PreconditionNotMet(
+                     "axis Type MUST ArrayAttribute for [%s] op", op->name()));
+  auto array_list = val.dyn_cast<::pir::ArrayAttribute>().AsVector();
+  std::vector<T> vec_res;
+  if (array_list.size() > 0) {
+    PADDLE_ENFORCE_EQ(array_list[0].isa<::pir::Int64Attribute>(),
+                      true,
+                      phi::errors::Unimplemented(
+                          "the 0th elementwise MUST be ir::Int64Attribute"));
+    for (size_t i = 0; i < array_list.size(); ++i) {
+      vec_res.push_back(array_list[i].dyn_cast<::pir::Int64Attribute>().data());
+    }
+  }
+  return vec_res;
+}
 
 namespace paddle::dialect {
 
@@ -327,7 +353,7 @@ bool SumOpInferSymbolicShape(pir::Operation *op,
 
   auto axis_gen_op = op->operand_source(1).defining_op();
   if (axis_gen_op->isa<paddle::dialect::FullIntArrayOp>()) {
-    std::vector<int64_t> axis = cinn::dialect::ir::GetVectorAttr(
+    std::vector<int64_t> axis = GetVectorAttr(
         axis_gen_op->dyn_cast<paddle::dialect::FullIntArrayOp>(), "value");
     if (axis.size() == 0) {
       reduce_all = true;
