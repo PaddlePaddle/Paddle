352fef580fcf1ee6280a131d547954226876e8f8
zyfncg <zhangyunfei07@baidu.com>
2024-01-23 03:12:48 +0000

    update test

--- test/ir/pir/cinn/symbolic/test_cinn_sub_graph_symbolic.py
+++ test/ir/pir/cinn/symbolic/test_cinn_sub_graph_symbolic.py
@@ -126,38 +126,38 @@ class TestCinnDyShapeBase(TestCinnSubGraphBase):
         out = net(self.x)
         return out
 
-    # def test_eval_symbolic(self):
-    # cinn_out = self.eval_symbolic(use_cinn=True)
-    # dy_out = self.eval_symbolic(use_cinn=False)
-    # np.testing.assert_allclose(cinn_out.numpy(), dy_out.numpy(), atol=1e-8)
+    def test_eval_symbolic(self):
+        cinn_out = self.eval_symbolic(use_cinn=True)
+        dy_out = self.eval_symbolic(use_cinn=False)
+        np.testing.assert_allclose(cinn_out.numpy(), dy_out.numpy(), atol=1e-8)
 
 
-# class TestCinnDyShapeBC(TestCinnSubGraphBase):
-#     def prepare_data(self):
-#         self.x_shape = [2, 4, 1]
-#         self.x = paddle.randn(self.x_shape, dtype="float32")
-#         self.x.stop_gradient = False
+class TestCinnDyShapeBC(TestCinnSubGraphBase):
+    def prepare_data(self):
+        self.x_shape = [2, 4, 1]
+        self.x = paddle.randn(self.x_shape, dtype="float32")
+        self.x.stop_gradient = False
 
-#         self.y_shape = [4, 5]
-#         self.y = paddle.randn(self.y_shape, dtype="float32")
-#         self.y.stop_gradient = False
+        self.y_shape = [4, 5]
+        self.y = paddle.randn(self.y_shape, dtype="float32")
+        self.y.stop_gradient = False
 
-#     def eval_symbolic(self, use_cinn):
-#         paddle.seed(2022)
-#         net = CINNBroadcastSubGraphNet()
-#         input_spec = [
-#             InputSpec(shape=[None, None, None], dtype='float32'),
-#             InputSpec(shape=[None, None], dtype='float32'),
-#         ]
-#         net = apply_to_static(net, use_cinn, input_spec)
-#         net.eval()
-#         out = net(self.x, self.y)
-#         return out
+    def eval_symbolic(self, use_cinn):
+        paddle.seed(2022)
+        net = CINNBroadcastSubGraphNet()
+        input_spec = [
+            InputSpec(shape=[None, None, None], dtype='float32'),
+            InputSpec(shape=[None, None], dtype='float32'),
+        ]
+        net = apply_to_static(net, use_cinn, input_spec)
+        net.eval()
+        out = net(self.x, self.y)
+        return out
 
-# def test_eval_symbolic(self):
-#     # cinn_out = self.eval_symbolic(use_cinn=True)
-#     dy_out = self.eval_symbolic(use_cinn=False)
-#     # np.testing.assert_allclose(cinn_out.numpy(), dy_out.numpy(), atol=1e-8)
+    def test_eval_symbolic(self):
+        # cinn_out = self.eval_symbolic(use_cinn=True)
+        dy_out = self.eval_symbolic(use_cinn=False)
+        # np.testing.assert_allclose(cinn_out.numpy(), dy_out.numpy(), atol=1e-8)
 
 
 class LlamaRMSNorm(paddle.nn.Layer):
