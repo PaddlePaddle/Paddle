0924ba738252b142425720e2ba0dd8eb2a04bb10
bukejiyu <395822456@qq.com>
2024-01-23 03:46:03 +0000

    fix conv2d pass

--- paddle/fluid/pir/transforms/fusion/conv2d_add_fuse_pass.cc
+++ paddle/fluid/pir/transforms/fusion/conv2d_add_fuse_pass.cc
@@ -14,6 +14,7 @@
 
 #include "paddle/fluid/pir/transforms/fusion/conv2d_add_fuse_pass.h"
 
+#include <string>
 #include "paddle/fluid/pir/dialect/operator/ir/pd_op.h"
 #include "paddle/fluid/pir/drr/include/drr_pattern_base.h"
 
@@ -38,7 +39,19 @@ class Conv2dAddFusePattern : public paddle::drr::DrrPatternBase {
     conv2d({&pat.Tensor("input"), &pat.Tensor("filter")},
            {&pat.Tensor("conv2d_out")});
     pat.Tensor("add_out") = add(pat.Tensor("conv2d_out"), pat.Tensor("bias"));
-
+    pat.RequireNativeCall(
+        [](const paddle::drr::MatchContext &match_ctx) -> bool {
+          auto padding_algorithm =
+              match_ctx.Attr<std::string>("padding_algorithm");
+          if (padding_algorithm != "EXPLICIT" || padding_algorithm != "SAME" ||
+              padding_algorithm != "VALID")
+            return false;
+          auto groups = match_ctx.Attr<int>("groups");
+          if (groups < 1) return false;
+          auto data_format = match_ctx.Attr<std::string>("data_format");
+          if (data_format != "NCHW" || data_format != "AnyLayout") return false;
+          return true;
+        });
     paddle::drr::ResultPattern res = pat.ResultPattern();
 
     const auto &fused_conv2d_add_act =
