86671848042f07fa06538b5595fd42c3189daad8
kevincheng2 <chengyf112@gmail.com>
2024-01-24 06:50:30 +0000

    merge code

--- test/legacy_test/test_batch_norm_op_prim_nhwc.py
+++ test/legacy_test/test_batch_norm_op_prim_nhwc.py
@@ -151,8 +151,8 @@ class TestBatchNormOpNHWCbf16(TestBatchNormOp):
     def initConfig(self):
         self.fw_comp_atol = 1e-3
         self.fw_comp_rtol = 1e-3
-        self.rev_comp_atol = 1e-2
-        self.rev_comp_rtol = 1e-2
+        self.rev_comp_atol = 1e-3
+        self.rev_comp_rtol = 1e-3
         self.cinn_atol = 1e-3
         self.cinn_rtol = 1e-3
         self.dtype = "uint16"
