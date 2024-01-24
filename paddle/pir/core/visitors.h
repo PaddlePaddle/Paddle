757d55aa0c8a26a2d1d8cfbf3184fd8368d7183d
chen2016013 <111894720+chen2016013@users.noreply.github.com>
2024-01-24 20:23:40 +0800

    Update visitors.h

--- paddle/pir/core/visitors.h
+++ paddle/pir/core/visitors.h
@@ -39,7 +39,7 @@ IR_API void Walk(Operation *op,
                  WalkOrder order);
 
 template <WalkOrder Order = WalkOrder::PostOrder, typename FuncTy>
-IR_API void Walk(Operation *op, FuncTy &&callback) {
+void Walk(Operation *op, FuncTy &&callback) {
   return Walk(op, callback, Order);
 }
 
