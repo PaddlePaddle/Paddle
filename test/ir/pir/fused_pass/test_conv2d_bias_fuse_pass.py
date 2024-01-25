3d1453ac6db9f6e8cbc197eb16688964b7c518ab
Wang Huan <wanghuan29@baidu.com>
2024-01-25 03:59:46 +0000

    refine

--- test/ir/pir/fused_pass/test_conv2d_bias_fuse_pass.py
+++ test/ir/pir/fused_pass/test_conv2d_bias_fuse_pass.py
@@ -32,25 +32,32 @@ class TestConv2dAddFusePass(PassTest):
             start_prog = paddle.static.Program()
             with paddle.pir.core.program_guard(main_prog, start_prog):
                 x = paddle.static.data(
-                    name='x', shape=[3, 1, 28, 28], dtype='float32'
+                    name='x', shape=[8, 6, 7, 8], dtype='float32'
                 )
-                y = paddle.static.data(
-                    name="y", shape=[3, 32, 28, 28], dtype="float32"
+                y = paddle.static.data(name="y", shape=[5], dtype="float32")
+                w_attr = paddle.ParamAttr(
+                    learning_rate=0.0,
+                    initializer=paddle.nn.initializer.Constant(value=0.0),
                 )
                 conv2d = paddle.nn.Conv2D(
-                    in_channels=1,
-                    out_channels=32,
-                    kernel_size=3,
-                    padding="SAME",
+                    in_channels=6,
+                    out_channels=2,
+                    kernel_size=[1, 1],
+                    groups=2,
+                    stride=[3, 3],
+                    padding=[4, 1, 3, 4],
+                    dilation=[2, 1],
                     data_format='NCHW',
                     bias_attr=False,
+                    weight_attr=w_attr,
                 )
+
                 out = paddle.add(conv2d(x), y)
                 out = paddle.assign(out)
                 self.pass_list = ['conv2d_bias_fuse_pass']
                 self.feeds = {
-                    "x": np.random.random((3, 1, 28, 28)).astype("float32"),
-                    "y": np.random.random((3, 32, 28, 28)).astype("float32"),
+                    "x": np.random.random((8, 6, 7, 8)).astype("float32"),
+                    "y": np.random.random(5).astype("float32"),
                 }
                 self.fetch_list = [out]
                 self.valid_op_map = {
