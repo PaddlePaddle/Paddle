4ed9955c14b9068ffeeef2b4d4ef7d8a498170ff
jiahongyu <jiahongyu@baidu.com>
2024-01-25 03:20:28 +0000

    Update bash flags

--- test/ir/pir/cinn/rope.sh
+++ test/ir/pir/cinn/rope.sh
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # static-frontend && dynamic-backend
-# GLOG_v=4 FLAGS_cinn_convert_static_dim_to_dynamic=2048:S0 FLAGS_enable_pir_api=1 FLAGS_cinn_bucket_compile=True python test_rope.py 2>&1 | tee /tmp/a
+# GLOG_v=4 FLAGS_cinn_convert_static_dim_to_dynamic_dim=2048:S0 FLAGS_enable_pir_api=1 FLAGS_cinn_bucket_compile=True python test_rope.py 2>&1 | tee /tmp/a
 
 # static-frontend && static-backend
 GLOG_v=4 FLAGS_enable_pir_api=1 FLAGS_cinn_bucket_compile=True python test_rope.py 2>&1 | tee /tmp/a
