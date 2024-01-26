9b245062bbb73e3a7cc49585956688a755b6a3b2
risemeup1 <515586620@qq.com>
2024-01-26 17:25:07 +0000

    test

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -1166,9 +1166,9 @@ function generate_api_spec() {
         pip install -r ${PADDLE_ROOT}/python/requirements.txt
     fi
     if [ -d "${PADDLE_ROOT}/build/python/dist/" ]; then
-        pip --no-cache-dir install ${PADDLE_ROOT}/build/python/dist/*whl
+        pip --no-cache-dir --no-deps install ${PADDLE_ROOT}/build/python/dist/*whl
     elif [ -d "${PADDLE_ROOT}/dist/" ];then
-        pip --no-cache-dir install ${PADDLE_ROOT}/dist/*whl
+        pip --no-cache-dir --no-deps install ${PADDLE_ROOT}/dist/*whl
         mkdir ${PADDLE_ROOT}/build/python/dist/ && mv  ${PADDLE_ROOT}/dist/*whl  ${PADDLE_ROOT}/build/python/dist/
     fi
     spec_path=${PADDLE_ROOT}/paddle/fluid/API_${spec_kind}.spec
