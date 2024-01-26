5e61bab0a4c4763c96995cce33bdc0a24fb2d777
zyfncg <zhangyunfei07@baidu.com>
2024-01-26 02:49:45 +0000

    fix some infer symbolic shape

--- paddle/cinn/hlir/dialect/operator/ir/manual_op.cc
+++ paddle/cinn/hlir/dialect/operator/ir/manual_op.cc
@@ -434,8 +434,8 @@ bool GenerateShapeOp::InferSymbolicShape(
   }();
 
   // TODO(HongyuJia): use op->result(0) to infer the shape
-  std::vector<symbol::DimExpr> shape(
-      std::int64_t(substituted_dim_exprs.size()));
+  std::vector<symbol::DimExpr> shape{
+      std::int64_t(substituted_dim_exprs.size())};
   symbol::ShapeOrDataDimExprs shape_or_data_dim_exprs{
       symbol::TensorShapeOrDataDimExprs(shape, substituted_dim_exprs)};
 
