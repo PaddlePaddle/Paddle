3ba234af98cc7f4889f97f390b5197a74bc01890
Wang Huan <wanghuan29@baidu.com>
2024-01-30 06:39:29 +0000

    refine

--- python/paddle/dataset/common.py
+++ python/paddle/dataset/common.py
@@ -74,7 +74,9 @@ def md5file(fname):
 def download(url, module_name, md5sum, save_name=None):
     module_name = re.match("^[a-zA-Z0-9_/\\-]+$", module_name).group()
     if isinstance(save_name, str):
-        save_name = re.match("^[a-zA-Z0-9_/\\/.-]+$", save_name).group()
+        save_name = re.match(
+            "^(?:(?!\\.\\.)[a-zA-Z0-9_/\\.-])+$", save_name
+        ).group()
     dirname = os.path.join(DATA_HOME, module_name)
     if not os.path.exists(dirname):
         os.makedirs(dirname)
