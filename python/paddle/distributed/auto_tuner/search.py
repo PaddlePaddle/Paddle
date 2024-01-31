f15a0cf072afd902647884a3ab1dcf5507322207
caozhou <48191911+Caozhou1995@users.noreply.github.com>
2024-01-31 21:05:26 +0800

    [AutoConfig] add blacklist mechanism and scheduling in specified order (#60248)
    * add blacklist mechanism and scheduling in specified order
    
    * change best cfg
    
    * fix first few bug
    
    * fix resume from history
    
    ---------
    
    Co-authored-by: Difers <707065510@qq.com>

--- python/paddle/distributed/auto_tuner/search.py
+++ python/paddle/distributed/auto_tuner/search.py
@@ -49,10 +49,33 @@ class GridSearch(SearchAlgo):
         super().__init__(tuner_cfg)
         self.idx = 0
         self.all_tasks = search_all(tuner_cfg)
+        need_baseline = self.tuner_cfg.get("need_baseline", False)
+        self.baseline = None
+        if need_baseline:
+            from .utils import memory_sort
+
+            self.all_tasks.sort(key=memory_sort)
+        self.previous_cfg = None
 
     def search_once(self, history_cfgs):
         new_cfg = None
         stop = False
+        if history_cfgs:
+            if history_cfgs[-1].get("time", -1) > 0:
+                if self.baseline is None:
+                    from .utils import performance_sort
+
+                    self.baseline = history_cfgs[-1]
+                    self.all_tasks[self.idx :] = sorted(
+                        self.all_tasks[self.idx : len(self.all_tasks)],
+                        key=performance_sort,
+                    )
+                    if self.tuner_cfg.get("schedule_prior", False):
+                        from .utils import sort_by_sepecial
+
+                        self.all_tasks[self.idx :] = sort_by_sepecial(
+                            self.all_tasks[self.idx :], self.tuner_cfg
+                        )
         while not stop:
             if self.idx < len(self.all_tasks):
                 new_cfg = self.all_tasks[self.idx]
@@ -60,6 +83,7 @@ class GridSearch(SearchAlgo):
                 stop = not self.prune(self.tuner_cfg, new_cfg, history_cfgs)
             else:
                 return None
+
         return new_cfg
 
 
