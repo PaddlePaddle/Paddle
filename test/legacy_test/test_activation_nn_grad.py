919c08af7d3b21b6b448fce2edaf0f372244c9b4
HydrogenSulfate <490868991@qq.com>
2024-01-31 11:28:54 +0000

    update UT for cosdoublegrad

--- test/legacy_test/test_activation_nn_grad.py
+++ test/legacy_test/test_activation_nn_grad.py
@@ -17,6 +17,7 @@ import unittest
 import gradient_checker
 import numpy as np
 from decorator_helper import prog_scope
+from utils import dygraph_guard, static_guard
 
 import paddle
 import paddle.nn.functional as F
@@ -542,6 +543,42 @@ class TestCosDoubleGradCheck(unittest.TestCase):
             places.append(base.CUDAPlace(0))
         for p in places:
             self.func(p)
+
+
+class TestCosDoubleGradCheck2(unittest.TestCase):
+    def test_cos_double_dynamic(self):
+        with dygraph_guard():
+            x = paddle.randn([64, 64])
+            x = paddle.to_tensor(x, place=base.CPUPlace(), stop_gradient=False)
+            y = paddle.cos(x)
+            dx = paddle.grad(y, x, create_graph=True)
+            dxx_result = paddle.grad(dx, x)[0]
+            dxx_expected = -paddle.cos(x)
+            np.testing.assert_allclose(dxx_result.numpy(), dxx_expected.numpy())
+
+    def test_cos_double_static(self):
+        x_data = np.random.randn(64, 64).astype("float32")
+        with static_guard():
+            main_prog = paddle.static.Program()
+            starup_prog = paddle.static.Program()
+            with paddle.static.program_guard(main_prog, starup_prog):
+                x = paddle.assign(x_data)
+                x.stop_gradient = False
+                y = paddle.cos(x)
+                dx = paddle.static.gradients(y, x)
+                dxx = paddle.static.gradients(dx, x)[0]
+
+            exe = paddle.static.Executor(base.CUDAPlace(0))
+            exe.run(starup_prog)
+            (dxx_result,) = exe.run(main_prog, fetch_list=[dxx])
+            dxx_expected = -np.cos(x_data)
+            np.testing.assert_allclose(dxx_result, dxx_expected, 1e-6, 1e-6)
+
+            exe = paddle.static.Executor(base.CPUPlace())
+            exe.run(starup_prog)
+            (dxx_result,) = exe.run(main_prog, fetch_list=[dxx])
+            dxx_expected = -np.cos(x_data)
+            np.testing.assert_allclose(dxx_result, dxx_expected, 1e-6, 1e-6)
 
 
 class TestPowDoubleGradCheck1(unittest.TestCase):
