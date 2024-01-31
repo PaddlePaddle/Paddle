7c0cf065c695c9acaee62b9d39c02cb1f6415d4f
Reese Wang <rewang@nvidia.com>
2024-01-31 02:10:41 -0800

    Fix several TRT UTs

--- test/ir/inference/test_trt_convert_solve.py
+++ test/ir/inference/test_trt_convert_solve.py
@@ -90,7 +90,9 @@ class TrtConvertSolve(TrtLayerAutoScanTest):
         yield self.create_inference_config(), (1, 3), 1e-5
 
         self.trt_param.precision = paddle_infer.PrecisionType.Half
-        yield self.create_inference_config(), (1, 3), 1e-3
+        program_config.set_input_type(np.float16)
+        yield self.create_inference_config(), (1, 3), (1e-3, 1e-3)
+
 
     def test(self):
         self.run_test()
