596b2a6f379be337582a8ea8813e6a96d65d5ef2
zyfncg <zhangyunfei07@baidu.com>
2024-01-31 06:42:55 +0000

    fix test complige

--- test/cpp/pir/cinn/symbolic_lower_test.cc
+++ test/cpp/pir/cinn/symbolic_lower_test.cc
@@ -108,7 +108,7 @@ BuildGroupProgramForLowering() {
   value_to_shape_data.emplace(exp.result(0), value_to_shape_data.at(x));
   value_to_shape_data.emplace(reshape.result(0), value_to_shape_data.at(y));
   value_to_shape_data.emplace(sub.result(0), value_to_shape_data.at(y));
-  groups[0]->value_to_shape_or_data_exprs = value_to_shape_data;
+  groups[0]->set_value_to_shape_or_data_exprs(value_to_shape_data);
 
   return {program, groups};
 }
@@ -206,7 +206,7 @@ BuildBroadcastGroupProgramForLowering() {
       sub.result(0),
       symbol::ShapeOrDataDimExprs(
           symbol::TensorShapeOrDataDimExprs({y_dim_0, y_dim_1, y_dim_2})));
-  groups[0]->value_to_shape_or_data_exprs = value_to_shape_data;
+  groups[0]->set_value_to_shape_or_data_exprs(value_to_shape_data);
 
   return {program, groups};
 }
