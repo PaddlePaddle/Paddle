50203b41b5a057c74394ecfcc8f8d061cf6ec0c4
andsonder <changlu@keter.top>
2024-01-31 04:35:40 +0000

    update

--- python/paddle/distributed/auto_parallel/static/profiler_helper_static.py
+++ python/paddle/distributed/auto_parallel/static/profiler_helper_static.py
@@ -54,25 +54,24 @@ def process_job_log(log_data, device_id, multi_machine_idx=-1):
     step_times = []
     step_start_time = 0
     step_end_time = 0
-    recorded_start_time_ids = []
+
+    start_job_type = ""
 
     for i, match in enumerate(matches):
         job_id, job_type, micro_batch_id, job_start_time, job_end_time = match
+
+        if job_type != "default" and start_job_type == "":
+            start_job_type = job_type
 
         start_time = float(job_start_time.strip()) * 1000
         end_time = float(job_end_time.strip()) * 1000
 
         is_start_time_recorded = 0
 
-        if (
-            job_type.startswith("forward")
-            and micro_batch_id == "0"
-            and job_id not in recorded_start_time_ids
-        ):
+        if job_type == start_job_type and micro_batch_id == "0":
             if step_start_time != 0:
                 step_times.append([step_start_time, step_end_time])
             step_start_time = start_time
-            recorded_start_time_ids.append(job_id)
 
         step_end_time = end_time
 
