e300fcc3037cc5e4ca317321d23e344146942ba7
enkilee <luhputu0815@gmail.com>
2024-02-01 14:36:49 +0800

    fix

--- test/legacy_test/test_transpose_op.py
+++ test/legacy_test/test_transpose_op.py
@@ -515,7 +515,7 @@ class TestTransposeOpError(unittest.TestCase):
 
             self.assertRaises(TypeError, test_x_Variable_check)
 
-            if core.is_complied_with_cuda():
+            if core.is_compiled_with_cuda():
                 # the Input(x)'s dtype must be one of [bool, float16, bfloat16, float32, float64, int8, int16, int32, int64, uint8, uint16, complex64, complex128]
                 x_fp16 = paddle.static.data(
                     name='x1', shape=[-1, 10, 5, 3], dtype='float16'
