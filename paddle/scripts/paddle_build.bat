3dc885818d7759b2886c1be8b219c267fad93c3b
xuxinyi04 <xuxinyi04@baidu.com>
2024-02-01 03:48:05 +0000

    remove_tp_compile_cache

--- paddle/scripts/paddle_build.bat
+++ paddle/scripts/paddle_build.bat
@@ -489,32 +489,6 @@ echo %task_name%|findstr build >nul && (
     echo %task_name% is a PR-CI-Windows task, will try to reuse bos and local third_party cache both.
 )
 
-if not exist %THIRD_PARTY_PATH% (
-    echo There is no usable third_party cache in %THIRD_PARTY_PATH%, will download from bos.
-    pip install wget
-    if not exist %THIRD_PARTY_HOME% mkdir "%THIRD_PARTY_HOME%"
-    cd /d %THIRD_PARTY_HOME%
-    echo Getting third party: downloading ...
-    python -c "import wget;wget.download('https://paddle-windows.bj.bcebos.com/third_party/%sub_dir%/%md5%.tar.gz')" 2>nul
-    if !ERRORLEVEL! EQU 0 (
-        echo Getting third party: extracting ...
-        tar -xf %md5%.tar.gz
-        if !ERRORLEVEL! EQU 0 (
-            echo Get third party from bos successfully.
-        ) else (
-            echo Get third party failed, reason: extract failed, will build locally.
-        )
-        del %md5%.tar.gz
-    ) else (
-        echo Get third party failed, reason: download failed, will build locally.
-    )
-    if not exist %THIRD_PARTY_PATH% set UPLOAD_TP_FILE=ON
-    cd /d %work_dir%\%BUILD_DIR%
-) else (
-    echo Found reusable third_party cache in %THIRD_PARTY_PATH%, will reuse it.
-)
-
-
 :cmake_impl
 cd /d %work_dir%\%BUILD_DIR%
 echo cmake .. -G %GENERATOR% -DCMAKE_BUILD_TYPE=Release -DWITH_AVX=%WITH_AVX% -DWITH_GPU=%WITH_GPU% -DWITH_MKL=%WITH_MKL% ^
@@ -637,40 +611,6 @@ if %ERRORLEVEL% NEQ 0 (
         echo Build Paddle failed, will retry!
         goto :build_paddle
     )
-)
-
-if "%UPLOAD_TP_FILE%"=="ON" (
-    set BCE_FILE=%cache_dir%\bce-python-sdk-new\BosClient.py
-    echo Uploading third_party: checking bce ...
-    if not exist %cache_dir%\bce-python-sdk-new (
-        echo There is no bce in this PC, will install bce.
-        cd /d %cache_dir%
-        echo Download package from https://xly-devops.bj.bcebos.com/home/bos_new.tar.gz
-        python -c "import wget;wget.download('https://xly-devops.bj.bcebos.com/home/bos_new.tar.gz')"
-        python -c "import shutil;shutil.unpack_archive('bos_new.tar.gz', extract_dir='./bce-python-sdk-new',format='gztar')"
-    )
-    python -m pip install pycryptodome
-    python -m pip install bce-python-sdk==0.8.74
-    if !errorlevel! EQU 0 (
-        cd /d %THIRD_PARTY_HOME%
-        echo Uploading third_party: compressing ...
-        tar -zcf %md5%.tar.gz %md5%
-        if !errorlevel! EQU 0 (
-            echo Uploading third_party: uploading ...
-            python !BCE_FILE! %md5%.tar.gz paddle-windows/third_party/%sub_dir% 1>nul
-            if !errorlevel! EQU 0 (
-                echo Upload third party %md5% to bos paddle-windows/third_party/%sub_dir% successfully.
-            ) else (
-                echo Failed upload third party to bos, reason: upload failed.
-            )
-        ) else (
-            echo Failed upload third party to bos, reason: compress failed.
-        )
-        del %md5%.tar.gz
-    ) else (
-        echo Failed upload third party to bos, reason: install bce failed.
-    )
-    cd /d %work_dir%\%BUILD_DIR%
 )
 
 echo Build Paddle successfully!
