bb9b7b829fae730cfe8ddf53eebef13084511cee
SigureMo <sigure.qaq@gmail.com>
2024-02-03 22:04:53 +0800

    [Dy2St] Use hand write unparse instead of astor in Python3.8

--- test/dygraph_to_static/check_approval.py
+++ test/dygraph_to_static/check_approval.py
@@ -52,19 +52,29 @@ class Location:
     col_offset: int
 
 
-def ast_to_source_code(node):
+def ast_to_source_code(node: ast.AST):
     if sys.version_info >= (3, 9):
         ast.fix_missing_locations(node)
         return ast.unparse(node)
 
-    # Do not wrap lines even if they are too long
-    def pretty_source(source):
-        return ''.join(source)
-
-    import astor
-
-    source_code = astor.to_source(node, pretty_source=pretty_source)
-    return source_code
+    if isinstance(node, ast.Name):
+        return node.id
+    elif isinstance(node, ast.Attribute):
+        return f'{ast_to_source_code(node.value)}.{node.attr}'
+    elif isinstance(node, ast.Tuple):
+        return f'({", ".join(ast_to_source_code(elt) for elt in node.elts)})'
+    elif isinstance(node, ast.List):
+        return f'[{", ".join(ast_to_source_code(elt) for elt in node.elts)}]'
+    elif isinstance(node, ast.Constant):
+        return repr(node.value)
+    elif isinstance(node, ast.Call):
+        unparsed_args = [ast_to_source_code(arg) for arg in node.args]
+        unparsed_keywords = [
+            f'{kw.arg}={ast_to_source_code(kw.value)}' for kw in node.keywords
+        ]
+        return f'{ast_to_source_code(node.func)}({", ".join(unparsed_args + unparsed_keywords)})'
+    else:
+        raise NotImplementedError(f'Unsupported node type: {type(node)}')
 
 
 class Diagnostic:
