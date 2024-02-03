8446c5496b0cbaf58fb61e9b50b59a2da265dcdb
SigureMo <sigure.qaq@gmail.com>
2024-02-03 05:40:27 +0000

    remove print

--- python/paddle/jit/dy2static/transformers/tensorhook_transformer.py
+++ python/paddle/jit/dy2static/transformers/tensorhook_transformer.py
@@ -46,7 +46,6 @@ class RegisterHookTransformer(BaseTransformer):
         ]
         # Analyze the register_hook nodes name dependency
         dependents = {}
-        print(dependents)
         for n in regisiter_hook_nodes:
             if n not in stmts:
                 continue
