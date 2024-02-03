62ef0f9ef8c7f3d3917beec303713c1bd5a12190
张春乔 <83450930+Liyulingyue@users.noreply.github.com>
2024-02-03 21:04:22 +0800

    Update drr_pattern_context.h

--- paddle/fluid/pir/drr/include/drr_pattern_context.h
+++ paddle/fluid/pir/drr/include/drr_pattern_context.h
@@ -135,7 +135,7 @@ class DrrPatternContext {
   // void RequireEqual(const Attribute& first, const Attribute& second);
   void RequireEqual(const TensorShape& first, const TensorShape& second);
   TEST_API void RequireEqual(const TensorDataType& first, 
-                                                const TensorDataType& second);
+                             const TensorDataType& second);
   void RequireNativeCall(const ConstraintFunction& custom_fn);
 
   std::shared_ptr<SourcePatternGraph> source_pattern_graph_;
