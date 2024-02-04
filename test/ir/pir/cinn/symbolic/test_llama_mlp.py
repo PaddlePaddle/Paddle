dbd83e7311fd37ed52779e9342c044d7fe7c7d30
jiahongyu <jiahongyu@baidu.com>
2024-02-04 11:30:53 +0000

    Use FLAGS_pd_unittest_use_cinn

--- test/ir/pir/cinn/symbolic/test_llama_mlp.py
+++ test/ir/pir/cinn/symbolic/test_llama_mlp.py
@@ -74,11 +74,12 @@ class TestLlamaMLP(unittest.TestCase):
         return out
 
     def test_eval(self):
-        cinn_out = self.eval(use_cinn=True)
         dy_out = self.eval(use_cinn=False)
-        np.testing.assert_allclose(
-            cinn_out.numpy(), dy_out.numpy(), atol=1e-6, rtol=1e-6
-        )
+        if utils.unittest_use_cinn():
+            cinn_out = self.eval(use_cinn=True)
+            np.testing.assert_allclose(
+                cinn_out.numpy(), dy_out.numpy(), atol=1e-6, rtol=1e-6
+            )
 
 
 if __name__ == '__main__':
