3c121d300522d9aba67db32980bed4d56d115130
co63oc <co63oc@users.noreply.github.com>
2024-02-04 16:40:46 +0800

    [Cleanup][B-19]Replace to_variable (#61545)

--- python/paddle/base/dygraph/tensor_patch_methods.py
+++ python/paddle/base/dygraph/tensor_patch_methods.py
@@ -94,12 +94,12 @@ def monkey_patch_tensor():
             .. code-block:: python
 
                 >>> import paddle.base as base
-                >>> from paddle.base.dygraph.base import to_variable
+                >>> import paddle
                 >>> import numpy as np
 
                 >>> data = np.ones([3, 1024], dtype='float32')
                 >>> with base.dygraph.guard():
-                ...     tensor = to_variable(data)
+                ...     tensor = paddle.to_tensor(data)
                 ...     static_var = tensor._to_static_var()
         """
 
@@ -175,14 +175,14 @@ def monkey_patch_tensor():
             .. code-block:: python
 
                 >>> import paddle.base as base
-                >>> from paddle.base.dygraph.base import to_variable
+                >>> import paddle
                 >>> from paddle.nn import Linear
                 >>> import numpy as np
 
                 >>> data = np.ones([3, 1024], dtype='float32')
                 >>> with base.dygraph.guard():
                 ...     linear = Linear(1024, 4)
-                ...     t = to_variable(data)
+                ...     t = paddle.to_tensor(data)
                 ...     linear(t)  # call with default weight
                 ...     custom_weight = np.random.randn(1024, 4).astype("float32")
                 ...     linear.weight.set_value(custom_weight)  # change existing weight
