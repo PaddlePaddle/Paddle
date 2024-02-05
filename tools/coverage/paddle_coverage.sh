0c35aecbeaca54c7e7d51e9c5fb0ffbb687a5d45
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2024-02-05 15:02:46 +0800

    Update paddle_coverage.sh,test=document_fix

--- tools/coverage/paddle_coverage.sh
+++ tools/coverage/paddle_coverage.sh
@@ -252,7 +252,7 @@ fi
 
 if [ "$COVERAGE_LINES_ASSERT" = "1" ] || [ "$PYTHON_COVERAGE_LINES_ASSERT" = "1" ]; then
     echo "exit 9" > /tmp/paddle_coverage.result
-    if [${WITH_CINN} == "ON"]; then
+    if [ ${WITH_CINN} == "ON" ]; then
         echo "You must one RD(liuhongyu or lanxiang or zhenghuihuang or tianchao or zhangliujie)to approval this PR."
     fi
     exit 9
