7c674894c331dffa6c2fb29111da691d5b50b193
Wang Huan <wanghuan29@baidu.com>
2024-02-05 10:37:30 +0000

    refine

--- paddle/fluid/eager/api/manual/eager_manual/dygraph_forward_api.h
+++ paddle/fluid/eager/api/manual/eager_manual/dygraph_forward_api.h
@@ -76,9 +76,9 @@ sync_batch_norm__ad_func(const paddle::Tensor& x,
 
 paddle::Tensor multiply_ad_func(const paddle::Tensor& x,
                                 const paddle::Tensor& y);
+}  // namespace sparse
 
 paddle::Tensor cast_ad_func(const paddle::Tensor& x, phi::DataType dtype);
 
 paddle::Tensor& cast__ad_func(paddle::Tensor& x,  // NOLINT
                               phi::DataType dtype);
-}  // namespace sparse
