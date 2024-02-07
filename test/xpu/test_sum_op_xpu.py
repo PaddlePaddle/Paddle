6c2c2d2c0d2c4c06e91a15485ddac90e06a2e66b
co63oc <co63@163.com>
2024-02-07 20:13:19 +0800

    Fix

--- test/xpu/test_sum_op_xpu.py
+++ test/xpu/test_sum_op_xpu.py
@@ -167,10 +167,10 @@ class TestRaiseSumsError(unittest.TestCase):
 
         def test_out_type():
             data1 = paddle.static.data(
-                name="input1", shape=[10], dtype="float32"
+                name="input1_type", shape=[10], dtype="float32"
             )
             data2 = paddle.static.data(
-                name="input2", shape=[10], dtype="float32"
+                name="input2_type", shape=[10], dtype="float32"
             )
             out = [10]
             out = paddle.add_n([data1, data2])
@@ -179,10 +179,10 @@ class TestRaiseSumsError(unittest.TestCase):
 
         def test_out_dtype():
             data1 = paddle.static.data(
-                name="input1", shape=[10], dtype="float32"
+                name="input1_dtype", shape=[10], dtype="float32"
             )
             data2 = paddle.static.data(
-                name="input2", shape=[10], dtype="float32"
+                name="input2_dtype", shape=[10], dtype="float32"
             )
             out = paddle.static.data(name="out", shape=[10], dtype="int8")
             out = paddle.add_n([data1, data2])
