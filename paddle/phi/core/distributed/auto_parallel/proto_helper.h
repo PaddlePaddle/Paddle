6c062c7129360beec41fde9f264b03ead833ab35
liyulingyue <852433440@qq.com>
2024-02-09 21:20:39 +0800

    add TEST_API for to_proto

--- paddle/phi/core/distributed/auto_parallel/proto_helper.h
+++ paddle/phi/core/distributed/auto_parallel/proto_helper.h
@@ -27,7 +27,8 @@ class Link;
 class DeviceMesh;
 class DistributedMapper;
 }  // namespace auto_parallel
-auto_parallel::TensorDistAttrProto to_proto(const TensorDistAttr& dist_attr);
+TEST_API auto_parallel::TensorDistAttrProto to_proto(
+    const TensorDistAttr& dist_attr);
 TEST_API auto_parallel::ProcessMeshProto to_proto(const ProcessMesh& dist_attr);
 
 auto_parallel::DeviceCapabilityProto to_proto(
