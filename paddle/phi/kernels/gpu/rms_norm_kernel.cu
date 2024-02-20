22c0f39842ff2d753d9cad349d6197938e24dbe0
liuzhenhai93 <liuzhenhai93@outlook.com>
2024-02-20 10:52:02 +0800

    copy right

--- paddle/phi/kernels/gpu/rms_norm_kernel.cu
+++ paddle/phi/kernels/gpu/rms_norm_kernel.cu
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,27 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-// Original OneFlow copyright notice:
-
-/*
-Copyright 2020 The OneFlow Authors. All rights reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// https://github.com/Oneflow-Inc/oneflow/blob/master/oneflow/core/cuda/layer_norm.cuh
-// The following code modified from OneFlow's implementation, and change to use
-// single Pass algorithm. Support Int8 quant, dequant Load/Store implementation.
 
 #include "paddle/phi/kernels/rms_norm_kernel.h"
 #include <assert.h>
