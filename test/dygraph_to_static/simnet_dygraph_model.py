874a120f206e4d67c1e4a07efcad477b1c423c2c
PommesPeter <me@pommespeter.space>
2024-02-21 23:25:27 +0800

    :pencil2: Fixed error

--- test/dygraph_to_static/simnet_dygraph_model.py
+++ test/dygraph_to_static/simnet_dygraph_model.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+x  #   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -298,6 +298,7 @@ class FC(paddle.nn.Layer):
 
     Examples:
         .. code-block:: python
+          import paddle
           import paddle.base as base
           from paddle.base.dygraph import FC
           import numpy as np
