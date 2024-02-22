a07fd2edd5c2c9772d7aa2f4dd05065512b98302
enkilee <jeffrey0122@163.com>
2024-02-22 16:12:56 +0800

    fix

--- test/cpp/pir/pattern_rewrite/pattern_rewrite_test.cc
+++ test/cpp/pir/pattern_rewrite/pattern_rewrite_test.cc
@@ -87,12 +87,12 @@ void Operation1::VerifySig() {
   auto &attributes = this->attributes();
   if (attributes.count("op2_attr1") == 0 ||
       (!attributes.at("op2_attr1").isa<pir::StrAttribute>())) {
-    PADDLE_THROW(platform::errors::Fatal(
+    PADDLE_THROW(paddle::platform::errors::Fatal(
         "Type of attribute: parameter_name is not right."));
   }
   if (attributes.count("op2_attr2") == 0 ||
       (!attributes.at("op2_attr2").isa<pir::StrAttribute>())) {
-    PADDLE_THROW(platform::errors::Fatal(
+    PADDLE_THROW(paddle::platform::errors::Fatal(
         "Type of attribute: parameter_name is not right."));
   }
 }
