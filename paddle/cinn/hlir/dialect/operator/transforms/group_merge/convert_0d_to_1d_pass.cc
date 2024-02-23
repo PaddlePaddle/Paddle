456611e533554ac28828b6abee574c26362095d4
jiahongyu <jiahongyu@baidu.com>
2024-02-23 13:11:05 +0000

    [Dynamic Shape] Convert0DTo1DPass supports more case

--- paddle/cinn/hlir/dialect/operator/transforms/group_merge/convert_0d_to_1d_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/group_merge/convert_0d_to_1d_pass.cc
@@ -22,6 +22,7 @@
 #include "paddle/fluid/pir/dialect/operator/ir/op_attribute.h"
 #include "paddle/fluid/pir/dialect/operator/ir/pd_op.h"
 #include "paddle/pir/include/core/builtin_type.h"
+#include "paddle/pir/include/pattern_rewrite/pattern_rewrite_driver.h"
 
 namespace cinn {
 namespace dialect {
@@ -41,7 +42,7 @@ class FullOpPattern : public pir::OpRewritePattern<paddle::dialect::FullOp> {
   }
 
   void Rewrite(paddle::dialect::FullOp op,
-               pir::PatternRewriter &rewriter) const override {
+               pir::PatternRewriter& rewriter) const override {
     float factor =
         op->attribute("value").dyn_cast<::pir::FloatAttribute>().data();
     phi::DataType dtype = op->attribute("dtype")
@@ -58,20 +59,128 @@ class FullOpPattern : public pir::OpRewritePattern<paddle::dialect::FullOp> {
   }
 };
 
-class Convert0DTo1DPass : public pir::PatternRewritePass {
+class CombineOpPattern : public pir::OpRewritePattern<pir::CombineOp> {
  public:
-  Convert0DTo1DPass() : pir::PatternRewritePass("convert_0D_to_1D", 1) {}
+  using pir::OpRewritePattern<pir::CombineOp>::OpRewritePattern;
 
-  pir::RewritePatternSet InitializePatterns(pir::IrContext *context) override {
+  bool Match(pir::CombineOp op) const override {
+    for (std::size_t i = 1; i < op->operands().size(); ++i) {
+      if (op.operand_source(i).type() != op.operand_source(0).type()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void Rewrite(pir::CombineOp op,
+               pir::PatternRewriter& rewriter) const override {
+    pir::Builder builder(rewriter.ir_context());
+
+    const std::vector<pir::Type> inputs_type = [&]() {
+      std::vector<pir::Type> types;
+      for (auto value : op->operands_source()) {
+        types.push_back(value.type());
+      }
+      return types;
+    }();
+    op.result(0).set_type(builder.vec_type(inputs_type));
+  }
+};
+
+class Convert0DTo1DPass : public pir::Pass {
+ public:
+  Convert0DTo1DPass() : pir::Pass("convert_0D_to_1D", 1) {}
+
+  bool Initialize(pir::IrContext* context) override {
     pir::RewritePatternSet ps(context);
     ps.Add<FullOpPattern>(context);
-
-    return ps;
+    ps.Add<CombineOpPattern>(context);
+    patterns_ = pir::FrozenRewritePatternSet(std::move(ps));
+    return true;
   }
 
-  bool CanApplyOn(pir::Operation *op) const override {
+  void Run(pir::Operation* op) override {
+    pir::GreedyRewriteConfig cfg;
+    cfg.use_top_down_traversal = true;
+    cfg.max_iterations = 10;
+    for (uint32_t i = 0; i < op->num_regions(); ++i) {
+      for (auto& block : op->region(i)) {
+        ConvertBlock0DTo1D(block);
+        for (auto& op : block) {
+          if (op.isa<paddle::dialect::FullOp>() || op.isa<pir::CombineOp>()) {
+            const auto& [_, num_rewrites] =
+                pir::ApplyPatternsGreedily(&op, patterns_, cfg);
+            AddStatistics(num_rewrites);
+          }
+        }
+      }
+    }
+  }
+
+  bool CanApplyOn(pir::Operation* op) const override {
     return op->isa<pir::ModuleOp>() && op->num_regions() > 0;
   }
+
+  pir::DenseTensorType Make1DTensorType(
+      const pir::DenseTensorType& tensor_type) {
+    return pir::DenseTensorType::get(pir::IrContext::Instance(),
+                                     tensor_type.dtype(),
+                                     {1},
+                                     tensor_type.data_layout(),
+                                     tensor_type.lod(),
+                                     tensor_type.offset());
+  }
+
+  void ConvertValue0DTo1D(pir::Value operand) {
+    if (const auto& tensor_type =
+            operand.type().dyn_cast<pir::DenseTensorType>()) {
+      if (tensor_type.dims().size() == 1) {
+        operand.set_type(Make1DTensorType(tensor_type));
+      }
+    } else if (const auto& vector_tensor_type =
+                   operand.type().dyn_cast<pir::VectorType>()) {
+      pir::Builder builder(pir::IrContext::Instance());
+
+      const std::vector<pir::Type> inputs_type = [&]() {
+        std::vector<pir::Type> types;
+        for (std::size_t i = 0; i < vector_tensor_type.size(); ++i) {
+          CHECK(vector_tensor_type[i].isa<pir::DenseTensorType>());
+          const auto& dense_type =
+              vector_tensor_type[i].dyn_cast<pir::DenseTensorType>();
+          types.push_back(dense_type.dims().size() == 1
+                              ? Make1DTensorType(dense_type)
+                              : vector_tensor_type[i]);
+        }
+        return types;
+      }();
+      operand.set_type(builder.vec_type(inputs_type));
+    } else {
+      LOG(FATAL) << "Unsupported operand type: " << operand.type();
+    }
+  }
+
+  void ConvertOperation0DTo1D(const pir::Operation& op) {
+    for (std::size_t i = 0; i < op.num_operands(); ++i) {
+      ConvertValue0DTo1D(op.operand_source(i));
+    }
+    for (std::size_t i = 0; i < op.num_results(); ++i) {
+      ConvertValue0DTo1D(op.result(i));
+    }
+  }
+
+  void ConvertBlock0DTo1D(const pir::Block& block) {
+    for (auto& op : block) {
+      ConvertOperation0DTo1D(op);
+      for (std::size_t i = 0; i < op.num_regions(); ++i) {
+        for (auto& inner_block : op.region(i)) {
+          ConvertBlock0DTo1D(inner_block);
+        }
+      }
+    }
+  }
+
+ private:
+  pir::FrozenRewritePatternSet patterns_;
 };
 
 }  // namespace
