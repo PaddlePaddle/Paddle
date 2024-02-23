934afd220ead2818c60a77dd4c2742e821548e82
Bo Zhang <105368690+zhangbopd@users.noreply.github.com>
2024-02-23 16:24:40 +0800

    [PIR][DynamicShape] Add InferSymbolicShape interface for WhileOp (#61939)
    * add while infer
    
    * yield
    
    * bug fix

--- test/ir/pir/cinn/symbolic/test_if_else_dy.py
+++ test/ir/pir/cinn/symbolic/test_if_else_dy.py
@@ -27,53 +27,64 @@ sys.path.append(dirname(dirname(__file__)))
 import utils
 
 
-class WhileExpSub(nn.Layer):
+class IfSubgraph(nn.Layer):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x):
-        loop_count = paddle.full([1], 0)
-        while x.sum() > paddle.full([1], 0) and loop_count < paddle.full(
-            [1], 1
-        ):
-            x = paddle.exp(x) - x
-            loop_count += 1
-        x = paddle.exp(x)
-        return x
+    def exp_sub(self, x):
+        y = paddle.exp(x)
+        return y - x
+
+    def forward(self, x, y):
+        if x.shape[-1] > 1:
+            x = self.exp_sub(x)
+        else:
+            y = paddle.abs(y)
+        x = paddle.nn.functional.relu(x)
+        y = paddle.logical_not(y)
+        return x, y
 
 
-class TestWhile(unittest.TestCase):
+class TestIfSubgraph(unittest.TestCase):
     def setUp(self):
-        paddle.seed(2022)
+        paddle.seed(2024)
         self.prepare_data()
 
     def prepare_data(self):
-        self.shape = [1, 2048, 768]
+        self.shape = [1, 2048]
         self.x = paddle.randn(self.shape, dtype="float32")
         self.x.stop_gradient = False
+
+        self.y_shape = [2, 256]
+        self.y = paddle.randn(self.y_shape, dtype="float32")
+        self.y.stop_gradient = False
 
     def check_jit_kernel_info(self, static_fn):
         utils.check_jit_kernel_number(static_fn, 1)
         utils.check_jit_kernel_structure(static_fn, {utils.JIT_KERNEL_NAME: 1})
 
     def eval(self, use_cinn):
-        net = WhileExpSub()
+        net = IfSubgraph()
         input_spec = [
-            InputSpec(shape=[1, None, 768], dtype='float32'),
+            InputSpec(shape=[None, None], dtype="float32"),
+            InputSpec(shape=[None, None], dtype="float32"),
         ]
         net = utils.apply_to_static(net, use_cinn, input_spec)
         net.eval()
-        out = net(self.x)
+        out = net(self.x, self.y)
         if use_cinn:
             self.check_jit_kernel_info(net.forward)
         return out
 
     def test_eval(self):
-        dy_out = self.eval(use_cinn=False)
+        dy_out_x, dy_out_y = self.eval(use_cinn=False)
         if utils.unittest_use_cinn():
-            cinn_out = self.eval(use_cinn=True)
+            cinn_out_x, cinn_out_y = self.eval(use_cinn=True)
             np.testing.assert_allclose(
-                cinn_out.numpy(), dy_out.numpy(), atol=1e-6, rtol=1e-6
+                cinn_out_x.numpy(), dy_out_x.numpy(), atol=1e-6, rtol=1e-6
+            )
+            np.testing.assert_allclose(
+                cinn_out_y.numpy(), dy_out_y.numpy(), atol=1e-6, rtol=1e-6
             )
 
 
