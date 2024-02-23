e04151434779c95f9d1199a5329a62237cf0ce5d
zyfncg <zhangyunfei07@baidu.com>
2024-02-23 03:29:25 +0000

    fix conflict

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/cinn_op_infer_sym.h
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/cinn_op_infer_sym.h
@@ -40,7 +40,4 @@ bool ReduceSumOpInferSymbolicShape(
 bool ReshapeOpInferSymbolicShape(
     pir::Operation *op, pir::ShapeConstraintIRAnalysis *shape_analysis);
 
-bool SliceOpInferSymbolicShape(pir::Operation *op,
-                               pir::ShapeConstraintIRAnalysis *shape_analysis);
-
 }  // namespace cinn::dialect
