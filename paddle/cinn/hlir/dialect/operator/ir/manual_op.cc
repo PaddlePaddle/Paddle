97bb3d988a69a45c440513d7ef7e4f435feb29ee
zyfncg <zhangyunfei07@baidu.com>
2024-02-23 02:46:42 +0000

    process 0D Tensor

--- paddle/cinn/hlir/dialect/operator/ir/manual_op.cc
+++ paddle/cinn/hlir/dialect/operator/ir/manual_op.cc
@@ -267,13 +267,12 @@ void GenerateShapeOp::Build(
     const std::vector<pir::Value>& inputs,
     const std::vector<pir::Attribute>& output_dim_exprs,
     const GenerateShapeOp::SymbolBindings& symbol_bindings) {
-  // CHECK(!inputs.empty()) << ". output_dim_exprs: " << [&] {
-  //   std::stringstream ss;
-  //   for (const auto& attr : output_dim_exprs) {
-  //     ss << attr;
-  //   }
-  //   return ss.str();
-  // }();
+  if (inputs.empty()) {
+    VLOG(4) << "GenerateShapeOp inputs is empty";
+    for (const auto& attr : output_dim_exprs) {
+      CHECK(attr.isa<pir::Int64Attribute>());
+    }
+  }
   argument.AddInputs(inputs);
   argument.AddAttribute("output_dim_exprs",
                         builder.array_attr(output_dim_exprs));
