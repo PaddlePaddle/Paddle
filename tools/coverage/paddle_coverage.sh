a6aa7b7f604a2ec06fce3944a920d9cebb48191b
Yangrl <2535184404@qq.com>
2024-02-28 09:48:17 +0000

    update

--- tools/coverage/paddle_coverage.sh
+++ tools/coverage/paddle_coverage.sh
@@ -250,7 +250,7 @@ fi
 
 if [ "$COVERAGE_LINES_ASSERT" = "1" ] || [ "$PYTHON_COVERAGE_LINES_ASSERT" = "1" ]; then
     echo "exit 9" > /tmp/paddle_coverage.result
-    if [ "$WITH_CINN" = "ON" ]; then
+    if [ "${WITH_CINN}" = "ON" ]; then
         echo "You must one RD(liuhongyu or lanxiang or zhenghuihuang or tianchao zhangliujie)to approval this PR."
     exit 9
 fi
