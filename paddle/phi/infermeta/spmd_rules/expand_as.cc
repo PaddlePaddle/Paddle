546e11bc4463691981242417dcde77aa07387c73
liuzhenhai93 <liuzhenhai93@outlook.com>
2024-02-28 12:23:35 +0800

    compile

--- paddle/phi/infermeta/spmd_rules/expand_as.cc
+++ paddle/phi/infermeta/spmd_rules/expand_as.cc
@@ -10,6 +10,8 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/phi/infermeta/spmd_rules/expand_as.h"
+
+#include "glog/logging.h"
 #include "paddle/phi/infermeta/spmd_rules/spmd_rule_macro_define.h"
 #include "paddle/phi/infermeta/spmd_rules/utils.h"
 
@@ -20,14 +22,12 @@ std::tuple<TensorDistAttr, TensorDistAttr> AlignExpandAsDistAttrs(
     const DistMetaTensor& x, const DistMetaTensor& y) {
   EXTRACT_SHAPE_AND_DIST_ATTR(x);
   EXTRACT_SHAPE_AND_DIST_ATTR(y);
-  LOG_SPMD_INPUT(x);
-  LOG_SPMD_INPUT(y);
   auto x_dist_attr_dst = CopyTensorDistAttrForOutput(x_dist_attr_src);
   auto y_dist_attr_dst = CopyTensorDistAttrForOutput(y_dist_attr_src);
   auto x_dims_mapping_dst = x_dims_mapping_src;
   auto y_dims_mapping_dst = y_dims_mapping_src;
-  int dims_diff = y_ndims - x_ndims;
-  for (int i = 0; i < y_ndims; ++i) {
+  int dims_diff = y_ndim - x_ndim;
+  for (int i = 0; i < y_ndim; ++i) {
     if (i >= dims_diff) {
       if (x_shape[i - dims_diff] == y_shape[i]) {
         x_dims_mapping_dst[i - dims_diff] = y_dims_mapping_src[i];
@@ -38,6 +38,8 @@ std::tuple<TensorDistAttr, TensorDistAttr> AlignExpandAsDistAttrs(
   }
   x_dist_attr_dst.set_dims_mapping(x_dims_mapping_dst);
   y_dist_attr_dst.set_dims_mapping(y_dims_mapping_dst);
+  LOG_SPMD_INPUT(x);
+  LOG_SPMD_INPUT(y);
   return {x_dist_attr_dst, y_dist_attr_dst};
 }
 
@@ -60,11 +62,11 @@ SpmdInfo ExpandAsGradInferSpmd(const DistMetaTensor& x,
                                const DistMetaTensor& out_grad,
                                const std::vector<int64_t>& target_shape) {
   auto [x_dist_attr, y_dist_attr] = AlignExpandAsDistAttrs(x, out_grad);
-  const auto& x_dims_mapping = x_dist_attr.get_dims_mapping();
-  const auto& y_dims_mapping = y_dist_attr.get_dims_mapping();
+  const auto& x_dims_mapping = x_dist_attr.dims_mapping();
+  const auto& y_dims_mapping = y_dist_attr.dims_mapping();
 
   // handle partial grad
-  auto x_grad_dist_attr = CopyTensorDistAttrForOutput(x_dist_attr);
+  auto x_grad_dist_attr = x_dist_attr;
   int x_ndims = x_dims_mapping.size();
   int y_ndims = y_dims_mapping.size();
   int dims_diff = y_ndims - x_ndims;
@@ -76,7 +78,7 @@ SpmdInfo ExpandAsGradInferSpmd(const DistMetaTensor& x,
       }
     }
   }
-  x_grad_dist_attr.set_partial(partial);
+  x_grad_dist_attr.set_partial_status(partial);
   return {{x_dist_attr, y_dist_attr}, {x_grad_dist_attr}};
 }
 
