0c18e69d2d2441c5d94431c002478a8c2f6ede75
GGBond8488 <857631483@qq.com>
2024-02-29 09:09:06 +0000

    add full jacbian test mode

--- test/legacy_test/autograd_checker_helper.py
+++ test/legacy_test/autograd_checker_helper.py
@@ -12,9 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections.abc import Sequence
+from itertools import product
+from logging import warning
+
 import numpy as np
 
 import paddle
+from paddle import base
 from paddle.autograd.backward_utils import ValueDict
 from paddle.base import core
 from paddle.base.backward import _as_list
@@ -24,87 +29,170 @@ __all__ = ['check_vjp']
 EPS = 1e-4
 
 default_gradient_tolerance = {
-    np.dtype(np.float16): 1e-2,
-    np.dtype(np.float32): 2e-3,
-    np.dtype(np.float64): 1e-5,
-    np.dtype(np.complex64): 1e-3,
-    np.dtype(np.complex128): 1e-5,
+    np.float16: 1e-2,
+    np.float32: 2e-3,
+    np.float64: 1e-5,
+    np.complex64: 1e-3,
+    np.complex128: 1e-5,
 }
 
 
-def check_close(
-    eager_vjps_np,
-    static_vjps_np,
-    numerical_jvps_np,
-    tangents,
-    cotangents,
-    atol,
-    rtol,
+def _product(t):
+    return int(np.prod(t))
+
+
+def make_jacobian(x, y_size, np_dtype):
+    if isinstance(x, (base.framework.Variable, paddle.pir.Value)):
+        return np.zeros((_product(x.shape), y_size), dtype=np_dtype)
+    elif isinstance(x, Sequence):
+        jacobians = list(
+            filter(
+                lambda t: t is not None,
+                (make_jacobian(item, y_size, np_dtype) for item in x),
+            )
+        )
+        return jacobians
+    else:
+        pass
+
+
+def _compute_numerical_jacobian(program, x, y, feeds, eps):
+    if not isinstance(x, paddle.pir.Value):
+        raise TypeError('x is not Value')
+
+    # To compute the jacobian, treat x and y as one-dimensional vectors.
+    y = _as_list(y)
+    exe = paddle.static.Executor()
+
+    def run():
+        res = exe.run(program, feeds, fetch_list=[y])
+        y_res = res[: len(y)]
+        return [yi.flatten() for yi in y_res]
+
+    x_name = x.get_defining_op().attrs()['name']
+    x_shape = x.shape
+    x_size = _product(x_shape)
+    np_type = dtype_to_np_dtype(x.dtype)
+    np_t = np.array(feeds[x_name]).astype(np_type)
+    np_t = np_t.flatten()
+    jacobian = [make_jacobian(x, _product(yi.shape), np_type) for yi in y]
+
+    for i in range(x_size):
+        orig = np_t[i]
+        x_pos = orig + eps
+        np_t[i] = x_pos
+        np_f = np_t.reshape(x_shape)
+        feeds[x_name] = np_f
+        y_pos = run()
+
+        x_neg = orig - eps
+        np_t[i] = x_neg
+        np_f = np_t.reshape(x_shape)
+        feeds[x_name] = np_f
+        y_neg = run()
+
+        np_t[i] = orig
+        for j in range(len(y)):
+            ret = (y_pos[j] - y_neg[j]) / eps / 2.0
+            jacobian[j][i, :] = ret
+
+    return jacobian
+
+
+def _compute_analytical_jacobian(program, x, i, y, grads, feeds, name):
+    if not isinstance(x, (list, paddle.pir.Value)):
+        raise TypeError('x is not Value or list of Value')
+    np_type = dtype_to_np_dtype(y[i].dtype)
+    exe = paddle.static.Executor()
+    y_size = _product(y[i].shape)
+    x = _as_list(x)
+    jacobian = make_jacobian(x, y_size, np_type)
+
+    # get the name in feeds of dyi
+    np_t = np.array(feeds[name]).astype(np_type)
+    shape = np_t.shape
+    np_t = np_t.flatten()
+    for i in range(y_size):
+        np_t[i] = 1
+        np_f = np_t.reshape(shape)
+        feeds[name] = np_f
+        res = exe.run(program, feed=feeds, fetch_list=[grads])
+        dx_res = res[: len(grads)]
+        for j in range(len(grads)):
+            if dx_res[j] is not None:
+                jacobian[j][:, i] = dx_res[j].flatten()
+            else:
+                jacobian[j][:, i] = np.zeros(
+                    grads[j].shape, dtype=np_type
+                ).flatten()
+
+        np_t[i] = 0
+        np_f = np_t.reshape(shape)
+        feeds[name] = np_f
+
+    return jacobian
+
+
+def grad_check(
+    inputs,
+    outputs,
+    last_grads_in,
+    feeds=None,
+    fetch_list=None,
+    program=None,
+    eps=1e-6,
+    atol=1e-5,
+    rtol=1e-3,
+    raise_exception=True,
 ):
-    eager_vjps_np = eager_vjps_np[0].reshape([-1])
-    static_vjps_np = static_vjps_np[0].reshape([-1])
-    numerical_jvps_np = numerical_jvps_np[0].reshape([-1])
-    tangents = tangents[0].reshape([-1])
-    cotangents = cotangents[0].reshape([-1])
-    dtype = eager_vjps_np.dtype
-    atol = atol if atol else default_gradient_tolerance[dtype]
-    rtol = rtol if rtol else default_gradient_tolerance[dtype]
-    np.testing.assert_allclose(
-        eager_vjps_np,
-        static_vjps_np,
-        atol=atol,
-        rtol=rtol,
-        err_msg="The grad of the eager is not equal to the static",
-    )
-    inner_dot_ret = np.dot(eager_vjps_np, tangents)
-    inner_dot_ret_expect = np.dot(cotangents, numerical_jvps_np)
-    np.testing.assert_allclose(
-        inner_dot_ret,
-        inner_dot_ret_expect,
-        rtol=rtol,
-        err_msg="The grad of analytical is not equal to the numerical",
-    )
+    def fail_test(msg):
+        if raise_exception:
+            raise RuntimeError(msg)
+        return False
 
-
-# Use the existing vjp method to calculate n order, provided that n-1 order is correct
-def f_vjp_wrapper(func):
-    def inner_wrapper(args):
-        out, vjp_result = paddle.incubate.autograd.vjp(func, args)
-        return vjp_result
-
-    return inner_wrapper
-
-
-def derivative(func, primals, order=1):
-    if order > 1:
-        f_vjp = f_vjp_wrapper(func)
-        outputs = f_vjp(primals)
-        return derivative(f_vjp, _as_list(outputs), order - 1)
-    return func(primals)
-
-
-def derivate_wrapper(func, order=1):
-    def inner_wrapper(primals):
-        return derivative(func, primals, order=order)
-
-    return inner_wrapper
+    analytical = []
+    for i in range(len(outputs)):
+        name = last_grads_in[i].name
+        feeds.update(
+            {
+                name: np.zeros(
+                    outputs[i].shape, dtype=dtype_to_np_dtype(outputs[i].dtype)
+                )
+            }
+        )
+    for i in range(len(outputs)):
+        analytical.append(
+            _compute_analytical_jacobian(
+                program,
+                inputs,
+                i,
+                outputs,
+                fetch_list,
+                feeds,
+                last_grads_in[i].name,
+            )
+        )
+    numerical = [
+        _compute_numerical_jacobian(program, input, outputs, feeds, eps)
+        for input in inputs
+    ]
+    for i, (x_idx, y_idx) in enumerate(
+        product(*[range(len(inputs)), range(len(outputs))])
+    ):
+        a = analytical[y_idx][x_idx]
+        n = numerical[x_idx][y_idx]
+        if not np.allclose(a, n, rtol, atol):
+            msg = (
+                f'Jacobian mismatch for output {y_idx} in y'
+                f'with respect to input {x_idx} in x,\n'
+                f'numerical:{n}\nanalytical:{a}\n'
+            )
+            return fail_test(msg)
+    return True
 
 
 def numerical_jvp(func, primals, tangents, order=1, eps=EPS):
-    func = derivate_wrapper(func, order)
-    delta = []
-    for value in tangents:
-        v = paddle.scale(value, eps)
-        delta.append(v)
-    f_pos = func(list(map(paddle.add, primals, delta)))
-    f_pos = _as_list(f_pos)
-    f_neg = func(list(map(paddle.subtract, primals, delta)))
-    f_neg = _as_list(f_neg)
-    sub_deltas = list(map(paddle.subtract, f_pos, f_neg))
-    jvp_rets = []
-    for sub_delta in sub_deltas:
-        jvp_rets.append(paddle.scale(sub_delta, 0.5 / eps))
-    return jvp_rets
+    pass
 
 
 def dtype_to_np_dtype(dtype):
@@ -133,8 +221,12 @@ def get_eager_vjp(func, inputs, tangents=None, order=1):
 
 
 def _get_eager_vjp(inputs, outputs, tangents, order):
-    create_graph = True
-    allow_unused = True
+    if order > 1:
+        create_graph = True
+        allow_unused = True
+    else:
+        create_graph = False
+        allow_unused = False
 
     d_inputs = paddle.grad(
         outputs=outputs,
@@ -156,7 +248,7 @@ def _get_eager_vjp(inputs, outputs, tangents, order):
     return d_inputs
 
 
-def get_static_vjp(func, inputs, tangents, order=1):
+def get_static_vjp(func, inputs, tangents, order, atol, rtol, eps):
     tangents = _as_list(tangents)
     tangents = [tangent.numpy() for tangent in tangents]
     paddle.enable_static()
@@ -181,21 +273,40 @@ def get_static_vjp(func, inputs, tangents, order=1):
     for input in input_vars:
         pir_inputs.append(op_map[input])
     pir_outputs = []
-    for output in outputs:
-        pir_outputs.append(op_map[output])
+    grads_in_init = []
     with paddle.static.program_guard(program):
-        feeds, pre_outputs, d_inputs = _get_static_vjp(
-            pir_inputs, pir_outputs, feeds, tangents, order
+        for idx, output in enumerate(outputs):
+            pir_outputs.append(op_map[output])
+            np_type = dtype_to_np_dtype(input.dtype)
+            grad_in_var = paddle.static.data(
+                'grad_in_' + str(idx), output.shape, dtype=np_type
+            )
+            grads_in_init.append(grad_in_var)
+            feeds.update({'grad_in_' + str(idx): tangents[idx]})
+        feeds, pre_outputs, d_inputs, last_grads_in = _get_static_vjp_program(
+            pir_inputs, pir_outputs, feeds, grads_in_init, order
         )
         exe = paddle.static.Executor()
         res = exe.run(program, feed=feeds, fetch_list=[d_inputs])
-    print(program)
+    if not d_inputs:
+        warning(f"{func.__name__} don't support {order}s derivative")
+        return []
+    grad_check(
+        pir_inputs,
+        pre_outputs,
+        last_grads_in,
+        feeds,
+        d_inputs,
+        program,
+        eps,
+        atol,
+        rtol,
+    )
     paddle.disable_static()
-
     return res
 
 
-def _get_static_vjp(inputs, outputs, feeds, tangents, order):
+def _get_static_vjp_program(inputs, outputs, feeds, grads_in, order):
     def _require_grads(vars):
         for var in vars:
             var.stop_gradient = False
@@ -203,37 +314,36 @@ def _get_static_vjp(inputs, outputs, feeds, tangents, order):
 
     inputs = _as_list(inputs)
     outputs = _as_list(outputs)
-    tangents = _as_list(tangents)
     _require_grads(inputs)
     _require_grads(outputs)
-    y_grads = []
-    for idx, output in enumerate(outputs):
-        output.persistable = True
-        np_type = dtype_to_np_dtype(output.dtype)
-        dy = paddle.static.data(
-            name=f'tangents_{order}_{idx}',
-            shape=output.shape,
-            dtype=np_type,
-        )
-        feeds.update({f'tangents_{order}_{idx}': tangents[idx]})
-        y_grads.append(dy)
-    _require_grads(y_grads)
-    d_inputs = paddle.base.gradients(outputs, inputs, y_grads)
+    _require_grads(grads_in)
+    d_inputs = paddle.base.gradients(outputs, inputs, grads_in)
     d_inputs = [d_input for d_input in d_inputs if d_input is not None]
     _require_grads(d_inputs)
 
     if order > 1:
         ddys = []
-        for d_input in d_inputs:
-            ddy = np.ones(d_input.shape, dtype=dtype_to_np_dtype(d_input.dtype))
+        for idx, d_input in enumerate(d_inputs):
+            np_type = dtype_to_np_dtype(d_input.dtype)
+            ddy = paddle.static.data(
+                name=f'dy_{idx}_{order}',
+                shape=d_input.shape,
+                dtype=np_type,
+            )
+            ones = np.ones(d_input.shape, dtype=np_type)
+            feeds.update({f'dy_{idx}_{order}': ones})
             ddys.append(ddy)
-        return _get_static_vjp(inputs, d_inputs, feeds, ddys, order - 1)
-    return feeds, outputs, d_inputs
+        _require_grads(ddys)
+        return _get_static_vjp_program(inputs, d_inputs, feeds, ddys, order - 1)
+    return feeds, outputs, d_inputs, grads_in
 
 
-def check_vjp(func, args, order=2, atol=None, rtol=None, eps=EPS):
+def check_vjp(func, args, order=1, atol=None, rtol=None, eps=EPS):
     args = _as_list(args)
     tangents = []
+    np_type = dtype_to_np_dtype(args[0].dtype)
+    atol = atol if atol else default_gradient_tolerance[np_type]
+    rtol = rtol if rtol else default_gradient_tolerance[np_type]
     for arg in args:
         t = paddle.randn(arg.shape, arg.dtype)
         tangents.append(t)
@@ -244,17 +354,11 @@ def check_vjp(func, args, order=2, atol=None, rtol=None, eps=EPS):
     eager_vjps_np = []
     for eager_vjp in eager_vjps:
         eager_vjps_np.append(eager_vjp.numpy())
-    static_vjps_np = get_static_vjp(func, args, cotangents, order=order)
-    numerical_jvps = numerical_jvp(func, args, tangents, order=order, eps=eps)
-    numerical_jvps_np = []
-    for num_jvp in numerical_jvps:
-        numerical_jvps_np.append(num_jvp.numpy())
-    check_close(
+    static_vjps_np = get_static_vjp(
+        func, args, cotangents, order, atol, rtol, eps=EPS
+    )
+    np.testing.assert_allclose(
         eager_vjps_np,
         static_vjps_np,
-        numerical_jvps_np,
-        tangents,
-        cotangents,
-        atol,
-        rtol,
+        err_msg="static vjp must be equal to eager",
     )
