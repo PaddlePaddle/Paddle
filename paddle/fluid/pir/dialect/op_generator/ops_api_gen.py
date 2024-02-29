9d64ee38e1a4bb2d0e42d41d39c170f8d021721b
Zhan Rongrui <2742392377@qq.com>
2024-02-29 04:40:28 +0000

    fix

--- paddle/fluid/pir/dialect/op_generator/ops_api_gen.py
+++ paddle/fluid/pir/dialect/op_generator/ops_api_gen.py
@@ -70,7 +70,6 @@ OPS_API_TEMPLATE = """
 
 NEED_GEN_STATIC_ONLY_APIS = [
     'fetch',
-    'fused_multi_transformer',
     'fused_bias_dropout_residual_layer_norm',
     'fused_embedding_eltwise_layernorm',
     'fused_fc_elementwise_layernorm',
