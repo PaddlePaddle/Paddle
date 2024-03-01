8405359457e5681bdf1f7878144d9d14ea64e596
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2024-03-01 11:18:26 +0800

    Update paddle_coverage.sh

--- tools/coverage/paddle_coverage.sh
+++ tools/coverage/paddle_coverage.sh
@@ -33,7 +33,6 @@ make install
 
 cd /paddle/build
 
-python ${PADDLE_ROOT}/tools/coverage/gcda_clean.py ${GIT_PR_ID} || exit 101
 
 lcov --capture -d ./ -o coverage.info --rc lcov_branch_coverage=0
 
