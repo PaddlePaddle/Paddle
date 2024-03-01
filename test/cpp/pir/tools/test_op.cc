4d0be7f12b2c6d6ee629c2bc5d9dd587ae5f8f6e
cyberslack_lee <jeffrey0122@163.com>
2024-03-01 10:57:48 +0800

    [clang-tidy] NO.24 enable hicpp-exception-baseclass (#61691)

--- test/cpp/pir/tools/test_op.cc
+++ test/cpp/pir/tools/test_op.cc
@@ -11,10 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #include "test/cpp/pir/tools/test_op.h"
 #include "paddle/common/enforce.h"
+#include "paddle/fluid/platform/errors.h"
+#include "paddle/phi/core/enforce.h"
 #include "paddle/pir/include/core/builtin_attribute.h"
-
 namespace test {
 
 void RegionOp::Build(pir::Builder &builder, pir::OperationArgument &argument) {
@@ -50,11 +52,13 @@ void Operation1::VerifySig() const {
   auto &attributes = this->attributes();
   if (attributes.count("op1_attr1") == 0 ||
       !attributes.at("op1_attr1").isa<pir::StrAttribute>()) {
-    throw("Type of attribute: parameter_name is not right.");
+    PADDLE_THROW(paddle::platform::errors::Fatal(
+        "Type of attribute: parameter_name is not right."));
   }
   if (attributes.count("op1_attr2") == 0 ||
       !attributes.at("op1_attr2").isa<pir::StrAttribute>()) {
-    throw("Type of attribute: parameter_name is not right.");
+    PADDLE_THROW(paddle::platform::errors::Fatal(
+        "Type of attribute: parameter_name is not right."));
   }
 }
 
