29096744be46430cdb43d617afc2960cec5dc6cb
SigureMo <sigure.qaq@gmail.com>
2024-03-01 03:18:20 +0000

    tmp commit for enable TestDy2StIfElseBackward

--- test/dygraph_to_static/test_ifelse.py
+++ test/dygraph_to_static/test_ifelse.py
@@ -554,6 +554,7 @@ class IfElseNet(paddle.nn.Layer):
         a = paddle.matmul(a, self.param)
         a = paddle.reshape(a, (2, 4))
         cond = paddle.to_tensor([10])
+        b = b.broadcast_to(self.param.shape)
         if paddle.equal(cond, 10):
             a_argmax = a.argmax(axis=-1)
             b = b + self.param
@@ -564,6 +565,7 @@ class IfElseNet(paddle.nn.Layer):
 
 class TestDy2StIfElseBackward(Dy2StTestBase):
     # TODO(zhangbo): open pir test (IfOp grad execution not yet supported)
+    @test_legacy_and_pt_and_pir
     @disable_test_case((ToStaticMode.AST, IrMode.PT))
     def test_run_backward(self):
         a = paddle.randn((4, 3), dtype='float32')
