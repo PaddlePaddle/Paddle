4cb8897acb1295416500d1a5cfa815d95d2a8f4f
AyaseNana <13659110308@163.com>
2024-03-03 16:22:59 +0800

    update ctcloss add use_softmax and zero_infinity

--- python/paddle/nn/layer/loss.py
+++ python/paddle/nn/layer/loss.py
@@ -1195,12 +1195,13 @@ class CTCLoss(Layer):
         reduction (string, optional): Indicate how to average the loss, the candicates are ``'none'`` | ``'mean'`` | ``'sum'``. If :attr:`reduction` is ``'mean'``, the output loss will be divided by the label_lengths, and then return the mean of quotient; If :attr:`reduction` is ``'sum'``, return the sum of loss; If :attr:`reduction` is ``'none'``, no reduction will be applied. Default is ``'mean'``.
 
     Shape:
-        - log_probs (Tensor): The unscaled probability sequence with padding, which is a 3-D Tensor. The tensor shape is [max_logit_length, batch_size, num_classes + 1], where max_logit_length is the longest length of input logit sequence. The data type should be float32 or float64.
+        - log_probs (Tensor): The probability sequence with padding, which is a 3-D Tensor. The tensor shape is [max_logit_length, batch_size, num_classes + 1], where max_logit_length is the longest length of input logit sequence. The data type should be float32 or float64.  Whether the input probabilities should be scaled depend on the value of ``use_softmax``.
         - labels (Tensor): The ground truth sequence with padding, which must be a 3-D Tensor. The tensor shape is [batch_size, max_label_length], where max_label_length is the longest length of label sequence. The data type must be int32.
         - input_lengths (Tensor): The length for each input sequence, it should have shape [batch_size] and dtype int64.
         - label_lengths (Tensor): The length for each label sequence, it should have shape [batch_size] and dtype int64.
         - norm_by_times (bool, optional): Whether to normalize the gradients by the number of time-step, which is also the sequence's length. There is no need to normalize the gradients if reduction mode is 'mean'. Default: False.
-
+        - use_softmax (bool, optional): Whether to perform logsoftmax on the input ``log_probs``. Default value is True, which means the input ``log_probs`` are regard as unscaled logits. When set to False, the input ``log_probs`` should be the output of logsoftmax.
+        - zero_infinity (bool, optional): Whether to set the infinite loss and its associated gradients to zero. Default value is False.
     Returns:
         Tensor, The Connectionist Temporal Classification (CTC) loss between ``log_probs`` and  ``labels``. If attr:`reduction` is ``'none'``, the shape of loss is [batch_size], otherwise, the shape of loss is []. Data type is the same as ``log_probs``.
 
@@ -1257,6 +1258,8 @@ class CTCLoss(Layer):
         input_lengths,
         label_lengths,
         norm_by_times=False,
+        use_softmax=True,
+        zero_infinity=False,
     ):
         return paddle.nn.functional.ctc_loss(
             log_probs,
@@ -1266,6 +1269,8 @@ class CTCLoss(Layer):
             self.blank,
             self.reduction,
             norm_by_times=norm_by_times,
+            use_softmax=use_softmax,
+            zero_infinity=zero_infinity,
         )
 
 
