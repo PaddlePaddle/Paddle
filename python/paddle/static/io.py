a71d5c25d62deab21865b0de19c5b8e4b09df598
Omri Alon <oalon@salesforce.com>
2024-03-03 13:24:01 +0200

    remove

--- python/paddle/static/io.py
+++ python/paddle/static/io.py
@@ -146,7 +146,7 @@ def _safe_load_pickle(file, encoding="ASCII"):
     try:
         load_dict = pickle.Unpickler(file, encoding=encoding).load()
     except pickle.UnpicklingError:
-        print("Error occurred during unpickling.")
+        print("Error occurred during unpickling")
         load_dict = None
     return load_dict
 
