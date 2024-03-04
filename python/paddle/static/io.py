c18f8396f5dde4a086e501aa52ee6a99369fc2d2
Omri Alon <oalon@salesforce.com>
2024-03-04 10:41:33 +0200

    change to raise instead of print

--- python/paddle/static/io.py
+++ python/paddle/static/io.py
@@ -146,8 +146,7 @@ def _safe_load_pickle(file, encoding="ASCII"):
     try:
         load_dict = pickle.Unpickler(file, encoding=encoding).load()
     except pickle.UnpicklingError:
-        print("Error occurred during unpickling.")
-        load_dict = None
+        raise RuntimeError("Error occurred during unpickling.")
     return load_dict
 
 
