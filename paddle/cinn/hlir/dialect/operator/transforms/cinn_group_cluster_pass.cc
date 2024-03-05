cf5a0eeab71bd3b80d73fdaf20cd4b504081aefc
phlrain <phliuhongyu@126.com>
2024-03-05 19:10:20 +0800

    remove apply check

--- paddle/cinn/hlir/dialect/operator/transforms/cinn_group_cluster_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/cinn_group_cluster_pass.cc
@@ -876,7 +876,7 @@ class CinnGroupClusterPass : public pir::PatternRewritePass {
   }
 
   bool CanApplyOn(pir::Operation* op) const override {
-    return op->isa<pir::ModuleOp>() && op->num_regions() > 0;
+    return op->num_regions() > 0;
   }
 };
 
