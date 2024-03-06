c7b3acfae3db2372788ef4b7ca2c3cc591982bb8
BiynXu <62832681+BiynXu@users.noreply.github.com>
2024-03-06 14:08:53 +0800

    fix group copy (#62409)

--- paddle/cinn/hlir/dialect/operator/transforms/add_cinn_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/add_cinn_pass.cc
@@ -134,7 +134,6 @@ void ApplyGroupOpPass(::pir::Program* program,
 
   pass_manager->AddPass(cinn::dialect::ir::CreateDynamicReshapeOpPass());
   pass_manager->AddPass(cinn::dialect::ir::CreateReplaceDynamicExpandOpPass());
-
   pass_manager->AddPass(pir::CreateDeadCodeEliminationPass());
 
   pass_manager->Run(program);
