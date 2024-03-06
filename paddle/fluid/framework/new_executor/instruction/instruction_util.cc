2291b0ee5c7bebb6ef8884d238221584048267a8
zhangbo9674 <zhangbo54@baidu.com>
2024-03-06 10:29:20 +0000

    fix

--- paddle/fluid/framework/new_executor/instruction/instruction_util.cc
+++ paddle/fluid/framework/new_executor/instruction/instruction_util.cc
@@ -368,12 +368,13 @@ void InsertInplacedExternalInputsToOuts(
                       .dyn_cast<pir::StrAttribute>()
                       .AsString();
       }
-      pir::OpInfo op_info =
-          pir::IrContext::Instance()->GetRegisteredOpInfo(op_name);
+
+      auto info = pir::IrContext::Instance()
+                      ->GetRegisteredOpInfo(op_name)
+                      .GetInterfaceImpl<paddle::dialect::OpYamlInfoInterface>()
+                      ->get_op_info_(op_name);
       paddle::dialect::OpYamlInfoParser yaml_parser(
-          op_info.GetInterfaceImpl<paddle::dialect::OpYamlInfoInterface>()
-              ->get_op_info_(op_name),
-          paddle::dialect::IsLegacyOp(yaml_parser.OpRuntimeInfo().kernel_func));
+          info, paddle::dialect::IsLegacyOp(std::get<3>(info).kernel_func));
 
       for (size_t i = 0; i < op.num_results(); ++i) {
         pir::Value value = op.result(i);
