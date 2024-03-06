cfd4408bbf3e30b0b071c178bb301f287ed2059d
kangguangli <kangguangli@hotmail.com>
2024-03-06 09:32:15 +0000

    fix reduce op translation

--- paddle/fluid/ir_adaptor/translator/op_translator.cc
+++ paddle/fluid/ir_adaptor/translator/op_translator.cc
@@ -2294,7 +2294,19 @@ pir::Attribute TranslateReduceAll(pir::IrContext* ctx,
   }
 
   if (reduce_all) {
-    return pir::ArrayAttribute::get(ctx, std::vector<pir::Attribute>{});
+    auto x_names = op_desc.Input("X");
+    auto x_name = x_names[0];
+    const VarDesc* var_desc = op_desc.Block()->FindVarRecursive(x_name);
+    IR_ENFORCE(var_desc != nullptr,
+               "VarDesc `%s` should be exist in legacy program",
+               x_name);
+
+    int axis_size = var_desc->GetShape().size();
+    std::vector<pir::Attribute> axis;
+    for (int64_t i = 0; i < axis_size; i++) {
+      axis.push_back(pir::Int64Attribute::get(ctx, i));
+    }
+    return pir::ArrayAttribute::get(ctx, axis);
   }
 
   auto& attribute_translator = AttributeTranslator::instance();
@@ -3178,8 +3190,6 @@ OpTranslator::OpTranslator() {
   special_handlers["lookup_table_v2_grad"] = EmbeddingGradOpTranscriber();
   special_handlers["one_hot_v2"] = OneHotTranscriber();
   special_handlers["randint"] = RandIntOpTranscriber();
-  special_handlers["reduce_all"] = ReduceOpTranscriber();
-  special_handlers["reduce_any"] = ReduceOpTranscriber();
   special_handlers["repeat_interleave"] = RepeatInterLeaveOpTranscriber();
   special_handlers["repeat_interleave_grad"] =
       RepeatInterLeaveGradOpTranscriber();
@@ -3199,6 +3209,16 @@ OpTranslator::OpTranslator() {
   special_handlers["mul_grad"] = MulGradOpTranscriber();
   special_handlers["select_input"] = SelectInputOpTranscriber();
   special_handlers["select_output"] = SelectOutputOpTranscriber();
+
+  // reduce operations
+  special_handlers["reduce_all"] = ReduceOpTranscriber();
+  special_handlers["reduce_any"] = ReduceOpTranscriber();
+  special_handlers["reduce_amax"] = ReduceOpTranscriber();
+  special_handlers["reduce_amin"] = ReduceOpTranscriber();
+  special_handlers["reduce_max"] = ReduceOpTranscriber();
+  special_handlers["reduce_min"] = ReduceOpTranscriber();
+  special_handlers["reduce_mean"] = ReduceOpTranscriber();
+  special_handlers["reduce_sum"] = ReduceOpTranscriber();
 
   // To adapt LodTensorArray
   special_handlers["lod_array_length"] = LodArrayLengthOpTranscriber();
