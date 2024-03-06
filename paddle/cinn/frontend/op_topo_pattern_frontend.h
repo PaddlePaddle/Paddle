3b39893b6819572e6438f2b5e45594d0468ecab4
jiahy0825 <jiahongyu@baidu.com>
2024-03-06 09:41:34 +0000

    op_topo_pattern_fronten

---
+++ paddle/cinn/frontend/op_topo_pattern_frontend.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <unordered_map>
+#include "paddle/cinn/api/op_topo_pattern.h"
+#include "paddle/pir/include/core/operation.h"
+
+namespace cinn::frontend {
+
+struct FrontendPattern {};
+
+}
+
+namespace cinn::api {
+
+template<>
+struct InjectiveSourcePattern<cinn::frontend::FrontendPattern> {
+  std::vector<const pir::Operation*> ops;
+};
+
+template<>
+struct SingleReductionOpPattern<cinn::frontend::FrontendPattern> {
+  const pir::Operation* reduce_op;
+};
+
+struct ShardableAxes {
+  int axis;
+  std::string axis_name;
+};
+
+struct ShardableAxesSignature {
+  using OpOperand = std::pair<const pir::Operation*, /*operand index*/int>;
+
+  ShardableAxes output_shardable_axes;
+  std::unordered_map<OpOperand, ShardableAxes> input_shardable_axes;
+};
+
+template<>
+struct PartialShardablePattern<cinn::frontend::FrontendPattern> {
+  std::vector<const pir::Operation*> ops;
+  ShardableAxesSignature shardable_axes_signature;
+};
+
+}
+
+namespace cinn::frontend {
+
+using GroupPattern = api::OpTopoPattern<FrontendPattern>;
+
+}
