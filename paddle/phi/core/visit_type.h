d23aa3f2f169a70dc09873b73f2e50ab521deb4f
tongkai <1092019531@qq.com>
2024-03-07 21:49:41 +0800

    remove VLOG

--- paddle/phi/core/visit_type.h
+++ paddle/phi/core/visit_type.h
@@ -14,7 +14,6 @@ limitations under the License. */
 
 #pragma once
 
-#include <glog/logging.h>
 #include "paddle/common/exception.h"
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/core/kernel_factory.h"
@@ -479,10 +478,6 @@ namespace phi {
         phi::KernelFactory::Instance().SelectKernelOrThrowError(kernel_name, \
                                                                 kernel_key); \
     const auto& kernel = kernel_result.kernel;                               \
-    if (kernel_result.has_fallback_cpu) {                                    \
-      VLOG(6) << "missing kernel: " << kernel_name;                          \
-    }                                                                        \
-    VLOG(6) << kernel_name << " kernel: " << kernel;                         \
     auto* kernel_fn = kernel.GetVariadicKernelFn<kernel_signature>();        \
     (*kernel_fn)(__VA_ARGS__);                                               \
   }()
