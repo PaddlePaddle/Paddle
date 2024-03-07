e74e82f0c0dcdbbb7db3d1207de43c4f39ad8166
yulangz <1301481108@qq.com>
2024-03-07 06:25:49 +0000

    remove create_numpy_inputs

--- test/ir/pir/cinn/symbolic/test_sub_graph_stable_diffusion_24_st.py
+++ test/ir/pir/cinn/symbolic/test_sub_graph_stable_diffusion_24_st.py
@@ -56,14 +56,6 @@ def create_paddle_inputs():
     return inputs
 
 
-def create_numpy_inputs():
-    inputs = (
-        np.random.random(size=[1, 640, 1, 1]).astype('float32'),
-        np.random.randint(low=0, high=10, size=[2], dtype='int32'),
-    )
-    return inputs
-
-
 class TestLayer(unittest.TestCase):
     def setUp(self):
         self.inputs = create_paddle_inputs()
