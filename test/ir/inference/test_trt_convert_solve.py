2dbef0f15905169edf13f5c7ecfc29f8c95f8685
Reese Wang <rewang@nvidia.com>
2024-03-07 10:50:28 +0800

    Enhance several unit tests (#62477)
    * Manually release predictor_tuned
    
    Signed-off-by: rewang <rewang@nvidia.com>
    
    * Add indices to no_cast_list to keep it as fp32
    
    Signed-off-by: rewang <rewang@nvidia.com>
    
    * Set both atol and rtol for the fp16 test_trt_convert_solve
    
    Signed-off-by: rewang <rewang@nvidia.com>
    
    * Merge branch 'rewang/fix_test_sparse_fused_attention_seed' into 'nv-2.6.0'
    
    Fix test_sparse_fused_attention random seed
    
    See merge request dl/paddle/paddle!312
    
    ---------
    
    Signed-off-by: rewang <rewang@nvidia.com>
    Co-authored-by: Ryan Jeng <rjeng@nvidia.com>

--- test/ir/inference/test_trt_convert_solve.py
+++ test/ir/inference/test_trt_convert_solve.py
@@ -90,7 +90,7 @@ class TrtConvertSolve(TrtLayerAutoScanTest):
         yield self.create_inference_config(), (1, 3), 1e-5
 
         self.trt_param.precision = paddle_infer.PrecisionType.Half
-        yield self.create_inference_config(), (1, 3), 1e-3
+        yield self.create_inference_config(), (1, 3), (1e-3, 1e-3)
 
     def test(self):
         self.run_test()
