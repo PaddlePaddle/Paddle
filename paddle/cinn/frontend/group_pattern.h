92bf72b6286ce3a61c7af6923964e825de133baf
feifei-111 <2364819892@qq.com>
2024-03-07 06:21:52 +0000

    update

--- paddle/cinn/frontend/group_pattern.h
+++ paddle/cinn/frontend/group_pattern.h
@@ -16,17 +16,21 @@ namespace cinn::api {
 
 template<>
 struct ErrorPattern<frontend::FrontendPattern> {
+  explicit ErrorPattern(const ErrorPattern<frontend::FrontendPatterns>& other) = default;
+
   const pir::Operation* op;
   std::string error_string;
 };
 
 template<>
 struct InjectiveSourcePattern<frontend::FrontendPattern> {
+  explicit InjectiveSourcePattern(const InjectiveSourcePattern<frontend::FrontendPatterns>& other) = default;
   std::vector<const pir::Operation*> ops;
 };
 
 template<>
 struct SingleReductionOpPattern<frontend::FrontendPattern> {
+  explicit SingleReductionOpPattern(const SingleReductionOpPattern<frontend::FrontendPatterns>& other) = default;
   const pir::Operation* reduce_op;
 };
 
@@ -51,8 +55,14 @@ struct ShardableAxesSignature {
 
 template<>
 struct PartialShardablePattern<frontend::FrontendPattern> {
+  explicit PartialShardablePattern(const PartialShardablePattern<frontend::FrontendPatterns>& other) = default;
   std::vector<const pir::Operation*> ops;
   ShardableAxesSignature shardable_axes_signature;
+};
+
+template<>
+struct ReductionPattern<frontend::FrontendPattern> {
+  explicit ReductionPattern(const ReductionPattern<frontend::FrontendPatterns>& other) = default;
 };
 
 }
