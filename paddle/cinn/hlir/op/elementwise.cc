49ba6e5b6c986a8e37aabe1093425676c5693c95
6clc <chaoliu.lc@foxmail.com>
2024-03-08 16:59:21 +0800

    cinn(op): fix generate shape op

--- paddle/cinn/hlir/op/elementwise.cc
+++ paddle/cinn/hlir/op/elementwise.cc
@@ -29,6 +29,8 @@
 #include "paddle/cinn/ir/ir_base.h"
 #include "paddle/cinn/ir/op/ir_operators.h"
 #include "paddle/cinn/utils/functional.h"
+#include "paddle/common/enforce.h"
+#include "paddle/phi/core/enforce.h"
 
 namespace cinn {
 namespace hlir {
@@ -530,8 +532,9 @@ std::shared_ptr<OpStrategy> StrategyForFillConstantSymbolic(
     const Target &target) {
   framework::CINNCompute fill_constant_compute(
       [=](lang::Args args, lang::RetValue *ret) {
-        CHECK(!args.empty()) << "The input argument of fill_constant compute "
-                                "is empty! Please check.";
+        PADDLE_ENFORCE(!args.empty(),
+                       "The input argument of fill_constant compute "
+                       "is empty! Please check.");
         bool force_cpu = false;
         CHECK(attrs.attr_store.count("shape"));
         auto shape = absl::get<std::vector<int>>(attrs.attr_store.at("shape"));
@@ -1254,40 +1257,36 @@ std::shared_ptr<framework::OpStrategy> StrategyForGenerateShapeSymbolic(
     const std::vector<Type> &out_type,
     const std::vector<std::vector<ir::Dim>> &output_shapes,
     const Target &target) {
-  framework::CINNCompute cast_compute(
-      [=](lang::Args args, lang::RetValue *ret) {
-        CHECK(!args.empty())
-            << "The input arguments of Cast compute is empty! Please check.\n";
-        CINNValuePack pack_args = args[0];
-        CHECK_GE(pack_args.size(), 1U)
-            << "at least 1 input tensors for Cast compute\n";
-        Expr A = pack_args[0];
-        CHECK(A.as_tensor());
-        CHECK(!output_shapes.empty());
-        auto tensor_A = A.as_tensor_ref();
-        auto stages = CreateStages({tensor_A});
-        VLOG(3) << "A shape: " << utils::Join(tensor_A->shape, ", ")
-                << ", output_shapes: " << utils::Join(output_shapes[0], ", ");
-        CHECK_EQ(pack_args.size(), 2U);
-        std::string tensor_name = pack_args[1].operator std::string();
-        ir::Tensor out(ir::_Tensor_::Make(/*name=*/tensor_name,
-                                          /*dtype=*/tensor_A->type(),
-                                          /*shape=*/
-                                          {
-                                              Expr(1),
-                                          },
-                                          /*domain=*/
-                                          {
-                                              Expr(1),
-                                          }));
-        std::vector<CINNValue> res;
-        stages->InsertLazily(out);
-        res.push_back(CINNValue(out));
-        CHECK(!out_type.empty())
-            << "Output type of Cast is empty! Please check.\n";
-        res.push_back(CINNValue(stages));
-        *ret = CINNValuePack{res};
-      });
+  framework::CINNCompute cast_compute([=](lang::Args args,
+                                          lang::RetValue *ret) {
+    PADDLE_ENFORCE(
+        !args.empty(),
+        "The input arguments of Cast compute is empty! Please check.\n");
+    CINNValuePack pack_args = args[0];
+    PADDLE_ENFORCE_GE(
+        pack_args->size(), 1U, "at least 1 input tensors for Cast compute\n");
+    auto stages = CreateStages({});
+
+    std::string tensor_name = pack_args[1].operator std::string();
+    ir::Tensor out(ir::_Tensor_::Make(/*name=*/tensor_name,
+                                      /*dtype=*/common::type_of<int64_t>(),
+                                      /*shape=*/
+                                      {
+                                          Expr(1),
+                                      },
+                                      /*domain=*/
+                                      {
+                                          Expr(1),
+                                      }));
+    std::vector<CINNValue> res;
+    stages->InsertLazily(out);
+    res.push_back(CINNValue(out));
+    PADDLE_ENFORCE(!out_type.empty(),
+                   "Output type of Cast is empty! Please check.\n");
+
+    res.push_back(CINNValue(stages));
+    *ret = CINNValuePack{res};
+  });
 
   auto strategy = std::make_shared<framework::OpStrategy>();
   strategy->AddImpl(cast_compute, lang::PackedFunc(), "strategy.store.x86", 1);
