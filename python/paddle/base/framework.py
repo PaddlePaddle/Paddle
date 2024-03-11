79221d1e9ab487992ec66bcd70c2ee4a0e44a76b
SigureMo <sigure.qaq@gmail.com>
2024-03-11 14:00:31 +0000

    use `use_pir_api` in convert_np_dtype_to_dtype_ to avoid skip numpy dtype conversion cause a cache miss error

--- python/paddle/base/framework.py
+++ python/paddle/base/framework.py
@@ -1262,7 +1262,7 @@ def convert_np_dtype_to_dtype_(np_dtype):
         core.VarDesc.VarType / core.DataType : The data type in Paddle.
 
     """
-    if in_pir_mode():
+    if use_pir_api():
         return pir.core.convert_np_dtype_to_dtype_(np_dtype)
 
     # Convert the data type string to numpy data type.
