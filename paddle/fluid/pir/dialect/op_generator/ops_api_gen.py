ad6b1c1268a2668d5c52d8ee4990a0c07ed3da5f
cyberslack_lee <jeffrey0122@163.com>
2024-03-14 14:03:03 +0800

    【PIR Dist Op Reg No.6】 reg c_scatter (#62369)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- paddle/fluid/pir/dialect/op_generator/ops_api_gen.py
+++ paddle/fluid/pir/dialect/op_generator/ops_api_gen.py
@@ -175,6 +175,7 @@ NO_NEED_GEN_STATIC_ONLY_APIS = [
     'c_reduce_min_',
     'c_reduce_prod',
     'c_reduce_prod_',
+    'c_scatter',
     'push_sparse_v2',
     'push_sparse_v2_',
     'partial_send',
