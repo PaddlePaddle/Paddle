51f76f6cfd16860174f2a42877546e73fca233ef
Charles-hit <wanghao107@baidu.com>
2024-03-15 09:14:08 +0000

    auto recompute support dy2static

--- python/paddle/jit/dy2static/pir_partial_program.py
+++ python/paddle/jit/dy2static/pir_partial_program.py
@@ -390,7 +390,13 @@ class PartialProgramLayerHook:
         ...
 
     def after_append_backward(
-        self, whole_program, src_vars, backward_start_idx
+        self,
+        whole_program,
+        inputs,
+        src_vars,
+        grad_outputs,
+        forward_end_idx,
+        backward_start_idx,
     ):
         ...
 
@@ -457,7 +463,7 @@ class PartialProgramLayer:
 
         # program_id -> list(scope)
         self._scope_cache = {}
-        self._hooker = None
+        self._hooker = []
         self._backend = kwargs.get('backend', None)
         self._grad_var_names = {}
         self._debug_name = None
@@ -530,8 +536,8 @@ class PartialProgramLayer:
             data = np.array(lr_value).astype(convert_dtype(lr_var.dtype))
             lr_var.set_value(data)
 
-    def set_hooker(self, hooker):
-        self._hooker = hooker
+    def add_hooker(self, hooker):
+        self._hooker.append(hooker)
 
     def _get_scope(self, program_id=None, use_scope_cache=False):
         if not use_scope_cache:
@@ -567,8 +573,8 @@ class PartialProgramLayer:
 
             # TODO(xiongkun) who to transfer the pruning program?
             infer_program = self.origin_runable_program.clone()
-            if self._hooker:
-                self._hooker.after_infer(infer_program)
+            for hooker in self._hooker:
+                hooker.after_infer(infer_program)
             infer_program.apply_pir_program_pass(pass_fn)
             return infer_program
         else:
@@ -733,10 +739,8 @@ class PartialProgramLayer:
         program = train_runnable_program.program
         targets = train_runnable_program.out_values
         # TODO(@zhuoge): refine the interface, use runable_program to apply passes.
-        if self._hooker:
-            program, targets = self._hooker.before_append_backward(
-                program, targets
-            )
+        for hooker in self._hooker:
+            program, targets = hooker.before_append_backward(program, targets)
         inputs = train_runnable_program.x_values
         params = train_runnable_program.param_values
         combined_inputs = list(itertools.chain(inputs, params))
@@ -802,13 +806,18 @@ class PartialProgramLayer:
                                 forward_end_idx = idx + 1
                                 break
 
-            if self._hooker:
+            for hooker in self._hooker:
                 (
                     program,
                     forward_end_idx,
                     targets,
-                ) = self._hooker.after_append_backward(
-                    program, targets, forward_end_idx
+                ) = hooker.after_append_backward(
+                    program,
+                    combined_inputs,
+                    targets,
+                    forward_outputs_grads,
+                    forward_end_idx,
+                    forward_end_idx + op_between_forward_and_backward,
                 )
             # TODO: add later
             # self.prepare_gradient_aggregation(
--- python/paddle/jit/dy2static/program_translator.py
+++ python/paddle/jit/dy2static/program_translator.py
@@ -1513,7 +1513,15 @@ class PirPrimHooker(PirPartialProgramLayerHook):
                 return forward_program, dst_vars
             return forward_program, src_vars
 
-    def after_append_backward(self, whole_program, src_vars, forward_end_idx):
+    def after_append_backward(
+        self,
+        whole_program,
+        inputs,
+        src_vars,
+        grad_outputs,
+        forward_end_idx,
+        backward_start_idx,
+    ):
         with backend_guard(self.backend):
             if core._is_fwd_prim_enabled() and len(self.custom_vjps) != 0:
                 backward_length = (
@@ -1540,6 +1548,34 @@ class PirPrimHooker(PirPartialProgramLayerHook):
                     len(infer_program.program.global_block().ops),
                 )
             return
+
+
+class PirAutoRecomputeHooker(PirPartialProgramLayerHook):
+    def __init__(self, recompute_ops=None):
+        self.recompute_ops = recompute_ops
+
+    def before_append_backward(self, forward_program, src_vars):
+        return forward_program, src_vars
+
+    def after_append_backward(
+        self,
+        whole_program,
+        inputs,
+        src_vars,
+        grad_outputs,
+        forward_end_idx,
+        backward_start_idx,
+    ):
+        if core._enable_auto_recompute():
+            whole_program, forward_end_idx = decomposition.auto_recompute(
+                whole_program,
+                inputs,
+                src_vars,
+                grad_outputs,
+                forward_end_idx,
+                backward_start_idx,
+            )
+        return whole_program, forward_end_idx, src_vars
 
 
 class ProgramCache:
@@ -1626,13 +1662,15 @@ class ProgramCache:
         with backend_guard(backend):
             if core._is_fwd_prim_enabled():
                 if use_pir_api():
-                    partial_program.set_hooker(
+                    partial_program.add_hooker(
                         PirPrimHooker(concrete_program.main_program, backend)
                     )
                 else:
                     partial_program.set_hooker(
                         PrimHooker(concrete_program.main_program, backend)
                     )
+        if use_pir_api() and core._enable_auto_recompute():
+            partial_program.add_hooker(PirAutoRecomputeHooker())
         return concrete_program, partial_program
 
     def __getitem__(self, item):
