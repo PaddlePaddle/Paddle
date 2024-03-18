b3d336e20290ac45b02059389e2767d05d1bb86e
ooooo <3164076421@qq.com>
2024-03-18 20:31:17 +0800

    remove check_vale_type

--- python/paddle/base/data_feeder.py
+++ python/paddle/base/data_feeder.py
@@ -260,34 +260,6 @@ def check_shape(
         check_dtype(shape.dtype, 'shape', expected_tensor_dtype, op_name)
 
 
-def check_value_shape(
-    shape,
-    op_name,
-    expected_shape_type=(list, tuple, Value),
-    expected_element_type=(int, Value),
-    expected_tensor_dtype=('int32', 'int64'),
-):
-    # See NOTE [ Why skip dynamic graph check ]
-    if in_dygraph_mode():
-        return
-    check_type(shape, 'shape', expected_shape_type, op_name)
-    if expected_element_type is not None and not isinstance(shape, Value):
-        for item in shape:
-            check_type(item, 'element of shape', expected_element_type, op_name)
-            if expected_tensor_dtype is not None and isinstance(item, Value):
-                check_dtype(
-                    item.dtype,
-                    'element of shape',
-                    expected_tensor_dtype,
-                    op_name,
-                    'If element of shape is Tensor, its data type should be {}'.format(
-                        ', '.join(expected_tensor_dtype)
-                    ),
-                )
-    if expected_tensor_dtype is not None and isinstance(shape, Value):
-        check_dtype(shape.dtype, 'shape', expected_tensor_dtype, op_name)
-
-
 class DataToLoDTensorConverter:
     def __init__(self, place, lod_level, shape, dtype):
         self.place = place
