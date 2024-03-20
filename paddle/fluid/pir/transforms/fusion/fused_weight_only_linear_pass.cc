17fd1274774b733629d79b8304bebfb5a259dd93
Yuanle Liu <yuanlehome@163.com>
2024-03-20 10:36:53 +0800

    [PIR][Inference] Fix fused_weight_only_linear_pass (#62821)
    * fix fused_weight_only_linear_pass
    
    * update
    
    * fix

--- paddle/fluid/pir/transforms/fusion/fused_weight_only_linear_pass.cc
+++ paddle/fluid/pir/transforms/fusion/fused_weight_only_linear_pass.cc
@@ -37,9 +37,20 @@ int getSMVersion() {
   return sm_version;
 }
 
-class FusedWeightOnlyLinearPattern : public paddle::drr::DrrPatternBase {
+class FusedWeightOnlyLinearWithBiasPattern
+    : public paddle::drr::DrrPatternBase {
+ private:
+  bool reverse_;
+
  public:
-  std::string name() const override { return "FusedWeightOnlyLinearPattern"; }
+  explicit FusedWeightOnlyLinearWithBiasPattern(bool reverse)
+      : reverse_(reverse) {}
+
+  std::string name() const override {
+    return "FusedWeightOnlyLinearWithBiasPattern";
+  }
+
+  uint32_t benefit() const override { return 2; }
 
   void operator()(paddle::drr::DrrPatternContext *ctx) const override {
     //
@@ -52,7 +63,10 @@ class FusedWeightOnlyLinearPattern : public paddle::drr::DrrPatternBase {
                 {"transpose_y", src.Attr("matmul_transpose_y")}});
     src.Tensor("matmul_out") = matmul(src.Tensor("x"), src.Tensor("w"));
     const auto &add = src.Op(paddle::dialect::AddOp::name());
-    src.Tensor("add_out") = add(src.Tensor("matmul_out"), src.Tensor("bias"));
+
+    src.Tensor("add_out") =
+        reverse_ ? add(src.Tensor("matmul_out"), src.Tensor("bias"))
+                 : add(src.Tensor("bias"), src.Tensor("matmul_out"));
 
     //
     // Constraints.
@@ -70,7 +84,7 @@ class FusedWeightOnlyLinearPattern : public paddle::drr::DrrPatternBase {
           auto x_dims = pir::GetShapeFromValue(match_ctx.Tensor("x"));
           auto bias_dims = pir::GetShapeFromValue(match_ctx.Tensor("bias"));
           if (!(w_dims.size() == 2 && x_dims.size() >= 2 &&
-                bias_dims.size() == 1)) {
+                bias_dims.size() == x_dims.size())) {
             return false;
           }
 
@@ -81,7 +95,7 @@ class FusedWeightOnlyLinearPattern : public paddle::drr::DrrPatternBase {
               !w_dtype.isa<pir::BFloat16Type>())
             return false;
 
-          if (x_dims.at(x_dims.size() - 1) != w_dims.at(1)) return false;
+          if (x_dims.at(x_dims.size() - 1) != w_dims.at(0)) return false;
 
           return true;
         });
@@ -112,6 +126,81 @@ class FusedWeightOnlyLinearPattern : public paddle::drr::DrrPatternBase {
   }
 };
 
+class FusedWeightOnlyLinearNoBiasPattern : public paddle::drr::DrrPatternBase {
+ public:
+  std::string name() const override {
+    return "FusedWeightOnlyLinearNoBiasPattern";
+  }
+
+  uint32_t benefit() const override { return 1; }
+
+  void operator()(paddle::drr::DrrPatternContext *ctx) const override {
+    //
+    // Source Pattern.
+    //
+    paddle::drr::SourcePattern src = ctx->SourcePattern();
+    const auto &matmul =
+        src.Op(paddle::dialect::MatmulOp::name(),
+               {{"transpose_x", src.Attr("matmul_transpose_x")},
+                {"transpose_y", src.Attr("matmul_transpose_y")}});
+    src.Tensor("matmul_out") = matmul(src.Tensor("x"), src.Tensor("w"));
+
+    //
+    // Constraints.
+    //
+    src.RequireNativeCall(
+        [](const paddle::drr::MatchContext &match_ctx) -> bool {
+          if (!pir::ValueIsPersistable(match_ctx.Tensor("w"))) {
+            return false;
+          }
+          bool matmul_trans_x = match_ctx.Attr<bool>("matmul_transpose_x");
+          bool matmul_trans_y = match_ctx.Attr<bool>("matmul_transpose_y");
+          if (matmul_trans_x || matmul_trans_y) return false;
+
+          auto w_dims = pir::GetShapeFromValue(match_ctx.Tensor("w"));
+          auto x_dims = pir::GetShapeFromValue(match_ctx.Tensor("x"));
+          if (!(w_dims.size() == 2 && x_dims.size() >= 2)) {
+            return false;
+          }
+
+          if (w_dims.at(0) % 64 != 0 || w_dims.at(1) % 16 != 0) return false;
+
+          auto w_dtype = pir::GetDataTypeFromValue(match_ctx.Tensor("w"));
+          if (!w_dtype.isa<pir::Float16Type>() &&
+              !w_dtype.isa<pir::BFloat16Type>())
+            return false;
+
+          if (x_dims.at(x_dims.size() - 1) != w_dims.at(0)) return false;
+
+          return true;
+        });
+    //
+    // Result Pattern.
+    //
+    paddle::drr::ResultPattern res = src.ResultPattern();
+
+    const auto &weight_quantize =
+        res.Op(paddle::dialect::WeightQuantizeOp::name(),
+               {{"algo", res.StrAttr("weight_only_int8")},
+                {"arch", res.Int32Attr(getSMVersion())},
+                {"group_size", res.Int32Attr(-1)}});
+    weight_quantize({&res.Tensor("w")},
+                    {&res.Tensor("quanted_weight_tensor"),
+                     &res.Tensor("weight_scale_tensor")});
+
+    const auto &weight_only_linear =
+        res.Op(paddle::dialect::WeightOnlyLinearOp::name(),
+               {{"weight_dtype", res.StrAttr("int8")},
+                {"arch", res.Int32Attr(getSMVersion())},
+                {"group_size", res.Int32Attr(-1)}});
+    weight_only_linear({&res.Tensor("x"),
+                        &res.Tensor("quanted_weight_tensor"),
+                        &res.InputNoneTensor(),
+                        &res.Tensor("weight_scale_tensor")},
+                       {&res.Tensor("matmul_out")});
+  }
+};
+
 class FusedWeightOnlyLinearPass : public pir::PatternRewritePass {
  public:
   FusedWeightOnlyLinearPass()
@@ -119,8 +208,23 @@ class FusedWeightOnlyLinearPass : public pir::PatternRewritePass {
 
   pir::RewritePatternSet InitializePatterns(pir::IrContext *context) override {
     pir::RewritePatternSet ps(context);
-    ps.Add(paddle::drr::Create<FusedWeightOnlyLinearPattern>(context));
+    ps.Add(paddle::drr::Create<FusedWeightOnlyLinearWithBiasPattern>(context,
+                                                                     true));
+    ps.Add(paddle::drr::Create<FusedWeightOnlyLinearWithBiasPattern>(context,
+                                                                     false));
+    ps.Add(paddle::drr::Create<FusedWeightOnlyLinearNoBiasPattern>(context));
     return ps;
+  }
+
+  pir::GreedyRewriteConfig InitializeConfig() override {
+    pir::GreedyRewriteConfig config;
+
+    // NOTE(liuyuanle): Ensure that WithBiasPattern is executed before
+    // NoBiasPattern.
+    config.use_top_down_traversal = false;
+
+    config.max_iterations = 10;
+    return config;
   }
 
   bool CanApplyOn(pir::Operation *op) const override {
