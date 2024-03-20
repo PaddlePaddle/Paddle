a3b40cf09e501ccf925ec3202dda17436a8527cd
zhanglirong1999 <1695074375@qq.com>
2024-03-20 14:40:10 +0800

    change opt_level to 3

--- paddle/fluid/pir/transforms/onednn/conv_elementwise_add_mkldnn_fuse_pass.cc
+++ paddle/fluid/pir/transforms/onednn/conv_elementwise_add_mkldnn_fuse_pass.cc
@@ -365,7 +365,7 @@ class FusedConvBiasElementwiseAddAsYPattern
 class ConvElementwiseAddFusePass : public pir::PatternRewritePass {
  public:
   ConvElementwiseAddFusePass()
-      : pir::PatternRewritePass("conv_elementwise_add_mkldnn_fuse_pass", 2) {}
+      : pir::PatternRewritePass("conv_elementwise_add_mkldnn_fuse_pass", 3) {}
 
   pir::RewritePatternSet InitializePatterns(pir::IrContext *context) override {
     pir::RewritePatternSet ps(context);
