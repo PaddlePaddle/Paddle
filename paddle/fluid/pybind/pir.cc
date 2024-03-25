4768ff67ee11816405dd4d5b1979d510279bbef5
Zhang,Lirong <56445728+zhanglirong1999@users.noreply.github.com>
2024-03-25 11:25:01 +0800

    [OneDNN][PIR] conv elementwise add mkldnn fuse pass (#62713)
    * First commit of conv add pass
    
    * Fix some bug
    
    * return ps
    
    * fix header
    
    * commit conv + bias + add pattern
    
    * remove persistable
    
    * Add None tensor to match pattern
    
    * format file
    
    * add graph in test case
    
    * fix graph style
    
    * add r for comment style
    
    * change opt_level to 3
    
    * delete useless pass pattern
    
    * Set fused_conv2d attribut from source

--- paddle/fluid/pybind/pir.cc
+++ paddle/fluid/pybind/pir.cc
@@ -96,6 +96,7 @@
 
 #ifdef PADDLE_WITH_DNNL
 #include "paddle/fluid/pir/transforms/onednn/batch_norm_act_fuse_pass.h"
+#include "paddle/fluid/pir/transforms/onednn/conv_elementwise_add_mkldnn_fuse_pass.h"
 #include "paddle/fluid/pir/transforms/onednn/matmul_elementwise_add_fuse_pass.h"
 #endif
 
@@ -154,6 +155,7 @@ USE_PIR_PASS(fused_dot_product_attention_pass);
 #ifdef PADDLE_WITH_DNNL
 USE_PIR_PASS(batch_norm_act_fuse_pass);
 USE_PIR_PASS(matmul_elementwise_add_fuse_pass);
+USE_PIR_PASS(conv_elementwise_add_mkldnn_fuse_pass);
 #endif
 
 COMMON_DECLARE_bool(print_ir);
