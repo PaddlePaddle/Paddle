e73dd27f8f27a31a7c6223fee35931739186fafe
yxy <ltyxy@buaa.edu.cn>
2024-03-25 07:33:01 +0000

    add test for RepeatInterleaveOp

--- test/ir/pir/cinn/symbolic/test_infer_sym_shape_nullary_op.py
+++ test/ir/pir/cinn/symbolic/test_infer_sym_shape_nullary_op.py
@@ -113,6 +113,39 @@ class GaussianOpInferSymbolicShapeTest(TestBase):
         return True
 
 
+class RepeatInterleaveNet(paddle.nn.Layer):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        out = paddle.repeat_interleave(x, 2, axis=0)
+        out = paddle.repeat_interleave(x, 2, axis=1)
+        out = paddle.repeat_interleave(x, 2, axis=-1)
+        out = paddle.repeat_interleave(x, 2, axis=-2)
+        return out
+
+
+class RepeatInterleaveOpInferSymbolicShapeTest(TestBase):
+    def prepare_data(self):
+        self.expected = [
+            'shape[Mul(S0, 2), S1], data[NULL]',
+            'shape[S0, Mul(S1, 2)], data[NULL]',
+            'shape[S0, S1, Mul(S2, 2)], data[NULL]',
+            'shape[S0, Mul(S1, 2), S2], data[NULL]',
+        ]
+
+    def test_eval_symbolic(self):
+        net = RepeatInterleaveNet()
+        x_spec = InputSpec(shape=[None, None, None], dtype='float32')
+        input_spec = [x_spec]
+        net = apply_to_static(net, False, input_spec)
+        net.eval()
+        check_infer_results(
+            net, input_spec, 'pd_op.repeat_interleave', self.expected
+        )
+        return True
+
+
 class UniformNet(paddle.nn.Layer):
     def __init__(self):
         super().__init__()
