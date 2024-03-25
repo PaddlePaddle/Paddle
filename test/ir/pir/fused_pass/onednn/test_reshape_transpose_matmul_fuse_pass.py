871bfe54de7d6b5da44965492d7f6a5670f4d16c
Xinyi1, Li <xinyi1.li@intel.com>
2024-03-25 16:31:33 +0800

    add new pattern for matmul_elementwise_add_fuse_pass

--- test/ir/pir/fused_pass/onednn/test_reshape_transpose_matmul_fuse_pass.py
+++ test/ir/pir/fused_pass/onednn/test_reshape_transpose_matmul_fuse_pass.py
@@ -173,5 +173,62 @@ class TestReshapeTranspoeMatmulFusePatternCase3(PassTest):
         self.check_pass_correct()
 
 
+@unittest.skipIf(
+    not paddle.base.core.is_compiled_with_mkldnn(),
+    "Test case only for OneDNN pass.",
+)
+class TestReshapeTransposeMatmulAddFusePattern(PassTest):
+    def is_program_valid(self, program=None):
+        return True
+
+    def build_ir_program(self):
+        with paddle.pir_utils.IrGuard():
+            main_prog = paddle.static.Program()
+            start_prog = paddle.static.Program()
+            with paddle.pir.core.program_guard(main_prog, start_prog):
+                x = paddle.static.data(
+                    name='x', shape=[5, 5, 5, 5], dtype='float32'
+                )
+                y = paddle.static.data(
+                    name='y', shape=[5, 5, 5, 5], dtype='float32'
+                )
+                bias = paddle.static.create_parameter(
+                    name="bias", shape=[1], dtype='float32'
+                )
+
+                reshape_out = paddle.reshape(y, [0, 0, 0, 0])
+                transpose_out = paddle.transpose(reshape_out, perm=[0, 2, 3, 1])
+                matmul_out = paddle.matmul(x, transpose_out)
+                out = paddle.add(matmul_out, bias)
+                out = paddle.assign(out)
+                self.pass_list = [
+                    'reshape_transpose_matmul_fuse_pass',
+                    'matmul_elementwise_add_fuse_pass',
+                ]
+                self.feeds = {
+                    "x": np.random.random((5, 5, 5, 5)).astype("float32"),
+                    "y": np.random.random((5, 5, 5, 5)).astype("float32"),
+                    "bias": np.random.random(1).astype("float32"),
+                }
+                self.fetch_list = [out]
+                self.valid_op_map = {
+                    "onednn_op.fused_matmul": 1,
+                    "pd_op.reshape": 0,
+                    "pd_op.transpose": 0,
+                    "pd_op.matmul": 0,
+                    "pd_op.add": 0,
+                }
+                return [main_prog, start_prog]
+
+    def sample_program(self):
+        yield self.build_ir_program(), False
+
+    def setUp(self):
+        self.places.append(paddle.CPUPlace())
+
+    def test_check_output(self):
+        self.check_pass_correct()
+
+
 if __name__ == "__main__":
     unittest.main()
