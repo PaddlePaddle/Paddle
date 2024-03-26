aa68da2413abea40ea30cc8ff31114071a0a9a43
yinfan98 <32722923+yinfan98@users.noreply.github.com>
2024-03-26 23:12:01 +0800

    Update passes.h

--- paddle/fluid/pir/transforms/passes.h
+++ paddle/fluid/pir/transforms/passes.h
@@ -37,6 +37,7 @@ USE_PIR_PASS(conv2d_add_fuse_pass);
 USE_PIR_PASS(conv2d_add_act_fuse_pass);
 USE_PIR_PASS(embedding_eltwise_layernorm_fuse_pass);
 USE_PIR_PASS(fused_dot_product_attention_pass);
+USE_PIR_PASS(flash_attn_fuse_pass);
 
 #ifdef PADDLE_WITH_DNNL
 USE_PIR_PASS(batch_norm_act_fuse_pass);
