c7984068cd4292f7c68ecfb45cc6e610b2fc05c6
yinfan98 <32722923+yinfan98@users.noreply.github.com>
2024-03-30 21:27:31 +0800

    Update fused_flash_attention_pass.cc

--- paddle/fluid/pir/transforms/gpu/fused_flash_attention_pass.cc
+++ paddle/fluid/pir/transforms/gpu/fused_flash_attention_pass.cc
@@ -522,6 +522,217 @@ class FlashAttnPatternOutscaleNoCast : public paddle::drr::DrrPatternBase {
   }
 };
 
+// TransposeSliceFlashAttn
+class TransposeSliceFlashAttnPattern : public paddle::drr::DrrPatternBase {
+ public:
+  std::string name() const override { return "TransposeSliceFlashAttnPattern"; }
+
+  void operator()(paddle::drr::DrrPatternContext *ctx) const override {
+    
+    paddle::drr::SourcePattern src = ctx->SourcePattern();
+    // transpose
+    const auto &transpose_qkv = src.Op("pd_op.transpose",
+                                       {{"perm", src.Attr("perm")}});
+    src.Tensor("qkv_transpose") = transpose_qkv(src.Tensor("qkv"));
+    // slice q -> [b, head, s, head_dim]
+    const auto &slice_q = src.Op(paddle::dialect::SliceOp::name(),
+                                {{"axes", src.Attr("axes_q")},
+                                 {"infer_flags", src.Attr("infer_flags_q")},
+                                 {"decrease_axis", src.Attr("decrease_axis_q")}});
+    const auto &full_int_array_q1 = src.Op("pd_op.full_int_array");
+    const auto &full_int_array_q2 = src.Op("pd_op.full_int_array");
+    src.Tensor("q") = slice_q(src.Tensor("qkv_transpose"), full_int_array_q1(), full_int_array_q2());
+    // slice k -> [b, head, s, head_dim]
+    const auto &slice_k = src.Op(paddle::dialect::SliceOp::name(),
+                                {{"axes", src.Attr("axes_k")},
+                                 {"infer_flags", src.Attr("infer_flags_k")},
+                                 {"decrease_axis", src.Attr("decrease_axis_k")}});
+    const auto &full_int_array_k1 = src.Op("pd_op.full_int_array");
+    const auto &full_int_array_k2 = src.Op("pd_op.full_int_array");
+    src.Tensor("k") = slice_k(src.Tensor("qkv_transpose"), full_int_array_k1(), full_int_array_k2());
+    // slice v -> [b, head, s, head_dim]
+    const auto &slice_v = src.Op(paddle::dialect::SliceOp::name(),
+                                {{"axes", src.Attr("axes_v")},
+                                 {"infer_flags", src.Attr("infer_flags_v")},
+                                 {"decrease_axis", src.Attr("decrease_axis_v")}});
+    const auto &full_int_array_v1 = src.Op("pd_op.full_int_array");
+    const auto &full_int_array_v2 = src.Op("pd_op.full_int_array");
+    src.Tensor("v") = slice_v(src.Tensor("qkv_transpose"), full_int_array_v1(), full_int_array_v2());
+
+    // k[b, head, s, head_dim] -> transpose -> k[b, head, head_dim, s]
+    const auto &transpose_k = src.Op("pd_op.transpose");
+    src.Tensor("k_transpose_out") = transpose_k(src.Tensor("k"));
+    // qk
+    const auto &qk_matmul =
+        src.Op("pd_op.matmul",
+               {{"transpose_x", src.Attr("matmul_qk_transpose_x")},
+                {"transpose_y", src.Attr("matmul_qk_transpose_y")}});
+    src.Tensor("qk_out") =
+        qk_matmul(src.Tensor("q"), src.Tensor("k_transpose_out"));
+    //scale
+    const auto &scale_out = src.Op("pd_op.scale");
+    const auto &full_scale =
+        src.Op("pd_op.full", {{"value", src.Attr("scale_out_value")}});
+    src.Tensor("qk_scale_out") =
+        scale_out(src.Tensor("qk_out"), full_scale());
+
+    // mask
+    const auto &mask_add = src.Op("pd_op.add");
+    src.Tensor("mask_add_out") =
+        mask_add(src.Tensor("qk_scale_out"), src.Tensor("mask"));
+
+    // softmax
+    const auto &softmax =
+        src.Op("pd_op.softmax", {{"axis", src.Attr("softmax_axis")}});
+    src.Tensor("softmax_out") = softmax(src.Tensor("softmax_cast1_out"));
+    // o
+    const auto &context_matmul =
+        src.Op("pd_op.matmul",
+               {{"transpose_x", src.Attr("context_matmul_transpose_x")},
+                {"transpose_y", src.Attr("context_matmul_transpose_y")}});
+    src.Tensor("context_matmul_out") = context_matmul(
+        src.Tensor("softmax_out"), src.Tensor("v"));
+    // [b, head, s, head_dim] -> [b, s, head, head_dim]
+    const auto &o_transpose = src.Op("pd_op.transpose");
+    src.Tensor("out") = o_transpose(src.Tensor("context_matmul_out"));
+
+    // Constraints
+    src.RequireNativeCall(
+        [](const paddle::drr::MatchContext &match_ctx) -> bool {
+          // softmax
+          const auto &softmax_axis = match_ctx.Attr<int>("softmax_axis");
+          if (softmax_axis != -1 && softmax_axis != 3) return false;
+          // matmul transpose
+          bool matmul_qk_transpose_x =
+              match_ctx.Attr<bool>("matmul_qk_transpose_x");
+          bool matmul_qk_transpose_y =
+              match_ctx.Attr<bool>("matmul_qk_transpose_y");
+          if (matmul_qk_transpose_x || matmul_qk_transpose_y) return false;
+
+          bool matmul_o_transpose_x =
+              match_ctx.Attr<bool>("context_matmul_transpose_x");
+          bool matmul_o_transpose_y =
+              match_ctx.Attr<bool>("context_matmul_transpose_y");
+          if (matmul_o_transpose_x || matmul_o_transpose_y) return false;
+          // tensor shape
+          auto q =
+              pir::GetShapeFromValue(match_ctx.Tensor("q"));
+          auto k =
+              pir::GetShapeFromValue(match_ctx.Tensor("k"));
+          auto v =
+              pir::GetShapeFromValue(match_ctx.Tensor("v"));
+          if (q.size() != 4 || k.size() != 4 || v.size() != 4 ||
+              !(q.at(0) == k.at(0) && k.at(0) == v.at(0)) ||
+              !(q.at(1) == k.at(1) && k.at(1) == v.at(1)) ||
+              !(q.at(3) == k.at(3) && k.at(3) == v.at(3))) {
+            return false;
+          }
+          // add shape
+          auto mask_add = pir::GetShapeFromValue(match_ctx.Tensor("mask"));
+          if (mask_add.size() != 4) {
+            return false;
+          }
+
+          return true;
+    });
+
+    //
+    // Result Pattern.
+    //
+    paddle::drr::ResultPattern res = src.ResultPattern();
+    // flash_attn impl
+    const auto &perm_res = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int> {
+          auto perm = match_ctx.Attr<std::vector<int>>("perm");
+          return perm;
+        });
+    // transpose
+    const auto &transpose_qkv_res = res.Op("pd_op.transpose",
+                                           {{"perm", perm_res}});
+    res.Tensor("qkv_transpose") = transpose_qkv_res(res.Tensor("qkv"));
+    // slice q -> [b, head, s, head_dim]
+    const auto &axes_q_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto axes_q = match_ctx.Attr<std::vector<int64_t>>("axes_q");
+          return axes_q;
+        });
+    const auto &infer_flags_q_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto infer_flags_q = match_ctx.Attr<std::vector<int64_t>>("infer_flags_q");
+          return infer_flags_q;
+        });
+    const auto &decrease_axis_q_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto decrease_axis_q = match_ctx.Attr<std::vector<int64_t>>("decrease_axis_q");
+          return decrease_axis_q;
+        });
+    const auto &slice_q_res = res.Op(paddle::dialect::SliceOp::name(),
+                                   {{"axes", axes_q_out},
+                                    {"infer_flags", infer_flags_q_out},
+                                    {"decrease_axis", decrease_axis_q_out}});
+    res.Tensor("q") = slice_q_res(res.Tensor("qkv_transpose"), res.OutputNoneTensor(), res.OutputNoneTensor());
+    // slice k -> [b, head, s, head_dim]
+    const auto &axes_k_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto axes_k = match_ctx.Attr<std::vector<int64_t>>("axes_k");
+          return axes_k;
+        });
+    const auto &infer_flags_k_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto infer_flags_k = match_ctx.Attr<std::vector<int64_t>>("infer_flags_k");
+          return infer_flags_k;
+        });
+    const auto &decrease_axis_k_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto decrease_axis_k = match_ctx.Attr<std::vector<int64_t>>("decrease_axis_k");
+          return decrease_axis_k;
+        });
+    printf("here");
+    const auto &slice_k_res = res.Op(paddle::dialect::SliceOp::name(),
+                                   {{"axes", axes_k_out},
+                                    {"infer_flags", infer_flags_k_out},
+                                    {"decrease_axis", decrease_axis_k_out}});
+    res.Tensor("k") = slice_k_res(res.Tensor("qkv_transpose"), res.OutputNoneTensor(), res.OutputNoneTensor());
+    // slice v -> [b, head, s, head_dim]
+    const auto &axes_v_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto axes_v = match_ctx.Attr<std::vector<int64_t>>("axes_v");
+          return axes_v;
+        });
+    const auto &infer_flags_v_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto infer_flags_v = match_ctx.Attr<std::vector<int64_t>>("infer_flags_v");
+          return infer_flags_v;
+        });
+    const auto &decrease_axis_v_out = res.ComputeAttr(
+        [](const paddle::drr::MatchContext &match_ctx) -> std::vector<int64_t> {
+          auto decrease_axis_v = match_ctx.Attr<std::vector<int64_t>>("decrease_axis_v");
+          return decrease_axis_v;
+        });
+    const auto &slice_v_res = res.Op(paddle::dialect::SliceOp::name(),
+                                   {{"axes", axes_v_out},
+                                    {"infer_flags", infer_flags_v_out},
+                                    {"decrease_axis", decrease_axis_v_out}});
+    res.Tensor("v") = slice_v_res(res.Tensor("qkv_transpose"), res.OutputNoneTensor(), res.OutputNoneTensor());
+    printf("here");
+    const auto &flash_attn = res.Op("pd_op.flash_attn",
+                                    {{{"dropout", res.Float32Attr(0.0)},
+                                      {"causal", res.BoolAttr(false)},
+                                      {"return_softmax", res.BoolAttr(false)},
+                                      {"is_test", res.BoolAttr(false)},
+                                      {"rng_name", res.StrAttr("")}}});
+    flash_attn({&res.Tensor("q"),
+                &res.Tensor("k"),
+                &res.Tensor("v"),
+                &res.InputNoneTensor(),
+                &res.Tensor("mask")},
+               {&res.Tensor("out"),
+                &res.Tensor("softmax"),
+                &res.Tensor("softmax_lse"),
+                &res.Tensor("seed_offset")});
+  }
+};
+
 class FusedFlashAttnPass : public pir::PatternRewritePass {
  public:
   FusedFlashAttnPass() : pir::PatternRewritePass("fused_flash_attn_pass", 2) {}
@@ -533,7 +744,7 @@ class FusedFlashAttnPass : public pir::PatternRewritePass {
     ps.Add(paddle::drr::Create<FlashAttnPatternQscaleNoCast>(context));
     ps.Add(paddle::drr::Create<FlashAttnPatternOutscaleCast>(context));
     ps.Add(paddle::drr::Create<FlashAttnPatternOutscaleNoCast>(context));
-
+    ps.Add(paddle::drr::Create<TransposeSliceFlashAttnPattern>(context);
     return ps;
   }
 };
