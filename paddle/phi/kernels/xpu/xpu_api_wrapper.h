66264e4345e835fa5d073f27a508d33394c5e758
tianshuo78520a <707759223@qq.com>
2024-04-07 08:26:29 +0000

    Fix xpu build

--- paddle/phi/kernels/xpu/xpu_api_wrapper.h
+++ paddle/phi/kernels/xpu/xpu_api_wrapper.h
@@ -41,7 +41,6 @@ enum XPUFCCalcType {
 
 template <typename T>
 XPUFCCalcType FCCalcType() {
-  if (std::getenv("XPU_PADDLE_FC_FLOAT16") != nullptr &&
   const char* xpu_paddle_fc_float16 = std::getenv("XPU_PADDLE_FC_FLOAT16");
   if (xpu_paddle_fc_float16 != nullptr &&
       (std::is_same<phi::dtype::float16, T>::value ||
