49c7cc7f1225f27853c0b10f624c9e0202090fe8
linkk08 <124329195+linkk08@users.noreply.github.com>
2024-04-07 14:14:24 +0800

    add group_norm_silu_xpu_fuse  (#62689)

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -538,6 +538,7 @@ XpuPassStrategy::XpuPassStrategy() : PassStrategy({}) {
       "cast_embedding_trans_ids_to_int32_pass",
       "delete_elementwise_mul_op_pass",
       "generate_sequence_xpu_fuse_pass",
+      "group_norm_silu_xpu_fuse_pass",
       "embedding_with_eltwise_add_xpu_fuse_pass",
       "qk_qkv_attention_xpu_fuse_pass",
       "multi_encoder_xpu_fuse_pass",
