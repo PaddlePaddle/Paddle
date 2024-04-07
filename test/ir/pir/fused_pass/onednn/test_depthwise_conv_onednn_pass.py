259766356772ee7041faa07158c3222c517ced49
zhanglirong1999 <1695074375@qq.com>
2024-04-07 13:39:46 +0800

    delete skipif

--- test/ir/pir/fused_pass/onednn/test_depthwise_conv_onednn_pass.py
+++ test/ir/pir/fused_pass/onednn/test_depthwise_conv_onednn_pass.py
@@ -21,10 +21,6 @@ import paddle
 paddle.enable_static()
 
 
-@unittest.skipIf(
-    not paddle.base.core.is_compiled_with_mkldnn(),
-    "Test case only for OneDNN pass.",
-)
 class TestConv2dAddFusePass(PassTest):
     def is_program_valid(self, program=None):
         return True
