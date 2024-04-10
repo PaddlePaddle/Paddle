f2aa2cea06ce6d66470ec06dcb9ca84da8032fd4
chenzhiyang <1792266893@qq.com>
2024-04-10 06:38:37 +0000

    refine merge

--- paddle/fluid/pir/serialize_deserialize/src/save_load_parameters.cc
+++ paddle/fluid/pir/serialize_deserialize/src/save_load_parameters.cc
@@ -9,7 +9,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#include "paddle/fluid/pir/serialize_deserialize/save_load_parameters.h"
+#include "paddle/fluid/pir/serialize_deserialize/include/save_load_parameters.h"
 
 #include <cstdint>
 #include <fstream>
