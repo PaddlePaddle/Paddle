b7714e233faf489e1322e9ab9e9b2ec81b6c58ff
Zhang,Lirong <56445728+zhanglirong1999@users.noreply.github.com>
2024-04-10 19:45:56 +0800

    [OneDNN][PIR] MKLDNN Conv activation fusion pass (#63145)
    * rebase branch
    
    * add , for CI failed
    
    * update pass
    
    * format
    
    * rename
    
    * regresh ut and pass code
    
    * style code

--- paddle/fluid/pir/transforms/passes.h
+++ paddle/fluid/pir/transforms/passes.h
@@ -53,6 +53,7 @@ USE_PIR_PASS(matmul_transpose_reshape_fuse_pass);
 USE_PIR_PASS(matmul_elementwise_add_fuse_pass);
 USE_PIR_PASS(matmul_activation_fuse_pass);
 USE_PIR_PASS(conv_elementwise_add_onednn_fuse_pass);
+USE_PIR_PASS(conv_activation_onednn_fuse_pass);
 #endif
 
 #ifdef PADDLE_WITH_XPU
