6890d2e2f1ec190bac4f4106dd2b217fd179b03e
0x45f <wangzhen45@baidu.com>
2024-04-11 08:15:49 +0000

    Refine code

--- paddle/fluid/pir/transforms/general/remove_shadow_feed_pass.cc
+++ paddle/fluid/pir/transforms/general/remove_shadow_feed_pass.cc
@@ -32,14 +32,10 @@ std::unique_ptr<paddle::dialect::OpYamlInfoParser> GetOpYamlInfoParser(
     pir::Operation *op) {
   std::unique_ptr<paddle::dialect::OpYamlInfoParser> op_info_parser(nullptr);
   std::string op_name = op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name();
-  VLOG(1) << "GetOpYamlInfoParser op_name: " << op_name;
   auto op_info = pir::IrContext::Instance()->GetRegisteredOpInfo(op_name);
-  VLOG(1) << "GetOpYamlInfoParser HasInterface: "
-          << op_info.HasInterface<paddle::dialect::OpYamlInfoInterface>();
   if (op_info.HasInterface<paddle::dialect::OpYamlInfoInterface>()) {
     auto impl =
         op_info.GetInterfaceImpl<paddle::dialect::OpYamlInfoInterface>();
-    VLOG(1) << "GetOpYamlInfoParser impl: " << impl;
     auto op_info_tuple = impl->get_op_info_(op_name);
     op_info_parser = std::make_unique<paddle::dialect::OpYamlInfoParser>(
         op_info_tuple, paddle::dialect::IsLegacyOp(op_name));
@@ -62,34 +58,99 @@ class RemoveShadowFeedPattern : public pir::RewritePattern {
         scope_(scope) {
     for (auto &[name, value] : block->kwargs()) {
       kwargs_map_[value] = name;
-      VLOG(1) << "RemoveShadowFeedPattern name: " << name;
     }
   }
 
-  bool MatchAndRewrite(pir::Operation *op,
-                       pir::PatternRewriter &rewriter) const override {
-    VLOG(1) << "RemoveShadowFeedPattern isa PhiKernelOp: "
-            << op->isa<paddle::dialect::PhiKernelOp>();
-    if (!op->isa<paddle::dialect::PhiKernelOp>()) {
-      VLOG(1) << "RemoveShadowFeedPattern op name: " << op->name();
-    }
+  // bool MatchAndRewrite(pir::Operation *op,
+  //                      pir::PatternRewriter &rewriter) const override {
+  //   VLOG(1) << "RemoveShadowFeedPattern isa PhiKernelOp: "
+  //           << op->isa<paddle::dialect::PhiKernelOp>();
+  //   if (!op->isa<paddle::dialect::PhiKernelOp>()) {
+  //     VLOG(1) << "RemoveShadowFeedPattern op name: " << op->name();
+  //   }
+  //   if (op->isa<paddle::dialect::PhiKernelOp>() &&
+  //       op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name() ==
+  //           "pd_op.shadow_feed") {
+  //     VLOG(1) << "RemoveShadowFeedPattern find pd_op.shadow_feed";
+  //     auto in = op->operand_source(0);
+  //     if (!kwargs_map_.count(in)) {
+  //       VLOG(1) << "RemoveShadowFeedPattern kwargs_map_ not contain var!";
+  //       return false;
+  //     }
+  //     auto in_name = kwargs_map_.at(in);
+  //     VLOG(1) << "RemoveShadowFeedPattern in_name: " << in_name;
+  //     auto *var = scope_->FindVar(in_name);
+  //     phi::Place var_place;
+  //     if (var->IsType<phi::DenseTensor>()) {
+  //       auto &tensor = var->Get<phi::DenseTensor>();
+  //       VLOG(1) << "RemoveShadowFeedPattern tensor.initialized(): "
+  //               << tensor.initialized();
+  //       if (tensor.initialized()) {
+  //         var_place = tensor.place();
+  //       } else {
+  //         var_place = place_;
+  //       }
+  //     } else {
+  //       PADDLE_THROW(paddle::platform::errors::InvalidArgument(
+  //           "RemoveShadowFeedPattern only support output "
+  //           "variable of type DenseTensor, SelectedRows or
+  //           VariableRefArray"));
+  //     }
+  //     if (var_place == place_) {
+  //       VLOG(1) << "RemoveShadowFeedPattern remove pd_op.shadow_feed";
+  //       auto out = op->result(0);
+  //       in.set_type(out.type());
+  //       rewriter.ReplaceAllUsesWith(out, in);
+  //       rewriter.EraseOp(op);
+  //       return true;
+  //     }
+  //     return false;
+  //   } else if (op->isa<paddle::dialect::PhiKernelOp>() &&
+  //              op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name() !=
+  //                  "pd_op.shadow_feed") {
+  //     VLOG(1) << "auto op_info_parser = GetOpYamlInfoParser(op)";
+  //     auto op_info_parser = GetOpYamlInfoParser(op);
+  //     VLOG(1) << "finish auto op_info_parser = GetOpYamlInfoParser(op), "
+  //                "op->num_operands(): "
+  //             << op->num_operands();
+  //     for (size_t i = 0; i < op->num_operands(); ++i) {
+  //       auto define_op = op->operand_source(i).defining_op();
+  //       VLOG(1) << "define_op: " << define_op;
+  //       VLOG(1) << "op_info_parser->IsTensorAttribute(i): "
+  //               << op_info_parser->IsTensorAttribute(i);
+
+  //       if (op_info_parser->IsTensorAttribute(i) && define_op &&
+  //           define_op->isa<paddle::dialect::PhiKernelOp>() &&
+  //           define_op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name() ==
+  //               "pd_op.shadow_feed") {
+  //         auto in = define_op->operand_source(0);
+  //         auto out = define_op->result(0);
+  //         rewriter.ReplaceAllUsesWith(out, in);
+  //         rewriter.EraseOp(define_op);
+  //         VLOG(1) << "RemoveShadowFeedPattern remove IsTensorAttribute "
+  //                    "shadow_feed: "
+  //                 << op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name();
+  //         return true;
+  //       }
+  //     }
+  //     return false;
+  //   }
+  //   return false;
+  // }
+
+  bool IsSamePlaceShadowFeed(pir::Operation *op) const {
     if (op->isa<paddle::dialect::PhiKernelOp>() &&
         op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name() ==
             "pd_op.shadow_feed") {
-      VLOG(1) << "RemoveShadowFeedPattern find pd_op.shadow_feed";
       auto in = op->operand_source(0);
       if (!kwargs_map_.count(in)) {
-        VLOG(1) << "RemoveShadowFeedPattern kwargs_map_ not contain var!";
         return false;
       }
       auto in_name = kwargs_map_.at(in);
-      VLOG(1) << "RemoveShadowFeedPattern in_name: " << in_name;
       auto *var = scope_->FindVar(in_name);
       phi::Place var_place;
       if (var->IsType<phi::DenseTensor>()) {
         auto &tensor = var->Get<phi::DenseTensor>();
-        VLOG(1) << "RemoveShadowFeedPattern tensor.initialized(): "
-                << tensor.initialized();
         if (tensor.initialized()) {
           var_place = tensor.place();
         } else {
@@ -100,109 +161,44 @@ class RemoveShadowFeedPattern : public pir::RewritePattern {
             "RemoveShadowFeedPattern only support output "
             "variable of type DenseTensor, SelectedRows or VariableRefArray"));
       }
-      if (var_place == place_) {
-        VLOG(1) << "RemoveShadowFeedPattern remove pd_op.shadow_feed";
-        auto out = op->result(0);
-        in.set_type(out.type());
-        rewriter.ReplaceAllUsesWith(out, in);
-        rewriter.EraseOp(op);
-        return true;
-      }
-      return false;
-    } else if (op->isa<paddle::dialect::PhiKernelOp>() &&
-               op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name() !=
-                   "pd_op.shadow_feed") {
-      VLOG(1) << "auto op_info_parser = GetOpYamlInfoParser(op)";
-      auto op_info_parser = GetOpYamlInfoParser(op);
-      VLOG(1) << "finish auto op_info_parser = GetOpYamlInfoParser(op), "
-                 "op->num_operands(): "
-              << op->num_operands();
-      for (size_t i = 0; i < op->num_operands(); ++i) {
-        VLOG(1) << "1111111111";
-        auto define_op = op->operand_source(i).defining_op();
-        VLOG(1) << "define_op: " << define_op;
-        VLOG(1) << "op_info_parser->IsTensorAttribute(i): "
-                << op_info_parser->IsTensorAttribute(i);
+      return var_place == place_;
+    }
+    return false;
+  }
 
-        if (op_info_parser->IsTensorAttribute(i) && define_op &&
-            define_op->isa<paddle::dialect::PhiKernelOp>() &&
-            define_op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name() ==
-                "pd_op.shadow_feed") {
-          VLOG(1) << "22222222222";
-          auto in = define_op->operand_source(0);
-          auto out = define_op->result(0);
-          VLOG(1) << "333333333333";
-          rewriter.ReplaceAllUsesWith(out, in);
-          VLOG(1) << "44444444444444";
-          rewriter.EraseOp(define_op);
-          VLOG(1) << "RemoveShadowFeedPattern remove IsTensorAttribute "
-                     "shadow_feed: "
-                  << op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name();
-          return true;
+  bool IsTensorAttrShadowFeed(pir::Operation *op) const {
+    if (op->isa<paddle::dialect::PhiKernelOp>() &&
+        op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name() ==
+            "pd_op.shadow_feed") {
+      auto out = op->result(0);
+      if (out.use_count() == 1) {
+        auto use_op = out.first_use().owner();
+        auto op_info_parser = GetOpYamlInfoParser(use_op);
+        for (size_t i = 0; i < use_op->num_operands(); ++i) {
+          if (out == use_op->operand_source(i) &&
+              op_info_parser->IsTensorAttribute(i)) {
+            return true;
+          }
         }
+        return false;
       }
       return false;
     }
     return false;
   }
 
-  // bool Match(pir::Operation* op) const override {
-  //   VLOG(1) << "RemoveShadowFeedPattern isa PhiKernelOp: " <<
-  //   op->isa<paddle::dialect::PhiKernelOp>(); if
-  //   (!op->isa<paddle::dialect::PhiKernelOp>()) {
-  //     VLOG(1) << "RemoveShadowFeedPattern op name: " << op->name();
-  //   }
-  //   if (op->isa<paddle::dialect::PhiKernelOp>() &&
-  //   op->dyn_cast<paddle::dialect::PhiKernelOp>().op_name() ==
-  //   "pd_op.shadow_feed") {
-  //       VLOG(1) << "RemoveShadowFeedPattern find pd_op.shadow_feed";
-  //       auto in = op->operand_source(0);
-  //       if (!kwargs_map_.count(in)) {
-  //         VLOG(1) << "RemoveShadowFeedPattern kwargs_map_ not contain var!";
-  //         return false;
-  //       }
-  //       auto in_name = kwargs_map_.at(in);
-  //       VLOG(1) << "RemoveShadowFeedPattern in_name: " << in_name;
-  //       auto *var = scope_->FindVar(in_name);
-  //       phi::Place var_place;
-  //       if (var->IsType<phi::DenseTensor>()) {
-  //         auto &tensor = var->Get<phi::DenseTensor>();
-  //         VLOG(1) << "RemoveShadowFeedPattern tensor.initialized(): " <<
-  //         tensor.initialized(); if (tensor.initialized()) {
-  //           var_place = tensor.place();
-  //         }
-  //       } else {
-  //         PADDLE_THROW(paddle::platform::errors::InvalidArgument(
-  //             "RemoveShadowFeedPattern only support output "
-  //             "variable of type DenseTensor, SelectedRows or
-  //             VariableRefArray"));
-  //       }
-  //       if (var_place == place_) {
-  //           return true;
-  //       }
-  //       return false;
-  //   } else {
-  //     auto op_info_parser = paddle::dialect::GetOpYamlInfoParser(op);
-  //     for (size_t i = 0; i < op->num_operands(); ++i) {
+  bool Match(pir::Operation *op) const override {
+    return IsSamePlaceShadowFeed(op) || IsTensorAttrShadowFeed(op);
+  }
 
-  //     }
-  //   // if (op_info_parser->IsTensorAttribute(i) &&
-  //   //     new_in.defining_op()->isa<PhiKernelOp>() &&
-  //   //     new_in.defining_op()->dyn_cast<PhiKernelOp>().op_name() ==
-  //   //         "pd_op.shadow_feed")
-  //   }
-  //   return false;
-  // }
-
-  // void Rewrite(pir::Operation* op,
-  //              pir::PatternRewriter& rewriter) const override {  // NOLINT
-  //   VLOG(1) << "RemoveShadowFeedPattern remove pd_op.shadow_feed";
-  //   auto in = op->operand_source(0);
-  //   auto out = op->result(0);
-  //   in.set_type(out.type());
-  //   rewriter.ReplaceAllUsesWith(out, in);
-  //   rewriter.EraseOp(op);
-  // }
+  void Rewrite(pir::Operation *op,
+               pir::PatternRewriter &rewriter) const override {  // NOLINT
+    auto in = op->operand_source(0);
+    auto out = op->result(0);
+    in.set_type(out.type());
+    rewriter.ReplaceAllUsesWith(out, in);
+    rewriter.EraseOp(op);
+  }
 
  private:
   const pir::Block *block_;
