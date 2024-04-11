68f543cabdf97eb865b02890aad63aba9528584e
jiawenxuan <jiawenxuan@baidu.com>
2024-04-11 11:45:12 +0000

    intgrate ConstraintsManager into the main process

--- paddle/pir/include/dialect/shape/utils/shape_or_data_expr.h
+++ paddle/pir/include/dialect/shape/utils/shape_or_data_expr.h
@@ -166,6 +166,14 @@ class ShapeOrDataDimExprs : public ShapeOrDataDimExprsBase {
   }
 };
 
+IR_API TensorShapeOrDataDimExprs SubstituteTensorShapeOrData(
+    const TensorShapeOrDataDimExprs& shape_or_data,
+    const std::unordered_map<DimExpr, DimExpr>& substitution_pattern);
+
+IR_API ShapeOrDataDimExprs SubstituteShapeOrData(
+    const ShapeOrDataDimExprs& shape_or_data,
+    const std::unordered_map<DimExpr, DimExpr>& substitution_pattern);
+
 IR_API std::ostream& operator<<(std::ostream&,
                                 const ShapeOrDataDimExprs& dim_expr);
 }  // namespace symbol
