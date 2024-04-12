95ecdce9701391dffff142b2d7fd873ad102bff6
co63oc <co63@163.com>
2024-04-13 06:30:41 +0800

    Fix

--- test/white_list/check_op_sequence_instance_0_input_white_list.py
+++ test/white_list/check_op_sequence_instance_0_input_white_list.py
@@ -25,18 +25,8 @@ import sys
 NEED_TO_FIX_OP_LIST = [
     'sequence_concat',
     'sequence_conv',
-    'sequence_enumerate',
-    'sequence_erase',
-    'sequence_expand_as',
-    'sequence_expand',
     'sequence_mask',
-    'sequence_pad',
-    'sequence_reshape',
-    'sequence_reverse',
-    'sequence_scatter',
-    'sequence_slice',
     'sequence_softmax',
-    'sequence_unpad',
 ]
 
 op_name = sys.argv[1]
