6acb7f995bae8f7db675ba6703b7a750e49ef1c4
Sonder <55493212+AndSonder@users.noreply.github.com>
2024-04-15 13:18:50 +0800

    fix

--- python/paddle/distributed/passes/pipeline_scheduler_pass/__init__.py
+++ python/paddle/distributed/passes/pipeline_scheduler_pass/__init__.py
@@ -19,6 +19,8 @@ from .pipeline_1f1b import Pipeline1F1BPass  # noqa: F401
 from .pipeline_eager_1f1b import PipelineEager1F1BPass  # noqa: F401
 from .pipeline_fthenb import PipelineFThenBPass  # noqa: F401
 from .pipeline_vpp import PipelineVirtualPipelinePass  # noqa: F401
+from .pipeline_zero_bubble import PipelineZeroBubblePipelinePass  # noqa: F401
+
 
 __all__ = []
 
