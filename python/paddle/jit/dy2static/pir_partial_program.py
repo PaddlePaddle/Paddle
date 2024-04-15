e2bd0d6bfbb3057a5cfa2b74b93133de6073fcfd
SigureMo <sigure.qaq@gmail.com>
2024-04-15 06:34:42 +0000

    [Dy2St][PIR] Use `FLAGS_use_cinn` flag in PIR mode

--- python/paddle/jit/dy2static/pir_partial_program.py
+++ python/paddle/jit/dy2static/pir_partial_program.py
@@ -29,7 +29,7 @@ from paddle.base.dygraph.base import switch_to_static_graph
 from paddle.optimizer.lr import LRScheduler
 from paddle.pir import Value, fake_value, is_fake_value
 
-from .utils import RETURN_NO_VALUE_MAGIC_NUM, backend_guard
+from .utils import RETURN_NO_VALUE_MAGIC_NUM, backend_guard, cinn_is_enabled
 
 __all__ = []
 
@@ -566,7 +566,7 @@ class PartialProgramLayer:
                 pm.run(forward_program)
 
                 # if-else pass
-                if self._build_strategy.build_cinn_pass:
+                if cinn_is_enabled(self._build_strategy, self._backend):
                     paddle.base.libpaddle.pir.apply_cinn_pass(forward_program)
 
                 return forward_program, backward_program
@@ -586,7 +586,7 @@ class PartialProgramLayer:
             self._set_grad_type(self._params, train_program)
 
             def pass_fn(forward_program, backward_program):
-                if self._build_strategy.build_cinn_pass:
+                if cinn_is_enabled(self._build_strategy, self._backend):
                     paddle.base.libpaddle.pir.apply_cinn_pass(forward_program)
                     paddle.base.libpaddle.pir.apply_cinn_pass(backward_program)
                 return forward_program, backward_program
