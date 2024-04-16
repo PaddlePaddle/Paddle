06efb186f40f0755bb7c231990c7a6b80ac199ea
iosmers <yinwei_hust@163.com>
2024-04-16 12:51:50 +0800

    update

--- python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/hybrid_parallel_optimizer.py
+++ python/paddle/distributed/fleet/meta_optimizers/dygraph_optimizer/hybrid_parallel_optimizer.py
@@ -334,10 +334,10 @@ class HybridParallelOptimizer:
     def _set_all_gather_overlap_forward(
         self, all_gather_overlap_forward, layers=None
     ):
-        self._all_gather_overlap_forward = all_gather_overlap_forward
+        self.all_gather_overlap_forward = all_gather_overlap_forward
         self._layers = layers
         self._inner_opt._set_all_gather_overlap_forward(
-            self._set_all_gather_overlap_forward, self._layers
+            self.all_gather_overlap_forward, self._layers
         )
 
     def _insert_sync(self, sync_var, src, mp_group, sync_mode):
