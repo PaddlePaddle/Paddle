e790d72e429d7f276c14e6cc9080f492fdf1a374
co63oc <co63@163.com>
2024-04-17 13:00:28 +0800

    Fix

--- test/cinn/op_mappers/test_lookup_table_op.py
+++ test/cinn/op_mappers/test_lookup_table_op.py
@@ -19,50 +19,6 @@ from op_mapper_test import OpMapperTest
 import paddle
 
 
-class TestLookupTableOp(OpMapperTest):
-    def init_input_data(self):
-        self.feed_data = {
-            "w": self.random([10, 3], "float32"),
-            "ids": self.random([5, 1], "int64", 0, 9),
-        }
-
-    def set_op_type(self):
-        return "lookup_table"
-
-    def set_op_inputs(self):
-        w = paddle.static.data(
-            name="w",
-            shape=self.feed_data["w"].shape,
-            dtype=self.feed_data["w"].dtype,
-        )
-        ids = paddle.static.data(
-            name="ids",
-            shape=self.feed_data["ids"].shape,
-            dtype=self.feed_data["ids"].dtype,
-        )
-        return {"W": [w], "Ids": [ids]}
-
-    def set_op_attrs(self):
-        return {"padding_idx": -1}
-
-    def set_op_outputs(self):
-        return {"Out": [str(self.feed_data["w"].dtype)]}
-
-    def test_check_results(self):
-        self.check_outputs_and_grads()
-
-
-class TestLookupTableOpCase1(TestLookupTableOp):
-    def init_input_data(self):
-        self.feed_data = {
-            "w": self.random([32, 64], "float64"),
-            "ids": self.random([10, 1], "int64", 0, 31),
-        }
-
-    def set_op_attrs(self):
-        return {"padding_idx": 1}
-
-
 class TestLookupTableV2Op(OpMapperTest):
     def init_input_data(self):
         self.feed_data = {
