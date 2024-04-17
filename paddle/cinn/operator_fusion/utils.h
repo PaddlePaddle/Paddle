1d36f2f35f851e742603d4cf7ebf0fbe7cd05933
BiynXu <244524405@qq.com>
2024-04-17 15:45:50 +0800

    fix reduction on bool

--- paddle/cinn/operator_fusion/utils.h
+++ paddle/cinn/operator_fusion/utils.h
@@ -49,6 +49,13 @@ static std::vector<int64_t> GetReduceAxisIdx(pir::Operation* reduce_op) {
   const auto& attr_val = reduce_op->attributes().at("dim");
   CHECK(attr_val.isa<::pir::ArrayAttribute>());
   const auto& axis_attr = attr_val.dyn_cast<::pir::ArrayAttribute>();
+  if (axis_attr.empty()) {
+    std::vector<int64_t> all_axis;
+    for (int i = 0; i < input_rank; ++i) {
+      all_axis.push_back(i);
+    }
+    return all_axis;
+  }
   std::vector<int64_t> reduce_axis_idx;
   for (int i = 0; i < axis_attr.size(); ++i) {
     int64_t axis = axis_attr.at(i).dyn_cast<::pir::Int64Attribute>().data();
