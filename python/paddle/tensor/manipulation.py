2cf2c69fea4a218ff9426c2db536da47d58b7d5a
unknown <1368198410@qq.com>
2024-04-17 19:33:13 +0800

    add illustration for reshape API

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -4347,8 +4347,10 @@ def reshape(x, shape, name=None):
 
     The following figure illustrates the first example -- a 3D tensor of shape [2, 4, 6] is transformed into a 2D tensor of shape [6, 8], during which the order and values of the elements in the tensor remain unchanged. The elements in the two subdiagrams correspond to each other, clearly demonstrating how the reshape API works.
 
-    .. image:: ../../../../docs/docs/images/api_legend/reshape.png
+    .. image:: https://github.com/PaddlePaddle/docs/blob/develop/docs/images/api_legend/reshape.png
+        :width: 800
         :alt: legend of reshape API
+        :align: center
 
     Args:
         x (Tensor): An N-D Tensor. The data type is ``float16``, ``float32``, ``float64``, ``int16``, ``int32``, ``int64``, ``int8``, ``uint8``, ``complex64``, ``complex128``, ``bfloat16`` or ``bool``.
