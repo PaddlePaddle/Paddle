412e2797e488c2d080b7d3bae244c239e3e5ce16
Zhan Rongrui <2742392377@qq.com>
2024-04-19 05:07:49 +0000

    support linux sccache

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -14,6 +14,23 @@ if(NOT WIN32)
     set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE_PATH})
     set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PATH})
     set(CMAKE_CUDA_COMPILER_LAUNCHER ${CCACHE_PATH})
+  elseif("${CMAKE_GENERATOR}" STREQUAL "Ninja")
+    # (Note:zhouwei25) Only Ninja Generator can support sccache now
+    find_program(SCCACHE_PATH sccache)
+
+    if(SCCACHE_PATH)
+      execute_process(COMMAND sccache -V OUTPUT_VARIABLE sccache_version)
+      message(
+        STATUS
+          "sccache is founded, use [${SCCACHE_PATH}] to speed up compile on Windows."
+      )
+
+      set(CMAKE_C_COMPILER_LAUNCHER ${SCCACHE_PATH})
+      set(CMAKE_CXX_COMPILER_LAUNCHER ${SCCACHE_PATH})
+      # (Note:zhouwei25) sccache for cuda compiler has bug so that it can't be hit
+      # refer to https://github.com/mozilla/sccache/issues/1017, so we fix it
+      set(CMAKE_CUDA_COMPILER_LAUNCHER ${SCCACHE_PATH})
+    endif()
   endif()
 elseif("${CMAKE_GENERATOR}" STREQUAL "Ninja")
   # (Note:zhouwei25) Only Ninja Generator can support sccache now
