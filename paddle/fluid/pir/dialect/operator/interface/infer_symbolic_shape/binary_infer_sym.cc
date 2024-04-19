ac8fd099e462e24631f13a142cd008681bdc2eca
hxzd5568 <xiazichao@baidu.com>
2024-04-19 07:22:37 +0000

    [CINN] Add symbolic isclose op

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
@@ -478,6 +478,15 @@ bool SearchsortedOpInferSymbolicShape(
   return true;
 }
 
+bool IscloseOpInferSymbolicShape(
+    pir::Operation *op, pir::ShapeConstraintIRAnalysis *shape_analysis) {
+  // The shape of output is the same as input `values` (op->operand_source(1))
+  const symbol::ShapeOrDataDimExprs &operand_shape_or_data =
+      shape_analysis->GetShapeOrDataForValue(op->operand_source(1));
+  shape_analysis->SetShapeOrDataForValue(op->result(0), operand_shape_or_data);
+  return true;
+}
+
 bool TakeAlongAxisOpInferSymbolicShape(
     pir::Operation *op, pir::ShapeConstraintIRAnalysis *shape_analysis) {
   // input
@@ -544,3 +553,7 @@ bool TopPSamplingOpInferSymbolicShape(
 }
 
 }  // namespace paddle::dialect
+
+namespace cinn::dialect {
+using paddle::dialect::IscloseOpInferSymbolicShape;
+}
