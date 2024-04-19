9eb14b99dba31407387fb07c12bdf982e14fc663
Zhang,Lirong <56445728+zhanglirong1999@users.noreply.github.com>
2024-04-19 10:39:43 +0800

    [OneDNN][PIR] Add elementwise_act_onednn_fuse_pass (#63516)
    * first commit of elementwise act pass
    
    * update pass
    
    * style code
    
    * style
    
    * change error header
    
    * update for review
    
    * ut addPass change
    
    * change some code style

--- paddle/fluid/inference/api/paddle_pass_builder.cc
+++ paddle/fluid/inference/api/paddle_pass_builder.cc
@@ -631,7 +631,8 @@ const std::vector<std::string> kPirMkldnnPasses{
     "matmul_activation_fuse_pass",
     "conv_elementwise_add_onednn_fuse_pass",
     "conv_activation_onednn_fuse_pass",
-    "conv_concat_activation_onednn_fuse_pass"};
+    "conv_concat_activation_onednn_fuse_pass",
+    "elementwise_act_onednn_fuse_pass"};
 
 const std::vector<std::string> kPirCpuPasses{};
 
