5c90eacfe7e972eb8f859c66ed98fbafdf92c463
gouzi <530971494@qq.com>
2024-04-19 17:47:43 +0800

    remove breakpoint

--- test/dygraph_to_static/test_no_gradient.py
+++ test/dygraph_to_static/test_no_gradient.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-breakpoint()  # noqa: T100
+# breakpoint()
 import unittest
 
 import numpy
