be140078bee0758034be48ff232588975d99edd5
jiahongyu <jiahongyu@baidu.com>
2024-04-19 07:49:49 +0000

    [CINN] Polish Slice Compute

--- paddle/cinn/hlir/pe/transform.cc
+++ paddle/cinn/hlir/pe/transform.cc
@@ -1056,6 +1056,17 @@ ir::Tensor Transpose(const ir::Tensor& input,
       output_name);
 }
 
+int UpdateNegAxis(int axis, int rank) {
+  if (axis < 0) {
+    PADDLE_ENFORCE_GE(
+        axis + rank,
+        0,
+        ::common::errors::InvalidArgument("The axis of slice is out of range"));
+    return axis + rank;
+  }
+  return axis;
+}
+
 ir::Tensor Slice(const ir::Tensor& A,
                  const std::vector<int>& starts,
                  const std::vector<int>& const_axes,
@@ -1067,21 +1078,13 @@ ir::Tensor Slice(const ir::Tensor& A,
   for (const auto& shape : A->shape) {
     input_shape.emplace_back(shape.as_int32());
   }
-  const auto UpdateNegAxis = [rank = A->shape.size()](const int axis) -> int {
-    if (axis < 0) {
-      PADDLE_ENFORCE_GE(axis + rank,
-                        0,
-                        ::common::errors::InvalidArgument(
-                            "The axis of slice is out of range"));
-      return axis + rank;
-    }
-    return axis;
-  };
   std::vector<int> axes;
   std::transform(const_axes.begin(),
                  const_axes.end(),
                  std::back_inserter(axes),
-                 UpdateNegAxis);
+                 [rank = A->shape.size()](const int axis) -> int {
+                   return UpdateNegAxis(axis, rank);
+                 });
   std::vector<int> new_starts(starts);
   for (int i = 0; i < axes.size(); i++) {
     if (new_starts[i] < -input_shape[axes[i]]) {
@@ -1141,21 +1144,13 @@ ir::Tensor SliceSymbolic(const ir::Tensor& A,
                  starts.end(),
                  std::back_inserter(new_starts),
                  [](const int start) { return ir::Expr(start); });
-  const auto UpdateNegAxis = [rank = A->shape.size()](const int axis) -> int {
-    if (axis < 0) {
-      PADDLE_ENFORCE_GE(axis + rank,
-                        0,
-                        ::common::errors::InvalidArgument(
-                            "The axis of slice is out of range"));
-      return axis + rank;
-    }
-    return axis;
-  };
   std::vector<int> axes;
   std::transform(const_axes.begin(),
                  const_axes.end(),
                  std::back_inserter(axes),
-                 UpdateNegAxis);
+                 [rank = A->shape.size()](const int axis) -> int {
+                   return UpdateNegAxis(axis, rank);
+                 });
 
   for (int i = 0; i < axes.size(); i++) {
     if (input_shape[axes[i]].is_constant()) {
