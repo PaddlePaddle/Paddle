0c1bf0828fa2d83cc9742d510ba83e05ae1483a5
xingmingyyj <zxm_3791@163.com>
2024-04-22 06:58:00 +0000

    fix

--- paddle/fluid/ir_adaptor/translator/op_compat_gen.py
+++ paddle/fluid/ir_adaptor/translator/op_compat_gen.py
@@ -169,6 +169,10 @@ def OpNameNormalizerInitialization(
         {"out_grad_in": "Out@GRAD", "out_grad_out": "Out@GRAD"}
     )
 
+    op_mutable_attributes["reduce_mean"] = set()
+    op_mutable_attributes["reduce_mean"].add("axis")
+    op_mutable_attributes["reduce_mean"]["axis"] = []
+
     op_name_normalizer_template = env.get_template("op_compat_info.cc.j2")
     with open(output_source_file, 'wt') as f:
         op_compat_definition = op_name_normalizer_template.render(
