0ca6f1de8f09e033ce41149a2ce29b99a6f9bb48
phlrain <phliuhongyu@126.com>
2024-04-22 10:36:28 +0800

    add sigmoid convert to cinn pass

--- paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
@@ -886,6 +886,52 @@ class UnsqueezeOpPattern
   }
 };
 
+class SigmoidOpPattern
+    : public pir::OpRewritePattern<paddle::dialect::SigmoidOp> {
+ public:
+  using pir::OpRewritePattern<paddle::dialect::SigmoidOp>::OpRewritePattern;
+
+  bool MatchAndRewrite(paddle::dialect::SigmoidOp op,
+                       pir::PatternRewriter &rewriter) const override {
+    auto input_dtype = paddle::dialect::TransToPhiDataType(
+        op->operand_source(0)
+            .type()
+            .dyn_cast<paddle::dialect::DenseTensorType>()
+            .dtype());
+
+    auto in = op->operand_source(0);
+    bool need_cast = (input_dtype == phi::DataType::FLOAT16 ||
+                      input_dtype == phi::DataType::BFLOAT16 ||
+                      input_dtype == phi::DataType::UINT16);
+    if (need_cast) {
+      in = rewriter.Build<paddle::dialect::CastOp>(in, phi::DataType::FLOAT32)
+               .result(0);
+    }
+
+    // 1 / ( 1 + exp(-x))
+    auto one = rewriter
+                   .Build<paddle::dialect::FullOp>(
+                       std::vector<int64_t>({1}), 1.0, phi::DataType::FLOAT32)
+                   .result(0);
+    auto minus_x =
+        rewriter.Build<paddle::dialect::ScaleOp>(in, -1.0, 0.0).result(0);
+    auto exp = rewriter.Build<paddle::dialect::ExpOp>(minus_x).result(0);
+    auto add_exp = rewriter.Build<paddle::dialect::AddOp>(one, exp).result(0);
+    auto div =
+        rewriter.Build<paddle::dialect::DivideOp>(one, add_exp).result(0);
+
+    if (need_cast) {
+      div = rewriter.Build<paddle::dialect::CastOp>(div, input_dtype).result(0);
+    }
+
+    rewriter.ReplaceAllUsesWith(op.result(0), div);
+
+    rewriter.EraseOp(op);
+
+    return true;
+  }
+};
+
 PdOpToCinnOpPass::PdOpToCinnOpPass()
     : pir::PatternRewritePass("pd_to_cinn_pass", 1) {}
 
@@ -911,6 +957,7 @@ pir::RewritePatternSet PdOpToCinnOpPass::InitializePatterns(
   ps.Add<RefreshCombineOpPattern>(context);
   ps.Add<SqueezeOpPattern>(context);
   ps.Add<UnsqueezeOpPattern>(context);
+  ps.Add<SigmoidOpPattern>(context);
 
   return ps;
 }
