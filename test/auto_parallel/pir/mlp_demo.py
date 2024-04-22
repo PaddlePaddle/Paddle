b70a3d07da9f57e877ad838c09074818d92fcaa4
liangjianzhong <liangjianzhong@baidu.com>
2024-04-22 17:24:13 +0800

    pp runable demo

--- test/auto_parallel/pir/mlp_demo.py
+++ test/auto_parallel/pir/mlp_demo.py
@@ -15,7 +15,7 @@
 import unittest
 
 import numpy as np
-from test_to_static_pir_program import create_data_loader
+from test_to_static_pir_program import DemoNet, create_data_loader
 
 import paddle
 import paddle.distributed as dist
@@ -65,123 +65,49 @@ class PPDemoNet(nn.Layer):
         return out
 
 
-# class TestMLPTensorParallel(unittest.TestCase):
-#     def test_to_static_program(self):
-#         paddle.base.set_flags({'FLAGS_enable_pir_api': 1})
-#         mesh = dist.ProcessMesh([0, 1], dim_names=["x"])
-#         mp_layer = DemoNet(mesh, True)
-#         opt = paddle.optimizer.SGD(
-#             learning_rate=0.1, parameters=mp_layer.parameters()
-#         )
-#         loss_fn = nn.MSELoss()
-#         loader = create_data_loader()
-#         dist_loader = dist.shard_dataloader(loader, meshes=[mesh])
-#         dist_model = dist.to_static(mp_layer, dist_loader, loss_fn, opt)
-
-#         dist_model.train()
-#         mode = "train"
-
-#         # TODO(2024-Q2) hack for engine api
-#         dist_model._engine._has_prepared[mode] = True
-#         dist_model._mode = mode
-#         dist_model._engine._mode = mode
-#         paddle.disable_static()
-#         dist_model._engine._initialize(mode)
-#         dist_model._engine._executor = paddle.static.Executor(
-#             _current_expected_place()
-#         )
-#         dist_model._engine._init_comm()
-
-#         for batch_id, (image, label) in enumerate(dist_loader()):
-#             loss = dist_model(image, label)
-
-
-# class TestMLPReplicated(unittest.TestCase):
-#     def test_to_static_program(self):
-#         paddle.base.set_flags({'FLAGS_enable_pir_api': 1})
-#         mesh = dist.ProcessMesh([0, 1], dim_names=["x"])
-#         replicated_layer = DemoNet(mesh, False)
-#         opt = paddle.optimizer.SGD(
-#             learning_rate=0.1, parameters=replicated_layer.parameters()
-#         )
-#         loss_fn = nn.MSELoss()
-#         loader = create_data_loader()
-#         dist_loader = dist.shard_dataloader(loader, meshes=[mesh])
-#         dist_model = dist.to_static(replicated_layer, dist_loader, loss_fn, opt)
-
-#         dist_model.train()
-#         mode = "train"
-#         # dist_model._engine._build(mode)
-#         # main_program = dist_model._engine._pir_main_progs[mode]
-
-#         # TODO(2024-Q2) hack for engine api
-#         # main_program = dist_model._engine._pir_main_progs[mode]
-#         dist_model._engine._has_prepared[mode] = True
-#         dist_model._mode = mode
-#         dist_model._engine._mode = mode
-#         paddle.disable_static()
-#         dist_model._engine._initialize(mode)
-#         dist_model._engine._executor = paddle.static.Executor(
-#             _current_expected_place()
-#         )
-
-#         for batch_id, (image, label) in enumerate(dist_loader()):
-#             loss = dist_model(image, label)
-
-
-class TestMLPPipelineParallel(unittest.TestCase):
+class TestMLPTensorParallel(unittest.TestCase):
     def test_to_static_program(self):
         paddle.base.set_flags({'FLAGS_enable_pir_api': 1})
-        mesh1 = dist.ProcessMesh([0], dim_names=["x"])
-        mesh2 = dist.ProcessMesh([1], dim_names=["y"])
-        pp_layer = PPDemoNet(mesh1, mesh2)
+        mesh = dist.ProcessMesh([0, 1], dim_names=["x"])
+        mp_layer = DemoNet(mesh, True)
         opt = paddle.optimizer.SGD(
-            learning_rate=0.1, parameters=pp_layer.parameters()
+            learning_rate=0.1, parameters=mp_layer.parameters()
         )
         loss_fn = nn.MSELoss()
         loader = create_data_loader()
-        dist_loader = dist.shard_dataloader(loader, meshes=[mesh1, mesh2])
-        dist_model = dist.to_static(pp_layer, dist_loader, loss_fn, opt)
+        dist_loader = dist.shard_dataloader(loader, meshes=[mesh])
+        dist_model = dist.to_static(mp_layer, dist_loader, loss_fn, opt)
 
-        engine = dist_model._engine
-        engine._build("train")
-        dist_program = engine._fwd_main_progs["train"]
-        print("================" + "fwd mix program" + "================")
-        print(dist_program)
-        dist_program = paddle.base.libpaddle.pir.apply_mix2dist_pass(
-            dist_program
-        )
-        print("================" + "fwd dist program" + "================")
-        print(dist_program)
-        loss = dist_program.get_output_value_by_name(engine._loss_names[0])
-        with paddle.static.program_guard(dist_program):
-            params_grads = paddle.autograd.ir_backward.append_backward(loss)
-            print(
-                "================"
-                + "fwd + bw dist program"
-                + "================"
-            )
-            print(dist_program)
-            engine._optimizer._apply_optimize(
-                loss, startup_program=None, params_grads=params_grads
-            )
-            print(
-                "================"
-                + "fwd + bw + opt + dist program"
-                + "================"
-            )
-        print(dist_program)
+        dist_model.train()
+        mode = "train"
 
-        from paddle.distributed.auto_parallel.static.pir_pass import (
-            apply_partition_pass,
+        # TODO(2024-Q2) hack for engine api
+        dist_model._engine._has_prepared[mode] = True
+        dist_model._mode = mode
+        dist_model._engine._mode = mode
+        paddle.disable_static()
+        dist_model._engine._initialize(mode)
+        dist_model._engine._executor = paddle.static.Executor(
+            _current_expected_place()
         )
+        dist_model._engine._init_comm()
 
-        dist_program = apply_partition_pass(dist_program)
-        print(
-            "================" + "partitioner dist program" + "================"
+        for batch_id, (image, label) in enumerate(dist_loader()):
+            loss = dist_model(image, label)
+
+
+class TestMLPReplicated(unittest.TestCase):
+    def test_to_static_program(self):
+        paddle.base.set_flags({'FLAGS_enable_pir_api': 1})
+        mesh = dist.ProcessMesh([0, 1], dim_names=["x"])
+        replicated_layer = DemoNet(mesh, False)
+        opt = paddle.optimizer.SGD(
+            learning_rate=0.1, parameters=replicated_layer.parameters()
         )
-        dist_program = apply_partition_pass(dist_program)
-        print(dist_program)
+        loss_fn = nn.MSELoss()
+        loader = create_data_loader()
+        dist_loader = dist.shard_dataloader(loader, meshes=[mesh])
+        dist_model = dist.to_static(replicated_layer, dist_loader, loss_fn, opt)
 
         dist_model.train()
         mode = "train"
@@ -201,6 +127,38 @@ class TestMLPPipelineParallel(unittest.TestCase):
 
         for batch_id, (image, label) in enumerate(dist_loader()):
             loss = dist_model(image, label)
+
+
+class TestMLPPipelineParallel(unittest.TestCase):
+    def test_to_static_program(self):
+        paddle.base.set_flags({'FLAGS_enable_pir_api': 1})
+        mesh1 = dist.ProcessMesh([0], dim_names=["x"])
+        mesh2 = dist.ProcessMesh([1], dim_names=["y"])
+        pp_layer = PPDemoNet(mesh1, mesh2)
+        opt = paddle.optimizer.SGD(
+            learning_rate=0.1, parameters=pp_layer.parameters()
+        )
+        loss_fn = nn.MSELoss()
+        loader = create_data_loader()
+        dist_loader = dist.shard_dataloader(loader, meshes=[mesh1, mesh2])
+        dist_model = dist.to_static(pp_layer, dist_loader, loss_fn, opt)
+        dist_model.train()
+        mode = "train"
+
+        # TODO(2024-Q2) hack for engine api
+        dist_model._engine._has_prepared[mode] = True
+        dist_model._mode = mode
+        dist_model._engine._mode = mode
+        paddle.disable_static()
+        dist_model._engine._initialize(mode)
+        dist_model._engine._executor = paddle.static.Executor(
+            _current_expected_place()
+        )
+        dist_model._engine._init_comm()
+
+        for batch_id, (image, label) in enumerate(dist_loader()):
+            loss = dist_model(image, label)
+            print(loss)
 
 
 if __name__ == "__main__":
