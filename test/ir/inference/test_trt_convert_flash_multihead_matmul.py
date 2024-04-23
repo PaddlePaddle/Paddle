04683a8da8b2a76c39b7d39dc7acb0727adc6e4a
ylichen <ylichen@nvidia.com>
2024-04-22 17:07:21 -0700

    Set higher optimization level for implementation

--- test/ir/inference/test_trt_convert_flash_multihead_matmul.py
+++ test/ir/inference/test_trt_convert_flash_multihead_matmul.py
@@ -24,6 +24,10 @@ import paddle.inference as paddle_infer
 
 
 class TrtConvertFlashMultiHeadMatmulTest(TrtLayerAutoScanTest):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.optimization_level = 5
+
     def is_program_valid(self, program_config: ProgramConfig) -> bool:
         ver = paddle_infer.get_trt_compile_version()
         if ver[0] * 1000 + ver[1] * 100 + ver[2] * 10 < 8520:
@@ -320,6 +324,10 @@ class TrtConvertFlashMultiHeadMatmulTest(TrtLayerAutoScanTest):
 
 
 class TrtConvertFlashMultiHeadMatmulWeightInputTest(TrtLayerAutoScanTest):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.optimization_level = 5
+
     def is_program_valid(self, program_config: ProgramConfig) -> bool:
         ver = paddle_infer.get_trt_compile_version()
         if ver[0] * 1000 + ver[1] * 100 + ver[2] * 10 < 8520:
