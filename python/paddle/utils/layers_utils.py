7dea0f6778170fb1b4c5324d48f3f96acb115f7b
HydrogenSulfate <490868991@qq.com>
2024-04-23 09:32:48 +0000

    keep inference model output keys same order with dynamic model

--- python/paddle/utils/layers_utils.py
+++ python/paddle/utils/layers_utils.py
@@ -145,7 +145,7 @@ def _sorted(dict_):
     Returns a sorted list of the dict keys, with error if keys not sortable.
     """
     try:
-        return sorted(dict_.keys())
+        return list(dict_.keys())
     except TypeError:
         raise TypeError("nest only supports dicts with sortable keys.")
 
