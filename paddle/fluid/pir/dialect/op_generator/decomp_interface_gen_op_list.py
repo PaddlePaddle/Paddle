8cfefac54d8f1d9bc5d0b0170f0ed7c3342eba01
Eddie-Wang1120 <wangjinheng1120@163.com>
2024-04-23 13:28:50 +0000

    add bmm prim

--- paddle/fluid/pir/dialect/op_generator/decomp_interface_gen_op_list.py
+++ paddle/fluid/pir/dialect/op_generator/decomp_interface_gen_op_list.py
@@ -23,6 +23,7 @@ decomp_interface_declare_gen_op_list = [
     "any",
     "batch_norm",
     "batch_norm_",
+    "bmm",
     "dropout",
     "elu",
     "embedding",
@@ -57,6 +58,7 @@ decomp_interface_declare_gen_op_list = [
 # manual decomp interface implementation are located in manual_op_decomp.cc
 decomp_interface_implementation_gen_op_list = [
     "add_n",
+    "bmm",
     "dropout",
     "elu",
     "embedding",
