67e17654d01e6769c0f4e13b07d57c8b39ec0fda
SigureMo <sigure.qaq@gmail.com>
2024-04-25 00:57:22 +0800

    [CI] Remove github installation in CI

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3850,9 +3850,7 @@ function run_setup(){
         INFERENCE_DEMO_INSTALL_DIR=${INFERENCE_DEMO_INSTALL_DIR:-/root/.cache/inference_demo}
     fi
 
-    pip uninstall -y PyGithub
-    pip install github
-    pip install PyGithub
+    pip install -U PyGithub
     python ${PADDLE_ROOT}/tools/check_only_change_python_files.py
     if [ -f "${PADDLE_ROOT}/build/only_change_python_file.txt" ];then
          export WITH_CPP_TEST=OFF
