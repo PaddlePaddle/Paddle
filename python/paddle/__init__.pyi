dd9e04e045d2c529a2ed927332cf2a9577b8b3f1
Asthestarsfalll <1186454801@qq.com>
2024-04-24 19:53:42 +0800

    add Tensor

--- python/paddle/__init__.pyi
+++ python/paddle/__init__.pyi
@@ -1,5 +1,27 @@
 from __future__ import annotations
 
+from .framework.dtype import (
+    bfloat16 as bfloat16,
+    bool as bool,
+    complex64 as complex64,
+    complex128 as complex128,
+    dtype as dtype,
+    finfo as finfo,
+    float16 as float16,
+    float32 as float32,
+    float64 as float64,
+    iinfo as iinfo,
+    int8 as int8,
+    int16 as int16,
+    int32 as int32,
+    int64 as int64,
+    uint8 as uint8,
+)
+
+class Tensor: ...
+
+def to_tensor(data, dtype=None, place=None, stop_gradient=True) -> Tensor: ...
+
 class CPUPlace: ...
 
 class CUDAPlace:
