f22574cbc93a5f43d70f672f073b19dd7ff8dc60
tianshuo78520a <707759223@qq.com>
2024-04-26 09:16:22 +0000

    Fix cmake

--- test/cpp/inference/test.cmake
+++ test/cpp/inference/test.cmake
@@ -29,28 +29,6 @@ function(inference_download INSTALL_DIR URL FILENAME)
     INSTALL_COMMAND "")
 endfunction()
 
-function(inference_download_and_uncompress INSTALL_DIR URL FILENAME CHECK_SUM)
-  message(STATUS "Download inference test stuff from ${URL}/${FILENAME}")
-  string(REGEX REPLACE "[-%./\\]" "_" FILENAME_EX ${FILENAME})
-  string(REGEX MATCH "[^/\\]+$" DOWNLOAD_NAME ${FILENAME})
-  set(EXTERNAL_PROJECT_NAME "extern_download_${FILENAME_EX}")
-  set(UNPACK_DIR "${INSTALL_DIR}/src/${EXTERNAL_PROJECT_NAME}")
-  ExternalProject_Add(
-    ${EXTERNAL_PROJECT_NAME}
-    ${EXTERNAL_PROJECT_LOG_ARGS}
-    PREFIX ${INSTALL_DIR}
-    URL ${URL}/${FILENAME}
-    URL_HASH MD5=${CHECK_SUM}
-    DOWNLOAD_DIR ${INSTALL_DIR}
-    DOWNLOAD_NO_EXTRACT 1
-    DOWNLOAD_NO_PROGRESS 1
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${CMAKE_COMMAND} -E chdir ${INSTALL_DIR} ${CMAKE_COMMAND} -E
-                  tar xzf ${DOWNLOAD_NAME}
-    UPDATE_COMMAND ""
-    INSTALL_COMMAND "")
-endfunction()
-
 function(inference_download_and_uncompress_without_verify INSTALL_DIR URL
          FILENAME)
   message(STATUS "Download inference test stuff from ${URL}/${FILENAME}")
