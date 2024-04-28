dfae6867308c9f68a033dfa479a82473ce83689d
lizexu <2694294196@qq.com>
2024-04-28 03:07:07 +0000

    kd

--- paddle/fluid/pir/transforms/gpu/fused_rotary_position_embedding.cc
+++ paddle/fluid/pir/transforms/gpu/fused_rotary_position_embedding.cc
@@ -39,20 +39,13 @@ class FusedRotaryPositionEmbeddingPattern : public paddle::drr::DrrPatternBase {
     const auto &unsqueeze = pat.Op(paddle::dialect::UnsqueezeOp::name());
     const auto &unsqueeze_1 = pat.Op(paddle::dialect::UnsqueezeOp::name());
     const auto &unsqueeze_2 = pat.Op(paddle::dialect::UnsqueezeOp::name());
+    const auto &unsqueeze_4 = pat.Op(paddle::dialect::UnsqueezeOp::name());
     const auto &add = pat.Op(paddle::dialect::AddOp::name());
     const auto &add_1 = pat.Op(paddle::dialect::AddOp::name());
-    const auto &matmul_op = pat.Op(paddle::dialect::MatmulOp::name(),
-                                   {{"transpose_x", pat.Attr("trans_x")},
-                                    {"transpose_y", pat.Attr("trans_y")}});
-    const auto &matmul_op_1 = pat.Op(paddle::dialect::MatmulOp::name(),
-                                     {{"transpose_x", pat.Attr("trans_x_1")},
-                                      {"transpose_y", pat.Attr("trans_y_1")}});
-    const auto &matmul_op_2 = pat.Op(paddle::dialect::MatmulOp::name(),
-                                     {{"transpose_x", pat.Attr("trans_x_2")},
-                                      {"transpose_y", pat.Attr("trans_y_2")}});
-    const auto &matmul_op_3 = pat.Op(paddle::dialect::MatmulOp::name(),
-                                     {{"transpose_x", pat.Attr("trans_x_3")},
-                                      {"transpose_y", pat.Attr("trans_y_3")}});
+    const auto &multiply1 = pat.Op(paddle::dialect::MultiplyOp::name());
+    const auto &multiply2 = pat.Op(paddle::dialect::MultiplyOp::name());
+    const auto &multiply3 = pat.Op(paddle::dialect::MultiplyOp::name());
+    const auto &multiply4 = pat.Op(paddle::dialect::MultiplyOp::name());
 
     const auto &slice_q =
         pat.Op(paddle::dialect::SliceOp::name(),
@@ -133,6 +126,8 @@ class FusedRotaryPositionEmbeddingPattern : public paddle::drr::DrrPatternBase {
                                 {{"value", pat.Attr("full_6_value")}});
     const auto &full_7 = pat.Op(paddle::dialect::FullIntArrayOp::name(),
                                 {{"value", pat.Attr("full_7_value")}});
+    const auto &full_8 = pat.Op(paddle::dialect::FullIntArrayOp::name(),
+                                {{"value", pat.Attr("full_8_value")}});
 
     const auto &concat_op = pat.Op(paddle::dialect::ConcatOp::name());
     const auto &combine = pat.Op(pir::CombineOp::name());
@@ -145,23 +140,28 @@ class FusedRotaryPositionEmbeddingPattern : public paddle::drr::DrrPatternBase {
               {&pat.Tensor("squeeze_out_sin"), &pat.Tensor("xshape")});
 
     unsqueeze({&pat.Tensor("position_ids"), &full_3()},
-              {&pat.Tensor("unsqueeze_s_out"), &pat.Tensor("xshape")});
-    pat.Tensor("gather_nd_out_cos") =
-        gather_nd(pat.Tensor("squeeze_out_cos"), pat.Tensor("unsqueeze_s_out"));
-    pat.Tensor("gather_nd_out_sin") =
-        gather_nd(pat.Tensor("squeeze_out_sin"), pat.Tensor("unsqueeze_s_out"));
+              {&pat.Tensor("unsqueeze_s_out_cos"), &pat.Tensor("xshape")});
+    unsqueeze_4({&pat.Tensor("position_ids"), &full_8()},
+                {&pat.Tensor("unsqueeze_s_out_sin"), &pat.Tensor("xshape")});
+
+    pat.Tensor("gather_nd_out_cos") = gather_nd(
+        pat.Tensor("squeeze_out_cos"), pat.Tensor("unsqueeze_s_out_cos"));
+    pat.Tensor("gather_nd_out_sin") = gather_nd(
+        pat.Tensor("squeeze_out_sin"), pat.Tensor("unsqueeze_s_out_sin"));
 
     unsqueeze_1({&pat.Tensor("gather_nd_out_cos"), &full_4()},
                 {&pat.Tensor("unsqueeze_out_cos"), &pat.Tensor("xshape")});
+
     unsqueeze_2({&pat.Tensor("gather_nd_out_sin"), &full_5()},
                 {&pat.Tensor("unsqueeze_out_sin"), &pat.Tensor("xshape")});
-    matmul_op({&pat.Tensor("unsqueeze_out_cos"), &pat.Tensor("q")},
-              {&pat.Tensor("tmp_25")});
-    matmul_op_1({&pat.Tensor("unsqueeze_out_cos"), &pat.Tensor("k")},
-                {&pat.Tensor("tmp_29")});
+
+    // 160是unsqueeze_out_cos
+    pat.Tensor("tmp_25") =
+        multiply1(pat.Tensor("unsqueeze_out_cos"), pat.Tensor("q"));
 
     pat.Tensor("q_slice_out1") =
         slice_q(pat.Tensor("q"), full_int_array_q1(), full_int_array_q2());
+    // 129是q
     pat.Tensor("q_slice_out2") =
         slice_q_1(pat.Tensor("q"), full_int_array_q3(), full_int_array_q4());
     scale_op({&pat.Tensor("q_slice_out2"), &full_op()},
@@ -170,9 +170,14 @@ class FusedRotaryPositionEmbeddingPattern : public paddle::drr::DrrPatternBase {
     combine({&pat.Tensor("scale_out")}, {&pat.Tensor("combine_out")});
     concat_op({&pat.Tensor("combine_out"), &full_6()},
               {&pat.Tensor("concat_out")});
-    matmul_op_2({&pat.Tensor("unsqueeze_out_sin"), &pat.Tensor("concat_out")},
-                {&pat.Tensor("tmp_27")});
+    // 167是unsqueeze_out_sin
+    pat.Tensor("tmp_27") =
+        multiply3(pat.Tensor("unsqueeze_out_sin"), pat.Tensor("concat_out"));
+
     pat.Tensor("tmp_28") = add(pat.Tensor("tmp_27"), pat.Tensor("tmp_25"));
+    // 132是k
+    pat.Tensor("tmp_29") =
+        multiply2(pat.Tensor("unsqueeze_out_cos"), pat.Tensor("k"));
 
     pat.Tensor("k_slice_out1") =
         slice_k(pat.Tensor("k"), full_int_array_q5(), full_int_array_q6());
@@ -183,9 +188,11 @@ class FusedRotaryPositionEmbeddingPattern : public paddle::drr::DrrPatternBase {
     combine_k({&pat.Tensor("scale_out_k")}, {&pat.Tensor("combine_out_k")});
     concat_op_k({&pat.Tensor("combine_out_k"), &full_7()},
                 {&pat.Tensor("concat_out_k")});
-    matmul_op_3({&pat.Tensor("unsqueeze_out_sin"), &pat.Tensor("concat_out_k")},
-                {&pat.Tensor("tmp_31")});
+
+    pat.Tensor("tmp_31") =
+        multiply4(pat.Tensor("unsqueeze_out_sin"), pat.Tensor("concat_out_k"));
     pat.Tensor("tmp_32") = add_1(pat.Tensor("tmp_31"), pat.Tensor("tmp_29"));
+
     pat.RequireNativeCall([&](const paddle::drr::MatchContext &match_ctx) {
       auto check_axes = [&](const std::vector<int64_t> &axes) {
         std::vector<int64_t> expected_axes = {0, 2};
@@ -244,11 +251,12 @@ class FusedRotaryPositionEmbeddingPattern : public paddle::drr::DrrPatternBase {
     paddle::drr::ResultPattern res = pat.ResultPattern();
     const auto &fused_rotary_position_embedding =
         res.Op(paddle::dialect::FusedRotaryPositionEmbeddingOp::name(),
-               {{
+               {
                    {"use_neox_rotary_stype", res.BoolAttr(true)},
                    {"time_major", res.BoolAttr(false)},
                    {"rotary_emb_base", res.Float32Attr(10000.0)},
-               }});
+               });
+
     fused_rotary_position_embedding(
         {&res.Tensor("q"),
          &res.Tensor("k"),
