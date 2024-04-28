6852198a2e5a28f122ed65832778cbd7e7d00478
YuanRisheng <yuanrisheng@baidu.com>
2024-04-28 13:04:34 +0000

    perfect paddle_build.sh

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -1438,25 +1438,15 @@ function collect_failed_tests() {
 # getting qucik disable ut list
 function get_quickly_disable_ut() {
     python -m pip install httpx
-    if [ $deprecated_test_path ];then
-        cd $deprecated_test_path
-        deprecated_ut=`ctest -N | awk -F ': ' '{print $2}' | sed '/^$/d' | sed '$d' | sed 's/^/|^/' | sed 's/$/$/'`
-        deprecated_ut=`echo $deprecated_ut | sed 's/ //g'`
-        cd -
-    fi
     if disable_ut_quickly=$(python ${PADDLE_ROOT}/tools/get_quick_disable_lt.py); then
-        disable_ut_quickly="${disable_ut_quickly}${deprecated_ut}"
         echo "========================================="
         echo "The following unittests have been disabled:"
         echo ${disable_ut_quickly}
         echo "========================================="
     else
-        if [ $deprecated_ut ];then
-            disable_ut_quickly="${deprecated_ut}"
-        else
-            exit 102
-            disable_ut_quickly='disable_ut'
-        fi
+
+        exit 102
+        disable_ut_quickly='disable_ut'
     fi
 }
 
@@ -4478,7 +4468,7 @@ function main() {
       cicheck_py37_pir)
         export FLAGS_enable_pir_api=1
         # disable deprecated test in pir
-        deprecated_test_path=${PADDLE_ROOT}/build/test/deprecated/
+        rm -rf ${PADDLE_ROOT}/build/test/deprecated/
         run_linux_cpu_test ${PYTHON_ABI:-""} ${PROC_RUN:-1}
         ;;
       test_cicheck_py37)
