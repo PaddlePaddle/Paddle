e0bcd27339d575a3576b1b726bb497172b173ac1
ApricityXX <1470600398@qq.com>
2024-04-28 16:06:14 +0800

    copy_2_4.28

--- paddle/fluid/pir/dialect/distributed/ir/dist_op.cc
+++ paddle/fluid/pir/dialect/distributed/ir/dist_op.cc
@@ -141,6 +141,7 @@ void ShardTensorOp::Build(pir::Builder& builder,
 
   VLOG(4) << "Builder construction outputs";
   auto global_dims = input_tensor_type.dims();
+  const auto& process_mesh_shape = process_mesh_attr.shape();
    PADDLE_ENFORCE_EQ(static_cast<int>(dims_mapping.size()),
                     global_dims.size(),
                     common::errors::PreconditionNotMet(
@@ -324,9 +325,9 @@ void ReshardOp::Build(pir::Builder& builder,
 
   VLOG(4) << "Builder construction outputs";
   auto global_dims = input_tensor_type.global_ddim();
-  //auto process_mesh_attr = tensor_dist_attr.process_mesh_attr();
+  const auto& process_mesh_attr = tensor_dist_attr.process_mesh_attr();
   const auto& dims_mapping = tensor_dist_attr.dims_mapping();
-  //auto process_mesh_shape = process_mesh_attr.shape();
+  const auto& process_mesh_shape = process_mesh_attr.shape();
    PADDLE_ENFORCE_EQ(static_cast<int>(dims_mapping.size()),
                     global_dims.size(),
                     common::errors::PreconditionNotMet(
