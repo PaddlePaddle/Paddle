f3f52637da2222ec5b091f38140113c5eb799de6
lizexu <2694294196@qq.com>
2024-04-29 12:09:45 +0000

    单测改了float16

--- test/ir/pir/fused_pass/test_pir_fused_rotary_position_embedding_pass.py
+++ test/ir/pir/fused_pass/test_pir_fused_rotary_position_embedding_pass.py
@@ -101,25 +101,25 @@ slice slice                    \                   squeeze       unsqueeze      
                                     main_prog, start_prog
                                 ):
                                     q = paddle.static.data(
-                                        name="q", shape=q_shape, dtype='float32'
+                                        name="q", shape=q_shape, dtype='float16'
                                     )
                                     k = paddle.static.data(
-                                        name="k", shape=k_shape, dtype='float32'
+                                        name="k", shape=k_shape, dtype='float16'
                                     )
                                     cos = paddle.static.data(
                                         name="cos",
                                         shape=cos_shape,
-                                        dtype='float32',
+                                        dtype='float16',
                                     )
                                     sin = paddle.static.data(
                                         name="sin",
                                         shape=sin_shape,
-                                        dtype='float32',
+                                        dtype='float16',
                                     )
                                     position_ids = paddle.static.data(
                                         name="position_ids",
                                         shape=position_ids_shape,
-                                        dtype='int32',
+                                        dtype='int64',
                                     )
                                     cos = cos.squeeze(axis=[0, 2])
                                     sin = sin.squeeze(axis=[0, 2])
@@ -143,34 +143,35 @@ slice slice                    \                   squeeze       unsqueeze      
                                     self.pass_list = [
                                         'fused_rotary_position_embedding_pass'
                                     ]
+                                    # print("paddle.get_default_dtype()",paddle.get_default_dtype())
                                     self.feeds = {
                                         "q": np.random.random(q_shape).astype(
-                                            paddle.get_default_dtype()
+                                            'float16'
                                         ),
                                         "k": np.random.random(k_shape).astype(
-                                            paddle.get_default_dtype()
+                                            'float16'
                                         ),
                                         "cos": np.random.random(
                                             cos_shape
-                                        ).astype(paddle.get_default_dtype()),
+                                        ).astype('float16'),
                                         "sin": np.random.random(
                                             sin_shape
-                                        ).astype(paddle.get_default_dtype()),
+                                        ).astype('float16'),
                                         "position_ids": np.random.random(
                                             position_ids_shape
-                                        ).astype('int32'),
+                                        ).astype('int64'),
                                     }
                                     self.fetch_list = [q_embed, k_embed]
                                     self.valid_op_map = {
                                         "pd_op.squeeze": 0,
                                         "pd_op.unsqueeze": 0,
                                         "pd_op.concat": 0,
-                                        "pd_op.multiply": 0,
                                         "pd_op.full": 0,
                                         "pd_op.full_int_array": 0,
                                         "pd_op.add": 0,
                                         "pd_op.slice": 0,
                                         "pd_op.scale": 0,
+                                        "pd_op.multiply": 0,
                                         "builtin.combine": 0,
                                         "pd_op.gather_nd": 0,
                                         "pd_op.fused_rotary_position_embedding": 1,
