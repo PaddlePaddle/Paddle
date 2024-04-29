7091c846f883e028be7119fa87b38a14b21e063e
Xiao Xiyuan <945428667@qq.com>
2024-04-29 11:09:09 +0800

    Update python/paddle/nn/functional/flash_attention.py
    Co-authored-by: zachary sun <70642955+sunzhongkai588@users.noreply.github.com>

--- python/paddle/nn/functional/flash_attention.py
+++ python/paddle/nn/functional/flash_attention.py
@@ -342,8 +342,8 @@ def flash_attn_qkvpacked(
                         :ref:`api_guide_Name`.
 
     Returns:
-        out(Tensor): The attention tensor. 4-D tensor with shape: [batch_size, seq_len, num_heads, head_dim]. The dtype can be float16 or bfloat16.
-        softmax(Tensor): The softmax tensor. None if return_softmax is False.
+        - out(Tensor). The attention tensor. 4-D tensor with shape: [batch_size, seq_len, num_heads, head_dim]. The dtype can be float16 or bfloat16.
+        - softmax(Tensor). The softmax tensor. None if return_softmax is False.
 
     Examples:
         .. code-block:: python
