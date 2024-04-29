80d1bab423da7523e64c9cd51a4ffc0171730dd9
co63oc <co63oc@users.noreply.github.com>
2024-04-29 16:52:32 +0800

    Fix

--- paddle/fluid/eager/utils.h
+++ paddle/fluid/eager/utils.h
@@ -149,6 +149,8 @@ class TEST_API EagerUtils {
   static std::vector<AutogradMeta*> nullable_autograd_meta(
       const std::vector<paddle::Tensor>& targets);
   static std::vector<AutogradMeta*> nullable_autograd_meta(
+      const paddle::optional<std::vector<paddle::Tensor>>& targets);
+  static std::vector<AutogradMeta*> nullable_autograd_meta(
       const std::vector<paddle::Tensor*>& targets);
   static std::vector<AutogradMeta*> nullable_autograd_meta(
       const paddle::optional<std::vector<paddle::Tensor*>>& targets);
