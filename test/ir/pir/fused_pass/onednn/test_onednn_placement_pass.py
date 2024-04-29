7882788a2a9a6e0aa7c6e3ce86f6657a131527f5
zhanglirong1999 <1695074375@qq.com>
2024-04-29 16:56:47 +0800

    delete sys

--- test/ir/pir/fused_pass/onednn/test_onednn_placement_pass.py
+++ test/ir/pir/fused_pass/onednn/test_onednn_placement_pass.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import unittest
-import sys
 
-sys.path.append("../")
 import numpy as np
 from pass_test import PassTest
 
