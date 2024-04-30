e0cae958e55966dfe21c2187ff8bf2dbb8539928
co63oc <co63@163.com>
2024-04-30 21:15:17 +0800

    Fix

--- paddle/phi/kernels/crf_decoding_kernel.h
+++ paddle/phi/kernels/crf_decoding_kernel.h
@@ -16,51 +16,16 @@
 #include <limits>
 
 #include "paddle/phi/core/dense_tensor.h"
-#include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/funcs/eigen/common.h"
-#include "paddle/phi/kernels/funcs/jit/kernels.h"
-#include "paddle/phi/kernels/funcs/math_function.h"
+#include "paddle/utils/optional.h"
 
 namespace phi {
 
 template <typename T, typename Context>
-void Decode(const Context& dev_ctx,
-            const phi::DenseTensor& emission_weights,
-            const phi::DenseTensor& transition_weights,
-            phi::DenseTensor* decoded_path) {
-  auto emission_dims = emission_weights.dims();
-  const size_t seq_len = emission_dims[0];
-  const size_t tag_num = emission_dims[1];
-  const T* x = emission_weights.data<T>();
-  const T* w = transition_weights.data<T>();
-  int64_t* path = decoded_path->data<int64_t>();
-
-  // alpha is a memo table. An element alpha(k, v) records the score of the
-  // best sequence of tags from position 1 to position k with v being the end
-  // tag.
-  phi::DenseTensor alpha;
-  alpha.Resize(emission_dims);
-  T* alpha_value = dev_ctx.template Alloc<T>(&alpha);
-  phi::DenseTensor track;
-  track.Resize(emission_dims);
-  int* track_value = dev_ctx.template Alloc<int>(&track);
-  auto ker = phi::jit::KernelFuncs<phi::jit::CRFDecodingTuple<T>,
-                                   phi::CPUPlace>::Cache()
-                 .At(tag_num);
-  ker(static_cast<int>(seq_len), x, w, alpha_value, track_value, tag_num);
-  T max_score = -std::numeric_limits<T>::max();
-  int max_i = 0;
-  for (size_t i = 0; i < tag_num; ++i) {
-    T score = alpha_value[(seq_len - 1) * tag_num + i] + w[tag_num + i];
-    if (score > max_score) {
-      max_score = score;
-      max_i = i;
-    }
-  }
-  path[seq_len - 1] = max_i;
-  for (int k = seq_len - 1; k >= 1; --k) {
-    path[k - 1] = max_i = track_value[k * tag_num + max_i];
-  }
-}
+void CRFDecodingOpKernel(const Context& dev_ctx,
+                         const DenseTensor& emission,
+                         const DenseTensor& transition,
+                         const paddle::optional<DenseTensor>& label,
+                         const paddle::optional<DenseTensor>& length,
+                         DenseTensor* viterbi_path);
 
 }  // namespace phi
