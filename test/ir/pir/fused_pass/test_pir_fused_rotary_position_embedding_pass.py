129f8dc817c8b8856d881c5ba85010f7786d1830
lizexu <2694294196@qq.com>
2024-04-30 09:57:40 +0000

    单测还是报错,哎

--- test/ir/pir/fused_pass/test_pir_fused_rotary_position_embedding_pass.py
+++ test/ir/pir/fused_pass/test_pir_fused_rotary_position_embedding_pass.py
@@ -22,6 +22,8 @@ from paddle.base import core
 
 paddle.enable_static()
 
+position_ids_list = [[7, 5, 4, 6, 3, 1, 2, 0], [3, 1, 4, 0, 7, 6, 5, 2]]
+
 
 class TestFusedRotaryPositionEmbeddingPass(PassTest):
     r"""
@@ -82,11 +84,84 @@ slice slice                    \                   squeeze       unsqueeze      
     def is_program_valid(self, program=None):
         return True
 
-    def rotate_half(x):
+    def rotate_half(self, x):
         """Rotates half the hidden dims of the input."""
         x1 = x[..., : x.shape[-1] // 2]
         x2 = x[..., x.shape[-1] // 2 :]
         return paddle.concat([-x2, x1], axis=-1)  # shape is the same as x
+
+    def deal_qkv(self, init_value):
+        if init_value is None:
+            return None
+        perm = [0, 2, 1, 3]
+        return paddle.transpose(x=init_value, perm=perm)
+
+    def mult_qkv(self, value, cos_tensor, sin_tensor):
+        if value is None:
+            return None
+        rotate_half_q = paddle.reshape(
+            paddle.stack(
+                [-value[:, :, :, 1::2], value[:, :, :, 0::2]], axis=-1
+            ),
+            paddle.shape(value),
+        )
+        query = paddle.add(
+            paddle.multiply(value, cos_tensor),
+            paddle.multiply(rotate_half_q, sin_tensor),
+        )
+        return query
+
+    def multi_qkv_rotate_half(self, value, cos_tensor, sin_tensor):
+        if value is None:
+            return None
+        rotate_half_q = paddle.reshape(
+            paddle.concat(
+                [
+                    -value[..., value.shape[-1] // 2 :],
+                    value[..., : value.shape[-1] // 2],
+                ],
+                axis=-1,
+            ),
+            paddle.shape(value),
+        )
+        query = paddle.add(
+            paddle.multiply(value, cos_tensor),
+            paddle.multiply(rotate_half_q, sin_tensor),
+        )
+        return query
+
+    def get_sin_cos_tensor(self, seq_len, head_dim, sign=1):
+        pos_seq = np.arange(0, seq_len, 1, dtype=np.float16)
+        indices = np.arange(0, head_dim, 2, dtype=np.float16)
+
+        indices = 1 / 10000 ** (indices / head_dim)
+        sinusoid_inp = (
+            pos_seq[:, np.newaxis] * indices[np.newaxis, :]
+        )  # 使用 np.newaxis 增加维度
+        sin_sin = np.empty((seq_len * head_dim), dtype=np.float16)
+        cos_cos = np.empty((seq_len * head_dim), dtype=np.float16)
+        numpy_array = sinusoid_inp
+        print("numpy_array:", numpy_array)
+        iter_array = np.nditer(numpy_array)
+
+        i = 0
+
+        for value in iter_array:
+            sin_sin[i * 2] = sign * np.sin(value)
+            cos_cos[i * 2 + 0] = np.cos(value)
+            sin_sin[i * 2 + 1] = np.sin(value)
+            cos_cos[i * 2 + 1] = np.cos(value)
+            i += 1
+        tensor_sin = sin_sin.reshape(1, seq_len, 1, head_dim)
+        tensor_cos = cos_cos.reshape(1, seq_len, 1, head_dim)
+
+        return tensor_sin, tensor_cos
+
+    def get_numpy_tensor(self, shape, dtype):
+        if shape is None:
+            return None
+        # 使用 numpy 创建随机数组，并指定数据类型
+        return np.random.randn(*shape).astype(dtype)
 
     def sample_program(self):
         for q_shape in [[2, 8, 2, 16]]:
@@ -122,66 +197,71 @@ slice slice                    \                   squeeze       unsqueeze      
                                         dtype='int64',
                                     )
 
-                                    # perm=[0,2,1,3]
-                                    # q=paddle.transpose(x=q,perm=perm)
-                                    # k=paddle.transpose(x=k,perm=perm)
-
-                                    # print("cos",cos)
-                                    # cos_tensor=cos.squeeze(axis=[0, 2])
-                                    # sin_tensor=sin.squeeze(axis=[0, 2])
-                                    # print("sin_tensor", sin_tensor.shape)
-
-                                    # sin_tensor=sin_tensor[position_ids].unsqueeze(2)
-                                    # cos_tensor=cos_tensor[position_ids].unsqueeze(2)
-                                    # print("cos_tensor", cos_tensor.shape)
-
-                                    # perm=[0,2,1,3]
-                                    # sin_tensor=paddle.transpose(x=sin_tensor,perm=perm)
-                                    # cos_tensor=paddle.transpose(x=cos_tensor,perm=perm)
-                                    # print("sin_tensor的shape",sin_tensor.shape)
-                                    # print("cos_tensor的shape", cos_tensor.shape)
-
                                     cos = cos.squeeze(axis=[0, 2])
                                     sin = sin.squeeze(axis=[0, 2])
                                     cos = cos[position_ids].unsqueeze(2)
                                     sin = sin[position_ids].unsqueeze(2)
 
                                     q_embed = (q * cos) + (
-                                        TestFusedRotaryPositionEmbeddingPass.rotate_half(
-                                            q
-                                        )
-                                        * sin
+                                        self.rotate_half(q) * sin
                                     )
                                     k_embed = (k * cos) + (
-                                        TestFusedRotaryPositionEmbeddingPass.rotate_half(
-                                            k
-                                        )
-                                        * sin
+                                        self.rotate_half(k) * sin
                                     )
+                                    print("q_embed", q_embed)
+                                    print("k_embed", k_embed)
                                     out1 = paddle.assign(q_embed)
                                     out2 = paddle.assign(k_embed)
+
+                                    position_ids_values = np.array(
+                                        [
+                                            [7, 5, 4, 6, 3, 1, 2, 0],
+                                            [3, 1, 4, 0, 7, 6, 5, 2],
+                                        ],
+                                        dtype='int64',
+                                    )
+                                    print(
+                                        "positon_ids_values",
+                                        position_ids_values,
+                                    )
+
                                     # print("q_embed", q_embed)
                                     # print("k_embed", k_embed)
                                     self.pass_list = [
                                         'fused_rotary_position_embedding_pass'
                                     ]
+                                    tensor_q = self.get_numpy_tensor(
+                                        q_shape, 'float16'
+                                    )
+                                    tensor_k = self.get_numpy_tensor(
+                                        k_shape, 'float16'
+                                    )
+                                    print("tensor_q", tensor_q)
+                                    print("tensor_k", tensor_k)
+                                    (
+                                        tensor_sin,
+                                        tensor_cos,
+                                    ) = self.get_sin_cos_tensor(
+                                        tensor_q.shape[1], tensor_q.shape[3]
+                                    )
+                                    print("tensor_sin", tensor_sin)
+                                    print("tensor_cos", tensor_cos)
+                                    position_ids_value = paddle.to_tensor(
+                                        position_ids_list, dtype='int64'
+                                    )
+                                    print(
+                                        "已转换 position_ids 为张量:",
+                                        position_ids_value,
+                                    )
+                                    # position_ids_data = np.array([[7, 5, 4, 6, 3, 1, 2, 0], [3, 1, 4, 0, 7, 6, 5, 2]], dtype='int64')
+
                                     # print("paddle.get_default_dtype()",paddle.get_default_dtype())
                                     self.feeds = {
-                                        "q": np.random.random(q_shape).astype(
-                                            'float16'
-                                        ),
-                                        "k": np.random.random(k_shape).astype(
-                                            'float16'
-                                        ),
-                                        "cos": np.random.random(
-                                            cos_shape
-                                        ).astype('float16'),
-                                        "sin": np.random.random(
-                                            sin_shape
-                                        ).astype('float16'),
-                                        "position_ids": np.random.random(
-                                            position_ids_shape
-                                        ).astype('int64'),
+                                        "q": tensor_q,
+                                        "k": tensor_k,
+                                        "cos": tensor_cos,
+                                        "sin": tensor_sin,
+                                        "position_ids": position_ids_values,
                                     }
                                     self.fetch_list = [out1, out2]
                                     self.valid_op_map = {
@@ -189,8 +269,6 @@ slice slice                    \                   squeeze       unsqueeze      
                                         "pd_op.unsqueeze": 0,
                                         "pd_op.concat": 0,
                                         "pd_op.multiply": 0,
-                                        "pd_op.full": 0,
-                                        "pd_op.full_int_array": 0,
                                         "pd_op.add": 0,
                                         "pd_op.slice": 0,
                                         "pd_op.scale": 0,
