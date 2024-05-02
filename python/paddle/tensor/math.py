ce613b2488bcea03aa2d72eaa7932071b6f881d3
megemini <megemini@outlook.com>
2024-05-02 22:31:33 +0800

    change math.py type annotation

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -214,7 +214,9 @@ def log_(x, name=None):
         return _C_ops.log_(x)
 
 
-def scale(x, scale=1.0, bias=0.0, bias_after_scale=True, act=None, name=None):
+def scale(
+    x, scale: int = 1, bias=0.0, bias_after_scale=True, act=None, name=None
+):
     """
     Scale operator.
 
