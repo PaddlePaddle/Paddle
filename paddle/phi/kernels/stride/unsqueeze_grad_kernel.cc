1e5b07696fc7e700b008d4e8add68208ab548a06
ApricityXX <1470600398@qq.com>
2024-05-02 23:33:48 +0800

    5.2

--- paddle/phi/kernels/stride/unsqueeze_grad_kernel.cc
+++ paddle/phi/kernels/stride/unsqueeze_grad_kernel.cc
@@ -23,7 +23,7 @@ void UnsqueezeGradStridedKernel(const Context& dev_ctx,
                                 const DenseTensor& x_shape,
                                 const DenseTensor& dout,
                                 DenseTensor* dx) {
-  auto xshape_dims = x_shape.dims();
+  const auto& xshape_dims = x_shape.dims();
   auto x_dims = common::slice_ddim(xshape_dims, 1, xshape_dims.size());
   ReshapeStridedKernel<Context>(
       dev_ctx, dout, IntArray(common::vectorize<int64_t>(x_dims)), dx, nullptr);
