6cf1b06286d2903c6f86facdad97acbd30ff0a4e
co63oc <co63@163.com>
2024-05-07 19:04:57 +0800

    Fix

--- paddle/phi/kernels/legacy/cpu/fused_elementwise_kernel.cc
+++ paddle/phi/kernels/legacy/cpu/fused_elementwise_kernel.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/api/ext/dispatch.h"
 #include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/common/complex.h"
