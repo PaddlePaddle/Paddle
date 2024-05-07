04ba90f4fa8ff9f085b8da5113722d91ec13acc0
walk_alone <2390335608@qq.com>
2024-05-07 20:07:43 +0800

    minor changes

--- paddle/phi/api/profiler/device_tracer.cc
+++ paddle/phi/api/profiler/device_tracer.cc
@@ -318,7 +318,7 @@ void initCuptiCbidStr();
 class DeviceTracerImpl : public DeviceTracer {
  public:
   DeviceTracerImpl()
-      : subscriber_{}, enabled_(false), start_ns_(0), end_ns_(0) {
+      : enabled_(false), start_ns_(0), end_ns_(0) {
 #ifdef PADDLE_WITH_CUPTI
     initCuptiCbidStr();
 #endif
