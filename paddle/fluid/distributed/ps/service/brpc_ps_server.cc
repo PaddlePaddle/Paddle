3424b3a32b3b26792afb81424576b60b74f2a8d1
ApricityXX <1470600398@qq.com>
2024-05-07 17:07:15 +0800

    5.7

--- paddle/fluid/distributed/ps/service/brpc_ps_server.cc
+++ paddle/fluid/distributed/ps/service/brpc_ps_server.cc
@@ -15,7 +15,6 @@
 #include "paddle/fluid/distributed/ps/service/brpc_ps_server.h"
 
 #include <thread>  // NOLINT
-
 #include "butil/object_pool.h"
 #include "paddle/fluid/distributed/common/cost_timer.h"
 #include "paddle/fluid/distributed/ps/table/depends/sparse_utils.h"
