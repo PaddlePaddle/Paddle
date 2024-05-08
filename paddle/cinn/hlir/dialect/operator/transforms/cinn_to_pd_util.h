6fa3f8e99e7d5ab12b4a4c0718b4140eda2008f3
zyfncg <zhangyunfei07@baidu.com>
2024-05-08 08:53:15 +0000

    add shape_ops_fallback_to_phi_pass

--- paddle/cinn/hlir/dialect/operator/transforms/cinn_to_pd_util.h
+++ paddle/cinn/hlir/dialect/operator/transforms/cinn_to_pd_util.h
@@ -20,6 +20,7 @@
 #include "paddle/common/enforce.h"
 
 namespace pir {
+class IrMapping;
 class Block;
 class Operation;
 class Builder;
@@ -27,8 +28,8 @@ class Builder;
 
 namespace cinn::dialect::details {
 
-using TRule =
-    std::function<::pir::Operation*(::pir::Operation*, const ::pir::Builder&)>;
+using TRule = std::function<::pir::Operation*(
+    ::pir::Operation*, ::pir::IrMapping&, ::pir::Builder&)>;
 
 class TransformContext {
  private:
@@ -86,6 +87,8 @@ class TransformRegistrar {
 
 void RewriteCinnOpToPdOp(const ::pir::Block& src_block,
                          ::pir::Block* target_block);
-::pir::Operation* RewriteCinnOpToPdOp(::pir::Operation*, const ::pir::Builder&);
+::pir::Operation* RewriteCinnOpToPdOp(::pir::Operation*,
+                                      ::pir::IrMapping&,  // NOLINT
+                                      ::pir::Builder&);   // NOLINT
 
 }  // namespace cinn::dialect::details
