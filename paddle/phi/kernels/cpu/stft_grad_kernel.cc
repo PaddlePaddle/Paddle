faed8c15421f2ccfe5ee2101173308569ac07b1e
co63oc <co63@163.com>
2024-05-10 09:08:08 +0800

    Fix

--- paddle/phi/kernels/cpu/stft_grad_kernel.cc
+++ paddle/phi/kernels/cpu/stft_grad_kernel.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "paddle/phi/backends/cpu/cpu_context.h"
+#include "paddle/phi/common/type_traits.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/impl/stft_grad_kernel_impl.h"
 #include "paddle/phi/kernels/stft_kernel.h"
--- paddle/phi/kernels/gpu/stft_grad_kernel.cu
+++ paddle/phi/kernels/gpu/stft_grad_kernel.cu
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "paddle/phi/common/type_traits.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/impl/stft_grad_kernel_impl.h"
 #include "paddle/phi/kernels/stft_kernel.h"
