c3612f33907fea82e83ba3a38fc0ba441d776485
6clc <chaoliu.lc@foxmail.com>
2024-05-10 15:23:24 +0800

    cinn(backend): remove no use header file

--- paddle/cinn/ir/group_schedule/dy_shape_group_scheduler.cc
+++ paddle/cinn/ir/group_schedule/dy_shape_group_scheduler.cc
@@ -15,8 +15,6 @@
 #include "paddle/cinn/ir/group_schedule/dy_shape_group_scheduler.h"
 #include "paddle/cinn/common/cas.h"
 #include "paddle/cinn/ir/group_schedule/tactic/compute_inline_tactic.h"
-#include "paddle/cinn/ir/group_schedule/tactic/loop_reorder_alignment_tactic.h"
-#include "paddle/cinn/ir/group_schedule/tactic/optimize_reduction_tactic.h"
 #include "paddle/cinn/ir/group_schedule/tactic/tile_first_general_tactic.h"
 #include "paddle/cinn/ir/ir_analyzer/ir_analyzer.h"
 #include "paddle/cinn/ir/op/ir_operators.h"
