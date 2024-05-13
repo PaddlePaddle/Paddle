b8d83e209b7c3c0b6d8c86f23d607538bd479b7e
xingmingyyj <135400902+xingmingyyj@users.noreply.github.com>
2024-05-13 15:33:57 +0800

    Update op_compat_gen.py

--- paddle/fluid/ir_adaptor/translator/op_compat_gen.py
+++ paddle/fluid/ir_adaptor/translator/op_compat_gen.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import argparse
 from pathlib import Path
 from typing import Dict, List, Set
