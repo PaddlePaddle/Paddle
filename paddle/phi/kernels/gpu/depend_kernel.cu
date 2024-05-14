7783587bb84a016700114c42c8abec573e00300f
co63oc <co63@163.com>
2024-05-14 14:25:03 +0800

    Fix

--- paddle/phi/kernels/gpu/depend_kernel.cu
+++ paddle/phi/kernels/gpu/depend_kernel.cu
@@ -25,5 +25,6 @@ PD_REGISTER_KERNEL(depend,
                    double,
                    int,
                    int64_t,
+                   phi::dtype::float16,
                    phi::dtype::complex<float>,
                    phi::dtype::complex<double>) {}
