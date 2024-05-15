d1bb64df846da781064fd8ebfa90d8d3953ffc9e
Zhan Rongrui <2742392377@qq.com>
2024-05-15 06:44:54 +0000

    add test

---
+++ test/sot/test_dynamic_shape.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import os
+import unittest
+
+from test_case_base import (
+    TestCaseBase,
+    test_instruction_translator_cache_context,
+)
+
+import paddle
+
+
+def foo(x):
+    s = x.shape[0]
+    return x + s
+
+
+def bar(x, n):
+    return x + n
+
+
+class TestOpcodeExecutorDynamicShapeCache(TestCaseBase):
+    def test_dynamic_int_input_cache_hit(self):
+        os.environ["SOT_ALLOW_DYNAMIC_SHAPE"] = "True"
+        with test_instruction_translator_cache_context() as ctx:
+            self.assert_results(bar, paddle.randn([2, 3]), 1)
+            self.assertEqual(ctx.translate_count, 1)
+            self.assert_results(bar, paddle.randn([2, 3]), 2)
+            self.assertEqual(ctx.translate_count, 2)
+            self.assert_results(bar, paddle.randn([2, 3]), 3)
+            self.assertEqual(ctx.translate_count, 2)
+            self.assert_results(bar, paddle.randn([2, 3]), 4)
+            self.assertEqual(ctx.translate_count, 2)
+            self.assert_results(bar, paddle.randn([2, 3]), 5)
+            self.assertEqual(ctx.translate_count, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
