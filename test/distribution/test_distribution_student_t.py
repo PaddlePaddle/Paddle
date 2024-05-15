de6932fcb8e88d7cd1c975856c0994aa7b8f6d52
AyaseNana <13659110308@163.com>
2024-05-15 11:26:55 +0800

    fix test coverage

--- test/distribution/test_distribution_student_t.py
+++ test/distribution/test_distribution_student_t.py
@@ -29,9 +29,9 @@ from paddle.distribution.student_t import StudentT
     [
         (
             'one-dim',
-            np.array(10.0).astype("float32"),
-            np.array(1.0).astype("float32"),
-            np.array(2.0).astype("float32"),
+            10.0,
+            1.0,
+            2.0,
         ),
         (
             'multi-dim',
@@ -49,40 +49,58 @@ from paddle.distribution.student_t import StudentT
 )
 class TestStudentT(unittest.TestCase):
     def setUp(self):
-        self._dist = StudentT(
-            df=paddle.to_tensor(self.df),
-            loc=paddle.to_tensor(self.loc),
-            scale=paddle.to_tensor(self.scale),
+        df = (
+            self.df if isinstance(self.df, float) else paddle.to_tensor(self.df)
         )
+        loc = (
+            self.loc
+            if isinstance(self.loc, float)
+            else paddle.to_tensor(self.loc)
+        )
+        scale = (
+            self.scale
+            if isinstance(self.scale, float)
+            else paddle.to_tensor(self.scale)
+        )
+        self._dist = StudentT(df, loc, scale)
 
     def test_mean(self):
         mean = self._dist.mean
-        self.assertEqual(mean.numpy().dtype, self.df.dtype)
+        target_dtype = (
+            "float32" if isinstance(self.df, float) else self.df.dtype
+        )
+        self.assertEqual(mean.numpy().dtype, target_dtype)
         np.testing.assert_allclose(
             mean,
             self._np_mean(),
-            rtol=config.RTOL.get(str(self.df.dtype)),
-            atol=config.ATOL.get(str(self.df.dtype)),
+            rtol=config.RTOL.get(str(target_dtype)),
+            atol=config.ATOL.get(str(target_dtype)),
         )
 
     def test_variance(self):
         var = self._dist.variance
-        self.assertEqual(var.numpy().dtype, self.df.dtype)
+        target_dtype = (
+            "float32" if isinstance(self.df, float) else self.df.dtype
+        )
+        self.assertEqual(var.numpy().dtype, target_dtype)
         np.testing.assert_allclose(
             var,
             self._np_variance(),
-            rtol=config.RTOL.get(str(self.df.dtype)),
-            atol=config.ATOL.get(str(self.df.dtype)),
+            rtol=config.RTOL.get(str(target_dtype)),
+            atol=config.ATOL.get(str(target_dtype)),
         )
 
     def test_entropy(self):
         entropy = self._dist.entropy()
-        self.assertEqual(entropy.numpy().dtype, self.df.dtype)
+        target_dtype = (
+            "float32" if isinstance(self.df, float) else self.df.dtype
+        )
+        self.assertEqual(entropy.numpy().dtype, target_dtype)
         np.testing.assert_allclose(
             entropy,
             self._np_entropy(),
-            rtol=config.RTOL.get(str(self.df.dtype)),
-            atol=config.ATOL.get(str(self.df.dtype)),
+            rtol=config.RTOL.get(str(target_dtype)),
+            atol=config.ATOL.get(str(target_dtype)),
         )
 
     def test_sample(self):
@@ -99,52 +117,61 @@ class TestStudentT(unittest.TestCase):
         sample_variance = samples.var(axis=0)
 
         np.testing.assert_allclose(
-            sample_mean, self._dist.mean, atol=0, rtol=0.20
+            sample_mean, self._dist.mean, atol=0, rtol=0.30
         )
         np.testing.assert_allclose(
-            sample_variance, self._dist.variance, atol=0, rtol=0.20
+            sample_variance, self._dist.variance, atol=0, rtol=0.30
         )
 
     def _np_variance(self):
-        if self.df.dtype == np.float32:
+        if isinstance(self.df, np.ndarray) and self.df.dtype == np.float32:
             df = self.df.astype("float64")
         else:
             df = self.df
-        if self.loc.dtype == np.float32:
+        if isinstance(self.loc, np.ndarray) and self.loc.dtype == np.float32:
             loc = self.loc.astype("float64")
         else:
             loc = self.loc
-        if self.scale.dtype == np.float32:
+        if (
+            isinstance(self.scale, np.ndarray)
+            and self.scale.dtype == np.float32
+        ):
             scale = self.scale.astype("float64")
         else:
             scale = self.scale
         return scipy.stats.t.var(df, loc, scale)
 
     def _np_mean(self):
-        if self.df.dtype == np.float32:
+        if isinstance(self.df, np.ndarray) and self.df.dtype == np.float32:
             df = self.df.astype("float64")
         else:
             df = self.df
-        if self.loc.dtype == np.float32:
+        if isinstance(self.loc, np.ndarray) and self.loc.dtype == np.float32:
             loc = self.loc.astype("float64")
         else:
             loc = self.loc
-        if self.scale.dtype == np.float32:
+        if (
+            isinstance(self.scale, np.ndarray)
+            and self.scale.dtype == np.float32
+        ):
             scale = self.scale.astype("float64")
         else:
             scale = self.scale
         return scipy.stats.t.mean(df, loc, scale)
 
     def _np_entropy(self):
-        if self.df.dtype == np.float32:
+        if isinstance(self.df, np.ndarray) and self.df.dtype == np.float32:
             df = self.df.astype("float64")
         else:
             df = self.df
-        if self.loc.dtype == np.float32:
+        if isinstance(self.loc, np.ndarray) and self.loc.dtype == np.float32:
             loc = self.loc.astype("float64")
         else:
             loc = self.loc
-        if self.scale.dtype == np.float32:
+        if (
+            isinstance(self.scale, np.ndarray)
+            and self.scale.dtype == np.float32
+        ):
             scale = self.scale.astype("float64")
         else:
             scale = self.scale
@@ -157,9 +184,9 @@ class TestStudentT(unittest.TestCase):
     [
         (
             'one-dim',
-            np.array(10.0).astype("float32"),
-            np.array(0.0).astype("float32"),
-            np.array(1.0).astype("float32"),
+            10.0,
+            0.0,
+            1.0,
             np.array(3.3).astype("float32"),
         ),
         (
@@ -180,26 +207,41 @@ class TestStudentT(unittest.TestCase):
 )
 class TestStudentTProbs(unittest.TestCase):
     def setUp(self):
-        self._dist = StudentT(
-            df=paddle.to_tensor(self.df),
-            loc=paddle.to_tensor(self.loc),
-            scale=paddle.to_tensor(self.scale),
+        df = (
+            self.df if isinstance(self.df, float) else paddle.to_tensor(self.df)
         )
+        loc = (
+            self.loc
+            if isinstance(self.loc, float)
+            else paddle.to_tensor(self.loc)
+        )
+        scale = (
+            self.scale
+            if isinstance(self.scale, float)
+            else paddle.to_tensor(self.scale)
+        )
+        self._dist = StudentT(df, loc, scale)
 
     def test_prob(self):
+        target_dtype = (
+            "float32" if isinstance(self.df, float) else self.df.dtype
+        )
         np.testing.assert_allclose(
             self._dist.prob(paddle.to_tensor(self.value)),
             scipy.stats.t.pdf(self.value, self.df, self.loc, self.scale),
-            rtol=config.RTOL.get(str(self.df.dtype)),
-            atol=config.ATOL.get(str(self.df.dtype)),
+            rtol=config.RTOL.get(str(target_dtype)),
+            atol=config.ATOL.get(str(target_dtype)),
         )
 
     def test_log_prob(self):
+        target_dtype = (
+            "float32" if isinstance(self.df, float) else self.df.dtype
+        )
         np.testing.assert_allclose(
             self._dist.log_prob(paddle.to_tensor(self.value)),
             scipy.stats.t.logpdf(self.value, self.df, self.loc, self.scale),
-            rtol=config.RTOL.get(str(self.df.dtype)),
-            atol=config.ATOL.get(str(self.df.dtype)),
+            rtol=config.RTOL.get(str(target_dtype)),
+            atol=config.ATOL.get(str(target_dtype)),
         )
 
 
