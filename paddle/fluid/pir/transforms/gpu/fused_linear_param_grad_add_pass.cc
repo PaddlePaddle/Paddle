d948b25a4eaf5fef92e4525e2071a37a16433996
zhangbo9674 <zhangbo54@baidu.com>
2024-05-15 06:45:07 +0000

    add

--- paddle/fluid/pir/transforms/gpu/fused_linear_param_grad_add_pass.cc
+++ paddle/fluid/pir/transforms/gpu/fused_linear_param_grad_add_pass.cc
@@ -416,65 +416,6 @@ class FusedMatmulAddGradAddbPattern : public paddle::drr::DrrPatternBase {
         {&res.Tensor("dweight_out"), &res.Tensor("dbias")});
   }
 };
-
-class FusedAllReduceSplitPattern : public paddle::drr::DrrPatternBase {
- public:
-  std::string name() const override { return "FusedAllReduceSplitPattern"; }
-
-  void operator()(paddle::drr::DrrPatternContext *ctx) const override {
-    paddle::drr::SourcePattern pat = ctx->SourcePattern();
-
-    const auto &matmul = pat.Op(paddle::dialect::MatmulOp::name(),
-                                {{"transpose_x", pat.Attr("trans_x")},
-                                 {"transpose_y", pat.Attr("trans_y")}});
-    const auto &c_allreduce_sum_ =
-        pat.Op(paddle::dialect::CAllreduceSum_Op::name(),
-               {{"ring_id", pat.Attr("ring_id")},
-                {"use_calc_stream", pat.Attr("use_calc_stream")}});
-    const auto &assign = pat.Op(paddle::dialect::AssignOp::name());
-    const auto &full = pat.Op(paddle::dialect::FullOp::name());
-    const auto &split_with_num = pat.Op(paddle::dialect::SplitWithNumOp::name(),
-                                        {{"num", pat.Attr("num")}});
-    const auto &builtin_slice =
-        pat.Op(pir::SliceOp::name(), {{"index", pat.Attr("index")}});
-
-    pat.Tensor("input_grad_partial") =
-        matmul(pat.Tensor("out_grad"), pat.Tensor("weight"));
-    pat.Tensor("input_grad") =
-        c_allreduce_sum_(pat.Tensor("input_grad_partial"));
-    pat.Tensor("input_grad_tmp") = assign(pat.Tensor("input_grad"));
-    pat.Tensor("split_num") = full();
-    pat.Tensor("input_grad_group") =
-        split_with_num(pat.Tensor("input_grad_tmp"), pat.Tensor("split_num"));
-    pat.Tensor("out") = builtin_slice(pat.Tensor("input_grad_group"));
-
-    pat.AddConstraint([&](const paddle::drr::MatchContext &match_ctx) {
-      const auto &x_trans = match_ctx.Attr<bool>("trans_x");
-      const auto &y_trans = match_ctx.Attr<bool>("trans_y");
-      auto input_grad_partial_count =
-          match_ctx.Tensor("input_grad_partial").use_count();
-      auto input_grad_count = match_ctx.Tensor("input_grad").use_count();
-      auto input_grad_tmp_count =
-          match_ctx.Tensor("input_grad_tmp").use_count();
-      auto input_grad_group_count =
-          match_ctx.Tensor("input_grad_group").use_count();
-      return (x_trans == false && y_trans == true &&
-              input_grad_partial_count == 1 && input_grad_count == 1 &&
-              input_grad_tmp_count == 1 && input_grad_group_count == 1);
-    });
-
-    paddle::drr::ResultPattern res = pat.ResultPattern();
-
-    const auto &c_reducescatter =
-        res.Op(paddle::dialect::CReducescatterOp::name(),
-               {{"ring_id", pat.Attr("ring_id")},
-                {"nranks", pat.Attr("num")},
-                {"use_calc_stream", pat.Attr("use_calc_stream")}});
-
-    c_reducescatter({&res.Tensor("input_grad_partial")}, {&res.Tensor("out")});
-  }
-};
-
 class FusedLinearParamGradAddPass : public pir::PatternRewritePass {
  public:
   FusedLinearParamGradAddPass()
@@ -489,7 +430,6 @@ class FusedLinearParamGradAddPass : public pir::PatternRewritePass {
     ps.Add(paddle::drr::Create<FusedMatmulAddGradAddaPattern>(context));
     ps.Add(paddle::drr::Create<FusedMatmulAddGradAddbPattern>(context));
     ps.Add(paddle::drr::Create<FusedMatmulReshapeMatmulAddPattern>(context));
-    ps.Add(paddle::drr::Create<FusedAllReduceSplitPattern>(context));
 
     return ps;
   }
