39d680cc1bfa9412542b583c4184e428d8c759ce
东百月 <2681514899@qq.com>
2024-05-17 14:13:35 +0800

    [CINN][New Hardware Update] extend SplitCudaAndHostModule (#64345)
    * [CINN][New Hardware Update] rename SplitCudaAndHostModule
    
    * rename SplitCudaAndHostModule to SplitDeviceAndHostModule
    
    * [CINN][New Hardware Update] fix CMakeLists
    
    * [CINN][New Hardware Update] extend SplitDeviceAndHostModule
    
    * fix review

--- paddle/cinn/hlir/framework/pir/op_lowering_impl.cc
+++ paddle/cinn/hlir/framework/pir/op_lowering_impl.cc
@@ -18,7 +18,7 @@
 
 #include "paddle/cinn/adt/map_expr_ctx.h"
 #include "paddle/cinn/ast_gen_ius/tensor_group.h"
-#include "paddle/cinn/backends/codegen_cuda_util.h"
+#include "paddle/cinn/backends/codegen_device_util.h"
 #include "paddle/cinn/hlir/dialect/operator/ir/manual_op.h"
 #include "paddle/cinn/hlir/framework/compile_error.h"
 #include "paddle/cinn/hlir/framework/pir/op_lowering_util.h"
