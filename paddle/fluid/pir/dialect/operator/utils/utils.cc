facf708da822cb402c29a423f635205ade8cfad8
YuanRisheng <yuanrisheng@baidu.com>
2024-05-19 10:10:02 +0000

    fix bug

--- paddle/fluid/pir/dialect/operator/utils/utils.cc
+++ paddle/fluid/pir/dialect/operator/utils/utils.cc
@@ -84,10 +84,6 @@ const std::unordered_set<std::string> LegacyOpList = {
     LrnGradOp::name(),
     MovingAverageAbsMaxScaleOp::name(),
     MovingAverageAbsMaxScale_Op::name(),
-    QuantizeLinearOp::name(),
-    QuantizeLinear_Op::name(),
-    DequantizeLinearOp::name(),
-    DequantizeLinear_Op::name(),
 #ifdef PADDLE_WITH_DNNL
     paddle::onednn::dialect::LrnOp::name(),
     paddle::onednn::dialect::LrnGradOp::name(),
