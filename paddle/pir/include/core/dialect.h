1c8353cd64be2e0b2bd2c3599b5a6fda7b85fbf1
walk_alone <2390335608@qq.com>
2024-05-19 20:48:14 +0800

    minor changes

--- paddle/pir/include/core/dialect.h
+++ paddle/pir/include/core/dialect.h
@@ -103,7 +103,7 @@ class IR_API Dialect {
                                  ConcreteOp::interface_set(),
                                  ConcreteOp::GetTraitSet(),
                                  ConcreteOp::attributes_num,
-                                 ConcreteOp::attributes_name,
+                                 &CreateArrayLikeOp::attributes_name[0], 
                                  ConcreteOp::VerifySigInvariants,
                                  ConcreteOp::VerifyRegionInvariants);
   }
