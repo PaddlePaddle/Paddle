23bb19541f1d18a7be23cc461f5c99a319d1d2c1
jiawenxuan <jiawenxuan@baidu.com>
2024-05-24 08:33:56 +0000

    fix

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
@@ -446,7 +446,7 @@ bool MatmulOpInferSymbolicShape(pir::Operation *op,
   if (ndims_x >= 2 && ndims_y >= 2) {
     infer_context->AddEqualCstr(dims_x_new[ndims_x - 1],
                                 dims_y_new[ndims_y - 2]);
-    for (int i = 3; i <= ndims_x && i <= ndims_y; i++) {
+    for (auto i = 3; i <= ndims_x && i <= ndims_y; i++) {
       infer_context->AddBroadcastableCstr(dims_x_new[ndims_x - i],
                                           dims_y_new[ndims_y - i]);
     }
