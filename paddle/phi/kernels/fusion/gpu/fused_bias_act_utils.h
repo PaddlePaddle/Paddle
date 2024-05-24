13237adfd2489de992e40e5cdd7acb7e05170beb
co63oc <co63@163.com>
2024-05-25 05:56:17 +0800

    Fix

--- paddle/phi/kernels/fusion/gpu/fused_bias_act_utils.h
+++ paddle/phi/kernels/fusion/gpu/fused_bias_act_utils.h
@@ -33,7 +33,6 @@
 namespace phi {
 namespace fusion {
 
-#ifndef PADDLE_WITH_HIP
 template <typename T>
 struct GeluComputeType;
 
@@ -111,10 +110,15 @@ struct ReluFunctor {
 template <typename T>
 struct FastGeluFunctor {
   inline __device__ T operator()(const T x) const {
+#ifdef PADDLE_WITH_HIP
+    assert(0 && "ROCM does not support FastGelu");
+#else
     return phi::GeluFwd<T, true>(x);
+#endif
   }
 };
 
+#ifndef PADDLE_WITH_HIP
 inline cudaError_t GetNumBlocks(int64_t n, int *num_blocks) {
   constexpr int kBlockSize = 128;
   constexpr int kNumWaves = 16;
