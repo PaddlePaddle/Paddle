bc2ae196ef53c068cd31f4572bff84b8bd2462d3
ApricityXX <1470600398@qq.com>
2024-05-25 15:02:54 +0800

    5.25

--- paddle/fluid/distributed/test/ctr_accessor_test.cc
+++ paddle/fluid/distributed/test/ctr_accessor_test.cc
@@ -181,10 +181,17 @@ TEST(downpour_feature_value_accessor_test, test_update) {
     std::vector<float> embed_g2sum;
     std::vector<float> embedx_w;
     std::vector<float> embedx_g2sum;
-     DownpourSparseValueTest()
-        : slot(0.0f), unseen_days(0.0f), delta_score(0.0f), show(0.0f), click(0.0f), embed_w(0.0f),
-          embed_g2sum(1, 0.0f), embedx_w(8, 0.0f), embedx_g2sum(0, 0.0f) {
-    }
+    DownpourSparseValueTest()
+        : slot(0),
+          unseen_days(0),
+          delta_score(0),
+          show(0),
+          click(0),
+          embed_w(0),
+          embed_g2sum(),
+          embedx_w(),
+          embedx_g2sum() {}
+
 
     void to_array(float* ptr, size_t dim) {
       ptr[0] = slot;
@@ -214,9 +221,9 @@ TEST(downpour_feature_value_accessor_test, test_update) {
     float click;
     float embed_g;
     std::vector<float> embedx_g;
-     DownpourSparsePushValueTest()
-        : slot(0.0f), show(0.0f), click(0.0f), embed_g(0.0f), embedx_g(0, 0.0f) {
-    }
+    DownpourSparsePushValueTest()
+        : slot(0), show(0), click(0), embed_g(0), embedx_g() {}
+
   };
   std::vector<float*> exp_value;
   for (auto i = 0u; i < item_size; ++i) {
