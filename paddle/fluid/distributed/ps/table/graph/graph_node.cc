4cb932949656e315241cfd1aee8bb9dd0c34d46e
ApricityXX <1470600398@qq.com>
2024-05-28 13:23:15 +0800

    5.28

--- paddle/fluid/distributed/ps/table/graph/graph_node.cc
+++ paddle/fluid/distributed/ps/table/graph/graph_node.cc
@@ -77,7 +77,8 @@ void GraphNode::build_sampler(std::string sample_type) {
   if (sampler != nullptr) {
     sampler->build(edges);
   } else {
-    throw std::runtime_error("Failed to create a sampler of type: " + sample_type);
+    throw std::runtime_error("Failed to create a sampler of type: " +
+                             sample_type);
   }
 }
 void FeatureNode::to_buffer(char* buffer, bool need_feature) {
