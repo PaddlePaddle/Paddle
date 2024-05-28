54c5261d06d7e9b91c7ef967bcf9f4cbdf7bed52
walkalone20 <73780235+walkalone20@users.noreply.github.com>
2024-05-28 10:06:33 +0800

    【Hackathon 6th Fundable Projects 2 No.53】Fix readability-string-compare-final (#64522)
    * no string.compare
    
    * strcmp
    
    * minor changes
    
    * str cmp cond

--- paddle/fluid/pir/transforms/pd_op_to_kernel_pass.cc
+++ paddle/fluid/pir/transforms/pd_op_to_kernel_pass.cc
@@ -283,8 +283,8 @@ static phi::Backend DeriveBackend(const std::string& op,
                                   phi::Backend kernel_backend,
                                   size_t input_index) {
   // NOTE: Parameters are initialized on executor place defined
-  if ((op.compare(pir::SetParameterOp::name()) == 0 ||
-       op.compare(pir::ShadowOutputOp::name()) == 0) &&
+  if ((op == pir::SetParameterOp::name() ||
+       op == pir::ShadowOutputOp::name()) &&
       place.GetType() == phi::AllocationType::GPU) {
     return phi::TransToPhiBackend(place);
   }
