f6fbc621d7419f0a00ebf3b12d729beca70fab08
co63oc <co63@163.com>
2024-05-28 08:46:52 +0800

    Fix

--- paddle/phi/kernels/fusion/gpu/fused_multi_transformer_op.cu.h
+++ paddle/phi/kernels/fusion/gpu/fused_multi_transformer_op.cu.h
@@ -22,15 +22,6 @@ limitations under the License. */
 #include <fstream>
 #include <iomanip>
 
-#if defined(PADDLE_WITH_NCCL) || defined(PADDLE_WITH_RCCL)
-#include "paddle/fluid/distributed/collective/process_group_nccl.h"
-#include "paddle/fluid/platform/collective_helper.h"
-#include "paddle/fluid/platform/device/gpu/nccl_helper.h"
-#include "paddle/phi/core/distributed/nccl_comm_context.h"
-COMMON_DECLARE_bool(dynamic_static_unified_comm);
-#endif
-
-#include "paddle/fluid/operators/fused/fused_attention_utils.h"
 #include "paddle/phi/kernels/flash_attn_kernel.h"
 #include "paddle/phi/kernels/funcs/load_store_util.h"
 #include "paddle/phi/kernels/fusion/gpu/fused_bias_act_utils.h"
