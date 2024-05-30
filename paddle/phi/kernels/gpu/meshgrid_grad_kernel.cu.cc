90b0acefe756582ab50879da3621d3df3ded8b7f
AyaseNana <13659110308@163.com>
2024-05-30 10:50:52 +0800

    update expand py add complex

--- paddle/phi/kernels/gpu/meshgrid_grad_kernel.cu.cc
+++ paddle/phi/kernels/gpu/meshgrid_grad_kernel.cu.cc
@@ -27,4 +27,6 @@ PD_REGISTER_KERNEL(meshgrid_grad,
                    double,
                    int,
                    int64_t,
-                   phi::dtype::bfloat16) {}
+                   phi::dtype::bfloat16,
+                   phi::dtype::complex<float>,
+                   phi::dtype::complex<double>) {}
