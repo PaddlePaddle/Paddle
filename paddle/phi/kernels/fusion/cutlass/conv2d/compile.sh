253b4e796dd11dc2e1b95c3cdd334c255fbf5ebe
YKTian-x2b <2084984251@qq.com>
2024-05-31 03:51:45 +0000

    for case 80;86

--- paddle/phi/kernels/fusion/cutlass/conv2d/compile.sh
+++ paddle/phi/kernels/fusion/cutlass/conv2d/compile.sh
@@ -40,7 +40,7 @@ esac
 
 cutlass_repo_directory="cutlass"
 if [ ! -d "$cutlass_repo_directory" ]; then
-     git clone --branch v3.0.0  https://github.com/NVIDIA/cutlass
+    git clone --branch v3.0.0  https://github.com/NVIDIA/cutlass
 fi
 
 
