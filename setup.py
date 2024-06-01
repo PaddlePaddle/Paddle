cf7433498b0aaac2efab0bd55293cfafee6bf4b5
Silver Ling <silver.ling@outlook.com>
2024-06-01 09:45:06 +0000

    [HACKATHON 6th] rename phi_main to phi_core & phi_kernel_gpu to phi_gpu

--- setup.py
+++ setup.py
@@ -1040,7 +1040,7 @@ def get_package_data_and_package_dir():
         ]
         shutil.copy(env_dict.get("PHI_LIB"), libs_path)
         package_data['paddle.libs'] += [
-            ('libphi_main' if os.name != 'nt' else 'phi_main') + ext_suffix
+            ('libphi_core' if os.name != 'nt' else 'phi_core') + ext_suffix
         ]
         shutil.copy(env_dict.get("PHI_CORE_LIB"), libs_path)
         if (
@@ -1048,8 +1048,7 @@ def get_package_data_and_package_dir():
             or env_dict.get("WITH_ROCM") == "ON"
         ):
             package_data['paddle.libs'] += [
-                ('libphi_kernel_gpu' if os.name != 'nt' else 'phi_kernel_gpu')
-                + ext_suffix
+                ('libphi_gpu' if os.name != 'nt' else 'phi_gpu') + ext_suffix
             ]
             shutil.copy(env_dict.get("PHI_GPU_LIB"), libs_path)
 
