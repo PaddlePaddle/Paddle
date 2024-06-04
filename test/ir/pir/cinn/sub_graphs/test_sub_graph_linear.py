faf3fe4ebe4b819f5ea75e8922bf813d8175e76d
xiaoyao0115 <58548582+xiaoyao0115@users.noreply.github.com>
2024-06-04 12:01:41 +0800

    support ci train for all subgraphs (#64831)

--- test/ir/pir/cinn/sub_graphs/test_sub_graph_linear.py
+++ test/ir/pir/cinn/sub_graphs/test_sub_graph_linear.py
@@ -54,6 +54,7 @@ class TestLinear(TestBase):
         ]
         self.inputs = (paddle.rand(shape=[10, 64], dtype=paddle.float32),)
         self.net = LinearCase
+        self.with_train = False
 
 
 if __name__ == '__main__':
