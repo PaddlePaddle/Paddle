2a261d2f9a11cbc9deb22cef6d51b9432e0f56f7
phlrain <phliuhongyu@126.com>
2024-06-04 09:50:28 +0800

    remove useless code

--- paddle/cinn/hlir/dialect/operator/transforms/add_cinn_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/add_cinn_pass.cc
@@ -31,7 +31,6 @@
 #include "paddle/cinn/hlir/dialect/operator/transforms/add_store_in_fusion_op_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/cinn_group_cluster_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/conv2d_transpose_filter_pass.h"
-#include "paddle/cinn/hlir/dialect/operator/transforms/convert_fa_to_qkvmha_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/convert_memory_effec_attn_to_flash_attn_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/dynamic_reshape_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/fold_manipulation_ops_pass.h"
