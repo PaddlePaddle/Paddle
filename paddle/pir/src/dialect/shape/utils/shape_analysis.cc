7e4411adb1f8e8b63130cac89cac05fbfefc841b
Hongqing-work <Shine_CHQ@outlook.com>
2024-06-05 08:02:21 +0000

    [CINN]ignore null type shape get

--- paddle/pir/src/dialect/shape/utils/shape_analysis.cc
+++ paddle/pir/src/dialect/shape/utils/shape_analysis.cc
@@ -60,6 +60,7 @@ InferSymbolicShapeContext::GetShapeOrDataForValue(Value val) const {
     return empty;
   }
   if (!HasShapeOrDataForValue(val)) {
+    VLOG(3) << "fail value defining op: " << val.defining_op()->name();
     PADDLE_THROW(phi::errors::Fatal(
         "Fail to GetShapeOrDataForValue on InferSymbolicShape!"));
   }
@@ -380,21 +381,22 @@ void ShapeConstraintIRAnalysis::InferShapeOrDataForValue(Value val) {
     if (infer_symbolic_shape_interface) {
       infer_symbolic_shape_interface.InferSymbolicShape(&context_);
       for (auto& result_value : op->results()) {
-        if (result_value && (!context_.HasShapeOrDataForValue(result_value))) {
+        if (!result_value || !result_value.type()) {
+          continue;
+        }
+        if (!context_.HasShapeOrDataForValue(result_value)) {
           PADDLE_THROW(phi::errors::Fatal(op->name() +
                                           " HAS ERROR on InferSymbolicShape!"));
         }
       }
     } else {
-      // TODO(Hongqing-work): throw it after the shape analysis reconstruct
-      // is done.
-      // PADDLE_THROW(phi::errors::Unimplemented(
-      //     val.defining_op()->name() +
-      //     " DOES NOT have InferSymbolicShapeInterface!"));
       LOG(WARNING) << op->name()
                    << " DOES NOT have InferSymbolicShapeInterface!";
       for (auto& result_value : op->results()) {
-        if (result_value && (!context_.HasShapeOrDataForValue(result_value))) {
+        if (!result_value || !result_value.type()) {
+          continue;
+        }
+        if (!context_.HasShapeOrDataForValue(result_value)) {
           SetSymbolForValueByStaticShape(result_value);
         }
       }
