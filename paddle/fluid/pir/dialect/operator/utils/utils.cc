f345c84bd7c9382208c7053e622f904fa6c089d8
enkilee <jeffrey0122@163.com>
2024-06-06 12:28:31 +0800

    fix

--- paddle/fluid/pir/dialect/operator/utils/utils.cc
+++ paddle/fluid/pir/dialect/operator/utils/utils.cc
@@ -80,7 +80,6 @@ const std::unordered_set<std::string> LegacyOpList = {
     LrnOp::name(),
     LrnGradOp::name(),
     MovingAverageAbsMaxScaleOp::name(),
-    MovingAverageAbsMaxScale_Op::name(),
 #ifdef PADDLE_WITH_DNNL
     paddle::onednn::dialect::LrnOp::name(),
     paddle::onednn::dialect::LrnGradOp::name(),
