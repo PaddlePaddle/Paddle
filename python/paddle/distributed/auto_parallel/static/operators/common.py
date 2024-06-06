a758e51741e856fd61433a6dd60fcf1776021938
heavyrain_lzy <1528794076@qq.com>
2024-06-06 11:53:10 +0800

    add sharding_broadcast_overlap in VPP

--- python/paddle/distributed/auto_parallel/static/operators/common.py
+++ python/paddle/distributed/auto_parallel/static/operators/common.py
@@ -682,6 +682,14 @@ def is_data_parallel_reduce_op(op):
     )
 
 
+def is_data_parallel_broadcast_op(op):
+    return (
+        op.type == "c_broadcast"
+        and op.desc.has_attr("op_namescope")
+        and ParallelMode.DataParallel in op.desc.attr("op_namescope")
+    )
+
+
 def is_amp_flag_sync_op(op):
     return (
         op.type == "c_allreduce_max"
