15a07a71f5a8d3c7bd8c455788f516c9520e6518
ZhenxingLi <zhenxingli@hust.edu.cn>
2024-06-06 10:18:53 +0800

    Update instruction_util.cc

--- paddle/fluid/framework/new_executor/instruction/instruction_util.cc
+++ paddle/fluid/framework/new_executor/instruction/instruction_util.cc
@@ -134,6 +134,32 @@ platform::DeviceContext* ParseDeviceContext(
       }
       return dev_ctx;
     }
+    if (op_attributes.count("ring_id") != 0) {
+      int ring_id =
+          op_attributes.at("ring_id").dyn_cast<pir::Int32Attribute>().data();
+      const auto& comm_context_manager =
+          phi::distributed::CommContextManager::GetInstance();
+      if (comm_context_manager.Has(std::to_string(ring_id))) {
+        auto comm_context = comm_context_manager.Get(std::to_string(ring_id));
+        if (FLAGS_dynamic_static_unified_comm) {
+          dev_ctx = static_cast<platform::DeviceContext*>(
+              static_cast<phi::distributed::NCCLCommContext*>(comm_context)
+                  ->GetDevContext());
+        } else {
+          dev_ctx = platform::NCCLCommContext::Instance()
+                        .Get(ring_id, place)
+                        ->dev_context();
+        }
+        dev_ctx->SetCommContext(comm_context);
+        return dev_ctx;
+      } else {
+        VLOG(3) << "op: "
+                << op_attributes.at("op_name")
+                       .dyn_cast<pir::StrAttribute>()
+                       .AsString()
+                << ", ring_id: " << ring_id << ", get comm_context failed!";
+      }
+    }
 #endif
   }
 
