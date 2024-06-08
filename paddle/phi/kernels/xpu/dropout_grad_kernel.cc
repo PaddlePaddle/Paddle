810ae19c42403d354b95642bbb08517ead96ad00
Jianbang Yang <yangjianbang112@gmail.com>
2024-06-08 22:41:37 +0800

    fix

--- paddle/phi/kernels/xpu/dropout_grad_kernel.cc
+++ paddle/phi/kernels/xpu/dropout_grad_kernel.cc
@@ -108,4 +108,5 @@ PD_REGISTER_KERNEL(dropout_grad,
                    ALL_LAYOUT,
                    phi::DropoutGradRawKernel,
                    float,
-                   phi::dtype::float16) {}
+                   phi::dtype::float16,
+                   phi::dtype::bfloat16) {}
