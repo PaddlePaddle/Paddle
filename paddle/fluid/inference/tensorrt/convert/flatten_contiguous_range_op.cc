84ebca9fb5ee8b641834f8f83f2a6a1ad19d56df
walkalone20 <73780235+walkalone20@users.noreply.github.com>
2024-06-12 10:46:59 +0800

    【Hackathon 6th Fundable Projects 2 No.29】 Fix modernize-concat-nested-namespaces-part-19 (#64773)
    * part 19
    
    * part 19
    
    * format
    
    * restore

--- paddle/fluid/inference/tensorrt/convert/flatten_contiguous_range_op.cc
+++ paddle/fluid/inference/tensorrt/convert/flatten_contiguous_range_op.cc
@@ -11,9 +11,7 @@ limitations under the License. */
 
 #include "paddle/fluid/inference/tensorrt/convert/op_converter.h"
 
-namespace paddle {
-namespace inference {
-namespace tensorrt {
+namespace paddle::inference::tensorrt {
 /*
  * flatten_contiguous_range trt converter
  */
@@ -174,9 +172,7 @@ class FlattenContiguousRangeOpConverter : public OpConverter {
   }
 };
 
-}  // namespace tensorrt
-}  // namespace inference
-}  // namespace paddle
+}  // namespace paddle::inference::tensorrt
 
 REGISTER_TRT_OP_CONVERTER(flatten_contiguous_range,
                           FlattenContiguousRangeOpConverter);
