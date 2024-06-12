4152a70acd51f52cd536d8655e058c9790a0140c
drryanhuang <zihaohuang@aliyun.com>
2024-06-12 08:58:54 +0000

    radam

--- python/paddle/optimizer/radam.py
+++ python/paddle/optimizer/radam.py
@@ -12,10 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import warnings
 
+import paddle
 from paddle import _C_ops
 from paddle.base.libpaddle import DataType
+from paddle.nn.clip import GradientClipBase
 
 from ..base import core, framework
 from ..base.framework import (
@@ -54,29 +58,29 @@ class RAdam(Optimizer):
         \end{aligned}
 
     Args:
-        learning_rate (float|LRScheduler, optional): The learning rate used to update ``Parameter``.
+        learning_rate (float|LRScheduler|None, optional): The learning rate used to update ``Parameter``.
             It can be a float value or a LRScheduler. The default value is 0.001.
-        parameters (list|tuple, optional): List/Tuple of ``Tensor`` names to update to minimize ``loss``.
+        parameters (list|tuple|None, optional): List/Tuple of ``Tensor`` names to update to minimize ``loss``.
             This parameter is required in dygraph mode. And you can specify different options for
             different parameter groups such as the learning rate, weight decay, etc,
             then the parameters are list of dict. Note that the learning_rate in parameter groups
             represents the scale of base learning_rate.
             The default value is None in static graph mode, at this time all parameters will be updated.
-        beta1 (float|Tensor, optional): The exponential decay rate for the 1st moment estimates.
+        beta1 (float|Tensor|None, optional): The exponential decay rate for the 1st moment estimates.
             It should be a float number or a 0-D Tensor with shape [] and data type as float32.
             The default value is 0.9.
-        beta2 (float|Tensor, optional): The exponential decay rate for the 2nd moment estimates.
+        beta2 (float|Tensor|None, optional): The exponential decay rate for the 2nd moment estimates.
             It should be a float number or a 0-D Tensor with shape [] and data type as float32.
             The default value is 0.999.
-        epsilon (float, optional): A small float value for numerical stability.
+        epsilon (float|None, optional): A small float value for numerical stability.
             The default value is 1e-08.
-        weight_decay (float|Tensor, optional): The weight decay coefficient, it can be float or Tensor.
+        weight_decay (float|Tensor|None, optional): The weight decay coefficient, it can be float or Tensor.
             Default None, meaning there is no regularization.
-        grad_clip (GradientClipBase, optional): Gradient clipping strategy, it's an instance of
+        grad_clip (GradientClipBase|None, optional): Gradient clipping strategy, it's an instance of
             some derived class of ``GradientClipBase`` . There are three clipping strategies
             ( :ref:`api_paddle_nn_ClipGradByGlobalNorm` , :ref:`api_paddle_nn_ClipGradByNorm` ,
             :ref:`api_paddle_nn_ClipGradByValue` ). Default None, meaning there is no gradient clipping.
-        name (str, optional): Normally there is no need for user to set this property.
+        name (str|None, optional): Normally there is no need for user to set this property.
             For more information, please refer to :ref:`api_guide_Name`.
             The default value is None.
 
@@ -133,14 +137,14 @@ class RAdam(Optimizer):
 
     def __init__(
         self,
-        learning_rate=0.001,
-        beta1=0.9,
-        beta2=0.999,
-        epsilon=1.0e-8,
-        parameters=None,
-        weight_decay=None,
-        grad_clip=None,
-        name=None,
+        learning_rate: float = 0.001,
+        beta1: float | paddle.Tensor = 0.9,
+        beta2: float | paddle.Tensor = 0.999,
+        epsilon: float = 1.0e-8,
+        parameters: list | tuple | None = None,
+        weight_decay: float | paddle.Tensor | None = None,
+        grad_clip: GradientClipBase | None = None,
+        name: str | None = None,
     ):
         if isinstance(learning_rate, (float, int)) and not 0.0 <= learning_rate:
             raise ValueError(
