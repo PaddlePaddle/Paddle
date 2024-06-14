c8ab7ee44453e8923b0a33ba52ed0e1eba059655
ooooo <3164076421@qq.com>
2024-06-14 12:33:46 +0800

    update

--- python/paddle/optimizer/adam.py
+++ python/paddle/optimizer/adam.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import warnings
 from collections import defaultdict
 from typing import TYPE_CHECKING, NotRequired, Sequence
