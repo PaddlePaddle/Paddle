d198672671893df102d5a7ea27dfaf39e2c08d29
PuQing <me@puqing.work>
2024-06-17 15:05:03 +0000

    fix doc

--- python/paddle/tensor/linalg.py
+++ python/paddle/tensor/linalg.py
@@ -2265,15 +2265,18 @@ def histogram(
 def histogram_bin_edges(input, bins=100, range=None, name=None):
     """
     Computes only the edges of the bins used by the histogram function.
+
     Args:
         input (Tensor): A Tensor(or LoDTensor) with shape :math:`[N_1, N_2,..., N_k]` . The data type of the input Tensor
             should be float32, float64, int32, int64.
         bins (int, optional): number of histogram bins.
-        range (list | tuple):  The lower and upper range of the bins. If None, `range` is simply (input.min(), input.max()).
+        range (list | tuple): The lower and upper range of the bins. If None, `range` is simply (input.min(), input.max()).
             The first element of the range must be less than or equal to the second. Default: None.
         name (str, optional): For details, please refer to :ref:`api_guide_Name`. Generally, no setting is required. Default: None.
+
     Returns:
         Tensor: the values of the histogram and the bin edges. The output data type will be float32.
+
     Examples:
         .. code-block:: python
 
