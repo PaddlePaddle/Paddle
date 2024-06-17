f1e08a4a02a16cfacebd9841e8a9d0f9c1341711
huangjiyi <947613776@qq.com>
2024-06-17 07:54:23 +0000

    update

--- paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
@@ -74,14 +74,14 @@ class SumOpPattern : public pir::OpRewritePattern<paddle::dialect::SumOp> {
     // get attribute value from full_int_array op
     const std::vector<int64_t> axis = GetVectorFromIntArrayAttribute<int64_t>(
         full_int_array_op.attribute("value").dyn_cast<pir::ArrayAttribute>());
+    const bool keepdim =
+        op.attribute("keepdim").dyn_cast<::pir::BoolAttribute>().data();
     const auto &dtype = op.attribute("dtype")
                             .dyn_cast<paddle::dialect::DataTypeAttribute>()
                             .data();
-    const bool keepdim =
-        op.attribute("keepdim").dyn_cast<::pir::BoolAttribute>().data();
 
     auto cinn_reduce = rewriter.Build<cinn::dialect::ReduceSumOp>(
-        op->operand_source(0), axis, dtype, keepdim);
+        op->operand_source(0), axis, keepdim, dtype);
     rewriter.ReplaceAllUsesWith(op.result(0), cinn_reduce.result(0));
     rewriter.EraseOp(op);
     if (full_int_array_op->use_empty()) {
