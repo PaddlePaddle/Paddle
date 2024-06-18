0577e8e2ab29265234ace2cd58b887c83c0ccc10
gouzi <530971494@qq.com>
2024-06-18 14:15:51 +0800

    [Typing][A-9] Add type annotations for `paddle/tensor/ops.py`

--- python/paddle/tensor/layer_function_generator.py
+++ python/paddle/tensor/layer_function_generator.py
@@ -11,8 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations
 
 import re
+from typing import TYPE_CHECKING
 
 from paddle import _C_ops, _legacy_C_ops
 
@@ -26,6 +28,10 @@ from ..framework import (
     in_dynamic_mode,
     in_dynamic_or_pir_mode,
 )
+
+if TYPE_CHECKING:
+    from paddle import Tensor
+    from paddle.base.libpaddle import DataType
 
 __all__ = []
 
@@ -46,7 +52,7 @@ def _convert_(name):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
-def generate_layer_fn(op_type):
+def generate_layer_fn(op_type: str):
     """Register the Python layer for an Operator.
 
     Args:
@@ -85,7 +91,7 @@ def generate_layer_fn(op_type):
     o_name = not_intermediate_outputs[0].name
     intermediate_output_names = [output.name for output in intermediate_outputs]
 
-    def infer_and_check_dtype(op_proto, *args, **kwargs):
+    def infer_and_check_dtype(op_proto, *args, **kwargs) -> DataType:
         """
         This function performs the sanity check for dtype and
         instance type.
@@ -124,7 +130,7 @@ def generate_layer_fn(op_type):
                 dtype = core.VarDesc.VarType.FP32
         return dtype
 
-    def func(*args, **kwargs):
+    def func(*args, **kwargs) -> Tensor:
         helper = LayerHelper(op_type, **kwargs)
 
         dtype = infer_and_check_dtype(op_proto, *args, **kwargs)
@@ -160,7 +166,7 @@ def generate_layer_fn(op_type):
     return func
 
 
-def generate_activation_fn(op_type):
+def generate_activation_fn(op_type: str):
     """Register the Python layer for an Operator without Attribute.
 
     Args:
@@ -171,7 +177,7 @@ def generate_activation_fn(op_type):
 
     """
 
-    def func(x, name=None):
+    def func(x, name: str | None = None) -> Tensor:
         if in_dynamic_or_pir_mode():
             if hasattr(_C_ops, op_type):
                 op = getattr(_C_ops, op_type)
