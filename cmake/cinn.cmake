afe78707cfd615047aeffa2bd52700ee4fcb3671
Aurelius84 <zhangliujie@baidu.com>
2024-06-20 15:54:16 +0800

    fix typo

--- cmake/cinn.cmake
+++ cmake/cinn.cmake
@@ -164,7 +164,6 @@ cinn_cc_library(
   absl
   isl
   ginac
-  pybind
   op_fusion
   cinn_op_dialect
   ${jitify_deps})
@@ -173,7 +172,8 @@ add_dependencies(cinnapi GEN_LLVM_RUNTIME_IR_HEADER ${core_deps})
 target_link_libraries(cinnapi op_dialect pir phi)
 add_dependencies(cinnapi op_dialect pir phi)
 
-# target_link_libraries(cinnapi ${PYTHON_LIBRARIES})
+add_dependencies(cinnapi python)
+target_link_libraries(cinnapi "-Wl,-undefined,dynamic_lookup")
 
 if(WITH_MKL)
   target_link_libraries(cinnapi cinn_mklml)
