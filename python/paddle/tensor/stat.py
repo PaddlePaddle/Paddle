e9833872039330d9a5439e99111ada9e355f6273
Nyakku Shigure <sigure.qaq@gmail.com>
2024-06-21 11:34:48 +0800

    Update python/paddle/tensor/stat.py

--- python/paddle/tensor/stat.py
+++ python/paddle/tensor/stat.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: define statistical functions of a tensor
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Literal, Sequence, overload
