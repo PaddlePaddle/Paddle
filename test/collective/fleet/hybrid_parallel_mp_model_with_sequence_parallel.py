0f17fe9ec831a9807d583bc51e7cfe1c8829f634
ShenLiang <1422485404@qq.com>
2024-06-24 15:54:34 +0800

    [Distributed]Fix task wait bug of batch_isend_irecv (#65369)
    * fix bug of batch_isend_irecv
    
    * add colescing ops
    
    * add colescing ops
    
    * support bkcl
    
    * support bkcl
    
    * fix bug in bkcl
    
    * add utest
    
    * add utest
    
    * add bug of batch_isend_irecv
    
    add check
    
    no namespace
    
    add check
    
    fix utest
    
    fix utest

--- test/collective/fleet/hybrid_parallel_mp_model_with_sequence_parallel.py
+++ test/collective/fleet/hybrid_parallel_mp_model_with_sequence_parallel.py
@@ -614,6 +614,7 @@ class SimpleSPNetWithoutBias(paddle.nn.Layer):
         x = self.embedding(x)
 
         x = paddle.transpose(x, perm=[1, 0, 2])
+        x = x.contiguous()
         x = spu.ScatterOp.apply(x)
 
         x = self.linear1(x)
