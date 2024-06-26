39eb89a7d0424fd35edfd6adc4ede8dab952c771
co63oc <co63oc@users.noreply.github.com>
2024-06-26 15:50:35 +0800

    【Hackathon 6th Fundable Projects 3 No.362】 sparse_momentum static (#65402)
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * ci

--- paddle/phi/kernels/gpu/sparse_momentum_kernel.cu
+++ paddle/phi/kernels/gpu/sparse_momentum_kernel.cu
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,16 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/operators/optimizers/sparse_momentum_op.h"
-#include "paddle/fluid/framework/op_registry.h"
 #include "paddle/phi/common/float16.h"
+#include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/impl/sparse_momentum_kernel_impl.h"
 
-namespace ops = paddle::operators;
-
-PD_REGISTER_STRUCT_KERNEL(sparse_momentum,
-                          GPU,
-                          ALL_LAYOUT,
-                          ops::SparseMomentumOpKernel,
-                          float,
-                          double,
-                          phi::dtype::float16) {}
+PD_REGISTER_KERNEL(sparse_momentum,
+                   GPU,
+                   ALL_LAYOUT,
+                   phi::SparseMomentumOpKernel,
+                   float,
+                   double,
+                   phi::dtype::float16) {}
