ec33a009a27865e991c2f48b2bf26a2d90f43dfb
lizexu <2694294196@qq.com>
2024-06-26 09:04:22 +0000

    logging.h放到.cc,修改了conv2d,增加了dropout

--- paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
+++ paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
@@ -13,7 +13,9 @@
 // limitations under the License.
 
 #include "paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.h"
+#include <glog/logging.h>
 
+#include "paddle/fluid/inference/tensorrt/helper.h"
 #include "paddle/fluid/pir/dialect/operator/ir/op_attribute.h"
 #include "paddle/fluid/pir/dialect/operator/ir/pd_op.h"
 #include "paddle/fluid/pir/utils/general_functions.h"
@@ -46,10 +48,10 @@ DEFINE_GENERAL_PATTERN(Matmul, paddle::dialect::MatmulOp)
 DEFINE_GENERAL_PATTERN(BatchNorm, paddle::dialect::BatchNormOp)
 DEFINE_GENERAL_PATTERN(BatchNorm_, paddle::dialect::BatchNorm_Op)
 DEFINE_GENERAL_PATTERN(Softmax, paddle::dialect::SoftmaxOp)
-DEFINE_GENERAL_PATTERN(Conv2d, paddle::dialect::Conv2dOp)
 DEFINE_GENERAL_PATTERN(Relu, paddle::dialect::ReluOp)
 DEFINE_GENERAL_PATTERN(FullIntArray, paddle::dialect::FullIntArrayOp)
 DEFINE_GENERAL_PATTERN(Reshape, paddle::dialect::ReshapeOp)
+DEFINE_GENERAL_PATTERN(Dropout, paddle::dialect::DropoutOp)
 #undef DEFINE_GENERAL_PATTERN
 
 class Pool2dOpPattern
@@ -125,6 +127,42 @@ class Pool2dOpPattern
   }
 };
 
+class Conv2dOpPattern
+    : public pir::OpRewritePattern<paddle::dialect::Conv2dOp> {
+ public:
+  using pir::OpRewritePattern<paddle::dialect::Conv2dOp>::OpRewritePattern;
+  bool MatchAndRewrite(paddle::dialect::Conv2dOp op,
+                       pir::PatternRewriter &rewriter) const override {
+    if (op->HasAttribute(kCanRunTrtAttr) &&
+        op->attribute<pir::BoolAttribute>(kCanRunTrtAttr).data()) {
+      return false;
+    }
+#if IS_TRT_VERSION_LT(7000)
+    if (op->HasAttribute("padding_algorithm")) {
+      std::string padding_algorithm =
+          op->attribute<pir::StrAttribute>("padding_algorithm").AsString();
+      if (padding_algorithm == "SAME" && op->HasAttribute("strides")) {
+        auto strides_attr = op->attribute<pir::ArrayAttribute>("strides");
+        std::vector<int> strides;
+        for (const auto &attr : strides_attr.AsVector()) {
+          strides.push_back(attr.dyn_cast<pir::Int32Attribute>().data());
+        }
+        if (strides.size() > 1) {
+          for (size_t i = 0; i < strides.size(); ++i) {
+            if (strides[i] > 1) {
+              VLOG(3) << "The stride size should be 1 or less than 1";
+              return false;
+            }
+          }
+        }
+      }
+    }
+#endif
+    op->set_attribute(kCanRunTrtAttr, rewriter.bool_attr(true));
+    return true;
+  }
+};
+
 class TrtOpMarkerPass : public pir::PatternRewritePass {
  public:
   TrtOpMarkerPass() : pir::PatternRewritePass("trt_op_marker_pass", 2) {}
@@ -138,12 +176,13 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ADD_PATTERN(BatchNorm)
     ADD_PATTERN(BatchNorm_)
     ADD_PATTERN(Softmax)
-    ADD_PATTERN(Conv2d)
     ADD_PATTERN(Relu)
     ADD_PATTERN(FullIntArray)
     ADD_PATTERN(Reshape)
+    ADD_PATTERN(Dropout)
 #undef ADD_PATTERN
     ps.Add(std::make_unique<Pool2dOpPattern>(context));
+    ps.Add(std::make_unique<Conv2dOpPattern>(context));
     return ps;
   }
 };
