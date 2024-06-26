029d8e247543a1dd249cdefb1829e901f46ee5a4
Wang Huan <wanghuan29@baidu.com>
2024-06-26 09:07:46 +0000

    add flags for skip old ir or pir

--- python/paddle/pir_utils.py
+++ python/paddle/pir_utils.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 
+import os
 from functools import wraps
 
 import paddle
@@ -185,10 +186,18 @@ class DygraphOldIrGuard:
 def test_with_pir_api(func):
     @wraps(func)
     def impl(*args, **kwargs):
-        with OldIrGuard():
-            func(*args, **kwargs)
-        with IrGuard():
-            func(*args, **kwargs)
+        skip_old_ir = os.environ.get("FLAGS_CI_skip_old_ir", "False")
+        skip_pir = os.environ.get("FLAGS_CI_skip_pir", "False")
+        if not skip_old_ir:
+            with OldIrGuard():
+                func(*args, **kwargs)
+        else:
+            raise ValueError("skip_old_ir")
+        if not skip_pir:
+            with IrGuard():
+                func(*args, **kwargs)
+        else:
+            raise ValueError("skip_pir")
 
     return impl
 
