22e9e5f1853bb8301f657faf08bb4f79fe38f860
gouzil <66515297+gouzil@users.noreply.github.com>
2024-06-29 11:15:09 +0800

    Update python/paddle/vision/transforms/transforms.py

--- python/paddle/vision/transforms/transforms.py
+++ python/paddle/vision/transforms/transforms.py
@@ -170,7 +170,7 @@ class Compose(_Transform[_InputT, _RetT]):
                 stack_info = traceback.format_exc()
                 print(
                     f"fail to perform transform [{f}] with error: "
-                    f"{e} and stack:\n{stack_info!s}"
+                    f"{e} and stack:\n{stack_info}"
                 )
                 raise e
         return data
