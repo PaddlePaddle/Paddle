1b9e655498460249ffbf87c3cec280830fbcf99b
ForFishes <2282912238@qq.com>
2024-06-30 12:40:57 +0800

    support async load

---
+++ test/legacy_test/test_async_offload_reload.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+
+import paddle
+from paddle.incubate.tensor.manipulation import (
+    async_offload,
+    async_reload,
+    create_async_load,
+)
+
+
+class TestSaveLoadLargeParameters(unittest.TestCase):
+    def test_large_parameters_paddle_save(self):
+        loader = create_async_load()
+        data0 = paddle.randn([10000, 50])
+        data1 = paddle.randn([50, 50])
+        data1 = paddle.randn([50, 50])
+        cpu_data, task = async_offload(data0, loader)
+        res = paddle.matmul(data1, data1)
+        task.wait()
+        gpu_data = async_reload(cpu_data, loader)
+        res = paddle.matmul(data1, data1)
+        task.wait()
+
+
+if __name__ == '__main__':
+    unittest.main()
