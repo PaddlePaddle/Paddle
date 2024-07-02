a675a1ee57b12f5a290686db3292acce5643ccb7
co63oc <co63@163.com>
2024-07-02 11:13:11 +0800

    Fix

--- paddle/phi/kernels/impl/sequence_expand_kernel_impl.h
+++ paddle/phi/kernels/impl/sequence_expand_kernel_impl.h
@@ -41,41 +41,6 @@ struct SequenceExpandGradFunctor {
                   phi::DenseTensor* dx);
 };
 
-template <typename T>
-struct SequenceExpandFunctor<phi::CPUContext, T> {
-  void operator()(const phi::CPUContext& context UNUSED,
-                  const phi::DenseTensor& x,
-                  const phi::Vector<size_t>& x_lod,   /*expand source lod*/
-                  const phi::Vector<size_t>& ref_lod, /*expand referenced lod*/
-                  phi::DenseTensor* out) {
-    int out_offset = 0;
-    int x_item_length = x.numel() / x.dims()[0];
-    auto out_data = out->data<T>();
-    auto x_data = x.data<T>();
-    for (size_t i = 1; i < ref_lod.size(); ++i) {
-      int repeat_num = ref_lod[i] - ref_lod[i - 1];
-      int x_start = x_lod[i - 1];
-      int x_end = x_lod[i];
-      int x_seq_len = x_end - x_start;
-      if (repeat_num > 0) {
-        int out_start = out_offset;
-        if (out->lod().size() == 1) {
-          out_start = out->lod()[0][out_offset];
-        }
-        for (int j = 0; j < repeat_num; j++) {
-          for (int k = 0; k < x_seq_len; k++) {
-            for (int l = 0; l < x_item_length; l++) {
-              out_data[(out_start + j * x_seq_len + k) * x_item_length + l] =
-                  x_data[(x_start + k) * x_item_length + l];
-            }
-          }
-        }
-      }
-      out_offset += repeat_num;
-    }
-  }
-};
-
 template <typename T, typename Context>
 void SequenceExpandKernel(const Context& dev_ctx,
                           const DenseTensor& x_in,
@@ -204,46 +169,6 @@ void SequenceExpandKernel(const Context& dev_ctx,
   functor(dev_ctx, *x, ref_x_lod, y_lod[ref_level], out);
 }
 
-/*
- *Given Grad(Out)
- *
- *    Grad(Out).lod = [[0,                            2],
- *                     [0,              3,            6]]
- *    Grad(Out).data = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
- * Then
- *    Grad(X).data = [(0.1 + 0.2 + 0.3), (0.4 + 0.5 + 0.6)]
- *                 = [0.6, 1.5]
- *    Grad(X).lod = Input(X).lod
- *
- * */
-template <typename T>
-struct SequenceExpandGradFunctor<phi::CPUContext, T> {
-  void operator()(const phi::CPUContext& context,
-                  const phi::DenseTensor& dout,
-                  const phi::Vector<size_t>& x_lod,   /*expand source lod*/
-                  const phi::Vector<size_t>& ref_lod, /*expand referenced lod*/
-                  phi::DenseTensor* dx) {
-    int dout_offset = 0;
-    for (size_t i = 1; i < ref_lod.size(); ++i) {
-      int repeat_num = ref_lod[i] - ref_lod[i - 1];
-      if (repeat_num > 0) {
-        int x_start = x_lod[i - 1];
-        int x_end = x_lod[i];
-        int x_seq_len = x_end - x_start;
-        if (x_seq_len == 0) continue;
-        auto dx_sub = dx->Slice(x_start, x_end);
-        dx_sub.Resize(common::flatten_to_1d(dx_sub.dims()));
-        int dout_end = dout_offset + repeat_num * x_seq_len;
-        auto dout_sub = dout.Slice(dout_offset, dout_end);
-        dout_sub.Resize({repeat_num, dx_sub.dims()[0]});
-        phi::funcs::ColwiseSum<phi::CPUContext, T> col_sum;
-        col_sum(context, dout_sub, &dx_sub);
-        dout_offset += repeat_num * x_seq_len;
-      }
-    }
-  }
-};
-
 template <typename T, typename Context>
 void SequenceExpandGradKernel(const Context& dev_ctx,
                               const DenseTensor& x_in,
@@ -282,4 +207,19 @@ void SequenceExpandGradKernel(const Context& dev_ctx,
   SequenceExpandGradFunctor<Context, T> functor;
   functor(dev_ctx, *g_out, ref_x_lod, ref_lod, g_x);
 }
+
+// for GPU kernel
+inline void GetOutputOffset(const phi::Vector<size_t>& x_lod,
+                            const phi::Vector<size_t>& ref_lod,
+                            phi::Vector<size_t>* out_offset) {
+  size_t offset = 0;
+  int lod_size = static_cast<int>(x_lod.size());
+  for (int i = 0; i < static_cast<int>(x_lod.size()); ++i) {
+    (*out_offset)[i] = offset;
+    if (i < lod_size - 1) {
+      offset += (ref_lod[i + 1] - ref_lod[i]) * (x_lod[i + 1] - x_lod[i]);
+    }
+  }
+}
+
 }  // namespace phi
