578328aecd930d3cc92718e9dd6734d46876b984
risemeup1 <515586620@qq.com>
2024-07-02 11:14:12 +0000

    fix,test=document_fix

--- test/legacy_test/test_transforms.py
+++ test/legacy_test/test_transforms.py
@@ -930,7 +930,6 @@ class TestFunctional(unittest.TestCase):
         pil_img = Image.fromarray(np_img).convert('I')
         pil_tensor = F.to_tensor(pil_img)
 
-        # pil_img = Image.fromarray(np_img).convert('I;16')
         pil_img_16bit = Image.new('I;16', pil_img.size)
         pil_img_16bit.paste(pil_img)
         pil_tensor = F.to_tensor(pil_img)
