d3aec45bf44b8bf077aabeeeaf004ed112a1793e
ooooo <3164076421@qq.com>
2024-07-03 09:44:01 +0800

    add # type: ignore

--- python/paddle/io/dataloader/worker.py
+++ python/paddle/io/dataloader/worker.py
@@ -116,10 +116,10 @@ def get_worker_info() -> WorkerInfo | None:
             ...             iter_start = self.start
             ...             iter_end = self.end
             ...         else:
-            ...             per_worker = int(
+            ...             per_worker = int(  # type: ignore
             ...                 math.ceil((self.end - self.start) / float(
             ...                     worker_info.num_workers)))
-            ...             worker_id = worker_info.id
+            ...             worker_id = worker_info.id  # type: ignore
             ...             iter_start = self.start + worker_id * per_worker
             ...             iter_end = min(iter_start + per_worker, self.end)
             ...
