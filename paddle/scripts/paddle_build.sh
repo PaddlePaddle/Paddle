d431ed5025d9b354092072ae4d85d33bf844310f
lj970926 <1783973490@qq.com>
2024-07-03 17:15:31 +0000

    fix ci bugs

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -2369,6 +2369,9 @@ set +x
         ut_startTime_s=`date +%s`
         test_cases=$(ctest -N -V -LE "(RUN_TYPE=DIST_KUNLUN)" | grep "_xpu" )        # cases list which would be run exclusively
         get_quickly_disable_ut||disable_ut_quickly='disable_ut'   # indicate whether the case was in quickly disable list
+        # XPU has extra log to stdout when import paddle now, so we filter out then.
+        # To be removed after the version update of bkcl.
+        disable_ut_quickly="${disable_ut_quickly%% *}"
         while read -r line; do
             if [[ "$line" == "" ]]; then
                 continue
