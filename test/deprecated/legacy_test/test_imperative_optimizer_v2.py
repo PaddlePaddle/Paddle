56ed1bb81b9d114da16bfabe375deecdc42d1fde
SigureMo <sigure.qaq@gmail.com>
2024-07-08 05:11:20 +0000

    fix a missing _manual_program_seed

--- test/deprecated/legacy_test/test_imperative_optimizer_v2.py
+++ test/deprecated/legacy_test/test_imperative_optimizer_v2.py
@@ -20,12 +20,37 @@ from test_imperative_base import new_program_scope
 
 import paddle
 from paddle import base
+from paddle.autograd.backward_utils import ValueDict
 from paddle.base import core
 from paddle.distributed.fleet.meta_optimizers import DGCMomentumOptimizer
 from paddle.pir_utils import test_with_pir_api
 
 # Note(wangzhongpu)
 # In dygraph, don't support ModelAverage, DGCMomentumOptimizer, ExponentialMovingAverage, PipelineOptimizer, LookaheadOptimizer, RecomputeOptimizer.
+
+
+def create_parameter_mapping(startup_program, main_program):
+    startup_params = {}
+    main_params = {}
+    parameter_mapping = ValueDict()
+    for op in startup_program.global_block().ops:
+        if op.name() == "builtin.set_parameter":
+            name = op.attrs()["parameter_name"]
+            param = op.operand(0).source()
+            startup_params[name] = param
+
+    for op in main_program.global_block().ops:
+        if op.name() == "builtin.parameter":
+            name = op.attrs()["parameter_name"]
+            param = op.result(0)
+            main_params[name] = param
+
+    assert len(startup_params) == len(main_params)
+    for name, startup_param in startup_params.items():
+        assert name in main_params
+        main_param = main_params[name]
+        parameter_mapping[main_param] = startup_param
+    return parameter_mapping
 
 
 class MLP(paddle.nn.Layer):
@@ -102,6 +127,13 @@ class TestImperativeOptimizerBase(unittest.TestCase):
 
         paddle.disable_static(place)
         paddle.seed(seed)
+        if paddle.framework.use_pir_api():
+            with paddle.pir_utils.OldIrGuard():
+                # Note: dygraph use self.main_program.global_block().create_parameter(), it's need manual seed to old Program
+                paddle.framework.random._manual_program_seed(seed)
+            paddle.framework.random._manual_program_seed(seed)
+        else:
+            paddle.framework.random._manual_program_seed(seed)
 
         mlp = MLP()
         optimizer = self.get_optimizer_dygraph(parameter_list=mlp.parameters())
@@ -197,17 +229,25 @@ class TestImperativeOptimizerBase(unittest.TestCase):
                 static_param_name_list.append(param.name)
                 static_params.append(param)
 
+            if paddle.framework.use_pir_api():
+                parameter_mapping = create_parameter_mapping(
+                    paddle.static.default_startup_program(),
+                    paddle.static.default_main_program(),
+                )
+                startup_params = [
+                    parameter_mapping[param] for param in static_params
+                ]
+            else:
+                startup_params = static_params
+
             out = exe.run(
-                base.default_startup_program(),
+                paddle.static.default_startup_program(),
+                fetch_list=startup_params,
             )
 
-            for i in range(len(static_param_name_list)):
+            for i in range(len(static_params)):
                 param_name = static_param_name_list[i]
-                static_param_init_value[param_name] = np.asarray(
-                    paddle.static.global_scope()
-                    .find_var(param_name)
-                    .get_tensor()
-                )
+                static_param_init_value[param_name] = out[i]
 
             for batch_id, data in enumerate(train_reader()):
                 if batch_id >= self.batch_num:
