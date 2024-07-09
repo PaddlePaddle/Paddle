7f07d254571394726202ccae4776ae5306b0daf7
megemini <megemini@outlook.com>
2024-07-09 20:39:21 +0800

    [Fix] Dataset generics type checking

--- python/paddle/io/dataloader/dataset.py
+++ python/paddle/io/dataloader/dataset.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# type: ignore[type-arg]
+# Ignore type checking for `Dataset` generics.
+
 from __future__ import annotations
 
 import bisect
