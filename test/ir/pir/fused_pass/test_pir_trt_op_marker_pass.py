03b777b5742dbfd1a2d1dbb74cd9fb57f9a4fc46
lizexu <2694294196@qq.com>
2024-07-11 07:04:09 +0000

    加了silu,修改GroupNrom,加了Matmul,Scale单测

--- test/ir/pir/fused_pass/test_pir_trt_op_marker_pass.py
+++ test/ir/pir/fused_pass/test_pir_trt_op_marker_pass.py
@@ -85,5 +85,143 @@ class TestTRTPattern(PassTest):
         self.check_pass_correct()
 
 
+class TestMatmulScaleTRTPattern(PassTest):
+    def is_program_valid(self, program=None):
+        return True
+
+    def sample_program(self):
+        for x_shape in [[3, 2]]:
+            for w_shape in [[2, 3]]:
+                for scale_bias in [1e-7]:
+                    for scale_value in [2.0]:
+                        for bias_after_scale in [True]:
+                            with paddle.pir_utils.IrGuard():
+                                main_prog = paddle.static.Program()
+                                start_prog = paddle.static.Program()
+                                with paddle.static.program_guard(
+                                    main_prog, start_prog
+                                ):
+                                    x = paddle.static.data(
+                                        name='x', shape=x_shape, dtype='float32'
+                                    )
+                                    w = paddle.static.data(
+                                        name='w', shape=w_shape, dtype='float32'
+                                    )
+                                    out = paddle.scale(
+                                        paddle.matmul(x, w),
+                                        scale=scale_value,
+                                        bias=scale_bias,
+                                        bias_after_scale=bias_after_scale,
+                                    )
+                                    out = paddle.assign(out)
+                                    self.pass_attr_list = [
+                                        {'trt_op_marker_pass': {}}
+                                    ]
+                                    self.feeds = {
+                                        "x": np.random.random(x_shape).astype(
+                                            "float32"
+                                        ),
+                                        "w": np.random.random(w_shape).astype(
+                                            "float32"
+                                        ),
+                                    }
+                                    self.fetch_list = [out]
+                                    self.valid_op_map = {
+                                        "pd_op.conv2d": 0,
+                                    }
+                                    yield [main_prog, start_prog], False
+
+    def setUp(self):
+        self.places.append(paddle.CPUPlace())
+        if core.is_compiled_with_cuda():
+            self.places.append(paddle.CUDAPlace(0))
+
+    def test_check_output(self):
+        self.check_pass_correct()
+
+
+class TestGroupNormSiluTRTPattern(PassTest):
+    def is_program_valid(self, program=None):
+        return True
+
+    def sample_program(self):
+        for x_shape in [[2, 6, 4, 2]]:
+            dtype = None
+            if core.is_compiled_with_xpu():
+                dtype = 'float32'
+            elif core.is_compiled_with_cuda():
+                dtype = 'float16'
+            for epilson in [1e-5]:
+                for groups in [2]:
+                    rand_value = (
+                        0.001
+                        * paddle.rand(shape=[x_shape[1]], dtype=dtype).numpy()
+                    )
+                    with paddle.pir_utils.IrGuard():
+                        start_prog = paddle.static.Program()
+                        main_prog = paddle.static.Program()
+                        with paddle.pir.core.program_guard(
+                            main_prog, start_prog
+                        ):
+                            x = paddle.static.data(
+                                name='x', shape=x_shape, dtype=dtype
+                            )
+                            w = create_parameter(
+                                shape=[x_shape[1]],
+                                dtype=dtype,
+                                initializer=paddle.nn.initializer.Assign(
+                                    rand_value
+                                ),
+                            )
+                            b = create_parameter(
+                                shape=[x_shape[1]],
+                                dtype=dtype,
+                                initializer=paddle.nn.initializer.Assign(
+                                    rand_value
+                                ),
+                            )
+                            group_norm_out = paddle.nn.functional.group_norm(
+                                x,
+                                num_groups=groups,
+                                epsilon=epilson,
+                                weight=w,
+                                bias=b,
+                                data_format="NCHW",
+                            )
+                            out = paddle.nn.functional.silu(group_norm_out)
+                            out = paddle.assign(out)
+                            if core.is_compiled_with_xpu():
+                                self.pass_attr_list = [
+                                    {'trt_op_marker_pass': {}}
+                                ]
+                            elif core.is_compiled_with_cuda():
+                                self.pass_attr_list = [
+                                    {'trt_op_marker_pass': {}}
+                                ]
+                            self.feeds = {
+                                "x": np.random.random(x_shape).astype(dtype),
+                            }
+                            self.fetch_list = [out]
+                            if core.is_compiled_with_xpu():
+                                self.valid_op_map = {
+                                    "pd_op.group_norm_silu_xpu": 0,
+                                }
+                            elif core.is_compiled_with_cuda():
+                                self.valid_op_map = {
+                                    "pd_op.add_group_norm_silu": 0,
+                                }
+
+                            yield [main_prog, start_prog], False
+
+    def setUp(self):
+        if core.is_compiled_with_xpu():
+            self.places.append(paddle.XPUPlace(0))
+        elif core.is_compiled_with_cuda():
+            self.places.append(paddle.CUDAPlace(0))
+
+    def test_check_output(self):
+        self.check_pass_correct()
+
+
 if __name__ == "__main__":
     unittest.main()
