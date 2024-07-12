d8ea18126f8879f24bbac41d2add29a8b4edc638
BiynXu <244524405@qq.com>
2024-07-12 14:37:16 +0800

    [PIR] Fix pir unittest test_norm_all

--- python/paddle/tensor/linalg.py
+++ python/paddle/tensor/linalg.py
@@ -495,7 +495,7 @@ def vector_norm(
     def inf_norm(
         input, porder=None, axis=axis, keepdim=False, asvector=False, name=None
     ):
-        if in_dynamic_mode():
+        if in_dynamic_or_pir_mode():
             out = _C_ops.abs(input)
             if porder == np.float64('inf'):
                 return _C_ops.max(out, axis, keepdim)
