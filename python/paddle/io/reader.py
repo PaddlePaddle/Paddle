9183244de7d8b71b545ac52e81c5af1f8080d55c
SigureMo <sigure.qaq@gmail.com>
2024-07-13 12:20:24 +0800

    ignore generic

--- python/paddle/io/reader.py
+++ python/paddle/io/reader.py
@@ -386,7 +386,7 @@ class DataLoader:
             >>> CLASS_NUM = 10
 
             >>> # define a random dataset
-            >>> class RandomDataset(Dataset):
+            >>> class RandomDataset(Dataset):  # type: ignore[type-arg]
             ...     def __init__(self, num_samples):
             ...         self.num_samples = num_samples
             ...
