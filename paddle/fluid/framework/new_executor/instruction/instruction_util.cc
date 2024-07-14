48f453708a32098a7b377a3cc8e5394ed1b1c1ab
ZhenxingLi <zhenxingli@hust.edu.cn>
2024-07-14 20:54:47 +0800

    Update instruction_util.cc

--- paddle/fluid/framework/new_executor/instruction/instruction_util.cc
+++ paddle/fluid/framework/new_executor/instruction/instruction_util.cc
@@ -145,7 +145,7 @@ platform::DeviceContext* ParseDeviceContext(
           static_cast<phi::distributed::NCCLCommContext*>(comm_context)
               ->GetDevContext()
               ->SetCommContext(comm_context);
-          if (typeid(op) == typeid(pir::Operation*)) {
+          if (op_name.compare(paddle::dialect::CReducescatterOp::name()) == 0:) {
             return dev_ctx;
           }
         } else {
