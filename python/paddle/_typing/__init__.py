d430d35620df8e2e1551ea731b1e6b47b9cfb22a
megemini <megemini@outlook.com>
2024-07-15 14:57:31 +0800

    [Add] typing

--- python/paddle/_typing/__init__.py
+++ python/paddle/_typing/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import TYPE_CHECKING
+
 # Backport
 from .backport import EllipsisType as EllipsisType
 
@@ -61,3 +63,6 @@ from .shape import (
     Size6 as Size6,
     SizeN as SizeN,
 )
+
+if TYPE_CHECKING:
+    from _libpaddle import task as task
