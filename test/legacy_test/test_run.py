6856e81259d3d225f83abd24d0393ac261183ab4
ZhenxingLi <zhenxingli@hust.edu.cn>
2024-07-17 20:01:45 +0800

    Update test_run.py

--- test/legacy_test/test_run.py
+++ test/legacy_test/test_run.py
@@ -205,5 +205,5 @@ class PS_Test(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    os.environ["FLAGS_dynamic_static_unified_comm"] = "0"
+    os.environ["FLAGS_dynamic_static_unified_comm"] = "1"
     unittest.main()
