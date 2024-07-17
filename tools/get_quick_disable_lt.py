240bf4562b41191b1aaa1778d920c2232a2b4178
tianshuo78520a <707759223@qq.com>
2024-07-17 06:58:36 +0000

    Add 3.0 disable_ut

--- tools/get_quick_disable_lt.py
+++ tools/get_quick_disable_lt.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import ssl
 import sys
 
@@ -27,7 +28,10 @@ def download_file():
     if sysstr == 'win32':
         url = "https://sys-p0.bj.bcebos.com/prec/{}".format('disable_ut_win')
     else:
-        url = "https://sys-p0.bj.bcebos.com/prec/{}".format('disable_ut')
+        if os.environ.get('BRANCH') == "release/3.0-beta":
+            url = "https://sys-p0.bj.bcebos.com/prec/{}".format('disable_ut_30')
+        else:
+            url = "https://sys-p0.bj.bcebos.com/prec/{}".format('disable_ut')
 
     if paddle.is_compiled_with_rocm():
         url = "https://sys-p0.bj.bcebos.com/prec/{}".format('disable_ut_rocm')
