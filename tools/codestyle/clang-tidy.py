2e9c2408704acd967cc2ba7295da62a8c2679630
co63oc <co63@163.com>
2024-07-19 06:41:07 +0800

    Fix

--- tools/codestyle/clang-tidy.py
+++ tools/codestyle/clang-tidy.py
@@ -485,10 +485,7 @@ if __name__ == '__main__':
         print(
             "clang-tidy not found, attempting auto-install...", file=sys.stderr
         )
-        try:
-            subprocess.check_output(
-                'pip install --no-cache clang-tidy=="15.0.2.1"', shell=True
-            )
-        except:
-            sys.exit(0)
+        subprocess.check_output(
+            'pip install --no-cache clang-tidy=="15.0.2.1"', shell=True
+        )
     main()
