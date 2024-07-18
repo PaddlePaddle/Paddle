7ea88020530e81d451a4dce3c3f3cbd09a300f56
LielinJiang <jianglielin@baidu.com>
2024-07-18 08:13:44 +0000

    fix unittest

--- paddle/phi/kernels/cpu/sigmoid_cross_entropy_with_logits_grad_kernel.cc
+++ paddle/phi/kernels/cpu/sigmoid_cross_entropy_with_logits_grad_kernel.cc
@@ -43,18 +43,30 @@ void SigmoidCrossEntropyWithLogitsGradKernel(
     T x = x_data[idx];
     T label = label_data[idx];
     T dout = dout_data[idx];
-    T pos_weight_idx = pos_weight_data == nullptr ? 1 : pos_weight_data[idx];
     if (static_cast<int>(label) == ignore_index) {
       dx_data[idx] = static_cast<T>(0.);
     } else {
-      T max_val = x < 0 ? -x : 0;
-      T term1 = (x < 0) ? static_cast<T>(-1) : static_cast<T>(0);
-      T down1 = std::exp(-max_val);
-      T down2 = std::exp(-x - max_val);
-      T term2 = down1 * (-term1) + down2 * (-1 - term1);
-      T term3 = (static_cast<T>(1.) - label);
-      T diff = pos_weight_idx * (term2 / (down1 + down2) + term1) + term3;
-      dx_data[idx] = dout * diff;
+      if (pos_weight_data == nullptr) {
+        T term1 = (x > 0) ? static_cast<T>(1) : static_cast<T>(0);
+        T e_x = std::exp(-std::abs(x));
+        T down = 1 + e_x;
+        T abs_grad = (x >= 0) ? static_cast<T>(1) : static_cast<T>(-1);
+        T up = -e_x * abs_grad;
+        T term3 = up / down;
+
+        T diff = term1 - label + term3;
+        dx_data[idx] = dout * diff;
+      } else {
+        T max_val = x < 0 ? -x : 0;
+        T term1 = (x < 0) ? static_cast<T>(-1) : static_cast<T>(0);
+        T down1 = std::exp(-max_val);
+        T down2 = std::exp(-x - max_val);
+        T term2 = down1 * (-term1) + down2 * (-1 - term1);
+        T term3 = (static_cast<T>(1.) - label);
+        T diff =
+            pos_weight_data[idx] * (term2 / (down1 + down2) + term1) + term3;
+        dx_data[idx] = dout * diff;
+      }
     }
   }
   if (normalize) {
