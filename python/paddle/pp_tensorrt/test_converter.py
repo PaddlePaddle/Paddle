2067f7c8e58f8b2a60ee319b4c48a84f1a736038
lizexu <2694294196@qq.com>
2024-07-18 02:52:09 +0000

    忘了加test_converter.py

--- python/paddle/pp_tensorrt/test_converter.py
+++ python/paddle/pp_tensorrt/test_converter.py
@@ -1,17 +1,30 @@
-import pytest
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
-import paddle
-from util import (
-    run_pir_pass,
-    get_dummy_program,
-    get_bert_program,
-    get_r50_program,
-    forbid_op_lower_trt,
-    enforce_op_lower_trt,
-    warmup_shape_infer,
-    predict_program,
-)
 from converter import PaddleToTensorRTConverter
+from util import (
+    forbid_op_lower_trt,
+    get_bert_program,
+    get_dummy_program,
+    get_r50_program,
+    predict_program,
+    run_pir_pass,
+    warmup_shape_infer,
+)
+
+import paddle
 
 
 def test_paddle_to_tensorrt_conversion_dummy():
