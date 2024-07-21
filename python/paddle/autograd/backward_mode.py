6615e82452254736026b84be97673f17719ddc7b
tlxd <3553659915@qq.com>
2024-07-21 13:40:13 +0800

    tlxd0.2

--- python/paddle/autograd/backward_mode.py
+++ python/paddle/autograd/backward_mode.py
@@ -14,14 +14,14 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence
 
 import paddle
 from paddle.base import core, framework
 from paddle.base.backward import gradients_with_optimizer  # noqa: F401
 
 if TYPE_CHECKING:
-    from .. import Tensor
+    from paddle import Tensor
 
 
 __all__ = []
@@ -30,7 +30,7 @@ __all__ = []
 @framework.dygraph_only
 def backward(
     tensors: list[Tensor],
-    grad_tensors: list[Tensor, None] | None = None,
+    grad_tensors: list[Tensor | None] | None = None,
     retain_graph: bool = False,
 ) -> None:
     """
@@ -93,20 +93,20 @@ def backward(
     """
 
     def check_tensors(
-        in_out_list: list[Tensor] | tuple[Tensor, ...] | Tensor, name: str
-    ) -> list[Tensor] | tuple[Tensor, ...]:
+        in_out_list: Sequence[Tensor] | Tensor, name: str
+    ) -> Sequence[Tensor]:
         assert in_out_list is not None, f"{name} should not be None"
 
         if isinstance(in_out_list, (list, tuple)):
             assert len(in_out_list) > 0, f"{name} cannot be empty"
             for each_var in in_out_list:
                 assert isinstance(
-                    each_var, (paddle.Tensor, core.eager.Tensor)
+                    each_var, paddle.Tensor
                 ), f"Elements of {name} must be paddle.Tensor"
             return in_out_list
         else:
             assert isinstance(
-                in_out_list, (paddle.Tensor, core.eager.Tensor)
+                in_out_list, paddle.Tensor
             ), f"{name} must be Tensor or list of Tensor"
             return [in_out_list]
 
@@ -123,7 +123,7 @@ def backward(
         for each_tensor in grad_tensors:
             if each_tensor is not None:
                 assert isinstance(
-                    each_tensor, (paddle.Tensor, core.eager.Tensor)
+                    each_tensor, paddle.Tensor
                 ), "The argument 'grad_tensors' of paddle.autograd.backward is invalid, it can be 'None', 'paddle.Tensor' or 'list[None/paddle.Tensor]'."
     else:
         grad_tensors = []
