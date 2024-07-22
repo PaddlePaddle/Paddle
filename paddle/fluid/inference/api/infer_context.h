12861f1299f43b728260b2fef4b8eaf94d7f3bc8
cmcamdy <1027740945@qq.com>
2024-07-22 12:15:08 +0800

    fix xpu l3 cache bug

--- paddle/fluid/inference/api/infer_context.h
+++ paddle/fluid/inference/api/infer_context.h
@@ -18,6 +18,7 @@
 #ifdef PADDLE_WITH_XPU
 #include "paddle/phi/backends/xpu/xpu_l3_strategy.h"
 #endif
+#include <unordered_set>
 
 namespace paddle {
 
@@ -79,6 +80,8 @@ class InferXPUContext : public phi::XPUContext {
                          bool fc_autotune_file_writeback,
                          const phi::Place& place);
 
+  void SetOutHolder(phi::Allocation* holder);
+
  private:
   size_t l3_size_{0};
   void* l3_ptr_{nullptr};
@@ -90,6 +93,8 @@ class InferXPUContext : public phi::XPUContext {
   mutable std::unordered_map<phi::Allocation*,
                              std::pair<phi::Allocation*, bool>>
       holder_map_;
+
+  mutable std::unordered_set<phi::Allocation*> output_holder_set_;
   phi::XPUL3Planner l3_plan_;
 };
 #endif
