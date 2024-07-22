73b1b68f1f139b378d9e78ae4ede58a06e745c53
megemini <megemini@outlook.com>
2024-07-22 14:21:36 +0800

    [patch] pybind11-stubgen

--- tools/gen_pybind11_stub.py
+++ tools/gen_pybind11_stub.py
@@ -14,7 +14,103 @@
 
 from __future__ import annotations
 
-import subprocess
+import functools
+import inspect
+import keyword
+import logging
+
+from pybind11_stubgen import (
+    CLIArgs,
+    Printer,
+    Writer,
+    run,
+    stub_parser_from_args,
+    to_output_and_subdir,
+)
+
+
+def patch_pybind11_stubgen_printer():
+    # patch name with suffix '_' if `name` is a keyword like `in`
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for arg in args:
+                if hasattr(arg, 'name') and keyword.iskeyword(arg.name):
+                    arg.name += '_'
+            for k, w in kwargs.items():
+                if hasattr(w, 'name') and keyword.iskeyword(arg.name):
+                    kwargs[k].name += '_'
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    for name, value in inspect.getmembers(Printer):
+        if inspect.isfunction(value) and name.startswith('print_'):
+            setattr(Printer, name, decorator(getattr(Printer, name)))
+
+    # patch invalid exp with `"` as a `typing.Any`
+    def print_invalid_exp(self, invalid_expr) -> str:
+        if self.invalid_expr_as_ellipses:
+            return "..."
+        return f'"{invalid_expr.text}"'
+
+    Printer.print_invalid_exp = print_invalid_exp
+
+
+def main(
+    output_dir: str, module_name: str, ignore_all_errors: bool = False
+) -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(name)s - [%(levelname)7s] %(message)s",
+    )
+
+    args = CLIArgs(
+        output_dir=output_dir,
+        root_suffix=None,
+        ignore_invalid_expressions=None,
+        ignore_invalid_identifiers=None,
+        ignore_unresolved_names=None,
+        ignore_all_errors=ignore_all_errors,
+        enum_class_locations=[],
+        numpy_array_wrap_with_annotated=False,
+        numpy_array_use_type_var=False,
+        numpy_array_remove_parameters=False,
+        print_invalid_expressions_as_is=True,
+        print_safe_value_reprs=None,
+        exit_code=False,
+        dry_run=False,
+        stub_extension='pyi',
+        module_name=module_name,
+    )
+
+    parser = stub_parser_from_args(args)
+    printer = Printer(
+        invalid_expr_as_ellipses=not args.print_invalid_expressions_as_is
+    )
+
+    out_dir, sub_dir = to_output_and_subdir(
+        output_dir=args.output_dir,
+        module_name=args.module_name,
+        root_suffix=args.root_suffix,
+    )
+
+    run(
+        parser,
+        printer,
+        args.module_name,
+        out_dir,
+        sub_dir=sub_dir,
+        dry_run=args.dry_run,
+        writer=Writer(stub_ext=args.stub_extension),
+    )
+
 
 if __name__ == '__main__':
-    subprocess.run(['pybind11-stubgen', ''])
+    patch_pybind11_stubgen_printer()
+
+    main(
+        output_dir='/home/shun/venv38dev/lib/python3.8/site-packages/',
+        module_name='paddle.base.core',
+        ignore_all_errors=True,
+    )
