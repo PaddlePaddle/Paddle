346544bb162ec50df03753129d487cceaf579981
Caogration <120171783+Caogration@users.noreply.github.com>
2024-07-23 16:21:22 +0800

    Update test_tensor.py with pre-commit of import subsquence

--- test/legacy_test/test_tensor.py
+++ test/legacy_test/test_tensor.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import numbers
+import os
 import unittest
 
 import numpy as np
