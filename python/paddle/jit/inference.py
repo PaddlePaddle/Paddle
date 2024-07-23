a0a7ad469632f3cdf6ea0e92d75a669e78b07626
zhoutianzi666 <17801055074@163.com>
2024-07-23 11:56:04 +0000

    add a kong line

--- python/paddle/jit/inference.py
+++ python/paddle/jit/inference.py
@@ -396,6 +396,7 @@ def paddle_inference_decorator(function=None, **kwargs):
     Converts dynamic graph APIs into static graph saved in disk. Then will use Paddle Inference to predictor based on
     the static model in the disk.
     This function return a callable function, user can use it to inference just like dynamic function.
+    
     Args:
         function (callable): Callable dynamic graph function. It must be a member function of paddle.nn.Layer.
         If it used as a decorator, the decorated function will be parsed as this parameter.
