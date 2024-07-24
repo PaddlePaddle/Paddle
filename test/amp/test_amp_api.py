7fc75e714f265002ac3772dfc6fcbc54d5336ade
ooooo <3164076421@qq.com>
2024-07-24 16:12:14 +0800

    fix pir ut

--- test/amp/test_amp_api.py
+++ test/amp/test_amp_api.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import unittest
+from contextlib import contextmanager
 
 import numpy as np
 from amp_base_models import AmpTestBase
@@ -413,6 +414,16 @@ class SimpleModelIncludeSetValue(nn.Layer):
         return z
 
 
+# Copy from ../dygraph_to_static/dygraph_to_static_utils.py
+@contextmanager
+def pir_dygraph_guard():
+    in_dygraph_mode = paddle.in_dynamic_mode()
+    with paddle.pir_utils.IrGuard():
+        if in_dygraph_mode:
+            paddle.disable_static()
+        yield
+
+
 @unittest.skipIf(
     not core.is_compiled_with_cuda() and not core.is_compiled_with_xpu(),
     "Require compiled with CUDA or XPU.",
@@ -448,7 +459,7 @@ class TestDy2STWithSetValue(AmpTestBase):
         func = paddle.jit.to_static(func, full_graph=True)
         input = paddle.randn((2, 3))
 
-        with paddle.amp.auto_cast(level='O2'):
+        with paddle.amp.auto_cast(level='O2', use_promote=False):
             res = func(input)
             loss = res.sum()
             prog = func.forward.get_concrete_program(input)[1].forward_program
@@ -460,27 +471,31 @@ class TestDy2STWithSetValue(AmpTestBase):
         )
 
     def test_pir_op_called_as_expected(self):
-        if not paddle.framework.use_pir_api():
-            return
         expected_fp16_calls = {
             "pd_op.cast_": 1,
             "pd_op.layer_norm": 1,
-            "pd_op.scale": 3,
+            "pd_op.scale": 1,
+            "pd_op.scale_": 2,
             "pd_op.set_value_with_tensor_": 1,
         }
 
-        func = SimpleModelIncludeSetValue()
-        func = paddle.amp.decorate(func, level='O2')
-        func = paddle.jit.to_static(func, full_graph=True)
-        input = paddle.randn((2, 3))
+        with pir_dygraph_guard():
+            func = SimpleModelIncludeSetValue()
+            func = paddle.amp.decorate(func, level='O2')
+            func = paddle.jit.to_static(func, full_graph=True)
+            input = paddle.randn((2, 3))
 
-        with paddle.amp.auto_cast(level='O2'):
-            res = func(input)
-            loss = res.sum()
-            paddle.amp.debugging.disable_operator_stats_collection()
-            op_stats = paddle.base.core.get_low_precision_op_list()
-        loss.backward()
-        self._check_op_calls(op_stats, expected_fp16_calls=expected_fp16_calls)
+            paddle.amp.debugging.enable_operator_stats_collection()
+            with paddle.amp.auto_cast(level='O2', use_promote=False):
+                res = func(input)
+                loss = res.sum()
+                paddle.amp.debugging.disable_operator_stats_collection()
+                op_stats = paddle.base.core.get_low_precision_op_list()
+
+            loss.backward()
+            self._check_op_calls(
+                op_stats, expected_fp16_calls=expected_fp16_calls
+            )
 
 
 if __name__ == '__main__':
