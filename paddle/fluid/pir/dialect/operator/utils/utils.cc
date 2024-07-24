bf71ee45e06edbf549fd3b726ffa1fb1fdb5943b
liym27 <33742067+liym27@users.noreply.github.com>
2024-07-24 11:34:24 +0800

    do not rm c_allgather name from LegacyOpList

--- paddle/fluid/pir/dialect/operator/utils/utils.cc
+++ paddle/fluid/pir/dialect/operator/utils/utils.cc
@@ -53,6 +53,7 @@ const std::unordered_set<std::string> LegacyOpList = {
     CReduceSum_Op::name(),
     CAllreduceMax_Op::name(),
     CAllreduceMin_Op::name(),
+    CAllgatherOp::name(),
     CSoftmaxWithCrossEntropyOp::name(),
     CSoftmaxWithCrossEntropyGradOp::name(),
     CSplitOp::name(),
