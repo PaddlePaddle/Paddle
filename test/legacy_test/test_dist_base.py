7590ef0e392ef7616b164a83e66748d91f7c56ae
co63oc <co63@163.com>
2024-07-28 10:14:33 +0800

    Fix

--- test/legacy_test/test_dist_base.py
+++ test/legacy_test/test_dist_base.py
@@ -1669,7 +1669,7 @@ class TestDistBase(unittest.TestCase):
             "NCCL_P2P_DISABLE": "1",
             "NCCL_SHM_DISABLE": "1",
             "FLAGS_new_executor_static_build": "1",
-            "FLAGS_dynamic_static_unified_comm": "0",
+            "FLAGS_dynamic_static_unified_comm": "1",
         }
 
         if check_error_log:
