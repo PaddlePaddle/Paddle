ad2212ca8f977601e72cae6c76ee0c4216d6c21a
ZhenxingLi <zhenxingli@hust.edu.cn>
2024-07-29 15:33:45 +0800

    Update test_collective_api_base.py

--- test/legacy_test/test_collective_api_base.py
+++ test/legacy_test/test_collective_api_base.py
@@ -125,7 +125,9 @@ class TestCollectiveAPIRunnerBase:
         rank = args["trainerid"]
         current_endpoint = args["currentendpoint"]
         nranks = 2
-        if args["use_comm_context"]:
+        if args['static_mode'] and (
+            args["use_comm_context"] or args["dynamic_static_unified_comm"]
+        ):
             paddle.distributed.collective._init_parallel_env(args["backend"])
         else:
             paddle.distributed.init_parallel_env()
