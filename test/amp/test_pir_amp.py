b5ca4a7251c118b7739d9c05cfe146004f2921bd
jeff41404 <jeff41404@gmail.com>
2024-07-31 12:35:03 +0800

    fix amp bf16 error in dygraph with pir

--- test/amp/test_pir_amp.py
+++ test/amp/test_pir_amp.py
@@ -64,6 +64,32 @@ class TestPirAMPProgram(unittest.TestCase):
             np.testing.assert_equal(len(_white_list), 0)
             np.testing.assert_equal(len(_black_list), 0)
 
+    def test_linear_amp_bf16_o1(self):
+        if not core.is_compiled_with_cuda():
+            return
+        with paddle.pir_utils.IrGuard():
+            startup = paddle.static.Program()
+            main = paddle.static.Program()
+            with paddle.static.program_guard(main, startup):
+                x = paddle.static.data('x', [3, 4], 'float32')
+                linear = paddle.nn.Linear(4, 5)
+                with paddle.amp.auto_cast(
+                    level='O1', dtype='bfloat16', use_promote=True
+                ):
+                    out1 = linear(x)
+                    out2 = paddle.mean(out1)
+
+            cast_op_count = 0
+            for op in main.global_block().ops:
+                if op.name() == 'pd_op.cast':
+                    cast_op_count += 1
+            np.testing.assert_equal(out1.dtype, core.DataType.FLOAT32)
+            np.testing.assert_equal(out2.dtype, core.DataType.FLOAT32)
+            np.testing.assert_equal(cast_op_count, 3)
+            _white_list, _black_list = core._get_amp_op_list()
+            np.testing.assert_equal(len(_white_list), 0)
+            np.testing.assert_equal(len(_black_list), 0)
+
     def test_linear_amp_o2_without_scaler(self):
         if not core.is_compiled_with_cuda():
             return
@@ -139,6 +165,47 @@ class TestPirAMPProgram(unittest.TestCase):
                 if op.name() == 'pd_op.cast':
                     cast_op_count += 1
             np.testing.assert_equal(cast_op_count, 5)
+            place = paddle.CUDAPlace(0)
+            exe = paddle.static.Executor(place)
+            exe.run(startup)
+            result = exe.run(
+                main,
+                feed={'x': np.random.rand(3, 4).astype('float32')},
+                fetch_list=[loss],
+            )
+
+    def test_linear_amp_bf16_o2_without_scaler(self):
+        if not core.is_compiled_with_cuda():
+            return
+        with paddle.pir_utils.IrGuard():
+            startup = paddle.static.Program()
+            main = paddle.static.Program()
+            with paddle.static.program_guard(main, startup):
+                x = paddle.static.data('x', [3, 4], 'float32')
+                linear = paddle.nn.Linear(4, 5)
+                optimizer = paddle.optimizer.Adam(
+                    learning_rate=0.001, parameters=linear.parameters()
+                )
+                linear, optimizer = paddle.amp.decorate(
+                    models=linear,
+                    optimizers=optimizer,
+                    level='O2',
+                    dtype='bfloat16',
+                    master_weight=True,
+                    master_grad=True,
+                )
+
+                with paddle.amp.auto_cast(
+                    level='O2', dtype='bfloat16', use_promote=True
+                ):
+                    out = linear(x)
+                    loss = paddle.mean(out)
+                optimizer.minimize(loss)
+            cast_op_count = 0
+            for op in main.global_block().ops:
+                if op.name() == 'pd_op.cast':
+                    cast_op_count += 1
+            np.testing.assert_equal(cast_op_count, 3)
             place = paddle.CUDAPlace(0)
             exe = paddle.static.Executor(place)
             exe.run(startup)
