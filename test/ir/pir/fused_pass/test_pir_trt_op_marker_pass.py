4c41ffc5b3ed15b14e853d16ce1fedf868bd5d6e
layssy <lay1211252464@163.com>
2024-08-01 07:02:52 +0000

    添加split_with_num的maker和单测

--- test/ir/pir/fused_pass/test_pir_trt_op_marker_pass.py
+++ test/ir/pir/fused_pass/test_pir_trt_op_marker_pass.py
@@ -544,6 +544,41 @@ class TestNonZeroTRTPattern(PassTest):
     def test_check_output(self):
         self.check_pass_correct()
 
+class TestSplitWithNumTRTPattern(PassTest):
+    def is_program_valid(self, program=None):
+        return True
+    def sample_program(self):
+        with paddle.pir_utils.IrGuard():
+            main_prog = paddle.static.Program()
+            start_prog = paddle.static.Program()
+            with paddle.pir.core.program_guard(main_prog, start_prog):
+                x = paddle.static.data(
+                    name='x', shape=[3,9,5], dtype='int64'
+                )
+                num_or_sections = 3
+                axis = 1
+                split_out = paddle.split(x, num_or_sections=num_or_sections, axis=axis)
+                print(f'split_out:{split_out}')
+                out = paddle.assign(split_out[0])
+                out1 = paddle.assign(split_out[1])
+                out2 = paddle.assign(split_out[2])
+                self.pass_attr_list = [{'trt_op_marker_pass': {}}]
+                self.feeds = {
+                    "x": np.random.random([3,9,5]).astype("int64"),
+                }
+
+                self.fetch_list = [out,out1,out2]
+                self.valid_op_map = {
+                    "pd_op.fusion_transpose_flatten_concat": 0,
+                }
+                yield [main_prog, start_prog], False
+
+    def setUp(self):
+        if core.is_compiled_with_cuda():
+            self.places.append(paddle.CUDAPlace(0))
+
+    def test_check_output(self):
+        self.check_pass_correct()
 
 class TestGeluTRTPattern(PassTest):
     def is_program_valid(self, program=None):
