2adbcede414ec306d18c714a14fc90cdaadeafc9
gouzi <530971494@qq.com>
2024-08-03 18:09:36 +0800

    fix windows

--- cmake/external/glog.cmake
+++ cmake/external/glog.cmake
@@ -19,14 +19,13 @@ set(GLOG_INSTALL_DIR ${THIRD_PARTY_PATH}/install/glog)
 set(GLOG_INCLUDE_DIR
     "${GLOG_INSTALL_DIR}/include"
     CACHE PATH "glog include directory." FORCE)
-set(GLOG_TAG v0.4.0)
 set(SOURCE_DIR ${PADDLE_SOURCE_DIR}/third_party/glog)
 add_definitions(-DGLOG_USE_GLOG_EXPORT)
 if(WIN32)
   set(GLOG_LIBRARIES
       "${GLOG_INSTALL_DIR}/lib/glog.lib"
       CACHE FILEPATH "glog library." FORCE)
-  set(GLOG_CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4267 /wd4530")
+  set(GLOG_CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
   add_definitions("/DGOOGLE_GLOG_DLL_DECL=")
 elseif(APPLE)
   set(GLOG_LIBRARIES
