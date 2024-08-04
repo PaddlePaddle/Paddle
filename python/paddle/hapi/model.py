9f999f2bddee2eac8a39322cbbc8fba87e20aaec
megemini <megemini@outlook.com>
2024-08-04 14:27:06 +0800

    [Add] typing

--- python/paddle/hapi/model.py
+++ python/paddle/hapi/model.py
@@ -24,6 +24,7 @@ from typing import (
     TYPE_CHECKING,
     Any,
     Callable,
+    List,
     Literal,
     Sequence,
     Union,
@@ -69,8 +70,8 @@ if TYPE_CHECKING:
     _InputBatch: TypeAlias = Union[
         Tensor,
         npt.NDArray[Any],
-        list[Tensor],
-        list[npt.NDArray[Any]],
+        List[Tensor],
+        List[npt.NDArray[Any]],
     ]
 
 
