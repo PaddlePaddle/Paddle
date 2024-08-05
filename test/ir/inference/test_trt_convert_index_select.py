29fe24ed6fb413ef751b5b63793276627b202bdb
BHmingyang <zhouhy230205@gmail.com>
2024-08-05 13:24:58 +0800

    Use stardand collections for type hints in some py files

--- test/ir/inference/test_trt_convert_index_select.py
+++ test/ir/inference/test_trt_convert_index_select.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import unittest
 from functools import partial
-from typing import List
 
 import numpy as np
 from program_config import ProgramConfig, TensorConfig
@@ -96,7 +97,7 @@ class TrtConvertIndexSelectTest(TrtLayerAutoScanTest):
 
     def sample_predictor_configs(
         self, program_config
-    ) -> (paddle_infer.Config, List[int], float):
+    ) -> (paddle_infer.Config, list[int], float):
         def generate_dynamic_shape(attrs):
             if len(self.shape) == 1:
                 self.dynamic_shape.min_input_shape = {
