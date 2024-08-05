16a5571e48dcb54c49d9fb8d7b36ebb4727b8b8b
lizexu <2694294196@qq.com>
2024-08-05 11:35:52 +0000

    fix

--- paddle/pir/src/core/ir_printer.cc
+++ paddle/pir/src/core/ir_printer.cc
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <algorithm>
 #include <list>
 #include <ostream>
 #include <string>
