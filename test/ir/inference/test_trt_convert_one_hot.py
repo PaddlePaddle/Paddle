b1d1784a0b1d826ef202b5a4e964c5fbf01f8af4
MufanColin <76479709+MufanColin@users.noreply.github.com>
2024-08-05 10:51:00 +0800

    Fixed 202

--- test/ir/inference/test_trt_convert_one_hot.py
+++ test/ir/inference/test_trt_convert_one_hot.py
@@ -84,7 +84,7 @@ class TrtConvertOneHotTest(TrtLayerAutoScanTest):
 
     def sample_predictor_configs(
         self, program_config
-    ) -> (paddle_infer.Config, list[int], float):
+    ) -> tuple[paddle_infer.Config, list[int], float]:
         def generate_dynamic_shape(attrs):
             if self.dims == 1:
                 self.dynamic_shape.min_input_shape = {
