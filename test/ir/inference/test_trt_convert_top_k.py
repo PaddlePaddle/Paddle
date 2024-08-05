447344a79f42dfb1374d906d77feb25a35a68403
Nyakku Shigure <sigure.qaq@gmail.com>
2024-08-06 00:10:33 +0800

    Update test/ir/inference/test_trt_convert_top_k.py

--- test/ir/inference/test_trt_convert_top_k.py
+++ test/ir/inference/test_trt_convert_top_k.py
@@ -78,7 +78,7 @@ class TrtConvertActivationTest(TrtLayerAutoScanTest):
 
     def sample_predictor_configs(
         self, program_config
-    ) -> (paddle_infer.Config, list[int], float):
+    ) -> tuple[paddle_infer.Config, list[int], float]:
         def generate_dynamic_shape(attrs):
             if self.dims == 1:
                 self.dynamic_shape.min_input_shape = {"input_data": [1]}
