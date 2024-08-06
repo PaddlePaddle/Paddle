b90a75ba7e1eae2b6c2767b304c19c4117cbd551
Fripping <15010770306@163.com>
2024-08-06 15:03:56 +0800

    update cinn api

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/.ipynb_checkpoints/binary_infer_sym-checkpoint.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/.ipynb_checkpoints/binary_infer_sym-checkpoint.cc
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.h"
 #include "paddle/common/ddim.h"
+#include "paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.h"
 #include "paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/infer_sym_utils.h"
 
 namespace {
