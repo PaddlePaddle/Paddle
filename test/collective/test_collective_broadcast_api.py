1ac8b81fd81d4e3f9add158dba55fb5d3927060d
ZhenxingLi <zhenxingli@hust.edu.cn>
2024-08-06 21:27:21 +0800

    Update test_collective_broadcast_api.py

--- test/collective/test_collective_broadcast_api.py
+++ test/collective/test_collective_broadcast_api.py
@@ -54,6 +54,29 @@ class TestCollectiveBroadcastAPI(TestDistBase):
                 need_envs={"USE_COMM_CONTEXT": "1"},
             )
 
+    def test_broadcast_nccl_with_new_comm_pir(self):
+        dtypes_to_test = [
+            "float16",
+            "float32",
+            "float64",
+            "int32",
+            "int64",
+            "int8",
+            "uint8",
+            "bool",
+        ]
+        for dtype in dtypes_to_test:
+            self.check_with_place(
+                "collective_broadcast_api.py",
+                "broadcast",
+                "nccl",
+                dtype=dtype,
+                need_envs={
+                    "FLAGS_dynamic_static_unified_comm": "true",
+                    "FLAGS_enable_pir_in_executor": "1",
+                },
+            )
+
     def test_broadcast_gloo(self):
         self.check_with_place(
             "collective_broadcast_api.py",
