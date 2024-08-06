69c72a59e744171db06c6f1829c8e599f2b381fb
crazyxiaoxi <113622186+crazyxiaoxi@users.noreply.github.com>
2024-08-06 12:33:30 +0800

    static check

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
@@ -94,7 +94,7 @@ bool BoxClipOpInferSymbolicShape(
     pir::Operation *op, pir::InferSymbolicShapeContext *infer_context) {
   const auto &input_shape =
       infer_context->GetShapeOrDataForValue(op->operand_source(0));
-  
+
   // Set output shape to be the same as input shape
   std::vector<symbol::DimExpr> output_shape = input_shape.shape();
   infer_context->SetShapeOrDataForValue(
