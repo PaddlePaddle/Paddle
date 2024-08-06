e7a72a5c207cdf9f174734c3feac36a409fd2437
Caogration <120171783+Caogration@users.noreply.github.com>
2024-08-06 10:25:59 +0800

    Revert auto_cast.py

--- python/paddle/amp/auto_cast.py
+++ python/paddle/amp/auto_cast.py
@@ -19,6 +19,8 @@ from typing import (
     TYPE_CHECKING,
     Any,
     Callable,
+    ContextManager,
+    List,
     Literal,
     Protocol,
     TypeVar,
@@ -41,8 +43,7 @@ from paddle.static.amp.decorator import OptimizerWithMixedPrecision
 from .amp_lists import black_list, white_list
 
 if TYPE_CHECKING:
-    from collections.abc import Generator
-    from contextlib import AbstractContextManager
+    from typing import Generator
 
     from typing_extensions import TypeAlias, TypeGuard
 
@@ -75,8 +76,8 @@ if TYPE_CHECKING:
             ...
 
 
-_ModelsT = TypeVar("_ModelsT", "Layer", list["Layer"])
-_OptimizersT = TypeVar("_OptimizersT", "_OptimizerLike", list["_OptimizerLike"])
+_ModelsT = TypeVar("_ModelsT", "Layer", List["Layer"])
+_OptimizersT = TypeVar("_OptimizersT", "_OptimizerLike", List["_OptimizerLike"])
 
 
 AMP_RELATED_FLAGS = [
@@ -1021,7 +1022,7 @@ def auto_cast(
     level: _AmpLevelLiteral = 'O1',
     dtype: _DTypeLiteral = 'float16',
     use_promote: bool = True,
-) -> AbstractContextManager:
+) -> ContextManager:
     """
     Create a context which enables auto-mixed-precision(AMP) of operators executed in dynamic graph mode.
     If enabled, the input data type (float32, float16 or bfloat16) of each operator is decided
