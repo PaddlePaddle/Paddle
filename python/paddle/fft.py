44bf6706836f26291e10ee16a0eaa4912ac01a42
Fripping <124574028+Fripping@users.noreply.github.com>
2024-08-07 12:17:15 +0800

    Update fft.py

--- python/paddle/fft.py
+++ python/paddle/fft.py
@@ -15,7 +15,7 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING
 
 import numpy as np
 
@@ -29,6 +29,8 @@ from .tensor.attribute import is_floating_point, is_integer
 from .tensor.creation import _complex_to_real_dtype, _real_to_complex_dtype
 
 if TYPE_CHECKING:
+    from typing import Literal
+    
     from paddle import Tensor
     from paddle._typing import DTypeLike
 
