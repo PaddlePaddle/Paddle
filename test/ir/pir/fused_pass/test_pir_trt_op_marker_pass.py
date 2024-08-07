2b78dec4957866832247a36b3beb890d76dd645a
layssy <lay1211252464A@163.com>
2024-08-07 03:06:05 +0000

    添加matmul算子单测

--- test/ir/pir/fused_pass/test_pir_trt_op_marker_pass.py
+++ test/ir/pir/fused_pass/test_pir_trt_op_marker_pass.py
@@ -1654,5 +1654,36 @@ class TestRemainderTRTPattern(PassTest):
 
     def test_check_output(self):
         self.check_pass_correct()  
+
+class TestMatmulTRTPattern(PassTest):
+    def is_program_valid(self, program=None):
+        return True
+
+    def sample_program(self):
+        with paddle.pir_utils.IrGuard():
+            main_prog = paddle.static.Program()
+            start_prog = paddle.static.Program()
+            with paddle.pir.core.program_guard(main_prog, start_prog):
+                x = paddle.static.data(name='x', shape=[10,5,2], dtype='float32')
+                y = paddle.static.data(name='y', shape=[10,2,5], dtype='float32')
+                matmul_out =  paddle.matmul(x, y)
+                out = paddle.assign(matmul_out)
+                self.pass_attr_list = [{'trt_op_marker_pass': {}}]
+                self.feeds = {
+                    "x":np.random.random([10,5,2]).astype("float32"),
+                    "y": np.random.random([10,2,5]).astype("float32"),
+                }
+                self.fetch_list = [out]
+                self.valid_op_map = {
+                    "pd_op.fusion_transpose_flatten_concat": 0,
+                }
+                yield [main_prog, start_prog], False
+
+    def setUp(self):
+        if core.is_compiled_with_cuda():
+            self.places.append(paddle.CUDAPlace(0))
+
+    def test_check_output(self):
+        self.check_pass_correct()  
 if __name__ == "__main__":
     unittest.main()
