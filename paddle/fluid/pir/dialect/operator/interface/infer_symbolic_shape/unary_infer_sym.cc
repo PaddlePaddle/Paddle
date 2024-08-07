2a2ca540b63f44eb286a9732338a4348e7dbeb1b
MufanColin <76479709+MufanColin@users.noreply.github.com>
2024-08-07 14:37:17 +0800

    Finished norm and p_norm

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
@@ -1158,12 +1158,33 @@ bool MeanAllOpInferSymbolicShape(
 //   return true;
 // }
 
-// bool NormOpInferSymbolicShape(pir::Operation *op,
-//                               pir::InferSymbolicShapeContext *infer_context)
-//                               {
-//   // pass
-//   return true;
-// }
+bool NormOpInferSymbolicShape(pir::Operation *op,
+                              pir::InferSymbolicShapeContext *infer_context) {
+  auto x_shape_or_data =
+      infer_context->GetShapeOrDataForValue(op->operand_source(0));
+  const auto &x_shape = x_shape_or_data.shape();
+
+  infer_context->SetShapeOrDataForValue(
+      op->result(0),
+      symbol::ShapeOrDataDimExprs{symbol::TensorShapeOrDataDimExprs(x_shape)});
+
+  int axis = op->attribute<pir::Int32Attribute>("axis").data();
+  float epsilon = op->attribute<pir::FloatAttribute>("epsilon").data();
+  bool is_test = op->attribute<pir::BoolAttribute>("is_test").data();
+
+  if (!is_test) {
+    if (axis < 0) axis += x_shape.size();
+
+    auto norm_shape = x_shape;
+    norm_shape[axis] = symbol::DimExpr(1);
+    infer_context->SetShapeOrDataForValue(
+        op->result(1),
+        symbol::ShapeOrDataDimExprs{
+            symbol::TensorShapeOrDataDimExprs(norm_shape)});
+  }
+
+  return true;
+}
 
 bool NonzeroOpInferSymbolicShape(
     pir::Operation *op, pir::InferSymbolicShapeContext *infer_context) {
@@ -1198,12 +1219,76 @@ bool NumelOpInferSymbolicShape(pir::Operation *op,
 
   return true;
 }
-// bool P_NormOpInferSymbolicShape(pir::Operation *op,
-//                                 pir::InferSymbolicShapeContext
-//                                 *infer_context) {
-//   // pass
-//   return true;
-// }
+
+bool P_NormOpInferSymbolicShape(pir::Operation *op,
+                                pir::InferSymbolicShapeContext *infer_context) {
+  auto x_shape_or_data =
+      infer_context->GetShapeOrDataForValue(op->operand_source(0));
+  const auto &x_shape = x_shape_or_data.shape();
+  auto x_rank = x_shape.size();
+
+  float porder = op->attribute<pir::FloatAttribute>("porder").data();
+  int axis = op->attribute<pir::Int32Attribute>("axis").data();
+  float epsilon = op->attribute<pir::FloatAttribute>("epsilon").data();
+  bool keepdim = op->attribute<pir::BoolAttribute>("keepdim").data();
+  bool asvector = op->attribute<pir::BoolAttribute>("asvector").data();
+
+  PADDLE_ENFORCE_GE(axis,
+                    -x_rank,
+                    common::errors::InvalidArgument(
+                        "Attr(axis) value should be in range [-R, R-1], R is "
+                        "the rank of Input(X). But received axis: %d, R: %d. "
+                        "Current Input(X)'s shape is=[%s].",
+                        axis,
+                        x_rank,
+                        x_shape));
+  PADDLE_ENFORCE_LT(axis,
+                    x_rank,
+                    common::errors::InvalidArgument(
+                        "Attr(axis) value should be in range [-R, R-1], R is "
+                        "the rank of Input(X). But received axis: %d, R: %d. "
+                        "Current Input(X)'s shape is=[%s].",
+                        axis,
+                        x_rank,
+                        x_shape));
+
+  std::vector<symbol::DimExpr> out_dim_vector;
+
+  if (asvector) {
+    if (keepdim) {
+      for (size_t i = 0; i < x_rank; ++i) {
+        out_dim_vector.emplace_back(symbol::DimExpr(1));
+      }
+    } else {
+      out_dim_vector = {};
+    }
+  } else {
+    if (axis < 0) axis += x_rank;
+
+    if (keepdim) {
+      for (size_t i = 0; i < x_rank; ++i) {
+        if (static_cast<int>(i) == axis) {
+          out_dim_vector.emplace_back(symbol::DimExpr(1));
+        } else {
+          out_dim_vector.emplace_back(x_shape[i]);
+        }
+      }
+    } else {
+      for (size_t i = 0; i < x_rank; ++i) {
+        if (static_cast<int>(i) != axis) {
+          out_dim_vector.emplace_back(x_shape[i]);
+        }
+      }
+    }
+  }
+
+  infer_context->SetShapeOrDataForValue(
+      op->result(0),
+      symbol::ShapeOrDataDimExprs{
+          symbol::TensorShapeOrDataDimExprs(out_dim_vector)});
+
+  return true;
+}
 
 // bool PartialSumOpInferSymbolicShape(pir::Operation *op,
 //                                     pir::InferSymbolicShapeContext
