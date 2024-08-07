c6de16c622364896dae765feccf048bc011af6a7
Uanu <92366232+uanu2002@users.noreply.github.com>
2024-08-07 10:18:35 +0800

    【BUAA】【Infer Symbolic Shape】add mv, shadow_feed, share_data_ (#66956)
    * add mv, shadow_feed, share_data_
    
    * fix sharedata
    
    * fix codestyle
    
    * fix

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
@@ -720,6 +720,33 @@ bool MatmulOpInferSymbolicShape(pir::Operation *op,
   return true;
 }
 
+bool MvOpInferSymbolicShape(pir::Operation *op,
+                            pir::InferSymbolicShapeContext *infer_context) {
+  const auto &x_shape_or_data =
+      infer_context->GetShapeOrDataForValue(op->operand_source(0));
+  const auto &vec_shape_or_data =
+      infer_context->GetShapeOrDataForValue(op->operand_source(1));
+  PADDLE_ENFORCE_EQ(
+      x_shape_or_data.shape().size(),
+      2,
+      phi::errors::InvalidArgument("The rank of input X should be 2, but is %d",
+                                   x_shape_or_data.shape().size()));
+  PADDLE_ENFORCE_EQ(vec_shape_or_data.shape().size(),
+                    1,
+                    phi::errors::InvalidArgument(
+                        "The rank of input Vec should be 1, but is %d",
+                        vec_shape_or_data.shape().size()));
+  infer_context->AddEqualCstr(x_shape_or_data.shape()[1],
+                              vec_shape_or_data.shape()[0]);
+
+  std::vector<symbol::DimExpr> out_shape = {x_shape_or_data.shape()[0]};
+  infer_context->SetShapeOrDataForValue(
+      op->result(0),
+      symbol::ShapeOrDataDimExprs{
+          symbol::TensorShapeOrDataDimExprs(out_shape)});
+  return true;
+}
+
 // bool PullBoxSparseOpInferSymbolicShape(pir::Operation *op,
 //                                        pir::InferSymbolicShapeContext
 //                                        *infer_context) {
