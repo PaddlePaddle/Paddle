63cc8f6354f9044b0f327bbbaf6bdea6a9f1d4b2
lwkhahaha <lwk13522087721@163.com>
2024-08-09 11:21:24 +0800

    test

--- paddle/cinn/ir/ir_analyzer/ir_analyzer.cc
+++ paddle/cinn/ir/ir_analyzer/ir_analyzer.cc
@@ -70,11 +70,11 @@ std::vector<Expr> GetLoops(const std::vector<Expr>& exprs, const Expr& block) {
       block.As<ir::ScheduleBlockRealize>(),
       phi::errors::InvalidArgument(
           "The block must be convertible to ir::ScheduleBlockRealize."));
-  PADDLE_ENFORCE_EQ(block.As<ir::ScheduleBlockRealize>()
-                        ->schedule_block.As<ir::ScheduleBlock>(),
-                    true,
-                    phi::errors::InvalidArgument(
-                        "Cannot cast block to ir::ScheduleBlockRealize."));
+  PADDLE_ENFORCE_NOT_NULL(
+      block.As<ir::ScheduleBlockRealize>()
+          ->schedule_block.As<ir::ScheduleBlock>(),
+      phi::errors::InvalidArgument(
+          "Cannot cast block to ir::ScheduleBlockRealize."));
   std::string block_name = block.As<ir::ScheduleBlockRealize>()
                                ->schedule_block.As<ir::ScheduleBlock>()
                                ->name;
@@ -197,11 +197,10 @@ Expr AddUnitLoop(const std::vector<Expr>& exprs, const Expr& block) {
       true,
       phi::errors::InvalidArgument(
           "The block is not convertible to ir::ScheduleBlockRealize. It must "
-          "be convertible to ir::ScheduleBlockRealize."))
-  PADDLE_ENFORCE_EQ(
+          "be convertible to ir::ScheduleBlockRealize."));
+  PADDLE_ENFORCE_NOT_NULL(
       block.As<ir::ScheduleBlockRealize>()
           ->schedule_block.As<ir::ScheduleBlock>(),
-      true,
       phi::errors::InvalidArgument(
           "The schedule_block must be convertible to ir::ScheduleBlock."))
   std::string block_name = block.As<ir::ScheduleBlockRealize>()
