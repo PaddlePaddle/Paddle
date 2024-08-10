7f8f1f3f66eed60245a41831cae4a0f1f044b5cd
MufanColin <76479709+MufanColin@users.noreply.github.com>
2024-08-10 20:32:26 +0800

    added .shape()

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/multiary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/multiary_infer_sym.cc
@@ -829,7 +829,8 @@ bool EditDistanceOpInferSymbolicShape(
     infer_context->AddEqualCstr(refs_dims[1], one);
   }
 
-  infer_context->SetShapeOrDataForValue(op->result(0), refs_shape_or_data);
+  infer_context->SetShapeOrDataForValue(op->result(0),
+                                        refs_shape_or_data.shape());
   symbol::ShapeOrDataDimExprs single_dim_expr(symbol::TensorShapeOrDataDimExprs(
       std::vector<symbol::DimExpr>{symbol::DimExpr(1)}));
   infer_context->SetShapeOrDataForValue(
