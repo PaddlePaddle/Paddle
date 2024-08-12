6ae685135f5222383b70605cb3e9650f76cd1e38
lwkhahaha <lwk13522087721@163.com>
2024-08-12 13:34:14 +0800

    【Infer Symbolic Shape BUAA No.164】Add psroi_pool op

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/multiary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/multiary_infer_sym.cc
@@ -1374,7 +1374,7 @@ bool PsroiPoolOpInferSymbolicShape(
       infer_context->GetShapeOrDataForValue(op->operand_source(2));
   const std::vector<symbol::DimExpr> &rois_num_dims =
       rois_num_shape_or_data.shape();
-  if (rois_num_dims) {
+  if (!rois_num_dims.empty()) {
     PADDLE_ENFORCE_EQ(
         rois_num_dims.size(),
         1,
