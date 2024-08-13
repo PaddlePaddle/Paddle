4ce160597d35b7174ac564a177c90b6a143e5aac
inaomIIsFarell <1344594208@qq.com>
2024-08-13 15:15:12 +0800

    [Typing][C-86]Add type annotations for `python/paddle/incubate/operators/graph_khop_sampler.py`

--- python/paddle/incubate/operators/graph_khop_sampler.py
+++ python/paddle/incubate/operators/graph_khop_sampler.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# doctest: +SKIP('Number of sample edges dismatch, the sample kernel has error.')
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Literal, overload
