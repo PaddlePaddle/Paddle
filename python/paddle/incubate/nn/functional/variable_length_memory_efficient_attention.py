68be16aa1a86d8112ae94a25b5f7d9f73d206f19
inaomIIsFarell <1344594208@qq.com>
2024-08-13 15:16:04 +0800

    [Typing][C-80]Add type annotations for `python/paddle/incubate/nn/functional/variable_length_memory_efficient_attention.py`

--- python/paddle/incubate/nn/functional/variable_length_memory_efficient_attention.py
+++ python/paddle/incubate/nn/functional/variable_length_memory_efficient_attention.py
@@ -18,6 +18,8 @@
 #
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
+
+# doctest: +SKIP('Number of sample edges dismatch, the sample kernel has error.')
 from __future__ import annotations
 
 import math
