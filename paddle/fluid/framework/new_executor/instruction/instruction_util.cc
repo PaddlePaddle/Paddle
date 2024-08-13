267f0944e61a600541732cfac203cfba54bdaf9c
liym27 <33742067+liym27@users.noreply.github.com>
2024-08-14 01:10:59 +0800

    fix

--- paddle/fluid/framework/new_executor/instruction/instruction_util.cc
+++ paddle/fluid/framework/new_executor/instruction/instruction_util.cc
@@ -137,38 +137,35 @@ phi::DeviceContext* ParseDeviceContext(pir::Operation* op,
     // handle comm op
     if (op_attributes.count("ring_id") != 0 &&
         FLAGS_dynamic_static_unified_comm) {
-      // 1. check
       int ring_id =
           op_attributes.at("ring_id").dyn_cast<pir::Int32Attribute>().data();
       const auto& comm_context_manager =
           phi::distributed::CommContextManager::GetInstance();
-      PADDLE_ENFORCE_EQ(
-          comm_context_manager.Has(std::to_string(ring_id)),
-          true,
-          common::errors::PreconditionNotMet(
-              "ring_id %d not found in comm_context_manager for op %s",
-              ring_id,
-              op_name.c_str()));
-      // 2. get dev_ctx and set comm_context
-      auto comm_context = comm_context_manager.Get(std::to_string(ring_id));
-      dev_ctx = static_cast<platform::DeviceContext*>(
-          static_cast<phi::distributed::NCCLCommContext*>(comm_context)
-              ->GetDevContext());
-      dev_ctx->SetCommContext(comm_context);
+      if (comm_context_manager.Has(std::to_string(ring_id))) {
+        // get dev_ctx and set comm_context
+        auto comm_context = comm_context_manager.Get(std::to_string(ring_id));
+        dev_ctx = static_cast<platform::DeviceContext*>(
+            static_cast<phi::distributed::NCCLCommContext*>(comm_context)
+                ->GetDevContext());
+        dev_ctx->SetCommContext(comm_context);
 
-      // 3. set default stream if need
-      if (phi::is_gpu_place(place) && execution_stream == kDefaultStream) {
-        if (origin_dev_ctx != nullptr) {
-          auto default_stream =
-              static_cast<phi::GPUContext*>(origin_dev_ctx)->cuda_stream();
-          static_cast<phi::GPUContext*>(dev_ctx)->SetCUDAStream(default_stream,
-                                                                false);
-        } else {
-          VLOG(3) << "op " << op_name << " ring_id " << ring_id
-                  << " origin_dev_ctx is nullptr";
+        // set default stream if need
+        if (phi::is_gpu_place(place) && execution_stream == kDefaultStream) {
+          if (origin_dev_ctx != nullptr) {
+            auto default_stream =
+                static_cast<phi::GPUContext*>(origin_dev_ctx)->cuda_stream();
+            static_cast<phi::GPUContext*>(dev_ctx)->SetCUDAStream(
+                default_stream, false);
+          } else {
+            VLOG(3) << " origin_dev_ctx is nullptr for op " << op_name
+                    << " ring_id " << ring_id;
+          }
         }
+        return dev_ctx;
+      } else {
+        VLOG(3) << "ring_id " << ring_id
+                << " not found in comm_context_manager for op " << op_name;
       }
-      return dev_ctx;
     }
 #endif
   }
