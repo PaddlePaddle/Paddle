35efafcbb561ff0b6cd4caf5e9d92a46583ad788
crazyxiaoxi <113622186+crazyxiaoxi@users.noreply.github.com>
2024-08-16 14:01:44 +0800

    try

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/multiary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/multiary_infer_sym.cc
@@ -1475,7 +1475,7 @@ bool SpectralNormOpInferSymbolicShape(
   if (u_shape[0].Get<std::int64_t>() > 0 && h.Get<std::int64_t>() > 0) {
     infer_context->AddEqualCstr(u_shape[0], h);
   }
-  if (v_shape[0].Get<std::int64_t>() > 0 && w > symbol::DimExpr(0)) {
+  if (v_shape[0].Get<std::int64_t>() > 0 && w.Get<std::int64_t>() > 0) {
     infer_context->AddEqualCstr(v_shape[0], w);
   }
 
