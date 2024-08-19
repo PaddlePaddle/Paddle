9f9c625af721ec0899b4b5eee4fbe7b4742bc4c9
liujinnan <1823192871@qq.com>
2024-08-19 10:47:42 +0000

    fix is_zero bug

--- paddle/cinn/common/ir_util.cc
+++ paddle/cinn/common/ir_util.cc
@@ -250,7 +250,7 @@ bool is_zero(Expr v) {
   auto *float_n = v.As<ir::FloatImm>();
 
   if (int_n) return int_n->value == 0;
-  if (float_n) return float_n->value = 0.f;
+  if (float_n) return float_n->value == 0.f;
   return false;
 }
 
