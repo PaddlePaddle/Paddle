c900d1ede1c9c40299886a0bdcb1f5766ae1321a
risemeup1 <515586620@qq.com>
2024-08-21 13:39:19 +0000

    fix

--- python/paddle/utils/cpp_extension/cpp_extension.py
+++ python/paddle/utils/cpp_extension/cpp_extension.py
@@ -25,9 +25,6 @@ import re
 import setuptools
 from setuptools.command.easy_install import easy_install
 from setuptools.command.build_ext import build_ext
-from setuptools._distutils.dep_util import newer_group
-from setuptools.dist import DistutilsSetupError
-from setuptools._distutils import log
 from distutils.command.build import build
 
 
@@ -369,6 +366,52 @@ def _generate_extension_name(sources):
     return '_'.join(file_prefix)
 
 
+def paddle_single_extension_compile(
+    self,
+    sources,
+    output_dir=None,
+    macros=None,
+    include_dirs=None,
+    debug=False,
+    extra_preargs=None,
+    extra_postargs=None,
+    depends=None,
+):
+    # A concrete compiler class can either override this method
+    # entirely or implement _compile().
+    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(
+        output_dir, macros, include_dirs, sources, depends, extra_postargs
+    )
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+    # Create a thread pool
+    worke_number = min(os.cpu_count(), len(objects))
+    with ThreadPoolExecutor(max_workers=worke_number) as executor:
+        # Submit all compilation tasks to the thread pool.
+        futures = {
+            executor.submit(
+                self._compile,
+                obj,
+                build[obj][0],
+                build[obj][1],
+                cc_args,
+                extra_postargs,
+                pp_opts,
+            ): obj
+            for obj in objects
+        }
+
+        for future in concurrent.futures.as_completed(futures):
+            obj = futures[future]
+            try:
+                future.result()
+            except Exception as exc:
+                print(f'{obj!r} generated an exception: {exc}')
+            else:
+                print(f'{obj} is compiled')
+    # Return *all* object filenames, not just the ones we just built.
+    return objects
+
+
 class BuildExtension(build_ext):
     """
     Inherited from setuptools.command.build_ext to customize how to apply
@@ -416,6 +459,12 @@ class BuildExtension(build_ext):
         # and `load` interface rely on this attribute.
         if self.output_dir is not None:
             self.build_lib = self.output_dir
+
+    def build_extension(self, ext):
+        orignal_compile = self.compiler.__class__.compile
+        self.compiler.__class__.compile = paddle_single_extension_compile
+        super().build_extension(ext)
+        self.compiler.__class__.compile = orignal_compile
 
     def build_extensions(self) -> None:
         if OS_NAME.startswith("darwin"):
@@ -657,101 +706,6 @@ class BuildExtension(build_ext):
         # Reset runtime library path on MacOS platform
         so_path = self.get_ext_fullpath(self.extensions[0]._full_name)
         _reset_so_rpath(so_path)
-
-    def build_extension(self, ext):
-        sources = ext.sources
-        if sources is None or not isinstance(sources, (list, tuple)):
-            raise DistutilsSetupError(
-                f"in 'ext_modules' option (extension '{ext.name}'), "
-                "'sources' must be present and must be "
-                "a list of source filenames"
-            )
-        # sort to make the resulting .so file build reproducible
-        sources = sorted(sources)
-
-        ext_path = self.get_ext_fullpath(ext.name)
-        depends = sources + ext.depends
-        if not (self.force or newer_group(depends, ext_path, 'newer')):
-            log.debug("skipping '%s' extension (up-to-date)", ext.name)
-            return
-        else:
-            log.info("building '%s' extension", ext.name)
-
-        # First, scan the sources for SWIG definition files (.i), run
-        # SWIG on 'em to create .c files, and modify the sources list
-        # accordingly.
-        sources = self.swig_sources(sources, ext)
-
-        # Next, compile the source code to object files.
-
-        # XXX not honouring 'define_macros' or 'undef_macros' -- the
-        # CCompiler API needs to change to accommodate this, and I
-        # want to do one thing at a time!
-
-        # Two possible sources for extra compiler arguments:
-        #   - 'extra_compile_args' in Extension object
-        #   - CFLAGS environment variable (not particularly
-        #     elegant, but people seem to expect it and I
-        #     guess it's useful)
-        # The environment variable should take precedence, and
-        # any sensible compiler will give precedence to later
-        # command line args.  Hence we combine them in order:
-        extra_args = ext.extra_compile_args or []
-
-        macros = ext.define_macros[:]
-        for undef in ext.undef_macros:
-            macros.append((undef,))
-
-        # 创建一个线程池
-        worke_number = os.cpu_count()
-        with ThreadPoolExecutor(max_workers=worke_number) as executor:
-            # 提交所有的编译任务到线程池
-            futures = {
-                executor.submit(
-                    self.compiler.compile,
-                    [source],
-                    output_dir=self.build_temp,
-                    macros=macros,
-                    include_dirs=ext.include_dirs,
-                    debug=self.debug,
-                    extra_postargs=extra_args,
-                    depends=ext.depends,
-                ): source
-                for source in sources
-            }
-
-        # 等待所有任务完成，并获取结果
-        objects = [
-            future.result()[0]
-            for future in concurrent.futures.as_completed(futures)
-        ]
-
-        # XXX outdated variable, kept here in case third-part code
-        # needs it.
-        self._built_objects = objects[:]
-
-        # Now link the object files together into a "shared object" --
-        # of course, first we have to figure out all the other things
-        # that go into the mix.
-        if ext.extra_objects:
-            objects.extend(ext.extra_objects)
-        extra_args = ext.extra_link_args or []
-
-        # Detect target language, if not provided
-        language = ext.language or self.compiler.detect_language(sources)
-
-        self.compiler.link_shared_object(
-            objects,
-            ext_path,
-            libraries=self.get_libraries(ext),
-            library_dirs=ext.library_dirs,
-            runtime_library_dirs=ext.runtime_library_dirs,
-            extra_postargs=extra_args,
-            export_symbols=self.get_export_symbols(ext),
-            debug=self.debug,
-            build_temp=self.build_temp,
-            target_lang=language,
-        )
 
     def get_ext_filename(self, fullname: str) -> str:
         # for example: customized_extension.cpython-37m-x86_64-linux-gnu.so
