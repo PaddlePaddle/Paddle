6d980b5fd67bccac4e6b411d2bfa94adee4779a9
zhanglirong1999 <1695074375@qq.com>
2024-08-21 13:15:24 +0800

    delete useless

--- paddle/fluid/pir/transforms/onednn/cpu_bfloat16_placement_pass.cc
+++ paddle/fluid/pir/transforms/onednn/cpu_bfloat16_placement_pass.cc
@@ -1,4 +1,3 @@
-// REGISTER_IR_PASS(cpu_bfloat16_placement_pass, OneDNNPlacementBf16Pass);
 // Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
