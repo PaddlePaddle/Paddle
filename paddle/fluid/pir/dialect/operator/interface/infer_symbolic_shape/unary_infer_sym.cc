600f77f740e5aad7bfb603dab6200bf010f59498
Jeff114514 <928430299@qq.com>
2024-08-22 23:37:53 +0800

    fix1

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/unary_infer_sym.cc
@@ -1723,7 +1723,7 @@ bool NanmedianOpInferSymbolicShape(
     for (size_t i = 0; i < axis_list.size(); i++) {
       if (axis_list[i] < 0) {
         axis_list[i] += x_rank;
-        infer_context->AddEqualCstr(axis_list[i], DimExpr(0))
+        infer_context->AddEqualCstr(axis_list[i], symbol::DimExpr(0));
       }
       if (x_rank == 0) {
         infer_context->AddGreatThanOneCstr(axis_list[i]);
