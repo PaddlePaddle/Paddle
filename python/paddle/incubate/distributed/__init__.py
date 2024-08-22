552773a934f6691990fa44267ebaa27fe46f0efe
megemini <megemini@outlook.com>
2024-08-22 21:20:23 +0800

    [Fix] re export

--- python/paddle/incubate/distributed/__init__.py
+++ python/paddle/incubate/distributed/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import utils  # noqa: F401
+from . import utils as utils
--- python/paddle/incubate/distributed/utils/__init__.py
+++ python/paddle/incubate/distributed/utils/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import io  # noqa: F401
+from . import io as io
