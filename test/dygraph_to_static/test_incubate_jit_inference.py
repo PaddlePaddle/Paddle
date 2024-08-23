a358089bc0afe0be7e4c1f400750e2c64f6796ce
zhoutianzi666 <17801055074@163.com>
2024-08-23 03:40:51 +0000

    commit

--- test/dygraph_to_static/test_incubate_jit_inference.py
+++ test/dygraph_to_static/test_incubate_jit_inference.py
@@ -45,6 +45,7 @@ class TestLayer2(paddle.nn.Layer):
     def __init__(self, hidd):
         super().__init__()
         self.fn = paddle.nn.Linear(hidd, hidd, bias_attr=True)
+
     def forward(self, x_list, bool_value, my_dict={}):
         x = x_list[0]
         y = my_dict["y"]
@@ -61,6 +62,61 @@ class TestLayer2(paddle.nn.Layer):
             x = 2 * x
         return x
 
+
+class TestToStaticInfenrenceModel(Dy2StTestBase):
+    @test_ast_only
+    @test_legacy_and_pt_and_pir
+    def test_dygraph_static_same_result(self):
+        hidd = 1024
+        batch = 4096
+        dtype = "float32"
+        x = paddle.rand([batch, hidd], dtype=dtype)
+        my_layer = TestLayer1(hidd)
+        result0 = my_layer(x).numpy()
+        my_static_layer = paddle.incubate.jit.inference(my_layer)
+        result1 = my_layer(x).numpy()
+        np.testing.assert_allclose(result0, result1, rtol=0.001, atol=1e-05)
+
+
+class TestToStaticInfenrenceTensorRTModel(Dy2StTestBase):
+    @test_ast_only
+    def test_dygraph_static_same_result(self):
+        if paddle_infer.get_trt_compile_version()[0] == 0:
+            return
+        hidd = 1024
+        batch = 4096
+        dtype = "float32"
+        x = paddle.rand([batch, hidd], dtype=dtype)
+        my_layer = TestLayer1(hidd)
+        result0 = my_layer(x).numpy()
+        my_static_layer = paddle.incubate.jit.inference(my_layer, with_trt=True)
+        result1 = my_layer(x).numpy()
+        np.testing.assert_allclose(result0, result1, rtol=0.001, atol=1e-05)
+
+
+class TestToStaticInfenrenceFunc(Dy2StTestBase):
+    @test_ast_only
+    @test_legacy_and_pt_and_pir
+    def test_dygraph_static_same_result(self):
+        hidd = 1024
+        batch = 4096
+        dtype = "float32"
+        # test dynamic shape
+        x = paddle.rand([batch, hidd], dtype=dtype)
+        y = paddle.rand([batch + 1, hidd], dtype=dtype)
+        my_layer = TestLayer1(hidd)
+        result_x0 = my_layer(x).numpy()
+        result_y0 = my_layer(y).numpy()
+
+        my_layer.func = paddle.incubate.jit.inference(my_layer.func)
+        my_layer.func = paddle.incubate.jit.inference(my_layer.func)
+
+        result_x1 = my_layer(x).numpy()
+        result_y1 = my_layer(y).numpy()
+        np.testing.assert_allclose(result_x0, result_x1, rtol=0.001, atol=1e-05)
+        np.testing.assert_allclose(result_y0, result_y1, rtol=0.001, atol=1e-05)
+
+
 class TestToStaticInputListModel(Dy2StTestBase):
     @test_ast_only
     def test_dygraph_static_same_result(self):
@@ -69,7 +125,7 @@ class TestToStaticInputListModel(Dy2StTestBase):
         dtype = "float32"
         x = paddle.rand([batch, hidd], dtype=dtype)
         my_layer = TestLayer2(hidd)
-        my_dict = {"y": x+x}
+        my_dict = {"y": x + x}
         result0 = my_layer([x, x], bool_value=True, my_dict=my_dict).numpy()
         my_static_layer = paddle.incubate.jit.inference(my_layer)
         my_static_layer = paddle.incubate.jit.inference(my_layer)
