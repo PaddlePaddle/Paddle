9efe720ab1fde7b721c98a8fbe291f274fd17fbc
co63oc <co63oc@users.noreply.github.com>
2024-08-25 19:20:54 +0800

    Fix

--- paddle/phi/core/platform/profiler/cpu_utilization.cc
+++ paddle/phi/core/platform/profiler/cpu_utilization.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define GLOG_NO_ABBREVIATED_SEVERITIES  // msvc conflict logging with windows.h
 #include "paddle/phi/core/platform/profiler/cpu_utilization.h"
 #include <array>
 #include "glog/logging.h"
