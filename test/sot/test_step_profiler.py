c0036897171d66bcebe39e7ea1c327e8b48aa8c3
SigureMo <sigure.qaq@gmail.com>
2024-08-26 11:44:21 +0000

    disable strict mode in profiler ut

--- test/sot/test_step_profiler.py
+++ test/sot/test_step_profiler.py
@@ -18,7 +18,7 @@ import unittest
 
 import paddle
 from paddle.jit import sot
-from paddle.jit.sot.utils import sot_step_profiler_guard
+from paddle.jit.sot.utils import sot_step_profiler_guard, strict_mode_guard
 
 
 class SimpleModel(paddle.nn.Layer):
@@ -43,6 +43,7 @@ class SimpleModel(paddle.nn.Layer):
 
 class TestStepProfilerSmokeTest(unittest.TestCase):
     @sot_step_profiler_guard(True)
+    @strict_mode_guard(False)
     def test_step_profiler_smoke(self):
         model = SimpleModel()
         model = paddle.jit.to_static(model, full_graph=False)
