1b2ceff355f7ebb977be99df6e617bfda7192623
SigureMo <sigure.qaq@gmail.com>
2024-08-31 11:03:34 +0000

    [SOT] Restore short circuit mechanism in guard

--- python/paddle/jit/sot/opcode_translator/executor/executor_cache.py
+++ python/paddle/jit/sot/opcode_translator/executor/executor_cache.py
@@ -42,7 +42,7 @@ GuardedFunctions = List[GuardedFunction]
 
 dummy_guard: Guard = lambda frame: True
 dummy_guard.expr = "lambda frame: True"
-dummy_guard.lambda_expr = "lambda frame: True"
+dummy_guard.inlined_expr = "lambda frame: True"
 
 
 class OpcodeExecutorCache(metaclass=Singleton):
@@ -131,6 +131,11 @@ class OpcodeExecutorCache(metaclass=Singleton):
                     )
             except Exception as e:
                 log(2, f"[Cache]: Guard function error: {e}\n")
+                log(
+                    2,
+                    f"[Cache]: Guard string is: {getattr(guard_fn, 'expr', 'None')}\n",
+                )
+
                 continue
 
         log(2, "[Cache]: all guards missed\n")
@@ -171,7 +176,7 @@ class OpcodeExecutorCache(metaclass=Singleton):
 
     def analyse_guard_error(self, guard_fn, frame):
         def inner():
-            guard_expr = guard_fn.lambda_expr
+            guard_expr = guard_fn.inlined_expr
             lambda_head = "lambda frame: "
             guard_expr = guard_expr.replace(lambda_head, "")
             guards = guard_expr.split(" and ")
--- python/paddle/jit/sot/utils/__init__.py
+++ python/paddle/jit/sot/utils/__init__.py
@@ -54,7 +54,7 @@ from .utils import (  # noqa: F401
     StepInfoManager,
     StepState,
     count_if,
-    current_tmp_name_records,
+    current_symbol_registry,
     execute_time,
     flatten,
     flatten_extend,
@@ -78,5 +78,5 @@ from .utils import (  # noqa: F401
     meta_str,
     no_eval_frame,
     printable,
-    tmp_name_guard,
+    switch_symbol_registry,
 )
