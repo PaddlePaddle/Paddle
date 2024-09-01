25c3bbf225ab4b5e50e7a9a9c25da25502accfab
PuQing <me@puqing.work>
2024-09-01 22:47:55 +0800

    Remove duplicate test cases

--- tools/test_run_by_protobuf_3.py
+++ tools/test_run_by_protobuf_3.py
@@ -56,8 +56,6 @@ PROTOBUF3_NEEDED_TESTING_LIST = [
     'test_dist_inplace_addto_pass',
     'test_communicator_half_async',
     'test_fleet_graph_executor',
-    'test_communicator_half_async',
-    'test_fleet_graph_executor',
     'test_fleet_with_asp_sharding',
 ]
 
