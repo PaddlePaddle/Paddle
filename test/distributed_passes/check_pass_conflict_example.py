474af5577f3b53a213c58f84efcd960cb4444a73
ooooo <3164076421@qq.com>
2024-09-02 14:25:23 +0800

    update

--- test/distributed_passes/check_pass_conflict_example.py
+++ test/distributed_passes/check_pass_conflict_example.py
@@ -12,12 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import unittest
 
-sys.path.append("../deprecated/distributed_passes")
-
-from dist_pass_test_base_deprecated import PassConflictChecker
+from dist_pass_test_base import PassConflictChecker
 from model_zoo import resnet_model
 
 from paddle.distributed.passes import new_pass
