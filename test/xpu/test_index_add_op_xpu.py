5898ac14c6313bd53081301f3afaec7c5988ec22
zhouquan32 <458178300@qq.com>
2024-09-03 15:00:46 +0800

    fix test_index_add_op_xpu.py code style

--- test/xpu/test_index_add_op_xpu.py
+++ test/xpu/test_index_add_op_xpu.py
@@ -1,26 +1,30 @@
-#Copyright(c) 2022 PaddlePaddle Authors.All Rights Reserved.
+# Copyright(c) 2022 PaddlePaddle Authors.All Rights Reserved.
 #
-#Licensed under the Apache License, Version 2.0(the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0(the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#http:  // www.apache.org/licenses/LICENSE-2.0
+# http:  // www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import unittest
 
-    import numpy as np from op_test_xpu import XPUOpTest
+import numpy as np
+from op_test_xpu import XPUOpTest
+import paddle
 
-    import paddle
+paddle.enable_static()
 
-    paddle.enable_static()
 
-        def compute_index_add_ref(axis, x_shape, x_np, add_value_shape, add_value_np, index_size, index_np) : if axis< 0:
+def compute_index_add_ref(
+    axis, x_shape, x_np, add_value_shape, add_value_np, index_size, index_np
+):
+    if axis < 0:
         axis = axis + len(x_shape)
     if axis != 0:
         outer_loop = np.prod(x_shape[:axis]).astype(int)
