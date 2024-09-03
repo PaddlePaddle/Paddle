d8feb7dfe962206ebf268c933e057cd3f20edd88
Hongqing-work <Shine_CHQ@outlook.com>
2024-09-03 06:37:01 +0000

    fix

--- paddle/phi/infermeta/unary.cc
+++ paddle/phi/infermeta/unary.cc
@@ -1334,17 +1334,29 @@ void ExpandInferMeta(const MetaTensor& x,
   const auto& out_shape = [&]() -> std::vector<int64_t> {
     std::vector<int64_t> res = expand_shape;
     int x_rank = x_dims.size();
-    for (int x_idx = x_rank - 1, out_idx = out_rank - 1; x_idx >= 0;
-         --x_idx, --out_idx) {
-      if (x_dims[x_idx] == -1) {  // NOLINT
-        res[out_idx] = -1;
-      } else if (expand_shape[out_idx] == -1) {
-        res[out_idx] = x_dims[x_idx];
-      } else if (expand_shape[out_idx] == -2) {
-        // We use -2 to represent the element in expand_shape is a var.
-        res[out_idx] = -1;
+    const auto& DealWithMinusOne = [&]() {
+      for (int x_idx = x_rank - 1, out_idx = out_rank - 1; x_idx >= 0;
+           x_idx--, out_idx--) {
+        if (res[out_idx] == -1) {
+          res[out_idx] = x_dims[x_idx];
+        }
       }
-    }
+    };
+    const auto& DealWithMinusTwo = [&]() {
+      // We use -2 to represent the element in expand_shape is a var.
+      for (int x_idx = x_rank - 1, out_idx = out_rank - 1; out_idx >= 0;
+           x_idx--, out_idx--) {
+        if (res[out_idx] == -2) {
+          if (x_idx >= 0 && x_dims[x_idx] > 1) {
+            res[out_idx] = x_dims[x_idx];
+          } else {
+            res[out_idx] = -1;
+          }
+        }
+      }
+    };
+    DealWithMinusOne();
+    DealWithMinusTwo();
     return res;
   }();
 
