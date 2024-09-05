912ce758bb9e16172d48b5639a9e98097b057d2f
yuanlehome <yuanlehome@163.com>
2024-09-05 10:44:54 +0000

    update

--- setup.py
+++ setup.py
@@ -442,7 +442,10 @@ def get_cinn_version() -> str:
 
 def get_cuda_archs() -> list[int]:
     compiled_cuda_archs = env_dict.get("COMPILED_CUDA_ARCHS")
-    return [int(arch) for arch in compiled_cuda_archs.split()]
+    if isinstance(compiled_cuda_archs, str):
+        return [int(arch) for arch in compiled_cuda_archs.split()]
+    else:
+        return []
 
 
 def get_tensorrt_version() -> str:
