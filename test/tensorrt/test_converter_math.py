e7b08ec131f3702d4a4df41b4ba6bdfa676dca3c
layssy <lay1211252464@163.com>
2024-09-07 14:25:55 +0800

    add max sub mul converter单测文件

--- test/tensorrt/test_converter_math.py
+++ test/tensorrt/test_converter_math.py
@@ -37,5 +37,50 @@ class TestMaxTRTPattern(TensorRTBaseTest):
         self.check_trt_result()
 
 
+class TestDivideTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = paddle.divide
+        self.api_args = {
+            "x": np.random.randn(2, 3).astype(np.float32),
+            "y": np.random.randn(2, 3).astype(np.float32),
+        }
+        self.program_config = {"feed_list": ["x", "y"]}
+        self.min_shape = {"x": [1, 3], "y": [1, 3]}
+        self.max_shape = {"x": [5, 3], "y": [5, 3]}
+
+    def test_trt_result(self):
+        self.check_trt_result()
+
+
+class TestMultiplyTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = paddle.multiply
+        self.api_args = {
+            "x": np.random.randn(2, 3).astype(np.float32),
+            "y": np.random.randn(2, 3).astype(np.float32),
+        }
+        self.program_config = {"feed_list": ["x", "y"]}
+        self.min_shape = {"x": [1, 3], "y": [1, 3]}
+        self.max_shape = {"x": [5, 3], "y": [5, 3]}
+
+    def test_trt_result(self):
+        self.check_trt_result()
+
+
+class TestSubstractTRTPattern(TensorRTBaseTest):
+    def setUp(self):
+        self.python_api = paddle.subtract
+        self.api_args = {
+            "x": np.random.randn(2, 3).astype(np.float32),
+            "y": np.random.randn(2, 3).astype(np.float32),
+        }
+        self.program_config = {"feed_list": ["x", "y"]}
+        self.min_shape = {"x": [1, 3], "y": [1, 3]}
+        self.max_shape = {"x": [5, 3], "y": [5, 3]}
+
+    def test_trt_result(self):
+        self.check_trt_result()
+
+
 if __name__ == '__main__':
     unittest.main()
