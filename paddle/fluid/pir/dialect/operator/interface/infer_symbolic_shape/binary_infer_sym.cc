b8fb50fbf2c7771d394fde143f9e6e5c84112e28
Caogration <120171783+Caogration@users.noreply.github.com>
2024-09-12 19:09:30 +0800

    Update binary_infer_sym.cc with size_t instead of int

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/binary_infer_sym.cc
@@ -1597,7 +1597,7 @@ bool UnpoolOpInferSymbolicShape(pir::Operation *op,
   if (attributes.find("output_size") != attributes.end()) {
     std::vector<int64_t> output_size_int = 
         details::GetVectorAttr<int64_t>(op, "output_size");
-    for(int i = 0; i < output_size_int.size(); i++){
+    for(size_t i = 0; i < output_size_int.size(); i++){
       output_size.emplace_back(symbol::DimExpr(output_size_int[i]));
     }
   } else if (op->operand_source(2)) {
