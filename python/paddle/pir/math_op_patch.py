724ff89023cc00433a02a0077b80a6efecf7f0c5
gouzi <530971494@qq.com>
2024-09-12 14:10:12 +0800

    fix review

--- python/paddle/pir/math_op_patch.py
+++ python/paddle/pir/math_op_patch.py
@@ -14,6 +14,7 @@
 
 
 import inspect
+import textwrap
 import warnings
 
 import numpy as np
@@ -490,29 +491,33 @@ def monkey_patch_value():
         )
 
     def _bool_(self):
-        raise TypeError(
-            "bool(Tensor) is not supported in static graph mode. Because the value is not available during the grouping phase. \n"
-            "1. if you don't call bool(Tensor) explicitly, then it's usually triggered implicitly because of the control flow, for example:\n"
-            "   >>> if x > 0:\n"
-            "          ^ Tensor, x > 0 is also a Tensor, if x > 0 triggers bool(Tensor)\n"
-            "   ...     y = y + 1\n"
-            "2. If you are checking for Tensor values, then consider checking only at dynamic graphs, for example:\n"
-            "   Modify the following code\n"
-            "   >>> if x > 0:\n"
-            '   ...     raise ValueError("x should be positive")\n'
-            "   to\n"
-            "   >>> if paddle.in_dynamic_mode() and x < 0:\n"
-            '   >>>     raise ValueError("x should be positive")\n'
-            "3. If you need to control the flow of execution based on the value of the Tensor, then you need to rewrite the code as a control flow, for example:\n"
-            "   Modify the following code\n"
-            "   >>> if x < y:\n"
-            "   ...     y = y + 1\n"
-            "   ... else:\n"
-            "   ...     y = y - 1\n"
-            "   to\n"
-            "   >>> pred = paddle.less_than(x=x, y=y, name=None)\n"
-            "   >>> y = paddle.static.nn.cond(pred, lambda: y + 1, lambda: y - 1)\n"
-        )
+        error_msg = """\
+            bool(Tensor) is not supported in static graph mode. Because the value is not available during the static mode.
+            If you haven't call bool(Tensor) explicitly, it's usually triggered by implicitly the control flow, for example:
+                >>> if x > 0:
+                        ^ `x` is Tensor, `x` > 0 is also a Tensor, `if x > 0` triggers bool(Tensor)
+                ...     y = y + 1
+
+            There are two common scenarios and fixes available:
+            If you are checking for Tensor values, then consider checking only at dynamic graphs, for example:
+                Modify the following code
+                >>> if x > 0:
+                ...     raise ValueError("x should be positive")
+                to
+                >>> if paddle.in_dynamic_mode() and x < 0:
+                >>>     raise ValueError("x should be positive")
+            If you need to control the flow of execution based on the value of the Tensor, then you need to rewrite the code as a control flow, for example:
+                Modify the following code
+                >>> if x < y:
+                ...     y = y + 1
+                ... else:
+                ...     y = y - 1
+                to
+                >>> pred = paddle.less_than(x=x, y=y, name=None)
+                >>> y = paddle.static.nn.cond(pred, lambda: y + 1, lambda: y - 1)
+                more info please refer to https://www.paddlepaddle.org.cn/documentation/docs/zh/api/paddle/static/nn/cond_cn.html
+            """
+        raise TypeError(textwrap.dedent(error_msg))
 
     def clone(self):
         """
