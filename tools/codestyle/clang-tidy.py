da928e207d40af3fb7055d3bba018c556e2ffa84
walk_alone <2390335608@qq.com>
2024-09-15 17:15:08 +0800

    retry

--- tools/codestyle/clang-tidy.py
+++ tools/codestyle/clang-tidy.py
@@ -427,10 +427,9 @@ def main():
             t.daemon = True
             t.start()
 
-        with open('./tools/codestyle/diff_files.txt', 'r') as f:
-            files = f.read().splitlines()
-
-            for name in files:
+        # Fill the queue with files.
+        for name in files:
+            if file_name_re.search(name):
                 task_queue.put(name)
 
         # Wait for all threads to be done.
