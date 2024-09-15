39aaf15967d25908718a80d87d7945c86a7cbb18
Aoraki-Dream <hellomarvellous16@gmail.com>
2024-09-15 02:42:29 +0000

    add complex support for log1p

--- test/legacy_test/test_sparse_unary_op.py
+++ test/legacy_test/test_sparse_unary_op.py
@@ -194,7 +194,11 @@ class TestSparseUnary(unittest.TestCase):
         self.compare_with_dense(paddle.square, paddle.sparse.square)
 
     def test_sparse_log1p(self):
-        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p)
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'float16')
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'float32')
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'float64')
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'complex64')
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'complex128')
 
     def test_sparse_relu(self):
         self.compare_with_dense(paddle.nn.ReLU(), paddle.sparse.nn.ReLU())
@@ -436,7 +440,11 @@ class TestSparseUnaryStatic(unittest.TestCase):
         self.compare_with_dense(paddle.square, paddle.sparse.square)
 
     def test_sparse_log1p(self):
-        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p)
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'float16')
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'float32')
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'float64')
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'complex64')
+        self.compare_with_dense(paddle.log1p, paddle.sparse.log1p, 'complex128')
 
     def test_sparse_relu(self):
         self.compare_with_dense(paddle.nn.ReLU(), paddle.sparse.nn.ReLU())
