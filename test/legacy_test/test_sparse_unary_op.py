352d8b0a7883cc51323824159185bae90a48508a
Aoraki-Dream <hellomarvellous16@gmail.com>
2024-09-16 12:04:46 +0000

    add complex support for asinh_coo/asinh_csr

--- test/legacy_test/test_sparse_unary_op.py
+++ test/legacy_test/test_sparse_unary_op.py
@@ -186,7 +186,11 @@ class TestSparseUnary(unittest.TestCase):
         self.compare_with_dense(paddle.tanh, paddle.sparse.tanh)
 
     def test_sparse_asinh(self):
-        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh)
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'float16')
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'float32')
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'float64')
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'complex64')
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'complex128')
 
     def test_sparse_atanh(self):
         self.compare_with_dense(paddle.atanh, paddle.sparse.atanh)
@@ -432,7 +436,11 @@ class TestSparseUnaryStatic(unittest.TestCase):
         self.compare_with_dense(paddle.tanh, paddle.sparse.tanh)
 
     def test_sparse_asinh(self):
-        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh)
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'float16')
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'float32')
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'float64')
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'complex64')
+        self.compare_with_dense(paddle.asinh, paddle.sparse.asinh, 'complex128')
 
     def test_sparse_atanh(self):
         self.compare_with_dense(paddle.atanh, paddle.sparse.atanh)
