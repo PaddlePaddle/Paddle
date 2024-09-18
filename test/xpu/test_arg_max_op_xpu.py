9a02c21776a6847c32828874a2168edccb759633
skywalker2012 <108259496+skywalker2012@users.noreply.github.com>
2024-09-18 17:49:27 +0800

    restore previous way

--- test/xpu/test_arg_max_op_xpu.py
+++ test/xpu/test_arg_max_op_xpu.py
@@ -27,12 +27,9 @@ import paddle
 paddle.enable_static()
 
 
-# Combine arg_max and arg_min tests
-
-
-class XPUTestArgMinMax(XPUOpTestWrapper):
-    def __init__(self, op_name):
-        self.op_name = op_name
+class XPUTestArgMax(XPUOpTestWrapper):
+    def __init__(self):
+        self.op_name = 'arg_max'
 
     class XPUBaseTestCase(XPUOpTest):
         def initTestCase(self):
@@ -40,89 +37,77 @@ class XPUTestArgMinMax(XPUOpTestWrapper):
             self.axis = 1
 
         def setUp(self):
-            self.op_type = self.__class__.op_name
+            self.op_type = 'arg_max'
             self.dtype = self.in_type
             self.initTestCase()
 
             self.x = (np.random.random(self.dims)).astype(self.dtype)
             self.inputs = {'X': self.x}
             self.attrs = {'axis': self.axis, 'use_xpu': True}
-            if self.op_type == 'arg_max':
-                self.outputs = {'Out': np.argmax(self.x, axis=self.axis)}
-            else:
-                self.outputs = {'Out': np.argmin(self.x, axis=self.axis)}
+            self.outputs = {'Out': np.argmax(self.x, axis=self.axis)}
 
         def test_check_output(self):
             if paddle.is_compiled_with_xpu():
                 place = paddle.XPUPlace(0)
                 self.check_output_with_place(place)
 
-    class TestCase1(XPUBaseTestCase):
+    class TestArgMaxCase1(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (3, 4, 5)
             self.axis = -1
 
-    class TestCase2(XPUBaseTestCase):
+    class TestArgMaxCase2(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (3, 4, 5)
             self.axis = 0
 
-    class TestCase3(XPUBaseTestCase):
+    class TestArgMaxCase3(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (3, 4, 5)
             self.axis = 1
 
-    class TestCase4(XPUBaseTestCase):
+    class TestArgMaxCase4(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (3, 4, 5)
             self.axis = 2
 
-    class TestCase5(XPUBaseTestCase):
+    class TestArgMaxCase5(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (3, 4)
             self.axis = -1
 
-    class TestCase6(XPUBaseTestCase):
+    class TestArgMaxCase6(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (3, 4)
             self.axis = 0
 
-    class TestCase7(XPUBaseTestCase):
+    class TestArgMaxCase7(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (3, 4)
             self.axis = 1
 
-    class TestCase8(XPUBaseTestCase):
+    class TestArgMaxCase8(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (1,)
             self.axis = 0
 
-    class TestCase9(XPUBaseTestCase):
+    class TestArgMaxCase9(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (2,)
             self.axis = 0
 
-    class TestCase10(XPUBaseTestCase):
+    class TestArgMaxCase10(XPUBaseTestCase):
         def initTestCase(self):
             self.dims = (3,)
             self.axis = 0
 
 
-# Create arg_max and arg_min tests
-
 support_types = get_xpu_op_support_types('arg_max')
 for stype in support_types:
-    test_class = XPUTestArgMinMax('arg_max')
-    create_test_class(globals(), test_class, stype)
-
-support_types = get_xpu_op_support_types('arg_min')
-for stype in support_types:
-    test_class = XPUTestArgMinMax('arg_min')
-    create_test_class(globals(), test_class, stype)
+    create_test_class(globals(), XPUTestArgMax, stype)
 
 
-# API Tests for arg_max and arg_min
-class TestArgMinMaxAPI(unittest.TestCase):
+class TestArgMaxAPI(unittest.TestCase):
     def initTestCase(self):
         self.dims = (3, 4, 5)
         self.dtype = 'float32'
@@ -134,29 +119,23 @@ class TestArgMinMaxAPI(unittest.TestCase):
         self.place = [paddle.XPUPlace(0)]
 
     def test_dygraph_api(self):
-        def run(place, op_name):
+        def run(place):
             paddle.disable_static(place)
             np.random.seed(2021)
             numpy_input = (np.random.random(self.dims)).astype(self.dtype)
             tensor_input = paddle.to_tensor(numpy_input)
-            if op_name == 'arg_max':
-                numpy_output = np.argmax(numpy_input, axis=self.axis)
-                paddle_output = paddle.argmax(tensor_input, axis=self.axis)
-            else:
-                numpy_output = np.argmin(numpy_input, axis=self.axis)
-                paddle_output = paddle.argmin(tensor_input, axis=self.axis)
-
+            numpy_output = np.argmax(numpy_input, axis=self.axis)
+            paddle_output = paddle.argmax(tensor_input, axis=self.axis)
             np.testing.assert_allclose(
                 numpy_output, paddle_output.numpy(), rtol=1e-05
             )
             paddle.enable_static()
 
         for place in self.place:
-            run(place, 'arg_max')
-            run(place, 'arg_min')
+            run(place)
 
 
-class TestArgMinMaxAPI_2(unittest.TestCase):
+class TestArgMaxAPI_2(unittest.TestCase):
     def initTestCase(self):
         self.dims = (3, 4, 5)
         self.dtype = 'float32'
@@ -169,26 +148,17 @@ class TestArgMinMaxAPI_2(unittest.TestCase):
         self.place = [paddle.XPUPlace(0)]
 
     def test_dygraph_api(self):
-        def run(place, op_name):
+        def run(place):
             paddle.disable_static(place)
             np.random.seed(2021)
             numpy_input = (np.random.random(self.dims)).astype(self.dtype)
             tensor_input = paddle.to_tensor(numpy_input)
-            if op_name == 'arg_max':
-                numpy_output = np.argmax(numpy_input, axis=self.axis).reshape(
-                    1, 4, 5
-                )
-                paddle_output = paddle.argmax(
-                    tensor_input, axis=self.axis, keepdim=self.keep_dims
-                )
-            else:
-                numpy_output = np.argmin(numpy_input, axis=self.axis).reshape(
-                    1, 4, 5
-                )
-                paddle_output = paddle.argmin(
-                    tensor_input, axis=self.axis, keepdim=self.keep_dims
-                )
-
+            numpy_output = np.argmax(numpy_input, axis=self.axis).reshape(
+                1, 4, 5
+            )
+            paddle_output = paddle.argmax(
+                tensor_input, axis=self.axis, keepdim=self.keep_dims
+            )
             np.testing.assert_allclose(
                 numpy_output, paddle_output.numpy(), rtol=1e-05
             )
@@ -196,8 +166,7 @@ class TestArgMinMaxAPI_2(unittest.TestCase):
             paddle.enable_static()
 
         for place in self.place:
-            run(place, 'arg_max')
-            run(place, 'arg_min')
+            run(place)
 
 
 if __name__ == '__main__':
