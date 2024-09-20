a77c5434bc69d7c9da8ff7cc4cfc80f718a5cd65
Aoraki-Dream <131992218+Aoraki-Dream@users.noreply.github.com>
2024-09-20 14:20:25 +0800

    【complex op No.36-38,41,42,49,51】asin_coo/asin_csr(sparse),asinh_coo/asinh_csr(sparse),atan_coo/atan_csr(sparse),expm1_coo/expm1_csr(sparse),log1p_coo/log1p_csr(sparse),square_coo/square_csr(sparse),tanh_coo/tanh_csr(sparse)      (#68326)
    * add complex support for asin_coo/asin_csr
    
    * fix error
    
    * add complex support for asinh_coo/asinh_csr
    
    * add complex support for atan_coo/atan_csr
    
    * add complex support for atanh
    
    * add complex support for expm1
    
    * add complex support for log1p
    
    * add complex support for square_coo/square_csr(sparse)
    
    * add complex support for tanh_coo/tanh_csr

--- python/paddle/sparse/unary.py
+++ python/paddle/sparse/unary.py
@@ -126,7 +126,7 @@ def asin(x: Tensor, name: str | None = None) -> Tensor:
         out = asin(x)
 
     Parameters:
-        x (Tensor): The input Sparse Tensor with data type float32, float64.
+        x (Tensor): The input Sparse Tensor with data type float32, float64, complex64, complex128.
         name (str|None, optional): Name for the operation (optional, default is None).
             For more information, please refer to :ref:`api_guide_Name`.
 
@@ -311,7 +311,7 @@ def atan(x: Tensor, name: str | None = None) -> Tensor:
         out = atan(x)
 
     Parameters:
-        x (Tensor): The input Sparse Tensor with data type float32, float64.
+        x (Tensor): The input Sparse Tensor with data type float32, float64, complex64, complex128.
         name (str|None, optional): Name for the operation (optional, default is None).
             For more information, please refer to :ref:`api_guide_Name`.
 
@@ -381,7 +381,7 @@ def asinh(x: Tensor, name: str | None = None) -> Tensor:
         out = asinh(x)
 
     Parameters:
-        x (Tensor): The input Sparse Tensor with data type float32, float64.
+        x (Tensor): The input Sparse Tensor with data type float32, float64, complex64, complex128.
         name (str|None, optional): Name for the operation (optional, default is None).
             For more information, please refer to :ref:`api_guide_Name`.
 
@@ -451,7 +451,7 @@ def tanh(x: Tensor, name: str | None = None) -> Tensor:
         out = tanh(x)
 
     Parameters:
-        x (Tensor): The input Sparse Tensor with data type float32, float64.
+        x (Tensor): The input Sparse Tensor with data type float32, float64, complex64, complex128.
         name (str|None, optional): Name for the operation (optional, default is None).
             For more information, please refer to :ref:`api_guide_Name`.
 
@@ -486,7 +486,7 @@ def square(x: Tensor, name: str | None = None) -> Tensor:
         out = square(x)
 
     Parameters:
-        x (Tensor): The input Sparse Tensor with data type float32, float64.
+        x (Tensor): The input Sparse Tensor with data type float32, float64, complex64, complex128.
         name (str|None, optional): Name for the operation (optional, default is None).
             For more information, please refer to :ref:`api_guide_Name`.
 
@@ -556,7 +556,7 @@ def log1p(x: Tensor, name: str | None = None) -> Tensor:
         out = ln(1+x)
 
     Parameters:
-        x (Tensor): The input Sparse Tensor with data type float32, float64.
+        x (Tensor): The input Sparse Tensor with data type float32, float64, complex64, complex128.
         name (str|None, optional): Name for the operation (optional, default is None).
             For more information, please refer to :ref:`api_guide_Name`.
 
@@ -853,7 +853,7 @@ def expm1(x: Tensor, name: str | None = None) -> Tensor:
         out = exp(x) - 1
 
     Parameters:
-        x (Tensor): The input Sparse Tensor with data type float32, float64.
+        x (Tensor): The input Sparse Tensor with data type float32, float64, complex64, complex128.
         name (str|None, optional): Name for the operation (optional, default is None).
             For more information, please refer to :ref:`api_guide_Name`.
 
