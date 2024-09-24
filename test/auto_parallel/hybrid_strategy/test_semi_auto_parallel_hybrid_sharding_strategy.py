58519f88658107e99775b8c3b249a1440e5a5286
liym27 <33742067+liym27@users.noreply.github.com>
2024-09-24 20:13:27 +0800

    fix

--- test/auto_parallel/hybrid_strategy/test_semi_auto_parallel_hybrid_sharding_strategy.py
+++ test/auto_parallel/hybrid_strategy/test_semi_auto_parallel_hybrid_sharding_strategy.py
@@ -11,10 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import os
 import unittest
 
 import collective.test_communication_api_base as test_base
+
+os.environ['FLAGS_enable_pir_api'] = '0'
 
 
 class TestSemiAutoParallelInShardingStrategy(
