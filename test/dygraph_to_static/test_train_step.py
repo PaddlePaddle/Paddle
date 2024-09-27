7b95fe21db224650d77321dd2b0998cde99fcfc2
megemini <megemini@outlook.com>
2024-09-27 22:12:55 +0800

    [Debug] test_train_step

--- test/dygraph_to_static/test_train_step.py
+++ test/dygraph_to_static/test_train_step.py
@@ -146,16 +146,17 @@ class TestTrainStepTinyModelAdamax(TestTrainStepTinyModel):
         self.rtol = 1e-4
 
 
-class TestTrainStepTinyModelAdamW(TestTrainStepTinyModel):
-    def setUp(self):
-        self.input = paddle.randn([10000, 10])
-        self.net_creator = TinyModel
-        self.lr_creator = lambda: 0.001
-        self.optimizer_creator = paddle.optimizer.AdamW
-        self.loss_fn = loss_fn_tiny_model
-        self.train_step_func = train_step_tiny_model
-        self.steps = 3
-        self.rtol = 1e-4
+# TODO(megemini)
+# class TestTrainStepTinyModelAdamW(TestTrainStepTinyModel):
+#     def setUp(self):
+#         self.input = paddle.randn([10000, 10])
+#         self.net_creator = TinyModel
+#         self.lr_creator = lambda: 0.001
+#         self.optimizer_creator = paddle.optimizer.AdamW
+#         self.loss_fn = loss_fn_tiny_model
+#         self.train_step_func = train_step_tiny_model
+#         self.steps = 3
+#         self.rtol = 1e-4
 
 
 class TestTrainStepTinyModelLamb(TestTrainStepTinyModel):
