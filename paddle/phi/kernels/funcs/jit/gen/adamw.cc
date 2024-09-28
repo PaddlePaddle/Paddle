be35004b9bab5e0c681466f64a9dafc1bd2dcd89
megemini <megemini@outlook.com>
2024-09-29 07:47:40 +0800

    [Debug] assembly vmovups ptr

--- paddle/phi/kernels/funcs/jit/gen/adamw.cc
+++ paddle/phi/kernels/funcs/jit/gen/adamw.cc
@@ -124,7 +124,9 @@ void AdamWJitCode::mainCode() {
   cmp(reg_amsgrad, 0);
   je(".without_amsgrad", T_NEAR);
   // load mom2_max
-  vmovups(ymm13 | k1, ptr[reg_mom2_max_ptr + reg_offset]);
+  // vmovups(ymm13 | k1, ptr[reg_mom2_max_ptr + reg_offset]);
+  vmovups(ymm13 | k1, ptr[reg_mom2_ptr + reg_offset]);
+
   // // compare mom2 and mom2_max and save to mom2
   // vmaxps(ymm10 | k1, ymm10, ymm13);
   // // store mom2_max
