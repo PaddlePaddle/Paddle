ab242367043fe6c705a519ef57d5190effe1406a
layssy <lay1211252464@163.com>
2024-09-29 10:46:28 +0800

    add convd_transpose converter

--- python/paddle/tensorrt/impls/conv.py
+++ python/paddle/tensorrt/impls/conv.py
@@ -15,6 +15,7 @@
 
 import tensorrt as trt
 
+from paddle.tensorrt.converter_utils import has_dynamic_shape
 from paddle.tensorrt.register import converter_registry
 
 
@@ -39,4 +40,59 @@ def conv2d_converter(network, paddle_op, inputs):
     conv_layer.dilation_nd = dilation
     conv_layer.num_groups = groups
 
+    return conv_layer.get_output(0)
+
+
+@converter_registry.register("pd_op.conv2d_transpose", trt_version="8.x")
+def conv2d_transpose_converter(network, paddle_op, inputs):
+    """
+    问题：现在convWD_TRANSPOSE 返回的答案是0 需要排查
+    """
+    input_tensor, weight, output_size = inputs
+    input_tensor_shape = paddle_op.operands()[0].source().shape
+    weight_shape = paddle_op.operands()[1].source().shape
+    if has_dynamic_shape(input_tensor.shape):
+        assert (
+            input_tensor.shape[1] != -1
+        ), "Channel dim can't be dynamic for transpose convolution."
+
+    padding = paddle_op.attrs().get("paddings", [0, 0])
+    stride = paddle_op.attrs().get("strides", [1, 1])
+    dilation = paddle_op.attrs().get("dilations", [1, 1])
+    groups = paddle_op.attrs().get("groups", 1)
+
+    # weight_tensor = network.add_constant(weight_shape, weight).get_output(0)
+    kernel_shape = trt.Dims((weight_shape[2], weight_shape[3]))
+
+    conv_layer = network.add_convolution_nd(
+        input_tensor, weight_shape[0], kernel_shape, weight
+    )
+    conv_layer.stride_nd = stride
+    conv_layer.padding_nd = padding
+    conv_layer.dilation_nd = dilation
+    conv_layer.num_groups = groups
+    kernel_shape = trt.Dims((weight_shape[2], weight_shape[3]))
+    if network.has_explicit_precision:
+        dummy_weight = trt.Weights()
+        layer = network.add_deconvolution_nd(
+            input=input_tensor,
+            num_output_maps=weight_shape[1] * groups,
+            kernel_shape=weight_shape[2:],
+            kernel=dummy_weight,
+            # bias=bias,
+        )
+        layer.set_input(1, weight)
+    else:
+        layer = network.add_deconvolution_nd(
+            input=input_tensor,
+            num_output_maps=weight_shape[1] * groups,
+            kernel_shape=kernel_shape,
+            kernel=weight,
+            # bias=bias,
+        )
+    layer.stride_nd = stride
+    layer.padding_nd = padding
+    layer.dilation_nd = dilation
+    if groups is not None:
+        layer.num_groups = groups
     return conv_layer.get_output(0)
