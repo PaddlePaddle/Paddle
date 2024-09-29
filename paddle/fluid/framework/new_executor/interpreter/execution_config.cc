44d942628cd5c2f810714f429373eacf056656f8
chenruibiao <chenruibiao@baidu.com>
2024-09-29 21:36:30 +0800

    Update code

--- paddle/fluid/framework/new_executor/interpreter/execution_config.cc
+++ paddle/fluid/framework/new_executor/interpreter/execution_config.cc
@@ -24,15 +24,14 @@
 #include "paddle/phi/backends/xpu/xpu_info.h"
 #include "paddle/utils/string/string_helper.h"
 
-// FLAGS_force_sync_ops is used to finer control the op-sunc in executor.
+// FLAGS_force_sync_ops is used to finer control the op-sync in executor.
 // The format is: "micro_batch_id, job_name, op_id, op_name | micro_batch_id,
 // job_name, op_id, op_name | ...". Keep spaces to syncs all name/id. Example:
 // 1. sync the recv_v2 op in the second backward-job of 1F1B scheduling:
 // FLAGS_force_sync_ops="1, backward, , recv_v2"
 // 2. sync the full op with op_id=5: FLAGS_force_sync_ops=" , , 5, full"
-// 3. sync all op in the first default-job: FLAGS_force_sync_ops="0, default, ,
-// "
-// 4. sync all op in the forward-job and backward-job: FLAGS_force_sync_ops=" ,
+// 3. sync all ops in the first default-job: FLAGS_force_sync_ops="0,default,,
+// 4. sync all ops in the forward-job and backward-job: FLAGS_force_sync_ops=" ,
 // forward, , | , backward, , , "
 PHI_DEFINE_EXPORTED_string(force_sync_ops,
                            "",
--- paddle/fluid/framework/new_executor/interpreter/execution_config.h
+++ paddle/fluid/framework/new_executor/interpreter/execution_config.h
@@ -26,9 +26,6 @@ namespace interpreter {
 
 struct ExecutionConfig {
   bool create_local_scope{true};
-
-  bool sync_after_op_launch{false};  // unused in run, just for debug
-
   bool used_for_cinn{false};
   bool used_for_control_flow_op{false};
   bool used_for_jit{false};
