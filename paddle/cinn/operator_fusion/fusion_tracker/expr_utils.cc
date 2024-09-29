08a4782b41f4acf1d5d09fb10ce51623cf7bc7ed
你猜 <117290962+GoldenStain@users.noreply.github.com>
2024-09-30 03:02:55 +0800

    fix syntax error in expr_utils.cc

--- paddle/cinn/operator_fusion/fusion_tracker/expr_utils.cc
+++ paddle/cinn/operator_fusion/fusion_tracker/expr_utils.cc
@@ -130,7 +130,7 @@ std::vector<ir::Expr> TopoSort(const std::vector<ir::Expr>& op_exprs) {
   }
   PADDLE_ENFORCE_EQ(result.size(),
                     op_exprs.size(),
-                    common::errors::PreconditionNotMet(
+                    ::common::errors::PreconditionNotMet(
                         "[Error info] the size of result should be equal to "
                         "the size of op_exprs."));
   std::vector<ir::Expr> sorted_result;
