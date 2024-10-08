c0a8fa2488acadd0dc370665e3b4ef2f1530b4c5
sneaxiy <sneaxiy@126.com>
2024-10-08 14:59:14 +0800

    remove FLAGS_use_paddle_recall_error caused by SL's complaints

--- python/paddle/framework/recall_error.py
+++ python/paddle/framework/recall_error.py
@@ -12,31 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 import paddle
 
 
-def use_paddle_recall_error():
-    val = os.getenv("FLAGS_use_paddle_recall_error", "1").strip().lower()
-    if val in ('y', 'yes', 't', 'true', 'on', '1'):
-        return True
-    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
-        return False
-    else:
-        raise ValueError(f"invalid truth value {val}")
-
-
-if use_paddle_recall_error():
-    AADIFF_ERROR = "PaddleRecall error(101): AAdiff"
-    LOSS_NAN_ERROR = "PaddleRecall error(102): LossNan"
-    SHARDING_PAD_NON_ZERO_ERROR = "PaddleRecall error(103): ShardingPadNonZero"
-    LOSS_INF_ERROR = "PaddleRecall error(104): LossInf"
-else:
-    AADIFF_ERROR = "CUDA error(1001)"
-    LOSS_NAN_ERROR = "CUDA error(1002)"
-    SHARDING_PAD_NON_ZERO_ERROR = "CUDA error(1003)"
-    LOSS_INF_ERROR = "CUDA error(1004)"
+AADIFF_ERROR = "PaddleRecall error(101): AAdiff"
+LOSS_NAN_ERROR = "PaddleRecall error(102): LossNan"
+SHARDING_PAD_NON_ZERO_ERROR = "PaddleRecall error(103): ShardingPadNonZero"
+LOSS_INF_ERROR = "PaddleRecall error(104): LossInf"
 
 
 def check_naninf(tensor):
