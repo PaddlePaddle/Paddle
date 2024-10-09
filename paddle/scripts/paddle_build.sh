0947f34f29cf1303432a5eb37a5ad8d458f657f9
walk_alone <2390335608@qq.com>
2024-10-09 20:40:37 +0800

    minor changes

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -3934,8 +3934,8 @@ function clang-tidy_check() {
     -extra-arg=-Wno-overloaded-virtual  \
     -extra-arg=-Wno-defaulted-function-deleted  \
     -extra-arg=-Wno-delete-non-abstract-non-virtual-dtor  \
-    # -extra-arg=-Wno-return-type-c-linkage 2>&1)
     -extra-arg=-Wno-return-type-c-linkage
+    # -extra-arg=-Wno-return-type-c-linkage 2>&1)
 
     endTime_s=`date +%s`
     [ -n "$startTime_firstBuild" ] && startTime_s=$startTime_firstBuild
