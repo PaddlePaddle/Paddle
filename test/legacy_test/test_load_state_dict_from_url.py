0f2f561eac0c075b6f0f698fe200f21db46d5d40
zty-king <17786324919@163.com>
2024-10-09 20:40:09 +0800

    上传开发的url和单测文件

--- test/legacy_test/test_load_state_dict_from_url.py
+++ test/legacy_test/test_load_state_dict_from_url.py
@@ -23,59 +23,15 @@ class TestLoadStateDictFromUrl(unittest.TestCase):
         self.weight_path = '/paddle/test_zty/test/resnet18.pdparams'
 
     def test_load_state_dict_from_url(self):
-        # Verify the consistency of the local files loaded by paddle.load with the local files loaded load_state_dict_from_url
-
-        model1 = self.model
-        weight1 = paddle.load(self.weight_path)
+        #Compare using load_state_dict_from_url to download and load an uncompressed weight file and a compressed weight file(ZIP)
+        weight1=paddle.hapi.hub.load_state_dict_from_url(url='https://paddle-hapi.bj.bcebos.com/models/resnet18.pdparams', model_dir="./test/test1")
+        model1 = resnet18(pretrained=False)
         model1.set_state_dict(weight1)
-        model2 = self.model
-        weight2 = paddle.hapi.hub.load_state_dict_from_url(
-            url='https://paddle-hapi.bj.bcebos.com/models/resnet18.pdparams',
-            model_dir="/paddle/test_zty/test",
-        )
+        weight2=paddle.hapi.hub.load_state_dict_from_url(url='https://x2paddle.bj.bcebos.com/resnet18.zip', model_dir="./test/test2")
+        model2 = resnet18(pretrained=False)
         model2.set_state_dict(weight2)
         are_parameters_equal = True
-        for (name1, param1), (name2, param2) in zip(
-            model1.named_parameters(), model2.named_parameters()
-        ):
-            if name1 != name2 or not paddle.allclose(param1, param2):
-                are_parameters_equal = False
-                break
-        assert are_parameters_equal
-        # Verify the consistency of the local file loaded by paddle.load with the model weight file that load_state_dict_from_url downloaded and loaded
-
-        model1 = self.model
-        weight1 = paddle.load(self.weight_path)
-        model1.set_state_dict(weight1)
-        model2 = self.model
-        weight2 = paddle.hapi.hub.load_state_dict_from_url(
-            url='https://paddle-hapi.bj.bcebos.com/models/resnet18.pdparams',
-            model_dir="/paddle/test_zty/test1",
-        )
-        model2.set_state_dict(weight2)
-        are_parameters_equal = True
-        for (name1, param1), (name2, param2) in zip(
-            model1.named_parameters(), model2.named_parameters()
-        ):
-            if name1 != name2 or not paddle.allclose(param1, param2):
-                are_parameters_equal = False
-                break
-        assert are_parameters_equal
-
-        #  Verify the consistency of the local file loaded by paddle.load with the model weight file that load_state_dict_from_url downloaded, unzipped (ZIP) and loaded
-        model1 = self.model
-        weight1 = paddle.load(self.weight_path)
-        model1.set_state_dict(weight1)
-        model2 = self.model
-        weight2 = paddle.hapi.hub.load_state_dict_from_url(
-            url='http://127.0.0.1:9100/download/resnet18.zip',
-            model_dir="/paddle/test_zty/test2",
-        )
-        model2.set_state_dict(weight2)
-        are_parameters_equal = True
-        for (name1, param1), (name2, param2) in zip(
-            model1.named_parameters(), model2.named_parameters()
-        ):
+        for (name1, param1), (name2, param2) in zip(model1.named_parameters(), model2.named_parameters()):
             if name1 != name2 or not paddle.allclose(param1, param2):
                 are_parameters_equal = False
                 break
