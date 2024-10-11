ca39d9a255d94dc60120359765cae91db149c727
Hanyonggong <1229369094@qq.com>
2024-10-11 07:32:30 +0000

    mod marker

--- paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
+++ paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
@@ -75,7 +75,6 @@ DEFINE_GENERAL_PATTERN(Sigmoid, paddle::dialect::SigmoidOp)
 DEFINE_GENERAL_PATTERN(Sqrt, paddle::dialect::SqrtOp)
 DEFINE_GENERAL_PATTERN(Hardsigmoid, paddle::dialect::HardsigmoidOp)
 DEFINE_GENERAL_PATTERN(Hardswish, paddle::dialect::HardswishOp)
-DEFINE_GENERAL_PATTERN(Tanh, paddle::dialect::TanhOp)
 
 #undef DEFINE_GENERAL_PATTERN
 
@@ -1279,6 +1278,29 @@ class MeanOpPattern : public pir::OpRewritePattern<paddle::dialect::MeanOp> {
   }
 };
 
+class TanhOpPattern : public pir::OpRewritePattern<paddle::dialect::TanhOp> {
+ public:
+  using pir::OpRewritePattern<paddle::dialect::TanhOp>::OpRewritePattern;
+  bool MatchAndRewrite(paddle::dialect::TanhOp op,
+                       pir::PatternRewriter &rewriter) const override {
+    if (op->HasAttribute(kCanRunTrtAttr) &&
+        op.attribute<pir::BoolAttribute>(kCanRunTrtAttr).data()) {
+      return false;
+    }
+    pir::Value x = op.operand_source(0);
+    auto x_type = x.type().dyn_cast<paddle::dialect::DenseTensorType>();
+    auto x_shape = x_type.dims();
+    int dims = x_shape.size();
+    if (dims < 1) {
+      VLOG(3) << "Tanh op does not support 0 dim input when TensorRT < 8.6.";
+      return false;
+    }
+
+    op->set_attribute(kCanRunTrtAttr, rewriter.bool_attr(true));
+    return true;
+  }
+};
+
 class TrtOpMarkerPass : public pir::PatternRewritePass {
  public:
   TrtOpMarkerPass() : pir::PatternRewritePass("trt_op_marker_pass", 2) {}
@@ -1313,7 +1335,6 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ADD_PATTERN(Sqrt)
     ADD_PATTERN(Hardsigmoid)
     ADD_PATTERN(Hardswish)
-    ADD_PATTERN(Tanh)
 #if IS_TRT_VERSION_GE(8600)
     ADD_PATTERN(Layer_norm)
 #endif
@@ -1352,6 +1373,7 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ps.Add(std::make_unique<MulticlassNms3OpPattern>(context));
     ps.Add(std::make_unique<ArgmaxOpPattern>(context));
     ps.Add(std::make_unique<MaxOpPattern>(context));
+    ps.Add(std::make_unique<TanhOpPattern>(context));
     return ps;
   }
 };
