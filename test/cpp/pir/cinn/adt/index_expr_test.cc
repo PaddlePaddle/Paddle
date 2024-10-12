867b678ac3c34064ae57c246acc25fd60cfb138d
liuruyan <1823192871@qq.com>
2024-10-12 05:26:14 +0000

    add headfile in ut

--- test/cpp/pir/cinn/adt/index_expr_test.cc
+++ test/cpp/pir/cinn/adt/index_expr_test.cc
@@ -15,6 +15,9 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 #include "paddle/cinn/ir/op/ir_operators.h"
+#include "paddle/cinn/ir/ir.h"
+#include "paddle/cinn/ir/ir_base.h"
+#include "paddle/cinn/ir/ir_mutator.h"
 
 namespace cinn {
 namespace common {
