0e4efaed38e55dc15f390c1a7b5d920b0512345e
ooo oo <3164076421@qq.com>
2024-10-13 11:27:51 +0800

    refine logic

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/nullary_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/nullary_infer_sym.cc
@@ -251,7 +251,10 @@ bool EyeOpInferSymbolicShape(pir::Operation *op,
   std::vector<symbol::DimExpr> out_shape;
   symbol::DimExpr num_columns_dim;
   symbol::DimExpr num_rows_dim;
-  if (op->operand_source(0)) {
+  if (op->HasAttribute("num_rows")) {
+    int num_rows_int = op->attribute<pir::Int64Attribute>("num_rows").data();
+    num_rows_dim = symbol::DimExpr(num_rows_int);
+  } else if (op->operand_source(0)) {
     const auto &num_rows_shape_or_data =
         infer_context->GetShapeOrDataForValue(op->operand_source(0));
     if (num_rows_shape_or_data.data().has_value()) {
@@ -260,15 +263,20 @@ bool EyeOpInferSymbolicShape(pir::Operation *op,
     } else {
       num_rows_dim = symbol::DimExpr(infer_context->GetNextSymName());
     }
-  } else if (op->HasAttribute("num_rows")) {
-    int num_rows_int = op->attribute<pir::Int64Attribute>("num_rows").data();
-    num_rows_dim = symbol::DimExpr(num_rows_int);
   } else {
     PADDLE_THROW(
         common::errors::InvalidArgument("The num_rows must be set for Eyeop."));
   }
 
-  if (op->operand_source(1)) {
+  if (op->HasAttribute("num_columns")) {
+    int num_columns_int =
+        op->attribute<pir::Int64Attribute>("num_columns").data();
+    if (num_columns_int == -1) {
+      num_columns_dim = num_rows_dim;
+    } else {
+      num_columns_dim = symbol::DimExpr(num_columns_int);
+    }
+  } else if (op->operand_source(1)) {
     const auto &num_columns_shape_or_data =
         infer_context->GetShapeOrDataForValue(op->operand_source(1));
     if (num_columns_shape_or_data.data().has_value()) {
@@ -276,14 +284,6 @@ bool EyeOpInferSymbolicShape(pir::Operation *op,
           symbol::DimExpr(num_columns_shape_or_data.data().value().at(0));
     } else {
       num_columns_dim = symbol::DimExpr(infer_context->GetNextSymName());
-    }
-  } else if (op->HasAttribute("num_columns")) {
-    int num_columns_int =
-        op->attribute<pir::Int64Attribute>("num_columns").data();
-    if (num_columns_int == -1) {
-      num_columns_dim = num_rows_dim;
-    } else {
-      num_columns_dim = symbol::DimExpr(num_columns_int);
     }
   } else {
     PADDLE_THROW(common::errors::InvalidArgument(
