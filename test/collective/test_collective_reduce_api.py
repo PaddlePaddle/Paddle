d7ad9478c4947ca835ebc7ba3abdd37a2332bb43
xuexixi <xuexixi@baidu.com>
2024-10-15 01:11:31 +0800

    Fix out of time

--- test/collective/test_collective_reduce_api.py
+++ test/collective/test_collective_reduce_api.py
@@ -58,28 +58,6 @@ class TestCollectiveReduceAPI(TestDistBase):
                         need_envs={"USE_COMM_CONTEXT": "1"},
                     )
 
-    def test_reduce_nccl_with_new_comm(self):
-        dtypes_to_test = [
-            "float16",
-            "float32",
-            "float64",
-            "int32",
-            "int64",
-        ]
-        red_types_to_test = [
-            dist.ReduceOp.SUM,
-        ]
-        for dtype in dtypes_to_test:
-            if paddle.base.core.is_compiled_with_cuda():
-                for red_type in red_types_to_test:
-                    self.check_with_place(
-                        "collective_reduce_api.py",
-                        "reduce",
-                        "nccl",
-                        dtype=dtype,
-                        reduce_type=red_type,
-                    )
-
     def test_reduce_nccl_with_new_comm_pir(self):
         dtypes_to_test = [
             "float16",
