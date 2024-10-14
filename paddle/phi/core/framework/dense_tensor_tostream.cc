86c3d1342ab70d2ae2de7ddb61b2f928920fa756
co63oc <co63oc@users.noreply.github.com>
2024-10-14 16:57:05 +0800

    Fix

--- paddle/phi/core/framework/dense_tensor_tostream.cc
+++ paddle/phi/core/framework/dense_tensor_tostream.cc
@@ -26,6 +26,7 @@
 #include "paddle/phi/common/memory_utils.h"
 #include "paddle/phi/core/compat/convert_utils.h"
 #include "paddle/phi/core/framework/convert_utils.h"
+#include "paddle/phi/core/kernel_factory.h"
 #include "paddle/phi/core/tensor_utils.h"
 #include "paddle/phi/kernels/contiguous_kernel.h"
 
