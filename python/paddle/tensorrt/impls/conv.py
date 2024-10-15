159aa581d5263abdf371713b2df53d6061c96759
YuanRisheng <yuanrisheng@baidu.com>
2024-10-15 14:16:19 +0800

    [Inference]Add dropout and depthwise_conv2d (#68593)
    * add dropout and depthwise_conv2d
    
    * add unittest
    
    * perfect utils
    
    * re trigger
    
    * re trigger
    
    * re trigger

--- python/paddle/tensorrt/impls/conv.py
+++ python/paddle/tensorrt/impls/conv.py
@@ -18,6 +18,7 @@ import tensorrt as trt
 from paddle.tensorrt.register import converter_registry
 
 
+@converter_registry.register("pd_op.depthwise_conv2d", trt_version="8.x")
 @converter_registry.register("pd_op.conv2d", trt_version="8.x")
 def conv2d_converter(network, paddle_op, inputs):
     input_tensor, weight = inputs
