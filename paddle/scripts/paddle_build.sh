a1777e3a2aa021266a1cfb79ddd3e7d285adf216
walk_alone <2390335608@qq.com>
2024-10-17 16:28:13 +0800

    test

--- paddle/scripts/paddle_build.sh
+++ paddle/scripts/paddle_build.sh
@@ -4151,8 +4151,8 @@ function clang-tidy_check() {
     echo "Clang Tidy output length: $[ $length ]"
     for str in "${S[@]}"; do
         count=$(echo -n "$T" | grep -o "$str" | wc -l)
-        echo "str: $[ $str] count: $[ $count ]"
-        if [ "$[ $count ]" -ge 2 ]; then
+        echo "str: $[ $str ] count: $[ $count ]"
+        if [ "$count" -ge 2 ]; then
             echo "check error: $[ $s ]"
             check_error=1
         fi
