ad7b11b9a948810eeda015a84158f11d6f478546
yinfan98 <1106310035@qq.com>
2024-10-17 02:21:06 +0000

    delete some useless

--- python/paddle/_typing/libs/libpaddle/var_names.pyi
+++
@@ -1,9 +0,0 @@
-"""
-The module will return special predefined variable name in Paddle
-"""
-from __future__ import annotations
-__all__ = ['empty', 'temp']
-def empty() -> str:
-    ...
-def temp() -> str:
-    ...
