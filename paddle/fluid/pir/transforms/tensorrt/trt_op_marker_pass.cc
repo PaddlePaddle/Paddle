98d057c22680964ac5ce295d58369370b0bdc2fc
lizexu <2694294196@qq.com>
2024-10-21 03:12:38 +0000

    kg

--- paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
+++ paddle/fluid/pir/transforms/tensorrt/trt_op_marker_pass.cc
@@ -1459,6 +1459,49 @@ class NearestInterV2Pattern
   }
 };
 
+class StackOpPattern : public pir::OpRewritePattern<paddle::dialect::StackOp> {
+ public:
+  using pir::OpRewritePattern<paddle::dialect::StackOp>::OpRewritePattern;
+  bool MatchAndRewrite(paddle::dialect::StackOp op,
+                       pir::PatternRewriter &rewriter) const override {
+    if (op->HasAttribute(kCanRunTrtAttr) &&
+        op->attribute<pir::BoolAttribute>(kCanRunTrtAttr).data()) {
+      return false;
+    }
+
+    pir::Value x = op.operand_source(0);
+    int rank;
+    auto x_type = x.type();
+    if (x_type.isa<pir::VectorType>()) {
+      rank = x_type.dyn_cast<pir::VectorType>().size();
+    }else{
+      auto x_shape = op.operand_source(0)
+                        .type()
+                        .dyn_cast<paddle::dialect::DenseTensorType>()
+                        .dims();
+      rank=x_shape.size();
+    }
+  
+    int axis;
+    if(op->HasAttribute("axis")){
+      axis = op->attribute<pir::Int32Attribute>("axis").data();
+    }else{
+      axis=-1;
+    }
+    if(axis > rank || axis < -(rank+1))
+    {
+      VLOG(3) << "Invalid axis value: " << axis 
+              << ". Axis should be in range [-" << (rank + 1) 
+              << ", " << rank << "], where rank is " << rank << ".";
+      return false;
+    }
+  
+    op->set_attribute(kCanRunTrtAttr, rewriter.bool_attr(true));
+    return true;
+  }
+};
+
+
 class TrtOpMarkerPass : public pir::PatternRewritePass {
  public:
   TrtOpMarkerPass() : pir::PatternRewritePass("trt_op_marker_pass", 2) {}
@@ -1534,6 +1577,7 @@ class TrtOpMarkerPass : public pir::PatternRewritePass {
     ps.Add(std::make_unique<MaxOpPattern>(context));
     ps.Add(std::make_unique<BilinearInterpV2Pattern>(context));
     ps.Add(std::make_unique<NearestInterV2Pattern>(context));
+    ps.Add(std::make_unique<StackOpPattern>(context));
     return ps;
   }
 };
