af39c557aa323fe9cdd9dab2706a7c1b888c48a5
zachary sun <sunzhongkai@baidu.com>
2024-10-22 22:01:34 +0800

    fix

--- python/paddle/nn/functional/flash_attention.py
+++ python/paddle/nn/functional/flash_attention.py
@@ -1116,63 +1116,63 @@ def flashmask_attention(
 
     This module provides the official implementation of the FlashMask algorithm as described in the paper. For more details, please refer to the paper available at: https://arxiv.org/abs/2410.01359.
 
-    Equation:
-        The core equation utilized in FlashMask is as follows:
+    The core equation utilized in FlashMask is as follows:
 
-        .. math::
+    .. math::
 
-            \text{result} = \text{softmax}\left(\frac{Q \cdot K^T}{\sqrt{d}} + M\right) \cdot V
+        \text{result} = \text{softmax}\left(\frac{Q \cdot K^T}{\sqrt{d}} + M\right) \cdot V
 
-        In this equation:
+    In this equation:
+
         - ``Q``, ``K``, and ``V`` are the input tensors to the attention module.
         - All these tensors share the same dimensions.
         - ``d`` denotes the size of the last dimension of these tensors.
         - ``M`` represents the column-wise sparse mask introduced by FlashMask.
 
     Args
-    ::::::::::::
-        - **query** (Tensor) - The query tensor in the attention module.
-                        A 4-D tensor with shape [batch_size, q_seq_len, num_heads, head_dim].
-                        The dtype can be float16 or bfloat16.
-        - **key** (Tensor) - The key tensor in the attention module.
-                        A 4-D tensor with shape [batch_size, k_seq_len, k_num_heads, head_dim].
-                        The dtype can be float16 or bfloat16.
-        - **value** (Tensor) - The value tensor in the attention module.
-                        A 4-D tensor with shape [batch_size, k_seq_len, k_num_heads, head_dim].
-                        The dtype can be float16 or bfloat16.
-        - **startend_row_indices** (Tensor)
-                        - A column-wise sparse attention mask row indices tensor.
-                            A 4-D tensor with shape [batch_size, k_num_heads, k_seq_len, {1, 2, 4}].
-                            The dtype must be int32. k_num_heads can be 1 or the same as key's num_heads. When num_heads is 1, it will be broadcast to match key's num_heads.
-                            Depending on the value of the causal parameter, startend_row_indices can take different shapes and meanings.
-                        - When `causal=True` and the shape is [batch_size, k_num_heads, k_seq_len, 1],
-                            indicating unidirectional attention. The value represents the starting row index of the left
-                            lower triangular mask in the dense mask. The value startend_row_indices[..., 0] indicates that elements in the lower left triangle of the attention score matrix starting from the startend_row_indices[..., 0]-th row downwards (inclusive) will be masked.
-                        - When `causal=True` and the shape is [batch_size, k_num_heads, k_seq_len, 2],
-                            indicating unidirectional attention. The values represent the starting and ending row indices of
-                            the left lower triangular mask in the dense mask. The values startend_row_indices[..., 0:2] in startend_row_indices indicate that elements in the lower left triangle of the attention score matrix starting from the startend_row_indices[..., 0]-th row downwards (inclusive) but above the startend_row_indices[..., 1]-th row (exclusive) will be masked.
-                        - When `causal=False` and the shape is [batch_size, k_num_heads, k_seq_len, 2],
-                            indicating bidirectional attention. The values represent the starting row index of the left
-                            lower triangular mask and the ending row index of the right upper triangular mask in the dense mask. The values startend_row_indices[..., 0:2] in startend_row_indices indicate that elements in the lower left triangle of the attention score matrix starting from the startend_row_indices[..., 0]-th row downwards (inclusive) will be masked, and elements in the upper right triangle starting from the startend_row_indices[..., 1]-th row upwards (exclusive) will be masked.
-                        - When `causal=False` and the shape is [batch_size, k_num_heads, k_seq_len, 4] ,
-                            indicating bidirectional attention. The values represent the start and end row indices of the
-                            left lower triangular mask and the start and end row indices of the right upper triangular mask in the dense mask. The values startend_row_indices[..., 0:4] in startend_row_indices indicate that elements in the lower left triangle of the attention score matrix starting from the startend_row_indices[..., 0]-th row downwards (inclusive) but above the startend_row_indices[..., 1] row (exclusive) will be masked, and elements in the upper right triangle starting from the startend_row_indices[..., 2]-th row downwards (inclusive) but above the startend_row_indices[..., 3] row (exclusive) will be masked.
-        - **dropout** (float) - The dropout ratio. Default is 0.0.
-        - **causal** (bool) - Whether to enable causal mode. Default is False.
-        - **window_size** (int|tuple, optional) - Indicates the window size of sliding window local attention.
-                        If causal mode is enabled, Query at position i will only attend to keys between [i - window_size, i] or [i - window_size[0], i].
-                        If causal mode is disabled, Query at position i will only attend to keys between [i - window_size, i + window_size] or [i - window_size[0], i + window_size[1]].
-        - **return_softmax_lse** (bool) - Whether to return the log-sum-exp of the softmax. Default is False.
-        - **return_seed_offset** (bool) - Whether to return the random seed offset. Default is False.
-        - **fixed_seed_offset** (Tensor, optional): With fixed seed, offset for dropout mask.
-        - **rng_name** (str) - The name to select Generator.
-        - **training** (bool) - Whether the module is in training mode. Default is True.
-        - **name** (str, optional) - Name of the operation. Default is None. Normally, users do not need to set this property.
-                        For more information, refer to :ref:`api_guide_Name` .
+        query (Tensor):  The query tensor in the attention module.
+          A 4-D tensor with shape [batch_size, q_seq_len, num_heads, head_dim].
+          The dtype can be float16 or bfloat16.
+        key (Tensor): The key tensor in the attention module.
+          A 4-D tensor with shape [batch_size, k_seq_len, k_num_heads, head_dim].
+          The dtype can be float16 or bfloat16.
+        value (Tensor): The value tensor in the attention module.
+          A 4-D tensor with shape [batch_size, k_seq_len, k_num_heads, head_dim].
+          The dtype can be float16 or bfloat16.
+        startend_row_indices(Tensor):
+          A column-wise sparse attention mask row indices tensor.
+          A 4-D tensor with shape [batch_size, k_num_heads, k_seq_len, {1, 2, 4}].
+          The dtype must be int32. k_num_heads can be 1 or the same as key's num_heads. When num_heads is 1, it will be broadcast to match key's num_heads.
+          Depending on the value of the causal parameter, startend_row_indices can take different shapes and meanings.
+
+            - When `causal=True` and the shape is [batch_size, k_num_heads, k_seq_len, 1],
+              indicating unidirectional attention. The value represents the starting row index of the left
+              lower triangular mask in the dense mask. The value startend_row_indices[..., 0] indicates that elements in the lower left triangle of the attention score matrix starting from the startend_row_indices[..., 0]-th row downwards (inclusive) will be masked.
+            - When `causal=True` and the shape is [batch_size, k_num_heads, k_seq_len, 2],
+              indicating unidirectional attention. The values represent the starting and ending row indices of
+              the left lower triangular mask in the dense mask. The values startend_row_indices[..., 0:2] in startend_row_indices indicate that elements in the lower left triangle of the attention score matrix starting from the startend_row_indices[..., 0]-th row downwards (inclusive) but above the startend_row_indices[..., 1]-th row (exclusive) will be masked.
+            - When `causal=False` and the shape is [batch_size, k_num_heads, k_seq_len, 2],
+              indicating bidirectional attention. The values represent the starting row index of the left
+              lower triangular mask and the ending row index of the right upper triangular mask in the dense mask. The values startend_row_indices[..., 0:2] in startend_row_indices indicate that elements in the lower left triangle of the attention score matrix starting from the startend_row_indices[..., 0]-th row downwards (inclusive) will be masked, and elements in the upper right triangle starting from the startend_row_indices[..., 1]-th row upwards (exclusive) will be masked.
+            - When `causal=False` and the shape is [batch_size, k_num_heads, k_seq_len, 4] ,
+              indicating bidirectional attention. The values represent the start and end row indices of the
+              left lower triangular mask and the start and end row indices of the right upper triangular mask in the dense mask. The values startend_row_indices[..., 0:4] in startend_row_indices indicate that elements in the lower left triangle of the attention score matrix starting from the startend_row_indices[..., 0]-th row downwards (inclusive) but above the startend_row_indices[..., 1] row (exclusive) will be masked, and elements in the upper right triangle starting from the startend_row_indices[..., 2]-th row downwards (inclusive) but above the startend_row_indices[..., 3] row (exclusive) will be masked.
+
+        dropout (float):  - The dropout ratio. Default is 0.0.
+        causal (bool): Whether to enable causal mode. Default is False.
+        window_size (int|tuple, optional): Indicates the window size of sliding window local attention.
+          If causal mode is enabled, Query at position i will only attend to keys between [i - window_size, i] or [i - window_size[0], i].
+          If causal mode is disabled, Query at position i will only attend to keys between [i - window_size, i + window_size] or [i - window_size[0], i + window_size[1]].
+        return_softmax_lse (bool): Whether to return the log-sum-exp of the softmax. Default is False.
+        return_seed_offset (bool): Whether to return the random seed offset. Default is False.
+        fixed_seed_of fset(Tensor, optional): With fixed seed, offset for dropout mask.
+        rng_name (str): The name to select Generator.
+        training (bool): Whether the module is in training mode. Default is True.
+        name (str, optional): Name of the operation. Default is None. Normally, users do not need to set this property.
+          For more information, refer to :ref:`api_guide_Name` .
 
     Returns
-    ::::::::::::
-        Tensor: The computed attention result with the same shape as the input `query`.
+        Tensor. The computed attention result with the same shape as the input `query`.
 
     Warning:
         This API only supports inputs with dtype float16 and bfloat16.
