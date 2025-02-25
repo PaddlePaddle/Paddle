d2534f454326a38f80acc322b945a6cdd8c048d4
tianshuo78520a <707759223@qq.com>
2025-02-25 11:00:46 +0800

    Fix (#71248)

--- paddle/fluid/distributed/collective/async_load.cc
+++ paddle/fluid/distributed/collective/async_load.cc
@@ -156,7 +156,7 @@ std::shared_ptr<AsyncLoad::Task> AsyncLoad::OffloadWithOffset(
     gpu_place_ = place;
     place_to_calc_event_.emplace(
         key, platform::DeviceEvent(place, platform::GenerateDeviceEventFlag()));
-    load_ctx_ = std::move(std::make_unique<phi::GPUContext>(place));
+    load_ctx_ = std::make_unique<phi::GPUContext>(place);
   }
   SyncCalcStream(gpu_place_, load_ctx_.get(), place_to_calc_event_.at(key));
 
