5e01f7586e0cccf93cc9a1c5ec7a93d02403ee05
Gu Shiwei <gushiwei@baidu.com>
2025-03-05 11:04:25 +0800

    fix cuda126 linux (#71363)

--- paddle/phi/kernels/funcs/shuffle_batch.cu.h
+++ paddle/phi/kernels/funcs/shuffle_batch.cu.h
@@ -151,9 +151,9 @@ struct write_output_op_fixed {
   OutputIterT out;
 // flag contains inclusive scan of valid keys
 // perform gather using valid keys
-#if CUDA_VERSION < 12060
-  __thrust_exec_check_disable__ __host__ __device__ std::size_t operator()(
-      key_flag_tuple_fixed x) {
+#if CUDA_VERSION >= 12060 && defined(_WIN32)
+  _CCCL_EXEC_CHECK_DISABLE
+  _CCCL_HOST_DEVICE std::size_t operator()(key_flag_tuple_fixed x) {
     if (x.key < m) {
       // -1 because inclusive scan
       out[x.flag - 1] = in[x.key];
@@ -161,8 +161,8 @@ struct write_output_op_fixed {
     return 0;  // Discarded
   }
 #else
-  _CCCL_EXEC_CHECK_DISABLE
-  _CCCL_HOST_DEVICE std::size_t operator()(key_flag_tuple_fixed x) {
+  __thrust_exec_check_disable__ __host__ __device__ std::size_t operator()(
+      key_flag_tuple_fixed x) {
     if (x.key < m) {
       // -1 because inclusive scan
       out[x.flag - 1] = in[x.key];
