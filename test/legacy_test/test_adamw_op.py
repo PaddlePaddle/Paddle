a5272d9fd6a472adb42e078a2884858cb51b25a5
risemeup1 <62429225+risemeup1@users.noreply.github.com>
2025-03-10 10:29:52 +0800

    support_adamw_moment1_bfloat16 (#71495)
    * support_adamw_moment1_bfloat16
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- test/legacy_test/test_adamw_op.py
+++ test/legacy_test/test_adamw_op.py
@@ -21,7 +21,7 @@ import numpy as np
 from op_test import OpTest
 
 import paddle
-from paddle import base
+from paddle import base, nn
 from paddle.framework import core
 
 
@@ -176,6 +176,7 @@ class TestAdamW(OpTest):
         }
 
     def test_check_output(self):
+
         self.check_output(no_check_set=self.no_check_set, check_pir=True)
 
 
@@ -1720,6 +1721,246 @@ class TestAdamWOpLayerwiseLRAMSGrad(TestAdamWOpLayerwiseLR):
         else:
             self.amsgrad = True
             self.no_check_set = None
+
+
+@unittest.skipIf(
+    core.is_compiled_with_xpu(),
+    "core is not compiled with XPU",
+)
+class TestAdamwMomentBfloat16Amp(unittest.TestCase):
+    def setUp(self):
+        self.amsgrad = False
+        self.num_steps = 10  # 增加训练的 step 数
+
+    def test_adamw_moment_bfloat16_amp(self):
+        paddle.disable_static()
+        value = np.arange(26).reshape(2, 13).astype("float32")
+        a = paddle.to_tensor(value)
+        a.stop_gradient = True
+
+        paddle.seed(111)
+        linear1 = nn.Linear(13, 5)
+        paddle.seed(111)
+        linear2 = nn.Linear(13, 5)
+
+        adam1 = paddle.optimizer.AdamW(
+            learning_rate=0.01,
+            parameters=linear1.parameters(),
+            apply_decay_param_fun=lambda name: True,
+            weight_decay=0.01,
+            amsgrad=self.amsgrad,
+        )
+        adam2 = paddle.optimizer.AdamW(
+            learning_rate=0.01,
+            parameters=linear2.parameters(),
+            apply_decay_param_fun=lambda name: True,
+            weight_decay=0.01,
+            amsgrad=self.amsgrad,
+        )
+
+        for _ in range(self.num_steps):
+            output1 = linear1(a)
+            loss1 = paddle.mean(output1)
+            loss1.backward()
+            adam1.step()
+            adam1.clear_grad()
+
+        model, optimizer = paddle.amp.decorate(
+            models=linear2, optimizers=adam2, level='O2', dtype="bfloat16"
+        )
+
+        for _ in range(self.num_steps):
+            with paddle.amp.auto_cast(
+                dtype="bfloat16",
+                enable=True,
+                custom_white_list=None,
+                custom_black_list=None,
+                level="O2",
+            ):
+                output2 = model(a)
+                loss2 = paddle.mean(output2)
+                loss2.backward()
+                optimizer.step()
+                optimizer.clear_grad()
+
+        np.testing.assert_allclose(
+            loss1.astype(paddle.float32).numpy(),
+            loss2.astype(paddle.float32).numpy(),
+            rtol=1e-2,
+            atol=1e-2,
+        )
+
+        for param1, param2 in zip(linear1.parameters(), linear2.parameters()):
+            np.testing.assert_allclose(
+                param1.astype(paddle.float32).numpy(),
+                param2.astype(paddle.float32).numpy(),
+                rtol=1e-2,
+                atol=1e-2,
+            )
+
+    def _get_places(self):
+        places = []
+        if paddle.is_compiled_with_cuda():
+            places.append('gpu')
+        if paddle.is_compiled_with_xpu():
+            places.append('xpu')
+        return places
+
+    def _test_adamw_op_dygraph_place_amp_with_maingrad_with_moment_bf16(
+        self, place, shape, use_main_grad
+    ):
+        paddle.disable_static()
+        paddle.seed(10)
+        paddle.set_device(place)
+
+        found_inf = None
+
+        _weight_decay = 0.1
+        with_decay = True
+        _lazy_mode = False
+        find_master = True
+
+        _epsilon = 1e-8
+
+        _beta1 = 0.9
+        _beta2 = 0.99
+        lr_ratio_ = 1.0
+
+        lr_rate = 1e-8
+
+        param = paddle.randn(shape).astype(paddle.bfloat16)
+        master_weight = param.astype(paddle.float32)
+        grad = paddle.randn(shape).astype(paddle.bfloat16)
+        main_grad = grad.astype(paddle.float32)
+        moment1 = paddle.randn(shape).astype(paddle.bfloat16)
+        moment2 = paddle.randn(shape).astype(paddle.bfloat16).abs()
+        moment2_max = paddle.zeros(shape).astype(paddle.bfloat16)
+        lr = paddle.zeros([1]).astype(paddle.float32)
+        lr[0] = lr_rate
+        beta1_pow_acc = paddle.ones([1]).astype(paddle.float32)
+        beta1_pow_acc[0] = _beta1**10
+        beta2_pow_acc = paddle.ones([1]).astype(paddle.float32)
+        beta2_pow_acc[0] = _beta2**10
+
+        ref_param = param.astype(paddle.float32).clone().detach()
+        ref_beta1_pow_acc = (
+            beta1_pow_acc.astype(paddle.float32).clone().detach()
+        )
+        ref_beta2_pow_acc = (
+            beta2_pow_acc.astype(paddle.float32).clone().detach()
+        )
+        ref_moment_1 = moment1.astype(paddle.bfloat16).clone().detach()
+        ref_moment_2 = moment2.astype(paddle.bfloat16).clone().detach()
+        ref_moment_2_max = moment2_max.astype(paddle.bfloat16).clone().detach()
+
+        # reference code
+        _, _, _, _, _, _, _ = paddle._C_ops.adamw_(
+            ref_param,
+            main_grad,
+            lr,
+            ref_moment_1,
+            ref_moment_2,
+            ref_moment_2_max,
+            ref_beta1_pow_acc,
+            ref_beta2_pow_acc,
+            master_weight,
+            found_inf,
+            _beta1,
+            _beta2,
+            _epsilon,
+            lr_ratio_,
+            _weight_decay,
+            with_decay,
+            _lazy_mode,
+            1000,
+            False,
+            False,
+            self.amsgrad,
+        )
+
+        if use_main_grad:
+            _, _, _, _, _, _, _ = paddle._C_ops.adamw_(
+                param,
+                main_grad,
+                lr,
+                moment1,
+                moment2,
+                moment2_max,
+                beta1_pow_acc,
+                beta2_pow_acc,
+                master_weight,
+                found_inf,
+                _beta1,
+                _beta2,
+                _epsilon,
+                lr_ratio_,
+                _weight_decay,
+                with_decay,
+                _lazy_mode,
+                1000,
+                find_master,
+                False,
+                self.amsgrad,
+            )
+            np.testing.assert_allclose(
+                param.astype("float32").numpy(), ref_param.numpy(), rtol=1e-2
+            )
+
+            if self.amsgrad:
+                np.testing.assert_allclose(
+                    master_weight.numpy(), ref_param.numpy(), rtol=1e-4
+                )
+            else:
+                np.testing.assert_allclose(
+                    master_weight.numpy(), ref_param.numpy(), rtol=1e-6
+                )
+
+        else:
+            _, _, _, _, _, _, _ = paddle._C_ops.adamw_(
+                param,
+                grad,
+                lr,
+                moment1,
+                moment2,
+                moment2_max,
+                beta1_pow_acc,
+                beta2_pow_acc,
+                master_weight,
+                found_inf,
+                _beta1,
+                _beta2,
+                _epsilon,
+                lr_ratio_,
+                _weight_decay,
+                with_decay,
+                _lazy_mode,
+                1000,
+                find_master,
+                False,
+                self.amsgrad,
+            )
+            np.testing.assert_allclose(
+                param.astype("float32").numpy(), ref_param.numpy(), rtol=1e-2
+            )
+
+            if self.amsgrad:
+                np.testing.assert_allclose(
+                    master_weight.numpy(), ref_param.numpy(), rtol=1e-4
+                )
+            else:
+                np.testing.assert_allclose(
+                    master_weight.numpy(), ref_param.numpy(), rtol=1e-6
+                )
+
+    def test_adamw_op_dygraph_place_amp_with_maingrad_with_moment_bf16(self):
+        for _ in range(10):
+            shape = paddle.randint(1, 1024, [2])
+            for place in self._get_places():
+                use_main_grad_list = [True, False]
+                for use_main_grad in use_main_grad_list:
+                    self._test_adamw_op_dygraph_place_amp_with_maingrad_with_moment_bf16(
+                        place, shape, use_main_grad
+                    )
 
 
 if __name__ == "__main__":
