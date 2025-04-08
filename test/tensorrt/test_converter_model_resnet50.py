d26d0ae8565ec8b3a2b840e1798063978e244678
lizexu123 <39205361+lizexu123@users.noreply.github.com>
2025-04-08 10:48:16 +0800

    【Paddle TensorRT】Pir-trt support TensorRT Refittable (#71501)
    * pir-refittable
    
    * 修改pd_op.layer_norm converter
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * merge
    
    * Revert "fix"
    
    This reverts commit 5a4df6e4c7f4b9bd8aa6bf1ffcae22f076d20d00.
    
    * merge
    
    * fix
    
    * 功能完成
    
    * resnet50能够refit
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * review修改完毕
    
    * Add a new class called RefitManager that is solely used for managing content related to refitting.
    
    * add forbid_cast_op
    
    * codestyple-check
    
    * Add a convert function to ensure that the model name and weight name provided by the user are consistent, and fix the issue of duplicate naming between pd_op.batch_norm and weight_to_tensor.
    
    * fix the issue of duplicate naming
    
    * Only refit those with inputs from builtin.parameters
    
    * Fix the compilation and linking errors in test_tensorrt_engine_instruction.cc.
    
    * Remove unnecessary code from util.py
    
    * fix pd_op.fused_bias_dropout_residual_layer_norm
    
    * delete ceshi.md
    
    * deleted the extra comments
    
    * fix pd_op.batch_norm
    
    * fix
    
    * fix
    
    * fix
    
    * Added an extra unit test by mistake, please delete it.
    
    * fix pd_op.fused_conv2d_add_act
    
    * fix pd_op.fused_conv2d_add_act
    
    * Add the bias to pd_op.fused_conv2d_add_act.
    
    * The input for the bias might also be a builtin.constant
    
    * fix pd_op.fused_conv2d_add_act
    
    * fix bug
    
    * fix TestInstanceNormWith3DInputTRTPattern

--- test/tensorrt/test_converter_model_resnet50.py
+++ test/tensorrt/test_converter_model_resnet50.py
@@ -19,6 +19,7 @@ import unittest
 import numpy as np
 from get_program import (
     get_r50_program,
+    get_r50_refit_program,
 )
 
 import paddle
@@ -96,6 +97,66 @@ class TestConverterResNet50(unittest.TestCase):
             err_msg="Outputs are not within the 1e-3 tolerance",
         )
 
+    def test_refit(self):
+        # Step1: get program and init fake inputs
+        paddle.enable_static()
+        save_path = os.path.join(self.temp_dir.name, 'resnet50')
+        program, scope, param_dict = get_r50_refit_program(save_path)
+
+        # Set input
+        input_config = Input(
+            min_input_shape=(1, 3, 224, 224),
+            optim_input_shape=(1, 3, 224, 224),
+            max_input_shape=(4, 3, 224, 224),
+            input_data_type='float32',
+        )
+        _, input_optim_data, _ = input_config.generate_input_data()
+
+        # Create a TensorRTConfig with inputs as a required field.
+        trt_config = TensorRTConfig(inputs=[input_config])
+
+        output_var = program.list_vars()[-1]
+
+        # get original results(for tests only)
+
+        output_expected = predict_program(
+            program, {"input": input_optim_data}, [output_var]
+        )
+
+        trt_save_path = os.path.join(self.temp_dir.name, 'resnet50trt')
+        trt_config.save_model_dir = trt_save_path
+        trt_config.refit_params_path = save_path + '.pdiparams'
+        model_dir = save_path
+
+        program_with_trt = paddle.tensorrt.convert(model_dir, trt_config)
+        config = paddle_infer.Config(
+            trt_config.save_model_dir + '.json',
+            trt_config.save_model_dir + '.pdiparams',
+        )
+        config.switch_ir_debug(True)
+        if paddle.is_compiled_with_cuda():
+            config.enable_use_gpu(100, 0)
+        else:
+            config.disable_gpu()
+        predictor = paddle_infer.create_predictor(config)
+
+        paddle.disable_static()
+        for i, input_instrance in enumerate(trt_config.inputs):
+            min_data, _, max_data = input_instrance.generate_input_data()
+            model_inputs = paddle.to_tensor(min_data)
+            output_converted = predictor.run([model_inputs])
+
+        output_expected = standardize(output_expected[0])
+        output_trt = standardize(output_converted[0].numpy())
+
+        np.testing.assert_allclose(
+            output_expected,
+            output_trt,
+            rtol=1e-1,
+            atol=1e-1,
+            err_msg="Outputs are not within the 1e-1 tolerance",
+        )
+
     def test_paddle_to_tensorrt_conversion_r50_collect_shape(self):
         # Step1: get program and init fake inputs
         program, scope, param_dict = get_r50_program()
@@ -134,9 +195,9 @@ class TestConverterResNet50(unittest.TestCase):
         np.testing.assert_allclose(
             output_expected,
             output_trt,
-            rtol=1e-3,
-            atol=1e-3,
-            err_msg="Outputs are not within the 1e-3 tolerance",
+            rtol=1e-2,
+            atol=1e-2,
+            err_msg="Outputs are not within the 1e-2 tolerance",
         )
 
     def test_convert_quant_model(self):
