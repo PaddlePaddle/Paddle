f96a96e5b1423a5a0baf21def12d6489a1862ac7
gushiwei <gushiwei@baidu.com>
2025-04-09 07:50:38 +0000

    test=document_fix ulimit

--- ci/run_setup.sh
+++ ci/run_setup.sh
@@ -37,7 +37,6 @@ if [ "$4" == "kunlun" ]; then
     echo "::group::Installing ninja-build"
     apt install ninja-build -y
     echo "::endgroup::"
-    ulimit -n 102400
     export PATH=/usr/local/bin:${PATH}
     ln -sf $(which python3.10) /usr/local/bin/python
     ln -sf $(which pip3.10) /usr/local/bin/pip
