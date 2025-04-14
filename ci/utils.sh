c25c071066c6ea50c6d33be43a37ea46fa26e147
gushiwei <gushiwei@baidu.com>
2025-04-14 12:03:26 +0000

    test=document_fix

--- ci/utils.sh
+++ ci/utils.sh
@@ -221,6 +221,22 @@ function clean_build_files() {
     done
 }
 
+function determine_xpu_runner() {
+    runner_name=$1
+
+    if [[ $runner_name == "paddle-1" ]]; then
+        echo "ASCEND_RT_VISIBLE_DEVICES=0,1,2,3" >> $GITHUB_ENV
+    elif [[ $runner_name == "paddle-2" ]]; then
+        echo "ASCEND_RT_VISIBLE_DEVICES=4,5,6,7" >> $GITHUB_ENV
+    elif [[ $runner_name == "paddle-3" ]]; then
+        echo "CUDA_VISIBLE_DEVICES=8,9,10,11" >> $GITHUB_ENV
+    elif [[ $runner_name == "paddle-4" ]]; then
+        echo "ASCEND_RT_VISIBLE_DEVICES=12,13,14,15" >> $GITHUB_ENV
+    else
+        echo "Unknown runner name: $runner_name"
+        exit 1
+    fi
+}
 
 function check_excode() {
     if [[ $EXCODE -eq 0 ]];then
