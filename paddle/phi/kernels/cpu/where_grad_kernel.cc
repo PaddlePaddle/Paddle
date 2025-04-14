6b29ca45f828abbbf58ce96e80d5e98aefc5e8ac
Kyotak <2023352046@email.szu.edu.cn>
2025-04-14 20:27:23 +0800

    update

--- paddle/phi/kernels/cpu/where_grad_kernel.cc
+++ paddle/phi/kernels/cpu/where_grad_kernel.cc
@@ -33,13 +33,13 @@ void WhereGradKernel(const Context& ctx,
   if (x_grad != nullptr) {
     auto* dx = ctx.template Alloc<T>(x_grad);
     for (int i = 0; i < numel; i++) {
-      dx[i] = dout[i] * (cond_data[i] ? 1. : 0.);
+      dx[i] = cond_data[i] ? dout[i] : T{};
     }
   }
   if (y_grad != nullptr) {
     auto* dy = ctx.template Alloc<T>(y_grad);
     for (int i = 0; i < numel; i++) {
-      dy[i] = dout[i] * (cond_data[i] ? 0. : 1.);
+      dy[i] = cond_data[i] ? T{} : dout[i];
     }
   }
 }
