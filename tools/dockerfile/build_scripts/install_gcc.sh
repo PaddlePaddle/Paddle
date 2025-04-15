0c484b2fdc95e3f7021c74ea96bfd7228ed5ee1f
Jingxiao GU <3947651+dawnwords@users.noreply.github.com>
2025-04-15 15:11:59 +0800

    fix: install_gcc.sh typo to reduce image size

--- tools/dockerfile/build_scripts/install_gcc.sh
+++ tools/dockerfile/build_scripts/install_gcc.sh
@@ -71,7 +71,7 @@ elif [ "$1" == "gcc121" ]; then
   cd .. && mkdir temp_gcc121 && cd temp_gcc121 && \
   ../gcc-12.1.0/configure --prefix=/usr/local/gcc-12.1 --enable-checking=release --enable-languages=c,c++ --disable-multilib && \
   make -j8 && make install
-  cd .. && rm -rf temp_gcc122 gcc-12.1.0 gcc-12.1.0.tar.gz
+  cd .. && rm -rf temp_gcc121 gcc-12.1.0 gcc-12.1.0.tar.gz
   cp ${lib_so_6} ${lib_so_6}.bak  && rm -f ${lib_so_6} &&
   ln -s /usr/local/gcc-12.1/lib64/libstdc++.so.6 ${lib_so_6} && \
   cp /usr/local/gcc-12.1/lib64/libstdc++.so.6.0.30 ${lib_path}
