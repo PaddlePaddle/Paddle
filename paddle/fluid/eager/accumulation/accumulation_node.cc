3ab805a400997c822d93234fcffcc0b39ebb1abe
xuexixi <xuexixi@baidu.com>
2025-04-16 11:05:49 +0800

    fix custom grad accumulation

--- paddle/fluid/eager/accumulation/accumulation_node.cc
+++ paddle/fluid/eager/accumulation/accumulation_node.cc
@@ -134,7 +134,12 @@ static void CopyOrAddTensor(paddle::Tensor* tensor,
                 tensor->impl());
         paddle::Tensor tensor_values(
             std::make_shared<phi::DenseTensor>(tensor_dist->value()));
-        paddle::imperative::TensorAdd<paddle::Tensor>(t_values, &tensor_values);
+        if (tensor_values.initialized() == false) {
+          VLOG(3) << "Move Tensor ptr: " << t.impl();
+          *tensor = t;
+        }else {
+          paddle::imperative::TensorAdd<paddle::Tensor>(t_values, &tensor_values);
+        }
       } else {
         // TODO(jiabin): Support Other TensorBase later
         // TODO(zhanlve): Replace SelectedRowsAddTensor with
