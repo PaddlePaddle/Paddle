0b725586f8364a42bba183d4e1e25e04dc8c1890
Junjie Zhang <1356732652@qq.com>
2025-04-17 15:39:25 +0800

    fix

--- python/paddle/tensorrt/converter_utils.py
+++ python/paddle/tensorrt/converter_utils.py
@@ -754,27 +754,12 @@ def convert_conv2d(network, paddle_op, inputs):
     set_layer_name(layer, paddle_op)
     support_fp32_mix_precision(paddle_op.name(), layer)
 
-    filter_param = paddle_op.operands()[1].source().get_defining_op()
-    try:
-        filter_name = filter_param.attrs()['parameter_name']
-    except:
-        while filter_param.name() not in [
-            "builtin.parameter",
-            "builtin.constant",
-        ]:
-            filter_param = filter_param.operands()[0].source().get_defining_op()
-            if filter_param.name() in [
-                "builtin.parameter",
-                "builtin.constant",
-            ]:
-                filter_name = filter_param.attrs()['parameter_name']
-                break
-        else:
-            raise ValueError(
-                f"Unsupported filter source operation: {filter_param.name()}"
-            )
-    refit_manager = RefitManager()
-    refit_manager.set_mapping(filter_name, filter_name, RefitRole.CONSTANT)
+    trt_manager = TensorRTConfigManager()
+    if trt_manager.get_refit_params_path():
+        filter_param = paddle_op.operands()[1].source()
+        filter_name = filter_param.get_defining_op().attrs()['parameter_name']
+        refit_manager = RefitManager()
+        refit_manager.set_mapping(filter_name, filter_name, RefitRole.CONSTANT)
 
     return layer.get_output(0)
 
@@ -856,27 +841,12 @@ def convert_conv3d(network, paddle_op, inputs):
 
     layer.dilation_nd = nv_dilations
     set_layer_name(layer, paddle_op)
-    filter_param = paddle_op.operands()[1].source().get_defining_op()
-    try:
-        filter_name = filter_param.attrs()['parameter_name']
-    except:
-        while filter_param.name() not in [
-            "builtin.parameter",
-            "builtin.constant",
-        ]:
-            filter_param = filter_param.operands()[0].source().get_defining_op()
-            if filter_param.name() in [
-                "builtin.parameter",
-                "builtin.constant",
-            ]:
-                filter_name = filter_param.attrs()['parameter_name']
-                break
-        else:
-            raise ValueError(
-                f"Unsupported filter source operation: {filter_param.name()}"
-            )
-    refit_manager = RefitManager()
-    refit_manager.set_mapping(filter_name, filter_name, RefitRole.CONSTANT)
+    trt_manager = TensorRTConfigManager()
+    if trt_manager.get_refit_params_path():
+        filter_param = paddle_op.operands()[1].source()
+        filter_name = filter_param.get_defining_op().attrs()['parameter_name']
+        refit_manager = RefitManager()
+        refit_manager.set_mapping(filter_name, filter_name, RefitRole.CONSTANT)
 
     return layer.get_output(0)
 
