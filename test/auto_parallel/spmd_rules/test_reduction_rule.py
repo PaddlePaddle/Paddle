4d8146586201c1e830b958fd2222b30d24dd3942
Glencsa <3501406249@qq.com>
2025-04-17 11:18:58 +0800

    add CI to test min ops.

--- test/auto_parallel/spmd_rules/test_reduction_rule.py
+++ test/auto_parallel/spmd_rules/test_reduction_rule.py
@@ -482,5 +482,14 @@ class TestSquaredL2NormSPMDRule(TestReductionSPMDRule):
         self.kernel = "squared_l2_norm"
 
 
+class TestMinSPMDRule(TestReductionSPMDRule):
+    """
+    Unit tests for min spmd rule.
+    """
+
+    def config(self):
+        self.kernel = "min"
+
+
 if __name__ == "__main__":
     unittest.main()
