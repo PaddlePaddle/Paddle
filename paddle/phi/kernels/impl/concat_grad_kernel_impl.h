e102c302250e4bad67311cbddd5061d894ab4c18
HydrogenSulfate <490868991@qq.com>
2025-04-22 19:46:43 +0800

    fix concat grad kernel

--- paddle/phi/kernels/impl/concat_grad_kernel_impl.h
+++ paddle/phi/kernels/impl/concat_grad_kernel_impl.h
@@ -45,13 +45,8 @@ void ConcatGradKernel(const Context& dev_ctx,
   // get output tensor that the name is not kEmptyVarName
   std::vector<DenseTensor*> outputs;
   for (size_t j = 0; j < outs.size(); ++j) {
-    if (outs[j] && outs[j]->numel() != 0UL) {
-      dev_ctx.template Alloc<T>(outs[j]);
-
-      outputs.push_back(outs[j]);
-    } else {
-      outputs.push_back(nullptr);
-    }
+    dev_ctx.template Alloc<T>(outs[j]);
+    outputs.push_back(outs[j]);
   }
 
   // Sometimes direct copies will be faster, this maybe need deeply analysis.
