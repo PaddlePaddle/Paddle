2dba292f683daa3e46b3c46d91e743edb4faa666
gushiwei <gushiwei@baidu.com>
2025-04-22 02:45:34 +0000

    test=document_fix

--- ci/coverage_judge.sh
+++ ci/coverage_judge.sh
@@ -33,7 +33,7 @@ if [ "$COVERAGE_LINES_ASSERT" = "1" ] || [ "$PYTHON_COVERAGE_LINES_ASSERT" = "1"
     echo "exit 9" > /tmp/paddle_coverage.result
     python ${PADDLE_ROOT}/tools/get_pr_title.py skip_coverage_check && NOT_CHECK_COVERAGE_PR=1
     if [[ "${NOT_CHECK_COVERAGE_PR}" = "1" ]];then
-        echo "Skip coverage check in the PR-CI-Coverage pipeline."
+        echo "Skip coverage check in the Coverage pipeline."
         exit 0
     fi
     exit 9
