047f483020ff10cc8e4593f5146f3bf02f777bb2
gushiwei <gushiwei@baidu.com>
2025-04-23 11:21:54 +0000

    test=document_fix cfs

--- ci/utils.sh
+++ ci/utils.sh
@@ -1044,6 +1044,7 @@ set +x
         echo "ipipe_log_param_Rerun_TestCases_Total_Time: $[ $rerun_ut_endTime_s - $rerun_ut_startTime_s ]s"
         echo "ipipe_log_param_Rerun_TestCases_Total_Time: $[ $rerun_ut_endTime_s - $rerun_ut_startTime_s ]s" >> ${PADDLE_ROOT}/build/build_summary.txt
         if [ "$WITH_ROCM" != "ON" ];then
+            mkdir -p /home/data/cfs/coverage/${PR_ID}/${COMMIT_ID}/
             cp $PADDLE_ROOT/build/Testing/Temporary/CTestCostData.txt /home/data/cfs/coverage/${PR_ID}/${COMMIT_ID}/
         fi
         if [[ "$EXIT_CODE" != "0" ]]; then
