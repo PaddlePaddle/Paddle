04737eb84353659c58c511afbd2f6ebce97d2bde
feixi21 <1802550529@qq.com>
2025-04-24 15:35:31 +0800

    fix bugs

--- paddle/phi/kernels/moe_unzip_kernel.cc
+++ paddle/phi/kernels/moe_unzip_kernel.cc
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#pragma once
-
 #include "paddle/phi/kernels/moe_unzip_kernel.h"
 
 #include "paddle/phi/backends/all_context.h"
