75437eaecc73330377ea190b26725dc102c21dcf
gushiwei <gushiwei@baidu.com>
2025-04-24 14:26:49 +0000

    test=document_fix

--- ci/download_tp_cache.sh
+++ ci/download_tp_cache.sh
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cmake_md5=`find ${work_dir} -path './cmake/*' -type f |xargs md5sum|md5sum |awk '{print $1}'`
-third_party_md5=`find ${work_dir} -path './third_party/*' -type f |xargs md5sum|md5sum|awk '{print $1}'`
+cmake_md5=`find ${GITHUB_WORKSPACE} -path './cmake/*' -type f |xargs md5sum|md5sum |awk '{print $1}'`
+third_party_md5=`find ${GITHUB_WORKSPACE} -path './third_party/*' -type f |xargs md5sum|md5sum|awk '{print $1}'`
 export md5_content=`echo ${cmake_md5} ${third_party_md5} |md5sum|awk '{print $1}'`
 xz_dir="$1" && echo "xz_dir=${xz_dir}" >> $GITHUB_ENV
 xz_file_tar="${xz_dir}/${md5_content}.tar"
@@ -22,12 +22,12 @@ if [ ! -f "${xz_file}" ];then
     set +e
     wget -q --no-proxy --no-check-certificate https://paddle-github-action.bj.bcebos.com/home/.cache/$2/third_party/${md5_content}.tar.xz; result=$?
     if [ $result -eq 0 ];then
-        mkdir -p ${work_dir}/build
+        mkdir -p ${GITHUB_WORKSPACE}/build
         mv ${md5_content}.tar.xz ${xz_dir}
     else
         mkdir -p ${xz_dir}
         echo "update_cached_package=ON" >> $GITHUB_ENV
     fi
 else
-    mkdir -p ${work_dir}/build
+    mkdir -p ${GITHUB_WORKSPACE}/build
 fi
