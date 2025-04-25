b279aea39b68fb7ef30cc35b7f2fc330e5df6f00
ZhenxingLi <zhenxingli@hust.edu.cn>
2025-04-25 19:06:45 +0800

    Update optimizer.py

--- python/paddle/optimizer/optimizer.py
+++ python/paddle/optimizer/optimizer.py
@@ -2000,9 +2000,16 @@ class Optimizer:
             for param in self._param_groups:
                 if param.stop_gradient:
                     continue
-                if param._grad_ivar() is not None:
-                    grad_var = param._grad_ivar()
-                    params_grads.append((param, grad_var))
+                enable_inplace_master_grad = (
+                    os.getenv("FLAGS_enable_inplace_master_grad") == '1'
+                )
+                if not enable_inplace_master_grad:
+                    if param._grad_ivar() is not None:
+                        grad_var = param._grad_ivar()
+                        params_grads.append((param, grad_var))
+                else:
+                    if param.main_grad is not None:
+                        params_grads.append((param, param.main_grad))
 
             self._apply_optimize(
                 loss=None,
