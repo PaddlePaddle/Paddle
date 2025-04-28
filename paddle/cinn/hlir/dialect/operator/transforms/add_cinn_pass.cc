fbe4274dc9d4ed4da969fc0ca477dadae22b2ff6
huangjiyi <43315610+huangjiyi@users.noreply.github.com>
2025-04-28 10:35:19 +0800

    [CINN] Implement FoldOutputDataDerivableOpsPass (#72423)

--- paddle/cinn/hlir/dialect/operator/transforms/add_cinn_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/add_cinn_pass.cc
@@ -38,9 +38,9 @@
 #include "paddle/cinn/hlir/dialect/operator/transforms/convert_fa_to_qkvmha_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/convert_memory_effec_attn_to_flash_attn_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/dynamic_reshape_pass.h"
-#include "paddle/cinn/hlir/dialect/operator/transforms/fold_assign_value_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/fold_full_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/fold_manipulation_ops_pass.h"
+#include "paddle/cinn/hlir/dialect/operator/transforms/fold_output_data_derivable_ops_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/fuse_parallel_matmul_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/fuse_shape_ops_into_generate_shape_op_pass.h"
 #include "paddle/cinn/hlir/dialect/operator/transforms/fusion_fallback_pass.h"
@@ -149,7 +149,8 @@ void ApplyPdToCinnPass(
   }
   pass_manager->AddPass(cinn::dialect::ir::CreateRemoveAssignOutPass());
   pass_manager->AddPass(cinn::dialect::ir::CreateFoldFullOpPass());
-  pass_manager->AddPass(cinn::dialect::ir::CreateFoldAssignValueOpPass());
+  pass_manager->AddPass(
+      cinn::dialect::ir::CreateFoldOutputDataDerivableOpsPass());
   pass_manager->AddPass(cinn::dialect::ir::CreateConv2dTransposeFilterPass());
   pass_manager->AddPass(cinn::dialect::ir::CreateConvertMEA2FAPass());
   pass_manager->AddPass(cinn::dialect::ir::CreateConvertFA2QKVMHAPass());
