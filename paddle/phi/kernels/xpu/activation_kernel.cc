027b5e906b5513e646ba187bb5a5d35ccb46a0b9
zhangyikun02 <48021248+zhangyk0314@users.noreply.github.com>
2025-04-29 13:59:53 +0800

    [XPU] tanh support type of bfloat16 (#72528)

--- paddle/phi/kernels/xpu/activation_kernel.cc
+++ paddle/phi/kernels/xpu/activation_kernel.cc
@@ -681,8 +681,13 @@ PD_REGISTER_KERNEL(sqrt,
                    phi::dtype::float16,
                    phi::dtype::bfloat16) {}
 
-PD_REGISTER_KERNEL(
-    tanh, XPU, ALL_LAYOUT, phi::TanhKernel, float, phi::dtype::float16) {}
+PD_REGISTER_KERNEL(tanh,
+                   XPU,
+                   ALL_LAYOUT,
+                   phi::TanhKernel,
+                   float,
+                   phi::dtype::float16,
+                   phi::dtype::bfloat16) {}
 
 PD_REGISTER_KERNEL(square,
                    XPU,
