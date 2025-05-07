d220539b3786a894673579506c6efac17d8430ca
Liu Yiqun <liuyiqun01@baidu.com>
2025-05-07 15:55:55 +0800

    Fix compiling when cinn is not enabled.

--- paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/ap_infer_sym.cc
+++ paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/ap_infer_sym.cc
@@ -13,17 +13,28 @@
 // limitations under the License.
 
 #include "paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/ap_infer_sym.h"
-#include "paddle/ap/include/paddle/pir/infer_symbolic_shape_util.h"
+
 #include "paddle/common/ddim.h"
+#include "paddle/common/enforce.h"
 #include "paddle/common/layout.h"
 #include "paddle/fluid/pir/dialect/operator/interface/infer_symbolic_shape/infer_sym_utils.h"
 #include "paddle/fluid/pir/dialect/operator/ir/op_attribute.h"
+
+#ifdef PADDLE_WITH_CINN
+#include "paddle/ap/include/paddle/pir/infer_symbolic_shape_util.h"
+#endif
 
 namespace paddle::dialect {
 
 bool ApFacadeOpInferSymbolicShape(
     pir::Operation *op, pir::InferSymbolicShapeContext *infer_context) {
+#ifdef PADDLE_WITH_CINN
   return ap::dialect::PdOpApFacadeOpInferSymbolicShape(op, infer_context);
+#else
+  PADDLE_THROW(phi::errors::Unimplemented(
+      "ap_facade is not implemented when cinn is not enabled."));
+  return false;
+#endif
 }
 
 }  // namespace paddle::dialect
