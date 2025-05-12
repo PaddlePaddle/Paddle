b8346aaf221166621d95ecbf8dd388a39ad2e610
fangfangssj <99968055+fangfangssj@users.noreply.github.com>
2025-05-12 17:26:36 +0800

    【Paddle Tensor 规范化第二期】elementwise_pow support complex (#72518)
    * add complex
    
    * add test
    
    * fix ci
    
    * ci test
    
    * ci test
    
    * ci test
    
    * fix test
    
    * fix test

--- paddle/phi/kernels/kps/elementwise_kernel.cu
+++ paddle/phi/kernels/kps/elementwise_kernel.cu
@@ -252,7 +252,9 @@ PD_REGISTER_KERNEL(elementwise_pow,
                    int,
                    int64_t,
                    phi::dtype::float16,
-                   phi::dtype::bfloat16) {}
+                   phi::dtype::bfloat16,
+                   phi::dtype::complex<float>,
+                   phi::dtype::complex<double>) {}
 PD_REGISTER_KERNEL(copysign,
                    GPU,
                    ALL_LAYOUT,
