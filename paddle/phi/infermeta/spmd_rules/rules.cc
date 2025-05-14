8747e1be41223c478f5c3c868fe957febd1ea068
ooooo <3164076421@qq.com>
2025-05-14 22:01:05 +0800

    fix rules

--- paddle/phi/infermeta/spmd_rules/rules.cc
+++ paddle/phi/infermeta/spmd_rules/rules.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/phi/infermeta/spmd_rules/rules.h"
-#include "paddle/phi/infermeta/spmd_rules/topk.h"
 
 /**
  * Design Notes:
