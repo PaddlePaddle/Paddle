607b9d3ba46c3e027902490126a7f2feb9930918
mayiru1021 <1534113846@qq.com>
2025-05-14 09:29:07 +0000

    test_inv可以通过，test_matmul的问题在pr中已提出

--- paddle/phi/kernels/funcs/elementwise_base.h
+++ paddle/phi/kernels/funcs/elementwise_base.h
@@ -285,11 +285,6 @@ void CommonForwardBroadcastCPU(const DenseTensor &x,
   const T *x_data = x.data<T>();
   const T *y_data = y.data<T>();
 
-  // PADDLE_ENFORCE_NOT_NULL(
-  //     x_data, errors::InvalidArgument("The input X should not be empty."));
-  // PADDLE_ENFORCE_NOT_NULL(
-  //     y_data, errors::InvalidArgument("The input Y should not be empty."));
-
   OutType *out_data = ctx.Alloc<OutType>(z);
 
   const int64_t out_size = std::accumulate(out_dims_array,
