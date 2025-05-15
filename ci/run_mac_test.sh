d15c8021faa053df1428092be141669f25bd97c6
tianshuo78520a <707759223@qq.com>
2025-05-15 07:07:47 +0000

    test=document_fix

--- ci/run_mac_test.sh
+++ ci/run_mac_test.sh
@@ -206,7 +206,7 @@ EOF
                                     retry_unittests_regular="$retry_unittests_regular|^$line$"
                                 fi
                             done
-                        echo y |xargs rm -rf $tmp_dir/*
+			rm -rf `find $tmp_dir -type f `
                         failed_test_lists=''
                         ctest -R "($retry_unittests_regular)" --output-on-failure -j 4 | tee $tmpfile
                         collect_failed_tests
