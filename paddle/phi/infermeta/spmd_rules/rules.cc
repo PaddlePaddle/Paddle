c00a398efc38a3fa53466b90478f502fe3174294
ooooo <3164076421@qq.com>
2025-05-15 21:23:15 +0800

    add spmd_rule for roll and roll_grad

--- paddle/phi/infermeta/spmd_rules/rules.cc
+++ paddle/phi/infermeta/spmd_rules/rules.cc
@@ -752,4 +752,9 @@ PD_REGISTER_SPMD_RULE(nonzero,
 
 // add_n
 PD_REGISTER_SPMD_RULE(add_n, PD_INFER_SPMD(phi::distributed::AddNInferSpmd));
+
+// roll
+PD_REGISTER_SPMD_RULE(roll,
+                      PD_INFER_SPMD(phi::distributed::RollInferSpmd),
+                      PD_INFER_SPMD(phi::distributed::RollGradInferSpmd));
 }  // namespace phi::distributed
