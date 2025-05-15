6657d11a8bf886d728a6271da9cf3f83911602cf
tianshuo78520a <707759223@qq.com>
2025-05-15 09:03:09 +0000

    test=document_fix

--- ci/run_mac_test.sh
+++ ci/run_mac_test.sh
@@ -147,7 +147,7 @@ EOF
             echo "Unittests with nightly labels  are only run at night"
             echo "========================================="
         fi
-        check_approvals_of_unittest 2
+        check_approvals_of_unittest
         # serial_list: Some single tests need to reduce concurrency
         single_list="^test_cdist$|^test_resnet$|^test_concat_op$|^test_transformer$|^test_bert_with_stride$|^test_paddle_save_load$"
         get_precision_ut_mac
@@ -328,6 +328,30 @@ function show_ut_retry_result() {
             exit 8;
         fi
     fi
+}
+
+function check_approvals_of_unittest() {
+    set +x
+    if [ "$GITHUB_API_TOKEN" == "" ] || [ "$GIT_PR_ID" == "" ]; then
+        return 0
+    fi
+
+    unittest_spec_diff=`python ${PADDLE_ROOT}/tools/diff_unittest.py ${PADDLE_ROOT}/paddle/fluid/UNITTEST_DEV.spec ${PADDLE_ROOT}/paddle/fluid/UNITTEST_PR.spec`
+    if [ "$unittest_spec_diff" != "" ]; then
+        approval_line=`curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/Paddle/pulls/${GIT_PR_ID}/reviews?per_page=10000`
+        APPROVALS=`echo ${approval_line}|python ${PADDLE_ROOT}/tools/check_pr_approval.py 1 22165420 52485244 32428676 45041955`
+        echo "current pr ${GIT_PR_ID} got approvals: ${APPROVALS}"
+        if [ "${APPROVALS}" == "FALSE" ]; then
+            echo "************************************"
+            echo -e "It is forbidden to disable or delete the unit-test.\n"
+            echo -e "If you must delete it temporarily, please add it to[https://github.com/PaddlePaddle/Paddle/wiki/Temporarily-disabled-Unit-Test]."
+            echo -e "Then you must have one RD (kolinwei(recommended), chalsliu, XieYunshen or zhouwei25) approval for the deletion of unit-test. \n"
+            echo -e "If you have any problems about deleting unit-test, please read the specification [https://github.com/PaddlePaddle/Paddle/wiki/Deleting-unit-test-is-forbidden]. \n"
+            echo -e "Following unit-tests are deleted in this PR: \n ${unittest_spec_diff} \n"
+            echo "************************************"
+            exit 6
+        fi
+   fi
 }
 
 run_mac_test ${PYTHON_ABI:-""} ${PROC_RUN:-1}
