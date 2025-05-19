6ced123ec710e2f09deedf55a19c17a2ad4b055e
SUN Dong <1395924413@qq.com>
2025-05-19 23:25:58 +0800

    Update matrix_power_grad_kernel_impl.h
    fix error

--- paddle/phi/kernels/impl/matrix_power_grad_kernel_impl.h
+++ paddle/phi/kernels/impl/matrix_power_grad_kernel_impl.h
@@ -194,7 +194,7 @@ void MatrixPowerGradKernel(const Context& ctx,
   auto dOut = &out_grad;
   auto dX = x_grad;
   if (x_grad && x_grad->numel() == 0) {
-    x_grad->Resize(x_grad.dims());
+    x_grad->Resize(x_grad->dims());
     ctx.template Alloc<T>(x_grad);
     return;
   }
