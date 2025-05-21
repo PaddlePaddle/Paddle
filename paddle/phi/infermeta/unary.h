d61bfc123dd79c3daa987bc6f26cbbee9891b2c8
Kyotak <yuhongh@qq.com>
2025-05-21 19:08:26 +0800

    fix

--- paddle/phi/infermeta/unary.h
+++ paddle/phi/infermeta/unary.h
@@ -621,13 +621,13 @@ void StrictReduceIntArrayAxisInferMetaBase(const MetaTensor& x,
                                            bool keep_dim,
                                            bool reduce_all,
                                            MetaTensor* out,
-                                           MetaConfig config);
+                                           MetaConfig config = MetaConfig());
 
 void StrictReduceIntArrayAxisInferMeta(const MetaTensor& x,
                                        const IntArray& axis,
                                        bool keep_dim,
                                        MetaTensor* out,
-                                       MetaConfig config);
+                                       MetaConfig config = MetaConfig());
 
 void ReduceScatterInferMeta(const MetaTensor& x, int nranks, MetaTensor* out);
 
