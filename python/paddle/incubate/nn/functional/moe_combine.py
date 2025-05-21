df96e812e1a1d0746f54fbc3a84a0de54cf3e487
pesionzhao <pesionzhao@gmail.com>
2025-05-21 06:02:22 +0000

    update python API

--- python/paddle/incubate/nn/functional/moe_combine.py
+++ python/paddle/incubate/nn/functional/moe_combine.py
@@ -32,32 +32,3 @@ def moe_combine(
     }
     helper.append_op(type='moe_combine', inputs=inputs, outputs={'y': y})
     return y
-
-if __name__ == "__main__":
-    print("This module is not for direct use.")
-    x = paddle.arange(1, 16).view((5, 3)).astype('float32')
-    combine_weights = paddle.to_tensor([
-        [0, 0],
-        [0, 0],
-        [0.5, 0.5],
-        [0.5, 0.5],
-        [0.5, 0.5]
-    ])
-
-    # 分散索引
-    scatter_index = paddle.to_tensor([
-    [0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0]
-    ]).astype('int32')
-
-    # 输出计算
-    output = paddle.zeros((5, 3))
-    for s in range(5):
-        expert0_idx = scatter_index[0, s]
-        expert1_idx = scatter_index[1, s]
-        output[s] = (
-            x[expert0_idx] * combine_weights[s, 0] + 
-            x[expert1_idx] * combine_weights[s, 1]
-        )
-    print(output)
-    print(moe_combine(x, combine_weights, scatter_index))
