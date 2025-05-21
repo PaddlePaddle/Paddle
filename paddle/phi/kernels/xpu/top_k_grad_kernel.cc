a3ca9a2e07d1bacf904b1d5625bab906a47289eb
Jianbang Yang <yangjianbang112@gmail.com>
2025-05-21 21:34:57 +0800

    fix

--- paddle/phi/kernels/xpu/top_k_grad_kernel.cc
+++ paddle/phi/kernels/xpu/top_k_grad_kernel.cc
@@ -52,7 +52,6 @@ void TopkGradKernel(const Context& dev_ctx,
   xpu::ctx_guard RAII_GUARD(dev_ctx.x_context());
 
   const auto& in_dims = x.dims();
-  int k = k_scalar.to<int>();
 
   // get the real the axis and the k
   if (axis < 0) {
@@ -62,7 +61,7 @@ void TopkGradKernel(const Context& dev_ctx,
   // allocate the xpu memory for the x_grad
   T* x_grad_data = dev_ctx.template Alloc<T>(x_grad);
   const T* out_grad_data = out_grad.data<T>();
-  int64_t* indices_data = const_cast<int64_t*>(indices.data<int64_t>());
+  const int64_t* indices_data = indices.data<int64_t>();
 
   if (in_dims.size() == 0) {
     phi::Copy<Context>(dev_ctx, out_grad, dev_ctx.GetPlace(), false, x_grad);
@@ -78,63 +77,18 @@ void TopkGradKernel(const Context& dev_ctx,
                          x_grad->dtype(),
                          x_grad);
 
-  // We want to add a different constant value to each row of a 2D matrix for
-  // the following scatter kernel. For example: If n = 5 and the input matrix
-  // is:
-  // [[0, 1, 2],
-  //  [3, 4, 5],
-  //  [6, 7, 8]]
-  //
-  // Then:
-  // Row 1 → add 0 * 5 = 0
-  // Row 2 → add 1 * 5 = 5
-  // Row 3 → add 2 * 5 = 10
-  //
-  // Result:
-  // [[ 0,  1,  2],
-  //  [ 8,  9, 10],
-  //  [16, 17, 18]]
-
-  PADDLE_ENFORCE_EQ(indices.numel(),
-                    pre * k * post,
-                    ::common::errors::InvalidArgument(
-                        "indices.numel() must be pre * k * post, but got "
-                        "indices.numel() = %ld, pre "
-                        "= %ld, k = %d, n = %ld, post = %ld, axis=%ld",
-                        indices.numel(),
-                        pre,
-                        k,
-                        n,
-                        post,
-                        axis));
-
-  int64_t* offsets = RAII_GUARD.alloc<int64_t>(pre * post);
-  int64_t* accmulated_indices = RAII_GUARD.alloc<int64_t>(indices.numel());
-
-  int ret =
-      xpu::range<int64_t>(dev_ctx.x_context(), offsets, 0LL, n, pre * post);
-  PADDLE_ENFORCE_XDNN_SUCCESS(ret, "range");
-
-  ret = xpu::broadcast_mul<int64_t>(dev_ctx.x_context(),
-                                    indices_data,
-                                    offsets,
-                                    accmulated_indices,
-                                    {pre, k, post},
-                                    {pre, 1, post});
-  PADDLE_ENFORCE_XDNN_SUCCESS(ret, "broadcast_mul");
-
-  const xpu::VectorParam<int64_t> indices_vp = {
-      nullptr, indices.numel(), accmulated_indices};
   // launch the xpu kernel to assign the grad
-  ret = xpu::scatter<XPUType, int64_t>(
+  int ret = xpu::scatter_element<XPUType, int64_t>(
       dev_ctx.x_context(),
       reinterpret_cast<const XPUType*>(x_grad_data),
       reinterpret_cast<const XPUType*>(out_grad_data),
+      indices_data,
       reinterpret_cast<XPUType*>(x_grad_data),
-      indices_vp,
       common::vectorize(x_grad->dims()),
+      common::vectorize(out_grad.dims()),
+      common::vectorize(indices.dims()),
       axis,
-      /*is_overwrite*/ true);
+      /*reduction=override*/ 0);
   PADDLE_ENFORCE_XDNN_SUCCESS(ret, "scatter");
 }
 
---
+++ test/xpu/test_top_k_op_xpu.py
@@ -0,0 +1,115 @@
+#   Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+from get_test_cover_info import (
+    XPUOpTestWrapper,
+    create_test_class,
+    get_xpu_op_support_types,
+)
+from op_test_xpu import XPUOpTest
+
+import paddle
+
+paddle.enable_static()
+
+
+def api_wrapper(x, k):
+    return paddle._legacy_C_ops.top_k(x, "k", k)
+
+
+def random_unique_float(shape, dtype):
+    # create a random float array with 10x length
+    numel = np.prod(shape)
+    arr = np.random.uniform(-10.0, 10.0, numel * 10).astype(dtype)
+    arr = np.unique(arr)
+    assert (
+        arr.shape[0] >= numel
+    ), f"failed to create enough unique values: {arr.shape[0]} vs {numel}"
+    arr = arr[:numel]
+    np.random.shuffle(arr)
+    arr = arr.reshape(shape)
+    return arr
+
+
+def numpy_topk(x, k=1, axis=-1, largest=True):
+    if axis < 0:
+        axis = len(x.shape) + axis
+    if largest:
+        indices = np.argsort(-x, axis=axis)
+    else:
+        indices = np.argsort(x, axis=axis)
+    if largest:
+        value = -np.sort(-x, axis=axis)
+    else:
+        value = np.sort(x, axis=axis)
+    indices = indices.take(indices=range(0, k), axis=axis)
+    value = value.take(indices=range(0, k), axis=axis)
+    return value, indices
+
+
+class XPUTestTopKOp(XPUOpTestWrapper):
+    def __init__(self):
+        self.op_name = 'top_k'
+        self.use_dynamic_create_class = False
+
+    class TestTopkOp(XPUOpTest):
+        def setUp(self):
+            self.place = paddle.XPUPlace(0)
+            self.variable_k = False
+            self.op_type = "top_k"
+            self.python_api = api_wrapper
+            self.init_args()
+            self.init_dtype()
+
+            k = self.top_k
+            input = np.random.random((self.row, k)).astype(self.dtype)
+            output = np.ndarray((self.row, k))
+            indices = np.ndarray((self.row, k)).astype("int64")
+            self.inputs = {'X': input}
+
+            if self.variable_k:
+                self.inputs['K'] = np.array([k]).astype("int32")
+            else:
+                self.attrs = {'k': k}
+
+            for rowid in range(self.row):
+                row = input[rowid]
+                output[rowid] = np.sort(row)[::-1][:k]
+                indices[rowid] = row.argsort()[::-1][:k]
+
+            self.outputs = {'Out': output, 'Indices': indices}
+
+        def init_dtype(self):
+            self.dtype = self.in_type
+
+        def test_check_output(self):
+            self.check_output_with_place(self.place)
+
+        def test_check_grad(self):
+            self.check_grad_with_place(self.place, ['X'], 'Out')
+
+        def init_args(self):
+            self.row = 100
+            self.top_k = 1
+
+
+support_types = get_xpu_op_support_types('top_k')
+for stype in support_types:
+    create_test_class(globals(), XPUTestTopKOp, stype)
+
+if __name__ == "__main__":
+    unittest.main()
