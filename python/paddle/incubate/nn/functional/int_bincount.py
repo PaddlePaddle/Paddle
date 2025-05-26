8797589dc7518d54916e0935deadee99a7406bbd
zhenghuaijin <zhenghuaijin@baidu.com>
2025-05-27 02:24:43 +0800

    finish rms norm bwd

--- python/paddle/incubate/nn/functional/int_bincount.py
+++ python/paddle/incubate/nn/functional/int_bincount.py
@@ -4,8 +4,8 @@ from paddle.fluid.data_feeder import convert_dtype
 
 
 def int_bincount(x, low, high, dtype=None, name=None):
-    if in_dynamic_or_pir_mode():
-        return _C_ops.moe_gate_dispatch_permute(x, gate_logits, corr_bias, k, capacity, world_size)
+    # if in_dynamic_or_pir_mode():
+    #     return _C_ops.moe_gate_dispatch_permute(x, gate_logits, corr_bias, k, capacity, world_size)
     helper = LayerHelper("int_bincount", **locals())
     out_dtype = dtype if dtype is not None else x.dtype
     y = helper.create_variable_for_type_inference(dtype=out_dtype)
