8d425d6958e02dc4d813360eefc67e6bc7bd4e70
co63oc <co63oc@users.noreply.github.com>
2025-05-26 10:10:20 +0800

    Fix

--- paddle/phi/kernels/cpu/mv_grad_kernel.cc
+++ paddle/phi/kernels/cpu/mv_grad_kernel.cc
@@ -16,6 +16,7 @@
 
 #include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/core/kernel_registry.h"
+#include "paddle/phi/kernels/full_kernel.h"
 #include "paddle/phi/kernels/funcs/blas/blas.h"
 
 namespace phi {
