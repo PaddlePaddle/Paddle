60b0353af54712339f333fb6c84c69cd87465928
co63oc <co63oc@users.noreply.github.com>
2025-05-26 17:02:18 +0800

    Fix

--- paddle/phi/kernels/cpu/nanmedian_kernel.cc
+++ paddle/phi/kernels/cpu/nanmedian_kernel.cc
@@ -220,19 +220,13 @@ void NanmedianKernel(const Context& dev_ctx,
                      DenseTensor* out,
                      DenseTensor* median_index) {
   if (x.numel() == 0) {
-    dev_ctx.template Alloc<T>(out);
-    dev_ctx.template Alloc<int64_t>(median_index);
-    if (out && out->numel() != 0) {
-      out->Resize(common::make_ddim({}));
-      median_index->Resize(common::make_ddim({}));
-      phi::Full<T, Context>(
-          dev_ctx, phi::IntArray(common::vectorize(out->dims())), NAN, out);
-      phi::Full<int64_t, Context>(
-          dev_ctx,
-          phi::IntArray(common::vectorize(median_index->dims())),
-          0,
-          median_index);
-    }
+    phi::Full<T, Context>(
+        dev_ctx, phi::IntArray(common::vectorize(out->dims())), NAN, out);
+    phi::Full<int64_t, Context>(
+        dev_ctx,
+        phi::IntArray(common::vectorize(median_index->dims())),
+        0,
+        median_index);
     return;
   }
   DenseTensor tmp_x;
