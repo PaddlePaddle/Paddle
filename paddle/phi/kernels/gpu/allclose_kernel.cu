089e5a8858a4dbc7776e1fc7ac1cf1dbe479d7f0
co63oc <co63oc@users.noreply.github.com>
2025-05-27 12:00:05 +0800

    Fix

--- paddle/phi/kernels/gpu/allclose_kernel.cu
+++ paddle/phi/kernels/gpu/allclose_kernel.cu
@@ -21,7 +21,7 @@
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/core/enforce.h"
 #include "paddle/phi/core/kernel_registry.h"
-
+#include "paddle/phi/kernels/full_kernel.h"
 namespace phi {
 
 template <typename T>
