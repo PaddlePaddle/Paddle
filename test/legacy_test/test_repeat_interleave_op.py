1b459f8158a8268b642a891049f4e2628fae2806
co63oc <co63oc@users.noreply.github.com>
2025-05-28 09:21:37 +0800

    Fix

--- test/legacy_test/test_repeat_interleave_op.py
+++ test/legacy_test/test_repeat_interleave_op.py
@@ -101,12 +101,11 @@ class TestRepeatInterleaveOp2(OpTest):
         self.check_grad(['X'], 'Out', check_pir=True)
 
 
-class TestRepeatInterleaveOp_ZeroSize(TestRepeatInterleaveOp):
+class TestRepeatInterleaveOp_ZeroSize(TestRepeatInterleaveOp2):
     def init_dtype_type(self):
         self.dim = 1
         self.x_type = np.float64
-        self.index_type = np.int64
-        self.x_shape = (0, 4, 5)
+        self.x_shape = (8, 0, 5)
         self.index_size = self.x_shape[self.dim]
 
 
