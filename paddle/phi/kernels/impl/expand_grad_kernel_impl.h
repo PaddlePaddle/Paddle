e76e8aeb611a62c807eb0398b6de79d3d0b45349
ooooo <3164076421@qq.com>
2025-05-29 00:35:17 +0800

    fix

--- paddle/phi/kernels/impl/expand_grad_kernel_impl.h
+++ paddle/phi/kernels/impl/expand_grad_kernel_impl.h
@@ -16,36 +16,11 @@
 
 #include "paddle/phi/core/tensor_utils.h"
 #include "paddle/phi/kernels/full_kernel.h"
-#include "paddle/phi/kernels/funcs/eigen/common.h"
-#include "paddle/phi/kernels/funcs/eigen/eigen_function.h"
+#include "paddle/phi/kernels/funcs/elementwise_base.h"
 #include "paddle/phi/kernels/impl/expand_kernel_impl.h"
+#include "paddle/phi/kernels/reduce_sum_kernel.h"
 
 namespace phi {
-template <typename Context, typename T, int Dims>
-void ExpandBackward(const Context& ctx,
-                    const DenseTensor& out_grad,
-                    const std::vector<int>& reshape_dims_vec,
-                    const std::vector<int>& reduce_dims_vec,
-                    DenseTensor* in_grad) {
-  size_t reshape_size = reshape_dims_vec.size();
-  size_t reduce_size = reduce_dims_vec.size();
-  ctx.template Alloc<T>(in_grad);
-  in_grad->data<T>();
-
-  auto x_grad = EigenVector<T>::Flatten(*in_grad);
-  Eigen::DSizes<Eigen::DenseIndex, Dims * 2> reshape_dims;
-  for (size_t i = 0; i < reshape_size; ++i) {
-    reshape_dims[i] = reshape_dims_vec[i];
-  }
-  Eigen::DSizes<Eigen::DenseIndex, Dims> reduce_dims;
-  for (size_t i = 0; i < reduce_size; ++i) {
-    reduce_dims[i] = reduce_dims_vec[i];
-  }
-  auto out_grad0 = EigenVector<T>::Flatten(out_grad);
-  auto& place = *ctx.eigen_device();
-  phi::funcs::EigenBroadcastGrad<std::decay_t<decltype(place)>, T, Dims>::Eval(
-      place, x_grad, out_grad0, reduce_dims, reshape_dims);
-}
 
 template <typename T, typename Context>
 void ExpandGradKernel(const Context& ctx,
@@ -53,8 +28,6 @@ void ExpandGradKernel(const Context& ctx,
                       const DenseTensor& out_grad,
                       const IntArray& shape,
                       DenseTensor* in_grad) {
-  auto expand_shape = shape.GetData();
-  auto x_dims = x.dims();
   if (x.numel() == 0 || out_grad.numel() == 0 ||
       (in_grad && in_grad->numel() == 0)) {
     ctx.template Alloc<T>(in_grad);
@@ -69,90 +42,10 @@ void ExpandGradKernel(const Context& ctx,
     phi::Copy(ctx, out_grad, ctx.GetPlace(), false, in_grad);
     return;
   }
-  auto vec_in_dims = common::vectorize<int>(x_dims);
-  auto diff = expand_shape.size() - vec_in_dims.size();
-  vec_in_dims.insert(vec_in_dims.begin(), diff, 1);
-  // 1. reshape_dims_vec is the broadcast parameter.
-  // 2. reduce_dims_vec is the dimension parameter to compute gradients. For
-  //    each dimension expanded, the gradients should be summed to original
-  //    size.
-  std::vector<int> repeat_times(vec_in_dims.size());
-  for (size_t i = 0; i < vec_in_dims.size(); ++i) {
-    if (expand_shape[i] < 0) {
-      repeat_times[i] = 1;
-    } else {
-      repeat_times[i] = expand_shape[i] / vec_in_dims[i];
-    }
-  }
-  std::vector<int> reshape_dims_vec;
-  std::vector<int> reduce_dims_vec;
-  for (size_t i = 0; i < repeat_times.size(); ++i) {
-    reduce_dims_vec.push_back(reshape_dims_vec.size());
-    reshape_dims_vec.push_back(repeat_times[i]);
-    reshape_dims_vec.push_back(vec_in_dims[i]);
-  }
-
-  int dims = reduce_dims_vec.size();
-
-  PADDLE_ENFORCE_GE(dims,
-                    0,
-                    common::errors::InvalidArgument(
-                        "The rank of the input 'Out@GRAD' for "
-                        "expand_v2_grad op must be greater than or "
-                        "equal to 0, but the value received is %d.",
-                        dims));
-  PADDLE_ENFORCE_LE(dims,
-                    MAX_RANK_SUPPORTED,
-                    common::errors::InvalidArgument(
-                        "The rank of the input 'Out@GRAD' for "
-                        "expand_v2_grad op must be less than or equal "
-                        "to %d, but the value received is %d.",
-                        MAX_RANK_SUPPORTED,
-                        dims));
-  switch (dims) {
-    case 0:
-      ExpandBackward<Context, T, 1>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    case 1:
-      ExpandBackward<Context, T, 1>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    case 2:
-      ExpandBackward<Context, T, 2>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    case 3:
-      ExpandBackward<Context, T, 3>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    case 4:
-      ExpandBackward<Context, T, 4>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    case 5:
-      ExpandBackward<Context, T, 5>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    case 6:
-      ExpandBackward<Context, T, 6>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    case 7:
-      ExpandBackward<Context, T, 7>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    case 8:
-      ExpandBackward<Context, T, 8>(
-          ctx, out_grad, reshape_dims_vec, reduce_dims_vec, in_grad);
-      break;
-    default:
-      PADDLE_THROW(common::errors::InvalidArgument(
-          "Only support tensor with rank being between 1 and %d. But "
-          "received tensor's rank = %d.",
-          MAX_RANK_SUPPORTED,
-          dims));
-  }
+  std::vector<int> reduce_dims =
+      funcs::GetReduceDim(in_grad->dims(), out_grad.dims(), -1);
+  phi::SumKernel<T, Context>(
+      ctx, out_grad, reduce_dims, out_grad.dtype(), false, in_grad);
 }
 
 }  // namespace phi
