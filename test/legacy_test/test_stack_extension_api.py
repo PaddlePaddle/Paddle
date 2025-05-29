eaf1dddacbd60845eb56e6d80c38309e6d072f64
enkilee <jeffrey0122@163.com>
2025-05-29 14:36:59 +0800

    fix

--- test/legacy_test/test_stack_extension_api.py
+++ test/legacy_test/test_stack_extension_api.py
@@ -258,22 +258,14 @@ class TestHStack(BaseTest, BaseCases):
             )
 
 
-class TestHStackZeroDim1(BaseTest, BaseCases):
-    def setUp(self):
-        self.func_paddle = paddle.hstack
-        self.func_numpy = np.hstack
-
+class TestHStackZeroDim1(TestHStack):
     def test_mix_ndim(self):
         d0 = generate_data([], count=1, dtype='float64')
         d1 = generate_data([], count=1, dtype='float64')
         self._test_all(rearrange_data(d0, d1))
 
 
-class TestHStackZeroDim2(BaseTest, BaseCases):
-    def setUp(self):
-        self.func_paddle = paddle.hstack
-        self.func_numpy = np.hstack
-
+class TestHStackZeroDim2(TestHStack):
     def test_mix_ndim(self):
         d0 = generate_data([1], count=1, dtype='float64')
         d1 = generate_data([1], count=1, dtype='float64')
