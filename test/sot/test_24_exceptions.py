252474e4a4be494cb1f86cb46dc330809604a87f
DrRyanHuang <zihaohuang@aliyun.com>
2025-05-30 14:30:10 +0000

    adaptation for 3.11+

--- test/sot/test_24_exceptions.py
+++ test/sot/test_24_exceptions.py
@@ -962,7 +962,7 @@ class TestBuiltinFunctionRaiseExceptionGuard(TestCaseBase):
         self.assert_results(foo_mod, 10)
         self.assert_exceptions(
             ZeroDivisionError,
-            "integer division or modulo by zero",
+            "integer (.)* modulo by zero",
             foo_mod,
             0,
         )
