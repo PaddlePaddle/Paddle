7baf634af0393437130f3b014f9d25fa3e02972f
co63oc <co63oc@users.noreply.github.com>
2025-06-01 15:29:39 +0800

    Fix

--- python/paddle/tensor/stat.py
+++ python/paddle/tensor/stat.py
@@ -209,7 +209,7 @@ def var(
         n = n - 1.0
     n.stop_gradient = True
     out /= n
-    if paddle.numel(x) == 0:
+    if paddle.numel(x).item() == 0:
         out_nan = paddle.full_like(out, paddle.nan)
         out_nan.stop_gradient = out.stop_gradient
         out = out_nan
