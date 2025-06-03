560e75dca00f96c3fbf9bf0b84e60a8376963de6
co63oc <co63oc@users.noreply.github.com>
2025-06-03 17:17:01 +0800

    Fix

--- paddle/phi/kernels/impl/fft_kernel_impl.h
+++ paddle/phi/kernels/impl/fft_kernel_impl.h
@@ -34,6 +34,13 @@ void FFTC2CKernel(const Context& ctx,
                   DenseTensor* out) {
   ctx.template Alloc<T>(out);
   if (x.numel() == 0) {
+    /*
+    This will return 0:
+    >>> scipy.fft.fft2(np.random.random([3, 0, 1, 2]), s=(1, 2), axes=(0, 1),
+norm='backward') array([[[[0.-0.j, 0.-0.j]],
+
+        [[0.-0.j, 0.-0.j]]]])
+    */
     phi::Full<T, Context>(
         ctx, phi::IntArray(common::vectorize(out->dims())), 0, out);
     return;
