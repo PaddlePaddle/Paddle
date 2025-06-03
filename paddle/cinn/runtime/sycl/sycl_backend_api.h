e0c1a6333ab7f45f8abddd6ff951fbd2bd874f13
feixi21 <1802550529@qq.com>
2025-06-03 16:42:59 +0800

    sycl reuse paddle stream

--- paddle/cinn/runtime/sycl/sycl_backend_api.h
+++ paddle/cinn/runtime/sycl/sycl_backend_api.h
@@ -20,6 +20,7 @@
 #include "paddle/cinn/common/target.h"
 #include "paddle/cinn/runtime/backend_api.h"
 #include "paddle/common/enforce.h"
+#include <hip/hip_runtime.h>
 using cinn::common::Arch;
 
 namespace cinn {
@@ -103,7 +104,7 @@ class SYCLBackendAPI final : public BackendAPI {
               MemcpyType type) final;
   void device_sync() final;
   void stream_sync(void* stream) final;
-  ::sycl::queue* get_now_queue();
+  ::sycl::queue* get_now_queue(void* stream);
   std::string GetGpuVersion();
   std::array<int, 3> get_max_grid_dims(
       std::optional<int> device_id = std::nullopt) final;
@@ -121,6 +122,9 @@ class SYCLBackendAPI final : public BackendAPI {
   int now_device_id = 0;
   // whether the BackendAPI is initialized.
   bool initialized_{false};
+  hipDevice_t device_;Add commentMore actions
+  hipCtx_t context_;
+  
 };
 }  // namespace sycl
 }  // namespace runtime
