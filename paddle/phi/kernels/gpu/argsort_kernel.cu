a25827464fd94be997b84daccc44386c7c0b5c61
chen huangrun <1181749441@qq.com>
2025-06-09 16:20:27 +0800

    fix

--- paddle/phi/kernels/gpu/argsort_kernel.cu
+++ paddle/phi/kernels/gpu/argsort_kernel.cu
@@ -273,13 +273,17 @@ void ArgFullSort(const phi::GPUContext& dev_ctx,
   }
 }
 template <typename T, typename IndType>
-void PerSort(const thrust::cuda_cub::par_t::stream_attachment_type exec_policy,
-             T* out_data,
+void PerSort(T* out_data,
              int64_t* ids_data,
              IndType start,
              IndType end,
              bool stable,
              bool descending) {
+#ifdef PADDLE_WITH_CUDA
+  const auto& exec_policy = thrust::cuda::par.on(dev_ctx.stream());
+#else
+  const auto& exec_policy = thrust::hip::par.on(dev_ctx.stream());
+#endif
   if (stable) {
     if (descending) {
       thrust::stable_sort_by_key(exec_policy,
@@ -353,8 +357,7 @@ void ArgsortKernel(const Context& dev_ctx,
     int64_t start = 0;
     int64_t end = std::min(start + per_number, size);
     if (end == size) {
-      PerSort<T, int64_t>(
-          exec_policy, out_data, ids_data, start, end, stable, descending);
+      PerSort<T, int64_t>(out_data, ids_data, start, end, stable, descending);
     } else {
       // Sorting the segments and then merging them
       DenseTensor temp;
@@ -365,8 +368,7 @@ void ArgsortKernel(const Context& dev_ctx,
       int64_t* temp_ids = dev_ctx.template Alloc<int64_t>(&ids);
 
       while (start != size) {
-        PerSort<T, int64_t>(
-            exec_policy, out_data, ids_data, start, end, stable, descending);
+        PerSort<T, int64_t>(out_data, ids_data, start, end, stable, descending);
         if (start != 0) {
           auto config = phi::backends::gpu::GetGpuLaunchConfig1D(dev_ctx, end);
           merge_kernel<<<config.block_per_grid.x,
