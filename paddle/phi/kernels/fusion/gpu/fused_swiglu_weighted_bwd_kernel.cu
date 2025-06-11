94dd865c757ab0e12d457eafd49e9de4cac7bcb6
shanjiang7 <jiangshan.nsfz@outlook.com>
2025-06-11 15:17:18 +0800

    adapt for CI

--- paddle/phi/kernels/fusion/gpu/fused_swiglu_weighted_bwd_kernel.cu
+++ paddle/phi/kernels/fusion/gpu/fused_swiglu_weighted_bwd_kernel.cu
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/kernels/fused_swiglu_weighted_bwd.h"
+#include "paddle/phi/kernels/fused_swiglu_weighted_bwd_kernel.h"
 #include <cuda_runtime.h>
 #include <vector>
 #include "paddle/phi/backends/gpu/gpu_context.h"
@@ -321,6 +321,8 @@ PD_REGISTER_KERNEL(fused_swiglu_weighted_bwd,
                    GPU,
                    ALL_LAYOUT,
                    phi::FusedSwigluWeightedBwdKernel,
+                   float,
+                   double,
                    phi::dtype::bfloat16) {
   kernel->OutputAt(0).SetDataType(phi::DataType::BFLOAT16);
   kernel->OutputAt(1).SetDataType(phi::DataType::FLOAT32);
