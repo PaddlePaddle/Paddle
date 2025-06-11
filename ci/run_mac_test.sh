147a8fb2d4d3df3ca12436a69085bff56349e922
tianshuo78520a <707759223@qq.com>
2025-06-11 02:05:40 +0000

    fix

--- ci/run_mac_test.sh
+++ ci/run_mac_test.sh
@@ -15,7 +15,8 @@
 function run_mac_test() {
     export FLAGS_PIR_OPTEST=True
     export FLAGS_CI_PIPELINE=mac
-
+    tmp_dir='~/tmp'
+    mkdir -p $tmp_dir
     mkdir -p ${PADDLE_ROOT}/build
     cd ${PADDLE_ROOT}/build
     if [ ${WITH_TESTING:-ON} == "ON" ] ; then
@@ -207,8 +208,6 @@ EOF
                                 fi
                             done
 			find $tmp_dir -delete
-			echo 123
-			ls $tmp_dir
                         failed_test_lists=''
                         ctest -R "($retry_unittests_regular)" --output-on-failure -j 4 | tee $tmpfile
                         collect_failed_tests
