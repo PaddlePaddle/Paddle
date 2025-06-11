075fe4cdaeea816a277d5e8f79c5ccfc884d0942
xuexixi <xuexixi@baidu.com>
2025-06-11 19:07:24 +0800

    add dynamic path

--- test/collective/collective_allreduce_api_dygraph.py
+++ test/collective/collective_allreduce_api_dygraph.py
@@ -12,11 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-
-sys.path.append("../legacy_test/")
-
-import test_collective_api_base as test_base
+import legacy_test.test_collective_api_base as test_base
 
 import paddle
 import paddle.distributed as dist
