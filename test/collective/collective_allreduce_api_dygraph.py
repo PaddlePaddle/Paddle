6fbec7a176f255e880760efc575f4bd6a8555aeb
xuexixi <xuexixi@baidu.com>
2025-06-11 10:55:26 +0800

    fix collective test

--- test/collective/collective_allreduce_api_dygraph.py
+++ test/collective/collective_allreduce_api_dygraph.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+
+sys.path.append("../legacy_test/")
+
 import test_collective_api_base as test_base
 
 import paddle
