86500ee674ecba39bd0e4a06e986d7bf257b55e5
co63oc <co63oc@users.noreply.github.com>
2025-06-14 18:21:00 +0800

    Fix

--- test/legacy_test/test_max_min_amax_amin_op.py
+++ test/legacy_test/test_max_min_amax_amin_op.py
@@ -264,5 +264,23 @@ class TestMaxMinAmaxAminAPI7(TestMaxMinAmaxAminAPI):
         _test_dygraph('min')
 
 
+class TestMaxMinAmaxAminAPI_ZeroSize(TestMaxMinAmaxAminAPI):
+    def init_case(self):
+        self.x_np = np.random.randn(1, 0, 10).astype(np.float32)
+        self.shape = [1, 0, 10]
+        self.dtype = 'float32'
+        self.axis = 0
+        self.keepdim = False
+
+
+class TestMaxMinAmaxAminAPI_ZeroSize2(TestMaxMinAmaxAminAPI):
+    def init_case(self):
+        self.x_np = np.random.randn(1, 0, 10).astype(np.float32)
+        self.shape = [1, 0, 10]
+        self.dtype = 'float32'
+        self.axis = -1
+        self.keepdim = True
+
+
 if __name__ == '__main__':
     unittest.main()
