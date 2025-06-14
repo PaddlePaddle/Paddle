0b06184baa30c6d28557265a91235cfa33dcf984
ooooo <3164076421@qq.com>
2025-06-14 23:35:58 +0800

    fix

--- paddle/phi/kernels/funcs/complex_functors.h
+++ paddle/phi/kernels/funcs/complex_functors.h
@@ -377,7 +377,13 @@ struct AngleFunctor<T, phi::funcs::NoComplex<T, dtype::Real<T>>> {
       : input_(input), output_(output), numel_(numel) {}
 
   HOSTDEVICE void operator()(int64_t idx) const {
-    if constexpr (std::is_floating_point_v<T>) {
+    if constexpr (std::is_same_v<T, phi::dtype::bfloat16> ||
+                  std::is_same_v<T, phi::dtype::float16>) {
+      if (phi::dtype::isnan(input_[idx])) {
+        output_[idx] = input_[idx];
+        return;
+      }
+    } else if constexpr (std::is_floating_point_v<T>) {
 #if defined(__CUDACC__) || defined(__HIPCC__)
       if (::isnan(input_[idx])) {
         output_[idx] = input_[idx];
@@ -389,16 +395,7 @@ struct AngleFunctor<T, phi::funcs::NoComplex<T, dtype::Real<T>>> {
         return;
       }
 #endif
-    } else {
-      if constexpr (std::is_same_v<T, phi::dtype::bfloat16> ||
-                    std::is_same_v<T, phi::dtype::float16>) {
-        if (phi::dtype::isnan(input_[idx])) {
-          output_[idx] = input_[idx];
-          return;
-        }
-      }
     }
-
     output_[idx] = input_[idx] < static_cast<T>(0) ? M_PI : 0;
   }
 
