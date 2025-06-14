5a56dca5dbbf799c1b0b3e9320a3f29703cef85b
co63oc <co63oc@users.noreply.github.com>
2025-06-15 06:48:00 +0800

    Fix

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -2869,15 +2869,30 @@ def inner(x: Tensor, y: Tensor, name: str | None = None) -> Tensor:
 
 
     """
+    xshape = x.shape
+    yshape = y.shape
     if in_dynamic_mode() and (x.size == 1 or y.size == 1):
-        return multiply(x, y)
+        if len(xshape) > 1 and len(yshape) > 1:
+            dstshape = list(xshape[:-1]) + list(yshape[:-1])
+            return multiply(x, y).reshape(dstshape)
+        else:
+            return multiply(x, y)
     else:
-        xshape = x.shape
-        yshape = y.shape
         dstshape = list(xshape[:-1]) + list(yshape[:-1])
-
-        nx = x.reshape((-1, xshape[-1]))
-        ny = y.reshape((-1, yshape[-1]))
+        if xshape[-1] == 0:  # If the last dimension is 0
+            if len(xshape) == 1:  # shape is [0]
+                nx = x.reshape((1, 0))
+            else:
+                nx = x.reshape((math.prod(xshape[:-1]), 0))
+        else:
+            nx = x.reshape((-1, xshape[-1]))
+        if yshape[-1] == 0:  # If the last dimension is 0
+            if len(yshape) == 1:  # shape is [0]
+                ny = y.reshape((1, 0))
+            else:
+                ny = y.reshape((math.prod(yshape[:-1]), 0))
+        else:
+            ny = y.reshape((-1, yshape[-1]))
 
         def __check_input(x, y):
             var_names = {'x': x, 'y': y}
@@ -2942,8 +2957,16 @@ def outer(x: Tensor, y: Tensor, name: str | None = None) -> Tensor:
 
 
     """
-    nx = x.reshape((-1, 1))
-    ny = y.reshape((1, -1))
+    xshape = x.shape
+    yshape = y.shape
+    if math.prod(xshape) == 0:  # If the size is 0
+        nx = x.reshape((0, 0))
+    else:
+        nx = x.reshape((-1, 1))
+    if math.prod(yshape) == 0:  # If the size is 0
+        ny = y.reshape((0, 0))
+    else:
+        ny = y.reshape((1, -1))
 
     if in_dynamic_mode():
         return _C_ops.matmul(nx, ny, False, False)
