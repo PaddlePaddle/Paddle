bf54618c579e232133190e5db8890c08ac0dbf31
changeyoung98 <1792266893@qq.com>
2025-06-16 12:33:18 +0000

    fix gpu cond

--- paddle/fluid/pybind/slice_utils.h
+++ paddle/fluid/pybind/slice_utils.h
@@ -630,12 +630,18 @@ static paddle::Tensor dealWithAdvancedIndex(
     transed_tensor = tensor;
   } else {
     *out_is_view = true;
-    if (!single_value || !int_tensor_only ||
-        *pos_of_new_dim == 0) {  // getitem to be removed
-      transed_tensor = transpose_ad_func(tensor, *trans_dim);
-    } else {
+#ifdef PADDLE_WITH_CUDA
+    // Remove the conditions when all cases are supported.
+    // Getitem combine cases to be added.
+    if (tensor.is_gpu() && single_value && int_tensor_only &&
+        *pos_of_new_dim != 0) {
       transed_tensor = tensor;
+    } else {
+      transed_tensor = transpose_ad_func(tensor, *trans_dim);
     }
+#else
+    transed_tensor = transpose_ad_func(tensor, *trans_dim);
+#endif
   }
 
   if (is_for_setitem) {
