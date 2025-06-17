79476b4e61a017b854118938efdbee2ed7ba74a9
wangmingkai02 <wangmingkai02@baidu.com>
2025-06-17 07:51:11 +0000

    Add ENV_SOT_ENABLE_0_SIZE_FALLBACK

--- python/paddle/jit/sot/utils/envs.py
+++ python/paddle/jit/sot/utils/envs.py
@@ -150,6 +150,9 @@ ENV_SOT_FORCE_FALLBACK_SIR_IDS = StringEnvironmentVariable(
     "SOT_FORCE_FALLBACK_SIR_IDS", ""
 )
 ENV_SOT_TRACE_NUMPY = BooleanEnvironmentVariable("ENV_SOT_TRACE_NUMPY", True)
+ENV_SOT_ENABLE_0_SIZE_FALLBACK = BooleanEnvironmentVariable(
+    "SOT_ENABLE_0_SIZE_FALLBACK", False
+)
 
 
 def update_ce_flags():
