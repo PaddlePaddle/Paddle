9a4e49d089f1f63470d16a27cdf822a47ebacc24
co63oc <co63oc@users.noreply.github.com>
2025-06-17 21:28:23 +0800

    Fix

--- test/legacy_test/test_imperative_tensor_clear_gradient.py
+++ test/legacy_test/test_imperative_tensor_clear_gradient.py
@@ -81,9 +81,8 @@ class TestDygraphClearGradient(TestCase):
 
         # actual result
         gradient_actual = linear.weight.grad
-        print(gradient_actual)
         # expected result
-        self.assertTrue(np.empty(gradient_actual))
+        self.assertIsNone(gradient_actual)
 
 
 if __name__ == '__main__':
