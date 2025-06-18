1e69872ad4d2a19221cda0c340e4cf35ecfdf1b1
co63oc <co63oc@users.noreply.github.com>
2025-06-18 14:30:59 +0800

    Fix

--- test/legacy_test/test_bce_loss.py
+++ test/legacy_test/test_bce_loss.py
@@ -336,6 +336,11 @@ class TestBceLossOpStaticFP16(unittest.TestCase):
         paddle.disable_static()
 
 
+class TestBceLossOp_ZeroSize(TestBceLossOp):
+    def init_test_cast(self):
+        self.shape = [0, 1, 2]
+
+
 if __name__ == "__main__":
     paddle.enable_static()
     unittest.main()
