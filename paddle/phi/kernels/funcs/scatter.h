2b74b834c6822078c17475a2ea886f7a45c57f45
chenruibiao <chenruibiao@baidu.com>
2025-06-19 10:43:39 +0800

    Update

--- paddle/phi/kernels/funcs/scatter.h
+++ paddle/phi/kernels/funcs/scatter.h
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #pragma once
+#include <glog/logging.h>
 #include <cstring>
 #include <string>
 #include <unordered_set>
