03b4b95c274b6392ea0546fae1896bfc0c6e869f
SigureMo <sigure.qaq@gmail.com>
2025-06-23 00:23:30 +0800

    update year and remove pragma once in cc file

--- paddle/fluid/eager/to_static/run_program_impl.cc
+++ paddle/fluid/eager/to_static/run_program_impl.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#pragma once
 #include "paddle/fluid/eager/to_static/run_program_impl.h"
 
 #include "paddle/fluid/eager/api/utils/global_utils.h"
