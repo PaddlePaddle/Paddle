2acb7d4ec32a4202e2c1fca5efd2dd2c2ea31be3
chen huangrun <1181749441@qq.com>
2025-06-23 17:09:16 +0000

    fix

--- test/amp/test_tensor_checker.py
+++ test/amp/test_tensor_checker.py
@@ -49,9 +49,7 @@ class TestTensorChecker(unittest.TestCase):
         )
         y = paddle.to_tensor([0, 0, 1], dtype='float32')
         try:
-            print("------------")
             res = paddle.pow(x, y)
-            print(res)
             # test backward
             paddle.autograd.backward([res])
             res = paddle.divide(y, x)
@@ -103,7 +101,7 @@ class TestTensorChecker(unittest.TestCase):
                         f"-- [iter_id={iter_id}, place={place}] num_nan={num_nan}, num_inf={num_inf}"
                     )
                     self.assertEqual(
-                        0,
+                        1,
                         num_nan,
                         f"Expected num_nan to be 1, but received {num_nan}, place={place}.",
                     )
