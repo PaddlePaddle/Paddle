07006b346f002c5c76ad6ed399d908fe17c80213
liuruyan <1823192871@qq.com>
2025-06-23 10:50:48 +0800

    fix ut bug

--- test/legacy_test/test_incubate_int_bincount.py
+++ test/legacy_test/test_incubate_int_bincount.py
@@ -33,13 +33,13 @@ class TestIntBincount(unittest.TestCase):
     def test_empty_input(self):
         x = paddle.to_tensor([], dtype=paddle.int32)
         out = int_bincount(x, low=0, high=10, dtype=paddle.int32)
-        self.assertEqual(out.shape, [11])
+        self.assertEqual(out.shape, [10])
         self.assertEqual(out.sum().item(), 0)
 
     def test_different_dtypes(self):
         x = paddle.to_tensor([1, 3, 5, 3, 1], dtype=paddle.int64)
         out = int_bincount(x, low=1, high=6, dtype=paddle.int64)
-        expected = np.array([2, 0, 2, 0, 1, 0])
+        expected = np.array([2, 0, 2, 0, 1])
         np.testing.assert_array_equal(out.numpy(), expected)
 
 
