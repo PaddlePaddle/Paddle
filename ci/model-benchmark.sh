63ab510ffcffe19cdb0b5752e8d97a0a89e8dea8
gushiwei <gushiwei@baidu.com>
2025-06-25 14:25:08 +0000

    test=document_fix

--- ci/model-benchmark.sh
+++ ci/model-benchmark.sh
@@ -66,8 +66,7 @@ update_summary () {
 wget_paddle () {
     set_python
     cd ${ROOT_DIR}
-    # wget -q --no-proxy https://paddle-github-action.bj.bcebos.com/PR/build/${PR_ID}/${COMMIT_ID}/build.tar.gz --no-check-certificate
-    wget -q --no-proxy https://paddle-github-action.bj.bcebos.com/PR/build/73584/08c0b24a87a07a5dae413adc64dc33b9abfcd4c0/build.tar.gz --no-check-certificate
+    wget -q --no-proxy https://paddle-github-action.bj.bcebos.com/PR/build/${PR_ID}/${COMMIT_ID}/build.tar.gz --no-check-certificate
     tar --use-compress-program="pzstd -1" -xpf build.tar.gz
     cd ${ROOT_DIR}/paddle
     git checkout test
