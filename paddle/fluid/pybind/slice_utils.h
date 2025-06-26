ca483ef845359378891e5b5fb9f23ab86307c563
changeyoung98 <1792266893@qq.com>
2025-06-26 05:02:26 +0000

    slice-check

--- paddle/fluid/pybind/slice_utils.h
+++ paddle/fluid/pybind/slice_utils.h
@@ -607,12 +607,16 @@ static paddle::Tensor dealWithAdvancedIndex(
     std::vector<int>* trans_dim,
     bool* out_is_view) {
   int p = 0;
+  bool int_tensor_only = true;
   for (size_t i = 0; i < advanced_index_dim->size(); ++i) {
     auto index_dim = (*advanced_index_dim)[i];
     if (index_dim != -1) {
       // size of advanced_index is same to number of non -1 element in
       // advanced_index_dim
       auto index = (*advanced_index)[p++];
+      if (index.dtype() == phi::DataType::BOOL) {
+        int_tensor_only = false;
+      }
 
       if (index_dim == 0) {
         // case 1: advanced indices at axis 0, the new dim will be at first.
@@ -651,7 +655,8 @@ static paddle::Tensor dealWithAdvancedIndex(
     *out_is_view = true;
 #ifdef PADDLE_WITH_CUDA
     // Remove the conditions when all cases are supported.
-    if (tensor.is_gpu() && *pos_of_new_dim != 0) {
+    if (tensor.is_gpu() && *pos_of_new_dim != 0 &&
+        (is_for_setitem || int_tensor_only)) {
       transed_tensor = tensor;
     } else {
       transed_tensor = transpose_ad_func(tensor, *trans_dim);
