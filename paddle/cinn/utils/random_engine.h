8639ee9ceccb4604f0a32f141e4fbb917fa8bcb7
ApricityXX <1470600398@qq.com>
2025-06-28 21:17:48 +0800

    628

--- paddle/cinn/utils/random_engine.h
+++ paddle/cinn/utils/random_engine.h
@@ -85,7 +85,7 @@ class LinearRandomEngine {
   // Move the state to the next and return the new state
   result_type Next() {
     *state_ = (increment + (*state_) * multiplier) % modulus;
-    return (result_type)*state_;
+    return (result_type)(*state_);
   }
 
  private:
