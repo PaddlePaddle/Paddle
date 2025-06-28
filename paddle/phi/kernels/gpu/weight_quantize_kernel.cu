fb7f3800bc886cfb027a4563a8fd199c652df205
zhoutianzi666 <17801055074@163.com>
2025-06-28 14:45:20 +0800

    support load int8 weight using wint8

--- paddle/phi/kernels/gpu/weight_quantize_kernel.cu
+++ paddle/phi/kernels/gpu/weight_quantize_kernel.cu
@@ -65,14 +65,26 @@ void WeightQuantizeKernel(const Context& dev_ctx,
                                  algo);
     trans(dev_ctx, quanted_x, out, axis);
   } else if (algo == "weight_only_int8") {
+
     dev_ctx.template Alloc<T>(scale);
-    weight_quant_gpu<T, Context>(dev_ctx,
-                                 x.data<T>(),
-                                 quanted_x.data<int8_t>(),
-                                 scale->data<T>(),
-                                 weight_shape,
-                                 arch,
-                                 algo);
+
+    if (std::is_same<T, int8_t>::value) {
+      // you are loading already quantized weight, so we skip doing quantize.
+      // and just copy!
+      cudaMemcpy(quanted_x.data<int8_t>(), 
+                 x.data<T>(), 
+                 x.numel(), 
+                 cudaMemcpyDeviceToDevice );
+    } else {
+      weight_quant_gpu<T, Context>(dev_ctx,
+                                  x.data<T>(),
+                                  quanted_x.data<int8_t>(),
+                                  scale->data<T>(),
+                                  weight_shape,
+                                  arch,
+                                  algo);
+      }
+
 #ifdef PADDLE_WITH_HIP
     std::vector<int> axis = {1, 0};
     funcs::Transpose<Context, int8_t, 2> trans;
