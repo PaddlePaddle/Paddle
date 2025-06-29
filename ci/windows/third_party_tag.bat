362af05cacf6cf6c5112706c0cae30d528c5a649
gushiwei <gushiwei@baidu.com>
2025-06-29 17:58:12 +0000

    test=document_fix

--- ci/windows/third_party_tag.bat
+++ ci/windows/third_party_tag.bat
@@ -1,28 +1,28 @@
 
-pushd third_party/gloo
+pushd third_party\gloo
 git fetch --tags
 popd
 
-pushd third_party/protobuf
+pushd third_party\protobuf
 git fetch --tags
 popd
 
-pushd third_party/gtest
+pushd third_party\gtest
 git fetch --tags
 popd
 
-pushd third_party/pocketfft
+pushd third_party\pocketfft
 git fetch --tags
 popd
 
-pushd third_party/pybind
+pushd third_party\pybind
 git fetch --tags
 popd
 
-pushd third_party/brpc
+pushd third_party\brpc
 git fetch --tags
 popd
 
-pushd third_party/rocksdb
+pushd third_party\rocksdb
 git fetch origin 6.19.fb
 popd
