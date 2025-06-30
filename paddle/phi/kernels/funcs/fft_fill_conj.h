04003cd4d648dc8b6286390687ceb6caaf56a913
co63oc <co63oc@users.noreply.github.com>
2025-06-30 12:52:22 +0800

    Fix

--- paddle/phi/kernels/funcs/fft_fill_conj.h
+++ paddle/phi/kernels/funcs/fft_fill_conj.h
@@ -137,7 +137,7 @@ struct FFTFillConjFunctor {
 };
 
 template <typename DeviceContext, typename C>
-void FFTFillConj(const DeviceContext& ctx,
+void FFTFillConj(const DeviceContext& dev_ctx,
                  const DenseTensor* src,
                  DenseTensor* dst,
                  const std::vector<int64_t>& axes) {
@@ -172,7 +172,7 @@ void FFTFillConj(const DeviceContext& ctx,
   const auto dst_shape = dst_shape_v.data();
   const auto p_is_fft_axis = _is_fft_axis.get();
 #endif
-  ForRange<DeviceContext> for_range(ctx, dst->numel());
+  ForRange<DeviceContext> for_range(dev_ctx, dst->numel());
   FFTFillConjFunctor<C> fill_conj_functor(src_data,
                                           dst_data,
                                           src_strides,
