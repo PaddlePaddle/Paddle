e3291f2b245f8ec6644e856778789fd21b47fb6c
gushiwei <gushiwei@baidu.com>
2025-06-30 18:38:03 +0000

    win-gpu test=document_fix

--- ci/windows/config_env.bat
+++ ci/windows/config_env.bat
@@ -24,3 +24,18 @@ powershell -Command "Stop-Process -Name 'rc' -Force 2>$null"
 powershell -Command "Stop-Process -Name 'cl' -Force 2>$null"
 powershell -Command "Stop-Process -Name 'lib' -Force 2>$null"
 powershell -Command "Stop-Process -Name 'python' -Force 2>$null"
+
+pip config set global.trusted-host pypi.org
+pip config set global.trusted-host files.pythonhosted.org
+pip config set global.trusted-host pypi.python.org
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip config set install.trusted-host pypi.tuna.tsinghua.edu.cn
+git config --global core.longpaths true
+git config --global user.name "PaddleCI"
+git config --global user.email "paddle_ci@example.com"
+
+git remote add upstream https://github.com/PaddlePaddle/Paddle.git
+
+git --no-pager pull upstream %BRANCH% --no-edit
+if %errorlevel% NEQ 0 exit /b 1
+if exist .git\index.lock del .git\index.lock 2>NUL
