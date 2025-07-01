dbba78b553351a68c16a81d696d80a5e28828fec
changeyoung98 <1792266893@qq.com>
2025-07-01 03:04:25 +0000

    slice-check

--- paddle/fluid/pybind/eager_method.cc
+++ paddle/fluid/pybind/eager_method.cc
@@ -2099,7 +2099,7 @@ static PyObject* tensor__setitem_dygraph(TensorObject* self,
             mesh, self->tensor, transed_sub_tensor, value_tensor);
       }
       paddle::Tensor mask_tensor;
-      if (!out_is_view &&
+      if (pos_of_new_dim == 0 &&
           MaskedFillDispatching(
               transed_sub_tensor, transed_index, &mask_tensor, &value_tensor)) {
         transed_sub_tensor =
