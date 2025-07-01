f404de9fee029df97eee800cb3eb75d4e1d2b6e0
gushiwei <gushiwei@baidu.com>
2025-07-01 09:09:16 +0000

    test=document

--- ci/windows/config_env.bat
+++ ci/windows/config_env.bat
@@ -1,29 +1,12 @@
-taskkill /f /im cmake.exe /t 2>NUL
-taskkill /f /im ninja.exe /t 2>NUL
-taskkill /f /im MSBuild.exe /t 2>NUL
-taskkill /f /im cl.exe /t 2>NUL
-taskkill /f /im lib.exe /t 2>NUL
-taskkill /f /im link.exe /t 2>NUL
-taskkill /f /im vctip.exe /t 2>NUL
-taskkill /f /im cvtres.exe /t 2>NUL
-taskkill /f /im rc.exe /t 2>NUL
-taskkill /f /im mspdbsrv.exe /t 2>NUL
-taskkill /f /im csc.exe /t 2>NUL
-taskkill /f /im python.exe /t 2>NUL
-taskkill /f /im nvcc.exe /t 2>NUL
-taskkill /f /im cicc.exe /t 2>NUL
-taskkill /f /im ptxas.exe /t 2>NUL
-taskkill /f /im op_function_generator.exe /t 2>NUL
-taskkill /f /im busybox64.exe /t 2>NUL
-taskkill /f /im eager_generator.exe /t 2>NUL
-taskkill /f /im eager_legacy_op_function_generator.exe /t 2>NUL
-powershell -Command "Stop-Process -Name 'eager_generator' -Force 2>$null"
-powershell -Command "Stop-Process -Name 'eager_legacy_op_function_generator' -Force 2>$null"
-powershell -Command "Stop-Process -Name 'cvtres' -Force 2>$null"
-powershell -Command "Stop-Process -Name 'rc' -Force 2>$null"
-powershell -Command "Stop-Process -Name 'cl' -Force 2>$null"
-powershell -Command "Stop-Process -Name 'lib' -Force 2>$null"
-powershell -Command "Stop-Process -Name 'python' -Force 2>$null"
+set work_dir=%cd%
+if not defined cache_dir set cache_dir=%work_dir:Paddle=cache%
+if not exist %cache_dir%\tools (
+    cd /d %cache_dir%
+    python -m pip install wget
+    python -c "import wget;wget.download('https://paddle-ci.gz.bcebos.com/window_requirement/tools.zip')"
+    tar xf tools.zip
+    cd /d %work_dir%
+)
 
 pip config set global.trusted-host pypi.org
 pip config set global.trusted-host files.pythonhosted.org
