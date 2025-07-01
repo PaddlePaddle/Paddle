5cbaa9dda347f17134cdf388b4b70d354f0edaf8
chenyuyan <2903319587@qq.com>
2025-07-01 14:36:56 +0800

    fix static check

--- python/paddle/distributed/fleet/base/topology.py
+++ python/paddle/distributed/fleet/base/topology.py
@@ -29,7 +29,6 @@ from paddle.distributed.utils.nccl_utils import check_nccl_version_for_p2p
 from ..utils.log_util import logger
 
 if TYPE_CHECKING:
-
     from paddle.base.libpaddle import NCCLConfig
     from paddle.distributed.collective import Group
 
@@ -92,8 +91,9 @@ def create_nccl_config(
             >>> # doctest: +REQUIRES(env: DISTRIBUTED)
             >>> import paddle
             >>> import paddle.distributed as dist
+            >>> from typing import Union
             >>> dist.init_parallel_env()
-            >>> nccl_config: dict[str, int | str] = {"commName":"tp_comm","ll_buffsize":0,"ll128_buffsize":0,"simple_buffsize":1024,"buffsize_align":1024,"nchannels":4,"algoStr":"Ring","protoStr":"Simple",}
+            >>> nccl_config: dict[str, Union[int, str]] = {"commName":"tp_comm","ll_buffsize":0,"ll128_buffsize":0,"simple_buffsize":1024,"buffsize_align":1024,"nchannels":4,"algoStr":"Ring","protoStr":"Simple",}
             >>> ranks=[0,1,2,3,4,5,6,7]
             >>> nccl_config=dist.create_nccl_config(nccl_config)
             >>> pg=dist.new_group(ranks, nccl_config=nccl_config)
