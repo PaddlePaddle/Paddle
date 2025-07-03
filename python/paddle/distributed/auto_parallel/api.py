c06c077be102b63b1796da49873c96269f7b2ac0
ZhenxingLi <lizhenxing02@baidu.com>
2025-07-03 10:28:00 +0800

    Update api.py

--- python/paddle/distributed/auto_parallel/api.py
+++ python/paddle/distributed/auto_parallel/api.py
@@ -1939,14 +1939,14 @@ class _ShardOptimizer(Optimizer):
     def _apply_optimize(
         self, loss, startup_program, params_grads, param_group_idx=0
     ):
-        if self.enable_tensor_fusion:
-            # tensor fusion fuse params/grads into large chunks, no need _fused_comm_before_apply_optimize.
-            params_grads = self._tensor_fusion(params_grads)
-        else:
-            # Fuse the communication of gradients prior to the optimization operation in the dynamic mode.
-            if paddle.in_dynamic_mode() and isinstance(
-                self._shard_fn, ShardingStage1
-            ):
+        if paddle.in_dynamic_mode() and isinstance(
+            self._shard_fn, ShardingStage1
+        ):
+            if self.enable_tensor_fusion:
+                # tensor fusion fuse params/grads into large chunks, no need _fused_comm_before_apply_optimize.
+                params_grads = self._tensor_fusion(params_grads)
+            else:
+                # Fuse the communication of gradients prior to the optimization operation in the dynamic mode.
                 # Get fuse optimization flag.
                 def get_env(flag_name):
                     if os.getenv(flag_name) in ['True', 'true', '1']:
