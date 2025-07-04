324f8d77fa704c5cf37f61f2d763b82812821059
gushiwei <gushiwei@baidu.com>
2025-07-04 07:45:07 +0000

    test=document_fix build

--- ci/windows/clean_env.bat
+++ ci/windows/clean_env.bat
@@ -1,7 +1,4 @@
 @ECHO ON
-echo ----------------------------------------------------------------------
-dir Paddle\third_party\openvino
-echo ----------------------------------------------------------------------
 taskkill /f /im cmake.exe /t 2>NUL
 taskkill /f /im ninja.exe /t 2>NUL
 taskkill /f /im MSBuild.exe /t 2>NUL
@@ -10,9 +7,6 @@ taskkill /f /im lib.exe /t 2>NUL
 taskkill /f /im link.exe /t 2>NUL
 taskkill /f /im vctip.exe /t 2>NUL
 taskkill /f /im cvtres.exe /t 2>NUL
-echo ----------------------------------------------------------------------
-dir Paddle\third_party\openvino
-echo ----------------------------------------------------------------------
 taskkill /f /im rc.exe /t 2>NUL
 taskkill /f /im mspdbsrv.exe /t 2>NUL
 taskkill /f /im csc.exe /t 2>NUL
@@ -20,16 +14,10 @@ taskkill /f /im python.exe /t 2>NUL
 taskkill /f /im nvcc.exe /t 2>NUL
 taskkill /f /im cicc.exe /t 2>NUL
 taskkill /f /im ptxas.exe /t 2>NUL
-echo ----------------------------------------------------------------------
-dir Paddle\third_party\openvino
-echo ----------------------------------------------------------------------
 taskkill /f /im op_function_generator.exe /t 2>NUL
 taskkill /f /im busybox64.exe /t 2>NUL
 taskkill /f /im eager_generator.exe /t 2>NUL
 taskkill /f /im eager_legacy_op_function_generator.exe /t 2>NUL
-echo ----------------------------------------------------------------------
-dir Paddle\third_party\openvino
-echo ----------------------------------------------------------------------
 powershell -Command "Stop-Process -Name 'eager_generator' -Force 2>$null"
 powershell -Command "Stop-Process -Name 'eager_legacy_op_function_generator' -Force 2>$null"
 powershell -Command "Stop-Process -Name 'cvtres' -Force 2>$null"
@@ -37,6 +25,3 @@ powershell -Command "Stop-Process -Name 'rc' -Force 2>$null"
 powershell -Command "Stop-Process -Name 'cl' -Force 2>$null"
 powershell -Command "Stop-Process -Name 'lib' -Force 2>$null"
 powershell -Command "Stop-Process -Name 'python' -Force 2>$null" || ver >nul
-echo ----------------------------------------------------------------------
-dir Paddle\third_party\openvino
-echo ----------------------------------------------------------------------
