7e8e499cf9ad6bb9bf8745bc6c798ed012c473ee
co63oc <co63oc@users.noreply.github.com>
2025-07-04 14:38:51 +0800

    Fix

--- paddle/phi/kernels/gpu/expand_as_grad_kernel.cu
+++ paddle/phi/kernels/gpu/expand_as_grad_kernel.cu
@@ -23,7 +23,7 @@
 namespace phi {
 
 template <typename T, typename Context>
-void ExpandAsGradKernel(const Context& context,
+void ExpandAsGradKernel(const Context& dev_ctx,
                         const DenseTensor& x,
                         const DenseTensor& out_grad,
                         const std::vector<int64_t>& target_shape,
@@ -41,14 +41,14 @@ void ExpandAsGradKernel(const Context& context,
                               "to 6, but the value received is %d.",
                               out_rank));
 
-  context.template Alloc<T>(in_grad);
+  dev_ctx.template Alloc<T>(in_grad);
   if (in_dims == out_dims) {
-    phi::Copy(context, out_grad, context.GetPlace(), false, in_grad);
+    phi::Copy(dev_ctx, out_grad, dev_ctx.GetPlace(), false, in_grad);
   } else {
     std::vector<int> reduce_dims = funcs::GetReduceDim(in_dims, out_dims, -1);
 
     phi::SumKernel<T, Context>(
-        context, out_grad, reduce_dims, out_grad.dtype(), false, in_grad);
+        dev_ctx, out_grad, reduce_dims, out_grad.dtype(), false, in_grad);
   }
 }
 
