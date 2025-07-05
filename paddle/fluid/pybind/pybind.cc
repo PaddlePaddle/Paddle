723476cf374f03689368963703ccd3f9d083007f
zeroRains <linjunlu@zerorains.top>
2025-07-05 15:43:08 +0800

    fix

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -34,7 +34,6 @@ limitations under the License. */
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <winbase.h>
 #include <windows.h>
 #include <winternl.h>
 #include <codecvt>
