cdfe9e0982fea525851161784f5404a6d192ed48
Pan Zhaowu <panzhaowu@baidu.com>
2025-07-06 16:06:30 +0800

    test docfix

--- python/paddle/nn/functional/moe_unpermute.py
+++ python/paddle/nn/functional/moe_unpermute.py
@@ -58,10 +58,10 @@ def moe_unpermute(
     Examples:
         .. code-block:: python
 
+            >>> # doctest: +REQUIRES(env:GPU)
             >>> # doctest: +SKIP('This is only support in cuda 12.0+')
             >>> import paddle
             >>> import paddle.nn.functional as F
-            >>> paddle.set_device('gpu')
             >>> hidden_states = paddle.randn([3, 128], dtype='bfloat16')
             >>> expert_routemap_topk = paddle.to_tensor([[-1, 0, -1, -1, 2, -1, -1, -1],
             ...                                          [1, -1, -1, -1, -1, -1, -1, -1],
