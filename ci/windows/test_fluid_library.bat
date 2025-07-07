35ce354312a5dd5e33416d857b3109a1cd8377b5
gushiwei <gushiwei@baidu.com>
2025-07-07 15:56:39 +0000

    test=document_fix

--- ci/windows/test_fluid_library.bat
+++ ci/windows/test_fluid_library.bat
@@ -1,21 +1,31 @@
 setlocal enabledelayedexpansion
 
-call :test_inference || goto test_inference_ut_error
+call :test_inference || goto test_inference_error
 goto :eof
 
 :test_inference
+@ECHO ON
 echo    ========================================
-echo    Step 7. Testing fluid library with infer_ut for inference ...
+echo    Step 5. Testing fluid library for inference ...
 echo    ========================================
 
 cd %BUILD_DIR%
-call "%PYTHON_VENV_ROOT%\Scripts\activate.bat"
-cd /d %work_dir%\test\cpp\inference\infer_ut
-%cache_dir%\tools\busybox64.exe bash run.sh %work_dir:\=/% %WITH_MKL% %WITH_GPU% %cache_dir:\=/%/inference_demo %TENSORRT_ROOT% %WITH_ONNXRUNTIME% %MSVC_STATIC_CRT% "%CUDA_TOOLKIT_ROOT_DIR%"
+tree /F %cd%\paddle_inference_install_dir\paddle
+%cache_dir%\tools\busybox64.exe du -h -d 0 %cd%\paddle_inference_install_dir > lib_size.txt
+type lib_size.txt
+set /p libsize=< lib_size.txt
+for /F %%i in ("%libsize%") do echo "Windows Paddle_Inference Size: !libsize_m!M"
+for /F %%i in ("%libsize%") do echo ipipe_log_param_Windows_Paddle_Inference_Size: !libsize_m!M
+
+cd /d %work_dir%\paddle\fluid\inference\api\demo_ci
+%cache_dir%\tools\busybox64.exe bash run.sh %work_dir:\=/% %WITH_MKL% %WITH_GPU% %cache_dir:\=/%/inference_demo %WITH_TENSORRT% %TENSORRT_ROOT% %WITH_ONNXRUNTIME% %MSVC_STATIC_CRT% "%CUDA_TOOLKIT_ROOT_DIR%"
+
 goto:eof
 
-:test_inference_ut_error
+:test_inference_error
 ::echo 1 > %cache_dir%\error_code.txt
 ::type %cache_dir%\error_code.txt
-echo Testing fluid library with infer_ut for inference failed!
+echo    ==========================================
+echo    Testing inference library failed!
+echo    ==========================================
 exit /b 1
