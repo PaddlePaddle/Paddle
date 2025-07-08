0539ec91e2dc6f6114a49adff314744cffac93dd
gushiwei <gushiwei@baidu.com>
2025-07-08 14:58:55 +0000

    test=document_fix

--- ci/windows/build.bat
+++ ci/windows/build.bat
@@ -58,8 +58,8 @@ rem set vs language to english to block showIncludes, this need vs has installed
 set VSLANG=1033
 rem Configure the environment for 64-bit builds. 'DISTUTILS_USE_SDK' indicates that the user has selected the compiler.
 if not defined vcvars64_dir (
-    set "vcvars64_dir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-    echo vcvars64_dir=%vcvars64_dir%>> %GITHUB_ENV%
+    set "vcvars64_dir=C:\Program Files (x86^)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+    echo vcvars64_dir=!vcvars64_dir!>> %GITHUB_ENV%
 )
 echo %vcvars64_dir%
 call %vcvars64_dir%
