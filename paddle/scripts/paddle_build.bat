e4aabd83f0738a38883c84cba02048c826661fb5
gushiwei <gushiwei@baidu.com>
2025-07-08 17:10:47 +0000

    test=document_fix

--- paddle/scripts/paddle_build.bat
+++ paddle/scripts/paddle_build.bat
@@ -211,11 +211,11 @@ if "%WITH_SCCACHE%"=="ON" (
     set SCCACHE_ERROR_LOG=%SCCACHE_ROOT%\sccache_log.txt
     set SCCACHE_LOG=quiet
 
-    :: Distributed storage on windows
-    set SCCACHE_ENDPOINT=s3.bj.bcebos.com
-    set SCCACHE_BUCKET=paddle-windows
-    set SCCACHE_S3_KEY_PREFIX=sccache/
-    set SCCACHE_S3_USE_SSL=true
+    @REM :: Distributed storage on windows
+    @REM set SCCACHE_ENDPOINT=s3.bj.bcebos.com
+    @REM set SCCACHE_BUCKET=paddle-windows
+    @REM set SCCACHE_S3_KEY_PREFIX=sccache/
+    @REM set SCCACHE_S3_USE_SSL=true
 
     sccache --start-server
     sccache -z
