6105abdf8c949c4f7b35380a1bd429aec48681cc
Liang Shuhao <liangshuhao@baidu.com>
2025-07-22 11:18:19 +0000

    Optimize fused_transpose_split_quant kernel

--- paddle/phi/kernels/fusion/gpu/fused_transpose_split_quant_kernel.cu
+++ paddle/phi/kernels/fusion/gpu/fused_transpose_split_quant_kernel.cu
@@ -30,88 +30,89 @@ struct __align__(sizeof(T) * VecSize) VecType {
 };
 
 template <int VecSize>
-__device__ void BlockLoad(const phi::bfloat16* X,
-                          __nv_bfloat16 input[4][4],
+__device__ void BlockLoad(const phi::bfloat16* input,
+                          __nv_bfloat16 x[8][4],
                           size_t K) {
-  for (size_t i = 0; i < 4; i++) {
-    size_t off_m = blockIdx.x * 128 + threadIdx.y + i * 32;
+  for (uint32_t i = 0; i < 8; i++) {
+    size_t off_m = blockIdx.x * size_t(128) + threadIdx.y + i * 16;
     size_t off_k = blockIdx.y * 128 + threadIdx.x * VecSize;
     size_t offset = off_m * K + off_k;
 
-    for (size_t j = 0; j < 4; j += VecSize) {
+    for (uint32_t j = 0; j < 4; j += VecSize) {
       if (off_k + j * 32 < K) {
         size_t idx = offset + j * 32;
         using LoadT = VecType<__nv_bfloat16, VecSize>;
-        LoadT data = *reinterpret_cast<const LoadT*>(X + idx);
-        for (int k = 0; k < VecSize; k++) {
-          input[i][j + k] = data[k];
+        LoadT data = *reinterpret_cast<const LoadT*>(input + idx);
+        for (uint32_t k = 0; k < VecSize; k++) {
+          x[i][j + k] = data[k];
         }
       }
     }
   }
 }
 
-__device__ void BlockColumnMax(const __nv_bfloat16 input[4][4],
-                               __nv_bfloat16 amax[4],
-                               __nv_bfloat16* shm) {
-  // Reduce [(4), 32, 32, 4] => [32, 32, 4]
+template <bool Pow2Scales>
+__device__ void BlockColumnScale(const __nv_bfloat16 x[8][4],
+                                 float col_scale[128],
+                                 __nv_bfloat16* shm) {
+  // reduce [(8), 16, 32, 4] => [16, 32, 4]
   __nv_bfloat16 warp_max[4];
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      __nv_bfloat16 t = BF16_ABS(input[i][j]);
+  for (uint32_t i = 0; i < 8; i++) {
+    for (uint32_t j = 0; j < 4; j++) {
+      __nv_bfloat16 t = BF16_ABS(x[i][j]);
       warp_max[j] = i == 0 ? t : BF16_MAX(warp_max[j], t);
     }
   }
 
-  // Reduce [(32), 32, 4] => [32, 4]
-  for (int i = 0; i < 4; i++) {
-    shm[threadIdx.y * 128 + i * 32 + threadIdx.x] = warp_max[i];
+  // reduce [(16), 32, 4] => [8, 32, 4]
+  if (threadIdx.y >= 8) {
+    for (uint32_t j = 0; j < 4; j++) {
+      shm[(threadIdx.y - 8) * 128 + threadIdx.x + j * 32] = warp_max[j];
+    }
   }
   __syncthreads();
-  for (int offset = 16; offset > 0; offset /= 2) {
+
+  // reduce [(8), 32, 4] => [32, 4]
+  for (uint32_t offset = 8; offset > 0; offset /= 2) {
     if (threadIdx.y < offset) {
-      for (int i = 0; i < 4; i++) {
-        shm[threadIdx.y * 128 + i * 32 + threadIdx.x] =
-            BF16_MAX(shm[threadIdx.y * 128 + i * 32 + threadIdx.x],
-                     shm[(threadIdx.y + offset) * 128 + i * 32 + threadIdx.x]);
+      for (uint32_t j = 0; j < 4; j++) {
+        __nv_bfloat16 other =
+            offset == 8
+                ? warp_max[j]
+                : shm[(threadIdx.y + offset) * 128 + threadIdx.x + j * 32];
+        __nv_bfloat16 next_val =
+            BF16_MAX(shm[threadIdx.y * 128 + threadIdx.x + j * 32], other);
+        if (offset > 1) {
+          shm[threadIdx.y * 128 + threadIdx.x + j * 32] = next_val;
+        } else {
+          col_scale[threadIdx.x + j * 32] =
+              ComputeScale<__nv_bfloat16, __nv_fp8_e4m3, Pow2Scales>(
+                  static_cast<float>(next_val), 0.0f);
+        }
       }
     }
     __syncthreads();
   }
-
-  for (int i = 0; i < 4; i++) {
-    amax[i] = shm[i * 32 + threadIdx.x];
-  }
-  __syncthreads();
 }
 
-template <typename OutT, bool Pow2Scales, int VecSize>
+template <typename OutT, int VecSize>
 __device__ void BlockStoreScale(float* scale,
                                 size_t off_m,
-                                __nv_bfloat16 amax[4],
-                                float scale_inv[4],
+                                float col_scale[128],
                                 size_t K) {
-  float scale_out[4];
-  for (int i = 0; i < 4; i++) {
-    scale_inv[i] = ComputeScale<__nv_bfloat16, OutT, Pow2Scales>(
-        static_cast<float>(amax[i]), 0.0f);
-    scale_out[i] = __frcp_rn(scale_inv[i]);
-  }
-  if (threadIdx.y == 0) {
-    size_t idx_m = blockIdx.x - off_m / 128;
-    size_t off_k = blockIdx.y * 128 + threadIdx.x * VecSize;
-    size_t offset = idx_m * K + off_k;
-
-    for (size_t j = 0; j < 4; j += VecSize) {
-      if (off_k + j * 32 < K) {
-        size_t idx = offset + j * 32;
-        using StoreT = VecType<float, VecSize>;
-        StoreT data;
-        for (int k = 0; k < VecSize; k++) {
-          data[k] = scale_out[j + k];
-        }
-        *reinterpret_cast<StoreT*>(scale + idx) = data;
-      }
+  if (threadIdx.y < 4) {
+    uint32_t off = threadIdx.y * 32 + threadIdx.x;
+    if constexpr (VecSize == 4) {
+      off = (off % 4) * 32 + off / 4;
+    } else if constexpr (VecSize == 2) {
+      off = (off / 64) * 64 + (off % 2) * 32 + (off % 64) / 2;
+    }
+    float scale_out = 1.0f / col_scale[off];
+    size_t idx_y = blockIdx.x - off_m / 128;
+    size_t idx_x = blockIdx.y * 128 + threadIdx.y * 32 + threadIdx.x;
+    size_t idx = idx_y * K + idx_x;
+    if (idx_x < K) {
+      scale[idx] = scale_out;
     }
   }
 }
@@ -122,28 +123,41 @@ __device__ void BlockStoreOut(OutT* out,
                               size_t cur_tokens,
                               const OutT shm[128][129],
                               size_t K) {
-  for (size_t i = 0; i < 4; i++) {
-    size_t idx_m = blockIdx.x * 128 + threadIdx.x * 4;
-    size_t idx_k = blockIdx.y * 128 + threadIdx.y + i * 32;
+  for (uint32_t i = 0; i < 8; i++) {
+    size_t idx_m = blockIdx.x * size_t(128) + threadIdx.x * 4;
+    size_t idx_k = blockIdx.y * 128 + threadIdx.y + i * 16;
     size_t idx = idx_k * cur_tokens + (idx_m - off_m);
 
     if (idx_k < K) {
       using StoreT = VecType<OutT, VecSize>;
       StoreT data;
-      for (int j = 0; j < VecSize; j++) {
-        data[j] = shm[i * 32 + threadIdx.y][threadIdx.x * 4 + j];
+      for (uint32_t j = 0; j < VecSize; j++) {
+        data[j] = shm[i * 16 + threadIdx.y][threadIdx.x * 4 + j];
       }
       *reinterpret_cast<StoreT*>(out + idx) = data;
     }
   }
 }
 
-__device__ std::pair<size_t, size_t> GetExpertIdx(int64_t* tokens_per_expert,
-                                                  size_t num_experts) {
-  __shared__ size_t expert_idx_, off_m_;
+template <typename OutT, bool Pow2Scales, int VecSize>
+__global__ void __launch_bounds__(512)
+    FusedTransposeSplitQuantKernel(const phi::bfloat16* __restrict__ input,
+                                   int64_t* __restrict__ meta,
+                                   size_t num_experts,
+                                   size_t K) {
+  __shared__ OutT shm[128][129];
+  int64_t* tokens_per_expert = meta;
+  OutT** out_ptrs = reinterpret_cast<OutT**>(meta + num_experts);
+  float** scale_ptrs = reinterpret_cast<float**>(meta + num_experts * 2);
 
+  // 1. Load 128x128 elements from input
+  __nv_bfloat16 x[8][4];
+  BlockLoad<VecSize>(input, x, K);
+
+  // 2. Get expert index and offset of the current block
+  __shared__ size_t expert_info[2];
   if (threadIdx.x == 0 && threadIdx.y == 0) {
-    size_t idx_m = blockIdx.x * 128;
+    size_t idx_m = blockIdx.x * size_t(128);
     size_t off_m = 0, next_off_m = 0;
     size_t expert_idx;
     for (expert_idx = 0; expert_idx < num_experts; expert_idx++) {
@@ -153,57 +167,34 @@ __device__ std::pair<size_t, size_t> GetExpertIdx(int64_t* tokens_per_expert,
       }
       off_m = next_off_m;
     }
-    expert_idx_ = expert_idx;
-    off_m_ = off_m;
+    expert_info[0] = expert_idx;
+    expert_info[1] = off_m;
   }
-  __syncthreads();
 
-  return {expert_idx_, off_m_};
-}
+  // 3. Calculate scale along the column
+  __shared__ float col_scale[128];
+  BlockColumnScale<Pow2Scales>(
+      x, col_scale, reinterpret_cast<__nv_bfloat16*>(shm));
 
-template <typename OutT, bool Pow2Scales, int VecSize>
-__global__ void __launch_bounds__(1024)
-    FusedTransposeSplitQuantKernel(const phi::bfloat16* __restrict__ X,
-                                   int64_t* __restrict__ meta,
-                                   size_t num_experts,
-                                   size_t K) {
-  __shared__ OutT shm[128][129];
-  int64_t* tokens_per_expert = meta;
-  OutT** out_ptrs = reinterpret_cast<OutT**>(meta + num_experts);
-  float** scale_ptrs = reinterpret_cast<float**>(meta + num_experts * 2);
+  // 4. Store scale
+  const size_t expert_idx = expert_info[0];
+  const size_t off_m = expert_info[1];
+  BlockStoreScale<OutT, VecSize>(scale_ptrs[expert_idx], off_m, col_scale, K);
 
-  // Get expert_idx and offset at the M dim of the current block
-  auto expert_info = GetExpertIdx(tokens_per_expert, num_experts);
-  size_t expert_idx = expert_info.first;
-  size_t off_m = expert_info.second;
-
-  // Load 128x128 elements from X
-  __nv_bfloat16 input[4][4];
-  BlockLoad<VecSize>(X, input, K);
-
-  // Find the maximum of each 128 elements on the M axis
-  __nv_bfloat16 amax[4];
-  BlockColumnMax(input, amax, reinterpret_cast<__nv_bfloat16*>(shm));
-
-  // Compute scale and scale_inv, then store scale back
-  float scale_inv[4];
-  BlockStoreScale<OutT, Pow2Scales, VecSize>(
-      scale_ptrs[expert_idx], off_m, amax, scale_inv, K);
-
-  // Scale X and save into shared memory with transposed layout
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j += VecSize) {
-      for (int k = 0; k < VecSize; k++) {
-        float input_fp32 = static_cast<float>(input[i][j + k]);
-        float output_scaled = input_fp32 * scale_inv[j + k];
-        shm[threadIdx.x * VecSize + j * 32 + k][i * 32 + threadIdx.y] =
-            static_cast<OutT>(output_scaled);
+  // 5. Scale x and save into shared memory with transposed layout
+  for (uint32_t i = 0; i < 8; i++) {
+    for (uint32_t j = 0; j < 4; j += VecSize) {
+      for (uint32_t k = 0; k < VecSize; k++) {
+        float x_fp32 = static_cast<float>(x[i][j + k]);
+        float x_scaled = x_fp32 * col_scale[threadIdx.x + (j + k) * 32];
+        shm[threadIdx.x * VecSize + j * 32 + k][i * 16 + threadIdx.y] =
+            static_cast<OutT>(x_scaled);
       }
     }
   }
   __syncthreads();
 
-  // Store 128x128 elements back
+  // 6. Store 128x128 elements back
   // Note: out is always 4x vectorizable.
   BlockStoreOut<OutT, 4>(
       out_ptrs[expert_idx], off_m, tokens_per_expert[expert_idx], shm, K);
@@ -265,7 +256,7 @@ void FusedTransposeSplitQuantKernel(
   auto stream = dev_ctx.stream();
 
   dim3 grid(M / 128, (K + 127) / 128);
-  dim3 block(32, 32);
+  dim3 block(32, 16);
 
 #define LAUNCH_KERNEL(POW_2_SCALES, VEC_SIZE)                      \
   FusedTransposeSplitQuantKernel<phi::dtype::float8_e4m3fn,        \
