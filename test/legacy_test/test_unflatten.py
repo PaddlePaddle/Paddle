6b65f6d5c906be4f3c9bb75152e82e134c350b0a
luyl975 <abc165276@gmail.com>
2025-07-28 14:11:31 +0800

    change unit

--- test/legacy_test/test_unflatten.py
+++ test/legacy_test/test_unflatten.py
@@ -142,14 +142,6 @@ class TestUnflattenInputZeroSize(TestUnflattenAPI):
         self.shape_is_tensor = False
 
 
-class TestUnflattenInputZeroSize2(TestUnflattenAPI):
-    def set_args(self):
-        self.x = np.random.rand(4, 0, 16).astype('int16')
-        self.axis = 0
-        self.shape = (-1, 2)
-        self.shape_is_tensor = False
-
-
 class TestUnflattenInputZeroSizeError(unittest.TestCase):
     def test_errors(self):
         paddle.disable_static()
