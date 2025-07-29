2c872526913c8152d6967a8f173a17d0b757a9b8
SigureMo <sigure.qaq@gmail.com>
2025-07-29 13:03:46 +0800

    include only when avaliable

--- paddle/phi/core/cuda_stream.cc
+++ paddle/phi/core/cuda_stream.cc
@@ -12,7 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
 #include "paddle/phi/core/cuda_stream.h"
+#endif
 
 #include "glog/logging.h"
 
