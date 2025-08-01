72c335beaeab57c1d4ef1bd6780227513638542d
Yutian Rao <72854228+raoyutian@users.noreply.github.com>
2025-08-01 10:17:36 +0800

    fix GetInputTypes() & GetOutputTypes() when using .json model

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -2505,7 +2505,7 @@ AnalysisPredictor::GetInputTypes() {
       auto tensor= GetInputTensor(name);
       input_type[name] = tensor->type();
     }
-  }else{
+  } else {
     for (const auto &name : names) {
       auto *var = inference_program_->Block(0).FindVar(name);
       PADDLE_ENFORCE_NOT_NULL(
@@ -2574,7 +2574,7 @@ AnalysisPredictor::GetOutputTypes() {
       auto tensor = GetOutputTensor(name);
       output_type[name] = tensor->type();
     }
-  }else{
+  } else {
     for (const auto &name : names) {
       auto *var = inference_program_->Block(0).FindVar(name);
       PADDLE_ENFORCE_NOT_NULL(
