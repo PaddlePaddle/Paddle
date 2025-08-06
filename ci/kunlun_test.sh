7367f5dc77f422a39ecc116a9f3afd6590ca2552
tianshuo78520a <707759223@qq.com>
2025-08-06 03:19:12 +0000

    fix xpu

--- ci/kunlun_test.sh
+++ ci/kunlun_test.sh
@@ -161,7 +161,7 @@ set +x
             pip install -e .
 
             #install paddle x dependency
-            paddlex --install PaddleClas
+            paddlex --install PaddleClas -y
 
             #download paddle dataset
             wget -q https://paddle-model-ecology.bj.bcebos.com/paddlex/data/cls_flowers_examples.tar -P ./dataset
