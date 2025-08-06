61e6768ba3011080377c995d698566b5615cfd72
Yi Liu <liuyi39@baidu.com>
2025-08-06 12:15:36 +0000

    fix whitespace

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -949,7 +949,7 @@ def to_tensor(
             [[0.10000000, 0.20000000],
              [0.30000001, 0.40000001]])
 
-             >>> paddle.to_tensor([[0.1, 0.2], [0.3, 0.4]], device=paddle.CPUPlace(), stop_gradient=False)
+            >>> paddle.to_tensor([[0.1, 0.2], [0.3, 0.4]], device=paddle.CPUPlace(), stop_gradient=False)
             Tensor(shape=[2, 2], dtype=float32, place=Place(cpu), stop_gradient=False,
             [[0.10000000, 0.20000000],
              [0.30000001, 0.40000001]])
