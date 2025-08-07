2303147eb4de3f765fe51ff03a35834977bf4287
SylarTiaNII <15840554235@163.com>
2025-08-07 22:29:51 +0800

    [LLM] fix ipc api of big size tensor

--- paddle/fluid/pybind/tensor.cc
+++ paddle/fluid/pybind/tensor.cc
@@ -795,7 +795,7 @@ void BindTensor(pybind11::module &m) {  // NOLINT
              tensor.ResetHolderWithType(
                  shared_reader_holder,
                  static_cast<phi::DataType>(t[3].cast<int>()));
-             tensor.Resize(common::make_ddim(t[4].cast<std::vector<int>>()));
+             tensor.Resize(common::make_ddim(t[4].cast<std::vector<int64_t>>()));
 
              return tensor;
            },
