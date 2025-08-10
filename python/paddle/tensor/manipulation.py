b6dc10c395679c3395af616133ba51238e395d4a
zhengshengning <ningzhengsheng@163.com>
2025-08-11 00:09:35 +0800

    opt reshape alias

--- python/paddle/tensor/manipulation.py
+++ python/paddle/tensor/manipulation.py
@@ -24,7 +24,10 @@ from typing_extensions import overload
 import paddle
 from paddle import _C_ops
 from paddle.tensor import fill_constant
-from paddle.utils.decorator_utils import ParamAliasDecorator
+from paddle.utils.decorator_utils import (
+    ParamAliasDecorator,
+    reshape_param_alias,
+)
 from paddle.utils.inplace_utils import inplace_apis_in_dygraph_only
 
 from ..base.data_feeder import (
@@ -4976,7 +4979,7 @@ def expand(x: Tensor, shape: ShapeLike, name: str | None = None) -> Tensor:
         return out
 
 
-@ParamAliasDecorator({"x": ["input"]})
+@reshape_param_alias({"x": ["input"]})
 def reshape(x: Tensor, shape: ShapeLike, name: str | None = None) -> Tensor:
     """
     Changes the shape of ``x`` without changing its data.
