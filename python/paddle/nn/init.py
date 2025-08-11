e2b2cc9a2a789ceb3a0e8fd255e00bb12ac06534
zhiminzhang0830 <452516515@qq.com>
2025-08-11 08:57:50 +0000

    add xavier_uniform_, kaiming_normal_, uniform_

--- python/paddle/nn/init.py
+++ python/paddle/nn/init.py
@@ -19,7 +19,12 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import paddle
 
-from .initializer.kaiming import KaimingUniform
+from .initializer.initializer import calculate_gain as calculate_gain_
+from .initializer.kaiming import KaimingNormal, KaimingUniform
+from .initializer.uniform import Uniform
+from .initializer.xavier import XavierUniform
+
+calculate_gain = calculate_gain_
 
 
 def kaiming_uniform_(
@@ -49,5 +54,88 @@ def kaiming_uniform_(
     init = KaimingUniform(
         negative_slope=a, nonlinearity=nonlinearity, mode=mode
     )
+
+    return init(tensor, block=block)
+
+
+def kaiming_normal_(
+    tensor: paddle.Tensor,
+    a: float = 0,
+    mode: str = "fan_in",
+    nonlinearity: str = "leaky_relu",
+    block: paddle.pir.Block | None = None,
+) -> paddle.Tensor | None:
+    """Modify tensor inplace using Kaiming normal method.
+
+    Args:
+        tensor (Tensor):  Paddle Tensor.
+        a (float, optional): The negative slope of the rectifier used after this layer.
+            Defaults to 0.
+        mode (str, optional): Mode to compute the fan. Choose from ["fan_in", "fan_out"].
+            When set to 'fan_in', the fan_in parameter is used for initialization.
+            When set to 'fan_out', the out_features of trainable Tensor will be used.
+            Default is 'fan_in'.
+        nonlinearity (str, optional): Nonlinearity method name. Defaults to "leaky_relu".
+        block (Block|None, optional): The block in which initialization ops
+                should be added. Used in static graph only, default None.
+
+    Returns:
+        Tensor: Initialized tensor.
+    """
+    init = KaimingNormal(negative_slope=a, nonlinearity=nonlinearity, mode=mode)
+
+    return init(tensor, block=block)
+
+
+def xavier_uniform_(
+    tensor: paddle.Tensor,
+    gain: float = 1.0,
+    fan_in: float | None = None,
+    fan_out: float | None = None,
+    block: paddle.pir.Block | None = None,
+) -> paddle.Tensor | None:
+    """Modify tensor inplace using Xavier uniform method.
+
+    Args:
+        tensor (Tensor):  Paddle Tensor.
+        gain (float, optional): Scaling Tensor. Default is 1.0.
+        fan_in (float|None, optional): fan_in for Xavier initialization, which is
+                inferred from the Tensor. Default is None.
+        fan_out (float|None, optional): fan_out for Xavier initialization, which is
+                 inferred from the Tensor. Default is None.
+        block (Block|None, optional): The block in which initialization ops
+                should be added. Used in static graph only, default None.
+
+    Returns:
+        Tensor: Initialized tensor.
+    """
+    init = XavierUniform(
+        gain=gain,
+        fan_in=fan_in,
+        fan_out=fan_out,
+    )
+
+    return init(tensor, block=block)
+
+
+def uniform_(
+    tensor: paddle.Tensor,
+    a: float = 0.0,
+    b: float = 1.0,
+    block: paddle.pir.Block | None = None,
+) -> paddle.Tensor | None:
+    """Modify tensor inplace using uniform method.
+
+    Args:
+        tensor (Tensor):  Paddle Tensor.
+        low (float, optional): Lower boundary of the uniform distribution. Default is :math:`-1.0`.
+        high (float, optional): Upper boundary of the uniform distribution. Default is :math:`1.0`.
+        block (Block|None, optional): The block in which initialization ops
+                should be added. Used in static graph only, default None.
+
+    Returns:
+        Tensor: Initialized tensor.
+    """
+    init = Uniform(low=a, high=b)
 
     return init(tensor, block=block)
