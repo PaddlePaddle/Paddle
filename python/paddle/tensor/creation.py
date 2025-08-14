97d33651f7b4e240c8b7730979e79601673fb79a
zeroRains <linjunlu@zerorains.top>
2025-08-14 19:49:04 +0800

    fix bug

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -24,7 +24,7 @@ import numpy as np
 
 import paddle
 from paddle import _C_ops
-from paddle.utils.decorator_utils import SizeArgsDecorator
+from paddle.utils.decorator_utils import ParamAliasDecorator, SizeArgsDecorator
 from paddle.utils.inplace_utils import inplace_apis_in_dygraph_only
 
 from ..base.data_feeder import (
