44c7b27fb3d06cb3f67e282d585734fb5bfe6abf
changeyoung98 <1792266893@qq.com>
2025-08-14 03:02:48 +0000

    add safetensors to paddle save/load

--- test/legacy_test/test_paddle_save_load.py
+++ test/legacy_test/test_paddle_save_load.py
@@ -160,6 +160,25 @@ class TestSaveLoadPickle(unittest.TestCase):
                     dict_load[key].numpy(), value.numpy()
                 )
 
+    def test_safetensors(self):
+        # enable dygraph mode
+        paddle.disable_static()
+        # create network
+        layer = LinearNet()
+        save_dict = layer.state_dict()
+
+        path = os.path.join(
+            self.temp_dir.name,
+            "test_paddle_save_load_safetensors",
+            "layer.safetensors",
+        )
+
+        paddle.save(save_dict, path, safetensors=True)
+        dict_load = paddle.load(path, safetensors=True)
+        # compare results before and after saving
+        for key, value in save_dict.items():
+            np.testing.assert_array_equal(dict_load[key].numpy(), value.numpy())
+
 
 class TestSaveLoadAny(unittest.TestCase):
     def setUp(self):
