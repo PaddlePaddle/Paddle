ed08c56076a6fbe225a5d6f5718c8f8d381de156
Starrysea996 <2462405885@qq.com>
2025-08-15 14:45:19 +0800

    fix test scatter reduce

--- test/legacy_test/test_scatter_reduce_op.py
+++ test/legacy_test/test_scatter_reduce_op.py
@@ -27,7 +27,7 @@ from paddle.static import InputSpec
 def scatter_reduce_net(x, axis=-1):
     index = paddle.full_like(x, fill_value=2, dtype='int64')
     n = paddle.numel(x)
-    ind = paddle.arange(n, dtype='float32')
+    ind = paddle.arange(n, dtype='int32').astype(x.dtype)
     value = paddle.reshape(ind, x.shape)
     return paddle.scatter_reduce(x, axis, index, value, reduce='sum')
 
