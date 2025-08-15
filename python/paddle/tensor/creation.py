deb29f09d4ffd23017c249c174e0e56a828ed72a
HydrogenSulfate <490868991@qq.com>
2025-08-15 22:58:23 +0800

    update range docstring

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -1979,23 +1979,23 @@ def range(
 
             >>> import paddle
 
-            >>> out1 = paddle.arange(5)
+            >>> out1 = paddle.range(5)
             >>> print(out1.numpy())
-            [0 1 2 3 4]
+            [0 1 2 3 4 5]
 
-            >>> out2 = paddle.arange(3, 9, 2.0)
+            >>> out2 = paddle.range(3, 9, 2.0)
             >>> print(out2.numpy())
-            [3. 5. 7.]
+            [3. 5. 7. 9.]
 
             >>> # use 4.999 instead of 5.0 to avoid floating point rounding errors
-            >>> out3 = paddle.arange(4.999, dtype='float32')
+            >>> out3 = paddle.range(4.999, dtype='float32')
             >>> print(out3.numpy())
             [0. 1. 2. 3. 4.]
 
             >>> start_var = paddle.to_tensor(3)
-            >>> out4 = paddle.arange(start_var, 7)
+            >>> out4 = paddle.range(start_var, 7)
             >>> print(out4.numpy())
-            [3 4 5 6]
+            [3 4 5 6 7]
 
     """
     if end is None:
