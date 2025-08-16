6e7e008ef2e3fe94343792c9dc7532634e873c3d
gouzi <530971494@qq.com>
2025-08-16 23:07:37 +0800

    [CodeStyle] `black -> ruff format` migration - part 2

--- python/paddle/distributed/fleet/base/role_maker.py
+++ python/paddle/distributed/fleet/base/role_maker.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Definition of Role Makers."""
+
 from __future__ import annotations
 
 import os
