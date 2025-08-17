c214bd22b7222a26ec240fe8e8ce5205e546a673
Nyakku Shigure <sigure.qaq@gmail.com>
2025-08-17 15:00:12 +0800

    Update test/ir/inference/test_matmul_scale_fuse_pass.py

--- test/ir/inference/test_matmul_scale_fuse_pass.py
+++ test/ir/inference/test_matmul_scale_fuse_pass.py
@@ -33,9 +33,7 @@ class TestMatmulScaleFusePass(PassAutoScanTest):
         config = self.create_inference_config(use_gpu=False)
         yield (
             config,
-            [
-                "matmul",
-            ],
+            ["matmul"],
             (1e-5, 1e-5),
         )
 
