8e0626a3a0676b5a3448ab6349ae9c9e53b89bf3
co63oc <co63oc@users.noreply.github.com>
2025-08-18 08:23:55 +0800

    Fix

--- test/cpp/fluid/framework/op_registry_test.cc
+++ test/cpp/fluid/framework/op_registry_test.cc
@@ -338,7 +338,7 @@ REGISTER_OP_KERNEL(op_with_multi_kernel,
                    phi::CPUPlace,
                    paddle::framework::OpMultiKernelTest<CPUContext, float>);
 REGISTER_OP_KERNEL(op_with_multi_kernel,
-                   MKLDNN,
+                   ONEDNN,
                    phi::CPUPlace,
                    paddle::framework::OpMultiKernelTest2<CPUContext, float>);
 REGISTER_OP_KERNEL(
