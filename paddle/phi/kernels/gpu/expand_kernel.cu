97dbf90012ece33cf85d38fb01a2985048d81b5f
enkilee <jeffrey0122@163.com>
2025-08-21 16:41:18 +0800

    fix

--- paddle/phi/kernels/gpu/expand_kernel.cu
+++ paddle/phi/kernels/gpu/expand_kernel.cu
@@ -29,6 +29,11 @@ void ExpandKernel(const Context& dev_ctx,
                   DenseTensor* out) {
   auto in_dims = x.dims();
   auto expand_shape = shape.GetData();
+  PADDLE_ENFORCE_GT(
+      expand_shape.size(),
+      0,
+      common::errors::InvalidArgument("The target shape for expand cannot be "
+                                      "empty. Please provide a valid shape."));
   auto vec_in_dims = common::vectorize<int64_t>(in_dims);
   auto diff = expand_shape.size() - vec_in_dims.size();
   PADDLE_ENFORCE_GE(
