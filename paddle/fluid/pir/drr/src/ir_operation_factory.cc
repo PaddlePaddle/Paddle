272a9427f6f80d1499335939720e76482c0d50ae
co63oc <co63oc@users.noreply.github.com>
2025-08-22 10:24:19 +0800

    Fix

--- paddle/fluid/pir/drr/src/ir_operation_factory.cc
+++ paddle/fluid/pir/drr/src/ir_operation_factory.cc
@@ -274,6 +274,14 @@ void OperationFactory::RegisterManualOpCreator() {
           std::string mkldnn_data_type = attrs.at("mkldnn_data_type")
                                              .dyn_cast<pir::StrAttribute>()
                                              .AsString();
+          PADDLE_ENFORCE_EQ(attrs.find("onednn_data_type") != attrs.end(),
+                            true,
+                            common::errors::InvalidArgument(
+                                "'onednn_data_type' Attribute is expected "
+                                "for Conv2dTransposeBiasOp. "));
+          std::string onednn_data_type = attrs.at("onednn_data_type")
+                                             .dyn_cast<pir::StrAttribute>()
+                                             .AsString();
 
           PADDLE_ENFORCE_EQ(attrs.find("fuse_relu") != attrs.end(),
                             true,
@@ -323,6 +331,7 @@ void OperationFactory::RegisterManualOpCreator() {
               is_test,
               force_fp32_output,
               mkldnn_data_type,
+              onednn_data_type,
               fuse_relu,
               fuse_activation,
               fuse_alpha,
