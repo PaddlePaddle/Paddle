f9a84b0c7a479bcf7d4bd97ce95a86f21ac43ccc
Ayakouji <yuhongh@qq.com>
2025-08-26 20:00:47 +0800

    update

--- tools/static_mode_white_list.py
+++ tools/static_mode_white_list.py
@@ -613,7 +613,6 @@ STATIC_MODE_TESTING_LIST = [
     'test_slice_op_xpu',
     'test_generate_proposals_v2_op',
     'test_lamb_op_xpu',
-    'test_model_cast_to_bf16',
     'test_sgd_op_bf16',
     'test_c_embedding_op',
     'test_class_center_sample_op',
