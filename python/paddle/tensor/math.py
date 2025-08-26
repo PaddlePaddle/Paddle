d3d81798ed4139fc6a901b5a2f03f723321e355a
DanielSun11 <1395924413@qq.com>
2025-08-26 16:06:36 +0800

    sink matmul and multiply

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -30,6 +30,7 @@ from paddle._C_ops import (  # noqa: F401
     isfinite,
     isinf,
     isnan,
+    multiply,
 )
 from paddle.base.libpaddle import DataType
 from paddle.common_ops_import import VarDesc, dygraph_utils
@@ -1264,55 +1265,55 @@ floor_mod_.__doc__ = r"""
     """
 
 
-def multiply(x: Tensor, y: Tensor, name: str | None = None) -> Tensor:
-    """
-    multiply two tensors element-wise. The equation is:
+# def multiply(x: Tensor, y: Tensor, name: str | None = None) -> Tensor:
+#     """
+#     multiply two tensors element-wise. The equation is:
 
-    .. math::
-        out = x * y
+#     .. math::
+#         out = x * y
 
-    Note:
-        Supported shape of :attr:`x` and :attr:`y` for this operator:
-        1. `x.shape` == `y.shape`.
-        2. `x.shape` could be the continuous subsequence of `y.shape`.
-        ``paddle.multiply`` supports broadcasting. If you would like to know more about broadcasting, please refer to `Introduction to Tensor`_ .
+#     Note:
+#         Supported shape of :attr:`x` and :attr:`y` for this operator:
+#         1. `x.shape` == `y.shape`.
+#         2. `x.shape` could be the continuous subsequence of `y.shape`.
+#         ``paddle.multiply`` supports broadcasting. If you would like to know more about broadcasting, please refer to `Introduction to Tensor`_ .
 
-        .. _Introduction to Tensor: ../../guides/beginner/tensor_en.html#chapter5-broadcasting-of-tensor
+#         .. _Introduction to Tensor: ../../guides/beginner/tensor_en.html#chapter5-broadcasting-of-tensor
 
-    Args:
-        x (Tensor): the input tensor, its data type should be one of bfloat16, float16, float32, float64, int32, int64, bool, complex64, complex128.
-        y (Tensor): the input tensor, its data type should be one of bfloat16, float16, float32, float64, int32, int64, bool, complex64, complex128.
-        name (str|None, optional): Name for the operation (optional, default is None). For more information, please refer to :ref:`api_guide_Name`.
+#     Args:
+#         x (Tensor): the input tensor, its data type should be one of bfloat16, float16, float32, float64, int32, int64, bool, complex64, complex128.
+#         y (Tensor): the input tensor, its data type should be one of bfloat16, float16, float32, float64, int32, int64, bool, complex64, complex128.
+#         name (str|None, optional): Name for the operation (optional, default is None). For more information, please refer to :ref:`api_guide_Name`.
 
-    Returns:
-        N-D Tensor. A location into which the result is stored. If :attr:`x`, :attr:`y` have different shapes and are "broadcastable", the resulting tensor shape is the shape of :attr:`x` and :attr:`y` after broadcasting. If :attr:`x`, :attr:`y` have the same shape, its shape is the same as :attr:`x` and :attr:`y`.
+#     Returns:
+#         N-D Tensor. A location into which the result is stored. If :attr:`x`, :attr:`y` have different shapes and are "broadcastable", the resulting tensor shape is the shape of :attr:`x` and :attr:`y` after broadcasting. If :attr:`x`, :attr:`y` have the same shape, its shape is the same as :attr:`x` and :attr:`y`.
 
-    Examples:
+#     Examples:
 
-        .. code-block:: python
+#         .. code-block:: python
 
-            >>> import paddle
+#             >>> import paddle
 
-            >>> x = paddle.to_tensor([[1, 2], [3, 4]])
-            >>> y = paddle.to_tensor([[5, 6], [7, 8]])
-            >>> res = paddle.multiply(x, y)
-            >>> print(res)
-            Tensor(shape=[2, 2], dtype=int64, place=Place(cpu), stop_gradient=True,
-            [[5 , 12],
-             [21, 32]])
-            >>> x = paddle.to_tensor([[[1, 2, 3], [1, 2, 3]]])
-            >>> y = paddle.to_tensor([2])
-            >>> res = paddle.multiply(x, y)
-            >>> print(res)
-            Tensor(shape=[1, 2, 3], dtype=int64, place=Place(cpu), stop_gradient=True,
-            [[[2, 4, 6],
-              [2, 4, 6]]])
+#             >>> x = paddle.to_tensor([[1, 2], [3, 4]])
+#             >>> y = paddle.to_tensor([[5, 6], [7, 8]])
+#             >>> res = paddle.multiply(x, y)
+#             >>> print(res)
+#             Tensor(shape=[2, 2], dtype=int64, place=Place(cpu), stop_gradient=True,
+#             [[5 , 12],
+#              [21, 32]])
+#             >>> x = paddle.to_tensor([[[1, 2, 3], [1, 2, 3]]])
+#             >>> y = paddle.to_tensor([2])
+#             >>> res = paddle.multiply(x, y)
+#             >>> print(res)
+#             Tensor(shape=[1, 2, 3], dtype=int64, place=Place(cpu), stop_gradient=True,
+#             [[[2, 4, 6],
+#               [2, 4, 6]]])
 
-    """
-    if in_dynamic_or_pir_mode():
-        return _C_ops.multiply(x, y)
-    else:
-        return _elementwise_op(LayerHelper('elementwise_mul', **locals()))
+#     """
+#     if in_dynamic_or_pir_mode():
+#         return _C_ops.multiply(x, y)
+#     else:
+#         return _elementwise_op(LayerHelper('elementwise_mul', **locals()))
 
 
 @inplace_apis_in_dygraph_only
