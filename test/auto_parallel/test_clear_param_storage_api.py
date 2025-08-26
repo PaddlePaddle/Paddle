3842195b256d8c04c86499cd13d5cdd72e4b039d
liujinnan <1823192871@qq.com>
2025-08-26 19:50:12 +0800

    add ut

--- test/auto_parallel/test_clear_param_storage_api.py
+++ test/auto_parallel/test_clear_param_storage_api.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,30 +14,29 @@
 
 import unittest
 
-import paddle
+import collective.test_communication_api_base as test_base
 
 
-class TestClearParamStorage(unittest.TestCase):
-    def test_clear_param_storage(self):
-        class TestLayer(paddle.nn.Layer):
-            def __init__(self, dtype):
-                super().__init__()
-                self._w = self.create_parameter([2, 3], dtype=dtype)
-                self._b = self.create_parameter([2, 3], dtype=dtype)
-                self._w.color = {"color": "_w"}
-                self._b.color = {"color": "_b"}
+class TestSemiAutoParallelMoeUtilsAPI(test_base.CommunicationTestDistBase):
+    def setUp(self):
+        super().setUp(num_of_devices=2, timeout=120)
+        self._default_envs = {
+            "dtype": "float32",
+        }
+        self._changeable_envs = {
+            "backend": ["gpu"],
+        }
 
-            @paddle.amp.debugging.check_layer_numerics
-            def forward(self, x):
-                return x * self._w + self._b
+    def test_moe_utils(self):
+        envs_list = test_base.gen_product_envs_list(
+            self._default_envs, self._changeable_envs
+        )
+        for envs in envs_list:
+            self.run_test_case(
+                "clear_param_storage_api.py",
+                user_defined_envs=envs,
+            )
 
-        dtype = 'float32'
-        model = TestLayer(dtype)
-        adam = paddle.optimizer.Adam(parameters=model.parameters())
-        adam.clear_param_storage("_w")
-        adam.clear_param_storage("_b")
-        adam.reset_param_storage()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
