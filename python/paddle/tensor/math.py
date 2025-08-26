0cd0a5d4f02a12c9c3f76aa889a174008b3921cf
zhanghonggeng <zhanghonggeng@baidu.com>
2025-08-26 08:20:16 +0000

    [API compatibility]Fix trunc mode in divide op when inputs are integer

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -1043,6 +1043,32 @@ def divide(
     elif rounding_mode == "trunc":
         if in_dynamic_or_pir_mode():
             tmp = _C_ops.divide(x, y)
+
+            if x.dtype in (
+                paddle.uint8,
+                paddle.int8,
+                paddle.int16,
+                paddle.int32,
+                paddle.int64,
+            ) and y.dtype in (
+                paddle.uint8,
+                paddle.int8,
+                paddle.int16,
+                paddle.int32,
+                paddle.int64,
+            ):
+                if x.dtype == paddle.int64 or y.dtype == paddle.int64:
+                    target_dtype = paddle.int64
+                elif x.dtype == paddle.int32 or y.dtype == paddle.int32:
+                    target_dtype = paddle.int32
+                elif x.dtype == paddle.int16 or y.dtype == paddle.int16:
+                    target_dtype = paddle.int16
+                elif x.dtype == paddle.int8 or y.dtype == paddle.int8:
+                    target_dtype = paddle.int8
+                else:
+                    target_dtype = paddle.uint8
+                tmp = _C_ops.cast(tmp, target_dtype)
+
             res = _C_ops.trunc(tmp, out=out)
         else:
             tmp = _elementwise_op(LayerHelper('elementwise_div', **locals()))
@@ -1096,6 +1122,30 @@ def divide_(
         res = _C_ops.divide_(x, y)
     elif rounding_mode == "trunc":
         tmp = _C_ops.divide_(x, y)
+        if x.dtype in (
+            paddle.uint8,
+            paddle.int8,
+            paddle.int16,
+            paddle.int32,
+            paddle.int64,
+        ) and y.dtype in (
+            paddle.uint8,
+            paddle.int8,
+            paddle.int16,
+            paddle.int32,
+            paddle.int64,
+        ):
+            if x.dtype == paddle.int64 or y.dtype == paddle.int64:
+                target_dtype = paddle.int64
+            elif x.dtype == paddle.int32 or y.dtype == paddle.int32:
+                target_dtype = paddle.int32
+            elif x.dtype == paddle.int16 or y.dtype == paddle.int16:
+                target_dtype = paddle.int16
+            elif x.dtype == paddle.int8 or y.dtype == paddle.int8:
+                target_dtype = paddle.int8
+            else:
+                target_dtype = paddle.uint8
+            tmp = _C_ops.cast_(tmp, target_dtype)
         res = _C_ops.trunc_(tmp)
     elif rounding_mode == "floor":
         res = _C_ops.floor_divide_(x, y)
