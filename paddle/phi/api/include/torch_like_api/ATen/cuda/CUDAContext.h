4295bcf06ff878eb15e232845ed8fc09502af3a5
geyuqiang <geyuqiang@baidu.com>
2025-08-27 03:57:53 +0000

    add Exception.h to fix exception compile error

--- paddle/phi/api/include/torch_like_api/ATen/cuda/CUDAContext.h
+++ paddle/phi/api/include/torch_like_api/ATen/cuda/CUDAContext.h
@@ -14,7 +14,7 @@
 
 #pragma once
 
-#include <ATen/cuda/CUDAContextLight.h>
+// #include <ATen/cuda/CUDAContextLight.h>
 
 // Preserved for BC, as many files depend on these includes
 // #include <ATen/Context.h>
