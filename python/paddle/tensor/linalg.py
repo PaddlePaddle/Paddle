70ccc9a83eeea6852bff13672986f2b813eb00aa
zeroRains <linjunlu@zerorains.top>
2025-08-27 21:26:00 +0800

    support paddle.norm

--- python/paddle/tensor/linalg.py
+++ python/paddle/tensor/linalg.py
@@ -1184,6 +1184,7 @@ def norm(
     p: float | _POrder | None = None,
     axis: int | list[int] | tuple[int, int] | None = None,
     keepdim: bool = False,
+    out: paddle.Tensor | None = None,
     dtype: paddle._typing.DTypeLike | None = None,
     name: str | None = None,
 ) -> Tensor:
@@ -1328,28 +1329,36 @@ def norm(
         x = x.astype(dtype)
     if isinstance(p, str):
         if p == "fro" and (axis is None or isinstance(axis, int)):
-            return vector_norm(
+            output = vector_norm(
                 x,
                 p=2,
                 axis=axis,
                 keepdim=keepdim,
                 name=name,
             )
-        if axis is None:
-            axis = list(range(x.ndim))
-        return matrix_norm(x=x, p=p, axis=axis, keepdim=keepdim, name=name)
+        else:
+            if axis is None:
+                axis = list(range(x.ndim))
+            output = matrix_norm(
+                x=x, p=p, axis=axis, keepdim=keepdim, name=name
+            )
     else:
         p = 2.0 if p is None else p
         if isinstance(axis, list) and len(axis) == 2:
-            return matrix_norm(x=x, p=p, axis=axis, keepdim=keepdim, name=name)
+            output = matrix_norm(
+                x=x, p=p, axis=axis, keepdim=keepdim, name=name
+            )
         else:
-            return vector_norm(
+            output = vector_norm(
                 x,
                 p=p,
                 axis=axis,
                 keepdim=keepdim,
                 name=name,
             )
+    if out is not None:
+        paddle.assign(output, out=out)
+    return output
 
 
 def dist(x: Tensor, y: Tensor, p: float = 2, name: str | None = None) -> Tensor:
