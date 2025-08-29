f5a27cd40999fd912b4981ee60fe9fe0936f38e6
SigureMo <sigure.qaq@gmail.com>
2025-08-29 22:39:36 +0800

    remove trailing whitespace

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Compatibility Note: The design of certain PaddlePaddle public APIs 
-# incorporates principles from PyTorch and NumPy, maintaining compatibility 
-# with PyTorch's API conventions in terms of function signatures and 
-# parameter semantics. It is important to clarify that these APIs are 
+# Compatibility Note: The design of certain PaddlePaddle public APIs
+# incorporates principles from PyTorch and NumPy, maintaining compatibility
+# with PyTorch's API conventions in terms of function signatures and
+# parameter semantics. It is important to clarify that these APIs are
 # implemented as independent modules with no runtime dependency on PyTorch.
 
 import math
