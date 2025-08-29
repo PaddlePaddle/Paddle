fix stop_gradient of out

test/legacy_test/test_eye.py
@@ -116,10 +116,12 @@ class TestCreationOut(unittest.TestCase):
     def test_eye(self):
         x = paddle.randn([2, 2])
         t = paddle.empty_like(x)
-        y = paddle.eye(x.shape[0], x.shape[1], out=t)
+        y = paddle.eye(x.shape[0], x.shape[1], out=t, requires_grad=True)
         np.testing.assert_allclose(t.numpy(), np.eye(x.shape[0], x.shape[1]))
         np.testing.assert_allclose(y.numpy(), np.eye(x.shape[0], x.shape[1]))
         self.assertEqual(t.data_ptr(), y.data_ptr())
+        self.assertEqual(y.stop_gradient, False)
+        self.assertEqual(t.stop_gradient, False)
 
 
 if __name__ == '__main__':

test/legacy_test/test_full.py
@@ -286,10 +286,12 @@ class TestCreationOut(unittest.TestCase):
     def test_full(self):
         x = paddle.randn([2, 2])
         t = paddle.empty_like(x)
-        y = paddle.full(x.shape, self.constant, out=t)
+        y = paddle.full(x.shape, self.constant, out=t, requires_grad=True)
         np.testing.assert_allclose(t.numpy(), np.full(x.shape, self.constant))
         np.testing.assert_allclose(y.numpy(), np.full(x.shape, self.constant))
         self.assertEqual(t.data_ptr(), y.data_ptr())
+        self.assertEqual(y.stop_gradient, False)
+        self.assertEqual(t.stop_gradient, False)
 
 
 if __name__ == '__main__':

test/legacy_test/test_zeros.py
@@ -321,10 +321,12 @@ class TestCreationOut(unittest.TestCase):
     def test_zeros(self):
         x = paddle.randn([2, 2])
         t = paddle.empty_like(x)
-        y = paddle.zeros(x.shape, out=t)
+        y = paddle.zeros(x.shape, out=t, requires_grad=True)
         np.testing.assert_allclose(t.numpy(), np.zeros(x.shape))
         np.testing.assert_allclose(y.numpy(), np.zeros(x.shape))
         self.assertEqual(t.data_ptr(), y.data_ptr())
+        self.assertEqual(y.stop_gradient, False)
+        self.assertEqual(t.stop_gradient, False)
 
 
 if __name__ == '__main__':