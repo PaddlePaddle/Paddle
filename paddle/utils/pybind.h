b0a182d6d4f0b8da885ad976ddecb6957aaebc40
yongqiangma <xing.wo@163.com>
2025-08-30 00:04:31 +0000

    fix pybind

--- paddle/utils/pybind.h
+++ paddle/utils/pybind.h
@@ -14,7 +14,9 @@
 
 #pragma once
 
+#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
 #include <ATen/core/TensorBody.h>
+#endif
 #include "paddle/phi/api/include/tensor.h"
 #ifdef PADDLE_WITH_DISTRIBUTE
 #include "paddle/phi/core/distributed/auto_parallel/dist_tensor.h"
@@ -129,6 +131,7 @@ struct optional_caster<paddle::optional<paddle::Tensor>> {
                        const_name("Optional[paddle::Tensor]"));
 };
 
+#if defined(PADDLE_WITH_CUDA) || defined(PADDLE_WITH_HIP)
 template <>
 struct type_caster<at::Tensor> {
  public:
@@ -148,8 +151,7 @@ struct type_caster<at::Tensor> {
                      return_value_policy /* policy */,
                      handle /* parent */) {
     const auto& src_pd_tensor = src._PD_GetInner();
-    // TODO(GhostScreaming): pipeline parallel may return a uninitialized
-    // DistTensor, it should not return None.
+
 #ifdef PADDLE_WITH_DISTRIBUTE
     bool return_none =
         phi::distributed::DistTensor::classof(src_pd_tensor.impl().get())
@@ -162,7 +164,7 @@ struct type_caster<at::Tensor> {
         src_pd_tensor, return_none /* return_py_none_if_not_initialize */));
   }
 };
-
+#endif
 // Pybind11 bindings for optional types.
 // http://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html#c-17-library-containers
 template <typename T>
