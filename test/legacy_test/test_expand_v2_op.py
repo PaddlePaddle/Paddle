ca7f51cd7010f07766f8480fc2730aa309b0d1ea
enkilee <jeffrey0122@163.com>
2025-09-02 15:28:55 +0800

    add test

--- test/legacy_test/test_expand_v2_op.py
+++ test/legacy_test/test_expand_v2_op.py
@@ -428,6 +428,11 @@ class TestExpandV2Error(unittest.TestCase):
             self.assertRaises(ValueError, paddle.tensor.expand, x2, shape)
             x2.stop_gradient = True
             self.assertRaises(TypeError, paddle.tensor.expand, x2, 1)
+            x3 = paddle.static.data(name='x3', shape=[1, 1, 1], dtype="int64")
+            shape_empty = paddle.static.data(
+                name='shape_empty', shape=[0], dtype="int32"
+            )
+            self.assertRaises(ValueError, paddle.tensor.expand, x3, shape_empty)
 
 
 # Test python API
