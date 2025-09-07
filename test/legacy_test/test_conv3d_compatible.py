776012d944bf088d4ef40e3536866795bc87617a
cangtianhuang <l1903374751@gmail.com>
2025-09-07 16:28:58 +0000

    support cmp for conv

---
+++ test/legacy_test/test_conv3d_compatible.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+
+import paddle
+from paddle import base
+
+
+def get_places():
+    places = []
+    if base.is_compiled_with_cuda():
+        places.append(paddle.CUDAPlace(0))
+    places.append(paddle.CPUPlace())
+    return places
+
+
+class TestConv3dAPI_Compatibility(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(2025)
+        self.places = get_places()
+        self.shape_x = [2, 3, 8, 8, 8]  # NCDHW
+        self.shape_w = [6, 3, 3, 3, 3]  # Co, Cin, kD, kH, kW
+        self.dtype = "float32"
+        self.init_data()
+
+    def init_data(self):
+        self.np_x = np.random.rand(*self.shape_x).astype(self.dtype)
+        self.np_w = np.random.rand(*self.shape_w).astype(self.dtype)
+
+    def test_dygraph_Compatibility(self):
+        paddle.disable_static()
+        x = paddle.to_tensor(self.np_x)
+        w = paddle.to_tensor(self.np_w)
+
+        # Reference output
+        ref_out = paddle.nn.functional.conv3d(x, w)
+
+        paddle_dygraph_out = []
+        # Position args (args)
+        out1 = paddle.nn.functional.conv3d(x, w)
+        paddle_dygraph_out.append(out1)
+        # Key words args (kwargs) for paddle
+        out2 = paddle.nn.functional.conv3d(x=x, weight=w)
+        paddle_dygraph_out.append(out2)
+        # Key words args for alias compatibility
+        out3 = paddle.nn.functional.conv3d(input=x, weight=w)
+        paddle_dygraph_out.append(out3)
+        # Combined args and kwargs
+        out4 = paddle.nn.functional.conv3d(x, weight=w)
+        paddle_dygraph_out.append(out4)
+
+        # Check all dygraph results against reference
+        for out in paddle_dygraph_out:
+            np.testing.assert_allclose(ref_out.numpy(), out.numpy(), rtol=1e-05)
+        paddle.enable_static()
+
+    def test_static_Compatibility(self):
+        paddle.enable_static()
+
+        # Calculate reference output in dygraph mode for comparison
+        ref_out = paddle.nn.functional.conv3d(
+            paddle.to_tensor(self.np_x), paddle.to_tensor(self.np_w)
+        ).numpy()
+
+        fetch_list = []
+        main = paddle.static.Program()
+        startup = paddle.static.Program()
+        with base.program_guard(main, startup):
+            x = paddle.static.data(
+                name="x", shape=self.shape_x, dtype=self.dtype
+            )
+            w = paddle.static.data(
+                name="w", shape=self.shape_w, dtype=self.dtype
+            )
+
+            # Position args (args)
+            out1 = paddle.nn.functional.conv3d(x, w)
+            fetch_list.append(out1)
+            # Key words args (kwargs) for paddle
+            out2 = paddle.nn.functional.conv3d(x=x, weight=w)
+            fetch_list.append(out2)
+            # Key words args for alias compatibility
+            out3 = paddle.nn.functional.conv3d(input=x, weight=w)
+            fetch_list.append(out3)
+            # Combined args and kwargs
+            out4 = paddle.nn.functional.conv3d(x, weight=w)
+            fetch_list.append(out4)
+
+            for place in self.places:
+                exe = base.Executor(place)
+                fetches = exe.run(
+                    main,
+                    feed={"x": self.np_x, "w": self.np_w},
+                    fetch_list=fetch_list,
+                )
+                for out in fetches:
+                    np.testing.assert_allclose(out, ref_out, rtol=1e-05)
+
+
+if __name__ == "__main__":
+    unittest.main()
