c3717a7d7b224cf1a7a6d67aad64d59a33bb3269
SigureMo <sigure.qaq@gmail.com>
2025-09-09 00:11:39 +0800

    [Typing] Fix missing generic type arg in decorator caused type propagation break

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -3470,7 +3470,7 @@ def clone(x: paddle.Tensor, name: str | None = None) -> paddle.Tensor:
             >>> y.backward()
             >>> print(clone_x.grad.numpy())  # type: ignore
             [3. 3.]
-            >>> print(x.grad.numpy())
+            >>> print(x.grad.numpy())  # type: ignore
             [3. 3.]
     """
     return x.clone()
