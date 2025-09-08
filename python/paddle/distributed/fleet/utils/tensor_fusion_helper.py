91ef7db7564471dfa4cb454cebbf477cd6854b1c
Wennie396 <wn295448@buaa.edu.cn>
2025-09-08 11:36:32 +0800

    add chunk offload optimizer

--- python/paddle/distributed/fleet/utils/tensor_fusion_helper.py
+++ python/paddle/distributed/fleet/utils/tensor_fusion_helper.py
@@ -81,6 +81,7 @@ def assign_group_by_size(parameters, group_size=128 * 1024 * 1024):
 
     var_groups = OrderedDict()
     group_msg = []
+    param_sizes = []
     for group_idx, indices in enumerate(group_indices):
         group_size = 0
         for index in indices:
@@ -90,9 +91,11 @@ def assign_group_by_size(parameters, group_size=128 * 1024 * 1024):
         bytes = group_size * core.size_of_dtype(dtype)
         msg = f"group_{group_idx}: {bytes / 1024 ** 2:.4f} MB, dtype: {dtype!s}"
         group_msg.append(msg)
+        param_size_G = bytes / 1024**3
+        param_sizes.append(param_size_G)
 
     logger.info(f"Tensor Fusion Group Info:\n{group_msg}\n")
-    return var_groups
+    return var_groups, group_size
 
 
 def flatten_dense_tensors(
