5350052a4e5b245c0d78a3552402f7ef05d36334
Ayakouji <yuhongh@qq.com>
2025-09-08 19:42:00 +0800

    update

--- test/legacy_test/test_tensor_place.py
+++ test/legacy_test/test_tensor_place.py
@@ -43,13 +43,13 @@ class TestPlace(unittest.TestCase):
 
 class TestGetDevice(unittest.TestCase):
     def test_cpu_tensor(self):
-        x = paddle.ones([2, 3], place=paddle.CPUPlace())
+        x = paddle.to_tensor([1, 2, 3], place=paddle.CPUPlace())
         self.assertEqual(x.get_device(), x.place.gpu_device_id())
 
     def test_gpu_tensor(self):
         if not paddle.is_compiled_with_cuda():
             return
-        y = paddle.ones([2, 3], place=paddle.CUDAPlace(0))
+        y = paddle.to_tensor([1, 2, 3], place=paddle.CUDAPlace(0))
         self.assertEqual(y.get_device(), y.place.gpu_device_id())
 
 
