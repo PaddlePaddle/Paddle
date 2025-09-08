7d0ca66db5a40d37504b63672fb7229b8e3c07a3
DrRyanHuang <zihaohuang@aliyun.com>
2025-09-08 06:21:29 +0000

    2020 -> 2025

--- test/dygraph_to_static/test_cudagraph.py
+++ test/dygraph_to_static/test_cudagraph.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for logical operators of Dynamic-to-Static.
-Only test simple cases here. The complex test samples like nested ifelse
-or nested loop have been covered in file test_ifelse.py and test_loop.py"""
 
 import unittest
 from contextlib import contextmanager
