cb3c54bb8aa9392c6225e165ff7c11c8b5eab75f
SigureMo <sigure.qaq@gmail.com>
2025-09-10 12:32:39 +0800

    add unittest for cpu

--- test/legacy_test/test_tvm_ffi.py
+++ test/legacy_test/test_tvm_ffi.py
@@ -18,13 +18,20 @@ import paddle
 
 
 class TestTVMFFI(unittest.TestCase):
-    def test_tvm_ffi_env_stream(self):
+    def test_tvm_ffi_env_stream_for_gpu_tensor(self):
         if not paddle.is_compiled_with_cuda():
             return
-        tensor = paddle.to_tensor([1.0, 2.0, 3.0])
+        tensor = paddle.to_tensor([1.0, 2.0, 3.0]).cuda()
         current_raw_stream_ptr = tensor.__tvm_ffi_env_stream__()
         self.assertIsInstance(current_raw_stream_ptr, int)
         self.assertNotEqual(current_raw_stream_ptr, 0)
+
+    def test_tvm_ffi_env_stream_for_cpu_tensor(self):
+        tensor = paddle.to_tensor([1.0, 2.0, 3.0]).cpu()
+        with self.assertRaisesRegex(
+            RuntimeError, r"the __tvm_ffi_env_stream__ method"
+        ):
+            tensor.__tvm_ffi_env_stream__()
 
 
 if __name__ == '__main__':
