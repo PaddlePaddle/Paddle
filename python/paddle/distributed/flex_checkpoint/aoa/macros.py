8f6075948e57a713dd2dde9ebe00a6d54153d332
xingmingyyj <zxm_3791@163.com>
2025-09-12 14:52:40 +0800

    add macros

--- python/paddle/distributed/flex_checkpoint/aoa/macros.py
+++ python/paddle/distributed/flex_checkpoint/aoa/macros.py
@@ -55,8 +55,9 @@ GLOBAL_ATTRIBUTE_KEYWORDS = [
     'num_heads',
     'num_key_value_groups',
     'permute',
+    'fused_ffn_bias',
+    'fused_qkv_old_bias',
 ]
-
 
 # star_macro must be called after layer_id_macro
 @macro(name='star_macro', priority=3)
@@ -126,21 +127,29 @@ def layer_id_macro(tokens, expression, context):
 
     for layer_id in range(num_layers):
         expr = ""
+        skip = False  
         for token in tokens:
             if token.type == TokenType.IDENTIFIER:
                 if LAYER_ID_MACRO_TAG in token.value:
-                    expr += token.value.replace(
+                    replaced_token_value = token.value.replace(
                         LAYER_ID_MACRO_TAG, str(layer_id)
                     )
+                    if replaced_token_value not in context.get_all_src_state_keys():
+                        skip = True
+                        break
+                    expr += replaced_token_value
                 elif token.value not in GLOBAL_ATTRIBUTE_KEYWORDS:
                     expr += f"{token.value}.layer.{layer_id}"
                 else:
                     expr += token.value
             else:
                 expr += token.value
-        expanded_expressions.append(expr)
+        if not skip:
+            expanded_expressions.append(expr)
 
     return expanded_expressions
+
+
 
 
 @macro(name='array_macro', priority=2)
@@ -603,3 +612,282 @@ def fused_qkv(tokens, expression, context):
 
     else:
         return expression
+
+@macro(name='fused_ffn_bias_macro', priority=4)
+def fused_ffn_bias_macro(tokens, expression, context):
+    FUSED_FFN_BIAS_TAG = "fused_ffn_bias"
+    if not any(tkn.value == FUSED_FFN_BIAS_TAG for tkn in tokens):
+        return expression
+    rarrow_pos = None
+    fused_ffn_bias_pos = None
+    for idx, token in enumerate(tokens):
+        if token.type == TokenType.RARROW and rarrow_pos is None:
+            rarrow_pos = idx
+        elif (
+            token.type == TokenType.IDENTIFIER and token.value == FUSED_FFN_BIAS_TAG
+        ):
+            fused_ffn_bias_pos = idx
+    assert rarrow_pos is not None, "No -> found in expression."
+    assert fused_ffn_bias_pos is not None, "No fused_ffn_bias tag found in expression."
+    results = []
+    if rarrow_pos == 1:
+        src_ffn_weight_name = tokens[0].value
+        if fused_ffn_bias_pos == 4:
+            dst_ffn_weight_name = tokens[2].value
+        else:
+            dst_ffn_weight_name = None
+        src_state_shard_num = context.get_src_state_shard_num(
+            src_ffn_weight_name
+        )
+        dst_state_shard_num = (
+            context.get_dst_state_shard_num(dst_ffn_weight_name)
+            if dst_ffn_weight_name is not None
+            else 1
+        )
+        splited_num = math.lcm(src_state_shard_num, dst_state_shard_num)
+
+        configs = [
+            (src_state_shard_num, src_ffn_weight_name),
+            (dst_state_shard_num, dst_ffn_weight_name),
+        ]
+        split_config = [("GATE", splited_num), ("UP", splited_num)]
+
+        def gen_expr(tp_degree, splited_num, tp_rank, comp):
+            return ",".join(
+                f"fused_ffn_bias_tmp.{comp}_{tp_rank * splited_num // tp_degree + idx}"
+                for idx in range(splited_num // tp_degree)
+            )
+
+        for idx, (tp_degree, ffn_weight_name) in enumerate(configs):
+            ffn_parts = [
+                gen_expr(tp_degree, n, tp_rank, c)
+                for tp_rank in range(tp_degree)
+                for c, n in split_config
+            ]
+            if idx == 0:
+                results.append(
+                    f"{ffn_weight_name}  -> {','.join(ffn_parts)}, axis=0"
+                )
+            elif ffn_weight_name is not None:
+                results.append(
+                    f"{','.join(ffn_parts)} -> {ffn_weight_name}, axis=0"
+                )
+        if fused_ffn_bias_pos > 4:
+
+            def _generate_expr(prefix, count, target_name):
+                elements = ",".join(
+                    f"fused_ffn_bias_tmp.{prefix}_{i}" for i in range(count)
+                )
+                return f"{elements} -> {target_name}, axis=0"
+
+            gate_name = tokens[2].value
+            up_name = tokens[4].value
+
+            results.append(_generate_expr("GATE", splited_num, gate_name))
+            results.append(_generate_expr("UP", splited_num, up_name))
+
+    elif rarrow_pos == 3:
+        gate_name = tokens[0].value
+        up_name = tokens[2].value
+        dst_ffn_weight_name = tokens[4].value
+
+        fused_gate_up_tmp_name = f"{gate_name}.{up_name}.tmp"
+        results.append(
+            f"{gate_name},{up_name}  ->  {fused_gate_up_tmp_name}, axis=0"
+        )
+        dst_state_shard_num = context.get_dst_state_shard_num(
+            dst_ffn_weight_name
+        )
+
+        configs = [
+            (1, fused_gate_up_tmp_name),
+            (dst_state_shard_num, dst_ffn_weight_name),
+        ]
+
+        split_config = [
+            ("GATE", dst_state_shard_num),
+            ("UP", dst_state_shard_num),
+        ]
+
+        def gen_expr(tp_degree, splited_num, tp_rank, comp):
+            return ",".join(
+                f"fused_ffn_bias_tmp.{comp}_{tp_rank * splited_num // tp_degree + idx}"
+                for idx in range(splited_num // tp_degree)
+            )
+
+        for idx, (tp_degree, ffn_weight_name) in enumerate(configs):
+            ffn_parts = [
+                gen_expr(tp_degree, n, tp_rank, c)
+                for tp_rank in range(tp_degree)
+                for c, n in split_config
+            ]
+            if idx == 0:
+                results.append(
+                    f"{ffn_weight_name}  -> {','.join(ffn_parts)}, axis=0"
+                )
+            else:
+                results.append(
+                    f"{','.join(ffn_parts)} -> {ffn_weight_name}, axis=0"
+                )
+    else:
+        raise ValueError(f"Unsupported fused_ffn_bias macro format: {expression}.")
+    return results
+
+@macro(name='fused_qkv_old_bias_macro', priority=4)
+def fused_qkv_old_bias_macro(tokens, expression, context):
+    FUSED_QKV_OLD_BIAS_TAG = "fused_qkv_old_bias"
+    if not any(tkn.value == FUSED_QKV_OLD_BIAS_TAG for tkn in tokens):
+        return expression
+
+    attn_head_num = None
+    num_key_value_groups = None
+    fused_qkv_old_bias_pos = None
+    rarrow_pos = None
+    right_var_end_pos = None
+
+    for idx, token in enumerate(tokens):
+        if token.type == TokenType.IDENTIFIER:
+            if token.value == "num_heads" and idx + 2 < len(tokens):
+                attn_head_num = int(tokens[idx + 2].value)
+            elif token.value == "num_key_value_groups" and idx + 2 < len(
+                tokens
+            ):
+                num_key_value_groups = int(tokens[idx + 2].value)
+            elif token.value == FUSED_QKV_OLD_BIAS_TAG:
+                fused_qkv_old_bias_pos = idx
+        elif token.type == TokenType.RARROW and rarrow_pos is None:
+            rarrow_pos = idx
+        if (
+            right_var_end_pos is None
+            and token.type == TokenType.IDENTIFIER
+            and token.value
+            in {FUSED_QKV_OLD_BIAS_TAG, "num_heads", "num_key_value_groups"}
+        ):
+            right_var_end_pos = idx + 1
+
+    assert attn_head_num and attn_head_num > 0, "num_heads must be positive."
+    assert num_key_value_groups and num_key_value_groups > 0, (
+        "num_key_value_groups must be positive."
+    )
+    assert fused_qkv_old_bias_pos is not None, (
+        "No fused_qkv_old_bias tag found in expression."
+    )
+    assert rarrow_pos is not None, "No -> found in expression."
+    assert attn_head_num % num_key_value_groups == 0, (
+        "num_heads must be divisible by num_key_value_groups."
+    )
+
+    results = []
+    num_key_value_heads = num_key_value_groups
+    if rarrow_pos == 1:
+        src_qkv_weight_name = tokens[0].value
+        if fused_qkv_old_bias_pos > 4:
+            dst_qkv_weight_name = None
+        else:
+            dst_qkv_weight_name = tokens[2].value
+
+        src_state_shard_num = context.get_src_state_shard_num(
+            src_qkv_weight_name
+        )
+        dst_state_shard_num = (
+            context.get_dst_state_shard_num(dst_qkv_weight_name)
+            if dst_qkv_weight_name is not None
+            else 1
+        )
+
+        configs = [
+            (src_state_shard_num, src_qkv_weight_name),
+            (dst_state_shard_num, dst_qkv_weight_name),
+        ]
+
+        head_config = [
+            ("Q", attn_head_num),
+            ("K", num_key_value_heads),
+            ("V", num_key_value_heads),
+        ]
+
+        def gen_expr(tp_degree, num_heads, tp_rank, comp):
+            start = tp_rank * num_heads // tp_degree
+            count = num_heads // tp_degree
+            return ",".join(
+                f"fused_qkv_old_bias_tmp.{comp}_{i}"
+                for i in range(start, start + count)
+            )
+
+        for idx, (tp_degree, qkv_weight_name) in enumerate(configs):
+            qkv_parts = [
+                gen_expr(tp_degree, n, tp_rank, c)
+                for tp_rank in range(tp_degree)
+                for c, n in head_config
+            ]
+            if idx == 0:
+                mapping = f"{qkv_weight_name} -> {','.join(qkv_parts)}, axis=0"
+                results.append(mapping)
+            elif qkv_weight_name is not None:
+                mapping = f"{','.join(qkv_parts)} -> {qkv_weight_name}, axis=0"
+                results.append(mapping)
+
+        if fused_qkv_old_bias_pos > 4:
+
+            def _generate_expr(prefix, count, target_name):
+                elements = ",".join(
+                    f"fused_qkv_old_bias_tmp.{prefix}_{i}" for i in range(count)
+                )
+                return f"{elements} -> {target_name}, axis=1"
+
+            q_name = tokens[2].value
+            k_name = tokens[4].value
+            v_name = tokens[6].value
+
+            results.append(_generate_expr("Q", attn_head_num, q_name))
+            results.append(_generate_expr("K", num_key_value_heads, k_name))
+            results.append(_generate_expr("V", num_key_value_heads, v_name))
+    elif rarrow_pos == 5:
+        q_name = tokens[0].value
+        k_name = tokens[2].value
+        v_name = tokens[4].value
+        dst_qkv_weight_name = tokens[6].value
+
+        fused_qkv_tmp_name = f"{q_name}.{k_name}.{v_name}.tmp"
+        results.append(
+            f"{q_name},{k_name},{v_name}  ->  {fused_qkv_tmp_name}, axis=0"
+        )
+        dst_state_shard_num = context.get_dst_state_shard_num(
+            dst_qkv_weight_name
+        )
+
+        configs = [
+            (1, fused_qkv_tmp_name),
+            (dst_state_shard_num, dst_qkv_weight_name),
+        ]
+
+        head_config = [
+            ("Q", attn_head_num),
+            ("K", num_key_value_heads),
+            ("V", num_key_value_heads),
+        ]
+
+        def gen_expr(tp_degree, num_heads, tp_rank, comp):
+            start = tp_rank * num_heads // tp_degree
+            count = num_heads // tp_degree
+            return ",".join(
+                f"fused_qkv_old_bias_tmp.{comp}_{i}"
+                for i in range(start, start + count)
+            )
+
+        for idx, (tp_degree, qkv_weight_name) in enumerate(configs):
+            qkv_parts = [
+                gen_expr(tp_degree, n, tp_rank, c)
+                for tp_rank in range(tp_degree)
+                for c, n in head_config
+            ]
+            if idx == 0:
+                mapping = f"{qkv_weight_name} -> {','.join(qkv_parts)}, axis=0"
+            else:
+                mapping = f"{','.join(qkv_parts)} -> {qkv_weight_name}, axis=0"
+            results.append(mapping)
+    else:
+        raise ValueError(
+            f"Unsupported fused_qkv_old_bias macro format: {expression}."
+        )
+    return results
