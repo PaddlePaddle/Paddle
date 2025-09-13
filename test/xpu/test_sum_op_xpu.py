f6471db755811d34a99f6a61f8041e1135937f5c
gouzi <530971494@qq.com>
2025-09-13 22:19:45 +0800

    fix xpu

--- test/xpu/test_sum_op_xpu.py
+++ test/xpu/test_sum_op_xpu.py
@@ -170,11 +170,11 @@ class TestSumOpError(unittest.TestCase):
     def test_errors(self):
         def test_empty_list_input():
             with base.dygraph.guard():
-                base._C_ops.sum([])
+                paddle._C_ops.sum([])
 
         def test_list_of_none_input():
             with base.dygraph.guard():
-                base._C_ops.sum([None])
+                paddle._C_ops.sum([None])
 
         self.assertRaises(ValueError, test_empty_list_input)
         self.assertRaises(ValueError, test_list_of_none_input)
