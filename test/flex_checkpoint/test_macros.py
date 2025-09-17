b89a50225990aae99fcee913e281316bd1a84055
xingmingyyj <zxm_3791@163.com>
2025-09-17 23:23:10 +0800

    fix test

--- test/flex_checkpoint/test_macros.py
+++ test/flex_checkpoint/test_macros.py
@@ -64,13 +64,13 @@ class MacroContext:
             )
         prefix, suffix = name_with_layer_id.split(layer_id_macro_tag, 1)
         pattern = re.compile(rf"{re.escape(prefix)}(\d+){re.escape(suffix)}")
-        max_layer = 0
+        match_layer_id = set()
         for key in self.get_all_dst_state_keys():
             match = pattern.fullmatch(key)
             if match:
                 layer_num = int(match.group(1))
-                max_layer = max(max_layer, layer_num)
-        return max_layer + 1
+                match_layer_id.add(layer_num)
+        return match_layer_id
 
     def get_src_state_shard_num(self, src_state_key: str) -> int:
         return 2
