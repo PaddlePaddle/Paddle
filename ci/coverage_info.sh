937f804a554e61114ad02723c81ff5585907ab1d
tianshuo78520a <707759223@qq.com>
2025-09-19 08:38:25 +0000

    update

--- ci/coverage_info.sh
+++ ci/coverage_info.sh
@@ -41,6 +41,12 @@ mkdir coverage_files
 
 function gen_full_report() {
     lcov --extract coverage.info \
+	"${PADDLE_ROOT}/paddle/fluid/framework/*" \
+        "${PADDLE_ROOT}/paddle/fluid/imperative/*" \
+        "${PADDLE_ROOT}/paddle/fluid/inference/*" \
+        "${PADDLE_ROOT}/paddle/fluid/memory/*" \
+        "${PADDLE_ROOT}/paddle/fluid/operators/*" \
+        "${PADDLE_ROOT}/paddle/fluid/eager/*" \
         "${PADDLE_ROOT}/paddle/phi/*" \
         "${PADDLE_ROOT}/paddle/pir/*" \
         "${PADDLE_ROOT}/paddle/ap/*" \
