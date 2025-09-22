48c4e616d836e44d112e8ce8878304af47329dcb
fanhaoxuee <1470600398@qq.com>
2025-09-22 16:12:13 +0800

    2nd_batch_36

--- paddle/ap/include/axpr/string_method_class.h
+++ paddle/ap/include/axpr/string_method_class.h
@@ -92,12 +92,19 @@ struct StringMethodClass {
   std::string Replace(std::string self,
                       const std::string& pattern,
                       const std::string& replacement) {
-    while (true) {
-      std::size_t pos = self.find(pattern);
-      if (pos == std::string::npos) {
-        break;
+    if (pattern.empty()) {
+      std::string result;
+      for (char c : self) {
+        result += replacement;
+        result += c;
       }
-      self = self.replace(pos, pattern.size(), replacement);
+      result += replacement;
+      return result;
+    }
+    std::size_t pos = 0;
+    while ((pos = self.find(pattern, pos)) != std::string::npos) {
+      self.replace(pos, pattern.size(), replacement);
+      pos += replacement.size();
     }
     return self;
   }
