0194bf410497179338a12eae47378b084e86b5b4
DanielSun11 <1395924413@qq.com>
2025-09-25 16:09:42 +0000

    fix build error for mac

--- paddle/fluid/platform/init.cc
+++ paddle/fluid/platform/init.cc
@@ -292,7 +292,7 @@ const char *ParseSignalErrorString(const std::string &str) {
 }
 
 // Handle SIGSEGV, SIGILL, SIGFPE, SIGABRT, SIGBUS, and SIGTERM.
-void SignalHandle(const char *data, size_t size) {
+void SignalHandle(const char *data, int size) {
   try {
     // NOTE1: The glog FailureSignalHandler dumped messages
     //   are deal with line by line
