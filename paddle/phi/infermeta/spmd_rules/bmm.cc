e177dc9dd6b86519aab905e84d1b2024ad283cd8
ooooo <3164076421@qq.com>
2025-09-26 22:28:06 +0800

    fix tests

--- paddle/phi/infermeta/spmd_rules/bmm.cc
+++ paddle/phi/infermeta/spmd_rules/bmm.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+/* Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -14,6 +14,8 @@ limitations under the License. */
 #include "glog/logging.h"
 
 #include "paddle/phi/core/distributed/auto_parallel/dist_attr.h"
+#include "paddle/phi/core/distributed/auto_parallel/inferspmd_utils.h"
+#include "paddle/phi/core/distributed/auto_parallel/utils.h"
 #include "paddle/phi/infermeta/spmd_rules/matmul.h"
 #include "paddle/phi/infermeta/spmd_rules/utils.h"
 
