0e83bc85c03f9b89f906cdc116469292627ac0fc
fanhaoxuee <1470600398@qq.com>
2025-09-29 14:10:28 +0800

    929

--- paddle/phi/core/dense_tensor.h
+++ paddle/phi/core/dense_tensor.h
@@ -15,7 +15,9 @@ limitations under the License. */
 #pragma once
 
 #include "paddle/phi/core/allocator.h"
+#include "paddle/phi/core/dense_tensor_meta.h"
 #include "paddle/phi/core/enforce.h"
+#include "paddle/phi/core/lod.h"
 #include "paddle/phi/core/storage_properties.h"
 #include "paddle/phi/core/stream.h"
 #include "paddle/phi/core/tensor_base.h"
