b7421191e8c65d9ba414864d084e3b539c7be68b
youge325 <A372707325@126.com>
2025-10-02 10:07:26 +0800

    [API compatibility] fix bugs when compile with cuda13 on Windows

--- paddle/phi/backends/dynload/dynamic_loader.cc
+++ paddle/phi/backends/dynload/dynamic_loader.cc
@@ -449,9 +449,15 @@ void* GetCublasDsoHandle() {
     return GetDsoHandleFromSearchPath(
         FLAGS_cuda_dir, win_cublas_lib, true, {cuda_lib_path});
 #endif
+  } else if (CUDA_VERSION >= 13000 && CUDA_VERSION < 14000) {
+#ifdef PADDLE_WITH_PIP_CUDA_LIBRARIES
+    return GetDsoHandleFromSearchPath(FLAGS_cublas_dir, "libcublasLt.so.13");
+#else
+    return GetDsoHandleFromSearchPath(FLAGS_cublas_dir, "libcublasLt.so");
+#endif
   } else {
     std::string warning_msg(
-        "Your CUDA_VERSION is less than 11 or greater than 13, paddle "
+        "Your CUDA_VERSION is less than 11 or greater than 14, paddle "
         "temporarily no longer supports");
     return nullptr;
   }
