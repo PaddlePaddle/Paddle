29b16f11c506823ed7d23a4d0d6d03f7610b1484
nyx-c-language <nyxchaoji123@163.com>
2025-10-03 18:16:07 +0800

    fix 启航21 ap_facade_kernel

--- paddle/phi/kernels/ap_facade_kernel.h
+++ paddle/phi/kernels/ap_facade_kernel.h
@@ -1,7 +1,7 @@
-// Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/common/enforce.h"
+#pragma once
+
 #include "paddle/phi/backends/gpu/gpu_context.h"
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/ap_facade_kernel.h"
 
 namespace phi {
 
@@ -28,22 +28,6 @@ void ApFacadeKernel(const Context& dev_ctx,
                     const std::string& infer_meta_func_name,
                     const std::string& infer_symbolic_func_name,
                     const std::string& serialized_attributes,
-                    std::vector<DenseTensor*> outs) {
-  PADDLE_THROW(
-      common::errors::Unimplemented("ap_facade has no kernel registered."));
-}
+                    std::vector<DenseTensor*> outs);
 
 }  // namespace phi
-
-PD_REGISTER_KERNEL(ap_facade,
-                   GPU,
-                   ALL_LAYOUT,
-                   phi::ApFacadeKernel,
-                   float,
-                   double,
-                   int,
-                   phi::bfloat16,
-                   phi::float16,
-                   int64_t,
-                   phi::complex64,
-                   phi::complex128) {}
