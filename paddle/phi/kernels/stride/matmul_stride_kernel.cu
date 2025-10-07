a005f243477c918fada34df6e662891d26f0dccb
Eddie-Wang1120 <wangjinheng1120@163.com>
2025-10-07 01:01:07 +0000

    init

--- paddle/phi/kernels/stride/matmul_stride_kernel.cu
+++ paddle/phi/kernels/stride/matmul_stride_kernel.cu
@@ -111,21 +111,21 @@ void MatmulStrideKernel(const Context &dev_ctx,
   DenseTensor x_;
   DenseTensor y_;
 
-  if (!FLAGS_use_stride_compute_kernel) {
-    if (!x.meta().is_contiguous()) {
-      x_ = Tensor2Contiguous<Context>(dev_ctx, x);
-    } else {
-      x_ = x;
-    }
-    if (!y.meta().is_contiguous()) {
-      y_ = Tensor2Contiguous<Context>(dev_ctx, y);
-    } else {
-      y_ = y;
-    }
+  // if (!FLAGS_use_stride_compute_kernel) {
+  if (!x.meta().is_contiguous()) {
+    x_ = Tensor2Contiguous<Context>(dev_ctx, x);
   } else {
     x_ = x;
+  }
+  if (!y.meta().is_contiguous()) {
+    y_ = Tensor2Contiguous<Context>(dev_ctx, y);
+  } else {
     y_ = y;
   }
+  // } else {
+  //   x_ = x;
+  //   y_ = y;
+  // }
 
   if (x_.meta().is_contiguous() && y_.meta().is_contiguous()) {
     auto meta = out->meta();
@@ -136,68 +136,69 @@ void MatmulStrideKernel(const Context &dev_ctx,
     return;
   }
 
-  if (!FLAGS_use_stride_compute_kernel) {
-    PADDLE_THROW(
-        common::errors::Fatal("FLAGS_use_stride_compute_kernel is closed. "
-                              "Kernel using DenseTensorIterator "
-                              "be called, something wrong has happened!"));
-  }
+  // if (!FLAGS_use_stride_compute_kernel) {
+  //   PADDLE_THROW(
+  //       common::errors::Fatal("FLAGS_use_stride_compute_kernel is closed. "
+  //                             "Kernel using DenseTensorIterator "
+  //                             "be called, something wrong has happened!"));
+  // }
 
-  auto x_meta = x.meta();
-  DDim x_stride = x_meta.strides;
-  DDim x_shape = x_meta.dims;
-  std::vector<int> x_axis;
-  auto y_meta = y.meta();
-  DDim y_stride = y_meta.strides;
-  DDim y_shape = y_meta.dims;
-  std::vector<int> y_axis;
+  // auto x_meta = x.meta();
+  // DDim x_stride = x_meta.strides;
+  // DDim x_shape = x_meta.dims;
+  // std::vector<int> x_axis;
+  // auto y_meta = y.meta();
+  // DDim y_stride = y_meta.strides;
+  // DDim y_shape = y_meta.dims;
+  // std::vector<int> y_axis;
 
-  if (!x.meta().is_contiguous() && is_only_transposed_tensor(x_meta.dims,
-                                                             x_meta.strides,
-                                                             x_meta.offset,
-                                                             &x_shape,
-                                                             &x_stride,
-                                                             &x_axis)) {
-    auto x_trans_dims = x_axis.size();
-    if (x_axis[x_trans_dims - 1] == x_trans_dims - 2 &&
-        x_axis[x_trans_dims - 2] == x_trans_dims - 1) {
-      transpose_x = !transpose_x;
-      x_meta.dims = x_shape;
-      x_meta.strides = x_stride;
-      x_meta.offset = x.offset();
-      x_.set_meta(x_meta);
-    }
-  }
+  // if (!x.meta().is_contiguous() && is_only_transposed_tensor(x_meta.dims,
+  //                                                            x_meta.strides,
+  //                                                            x_meta.offset,
+  //                                                            &x_shape,
+  //                                                            &x_stride,
+  //                                                            &x_axis)) {
+  //   auto x_trans_dims = x_axis.size();
+  //   if (x_axis[x_trans_dims - 1] == x_trans_dims - 2 &&
+  //       x_axis[x_trans_dims - 2] == x_trans_dims - 1) {
+  //     transpose_x = !transpose_x;
+  //     x_meta.dims = x_shape;
+  //     x_meta.strides = x_stride;
+  //     x_meta.offset = x.offset();
+  //     x_.set_meta(x_meta);
+  //   }
+  // }
 
-  if (!x_.meta().is_contiguous()) {
-    x_ = Tensor2Contiguous<Context>(dev_ctx, x);
-  }
+  // if (!x_.meta().is_contiguous()) {
+  //   x_ = Tensor2Contiguous<Context>(dev_ctx, x);
+  // }
 
-  if (!y.meta().is_contiguous() && is_only_transposed_tensor(y_meta.dims,
-                                                             y_meta.strides,
-                                                             y_meta.offset,
-                                                             &y_shape,
-                                                             &y_stride,
-                                                             &y_axis)) {
-    auto y_trans_dims = y_axis.size();
-    if (y_axis[y_trans_dims - 1] == y_trans_dims - 2 &&
-        y_axis[y_trans_dims - 2] == y_trans_dims - 1) {
-      transpose_y = !transpose_y;
-      y_meta.dims = y_shape;
-      y_meta.strides = y_stride;
-      y_meta.offset = y.offset();
-      y_.set_meta(y_meta);
-    }
-  }
+  // if (!y.meta().is_contiguous() && is_only_transposed_tensor(y_meta.dims,
+  //                                                            y_meta.strides,
+  //                                                            y_meta.offset,
+  //                                                            &y_shape,
+  //                                                            &y_stride,
+  //                                                            &y_axis)) {
+  //   auto y_trans_dims = y_axis.size();
+  //   if (y_axis[y_trans_dims - 1] == y_trans_dims - 2 &&
+  //       y_axis[y_trans_dims - 2] == y_trans_dims - 1) {
+  //     transpose_y = !transpose_y;
+  //     y_meta.dims = y_shape;
+  //     y_meta.strides = y_stride;
+  //     y_meta.offset = y.offset();
+  //     y_.set_meta(y_meta);
+  //   }
+  // }
 
-  if (!y_.meta().is_contiguous()) {
-    y_ = Tensor2Contiguous<Context>(dev_ctx, y);
-  }
+  // if (!y_.meta().is_contiguous()) {
+  //   y_ = Tensor2Contiguous<Context>(dev_ctx, y);
+  // }
 
-  auto meta = out->meta();
-  meta.strides = meta.calc_strides(out->dims());
-  out->set_meta(meta);
-  phi::MatmulKernel<T, Context>(dev_ctx, x_, y_, transpose_x, transpose_y, out);
+  // auto meta = out->meta();
+  // meta.strides = meta.calc_strides(out->dims());
+  // out->set_meta(meta);
+  // phi::MatmulKernel<T, Context>(dev_ctx, x_, y_, transpose_x, transpose_y,
+  // out);
 }
 
 }  // namespace phi
