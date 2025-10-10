4dbe344882e9310a3aa6c2867955621d00727276
Yizhan <yizhanhuang2002@gmail.com>
2025-10-10 02:22:09 -0700

    fix: add new TestSigmoidFp32_Comp to verify forward decomposition correctness   of sigmoid under FP32

--- test/legacy_test/test_activation_op.py
+++ test/legacy_test/test_activation_op.py
@@ -548,6 +548,46 @@ class TestSigmoidBF16(OpTest):
         )
 
 
+class TestSigmoidFp32_Comp(OpTest):
+    def setUp(self):
+        self.op_type = "sigmoid"
+        self.prim_op_type = "comp"
+        self.python_api = paddle.nn.functional.sigmoid
+        self.public_python_api = paddle.nn.functional.sigmoid
+        self.init_dtype()
+        self.init_shape()
+        self.if_enable_cinn()
+        np.random.seed(1024)
+        x = np.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        out = 1.0 / (1.0 + np.exp(-x))
+
+        self.inputs = {'X': OpTest.np_dtype_to_base_dtype(x)}
+        self.outputs = {'Out': out}
+
+    def test_check_output(self):
+        self.check_output(check_pir=True, check_symbol_infer=False)
+
+    def test_check_grad(self):
+        self.check_grad(
+            ['X'],
+            'Out',
+            check_prim=False,
+            check_pir=True,
+            check_prim_pir=False,
+            max_relative_error=1e-2,
+            numeric_grad_delta=2e-2,
+        )
+
+    def init_dtype(self):
+        self.dtype = np.float32
+
+    def init_shape(self):
+        self.shape = [11, 17]
+
+    def if_enable_cinn(self):
+        self.enable_cinn = False
+
+
 '''
 class TestSigmoidBF16_ZeroDim(TestSigmoidBF16):
 
