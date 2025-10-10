e14d38eb35f1560e24e095cd8f9b67587b7a746d
youge325 <A372707325@126.com>
2025-10-10 15:55:05 +0800

    fix index path

--- paddle/phi/kernels/gpu/collect_fpn_proposals_kernel.cu
+++ paddle/phi/kernels/gpu/collect_fpn_proposals_kernel.cu
@@ -25,12 +25,12 @@ namespace cub = hipcub;
 #include "paddle/phi/core/allocator.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/core/mixed_vector.h"
-#include "paddle/phi/kernels/collect_fpn_proposals_kernel.h"
 #include "paddle/phi/kernels/funcs/concat_and_split_functor.h"
 #include "paddle/phi/kernels/funcs/detection/bbox_util.h"
 #include "paddle/phi/kernels/funcs/for_range.h"
 #include "paddle/phi/kernels/funcs/gather.cu.h"
 #include "paddle/phi/kernels/funcs/strided_memcpy.h"
+#include "paddle/phi/kernels/gpu/collect_fpn_proposals_kernel.h"
 #include "paddle/phi/kernels/impl/collect_fpn_proposals_kernel_impl.h"
 #include "paddle/utils/optional.h"
 
