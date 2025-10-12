93e04da11b78d8582ed02b138b824ea193992f21
ApricityXX <1470600398@qq.com>
2025-10-12 15:03:05 +0800

    1012

--- test/auto_parallel/dtensor_to_local_api.py
+++ test/auto_parallel/dtensor_to_local_api.py
@@ -53,6 +53,7 @@ class TestDtensorToLocalAPI:
         tensor3.register_hook(self.check_grad_mesh(None, None))
         tensor3.backward()
 
+    #
     def check_grad_mesh(self, org_mesh, org_placements):
         def _check_mesh(grad):
             if hasattr(grad, "process_mesh") and hasattr(grad, "placements"):
