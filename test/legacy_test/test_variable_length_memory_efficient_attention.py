f97aa4d70e5f489815015e60fe1c6b0ef6167643
co63oc <4617245+co63oc@users.noreply.github.com>
2025-10-15 07:11:27 +0800

    fix

--- test/legacy_test/test_variable_length_memory_efficient_attention.py
+++ test/legacy_test/test_variable_length_memory_efficient_attention.py
@@ -96,8 +96,9 @@ def naive_attention_impl(query, key, value, mask, scale):
 
 
 @unittest.skipIf(
-    not (core.is_compiled_with_cuda() or is_custom_device()),
-    "core is not compiled with CUDA",
+    not (core.is_compiled_with_cuda() or is_custom_device())
+    or get_cuda_version() < 11020,
+    "core is not compiled with CUDA and cuda version need larger than or equal to 11.2",
 )
 class TestMemEffAttentionVariableAPI(unittest.TestCase):
     def setUp(self):
@@ -217,6 +218,7 @@ class TestMemEffAPIVariableDtypeFP16(TestMemEffAttentionVariableAPI):
 
 @unittest.skipIf(
     not (core.is_compiled_with_cuda() or is_custom_device())
+    or get_cuda_version() < 11020
     or get_cuda_arch() < 8,
     "MemEffAPIVariableDtypeBF16 requires CUDA >= 11.2 and CUDA_ARCH >= 8",
 )
@@ -261,8 +263,9 @@ class TestMemEffAPIVariableDtypeBF16(TestMemEffAttentionVariableAPI):
 
 
 @unittest.skipIf(
-    not (core.is_compiled_with_cuda() or is_custom_device()),
-    "core is not compiled with CUDA",
+    not (core.is_compiled_with_cuda() or is_custom_device())
+    or get_cuda_version() < 11020,
+    "core is not compiled with CUDA and cuda version need larger than or equal to 11.2",
 )
 class TestMemEffAPIVariableDtypeFP16Static(unittest.TestCase):
     def setUp(self):
@@ -355,8 +358,9 @@ class TestMemEffAPIVariableDtypeFP16Static(unittest.TestCase):
 
 
 @unittest.skipIf(
-    not (core.is_compiled_with_cuda() or is_custom_device()),
-    "core is not compiled with CUDA",
+    not (core.is_compiled_with_cuda() or is_custom_device())
+    or get_cuda_version() < 11020,
+    "core is not compiled with CUDA and cuda version need larger than or equal to 11.2",
 )
 class TestMemEffAttentionVariableAPI_ZeroSize(unittest.TestCase):
     def setUp(self):
