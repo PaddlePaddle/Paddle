b6625008e87e0a5a7f807177179c4b0e9f5248ef
fanhaoxuee <1470600398@qq.com>
2025-10-15 09:35:12 +0800

    1015

--- paddle/fluid/eager/api/manual/eager_manual/forwards/dtensor_from_local_fwd_func.cc
+++ paddle/fluid/eager/api/manual/eager_manual/forwards/dtensor_from_local_fwd_func.cc
@@ -97,12 +97,14 @@ paddle::Tensor dtensor_from_local_ad_function(
     egr::EagerUtils::PassStopGradient(false, out_autograd_meta);
 
     // SetGradOutMeta & SetEdges
+    // SetGradOutMeta & SetEdge replacement
     if (input_autograd_meta) {
       grad_node->SetGradOutMeta(input, 0);
-      egr::EagerUtils::SetEdge(input_autograd_meta, grad_node);
-    }else{
+      egr::EagerUtils::AddEdgeBetween(input_autograd_meta, grad_node);
+    } else {
       grad_node->SetGradOutMeta(input, 0);
     }
+
     // SetOutRank & SetHistory & SetGradInMeta
     if (out_autograd_meta) {
       egr::EagerUtils::SetOutRankWithSlot(out_autograd_meta, 0);
