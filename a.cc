7909f1a1d1cf7acf74ae86eb6f6c4f3778144491
tianshuo78520a <707759223@qq.com>
2025-10-15 01:23:39 +0000

    update

--- a.cc
+++ a.cc
@@ -1,5 +1,4 @@
 int* ptr = new int(42);
-
 int* arr = new int[10];
 
 class MyClass {
--- ci/check_approval.sh
+++ ci/check_approval.sh
@@ -551,7 +551,7 @@ fi
 
 USE_NEW=`git diff -- '*.h' '*.cc' '*.cuh' '*.cu' | grep -w "new" | grep -v "//" || true`
 USE_MALLOC=`git diff -- '*.h' '*.c' | grep -w "malloc(" | grep -v "//" || true`
-if [[ "${USE_NEW}" || "${USE_MALLOC}" ]];then
+if [[ -n "${USE_NEW}" ]] || [[ -n "${USE_MALLOC}" ]];then
     echo_line="You must have one RD (phlrain or sneaxiy) approval you used new or malloc in a C++ function"
     check_approval 1 phlrain sneaxiy
 fi
