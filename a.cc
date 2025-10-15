b3e251ab2d668a32696e63a21f06cff421f4c1ab
tianshuo78520a <707759223@qq.com>
2025-10-15 01:16:42 +0000

    update

--- a.cc
+++ a.cc
@@ -1,14 +1,11 @@
-// 创建单个整数
 int* ptr = new int(42);
 
-// 创建数组
 int* arr = new int[10];
 
-// 创建对象
 class MyClass {
 public:
-    MyClass() { std::cout << "构造函数\n"; }
-    ~MyClass() { std::cout << "析构函数\n"; }
+    MyClass() { std::cout << "constructor\n"; }
+    ~MyClass() { std::cout << "destructor\n"; }
 };
 
 MyClass* obj = new MyClass();
