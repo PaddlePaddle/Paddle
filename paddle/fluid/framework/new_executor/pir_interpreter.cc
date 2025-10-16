0c3e4397342da0e3dfc00f8edc84168745e76e22
DrRyanHuang <zihaohuang@aliyun.com>
2025-10-16 11:08:17 +0800

    refine exception log info

--- paddle/fluid/framework/new_executor/pir_interpreter.cc
+++ paddle/fluid/framework/new_executor/pir_interpreter.cc
@@ -2038,7 +2038,7 @@ void PirInterpreter::RunInstructionBase(InstructionBase* instr_node) {
     exception_holder_.Catch(std::current_exception());
   } catch (std::exception& ex) {
     LOG(WARNING) << instr_node->Name() << " raises an exception "
-                 << common::demangle(typeid(ex).name());
+                 << common::demangle(typeid(ex).name()) << ": " << ex.what();
     exception_holder_.Catch(std::current_exception());
   } catch (...) {
     LOG(WARNING) << instr_node->Name() << " raises an unknown exception";
