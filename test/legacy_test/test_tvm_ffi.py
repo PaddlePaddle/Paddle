cae11a2ebafdb96cd545de476d0ac8b9b9cdb4be
SigureMo <sigure.qaq@gmail.com>
2025-10-17 05:06:23 +0800

    bump b20 and remove another windows skip

--- test/legacy_test/test_tvm_ffi.py
+++ test/legacy_test/test_tvm_ffi.py
@@ -14,7 +14,6 @@
 
 from __future__ import annotations
 
-import platform
 import unittest
 from typing import TYPE_CHECKING
 
@@ -75,9 +74,6 @@ class TestCDLPackExchangeAPI(unittest.TestCase):
             return
         if paddle.is_compiled_with_rocm():
             # Skip on DCU because CUDA_HOME is not available
-            return
-        if platform.system() == "Windows":
-            # Temporary skip this test case on windows because compile bug on TVM FFI
             return
         cpp_sources = r"""
             void add_one_cuda(tvm::ffi::TensorView x, tvm::ffi::TensorView y);
