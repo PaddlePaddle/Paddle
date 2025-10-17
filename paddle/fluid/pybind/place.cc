f513e5f78fd269b9b0a225b638497373b74d5e0c
ice <offical@byterain.co>
2025-10-17 12:29:46 +0800

    fix: build error

--- paddle/fluid/pybind/place.cc
+++ paddle/fluid/pybind/place.cc
@@ -308,7 +308,7 @@ void BindPlace(pybind11::module &m) {  // NOLINT
                    phi::DeviceManager::GetDeviceCount(device_type));
                if (UNLIKELY(dev_id >= dev_count)) {
                  if (dev_count == 0) {
-                   if (defined(PADDLE_WITH_CUDA)) {
+                   #if defined(PADDLE_WITH_CUDA)
                      LOG(ERROR) << "Cannot use " << device_type
                                 << " because there is no " << device_type
                                 << " detected on your machine."
@@ -317,11 +317,12 @@ void BindPlace(pybind11::module &m) {  // NOLINT
                                 << "Device type: " << device_type
                                 << ", CUDA_VISIBLE_DEVICES: "
                                 << std::getenv("CUDA_VISIBLE_DEVICES")
-                   } else {
+                   #else
                      LOG(ERROR) << "Cannot use " << device_type
                                 << " because there is no " << device_type
                                 << " detected on your machine.";
                    }
+                   #endif
                    PADDLE_THROW(::common::errors::InvalidArgument(
                        "use wrong place, Please check."));
                  } else {
