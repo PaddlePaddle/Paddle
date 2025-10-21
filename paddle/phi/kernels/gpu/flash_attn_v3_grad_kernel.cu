e14a08ed85b69f14dc6f51a9c0af2e978d0254ff
umiswing <umiswing@foxmail.com>
2025-10-21 22:17:04 +0800

    fix codestyle

--- paddle/phi/kernels/gpu/flash_attn_v3_grad_kernel.cu
+++ paddle/phi/kernels/gpu/flash_attn_v3_grad_kernel.cu
@@ -1026,9 +1026,7 @@ void FlashMaskV2GradBaseKernel(
                 ? 64
                 : (is_causal && softcap || is_flashmask > 0.0 ? 96 : 128)
           : (head_size_rounded <= 128
-                 ? (is_causal || is_local || softcap > 0.0
-                        ? 64
-                        : 64)
+                 ? (is_causal || is_local || softcap > 0.0 ? 64 : 64)
                  : 64);
 
   int const kBlockM_sm80 = head_size_rounded <= 64 ? 128 : 64;
@@ -1038,8 +1036,10 @@ void FlashMaskV2GradBaseKernel(
                  : (arch == 86 || arch == 89 ? kBlockM_sm86 : kBlockM_sm80);
   int const kBlockN_sm90 =
       head_size_rounded <= 64 && (is_flashmask && !is_causal) ? 96
-      : head_size_rounded <= 128 ? (is_causal || is_local || softcap > 0.f || seqlen_q >= 8192 ? 128 : 64)
-                                 : (head_size_rounded <= 192 ? 96 : 80);
+      : head_size_rounded <= 128
+          ? (is_causal || is_local || softcap > 0.f || seqlen_q >= 8192 ? 128
+                                                                        : 64)
+          : (head_size_rounded <= 192 ? 96 : 80);
   int const kBlockN_sm80 =
       head_size_rounded <= 128 ? 128 : (head_size_rounded <= 192 ? 80 : 64);
   int const kBlockN_sm86 =
