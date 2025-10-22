a24e9f96d8a60ed308040931731dcc1e96545752
Le-soleile <3516093767@qq.com>
2025-10-22 11:45:47 +0000

    Fix

--- paddle/phi/kernels/gpu/lookup_table_grad_kernel.cu
+++ paddle/phi/kernels/gpu/lookup_table_grad_kernel.cu
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "paddle/phi/kernels/lookup_table_grad_kernel.h"
+#include "paddle/phi/kernels/gpu/lookup_table_grad_kernel.h"
 #include "paddle/phi/backends/gpu/gpu_primitives.h"
 #include "paddle/phi/common/memory_utils.h"
 #include "paddle/phi/core/kernel_registry.h"
