46c20d8fe33fca933d19d49c249f89ba38e93e50
ooooo <3164076421@qq.com>
2025-10-26 02:45:51 +0800

    [CI] Add new GPU kernel interception

---
+++ ci/check_gpu_kernel_approval.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+pr_whl_path=${PADDLE_ROOT}/build/pr_whl/
+dev_whl_path=${PADDLE_ROOT}/build/python/dist/
+approval_line=$(curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/Paddle/pulls/${GIT_PR_ID}/reviews?per_page=10000)
+
+# Install dev wheel
+pip install "${dev_whl_path}"/*.whl
+python -c 'import paddle;import json;print(json.dumps(paddle.base.core._get_all_register_op_kernels("phi"), indent=4))' > dev_phi_kernels.json
+# Install pr wheel
+pip install "${pr_whl_path}"/*.whl
+python -c 'import paddle;import json;print(json.dumps(paddle.base.core._get_all_register_op_kernels("phi"), indent=4))' > pr_phi_kernels.json
+
+
+if ! python "${PADDLE_ROOT}/ci/gpu_kernel_compare.py" dev_phi_kernels.json pr_phi_kernels.json; then
+  APPROVALS=$(echo "${approval_line}"|python "${PADDLE_ROOT}"/tools/check_pr_approval.py 1 wanghuancoder)
+  if [[ "${APPROVALS}" == "FALSE" ]]; then
+	echo "**************************************************************"
+	echo "Please ensure the added GPU kernel supports large tensors, defined as those with a number of elements (numel) greater than 2^31 - 1. In the PR description, please describe how you tested this scenario and validated the data accuracy."
+	echo "You must have one RD (wanghuancoder) approval"
+	echo "**************************************************************"
+	exit 9
+  fi
+fi
