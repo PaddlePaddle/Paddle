644eaccc7c00a6e52f4613f479d286ec0991eec6
Le-soleile <3516093767@qq.com>
2025-10-29 07:08:44 +0000

    Fix ci

--- paddle/phi/kernels/cpu/lookup_table_grad_kernel.cc
+++ paddle/phi/kernels/cpu/lookup_table_grad_kernel.cc
@@ -14,8 +14,8 @@
 
 #include <string>
 #include <vector>
-#include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/backends/cpu/cpu_context.h"
+#include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/enforce.h"
 #include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/funcs/blas/blas.h"
