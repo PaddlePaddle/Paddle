eaf09353073b9d579ce62bf614461ac7d7e368ae
huangjiyi <947613776@qq.com>
2025-10-30 10:29:56 +0800

    support __ne__ for unsupported type

--- paddle/fluid/pybind/eager_math_op_patch.cc
+++ paddle/fluid/pybind/eager_math_op_patch.cc
@@ -2190,8 +2190,16 @@ static PyObject* tensor__ne__method(TensorObject* self,
       other_tensor = paddle::empty({}, phi::DataType::FLOAT32, place);
       InitTensorWithNumpyValue(numpy_value, place, &other_tensor);
     } else {
-      paddle::experimental::Scalar value =
-          CastPyArg2Scalar(other_obj, "__ne__", 0);
+      paddle::experimental::Scalar value;
+
+      // return True if other_obj is unsupported type
+      try {
+        value = CastPyArg2Scalar(other_obj, "__ne__", 0);
+      } catch (const ::common::enforce::EnforceNotMet& e) {
+        Py_INCREF(Py_True);
+        return Py_True;
+      }
+
       if (PyComplex_Check(other_obj)) {
         eager_gil_scoped_release guard;
         other_tensor =
