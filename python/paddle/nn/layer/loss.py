855657b3c520cec1c3c964986d68ebda057052c9
ice <offical@byterain.co>
2025-10-31 18:25:33 +0800

    Add Optional import to loss.py

--- python/paddle/nn/layer/loss.py
+++ python/paddle/nn/layer/loss.py
@@ -14,7 +14,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING, Callable, Optional
 
 import paddle
 from paddle import base, in_dynamic_mode
