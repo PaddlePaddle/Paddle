16dbe112ebb6a041b4d4cd663c8191f52bbb617a
wenfei qi <ms1419344875@outlook.com>
2025-11-05 16:55:55 -0800

    Move unittest from python/unittest_py/ to test/tensor and remove old test files

--- test/tensor/test_empty_tensor_indexing.py
+++ test/tensor/test_empty_tensor_indexing.py
@@ -12,31 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import paddle
-
-# cum_nodes = paddle.zeros([2, ])
-# batch = paddle.empty([0,], dtype=paddle.int64)
-# edge_index = paddle.empty([2, 0], dtype=paddle.int64)
-
-# print(edge_index)
-
-# print(cum_nodes[batch][edge_index[0]])
-# print(cum_nodes[batch][edge_index[1]])
-
-# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import unittest
 
 import paddle
