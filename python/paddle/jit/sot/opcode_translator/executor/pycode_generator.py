5f93e3c9ca84bd36b812f1a24846b1e636fb0587
gouzi <530971494@qq.com>
2025-11-07 11:17:03 +0800

    [SOT][3.14] use `NB_SUBSCR` replace `BINARY_SUBSCR`

--- python/paddle/jit/sot/opcode_translator/executor/pycode_generator.py
+++ python/paddle/jit/sot/opcode_translator/executor/pycode_generator.py
@@ -805,7 +805,11 @@ class PyCodeGen:
         return self.add_instr("STORE_SUBSCR")
 
     def gen_subscribe(self):
-        return self.add_instr("BINARY_SUBSCR")
+        if sys.version_info < (3, 14):
+            return self.add_instr("BINARY_SUBSCR")
+        # see: https://docs.python.org/3.14/library/dis.html#opcode-BINARY_OP and
+        # https://github.com/python/cpython/blob/0e46c0499413bc5f9f8336fe76e2e67cf93f64d8/Include/opcode.h#L36
+        return self.add_instr("BINARY_OP", arg=26)
 
     def gen_build_tuple(self, count):
         return self.add_instr("BUILD_TUPLE", arg=count, argval=count)
