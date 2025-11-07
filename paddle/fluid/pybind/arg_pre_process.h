90e291a3403a47f185cc98e0803741640694f997
DanielSun11 <1395924413@qq.com>
2025-11-07 06:07:54 +0000

    Revert "rm unused code"
    This reverts commit 0523ebbe4a2d26db9103322245defb67a2e20045.
    
    e

--- paddle/fluid/pybind/arg_pre_process.h
+++ paddle/fluid/pybind/arg_pre_process.h
@@ -43,6 +43,7 @@ void LogsumexpPreProcess(Tensor* x, std::vector<int>* axis, bool* reduce_all);
 void LogsumexpPreProcess(Value* x, std::vector<int>* axis, bool* reduce_all);
 
 void SumPreProcess(Value* x, Value* axis);
+void IsClosePreProcess(Value* x, Value* y, Value* rtol, Value* atol);
 }  // namespace pybind
 
 }  // namespace paddle
