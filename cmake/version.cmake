6e7bd1947c18c3fdc602c5117d13ebf126f112bf
gushiwei <gushiwei@baidu.com>
2025-11-08 10:04:22 +0000

    version

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,17 +1,21 @@
 # Get the latest git tag.
 set(PADDLE_VERSION $ENV{PADDLE_VERSION})
-if(WITH_NIGHTLY_BUILD)
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} show -s --format=%ci HEAD
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_COMMIT_TIME
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-  string(REGEX REPLACE " (.*)$" "" DATE_ONLY "${GIT_COMMIT_TIME}")
-  string(REPLACE "-" "" DATE_ONLY "${DATE_ONLY}")
-  # Print the last commit date
-  message(STATUS "Last commit date: ${DATE_ONLY}")
+
+execute_process(
+  COMMAND ${GIT_EXECUTABLE} show -s --format=%ci HEAD
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  OUTPUT_VARIABLE GIT_COMMIT_TIME
+  OUTPUT_STRIP_TRAILING_WHITESPACE)
+string(REGEX REPLACE " (.*)$" "" DATE_ONLY "${GIT_COMMIT_TIME}")
+string(REPLACE "-" "" DATE_ONLY "${DATE_ONLY}")
+# Print the last commit date
+message(STATUS "Last commit date: ${DATE_ONLY}")
+if(NOT PADDLE_VERSION)
+  set(PADDLE_VERSION "dev${DATE_ONLY}")
+else()
   set(PADDLE_VERSION "${PADDLE_VERSION}.dev${DATE_ONLY}")
 endif()
+
 set(tmp_version "HEAD")
 set(TAG_VERSION_REGEX "[0-9]+\\.[0-9]+\\.[0-9]+(\\.(a|b|rc)\\.[0-9]+)?")
 set(COMMIT_VERSION_REGEX "[0-9a-f]+[0-9a-f]+[0-9a-f]+[0-9a-f]+[0-9a-f]+")
