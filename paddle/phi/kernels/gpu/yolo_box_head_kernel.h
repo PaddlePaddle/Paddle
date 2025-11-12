c504efaec774f170ff9ffde6bdbe49eb3d8a350e
Le-soleile <3516093767@qq.com>
2025-11-12 10:27:54 +0000

    fix

--- paddle/phi/kernels/gpu/yolo_box_head_kernel.h
+++ paddle/phi/kernels/gpu/yolo_box_head_kernel.h
@@ -15,9 +15,9 @@
 #pragma once
 
 #include <vector>
-
 #include "paddle/phi/core/dense_tensor.h"
 #include "paddle/phi/core/kernel_registry.h"
+
 namespace phi {
 
 // Function declaration for the YOLO box head kernel
