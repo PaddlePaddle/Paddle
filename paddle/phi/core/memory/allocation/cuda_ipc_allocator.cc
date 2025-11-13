aaa422e834eb865667accf84f190e0841c4141a3
fxyfxy777 <137464345+fxyfxy777@users.noreply.github.com>
2025-11-13 15:18:39 +0800

    Update cuda_ipc_allocator.cc

--- paddle/phi/core/memory/allocation/cuda_ipc_allocator.cc
+++ paddle/phi/core/memory/allocation/cuda_ipc_allocator.cc
@@ -71,14 +71,18 @@ void IpcCollect() {
   VLOG(6) << "The number of IPC handles before collection:" << before;
 
   for (auto it = ipc_handle_to_baseptr_.begin();
-       it != ipc_handle_to_baseptr_.end();
-       ++it) {
-    if (it->second.expired()) it = ipc_handle_to_baseptr_.erase(it);
+       it != ipc_handle_to_baseptr_.end();) {
+    if (it->second.expired()) {
+      it = ipc_handle_to_baseptr_.erase(it);
+    } else {
+      VLOG(6) << " Valid ipc handle is not expired";
+      ++it;
+    }
   }
 
   size_t after = ipc_handle_to_baseptr_.size();
   size_t collected = before - after;
-  VLOG(6) << "IpcCollect: collected " << collected << " expired IPC handles"
+  VLOG(1) << "IpcCollect: collected " << collected << " expired IPC handles"
           << "out of " << before << " total handles";
 }
 
