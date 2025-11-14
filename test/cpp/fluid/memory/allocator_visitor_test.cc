9797179ebc276c4a19efd69a83e49d01182be561
liujinnan <1823192871@qq.com>
2025-11-14 14:12:26 +0800

    fix ci

--- test/cpp/fluid/memory/allocator_visitor_test.cc
+++ test/cpp/fluid/memory/allocator_visitor_test.cc
@@ -11,21 +11,28 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #include "paddle/phi/core/memory/allocation/allocator.h"
-#include "paddle/phi/core/memory/allocation/naive_best_fit_allocator.h"
-#include "paddle/phi/core/memory/mem_visitor.h"
+#include "paddle/phi/core/memory/allocation/cuda_virtual_mem_allocator.h"
+#include "paddle/phi/core/memory/allocation/retry_allocator.h"
+#include "paddle/phi/core/memory/allocation/virtual_memory_auto_growth_best_fit_allocator.h"
 #include "paddle/phi/core/platform/device/gpu/gpu_info.h"
-
+#ifdef PADDLE_WITH_CUDA
+#include <cuda.h>
+#include <cuda_runtime.h>
+#endif
 #include "gtest/gtest.h"
 namespace paddle {
 namespace memory {
 namespace allocation {
 
 TEST(VirtualMemoryAutoGrowthBestFitAllocator, TestAllocatorVisitor) {
-  NaiveBestFitAllocator alloc{phi::CPUPlace()};
-  memory::AllocatorVisitor free_memory_metrics_visitor;
-  alloc.Accept(&free_memory_metrics_visitor);
+  auto vmm_cuda_allocator =
+      std::make_shared<CUDAVirtualMemAllocator>(phi::GPUPlace());
+  auto vma_allocator =
+      std::make_shared<VirtualMemoryAutoGrowthBestFitAllocator>(
+          vmm_cuda_allocator, platform::GpuMinChunkSize(), phi::GPUPlace());
+  memory::AllocatorVisitor visitor;
+  vma_allocator->Accept(&visitor);
 }
 
 }  // namespace allocation
