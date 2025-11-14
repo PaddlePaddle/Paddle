0f7406b4b60522881ce16ed4f1339632a8d7c78d
ooooo <3164076421@qq.com>
2025-11-14 10:08:57 +0000

    fix tests

--- test/legacy_test/test_compat_avg_pool_api.py
+++ test/legacy_test/test_compat_avg_pool_api.py
@@ -16,201 +16,11 @@ import unittest
 
 import numpy as np
 from op_test import get_places
+from test_pool1d_api import avg_pool1D_forward_naive
+from test_pool2d_api import avg_pool2D_forward_naive
+from test_pool3d_op import avg_pool3D_forward_naive
 
 import paddle
-
-
-def avg_pool1d_forward_naive(
-    x,
-    kernel_size,
-    stride,
-    padding,
-    ceil_mode=False,
-    count_include_pad=True,
-):
-    N, C, L_in = x.shape
-    L_out = (
-        (L_in + 2 * padding[0] - kernel_size[0] + stride[0] - 1) // stride[0]
-        + 1
-        if ceil_mode
-        else (L_in + 2 * padding[0] - kernel_size[0]) // stride[0] + 1
-    )
-
-    out = np.zeros((N, C, L_out))
-    for i in range(L_out):
-        l_start = i * stride[0] - padding[0]
-        l_end = l_start + kernel_size[0]
-
-        x_masked = np.zeros((N, C, kernel_size[0]))
-        for n in range(N):
-            for c in range(C):
-                for l in range(kernel_size[0]):
-                    if l_start + l >= 0 and l_start + l < L_in:
-                        x_masked[n, c, l] = x[n, c, l_start + l]
-
-        if count_include_pad:
-            field_size = kernel_size[0]
-        else:
-            field_size = np.sum(
-                (np.arange(l_start, l_end) >= 0)
-                & (np.arange(l_start, l_end) < L_in)
-            )
-            if field_size == 0:
-                field_size = 1
-
-        out[:, :, i] = np.sum(x_masked, axis=2) / field_size
-    return out
-
-
-def avg_pool2d_forward_naive(
-    x,
-    kernel_size,
-    stride,
-    padding,
-    ceil_mode=False,
-    count_include_pad=True,
-    divisor_override=None,
-):
-    N, C, H_in, W_in = x.shape
-    H_out = (
-        (H_in + 2 * padding[0] - kernel_size[0] + stride[0] - 1) // stride[0]
-        + 1
-        if ceil_mode
-        else (H_in + 2 * padding[0] - kernel_size[0]) // stride[0] + 1
-    )
-    W_out = (
-        (W_in + 2 * padding[1] - kernel_size[1] + stride[1] - 1) // stride[1]
-        + 1
-        if ceil_mode
-        else (W_in + 2 * padding[1] - kernel_size[1]) // stride[1] + 1
-    )
-
-    out = np.zeros((N, C, H_out, W_out))
-    for i in range(H_out):
-        for j in range(W_out):
-            h_start = i * stride[0] - padding[0]
-            h_end = h_start + kernel_size[0]
-            w_start = j * stride[1] - padding[1]
-            w_end = w_start + kernel_size[1]
-
-            x_masked = np.zeros((N, C, kernel_size[0], kernel_size[1]))
-            for n in range(N):
-                for c in range(C):
-                    for h in range(kernel_size[0]):
-                        for w in range(kernel_size[1]):
-                            if (
-                                h_start + h >= 0
-                                and h_start + h < H_in
-                                and w_start + w >= 0
-                                and w_start + w < W_in
-                            ):
-                                x_masked[n, c, h, w] = x[
-                                    n, c, h_start + h, w_start + w
-                                ]
-            if divisor_override is not None:
-                field_size = divisor_override
-            elif count_include_pad:
-                field_size = kernel_size[0] * kernel_size[1]
-            else:
-                field_size = np.sum(
-                    (np.arange(h_start, h_end)[:, None] >= 0)
-                    & (np.arange(h_start, h_end)[:, None] < H_in)
-                    & (np.arange(w_start, w_end) >= 0)
-                    & (np.arange(w_start, w_end) < W_in)
-                )
-                if field_size == 0:
-                    field_size = 1
-
-            out[:, :, i, j] = np.sum(x_masked, axis=(2, 3)) / field_size
-    return out
-
-
-def avg_pool3d_forward_naive(
-    x,
-    kernel_size,
-    stride,
-    padding,
-    ceil_mode=False,
-    count_include_pad=True,
-    divisor_override=None,
-):
-    N, C, D_in, H_in, W_in = x.shape
-    D_out = (
-        (D_in + 2 * padding[0] - kernel_size[0] + stride[0] - 1) // stride[0]
-        + 1
-        if ceil_mode
-        else (D_in + 2 * padding[0] - kernel_size[0]) // stride[0] + 1
-    )
-    H_out = (
-        (H_in + 2 * padding[1] - kernel_size[1] + stride[1] - 1) // stride[1]
-        + 1
-        if ceil_mode
-        else (H_in + 2 * padding[1] - kernel_size[1]) // stride[1] + 1
-    )
-    W_out = (
-        (W_in + 2 * padding[2] - kernel_size[2] + stride[2] - 1) // stride[2]
-        + 1
-        if ceil_mode
-        else (W_in + 2 * padding[2] - kernel_size[2]) // stride[2] + 1
-    )
-
-    out = np.zeros((N, C, D_out, H_out, W_out))
-    for i in range(D_out):
-        for j in range(H_out):
-            for k in range(W_out):
-                d_start = i * stride[0] - padding[0]
-                d_end = d_start + kernel_size[0]
-                h_start = j * stride[1] - padding[1]
-                h_end = h_start + kernel_size[1]
-                w_start = k * stride[2] - padding[2]
-                w_end = w_start + kernel_size[2]
-
-                x_masked = np.zeros(
-                    (N, C, kernel_size[0], kernel_size[1], kernel_size[2])
-                )
-                for n in range(N):
-                    for c in range(C):
-                        for d in range(kernel_size[0]):
-                            for h in range(kernel_size[1]):
-                                for w in range(kernel_size[2]):
-                                    if (
-                                        d_start + d >= 0
-                                        and d_start + d < D_in
-                                        and h_start + h >= 0
-                                        and h_start + h < H_in
-                                        and w_start + w >= 0
-                                        and w_start + w < W_in
-                                    ):
-                                        x_masked[n, c, d, h, w] = x[
-                                            n,
-                                            c,
-                                            d_start + d,
-                                            h_start + h,
-                                            w_start + w,
-                                        ]
-
-                if divisor_override is not None:
-                    field_size = divisor_override
-                elif count_include_pad:
-                    field_size = (
-                        kernel_size[0] * kernel_size[1] * kernel_size[2]
-                    )
-                else:
-                    field_size = np.sum(
-                        (np.arange(d_start, d_end)[:, None, None] >= 0)
-                        & (np.arange(d_start, d_end)[:, None, None] < D_in)
-                        & (np.arange(h_start, h_end)[:, None] >= 0)
-                        & (np.arange(h_start, h_end)[:, None] < H_in)
-                        & (np.arange(w_start, w_end) >= 0)
-                        & (np.arange(w_start, w_end) < W_in)
-                    )
-                    if field_size == 0:
-                        field_size = 1
-
-                out[:, :, i, j, k] = (
-                    np.sum(x_masked, axis=(2, 3, 4)) / field_size
-                )
-    return out
 
 
 class TestCompatAvgPool1DAPI(unittest.TestCase):
@@ -247,13 +57,13 @@ class TestCompatAvgPool1DAPI(unittest.TestCase):
             if isinstance(padding, int):
                 padding = [padding]
 
-            result_np = avg_pool1d_forward_naive(
+            result_np = avg_pool1D_forward_naive(
                 self.input_np,
                 kernel_size,
                 stride,
                 padding,
-                ceil_mode,
-                count_include_pad,
+                ceil_mode=ceil_mode,
+                exclusive=not count_include_pad,
             )
             np.testing.assert_allclose(result_pd.numpy(), result_np, rtol=1e-05)
 
@@ -305,20 +115,25 @@ class TestCompatAvgPool2DAPI(unittest.TestCase):
             if isinstance(padding, int):
                 padding = [padding, padding]
 
-            result_np = avg_pool2d_forward_naive(
+            result_np = avg_pool2D_forward_naive(
                 self.input_np,
                 kernel_size,
                 stride,
                 padding,
-                ceil_mode,
-                count_include_pad,
-                divisor_override,
+                ceil_mode=ceil_mode,
+                exclusive=not count_include_pad,
             )
+            if divisor_override is not None:
+                result_np = (
+                    result_np
+                    * (kernel_size[0] * kernel_size[1])
+                    / divisor_override
+                )
             np.testing.assert_allclose(result_pd.numpy(), result_np, rtol=1e-05)
 
     def test_all_cases(self):
         self.run_test_case(2, 2, 0, False, True, None)
-        self.run_test_case((3, 3), (1, 1), (1, 1), False, True, None)
+        self.run_test_case([3, 3], [1, 1], [1, 1], False, True, None)
         self.run_test_case(3, 2, 0, True, True, None)
         self.run_test_case(3, 2, 1, True, False, None)
         self.run_test_case(3, None, 0, False, True, None)
@@ -367,20 +182,25 @@ class TestCompatAvgPool3DAPI(unittest.TestCase):
             if isinstance(padding, int):
                 padding = [padding, padding, padding]
 
-            result_np = avg_pool3d_forward_naive(
+            result_np = avg_pool3D_forward_naive(
                 self.input_np,
                 kernel_size,
                 stride,
                 padding,
-                ceil_mode,
-                count_include_pad,
-                divisor_override,
+                ceil_mode=ceil_mode,
+                exclusive=not count_include_pad,
             )
+            if divisor_override is not None:
+                result_np = (
+                    result_np
+                    * (kernel_size[0] * kernel_size[1] * kernel_size[2])
+                    / divisor_override
+                )
             np.testing.assert_allclose(result_pd.numpy(), result_np, rtol=1e-05)
 
     def test_all_cases(self):
         self.run_test_case(2, 2, 0, False, True, None)
-        self.run_test_case((3, 3, 3), (1, 1, 1), (1, 1, 1), False, True, None)
+        self.run_test_case([3, 3, 3], [1, 1, 1], [1, 1, 1], False, True, None)
         self.run_test_case(3, 2, 0, True, True, None)
         self.run_test_case(3, 2, 1, True, False, None)
         self.run_test_case(3, None, 0, False, True, None)
