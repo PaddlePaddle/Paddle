e2a96bae08aa8ce0b73b9ee88715b0ff890aacfb
SigureMo <sigure.qaq@gmail.com>
2025-11-26 11:13:37 +0800

    use python${PY_VERSION} in test stage

--- ci/coverage_test.sh
+++ ci/coverage_test.sh
@@ -52,8 +52,8 @@ unset GREP_OPTIONS
 source $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/utils.sh
 init
 
-ln -sf $(which python3.9) /usr/local/bin/python
-ln -sf $(which pip3.9) /usr/local/bin/pip
+ln -sf $(which python${PY_VERSION}) /usr/local/bin/python
+ln -sf $(which pip${PY_VERSION}) /usr/local/bin/pip
 
 echo "::group::Install zstd"
 apt install zstd -y
