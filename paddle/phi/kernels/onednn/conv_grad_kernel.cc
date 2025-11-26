54b36fff88bd9d2793483118bc2fcbf3e848ddae
co63oc <4617245+co63oc@users.noreply.github.com>
2025-11-26 10:15:50 +0800

    fix kNHWC in kernels/onednn

--- paddle/phi/kernels/onednn/conv_grad_kernel.cc
+++ paddle/phi/kernels/onednn/conv_grad_kernel.cc
@@ -243,7 +243,7 @@ KernelKey ConvGradGetKernelTypeForVar(const GetKernelTypeForVarContext* ctx) {
     auto dl = common::StringToDataLayout(data_format);
     // Some models may have intentionally set "AnyLayout" for pool
     // op. Treat this as NCHW (default data_format value)
-    if (dl != phi::DataLayout::kAnyLayout) {
+    if (dl != phi::DataLayout::ANY) {
       return phi::KernelKey(tensor.place(), dl, expected_kernel_type.dtype());
     }
   }
