93d2451d8536cdc4ba854cf8f11d6af4455290be
Pan Zhaowu <panzhaowu@baidu.com>
2025-11-27 19:07:06 +0800

    disable dist tensor case

--- paddle/fluid/pybind/eager_custom_python_api.h
+++ paddle/fluid/pybind/eager_custom_python_api.h
@@ -46,7 +46,8 @@ static PyObject *eager_api_linear(PyObject *self,
       }
 #if (defined(PADDLE_WITH_CUDA) && CUDA_VERSION >= 11000 && \
      !(defined(_WIN32) || defined(WIN32)))
-      if (!FLAGS_use_legacy_gemm) [[likely]] {  // NOLINT
+      if (!FLAGS_use_legacy_gemm && !bias.is_dist_tensor())  // NOLINT
+          [[likely]] {                                       // NOLINT
         // TODO(Pan Zhaowu): Add proper broadcast logic for batchsize unaligned
         // batch-gemm. Currently handles: (B..., k) x (k, n) -> (B..., n), with
         // 1D or scalar bias.
