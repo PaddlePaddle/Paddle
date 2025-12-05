85d4c52fff720e6badeb0f278d4c9f85700afb60
Yami <156195357+Le-soleile@users.noreply.github.com>
2025-12-05 18:40:50 +0800

    Update partial_concat_kernel.h

--- paddle/phi/kernels/partial_concat_kernel.h
+++ paddle/phi/kernels/partial_concat_kernel.h
@@ -24,10 +24,10 @@ namespace phi {
 
 template <typename T, typename Context>
 void PartialConcatOpCUDAKernel(const Context& dev_ctx,
-                         const std::vector<const DenseTensor*>& x,
-                         int start_index,
-                         int length,
-                         DenseTensor* out);
+                               const std::vector<const DenseTensor*>& x,
+                               int start_index,
+                               int length,
+                               DenseTensor* out);
 
 template <typename T, typename Context>
 void PartialConcatGradientOpKernel(const Context& dev_ctx,
