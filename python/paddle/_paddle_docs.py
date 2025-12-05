ed886ef0158d1059dadcd0223e784c5004a44eec
Manfredss <ms1419344875@outlook.com>
2025-12-05 10:27:00 +0800

    fix docs

--- python/paddle/_paddle_docs.py
+++ python/paddle/_paddle_docs.py
@@ -3117,8 +3117,6 @@ add_doc_and_signature(
     def asinh(
         x: Tensor,
         name: str | None = None,
-        *,
-        out: Tensor | None = None
     ) -> Tensor
     """,
 )
@@ -3162,8 +3160,6 @@ add_doc_and_signature(
     def atan(
         x: Tensor,
         name: str | None = None,
-        *,
-        out: Tensor | None = None
     ) -> Tensor
     """,
 )
