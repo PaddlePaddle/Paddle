5aededaca508a435e39b406d7784843ecf16499e
Ricardo-shuo-liu <13838152117@139.com>
2025-12-07 09:33:56 +0800

    fix

--- python/paddle/tensor/creation.py
+++ python/paddle/tensor/creation.py
@@ -3837,8 +3837,12 @@ def complex(
             >>> z = paddle.complex(x, y)
             >>> print(z)
             Tensor(shape=[2, 3], dtype=complex64, place=Place(cpu), stop_gradient=True,
-            [[0j    , 1j    , 2j    ],
-             [(1+0j), (1+1j), (1+2j)]])
+            [[(0.00000000+0.00000000j),
+            (0.00000000+1.00000000j),
+            (0.00000000+2.00000000j)],
+            [(1.00000000+0.00000000j),
+            (1.00000000+1.00000000j),
+            (1.00000000+2.00000000j)]])
     """
     if in_dynamic_or_pir_mode():
         return _C_ops.complex(real, imag, out=out)
