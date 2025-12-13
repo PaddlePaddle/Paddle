e85b587fa31a8e775e1caf2b5d7632afed646ad2
WintersMontagne10335 <zhhy10335@163.com>
2025-12-13 03:34:08 +0000

    update

--- paddle/phi/kernels/funcs/pooling.cu
+++ paddle/phi/kernels/funcs/pooling.cu
@@ -2591,13 +2591,21 @@ __global__ void KernelMaxPool2dWithDilationsAndIdx(
     hstart = h_offset * stride_height - padding_height;
     wstart = w_offset * stride_width - padding_width;
 
-    if (dilation_height > static_cast<IndexT>(1)) {
+    if (dilation_height > static_cast<IndexT>(1) ||
+        dilation_width > static_cast<IndexT>(1)) {
       hend = hstart + (ksize_height - 1) * dilation_height + 1;
       while (hstart < static_cast<IndexT>(0)) hstart += dilation_height;
       while (hend > input_height) hend -= dilation_height;
+
+      wend = wstart + (ksize_width - 1) * dilation_width + 1;
+      while (wstart < static_cast<IndexT>(0)) wstart += dilation_width;
+      while (wend > input_width) wend -= dilation_width;
     } else {
       hend = min(hstart + ksize_height, input_height);
       hstart = max(hstart, static_cast<IndexT>(0));
+
+      wend = min(wstart + ksize_width, input_width);
+      wstart = max(wstart, static_cast<IndexT>(0));
     }
 
     // hend = min(hstart + ksize_height, input_height);
@@ -2612,8 +2620,8 @@ __global__ void KernelMaxPool2dWithDilationsAndIdx(
     //   wstart = max(wstart, static_cast<IndexT>(0));
     // }
 
-    wend = min(wstart + ksize_width, input_width);
-    wstart = max(wstart, static_cast<IndexT>(0));
+    // wend = min(wstart + ksize_width, input_width);
+    // wstart = max(wstart, static_cast<IndexT>(0));
 
     T1 ele = static_cast<T1>(-FLT_MAX);
     IndexT max_index = -1;
