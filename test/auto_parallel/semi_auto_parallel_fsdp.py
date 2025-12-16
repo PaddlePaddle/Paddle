b52c53de99b4a4ed1ef246d7b3a81fa185ed042b
ZhenxingLi <lizhenxing02@baidu.com>
2025-12-16 22:04:44 +0800

    review fix

--- test/auto_parallel/semi_auto_parallel_fsdp.py
+++ test/auto_parallel/semi_auto_parallel_fsdp.py
@@ -14,9 +14,27 @@
 
 import os
 
+import numpy as np
+
 import paddle
 import paddle.distributed as dist
-from paddle.distributed.auto_parallel.fully_shard import FullyShardAuto
+from paddle.distributed.sharding.fully_shard import fully_shard
+from paddle.io import DataLoader, Dataset, DistributedBatchSampler
+
+
+class RandomDataset(Dataset):
+    def __init__(self, num_samples=100, input_dim=10):
+        self.num_samples = num_samples
+        self.input_dim = input_dim
+
+    def __getitem__(self, idx):
+        np.random.seed(2025)
+        data = np.random.randn(self.input_dim).astype('float32')
+        label = np.random.randn(self.input_dim).astype('float32')
+        return data, label
+
+    def __len__(self):
+        return self.num_samples
 
 
 class TestSemiAutoParallelFSDP:
@@ -26,58 +44,76 @@ class TestSemiAutoParallelFSDP:
         self._mesh = dist.ProcessMesh([0, 1], dim_names=["x"])
         self.gradient_accumulation_steps = 2
 
+    def create_dist_loader(self):
+        dataset = RandomDataset(num_samples=10, input_dim=10)
+
+        sampler = DistributedBatchSampler(
+            dataset=dataset, batch_size=2, shuffle=True, drop_last=False
+        )
+
+        loader = DataLoader(
+            dataset=dataset,
+            batch_sampler=sampler,
+        )
+
+        return loader
+
     def test_sharding_stage_3(self):
         paddle.seed(self._seed)
-        linear = paddle.nn.Linear(10, 10)
-        batch = paddle.rand(shape=[10, 10])
-        # shard the input by sharding degree
-        batch = dist.shard_tensor(batch, self._mesh, [dist.Shard(0)])
-        opt = paddle.optimizer.AdamW(parameters=linear.parameters())
+        model = paddle.nn.Linear(10, 10)
+        data_loader = self.create_dist_loader()
+        dist_loader = dist.shard_dataloader(
+            dataloader=data_loader,
+            meshes=[self._mesh],
+        )
+        opt = paddle.optimizer.AdamW(parameters=model.parameters())
 
         # use sharding stage 3
         opt = dist.shard_optimizer(opt, dist.ShardingStage3("x", self._mesh))
 
         stage_losses = []
         tr_loss_add = float(0)
-        for step in range(6):
-            with paddle.amp.auto_cast(level='O2'):
-                tr_loss = linear(batch)
-                tr_loss.backward()
-                tr_loss_add += tr_loss
-                if (step + 1) % self.gradient_accumulation_steps == 0:
-                    tr_loss_add /= self.gradient_accumulation_steps
-                    tr_loss = tr_loss_add
-                    opt.step()
-                    opt.clear_grad()
-                stage_losses.append(tr_loss._md5sum())
-        os.environ["skip_sharding3_output_reshard"] = "0"
+        step = 0
+        for batch in dist_loader:
+            tr_loss = model(batch[0])
+            tr_loss.backward()
+            tr_loss_add += tr_loss
+            if (step + 1) % self.gradient_accumulation_steps == 0:
+                tr_loss_add /= self.gradient_accumulation_steps
+                tr_loss = tr_loss_add
+                opt.step()
+                opt.clear_grad()
+            step += 1
+            stage_losses.append(tr_loss._md5sum())
         return stage_losses
 
     def test_fsdp(self):
         paddle.seed(self._seed)
-        linear = paddle.nn.Linear(10, 10)
-        batch = paddle.rand(shape=[10, 10])
-        # shard the input by sharding degree
-        batch = dist.shard_tensor(batch, self._mesh, [dist.Shard(0)])
-        opt = paddle.optimizer.AdamW(parameters=linear.parameters())
+        model = paddle.nn.Linear(10, 10)
+        data_loader = self.create_dist_loader()
+        dist_loader = dist.shard_dataloader(
+            dataloader=data_loader,
+            meshes=[self._mesh],
+        )
+        opt = paddle.optimizer.AdamW(parameters=model.parameters())
 
         # use FSDP
-        FullyShardAuto(linear, self._mesh)
+        fully_shard(model, mesh=self._mesh)
 
         stage_losses = []
         tr_loss_add = float(0)
-        for step in range(6):
-            with paddle.amp.auto_cast(level='O2'):
-                tr_loss = linear(batch)
-                tr_loss.backward()
-                tr_loss_add += tr_loss
-                if (step + 1) % self.gradient_accumulation_steps == 0:
-                    tr_loss_add /= self.gradient_accumulation_steps
-                    tr_loss = tr_loss_add
-                    opt.step()
-                    opt.clear_grad()
-                stage_losses.append(tr_loss._md5sum())
-        os.environ["skip_sharding3_output_reshard"] = "0"
+        step = 0
+        for batch in dist_loader:
+            tr_loss = model(batch[0])
+            tr_loss.backward()
+            tr_loss_add += tr_loss
+            if (step + 1) % self.gradient_accumulation_steps == 0:
+                tr_loss_add /= self.gradient_accumulation_steps
+                tr_loss = tr_loss_add
+                opt.step()
+                opt.clear_grad()
+            step += 1
+            stage_losses.append(tr_loss._md5sum())
         return stage_losses
 
     def run_test_case(self):
