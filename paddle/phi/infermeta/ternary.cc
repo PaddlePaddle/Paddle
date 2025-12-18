11a734328469963c9f18f4011120e7b0d768c738
youge325 <A372707325@126.com>
2025-12-19 00:22:53 +0800

    fix broadcast dim check

--- paddle/phi/infermeta/ternary.cc
+++ paddle/phi/infermeta/ternary.cc
@@ -212,29 +212,28 @@ void BaddbmmInferMeta(const MetaTensor& input,
                               x_dims[2],
                               y_dims[1]));
 
-  std::vector<int64_t> matmul_dims;
-  matmul_dims.push_back(x_dims[0]);
-  matmul_dims.push_back(x_dims[1]);
-  matmul_dims.push_back(y_dims[2]);
+  PADDLE_ENFORCE_EQ(
+      (ndim_input == 3 && (input_dims[0] == x_dims[0] || input_dims[0] == 1) &&
+       (input_dims[1] == x_dims[1] || input_dims[1] == 1) &&
+       (input_dims[2] == y_dims[2] || input_dims[2] == 1)) ||
+          (ndim_input == 2 &&
+           (input_dims[0] == x_dims[1] || input_dims[0] == 1) &&
+           (input_dims[1] == y_dims[2] || input_dims[1] == 1)),
+      true,
+      errors::InvalidArgument("The input tensor must be broadcastable with the "
+                              "multiplication of x and y."
+                              "But received input's shape = [%s], x's shape = "
+                              "[%s], y's shape = [%s].",
+                              input_dims,
+                              x_dims,
+                              y_dims));
 
-  // Check if input and matmul result can be broadcast
-  auto matmul_ddim = common::make_ddim(matmul_dims);
-  int max_dim = std::max(input_dims.size(), matmul_ddim.size());
-  int axis = std::abs(static_cast<int>(input_dims.size()) -
-                      static_cast<int>(matmul_ddim.size()));
-  std::vector<int64_t> input_dims_array(max_dim);
-  std::vector<int64_t> matmul_dims_array(max_dim);
-  std::vector<int64_t> out_dims_array(max_dim);
+  std::vector<int64_t> output_dims;
+  output_dims.push_back(x_dims[0]);
+  output_dims.push_back(x_dims[1]);
+  output_dims.push_back(y_dims[2]);
 
-  funcs::GetBroadcastDimsArrays(input_dims,
-                                matmul_ddim,
-                                input_dims_array.data(),
-                                matmul_dims_array.data(),
-                                out_dims_array.data(),
-                                max_dim,
-                                axis);
-
-  out->set_dims(common::make_ddim(out_dims_array));
+  out->set_dims(common::make_ddim(output_dims));
   out->share_lod(input);
   // Set output dtype based on out_dtype parameter
   if (out_dtype != phi::DataType::UNDEFINED) {
