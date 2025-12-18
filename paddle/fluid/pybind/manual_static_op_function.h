2145b0ded2b7b36b00285f3423d10f5e97a67174
SigureMo <sigure.qaq@gmail.com>
2025-12-18 15:37:21 +0800

    cleanup comments

--- paddle/fluid/pybind/manual_static_op_function.h
+++ paddle/fluid/pybind/manual_static_op_function.h
@@ -993,19 +993,6 @@ static PyObject *run_custom_op(PyObject *self,
   }
 }
 
-// template <typename T>
-// std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec) {
-//   os << "[";
-//   for (size_t i = 0; i < vec.size(); ++i) {
-//     os << vec[i];
-//     if (i != vec.size() - 1) {
-//       os << ", ";
-//     }
-//   }
-//   os << "]";
-//   return os;
-// }
-
 using IrTensor = paddle::dialect::IrTensor;
 
 template <typename T>
