615a99ced549c8ee32c3fa70f8694e629ed2d2fc
gushiwei <gushiwei@baidu.com>
2025-12-20 15:44:12 +0000

    fix

--- cmake/external/warprnnt.cmake
+++ cmake/external/warprnnt.cmake
@@ -34,7 +34,7 @@ else()
   set(WARPCTC_PATCH_CUDA_COMMAND
       git checkout -- . && git checkout ${WARPRNNT_TAG} && patch -Nd
       ${SOURCE_DIR} <
-      ${PADDLE_SOURCE_DIR}/patches/warprnnt/CMakeLists.txt.patch)
+      ${PADDLE_SOURCE_DIR}/patches/warprnnt/CMakeLists.txt.cuda.patch)
 endif()
 if(WITH_ROCM)
   set(WARPRNNT_PATCH_ROCM_COMMAND
