233096ed9ce222c93e4ad6f72e97cce67b37ce86
zhengshengning <ningzhengsheng@baidu.com>
2025-12-25 14:16:15 +0800

    add _C_ops.rms_norm (#76930)
    * mv fastln to ln
    
    * fix build error on win and hip
    
    * fix build error for infer
    
    * support backward
    
    * fix error
    
    * merge fast_rms_norm and fused_rms_norm_ext to rms_norm
    
    * fix CMake error
    
    * remove windows version
    
    * add rms_norm for xpu
    
    * fix cmake
    
    * fix cmake2
    
    * fix test_rms_norm
    
    * rms_norm align torch
    
    * fix test_rms_norm_xpu
    
    * remove fast_rms_norm in rms_norm
    
    * rename rms_norm_nzs to rms_norm
    
    * fix
    
    * fix RmsNormInferMeta
    
    * mv fused_rms_norm_quant form ops.yaml to fused_ops.yaml
    
    * revert mv fused_rms_norm_quant form ops.yaml to fused_ops.yaml
    
    * fix rms_norm docs
    
    * fix doctest
    
    * fix doctest
    
    * fix test_rms_norm
    
    * fix test cmakefile
    
    * fix test_rms_norm
    
    * add InferSymbolicShapeInterface
    
    * add check_pir
    
    ---------
    
    Co-authored-by: DanielSun11 <1395924413@qq.com>

--- paddle/phi/infermeta/binary.h
+++ paddle/phi/infermeta/binary.h
@@ -773,6 +773,12 @@ PADDLE_API void ReduceAsInferMeta(const MetaTensor& x,
                                   const MetaTensor& target,
                                   MetaTensor* out);
 
+PADDLE_API void RmsNormInferMeta(const MetaTensor& x,
+                                 const MetaTensor& scale,
+                                 float epsilon,
+                                 MetaTensor* y,
+                                 MetaTensor* invvar);
+
 PADDLE_API void SoftmaxMaskFuseInferMeta(const MetaTensor& x,
                                          const MetaTensor& mask,
                                          MetaTensor* out);
