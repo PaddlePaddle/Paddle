dda718f62a37a49396922484897fb95ded5d73c2
gushiwei <gushiwei@baidu.com>
2025-12-25 11:35:04 +0000

    add dev pr check

--- tools/CheckPRTemplate.py
+++ tools/CheckPRTemplate.py
@@ -26,7 +26,7 @@ if BRANCH.startswith("develop"):
     REPO_TEMPLATE = {
         "Paddle": r'''### PR Category(.*[^\s].*)### PR Types(.*[^\s].*)### Description(.*[^\s].*)'''
     }
-elif BRANCH.startswith("fleety_"):
+elif BRANCH.startswith("release"):
     REPO_TEMPLATE = {
         "Paddle": r'''### PR Category(.*[^\s].*)### PR Types(.*[^\s].*)### Description(.*?devPR:https://github\.com/PaddlePaddle/Paddle/pull/.*?)(?:\n###|\Z)'''
     }
@@ -100,13 +100,13 @@ def parameter_accuracy(body):
                     single_mess += f'{i}.'
             if len(single_mess) != 0:
                 message += f'{key} should be in {test_list}. but now is [{single_mess}].'
-    if BRANCH.startswith("fleety_"):
+    if BRANCH.startswith("release"):
         PR_dic['Description'] = body[changes_end + len('### Description') :]
         des_pr_id = extract_pr_links(PR_dic['Description'])
         if len(des_pr_id) == 0 or not check_link_accessible(
             "https://github.com/PaddlePaddle/Paddle/pull/" + str(des_pr_id[0])
         ):
-            message += 'The PR link does not exist. To merge into the fleety branch, you need to merge into the develop branch first and then cherry-pick it to the fleety branch. Please merge into develop first and fill in the PR link in the Description'
+            message += 'The PR link does not exist. To merge into the release/3.3 branch, you need to merge into the develop branch first and then cherry-pick it to the release/3.3 branch. Please merge into develop first and fill in the PR link in the Description'
     return message
 
 
@@ -144,8 +144,8 @@ def checkPRTemplate(repo, body, CHECK_TEMPLATE):
     elif result is None:
         res = False
         message = parameter_accuracy(body)
-        if BRANCH.startswith("fleety_") and len(message) == 0:
-            message = 'The PR link does not exist. To merge into the fleety branch, you need to merge into the develop branch first and then cherry-pick it to the fleety branch. Please merge into develop first and fill in the PR link in the Description'
+        if BRANCH.startswith("release") and len(message) == 0:
+            message = 'The PR link does not exist. To merge into the release/3.3 branch, you need to merge into the develop branch first and then cherry-pick it to the release/3.3 branch. Please merge into develop first and fill in the PR link in the Description'
     return res, message
 
 
