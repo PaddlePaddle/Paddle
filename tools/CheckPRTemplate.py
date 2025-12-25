e7504a40b00a956096bd6008d7818f8d95b112de
gushiwei <gushiwei@baidu.com>
2025-12-25 11:59:19 +0000

    add dev pr check

--- tools/CheckPRTemplate.py
+++ tools/CheckPRTemplate.py
@@ -106,7 +106,7 @@ def parameter_accuracy(body):
         if len(des_pr_id) == 0 or not check_link_accessible(
             "https://github.com/PaddlePaddle/Paddle/pull/" + str(des_pr_id[0])
         ):
-            message += 'The PR link does not exist. To merge into the release/3.3 branch, you need to merge into the develop branch first and then cherry-pick it to the release/3.3 branch. Please merge into develop first and fill in the PR link in the Description'
+            message += 'The PR link does not exist. To merge into the release/3.3 branch, you need to merge into the develop branch first and then cherry-pick it to the release/3.3 branch. Please merge into develop first and fill in the PR link in the Description.Use this format devPR:https://github.com/PaddlePaddle/Paddle/pull/xxxx'
     return message
 
 
@@ -145,7 +145,7 @@ def checkPRTemplate(repo, body, CHECK_TEMPLATE):
         res = False
         message = parameter_accuracy(body)
         if BRANCH.startswith("release") and len(message) == 0:
-            message = 'The PR link does not exist. To merge into the release/3.3 branch, you need to merge into the develop branch first and then cherry-pick it to the release/3.3 branch. Please merge into develop first and fill in the PR link in the Description'
+            message = 'The PR link does not exist. To merge into the release/3.3 branch, you need to merge into the develop branch first and then cherry-pick it to the release/3.3 branch. Please merge into develop first and fill in the PR link in the Description.Use this format devPR:https://github.com/PaddlePaddle/Paddle/pull/xxxx'
     return res, message
 
 
