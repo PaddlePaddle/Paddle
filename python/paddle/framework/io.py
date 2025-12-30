2055d89d2a9166253405294ab31d451dc802f2cb
yyxxddjj <2505553080@qq.com>
2025-12-30 13:14:40 +0800

    Fix CI failure in paddle.save example 3 (PIR compatibility)

--- python/paddle/framework/io.py
+++ python/paddle/framework/io.py
@@ -851,7 +851,7 @@ def save(
             >>> path = 'example/model.pdparams'
             >>> paddle.save(obj, path)
 
-        .. code-block:: pycon
+        .. code-block:: python
             :name: code-example-3
 
             >>> # example 3: static graph
