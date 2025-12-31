03914c05c589c0e1bb215526ef517ace0fea3bab
zrr1999 <2742392377@qq.com>
2025-12-31 02:10:49 +0000

    fix cpu

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -5037,7 +5037,10 @@ def lerp(
 
     """
     if isinstance(weight, float):
-        weight = paddle.full(shape=[], fill_value=weight, dtype="float64")
+        if x.is_cuda:
+            weight = paddle.full(shape=[], fill_value=weight, dtype="float64")
+        else:
+            weight = paddle.full(shape=[], fill_value=weight, dtype=x.dtype)
 
     if in_dynamic_or_pir_mode():
         return _C_ops.lerp(x, y, weight)
