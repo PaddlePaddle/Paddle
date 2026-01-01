9340fb749db9a40973328eddc4554032196782e2
ooooo <3164076421@qq.com>
2026-01-01 09:22:59 +0000

    fix typos

--- python/paddle/audio/datasets/esc50.py
+++ python/paddle/audio/datasets/esc50.py
@@ -77,11 +77,11 @@ class ESC50(AudioClassificationDataset):
             ...     # do something with audio, label
             ...     print(audio.shape, label)
             ...     # [audio_data_length] , label_id
-            paddle.Size([40, 1723]) 0
-            paddle.Size([40, 1723]) 14
-            paddle.Size([40, 1723]) 36
-            paddle.Size([40, 1723]) 36
-            paddle.Size([40, 1723]) 19
+            paddle.Size([220500]) 0
+            paddle.Size([220500]) 14
+            paddle.Size([220500]) 36
+            paddle.Size([220500]) 36
+            paddle.Size([220500]) 19
 
             >>> esc50_dataset = paddle.audio.datasets.ESC50(
             ...     mode=mode,  # type: ignore[arg-type]
