1c3ffa0ebbac9387bf3a2614007d3f0f2e31c25f
ooooo <3164076421@qq.com>
2026-01-03 00:37:37 +0800

    add timeout

--- python/paddle/hapi/callbacks.py
+++ python/paddle/hapi/callbacks.py
@@ -719,6 +719,7 @@ class LRScheduler(Callback):
     Examples:
         .. code-block:: pycon
 
+            >>> # doctest: +TIMEOUT(30)
             >>> import paddle
             >>> import paddle.vision.transforms as T
             >>> from paddle.static import InputSpec
