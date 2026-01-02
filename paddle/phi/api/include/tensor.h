b2f0eed5d8acf3a51c87e35268325269b0fbb7eb
SigureMo <sigure.qaq@gmail.com>
2026-01-03 01:05:47 +0800

    revert trigger compile

--- paddle/phi/api/include/tensor.h
+++ paddle/phi/api/include/tensor.h
@@ -776,13 +776,6 @@ class PADDLE_API Tensor final {
   Tensor tile(const IntArray& repeat_times = {}) const;
 };
 
-inline void UnusedAPITriggerRecompile() {
-  std::vector<Tensor> tensors;
-  for (const auto& t : tensors) {
-    std::cout << t.numel();
-  }
-}
-
 PADDLE_API Tensor operator+(const Scalar& x, const Tensor& y);
 
 PADDLE_API Tensor operator-(const Scalar& x, const Tensor& y);
