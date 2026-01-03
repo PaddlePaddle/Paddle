0d3ace0036c611ae639242232aab930e4f00992c
Ricardo-shuo-liu <13838152117@139.com>
2026-01-03 20:28:25 +0800

    .

--- python/paddle/device/cuda/__init__.py
+++ python/paddle/device/cuda/__init__.py
@@ -27,7 +27,6 @@ from .streams import Event, Stream, create_event, create_stream  # noqa: F401
 
 if TYPE_CHECKING:
     from paddle import CUDAPlace, CustomPlace
-    from paddle.base.libpaddle import _gpuDeviceProperties
 
     _CudaPlaceLike: TypeAlias = Union[
         CUDAPlace,
@@ -595,7 +594,7 @@ def stream_guard(stream: Stream) -> NoReturn:
 
 def get_device_properties(
     device: _CudaPlaceLike | None = None,
-) -> _gpuDeviceProperties:
+):
     '''
     Return the properties of given device.
 
