17e4791311b4df5e3b75a1a3e5e6358b50f59c60
enkilee <jeffrey0122@163.com>
2026-01-06 16:54:21 +0800

    add i1 and i1e

--- test/legacy_test/test_i1_op.py
+++ test/legacy_test/test_i1_op.py
@@ -166,5 +166,72 @@ class TestI1Op_ZeroSize(OpTest):
         self.check_grad(['x'], 'out')
 
 
+class TestI1API_Compatibility(unittest.TestCase):
+    def setUp(self):
+        self.x = np.array([0, 1, 2, 3, 4, 5])
+        self.out = reference_i1(self.x)
+        self.dtype = "float64"
+        self.place = get_places()
+
+    def test_dygraph_Compatibility(self):
+        paddle.disable_static()
+        x = paddle.to_tensor(self.x)
+        paddle_dygraph_out = []
+        # Position args (args)
+        out1 = paddle.i1(x)
+        paddle_dygraph_out.append(out1)
+        # Key words args (kwargs) for paddle
+        out2 = paddle.i1(x=x)
+        paddle_dygraph_out.append(out2)
+        # Key words args for torch
+        out3 = paddle.i1(input=x)
+        paddle_dygraph_out.append(out3)
+
+        # Tensor method args
+        out4 = paddle.empty([])
+        out5 = x.i1(x, out=out4)
+        paddle_dygraph_out.append(out4)
+        paddle_dygraph_out.append(out5)
+        # Tensor method kwargs
+        out6 = x.i1()
+        paddle_dygraph_out.append(out6)
+        # Test out
+        out7 = paddle.empty([])
+        paddle.i1(x, out=out7)
+        paddle_dygraph_out.append(out7)
+        # scipy reference  out
+        ref_out = reference_i1(self.x)
+        # Check
+        for out in paddle_dygraph_out:
+            np.testing.assert_allclose(ref_out, out.numpy(), rtol=1e-5)
+        paddle.enable_static()
+
+    def test_static_Compatibility(self):
+        main = paddle.static.Program()
+        startup = paddle.static.Program()
+        with paddle.static.program_guard(main, startup):
+            x = paddle.static.data(
+                name="x", shape=self.x.shape, dtype=self.dtype
+            )
+            # Position args (args)
+            out1 = paddle.i1(x)
+            # Key words args (kwargs) for paddle
+            out2 = paddle.i1(x=x)
+            # Key words args for torch
+            out3 = paddle.i1(input=x)
+            # Tensor method args
+            out4 = x.i1()
+
+            exe = paddle.static.Executor(self.place)
+            fetches = exe.run(
+                main,
+                feed={"x": self.x},
+                fetch_list=[out1, out2, out3, out4],
+            )
+            ref_out = reference_i1(self.x)
+            for out in fetches:
+                np.testing.assert_allclose(out, ref_out, rtol=1e-5)
+
+
 if __name__ == "__main__":
     unittest.main()
