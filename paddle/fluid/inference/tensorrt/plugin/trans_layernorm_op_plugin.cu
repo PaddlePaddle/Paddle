82fa6cada0a2738d9aecb4186d56bc3e9a626a41
co63oc <4617245+co63oc@users.noreply.github.com>
2026-01-06 11:12:50 +0800

    optimize phi::CPUPlace in paddle/fluid/inference/

--- paddle/fluid/inference/tensorrt/plugin/trans_layernorm_op_plugin.cu
+++ paddle/fluid/inference/tensorrt/plugin/trans_layernorm_op_plugin.cu
@@ -363,7 +363,7 @@ int TransLayerNormPluginDynamic::enqueue(
   auto input_type = input_desc[0].type;
 
   phi::DeviceContextPool &pool = phi::DeviceContextPool::Instance();
-  phi::GPUPlace place(platform::GetCurrentDeviceId());
+  GPUPlace place(platform::GetCurrentDeviceId());
   auto *device_context = static_cast<phi::GPUContext *>(pool.Get(place));
   const phi::GPUContext &dev_ctx = *device_context;
 
