62ab3df0350d22326d245599eb8392a4739266bc
co63oc <4617245+co63oc@users.noreply.github.com>
2026-01-10 20:11:48 +0800

    optimize phi::CPUPlace in paddle/phi/core/memory/

--- paddle/phi/core/memory/allocation/virtual_memory_auto_growth_best_fit_allocator.cc
+++ paddle/phi/core/memory/allocation/virtual_memory_auto_growth_best_fit_allocator.cc
@@ -115,7 +115,7 @@ VirtualMemoryAutoGrowthBestFitAllocator::
     VirtualMemoryAutoGrowthBestFitAllocator(
         const std::shared_ptr<Allocator> &underlying_allocator,
         size_t alignment,
-        const phi::GPUPlace &place)
+        const GPUPlace &place)
     : underlying_allocator_(
           std::make_shared<AlignedAllocator>(underlying_allocator, alignment)),
       alignment_(alignment),
