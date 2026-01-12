15cc8a9be5b3c071dbb8e64f32aa3ed81c83e3cb
Nyako Shigure <shigure_nyako@outlook.com>
2026-01-13 00:31:54 +0800

    [CodeStyle][Xdoctest][326-328,333] Fix example code for API `paddle.Model`, `paddle.callbacks.{EarlyStopping,LRScheduler}` and `paddle.distribution.Gumbel`

--- python/paddle/hapi/callbacks.py
+++ python/paddle/hapi/callbacks.py
@@ -837,7 +837,7 @@ class EarlyStopping(Callback):
         save_best_model(bool): Whether to save best model. Default: True.
 
     Examples:
-        .. code-block:: python
+        .. code-block:: pycon
 
             >>> import paddle
             >>> from paddle import Model
@@ -852,12 +852,15 @@ class EarlyStopping(Callback):
             >>> sample_num = 200
             >>> save_dir = './best_model_checkpoint'
             >>> transform = T.Compose(
-            ...     [T.Transpose(), T.Normalize([127.5], [127.5])])
+            ...     [T.Transpose(), T.Normalize([127.5], [127.5])],
+            ... )
             >>> train_dataset = MNIST(mode='train', transform=transform)
             >>> val_dataset = MNIST(mode='test', transform=transform)
             >>> net = LeNet()
             >>> optim = paddle.optimizer.Adam(
-            ...     learning_rate=0.001, parameters=net.parameters())
+            ...     learning_rate=0.001,
+            ...     parameters=net.parameters(),
+            ... )
 
             >>> inputs = [InputSpec([None, 1, 28, 28], 'float32', 'x')]
             >>> labels = [InputSpec([None, 1], 'int64', 'label')]
@@ -866,7 +869,8 @@ class EarlyStopping(Callback):
             >>> model.prepare(
             ...     optim,
             ...     loss=CrossEntropyLoss(reduction="sum"),
-            ...     metrics=[Accuracy()])
+            ...     metrics=[Accuracy()],
+            ... )
             >>> callbacks = paddle.callbacks.EarlyStopping(
             ...     'loss',
             ...     mode='min',
@@ -874,15 +878,18 @@ class EarlyStopping(Callback):
             ...     verbose=1,
             ...     min_delta=0,
             ...     baseline=None,
-            ...     save_best_model=True)
-            >>> model.fit(train_dataset,
-            ...           val_dataset,
-            ...           batch_size=64,
-            ...           log_freq=200,
-            ...           save_freq=10,
-            ...           save_dir=save_dir,
-            ...           epochs=20,
-            ...           callbacks=[callbacks])
+            ...     save_best_model=True,
+            ... )
+            >>> model.fit(
+            ...     train_dataset,
+            ...     val_dataset,
+            ...     batch_size=64,
+            ...     log_freq=200,
+            ...     save_freq=10,
+            ...     save_dir=save_dir,
+            ...     epochs=20,
+            ...     callbacks=[callbacks],
+            ... )
     """
 
     def __init__(
