3c79886bb0e49cfba0220f542114de14b375ba49
cangtianhuang <l1903374751@gmail.com>
2026-01-14 17:33:21 +0800

    set Python_EXECUTABLE

--- cmake/external/cutlass.cmake
+++ cmake/external/cutlass.cmake
@@ -26,7 +26,10 @@ add_definitions("-DPADDLE_WITH_CUTLASS")
 add_definitions("-DSPCONV_WITH_CUTLASS=0")
 
 if(NOT PYTHON_EXECUTABLE)
-  find_package(Python ${PY_VERSION} REQUIRED COMPONENTS Interpreter)
+  find_package(
+    Python ${PY_VERSION}
+    COMPONENTS Interpreter
+    REQUIRED)
 endif()
 
 ExternalProject_Add(
