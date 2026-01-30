0d3028376d16ce2859fff84a0834757733165e54
algorithm1832 <737634857@qq.com>
2026-01-30 06:10:24 +0000

    move paddle.utils.data to package

--- setup.py
+++ setup.py
@@ -2508,6 +2508,7 @@ def get_setup_parameters():
         'paddle',
         'paddle.libs',
         'paddle.utils',
+        'paddle.utils.data',
         'paddle.utils.gast',
         'paddle.utils.cpp_extension',
         'paddle.dataset',
