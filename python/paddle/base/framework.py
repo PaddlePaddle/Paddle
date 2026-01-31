b4603f4cf3080858f17088f928547fab64a782ff
Nyako Shigure <shigure_nyako@outlook.com>
2026-01-31 18:36:23 +0800

    docs: restore empty line in device_guard example

--- python/paddle/base/framework.py
+++ python/paddle/base/framework.py
@@ -8304,6 +8304,7 @@ def device_guard(device: str | None = None) -> Generator[None, None, None]:
     Examples:
         .. code-block:: pycon
 
+
             >>> # doctest: +REQUIRES(env:GPU)
             >>> import paddle
             >>> paddle.device.set_device('gpu')
