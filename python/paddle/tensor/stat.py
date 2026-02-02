499df95b6a3e55fd606b822dfe26af5cb0be66ad
SigureMo <sigure.qaq@gmail.com>
2026-02-03 00:36:06 +0800

    add magic trailing comma

--- python/paddle/tensor/stat.py
+++ python/paddle/tensor/stat.py
@@ -1118,7 +1118,13 @@ def nanquantile(
 
             >>> import paddle
 
-            >>> x = paddle.to_tensor([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]], dtype="float32")
+            >>> x = paddle.to_tensor(
+            ...     [
+            ...         [0, 1, 2, 3, 4],
+            ...         [5, 6, 7, 8, 9],
+            ...     ],
+            ...     dtype="float32",
+            ... )
             >>> x[0, 0] = float("nan")
 
             >>> y1 = paddle.nanquantile(x, q=0.5, axis=[0, 1])
