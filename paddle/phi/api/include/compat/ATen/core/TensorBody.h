d203d65418cb05d7331af374aa89dbaf7d17fb1e
Le-soleile <3516093767@qq.com>
2026-02-02 17:53:02 +0800

    add include

--- paddle/phi/api/include/compat/ATen/core/TensorBody.h
+++ paddle/phi/api/include/compat/ATen/core/TensorBody.h
@@ -18,6 +18,7 @@
 #include <ATen/indexing.h>
 #include <c10/core/Backend.h>
 #include <c10/core/Scalar.h>
+#include <c10/core/SymIntArrayRef.h>
 #include <optional>
 #include "paddle/phi/api/include/tensor.h"
 #include "paddle/phi/core/dense_tensor.h"
