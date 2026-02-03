92ae4f8291b0998dddbcbfdfd2aa443309403f36
algorithm1832 <737634857@qq.com>
2026-02-03 07:55:05 +0000

    remove unnecessary import

--- python/paddle/utils/data/__init__.py
+++ python/paddle/utils/data/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import dataloader as dataloader
 from .dataloader import (
     default_collate,
     get_worker_info,
