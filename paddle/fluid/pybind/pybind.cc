0e2136db366ce9f1ca33e46daaf121edb9ac1647
Wang Huan <wanghuan29@baidu.com>
2026-02-04 06:08:15 +0000

    refine

--- paddle/fluid/pybind/pybind.cc
+++ paddle/fluid/pybind/pybind.cc
@@ -3318,8 +3318,11 @@ All parameter, weight, gradient are variables in Paddle.
   m.def("_cuda_synchronize", [](const phi::GPUPlace &place) {
     phi::DeviceContextPool::Instance().Get(place)->Wait();
   });
-  m.def("_check_last_cuda_error",
-        []() { PADDLE_ENFORCE_GPU_SUCCESS(cudaGetLastError()); });
+  m.def("_check_last_cuda_error", []() {
+#if defined(PADDLE_WITH_CUDA)
+    PADDLE_ENFORCE_GPU_SUCCESS(cudaGetLastError());
+#endif
+  });
   m.def("_set_warmup", [](bool warmup) {
 #if defined(PADDLE_WITH_CUDA)
     paddle::memory::allocation::AutoGrowthBestFitAllocatorV2State::GetInstance()
