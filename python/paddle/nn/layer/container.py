a0bf82a6ae7457cde37ec2ccbf0322b1002fc364
Nyako Shigure <shigure_nyako@outlook.com>
2026-02-08 06:08:05 +0800

    [CodeStyle] Format OrderedDict example in docstring

--- python/paddle/nn/layer/container.py
+++ python/paddle/nn/layer/container.py
@@ -51,11 +51,13 @@ class LayerDict(Layer):
             >>> import numpy as np
             >>> from collections import OrderedDict
 
-            >>> sublayers = OrderedDict([
-            ...     ('conv1d', paddle.nn.Conv1D(3, 2, 3)),
-            ...     ('conv2d', paddle.nn.Conv2D(3, 2, 3)),
-            ...     ('conv3d', paddle.nn.Conv3D(4, 6, (3, 3, 3))),
-            >>> ])
+            >>> sublayers = OrderedDict(
+            ...     [
+            ...         ('conv1d', paddle.nn.Conv1D(3, 2, 3)),
+            ...         ('conv2d', paddle.nn.Conv2D(3, 2, 3)),
+            ...         ('conv3d', paddle.nn.Conv3D(4, 6, (3, 3, 3))),
+            ...     ]
+            ... )
 
             >>> layers_dict = paddle.nn.LayerDict(sublayers=sublayers)
 
