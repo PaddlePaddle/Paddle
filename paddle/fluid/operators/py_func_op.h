d0265ee1344b00869f47b3f8620204a3d787837e
木木 <2587018726@qq.com>
2026-02-08 13:28:05 +0800

    [Doc] Add C++ examples for py_func_op.h (task 247)
    Add C++ usage examples for paddle.static.nn.py_func, complete task 247.

--- paddle/fluid/operators/py_func_op.h
+++ paddle/fluid/operators/py_func_op.h
@@ -22,3 +22,14 @@ size_t AppendPythonCallableObjectAndReturnId(const ::pybind11::object &py_obj);
 
 }  // namespace operators
 }  // namespace paddle
+// C++示例：使用AppendPythonCallableObjectAndReturnId
+// 功能：注册Python可调用对象并获取ID
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+// 示例1：注册简单Python函数
+py::object py_add_func = py::module::import("__main__").attr("add");
+size_t add_func_id = paddle::operators::AppendPythonCallableObjectAndReturnId(py_add_func);
+// 示例2：注册带参数的Python函数
+py::object py_multiply_func = py::module::import("__main__").attr("multiply");
+size_t multiply_func_id = paddle::operators::AppendPythonCallableObjectAndReturnId(py_multiply_func);
