9af2f99228806de79d3559714f6b39a609ba4d6c
Nyako Shigure <shigure_nyako@outlook.com>
2026-02-09 16:29:17 +0800

    [CodeStyle][DocFormat] Address AdamW example review comment

--- python/paddle/optimizer/adamw.py
+++ python/paddle/optimizer/adamw.py
@@ -648,7 +648,7 @@ class AdamW(Optimizer):
                 >>> a = paddle.rand([2, 13], dtype="float32")
                 >>> linear = paddle.nn.Linear(13, 5)
                 >>> # This can be any optimizer supported by dygraph.
-                >>> opt = paddle.optimizer.AdamW(learning_rate=0.01, parameters=linear.parameters())
+                >>> opt = paddle.optimizer.AdamW(learning_rate=0.01, parameters=linear.parameters(),)
                 >>> out = linear(a)
                 >>> out.backward()
                 >>> opt.step()
