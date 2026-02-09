7ec6670483993dd87645e0c2cfa2e49d69f0c3a9
cangtianhuang <l1903374751@gmail.com>
2026-02-09 10:05:16 +0800

    refine

--- cmake/external/python.cmake
+++ cmake/external/python.cmake
@@ -20,8 +20,6 @@ if(DEFINED PYTHON_EXECUTABLE AND NOT DEFINED Python_EXECUTABLE)
   set(Python_EXECUTABLE ${PYTHON_EXECUTABLE})
 endif()
 
-set(Python_FIND_VIRTUALENV FIRST)
-
 # Find Python with minimum PY_VERSION specified or will raise error!
 find_package(
   Python ${PY_VERSION}
