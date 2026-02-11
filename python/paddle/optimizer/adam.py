c0680abaaa16cd140b54ba6b11cb86c17eaf446f
Nyako Shigure <shigure_nyako@outlook.com>
2026-02-11 09:12:31 +0800

    [CodeStyle][DocFormat][41] format adam docstring example

--- python/paddle/optimizer/adam.py
+++ python/paddle/optimizer/adam.py
@@ -485,7 +485,10 @@ class Adam(Optimizer):
                 >>> a = paddle.rand([2, 13], dtype="float32")
                 >>> linear = paddle.nn.Linear(13, 5)
                 >>> # This can be any optimizer supported by dygraph.
-                >>> adam = paddle.optimizer.Adam(learning_rate=0.01, parameters=linear.parameters(),)
+                >>> adam = paddle.optimizer.Adam(
+                ...     learning_rate=0.01,
+                ...     parameters=linear.parameters(),
+                ... )
                 >>> out = linear(a)
                 >>> out.backward()
                 >>> adam.step()
