500c8d7f74fb58a24f1f24d9aff26ab96bb2c32c
Le-soleile <3516093767@qq.com>
2026-02-13 16:08:12 +0800

    modify include conflict with develop

--- paddle/phi/api/include/compat/ATen/core/TensorBody.h
+++ paddle/phi/api/include/compat/ATen/core/TensorBody.h
@@ -17,11 +17,24 @@
 #include <ATen/core/TensorBase.h>
 #include <ATen/indexing.h>
 #include <c10/core/Backend.h>
+#include <c10/core/Scalar.h>
 #include <c10/util/OptionalArrayRef.h>
+#include "paddle/phi/api/include/api.h"
+#include "paddle/phi/api/include/tensor.h"
+#include "paddle/phi/common/int_array.h"
+#include "paddle/phi/common/scalar.h"
+#include "paddle/phi/core/dense_tensor.h"
+#include "paddle/phi/core/memory/malloc.h"
+
+#ifdef PADDLE_WITH_CUDA
+#include <cuda_runtime_api.h>
+#endif
+
+#include <c10/core/Device.h>
 #include <optional>
+#include <utility>
 #include <vector>
 #include "paddle/common/ddim.h"
-#include "paddle/phi/api/include/tensor.h"
 #include "paddle/phi/common/place.h"
 
 namespace at {  // NOLINT(build/namespaces)
