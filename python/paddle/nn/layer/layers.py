362295f4f7e22a8f4491c75b17f73cf6411e5e22
Nyako Shigure <shigure_nyako@outlook.com>
2026-02-18 11:03:12 +0800

    fix trailing comma in layers pycon example

--- python/paddle/nn/layer/layers.py
+++ python/paddle/nn/layer/layers.py
@@ -1707,7 +1707,10 @@ class Layer:
                 >>> value = np.arange(26).reshape(2, 13).astype("float32")
                 >>> a = paddle.to_tensor(value)
                 >>> linear = paddle.nn.Linear(13, 5)
-                >>> adam = paddle.optimizer.Adam(learning_rate=0.01, parameters=linear.parameters())
+                >>> adam = paddle.optimizer.Adam(
+                ...     learning_rate=0.01,
+                ...     parameters=linear.parameters(),
+                ... )
                 >>> out = linear(a)
                 >>> out.backward()
                 >>> adam.step()
