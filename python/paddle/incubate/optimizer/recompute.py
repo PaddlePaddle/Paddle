af9fa3ec4200bc216f4362847e64192934d94949
Nyako Shigure <shigure_nyako@outlook.com>
2026-02-20 07:25:44 +0800

    [CodeStyle][DocFormat][131] Use pycon marker in incubate optimizer docs examples

--- python/paddle/incubate/optimizer/recompute.py
+++ python/paddle/incubate/optimizer/recompute.py
@@ -47,7 +47,7 @@ class RecomputeOptimizer(Optimizer):
         optimizer (Optimizer): The optimizer that is applied to parameters.
 
     Examples:
-        .. code-block:: python
+        .. code-block:: pycon
 
             >>> import paddle
             >>> import numpy as np
@@ -55,20 +55,19 @@ class RecomputeOptimizer(Optimizer):
             >>> paddle.enable_static()
 
             >>> def gen_data():
-            ...     return {"x": np.random.random(size=(32, 32)).astype('float32'),
-            ...     "y": np.random.randint(2, size=(32, 1)).astype('int64')}
+            ...     return {
+            ...         "x": np.random.random(size=(32, 32)).astype('float32'),
+            ...         "y": np.random.randint(2, size=(32, 1)).astype('int64'),
+            ...     }
             >>> def mlp(input_x, input_y, hid_dim=128, label_dim=2):
             ...     print(input_x)
             ...     fc_1 = paddle.static.nn.fc(x=input_x, size=hid_dim)
             ...     prediction = paddle.static.nn.fc(x=[fc_1], size=label_dim, activation='softmax')
-            ...     cost = paddle.nn.functional.cross_entropy(
-            ...         input=prediction, label=input_y,
-            ...         reduction='none', use_softmax=False
-            ...     )
+            ...     cost = paddle.nn.functional.cross_entropy(input=prediction, label=input_y, reduction='none', use_softmax=False)
             ...     sum_cost = paddle.mean(cost)
             ...     return sum_cost, fc_1, prediction
-            >>> input_x = paddle.static.data(name="x", shape=[-1,32], dtype='float32')
-            >>> input_y = paddle.static.data(name="y", shape=[-1,1], dtype='int64')
+            >>> input_x = paddle.static.data(name="x", shape=[-1, 32], dtype='float32')
+            >>> input_y = paddle.static.data(name="y", shape=[-1, 1], dtype='int64')
             >>> cost, fc_1, pred = mlp(input_x, input_y)
 
             >>> sgd = paddle.optimizer.Adam(learning_rate=0.01)
@@ -84,9 +83,7 @@ class RecomputeOptimizer(Optimizer):
             >>> step = 10
 
             >>> for i in range(step):
-            ...     cost_val = exe.run(feed=gen_data(),
-            ...             program=paddle.static.default_main_program(),
-            ...             fetch_list=[cost.name])
+            ...     cost_val = exe.run(feed=gen_data(), program=paddle.static.default_main_program(), fetch_list=[cost.name])
             ...     print("step=%d cost=%f" % (i, cost_val[0]))
             var x : DENSE_TENSOR.shape(-1, 32).dtype(float32).stop_gradient(True)
             Finished optimize
@@ -142,7 +139,7 @@ class RecomputeOptimizer(Optimizer):
             state_dict: the dict load by load_persistable method
 
         Examples:
-            .. code-block:: python
+            .. code-block:: pycon
 
                 >>> import paddle
 
@@ -150,15 +147,12 @@ class RecomputeOptimizer(Optimizer):
                 >>> def mlp(input_x, input_y, hid_dim=128, label_dim=2):
                 ...     fc_1 = paddle.static.nn.fc(x=input_x, size=hid_dim)
                 ...     prediction = paddle.static.nn.fc(x=[fc_1], size=label_dim, activation='softmax')
-                ...     cost = paddle.nn.functional.cross_entropy(
-                ...         input=prediction, label=input_y,
-                ...         reduction='none', use_softmax=False
-                ...     )
+                ...     cost = paddle.nn.functional.cross_entropy(input=prediction, label=input_y, reduction='none', use_softmax=False)
                 ...     sum_cost = paddle.mean(cost)
                 ...     return sum_cost, fc_1, prediction
 
-                >>> input_x = paddle.static.data(name="x", shape=[-1,32], dtype='float32')
-                >>> input_y = paddle.static.data(name="y", shape=[-1,1], dtype='int64')
+                >>> input_x = paddle.static.data(name="x", shape=[-1, 32], dtype='float32')
+                >>> input_y = paddle.static.data(name="y", shape=[-1, 1], dtype='int64')
                 >>> cost, fc_1, pred = mlp(input_x, input_y)
                 >>> print("Finished FF")
                 Finished FF
@@ -188,7 +182,7 @@ class RecomputeOptimizer(Optimizer):
             list: A list of operators appended to the current program.
 
         Examples:
-            .. code-block:: python
+            .. code-block:: pycon
 
                 >>> import paddle
                 >>> import paddle.base.framework as framework
@@ -198,15 +192,12 @@ class RecomputeOptimizer(Optimizer):
                 >>> def mlp(input_x, input_y, hid_dim=128, label_dim=2):
                 ...     fc_1 = paddle.static.nn.fc(x=input_x, size=hid_dim)
                 ...     prediction = paddle.static.nn.fc(x=[fc_1], size=label_dim, activation='softmax')
-                ...     cost = paddle.nn.functional.cross_entropy(
-                ...         input=prediction, label=input_y,
-                ...         reduction='none', use_softmax=False
-                ...     )
+                ...     cost = paddle.nn.functional.cross_entropy(input=prediction, label=input_y, reduction='none', use_softmax=False)
                 ...     sum_cost = paddle.mean(cost)
                 ...     return sum_cost, fc_1, prediction
 
-                >>> input_x = paddle.static.data(name="x", shape=[-1,32], dtype='float32')
-                >>> input_y = paddle.static.data(name="y", shape=[-1,1], dtype='int64')
+                >>> input_x = paddle.static.data(name="x", shape=[-1, 32], dtype='float32')
+                >>> input_y = paddle.static.data(name="y", shape=[-1, 1], dtype='int64')
                 >>> cost, fc_1, pred = mlp(input_x, input_y)
                 >>> print("Finished FF")
                 Finished FF
@@ -214,11 +205,7 @@ class RecomputeOptimizer(Optimizer):
                 >>> sgd = paddle.optimizer.Adam(learning_rate=0.01)
                 >>> sgd = paddle.incubate.optimizer.RecomputeOptimizer(sgd)
                 >>> sgd._set_checkpoints([fc_1, pred])
-                >>> params_grads = sgd.backward(
-                ...     cost,
-                ...     startup_program=None,
-                ...     parameter_list=None,
-                ...     no_grad_set=None)
+                >>> params_grads = sgd.backward(cost, startup_program=None, parameter_list=None, no_grad_set=None)
 
                 >>> program = cost.block.program
                 >>> with framework.program_guard(program, None):
@@ -636,7 +623,7 @@ class RecomputeOptimizer(Optimizer):
             checkpoints (list): list of Variables as checkpoints
 
         Examples:
-            .. code-block:: python
+            .. code-block:: pycon
 
                 >>> import paddle
 
@@ -645,15 +632,12 @@ class RecomputeOptimizer(Optimizer):
                 >>> def mlp(input_x, input_y, hid_dim=128, label_dim=2):
                 ...     fc_1 = paddle.static.nn.fc(x=input_x, size=hid_dim)
                 ...     prediction = paddle.static.nn.fc(x=[fc_1], size=label_dim, activation='softmax')
-                ...     cost = paddle.nn.functional.cross_entropy(
-                ...         input=prediction, label=input_y,
-                ...         reduction='none', use_softmax=False
-                ...     )
+                ...     cost = paddle.nn.functional.cross_entropy(input=prediction, label=input_y, reduction='none', use_softmax=False)
                 ...     sum_cost = paddle.mean(cost)
                 ...     return sum_cost, fc_1, prediction
 
-                >>> input_x = paddle.static.data(name="x", shape=[-1,32], dtype='float32')
-                >>> input_y = paddle.static.data(name="y", shape=[-1,1], dtype='int64')
+                >>> input_x = paddle.static.data(name="x", shape=[-1, 32], dtype='float32')
+                >>> input_y = paddle.static.data(name="y", shape=[-1, 1], dtype='int64')
                 >>> cost, fc_1, pred = mlp(input_x, input_y)
                 >>> print("Finished FF")
                 Finished FF
@@ -661,11 +645,7 @@ class RecomputeOptimizer(Optimizer):
                 >>> sgd = paddle.optimizer.Adam(learning_rate=0.01)
                 >>> sgd = paddle.incubate.optimizer.RecomputeOptimizer(sgd)
                 >>> sgd._set_checkpoints([fc_1, pred])
-                >>> params_grads = sgd.backward(
-                ...     cost,
-                ...     startup_program=None,
-                ...     parameter_list=None,
-                ...     no_grad_set=None)
+                >>> params_grads = sgd.backward(cost, startup_program=None, parameter_list=None, no_grad_set=None)
                 >>> print("Finished backward")
                 Finished backward
         """
@@ -719,7 +699,7 @@ class RecomputeOptimizer(Optimizer):
                 in `parameter_list`.
             params_grads (list): list of (param, grad) pair to do optimization.
         Examples:
-            .. code-block:: python
+            .. code-block:: pycon
 
                 >>> import paddle
 
@@ -728,15 +708,12 @@ class RecomputeOptimizer(Optimizer):
                 >>> def mlp(input_x, input_y, hid_dim=128, label_dim=2):
                 ...     fc_1 = paddle.static.nn.fc(x=input_x, size=hid_dim)
                 ...     prediction = paddle.static.nn.fc(x=[fc_1], size=label_dim, activation='softmax')
-                ...     cost = paddle.nn.functional.cross_entropy(
-                ...         input=prediction, label=input_y,
-                ...         reduction='none', use_softmax=False
-                ...     )
+                ...     cost = paddle.nn.functional.cross_entropy(input=prediction, label=input_y, reduction='none', use_softmax=False)
                 ...     sum_cost = paddle.mean(cost)
                 ...     return sum_cost, fc_1, prediction
 
-                >>> input_x = paddle.static.data(name="x", shape=[-1,32], dtype='float32')
-                >>> input_y = paddle.static.data(name="y", shape=[-1,1], dtype='int64')
+                >>> input_x = paddle.static.data(name="x", shape=[-1, 32], dtype='float32')
+                >>> input_y = paddle.static.data(name="y", shape=[-1, 1], dtype='int64')
                 >>> cost, fc_1, pred = mlp(input_x, input_y)
                 >>> print("Finished FF")
                 Finished FF
@@ -744,14 +721,9 @@ class RecomputeOptimizer(Optimizer):
                 >>> sgd = paddle.optimizer.Adam(learning_rate=0.01)
                 >>> sgd = paddle.incubate.optimizer.RecomputeOptimizer(sgd)
                 >>> sgd._set_checkpoints([fc_1, pred])
-                >>> params_grads = sgd.backward(
-                ...     cost,
-                ...     startup_program=None,
-                ...     parameter_list=None,
-                ...     no_grad_set=None)
+                >>> params_grads = sgd.backward(cost, startup_program=None, parameter_list=None, no_grad_set=None)
 
-                >>> optimize_ops = sgd.apply_optimize(
-                ...     cost, startup_program=None, params_grads=params_grads)
+                >>> optimize_ops = sgd.apply_optimize(cost, startup_program=None, params_grads=params_grads)
 
                 >>> print("Finished apply_optimize")
                 Finished apply_optimize
