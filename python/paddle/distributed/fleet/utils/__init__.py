11941a19cb2a060946ee16390460e57163c8af5c
Nyako Shigure <shigure_nyako@outlook.com>
2026-02-19 20:25:40 +0800

    [CodeStyle][DocFormat][119] apply ruff-format for pre-commit

--- python/paddle/distributed/fleet/utils/__init__.py
+++ python/paddle/distributed/fleet/utils/__init__.py
@@ -72,23 +72,13 @@ def recompute(
             ...         (block_name + "_relu_2", paddle.nn.ReLU()),
             ...     )
             ...     if is_last:
-            ...         block.add_sublayer(
-            ...             block_name + "_fc_2",
-            ...             paddle.nn.Linear(
-            ...                 input_size, 1, bias_attr=False
-            ...             )
-            ...         )
+            ...         block.add_sublayer(block_name + "_fc_2", paddle.nn.Linear(input_size, 1, bias_attr=False))
             ...     else:
-            ...         block.add_sublayer(
-            ...             block_name + "_fc_2",
-            ...             paddle.nn.Linear(input_size, input_size, bias_attr=False)
-            ...         )
+            ...         block.add_sublayer(block_name + "_fc_2", paddle.nn.Linear(input_size, input_size, bias_attr=False))
             ...     return block
 
             >>> class Naive_fc_net(paddle.nn.Layer):
-            ...     def __init__(self, input_size=10,
-            ...                 recompute_blocks=[1, 3],
-            ...                 recompute_kwargs={}):
+            ...     def __init__(self, input_size=10, recompute_blocks=[1, 3], recompute_kwargs={}):
             ...         super().__init__()
             ...         self.recompute_blocks = recompute_blocks
             ...         self.recompute_kwargs = recompute_kwargs
@@ -98,6 +88,7 @@ def recompute(
             ...         self.runfunc3 = get_fc_block(3, input_size, is_last=False)
             ...         self.runfunc4 = get_fc_block(4, input_size, is_last=True)
             ...         self.total_func = [self.runfunc0, self.runfunc1, self.runfunc2, self.runfunc3, self.runfunc4]
+            ...
             ...     def forward(self, inputs):
             ...         nums = len(self.total_func)
             ...         for i in range(nums):
@@ -114,10 +105,7 @@ def recompute(
             ...     if cuda_state:
             ...         paddle.set_cuda_rng_state(cuda_state)
             ...     batch_size, input_size = 1, 10
-            ...     model = Naive_fc_net(
-            ...         input_size,
-            ...         recompute_blocks=recompute_block,
-            ...         recompute_kwargs=recompute_kwargs)
+            ...     model = Naive_fc_net(input_size, recompute_blocks=recompute_block, recompute_kwargs=recompute_kwargs)
             ...     optimizer = paddle.optimizer.SGD(learning_rate=0.01, parameters=model.parameters())
             ...     loss_ = []
             ...     param_ = []
@@ -136,9 +124,7 @@ def recompute(
 
             >>> cuda_state = paddle.get_cuda_rng_state()
             >>> # without recompute
-            >>> loss_ref, param_ref, grad_ref = run_model(
-            ...     cuda_state, recompute_block=[]
-            ... )
+            >>> loss_ref, param_ref, grad_ref = run_model(cuda_state, recompute_block=[])
 
             >>> loss, param, grad = run_model(cuda_state, recompute_block=[1, 2])
             >>> print("normal_loss: {}, recompute_loss: {}".format(loss_ref, loss))
