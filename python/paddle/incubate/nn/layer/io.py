59edb35ec388ebf0e8ddcc49df5a580b3f7111b0
Nyako Shigure <shigure_nyako@outlook.com>
2026-02-21 01:25:44 +0800

    [CodeStyle][DocFormat][149] Add trailing comma in pycon example

--- python/paddle/incubate/nn/layer/io.py
+++ python/paddle/incubate/nn/layer/io.py
@@ -64,7 +64,11 @@ class ListenAndServ:
             >>> with paddle.static.program_guard(main):
             ...     serv = ListenAndServ("127.0.0.1:6170", ["X"], optimizer_mode=False)
             ...     with serv.do():
-            ...         x = paddle.static.data(shape=[32, 32], dtype='float32', name="X")
+            ...         x = paddle.static.data(
+            ...             shape=[32, 32],
+            ...             dtype='float32',
+            ...             name="X",
+            ...         )
             ...         paddle.nn.initializer.Constant(value=1.0)(x, main.global_block())
             ...         paddle.scale(x=x, scale=10.0)
 
