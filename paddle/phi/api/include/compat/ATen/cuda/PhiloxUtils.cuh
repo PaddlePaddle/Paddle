66b2740818f8a7738c0d0977268c773ddf1a8a5e
youge325 <A372707325@126.com>
2026-02-28 16:56:57 +0800

    [Cpp API Compatibility] add Philox related APIs

---
+++ paddle/phi/api/include/compat/ATen/cuda/PhiloxUtils.cuh
@@ -0,0 +1,63 @@
+// Copyright (c) 2026 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// #The file has been adapted from pytorch project
+// #Licensed under  BSD-style license -
+// https://github.com/pytorch/pytorch/blob/main/LICENSE
+
+#pragma once
+
+#include <ATen/cuda/PhiloxCudaState.h>
+#include <tuple>
+
+namespace at::cuda::philox {
+
+// In-kernel call to retrieve philox seed and offset from a PhiloxCudaState
+// instance whether that instance was created with graph capture underway or
+// not. See Note [CUDA Graph-safe RNG states].
+//
+// We can't write a __device__ function in CUDAGeneratorImpl.h, because it's in
+// ATen. Also, whatever call unpacks PhiloxCudaState in consumer kernels must be
+// inlineable. Easiest thing that comes to mind is, define a __device__ unpack
+// helper here, in ATen/cuda.
+//
+// The raw definition lives in its own file so jit codegen can easily copy it.
+__host__ __device__ __forceinline__ std::tuple<uint64_t, uint64_t> unpack(
+    at::PhiloxCudaState arg) {
+  if (arg.captured_) {
+    // static_cast avoids "warning: invalid narrowing conversion from "long" to
+    // "unsigned long".
+    // *(arg.offset_.ptr) is a broadcast load of a single int64_t to the entire
+    // kernel. For most threads' reads it will hit in cache, so it shouldn't
+    // hurt performance.
+    return std::make_tuple(
+        static_cast<uint64_t>(*arg.seed_.ptr),
+        static_cast<uint64_t>(*(arg.offset_.ptr) + arg.offset_intragraph_));
+  } else {
+    return std::make_tuple(arg.seed_.val, arg.offset_.val);
+  }
+}
+
+// Adapted from TE
+// extract seed and offset from PhiloxCudaState
+__global__ void unpack_cudnn(at::PhiloxCudaState arg,
+                             int64_t* seed_ptr,
+                             int64_t* offset_ptr);
+
+void unpack_cudnn_wrapper(at::PhiloxCudaState arg,
+                          int64_t* seed_ptr,
+                          int64_t* offset_ptr,
+                          cudaStream_t stream);
+
+}  // namespace at::cuda::philox
