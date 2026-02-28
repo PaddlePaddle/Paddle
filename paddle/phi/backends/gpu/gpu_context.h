6435716522db76e70615a4b3ad1792e88be1d238
Zhaowu Pan <panzhaowu@baidu.com>
2026-02-28 10:09:44 +0800

    Cherry pick workspace related changes (#77876)
    * cp_workspace_opt
    
    * fix lag
    
    * cp
    
    * cp ixuva
    
    * cp optest fix

--- paddle/phi/backends/gpu/gpu_context.h
+++ paddle/phi/backends/gpu/gpu_context.h
@@ -26,6 +26,7 @@ limitations under the License. */
 #include <array>
 #include <functional>
 #include <mutex>
+#include <utility>
 
 #include "paddle/phi/backends/gpu/forwards.h"
 #include "paddle/phi/backends/gpu/gpu_decls.h"
@@ -115,6 +116,10 @@ class PADDLE_API GPUContext : public DeviceContext,
 
   /*! \brief  Return cublasLt handle in the device context. */
   blasLtHandle_t cublaslt_handle() const;
+
+  /*! \brief  Return persistent cublasLt workspace (grow-only, multi-stream
+   * safe). */
+  std::pair<void*, size_t> cublaslt_workspace(size_t required_size) const;
 
   /*! \brief  Return cusolver handle in the device context. */
   solverHandle_t cusolver_dn_handle() const;
