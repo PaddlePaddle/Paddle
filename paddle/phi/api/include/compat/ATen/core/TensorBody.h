1713cf3b692601e6100b18407e6ea8c0a826573c
Le-soleile <3516093767@qq.com>
2026-03-02 19:06:04 +0800

    fix

--- paddle/phi/api/include/compat/ATen/core/TensorBody.h
+++ paddle/phi/api/include/compat/ATen/core/TensorBody.h
@@ -697,7 +697,6 @@ class Tensor : public TensorBase {
   PaddleTensor& _PD_GetInner() { return tensor_; }
 };  // NOLINT(readability/braces)
 }  // namespace at
-
 namespace torch {
 using at::Tensor;
 }  // namespace torch
