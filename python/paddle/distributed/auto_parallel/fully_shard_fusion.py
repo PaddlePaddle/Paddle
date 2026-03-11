1f71d05b164fc2fb1f954482706dbcf387feed52
ZhenxingLi <lizhenxing02@baidu.com>
2026-03-11 21:47:03 +0800

    update

--- python/paddle/distributed/auto_parallel/fully_shard_fusion.py
+++ python/paddle/distributed/auto_parallel/fully_shard_fusion.py
@@ -335,6 +335,7 @@ class FSDPCommManager:
         # for double buffer mechanism config
         self.double_buffer_limit = double_buffer_limit
         self.buffer_cnt_in_using = 0
+        self.need_zero_grads = True
 
     def _release_one_buffer_if_needed(self):
         # Release a buffer with the READY status if needed
@@ -444,6 +445,11 @@ class FSDPCommManager:
                 group.params_buffer.status = BufferState.READY
 
     def reduce_scatter_grads(self, param):
+        if self.need_zero_grads:
+            self.need_zero_grads = False
+            for group in self.buffer_manager.buffer_groups:
+                if group.grads_buffer is not None:
+                    group.grads_buffer.data_buffer.zero_()
         gid = self.buffer_manager.param_to_buffer_id[param.name]
         group = self.buffer_manager.buffer_groups[gid]
         group.grads_use_cnt += 1
@@ -460,11 +466,11 @@ class FSDPCommManager:
 
             tmp_buffer = grads_buffer.get_tmp_buffer()
             shard_size = grads_buffer.data_buffer.shape[0]
-            rs_output = tmp_buffer._slice(0, shard_size)
+            grad_buffer_shard = tmp_buffer._slice(0, shard_size)
             if self.enable_overlap:
                 # Comm grads async and check all comm_task before optimizer update
                 grads_buffer.comm_task = paddle.distributed.reduce_scatter(
-                    rs_output,
+                    grad_buffer_shard,
                     tmp_buffer,
                     op=paddle.distributed.ReduceOp.SUM,
                     group=self.buffer_manager._fsdp_group,
@@ -475,14 +481,13 @@ class FSDPCommManager:
                 self.grad_reduce_queue.append(grads_buffer)
             else:
                 paddle.distributed.reduce_scatter(
-                    rs_output,
+                    grad_buffer_shard,
                     tmp_buffer,
                     op=paddle.distributed.ReduceOp.SUM,
                     group=self.buffer_manager._fsdp_group,
                     sync_op=False,
                 ).wait()
-
-                grads_buffer.data_buffer.add_(rs_output)
+                grads_buffer.data_buffer.add_(grad_buffer_shard)
                 grads_buffer.clear_tmp_buffer()
 
     def _wait_for_grad_comm(self, queue_limit=2):
@@ -493,9 +498,10 @@ class FSDPCommManager:
             if grads_buffer.comm_task is not None:
                 grads_buffer.comm_task.wait()
                 grads_buffer.comm_task = None
-            shard_size = grads_buffer.data_buffer.shape[0]
-            rs_output = grads_buffer.tmp_data_buffer._slice(0, shard_size)
-            grads_buffer.data_buffer.add_(rs_output)
+                tmp_buffer = grads_buffer.get_tmp_buffer()
+                shard_size = grads_buffer.data_buffer.shape[0]
+                grad_buffer_shard = tmp_buffer._slice(0, shard_size)
+                grads_buffer.data_buffer.add_(grad_buffer_shard)
             grads_buffer.clear_tmp_buffer()
 
     def finish_grads_sync(self):
@@ -585,6 +591,7 @@ class FullyShardFusion:
     def comm_sync_and_reset_status(self):
         self.comm_manager.finish_grads_sync()
         self.comm_manager.reset_params_buffer_status()
+        self.comm_manager.need_zero_grads = True
         # Reset main_grad for all trainable parameters
         for param in self.model.parameters():
             if param.trainable:
@@ -627,7 +634,7 @@ class FullyShardFusion:
                         grad.process_mesh,
                         grad.placements,
                     )
-                    param.main_grad._local_value().add_(grad._local_value())
+                    param.main_grad._local_value().copy_(grad._local_value())
                     grad._clear_data()
                 comm_manager.shard_params([param], is_backward=True)
                 comm_manager.reduce_scatter_grads(param)
