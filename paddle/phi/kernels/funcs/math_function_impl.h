ce74db05887ddaa47473d6739262db3e26d5b2fd
zrr1999 <2742392377@qq.com>
2026-03-18 12:21:52 +0000

    fix transpose

--- paddle/phi/kernels/funcs/math_function_impl.h
+++ paddle/phi/kernels/funcs/math_function_impl.h
@@ -59,12 +59,7 @@ void Transpose<DeviceContext, T, Rank>::operator()(
   // use 32bit index to speed up computation
   bool use_32bit_index = eigen_out.size() < Eigen::NumTraits<int>::highest();
   bool is_gpu_place = dev_ctx.GetPlace().GetType() == AllocationType::GPU;
-  if (use_32bit_index && is_gpu_place) {
-    To32BitIndex(eigen_out).device(*dev) =
-        To32BitIndex(eigen_in).shuffle(permute);
-  } else {
-    eigen_out.device(*dev) = eigen_in.shuffle(permute);
-  }
+  eigen_out.device(*dev) = eigen_in.shuffle(permute);
 }
 
 template <typename DeviceContext, typename T>
