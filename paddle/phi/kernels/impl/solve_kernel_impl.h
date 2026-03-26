07ebf2aa1776be9ad32e368ca1aa75f2fecf27b2
co63oc <4617245+co63oc@users.noreply.github.com>
2026-03-26 11:56:26 +0800

    fix

--- paddle/phi/kernels/impl/solve_kernel_impl.h
+++ paddle/phi/kernels/impl/solve_kernel_impl.h
@@ -187,10 +187,10 @@ void SolveKernel(const Context& dev_ctx,
   if (x.numel() == 0 || y.numel() == 0) {
     auto x_dims = x.dims();
     auto y_dims = y.dims();
-    std::vector<int> out_dims;
+    std::vector<int64_t> out_dims;
     if (y_dims.size() == 1) {
       out_dims =
-          std::vector<int>(x_dims.Get(), x_dims.Get() + x_dims.size() - 2);
+          std::vector<int64_t>(x_dims.Get(), x_dims.Get() + x_dims.size() - 2);
       out_dims.push_back(y_dims[y_dims.size() - 1]);
     } else {
       // broadcast
