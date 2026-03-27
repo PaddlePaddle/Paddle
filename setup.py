8a7e406a7c5c7d56ec1cb8d13148299ca3f0f063
risemeup1 <515586620@qq.com>
2026-03-28 01:25:07 +0800

    export dnn.h

--- setup.py
+++ setup.py
@@ -2331,17 +2331,12 @@ def get_headers():
 
     if env_dict.get("WITH_ONEDNN") == 'ON':
         headers += list(
-            find_files('*', env_dict.get("ONEDNN_INSTALL_DIR") + '/include')
-        )  # mkldnn
-
-    if env_dict.get("WITH_MKL") == 'ON':
-        headers += list(
             find_files(
                 '*',
                 env_dict.get("ONEDNN_INSTALL_DIR") + '/include',
                 recursive=True,
             )
-        )
+        )  # mkldnn
 
     if env_dict.get("WITH_OPENVINO") == 'ON':
         headers += list(
