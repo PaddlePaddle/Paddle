14f01e44f788410eb223270b6d2ed9ffd526c4c4
youge325 <A372707325@126.com>
2026-04-03 20:56:19 +0800

    [Cpp API Compatibility] Complement ScalarType

---
+++ paddle/phi/api/include/compat/c10/util/qint8.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2026 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cstdint>
+
+namespace c10 {
+
+struct qint8 {
+  constexpr qint8() = default;
+  explicit constexpr qint8(int8_t value) : val_(value) {}
+
+  int8_t val_{0};
+};
+
+}  // namespace c10
+
+namespace at {
+using c10::qint8;
+}  // namespace at
+
+namespace torch {
+using c10::qint8;
+}  // namespace torch
