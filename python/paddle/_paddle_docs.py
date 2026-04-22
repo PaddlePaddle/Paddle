f28ebcd4c8681cca8c2e578569f9dcfebfaff901
Manfredss <ms1419344875@outlook.com>
2026-04-23 00:23:06 +0800

    add SKIP per example code

--- python/paddle/_paddle_docs.py
+++ python/paddle/_paddle_docs.py
@@ -356,6 +356,7 @@ add_doc_and_signature(
             >>> # doctest: +SKIP("This API is newly added, doctest to be enabled later")
             >>> import paddle
 
+            >>> # doctest: +SKIP("This API is newly added, doctest to be enabled later")
             >>> x = paddle.to_tensor([[0.1, 0.9, 0.9, 0.9],
             ...                       [0.9, 0.9, 0.6, 0.7]],
             ...                       dtype='float64', stop_gradient=False)
@@ -367,6 +368,7 @@ add_doc_and_signature(
             Tensor(shape=[], dtype=float64, place=Place(cpu), stop_gradient=False,
             0.90000000)
 
+            >>> # doctest: +SKIP("This API is newly added, doctest to be enabled later")
             >>> min_val, max_val = paddle.aminmax(x, axis=0)
             >>> min_val
             Tensor(shape=[4], dtype=float64, place=Place(cpu), stop_gradient=False,
@@ -375,6 +377,7 @@ add_doc_and_signature(
             Tensor(shape=[4], dtype=float64, place=Place(cpu), stop_gradient=False,
             [0.90000000, 0.90000000, 0.90000000, 0.90000000])
 
+            >>> # doctest: +SKIP("This API is newly added, doctest to be enabled later")
             >>> min_val, max_val = paddle.aminmax(x, axis=1, keepdim=True)
             >>> min_val
             Tensor(shape=[2, 1], dtype=float64, place=Place(cpu), stop_gradient=False,
