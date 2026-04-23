1d488599ac5f4eef611f0125fedd704b93b1b7f4
Manfredss <ms1419344875@outlook.com>
2026-04-24 06:13:06 +0800

    Conv2d also fails, try Conv3d

--- test/legacy_test/test_api_compatibility_part3.py
+++ test/legacy_test/test_api_compatibility_part3.py
@@ -2141,6 +2141,10 @@ class TestConv1dTransposeAPI(unittest.TestCase):
 
 
 # Test conv2d_transpose / conv_transpose2d compatibility
+@unittest.skipIf(
+    sys.platform == 'win32',
+    "Conv transpose compatibility tests not supported on Windows-Inference",
+)
 class TestConv2dTransposeAPI(unittest.TestCase):
     def setUp(self):
         np.random.seed(2025)
@@ -2273,10 +2277,6 @@ class TestConv2dTransposeAPI(unittest.TestCase):
 
 
 # Test conv3d_transpose / conv_transpose3d compatibility
-@unittest.skipIf(
-    sys.platform == 'win32',
-    "Conv transpose compatibility tests not supported on Windows-Inference",
-)
 class TestConv3dTransposeAPI(unittest.TestCase):
     def setUp(self):
         np.random.seed(2025)
