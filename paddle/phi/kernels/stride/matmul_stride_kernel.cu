5a13c4347eaf95ce3e6f40f5f16c12ecc202e966
Zhaowu Pan <panzhaowu@baidu.com>
2026-04-27 10:16:20 +0800

    Cp Matmul precision alignment fix in 2d strided weight (#78749)
    * cp wstride fix
    
    * Update copyright year in matmul_stride_kernel.cu

--- paddle/phi/kernels/stride/matmul_stride_kernel.cu
+++ paddle/phi/kernels/stride/matmul_stride_kernel.cu
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+// Copyright (c) 2026 PaddlePaddle Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -185,7 +185,7 @@ void MatmulStrideKernel(const Context &dev_ctx,
                                                              &y_stride,
                                                              &y_axis)) {
     auto y_trans_dims = y_axis.size();
-    if (y_axis.size() > 2 && y_axis[y_trans_dims - 1] == y_trans_dims - 2 &&
+    if (y_axis.size() >= 2 && y_axis[y_trans_dims - 1] == y_trans_dims - 2 &&
         y_axis[y_trans_dims - 2] == y_trans_dims - 1) {
       transpose_y = !transpose_y;
       y_meta.dims = y_shape;
