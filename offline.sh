ac6f4593b7c4c1d62aa28a495dbe6c15c7a593bf
lonelygsh <490991455@qq.com>
2026-04-29 17:22:26 +0800

    update

--- offline.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=/root/paddlejob/workspace/env_run/output/lizexu/Paddle-feisheng/test/legacy_test:$PYTHONPATH
-export PYTHONPATH=/root/paddlejob/workspace/env_run/output/lizexu/Paddle-feisheng:$PYTHONPATH
-python -m pytest -sv test/legacy_test/test_cuda_graph.py
