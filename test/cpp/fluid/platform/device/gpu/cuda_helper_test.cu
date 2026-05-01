6e69019cb32879e8d1157e7d23fa3a6ad08aaa00
co63oc <4617245+co63oc@users.noreply.github.com>
2026-05-01 13:56:56 +0800

    fix

--- test/cpp/fluid/platform/device/gpu/cuda_helper_test.cu
+++ test/cpp/fluid/platform/device/gpu/cuda_helper_test.cu
@@ -23,7 +23,7 @@
 
 #include "paddle/phi/backends/gpu/gpu_device_function.h"
 #include "paddle/phi/backends/gpu/gpu_primitives.h"
-#include "paddle/phi/common/float16.h"
+#include "paddle/phi/common/data_type.h"
 #include "paddle/phi/core/platform/device/gpu/gpu_helper.h"
 
 using phi::float16;
