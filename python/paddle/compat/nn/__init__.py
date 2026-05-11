875a85bc28cb4dbb58586fbaaa52433d4eab27f0
Le-soleile <3516093767@qq.com>
2026-05-11 09:29:24 -0400

    Modify Judgment conditions

--- python/paddle/compat/nn/__init__.py
+++ python/paddle/compat/nn/__init__.py
@@ -601,9 +601,9 @@ class Linear(nn.Layer):
         """
 
         bound = 1 / sqrt(self.in_features) if self.in_features > 0 else 0
-        if all(dim > 0 for dim in self.weight.shape):
+        if self.weight.size > 0:
             nn.init.uniform_(self.weight, -bound, bound)
-        if self.bias is not None and all(dim > 0 for dim in self.bias.shape):
+        if self.bias is not None and self.bias.size > 0:
             nn.init.uniform_(self.bias, -bound, bound)
 
 
