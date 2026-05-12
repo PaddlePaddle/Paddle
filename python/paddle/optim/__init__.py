d21c93309a8f01ac094bf209666d5aa8d751ee3b
ALGO1832 <737634857@qq.com>
2026-05-12 17:31:25 +0800

    [API Compatibility] Add alias for optimizers in `paddle.optimizer` (#78931)
    * add alias for optimizers in paddle.optimizer
    
    * add alias for TensorDataset and refine test
    
    * add decorator, overload, test
    
    * merge api alias tests

---
+++ python/paddle/optim/__init__.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2026 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .adadelta import Adadelta
+from .adagrad import Adagrad
+from .adam import Adam
+from .adamax import Adamax
+from .adamw import AdamW
+from .asgd import ASGD
+from .lbfgs import LBFGS
+from .muon import Muon
+from .nadam import NAdam
+from .optimizer import Optimizer
+from .radam import RAdam
+from .rmsprop import RMSProp
+from .rprop import Rprop
+from .sgd import SGD
+
+__all__ = [
+    "Adadelta",
+    "Adagrad",
+    "Adam",
+    "Adamax",
+    "AdamW",
+    "ASGD",
+    "LBFGS",
+    "Muon",
+    "NAdam",
+    "Optimizer",
+    "RAdam",
+    "RMSProp",
+    "Rprop",
+    "SGD",
+]
