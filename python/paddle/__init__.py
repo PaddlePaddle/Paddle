520e8390b8b06f1d7ef1b30f3df55d38ad2a6c9e
Le-soleile <3516093767@qq.com>
2026-05-22 11:09:07 -0400

    Modify alias method

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -259,78 +259,10 @@ from paddle import (
     vision as vision,
 )
 
-
-def _set_module_alias(alias_path, origin_module, submodule_names=()):
-    _sys.modules[alias_path] = origin_module
-    for submodule_name in submodule_names:
-        origin_path = f'{origin_module.__name__}.{submodule_name}'
-        __import__(origin_path)
-        _sys.modules[f'{alias_path}.{submodule_name}'] = _sys.modules[
-            origin_path
-        ]
-
-
 optim = optimizer
 distributions = distribution
-_set_module_alias(
-    'paddle.optim',
-    optimizer,
-    (
-        'adadelta',
-        'adagrad',
-        'adam',
-        'adamax',
-        'adamw',
-        'asgd',
-        'lamb',
-        'lbfgs',
-        'lr',
-        'momentum',
-        'muon',
-        'nadam',
-        'optimizer',
-        'radam',
-        'rmsprop',
-        'rprop',
-        'sgd',
-    ),
-)
-_set_module_alias(
-    'paddle.distributions',
-    distribution,
-    (
-        'bernoulli',
-        'beta',
-        'binomial',
-        'categorical',
-        'cauchy',
-        'chi2',
-        'constraint',
-        'continuous_bernoulli',
-        'dirichlet',
-        'distribution',
-        'exponential',
-        'exponential_family',
-        'gamma',
-        'geometric',
-        'gumbel',
-        'independent',
-        'kl',
-        'laplace',
-        'lkj_cholesky',
-        'lognormal',
-        'multinomial',
-        'multivariate_normal',
-        'normal',
-        'poisson',
-        'student_t',
-        'transform',
-        'transformed_distribution',
-        'uniform',
-        'variable',
-    ),
-)
-del _set_module_alias
+_sys.modules['paddle.optim'] = optimizer
+_sys.modules['paddle.distributions'] = distribution
 
 # high-level api
 from . import (
