60c6a60f2501768187c2121c07ceabcf29cc719a
algorithm1832 <737634857@qq.com>
2026-05-27 07:45:08 +0000

    fix param out

--- python/paddle/tensor/math.py
+++ python/paddle/tensor/math.py
@@ -1757,7 +1757,7 @@ def nansum(
         paddle.core.is_compiled_with_cuda()
         or paddle.core.is_compiled_with_rocm()
     ):
-        return _C_ops.nansum(x, axis, dtype, keepdim, out)
+        return _C_ops.nansum(x, axis, dtype, keepdim, out=out)
     else:
         zero_tensor = paddle.zeros_like(x)
         tmp_tensor = paddle.where(isnan(x), zero_tensor, x)
