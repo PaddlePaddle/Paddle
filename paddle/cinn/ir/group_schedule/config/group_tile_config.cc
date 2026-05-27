a6bc2588ab14fbaa6dc3f104eb9dd5a3c849294d
Rainchar9119 <1134601163@qq.com>
2026-05-27 15:26:40 +0800

    Update paddle/cinn/ir/group_schedule/config/group_tile_config.cc
    Co-authored-by: risemeup1111 <13125134909@163.com>

--- paddle/cinn/ir/group_schedule/config/group_tile_config.cc
+++ paddle/cinn/ir/group_schedule/config/group_tile_config.cc
@@ -1063,8 +1063,8 @@ TileConfigMap BuildStaticSpatialConfig(
     if (hw_max_bps > 0) blocks_per_sm = std::min(blocks_per_sm, hw_max_bps);
     blocks_per_sm = std::max(1, blocks_per_sm);
     int64_t rd_block_num = std::max(int64_t(1),
-        FloorPow2(static_cast<int64_t>(sm_count) * blocks_per_sm /
-                  sp_block_num));
+                                    FloorPow2(static_cast<int64_t>(sm_count) *
+                                              blocks_per_sm / sp_block_num));
 
     if (rd_block_num > 1 && base_info->can_apply_grid_reduce) {
       int64_t rd_threshold =
