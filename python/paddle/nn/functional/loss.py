72e139da04d615705fb44b93d329af59b1ffaed8
Nyakku Shigure <sigure.qaq@gmail.com>
2026-06-10 23:17:51 +0800

    Apply suggestions from code review
    Co-authored-by: Nyakku Shigure <sigure.qaq@gmail.com>

--- python/paddle/nn/functional/loss.py
+++ python/paddle/nn/functional/loss.py
@@ -2530,7 +2530,7 @@ def margin_cross_entropy(
             >>> num_class_per_card = [4, 8]
             >>> num_classes = paddle.sum(paddle.to_tensor(num_class_per_card))
 
-            >>> label = paddle.randint(low=0, high=int(num_classes.item()), size=[batch_size], dtype='int64')  # type: ignore[call-overload,arg-type]
+            >>> label = paddle.randint(low=0, high=int(num_classes.item()), size=[batch_size], dtype='int64')  # type: ignore[call-overload, arg-type]
             >>> label_list: List[paddle.Tensor] = []
             >>> dist.all_gather(label_list, label)
             >>> label = paddle.concat(label_list, axis=0)
