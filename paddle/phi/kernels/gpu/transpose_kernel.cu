30ee5a2a716d550677f361286377cfd7e4e60786
feixi139 <18210811610@163.com>
2026-07-07 11:27:07 +0000

    move fp8_fast_transpose_kernel to .cu to eliminate duplicate CUDA kernel registration

--- paddle/phi/kernels/gpu/transpose_kernel.cu
+++ paddle/phi/kernels/gpu/transpose_kernel.cu
@@ -24,6 +24,137 @@
 #include "paddle/phi/kernels/impl/transpose_grad_kernel_impl.h"
 
 namespace phi {
+namespace funcs {
+
+typedef struct alignas(8) fp8x8_t {
+  union data_t {
+    phi::float8_e4m3fn scalar[8];
+    uint2 vector;
+  };
+  data_t data;
+
+  __device__ __forceinline__ void load(const void* ptr) {
+    data = *reinterpret_cast<const data_t*>(ptr);
+  }
+
+  __device__ __forceinline__ void store(void* ptr) const {
+    *reinterpret_cast<data_t*>(ptr) = data;
+  }
+} fp8x8_t;
+
+static constexpr int kFp8VecSize = 8;
+static constexpr int kFp8BlockDim = 16;
+static constexpr int kFp8BlockTileSize = 128;
+static constexpr int kFp8BlockTileWidth = kFp8BlockTileSize;
+static constexpr int kFp8BlockTileHeight = kFp8BlockTileSize;
+static constexpr int kFp8ThreadTileDim = kFp8BlockTileSize / kFp8BlockDim;
+
+__global__ void __launch_bounds__(kFp8BlockDim* kFp8BlockDim)
+    fp8_fast_transpose_kernel(const phi::float8_e4m3fn* __restrict__ src,
+                              phi::float8_e4m3fn* __restrict__ dst,
+                              uint32_t B,
+                              uint32_t M,
+                              uint32_t N,
+                              size_t batch_stride) {
+  __shared__ __align__(1024)
+      fp8x8_t smem[kFp8BlockTileHeight][kFp8BlockTileWidth / kFp8VecSize + 1];
+
+  fp8x8_t local_tile[kFp8VecSize];
+  fp8x8_t local_tile_transposed[kFp8VecSize];
+
+  const uint32_t tid_x = threadIdx.x;
+  const uint32_t tid_y = threadIdx.y;
+
+  const uint32_t block_x = blockIdx.x;
+  const uint32_t block_y = blockIdx.y;
+  const uint32_t block_z = blockIdx.z;
+
+  const uint32_t global_m_offset = block_y * kFp8BlockTileHeight;
+  const uint32_t global_n_offset = block_x * kFp8BlockTileWidth;
+
+  const size_t current_batch_offset =
+      static_cast<size_t>(batch_stride) * block_z;
+
+#pragma unroll
+  for (uint32_t k = 0; k < kFp8ThreadTileDim; ++k) {
+    const uint32_t src_global_row =
+        global_m_offset + tid_y * kFp8ThreadTileDim + k;
+    const uint32_t src_global_col_start =
+        global_n_offset + tid_x * kFp8ThreadTileDim;
+
+    const phi::float8_e4m3fn* src_ptr =
+        src + current_batch_offset + static_cast<size_t>(src_global_row) * N +
+        src_global_col_start;
+    local_tile[k].load(src_ptr);
+  }
+
+#pragma unroll
+  for (uint32_t k_row = 0; k_row < kFp8ThreadTileDim; ++k_row) {
+#pragma unroll
+    for (uint32_t k_col = 0; k_col < kFp8ThreadTileDim; ++k_col) {
+      local_tile_transposed[k_col].data.scalar[k_row] =
+          local_tile[k_row].data.scalar[k_col];
+    }
+  }
+
+#pragma unroll
+  for (uint32_t k = 0; k < kFp8ThreadTileDim; ++k) {
+    const uint32_t smem_row = tid_x * kFp8ThreadTileDim + k;
+    const uint32_t smem_col_start = tid_y * kFp8ThreadTileDim / 8;
+    smem[smem_row][smem_col_start] = local_tile_transposed[k];
+  }
+
+  __syncthreads();
+
+#pragma unroll
+  for (uint32_t k = 0; k < kFp8ThreadTileDim; ++k) {
+    const uint32_t dst_global_row =
+        global_n_offset + tid_y * kFp8ThreadTileDim + k;
+    const uint32_t dst_global_col_start =
+        global_m_offset + tid_x * kFp8ThreadTileDim;
+
+    size_t offset = current_batch_offset +
+                    static_cast<size_t>(dst_global_row) * M +
+                    dst_global_col_start;
+    phi::float8_e4m3fn* dst_ptr = dst + offset;
+
+    fp8x8_t output_block;
+    const uint32_t smem_row = tid_y * kFp8ThreadTileDim + k;
+    const uint32_t smem_col = tid_x * kFp8ThreadTileDim / kFp8VecSize;
+    output_block = smem[smem_row][smem_col];
+    output_block.store(dst_ptr);
+  }
+}
+
+template <typename T, typename IndexType>
+void dispatch_fp8_fast_transpose_kernel(const GPUContext& d,
+                                        const T* input,
+                                        const uint32_t B,
+                                        const uint32_t M,
+                                        const uint32_t N,
+                                        T* output) {
+  dim3 grid, block;
+  block.x = kFp8BlockDim;
+  block.y = kFp8BlockDim;
+
+  grid.z = B;
+  grid.y = M / kFp8BlockTileSize;
+  grid.x = N / kFp8BlockTileSize;
+
+  fp8_fast_transpose_kernel<<<grid, block, 0, d.stream()>>>(
+      input, output, B, M, N, static_cast<size_t>(M) * static_cast<size_t>(N));
+}
+
+template void dispatch_fp8_fast_transpose_kernel<phi::float8_e4m3fn, int>(
+    const GPUContext& d,
+    const phi::float8_e4m3fn* input,
+    const uint32_t B,
+    const uint32_t M,
+    const uint32_t N,
+    phi::float8_e4m3fn* output);
+
+}  // namespace funcs
+
 template <typename T, typename Context>
 void TransposeKernel(const Context& dev_ctx,
                      const DenseTensor& x,
