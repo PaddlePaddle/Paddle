c002606a1d33ca2f3651eedc33912baca4e3cf12
ALGO1832 <737634857@qq.com>
2026-07-13 19:08:26 +0800

    [API Compatibility] `paddle.optim` related apis (#79341)
    * support kwarg maximize
    
    * add alias for AdamW, Adagrad, SGD
    
    * support lr_decay for Adagrad
    
    * add alias for Optimizer
    
    * remove uncapable test
    
    * add lr value test in lr_decay test and add default value test in maximize test
    
    * override _create_param_lr for lr_decay and update test
    
    * add test for LRScheduler+lr_decay case
    
    * include step in state_dict
    
    * add test for maximize with tensor_fusion
    
    * add param group test for maximize
    
    * support maximize in _ShardOptimizer
    
    * attempt fix maximize in _tensor_fusion
    
    * remove maximize process in _build_fuse_param_view
    
    * [edit by agent] attempt use ci to test maximize
    
    * revert maximize related changes
    
    * apply maximize in _create_param_lr
    
    * split test into two
    
    * revert to grad implementation for maximize
    
    * add test to verify maximize in distributed
    
    * fix test
    
    * update imports

--- python/paddle/optim/__init__.py
+++ python/paddle/optim/__init__.py
@@ -17,50 +17,42 @@ import sys as _sys
 from paddle.optimizer import (
     ASGD as ASGD,
     LBFGS as LBFGS,
-    SGD as SGD,
     Adadelta as Adadelta,
-    Adagrad as Adagrad,
     Adam as Adam,
     Adamax as Adamax,
-    AdamW as AdamW,
     Muon as Muon,
     NAdam as NAdam,
-    Optimizer as Optimizer,
     RAdam as RAdam,
     RMSProp as RMSProp,
     Rprop as Rprop,
     adadelta,
-    adagrad,
     adam,
     adamax,
-    adamw,
     asgd,
     lbfgs,
     muon,
     nadam,
-    optimizer,
     radam,
     rmsprop,
     rprop,
-    sgd,
 )
 
 from . import lr_scheduler  # noqa: F401
+from .adagrad import Adagrad as Adagrad
+from .adamw import AdamW as AdamW
+from .optimizer import Optimizer as Optimizer
+from .sgd import SGD as SGD
 
 _sys.modules['paddle.optim.adadelta'] = adadelta
-_sys.modules['paddle.optim.adagrad'] = adagrad
 _sys.modules['paddle.optim.adam'] = adam
 _sys.modules['paddle.optim.adamax'] = adamax
-_sys.modules['paddle.optim.adamw'] = adamw
 _sys.modules['paddle.optim.asgd'] = asgd
 _sys.modules['paddle.optim.lbfgs'] = lbfgs
 _sys.modules['paddle.optim.muon'] = muon
 _sys.modules['paddle.optim.nadam'] = nadam
-_sys.modules['paddle.optim.optimizer'] = optimizer
 _sys.modules['paddle.optim.radam'] = radam
 _sys.modules['paddle.optim.rmsprop'] = rmsprop
 _sys.modules['paddle.optim.rprop'] = rprop
-_sys.modules['paddle.optim.sgd'] = sgd
 
 __all__ = [
     "Adadelta",
---
+++ python/paddle/optim/optimizer.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2026 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from paddle.optimizer import Optimizer as PaddleOptimizer
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from paddle import Tensor
+    from paddle.optimizer.optimizer import _ParameterConfig
+
+
+class Optimizer(PaddleOptimizer):
+    def __init__(
+        self,
+        params: Sequence[Tensor] | Sequence[_ParameterConfig] | None,
+        defaults: dict[str, Any],
+    ) -> None:
+        lr = defaults.pop('lr', None)
+        learning_rate = defaults.pop('learning_rate', None)
+        if lr is not None and learning_rate is not None:
+            raise ValueError(
+                "Cannot specify both 'lr' and 'learning_rate' in defaults."
+            )
+        lr = lr if lr is not None else learning_rate
+
+        weight_decay = defaults.pop('weight_decay', None)
+        grad_clip = defaults.pop('grad_clip', None)
+        maximize = defaults.pop('maximize', False)
+
+        super().__init__(
+            learning_rate=lr,
+            parameters=params,
+            weight_decay=weight_decay,
+            grad_clip=grad_clip,
+            maximize=maximize,
+        )
